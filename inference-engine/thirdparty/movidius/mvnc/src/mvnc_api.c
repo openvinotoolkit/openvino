bdf98204cae6ab87d55be6c11ac9ae2e3d0187a9
Alexey Suhov <asuhov@users.noreply.github.com>
2019-05-28 15:40:53 +0300

    updated license headers in movidius sources (#163)

--- inference-engine/thirdparty/movidius/mvnc/src/mvnc_api.c
+++ inference-engine/thirdparty/movidius/mvnc/src/mvnc_api.c
@@ -1,20 +1,6 @@
-/*
-* Copyright 2017-2019 Intel Corporation.
-* The source code, information and material ("Material") contained herein is
-* owned by Intel Corporation or its suppliers or licensors, and title to such
-* Material remains with Intel Corporation or its suppliers or licensors.
-* The Material contains proprietary information of Intel or its suppliers and
-* licensors. The Material is protected by worldwide copyright laws and treaty
-* provisions.
-* No part of the Material may be used, copied, reproduced, modified, published,
-* uploaded, posted, transmitted, distributed or disclosed in any way without
-* Intel's prior express written permission. No license under any patent,
-* copyright or other intellectual property rights in the Material is granted to
-* or conferred upon you, either expressly, by implication, inducement, estoppel
-* or otherwise.
-* Any license under such intellectual property rights must be express and
-* approved by Intel in writing.
-*/
+// Copyright (C) 2018-2019 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
 
 #define _GNU_SOURCE
 
