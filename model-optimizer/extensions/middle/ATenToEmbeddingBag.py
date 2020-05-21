adab635f6f1d73e1d23dac7148334846b0415297
Maxim, Vafin <maxim.vafin@intel.com>
2020-05-21 14:46:59 +0300

    Fix per_sample_weights case

--- model-optimizer/extensions/middle/ATenToEmbeddingBag.py
+++ model-optimizer/extensions/middle/ATenToEmbeddingBag.py
@@ -14,12 +14,18 @@
  limitations under the License.
 """
 
+import numpy as np
+
 from extensions.ops.embedding_bag import EmbeddingBagOffsetsSum, EmbeddingBagPackedSum
-from mo.front.common.replacement import FrontReplacementPattern
+from mo.front.common.partial_infer.utils import int64_array
+from mo.front.tf.graph_utils import create_op_with_const_inputs
 from mo.graph.graph import Graph, rename_node
+from mo.middle.replacement import MiddleReplacementPattern
+from mo.ops.concat import Concat
+from mo.ops.const import Const
 
 
-class AtenToEmbeddingBag(FrontReplacementPattern):
+class AtenToEmbeddingBag(MiddleReplacementPattern):
     """
     Converts the ATen layer to EmbeddingBag layer.
     """
@@ -30,16 +36,34 @@ class AtenToEmbeddingBag(FrontReplacementPattern):
             assert node.soft_get('mode') == 0, 'ATen::embedding_bag has unsupported mode, only "sum" ' \
                                                'mode is supported for node {}.'.format(node.id)
             node_name = node.name
-            rename_node(node, node_name + '/Old')
-            if node.in_port(2).disconnected():
+            rename_node(node, node_name + '/TBR')
+            is_packed = False
+            if len(node.in_ports()) < 3 or node.in_port(2).disconnected():
+                is_packed = True
                 embedding_bag = EmbeddingBagPackedSum(graph, {'name': node_name}).create_node()
-                per_sample_weights_port_id = 2
             else:
                 embedding_bag = EmbeddingBagOffsetsSum(graph, {'name': node_name}).create_node()
-                per_sample_weights_port_id = 3
                 node.in_port(2).get_connection().set_destination(embedding_bag.in_port(2))
+            rename_node(embedding_bag, node_name)
             node.in_port(0).get_connection().set_destination(embedding_bag.in_port(0))
             node.in_port(1).get_connection().set_destination(embedding_bag.in_port(1))
             node.out_port(0).get_connection().set_source(embedding_bag.out_port(0))
             if len(node.in_ports()) == 4 and not node.in_port(3).disconnected():
-                node.in_port(3).get_connection().set_destination(embedding_bag.in_port(per_sample_weights_port_id))
+                if is_packed:
+                    node.in_port(3).get_connection().set_destination(embedding_bag.in_port(2))
+                else:
+                    # connect per_sample_weights
+                    node.in_port(3).get_connection().set_destination(embedding_bag.in_port(4))
+
+                    weights_shape = embedding_bag.in_port(0).data.get_shape()
+
+                    # expand embedding table with zeros
+                    default_embeddings = np.zeros([1, weights_shape[-1]])
+                    weights_concat = create_op_with_const_inputs(graph, Concat, {1: default_embeddings},
+                                                                 {'axis': 0, 'in_ports_count': 2})
+                    embedding_bag.in_port(0).get_connection().set_destination(weights_concat.in_port(0))
+                    weights_concat.out_port(0).connect(embedding_bag.in_port(0))
+
+                    # point default index to expanded part of embedding table
+                    default_index = Const(graph, {'value': int64_array(weights_shape[0])}).create_node()
+                    default_index.out_port(0).connect(embedding_bag.in_port(3))
---
+++ model-optimizer/extensions/middle/ATenToEmbeddingBag_test.py
@@ -0,0 +1,146 @@
+"""
+ Copyright (C) 2020 Intel Corporation
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
+
+import unittest
+
+import numpy as np
+
+from extensions.middle.ATenToEmbeddingBag import AtenToEmbeddingBag
+from mo.front.common.partial_infer.utils import int64_array
+from mo.utils.ir_engine.compare_graphs import compare_graphs
+from mo.utils.unittest.graph import build_graph, const, regular_op_with_shaped_data, valued_const_with_data, result, \
+    connect
+
+nodes_attributes = {
+    **const('weights_inp', np.random.randn(1000, 10)),
+    'indices_inp': {'shape': None, 'type': 'Parameter', 'kind': 'op', 'op': 'Parameter'},
+    'offsets_inp': {'shape': None, 'type': 'Parameter', 'kind': 'op', 'op': 'Parameter'},
+    **const('per_sample_weights', np.random.randn(20)),
+    'aten': {'type': None, 'kind': 'op', 'op': 'ATen', 'mode': 0, 'operator': 'embedding_bag', 'name': 'my_aten'},
+    'result': {'type': 'Result', 'value': None, 'kind': 'op', 'op': 'Result'},
+
+    # new EmbeddingBag layer
+    'emb_bag': {'type': 'EmbeddingBagOffsetsSum', 'kind': 'op', 'op': 'EmbeddingBagOffsetsSum'},
+}
+
+
+class AtenToEmbeddingBagTest(unittest.TestCase):
+    def test(self):
+        nodes = {
+            **valued_const_with_data('weights_inp', np.random.randn(100, 2)),
+            **regular_op_with_shaped_data('indices_inp', [20], {'type': 'Parameter'}),
+            **regular_op_with_shaped_data('offsets_inp', [10], {'type': 'Parameter'}),
+            **regular_op_with_shaped_data('aten', [10, 2],
+                                          {'type': None, 'kind': 'op', 'op': 'ATen', 'mode': 0,
+                                           'operator': 'embedding_bag', 'name': 'my_aten'}),
+
+            **regular_op_with_shaped_data('emb_bag', [10, 2], {'type': 'EmbeddingBagOffsetsSum', 'kind': 'op',
+                                                               'op': 'EmbeddingBagOffsetsSum'}),
+            **result('result'),
+        }
+        edges = [*connect('weights_inp', '0:aten'),
+                 *connect('indices_inp', '1:aten'),
+                 *connect('offsets_inp', '2:aten'),
+                 *connect('aten', 'result'),
+                 ]
+        graph = build_graph(nodes, edges)
+
+        edges_ref = [*connect('weights_inp', '0:emb_bag'),
+                     *connect('indices_inp', '1:emb_bag'),
+                     *connect('offsets_inp', '2:emb_bag'),
+                     *connect('emb_bag', 'result'),
+                     ]
+
+        graph_ref = build_graph(nodes, edges_ref)
+
+        AtenToEmbeddingBag().find_and_replace_pattern(graph)
+
+        (flag, resp) = compare_graphs(graph, graph_ref, 'result')
+        self.assertTrue(flag, resp)
+
+
+    def test_packed(self):
+        nodes = {
+            **valued_const_with_data('weights_inp', np.random.randn(100, 4)),
+            **regular_op_with_shaped_data('indices_inp', [10, 2], {'type': 'Parameter'}),
+            **regular_op_with_shaped_data('aten', [10, 4],
+                                          {'type': None, 'kind': 'op', 'op': 'ATen', 'mode': 0,
+                                           'operator': 'embedding_bag', 'name': 'my_aten'}),
+
+            **regular_op_with_shaped_data('emb_bag', [10, 4], {'type': 'EmbeddingBagPackedSum', 'kind': 'op',
+                                                               'op': 'EmbeddingBagPackedSum'}),
+            **result('result'),
+        }
+        edges = [*connect('weights_inp', '0:aten'),
+                 *connect('indices_inp', '1:aten'),
+                 *connect('aten', 'result'),
+                 ]
+        graph = build_graph(nodes, edges)
+
+        edges_ref = [*connect('weights_inp', '0:emb_bag'),
+                     *connect('indices_inp', '1:emb_bag'),
+                     *connect('emb_bag', 'result'),
+                     ]
+
+        graph_ref = build_graph(nodes, edges_ref)
+
+        AtenToEmbeddingBag().find_and_replace_pattern(graph)
+
+        (flag, resp) = compare_graphs(graph, graph_ref, 'result')
+        self.assertTrue(flag, resp)
+
+    def test_per_sample_weights(self):
+        nodes = {
+            **valued_const_with_data('weights_inp', np.random.randn(100, 2)),
+            **regular_op_with_shaped_data('indices_inp', [20], {'type': 'Parameter'}),
+            **regular_op_with_shaped_data('offsets_inp', [10], {'type': 'Parameter'}),
+            **regular_op_with_shaped_data('per_sample_weights', [20], {'type': 'Parameter'}),
+            **regular_op_with_shaped_data('aten', [10, 2],
+                                          {'type': None, 'kind': 'op', 'op': 'ATen', 'mode': 0,
+                                           'operator': 'embedding_bag', 'name': 'my_aten'}),
+
+            **regular_op_with_shaped_data('emb_bag', [10, 2], {'type': 'EmbeddingBagOffsetsSum', 'kind': 'op',
+                                                               'op': 'EmbeddingBagOffsetsSum'}),
+            **valued_const_with_data('zeros', np.zeros([1, 2])),
+            **regular_op_with_shaped_data('concat', None, {'type': 'Concat', 'kind': 'op', 'op': 'Concat'}),
+            'def_index': {'kind': 'op', 'value': int64_array(100), 'shape': None, 'type': 'Const'},
+            'def_index_d': {'kind': 'data', 'value': None, 'shape': None},
+            **result('result'),
+        }
+        edges = [*connect('weights_inp', '0:aten'),
+                 *connect('indices_inp', '1:aten'),
+                 *connect('offsets_inp', '2:aten'),
+                 *connect('per_sample_weights', '3:aten'),
+                 *connect('aten', 'result'),
+                 ]
+        graph = build_graph(nodes, edges)
+
+        edges_ref = [*connect('weights_inp', '0:concat'),
+                     *connect('zeros', '1:concat'),
+                     *connect('concat', '0:emb_bag'),
+                     *connect('indices_inp', '1:emb_bag'),
+                     *connect('offsets_inp', '2:emb_bag'),
+                     *connect('def_index', '3:emb_bag'),
+                     *connect('per_sample_weights', '4:emb_bag'),
+                     *connect('emb_bag', 'result'),
+                     ]
+
+        graph_ref = build_graph(nodes, edges_ref)
+
+        AtenToEmbeddingBag().find_and_replace_pattern(graph)
+
+        (flag, resp) = compare_graphs(graph, graph_ref, 'result')
+        self.assertTrue(flag, resp)
