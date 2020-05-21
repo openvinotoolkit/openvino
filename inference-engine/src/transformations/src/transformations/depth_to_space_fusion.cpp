df9d7b364cbe92b3d5752f3a4ff15f7e45965d62
Ivan Tikhonov <ivan.tikhonov@intel.com>
2020-05-21 13:11:34 +0300

    renaming

--- inference-engine/src/transformations/src/transformations/depth_to_space_fusion.cpp
+++ inference-engine/src/transformations/src/transformations/depth_to_space_fusion.cpp
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "transformations/fused_depth_to_space.hpp"
+#include "transformations/depth_to_space_fusion.hpp"
 
 #include <memory>
 #include <vector>
