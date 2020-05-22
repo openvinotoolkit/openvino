f4a30ffbd5b1272cf649c830447ed883c2109b01
Efode, Irina <irina.efode@intel.com>
2020-05-22 19:01:43 +0300

    [IE TESTS] Fix Win

--- inference-engine/tests/ie_test_utils/functional_test_utils/plugin_cache.hpp
+++ inference-engine/tests/ie_test_utils/functional_test_utils/plugin_cache.hpp
@@ -4,23 +4,27 @@
 
 #pragma once
 
+#include <memory>
+#include <mutex>
 #include <string>
 
 #include <ie_core.hpp>
 
 class PluginCache {
 public:
-    static PluginCache& get();
+    std::shared_ptr<InferenceEngine::Core> ie(const std::string &deviceToCheck = std::string());
 
-    std::shared_ptr<InferenceEngine::Core> ie(const std::string &deviceToCheck = std::string()) const;
+    static PluginCache& get();
 
     void reset();
 
-public:
     PluginCache(const PluginCache&) = delete;
     PluginCache& operator=(const PluginCache&) = delete;
 
 private:
     PluginCache();
     ~PluginCache() = default;
+
+    std::mutex g_mtx;
+    std::shared_ptr<InferenceEngine::Core> ie_core;
 };
