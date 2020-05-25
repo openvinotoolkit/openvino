d8c59804d8a0457c38d555767987b1223ac95922
Efode, Irina <irina.efode@intel.com>
2020-05-25 13:07:02 +0300

    [IE TESTS] Move ie_blob_proxy tests

--- inference-engine/tests/unit/inference_engine/ie_blob_proxy.cpp
+++ inference-engine/tests/unit/inference_engine/ie_blob_proxy.cpp
@@ -2,11 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include <ie_blob_proxy.hpp>
+
 #include <gtest/gtest.h>
 #include <gmock/gmock-spec-builders.h>
-
-#include <ie_blob.h>
-#include <ie_blob_proxy.hpp>
 
 #include "unit_test_utils/mocks/mock_allocator.hpp"
 
@@ -28,10 +27,6 @@ protected:
     shared_ptr<MockAllocator> createMockAllocator() {
         return shared_ptr<MockAllocator>(new MockAllocator());
     }
-
-
-public:
-
 };
 
 TEST_F(BlobProxyTests, convertByteBlobToFloat) {
@@ -39,7 +34,7 @@ TEST_F(BlobProxyTests, convertByteBlobToFloat) {
     float test_array[size] = {2.2f, 3.5f, 1.1f, 0.0f};
     TBlob<uint8_t>::Ptr b(new TBlob<uint8_t>({ Precision::U8, {size * sizeof(float)}, C }));
     b->allocate();
-    uint8_t *sPtr = (uint8_t *) test_array;
+    uint8_t *sPtr = reinterpret_cast<uint8_t *>(test_array);
     uint8_t *dPtr = b->data();
     ASSERT_EQ(b->size(), size * sizeof(float));
     for (size_t i = 0; i < b->size(); i++) {
@@ -47,8 +42,7 @@ TEST_F(BlobProxyTests, convertByteBlobToFloat) {
     }
 }
 
-TEST_F(BlobProxyTests, shouldNotDeAllocate)
-{
+TEST_F(BlobProxyTests, shouldNotDeAllocate) {
     SizeVector v = {1, 2, 3};
     auto allocator = createMockAllocator();
 
@@ -64,15 +58,14 @@ TEST_F(BlobProxyTests, shouldNotDeAllocate)
 }
 
 
-TEST_F(BlobProxyTests, canAccessProxyBlobUsingBaseMethod)
-{
+TEST_F(BlobProxyTests, canAccessProxyBlobUsingBaseMethod) {
     SizeVector v = {1, 2, 3};
     auto allocator = createMockAllocator();
 
-    float data[] = {5,6,7,8,9,10};
+    float data[] = {5, 6, 7, 8, 9, 10};
 
-    EXPECT_CALL(*allocator.get(), alloc(1 * 2 * 3 * sizeof(float))).WillRepeatedly(Return((void*)1));
-    EXPECT_CALL(*allocator.get(), lock(_,LOCK_FOR_WRITE)).WillRepeatedly(Return(data));
+    EXPECT_CALL(*allocator.get(), alloc(1 * 2 * 3 * sizeof(float))).WillRepeatedly(Return(reinterpret_cast<void*>(1) ));
+    EXPECT_CALL(*allocator.get(), lock(_, LOCK_FOR_WRITE)).WillRepeatedly(Return(data));
     EXPECT_CALL(*allocator.get(), unlock(_)).Times(1);
     EXPECT_CALL(*allocator.get(), free(_)).Times(1);
 
@@ -86,20 +79,19 @@ TEST_F(BlobProxyTests, canAccessProxyBlobUsingBaseMethod)
     TBlobProxy<float> proxy(Precision::FP32, C, spBlob, 2, {2});
 
     auto proxyBuffer = proxy.buffer();
-    float *ptr = (float*)(void*)proxyBuffer;
+    float *ptr = proxyBuffer.as<float*>();
     ASSERT_EQ(ptr[2] , 9);
 }
 
-TEST_F(BlobProxyTests, canAccessProxyBlobUsingHelpers)
-{
+TEST_F(BlobProxyTests, canAccessProxyBlobUsingHelpers) {
     SizeVector v = {1, 2, 3};
     auto allocator = createMockAllocator();
 
-    float data[] = {5,6,7, 8, 9, 10};
+    float data[] = {5, 6, 7, 8, 9, 10};
 
-    EXPECT_CALL(*allocator.get(), alloc(1 * 2 * 3 * sizeof(float))).WillRepeatedly(Return((void*)1));
-    EXPECT_CALL(*allocator.get(), lock(_,LOCK_FOR_WRITE)).WillOnce(Return(data));
-    EXPECT_CALL(*allocator.get(), lock(_,LOCK_FOR_READ)).WillOnce(Return(data));
+    EXPECT_CALL(*allocator.get(), alloc(1 * 2 * 3 * sizeof(float))).WillRepeatedly(Return(reinterpret_cast<void*>(1)));
+    EXPECT_CALL(*allocator.get(), lock(_, LOCK_FOR_WRITE)).WillOnce(Return(data));
+    EXPECT_CALL(*allocator.get(), lock(_, LOCK_FOR_READ)).WillOnce(Return(data));
     EXPECT_CALL(*allocator.get(), unlock(_)).Times(2);
     EXPECT_CALL(*allocator.get(), free(_)).Times(1);
 
@@ -113,30 +105,29 @@ TEST_F(BlobProxyTests, canAccessProxyBlobUsingHelpers)
     TBlobProxy<float> proxy(Precision::FP32, C, spBlob, 2, {2});
 
     auto proxyData = proxy.data();
-    float *ptr = (float * )&proxyData[0];
+    float *ptr = reinterpret_cast<float*>(&proxyData[0]);
     ASSERT_EQ(ptr[2] , 9);
 
     auto readOnly = proxy.readOnly();
-    ptr = (float * )&readOnly[0];
+    ptr = readOnly.as<float*>();
     ASSERT_EQ(ptr[2] , 9);
 }
 
-TEST_F(BlobProxyTests, canCreateProxyBlobFromDifferentBaseBlobType)
-{
+TEST_F(BlobProxyTests, canCreateProxyBlobFromDifferentBaseBlobType) {
     SizeVector v = {1, 2, 3};
     auto allocator = createMockAllocator();
 
     uint8_t data[] = {5, 6, 7, 8, 9, 10};
 
-    EXPECT_CALL(*allocator.get(), alloc(1 * 2 * 3 * sizeof(uint8_t))).WillRepeatedly(Return((void*)1));
-    EXPECT_CALL(*allocator.get(), lock(_,LOCK_FOR_READ)).WillOnce(Return(data));
+    EXPECT_CALL(*allocator.get(), alloc(1 * 2 * 3 * sizeof(uint8_t))).WillRepeatedly(Return(reinterpret_cast<void*>(1)));
+    EXPECT_CALL(*allocator.get(), lock(_, LOCK_FOR_READ)).WillOnce(Return(data));
     EXPECT_CALL(*allocator.get(), unlock(_)).Times(1);
     EXPECT_CALL(*allocator.get(), free(_)).Times(1);
 
     TBlob<uint8_t > blob({ Precision::U8, v, CHW }, dynamic_pointer_cast<IAllocator>(allocator));
     blob.allocate();
 
-    Blob::Ptr spBlob (&blob, [](Blob*){
+    Blob::Ptr spBlob(&blob, [](Blob*){
         //don't delete
     });
 
@@ -153,13 +144,13 @@ TEST_F(BlobProxyTests, canNotCreateBlobWithOffsetOfSizeOutOfOriginal) {
     SizeVector v = {1, 1, 3};
     auto allocator = createMockAllocator();
 
-    EXPECT_CALL(*allocator.get(), alloc(1 * 1 * 3 * sizeof(float))).WillRepeatedly(Return((void*)1));
+    EXPECT_CALL(*allocator.get(), alloc(1 * 1 * 3 * sizeof(float))).WillRepeatedly(Return(reinterpret_cast<void*>(1)));
     EXPECT_CALL(*allocator.get(), free(_)).Times(1);
 
     TBlob<float> blob({ Precision::FP32, v, CHW }, dynamic_pointer_cast<IAllocator>(allocator));
     blob.allocate();
 
-    Blob::Ptr spBlob (&blob, [](Blob*){
+    Blob::Ptr spBlob(&blob, [](Blob*){
         //don't delete
     });
 
@@ -169,29 +160,28 @@ TEST_F(BlobProxyTests, canNotCreateBlobWithOffsetOfSizeOutOfOriginal) {
 }
 
 
-TEST_F(BlobProxyTests, canAccessBothArraysAfterProxying)
-{
+TEST_F(BlobProxyTests, canAccessBothArraysAfterProxying) {
     SizeVector v = {1, 2, 4};
     auto allocator = createMockAllocator();
 
     uint8_t data[] = {5, 6, 7, 8, 9, 10, 11, 12};
 
-    EXPECT_CALL(*allocator.get(), alloc(1 * 2 * 4 * sizeof(uint8_t))).WillRepeatedly(Return((void*)1));
-    EXPECT_CALL(*allocator.get(), lock(_,LOCK_FOR_READ)).Times(2).WillRepeatedly(Return(data));
+    EXPECT_CALL(*allocator.get(), alloc(1 * 2 * 4 * sizeof(uint8_t))).WillRepeatedly(Return(reinterpret_cast<void*>(1)));
+    EXPECT_CALL(*allocator.get(), lock(_, LOCK_FOR_READ)).Times(2).WillRepeatedly(Return(data));
     EXPECT_CALL(*allocator.get(), unlock(_)).Times(2);
     EXPECT_CALL(*allocator.get(), free(_)).Times(1);
 
     TBlob<uint8_t> blob({ Precision::U8, v, CHW }, dynamic_pointer_cast<IAllocator>(allocator));
     blob.allocate();
 
-    Blob::Ptr spBlob (&blob, [](Blob*){
+    Blob::Ptr spBlob(&blob, [](Blob*){
         //don't delete
     });
 
     TBlobProxy<short> proxy(Precision::I16, C, spBlob, 2, {3});
 
     auto readOnly = proxy.readOnly();
-    short * ptr = (short * )&readOnly[0];
+    short* ptr = readOnly.as<short*>();
     ASSERT_EQ(ptr[0] , MAKE_SHORT(data[2], data[3]));
     ASSERT_EQ(ptr[1] , MAKE_SHORT(data[4], data[5]));
 
@@ -200,7 +190,6 @@ TEST_F(BlobProxyTests, canAccessBothArraysAfterProxying)
 
     ASSERT_EQ(origPtr[0] , 5);
     ASSERT_EQ(origPtr[1] , 6);
-
 }
 
 TEST_F(BlobProxyTests, convertTwoByteBlobToFloat) {
@@ -208,11 +197,11 @@ TEST_F(BlobProxyTests, convertTwoByteBlobToFloat) {
     float test_array[size] = {2.2f, 3.5f, 1.1f, 0.0f};
     TBlob<uint16_t>::Ptr b(new TBlob<uint16_t>(TensorDesc(Precision::U16, {size*sizeof(float) / sizeof(uint16_t)}, C)));
     b->allocate();
-    uint16_t *sPtr = (uint16_t *) test_array;
+    uint16_t *sPtr = reinterpret_cast<uint16_t *>(test_array);
     uint16_t *dPtr = b->data();
     ASSERT_EQ(b->byteSize(), size*sizeof(float));
     ASSERT_EQ(b->size(), size*sizeof(float)/sizeof(uint16_t));
-    for(size_t i = 0; i < b->size(); i++) {
+    for (size_t i = 0; i < b->size(); i++) {
         dPtr[i] = sPtr[i];
     }
 
@@ -237,7 +226,7 @@ TEST_F(BlobProxyTests, throwsIfSmallProxyObjectSize) {
         TBlobProxy<uint8_t> proxy(Precision::U8, C, b, 0, { b->byteSize() + 1 });
         FAIL() << "Should have failed by now: proxy size is larger than blob size";
     }
-    catch (InferenceEngine::details::InferenceEngineException ex) {};
+    catch (InferenceEngine::details::InferenceEngineException ex) {}
 }
 
 TEST_F(BlobProxyTests, canReturnConstantData) {
