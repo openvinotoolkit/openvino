8ea28d482e63e45e2f24646fd4d8bf1de2a34ed6
Mironov <iliya.mironov@intel.com>
2020-05-25 15:56:06 +0300

    Add softplus support

---
+++ model-optimizer/extensions/front/tf/softplus_ext.py
@@ -0,0 +1,32 @@
+"""
+ Copyright (C) 2020 Intel Corporation
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
+
+from mo.front.extractor import FrontExtractorOp
+from mo.ops.softmax import LogSoftmax, Softmax
+
+
+class SoftmaxExtractor(FrontExtractorOp):
+    op = 'Softplus'
+    enabled = True
+
+    @classmethod
+    def extract(cls, node):
+        # the default value for the TF Softmax is -1
+        # axis = -1
+        # if 'axis' in node.pb.attr:
+        #     axis = node.pb.attr['axis'].i
+        # Softmax.update_node_stat(node, {'axis': axis})
+        return cls.enabled
