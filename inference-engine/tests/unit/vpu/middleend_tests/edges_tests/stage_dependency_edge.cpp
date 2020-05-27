5e02edf73086f4485359ba9331e1ace69d75c6bc
Andrew Bakalin <andrew.bakalin@intel.com>
2020-05-27 11:14:02 +0300

    [VPU][GT] Refine edges methods (#550)
    * [VPU][GT] Extract order manipulation into separate methods
    
    * [VPU][GT] Rename data -> dependency
    
    * [VPU][GT] Extend unit tests
    
    * [VPU][GT] Introduce replacement and removal methods for StageDependency
    
    * [VPU][GT] Update DataToShape connection methods

--- inference-engine/tests/unit/vpu/middleend_tests/edges_tests/stage_dependency_edge.cpp
+++ inference-engine/tests/unit/vpu/middleend_tests/edges_tests/stage_dependency_edge.cpp
@@ -15,14 +15,53 @@ protected:
 
         ASSERT_NO_FATAL_FAILURE(InitCompileEnv());
 
-        _middleEnd = passManager->buildMiddleEnd();
         _testModel = CreateTestModel();
     }
 
 protected:
     TestModel _testModel;
-    PassSet::Ptr _middleEnd = nullptr;
 };
+
+TEST_F(StageDependencyEdgeProcessingTests, AddStageDependencyAssertsOnNetworkInput) {
+    //
+    //                    -> [Output]
+    // [Input] -> (Stage)
+    //                    -> [Output]
+    //
+
+    const DataDesc desc{1};
+
+    _testModel.createInputs({desc});
+    _testModel.createOutputs({desc, desc});
+
+    auto stage = _testModel.addStage({InputInfo::fromNetwork()}, {OutputInfo::fromNetwork(0),
+                                                                  OutputInfo::fromNetwork(1)});
+
+    auto model = _testModel.getBaseModel();
+
+    ASSERT_ANY_THROW(model->addStageDependency(stage, _testModel.getInputs().front()));
+}
+
+TEST_F(StageDependencyEdgeProcessingTests, AddStageDependencyAssertsOnStageInput) {
+    //
+    //                    -> [Output]
+    // [Input] -> (Stage)
+    //                    -> [Data] -> (Stage) -> [Output]
+    //
+
+    const DataDesc desc{1};
+
+    _testModel.createInputs({desc});
+    _testModel.createOutputs({desc, desc});
+
+    _testModel.addStage({InputInfo::fromNetwork()}, {OutputInfo::intermediate(desc),
+                                                     OutputInfo::fromNetwork(1)});
+    auto stage = _testModel.addStage({InputInfo::fromPrevStage(0).output(0)}, {OutputInfo::fromNetwork(0)});
+
+    auto model = _testModel.getBaseModel();
+
+    ASSERT_ANY_THROW(model->addStageDependency(stage, stage->input(0)));
+}
 
 TEST_F(StageDependencyEdgeProcessingTests, AddStageDependencyDoesNotAssertOnOutputData) {
     //
@@ -44,6 +83,29 @@ TEST_F(StageDependencyEdgeProcessingTests, AddStageDependencyDoesNotAssertOnOutp
     auto model = _testModel.getBaseModel();
 
     ASSERT_NO_THROW(model->addStageDependency(dependentStage, dependencyProducer->output(0)));
+}
+
+TEST_F(StageDependencyEdgeProcessingTests, AddStageDependencyAssertsIfDependencyExists) {
+    //
+    //                    -> [Data] -> (Stage) -> [Output]
+    // [Input] -> (Stage)                            |
+    //                    -> [Data] ------------> (Stage) -> [Output]
+    //
+
+    const DataDesc desc{1};
+
+    _testModel.createInputs({desc});
+    _testModel.createOutputs({desc, desc});
+
+    _testModel.addStage({InputInfo::fromNetwork()}, {OutputInfo::intermediate(desc),
+                                                     OutputInfo::intermediate(desc)});
+    auto dependentStage = _testModel.addStage({InputInfo::fromPrevStage(0).output(0)}, {OutputInfo::fromNetwork(0)});
+    auto dependencyProducer = _testModel.addStage({InputInfo::fromPrevStage(0).output(1)}, {OutputInfo::fromNetwork(1)});
+
+    auto model = _testModel.getBaseModel();
+
+    ASSERT_NO_THROW(model->addStageDependency(dependentStage, dependencyProducer->output(0)));
+    ASSERT_ANY_THROW(model->addStageDependency(dependentStage, dependencyProducer->output(0)));
 }
 
 TEST_F(StageDependencyEdgeProcessingTests, NetWithTwoStagesHasCorrectExecOrder) {
@@ -99,6 +161,246 @@ TEST_F(StageDependencyEdgeProcessingTests, NetWithThreeStagesHasCorrectExecOrder
     ASSERT_NO_THROW(model->addStageDependency(dependentStage, dependencyProducer->output(0)));
 
     ASSERT_TRUE(checkExecutionOrder(model, {dependencyProducer->id(), dependentStage->id()}));
+}
+
+TEST_F(StageDependencyEdgeProcessingTests, ReplaceStageDependencyAssertsOnNetworkInput) {
+    //
+    //                    -> [Data] -> (Stage) -> [Output]
+    // [Input] -> (Stage)                            |
+    //                    -> [Data] ------------> (Stage) -> [Output]
+    //
+
+    const DataDesc desc{1};
+
+    _testModel.createInputs({desc});
+    _testModel.createOutputs({desc, desc});
+
+    _testModel.addStage({InputInfo::fromNetwork()}, {OutputInfo::intermediate(desc),
+                                                     OutputInfo::intermediate(desc)});
+    auto dependentStage = _testModel.addStage({InputInfo::fromPrevStage(0).output(0)}, {OutputInfo::fromNetwork(0)});
+    auto dependencyProducer = _testModel.addStage({InputInfo::fromPrevStage(0).output(1)}, {OutputInfo::fromNetwork(1)});
+
+    auto model = _testModel.getBaseModel();
+
+    ASSERT_NO_THROW(model->addStageDependency(dependentStage, dependencyProducer->output(0)));
+
+    const auto edge = dependencyProducer->output(0)->dependentStagesEdges().front();
+
+    ASSERT_ANY_THROW(model->replaceStageDependency(edge, _testModel.getInputs().front()));
+}
+
+TEST_F(StageDependencyEdgeProcessingTests, ReplaceStageDependencyAssertsOnStageInput) {
+    //
+    //                    -> [Data] -> (Stage) -> [Output]
+    // [Input] -> (Stage)                            |
+    //                    -> [Data] ------------> (Stage) -> [Output]
+    //
+
+    const DataDesc desc{1};
+
+    _testModel.createInputs({desc});
+    _testModel.createOutputs({desc, desc});
+
+    _testModel.addStage({InputInfo::fromNetwork()}, {OutputInfo::intermediate(desc),
+                                                     OutputInfo::intermediate(desc)});
+    auto dependentStage = _testModel.addStage({InputInfo::fromPrevStage(0).output(0)}, {OutputInfo::fromNetwork(0)});
+    auto dependencyProducer = _testModel.addStage({InputInfo::fromPrevStage(0).output(1)}, {OutputInfo::fromNetwork(1)});
+
+    auto model = _testModel.getBaseModel();
+
+    ASSERT_NO_THROW(model->addStageDependency(dependentStage, dependencyProducer->output(0)));
+
+    const auto edge = dependencyProducer->output(0)->dependentStagesEdges().front();
+
+    ASSERT_ANY_THROW(model->replaceStageDependency(edge, dependentStage->input(0)));
+}
+
+TEST_F(StageDependencyEdgeProcessingTests, ReplaceStageDependencyAssertsIfDependencyExists) {
+    //
+    //                    -> [Data] -> (Stage) -> [Output]
+    // [Input] -> (Stage)                            |
+    //                    -> [Data] ------------> (Stage) -> [Output]
+    //
+
+    const DataDesc desc{1};
+
+    _testModel.createInputs({desc});
+    _testModel.createOutputs({desc, desc});
+
+    _testModel.addStage({InputInfo::fromNetwork()}, {OutputInfo::intermediate(desc),
+                                                     OutputInfo::intermediate(desc)});
+    auto dependentStage = _testModel.addStage({InputInfo::fromPrevStage(0).output(0)}, {OutputInfo::fromNetwork(0)});
+    auto dependencyProducer = _testModel.addStage({InputInfo::fromPrevStage(0).output(1)}, {OutputInfo::fromNetwork(1)});
+
+    auto model = _testModel.getBaseModel();
+
+    ASSERT_NO_THROW(model->addStageDependency(dependentStage, dependencyProducer->output(0)));
+
+    const auto edge = dependencyProducer->output(0)->dependentStagesEdges().front();
+
+    ASSERT_ANY_THROW(model->replaceStageDependency(edge, dependencyProducer->output(0)));
+}
+
+TEST_F(StageDependencyEdgeProcessingTests, ReplaceStageDependencyReplacesConnection) {
+    //
+    //                    -> [Data] -> (Stage) -> [Output]
+    //
+    // [Input] -> (Stage) -> [Data] -> (Stage) -> [Output]
+    //                                               |
+    //                    -> [Data] ------------> (Stage) -> [Output]
+    //
+
+    const DataDesc desc{1};
+
+    _testModel.createInputs({desc});
+    _testModel.createOutputs({desc, desc, desc});
+
+    _testModel.addStage({InputInfo::fromNetwork()}, {OutputInfo::intermediate(desc),
+                                                     OutputInfo::intermediate(desc),
+                                                     OutputInfo::intermediate(desc)});
+    auto dependentStage = _testModel.addStage({InputInfo::fromPrevStage(0).output(0)}, {OutputInfo::fromNetwork(0)});
+    auto initialDependencyProducer = _testModel.addStage({InputInfo::fromPrevStage(0).output(1)}, {OutputInfo::fromNetwork(1)});
+    auto resultDependencyProducer = _testModel.addStage({InputInfo::fromPrevStage(0).output(2)}, {OutputInfo::fromNetwork(2)});
+
+    auto model = _testModel.getBaseModel();
+
+    ASSERT_NO_THROW(model->addStageDependency(dependentStage, initialDependencyProducer->output(0)));
+
+    ASSERT_TRUE(checkExecutionOrder(model, {initialDependencyProducer->id(), dependentStage->id()}));
+    ASSERT_TRUE(checkExecutionOrder(model, {dependentStage->id(), resultDependencyProducer->id()}));
+
+    const auto edge = initialDependencyProducer->output(0)->dependentStagesEdges().front();
+
+    ASSERT_NO_THROW(model->replaceStageDependency(edge, resultDependencyProducer->output(0)));
+
+    ASSERT_EQ(initialDependencyProducer->output(0)->dependentStagesEdges().size(), 0);
+    ASSERT_EQ(edge->dependency(), resultDependencyProducer->output(0));
+
+    ASSERT_TRUE(checkExecutionOrder(model, {resultDependencyProducer->id(), dependentStage->id()}));
+}
+
+TEST_F(StageDependencyEdgeProcessingTests, ReplaceDependentStageAssertsOnStageInput) {
+    //
+    //                    -----------> [Data] -> (Stage) -> [Output]
+    // [Input] -> (Stage)                 |
+    //                    -> [Data] -> (Stage) -> [Output]
+    //
+
+    const DataDesc desc{1};
+
+    _testModel.createInputs({desc});
+    _testModel.createOutputs({desc, desc});
+
+    auto dependencyProducer = _testModel.addStage({InputInfo::fromNetwork()}, {OutputInfo::intermediate(desc),
+                                                     OutputInfo::intermediate(desc)});
+    auto initialDependentStage = _testModel.addStage({InputInfo::fromPrevStage(0).output(0)}, {OutputInfo::fromNetwork(0)});
+    auto resultDependentStage = _testModel.addStage({InputInfo::fromPrevStage(0).output(1)}, {OutputInfo::fromNetwork(1)});
+
+    auto model = _testModel.getBaseModel();
+
+    ASSERT_NO_THROW(model->addStageDependency(initialDependentStage, dependencyProducer->output(1)));
+
+    const auto edge = dependencyProducer->output(1)->dependentStagesEdges().front();
+
+    ASSERT_ANY_THROW(model->replaceDependentStage(edge, resultDependentStage));
+}
+
+TEST_F(StageDependencyEdgeProcessingTests, ReplaceDependentStageAssertsIfDependencyExists) {
+    //
+    //                    -> [Data] -> (Stage) -> [Output]
+    // [Input] -> (Stage)                            |
+    //                    -> [Data] ------------> (Stage) -> [Output]
+    //
+
+    const DataDesc desc{1};
+
+    _testModel.createInputs({desc});
+    _testModel.createOutputs({desc, desc});
+
+    _testModel.addStage({InputInfo::fromNetwork()}, {OutputInfo::intermediate(desc),
+                                                     OutputInfo::intermediate(desc)});
+    auto dependentStage = _testModel.addStage({InputInfo::fromPrevStage(0).output(0)}, {OutputInfo::fromNetwork(0)});
+    auto dependencyProducer = _testModel.addStage({InputInfo::fromPrevStage(0).output(1)}, {OutputInfo::fromNetwork(1)});
+
+    auto model = _testModel.getBaseModel();
+
+    ASSERT_NO_THROW(model->addStageDependency(dependentStage, dependencyProducer->output(0)));
+
+    const auto edge = dependencyProducer->output(0)->dependentStagesEdges().front();
+
+    ASSERT_ANY_THROW(model->replaceDependentStage(edge, dependentStage));
+}
+
+TEST_F(StageDependencyEdgeProcessingTests, ReplaceDependentStageReplacesConnection) {
+    //
+    //                    -> [Data] -> (Stage) -> [Output]
+    //
+    // [Input] -> (Stage) -> [Data] -> (Stage) -> [Output]
+    //                                                |
+    //                    -> [Data] ------------> (Stage) -> [Output]
+    //
+
+    const DataDesc desc{1};
+
+    _testModel.createInputs({desc});
+    _testModel.createOutputs({desc, desc, desc});
+
+    _testModel.addStage({InputInfo::fromNetwork()}, {OutputInfo::intermediate(desc),
+                                                     OutputInfo::intermediate(desc),
+                                                     OutputInfo::intermediate(desc)});
+    auto initialDependentStage = _testModel.addStage({InputInfo::fromPrevStage(0).output(0)}, {OutputInfo::fromNetwork(0)});
+    auto resultDependentStage = _testModel.addStage({InputInfo::fromPrevStage(0).output(1)}, {OutputInfo::fromNetwork(1)});
+    auto dependencyProducer = _testModel.addStage({InputInfo::fromPrevStage(0).output(2)}, {OutputInfo::fromNetwork(2)});
+
+    auto model = _testModel.getBaseModel();
+
+    ASSERT_NO_THROW(model->addStageDependency(initialDependentStage, dependencyProducer->output(0)));
+
+    ASSERT_TRUE(checkExecutionOrder(model, {dependencyProducer->id(), initialDependentStage->id()}));
+    ASSERT_TRUE(checkExecutionOrder(model, {resultDependentStage->id(), dependencyProducer->id()}));
+
+    const auto edge = dependencyProducer->output(0)->dependentStagesEdges().front();
+
+    ASSERT_NO_THROW(model->replaceDependentStage(edge, resultDependentStage));
+
+    ASSERT_EQ(edge->dependentStage(), resultDependentStage);
+
+    ASSERT_TRUE(checkExecutionOrder(model, {dependencyProducer->id(), resultDependentStage->id()}));
+}
+
+TEST_F(StageDependencyEdgeProcessingTests, RemoveStageDependencyUpdatesNextPrevStages) {
+    //
+    //                    -> [Data] -> (Stage) -> [Output]
+    // [Input] -> (Stage)                            |
+    //                    -> [Data] ------------> (Stage) -> [Output]
+    //
+
+    const DataDesc desc{1};
+
+    _testModel.createInputs({desc});
+    _testModel.createOutputs({desc, desc});
+
+    _testModel.addStage({InputInfo::fromNetwork()}, {OutputInfo::intermediate(desc),
+                                                     OutputInfo::intermediate(desc)});
+    auto dependentStage = _testModel.addStage({InputInfo::fromPrevStage(0).output(0)}, {OutputInfo::fromNetwork(0)});
+    auto dependencyProducer = _testModel.addStage({InputInfo::fromPrevStage(0).output(1)}, {OutputInfo::fromNetwork(1)});
+
+    auto model = _testModel.getBaseModel();
+
+    ASSERT_NO_THROW(model->addStageDependency(dependentStage, dependencyProducer->output(0)));
+
+    const auto edge = dependencyProducer->output(0)->dependentStagesEdges().front();
+
+    ASSERT_NO_THROW(model->removeStageDependency(edge));
+
+    const auto prevStages = dependentStage->prevStages();
+    const auto nextStages = dependencyProducer->prevStages();
+
+    auto it = std::find(prevStages.begin(), prevStages.end(), dependencyProducer);
+    ASSERT_EQ(it, prevStages.end());
+
+    it = std::find(nextStages.begin(), nextStages.end(), dependentStage);
+    ASSERT_EQ(it, nextStages.end());
 }
 
 } // namespace vpu
