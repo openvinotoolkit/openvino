47b1e661dfdc7aa0241942b209e6eb469b556f99
Egor Churaev <egor.churaev@gmail.com>
2020-05-27 11:47:16 +0300

    [IE CLDNN] Implement CumSum operation (#533)
    CumSum performs cumulative summation of the input elements along the given axis.
    
    Details:
    By default, it will do the sum inclusively meaning the first element is
    copied as is. Through an "exclusive" attribute, this behavior can change
    to exclude the first element. It can also perform summation in the
    opposite direction of the axis. For that, set "reverse" attribute to
    true.
    
    JIRA: 29994

---
+++ inference-engine/tests/functional/plugin/gpu/shared_tests_instances/single_layer_tests/cum_sum.cpp
@@ -0,0 +1,48 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <vector>
+
+#include "single_layer_tests/cum_sum.hpp"
+#include "common_test_utils/test_constants.hpp"
+
+using namespace LayerTestsDefinitions;
+
+namespace {
+
+std::vector<std::vector<size_t>> inShapes = {
+        {10, 10},
+        {10, 10, 10},
+        {10, 10, 10, 10},
+        {10, 10, 10, 10, 10},
+        {10, 10, 10, 10, 10, 10},
+};
+std::vector<int64_t> axes = {-1, 0, 1};
+std::vector<bool> exclusive = {false, true};
+std::vector<bool> reverse = {false, true};
+std::vector<InferenceEngine::Precision> precisions = {InferenceEngine::Precision::FP32,
+                                                      InferenceEngine::Precision::FP16};
+
+std::vector<std::vector<size_t>> shape1d = {{10}};
+std::vector<int64_t> axis1d = {0};
+INSTANTIATE_TEST_CASE_P(CumSum1D, CumSumLayerTest,
+                        ::testing::Combine(
+                                ::testing::ValuesIn(shape1d),
+                                ::testing::ValuesIn(precisions),
+                                ::testing::ValuesIn(axis1d),
+                                ::testing::ValuesIn(exclusive),
+                                ::testing::ValuesIn(reverse),
+                                ::testing::Values(CommonTestUtils::DEVICE_GPU)),
+                        CumSumLayerTest::getTestCaseName);
+
+INSTANTIATE_TEST_CASE_P(CumSum, CumSumLayerTest,
+                        ::testing::Combine(
+                                ::testing::ValuesIn(inShapes),
+                                ::testing::ValuesIn(precisions),
+                                ::testing::ValuesIn(axes),
+                                ::testing::ValuesIn(exclusive),
+                                ::testing::ValuesIn(reverse),
+                                ::testing::Values(CommonTestUtils::DEVICE_GPU)),
+                        CumSumLayerTest::getTestCaseName);
+}  // namespace
