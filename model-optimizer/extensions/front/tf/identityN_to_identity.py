ce94a63222acf322be8b8d13a02fa123c2569a3f
Evgenya Stepyreva <evgenya.stepyreva@intel.com>
2020-05-29 09:11:22 +0300

    [ MO: CVS-32286 ] IdentityN fix (#668)

--- model-optimizer/extensions/front/tf/identityN_to_identity.py
+++ model-optimizer/extensions/front/tf/identityN_to_identity.py
@@ -29,6 +29,11 @@ class IdentityN_to_Identity(FrontReplacementPattern):
         IdentityN                   Identity    Identity
         /       \                       |           |
     output_0    output_1            output_0    output_1
+
+    ATTENTION: not all in/outputs of the IdentityN may survive during ModelOptimizer pipeline.
+    And it breaks the original operation semantics.
+    For example, output_1 may be not be used during network output computations.
+    To preserve this unused in/output ports we disconnect the corresponding out/input port.
     """
     enabled = True
 
@@ -41,11 +46,19 @@ class IdentityN_to_Identity(FrontReplacementPattern):
         dtypes = node.data_types
 
         for idx, port in node.in_ports().items():
-            assert node.is_out_port_connected(idx), 'IdentityN {} has inconsistent input and output ports'.format(name)
+            if not node.is_in_port_connected(idx) or not node.is_out_port_connected(idx):
+                # ATTENTION section in the description above
+                continue
             assert idx < len(dtypes), 'IdentityN {} has inconsistent `data_types` attribute {}'.format(name, dtypes)
             identity = Identity(graph, {'name': '{}/{}_port'.format(name, idx), 'data_type': dtypes[idx]}).create_node()
             port.get_connection().set_destination(identity.in_port(0))
             node.out_port(idx).get_connection().set_source(identity.out_port(0))
+
+        # ATTENTION section in the description above
+        for in_port in node.in_ports().values():
+            in_port.disconnect()
+        for out_port in node.out_ports().values():
+            out_port.disconnect()
 
     def find_and_replace_pattern(self, graph: Graph):
         for identityN in graph.get_op_nodes(op='IdentityN'):
