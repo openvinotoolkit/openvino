a93c028acc22f0ab23c29a9bef0a250e3123cf6f
emmanuelattia-philips <66060489+emmanuelattia-philips@users.noreply.github.com>
2020-05-31 01:25:39 +0200

    Added ie_core_read_network_from_memory to the C API ie_bridge. (#674)
    * * Added ie_core_read_network_from_memory to the C ie_bridge.
    
    * Added size argument for xml_content, fixed const correctness of the weight_blob, fixed unit test
    
    * * Removed debug message
    
    * Changed variables names from model_xxx to weights_xxx to be more consistent with the argument name of the tested function.
    
    * Added a description for xml_content_size in ie_core_read_network_from_memory.
    
    * * xml_content is now passed as uint8_t
    * reading function factorized in the unit-test

--- inference-engine/ie_bridges/c/src/ie_c_api.cpp
+++ inference-engine/ie_bridges/c/src/ie_c_api.cpp
@@ -310,6 +310,28 @@ IEStatusCode ie_core_read_network(ie_core_t *core, const char *xml, const char *
     return status;
 }
 
+IEStatusCode ie_core_read_network_from_memory(ie_core_t *core, const uint8_t *xml_content, size_t xml_content_size, \
+        const ie_blob_t *weight_blob, ie_network_t **network) {
+    if (core == nullptr || xml_content == nullptr || network == nullptr || weight_blob == nullptr) {
+        return IEStatusCode::GENERAL_ERROR;
+    }
+
+    IEStatusCode status = IEStatusCode::OK;
+
+    try {
+        std::unique_ptr<ie_network_t> network_result(new ie_network_t);
+        network_result->object = core->object.ReadNetwork(std::string(reinterpret_cast<const char *>(xml_content),
+            reinterpret_cast<const char *>(xml_content + xml_content_size)), weight_blob->object);
+        *network = network_result.release();
+    } catch (const IE::details::InferenceEngineException& e) {
+        return e.hasStatus() ? status_map[e.getStatus()] : IEStatusCode::UNEXPECTED;
+    } catch (...) {
+        return IEStatusCode::UNEXPECTED;
+    }
+
+    return status;
+}
+
 IEStatusCode ie_core_load_network(ie_core_t *core, const ie_network_t *network, const char *device_name, \
         const ie_config_t *config, ie_executable_network_t **exe_network) {
     IEStatusCode status = IEStatusCode::OK;
