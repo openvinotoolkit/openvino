fc194f257ef4bfb5a1a80aa11208153fc38415da
Ilya Lavrenov <ilya.lavrenov@intel.com>
2020-06-01 20:10:09 +0300

    Re-use new execution graph in tests

--- inference-engine/tests/functional/plugin/shared/src/behavior/exec_graph_info.cpp
+++ inference-engine/tests/functional/plugin/shared/src/behavior/exec_graph_info.cpp
@@ -9,6 +9,8 @@
 
 #include <ie_core.hpp>
 #include <details/ie_cnn_network_tools.h>
+#include <ngraph/function.hpp>
+#include <ngraph/variant.hpp>
 
 #include "common_test_utils/common_utils.hpp"
 #include "functional_test_utils/plugin_cache.hpp"
@@ -83,25 +85,56 @@ TEST_P(ExecGraphTests, CheckExecGraphInfoBeforeExecution) {
             originalLayersMap[layer->get_friendly_name()] = 0;
         }
         int IteratorForLayersConstant = 0;
-        // Store all the layers from the executable graph information represented as CNNNetwork
-        const std::vector<InferenceEngine::CNNLayerPtr> execGraphLayers =
-                InferenceEngine::details::CNNNetSortTopologically(execGraph);
-        for (const auto &execLayer : execGraphLayers) {
-            IE_SUPPRESS_DEPRECATED_START
-            // Each layer from the execGraphInfo network must have PM data option set
-            ASSERT_EQ("not_executed", execLayer->params[ExecGraphInfoSerialization::PERF_COUNTER]);
-            // Parse origin layer names (fused/merged layers) from the executable graph
-            // and compare with layers from the original model
-            auto origFromExecLayer = execLayer->params[ExecGraphInfoSerialization::ORIGINAL_NAMES];
-            if (origFromExecLayer == "")
-                IteratorForLayersConstant++;
-            std::vector<std::string> origFromExecLayerSep = separateStrToVec(origFromExecLayer, ',');
-            std::for_each(origFromExecLayerSep.begin(), origFromExecLayerSep.end(), [&](const std::string &layer) {
-                auto origLayer = originalLayersMap.find(layer);
-                ASSERT_NE(originalLayersMap.end(), origLayer) << layer;
-                origLayer->second++;
-            });
+
+        if (auto function = execGraph.getFunction()) {
+            for (const auto & op : function->get_ops()) {
+                const auto & rtInfo = op->get_rt_info();
+
+                auto getExecValue = [&rtInfo](const std::string & paramName) -> std::string {
+                    auto it = rtInfo.find(paramName);
+                    IE_ASSERT(rtInfo.end() != it);
+                    auto value = std::dynamic_pointer_cast<ngraph::VariantImpl<std::string>>(it->second);
+                    IE_ASSERT(nullptr != value);
+
+                    return value->get();
+                };
+
+                // Each layer from the execGraphInfo network must have PM data option set
+                ASSERT_EQ("not_executed", getExecValue(ExecGraphInfoSerialization::PERF_COUNTER));
+                // Parse origin layer names (fused/merged layers) from the executable graph
+                // and compare with layers from the original model
+                auto origFromExecLayer = getExecValue(ExecGraphInfoSerialization::ORIGINAL_NAMES);
+                if (origFromExecLayer == "")
+                    IteratorForLayersConstant++;
+                std::vector<std::string> origFromExecLayerSep = separateStrToVec(origFromExecLayer, ',');
+                std::for_each(origFromExecLayerSep.begin(), origFromExecLayerSep.end(), [&](const std::string &layer) {
+                    auto origLayer = originalLayersMap.find(layer);
+                    ASSERT_NE(originalLayersMap.end(), origLayer) << layer;
+                    origLayer->second++;
+                });
+            }
+        } else {
+            // Store all the layers from the executable graph information represented as CNNNetwork
+            const std::vector<InferenceEngine::CNNLayerPtr> execGraphLayers =
+                    InferenceEngine::details::CNNNetSortTopologically(execGraph);
+            for (const auto &execLayer : execGraphLayers) {
+                IE_SUPPRESS_DEPRECATED_START
+                // Each layer from the execGraphInfo network must have PM data option set
+                ASSERT_EQ("not_executed", execLayer->params[ExecGraphInfoSerialization::PERF_COUNTER]);
+                // Parse origin layer names (fused/merged layers) from the executable graph
+                // and compare with layers from the original model
+                auto origFromExecLayer = execLayer->params[ExecGraphInfoSerialization::ORIGINAL_NAMES];
+                if (origFromExecLayer == "")
+                    IteratorForLayersConstant++;
+                std::vector<std::string> origFromExecLayerSep = separateStrToVec(origFromExecLayer, ',');
+                std::for_each(origFromExecLayerSep.begin(), origFromExecLayerSep.end(), [&](const std::string &layer) {
+                    auto origLayer = originalLayersMap.find(layer);
+                    ASSERT_NE(originalLayersMap.end(), origLayer) << layer;
+                    origLayer->second++;
+                });
+            }
         }
+
         // All layers from the original IR must be present with in ExecGraphInfo
         for (auto &layer : originalLayersMap) {
             if ((layer.second == 0) && (IteratorForLayersConstant > 0)) {
@@ -141,31 +174,68 @@ TEST_P(ExecGraphTests, CheckExecGraphInfoAfterExecution) {
         }
         int IteratorForLayersConstant = 0;
         // Store all the layers from the executable graph information represented as CNNNetwork
-        const std::vector<InferenceEngine::CNNLayerPtr> execGraphLayers =
-                InferenceEngine::details::CNNNetSortTopologically(execGraph);
         bool has_layer_with_valid_time = false;
-        for (const auto &execLayer : execGraphLayers) {
-            IE_SUPPRESS_DEPRECATED_START
-            // At least one layer in the topology should be executed and have valid perf counter value
-            try {
-                float x = static_cast<float>(std::atof(
-                        execLayer->params[ExecGraphInfoSerialization::PERF_COUNTER].c_str()));
-                ASSERT_GE(x, 0.0f);
-                has_layer_with_valid_time = true;
-            } catch (std::exception &) {}
 
-            // Parse origin layer names (fused/merged layers) from the executable graph
-            // and compare with layers from the original model
-            auto origFromExecLayer = execLayer->params[ExecGraphInfoSerialization::ORIGINAL_NAMES];
-            std::vector<std::string> origFromExecLayerSep = separateStrToVec(origFromExecLayer, ',');
-            if (origFromExecLayer == "")
-                IteratorForLayersConstant++;
-            std::for_each(origFromExecLayerSep.begin(), origFromExecLayerSep.end(), [&](const std::string &layer) {
-                auto origLayer = originalLayersMap.find(layer);
-                ASSERT_NE(originalLayersMap.end(), origLayer) << layer;
-                origLayer->second++;
-            });
+        if (auto function = execGraph.getFunction()) {
+            for (const auto & op : function->get_ops()) {
+                const auto & rtInfo = op->get_rt_info();
+
+                auto getExecValue = [&rtInfo](const std::string & paramName) -> std::string {
+                    auto it = rtInfo.find(paramName);
+                    IE_ASSERT(rtInfo.end() != it);
+                    auto value = std::dynamic_pointer_cast<ngraph::VariantImpl<std::string>>(it->second);
+                    IE_ASSERT(nullptr != value);
+
+                    return value->get();
+                };
+
+                // At least one layer in the topology should be executed and have valid perf counter value
+                try {
+                    float x = static_cast<float>(std::atof(
+                            getExecValue(ExecGraphInfoSerialization::PERF_COUNTER).c_str()));
+                    ASSERT_GE(x, 0.0f);
+                    has_layer_with_valid_time = true;
+                } catch (std::exception &) {}
+
+                // Parse origin layer names (fused/merged layers) from the executable graph
+                // and compare with layers from the original model
+                auto origFromExecLayer = getExecValue(ExecGraphInfoSerialization::ORIGINAL_NAMES);
+                std::vector<std::string> origFromExecLayerSep = separateStrToVec(origFromExecLayer, ',');
+                if (origFromExecLayer == "")
+                    IteratorForLayersConstant++;
+                std::for_each(origFromExecLayerSep.begin(), origFromExecLayerSep.end(), [&](const std::string &layer) {
+                    auto origLayer = originalLayersMap.find(layer);
+                    ASSERT_NE(originalLayersMap.end(), origLayer) << layer;
+                    origLayer->second++;
+                });
+            }
+        } else {
+            const std::vector<InferenceEngine::CNNLayerPtr> execGraphLayers =
+                InferenceEngine::details::CNNNetSortTopologically(execGraph);
+            for (const auto &execLayer : execGraphLayers) {
+                IE_SUPPRESS_DEPRECATED_START
+                // At least one layer in the topology should be executed and have valid perf counter value
+                try {
+                    float x = static_cast<float>(std::atof(
+                            execLayer->params[ExecGraphInfoSerialization::PERF_COUNTER].c_str()));
+                    ASSERT_GE(x, 0.0f);
+                    has_layer_with_valid_time = true;
+                } catch (std::exception &) {}
+
+                // Parse origin layer names (fused/merged layers) from the executable graph
+                // and compare with layers from the original model
+                auto origFromExecLayer = execLayer->params[ExecGraphInfoSerialization::ORIGINAL_NAMES];
+                std::vector<std::string> origFromExecLayerSep = separateStrToVec(origFromExecLayer, ',');
+                if (origFromExecLayer == "")
+                    IteratorForLayersConstant++;
+                std::for_each(origFromExecLayerSep.begin(), origFromExecLayerSep.end(), [&](const std::string &layer) {
+                    auto origLayer = originalLayersMap.find(layer);
+                    ASSERT_NE(originalLayersMap.end(), origLayer) << layer;
+                    origLayer->second++;
+                });
+            }
         }
+
         ASSERT_TRUE(has_layer_with_valid_time);
 
         // All layers from the original IR must be present within ExecGraphInfo
