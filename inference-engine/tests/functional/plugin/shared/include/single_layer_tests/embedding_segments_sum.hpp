bc92c8d5dcb47f996c583d3c5caabd1df9cb2027
Nikolay Shchegolev <nikolay.shchegolev@intel.com>
2020-06-02 21:56:17 +0300

    [CPU] EmbeddingBagOffsetsSum, EmbeddingBagPackedSum, EmbeddingSegmentsSum operations. (#576)
    * [CPU] EmbeddingBagOffsetsSum, EmbeddingBagPackedSum, EmbeddingSegmentsSum operations.
    
    * Performance fix
    
    * Perf v2
    
    * Code style

---
+++ inference-engine/tests/functional/plugin/shared/include/single_layer_tests/embedding_segments_sum.hpp
@@ -0,0 +1,40 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <tuple>
+#include <vector>
+#include <string>
+
+#include "functional_test_utils/layer_test_utils.hpp"
+
+typedef std::tuple<
+        std::vector<size_t>, // emb_table_shape
+        std::vector<size_t>, // indices
+        std::vector<size_t>, // segment_ids
+        size_t,              // num_segments
+        size_t,              // default_index
+        bool,                // with_weights
+        bool                 // with_def_index
+    > embeddingSegmentsSumParams;
+
+typedef std::tuple<
+        embeddingSegmentsSumParams,
+        InferenceEngine::Precision, // embedding table
+        InferenceEngine::Precision, // indices
+        LayerTestsUtils::TargetDevice> embeddingSegmentsSumLayerTestParamsSet;
+
+namespace LayerTestsDefinitions {
+
+class EmbeddingSegmentsSumLayerTest : public testing::WithParamInterface<embeddingSegmentsSumLayerTestParamsSet>,
+            public LayerTestsUtils::LayerTestsCommon {
+public:
+    static std::string getTestCaseName(testing::TestParamInfo<embeddingSegmentsSumLayerTestParamsSet> obj);
+
+protected:
+    void SetUp() override;
+};
+
+}  // namespace LayerTestsDefinitions
