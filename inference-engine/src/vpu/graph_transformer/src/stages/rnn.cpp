081a76f6958a8d5bde213290b06aee85d35b533e
DariaMityagina <daria.mityagina@intel.com>
2020-06-04 17:29:25 +0300

    Duplicated code is deleted. nCells and nBatches are initialized to zero.

--- inference-engine/src/vpu/graph_transformer/src/stages/rnn.cpp
+++ inference-engine/src/vpu/graph_transformer/src/stages/rnn.cpp
@@ -131,8 +131,8 @@ void FrontEnd::parseRNN(const Model& model, const ie::CNNLayerPtr& _layer, const
     Data weights, biases;
     std::tie(weights, biases) = getWeightsAndBiases(model, layer);
 
-    size_t nCells = inputs[0]->desc().dim(Dim::H);
-    size_t nBatches = inputs[0]->desc().dim(Dim::C);
+    size_t nCells = 0;
+    size_t nBatches = 0;
     size_t inputSize = inputs[0]->desc().dim(Dim::W);
 
     if (layer->axis == 1) {
@@ -164,7 +164,7 @@ void FrontEnd::parseRNN(const Model& model, const ie::CNNLayerPtr& _layer, const
     IE_ASSERT(stateSize * ngates == biasesSize);
 
     /* weights repacking */
-    const auto generator = [&weights, stateSize, inputSize, ngates](const ie::Blob::Ptr& blob) {
+    const auto generator = [&weights, stateSize, inputSize, ngates, outputs](const ie::Blob::Ptr& blob) {
         auto newWeightsPtr = blob->buffer().as<fp16_t*>();
 
         auto content = weights->content();
@@ -185,57 +185,29 @@ void FrontEnd::parseRNN(const Model& model, const ie::CNNLayerPtr& _layer, const
     };
 
     auto newWeights = model->addConstData(_layer->name + "@weights", weights->desc(), generator);
-
-    if (outputs.size() == 1) {
-        auto stateCellFinal = model->addFakeData();
-        auto stage = model->addNewStage<LSTMCellStage>(
-            layer->name,
-            StageType::LSTMCell,
-            layer,
-            {inputs[0], inputs[1], inputs[2], newWeights, biases},
-            {outputs[0], stateCellFinal});
-        if (nCells > 1)
-            model->addTempBuffer(stage, DataDesc({stateSize}));
-
-        bool RNNForward = layer->direction == ie::RNNSequenceLayer::FWD;
-        stage->attrs().set<bool>("RNNForward", RNNForward);
-        stage->attrs().set<int>("nCells", nCells);
-        stage->attrs().set<int>("nBatches", nBatches);
-    }
+    auto stateCellFinal = model->addFakeData();
+    auto oututKeeper = {outputs[0], stateCellFinal};
 
     if (outputs.size() == 2) {
-        auto stage = model->addNewStage<LSTMCellStage>(
+        oututKeeper = {outputs[0], outputs[1]};
+    } else if (outputs.size() == 3) {
+        oututKeeper = {outputs[0], outputs[1], outputs[2]};
+    }
+
+    auto stage = model->addNewStage<LSTMCellStage>(
         layer->name,
         StageType::LSTMCell,
         layer,
         {inputs[0], inputs[1], inputs[2], newWeights, biases},
-        {outputs[0], outputs[1]});
+        oututKeeper);
 
-        if (nCells > 1)
-            model->addTempBuffer(stage, DataDesc({stateSize}));
+    if (nCells > 1)
+        model->addTempBuffer(stage, DataDesc({stateSize}));
 
-        bool RNNForward = layer->direction == ie::RNNSequenceLayer::FWD;
-        stage->attrs().set<bool>("RNNForward", RNNForward);
-        stage->attrs().set<int>("nCells", nCells);
-        stage->attrs().set<int>("nBatches", nBatches);
-    }
-
-    if (outputs.size() == 3) {
-        auto stage = model->addNewStage<LSTMCellStage>(
-        layer->name,
-        StageType::LSTMCell,
-        layer,
-        {inputs[0], inputs[1], inputs[2], newWeights, biases},
-        {outputs[0], outputs[1], outputs[2]});
-
-        if (nCells > 1)
-            model->addTempBuffer(stage, DataDesc({stateSize}));
-
-        bool RNNForward = layer->direction == ie::RNNSequenceLayer::FWD;
-        stage->attrs().set<bool>("RNNForward", RNNForward);
-        stage->attrs().set<int>("nCells", nCells);
-        stage->attrs().set<int>("nBatches", nBatches);
-    }
+    bool RNNForward = layer->direction == ie::RNNSequenceLayer::FWD;
+    stage->attrs().set<bool>("RNNForward", RNNForward);
+    stage->attrs().set<int>("nCells", nCells);
+    stage->attrs().set<int>("nBatches", nBatches);
 }
 
 void FrontEnd::parseLSTMCell(const Model& model, const ie::CNNLayerPtr& _layer, const DataVector &inputs, const DataVector &outputs) {
