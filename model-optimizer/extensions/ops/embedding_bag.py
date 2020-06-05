a296137724bce4c7a1b1578fcd0025c6a363ba05
Maxim, Vafin <maxim.vafin@intel.com>
2020-06-05 15:55:36 +0300

    Improve error messages

--- model-optimizer/extensions/ops/embedding_bag.py
+++ model-optimizer/extensions/ops/embedding_bag.py
@@ -54,16 +54,16 @@ class EmbeddingBagOffsetsSum(EmbeddingBagBase):
         name = node.soft_get('name', node.id)
 
         connected_in_ports = {idx: port for idx, port in node.in_ports().items() if not port.disconnected()}
-        assert len(connected_in_ports) >= 3 and 0 in connected_in_ports and 1 in connected_in_ports and \
-               2 in connected_in_ports, "EmbeddingBag should have at least 3 connected input port, but it doesn't " \
-                                        "for node: `{}`. Ports: {}".format(name, connected_in_ports)
+        assert len(connected_in_ports) >= 3 and all(p in connected_in_ports for p in [0, 1, 2]), \
+            "EmbeddingBagOffsetsSum should have at least 3 connected input port, but it doesn't " \
+            "for node: `{}`. Ports: {}".format(name, connected_in_ports)
 
         weights_shape = node.in_port(0).data.get_shape()
-        assert len(weights_shape) >= 2
-        input_shape = node.in_port(1).data.get_shape()
-        assert input_shape is not None
+        assert len(weights_shape) >= 2,\
+            "EmbeddingBagOffsetsSum should have at least 2D weights for node: `{}`".format(name)
         offsets_shape = node.in_port(2).data.get_shape()
-        assert offsets_shape is not None and len(offsets_shape) == 1
+        assert offsets_shape is not None and len(offsets_shape) == 1,\
+            "Rank of the offsets in EmbeddingBagOffsetsSum should be equal to 1 for node: `{}`".format(name)
 
         node.out_port(0).data.set_shape(np.concatenate((offsets_shape[:1], weights_shape[1:])))
 
@@ -78,14 +78,14 @@ class EmbeddingBagPackedSum(EmbeddingBagBase):
         name = node.soft_get('name', node.id)
 
         connected_in_ports = {idx: port for idx, port in node.in_ports().items() if not port.disconnected()}
-        assert len(connected_in_ports) >= 2 and 0 in connected_in_ports and 1 in connected_in_ports, \
+        assert len(connected_in_ports) >= 2 and all(p in connected_in_ports for p in [0, 1]), \
             "EmbeddingBagPackedSum should have at least 2 connected input port, but it doesn't for node: `{}`. " \
             "Ports: {}".format(name, connected_in_ports)
 
         weights_shape = node.in_port(0).data.get_shape()
-        assert len(weights_shape) >= 2
+        assert len(weights_shape) >= 2, \
+            "EmbeddingBagPackedSum should have at least 2D weights for node: `{}`".format(name)
         input_shape = node.in_port(1).data.get_shape()
-        assert input_shape is not None
 
         node.out_port(0).data.set_shape(np.concatenate((input_shape[:1], weights_shape[1:])))
 
@@ -100,16 +100,17 @@ class EmbeddingSegmentsSum(EmbeddingBagBase):
         name = node.soft_get('name', node.id)
 
         connected_in_ports = {idx: port for idx, port in node.in_ports().items() if not port.disconnected()}
-        assert len(connected_in_ports) >= 4 and 0 in connected_in_ports and 1 in connected_in_ports and \
-               2 in connected_in_ports and 3 in connected_in_ports, \
+        assert len(connected_in_ports) >= 4 and all(p in connected_in_ports for p in [0, 1, 2, 3]), \
             "EmbeddingSegmentsSum should have at least 4 connected input port, but it doesn't for node: `{}`. " \
             "Ports: {}".format(name, connected_in_ports)
 
         weights_shape = node.in_port(0).data.get_shape()
-        assert len(weights_shape) >= 2
+        assert len(weights_shape) >= 2,\
+            "EmbeddingSegmentsSum should have at least 2D weights for node: `{}`".format(name)
         indices_shape = node.in_port(1).data.get_shape()
         segment_ids = node.in_port(2).data.get_shape()
-        assert len(indices_shape) == 1 and len(segment_ids) == 1 and indices_shape == segment_ids
+        assert len(indices_shape) == 1 and len(segment_ids) == 1 and indices_shape == segment_ids,\
+            "Both indices and segment_ids should have the same shape for node: `{}`".format(name)
         num_segments = node.in_port(3).data.get_value()
         assert num_segments is not None, "EmbeddingSegmentsSum should have a constant num_segments provided, but it " \
                                          "doesn't for node: `{}`.".format(name)
