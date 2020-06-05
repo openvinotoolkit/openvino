e50a47b930c90a943e2b33cdbbf305ba23e194de
Egor Churaev <egor.churaev@intel.com>
2020-06-05 10:16:27 +0300

    [IE CLDNN] Implement NormalizeL2 int8 kernels (#720)

--- inference-engine/thirdparty/clDNN/tests/test_cases/fusings_gpu_test.cpp
+++ inference-engine/thirdparty/clDNN/tests/test_cases/fusings_gpu_test.cpp
@@ -85,6 +85,17 @@ struct gemm_test_params {
     size_t expected_not_fused_primitives;
 };
 
+struct normalize_test_params {
+    tensor in_shape;
+    data_types data_type;
+    format input_format;
+    data_types default_type;
+    format default_format;
+    bool across_spatial;
+    size_t expected_fused_primitives;
+    size_t expected_not_fused_primitives;
+};
+
 template<typename T>
 class BaseFusingTest : public ::testing::TestWithParam<T> {
 public:
@@ -429,6 +440,8 @@ public:
 
 #define CASE_GEMM_2IN_U8S8_1 {{1, 1, 4, 2}, {1, 1, 8, 4}}, tensor{1}, tensor{0}, data_types::u8,  data_types::i8,  data_types::u8, format::bfyx, data_types::f32, format::bfyx
 #define CASE_GEMM_2IN_S8U8_1 {{1, 2, 64, 128}, {1, 2, 256, 64}}, tensor{1}, tensor{0}, data_types::i8,  data_types::u8,  data_types::u8, format::bfyx, data_types::f32, format::bfyx
+
+#define CASE_NORMALIZE_I8_1 {1, 2, 3, 3}, data_types::u8, format::bfyx, data_types::f32, format::bfyx
 
 /* ----------------------------------------------------------------------------------------------------- */
 /* ---------------------------------------- FP32 convolution cases ------------------------------------- */
@@ -4161,7 +4174,7 @@ INSTANTIATE_TEST_CASE_P(fusings_gpu, permute_activation_scale_eltwise,
                             permute_params{CASE_PERMUTE_F32_5, 2, 5},
                             permute_params{CASE_PERMUTE_F32_6, 2, 5},
                             permute_params{CASE_PERMUTE_F32_7, 2, 5},
-                            
+
                             permute_params{CASE_PERMUTE_F16_0, 2, 5},
                             permute_params{CASE_PERMUTE_F16_1, 2, 5},
                             permute_params{CASE_PERMUTE_F16_2, 2, 5},
@@ -4169,34 +4182,34 @@ INSTANTIATE_TEST_CASE_P(fusings_gpu, permute_activation_scale_eltwise,
                             permute_params{CASE_PERMUTE_F16_4, 2, 5},
                             permute_params{CASE_PERMUTE_F16_5, 2, 5},
                             permute_params{CASE_PERMUTE_F16_6, 2, 5},
-                            
+
                             permute_params{CASE_PERMUTE_S8_0, 2, 5},
                             permute_params{CASE_PERMUTE_S8_1, 2, 5},
                             permute_params{CASE_PERMUTE_S8_2, 2, 5},
                             permute_params{CASE_PERMUTE_S8_3, 2, 5},
-                                                                 
+
                             permute_params{CASE_PERMUTE_U8_0, 2, 5},
                             permute_params{CASE_PERMUTE_U8_1, 2, 5},
                             permute_params{CASE_PERMUTE_U8_2, 2, 5},
                             permute_params{CASE_PERMUTE_U8_3, 2, 5},
-                            
+
                             permute_params{CASE_PERMUTE_F32_3D_0, 2, 5},
                             permute_params{CASE_PERMUTE_F32_3D_1, 2, 5},
                             permute_params{CASE_PERMUTE_F32_3D_2, 2, 5},
                             permute_params{CASE_PERMUTE_F32_3D_3, 2, 5},
                             permute_params{CASE_PERMUTE_F32_3D_4, 2, 5},
-                                
+
                             permute_params{CASE_PERMUTE_F16_3D_0, 2, 5},
                             permute_params{CASE_PERMUTE_F16_3D_1, 2, 5},
                             permute_params{CASE_PERMUTE_F16_3D_2, 2, 5},
                             permute_params{CASE_PERMUTE_F16_3D_3, 2, 5},
                             permute_params{CASE_PERMUTE_F16_3D_4, 2, 5},
-                            
+
                             permute_params{CASE_PERMUTE_S8_3D_0, 2, 5},
                             permute_params{CASE_PERMUTE_S8_3D_1, 2, 5},
                             permute_params{CASE_PERMUTE_S8_3D_2, 2, 5},
                             permute_params{CASE_PERMUTE_S8_3D_3, 2, 5},
-                                                                 
+
                             permute_params{CASE_PERMUTE_U8_3D_0, 2, 5},
                             permute_params{CASE_PERMUTE_U8_3D_1, 2, 5},
                             permute_params{CASE_PERMUTE_U8_3D_2, 2, 5},
@@ -4235,7 +4248,7 @@ INSTANTIATE_TEST_CASE_P(fusings_gpu, permute_scale_eltwise_quant_u8,
                             permute_params{CASE_PERMUTE_F32_5, 2, 5},
                             permute_params{CASE_PERMUTE_F32_6, 2, 5},
                             permute_params{CASE_PERMUTE_F32_7, 2, 5},
-                                                                  
+
                             permute_params{CASE_PERMUTE_F16_0, 2, 5},
                             permute_params{CASE_PERMUTE_F16_1, 2, 5},
                             permute_params{CASE_PERMUTE_F16_2, 2, 5},
@@ -4248,18 +4261,18 @@ INSTANTIATE_TEST_CASE_P(fusings_gpu, permute_scale_eltwise_quant_u8,
                             permute_params{CASE_PERMUTE_S8_1, 2, 5},
                             permute_params{CASE_PERMUTE_S8_2, 2, 5},
                             permute_params{CASE_PERMUTE_S8_3, 2, 5},
-                                                                 
+
                             permute_params{CASE_PERMUTE_U8_0, 2, 5},
                             permute_params{CASE_PERMUTE_U8_1, 2, 5},
                             permute_params{CASE_PERMUTE_U8_2, 2, 5},
                             permute_params{CASE_PERMUTE_U8_3, 2, 5},
-                            
+
                             permute_params{CASE_PERMUTE_F32_3D_0, 2, 5},
                             permute_params{CASE_PERMUTE_F32_3D_1, 2, 5},
                             permute_params{CASE_PERMUTE_F32_3D_2, 2, 5},
                             permute_params{CASE_PERMUTE_F32_3D_3, 2, 5},
                             permute_params{CASE_PERMUTE_F32_3D_4, 2, 5},
-                                                                     
+
                             permute_params{CASE_PERMUTE_F16_3D_0, 2, 5},
                             permute_params{CASE_PERMUTE_F16_3D_1, 2, 5},
                             permute_params{CASE_PERMUTE_F16_3D_2, 2, 5},
@@ -4270,7 +4283,7 @@ INSTANTIATE_TEST_CASE_P(fusings_gpu, permute_scale_eltwise_quant_u8,
                             permute_params{CASE_PERMUTE_S8_3D_1, 2, 5},
                             permute_params{CASE_PERMUTE_S8_3D_2, 2, 5},
                             permute_params{CASE_PERMUTE_S8_3D_3, 2, 5},
-                                                                    
+
                             permute_params{CASE_PERMUTE_U8_3D_0, 2, 5},
                             permute_params{CASE_PERMUTE_U8_3D_1, 2, 5},
                             permute_params{CASE_PERMUTE_U8_3D_2, 2, 5},
@@ -4321,19 +4334,19 @@ INSTANTIATE_TEST_CASE_P(fusings_gpu, permute_scale_actv_eltw_scale_actv_quant_i8
                             permute_params{CASE_PERMUTE_F16_4, 2, 8},
                             permute_params{CASE_PERMUTE_F16_5, 2, 8},
                             permute_params{CASE_PERMUTE_F16_6, 2, 8},
-                                
+
                             permute_params{CASE_PERMUTE_S8_0, 2, 8},
                             permute_params{CASE_PERMUTE_S8_1, 2, 8},
                             permute_params{CASE_PERMUTE_S8_2, 2, 8},
                             permute_params{CASE_PERMUTE_S8_3, 2, 8},
-                                                                 
+
                             permute_params{CASE_PERMUTE_U8_0, 2, 8},
                             permute_params{CASE_PERMUTE_U8_1, 2, 8},
                             permute_params{CASE_PERMUTE_U8_2, 2, 8},
                             permute_params{CASE_PERMUTE_U8_3, 2, 8},
-                             
+
                             permute_params{CASE_PERMUTE_F32_3D_0, 2, 8},
-                            permute_params{CASE_PERMUTE_F32_3D_1, 2, 8}, 
+                            permute_params{CASE_PERMUTE_F32_3D_1, 2, 8},
                             permute_params{CASE_PERMUTE_F32_3D_2, 2, 8},
                             permute_params{CASE_PERMUTE_F32_3D_3, 2, 8},
                             permute_params{CASE_PERMUTE_F32_3D_4, 2, 8},
@@ -4348,7 +4361,7 @@ INSTANTIATE_TEST_CASE_P(fusings_gpu, permute_scale_actv_eltw_scale_actv_quant_i8
                             permute_params{CASE_PERMUTE_S8_3D_1, 2, 8},
                             permute_params{CASE_PERMUTE_S8_3D_2, 2, 8},
                             permute_params{CASE_PERMUTE_S8_3D_3, 2, 8},
-                                                                    
+
                             permute_params{CASE_PERMUTE_U8_3D_0, 2, 8},
                             permute_params{CASE_PERMUTE_U8_3D_1, 2, 8},
                             permute_params{CASE_PERMUTE_U8_3D_2, 2, 8},
@@ -4387,7 +4400,7 @@ INSTANTIATE_TEST_CASE_P(fusings_gpu, permute_scale_eltwise_actv_scale_actv,
                             permute_params{CASE_PERMUTE_F32_5, 2, 7},
                             permute_params{CASE_PERMUTE_F32_6, 2, 7},
                             permute_params{CASE_PERMUTE_F32_7, 2, 7},
-                                                                  
+
                             permute_params{CASE_PERMUTE_F16_0, 2, 7},
                             permute_params{CASE_PERMUTE_F16_1, 2, 7},
                             permute_params{CASE_PERMUTE_F16_2, 2, 7},
@@ -4395,36 +4408,102 @@ INSTANTIATE_TEST_CASE_P(fusings_gpu, permute_scale_eltwise_actv_scale_actv,
                             permute_params{CASE_PERMUTE_F16_4, 2, 7},
                             permute_params{CASE_PERMUTE_F16_5, 2, 7},
                             permute_params{CASE_PERMUTE_F16_6, 2, 7},
-                            
+
                             permute_params{CASE_PERMUTE_S8_0, 2, 7},
                             permute_params{CASE_PERMUTE_S8_1, 2, 7},
                             permute_params{CASE_PERMUTE_S8_2, 2, 7},
                             permute_params{CASE_PERMUTE_S8_3, 2, 7},
-                                                                 
+
                             permute_params{CASE_PERMUTE_U8_0, 2, 7},
                             permute_params{CASE_PERMUTE_U8_1, 2, 7},
                             permute_params{CASE_PERMUTE_U8_2, 2, 7},
                             permute_params{CASE_PERMUTE_U8_3, 2, 7},
-                            
+
                             permute_params{CASE_PERMUTE_F32_3D_0, 2, 7},
                             permute_params{CASE_PERMUTE_F32_3D_1, 2, 7},
                             permute_params{CASE_PERMUTE_F32_3D_2, 2, 7},
                             permute_params{CASE_PERMUTE_F32_3D_3, 2, 7},
                             permute_params{CASE_PERMUTE_F32_3D_4, 2, 7},
-                                                                     
+
                             permute_params{CASE_PERMUTE_F16_3D_0, 2, 7},
                             permute_params{CASE_PERMUTE_F16_3D_1, 2, 7},
                             permute_params{CASE_PERMUTE_F16_3D_2, 2, 7},
                             permute_params{CASE_PERMUTE_F16_3D_3, 2, 7},
                             permute_params{CASE_PERMUTE_F16_3D_4, 2, 7},
-                            
+
                             permute_params{CASE_PERMUTE_S8_3D_0, 2, 7},
                             permute_params{CASE_PERMUTE_S8_3D_1, 2, 7},
                             permute_params{CASE_PERMUTE_S8_3D_2, 2, 7},
                             permute_params{CASE_PERMUTE_S8_3D_3, 2, 7},
-                                                                    
+
                             permute_params{CASE_PERMUTE_U8_3D_0, 2, 7},
                             permute_params{CASE_PERMUTE_U8_3D_1, 2, 7},
                             permute_params{CASE_PERMUTE_U8_3D_2, 2, 7},
                             permute_params{CASE_PERMUTE_U8_3D_3, 2, 7},
+                        }), );
+
+class NormalizeFusingTest : public ::BaseFusingTest<normalize_test_params> {
+public:
+    void execute(normalize_test_params& p) {
+        auto input_prim = get_mem(get_input_layout(p));
+        network network_not_fused(this->engine, this->topology_non_fused, bo_not_fused);
+        network network_fused(this->engine, this->topology_fused, bo_fused);
+        network_fused.set_input_data("input", input_prim);
+        network_not_fused.set_input_data("input", input_prim);
+
+        compare(network_not_fused, network_fused, p);
+    }
+    layout get_input_layout(normalize_test_params& p) { return layout{p.data_type, p.input_format, p.in_shape}; }
+    layout get_per_channel_layout(normalize_test_params& p) {
+        return layout{p.default_type, p.default_format, tensor{1, p.in_shape.feature[0], 1, 1}};
+    }
+    layout get_weights_layout(normalize_test_params& p) { return layout {p.default_type, p.default_format, tensor{1, p.in_shape.feature[0], 1, 1}}; }
+};
+
+class normalize_i8_quantize : public NormalizeFusingTest {};
+TEST_P(normalize_i8_quantize, basic) {
+    auto p = GetParam();
+    create_topologies(
+        input_layout("input", get_input_layout(p)),
+        data("weights", get_mem(get_weights_layout(p))),
+        data("in_lo", get_mem(get_single_element_layout(p), min_random, 0)),
+        data("in_hi", get_mem(get_single_element_layout(p), 1, max_random)),
+        data("out_lo", get_mem(get_single_element_layout(p), 0)),
+        data("out_hi", get_mem(get_single_element_layout(p), 255)),
+        normalize("normalizel2", "input", "weights", p.across_spatial),
+        quantize("quantize", "normalizel2", "in_lo", "in_hi", "out_lo", "out_hi", 255, data_types::u8),
+        reorder("output_reorder", "quantize", p.default_format, data_types::f32));
+
+    tolerance = 1;
+    execute(p);
+}
+
+INSTANTIATE_TEST_CASE_P(fusings_gpu,
+                        normalize_i8_quantize,
+                        ::testing::ValuesIn(std::vector<normalize_test_params>{
+                            normalize_test_params{CASE_NORMALIZE_I8_1, false, 2, 3},
+                            normalize_test_params{CASE_NORMALIZE_I8_1, true, 2, 3},
+                        }), );
+
+class normalize_i8_float : public NormalizeFusingTest {};
+TEST_P(normalize_i8_float, basic) {
+    auto p = GetParam();
+    create_topologies(
+        input_layout("input", get_input_layout(p)),
+        data("weights", get_mem(get_weights_layout(p))),
+        data("scale_data", get_mem(get_per_channel_layout(p), 1.0f/255)),
+        normalize("normalizel2", "input", "weights", p.across_spatial),
+        scale("scale", "normalizel2", "scale_data"),
+        activation("activation", "scale", activation_func::abs),
+        reorder("output_reorder", "activation", p.default_format, data_types::f32));
+
+    tolerance = 1e-05f;
+    execute(p);
+}
+
+INSTANTIATE_TEST_CASE_P(fusings_gpu,
+                        normalize_i8_float,
+                        ::testing::ValuesIn(std::vector<normalize_test_params>{
+                            normalize_test_params{CASE_NORMALIZE_I8_1, false, 2, 4},
+                            normalize_test_params{CASE_NORMALIZE_I8_1, true, 2, 4},
                         }), );
