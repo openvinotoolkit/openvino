7b4165fad146932bb09c6165d5275b27609d206b
Alexander Chaiko <alexander.chaiko@intel.com>
2020-06-06 02:58:56 +0200

    Fix deconvolution regression in fcn

--- inference-engine/thirdparty/clDNN/src/layout_optimizer.cpp
+++ inference-engine/thirdparty/clDNN/src/layout_optimizer.cpp
@@ -753,7 +753,7 @@ layout layout_optimizer::get_expected_layout(layout const& current_layout,
     } else if (_optimization_attributes.b_fs_yx_fsv16_network &&
                deconvolution_b_fs_yx_fsv16_opt(current_layout, output_or_weights_layout, prim)) {
         expected_tensor = current_layout.size;
-        if (expected_tensor.feature[0] < 16) {
+        if (expected_tensor.feature[0] % 16 != 0) {
             expected_format = cldnn::format::bfyx;
         } else
             expected_format = cldnn::format::b_fs_yx_fsv16;
