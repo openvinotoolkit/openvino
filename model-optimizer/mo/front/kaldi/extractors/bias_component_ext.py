8e6df4166a0d897e9c5ca3ead3ca53290db76e66
Pavel Esir <pavel.esir@intel.com>
2020-06-08 21:37:44 +0300

    Add fixedscale(bias) components to Kaldi (#725)
    * Added fixed scale(bias) components
    
    * Successfully converted after adding fixed bias,scale components
    
    * Added unittests

--- model-optimizer/mo/front/kaldi/extractors/bias_component_ext.py
+++ model-optimizer/mo/front/kaldi/extractors/bias_component_ext.py
@@ -21,44 +21,24 @@ from mo.front.kaldi.utils import read_binary_vector
 from mo.ops.scale_shift import ScaleShiftOp
 
 
-class NaturalGradientPerElementScaleComponentFrontExtractor(FrontExtractorOp):
-    op = 'naturalgradientperelementscalecomponent'
+class FixedBiasComponentFrontExtractor(FrontExtractorOp):
+    op = 'fixedbiascomponent'
     enabled = True
 
     @classmethod
     def extract(cls, node):
         pb = node.parameters
-        collect_until_token(pb, b'<Params>')
-        weights = read_binary_vector(pb)
-        find_next_tag(pb)
-        read_placeholder(pb, 1)
-
-        mapping_rule = {
-            'layout': 'NCHW'
-        }
-        embed_input(mapping_rule, 1, 'weights', weights)
-
-        ScaleShiftOp.update_node_stat(node, mapping_rule)
-        return cls.enabled
-
-
-class FixedScaleComponentFrontExtractor(FrontExtractorOp):
-    op = 'fixedscalecomponent'
-    enabled = True
-
-    @classmethod
-    def extract(cls, node):
-        pb = node.parameters
-        collect_until_token(pb, b'<Scales>')
-        weights = read_binary_vector(pb)
+        collect_until_token(pb, b'<Bias>')
+        biases = read_binary_vector(pb)
         find_next_tag(pb)
         read_placeholder(pb, 1)
 
         mapping_rule = {
             'layout': 'NCHW',
-            'out-size': weights.shape[0],
+            'bias_term': True,
+            'out-size': biases.shape[0],
         }
-        embed_input(mapping_rule, 1, 'weights', weights)
+        embed_input(mapping_rule, 2, 'biases', biases)
 
         ScaleShiftOp.update_node_stat(node, mapping_rule)
         return cls.enabled
