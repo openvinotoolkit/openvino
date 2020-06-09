30e724239eaa09d4d72d58ddae898b547fea9f20
Chetverikov Anton <anton.chetverikov@intel.com>
2020-06-10 00:07:40 +0300

    Fix compilation error

--- inference-engine/src/transformations/include/transformations/convert_extract_image_patches_to_reorg_yolo.hpp
+++ inference-engine/src/transformations/include/transformations/convert_extract_image_patches_to_reorg_yolo.hpp
@@ -14,7 +14,7 @@
 namespace ngraph {
 namespace pass {
 
-class INFERENCE_ENGINE_API_CLASS(ConvertExtractImagePatchesToReorgYolo);
+class TRANSFORMATIONS_API(ConvertExtractImagePatchesToReorgYolo);
 
 }  // namespace pass
 }  // namespace ngraph
