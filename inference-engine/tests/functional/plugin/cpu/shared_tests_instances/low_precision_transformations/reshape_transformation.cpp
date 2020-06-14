b4913a20a452f745a29772651865fe8ffa3a90de
Edward Shogulin <edward.shogulin@intel.com>
2020-06-15 00:56:53 +0300

    [LPT] Reshape transformation

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/low_precision_transformations/reshape_transformation.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/low_precision_transformations/reshape_transformation.cpp
@@ -21,13 +21,18 @@ const std::vector<InferenceEngine::details::LayerTransformation::Params> trasfor
     LayerTestsUtils::LayerTransformationParamsFactory::createParamsU8I8()
 };
 
+const std::vector<LayerTestsUtils::LayerTransformation::LptVersion> versions = {
+    LayerTestsUtils::LayerTransformation::LptVersion::cnnNetwork,
+    LayerTestsUtils::LayerTransformation::LptVersion::nGraph
+};
 
-INSTANTIATE_TEST_CASE_P(LPT, ReshapeTransformation,
+INSTANTIATE_TEST_CASE_P(DISABLED_LPT, ReshapeTransformation,
     ::testing::Combine(
         ::testing::ValuesIn(netPrecisions),
         ::testing::Values(InferenceEngine::SizeVector({ 1, 3, 16, 16 })),
         ::testing::Values(CommonTestUtils::DEVICE_CPU),
-        ::testing::ValuesIn(trasformationParamValues)),
+        ::testing::ValuesIn(trasformationParamValues),
+        ::testing::ValuesIn(versions)),
     ReshapeTransformation::getTestCaseName);
 }  // namespace
 
--- inference-engine/tests/functional/plugin/shared/src/low_precision_transformations/reshape_transformation.cpp
+++ inference-engine/tests/functional/plugin/shared/src/low_precision_transformations/reshape_transformation.cpp
@@ -8,15 +8,10 @@
 #include <tuple>
 #include <vector>
 #include <string>
-
 #include <ie_core.hpp>
 
-#include "common_test_utils/common_utils.hpp"
-#include "functional_test_utils/plugin_cache.hpp"
-#include "functional_test_utils/layer_test_utils.hpp"
-#include "functional_test_utils/blob_utils.hpp"
-#include "ngraph_functions/pass/convert_prc.hpp"
 #include "ngraph_functions/builders.hpp"
+#include <transformations/init_node_info.hpp>
 
 
 namespace LayerTestsDefinitions {
@@ -26,19 +21,21 @@ std::string ReshapeTransformation::getTestCaseName(testing::TestParamInfo<LayerT
     InferenceEngine::SizeVector inputShapes;
     std::string targetDevice;
     InferenceEngine::details::LayerTransformation::Params params;
-    std::tie(netPrecision, inputShapes, targetDevice, params) = obj.param;
+    LayerTestsUtils::LayerTransformation::LptVersion version;
+    std::tie(netPrecision, inputShapes, targetDevice, params, version) = obj.param;
 
-    std::ostringstream result;
-    result << netPrecision.name() << "_" << targetDevice << "_" << toString(params);
-    return result.str();
+    return getTestCaseNameByParams(netPrecision, inputShapes, targetDevice, params, version);
 }
 
 void ReshapeTransformation::SetUp() {
     InferenceEngine::SizeVector inputShape;
     InferenceEngine::Precision netPrecision;
     InferenceEngine::details::LayerTransformation::Params params;
-    std::tie(netPrecision, inputShape, targetDevice, params) = this->GetParam();
+    LayerTestsUtils::LayerTransformation::LptVersion version;
+    std::tie(netPrecision, inputShape, targetDevice, params, version) = this->GetParam();
     const auto precision = FuncTestUtils::PrecisionUtils::convertIE2nGraphPrc(netPrecision);
+
+    ConfigurePlugin(version);
 
     const float k = 50.f;
 
@@ -63,14 +60,17 @@ void ReshapeTransformation::SetUp() {
     ngraph::ResultVector results {std::make_shared<ngraph::opset1::Result>(convolution)};
     function = std::make_shared<ngraph::Function>(results, ngraph::ParameterVector { input }, "ReshapeTransformation");
 
-    validate();
+    if (version == LptVersion::cnnNetwork) {
+        validate();
+    }
 }
 
 void ReshapeTransformation::validate() {
     InferenceEngine::SizeVector inputShape;
     InferenceEngine::Precision netPrecision;
     InferenceEngine::details::LayerTransformation::Params params;
-    std::tie(netPrecision, inputShape, targetDevice, params) = this->GetParam();
+    LayerTestsUtils::LayerTransformation::LptVersion version;
+    std::tie(netPrecision, inputShape, targetDevice, params, version) = this->GetParam();
 
     const InferenceEngine::CNNNetwork network = transform(params);
 
