b50a89ebad19f90bf7fc62f32b54eb7cfd5b60cf
Ilya Lavrenov <ilya.lavrenov@intel.com>
2020-06-16 15:03:32 +0300

    Removed PluginDispatcher; IEPlugin from python API (#920)

--- inference-engine/tests/ie_test_utils/unit_test_utils/empty.cpp
+++ inference-engine/tests/ie_test_utils/unit_test_utils/empty.cpp
@@ -11,7 +11,6 @@
 #include "unit_test_utils/mocks/mock_iinfer_request.hpp"
 #include "unit_test_utils/mocks/mock_iinference_plugin.hpp"
 #include "unit_test_utils/mocks/mock_not_empty_icnn_network.hpp"
-#include "unit_test_utils/mocks/mock_plugin_dispatcher.hpp"
 
 #include "unit_test_utils/mocks/cpp_interfaces/mock_plugin_impl.hpp"
 #include "unit_test_utils/mocks/cpp_interfaces/mock_task_executor.hpp"
