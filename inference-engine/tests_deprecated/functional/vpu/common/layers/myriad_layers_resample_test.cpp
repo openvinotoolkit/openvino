289217e8cc546504abae039eb4baf5d0f9046b12
Anton Dudchenko <anton.dudchenko@intel.com>
2020-06-16 21:42:42 +0300

    Enable Resample layer tests

--- inference-engine/tests_deprecated/functional/vpu/common/layers/myriad_layers_resample_test.cpp
+++ inference-engine/tests_deprecated/functional/vpu/common/layers/myriad_layers_resample_test.cpp
@@ -4,9 +4,8 @@
 
 #include "myriad_layers_resample_test.hpp"
 
-// #-31522
 INSTANTIATE_TEST_CASE_P(
-	DISABLED_accuracy, myriadResampleLayerTests_smoke,
+	accuracy, myriadResampleLayerTests_smoke,
 	::testing::Combine(
 		::testing::ValuesIn(s_ResampleInput),
 		::testing::Values<Factor>(2.0f, 0.5f),
