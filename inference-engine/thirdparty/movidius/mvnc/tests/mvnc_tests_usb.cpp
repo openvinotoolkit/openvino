8c840d02968a1f4842fa4e630ba4c017de68d68b
Nikita Kudriavtsev <nikita.kudriavtsev@intel.com>
2020-06-18 13:49:20 +0300

    Changes: (#982)
    - Named structures in bmp.h to avoid MSFT compiler error
    - Fix for non-void function with missing return statement to avoid Intel compiler error
    - Enabled "smoke_ExportUsingFileNameImportFromStreamNoThrowWithDeviceName" test
    - Fix for MvncTest

--- inference-engine/thirdparty/movidius/mvnc/tests/mvnc_tests_usb.cpp
+++ inference-engine/thirdparty/movidius/mvnc/tests/mvnc_tests_usb.cpp
@@ -28,7 +28,7 @@ TEST_F(MvncOpenUSBDevice, ShouldOpenDeviceAfterChangeConnectTimeoutFromZero) {
 
     ASSERT_NO_ERROR(ncSetDeviceConnectTimeout(0));
     ASSERT_ERROR(ncDeviceOpen(&deviceHandle, deviceDesc, m_ncDeviceOpenParams));
-    std::this_thread::sleep_for(3_sec);
+    std::this_thread::sleep_for(5_sec);
     ASSERT_NO_ERROR(ncDeviceResetAll());
 
     ASSERT_NO_ERROR(ncSetDeviceConnectTimeout(30));
