c6a19dc0674d58cb88eb8472e6e68d29b2e2ce8b
tomdol <tomasz.dolbniak@intel.com>
2020-06-18 14:17:44 +0200

    Missing license headers

--- ngraph/test/util/engine/engine_factory.hpp
+++ ngraph/test/util/engine/engine_factory.hpp
@@ -1,3 +1,19 @@
+//*****************************************************************************
+// Copyright 2017-2020 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+
 #pragma once
 
 #include "ngraph/function.hpp"
@@ -15,7 +31,8 @@ namespace ngraph
 
         namespace
         {
-            /// A factory that can create engines supporting devices
+            /// A factory that can create engines supporting devices but not dynamic backends.
+            /// Currently: IE_CPU_Backend and IE_GPU_Backend
             template <typename Engine>
             typename std::enable_if<EngineTraits<Engine>::supports_devices, Engine>::type
                 create_engine_impl(const std::shared_ptr<ngraph::Function> function,
@@ -24,7 +41,8 @@ namespace ngraph
                 return Engine{function, EngineTraits<Engine>::device};
             }
 
-            /// A factory that can create engines supporting dynamic backends
+            /// A factory that can create engines which support dynamic backends
+            /// but do not support devices. Currently: INTERPRETER_Engine
             template <typename Engine>
             typename std::enable_if<EngineTraits<Engine>::supports_dynamic, Engine>::type
                 create_engine_impl(const std::shared_ptr<ngraph::Function> function,
