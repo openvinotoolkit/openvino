88d1f24a34a070d172245ea3cbf0968d71a10a41
Andrey Sokolov <andrey.sokolov@intel.com>
2020-06-19 14:48:53 +0300

    [IE VPU] use optimized ReduceMean instead of GlobalPooling (#629)

--- inference-engine/tests/ngraph_functions/include/ngraph_functions/builders.hpp
+++ inference-engine/tests/ngraph_functions/include/ngraph_functions/builders.hpp
@@ -249,5 +249,15 @@ std::shared_ptr<ngraph::Node> makeReduce(std::vector<ngraph::Output<Node>> &in,
                                          bool keepDims,
                                          ngraph::helpers::ReductionType reductionType);
 
+std::shared_ptr<Node> makePooling(const ngraph::Output<Node> &in,
+                                  const std::vector<size_t> &strides,
+                                  const std::vector<size_t> &padsBegin,
+                                  const std::vector<size_t> &padsEnd,
+                                  const std::vector<size_t> &kernel,
+                                  const op::RoundingType &roundingType,
+                                  const op::PadType &padType,
+                                  bool excludePad,
+                                  const ngraph::helpers::PoolingTypes &poolType);
+
 }  // namespace builder
 }  // namespace ngraph
