49fc595a7fe46b25800a00276f507d850fa831e8
Ivan Tikhonov <ivan.tikhonov@intel.com>
2020-06-19 14:18:39 +0300

    fix kaldi models with memory (batch > 1)

--- inference-engine/src/legacy_api/src/convert_function_to_cnn_network.cpp
+++ inference-engine/src/legacy_api/src/convert_function_to_cnn_network.cpp
@@ -760,6 +760,17 @@ std::shared_ptr<CNNNetworkImpl> convertFunctionToICNNNetwork(const std::shared_p
         cnnLayer->insData.resize(inputCount);
 
         for (size_t i = 0; i < layer->get_output_size(); i++) {
+            // Memory node with index = 1 has no inputs according to the specification.
+            // For proper conversion, we must cut off all the layers and data nodes above ReadValue,
+            // if they are connected only with this layer.
+            bool all_to_read_value = !layer->get_output_inputs(i).empty();
+            for (const auto& output_input : layer->get_output_inputs(i)) {
+                all_to_read_value
+                    &= std::dynamic_pointer_cast<ngraph::op::ReadValue>(output_input->get_node()) != nullptr;
+            }
+            if (all_to_read_value)
+                continue;
+
             if (cnnLayer->type == "Memory" && cnnLayer->params["index"] == "0") {
                 cnnLayer->outData.clear();
                 continue;
