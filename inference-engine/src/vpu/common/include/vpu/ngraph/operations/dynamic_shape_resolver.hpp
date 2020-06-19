a5e5af068fcf007aad510b58a49b539297dbbe8f
Maksim Doronin <maksim.doronin@intel.com>
2020-06-19 13:22:31 +0300

    [IE VPU] Evaluate DSR (#770)
    * [IE VPU] Add evaluate method to DSR
    
    * [IE VPU] Enable DSR_Reshape tests
    
    * [IE VPU] Improvements in DSR op
    
    * [IE VPU] Fix typo in copyBlobAccordingUpperBound
    
    * [IE VPU] Support dynamic inputs
    
    * [IE VPU] Use dynamic inputs in tests
    
    * [IE VPU] Improve conditions in propogateDynamism pass
    
    * [IE VPU] Fix Myriad2 tests via dosabling reorder
    
    * [IE VPU] make error message more explicit
    
    * [IE VPU] Fix Win compilation: std::stoi in <string>
    
    * [IE VPU] Improve data transferring to work with ND tensors
    
    * [IE VPU] Avoid ODR in myriad common test utils
    
    * [IE VPU] Split code in propagate dynamism into separate methods
    
    * [IE VPU] Simplify conditions in DSR parsing
    
    * [IE VPU] Emplace data in initialStages when remove stage order

--- inference-engine/src/vpu/common/include/vpu/ngraph/operations/dynamic_shape_resolver.hpp
+++ inference-engine/src/vpu/common/include/vpu/ngraph/operations/dynamic_shape_resolver.hpp
@@ -5,10 +5,16 @@
 #pragma once
 
 #include "ngraph/op/op.hpp"
+#include "ngraph/runtime/host_tensor.hpp"
 
 #include <memory>
 
 namespace ngraph { namespace vpu { namespace op {
+
+enum class DynamicShapeResolverMode {
+    INFER_UPPER_BOUND_SHAPE,
+    INFER_DYNAMIC_SHAPE
+};
 
 class DynamicShapeResolver : public ngraph::op::Op {
 public:
@@ -16,13 +22,23 @@ public:
 
     const NodeTypeInfo& get_type_info() const override { return type_info; }
 
-    DynamicShapeResolver(const Output<Node>& tensorWithData, const Output<Node>& tensorWithDims);
+    DynamicShapeResolver(const Output<Node>& tensorWithData,
+                         const Output<Node>& tensorWithDims,
+                         const DynamicShapeResolverMode& mode = DynamicShapeResolverMode::INFER_UPPER_BOUND_SHAPE);
 
     void validate_and_infer_types() override;
 
     std::shared_ptr<Node> copy_with_new_args(const NodeVector& new_args) const override;
 
     bool visit_attributes(ngraph::AttributeVisitor& visitor) override;
+
+    bool evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) override;
+
+    void setMode(DynamicShapeResolverMode mode) { m_mode = mode; }
+    DynamicShapeResolverMode getMode() { return m_mode; }
+
+private:
+    DynamicShapeResolverMode m_mode;
 };
 
 }  // namespace op
