4675914c24c7eddc80cb88abfd8ec8d7d5c7f484
Adam Osewski <adam.osewski@intel.com>
2020-06-19 17:05:10 +0200

    UT for caching attribute values and trisitivity

--- ngraph/python/test/ngraph/test_dyn_attributes.py
+++ ngraph/python/test/ngraph/test_dyn_attributes.py
@@ -18,6 +18,24 @@ import pytest
 
 import ngraph as ng
 
+@pytest.fixture()
+def _proposal_node():
+    attributes = {
+        "attrs.base_size": np.uint16(1),
+        "attrs.pre_nms_topn": np.uint16(20),
+        "attrs.post_nms_topn": np.uint16(64),
+        "attrs.nms_thresh": np.float64(0.34),
+        "attrs.feat_stride": np.uint16(16),
+        "attrs.min_size": np.uint16(32),
+        "attrs.ratio": np.array([0.1, 1.5, 2.0, 2.5], dtype=np.float64),
+        "attrs.scale": np.array([2, 3, 3, 4], dtype=np.float64),
+    }
+    batch_size = 7
+
+    class_probs = ng.parameter([batch_size, 12, 34, 62], np.float64, "class_probs")
+    class_logits = ng.parameter([batch_size, 24, 34, 62], np.float64, "class_logits")
+    image_shape = ng.parameter([3], np.float64, "image_shape")
+    return ng.proposal(class_probs, class_logits, image_shape, attributes)
 
 @pytest.mark.parametrize(
     "int_dtype, fp_dtype",
@@ -143,26 +161,24 @@ def test_dynamic_set_attribute_value(int_dtype, fp_dtype):
     assert node.get_attrs_framework() == "OpenVINO"
 
 
-def test_dynamic_attr_cache():
-    attributes = {
-        "attrs.base_size": np.uint16(1),
-        "attrs.pre_nms_topn": np.uint16(20),
-        "attrs.post_nms_topn": np.uint16(64),
-        "attrs.nms_thresh": np.float64(0.34),
-        "attrs.feat_stride": np.uint16(16),
-        "attrs.min_size": np.uint16(32),
-        "attrs.ratio": np.array([0.1, 1.5, 2.0, 2.5], dtype=np.float64),
-        "attrs.scale": np.array([2, 3, 3, 4], dtype=np.float64),
-    }
-    batch_size = 7
-
-    class_probs = ng.parameter([batch_size, 12, 34, 62], np.float64, "class_probs")
-    class_logits = ng.parameter([batch_size, 24, 34, 62], np.float64, "class_logits")
-    image_shape = ng.parameter([3], np.float64, "image_shape")
-    node = ng.proposal(class_probs, class_logits, image_shape, attributes)
+def test_dynamic_attr_cache(_proposal_node):
+    node = _proposal_node
 
     assert node._attr_cache_valid
     node.set_attrs_nms_thresh(1.3453678102)
     assert not node._attr_cache_valid
     assert np.isclose(node.get_attrs_nms_thresh(), np.float64(1.3453678102))
     assert node._attr_cache_valid
+
+
+def test_dynamic_attr_transitivity(_proposal_node):
+    node = _proposal_node
+    node2 = node
+
+    node.set_attrs_ratio(np.array([1.1, 2.5, 3.0, 4.5], dtype=np.float64))
+    assert np.allclose(node.get_attrs_ratio(), np.array([1.1, 2.5, 3.0, 4.5], dtype=np.float64))
+    assert np.allclose(node2.get_attrs_ratio(), np.array([1.1, 2.5, 3.0, 4.5], dtype=np.float64))
+
+    node2.set_attrs_scale(np.array([2.1, 3.2, 3.3, 4.4], dtype=np.float64))
+    assert np.allclose(node2.get_attrs_scale(), np.array([2.1, 3.2, 3.3, 4.4], dtype=np.float64))
+    assert np.allclose(node.get_attrs_scale(), np.array([2.1, 3.2, 3.3, 4.4], dtype=np.float64))
