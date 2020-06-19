816046791359aa5bb265cb460c16ec395dabed8e
Maksim Doronin <maksim.doronin@intel.com>
2020-06-19 17:57:06 +0300

    [IE VPU] Enable DSR_UnaryEltwise tests

--- inference-engine/tests/functional/plugin/myriad/subgraph_tests/dsr_unary_elementwise.cpp
+++ inference-engine/tests/functional/plugin/myriad/subgraph_tests/dsr_unary_elementwise.cpp
@@ -2,40 +2,48 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include "dsr_tests_common.hpp"
+
 #include <functional_test_utils/layer_test_utils.hpp>
 #include <ngraph_functions/builders.hpp>
 #include <vpu/ngraph/operations/dynamic_shape_resolver.hpp>
 
 namespace {
 
-using DataType = ngraph::element::Type_t;
-using DataDims = ngraph::Shape;
+using namespace LayerTestsUtils::vpu;
 
 using Parameters = std::tuple<
     DataType,
-    DataDims,
+    DataShapeWithUpperBound,
     ngraph::NodeTypeInfo,
     LayerTestsUtils::TargetDevice
 >;
 
-class DSR_UnaryElementwise : public testing::WithParamInterface<Parameters>,
-        public LayerTestsUtils::LayerTestsCommon {
+class DSR_UnaryElementwise : public testing::WithParamInterface<Parameters>, public DSR_TestsCommon {
 protected:
-    void SetUp() override {
+    std::shared_ptr<ngraph::Node> createTestedOp() override {
         const auto& parameters = GetParam();
-        const auto& dataType = std::get<0>(parameters);
-        const auto& dataDims = std::get<1>(parameters);
+        const auto& inDataType = std::get<0>(parameters);
+        const auto& inDataShapes = std::get<1>(parameters);
         const auto& type_info = std::get<2>(parameters);
         targetDevice = std::get<3>(parameters);
 
-        const auto data = std::make_shared<ngraph::opset3::Parameter>(dataType, dataDims);
-        const auto dims = std::make_shared<ngraph::opset3::Parameter>(ngraph::element::i64, ngraph::Shape{dataDims.size()});
-        const auto dsr  = std::make_shared<ngraph::vpu::op::DynamicShapeResolver>(data, dims);
+        const auto inputSubgraph = createInputSubgraphWithDSR(inDataType, inDataShapes);
 
-        const auto node = ngraph::helpers::getNodeSharedPtr(type_info, {dsr});
+        const auto eltwise = ngraph::helpers::getNodeSharedPtr(type_info, {inputSubgraph});
 
-        const auto result = std::make_shared<ngraph::opset3::Result>(node);
-        function = std::make_shared<ngraph::Function>(ngraph::ResultVector{result}, ngraph::ParameterVector{data, dims}, type_info.name);
+        return eltwise;
+    }
+
+    InferenceEngine::Blob::Ptr GenerateInput(const InferenceEngine::InputInfo &info) const override {
+        const auto& parameters = GetParam();
+        const auto& type_info = std::get<2>(parameters);
+
+        // Log(0) = inf, therefore we have to fill data with values starts with 1
+        if (type_info == ngraph::opset3::Log::type_info && m_shapes.find(info.name()) == m_shapes.end()) {
+            return FuncTestUtils::createAndFillBlob(info.getTensorDesc(), 10, 1, 1);
+        }
+        return DSR_TestsCommon::GenerateInput(info);
     }
 };
 
@@ -43,12 +51,12 @@ TEST_P(DSR_UnaryElementwise, CompareWithReference) {
     Run();
 }
 
-INSTANTIATE_TEST_CASE_P(DISABLED_DynamicUnaryElementwise, DSR_UnaryElementwise,
+INSTANTIATE_TEST_CASE_P(DynamicUnaryElementwise, DSR_UnaryElementwise,
     ::testing::Combine(
-        ::testing::Values(ngraph::element::f16, ngraph::element::f32, ngraph::element::i32),
-        ::testing::Values(ngraph::Shape{1, 800}),
-        ::testing::Values(ngraph::opset3::Exp::type_info,
-                          ngraph::opset3::Floor::type_info,
+        ::testing::Values(ngraph::element::f16, ngraph::element::f32),
+        ::testing::Values(DataShapeWithUpperBound{ngraph::Shape{8, 800}, ngraph::Shape{10, 1000}}),
+        ::testing::Values(ngraph::opset3::Floor::type_info,
+                          ngraph::opset3::Exp::type_info,
                           ngraph::opset3::Log::type_info,
                           ngraph::opset3::Relu::type_info,
                           ngraph::opset3::Sigmoid::type_info,
