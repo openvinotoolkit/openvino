967b1e8df8f9ac8010165b1b58267e6dbfff3022
VGavrilov <vladimir.gavrilov@intel.com>
2020-06-19 15:43:50 +0300

    Written conversion of Interpolate-1 into Interpolate-3.

--- model-optimizer/extensions/front/InterpolateV1ToInterpolateV3.py
+++ model-optimizer/extensions/front/InterpolateV1ToInterpolateV3.py
@@ -14,16 +14,34 @@
  limitations under the License.
 """
 
+from mo.front.common.partial_infer.utils import int64_array
 from mo.front.common.replacement import FrontReplacementOp
 from mo.graph.graph import Graph
 
 
 class InterpolateV1ToInterpolateV3(FrontReplacementOp):
     op = 'Interpolate'
-    enabled = False
+    enabled = True
 
     def replace_sub_graph(self, graph: Graph, match: dict):
         node = match['op']
         opset = node.get_opset()
         if opset != 'opset1':
             return
+
+        if int(node.soft_get('align_corners', default=0)):
+            node['coordinate_transformation_mode'] = 'align_corners'
+            del node['align_corners']
+        else:
+            node['coordinate_transformation_mode'] = 'half_pixel'
+
+        node['nearest_mode'] = 'round_prefer_floor'
+        node['cube_coeff'] = -0.75
+
+        pads_begin = node.soft_get('pads_begin', 0)
+        pads_end = node.soft_get('pads_end', 0)
+
+        node['pads_begin'] = int64_array([pads_begin] if not isinstance(pads_begin, list) else pads_begin)
+        node['pads_end'] = int64_array([pads_end] if not isinstance(pads_end, list) else pads_end)
+
+        node.version = 'opset3'
