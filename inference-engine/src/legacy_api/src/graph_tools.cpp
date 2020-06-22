ab75121a91f5193e7498a22c3500b5a8764285ef
Ilya Lavrenov <ilya.lavrenov@intel.com>
2020-06-22 11:35:44 +0300

    Remove some stuff from legacy library (#1043)

--- inference-engine/src/legacy_api/src/graph_tools.cpp
+++ inference-engine/src/legacy_api/src/graph_tools.cpp
@@ -35,25 +35,4 @@ std::vector<CNNLayerPtr> CNNNetSortTopologically(const ICNNNetwork& network) {
 
 }  // namespace details
 
-void CNNNetSubstituteLayer(InferenceEngine::ICNNNetwork& network, const InferenceEngine::CNNLayerPtr& layer,
-                           const InferenceEngine::CNNLayerPtr& newLayer) {
-    IE_ASSERT(layer->name == newLayer->name);
-
-    // Redirect srd data
-    for (auto& src : layer->insData) {
-        src.lock()->getInputTo()[layer->name] = newLayer;
-    }
-    newLayer->insData = layer->insData;
-
-    // Redirect dst data
-    for (auto& dst : layer->outData) {
-        dst->getCreatorLayer() = newLayer;
-    }
-    newLayer->outData = layer->outData;
-
-    IE_SUPPRESS_DEPRECATED_START
-    network.addLayer(newLayer);
-    IE_SUPPRESS_DEPRECATED_END
-}
-
 }  // namespace InferenceEngine
