1972e831000b12d2540b877c248101005a169aea
tomdol <tomasz.dolbniak@intel.com>
2020-06-23 17:51:28 +0200

    DequantizeLinear 10 as a subgraph

--- ngraph/src/ngraph/frontend/onnx_import/op/dequantize_linear.cpp
+++ ngraph/src/ngraph/frontend/onnx_import/op/dequantize_linear.cpp
@@ -33,54 +33,159 @@ namespace ngraph
     {
         namespace op
         {
-            namespace set_1
+            namespace
             {
-                NodeVector dequantize_linear(const Node& node)
+                std::shared_ptr<ngraph::Node> get_zero_point(const NodeVector& inputs)
                 {
-                    NodeVector inputs{node.get_ng_inputs()};
-                    std::shared_ptr<ngraph::Node> x = inputs.at(0);
-                    std::shared_ptr<ngraph::Node> x_scale = inputs.at(1);
-                    std::shared_ptr<ngraph::Node> zero_point;
                     if (inputs.size() == 3 && !inputs.at(2)->is_null())
                     {
-                        zero_point = inputs.at(2);
+                        auto zero_point = inputs[2];
+
+                        if (zero_point->get_element_type() != element::f32)
+                        {
+                            zero_point =
+                                std::make_shared<default_opset::Convert>(zero_point, element::f32);
+                        }
+
+                        return zero_point;
                     }
                     else
                     {
-                        zero_point =
-                            ngraph::builder::make_constant(x->get_element_type(), Shape{}, 0);
+                        return default_opset::Constant::create(element::f32, Shape{}, {0});
                     }
-
-                    Shape y_scale_shape = x_scale->get_shape();
-                    Shape y_zero_point_shape = zero_point->get_shape();
-
-                    // get axis twice with two default values to see if it is set
-                    int64_t axis_0{node.get_attribute_value<int64_t>("axis", 0)};
-                    int64_t axis_1{node.get_attribute_value<int64_t>("axis", 1)};
-
-                    const auto data_rank = x->get_output_partial_shape(0).rank();
-                    AxisSet axes;
-                    // if axis attribute is set
-                    if (axis_0 == axis_1)
-                    {
-                        axes.insert(
-                            ngraph::normalize_axis(node.get_description(), axis_0, data_rank));
-                    }
-
-                    if (x->get_element_type() != zero_point->get_element_type())
-                    {
-                        zero_point = std::make_shared<default_opset::Convert>(
-                            zero_point, x->get_element_type());
-                    }
-
-                    return {std::make_shared<ngraph::opset0::Dequantize>(
-                        x, x_scale, zero_point, x_scale->get_element_type(), axes)};
                 }
 
-            } // namespace set_1
+                constexpr const int64_t DEFAULT_AXIS = 1;
 
-        } // namespace op
+                // void validate_input(const char* input_name,
+                //                     const NodeVector& inputs,
+                //                     const size_t validated_input_index,
+                //                     const int64_t axis,
+                //                     const std::set<element::Type> allowed_types)
+                // {
+                //     const auto& validated_input = inputs[validated_input_index];
+                //     const auto validated_input_rank =
+                //         validated_input->get_output_partial_shape(0).rank();
 
-    } // namespace onnx_import
+                //     NGRAPH_CHECK(validated_input_rank.same_scheme({0}) ||
+                //                      validated_input_rank.same_scheme({1}),
+                //                  input_name,
+                //                  " needs to be a scalar or a vector.");
 
-} // namespace ngraph
+                //     if (validated_input_rank.same_scheme({1}))
+                //     {
+                //         const auto x_input_shape = inputs[0]->get_output_partial_shape(0);
+                //         const auto dim_at_axis = x_input_shape[axis];
+
+                //         NGRAPH_CHECK(dim_at_axis.compatible(validated_input_rank.get_length()),
+                //                      "The number of ",
+                //                      input_name,
+                //                      " elements has to match the dimension value pointed to by
+                //                      the "
+                //                      "axis attribute.");
+                //     }
+
+                //     const bool data_type_ok =
+                //         allowed_types.count(validated_input->get_element_type());
+
+                //     NGRAPH_CHECK(data_type_ok,
+                //                  "Incorrect data type of the ",
+                //                  input_name,
+                //                  " input: ",
+                //                  input->get_element_type());
+                // }
+            }
+            namespace set_1
+            {
+                namespace
+                {
+                    void validate_scalar_input(const char* input_name,
+                                               const std::shared_ptr<ngraph::Node> input,
+                                               const std::set<element::Type> allowed_types = {})
+                    {
+                        const auto validated_input_rank = input->get_output_partial_shape(0).rank();
+
+                        NGRAPH_CHECK(validated_input_rank.same_scheme({0}),
+                                     input_name,
+                                     " needs to be a scalar.");
+
+                        if (!allowed_types.empty())
+                        {
+                            const bool data_type_ok =
+                                allowed_types.count(input->get_element_type());
+                            NGRAPH_CHECK(data_type_ok,
+                                         "Incorrect data type of the ",
+                                         input_name,
+                                         " input: ",
+                                         input->get_element_type());
+                        }
+                    }
+                }
+
+                NodeVector dequantize_linear(const Node& node)
+                {
+                    const NodeVector inputs{node.get_ng_inputs()};
+
+                    NGRAPH_CHECK(
+                        2 <= inputs.size() && inputs.size() <= 3,
+                        "The DequantizeLinear op expects 2 required and one optional input. Got: ",
+                        inputs.size());
+
+                    const auto x = inputs[0];
+                    const auto scale = inputs[1];
+                    const auto zero_point = get_zero_point(inputs);
+
+                    validate_scalar_input("Dequantization scale", scale, {element::f32});
+                    validate_scalar_input("Zero point", zero_point);
+
+                    const auto converted_x =
+                        std::make_shared<default_opset::Convert>(x, element::f32);
+
+                    return {std::make_shared<default_opset::Multiply>(
+                        std::make_shared<default_opset::Subtract>(converted_x, zero_point), scale)};
+                }
+
+                // namespace set_13
+                // {
+                    // NodeVector dequantize_linear(const Node& node)
+                    // {
+                    //     const NodeVector inputs{node.get_ng_inputs()};
+
+                    //     NGRAPH_CHECK(
+                    //         2 <= inputs.size() <= 3,
+                    //         "The DequantizeLinear op expects 2 required and one optional input.
+                    //         Got:
+                    //         ",
+                    //         inputs.size());
+
+                    //     const std::shared_ptr<ngraph::Node> x = inputs[0];
+                    //     const std::shared_ptr<ngraph::Node> scale = inputs[1];
+                    //     const std::shared_ptr<ngraph::Node> zero_point = get_zero_point(inputs);
+
+                    //     const auto x_rank = x->get_output_partial_shape(0).rank();
+
+                    //     NGRAPH_CHECK(x_rank.is_static(),
+                    //                  "Rank of the input data tensor has to be known/static.");
+
+                    //     int64_t axis{node.get_attribute_value<int64_t>("axis", 1)};
+                    //     axis = ngraph::normalize_axis(node.get_description(), axis, x_rank);
+
+                    //     validate_input("Dequantization scale", scale, axis, {element::f32});
+                    //     validate_input(
+                    //         "Zero point", zero_point, axis, {element::i8, element::u8,
+                    //         element::i32});
+
+                    //     if (x->get_element_type() != zero_point->get_element_type())
+                    //     {
+                    //         zero_point = std::make_shared<default_opset::Convert>(
+                    //             zero_point, x->get_element_type());
+                    //     }
+
+                    //     return {std::make_shared<ngraph::opset0::Dequantize>(
+                    //         x, x_scale, zero_point, x_scale->get_element_type(), axes)};
+                    // }
+                // }
+            }
+        }
+    }
+}
