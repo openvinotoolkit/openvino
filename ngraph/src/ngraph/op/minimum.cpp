7cff005ada6024adbb8daa8e62cac0e9dccfc43c
Ilya Churaev <ilya.churaev@intel.com>
2020-06-23 12:17:40 +0300

    Disable ref implementations (#951)
    * Add NGRAPH_EVALUATE_ENABLE flag and disable all reference implementations
    
    * Enable some evaluate methods
    
    * Added dynamic library with reference implementations
    
    * Fixed tests
    
    * Enabled unsqueeze  CF
    
    * Removed nGraph test library
    
    * Disable all nGraph tests to check
    
    * Enable some reference implementations
    
    * Added debug message
    
    * EVALUATE true
    
    * Revert "Disable all nGraph tests to check"
    
    This reverts commit 38bca3ed3dfed029e892fe609ea7e48c5cfadb67.
    
    * Enable some implementations
    
    * Removed some TYPE_CASE reference implementations
    
    * Fixed reshape
    
    * Revert types for Broadcast and Add
    
    * Disabled failing gpu_engine.user_context test
    
    * Disabled failed nGraph tests
    
    * Add u8 for non_zero
    
    * Revert "Added debug message"
    
    This reverts commit 4b9f4894f5ae9963426830ac5e5eb833af8847aa.
    
    * Revert "Enable some reference implementations"
    
    This reverts commit d2001a636df7504e0ad5abe5c98725ef0be07379.
    
    Revert "Enabled unsqueeze  CF"
    
    This reverts commit 814a8e52cb2b673446d24e54ed11af1dd3d80fad.
    
    Revert "Enable some evaluate methods"
    
    This reverts commit 73767b8942d857bf60317f29120c98c528344a04.
    
    * Revert "Add NGRAPH_EVALUATE_ENABLE flag and disable all reference implementations"
    
    This reverts commit cfaa7d7e7bf34b617f53a556d24fea2189372592.

--- ngraph/src/ngraph/op/minimum.cpp
+++ ngraph/src/ngraph/op/minimum.cpp
@@ -89,29 +89,17 @@ namespace
         out->set_broadcast(broadcast_spec, arg0, arg1);
         switch (arg0->get_element_type())
         {
-            TYPE_CASE(i8)(arg0, arg1, out, broadcast_spec);
-            break;
-            TYPE_CASE(i16)(arg0, arg1, out, broadcast_spec);
-            break;
             TYPE_CASE(i32)(arg0, arg1, out, broadcast_spec);
             break;
             TYPE_CASE(i64)(arg0, arg1, out, broadcast_spec);
-            break;
-            TYPE_CASE(u8)(arg0, arg1, out, broadcast_spec);
-            break;
-            TYPE_CASE(u16)(arg0, arg1, out, broadcast_spec);
             break;
             TYPE_CASE(u32)(arg0, arg1, out, broadcast_spec);
             break;
             TYPE_CASE(u64)(arg0, arg1, out, broadcast_spec);
             break;
-            TYPE_CASE(bf16)(arg0, arg1, out, broadcast_spec);
-            break;
             TYPE_CASE(f16)(arg0, arg1, out, broadcast_spec);
             break;
             TYPE_CASE(f32)(arg0, arg1, out, broadcast_spec);
-            break;
-            TYPE_CASE(f64)(arg0, arg1, out, broadcast_spec);
             break;
         default: rc = false; break;
         }
