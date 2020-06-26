cbf6d9338728a689047b0a8276aa57ce8bc68ae5
Anton Chetverikov <Anton.Chetverikov@intel.com>
2020-06-26 13:41:49 +0300

    Fix const node non-deterministic names (part 1) (#996)
    * Update node names

--- model-optimizer/mo/utils/shape.py
+++ model-optimizer/mo/utils/shape.py
@@ -65,14 +65,14 @@ def get_range_node_of_idxs(rank: Node, begin: int, end: int,
     end_idx = get_canonical_axis_index_node(rank, end)
 
     if not include_begin:
-        const = Const(graph, {'value': int64_array([1])}).create_node()
+        const = Const(graph, {'value': int64_array([1]), 'name': name + '/exclude_begin/value'}).create_node()
         add = Add(graph, {'name': name + '/exclude_begin'}).create_node()
         start_idx.out_port(0).connect(add.in_port(0))
         const.out_port(0).connect(add.in_port(1))
         start_idx = add
 
     if include_end:
-        const = Const(graph, {'value': int64_array([1])}).create_node()
+        const = Const(graph, {'value': int64_array([1]), 'name': name + '/including_end/value'}).create_node()
         add = Add(graph, {'name': name + '/including_end'}).create_node()
         end_idx.out_port(0).connect(add.in_port(0))
         const.out_port(0).connect(add.in_port(1))
@@ -187,7 +187,10 @@ def new_shape_node_from_shape_nodes(input_shape_nodes: list):
     :return: the node producing concatenated values of nodes from the "input_shape_nodes"
     """
     assert len(input_shape_nodes) > 0, 'The list of input shape nodes should be non-empty'
-    new_shape_node = Concat(input_shape_nodes[0].graph, {'axis': 0}).create_node()
+    new_shape_node = Concat(input_shape_nodes[0].graph,
+                            {'axis': 0,
+                             'name': input_shape_nodes[0].soft_get('name', input_shape_nodes[0].id) + '/shapes_concat'}
+                            ).create_node()
 
     for ind, input_node in enumerate(input_shape_nodes):
         new_shape_node.add_input_port(ind)
