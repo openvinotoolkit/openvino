28dee0bd1a5943cf1c7b0767a13af6c817a8878e
Edward Shogulin <edward.shogulin@intel.com>
2020-06-27 12:02:13 +0300

    [LPT] Subtract transformation

---
+++ inference-engine/src/transformations/src/transformations/low_precision/subtract.cpp
@@ -0,0 +1,138 @@
+﻿// Copyright (C) 2018-2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "transformations/low_precision/subtract.hpp"
+
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+#include <cassert>
+
+#include "transformations/low_precision/common/ie_lpt_exception.hpp"
+#include "transformations/low_precision/network_helper.hpp"
+#include "ngraph_ops/multiply_add.hpp"
+
+// TODO: remove after debugging
+#include <ngraph/pass/visualize_tree.hpp>
+
+namespace ngraph {
+namespace pass {
+namespace low_precision {
+
+void SubtractTransformation::registerMatcherIn(GraphRewrite &pass, TransformationContext &context) const {
+    addPattern(
+        pass,
+        context,
+        make_op_pattern<opset1::Subtract>({ make_op_label<opset1::Multiply>(), make_op_label<opset1::Constant>() }));
+
+    addPattern(
+        pass,
+        context,
+        make_op_pattern<opset1::Subtract>({ make_op_label<opset1::Convert>(), make_op_label<opset1::Constant>() }));
+}
+
+void SubtractTransformation::transform(TransformationContext& context, ngraph::pattern::Matcher &m) const {
+    // ngraph::pass::VisualizeTree("C:\\Projects\\temp\\test.transformed").run_on_module(std::vector<std::shared_ptr<ngraph::Function>>{ context.network });
+
+    std::shared_ptr<opset1::Subtract> subtract = as_type_ptr<opset1::Subtract>(m.get_match_root());
+    const ngraph::element::Type originalPrecision = subtract->get_output_element_type(0);
+
+    const FakeQuantizeDequantization dequantization = ngraph::pass::low_precision::getDequantization(subtract);
+    if (dequantization.multiply != nullptr) {
+        // TODO: NO TESTS!!!
+        // before: Y = SC * X - SH, after:  Y = (X - SH') * SC
+        //    SC * X - SH = SC * X - SH' * SC
+        //    SH' = SH / SC
+        std::shared_ptr<opset1::Subtract> newSubtract = as_type_ptr<opset1::Subtract>(subtract->copy_with_new_inputs({
+            dequantization.multiply->get_input_node_shared_ptr(0),
+            ngraph::pass::low_precision::fold<ngraph::opset1::Divide>(
+                subtract->get_input_node_shared_ptr(1),
+                dequantization.multiply->get_input_node_shared_ptr(1))
+        }));
+
+        std::shared_ptr<Node> newMultiply = dequantization.multiply->copy_with_new_inputs({
+            newSubtract,
+            dequantization.multiply->input_value(1)
+        });
+
+        replace_node(subtract, newMultiply);
+        subtract = newSubtract;
+    }
+
+    if (dequantization.subtract != nullptr) {
+        // TODO: NO TESTS!!!
+        std::shared_ptr<opset1::Subtract> newSubtract = as_type_ptr<opset1::Subtract>(subtract->copy_with_new_inputs({
+            dequantization.subtract->get_input_node_shared_ptr(0),
+            ngraph::pass::low_precision::fold<ngraph::opset1::Add>(
+                subtract->get_input_node_shared_ptr(1),
+                dequantization.subtract->get_input_node_shared_ptr(1))
+        }));
+
+        replace_node(subtract, newSubtract);
+        subtract = newSubtract;
+    }
+
+    if (dequantization.convert != nullptr) {
+        std::shared_ptr<Node> newSubtract = optimizeSubtract(subtract);
+        newSubtract->set_output_type(0, originalPrecision, newSubtract->get_output_partial_shape(0));
+
+        replace_node(newSubtract, std::make_shared<op::TypeRelaxed<opset1::Subtract>>(
+            newSubtract->get_input_node_shared_ptr(0),
+            newSubtract->get_input_node_shared_ptr(1)));
+    }
+
+    // ngraph::pass::VisualizeTree("C:\\Projects\\temp\\test.transformed").run_on_module(std::vector<std::shared_ptr<ngraph::Function>>{ context.network });
+
+    // TODO: dequantization operation handling: getDequantizationOperations()
+    // TODO: any operation below can have several children - should be handled in getDequantizationOperations()
+
+    //std::shared_ptr<opset1::Multiply> multiply = as_type_ptr<opset1::Multiply>(subtract2->input_value(0).get_node_shared_ptr());
+    //if (multiply != nullptr) {
+    //    // TODO: move to canBeTransformed?
+    //    std::shared_ptr<opset1::Constant> scaleConstant = as_type_ptr<opset1::Constant>(multiply->input_value(1).get_node_shared_ptr());
+    //    const std::vector<float> scales = scaleConstant->cast_vector<float>();
+    //    if (std::any_of(scales.begin(), scales.end(), [](float value) { return value < 0.0; })) {
+    //        return;
+    //    }
+    //}
+
+    //std::shared_ptr<Node> parent = multiply->input_value(0).get_node_shared_ptr();
+    //std::shared_ptr<opset1::Subtract> subtract = as_type_ptr<opset1::Subtract>(parent);
+    //if (subtract != nullptr) {
+    //    parent = subtract->get_input_node_shared_ptr(0);
+    //}
+
+    //std::shared_ptr<opset1::Convert> convert = as_type_ptr<opset1::Convert>(parent);
+    //if (convert != nullptr) {
+    //    parent = convert->get_input_node_shared_ptr(0);
+    //}
+
+    //const Output<Node> dataNode = parent;
+
+    //// TODO: dequantization operation handling: insertDequantizationOperations()
+
+    //std::shared_ptr<Node> newPooling = subtract->copy_with_new_inputs({ dataNode });
+
+    //std::shared_ptr<Node> replacement = multiply->copy_with_new_inputs({
+    //    subtract ?
+    //        (convert ?
+    //            subtract->copy_with_new_inputs({convert->copy_with_new_inputs({newPooling}), subtract->get_input_node_shared_ptr(1)}) :
+    //            subtract->copy_with_new_inputs({newPooling, subtract->get_input_node_shared_ptr(1)})) :
+    //        (convert ? convert->copy_with_new_inputs({newPooling}) : newPooling),
+    //    multiply->input_value(1) });
+
+    //NetworkHelper::setOutDataPrecision(newPooling, newPooling->get_input_element_type(0));
+    //replace_node(subtract, replacement);
+
+    // ngraph::pass::VisualizeTree("C:\\Projects\\temp\\test.transformed").run_on_module(std::vector<std::shared_ptr<ngraph::Function>>{ context.network });
+
+    // TODO: NAMES!
+}
+
+} // namespace low_precision
+} // namespace pass
+} // namespace ngraph
---
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/low_precision_transformations/subtract_transformation.cpp
@@ -0,0 +1,41 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <vector>
+
+#include "low_precision_transformations/subtract_transformation.hpp"
+#include "common_test_utils/test_constants.hpp"
+
+using namespace LayerTestsDefinitions;
+
+namespace {
+const std::vector<InferenceEngine::Precision> netPrecisions = {
+        InferenceEngine::Precision::FP32,
+        InferenceEngine::Precision::FP16
+};
+
+const std::vector<InferenceEngine::details::LayerTransformation::Params> trasformationParamValues = {
+    LayerTestsUtils::LayerTransformationParamsFactory::createParams().setUpdatePrecisions(true),
+    LayerTestsUtils::LayerTransformationParamsFactory::createParams().setUpdatePrecisions(false),
+    LayerTestsUtils::LayerTransformationParamsFactory::createParamsU8I8()
+};
+
+const std::vector<LayerTestsUtils::LayerTransformation::LptVersion> versions = {
+    LayerTestsUtils::LayerTransformation::LptVersion::cnnNetwork,
+    LayerTestsUtils::LayerTransformation::LptVersion::nGraph
+};
+
+INSTANTIATE_TEST_CASE_P(DISABLED_LPT, SubtractTransformation,
+    ::testing::Combine(
+        ::testing::ValuesIn(netPrecisions),
+        ::testing::Values(InferenceEngine::SizeVector({ 1, 3, 16, 16 })),
+        ::testing::Values(CommonTestUtils::DEVICE_CPU),
+        ::testing::ValuesIn(trasformationParamValues),
+        ::testing::ValuesIn(versions)),
+    SubtractTransformation::getTestCaseName);
+}  // namespace
+
+
+
+
