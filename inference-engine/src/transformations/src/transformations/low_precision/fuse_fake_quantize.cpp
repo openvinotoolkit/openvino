056f3b75710622b7e76d792211df446269a271fa
Edward Shogulin <edward.shogulin@intel.com>
2020-06-29 00:10:35 +0300

    [LPT] FuseFakeQuantize transformation - NO TESTS

---
+++ inference-engine/src/transformations/src/transformations/low_precision/fuse_fake_quantize.cpp
@@ -0,0 +1,129 @@
+﻿// Copyright (C) 2018-2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "transformations/low_precision/fuse_fake_quantize.hpp"
+
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+#include <cassert>
+
+#include "transformations/low_precision/common/ie_lpt_exception.hpp"
+#include "transformations/low_precision/network_helper.hpp"
+
+// TODO: remove after debugging
+#include <ngraph/pass/visualize_tree.hpp>
+
+namespace ngraph {
+namespace pass {
+namespace low_precision {
+
+void FuseFakeQuantizeTransformation::registerMatcherIn(GraphRewrite &pass, TransformationContext &context) const {
+    // TODO: patterns below doesn't work - explore later:
+
+    // addPattern(
+    //    pass,
+    //    context,
+    //    make_op_pattern<opset1::FakeQuantize>({ make_op_label<opset1::Multiply>() }));
+
+    // addPattern(
+    //    pass,
+    //    context,
+    //    make_op_pattern<opset1::FakeQuantize>({ make_op_label<opset1::Subtract>() }));
+
+    // addPattern(
+    //    pass,
+    //    context,
+    //    make_op_pattern<opset1::FakeQuantize>({ make_op_label<opset1::Convert>() }));
+
+    // TODO: workaround
+    addSingleNodePattern<opset1::FakeQuantize>(pass, context);
+}
+
+void FuseFakeQuantizeTransformation::transform(TransformationContext& context, ngraph::pattern::Matcher &m) const {
+    // ngraph::pass::VisualizeTree("C:\\Projects\\temp\\test.transformed").run_on_module(std::vector<std::shared_ptr<ngraph::Function>>{ context.network });
+
+    std::shared_ptr<opset1::FakeQuantize> fakeQuantize = as_type_ptr<ngraph::opset1::FakeQuantize>(m.get_match_root());
+    const FakeQuantizeDequantization dequantization = ngraph::pass::low_precision::getDequantization(fakeQuantize->shared_from_this());
+    if (dequantization.empty()) {
+        return;
+    }
+
+    std::cout << "FuseFakeQuantizeTransformation::transform: STARTED: " << fakeQuantize->get_friendly_name() << std::endl;
+
+    //if (dequantization.multiply != nullptr) {
+    //    std::shared_ptr<opset1::Constant> multiplyConst = as_type_ptr<opset1::Constant>(dequantization.multiply->get_input_node_shared_ptr(1));
+
+    //    if (dequantization.multiply->get_input_shape(1).size() > 1ul) {
+    //        if (!multiplyConst->get_all_data_elements_bitwise_identical()) {
+    //            // TODO: can we fuse not only per tensor dequantization values?
+    //            std::cout << "FuseFakeQuantizeTransformation::transform: " << fakeQuantize->get_friendly_name() << ": " << dequantization.multiply->get_input_shape(1) << std::endl;
+    //            return;
+    //        }
+    //    }
+    //}
+
+    std::shared_ptr<Node> parent;
+    std::shared_ptr<Node> inputLowConst = fakeQuantize->get_input_node_shared_ptr(1);
+    std::shared_ptr<Node> inputHightConst = fakeQuantize->get_input_node_shared_ptr(2);
+    // std::shared_ptr<Node> outputLowConst = fakeQuantize->get_input_node_shared_ptr(3);
+    // std::shared_ptr<Node> outputHightConst = fakeQuantize->get_input_node_shared_ptr(4);
+
+    if (dequantization.multiply != nullptr) {
+        inputLowConst = fold<opset1::Divide>(inputLowConst, dequantization.multiply->get_input_node_shared_ptr(1));
+        inputHightConst = fold<opset1::Divide>(inputHightConst, dequantization.multiply->get_input_node_shared_ptr(1));
+
+        // TODO: dims hardcoded: only for 3D
+        Shape shape = inputLowConst->get_output_shape(0);
+        if ((shape.size() < fakeQuantize->get_output_shape(0).size()) && (shape.size() > 0)) {
+            inputLowConst = fold<opset1::Unsqueeze>(
+                inputLowConst,
+                std::make_shared<opset1::Constant>(
+                    ngraph::element::i32,
+                    Shape{ 1 },
+                    std::vector<size_t>({ 0ul })));
+
+            inputHightConst = fold<opset1::Unsqueeze>(
+                inputHightConst,
+                std::make_shared<opset1::Constant>(
+                    ngraph::element::i32,
+                    Shape{ 1 },
+                    std::vector<size_t>({ 0ul })));
+        }
+        parent = dequantization.multiply->get_input_node_shared_ptr(0);
+    }
+
+    if (dequantization.subtract != nullptr) {
+        // TODO: NO TESTS!!!
+        inputLowConst = fold<opset1::Add>(inputLowConst, dequantization.subtract->get_input_node_shared_ptr(1));
+        inputHightConst = fold<opset1::Add>(inputHightConst, dequantization.subtract->get_input_node_shared_ptr(1));
+        parent = dequantization.subtract->get_input_node_shared_ptr(0);
+    }
+
+    if (dequantization.convert != nullptr) {
+        parent = dequantization.convert->get_input_node_shared_ptr(0);
+    }
+
+    std::shared_ptr<opset1::FakeQuantize> newFakeQuantize = std::make_shared<ngraph::op::TypeRelaxed<opset1::FakeQuantize>>(
+        parent,
+        inputLowConst,
+        inputHightConst,
+        fakeQuantize->input_value(3),
+        fakeQuantize->input_value(4),
+        fakeQuantize->get_levels(),
+        fakeQuantize->get_auto_broadcast());
+    replace_node(fakeQuantize, newFakeQuantize);
+    newFakeQuantize->set_friendly_name(fakeQuantize->get_friendly_name());
+
+    std::cout << "FuseFakeQuantizeTransformation::transform: DONE: " << fakeQuantize->get_friendly_name() << std::endl;
+
+    // ngraph::pass::VisualizeTree("C:\\Projects\\temp\\test.transformed").run_on_module(std::vector<std::shared_ptr<ngraph::Function>>{ context.network });
+}
+
+} // namespace low_precision
+} // namespace pass
+} // namespace ngraph
---
+++ inference-engine/tests/ngraph_functions/src/low_precision_transformations/fuse_fake_quantize_function.cpp
@@ -0,0 +1,74 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "ngraph_functions/low_precision_transformations/fuse_fake_quantize_function.hpp"
+
+#include <ngraph/opsets/opset1.hpp>
+#include "ngraph_ops/multiply_add.hpp"
+#include "ngraph_ops/type_relaxed.hpp"
+#include "ngraph_functions/subgraph_builders.hpp"
+#include "transformations/low_precision/network_helper.hpp"
+
+namespace ngraph {
+namespace builder {
+namespace subgraph {
+
+using namespace ngraph::pass;
+
+std::shared_ptr<ngraph::Function> FuseFakeQuantizeFunction::getOriginal(
+    const ngraph::element::Type precision,
+    const ngraph::Shape& inputShape,
+    const ngraph::pass::low_precision::LayerTransformation::Params& params,
+    const FakeQuantizeOnData& fakeQuantizeOnData) {
+    const auto input = std::make_shared<ngraph::opset1::Parameter>(precision, ngraph::Shape(inputShape));
+    input->set_friendly_name("input");
+
+    Shape multiplyConstShape(4ul, 1ul);
+    multiplyConstShape[1] = inputShape[1];
+    std::shared_ptr<ngraph::opset1::Multiply> multiply = std::make_shared<ngraph::opset1::Multiply>(
+        input,
+        ngraph::builder::makeConstant(precision, multiplyConstShape, {}, true));
+
+    const auto fakeQuantize = ngraph::builder::makeFakeQuantize(
+        multiply, precision, fakeQuantizeOnData.quantizationLevel, fakeQuantizeOnData.constantShape,
+        fakeQuantizeOnData.inputLowValues, fakeQuantizeOnData.inputHighValues, fakeQuantizeOnData.outputLowValues, fakeQuantizeOnData.outputHighValues);
+    fakeQuantize->set_friendly_name("fakeQuantize");
+
+    ngraph::ResultVector results{ std::make_shared<ngraph::opset1::Result>(fakeQuantize) };
+    return std::make_shared<ngraph::Function>(results, ngraph::ParameterVector{ input }, "FakeQuantizeFunction");
+}
+
+std::shared_ptr<ngraph::Function> FuseFakeQuantizeFunction::getReference(
+    const ngraph::element::Type precision,
+    const ngraph::Shape& inputShape,
+    const ngraph::pass::low_precision::LayerTransformation::Params& params,
+    const FakeQuantizeOnData& fakeQuantizeOnData,
+    const std::vector<float>& expectedSubtractValues) {
+    const auto input = std::make_shared<ngraph::opset1::Parameter>(precision, ngraph::Shape(inputShape));
+    input->set_friendly_name("input");
+
+    // TODO: way to instantiate TypeRelaxed FakeQuantize
+    // TODO: use wrapper later
+    auto inputLowNode = ngraph::builder::makeConstant(
+        precision, fakeQuantizeOnData.constantShape, fakeQuantizeOnData.inputLowValues, fakeQuantizeOnData.inputLowValues.empty());
+    auto inputHighNode = ngraph::builder::makeConstant(
+        precision, fakeQuantizeOnData.constantShape, fakeQuantizeOnData.inputHighValues, fakeQuantizeOnData.inputHighValues.empty());
+    auto outputLowNode = ngraph::builder::makeConstant(
+        precision, fakeQuantizeOnData.constantShape, fakeQuantizeOnData.outputLowValues, fakeQuantizeOnData.outputLowValues.empty());
+    auto outputHighNode = ngraph::builder::makeConstant(
+        precision, fakeQuantizeOnData.constantShape, fakeQuantizeOnData.outputHighValues, fakeQuantizeOnData.outputHighValues.empty());
+    // auto fakeQuantize = std::make_shared<ngraph::opset1::FakeQuantize>(
+    //    input, inputLowNode, inputHighNode, outputLowNode, outputHighNode, fakeQuantizeOnData.quantizationLevel);
+    std::shared_ptr<ngraph::opset1::FakeQuantize> fakeQuantize = std::make_shared<ngraph::op::TypeRelaxed<ngraph::opset1::FakeQuantize>>(
+        input, inputLowNode, inputHighNode, outputLowNode, outputHighNode, fakeQuantizeOnData.quantizationLevel);
+
+    ngraph::pass::low_precision::NetworkHelper::setOutDataPrecision(fakeQuantize, params.precisionsOnActivations[0]);
+
+    ngraph::ResultVector results{ std::make_shared<ngraph::opset1::Result>(fakeQuantize) };
+    return std::make_shared<ngraph::Function>(results, ngraph::ParameterVector{ input }, "FakeQuantizeFunction");
+}
+
+}  // namespace subgraph
+}  // namespace builder
+}  // namespace ngraph
