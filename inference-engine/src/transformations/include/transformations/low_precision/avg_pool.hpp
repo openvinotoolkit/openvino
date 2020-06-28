aae87bab1ee0feb80f3a199d850c8bd882772afd
Edward Shogulin <edward.shogulin@intel.com>
2020-06-29 01:46:05 +0300

    [LPT] AvgPool transformation

---
+++ inference-engine/src/transformations/include/transformations/low_precision/avg_pool.hpp
@@ -0,0 +1,26 @@
+// Copyright (C) 2018-2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <algorithm>
+
+#include "transformations/low_precision/layer_transformation.hpp"
+
+namespace ngraph {
+namespace pass {
+namespace low_precision {
+
+// TODO: TransparentBaseTransformation
+class TRANSFORMATIONS_API AvgPoolTransformation : public LayerTransformation {
+public:
+    AvgPoolTransformation(const Params& params) : LayerTransformation(params) {}
+    ~AvgPoolTransformation() override {}
+    void registerMatcherIn(GraphRewrite& pass, TransformationContext& context) const override;
+    void transform(TransformationContext& context, ngraph::pattern::Matcher &m) const override;
+};
+
+} // namespace low_precision
+} // namespace pass
+} // namespace ngraph
