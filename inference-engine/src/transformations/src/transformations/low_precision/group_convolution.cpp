31d52b9c4f153f3aea4d0f70d0c0bf8bb645760b
Edward Shogulin <edward.shogulin@intel.com>
2020-06-29 11:19:04 +0300

    [LPT] GroupConvolution transformation

--- inference-engine/src/transformations/src/transformations/low_precision/group_convolution.cpp
+++ inference-engine/src/transformations/src/transformations/low_precision/group_convolution.cpp
@@ -24,19 +24,23 @@ GroupConvolutionTransformation::GroupConvolutionTransformation(const Params& par
 }
 
 void GroupConvolutionTransformation::registerMatcherIn(GraphRewrite &pass, TransformationContext &context) const {
-    addPattern(
-        pass,
-        context,
-        make_op_pattern<opset1::GroupConvolution>({ make_op_label<opset1::Multiply>(), make_op_label<opset1::FakeQuantize>()}));
+    // addPattern(
+    //    pass,
+    //    context,
+    //    make_op_pattern<opset1::GroupConvolution>({ make_op_label<opset1::Multiply>(), make_op_label<opset1::FakeQuantize>()}));
 
-    addPattern(
-        pass,
-        context,
-        make_op_pattern<opset1::GroupConvolution>({ make_op_label<opset1::Multiply>(), make_op_label<opset1::Multiply>() }));
+    // addPattern(
+    //    pass,
+    //    context,
+    //    make_op_pattern<opset1::GroupConvolution>({ make_op_label<opset1::Multiply>(), make_op_label<opset1::Multiply>() }));
+
+    addSingleNodePattern<opset1::GroupConvolution>(pass, context);
 }
 
 // TODO: not completed
 void GroupConvolutionTransformation::transform(TransformationContext &context, ngraph::pattern::Matcher &m) const {
+    // TODO: per channel queantization is supported - fix later
+
     auto convolution = m.get_match_root();
     if (!GroupConvolutionTransformation::canBeTransformed(context, convolution)) {
         return;
