93ac84f5dd108b90380c5054d61b01b28ba1bcb5
Alexander Chaiko <alexander.chaiko@intel.com>
2020-07-01 14:58:05 +0200

    [IE CLDNN] Restore crop heuristic to filter out shufflenet regression

--- inference-engine/thirdparty/clDNN/src/program.cpp
+++ inference-engine/thirdparty/clDNN/src/program.cpp
@@ -1164,6 +1164,21 @@ void program_impl::set_layout_optimizer_attributes(layout_optimizer& lo) {
             prim.type() != cldnn::mutable_data::type_id())
             can_use_fsv16 = false;
 
+        // WA to keep fsv16 layout disabled for some topologies where it leads to regressions.
+        if (prim.type() == cldnn::crop::type_id()) {
+            /*for (auto p : prim.get_dependencies()) {
+                std::cout << "+-- " << p->id() << "  " << type_to_str(p->get_primitive()) << std::endl;
+                for (auto pp : p->get_dependencies()) {
+                    std::cout << "+---- " << pp->id() << "  " << type_to_str(pp->get_primitive()) << std::endl;
+                }
+            }*/
+            if (prim.get_dependencies()[0]->is_type<reshape>() &&
+                prim.get_dependencies()[0]->get_dependencies()[0]->is_type<permute>()) {
+                //std::cout << "fsv16 suppressed" << std::endl;
+                can_use_fsv16 = false;
+            }
+        }
+
         if (prim.is_in_data_flow() &&
             prim.type() != cldnn::convolution::type_id() &&
             prim.type() != cldnn::pooling::type_id() &&
