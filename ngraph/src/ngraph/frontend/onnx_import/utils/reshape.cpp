0659b2f8a196323a198b7e9c70102458d3bc4ce6
Tomasz Socha <tomasz.socha@intel.com>
2020-07-03 12:59:03 +0200

    Fix builder::reshape while reshaping to scalar

--- ngraph/src/ngraph/frontend/onnx_import/utils/reshape.cpp
+++ ngraph/src/ngraph/frontend/onnx_import/utils/reshape.cpp
@@ -92,7 +92,7 @@ namespace ngraph
                 Shape node_shape = node->get_shape();
 
                 // If node is already a scalar, return original
-                if (node_shape.empty())
+                if (is_scalar(node_shape))
                 {
                     return node;
                 }
@@ -110,10 +110,7 @@ namespace ngraph
                         node->get_element_type(), ngraph::Shape{}, value);
                 }
 
-                const auto axis =
-                    default_opset::Constant::create(element::i64, ngraph::Shape{}, {0});
-
-                return std::make_shared<default_opset::Squeeze>(node, axis);
+                return builder::opset1::reshape(node, Shape{});
             }
 
         } // namespace  reshape
