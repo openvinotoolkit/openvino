7b8ae3a9114d8a6b209fbfaf42f93f61a38bb091
Zaytsev, Anton <anton.zaytsev@intel.com>
2020-07-03 14:02:56 +0300

    [IE TESTS] add single layer test ShapeOf

---
+++ inference-engine/tests/functional/plugin/shared/src/single_layer_tests/shape_of.cpp
@@ -0,0 +1,52 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <tuple>
+#include <string>
+#include <vector>
+#include <memory>
+#include <functional>
+#include <functional_test_utils/skip_tests_config.hpp>
+
+#include "ie_core.hpp"
+#include "ngraph_functions/utils/ngraph_helpers.hpp"
+
+#include "common_test_utils/common_utils.hpp"
+#include "functional_test_utils/blob_utils.hpp"
+#include "functional_test_utils/plugin_cache.hpp"
+#include "functional_test_utils/layer_test_utils.hpp"
+
+#include "single_layer_tests/shape_of.hpp"
+
+namespace LayerTestsDefinitions {
+
+    std::string ShapeOfLayerTest::getTestCaseName(testing::TestParamInfo<shapeOfParams> obj) {
+        InferenceEngine::SizeVector inputShapes;
+        InferenceEngine::Precision inputPrecision;
+        std::string targetDevice;
+        std::tie(inputPrecision, inputShapes, targetDevice) = obj.param;
+        std::ostringstream result;
+        result << "IS=" << CommonTestUtils::vec2str(inputShapes) << "_";
+        result << "Precision=" << inputPrecision.name() << "_";
+        result << "TargetDevice=" << targetDevice;
+        return result.str();
+    }
+
+    void ShapeOfLayerTest::SetUp() {
+        InferenceEngine::SizeVector inputShapes;
+        InferenceEngine::Precision inputPrecision;
+        std::tie(inputPrecision, inputShapes, targetDevice) = this->GetParam();
+        auto inType = FuncTestUtils::PrecisionUtils::convertIE2nGraphPrc(inputPrecision);
+        auto param = ngraph::builder::makeParams(inType, {inputShapes});
+        auto paramOuts = ngraph::helpers::convert2OutputVector(ngraph::helpers::castOps2Nodes<ngraph::op::Parameter>(param));
+        auto shapeOf = std::make_shared<ngraph::op::v3::ShapeOf>(paramOuts[0], inType);
+        ngraph::ResultVector results{std::make_shared<ngraph::opset1::Result>(shapeOf)};
+        function = std::make_shared<ngraph::Function>(results, param, "shapeOf");
+    }
+
+    TEST_P(ShapeOfLayerTest, CompareWithRefs) {
+        Run();
+    };
+
+}  // namespace LayerTestsDefinitions
