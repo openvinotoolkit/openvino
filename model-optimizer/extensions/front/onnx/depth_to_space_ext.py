389c8304042b4079053a1c16061e99c88fd17f57
Vafin, Maxim <maxim.vafin@intel.com>
2020-07-06 18:10:51 +0300

    Add assert that block size is specified

--- model-optimizer/extensions/front/onnx/depth_to_space_ext.py
+++ model-optimizer/extensions/front/onnx/depth_to_space_ext.py
@@ -29,6 +29,8 @@ class DepthToSpaceFrontExtractor(FrontExtractorOp):
         # update the attributes of the node
         node_name = node.soft_get('name', node.id)
         block_size = onnx_attr(node, 'blocksize', 'i', default=None)
+        assert block_size is not None, \
+            'DepthToSpace should have "blocksize" attribute specified for node {}'.format(node_name)
         onnx_mode = onnx_attr(node, 'mode', 's', default=b'DCR').decode()
         assert onnx_mode in ['DCR', 'CRD'], 'Unrecognized mode provided for DepthToSpace node {}'.format(node_name)
         if onnx_mode == 'DCR':
