82b1d688d68eba5c289f786c3f971e16b7d630b5
Irina Efode <irina.efode@intel.com>
2020-07-07 11:47:13 +0300

    [IE TESTS] Add Convert, ConvertLike single layer tests. Refactoring `Range` (#1212)

---
+++ inference-engine/tests/functional/plugin/shared/include/subgraph_tests/range_add.hpp
@@ -0,0 +1,26 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <tuple>
+#include <string>
+#include <vector>
+#include <memory>
+
+#include "functional_test_utils/layer_test_utils.hpp"
+#include "ngraph_functions/builders.hpp"
+
+#include "single_layer_tests/range.hpp"
+
+namespace LayerTestsDefinitions {
+
+class RangeAddSubgraphTest : public testing::WithParamInterface<RangeParams>,
+                             public LayerTestsUtils::LayerTestsCommon {
+public:
+    static std::string getTestCaseName(testing::TestParamInfo<RangeParams> obj);
+protected:
+    void SetUp() override;
+};
+}  // namespace LayerTestsDefinitions
