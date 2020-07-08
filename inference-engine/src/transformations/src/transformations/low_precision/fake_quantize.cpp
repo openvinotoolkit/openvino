c5491a0bbc3139b7af8c6a3b5932be6aefa61c40
Edward Shogulin <edward.shogulin@intel.com>
2020-07-08 10:41:17 +0300

    [LPT] FakeQuantize transformation

--- inference-engine/src/transformations/src/transformations/low_precision/fake_quantize.cpp
+++ inference-engine/src/transformations/src/transformations/low_precision/fake_quantize.cpp
@@ -137,148 +137,23 @@ void FakeQuantizeTransformation::transform(TransformationContext& context, ngrap
 
     // TODO: for debuging only - remove later
     auto dequantize = as_type_ptr<ngraph::Node>(std::get<1>(QDQ));
-    dequantize->set_friendly_name(layer->get_friendly_name());
+    // dequantize->set_friendly_name(layer->get_friendly_name());
 
 
     // TODO: Get rid of this.
-    const std::string friendlyName = layer->get_friendly_name();
-    context.quantizedFakeQuantizeNames.insert(friendlyName);
+    // const std::string friendlyName = layer->get_friendly_name();
+    // context.quantizedFakeQuantizeNames.insert(friendlyName);
 
     // ngraph::pass::VisualizeTree("C:\\Projects\\temp\\test.transformed").run_on_module(transformedModule);
 
     // std::cout << "FakeQuantizeTransformation::transform: " << layer->get_friendly_name() << std::endl;
+
+    updateOutput(context, dequantize, layer);
 }
 
 bool FakeQuantizeTransformation::isPrecisionPreserved(std::shared_ptr<Node>) const noexcept {
     return false;
 }
-
-#if 0 // TODO LPT-TO-NGRAPH
-void FakeQuantizeTransformation::fuseScaleShift(TransformationContext& context, CNNLayerPtr fakeQuantizeLayer,
-                                                CNNLayerPtr scaleShift) const {
-    // TODO: add check if previous blobs precision is enough to store current values
-    const Blob::Ptr scalesBlob = CNNNetworkHelper::getBlob(scaleShift, "weights");
-    std::shared_ptr<float> scalesBufferPtr = CNNNetworkHelper::getFloatData(scalesBlob);
-
-    const Blob::Ptr shiftsBlob = CNNNetworkHelper::getBlob(scaleShift, "biases");
-    std::shared_ptr<float> shiftsBufferPtr = CNNNetworkHelper::getFloatData(shiftsBlob);
-
-    if (scalesBlob->size() != shiftsBlob->size())
-        THROW_TRANSFORMATION_EXCEPTION << "Scales and shifts values count are different for " << scaleShift->name;
-
-    const float* shiftsBuffer = shiftsBufferPtr.get();
-    const float* scalesBuffer = scalesBufferPtr.get();
-    // Don't fuse when there is a negative scale, because it leads to invalid results of FQ
-    for (size_t i = 0lu; i < scalesBlob->size(); ++i) {
-        if (scalesBuffer[i] <= 0.0f) return;
-    }
-
-    CNNLayerPtr inputLow = CNNNetworkHelper::getParent(*fakeQuantizeLayer, 1);
-    CNNLayerPtr inputHigh = CNNNetworkHelper::getParent(*fakeQuantizeLayer, 2);
-
-    Layout layout;
-    size_t channelIndex;
-    const DataPtr insData = scaleShift->insData[0].lock();
-    if (insData == nullptr) {
-        THROW_IE_LPT_EXCEPTION(*scaleShift) << "input data is absent";
-    }
-    const size_t inputDims = insData->getDims().size();
-    switch (inputDims) {
-        case 5: {
-            layout = Layout::NCDHW;
-            channelIndex = 1ul;
-            break;
-        }
-        case 4: {
-            layout = Layout::NCHW;
-            channelIndex = 1ul;
-            break;
-        }
-        case 3: {
-            layout = Layout::BLOCKED;
-            channelIndex = 1ul;
-            break;
-        }
-        case 2: {
-            layout = Layout::NC;
-            channelIndex = 1ul;
-            break;
-        }
-        case 1: {
-            layout = Layout::C;
-            channelIndex = 0ul;
-            break;
-        }
-        default: THROW_TRANSFORMATION_EXCEPTION << "FakeQuantizeTransform: unexpected dimensions count " << inputDims << " in ScaleShift optimization";
-    }
-    std::vector<size_t> dims(inputDims, 1lu);
-    dims[channelIndex] = scalesBlob->size();
-
-    const QuantizationDetails quantizationDetails = QuantizationDetails::getDetails(*fakeQuantizeLayer);
-
-    Blob::Ptr targetInputLowBufferPtr = reshapeWeightsIntervalConst(*inputLow, dims, layout);
-    auto targetInputLowBuffer = CNNNetworkHelper::getFloatData(targetInputLowBufferPtr);
-    Blob::Ptr targetInputHighBufferPtr = reshapeWeightsIntervalConst(*inputHigh, dims, layout);
-    auto targetInputHighBuffer = CNNNetworkHelper::getFloatData(targetInputHighBufferPtr);
-
-    for (size_t i = 0lu; i < scalesBlob->size(); ++i) {
-        auto q_lo = quantizationDetails.getInputLowValue(i);
-        auto q_hi = quantizationDetails.getInputHighValue(i);
-        auto sc = scalesBlob->size() == 1 ? scalesBuffer[0] : scalesBuffer[i];
-        auto sh = shiftsBlob->size() == 1 ? shiftsBuffer[0] : shiftsBuffer[i];
-        targetInputLowBuffer.get()[i] = (q_lo - sh) / sc;
-        targetInputHighBuffer.get()[i] = (q_hi - sh) / sc;
-    }
-
-    CNNNetworkHelper::fillBlobByFP32(targetInputLowBufferPtr, targetInputLowBuffer.get());
-    CNNNetworkHelper::fillBlobByFP32(targetInputHighBufferPtr, targetInputHighBuffer.get());
-
-    reshapeFakeQuantize(*fakeQuantizeLayer, dims, layout);
-
-    CNNNetworkHelper::removeLayer(context.network, scaleShift);
-    context.removeLayer(*scaleShift);
-}
-
-Blob::Ptr FakeQuantizeTransformation::reshapeWeightsIntervalConst(CNNLayer& constLayer, const std::vector<size_t>& dims,
-                                                                  const Layout layout) {
-    if (constLayer.blobs.size() != 1lu) {
-        THROW_TRANSFORMATION_EXCEPTION << "Unexpected blobs count " << constLayer.blobs.size() << " for layer " << constLayer.name;
-    }
-    if (constLayer.outData.size() != 1lu)
-        THROW_TRANSFORMATION_EXCEPTION << "Unexpected outputs for layer " << constLayer.name;
-
-    auto it = constLayer.blobs.find("custom");
-    if (it == constLayer.blobs.end()) THROW_TRANSFORMATION_EXCEPTION << "blob 'custom' was not found for layer " << constLayer.name;
-
-    const Precision& srcPrecision = it->second->getTensorDesc().getPrecision();
-
-    Blob::Ptr targetBlob = CNNNetworkHelper::makeNewBlobPtr({srcPrecision, dims, layout});
-    targetBlob->allocate();
-    constLayer.blobs["custom"] = targetBlob;
-
-    constLayer.outData[0]->reshape(dims, layout);
-
-    return targetBlob;
-}
-
-void FakeQuantizeTransformation::reshapeFakeQuantize(
-        CNNLayer& fakeQuantizeLayer,
-        const std::vector<size_t>& dims,
-        const Layout layout) {
-    DataPtr inputLowData = fakeQuantizeLayer.insData[1].lock();
-    if (inputLowData == nullptr) {
-        THROW_TRANSFORMATION_EXCEPTION << "input low interval data is absent";
-    }
-    inputLowData->reshape(dims, layout);
-
-    DataPtr inputHighData = fakeQuantizeLayer.insData[2].lock();
-    if (inputHighData == nullptr) {
-        THROW_TRANSFORMATION_EXCEPTION << "input hight interval data is absent";
-    }
-    inputHighData->reshape(dims, layout);
-}
-
-#endif
 
 } // namespace low_precision
 } // namespace pass
