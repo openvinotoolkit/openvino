90842b3759d93b2068caa972b9b93f400e91de58
VGavrilov <vladimir.gavrilov@intel.com>
2020-07-13 15:54:21 +0300

    Started to write conversion of Power+ReduceSum+Add+Power+Power+Multiply to NormalizeL2.

---
+++ inference-engine/src/transformations/include/transformations/convert_power_sum_add_rsqrt_to_normalizel2.hpp
@@ -0,0 +1,35 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <vector>
+#include <memory>
+#include <algorithm>
+
+#include <transformations_visibility.hpp>
+
+#include <ngraph/pass/graph_rewrite.hpp>
+#include <ngraph/opsets/opset1.hpp>
+#include <ngraph/validation_util.hpp>
+#include <ngraph/rt_info.hpp>
+
+
+namespace ngraph {
+namespace pass {
+
+class TRANSFORMATIONS_API ConvertPowerSumAddRsqrtToNormalizeL2;
+
+}  // namespace pass
+}  // namespace ngraph
+
+class ngraph::pass::ConvertPowerSumAddRsqrtToNormalizeL2: public ngraph::pass::GraphRewrite {
+public:
+    ConvertPowerSumAddRsqrtToNormalizeL2() : GraphRewrite() {
+        convert_to_normalize_l2();
+    }
+
+private:
+    void convert_to_normalize_l2();
+};
---
+++ inference-engine/src/transformations/src/transformations/convert_power_sum_add_rsqrt_to_normalizel2.cpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2018-2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "transformations/convert_power_sum_add_rsqrt_to_normalizel2.hpp"
+
+#include <memory>
+#include <vector>
+
+void ngraph::pass::ConvertPowerSumAddRsqrtToNormalizeL2::convert_to_normalize_l2() {
+}
