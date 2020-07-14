cd69bb240a3e06b53c7541944130ca58469abdb0
VGavrilov <vladimir.gavrilov@intel.com>
2020-07-14 09:24:02 +0300

    Some additions.

--- inference-engine/src/transformations/src/transformations/convert_power_sum_add_rsqrt_to_normalizel2.cpp
+++ inference-engine/src/transformations/src/transformations/convert_power_sum_add_rsqrt_to_normalizel2.cpp
@@ -6,6 +6,9 @@
 
 #include <memory>
 #include <vector>
+#include <ngraph/ngraph.hpp>
 
 void ngraph::pass::ConvertPowerSumAddRsqrtToNormalizeL2::convert_to_normalize_l2() {
+    auto data   = std::make_shared<pattern::op::Label>(element::f32, Shape{1, 1, 1, 1});
+    auto square = ngraph::op::Constant::create(element::f32, Shape{1, 1, 1, 1}, {2.0});
 }
