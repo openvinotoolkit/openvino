8c204a9eb7bb1101dfc31acc5f042e99cd0a5222
Your Name <you@example.com>
2020-07-14 16:56:14 +0300

    Fix mish value propogation unit test

--- ngraph/test/op_eval/mish.cpp
+++ ngraph/test/op_eval/mish.cpp
@@ -32,11 +32,11 @@ using namespace ngraph;
 TEST(op_eval, mish_0D)
 {
     auto p = make_shared<op::Parameter>(element::f32, Shape{});
-    auto mish = make_shared<op::v4::Mish>(p, element::f32);
+    auto mish = make_shared<op::v4::Mish>(p);
     auto fun = make_shared<Function>(OutputVector{mish}, ParameterVector{p});
 
     std::vector<std::vector<float>> inputs{{-1.0}, {1.0}, {20.0}};
-    std::vector<std::vector<float>> expected_result{{0}, {0}, {0}};
+    std::vector<std::vector<float>> expected_result{{-0.303401}, {0.86509835720062256}, {20.0}};
 
     for (size_t i = 0; i < inputs.size(); i++)
     {
@@ -44,8 +44,8 @@ TEST(op_eval, mish_0D)
         ASSERT_TRUE(
             fun->evaluate({result}, {make_host_tensor<element::Type_t::f32>(Shape{}, inputs[i])}));
         EXPECT_EQ(result->get_element_type(), element::f32);
-        EXPECT_EQ(result->get_shape(), (Shape{1}));
+        EXPECT_EQ(result->get_shape(), (Shape{}));
         auto result_data = read_vector<float>(result);
-        ASSERT_EQ(result_data, expected_result[i]);
+        EXPECT_NEAR(result_data[0], expected_result[i][0], 0.3);
     }
 }
