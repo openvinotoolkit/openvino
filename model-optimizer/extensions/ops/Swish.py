133b3c862ba9e45aceb47f12810c5d8d9a1c0915
Chetverikov Anton <anton.chetverikov@intel.com>
2020-07-15 15:58:11 +0300

    Implement Swish operation in MO

---
+++ model-optimizer/extensions/ops/Swish.py
@@ -0,0 +1,46 @@
+"""
+ Copyright (C) 2018-2020 Intel Corporation
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
+
+import numpy as np
+
+from mo.graph.graph import Node, Graph
+from mo.ops.op import Op
+
+
+class Swish(Op):
+    op = 'Swish'
+    enabled = False
+
+    def __init__(self, graph: Graph, attrs: dict):
+        super().__init__(graph, {
+            'op': self.op,
+            'type': self.op,
+            'version': 'opset4',
+            'infer': self.infer,
+            'in_ports_count': 2,
+            'out_ports_count': 1,
+        }, attrs)
+
+    @staticmethod
+    def infer(node: Node):
+        input_node = node.in_node(0)
+        data_value = node.in_port(0).data.get_value()
+        beta_value = 1.0 if node.in_port(0).disconnected() else node.in_port(0).data.get_value()
+        if data_value is not None and beta_value is not None:
+            node.out_port(0).data.set_value(data_value / (1.0 + np.exp(beta_value * data_value * -1.0)))
+            return
+
+        node.out_port(0).data.set_shape(input_node.shape.copy())
