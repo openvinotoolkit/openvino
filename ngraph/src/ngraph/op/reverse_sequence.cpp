087b9ab3b4359dae03fa2811990baef614d61b9d
Jan Iwaszkiewicz <jan.iwaszkiewicz@intel.com>
2020-07-15 12:48:53 +0200

    Reverse Sequence code clean up (#1303)

--- ngraph/src/ngraph/op/reverse_sequence.cpp
+++ ngraph/src/ngraph/op/reverse_sequence.cpp
@@ -62,7 +62,7 @@ void op::ReverseSequence::validate_and_infer_types()
         this,
         indices_rank.is_dynamic() || indices_rank.get_length() == 1,
         "Sequence indices must be a 1-dimensional tensor (sequence indices shape: ",
-        get_input_partial_shape(1),
+        indices_shape,
         ").");
 
     PartialShape output_shape{input_shape};
@@ -94,7 +94,5 @@ void op::ReverseSequence::validate_and_infer_types()
 shared_ptr<Node> op::ReverseSequence::clone_with_new_inputs(const OutputVector& new_args) const
 {
     check_new_args_count(this, new_args);
-    auto res =
-        make_shared<ReverseSequence>(new_args.at(0), new_args.at(1), m_batch_axis, m_seq_axis);
-    return move(res);
+    return make_shared<ReverseSequence>(new_args.at(0), new_args.at(1), m_batch_axis, m_seq_axis);
 }
