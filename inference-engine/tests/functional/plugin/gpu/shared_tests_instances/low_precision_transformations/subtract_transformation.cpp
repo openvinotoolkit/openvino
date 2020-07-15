05e4684259676de8f7b2ad27deb3f25a6e81dcbb
Zinoviev, Vladimir <vladimir.zinoviev@intel.com>
2020-07-15 20:42:53 +0300

    [LPT] temporary comment subtract transformation test for CI build

--- inference-engine/tests/functional/plugin/gpu/shared_tests_instances/low_precision_transformations/subtract_transformation.cpp
+++ inference-engine/tests/functional/plugin/gpu/shared_tests_instances/low_precision_transformations/subtract_transformation.cpp
@@ -23,14 +23,14 @@ const std::vector<LayerTestsUtils::LayerTransformation::LptVersion> versions = {
     // LayerTestsUtils::LayerTransformation::LptVersion::nGraph
 };
 
-INSTANTIATE_TEST_CASE_P(DISABLED_LPT, MultiplyTransformation,
-    ::testing::Combine(
-        ::testing::ValuesIn(netPrecisions),
-        ::testing::Values(InferenceEngine::SizeVector({ 1, 3, 16, 16 })),
-        ::testing::Values(CommonTestUtils::DEVICE_GPU),
-        ::testing::ValuesIn(trasformationParamValues),
-        ::testing::ValuesIn(versions)),
-    MultiplyTransformation::getTestCaseName);
+//INSTANTIATE_TEST_CASE_P(DISABLED_LPT, MultiplyTransformation,
+//    ::testing::Combine(
+//        ::testing::ValuesIn(netPrecisions),
+//        ::testing::Values(InferenceEngine::SizeVector({ 1, 3, 16, 16 })),
+//        ::testing::Values(CommonTestUtils::DEVICE_GPU),
+//        ::testing::ValuesIn(trasformationParamValues),
+//        ::testing::ValuesIn(versions)),
+//    MultiplyTransformation::getTestCaseName);
 }  // namespace
 
 
