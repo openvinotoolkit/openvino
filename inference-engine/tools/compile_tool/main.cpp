df4709a305af8238a675a2836e5a895629a368cf
Nikita Kudriavtsev <nikita.kudriavtsev@intel.com>
2020-07-16 19:54:53 +0300

    [IE][VPU]: CMX limit compile option (#1268)
    In some networks, mvTensor would request a large CMX-DMA transfer (<512K). That starves DMA for other timing critical tasks such as SIPP. Limit CMX-DMA request size as an option in myriad_compile:
    * Add compile option TILING_CMX_LIMIT_KB
    * Declare compile option TILING_CMX_LIMIT_KB in IE tools (compile_tool and vpu_compile)
    * Add tests for compile option TILING_CMX_LIMIT_KB. Small fix for naming behavior tests.

--- inference-engine/tools/compile_tool/main.cpp
+++ inference-engine/tools/compile_tool/main.cpp
@@ -39,6 +39,9 @@ static constexpr char number_of_shaves_message[] = "Optional. Specifies number o
 static constexpr char number_of_cmx_slices_message[] = "Optional. Specifies number of CMX slices."
                                                        " Should be set with \"VPU_NUMBER_OF_SHAVES\"."
                                                        " Overwrites value from config.";
+static constexpr char tiling_cmx_limit_message[] = "Optional. Specifies CMX limit for data tiling."
+                                                       " Value should be equal or greater than -1."
+                                                       " Overwrites value from config.";
 static constexpr char inputs_precision_message[] = "Optional. Specifies precision for all input layers of the network."
                                                    " Supported values: FP32, FP16, U8. Default value: FP16.";
 static constexpr char outputs_precision_message[] = "Optional. Specifies precision for all output layers of the network."
@@ -70,6 +73,7 @@ DEFINE_string(ol, "", outputs_layout_message);
 DEFINE_string(VPU_MYRIAD_PLATFORM, "", platform_message);
 DEFINE_string(VPU_NUMBER_OF_SHAVES, "", number_of_shaves_message);
 DEFINE_string(VPU_NUMBER_OF_CMX_SLICES, "", number_of_cmx_slices_message);
+DEFINE_string(VPU_TILING_CMX_LIMIT_KB, "", tiling_cmx_limit_message);
 DEFINE_string(DLA_ARCH_NAME, "", dla_arch_name);
 
 static void showUsage() {
@@ -91,6 +95,7 @@ static void showUsage() {
     std::cout << "      -VPU_MYRIAD_PLATFORM       <value>     "   << platform_message             << std::endl;
     std::cout << "      -VPU_NUMBER_OF_SHAVES      <value>     "   << number_of_shaves_message     << std::endl;
     std::cout << "      -VPU_NUMBER_OF_CMX_SLICES  <value>     "   << number_of_cmx_slices_message << std::endl;
+    std::cout << "      -VPU_TILING_CMX_LIMIT_KB   <value>     "   << tiling_cmx_limit_message     << std::endl;
     std::cout << "    DLA options:                             "                                   << std::endl;
     std::cout << "      -DLA_ARCH_NAME             <value>     "   << dla_arch_name                << std::endl;
     std::cout << std::endl;
@@ -162,6 +167,10 @@ static std::map<std::string, std::string> configure(const std::string &configFil
 
     if (!FLAGS_VPU_NUMBER_OF_CMX_SLICES.empty()) {
         config[VPU_CONFIG_KEY(NUMBER_OF_CMX_SLICES)] = FLAGS_VPU_NUMBER_OF_CMX_SLICES;
+    }
+
+    if (!FLAGS_VPU_TILING_CMX_LIMIT_KB.empty()) {
+        config[VPU_CONFIG_KEY(TILING_CMX_LIMIT_KB)] = FLAGS_VPU_TILING_CMX_LIMIT_KB;
     }
 
     if (!FLAGS_DLA_ARCH_NAME.empty()) {
