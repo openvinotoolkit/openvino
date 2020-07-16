a103611482ae116eaec619685fb9d480f8395b31
Chetverikov Anton <anton.chetverikov@intel.com>
2020-07-16 11:55:52 +0300

    Add extenders for Swish operation

---
+++ model-optimizer/extensions/front/mxnet/Swish_ext.py
@@ -0,0 +1,37 @@
+"""
+ Copyright (C) 2018-2020 Intel Corporation
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
+
+from mo.front.extractor import FrontExtractorOp
+from mo.front.mxnet.extractors.utils import get_mxnet_layer_attrs
+from extensions.ops.Swish import Swish
+
+
+class SwishExtractor(FrontExtractorOp):
+    op = 'Swish'
+    enabled = True
+
+    @classmethod
+    def extract(cls, node):
+        attrs = get_mxnet_layer_attrs(node.symbol_dict)
+
+        update_attrs = {
+            'type': 'Swish',
+            'beta': attrs.float('beta', 1.0)
+        }
+
+        # update the attributes of the node
+        Swish.update_node_stat(node, update_attrs)
+        return cls.enabled
