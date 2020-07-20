3e03f292b5e4faee6e98e78040541a6f985893fc
Jozef Daniecki <jozef.daniecki@intel.com>
2020-07-20 10:18:03 +0200

    Add Acosh operator to ONNX importer opset4 (#1351)

--- ngraph/src/ngraph/frontend/onnx_import/default_opset.hpp
+++ ngraph/src/ngraph/frontend/onnx_import/default_opset.hpp
@@ -1,9 +1,9 @@
-#include "ngraph/opsets/opset3.hpp"
+#include "ngraph/opsets/opset4.hpp"
 
 namespace ngraph
 {
     namespace onnx_import
     {
-        namespace default_opset = ngraph::opset3;
+        namespace default_opset = ngraph::opset4;
     }
 }
