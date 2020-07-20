13a1cb2d6871f2fbd698c4c1774a2dd99ee3523b
Kudriavtsev Nikita <nikita.kudriavtsev@intel.com>
2020-07-20 21:30:31 +0300

    Added BlockingQueue

---
+++ inference-engine/thirdparty/movidius/XLink/new/include/blocking_queue.hpp
@@ -0,0 +1,21 @@
+// Copyright (C) 2018-2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <queue>
+
+namespace XLinkPro {
+    
+    template <typename T>
+    class BlockingQueue {
+    public:
+    private:
+
+        std::queue<T> _queue;
+
+
+    };
+}
+
