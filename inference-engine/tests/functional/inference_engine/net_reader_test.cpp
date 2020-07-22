767a49cf42324c32c24d11cebfcdbbb1e55534b0
vurusovs <vitaliy.urusovskij@intel.com>
2020-07-22 17:41:15 +0300

    [IE Tests] Add `NetReaderTest.ThrowIfIRVersionGreaterThan10` func test

--- inference-engine/tests/functional/inference_engine/net_reader_test.cpp
+++ inference-engine/tests/functional/inference_engine/net_reader_test.cpp
@@ -221,6 +221,43 @@ TEST(NetReaderTest, IRSupportModelDetection) {
     }
 }
 
+TEST(NetReaderTest, ThrowIfIRVersionGreaterThan10) {
+    InferenceEngine::Core ie;
+
+    static char const *model = R"V0G0N(<net name="Network" version="11" some_attribute="Test Attribute">
+    <layers>
+        <layer name="in1" type="Parameter" id="0" version="opset1">
+            <data element_type="f32" shape="1,3,22,22"/>
+            <output>
+                <port id="0" precision="FP32">
+                    <dim>1</dim>
+                    <dim>3</dim>
+                    <dim>22</dim>
+                    <dim>22</dim>
+                </port>
+            </output>
+        </layer>
+        <layer name="output" type="Result" id="1" version="opset1">
+            <input>
+                <port id="0" precision="FP32">
+                    <dim>1</dim>
+                    <dim>3</dim>
+                    <dim>22</dim>
+                    <dim>22</dim>
+                </port>
+            </input>
+        </layer>
+    </layers>
+    <edges>
+        <edge from-layer="0" from-port="0" to-layer="1" to-port="0"/>
+    </edges>
+</net>
+)V0G0N";
+
+    InferenceEngine::Blob::CPtr weights;
+    ASSERT_THROW(ie.ReadNetwork(model, weights), InferenceEngine::details::InferenceEngineException);
+}
+
 std::string getTestCaseName(testing::TestParamInfo<NetReaderTestParams> testParams) {
     InferenceEngine::SizeVector dims;
     InferenceEngine::Precision prc;
