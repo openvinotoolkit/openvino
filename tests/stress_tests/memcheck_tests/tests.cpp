11c816e3ce0de3722b886f2c395c525e2f676c92
vurusovs <vitaliy.urusovskij@intel.com>
2020-07-22 16:18:15 +0300

    [Stress] Organize MemCheckTests through test class SetUp

--- tests/stress_tests/memcheck_tests/tests.cpp
+++ tests/stress_tests/memcheck_tests/tests.cpp
@@ -14,46 +14,51 @@
 using namespace InferenceEngine;
 
 
-#define checkRefVmValues()                                                                                  \
-    if (!Environment::Instance().getCollectResultsOnly()) {                                                 \
-        ASSERT_GT(test_refs.references[VMSIZE], 0) << "Reference value of VmSize is less than 0. Value: "   \
-                                           << test_refs.references[VMSIZE];                                 \
-        ASSERT_GT(test_refs.references[VMPEAK], 0) << "Reference value of VmPeak is less than 0. Value: "   \
-                                           << test_refs.references[VMPEAK];                                 \
-        ASSERT_GT(test_refs.references[VMRSS], 0) << "Reference value of VmRSS is less than 0. Value: "     \
-                                          << test_refs.references[VMRSS];                                   \
-        ASSERT_GT(test_refs.references[VMHWM], 0) << "Reference value of VmHWM is less than 0. Value: "     \
-                                          << test_refs.references[VMHWM];                                   \
-    }
-
 class MemCheckTestSuite : public ::testing::TestWithParam<TestCase> {
+public:
+    std::string test_name, model, model_name, device;
+    TestReferences test_refs;
+
+    void SetUp() override {
+        const ::testing::TestInfo* const test_info = ::testing::UnitTest::GetInstance()->current_test_info();
+        test_name = std::string(test_info->name()).substr(0, std::string(test_info->name()).find('/'));
+        //const std::string full_test_name = std::string(test_info->test_case_name()) + "." + std::string(test_info->name());
+
+        const auto& test_params = GetParam();
+        model = test_params.model;
+        model_name = test_params.model_name;
+        device = test_params.device;
+
+        test_refs.collect_vm_values_for_test(test_name, test_params);
+        if (!Environment::Instance().getCollectResultsOnly()) {
+            ASSERT_GT(test_refs.references[VMSIZE], 0) << "Reference value of VmSize is less than 0. Value: "
+                                               << test_refs.references[VMSIZE];
+            ASSERT_GT(test_refs.references[VMPEAK], 0) << "Reference value of VmPeak is less than 0. Value: "
+                                               << test_refs.references[VMPEAK];
+            ASSERT_GT(test_refs.references[VMRSS], 0) << "Reference value of VmRSS is less than 0. Value: "
+                                              << test_refs.references[VMRSS];
+            ASSERT_GT(test_refs.references[VMHWM], 0) << "Reference value of VmHWM is less than 0. Value: "
+                                              << test_refs.references[VMHWM];
+        }
+    }
 };
 
 // tests_pipelines/tests_pipelines.cpp
 TEST_P(MemCheckTestSuite, create_exenetwork) {
-    const ::testing::TestInfo* const test_info = ::testing::UnitTest::GetInstance()->current_test_info();
-    const std::string test_name = std::string(test_info->name()).substr(0, std::string(test_info->name()).find('/'));
-    //const std::string full_test_name = std::string(test_info->test_case_name()) + "." + std::string(test_info->name());
-    auto test_params = GetParam();
-
-    TestReferences test_refs;
-    test_refs.collect_vm_values_for_test(test_name, test_params);
-    checkRefVmValues();
-
-    log_info("Create ExecutableNetwork from network: \"" << test_params.model
-                                                         << "\" for device: \"" << test_params.device << "\"");
+    log_info("Create ExecutableNetwork from network: \"" << model
+                                                         << "\" for device: \"" << device << "\"");
     auto test_pipeline = [&]{
         MemCheckPipeline memCheckPipeline;
 
         Core ie;
-        ie.GetVersions(test_params.device);
-        CNNNetwork cnnNetwork = ie.ReadNetwork(test_params.model);
-        ExecutableNetwork exeNetwork = ie.LoadNetwork(cnnNetwork, test_params.device);
+        ie.GetVersions(device);
+        CNNNetwork cnnNetwork = ie.ReadNetwork(model);
+        ExecutableNetwork exeNetwork = ie.LoadNetwork(cnnNetwork, device);
 
         log_info("Memory consumption after LoadNetwork:");
         memCheckPipeline.record_measures(test_name);
 
-        log_debug(memCheckPipeline.get_reference_record_for_test(test_name, test_params.model_name, test_params.device));
+        log_debug(memCheckPipeline.get_reference_record_for_test(test_name, model_name, device));
         return memCheckPipeline.measure();
     };
 
@@ -62,24 +67,15 @@ TEST_P(MemCheckTestSuite, create_exenetwork) {
 }
 
 TEST_P(MemCheckTestSuite, infer_request_inference) {
-    const ::testing::TestInfo* const test_info = ::testing::UnitTest::GetInstance()->current_test_info();
-    const std::string test_name = std::string(test_info->name()).substr(0, std::string(test_info->name()).find('/'));
-    //const std::string full_test_name = std::string(test_info->test_case_name()) + "." + std::string(test_info->name());
-    auto test_params = GetParam();
-
-    TestReferences test_refs;
-    test_refs.collect_vm_values_for_test(test_name, test_params);
-    checkRefVmValues();
-
-    log_info("Inference of InferRequest from network: \"" << test_params.model
-                                                          << "\" for device: \"" << test_params.device << "\"");
+    log_info("Inference of InferRequest from network: \"" << model
+                                                          << "\" for device: \"" << device << "\"");
     auto test_pipeline = [&]{
         MemCheckPipeline memCheckPipeline;
 
         Core ie;
-        ie.GetVersions(test_params.device);
-        CNNNetwork cnnNetwork = ie.ReadNetwork(test_params.model);
-        ExecutableNetwork exeNetwork = ie.LoadNetwork(cnnNetwork, test_params.device);
+        ie.GetVersions(device);
+        CNNNetwork cnnNetwork = ie.ReadNetwork(model);
+        ExecutableNetwork exeNetwork = ie.LoadNetwork(cnnNetwork, device);
         InferRequest inferRequest = exeNetwork.CreateInferRequest();
         inferRequest.Infer();
         OutputsDataMap output_info(cnnNetwork.getOutputsInfo());
@@ -89,7 +85,7 @@ TEST_P(MemCheckTestSuite, infer_request_inference) {
         log_info("Memory consumption after Inference:");
         memCheckPipeline.record_measures(test_name);
 
-        log_debug(memCheckPipeline.get_reference_record_for_test(test_name, test_params.model_name, test_params.device));
+        log_debug(memCheckPipeline.get_reference_record_for_test(test_name, model_name, device));
         return memCheckPipeline.measure();
     };
 
