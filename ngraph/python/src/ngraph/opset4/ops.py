b697e69f4cdd3ee30351417bb66389c07049e1ab
Pavel Esir <pavel.esir@intel.com>
2020-07-23 12:01:21 +0300

    add preliminary support of Proposal-4 in nGraph
    renamed logits -> bbox_deltas
    
    updated ngraph unittests for Proposal
    
    removed validate_and_infer_types Proposal-4
    
    removed validate_and_infer_types Proposal-4
    
    changed validate_and_infer_types in parent class of Proposal
    
    removed get_output_size
    
    successfully inferred Proposal on SSH and Faster-RCNN
    
    added unittests for Proposal-4
    
    added unittests for Proposal-4
    
    added unittests for Proposal-4
    
    returned back default namespace for Proposal
    
    reduced number of outputs in v0::Proposal
    
    correct conversion of Proposal-4 -> propodal_ie with 2 outputs
    
    removed creator for proposal v0
    
    removed converter for proposal v0
    
    added Proposal-4 to MO
    
    removed `for_deformable` attribute
    
    added Proposal-4 to MO and nGraph Python API
    
    removed typo in Proposal-4 specification
    
    style corrections
    
    style corrections and removed some redundant code
    
    rename proposal Python api test
    
    removed 'attrs' context from visitor
    
    returned back AttrVisitor to check if passes OpenVINO ONNX pipeline
    
    Should pass OpenVINO ONNX pipeline (returned back AttrVisitor just to check)
    
    python api for Proposal-4 works ok
    
    (style correction) python api for Proposal-4 works ok
    
    parametrized proposal_ie some other corrections
    
    removed 'attrs.' context from nGraph Python API tests for Proposal
    
    minor corrections in replacer proposal->proposal_ie
    
    corrected Python API OpenVINO-ONNX tests should pass
    
    Improved workaround for AttributeVisitor for Proposal
    
    Add additional check of im_info tensor shape to Proposal node in MKLDNNPlugin
    
    😠 removed 4 extra spaces from test_dyn_attributes.py to match The Style
    
    added new nGraph RTTI declarations, removed throwing exception in transformation
    
    added new nGraph RTTI declarations, removed throwing exception in transformation, corrected exception in MKLDNNplugin
    
    corrected im_info size checking in Proposal node of MKLDNNPlugin

--- ngraph/python/src/ngraph/opset4/ops.py
+++ ngraph/python/src/ngraph/opset4/ops.py
@@ -196,3 +196,120 @@ def atanh(node: NodeInput, name: Optional[str] = None) -> Node:
     :return: New node with arctanh operation applied on it.
     """
     return _get_node_factory_opset4().create("Atanh", [node])
+
+
+@nameable_op
+def proposal(
+        class_probs: Node,
+        bbox_deltas: Node,
+        image_shape: NodeInput,
+        attrs: dict,
+        name: Optional[str] = None,
+) -> Node:
+    """Filter bounding boxes and outputs only those with the highest prediction confidence.
+
+    :param  class_probs:        4D input floating point tensor with class prediction scores.
+    :param  bbox_deltas:        4D input floating point tensor with corrected predictions of bounding boxes
+    :param  image_shape:        The 1D input tensor with 3 or 4 elements describing image shape.
+    :param  attrs:              The dictionary containing key, value pairs for attributes.
+    :param  name:               Optional name for the output node.
+    * base_size     The size of the anchor to which scale and ratio attributes are applied.
+                    Range of values: a positive unsigned integer number
+                    Default value: None
+                    Required: yes
+    * pre_nms_topn  The number of bounding boxes before the NMS operation.
+                    Range of values: a positive unsigned integer number
+                    Default value: None
+                    Required: yes
+    * post_nms_topn The number of bounding boxes after the NMS operation.
+                    Range of values: a positive unsigned integer number
+                    Default value: None
+                    Required: yes
+    * nms_thresh    The minimum value of the proposal to be taken into consideration.
+                    Range of values: a positive floating-point number
+                    Default value: None
+                    Required: yes
+    * feat_stride   The step size to slide over boxes (in pixels).
+                    Range of values: a positive unsigned integer
+                    Default value: None
+                    Required: yes
+    * min_size      The minimum size of box to be taken into consideration.
+                    Range of values: a positive unsigned integer number
+                    Default value: None
+                    Required: yes
+    * ratio         The ratios for anchor generation.
+                    Range of values: a list of floating-point numbers
+                    Default value: None
+                    Required: yes
+    * scale         The scales for anchor generation.
+                    Range of values: a list of floating-point numbers
+                    Default value: None
+                    Required: yes
+    * clip_before_nms   The flag that specifies whether to perform clip bounding boxes before
+                        non-maximum suppression or not.
+                        Range of values: True or False
+                        Default value: True
+                        Required: no
+    * clip_after_nms    The flag that specifies whether to perform clip bounding boxes after
+                        non-maximum suppression or not.
+                        Range of values: True or False
+                        Default value: False
+                        Required: no
+    * normalize     The flag that specifies whether to perform normalization of output boxes to
+                    [0,1] interval or not.
+                    Range of values: True or False
+                    Default value: False
+                    Required: no
+    * box_size_scale    Specifies the scale factor applied to logits of box sizes before decoding.
+                        Range of values: a positive floating-point number
+                        Default value: 1.0
+                        Required: no
+    * box_coordinate_scale  Specifies the scale factor applied to logits of box coordinates
+                            before decoding.
+                            Range of values: a positive floating-point number
+                            Default value: 1.0
+                            Required: no
+    * framework     Specifies how the box coordinates are calculated.
+                    Range of values: "" (empty string) - calculate box coordinates like in Caffe*
+                                     tensorflow - calculate box coordinates like in the TensorFlow*
+                                                  Object Detection API models
+                    Default value: "" (empty string)
+                    Required: no
+    Example of attribute dictionary:
+    .. code-block:: python
+        # just required ones
+        attrs = {
+            'base_size': 85,
+            'pre_nms_topn': 10,
+            'post_nms_topn': 20,
+            'nms_thresh': 0.34,
+            'feat_stride': 16,
+            'min_size': 32,
+            'ratio': [0.1, 1.5, 2.0, 2.5],
+            'scale': [2, 3, 3, 4],
+        }
+    Optional attributes which are absent from dictionary will be set with corresponding default.
+    :return: Node representing Proposal operation.
+    """
+    requirements = [
+        ("base_size", True, np.unsignedinteger, is_positive_value),
+        ("pre_nms_topn", True, np.unsignedinteger, is_positive_value),
+        ("post_nms_topn", True, np.unsignedinteger, is_positive_value),
+        ("nms_thresh", True, np.floating, is_positive_value),
+        ("feat_stride", True, np.unsignedinteger, is_positive_value),
+        ("min_size", True, np.unsignedinteger, is_positive_value),
+        ("ratio", True, np.floating, None),
+        ("scale", True, np.floating, None),
+        ("clip_before_nms", False, np.bool_, None),
+        ("clip_after_nms", False, np.bool_, None),
+        ("normalize", False, np.bool_, None),
+        ("box_size_scale", False, np.floating, is_positive_value),
+        ("box_coordinate_scale", False, np.floating, is_positive_value),
+        ("framework", False, np.str_, None),
+    ]
+
+    check_valid_attributes("Proposal", attrs, requirements)
+
+    return _get_node_factory_opset4().create(
+        "Proposal", [class_probs, bbox_deltas, as_node(image_shape)], attrs
+    )
