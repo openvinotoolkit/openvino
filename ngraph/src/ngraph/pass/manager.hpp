1716633af186001b3ceff5e319ddc2e208ff9fc5
Gleb Kazantaev <gleb.kazantaev@intel.com>
2020-07-27 19:47:37 +0300

    nGraph Transformations refactoring (#931)
    This PR introduces next changes:
    1. Transformations *_tbl.hpp files were replaced with direct registration in cpp files.
    2. Plugins use pass::Manager to call conversion passes.
    3. Transformations callback was moved to PassBase class as there is no more need to keep it in separate class
    4. All pattern based transformations must be inherited from MatcherPass class. GraphRewrite class will be used only for matchers registration and execution on function.
    MatcherPass class adds new features to pattern-based transformations approach:
    * Allows to run matcher pass on a single node.
    * Operations that were created inside transformation callback can be added to execution list to be available for pattern matching within single GraphRewrite.
    5. GraphRewrite MatchClosure was replaced with MatcherPass. So all matchers will be registered as a MatcherPass.
    6. Added pass::Manager::clear_state() method to avoid dependency with nodes that no longer belongs to function after replacement.
    7.  Some representative transformations were updated to use MatcherPass as an example.
    8.  Mul->Add sequence fusion transformation was replaced with LinOpSequenceFusion.
    9. Pattern and callback registration code was moved to class c-tors (will be finished for remaining passes in other PR) .
    10. Updated pass::Manager to get pass names only when NGRAPH_PROFILE_PASS_ENABLE enabled.
    11. Moving towards removing PassProperty.
    12. Added ngraph::pattern::wrap_type<T>(inputs, pred) to simplify pattern creation.
    13. GraphRewrite was updated to execute MatcherPass more efficient.

--- ngraph/src/ngraph/pass/manager.hpp
+++ ngraph/src/ngraph/pass/manager.hpp
@@ -63,6 +63,29 @@ public:
     /// each registered pass
     /// \param new_state Value "true" enables Validate pass run; "false", otherwise
     void set_per_pass_validation(bool new_state) { m_per_pass_validation = new_state; }
+    /// \brief Callback is a lambda function that can be used by registered transformations.
+    /// The main purpose of this callback is to provide a way for plugins to disable/enable
+    /// transformations. In some cases plugins may want not to execute some transformations.
+    /// For example plugin can disable unpleasant decompositions because of performance reasons.
+    /// Callback example:
+    /// auto callback = [](const std::shared_ptr<const ngraph::Node> & node) -> bool {
+    ///     return std::dynamic_pointer_cast<const ngraph::opset3::DepthToSpace>(node) != nullptr;
+    /// };
+    /// This callback returns true in case of DepthToSpace operation. So when execution DepthToSpace
+    /// decomposition pass will check is this decomposition needed or plugin can execute this
+    /// operation directly. And of course on transformation side we need to have a response for this
+    /// callback.
+    /// if (m_transformation_callback(batch_to_space)) {
+    ///     return false;
+    /// }
+    /// \param callback lamda function that returns true in case if node is supported by plugin and
+    /// transformation is not needed
+    void set_callback(param_callback callback)
+    {
+        m_transformation_callback = callback;
+        m_has_default_callback = false;
+    }
+
 private:
     template <typename T, class... Args>
     std::shared_ptr<T> push_pass(Args&&... args)
@@ -71,23 +94,14 @@ private:
         auto pass = std::make_shared<T>(std::forward<Args>(args)...);
         auto pass_base = std::static_pointer_cast<PassBase>(pass);
         m_pass_list.push_back(pass_base);
-        if (m_visualize || m_serialize)
-        {
-#ifdef _WIN32
-            // MSVC produce a human-readable type name like class ngraph::pass::LikeReplacement
-            // by typeid(T).name(). Later ofstream doesn't accept it as a valid file name.
-            //
-            std::string str = typeid(T).name();
-            auto pos = str.find_last_of(":");
-            m_pass_names.push_back(str.substr(pos + 1));
-#elif defined(__linux) || defined(__APPLE__)
-            m_pass_names.push_back(typeid(T).name());
-#endif
-        }
         return pass;
     }
 
-    std::vector<std::string> m_pass_names;
+    param_callback m_transformation_callback = [](const std::shared_ptr<const Node>&) -> bool {
+        return false;
+    };
+    bool m_has_default_callback = true;
+
     std::vector<std::shared_ptr<PassBase>> m_pass_list;
     ManagerState m_state;
     PassConfig m_pass_config;
