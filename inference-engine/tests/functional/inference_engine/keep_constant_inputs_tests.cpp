52ced6f1dd039ac1ba2d36485d088974d6815217
Alexey Tarakanov <alexey.tarakanov@intel.com>
2020-07-27 12:13:34 +0300

    transformations_callback was removed

--- inference-engine/tests/functional/inference_engine/keep_constant_inputs_tests.cpp
+++ inference-engine/tests/functional/inference_engine/keep_constant_inputs_tests.cpp
@@ -43,31 +43,11 @@ int numberOfInputsForLayerInCNNNetwork(const InferenceEngine::CNNNetwork& networ
 
 void transformNetwork(std::shared_ptr<InferenceEngine::ICNNNetwork> clonedNetwork, bool keep_constant_inputs) {
     if (clonedNetwork->getFunction()) {
-        const auto transformations_callback = [](const std::shared_ptr<const ::ngraph::Node> &node) -> bool {
-            // DepthToSpace node implementation supports only equal input/output tensors with rank <= 5
-            if (auto dtsOp = std::dynamic_pointer_cast<const ::ngraph::opset3::DepthToSpace>(node)) {
-                return dtsOp->input_value(0).get_shape().size() <= 5lu &&
-                       dtsOp->input_value(0).get_shape().size() == dtsOp->get_output_shape(0).size();
-            }
-
-            // SpaceToDepth node implementation supports only equal input/output tensors with rank <= 5
-            if (auto stdOp = std::dynamic_pointer_cast<const ::ngraph::opset3::SpaceToDepth>(node)) {
-                return stdOp->input_value(0).get_shape().size() <= 5lu &&
-                       stdOp->input_value(0).get_shape().size() == stdOp->get_output_shape(0).size();
-            }
-
-            return std::dynamic_pointer_cast<const ::ngraph::opset2::Gelu>(node) ||
-                   std::dynamic_pointer_cast<const ::ngraph::opset2::BatchToSpace>(node) ||
-                   std::dynamic_pointer_cast<const ::ngraph::opset2::SpaceToBatch>(node) ||
-                   std::dynamic_pointer_cast<const ::ngraph::opset3::ShuffleChannels>(node);
-        };
         auto nGraphFunc = clonedNetwork->getFunction();
-
-        // Note: instead of running all Conversion Transformations you can make up your own transformation pipeline
-        ngraph::pass::CommonOptimizations(transformations_callback).run_on_function(nGraphFunc);
-        ngraph::pass::ConvertOpSet3ToOpSet2(transformations_callback).run_on_function(nGraphFunc);
-        ngraph::pass::ConvertOpSet2ToOpSet1(transformations_callback).run_on_function(nGraphFunc);
-        ngraph::pass::ConvertOpSet1ToLegacy(transformations_callback).run_on_function(nGraphFunc);
+        ngraph::pass::CommonOptimizations().run_on_function(nGraphFunc);
+        ngraph::pass::ConvertOpSet3ToOpSet2().run_on_function(nGraphFunc);
+        ngraph::pass::ConvertOpSet2ToOpSet1().run_on_function(nGraphFunc);
+        ngraph::pass::ConvertOpSet1ToLegacy().run_on_function(nGraphFunc);
         clonedNetwork = InferenceEngine::details::convertFunctionToICNNNetwork(nGraphFunc, *clonedNetwork, keep_constant_inputs);
     }
 }
