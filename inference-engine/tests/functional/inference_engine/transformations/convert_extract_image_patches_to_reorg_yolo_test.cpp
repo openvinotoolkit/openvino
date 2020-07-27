6e2939060626becb53dd485f1a9f7a9e6db0008d
Chetverikov Anton <anton.chetverikov@intel.com>
2020-07-27 19:10:29 +0300

    Fix test

--- inference-engine/tests/functional/inference_engine/transformations/convert_extract_image_patches_to_reorg_yolo_test.cpp
+++ inference-engine/tests/functional/inference_engine/transformations/convert_extract_image_patches_to_reorg_yolo_test.cpp
@@ -18,10 +18,8 @@
 #include <ngraph/opsets/opset1.hpp>
 #include <ngraph/opsets/opset3.hpp>
 #include <ngraph/pass/constant_folding.hpp>
-#include <ngraph_ops/crop_ie.hpp>
 
 #include <transformations/convert_extract_image_patches_to_reorg_yolo.hpp>
-#include <ngraph/op/reshape.hpp>
 #include <transformations/utils/utils.hpp>
 #include <transformations/init_node_info.hpp>
 
@@ -34,9 +32,9 @@ TEST(TransformationTests, ConvertExtractImagePatchesToReorgYoloTests) {
     {
         auto input = std::make_shared<ngraph::opset1::Parameter>(ngraph::element::f32, ngraph::Shape{ 1, 3, 10, 10 });
 
-        std::vector<int64_t> sizes = { 5, 5 };
-        std::vector<int64_t> strides = { 5, 5 };
-        std::vector<int64_t> rates = { 1, 1 };
+        auto sizes = Shape{ 5, 5 };
+        auto strides = Stride{ 5, 5 };
+        auto rates = Shape{ 1, 1 };
         ngraph::op::PadType auto_pad = ngraph::op::PadType::VALID;
 
 
@@ -67,11 +65,10 @@ TEST(TransformationTests, ConvertExtractImagePatchesToReorgYoloTestsNegative) {
     {
         auto input = std::make_shared<ngraph::opset1::Parameter>(ngraph::element::f32, ngraph::Shape{ 1, 3, 10, 10 });
 
-        std::vector<int64_t> sizes = { 5, 5 };
-        std::vector<int64_t> strides = { 5, 5 };
-        std::vector<int64_t> rates = { 1, 1 };
+        auto sizes = Shape{ 5, 5 };
+        auto strides = Stride{ 5, 5 };
+        auto rates = Shape{ 1, 1 };
         ngraph::op::PadType auto_pad = ngraph::op::PadType::SAME_LOWER;
-
 
         auto eip = std::make_shared<ngraph::opset3::ExtractImagePatches>(input, sizes, strides, rates, auto_pad);
 
@@ -84,11 +81,14 @@ TEST(TransformationTests, ConvertExtractImagePatchesToReorgYoloTestsNegative) {
     {
         auto input = std::make_shared<ngraph::opset1::Parameter>(ngraph::element::f32, ngraph::Shape{ 1, 3, 10, 10 });
 
-        std::vector<int64_t> stride = { 5, 5 };
+        auto sizes = Shape{ 5, 5 };
+        auto strides = Stride{ 5, 5 };
+        auto rates = Shape{ 1, 1 };
+        ngraph::op::PadType auto_pad = ngraph::op::PadType::SAME_LOWER;
 
-        auto reorg_yolo = std::make_shared<ngraph::opset3::ReorgYolo>(input, stride);
+        auto eip = std::make_shared<ngraph::opset3::ExtractImagePatches>(input, sizes, strides, rates, auto_pad);
 
-        f_ref = std::make_shared<ngraph::Function>(ngraph::NodeVector{ reorg_yolo }, ngraph::ParameterVector{ input });
+        f_ref = std::make_shared<ngraph::Function>(ngraph::NodeVector{ eip }, ngraph::ParameterVector{ input });
     }
 
     auto res = compare_functions(f, f_ref);
