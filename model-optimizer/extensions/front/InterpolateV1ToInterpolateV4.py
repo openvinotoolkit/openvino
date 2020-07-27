71fd41ccc14702c7eb2cda010f50b6575fed99c4
VGavrilov <vladimir.gavrilov@intel.com>
2020-07-27 10:21:34 +0300

    Now the transformation InterpolateV1ToInterpolateV4 uses find_and_replace_pattern but not replace_sub_graph.

--- model-optimizer/extensions/front/InterpolateV1ToInterpolateV4.py
+++ model-optimizer/extensions/front/InterpolateV1ToInterpolateV4.py
@@ -16,7 +16,7 @@
 
 from extensions.ops.interpolate import Interpolate
 from mo.front.common.partial_infer.utils import int64_array
-from mo.front.common.replacement import FrontReplacementSubgraph
+from mo.front.common.replacement import FrontReplacementPattern
 from mo.graph.graph import Graph
 from mo.ops.const import Const
 
@@ -25,7 +25,7 @@ def correct_pad(pad):
     return int64_array([pad] if not isinstance(pad, list) else pad)
 
 
-class InterpolateV1ToInterpolateV4(FrontReplacementSubgraph):
+class InterpolateV1ToInterpolateV4(FrontReplacementPattern):
     """
     This transformation replaces the operation Interpolate-1 with the operation Interpolate-3.
     """
@@ -35,32 +35,22 @@ class InterpolateV1ToInterpolateV4(FrontReplacementSubgraph):
         from extensions.front.InterpolateNormalizer import InterpolateNormalizer
         return [InterpolateNormalizer]
 
-    def pattern(self):
-        return dict(
-            nodes=[('op', {'type': 'Interpolate'})],
-            edges=[],
-        )
-
-    def replace_sub_graph(self, graph: Graph, match: dict):
-        node = match['op']
-        opset = node.get_opset()
-        if opset != 'opset1':
-            return
-
-        transformation_mode = 'align_corners' if int(node.soft_get('align_corners', 0)) else 'half_pixel'
-        input_node = node.in_port(0).get_source().node
-        sizes = node.in_port(1).get_source().node
-        axes_node = Const(graph, {'name': node.name + '/axis_', 'value': int64_array(node.axes)}).create_node()
-        interpolate4 = Interpolate(graph,
-                                   {
-                                       'mode': node.mode,
-                                       'antialias': node.antialias,
-                                       'coordinate_transformation_mode': transformation_mode,
-                                       'pads_begin': correct_pad(node.soft_get('pads_begin', 0)),
-                                       'pads_end': correct_pad(node.soft_get('pads_end', 0)),
-                                       'nearest_mode': 'round_prefer_floor',
-                                       'cube_coeff': -0.75,
-                                       'version': 'opset4',
-                                       'in_ports_count': 3,
-                                   }).create_node([input_node, sizes, axes_node])
-        node.replace_node(interpolate4)
+    def find_and_replace_pattern(self, graph: Graph):
+        for node in graph.get_op_nodes(op='Interpolate', opset='opset1'):
+            transformation_mode = 'align_corners' if int(node.soft_get('align_corners', 0)) else 'half_pixel'
+            input_node = node.in_port(0).get_source().node
+            sizes = node.in_port(1).get_source().node
+            axes_node = Const(graph, {'name': node.name + '/axis_', 'value': int64_array(node.axes)}).create_node()
+            interpolate4 = Interpolate(graph,
+                                       {
+                                           'mode': node.mode,
+                                           'antialias': node.antialias,
+                                           'coordinate_transformation_mode': transformation_mode,
+                                           'pads_begin': correct_pad(node.soft_get('pads_begin', 0)),
+                                           'pads_end': correct_pad(node.soft_get('pads_end', 0)),
+                                           'nearest_mode': 'round_prefer_floor',
+                                           'cube_coeff': -0.75,
+                                           'version': 'opset4',
+                                           'in_ports_count': 3,
+                                       }).create_node([input_node, sizes, axes_node])
+            node.replace_node(interpolate4)
