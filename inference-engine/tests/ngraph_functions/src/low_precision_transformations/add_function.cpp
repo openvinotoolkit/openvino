9ea55c8bf399fad6f288ed2c2efc51c28ec03c6c
Zinoviev, Vladimir <vladimir.zinoviev@intel.com>
2020-07-28 23:21:34 +0300

    [LPT] Add func. test fix

--- inference-engine/tests/ngraph_functions/src/low_precision_transformations/add_function.cpp
+++ inference-engine/tests/ngraph_functions/src/low_precision_transformations/add_function.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "ngraph_functions/low_precision_transformations/add_function.hpp"
+#include "transformations/low_precision/network_helper.hpp"
 
 #include <ngraph/opsets/opset1.hpp>
 #include "ngraph_functions/subgraph_builders.hpp"
@@ -19,7 +20,7 @@ std::shared_ptr<ngraph::Function> AddFunction::getOriginal(
     const AddActualValues& actualValues) {
     const auto input1 = std::make_shared<ngraph::opset1::Parameter>(
         actualValues.precision1,
-        ngraph::Shape(inputShape));
+        broadcast ? ngraph::Shape({ inputShape[0], inputShape[1], 1, 1 }) : ngraph::Shape(inputShape));
     std::shared_ptr<ngraph::Node> parent1 = input1;
 
     const std::shared_ptr<ngraph::Node> convert1 = std::make_shared<ngraph::opset1::Convert>(parent1, precision);
@@ -40,9 +41,8 @@ std::shared_ptr<ngraph::Function> AddFunction::getOriginal(
     }
 
     const auto input2 = std::make_shared<ngraph::opset1::Parameter>(
-        actualValues.precision2,
-        broadcast ? ngraph::Shape({ inputShape[0], inputShape[1], 1, 1 }) : ngraph::Shape(inputShape));
-    std::shared_ptr<ngraph::Node> parent2 = input1;
+        actualValues.precision2, ngraph::Shape(inputShape));
+    std::shared_ptr<ngraph::Node> parent2 = input2;
 
     const std::shared_ptr<ngraph::Node> convert2 = std::make_shared<ngraph::opset1::Convert>(parent2, precision);
     parent2 = convert2;
@@ -75,7 +75,7 @@ std::shared_ptr<ngraph::Function> AddFunction::getReference(
     const AddExpectedValues& expectedValues) {
     const auto input1 = std::make_shared<ngraph::opset1::Parameter>(
         expectedValues.precision1,
-        ngraph::Shape(inputShape));
+        broadcast ? ngraph::Shape({ inputShape[0], inputShape[1], 1, 1 }) : ngraph::Shape(inputShape));
     std::shared_ptr<ngraph::Node> parent1 = input1;
 
     const std::shared_ptr<ngraph::Node> convert1 = std::make_shared<ngraph::opset1::Convert>(parent1, precision);
@@ -95,22 +95,21 @@ std::shared_ptr<ngraph::Function> AddFunction::getReference(
         parent1 = multiply1;
     }
 
-    const auto input2 = std::make_shared<ngraph::opset1::Parameter>(
-        expectedValues.precision2,
-        broadcast ? ngraph::Shape({ inputShape[0], inputShape[1], 1, 1 }) : ngraph::Shape(inputShape));
-    std::shared_ptr<ngraph::Node> parent2 = input1;
+    auto input2 = std::make_shared<ngraph::opset1::Parameter>(
+        precision, ngraph::Shape(inputShape));
+    std::shared_ptr<ngraph::Node> parent2 = input2;
 
-    const std::shared_ptr<ngraph::Node> convert2 = std::make_shared<ngraph::opset1::Convert>(parent2, precision);
-    parent2 = convert2;
+    const auto add = std::make_shared< ngraph::op::TypeRelaxed<ngraph::opset1::Add> >(parent1, parent2);
 
-    const auto add = std::make_shared< ngraph::opset1::Add >(parent1, parent2);
+    const auto relaxedInput2 = as_type_ptr < ngraph::op::TypeRelaxed<ngraph::opset1::Parameter> > (
+        ngraph::pass::low_precision::NetworkHelper::setOutDataPrecision(input2, expectedValues.precision2));
 
     const auto multiply = std::make_shared< ngraph::opset1::Multiply >(
         add,
         std::make_shared<ngraph::opset1::Constant>(precision, Shape({ expectedValues.mutliplyValuesAfter.size() }), expectedValues.mutliplyValuesAfter));
 
     ngraph::ResultVector results{ std::make_shared<ngraph::opset1::Result>(multiply) };
-    return std::make_shared<ngraph::Function>(results, ngraph::ParameterVector{ input1, input2 }, "AddTransformation");
+    return std::make_shared<ngraph::Function>(results, ngraph::ParameterVector{ input1, relaxedInput2 }, "AddTransformation");
 }
 
 }  // namespace subgraph
