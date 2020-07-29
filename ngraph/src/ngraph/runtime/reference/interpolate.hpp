e7586282c467e9f708e3d8e92b9a1f5c53adaa2c
VGavrilov <vladimir.gavrilov@intel.com>
2020-07-29 10:52:56 +0300

    Started to write helper class to write NGRAPH reference implementation of Interpolate-4.

--- ngraph/src/ngraph/runtime/reference/interpolate.hpp
+++ ngraph/src/ngraph/runtime/reference/interpolate.hpp
@@ -16,8 +16,9 @@
 
 #pragma once
 
+#include <cmath>
 #include <cstddef>
-
+#include <functional>
 #include "ngraph/shape_util.hpp"
 
 namespace ngraph
@@ -26,6 +27,77 @@ namespace ngraph
     {
         namespace reference
         {
+            using Nearest_mode = op::v4::Interpolate::NearestMode;
+
+            class GetNearestPixel final
+            {
+            public:
+                GetNearestPixel() :
+                    GetNearestPixel(Nearest_mode::round_prefer_floor)
+                {
+                }
+
+                GetNearestPixel(Nearest_mode mode) :
+                    m_mode{mode}, m_func{get_func(mode)}
+                {
+                }
+
+                ~GetNearestPixel() = default;
+
+                int64_t operator()(float original, bool is_downsample) const
+                {
+                    return m_func(original, is_downsample);
+                }
+            private:
+                using Func = std::function<int64_t(float, bool)>;
+
+                Nearest_mode m_mode;
+                Func m_func;
+
+                Func get_func(Nearest_mode mode)
+                {
+                    switch (mode)
+                    {
+                    case Nearest_mode::round_prefer_floor:
+                        return [](float x_original, bool){
+                            if (x_original == static_cast<int64_t>(x_original) + 0.5f)
+                            {
+                                return static_cast<int64_t>(std::floor(x_original));
+                            }
+                            return static_cast<int64_t>(std::round(x_original));
+                        };
+                    case round_prefer_ceil:
+                        return [](float x_original, bool){
+                            return static_cast<int64_t>(std::round(x_original));
+                        };
+                    case Nearest_mode::floor:
+                        return [](float x_original, bool) {
+                            return static_cast<int64_t>(std::floor(x_original));
+                        };
+                        break;
+                    case Nearest_mode::ceil:
+                        return [](float x_original, bool) {
+                            return static_cast<int64_t>(std::ceil(x_original));
+                        };
+                        break;
+                    case Nearest_mode::simple:
+                        return  [](float x_original, bool is_downsample) {
+                            if (is_downsample)
+                            {
+                                return static_cast<int64_t>(std::ceil(x_original));
+                            }
+                            else
+                            {
+                                return static_cast<int64_t>(x_original);
+                            }
+                        };
+                        break;
+                    }
+                }
+            };
+
+            class GetOriginalCoordinate{};
+
             template <typename T>
             void nearest_interpolate(const T* input_data,
                                      const T* target_spatial_shape,
