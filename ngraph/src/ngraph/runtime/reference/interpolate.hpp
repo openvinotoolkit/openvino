43d9fa3646777cb0c684428d439af9666e7f3cbb
VGavrilov <vladimir.gavrilov@intel.com>
2020-07-29 12:02:41 +0300

    Started to write class implementing evaluation of Interpolate-4.

--- ngraph/src/ngraph/runtime/reference/interpolate.hpp
+++ ngraph/src/ngraph/runtime/reference/interpolate.hpp
@@ -28,7 +28,10 @@ namespace ngraph
         namespace reference
         {
             using Nearest_mode = op::v4::Interpolate::NearestMode;
+            using Transform_mode = ngraph::op::v4::Interpolate::CoordinateTransformMode;
+            using InterpolateMode = op::v4::Interpolate::InterpolateMode;
 
+            template <typename T>
             class GetNearestPixel final
             {
             public:
@@ -45,12 +48,12 @@ namespace ngraph
 
                 ~GetNearestPixel() = default;
 
-                int64_t operator()(float original, bool is_downsample) const
+                int64_t operator()(T original, bool is_downsample) const
                 {
                     return m_func(original, is_downsample);
                 }
             private:
-                using Func = std::function<int64_t(float, bool)>;
+                using Func = std::function<int64_t(T, bool)>;
 
                 Nearest_mode m_mode;
                 Func m_func;
@@ -60,7 +63,7 @@ namespace ngraph
                     switch (mode)
                     {
                     case Nearest_mode::round_prefer_floor:
-                        return [](float x_original, bool) {
+                        return [](T x_original, bool) {
                             if (x_original == static_cast<int64_t>(x_original) + 0.5f)
                             {
                                 return static_cast<int64_t>(std::floor(x_original));
@@ -68,21 +71,21 @@ namespace ngraph
                             return static_cast<int64_t>(std::round(x_original));
                         };
                     case round_prefer_ceil:
-                        return [](float x_original, bool) {
+                        return [](T x_original, bool) {
                             return static_cast<int64_t>(std::round(x_original));
                         };
                     case Nearest_mode::floor:
-                        return [](float x_original, bool) {
+                        return [](T x_original, bool) {
                             return static_cast<int64_t>(std::floor(x_original));
                         };
                         break;
                     case Nearest_mode::ceil:
-                        return [](float x_original, bool) {
+                        return [](T x_original, bool) {
                             return static_cast<int64_t>(std::ceil(x_original));
                         };
                         break;
                     case Nearest_mode::simple:
-                        return [](float x_original, bool is_downsample) {
+                        return [](T x_original, bool is_downsample) {
                             if (is_downsample)
                             {
                                 return static_cast<int64_t>(std::ceil(x_original));
@@ -97,57 +100,72 @@ namespace ngraph
                 }
             };
 
-            class GetOriginalCoordinate
+            template <typename T>
+            class GetOriginalCoordinate final
             {
+            public:
+                GetOriginalCoordinate()
+                    : GetOriginalCoordinate(Transform_mode::half_pixel)
+                {
+                }
+
+                GetOriginalCoordinate(Transform_mode mode)
+                    : m_mode{mode}
+                    , m_func{get_func(mode)}
+                {
+                }
+
+                ~GetOriginalCoordinate() = default;
+
+                T operator()(T x_resized, T x_scale, T length_resized, T length_original)
+                {
+                    return m_func(x_resized, x_scale, length_resized, length_original);
+                }
+            private:
+                using Func = std::function<T(T, T, T, T)>;
+
+                Transform_mode m_mode;
+                Func m_func;
+
+                Func get_func(Transform_mode mode)
+                {
+                    switch (mode)
+                    {
+                    case Transform_mode::half_pixel:
+                        return [](T x_resized, T x_scale, T, T) {
+                            return ((x_resized + 0.5f) / x_scale) - 0.5f;
+                        };
+                        break;
+                    case Transform_mode::pytorch_half_pixel:
+                        return [](T x_resized, T x_scale, T length_resized, T) {
+                            return length_resized > 1 ? (x_resized + 0.5f) / x_scale - 0.5f : 0.0f;
+                        };
+                        break;
+                    case Transform_mode::asymmetric:
+                        return [](T x_resized, T x_scale, T, T) {
+                            return x_resized / x_scale;
+                        };
+                        break;
+                    case Transform_mode::tf_half_pixel_for_nn:
+                        return [](T x_resized, T x_scale, T, T) {
+                            return (x_resized + 0.5f) / x_scale;
+                        };
+                        break;
+                    case Transform_mode::align_corners:
+                         return [](T x_resized, T, T length_resized, T length_original) {
+                            return length_resized == 1 ? 0 :
+                                x_resized * (length_original - 1) / (length_resized - 1);
+                        };
+                        break;
+                    }
+                }
             };
 
             template <typename T>
-            void nearest_interpolate(const T* input_data,
-                                     const T* target_spatial_shape,
-                                     const T* axes,
-                                     T* out,
-                                     const Shape& input_data_shape,
-                                     const Shape& target_spatial_shape_size,
-                                     const Shape& axes_size,
-                                     const op::v4::Interpolate::InterpolateAttrs& attrs)
+            class InterpolateEval
             {
-            }
+            };
 
-            template <typename T>
-            void linear_interpolate(const T* input_data,
-                                    const T* target_spatial_shape,
-                                    const T* axes,
-                                    T* out,
-                                    const Shape& input_data_shape,
-                                    const Shape& target_spatial_shape_size,
-                                    const Shape& axes_size,
-                                    const op::v4::Interpolate::InterpolateAttrs& attrs)
-            {
-            }
-
-            template <typename T>
-            void linear_onnx_interpolate(const T* input_data,
-                                         const T* target_spatial_shape,
-                                         const T* axes,
-                                         T* out,
-                                         const Shape& input_data_shape,
-                                         const Shape& target_spatial_shape_size,
-                                         const Shape& axes_size,
-                                         const op::v4::Interpolate::InterpolateAttrs& attrs)
-            {
-            }
-
-            template <typename T>
-            void cubic_interpolate(const T* input_data,
-                                   const T* target_spatial_shape,
-                                   const T* axes,
-                                   T* out,
-                                   const Shape& input_data_shape,
-                                   const Shape& target_spatial_shape_size,
-                                   const Shape& axes_size,
-                                   const op::v4::Interpolate::InterpolateAttrs& attrs)
-            {
-            }
 
             template <typename T>
             void interpolate(const T* input_data,
@@ -159,49 +177,6 @@ namespace ngraph
                              const Shape& axes_size,
                              const op::v4::Interpolate::InterpolateAttrs& attrs)
             {
-                switch (attrs.mode)
-                {
-                case op::v4::Interpolate::InterpolateMode::nearest:
-                    nearest_interpolate<T>(input_data,
-                                           target_spatial_shape,
-                                           axes,
-                                           out,
-                                           input_data_shape,
-                                           target_spatial_shape_size,
-                                           axes_size,
-                                           attrs);
-                    break;
-                case op::v4::Interpolate::InterpolateMode::linear:
-                    linear_interpolate<T>(input_data,
-                                          target_spatial_shape,
-                                          axes,
-                                          out,
-                                          input_data_shape,
-                                          target_spatial_shape_size,
-                                          axes_size,
-                                          attrs);
-                    break;
-                case op::v4::Interpolate::InterpolateMode::linear_onnx:
-                    linear_onnx_interpolate<T>(input_data,
-                                               target_spatial_shape,
-                                               axes,
-                                               out,
-                                               input_data_shape,
-                                               target_spatial_shape_size,
-                                               axes_size,
-                                               attrs);
-                    break;
-                case op::v4::Interpolate::InterpolateMode::cubic:
-                    cubic_interpolate<T>(input_data,
-                                         target_spatial_shape,
-                                         axes,
-                                         out,
-                                         input_data_shape,
-                                         target_spatial_shape_size,
-                                         axes_size,
-                                         attrs);
-                    break;
-                }
             }
         }
     }
