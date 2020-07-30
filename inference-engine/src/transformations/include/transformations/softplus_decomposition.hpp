da065ee131f19a38b5c59e201b01e93d2e371384
Chetverikov Anton <anton.chetverikov@intel.com>
2020-07-31 01:08:29 +0300

    Add SoftPlus decomposition transformation

---
+++ inference-engine/src/transformations/include/transformations/softplus_decomposition.hpp
@@ -0,0 +1,27 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <vector>
+#include <memory>
+
+#include <transformations_visibility.hpp>
+
+#include <ngraph/ngraph.hpp>
+#include <ngraph/pass/graph_rewrite.hpp>
+#include "ngraph/pattern/matcher.hpp"
+
+namespace ngraph {
+    namespace pass {
+
+        class TRANSFORMATIONS_API SoftPlusDecomposition;
+
+    }  // namespace pass
+}  // namespace ngraph
+
+class ngraph::pass::SoftPlusDecomposition : public ngraph::pass::MatcherPass {
+public:
+    SoftPlusDecomposition();
+};
---
+++ inference-engine/src/transformations/src/transformations/softplus_decomposition.cpp
@@ -0,0 +1,35 @@
+// Copyright (C) 2018-2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "transformations/softplus_decomposition.hpp"
+
+#include <memory>
+#include <vector>
+
+#include <ngraph/opsets/opset4.hpp>
+#include <ngraph/rt_info.hpp>
+#include <ngraph/pattern/op/wrap_type.hpp>
+
+ngraph::pass::SoftPlusDecomposition::SoftPlusDecomposition() {
+    auto input = ngraph::pattern::any_input();
+    auto softplus = std::make_shared<ngraph::opset4::SoftPlus>(input);
+
+    ngraph::graph_rewrite_callback callback = [=](ngraph::pattern::Matcher& m) {
+        auto & pattern_to_output = m.get_pattern_value_map();
+        auto softplus_input = pattern_to_output.at(input);
+
+        auto exp = std::make_shared<ngraph::opset4::Exp>(input);
+        auto add = std::make_shared<ngraph::opset4::Add>(exp,
+            opset4::Constant::create(ngraph::element::f64, ngraph::Shape{ 1 }, { -1 }));
+        auto log = std::make_shared<ngraph::opset4::Log>(add);
+
+        mish->set_friendly_name(m.get_match_root()->get_friendly_name());
+        ngraph::copy_runtime_info(pattern_to_output.at(softplus).get_node_shared_ptr(), {log, add, exp});
+        ngraph::replace_node(m.get_match_root(), log);
+        return true;
+    };
+
+    auto m = std::make_shared<ngraph::pattern::Matcher>(mul, "SoftPlusDecomposition");
+    register_matcher(m, callback);
+}
