9299e32df0aade4104f7072b67f2071a1031eab6
Andrey Somsikov <andrey.somsikov@intel.com>
2020-07-30 10:17:56 +0300

    Add product version to memcheck records (#1508)

--- tests/stress_tests/scripts/memcheck_upload.py
+++ tests/stress_tests/scripts/memcheck_upload.py
@@ -68,6 +68,7 @@ def metadata_from_manifest(manifest):
         'repo_url': repo_trigger['url'],
         'target_branch': repo_trigger['target_branch'],
         'event_type': manifest['components'][PRODUCT_NAME]['build_event'].lower(),
+        f'{PRODUCT_NAME}_version': manifest['components'][PRODUCT_NAME]['version'],
     }
 
 
