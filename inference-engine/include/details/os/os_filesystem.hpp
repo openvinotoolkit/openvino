5a5954636c35c218b4e5a1bb244263e40bfc9ad1
Ilya Lavrenov <ilya.lavrenov@intel.com>
2020-08-02 23:37:47 +0300

    Implement unicode conversion using Windows native functions

--- inference-engine/include/details/os/os_filesystem.hpp
+++ inference-engine/include/details/os/os_filesystem.hpp
@@ -11,7 +11,15 @@
 
 #include "ie_api.h"
 
-#ifdef ENABLE_UNICODE_PATH_SUPPORT
+#ifdef _WIN32
+# ifndef NOMINMAX
+#  define NOMINMAX
+# endif
+# ifndef _WINSOCKAPI_
+#  define _WINSOCKAPI_
+# endif
+# include <Windows.h>
+#elif defined ENABLE_UNICODE_PATH_SUPPORT
 # include <codecvt>
 #endif
 
@@ -30,17 +38,32 @@ using enableIfSupportedChar = typename std::enable_if<(std::is_same<C, char>::va
  * @brief Conversion from wide character string to a single-byte chain.
  */
 inline const std::string wStringtoMBCSstringChar(const std::wstring& wstr) {
+#ifdef _WIN32
+    int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
+    std::string strTo(size_needed, 0);
+    WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
+    return strTo;
+#else
     std::wstring_convert<std::codecvt_utf8<wchar_t>> wstring_decoder;
     return wstring_decoder.to_bytes(wstr);
+#endif
 }
 
 /**
  * @brief Conversion from single-byte chain to wide character string.
  */
 inline const std::wstring multiByteCharToWString(const char* str) {
+#ifdef _WIN32
+    auto strSize = std::strlen(str);
+    int size_needed = MultiByteToWideChar(CP_UTF8, 0, str, std::strlen(str), NULL, 0);
+    std::wstring wstrTo(size_needed, 0);
+    MultiByteToWideChar(CP_UTF8, 0, str, strSize, &wstrTo[0], size_needed);
+    return wstrTo;
+#else
     std::wstring_convert<std::codecvt_utf8<wchar_t>> wstring_encoder;
     std::wstring result = wstring_encoder.from_bytes(str);
     return result;
+#endif
 }
 
 #endif  // ENABLE_UNICODE_PATH_SUPPORT
