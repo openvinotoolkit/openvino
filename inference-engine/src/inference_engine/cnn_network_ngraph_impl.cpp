187fef221e33e7974ce20fa273c1fe428964a56d
Ilya Churaev <ilya.churaev@intel.com>
2020-08-03 18:23:02 +0300

    Use clone_function instead of specialize_function (#1523)
    * Try to use clone_function instead of specialize_function
    
    * Try to fix stress tests
    
    * Remove redundant specialize_function
    
    * Fixed TI clone
    
    * Removed redundant code
    
    * Uncomment threading tests
    
    * Fixed docs
    
    * copy function friendly name too
    
    * Fixed copy rt_info
    
    * Fixed comments

--- inference-engine/src/inference_engine/cnn_network_ngraph_impl.cpp
+++ inference-engine/src/inference_engine/cnn_network_ngraph_impl.cpp
@@ -14,6 +14,8 @@
 #include <vector>
 #include <unordered_set>
 #include <ngraph/ngraph.hpp>
+#include <ngraph/graph_util.hpp>
+#include <ngraph/pass/constant_folding.hpp>
 #include <ngraph/pass/manager.hpp>
 #include <ngraph/pass/get_output_element_elimination.hpp>
 #include <set>
@@ -39,30 +41,15 @@ using InferenceEngine::details::CNNNetworkNGraphImpl;
 using ngraph::Function;
 
 static std::shared_ptr<ngraph::Function> copyFunction(const std::shared_ptr<const ngraph::Function>& func,
-                                                      bool constFolding,
-                                                      const std::map<std::string, std::vector<size_t>>& inputShapes) {
+                                                      bool constFolding) {
     OV_ITT_SCOPED_TASK(itt::domains::IE, "copyFunction");
 
     ::ngraph::op::GenericIE::DisableReshape noReshape(func);
-    auto original_parameters = func->get_parameters();
+    auto specialized_function = ngraph::clone_function(*func);
 
-    std::vector<::ngraph::element::Type> new_types;
-    std::vector<::ngraph::PartialShape> new_shapes;
-
-    for (const auto &parameter : original_parameters) {
-        if (inputShapes.find(parameter->get_friendly_name()) != inputShapes.end()) {
-            new_shapes.emplace_back(inputShapes.at(parameter->get_friendly_name()));
-        } else {
-            new_shapes.emplace_back(parameter->get_partial_shape());
-        }
-        new_types.emplace_back(parameter->get_element_type());
+    if (constFolding) {
+        ngraph::pass::ConstantFolding().run_on_function(specialized_function);
     }
-    IE_ASSERT(original_parameters.size() == new_types.size());
-    IE_ASSERT(original_parameters.size() == new_shapes.size());
-
-    // TODO: remove const cast if specialize function works with constant ngraph function
-    auto specialized_function = ::ngraph::specialize_function(std::const_pointer_cast<ngraph::Function>(func), new_types, new_shapes,
-                                                              std::vector<void*>(new_shapes.size(), nullptr), constFolding, true);
     // TODO: remove this code after the fix on the nGraph side
     ::ngraph::pass::GetOutputElementElimination goe_elimination;
     for (auto n : specialized_function->get_ops()) {
@@ -153,7 +140,7 @@ CNNNetworkNGraphImpl::CNNNetworkNGraphImpl(const ICNNNetwork& network) {
         THROW_IE_EXCEPTION << "Cannot create CNNNetwork with nGraph from legacy network format!";
     }
 
-    _ngraph_function = copyFunction(network.getFunction(), false, {});
+    _ngraph_function = copyFunction(network.getFunction(), false);
     InputsDataMap inputs;
     OutputsDataMap outputs;
     network.getInputsInfo(inputs);
@@ -293,8 +280,8 @@ size_t CNNNetworkNGraphImpl::getBatchSize() const noexcept {
     return shape[0];
 }
 
-std::shared_ptr<ngraph::Function> CNNNetworkNGraphImpl::cloneFunction(bool constFolding, const std::map<std::string, std::vector<size_t>>& inputShapes) const {
-    return copyFunction(_ngraph_function, constFolding, inputShapes);
+std::shared_ptr<ngraph::Function> CNNNetworkNGraphImpl::cloneFunction(bool constFolding) const {
+    return copyFunction(_ngraph_function, constFolding);
 }
 
 void CNNNetworkNGraphImpl::reshape() {
@@ -329,7 +316,7 @@ CNNNetworkNGraphImpl::reshape(const std::map<std::string, std::vector<size_t>>& 
         _ngraph_function->validate_nodes_and_infer_types();
 
         {
-            auto specialized_ngraph_function = cloneFunction(true, inputShapes);
+            auto specialized_ngraph_function = cloneFunction(true);
             // Call this transformation because OneHot IE and nGraph have different output precisions
             {
                 OV_ITT_SCOPED_TASK(itt::domains::IE, "CNNNetworkNGraphImpl::ConvertOneHot");
