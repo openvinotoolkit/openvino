65298d406d315622e95ef5f3653d6e18b36d400e
VGavrilov <vladimir.gavrilov@intel.com>
2020-08-03 10:08:50 +0300

    Some fixes in Interpolate-4 test.

--- ngraph/test/type_prop/interpolate.cpp
+++ ngraph/test/type_prop/interpolate.cpp
@@ -27,7 +27,7 @@ TEST(type_prop, interpolate_v4)
     using Nearest_mode = op::v4::Interpolate::NearestMode;
     using InterpolateAttrs = op::v4::Interpolate::InterpolateAttrs;
 
-    auto image = make_shared<op::Parameter>(element::f32, Shape{2, 2, 30, 60});
+    auto image = std::make_shared<op::Parameter>(element::f32, Shape{2, 2, 30, 60});
     auto output_shape = op::Constant::create<int64_t>(element::i64, Shape{2}, {15, 30});
     auto axes = op::Constant::create<int64_t>(element::i64, Shape{2}, {2, 3});
 
@@ -39,28 +39,7 @@ TEST(type_prop, interpolate_v4)
     attrs.pads_begin = {0, 0, 0, 0};
     attrs.pads_end = {0, 0, 0, 0};
     attrs.cube_coeff = -0.75;
-    auto op = make_shared<op::v4::Interpolate>(image, output_shape, axes, attrs);
+    auto op = std::make_shared<op::v4::Interpolate>(image, output_shape, axes, attrs);
 
-//     EXPECT_EQ(op->get_element_type(), element::f32);
     EXPECT_EQ(op->get_shape(), (Shape{2, 2, 15, 30}));
-//     auto dyn_output_shape = make_shared<op::Parameter>(element::i64, Shape{2});
-//     auto data = make_shared<op::Parameter>(element::f32, Shape{1, 3, 6});
-//     auto mish_func = make_shared<op::v4::Mish>(data);
-//     EXPECT_EQ(mish_func->get_element_type(), element::f32);
-//     EXPECT_EQ(mish_func->get_shape(), (Shape{1, 3, 6}));
 }
-
-
-// TEST(type_prop_layers, interpolate_v4)
-// {
-//     auto image = make_shared<op::Parameter>(element::f32, Shape{2, 2, 33, 65});
-//     auto dyn_output_shape = make_shared<op::Parameter>(element::i64, Shape{2});
-//     auto output_shape = op::Constant::create<int64_t>(element::i64, Shape{2}, {15, 30});
-//     auto axes = op::Constant::create<int64_t>(element::i64, Shape{2}, {2, 3});
-//     ASSERT_EQ(op->get_shape(), (Shape{2, 2, 15, 30}));
-//
-//     EXPECT_TRUE(make_shared<Interpolate>(image, dyn_output_shape, attrs)
-//                     ->get_output_partial_shape(0)
-//                     .same_scheme(PartialShape{2, 2, Dimension::dynamic(),
-//                     Dimension::dynamic()}));
-// }
