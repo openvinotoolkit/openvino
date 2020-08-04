5ac231a581bf2bef4b1d420e4c063fd41528f43d
Ivan Tikhonov <ivan.tikhonov@intel.com>
2020-08-04 12:35:24 +0300

    Align with the specification SpaceToBatch, BatchToSpace ops (#1140)
    * Aligned SpaceToBatch/BatchToSpace with the spec, converted from fused_op to op
    
    * Implemented transformation to decompose STB/BTS
    
    * Added unit tests
    
    * Added new mode (INTERPRETER_TRANSFOMATIONS) for functional tests

--- inference-engine/src/transformations/include/transformations/convert_batch_to_space.hpp
+++ inference-engine/src/transformations/include/transformations/convert_batch_to_space.hpp
@@ -20,14 +20,29 @@ class TRANSFORMATIONS_API ConvertBatchToSpace;
 }  // namespace pass
 }  // namespace ngraph
 
-class ngraph::pass::ConvertBatchToSpace: public ngraph::pass::GraphRewrite {
+/**
+ * @ingroup ie_transformation_common_api
+ * @brief ConvertBatchToSpace transformation decomposes BatchToSpace layer to Reshape->Transpose->Reshape->Crop.
+ *
+ * @param convert_by_elements - reduces the maximum number of dimensions that arise during the transformation
+ * if enabled. Default value: true.
+ *  false - BatchToSpace decomposes to Reshape->Transpose->Reshape->Crop. During transformation, the number of
+ *  tensor dimensions can be increased by length of block_shape input of BatchToSpace layer.
+ *  true - BatchToSpace decomposes to N x (Reshape->Transpose->Reshape)->Crop, where N = length of block_shape input
+ *  of BatchToSpace layer. During transformation, the number of tensor dimensions can be increased by 1.
+ *
+ */
+
+class ngraph::pass::ConvertBatchToSpace: public ngraph::pass::MatcherPass {
 public:
-    ConvertBatchToSpace() : GraphRewrite() {
-        // convert_batch_to_space();
-        convert_batch_to_space_ie_side();
+    explicit ConvertBatchToSpace(bool convert_by_elements = true) : MatcherPass() {
+        if (convert_by_elements)
+            convert_batch_to_space_by_elements();
+        else
+            convert_batch_to_space();
     }
 
 private:
     void convert_batch_to_space();
-    void convert_batch_to_space_ie_side();
+    void convert_batch_to_space_by_elements();
 };
