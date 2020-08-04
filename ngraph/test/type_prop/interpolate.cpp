17083eed2dc1de4ba87d6a94898521c944719518
VGavrilov <vladimir.gavrilov@intel.com>
2020-08-04 16:18:50 +0300

    Added test for Interpolate-4.

--- ngraph/test/type_prop/interpolate.cpp
+++ ngraph/test/type_prop/interpolate.cpp
@@ -76,3 +76,60 @@ TEST(type_prop, interpolate_v4_partial)
     auto interp_part = std::make_shared<op::v4::Interpolate>(partial_param, scales, axes, attrs);
     ASSERT_TRUE(interp_part->get_output_partial_shape(0).same_scheme(PartialShape::dynamic()));
 }
+
+TEST(type_prop, interpolate_v4_partial_static_rank)
+{
+    using InterpolateMode = op::v4::Interpolate::InterpolateMode;
+    using CoordinateTransformMode = op::v4::Interpolate::CoordinateTransformMode;
+    using Nearest_mode = op::v4::Interpolate::NearestMode;
+    using InterpolateAttrs = op::v4::Interpolate::InterpolateAttrs;
+
+    auto partial_shape = PartialShape{2, 2, Dimension::dynamic(), Dimension::dynamic()};
+
+    auto image = std::make_shared<op::Parameter>(element::f32, partial_shape);
+    auto scales = op::Constant::create<float>(element::f32, Shape{2}, {0.5f, 0.5f});
+    auto axes = op::Constant::create<int64_t>(element::i64, Shape{2}, {2, 3});
+
+    InterpolateAttrs attrs;
+    attrs.mode = InterpolateMode::nearest;
+    attrs.coordinate_transformation_mode = CoordinateTransformMode::half_pixel;
+    attrs.nearest_mode = Nearest_mode::round_prefer_floor;
+    attrs.antialias = false;
+    attrs.pads_begin = {0, 0, 0, 0};
+    attrs.pads_end = {0, 0, 0, 0};
+    attrs.cube_coeff = -0.75;
+    auto interp = std::make_shared<op::v4::Interpolate>(image, scales, axes, attrs);
+
+    EXPECT_EQ(interp->get_element_type(), element::f32);
+    ASSERT_TRUE(interp->get_output_partial_shape(0).same_scheme(partial_shape));
+    ASSERT_TRUE(interp->get_output_partial_shape(0).rank().is_static());
+}
+
+TEST(type_prop, interpolate_v4_partial_static_rank2)
+{
+    using InterpolateMode = op::v4::Interpolate::InterpolateMode;
+    using CoordinateTransformMode = op::v4::Interpolate::CoordinateTransformMode;
+    using Nearest_mode = op::v4::Interpolate::NearestMode;
+    using InterpolateAttrs = op::v4::Interpolate::InterpolateAttrs;
+
+    auto partial_shape = PartialShape{Dimension::dynamic(), Dimension::dynamic(), 10, 20};
+    auto out_shape = PartialShape{Dimension::dynamic(), Dimension::dynamic(), 5, 10};
+
+    auto image = std::make_shared<op::Parameter>(element::f32, partial_shape);
+    auto scales = op::Constant::create<float>(element::f32, Shape{2}, {0.5f, 0.5f});
+    auto axes = op::Constant::create<int64_t>(element::i64, Shape{2}, {2, 3});
+
+    InterpolateAttrs attrs;
+    attrs.mode = InterpolateMode::nearest;
+    attrs.coordinate_transformation_mode = CoordinateTransformMode::half_pixel;
+    attrs.nearest_mode = Nearest_mode::round_prefer_floor;
+    attrs.antialias = false;
+    attrs.pads_begin = {0, 0, 0, 0};
+    attrs.pads_end = {0, 0, 0, 0};
+    attrs.cube_coeff = -0.75;
+    auto interp = std::make_shared<op::v4::Interpolate>(image, scales, axes, attrs);
+
+    EXPECT_EQ(interp->get_element_type(), element::f32);
+    ASSERT_TRUE(interp->get_output_partial_shape(0).same_scheme(out_shape));
+    ASSERT_TRUE(interp->get_output_partial_shape(0).rank().is_static());
+}
