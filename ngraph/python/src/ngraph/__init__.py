b08f84003b0d9288153a1fdd5c64b1ed66ffa1b7
Roman Kazantsev <roman.kazantsev@intel.com>
2020-08-05 14:11:29 +0300

    Add CTCLoss op to nGraph Python API
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- ngraph/python/src/ngraph/__init__.py
+++ ngraph/python/src/ngraph/__init__.py
@@ -51,6 +51,7 @@ from ngraph.opset4 import convolution_backprop_data
 from ngraph.opset4 import cos
 from ngraph.opset4 import cosh
 from ngraph.opset4 import ctc_greedy_decoder
+from ngraph.opset4 import ctc_loss
 from ngraph.opset4 import cum_sum
 from ngraph.opset4 import cum_sum as cumsum
 from ngraph.opset4 import deformable_convolution
