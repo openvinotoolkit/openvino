9a8824958d80116a093e5f337a0f1e20ac685f97
VGavrilov <vladimir.gavrilov@intel.com>
2020-08-06 11:15:41 +0300

    Started to write tests for method evaluate() of Interpolate-4.

---
+++ ngraph/test/op_eval/interpolate.cpp
@@ -0,0 +1,34 @@
+//*****************************************************************************
+// Copyright 2017-2020 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+
+#include <string>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "ngraph/op/mish.hpp"
+#include "ngraph/runtime/host_tensor.hpp"
+#include "ngraph/validation_util.hpp"
+#include "runtime/backend.hpp"
+#include "util/test_tools.hpp"
+#include "util/type_prop.hpp"
+
+using namespace ngraph;
+
+TEST(op_eval, interpolate_v4_resize_downsample_scales_cubic)
+{
+    ASSERT_TRUE(true);
+}
