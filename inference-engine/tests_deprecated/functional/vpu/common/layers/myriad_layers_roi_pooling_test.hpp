db667ffc3d8f93dafcb26c5a3d58b34b1174615f
kora6 <kora6@github.com>
2020-08-06 16:32:53 +0300

    [VPU][TEST] Test case for ROIPooling was added

--- inference-engine/tests_deprecated/functional/vpu/common/layers/myriad_layers_roi_pooling_test.hpp
+++ inference-engine/tests_deprecated/functional/vpu/common/layers/myriad_layers_roi_pooling_test.hpp
@@ -306,7 +306,7 @@ static std::vector<Dims> s_ROIPoolingLayerInput = {
 
 static std::vector<roi_pooling_param> s_ROIPoolingLayerParam = {
     {{224, 224, 7, 7, 0.0625}},
-    {{224, 224, 45, 45, 1}}
+    {{224, 224, 32, 32, 1}}
 };
 
 static std::vector<uint32_t> s_ROIPoolingNumRois = {
