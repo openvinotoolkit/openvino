29db150c523273cf1c31e9c9118c15455f809ff9
Vafin, Maxim <maxim.vafin@intel.com>
2020-08-06 12:03:12 +0300

    Implement evaluate for Range-4

--- ngraph/core/src/op/range.cpp
+++ ngraph/core/src/op/range.cpp
@@ -46,6 +46,17 @@ bool ngraph::op::v4::Range::visit_attributes(AttributeVisitor& visitor)
 
 void op::v4::Range::validate_and_infer_types()
 {
+    set_input_is_relevant_to_shape(0);
+    set_input_is_relevant_to_shape(1);
+    set_input_is_relevant_to_shape(2);
+
+    NODE_VALIDATION_CHECK(
+        this, get_input_partial_shape(0).compatible(Shape{}), "'start' input is not a scalar");
+    NODE_VALIDATION_CHECK(
+        this, get_input_partial_shape(1).compatible(Shape{}), "'stop' input is not a scalar");
+    NODE_VALIDATION_CHECK(
+        this, get_input_partial_shape(2).compatible(Shape{}), "'step' input is not a scalar");
+
     auto const_start = as_type_ptr<op::Constant>(this->input_value(0).get_node_shared_ptr());
     auto const_stop = as_type_ptr<op::Constant>(this->input_value(1).get_node_shared_ptr());
     auto const_step = as_type_ptr<op::Constant>(this->input_value(2).get_node_shared_ptr());
@@ -60,7 +71,7 @@ void op::v4::Range::validate_and_infer_types()
         NODE_VALIDATION_CHECK(this, start_val.size() == 1);
         start = start_val[0];
         NODE_VALIDATION_CHECK(
-            this, std::isfinite(start) && !isnan(start), "'start' cannot be nan or infinite.");
+            this, std::isfinite(start) && !std::isnan(start), "'start' cannot be nan or infinite.");
     }
 
     if (const_stop != nullptr)
@@ -69,7 +80,7 @@ void op::v4::Range::validate_and_infer_types()
         NODE_VALIDATION_CHECK(this, stop_val.size() == 1);
         stop = stop_val[0];
         NODE_VALIDATION_CHECK(
-            this, std::isfinite(stop) && !isnan(stop), "'stop' cannot be nan or infinite.");
+            this, std::isfinite(stop) && !std::isnan(stop), "'stop' cannot be nan or infinite.");
     }
 
     if (const_step != nullptr)
@@ -78,7 +89,7 @@ void op::v4::Range::validate_and_infer_types()
         NODE_VALIDATION_CHECK(this, step_val.size() == 1);
         step = step_val[0];
         NODE_VALIDATION_CHECK(
-            this, std::isfinite(step) && !isnan(step), "'step' cannot be nan or infinite.");
+            this, std::isfinite(step) && !std::isnan(step), "'step' cannot be nan or infinite.");
     }
 
     PartialShape result{PartialShape::dynamic(1)};
@@ -106,11 +117,136 @@ void op::v4::Range::validate_and_infer_types()
 shared_ptr<Node> op::v4::Range::clone_with_new_inputs(const OutputVector& new_args) const
 {
     check_new_args_count(this, new_args);
-    return make_shared<Range>(new_args.at(0), new_args.at(1), new_args.at(2), m_output_type);
+    return make_shared<v4::Range>(new_args.at(0), new_args.at(1), new_args.at(2), m_output_type);
 }
 
-bool op::v4::Range::evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs)
+template <typename T>
+bool get_casted_value(const HostTensorPtr& tensor, T* val)
 {
+    switch (tensor->get_element_type())
+    {
+    case element::Type_t::bf16:
+        *val = static_cast<T>(*tensor->get_data_ptr<element::Type_t::bf16>());
+        break;
+    case element::Type_t::f16:
+        *val = static_cast<T>(*tensor->get_data_ptr<element::Type_t::f16>());
+        break;
+    case element::Type_t::f32:
+        *val = static_cast<T>(*tensor->get_data_ptr<element::Type_t::f32>());
+        break;
+    case element::Type_t::f64:
+        *val = static_cast<T>(*tensor->get_data_ptr<element::Type_t::f64>());
+        break;
+    case element::Type_t::i8:
+        *val = static_cast<T>(*tensor->get_data_ptr<element::Type_t::i8>());
+        break;
+    case element::Type_t::i16:
+        *val = static_cast<T>(*tensor->get_data_ptr<element::Type_t::i16>());
+        break;
+    case element::Type_t::i32:
+        *val = static_cast<T>(*tensor->get_data_ptr<element::Type_t::i32>());
+        break;
+    case element::Type_t::i64:
+        *val = static_cast<T>(*tensor->get_data_ptr<element::Type_t::i64>());
+        break;
+    case element::Type_t::u8:
+        *val = static_cast<T>(*tensor->get_data_ptr<element::Type_t::u8>());
+        break;
+    case element::Type_t::u16:
+        *val = static_cast<T>(*tensor->get_data_ptr<element::Type_t::u16>());
+        break;
+    case element::Type_t::u32:
+        *val = static_cast<T>(*tensor->get_data_ptr<element::Type_t::u32>());
+        break;
+    case element::Type_t::u64:
+        *val = static_cast<T>(*tensor->get_data_ptr<element::Type_t::u64>());
+        break;
+    default: return false;
+    }
+    return true;
+}
+
+template <element::Type_t ET>
+bool evaluate_v4_range(const HostTensorPtr& out,
+                       const HostTensorPtr& start,
+                       const HostTensorPtr& stop,
+                       const HostTensorPtr& step)
+{
+    using T = typename element_type_traits<ET>::value_type;
+    T start_val;
+    T stop_val;
+    T step_val;
+    if (!(get_casted_value<T>(start, &start_val) && get_casted_value<T>(stop, &stop_val) &&
+          get_casted_value<T>(step, &step_val)))
+    {
+        return false;
+    }
+
+    int64_t out_size = 0;
+
+    int64_t steps = static_cast<int64_t>(std::ceil(double(stop_val - start_val) / step_val));
+    if (steps > 0)
+    {
+        out_size = steps;
+    }
+    Shape out_shape = Shape({static_cast<size_t>(out_size)});
+    out->set_shape(out_shape);
+    runtime::reference::range(&start_val, &step_val, out_shape, out->get_data_ptr<ET>());
+    return true;
+}
+
+bool op::v4::Range::evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const
+{
+    HostTensorPtr out = outputs[0];
+    HostTensorPtr start = inputs[0];
+    HostTensorPtr stop = inputs[1];
+    HostTensorPtr step = inputs[2];
+    switch (m_output_type)
+    {
+    case element::Type_t::bf16:
+        return evaluate_v4_range<element::Type_t::bf16>(out, start, stop, step);
+        break;
+    case element::Type_t::f16:
+        return evaluate_v4_range<element::Type_t::f16>(out, start, stop, step);
+        break;
+    case element::Type_t::f32:
+        return evaluate_v4_range<element::Type_t::f32>(out, start, stop, step);
+        break;
+    case element::Type_t::f64:
+        return evaluate_v4_range<element::Type_t::f64>(out, start, stop, step);
+        break;
+    case element::Type_t::i8:
+        return evaluate_v4_range<element::Type_t::i8>(out, start, stop, step);
+        break;
+    case element::Type_t::i16:
+        return evaluate_v4_range<element::Type_t::i16>(out, start, stop, step);
+        break;
+    case element::Type_t::i32:
+        return evaluate_v4_range<element::Type_t::i32>(out, start, stop, step);
+        break;
+    case element::Type_t::i64:
+        return evaluate_v4_range<element::Type_t::i64>(out, start, stop, step);
+        break;
+    case element::Type_t::u8:
+        return evaluate_v4_range<element::Type_t::u8>(out, start, stop, step);
+        break;
+    case element::Type_t::u16:
+        return evaluate_v4_range<element::Type_t::u16>(out, start, stop, step);
+        break;
+    case element::Type_t::u32:
+        return evaluate_v4_range<element::Type_t::u32>(out, start, stop, step);
+        break;
+    case element::Type_t::u64:
+        return evaluate_v4_range<element::Type_t::u64>(out, start, stop, step);
+        break;
+    case element::Type_t::dynamic:
+    case element::Type_t::u1:
+    case element::Type_t::undefined:
+    case element::Type_t::boolean:
+        NODE_VALIDATION_CHECK(
+            this, false, "Internal nGraph error: unsupported element type: ", m_output_type);
+        break;
+    }
     return false;
 }
 
