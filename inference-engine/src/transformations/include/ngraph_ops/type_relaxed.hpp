b7502c0fe355c3c567e46c9cc21ea57e75090053
Edward Shogulin <edward.shogulin@intel.com>
2020-08-07 01:10:58 +0300

    [nGraph] workarounds in TypeRelaxed

--- inference-engine/src/transformations/include/ngraph_ops/type_relaxed.hpp
+++ inference-engine/src/transformations/include/ngraph_ops/type_relaxed.hpp
@@ -12,6 +12,7 @@
 #include <transformations_visibility.hpp>
 
 #include "ngraph/op/op.hpp"
+#include "ngraph/opsets/opset1.hpp"
 
 namespace ngraph {
 namespace op {
@@ -162,12 +163,23 @@ void TypeRelaxed<BaseOp>::validate_and_infer_types() {
         }
     }
 
-    BaseOp::validate_and_infer_types();
+    // TODO: workaround
+    if (is_type<ngraph::opset1::Interpolate>(this)) {
+        // Restore original input data types
+        for (size_t i = 0; i < BaseOp::get_input_size(); ++i) {
+            BaseOp::get_input_tensor(i).set_tensor_type(old_input_types[i], BaseOp::get_input_partial_shape(i));
+        }
 
-    // Restore original input data types
-    for (size_t i = 0; i < BaseOp::get_input_size(); ++i) {
-        BaseOp::get_input_tensor(i).set_tensor_type(old_input_types[i], BaseOp::get_input_partial_shape(i));
+        BaseOp::validate_and_infer_types();
+    } else {
+        BaseOp::validate_and_infer_types();
+
+        // Restore original input data types
+        for (size_t i = 0; i < BaseOp::get_input_size(); ++i) {
+            BaseOp::get_input_tensor(i).set_tensor_type(old_input_types[i], BaseOp::get_input_partial_shape(i));
+        }
     }
+
 
     // Override (some) output types
     for (size_t i = 0; i < BaseOp::get_output_size(); ++i) {
