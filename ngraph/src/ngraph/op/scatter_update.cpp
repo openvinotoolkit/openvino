882b20d64ba329675e7c7ec743e093c942f87c35
Adam Osewski <adam.osewski@intel.com>
2020-08-07 10:46:54 +0200

    Reference implementation for ScatterUpdate and use of it in evaluate.

--- ngraph/src/ngraph/op/scatter_update.cpp
+++ ngraph/src/ngraph/op/scatter_update.cpp
@@ -15,7 +15,11 @@
 //*****************************************************************************
 
 #include "ngraph/op/scatter_update.hpp"
+#include "ngraph/runtime/reference/scatter_update.hpp"
 #include "ngraph/shape.hpp"
+#include "ngraph/type/element_type.hpp"
+#include "ngraph/type/element_type_traits.hpp"
+#include "ngraph/validation_util.hpp"
 
 using namespace std;
 using namespace ngraph;
@@ -35,4 +39,133 @@ shared_ptr<Node> op::v3::ScatterUpdate::clone_with_new_inputs(const OutputVector
     check_new_args_count(this, new_args);
     return make_shared<v3::ScatterUpdate>(
         new_args.at(0), new_args.at(1), new_args.at(2), new_args.at(3));
+}
+
+namespace
+{
+    template <element::Type_t DT, element::Type_t IT>
+    bool evaluate(const HostTensorPtr& data,
+                  const HostTensorPtr& indices,
+                  const HostTensorPtr& updates,
+                  const HostTensorPtr& out,
+                  const int64_t normalized_axis)
+    {
+        using DataType = typename element_type_traits<DT>::value_type;
+        using IndicesType = typename element_type_traits<IT>::value_type;
+
+        out->set_shape(data->get_shape());
+        runtime::reference::scatter_update<DataType, IndicesType>(
+            data->get_data_ptr<DataType>(),
+            indices->get_data_ptr<IndicesType>(),
+            updates->get_data_ptr<DataType>(),
+            normalized_axis,
+            out->get_data_ptr<DataType>(),
+            data->get_shape(),
+            indices->get_shape(),
+            updates->get_shape());
+
+        return true;
+    }
+
+    template <element::Type_t DT>
+    bool evaluate(const HostTensorPtr& data,
+                  const HostTensorPtr& indices,
+                  const HostTensorPtr& updates,
+                  const HostTensorPtr& out,
+                  const int64_t normalized_axis)
+    {
+        // Dispatch specialization based on indicies data type.
+        bool rc = true;
+
+        switch (indices->get_element_type())
+        {
+        case element::Type_t::i8:
+        case element::Type_t::u8:
+            rc = evaluate<DT, element::Type_t::u8>(data, indices, updates, out, normalized_axis);
+            break;
+        case element::Type_t::i16:
+        case element::Type_t::u16:
+            rc = evaluate<DT, element::Type_t::u16>(data, indices, updates, out, normalized_axis);
+            break;
+        case element::Type_t::i32:
+        case element::Type_t::u32:
+            rc = evaluate<DT, element::Type_t::u32>(data, indices, updates, out, normalized_axis);
+            break;
+        case element::Type_t::i64:
+        case element::Type_t::u64:
+            rc = evaluate<DT, element::Type_t::u64>(data, indices, updates, out, normalized_axis);
+            break;
+        default: rc = false; break;
+        }
+        return rc;
+    }
+
+    bool evaluate_scatter_update(const HostTensorPtr& data,
+                                 const HostTensorPtr& indices,
+                                 const HostTensorPtr& updates,
+                                 const HostTensorPtr& out,
+                                 const int64_t normalized_axis)
+    {
+        // Dispatch based on data, updates and output data type.
+        bool rc = true;
+        switch (out->get_element_type())
+        {
+        case element::Type_t::i32:
+        case element::Type_t::u32:
+            rc = evaluate<element::Type_t::u32>(data, indices, updates, out, normalized_axis);
+            break;
+        case element::Type_t::i64:
+        case element::Type_t::u64:
+            rc = evaluate<element::Type_t::u64>(data, indices, updates, out, normalized_axis);
+            break;
+            TYPE_CASE(f16)(data, indices, updates, out, normalized_axis);
+            break;
+            TYPE_CASE(f32)(data, indices, updates, out, normalized_axis);
+            break;
+        default: rc = false; break;
+        }
+        return rc;
+    }
+}
+
+bool op::v3::ScatterUpdate::evaluate(const HostTensorVector& outputs,
+                                     const HostTensorVector& inputs) const
+{
+    const auto& data = inputs[0];
+    const auto& indices = inputs[1];
+    const auto& updates = inputs[2];
+    const auto& axis = inputs[3];
+    const auto& out = outputs[0];
+
+    int64_t axis_val = 0;
+    switch (axis->get_element_type())
+    {
+    case element::Type_t::i8: axis_val = axis->get_data_ptr<element::Type_t::i8>()[0]; break;
+    case element::Type_t::i16: axis_val = axis->get_data_ptr<element::Type_t::i16>()[0]; break;
+    case element::Type_t::i32: axis_val = axis->get_data_ptr<element::Type_t::i32>()[0]; break;
+    case element::Type_t::i64: axis_val = axis->get_data_ptr<element::Type_t::i64>()[0]; break;
+    case element::Type_t::u8: axis_val = axis->get_data_ptr<element::Type_t::u8>()[0]; break;
+    case element::Type_t::u16: axis_val = axis->get_data_ptr<element::Type_t::u16>()[0]; break;
+    case element::Type_t::u32: axis_val = axis->get_data_ptr<element::Type_t::u32>()[0]; break;
+    case element::Type_t::u64: axis_val = axis->get_data_ptr<element::Type_t::u64>()[0]; break;
+    default: throw ngraph_error("axis element type is not integral data type");
+    }
+
+    const auto& input_rank = get_input_partial_shape(0).rank();
+    int64_t normalized_axis = axis_val;
+
+    if (normalized_axis < 0)
+    {
+        if (input_rank.is_static())
+        {
+            normalized_axis = ngraph::normalize_axis(this, axis_val, input_rank);
+        }
+        else
+        {
+            normalized_axis = ngraph::normalize_axis(
+                this, axis_val, static_cast<int64_t>(data->get_shape().size()));
+        }
+    }
+
+    return evaluate_scatter_update(data, indices, updates, out, normalized_axis);
 }
---
+++ ngraph/src/ngraph/runtime/reference/scatter_update.hpp
@@ -0,0 +1,115 @@
+//*****************************************************************************
+// Copyright 2017-2020 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+
+#pragma once
+
+#include <cstring>
+
+#include "ngraph/check.hpp"
+#include "ngraph/coordinate_transform.hpp"
+#include "ngraph/shape.hpp"
+
+namespace ngraph
+{
+    namespace runtime
+    {
+        namespace reference
+        {
+            template <typename DataType, typename IndicesType>
+            void scatter_update(const DataType* input_data,
+                                const IndicesType* indices,
+                                const DataType* updates,
+                                const int64_t& axis,
+                                DataType* out_buf,
+                                const Shape& data_shape,
+                                const Shape& indices_shape,
+                                const Shape& updates_shape)
+            {
+                // Copy inputs to out
+                std::memcpy(out_buf, input_data, sizeof(DataType) * shape_size(data_shape));
+
+                // Algorithm overview
+                // data[..., indices[m, n, ..., p], ...] = updates[..., m, n, ..., p, ...]
+                // where first ... in the data corresponds to first axis dimensions,
+                // last ... in the data corresponds to the rank(data) - (axis + 1) dimensions.
+
+                //
+                // for i_coord in indices[m, n, ..., p]:
+                //     i_idx = index(i_coord)
+                //     for d_coord in slice data[..., i_idx, ...] &&
+                //     for u_coord in slice updates[..., i_coord, ...]:
+                //          data[index(d_coord)] = updates[index(u_coord)]
+
+                CoordinateTransform indices_transform{indices_shape};
+                CoordinateTransform data_transform{data_shape};
+
+                size_t indices_ndim = indices_shape.size();
+                size_t updates_ndim = updates_shape.size();
+
+                // Create an outer CoordinateTransform for "update", which would allow to
+                // iterate only over "indicies" dimensions:
+                // set to "1" all non-indices dimensions
+                // updates[1, ..., 1, m, n, ..., p, 1, 1,..., 1]
+                Coordinate updates_indices_start_corner(updates_ndim, 0);
+                Coordinate updates_indices_end_corner(updates_ndim, 1);
+                for (size_t i = 0; i < indices_ndim; ++i)
+                {
+                    updates_indices_end_corner[axis + i] = updates_shape[axis + i];
+                }
+                CoordinateTransform updates_indices_transform(
+                    updates_shape, updates_indices_start_corner, updates_indices_end_corner);
+                // Is needed to simultaneously iterate over updates coordinates while
+                // iterating over indices.
+                auto updates_indices_coord_iter = updates_indices_transform.begin();
+
+                for (const Coordinate& indices_cord : indices_transform)
+                {
+                    const size_t indices_idx = indices_transform.index(indices_cord);
+                    IndicesType slice_index = indices[indices_idx];
+
+                    // Define the extent of coordinates which will be updated.
+                    Coordinate out_start_corner(data_shape.size(), 0);
+                    Coordinate out_end_corner(data_shape);
+                    out_start_corner[axis] = static_cast<size_t>(slice_index);
+                    out_end_corner[axis] = out_start_corner[axis] + 1;
+                    CoordinateTransform out_transform(data_shape, out_start_corner, out_end_corner);
+
+                    // Define the CoordinateTransform for updates coordinates.
+                    // All except indices-dimensions.
+                    Coordinate updates_update_start_corner = *updates_indices_coord_iter;
+                    Coordinate updates_update_end_corner(updates_shape);
+                    for (size_t i = 0; i < indices_ndim; ++i)
+                    {
+                        updates_update_end_corner[axis + i] =
+                            updates_update_start_corner[axis + i] + 1;
+                    }
+                    // udpates_shape (start): [ 0, ...,  0, m  , n  , ... p  ,  0, ...,  0]
+                    // updates_shape (end):   [-1, ..., -1, m+1, n+1, ... p+1, -1, ..., -1]
+                    CoordinateTransform updates_update_transform(
+                        updates_shape, updates_update_start_corner, updates_update_end_corner);
+                    auto updates_update_coord_iter = updates_update_transform.begin();
+                    for (const Coordinate& out_cord : out_transform)
+                    {
+                        out_buf[out_transform.index(out_cord)] =
+                            updates[updates_update_transform.index(*updates_update_coord_iter)];
+                        updates_update_coord_iter++;
+                    }
+                    updates_indices_coord_iter++;
+                }
+            }
+        }
+    }
+}
