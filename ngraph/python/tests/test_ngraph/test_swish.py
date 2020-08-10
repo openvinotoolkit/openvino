d760cf5c3d991ca475228647487c01fd11f6e72e
Evgeny Lazarev <evgeny.lazarev@intel.com>
2020-08-10 08:38:43 +0300

    Fixed unit test for Swish

--- ngraph/python/tests/test_ngraph/test_swish.py
+++ ngraph/python/tests/test_ngraph/test_swish.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ******************************************************************************
-import numpy as np
-
 import ngraph as ng
 from ngraph.impl import Shape, Type
 
