97ebdf84d059b1e9d138c0e07ec07769a1dd154b
Ilya Lavrenov <ilya.lavrenov@intel.com>
2020-08-11 07:06:24 +0300

    Moved plugin-specific utils from blob_factory.hpp (#1710)

--- inference-engine/src/inference_engine/blob_factory.cpp
+++ inference-engine/src/inference_engine/blob_factory.cpp
@@ -22,33 +22,3 @@ InferenceEngine::Blob::Ptr make_blob_with_precision(const InferenceEngine::Tenso
 InferenceEngine::Blob::Ptr make_plain_blob(InferenceEngine::Precision prec, const InferenceEngine::SizeVector dims) {
     return make_blob_with_precision({prec, dims, InferenceEngine::TensorDesc::getLayoutByDims(dims)});
 }
-
-InferenceEngine::Blob::Ptr CreateBlobFromData(const InferenceEngine::DataPtr& data) {
-    // TODO Here some decision should be made about the layout.
-    // For now we just pass the layout and use conversion to NCHW for ANY.
-    InferenceEngine::Layout targetLayout = data->getLayout();
-    if (data->getLayout() == InferenceEngine::Layout::ANY) {
-        targetLayout = InferenceEngine::Layout::NCHW;
-    }
-
-    InferenceEngine::TensorDesc desc(data->getPrecision(), data->getTensorDesc().getDims(), targetLayout);
-
-    switch (data->getPrecision()) {
-    case InferenceEngine::Precision::FP32:
-        return std::make_shared<InferenceEngine::TBlob<float>>(desc);
-    case InferenceEngine::Precision::Q78:
-    case InferenceEngine::Precision::I16:
-    case InferenceEngine::Precision::FP16:
-        return std::make_shared<InferenceEngine::TBlob<short>>(desc);
-    case InferenceEngine::Precision::U8:
-        return std::make_shared<InferenceEngine::TBlob<uint8_t>>(desc);
-    case InferenceEngine::Precision::I8:
-        return std::make_shared<InferenceEngine::TBlob<int8_t>>(desc);
-    case InferenceEngine::Precision::I32:
-        return std::make_shared<InferenceEngine::TBlob<int32_t>>(desc);
-    case InferenceEngine::Precision::BF16:
-            return std::make_shared<InferenceEngine::TBlob<short>>(desc);
-    default:
-        THROW_IE_EXCEPTION << "precision is no set";
-    }
-}
