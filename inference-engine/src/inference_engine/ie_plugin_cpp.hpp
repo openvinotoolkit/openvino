dd2826998947c7e9c9092b5dc02ec6b15149ecc0
Ilya Lavrenov <ilya.lavrenov@intel.com>
2020-08-13 16:38:31 +0300

    Fixed exception catch / throw

--- inference-engine/src/inference_engine/ie_plugin_cpp.hpp
+++ inference-engine/src/inference_engine/ie_plugin_cpp.hpp
@@ -19,13 +19,18 @@
 #include "details/ie_exception_conversion.hpp"
 #include "ie_plugin_ptr.hpp"
 
-#define CALL_RETURN_FNC(function, ...)         \
+#define CALL_STATEMENT(...)                                                                        \
     if (!actual) THROW_IE_EXCEPTION << "Wrapper used in the CALL_RETURN_FNC was not initialized."; \
-    return actual->function(__VA_ARGS__);
-
-#define CALL_FNC(function, ...)         \
-    if (!actual) THROW_IE_EXCEPTION << "Wrapper used in the CALL_RETURN_FNC was not initialized."; \
-    actual->function(__VA_ARGS__);
+    try {                                                                                          \
+        __VA_ARGS__;                                                                               \
+    } catch (const InferenceEngine::details::InferenceEngineException& iex) {                      \
+        InferenceEngine::details::extract_exception(iex.hasStatus() ?                              \
+            iex.getStatus() : GENERAL_ERROR, iex.what());                                          \
+    } catch (const std::exception& ex) {                                                           \
+        InferenceEngine::details::extract_exception(GENERAL_ERROR, ex.what());                     \
+    } catch (...) {                                                                                \
+        InferenceEngine::details::extract_exception(UNEXPECTED, "");                               \
+    }
 
 namespace InferenceEngine {
 
@@ -38,14 +43,8 @@ class InferencePlugin {
     InferenceEnginePluginPtr actual;
 
 public:
-    /** @brief A default constructor */
     InferencePlugin() = default;
 
-    /**
-     * @brief Constructs a plugin instance from the given pointer.
-     *
-     * @param pointer Initialized Plugin pointer
-     */
     explicit InferencePlugin(const InferenceEnginePluginPtr& pointer): actual(pointer) {
         if (actual == nullptr) {
             THROW_IE_EXCEPTION << "InferencePlugin wrapper was not initialized.";
@@ -60,120 +59,72 @@ public:
     }
 
     void SetName(const std::string & deviceName) {
-        CALL_FNC(SetName, deviceName);
+        CALL_STATEMENT(actual->SetName(deviceName));
     }
 
     void SetCore(ICore* core) {
-        CALL_FNC(SetCore, core);
+        CALL_STATEMENT(actual->SetCore(core));
     }
 
-    /**
-     * @copybrief IInferencePlugin::GetVersion
-     *
-     * Wraps IInferencePlugin::GetVersion
-     * @return A plugin version
-     */
     const Version GetVersion() const {
-        CALL_RETURN_FNC(GetVersion);
+        CALL_STATEMENT(return actual->GetVersion());
     }
 
-    /**
-     * @copybrief InferencePlugin::LoadNetwork
-     *
-     * Wraps IInferencePlugin::LoadNetwork
-     * @param network A network object to load
-     * @param config A map of configuration options
-     * @return Created Executable Network object
-     */
     ExecutableNetwork LoadNetwork(CNNNetwork network, const std::map<std::string, std::string>& config) {
         IExecutableNetwork::Ptr ret;
-        CALL_FNC(LoadNetwork, ret, network, config);
-        if (ret.get() == nullptr) THROW_IE_EXCEPTION << "Internal error: pointer to executable network is null";
+        CALL_STATEMENT(actual->LoadNetwork(ret, network, config));
         return ExecutableNetwork(ret, actual);
     }
 
-    /**
-     * @copybrief IInferencePlugin::AddExtension
-     *
-     * Wraps IInferencePlugin::AddExtension
-     *
-     * @param extension Pointer to loaded Extension
-     */
     void AddExtension(InferenceEngine::IExtensionPtr extension) {
-        CALL_FNC(AddExtension, extension);
+        CALL_STATEMENT(actual->AddExtension(extension));
     }
 
-    /**
-     * @copybrief IInferencePlugin::SetConfig
-     *
-     * Wraps IInferencePlugin::SetConfig
-     * @param config A configuration map
-     */
     void SetConfig(const std::map<std::string, std::string>& config) {
-        CALL_FNC(SetConfig, config);
+        CALL_STATEMENT(actual->SetConfig(config));
     }
 
-    /**
-     * @copybrief IInferencePlugin::ImportNetwork
-     *
-     * Wraps IInferencePlugin::ImportNetwork
-     * @param modelFileName A path to the imported network
-     * @param config A configuration map
-     * @return Created Executable Network object
-     */
     ExecutableNetwork ImportNetwork(const std::string& modelFileName,
                                     const std::map<std::string, std::string>& config) {
-        if (!actual) THROW_IE_EXCEPTION << "Wrapper used in the CALL_RETURN_FNC was not initialized."; \
-        auto ret = actual->ImportNetwork(modelFileName, config);
-        return ExecutableNetwork(ret, actual);
+        CALL_STATEMENT(return ExecutableNetwork(actual->ImportNetwork(modelFileName, config), actual));
     }
 
-    /**
-     * @copybrief IInferencePlugin::QueryNetwork
-     *
-     * Wraps IInferencePlugin::QueryNetwork
-     *
-     * @param network A network object to query
-     * @param config A configuration map
-     * @param res Query results
-     */
     void QueryNetwork(const ICNNNetwork& network, const std::map<std::string, std::string>& config,
                       QueryNetworkResult& res) const {
-        if (actual == nullptr) THROW_IE_EXCEPTION << "InferencePlugin wrapper was not initialized";
-        actual->QueryNetwork(network, config, res);
+        CALL_STATEMENT(actual->QueryNetwork(network, config, res));
         if (res.rc != OK) THROW_IE_EXCEPTION << res.resp.msg;
     }
 
     ExecutableNetwork ImportNetwork(std::istream& networkModel,
                                     const std::map<std::string, std::string> &config) {
-        CALL_RETURN_FNC(ImportNetwork, networkModel, config);
+        CALL_STATEMENT(return actual->ImportNetwork(networkModel, config));
     }
 
     Parameter GetMetric(const std::string& name, const std::map<std::string, Parameter>& options) const {
-        CALL_RETURN_FNC(GetMetric, name, options);
+        CALL_STATEMENT(return actual->GetMetric(name, options));
     }
 
     ExecutableNetwork LoadNetwork(const ICNNNetwork& network, const std::map<std::string, std::string>& config,
                                   RemoteContext::Ptr context) {
-        CALL_RETURN_FNC(LoadNetwork, network, config, context);
+        CALL_STATEMENT(return actual->LoadNetwork(network, config, context));
     }
 
     RemoteContext::Ptr CreateContext(const ParamMap& params) {
-        CALL_RETURN_FNC(CreateContext, params);
+        CALL_STATEMENT(return actual->CreateContext(params));
     }
 
     RemoteContext::Ptr GetDefaultContext() {
-        CALL_RETURN_FNC(GetDefaultContext);
+        CALL_STATEMENT(return actual->GetDefaultContext());
     }
 
     ExecutableNetwork ImportNetwork(std::istream& networkModel,
                                     const RemoteContext::Ptr& context,
                                     const std::map<std::string, std::string>& config) {
-        CALL_RETURN_FNC(ImportNetwork, networkModel, context, config);
+        CALL_STATEMENT(return actual->ImportNetwork(networkModel, context, config));
     }
 
     Parameter GetConfig(const std::string& name, const std::map<std::string, Parameter>& options) const {
-        CALL_RETURN_FNC(GetConfig, name, options);
+        CALL_STATEMENT(return actual->GetConfig(name, options));
     }
 
     /**
@@ -192,3 +143,5 @@ public:
     using Ptr = std::shared_ptr<InferencePlugin>;
 };
 }  // namespace InferenceEngine
+
+#undef CALL_STATEMENT
