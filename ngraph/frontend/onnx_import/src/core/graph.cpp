e116a09014ac1dbf11ba541a4f521ffd37b152b5
tomdol <tomasz.dolbniak@intel.com>
2020-08-13 20:32:25 +0200

    Accept empty initializers from ONNX models

--- ngraph/frontend/onnx_import/src/core/graph.cpp
+++ ngraph/frontend/onnx_import/src/core/graph.cpp
@@ -86,12 +86,23 @@ namespace ngraph
                 if (initializer_tensor.has_name())
                 {
                     Tensor tensor = Tensor{initializer_tensor};
-                    initializers.emplace(initializer_tensor.name(), tensor);
 
                     // For each initializer create a Constant node and store it in cache
-                    auto ng_constant = tensor.get_ng_constant();
-                    add_provenance_tag_to_initializer(tensor, ng_constant);
-                    m_cache->emplace_node(initializer_tensor.name(), std::move(ng_constant));
+                    try
+                    {
+                        auto ng_constant = tensor.get_ng_constant();
+                        initializers.emplace(initializer_tensor.name(), tensor);
+                        add_provenance_tag_to_initializer(tensor, ng_constant);
+                        m_cache->emplace_node(initializer_tensor.name(), std::move(ng_constant));
+                    }
+                    catch (const ngraph::ngraph_error& exc)
+                    {
+                        NGRAPH_WARN << "Could not create an nGraph Constant for initializer '"
+                                    << initializer_tensor.name()
+                                    << "'. Each input referencing it will be replaced with a "
+                                       "NullNode. Detailed error:\n"
+                                    << exc.what();
+                    }
                 }
             }
 
