a4dcfed1a9c67e8fe611684711a38af6236afc35
Ilya Lavrenov <ilya.lavrenov@intel.com>
2020-08-14 12:11:54 +0300

    Simplified plugin interfaces (#1745)
    * Simplified plugin interface
    
    * Allow not implemented
    
    * Fixes
    
    * Fixed CPU plugin tests
    
    * Fixed tests dependencies
    
    * Fixes
    
    * Fixed GPU plugin compilation
    
    * Renamed plugin
    
    * Fixes
    
    * Removed tests for plugin base
    
    * Fix2
    
    * Fix 2
    
    * Define a macro to define plugin creation function
    
    * Clean-up
    
    * Fixed OSX build
    
    * Fixed CentOS
    
    * Fixed exception catch / throw
    
    * Fixed clang issue
    
    * Fixed python tests on macOsx

--- inference-engine/tests/ie_test_utils/unit_test_utils/empty.cpp
+++ inference-engine/tests/ie_test_utils/unit_test_utils/empty.cpp
@@ -7,10 +7,8 @@
 #include "unit_test_utils/mocks/mock_ie_imemory_state.hpp"
 #include "unit_test_utils/mocks/mock_iexecutable_network.hpp"
 #include "unit_test_utils/mocks/mock_iinfer_request.hpp"
-#include "unit_test_utils/mocks/mock_iinference_plugin.hpp"
 #include "unit_test_utils/mocks/mock_not_empty_icnn_network.hpp"
 
-#include "unit_test_utils/mocks/cpp_interfaces/mock_plugin_impl.hpp"
 #include "unit_test_utils/mocks/cpp_interfaces/mock_task_executor.hpp"
 
 #include "unit_test_utils/mocks/cpp_interfaces/impl/mock_async_infer_request_default.hpp"
@@ -26,6 +24,7 @@
 #include "unit_test_utils/mocks/cpp_interfaces/interface/mock_iexecutable_network_internal.hpp"
 #include "unit_test_utils/mocks/cpp_interfaces/interface/mock_iinfer_request_internal.hpp"
 #include "unit_test_utils/mocks/cpp_interfaces/interface/mock_imemory_state_internal.hpp"
+#include "unit_test_utils/mocks/cpp_interfaces/interface/mock_iinference_plugin.hpp"
 
 #include "unit_test_utils/mocks/shape_infer/mock_input_controller.hpp"
 #include "unit_test_utils/mocks/shape_infer/mock_ishape_infer_impl.hpp"
