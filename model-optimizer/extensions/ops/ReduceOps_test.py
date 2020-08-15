1bd6eaf595a1eb585118e3f8fd9ca743a4844718
Evgeny Lazarev <evgeny.lazarev@intel.com>
2020-08-15 22:20:06 +0300

    Removed ReduceLp. Added ReduceL1 and ReduceL2

--- model-optimizer/extensions/ops/ReduceOps_test.py
+++ model-optimizer/extensions/ops/ReduceOps_test.py
@@ -28,7 +28,6 @@ nodes_attributes = {
     **regular_op_with_shaped_data('data', [1, 3, 224, 224], {'type': 'Parameter', 'value': None,
                                                              '_out_port_data_type': {0: np.float32}}),
     **valued_const_with_data('axis', int64_array(0)),
-    **valued_const_with_data('p', int64_array(2)),
     **regular_op_with_shaped_data('reduce_lp', None, {'op': 'ReduceLp', 'type': None, 'name': 'my_reduce_lp'}),
     **regular_op_with_shaped_data('identity', None, {'op': 'Identity', 'name': 'identity'}),
     **result('output'),
@@ -49,23 +48,22 @@ class ReduceLpTest(unittest.TestCase):
     ])
     def test_reduce_lp(self, shape, axes, keepdims, p):
         data = np.reshape(np.arange(1, np.prod(shape) + 1, dtype=np.float32), shape)
-        reduced = np.power(np.sum(a=np.abs(np.power(data, p)), axis=tuple(axes), keepdims=keepdims), 1/p)
+        reduced = np.power(np.sum(a=np.abs(np.power(data, p)), axis=tuple(axes), keepdims=keepdims), 1 / p)
         axis = int64_array(axes)
         p = int64_array(p)
         graph = build_graph(nodes_attributes,
                             [*connect('data', '0:reduce_lp'),
                              *connect('axis', '1:reduce_lp'),
-                             *connect('p', '2:reduce_lp'),
                              *connect('reduce_lp', '0:identity'),
                              ('identity', 'identity_d', {'out': 0}),
                              ('identity_d', 'output')
                              ],
                             {'data_d': {'value': data, 'shape': data.shape},
                              'axis_d': {'value': axis, 'shape': axis.shape},
-                             'p_d': {'value': p, 'shape': p.shape},
                              'reduce_lp': {'keep_dims': keepdims}},
                             nodes_with_edges_only=True)
 
         reduce_node = Node(graph, 'reduce_lp')
+        reduce_node.op = reduce_node.type = 'ReduceL' + str(p)
         reduce_infer(reduce_node)
         self.assertTrue(np.array_equal(reduce_node.out_port(0).data.get_value(), reduced))
