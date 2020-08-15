38fb836191ae6f8d797adbf52e3151a229f9fd24
VGavrilov <vladimir.gavrilov@intel.com>
2020-08-15 12:15:04 +0300

    Now the transformation ONNXResize11ToInterpolate generates Interpolate-4 with 4 inputs.

--- model-optimizer/extensions/middle/ONNXResize11ToInterpolateV4.py
+++ model-optimizer/extensions/middle/ONNXResize11ToInterpolateV4.py
@@ -17,10 +17,12 @@
 import logging as log
 import numpy as np
 
-from extensions.ops.elementwise import Mul
+from extensions.ops.Cast import Cast
+from extensions.ops.elementwise import Add, Div, Mul
 from extensions.ops.interpolate import Interpolate
-from mo.front.common.layout import get_height_dim, get_width_dim, get_depth_dim
-from mo.front.common.partial_infer.utils import int64_array
+from mo.front.common.layout import get_depth_dim, get_height_dim, get_width_dim
+from mo.front.common.partial_infer.utils import int64_array, float_array
+from mo.front.tf.graph_utils import create_op_with_const_inputs
 from mo.middle.replacement import MiddleReplacementPattern
 from mo.graph.graph import Graph, rename_nodes
 from mo.ops.const import Const
@@ -70,59 +72,92 @@ class ONNXResize11ToInterpolate4(MiddleReplacementPattern):
             begin_dim = get_depth_dim(layout, input_rank)
             end_dim = get_width_dim(layout, input_rank) + 1
 
-        begin_slice = Const(graph, {'name': resize_name + '/begin_slice_',
-                                    'value': int64_array([begin_dim])}).create_node()
-        end_slice = Const(graph, {'name': resize_name + '/end_slice_',
-                                  'value': int64_array([end_dim])}).create_node()
-        strided_slice = StridedSlice(graph,
-                                     {'name': resize_name + '/StridedSlice_',
-                                      'begin_mask': int64_array([1]),
-                                      'end_mask': int64_array([1]),
-                                      'new_axis_mask': int64_array([0]),
-                                      'shrink_axis_mask': int64_array([0]),
-                                      'ellipsis_mask': int64_array([0]),
-                                      }).create_node()
-
-        begin_slice.out_port(0).connect(strided_slice.in_port(1))
-        end_slice.out_port(0).connect(strided_slice.in_port(2))
-
+        sizes_ss = create_op_with_const_inputs(graph, StridedSlice,
+                                               {1: int64_array([begin_dim]),
+                                                2: int64_array([end_dim]),
+                                                3: int64_array([1])},
+                                               {'name': resize_name + '/StridedSlice_',
+                                                'begin_mask': int64_array([1]),
+                                                'end_mask': int64_array([1]),
+                                                'new_axis_mask': int64_array([0]),
+                                                'shrink_axis_mask': int64_array([0]),
+                                                'ellipsis_mask': int64_array([0])})
+        scales_ss = create_op_with_const_inputs(graph, StridedSlice,
+                                                {1: int64_array([begin_dim]),
+                                                 2: int64_array([end_dim]),
+                                                 3: int64_array([1])},
+                                                {'name': resize_name + '/StridedSlice_',
+                                                 'begin_mask': int64_array([1]),
+                                                 'end_mask': int64_array([1]),
+                                                 'new_axis_mask': int64_array([0]),
+                                                 'shrink_axis_mask': int64_array([0]),
+                                                 'ellipsis_mask': int64_array([0])})
         axes_node = Const(graph,
-                          {
-                              'name': resize_name + '/axis_',
-                              'value': int64_array(np.arange(begin_dim, end_dim))
-                          }).create_node()
+                          {'name': resize_name + '/axis_',
+                           'value': int64_array(np.arange(begin_dim, end_dim))}).create_node()
+
+        shape_calculation_mode = 'scales' if num_of_inputs == 3 else 'sizes'
+
         interpolate_node = Interpolate(graph, {'version': 'opset4',
                                                'mode': convert_mode(resize.mode),
                                                'coordinate_transformation_mode': resize.coordinate_transformation_mode,
-                                               'cube_coeff': resize.cubic_coeff_a,
+                                               'cube_coeff': resize.cubic_coeff,
                                                'nearest_mode': resize.nearest_mode,
                                                'pads_begin': int64_array([0]),
                                                'pads_end': int64_array([0]),
                                                'antialias': 0,
-                                               'in_ports_count': 3}).create_node()
+                                               'shape_calculation_mode': shape_calculation_mode,
+                                               'in_ports_count': 4}).create_node()
 
-        strided_slice.out_port(0).connect(interpolate_node.in_port(1))
-        axes_node.out_port(0).connect(interpolate_node.in_port(2))
-
-        connection_of_resize_input = resize.in_port(0).get_connection()
-        connection_of_resize_input.set_destination(interpolate_node.in_port(0))
+        axes_node.out_port(0).connect(interpolate_node.in_port(3))
+        shape_of = Shape(graph, {'name': resize_name + '/ShapeOf_'}).create_node()
 
         if num_of_inputs == 3:
-            shape_of = Shape(graph, {'name': resize_name + '/ShapeOf_'}).create_node()
+            cast_shape_to_float = Cast(graph, {'dst_type': np.float32}).create_node()
             mul_node = Mul(graph, {'name': resize_name + '/Mul_'}).create_node()
+            shape_of.out_port(0).connect(cast_shape_to_float.in_port(0))
+            cast_shape_to_float.out_port(0).connect(mul_node.in_port(0))
+            cast_mul_result_to_int = Cast(graph, {'dst_type': np.int64}).create_node()
+            mul_node.out_port(0).connect(cast_mul_result_to_int.in_port(0))
+            cast_mul_result_to_int.out_port(0).connect(sizes_ss.in_port(0))
+            sizes_ss.out_port(0).connect(interpolate_node.in_port(1))
+            scales_ss.out_port(0).connect(interpolate_node.in_port(2))
 
-            shape_of.out_port(0).connect(mul_node.in_port(0))
-            mul_node.out_port(0).connect(strided_slice.in_port(0))
+            rename_nodes([(resize, resize_name + '/delete'), (interpolate_node, resize_name)])
 
-            connection_of_resize_input.get_source().connect(shape_of.in_port(0))
+            connection_of_resize_input = resize.in_port(0).get_connection()
+            connection_of_resize_input.set_destination(interpolate_node.in_port(0))
 
             connection_of_scales = resize.in_port(2).get_connection()
-            connection_of_scales.set_destination(mul_node.in_port(1))
-        else:
-            connection_of_sizes = resize.in_port(3).get_connection()
-            connection_of_sizes.set_destination(strided_slice.in_port(0))
+            connection_of_scales.set_destination(scales_ss.in_port(0))
 
-        rename_nodes([(resize, resize_name + '/delete'), (interpolate_node, resize_name)])
+            connection_of_resize_input.get_source().connect(shape_of.in_port(0))
+            connection_of_scales.get_source().connect(mul_node.in_port(1))
+        else:
+            cast_shape_to_float = Cast(graph, {'dst_type': np.float32}).create_node()
+            cast_sizes_to_float = Cast(graph, {'dst_type': np.float32}).create_node()
+            div_node = Div(graph, {'name': resize_name + '/Div_'}).create_node()
+            cast_sizes_to_float.out_port(0).connect(div_node.in_port(0))
+            cast_shape_to_float.out_port(0).connect(div_node.in_port(1))
+            shape_of.out_port(0).connect(cast_shape_to_float.in_port(0))
+            add_node = create_op_with_const_inputs(graph, Add,
+                                                   {1: float_array([1.0e-5])},
+                                                   {'name': resize_name + '/Add_'})
+            div_node.out_port(0).connect(add_node.in_port(0))
+            add_node.out_port(0).connect(scales_ss.in_port(0))
+            scales_ss.out_port(0).connect(interpolate_node.in_port(2))
+            sizes_ss.out_port(0).connect(interpolate_node.in_port(1))
+
+            rename_nodes([(resize, resize_name + '/delete'), (interpolate_node, resize_name)])
+
+            connection_of_resize_input = resize.in_port(0).get_connection()
+            connection_of_resize_input.set_destination(interpolate_node.in_port(0))
+
+            connection_of_sizes = resize.in_port(3).get_connection()
+            connection_of_sizes.set_destination(sizes_ss.in_port(0))
+
+            connection_of_resize_input.get_source().connect(shape_of.in_port(0))
+            connection_of_sizes.get_source().connect(cast_sizes_to_float.in_port(0))
 
         resize.out_port(0).get_connection().set_source(interpolate_node.out_port(0))
 
