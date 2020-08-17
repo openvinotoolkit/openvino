0745f4e0f93466ff65dbfa1d081216a649b68ab7
Anna Alberska <anna.alberska@intel.com>
2020-08-17 12:03:35 +0200

    [GNA] check whether permute operation is last one in the model

--- inference-engine/src/gna_plugin/optimizer/gna_pass_manager.cpp
+++ inference-engine/src/gna_plugin/optimizer/gna_pass_manager.cpp
@@ -591,19 +591,21 @@ void RemovePermutationsNHWCToNCHWPass::run() {
     for (auto&& toRemove : permutationsToRemove) {
         gnalog() << toRemove->type << " layer '" << toRemove->name << "' will be removed" << '\n';
 
-        auto next = getInputTo(toRemove->outData.front()).begin()->second;
-        if (LayerInfo(next).isConvolution()) {
-            next->input()->setDims(toRemove->input()->getDims());
-            next->input()->setLayout(Layout::NHWC);
-            auto layerBeforePermute = CNNNetPrevLayer(toRemove);
-            layerBeforePermute->outData[0]->setLayout(Layout::NHWC);
+        if (getInputTo(toRemove->outData.front()).size()) {
+            auto next = getInputTo(toRemove->outData.front()).begin()->second;
+            if (LayerInfo(next).isConvolution()) {
+                next->input()->setDims(toRemove->input()->getDims());
+                next->input()->setLayout(Layout::NHWC);
+                auto layerBeforePermute = CNNNetPrevLayer(toRemove);
+                layerBeforePermute->outData[0]->setLayout(Layout::NHWC);
 
-            auto& convolution = dynamic_cast<ConvolutionLayer&>(*next);
-            if (convolution._kernel_y != 1) {
-                THROW_GNA_LAYER_EXCEPTION(next) << "this case is not implemented yet";
+                auto& convolution = dynamic_cast<ConvolutionLayer&>(*next);
+                if (convolution._kernel_y != 1) {
+                    THROW_GNA_LAYER_EXCEPTION(next) << "this case is not implemented yet";
+                }
+                auto in_channels = next->input()->getDims()[3];
+                convolution._kernel_y = in_channels;
             }
-            auto in_channels = next->input()->getDims()[3];
-            convolution._kernel_y = in_channels;
         }
         auto prev = CNNNetPrevLayer(toRemove);
         if (LayerInfo(prev).isConvolution()) {
