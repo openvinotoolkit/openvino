44bf15cdd938164514f2efa845fd88c70e3fdf51
Gleb Kazantaev <gleb.kazantaev@intel.com>
2020-08-17 20:16:06 +0300

    Added ConvertPadToConvolution pass

---
+++ inference-engine/src/transformations/include/transformations/convert_pad_to_conv.hpp
@@ -0,0 +1,34 @@
+// Copyright (C) 2018-2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <vector>
+#include <memory>
+
+#include <transformations_visibility.hpp>
+
+#include <ngraph/pass/graph_rewrite.hpp>
+
+namespace ngraph {
+namespace pass {
+
+class TRANSFORMATIONS_API ConvertPadToConvolution;
+
+}  // namespace pass
+}  // namespace ngraph
+
+/**
+ * @ingroup ie_transformation_common_api
+ * @brief ConvertPadToConvolution transformation replaces Pad operation
+ * with Convolution but has some restrictions on Pad parameters:
+ * 1. PadMode must be Constant and value is equal to 0
+ * 2. Padding must be applied only for spatial dimensions
+ * 3. Input shape rank must be static and greater than 3
+ */
+
+class ngraph::pass::ConvertPadToConvolution: public ngraph::pass::MatcherPass {
+public:
+    ConvertPadToConvolution();
+};
