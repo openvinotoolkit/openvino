aa35c59c5ffd1f9f0818c89aa16f21ddc832f646
Gleb Kazantaev <gleb.kazantaev@intel.com>
2020-08-17 15:39:58 +0300

    FGTT for CF (#1792)
    * Added FGTT for ConstantFolding
    
    * Added test for CF disabling

--- ngraph/core/src/pass/constant_folding_scatter.cpp
+++ ngraph/core/src/pass/constant_folding_scatter.cpp
@@ -173,7 +173,8 @@ void pass::ConstantFolding::construct_constant_scatter_elements_update()
     auto scatter_elem_updt = make_shared<op::v3::ScatterElementsUpdate>(
         data_label, indices_label, updates_label, axis_label);
 
-    auto constant_scatter_elem_updt_callback = [data_label,
+    auto constant_scatter_elem_updt_callback = [this,
+                                                data_label,
                                                 indices_label,
                                                 updates_label,
                                                 axis_label](pattern::Matcher& m) {
@@ -187,6 +188,9 @@ void pass::ConstantFolding::construct_constant_scatter_elements_update()
         const auto updates = static_pointer_cast<op::Constant>(pattern_map[updates_label]);
         const auto axis = static_pointer_cast<op::Constant>(pattern_map[axis_label]);
         const auto scatter_elem_updt = m.get_match_root();
+
+        if (cf_is_disabled(scatter_elem_updt))
+            return false;
 
         NGRAPH_CHECK(revalidate_and_ensure_static(scatter_elem_updt));
 
