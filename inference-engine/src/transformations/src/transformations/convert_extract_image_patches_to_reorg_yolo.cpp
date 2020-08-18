4c907621223c182d16d5bfa78b28808b945a33ea
Chetverikov Anton <anton.chetverikov@intel.com>
2020-08-18 19:15:26 +0300

    Resolve review comments

--- inference-engine/src/transformations/src/transformations/convert_extract_image_patches_to_reorg_yolo.cpp
+++ inference-engine/src/transformations/src/transformations/convert_extract_image_patches_to_reorg_yolo.cpp
@@ -20,6 +20,17 @@ ngraph::pass::ConvertExtractImagePatchesToReorgYolo::ConvertExtractImagePatchesT
         auto &pattern_to_output = m.get_pattern_value_map();
         auto extract_image_patches =  std::dynamic_pointer_cast<ngraph::opset3::ExtractImagePatches>(m.get_match_root());
 
+        /*
+         * In this transformation we raplace ExtractImagePatches operation to ReorgYolo operation
+         * if ExtractImagePatches operation attributes obey the following conditions:
+         *
+         * EIP.sizes = EIP.strides
+         * EIP.rates = {1, 1}
+         * EIP.PadType = VALID
+         * Spatial dimensions of input tensor must be divisible by EIP.strides
+         *
+         */
+
         if (!extract_image_patches || m_transformation_callback(extract_image_patches)) {
             return false;
         }
@@ -42,22 +53,27 @@ ngraph::pass::ConvertExtractImagePatchesToReorgYolo::ConvertExtractImagePatchesT
             return false;
         }
 
+        // Check that ExtractImagePatches input have shape rank == 4
         if (p_shape_input.rank().get_length() != 4) {
             return false;
         }
 
+        // Check that ExtractImagePatches input spatial dimensions are not dynamic
         if (p_shape_input[2].is_dynamic() || p_shape_input[3].is_dynamic()) {
             return false;
         }
 
+        // Check that ExtractImagePatches input spatial dimensions are divisible by EIP.strides
         if (p_shape_input[2].get_length() % strides[0] != 0 || p_shape_input[3].get_length() % strides[1] != 0) {
             return false;
         }
 
+        // Check that EIP.sizes = EIP.strides
         if (sizes[0] != strides[0] || sizes[1] != strides[1]) {
             return false;
         }
 
+        // Check that EIP.rates = {1, 1}
         if (rates[0] != 1 || rates[1] != 1) {
             return false;
         }
@@ -65,9 +81,9 @@ ngraph::pass::ConvertExtractImagePatchesToReorgYolo::ConvertExtractImagePatchesT
         auto reorg_yolo = std::make_shared<ngraph::opset3::ReorgYolo>(extract_image_patches->input(0).get_source_output(),
             ngraph::Strides{extract_image_patches->get_strides()});
 
-        reorg_yolo->set_friendly_name(m.get_match_root()->get_friendly_name());
-        ngraph::copy_runtime_info(pattern_to_output.at(eip).get_node_shared_ptr(), reorg_yolo);
-        ngraph::replace_node(m.get_match_root(), reorg_yolo);
+        reorg_yolo->set_friendly_name(extract_image_patches->get_friendly_name());
+        ngraph::copy_runtime_info(extract_image_patches, reorg_yolo);
+        ngraph::replace_node(extract_image_patches, reorg_yolo);
         return true;
     };
 
