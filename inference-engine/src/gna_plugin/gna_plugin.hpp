65342799906bbd4817085593091ab9a911fb1649
Denis Orlov <denis.orlov@intel.com>
2020-08-18 16:11:10 +0300

    Merge changes from 2020.4.0.1 (#1809)
    * [GNA] Propagate QoS timeout to the calling app (#1188)
    
    * [GNA] Support timeout value set in Wait (#1499)
    
    * [GNA] stored request id for completed sync infer request in order to get status later using wait() (#1458)
    
    * stored request id for completed async infer request in order to get it's status later
    
    * preserved status not started for multiple sequential calls to wait()
    
    Co-authored-by: Denis Orlov <denis.orlov@intel.com>
    
    * [GNA] Fix callbacks (#1607)
    
    Co-authored-by: Eugene Smirnov <eugene.smirnov@intel.com>

--- inference-engine/src/gna_plugin/gna_plugin.hpp
+++ inference-engine/src/gna_plugin/gna_plugin.hpp
@@ -98,7 +98,7 @@ class GNAPlugin : public InferenceEngine::IInferencePlugin {
 
     void LoadNetwork(InferenceEngine::ICNNNetwork &network);
 
-    void Infer(const InferenceEngine::BlobMap &input, InferenceEngine::BlobMap &result);
+    bool Infer(const InferenceEngine::BlobMap &input, InferenceEngine::BlobMap &result);
     void GetPerformanceCounts(std::map<std::string, InferenceEngine::InferenceEngineProfileInfo> &perfMap);
     void AddExtension(InferenceEngine::IExtensionPtr extension) override;
 
@@ -109,7 +109,7 @@ class GNAPlugin : public InferenceEngine::IInferencePlugin {
     InferenceEngine::ExecutableNetwork LoadNetwork(const InferenceEngine::ICNNNetwork &network,
                                   const std::map<std::string, std::string> &config_map,
                                   InferenceEngine::RemoteContext::Ptr context) override { THROW_GNA_EXCEPTION << "Not implemented"; }
-    void Infer(const InferenceEngine::Blob &input, InferenceEngine::Blob &result);
+    bool Infer(const InferenceEngine::Blob &input, InferenceEngine::Blob &result);
     void SetCore(InferenceEngine::ICore*) noexcept override {}
     InferenceEngine::ICore* GetCore() const noexcept override {return nullptr;}
     void Reset();
@@ -117,7 +117,8 @@ class GNAPlugin : public InferenceEngine::IInferencePlugin {
                       const std::map<std::string, std::string>& config,
                       InferenceEngine::QueryNetworkResult &res) const override;
     uint32_t QueueInference(const InferenceEngine::BlobMap &input, InferenceEngine::BlobMap &result);
-    void Wait(uint32_t idx = 0);
+    bool Wait(uint32_t idx);
+    bool WaitFor(uint32_t idx, int64_t millisTimeout);
 
     InferenceEngine::Parameter GetConfig(const std::string& name,
                                          const std::map<std::string, InferenceEngine::Parameter> & options) const override;
