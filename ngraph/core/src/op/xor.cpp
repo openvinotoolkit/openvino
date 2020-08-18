e9ff6c4db6b6247627c99009f42e67c3e5c3efad
Ilya Lavrenov <ilya.lavrenov@intel.com>
2020-08-18 16:50:37 +0300

    Ngraph: hide itt.hpp to src (#1834)
    * Use proper include for log.hpp
    
    * Hide itt.hpp to src folder
    
    * Removed CodeWriter

--- ngraph/core/src/op/xor.cpp
+++ ngraph/core/src/op/xor.cpp
@@ -15,7 +15,7 @@
 //*****************************************************************************
 
 #include "ngraph/op/xor.hpp"
-#include "ngraph/itt.hpp"
+#include "itt.hpp"
 #include "ngraph/runtime/host_tensor.hpp"
 #include "ngraph/runtime/reference/xor.hpp"
 
