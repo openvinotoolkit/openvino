af6f99525afcb207da909426118ab6db25f82bcb
Gleb Kazantaev <gleb.kazantaev@intel.com>
2020-08-18 11:10:24 +0300

    ConvertPrecision for element::boolean (#1772)
    * Added bool to u8 conversion
    
    * Added opset1::ShapeOf handler
    
    * Added ReduceLogicalAnd/Or support in ConvertPrecision pass
    
    * Moved static map inside function; Updated callbacks
    
    * Removed header
    
    * Fixed tyle relaxed for cases when the same output consumes by multiple inputs in the same operation; added tests; fixed input types setting for already created type relaxed operations

--- ngraph/core/src/op/shape_of.cpp
+++ ngraph/core/src/op/shape_of.cpp
@@ -170,7 +170,7 @@ bool op::v3::ShapeOf::constant_fold(OutputVector& output_values, const OutputVec
 }
 
 // op::v0::ShapeOf
-constexpr NodeTypeInfo op::v0::ShapeOf::type_info;
+NGRAPH_RTTI_DEFINITION(op::v0::ShapeOf, "ShapeOf", 0);
 
 op::v0::ShapeOf::ShapeOf(const Output<Node>& arg)
     : Op({arg})
