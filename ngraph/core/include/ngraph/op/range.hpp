0f664cbad0717130436df5e4797b6f4ddab91d05
Maxim Vafin <maxim.vafin@intel.com>
2020-08-19 07:58:41 +0300

    Add Range-4 support (#1552)
    * Add Range-4 support
    
    * Add type infer and IR Reader extender
    
    * Implement ngraph Range-4
    
    * Implement evaluate for Range-4
    
    * Remove not needed types
    
    * Resolve review feedback
    
    * Add RTTI defines

--- ngraph/core/include/ngraph/op/range.hpp
+++ ngraph/core/include/ngraph/op/range.hpp
@@ -23,6 +23,42 @@ namespace ngraph
 {
     namespace op
     {
+        namespace v4
+        {
+            /// \brief Range operation, analogous to `arange()` in Numpy.
+            class NGRAPH_API Range : public Op
+            {
+            public:
+                NGRAPH_RTTI_DECLARATION;
+                /// \brief Constructs an unitialized range operation.
+                Range() = default;
+
+                /// \brief Constructs a range operation.
+                ///
+                /// \param start The tensor producing the start value. Must be a scalar of integer
+                ///              element type, and same element type as `stop` and `step`.
+                /// \param stop The tensor producing the stop value. Must be a scalar of integer
+                ///             element type, and same element type as `start` and `step`.
+                /// \param step The tensor producing the step value. Must be a scalar of integer
+                ///             element type, and same element type as `start` and `stop`.
+                /// \param output_type The type of the output.
+                Range(const Output<Node>& start,
+                      const Output<Node>& stop,
+                      const Output<Node>& step,
+                      element::Type output_type);
+
+                bool visit_attributes(AttributeVisitor& visitor) override;
+                void validate_and_infer_types() override;
+
+                virtual std::shared_ptr<Node>
+                    clone_with_new_inputs(const OutputVector& new_args) const override;
+                bool evaluate(const HostTensorVector& outputs,
+                              const HostTensorVector& inputs) const override;
+
+            private:
+                element::Type m_output_type;
+            };
+        }
         namespace v0
         {
             /// \brief Range operation, analogous to `range()` in Python.
