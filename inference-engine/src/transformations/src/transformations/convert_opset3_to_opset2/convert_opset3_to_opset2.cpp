4925485c14e7099a9b0c30d5654eb278b0acc6cd
Anton Chetverikov <Anton.Chetverikov@intel.com>
2020-08-19 13:23:41 +0300

    ExtractImagePatches MO implementation and nGraph transformation (#739)

--- inference-engine/src/transformations/src/transformations/convert_opset3_to_opset2/convert_opset3_to_opset2.cpp
+++ inference-engine/src/transformations/src/transformations/convert_opset3_to_opset2/convert_opset3_to_opset2.cpp
@@ -9,6 +9,7 @@
 #include "transformations/convert_opset3_to_opset2/convert_shapeof3.hpp"
 #include "transformations/convert_opset3_to_opset2/convert_shuffle_channels3.hpp"
 #include "transformations/convert_opset3_to_opset2/convert_topk3.hpp"
+#include "transformations/convert_extract_image_patches_to_reorg_yolo.hpp"
 #include "transformations/itt.hpp"
 
 #include <memory>
@@ -26,6 +27,7 @@ bool ngraph::pass::ConvertOpSet3ToOpSet2::run_on_function(std::shared_ptr<ngraph
     manager.register_pass<ngraph::pass::ConvertShapeOf3>();
     manager.register_pass<ngraph::pass::ConvertShuffleChannels3>();
     manager.register_pass<ngraph::pass::ConvertTopK3>();
+    manager.register_pass<ngraph::pass::ConvertExtractImagePatchesToReorgYolo>();
 
     manager.set_callback(m_transformation_callback);
     manager.run_passes(f);
