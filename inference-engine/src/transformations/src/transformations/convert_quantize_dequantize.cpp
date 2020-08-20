551ee5d908f56798ba082ce989d15cf7e8dc6fb1
Mateusz Tabaka <mateusz.tabaka@intel.com>
2020-08-20 18:25:22 +0200

    Changes after review:
    - description for transformation
    - remove NGRAPH_CHECK and move some checks from callback to predicates in pattern
    - check if out_low/high are broadcastable for FQ's first input
    - fix params to copy_runtime_info

--- inference-engine/src/transformations/src/transformations/convert_quantize_dequantize.cpp
+++ inference-engine/src/transformations/src/transformations/convert_quantize_dequantize.cpp
@@ -12,6 +12,48 @@
 #include <ngraph/pattern/op/wrap_type.hpp>
 
 
+// ConvertQuantizeDequantize converts Quantize/Dequantize pair to a single FakeQuantize.
+// Since Quantize is decomposed to FakeQuantize and Dequantize is decomposed to Subtract->Multiply,
+// the full pattern to match is presented on the left hand side of the graph below.
+// On the right hand side is the graph after transformation.
+// Currently transformation supports only i8 and u8 quantized data type.
+// That implies 'levels' attribute to be 256, as well as (output_low, output_high) be (-128, 127) or (0, 255) (depends on sign of the quantized data type).
+// Another limitation is that 'zero_point' and 'scale' have to be broadcastable to the output of FakeQuantize.
+//
+//
+//                                   |  |  |  |  |
+//                                   |  |  |  |  |
+//                                   v  v  v  v  v
+//                                  +------------+
+//                                  |FakeQuantize|
+//                                  +------------+
+//                                        |
+//                                        v
+//                              +---------------------+
+//                              |      Convert        |
+//                              |(e.g. from f32 to u8)|
+//                              +---------+-----------+                            |  |  |  |  |
+//                                        |                                        |  |  |  |  |
+//                                        v                                        v  v  v  v  v
+//                              +---------------------+                           +------------+
+//                              |      Convert        |            ====>          |FakeQuantize|
+//                              |  (from u8 to f32)   |                           +------------+
+//                              +---------+-----------+                                 |
+//                                        |                                             v
+//                                        v
+//                  +----------+    +------------+
+//                  |zero point|--->|  Subtract  |
+//                  +----------+    +-----+------+
+//                                        |
+//                                        v
+//                   +---------+    +------------+
+//                   |  scale  |--->|  Multiply  |
+//                   +---------+    +-----+------+
+//                                        |
+//                                        v
+//
+
+
 ngraph::pass::ConvertQuantizeDequantize::ConvertQuantizeDequantize() {
     auto data_pattern = ngraph::pattern::any_input();
     auto input_low_pattern = ngraph::pattern::any_input();
@@ -21,8 +63,15 @@ ngraph::pass::ConvertQuantizeDequantize::ConvertQuantizeDequantize() {
     auto fq_pattern = ngraph::pattern::wrap_type<opset1::FakeQuantize>({data_pattern, input_low_pattern,
                                                                        input_high_pattern, output_low_pattern,
                                                                        output_high_pattern});
-    auto convert1_pattern = ngraph::pattern::wrap_type<opset1::Convert>({fq_pattern});
-    auto convert2_pattern = ngraph::pattern::wrap_type<opset1::Convert>({convert1_pattern});
+    auto convert1_pattern = ngraph::pattern::wrap_type<opset1::Convert>({fq_pattern},
+                                                                        [] (const Output<Node>& output) -> bool {
+                                                                            const auto& type = output.get_element_type();
+                                                                            return type == element::i8 || type == element::u8;
+                                                                        });
+    auto convert2_pattern = ngraph::pattern::wrap_type<opset1::Convert>({convert1_pattern},
+                                                                        [] (const Output<Node>& output) -> bool {
+                                                                            return output.get_element_type() == element::f32;
+                                                                        });
     auto zero_point_pattern = ngraph::pattern::any_input();
     auto sub_pattern = ngraph::pattern::wrap_type<opset1::Subtract>({convert2_pattern, zero_point_pattern}, pattern::consumers_count(1));
     auto scale_pattern = ngraph::pattern::any_input();
@@ -34,24 +83,27 @@ ngraph::pass::ConvertQuantizeDequantize::ConvertQuantizeDequantize() {
             return false;
 
         auto pattern_map = m.get_pattern_map();
-
-        auto convert2 = pattern_map[convert2_pattern];
-        NGRAPH_CHECK(convert2->get_element_type() == element::f32);
-
+        auto data = pattern_map[data_pattern];
+        auto input_low = pattern_map[input_low_pattern];
+        auto input_high = pattern_map[input_high_pattern];
+        auto output_low = std::dynamic_pointer_cast<opset1::Constant>(pattern_map[output_low_pattern]);
+        auto output_high = std::dynamic_pointer_cast<opset1::Constant>(pattern_map[output_high_pattern]);
         auto fq = std::dynamic_pointer_cast<opset1::FakeQuantize>(pattern_map[fq_pattern]);
-        NGRAPH_CHECK(fq != nullptr);
-        size_t levels = fq->get_levels();
-        NGRAPH_CHECK(levels == 256);
-
-        auto output_low = pattern_map[output_low_pattern];
-        auto output_high = pattern_map[output_high_pattern];
-        auto output_low_const = std::dynamic_pointer_cast<opset1::Constant>(output_low);
-        auto output_high_const = std::dynamic_pointer_cast<opset1::Constant>(output_high);
-        auto out_low_val = output_low_const->cast_vector<float>();
-        auto out_high_val = output_high_const->cast_vector<float>();
-        NGRAPH_CHECK(out_low_val.size() == 1 && out_high_val.size() == 1);
-
+        auto zero_point = pattern_map[zero_point_pattern];
+        auto scale = pattern_map[scale_pattern];
         auto convert1 = pattern_map[convert1_pattern];
+        auto convert2 = pattern_map[convert2_pattern];
+        auto sub = pattern_map[sub_pattern];
+
+        size_t levels = fq->get_levels();
+        if (levels != 256)
+            return false;
+
+        auto out_low_val = output_low->cast_vector<float>();
+        auto out_high_val = output_high->cast_vector<float>();
+        if (out_low_val.size() != 1 || out_high_val.size() != 1)
+            return false;
+
         const auto& type = convert1->get_element_type();
         switch (type) {
         case element::Type_t::i8:
@@ -63,22 +115,28 @@ ngraph::pass::ConvertQuantizeDequantize::ConvertQuantizeDequantize() {
                 return false;
             break;
         default:
-            NGRAPH_CHECK(false, "not supported data type" + type.get_type_name());
+            return false;
         }
 
-        auto zero_point = pattern_map[zero_point_pattern];
-        auto scale = pattern_map[scale_pattern];
         auto new_out_low = std::make_shared<ngraph::opset1::Multiply>(
                 std::make_shared<ngraph::opset1::Subtract>(output_low, zero_point), scale);
         auto new_out_high = std::make_shared<ngraph::opset1::Multiply>(
                 std::make_shared<ngraph::opset1::Subtract>(output_high, zero_point), scale);
-        auto data = pattern_map[data_pattern];
-        auto input_low = pattern_map[input_low_pattern];
-        auto input_high = pattern_map[input_high_pattern];
+
+        // check if new_out_low/high shapes are broadcastable to FQ's input
+        auto data_shape = data->get_output_partial_shape(0);
+        auto out_low_shape = new_out_low->get_output_partial_shape(0);
+        if (out_low_shape.rank().get_length() > data_shape.rank().get_length())
+            return false;
+        auto out_high_shape = new_out_high->get_output_partial_shape(0);
+        if (out_high_shape.rank().get_length() > data_shape.rank().get_length())
+            return false;
+
         auto fake_q = std::make_shared<ngraph::opset1::FakeQuantize>(data, input_low, input_high, new_out_low, new_out_high, levels);
         fake_q->set_friendly_name(mul->get_friendly_name());
 
-        copy_runtime_info(mul, {data, input_low, input_high, new_out_low, new_out_high, fake_q});
+        copy_runtime_info({data, input_low, input_high, output_low, output_high, fq, zero_point, scale, convert1, convert2, sub, mul},
+                          {data, input_low, input_high, new_out_low, new_out_high, fake_q});
         replace_node(mul, fake_q);
 
         return true;
--- inference-engine/tests/functional/inference_engine/transformations/convert_quantize_dequantize.cpp
+++ inference-engine/tests/functional/inference_engine/transformations/convert_quantize_dequantize.cpp
@@ -23,46 +23,183 @@ using namespace testing;
 using namespace ngraph;
 
 
-TEST(TransformationTests, ConvertQuantizeDequantize) {
-    size_t levels = 256;
-    std::shared_ptr<ngraph::Function> f(nullptr), f_ref(nullptr);
+template <typename T>
+std::shared_ptr<Function> create_q_dq_function(const Shape& data_shape, float in_low, float in_high, float out_low, float out_high,
+                                               const Shape& zero_point_shape, std::vector<T> zero_point_values,
+                                               const Shape& scale_shape, std::vector<float> scale_values, size_t levels) {
+    auto data = std::make_shared<opset1::Parameter>(element::f32, data_shape);
+    auto input_low = opset1::Constant::create(element::f32, Shape{}, {in_low});
+    auto input_high = opset1::Constant::create(element::f32, Shape{}, {in_high});
+    auto output_low = opset1::Constant::create(element::f32, Shape{}, {out_low});
+    auto output_high = opset1::Constant::create(element::f32, Shape{}, {out_high});
+    auto fq = std::make_shared<opset1::FakeQuantize>(data, input_low,
+                                                     input_high, output_low,
+                                                     output_high, levels);
+    auto convert1 = std::make_shared<opset1::Convert>(fq, element::from<T>());
+    auto convert2 = std::make_shared<opset1::Convert>(convert1, element::f32);
+    auto zero_point = std::make_shared<opset1::Convert>(opset1::Constant::create(element::from<T>(), zero_point_shape, zero_point_values), element::f32);
+    auto sub = std::make_shared<opset1::Subtract>(convert2, zero_point);
+    auto scale = opset1::Constant::create(element::f32, scale_shape, scale_values);
+    auto mul = std::make_shared<opset1::Multiply>(sub, scale);
+
+    return std::make_shared<Function>(NodeVector{mul}, ParameterVector{data});
+}
+
+template <typename T>
+void positive_test(const Shape& data_shape, float in_low, float in_high, float out_low, float out_high,
+                   const Shape& zero_point_shape, std::vector<T> zero_point_values,
+                   const Shape& scale_shape, std::vector<float> scale_values, size_t levels) {
+    std::shared_ptr<Function> f(nullptr), f_ref(nullptr);
     {
-        auto data = ngraph::opset1::Constant::create(ngraph::element::f32, ngraph::Shape{3, 1, 2}, {0, 1, 2, 3, 4, 5});
-        auto input_low = ngraph::opset1::Constant::create(ngraph::element::f32, ngraph::Shape{}, {0});
-        auto input_high = ngraph::opset1::Constant::create(ngraph::element::f32, ngraph::Shape{}, {5});
-        auto output_low = ngraph::opset1::Constant::create(ngraph::element::f32, ngraph::Shape{}, {-128});
-        auto output_high = ngraph::opset1::Constant::create(ngraph::element::f32, ngraph::Shape{}, {127});
-        auto fq = std::make_shared<opset1::FakeQuantize>(data, input_low,
-                                                         input_high, output_low,
-                                                         output_high, levels);
-        auto convert1 = std::make_shared<opset1::Convert>(fq, element::i8);
-        auto convert2 = std::make_shared<opset1::Convert>(convert1, element::f32);
-        auto zero_point = ngraph::opset1::Constant::create(ngraph::element::f32, ngraph::Shape{}, {2});
-        auto sub = std::make_shared<opset1::Subtract>(convert2, zero_point);
-        auto scale = ngraph::opset1::Constant::create(ngraph::element::f32, ngraph::Shape{}, {3});
-        auto mul = std::make_shared<opset1::Multiply>(sub, scale);
-
-        f = std::make_shared<ngraph::Function>(ngraph::NodeVector{mul}, ngraph::ParameterVector{});
-
-        ngraph::pass::Manager m;
-        m.register_pass<ngraph::pass::InitNodeInfo>();
-        m.register_pass<ngraph::pass::ConvertQuantizeDequantize>();
-        m.register_pass<ngraph::pass::ConstantFolding>();
+        f = create_q_dq_function(data_shape, in_low, in_high, out_low, out_high,
+                                 zero_point_shape, zero_point_values, scale_shape, scale_values, levels);
+        pass::Manager m;
+        m.register_pass<pass::InitNodeInfo>();
+        m.register_pass<pass::ConvertQuantizeDequantize>();
+        m.register_pass<pass::ConstantFolding>();
         m.run_passes(f);
+        ASSERT_NO_THROW(check_rt_info(f));
     }
 
     {
-        auto data = ngraph::opset1::Constant::create(ngraph::element::f32, ngraph::Shape{3, 1, 2}, {0, 1, 2, 3, 4, 5});
-        auto input_low = ngraph::opset1::Constant::create(ngraph::element::f32, ngraph::Shape{}, {0});
-        auto input_high = ngraph::opset1::Constant::create(ngraph::element::f32, ngraph::Shape{}, {5});
-        auto output_low = ngraph::opset1::Constant::create(ngraph::element::f32, ngraph::Shape{}, {(-128 - 2) * 3});
-        auto output_high = ngraph::opset1::Constant::create(ngraph::element::f32, ngraph::Shape{}, {(127 - 2) * 3});
+        auto data = std::make_shared<opset1::Parameter>(element::f32, data_shape);
+        auto input_low = opset1::Constant::create(element::f32, Shape{}, {in_low});
+        auto input_high = opset1::Constant::create(element::f32, Shape{}, {in_high});
+        auto output_low = opset1::Constant::create(element::f32, Shape{}, {(out_low - zero_point_values[0]) * scale_values[0]});
+        auto output_high = opset1::Constant::create(element::f32, Shape{}, {(out_high - zero_point_values[0]) * scale_values[0]});
         auto fq = std::make_shared<opset1::FakeQuantize>(data, input_low,
                                                          input_high, output_low,
                                                          output_high, levels);
-        f_ref = std::make_shared<ngraph::Function>(ngraph::NodeVector{fq}, ngraph::ParameterVector{});
+        f_ref = std::make_shared<Function>(NodeVector{fq}, ParameterVector{data});
     }
 
     auto res = compare_functions(f, f_ref);
     ASSERT_TRUE(res.first) << res.second;
+}
+
+TEST(TransformationTests, ConvertQuantizeDequantizeINT8) {
+    std::shared_ptr<Function> f(nullptr), f_ref(nullptr);
+    Shape data_shape{3, 1, 2};
+    float in_low = 0;
+    float in_high = 5;
+    float out_low = -128;
+    float out_high = 127;
+    Shape zero_point_shape{};
+    std::vector<int8_t> zero_point_values{2};
+    Shape scale_shape{};
+    std::vector<float> scale_values{3};
+    size_t levels = 256;
+
+    positive_test(data_shape, in_low, in_high, out_low, out_high,
+                  zero_point_shape, zero_point_values, scale_shape, scale_values, levels);
+}
+
+TEST(TransformationTests, ConvertQuantizeDequantizeUINT8) {
+    std::shared_ptr<Function> f(nullptr), f_ref(nullptr);
+    Shape data_shape{3, 1, 2};
+    float in_low = 0;
+    float in_high = 5;
+    float out_low = 0;
+    float out_high = 255;
+    Shape zero_point_shape{};
+    std::vector<uint8_t> zero_point_values{2};
+    Shape scale_shape{};
+    std::vector<float> scale_values{3};
+    size_t levels = 256;
+
+    positive_test(data_shape, in_low, in_high, out_low, out_high,
+                  zero_point_shape, zero_point_values, scale_shape, scale_values, levels);
+}
+
+template <typename T>
+void negative_test(const Shape& data_shape, float in_low, float in_high, float out_low, float out_high,
+                   const Shape& zero_point_shape, std::vector<T> zero_point_values,
+                   const Shape& scale_shape, std::vector<float> scale_values, size_t levels) {
+    std::shared_ptr<Function> f(nullptr), f_ref(nullptr);
+    {
+        f = create_q_dq_function(data_shape, in_low, in_high, out_low, out_high,
+                                 zero_point_shape, zero_point_values, scale_shape, scale_values, levels);
+        pass::Manager m;
+        m.register_pass<pass::InitNodeInfo>();
+        m.register_pass<pass::ConvertQuantizeDequantize>();
+        m.run_passes(f);
+        ASSERT_NO_THROW(check_rt_info(f));
+    }
+
+    {
+        // negative test so the transformation does not fire and reference is the same graph as original
+        f_ref = create_q_dq_function(data_shape, in_low, in_high, out_low, out_high,
+                                 zero_point_shape, zero_point_values, scale_shape, scale_values, levels);
+    }
+    auto res = compare_functions(f, f_ref);
+    ASSERT_TRUE(res.first) << res.second;
+}
+
+TEST(TransformationTests, ConvertQuantizeDequantizeZeroPointNotBroadcastable) {
+    std::shared_ptr<Function> f(nullptr), f_ref(nullptr);
+    Shape data_shape{3, 1, 2};
+    float in_low = 0;
+    float in_high = 5;
+    float out_low = -128;
+    float out_high = 127;
+    Shape zero_point_shape{1, 1, 1, 1};
+    std::vector<int8_t> zero_point_values{2};
+    Shape scale_shape{1};
+    std::vector<float> scale_values{3};
+    size_t levels = 256;
+
+    negative_test(data_shape, in_low, in_high, out_low, out_high,
+                  zero_point_shape, zero_point_values, scale_shape, scale_values, levels);
+}
+
+TEST(TransformationTests, ConvertQuantizeDequantizeScaleNotBroadcastable) {
+    std::shared_ptr<Function> f(nullptr), f_ref(nullptr);
+    Shape data_shape{3, 1, 2};
+    float in_low = 0;
+    float in_high = 5;
+    float out_low = -128;
+    float out_high = 127;
+    Shape zero_point_shape{};
+    std::vector<int8_t> zero_point_values{2};
+    Shape scale_shape{1, 1, 1, 1};
+    std::vector<float> scale_values{3};
+    size_t levels = 256;
+
+    negative_test(data_shape, in_low, in_high, out_low, out_high,
+                  zero_point_shape, zero_point_values, scale_shape, scale_values, levels);
+}
+
+TEST(TransformationTests, ConvertQuantizeDequantizeInvalidLevels) {
+    std::shared_ptr<Function> f(nullptr), f_ref(nullptr);
+    Shape data_shape{3, 1, 2};
+    float in_low = 0;
+    float in_high = 5;
+    float out_low = -128;
+    float out_high = 127;
+    Shape zero_point_shape{};
+    std::vector<int8_t> zero_point_values{2};
+    Shape scale_shape{};
+    std::vector<float> scale_values{3};
+    size_t levels = 127;
+
+    negative_test(data_shape, in_low, in_high, out_low, out_high,
+                  zero_point_shape, zero_point_values, scale_shape, scale_values, levels);
+}
+
+TEST(TransformationTests, ConvertQuantizeDequantizeInvalidOutLowOutHigh) {
+    std::shared_ptr<Function> f(nullptr), f_ref(nullptr);
+    Shape data_shape{3, 1, 2};
+    float in_low = 0;
+    float in_high = 5;
+    // (-128, 127) are invalid for uin8_t data type
+    float out_low = -128;
+    float out_high = 127;
+    Shape zero_point_shape{};
+    std::vector<uint8_t> zero_point_values{2};
+    Shape scale_shape{};
+    std::vector<float> scale_values{3};
+    size_t levels = 256;
+
+    negative_test(data_shape, in_low, in_high, out_low, out_high,
+                  zero_point_shape, zero_point_values, scale_shape, scale_values, levels);
 }
