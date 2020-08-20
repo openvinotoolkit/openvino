d388d395f39567d3c05655f8ef51db2891cb595d
Ilya Lavrenov <ilya.lavrenov@intel.com>
2020-08-20 18:34:19 +0300

    Reuse make_executable_network in ImportNetwork (#1869)
    * Reuse make_executable_network for ImportNetwork
    
    * make_executable_network returns ExecutableNetwork cpp wrapper

--- inference-engine/src/hetero_plugin/hetero_plugin.cpp
+++ inference-engine/src/hetero_plugin/hetero_plugin.cpp
@@ -86,12 +86,8 @@ ExecutableNetwork Engine::ImportNetworkImpl(std::istream& heteroModel, const Con
         THROW_IE_EXCEPTION << "Please, work with HETERO device via InferencEngine::Core object";
     }
 
-    IExecutableNetwork::Ptr executableNetwork;
-    executableNetwork.reset(new ExecutableNetworkBase<ExecutableNetworkInternal>(
-                                std::make_shared<HeteroExecutableNetwork>(heteroModel, mergeConfigs(_config, config), this)),
-                            [](InferenceEngine::details::IRelease *p) {p->Release();});
-
-    return ExecutableNetwork{executableNetwork};
+    return make_executable_network(std::make_shared<HeteroExecutableNetwork>(heteroModel,
+        mergeConfigs(_config, config), this));
 }
 
 Engine::Configs Engine::GetSupportedConfig(const Engine::Configs& config, const std::string & deviceName) const {
