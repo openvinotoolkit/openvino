cc88d85e8b51a5b61807a3863cb8e95be53b853e
Chetverikov Anton <anton.chetverikov@intel.com>
2020-08-21 21:33:31 +0300

    Add negative test for transformation

--- inference-engine/tests/functional/inference_engine/transformations/softplus_fusion_test.cpp
+++ inference-engine/tests/functional/inference_engine/transformations/softplus_fusion_test.cpp
@@ -75,3 +75,35 @@ TEST(TransformationTests, SoftPlusFusingDynamic) {
     auto res = compare_functions(f, f_ref);
     ASSERT_TRUE(res.first) << res.second;
 }
+
+TEST(TransformationTests, SoftPlusFusingNegative) {
+    std::shared_ptr<ngraph::Function> f(nullptr), f_ref(nullptr);
+    {
+        auto input0 = std::make_shared<ngraph::opset4::Parameter>(ngraph::element::f32, ngraph::PartialShape::dynamic(1));
+        auto exp = std::make_shared<ngraph::opset4::Exp>(input0);
+        auto input_const = ngraph::opset4::Constant::create(ngraph::element::f32, ngraph::Shape{1}, {-1.0});
+        auto add = std::make_shared<ngraph::opset4::Add>(exp, input_const);
+        auto log = std::make_shared<ngraph::opset4::Log>(add);
+
+        f = std::make_shared<ngraph::Function>(ngraph::NodeVector{log}, ngraph::ParameterVector{input0});
+
+        ngraph::pass::Manager manager;
+        manager.register_pass<ngraph::pass::InitNodeInfo>();
+        manager.register_pass<ngraph::pass::SoftPlusFusion>();
+        manager.run_passes(f);
+        ASSERT_NO_THROW(check_rt_info(f));
+    }
+
+    {
+        auto input0 = std::make_shared<ngraph::opset4::Parameter>(ngraph::element::f32, ngraph::PartialShape::dynamic(1));
+        auto exp = std::make_shared<ngraph::opset4::Exp>(input0);
+        auto input_const = ngraph::opset4::Constant::create(ngraph::element::f32, ngraph::Shape{1}, {-1.0});
+        auto add = std::make_shared<ngraph::opset4::Add>(exp, input_const);
+        auto log = std::make_shared<ngraph::opset4::Log>(add);
+
+        f_ref = std::make_shared<ngraph::Function>(ngraph::NodeVector{log}, ngraph::ParameterVector{input0});
+    }
+
+    auto res = compare_functions(f, f_ref);
+    ASSERT_TRUE(res.first) << res.second;
+}
