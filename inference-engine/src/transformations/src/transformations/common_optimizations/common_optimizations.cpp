b6d0a0cdee995d429c030584438f01cfbe6429eb
Ivan Tikhonov <ivan.tikhonov@intel.com>
2020-08-24 14:31:32 +0300

    Partial revert a commit with reference implementations of PriorBox(Clustered) ops (#1878)
    * partial revert a commit with reference implementations of PriorBox(Clustered), disable references for this ops
    
    * ngraph codestyle
    
    * disable const folding unit tests for PriorBox(Clustered)
    
    * fix arm build
    
    * fix unit test
    
    * Revert "fix unit test"
    
    This reverts commit 1fe59e55d620c430e81c52edd3fc46f2b5b1a9b9.

--- inference-engine/src/transformations/src/transformations/common_optimizations/common_optimizations.cpp
+++ inference-engine/src/transformations/src/transformations/common_optimizations/common_optimizations.cpp
@@ -7,6 +7,7 @@
 #include "transformations/common_optimizations/algebraic_simplification.hpp"
 #include "transformations/common_optimizations/nop_elimination.hpp"
 #include "transformations/common_optimizations/common_optimizations.hpp"
+#include "transformations/convert_opset1_to_legacy/convert_prior_to_ie_prior.hpp"
 #include "transformations/depth_to_space_fusion.hpp"
 #include "transformations/optimize_strided_slice.hpp"
 #include "transformations/convert_scatter_elements_to_scatter.hpp"
@@ -28,6 +29,8 @@ bool ngraph::pass::CommonOptimizations::run_on_function(std::shared_ptr<ngraph::
 
     // This pass must be called first in pipeline
     manager.register_pass<ngraph::pass::InitNodeInfo>();
+    manager.register_pass<ngraph::pass::ConvertPriorBox>();  // WA: ConvertPriorBox must be executed before CF
+    manager.register_pass<ngraph::pass::ConstantFolding>();
     manager.register_pass<ngraph::pass::RemoveFilteringBoxesBySize>(); // Resolves dynamism (replaces NonZero), CF needed
     manager.register_pass<ngraph::pass::ConstantFolding>();
     manager.register_pass<ngraph::pass::StridedSliceOptimization>(); // depends on CF
