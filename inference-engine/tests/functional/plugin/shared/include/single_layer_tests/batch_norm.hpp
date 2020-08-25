4f8f3ab12f219dae54a71602015d8c7e716ad108
Liubov Batanina <liubov.batanina@intel.com>
2020-08-25 16:28:05 +0300

    Added BatchNormInference tests (#1927)

---
+++ inference-engine/tests/functional/plugin/shared/include/single_layer_tests/batch_norm.hpp
@@ -0,0 +1,28 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include "functional_test_utils/layer_test_utils.hpp"
+#include "ngraph_functions/builders.hpp"
+
+typedef std::tuple<
+        double,                        // epsilon
+        InferenceEngine::Precision,    // Net precision
+        InferenceEngine::SizeVector,   // Input shapes
+        LayerTestsUtils::TargetDevice  // Target device name
+> BatchNormLayerTestParams;
+
+namespace LayerTestsDefinitions {
+
+class BatchNormLayerTest : public testing::WithParamInterface<BatchNormLayerTestParams>,
+                           public LayerTestsUtils::LayerTestsCommon {
+public:
+    static std::string getTestCaseName(const testing::TestParamInfo<BatchNormLayerTestParams>& obj);
+
+protected:
+    void SetUp() override;
+};
+
+}  // namespace LayerTestsDefinitions
