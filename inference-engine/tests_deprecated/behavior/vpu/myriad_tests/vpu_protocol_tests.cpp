98b16bdbcd0198017c9b66d21bd049ff7f705237
Chekhonin Andrey <andrey.chekhonin@intel.com>
2020-08-26 00:54:32 +0300

    [VPU] Added test - no warning when patch firmware

--- inference-engine/tests_deprecated/behavior/vpu/myriad_tests/vpu_protocol_tests.cpp
+++ inference-engine/tests_deprecated/behavior/vpu/myriad_tests/vpu_protocol_tests.cpp
@@ -26,6 +26,30 @@ TEST_P(MyriadProtocolTests, CanInferenceWithProtocol) {
     ASSERT_EQ(statusCode, StatusCode::OK) << resp.msg;
 }
 
+
+
+TEST_P(MyriadProtocolTests, NoWarningIfPatchFirmwareForUSBDevice) {
+    if (protocol != NC_USB) {
+        GTEST_SKIP();
+    }
+
+    char buff[8192] = {};
+    setbuf(stdout, buff);
+
+    auto network = ie->ReadNetwork(FuncTestUtils::TestModel::convReluNormPoolFcModelFP16.model_xml_str,
+                                   FuncTestUtils::TestModel::convReluNormPoolFcModelFP16.weights_blob);
+
+    std::map<std::string, std::string> config = {{CONFIG_KEY(LOG_LEVEL), CONFIG_VALUE(LOG_WARNING)}};
+
+    InferenceEngine::IExecutableNetwork::Ptr exe_network =
+            ie->LoadNetwork(network, "MYRIAD", config);
+    setbuf(stdout, NULL);
+    std::string warningMessege("Fail to patch");
+    std::string content(buff);
+    auto foundWarning = content.find(warningMessege);
+    ASSERT_TRUE(foundWarning == std::string::npos);
+}
+
 INSTANTIATE_TEST_CASE_P(smoke_VPUConfigProtocolTests,
                         MyriadProtocolTests,
                         ::testing::ValuesIn(myriadProtocols),
