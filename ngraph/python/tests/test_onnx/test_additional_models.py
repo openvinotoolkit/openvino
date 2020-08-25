f213ad312391e6abf0a3c41b28b7e8ad83024374
Mateusz Tabaka <mateusz.tabaka@intel.com>
2020-08-25 12:18:26 +0200

    add a comment

--- ngraph/python/tests/test_onnx/test_additional_models.py
+++ ngraph/python/tests/test_onnx/test_additional_models.py
@@ -58,6 +58,7 @@ for path in Path(MODELS_ROOT_DIR).rglob("*.onnx"):
         model = {"model_name": path, "model_file": file, "dir": mdir}
         basedir = os.path.basename(mdir)
         if basedir in tolerance_map:
+            # updated model looks now: {"model_name": path, "model_file": file, "dir": mdir, "atol": ..., "rtol": ...}
             model.update(tolerance_map[basedir])
         zoo_models.append(model)
 
