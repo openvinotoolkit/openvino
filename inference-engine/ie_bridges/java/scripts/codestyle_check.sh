c03083318bc7942f6ce59da25e10e64b46b8aefe
Anna Likholat <anna.likholat@intel.com>
2020-08-26 22:55:01 +0300

    fixes

--- inference-engine/ie_bridges/java/scripts/codestyle_check.sh
+++
@@ -1,5 +0,0 @@
-cd ~/openvino/bin/intel64/Release/lib/
-wget -nc https://github.com/google/google-java-format/releases/download/google-java-format-1.9/google-java-format-1.9-all-deps.jar
-
-java -jar google-java-format-1.9-all-deps.jar --set-exit-if-changed |
-     -r ~/openvino/inference-engine/ie_bridges/java/tests/BlobTests.java ~/openvino/inference-engine/ie_bridges/java/tests/BlobTests.java
