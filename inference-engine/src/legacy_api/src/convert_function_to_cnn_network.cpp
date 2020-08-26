43ccbc6c8c61cd2e5a7e3acc2e11845dfb6d683b
VGavrilov <vladimir.gavrilov@intel.com>
2020-08-26 13:09:52 +0300

    Added conversion from NGRAPH to CNNLayer.

--- inference-engine/src/legacy_api/src/convert_function_to_cnn_network.cpp
+++ inference-engine/src/legacy_api/src/convert_function_to_cnn_network.cpp
@@ -676,6 +676,7 @@ void convertFunctionToICNNNetwork(const std::shared_ptr<const ::ngraph::Function
                 std::make_shared<Builder::NodeConverter<::ngraph::op::HardSigmoid_IE>>(),
                 std::make_shared<Builder::NodeConverter<::ngraph::op::v1::LogicalNot>>(),
                 std::make_shared<Builder::NodeConverter<::ngraph::op::ShuffleChannels>>(),
+                std::make_shared<Builder::NodeConverter<::ngraph::op::v4::Interpolate>>(),
                 std::make_shared<Builder::NodeConverter<::ExecGraphInfoSerialization::ExecutionNode>>(),
         };
         CNNLayerPtr result;
