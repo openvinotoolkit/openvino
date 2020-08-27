55c72a0ef33a23f540d1890ce413c068c5b6ea4c
Alexey Varyzgin <alexey.varyzgin@intel.com>
2020-08-27 13:49:31 +0300

    [CPU] [MSVC] memcpy and memcpy_s were unified for all layers and SIMD functionality was enabled on VS2019 (#1374)

--- inference-engine/src/mkldnn_plugin/nodes/common/cpu_memcpy.h
+++ inference-engine/src/mkldnn_plugin/nodes/common/cpu_memcpy.h
@@ -11,9 +11,9 @@
  * @brief Copies bytes between buffers with security enhancements
  * Copies count bytes from src to dest. If the source and destination
  * overlap, the behavior is undefined.
- * @param dest
+ * @param dst
  * pointer to the object to copy to
- * @param destsz
+ * @param dst_size
  * max number of bytes to modify in the destination (typically the size
  * of the destination object)
  * @param src
@@ -23,16 +23,28 @@
  @return zero on success and non-zero value on error.
  */
 
-inline int simple_copy(void* dest, size_t destsz, void const* src, size_t count) {
+inline void cpu_memcpy(void* dst, const void* src, size_t count) {
+#ifdef _WIN32
+    memcpy_s(dst, count, src, count);
+#else
+    std::memcpy(dst, src, count);
+#endif
+}
+
+inline int cpu_memcpy_s(void* dst, size_t dst_size, const void* src, size_t count) {
     size_t i;
-    if (!src || count > destsz ||
-        count > (dest > src ? ((uintptr_t)dest - (uintptr_t)src)
-                            : ((uintptr_t)src - (uintptr_t)dest))) {
+    if (!src ||
+        count > dst_size ||
+        count > (dst > src ? ((uintptr_t)dst - (uintptr_t)src) : ((uintptr_t)src - (uintptr_t)dst))) {
         // zero out dest if error detected
-        memset(dest, 0, destsz);
+        std::memset(dst, 0, dst_size);
         return -1;
     }
 
-    for (i = 0; i < count; ++i) (reinterpret_cast<uint8_t*>(dest))[i] = (reinterpret_cast<const uint8_t*>(src))[i];
+#ifdef _WIN32
+    memcpy_s(dst, dst_size, src, count);
+#else
+    std::memcpy(dst, src, count);
+#endif
     return 0;
 }
