8b2c12967d336ad8cd1a169e18c9577efdf3f014
Dmitry Kurtaev <dmitry.kurtaev+github@gmail.com>
2020-08-27 17:10:24 +0300

    [JAVA] Use JUnit 4 features to log Java tests log (#1953)
    * [JAVA] Use JUnit 4 features to log Java tests log
    
    * [JAVA] Add device parameter for Java tests

--- inference-engine/ie_bridges/java/tests/OpenVinoTestRunner.java
+++ inference-engine/ie_bridges/java/tests/OpenVinoTestRunner.java
@@ -3,13 +3,17 @@ import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 public class OpenVinoTestRunner {
+
     public static void main(String[] args) {
+        ArgumentParser parser = new ArgumentParser("");
+        parser.addArgument("-d", "device to test");
+        parser.parseArgs(args);
+        IETest.device = parser.get("-d", "CPU");
+
         Result result = JUnitCore.runClasses(TestsSuite.class);
 		
         for (Failure failure : result.getFailures()) {
            System.out.println(failure.toString());
         }
-          
-        System.out.println(result.wasSuccessful());
     }
 }
