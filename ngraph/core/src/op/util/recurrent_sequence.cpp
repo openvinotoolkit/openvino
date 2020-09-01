7f4153e4823d7b79b2373dcf480760c1959b7aa3
Szymon Durawa <szymon.durawa@intel.com>
2020-09-01 11:01:20 +0200

    Dynamic shape cell (#1683)
    * validate_and_infer_types() implementation
    
    * input parameter validation for LSTM, GRU and RNN
    
    * style-check applied
    
    * Add LSTMSequence dynamic shape validation and test props for RNNCell, GRUCell, LSTMCell and LSTMSequence.
    
    * recurrent_sequence.hpp moved to ngraph/core/include/ngraph/op/util/
    
    * style check applied
    
    * removed unused variable from LSTMSequence::validate_and_infer_types
    
    * Add missing newline mark at the end of file.
    
    * Add supression macro for FusedOp deprecation.
    
    * Add element type initialization
    
    * Apply PR review remarks
    
    * Rewrite tests to use gtest exception assertions.

---
+++ ngraph/core/src/op/util/recurrent_sequence.cpp
@@ -0,0 +1,72 @@
+//*****************************************************************************
+// Copyright 2017-2020 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+
+#include "recurrent_sequence.hpp"
+
+using namespace std;
+using namespace ngraph;
+
+void op::util::validate_seq_input_rank_dimension(const std::vector<ngraph::PartialShape>& input)
+{
+    enum
+    {
+        X,
+        initial_hidden_state,
+        sequence_lengths,
+        W,
+        R,
+        B
+    };
+
+    // Verify static ranks for all inputs
+    for (size_t i = 0; i < input.size(); i++)
+    {
+        NGRAPH_CHECK((input[i].rank().is_static()),
+                     "RNN Sequence supports only static rank for input tensors.");
+    }
+
+    for (size_t i = 0; i < input.size(); i++)
+    {
+        if (i == B)
+        {
+            // verify B input dimension which is 2D
+            NGRAPH_CHECK((input[i].rank().get_length() == 2),
+                         "RNN Sequence B input tensor dimension is not correct.");
+        }
+        else if (i == sequence_lengths)
+        {
+            // verify sequence_length input dimension which is 1D
+            NGRAPH_CHECK((input[i].rank().get_length() == 1),
+                         "RNN Sequence sequence_lengths input tensor dimension is not correct.");
+        }
+        else
+        {
+            // Verify all other input dimensions which are 3D tensor types
+            NGRAPH_CHECK((input[i].rank().get_length() == 3),
+                         "RNN Sequence input tensor dimension is not correct for ",
+                         i,
+                         " input parameter. Current input length: ",
+                         input[i].rank().get_length());
+        }
+    }
+
+    // Compare input_size dimension for X and W inputs
+    const auto& x_pshape = input.at(X);
+    const auto& w_pshape = input.at(W);
+
+    NGRAPH_CHECK((x_pshape[2].compatible(w_pshape[2])),
+                 "RNN Sequence mismatched input_size dimension.");
+}
