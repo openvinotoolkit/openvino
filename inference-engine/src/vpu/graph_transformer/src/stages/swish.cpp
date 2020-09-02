3cadeecb272bf4aba9f2621a9ecf87af444268c5
Kudriavtsev Nikita <nikita.kudriavtsev@intel.com>
2020-09-02 07:21:00 +0300

    [IE Myriad] Fix Swish layer serialization.

--- inference-engine/src/vpu/graph_transformer/src/stages/swish.cpp
+++ inference-engine/src/vpu/graph_transformer/src/stages/swish.cpp
@@ -18,21 +18,27 @@ private:
         return std::make_shared<SwishStage>(*this);
     }
 
-    void serializeParamsImpl(BlobSerializer&) const override {
+    void serializeParamsImpl(BlobSerializer& serializer) const override {
+        auto beta = attrs().get<float>("beta");
+
+        serializer.append(static_cast<float>(beta));
     }
 };
 
 }  // namespace
 
 void FrontEnd::parseSwish(const Model& model, const ie::CNNLayerPtr& layer, const DataVector& inputs, const DataVector& outputs) const {
-    VPU_THROW_UNLESS((inputs.size() == 1) || ((inputs.size() == 2)),
-                     "Swish stage with name %s must have 1 or 2 inputs, "
+    VPU_THROW_UNLESS((inputs.size() == 1),
+                     "Swish stage with name %s must have 1 input, "
                      "actually provided %d", layer->name, inputs.size());
     VPU_THROW_UNLESS(outputs.size() == 1,
                      "Swish stage with name %s must have only 1 output, "
                      "actually provided %d", layer->name, outputs.size());
 
-    model->addNewStage<SwishStage>(layer->name, StageType::Swish, layer, inputs, outputs);
+    auto stage = model->addNewStage<SwishStage>(
+        layer->name, StageType::Swish, layer, inputs, outputs);
+
+    stage->attrs().set<float>("beta", layer->GetParamAsFloat("alpha"));
 }
 
 }  // namespace vpu
