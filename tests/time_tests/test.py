4e48b4ddf79ce9abcd521c9ec407aeb687410ea6
vurusovs <vitaliy.urusovskij@intel.com>
2020-09-03 23:57:24 +0300

    Next version

--- tests/time_tests/test.py
+++ tests/time_tests/test.py
@@ -11,10 +11,41 @@ Options[*]:
 [*] For more information see conftest.py
 """
 
-from stress_tests.scripts.run_memcheck import run
 import statistics as Statistics
 from pathlib import Path
 import tempfile
+import subprocess
+import logging
+
+
+def run(args, log=None, verbose=True):
+    """ Run command
+    """
+    if log is None:
+        log = logging.getLogger('run_memcheck')
+    log_out = log.info if verbose else log.debug
+
+    log.info(f'========== cmd: {" ".join(args)}')  # pylint: disable=logging-format-interpolation
+
+    proc = subprocess.Popen(args,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT,
+                            encoding='utf-8',
+                            universal_newlines=True)
+    output = []
+    for line in iter(proc.stdout.readline, ''):
+        log_out(line.strip('\n'))
+        output.append(line)
+        if line or proc.poll() is None:
+            continue
+        break
+    outs = proc.communicate()[0]
+
+    if outs:
+        log_out(outs.strip('\n'))
+        output.append(outs)
+    log.info('========== Completed. Exit code: %d', proc.returncode)
+    return proc.returncode, ''.join(output)
 
 
 def read_statistics(statistics_path, statistics):
@@ -40,7 +71,7 @@ def test_run(instance, binary, niter, omz, mo, out_dir, no_venv):
         # TODO: call OMZ and fill model_path (it may be done by parsing MO log)
         retcode = 1
         msg = ""
-        assert retcode != 0, msg
+        assert retcode == 0, msg
     else:
         model_path = instance["model"]["path"]
 
@@ -50,13 +81,17 @@ def test_run(instance, binary, niter, omz, mo, out_dir, no_venv):
                   "-d", instance["device"]["name"]]
 
     # Execute binary and collect statistics
+    statistics_dir = (Path(".") / "statistics_dir").absolute()
+    statistics_dir.mkdir(parents=True, exist_ok=True)
+
     statistics = {}
     for run_iter in range(niter):
-        statistics_path = (Path(".") / "statistics_dir" / Path(tempfile.NamedTemporaryFile().name).stem).absolute()
-        cmd = cmd_common + ["-s", statistics_path]
+        statistics_path = statistics_dir / Path(tempfile.NamedTemporaryFile().name).stem
+        cmd = cmd_common + ["-s", str(statistics_path)]
         retcode, msg = run(cmd)
         # TODO: replace asserts with statuses handling to upload status to a DB
-        assert retcode != 0, "Run of binary '{}' failed: {}".format(binary, msg)
+        assert retcode == 0, "Run of binary '{}' failed: {}".format(binary, msg)
         statistics = read_statistics(statistics_path, statistics)
 
     aggregated_stats = aggregate_statistics(statistics)
+    print(aggregated_stats)
