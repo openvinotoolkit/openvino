f6d4d98b13a7e5848208a01db4db84aa65b7363b
tomdol <tomasz.dolbniak@intel.com>
2020-09-08 22:34:02 +0200

    Parametrized UTs for FQMul transform

--- inference-engine/tests/functional/inference_engine/transformations/fq_mul_fusion_test.cpp
+++ inference-engine/tests/functional/inference_engine/transformations/fq_mul_fusion_test.cpp
@@ -20,65 +20,70 @@
 
 namespace LayerTestsDefinitions {
 
-using FQMulFusionParams = std::tuple<int, std::string>;
+using FQMulFusionParams =
+    std::tuple<ngraph::Shape,  // FQ data shape
+               ngraph::Shape,  // in_* shape
+               ngraph::Shape,  // out_* shape
+               ngraph::Shape>; // Mul constant shape
 
 class FQMulFusion : public testing::WithParamInterface<FQMulFusionParams>,
                     public CommonTestUtils::TestsCommon {
 public:
-  void SetUp() override {
-    const auto data = ngraph::opset4::Constant::create(
-        ngraph::element::Type_t::f32, ngraph::Shape{64, 3, 7, 7}, {0.0f});
-    const auto in_low = ngraph::opset4::Constant::create(
-        ngraph::element::Type_t::f32, ngraph::Shape{}, {-0.5f});
-    const auto in_high = ngraph::opset4::Constant::create(
-        ngraph::element::Type_t::f32, ngraph::Shape{}, {0.5f});
-    const auto out_low = ngraph::opset4::Constant::create(
-        ngraph::element::Type_t::f32, ngraph::Shape{}, {0.0f});
-    const auto out_high = ngraph::opset4::Constant::create(
-        ngraph::element::Type_t::f32, ngraph::Shape{}, {100.0f});
-    const auto fq = std::make_shared<ngraph::opset4::FakeQuantize>(
-        data, in_low, in_high, out_low, out_high, 255);
+    void SetUp() override {
+        ngraph::Shape data_shape, in_shape, out_shape, mul_const_shape;
+        std::tie(data_shape, in_shape, out_shape, mul_const_shape) =
+            this->GetParam();
 
-    const auto mul_value = ngraph::opset4::Constant::create(
-        ngraph::element::Type_t::f32, ngraph::Shape{64, 1, 1, 1}, {3.14f});
-    const auto mul = std::make_shared<ngraph::opset4::Multiply>(fq, mul_value);
+        const auto data = ngraph::opset4::Constant::create(
+            ngraph::element::Type_t::f32, data_shape, {0.0f});
+        const auto in_low = ngraph::opset4::Constant::create(
+            ngraph::element::Type_t::f32, in_shape, {-0.5f});
+        const auto in_high = ngraph::opset4::Constant::create(
+            ngraph::element::Type_t::f32, in_shape, {0.5f});
+        const auto out_low = ngraph::opset4::Constant::create(
+            ngraph::element::Type_t::f32, out_shape, {0.0f});
+        const auto out_high = ngraph::opset4::Constant::create(
+            ngraph::element::Type_t::f32, out_shape, {100.0f});
+        const auto fq = std::make_shared<ngraph::opset4::FakeQuantize>(
+            data, in_low, in_high, out_low, out_high, 255);
 
-    m_function = std::make_shared<ngraph::Function>(
-        ngraph::OutputVector{mul}, ngraph::ParameterVector{}, "FQMulFusion");
+        const auto mul_value = ngraph::opset4::Constant::create(
+            ngraph::element::Type_t::f32, mul_const_shape, {3.14f});
+        const auto mul = std::make_shared<ngraph::opset4::Multiply>(fq, mul_value);
 
-    const auto expected_data = ngraph::opset4::Constant::create(
-        ngraph::element::Type_t::f32, ngraph::Shape{64, 3, 7, 7}, {0.0f});
-    const auto expected_in_low = ngraph::opset4::Constant::create(
-        ngraph::element::Type_t::f32, ngraph::Shape{}, {-0.5f});
-    const auto expected_in_high = ngraph::opset4::Constant::create(
-        ngraph::element::Type_t::f32, ngraph::Shape{}, {0.5f});
-    const auto expected_out_low = ngraph::opset4::Constant::create(
-        ngraph::element::Type_t::f32, ngraph::Shape{}, {0.0f});
-    const auto expected_out_high = ngraph::opset4::Constant::create(
-        ngraph::element::Type_t::f32, ngraph::Shape{}, {100.0f});
+        m_function = std::make_shared<ngraph::Function>(
+            ngraph::OutputVector{mul}, ngraph::ParameterVector{}, "FQMulFusion");
 
-    const auto expected_mul_value = ngraph::opset4::Constant::create(
-        ngraph::element::Type_t::f32, ngraph::Shape{64, 1, 1, 1}, {3.14f});
-    const auto expected_mul_out_low =
-        std::make_shared<ngraph::opset4::Multiply>(expected_out_low,
-                                                   expected_mul_value);
-    const auto expected_mul_out_high =
-        std::make_shared<ngraph::opset4::Multiply>(expected_out_high,
-                                                   expected_mul_value);
-    const auto expected_fq = std::make_shared<ngraph::opset4::FakeQuantize>(
-        expected_data, expected_in_low, expected_in_high, expected_mul_out_low,
-        expected_mul_out_high, 255);
+        const auto expected_data = ngraph::opset4::Constant::create(
+            ngraph::element::Type_t::f32, data_shape, {0.0f});
+        const auto expected_in_low = ngraph::opset4::Constant::create(
+            ngraph::element::Type_t::f32, in_shape, {-0.5f});
+        const auto expected_in_high = ngraph::opset4::Constant::create(
+            ngraph::element::Type_t::f32, in_shape, {0.5f});
+        const auto expected_out_low = ngraph::opset4::Constant::create(
+            ngraph::element::Type_t::f32, out_shape, {0.0f});
+        const auto expected_out_high = ngraph::opset4::Constant::create(
+            ngraph::element::Type_t::f32, out_shape, {100.0f});
 
-    m_expected_function = std::make_shared<ngraph::Function>(
-        ngraph::OutputVector{expected_fq}, ngraph::ParameterVector{},
-        "FQMulFusion_expected");
-  }
+        const auto expected_mul_value = ngraph::opset4::Constant::create(
+            ngraph::element::Type_t::f32, mul_const_shape, {3.14f});
+        const auto expected_mul_out_low =
+            std::make_shared<ngraph::opset4::Multiply>(expected_out_low, expected_mul_value);
+        const auto expected_mul_out_high =
+            std::make_shared<ngraph::opset4::Multiply>(expected_out_high, expected_mul_value);
+        const auto expected_fq = std::make_shared<ngraph::opset4::FakeQuantize>(
+            expected_data, expected_in_low, expected_in_high, expected_mul_out_low,
+            expected_mul_out_high, 255);
+
+        m_expected_function = std::make_shared<ngraph::Function>(
+            ngraph::OutputVector{expected_fq}, ngraph::ParameterVector{}, "FQMulFusion_expected");
+    }
 
   std::shared_ptr<ngraph::Function> m_function;
   std::shared_ptr<ngraph::Function> m_expected_function;
 };
 
-TEST_P(FQMulFusion, FusionOccurred) {
+TEST_P(FQMulFusion, ExpectFusion) {
   ngraph::pass::Manager manager;
   manager.register_pass<ngraph::pass::FakeQuantizeMulFusion>();
 
@@ -89,9 +94,35 @@ TEST_P(FQMulFusion, FusionOccurred) {
 };
 
 namespace {
-INSTANTIATE_TEST_CASE_P(FQMulFusionTests, FQMulFusion,
-                        ::testing::Combine(::testing::Values(1),
-                                           ::testing::Values("test1")));
+INSTANTIATE_TEST_CASE_P(MulConstBcastedToDataShape, FQMulFusion,
+                        ::testing::Combine(::testing::Values(ngraph::Shape{64, 3, 7, 7}),
+                                           ::testing::Values(ngraph::Shape{}),
+                                           ::testing::Values(ngraph::Shape{}),
+                                           ::testing::Values(ngraph::Shape{64, 1, 1, 1})));
+
+INSTANTIATE_TEST_CASE_P(FQInputs1D, FQMulFusion,
+                        ::testing::Combine(::testing::Values(ngraph::Shape{64, 3, 7, 7}),
+                                           ::testing::Values(ngraph::Shape{1}),
+                                           ::testing::Values(ngraph::Shape{}),
+                                           ::testing::Values(ngraph::Shape{64, 1, 1, 1})));
+
+INSTANTIATE_TEST_CASE_P(FQOutputs1D, FQMulFusion,
+                        ::testing::Combine(::testing::Values(ngraph::Shape{64, 3, 7, 7}),
+                                           ::testing::Values(ngraph::Shape{}),
+                                           ::testing::Values(ngraph::Shape{1}),
+                                           ::testing::Values(ngraph::Shape{64, 1, 1, 1})));
+
+INSTANTIATE_TEST_CASE_P(FQInputsOutputs1D, FQMulFusion,
+                        ::testing::Combine(::testing::Values(ngraph::Shape{64, 3, 7, 7}),
+                                           ::testing::Values(ngraph::Shape{1}),
+                                           ::testing::Values(ngraph::Shape{1}),
+                                           ::testing::Values(ngraph::Shape{64, 1, 1, 1})));
+
+INSTANTIATE_TEST_CASE_P(MulConstScalar, FQMulFusion,
+                        ::testing::Combine(::testing::Values(ngraph::Shape{64, 3, 7, 7}),
+                                           ::testing::Values(ngraph::Shape{}),
+                                           ::testing::Values(ngraph::Shape{}),
+                                           ::testing::Values(ngraph::Shape{})));
 } // namespace
 
 } // namespace LayerTestsDefinitions
