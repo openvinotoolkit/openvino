373cb389c3ac566da9823e0bb9717cce1df78c9e
tomdol <tomasz.dolbniak@intel.com>
2020-09-08 21:03:49 +0200

    Basic UT for the transform

--- inference-engine/tests/functional/inference_engine/transformations/fq_mul_fusion_test.cpp
+++ inference-engine/tests/functional/inference_engine/transformations/fq_mul_fusion_test.cpp
@@ -9,6 +9,7 @@
 #include <ie_core.hpp>
 
 #include "common_test_utils/common_utils.hpp"
+#include "common_test_utils/ngraph_test_utils.hpp"
 #include "common_test_utils/test_common.hpp"
 #include "functional_test_utils/plugin_cache.hpp"
 
@@ -38,27 +39,53 @@ public:
     const auto fq = std::make_shared<ngraph::opset4::FakeQuantize>(
         data, in_low, in_high, out_low, out_high, 255);
 
-    // ngraph::builder::makeConstant<float>(ngPrc, outFormShapes2, {}, true);
     const auto mul_value = ngraph::opset4::Constant::create(
         ngraph::element::Type_t::f32, ngraph::Shape{64, 1, 1, 1}, {3.14f});
     const auto mul = std::make_shared<ngraph::opset4::Multiply>(fq, mul_value);
 
     m_function = std::make_shared<ngraph::Function>(
         ngraph::OutputVector{mul}, ngraph::ParameterVector{}, "FQMulFusion");
+
+    const auto expected_data = ngraph::opset4::Constant::create(
+        ngraph::element::Type_t::f32, ngraph::Shape{64, 3, 7, 7}, {0.0f});
+    const auto expected_in_low = ngraph::opset4::Constant::create(
+        ngraph::element::Type_t::f32, ngraph::Shape{}, {-0.5f});
+    const auto expected_in_high = ngraph::opset4::Constant::create(
+        ngraph::element::Type_t::f32, ngraph::Shape{}, {0.5f});
+    const auto expected_out_low = ngraph::opset4::Constant::create(
+        ngraph::element::Type_t::f32, ngraph::Shape{}, {0.0f});
+    const auto expected_out_high = ngraph::opset4::Constant::create(
+        ngraph::element::Type_t::f32, ngraph::Shape{}, {100.0f});
+
+    const auto expected_mul_value = ngraph::opset4::Constant::create(
+        ngraph::element::Type_t::f32, ngraph::Shape{64, 1, 1, 1}, {3.14f});
+    const auto expected_mul_out_low =
+        std::make_shared<ngraph::opset4::Multiply>(expected_out_low,
+                                                   expected_mul_value);
+    const auto expected_mul_out_high =
+        std::make_shared<ngraph::opset4::Multiply>(expected_out_high,
+                                                   expected_mul_value);
+    const auto expected_fq = std::make_shared<ngraph::opset4::FakeQuantize>(
+        expected_data, expected_in_low, expected_in_high, expected_mul_out_low,
+        expected_mul_out_high, 255);
+
+    m_expected_function = std::make_shared<ngraph::Function>(
+        ngraph::OutputVector{expected_fq}, ngraph::ParameterVector{},
+        "FQMulFusion_expected");
   }
 
   std::shared_ptr<ngraph::Function> m_function;
+  std::shared_ptr<ngraph::Function> m_expected_function;
 };
 
-TEST_P(FQMulFusion, ValidateFusedSubgraph) {
+TEST_P(FQMulFusion, FusionOccurred) {
   ngraph::pass::Manager manager;
   manager.register_pass<ngraph::pass::FakeQuantizeMulFusion>();
 
-  auto cloned_function = ngraph::clone_function(*m_function);
-  manager.run_passes(cloned_function);
+  manager.run_passes(m_function);
 
-  // additional Mul node should be added
-  ASSERT_EQ(cloned_function->get_ops().size(), m_function->get_ops().size());
+  const auto res = compare_functions(m_function, m_expected_function);
+  ASSERT_TRUE(res.first) << res.second;
 };
 
 namespace {
