0eb2c1e20c33c047b988433a86bc251405e12ea4
vurusovs <vitaliy.urusovskij@intel.com>
2020-09-08 14:57:05 +0300

    WIP

---
+++ tests/time_tests/test_executable.py
@@ -0,0 +1,36 @@
+"""Main entry-point to run E2E OSS tests.
+TODO: update
+Default run:
+$ pytest test.py
+
+Options[*]:
+--modules       Paths to tests
+--env_conf      Path to environment config
+--test_conf     Path to test config
+
+[*] For more information see conftest.py
+"""
+
+from pathlib import Path
+import logging
+
+from run_executable import run_executable
+
+
+def test_run(instance, executable, niter, omz, mo, out_dir, no_venv):
+    """Parameterized test.
+
+    :param instance: test instance
+    """
+    # Prepare model to get model_path
+    model_path = ""
+    if instance["model"]["source"] == "omz":
+        # TODO: call OMZ and fill model_path (it may be done by parsing MO log)
+        retcode = 1
+        msg = ""
+        assert retcode == 0, msg
+    else:
+        model_path = instance["model"]["path"]
+
+    retcode, aggr_stats = run_executable(executable, model_path, instance["device"]["name"], niter, log=logging)
+    assert retcode == 0, "Something went wrong"     # TODO: update
