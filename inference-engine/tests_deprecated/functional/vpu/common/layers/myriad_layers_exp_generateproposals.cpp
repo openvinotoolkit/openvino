ddc753b98d22d05b70a2b7bc6e105e0383ffd068
Evgeny Latkin <evgeny.latkin@intel.com>
2020-09-08 18:28:01 +0300

    Myriad: re-enable ExpGenerateProposals tests

--- inference-engine/tests_deprecated/functional/vpu/common/layers/myriad_layers_exp_generateproposals.cpp
+++ inference-engine/tests_deprecated/functional/vpu/common/layers/myriad_layers_exp_generateproposals.cpp
@@ -4,8 +4,7 @@
 
 #include "myriad_layers_exp_generateproposals_test.hpp"
 
-//#-38589
-INSTANTIATE_TEST_CASE_P(DISABLED_accuracy, myriadLayersTestsExpGenerateProposals_smoke,
+INSTANTIATE_TEST_CASE_P(accuracy, myriadLayersTestsExpGenerateProposals_smoke,
     ::testing::Combine(
         ::testing::ValuesIn(s_ExpGenerateProposalsLayerScores),
         ::testing::ValuesIn(s_ExpGenerateProposalsLayerImInfo),
