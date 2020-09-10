c7633e7016331e5453c92799a8f52a44ce146502
Anton Dudchenko <anton.dudchenko@intel.com>
2020-09-10 14:49:19 +0300

    [IE][VPU]: Support ngraph::Function in VPU QueryNetwork (#1929)
    * Support ngraph::Function in VPU QueryNetwork

--- inference-engine/src/vpu/myriad_plugin/myriad_plugin.cpp
+++ inference-engine/src/vpu/myriad_plugin/myriad_plugin.cpp
@@ -14,11 +14,15 @@
 
 #include <vpu/vpu_plugin_config.hpp>
 #include <vpu/parsed_config.hpp>
+#include <vpu/frontend/frontend.hpp>
 #include <vpu/utils/profiling.hpp>
 #include <vpu/utils/error.hpp>
 #include <transformations/tensor_iterator_transformations/apply_transformations_to_ti_body.hpp>
 #include <transformations/common_optimizations/common_optimizations.hpp>
+#include <transformations/rt_info/fused_names_attribute.hpp>
 #include <vpu/ngraph/transformations/convert_nms_4_to_nms_dynamic.hpp>
+#include <ngraph/op/util/op_types.hpp>
+#include <ngraph/opsets/opset3.hpp>
 
 #include "vpu/ngraph/transformations/dynamic_to_static_shape.hpp"
 #include "vpu/ngraph/transformations/eliminate_shapeof_after_dsr.hpp"
@@ -31,6 +35,7 @@ using namespace InferenceEngine;
 using namespace InferenceEngine::PluginConfigParams;
 using namespace InferenceEngine::VPUConfigParams;
 using namespace vpu::MyriadPlugin;
+
 
 ExecutableNetworkInternal::Ptr Engine::LoadExeNetworkImpl(
         const ICNNNetwork& network,
@@ -98,24 +103,160 @@ void Engine::QueryNetwork(
         VPU_THROW_UNLESS(!(std::find(deviceIDs.begin(), deviceIDs.end(), deviceName) == deviceIDs.end()), "Myriad device: {} not found.", deviceName);
     }
 
-    if (network.getFunction()) {
-        THROW_IE_EXCEPTION << NOT_IMPLEMENTED_str << " ngraph::Function is not supported natively";
-    }
+    if (auto function = network.getFunction()) {
+        std::unordered_set<std::string> originalOps;
+        for (auto& node : function->get_ops()) {
+            originalOps.emplace(node->get_friendly_name());
+        }
 
-    const auto log = std::make_shared<Logger>(
-        "GraphCompiler",
-        parsedConfigCopy.logLevel(),
-        defaultOutput(parsedConfigCopy.compilerLogFilePath()));
+        auto clonedNetwork = cloneNetwork(network);
+        auto convertedNetwork = vpu::FrontEnd::convertNetwork(*clonedNetwork);
 
-    const auto layerNames = getSupportedLayers(
-        network,
-        static_cast<Platform>(parsedConfigCopy.platform()),
-        parsedConfigCopy.compileConfig(),
-        log,
-        GetCore());
+        std::unordered_set<std::string> supported;
+        std::unordered_set<std::string> unsupported;
 
-    for (const auto& layerName : layerNames) {
-        res.supportedLayersMap.insert({ layerName, GetName() });
+        std::unordered_set<std::string> splitNames;
+        std::unordered_set<std::string> concatNames;
+
+        ngraph::NodeVector splits;
+        ngraph::NodeVector concats;
+
+        const auto isLayerSupported = [this, &splitNames, &concatNames, &concats, &splits](CNNNetworkIterator& layer) -> bool {
+                auto node = (*layer)->getNode();
+                if (std::dynamic_pointer_cast<const ::ngraph::opset3::Split>(node) != nullptr) {
+                    splitNames.emplace(node->get_friendly_name());
+                    splits.push_back(node);
+                    return false;
+                } else if (std::dynamic_pointer_cast<const ::ngraph::opset3::Concat>(node) != nullptr) {
+                    concatNames.emplace(node->get_friendly_name());
+                    concats.push_back(node);
+                    return false;
+                } else {
+                    auto stageBuilder = std::make_shared<StageBuilder>();
+                    auto frontEnd = std::make_shared<FrontEnd>(stageBuilder, GetCore());
+                    return frontEnd->isLayerSupported((*layer)->type);
+                }
+        };
+
+        for (CNNNetworkIterator itLayer{convertedNetwork.get()};
+             itLayer != CNNNetworkIterator();
+             itLayer++) {
+            const auto fusedNode = (*itLayer)->getNode();
+            if (fusedNode == nullptr) {
+                continue;
+            }
+
+            for (auto& fusedLayerName : ngraph::getFusedNamesVector(fusedNode)) {
+                if (contains(originalOps, fusedLayerName)) {
+                    if (isLayerSupported(itLayer)) {
+                        supported.emplace(fusedLayerName);
+                    } else {
+                        unsupported.emplace(fusedLayerName);
+                    }
+                }
+            }
+        }
+
+        for (const auto& layerName : supported) {
+            if (contains(unsupported, layerName)) {
+                supported.erase(layerName);
+            }
+        }
+
+        unsupported.clear();
+
+        std::function<void(std::shared_ptr<ngraph::Node>)> markParentSplitAsUnsupported = [&markParentSplitAsUnsupported, &supported, &splitNames]
+                                                                                          (const std::shared_ptr<ngraph::Node>& split) {
+            const auto inputs = split->inputs();
+            for (const auto& input : inputs) {
+                const auto& parentName = input.get_source_output().get_node()->get_friendly_name();
+                if (contains(supported, parentName) &&
+                    contains(splitNames, parentName)) {
+                    markParentSplitAsUnsupported(input.get_source_output().get_node_shared_ptr());
+                }
+            }
+            const auto& name = split->get_friendly_name();
+            if (contains(supported, name)) {
+                supported.erase(name);
+            }
+        };
+
+        for (const auto& split : splits) {
+            // We will mark split as a supported only if all consumers is supported
+            bool is_supported = true;
+            const auto outputs = split->outputs();
+            for (const auto& output : outputs) {
+                for (const auto& consumer : output.get_target_inputs()) {
+                    const auto& name = consumer.get_node()->get_friendly_name();
+                    if (!contains(supported, name) &&
+                        !contains(concatNames, name) &&
+                        !contains(splitNames, name)) {
+                        is_supported = false;
+                        break;
+                    }
+                }
+            }
+            if (is_supported) {
+                supported.emplace(split->get_friendly_name());
+            } else {
+                // If Split is not supported and it's parent is also Split, mark parent as unsupported
+                markParentSplitAsUnsupported(split);
+            }
+        }
+
+        for (const auto& concat : concats) {
+            // We will mark concat as a supported only if all parent layers is supported
+            bool is_supported = true;
+            const auto inputs = concat->inputs();
+            for (const auto& input : inputs) {
+                const auto& name = input.get_source_output().get_node()->get_friendly_name();
+                if (!contains(supported, name) &&
+                    !contains(concatNames, name)) {
+                    is_supported = false;
+                    break;
+                }
+            }
+            if (is_supported) {
+                supported.emplace(concat->get_friendly_name());
+            }
+        }
+
+        for (const auto& node : function->get_ops()) {
+            if (contains(supported, node->get_friendly_name())) {
+                for (const auto& inputNodeOutput : node->input_values()) {
+                    if (ngraph::op::is_constant(inputNodeOutput.get_node()) || ngraph::op::is_parameter(inputNodeOutput.get_node())) {
+                        supported.emplace(inputNodeOutput.get_node()->get_friendly_name());
+                    }
+                }
+                for (const auto& outputs : node->outputs()) {
+                    for (const auto& outputNodeInput : outputs.get_target_inputs()) {
+                        if (ngraph::op::is_output(outputNodeInput.get_node())) {
+                            supported.emplace(outputNodeInput.get_node()->get_friendly_name());
+                        }
+                    }
+                }
+            }
+        }
+
+        for (const auto& layerName : supported) {
+            res.supportedLayersMap.emplace(layerName, GetName());
+        }
+    } else {
+        const auto log = std::make_shared<Logger>(
+            "GraphCompiler",
+            parsedConfigCopy.logLevel(),
+            defaultOutput(parsedConfigCopy.compilerLogFilePath()));
+
+        const auto layerNames = getSupportedLayers(
+            network,
+            static_cast<Platform>(parsedConfigCopy.platform()),
+            parsedConfigCopy.compileConfig(),
+            log,
+            GetCore());
+
+        for (const auto& layerName : layerNames) {
+            res.supportedLayersMap.insert({ layerName, GetName() });
+        }
     }
 }
 
