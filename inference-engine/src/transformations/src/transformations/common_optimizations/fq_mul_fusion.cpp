4ab667b4e7066bfd58b8a3bd6e712ef4080c017e
tomdol <tomasz.dolbniak@intel.com>
2020-09-11 09:02:51 +0200

    Shape checking infrastructure skeleton

--- inference-engine/src/transformations/src/transformations/common_optimizations/fq_mul_fusion.cpp
+++ inference-engine/src/transformations/src/transformations/common_optimizations/fq_mul_fusion.cpp
@@ -13,20 +13,26 @@
 #include <ngraph/rt_info.hpp>
 
 namespace {
+  bool qualifies_for_fusion(const ngraph::PartialShape& data_shape,
+                            const ngraph::Shape& out_shape,
+                            const ngraph::Shape& multiplier_shape) {
+    return true;
+  }
+
   std::pair<ngraph::Output<ngraph::Node>, ngraph::Output<ngraph::Node>>
   get_adjusted_output_range(ngraph::Output<ngraph::Node> out_low,
                             ngraph::Output<ngraph::Node> out_high,
-                            ngraph::Output<ngraph::Node> mul_constant) {
-    const auto mul_out_low = std::make_shared<ngraph::opset4::Multiply>(out_low, mul_constant);
-    const auto mul_out_high = std::make_shared<ngraph::opset4::Multiply>(out_high, mul_constant);
+                            ngraph::Output<ngraph::Node> multiplier) {
+    const auto mul_out_low = std::make_shared<ngraph::opset4::Multiply>(out_low, multiplier);
+    const auto mul_out_high = std::make_shared<ngraph::opset4::Multiply>(out_high, multiplier);
 
     ngraph::OutputVector folded_out_low(1), folded_out_high(1);
 
-    if (!mul_out_low->constant_fold(folded_out_low, {out_low, mul_constant})) {
+    if (!mul_out_low->constant_fold(folded_out_low, {out_low, multiplier})) {
       throw ngraph::ngraph_error("Could not constant fold the output_low Multiply operation");
     }
 
-    if (!mul_out_high->constant_fold(folded_out_high, {out_high, mul_constant})) {
+    if (!mul_out_high->constant_fold(folded_out_high, {out_high, multiplier})) {
       throw ngraph::ngraph_error("Could not constant fold the output_high Multiply operation");
     }
 
@@ -55,11 +61,12 @@ namespace {
 //
 
 ngraph::pass::FakeQuantizeMulFusion::FakeQuantizeMulFusion() {
+  const auto data_p = ngraph::pattern::any_input();
   const auto fq_output_low_p = ngraph::pattern::wrap_type<opset4::Constant>();
   const auto fq_output_high_p = ngraph::pattern::wrap_type<opset4::Constant>();
 
   const auto fq_node_p = ngraph::pattern::wrap_type<opset4::FakeQuantize>(
-      {ngraph::pattern::any_input(),
+      {data_p,
        ngraph::pattern::wrap_type<opset4::Constant>(),
        ngraph::pattern::wrap_type<opset4::Constant>(),
        fq_output_low_p,
@@ -71,7 +78,7 @@ ngraph::pass::FakeQuantizeMulFusion::FakeQuantizeMulFusion() {
       {fq_node_p, mul_constant_p}, pattern::consumers_count(1));
 
   ngraph::matcher_pass_callback callback = [=](pattern::Matcher &m) {
-    const auto &pattern_map = m.get_pattern_value_map();
+    const auto& pattern_map = m.get_pattern_value_map();
 
     const auto fq_node = std::dynamic_pointer_cast<ngraph::opset4::FakeQuantize>(
         pattern_map.at(fq_node_p).get_node_shared_ptr());
@@ -79,26 +86,35 @@ ngraph::pass::FakeQuantizeMulFusion::FakeQuantizeMulFusion() {
       return false;
     }
 
+    const auto data = pattern_map.at(data_p);
     const auto output_low_const = pattern_map.at(fq_output_low_p);
     const auto output_high_const = pattern_map.at(fq_output_high_p);
-    const auto mul_node = pattern_map.at(mul_node_p).get_node_shared_ptr();
     const auto mul_constant = pattern_map.at(mul_constant_p);
 
-    const auto new_output_limits = get_adjusted_output_range(
-      output_low_const, output_high_const, mul_constant);
+    const ngraph::Shape& out_shape =
+      output_low_const.get_shape().size() > output_high_const.get_shape().size() ?
+        output_low_const.get_shape() : output_high_const.get_shape();
 
-    const auto new_fq_node = fq_node->clone_with_new_inputs({fq_node->input_value(0),
-                                                             fq_node->input_value(1),
-                                                             fq_node->input_value(2),
-                                                             new_output_limits.first,
-                                                             new_output_limits.second});
+    if (qualifies_for_fusion(data.get_partial_shape(), out_shape, mul_constant.get_shape())) {
+      const auto new_output_limits = get_adjusted_output_range(
+        output_low_const, output_high_const, mul_constant);
 
-    replace_node(mul_node, new_fq_node);
+      const auto new_fq_node = fq_node->clone_with_new_inputs({fq_node->input_value(0),
+                                                              fq_node->input_value(1),
+                                                              fq_node->input_value(2),
+                                                              new_output_limits.first,
+                                                              new_output_limits.second});
 
-    new_fq_node->set_friendly_name(fq_node->get_friendly_name());
-    copy_runtime_info({fq_node, mul_node}, new_fq_node);
+      const auto mul_node = pattern_map.at(mul_node_p).get_node_shared_ptr();
+      replace_node(mul_node, new_fq_node);
 
-    return true;
+      new_fq_node->set_friendly_name(fq_node->get_friendly_name());
+      copy_runtime_info({fq_node, mul_node}, new_fq_node);
+
+      return true;
+    } else {
+      return false;
+    }
   };
 
   auto m = std::make_shared<ngraph::pattern::Matcher>(mul_node_p,
