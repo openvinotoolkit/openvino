7b33953ff9f2a39c0b5bed60517b98ca08169ab1
mbencer <mateusz.bencer@intel.com>
2020-09-11 12:17:10 +0200

    changed way of adding ngraph.dll path

--- ngraph/python/tests/test_ngraph/__init__.py
+++ ngraph/python/tests/test_ngraph/__init__.py
@@ -1,14 +1,3 @@
-import os
-import sys
-import ngraph as ng
-
-if sys.platform == "win32":
-    # ngraph.dll is installed in PYTHON_INSTALL_DIR/Lib as default
-    # (which is 2 directories above ngraph module)
-    # and this path needs to be visible to the _pyngraph module.
-    #
-    #
-    # If you're using a custom installation of nGraph,
-    # add the location of ngraph.dll to your system PATH.
-    ngraph_dll_dir = os.path.join(os.path.dirname(ng.__file__), "..", "..")
-    os.environ["PATH"] = os.path.abspath(ngraph_dll_dir) + ";" + os.environ["PATH"]
+# ngraph.dll directory path visibility is needed to use _pyngraph module
+# import below causes adding this path to os.environ["PATH"]
+import ngraph
