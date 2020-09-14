1c0b8aafc10b32d8037a29c86cdb0cd579c2aeb5
Alexey Suhov <alexey.suhov@intel.com>
2020-09-14 18:47:19 +0300

    Revert "update OpenCV version to 4.5.0"
    This reverts commit 5d229a45649091d25c3e85f391f99ea88122e008.

--- inference-engine/cmake/dependencies.cmake
+++ inference-engine/cmake/dependencies.cmake
@@ -181,9 +181,9 @@ endif ()
 if (ENABLE_OPENCV)
     reset_deps_cache(OpenCV_DIR)
 
-    set(OPENCV_VERSION "4.5.0")
-    set(OPENCV_BUILD "36")
-    set(OPENCV_BUILD_YOCTO "36")
+    set(OPENCV_VERSION "4.3.0")
+    set(OPENCV_BUILD "060")
+    set(OPENCV_BUILD_YOCTO "073")
 
     if (${CMAKE_SYSTEM_PROCESSOR} STREQUAL "aarch64")
         if(DEFINED ENV{THIRDPARTY_SERVER_PATH})
