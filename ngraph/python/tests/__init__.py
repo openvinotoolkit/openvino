a6a71b4631171ce15b7983e46bbe08cfe8ce4323
Rafal Blaczkowski <rafal.blaczkowski@intel.com>
2020-09-14 13:07:47 +0200

    Add backend tests  to ONNX OpenVino CI (#1972)

--- ngraph/python/tests/__init__.py
+++ ngraph/python/tests/__init__.py
@@ -31,9 +31,14 @@ def xfail_test(reason="Mark the test as expected to fail", strict=True):
     return pytest.mark.xfail(reason=reason, strict=strict)
 
 
-xfail_issue_34314 = xfail_test(reason="RuntimeError: RNNCell operation has a form that is not "
-                               "supported.RNNCell_21204 should be converted to RNNCellIE operation")
 skip_segfault = pytest.mark.skip(reason="Segmentation fault error")
+xfail_issue_33540 = xfail_test(reason="RuntimeError: GRUCell operation has a form that is not supported "
+                                      "GRUCell_<number> should be converted to GRUCellIE operation")
+xfail_issue_33616 = xfail_test(reason="Add ceil_mode for Max and Avg pooling (reference implementation)")
+xfail_issue_34310 = xfail_test(reason="RuntimeError: Error of validate layer: LSTMSequence_<number> with "
+                                      "type: LSTMSequence. Layer is not instance of RNNLayer class")
+xfail_issue_34314 = xfail_test(reason="RuntimeError: RNNCell operation has a form that is not "
+                               "supported.RNNCell_<number> should be converted to RNNCellIE operation")
 xfail_issue_34323 = xfail_test(reason="RuntimeError: data [value] doesn't exist")
 xfail_issue_34327 = xfail_test(reason="RuntimeError: '<value>' layer has different "
                                       "IN and OUT channels number")
@@ -59,11 +64,11 @@ xfail_issue_35925 = xfail_test(reason="Assertion error - reduction ops results m
 xfail_issue_35926 = xfail_test(reason="RuntimeError: [NOT_IMPLEMENTED] Input image format I64 is "
                                       "not supported yet...")
 xfail_issue_35927 = xfail_test(reason="RuntimeError: B has zero dimension that is not allowable")
-xfail_issue_35929 = xfail_test(reason="CRuntimeError: Incorrect precision f64!")
+xfail_issue_35929 = xfail_test(reason="RuntimeError: Incorrect precision f64!")
 xfail_issue_35930 = xfail_test(reason="onnx.onnx_cpp2py_export.checker.ValidationError: "
                                       "Required attribute 'to' is missing.")
 xfail_issue_35932 = xfail_test(reason="Assertion error - logsoftmax results mismatch")
-xfail_issue_36437 = xfail_test(reason="RuntimeError: Cannot find blob with name: y")
+xfail_issue_36437 = xfail_test(reason="RuntimeError: Cannot find blob with name: <value>")
 xfail_issue_36476 = xfail_test(reason="RuntimeError: [NOT_IMPLEMENTED] Input image format U32 is "
                                "not supported yet...")
 xfail_issue_36478 = xfail_test(reason="RuntimeError: [NOT_IMPLEMENTED] Input image format U64 is "
@@ -74,13 +79,112 @@ xfail_issue_36480 = xfail_test(reason="RuntimeError: [NOT_FOUND] Unsupported pro
 xfail_issue_36481 = xfail_test(reason="TypeError: _get_node_factory() takes from 0 to 1 positional "
                                "arguments but 2 were given")
 xfail_issue_36483 = xfail_test(reason="RuntimeError: Unsupported primitive of type: "
-                               "Ceiling name: Ceiling_22669")
+                               "Ceiling name: <value>")
 xfail_issue_36485 = xfail_test(reason="RuntimeError: Check 'm_group >= 1' failed at "
                                "/openvino/ngraph/core/src/op/shuffle_channels.cpp:77:")
 xfail_issue_36486 = xfail_test(reason="RuntimeError: HardSigmoid operation should be converted "
                                       "to HardSigmoid_IE")
 xfail_issue_36487 = xfail_test(reason="Assertion error - mvn operator computation mismatch")
-
+xfail_issue_38084 = xfail_test(reason="RuntimeError: AssertionFailed: layer->get_output_partial_shape(i) "
+                                      "is_static() nGraph NonZero operation with name: result cannot be"
+                                      "converted to NonZero layer with name: result because output"
+                                      "with index 0 contains dynamic shapes: {2,?}. Try to use "
+                                      "CNNNetwork::reshape() method in order to specialize shapes "
+                                      "before the conversion.")
+xfail_issue_38085 = xfail_test(reason="RuntimeError: Interpolate operation should be converted to Interp")
+xfail_issue_38086 = xfail_test(reason="RuntimeError: Quantize layer input '<value>' doesn't have blobs")
+xfail_issue_38087 = xfail_test(reason="RuntimeError: Cannot cast to tensor desc. Format is unsupported!")
+xfail_issue_38088 = xfail_test(reason="RuntimeError: Check '((axis >= axis_range_min) && "
+                                      "(axis <= axis_range_max))' failed at "
+                                      "/openvino/ngraph/core/src/validation_util.cpp:913: "
+                                      "Split Parameter axis <value> out of the tensor rank range <value>.")
+xfail_issue_38089 = xfail_test(reason="RuntimeError: Node 2 contains empty child edge for index 0")
+xfail_issue_38090 = xfail_test(reason="AssertionError: Items types are not equal")
+xfail_issue_38091 = xfail_test(reason="AssertionError: Mismatched elements")
+xfail_issue_38699 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "ai.onnx.preview.training.Gradient")
+xfail_issue_38700 = xfail_test(reason="RuntimeError: nGraph does not support different sequence operations:"
+                                      "ConcatFromSequence, SequenceConstruct, SequenceAt, SplitToSequence,"
+                                      "SequenceEmpty, SequenceInsert, SequenceErase, SequenceLength ")
+xfail_issue_38701 = xfail_test(reason="RuntimeError: unsupported element type: STRING")
+xfail_issue_38702 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "Unique")
+xfail_issue_38704 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "TfIdfVectorizer")
+xfail_issue_38705 = xfail_test(reason="IndexError: deque::_M_range_check: __n (which is 0)"
+                                      ">= this->size() (which is 0)")
+xfail_issue_38706 = xfail_test(reason="RuntimeError: output_3.0 has zero dimension which is not allowed")
+xfail_issue_38707 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "SoftmaxCrossEntropyLoss")
+xfail_issue_38708 = xfail_test(reason="RuntimeError: While validating ONNX node '<Node(Slice): y>': "
+                                      "Axes input must be constant")
+xfail_issue_38709 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "Scan")
+xfail_issue_38710 = xfail_test(reason="RuntimeError: roi has zero dimension which is not allowed")
+xfail_issue_38711 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "GatherElements")
+xfail_issue_38712 = xfail_test(reason="RuntimeError: Check '(fmod == 1) "
+                                      "While validating ONNX node '<Node(Mod): z>': "
+                                      "Only 'fmod=1' mode is supported for mod operator.")
+xfail_issue_38713 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "ai.onnx.preview.training.Momentum")
+xfail_issue_38714 = xfail_test(reason="RuntimeError: While validating ONNX node '<Node(Resize): Y>'"
+                                      "Check 'element::Type::merge(element_type, element_type,"
+                                      "node->get_input_element_type(i))' "
+                                      "While validating node 'v1::<name> (sizes[0]:i64{4},"
+                                      "Convert_29306[0]:f32{4}) -> (dynamic?)' with friendly_name '<name>':"
+                                      "Argument element types are inconsistent.")
+xfail_issue_38715 = xfail_test(reason="RuntimeError: While validating ONNX node '<Node(OneHot): y>':"
+                                      "While validating node 'v1::OneHot OneHot_<number>"
+                                      "(Convert_13525[0]:i64{3}, depth[0]:f32{},"
+                                      "Squeeze_13532[0]:i32{}, Squeeze_13529[0]:i32{}) -> (dynamic?)'"
+                                      "with friendly_name 'OneHot_13534':"
+                                      "Depth must be integral element type.")
+xfail_issue_38716 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "MaxUnpool")
+xfail_issue_38717 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "GreaterOrEqual")
+xfail_issue_38718 = xfail_test(reason="nGraph does not support the following ONNX operations:"
+                                      "IsNaN and isInf")
+xfail_issue_38719 = xfail_test(reason="nGraph does not support the following ONNX operations: GatherND")
+xfail_issue_38720 = xfail_test(reason="nGraph does not support the following ONNX operations:"
+                                      "DynamicQuantizeLinear")
+xfail_issue_38721 = xfail_test(reason="RuntimeError: While validating ONNX node '<Node(Pow): z>': "
+                                      "While validating node 'v1::Power Power_<number>"
+                                      "(x[0]:f32{3}, y[0]:i64{3}) -> (dynamic?)' with friendly_name "
+                                      "'Power_<number>': Argument element types are inconsistent.")
+xfail_issue_38722 = xfail_test(reason="RuntimeError: While validating ONNX nodes MatMulInteger"
+                                      "and QLinearMatMul"
+                                      "Input0 scale and input0 zero point shape must be same and 1")
+xfail_issue_38723 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "QLinearConv")
+xfail_issue_38724 = xfail_test(reason="RuntimeError: While validating ONNX node '<Node(Resize): Y>':"
+                                      "tf_crop_and_resize - this type of coordinate transformation mode"
+                                      "is not supported. Choose one of the following modes:"
+                                      "tf_half_pixel_for_nn, asymmetric, align_corners, pytorch_half_pixel,"
+                                      "half_pixel")
+xfail_issue_38725 = xfail_test(reason="RuntimeError: While validating ONNX node '<Node(Loop):"
+                                      "value info has no element type specified")
+xfail_issue_38726 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "LessOrEqual")
+xfail_issue_38727 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "Einsum")
+xfail_issue_38728 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "Det")
+xfail_issue_38729 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "Compress")
+xfail_issue_38731 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "BitShift")
+xfail_issue_38732 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "ConvInteger")
+xfail_issue_38733 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "Celu")
+xfail_issue_38734 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "ai.onnx.preview.training.Adam")
+xfail_issue_38735 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "ai.onnx.preview.training.Adagrad")
+xfail_issue_38736 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
+                                      "NegativeLogLikelihoodLoss")
 
 # Model Zoo issues:
 xfail_issue_36533 = xfail_test(reason="AssertionError: zoo models results mismatch")
