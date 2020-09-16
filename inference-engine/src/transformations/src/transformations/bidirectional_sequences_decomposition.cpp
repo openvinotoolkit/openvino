d43c99b95ff0f334b269805e74bc5548dc6e07d5
Ivan Tikhonov <ivan.tikhonov@intel.com>
2020-09-16 10:49:40 +0300

    introduce new opset5, include GRU/RNN/LSTM Sequences to opset5

--- inference-engine/src/transformations/src/transformations/bidirectional_sequences_decomposition.cpp
+++ inference-engine/src/transformations/src/transformations/bidirectional_sequences_decomposition.cpp
@@ -7,26 +7,27 @@
 #include <memory>
 
 #include <ngraph/opsets/opset4.hpp>
+#include <ngraph/opsets/opset5.hpp>
 #include <ngraph/rt_info.hpp>
 #include <ngraph/pattern/op/wrap_type.hpp>
 
 ngraph::pass::BidirectionalLSTMSequenceDecomposition::BidirectionalLSTMSequenceDecomposition() {
-    auto lstm_sequence_ngraph = ngraph::pattern::wrap_type<ngraph::op::v5::LSTMSequence>();
+    auto lstm_sequence_ngraph = ngraph::pattern::wrap_type<ngraph::opset5::LSTMSequence>();
 
     ngraph::matcher_pass_callback callback = [](pattern::Matcher &m) {
-        auto lstm_sequence = std::dynamic_pointer_cast<ngraph::op::v5::LSTMSequence>(m.get_match_root());
+        auto lstm_sequence = std::dynamic_pointer_cast<ngraph::opset5::LSTMSequence>(m.get_match_root());
         if (!lstm_sequence) {
             return false;
         }
 
-        auto axis_0 = ngraph::opset4::Constant::create(element::i64, Shape{}, {0});
-        auto axis_1 = ngraph::opset4::Constant::create(element::i64, Shape{}, {1});
-        auto H = std::make_shared<opset4::Split>(lstm_sequence->input_value(1), axis_1, 2);
-        auto C = std::make_shared<opset4::Split>(lstm_sequence->input_value(2), axis_1, 2);
-        auto W = std::make_shared<opset4::Split>(lstm_sequence->input_value(4), axis_0, 2);
-        auto R = std::make_shared<opset4::Split>(lstm_sequence->input_value(5), axis_0, 2);
-        auto B = std::make_shared<opset4::Split>(lstm_sequence->input_value(6), axis_0, 2);
-        auto lstm_sequence_forward = std::make_shared<ngraph::op::v5::LSTMSequence>(
+        auto axis_0 = ngraph::opset5::Constant::create(element::i64, Shape{}, {0});
+        auto axis_1 = ngraph::opset5::Constant::create(element::i64, Shape{}, {1});
+        auto H = std::make_shared<opset5::Split>(lstm_sequence->input_value(1), axis_1, 2);
+        auto C = std::make_shared<opset5::Split>(lstm_sequence->input_value(2), axis_1, 2);
+        auto W = std::make_shared<opset5::Split>(lstm_sequence->input_value(4), axis_0, 2);
+        auto R = std::make_shared<opset5::Split>(lstm_sequence->input_value(5), axis_0, 2);
+        auto B = std::make_shared<opset5::Split>(lstm_sequence->input_value(6), axis_0, 2);
+        auto lstm_sequence_forward = std::make_shared<ngraph::opset5::LSTMSequence>(
                 lstm_sequence->input_value(0),
                 H->output(0),
                 C->output(0),
@@ -41,7 +42,7 @@ ngraph::pass::BidirectionalLSTMSequenceDecomposition::BidirectionalLSTMSequenceD
                 lstm_sequence->get_activations(),
                 lstm_sequence->get_clip());
 
-        auto lstm_sequence_reverse = std::make_shared<ngraph::op::v5::LSTMSequence>(
+        auto lstm_sequence_reverse = std::make_shared<ngraph::opset5::LSTMSequence>(
                 lstm_sequence->input_value(0),
                 H->output(1),
                 C->output(1),
@@ -56,11 +57,11 @@ ngraph::pass::BidirectionalLSTMSequenceDecomposition::BidirectionalLSTMSequenceD
                 lstm_sequence->get_activations(),
                 lstm_sequence->get_clip());
 
-        auto concat_0 = std::make_shared<opset4::Concat>(OutputVector{lstm_sequence_forward->output(0),
+        auto concat_0 = std::make_shared<opset5::Concat>(OutputVector{lstm_sequence_forward->output(0),
                                                           lstm_sequence_reverse->output(0)}, 1);
-        auto concat_1 = std::make_shared<opset4::Concat>(OutputVector{lstm_sequence_forward->output(1),
+        auto concat_1 = std::make_shared<opset5::Concat>(OutputVector{lstm_sequence_forward->output(1),
                                                           lstm_sequence_reverse->output(1)}, 1);
-        auto concat_2 = std::make_shared<opset4::Concat>(OutputVector{lstm_sequence_forward->output(2),
+        auto concat_2 = std::make_shared<opset5::Concat>(OutputVector{lstm_sequence_forward->output(2),
                                                           lstm_sequence_reverse->output(2)}, 1);
         ngraph::copy_runtime_info(lstm_sequence, {H, C, W, R, B, lstm_sequence_forward, lstm_sequence_reverse,
                                                   concat_0, concat_1, concat_2});
@@ -76,21 +77,21 @@ ngraph::pass::BidirectionalLSTMSequenceDecomposition::BidirectionalLSTMSequenceD
 }
 
 ngraph::pass::BidirectionalGRUSequenceDecomposition::BidirectionalGRUSequenceDecomposition() {
-    auto gru_sequence_ngraph = ngraph::pattern::wrap_type<ngraph::op::v5::GRUSequence>();
+    auto gru_sequence_ngraph = ngraph::pattern::wrap_type<ngraph::opset5::GRUSequence>();
 
     ngraph::matcher_pass_callback callback = [](pattern::Matcher &m) {
-        auto gru_sequence = std::dynamic_pointer_cast<ngraph::op::v5::GRUSequence>(m.get_match_root());
+        auto gru_sequence = std::dynamic_pointer_cast<ngraph::opset5::GRUSequence>(m.get_match_root());
         if (!gru_sequence) {
             return false;
         }
 
-        auto axis_0 = ngraph::opset4::Constant::create(element::i64, Shape{}, {0});
-        auto axis_1 = ngraph::opset4::Constant::create(element::i64, Shape{}, {1});
-        auto H = std::make_shared<opset4::Split>(gru_sequence->input_value(1), axis_1, 2);
-        auto W = std::make_shared<opset4::Split>(gru_sequence->input_value(3), axis_0, 2);
-        auto R = std::make_shared<opset4::Split>(gru_sequence->input_value(4), axis_0, 2);
-        auto B = std::make_shared<opset4::Split>(gru_sequence->input_value(5), axis_0, 2);
-        auto gru_sequence_forward = std::make_shared<ngraph::op::v5::GRUSequence>(
+        auto axis_0 = ngraph::opset5::Constant::create(element::i64, Shape{}, {0});
+        auto axis_1 = ngraph::opset5::Constant::create(element::i64, Shape{}, {1});
+        auto H = std::make_shared<opset5::Split>(gru_sequence->input_value(1), axis_1, 2);
+        auto W = std::make_shared<opset5::Split>(gru_sequence->input_value(3), axis_0, 2);
+        auto R = std::make_shared<opset5::Split>(gru_sequence->input_value(4), axis_0, 2);
+        auto B = std::make_shared<opset5::Split>(gru_sequence->input_value(5), axis_0, 2);
+        auto gru_sequence_forward = std::make_shared<ngraph::opset5::GRUSequence>(
                 gru_sequence->input_value(0),
                 H->output(0),
                 gru_sequence->input_value(2),
@@ -105,7 +106,7 @@ ngraph::pass::BidirectionalGRUSequenceDecomposition::BidirectionalGRUSequenceDec
                 gru_sequence->get_clip(),
                 gru_sequence->get_linear_before_reset());
 
-        auto gru_sequence_reverse = std::make_shared<ngraph::op::v5::GRUSequence>(
+        auto gru_sequence_reverse = std::make_shared<ngraph::opset5::GRUSequence>(
                 gru_sequence->input_value(0),
                 H->output(1),
                 gru_sequence->input_value(2),
@@ -120,9 +121,9 @@ ngraph::pass::BidirectionalGRUSequenceDecomposition::BidirectionalGRUSequenceDec
                 gru_sequence->get_clip(),
                 gru_sequence->get_linear_before_reset());
 
-        auto concat_0 = std::make_shared<opset4::Concat>(OutputVector{gru_sequence_forward->output(0),
+        auto concat_0 = std::make_shared<opset5::Concat>(OutputVector{gru_sequence_forward->output(0),
                                                                       gru_sequence_reverse->output(0)}, 1);
-        auto concat_1 = std::make_shared<opset4::Concat>(OutputVector{gru_sequence_forward->output(1),
+        auto concat_1 = std::make_shared<opset5::Concat>(OutputVector{gru_sequence_forward->output(1),
                                                                       gru_sequence_reverse->output(1)}, 1);
         ngraph::copy_runtime_info(gru_sequence, {H, W, R, B, gru_sequence_forward, gru_sequence_reverse,
                                                   concat_0, concat_1});
@@ -137,21 +138,21 @@ ngraph::pass::BidirectionalGRUSequenceDecomposition::BidirectionalGRUSequenceDec
 }
 
 ngraph::pass::BidirectionalRNNSequenceDecomposition::BidirectionalRNNSequenceDecomposition() {
-    auto rnn_sequence_ngraph = ngraph::pattern::wrap_type<ngraph::op::v5::RNNSequence>();
+    auto rnn_sequence_ngraph = ngraph::pattern::wrap_type<ngraph::opset5::RNNSequence>();
 
     ngraph::matcher_pass_callback callback = [](pattern::Matcher &m) {
-        auto rnn_sequence = std::dynamic_pointer_cast<ngraph::op::v5::RNNSequence>(m.get_match_root());
+        auto rnn_sequence = std::dynamic_pointer_cast<ngraph::opset5::RNNSequence>(m.get_match_root());
         if (!rnn_sequence) {
             return false;
         }
 
-        auto axis_0 = ngraph::opset4::Constant::create(element::i64, Shape{}, {0});
-        auto axis_1 = ngraph::opset4::Constant::create(element::i64, Shape{}, {1});
-        auto H = std::make_shared<opset4::Split>(rnn_sequence->input_value(1), axis_1, 2);
-        auto W = std::make_shared<opset4::Split>(rnn_sequence->input_value(3), axis_0, 2);
-        auto R = std::make_shared<opset4::Split>(rnn_sequence->input_value(4), axis_0, 2);
-        auto B = std::make_shared<opset4::Split>(rnn_sequence->input_value(5), axis_0, 2);
-        auto rnn_sequence_forward = std::make_shared<ngraph::op::v5::RNNSequence>(
+        auto axis_0 = ngraph::opset5::Constant::create(element::i64, Shape{}, {0});
+        auto axis_1 = ngraph::opset5::Constant::create(element::i64, Shape{}, {1});
+        auto H = std::make_shared<opset5::Split>(rnn_sequence->input_value(1), axis_1, 2);
+        auto W = std::make_shared<opset5::Split>(rnn_sequence->input_value(3), axis_0, 2);
+        auto R = std::make_shared<opset5::Split>(rnn_sequence->input_value(4), axis_0, 2);
+        auto B = std::make_shared<opset5::Split>(rnn_sequence->input_value(5), axis_0, 2);
+        auto rnn_sequence_forward = std::make_shared<ngraph::opset5::RNNSequence>(
                 rnn_sequence->input_value(0),
                 H->output(0),
                 rnn_sequence->input_value(2),
@@ -165,7 +166,7 @@ ngraph::pass::BidirectionalRNNSequenceDecomposition::BidirectionalRNNSequenceDec
                 rnn_sequence->get_activations_beta(),
                 rnn_sequence->get_clip());
 
-        auto rnn_sequence_reverse = std::make_shared<ngraph::op::v5::RNNSequence>(
+        auto rnn_sequence_reverse = std::make_shared<ngraph::opset5::RNNSequence>(
                 rnn_sequence->input_value(0),
                 H->output(1),
                 rnn_sequence->input_value(2),
@@ -179,9 +180,9 @@ ngraph::pass::BidirectionalRNNSequenceDecomposition::BidirectionalRNNSequenceDec
                 rnn_sequence->get_activations_beta(),
                 rnn_sequence->get_clip());
 
-        auto concat_0 = std::make_shared<opset4::Concat>(OutputVector{rnn_sequence_forward->output(0),
+        auto concat_0 = std::make_shared<opset5::Concat>(OutputVector{rnn_sequence_forward->output(0),
                                                                       rnn_sequence_reverse->output(0)}, 1);
-        auto concat_1 = std::make_shared<opset4::Concat>(OutputVector{rnn_sequence_forward->output(1),
+        auto concat_1 = std::make_shared<opset5::Concat>(OutputVector{rnn_sequence_forward->output(1),
                                                                       rnn_sequence_reverse->output(1)}, 1);
         ngraph::copy_runtime_info(rnn_sequence, {H, W, R, B, rnn_sequence_forward, rnn_sequence_reverse,
                                                  concat_0, concat_1});
