db9f0ec930558a2a4f880bc54bc5edcaa819a50c
Eugene Smirnov <eugene.smirnov@intel.com>
2020-09-21 14:22:14 +0300

    [GNA] fake quantize single layer tests for GNA plugin (#2060)
    * fake quantize single layer test for GNA plugin
    
    * implemented fakequantize for fp32 case as an activation function
    
    * added proper seed randomisation within single test run
    
    * [GNA] [FAKEQUANTIZE] fixed ref-fp32 implementation on GNA to use nearbyint instead of roundf
    
    * [GNA] [FAKEQUANTIZE] restored random seed
    
    * [GNA][FAKEQUANTIZE] disabled 4d and integer tests for FakeQuantize
    
    * [GNA][FAKEQUANTIZE]updated ngraph FakeQuantize builder to accept seed
    
    * [GNA][FAKEQUANTIZE]aligned FP calculations order on GNA with reference ngraph - this however gives more error
    
    * [CPU]build of FakeQuantise tests restored
    
    * [TESTS][FAKEQUANTIZE] ignore extra inferRequests for disabled tests
    
    * [GNA] Fixed legacy unit test failuers appeared due to extra check for possible segfault in import frames
    
    * [GNA] adopted fuse multiple identities for FakeQunatize layer
    
    * [GNA]fp32 runtime code review

--- inference-engine/tests/ngraph_functions/src/fake_quantize.cpp
+++ inference-engine/tests/ngraph_functions/src/fake_quantize.cpp
@@ -32,18 +32,19 @@ std::shared_ptr<Node> makeFakeQuantize(const ngraph::Output<Node> &in,
 std::shared_ptr<ngraph::Node> makeFakeQuantize(const ngraph::Output<ngraph::Node> &in,
                                                const ngraph::element::Type &type,
                                                std::size_t levels,
-                                               std::vector<size_t> constShapes) {
+                                               std::vector<size_t> constShapes,
+                                               const int32_t  seed) {
     size_t constDataSize = ngraph::shape_size(constShapes);
     std::vector<float> inputLowData, inputHighData, outputLowData, outputHighData;
-    inputLowData = NGraphFunctions::Utils::generateVector<ngraph::element::Type_t::f32>(constDataSize);
+    inputLowData = NGraphFunctions::Utils::generateVector<ngraph::element::Type_t::f32>(constDataSize, 10, 1, seed);
     if (levels != 2) {
-        inputHighData = NGraphFunctions::Utils::generateVector<ngraph::element::Type_t::f32>(constDataSize);
-        outputLowData = NGraphFunctions::Utils::generateVector<ngraph::element::Type_t::f32>(constDataSize);
-        outputHighData = NGraphFunctions::Utils::generateVector<ngraph::element::Type_t::f32>(constDataSize);
+        inputHighData = NGraphFunctions::Utils::generateVector<ngraph::element::Type_t::f32>(constDataSize, 10, 1, seed);
+        outputLowData = NGraphFunctions::Utils::generateVector<ngraph::element::Type_t::f32>(constDataSize, 10, 1, seed);
+        outputHighData = NGraphFunctions::Utils::generateVector<ngraph::element::Type_t::f32>(constDataSize, 10, 1, seed);
     } else {
         inputHighData = inputLowData;
-        outputLowData = NGraphFunctions::Utils::generateVector<ngraph::element::Type_t::f32>(constDataSize);
-        outputHighData = NGraphFunctions::Utils::generateVector<ngraph::element::Type_t::f32>(constDataSize);
+        outputLowData = NGraphFunctions::Utils::generateVector<ngraph::element::Type_t::f32>(constDataSize, 10, 1, seed);
+        outputHighData = NGraphFunctions::Utils::generateVector<ngraph::element::Type_t::f32>(constDataSize, 10, 1, seed);
 
         for (int i = 0; i < constDataSize; i++) {
             if (outputLowData[i] > outputHighData[i]) {
@@ -70,10 +71,10 @@ std::shared_ptr<ngraph::Node> makeFakeQuantize(const ngraph::Output<ngraph::Node
             outputHighData[i] += 1;
     }
 
-    auto inputLowNode = ngraph::builder::makeConstant(type, constShapes, inputLowData, inputLowData.empty());
-    auto inputHighNode = ngraph::builder::makeConstant(type, constShapes, inputHighData, inputHighData.empty());
-    auto outputLowNode = ngraph::builder::makeConstant(type, constShapes, outputLowData, outputLowData.empty());
-    auto outputHighNode = ngraph::builder::makeConstant(type, constShapes, outputHighData, outputHighData.empty());
+    auto inputLowNode = ngraph::builder::makeConstant(type, constShapes, inputLowData, inputLowData.empty(), seed);
+    auto inputHighNode = ngraph::builder::makeConstant(type, constShapes, inputHighData, inputHighData.empty(), seed);
+    auto outputLowNode = ngraph::builder::makeConstant(type, constShapes, outputLowData, outputLowData.empty(), seed);
+    auto outputHighNode = ngraph::builder::makeConstant(type, constShapes, outputHighData, outputHighData.empty(), seed);
 
     auto fq = std::make_shared<ngraph::opset1::FakeQuantize>(in, inputLowNode, inputHighNode, outputLowNode, outputHighNode, levels);
 
