7adf1c766a6205bc027cc6ac658cb0fd9396fbb5
Mikhail Treskin <mikhail.treskin@intel.com>
2020-09-23 14:06:34 +0300

    Add fake quantize reference

---
+++ ngraph/core/reference/include/ngraph/runtime/reference/fake_quantize.hpp
@@ -0,0 +1,184 @@
+//*****************************************************************************
+// Copyright 2017-2020 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+
+
+#pragma once
+
+#include <cmath>
+#include <cstddef>
+#include <vector>
+#include <numeric>
+#include <utility>
+
+
+#include "ngraph/shape.hpp"
+
+namespace ngraph {
+    namespace runtime {
+        namespace reference {
+            std::vector<size_t> calc_broadcast_index_offset(const std::vector<size_t> &memory_offsets,
+                                                            const std::vector<size_t> &broadcast_shape) {
+                std::vector<size_t> left_broadcastable_dims;
+                std::vector<size_t> right_broadcastable_dims;
+                for (size_t i = 0; i < broadcast_shape.size(); ++i) {
+                    if (broadcast_shape[i] == 1) {
+                        left_broadcastable_dims.push_back(i);
+                    } else {
+                        break;
+                    }
+                }
+                for (size_t i = broadcast_shape.size(); i >= 0; ++i) {
+                    if (broadcast_shape[i] == 1) {
+                        right_broadcastable_dims.push_back(i);
+                    } else {
+                        break;
+                    }
+                }
+                std::vector<size_t> broadcast_offsets;
+                for (size_t &i : left_broadcastable_dims) {
+                    broadcast_offsets[i] = memory_offsets[i];
+                }
+                // TODO: handle right_bradcastable_dims properly
+//                for (size_t &i : right_broadcastable_dims) {
+//                    broadcast_offsets[i] = memory_offsets[i];
+//                }
+                size_t right_bound = right_broadcastable_dims.empty() ? broadcast_shape.size() :
+                                     right_broadcastable_dims.back();
+                for (size_t i = left_broadcastable_dims.back() + 1; i < right_bound; ++i) {
+                    if (broadcast_shape[i] == 1) {
+                        broadcast_offsets[i] = memory_offsets[i];
+                    } else {
+                        broadcast_offsets[i] = std::accumulate(broadcast_offsets.begin() + i,
+                                                               broadcast_offsets.begin() + right_bound, 0,
+                                                               std::plus<size_t>());
+                    }
+                }
+                return broadcast_offsets;
+            }
+
+            size_t calc_full_broadcast_offset(const std::vector<size_t> &current_dims,
+                                              const std::vector<size_t> &offsets) {
+                size_t full_index_offset = 0;
+                for (size_t i = 0; i < current_dims.size(); ++i) {
+                    full_index_offset += offsets[i] * current_dims[i];
+                }
+                return full_index_offset;
+            }
+
+            void align_shape_sizes(Shape &shape, size_t target_size) {
+                for (size_t i = 0; i < shape.size() - target_size; ++i) {
+                    shape.insert(shape.begin(), 1);
+                }
+            }
+
+            void increment_current_dim(std::vector<size_t> &current_dims,
+                                       const std::vector<size_t> &shape,
+                                       size_t incremented_dim_number) {
+                current_dims[incremented_dim_number] += 1;
+                if (current_dims[incremented_dim_number] == shape[incremented_dim_number] and
+                    incremented_dim_number != 0) {
+                    for (size_t i = incremented_dim_number; i < shape.size(); ++i) {
+                        current_dims[i] = 0;
+                    }
+                    incremented_dim_number(current_dims, incremented_dim_number - 1, shape);
+                }
+
+            }
+
+            template<typename T>
+            void fake_quantize(const T *arg,
+                               T *out,
+                               T *in_low,
+                               T *in_high,
+                               T *out_low,
+                               T *out_high,
+                               Shape &arg_shape,
+                               Shape &in_low_shape,
+                               Shape &in_high_shape,
+                               Shape &out_low_shape,
+                               Shape &out_high_shape,
+                               size_t levels
+            ) {
+                std::vector<size_t> arg_memory_offsets(arg_shape.size(), 0);
+                for (size_t i = arg_shape.size() - 1; i >= 0; --i) {
+                    arg_memory_offsets[i] = std::accumulate(arg_shape.begin() + i, arg_shape.end(), 1,
+                                                            std::multiplies<size_t>());
+                }
+                align_shape_sizes(in_low_shape, arg_shape.size());
+                align_shape_sizes(in_high_shape, arg_shape.size());
+                align_shape_sizes(out_low_shape, arg_shape.size());
+                align_shape_sizes(out_high_shape, arg_shape.size());
+
+                std::vector<size_t> in_low_offsets, in_high_offsets, out_low_offsets;
+                std::vector<size_t> out_high_offsets = calc_broadcast_index_offset(arg_memory_offsets, out_high_shape);
+                bool in_low_trivial_broadcast, in_high_trivial_broadcast,
+                        out_low_trivial_broadcast, out_high_trivial_broadcast = false;
+                bool in_low_aligned, in_high_aligned, out_low_aligned, out_high_aligned = false;
+
+                auto check_trivial_broadcast = [&arg_shape, &arg_memory_offsets](Shape &shape_to_check,
+                                                                                 std::vector<size_t> &target_offsets,
+                                                                                 bool &trivial_broadcast,
+                                                                                 bool &aligned) {
+                    if (shape_size(shape_to_check) == 1 || shape_size(shape_to_check) == 0) {
+                        trivial_broadcast = true;
+                    } else if (shape_to_check == arg_shape) {
+                        aligned = true;
+                    } else {
+                        target_offsets = calc_broadcast_index_offset(arg_memory_offsets, shape_to_check);
+                    }
+                };
+                check_trivial_broadcast(in_low_shape, in_low_offsets, in_low_trivial_broadcast, in_low_aligned);
+                check_trivial_broadcast(in_high_shape, in_high_offsets, in_high_trivial_broadcast, in_high_aligned);
+                check_trivial_broadcast(out_low_shape, out_low_offsets, out_low_trivial_broadcast, out_low_aligned);
+                check_trivial_broadcast(out_high_shape, out_high_offsets, out_high_trivial_broadcast, out_high_aligned);
+
+                std::vector<size_t> current_dim(arg_shape.size(), 0);
+
+
+                auto get_value = [&current_dim](bool is_trivial_broadcast, bool is_aligned, T *data, size_t idx,
+                                                const std::vector<size_t> &offsets) {
+                    T val;
+                    if (is_aligned) {
+                        val = data[idx];
+                    } else if (is_trivial_broadcast) {
+                        val = data[0];
+                    } else {
+                        size_t index_offset = calc_full_broadcast_offset(current_dim, offsets);
+                        NGRAPH_CHECK(index_offset >= 0, "Incorrect index offset value!");
+                        val = data[idx - index_offset];
+                    }
+                    return val;
+                };
+                for (size_t i = 0; i < shape_size(arg_shape); ++i) {
+                    T in_low_val = get_value(in_low_trivial_broadcast, in_low_aligned, in_low, in_low_offsets);
+                    T in_high_val = get_value(in_high_trivial_broadcast, in_high_aligned, in_high, in_high_offsets);
+                    T out_low_val = get_value(out_low_trivial_broadcast, out_low_aligned, out_low, out_low_offsets);
+                    T out_high_val = get_value(out_high_trivial_broadcast, out_high_aligned, out_high, out_high_offsets);
+
+                    if (arg[i] <= in_low_val) {
+                        out[i] = std::roundf(out_low_val);
+                    } else if (arg[i] > in_high[i]) {
+                        out[i] = std::roundf(out_high_val);
+                    } else {
+                        const T value = std::roundf((arg[i] - in_low_val) / (in_high_val - in_low_val) * levels) /
+                                            levels * (out_high_val - out_low_val) + out_low_val;
+                        out[i] = std::roundf(value);
+                    }
+                }
+            }
+        }
+    }
+}
