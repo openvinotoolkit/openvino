976fc234215b83fc41710ad2b1b66e717dee77ca
DariaMityagina <daria.mityagina@intel.com>
2020-09-24 19:20:04 +0300

    [IE MyriadX] Add new operation Interpolate. Removing some methods (these will be in the mdk part)

--- inference-engine/src/vpu/graph_transformer/src/stages/Interpolate.cpp
+++ inference-engine/src/vpu/graph_transformer/src/stages/Interpolate.cpp
@@ -42,130 +42,119 @@ enum class InterpolateNearestMode {
     simple
 };
 
-namespace {
 class InterpolateStage final : public StageNode {
 public:
     using StageNode::StageNode;
 
-    InterpolateStage(const InferenceEngine::CNNLayerPtr& layer);
-    ~InterpolateStage() = default;
+    // struct InterpolateAttrs
+    // {
+    //     // specifies type of interpolation
+    //     // one of `nearest`, `linear`, `linear_onnx`, `cubic` Required.
+    //     InterpolateMode mode;
+    //     // specifies which input, sizes or scales, is used to calculate an output shape.
+    //     // one of `sizes`, `scales` Required
+    //     InterpolateShapeCalcMode shape_calculation_mode;
+    //     // specifies how to transform the coordinate in the resized tensor to the
+    //     // coordinate in the original tensor. one of `half_pixel`, `pytorch_half_pixel`,
+    //     // `asymmetric`, `tf_half_pixel_for_nn`, `align_corners`
+    //     InterpolateCoordTransMode coordinate_transformation_mode;
+    //     // specifies round mode when `mode == nearest` and is used only when `mode ==
+    //     // nearest`. one of `round_prefer_floor`, `round_prefer_ceil`, `floor`, `ceil`,
+    //     // `simple`
+    //     InterpolateNearestMode nearest_mode;
+    //     // a flag that specifies whether to perform anti-aliasing. default is `false`
+    //     bool antialias;
+    //     // specify the number of pixels to add to the beginning of the image being
+    //     // interpolated.
+    //     std::vector<size_t> pads_begin;
+    //     // specify the number of pixels to add to the end of the image being
+    //     // interpolated.
+    //     std::vector<size_t> pads_end;
+    //     // specifies the parameter *a* for cubic interpolation
+    //     // used only when `mode == cubic`
+    //     double cube_coeff;
 
-    struct InterpolateAttrs
-    {
-        // specifies type of interpolation
-        // one of `nearest`, `linear`, `linear_onnx`, `cubic` Required.
-        InterpolateMode mode;
-        // specifies which input, sizes or scales, is used to calculate an output shape.
-        // one of `sizes`, `scales` Required
-        InterpolateShapeCalcMode shape_calculation_mode;
-        // specifies how to transform the coordinate in the resized tensor to the
-        // coordinate in the original tensor. one of `half_pixel`, `pytorch_half_pixel`,
-        // `asymmetric`, `tf_half_pixel_for_nn`, `align_corners`
-        InterpolateCoordTransMode coordinate_transformation_mode;
-        // specifies round mode when `mode == nearest` and is used only when `mode ==
-        // nearest`. one of `round_prefer_floor`, `round_prefer_ceil`, `floor`, `ceil`,
-        // `simple`
-        InterpolateNearestMode nearest_mode;
-        // a flag that specifies whether to perform anti-aliasing. default is `false`
-        bool antialias;
-        // specify the number of pixels to add to the beginning of the image being
-        // interpolated.
-        std::vector<size_t> pads_begin;
-        // specify the number of pixels to add to the end of the image being
-        // interpolated.
-        std::vector<size_t> pads_end;
-        // specifies the parameter *a* for cubic interpolation
-        // used only when `mode == cubic`
-        double cube_coeff;
+    //     InterpolateAttrs(InterpolateMode mode,
+    //                      InterpolateShapeCalcMode shape_calculation_mode,
+    //                      InterpolateCoordTransMode coordinate_transformation_mode =
+    //                         InterpolateCoordTransMode::half_pixel,
+    //                      InterpolateNearestMode nearest_mode = InterpolateNearestMode::round_prefer_floor,
+    //                      bool antialias = false,
+    //                      std::vector<size_t> pads_begin = {0},
+    //                      std::vector<size_t> pads_end = {0},
+    //                      double cube_coeff = -0.75)
+    //         : mode(mode)
+    //         , shape_calculation_mode(shape_calculation_mode)
+    //         , coordinate_transformation_mode(coordinate_transformation_mode)
+    //         , nearest_mode(nearest_mode)
+    //         , antialias(antialias)
+    //         , pads_begin(pads_begin)
+    //         , pads_end(pads_end)
+    //         , cube_coeff(cube_coeff) {}
 
-        InterpolateAttrs(InterpolateMode mode,
-                         InterpolateShapeCalcMode shape_calculation_mode,
-                         InterpolateCoordTransMode coordinate_transformation_mode =
-                            InterpolateCoordTransMode::half_pixel,
-                         InterpolateNearestMode nearest_mode = InterpolateNearestMode::round_prefer_floor,
-                         bool antialias = false,
-                         std::vector<size_t> pads_begin = {0},
-                         std::vector<size_t> pads_end = {0},
-                         double cube_coeff = -0.75)
-            : mode(mode)
-            , shape_calculation_mode(shape_calculation_mode)
-            , coordinate_transformation_mode(coordinate_transformation_mode)
-            , nearest_mode(nearest_mode)
-            , antialias(antialias)
-            , pads_begin(pads_begin)
-            , pads_end(pads_end)
-            , cube_coeff(cube_coeff) {
-
-        }
-
-        InterpolateAttrs()
-            : mode(InterpolateMode::nearest)
-            , coordinate_transformation_mode(InterpolateCoordTransMode::half_pixel)
-            , nearest_mode(InterpolateNearestMode::round_prefer_floor)
-            , antialias(false)
-            , cube_coeff(-0.75f) {
-
-        }
-    } m_attrs;
-
-    void Interpolate();
-
-    // Without 'axes' input.
-    // image - Input image
-    // output_shape - Output shape of spatial axes
-    // scales - Scales of spatial axes, i.e. output_shape / input_shape
-    // attrs - Interpolation attributes
-    void Interpolate(const DataVector& image,
-                const DataVector& output_shape,
-                const DataVector& scales,
-                const InterpolateAttrs& attrs);
-
-    // With 'axes' input.
-    // image - Input image
-    // output_shape - Output shape of spatial axes
-    // scales - Scales of spatial axes, i.e. output_shape / input_shape
-    // axes - Interpolation axes
-    // attrs - Interpolation attributes
-    void Interpolate(const DataVector& image,
-                const DataVector& output_shape,
-                const DataVector& scales,
-                const DataVector& axes,
-                const InterpolateAttrs& attrs);
+    //     InterpolateAttrs()
+    //         : mode(InterpolateMode::nearest)
+    //         , coordinate_transformation_mode(InterpolateCoordTransMode::half_pixel)
+    //         , nearest_mode(InterpolateNearestMode::round_prefer_floor)
+    //         , antialias(false)
+    //         , cube_coeff(-0.75f) {}
+    // } m_attrs;
 
     const InterpolateAttrs& get_attrs() const { return m_attrs; }
 private:
-    void execute();
-    // nearest neighbor
-    void nearestNeighbor(const uint8_t *in_ptr_, uint8_t *out_ptr_, int batch, int channel, int ID, int IH, int IW,
-                         float fx, float fy, float fz, int OD, int OH, int OW);
-    void nearestNeighborReference(const uint8_t *in_ptr_, uint8_t *out_ptr_, int batch, int channel, int ID, int IH, int IW,
-                         float fx, float fy, float fz, int OD, int OH, int OW);
+    StagePtr cloneImpl() const override {
+        return std::make_shared<InterpolateStage>(*this);
+    }
 
-    // onnx linear
-    void linearOnnx(const uint8_t *in_ptr_, uint8_t *out_ptr_, int batch, int channel, int IH, int IW,
-                    float fx, float fy, int OH, int OW);
-    void linearOnnxReference(const uint8_t *in_ptr_, uint8_t *out_ptr_, int batch, int channel, int IH, int IW,
-                    float fx, float fy, int OH, int OW);
+    void propagateDataOrderImpl(StageDataInfo<DimsOrder>& orderInfo) override {
+        auto input = inputEdge(0)->input();
 
-    // linear
-    void linearInterpolation(const uint8_t *in_ptr_, uint8_t *out_ptr_, int batch, int channel, int ID, int IH, int IW,
-                    float fx, float fy, float fz, int OD, int OH, int OW, int kernel_width, bool antialias);
-    void linearReference(const uint8_t *in_ptr_, uint8_t *out_ptr_, int batch, int channelC, int ID, int IH, int IW,
-                    float fx, float fy, float fz, int OD, int OH, int OW, int kernel_width, bool antialias);
+        orderInfo.setOutput(outputEdge(0), input->desc().dimsOrder());
+    }
 
-    // cubic
-    std::vector<float> getCubicCoef(float a);
-    void cubicInterpolation(const uint8_t *in_ptr_, uint8_t *out_ptr_, int batch, int channel, int IH, int IW,
-                    float fx, float fy, int OH, int OW, float a);
-    void cubicReference(const uint8_t *in_ptr_, uint8_t *out_ptr_, int batch, int channel, int IH, int IW,
-                    float fx, float fy, int OH, int OW, float a);
+    void getDataStridesRequirementsImpl(StageDataInfo<StridesRequirement>& stridesInfo) override {
+    }
 
-    int nearestMode(bool isDown, float originalValue);
+    void finalizeDataLayoutImpl() override {
+    }
 
-    std::vector<float> getScales();
-    std::vector<int> getAxes();
+    void getBatchSupportInfoImpl(StageDataInfo<BatchSupport>& batchInfo) override {
+        batchInfo.setInput(inputEdge(0), BatchSupport::Split);
+        batchInfo.setOutput(outputEdge(0), BatchSupport::Split);
+    }
+
+    void initialCheckImpl() const override {
+        assertInputsOutputsTypes(this, {{DataType::FP16}}, {{DataType::FP16}});
+    }
+
+    void serializeParamsImpl(BlobSerializer& serializer) const override {
+        auto factor = attrs().get<float>("factor");
+        auto antialias = attrs().get<bool>("antialias");
+        // auto axis = attrs().get<InterpolateAxis>("InterpolateAxis");
+        // auto scales = attrs().get<InterpolateScales>("InterpolateScales");
+        auto sampleType = attrs().get<InterpolateMode>("type");
+        auto sampleNearestMode = attrs().get<InterpolateNearestMode>("nearestMode");
+        auto sampleShapeCalcMode = attrs().get<InterpolateShapeCalcMode>("shapeCalcMode");
+        auto sampleCoordTransMode = attrs().get<InterpolateCoordTransMode>("coordTransMode");
+
+        serializer.append(static_cast<int32_t>(antialias));
+        serializer.append(static_cast<float>(factor));
+        serializer.append(static_cast<uint32_t>(sampleType));
+        serializer.append(static_cast<uint32_t>(sampleNearestMode));
+        serializer.append(static_cast<uint32_t>(sampleShapeCalcMode));
+        serializer.append(static_cast<uint32_t>(sampleCoordTransMode));
+    }
+
+    void serializeDataImpl(BlobSerializer& serializer) const override {
+        auto input = inputEdge(0)->input();
+        auto output = outputEdge(0)->output();
+
+        input->serializeBuffer(serializer);
+        output->serializeBuffer(serializer);
+    }
 
     InterpolateMode mode = InterpolateMode::nearest;
+    InterpolateShapeCalcMode shape_calc_mode = InterpolateShapeCalcMode::sizes;
     InterpolateCoordTransMode coordTransMode = InterpolateCoordTransMode::half_pixel;
     InterpolateNearestMode nearestMode       = InterpolateNearestMode::round_prefer_floor;
 
@@ -186,144 +175,6 @@ private:
     InferenceEngine::Precision inputPrecision, outputPrecision;
     size_t srcDataSize, dstDataSize;
 };
-// }}
-
-
-void InterpolateStage::Interpolate() {};
-
-void InterpolateStage::Interpolate(const DataVector& image,
-            const DataVector& output_shape,
-            const DataVector& scales,
-            const InterpolateAttrs& attrs) {
-    
-}
-
-void InterpolateStage::Interpolate(const DataVector& image,
-            const DataVector& output_shape,
-            const DataVector& scales,
-            const DataVector& axes,
-            const InterpolateAttrs& attrs) {
-    
-}
-
-void InterpolateStage::execute() {
-
-    switch (mode) {
-        case InterpolateMode::nearest: {
-            nearestNeighbor();
-            break;
-        }
-        case InterpolateMode::linear: {
-            linearInterpolation();
-            break;
-        }
-        case InterpolateMode::linear_onnx: {
-            linearOnnx();
-            break;
-        }
-        case InterpolateMode::cubic: {
-            cubicInterpolation();
-            break;
-        }
-        default: {
-            THROW_IE_EXCEPTION << "Interpolate layer has unsupported interpolate mode: " << mode;
-        }
-    }
-}
-
-// Idea1: Look ate different methods below and decide wich operation (resample\interp) to choose
-// according to the parameters. But isn't it the concept of convert?
-// Still have to find a way of getting rid of conversion (?) 
-
-// nearest neighbor
-void InterpolateStage::nearestNeighbor(const uint8_t *in_ptr_, uint8_t *out_ptr_, int batch, int channel, int ID, int IH, int IW,
-                                        float fx, float fy, float fz, int OD, int OH, int OW) {
-    
-}
-void InterpolateStage::nearestNeighborReference(const uint8_t *in_ptr_, uint8_t *out_ptr_, int batch, int channel, int ID, int IH, int IW,
-                                        float fx, float fy, float fz, int OD, int OH, int OW) {
-    
-}
-
-// linear
-void InterpolateStage::linearInterpolation(const uint8_t *in_ptr_, uint8_t *out_ptr_, int batch, int channel, int ID, int IH, int IW,
-                                        float fx, float fy, float fz, int OD, int OH, int OW, int kernel_width, bool antialias) {
-    
-}
-
-void InterpolateStage::linearReference(const uint8_t *in_ptr_, uint8_t *out_ptr_, int batch, int channel, int ID, int IH, int IW,
-                                        float fx, float fy, float fz, int OD, int OH, int OW, int kernel_width, bool antialias) {
-    
-}
-
-// onnx linear
-void InterpolateStage::linearOnnx(const uint8_t *in_ptr_, uint8_t *out_ptr_, int batch, int channel, int IH, int IW,
-                                   float fx, float fy, int OH, int OW) {
-    
-}
-void InterpolateStage::linearOnnxReference(const uint8_t *in_ptr_, uint8_t *out_ptr_, int batch, int channel, int IH, int IW,
-                                   float fx, float fy, int OH, int OW) {
-    
-}
-
-// cubic
-std::vector<float> InterpolateStage::getCubicCoef(float a) {
-    return std::vector<float>(0);
-}
-
-void InterpolateStage::cubicInterpolation(const uint8_t *in_ptr_, uint8_t *out_ptr_, int batch, int channel, int IH, int IW,
-                              float fx, float fy, int OH, int OW, float a) {
-    
-}
-
-void InterpolateStage::cubicReference(const uint8_t *in_ptr_, uint8_t *out_ptr_, int batch, int channel, int IH, int IW,
-                              float fx, float fy, int OH, int OW, float a) {
-    
-}
-
-int InterpolateStage::nearestMode(bool isDown, float originalValue) {
-    switch (nearestMode) {
-        case InterpolateNearestMode::round_prefer_floor: {
-            if (originalValue == (static_cast<int>(originalValue) + 0.5f)) {
-                return static_cast<int>(std::floor(originalValue));
-            } else {
-                return static_cast<int>(std::round(originalValue));
-            }
-            break;
-        }
-        case InterpolateNearestMode::round_prefer_ceil: {
-            return static_cast<int>(std::round(originalValue));
-            break;
-        }
-        case InterpolateNearestMode::floor: {
-            return static_cast<int>(std::floor(originalValue));
-            break;
-        }
-        case InterpolateNearestMode::ceil: {
-            return static_cast<int>(std::ceil(originalValue));
-            break;
-        }
-        case InterpolateNearestMode::simple: {
-            if (isDown) {
-                return static_cast<int>(std::ceil(originalValue));
-            } else {
-                return static_cast<int>(originalValue);
-            }
-        }
-        default: {
-            THROW_IE_EXCEPTION << "Interpolate layer with name '" << getName() << "' does not support this nearest round mode";
-        }
-    }
-}
-
-std::vector<float> InterpolateStage::getScales() {
-    return scales;
-}
-
-std::vector<int> InterpolateStage::getAxes() {
-    return axes;
-}
-}  // namespace
 
 Stage StageBuilder::addInterpolateStage(
         const Model& model,
