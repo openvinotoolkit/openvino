3f59f1c590de7e3553b8c9a7d37aa67c05a6c5c4
Vladislav Golubev <vladislav.golubev@intel.com>
2020-09-29 20:03:47 +0300

    [LPT] MulAddToScaleshiftOrPower: out precision fix & tests

--- inference-engine/src/transformations/src/transformations/convert_opset1_to_legacy/convert_mul_add_to_scaleshift_or_power.cpp
+++ inference-engine/src/transformations/src/transformations/convert_opset1_to_legacy/convert_mul_add_to_scaleshift_or_power.cpp
@@ -175,7 +175,8 @@ void ngraph::pass::ConvertMulAddToScaleShiftOrPower::convert_mul_add_to_scaleshi
                 new_ops.push_back(biases_in);
             }
 
-            auto scaleshift = std::make_shared<ngraph::op::ScaleShiftIE>(data_node, weights_in, biases_in, weights_in->get_element_type());
+            auto output_type = m.get_match_root()->get_output_element_type(0);
+            auto scaleshift = std::make_shared<ngraph::op::ScaleShiftIE>(data_node, weights_in, biases_in, output_type);
             new_ops.push_back(scaleshift);
 
             scaleshift->set_friendly_name(add_node->get_friendly_name());
@@ -190,7 +191,8 @@ void ngraph::pass::ConvertMulAddToScaleShiftOrPower::convert_mul_add_to_scaleshi
                 return false;
             }
 
-            auto power = std::make_shared<ngraph::op::PowerIE>(data_node, 1., scale, shift, const_weights_node->get_output_element_type(0));
+            auto output_type = m.get_match_root()->get_output_element_type(0);
+            auto power = std::make_shared<ngraph::op::PowerIE>(data_node, 1., scale, shift, output_type);
             power->set_friendly_name(add_node->get_friendly_name());
             ngraph::copy_runtime_info({mul_node, add_node}, power);
             ngraph::replace_node(m.get_match_root(), power);
