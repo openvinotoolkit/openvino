fc6308d32b07f6555a18dfcb909beb5cba640248
Zinoviev, Vladimir <vladimir.zinoviev@intel.com>
2020-09-30 17:34:03 +0300

    [LPT] Multiply constant folding test

--- inference-engine/tests/ngraph_functions/src/low_precision_transformations/multiply_function.cpp
+++ inference-engine/tests/ngraph_functions/src/low_precision_transformations/multiply_function.cpp
@@ -22,12 +22,20 @@ std::shared_ptr<ngraph::Function> MultiplyFunction::getOriginal(
     const bool& broadcast,
     const ngraph::pass::low_precision::LayerTransformation::Params& params,
     const MultiplyActualValues& actualValues,
-    const bool& constInput) {
-    const auto input1 = std::make_shared<ngraph::opset1::Parameter>(
-        actualValues.precision1,
-        broadcast ? ngraph::Shape({ inputShape[0], inputShape[1], 1, 1 }) : ngraph::Shape(inputShape));
-    std::shared_ptr<ngraph::Node> parent1 = input1;
-
+    const bool& constInput,
+    const bool& constantFolding) {
+    std::shared_ptr<ngraph::Node> parent1;
+    std::shared_ptr<ngraph::opset1::Parameter> input1;
+    if (constantFolding) {
+        const auto const1 = std::make_shared<ngraph::opset1::Constant>(
+                actualValues.precision1, Shape({ 1, actualValues.mutliplyValues1.size(), 1, 1 }), actualValues.mutliplyValues1);
+        parent1 = const1;
+    } else {
+        input1 = std::make_shared<ngraph::opset1::Parameter>(
+                actualValues.precision1,
+                broadcast ? ngraph::Shape({inputShape[0], inputShape[1], 1, 1}) : ngraph::Shape(inputShape));
+        parent1 = input1;
+    }
     const std::shared_ptr<ngraph::Node> convert1 = std::make_shared<DequantizationConvert>(parent1, precision);
     parent1 = convert1;
 
@@ -39,7 +47,7 @@ std::shared_ptr<ngraph::Function> MultiplyFunction::getOriginal(
         parent1 = subtract1;
     }
 
-    if (!actualValues.mutliplyValues1.empty()) {
+    if (!actualValues.mutliplyValues1.empty() && !constantFolding) {
         const std::shared_ptr<ngraph::Node> multiply1 = std::make_shared<DequantizationMultiply >(
             parent1,
             std::make_shared<ngraph::opset1::Constant>(
@@ -49,7 +57,7 @@ std::shared_ptr<ngraph::Function> MultiplyFunction::getOriginal(
 
     std::shared_ptr<ngraph::Node> parent2;
     std::shared_ptr<ngraph::opset1::Parameter> input2;
-    if (constInput) {
+    if (constInput || constantFolding) {
         const auto const2 = std::make_shared<ngraph::opset1::Constant>(
             actualValues.precision2, Shape({ actualValues.mutliplyValues2.size() }), actualValues.mutliplyValues2);
         parent2 = const2;
@@ -83,7 +91,11 @@ std::shared_ptr<ngraph::Function> MultiplyFunction::getOriginal(
     multiply->set_friendly_name("output");
 
     ngraph::ResultVector results{ std::make_shared<ngraph::opset1::Result>(multiply) };
-    const auto inputs = constInput ? ngraph::ParameterVector{ input1 }: ngraph::ParameterVector{ input1, input2 };
+    const auto inputs = constantFolding ?
+            ngraph::ParameterVector{ } :
+            constInput ?
+                ngraph::ParameterVector{ input1 } :
+                ngraph::ParameterVector{ input1, input2 };
 
     return std::make_shared<ngraph::Function>(results, inputs, "MultiplyTransformation");
 }
@@ -94,43 +106,56 @@ std::shared_ptr<ngraph::Function> MultiplyFunction::getReference(
     const bool& broadcast,
     const ngraph::pass::low_precision::LayerTransformation::Params& params,
     const MultiplyExpectedValues& expectedValues,
-    const bool& constInput) {
-    const auto input1 = std::make_shared<ngraph::opset1::Parameter>(
-        expectedValues.precision1,
-        broadcast ? ngraph::Shape({ inputShape[0], inputShape[1], 1, 1 }) : ngraph::Shape(inputShape));
+    const bool& constInput,
+    const bool& constantFolding) {
+    std::shared_ptr<ngraph::Node> parent1;
+    std::shared_ptr<ngraph::opset1::Parameter> input1;
+    if (constantFolding) {
+        const auto const1 = std::make_shared<ngraph::opset1::Constant>(
+                expectedValues.precision1, Shape({1, expectedValues.mutliplyValues1.size(), 1, 1}),
+                expectedValues.mutliplyValues1);
+        parent1 = const1;
 
-    std::shared_ptr<ngraph::Node> parent1 = input1;
-    //if (!(expectedValues.subtractValues1.empty() && expectedValues.mutliplyValues1.empty())) {
-    const std::shared_ptr<ngraph::Node> convert1 = std::make_shared<DequantizationConvert>(parent1, precision);
-    parent1 = convert1;
-    //}
+        const1->set_friendly_name("output");
+    } else {
+        input1 = std::make_shared<ngraph::opset1::Parameter>(
+                expectedValues.precision1,
+                broadcast ? ngraph::Shape({inputShape[0], inputShape[1], 1, 1}) : ngraph::Shape(inputShape));
+        parent1 = input1;
+
+        //if (!(expectedValues.subtractValues1.empty() && expectedValues.mutliplyValues1.empty())) {
+        const std::shared_ptr<ngraph::Node> convert1 = std::make_shared<DequantizationConvert>(parent1, precision);
+        parent1 = convert1;
+        //}
+    }
 
     if (!expectedValues.subtractValues1.empty()) {
         const std::shared_ptr<ngraph::Node> subtract1 = std::make_shared<DequantizationSubtract>(
-            parent1,
-            std::make_shared<ngraph::opset1::Constant>(
-                precision, Shape({ expectedValues.subtractValues1.size() }), expectedValues.subtractValues1));
+                parent1,
+                std::make_shared<ngraph::opset1::Constant>(
+                        precision, Shape({expectedValues.subtractValues1.size()}), expectedValues.subtractValues1));
         parent1 = subtract1;
     }
 
-    if (!expectedValues.mutliplyValues1.empty()) {
+    if (!expectedValues.mutliplyValues1.empty() && !constantFolding) {
         const std::shared_ptr<ngraph::Node> multiply1 = std::make_shared<DequantizationMultiply>(
-            parent1,
-            std::make_shared<ngraph::opset1::Constant>(
-                precision, Shape({ expectedValues.mutliplyValues1.size() }), expectedValues.mutliplyValues1));
+                parent1,
+                std::make_shared<ngraph::opset1::Constant>(
+                        precision, Shape({expectedValues.mutliplyValues1.size()}), expectedValues.mutliplyValues1));
         parent1 = multiply1;
     }
 
     std::shared_ptr<ngraph::Node> parent2;
     std::shared_ptr<ngraph::opset1::Parameter> input2;
-    if (constInput) {
+    if (constInput && !constantFolding) {
         const auto const2 = std::make_shared<ngraph::opset1::Constant>(
-            expectedValues.precision2, Shape({ expectedValues.mutliplyValues2.size() }), expectedValues.mutliplyValues2);
+                expectedValues.precision2, Shape({expectedValues.mutliplyValues2.size()}),
+                expectedValues.mutliplyValues2);
         parent2 = const2;
     } else {
         input2 = std::make_shared<ngraph::opset1::Parameter>(
-            expectedValues.precision2,
-            ngraph::Shape(inputShape));
+                expectedValues.precision2,
+                ngraph::Shape(inputShape));
         parent2 = input2;
 
         if (!(expectedValues.subtractValues2.empty() && expectedValues.mutliplyValues2.empty())) {
@@ -140,33 +165,39 @@ std::shared_ptr<ngraph::Function> MultiplyFunction::getReference(
 
         if (!expectedValues.subtractValues2.empty()) {
             const std::shared_ptr<ngraph::Node> subtract2 = std::make_shared<DequantizationSubtract>(
-                parent2,
-                std::make_shared<ngraph::opset1::Constant>(
-                    precision, Shape({ expectedValues.subtractValues2.size() }), expectedValues.subtractValues2));
+                    parent2,
+                    std::make_shared<ngraph::opset1::Constant>(
+                            precision, Shape({expectedValues.subtractValues2.size()}), expectedValues.subtractValues2));
             parent2 = subtract2;
         }
 
         if (!expectedValues.mutliplyValues2.empty()) {
             const std::shared_ptr<ngraph::Node> multiply2 = std::make_shared<DequantizationMultiply>(
-                parent2,
-                std::make_shared<ngraph::opset1::Constant>(
-                    precision, Shape({ expectedValues.mutliplyValues2.size() }), expectedValues.mutliplyValues2));
+                    parent2,
+                    std::make_shared<ngraph::opset1::Constant>(
+                            precision, Shape({expectedValues.mutliplyValues2.size()}), expectedValues.mutliplyValues2));
             parent2 = multiply2;
         }
     }
+    std::shared_ptr<ngraph::Node> multiply;
+    if (!constantFolding) {
+        auto multiplyOriginal = ngraph::opset1::Multiply(
+                ngraph::op::TemporaryReplaceOutputType(parent1, element::f32).get(),
+                ngraph::op::TemporaryReplaceOutputType(parent2, element::f32).get());
 
-    auto multiplyOriginal = ngraph::opset1::Multiply(
-        ngraph::op::TemporaryReplaceOutputType(parent1, element::f32).get(),
-        ngraph::op::TemporaryReplaceOutputType(parent2, element::f32).get());
+        multiply = std::make_shared<ngraph::op::TypeRelaxed<ngraph::opset1::Multiply>>(
+                multiplyOriginal,
+                std::vector<element::Type>{element::f32, element::f32},
+                std::vector<element::Type>{});
+        multiply->set_friendly_name("output");
+    }
 
-    std::shared_ptr<ngraph::Node> multiply = std::make_shared<ngraph::op::TypeRelaxed<ngraph::opset1::Multiply>>(
-        multiplyOriginal,
-        std::vector<element::Type>{ element::f32, element::f32 },
-        std::vector<element::Type>{});
-    multiply->set_friendly_name("output");
-
-    ngraph::ResultVector results{ std::make_shared<ngraph::opset1::Result>(multiply) };
-    const auto inputs = constInput ? ngraph::ParameterVector{ input1 } : ngraph::ParameterVector{ input1, input2 };
+    ngraph::ResultVector results{ constantFolding ? std::make_shared<ngraph::opset1::Result>(parent1) : std::make_shared<ngraph::opset1::Result>(multiply) };
+    const auto inputs = constantFolding ?
+                        ngraph::ParameterVector{ } :
+                        constInput ?
+                        ngraph::ParameterVector{ input1 } :
+                        ngraph::ParameterVector{ input1, input2 };
 
     return std::make_shared<ngraph::Function>(results, inputs, "MultiplyTransformation");
 }
