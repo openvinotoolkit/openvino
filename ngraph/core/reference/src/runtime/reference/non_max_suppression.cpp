f13d8b279e87a0d163988c925fe67927df93f135
VGavrilov <vladimir.gavrilov@intel.com>
2020-10-01 16:23:39 +0300

    Continued to write the function non_max_suppression().

--- ngraph/core/reference/src/runtime/reference/non_max_suppression.cpp
+++ ngraph/core/reference/src/runtime/reference/non_max_suppression.cpp
@@ -16,9 +16,10 @@
 
 
 #include <algorithm>
+#include <array>
 #include <cmath>
-#include <functional>
 #include <vector>
+#include <queue>
 #include "ngraph/op/non_max_suppression.hpp"
 #include "ngraph/runtime/reference/non_max_suppression.hpp"
 #include "ngraph/shape.hpp"
@@ -28,17 +29,24 @@ using namespace ngraph::runtime::reference;
 
 struct Rectangle
 {
-    float y1;
-    float x1;
-    float y2;
-    float x2;
+    Rectangle(float y_left, float x_left, float y_right, float x_right)
+        : y1{y_left}
+        , x1{x_left}
+        , y2{y_right}
+        , x2{x_right}
+        {
+        }
+
+    Rectangle() = default;
+
+    float y1 = 0.0f;
+    float x1 = 0.0f;
+    float y2 = 0.f;
+    float x2 = 0.0f;
 };
 
-static float intersectionOverUnion(float* boxesI, float* boxesJ)
+static float intersectionOverUnion(const Rectangle& boxI, const Rectangle& boxJ)
 {
-    Rectangle boxI = *(reinterpret_cast<Rectangle*>(boxesI));
-    Rectangle boxJ = *(reinterpret_cast<Rectangle*>(boxesJ));
-
     float areaI = (boxI.y2 - boxI.y1) * (boxI.x2 - boxI.x1);
     float areaJ = (boxJ.y2 - boxJ.y1) * (boxJ.x2 - boxJ.x1);
 
@@ -58,12 +66,61 @@ static float intersectionOverUnion(float* boxesI, float* boxesJ)
     return intersection_area / (areaI + areaJ - intersection_area);
 }
 
-struct FilteredBoxes
+struct SelectedIndex
 {
-    float score;
-    int batch_index;
-    int class_index;
-    int box_index;
+  SelectedIndex(int64_t batch_idx, int64_t class_idx, int64_t box_idx)
+      : batch_index(batch_idx)
+      , class_index(class_idx)
+      , box_index(box_idx)
+      {
+      }
+
+  SelectedIndex() = default;
+
+  int64_t batch_index = 0;
+  int64_t class_index = 0;
+  int64_t box_index = 0;
+};
+
+struct SelectedScore
+{
+  SelectedScore(float batch_idx, float class_idx, float score)
+      : batch_index{batch_idx}
+      , class_index{class_idx}
+      , box_score{score}
+      {
+      }
+
+  SelectedScore() = default;
+
+  float batch_index = 0.0f;
+  float class_index = 0.0f;
+  float box_score = 0.0f;
+};
+
+struct BoxInfo
+{
+    BoxInfo(const Rectangle& r, int64_t idx, float sc, int64_t suppress_idx)
+        : box{r}
+        , index{idx}
+        , score{sc}
+        , suppress_begin_index{suppress_idx}
+    {
+      area = (box.y2 - box.y1) * (box.x2 - box.x1);
+    }
+
+    BoxInfo() = default;
+
+    inline bool operator<(const BoxInfo& rhs) const
+    {
+        return score < rhs.score || (score == rhs.score && index > rhs.index);
+    }
+
+    Rectangle box;
+    int64_t index = 0;
+    float score = 0.0f;
+    int64_t suppress_begin_index = 0;
+    float area = 0.0f;
 };
 
 namespace ngraph
@@ -100,13 +157,89 @@ namespace ngraph
                 // scores shape: {num_batches, num_classes, num_boxes}
                 size_t num_batches = scores_data_shape[0];
                 size_t num_classes = scores_data_shape[1];
+                size_t num_boxes = boxes_data_shape[1];
 
-                std::vector<FilteredBoxes> fb;
+                SelectedIndex* selected_indices_ptr =
+                    reinterpret_cast<SelectedIndex*>(selected_indices);
+                SelectedScore* selected_scores_ptr =
+                    reinterpret_cast<SelectedScore*>(selected_scores);
 
-                for (int batch = 0; batch < num_batches; batch++)
+                size_t boxesStrides = num_boxes * 4;
+                std::array<size_t, 2> scoresStrides = {num_classes * num_boxes, num_boxes};
+
+                size_t boxes_per_class = static_cast<size_t>(max_output_boxes_per_class);
+
+                for (size_t batch = 0; batch < num_batches; batch++)
                 {
-                    for (int class_idx = 0; class_idx < num_classes; class_idx++)
+                    float* boxesPtr = boxes + batch * boxesStrides;
+                    for (size_t class_idx = 0; class_idx < num_classes; class_idx++)
                     {
+                        float* scoresPtr = scores + batch * scoresStrides[0] +
+                                           class_idx * scoresStrides[1];
+                        std::vector<BoxInfo> candidate_boxes;
+                        candidate_boxes.reserve(num_boxes);
+
+                        for (size_t box_idx = 0; box_idx < num_boxes; box_idx++)
+                        {
+                            if (scoresPtr[box_idx] > score_threshold)
+                            {
+                                Rectangle* r = reinterpret_cast<Rectangle*>(boxesPtr) + box_idx;
+                                candidate_boxes.emplace_back(*r, box_idx, scoresPtr[box_idx], 0);
+                            }
+                        }
+
+                        std::priority_queue<BoxInfo> sorted_boxes(std::less<BoxInfo>(),
+                                                                  std::move(candidate_boxes));
+
+                        std::vector<BoxInfo> selected;
+                        // Get the next box with top score, filter by iou_threshold
+
+                        BoxInfo next_candidate;
+                        float original_score;
+
+                        while (!sorted_boxes.empty() && selected.size() < boxes_per_class)
+                        {
+                            next_candidate = sorted_boxes.top();
+                            original_score = next_candidate.score;
+                            sorted_boxes.pop();
+
+                            bool should_hard_suppress = false;
+                            for (int64_t j = static_cast<int64_t>(selected.size()) - 1;
+                                 j >= next_candidate.suppress_begin_index; --j)
+                            {
+                                float iou = intersectionOverUnion(next_candidate.box, selected[j].box);
+                                next_candidate.score *= func(iou);
+
+                                if (iou >= iou_threshold)
+                                {
+                                    should_hard_suppress = true
+                                    break;
+                                }
+
+                                if (next_candidate.score <= score_threshold)
+                                {
+                                    break;
+                                }
+                            }
+
+                            next_candidate.suppress_begin_index = selected.size();
+
+                            if (!should_hard_suppress) {
+                                if (next_candidate.score == original_score) {
+                                    // // Suppression has not occurred, so select next_candidate
+                                    // selected.push_back(next_candidate.box_index);
+                                    // selected_scores.push_back(next_candidate.score);
+                                    // continue;
+                                }
+                                if (next_candidate.score > score_threshold) {
+                                    // Soft suppression has occurred and current score is still greater than
+                                    // score_threshold; add next_candidate back onto priority queue.
+                                    candidate_priority_queue.push(next_candidate);
+                                }
+                            }
+                        }
+
+
                     }
                 }
             }
