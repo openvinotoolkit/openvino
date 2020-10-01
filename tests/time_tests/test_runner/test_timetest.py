639052d0b87ace66bb9c3c8063d6740d213bdd24
Vitaliy Urusovskij <vitaliy.urusovskij@intel.com>
2020-10-01 11:35:57 +0300

    Support references in test_timetest.py (#2431)

--- tests/time_tests/test_runner/test_timetest.py
+++ tests/time_tests/test_runner/test_timetest.py
@@ -18,6 +18,9 @@ from pathlib import Path
 import logging
 
 from scripts.run_timetest import run_timetest
+from test_runner.utils import expand_env_vars
+
+REFS_FACTOR = 1.2      # 120%
 
 
 def test_timetest(instance, executable, niter):
@@ -34,11 +37,27 @@ def test_timetest(instance, executable, niter):
     # Run executable
     exe_args = {
         "executable": Path(executable),
-        "model": Path(model_path),
+        "model": Path(expand_env_vars(model_path)),
         "device": instance["device"]["name"],
         "niter": niter
     }
     retcode, aggr_stats = run_timetest(exe_args, log=logging)
     assert retcode == 0, "Run of executable failed"
 
-    instance["results"] = aggr_stats    # append values to report to DB
+    # Add timetest results to submit to database and save in new test conf as references
+    instance["results"] = aggr_stats
+
+    # Compare with references
+    comparison_status = 0
+    for step_name, references in instance["references"].items():
+        for metric, reference_val in references.items():
+            if aggr_stats[step_name][metric] > reference_val * REFS_FACTOR:
+                logging.error("Comparison failed for '{}' step for '{}' metric. Reference: {}. Current values: {}"
+                              .format(step_name, metric, reference_val, aggr_stats[step_name][metric]))
+                comparison_status = 1
+            else:
+                logging.info("Comparison passed for '{}' step for '{}' metric. Reference: {}. Current values: {}"
+                             .format(step_name, metric, reference_val, aggr_stats[step_name][metric]))
+
+    assert comparison_status == 0, "Comparison with references failed"
+
