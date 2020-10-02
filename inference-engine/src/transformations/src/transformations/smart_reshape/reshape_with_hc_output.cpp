bf4520a2966956dc9fe27c5aa70d26423b8c59b4
yegor.kruglov <ykruglov@nnlvdp-mkaglins.inn.intel.com>
2020-10-02 13:45:49 +0300

    update tests

--- inference-engine/src/transformations/src/transformations/smart_reshape/reshape_with_hc_output.cpp
+++ inference-engine/src/transformations/src/transformations/smart_reshape/reshape_with_hc_output.cpp
@@ -34,13 +34,17 @@ bool relax_hc_reshape_followed_by_matmul(const ngraph::pattern::PatternValueMap 
     if (!reshape->get_output_partial_shape(0).rank().is_static())
         return false;
 
+    if (reshape_pattern->get_shape() == ngraph::Shape{0} || reshape_pattern->get_shape() == ngraph::Shape{1})
+        return false;
+
+    const auto &raw_idx = reshape_is_A_input ? (matmul->get_transpose_b() ? -1 : -2) : (matmul->get_transpose_a()
+                                                                                        ? -2 : -1);
+    const auto &idx = ngraph::normalize_axes(matmul->description(), {raw_idx},
+                                             reshape->get_output_partial_shape(0).rank());
+    const auto &C = ngraph::op::util::node_to_get_shape_value_of_indices_from_shape_source(shape_source, idx);
+    const auto &N = ngraph::opset4::Constant::create(ngraph::element::i64, {1}, {-1});
+
     if (reshape_pattern->get_shape() == ngraph::Shape{2}) {
-        const auto &raw_idx = reshape_is_A_input ? (matmul->get_transpose_b() ? -1 : -2) : (matmul->get_transpose_a()
-                                                                                            ? -2 : -1);
-        const auto &idx = ngraph::normalize_axes(matmul->description(), {raw_idx},
-                                                 reshape->get_output_partial_shape(0).rank());
-        const auto &C = ngraph::op::util::node_to_get_shape_value_of_indices_from_shape_source(shape_source, idx);
-        const auto &N = ngraph::opset4::Constant::create(ngraph::element::i64, {1}, {-1});
         const auto & pattern_vector = reshape_is_A_input ?
                               (matmul->get_transpose_a() ? ngraph::OutputVector({C, N}) : ngraph::OutputVector({N, C}))
                                                  :
@@ -51,49 +55,36 @@ bool relax_hc_reshape_followed_by_matmul(const ngraph::pattern::PatternValueMap 
         copy_runtime_info(reshape_pattern, new_reshape_pattern);
         reshape->input_value(1).replace(new_reshape_pattern->output(0));
         return true;
-    }
-
-    else if (reshape_pattern->get_shape() == ngraph::Shape{0} || reshape_pattern->get_shape() == ngraph::Shape{1})
-        return false;
-
-    else if (ngraph::is_type<ngraph::opset4::Constant>(reshape_pattern)) {
+    } else if (ngraph::is_type<ngraph::opset4::Constant>(reshape_pattern)) {
         auto const_reshape_pattern = std::dynamic_pointer_cast<ngraph::opset4::Constant>(reshape_pattern);
         const auto values = const_reshape_pattern->cast_vector<int64_t>();
         for (auto i : values)
             if (i <= 0)
                 return false;
-        const auto &raw_idx = reshape_is_A_input ? (matmul->get_transpose_b() ? -1 : -2) : (matmul->get_transpose_a()
-                                                                                            ? -2 : -1);
-        const auto &idx = ngraph::normalize_axes(matmul->description(), {raw_idx},
-                                                 reshape->get_output_partial_shape(0).rank());
-        const auto &C = ngraph::op::util::node_to_get_shape_value_of_indices_from_shape_source(shape_source, idx);
-        const auto &N = ngraph::opset4::Constant::create(ngraph::element::i64, {1}, {-1});
-
-        const auto &reshape_raw_idx = reshape_is_A_input ? (matmul->get_transpose_b() ? -2 : -1) :
-                                      (matmul->get_transpose_a() ? -1 : -2);
+        const auto &reshape_raw_idx = reshape_is_A_input ? (matmul->get_transpose_a() ? -1 : -2) :
+                                      (matmul->get_transpose_b() ? -2 : -1);
         const auto &reshape_idx = ngraph::normalize_axes(matmul->description(), {reshape_raw_idx},
                                                          reshape->get_output_partial_shape(0).rank());
         const auto &D = ngraph::op::util::node_to_get_shape_value_of_indices_from_shape_node(const_reshape_pattern,
                                                                                                reshape_idx);
         auto pattern_vector = reshape_is_A_input ?
-                              (matmul->get_transpose_a() ? ngraph::OutputVector({N, C, D}) : ngraph::OutputVector({N, D ,C}))
-                                                 :
-                              (matmul->get_transpose_b() ? ngraph::OutputVector({N, D, C}) : ngraph::OutputVector({N, C, D}));
-        if (reshape_pattern->get_shape() > ngraph::Shape{3}) {
+                (matmul->get_transpose_a() ? ngraph::OutputVector({N, C, D}) : ngraph::OutputVector({N, D, C})) :
+                (matmul->get_transpose_b() ? ngraph::OutputVector({N, D, C}) : ngraph::OutputVector({N, C, D}));
+        if (reshape->get_output_partial_shape(0).rank().get_length() > 3) {
             auto shape = reshape_pattern->get_shape();
             shape.erase(shape.begin());
             shape.erase(shape.end(), shape.end() - 1);
             auto old_raw = -3;
             std::vector<int64_t > axes;
-            for (auto i: shape) {
+            for (auto i : shape) {
                 axes.push_back(old_raw);
                 old_raw--;
             }
-            const auto &old_idx = ngraph::normalize_axes(matmul->description(), axes,
-                    reshape->get_output_partial_shape(0).rank());
-            const auto &M = ngraph::op::util::node_to_get_shape_value_of_indices_from_shape_node(const_reshape_pattern,
-                    old_idx);
-            pattern_vector.insert(pattern_vector.begin() + 1, M);
+            const auto &reshape_indices = ngraph::normalize_axes(matmul->description(), axes,
+                                                                  reshape->get_output_partial_shape(0).rank());
+            const auto &reshape_indices_value = ngraph::op::util::node_to_get_shape_value_of_indices_from_shape_node(const_reshape_pattern,
+                                                                                                                     reshape_indices);
+            pattern_vector.insert(pattern_vector.begin() + 1, reshape_indices_value);
         }
         const auto & new_reshape_pattern = std::make_shared<ngraph::opset4::Concat>(pattern_vector, 0);
         auto new_reshape = reshape->copy_with_new_inputs({reshape->input_value(0), new_reshape_pattern});
--- inference-engine/tests/functional/inference_engine/cnn_network/smart_reshape_tests.cpp
+++ inference-engine/tests/functional/inference_engine/cnn_network/smart_reshape_tests.cpp
@@ -31,14 +31,7 @@ struct ReshapeMatMulTestCase {
     std::vector<int64_t> reshape_pattern;
     bool transpose_a, transpose_b;
     reshape_map new_shapes;
-};
-
-struct ReshapeMatMulOutputValueTestCase {
-    bool reshape_is_A_input;
-    ngraph::PartialShape A_shape, B_shape;
-    std::vector<int64_t> reshape_pattern;
-    bool transpose_a, transpose_b;
-    ngraph::PartialShape ref_shape;
+    std::vector<uint64_t> ref_shape;
 };
 
 class CNNNGraphImplSmartReshapeTests : public CommonTestUtils::TestsCommon, public testing::WithParamInterface<std::tuple<ReshapeMatMulTestCase>> {
@@ -74,111 +67,40 @@ public:
         InferenceEngine::details::CNNNetworkNGraphImpl network(ngraph);
         const auto & resp = network.reshape(test_case.new_shapes, nullptr);
         ASSERT_EQ(resp, StatusCode::OK);
+        OutputsDataMap externalOutputsData;
+        network.getOutputsInfo(externalOutputsData);
+        const auto & out_shape = externalOutputsData.begin()->second->getDims();
+        ASSERT_EQ(out_shape, test_case.ref_shape);
     }
 };
-
-class CNNNGraphImplSmartReshapeOutputValueTests : public CommonTestUtils::TestsCommon, public testing::WithParamInterface<std::tuple<ReshapeMatMulOutputValueTestCase>> {
-public:
-    void SetUp() override {
-        const auto& parameters = GetParam();
-        const auto& test_case = std::get<0>(GetParam());
-
-        std::shared_ptr<ngraph::Function> ngraph;
-        {
-            auto input_A = std::make_shared<ngraph::opset4::Parameter>(ngraph::element::f32, test_case.A_shape);
-            input_A->set_friendly_name("input_A");
-            auto input_B = std::make_shared<ngraph::opset4::Parameter>(ngraph::element::f32, test_case.B_shape);
-            input_B->set_friendly_name("input_B");
-
-            auto reshape_pattern = std::make_shared<ngraph::opset4::Constant>(
-                    ngraph::element::i64, ngraph::Shape{test_case.reshape_pattern.size()}, test_case.reshape_pattern);
-            reshape_pattern->set_friendly_name("reshape_pattern");
-            auto reshape = std::make_shared<ngraph::opset4::Reshape>(test_case.reshape_is_A_input ? input_A : input_B, reshape_pattern, true);
-            reshape->set_friendly_name("reshape");
-
-            auto mat_mul = std::make_shared<ngraph::opset4::MatMul>(test_case.reshape_is_A_input ? reshape->output(0) : input_A->output(0),
-                                                                    test_case.reshape_is_A_input ? input_B->output(0) : reshape->output(0),
-                                                                    test_case.transpose_a, test_case.transpose_b);
-            mat_mul->set_friendly_name("matmul");
-
-            auto result = std::make_shared<ngraph::op::Result>(mat_mul);
-            ngraph::ParameterVector params = {input_A, input_B};
-            ngraph::ResultVector results = {result};
-            ngraph = std::make_shared<ngraph::Function>(results, params);
-
-            ngraph::pass::Manager manager;
-            manager.register_pass<ngraph::pass::InitNodeInfo>();
-            manager.register_pass<ngraph::pass::ReshapeAMatMul>();
-            manager.register_pass<ngraph::pass::ReshapeBMatMul>();
-            manager.register_pass<ngraph::pass::ConstantFolding>();
-            manager.run_passes(ngraph);
-        }
-        for (auto op : ngraph->get_ops()) {
-            if (auto concat = ngraph::as_type_ptr<ngraph::opset4::Concat>(op)) {
-                auto shape = concat->get_output_partial_shape(0);
-                ASSERT_EQ(shape, test_case.ref_shape);
-            }
-        }
-    }
-};
-
 
 TEST_P(CNNNGraphImplSmartReshapeTests, ReshapeMatMul) {
 }
 
-TEST_P(CNNNGraphImplSmartReshapeOutputValueTests, ReshapeMatmulOutputCheck){
-}
-
 INSTANTIATE_TEST_CASE_P(NGraph, CNNNGraphImplSmartReshapeTests, testing::Values(
-        ReshapeMatMulTestCase{true, {1, 20, 30}, {30, 40}, {20, -1}, false, false, {{"input_A", {2, 20, 30}}}},
-        ReshapeMatMulTestCase{true, {1, 20, 30}, {40, 30}, {20, -1}, false, true, {{"input_A", {2, 20, 30}}}},
-        ReshapeMatMulTestCase{true, {1, 30, 20}, {30, 20}, {-1, 20}, true, false, {{"input_A", {2, 30, 20}}}},
-        ReshapeMatMulTestCase{true, {1, 30, 20}, {40, 30}, {-1, 20}, true, true, {{"input_A", {2, 30, 20}}}},
-        ReshapeMatMulTestCase{false, {20, 30}, {1, 30, 40}, {-1, 40}, false, false, {{"input_B", {2, 30, 40}}}},
-        ReshapeMatMulTestCase{false, {20, 30}, {1, 40, 30}, {40, -1}, false, true, {{"input_B", {2, 40, 30}}}},
-        ReshapeMatMulTestCase{false, {30, 20}, {1, 30, 40}, {-1, 40}, true, false, {{"input_B", {2, 30, 40}}}},
-        ReshapeMatMulTestCase{false, {30, 20}, {1, 40, 30}, {40, -1}, true, true, {{"input_B", {2, 40, 30}}}},
-        ReshapeMatMulTestCase{true, {1, 2, 60, 20}, {1, 20, 30}, {2, 60, 20}, false, false, {{"input_A", {2, 2, 60, 20}}}},
-        ReshapeMatMulTestCase{true, {1, 2, 60, 20}, {1, 30, 20}, {2, 60, 20}, false, true, {{"input_A", {2, 2, 60, 20}}}},
-        ReshapeMatMulTestCase{true, {1, 2, 20, 60}, {1, 20, 30}, {2, 20, 60}, true, false, {{"input_A", {2, 2, 20, 60}}}},
-        ReshapeMatMulTestCase{true, {1, 2, 20, 60}, {1, 30, 20}, {2, 20, 60}, true, true, {{"input_A", {2, 2, 20, 60}}}},
-        ReshapeMatMulTestCase{false, {1, 30, 20}, {1, 2, 20, 60}, {2, 20, 60}, false, false, {{"input_B", {2, 2, 20, 60}}}},
-        ReshapeMatMulTestCase{false, {1, 30, 20}, {1, 2, 60, 20}, {2, 60, 20}, false, true, {{"input_B", {2, 2, 60, 20}}}},
-        ReshapeMatMulTestCase{false, {1, 20, 30}, {1, 2, 20, 60}, {2, 20, 60}, true, false, {{"input_B", {2, 2, 20, 60}}}},
-        ReshapeMatMulTestCase{false, {1, 20, 30}, {1, 2, 60, 20}, {2, 60, 20}, true, true, {{"input_B", {2, 2, 60, 20}}}},
-        ReshapeMatMulTestCase{true, {1, 5, 2, 60, 20,}, {1, 2, 20, 30}, {5, 2, 60, 20}, false, false, {{"input_A", {2, 5, 2, 60, 20}}}},
-        ReshapeMatMulTestCase{true, {1, 5, 2, 60, 20,}, {1, 2, 30, 20}, {5, 2, 60, 20}, false, true, {{"input_A", {2, 5, 2, 60, 20}}}},
-        ReshapeMatMulTestCase{true, {1, 5, 2, 20, 60,}, {1, 2, 20, 30}, {5, 2, 20, 60}, true, false, {{"input_A", {2, 5, 2, 20, 60}}}},
-        ReshapeMatMulTestCase{true, {1, 5, 2, 20, 60,}, {1, 2, 30, 20}, {5, 2, 20, 60}, true, true, {{"input_A", {2, 5, 2, 20, 60}}}},
-        ReshapeMatMulTestCase{false, {1, 2, 30, 20}, {1, 5, 2, 20, 60}, {5, 2, 20, 60}, false, false, {{"input_B", {2, 5, 2, 20, 60}}}},
-        ReshapeMatMulTestCase{false, {1, 2, 30, 20}, {1, 5, 2, 60, 20}, {5, 2, 60, 20}, false, true, {{"input_B", {2, 5, 2, 60, 20}}}},
-        ReshapeMatMulTestCase{false, {1, 2, 20, 30}, {1, 5, 2, 20, 60}, {5, 2, 20, 60}, true, false, {{"input_B", {2, 5, 2, 20, 60}}}},
-        ReshapeMatMulTestCase{false, {1, 2, 20, 30}, {1, 5, 2, 60, 20}, {5, 2, 60, 20}, true, true, {{"input_B", {2, 5, 2, 60, 20}}}}));
+        ReshapeMatMulTestCase{true, {1, 20, 30}, {30, 40}, {20, -1}, false, false, {{"input_A", {2, 20, 30}}}, {40, 40}},
+        ReshapeMatMulTestCase{true, {1, 20, 30}, {40, 30}, {20, -1}, false, true, {{"input_A", {2, 20, 30}}}, {40, 40}},
+        ReshapeMatMulTestCase{true, {1, 30, 20}, {30, 20}, {-1, 20}, true, false, {{"input_A", {2, 30, 20}}}, {40, 20}},
+        ReshapeMatMulTestCase{true, {1, 30, 20}, {40, 30}, {-1, 20}, true, true, {{"input_A", {2, 30, 20}}}, {40, 40}},
+        ReshapeMatMulTestCase{false, {20, 30}, {1, 30, 40}, {-1, 40}, false, false, {{"input_B", {2, 30, 40}}}, {20, 80}},
+        ReshapeMatMulTestCase{false, {20, 30}, {1, 40, 30}, {40, -1}, false, true, {{"input_B", {2, 40, 30}}}, {20, 80}},
+        ReshapeMatMulTestCase{false, {30, 20}, {1, 30, 40}, {-1, 40}, true, false, {{"input_B", {2, 30, 40}}}, {20, 80}},
+        ReshapeMatMulTestCase{false, {30, 20}, {1, 40, 30}, {40, -1}, true, true, {{"input_B", {2, 40, 30}}}, {20, 80}},
+        ReshapeMatMulTestCase{true, {1, 2, 60, 20}, {1, 20, 30}, {2, 60, 20}, false, false, {{"input_A", {2, 2, 60, 20}}}, {4, 60, 30}},
+        ReshapeMatMulTestCase{true, {1, 2, 60, 20}, {1, 30, 20}, {2, 60, 20}, false, true, {{"input_A", {2, 2, 60, 20}}}, {4, 60, 30}},
+        ReshapeMatMulTestCase{true, {1, 2, 20, 60}, {1, 20, 30}, {2, 20, 60}, true, false, {{"input_A", {2, 2, 20, 60}}}, {4, 60, 30}},
+        ReshapeMatMulTestCase{true, {1, 2, 20, 60}, {1, 30, 20}, {2, 20, 60}, true, true, {{"input_A", {2, 2, 20, 60}}}, {4, 60, 30}},
+        ReshapeMatMulTestCase{false, {1, 30, 20}, {1, 2, 20, 60}, {2, 20, 60}, false, false, {{"input_B", {2, 2, 20, 60}}}, {4, 30, 60}},
+        ReshapeMatMulTestCase{false, {1, 30, 20}, {1, 2, 60, 20}, {2, 60, 20}, false, true, {{"input_B", {2, 2, 60, 20}}}, {4, 30, 60}},
+        ReshapeMatMulTestCase{false, {1, 20, 30}, {1, 2, 20, 60}, {2, 20, 60}, true, false, {{"input_B", {2, 2, 20, 60}}}, {4, 30, 60}},
+        ReshapeMatMulTestCase{false, {1, 20, 30}, {1, 2, 60, 20}, {2, 60, 20}, true, true, {{"input_B", {2, 2, 60, 20}}}, {4, 30, 60}},
+        ReshapeMatMulTestCase{true, {1, 5, 2, 60, 20}, {1, 2, 20, 30}, {5, 2, 60, 20}, false, false, {{"input_A", {2, 5, 2, 60, 20}}}, {10, 2, 60, 30}},
+        ReshapeMatMulTestCase{true, {1, 5, 2, 60, 20}, {1, 2, 30, 20}, {5, 2, 60, 20}, false, true, {{"input_A", {2, 5, 2, 60, 20}}}, {10, 2, 60, 30}},
+        ReshapeMatMulTestCase{true, {1, 5, 2, 20, 60}, {1, 2, 20, 30}, {5, 2, 20, 60}, true, false, {{"input_A", {2, 5, 2, 20, 60}}}, {10, 2, 60, 30}},
+        ReshapeMatMulTestCase{true, {1, 5, 2, 20, 60}, {1, 2, 30, 20}, {5, 2, 20, 60}, true, true, {{"input_A", {2, 5, 2, 20, 60}}}, {10, 2, 60, 30}},
+        ReshapeMatMulTestCase{false, {1, 2, 30, 20}, {1, 5, 2, 20, 60}, {5, 2, 20, 60}, false, false, {{"input_B", {2, 5, 2, 20, 60}}}, {10, 2, 30, 60}},
+        ReshapeMatMulTestCase{false, {1, 2, 30, 20}, {1, 5, 2, 60, 20}, {5, 2, 60, 20}, false, true, {{"input_B", {2, 5, 2, 60, 20}}}, {10, 2, 30, 60}},
+        ReshapeMatMulTestCase{false, {1, 2, 20, 30}, {1, 5, 2, 20, 60}, {5, 2, 20, 60}, true, false, {{"input_B", {2, 5, 2, 20, 60}}}, {10, 2, 30, 60}},
+        ReshapeMatMulTestCase{false, {1, 2, 20, 30}, {1, 5, 2, 60, 20}, {5, 2, 60, 20}, true, true, {{"input_B", {2, 5, 2, 60, 20}}}, {10, 2, 30, 60}}));
 
-INSTANTIATE_TEST_CASE_P(NGraph, CNNNGraphImplSmartReshapeOutputValueTests, testing::Values(
-        ReshapeMatMulOutputValueTestCase{true, {1, 20, 30}, {30, 40}, {20, -1}, false, false, {-1, 30}},
-        ReshapeMatMulOutputValueTestCase{true, {1, 20, 30}, {40, 30}, {20, -1}, false, true, {-1, 30}},
-        ReshapeMatMulOutputValueTestCase{true, {1, 30, 20}, {30, 20}, {-1, 20}, true, false, {30, -1}},
-        ReshapeMatMulOutputValueTestCase{true, {1, 30, 20}, {40, 30}, {-1, 20}, true, true, {30, -1}},
-        ReshapeMatMulOutputValueTestCase{false, {20, 30}, {1, 30, 40}, {-1, 40}, false, false, {30, -1}},
-        ReshapeMatMulOutputValueTestCase{false, {20, 30}, {1, 40, 30}, {40, -1}, false, true, {-1, 30}},
-        ReshapeMatMulOutputValueTestCase{false, {30, 20}, {1, 30, 40}, {-1, 40}, true, false, {30, -1}},
-        ReshapeMatMulOutputValueTestCase{false, {30, 20}, {1, 40, 30}, {40, -1}, true, true, {-1, 30}},
-        ReshapeMatMulOutputValueTestCase{true, {1, 2, 60, 20}, {1, 20, 30}, {2, 60, 20}, false, false, {-1, 60, 20}},
-        ReshapeMatMulOutputValueTestCase{true, {1, 2, 60, 20}, {1, 30, 20}, {2, 60, 20}, false, true, {-1, 60, 20}},
-        ReshapeMatMulOutputValueTestCase{true, {1, 2, 20, 60}, {1, 20, 30}, {2, 20, 60}, true, false, {-1, 20, 60}},
-        ReshapeMatMulOutputValueTestCase{true, {1, 2, 20, 60}, {1, 30, 20}, {2, 20, 60}, true, true, {-1, 20, 60}},
-        ReshapeMatMulOutputValueTestCase{false, {1, 30, 20}, {1, 2, 20, 60}, {2, 20, 60}, false, false, {-1, 20, 60}},
-        ReshapeMatMulOutputValueTestCase{false, {1, 30, 20}, {1, 2, 60, 20}, {2, 60, 20}, false, true, {-1, 60, 20}},
-        ReshapeMatMulOutputValueTestCase{false, {1, 20, 30}, {1, 2, 20, 60}, {2, 20, 60}, true, false, {-1, 20, 60}},
-        ReshapeMatMulOutputValueTestCase{false, {1, 20, 30}, {1, 2, 60, 20}, {2, 60, 20}, true, true, {-1, 60, 20}},
-        ReshapeMatMulOutputValueTestCase{true, {1, 5, 2, 60, 20,}, {1, 2, 20, 30}, {5, 2, 60, 20}, false, false, {-1, 2, 60, 20}},
-        ReshapeMatMulOutputValueTestCase{true, {1, 5, 2, 60, 20,}, {1, 2, 30, 20}, {5, 2, 60, 20}, false, true, {-1, 2, 60, 20}},
-        ReshapeMatMulOutputValueTestCase{true, {1, 5, 2, 20, 60,}, {1, 2, 20, 30}, {5, 2, 20, 60}, true, false, {-1, 2, 20, 60}},
-        ReshapeMatMulOutputValueTestCase{true, {1, 5, 2, 20, 60,}, {1, 2, 30, 20}, {5, 2, 20, 60}, true, true, {-1, 2, 20, 60}},
-        ReshapeMatMulOutputValueTestCase{false, {1, 2, 30, 20}, {1, 5, 2, 20, 60}, {5, 2, 20, 60}, false, false, {-1, 2, 20, 60}},
-        ReshapeMatMulOutputValueTestCase{false, {1, 2, 30, 20}, {1, 5, 2, 60, 20}, {5, 2, 60, 20}, false, true, {-1, 2, 60, 20}},
-        ReshapeMatMulOutputValueTestCase{false, {1, 2, 20, 30}, {1, 5, 2, 20, 60}, {5, 2, 20, 60}, true, false, {-1, 2, 20, 60}},
-        ReshapeMatMulOutputValueTestCase{false, {1, 2, 20, 30}, {1, 5, 2, 60, 20}, {5, 2, 60, 20}, true, true, {-1, 2, 60, 20}}
-        ));
 }  // namespace
