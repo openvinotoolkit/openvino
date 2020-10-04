7d3376e51a9129fdca7f905172c605985dbe2aa6
Stepyreva, Evgenya <evgenya.stepyreva@intel.com>
2020-10-05 00:38:41 +0300

    Call DepthToSpaceFusion during SmartReshape

--- inference-engine/src/transformations/src/transformations/smart_reshape/smart_reshape.cpp
+++ inference-engine/src/transformations/src/transformations/smart_reshape/smart_reshape.cpp
@@ -11,6 +11,7 @@
 
 #include <ngraph/pass/manager.hpp>
 #include <transformations/init_node_info.hpp>
+#include <transformations/depth_to_space_fusion.hpp>
 
 bool ngraph::pass::SmartReshape::run_on_function(std::shared_ptr<ngraph::Function> f) {
     OV_ITT_SCOPED_TASK(itt::domains::IETransform, "ngraph::pass::SmartReshape");
@@ -19,6 +20,8 @@ bool ngraph::pass::SmartReshape::run_on_function(std::shared_ptr<ngraph::Functio
     ngraph::pass::Manager smart_reshape_manager;
     // This pass must be called first in pipeline
     smart_reshape_manager.register_pass<ngraph::pass::InitNodeInfo>();
+
+    smart_reshape_manager.register_pass<ngraph::pass::DepthToSpaceFusion>();
 
     auto doesnt_introduce_fake_dynamism = smart_reshape_manager.register_pass<ngraph::pass::GraphRewrite>();
     // fake dynamism here is shape dependence on the data
@@ -32,6 +35,8 @@ bool ngraph::pass::SmartReshape::run_on_function(std::shared_ptr<ngraph::Functio
     introduces_fake_dynamism->add_matcher<ngraph::pass::ReshapeSoftMaxReshape>();
     introduces_fake_dynamism->set_name("ngraph::pass::SmartReshape::DynamicShapeTransformations");
 
+    // DepthToSpaceFusion requires manager to have a callback
+    smart_reshape_manager.set_callback([](const std::shared_ptr<const ngraph::Node> & node) -> bool {return true;});
     smart_reshape_manager.run_passes(f);
     return true;
 }
