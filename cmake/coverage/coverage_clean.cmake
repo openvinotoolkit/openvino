0863be1dbc6091a1833f46a0a067209e2dd4dbf2
Ilya Lavrenov <ilya.lavrenov@intel.com>
2020-10-05 23:37:50 +0300

    Shellcheck tests for bash scripts (#2532)
    * Added bash scripts checks with shellcheck
    
    * Fixed indentations
    
    * Fixed setupvars.sh with shellcheck
    
    * Fixed MO scripts
    
    * Compilation with newer versions of shellcheck

--- cmake/coverage/coverage_clean.cmake
+++ cmake/coverage/coverage_clean.cmake
@@ -3,28 +3,28 @@
 #
 
 if(NOT DEFINED IE_COVERAGE_REPORTS)
-	message(FATAL_ERROR "IE_COVERAGE_REPORTS variable is not defined")
-	return()
+    message(FATAL_ERROR "IE_COVERAGE_REPORTS variable is not defined")
+    return()
 endif()
 
 file(REMOVE_RECURSE "${IE_COVERAGE_REPORTS}")
 
 if(NOT DEFINED IE_COVERAGE_DIRECTORY)
-	message(FATAL_ERROR "IE_COVERAGE_DIRECTORY variable is not defined")
-	return()
+    message(FATAL_ERROR "IE_COVERAGE_DIRECTORY variable is not defined")
+    return()
 endif()
 
 # remove .gcno files which are kept from the previous build
 
 file(GLOB_RECURSE gcno_files "${IE_COVERAGE_DIRECTORY}/*.gcno")
 foreach(file IN LISTS gcno_files)
-	string(REPLACE ".gcno" "" temp_file "${file}")
-	string(REGEX REPLACE "CMakeFiles/.+dir/" "" temp_file "${temp_file}")
-	string(REPLACE "${CMAKE_BINARY_DIRECTORY}" "${CMAKE_SOURCE_DIRECTORY}" source_file "${temp_file}")
+    string(REPLACE ".gcno" "" temp_file "${file}")
+    string(REGEX REPLACE "CMakeFiles/.+dir/" "" temp_file "${temp_file}")
+    string(REPLACE "${CMAKE_BINARY_DIRECTORY}" "${CMAKE_SOURCE_DIRECTORY}" source_file "${temp_file}")
 
-	if(NOT EXISTS "${source_file}")
-		file(REMOVE "${file}")
-		string(REPLACE "${CMAKE_BINARY_DIRECTORY}/" "" file "${file}")
-		message("Removing ${file}")
-	endif()
+    if(NOT EXISTS "${source_file}")
+        file(REMOVE "${file}")
+        string(REPLACE "${CMAKE_BINARY_DIRECTORY}/" "" file "${file}")
+        message("Removing ${file}")
+    endif()
 endforeach()
---
+++ cmake/shellcheck/shellcheck_process.cmake
@@ -0,0 +1,27 @@
+# Copyright (C) 2018-2020 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+#
+
+if(NOT DEFINED IE_SHELLCHECK_PROGRAM)
+    message(FATAL_ERROR "IE_SHELLCHECK_PROGRAM is not defined")
+endif()
+
+if(NOT DEFINED IE_SHELL_SCRIPT)
+    message(FATAL_ERROR "IE_SHELL_SCRIPT is not defined")
+endif()
+
+if(NOT DEFINED IE_SHELLCHECK_OUTPUT)
+    message(FATAL_ERROR "IE_SHELLCHECK_OUTPUT is not defined")
+endif()
+
+set(rules "SC1091,SC2236,SC2164,SC2086,SC2162,SC1090")
+execute_process(COMMAND ${IE_SHELLCHECK_PROGRAM} --exclude=${rules} ${IE_SHELL_SCRIPT}
+                OUTPUT_VARIABLE error_message
+                RESULT_VARIABLE exit_code
+                OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+file(WRITE "${IE_SHELLCHECK_OUTPUT}" "${error_message}")
+
+if(NOT exit_code EQUAL 0)
+    message(FATAL_ERROR "${error_message}")
+endif()
