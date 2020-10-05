62976003e3cea05ee35afa33992ededf09b12dc3
Ilya Lavrenov <ilya.lavrenov@intel.com>
2020-10-05 23:37:50 +0300

    Shellcheck tests for bash scripts (#2532)
    * Added bash scripts checks with shellcheck
    
    * Fixed indentations
    
    * Fixed setupvars.sh with shellcheck
    
    * Fixed MO scripts
    
    * Compilation with newer versions of shellcheck

--- inference-engine/scripts/dependencies.sh
+++ inference-engine/scripts/dependencies.sh
@@ -3,38 +3,38 @@
 # SPDX-License-Identifier: Apache-2.0
 
 if [ "$1" = "" ]; then
-		dep_arch=64
-	else
-		dep_arch=$1
+        dep_arch=64
+    else
+        dep_arch=$1
 fi
 
 item_path=""
 add_path() {
-	component=$1
-	item_path=""
+    component=$1
+    item_path=""
     echo "Read file: dependencies_${dep_arch}.txt"
     grep_component="\b${component}\b"
 
-	if [[ $(grep -m 1 "$grep_component" "dependencies_${dep_arch}.txt") ]];then
-		archive_path=$(grep -m 1 "$grep_component" "dependencies_${dep_arch}.txt" | sed -E "s/${component}=//g")
-		library_rpath=$(grep -m 1 "$grep_component" "ld_library_rpath_${dep_arch}.txt" | sed -E "s/${component}=//g")
-		filename=$(basename "$archive_path")
-		if [[ (! -d "$DL_SDK_TEMP/test_dependencies/$component/$filename") ||
-			    (-d "$DL_SDK_TEMP/test_dependencies/$component/$filename"  &&
-			    	! $(ls -A "$DL_SDK_TEMP/test_dependencies/$component/$filename")) ]]; then
-			mkdir -p "$DL_SDK_TEMP/test_dependencies/$component/$filename"
-			wget -q "$archive_path" -O "$DL_SDK_TEMP/test_dependencies/$filename"
-			if [[ $filename == *.zip ]]; then
-				unzip "$DL_SDK_TEMP/test_dependencies/$filename" -d "$DL_SDK_TEMP/test_dependencies/$component/$filename"
-			elif [[ $filename == *.7z ]]; then
-			    7za x -y "$DL_SDK_TEMP/test_dependencies/$filename" -o "$DL_SDK_TEMP/test_dependencies/$component/$filename"
-			else
-				tar xf "$DL_SDK_TEMP/test_dependencies/$filename" -C "$DL_SDK_TEMP/test_dependencies/$component/$filename"
-			fi
-			rm "$DL_SDK_TEMP/test_dependencies/$filename"
-		fi
-		item_path=$component/$filename/$library_rpath
-	fi
+    if [[ $(grep -m 1 "$grep_component" "dependencies_${dep_arch}.txt") ]];then
+        archive_path=$(grep -m 1 "$grep_component" "dependencies_${dep_arch}.txt" | sed -E "s/${component}=//g")
+        library_rpath=$(grep -m 1 "$grep_component" "ld_library_rpath_${dep_arch}.txt" | sed -E "s/${component}=//g")
+        filename=$(basename "$archive_path")
+        if [[ (! -d "$DL_SDK_TEMP/test_dependencies/$component/$filename") ||
+                (-d "$DL_SDK_TEMP/test_dependencies/$component/$filename"  &&
+                    ! $(ls -A "$DL_SDK_TEMP/test_dependencies/$component/$filename")) ]]; then
+            mkdir -p "$DL_SDK_TEMP/test_dependencies/$component/$filename"
+            wget -q "$archive_path" -O "$DL_SDK_TEMP/test_dependencies/$filename"
+            if [[ $filename == *.zip ]]; then
+                unzip "$DL_SDK_TEMP/test_dependencies/$filename" -d "$DL_SDK_TEMP/test_dependencies/$component/$filename"
+            elif [[ $filename == *.7z ]]; then
+                7za x -y "$DL_SDK_TEMP/test_dependencies/$filename" -o "$DL_SDK_TEMP/test_dependencies/$component/$filename"
+            else
+                tar xf "$DL_SDK_TEMP/test_dependencies/$filename" -C "$DL_SDK_TEMP/test_dependencies/$component/$filename"
+            fi
+            rm "$DL_SDK_TEMP/test_dependencies/$filename"
+        fi
+        item_path=$component/$filename/$library_rpath
+    fi
 }
 
 runtimes=(MKL CLDNN MYRIAD GNA DLIA OPENCV VPU_FIRMWARE_USB-MA2450 VPU_FIRMWARE_USB-MA2X8X HDDL OMP TBB AOCL_RTE LIBUSB)
