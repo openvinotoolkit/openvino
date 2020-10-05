c3056bbb56e7160d7861ced88c43c485de2fcc70
Zinoviev, Vladimir <vladimir.zinoviev@intel.com>
2020-10-05 20:37:35 +0300

    [LPT] Interpolate transformation enabled

--- inference-engine/tests/functional/plugin/shared/include/low_precision_transformations/interpolate_transformation.hpp
+++ inference-engine/tests/functional/plugin/shared/include/low_precision_transformations/interpolate_transformation.hpp
@@ -11,11 +11,35 @@
 
 namespace LayerTestsDefinitions {
 
+class interpAttributes {
+public:
+    ngraph::AxisSet axes;
+    std::string mode;
+    bool align_corners;
+    bool antialias;
+    std::vector<size_t> pads_begin;
+    std::vector<size_t> pads_end;
+
+    bool shouldBeTransformed;
+
+    interpAttributes() = default;
+
+    interpAttributes(const ngraph::AxisSet& axes,
+                     const std::string& mode,
+                     const bool& align_corners,
+                     const bool& antialias,
+                     const std::vector<size_t>& pads_begin,
+                     const std::vector<size_t>& pads_end,
+                     const bool& shouldBeTransformed = true) :
+            axes(axes), mode(mode), align_corners(align_corners),
+            antialias(antialias), pads_begin(pads_begin), pads_end(pads_end) {}
+};
+
 typedef std::tuple<
     ngraph::element::Type,
     std::pair<ngraph::Shape, ngraph::Shape>,
     std::string,
-    ngraph::op::InterpolateAttrs,
+    interpAttributes,
     LayerTestsUtils::LayerTransformation::LptVersion> InterpolateTransformationParams;
 
 class InterpolateTransformation :
