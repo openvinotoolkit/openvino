4c1ae9b33984324687d152056d09ccadc80c70c0
Kamil Magierski <kamil.magierski@intel.com>
2020-10-06 10:01:19 +0200

    [GNA] Issue 39975 - cascade concat fix (#2486)
    * concat input not used fix rough implementation
    
    * [GNA] Cascade concat input not assigned fix
    
    * reduce copying in recursive function
    
    * [GNA] Aligned cascade concat test

---
+++ inference-engine/tests/functional/plugin/gna/shared_tests_instances/subgraph_tests/multiple_concat.cpp
@@ -0,0 +1,70 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+#include <subgraph_tests/multiple_concat.hpp>
+#include "common_test_utils/test_constants.hpp"
+
+namespace SubgraphTestsDefinitions {
+namespace {
+std::vector<size_t> input_sizes_aligned = {
+    64,
+    576,
+};
+
+std::vector<size_t> constant_sizes_aligned = {
+    64,
+    32,
+};
+
+std::vector<size_t> input_sizes_unaligned = {
+    26,
+    99
+};
+
+std::vector<size_t> constant_sizes_unaligned = {
+    26,
+    99
+};
+
+std::map<std::string, std::string> additional_config = {
+    {"GNA_COMPACT_MODE", "NO"},
+    {"GNA_DEVICE_MODE", "GNA_SW_EXACT"},
+    {"GNA_SCALE_FACTOR_0", "3276.8"},
+};
+} // namespace
+
+INSTANTIATE_TEST_CASE_P(I_aligned_C_aligned, MultipleConcatTest,
+    ::testing::Combine(
+        ::testing::Values(CommonTestUtils::DEVICE_GNA),
+        ::testing::Values(InferenceEngine::Precision::FP32),
+        ::testing::ValuesIn(input_sizes_aligned),
+        ::testing::ValuesIn(constant_sizes_aligned),
+        ::testing::Values(additional_config)),
+    MultipleConcatTest::getTestCaseName);
+
+INSTANTIATE_TEST_CASE_P(I_aligned_C_unaligned, MultipleConcatTest,
+    ::testing::Combine(
+        ::testing::Values(CommonTestUtils::DEVICE_GNA),
+        ::testing::Values(InferenceEngine::Precision::FP32),
+        ::testing::ValuesIn(input_sizes_aligned),
+        ::testing::ValuesIn(constant_sizes_unaligned),
+        ::testing::Values(additional_config)),
+    MultipleConcatTest::getTestCaseName);
+
+INSTANTIATE_TEST_CASE_P(I_unaligned_C_aligned, MultipleConcatTest,
+    ::testing::Combine(
+        ::testing::Values(CommonTestUtils::DEVICE_GNA),
+        ::testing::Values(InferenceEngine::Precision::FP32),
+        ::testing::ValuesIn(input_sizes_unaligned),
+        ::testing::ValuesIn(constant_sizes_aligned),
+        ::testing::Values(additional_config)),
+    MultipleConcatTest::getTestCaseName);
+
+INSTANTIATE_TEST_CASE_P(I_unaligned_C_unaligned, MultipleConcatTest,
+    ::testing::Combine(
+        ::testing::Values(CommonTestUtils::DEVICE_GNA),
+        ::testing::Values(InferenceEngine::Precision::FP32),
+        ::testing::ValuesIn(input_sizes_unaligned),
+        ::testing::ValuesIn(constant_sizes_unaligned),
+        ::testing::Values(additional_config)),
+    MultipleConcatTest::getTestCaseName);
+} // namespace SubgraphTestsDefinitions
