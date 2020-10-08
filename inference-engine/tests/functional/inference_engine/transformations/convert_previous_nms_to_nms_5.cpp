ca757963ab4a005c7b1eb81427ea9a8703faf41d
VGavrilov <vladimir.gavrilov@intel.com>
2020-10-08 16:15:39 +0300

    Started to write tests for conversion of previous NMS to nGraph NMS-5.

---
+++ inference-engine/tests/functional/inference_engine/transformations/convert_previous_nms_to_nms_5.cpp
@@ -0,0 +1,72 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <gtest/gtest.h>
+
+#include <string>
+#include <memory>
+#include <queue>
+
+#include <ngraph/function.hpp>
+#include <ngraph/opsets/opset1.hpp>
+#include <ngraph/opsets/opset3.hpp>
+#include <ngraph/opsets/opset4.hpp>
+#include <ngraph/opsets/opset5.hpp>
+#include <transformations/convert_previous_nms_to_nms_5.hpp>
+#include <transformations/init_node_info.hpp>
+#include <transformations/utils/utils.hpp>
+#include <ngraph/pass/manager.hpp>
+
+#include "common_test_utils/ngraph_test_utils.hpp"
+
+using namespace testing;
+using namespace ngraph;
+
+TEST(TransformationTests, ConvertNMS4ToNMS5) {
+    std::shared_ptr<ngraph::Function> f(nullptr), f_ref(nullptr);
+    {
+        auto boxes = std::make_shared<opset4::Parameter>(element::f32, Shape{1, 1000, 4});
+        auto scores = std::make_shared<opset4::Parameter>(element::f32, Shape{1, 1, 1000});
+        auto max_output_boxes_per_class = opset4::Constant::create(element::i64, Shape{}, {10});
+        auto iou_threshold = opset4::Constant::create(element::f32, Shape{}, {0.75});
+        auto score_threshold = opset4::Constant::create(element::f32, Shape{}, {0.7});
+        auto nms = std::make_shared<opset4::NonMaxSuppression>(boxes, scores, max_output_boxes_per_class, iou_threshold, score_threshold,
+                                                               opset4::NonMaxSuppression::BoxEncodingType::CORNER, true);
+
+        f = std::make_shared<Function>(NodeVector{nms}, ParameterVector{boxes, scores});
+
+        pass::Manager m;
+        m.register_pass<pass::InitNodeInfo>();
+        m.register_pass<pass::ConvertNMS4ToNMS5>();
+        m.run_passes();
+        ASSERT_NO_THROW(check_rt_info(f));
+    }
+//     {
+//         auto data = std::make_shared<ngraph::opset1::Parameter>(ngraph::element::f32, ngraph::Shape{3, 1, 2});
+//         auto divide_constant = ngraph::opset1::Constant::create(ngraph::element::f32, ngraph::Shape{1}, {1.5});
+//         auto divide = std::make_shared<ngraph::opset1::Divide>(data, divide_constant);
+//
+//         f = std::make_shared<ngraph::Function>(ngraph::NodeVector{divide}, ngraph::ParameterVector{data});
+//
+//         ngraph::pass::Manager m;
+//         m.register_pass<ngraph::pass::InitNodeInfo>();
+//         m.register_pass<ngraph::pass::ConvertDivide>();
+//         m.run_passes(f);
+//         ASSERT_NO_THROW(check_rt_info(f));
+//     }
+//
+//     {
+//         auto data = std::make_shared<ngraph::opset1::Parameter>(ngraph::element::f32, ngraph::Shape{3, 1, 2});
+//         auto divide_constant = ngraph::opset1::Constant::create(ngraph::element::f32, ngraph::Shape{1}, {1.5});
+//         auto pow = std::make_shared<ngraph::opset1::Power>(divide_constant,
+//                                                            ngraph::opset1::Constant::create(ngraph::element::f32, ngraph::Shape{1}, {-1}));
+//         auto mul = std::make_shared<ngraph::opset1::Multiply>(data, pow);
+//
+//         f_ref = std::make_shared<ngraph::Function>(ngraph::NodeVector{mul}, ngraph::ParameterVector{data});
+//     }
+//
+//     auto res = compare_functions(f, f_ref);
+//     ASSERT_TRUE(res.first) << res.second;
+    ASSERT_TRUE(true) << "";
+}
