ad5442ba8cbc085932d85524d3ad49197aaa3bd7
Gleb Kazantaev <gleb.kazantaev@intel.com>
2020-10-08 09:16:28 +0300

    Updated ConstantFolding pass to propagate friendly name (#2572)
    * Fixed concat output name when decomposing shape_of in CF
    
    * Added friendly_name propagation for CF pass
    
    * Propagate runtime info inside ConstantFolding pass
    
    * Fixed rt info merge strategy to avoid cases when merge is not implemented but attributes number is equal to 1

--- inference-engine/src/inference_engine/ie_core.cpp
+++ inference-engine/src/inference_engine/ie_core.cpp
@@ -297,23 +297,6 @@ public:
         QueryNetworkResult res;
         auto parsed = parseDeviceNameIntoConfig(deviceName, config);
         GetCPPPluginByName(parsed._deviceName).QueryNetwork(network, parsed._config, res);
-        if (!network.getFunction())
-            return res;
-
-        // WA for constant folded operations (plugins should support all folded ops)
-        const auto& func = network.getFunction();
-        auto specialized_function = ngraph::clone_function(*func);
-
-        ngraph::pass::ConstantFolding().run_on_function(specialized_function);
-        std::unordered_set<std::string> operationNames;
-        for (const auto& op : specialized_function->get_ops())
-            operationNames.emplace(op->get_friendly_name());
-
-        for (const auto& op : func->get_ops()) {
-            if (operationNames.find(op->get_friendly_name()) != operationNames.end())
-                continue;
-            res.supportedLayersMap[op->get_friendly_name()] = deviceName;
-        }
         return res;
     }
 
