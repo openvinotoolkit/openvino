c6373ff5a868d959a2ac45740738f97d4c9a4495
pszmel <piotr.szmelczynski@intel.com>
2020-10-09 09:22:48 +0200

    add repeats parameter to tile

--- ngraph/core/reference/include/ngraph/runtime/reference/tile.hpp
+++ ngraph/core/reference/include/ngraph/runtime/reference/tile.hpp
@@ -31,7 +31,8 @@ namespace ngraph
                       char* out,
                       const Shape& in_shape,
                       const Shape& out_shape,
-                      size_t elem_size);
+                      size_t elem_size,
+                      std::vector<int64_t> repeats);
         }
     }
 }
