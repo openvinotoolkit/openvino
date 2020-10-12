d30183dd1801ae603ec143bda5bfa27f51125f76
Rafal Blaczkowski <rafal.blaczkowski@intel.com>
2020-10-12 12:07:53 +0200

    Update OpenVino ONNX CI and skip hangning test (#2619)
    * Update OpenVino ONNX CI and skip hangning test
    
    * OpenVino ONNX CI imporvements:
    
    *improve cleaning
    *add strict timeout of tests execution

--- ngraph/python/tests/test_onnx/test_zoo_models.py
+++ ngraph/python/tests/test_onnx/test_zoo_models.py
@@ -37,7 +37,8 @@ from tests import (BACKEND_NAME,
                    xfail_issue_35926,
                    xfail_issue_36535,
                    xfail_issue_36537,
-                   xfail_issue_36538)
+                   xfail_issue_36538,
+                   skip_issue_40589)
 
 _GITHUB_MODELS_LTS = "https://media.githubusercontent.com/media/onnx/models/master/"
 
@@ -616,6 +617,8 @@ xfail_issue_36535(test_cases.test_tinyyolov2_opset8_cpu)
 xfail_issue_36537(test_cases.test_shufflenet_v2_opset10_cpu)
 xfail_issue_36538(test_cases.test_yolov3_opset10_cpu)
 xfail_issue_36538(test_cases.test_tiny_yolov3_opset11_cpu)
+
+skip_issue_40589(test_cases.test_tinyyolov2_opset1_cpu)
 
 for test_case in test_cases_list:
     xfail_issue_36533(test_case)
