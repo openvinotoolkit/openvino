2d70210ef08972dfd6656cdd5e6a256a335680f3
Ilya Churaev <ilya.churaev@intel.com>
2020-10-13 13:26:37 +0300

    Added tests

---
+++ inference-engine/tests/functional/inference_engine/ngraph_reader/constant_tests.cpp
@@ -0,0 +1,54 @@
+// Copyright (C) 2018-2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <string>
+#include "ngraph_reader_tests.hpp"
+
+using namespace InferenceEngine;
+
+TEST_F(NGraphReaderTests, ReadConstantNetwork) {
+    std::string model = R"V0G0N(
+<net name="Network" version="10">
+    <layers>
+        <layer id="0" name="constant" type="Const" version="opset1">
+            <data offset="0" size="5808"/>
+            <output>
+                <port id="0" precision="FP32">
+                    <dim>1</dim>
+                    <dim>3</dim>
+                    <dim>22</dim>
+                    <dim>22</dim>
+                </port>
+            </output>
+        </layer>
+        <layer name="output" type="Result" id="2" version="opset1">
+            <input>
+                <port id="0" precision="FP32">
+                    <dim>1</dim>
+                    <dim>3</dim>
+                    <dim>22</dim>
+                    <dim>22</dim>
+                </port>
+            </input>
+        </layer>
+    </layers>
+    <edges>
+        <edge from-layer="0" from-port="0" to-layer="2" to-port="0"/>
+    </edges>
+</net>
+)V0G0N";
+
+        Core ie;
+        Blob::Ptr weights;
+
+        weights = make_shared_blob<uint8_t>(TensorDesc(Precision::U8, {5808}, Layout::C));
+        weights->allocate();
+
+        auto network = ie.ReadNetwork(model, weights);
+
+        IE_SUPPRESS_DEPRECATED_START
+        auto convertedNetwork = std::make_shared<InferenceEngine::details::CNNNetworkImpl>(network);
+
+        IE_SUPPRESS_DEPRECATED_END
+}
