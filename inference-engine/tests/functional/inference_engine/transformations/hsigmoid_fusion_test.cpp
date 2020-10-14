7cf554535f48524557f114362a68aa723ffd7dce
Your Name <you@example.com>
2020-10-14 17:14:17 +0300

    fix unit tests

--- inference-engine/tests/functional/inference_engine/transformations/hsigmoid_fusion_test.cpp
+++ inference-engine/tests/functional/inference_engine/transformations/hsigmoid_fusion_test.cpp
@@ -10,7 +10,7 @@
 #include <ngraph/function.hpp>
 #include <ngraph/opsets/opset5.hpp>
 #include <ngraph/pass/manager.hpp>
-#include <transformations/hsigmoid_fusion.hpp>
+#include <transformations/common_optimizations/hsigmoid_fusion.hpp>
 #include <transformations/init_node_info.hpp>
 #include <transformations/utils/utils.hpp>
 
