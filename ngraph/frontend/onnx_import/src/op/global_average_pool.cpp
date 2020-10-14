ac6c78a68481326ac43edcc0a93b1a2430f6926f
Tomasz Socha <tomasz.socha@intel.com>
2020-10-14 12:32:32 +0200

    Add missing documentation

--- ngraph/frontend/onnx_import/src/op/global_average_pool.cpp
+++ ngraph/frontend/onnx_import/src/op/global_average_pool.cpp
@@ -44,11 +44,21 @@ namespace ngraph
                                  "Provided data rank is: ",
                                  data_rank_value);
 
+                    // Generate axes for reduce operation which contain all spatial dims indexes.
+                    // Examples:
+                    // Input shape: [N, C, H, W]
+                    // Input spatial dimensions are H and W
+                    // Expected spatial dims indexes: [2, 3]
+                    //
+                    // Input shape: [N, C, H, W, D]
+                    // Input spatial dimensions are H, W and D
+                    // Expected spatial dims indexes: [2, 3, 4]
                     uint64_t data_spatial_rank = data_rank_value - 2;
                     auto reduce_axes_vector = std::vector<std::int64_t>(data_spatial_rank);
                     std::iota(reduce_axes_vector.begin(), reduce_axes_vector.end(), 2);
                     auto reduce_axes = default_opset::Constant::create(
                         element::i64, Shape{data_spatial_rank}, reduce_axes_vector);
+
                     return {std::make_shared<default_opset::ReduceMean>(data, reduce_axes, true)};
                 }
 
