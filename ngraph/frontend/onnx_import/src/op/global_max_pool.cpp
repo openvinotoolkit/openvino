469c282d19ac2e434438b07d0ca450d6ccc1f84b
Tomasz Socha <tomasz.socha@intel.com>
2020-10-15 19:04:43 +0200

    [ONNX] Replace global poolings with reduce operations (#2650)

--- ngraph/frontend/onnx_import/src/op/global_max_pool.cpp
+++ ngraph/frontend/onnx_import/src/op/global_max_pool.cpp
@@ -14,10 +14,12 @@
 // limitations under the License.
 //*****************************************************************************
 
+#include <numeric>
+#include <vector>
+
 #include "global_max_pool.hpp"
 #include "ngraph/node.hpp"
-#include "ngraph/op/max_pool.hpp"
-#include "onnx_import/utils/pooling_factory.hpp"
+#include "onnx_import/default_opset.hpp"
 
 namespace ngraph
 {
@@ -29,7 +31,35 @@ namespace ngraph
             {
                 OutputVector global_max_pool(const Node& node)
                 {
-                    return pooling::GlobalPoolingFactory(node).make_max_pool();
+                    auto data = node.get_ng_inputs()[0];
+                    auto data_rank = data.get_partial_shape().rank();
+
+                    NGRAPH_CHECK(data_rank.is_static(),
+                                 "The input data tensor's rank has to be known (static)");
+
+                    auto data_rank_value = data_rank.get_length();
+
+                    NGRAPH_CHECK(data_rank_value > 2,
+                                 "The input data tensor's rank has to be greater than 2."
+                                 "Provided data rank is: ",
+                                 data_rank_value);
+
+                    // Generate axes for reduce operation which contain all spatial dims indexes.
+                    // Examples:
+                    // Input shape: [N, C, H, W]
+                    // Input spatial dimensions are H and W
+                    // Expected spatial dims indexes: [2, 3]
+                    //
+                    // Input shape: [N, C, H, W, D]
+                    // Input spatial dimensions are H, W and D
+                    // Expected spatial dims indexes: [2, 3, 4]
+                    uint64_t data_spatial_rank = data_rank_value - 2;
+                    auto reduce_axes_vector = std::vector<std::int64_t>(data_spatial_rank);
+                    std::iota(reduce_axes_vector.begin(), reduce_axes_vector.end(), 2);
+                    auto reduce_axes = default_opset::Constant::create(
+                        element::i64, Shape{data_spatial_rank}, reduce_axes_vector);
+
+                    return {std::make_shared<default_opset::ReduceMax>(data, reduce_axes, true)};
                 }
 
             } // namespace set_1
