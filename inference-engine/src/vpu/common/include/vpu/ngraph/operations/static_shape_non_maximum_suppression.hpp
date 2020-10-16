e1c769d91f3148f8b2eb8f60a0e302adbc011459
Bakalin, Andrew <andrew.bakalin@intel.com>
2020-10-16 10:43:33 +0300

    [VPU][NGraph] Update StaticShapeNMS to be inherrited from NMS-5

--- inference-engine/src/vpu/common/include/vpu/ngraph/operations/static_shape_non_maximum_suppression.hpp
+++ inference-engine/src/vpu/common/include/vpu/ngraph/operations/static_shape_non_maximum_suppression.hpp
@@ -5,33 +5,43 @@
 #pragma once
 
 #include <ngraph/node.hpp>
-#include <ngraph/op/op.hpp>
+#include <ngraph/op/non_max_suppression.hpp>
 
 #include <memory>
 #include <vector>
 
 namespace ngraph { namespace vpu { namespace op {
 
-class StaticShapeNonMaxSuppression : public ngraph::op::v4::NonMaxSuppression {
+class StaticShapeNonMaxSuppression : public ngraph::op::v5::NonMaxSuppression {
 public:
     static constexpr NodeTypeInfo type_info{"StaticShapeNonMaxSuppression", 0};
     const NodeTypeInfo& get_type_info() const override { return type_info; }
     StaticShapeNonMaxSuppression() = default;
 
     StaticShapeNonMaxSuppression(const Output<Node>& boxes,
-                      const Output<Node>& scores,
-                      const Output<Node>& max_output_boxes_per_class,
-                      const Output<Node>& iou_threshold,
-                      const Output<Node>& score_threshold,
-                      const BoxEncodingType box_encoding = BoxEncodingType::CORNER,
-                      const bool sort_result_descending = true,
-                      const ngraph::element::Type& output_type = ngraph::element::i64);
+                                 const Output<Node>& scores,
+                                 const BoxEncodingType box_encoding = BoxEncodingType::CORNER,
+                                 const bool sort_result_descending = true,
+                                 const ngraph::element::Type& output_type = ngraph::element::i64);
 
     StaticShapeNonMaxSuppression(const Output<Node>& boxes,
-                      const Output<Node>& scores,
-                      const BoxEncodingType box_encoding = BoxEncodingType::CORNER,
-                      const bool sort_result_descending = true,
-                      const ngraph::element::Type& output_type = ngraph::element::i64);
+                                 const Output<Node>& scores,
+                                 const Output<Node>& max_output_boxes_per_class,
+                                 const Output<Node>& iou_threshold,
+                                 const Output<Node>& score_threshold,
+                                 const BoxEncodingType box_encoding = BoxEncodingType::CORNER,
+                                 const bool sort_result_descending = true,
+                                 const ngraph::element::Type& output_type = ngraph::element::i64);
+
+    StaticShapeNonMaxSuppression(const Output<Node>& boxes,
+                                 const Output<Node>& scores,
+                                 const Output<Node>& max_output_boxes_per_class,
+                                 const Output<Node>& iou_threshold,
+                                 const Output<Node>& score_threshold,
+                                 const Output<Node>& soft_nms_sigma,
+                                 const BoxEncodingType box_encoding = BoxEncodingType::CORNER,
+                                 const bool sort_result_descending = true,
+                                 const ngraph::element::Type& output_type = ngraph::element::i64);
 
     void validate_and_infer_types() override;
     std::shared_ptr<Node> clone_with_new_inputs(const OutputVector& new_args) const override;
--- inference-engine/src/vpu/common/src/ngraph/operations/static_shape_non_maximum_suppression.cpp
+++ inference-engine/src/vpu/common/src/ngraph/operations/static_shape_non_maximum_suppression.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <ngraph/opsets/opset3.hpp>
+#include <ngraph/opsets/opset5.hpp>
 #include "vpu/ngraph/operations/static_shape_non_maximum_suppression.hpp"
 
 #include "ngraph/runtime/host_tensor.hpp"
@@ -14,30 +15,48 @@ constexpr NodeTypeInfo StaticShapeNonMaxSuppression::type_info;
 StaticShapeNonMaxSuppression::StaticShapeNonMaxSuppression(
         const Output<Node>& boxes,
         const Output<Node>& scores,
-        const Output<Node>& max_output_boxes_per_class,
-        const Output<Node>& iou_threshold,
-        const Output<Node>& score_threshold,
         const StaticShapeNonMaxSuppression::BoxEncodingType box_encoding,
         const bool sort_result_descending,
         const element::Type& output_type)
-        : ngraph::op::v4::NonMaxSuppression(
-            boxes, scores, max_output_boxes_per_class, iou_threshold, score_threshold,
-            box_encoding, sort_result_descending, output_type) {
+        : ngraph::op::v5::NonMaxSuppression(boxes,
+              scores,
+              ngraph::opset3::Constant::create(element::i64, Shape{}, {0}),
+              ngraph::opset3::Constant::create(element::f32, Shape{}, {.0f}),
+              ngraph::opset3::Constant::create(element::f32, Shape{}, {.0f}),
+              ngraph::opset5::Constant::create(element::f32, Shape{}, {.0f}),
+              box_encoding, sort_result_descending, output_type) {
     constructor_validate_and_infer_types();
 }
 
 StaticShapeNonMaxSuppression::StaticShapeNonMaxSuppression(
         const Output<Node>& boxes,
         const Output<Node>& scores,
+        const Output<Node>& max_output_boxes_per_class,
+        const Output<Node>& iou_threshold,
+        const Output<Node>& score_threshold,
         const StaticShapeNonMaxSuppression::BoxEncodingType box_encoding,
         const bool sort_result_descending,
         const element::Type& output_type)
-        : ngraph::op::v4::NonMaxSuppression(boxes,
-              scores,
-              ngraph::opset3::Constant::create(element::i64, Shape{}, {0}),
-              ngraph::opset3::Constant::create(element::f32, Shape{}, {.0f}),
-              ngraph::opset3::Constant::create(element::f32, Shape{}, {.0f}),
-              box_encoding, sort_result_descending, output_type) {
+        : ngraph::op::v5::NonMaxSuppression(
+        boxes, scores, max_output_boxes_per_class, iou_threshold, score_threshold,
+        ngraph::opset5::Constant::create(element::f32, Shape{}, {.0f}),
+        box_encoding, sort_result_descending, output_type) {
+    constructor_validate_and_infer_types();
+}
+
+StaticShapeNonMaxSuppression::StaticShapeNonMaxSuppression(
+        const Output<Node>& boxes,
+        const Output<Node>& scores,
+        const Output<Node>& max_output_boxes_per_class,
+        const Output<Node>& iou_threshold,
+        const Output<Node>& score_threshold,
+        const Output<Node>& soft_nms_sigma,
+        const StaticShapeNonMaxSuppression::BoxEncodingType box_encoding,
+        const bool sort_result_descending,
+        const element::Type& output_type)
+        : ngraph::op::v5::NonMaxSuppression(
+        boxes, scores, max_output_boxes_per_class, iou_threshold, score_threshold,
+        soft_nms_sigma, box_encoding, sort_result_descending, output_type) {
     constructor_validate_and_infer_types();
 }
 
@@ -64,15 +83,34 @@ StaticShapeNonMaxSuppression::clone_with_new_inputs(const OutputVector& new_args
 }
 
 void StaticShapeNonMaxSuppression::validate_and_infer_types() {
-    ngraph::op::v4::NonMaxSuppression::validate_and_infer_types();
+    ngraph::op::v5::NonMaxSuppression::validate_and_infer_types();
 
-    const auto out_shape = this->get_output_partial_shape(0);
-    NODE_VALIDATION_CHECK(this, out_shape.is_static(),
-                          "StaticShapeNonMaxSuppression output shape is not fully defined: ", out_shape);
+    const auto boxesPS = get_input_partial_shape(0);
+    const auto scoresPS = get_input_partial_shape(1);
 
-    set_output_size(2);
-    set_output_type(0, m_output_type, out_shape);
-    set_output_type(1, m_output_type, Shape{2});
+    auto outShape = get_output_partial_shape(0);
+
+    if (boxesPS.rank().is_static() && scoresPS.rank().is_static()) {
+        const auto numBoxesDim = boxesPS[1];
+        const auto maxOutputBoxesPerClassNode = input_value(2).get_node_shared_ptr();
+        if (numBoxesDim.is_static() && scoresPS[0].is_static() && scoresPS[1].is_static() &&
+            ngraph::op::is_constant(maxOutputBoxesPerClassNode)) {
+            const auto numBoxes = numBoxesDim.get_length();
+            const auto numClasses = scoresPS[1].get_length();
+            const auto maxOutputBoxesPerClass = max_boxes_output_from_input();
+
+            outShape[0] = std::min(numBoxes, maxOutputBoxesPerClass) * numClasses *
+                                    scoresPS[0].get_length();
+        }
+    }
+
+    NODE_VALIDATION_CHECK(this, outShape.is_static(),
+                          "StaticShapeNonMaxSuppression output shape is not fully defined: ", outShape);
+
+    set_output_size(4);
+    set_output_type(0, m_output_type, outShape);
+    set_output_type(1, element::f32, outShape);
+    set_output_type(3, m_output_type, Shape{2});
 }
 
 }  // namespace op
