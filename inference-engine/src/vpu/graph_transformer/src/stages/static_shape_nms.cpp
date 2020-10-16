3f4a2780204a0120b3b69941217af23b52b7a42f
Bakalin, Andrew <andrew.bakalin@intel.com>
2020-10-16 11:19:56 +0300

    [VPU][GT] Update StaticShapeNMS stage to work with updated NGraph op

--- inference-engine/src/vpu/graph_transformer/src/stages/static_shape_nms.cpp
+++ inference-engine/src/vpu/graph_transformer/src/stages/static_shape_nms.cpp
@@ -6,6 +6,7 @@
 #include <vpu/frontend/frontend.hpp>
 
 #include <ngraph/op/non_max_suppression.hpp>
+#include <precision_utils.h>
 
 #include <memory>
 #include <set>
@@ -53,28 +54,51 @@ private:
 }  // namespace
 
 void FrontEnd::parseStaticShapeNMS(const Model& model, const ie::CNNLayerPtr& layer, const DataVector& inputs, const DataVector& outputs) const {
-    VPU_THROW_UNLESS(inputs.size() >= 2 && inputs.size() <= 5,
-        "StaticShapeNMS parsing failed, expected number of input is in range [2, 5], but {} provided",
-        inputs.size());
-    VPU_THROW_UNLESS(outputs.size() == 2,
-        "StaticShapeNMS parsing failed, expected number of outputs: 2, but {} provided",
-        outputs.size());
+    VPU_THROW_UNLESS(inputs.size() == 6,
+        "StaticShapeNMS with name {} parsing failed, expected number of inputs: 6, but {} provided",
+        layer->name, inputs.size());
+    VPU_THROW_UNLESS(outputs.size() == 4,
+        "StaticShapeNMS with name {} parsing failed, expected number of outputs: 4, but {} provided",
+        layer->name, outputs.size());
+
+    const auto softNMSSigmaData = inputs[5];
+    VPU_THROW_UNLESS(softNMSSigmaData->usage() == DataUsage::Const,
+        "StaticShapeNMS with name {} parsing failed: softNMSSigma should have usage {} while it actually has {}",
+        layer->type, DataUsage::Const, softNMSSigmaData->usage());
+    VPU_THROW_UNLESS(softNMSSigmaData->desc().totalDimSize() == 1,
+        "StaticShapeNMS with name {} parsing failed: softNMSSigma input should contain 1 value, while it has {} values",
+        layer->type, softNMSSigmaData->desc().totalDimSize());
+    const auto softNMSSigma = InferenceEngine::PrecisionUtils::f16tof32(softNMSSigmaData->content()->get<InferenceEngine::ie_fp16>()[0]);
+    VPU_THROW_UNLESS(softNMSSigma == 0,
+        "StaticShapeNMS with name {} parsing failed: the only supported value for softNMSSigma is 0, while it actually equal to  {}",
+        layer->name, softNMSSigma);
+
+    auto usedInputs = inputs;
+    // Erase unused softNMSSigma input
+    usedInputs.pop_back();
+
+    const auto& outIndices = outputs[0];
+    const auto& outScores = outputs[1];
+    const auto& validOutputs = outputs[2];
+    const auto& outShape = outputs[3];
+
+    VPU_THROW_UNLESS(outScores == nullptr,
+        "StaticShapeNMS with name {} parsing failed: selected_scores output is not supported {}",
+        layer->name);
+    VPU_THROW_UNLESS(validOutputs == nullptr,
+        "StaticShapeNMS with name {} parsing failed: valid_outputs output is not supported {}",
+        layer->name);
 
     const auto sortResultDescending = layer->GetParamAsBool("sort_result_descending");
     const auto boxEncoding = layer->GetParamAsString("box_encoding");
 
     VPU_THROW_UNLESS(sortResultDescending == false,
-        "StaticShapeNMS: parameter sortResultDescending=true is not supported on VPU");
+        "StaticShapeNMS with name {}: parameter sortResultDescending=true is not supported on VPU", layer->name);
     VPU_THROW_UNLESS(boxEncoding == "corner" || boxEncoding == "center",
-        "StaticShapeNMS: boxEncoding currently supports only two values: \"corner\" and \"center\" "
-        "while {} was provided", boxEncoding);
+        "StaticShapeNMS with name {}: boxEncoding currently supports only two values: \"corner\" and \"center\" "
+        "while {} was provided", layer->name, boxEncoding);
 
-    DataVector tempInputs = inputs;
-    for (auto fake = inputs.size(); fake < 5; fake++) {
-        tempInputs.push_back(model->addFakeData());
-    }
-
-    auto stage = model->addNewStage<StaticShapeNMS>(layer->name, StageType::StaticShapeNMS, layer, tempInputs, outputs);
+    auto stage = model->addNewStage<StaticShapeNMS>(layer->name, StageType::StaticShapeNMS, layer, usedInputs, DataVector{outIndices, outShape});
     stage->attrs().set<bool>("center_point_box", boxEncoding == "center");
 }
 
