62a013226aac63ed737fee1882ea613d950452d5
rnugmanx <ruslanx.nugmanov@intel.com>
2020-10-17 19:47:17 +0300

    onnx (private) extractor

--- model-optimizer/extensions/load/onnx/loader.py
+++ model-optimizer/extensions/load/onnx/loader.py
@@ -20,10 +20,15 @@ from __future__ import unicode_literals
 
 import logging as log
 
+from onnx.mapping import TENSOR_TYPE_TO_NP_TYPE
+from onnx.numpy_helper import to_array
+
 from extensions.load.loader import Loader
+from mo.front.common.partial_infer.utils import int64_array
 from mo.front.common.register_custom_ops import update_extractors_with_extensions, check_for_duplicates
 from mo.front.extractor import extract_node_attrs
 from mo.front.onnx.extractor import onnx_op_extractor, onnx_op_extractors
+from mo.front.onnx.extractors.utils import onnx_attr
 from mo.front.onnx.loader import load_onnx_model, protobuf2nx
 from mo.graph.graph import Graph
 from mo.utils.error import Error
@@ -44,7 +49,6 @@ class ONNXLoader(Loader):
         log.debug("Number of initializers in graph_def: {}".format(len(model_graph.initializer)))
         log.debug(
             "Number of real inputs in graph_def: {}".format(len(model_graph.input) - len(model_graph.initializer)))
-        update_extractors_with_extensions(onnx_op_extractors)
 
         try:
             protobuf2nx(graph, model_proto)
@@ -69,4 +73,51 @@ class ONNXLoader(Loader):
             graph.graph['fw_opset_version'] = None
 
         graph.check_empty_graph('protobuf2nx. It may happen due to problems with loaded model')
+
+
+class ONNXExtractor(Loader):
+    id = 'ONNXExtractor'
+    enabled = True
+
+    def run_after(self):
+        return [ONNXLoader]
+
+    def load(self, graph: Graph):
+        update_extractors_with_extensions(onnx_op_extractors)
         extract_node_attrs(graph, lambda node: onnx_op_extractor(node, check_for_duplicates(onnx_op_extractors)))
+
+
+class ONNXPrivateExtractor(Loader):
+    id = 'ONNXPrivateExtractor'
+    enabled = False
+
+    def run_after(self):
+        return [ONNXLoader]
+
+    def load(self, graph: Graph):
+        extract_node_attrs(graph, lambda node: onnx_op_extractor(node, {}))
+        for node in graph.get_op_nodes():
+            # for Const
+            if node.has('pb_init'):
+                value = to_array(node.pb_init)
+                node['shape'] = int64_array(value.shape)
+                node['data_type'] = value.dtype
+                node['value'] = True
+            # for Constant
+            try:
+                pb_value = onnx_attr(node, 'value', 't')
+                if pb_value is not None:
+                    value = to_array(pb_value)
+                    node['shape'] = int64_array(value.shape)
+                    node['data_type'] = value.dtype
+                    node['value'] = True
+            except:
+                pass
+            # for Input (Parameter)
+            if node.has('pb'):
+                try:
+                    t_type = node.pb.type.tensor_type
+                    node['shape'] = int64_array([d.dim_value for d in t_type.shape.dim])
+                    node['data_type'] = TENSOR_TYPE_TO_NP_TYPE[t_type.elem_type]
+                except:
+                    pass
