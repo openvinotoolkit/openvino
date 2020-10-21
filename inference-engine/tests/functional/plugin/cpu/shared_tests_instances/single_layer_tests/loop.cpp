828f6cb94879353589e8ee7062b71b9fe5ce86ef
Alexander Peskov <alexander.peskov@intel.com>
2020-10-22 02:49:51 +0300

    [TEST] Several more Loop test with static shapes
    Signed-off-by: Alexander Peskov <alexander.peskov@intel.com>

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/single_layer_tests/loop.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/single_layer_tests/loop.cpp
@@ -3,7 +3,6 @@
 //
 
 #include <vector>
-#include <ngraph/op/util/attr_types.hpp>
 #include "single_layer_tests/loop.hpp"
 #include "common_test_utils/test_constants.hpp"
 
@@ -12,9 +11,9 @@ using namespace LayerTestsDefinitions;
 namespace {
     // without clip values increase rapidly, so use only seq_lenghts = 2
     std::vector<bool> execute_first_iteration{true};
-    std::vector<bool> is_body_condition_const{true, false};
-    std::vector<bool> body_condition{true, false}; // works only if is_body_condition_const == true
-    std::vector<int64_t> trip_count{1, 10, -1}; // -1 means infinity
+    std::vector<bool> is_body_condition_const{true/*, false*/};
+    std::vector<bool> body_condition{true/*, false*/}; // works only if is_body_condition_const == true
+    std::vector<int64_t> trip_count{1, 10/*, -1*/}; // -1 means infinity
     std::vector<std::vector<std::pair<std::vector<size_t>, LOOP_IN_TYPE>>> inputs = {
             {{{32, 1, 10}, LOOP_IN_TYPE::INVARIANT}, {{32, 1, 10}, LOOP_IN_TYPE::INVARIANT}, {{32, 1, 10}, LOOP_IN_TYPE::MERGED}},
     };
@@ -31,4 +30,27 @@ namespace {
                                     ::testing::ValuesIn(netPrecisions),
                                     ::testing::Values(CommonTestUtils::DEVICE_CPU)),
                             LoopTest::getTestCaseName);
+
+    static const std::vector<std::tuple<bool, int64_t, int64_t, int64_t>> static_loop_types = {
+            // static_trip_count |  max | dynamic_exit | axis
+            {  true ,  5, -1, -1 },  // n_iter 5, no dynamic exit
+            {  true ,  5,  3, -1 },  // n_iter 3, dynamic exit on 3
+            {  true ,  5,  7, -1 },  // n_iter 5, dynamic exit not reached
+            {  true , -1,  5, -1 },  // n_iter 5, inf loop with dynamic exit on 5
+            {  true ,  5, -1,  1 },  // n_iter 5, const for loop with auto concatenated out
+            { false ,  5, -1, -1 },  // |
+            { false ,  5,  3, -1 },  // | same with dynamic trip count
+            { false ,  5,  7, -1 },  // |
+            { false , -1,  5, -1 },  // |
+    };
+
+    using namespace testing;
+    INSTANTIATE_TEST_CASE_P(smoke_StaticShapeLoop, StaticShapeLoopTest,
+                            Combine(
+                                    Values(true),
+                                    ValuesIn(static_loop_types),
+                                    Values<int64_t>(7),
+                                    Values<InferenceEngine::SizeVector>({2, 1, 4}),
+                                    Values<InferenceEngine::Precision>(InferenceEngine::Precision::FP32, InferenceEngine::Precision::I32),
+                                    Values(CommonTestUtils::DEVICE_CPU)));
 }  // namespace
