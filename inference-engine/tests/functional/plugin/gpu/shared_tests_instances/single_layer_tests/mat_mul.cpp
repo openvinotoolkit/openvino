1f2c11ae255723f3fb360b4f829aed96c38b180b
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2020-10-22 10:49:56 +0300

    [IE CLDNN] New func test instances

---
+++ inference-engine/tests/functional/plugin/gpu/shared_tests_instances/single_layer_tests/mat_mul.cpp
@@ -0,0 +1,44 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <vector>
+
+#include "single_layer_tests/mat_mul.hpp"
+
+using namespace LayerTestsDefinitions;
+
+namespace {
+
+const std::vector<InferenceEngine::Precision> inputPrecisions = {
+        InferenceEngine::Precision::FP32
+};
+
+const std::vector<std::vector<size_t>> shapesA = {
+        {1, 4, 5, 6}
+};
+
+const std::vector<std::vector<size_t>> shapesB = {
+        {1, 4, 6, 4}
+};
+
+std::vector<ngraph::helpers::InputLayerType> secondaryInputTypes = {
+        ngraph::helpers::InputLayerType::CONSTANT,
+        ngraph::helpers::InputLayerType::PARAMETER,
+};
+
+INSTANTIATE_TEST_CASE_P(smoke_MatMul, MatMulTest,
+        ::testing::Combine(
+                ::testing::ValuesIn(inputPrecisions),
+                ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
+                ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
+                ::testing::Values(InferenceEngine::Layout::ANY),
+                ::testing::ValuesIn(shapesA),
+                ::testing::ValuesIn(shapesB),
+                ::testing::Values(false),
+                ::testing::Values(false),
+                ::testing::ValuesIn(secondaryInputTypes),
+                ::testing::Values(CommonTestUtils::DEVICE_GPU)),
+        MatMulTest::getTestCaseName);
+
+} // namespace
