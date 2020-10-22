6e5a1453d5afeb37a29da7e3d71c2aa75436213f
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2020-10-22 17:09:37 +0300

    [IE CLDNN] Updated MatMul test

--- inference-engine/tests/functional/plugin/gpu/shared_tests_instances/subgraph_tests/multiply_add.cpp
+++ inference-engine/tests/functional/plugin/gpu/shared_tests_instances/subgraph_tests/multiply_add.cpp
@@ -16,7 +16,6 @@ const std::vector<InferenceEngine::Precision> netPrecisions = {
 };
 
 const std::vector<std::vector<size_t>> inputShapes = {
-        {3},
         {1, 3},
         {1, 3, 2},
         {1, 3, 2, 5},
