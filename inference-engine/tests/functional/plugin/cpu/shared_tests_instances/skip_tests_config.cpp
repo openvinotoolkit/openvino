cdb829d9d4b622595a596ffba6ea176c15327435
Ilya Churaev <ilya.churaev@intel.com>
2020-10-22 13:22:38 +0300

    Constant->Result networks (#2639)
    * Added tests
    
    * Changed iterator algorithm
    
    * Fixed legacy tests
    
    * Added plugin tests
    
    * Disabled some tests
    
    * Remover parameter tests
    
    * Fixed conversion
    
    * Use old approach for old tests
    
    * Temp commit
    
    * Fixed iterator
    
    * Fixed some tests
    
    * Change logic to compare iterators
    
    * Disabled CPU functional test
    
    * Temp commit
    
    * Disabled test for GPU
    
    * Fixed network copy
    
    * Try to fix test for Windows
    
    * Disabled test for GNA
    
    * Disable plugin tests
    
    * Disable legacy test
    
    * Remove redundant code

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
@@ -36,6 +36,8 @@ std::vector<std::string> disabledTestPatterns() {
 #if (defined(_WIN32) || defined(_WIN64))
         R"(.*(CoreThreadingTestsWithIterations).*(smoke_LoadNetworkAccuracy).*)",
 #endif
+        // TODO: Issue: 40957
+        R"(.*(ConstantResultSubgraphTest).*)",
         // TODO: Issue: 34348
         R"(.*IEClassGetAvailableDevices.*)",
         // TODO: Issue: 25533
---
+++ inference-engine/tests/functional/plugin/gna/shared_tests_instances/subgraph_tests/constant_result.cpp
@@ -0,0 +1,17 @@
+// Copyright (C) 2019 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <vector>
+
+#include "subgraph_tests/constant_result.hpp"
+#include "common_test_utils/test_constants.hpp"
+
+using namespace LayerTestsDefinitions;
+
+namespace {
+    INSTANTIATE_TEST_CASE_P(smoke_Check, ConstantResultSubgraphTest,
+                            ::testing::Values(CommonTestUtils::DEVICE_GNA),
+                            ConstantResultSubgraphTest::getTestCaseName);
+}  // namespace
+
