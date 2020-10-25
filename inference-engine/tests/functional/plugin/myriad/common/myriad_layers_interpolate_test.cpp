1258128e7d46c0fa053371e3b9e5108c7acf2dfd
DariaMityagina <daria.mityagina@intel.com>
2020-10-25 06:40:46 +0300

    [IE][VPU] - Interpolate-4, code style

--- inference-engine/tests/functional/plugin/myriad/common/myriad_layers_interpolate_test.cpp
+++ inference-engine/tests/functional/plugin/myriad/common/myriad_layers_interpolate_test.cpp
@@ -5,25 +5,25 @@
 #include "myriad_layers_interpolate_test.hpp"
 
 // INSTANTIATE_TEST_CASE_P(
-// 	accuracy, myriadInterpolateLayerTests_nightly,
-// 	::testing::Combine(
-// 		::testing::ValuesIn(s_InterpolateInput),
-// 		::testing::Values<Factor>(2.0f, 0.5f),
-// 		::testing::Values<Antialias>(false),
-// 		::testing::Values<HwOptimization>(false, true),
-// 		::testing::Values(""))
+// accuracy, myriadInterpolateLayerTests_nightly,
+// ::testing::Combine(
+// ::testing::ValuesIn(s_InterpolateInput),
+// ::testing::Values<Factor>(2.0f, 0.5f),
+// ::testing::Values<Antialias>(false),
+// ::testing::Values<HwOptimization>(false, true),
+// ::testing::Values(""))
 // );
 
-// #ifdef VPU_HAS_CUSTOM_KERNELS
+//#ifdef VPU_HAS_CUSTOM_KERNELS
 
 // INSTANTIATE_TEST_CASE_P(
-// 	accuracy_custom, myriadInterpolateLayerTests_nightly,
-// 	::testing::Combine(
-// 		::testing::ValuesIn(s_InterpolateInput),
-// 		::testing::Values<Factor>(2.0f),
-// 		::testing::Values<Antialias>(false, true),
-// 		::testing::Values<HwOptimization>(false, true),
-// 		::testing::Values(s_CustomConfig[1]))
+// accuracy_custom, myriadInterpolateLayerTests_nightly,
+// ::testing::Combine(
+// ::testing::ValuesIn(s_InterpolateInput),
+// ::testing::Values<Factor>(2.0f),
+// ::testing::Values<Antialias>(false, true),
+// ::testing::Values<HwOptimization>(false, true),
+// ::testing::Values(s_CustomConfig[1]))
 // );
 
 // #endif
