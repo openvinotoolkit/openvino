166ab89b95ee3ac63431b27d82d5ac55aeb7a480
Ilya Lavrenov <ilya.lavrenov@intel.com>
2020-10-26 14:10:17 +0300

    Reorganize LPT: (#2803)
    - inference_engine_lp_transformations keep ngraph LPT
    - inference_engine_lp_transformations_legacy keep old CNNLayer based LPT

---
+++ inference-engine/src/low_precision_transformations/include/low_precision/relu.hpp
@@ -0,0 +1,27 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <memory>
+#include <ngraph/ngraph.hpp>
+#include "low_precision/layer_transformation.hpp"
+
+namespace ngraph {
+namespace pass {
+namespace low_precision {
+
+class TRANSFORMATIONS_API ReluTransformation : public LayerTransformation {
+public:
+    ReluTransformation(const Params& params) : LayerTransformation(params) {}
+    ~ReluTransformation() override {}
+    void registerMatcherIn(GraphRewrite& pass, TransformationContext& context) const override;
+    bool transform(TransformationContext& context, ngraph::pattern::Matcher &m) const override;
+    bool isPrecisionPreserved(std::shared_ptr<Node> layer) const noexcept override;
+    bool canBeTransformed(const TransformationContext& context, std::shared_ptr<Node> op) const override;
+};
+
+} // namespace low_precision
+} // namespace pass
+} // namespace ngraph
