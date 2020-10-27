f44738145fa6d6960542844fec4ae0e0845653ad
tomdol <tomasz.dolbniak@intel.com>
2020-10-27 12:49:57 -0400

    Convert op CF pass removal

--- ngraph/core/include/ngraph/pass/constant_folding.hpp
+++ ngraph/core/include/ngraph/pass/constant_folding.hpp
@@ -37,7 +37,6 @@ public:
     {
         m_cfmap = cfmap;
         m_enable_shape_inference = true;
-        construct_constant_convert();
         construct_constant_logical_reduction();
         construct_constant_gather_with_subgraph();
         construct_constant_scatter_elements_update();
@@ -47,7 +46,6 @@ public:
     }
 
 private:
-    void construct_constant_convert();
     void construct_constant_logical_reduction();
     void construct_constant_gather_with_subgraph();
     void construct_constant_scatter_elements_update();
