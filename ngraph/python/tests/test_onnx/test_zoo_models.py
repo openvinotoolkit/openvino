b74255688db32b12a1cc37024546163d0d846070
mbencer <mateusz.bencer@intel.com>
2020-10-27 19:24:19 +0100

    revert test data

--- ngraph/python/tests/test_onnx/test_zoo_models.py
+++ ngraph/python/tests/test_onnx/test_zoo_models.py
@@ -42,7 +42,7 @@ from tests import (
     xfail_issue_39669,
     xfail_issue_38726)
 
-MODELS_ROOT_DIR = "/shared/"
+MODELS_ROOT_DIR = tests.MODEL_ZOO_DIR
 
 tolerance_map = {
     "arcface_lresnet100e_opset8": {"atol": 0.001, "rtol": 0.001},
