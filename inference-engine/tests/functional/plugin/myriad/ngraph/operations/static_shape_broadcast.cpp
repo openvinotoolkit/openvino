fd08c6076dd5cc3aa38a5db059c13ea39693c55f
Polina Brzezinskaya <polina.brzezinskaya@intel.com>
2020-10-27 11:49:35 +0300

    [IE][VPU]: Bidirectional mode for broadcast on vpu (#2627)
    * Support for bidirectional broadcast mode

--- inference-engine/tests/functional/plugin/myriad/ngraph/operations/static_shape_broadcast.cpp
+++ inference-engine/tests/functional/plugin/myriad/ngraph/operations/static_shape_broadcast.cpp
@@ -30,8 +30,14 @@ struct BroadcastExplicitShapes {
     AxesMapping axesMapping;
 };
 
+struct BroadcastBidirectionalShapes {
+    TensorShape srcShape;
+    TensorShape targetShape;
+    TensorShape outputShape;
+};
 using BroadcastNumpyTestParams = std::tuple<TensorType, BroadcastNumpyShapes>;
 using BroadcastExplicitTestParams = std::tuple<TensorType, BroadcastExplicitShapes>;
+using BroadcastBidirectionalTestParams = std::tuple<TensorType, BroadcastBidirectionalShapes>;
 
 class StaticShapeBroadcastNumpyTests
         : public CommonTestUtils::TestsCommon,
@@ -73,6 +79,27 @@ protected:
     std::shared_ptr<ngraph::opset3::Constant> m_axesMapping;
 };
 
+class StaticShapeBroadcastBidirectionalTests
+        : public CommonTestUtils::TestsCommon,
+          public testing::WithParamInterface<BroadcastBidirectionalTestParams> {
+public:
+    void SetUp() override {
+        const auto& parameters  = GetParam();
+        const auto& tensorType  = std::get<0>(parameters);
+        const auto& tensorShape = std::get<1>(parameters).srcShape;
+        const auto& targetShape = std::get<1>(parameters).targetShape;
+        const auto& outputShape = std::get<1>(parameters).outputShape;
+
+        m_tensor = std::make_shared<ngraph::opset3::Parameter>(tensorType, tensorShape);
+        m_tensorWithTargetShape = std::make_shared<ngraph::opset3::Parameter>(tensorType, targetShape);
+        m_tensorWithOutput = std::make_shared<ngraph::opset3::Parameter>(tensorType, outputShape);
+    }
+protected:
+    std::shared_ptr<ngraph::opset3::Parameter> m_tensor;
+    std::shared_ptr<ngraph::opset3::Parameter> m_tensorWithTargetShape;
+    std::shared_ptr<ngraph::opset3::Parameter> m_tensorWithOutput;
+};
+
 std::vector<BroadcastNumpyShapes> testNumpyStaticShapes {
         BroadcastNumpyShapes{TensorShape{1, 100}, TensorShape{4, 100}},
         BroadcastNumpyShapes{TensorShape{1, 100}, TensorShape{2, 4, 100}},
@@ -82,6 +109,15 @@ std::vector<BroadcastNumpyShapes> testNumpyStaticShapes {
 std::vector<BroadcastExplicitShapes> testExplicitStaticShapes {
         BroadcastExplicitShapes{TensorShape{16}, TensorShape{1, 16, 50, 50}, AxesMapping{1}},
         BroadcastExplicitShapes{TensorShape{50, 50}, TensorShape{1, 50, 50, 16}, AxesMapping{1, 2}},
+};
+
+std::vector<BroadcastBidirectionalShapes> testBidirectionalStaticShapes {
+        BroadcastBidirectionalShapes{TensorShape{1, 100}, TensorShape{4, 100}, TensorShape{4, 100}},
+        BroadcastBidirectionalShapes{TensorShape{1, 100}, TensorShape{2, 4, 100}, TensorShape{2, 4, 100}},
+        BroadcastBidirectionalShapes{TensorShape{16, 1, 1}, TensorShape{2, 16, 50, 50}, TensorShape{2, 16, 50, 50}},
+        BroadcastBidirectionalShapes{TensorShape{4, 100}, TensorShape{1, 100}, TensorShape{4, 100}},
+        BroadcastBidirectionalShapes{TensorShape{2, 4, 100}, TensorShape{1, 100}, {2, 4, 100}},
+        BroadcastBidirectionalShapes{TensorShape{2, 16, 1, 50}, TensorShape{16, 50, 1}, TensorShape{2, 16, 50, 50}},
 };
 
 std::vector<ngraph::element::Type> testNGraphNumericTypes {
@@ -129,13 +165,30 @@ TEST_P(StaticShapeBroadcastExplicitTests, CanValidateAndInferTypes) {
     ASSERT_NO_THROW(std::make_shared<ngraph::Function>(
             ngraph::OutputVector{op->output(0)},
             ngraph::ParameterVector{m_tensor, m_tensorWithTargetShape}));
-    ASSERT_EQ(m_tensorWithTargetShape->get_shape(), op->output(0).get_shape());
+    ASSERT_EQ(m_tensorWithTargetShape->get_shape(), op->get_output_shape(0));
 }
 
 INSTANTIATE_TEST_CASE_P(smoke_NGraph, StaticShapeBroadcastExplicitTests, testing::Combine(
         testing::ValuesIn(testNGraphNumericTypes),
         testing::ValuesIn(testExplicitStaticShapes))
 );
+
+TEST_P(StaticShapeBroadcastBidirectionalTests, CanValidateAndInferTypes) {
+    const auto shapeOf = std::make_shared<ngraph::opset3::ShapeOf>(m_tensorWithTargetShape);
+    std::shared_ptr<ngraph::vpu::op::StaticShapeBroadcast> op;
+    ASSERT_NO_THROW(op = std::make_shared<ngraph::vpu::op::StaticShapeBroadcast>(
+            m_tensor, shapeOf, ngraph::op::BroadcastType::BIDIRECTIONAL));
+    ASSERT_NO_THROW(std::make_shared<ngraph::Function>(
+            ngraph::OutputVector{op->output(0)},
+            ngraph::ParameterVector{m_tensor, m_tensorWithTargetShape}));
+    ASSERT_EQ(m_tensorWithOutput->get_shape(), op->output(0).get_shape());
+}
+
+INSTANTIATE_TEST_CASE_P(smoke_NGraph, StaticShapeBroadcastBidirectionalTests, testing::Combine(
+        testing::ValuesIn(testNGraphNumericTypes),
+        testing::ValuesIn(testBidirectionalStaticShapes))
+);
+
 
 //
 // Negative tests
@@ -171,15 +224,18 @@ INSTANTIATE_TEST_CASE_P(smoke_NGraph, StaticShapeBroadcastExplicitTestsNegativeN
         testing::Values(testExplicitStaticShapes[0]))
 );
 
-using StaticShapeBroadcastTestsNegativeMode = StaticShapeBroadcastNumpyTests;
-TEST_P(StaticShapeBroadcastTestsNegativeMode, ThrowsOnInvalidMode) {
+using StaticShapeBroadcastBidirectionalTestsNegativeNumInputs = StaticShapeBroadcastBidirectionalTests;
+TEST_P(StaticShapeBroadcastBidirectionalTestsNegativeNumInputs, ThrowsOnInvalidNumInputs) {
     const auto shapeOf = std::make_shared<ngraph::opset3::ShapeOf>(m_tensorWithTargetShape);
+    const auto axesMapping = std::make_shared<ngraph::opset3::Constant>(
+            ngraph::element::u64, ngraph::Shape{1}, 0);
     std::shared_ptr<ngraph::vpu::op::StaticShapeBroadcast> op;
     ASSERT_THROW(op = std::make_shared<ngraph::vpu::op::StaticShapeBroadcast>(
-            m_tensor, shapeOf, ngraph::op::BroadcastType::BIDIRECTIONAL),
+            m_tensor, shapeOf, axesMapping, ngraph::op::BroadcastType::BIDIRECTIONAL),
                  ngraph::NodeValidationFailure);
 }
 
+using StaticShapeBroadcastTestsNegativeMode = StaticShapeBroadcastNumpyTests;
 INSTANTIATE_TEST_CASE_P(smoke_NGraph, StaticShapeBroadcastTestsNegativeMode, testing::Combine(
         testing::Values(ngraph::element::f16),
         testing::Values(testNumpyStaticShapes[0]))
