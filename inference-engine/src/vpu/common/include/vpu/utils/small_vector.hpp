b90e06cff04e695a7176be6d05c21417cdbb0b62
Vladislav Vinogradov <vlad.vinogradov@intel.com>
2020-10-28 12:20:26 +0300

    [IE][CMAKE] Fix build issues with custom configuration (#2851)
    * FASTER build enabled
    * MYRIAD plugin disabled

--- inference-engine/src/vpu/common/include/vpu/utils/small_vector.hpp
+++ inference-engine/src/vpu/common/include/vpu/utils/small_vector.hpp
@@ -305,7 +305,7 @@ public:
     const_reverse_iterator crend() const noexcept { return _base.crend(); }
 
     bool empty() const noexcept { return _base.empty(); }
-#if ENABLE_MYRIAD
+#ifdef ENABLE_MYRIAD
     int size() const noexcept { return static_cast<int>(_base.size()); }
 #else
     size_t size() const noexcept { return _base.size(); }
