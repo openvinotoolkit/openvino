36032d7889a88b6f6a92f10fd3352bd1df25808e
tomdol <tomasz.dolbniak@intel.com>
2020-10-29 12:01:48 -0400

    ScatterElements CF pass removal

--- ngraph/core/include/ngraph/pass/constant_folding.hpp
+++ ngraph/core/include/ngraph/pass/constant_folding.hpp
@@ -38,13 +38,11 @@ public:
         m_cfmap = cfmap;
         m_enable_shape_inference = true;
         construct_constant_gather_with_subgraph();
-        construct_constant_scatter_elements_update();
         construct_constant_default();
     }
 
 private:
     void construct_constant_gather_with_subgraph();
-    void construct_constant_scatter_elements_update();
     void construct_constant_default();
 
     bool cf_is_disabled(const std::shared_ptr<Node>&);
