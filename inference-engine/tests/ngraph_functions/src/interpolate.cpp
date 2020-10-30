3ca55a93ecf1a21b1bc63cfcbe8f41ee5c8995a9
DariaMityagina <daria.mityagina@intel.com>
2020-10-30 14:41:59 +0300

    [IE][VPU] - Interpolate-4, code style

--- inference-engine/tests/ngraph_functions/src/interpolate.cpp
+++ inference-engine/tests/ngraph_functions/src/interpolate.cpp
@@ -18,11 +18,11 @@ std::shared_ptr<ngraph::Node> makeInterpolate(const std::vector<ngraph::Output<N
     std::vector<size_t> padBegin(1, 0), padEnd(1, 0);
     float coeff = -0.75;
     ngraph::op::v4::Interpolate::InterpolateMode mode = ngraph::op::v4::Interpolate::InterpolateMode::nearest;
-    ngraph::op::v4::Interpolate::ShapeCalcMode shapeCalcMode = ngraph::op::v4::Interpolate::ShapeCalcMode::scales;
-    ngraph::op::v4::Interpolate::CoordinateTransformMode coordinateTransformMode = ngraph::op::v4::Interpolate::CoordinateTransformMode::half_pixel;
+    ngraph::op::v4::Interpolate::ShapeCalcMode shapeCalc = ngraph::op::v4::Interpolate::ShapeCalcMode::scales;
+    ngraph::op::v4::Interpolate::CoordinateTransformMode coordTrans = ngraph::op::v4::Interpolate::CoordinateTransformMode::half_pixel;
     ngraph::op::v4::Interpolate::NearestMode nearestMode = ngraph::op::v4::Interpolate::NearestMode::round_prefer_floor;
 
-    const ngraph::op::v4::Interpolate::InterpolateAttrs interpolateAttributes(mode, shapeCalcMode, padBegin, padEnd, coordinateTransformMode, nearestMode, antialias, coeff);
+    const ngraph::op::v4::Interpolate::InterpolateAttrs interpolateAttributes(mode, shapeCalc, padBegin, padEnd, coordTrans, nearestMode, antialias, coeff);
 
     return std::make_shared<ngraph::opset4::Interpolate>(in[0], in[1], scales, axes, interpolateAttributes);
 }
