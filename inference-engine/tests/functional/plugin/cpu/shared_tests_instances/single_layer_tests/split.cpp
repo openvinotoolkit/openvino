cb4ed68620f9f04d63c466b8d0ad57da54a809b5
Aleksandr Korolev <aleksandr.korolev@intel.com>
2020-11-02 16:29:59 +0300

    [IE TESTS] Split Layer Test refactoring (#2915)
    Co-authored-by: kora6 <kora6@github.com>

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/single_layer_tests/split.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/single_layer_tests/split.cpp
@@ -25,7 +25,22 @@ INSTANTIATE_TEST_CASE_P(smoke_NumSplitsCheck, SplitLayerTest,
                                 ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
                                 ::testing::Values(InferenceEngine::Layout::ANY),
                                 ::testing::Values(InferenceEngine::Layout::ANY),
-                                ::testing::Values(std::vector<size_t >({30, 30, 30, 30})),
+                                ::testing::Values(std::vector<size_t>({30, 30, 30, 30})),
+                                ::testing::Values(std::vector<size_t>({})),
+                                ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+                        SplitLayerTest::getTestCaseName);
+
+INSTANTIATE_TEST_CASE_P(smoke_splitWithUnusedOutputsTest, SplitLayerTest,
+                        ::testing::Combine(
+                                ::testing::Values(5),
+                                ::testing::Values(0),
+                                ::testing::ValuesIn(netPrecisions),
+                                ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
+                                ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
+                                ::testing::Values(InferenceEngine::Layout::ANY),
+                                ::testing::Values(InferenceEngine::Layout::ANY),
+                                ::testing::Values(std::vector<size_t>({30, 30, 30, 30})),
+                                ::testing::Values(std::vector<size_t>({0, 3})),
                                 ::testing::Values(CommonTestUtils::DEVICE_CPU)),
                         SplitLayerTest::getTestCaseName);
 }  // namespace
