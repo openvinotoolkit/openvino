fb304195bb393b3e2bb8b241d45f8b92505cb454
mbencer <mateusz.bencer@intel.com>
2020-11-05 13:09:04 +0100

    [ONNX Importer] fixed tests

--- ngraph/test/onnx/onnx_import_controlflow.in.cpp
+++ ngraph/test/onnx/onnx_import_controlflow.in.cpp
@@ -87,8 +87,8 @@ NGRAPH_TEST(${BACKEND_NAME}, onnx_controlflow_loop_2d_no_identity_termination_co
     test_case.add_input<float>({0.f, 0.f});
 
     test_case.add_expected_output<float>(Shape{1, 2}, {6.f, 6.f});
-    test_case.add_expected_output<float>(Shape{5, 2},
-                                         {1.f, 1.f, 2.f, 2.f, 3.f, 3.f, 4.f, 4.f, 5.f, 5.f});
+    test_case.add_expected_output<float>(
+        Shape{6, 2}, {1.f, 1.f, 2.f, 2.f, 3.f, 3.f, 4.f, 4.f, 5.f, 5.f, 6.f, 6.f});
     test_case.run();
 }
 
@@ -122,8 +122,8 @@ NGRAPH_TEST(${BACKEND_NAME}, onnx_controlflow_loop_2d_const_no_identity_terminat
     // a_init
     test_case.add_input<float>({0.f, 0.f});
 
-    test_case.add_expected_output<float>(Shape{1, 2}, {3.f, 3.f});
-    test_case.add_expected_output<float>(Shape{3, 2}, {1.f, 1.f, 2.f, 2.f, 3.f, 3.f});
+    test_case.add_expected_output<float>(Shape{1, 2}, {4.f, 4.f});
+    test_case.add_expected_output<float>(Shape{4, 2}, {1, 1, 2, 2, 3, 3, 4, 4});
     test_case.run();
 }
 
@@ -148,9 +148,9 @@ NGRAPH_TEST(${BACKEND_NAME}, onnx_controlflow_loop_2d_both_cond_and_trip_count_a
     // a_init
     test_case.add_input<float>({0.f, 0.f});
 
-    test_case.add_expected_output<float>(Shape{1, 2}, {5.f, 5.f});
-    test_case.add_expected_output<float>(Shape{5, 2},
-                                         {1.f, 1.f, 2.f, 2.f, 3.f, 3.f, 4.f, 4.f, 5.f, 5.f});
+    test_case.add_expected_output<float>(Shape{1, 2}, {6.f, 6.f});
+    test_case.add_expected_output<float>(
+        Shape{6, 2}, {1.f, 1.f, 2.f, 2.f, 3.f, 3.f, 4.f, 4.f, 5.f, 5.f, 6.f, 6.f});
     test_case.run();
 }
 
