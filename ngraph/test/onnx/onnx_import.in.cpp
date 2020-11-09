7df93863cc121e1c95006c7a7820cd09a597319a
pszmel <piotr.szmelczynski@intel.com>
2020-11-09 14:59:22 +0100

    create onnx_model_hard_sigmoid test

--- ngraph/test/onnx/onnx_import.in.cpp
+++ ngraph/test/onnx/onnx_import.in.cpp
@@ -2783,3 +2783,21 @@ NGRAPH_TEST(${BACKEND_NAME}, onnx_model_logsoftmax13_2D)
                                           -0.44018966});
     test_case.run_with_tolerance_as_fp();
 }
+
+NGRAPH_TEST(${BACKEND_NAME}, onnx_model_hard_sigmoid)
+{
+    auto function = onnx_import::import_onnx_model(
+        file_util::path_join(SERIALIZED_ZOO, "onnx/hard_sigmoid.prototxt"));
+    
+    const auto inf = std::numeric_limits<float>::infinity();
+    const auto neg_inf = -std::numeric_limits<float>::infinity();
+    Inputs inputs;
+    inputs.emplace_back(std::vector<float>{inf, neg_inf, 0.0f, 1.0f});
+    inputs.emplace_back(0.2f);
+    inputs.emplace_back(0.5f);
+
+    auto test_case = test::TestCase<TestEngine>(function);
+    test_case.add_multiple_inputs(inputs);
+    test_case.add_expected_output<float>(Shape{4}, {1.0f, 0.0f, 0.5f, 0.699999988079071f});
+    test_case.run();
+}
