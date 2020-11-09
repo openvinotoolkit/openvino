70a8261c75e123a26c1b7dc3705e996876535ec5
Mikhail Treskin <mikhail.treskin@intel.com>
2020-11-09 19:43:14 +0300

    Fix disable_tests_skipping symbol redefinition

--- inference-engine/tests/ie_test_utils/functional_test_utils/skip_tests_config.hpp
+++ inference-engine/tests/ie_test_utils/functional_test_utils/skip_tests_config.hpp
@@ -14,19 +14,9 @@ std::vector<std::string> disabledTestPatterns();
 namespace FuncTestUtils {
 namespace SkipTestsConfig {
 
-bool disable_tests_skipping = false;
+extern bool disable_tests_skipping;
 
-inline bool currentTestIsDisabled() {
-    bool skip_test = false;
-    const auto fullName = ::testing::UnitTest::GetInstance()->current_test_info()->test_case_name()
-                          + std::string(".") + ::testing::UnitTest::GetInstance()->current_test_info()->name();
-    for (const auto &pattern : disabledTestPatterns()) {
-        std::regex re(pattern);
-        if (std::regex_match(fullName, re))
-            skip_test = true;
-    }
-    return skip_test && !disable_tests_skipping;
-}
+bool currentTestIsDisabled();
 
 }  // namespace SkipTestsConfig
 }  // namespace FuncTestUtils
