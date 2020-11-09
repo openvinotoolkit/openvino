371d44639d0f5dabf1cee54219f924d8f72a1aac
VGavrilov <vladimir.gavrilov@intel.com>
2020-11-09 17:33:33 +0300

    Code style fix.

--- ngraph/test/runtime/interpreter/int_executable.cpp
+++ ngraph/test/runtime/interpreter/int_executable.cpp
@@ -208,8 +208,8 @@ runtime::interpreter::INTExecutable::InfoForNMS5
     result.boxes_shape = inputs[boxes_port]->get_shape();
     result.scores_shape = inputs[scores_port]->get_shape();
 
-    result.boxes_data = prepare_boxes_data(
-        inputs[boxes_port], result.boxes_shape, nms5->get_box_encoding());
+    result.boxes_data =
+        prepare_boxes_data(inputs[boxes_port], result.boxes_shape, nms5->get_box_encoding());
     result.scores_data = prepare_scores_data(inputs[scores_port], result.scores_shape);
 
     result.out_shape_size = shape_size(result.out_shape);
