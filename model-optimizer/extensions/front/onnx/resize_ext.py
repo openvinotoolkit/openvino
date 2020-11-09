082e64f3c3fe7cbeffcb00f7481898daadfd2948
VGavrilov <vladimir.gavrilov@intel.com>
2020-11-09 10:12:06 +0300

    Added MO operation ONNXResize10 for ONNX Resize-10. Written the transformation that converts ONNXResize10 to Interpolate-4.

--- model-optimizer/extensions/front/onnx/resize_ext.py
+++ model-optimizer/extensions/front/onnx/resize_ext.py
@@ -1,12 +1,9 @@
 """
  Copyright (C) 2018-2020 Intel Corporation
-
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
-
       http://www.apache.org/licenses/LICENSE-2.0
-
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,7 +11,7 @@
  limitations under the License.
 """
 
-from extensions.ops.upsample import UpsampleOp
+from extensions.ops.ONNXResize10 import ONNXResize10Op
 from extensions.ops.ONNXResize11 import ONNXResize11Op
 from mo.front.extractor import FrontExtractorOp
 from mo.front.onnx.extractors.utils import onnx_attr, get_onnx_opset_version
@@ -43,5 +40,5 @@ class ResizeExtractor(FrontExtractorOp):
             ONNXResize11Op.update_node_stat(node, attrs)
         else:
             mode = onnx_attr(node, 'mode', 's', default=b'nearest').decode()
-            UpsampleOp.update_node_stat(node, {'mode': mode})
+            ONNXResize10Op.update_node_stat(node, {'mode': mode})
         return cls.enabled
