2b850a1af5e8ea26d03400ecdd0e7b4afca1ad76
myshevts <maxim.y.shevtsov@intel.com>
2020-11-11 19:45:02 +0300

    Tests for remote blobs support via MULTI: creating the shared_test in case the other (closed source) plugins would want to use that (in the private shared_tests instantiations). Also instantiating the remote blobs tests for the some basic combinations to test the MULTI supports them

---
+++ inference-engine/tests/functional/plugin/shared/src/multi/remote_blob_tests.cpp
@@ -0,0 +1,41 @@
+// Copyright (C) 2018-2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <string>
+#include <vector>
+#include <memory>
+
+#include "functional_test_utils/plugin_cache.hpp"
+#include "multi/multi_helpers.hpp"
+
+using namespace ::testing;
+using namespace InferenceEngine;
+
+TEST_P(MultiDevice_Test, canCreateContextThenRequestThenBlobsAndInfer) {
+    InferenceEngine::CNNNetwork net;
+    net = CNNNetwork(fn_ptr);
+    net.getInputsInfo().begin()->second->setLayout(Layout::NCHW);
+    net.getInputsInfo().begin()->second->setPrecision(Precision::U8);
+
+    auto ie = InferenceEngine::Core();
+
+    auto exec_net = ie.LoadNetwork(net, device_names);
+    if (expected_status) {
+        InferenceEngine::RemoteContext::Ptr ctx;
+        ASSERT_NE(ctx = exec_net.GetContext(), nullptr);
+        InferRequest req = exec_net.CreateInferRequest();
+        ASSERT_NE((std::shared_ptr<InferenceEngine::IInferRequest>)req, nullptr);
+        const InferenceEngine::ConstInputsDataMap inputInfo = exec_net.GetInputsInfo();
+        for (auto i : inputInfo) {
+            auto rblob = InferenceEngine::make_shared_blob(i.second->getTensorDesc(), ctx);
+            rblob->allocate();
+            req.SetBlob(i.first, rblob);
+        }
+        ASSERT_NO_THROW(req.StartAsync());
+        ASSERT_EQ(req.Wait(IInferRequest::RESULT_READY), StatusCode::OK);
+
+    } else {
+        ASSERT_THROW(exec_net.GetContext(), InferenceEngine::NotImplemented);
+    }
+}
