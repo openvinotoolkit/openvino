e6d9a0cedd4a7177c22b4aa164ccf0fa55de000c
VGavrilov <vladimir.gavrilov@intel.com>
2020-11-12 17:30:40 +0300

    Some additions.

--- inference-engine/src/legacy_api/src/transformations/convert_opset1_to_legacy/ssd_pattern_before_topk_fusion.cpp
+++ inference-engine/src/legacy_api/src/transformations/convert_opset1_to_legacy/ssd_pattern_before_topk_fusion.cpp
@@ -27,27 +27,70 @@ ngraph::pass::PatternBeforeTopKFusion::PatternBeforeTopKFusion() {
     auto m_gather_ie = ngraph::pattern::wrap_type<op::GatherIE>({m_shape_of, m_indices, m_gather_axis}, pattern::consumers_count(1));
     auto m_fst_unsqueeze_axis = ngraph::pattern::wrap_type<opset5::Constant>();
     auto m_fst_unsqueeze = ngraph::pattern::wrap_type<opset5::Unsqueeze>({m_gather_ie, m_fst_unsqueeze_axis}, pattern::consumers_count(1));
-    auto m_concat_axis = ngraph::pattern::wrap_type<opset5::Constant>();
-    auto m_concat = ngraph::pattern::wrap_type<opset5::Concat>({m_fst_unsqueeze, m_concat_axis}, pattern::consumers_count(1));
+    auto m_concat_const = ngraph::pattern::wrap_type<opset5::Constant>();
+    auto m_concat = ngraph::pattern::wrap_type<opset5::Concat>({m_concat_const, m_fst_unsqueeze}, pattern::consumers_count(1));
     auto m_fst_convert = ngraph::pattern::wrap_type<opset5::Convert>({m_concat}, pattern::consumers_count(1));
     auto m_reduce_axes = ngraph::pattern::wrap_type<opset5::Constant>();
     auto m_reduce =  ngraph::pattern::wrap_type<opset5::ReduceMin>({m_fst_convert, m_reduce_axes}, pattern::consumers_count(1));
     auto m_snd_convert = ngraph::pattern::wrap_type<opset5::Convert>({m_reduce}, pattern::consumers_count(1));
     auto m_snd_unsqueeze_axis = ngraph::pattern::wrap_type<opset5::Constant>();
-    auto m_snd_unsqueeze = ngraph::pattern::wrap_type<opset5::Unsqueeze>({m_snd_convert, snd_unsqueeze_axis});
+    auto m_snd_unsqueeze = ngraph::pattern::wrap_type<opset5::Unsqueeze>({m_snd_convert, m_snd_unsqueeze_axis});
 
-    ngraph::matcher_pass_callback callback = [=](ngraph::pattern::Matcher & m) -> bool {
+    auto is_applicable = [=](ngraph::pattern::Matcher & m) -> bool {
         auto & label_to_output = m.get_pattern_value_map();
 
         auto shape_of = label_to_output[m_shape_of].get_node_shared_ptr();
         auto data = label_to_output[m_data].get_node_shared_ptr();
-        auto data_shape = data->get_partial_shape();
+        auto data_shape = shape_of->get_input_partial_shape(0);
 
         if (data_shape.rank().is_dynamic() || data_shape.is_dynamic()) {
             return false;
         }
 
-        auto indices
+        auto indices = label_to_output[m_indices].get_node_shared_ptr();
+        auto indices_vector = indices->cast_vector<int64_t>();
+        if (indices_vector.size() != 1 || indices_vector[0] != 0) {
+            return false;
+        }
+
+        auto gather_axis = label_to_output[m_gather_axis].get_node_shared_ptr();
+        auto gather_axis_vector = gather_axis->cast_vector<int64_t>();
+        if (gather_axis_vector.size() != 1 || gather_axis_vector[0] != 0) {
+            return false;
+        }
+
+        auto fst_unsqueeze_axis = label_to_output[m_fst_unsqueeze_axis].get_node_shared_ptr();
+        auto fst_unsqueeze_axis_vector = fst_unsqueeze_axis->cast_vector<int64_t>();
+        if (fst_unsqueeze_axis_vector.size() != 1 || fst_unsqueeze_axis_vector[0] != 0) {
+            return false;
+        }
+
+        auto snd_unsqueeze_axis = label_to_output[m_snd_unsqueeze_axis].get_node_shared_ptr();
+        auto snd_unsqueeze_axis_vector = snd_unsqueeze_axis->cast_vector<int64_t>();
+        if (snd_unsqueeze_axis_vector.size() != 1 || snd_unsqueeze_axis_vector[0] != 0) {
+            return false;
+        }
+
+        auto concat_const = label_to_output[m_concat_const].get_node_shared_ptr();
+        auto concat_const_vector = concat_const->cast_vector<int64_t>();
+        if (concat_const_vector.size() != 1) {
+            return false;
+        }
+
+        return true;
+    };
+
+    auto folding = [=](ngraph::pattern::Matcher & m) -> Output<Node> {
+        Output<Node> result;
+        return result;
+    };
+
+    ngraph::matcher_pass_callback callback = [=](ngraph::pattern::Matcher & m) -> bool {
+        if (!is_applicable(m)) {
+            return false;
+        }
+
+        auto folded_constant = folding(m);
 
         return true;
     };
