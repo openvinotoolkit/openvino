5a5546bbaee12fb6b4dac364489496c5bdd1aedd
Alexander Peskov <alexander.peskov@intel.com>
2020-11-12 06:22:21 +0300

    Fix softmax
    Signed-off-by: Alexander Peskov <alexander.peskov@intel.com>

--- inference-engine/src/mkldnn_plugin/nodes/common/softmax.cpp
+++ inference-engine/src/mkldnn_plugin/nodes/common/softmax.cpp
@@ -49,7 +49,7 @@ struct jit_uni_softmax_kernel_f32 : public jit_uni_softmax_kernel, public jit_ge
     }
 
     void generate() override {
-        exp_injector.reset(new jit_uni_eltwise_injector_f32<isa>(this, mkldnn::impl::alg_kind::eltwise_exp, 0.f, 0.f, 0.0f));
+        exp_injector.reset(new jit_uni_eltwise_injector_f32<isa>(this, mkldnn::impl::alg_kind::eltwise_exp, 0.f, 0.f, 1.0f));
 
         this->preamble();
 
