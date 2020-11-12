5d83ead32c91606c33d1fdf007daeafeafe10788
Svetlana Dolinina <svetlana.a.dolinina@intel.com>
2020-11-12 12:40:43 +0300

    Move QueryState from ExecutableNetwork to InferRequest (#2818)
    * QueryState moved to InferRequest
    
    * deprecate ExecutableNetwork::QueryState,chaged tests (without any check yet)
    
    * fix build
    
    * review fixes + build fix
    
    * build fix + review changes
    
    * remove blank line
    
    * style fixes
    
    * test build fixes
    
    * style fix
    
    * style fix
    
    * fixed build of tests
    
    * fix
    
    * mac build fix
    
    * hddl plugin build fix
    
    * clean up unneeded implementation for method
    
    * fixed tests build
    
    * add implementation for getstate, correct getName for MklDNN
    
    * fixed description of state API in comments
    
    * lint fixes
    
    * Rename MemoryState to VariableState
    
    * added tests for cpu for VariableStates, several small fixes in tests and code
    
    * merge fix
    
    * lint fix
    
    * remove whitespaces
    
    * spaces fix
    
    * fix in test to make it workable for all plugins
    
    * fix typo
    
    * fix test for gna
    
    * remove extra comment
    
    * fix test for gna

--- inference-engine/src/gna_plugin/gna_plugin.cpp
+++ inference-engine/src/gna_plugin/gna_plugin.cpp
@@ -1186,11 +1186,11 @@ Blob::Ptr GNAPlugin::GetInputBlob(const std::string& name, InferenceEngine::Prec
     return inputBlob;
 }
 
-std::vector<InferenceEngine::MemoryStateInternal::Ptr>  GNAPlugin::QueryState() {
+std::vector<InferenceEngine::VariableStateInternal::Ptr>  GNAPlugin::QueryState() {
     if (memoryStates.size() != graphCompiler.memory_connection.size()) {
         memoryStates.clear();
         for (auto& connection : graphCompiler.memory_connection) {
-            auto state = std::make_shared<memory::GNAMemoryState>(connection.first, std::make_shared <GNAMemoryLayer>(connection.second));
+            auto state = std::make_shared<memory::GNAVariableState>(connection.first, std::make_shared <GNAMemoryLayer>(connection.second));
             memoryStates.emplace_back(state);
         }
     }
