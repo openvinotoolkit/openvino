openvinotoolkit/openvino

commit 3d00910b70c94271c4fbed567419e5f8ec4a0356

Propogate DEVICE_ID for functions working with RemoteContext (#3109)
* Propogate DEVICE_ID for functions working with RemoteContext

* More fixes for RemoteContext

* Fixed tests compilation with VariableState

inference-engine/include/gpu/gpu_ocl_wrapper.hpp
@@ -13,17 +13,34 @@
 /**
 * @brief Definitions required by Khronos headers
 */
-#define CL_HPP_ENABLE_EXCEPTIONS
-#define CL_HPP_MINIMUM_OPENCL_VERSION 120
-#define CL_HPP_TARGET_OPENCL_VERSION 120
 
-#if defined __GNUC__
+#ifndef CL_HPP_ENABLE_EXCEPTIONS
+# define CL_HPP_ENABLE_EXCEPTIONS
+#endif
+
+#ifdef CL_HPP_MINIMUM_OPENCL_VERSION
+# if CL_HPP_MINIMUM_OPENCL_VERSION <= 120
+#  error "CL_HPP_MINIMUM_OPENCL_VERSION must be >= 120"
+# endif
+#else
+# define CL_HPP_MINIMUM_OPENCL_VERSION 120
+#endif
+
+#ifdef CL_HPP_TARGET_OPENCL_VERSION
+# if CL_HPP_TARGET_OPENCL_VERSION <= 120
+#  error "CL_HPP_TARGET_OPENCL_VERSION must be >= 120"
+# endif
+#else
+# define CL_HPP_TARGET_OPENCL_VERSION 120
+#endif
+
+#ifdef __GNUC__
 # pragma GCC diagnostic push
 # pragma GCC system_header
 #endif
 
 #include <CL/cl2.hpp>
 
-#if defined __GNUC__
+#ifdef __GNUC__
 # pragma GCC diagnostic pop
 #endif

inference-engine/src/cldnn_engine/cldnn_engine.cpp
@@ -363,9 +363,9 @@ RemoteContext::Ptr clDNNEngine::CreateContext(const ParamMap& params) {
     }
 }
 
-RemoteContext::Ptr clDNNEngine::GetDefaultContext() {
+RemoteContext::Ptr clDNNEngine::GetDefaultContext(const ParamMap& params) {
     if (nullptr == m_defaultContext) {
-        m_defaultContext.reset(new CLDNNRemoteCLContext(shared_from_this(), ParamMap(), _impl->m_config));
+        m_defaultContext.reset(new CLDNNRemoteCLContext(shared_from_this(), params, _impl->m_config));
     }
     return std::dynamic_pointer_cast<RemoteContext>(m_defaultContext);
 }

inference-engine/src/cldnn_engine/cldnn_engine.h
@@ -46,7 +46,7 @@ public:
                                                      const std::map<std::string, std::string>& config) const override;
 
     InferenceEngine::RemoteContext::Ptr CreateContext(const InferenceEngine::ParamMap& params) override;
-    InferenceEngine::RemoteContext::Ptr GetDefaultContext() override;
+    InferenceEngine::RemoteContext::Ptr GetDefaultContext(const ParamMap& params) override;
 };
 
 };  // namespace CLDNNPlugin

inference-engine/src/gna_plugin/gna_plugin.hpp
@@ -123,7 +123,7 @@ class GNAPlugin : public InferenceEngine::IInferencePlugin {
     InferenceEngine::Parameter GetMetric(const std::string& name,
                                          const std::map<std::string, InferenceEngine::Parameter> & options) const override;
     InferenceEngine::RemoteContext::Ptr CreateContext(const InferenceEngine::ParamMap& params) override { THROW_GNA_EXCEPTION << "Not implemented"; }
-    InferenceEngine::RemoteContext::Ptr GetDefaultContext() override { THROW_GNA_EXCEPTION << "Not implemented"; }
+    InferenceEngine::RemoteContext::Ptr GetDefaultContext(const InferenceEngine::ParamMap&) override { THROW_GNA_EXCEPTION << "Not implemented"; }
 
     void Wait(uint32_t sync, InferenceEngine::Blob &result) { THROW_GNA_EXCEPTION << "Not implemented"; }
 

inference-engine/src/inference_engine/ie_core.cpp
@@ -598,45 +598,37 @@ void Core::AddExtension(const IExtensionPtr& extension) {
 ExecutableNetwork Core::LoadNetwork(const CNNNetwork& network, RemoteContext::Ptr context,
                                     const std::map<std::string, std::string>& config) {
     OV_ITT_SCOPED_TASK(itt::domains::IE, "Core::LoadNetwork");
-    std::map<std::string, std::string> config_ = config;
 
     if (context == nullptr) {
         THROW_IE_EXCEPTION << "Remote context is null";
     }
 
-    std::string deviceName_ = context->getDeviceName();
-    DeviceIDParser device(deviceName_);
-    std::string deviceName = device.getDeviceName();
-
-    return _impl->GetCPPPluginByName(deviceName).LoadNetwork(network, config_, context);
+    auto parsed = parseDeviceNameIntoConfig(context->getDeviceName(), config);
+    return _impl->GetCPPPluginByName(parsed._deviceName).LoadNetwork(network, parsed._config, context);
 }
 
-RemoteContext::Ptr Core::CreateContext(const std::string& deviceName_, const ParamMap& params) {
-    if (deviceName_.find("HETERO") == 0) {
-        THROW_IE_EXCEPTION << "HETERO device does not support remote contexts";
+RemoteContext::Ptr Core::CreateContext(const std::string& deviceName, const ParamMap& params) {
+    if (deviceName.find("HETERO") == 0) {
+        THROW_IE_EXCEPTION << "HETERO device does not support remote context";
     }
-    if (deviceName_.find("MULTI") == 0) {
-        THROW_IE_EXCEPTION << "MULTI device does not support remote contexts";
+    if (deviceName.find("MULTI") == 0) {
+        THROW_IE_EXCEPTION << "MULTI device does not support remote context";
     }
 
-    DeviceIDParser device(deviceName_);
-    std::string deviceName = device.getDeviceName();
-
-    return _impl->GetCPPPluginByName(deviceName).CreateContext(params);
+    auto parsed = parseDeviceNameIntoConfig(deviceName, params);
+    return _impl->GetCPPPluginByName(parsed._deviceName).CreateContext(parsed._config);
 }
 
-RemoteContext::Ptr Core::GetDefaultContext(const std::string& deviceName_) {
-    if (deviceName_.find("HETERO") == 0) {
-        THROW_IE_EXCEPTION << "HETERO device does not support remote contexts";
+RemoteContext::Ptr Core::GetDefaultContext(const std::string& deviceName) {
+    if (deviceName.find("HETERO") == 0) {
+        THROW_IE_EXCEPTION << "HETERO device does not support remote context";
     }
-    if (deviceName_.find("MULTI") == 0) {
-        THROW_IE_EXCEPTION << "MULTI device does not support remote contexts";
+    if (deviceName.find("MULTI") == 0) {
+        THROW_IE_EXCEPTION << "MULTI device does not support remote context";
     }
 
-    DeviceIDParser device(deviceName_);
-    std::string deviceName = device.getDeviceName();
-
-    return _impl->GetCPPPluginByName(deviceName).GetDefaultContext();
+    auto parsed = parseDeviceNameIntoConfig(deviceName, ParamMap());
+    return _impl->GetCPPPluginByName(parsed._deviceName).GetDefaultContext(parsed._config);
 }
 
 void Core::AddExtension(IExtensionPtr extension, const std::string& deviceName_) {

inference-engine/src/inference_engine/ie_plugin_cpp.hpp
@@ -118,8 +118,8 @@ public:
         CALL_STATEMENT(return actual->CreateContext(params));
     }
 
-    RemoteContext::Ptr GetDefaultContext() {
-        CALL_STATEMENT(return actual->GetDefaultContext());
+    RemoteContext::Ptr GetDefaultContext(const ParamMap& params) {
+        CALL_STATEMENT(return actual->GetDefaultContext(params));
     }
 
     ExecutableNetwork ImportNetwork(std::istream& networkModel,

inference-engine/src/plugin_api/cpp_interfaces/impl/ie_plugin_internal.hpp
@@ -150,7 +150,7 @@ public:
         THROW_IE_EXCEPTION << NOT_IMPLEMENTED_str;
     }
 
-    RemoteContext::Ptr GetDefaultContext() override {
+    RemoteContext::Ptr GetDefaultContext(const ParamMap& /*params*/) override {
         THROW_IE_EXCEPTION << NOT_IMPLEMENTED_str;
     }
 

inference-engine/src/plugin_api/cpp_interfaces/interface/ie_iplugin_internal.hpp
@@ -214,9 +214,10 @@ public:
 
     /**
      * @brief      Provides a default remote context instance if supported by a plugin
+     * @param[in]  params  The map of parameters
      * @return     The default context.
      */
-    virtual RemoteContext::Ptr GetDefaultContext() = 0;
+    virtual RemoteContext::Ptr GetDefaultContext(const ParamMap& params) = 0;
 
     /**
      * @deprecated Use ImportNetwork(std::istream& networkModel, const std::map<std::string, std::string>& config)

commit a017cbaf7284ebd178bae47c97e896992534e6f8

[BugFix] Fix compilation error caused by typo in #3109

inference-engine/include/gpu/gpu_ocl_wrapper.hpp
@@ -19,7 +19,7 @@
 #endif
 
 #ifdef CL_HPP_MINIMUM_OPENCL_VERSION
-# if CL_HPP_MINIMUM_OPENCL_VERSION <= 120
+# if CL_HPP_MINIMUM_OPENCL_VERSION < 120
 #  error "CL_HPP_MINIMUM_OPENCL_VERSION must be >= 120"
 # endif
 #else
@@ -27,7 +27,7 @@
 #endif
 
 #ifdef CL_HPP_TARGET_OPENCL_VERSION
-# if CL_HPP_TARGET_OPENCL_VERSION <= 120
+# if CL_HPP_TARGET_OPENCL_VERSION < 120
 #  error "CL_HPP_TARGET_OPENCL_VERSION must be >= 120"
 # endif
 #else