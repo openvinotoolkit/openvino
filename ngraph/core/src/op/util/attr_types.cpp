013faa1f19500ffdee25425d67338f2d9d9a6bb4
jdanieck <jozef.daniecki@intel.com>
2020-11-13 15:12:31 +0100

    Revert "Align MaxPool op attribute 'rounding_type' to spec."
    This reverts commit 243eeccff382de9b96d0b4a1caaedbdb28639994.

--- ngraph/core/src/op/util/attr_types.cpp
+++ ngraph/core/src/op/util/attr_types.cpp
@@ -66,7 +66,7 @@ namespace ngraph
     {
         static auto enum_names = EnumNames<op::RoundingType>(
             "op::RoundingType",
-            {{"floor", op::RoundingType::FLOOR}, {"ceil", op::RoundingType::CEIL}});
+            {{"FLOOR", op::RoundingType::FLOOR}, {"CEIL", op::RoundingType::CEIL}});
         return enum_names;
     }
 
