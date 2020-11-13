52d6ea1b3112e62e687038c91d2be3e0c43983ca
bszmelcz <bartosz.szmelczynski@intel.com>
2020-11-13 13:47:39 +0100

    remove unnecessary layers from the test

--- inference-engine/tests/functional/inference_engine/ngraph_reader/reorg_yolo_tests.cpp
+++ inference-engine/tests/functional/inference_engine/ngraph_reader/reorg_yolo_tests.cpp
@@ -39,8 +39,7 @@ TEST_F(NGraphReaderTests, ReadReorgYoloNetwork) {
 				</port>
 			</output>
 		</layer>
-		<layer id="2" name="yolov2/yolov2_feature/lower_features/downsample/YoloRegion" type="RegionYolo" version="opset1">
-			<data coords="4" classes="20" mask="0" num="5" axis="1" end_axis="3" do_softmax="1" anchors="1.3221,1.73145,3.19275,4.00944,5.05587,8.09892,9.47112,4.84053,11.2364,10.0071"/>
+		<layer id="2" name="365" type="Result" version="opset1">
 			<input>
 				<port id="0">
 					<dim>1</dim>
@@ -49,26 +48,11 @@ TEST_F(NGraphReaderTests, ReadReorgYoloNetwork) {
 					<dim>13</dim>
 				</port>
 			</input>
-			<output>
-				<port id="1" precision="FP32">
-					<dim>1</dim>
-					<dim>43264</dim>
-				</port>
-			</output>
-		</layer>
-		<layer id="3" name="365" type="Result" version="opset1">
-			<input>
-				<port id="0">
-					<dim>1</dim>
-					<dim>43264</dim>
-				</port>
-			</input>
 		</layer>
 	</layers>
 	<edges>
 		<edge from-layer="0" from-port="0" to-layer="1" to-port="0"/>
 		<edge from-layer="1" from-port="1" to-layer="2" to-port="0"/>
-		<edge from-layer="2" from-port="1" to-layer="3" to-port="0"/>
 	</edges>
 </net>
 )V0G0N";
@@ -104,27 +88,9 @@ TEST_F(NGraphReaderTests, ReadReorgYoloNetwork) {
 				</port>
 			</output>
 		</layer>
-		<layer id="2" name="yolov2/yolov2_feature/lower_features/downsample/YoloRegion" type="RegionYolo" version="opset1">
-			<data coords="4" mask="0" classes="20" num="5" axis="1" end_axis="3" do_softmax="1" anchors="1.3221,1.73145,3.19275,4.00944,5.05587,8.09892,9.47112,4.84053,11.2364,10.0071"/>
-			<input>
-				<port id="0">
-					<dim>1</dim>
-					<dim>104</dim>
-					<dim>32</dim>
-					<dim>13</dim>
-				</port>
-			</input>
-			<output>
-				<port id="1" precision="FP32">
-					<dim>1</dim>
-					<dim>43264</dim>
-				</port>
-			</output>
-		</layer>
 	</layers>
 	<edges>
 		<edge from-layer="0" from-port="0" to-layer="1" to-port="0"/>
-		<edge from-layer="1" from-port="1" to-layer="2" to-port="0"/>
 	</edges>
 </net>
 )V0G0N";
