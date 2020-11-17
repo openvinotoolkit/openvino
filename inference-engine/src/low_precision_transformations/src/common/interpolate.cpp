cba8d918d9634b8a14709dc150ae4c9deae9be00
Chenhu Wang <chenhu.wang@intel.com>
2020-11-17 15:42:34 +0800

    [CPU] Interpolate operation improvements (#2366)
    * interpolate improvement
    
    * JITTED cubic mode
    
    * fix 'code is too big' when JIT
    
    * extend test to cover tail code path
    
    * transformation of interpolate1 to interpolate4
    
    * add low precision transformation for interpolate4

--- inference-engine/src/low_precision_transformations/src/common/interpolate.cpp
+++ inference-engine/src/low_precision_transformations/src/common/interpolate.cpp
@@ -20,6 +20,16 @@ void InterpolateTransformation::registerMatcherIn(GraphRewrite& pass, Transforma
         pass,
         context,
         make_op_pattern<opset1::Interpolate>({ make_op_label<opset1::Multiply>(), make_op_label<opset1::Constant>() }));
+    addPattern(
+        pass,
+        context,
+        make_op_pattern<opset4::Interpolate>({ make_op_label<opset1::Multiply>(), make_op_label<opset1::Constant>(),
+            make_op_label<opset1::Constant>(), make_op_label<opset1::Constant>() }));
+    addPattern(
+        pass,
+        context,
+        make_op_pattern<opset4::Interpolate>({ make_op_label<opset1::Multiply>(), make_op_label<opset1::Constant>(),
+            make_op_label<opset1::Constant>() }));
 }
 
 bool InterpolateTransformation::transform(TransformationContext &context, ngraph::pattern::Matcher &m) const {
@@ -33,9 +43,19 @@ bool InterpolateTransformation::transform(TransformationContext &context, ngraph
 }
 
 bool InterpolateTransformation::isPrecisionPreserved(std::shared_ptr<Node> layer) const noexcept {
-    std::shared_ptr<opset1::Interpolate> interpolate = as_type_ptr<opset1::Interpolate>(layer);
-    const auto attrs = interpolate->get_attrs();
-    return attrs.mode == "nearest";
+    std::shared_ptr<opset1::Interpolate> interpolate1 = as_type_ptr<opset1::Interpolate>(layer);
+    if (interpolate1) {
+        const auto attrs = interpolate1->get_attrs();
+        return attrs.mode == "nearest";
+    }
+
+    std::shared_ptr<opset4::Interpolate> interpolate4 = as_type_ptr<opset4::Interpolate>(layer);
+    if (interpolate4) {
+        const auto attrs = interpolate4->get_attrs();
+        return attrs.mode == op::v4::Interpolate::InterpolateMode::nearest;
+    }
+
+    return false;
 }
 
 bool InterpolateTransformation::canBeTransformed(const TransformationContext& context, std::shared_ptr<Node> layer) const {
@@ -49,19 +69,46 @@ bool InterpolateTransformation::canBeTransformed(const TransformationContext& co
     if (dequantization.empty()) {
         return false;
     }
-    const auto interpolate = as_type_ptr<opset1::Interpolate>(layer);
-    const auto interpAttrs = interpolate->get_attrs();
 
-    if (interpAttrs.axes.count(0) || interpAttrs.axes.count(1)) {
-        return false;
+    const auto interpolate1 = as_type_ptr<opset1::Interpolate>(layer);
+    if (interpolate1) {
+        const auto interpAttrs = interpolate1->get_attrs();
+        if (interpAttrs.axes.count(0) || interpAttrs.axes.count(1)) {
+            return false;
+        }
+        if (interpAttrs.mode != "nearest") {
+            return false;
+        }
+        if (interpAttrs.pads_begin[0] != 0 || interpAttrs.pads_end[0] != 0 || interpAttrs.align_corners) {
+            return false;
+        }
     }
 
-    if (interpAttrs.mode != "nearest") {
-        return false;
-    }
+    const auto interpolate4 = as_type_ptr<opset4::Interpolate>(layer);
+    if (interpolate4) {
+        const auto interpAttrs = interpolate4->get_attrs();
 
-    if (interpAttrs.pads_begin[0] != 0 || interpAttrs.pads_end[0] != 0 || interpAttrs.align_corners) {
-        return false;
+        if (interpAttrs.mode != op::v4::Interpolate::InterpolateMode::nearest) {
+            return false;
+        }
+
+        auto pads_begin = interpAttrs.pads_begin;
+        for (int i = 0; i < pads_begin.size(); ++i) {
+            if (pads_begin[i] != 0) {
+                return false;
+            }
+        }
+
+        auto pads_end = interpAttrs.pads_end;
+        for (int i = 0; i < pads_end.size(); ++i) {
+            if (pads_end[i] != 0) {
+                return false;
+            }
+        }
+
+        if (interpAttrs.coordinate_transformation_mode == op::v4::Interpolate::CoordinateTransformMode::align_corners) {
+            return false;
+        }
     }
 
     return true;
