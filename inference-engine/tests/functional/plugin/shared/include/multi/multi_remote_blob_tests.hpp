76168e262c8bd88f609942e6ed0e5172ab1834fe
myshevts <maxim.y.shevtsov@intel.com>
2020-11-17 15:42:52 +0300

    further shuffled files, so that the initial MULTI tests are now part of the IE tests, yet specific instances do need separate targets

--- inference-engine/tests/functional/plugin/shared/include/multi/multi_remote_blob_tests.hpp
+++ inference-engine/tests/functional/plugin/shared/include/multi/multi_remote_blob_tests.hpp
@@ -5,7 +5,6 @@
 #include <string>
 #include <vector>
 #include "multi/multi_helpers.hpp"
-#include "common_test_utils/test_constants.hpp"
 #include "functional_test_utils/plugin_cache.hpp"
 
 TEST_P(MultiDevice_SupportTest, canCreateContextThenRequestThenBlobsAndInfer) {
@@ -35,25 +34,3 @@ TEST_P(MultiDevice_SupportTest, canCreateContextThenRequestThenBlobsAndInfer) {
         ASSERT_THROW(exec_net.GetContext(), InferenceEngine::NotImplemented);
     }
 }
-
-const std::vector<DevicesNamesAndSupportPair> device_names_and_support_for_remote_blobs {
-#if ENABLE_MKL_DNN
-        {{CPU}, false}, // CPU via MULTI
-    #if ENABLE_CLDNN
-        {{GPU, CPU}, true}, // GPU+CPU
-        {{CPU, GPU}, true}, // CPU+GPU
-    #endif
-    #if ENABLE_MYRIAD
-        {{CPU, MYRIAD}, false},  // CPU+MYX
-    #endif
-#endif
-#if ENABLE_CLDNN
-        {{GPU}, true}, // GPU via MULTI,
-#endif
-#if ENABLE_MYRIAD
-        {{MYRIAD}, false}, // MYX via MULTI
-#endif
-};
-
-INSTANTIATE_TEST_CASE_P(smoke_RemoteBlobMulti, MultiDevice_SupportTest,
-        ::testing::ValuesIn(device_names_and_support_for_remote_blobs), MultiDevice_SupportTest::getTestCaseName);
