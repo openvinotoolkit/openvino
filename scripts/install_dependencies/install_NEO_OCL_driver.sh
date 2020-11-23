43df553cb51d5fbe2ead879128c9cbfb1f5d1da4
Artyom Anokhov <artyom.anokhov@intel.com>
2020-11-23 19:38:04 +0300

    install_NEO_OCL_driver: Updated syntax

--- scripts/install_dependencies/install_NEO_OCL_driver.sh
+++ scripts/install_dependencies/install_NEO_OCL_driver.sh
@@ -36,15 +36,15 @@ AVAILABLE_DRIVERS=("19.41.14441" "20.35.17767")
 
 print_help()
 {
-   # Display Help
-   usage="Usage: $(basename "$0") [OPTIONS]...
+    # Display Help
+    usage="Usage: $(basename "$0") [OPTIONS]...
 Download and installs the Intel® Graphics Compute Runtime for OpenCL™ Driver on Linux
 
     Available options:
     -y                      Replace the currently installed driver with the newer version.
     -d, --install_driver    Manually select driver version to one of available to install drivers.
                             Default value: $INSTALL_DRIVER_VERSION
-                            Available to install drivers: ${AVAILABLE_DRIVERS[@]}
+                            Available to install drivers: ${AVAILABLE_DRIVERS[*]}
     -h, --help              display this help and exit" 
     echo "$usage"
 }
@@ -59,7 +59,7 @@ do
             INSTALL_DRIVER_VERSION=$user_chosen_driver
         else
             echo "ERROR: unable to install the driver ${user_chosen_driver}."
-            echo "Available values: ${AVAILABLE_DRIVERS[@]}"
+            echo "Available values: ${AVAILABLE_DRIVERS[*]}"
             exit -1
         fi
         shift
@@ -484,8 +484,8 @@ check_agreement()
 check_specific_generation()
 {
     echo "Checking processor generation..."
-    specific_generation=$(cat /proc/cpuinfo | grep -m1 'model name' | grep -E "i[357]-1[01][0-9]{2,4}N?G[147R]E?")
-    if [[ ! -z $specific_generation && $INSTALL_DRIVER_VERSION != '20.35.17767' ]]; then
+    specific_generation=$(grep -m1 'model name' /proc/cpuinfo | grep -E "i[357]-1[01][0-9]{2,4}N?G[147R]E?")
+    if [[ ! -z "$specific_generation" && "$INSTALL_DRIVER_VERSION" != '20.35.17767' ]]; then
         echo "$(basename "$0"): Detected 10th generation Intel® Core™ processor (formerly Ice Lake) or 11th generation Intel® Core™ processor (formerly Tiger Lake)."
         echo "Driver version 20.35.17767 is going to be installed to fully utilize hardware features and performance."
         while true; do
