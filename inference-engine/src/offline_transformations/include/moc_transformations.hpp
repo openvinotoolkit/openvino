9890ddc371f3f4cfee0bf196b04bb980ec20ecfc
Gleb Kazantaev <gleb.kazantaev@intel.com>
2020-11-24 12:45:30 +0300

    Added offline transformations library

---
+++ inference-engine/src/offline_transformations/include/moc_transformations.hpp
@@ -0,0 +1,38 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <vector>
+#include <memory>
+
+#include <transformations_visibility.hpp>
+
+#include <ngraph/pass/graph_rewrite.hpp>
+#include <cpp/ie_cnn_network.h>
+#include <ngraph/pass/manager.hpp>
+#include <transformations/common_optimizations/common_optimizations.hpp>
+#include <ngraph/pass/constant_folding.hpp>
+
+//namespace ngraph {
+//namespace pass {
+
+class TRANSFORMATIONS_API MOCTransformations;
+
+//}  // namespace pass
+//}  // namespace ngraph
+
+class MOCTransformations: public ngraph::pass::FunctionPass {
+public:
+    NGRAPH_RTTI_DECLARATION;
+    bool run_on_function(std::shared_ptr<ngraph::Function> f) override {
+        std::cout << "Hello MOC!" << std::endl;
+        ngraph::pass::Manager manager(get_pass_config());
+        manager.register_pass<ngraph::pass::CommonOptimizations>();
+        auto pass_config = manager.get_pass_config();
+        pass_config->disable<ngraph::pass::ConstantFolding>();
+        manager.run_passes(f);
+        return true;
+    }
+};
