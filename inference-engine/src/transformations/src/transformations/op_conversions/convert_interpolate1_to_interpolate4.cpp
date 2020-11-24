4f6ff0ba8d2e7dd9bd2e819e88f4878ceb3406d4
VGavrilov <vladimir.gavrilov@intel.com>
2020-11-24 15:36:39 +0300

    Written draft of the transformation Interpolate-1 -> Interpolate-4.

--- inference-engine/src/transformations/src/transformations/op_conversions/convert_interpolate1_to_interpolate4.cpp
+++ inference-engine/src/transformations/src/transformations/op_conversions/convert_interpolate1_to_interpolate4.cpp
@@ -15,46 +15,56 @@
 NGRAPH_RTTI_DEFINITION(ngraph::pass::ConvertInterpolate1ToInterpolate4, "ConvertInterpolate1ToInterpolate4", 0);
 
 ngraph::pass::ConvertInterpolate1ToInterpolate4::ConvertInterpolate1ToInterpolate4() {
-    auto interpolate1 = ngraph::pattern::wrap_type<ngraph::opset1::Interpolate>({pattern::any_input(pattern::has_static_rank()), pattern::any_input()});
+    auto interpolate1 = ngraph::pattern::wrap_type<ngraph::opset1::Interpolate>(ngraph::pattern::any_input(), ngraph::pattern::any_input(),
+                                                                                ngraph::op::v0::InterpolateAttrs{}));
     ngraph::matcher_pass_callback callback = [this](pattern::Matcher& m) {
-        auto interpolate1 = std::dynamic_pointer_cast<ngraph::opset1::Interpolate>(m.get_match_root());
-        if (!interpolate1)
+        auto interpolationV0 = std::dynamic_pointer_cast<ngraph::op::v0::Interpolate>(m.get_match_root());
+        if (!interpolationV0) {
             return false;
+        }
 
-        auto interpolate_attrs = interpolate1->get_attrs();
-        auto input_shape_rank = interpolate1->input(0).get_partial_shape().rank().get_length();
+        auto& inp_shape = interpolationV0->get_input_shape(0);
+        auto& out_shape = interpolationV0->get_output_shape(0);
+        auto attrsV0 = interpolationV0->get_attrs();
 
-        // attrs
-        auto mode_v4 = ngraph::op::v4::Interpolate::InterpolateMode();
-        if (interpolate_attrs.mode == "nearest") {
-            mode_v4 = ngraph::op::v4::Interpolate::InterpolateMode::nearest;
-        } else if (interpolate_attrs.mode == "cubic") {
-            mode_v4 = ngraph::op::v4::Interpolate::InterpolateMode::cubic;
-        } else if (interpolate_attrs.mode == "linear") {
-            mode_v4 = ngraph::op::v4::Interpolate::InterpolateMode::linear;
+        std::vector<float> scales;
+        for (std::size_t i = 0; i < inp_shape.size(); ++i) {
+            scales.emplace_back(static_cast<float>(inp_shape.at(i))/out_shape.at(i));
+        }
+        auto scalesConstant = ngraph::op::Constant::create(ngraph::element::f32, {scales.size()}, scales);
+        auto axisConstant = ngraph::op::Constant::create(ngraph::element::i64, {attrsV0.axes.size()},
+                                                         std::vector<std::size_t>{attrsV0.axes.begin(), attrsV0.axes.end()});
+
+        ngraph::op::v4::Interpolate::InterpolateAttrs attrsV4;
+
+        if (attrsV0.mode == "nearest") {
+            attrsV4.mode = ngraph::op::v4::Interpolate::InterpolateMode::nearest;
+        } else if (attrsV0.mode == "linear") {
+            attrsV4.mode = ngraph::op::v4::Interpolate::InterpolateMode::linear;
+        } else if (attrsV0.mode == "cubic") {
+            attrsV4.mode = ngraph::op::v4::Interpolate::InterpolateMode::cubic;
+        } else if (attrsV0.mode == "linear_onnx") {
+            attrsV4.mode = ngraph::op::v4::Interpolate::InterpolateMode::linear_onnx;
         } else {
             return false;
         }
-        auto nearest_mode_v4 = ngraph::op::v4::Interpolate::NearestMode::round_prefer_floor;
-        auto shape_calculation_mode_v4 = ngraph::op::v4::Interpolate::ShapeCalcMode::sizes;
-        auto coordinate_transformation_mode_v4 = interpolate_attrs.align_corners ? ngraph::op::v4::Interpolate::CoordinateTransformMode::align_corners :
-                                                ngraph::op::v4::Interpolate::CoordinateTransformMode::half_pixel;
-        auto interpolate4_attr = ngraph::op::v4::Interpolate::InterpolateAttrs(mode_v4, shape_calculation_mode_v4,
-            interpolate_attrs.pads_begin, interpolate_attrs.pads_end,
-            coordinate_transformation_mode_v4, nearest_mode_v4, interpolate_attrs.antialias, -0.75);
+        attrsV4.shape_calculation_mode = ngraph::op::v4::Interpolate::ShapeCalcMode::sizes;
+        attrsV4.nearest_mode = ngraph::op::v4::Interpolate::NearestMode::round_prefer_floor;
+        attrsV4.pads_begin = attrsV0.pads_begin;
+        attrsV4.pads_end = attrsV0.pads_end;
+        attrsV4.antialias = attrsV0.antialias;
+        attrsV4.coordinate_transformation_mode = ngraph::op::v4::Interpolate::CoordinateTransformMode::half_pixel;
+        attrsV4.cube_coeff = -0.75f;
+        if (attrsV0.align_corners) {
+            attrsV4.coordinate_transformation_mode = ngraph::op::v4::Interpolate::CoordinateTransformMode::align_corners;
+        }
 
-        // input
-        auto axes = interpolate_attrs.axes.to_vector();
-        auto axes_node = ngraph::opset4::Constant::create(element::i64, {axes.size()}, axes);
-        auto default_scales = std::vector<float>(axes.size(), 1.f);
-        auto scales_node = ngraph::opset4::Constant::create(element::f32, {axes.size()}, default_scales);
+        auto interpolateV4 = std::make_shared<ngraph::op::v4::Interpolate>(interpolationV0->input_value(0), interpolationV0->input_value(1),
+                                                                           scalesConstant, axisConstant, attrsV4);
 
-        auto interpolate4 = std::make_shared<ngraph::opset4::Interpolate>(interpolate1->input_value(0), interpolate1->input_value(1),
-            scales_node, axes_node, interpolate4_attr);
-
-        interpolate4->set_friendly_name(interpolate1->get_friendly_name());
-        ngraph::copy_runtime_info(interpolate1, interpolate4);
-        ngraph::replace_node(interpolate1, interpolate4);
+        interpolateV4->set_friendly_name(interpolationV0->get_friendly_name() + ".V4");
+        ngraph::copy_runtime_info(interpolationV0, interpolateV4);
+        ngraph::replace_node(interpolationV0, interpolateV4);
         return true;
     };
 
