04c0e862fb3e0a4fba5c8b04478902ee30115805
Gleb Kazantaev <gleb.kazantaev@intel.com>
2020-11-24 12:45:30 +0300

    Added offline transformations library

---
+++ inference-engine/src/offline_transformations/src/moc_transformations.cpp
@@ -0,0 +1,10 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <memory>
+
+#include "moc_transformations.hpp"
+
+
+NGRAPH_RTTI_DEFINITION(MOCTransformations, "MOCTransformations", 0);
