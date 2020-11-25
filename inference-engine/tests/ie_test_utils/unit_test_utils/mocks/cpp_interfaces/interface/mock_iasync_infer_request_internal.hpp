56ddc81dd9d2de6cf0024ec4a0fbc061e90e8631
Ilya Lavrenov <ilya.lavrenov@intel.com>
2020-11-25 20:19:40 +0300

    Fixes for documentation build

--- inference-engine/tests/ie_test_utils/unit_test_utils/mocks/cpp_interfaces/interface/mock_iasync_infer_request_internal.hpp
+++ inference-engine/tests/ie_test_utils/unit_test_utils/mocks/cpp_interfaces/interface/mock_iasync_infer_request_internal.hpp
@@ -11,7 +11,7 @@
 #include <vector>
 
 #include <cpp_interfaces/interface/ie_iinfer_async_request_internal.hpp>
-#include <cpp_interfaces/interface/ie_imemory_state_internal.hpp>
+#include <cpp_interfaces/interface/ie_ivariable_state_internal.hpp>
 
 class MockIAsyncInferRequestInternal : public InferenceEngine::IAsyncInferRequestInternal {
 public:
