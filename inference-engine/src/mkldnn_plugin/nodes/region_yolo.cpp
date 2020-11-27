2921cca11c6bae0f0964a2fb98b9666dc2c8c420
Alexey Varyzgin <alexey.varyzgin@intel.com>
2020-11-27 13:30:13 +0300

    [CPU BF16] Bfloat16 inference optimizations (#2633)
    * [CPU BF16] Greedy mode was added
    
    * [IE TESTS][BF16] Added support for operations with bf16 precision in the single layer tests.
    
    * Added cpu specific bfloat16 single layer tests for the jit_eltwise primitive.
    
    * [CPU TESTS] Activation and logical single layer tests fixes.
    
    * [IE TESTS] Fix activation single layer tests run.
    
    * [IE TESTS][CPU] CPUTestBase further refactoring.
    
    * [CPU BF16] Support for Bfloat16 type was added to the MVN layer. (#3)
    
    * [CPU BF16] MVN layer bfloat16 compatibility.
    
    * [CPU BF16] MVN bfloat16 minor fixes.
    
    * [CPU BF16] MVN node exception about BF16 support replaced with precision redefinition.
    
    * [CPU BF16] MVN layer bloat16 support fixed for quantization operations and blocking layout.
    
    * [CPU] Input and output precision checks were added to MVN layer.
    
    * [IE TESTS][CPU BF16] Most of the bloat16 tests have been fixed.
    
    * Bf16 crop layer (#4)
    
    * [IE TESTS][CPU] Cpu specific test for the Crop layer has been created.
    
    * [IE TESTS][CPU] Deprecated Crop single layer test removed.
    
    * [CPU BF16] Bfloat16 precision was added to the Crop layer.
    
    * [CPU BF16] Crop layer minor code improvements.
    
    * [IE TESTS][CPU] Crop layer test added 2D tensor tests.
    
    * [IE TESTS][CPU] Crop layer test, obsolete comment removed.
    
    * [IE TESTS][CPU] Fixed CropIE include path.
    
    * Crop test fix for older gcc compiler.
    
    * [CPU BF16] Reduce layer extended with bfloat16 support.
    
    * [IE TESTS][CPU] CPU specific single layer test for Reduce operation.
    
    * BF16 optimized layers
    
    * [CPU BF16] Bfloat16 custom type added to the MKLDNN plugin.
    
    * [CPU BF16] Mem alignment to 16 bytes added to bfloat16 class union.
    
    * [IE TESTS][CPU] Permute cpu specific single layer test and minor cpu tests fixes
    
    * MVN cpu single layer tests extended with nhwc ndhwc layouts.
    
    * Mod mode removed from Eltwise cpu single layer test.
    
    * Permute cpu specific single layer test.
    
    * Smoke keyword was added to the CPU single layer tests.
    
    * Normalize node was modified for BF16 support
    
    * [CPU BF16] The RegionYolo layer has been extended with the bfloat16 type support.
    
    * Resample node was extended with BF16
    
    * Select layer was enabled with BF16
    
    * psroi supports bf16 (#7)
    
    * reorders replaces converts (#9)
    
    * BF16 planar pooling was enabled
    
    * [CPU BF16] Cpu_convert added to the RegionYOLO node.
    
    * [IE TESTS][CPU] Crop single layer test has been rewritten using the StridedSlice operation.
    
    * [IE TESTS][CPU] Covert layer test extended with bf16 precision.
    
    * [CPU BF16] The bfloat16 class was renamed bfloat16_t and some refactoring has been done.
    
    * [CPU BF16] RegionYOLO and Softmax were aligned with the review.
    
    * [IE TESTS CPU] CPU single layer tests refactored according to the review suggestions.
    
    * [IE TESTS CPU] The Reduce CPU single layer test was extended with different mem orders.
    
    * [IE TESTS CPU] Minor fixes after the review.
    
    * [IE TESTS CPU] Common plugin configuration has been moved to PreparePluginConfiguration function.
    
    * Minor changes after review
    
    * StridedSlice, Select, ScaleShift notes were resolved
    
    * Fixes to the Reduce operation cpu test and minor fixes related to the review.
    
    * GPU eltwise tests fix.
    
    * psroi unrolled to the primary state; code clean (#12)
    
    * PSROIPooling layer with C++ optimizations
    
    * Minor fix for compatibility with CPUTestsBase for fuse_permute_reorder test.
    
    * Code clean & psroi rollbacked
    
    Co-authored-by: Maksim Kutakov <maksim.kutakov@intel.com>
    Co-authored-by: Maksim Kutakov <maxim.kutakov@gmail.com>
    Co-authored-by: Yury Gaydaychuk <yury.gaydaychuk@intel.com>

--- inference-engine/src/mkldnn_plugin/nodes/region_yolo.cpp
+++ inference-engine/src/mkldnn_plugin/nodes/region_yolo.cpp
@@ -5,13 +5,19 @@
 #include "base.hpp"
 #include "common/defs.h"
 #include "common/softmax.h"
+#include "common/cpu_convert.h"
 #include <vector>
 #include <algorithm>
 #include <memory>
 #include <ie_parallel.hpp>
+#include <mkldnn_extension_utils.h>
+#include "utils/bfloat16.hpp"
+#include "common/cpu_memcpy.h"
 #include "jit_generator.hpp"
 #include "jit_uni_eltwise.hpp"
 
+using namespace MKLDNNPlugin;
+using namespace mkldnn;
 using namespace mkldnn::impl::cpu;
 using namespace mkldnn::impl::utils;
 
@@ -22,9 +28,16 @@ namespace Cpu {
 #define GET_OFF(field) offsetof(jit_args_logistic, field)
 
 struct jit_args_logistic {
-    const float* src;
-    const float* dst;
+    const void* src;
+    void* dst;
     size_t work_amount;
+};
+
+struct jit_logistic_config_params {
+    InferenceEngine::Precision src_dt;
+    InferenceEngine::Precision dst_dt;
+    unsigned src_data_size;
+    unsigned dst_data_size;
 };
 
 struct jit_uni_logistic_kernel {
@@ -40,7 +53,7 @@ template <cpu_isa_t isa>
 struct jit_uni_logistic_kernel_f32 : public jit_uni_logistic_kernel, public jit_generator {
     DECLARE_CPU_JIT_AUX_FUNCTIONS(jit_uni_logistic_kernel_f32)
 
-    jit_uni_logistic_kernel_f32() : jit_uni_logistic_kernel(), jit_generator() {
+    jit_uni_logistic_kernel_f32(jit_logistic_config_params jcp) : jit_uni_logistic_kernel(), jit_generator() {
         exp_injector.reset(new jit_uni_eltwise_injector_f32<isa>(this, alg_kind::eltwise_exp, 0.f, 0.f));
 
         this->preamble();
@@ -59,12 +72,12 @@ struct jit_uni_logistic_kernel_f32 : public jit_uni_logistic_kernel, public jit_
             cmp(reg_work_amount, step);
             jl(tail_loop_label, T_NEAR);
 
-            uni_vmovups(vmm_src, ptr[reg_src]);
+            load_vector(vmm_src, ptr[reg_src], jcp.src_dt);
             compute_kernel();
-            uni_vmovups(ptr[reg_dst], vmm_src);
+            store_vector(ptr[reg_dst], vmm_src, jcp.dst_dt);
 
-            add(reg_src, step * sizeof(float));
-            add(reg_dst, step * sizeof(float));
+            add(reg_src, step * jcp.src_data_size);
+            add(reg_dst, step * jcp.dst_data_size);
             sub(reg_work_amount, step);
 
             jmp(main_loop_label, T_NEAR);
@@ -75,12 +88,12 @@ struct jit_uni_logistic_kernel_f32 : public jit_uni_logistic_kernel, public jit_
             cmp(reg_work_amount, step);
             jl(exit_label, T_NEAR);
 
-            movss(xmm_src, ptr[reg_src]);
+            load_scalar(xmm_src, ptr[reg_src], jcp.src_dt);
             compute_kernel();
-            movss(ptr[reg_dst], xmm_src);
+            store_scalar(ptr[reg_dst], xmm_src, jcp.dst_dt);
 
-            add(reg_src, step * sizeof(float));
-            add(reg_dst, step * sizeof(float));
+            add(reg_src, step * jcp.src_data_size);
+            add(reg_dst, step * jcp.dst_data_size);
             sub(reg_work_amount, step);
 
             jmp(tail_loop_label, T_NEAR);
@@ -164,6 +177,61 @@ private:
         int mask_sign = 0x80000000;  // 0 //  mask to extract sign
         int float_1   = 0x3f800000;  // 1 //  1.0f
     } vals_for_logistic_activate;
+
+    inline void load_vector(Vmm vmm_src, const Xbyak::Address &op, InferenceEngine::Precision src_dt) {
+        switch (src_dt) {
+            case InferenceEngine::Precision::FP32:
+                uni_vmovups(vmm_src, op);
+                break;
+            case InferenceEngine::Precision::BF16:
+                vpmovzxwd(vmm_src, op);
+                uni_vpslld(vmm_src, vmm_src, 16);
+                break;
+            default:
+                assert(!"unknown src_dt");
+        }
+    }
+    inline void store_vector(const Xbyak::Address &op, Vmm vmm_dst, InferenceEngine::Precision dst_dt) {
+        Xbyak::Ymm ymm_dst = Xbyak::Ymm(vmm_dst.getIdx());
+
+        switch (dst_dt) {
+            case InferenceEngine::Precision::FP32:
+                uni_vmovups(op, vmm_dst);
+                break;
+            case InferenceEngine::Precision::BF16:
+                vcvtneps2bf16(ymm_dst, vmm_dst);
+                uni_vmovups(op, ymm_dst);
+                break;
+            default:
+                assert(!"unknown dst_dt");
+        }
+    }
+    inline void load_scalar(Xbyak::Xmm xmm_src, const Xbyak::Address &op, InferenceEngine::Precision src_dt) {
+        switch (src_dt) {
+            case InferenceEngine::Precision::FP32:
+                movss(xmm_src, op);
+                break;
+            case InferenceEngine::Precision::BF16:
+                pinsrw(xmm_src, op, 0x0);
+                uni_vpslld(xmm_src, xmm_src, 16);
+                break;
+            default:
+                assert(!"unknown src_dt");
+        }
+    }
+    inline void store_scalar(const Xbyak::Address &op, Xbyak::Xmm xmm_dst, InferenceEngine::Precision dst_dt) {
+        switch (dst_dt) {
+            case InferenceEngine::Precision::FP32:
+                movss(op, xmm_dst);
+                break;
+            case InferenceEngine::Precision::BF16:
+                uni_vpsrld(xmm_dst, xmm_dst, 16);
+                pextrw(op, xmm_dst, 0x0);
+                break;
+           default:
+                assert(!"unknown dst_dt");
+        }
+    }
 };
 
 class RegionYoloImpl: public ExtLayerBase {
@@ -173,27 +241,48 @@ public:
             if (layer->insData.size() != 1 || layer->outData.empty())
                 THROW_IE_EXCEPTION << "Incorrect number of input/output edges!";
 
+            input_prec = layer->insData.front().lock()->getPrecision();
+            output_prec = layer->outData.front()->getPrecision();
+
+            if (input_prec != Precision::FP32 && input_prec != Precision::BF16) {
+                input_prec = Precision::FP32;
+            }
+
+            if (output_prec != Precision::FP32 && output_prec != Precision::BF16) {
+                output_prec = Precision::FP32;
+            }
+
+            if (Precision::BF16 == output_prec) {
+                if (!mayiuse(avx512_core_bf16)) {
+                    output_prec = Precision::FP32;
+                }
+            }
+
             classes = layer->GetParamAsInt("classes");
             coords = layer->GetParamAsInt("coords");
             num = layer->GetParamAsInt("num");
             do_softmax = layer->GetParamAsBool("do_softmax", true);
             mask = layer->GetParamAsInts("mask", {});
 
+            jit_logistic_config_params jcp;
+            jcp.src_dt = jcp.dst_dt = output_prec;
+            jcp.src_data_size = jcp.dst_data_size = output_prec.size();
+
             block_size = 1;
             if (mayiuse(avx512_common)) {
-                logistic_kernel.reset(new jit_uni_logistic_kernel_f32<avx512_common>());
+                logistic_kernel.reset(new jit_uni_logistic_kernel_f32<avx512_common>(jcp));
                 block_size = 16;
             } else if (mayiuse(avx2)) {
-                logistic_kernel.reset(new jit_uni_logistic_kernel_f32<avx2>());
+                logistic_kernel.reset(new jit_uni_logistic_kernel_f32<avx2>(jcp));
                 block_size = 8;
             } else if (mayiuse(sse42)) {
-                logistic_kernel.reset(new jit_uni_logistic_kernel_f32<sse42>());
+                logistic_kernel.reset(new jit_uni_logistic_kernel_f32<sse42>(jcp));
                 block_size = 4;
             }
 
-            softmax_kernel.reset(new SoftmaxGeneric());
+            softmax_kernel = std::make_shared<SoftmaxGeneric>(input_prec, output_prec);
 
-            addConfig(layer, {DataConfigurator(ConfLayout::PLN)}, {DataConfigurator(ConfLayout::PLN)});
+            addConfig(layer, {DataConfigurator(ConfLayout::PLN, input_prec)}, {DataConfigurator(ConfLayout::PLN, output_prec)});
         } catch (InferenceEngine::details::InferenceEngineException &ex) {
             errorMsg = ex.what();
         }
@@ -201,19 +290,12 @@ public:
 
     StatusCode execute(std::vector<Blob::Ptr>& inputs, std::vector<Blob::Ptr>& outputs,
                        ResponseDesc *resp) noexcept override {
-        const auto *src_data = inputs[0]->cbuffer().as<const float *>();
-        auto *dst_data = outputs[0]->buffer().as<float *>();
+        size_t mask_size = mask.size();
 
-        int mask_size = mask.size();
-
-        int IW = (inputs[0]->getTensorDesc().getDims().size() > 3) ? inputs[0]->getTensorDesc().getDims()[3] : 1;
-        int IH = (inputs[0]->getTensorDesc().getDims().size() > 2) ? inputs[0]->getTensorDesc().getDims()[2] : 1;
-        int IC = (inputs[0]->getTensorDesc().getDims().size() > 1) ? inputs[0]->getTensorDesc().getDims()[1] : 1;
-        int B = (inputs[0]->getTensorDesc().getDims().size() > 0) ? inputs[0]->getTensorDesc().getDims()[0] : 1;
-
-        parallel_for(B * IC * IH * IW, [&](int i) {
-            dst_data[i] = src_data[i];
-        });
+        size_t IW = (inputs[0]->getTensorDesc().getDims().size() > 3) ? inputs[0]->getTensorDesc().getDims()[3] : 1;
+        size_t IH = (inputs[0]->getTensorDesc().getDims().size() > 2) ? inputs[0]->getTensorDesc().getDims()[2] : 1;
+        size_t IC = (inputs[0]->getTensorDesc().getDims().size() > 1) ? inputs[0]->getTensorDesc().getDims()[1] : 1;
+        size_t B = (inputs[0]->getTensorDesc().getDims().size() > 0) ? inputs[0]->getTensorDesc().getDims()[0] : 1;
 
         int end_index = 0;
         int num_ = 0;
@@ -226,26 +308,41 @@ public:
             end_index = IW * IH * (classes + 1);
             num_ = mask_size;
         }
-        int inputs_size = IH * IW * num_ * (classes + coords + 1);
-        int total_size = 2 * IH * IW;
+        size_t inputs_size = IH * IW * num_ * (classes + coords + 1);
+        size_t total_size = 2 * IH * IW;
 
-        for (int b = 0; b < B; b++) {
-            for (int n = 0; n < num_; n++) {
-                int index = b * inputs_size + n * IW * IH * (classes + coords + 1);
-                calculate_logistic(index, total_size, dst_data);
+        const auto *src_data = inputs[0]->cbuffer().as<const uint8_t *>();
+        auto *dst_data = outputs[0]->buffer().as<uint8_t *>();
 
-                index = b * inputs_size + IW * IH * (n * (classes + coords + 1) + coords);
-                calculate_logistic(index, end_index, dst_data);
+        try {
+            cpu_convert(src_data, dst_data, inputs[0]->getTensorDesc().getPrecision(), outputs[0]->getTensorDesc().getPrecision(), B * IC * IH * IW);
+
+            for (int b = 0; b < B; b++) {
+                for (int n = 0; n < num_; n++) {
+                    size_t index = b * inputs_size + n * IW * IH * (classes + coords + 1);
+                    calculate_logistic(index, total_size, dst_data);
+
+                    index = b * inputs_size + IW * IH * (n * (classes + coords + 1) + coords);
+                    calculate_logistic(index, end_index, dst_data);
+                }
+            }
+
+            if (do_softmax) {
+                int index = IW * IH * (coords + 1);
+                int batch_offset = inputs_size / num;
+                for (int b = 0; b < B * num; b++) {
+                    softmax_kernel->execute(src_data + input_prec.size() * (index + b * batch_offset),
+                                            dst_data + output_prec.size() * (index + b * batch_offset), 1, classes, IH, IW);
+                }
             }
         }
-
-        if (do_softmax) {
-            int index = IW * IH * (coords + 1);
-            int batch_offset = inputs_size / num;
-            for (int b = 0; b < B * num; b++)
-                softmax_kernel->execute(src_data + index + b * batch_offset, dst_data + index + b * batch_offset, 1, classes, IH, IW);
+        catch (const std::exception& excp) {
+            snprintf(resp->msg, sizeof(resp->msg), "%s", excp.what());
+            return GENERAL_ERROR;
         }
-
+        catch(...) {
+            return GENERAL_ERROR;
+        }
         return OK;
     }
 
@@ -255,6 +352,7 @@ private:
     int num;
     float do_softmax;
     std::vector<int> mask;
+    Precision input_prec, output_prec;
 
     int block_size;
     std::shared_ptr<jit_uni_logistic_kernel> logistic_kernel;
@@ -281,7 +379,9 @@ private:
         return src;
     }
 
-    inline void calculate_logistic(int start_index, int count, float* dst_data) {
+
+    inline void calculate_logistic(size_t start_index, int count, uint8_t * dst_data) {
+        auto dst_data_size = output_prec.size();
         if (logistic_kernel) {
             int blocks_num = div_up(count, block_size);
             parallel_for(blocks_num, [&](int ib) {
@@ -289,15 +389,24 @@ private:
                 int work_amount = std::min(count - idx, block_size);
 
                 auto arg = jit_args_logistic();
-                arg.src = dst_data + start_index + idx;
-                arg.dst = dst_data + start_index + idx;
+                arg.src = arg.dst = dst_data + dst_data_size * (start_index + idx);
                 arg.work_amount = static_cast<size_t>(work_amount);
 
                 (*logistic_kernel)(&arg);
             });
         } else {
-            for (int i = 0; i < count; i++) {
-                dst_data[i + start_index] = logistic_scalar(dst_data[i + start_index]);
+            if (Precision::FP32 == output_prec) {
+                auto float_dst_data = reinterpret_cast<float*>(dst_data);
+                for (int i = 0; i < count; i++) {
+                    float_dst_data[i + start_index] = logistic_scalar(float_dst_data[i + start_index]);
+                }
+            } else if (Precision::BF16 == output_prec) {
+                auto bf16_dst_data = reinterpret_cast<bfloat16_t*>(dst_data);
+                for (int i = 0; i < count; i++) {
+                    bf16_dst_data[i + start_index] = logistic_scalar(bf16_dst_data[i + start_index]);
+                }
+            } else {
+                THROW_IE_EXCEPTION << "Unsupported precision configuration outPrc=" << output_prec.name();
             }
         }
     }
