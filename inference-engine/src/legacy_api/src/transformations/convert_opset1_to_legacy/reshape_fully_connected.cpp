e430a998a2bd5c859da01baa9423eed20ec0bd41
Lukasz Debski <lukasz.debski@intel.com>
2020-11-27 14:37:03 +0100

    Test test

--- inference-engine/src/legacy_api/src/transformations/convert_opset1_to_legacy/reshape_fully_connected.cpp
+++ inference-engine/src/legacy_api/src/transformations/convert_opset1_to_legacy/reshape_fully_connected.cpp
@@ -31,7 +31,8 @@ ngraph::pass::ReshapeFullyConnected::ReshapeFullyConnected() {
         auto input_shape = fc->input_value(0).get_shape();
         auto output_shape = fc->get_shape();
 
-        if (input_shape.size() == 2 || input_shape.size() == 3) {
+        // if (input_shape.size() == 2 || input_shape.size() == 3) {
+        if (input_shape.size() == 2) {
             return false;
         }
 
