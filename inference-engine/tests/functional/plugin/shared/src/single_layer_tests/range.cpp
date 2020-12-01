7798459bad85839c7bb5249db6f8f110c8c39e3a
ggalieroc <gabriele.galiero.casay@intel.com>
2020-12-01 16:45:38 +0100

    Range: Add single layer test class for Range-4

--- inference-engine/tests/functional/plugin/shared/src/single_layer_tests/range.cpp
+++ inference-engine/tests/functional/plugin/shared/src/single_layer_tests/range.cpp
@@ -74,4 +74,62 @@ TEST_P(RangeLayerTest, CompareWithRefs) {
     Run();
 }
 
+std::string RangeNumpyLayerTest::getTestCaseName(testing::TestParamInfo<RangeParams> obj) {
+    InferenceEngine::Precision netPrecision;
+    InferenceEngine::Precision inPrc, outPrc;
+    InferenceEngine::Layout inLayout, outLayout;
+    float start, stop, step;
+    std::string targetDevice;
+    std::tie(start, stop, step, netPrecision, inPrc, outPrc, inLayout, outLayout, targetDevice) = obj.param;
+
+    std::ostringstream result;
+    const char separator = '_';
+    result << "Start=" << start << separator;
+    result << "Stop=" << stop << separator;
+    result << "Step=" << step << separator;
+    result << "netPRC=" << netPrecision.name() << separator;
+    result << "inPRC=" << inPrc.name() << separator;
+    result << "outPRC=" << outPrc.name() << separator;
+    result << "inL=" << inLayout << separator;
+    result << "outL=" << outLayout << separator;
+    result << "trgDev=" << targetDevice;
+    return result.str();
+}
+
+void RangeNumpyLayerTest::Infer() {
+    inferRequest = executableNetwork.CreateInferRequest();
+    inputs.clear();
+
+    auto blobStart = inferRequest.GetBlob("start");
+    blobStart = FuncTestUtils::createAndFillBlobWithFloatArray(blobStart->getTensorDesc(), &start, 1);
+
+    auto blobStop = inferRequest.GetBlob("stop");
+    blobStop = FuncTestUtils::createAndFillBlobWithFloatArray(blobStop->getTensorDesc(), &stop, 1);
+
+    auto blobStep = inferRequest.GetBlob("step");
+    blobStep = FuncTestUtils::createAndFillBlobWithFloatArray(blobStep->getTensorDesc(), &step, 1);
+
+    inferRequest.Infer();
+}
+
+void RangeNumpyLayerTest::SetUp() {
+    InferenceEngine::Precision netPrecision;
+    std::tie(start, stop, step, netPrecision, inPrc, outPrc, inLayout, outLayout, targetDevice) = GetParam();
+    auto ngInPrc = FuncTestUtils::PrecisionUtils::convertIE2nGraphPrc(inPrc);
+    auto ngOutPrc = FuncTestUtils::PrecisionUtils::convertIE2nGraphPrc(outPrc);
+
+    auto params = ngraph::builder::makeParams(ngInPrc, {std::vector<size_t>(), std::vector<size_t>(), std::vector<size_t>()});
+    params[0]->set_friendly_name("start");
+    params[1]->set_friendly_name("stop");
+    params[2]->set_friendly_name("step");
+
+    auto range = std::make_shared<ngraph::opset4::Range>(params[0], params[1], params[2], ngOutPrc);
+    const ngraph::ResultVector results{std::make_shared<ngraph::opset1::Result>(range)};
+    function = std::make_shared<ngraph::Function>(results, params, "Range");
+}
+
+TEST_P(RangeNumpyLayerTest, CompareWithRefs) {
+    Run();
+}
+
 } // namespace LayerTestsDefinitions
