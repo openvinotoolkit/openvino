e12a58e2d76e5afa486adb7c1357fe2c3b222e94
bszmelcz <bartosz.szmelczynski@intel.com>
2020-12-02 13:24:30 +0100

    back to the previous .xml model

--- inference-engine/tests/functional/inference_engine/ngraph_reader/logical_not_tests.cpp
+++ inference-engine/tests/functional/inference_engine/ngraph_reader/logical_not_tests.cpp
@@ -6,78 +6,92 @@
 #include "ngraph_reader_tests.hpp"
 TEST_F(NGraphReaderTests, ReadLogicalNotNetwork) {
     std::string model = R"V0G0N(
-<net name="LogicalNot" version="10">
-	<layers>
-		<layer id="0" name="input_a" type="Parameter" version="opset1">
-			<data shape="256,56" element_type="boolean"/>
-			<output>
-				<port id="0" precision="BOOL">
-					<dim>256</dim>
-					<dim>56</dim>
-				</port>
-			</output>
-		</layer>
-		<layer id="1" name="LogicalNot" type="LogicalNot" version="opset1">
-			<input>
-				<port id="0">
-					<dim>256</dim>
-					<dim>56</dim>
-				</port>
-			</input>
-			<output>
-				<port id="1" precision="BOOL">
-					<dim>256</dim>
-					<dim>56</dim>
-				</port>
-			</output>
-		</layer>
-		<layer id="2" name="Identity/sink_port_0" type="Result" version="opset1">
-			<input>
-				<port id="0">
-					<dim>256</dim>
-					<dim>56</dim>
-				</port>
-			</input>
-		</layer>
-	</layers>
-	<edges>
-		<edge from-layer="0" from-port="0" to-layer="1" to-port="0"/>
-		<edge from-layer="1" from-port="1" to-layer="2" to-port="0"/>
-	</edges>
+<net name="Network" version="10">
+    <layers>
+        <layer name="in1" type="Parameter" id="0" version="opset1">
+            <data element_type="boolean" shape="1,3,22,22"/>
+            <output>
+                <port id="0" precision="BOOL">
+                    <dim>1</dim>
+                    <dim>3</dim>
+                    <dim>22</dim>
+                    <dim>22</dim>
+                </port>
+            </output>
+        </layer>
+        <layer name="LogicalNot" id="1" type="LogicalNot" version="opset1">
+            <input>
+                <port id="0" precision="BOOL">
+                    <dim>1</dim>
+                    <dim>3</dim>
+                    <dim>22</dim>
+                    <dim>22</dim>
+                </port>
+            </input>
+            <output>
+                <port id="2" precision="BOOL">
+                    <dim>1</dim>
+                    <dim>3</dim>
+                    <dim>22</dim>
+                    <dim>22</dim>
+                </port>
+            </output>
+        </layer>
+        <layer name="output" type="Result" id="2" version="opset1">
+            <input>
+                <port id="0" precision="BOOL">
+                    <dim>1</dim>
+                    <dim>3</dim>
+                    <dim>22</dim>
+                    <dim>22</dim>
+                </port>
+            </input>
+        </layer>
+    </layers>
+    <edges>
+        <edge from-layer="0" from-port="0" to-layer="1" to-port="0"/>
+        <edge from-layer="1" from-port="2" to-layer="2" to-port="0"/>
+    </edges>
 </net>
 )V0G0N";
-    std::string modelV7 = R"V0G0N(
-<net name="LogicalNot" version="7">
-	<layers>
-		<layer id="0" name="input_a" type="Input" version="opset1">
-			<output>
-				<port id="0" precision="BOOL">
-					<dim>256</dim>
-					<dim>56</dim>
-				</port>
-			</output>
-		</layer>
-		<layer id="1" name="LogicalNot" type="Activation" version="opset1">
-			<data type="not"/>
-			<input>
-				<port id="0">
-					<dim>256</dim>
-					<dim>56</dim>
-				</port>
-			</input>
-			<output>
-				<port id="1" precision="BOOL">
-					<dim>256</dim>
-					<dim>56</dim>
-				</port>
-			</output>
-		</layer>
-	</layers>
-	<edges>
-		<edge from-layer="0" from-port="0" to-layer="1" to-port="0"/>
-	</edges>
+    std::string modelV5 = R"V0G0N(
+<net name="Network" version="5" precision="BOOL" batch="1">
+    <layers>
+        <layer name="in1" type="Input" precision="BOOL" id="0">
+            <output>
+                <port id="0">
+                    <dim>1</dim>
+                    <dim>3</dim>
+                    <dim>22</dim>
+                    <dim>22</dim>
+                </port>
+            </output>
+        </layer>
+        <layer name="LogicalNot" id="1" type="Activation" precision="BOOL">
+            <data type="not"/>
+            <input>
+                <port id="0">
+                    <dim>1</dim>
+                    <dim>3</dim>
+                    <dim>22</dim>
+                    <dim>22</dim>
+                </port>
+            </input>
+            <output>
+                <port id="2">
+                    <dim>1</dim>
+                    <dim>3</dim>
+                    <dim>22</dim>
+                    <dim>22</dim>
+                </port>
+            </output>
+        </layer>
+    </layers>
+    <edges>
+        <edge from-layer="0" from-port="0" to-layer="1" to-port="0"/>
+    </edges>
 </net>
 )V0G0N";
 
-    compareIRs(model, modelV7);
+    compareIRs(model, modelV5, 0);
 }
