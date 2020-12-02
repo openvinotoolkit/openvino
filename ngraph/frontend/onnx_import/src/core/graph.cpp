9d556d84fabf4128495a432ce065c2ca988147ba
Bartosz Sledz <bartosz.sledz@intel.com>
2020-12-02 12:17:39 +0100

    Meaningful warning when nGraph Constant for initializer couldn't be created (#3443)

--- ngraph/frontend/onnx_import/src/core/graph.cpp
+++ ngraph/frontend/onnx_import/src/core/graph.cpp
@@ -95,9 +95,15 @@ namespace ngraph
                     }
                     catch (const ngraph::ngraph_error& exc)
                     {
-                        NGRAPH_WARN << "Could not create an nGraph Constant for initializer '"
-                                    << initializer_tensor.name() << "'. Detailed error:\n"
-                                    << exc.what();
+                        NGRAPH_WARN
+                            << "\nCould not create an nGraph Constant for initializer '"
+                            << initializer_tensor.name() << "'. \n"
+                            << "Constant with a 0 value was created, make sure connected input is "
+                               "optional.\n"
+                            << "Otherwise verify if the initializer contains a correct number of "
+                               "elements matching the initializer's shape. \n"
+                            << "Detailed error:\n"
+                            << exc.what();
                         ng_constant =
                             default_opset::Constant::create(tensor.get_ng_type(), Shape{}, {0});
                     }
