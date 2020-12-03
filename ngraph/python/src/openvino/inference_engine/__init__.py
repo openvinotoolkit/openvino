ec671b39bbdaa8101c342f38a8e2857a96e02c6a
Kuporosova <anastasia.kuporosova@intel.com>
2020-12-03 12:56:46 +0300

    return read_network as it should be

--- ngraph/python/src/openvino/inference_engine/__init__.py
+++ ngraph/python/src/openvino/inference_engine/__init__.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ******************************************************************************
+
+
 from openvino.pyopenvino import IECore
 from openvino.pyopenvino import IENetwork
 from openvino.pyopenvino import ExecutableNetwork
