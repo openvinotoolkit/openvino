02dfd90d8eb144c8788e8e45a27e8aa43c359ef5
Mateusz Tabaka <mateusz.tabaka@intel.com>
2020-12-07 13:57:43 +0100

    apply code format

--- ngraph/test/runtime/interpreter/evaluates_map.cpp
+++ ngraph/test/runtime/interpreter/evaluates_map.cpp
@@ -56,9 +56,9 @@
 #include "ngraph/runtime/reference/lrn.hpp"
 #include "ngraph/runtime/reference/mvn.hpp"
 #include "ngraph/runtime/reference/normalize_l2.hpp"
+#include "ngraph/runtime/reference/psroi_pooling.hpp"
 #include "ngraph/runtime/reference/region_yolo.hpp"
 #include "ngraph/runtime/reference/roi_pooling.hpp"
-#include "ngraph/runtime/reference/psroi_pooling.hpp"
 #include "ngraph/runtime/reference/scatter_nd_update.hpp"
 #include "ngraph/runtime/reference/squared_difference.hpp"
 #include "reference/elu.hpp"
@@ -1637,15 +1637,15 @@ namespace
     {
         using T = typename element_type_traits<ET>::value_type;
         runtime::reference::psroi_pooling<T>(inputs[0]->get_data_ptr<T>(),
-                inputs[0]->get_shape(),
-                inputs[1]->get_data_ptr<T>(),
-                inputs[1]->get_shape(),
-                outputs[0]->get_data_ptr<T>(),
-                outputs[0]->get_shape(),
-                op->get_mode(),
-                op->get_spatial_scale(),
-                op->get_spatial_bins_x(),
-                op->get_spatial_bins_y());
+                                             inputs[0]->get_shape(),
+                                             inputs[1]->get_data_ptr<T>(),
+                                             inputs[1]->get_shape(),
+                                             outputs[0]->get_data_ptr<T>(),
+                                             outputs[0]->get_shape(),
+                                             op->get_mode(),
+                                             op->get_spatial_scale(),
+                                             op->get_spatial_bins_x(),
+                                             op->get_spatial_bins_y());
 
         return true;
     }
