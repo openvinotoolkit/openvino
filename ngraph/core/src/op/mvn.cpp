aa8bc681301694ecbbffd900e82bbf3d7b3cda31
Vafin, Maxim <maxim.vafin@intel.com>
2020-12-08 18:29:37 +0300

    Fix build

--- ngraph/core/src/op/mvn.cpp
+++ ngraph/core/src/op/mvn.cpp
@@ -122,22 +122,25 @@ bool op::MVN::visit_attributes(AttributeVisitor& visitor)
 
 // ------------------------------ V6 ------------------------------
 
-template <>
-NGRAPH_API EnumNames<op::MVNEpsMode>& EnumNames<op::MVNEpsMode>::get()
+namespace ngraph
 {
-    static auto enum_names =
-        EnumNames<op::MVNEpsMode>("op::MVNEpsMode",
-                                  {{"OUTSIDE_SQRT", op::MVNEpsMode::OUTSIDE_SQRT},
-                                   {"INSIDE_SQRT", op::MVNEpsMode::INSIDE_SQRT}});
-    return enum_names;
-}
+    template <>
+    NGRAPH_API EnumNames<op::MVNEpsMode>& EnumNames<op::MVNEpsMode>::get()
+    {
+        static auto enum_names =
+            EnumNames<op::MVNEpsMode>("op::MVNEpsMode",
+                                      {{"OUTSIDE_SQRT", op::MVNEpsMode::OUTSIDE_SQRT},
+                                       {"INSIDE_SQRT", op::MVNEpsMode::INSIDE_SQRT}});
+        return enum_names;
+    }
 
-constexpr DiscreteTypeInfo AttributeAdapter<op::MVNEpsMode>::type_info;
+    constexpr DiscreteTypeInfo AttributeAdapter<op::MVNEpsMode>::type_info;
 
-std::ostream& op::operator<<(std::ostream& s, const op::MVNEpsMode& type)
-{
-    return s << as_string(type);
-}
+    std::ostream& op::operator<<(std::ostream& s, const op::MVNEpsMode& type)
+    {
+        return s << as_string(type);
+    }
+} // namespace ngraph
 
 NGRAPH_RTTI_DEFINITION(op::v6::MVN, "MVN", 6);
 
