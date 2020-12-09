c2b263ff3d5c38cfed745817771c806c341afd24
Tyukaev <nikolay.tyukaev@intel.com>
2020-12-10 02:50:20 +0300

    update build_main_layout.py

--- docs/doxygen/build_main_layout.py
+++ docs/doxygen/build_main_layout.py
@@ -6,10 +6,9 @@ from lxml import etree
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--openvino', type=str, required=True, default=None, help='openvino_docs.xml')
-    parser.add_argument('--out_file', type=str, required=True, default=None, help='main_layout.xml')
     args = parser.parse_args()
     result = build_layout(args.openvino)
-    with open(os.path.join(args.out_file), 'wb') as f:
+    with open(args.openvino, 'wb') as f:
         f.write(result)
 
 
