d433f91081c7a465cc64d41437c58e30b065e03b
Kazantaeva <anastasia.kazantaeva@intel.com>
2020-12-11 21:37:16 +0300

    Added subprocess

--- model-optimizer/mo/utils/record_event.py
+++ model-optimizer/mo/utils/record_event.py
@@ -3,4 +3,4 @@ import subprocess
 
 def record_event(event_name: str, app_name: str, app_version: str, event_segments: dict):
     subprocess.run(["python3", "record_lr_event.py", event_name, app_name, app_version,
-                    event_segments.keys()[0], event_segments.values()[0]])
+                    list(event_segments.keys())[0], list(event_segments.values())[0]])
