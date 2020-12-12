3c844783bb69df720f3d72bfd11eb397fb0fc3ea
iimironov <iliya.mironov@intel.com>
2020-12-12 19:29:01 +0300

    Refactoring some code

--- inference-engine/src/transformations/src/transformations/common_optimizations/broadcast_elementwise_fusion.cpp
+++ inference-engine/src/transformations/src/transformations/common_optimizations/broadcast_elementwise_fusion.cpp
@@ -4,15 +4,8 @@
 
 #include "transformations/common_optimizations/broadcast_elementwise_fusion.hpp"
 
-#include <memory>
-#include <vector>
-
 #include <ngraph/opsets/opset5.hpp>
-#include <ngraph/rt_info.hpp>
 #include <ngraph/pattern/op/wrap_type.hpp>
-#include "ngraph/op/util/binary_elementwise_arithmetic.hpp"
-
-#include <iostream>
 
 NGRAPH_RTTI_DEFINITION(ngraph::pass::BroadcastElementwiseFusion, "BroadcastElementwiseFusion", 0);
 
@@ -24,22 +17,18 @@ ngraph::pass::BroadcastElementwiseFusion::BroadcastElementwiseFusion() {
 
     ngraph::graph_rewrite_callback matcher_pass_callback = [](ngraph::pattern::Matcher& m) {
         auto elem = std::dynamic_pointer_cast<ngraph::op::util::BinaryElementwiseArithmetic>(m.get_match_root());
-
-        auto elem_input1 = elem->input_value(0);
         auto elem_input2 = elem->input_value(1);
-
-        auto input1_shape = elem->input(0).get_shape();
-        auto input2_shape = elem->input(1).get_shape();
-        if (input1_shape != input2_shape) {
-            return false;
-        }
 
         auto broadcast = elem_input2.get_node_shared_ptr();
         if (!broadcast) {
             return false;
         }
-        const auto & channel_dim = elem_input2.get_partial_shape()[1].get_length();
-        const auto & rank = elem_input2.get_partial_shape().rank().get_length();
+
+        auto input1_shape = elem->input(0).get_partial_shape();
+        auto broadcast_output_shape = broadcast->output(0).get_partial_shape();
+        if (input1_shape != broadcast_output_shape) {
+            return false;
+        }
 
         auto input = broadcast->input_value(0);
         copy_runtime_info(broadcast, input.get_node_shared_ptr());
