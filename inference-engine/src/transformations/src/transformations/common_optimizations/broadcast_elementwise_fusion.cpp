b6fcae82cc1b4ce077f3eb8ef2cef7d508d3e6be
iimironov <iliya.mironov@intel.com>
2020-12-15 15:56:05 +0300

    Refactoring transformation

--- inference-engine/src/transformations/src/transformations/common_optimizations/broadcast_elementwise_fusion.cpp
+++ inference-engine/src/transformations/src/transformations/common_optimizations/broadcast_elementwise_fusion.cpp
@@ -8,34 +8,34 @@
 #include <ngraph/pattern/op/wrap_type.hpp>
 
 NGRAPH_RTTI_DEFINITION(ngraph::pass::BroadcastElementwiseFusion, "BroadcastElementwiseFusion", 0);
+bool сompatible_shapeы(const ngraph::PartialShape & input_shape1, const ngraph::PartialShape & input_shape1) {
 
+}
 ngraph::pass::BroadcastElementwiseFusion::BroadcastElementwiseFusion() {
     auto input1 = ngraph::pattern::any_input();
     auto input2 = ngraph::pattern::any_input();
     auto broadcast = ngraph::pattern::wrap_type<ngraph::opset5::Broadcast>({input2, pattern::any_input()});
     auto elementwise = ngraph::pattern::wrap_type<ngraph::op::util::BinaryElementwiseArithmetic>({input1, broadcast});
 
-    ngraph::graph_rewrite_callback matcher_pass_callback = [](ngraph::pattern::Matcher& m) {
-        auto elem = std::dynamic_pointer_cast<ngraph::op::util::BinaryElementwiseArithmetic>(m.get_match_root());
-        auto broadcast = std::dynamic_pointer_cast<opset5::Broadcast>(elem->input_value(0).get_node_shared_ptr());
-        if (!broadcast) {
-            broadcast = std::dynamic_pointer_cast<opset5::Broadcast>(elem->input_value(1).get_node_shared_ptr());
-        }
-        if (!broadcast) {
+    ngraph::graph_rewrite_callback matcher_pass_callback = [=](ngraph::pattern::Matcher& m) {
+        auto &pattern_value = m.get_pattern_value_map();
+        auto broadcast_node = pattern_value[broadcast].get_node_shared_ptr();
+        auto elementwise_input_node = pattern_value[input1].get_node_shared_ptr();
+        if (!broadcast_node) {
             return false;
         }
 
-        auto input1_shape = elem->input(0).get_partial_shape();
-        auto broadcast_output_shape = broadcast->output(0).get_partial_shape();
-        if (input1_shape != broadcast_output_shape) {
+        auto elementwise_input_shape = elementwise_input_node->get_output_partial_shape(0);
+        auto broadcast_output_shape = broadcast_node->get_output_partial_shape(0);//output(0).get_partial_shape();
+        if (elementwise_input_shape != broadcast_output_shape) {
             return false;
         }
 
-        auto input = broadcast->input_value(0);
-        copy_runtime_info(broadcast, input.get_node_shared_ptr());
-        broadcast->output(0).replace(input);
+        auto input = broadcast_node->input_value(0);
+        copy_runtime_info(broadcast_node, input.get_node_shared_ptr());
+        broadcast_node->output(0).replace(input);
 
-        return true;
+        return false;
     };
 
     auto m = std::make_shared<ngraph::pattern::Matcher>(elementwise, "BroadcastElementwiseFusion");
