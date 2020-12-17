1c6b63296ea3d0ac01bef77cdf335fc3a4fc03d9
VGavrilov <vladimir.gavrilov@intel.com>
2020-12-17 17:22:06 +0300

    Small fixes.

--- ngraph/test/type_prop/interpolate.cpp
+++ ngraph/test/type_prop/interpolate.cpp
@@ -54,9 +54,9 @@ TEST(type_prop, interpolate_v4_non_constant_axes_scales)
     auto target_shape = std::make_shared<op::Parameter>(element::i64, Shape{15, 30});
     auto scales = op::Constant::create<float>(element::f32, Shape{2}, {0.5f, 0.5f});
 
-    auto start = std::make_shared<op::Constant>(et, Shape{}, std::vector<int32_t>{2});
-    auto stop = std::make_shared<op::Constant>(et, Shape{}, std::vector<int32_t>{4});
-    auto step = std::make_shared<op::Constant>(et, Shape{}, std::vector<int32_t>{1});
+    auto start = std::make_shared<op::Constant>(element::i32, Shape{}, std::vector<int32_t>{2});
+    auto stop = std::make_shared<op::Constant>(element::i32, Shape{}, std::vector<int32_t>{4});
+    auto step = std::make_shared<op::Constant>(element::i32, Shape{}, std::vector<int32_t>{1});
     auto axes = std::make_shared<op::v4::Range>(start, stop, step, element::i32);
 
     InterpolateAttrs attrs;
