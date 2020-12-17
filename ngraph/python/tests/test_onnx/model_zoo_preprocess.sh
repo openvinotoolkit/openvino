df67a15fd4cc2558e2933494a993fbfab4a6b167
Rafal Blaczkowski <rafal.blaczkowski@intel.com>
2020-12-17 14:12:47 +0100

    Enable automatic update of ONNX Model Zoo for ONNX CI (#3511)

--- ngraph/python/tests/test_onnx/model_zoo_preprocess.sh
+++ ngraph/python/tests/test_onnx/model_zoo_preprocess.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 set -e
 
-MODELS_DIR=false
-CLEAN_DIR=false
+# provide ONNX Model Zoo commit hash ID to update:
+ONNX_SHA=5c9f64f470c825ccbe1bbaa8d460c0463ff6efec
+
+MODELS_DIR="$HOME/.onnx/model_zoo"
 ENABLE_MSFT=false
-CLONE=false
+ENABLE_ONNX_MODELS_ZOO=false
+ENABLE_MSFT_MODELS=false
+FORCE_MODE=false
 
 function print_help {
     echo "Model preprocessing options:"
     echo "    -h display this help message"
-    echo "    -c clone ONNX models repository"
-    echo "    -m <DIR> set location of the models"
-    echo "    -f clean target directory(during clone or after enable MSFT models)"
-    echo "    -e download and prepare MSFT models"
+    echo "    -d <DIR> set location of the models (for onnx model ZOO and MSFT models)"
+    printf "    By default the models location is: %s\n" "$HOME/.onnx/model_zoo"
+    echo "    -o update Onnx Model Zoo models"
+    echo "    -m update MSFT models"
+    echo "    -f force update of a chosen model"
 }
 
-while getopts ":hcefm:" opt; do
+while getopts ":homfd:" opt; do
     case ${opt} in
         h )
             print_help
@@ -26,69 +31,132 @@ while getopts ":hcefm:" opt; do
         : )
             print_help
             ;;
-        c )
-            CLONE=true
-            ;;
-        m )
+        d )
             MODELS_DIR="$OPTARG"
             ;;
-        f )
-            CLEAN_DIR=true
+        o )
+            ENABLE_ONNX_MODELS_ZOO=true
             ;;
-        e )
-            ENABLE_MSFT=true
+        m )
+            ENABLE_MSFT_MODELS=true
+            ;;
+        f )
+            FORCE_MODE=true
             ;;
     esac
 done
 shift $((OPTIND -1))
 
-if [ "$MODELS_DIR" = false ] ; then
-    echo "Unknown location of the ZOO models"
+MODEL_ZOO_DIR="$MODELS_DIR/model_zoo"
+ONNX_MODELS_DIR="$MODEL_ZOO_DIR/onnx_model_zoo"
+MSFT_MODELS_DIR="$MODEL_ZOO_DIR/MSFT"
+
+function pull_and_postprocess_onnx_model_zoo() {
+    git fetch
+    git reset HEAD --hard
+
+    git checkout $ONNX_SHA
+
+    echo "Pulling models data via Git LFS for onnx model zoo repository"
+    git lfs pull --include="*" --exclude="*.onnx"
+    find "$ONNX_MODELS_DIR" -name "*.onnx" | while read filename; do rm "$filename"; done;
+
+    printf "Extracting tar.gz archives into %s\n" "$ONNX_MODELS_DIR"
+    find "$ONNX_MODELS_DIR" -name '*.tar.gz' -execdir sh -c 'BASEDIR=$(basename "{}" .tar.gz) && mkdir -p $BASEDIR' \; -execdir sh -c 'BASEDIR=$(basename "{}" .tar.gz) && tar --warning=no-unknown-keyword -xzf "{}" -C $BASEDIR' \;
+
+    echo "Postprocessing of ONNX Model Zoo models:"
+    echo "Fix yolo v4 model"
+    cd "$ONNX_MODELS_DIR/vision/object_detection_segmentation/yolov4/model/yolov4/yolov4/test_data_set"
+
+    mv input0.pb input_0.pb
+    mv input1.pb input_1.pb
+    mv input2.pb input_2.pb
+    mv output0.pb output_0.pb
+    mv output1.pb output_1.pb
+    mv output2.pb output_2.pb
+
+    echo "Fix roberta model"
+    cd "$ONNX_MODELS_DIR/text/machine_comprehension/roberta/model/roberta-sequence-classification-9/roberta-sequence-classification-9"
+    mkdir -p test_data_set_0
+    mv *.pb test_data_set_0/
+
+}
+
+function update_onnx_models() {
+    if [[ ! -d $ONNX_MODELS_DIR ]] ; then
+        echo "The ONNX Model Zoo repository doesn't exist on your filesystem then will be cloned"
+        git clone https://github.com/onnx/models.git "$ONNX_MODELS_DIR"
+        cd "$ONNX_MODELS_DIR"
+        pull_and_postprocess_onnx_model_zoo
+    else
+        # Check if ONNX Model Zoo directory consists of proper git repo
+        git_remote_url=`git -C $ONNX_MODELS_DIR config --local remote.origin.url 2> /dev/null 2>&1`
+        printf "ONNX Model Zoo repository exists: %s\n" "$ONNX_MODELS_DIR"
+        if [[ $git_remote_url = "https://github.com/onnx/models.git" ]]; then
+            printf "The proper github repository detected: %s\n" "$git_remote_url"
+        else
+            echo "The ONNX Model Zoo repository doesn't exist then will be cloned"
+            git clone https://github.com/onnx/models.git "$ONNX_MODELS_DIR"
+        fi
+    fi
+
+    cd "$ONNX_MODELS_DIR"
+    CURRENT_ONNX_MODELS_SHA=`git rev-parse HEAD`
+    if [[ $ONNX_SHA = $CURRENT_ONNX_MODELS_SHA ]] ; then
+        echo "ONNX Model Zoo repository is in the right state"
+    else
+        printf "Current onnx model zoo state is: %s \nChecking out to expected onnx model zoo state: %s \n\n" "$CURRENT_ONNX_MODELS_SHA" "$ONNX_SHA"
+        pull_and_postprocess_onnx_model_zoo
+    fi
+}
+
+function update_msft_models() {
+    wget https://onnxruntimetestdata.blob.core.windows.net/models/20191107.zip -O "$MSFT_MODELS_DIR.zip"
+    unzip "$MSFT_MODELS_DIR.zip" -d "$MSFT_MODELS_DIR" && rm "$MSFT_MODELS_DIR.zip"
+
+}
+
+function postprocess_msft_models() {
+    echo "Postprocessing of MSFT models:"
+
+    echo "Fix LSTM_Seq_lens_unpacked"
+    mv $MSFT_MODELS_DIR/opset9/LSTM_Seq_lens_unpacked/seq_lens_sorted $MSFT_MODELS_DIR/opset9/LSTM_Seq_lens_unpacked/test_data_set_0
+    mv $MSFT_MODELS_DIR/opset9/LSTM_Seq_lens_unpacked/seq_lens_unsorted $MSFT_MODELS_DIR/opset9/LSTM_Seq_lens_unpacked/test_data_set_1
+}
+
+if [[ $ENABLE_ONNX_MODELS_ZOO = false ]] && [[ $ENABLE_MSFT_MODELS = false ]] ; then
+    printf "Please choose an option to update chosen model:
+            -o to update ONNX Model ZOO
+            -m to update MSFT models"
     exit 170
 fi
 
-MODEL_ZOO_DIR="$MODELS_DIR/model_zoo"
-ONNX_MODELS_DIR="$MODELS_DIR/model_zoo/onnx_model_zoo"
-ONNX_MODELS_COMMIT_SHA="db621492211d75cce197c5fda500fff209e8ba6a"
-
-if [ "$CLONE" = true ] ; then
-    if [ "$CLEAN_DIR" = true ] ; then
-        rm -rf "$ONNX_MODELS_DIR"
-    fi
-    git clone https://github.com/onnx/models.git "$ONNX_MODELS_DIR"
+if [[ $MODELS_DIR = false ]] ; then
+    printf "Unknown location of the general models directory (onnx model ZOO and MSFT models)
+            Please specify the location using -d <DIR> flag"
+    exit 170
 fi
 
-mkdir -p "$ONNX_MODELS_DIR"
-cd "$ONNX_MODELS_DIR"
-# remove already downloaded models
-git clean -f -x -d
-git checkout .
-git fetch -p
-git checkout $ONNX_MODELS_COMMIT_SHA
-# pull models from the lfs repository
-# onnx models are included in the tar.gz archives
-git lfs pull --include="*" --exclude="*.onnx"
-find "$ONNX_MODELS_DIR" -name "*.onnx" | while read filename; do rm "$filename"; done;
-echo "extracting tar.gz archives..."
-find "$ONNX_MODELS_DIR" -name '*.tar.gz' -execdir sh -c 'BASEDIR=$(basename "{}" .tar.gz) && mkdir -p $BASEDIR' \; -execdir sh -c 'BASEDIR=$(basename "{}" .tar.gz) && tar -xzvf "{}" -C $BASEDIR' \;
-# fix yolo v4 model
-cd "$ONNX_MODELS_DIR/vision/object_detection_segmentation/yolov4/model/yolov4/yolov4/test_data_set"
-mv input0.pb input_0.pb
-mv input1.pb input_1.pb
-mv input2.pb input_2.pb
-mv output0.pb output_0.pb
-mv output1.pb output_1.pb
-mv output2.pb output_2.pb
-# fix roberta model
-cd "$ONNX_MODELS_DIR/text/machine_comprehension/roberta/model/roberta-sequence-classification-9/roberta-sequence-classification-9"
-mkdir test_data_set_0
-mv *.pb test_data_set_0/
 
-# Prepare MSFT models
-if [ "$ENABLE_MSFT" = true ] ; then
-    if [ "$CLEAN_DIR" = true ] ; then
-        rm -rf "$MODEL_ZOO_DIR/MSFT"
+# check if general model zoo directory exists (directory to store ONNX model zoo and MSFT models)
+if [[ ! -d $MODEL_ZOO_DIR ]] ; then
+    printf "The general model directory: %s doesn't exist on your filesystem then will be created \n" "$MODEL_ZOO_DIR"
+    mkdir -p $MODEL_ZOO_DIR
+else
+    printf "The general model directory: %s found\n" "$MODEL_ZOO_DIR"
+fi
+
+if [[ $ENABLE_ONNX_MODELS_ZOO = true ]] ; then
+    if [[ $FORCE_MODE = true ]]; then
+        rm -rf $ONNX_MODELS_DIR
     fi
-    wget https://onnxruntimetestdata.blob.core.windows.net/models/20191107.zip -O "$MODEL_ZOO_DIR/MSFT.zip"
-    unzip "$MODEL_ZOO_DIR/MSFT.zip" -d "$MODEL_ZOO_DIR" && rm "$MODEL_ZOO_DIR/MSFT.zip"
+    update_onnx_models
+fi
+
+if [[ $ENABLE_MSFT_MODELS = true ]] ; then
+    if [[ $FORCE_MODE = true ]]; then
+        rm -rf $MSFT_MODELS_DIR
+    fi
+    update_msft_models
+    postprocess_msft_models
 fi
