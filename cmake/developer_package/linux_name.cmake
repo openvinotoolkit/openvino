35ad4aa4ca199835291cee66a039ef2f0df54766
Ilya Lavrenov <ilya.lavrenov@intel.com>
2020-12-17 19:17:58 +0300

    Added fuzzing for CMAKE_MODULE_LINKER_FLAGS as well

--- cmake/developer_package/linux_name.cmake
+++ cmake/developer_package/linux_name.cmake
@@ -2,6 +2,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+include(target_flags)
+
 if (LINUX)
     function(get_linux_name res_var)
         if (NOT EXISTS "/etc/lsb-release")
@@ -11,7 +13,7 @@ if (LINUX)
             set(name_regex "NAME=\"([^ \"\n]*).*\"\n")
             set(version_regex "VERSION=\"([0-9]+(\\.[0-9]+)?)[^\n]*\"")
         else ()
-            #linux version detection using cat /etc/lsb-release
+            # linux version detection using cat /etc/lsb-release
             file(READ "/etc/lsb-release" release_data)
             set(name_regex "DISTRIB_ID=([^ \n]*)\n")
             set(version_regex "DISTRIB_RELEASE=([0-9]+(\\.[0-9]+)?)")
