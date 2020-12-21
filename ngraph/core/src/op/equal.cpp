f6739b63bda365005fa71fd12b2306e8c59b481e
Ilya Churaev <ilya.churaev@intel.com>
2020-12-21 14:32:40 +0300

    Enable Conditional Compilation for nGraph evaluate methods (#3666)
    * Added CC macro to nGraph
    
    * Add CC to evaluate methods
    
    * Fixed tests
    
    * Fixed comments
    
    * Add private evaluates
    
    * Fixed code style and names
    
    * Fixed code style
    
    * Fixed build

--- ngraph/core/src/op/equal.cpp
+++ ngraph/core/src/op/equal.cpp
@@ -50,20 +50,13 @@ namespace equal
         out->set_broadcast(broadcast_spec, arg0, arg1, element::boolean);
         switch (arg0->get_element_type())
         {
-            TYPE_CASE(boolean)(arg0, arg1, out, broadcast_spec);
-            break;
-            TYPE_CASE(i32)(arg0, arg1, out, broadcast_spec);
-            break;
-            TYPE_CASE(i64)(arg0, arg1, out, broadcast_spec);
-            break;
-            TYPE_CASE(u32)(arg0, arg1, out, broadcast_spec);
-            break;
-            TYPE_CASE(u64)(arg0, arg1, out, broadcast_spec);
-            break;
-            TYPE_CASE(f16)(arg0, arg1, out, broadcast_spec);
-            break;
-            TYPE_CASE(f32)(arg0, arg1, out, broadcast_spec);
-            break;
+            NGRAPH_TYPE_CASE(evaluate_equal, boolean, arg0, arg1, out, broadcast_spec);
+            NGRAPH_TYPE_CASE(evaluate_equal, i32, arg0, arg1, out, broadcast_spec);
+            NGRAPH_TYPE_CASE(evaluate_equal, i64, arg0, arg1, out, broadcast_spec);
+            NGRAPH_TYPE_CASE(evaluate_equal, u32, arg0, arg1, out, broadcast_spec);
+            NGRAPH_TYPE_CASE(evaluate_equal, u64, arg0, arg1, out, broadcast_spec);
+            NGRAPH_TYPE_CASE(evaluate_equal, f16, arg0, arg1, out, broadcast_spec);
+            NGRAPH_TYPE_CASE(evaluate_equal, f32, arg0, arg1, out, broadcast_spec);
         default: rc = false; break;
         }
         return rc;
@@ -90,6 +83,7 @@ shared_ptr<Node> op::v1::Equal::clone_with_new_inputs(const OutputVector& new_ar
 
 bool op::v1::Equal::evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const
 {
-    OV_ITT_SCOPED_TASK(itt::domains::nGraphOp, "op::v1::Equal::evaluate");
-    return equal::evaluate_equal(inputs[0], inputs[1], outputs[0], get_autob());
+    NGRAPH_OP_SCOPE(v1_Equal_evaluate,
+                    return equal::evaluate_equal(inputs[0], inputs[1], outputs[0], get_autob()));
+    return false;
 }
