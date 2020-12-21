c39bd86222709eed4ae2a68106fa42807944a902
tomdol <tomasz.dolbniak@intel.com>
2020-12-21 14:45:14 -0500

    Parsing prototxt models in separate file

--- ngraph/frontend/onnx_import/src/utils/parser.cpp
+++ ngraph/frontend/onnx_import/src/utils/parser.cpp
@@ -14,17 +14,55 @@
 // limitations under the License.
 //*****************************************************************************
 
+#include <fstream>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+#include <google/protobuf/text_format.h>
 #include <onnx/onnx_pb.h>
 
+#include "ngraph/except.hpp"
 #include "onnx_import/utils/parser.hpp"
 
 namespace ngraph
 {
+    namespace
+    {
+        void parse_from_stream(std::istream& model_stream, ONNX_NAMESPACE::ModelProto& model_proto)
+        {
+            if (!model_proto.ParseFromIstream(&model_stream))
+            {
+#ifdef NGRAPH_USE_PROTOBUF_LITE
+                throw ngraph_error(
+                    "Error during import of ONNX model provided as input stream "
+                    " with binary protobuf message.");
+#else
+                // Rewind to the beginning and clear stream state.
+                model_stream.clear();
+                model_stream.seekg(0);
+                google::protobuf::io::IstreamInputStream iistream(&model_stream);
+                // Try parsing input as a prototxt message
+                if (!google::protobuf::TextFormat::Parse(&iistream, &model_proto))
+                {
+                    throw ngraph_error(
+                        "Error during import of ONNX model provided as input stream with prototxt "
+                        "protobuf message.");
+                }
+#endif
+            }
+        }
+    } // namespace
+
     namespace onnx_import
     {
         void parse_from_file(const std::string& file_path, ONNX_NAMESPACE::ModelProto& model_proto)
         {
+            std::ifstream file_stream{file_path, std::ios::in | std::ios::binary};
+            if (!file_stream.is_open())
+            {
+                throw ngraph_error("Error during import of ONNX model expected to be in file: " +
+                                   file_path + ". Could not open the file.");
+            };
 
+            parse_from_stream(file_stream, model_proto);
         }
     } // namespace onnx_import
 } // namespace ngraph
