627f2256ded07e2e1d4468f9b3b3a22463144d00
Roman Kazantsev <roman.kazantsev@intel.com>
2020-12-22 12:26:54 +0300

    Remove old RandomUniform transformation and correct const values
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- model-optimizer/extensions/middle/DropoutWithRandomUniformReplacer.py
+++ model-optimizer/extensions/middle/DropoutWithRandomUniformReplacer.py
@@ -13,6 +13,7 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 """
+
 import numpy as np
 
 from typing import Dict
@@ -23,59 +24,15 @@ from mo.middle.replacement import MiddleReplacementPattern
 from mo.ops.broadcast import Broadcast
 
 
-class RandomUniformReplacer(MiddleReplacementPattern):
+class DropoutWithRandomUniformReplacer(MiddleReplacementPattern):
     """
-    TODO: fix this transformation because it is not valid for some cases
-    Replaces RandomUniform operation with Broadcast of ones in sub-graph:
-
-    ShapeOf ---> RandomUniform ---> Mul
-
-    """
-
-    enabled = True
-
-    @staticmethod
-    def pattern():
-        return dict(
-            nodes=[
-                ('shape', dict(op='ShapeOf')),
-                ('shape_data', dict()),
-                ('random_uniform', dict(op='RandomUniform')),
-                ('random_uniform_data', dict()),
-                ('mul', dict(op='Mul')),
-                ('mul_const', dict(op='Const')),
-                ('mul_const_data', dict())
-            ],
-            edges=[
-                ('shape', 'shape_data'),
-                ('shape_data', 'random_uniform'),
-                ('random_uniform', 'random_uniform_data'),
-                ('random_uniform_data', 'mul'),
-                ('mul_const', 'mul_const_data'),
-                ('mul_const_data', 'mul')
-            ]
-        )
-
-    @staticmethod
-    def replace_pattern(graph: Graph, match: Dict[str, Node]):
-        node = match['random_uniform']
-        node_name = node.soft_get('name', node.id)
-        data_type = match['mul_const'].out_port(0).get_data_type()
-        broadcast_node = create_op_with_const_inputs(graph, Broadcast, port_value_dict={0: np.array([1], dtype=data_type)},
-                                                     op_attrs={'name': node_name + '/Broadcast', 'mode': 'numpy'})
-        node.in_port(0).get_connection().set_destination(broadcast_node.in_port(1))
-        node.out_port(0).get_connection().set_source(broadcast_node.out_port(0))
-
-
-class DropoutReplacer(MiddleReplacementPattern):
-    """
-    Dropout (for inference mode) containing RandomUniform can be replaced with Broadcast of ones in sub-graph.
+    Dropout (for inference mode) with RandomUniform can be replaced with Broadcast of ones in a sub-graph.
 
     Dropout block:
     ShapeOf -> RandomUniform -> Mul ---> Add ---> Add -> Floor
                                 /\       /\       /\
                                 |        |        |
-                            Const(1)   Const(1)  Const(0)
+                            Const(1)   Const(0)  Const(1)
 
     Resulted block:
     ShapeOf -> Broadcast
@@ -84,9 +41,6 @@ class DropoutReplacer(MiddleReplacementPattern):
                Const(1)
     """
     enabled = True
-
-    def run_before(self):
-        return [RandomUniformReplacer]
 
     @staticmethod
     def pattern():
@@ -99,15 +53,15 @@ class DropoutReplacer(MiddleReplacementPattern):
                 ('mul', dict(op='Mul')),
                 ('mul_data', dict()),
                 ('mul_const', dict(op='Const')),
-                ('mul_const_data', dict(value=1)),
+                ('mul_const_data', dict(value=lambda v: v is not None and np.allclose(v, 1.0, atol=1e-6))),
                 ('add', dict(op='Add')),
                 ('add_data', dict()),
                 ('add_const', dict(op='Const')),
-                ('add_const_data', dict(value=0)),
+                ('add_const_data', dict(value=lambda v: v is not None and np.allclose(v, 0.0, atol=1e-6))),
                 ('add2', dict(op='Add')),
                 ('add2_data', dict()),
                 ('add2_const', dict(op='Const')),
-                ('add2_const_data', dict(value=1)),
+                ('add2_const_data', dict(value=lambda v: v is not None and np.allclose(v, 1.0, atol=1e-6))),
                 ('floor', dict(op='Floor')),
             ],
             edges=[
--- model-optimizer/extensions/middle/DropoutWithRandomUniformReplacer_test.py
+++ model-optimizer/extensions/middle/DropoutWithRandomUniformReplacer_test.py
@@ -13,11 +13,12 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 """
+
 import unittest
 
 import numpy as np
 
-from extensions.middle.RandomUniformReplacer import RandomUniformReplacer, DropoutReplacer
+from extensions.middle.DropoutWithRandomUniformReplacer import DropoutWithRandomUniformReplacer
 from mo.utils.ir_engine.compare_graphs import compare_graphs
 from mo.utils.unittest.graph import build_graph
 
@@ -29,8 +30,6 @@ nodes_attributes = {
     'mul': {'kind': 'op', 'op': 'Mul'},
     'mul_const': {'kind': 'op', 'op': 'Const'},
     'mul_const_data': {'kind': 'data', 'value': np.array([1], dtype=np.int32)},
-
-    # additional nodes for DropoutReplacerTest
     'mul_data': {'kind': 'data'},
     'add': {'kind': 'op', 'op': 'Add'},
     'add_data': {'kind': 'data'},
@@ -43,78 +42,16 @@ nodes_attributes = {
     'floor': {'kind': 'op', 'op': 'Floor', 'name': 'dropout/Floor'},
     'floor_data': {'kind': 'data'},
     'output': {'kind': 'op', 'op': 'Result'},
-    'broadcast_data': {'kind': 'data'},
 
-    # new nodes to be added
+    # nodes used in the transformation
     'broadcast': {'kind': 'op', 'op': 'Broadcast'},
     'broadcast_const': {'kind': 'op', 'op': 'Const'},
     'broadcast_const_data': {'kind': 'data', 'value': np.array([1], dtype=np.int32)},
+    'broadcast_data': {'kind': 'data'},
 }
 
 
-class RandomUniformReplacerTest(unittest.TestCase):
-    def test_1(self):
-        graph = build_graph(nodes_attributes,
-                            edges=[
-                                ('shape', 'shape_data'),
-                                ('shape_data', 'random_uniform'),
-                                ('random_uniform', 'random_uniform_data'),
-                                ('random_uniform_data', 'mul', {'in': 0}),
-                                ('mul_const', 'mul_const_data'),
-                                ('mul_const_data', 'mul', {'in': 1})
-                            ],
-                            nodes_with_edges_only=True)
-
-        ref_graph = build_graph(nodes_attributes,
-                                edges=[
-                                    ('shape', 'shape_data'),
-                                    ('shape_data', 'broadcast', {'in': 1}),
-                                    ('broadcast_const', 'broadcast_const_data'),
-                                    ('broadcast_const_data', 'broadcast', {'in': 0}),
-                                    ('broadcast', 'random_uniform_data'),
-                                    ('random_uniform_data', 'mul', {'in': 0}),
-                                    ('mul_const', 'mul_const_data'),
-                                    ('mul_const_data', 'mul', {'in': 1})
-                                ],
-                                nodes_with_edges_only=True)
-
-        RandomUniformReplacer().find_and_replace_pattern(graph)
-
-        flag, resp = compare_graphs(graph, ref_graph, 'mul', check_op_attrs=True)
-        self.assertTrue(flag, resp)
-
-    def test_2(self):
-        graph = build_graph(nodes_attributes,
-                            edges=[
-                                ('shape', 'shape_data'),
-                                ('shape_data', 'random_uniform'),
-                                ('random_uniform', 'random_uniform_data'),
-                                ('random_uniform_data', 'mul', {'in': 1}),
-                                ('mul_const', 'mul_const_data'),
-                                ('mul_const_data', 'mul', {'in': 0})
-                            ],
-                            nodes_with_edges_only=True)
-
-        ref_graph = build_graph(nodes_attributes,
-                                edges=[
-                                    ('shape', 'shape_data'),
-                                    ('shape_data', 'broadcast', {'in': 1}),
-                                    ('broadcast_const', 'broadcast_const_data'),
-                                    ('broadcast_const_data', 'broadcast', {'in': 0}),
-                                    ('broadcast', 'random_uniform_data'),
-                                    ('random_uniform_data', 'mul', {'in': 1}),
-                                    ('mul_const', 'mul_const_data'),
-                                    ('mul_const_data', 'mul', {'in': 0})
-                                ],
-                                nodes_with_edges_only=True)
-
-        RandomUniformReplacer().find_and_replace_pattern(graph)
-
-        flag, resp = compare_graphs(graph, ref_graph, 'mul', check_op_attrs=True)
-        self.assertTrue(flag, resp)
-
-
-class DropoutReplacerTest(unittest.TestCase):
+class DropoutWithRandomUniformReplacerReplacer(unittest.TestCase):
     def test_1(self):
         graph = build_graph(nodes_attributes,
                             edges=[
@@ -150,7 +87,7 @@ class DropoutReplacerTest(unittest.TestCase):
                                 ],
                                 nodes_with_edges_only=True)
 
-        DropoutReplacer().find_and_replace_pattern(graph)
+        DropoutWithRandomUniformReplacer().find_and_replace_pattern(graph)
 
         flag, resp = compare_graphs(graph, ref_graph, 'output', check_op_attrs=True)
         self.assertTrue(flag, resp)
