655c616bfd92a1ef44f4066af431ed99fd148cac
Volosenkov <eugeny.volosenkov@intel.com>
2020-12-22 22:47:54 +0300

    Add scale extractor

--- model-optimizer/extensions/front/caffe/scale_ext.py
+++ model-optimizer/extensions/front/caffe/scale_ext.py
@@ -17,6 +17,7 @@
 import numpy as np
 
 from mo.front.caffe.extractors.utils import embed_input, weights_biases
+from mo.front.extractor import FrontExtractorOp
 from mo.front.common.partial_infer.elemental import copy_shape_infer
 from mo.utils.utils import NamedAttrsClass
 from mo.ops.scale_shift import ScaleShiftOp
