ed5e552a6fd6c34f992489a599542fc0b3aa5552
Ilya Lavrenov <ilya.lavrenov@intel.com>
2020-12-25 18:59:53 +0300

    Fixed fuzzing

--- cmake/developer_package/fuzzing.cmake
+++ cmake/developer_package/fuzzing.cmake
@@ -2,30 +2,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-function(enable_fuzzing)
+macro(enable_fuzzing)
     # Enable (libFuzzer)[https://llvm.org/docs/LibFuzzer.html] if supported.
-    if(CMAKE_CXX_COMPILER_ID MATCHES "^(Apple)?Clang$" AND NOT WIN32)
-        # Communicate libfuzzer is enabled
-        set(WITH_LIBFUZZER ON PARENT_SCOPE)
-        add_compile_definitions(WITH_LIBFUZZER)
+    set(FUZZING_COMPILER_FLAGS "-fsanitize=fuzzer-no-link -fprofile-instr-generate -fcoverage-mapping")
+    set(FUZZING_LINKER_FLAGS "-fsanitize-coverage=trace-pc-guard -fprofile-instr-generate")
 
-        # Enable libfuzzer and code coverage
-        set(FUZZING_COMPILER_FLAGS "-fsanitize=fuzzer-no-link -fprofile-instr-generate -fcoverage-mapping")
-        set(FUZZING_LINKER_FLAGS "-fsanitize-coverage=trace-pc-guard -fprofile-instr-generate")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${FUZZING_COMPILER_FLAGS}")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${FUZZING_COMPILER_FLAGS}")
+    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${FUZZING_LINKER_FLAGS}")
+    set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} ${FUZZING_LINKER_FLAGS}")
 
-        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${FUZZING_COMPILER_FLAGS}" PARENT_SCOPE)
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${FUZZING_COMPILER_FLAGS}" PARENT_SCOPE)
-        set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${FUZZING_LINKER_FLAGS}" PARENT_SCOPE)
-        set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} ${FUZZING_LINKER_FLAGS}" PARENT_SCOPE)
-        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${FUZZING_LINKER_FLAGS}")
-    endif()
-endfunction(enable_fuzzing)
-
+    unset(FUZZING_COMPILER_FLAGS)
+    unset(FUZZING_LINKER_FLAGS)
+endmacro()
 
 function(add_fuzzer FUZZER_EXE_NAME FUZZER_SOURCES)
     add_executable(${FUZZER_EXE_NAME} ${FUZZER_SOURCES})
-    if(WITH_LIBFUZZER)
-        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=fuzzer" PARENT_SCOPE)
-    endif()
     target_link_libraries(${FUZZER_EXE_NAME} PRIVATE fuzz-testhelper)
+    if(ENABLE_FUZZING)
+        set_target_properties(${FUZZER_EXE_NAME} PROPERTIES LINK_FLAGS "-fsanitize=fuzzer")
+    endif()
 endfunction(add_fuzzer)
