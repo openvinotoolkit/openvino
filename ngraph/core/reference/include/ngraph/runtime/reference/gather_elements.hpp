6e8d1f6c4ca6e2568434490ccb36a85602f0ff8a
Pavel Esir <pavel.esir@intel.com>
2020-12-31 11:52:19 +0300

    🚀 optimized general solution; added separate calculation for 2D

--- ngraph/core/reference/include/ngraph/runtime/reference/gather_elements.hpp
+++ ngraph/core/reference/include/ngraph/runtime/reference/gather_elements.hpp
@@ -34,6 +34,8 @@ namespace ngraph
                                  const Shape& out_shape,
                                  int64_t axis)
             {
+                int64_t data_count = ngraph::shape_size(data_shape);
+
                 if (axis < 0)
                 {
                     axis += data_shape.size();
@@ -59,66 +61,91 @@ namespace ngraph
                     return;
                 }
 
+                // 2D case is most frequent in order to run faster simpler separate solution
+                // implemented
+                size_t num_rows = indices_shape[0];
+                size_t num_columns = indices_shape[1];
+                size_t data_num_columns = data_shape[1];
+                if (data_shape.size() == 2)
+                {
+                    size_t offset;
+                    if (axis == 0)
+                    {
+                        for (int64_t i = 0; i < num_rows; i++)
+                            for (int64_t j = 0; j < num_columns; j++)
+                            {
+                                offset = data_num_columns * indices[num_columns * i + j] + j;
+                                if (offset > data_count)
+                                {
+                                    throw std::domain_error{
+                                        "indices values of GatherElement exceed data size"};
+                                }
+                                out[num_columns * i + j] = data[offset];
+                            }
+                        return;
+                    }
+                    else // axis == 1
+                    {
+                        for (int64_t i = 0; i < num_rows; i++)
+                            for (int64_t j = 0; j < num_columns; j++)
+                            {
+                                offset = data_num_columns * i + indices[num_columns * i + j];
+                                if (offset > data_count)
+                                {
+                                    throw std::domain_error{
+                                        "indices values of GatherElement exceed data size"};
+                                }
+                                out[num_columns * i + j] = data[offset];
+                            }
+                        return;
+                    }
+                }
+
                 /*
-                 assume data and indices are 6D and axis = 2
-                 size of indices(N0,N1,N2,N3,N4,N5)
-                 size of data (N0,N1,N2',N3,N4,N5)
+                 assume data and indices are 5D and axis = 2
+                 size of indices(N0,N1,N2,N3,N4)
+                 size of data (N0,N1,N2',N3,N4)
 
                  the offset for indices will be
-                 N5*N4*N3*N2*N1*n0 + N5*N4*N3*N2*n1 + N5*N4*N3*n2 + N5*N4*n3 + N5*n4 + n5
+                 N4*N3*N2*N1*n0 + N4*N3*N2*n1 + N4*N3*n2 + N4*n3 + n4
                  and for data
-                 N5*N4*N3*N2'*N1*n0 + N5*N4*N3*N2'*n1 + N5*N4*N3*n2' + N5*N4*n3 + N5*n4 + n5
+                 N4*N3*N2'*N1*n0 + N4*N3*N2'*n1 + N4*N3*n2' + N4*n3 + n4
                  all values (except n2') are fixed or gradually increase
                  most of offset calculations are shared. We can rewrite offset for data as follows
 
-                 N5*N4*N3*N2'*(N1*n0 + n1) + N5*N4*N3*n2' + (N5*N4*n3 + N5*n4 + n5)
-                 N5*N4*N3*N2' - data_coeff
-                 (N1*n0 + n1) - outer_sum
-                 (N5*N4*n3 + N5*n4 + n5) - inner_sum
+                 offset = N4*N3*N2'(N1*n0 + n1) + N4*N3*n2' + (N4*n3 + n4)
+                 N4*N3*N2' - outer_sum_inc
+                 N4*N3*N2'(N1*n0 + n1) - outer_sum
+                 N4*N3*n2' - n2' is red from indices tensor n2' = indices[n0,n1,n2,n3,n4]
+                 (N4*n3 + n4) - inner_sum
                 */
 
-                size_t outer_sum = 0, inner_sum = 0;
+                size_t max_inner_sum = 1;
+                for (int i = axis + 1; i < indices_shape.size(); i++)
+                    max_inner_sum *= indices_shape[i];
 
-                // in 6D case with axis = 2
-                // N5*N4*N3
-                size_t max_inner_sum = ngraph::shape_size(
-                    std::vector<size_t>(indices_shape.begin() + axis + 1, indices_shape.end()));
-                // in 6D case with axis = 2
-                // N5*N4*N3*N2'
-                size_t data_coeff = ngraph::shape_size(
-                    std::vector<size_t>(data_shape.begin() + axis, data_shape.end()));
-                // in 6D case with axis = 2
-                // N5*N4*N3*N2
-                size_t outer_threshold_inc = ngraph::shape_size(
-                    std::vector<size_t>(indices_shape.begin() + axis, indices_shape.end()));
-                size_t outer_threshold = outer_threshold_inc;
+                size_t max_outer_sum = 1, outer_sum_inc = 1;
+                for (int i = 0; i < axis; i++)
+                    max_outer_sum *= indices_shape[i];
+                for (int i = axis; i < data_shape.size(); i++)
+                    outer_sum_inc *= data_shape[i];
+                max_outer_sum *= outer_sum_inc;
 
-                size_t count = ngraph::shape_size(indices_shape);
-                int64_t data_count = ngraph::shape_size(data_shape);
-                int64_t data_idx; // signed since indices is int32 or int64
-                for (size_t i = 0; i < count;)
-                {
-                    data_idx = data_coeff * outer_sum + max_inner_sum * indices[i] + inner_sum;
+                size_t offset, outer_sum, inner_sum, i, k;
 
-                    if (data_idx < 0 || data_idx > data_count)
-                    {
-                        throw std::domain_error{"indices values of GatherElement exceed data size"};
-                    }
-                    out[i] = data[data_idx];
-
-                    i++;
-                    if (i == outer_threshold)
-                    {
-                        outer_sum++;
-                        outer_threshold += outer_threshold_inc;
-                    }
-
-                    inner_sum++;
-                    if (inner_sum == max_inner_sum)
-                    {
-                        inner_sum = 0;
-                    }
-                }
+                for (outer_sum = 0, i = 0; outer_sum < max_outer_sum; outer_sum += outer_sum_inc)
+                    for (k = 0; k < indices_shape[axis]; k++)
+                        for (inner_sum = 0; inner_sum < max_inner_sum; inner_sum++)
+                        {
+                            offset = outer_sum + max_inner_sum * indices[i] + inner_sum;
+                            if (offset < 0 || offset > data_count)
+                            {
+                                throw std::domain_error{
+                                    "indices values of GatherElement exceed data size"};
+                            }
+                            out[i] = data[offset];
+                            i++;
+                        }
             }
         } // namespace reference
     }     // namespace runtime
