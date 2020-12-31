f461a25a8ddacf7d113e704fb12f15d0a2ac72b0
VGavrilov <vladimir.gavrilov@intel.com>
2020-12-31 11:19:20 +0300

    Written shape infer tests for nGraph operation ExperimentalDetectronROIFeatureExtractor (case when input shapes are partially dynamic).

--- ngraph/test/type_prop/experimental_detectron_roi_feature_extractor.cpp
+++ ngraph/test/type_prop/experimental_detectron_roi_feature_extractor.cpp
@@ -48,3 +48,47 @@ TEST(type_prop, detectron_roi_feature_extractor)
     ASSERT_EQ(roi->get_output_element_type(0), element::f32);
     EXPECT_EQ(roi->get_output_shape(0), (Shape{1000, 256, 14, 14}));
 }
+
+TEST(type_prop, detectron_roi_feature_extractor_dynamic)
+{
+    Attrs attrs;
+    attrs.aligned = false;
+    attrs.distribute_rois_between_levels = 1;
+    attrs.output_size = 14;
+    attrs.preserve_rois_order = 1;
+    attrs.sampling_ratio = 2;
+    attrs.pyramid_scales = {4, 8, 16, 32};
+    attrs.image_id = 0;
+
+    struct Shapes
+    {
+        PartialShape input_shape;
+        Dimension channels;
+    };
+
+    const auto dyn_dim = Dimension::dynamic();
+
+    std::vector<Shapes> shapes = {
+        {{1000, 4}, dyn_dim}, {{dyn_dim, 4}, 256}, {{dyn_dim, 4}, dyn_dim}};
+    for (const auto& s : shapes)
+    {
+        auto layer0_shape = PartialShape{1, s.channels, 200, 336};
+        auto layer1_shape = PartialShape{1, s.channels, 100, 168};
+        auto layer2_shape = PartialShape{1, s.channels, 50, 84};
+        auto layer3_shape = PartialShape{1, s.channels, 25, 42};
+
+        auto ref_out_shape = PartialShape{s.input_shape[0], s.channels, 14, 14};
+
+        auto input = std::make_shared<op::Parameter>(element::f32, s.input_shape);
+        auto pyramid_layer0 = std::make_shared<op::Parameter>(element::f32, layer0_shape);
+        auto pyramid_layer1 = std::make_shared<op::Parameter>(element::f32, layer1_shape);
+        auto pyramid_layer2 = std::make_shared<op::Parameter>(element::f32, layer2_shape);
+        auto pyramid_layer3 = std::make_shared<op::Parameter>(element::f32, layer3_shape);
+
+        auto roi = std::make_shared<ExperimentalROI>(
+            NodeVector{input, pyramid_layer0, pyramid_layer1, pyramid_layer2, pyramid_layer3}, attrs);
+
+        ASSERT_EQ(roi->get_output_element_type(0), element::f32);
+        ASSERT_TRUE(grid_gen->get_output_partial_shape(0).same_scheme(ref_out_shape));
+    }
+}
