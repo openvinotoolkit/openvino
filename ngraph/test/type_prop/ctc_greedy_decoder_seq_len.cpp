0ceae17ce6e0c24975cb71a042be6cc48ecd7ac5
iimironov <iliya.mironov@intel.com>
2021-01-11 18:03:50 +0300

    Fix code style

--- ngraph/test/type_prop/ctc_greedy_decoder_seq_len.cpp
+++ ngraph/test/type_prop/ctc_greedy_decoder_seq_len.cpp
@@ -62,9 +62,10 @@ TEST(type_prop, ctc_greedy_decoder_seq_len_static_shapes_with_dinemic_bi)
     auto P = make_shared<op::Parameter>(element::f32, logits_shape);
     auto I = make_shared<op::Parameter>(element::i32, seq_len_shape);
     auto BI = make_shared<op::Parameter>(element::i32, PartialShape{Dimension::dynamic()});
-    try {
+    try
+    {
         auto G = make_shared<op::v6::CTCGreedyDecoderSeqLen>(
-                P, I, BI, false, element::i64, element::i64);
+            P, I, BI, false, element::i64, element::i64);
         // Should have thrown, so fail if it didn't
         FAIL() << "Incorrect indices rank";
     }
