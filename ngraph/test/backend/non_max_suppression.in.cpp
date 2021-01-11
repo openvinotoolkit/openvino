4fe042b03601beba4030a97f7831764ff5bfbf77
Bartosz Sledz <bartosz.sledz@intel.com>
2021-01-11 23:28:06 +0100

    Add tests for platform shape mismatch

--- ngraph/test/backend/non_max_suppression.in.cpp
+++ ngraph/test/backend/non_max_suppression.in.cpp
@@ -612,3 +612,67 @@ NGRAPH_TEST(${BACKEND_NAME}, nonmaxsuppression_two_classes)
     EXPECT_EQ(expected_selected_scores, selected_scores_value);
     EXPECT_EQ(expected_valid_outputs, valid_outputs_value);
 }
+
+NGRAPH_TEST(${BACKEND_NAME}, nonmaxsuppression_with_score_threshold)
+{
+    std::vector<float> boxes_data = {0.0, 0.0,  1.0, 1.0,  0.0, 0.1,   1.0, 1.1,
+                                     0.0, -0.1, 1.0, 0.9,  0.0, 10.0,  1.0, 11.0,
+                                     0.0, 10.1, 1.0, 11.1, 0.0, 100.0, 1.0, 101.0};
+
+    std::vector<float> scores_data = {
+        0.9, 0.75, 0.6, 0.95, 0.5, 0.3};
+
+    const int64_t max_output_boxes_per_class_data = 2;
+    const float iou_threshold_data = 0.5f;
+    const float score_threshold_data = 0.4f;
+    const auto box_encoding = op::v5::NonMaxSuppression::BoxEncodingType::CORNER;
+    const auto boxes_shape = Shape{1, 6, 4};
+    const auto scores_shape = Shape{1, 1, 6};
+
+    const auto boxes = make_shared<op::Parameter>(element::f32, boxes_shape);
+    const auto scores = make_shared<op::Parameter>(element::f32, scores_shape);
+    auto max_output_boxes_per_class =
+        op::Constant::create<int64_t>(element::i64, Shape{}, {max_output_boxes_per_class_data});
+    auto iou_threshold = op::Constant::create<float>(element::f32, Shape{}, {iou_threshold_data});
+    auto score_threshold =
+        op::Constant::create<float>(element::f32, Shape{}, {score_threshold_data});
+    auto soft_nms_sigma = op::Constant::create<float>(element::f32, Shape{}, {0.0f});
+    auto nms = make_shared<op::v5::NonMaxSuppression>(boxes,
+                                                      scores,
+                                                      max_output_boxes_per_class,
+                                                      iou_threshold,
+                                                      score_threshold,
+                                                      soft_nms_sigma,
+                                                      box_encoding,
+                                                      false);
+
+    auto f = make_shared<Function>(nms, ParameterVector{boxes, scores});
+
+    auto backend = runtime::Backend::create("${BACKEND_NAME}");
+
+    auto selected_indeces = backend->create_tensor(element::i64, Shape{2, 3});
+    auto selected_scores = backend->create_tensor(element::f32, Shape{2, 3});
+    auto valid_outputs = backend->create_tensor(element::i64, Shape{1});
+
+    auto backend_boxes = backend->create_tensor(element::f32, boxes_shape);
+    auto backend_scores = backend->create_tensor(element::f32, scores_shape);
+    copy_data(backend_boxes, boxes_data);
+    copy_data(backend_scores, scores_data);
+
+    auto handle = backend->compile(f);
+
+    handle->call({selected_indeces, selected_scores, valid_outputs},
+                 {backend_boxes, backend_scores});
+
+    auto selected_indeces_value = read_vector<int64_t>(selected_indeces);
+    auto selected_scores_value = read_vector<float>(selected_scores);
+    auto valid_outputs_value = read_vector<int64_t>(valid_outputs);
+
+    std::vector<int64_t> expected_selected_indices = {0, 0, 3, 0, 0, 0};
+    std::vector<float> expected_selected_scores = {0.0, 0.0, 0.95, 0.0, 0.0, 0.9};
+    std::vector<int64_t> expected_valid_outputs = {2};
+
+    EXPECT_EQ(expected_selected_indices, selected_indeces_value);
+    EXPECT_EQ(expected_selected_scores, selected_scores_value);
+    EXPECT_EQ(expected_valid_outputs, valid_outputs_value);
+}
