9996994b8d517972fd13daef161865b137762ce6
Mateusz Tabaka <mateusz.tabaka@intel.com>
2021-01-12 14:12:57 +0100

    [ONNX] remove dangling Parameters from graph (#3774)
    We have models with inputs with no initializer and not connected to any other node.

--- ngraph/test/onnx/onnx_import.in.cpp
+++ ngraph/test/onnx/onnx_import.in.cpp
@@ -765,7 +765,6 @@ NGRAPH_TEST(${BACKEND_NAME}, onnx_model_softmax_0D)
         file_util::path_join(SERIALIZED_ZOO, "onnx/softmax_0D.prototxt"));
 
     auto test_case = test::TestCase<TestEngine>(function);
-    test_case.add_input<float>({3.141592});
     test_case.add_expected_output<float>({1.0});
     test_case.run();
 }
@@ -2597,7 +2596,6 @@ NGRAPH_TEST(${BACKEND_NAME}, onnx_model_eye_like)
         file_util::path_join(SERIALIZED_ZOO, "onnx/eye_like.prototxt"));
 
     auto test_case = test::TestCase<TestEngine>(function);
-    test_case.add_input<float>({0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f});
     test_case.add_expected_output<float>(
         Shape{3, 4}, {0.f, 0.f, 0.f, 0.f, 1.f, 0.f, 0.f, 0.f, 0.f, 1.f, 0.f, 0.f});
 
@@ -3285,5 +3283,17 @@ NGRAPH_TEST(${BACKEND_NAME}, onnx_model_hard_sigmoid)
 
     test_case.add_input<float>({inf, neg_inf, 0.0f, 1.0f});
     test_case.add_expected_output<float>(Shape{4}, {1.0f, 0.0f, 0.5f, 0.699999988079071f});
+    test_case.run();
+}
+
+NGRAPH_TEST(${BACKEND_NAME}, onnx_model_dangling_parameter)
+{
+    auto function = onnx_import::import_onnx_model(
+        file_util::path_join(SERIALIZED_ZOO, "onnx/dangling_parameter.prototxt"));
+
+    auto test_case = test::TestCase<TestEngine>(function);
+
+    test_case.add_input<float>({-1.0f, 2.0f, -3.0f});
+    test_case.add_expected_output<float>(Shape{3}, {1.0f, 2.0f, 3.0f});
     test_case.run();
 }
