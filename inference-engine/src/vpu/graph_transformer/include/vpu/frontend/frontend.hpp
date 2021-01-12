f894dc76e2b1fa7b27c816535473f1e1ed14905f
Andrey Sokolov <andrey.sokolov@intel.com>
2021-01-12 11:50:35 +0300

    [IE][VPU][TESTS]: add implementation of layer Round to Myriad Plugin (#3667)
    * [IE tests] add opset4, opset5 to the opset list
    * [IE VPU] add layer Round; support DTS

--- inference-engine/src/vpu/graph_transformer/include/vpu/frontend/frontend.hpp
+++ inference-engine/src/vpu/graph_transformer/include/vpu/frontend/frontend.hpp
@@ -160,6 +160,7 @@ public:
     void parseGatherND(const Model& model, const ie::CNNLayerPtr& layer, const DataVector& inputs, const DataVector& outputs) const;
     void parseHSwish(const Model& model, const ie::CNNLayerPtr& layer, const DataVector& inputs, const DataVector& outputs) const;
     void parseCeiling(const Model& model, const ie::CNNLayerPtr& layer, const DataVector& inputs, const DataVector& outputs) const;
+    void parseRound(const Model& model, const ie::CNNLayerPtr& layer, const DataVector& inputs, const DataVector& outputs) const;
 
     //
     // Special layers
