d04a97158d041a0a55974ade96e2ae9666de005e
Tomasz Dołbniak <tomasz.dolbniak@intel.com>
2021-01-13 15:02:01 +0100

    ONNX model editor class with input types modification support (#3714)

--- ngraph/frontend/onnx_import/include/onnx_import/onnx.hpp
+++ ngraph/frontend/onnx_import/include/onnx_import/onnx.hpp
@@ -23,6 +23,7 @@
 #include <string>
 
 #include "ngraph/function.hpp"
+#include "onnx_import/editor/editor.hpp"
 #include "onnx_import/utils/onnx_importer_visibility.hpp"
 
 /// \brief              Top level nGraph namespace.
@@ -85,6 +86,15 @@ namespace ngraph
         ONNX_IMPORTER_API
         std::shared_ptr<Function> import_onnx_model(const std::string& file_path);
 
+        /// \brief     Imports an ONNX model modified by the ONNXModelEditor wrapper.
+        ///
+        /// \note      Refer to the ONNXModelEditor documentation for available modifications.
+        ///
+        /// \param[in] model_editor  Reference to the model editor object instance.
+        ///
+        /// \return    An nGraph function representing the previously modified ONNX model.
+        ONNX_IMPORTER_API
+        std::shared_ptr<Function> import_onnx_model(const ONNXModelEditor& model_editor);
     } // namespace onnx_import
 
 } // namespace ngraph
