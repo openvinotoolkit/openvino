fbb6ec7f3eb0d6c13fac5cc6986aff55d6f31ca0
Tomasz Dołbniak <tomasz.dolbniak@intel.com>
2021-01-13 14:43:04 +0100

    nGraph copyright update for 2021 (#3832)

--- ngraph/test/op_version_tbl.hpp
+++ ngraph/test/op_version_tbl.hpp
@@ -1,5 +1,5 @@
 //*****************************************************************************
-// Copyright 2017-2020 Intel Corporation
+// Copyright 2017-2021 Intel Corporation
 //
 // Licensed under the Apache License, Version 2.0 (the "License", 0);
 // you may not use this file except in compliance with the License.
--- ngraph/test/type_prop/unary_ops.cpp
+++ ngraph/test/type_prop/unary_ops.cpp
@@ -1,5 +1,5 @@
 //*****************************************************************************
-// Copyright 2017-2020 Intel Corporation
+// Copyright 2017-2021 Intel Corporation
 //  Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
