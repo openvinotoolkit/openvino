0daee400edfc62c69fd69a8bc97ad6440be2249b
Mateusz Tabaka <mateusz.tabaka@intel.com>
2021-01-14 11:24:37 +0100

    [ONNX] don't hardcode broadcast shape in Mul, Add, Sub, Div (opset <= 6) operators (#3679)

--- ngraph/frontend/onnx_import/include/onnx_import/op/mul.hpp
+++ ngraph/frontend/onnx_import/include/onnx_import/op/mul.hpp
@@ -18,7 +18,6 @@
 
 #include <memory>
 
-#include "ngraph/builder/autobroadcast.hpp"
 #include "ngraph/node.hpp"
 #include "ngraph/op/broadcast.hpp"
 #include "ngraph/op/multiply.hpp"
@@ -37,14 +36,27 @@ namespace ngraph
                 {
                     const Output<ngraph::Node> lhs_node = node.get_ng_inputs().at(0);
                     Output<ngraph::Node> rhs_node = node.get_ng_inputs().at(1);
-                    auto lhs_rank = lhs_node.get_shape().size();
-                    auto rhs_rank = rhs_node.get_shape().size();
-                    auto axis = node.get_attribute_value<std::int64_t>("axis", lhs_rank - rhs_rank);
-                    // Unidirectional broadcast right node to left shape.
-                    rhs_node = ngraph::builder::opset1::legacy_broadcast_for_binary_operation(
-                        lhs_node, rhs_node, axis);
-                    return {std::make_shared<default_opset::Multiply>(
-                        lhs_node, rhs_node, ngraph::op::AutoBroadcastSpec::NONE)};
+                    bool broadcast = node.get_attribute_value<std::int64_t>("broadcast", 0);
+                    if (broadcast)
+                    {
+                        if (node.has_attribute("axis"))
+                        {
+                            // Unidirectional broadcast right node to left shape.
+                            auto axis = node.get_attribute_value<std::int64_t>("axis");
+                            rhs_node = std::make_shared<default_opset::Broadcast>(
+                                rhs_node,
+                                std::make_shared<default_opset::ShapeOf>(lhs_node),
+                                default_opset::Constant::create(element::i64, Shape{1}, {axis}));
+                        }
+                        else
+                        {
+                            rhs_node = std::make_shared<default_opset::Broadcast>(
+                                rhs_node, std::make_shared<default_opset::ShapeOf>(lhs_node));
+                        }
+                        return {std::make_shared<default_opset::Multiply>(
+                            lhs_node, rhs_node, ngraph::op::AutoBroadcastSpec::NONE)};
+                    }
+                    return {std::make_shared<default_opset::Multiply>(lhs_node, rhs_node)};
                 }
 
             } // namespace set_1
