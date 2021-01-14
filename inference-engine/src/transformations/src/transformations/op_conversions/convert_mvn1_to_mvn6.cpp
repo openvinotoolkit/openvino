27433b4ed559a50ac04cbd4ba4d874734ebd6c3f
Vafin, Maxim <maxim.vafin@intel.com>
2021-01-14 18:20:49 +0300

    Apply feedback

--- inference-engine/src/transformations/src/transformations/op_conversions/convert_mvn1_to_mvn6.cpp
+++ inference-engine/src/transformations/src/transformations/op_conversions/convert_mvn1_to_mvn6.cpp
@@ -15,21 +15,27 @@
 NGRAPH_RTTI_DEFINITION(ngraph::pass::ConvertMVN1ToMVN6, "ConvertMVN1ToMVN6", 0);
 
 ngraph::pass::ConvertMVN1ToMVN6::ConvertMVN1ToMVN6() {
-    auto mvn = pattern::wrap_type<ngraph::op::v0::MVN>();
+    auto mvn = pattern::wrap_type<ngraph::opset2::MVN>();
 
     ngraph::matcher_pass_callback callback = [](pattern::Matcher& m) {
-        auto mvn_node = std::dynamic_pointer_cast<ngraph::op::v0::MVN>(m.get_match_root());
+        auto mvn_node = std::dynamic_pointer_cast<ngraph::opset2::MVN>(m.get_match_root());
         if (!mvn_node) {
             return false;
         }
 
         const auto input = mvn_node->input_value(0);
-
+        auto input_rank = input.get_partial_shape().rank();
+        if (!input_rank.is_static()) {
+            return false;
+        }
         int64_t start_axis = 1 + (!mvn_node->get_across_channels());
+        if (input_rank.get_length() <= start_axis) {
+            return false;
+        }
         std::vector<int64_t> axes_v(input_rank.get_length() - start_axis);
         std::iota(axes_v.begin(), axes_v.end(), start_axis);
         auto axes = opset6::Constant::create(ngraph::element::i64, { axes_v.size() }, axes_v);
-        auto mvn6_node = std::make_shared<ngraph::op::v6::MVN>(input,
+        auto mvn6_node = std::make_shared<ngraph::opset6::MVN>(input,
             axes,
             mvn_node->get_normalize_variance(),
             mvn_node->get_eps(),
