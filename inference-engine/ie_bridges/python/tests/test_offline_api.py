385611ac2b0971dd8d751a6bee85d11c84fa057b
Gleb Kazantaev <gleb.kazantaev@intel.com>
2021-01-18 00:15:44 +0300

    Offline transformation API (#3408)
    * Added offline transformations library
    
    * Added python API for calling MOCTransformations
    
    * Added CF flag for MOC Transformations
    
    * Divided offline api to separate independent module
    
    * Update MOC pipeline to execute only fusions
    
    * Disable CF for PriorBox ops
    
    * Clean-up
    
    * Added python test
    
    * Removed transformation pipeline as it is not ready yet
    
    * Remove not related to this PR changes
    
    * Fixed build for dev package case; renamed to offline_transformations_api
    
    * Removed unrelated changes
    
    * Removed excess exports from cmake
    
    * Removed useless custom command from cmake

---
+++ inference-engine/ie_bridges/python/tests/test_offline_api.py
@@ -0,0 +1,28 @@
+from openvino.inference_engine import IECore, IENetwork
+from openvino.inference_engine.offline_api import ApplyMOCTransformations
+
+import ngraph as ng
+from ngraph.impl.op import Parameter
+from ngraph.impl import Function, Shape, Type
+
+from conftest import model_path
+
+
+test_net_xml, test_net_bin = model_path()
+
+
+def test_offline_api():
+    element_type = Type.f32
+    param = Parameter(element_type, Shape([1, 3, 22, 22]))
+    relu = ng.relu(param)
+    func = Function([relu], [param], 'test')
+    caps = Function.to_capsule(func)
+
+    cnnNetwork = IENetwork(caps)
+    assert cnnNetwork != None
+
+    ApplyMOCTransformations(cnnNetwork)
+
+    func2 = ng.function_from_cnn(cnnNetwork)
+    assert func2 != None
+    assert len(func2.get_ops()) == 3
