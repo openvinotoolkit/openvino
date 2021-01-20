307711554d484c0bfa17ac2037e1343667ab112c
prozen <piotr.rozen@intel.com>
2021-01-20 17:43:15 +0100

    fixed: gna split-concat-concat sequence issue fixed: gna permute-conv-maxpool-permute issue

--- inference-engine/src/gna_plugin/optimizer/gna_pass_manager.cpp
+++ inference-engine/src/gna_plugin/optimizer/gna_pass_manager.cpp
@@ -116,7 +116,7 @@ static CNNLayerPtr InsertCopyLayer(CNNLayerPtr prevLayer, CNNLayerPtr nextLayer,
                          copyLayer;
     getCreatorLayer(dataPtr) = copyWithQuant;
     copyWithQuant->outData.push_back(dataPtr);
-    CNNNetworkInsertLayer(prevLayer, nextLayer, copyWithQuant);
+    CNNNetworkInsertLayer(prevLayer, nextLayer, copyWithQuant, invalid_data_idx, beforeIdx);
     return copyWithQuant;
 }
 
@@ -822,12 +822,8 @@ void InsertCopyLayerPass::run() {
     // and copies before concat layers (one less copy than outputs)
     for (auto & l : *pLayers) {
         if (LayerInfo(l).isNonFunctional()) continue;
-        auto isNonFunctional = [](InferenceEngine::CNNLayerPtr layer) {
-            return LayerInfo(layer).isNonFunctional();
-        };
-        // Crop -> Concat, Input -> Split -> Concat and Concat -> Memory cases
-        if ((LayerInfo(l).isCrop() && !LayerInfo(l).isCropAffined()) || LayerInfo(l).isConcat() ||
-            (LayerInfo(l).isSplit() && LayerInfo(CNNNetPrevLayerSkipCertain(l, 0, isNonFunctional)).isInput())) {
+        // Crop -> Concat and Concat -> Memory cases
+        if ((LayerInfo(l).isCrop() && !LayerInfo(l).isCropAffined()) || LayerInfo(l).isConcat() || LayerInfo(l).isSplit() || LayerInfo(l).isMemory()) {
             std::vector<std::tuple<CNNLayerPtr, CNNLayerPtr, size_t>> copy_insertion_tuples;
             std::vector<std::tuple<CNNLayerPtr, CNNLayerPtr, size_t>> delayed_copy_insertion_tuples;
 
@@ -837,27 +833,29 @@ void InsertCopyLayerPass::run() {
                     auto original_child = childLayer.second;
                     auto original_parent = l;
                     auto current_layer = original_child;
-                    size_t input_idx = CNNLayerFindInsDataIdxes(output, original_child)[0];
+                    std::vector<int> connections = CNNLayerFindInsDataIdxes(output, original_child);
 
-                    while (LayerInfo(current_layer).isNonFunctional()) {
-                        if (current_layer->outData.size() == 0) break;
-                        if (getInputTo(current_layer->outData[0]).size() == 0) break;
+                    for (auto input_idx : connections) {
+                        while (LayerInfo(current_layer).isNonFunctional()) {
+                            if (current_layer->outData.size() == 0) break;
+                            if (getInputTo(current_layer->outData[0]).size() == 0) break;
 
-                        auto next_layer = CNNNetGetNextLayerSkipCertain(current_layer, 0, 0, [](CNNLayerPtr origin){return false;}).first;
-                        if (current_layer->outData.size() == 1 && getInputTo(current_layer->outData[0]).size() == 1 && original_child == current_layer) {
-                            original_child = next_layer;
-                            original_parent = current_layer;
-                            input_idx = CNNLayerFindInsDataIdxes(original_parent->outData[0], original_child)[0];
+                            auto next_layer = CNNNetGetNextLayerSkipCertain(current_layer, 0, 0, [](CNNLayerPtr origin) {return false; }).first;
+                            if (current_layer->outData.size() == 1 && getInputTo(current_layer->outData[0]).size() == 1 && original_child == current_layer) {
+                                original_child = next_layer;
+                                original_parent = current_layer;
+                                input_idx = CNNLayerFindInsDataIdxes(original_parent->outData[0], original_child)[0];
+                            }
+                            current_layer = next_layer;
                         }
-                        current_layer = next_layer;
-                    }
 
-                    if ((LayerInfo(l).isConcat() || LayerInfo(l).isCrop() || LayerInfo(l).isSplit()) && LayerInfo(current_layer).isMemory()) {
-                        // Concat|Split|Crop -> Memory case
-                        delayed_copy_insertion_tuples.push_back(std::make_tuple(original_parent, original_child, input_idx));
-                    } else if ((LayerInfo(l).isCrop() || LayerInfo(l).isSplit()) && LayerInfo(current_layer).isConcat()) {
-                        // Crop -> Concat case
-                        copy_insertion_tuples.push_back(std::make_tuple(original_parent, original_child, input_idx));
+                        if ((LayerInfo(l).isConcat() || LayerInfo(l).isCrop() || LayerInfo(l).isSplit()) && LayerInfo(current_layer).isMemory()) {
+                            // Concat|Split|Crop -> Memory case
+                            delayed_copy_insertion_tuples.push_back(std::make_tuple(original_parent, original_child, input_idx));
+                        } else if ((LayerInfo(l).isSplit() || LayerInfo(l).isMemory() || LayerInfo(l).isCrop()) && LayerInfo(current_layer).isConcat()) {
+                            // Split|Crop|Memory -> Concat case
+                            copy_insertion_tuples.push_back(std::make_tuple(original_parent, original_child, input_idx));
+                        }
                     }
                 }
             }
@@ -2005,7 +2003,7 @@ void MoveFakeQuantizeLayerIntoQuantParamsPass :: run() {
 }
 
 int PassManager::run(int index) {
-#if defined PLOT || defined ENABLE_V7_SERIALIZE
+#ifdef PLOT || defined ENABLE_V7_SERIALIZE
     auto dumpNetworkAfterPass = [&index, this] (std::shared_ptr<Pass> pass) {
         std::string name = std::string("gna_passes_") + (index < 10 ? "0" : "") + std::to_string(index) + "_" + pass->getName();
 #ifdef PLOT
