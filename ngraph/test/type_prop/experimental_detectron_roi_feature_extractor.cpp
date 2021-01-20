fb4f309cfc38fcaa7be537fe98e0ef3ad6db92a4
VGavrilov <vladimir.gavrilov@intel.com>
2021-01-20 11:04:57 +0300

    Added tests for input shapes of op::v6::ExperimentalDetectronROIFeatureExtractor in the case when inputs shapes consist of intervals.

--- ngraph/test/type_prop/experimental_detectron_roi_feature_extractor.cpp
+++ ngraph/test/type_prop/experimental_detectron_roi_feature_extractor.cpp
@@ -87,3 +87,85 @@ TEST(type_prop, detectron_roi_feature_extractor_dynamic)
         ASSERT_TRUE(roi->get_output_partial_shape(0).same_scheme(ref_out_shape));
     }
 }
+
+TEST(type_prop, detectron_roi_feature_extractor_intervals)
+{
+    Attrs attrs;
+    attrs.aligned = false;
+    attrs.output_size = 14;
+    attrs.sampling_ratio = 2;
+    attrs.pyramid_scales = {4, 8, 16, 32};
+
+    struct Shapes
+    {
+        PartialShape input_shape;
+        Dimension channels[4];
+    };
+
+    const auto dyn_dim = Dimension::dynamic();
+
+    std::vector<Shapes> shapes = {
+        {{1000, 4}, {Dimension(0, 128), Dimension(0, 256), Dimension(0, 64), Dimension(0, 33)}},
+        {{1000, 4}, {Dimension(0, 128), Dimension(0, 256), Dimension(0, 64), Dimension(33)}},
+        {{1000, 4}, {Dimension(0, 128), Dimension(0, 256), Dimension(64), Dimension(0, 33)}},
+        {{1000, 4}, {Dimension(0, 128), Dimension(0, 256), Dimension(64), Dimension(33)}},
+        {{1000, 4}, {Dimension(0, 128), Dimension(256), Dimension(0, 64), Dimension(0, 33)}},
+        {{1000, 4}, {Dimension(0, 128), Dimension(256), Dimension(0, 64), Dimension(33)}},
+        {{1000, 4}, {Dimension(0, 128), Dimension(256), Dimension(64), Dimension(0, 33)}},
+        {{1000, 4}, {Dimension(0, 128), Dimension(256), Dimension(64), Dimension(33)}},
+        {{1000, 4}, {Dimension(128), Dimension(0, 256), Dimension(0, 64), Dimension(0, 33)}},
+        {{1000, 4}, {Dimension(128), Dimension(0, 256), Dimension(0, 64), Dimension(33)}},
+        {{1000, 4}, {Dimension(128), Dimension(0, 256), Dimension(64), Dimension(0, 33)}},
+        {{1000, 4}, {Dimension(128), Dimension(0, 256), Dimension(64), Dimension(33)}},
+        {{1000, 4}, {Dimension(128), Dimension(256), Dimension(0, 64), Dimension(0, 33)}},
+        {{1000, 4}, {Dimension(128), Dimension(256), Dimension(0, 64), Dimension(33)}},
+        {{1000, 4}, {Dimension(128), Dimension(256), Dimension(64), Dimension(0, 33)}},
+        {{1000, 4}, {Dimension(128), Dimension(256), Dimension(64), Dimension(33)}},
+        {{dyn_dim, 4}, {Dimension(0, 128), Dimension(0, 256), Dimension(0, 64), Dimension(0, 33)}},
+        {{dyn_dim, 4}, {Dimension(0, 128), Dimension(0, 256), Dimension(0, 64), Dimension(33)}},
+        {{dyn_dim, 4}, {Dimension(0, 128), Dimension(0, 256), Dimension(64), Dimension(0, 33)}},
+        {{dyn_dim, 4}, {Dimension(0, 128), Dimension(0, 256), Dimension(64), Dimension(33)}},
+        {{dyn_dim, 4}, {Dimension(0, 128), Dimension(256), Dimension(0, 64), Dimension(0, 33)}},
+        {{dyn_dim, 4}, {Dimension(0, 128), Dimension(256), Dimension(0, 64), Dimension(33)}},
+        {{dyn_dim, 4}, {Dimension(0, 128), Dimension(256), Dimension(64), Dimension(0, 33)}},
+        {{dyn_dim, 4}, {Dimension(0, 128), Dimension(256), Dimension(64), Dimension(33)}},
+        {{dyn_dim, 4}, {Dimension(128), Dimension(0, 256), Dimension(0, 64), Dimension(0, 33)}},
+        {{dyn_dim, 4}, {Dimension(128), Dimension(0, 256), Dimension(0, 64), Dimension(33)}},
+        {{dyn_dim, 4}, {Dimension(128), Dimension(0, 256), Dimension(64), Dimension(0, 33)}},
+        {{dyn_dim, 4}, {Dimension(128), Dimension(0, 256), Dimension(64), Dimension(33)}},
+        {{dyn_dim, 4}, {Dimension(128), Dimension(256), Dimension(0, 64), Dimension(0, 33)}},
+        {{dyn_dim, 4}, {Dimension(128), Dimension(256), Dimension(0, 64), Dimension(33)}},
+        {{dyn_dim, 4}, {Dimension(128), Dimension(256), Dimension(64), Dimension(0, 33)}},
+        {{dyn_dim, 4}, {Dimension(128), Dimension(256), Dimension(64), Dimension(33)}}};
+
+    for (const auto& s : shapes)
+    {
+        auto layer0_channels = s.channels[0];
+        auto layer1_channels = s.channels[1];
+        auto layer2_channels = s.channels[2];
+        auto layer3_channels = s.channels[3];
+
+        auto layer0_shape = PartialShape{1, layer0_channels, 200, 336};
+        auto layer1_shape = PartialShape{1, layer1_channels, 100, 168};
+        auto layer2_shape = PartialShape{1, layer2_channels, 50, 84};
+        auto layer3_shape = PartialShape{1, layer3_channels, 25, 42};
+
+        auto expected_channels =
+            layer0_channels & layer1_channels & layer2_channels & layer3_channels;
+
+        auto ref_out_shape = PartialShape{s.input_shape[0], expected_channels, 14, 14};
+
+        auto input = std::make_shared<op::Parameter>(element::f32, s.input_shape);
+        auto pyramid_layer0 = std::make_shared<op::Parameter>(element::f32, layer0_shape);
+        auto pyramid_layer1 = std::make_shared<op::Parameter>(element::f32, layer1_shape);
+        auto pyramid_layer2 = std::make_shared<op::Parameter>(element::f32, layer2_shape);
+        auto pyramid_layer3 = std::make_shared<op::Parameter>(element::f32, layer3_shape);
+
+        auto roi = std::make_shared<ExperimentalROI>(
+            NodeVector{input, pyramid_layer0, pyramid_layer1, pyramid_layer2, pyramid_layer3},
+            attrs);
+
+        ASSERT_EQ(roi->get_output_element_type(0), element::f32);
+        ASSERT_TRUE(roi->get_output_partial_shape(0).same_scheme(ref_out_shape));
+    }
+}
