6382a255dd2ed9a14c8603d088bf3578bfca5cb9
jiwaszki <jan.iwaszkiewicz@intel.com>
2021-01-21 18:38:09 +0100

    Rename and remove xfails in onnx tests

--- ngraph/python/tests/__init__.py
+++ ngraph/python/tests/__init__.py
@@ -88,14 +88,8 @@ xfail_issue_35923 = xfail_test(reason="RuntimeError: PReLU without weights is no
 xfail_issue_35924 = xfail_test(reason="Assertion error - elu results mismatch")
 xfail_issue_35925 = xfail_test(reason="Assertion error - reduction ops results mismatch")
 xfail_issue_35927 = xfail_test(reason="RuntimeError: B has zero dimension that is not allowable")
-xfail_issue_40319 = xfail_test(reason="RuntimeError: [NOT_IMPLEMENTED] Input image format FP64 is not "
-                                      "supported yet...")
 xfail_issue_35930 = xfail_test(reason="onnx.onnx_cpp2py_export.checker.ValidationError: "
                                       "Required attribute 'to' is missing.")
-xfail_issue_36476 = xfail_test(reason="RuntimeError: [NOT_IMPLEMENTED] Input image format U32 is "
-                               "not supported yet...")
-xfail_issue_36478 = xfail_test(reason="RuntimeError: [NOT_IMPLEMENTED] Input image format U64 is "
-                               "not supported yet...")
 xfail_issue_36480 = xfail_test(reason="RuntimeError: [NOT_FOUND] Unsupported property dummy_option "
                                "by CPU plugin")
 xfail_issue_36485 = xfail_test(reason="RuntimeError: Check 'm_group >= 1' failed at "
@@ -184,6 +178,13 @@ xfail_issue_44976 = xfail_test(reason="E   RuntimeError: Quantize layer with nam
                                       "FakeQuantize_xxx has non const input on 1 port")
 xfail_issue_46762 = xfail_test(reason="Incorrect result of Minimum op if u16 data type is used")
 xfail_issue_46765 = xfail_test(reason="select_last_index attribute is not supported by ArgMin and ArgMax")
+xfail_issue_47317 = xfail_test(reason="RuntimeError: While validating ONNX node '<Node(Add): 2>': "
+                                      "Check shape_size(axes_shape) == input_rank' failed")
+xfail_issue_47323 = xfail_test(reason="RuntimeError: The plugin does not support FP64")
+xfail_issue_47324 = xfail_test(reason="RuntimeError: CumSum layer with name 'y' doesn't support 'axis' "
+                                      "input tensor with rank: 1")
+xfail_issue_47330 = xfail_test(reason="RuntimeError: Eltwise node with name `[name]` doesn't support "
+                                      "FP64 precision.")
 
 # Model MSFT issues:
 xfail_issue_37957 = xfail_test(reason="RuntimeError: nGraph does not support the following ONNX operations:"
