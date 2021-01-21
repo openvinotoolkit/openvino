33df24464c183a049db8297861070b538580e134
Mateusz Tabaka <mateusz.tabaka@intel.com>
2021-01-21 09:58:00 +0100

    Add PadFusion transformation (#3785)
    * Add PadFusion transformation
    
    Ticket: 46482
    
    * set pad explicit
    
    * fuse ifs
    
    * address review comments
    
    * fix signed to unsigned comparison warning
    
    * add MATCHER_SCOPE

--- inference-engine/src/transformations/src/transformations/common_optimizations/common_optimizations.cpp
+++ inference-engine/src/transformations/src/transformations/common_optimizations/common_optimizations.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2020 Intel Corporation
+// Copyright (C) 2020-2021 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
 
@@ -28,6 +28,7 @@
 #include "transformations/common_optimizations/convert_quantize_dequantize.hpp"
 #include "transformations/common_optimizations/relu_fake_quantize_fusion.hpp"
 #include "transformations/common_optimizations/clamp_fusion.hpp"
+#include "transformations/common_optimizations/pad_fusion.hpp"
 #include "transformations/op_conversions/bidirectional_sequences_decomposition.hpp"
 #include "transformations/op_conversions/convert_pad_to_group_conv.hpp"
 #include "transformations/op_conversions/convert_divide.hpp"
@@ -80,6 +81,7 @@ bool ngraph::pass::CommonOptimizations::run_on_function(std::shared_ptr<ngraph::
     manager.register_pass<ngraph::pass::ConvertPadToGroupConvolution, false>();
     manager.register_pass<ngraph::pass::NormalizeL2Fusion>();
     manager.register_pass<ngraph::pass::ClampFusion>();
+    manager.register_pass<ngraph::pass::PadFusion>();
 
     auto decomp = manager.register_pass<ngraph::pass::GraphRewrite>();
     decomp->add_matcher<ngraph::pass::BidirectionalLSTMSequenceDecomposition>();
