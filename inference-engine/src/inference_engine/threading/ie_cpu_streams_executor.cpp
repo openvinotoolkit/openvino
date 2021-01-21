563e30c9ef4ac76360d428fbc25f973f93fdc29d
myshevts <maxim.y.shevtsov@intel.com>
2021-01-21 10:50:51 +0300

    map to vector to simplify the things

--- inference-engine/src/inference_engine/threading/ie_cpu_streams_executor.cpp
+++ inference-engine/src/inference_engine/threading/ie_cpu_streams_executor.cpp
@@ -80,9 +80,9 @@ struct CPUStreamsExecutor::Impl {
                 } else {
                     // assigning the stream to the core type in the round-robin fashion
                     // wrapping around total_streams (i.e. how many streams all different core types can handle together)
-                    const auto total_streams = _impl->total_streams_on_core_types.rbegin()->second;
+                    const auto total_streams = _impl->total_streams_on_core_types.back().second;
                     const auto streamId_wrapped = _streamId % total_streams;
-                    const auto& selected_core_type = std::find_if(_impl->total_streams_on_core_types.begin(), _impl->total_streams_on_core_types.end(),
+                    const auto& selected_core_type = std::find_if(_impl->total_streams_on_core_types.cbegin(), _impl->total_streams_on_core_types.cend(),
                         [streamId_wrapped](const auto& p) { return p.second > streamId_wrapped; })->first;
                     _taskArena.reset(new tbb::task_arena{ tbb::task_arena::constraints{selected_core_type, concurrency} });
                     // TODO: REMOVE THE DEBUG PRINTF
@@ -209,12 +209,12 @@ struct CPUStreamsExecutor::Impl {
                 sum += num_streams_for_core_type;
                 // prefix sum, so the core type for a given stream id will be deduced just as a upper_bound
                 // (notice that the map keeps the elements in the descending order, so the big cores are populated first)
-                total_streams_on_core_types[type] = sum;
+                total_streams_on_core_types.push_back({type, sum});
             }
             // TODO: REMOVE THE DEBUG PRINTF
             printf("%s total_streams_on_core_types: [%d core_type, %d #streams], [%d core_type, %d #streams]) \n",
-                _config._name.c_str(), static_cast<int>(total_streams_on_core_types.begin()->first), total_streams_on_core_types.begin()->second,
-                static_cast<int>(total_streams_on_core_types.rbegin()->first), total_streams_on_core_types.rbegin()->second);
+                _config._name.c_str(), static_cast<int>(total_streams_on_core_types.front().first), total_streams_on_core_types.front().second,
+                static_cast<int>(total_streams_on_core_types.back().first), total_streams_on_core_types.back().second);
         #endif
         }
         for (auto streamId = 0; streamId < _config._streams; ++streamId) {
@@ -286,10 +286,12 @@ struct CPUStreamsExecutor::Impl {
     std::vector<int>                        _usedNumaNodes;
     ThreadLocal<std::shared_ptr<Stream>>    _streams;
     #if TBB_INTERFACE_VERSION >= 12010 // TBB with hybrid CPU aware task_arena api
-    // streams mapping to the core types
+    // stream id mapping to the core type
     // stored in the reversed order (so the big cores, with the highest core_type_id value, are populated first)
-    using MapStreamIdToCoreTypes = std::map<oneapi::tbb::core_type_id, int, std::greater<int>>;
-    MapStreamIdToCoreTypes total_streams_on_core_types;
+    // every entry is the core type and #streams that this AND ALL EARLIER entries can handle (prefix sum)
+    // (so mapping is actually just an upper_bound: core type is deduced from the entry for which the id < #streams)
+    using StreamIdToCoreTypes = std::vector<std::pair<oneapi::tbb::core_type_id, int>>;
+    StreamIdToCoreTypes total_streams_on_core_types;
     #endif
 };
 
