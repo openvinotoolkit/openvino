75253deb828cec8c703a74a1a6b148168c39ac29
VGavrilov <vladimir.gavrilov@intel.com>
2021-01-21 14:59:31 +0300

    Now linear_onnx_func throws an exception for incorrect input rank.

--- ngraph/core/reference/include/ngraph/runtime/reference/interpolate.hpp
+++ ngraph/core/reference/include/ngraph/runtime/reference/interpolate.hpp
@@ -648,7 +648,8 @@ namespace ngraph
                 case 4: linear_onnx4D_func(input_data, out); break;
                 case 3:
                 case 5: linear_onnx5D_func(input_data, out); break;
-                default:;
+                default: throw ngraph_error("Mode 'linear_onnx' of the layer Interpolate-4 supports"
+                                            " only 2D, 3D, 4D, or 5D input tensors."); break;
                 }
             }
 
