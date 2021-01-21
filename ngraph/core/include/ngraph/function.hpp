60c3f3a60dea1889a4ae7fcf75ecf3875208420f
sadolini <svetlana.a.dolinina@intel.com>
2021-01-21 10:23:58 +0300

    extended comments

--- ngraph/core/include/ngraph/function.hpp
+++ ngraph/core/include/ngraph/function.hpp
@@ -1,5 +1,5 @@
 //*****************************************************************************
-// Copyright 2017-2020 Intel Corporation
+// Copyright 2017-2021 Intel Corporation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -170,14 +170,27 @@ namespace ngraph
         /// \param result Result node to delete
         void remove_result(const std::shared_ptr<op::Result>& result);
 
-        /// \brief Add new Parameter nodes to the list. Method doesn't validate graph, it should be
-        /// done manually after all changes.
+        /// \brief Add new Parameter nodes to the list. 
+        ///
+        /// Method doesn't change or validate graph, it should be done manually.
+        /// For example, if you want to replace `ReadValue` node by `Parameter`, you should do the following steps:
+        /// * replace node `ReadValue` by `Parameter` in graph
+        /// * call add_parameter() to add new input to the list
+        /// * call graph validation to check correctness of changes 
+        ///
         /// \param params new Parameter nodes
         void add_parameters(const ParameterVector& params);
 
         /// \brief Delete Parameter node from the list of parameters. Method will not delete node
-        /// from graph. Attention: Indexing of parameters will be changed. \param param Parameter
-        /// node to delete
+        /// from graph. Attention: Indexing of parameters can be changed.
+        ///
+        /// Possible use of method is to replace input by variable. For it the following steps should be done:
+        /// * `Parameter` node should be replaced by `ReadValue`
+        /// * call remove_parameter(param) to remove input from the list
+        /// * check if any parameter indexes are saved/used somewhere, update it for all inputs because indexes can be changed
+        /// * call graph validation to check all changes
+        ///
+        /// \param param Parameter node to delete
         void remove_parameter(const std::shared_ptr<op::Parameter>& param);
 
     private:
