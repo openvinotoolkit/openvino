6bbc4cc435d7687379fd897874bdfcaa011afc21
Mikhail Treskin <mikhail.treskin@intel.com>
2021-01-21 16:59:39 +0300

    Fix FQ OP inheritance

--- ngraph/core/src/op/fake_quantize.cpp
+++ ngraph/core/src/op/fake_quantize.cpp
@@ -32,7 +32,7 @@ NGRAPH_SUPPRESS_DEPRECATED_START
 NGRAPH_RTTI_DEFINITION(op::FakeQuantize, "FakeQuantize", 0);
 
 op::FakeQuantize::FakeQuantize()
-    : FusedOp()
+    : Op()
     , m_levels()
 {
 }
