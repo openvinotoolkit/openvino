d5d57ef8a4ad37adc22aec72b174b3426bdcae62
blesniewski <bartosz.lesniewski@intel.com>
2021-01-27 00:53:15 +0100

    remove trivial creators

--- inference-engine/src/legacy_api/src/convert_function_to_cnn_network.cpp
+++ inference-engine/src/legacy_api/src/convert_function_to_cnn_network.cpp
@@ -1586,14 +1586,6 @@ InferenceEngine::details::CNNLayerCreator::CNNLayerCreator(const std::shared_ptr
         return res;
     });
 
-    addSpecificCreator({"PSROIPooling"}, [](const std::shared_ptr<::ngraph::Node> &node,
-                                            const std::map<std::string, std::string> &params) -> CNNLayerPtr {
-        LayerParams attrs = {node->get_friendly_name(), "PSROIPooling", details::convertPrecision(node->get_output_element_type(0))};
-        auto res = std::make_shared<InferenceEngine::CNNLayer>(attrs);
-        res->params = params;
-        return res;
-    });
-
     addSpecificCreator({"VariadicSplit"}, [](const std::shared_ptr<::ngraph::Node>& node,
                                              const std::map<std::string, std::string>& params) -> CNNLayerPtr {
         LayerParams attrs = {node->get_friendly_name(), "Split", details::convertPrecision(node->get_output_element_type(0))};
@@ -1613,14 +1605,6 @@ InferenceEngine::details::CNNLayerCreator::CNNLayerCreator(const std::shared_ptr
         }
 
         res->params["axis"] = Builder::asString(axis);
-        return res;
-    });
-
-    addSpecificCreator({"ReorgYolo"}, [](const std::shared_ptr<::ngraph::Node>& node,
-                                         const std::map<std::string, std::string>& params) -> CNNLayerPtr {
-        LayerParams attrs = {node->get_friendly_name(), "ReorgYolo", details::convertPrecision(node->get_output_element_type(0))};
-        auto res = std::make_shared<InferenceEngine::CNNLayer>(attrs);
-        res->params = params;
         return res;
     });
 }
