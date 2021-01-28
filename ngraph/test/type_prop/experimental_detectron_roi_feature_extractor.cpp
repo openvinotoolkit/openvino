aa8f9f7507c8293ca2f9cd2f4070aebabfeebeea
VGavrilov <vladimir.gavrilov@intel.com>
2021-01-28 10:02:44 +0300

    Started to rewrite the test type_prop.detectron_roi_feature_extractor_intervals_1 as a parametrized test.

--- ngraph/test/type_prop/experimental_detectron_roi_feature_extractor.cpp
+++ ngraph/test/type_prop/experimental_detectron_roi_feature_extractor.cpp
@@ -170,6 +170,69 @@ TEST(type_prop, detectron_roi_feature_extractor_intervals)
     }
 }
 
+struct ROIFeatureIntervalsTestParams{
+    PartialShape input_shape;
+    Dimension channels[4];
+    Dimension first_dims[4];
+};
+
+void run_roi_feature_test(const ROIFeatureIntervalsTestParams& params)
+{
+    Attrs attrs;
+    attrs.aligned = false;
+    attrs.output_size = 14;
+    attrs.sampling_ratio = 2;
+    attrs.pyramid_scales = {4, 8, 16, 32};
+
+    auto layer0_channels = params.channels[0];
+    auto layer1_channels = params.channels[1];
+    auto layer2_channels = params.channels[2];
+    auto layer3_channels = params.channels[3];
+
+    auto layer0_shape = PartialShape{params.first_dims[0], layer0_channels, 200, 336};
+    auto layer1_shape = PartialShape{params.first_dims[1], layer1_channels, 100, 168};
+    auto layer2_shape = PartialShape{params.first_dims[2], layer2_channels, 50, 84};
+    auto layer3_shape = PartialShape{params.first_dims[3], layer3_channels, 25, 42};
+
+    auto expected_channels = layer0_channels & layer1_channels & layer2_channels & layer3_channels;
+
+    auto ref_out_shape = PartialShape{params.input_shape[0], expected_channels, 14, 14};
+
+    auto input = std::make_shared<op::Parameter>(element::f32, params.input_shape);
+    auto pyramid_layer0 = std::make_shared<op::Parameter>(element::f32, layer0_shape);
+    auto pyramid_layer1 = std::make_shared<op::Parameter>(element::f32, layer1_shape);
+    auto pyramid_layer2 = std::make_shared<op::Parameter>(element::f32, layer2_shape);
+    auto pyramid_layer3 = std::make_shared<op::Parameter>(element::f32, layer3_shape);
+
+    auto roi = std::make_shared<ExperimentalROI>(
+        NodeVector{input, pyramid_layer0, pyramid_layer1, pyramid_layer2, pyramid_layer3}, attrs);
+
+    ASSERT_EQ(roi->get_output_element_type(0), element::f32);
+    ASSERT_TRUE(roi->get_output_partial_shape(0).same_scheme(ref_out_shape));
+}
+
+struct ROIFeatureIntervalsTest : ::testing::TestWithParam<ROIFeatureIntervalsTestParams>
+{
+};
+
+TEST_P(ROIFeatureIntervalsTest, detectron_roi_feature_extractor_intervals_2)
+{
+    run_roi_feature_test(GetParam());
+}
+
+INSTANTIATE_TEST_CASE_P(type_prop,
+                        ROIFeatureIntervalsTest,
+                        ::testing::Values(ROIFeatureIntervalsTestParams{{1000, Dimension(0, 5)},
+                                                                        {Dimension(0, 128),
+                                                                         Dimension(0, 256),
+                                                                         Dimension(0, 64),
+                                                                         Dimension(0, 33)},
+                                                                        {Dimension(0, 2),
+                                                                         Dimension(1, 3),
+                                                                         Dimension(0, 5),
+                                                                         Dimension(1, 2)}}),
+                        PrintToDummyParamName());
+
 TEST(type_prop, detectron_roi_feature_extractor_intervals_1)
 {
     Attrs attrs;
