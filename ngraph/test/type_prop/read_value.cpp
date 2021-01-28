e9e36c24456bc9f25a5f2648da6475b1c103e94e
ggalieroc <gabriele.galiero.casay@intel.com>
2021-01-28 15:47:30 +0100

    ReadValue: Added type_prop unit tests

--- ngraph/test/type_prop/read_value.cpp
+++ ngraph/test/type_prop/read_value.cpp
@@ -21,11 +21,54 @@
 using namespace std;
 using namespace ngraph;
 
-TEST(type_prop, read_value_deduce)
+TEST(type_prop, read_value_deduce_shape_basic)
 {
     auto input = make_shared<op::Parameter>(element::f32, Shape{1, 2, 64, 64});
-    auto read_value = make_shared<op::ReadValue>(input, "variable_id");
+    auto read_value = make_shared<op::v3::ReadValue>(input, "variable_id");
 
     ASSERT_EQ(read_value->get_element_type(), element::f32);
     ASSERT_EQ(read_value->get_shape(), (Shape{1, 2, 64, 64}));
+    ASSERT_EQ(read_value->get_variable_id(), "variable_id");
+
+    auto var_info = read_value->get_variable();
+    ASSERT_EQ(var_info->get_info().data_type, element::f32);
+    ASSERT_EQ(var_info->get_info().data_shape, (Shape{1, 2, 64, 64}));
+    ASSERT_EQ(var_info->get_info().variable_id, "variable_id");
+}
+
+TEST(type_prop, read_value_deduce_shape_dynamic)
+{
+    auto input =
+        make_shared<op::Parameter>(element::i64, PartialShape{Dimension::dynamic(), 2, 64, 64});
+    auto read_value = make_shared<op::v3::ReadValue>(input, "variable_id");
+
+    ASSERT_EQ(read_value->get_element_type(), element::i64);
+    ASSERT_TRUE(read_value->get_output_partial_shape(0).same_scheme(
+        PartialShape{Dimension::dynamic(), 2, 64, 64}));
+    ASSERT_EQ(read_value->get_variable_id(), "variable_id");
+
+    auto var_info = read_value->get_variable();
+    ASSERT_EQ(var_info->get_info().data_type, element::i64);
+    ASSERT_TRUE(
+        var_info->get_info().data_shape.same_scheme(PartialShape{Dimension::dynamic(), 2, 64, 64}));
+    ASSERT_EQ(var_info->get_info().variable_id, "variable_id");
+}
+
+TEST(type_prop, read_value_invalid_variable_id)
+{
+    auto input = make_shared<op::Parameter>(element::f16, Shape{1, 2, 64, 64});
+    try
+    {
+        auto read_value = make_shared<op::v3::ReadValue>(input, "");
+        // variable_id attribute cannot be an empty string
+        FAIL() << "Exception should be thrown";
+    }
+    catch (const NodeValidationFailure error)
+    {
+        EXPECT_HAS_SUBSTRING(error.what(), "Variable identifier may not be an empty string.");
+    }
+    catch (...)
+    {
+        FAIL() << "Test failed for unexpected reason";
+    }
 }
