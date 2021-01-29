9181d847293545617ee1ae10b34c573f0c914963
Tomasz Socha <tomasz.socha@intel.com>
2021-01-29 14:09:52 +0100

    Don't set tensor names for result output tensor

--- ngraph/test/onnx/onnx_tensor_names.cpp
+++ ngraph/test/onnx/onnx_tensor_names.cpp
@@ -45,8 +45,6 @@ NGRAPH_TEST(onnx_tensor_names, simple_model)
     ASSERT_EQ(ops[3]->get_output_tensor(0).get_names(),
               std::unordered_set<std::string>{"final_output"});
     ASSERT_EQ(ops[4]->get_friendly_name(), "final_output");
-    ASSERT_EQ(ops[4]->get_output_tensor(0).get_names(),
-              std::unordered_set<std::string>{"final_output"});
 
     ASSERT_EQ(function->get_result()->get_input_tensor(0).get_names(),
               std::unordered_set<std::string>{"final_output"});
@@ -69,9 +67,7 @@ NGRAPH_TEST(onnx_tensor_names, node_multiple_outputs)
     ASSERT_EQ(ops[2]->get_output_tensor(1).get_names(), std::unordered_set<std::string>{"indices"});
     // result nodes are generated in different order than function results.
     ASSERT_EQ(ops[3]->get_friendly_name(), "indices");
-    ASSERT_EQ(ops[3]->get_output_tensor(0).get_names(), std::unordered_set<std::string>{"indices"});
     ASSERT_EQ(ops[4]->get_friendly_name(), "values");
-    ASSERT_EQ(ops[4]->get_output_tensor(0).get_names(), std::unordered_set<std::string>{"values"});
 
     ASSERT_EQ(function->get_results()[0]->get_input_tensor(0).get_names(),
               std::unordered_set<std::string>{"values"});
