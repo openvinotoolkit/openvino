6f085f06f2ea18b0b71a9b90e420f0c196442b39
iimironov <iliya.mironov@intel.com>
2021-02-01 20:03:04 +0300

    Add replasment for ctc_greedy_decoder

--- model-optimizer/extensions/front/tf/CTCGreedyDecoderReplacement.py
+++ model-optimizer/extensions/front/tf/CTCGreedyDecoderReplacement.py
@@ -18,9 +18,10 @@ import logging as log
 
 import numpy as np
 
-from extensions.ops.Cast import Cast
-from extensions.front.Pack import Pack
 from extensions.front.FillToBroadcast import FillToBroadcast
+from extensions.front.Pack import Pack
+from extensions.ops.Cast import Cast
+from extensions.ops.transpose import Transpose
 from mo.front.common.partial_infer.utils import int64_array
 from mo.front.common.replacement import FrontReplacementSubgraph
 from mo.front.tf.graph_utils import create_op_with_const_inputs
@@ -53,11 +54,12 @@ class CTCGreedyDecoderReplacement(FrontReplacementSubgraph):
     @staticmethod
     def pattern(**kwargs):
         return dict(
-            nodes=[('decoder', dict(op='CTCGreedyDecoder')),
+            nodes=[('decoder', dict(op='CTCGreedyDecoderSeqLen')),
                    ('cast', dict(op='Cast')),
                    ('sparse_to_dense', dict(op='SparseToDense'))
                    ],
             edges=[('decoder', 'sparse_to_dense', {'out': 0}),
+                   ('decoder', 'sparse_to_dense', {'out': 2}),
                    ('decoder', 'cast', {'out': 1}),
                    ('cast', 'sparse_to_dense', {'out': 0})
                    ]
@@ -70,35 +72,17 @@ class CTCGreedyDecoderReplacement(FrontReplacementSubgraph):
         ctc_greedy_decoder = match['decoder']
         cast = match['cast']
         sparse_to_dense = match['sparse_to_dense']
-        sparse_to_dense_name = sparse_to_dense.soft_get('name', sparse_to_dense.id)
 
-        # disconnect SparseToDense and Cast nodes
-        sparse_to_dense.in_port(0).disconnect()
-        cast.in_port(0).disconnect()
-
-        # transform CTCGreedyDecoder output to TensorFlow's one:
-        # 1. squeeze the output to [N, T] shape
-        # 2. cast it to integer
-        squeeze_dec_seq = create_op_with_const_inputs(graph, Squeeze, {1: int64_array([2, 3])},
-                                                      {'name': sparse_to_dense_name})
-        squeeze_dec_seq.in_port(0).connect(ctc_greedy_decoder.out_port(0))
-        cast_to_int = Cast(graph, {'name': sparse_to_dense_name + '/CastToInt',
-                                   'dst_type': np.int32}).create_node()
-        cast_to_int.in_port(0).connect(squeeze_dec_seq.out_port(0))
-
-        # preserve output name from original graph
-        rename_nodes([(sparse_to_dense, sparse_to_dense_name + '/AbandonedName'),
-                      (cast_to_int, sparse_to_dense_name)])
+        ctc_data_permute = create_op_with_const_inputs(graph, Transpose, {1: int64_array([1, 0, 2])},
+                                                            {'name': ctc_greedy_decoder.name + '/ctc_data_permute'})
+        ctc_greedy_decoder.in_port(0).get_source().connect(ctc_data_permute.in_port(0))
+        ctc_greedy_decoder.in_port(0).get_connection().set_source(ctc_data_permute.out_port(0))
 
         # set output of the new sub-graph as a source for SparseToDense consumer
-        sparse_to_dense.out_port(0).get_connection().set_source(cast_to_int.out_port(0))
+        sparse_to_dense.out_port(0).get_connection().set_source(ctc_greedy_decoder.out_port(0))
 
         # remove no longer needed nodes
         graph.remove_nodes_from([sparse_to_dense.id, cast.id])
-
-        # mark CTCGreedyDecoder node as a node that requires transformation of sequence length to a mask format
-        # in the middle phase
-        ctc_greedy_decoder['use_mask_format'] = True
 
         # unless the second input of CTCGreedyDecoder is a parameter, it enforces MO to use --static-shape
         # to try getting the second input with a value
