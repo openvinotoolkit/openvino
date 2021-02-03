54546596a51655bbe36c2167f3bd5fca8506356f
bsledz <bartosz.sledz@intel.com>
2021-02-03 07:34:02 +0100

    Add support for multiple outputs in ed_roi_feature_extractor

--- ngraph/frontend/onnx_import/src/op/org.openvinotoolkit/experimental_detectron/roi_feature_extractor.hpp
+++ ngraph/frontend/onnx_import/src/op/org.openvinotoolkit/experimental_detectron/roi_feature_extractor.hpp
@@ -40,7 +40,8 @@ namespace ngraph
                     attrs.sampling_ratio = node.get_attribute_value<std::int64_t>("sampling_ratio", 2);
                     attrs.aligned = static_cast<bool>(node.get_attribute_value<std::int64_t>("aligned", 0));
                     attrs.pyramid_scales = node.get_attribute_value<std::vector<std::int64_t>>("pyramid_scales", {4, 8, 16, 32, 64});
-                    return {std::make_shared<ROIFeatureExtractor>(inputs, attrs)};
+                    auto roi_feature_extractor = std::make_shared<ROIFeatureExtractor>(inputs, attrs);
+                    return {roi_feature_extractor->output(0), roi_feature_extractor->output(1)};
                 }
 
             } // namespace set_1
