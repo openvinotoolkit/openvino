ec203486a121cb330d8c62ea5d6836d2653da519
Anton Pankratv <anton.pankratov@intel.com>
2021-02-04 11:19:44 +0300

    Implemented base cancelation implementation (#3805)

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
@@ -59,10 +59,8 @@ std::vector<std::string> disabledTestPatterns() {
         R"(.*Broadcast.*mode=BIDIRECTIONAL.*inNPrec=BOOL.*)",
         // TODO: Issue 43417 sporadic issue, looks like an issue in test, reproducible only on Windows platform
         R"(.*decomposition1_batch=5_hidden_size=10_input_size=30_.*tanh.relu.*_clip=0_linear_before_reset=1.*_targetDevice=CPU_.*)",
-        // TODO: Sporadic Issue: 45163
-        R"(.*Behavior.*CancellationTests.*canResetAfterCancelAsyncRequest.*)",
         // TODO: Issue 47556. [NGraph] CTCGreedyDecoderSeqLen. Invalid type transformation i64 to i32.
-        R"(.*(CTCGreedyDecoderSeqLenLayerTest).*(idxPRC=I64).*)"
+        R"(.*(CTCGreedyDecoderSeqLenLayerTest).*(idxPRC=I64).*)",
     };
 
     if (!InferenceEngine::with_cpu_x86_avx512_core()) {
