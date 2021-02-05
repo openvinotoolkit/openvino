7cad246bf7158ae857e1fc36249eebcb35cd964a
Somsikov, Andrey <andrey.somsikov@intel.com>
2021-02-05 13:15:55 +0300

    Fix cond comp artifacts path

--- tests/conditional_compilation/test_collect.py
+++ tests/conditional_compilation/test_collect.py
@@ -7,22 +7,23 @@
 
 import glob
 import os
+import sys
 
 from proc_utils import cmd_exec  # pylint: disable=import-error
 
 
-def test_cc_collect(model, sea_runtool, benchmark_app, collector_dir, artifacts):
+def test_cc_collect(test_id, model, sea_runtool, benchmark_app, collector_dir, artifacts):
     """ Test conditional compilation statistics collection
     """
-    out = artifacts / model.parent / model.stem
+    out = artifacts / test_id
     # cleanup old data if any
     prev_results = glob.glob(f"{out}.pid*.csv")
     for path in prev_results:
         os.remove(path)
     # run use case
-    returncode, _ = cmd_exec(
+    returncode, output = cmd_exec(
         [
-            "python",
+            sys.executable,
             str(sea_runtool),
             f"-o={out}",
             "-f=stat",
@@ -35,7 +36,7 @@ def test_cc_collect(model, sea_runtool, benchmark_app, collector_dir, artifacts)
             "-nireq=1",
         ]
     )
-    assert returncode == 0, f"Command exited with non-zero status {returncode}"
+    assert returncode == 0, f"Command exited with non-zero status {returncode}:\n {output}"
     assert (
         len(glob.glob(f"{out}.pid*.csv")) == 1
     ), f'Multiple or none "{out}.pid*.csv" files'
