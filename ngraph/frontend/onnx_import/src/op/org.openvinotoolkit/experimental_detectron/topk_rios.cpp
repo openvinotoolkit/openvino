e576941bad133570dc2042ccedd78811399af790
bsledz <bartosz.sledz@intel.com>
2021-02-08 13:24:15 +0100

    Fix default value for topk_rios

--- ngraph/frontend/onnx_import/src/op/org.openvinotoolkit/experimental_detectron/topk_rios.cpp
+++ ngraph/frontend/onnx_import/src/op/org.openvinotoolkit/experimental_detectron/topk_rios.cpp
@@ -34,7 +34,7 @@ namespace ngraph
                     auto input_rois = inputs[0];
                     auto rois_probs = inputs[1];
                     auto max_rois = static_cast<std::size_t>(
-                        node.get_attribute_value<std::int64_t>("max_rois", 0));
+                        node.get_attribute_value<std::int64_t>("max_rois", 1000));
 
                     return {std::make_shared<TopKROIs>(input_rois, rois_probs, max_rois)};
                 }
