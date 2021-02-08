20f14584d0567430d3e7b83975eed4aa8cf1f6c0
iimironov <iliya.mironov@intel.com>
2021-02-08 11:18:44 +0300

    Remove comments

--- inference-engine/src/transformations/src/transformations/op_conversions/simplify_ctc_greedy_decoder.cpp
+++ inference-engine/src/transformations/src/transformations/op_conversions/simplify_ctc_greedy_decoder.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2018-2021 Intel Corporation
+// Copyright (C) 2021 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
 
@@ -18,7 +18,6 @@ ngraph::pass::SimplifyCTCGreedyDecoder::SimplifyCTCGreedyDecoder() {
     auto decoder = pattern::wrap_type<opset6::CTCGreedyDecoderSeqLen>();
 
     ngraph::matcher_pass_callback callback = [=](ngraph::pattern::Matcher& m) {
-
         auto decoder_v6 = std::dynamic_pointer_cast<opset6::CTCGreedyDecoderSeqLen> (m.get_match_root());
         if (!decoder_v6) {
             return false;
@@ -86,58 +85,9 @@ ngraph::pass::SimplifyCTCGreedyDecoder::SimplifyCTCGreedyDecoder() {
                                                squeeze2_output_f, squeeze1_output_f, transpose_upper_bounds, bool_seq_mask, seq_mask, transpose_seq_mask,
                                                    output_i, where_equal_minus1, output_seq_mask, output_seq_len});
 
-        decoder_v6->output(0).replace(output_i->output(0));
-        decoder_v6->output(1).replace(output_seq_len->output(0));
-
-        /*auto data_shape = std::make_shared<ngraph::opset6::ShapeOf>(decoder_v6->input_value(0));
-        auto constT_0 = ngraph::opset6::Constant::create(element::i32, Shape{}, {-1});//TODO set axis 0
-        auto constT_1 = ngraph::opset6::Constant::create(element::i32, Shape{}, {1});
-        auto T = std::make_shared<ngraph::opset6::Gather>(data_shape, constT_1, constT_0);
-
-        auto constN_0 = ngraph::opset6::Constant::create(element::i32, Shape{}, {0});
-        auto constN_1 = ngraph::opset6::Constant::create(element::i32, Shape{}, {-1});
-        auto N = std::make_shared<ngraph::opset6::Gather>(data_shape, constN_0, constN_0);
-
-        auto start = opset6::Constant::create(seq_len_type, Shape{}, {1});
-        auto step = opset6::Constant::create(seq_len_type, Shape{}, {1});
-        auto plus1 = opset6::Constant::create(element::i64, Shape{}, {1});
-        auto Tplus = std::make_shared<ngraph::opset6::Add>(T, plus1);
-        auto range1T = std::make_shared<ngraph::opset6::Range>(start, Tplus, step, seq_len_type);
-
-        auto constUnsqueeze1 = ngraph::opset6::Constant::create(element::i32, Shape{1}, {0});
-        auto tT = std::make_shared<ngraph::opset6::Unsqueeze>(T, constUnsqueeze1);
-        auto constUnsqueeze2 = ngraph::opset6::Constant::create(element::i32, Shape{1}, {0});
-        auto tN = std::make_shared<ngraph::opset6::Unsqueeze>(N, constUnsqueeze2);
-        auto mask_shape = std::make_shared<ngraph::opset6::Concat>(
-                OutputVector{tT->output(0), tN->output(0)}, 0);
-        auto upper_bounds = std::make_shared<ngraph::opset6::Broadcast>(
-                decoder_v6->input_value(1), mask_shape->output(0));
-
-        auto transpose_upper_bounds = std::make_shared<ngraph::opset6::Transpose>(upper_bounds->output(0),
-                                                                     ngraph::opset6::Constant::create(element::i32,
-                                                                                                      Shape({2}), {1, 0}));
-
-        auto bool_seq_mask = std::make_shared<ngraph::opset6::GreaterEqual>(transpose_upper_bounds->output(0),
-                                                                            range1T->output(0));
-        std::cout << upper_bounds->output(0).get_partial_shape() <<std::endl;
-        std::cout << range1T->output(0).get_partial_shape() << std::endl;
-        std::cout << bool_seq_mask->output(0).get_partial_shape() << std::endl;
-        auto const_0f = ngraph::opset6::Constant::create(element::i32, Shape{}, {0});
-        auto const_1f = ngraph::opset6::Constant::create(element::i32, Shape{}, {1});
-        auto seq_mask = std::make_shared<ngraph::opset6::Select>(bool_seq_mask, const_1f, const_0f);
-        std::cout << seq_mask->output(0).get_partial_shape() << std::endl;
-
-        auto transpose_seq_mask = std::make_shared<ngraph::opset6::Transpose>(seq_mask->output(0),
-                                                                                  ngraph::opset6::Constant::create(element::i32,
-                                                                                                                   Shape({2}), {1, 0}));
-
-        auto simplified_decoder = std::make_shared<ngraph::opset6::CTCGreedyDecoder>(transpose,
-                                                                                     transpose_seq_mask->output(0),
-                                                                             decoder_v6->get_merge_repeated());
-        simplified_decoder->set_friendly_name(decoder_v6->get_friendly_name());
-        ngraph::copy_runtime_info(decoder_v6, {transpose, simplified_decoder, data_shape, T, N, Tplus, range1T, tT, tN, mask_shape,
-                                               transpose_seq_mask, transpose_upper_bounds, upper_bounds, bool_seq_mask, seq_mask});
-        decoder_v6->output(0).replace(simplified_decoder->output(0));*/
+        output_i->set_friendly_name(decoder_v6->get_friendly_name()+".0");
+        output_seq_len->set_friendly_name(decoder_v6->get_friendly_name()+".1");
+        ngraph::replace_node(decoder_v6, {output_i->output(0), output_seq_len->output(0)});
 
         return true;
     };
