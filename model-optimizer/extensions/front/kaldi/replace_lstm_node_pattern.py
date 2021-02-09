test fixes, added batch broadcasting to created constants

model-optimizer/extensions/front/kaldi/replace_lstm_node_pattern.py
@@ -1,5 +1,5 @@
 """
- Copyright (C) 2018-2020 Intel Corporation
+ Copyright (C) 2018-2021 Intel Corporation
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -57,28 +57,69 @@ def create_const_with_batch_from_input(input_out_port: Port, second_dim, value=0
     # create const with batch taken from Parameter
     graph = input_out_port.node.graph
     input_name = input_out_port.node.name
-    shape_of_input = Shape(graph, {'name': 'shape/' + input_name}).create_node()
-    shape_of_input.in_port(0).connect(input_out_port)
-    dim_for_get_batch = Const(graph, {'name': 'dim/crop_batch/'+shape_of_input.name,
-                                      'value': int64_array([1]), 'shape': int64_array([1])}).create_node()
-    get_batch = Crop(graph, {'name': 'crop_batch/' + shape_of_input.name,
-                             'axis': int64_array([0]), 'offset': int64_array([0])
-                             }).create_node()
-    get_batch.in_port(0).connect(shape_of_input.out_port(0))
-    get_batch.in_port(1).connect(dim_for_get_batch.out_port(0))
-    mem_shape_2nd_dim = Const(graph, {'name': 'gifo_r_weights_shape/'+input_name,
-                                      'value': int64_array([second_dim]),
-                                      'shape': int64_array([1])}).create_node()
-    mem_shape = Concat(graph, {'name': 'gather_memory_shape/' + input_name,
-                               'axis': 0, 'in_ports_count': 2}).create_node()
-    mem_shape.in_port(0).connect(get_batch.out_port(0))
-    mem_shape.in_port(1).connect(mem_shape_2nd_dim.out_port(0))
-    fill_value = Const(graph, {'name': 'fill_value/'+input_name,
-                               'value': np.array([value], dtype=precision), 'shape': int64_array([1])}).create_node()
-    init_value_prev_lstm_output = Broadcast(graph, {'name': 'init_value/'+input_name,
-                                                    }).create_node()
-    init_value_prev_lstm_output.in_port(0).connect(fill_value.out_port(0))
-    init_value_prev_lstm_output.in_port(1).connect(mem_shape.out_port(0))
+
+    shape_of_input = {}
+    if not input_out_port.disconnected():
+        for dest in input_out_port.get_destinations():
+            if dest.node.op == "ShapeOf":
+                shape_of_input = dest.node
+                break
+
+    if shape_of_input == {}:
+        shape_of_input = Shape(graph, {'name': 'shape/' + input_name}).create_node()
+        shape_of_input.in_port(0).connect(input_out_port)
+
+    get_batch = {}
+    if not shape_of_input.out_port(0).disconnected():
+        for dest in shape_of_input.out_port(0).get_destinations():
+            if dest.node.op == "Crop" and \
+                    dest.node.in_port(1).get_source().node.soft_get('value', []) == int64_array([1]):
+                get_batch = dest.node
+                break
+
+    if get_batch == {}:
+        dim_for_get_batch = Const(graph, {'name': 'dim/crop_batch/'+shape_of_input.name,
+                                          'value': int64_array([1]), 'shape': int64_array([1])}).create_node()
+        get_batch = Crop(graph, {'name': 'crop_batch/' + shape_of_input.name,
+                                 'axis': int64_array([0]), 'offset': int64_array([0])
+                                 }).create_node()
+        get_batch.in_port(0).connect(shape_of_input.out_port(0))
+        get_batch.in_port(1).connect(dim_for_get_batch.out_port(0))
+
+    mem_shape = {}
+    if not get_batch.out_port(0).disconnected():
+        for dest in get_batch.out_port(0).get_destinations():
+            if dest.node.op == "Concat" and \
+                    dest.node.in_port(1).get_source().node.soft_get('value', []) == int64_array([second_dim]):
+                mem_shape = dest.node
+                break
+
+    if mem_shape == {}:
+        mem_shape_2nd_dim = Const(graph, {'name': 'gifo_r_weights_shape/'+input_name,
+                                          'value': int64_array([second_dim]),
+                                          'shape': int64_array([1])}).create_node()
+        mem_shape = Concat(graph, {'name': 'gather_memory_shape/' + input_name,
+                                   'axis': 0, 'in_ports_count': 2}).create_node()
+        mem_shape.in_port(0).connect(get_batch.out_port(0))
+        mem_shape.in_port(1).connect(mem_shape_2nd_dim.out_port(0))
+
+    init_value_prev_lstm_output = {}
+    if not mem_shape.out_port(0).disconnected():
+        for dest in mem_shape.out_port(0).get_destinations():
+            if dest.node.op == "Broadcast" and \
+                    dest.node.in_port(1).get_source().node.soft_get('value', []) == np.array([value], dtype=precision):
+                init_value_prev_lstm_output = dest.node
+                break
+
+    if init_value_prev_lstm_output == {}:
+        fill_value = Const(graph, {'name': 'fill_value/'+input_name,
+                                   'value': np.array([value], dtype=precision),
+                                   'shape': int64_array([1])}).create_node()
+        init_value_prev_lstm_output = Broadcast(graph, {'name': 'init_value/'+input_name,
+                                                        }).create_node()
+        init_value_prev_lstm_output.in_port(0).connect(fill_value.out_port(0))
+        init_value_prev_lstm_output.in_port(1).connect(mem_shape.out_port(0))
+
     return init_value_prev_lstm_output
 
 

model-optimizer/extensions/middle/InsertSelect.py
@@ -1,5 +1,5 @@
 """
- Copyright (C) 2018-2020 Intel Corporation
+ Copyright (C) 2018-2021 Intel Corporation
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -24,7 +24,6 @@ from mo.middle.pattern_match import find_pattern_matches, inverse_dict
 from mo.middle.replacement import MiddleReplacementPattern
 from mo.ops.assign import Assign
 from mo.ops.concat import Concat
-from mo.ops.const import Const
 from mo.ops.crop import Crop
 from mo.ops.read_value import ReadValue
 from mo.ops.result import Result
@@ -85,13 +84,14 @@ class AddSelectBeforeMemoryNodePattern(MiddleReplacementPattern):
         if context_len == 1:
             return
 
+        batch_port = graph.get_op_nodes(op="Parameter")[0].out_port(0)
         in_node_port = node.in_port(0).get_source()
         in_node_shape = node.in_port(0).data.get_shape()
         node.in_port(0).disconnect()
 
         # add Select before saving state to avoid saving garbage
         select_node = Select(graph, {'name': 'select_' + node.name}).create_node()
-        zero_else = Const(graph, {'name': 'zero_else', 'value': np.zeros(in_node_shape)}).create_node()
+        zero_else = create_const_with_batch_from_input(batch_port, in_node_shape[1])
         select_node.in_port(1).connect(in_node_port)
         select_node.in_port(2).connect(zero_else.out_port(0))
 
@@ -126,15 +126,13 @@ class AddSelectBeforeMemoryNodePattern(MiddleReplacementPattern):
             ones = Node(graph, inverse_dict(counter_match)['const_1'])
             input_port = Node(graph, inverse_dict(counter_match)['crop_out']).out_port(0)
         else:
-            init_value_mem_out = create_const_with_batch_from_input(in_node_port, context_len, precision=np.int32)
+            init_value_mem_out = create_const_with_batch_from_input(batch_port, context_len, precision=np.int32)
             mem_out = ReadValue(graph, {'name': 'iteration_number',
                                         'variable_id': 'iteration_'+node.name}).create_node()
             mem_out.in_port(0).connect(init_value_mem_out.out_port(0))
             cut_first = Crop(graph, {'name': 'cut_first', 'axis': int64_array([1]),
                                      'offset': int64_array([1]), 'dim': int64_array([context_len-1])}).create_node()
             cut_first.in_port(0).connect(mem_out.out_port(0))
-            # batch added to Kaldi by MO and is not changing in graph
-            batch_port = node.graph.get_op_nodes(op="Parameter")[0].out_port(0)
             ones = create_const_with_batch_from_input(batch_port, 1, value=1, precision=np.int32)
             concat = Concat(graph, {'name': 'concat_ones', 'in_ports_count': 2, 'axis': 1}).create_node()
             concat.in_port(0).connect(cut_first.out_port(0))