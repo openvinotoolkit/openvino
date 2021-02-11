a33e2b697beb258476008f8833b9a856c06698e7
iimironov <iliya.mironov@intel.com>
2021-02-11 14:08:57 +0300

    fix tests

--- model-optimizer/extensions/ops/ctc_greedy_decoder_test.py
+++ model-optimizer/extensions/ops/ctc_greedy_decoder_test.py
@@ -31,6 +31,7 @@ nodes_attributes = {'logits': {'kind': 'op'},
                     'ctcgreedydecoder_node': {'op': 'CTCGreedyDecoderSeqLen', 'kind': 'op',
                                               'ctc_merge_repeated': True},
                     'output1': {'shape': None, 'value': None, 'kind': 'data'},
+                    'last_output1': {'shape': None, 'value': None, 'kind': 'op'},
                     'output2': {'shape': None, 'value': None, 'kind': 'data'}
                     }
 
@@ -40,7 +41,8 @@ edges1 = [('logits', 'logits_data'),
           ('logits_data', 'ctcgreedydecoder_node', {'in': 0}),
           ('seq_mask_data', 'ctcgreedydecoder_node', {'in': 1}),
           ('ctcgreedydecoder_node', 'output1', {'out': 0}),
-          ('ctcgreedydecoder_node', 'output2', {'out': 1})]
+          ('ctcgreedydecoder_node', 'output2', {'out': 1}),
+          ('output1', 'last_output1', {'out': 0}),]
 
 # valid test case
 inputs1 = {'logits_data': {'shape': int64_array([4, 100, 5])},
@@ -70,16 +72,12 @@ class TestCTCGreedyDecoder(unittest.TestCase):
 
         # prepare reference results
         ref_output1_shape = int64_array([4, 100])
-        ref_output2_shape = int64_array([4])
 
         # get the result
         res_output1_shape = graph.node['output1']['shape']
-        res_output2_shape = graph.node['output2']['shape']
 
         self.assertTrue(np.array_equal(ref_output1_shape, res_output1_shape),
                         'shapes do not match expected: {} and given: {}'.format(ref_output1_shape, res_output1_shape))
-        self.assertTrue(np.array_equal(ref_output2_shape, res_output2_shape),
-                        'shapes do not match expected: {} and given: {}'.format(ref_output2_shape, res_output2_shape))
 
     def test_infer_invalid1(self):
         graph = build_graph(nodes_attributes, edges1, inputs1_inv)
