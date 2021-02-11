de0d5a0d4269e10f769071f0cc486f83f224706d
Bakalin, Andrew <andrew.bakalin@intel.com>
2021-02-11 12:27:28 +0300

    [VPU] Get rid of evaluateTargetShape

--- inference-engine/src/vpu/common/include/vpu/ngraph/utilities.hpp
+++ inference-engine/src/vpu/common/include/vpu/ngraph/utilities.hpp
@@ -14,8 +14,6 @@
 
 namespace vpu {
 
-std::vector<std::int64_t> evaluateTargetShape(const ngraph::Output<ngraph::Node>& value);
-
 std::shared_ptr<ngraph::Node> shapeToConstant(const ngraph::element::Type& type, const ngraph::Shape& shape);
 
 std::shared_ptr<ngraph::Node> gatherShapeElements(const ngraph::Output<ngraph::Node>&, int startIndex, size_t elemCount);
