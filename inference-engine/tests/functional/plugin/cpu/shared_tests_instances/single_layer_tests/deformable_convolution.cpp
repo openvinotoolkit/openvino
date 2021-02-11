f5455065f6ce0ba06095f9a909d46b8fd21448cd
pszmel <piotr.szmelczynski@intel.com>
2021-02-11 13:51:53 +0100

    Create additional parameters set for cpu plugin SLT

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/single_layer_tests/deformable_convolution.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/single_layer_tests/deformable_convolution.cpp
@@ -24,6 +24,9 @@ const std::vector<std::vector<size_t>> dilations = {{1, 1}};
 const std::vector<size_t> groups = {1};
 const std::vector<size_t> defor_groups = {1};
 const std::vector<size_t> numOutChannels = {1, 5};
+const std::vector<size_t> multiple_defor_groups = {4};
+const std::vector<std::vector<size_t>> deform_vals = {{1, 200, 220, 220}};
+const std::vector<std::vector<size_t>> kernel = {{64, 4, 5, 5}};
 
 const auto deformableConv2DParams_ExplicitPadding = ::testing::Combine(
     ::testing::ValuesIn(deformable_vals),
@@ -40,6 +43,15 @@ const auto deformableConv2DParams_AutoPadValid = ::testing::Combine(
     ::testing::ValuesIn(dilations), ::testing::ValuesIn(groups),
     ::testing::ValuesIn(defor_groups), ::testing::ValuesIn(numOutChannels),
     ::testing::Values(ngraph::op::PadType::VALID));
+
+const auto deformableConv2DParams_DeformableGroups_AutoPadExplicit = ::testing::Combine(
+    ::testing::ValuesIn(deform_vals),
+    ::testing::ValuesIn(kernel), ::testing::ValuesIn(strides),
+    ::testing::Values(std::vector<ptrdiff_t>({0, 0})),
+    ::testing::Values(std::vector<ptrdiff_t>({0, 0})),
+    ::testing::ValuesIn(dilations), ::testing::ValuesIn(groups),
+    ::testing::ValuesIn(multiple_defor_groups), ::testing::ValuesIn(numOutChannels),
+    ::testing::Values(ngraph::op::PadType::EXPLICIT));
 
 INSTANTIATE_TEST_CASE_P(
     smoke_DeformableConvolution2D_ExplicitPadding, DeformableConvolutionLayerTest,
@@ -62,6 +74,18 @@ INSTANTIATE_TEST_CASE_P(
         ::testing::Values(InferenceEngine::Layout::ANY),
         ::testing::Values(InferenceEngine::Layout::ANY),
         ::testing::Values(std::vector<size_t>({1, 1, 30, 30})),
+        ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+    DeformableConvolutionLayerTest::getTestCaseName);
+
+INSTANTIATE_TEST_CASE_P(
+    smoke_DeformableConvolution2D_DeformableGroups_ExplicitPadding, DeformableConvolutionLayerTest,
+    ::testing::Combine(
+        deformableConv2DParams_DeformableGroups_AutoPadExplicit, ::testing::ValuesIn(netPrecisions),
+        ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
+        ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
+        ::testing::Values(InferenceEngine::Layout::ANY),
+        ::testing::Values(InferenceEngine::Layout::ANY),
+        ::testing::Values(std::vector<size_t>({1, 4, 224, 224})),
         ::testing::Values(CommonTestUtils::DEVICE_CPU)),
     DeformableConvolutionLayerTest::getTestCaseName);
 }  // namespace
