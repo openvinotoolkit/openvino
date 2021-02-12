d2579f2fda5b70af7a19d6d214352aaabbc357a5
Maksim Kutakov <maksim.kutakov@intel.com>
2021-02-12 17:44:57 +0300

    [CPU] MKLDNN NCHW pooling primitive performance fix. (#4235)

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
@@ -59,6 +59,8 @@ std::vector<std::string> disabledTestPatterns() {
         R"(.*Broadcast.*mode=BIDIRECTIONAL.*inNPrec=BOOL.*)",
         // TODO: Issue 43417 sporadic issue, looks like an issue in test, reproducible only on Windows platform
         R"(.*decomposition1_batch=5_hidden_size=10_input_size=30_.*tanh.relu.*_clip=0_linear_before_reset=1.*_targetDevice=CPU_.*)",
+        // Skip platforms that do not support BF16 (i.e. sse, avx, avx2)
+        R"(.*BF16.*(jit_avx(?!5)|jit_sse).*)",
     };
 
     if (!InferenceEngine::with_cpu_x86_avx512_core()) {
