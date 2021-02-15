8837388a8d8183e2845f3f83662223244e05cfee
tomdol <tomasz.dolbniak@intel.com>
2021-02-15 10:04:38 -0500

    Compiler error fix

--- ngraph/test/util/engine/shared_utils.cpp
+++ ngraph/test/util/engine/shared_utils.cpp
@@ -14,6 +14,7 @@
 // limitations under the License.
 //*****************************************************************************
 
+#include <cstdlib>
 #include <sstream>
 
 #include "shared_utils.hpp"
