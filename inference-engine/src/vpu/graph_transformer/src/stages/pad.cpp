dd796465ebc0eb7700f6f1160dff171384d43f50
rvyunov <roman.vyunov@intel.com>
2021-02-15 19:14:01 +0300

    [IE][VPU] Pad layer: enabling of 3D support

--- inference-engine/src/vpu/graph_transformer/src/stages/pad.cpp
+++ inference-engine/src/vpu/graph_transformer/src/stages/pad.cpp
@@ -93,20 +93,58 @@ void FrontEnd::parsePad(const Model& model, const ie::CNNLayerPtr& _layer, const
     auto layer = std::dynamic_pointer_cast<ie::PadLayer>(_layer);
     IE_ASSERT(layer != nullptr);
 
-    IE_ASSERT(layer->pads_begin.size() == 4);
-    IE_ASSERT(layer->pads_end.size() == 4);
+    const auto ndims = inputs[0]->desc().dimsOrder().numDims();
+    VPU_THROW_UNLESS(ndims == 3 || ndims == 4, "Layer %s support only 3D and 4D input", layer->name);
+
+    IE_ASSERT(layer->pads_begin.size() <= 4);
+    IE_ASSERT(layer->pads_end.size() <= 4);
+
+    unsigned int pads_beginN = 0;
+    unsigned int pads_beginC = 0;
+    unsigned int pads_beginH = 0;
+    unsigned int pads_beginW = 0;
+
+    unsigned int pads_endN = 0;
+    unsigned int pads_endC = 0;
+    unsigned int pads_endH = 0;
+    unsigned int pads_endW = 0;
+
+    switch (ndims) {
+        case 4:
+            pads_beginN = layer->pads_begin[0];
+            pads_beginC = layer->pads_begin[1];
+            pads_beginH = layer->pads_begin[2];
+            pads_beginW = layer->pads_begin[3];
+
+            pads_endN = layer->pads_end[0];
+            pads_endC = layer->pads_end[1];
+            pads_endH = layer->pads_end[2];
+            pads_endW = layer->pads_end[3];
+            break;
+        case 3:
+            pads_beginN = 0;
+            pads_beginC = layer->pads_begin[0];
+            pads_beginH = layer->pads_begin[1];
+            pads_beginW = layer->pads_begin[2];
+
+            pads_endN = 0;
+            pads_endC = layer->pads_end[0];
+            pads_endH = layer->pads_end[1];
+            pads_endW = layer->pads_end[2];
+            break;
+    }
 
     DimValues pads_begin;
-    pads_begin.set(Dim::W, layer->pads_begin[3]);
-    pads_begin.set(Dim::H, layer->pads_begin[2]);
-    pads_begin.set(Dim::C, layer->pads_begin[1]);
-    pads_begin.set(Dim::N, layer->pads_begin[0]);
+    pads_begin.set(Dim::W, pads_beginW);
+    pads_begin.set(Dim::H, pads_beginH);
+    pads_begin.set(Dim::C, pads_beginC);
+    pads_begin.set(Dim::N, pads_beginN);
 
     DimValues pads_end;
-    pads_end.set(Dim::W, layer->pads_end[3]);
-    pads_end.set(Dim::H, layer->pads_end[2]);
-    pads_end.set(Dim::C, layer->pads_end[1]);
-    pads_end.set(Dim::N, layer->pads_end[0]);
+    pads_end.set(Dim::W, pads_endW);
+    pads_end.set(Dim::H, pads_endH);
+    pads_end.set(Dim::C, pads_endC);
+    pads_end.set(Dim::N, pads_endN);
 
     _stageBuilder->addPadStage(
         model,
