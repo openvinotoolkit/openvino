a4376d4387ead1b1ccba5ea37018200a0905e252
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2021-02-15 05:35:03 +0100

    [ONNX] Remove linking libonnx from unit-test (#4298)
    * Remove linking libonnx from unit-test
    
    * Consider all flavors of protobuf libraries
    
    * Avoid passing on NOTFOUND properties
    
    * Set system deps for mac
    
    * Revert include dirs set up

--- ngraph/test/onnx/onnx_import_library.cpp
+++ ngraph/test/onnx/onnx_import_library.cpp
@@ -15,8 +15,6 @@
 //*****************************************************************************
 
 #include <onnx/onnx_pb.h>
-#include "onnx/defs/function.h"
-#include "onnx/defs/schema.h"
 
 #include "gtest/gtest.h"
 #include "util/test_control.hpp"
@@ -24,16 +22,6 @@
 using namespace ngraph;
 
 static std::string s_manifest = "${MANIFEST}";
-
-NGRAPH_TEST(onnx, get_function_op_with_version)
-{
-    const auto* schema =
-        ONNX_NAMESPACE::OpSchemaRegistry::Schema("MeanVarianceNormalization", 9, "");
-    EXPECT_TRUE(schema);
-    EXPECT_TRUE(schema->HasFunction());
-    auto func = schema->GetFunction();
-    EXPECT_EQ(func->name(), "MeanVarianceNormalization");
-}
 
 NGRAPH_TEST(onnx, check_ir_version_support)
 {
