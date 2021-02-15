2a3744b0e5506b36c0e8eee7a248614778c67fab
tomdol <tomasz.dolbniak@intel.com>
2021-02-15 05:53:13 -0500

    Docs update

--- ngraph/frontend/onnx_import/include/onnx_import/editor/editor.hpp
+++ ngraph/frontend/onnx_import/include/onnx_import/editor/editor.hpp
@@ -90,7 +90,7 @@ namespace ngraph
             /// \return A reference to ONNX ModelProto object containing the in-memory model
             ONNX_NAMESPACE::ModelProto& model() const;
 
-            /// \brief Returns a serialized ONNX model, possiblie modified by an instance of editor.
+            /// \brief Returns a serialized ONNX model, possibly modified by the editor.
             std::string model_string() const;
 
             /// \brief Returns a list of all inputs of the in-memory model, including initializers.
