496e622799095dc678e493d4cad16f45b1856b18
Kazantaev <gleb.kazantaev@intel.com>
2021-02-17 15:22:45 +0300

    Move back some unnecessary changes

--- model-optimizer/install_prerequisites/install_prerequisites.bat
+++ model-optimizer/install_prerequisites/install_prerequisites.bat
@@ -159,8 +159,7 @@ goto ie_search_end
 :ie_search_end
 
 echo *****************************************************************************************
-echo Warning: please expect that Model Optimizer conversion might be slow.
-echo You can boost conversion speed by installing protobuf-*.egg located in the
+echo Optional: To speed up model conversion process, install protobuf-*.egg located in the
 echo "model-optimizer\install_prerequisites" folder or building protobuf library from sources.
 echo For more information please refer to Model Optimizer FAQ, question #80.
 
