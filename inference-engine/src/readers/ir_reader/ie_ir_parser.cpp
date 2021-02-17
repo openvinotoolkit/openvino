0b494a7f2f58fd46793f29c0ec6407a53eb05584
Evgeny Lazarev <evgeny.lazarev@intel.com>
2021-02-17 12:19:44 +0300

    Changed check for dimension size to be non-negative (#4328)
    * Changed check for dimension size to be non-negative
    
    * Added unit test for reading model with ShapeOf from scalar and fixed IE IR parser
    
    * Added comment to the test
    
    * Fixed ShapeOfFromScalar test
    
    * Fixed incorrect merge
    
    * Added test for a negative value of "dim" in IR

--- inference-engine/src/readers/ir_reader/ie_ir_parser.cpp
+++ inference-engine/src/readers/ir_reader/ie_ir_parser.cpp
@@ -691,12 +691,12 @@ V10Parser::V10Parser::GenericLayerParams XmlDeserializer::parseGenericParams(
         port.portId = GetIntAttr(parentNode, "id");
 
         FOREACH_CHILD(node, parentNode, "dim") {
-            size_t dim = 0;
+            int64_t dim = 0;
             const pugi::char_t* dimVal = node.child_value();
             std::stringstream ss(dimVal);
-            if (!(ss >> dim) || dim == 0) {
+            if (!(ss >> dim) || dim < 0) {
                 THROW_IE_EXCEPTION << "dimension (" << dimVal << ") in node " << node.name()
-                                   << " must be a positive integer: at offset "
+                                   << " must be a non-negative integer: at offset "
                                    << node.offset_debug();
             }
             port.dims.push_back(dim);
