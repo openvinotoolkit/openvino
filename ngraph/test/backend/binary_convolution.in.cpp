354bb798f868d9c6e339826b9804b182060853ec
ggalieroc <gabriele.galiero.casay@intel.com>
2021-02-17 14:38:54 +0100

    Refactor backend test cases to compare with regular convolution

--- ngraph/test/backend/binary_convolution.in.cpp
+++ ngraph/test/backend/binary_convolution.in.cpp
@@ -16,6 +16,10 @@
 
 #include "gtest/gtest.h"
 #include "ngraph/ngraph.hpp"
+#include "ngraph/runtime/tensor.hpp"
+#include "runtime/backend.hpp"
+#include "util/all_close.hpp"
+#include "util/all_close_f.hpp"
 #include "util/engine/test_engines.hpp"
 #include "util/test_case.hpp"
 #include "util/test_control.hpp"
@@ -24,13 +28,12 @@ using namespace std;
 using namespace ngraph;
 
 static string s_manifest = "${MANIFEST}";
-using TestEngine = test::ENGINE_CLASS_NAME(${BACKEND_NAME});
 
 template <typename T_IN, typename T_KERN>
 static void BinaryConvolutionTest(const std::vector<T_IN>& inputs,
                                   const Shape inputs_shape,
                                   const std::vector<T_KERN>& filters,
-                                  const Shape filter_shape,
+                                  const Shape filters_shape,
                                   const std::vector<T_IN>& outputs,
                                   const Shape outputs_shape,
                                   const Strides& strides,
@@ -43,7 +46,7 @@ static void BinaryConvolutionTest(const std::vector<T_IN>& inputs,
     float pad_value = 0;
 
     auto inputs_param = make_shared<op::Parameter>(element::from<T_IN>(), inputs_shape);
-    auto filters_const = make_shared<op::Constant>(element::u1, filter_shape, &filters[0]);
+    auto filters_const = make_shared<op::Constant>(element::u1, filters_shape, &filters[0]);
     auto bin_conv = make_shared<op::v1::BinaryConvolution>(
         inputs_param,
         filters_const,
@@ -56,12 +59,52 @@ static void BinaryConvolutionTest(const std::vector<T_IN>& inputs,
         auto_pad);
     auto f = make_shared<Function>(bin_conv, ParameterVector{inputs_param});
 
-    auto test_case = test::TestCase<TestEngine>(f);
-    test_case.add_input<T_IN>(inputs);
-    test_case.add_expected_output<T_IN>(outputs_shape, outputs);
-    test_case.run();
+    auto backend = runtime::Backend::create("${BACKEND_NAME}");
+
+    auto input_tensor = backend->create_tensor(element::from<T_IN>(), inputs_shape);
+    copy_data(input_tensor, inputs);
+    auto result = backend->create_tensor(element::from<T_IN>(), outputs_shape);
+
+    auto handle = backend->compile(f);
+    handle->call_with_validate({result}, {input_tensor});
+    EXPECT_TRUE(test::all_close_f((outputs), read_vector<T_IN>(result), MIN_FLOAT_TOLERANCE_BITS));
 }
 
+template <typename T_IN>
+static void ConvolutionTest(const std::vector<T_IN>& inputs,
+                            const Shape inputs_shape,
+                            const std::vector<T_IN>& filters,
+                            const Shape filters_shape,
+                            const std::vector<T_IN>& outputs,
+                            const Shape outputs_shape,
+                            const Strides& strides,
+                            const CoordinateDiff& padding,
+                            const Strides& dilations)
+{
+    const CoordinateDiff pads_begin{padding};
+    const CoordinateDiff pads_end{padding};
+    const op::PadType auto_pad{op::PadType::EXPLICIT};
+
+    auto inputs_param = make_shared<op::Parameter>(element::from<T_IN>(), inputs_shape);
+    auto filters_param = make_shared<op::Parameter>(element::from<T_IN>(), filters_shape);
+    auto conv = make_shared<op::v1::Convolution>(
+        inputs_param, filters_param, strides, pads_begin, pads_end, dilations, auto_pad);
+    auto f = make_shared<Function>(conv, ParameterVector{inputs_param, filters_param});
+
+    auto backend = runtime::Backend::create("${BACKEND_NAME}");
+
+    auto input_tensor = backend->create_tensor(element::from<T_IN>(), inputs_shape);
+    copy_data(input_tensor, inputs);
+    auto filters_tensor = backend->create_tensor(element::from<T_IN>(), filters_shape);
+    copy_data(filters_tensor, filters);
+    auto result = backend->create_tensor(element::from<T_IN>(), outputs_shape);
+
+    auto handle = backend->compile(f);
+    handle->call_with_validate({result}, {input_tensor, filters_tensor});
+    EXPECT_TRUE(test::all_close_f((outputs), read_vector<T_IN>(result), MIN_FLOAT_TOLERANCE_BITS));
+}
+
+// clang-format off
 // --------------------- 1D convolution ------------------------------------------
 NGRAPH_TEST(${BACKEND_NAME}, bin_convolution_1D_1batch_1channel_no_padding)
 {
@@ -70,27 +113,40 @@ NGRAPH_TEST(${BACKEND_NAME}, bin_convolution_1D_1batch_1channel_no_padding)
     const Strides dilations{1};
 
     const Shape inputs_shape{1, 1, 5};
-    const std::vector<float> inputs{1.0f, 0.0f, 0.0f, 1.0f, 0.0f};
+    const std::vector<float> inputs_conv{1.0f, -1.0f, -1.0f, 1.0f, -1.0f};
+    const std::vector<float> inputs_bin_conv{1.0f, 0.0f, 0.0f, 1.0f, 0.0f};
 
-    const Shape filter_shape{1, 1, 3};
-    const std::vector<uint8_t> filters{160}; // filters 1D: {1.0f, 0.0f, 1.0f}
+    const Shape filters_shape{1, 1, 3};
+    const std::vector<float> filters_conv{1.0f, -1.0f, 1.0f};
+    const std::vector<uint8_t> filters_bin_conv{160}; // filters 1D: {1.0f, 0.0f, 1.0f}
 
     const Shape outputs_shape{1, 1, 3};
     const std::vector<float> outputs{1.0f, 1.0f, -3.0f};
 
-    BinaryConvolutionTest(inputs,
-                          inputs_shape,
-                          filters,
-                          filter_shape,
-                          outputs,
-                          outputs_shape,
-                          strides,
-                          padding,
-                          dilations);
+    BinaryConvolutionTest(
+        inputs_bin_conv,
+        inputs_shape,
+        filters_bin_conv,
+        filters_shape,
+        outputs,
+        outputs_shape,
+        strides,
+        padding,
+        dilations);
+
+    ConvolutionTest(
+        inputs_conv,
+        inputs_shape,
+        filters_conv,
+        filters_shape,
+        outputs,
+        outputs_shape,
+        strides,
+        padding,
+        dilations);
 }
 
 // --------------------- 2D convolution ------------------------------------------
-// clang-format off
 NGRAPH_TEST(${BACKEND_NAME}, bin_convolution_2D_1batch_1channel_no_padding)
 {
     const Strides strides{1, 1};
@@ -98,29 +154,50 @@ NGRAPH_TEST(${BACKEND_NAME}, bin_convolution_2D_1batch_1channel_no_padding)
     const Strides dilations{1, 1};
 
     const Shape inputs_shape{1, 1, 4, 4};
-    const std::vector<float> inputs{1.0f, 0.0f, 0.0f, 1.0f,
-                                    1.0f, 1.0f, 0.0f, 0.0f,
-                                    0.0f, 0.0f, 0.0f, 1.0f,
-                                    1.0f, 0.0f, 1.0f, 1.0f};
+    const std::vector<float> inputs_conv{1.0f, -1.0f, -1.0f, 1.0f,
+                                         1.0f, 1.0f, -1.0f, -1.0f,
+                                         -1.0f, -1.0f, -1.0f, 1.0f,
+                                         1.0f, -1.0f, 1.0f, 1.0f};
 
-    const Shape filter_shape{1, 1, 3, 3};
-    //filters 2D: {1.0f, 0.0f, 1.0f,
-    //             0.0f, 1.0f, 0.0f,
-    //             1.0f, 0.0f, 1.0f};
-    const std::vector<uint8_t> filters{170, 128};
+    const std::vector<float> inputs_bin_conv{1.0f, 0.0f, 0.0f, 1.0f,
+                                             1.0f, 1.0f, 0.0f, 0.0f,
+                                             0.0f, 0.0f, 0.0f, 1.0f,
+                                             1.0f, 0.0f, 1.0f, 1.0f};
+
+    const Shape filters_shape{1, 1, 3, 3};
+    const std::vector<float> filters_conv{1.0f, -1.0f, 1.0f,
+                                          -1.0f, 1.0f, -1.0f,
+                                          1.0f, -1.0f, 1.0f};
+
+    //BinaryConvolution filters 2D:      {1.0f, 0.0f, 1.0f,
+    //                                    0.0f, 1.0f, 0.0f,
+    //                                    1.0f, 0.0f, 1.0f};
+    const std::vector<uint8_t> filters_bin_conv{170, 128};
 
     const Shape outputs_shape{1, 1, 2, 2};
     const std::vector<float> outputs{1.0f, 1.0f,
-                                     3.0f, -1.0f};
+                                    3.0f, -1.0f};
 
-    BinaryConvolutionTest(inputs,
-                          inputs_shape,
-                          filters,
-                          filter_shape,
-                          outputs,
-                          outputs_shape,
-                          strides,
-                          padding,
-                          dilations);
+    BinaryConvolutionTest(
+        inputs_bin_conv,
+        inputs_shape,
+        filters_bin_conv,
+        filters_shape,
+        outputs,
+        outputs_shape,
+        strides,
+        padding,
+        dilations);
+
+    ConvolutionTest(
+        inputs_conv,
+        inputs_shape,
+        filters_conv,
+        filters_shape,
+        outputs,
+        outputs_shape,
+        strides,
+        padding,
+        dilations);
 }
 // clang-format on
