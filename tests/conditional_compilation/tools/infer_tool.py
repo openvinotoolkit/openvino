ec35415e75d19aab10507d4f9d60d43ac2af7c6f
omartiny <olesya.martinyuk@intel.com>
2021-02-18 17:25:57 +0300

    Replaced tool, improved codestyle with pylint, added copyright header

--- tests/conditional_compilation/tools/infer_tool.py
+++ tests/conditional_compilation/tools/infer_tool.py
@@ -1,3 +1,11 @@
+# !/usr/bin/env python3
+# Copyright (C) 2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+# pylint:disable=invalid-name,no-name-in-module,logging-format-interpolation,redefined-outer-name
+
+""" Tool for running inference and storing results in npz files.
+"""
 import argparse
 import logging as log
 import sys
@@ -8,7 +16,7 @@ from openvino.inference_engine import IENetwork, IECore
 log.basicConfig(format="[ %(levelname)s ] %(message)s", level=log.INFO, stream=sys.stdout)
 
 
-def python_api_infer(ir_path, device):
+def infer(ir_path, device):
     """
      Function to perform IE inference using python API "in place"
     :param ir_path: Path to XML file of IR
@@ -34,7 +42,11 @@ def python_api_infer(ir_path, device):
 
 
 def cli_parser():
-    parser = argparse.ArgumentParser(description='Python_api reproducer')
+    """
+    Function for parsing arguments from command line.
+    :return: ir path, device and output folder path variables.
+    """
+    parser = argparse.ArgumentParser(description='Arguments for python API inference')
     parser.add_argument('-m', dest='ir_path', required=True, help='Path to XML file of IR')
     parser.add_argument('-d', dest='device', help='Target device to infer on', default="CPU")
     parser.add_argument('-r', dest='out_path', default=None,
@@ -49,7 +61,7 @@ def cli_parser():
 if __name__ == "__main__":
     ir_path, device, out_path = cli_parser()
     bin_path = os.path.splitext(ir_path)[0] + '.bin'
-    results = python_api_infer(ir_path=ir_path, device=device)
+    results = infer(ir_path=ir_path, device=device)
     np.savez(out_path, **results)
     log.info("Path for inference results: {}".format(out_path))
     log.info("Inference results:")
