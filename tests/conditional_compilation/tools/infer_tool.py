7ab6ee9a01f86c3ec2f65d8f8ea4b825e0504e40
Olesya Martinyuk <olesya.martinyuk@intel.com>
2021-02-20 12:19:18 +0300

    Infer tool code style patch (#4429)
    * Moved input preparation to another function, cleared argparse, minor code improvements
    
    * Minor changes in function description

--- tests/conditional_compilation/tools/infer_tool.py
+++ tests/conditional_compilation/tools/infer_tool.py
@@ -11,27 +11,37 @@ import logging as log
 import sys
 import os
 import numpy as np
-from openvino.inference_engine import IENetwork, IECore
+from openvino.inference_engine import IECore
 
 log.basicConfig(format="[ %(levelname)s ] %(message)s", level=log.INFO, stream=sys.stdout)
 
 
+def input_preparation(net):
+    """
+    Function to prepare reproducible from run to run input data
+    :param net: IENetwork object
+    :return: Dict where keys are layers' names and values are numpy arrays with layers' shapes
+    """
+
+    feed_dict = {}
+    for layer_name, layer_data in net.inputs.items():
+        feed_dict.update({layer_name: np.ones(shape=layer_data.shape)})
+    return feed_dict
+
+
 def infer(ir_path, device):
     """
-     Function to perform IE inference using python API "in place"
+    Function to perform IE inference using python API "in place"
     :param ir_path: Path to XML file of IR
     :param device: Device name for inference
     :return: Dict containing out blob name and out data
     """
 
     bin_path = os.path.splitext(ir_path)[0] + '.bin'
-    net = IENetwork(model=ir_path, weights=bin_path)
-    feed_dict = {}
-    for layer, name in net.inputs.items():
-        feed_dict.update({layer: np.ones(shape=name.shape)})
     ie = IECore()
+    net = ie.read_network(model=ir_path, weights=bin_path)
     exec_net = ie.load_network(net, device)
-    res = exec_net.infer(inputs=feed_dict)
+    res = exec_net.infer(inputs=input_preparation(net))
 
     del net
     # It's important to delete executable network first to avoid double free in plugin offloading.
@@ -48,9 +58,9 @@ def cli_parser():
     """
     parser = argparse.ArgumentParser(description='Arguments for python API inference')
     parser.add_argument('-m', dest='ir_path', required=True, help='Path to XML file of IR')
-    parser.add_argument('-d', dest='device', help='Target device to infer on', default="CPU")
-    parser.add_argument('-r', dest='out_path', default=None,
-                        help='Dumps results to the output folder')
+    parser.add_argument('-d', dest='device', required=True, help='Target device to infer on')
+    parser.add_argument('-r', dest='out_path', required=True,
+                        help='Dumps results to the output file')
     args = parser.parse_args()
     ir_path = args.ir_path
     device = args.device
@@ -60,7 +70,6 @@ def cli_parser():
 
 if __name__ == "__main__":
     ir_path, device, out_path = cli_parser()
-    bin_path = os.path.splitext(ir_path)[0] + '.bin'
     results = infer(ir_path=ir_path, device=device)
     np.savez(out_path, **results)
     log.info("Path for inference results: {}".format(out_path))
