2370e4fbcc1e8ee0d2c63a1b8966b825a1e51fd2
pszmel <piotr.szmelczynski@intel.com>
2021-02-23 13:56:10 +0100

    add calculation of index of offset

--- ngraph/core/reference/include/ngraph/runtime/reference/deformable_convolution.hpp
+++ ngraph/core/reference/include/ngraph/runtime/reference/deformable_convolution.hpp
@@ -25,6 +25,99 @@ namespace ngraph
     {
         namespace reference
         {
+            template <typename T>
+            void convolve_3D_channels(const ConvolutionParams& p,
+                                      const T* batch,
+                                      const Shape& batch_shape,
+                                      const T* offset,
+                                      const Shape& offset_shape,
+                                      const T* filter,
+                                      const Shape& filter_shape,
+                                      T*& out)
+            {
+                const int input_size_z = batch_shape[1];
+                const int input_size_y = batch_shape[2];
+                const int input_size_x = batch_shape[3];
+                const int filter_size_z = filter_shape[1];
+                const int filter_size_y = filter_shape[2];
+                const int filter_size_x = filter_shape[3];
+                const int dilated_filter_size_z =
+                    filter_size_z + (filter_size_z - 1) * (p.dilation[0] - 1);
+                const int dilated_filter_size_y =
+                    filter_size_y + (filter_size_y - 1) * (p.dilation[1] - 1);
+                const int dilated_filter_size_x =
+                    filter_size_x + (filter_size_x - 1) * (p.dilation[2] - 1);
+
+                const Shape input_channel_shape(++batch_shape.begin(), batch_shape.end());
+                const size_t input_channel_size = shape_size(input_channel_shape);
+                const Shape filter_channel_shape(++filter_shape.begin(), filter_shape.end());
+                const size_t filter_channel_size = shape_size(filter_channel_shape);
+                int f_c = 0;
+                const Shape offset_spatial_dims_shape(++offset_shape.begin(), offset_shape.end());
+                const int spatial_size = shape_size(offset_spatial_dims_shape);
+                for (int i_z = -p.pads_begin[0];
+                     i_z <= (p.pads_end[0] + input_size_z - dilated_filter_size_z);
+                     i_z += p.strides[0])
+                {
+                    for (int i_y = -p.pads_begin[1];
+                         i_y <= (p.pads_end[1] + input_size_y - dilated_filter_size_y);
+                         i_y += p.strides[1])
+                    {
+                        for (int i_x = -p.pads_begin[2];
+                             i_x <= (p.pads_end[2] + input_size_x - dilated_filter_size_x);
+                             i_x += p.strides[2])
+                        {
+                            auto input_channel = batch;
+                            auto filter_channel = filter;
+                            T sum = 0;
+                            int off = 0;
+                            size_t filter_channels_count = filter_shape[0];
+                            while (filter_channels_count--)
+                            {
+                                for (int f_z = 0; f_z < filter_size_z; ++f_z)
+                                {
+                                    for (int f_y = 0; f_y < filter_size_y; ++f_y)
+                                    {
+                                        for (int f_x = 0; f_x < filter_size_x; ++f_x)
+                                        {
+                                            int rel_i_z = i_z + (f_z * p.dilation[0]);
+                                            int rel_i_y = i_y + (f_y * p.dilation[1]);
+                                            int rel_i_x = i_x + (f_x * p.dilation[2]);
+
+                                            bool padding = !(in_range(rel_i_x, {0, input_size_x}) &&
+                                                             in_range(rel_i_y, {0, input_size_y}) &&
+                                                             in_range(rel_i_z, {0, input_size_z}));
+                                            if (padding)
+                                                continue;
+
+                                            int f_buf_idx = (f_z * filter_size_y * filter_size_x) +
+                                                            (f_y * filter_size_x) + f_x;
+
+                                            int i_buf_idx =
+                                                (rel_i_z * input_size_y * input_size_x) +
+                                                (rel_i_y * input_size_x) + rel_i_x;
+
+                                            int y_offset =
+                                                offset[(f_buf_idx + off++) * spatial_size + f_c];
+                                            int x_offset =
+                                                offset[(f_buf_idx + off) * spatial_size + f_c];
+
+                                            sum += static_cast<T>(input_channel[i_buf_idx]) *
+                                                   static_cast<T>(filter_channel[f_buf_idx]);
+                                        }
+                                    }
+                                }
+                                f_c++;
+                                input_channel += input_channel_size;
+                                filter_channel += filter_channel_size;
+                            }
+                            *out = sum;
+                            ++out;
+                        }
+                    }
+                }
+            }
+
             void extend_to_3D(ConvolutionParams& p,
                               Shape& in_shape,
                               Shape& offset_shape,
@@ -76,31 +169,43 @@ namespace ngraph
                 // here we are extending spatial dimensions to 3D, because we are going to use 3D
                 // convolution implementation to convolve also in 1D & 2D case
                 Shape input_shape{in_shape};
-                Shape offset_shape(o_shape);
+                Shape offsets_shape(o_shape);
                 Shape filters_shape{f_shape};
                 if (in_shape.size() < 5)
                 {
-                    extend_to_3D(params, input_shape, offset_shape, filters_shape);
+                    extend_to_3D(params, input_shape, offsets_shape, filters_shape);
                 }
 
                 const size_t batches_count = input_shape[in_batch_axis];
                 const Shape batch_shape(++input_shape.begin(), input_shape.end());
                 const size_t batch_size = shape_size(batch_shape);
 
+                const Shape offset_batch_shape(++offsets_shape.begin(), offsets_shape.end());
+                const size_t offset_batch_size = shape_size(offset_batch_shape);
+
                 const size_t filters_count = filters_shape[filter_out_ch_axis];
                 const Shape filter_shape(++filters_shape.begin(), filters_shape.end());
                 const size_t filter_size = shape_size(filter_shape);
 
                 auto batch = in;
+                auto offsets = o;
                 for (size_t batch_idx = 0; batch_idx < batches_count; ++batch_idx)
                 {
                     auto filter = f;
                     for (size_t f_idx = 0; f_idx < filters_count; ++f_idx)
                     {
-                        convolve_3D_channels(params, batch, batch_shape, filter, filter_shape, out);
+                        convolve_3D_channels(params,
+                                             batch,
+                                             batch_shape,
+                                             offsets,
+                                             offset_batch_shape,
+                                             filter,
+                                             filter_shape,
+                                             out);
                         filter += filter_size;
                     }
                     batch += batch_size;
+                    offsets += offset_batch_size;
                 }
             }
 
