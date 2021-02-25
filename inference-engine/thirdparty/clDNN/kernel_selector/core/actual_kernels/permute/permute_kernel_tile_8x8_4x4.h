261cfbede81ddad19172fd6727c353c6f0ed06de
Taylor Yeonbok Lee <taylor.lee@intel.com>
2021-02-25 18:54:28 +0900

    [IE CLDNN] Added  a new permute kernel optimized with tiling  (#4424)

--- inference-engine/thirdparty/clDNN/kernel_selector/core/actual_kernels/permute/permute_kernel_tile_8x8_4x4.h
+++ inference-engine/thirdparty/clDNN/kernel_selector/core/actual_kernels/permute/permute_kernel_tile_8x8_4x4.h
@@ -18,23 +18,22 @@
 #include "permute_kernel_base.h"
 
 namespace kernel_selector {
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// PermuteKernelRef
+// PermuteKernel_tile_8x8_4x4
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-class PermuteKernelRef : public PermuteKernelBase {
+class PermuteKernel_tile_8x8_4x4 : public PermuteKernelBase {
 public:
     using Parent = PermuteKernelBase;
     using Parent::Parent;
-    PermuteKernelRef() : PermuteKernelBase("permute_ref") {}
-    virtual ~PermuteKernelRef() {}
+    PermuteKernel_tile_8x8_4x4() : PermuteKernelBase("permute_tile_8x8_4x4") {}
+    virtual ~PermuteKernel_tile_8x8_4x4() {}
 
+    virtual bool Validate(const Params& p, const optional_params& o) const override;
     KernelsPriority GetKernelsPriority(const Params& params, const optional_params& options) const;
     ParamsKey GetSupportedKey() const override;
-
 protected:
-    JitConstants GetJitConstants(const permute_params& params, const CommonDispatchData& dispatchData) const override;
-    CommonDispatchData SetDefault(const permute_params& params) const override;
+    JitConstants GetJitConstants(const permute_params& params, const CommonDispatchData& dispatchData) const;
+    CommonDispatchData SetDefault(const permute_params& params) const;
     std::vector<FusedOpType> GetSupportedFusedOps() const override {
         return {
             FusedOpType::ACTIVATION,
