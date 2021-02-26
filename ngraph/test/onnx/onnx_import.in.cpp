1deba1e8bab03823dfb36d3c70f87294ace62c82
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2021-02-26 11:23:11 +0100

    Improve add_v6 unit test readability

--- ngraph/test/onnx/onnx_import.in.cpp
+++ ngraph/test/onnx/onnx_import.in.cpp
@@ -3682,12 +3682,11 @@ NGRAPH_TEST(${BACKEND_NAME}, onnx_model_add_v6_broadcast_axes_1_2)
     auto test_case = test::TestCase<TestEngine>(function);
 
     Shape shape{1, 3, 2, 2};
-    std::vector<float> A(shape_size(shape));
-    std::iota(A.begin(), A.end(), 1);
+    std::vector<float> A(shape_size(shape), 0.f);
     test_case.add_input<float>(A);
-    test_case.add_input<float>({3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f});
+    test_case.add_input<float>({3.f, 4.f, 5.f, 6.f, 7.f, 8.f});
     test_case.add_expected_output<float>(
-        shape, {4.0f, 5.0f, 7.0f, 8.0f, 10.0f, 11.0f, 13.0f, 14.0f, 16.0f, 17.0f, 19.0f, 20.0f});
+        shape, {3.f, 3.f, 4.f, 4.f, 5.f, 5.f, 6.f, 6.f, 7.f, 7.f, 8.f, 8.f});
     test_case.run();
 }
 
