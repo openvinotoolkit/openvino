d9d19cf8e4dbd087c37d96642e23b42cd21aa429
Evgeny Lazarev <evgeny.lazarev@intel.com>
2021-02-26 12:47:49 +0300

    Reverted Gelu-2 to be fused op

--- ngraph/core/include/ngraph/op/gelu.hpp
+++ ngraph/core/include/ngraph/op/gelu.hpp
@@ -18,6 +18,7 @@
 
 #include "ngraph/node.hpp"
 #include "ngraph/op/op.hpp"
+#include "ngraph/op/util/fused_op.hpp"
 #include "ngraph/op/util/unary_elementwise_arithmetic.hpp"
 
 namespace ngraph
@@ -29,7 +30,7 @@ namespace ngraph
         {
             /// \brief Gaussian Error Linear Unit
             /// f(x) = 0.5 * x * (1 + erf( x / sqrt(2) )
-            class NGRAPH_API Gelu : public util::UnaryElementwiseArithmetic
+            class NGRAPH_API Gelu : public ngraph::op::util::FusedOp
             {
             public:
                 static constexpr NodeTypeInfo type_info{"Gelu", 0};
@@ -41,8 +42,9 @@ namespace ngraph
                 Gelu(const Output<Node>& data);
 
                 bool visit_attributes(AttributeVisitor& visitor) override;
+                virtual OutputVector decompose_op() const override;
 
-                void validate_and_infer_types() override;
+                void pre_validate_and_infer_types() override;
 
                 virtual std::shared_ptr<Node>
                     clone_with_new_inputs(const OutputVector& new_args) const override;
