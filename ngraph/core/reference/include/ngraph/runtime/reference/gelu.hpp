19b2634c1b773771c9d5a9a46a4cc7f909db5422
Evgeny Lazarev <evgeny.lazarev@intel.com>
2021-02-26 10:59:06 +0300

    Code style check

--- ngraph/core/reference/include/ngraph/runtime/reference/gelu.hpp
+++ ngraph/core/reference/include/ngraph/runtime/reference/gelu.hpp
@@ -43,7 +43,8 @@ namespace ngraph
                     for (size_t i = 0; i < count; i++)
                     {
                         auto& x = arg[i];
-                        out[i] = 0.5 * x * (1.0 + std::tanh(sqpi * (x + 0.044715 * std::pow(x, 3))));
+                        out[i] =
+                            0.5 * x * (1.0 + std::tanh(sqpi * (x + 0.044715 * std::pow(x, 3))));
                     }
                 }
             }
