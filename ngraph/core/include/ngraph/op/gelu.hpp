9196abdbfb5901820d46c612ae68d7dc42c7b33a
Evgeny Lazarev <evgeny.lazarev@intel.com>
2021-02-26 14:57:41 +0300

    Added default value for GeluApproximation mode for Gelu-6 op

--- ngraph/core/include/ngraph/op/gelu.hpp
+++ ngraph/core/include/ngraph/op/gelu.hpp
@@ -77,7 +77,7 @@ namespace ngraph
                 ///
                 /// \param data Input tensor
                 /// \param mode Approximation mode
-                Gelu(const Output<Node>& data, GeluApproximationMode mode);
+                Gelu(const Output<Node>& data, GeluApproximationMode mode = GeluApproximationMode::ERF);
 
                 bool visit_attributes(AttributeVisitor& visitor) override;
 
