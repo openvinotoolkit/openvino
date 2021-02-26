1fe8bed1d1ca9e34babf074e9d669692e1a48cee
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2021-02-26 10:31:58 +0300

    [Python] Fixed issues in runtime wheel (#4359)
    * Added rpath for plugins
    
    * Corrected dll pathes for PIP
    
    * Fixed plugin install names
    
    * Put libs to the sub-package
    
    * Corrected libraries path according to the new location
    
    * Added download and home page urls

--- inference-engine/ie_bridges/python/src/openvino/offline_transformations/__init__.py
+++ inference-engine/ie_bridges/python/src/openvino/offline_transformations/__init__.py
@@ -1,2 +1,14 @@
+import os
+import sys
+
+if sys.platform == "win32":
+    # PIP installs openvino dlls 3 directories above in openvino.libs by default
+    # and this path needs to be visible to the openvino modules
+    #
+    # If you're using a custom installation of openvino,
+    # add the location of openvino dlls to your system PATH.
+    openvino_dlls = os.path.join(os.path.dirname(__file__), "..", "..", "openvino", "libs")
+    os.environ["PATH"] = os.path.abspath(openvino_dlls) + ";" + os.environ["PATH"]
+
 from .offline_transformations_api import *
 __all__ = ['ApplyMOCTransformations']
