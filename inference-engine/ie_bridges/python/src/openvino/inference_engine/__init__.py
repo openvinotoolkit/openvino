91ed28e151d000d6cc8e8e86508d49360b72378b
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2021-02-26 10:31:58 +0300

    [Python] Fixed issues in runtime wheel (#4359)
    * Added rpath for plugins
    
    * Corrected dll pathes for PIP
    
    * Fixed plugin install names
    
    * Put libs to the sub-package
    
    * Corrected libraries path according to the new location
    
    * Added download and home page urls

--- inference-engine/ie_bridges/python/src/openvino/inference_engine/__init__.py
+++ inference-engine/ie_bridges/python/src/openvino/inference_engine/__init__.py
@@ -7,8 +7,8 @@ if sys.platform == "win32":
     #
     # If you're using a custom installation of openvino,
     # add the location of openvino dlls to your system PATH.
-    openvino_dll = os.path.join(os.path.dirname(__file__), "..", "..", "..", "openvino.libs")
-    os.environ["PATH"] = os.path.abspath(openvino_dll) + ";" + os.environ["PATH"]
+    openvino_dlls = os.path.join(os.path.dirname(__file__), "..", "..", "openvino", "libs")
+    os.environ["PATH"] = os.path.abspath(openvino_dlls) + ";" + os.environ["PATH"]
 
 from .ie_api import *
 __all__ = ['IENetwork', "TensorDesc", "IECore", "Blob", "PreProcessInfo", "get_version"]
