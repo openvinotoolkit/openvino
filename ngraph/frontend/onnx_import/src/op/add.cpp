e0297eada290dfba85f196024272bc3b7e27130e
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2021-02-26 11:45:50 +0100

    Remove useless or redundant includes

--- ngraph/frontend/onnx_import/src/op/add.cpp
+++ ngraph/frontend/onnx_import/src/op/add.cpp
@@ -14,12 +14,10 @@
 // limitations under the License.
 //*****************************************************************************
 
-#include <numeric>
-
+#include "op/add.hpp"
 #include "default_opset.hpp"
 #include "ngraph/builder/autobroadcast.hpp"
 #include "ngraph/shape.hpp"
-#include "op/add.hpp"
 
 namespace ngraph
 {
