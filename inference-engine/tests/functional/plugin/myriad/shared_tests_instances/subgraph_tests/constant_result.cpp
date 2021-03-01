bbd3af6689c7378aed874b2063e4afe3fc878cbb
Maxim Andronov <maxim.andronov@intel.com>
2021-03-01 12:16:06 +0300

    [IE TESTS] Update ConstantResultSubgraphTest (#4120)

--- inference-engine/tests/functional/plugin/myriad/shared_tests_instances/subgraph_tests/constant_result.cpp
+++ inference-engine/tests/functional/plugin/myriad/shared_tests_instances/subgraph_tests/constant_result.cpp
@@ -8,10 +8,39 @@
 #include "common_test_utils/test_constants.hpp"
 
 using namespace SubgraphTestsDefinitions;
+using namespace InferenceEngine;
 
 namespace {
-    INSTANTIATE_TEST_CASE_P(smoke_Check, ConstantResultSubgraphTest,
-                            ::testing::Values(CommonTestUtils::DEVICE_MYRIAD),
-                            ConstantResultSubgraphTest::getTestCaseName);
+
+const std::vector<ConstantSubgraphType> types = {
+    ConstantSubgraphType::SINGLE_COMPONENT,
+    ConstantSubgraphType::SEVERAL_COMPONENT
+};
+
+const std::vector<SizeVector> shapes = {
+    {1, 3, 10, 10},
+    {2, 3, 4, 5}
+};
+
+const std::vector<Precision> precisions = {
+    Precision::U8,
+    Precision::I8,
+    Precision::U16,
+    Precision::I16,
+    Precision::I32,
+    Precision::U64,
+    Precision::I64,
+    Precision::FP32,
+    Precision::BOOL
+};
+
+INSTANTIATE_TEST_CASE_P(smoke_Check, ConstantResultSubgraphTest,
+                        ::testing::Combine(
+                            ::testing::ValuesIn(types),
+                            ::testing::ValuesIn(shapes),
+                            ::testing::ValuesIn(precisions),
+                            ::testing::Values(CommonTestUtils::DEVICE_MYRIAD)),
+                        ConstantResultSubgraphTest::getTestCaseName);
+
 }  // namespace
 
--- inference-engine/tests/functional/shared_test_classes/include/shared_test_classes/subgraph/constant_result.hpp
+++ inference-engine/tests/functional/shared_test_classes/include/shared_test_classes/subgraph/constant_result.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2020 Intel Corporation
+// Copyright (C) 2020-2021 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
 
@@ -14,16 +14,27 @@
 
 namespace SubgraphTestsDefinitions {
 
-typedef std::tuple<
-            std::string                        // Device name
+enum class ConstantSubgraphType {
+    SINGLE_COMPONENT,
+    SEVERAL_COMPONENT
+};
+
+std::ostream& operator<<(std::ostream &os, ConstantSubgraphType type);
+
+typedef std::tuple <
+    ConstantSubgraphType,
+    InferenceEngine::SizeVector, // input shape
+    InferenceEngine::Precision,  // input precision
+    std::string                  // Device name
 > constResultParams;
 
 class ConstantResultSubgraphTest : public testing::WithParamInterface<constResultParams>,
                                    virtual public LayerTestsUtils::LayerTestsCommon {
 public:
     static std::string getTestCaseName(testing::TestParamInfo<constResultParams> obj);
+    void createGraph(ConstantSubgraphType type, const InferenceEngine::SizeVector &inputShape, const InferenceEngine::Precision &inputPrecision);
 protected:
     void SetUp() override;
 };
-}  // namespace SubgraphTestsDefinitions
 
+}  // namespace SubgraphTestsDefinitions
