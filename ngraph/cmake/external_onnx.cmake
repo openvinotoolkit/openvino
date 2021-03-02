f15f909df47e5cb6f0e6b9b846c6082336be0b50
mbencer <mateusz.bencer@intel.com>
2021-03-02 21:16:37 +0100

    make onnx_editor static lib

--- ngraph/cmake/external_onnx.cmake
+++ ngraph/cmake/external_onnx.cmake
@@ -47,10 +47,11 @@ macro(onnx_set_target_properties)
         target_compile_options(onnx PRIVATE -Wno-unused-variable -Wno-unused-parameter)
         target_compile_options(onnx_proto PRIVATE -Wno-unused-variable)
     endif()
+
     set_target_properties(onnx_proto PROPERTIES
-    CXX_VISIBILITY_PRESET default
-    C_VISIBILITY_PRESET default
-    VISIBILITY_INLINES_HIDDEN OFF)
+        CXX_VISIBILITY_PRESET default
+        C_VISIBILITY_PRESET default
+        VISIBILITY_INLINES_HIDDEN OFF)
     target_compile_definitions(onnx PUBLIC ONNX_BUILD_SHARED_LIBS)
 
     install(TARGETS onnx_proto
