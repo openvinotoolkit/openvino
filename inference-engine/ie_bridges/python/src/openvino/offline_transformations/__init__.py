9638f2b7cf9534264fabddded69e340c0d9732e2
mryzhov <mikhail.ryzhov@intel.com>
2021-03-03 19:13:21 +0300

    Python 3.8 support

--- inference-engine/ie_bridges/python/src/openvino/offline_transformations/__init__.py
+++ inference-engine/ie_bridges/python/src/openvino/offline_transformations/__init__.py
@@ -4,17 +4,28 @@ import os
 import sys
 
 if sys.platform == "win32":
-    # PIP installs openvino dlls 3 directories above in openvino.libs by default
+    # Installer, yum, pip installs openvino dlls to the different directory
     # and this path needs to be visible to the openvino modules
     #
     # If you're using a custom installation of openvino,
     # add the location of openvino dlls to your system PATH.
-    openvino_dlls = os.path.join(os.path.dirname(__file__), "..", "..", "openvino", "libs")
-    if (3, 8) <= sys.version_info:
-        # On Windows, with Python >= 3.8, DLLs are no longer imported from the PATH.
-        os.add_dll_directory(os.path.abspath(openvino_dlls))
-    else:
-        os.environ["PATH"] = os.path.abspath(openvino_dlls) + ";" + os.environ["PATH"]
+    openvino_libs = [
+            '../../../../deployment_tools/inference_engine/bin/intel64/Release',
+            '../../../../deployment_tools/inference_engine/bin/intel64/Debug',
+            '../../../../deployment_tools/inference_engine/external/hddl/bin',
+            '../../../../deployment_tools/inference_engine/external/gna/lib',
+            '../../../../deployment_tools/inference_engine/external/tbb/bin',
+            '../../../../deployment_tools/ngraph/lib',
+            '../../openvino/libs',  # pip specific directory
+        ]
+    for lib in openvino_libs:
+        lib_path = os.path.join(os.path.dirname(__file__), lib)
+        if os.path.isdir(lib_path):
+            # On Windows, with Python >= 3.8, DLLs are no longer imported from the PATH.
+            if (3, 8) <= sys.version_info:
+                os.add_dll_directory(os.path.abspath(lib_path))
+            else:
+                os.environ["PATH"] = os.path.abspath(lib_path) + ";" + os.environ["PATH"]
 
 from .offline_transformations_api import *
 __all__ = ['ApplyMOCTransformations']
