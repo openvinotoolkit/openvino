b2b2620ffb983dda21558a012355c9461d12088c
bsledz <bartosz.sledz@intel.com>
2021-03-03 08:40:23 +0100

    Add missing type

--- ngraph/frontend/onnx_import/src/op/org.openvinotoolkit/group_norm.cpp
+++ ngraph/frontend/onnx_import/src/op/org.openvinotoolkit/group_norm.cpp
@@ -82,7 +82,7 @@ namespace ngraph
 
                     size_t num_groups =
                         static_cast<size_t>(node.get_attribute_value<int64_t>("num_groups"));
-                    eps = node.get_attribute_value<float>("eps", 1e-5);
+                    float eps = node.get_attribute_value<float>("eps", 1e-5);
 
                     auto data_shape_node = std::make_shared<default_opset::ShapeOf>(data);
                     auto data_reshaped = std::make_shared<default_opset::Reshape>(
