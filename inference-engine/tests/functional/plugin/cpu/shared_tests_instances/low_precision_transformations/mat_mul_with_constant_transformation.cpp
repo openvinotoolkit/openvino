6a446f368b7ab81b75f207dd1628f7d6fb4c6a97
Vladislav Golubev <vladislav.golubev@intel.com>
2021-03-03 12:07:49 +0300

    [LPT] matmul with qdq on weights tests (#4283)

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/low_precision_transformations/mat_mul_with_constant_transformation.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/low_precision_transformations/mat_mul_with_constant_transformation.cpp
@@ -18,9 +18,19 @@ std::vector<MatMulWithConstantTransformationTestValues> testValues = {
     {
         { 2, 3, 4 },
         { 256ul, {{1, 1, 1}, {1, 1, 1}, {1, 3, 1}, {1, 3, 1}}, {0.f}, {255.f}, {0.f, 0.f, 0.f}, {255.f, 25.5f, 255.f} },
-        { 2, 4 },
-        std::vector<float>(4 * 2, 2.f),
+        { std::vector<float>(4 * 2, 2.f), ngraph::element::f32, ngraph::Shape{ 2, 4 } },
         { 256ul, {{1}, {1}, {2, 1}, {2, 1}}, {-128.f}, {127.f}, {-128.f, -12.8f}, {127.f, 12.7f} },
+        { {}, {}, {} },
+        "matMul/FC",
+        "U8"
+    },
+    // 3D with dequantize on weights
+    {
+        { 2, 3, 4 },
+        { 256ul, {{1, 1, 1}, {1, 1, 1}, {1, 3, 1}, {1, 3, 1}}, {0.f}, {255.f}, {0.f, 0.f, 0.f}, {255.f, 25.5f, 255.f} },
+        { std::vector<float>(4 * 2, 2.f), ngraph::element::i8, ngraph::Shape{ 2, 4 } },
+        {},
+        { ngraph::element::f32, {}, {0.1f} },
         "matMul/FC",
         "U8"
     },
@@ -28,9 +38,9 @@ std::vector<MatMulWithConstantTransformationTestValues> testValues = {
     {
         { 1, 3, 4 },
         { 256ul, {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}, {1, 1, 1}}, {-10.5f}, {4.5f}, {-10.5f}, {4.5f} },
-        { 2, 4 },
-        std::vector<float>(4 * 2, 2.f),
+        { std::vector<float>(4 * 2, 2.f), ngraph::element::f32, ngraph::Shape{ 2, 4 } },
         { 256ul, {{1}, {1}, {2, 1}, {2, 1}}, {-128.f}, {127.f}, {-128.f, -12.8f}, {127.f, 12.7f} },
+        { {}, {}, {} },
         "matMul/FC",
         "U8"
     },
@@ -38,9 +48,19 @@ std::vector<MatMulWithConstantTransformationTestValues> testValues = {
     {
         { 1, 1, 3, 4 },
         { 256ul, {{1, 1, 1}, {1, 1, 1}, {1, 3, 1}, {1, 3, 1}}, {0.f}, {255.f}, {0.f, 0.f, 0.f}, {255.f, 25.5f, 255.f} },
-        { 2, 4 },
-        std::vector<float>(4 * 2, 2.f),
+        { std::vector<float>(4 * 2, 2.f), ngraph::element::f32, ngraph::Shape{ 2, 4 } },
         { 256ul, {{1}, {1}, {2, 1}, {2, 1}}, {-128.f}, {127.f}, {-128.f, -12.8f}, {127.f, 12.7f} },
+        { {}, {}, {} },
+        "matMul/FC",
+        "U8"
+    },
+    // 4D with Dq on weights
+    {
+        { 1, 1, 3, 4 },
+        { 256ul, {{1, 1, 1}, {1, 1, 1}, {1, 3, 1}, {1, 3, 1}}, {0.f}, {255.f}, {0.f, 0.f, 0.f}, {255.f, 25.5f, 255.f} },
+        { std::vector<float>(4 * 2, 2.f), ngraph::element::i8, ngraph::Shape{ 2, 4 } },
+        {},
+        { ngraph::element::f32, {}, {{0.1f, 0.01}, ngraph::element::f32, ngraph::Shape{ 2, 1 }} },
         "matMul/FC",
         "U8"
     },
@@ -48,9 +68,9 @@ std::vector<MatMulWithConstantTransformationTestValues> testValues = {
     {
         { 1, 3, 4 },
         { 256ul, {{1}, {1}, {1}, {1}}, {0.f}, {255.f}, {0.f}, {25.5f} },
-        { 4, 4 },
-        std::vector<float>(4 * 4, 2.f),
+        { std::vector<float>(4 * 4, 2.f), ngraph::element::f32, ngraph::Shape{ 4, 4 } },
         { 256ul, {{1}, {1}, {1}, {1}}, {-128.f}, {127.f}, {-128.f}, {127.f} },
+        { {}, {}, {} },
         "matMul/FC",
         "U8"
     },
@@ -58,9 +78,19 @@ std::vector<MatMulWithConstantTransformationTestValues> testValues = {
     {
         { 2, 3 },
         { 256ul, {{1}, {1}, {2, 1}, {2, 1}}, {-10.f}, {5.f}, {-10.f, -5.f}, {5.f, 5.f} },
-        { 2, 3 },
-        std::vector<float>{1, 2, 3, 4, 5, 6},
+        { std::vector<float>{1, 2, 3, 4, 5, 6}, ngraph::element::f32, ngraph::Shape{ 2, 3 } },
         { 256ul, {{1}, {1}, {1}, {1}}, {-128.f}, {127.f}, {-12.8f}, {12.7f} },
+        { {}, {}, {} },
+        "matMul/1",
+        "U8"
+    },
+    // 2D with subtract on activations & Dq on weights
+    {
+        { 2, 3 },
+        { 256ul, {{1}, {1}, {2, 1}, {2, 1}}, {-10.f}, {5.f}, {-10.f, -5.f}, {5.f, 5.f} },
+        { std::vector<float>{1, 2, 3, 4, 5, 6}, ngraph::element::i8, ngraph::Shape{ 2, 3 } },
+        {},
+        { ngraph::element::f32, {}, {0.1f} },
         "matMul/1",
         "U8"
     }
