eee5a5f2f5cb1178e0ba75de0b26027f7cfcc57f
Bartosz Sochacki <bartosz.sochacki@intel.com>
2021-03-04 14:10:01 +0100

    [GNA] POT enabling (#4358)
    * POT enabling for GNA
    
    * POT enabling for GNA
    
    * POT enabling for GNA - accuracy improvements

--- inference-engine/src/gna_plugin/backend/make_pwl.cpp
+++ inference-engine/src/gna_plugin/backend/make_pwl.cpp
@@ -34,15 +34,20 @@ void make_gna_pwl(const DnnActivation  fun,
             gna_pwl[0].xBase = static_cast<int32_t> (INT32_MIN & XBASEMASK);  // zero out the 2 lsb
             if (fun == kActSigmoid) {
                 gnalog() <<  "=========================== Sigmoid Segments ===========================\n";
-                gna_pwl[0].yBase = gna_pwl[1].yBase = 0;
+                auto minVal = fun.fqParams.set? FLOAT_TO_INT16(*fun.fqParams.input_low * out_scale): 0;
+                gna_pwl[0].yBase = gna_pwl[1].yBase = minVal;
                 gna_pwl[1].xBase = (static_cast<int32_t> (in_scale * (-pwl[0].b / pwl[0].m))) & XBASEMASK;
             } else if (fun == kActTanh) {
                 gnalog() <<  "=========================== Tanh Segments ===========================\n";
-                gna_pwl[0].yBase = gna_pwl[1].yBase = static_cast<int16_t>(-1.0 * out_scale);
+                auto minVal = fun.fqParams.set ? FLOAT_TO_INT16(*fun.fqParams.input_low * out_scale) :
+                    static_cast<int16_t>(-1.0 * out_scale);
+                gna_pwl[0].yBase = gna_pwl[1].yBase = minVal;
                 gna_pwl[1].xBase = (static_cast<int32_t> (in_scale * (-1.0 - pwl[0].b) / pwl[0].m)) & XBASEMASK;
             } else {
                 gnalog() << "=========================== SoftSign Segments ===========================\n";
-                gna_pwl[0].yBase = gna_pwl[1].yBase = static_cast<int16_t>(-1.0 * out_scale);
+                auto minVal = fun.fqParams.set ? FLOAT_TO_INT16(*fun.fqParams.input_low * out_scale) :
+                    static_cast<int16_t>(-1.0 * out_scale);
+                gna_pwl[0].yBase = gna_pwl[1].yBase = minVal;
                 gna_pwl[1].xBase = (static_cast<int32_t> (in_scale * (-1.0 - pwl[0].b) / pwl[0].m)) & XBASEMASK;
             }
             gna_pwl[0].slope = 0;
@@ -74,9 +79,10 @@ void make_gna_pwl(const DnnActivation  fun,
                          << "\n";
             }
             // insert extra segment for xvalues > u_bound
+            auto maxVal = fun.fqParams.set ? *fun.fqParams.input_high : 1.0;
             gna_pwl[n_segments - 1].xBase =
                     ((uint32_t) (in_scale * (1.0 - pwl[pwl_size - 2].b) / pwl[pwl_size - 2].m)) & XBASEMASK;
-            gna_pwl[n_segments - 1].yBase = FLOAT_TO_INT16(1.0 * out_scale);
+            gna_pwl[n_segments - 1].yBase = FLOAT_TO_INT16(maxVal * out_scale);
             gna_pwl[n_segments - 1].slope = 0;
 
             gnalog() << (gna_pwl[n_segments - 1].xBase / in_scale)
@@ -223,9 +229,19 @@ void make_gna_pwl(const DnnActivation  fun,
             else
                 gnalog() << "=========================== LeakyReLU Segments ======================\n";
             int32_t x_lower = INT32_MIN;
+            int32_t x_upper = INT32_MAX;
             int16_t y_lower = INT16_MIN;
-            if (x_lower < y_lower * in_scale / out_scale) x_lower = FLOAT_TO_INT32(y_lower * in_scale / out_scale);
-            if (y_lower < x_lower * out_scale / in_scale) y_lower = FLOAT_TO_INT16(x_lower * out_scale / in_scale);
+            int16_t y_upper = INT16_MAX;
+            if (fun.fqParams.set) {
+                x_lower = FLOAT_TO_INT32(*fun.fqParams.input_low * 1.25 * in_scale);
+                x_upper = FLOAT_TO_INT32(*fun.fqParams.input_high * 1.25 * in_scale);
+                y_lower = FLOAT_TO_INT16(*fun.fqParams.input_low * 1.25 * out_scale);
+                y_upper = FLOAT_TO_INT16(*fun.fqParams.input_high * 1.25 * out_scale);
+            } else {
+                if (x_lower < y_lower * in_scale / out_scale) x_lower = FLOAT_TO_INT32(y_lower * in_scale / out_scale);
+                if (y_lower < x_lower * out_scale / in_scale) y_lower = FLOAT_TO_INT16(x_lower * out_scale / in_scale);
+            }
+
             gna_pwl[0].yBase = y_lower * fun.args.lrelu.negative_slope;
             s = gna_slope(fun.args.lrelu.negative_slope, in_scale, out_scale);
             gna_pwl[0].xBase = (x_lower & XBASEMASK) | s.slope_scale_index;  // zero out the 2 lsb
@@ -244,6 +260,18 @@ void make_gna_pwl(const DnnActivation  fun,
                     << " " << 0.0
                     << " " << (gna_pwl[1].slope * in_scale) / (out_scale*s.slope_scale)
                     << "\n";
+
+            if (fun.fqParams.set) {  // need a right segment
+                gna_pwl.push_back({
+                    static_cast<int32_t>(x_upper & XBASEMASK),  // zero out the 2 lsb
+                    y_upper,
+                    0 });
+
+                gnalog() << (x_upper & XBASEMASK) / in_scale
+                    << " " << gna_pwl[n_segments].yBase / out_scale
+                    << " " << 0
+                    << "\n";
+            }
             break;
         }
         case kActSign: {
@@ -281,11 +309,18 @@ void make_gna_pwl(const DnnActivation  fun,
             break;
         }
         case kActIdentity:
-        case kActKaldiLstmClipping: {
+        case kActKaldiLstmClipping:
+        case kActFakeQuantize: {
             int32_t x_lower = INT32_MIN;
             int32_t x_upper = INT32_MAX;
             int16_t y_lower = INT16_MIN;
             int16_t y_upper = INT16_MAX;
+            if (fun == kActFakeQuantize && fun.fqParams.set) {
+                x_lower = *fun.fqParams.input_low * in_scale;
+                x_upper = *fun.fqParams.input_high * in_scale;
+                y_lower = *fun.fqParams.input_low * out_scale;
+                y_upper = *fun.fqParams.input_high * out_scale;
+            }
             auto n_segments = 2;
             if (fun == kActKaldiLstmClipping) {
                 gnalog()  << "=========================== Clipping Segments ===========================\n";
@@ -311,6 +346,8 @@ void make_gna_pwl(const DnnActivation  fun,
                 if (x_upper > y_upper * in_scale / out_scale) x_upper = FLOAT_TO_INT32(y_upper * in_scale / out_scale);
                 if (y_lower < x_lower * out_scale / in_scale) y_lower = FLOAT_TO_INT16(x_lower * out_scale / in_scale);
                 if (y_upper > x_upper * out_scale / in_scale) y_upper = FLOAT_TO_INT16(x_upper * out_scale / in_scale);
+            } else if (fun == kActFakeQuantize) {
+                gnalog() << "=========================== Fake Quantize Segments ===========================\n";
             }
             gna_pwl.resize(n_segments);
             gna_pwl[0].xBase = INT32_MIN & XBASEMASK;  // zero out the 2 lsb
--- inference-engine/src/gna_plugin/layers/gna_layer_info.hpp
+++ inference-engine/src/gna_plugin/layers/gna_layer_info.hpp
@@ -103,7 +103,8 @@ class LayerInfo {
              "neglog",
              "neghalflog",
              "softsign",
-             "power"};
+             "power",
+             "fakequantize"};
 
         if (isPower()) {
             auto powerLayer = as<const InferenceEngine::PowerLayer*>();
@@ -157,7 +158,10 @@ class LayerInfo {
         IS_VALID();
         return nullptr != as<const InferenceEngine::ScaleShiftLayer*>();
     }
-
+    bool isSyntheticScaleShift() const noexcept {
+        IS_VALID();
+        return layer->name.find("SyntheticScaleShift") != std::string::npos;
+    }
     bool isEltwise() const noexcept {
         IS_VALID();
         return nullptr != as<const InferenceEngine::EltwiseLayer*>();
@@ -192,6 +196,18 @@ class LayerInfo {
     }
     bool isIdentity() const noexcept {
         return isOfType("identity");
+    }
+    bool isTanh() const noexcept {
+        return isOfType("tanh");
+    }
+    bool isSigmoid() const noexcept {
+        return isOfType("sigmoid");
+    }
+    bool isSoftSign() const noexcept {
+        return isOfType("softsign");
+    }
+    bool isClamp() const noexcept {
+        return isOfType("clamp");
     }
     bool isFullyConnected() const noexcept {
         return isOfType("FullyConnected") || isOfType("InnerProduct");
@@ -282,6 +298,9 @@ class LayerInfo {
 
     bool isCopyDelayed() const noexcept {
         return isOfType(DelayedCopyLayerName);
+    }
+    bool isWeightableIdentity() const noexcept {
+        return isConcatAlignFilter() || isSyntheticScaleShift() || isCropAffined();
     }
 
     size_t paddingSize() const {
