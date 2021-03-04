53fc4a87a3c46e7152c26323d048c362bb9c67de
mitruska <katarzyna.mitrus@intel.com>
2021-03-04 13:34:11 +0100

    Resolve python api xfails

--- ngraph/python/tests/__init__.py
+++ ngraph/python/tests/__init__.py
@@ -67,12 +67,6 @@ xfail_issue_35911 = xfail_test(reason="Assertion error: Pad model mismatch error
 xfail_issue_35912 = xfail_test(reason="RuntimeError: Error of validate layer: B with type: "
                                       "Pad. Cannot parse parameter pads_end  from IR for layer B. "
                                       "Value -1,0 cannot be casted to int.")
-xfail_issue_35916 = xfail_test(reason="RuntimeError: Unsupported input dims count for layer Z")
-xfail_issue_35917 = xfail_test(reason="RuntimeError: Unsupported input dims count for "
-                                      "layer MatMul")
-xfail_issue_35918 = xfail_test(reason="onnx.onnx_cpp2py_export.checker.ValidationError: "
-                                      "Mismatched attribute type in 'test_node : alpha'")
-xfail_issue_35921 = xfail_test(reason="ValueError - shapes mismatch in gemm")
 xfail_issue_35923 = xfail_test(reason="RuntimeError: PReLU without weights is not supported")
 xfail_issue_35925 = xfail_test(reason="Assertion error - reduction ops results mismatch")
 xfail_issue_35927 = xfail_test(reason="RuntimeError: B has zero dimension that is not allowable")
