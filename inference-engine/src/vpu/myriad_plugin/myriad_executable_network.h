e940672ac7095eb45920cafd69dde75b535c74b7
apankratovantonp <anton.pankratov@intel.com>
2021-03-04 18:18:54 +0300

    Removed IRelease Interface

--- inference-engine/src/vpu/myriad_plugin/myriad_executable_network.h
+++ inference-engine/src/vpu/myriad_plugin/myriad_executable_network.h
@@ -88,8 +88,7 @@ public:
         auto taskExecutorGetResult = getNextTaskExecutor();
         auto asyncThreadSafeImpl = std::make_shared<MyriadAsyncInferRequest>(
                 syncRequestImpl, _taskExecutor, _callbackExecutor, taskExecutorGetResult);
-        asyncRequest.reset(new ie::InferRequestBase(asyncThreadSafeImpl),
-                           [](ie::IInferRequest *p) { p->Release(); });
+        asyncRequest.reset(new ie::InferRequestBase(asyncThreadSafeImpl));
         asyncThreadSafeImpl->SetPointerToPublicInterface(asyncRequest);
         return asyncRequest;
     }
