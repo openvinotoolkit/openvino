89d39663c2789daca7c95d93a99e324d2451334f
Anastasia Popova <anastasia.popova@intel.com>
2021-03-05 11:01:20 +0300

    Renamed transformation, added comment.

--- model-optimizer/extensions/front/tf/assign_elimination.py
+++ model-optimizer/extensions/front/tf/assign_elimination.py
@@ -20,12 +20,16 @@ from mo.front.common.replacement import FrontReplacementPattern
 from mo.graph.graph import Graph
 
 
-class AssignElimination(FrontReplacementPattern):
+class AssignAndAssertElimination(FrontReplacementPattern):
+    # The solution with removal of Assign and Assert operations is temporary.
+    # The proper solution is to keep these operations until the partial inference
+    # phase when control flow edges are properly handled and later unnecessary ones are eliminated.
+    # In order to achieve this we need to implement control flow inference function
+    # for these operations similar to "Merge" and "Switch" operations.
     enabled = True
 
     def find_and_replace_pattern(self, graph: Graph):
         for node in graph.get_op_nodes():
-            if node.op in ["Assign", "AssignSub", "AssignAdd", "Assert"]:
-                node_op = node.op
+            if node.soft_get('op') in ["Assign", "AssignSub", "AssignAdd", "Assert"]:
+                log.debug('"{}" op with id="{}" was removed'.format(node.op, node.id))
                 graph.remove_node(node.id)
-                log.debug('"{}" op with id="{}" was removed'.format(node_op, node.id))
