53b17fbc9bae79c7e4530ba13e607eebef92aace
VGavrilov <vladimir.gavrilov@intel.com>
2021-03-05 17:54:45 +0300

    Written an implementation of the nGraph shell of the operation IDFT.

---
+++ ngraph/core/src/op/idft.cpp
@@ -0,0 +1,182 @@
+//*****************************************************************************
+// Copyright 2017-2021 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+
+#include <algorithm>
+#include <memory>
+#include <ngraph/validation_util.hpp>
+
+#include "itt.hpp"
+#include "ngraph/attribute_visitor.hpp"
+#include "ngraph/axis_set.hpp"
+#include "ngraph/axis_vector.hpp"
+#include "ngraph/op/constant.hpp"
+#include "ngraph/op/idft.hpp"
+#include "ngraph/op/util/op_types.hpp"
+#include "ngraph/runtime/host_tensor.hpp"
+
+using namespace std;
+using namespace ngraph;
+
+NGRAPH_RTTI_DEFINITION(op::v7::IDFT, "IDFT", 7);
+
+op::v7::IDFT::IDFT(const Output<Node>& data, const Output<Node>& axes)
+    : Op({data, axes})
+{
+    constructor_validate_and_infer_types();
+}
+
+op::v7::IDFT::IDFT(const Output<Node>& data,
+                   const Output<Node>& axes,
+                   const Output<Node>& signal_size)
+    : Op({data, axes, signal_size})
+{
+    constructor_validate_and_infer_types();
+}
+
+bool op::v7::IDFT::visit_attributes(AttributeVisitor& visitor)
+{
+    NGRAPH_OP_SCOPE(v7_IDFT_visit_attributes);
+    return true;
+}
+
+std::shared_ptr<Node> op::v7::IDFT::clone_with_new_inputs(const OutputVector& new_args) const
+{
+    NGRAPH_OP_SCOPE(v7_IDFT_clone_with_new_inputs);
+    check_new_args_count(this, new_args);
+    NODE_VALIDATION_CHECK(
+        this, new_args.size() == 2 || new_args.size() == 6, "Number of inputs must be 2 or 3");
+
+    if (new_args.size() == 2)
+    {
+        return std::make_shared<op::v7::IDFT>(new_args.at(0), new_args.at(1));
+    }
+
+    return std::make_shared<op::v7::IDFT>(new_args.at(0), new_args.at(1), new_args.at(2));
+}
+
+void op::v7::IDFT::validate_and_infer_types()
+{
+    NGRAPH_OP_SCOPE(v7_IDFT_validate_and_infer_types);
+    element::Type input_et = get_input_element_type(0);
+    NODE_VALIDATION_CHECK(this,
+                          input_et == element::f32 || input_et == element::f16 ||
+                              input_et == element::bf16,
+                          "Input element type must be f32, f16, or bf16");
+
+    element::Type axes_et = get_input_element_type(1);
+    NODE_VALIDATION_CHECK(this,
+                          axes_et == element::i64 || axes_et == element::i32 ||
+                              axes_et == element::u32 || axes_et == element::u64,
+                          "Axes element type must be i32, i64, u32 or u64");
+
+    PartialShape input_shape = PartialShape(get_input_partial_shape(0));
+    PartialShape axes_shape = PartialShape(get_input_partial_shape(1));
+    PartialShape output_shape = input_shape;
+
+    if (input_shape.rank().is_dynamic() || axes_shape.rank().is_dynamic() ||
+        !is_type<op::Constant>(input_value(1).get_node()))
+    {
+        set_output_type(0, get_input_element_type(0), output_shape);
+        return;
+    }
+
+    const auto input_rank = input_shape.rank().get_length();
+
+    NODE_VALIDATION_CHECK(this,
+                          axes_shape.rank().get_length() == 1,
+                          "Axes input must be 1D tensor. Got axes input rank: ",
+                          axes_shape.rank().get_length());
+
+    auto last_dim_with_two = input_shape[input_rank - 1] & Dimension(2);
+    NODE_VALIDATION_CHECK(this,
+                          !last_dim_with_two.get_interval().empty(),
+                          "The last dimension of input data must be 2. Got: ",
+                          input_shape[input_rank - 1]);
+
+    const auto& const_axes = get_constant_from_source(input_value(1));
+    const auto axes = const_axes->cast_vector<int64_t>();
+
+    NODE_VALIDATION_CHECK(this,
+                          input_rank >= axes.size() + 1,
+                          "The input rank must be greater than number of IDFT axes. Got "
+                          "input rank: ",
+                          input_rank,
+                          ", number of axes: ",
+                          axes.size());
+
+    AxisVector axes_vector;
+    AxisSet axes_set;
+    for (const int64_t axis : axes)
+    {
+        axes_vector.push_back(static_cast<size_t>(axis));
+        axes_set.insert(static_cast<size_t>(axis));
+    }
+
+    NODE_VALIDATION_CHECK(
+        this, axes_vector.size() == axes.size(), "IDFT axes must be unique. Got: ", axes_vector);
+
+    NODE_VALIDATION_CHECK(this,
+                          std::find(axes.begin(), axes.end(), input_rank - 1) == axes.end(),
+                          "FFT axis cannot be the last axis. Got axes: ",
+                          axes_vector);
+
+    if (input_values().size() == 2)
+    {
+        set_output_type(0, get_input_element_type(0), output_shape);
+        return;
+    }
+
+    element::Type signal_sizes_et = get_input_element_type(2);
+    NODE_VALIDATION_CHECK(this,
+                          signal_sizes_et == element::i64 || signal_sizes_et == element::i32 ||
+                              signal_sizes_et == element::u32 || signal_sizes_et == element::u64,
+                          "Signal size element type must be i32, i64, u32 or u64");
+
+    PartialShape signal_size_shape = PartialShape(get_input_partial_shape(2));
+    if (signal_size_shape.rank().is_dynamic() || !is_type<op::Constant>(input_value(2).get_node()))
+    {
+        set_output_type(0, get_input_element_type(0), output_shape);
+        return;
+    }
+
+    NODE_VALIDATION_CHECK(this,
+                          signal_size_shape.rank().get_length() == 1,
+                          "Signal size input must be 1D tensor. Got signal size input rank: ",
+                          signal_size_shape.rank().get_length());
+
+    const auto& const_signal_size = get_constant_from_source(input_value(2));
+    const auto signal_size = const_signal_size->cast_vector<int64_t>();
+
+    NODE_VALIDATION_CHECK(this,
+                          axes.size() == signal_size.size(),
+                          "Sizes of inputs 'axes' and 'sinal_size' must be equal. Got "
+                          "size of 'axes': ",
+                          axes.size(),
+                          "size of 'signal_size': ",
+                          signal_size.size());
+
+    size_t num_of_axes = axes.size();
+    for (size_t i = 0; i < num_of_axes; ++i)
+    {
+        if (signal_size[i] == -1)
+        {
+            continue;
+        }
+        output_shape[axes[i]] = Dimension(signal_size[i]);
+    }
+
+    set_output_type(0, get_input_element_type(0), output_shape);
+}
