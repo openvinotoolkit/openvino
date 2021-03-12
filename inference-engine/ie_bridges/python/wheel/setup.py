4f1bab0e0a16ec64a2883184543dd8cad5dbe69c
mryzhov <mikhail.ryzhov@intel.com>
2021-03-12 13:10:10 +0300

    Disabled shell=True

--- inference-engine/ie_bridges/python/wheel/setup.py
+++ inference-engine/ie_bridges/python/wheel/setup.py
@@ -217,10 +217,13 @@ def remove_rpath(file_path):
         :type file_path: pathlib.Path
     """
     if sys.platform == "darwin":
-        cmd = f'otool -l {file_path} ' \
-              f'| grep LC_RPATH -A 3 | grep -o "path.*" | cut -d \" \" -f2 ' \
-              f'| xargs -I{{}} install_name_tool -delete_rpath {{}} {file_path}'
-        ret_info = subprocess.run(cmd, shell=True, check=True)
+        cmd = ['otool', '-l', f'{file_path}',
+               '|', 'grep', 'LC_RPATH', '-A3',
+               '|', 'grep', '-o', '"path.*"',
+               '|', 'cut', '-d', '" "', '-f2',
+               '|', 'xargs', '-I{{}}',
+               'install_name_tool', '-delete_rpath', '{{}}', f'{file_path}']
+        ret_info = subprocess.run(list(cmd.split(" ")), check=True)
         if ret_info.returncode != 0:
             sys.exit(f"Could not remove rpath for {file_path}")
     else:
