8cb47515bb7db9f6414387542dbcac2957d0672f
Stepyreva, Evgenya <evgenya.stepyreva@intel.com>
2021-03-16 13:25:10 +0300

    Small shape propagation & evaluators update

--- ngraph/core/src/op/swish.cpp
+++ ngraph/core/src/op/swish.cpp
@@ -14,6 +14,7 @@
 // limitations under the License.
 //*****************************************************************************
 
+#include <ngraph/validation_util.hpp>
 #include "ngraph/op/swish.hpp"
 #include "itt.hpp"
 #include "ngraph/attribute_visitor.hpp"
@@ -112,9 +113,11 @@ namespace swish
     }
 
     bool
-        evaluate_swish(const HostTensorVector& inputs, const HostTensorPtr& out, const size_t count)
+        evaluate_swish(const HostTensorVector& inputs, const HostTensorPtr& out)
     {
         bool rc = true;
+        size_t count = shape_size(inputs[0]->get_shape());
+
         const HostTensorPtr arg0 = inputs[0];
         const HostTensorPtr arg1 = inputs.size() == 2 ? inputs[1] : nullptr;
         out->set_unary(arg0);
@@ -132,5 +135,7 @@ namespace swish
 bool op::v4::Swish::evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const
 {
     NGRAPH_OP_SCOPE(v4_Swish_evaluate);
-    return swish::evaluate_swish(inputs, outputs[0], shape_size(get_output_shape(0)));
+    NGRAPH_CHECK(this,
+                 validate_host_tensor_vector(outputs, 1) && validate_host_tensor_vector(inputs, 2));
+    return swish::evaluate_swish(inputs, outputs[0]);
 }
