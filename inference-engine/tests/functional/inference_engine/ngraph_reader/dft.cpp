eca371a8179303b9d8d37058cfa15bbeb4df8297
VGavrilov <vladimir.gavrilov@intel.com>
2021-03-16 10:14:40 +0300

    Added the second IE IR Reader test for the nGraph operation DFT.

--- inference-engine/tests/functional/inference_engine/ngraph_reader/dft.cpp
+++ inference-engine/tests/functional/inference_engine/ngraph_reader/dft.cpp
@@ -126,3 +126,161 @@ TEST_F(NGraphReaderTests, ReadDFTNetwork) {
         i64w[1] = 0;
     });
 }
+
+TEST_F(NGraphReaderTests, ReadDFTNetwork2) {
+    std::string model = R"V0G0N(
+<net name="deformable_convolution" version="10">
+    <layers>
+        <layer id="0" name="in1" type="Parameter" version="opset1">
+            <data shape="7,50,130,400,2" element_type="f32"/>
+            <output>
+                <port id="0" precision="FP32">
+                    <dim>7</dim>
+                    <dim>50</dim>
+                    <dim>130</dim>
+                    <dim>400</dim>
+                    <dim>2</dim>
+                </port>
+            </output>
+        </layer>
+        <layer id="1" name="axes" type="Const" version="opset1">
+            <data offset="0" size="24" shape="3" element_type="i64"/>
+            <output>
+                <port id="0" precision="I64">
+                    <dim>3</dim>
+                </port>
+            </output>
+        </layer>
+        <layer id="2" name="signal_size" type="Const" version="opset1">
+            <data offset="24" size="24" shape="3" element_type="i64"/>
+            <output>
+                <port id="0" precision="I64">
+                    <dim>3</dim>
+                </port>
+            </output>
+        </layer>
+        <layer id="3" name="dft" type="DFT" version="opset7">
+            <input>
+                <port id="0">
+                    <dim>7</dim>
+                    <dim>50</dim>
+                    <dim>130</dim>
+                    <dim>400</dim>
+                    <dim>2</dim>
+                </port>
+                <port id="1">
+                    <dim>3</dim>
+                </port>
+                <port id="2">
+                    <dim>3</dim>
+                </port>
+            </input>
+            <output>
+                <port id="3" precision="FP32">
+                    <dim>7</dim>
+                    <dim>40</dim>
+                    <dim>130</dim>
+                    <dim>600</dim>
+                    <dim>2</dim>
+                </port>
+            </output>
+        </layer>
+        <layer id="4" name="output" type="Result" version="opset1">
+            <input>
+                <port id="0">
+                    <dim>7</dim>
+                    <dim>40</dim>
+                    <dim>130</dim>
+                    <dim>600</dim>
+                    <dim>2</dim>
+                </port>
+            </input>
+        </layer>
+    </layers>
+    <edges>
+        <edge from-layer="0" from-port="0" to-layer="3" to-port="0"/>
+        <edge from-layer="1" from-port="0" to-layer="3" to-port="1"/>
+        <edge from-layer="2" from-port="0" to-layer="3" to-port="2"/>
+        <edge from-layer="3" from-port="3" to-layer="4" to-port="0"/>
+    </edges>
+</net>
+)V0G0N";
+    std::string modelV7 = R"V0G0N(
+<net name="deformable_convolution" version="7">
+    <layers>
+        <layer id="0" name="in1" type="Input" version="opset1">
+            <output>
+                <port id="0" precision="FP32">
+                    <dim>7</dim>
+                    <dim>50</dim>
+                    <dim>130</dim>
+                    <dim>400</dim>
+                    <dim>2</dim>
+                </port>
+            </output>
+        </layer>
+        <layer id="1" name="axes" type="Const" version="opset1">
+            <output>
+                <port id="0" precision="I64">
+                    <dim>3</dim>
+                </port>
+            </output>
+            <blobs>
+                <custom offset="0" size="24" precision="I64"/>
+            </blobs>
+        </layer>
+        <layer id="2" name="signal_size" type="Const" version="opset1">
+            <output>
+                <port id="0" precision="I64">
+                    <dim>3</dim>
+                </port>
+            </output>
+            <blobs>
+                <custom offset="24" size="24" precision="I64"/>
+            </blobs>
+        </layer>
+        <layer id="3" name="dft" type="DFT" version="opset7">
+            <input>
+                <port id="0">
+                    <dim>7</dim>
+                    <dim>50</dim>
+                    <dim>130</dim>
+                    <dim>400</dim>
+                    <dim>2</dim>
+                </port>
+                <port id="1">
+                    <dim>3</dim>
+                </port>
+                <port id="2">
+                    <dim>3</dim>
+                </port>
+            </input>
+            <output>
+                <port id="2" precision="FP32">
+                    <dim>7</dim>
+                    <dim>40</dim>
+                    <dim>130</dim>
+                    <dim>600</dim>
+                    <dim>2</dim>
+                </port>
+            </output>
+        </layer>
+    </layers>
+    <edges>
+        <edge from-layer="0" from-port="0" to-layer="3" to-port="0"/>
+        <edge from-layer="1" from-port="0" to-layer="3" to-port="1"/>
+        <edge from-layer="2" from-port="0" to-layer="3" to-port="2"/>
+    </edges>
+</net>
+)V0G0N";
+
+    compareIRs(model, modelV7, 48, [](Blob::Ptr& weights) {
+        auto * i64w = weights->buffer().as<int64_t*>();
+        i64w[0] = 3;
+        i64w[1] = 0;
+        i64w[2] = 1;
+        i64w[3] = 600;
+        i64w[4] = -1;
+        i64w[5] = 40;
+    });
+}
