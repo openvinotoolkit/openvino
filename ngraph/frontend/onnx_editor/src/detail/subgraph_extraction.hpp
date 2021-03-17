3d1a47b6a98cd7303b11e8a5aad46d0c93f26400
Mateusz Bencer <mateusz.bencer@intel.com>
2021-03-17 05:07:53 +0100

    Move ONNX Editor to separate target (#4293)
    * Subgraph extraction in ONNX models
    
    * Windows compilation error fix + docs update
    
    * Code cleanup after the first round of reviews
    
    * CI compilation error fix
    
    * Even more CI compilation error fixes
    
    * Proper usage of ADL in generic code
    
    * ONNX shape inference related code cleanup
    
    * Disable the onnx test utils when pb-lite is used
    
    * PB dependency removal from UT, strong types for input and output edges, more verbose errors in tests
    
    * Fix for the protobuf descriptor database corruption
    
    * testing visibility changes
    
    * Revert the changes that didn't work
    
    * Make tests green again?
    
    * Make the current tests pass
    
    * Remove the ONNX header from editor's tests
    
    * Switch from stable_partition to remove_if because of compiler bugs
    
    * Obsolete test removal and cmakelists cleanup in tests
    
    * Macos failed, reverting some changes
    
    * Handle the multiple output consumers UC
    
    * Keep the tensor name when replacing an initializer
    
    * Cutting a graph with multiple consumers of inputs and initializers
    
    * Subgraph extraction with multiple initializer consumers
    
    * Add CMakeList, move files
    
    * fixed headers locations
    
    * fixed calling tests
    
    * apply code styles
    
    * change namespace to onnx_editor
    
    * removd not needed dependencies to onnx_editor
    
    * Remove linking libonnx from unit-test
    
    * Consider all flavors of protobuf libraries
    
    * disable lto for onnx and onnx_proto
    
    * revert set ONNX_INCLUDE_DIR change
    
    * create new target onnx_common
    
    * cmake dependencies clean-up
    
    * Added onnx_common visibility
    
    * pass ModelProto as reference + remove redundant dependencies
    
    * expose onnx symbols by onnx_common
    
    * remove -fno-lto, move InputEdge and OutputEdge to other header
    
    * configure onnx linking for MSVC and APPLE
    
    * remove unused comment
    
    * fixed linking configuration for msvc and apple/clang
    
    * clean cmakes
    
    * added dependencies
    
    * remove onnx and onnx_proto from NGRAPH_EXPORT_TARGETS_ENABLE
    
    * make onnx and protobuf dynamic
    
    * cmake dependency clean-up
    
    * added onnx patch - make only onnx_proto shared
    
    * use protobuf should depend on BUILD_SHARED_LIBS
    
    * fixed patch
    
    * fixed install targets
    
    * add if to install protobuf
    
    * fixed onnx flag
    
    * move protobuf install
    
    * cmake dependencies
    
    * added protobuf header include dir + clean-up ng cmake
    
    * added protobuf to ng targets
    
    * fix apple build
    
    * dependencies clean-up
    
    * make onnx_editor static lib
    
    * remove onnx editor visibility
    
    * fixed protobuf export enablement
    
    * fix apple build
    
    * test build protobuf by fetch content
    
    * fix apple protobuf build
    
    * Docs update
    
    * Producer name update in test onnx models
    
    * More comments in the subgraph extraction code
    
    * Merge remote-tracking branch 'upstream/master' into mbencer/MoveEditorToSeparateLib
    
    * add dependency to onnx_proto for test utils
    
    * dependency to ngraph_test_util
    
    * ngraph_test_util dependency. part. 2
    
    * conditional dependency of onnx to ngraph_test_util
    
    * Apply suggestions from code review
    
    Co-authored-by: Tomasz Socha <tomasz.socha@intel.com>
    
    * review remarks
    
    * remove onnx_proto default visibility
    
    * camke remarks, not exporting onnx_common and onnx_editor to target set
    
    * [test fix macos CI] Revert "remove onnx_proto default visibility"
    
    * set onnx_proto visibility only for macos
    
    * [PyPI] remove rpath for dylib
    
    * Corrected cmd
    
    * fix protobuf-lite linking
    
    Co-authored-by: tomdol <tomasz.dolbniak@intel.com>
    Co-authored-by: Tomasz Jankowski <tomasz1.jankowski@intel.com>
    Co-authored-by: Mateusz Tabaka <mateusz.tabaka@intel.com>
    Co-authored-by: Tomasz Socha <tomasz.socha@intel.com>
    Co-authored-by: mryzhov <mikhail.ryzhov@intel.com>

--- ngraph/frontend/onnx_editor/src/detail/subgraph_extraction.hpp
+++ ngraph/frontend/onnx_editor/src/detail/subgraph_extraction.hpp
@@ -22,6 +22,8 @@
 #include <unordered_map>
 #include <vector>
 
+#include "onnx_editor/editor_types.hpp"
+
 namespace ONNX_NAMESPACE
 {
     class GraphProto;
@@ -31,58 +33,8 @@ namespace ONNX_NAMESPACE
 
 namespace ngraph
 {
-    enum class EdgeType
+    namespace onnx_editor
     {
-        INPUT,
-        OUTPUT
-    };
-
-    template <EdgeType>
-    struct Edge
-    {
-        Edge() = delete;
-        Edge(const int node_idx, std::string tensor_name)
-            : m_node_idx{node_idx}
-            , m_tensor_name{std::move(tensor_name)}
-        {
-        }
-
-        const int m_node_idx;
-        const std::string m_tensor_name;
-    };
-    namespace onnx_import
-    {
-        /// \brief Defines an edge connected to an input of any node in the graph.
-        ///        It consists of a node index in the processed ONNX model and the input name.
-        ///        The index should point to a node in the topological sort of the underlying graph
-        ///        which means it has to be in range:  0 <= node_idx < graph.node_size()
-        ///
-        ///        For a node number 5, with 3 inputs:
-        ///
-        ///            ----(in_A)---->  +--------+
-        ///            ----(in_B)---->  | node 5 |  ----(out)---->
-        ///            ----(in_C)---->  +--------+
-        ///
-        ///        there are 3 possible valid instances of this struct:
-        ///            InputEdge(5, "in_A")
-        ///            InputEdge(5, "in_B")
-        ///            InputEdge(5, "in_C")
-        using InputEdge = Edge<EdgeType::INPUT>;
-
-        /// \brief Defines an edge connected to an output of any node in the graph.
-        ///        It consists of a node index in the processed ONNX model and the output name.
-        ///
-        ///        For a node number 5, with 2 outputs:
-        ///
-        ///                             +--------+  ----(out1)---->
-        ///            ----(in_A)---->  | node 5 |
-        ///                             +--------+  ----(out2)---->
-        ///
-        ///        there are 2 possible valid instances of this struct:
-        ///            OutputEdge(5, "out1")
-        ///            OutputEdge(5, "out2")
-        using OutputEdge = Edge<EdgeType::OUTPUT>;
-
         /// \brief Subgraph extraction helper structure
         struct SubgraphExtractor
         {
@@ -160,5 +112,5 @@ namespace ngraph
             /// \param subgraph An object describing the subgraph to be extracted (elems to be kept)
             void extract_subgraph_from_onnx_model(const SubgraphComponents& subgraph);
         };
-    } // namespace onnx_import
+    } // namespace onnx_editor
 } // namespace ngraph
