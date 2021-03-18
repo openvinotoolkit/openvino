55d9987b32e3784214b05a7ff2885e052e50a3da
Maxim Andronov <maxim.andronov@intel.com>
2021-03-18 10:35:59 +0300

    [CPU] Deconvolution node migration on nGraph (#29)

--- inference-engine/src/mkldnn_plugin/mkldnn_graph_optimizer.h
+++ inference-engine/src/mkldnn_plugin/mkldnn_graph_optimizer.h
@@ -20,6 +20,7 @@ public:
 
 private:
     void FuseConvolutionAndBias(MKLDNNGraph &graph);
+    void FuseDeconvolutionAndSimpleOperation(MKLDNNGraph &graph);
     void FuseMultiplyAndAdd(MKLDNNGraph &graph);
     void MergeTwoEqualScaleShifts(MKLDNNGraph& graph);
     void FuseFullyConnectedAndSimpleOperation(MKLDNNGraph &graph);
