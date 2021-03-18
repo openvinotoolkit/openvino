064377092da97b7c2e11f60154d250e77764497e
Maxim Andronov <maxim.andronov@intel.com>
2021-03-18 19:21:39 +0300

    [CPU] ROIPooling, PSROIPooling, ROIAlign nodes migration on nGraph (#32)

--- inference-engine/src/mkldnn_plugin/mkldnn_node.h
+++ inference-engine/src/mkldnn_plugin/mkldnn_node.h
@@ -28,6 +28,7 @@
 #include <ngraph/ops.hpp>
 #include <ngraph/node.hpp>
 #include <ie_precision.hpp>
+#include "cpu_types.h"
 
 namespace MKLDNNPlugin {
 
@@ -95,72 +96,6 @@ enum Type {
     ReduceSumSquare,
     Reference,
     Roll
-};
-
-enum Algorithm {
-    Undefined,
-
-    // Pooling algorithms
-    PoolingMax,
-    PoolingAvg,
-
-    // Convolution algorithms
-    ConvolutionCommon,
-    ConvolutionGrouped,
-
-    // Deconvolution algorithms
-    DeconvolutionCommon,
-    DeconvolutionGrouped,
-
-    // Elementwise algorithms
-    EltwiseAdd,
-    EltwiseMultiply,
-    EltwiseSubtract,
-    EltwiseDivide,
-    EltwiseFloorMod,
-    EltwiseMod,
-    EltwiseMaximum,
-    EltwiseMinimum,
-    EltwiseSquaredDifference,
-    EltwisePowerDynamic,
-    EltwisePowerStatic,
-    EltwiseMulAdd,
-    EltwiseEqual,
-    EltwiseNotEqual,
-    EltwiseGreater,
-    EltwiseGreaterEqual,
-    EltwiseLess,
-    EltwiseLessEqual,
-    EltwiseLogicalAnd,
-    EltwiseLogicalOr,
-    EltwiseLogicalXor,
-    EltwiseLogicalNot,
-    EltwiseRelu,
-    EltwiseGelu,
-    EltwiseElu,
-    EltwiseTanh,
-    EltwiseSigmoid,
-    EltwiseSquare, // TODO [NM]: looks like unused - remove
-    EltwiseAbs,
-    EltwiseSqrt,
-    EltwiseLinear, // TODO [NM]: looks like unused - remove
-    EltwiseBoundedRelu, // TODO [NM]: looks like unused - remove
-    EltwiseSoftRelu, // TODO [NM]: looks like unused - remove
-    EltwiseRelu6, // TODO [NM]: looks like unused - remove
-    EltwiseExp,
-    EltwiseClamp,
-    EltwiseSwish,
-    EltwisePrelu,
-    EltwiseMish,
-    EltwiseHswish,
-    EltwiseHsigmoid,
-    EltwiseRoundHalfToEven,
-    EltwiseRoundHalfAwayFromZero,
-
-    // FakeQuantize algorithms
-    FQCommon,
-    FQQuantization,
-    FQBinarization
 };
 
 Type TypeFromName(const std::string type);
