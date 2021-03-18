e77fbb41eee7003e92bbcb504a2fab504a86877a
VGavrilov <vladimir.gavrilov@intel.com>
2021-03-18 11:02:27 +0300

    Deleted commented code.

--- ngraph/test/type_prop/dft.cpp
+++ ngraph/test/type_prop/dft.cpp
@@ -190,66 +190,6 @@ INSTANTIATE_TEST_CASE_P(
             {600, -1, 40}}),
     PrintToDummyParamName());
 
-// struct ConstantAxesAndConstantSignalSizeTestParams
-// {
-//     PartialShape input_shape;
-//     Shape axes_shape;
-//     Shape signal_size_shape;
-//     PartialShape ref_output_shape;
-//     std::vector<int64_t> axes;
-//     std::vector<int64_t> signal_size;
-// };
-//
-// struct ConstantAxesAndConstantSignalSizeTest
-//     : ::testing::TestWithParam<ConstantAxesAndConstantSignalSizeTestParams>
-// {
-// };
-//
-// TEST_P(ConstantAxesAndConstantSignalSizeTest, dft_constant_axes_and_constant_signal_size)
-// {
-//     auto params = GetParam();
-//
-//     auto data = std::make_shared<op::Parameter>(element::f32, params.input_shape);
-//     auto axes_input = op::Constant::create<int64_t>(element::i64, params.axes_shape, params.axes);
-//     auto signal_size_input =
-//         op::Constant::create<int64_t>(element::i64, params.signal_size_shape, params.signal_size);
-//     auto dft = std::make_shared<op::v7::DFT>(data, axes_input, signal_size_input);
-//
-//     EXPECT_EQ(dft->get_element_type(), element::f32);
-//     ASSERT_TRUE(dft->get_output_partial_shape(0).same_scheme(params.ref_output_shape));
-// }
-//
-// INSTANTIATE_TEST_CASE_P(
-//     type_prop,
-//     ConstantAxesAndConstantSignalSizeTest,
-//     ::testing::Values(
-//         ConstantAxesAndConstantSignalSizeTestParams{
-//             {2, 180, 180, 2}, {2}, {2}, {2, 180, 77, 2}, {1, 2}, {-1, 77}},
-//         ConstantAxesAndConstantSignalSizeTestParams{
-//             {2, 180, 180, 2}, {2}, {2}, {87, 180, 390, 2}, {2, 0}, {390, 87}},
-//         ConstantAxesAndConstantSignalSizeTestParams{
-//             {7, 50, 130, 400, 2}, {3}, {3}, {7, 40, 130, 600, 2}, {3, 0, 1}, {600, -1, 40}},
-//         ConstantAxesAndConstantSignalSizeTestParams{{2, Dimension(0, 200), 180, 2},
-//                                                     {2},
-//                                                     {2},
-//                                                     {2, Dimension(0, 200), 77, 2},
-//                                                     {1, 2},
-//                                                     {-1, 77}},
-//         ConstantAxesAndConstantSignalSizeTestParams{{Dimension(0, 18), 180, Dimension(0, 400), 2},
-//                                                     {2},
-//                                                     {2},
-//                                                     {87, 180, 390, 2},
-//                                                     {2, 0},
-//                                                     {390, 87}},
-//         ConstantAxesAndConstantSignalSizeTestParams{
-//             {Dimension(8, 129), 50, 130, Dimension(0, 500), 2},
-//             {3},
-//             {3},
-//             {Dimension(8, 129), 40, 130, 600, 2},
-//             {3, 0, 1},
-//             {600, -1, 40}}),
-//     PrintToDummyParamName());
-
 TEST(type_prop, dft_constant_axes_and_there_are_no_signal_size_dynamic_shapes2)
 {
     struct ShapesAndValues
