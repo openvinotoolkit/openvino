b0a049cd22c2bdcf459ec1cf570aa0f661c86b7d
Alexandra Sidorova <alexandra.sidorova@intel.com>
2021-03-18 19:07:29 +0300

    [CPU][IE TESTS] Added VariadicSplit and Pad subgraph tests (#4583)
    * [IE TESTS] Added VariadicSplit and Pad subgraph tests
    
    * Added more connectedIndexes

---
+++ inference-engine/tests/functional/plugin/shared/include/subgraph_tests/variadic_split_pad.hpp
@@ -0,0 +1,14 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+#pragma once
+
+#include "shared_test_classes/subgraph/variadic_split_pad.hpp"
+
+namespace SubgraphTestsDefinitions {
+
+TEST_P(VariadicSplitPad, CompareWithRefs){
+    Run();
+};
+
+}  // namespace SubgraphTestsDefinitions
