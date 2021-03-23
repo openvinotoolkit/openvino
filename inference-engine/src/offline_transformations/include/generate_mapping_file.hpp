6141477ef28773859ca402421897bdb823312d9a
Gleb Kazantaev <gleb.kazantaev@intel.com>
2021-03-23 18:32:43 +0300

    Test mapping file generation

--- inference-engine/src/offline_transformations/include/generate_mapping_file.hpp
+++ inference-engine/src/offline_transformations/include/generate_mapping_file.hpp
@@ -22,9 +22,11 @@ class GenerateMappingFile;
 
 class ngraph::pass::GenerateMappingFile: public ngraph::pass::FunctionPass {
     std::string m_path_to_file;
+    bool m_extract_name;
 public:
     NGRAPH_RTTI_DECLARATION;
-    explicit GenerateMappingFile(const std::string & path) : m_path_to_file(path) {}
+    explicit GenerateMappingFile(const std::string & path, bool extract_name = true)
+        : m_path_to_file(path), m_extract_name(extract_name) {}
 
     bool run_on_function(std::shared_ptr<ngraph::Function>) override;
 };
--- inference-engine/src/offline_transformations/src/generate_mapping_file.cpp
+++ inference-engine/src/offline_transformations/src/generate_mapping_file.cpp
@@ -16,20 +16,41 @@ bool ngraph::pass::GenerateMappingFile::run_on_function(std::shared_ptr<ngraph::
     pugi::xml_document xml_doc;
     pugi::xml_node root_node = xml_doc.append_child("mapping");
 
+    auto add_mapping = [&](const std::string & fw_name, const std::string & fw_port_name,
+                           const std::string & ir_name, const std::string & ir_port_name) {
+        auto map_node = root_node.append_child("map");
+        auto framework_node = map_node.append_child("framework");
+        auto ir_node = map_node.append_child("IR");
+
+        framework_node.append_attribute("name").set_value(fw_name.c_str());
+        framework_node.append_attribute("output_port_id").set_value(fw_port_name.c_str());
+
+        ir_node.append_attribute("name").set_value(ir_name.c_str());
+        ir_node.append_attribute("output_port_id").set_value(ir_port_name.c_str());
+    };
+
+    auto extract_name = [](const std::string & port_name) -> std::string {
+        std::string res;
+        for (auto ch : port_name) {
+            if (ch == ':') break;
+            res += ch;
+        }
+        return res;
+    };
+
     for (auto && node : f->get_ordered_ops()) {
         uint64_t port_index{node->inputs().size()};
         for (auto && output : node->outputs()) {
             const auto & t = output.get_tensor_ptr();
             for (const auto & port_name : t->get_names()) {
-                auto map_node = root_node.append_child("map");
-                auto framework_node = map_node.append_child("framework");
-                auto ir_node = map_node.append_child("IR");
+                const auto & node_name = node->get_friendly_name();
+                add_mapping(node_name, port_name, node_name, std::to_string(port_index));
 
-                framework_node.append_attribute("name").set_value(node->get_friendly_name().c_str());
-                framework_node.append_attribute("output_port_id").set_value(port_name.c_str());
-
-                ir_node.append_attribute("name").set_value(node->get_friendly_name().c_str());
-                ir_node.append_attribute("output_port_id").set_value(port_index);
+                if (m_extract_name) {
+                    for (auto &name : t->get_names()) {
+                        add_mapping(extract_name(name), port_name, node_name, std::to_string(port_index));
+                    }
+                }
             }
             ++port_index;
         }
