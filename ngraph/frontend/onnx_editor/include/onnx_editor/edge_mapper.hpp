e6a4d8bcc0d05f1f93e3ad8666b42afc649863ea
mbencer <mateusz.bencer@intel.com>
2021-03-24 13:34:51 +0100

    EdgeMapper documentation

--- ngraph/frontend/onnx_editor/include/onnx_editor/edge_mapper.hpp
+++ ngraph/frontend/onnx_editor/include/onnx_editor/edge_mapper.hpp
@@ -33,14 +33,53 @@ namespace ngraph
 {
     namespace onnx_editor
     {
+        /// \brief A class allows to determine InputEdge and OutputEdge by user-friendly onnx names.
         class EdgeMapper
         {
         public:
             EdgeMapper() = delete;
+
+            /// \brief Creates an edge mapper based on graph_proto state.
+            ///
+            /// \note If state of graph_proto will be changed, the information
+            ///       from edge mapper is outdated.
+            ///
+            /// \param graph_proto Reference to GraphProto object.
             EdgeMapper(const ONNX_NAMESPACE::GraphProto& graph_proto);
 
-            InputEdge to_input_edge(Node node, Input in) const;
-            OutputEdge to_output_edge(Node node, Output out) const;
+            /// \brief Returns the InputEdge based on a node (node name or output name)
+            ///        and an input (input name or input index).
+            ///
+            /// \note  The node name can be ambiguous (many nodes can have the same name).
+            ///        In such a case the algorthim try to match the given node name
+            ///        with the input name (given input index is not enough).
+            ///        If after such operation a found edge is unique, it is returned.
+            ///        If InputEdge cannot be determined based on given params the ngraph_error
+            ///        exception is thrown.
+            ///
+            /// \param node A node helper structure created based on a node name
+            ///             or a node output name.
+            ///
+            /// \param input A input helper structure created based on a input name
+            ///              or a input index.
+            InputEdge to_input_edge(Node node, Input input) const;
+
+            /// \brief Returns the OutputEdge based on a node (node name or output name)
+            ///        and an output (output name or output index).
+            ///
+            /// \note  The node name can be ambiguous (many nodes can have the same name).
+            ///        In such a case the algorthim try to match the given node name
+            ///        with the output name (given output index is not enough).
+            ///        If after such operation a found edge is unique, it is returned.
+            ///        If OutputEdge cannot be determined based on given params the ngraph_error
+            ///        exception is thrown.
+            ///
+            /// \param node A node helper structure created based on a node name
+            ///             or a node output name.
+            ///
+            /// \param output A output helper structure created based on a output name
+            ///               or a output index.
+            OutputEdge to_output_edge(Node node, Output output) const;
 
         private:
             std::vector<int> find_node_indexes(const std::string& node_name,
