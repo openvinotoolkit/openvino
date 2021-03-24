0173ccb92200c460cae276a487d03167ad353931
Vladislav Golubev <vladislav.golubev@intel.com>
2021-03-24 17:48:15 +0300

    [LPT][TESTS] ShuffleChannelsTransformation plugin tests

--- inference-engine/tests/functional/inference_engine/lp_transformations/shuffle_channles_transformation.cpp
+++ inference-engine/tests/functional/inference_engine/lp_transformations/shuffle_channles_transformation.cpp
@@ -143,8 +143,8 @@ const std::vector<ShuffleChannelsTransformationTestValues> testValues = {
             ngraph::element::u8,
             {
                 {ngraph::element::f32},
-                {{121.f, 122.f, 123.f, 124.f, 125.f, 126.f, 127.f, 128.f}, ngraph::element::f32, ngraph::Shape{ 1, 1, 8, 1 }},
-                {{1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f}, ngraph::element::f32, ngraph::Shape{ 1, 1, 8, 1 }}
+                {{121.f, 122.f, 123.f, 124.f, 125.f, 126.f, 127.f, 128.f}, ngraph::element::f32, ngraph::Shape{1, 1, 8, 1}},
+                {{1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f}, ngraph::element::f32, ngraph::Shape{1, 1, 8, 1}}
             }
         },
         {
@@ -215,8 +215,8 @@ const std::vector<ShuffleChannelsTransformationTestValues> testValues = {
             ngraph::element::i8,
             {
                 {ngraph::element::f32},
-                {{121.f, 122.f, 123.f, 124.f, 125.f, 126.f, 127.f, 128.f}, ngraph::element::f32, ngraph::Shape{ 1, 1, 8, 1 }},
-                {{1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f}, ngraph::element::f32, ngraph::Shape{ 1, 1, 8, 1 }}
+                {{121.f, 122.f, 123.f, 124.f, 125.f, 126.f, 127.f, 128.f}, ngraph::element::f32, ngraph::Shape{1, 1, 8, 1}},
+                {{1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f}, ngraph::element::f32, ngraph::Shape{1, 1, 8, 1}}
             }
         },
         {
