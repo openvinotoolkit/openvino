dafc636dd9335cb670580186daf56e1c06fc5393
Andrew Bakalin <andrew.bakalin@intel.com>
2021-03-24 12:57:02 +0300

    [VPU] Add option to disable preprocessing check inside the model (#4616)

--- inference-engine/src/vpu/graph_transformer/src/middleend/passes/weights_analysis.cpp
+++ inference-engine/src/vpu/graph_transformer/src/middleend/passes/weights_analysis.cpp
@@ -87,6 +87,11 @@ bool isScalable(const Stage& stage) {
 }
 
 bool checkGrowingOutput(const Model& model) {
+    const auto& env = CompileEnv::get();
+    if (!env.config.checkPreprocessingInsideModel) {
+        return false;
+    }
+
     static const float SCALE_THRESHOLD = 0.125f;
 
     for (const auto& stage : model->getStages()) {
