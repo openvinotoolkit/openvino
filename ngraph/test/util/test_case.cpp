69acacb324d351687da8956106c1d0591254d1ac
tomdol <tomasz.dolbniak@intel.com>
2021-03-25 10:14:07 -0400

    Run IR models via TestCase

---
+++ ngraph/test/util/test_case.cpp
@@ -0,0 +1,17 @@
+#include <ie_core.hpp>
+
+#include "test_case.hpp"
+
+namespace ngraph
+{
+    namespace test
+    {
+        std::shared_ptr<Function> function_from_ir(const std::string& xml_path,
+                                                   const std::string& bin_path)
+        {
+            InferenceEngine::Core c;
+            auto network = c.ReadNetwork(xml_path, bin_path);
+            return network.getFunction();
+        }
+    }
+}
