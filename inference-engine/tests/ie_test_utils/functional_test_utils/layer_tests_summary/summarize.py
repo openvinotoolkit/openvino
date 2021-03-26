9a66ff4137042bd5589e471ce0c27023830f43c5
Alexey Suhov <alexey.suhov@intel.com>
2021-03-26 17:54:28 +0300

    Align copyright notice in python scripts (CVS-51320) (#4974)
    * Align copyright notice in python scripts (CVS-51320)

--- inference-engine/tests/ie_test_utils/functional_test_utils/layer_tests_summary/summarize.py
+++ inference-engine/tests/ie_test_utils/functional_test_utils/layer_tests_summary/summarize.py
@@ -1,6 +1,5 @@
-# Copyright (C) 2021 Intel Corporation
+# Copyright (C) 2018-2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-#
 
 import xml.etree.ElementTree as ET
 from jinja2 import Environment, FileSystemLoader
