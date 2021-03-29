d12dbc100736710d4a353946e1d4cccd02c179b6
dpigasin <dmitry.pigasin@intel.com>
2021-03-29 16:55:31 +0300

    Add comments to the hello query device sample

--- inference-engine/ie_bridges/python/sample/hello_query_device/hello_query_device.py
+++ inference-engine/ie_bridges/python/sample/hello_query_device/hello_query_device.py
@@ -8,7 +8,8 @@ import sys
 from openvino.inference_engine import IECore
 
 
-def metric_to_string(metric) -> str:
+def param_to_string(metric) -> str:
+    '''Convert a list / tuple of parameters returned from IE to a string'''
     if isinstance(metric, (list, tuple)):
         return ', '.join([str(x) for x in metric])
     else:
@@ -18,20 +19,24 @@ def metric_to_string(metric) -> str:
 def main():
     log.basicConfig(format='[ %(levelname)s ] %(message)s', level=log.INFO, stream=sys.stdout)
 
+# ---------------------------Initialize inference engine core----------------------------------------------------------
+    log.info('Creating Inference Engine')
     ie = IECore()
 
+# ---------------------------Get metrics of available devices----------------------------------------------------------
     for device in ie.available_devices:
         log.info(f'{device} :')
         for metric in ie.get_metric(device, 'SUPPORTED_METRICS'):
             if metric not in ('SUPPORTED_METRICS', 'SUPPORTED_CONFIG_KEYS'):
-                log.info(f'\t{metric}: {metric_to_string(ie.get_metric(device, metric))}')
+                log.info(f'\t{metric}: {param_to_string(ie.get_metric(device, metric))}')
         log.info('')
 
         log.info('\tSUPPORTED_CONFIG_KEYS (default values):')
         for config_key in ie.get_metric(device, 'SUPPORTED_CONFIG_KEYS'):
-            log.info(f'\t\t{config_key}: {metric_to_string(ie.get_config(device, config_key))}')
+            log.info(f'\t\t{config_key}: {param_to_string(ie.get_config(device, config_key))}')
         log.info('')
 
+# ----------------------------------------------------------------------------------------------------------------------
     return 0
 
 
