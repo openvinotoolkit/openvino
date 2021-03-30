682cb0aeb866563cf20f383f062e8692f41289b4
Alexey Lebedev <alexey.lebedev@intel.com>
2021-03-30 16:49:04 +0300

    add parametrize in blob tests

--- ngraph/python/tests/test_inference_engine/test_blob.py
+++ ngraph/python/tests/test_inference_engine/test_blob.py
@@ -40,7 +40,22 @@ def test_get_buffer():
     blob = Blob(tensor_desc, array)
     assert np.array_equal(blob.buffer, array)
 
-def write_to_buffer(precision, numpy_precision):
+
+@pytest.mark.parametrize("precision, numpy_precision",[
+    ("FP32", np.float32),
+    ("FP64", np.float64),
+    ("FP16", np.float16),
+    ("I8", np.int8),
+    ("U8", np.uint8),
+    ("I32", np.int32),
+    ("I16", np.int16),
+    ("U16", np.uint16),
+    ("I64", np.int64),
+    ("BOOL", np.uint8),
+    ("BIN", np.int8),
+    ("BF16", np.float16),
+])
+def test_write_to_buffer(precision, numpy_precision):
     tensor_desc = TensorDesc(precision, [1, 3, 127, 127], "NCHW")
     array = np.zeros(shape=(1, 3, 127, 127), dtype=numpy_precision)
     blob = Blob(tensor_desc, array)
@@ -48,13 +63,6 @@ def write_to_buffer(precision, numpy_precision):
     blob.buffer[:] = ones_arr
     assert np.array_equal(blob.buffer, ones_arr)
 
-
-def test_write_to_buffer_fp32():
-     write_to_buffer("FP32", np.float32)
-
-
-def test_write_to_buffer_fp64():
-    write_to_buffer("FP64", np.float64)
 
 tensor_desc_12 = TensorDesc("FP32", [1, 3, 127, 127], "NCHW")
 array_12 = np.zeros(shape=(1, 3, 127, 127), dtype=np.float32)
@@ -72,46 +80,6 @@ def test_l():
     for i in range(10):
         f()
     assert np.array_equal(blob_12.buffer, f())
-
-@pytest.mark.skip(reason="Need to figure out how to implement right conversion")
-def test_write_to_buffer_fp16():
-    write_to_buffer("FP16",np.float16)
-
-
-def test_write_to_buffer_int8():
-    write_to_buffer("I8", np.int8)
-
-
-def test_write_to_buffer_uint8():
-    write_to_buffer("U8", np.uint8)
-
-
-def test_write_to_buffer_int32():
-    write_to_buffer("I32", np.int32)
-
-
-def test_write_to_buffer_int16():
-    write_to_buffer("I16", np.int16)
-
-
-def test_write_to_buffer_uint16():
-    write_to_buffer("U16", np.uint16)
-
-
-def test_write_to_buffer_int64():
-    write_to_buffer("I64", np.int64)
-
-
-def test_write_to_buffer_bool():
-    write_to_buffer("BOOL", np.uint8)
-
-
-def test_write_to_buffer_bin():
-    write_to_buffer("BIN", np.int8)
-
-
-def test_write_to_buffer_bf16():
-    write_to_buffer("BF16", np.float16)
 
 
 def test_write_numpy_scalar_int64():
