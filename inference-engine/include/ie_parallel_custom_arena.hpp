ba88a966d5dabfa9d4fc59a4ba19a42180a38fdc
Kochin, Ivan <ivan.kochin@intel.com>
2021-03-31 17:55:39 +0300

    Get back to the custom::task_arena interface

---
+++ inference-engine/include/ie_parallel_custom_arena.hpp
@@ -0,0 +1,153 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+/**
+ * @brief Contains declarations and custom threading interfaces based on TBB info and task_arena APIs.
+ *
+ * @file ie_parallel_custom.hpp
+ */
+
+#pragma once
+
+#include <cstddef>
+#include <type_traits>
+
+#define IE_THREAD_TBB 0
+#define IE_THREAD_OMP 1
+#define IE_THREAD_SEQ 2
+#define IE_THREAD_TBB_AUTO 3
+
+#if (IE_THREAD == IE_THREAD_TBB || IE_THREAD == IE_THREAD_TBB_AUTO)
+#ifndef NOMINMAX
+# define NOMINMAX
+#endif
+#ifndef TBB_PREVIEW_LOCAL_OBSERVER
+# define TBB_PREVIEW_LOCAL_OBSERVER 1
+#endif
+#ifndef TBB_PREVIEW_NUMA_SUPPORT
+# define TBB_PREVIEW_NUMA_SUPPORT 1
+#endif
+
+#include <mutex>
+
+#include "tbb/task_arena.h"
+#include "tbb/task_scheduler_observer.h"
+
+namespace custom {
+#define TBB_NUMA_SUPPORT_PRESENT TBB_INTERFACE_VERSION >= 11100 || defined(TBBBBIND_2_4_AVAILABLE)
+#define TBB_HYBRID_CPUS_SUPPORT_PRESENT defined(TBBBBIND_2_4_AVAILABLE)
+
+#if defined(TBBBBIND_2_4_AVAILABLE)
+using numa_node_id = int;
+using core_type_id = int;
+
+namespace detail {
+struct constraints {
+    constraints() = default;
+    constraints(numa_node_id id, int maximal_concurrency)
+        : numa_id{id}
+        , max_concurrency{maximal_concurrency}
+        , core_type{-1}
+        , max_threads_per_core{-1}
+    {}
+
+    constraints& set_numa_id(numa_node_id id) {
+        numa_id = id;
+        return *this;
+    }
+    constraints& set_max_concurrency(int maximal_concurrency) {
+        max_concurrency = maximal_concurrency;
+        return *this;
+    }
+
+    constraints& set_core_type(core_type_id id) {
+        core_type = id;
+        return *this;
+    }
+    constraints& set_max_threads_per_core(int threads_number) {
+        max_threads_per_core = threads_number;
+        return *this;
+    }
+
+    numa_node_id numa_id = -1;
+    int max_concurrency = -1;
+    core_type_id core_type = -1;
+    int max_threads_per_core = -1;
+};
+
+class binding_handler;
+
+class binding_observer : public tbb::task_scheduler_observer {
+    binding_handler* my_binding_handler;
+public:
+    binding_observer(tbb::task_arena& ta, int num_slots, const constraints& c);
+    void on_scheduler_entry(bool) override;
+    void on_scheduler_exit(bool) override;
+    ~binding_observer();
+};
+} // namespace detail
+
+class task_arena {
+    tbb::task_arena my_task_arena;
+    std::once_flag my_initialization_state;
+    detail::constraints my_constraints;
+    detail::binding_observer* my_binding_observer;
+
+public:
+    using constraints = detail::constraints;
+    static const int automatic = tbb::task_arena::automatic;
+
+    task_arena(int max_concurrency_ = automatic, unsigned reserved_for_masters = 1);
+    task_arena(const constraints& constraints_, unsigned reserved_for_masters = 1);
+    task_arena(const task_arena &s);
+
+    void initialize();
+    void initialize(int max_concurrency_, unsigned reserved_for_masters = 1);
+    void initialize(constraints constraints_, unsigned reserved_for_masters = 1);
+
+    //TODO: Make custom::task_arena inherited from tbb::task_arena
+    operator tbb::task_arena&() { return my_task_arena; }
+
+    int max_concurrency();
+
+    template<typename F>
+    void enqueue(F&& f) {
+        initialize();
+        my_task_arena.enqueue(std::forward<F>(f));
+    }
+    template<typename F>
+    auto execute(F&& f) -> decltype(f()) {
+        initialize();
+        return my_task_arena.execute(std::forward<F>(f));
+    }
+
+    ~task_arena();
+
+    friend tbb::task_arena& get_arena_ref(task_arena& ta);
+};
+
+namespace info {
+    std::vector<numa_node_id> numa_nodes();
+    int default_concurrency(numa_node_id id = task_arena::automatic);
+    std::vector<core_type_id> core_types();
+    int default_concurrency(task_arena::constraints c);
+} // namespace info
+
+inline tbb::task_arena& get_arena_ref(task_arena& ta) { return ta.my_task_arena; }
+
+#else /*defined(TBBBBIND_2_4_AVAILABLE)*/
+
+using task_arena = tbb::task_arena;
+inline tbb::task_arena& get_arena_ref(task_arena& ta) { return ta; }
+
+#if TBB_NUMA_SUPPORT_PRESENT
+using numa_node_id = tbb::numa_node_id;
+namespace info {
+    using namespace tbb::info;
+} // namespace info
+#endif /*TBB_NUMA_SUPPORT_PRESENT*/
+
+#endif /*defined(TBBBBIND_2_4_AVAILABLE)*/
+} // namespace custom
+#endif /*(IE_THREAD == IE_THREAD_TBB || IE_THREAD == IE_THREAD_TBB_AUTO)*/
--- inference-engine/src/inference_engine/threading/ie_cpu_streams_executor.cpp
+++ inference-engine/src/inference_engine/threading/ie_cpu_streams_executor.cpp
@@ -13,153 +13,15 @@
 #include <climits>
 #include <cassert>
 #include <utility>
-#include <iostream>
 
 #include "threading/ie_thread_local.hpp"
-#include "ie_parallel.hpp"
+#include "ie_parallel_custom_arena.hpp"
 #include "ie_system_conf.h"
 #include "threading/ie_thread_affinity.hpp"
 #include "threading/ie_cpu_streams_executor.hpp"
 #include <openvino/itt.hpp>
 
 using namespace openvino;
-
-#if IE_THREAD == IE_THREAD_TBB || IE_THREAD == IE_THREAD_TBB_AUTO
-namespace custom {
-namespace detail {
-
-extern "C" {
-void __TBB_internal_initialize_system_topology(
-    std::size_t groups_num,
-    int& numa_nodes_count, int*& numa_indexes_list,
-    int& core_types_count, int*& core_types_indexes_list
-);
-binding_handler* __TBB_internal_allocate_binding_handler(int number_of_slots, int numa_id, int core_type_id, int max_threads_per_core);
-void __TBB_internal_deallocate_binding_handler(binding_handler* handler_ptr);
-void __TBB_internal_apply_affinity(binding_handler* handler_ptr, int slot_num);
-void __TBB_internal_restore_affinity(binding_handler* handler_ptr, int slot_num);
-int __TBB_internal_get_default_concurrency(int numa_id, int core_type_id, int max_threads_per_core);
-}
-
-int get_processors_group_num() {
-#if defined(_WIN32) || defined(_WIN64)
-    SYSTEM_INFO si;
-    GetNativeSystemInfo(&si);
-
-    DWORD_PTR pam, sam, m = 1;
-    GetProcessAffinityMask(GetCurrentProcess(), &pam, &sam);
-    int nproc = 0;
-    for (std::size_t i = 0; i < sizeof(DWORD_PTR) * CHAR_BIT; ++i, m <<= 1) {
-        if ( pam & m )
-            ++nproc;
-    }
-    if (nproc == static_cast<int>(si.dwNumberOfProcessors)) {
-        return GetActiveProcessorGroupCount();
-    }
-#endif
-    return 1;
-}
-
-bool is_binding_environment_valid() {
-#if defined(_WIN32) && !defined(_WIN64)
-    static bool result = [] {
-        // For 32-bit Windows applications, process affinity masks can only support up to 32 logical CPUs.
-        SYSTEM_INFO si;
-        GetNativeSystemInfo(&si);
-        if (si.dwNumberOfProcessors > 32) return false;
-        return true;
-    }();
-    return result;
-#else
-    return true;
-#endif /* _WIN32 && !_WIN64 */
-}
-
-int  numa_nodes_count = 0;
-int* numa_nodes_indexes = nullptr;
-
-int  core_types_count = 0;
-int* core_types_indexes = nullptr;
-
-void initialize_system_topology() {
-    static std::once_flag is_topology_initialized;
-
-    std::call_once(is_topology_initialized, [&]{
-        if (is_binding_environment_valid()) {
-            __TBB_internal_initialize_system_topology(
-                get_processors_group_num(),
-                numa_nodes_count, numa_nodes_indexes,
-                core_types_count, core_types_indexes);
-        } else {
-            static int dummy_index = tbb::task_arena::automatic;
-
-            numa_nodes_count = 1;
-            numa_nodes_indexes = &dummy_index;
-
-            core_types_count = 1;
-            core_types_indexes = &dummy_index;
-        }
-    });
-}
-} // namespace detail
-
-binding_observer::binding_observer(tbb::task_arena& ta, int num_slots, int numa_id, core_type_id core_type, int max_threads_per_core)
-    : task_scheduler_observer(ta) {
-    detail::initialize_system_topology();
-    my_binding_handler = detail::__TBB_internal_allocate_binding_handler(num_slots, numa_id, core_type, max_threads_per_core);
-}
-
-void binding_observer::on_scheduler_entry(bool) {
-    detail::__TBB_internal_apply_affinity(my_binding_handler, tbb::this_task_arena::current_thread_index());
-}
-
-void binding_observer::on_scheduler_exit(bool) {
-    detail::__TBB_internal_restore_affinity(my_binding_handler, tbb::this_task_arena::current_thread_index());
-}
-
-binding_observer::~binding_observer() {
-    detail::__TBB_internal_deallocate_binding_handler(my_binding_handler);
-}
-
-binding_observer* construct_binding_observer(tbb::task_arena& ta, int num_slots,
-                                             numa_node_id numa_id = tbb::task_arena::automatic,
-                                             core_type_id core_type = tbb::task_arena::automatic,
-                                             int max_threads_per_core = tbb::task_arena::automatic) {
-    binding_observer* observer = nullptr;
-    if (detail::is_binding_environment_valid() &&
-      ((core_type >= 0 && info::core_types().size() > 1) || (numa_id >= 0 && info::numa_nodes().size() > 1) || max_threads_per_core > 0)) {
-        observer = new binding_observer(ta, num_slots, numa_id, core_type, max_threads_per_core);
-        observer->observe(true);
-    }
-    return observer;
-}
-
-namespace info {
-std::vector<numa_node_id> numa_nodes() {
-    detail::initialize_system_topology();
-    std::vector<numa_node_id> node_indices(detail::numa_nodes_count);
-    std::memcpy(node_indices.data(), detail::numa_nodes_indexes, detail::numa_nodes_count * sizeof(int));
-    return node_indices;
-}
-
-std::vector<core_type_id> core_types() {
-    detail::initialize_system_topology();
-    std::vector<numa_node_id> core_type_indexes(detail::core_types_count);
-    std::memcpy(core_type_indexes.data(), detail::core_types_indexes, detail::core_types_count * sizeof(int));
-    return core_type_indexes;
-}
-
-int default_concurrency(numa_node_id numa_id, core_type_id core_type_id, int max_threads_per_core) {
-    if (detail::is_binding_environment_valid()) {
-        detail::initialize_system_topology();
-        return detail::__TBB_internal_get_default_concurrency(numa_id, core_type_id, max_threads_per_core);
-    }
-    return tbb::this_task_arena::max_concurrency();
-}
-} // namespace info
-} // namespace custom
-
-#endif /*IE_THREAD == IE_THREAD_TBB || IE_THREAD == IE_THREAD_TBB_AUTO*/
 
 namespace InferenceEngine {
 struct CPUStreamsExecutor::Impl {
@@ -209,14 +71,41 @@ struct CPUStreamsExecutor::Impl {
                     ((_impl->_config._streams + _impl->_usedNumaNodes.size() - 1)/_impl->_usedNumaNodes.size()))
                 : _impl->_usedNumaNodes.at(_streamId % _impl->_usedNumaNodes.size());
 #if IE_THREAD == IE_THREAD_TBB || IE_THREAD == IE_THREAD_TBB_AUTO
-            auto latency_core_type = custom::info::core_types().back();
-            _taskArena.reset(new tbb::task_arena{
-                custom::info::default_concurrency(tbb::task_arena::automatic, latency_core_type)});
-            _observer.reset(custom::construct_binding_observer(
-                *_taskArena,
-                _taskArena->max_concurrency(),
-                /*numa_id*/tbb::task_arena::automatic,
-                /*core_type_id*/latency_core_type));
+            auto concurrency = (0 == _impl->_config._threadsPerStream) ? custom::task_arena::automatic : _impl->_config._threadsPerStream;
+            if (true/*ThreadBindingType::Hybrid== _impl->_config._threadBindingType*/) {
+#if TBB_HYBRID_CPUS_SUPPORT_PRESENT
+                auto latency_cores_type = custom::info::core_types().back();
+                _taskArena.reset(new custom::task_arena{
+                    custom::task_arena::constraints{}.
+                        set_core_type(latency_cores_type)
+                });
+#else
+                _taskArena.reset(new custom::task_arena{concurrency});
+#endif
+            } else if (ThreadBindingType::NUMA == _impl->_config._threadBindingType) {
+#if TBB_NUMA_SUPPORT_PRESENT
+                _taskArena.reset(new custom::task_arena{custom::task_arena::constraints{_numaNodeId, concurrency}});
+#else
+                _taskArena.reset(new custom::task_arena{concurrency});
+#endif
+            } else if ((0 != _impl->_config._threadsPerStream) || (ThreadBindingType::CORES == _impl->_config._threadBindingType)) {
+                _taskArena.reset(new custom::task_arena{concurrency});
+                if (ThreadBindingType::CORES == _impl->_config._threadBindingType) {
+                    CpuSet processMask;
+                    int    ncpus = 0;
+                    std::tie(processMask, ncpus) = GetProcessMask();
+                    if (nullptr != processMask) {
+                        _observer.reset(new Observer{*_taskArena,
+                                                     std::move(processMask),
+                                                     ncpus,
+                                                     _streamId,
+                                                     _impl->_config._threadsPerStream,
+                                                     _impl->_config._threadBindingStep,
+                                                     _impl->_config._threadBindingOffset});
+                        _observer->observe(true);
+                    }
+                }
+            }
 #elif IE_THREAD == IE_THREAD_OMP
             omp_set_num_threads(_impl->_config._threadsPerStream);
             if (!checkOpenMpEnvVars(false) && (ThreadBindingType::NONE != _impl->_config._threadBindingType)) {
@@ -261,8 +150,8 @@ struct CPUStreamsExecutor::Impl {
         bool _execute = false;
         std::queue<Task> _taskQueue;
 #if IE_THREAD == IE_THREAD_TBB || IE_THREAD == IE_THREAD_TBB_AUTO
-        std::unique_ptr<tbb::task_arena>          _taskArena;
-        std::unique_ptr<custom::binding_observer> _observer;
+        std::unique_ptr<custom::task_arena>          _taskArena;
+        std::unique_ptr<Observer> _observer;
 #endif
     };
 
