9ee4f7cc73fbff631df157ef9ccf92b6a5719b6b
VGavrilov <vladimir.gavrilov@intel.com>
2021-03-31 14:17:20 +0300

    Small change.

--- ngraph/core/reference/src/runtime/reference/fft.cpp
+++ ngraph/core/reference/src/runtime/reference/fft.cpp
@@ -231,13 +231,13 @@ namespace ngraph
                     {
                         std::cout << s << " ";
                     }
-                    std::cout << "    output_fft_strides: ";
+                    std::cout << "\n    output_fft_strides: ";
                     for (auto s : output_fft_strides)
                     {
                         std::cout << s << " ";
                     }
                     int64_t num_of_fft_axes = static_cast<int64_t>(fft_strides.size()) - 1;
-                    std::cout << "num_of_fft_axes: " << num_of_fft_axes << "\n";
+                    std::cout << "\nnum_of_fft_axes: " << num_of_fft_axes << "\n";
 
                     for (int64_t idx = 0; idx < fft_size; ++idx)
                     {
