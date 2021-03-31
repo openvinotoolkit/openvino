fb5866b39a670ade0d279406a992b7412ceb3795
Kruglov <yegor.kruglov@intel.com>
2021-03-31 13:27:12 +0300

    add an assert

--- model-optimizer/mo/utils/ir_reader/extenders/strided_slice_extender.py
+++ model-optimizer/mo/utils/ir_reader/extenders/strided_slice_extender.py
@@ -1,8 +1,6 @@
 # Copyright (C) 2018-2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-import numpy as np
-
 from mo.front.common.partial_infer.utils import int64_array
 from mo.ops.strided_slice import StridedSlice
 from mo.utils.graph import Node
@@ -14,12 +12,13 @@ class StridedSlice_extender(Extender):
 
     @staticmethod
     def extend(op: Node):
-        input_shape = op.in_port(0).data.get_shape()
         for attr in StridedSlice.get_mask_names():
             if op.has_and_set(attr):
                 Extender.attr_to_list(op, attr)
             else:
-                op[attr] = np.zeros_like(input_shape)
+                assert attr not in ['begin_mask', 'end_mask'],\
+                    '{} is not defined for the node {}'.format(attr, op.soft_get('name', op.id))
+                op[attr] = int64_array([0])
 
         op.begin_mask = int64_array([1 - i for i in op.begin_mask])
         op.end_mask = int64_array([1 - i for i in op.end_mask])
