ac01b257d9230e6c72fb287a2c72c884b0f661cc
jiwaszki <jan.iwaszkiewicz@intel.com>
2021-03-31 11:36:21 +0200

    Fix sync in InferQueue and add default callbacks

--- ngraph/python/src/pyopenvino/inference_engine/ie_infer_queue.cpp
+++ ngraph/python/src/pyopenvino/inference_engine/ie_infer_queue.cpp
@@ -43,14 +43,18 @@ public:
         , _idle_handles(idle_handles)
         , _user_ids(user_ids)
     {
+        this->setDefaultCallbacks();
     }
 
     ~InferQueue() { _requests.clear(); }
 
     size_t getIdleRequestId()
     {
+        // Wait for any of _idle_handles
+        py::gil_scoped_release release;
         std::unique_lock<std::mutex> lock(_mutex);
         _cv.wait(lock, [this] { return !(_idle_handles.empty()); });
+        py::gil_scoped_acquire acquire;
 
         size_t idle_request_id = _idle_handles.front();
         _idle_handles.pop();
@@ -60,10 +64,14 @@ public:
 
     std::vector<InferenceEngine::StatusCode> waitAll()
     {
-        std::vector<InferenceEngine::StatusCode> statuses;
-
+        // Wait for all requests to return with callback thus updating
+        // _idle_handles so it matches the size of requests
+        py::gil_scoped_release release;
         std::unique_lock<std::mutex> lock(_mutex);
         _cv.wait(lock, [this] { return _idle_handles.size() == _requests.size(); });
+        py::gil_scoped_acquire acquire;
+
+        std::vector<InferenceEngine::StatusCode> statuses;
 
         for (size_t handle = 0; handle < _requests.size(); handle++)
         {
@@ -74,21 +82,32 @@ public:
         return statuses;
     }
 
+    void setDefaultCallbacks()
+    {
+        py::function f_callback;
+        for (size_t handle = 0; handle < _requests.size(); handle++)
+        {
+            _requests[handle].SetCompletionCallback([this, handle /* ... */]() {
+                py::gil_scoped_acquire acquire;
+                _idle_handles.push(handle);
+                py::gil_scoped_release release;
+                _cv.notify_one();
+            });
+        }
+    }
+
     void setCustomCallbacks(py::function f_callback)
     {
         for (size_t handle = 0; handle < _requests.size(); handle++)
         {
             _requests[handle].SetCompletionCallback([this, f_callback, handle /* ... */]() {
-                // Acquire GIL and execute Python function, release GIL afterwards
+                // Acquire GIL, execute Python function and add idle handle to queue
+                // release GIL afterwards
                 py::gil_scoped_acquire acquire;
                 f_callback(_user_ids[handle], handle);
+                _idle_handles.push(handle);
                 py::gil_scoped_release release;
-
-                // lock queue and add idle handle to queue
-                {
-                    std::lock_guard<std::mutex> lock(_mutex);
-                    _idle_handles.push(handle);
-                }
+                // Notify locks in getIdleRequestId() or waitAll() functions
                 _cv.notify_one();
             });
         }
@@ -96,8 +115,6 @@ public:
 
     std::vector<InferenceEngine::InferRequest> _requests;
     std::vector<py::object> _user_ids; // user ID can be any Python object
-
-private:
     std::queue<size_t> _idle_handles;
     std::mutex _mutex;
     std::condition_variable _cv;
@@ -122,12 +139,14 @@ void regclass_InferQueue(py::module m)
     }));
 
     cls.def("infer", [](InferQueue& self, py::object user_id, const py::dict inputs) {
-        py::gil_scoped_release release;
-        // getIdleRequestId function has an intention to block InferQueue (C++)
+        // getIdleRequestId function has an intention to block InferQueue
         // until there is at least one idle (free to use) InferRequest
         auto handle = self.getIdleRequestId();
         // Set new inputs label/id from user
         self._user_ids[handle] = user_id;
+        // Now GIL can be released since every instruction from this point
+        // use unique handle
+        py::gil_scoped_release release;
         // Update inputs of picked InferRequest
         for (auto&& input : inputs)
         {
@@ -139,10 +158,7 @@ void regclass_InferQueue(py::module m)
         self._requests[handle].StartAsync();
     });
 
-    cls.def("wait_all", [](InferQueue& self) {
-        py::gil_scoped_release release;
-        return self.waitAll();
-    });
+    cls.def("wait_all", [](InferQueue& self) { return self.waitAll(); });
 
     cls.def("set_infer_callback",
             [](InferQueue& self, py::function f_callback) { self.setCustomCallbacks(f_callback); });
