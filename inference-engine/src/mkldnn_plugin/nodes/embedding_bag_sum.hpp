9f8e71333d9536ebd7c374313218101f058cae34
Nikolay Shchegolev <nikolay.shchegolev@intel.com>
2021-03-31 22:59:22 +0300

    [CPU] EmbeddingBag{OffsetSum, PackedSum, Sum} migration on nGraph.

--- inference-engine/src/mkldnn_plugin/nodes/embedding_bag_sum.hpp
+++ inference-engine/src/mkldnn_plugin/nodes/embedding_bag_sum.hpp
@@ -17,7 +17,7 @@ namespace Cpu {
 class MKLDNNEmbeddingBagSum : public ExtLayerBase {
 public:
     MKLDNNEmbeddingBagSum(
-        const CNNLayer* layer,
+        const std::shared_ptr<ngraph::Node>&,
         size_t requiredInputsNum,
         size_t indicesIdx,
         size_t perSampleWeightsIdx,
@@ -32,17 +32,16 @@ public:
 protected:
     virtual void initFromInputs(std::vector<Blob::Ptr>& inputs) = 0;
     virtual void getIndices(
-        size_t embIndex,
-        const size_t*& indicesRef,
+        int embIndex,
+        const int*& indicesRef,
         size_t& size,
-        size_t& weightsIdx,
+        int& weightsIdx,
         bool& withWeights) = 0;
 
     template<typename T>
-    void processData(std::vector<Blob::Ptr>& inputs, std::vector<Blob::Ptr>& outputs) noexcept;
+    StatusCode processData(std::vector<Blob::Ptr>& inputs, std::vector<Blob::Ptr>& outputs, ResponseDesc*) noexcept;
 
-    std::set<Precision> _supportedPrecisions;
-
+    const size_t EMB_TABLE_IDX = 0lu;
     const size_t INDICES_IDX;
     const size_t PER_SAMPLE_WEIGHTS_IDX;
     const size_t DEFAULT_INDEX_IDX;
@@ -50,12 +49,6 @@ protected:
     bool _withWeights = false;
     size_t _embDepth = 0;
     std::string _layerName;
-
-    using INT32 = PrecisionTrait<Precision::I32>::value_type;
-    using INT64 = PrecisionTrait<Precision::I64>::value_type;
-    using UINT64 = PrecisionTrait<Precision::U64>::value_type;
-
-    static const std::set<size_t> _supportedIndicesTypeSize;
 };
 
 }  // namespace Cpu
