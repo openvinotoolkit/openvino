5d11b5592c47df4f89d47fd2ee895c2586b1b547
bszmelcz <bartosz.szmelczynski@intel.com>
2021-03-31 12:43:46 +0200

    clean the FusedOp from mod operation

--- ngraph/core/src/op/mod.cpp
+++ ngraph/core/src/op/mod.cpp
@@ -12,58 +12,80 @@
 #include "ngraph/op/multiply.hpp"
 #include "ngraph/op/sign.hpp"
 #include "ngraph/op/subtract.hpp"
+#include "ngraph/runtime/reference/mod.hpp"
 
 using namespace std;
 using namespace ngraph;
 
 NGRAPH_SUPPRESS_DEPRECATED_START
 
+using namespace std;
+using namespace ngraph;
+
+
+namespace modop
+{
+    template <element::Type_t ET>
+    bool evaluate(const HostTensorPtr& arg0,
+                  const HostTensorPtr& arg1,
+                  const HostTensorPtr& out,
+                  const op::AutoBroadcastSpec& broadcast_spec)
+    {
+        runtime::reference::mod(arg0->get_data_ptr<ET>(),
+                                    arg1->get_data_ptr<ET>(),
+                                    out->get_data_ptr<ET>(),
+                                    arg0->get_shape(),
+                                    arg1->get_shape(),
+                                    broadcast_spec);
+        return true;
+    }
+
+    bool evaluate_mod(const HostTensorPtr& arg0,
+                          const HostTensorPtr& arg1,
+                          const HostTensorPtr& out,
+                          const op::AutoBroadcastSpec& broadcast_spec)
+    {
+        bool rc = true;
+        out->set_broadcast(broadcast_spec, arg0, arg1);
+        switch (arg0->get_element_type())
+        {
+            NGRAPH_TYPE_CASE(evaluate_mod, i32, arg0, arg1, out, broadcast_spec);
+            NGRAPH_TYPE_CASE(evaluate_mod, i64, arg0, arg1, out, broadcast_spec);
+            NGRAPH_TYPE_CASE(evaluate_mod, u32, arg0, arg1, out, broadcast_spec);
+            NGRAPH_TYPE_CASE(evaluate_mod, u64, arg0, arg1, out, broadcast_spec);
+            NGRAPH_TYPE_CASE(evaluate_mod, f16, arg0, arg1, out, broadcast_spec);
+            NGRAPH_TYPE_CASE(evaluate_mod, f32, arg0, arg1, out, broadcast_spec);
+        default: rc = false; break;
+        }
+        return rc;
+    }
+}
+
+// ------------------------------ v1 -------------------------------------------
+
 constexpr NodeTypeInfo op::v1::Mod::type_info;
 
-op::v1::Mod::Mod()
-    : FusedOp()
-    , m_auto_broadcast()
-{
-}
-
-op::v1::Mod::Mod(const Output<Node>& A,
-                 const Output<Node>& B,
-                 const AutoBroadcastSpec& auto_broadcast)
-    : FusedOp({A, B})
-    , m_auto_broadcast(auto_broadcast)
+op::v1::Mod::Mod(const Output<Node>& arg0,
+                         const Output<Node>& arg1,
+                         const AutoBroadcastSpec& auto_broadcast)
+    : BinaryElementwiseArithmetic(arg0, arg1, auto_broadcast)
 {
     constructor_validate_and_infer_types();
-}
-
-bool ngraph::op::v1::Mod::visit_attributes(AttributeVisitor& visitor)
-{
-    NGRAPH_OP_SCOPE(v1_Mod_visit_attributes);
-    visitor.on_attribute("auto_broadcast", m_auto_broadcast);
-    return true;
-}
-
-OutputVector op::v1::Mod::decompose_op() const
-{
-    const auto dividend = make_shared<op::Abs>(input_value(0));
-    const auto dividend_sign = make_shared<op::Sign>(input_value(0));
-    const auto dividend_et = dividend->get_element_type();
-    const auto divisor = make_shared<op::Abs>(input_value(1));
-
-    // truncated(a / b)
-    auto division = make_shared<op::Convert>(
-        make_shared<op::v1::Divide>(dividend, divisor, m_auto_broadcast), ngraph::element::i64);
-    division = make_shared<op::Convert>(division, dividend_et);
-    // truncated(a / b) * b
-    const auto multiplication = make_shared<op::v1::Multiply>(division, divisor, m_auto_broadcast);
-    // a mod b = a - truncated(a / b) * b
-    const auto mod = make_shared<op::v1::Subtract>(dividend, multiplication, m_auto_broadcast);
-
-    // apply sign of dividend
-    return {make_shared<op::v1::Multiply>(dividend_sign, mod, m_auto_broadcast)};
 }
 
 shared_ptr<Node> op::v1::Mod::clone_with_new_inputs(const OutputVector& new_args) const
 {
     NGRAPH_OP_SCOPE(v1_Mod_clone_with_new_inputs);
-    return make_shared<Mod>(new_args.at(0), new_args.at(1), m_auto_broadcast);
+    check_new_args_count(this, new_args);
+    return make_shared<Mod>(new_args.at(0), new_args.at(1), this->get_autob());
 }
+
+bool op::v1::Mod::evaluate(const HostTensorVector& outputs,
+                               const HostTensorVector& inputs) const
+{
+    NGRAPH_OP_SCOPE(v1_Mod_evaluate);
+    return modop::evaluate_mod(inputs[0], inputs[1], outputs[0], get_autob());
+}
+
+
+
