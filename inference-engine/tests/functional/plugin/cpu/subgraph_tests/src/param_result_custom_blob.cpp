6e2537e975c2c830350da559d2247ca50ceb653d
Maksim Kutakov <maksim.kutakov@intel.com>
2021-04-02 17:33:16 +0300

    "Different" precisions added to the test

--- inference-engine/tests/functional/plugin/cpu/subgraph_tests/src/param_result_custom_blob.cpp
+++ inference-engine/tests/functional/plugin/cpu/subgraph_tests/src/param_result_custom_blob.cpp
@@ -46,6 +46,11 @@ class ParameterResultCustomBlobTest : public ParameterResultSubgraphTest {
 TEST_P(ParameterResultCustomBlobTest, CompareWithRefs) {
     SKIP_IF_CURRENT_TEST_IS_DISABLED()
 
+    // Just to show that it is not possible to set different precisions for inputs and outputs with the same name.
+    // If it was possible, the input would have I8 precision and couldn't store data from the custom blob.
+    inPrc = Precision::I8;
+    outPrc = Precision::FP32;
+
     Run();
 }
 namespace {
