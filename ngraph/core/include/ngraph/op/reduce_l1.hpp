b71774fa749da4e34097efd38669c9ab37123cf0
Evgenya Stepyreva <evgenya.stepyreva@intel.com>
2021-04-05 13:29:21 +0300

    [ Transpose sinking ] Transpose->FQ->Reduce (#5026)
    * Utils: make_try_fold, clone_try_fold. Template node creation and attempt to fold it
    
    * RTTI for ArithmeticReduction(KeepDims)
    
    * Enriched ngraph::get_default_order overloads with ones for dynamic shape and rank
    
    * [ Transpose sinking ] Transpose->FQ->Reduce to FQ->Reduce->Transpose
    
    * Style: deleted empty line
    
    * RTTI in Reduction operations
    
    * RTTI for LogicalReductionKeepDims
    
    * Transpose: optimizations moved from algebraic simplification to TransposeSinking
    
    * renamed file
    
    * Fix test
    
    * keep_dims is always initialized
    
    * Apply suggestions from code review
    
    Co-authored-by: Gleb Kazantaev <gleb.nnstu@gmail.com>
    
    Co-authored-by: Gleb Kazantaev <gleb.nnstu@gmail.com>

--- ngraph/core/include/ngraph/op/reduce_l1.hpp
+++ ngraph/core/include/ngraph/op/reduce_l1.hpp
@@ -19,8 +19,7 @@ namespace ngraph
             class NGRAPH_API ReduceL1 : public util::ArithmeticReductionKeepDims
             {
             public:
-                static constexpr NodeTypeInfo type_info{"ReduceL1", 4};
-                const NodeTypeInfo& get_type_info() const override { return type_info; }
+                NGRAPH_RTTI_DECLARATION;
                 /// \brief Constructs a reducet L1-norm operation.
                 ReduceL1() = default;
                 /// \brief Constructs a reduce L1-norm operation.
