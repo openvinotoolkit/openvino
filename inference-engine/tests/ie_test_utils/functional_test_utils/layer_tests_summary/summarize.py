6a58b5b6e333b9f9db566673252bbe878633850e
pszmel <piotr.szmelczynski@intel.com>
2021-04-06 11:43:51 +0200

    add op to summarize.py

--- inference-engine/tests/ie_test_utils/functional_test_utils/layer_tests_summary/summarize.py
+++ inference-engine/tests/ie_test_utils/functional_test_utils/layer_tests_summary/summarize.py
@@ -45,6 +45,7 @@ verified_operations = [
     'ExperimentalDetectronPriorGridGenerator-6',
     'ExperimentalDetectronROIFeatureExtractor-6',
     'ExperimentalDetectronTopKROIs-6',
+    'FloorMod-1'
     'GRUSequence-5',
     'Gather-1',
     'GatherElements-6',
