0d6d2f9b63b33a4c810610876c11b796dafed5f8
jdanieck <jozef.daniecki@intel.com>
2021-04-07 17:28:57 +0200

    Fix groups & def_groups test case.

--- ngraph/test/backend/deformable_convolution.in.cpp
+++ ngraph/test/backend/deformable_convolution.in.cpp
@@ -533,58 +533,48 @@ NGRAPH_TEST(${BACKEND_NAME}, deformable_convolution_2D_zeroed_offsets_deforgroup
 NGRAPH_TEST(${BACKEND_NAME}, deformable_convolution_2D_zeroed_offsets_groups_and_deforgroups)
 {
     const Strides strides{1, 1};
-    const CoordinateDiff padding{1, 1};
+    const CoordinateDiff padding{0, 0};
     const Strides dilations{1, 1};
     const int64_t group = 2;
-    const int64_t deformable_group = 4;
+    const int64_t deformable_group = 2;
 
     const Shape inputs_shape{1, 4, 3, 3};
-    const std::vector<float> inputs{1.0f, 3.0f, 3.0f,
-                                    1.0f, 2.0f, 3.0f,
-                                    1.0f, 2.0f, 3.0f,
-                                    1.0f, 3.0f, 3.0f,
-                                    1.0f, 2.0f, 3.0f,
-                                    1.0f, 2.0f, 3.0f,
-                                    1.0f, 3.0f, 3.0f,
-                                    1.0f, 2.0f, 3.0f,
-                                    1.0f, 2.0f, 3.0f,
-                                    1.0f, 3.0f, 3.0f,
-                                    1.0f, 2.0f, 3.0f,
-                                    1.0f, 2.0f, 3.0f};
+    const std::vector<float> inputs{1.0f, 2.0f, 3.0f,
+                                    4.0f, 5.0f, 6.0f,
+                                    7.0f, 8.0f, 9.0f,
+                                    10.0f, 11.0f, 12.0f,
+                                    13.0f, 14.0f, 15.0f,
+                                    16.0f, 17.0f, 18.0f,
+                                    19.0f, 20.0f, 21.0f,
+                                    22.0f, 23.0f, 24.0f,
+                                    25.0f, 26.0f, 27.0f,
+                                    28.0f, 29.0f, 30.0f,
+                                    31.0f, 32.0f, 33.0f,
+                                    34.0f, 35.0f, 36.0f};
 
-    const Shape filter_shape{2, 4, 2, 2};
-    const std::vector<float> filter{1.0f, 0.0f,
-                                    3.0f, 0.0f,
-                                    3.0f, 0.0f,
-                                    1.0f, 0.0f,
-                                    3.0f, 0.0f,
-                                    3.0f, 0.0f,
-                                    1.0f, 0.0f,
-                                    3.0f, 0.0f,
-                                    1.0f, 0.0f,
-                                    3.0f, 0.0f,
-                                    3.0f, 0.0f,
-                                    1.0f, 0.0f,
-                                    3.0f, 0.0f,
-                                    3.0f, 0.0f,
-                                    1.0f, 0.0f,
-                                    3.0f, 0.0f};
+    const Shape filter_shape{2, 2, 2, 2};
+    const std::vector<float> filter{1.0f, 2.0f,
+                                    3.0f, 4.0f,
+                                    5.0f, 6.0f,
+                                    7.0f, 8.0f,
+                                    -1.0f, -2.0f,
+                                    -3.0f, -4.0f,
+                                    -5.0f, -6.0f,
+                                    -7.0f, -8.0f,
+                                    };
 
-    const Shape offsets_shape{1, 32, 4, 4};
+    const Shape offsets_shape{1, 32, 2, 2};
     const std::vector<float> offsets(ngraph::shape_size(offsets_shape), 0);
 
-    const Shape outputs_shape{1, 2, 4, 4};
-    const std::vector<float> outputs{4.0f,  12.0f, 28.0f, 0.0f,
-                                     36.0f, 52.0f, 80.0f, 28.0f,
-                                     72.0f, 92.0f, 56.0f, 12.0f,
-                                     32.0f, 48.0f, 32.0f, 8.0f,
-                                     4.0f,  12.0f, 28.0f, 0.0f,
-                                     36.0f, 52.0f, 80.0f, 28.0f,
-                                     72.0f, 92.0f, 56.0f, 12.0f,
-                                     32.0f, 48.0f, 32.0f, 8.0f};
+    const Shape outputs_shape{1, 2, 2, 2};
+    const std::vector<float> outputs{356.0f, 392.0f,
+                                     464.0f, 500.0f,
+                                     -1004.0f, -1040.0f,
+                                     -1112.0f, -1148.0f};
 
     DeformableConvolutionTest(inputs, inputs_shape, offsets, offsets_shape, filter,
-                              filter_shape, outputs, outputs_shape,strides, padding, dilations, group, deformable_group);
+                              filter_shape, outputs, outputs_shape, strides, padding,
+                              dilations, group, deformable_group);
 }
 
 // deformable convolution atrributes (integral offsets)
