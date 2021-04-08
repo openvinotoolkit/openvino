e59846f1e81b141705328cc00c563d463409e51f
Pavel Esir <pavel.esir@intel.com>
2021-04-08 16:32:07 +0300

    changed op::v7 -> opset7, op:v1 -> opset1

--- inference-engine/src/transformations/src/transformations/op_conversions/convert_gather_v1_to_gather_v7.cpp
+++ inference-engine/src/transformations/src/transformations/op_conversions/convert_gather_v1_to_gather_v7.cpp
@@ -4,7 +4,8 @@
 
 #include "transformations/op_conversions/convert_gather_v1_to_gather_v7.hpp"
 #include <ngraph/rt_info.hpp>
-#include <ngraph/op/gather.hpp>
+#include <ngraph/opsets/opset1.hpp>
+#include <ngraph/opsets/opset7.hpp>
 #include <ngraph/pattern/op/wrap_type.hpp>
 #include "itt.hpp"
 
@@ -14,10 +15,10 @@ ngraph::pass::ConvertGather1ToGather7::ConvertGather1ToGather7() {
     MATCHER_SCOPE(ConvertGather1ToGather7);
 
 
-    auto gather_v1 = pattern::wrap_type<ngraph::op::v1::Gather>();
+    auto gather_v1 = pattern::wrap_type<ngraph::opset1::Gather>();
 
     ngraph::matcher_pass_callback callback = [](pattern::Matcher& m) {
-        auto gather_v1_node = std::dynamic_pointer_cast<ngraph::op::v1::Gather>(m.get_match_root());
+        auto gather_v1_node = std::dynamic_pointer_cast<ngraph::opset1::Gather>(m.get_match_root());
         if (!gather_v1_node)
             return false;
 
@@ -25,7 +26,7 @@ ngraph::pass::ConvertGather1ToGather7::ConvertGather1ToGather7() {
         auto indices_input = gather_v1_node->input_value(1);
         auto axis_input = gather_v1_node->input_value(2);
 
-        auto gather_v7  = std::make_shared<ngraph::op::v7::Gather>(data_input, indices_input, axis_input);
+        auto gather_v7  = std::make_shared<ngraph::opset7::Gather>(data_input, indices_input, axis_input);
         gather_v7->set_friendly_name(gather_v1_node->get_friendly_name());
         ngraph::copy_runtime_info(gather_v1_node, gather_v7);
         ngraph::replace_node(gather_v1_node, gather_v7);
--- inference-engine/src/transformations/src/transformations/op_conversions/convert_gather_v7_to_gather_v1.cpp
+++ inference-engine/src/transformations/src/transformations/op_conversions/convert_gather_v7_to_gather_v1.cpp
@@ -3,7 +3,8 @@
 //
 
 #include "transformations/op_conversions/convert_gather_v7_to_gather_v1.hpp"
-#include <ngraph/op/gather.hpp>
+#include <ngraph/opsets/opset1.hpp>
+#include <ngraph/opsets/opset7.hpp>
 #include <ngraph/rt_info.hpp>
 #include <ngraph/pattern/op/wrap_type.hpp>
 
@@ -15,10 +16,10 @@ ngraph::pass::ConvertGather7ToGather1::ConvertGather7ToGather1() {
     MATCHER_SCOPE(ConvertGather7ToGather1);
 
 
-    auto gather_v7 = pattern::wrap_type<ngraph::op::v7::Gather>();
+    auto gather_v7 = pattern::wrap_type<ngraph::opset7::Gather>();
 
     ngraph::matcher_pass_callback callback = [](pattern::Matcher& m) {
-        auto gather_v7_node = std::dynamic_pointer_cast<ngraph::op::v7::Gather>(m.get_match_root());
+        auto gather_v7_node = std::dynamic_pointer_cast<ngraph::opset7::Gather>(m.get_match_root());
         if (!gather_v7_node)
             return false;
 
@@ -31,7 +32,7 @@ ngraph::pass::ConvertGather7ToGather1::ConvertGather7ToGather1() {
         auto indices_input = gather_v7_node->input_value(1);
         auto axis_input = gather_v7_node->input_value(2);
 
-        auto gather_v1  = std::make_shared<ngraph::op::v1::Gather>(data_input, indices_input, axis_input);
+        auto gather_v1  = std::make_shared<ngraph::opset1::Gather>(data_input, indices_input, axis_input);
         gather_v1->set_friendly_name(gather_v7_node->get_friendly_name());
         ngraph::copy_runtime_info(gather_v7_node, gather_v1);
         ngraph::replace_node(gather_v7_node, gather_v1);
