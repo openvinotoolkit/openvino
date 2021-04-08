4f61c0b92bd86bdae342403bb17da830dd566214
Shoujiang Ma <shoujiang.ma@intel.com>
2021-04-08 15:12:27 +0800

    Implement AUTO plugin
    Usage:
    	1. -d AUTO
    	2. -d ""
    
    Signed-off-by: Shoujiang Ma <shoujiang.ma@intel.com>

---
+++ inference-engine/src/auto_plugin/auto_exec_network.cpp
@@ -0,0 +1,78 @@
+// Copyright (C) 2018-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <map>
+#include <unordered_map>
+
+#include "ie_metric_helpers.hpp"
+#include "auto_exec_network.hpp"
+#include "auto_infer_request.hpp"
+
+namespace AutoPlugin {
+    using namespace InferenceEngine;
+
+AutoExecutableNetwork::AutoExecutableNetwork(const ExecutableNetwork& network,
+                                             const DeviceInformation& deviceInfo,
+                                             const bool               needPerfCounters) :
+    _deviceInfo(deviceInfo),
+    _network(network),
+    _config(deviceInfo.config.begin(), deviceInfo.config.end()),
+    _needPerfCounters(needPerfCounters) {
+}
+
+AutoExecutableNetwork::~AutoExecutableNetwork() = default;
+
+RemoteContext::Ptr AutoExecutableNetwork::GetContext() const {
+    try {
+        return _network.GetContext();
+    } catch (...) {
+        IE_THROW(NotImplemented);
+    }
+}
+
+IInferRequestInternal::Ptr AutoExecutableNetwork::CreateInferRequestImpl(InputsDataMap networkInputs,
+                                                                         OutputsDataMap networkOutputs) {
+    auto inferRequest = _network.CreateInferRequest();
+    return std::make_shared<AutoInferRequest>(networkInputs, networkOutputs, inferRequest);
+}
+
+IInferRequestInternal::Ptr AutoExecutableNetwork::CreateInferRequest() {
+    return CreateInferRequestImpl(_networkInputs, _networkOutputs);
+}
+
+Parameter AutoExecutableNetwork::GetMetric(const std::string &name) const {
+    if (name == METRIC_KEY(OPTIMAL_NUMBER_OF_INFER_REQUESTS)) {
+        unsigned int res = 0u;
+            try {
+              res = _network.GetMetric(METRIC_KEY(OPTIMAL_NUMBER_OF_INFER_REQUESTS)).as<unsigned int>();
+            } catch (const Exception &iie) {
+                  IE_THROW()
+                        << "Every device used with the Auto-Device should "
+                        << "support OPTIMAL_NUMBER_OF_INFER_REQUESTS ExecutableNetwork metric. "
+                        << "Failed to query the metric for the " << _deviceInfo.deviceName << " with error:" << iie.what();
+           }
+
+        IE_SET_METRIC_RETURN(OPTIMAL_NUMBER_OF_INFER_REQUESTS, res);
+    } else if (name == METRIC_KEY(NETWORK_NAME)) {
+        IE_SET_METRIC_RETURN(NETWORK_NAME, _network.GetMetric(
+            METRIC_KEY(NETWORK_NAME)).as<std::string>());
+    } else if (name == METRIC_KEY(SUPPORTED_METRICS)) {
+        IE_SET_METRIC_RETURN(SUPPORTED_METRICS, {
+            METRIC_KEY(OPTIMAL_NUMBER_OF_INFER_REQUESTS),
+            METRIC_KEY(SUPPORTED_METRICS),
+            METRIC_KEY(NETWORK_NAME),
+            METRIC_KEY(SUPPORTED_CONFIG_KEYS)
+        });
+    } else if (name == METRIC_KEY(SUPPORTED_CONFIG_KEYS)) {
+        std::vector<std::string> configKeys = {};
+        IE_SET_METRIC_RETURN(SUPPORTED_CONFIG_KEYS, configKeys);
+    } else {
+        IE_THROW() << "Unsupported Network metric: " << name;
+    }
+}
+
+}  // namespace AutoPlugin
---
+++ inference-engine/src/auto_plugin/auto_exec_network.hpp
@@ -0,0 +1,48 @@
+// Copyright (C) 2018-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <atomic>
+#include <mutex>
+#include <queue>
+#include <unordered_map>
+#include <map>
+#include <vector>
+#include <string>
+
+#include <cpp_interfaces/impl/ie_executable_network_thread_safe_default.hpp>
+#include <threading/ie_itask_executor.hpp>
+
+namespace AutoPlugin {
+
+using DeviceName = std::string;
+
+struct DeviceInformation {
+    DeviceName deviceName;
+    std::map<std::string, std::string> config;
+};
+
+class AutoExecutableNetwork : public InferenceEngine::ExecutableNetworkThreadSafeDefault {
+public:
+    using Ptr = std::shared_ptr<AutoExecutableNetwork>;
+
+    AutoExecutableNetwork(const InferenceEngine::ExecutableNetwork&                             network,
+                          const DeviceInformation&                                              deviceInfo,
+                          const bool                                                            needPerfCounters = false);
+
+    InferenceEngine::Parameter GetMetric(const std::string &name) const override;
+    InferenceEngine::IInferRequestInternal::Ptr CreateInferRequest() override;
+    InferenceEngine::IInferRequestInternal::Ptr CreateInferRequestImpl(InferenceEngine::InputsDataMap networkInputs,
+                                                                       InferenceEngine::OutputsDataMap networkOutputs) override;
+    InferenceEngine::RemoteContext::Ptr GetContext() const override;
+    ~AutoExecutableNetwork() override;
+
+    DeviceInformation                                            _deviceInfo;
+    InferenceEngine::ExecutableNetwork                           _network;
+    std::unordered_map<std::string, InferenceEngine::Parameter>  _config;
+    bool                                                         _needPerfCounters = false;
+};
+
+}  // namespace AutoPlugin
