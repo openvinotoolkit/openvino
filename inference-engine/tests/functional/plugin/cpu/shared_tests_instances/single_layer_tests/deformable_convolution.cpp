ef5138f497f0b6077eaf9a73936ab62ea390579f
jdanieck <jozef.daniecki@intel.com>
2021-04-08 15:25:59 +0200

    Revert "Add integer types to SLT."
    This reverts commit 2fefe8926da65171f64a2dc679ab548415736213.

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/single_layer_tests/deformable_convolution.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/single_layer_tests/deformable_convolution.cpp
@@ -12,8 +12,7 @@ using namespace LayerTestsDefinitions;
 namespace {
 
 const std::vector<InferenceEngine::Precision> netPrecisions = {
-    InferenceEngine::Precision::FP32, InferenceEngine::Precision::FP16,
-    InferenceEngine::Precision::I32, InferenceEngine::Precision::I16};
+    InferenceEngine::Precision::FP32, InferenceEngine::Precision::FP16};
 
 /* ============= 2D DeformableConvolution ============= */
 const std::vector<std::vector<size_t>> deformable_vals = {{1, 18, 28, 28}};
