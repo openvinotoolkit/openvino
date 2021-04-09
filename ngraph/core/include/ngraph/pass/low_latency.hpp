067313ec7cd984827ec2a777ce5e70bfc3f60c2d
Ivan Tikhonov <ivan.tikhonov@intel.com>
2021-04-09 03:24:02 +0300

    LowLatency 2.0: transformation and unit tests

--- ngraph/core/include/ngraph/pass/low_latency.hpp
+++ ngraph/core/include/ngraph/pass/low_latency.hpp
@@ -48,5 +48,37 @@ namespace ngraph
             NGRAPH_RTTI_DECLARATION;
             LowLatency();
         };
+
+        class NGRAPH_API LowLatency_v2 : public ngraph::pass::FunctionPass
+        {
+        public:
+            enum InitialValue {
+                PARAMETER,
+                CONST
+            };
+
+            LowLatency_v2() = default;
+
+            explicit LowLatency_v2(int64_t iterations)
+            : m_iterations(iterations) {
+            }
+
+            explicit LowLatency_v2(LowLatency_v2::InitialValue init_value)
+            : m_init_value(init_value) {
+            }
+
+            LowLatency_v2(LowLatency_v2::InitialValue init_value, int64_t iterations)
+            : m_init_value(init_value),
+              m_iterations(iterations)
+            {}
+
+            NGRAPH_RTTI_DECLARATION;
+            bool run_on_function(std::shared_ptr<ngraph::Function> f) override;
+
+        private:
+            InitialValue m_init_value = InitialValue::CONST;
+            int64_t m_iterations = 1;
+
+        };
     } // namespace pass
 } // namespace ngraph
--- ngraph/core/src/pass/low_latency.cpp
+++ ngraph/core/src/pass/low_latency.cpp
@@ -7,10 +7,16 @@
 #include <memory>
 
 #include <ngraph/opsets/opset6.hpp>
+#include <ngraph/opsets/opset7.hpp>
 #include <ngraph/pattern/op/wrap_type.hpp>
 #include <ngraph/variant.hpp>
+#include <ngraph/rt_info.hpp>
 
 NGRAPH_RTTI_DEFINITION(ngraph::pass::LowLatency, "LowLatency", 0);
+NGRAPH_RTTI_DEFINITION(ngraph::pass::LowLatency_v2, "LowLatency_v2", 0);
+
+using namespace std;
+using namespace ngraph;
 
 ngraph::pass::LowLatency::LowLatency()
 {
@@ -89,4 +95,127 @@ ngraph::pass::LowLatency::LowLatency()
 
     auto m = std::make_shared<ngraph::pattern::Matcher>(tensor_iterator, "LowLatency");
     register_matcher(m, callback);
+}
+
+void UnrollSingleIteration(const shared_ptr<op::util::SubGraphOp>& sub_graph_op, const shared_ptr<Function>& outer_f) {
+    using namespace opset7;
+
+    const auto& params = sub_graph_op->get_function()->get_parameters();
+    const auto& results = sub_graph_op->get_function()->get_results();
+
+    // before: Layer1 -> TI [input -> bodyParameter -> Layer2 -> ...]
+    // after:  Layer1 -> Layer2 ->...
+    for (const auto& in : sub_graph_op->get_input_descriptions()) {
+        const auto& connect_to = sub_graph_op->get_input_source_output(in->m_input_index);
+        for (auto& output : params.at(in->m_body_parameter_index)->outputs()) {
+            output.replace(connect_to);
+        }
+    }
+
+    // before: TI [...-> Layer1 -> Result -> output] -> Layer2 -> ...
+    // after:  ...-> Layer1 -> Layer2 -> ...
+    for (const auto& out : sub_graph_op->get_output_descriptions()) {
+        const auto& connect_to = results.at(out->m_body_value_index)->get_input_source_output(0);
+        for (auto& input_to : sub_graph_op->output(out->m_output_index).get_target_inputs()) {
+            input_to.replace_source_output(connect_to);
+        }
+    }
+    ngraph::copy_runtime_info(sub_graph_op, sub_graph_op->get_function()->get_ops());
+}
+
+Output<Node> create_init_subgraph(const Output<Node>& in_node) {
+    using namespace opset7;
+
+    auto const_zero = make_shared<Constant>(in_node.get_element_type(), Shape{1}, 0);
+    auto shape_of = make_shared<ShapeOf>(in_node);
+    auto broadcast = make_shared<Broadcast>(const_zero, shape_of);
+    return broadcast->output(0);
+}
+
+string generate_variable_name(const string& op_name, const string& param_name, int variable_idx) {
+    return op_name + "/" + param_name + "/" + "variable_" + to_string(variable_idx);
+}
+
+bool pass::LowLatency_v2::run_on_function(shared_ptr<Function> f) {
+    using namespace opset7;
+
+    SinkVector assigns;
+    for (const auto& op : f->get_ops()) {
+        if (const auto& sub_graph_op = dynamic_pointer_cast<op::util::SubGraphOp>(op)) {
+            if (const auto& loop = dynamic_pointer_cast<Loop>(sub_graph_op))
+            {
+                const auto& trip_count = dynamic_pointer_cast<Constant>(loop->get_input_node_shared_ptr(0));
+                const auto& num_iter = loop->get_num_iterations();
+                if (trip_count && num_iter > 0 && trip_count->get_output_target_inputs(0).size() == 1)
+                {
+                    auto iterations = std::make_shared<Constant>(element::i64, Shape{}, m_iterations);
+                    replace_node(trip_count, iterations);
+                }
+                else
+                {
+                    // this loop is not supported
+                    continue;
+                }
+            }
+
+            int64_t variable_id = 0;
+            const auto& func = sub_graph_op->get_function();
+            const auto& params = func->get_parameters();
+            for (const auto& in : sub_graph_op->get_input_descriptions()) {
+                // Process all back edges
+                if (const auto& merged_in = dynamic_pointer_cast<op::util::SubGraphOp::MergedInputDescription>(in)) {
+                    // create new Variable
+                    const string &param_name = params.at(merged_in->m_body_parameter_index)->get_friendly_name();
+                    const string &var_name = generate_variable_name(sub_graph_op->get_friendly_name(),
+                                                                    param_name,
+                                                                    variable_id);
+                    VariableInfo var_info{PartialShape::dynamic(), element::dynamic, var_name};
+                    auto variable = make_shared<Variable>(var_info);
+
+                    // insert ReadValue
+                    // Layers -> [new op: ReadValue] -> Subgraph operation
+                    const auto &input = sub_graph_op->input(merged_in->m_input_index);
+                    Output<Node> read_value_in = input.get_source_output();
+                    if (m_init_value == InitialValue::CONST) {
+                        read_value_in = create_init_subgraph(read_value_in);
+                    }
+                    auto read_value = make_shared<ReadValue>(read_value_in, variable);
+                    input.replace_source_output(read_value->output(0));
+                    read_value->set_friendly_name(var_name);
+
+                    // insert Assign
+                    // Subgraph operation -> [new op: Assign]
+                    //                    \
+                    //                      ---> Layers -> ...
+                    const auto& out_desc = sub_graph_op->get_output_descriptions();
+                    bool is_assign_found = std::any_of(out_desc.begin(), out_desc.end(),
+                                [&merged_in](const std::shared_ptr<op::util::SubGraphOp::OutputDescription>& out) {
+                                    return out->m_body_value_index == merged_in->m_body_value_index;
+                    });
+                    // Create new output if it doesn't exist.
+                    if (!is_assign_found) {
+                        sub_graph_op->get_iter_value(func->get_results().at(merged_in->m_body_value_index));
+                    }
+                    for (const auto &out : sub_graph_op->get_output_descriptions()) {
+                        if (out->m_body_value_index == merged_in->m_body_value_index) {
+                            auto assign = make_shared<Assign>(sub_graph_op->output(out->m_output_index), variable);
+                            ngraph::copy_runtime_info(sub_graph_op, assign );
+                            // control dependency so that ReadValue is processed before Assign
+                            assign->add_control_dependency(read_value);
+                            assigns.emplace_back(assign);
+                            is_assign_found = true;
+                            break;
+                        }
+                    }
+
+                }
+            }
+
+            if (sub_graph_op->get_num_iterations() == 1) {
+                UnrollSingleIteration(sub_graph_op, f);
+            }
+        }
+    }
+    f->add_sinks(assigns);
+    return true;
 }
