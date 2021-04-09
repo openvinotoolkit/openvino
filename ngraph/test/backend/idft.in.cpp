e3909a726c5058db6103e1da0941ce31ba9fc322
VGavrilov <vladimir.gavrilov@intel.com>
2021-04-09 11:31:17 +0300

    Small fix.

--- ngraph/test/backend/idft.in.cpp
+++ ngraph/test/backend/idft.in.cpp
@@ -639,18 +639,11 @@ NGRAPH_TEST(${BACKEND_NAME}, idft3d_eval_bfloat16)
     handle->call({idft_output}, {backend_data});
 
     auto result = bfloat16::to_float_vector(read_vector<bfloat16>(idft_output));
-    std::cout << "Actual result: ";
-    for (auto x : result)
+    size_t num_of_elems = result.size();
+    for (std::size_t j = 0; j < num_of_elems; ++j)
     {
-        std::cout << x << ", ";
+        EXPECT_NEAR(result[j], expected_bfloat_idft3d_result[j], 0.000003);
     }
-    std::cout << "\n";
-    EXPECT_TRUE(test::all_close_f(expected_bfloat_idft3d_result, result));
-//     size_t num_of_elems = result.size();
-//     for (std::size_t j = 0; j < num_of_elems; ++j)
-//     {
-//         EXPECT_NEAR(result[j], expected_result[j], 0.000003);
-//     }
 }
 
 NGRAPH_TEST(${BACKEND_NAME}, idft3d_eval_i32)
