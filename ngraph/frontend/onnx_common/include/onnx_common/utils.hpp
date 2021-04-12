eac4e2888488b3c262420b7a3f556d9b9952a1b4
mbencer <mateusz.bencer@intel.com>
2021-04-12 11:41:57 +0200

    support constant_fill according to onnx spec

--- ngraph/frontend/onnx_common/include/onnx_common/utils.hpp
+++ ngraph/frontend/onnx_common/include/onnx_common/utils.hpp
@@ -1,12 +1,21 @@
 // Copyright (C) 2018-2021 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
+#include "ngraph/type/element_type.hpp"
+
+namespace ONNX_NAMESPACE
+{
+    enum TensorProto_DataType;
+}
 
 namespace ngraph
 {
     namespace onnx_common
     {
         size_t get_onnx_data_size(int32_t onnx_type);
+        element::Type_t onnx_to_ng_data_type(const ONNX_NAMESPACE::TensorProto_DataType& onnx_type);
+        ONNX_NAMESPACE::TensorProto_DataType ng_to_onnx_data_type(const element::Type_t& ng_type);
+        bool is_supported_ng_type(const element::Type_t& ng_type);
 
     } // namespace onnx_editor
 } // namespace ngraph
--- ngraph/frontend/onnx_import/src/op/org.openvinotoolkit/constant_fill.cpp
+++ ngraph/frontend/onnx_import/src/op/org.openvinotoolkit/constant_fill.cpp
@@ -2,10 +2,14 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include <onnx/onnx_pb.h> // onnx types
+
 #include "default_opset.hpp"
 #include "exceptions.hpp"
 #include "ngraph/op/broadcast.hpp"
+#include "ngraph/op/concat.hpp"
 #include "ngraph/op/constant.hpp"
+#include "onnx_common/utils.hpp"
 
 namespace ngraph
 {
@@ -15,18 +19,43 @@ namespace ngraph
         {
             namespace set_1
             {
+                // ConstantFill is a deprecated experimental operator removed in ONNX 1.4
                 OutputVector constant_fill(const Node& node)
                 {
-                    const auto target_shape = node.get_ng_inputs().at(0);
-                    const auto fill_value = node.get_attribute_value<float>("fill_value", 0.f);
+                    Output<ngraph::Node> target_shape;
+                    const auto fill_value = node.get_attribute_value<float>("value", 0.f);
+                    const auto dtype = node.get_attribute_value<int64_t>(
+                        "dtype", static_cast<int64_t>(ONNX_NAMESPACE::TensorProto_DataType_FLOAT));
+                    const auto ng_type = onnx_common::onnx_to_ng_data_type(
+                        static_cast<ONNX_NAMESPACE::TensorProto_DataType>(dtype));
+                    Output<ngraph::Node> const_val_to_fill =
+                        default_opset::Constant::create(ng_type, {}, {fill_value});
                     const auto input_as_shape =
-                        node.get_attribute_value<float>("input_as_shape", 1.f);
-                    CHECK_VALID_NODE(node,
-                                     input_as_shape == 1,
-                                     "Only input_as_shape=1 is supported by ConstantFill");
+                        node.get_attribute_value<int64_t>("input_as_shape", 1);
+                    if (input_as_shape == 1) // use the first input as target shape
+                    {
+                        CHECK_VALID_NODE(
+                            node,
+                            node.get_ng_inputs().size() > 0,
+                            "The input which determines output shape was not provided");
+                        target_shape = node.get_ng_inputs().at(0);
+                        if (node.has_attribute("extra_shape"))
+                        {
+                            const auto extra_shape =
+                                node.get_attribute_value<std::vector<int64_t>>("extra_shape");
+                            const auto extra_shape_const = default_opset::Constant::create(
+                                target_shape.get_element_type(), {extra_shape.size()}, extra_shape);
+                            target_shape = std::make_shared<default_opset::Concat>(
+                                OutputVector{target_shape, extra_shape_const}, 0);
+                        }
+                    }
+                    else // use input_as_shape attribute as target shape
+                    {
+                        const auto shape = node.get_attribute_value<std::vector<int64_t>>("shape");
+                        target_shape =
+                            default_opset::Constant::create(ng_type, {shape.size()}, shape);
+                    }
 
-                    const auto const_val_to_fill =
-                        default_opset::Constant::create(element::f32, {}, {fill_value});
                     return {std::make_shared<default_opset::Broadcast>(const_val_to_fill,
                                                                        target_shape)};
                 }
