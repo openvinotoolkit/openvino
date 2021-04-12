7f84421f7ee61e8d2a17faeb4145bc09d9797312
prozen <piotr.rozen@intel.com>
2021-04-12 18:42:16 +0200

    add conversion of padded to valid convolution without other parameters change

--- inference-engine/src/gna_plugin/gna_plugin.cpp
+++ inference-engine/src/gna_plugin/gna_plugin.cpp
@@ -54,6 +54,8 @@
 #include <transformations/common_optimizations/pull_transpose_through_fq.hpp>
 #include <transformations/common_optimizations/relu_fake_quantize_fusion.hpp>
 #include <transformations/common_optimizations/add_fake_quantize_fusion.hpp>
+#include <transformations/op_conversions/convert_padded2valid_conv.hpp>
+#include <transformations/serialize.hpp>
 
 #include "transformations/remove_extra_reshapes.hpp"
 
@@ -662,6 +664,8 @@ void GNAPlugin::LoadNetwork(CNNNetwork & _network) {
         manager.register_pass<ngraph::pass::InitNodeInfo>();
         // WA: ConvertPriorBox must be executed before the 1st ConstantFolding pass
         manager.register_pass<ngraph::pass::ConvertPriorBox>();
+        manager.register_pass<ngraph::pass::ConvertPadded2ValidConv>();
+        //manager.register_pass<ngraph::pass::Serialize>("irv10.xml", "irv10.bin", ngraph::pass::Serialize::Version::IR_V10);
         manager.register_pass<ngraph::pass::CommonOptimizations>();
         manager.register_pass<ngraph::pass::ConvertOpSet3ToOpSet2>();
         manager.register_pass<ngraph::pass::ConvertOpSet2ToOpSet1>();
