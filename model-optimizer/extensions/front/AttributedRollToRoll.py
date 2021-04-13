b1348ebaed2f17a75523dc566c760001e877b927
Anastasia Popova <anastasia.popova@intel.com>
2021-04-13 12:36:07 +0300

    Replaced FrontReplacementPattern with FrontReplacementOp.

--- model-optimizer/extensions/front/AttributedRollToRoll.py
+++ model-optimizer/extensions/front/AttributedRollToRoll.py
@@ -2,32 +2,29 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from extensions.ops.roll import Roll
-from mo.front.common.replacement import FrontReplacementPattern
+from mo.front.common.replacement import FrontReplacementOp
+from mo.front.tf.graph_utils import create_op_with_const_inputs
 from mo.graph.graph import Graph, rename_nodes
-from mo.ops.const import Const
 
 
-class AttributedRollToRoll(FrontReplacementPattern):
+class AttributedRollToRoll(FrontReplacementOp):
     """
     This transformation converts AttributedRoll operation (axes and shift are specified as attributes) to Roll
     operation (Inference Engine semantic).
     """
     enabled = True
+    op = 'AttributedRoll'
 
-    def find_and_replace_pattern(self, graph: Graph):
-        for attr_roll in graph.get_op_nodes(op='AttributedRoll'):
-            original_name = attr_roll.soft_get('name', attr_roll.id)
+    def replace_sub_graph(self, graph: Graph, match: dict):
+        attr_roll = match['op']
+        original_name = attr_roll.soft_get('name', attr_roll.id)
+        port_value_dict = {1: attr_roll.shift}
+        if attr_roll.has_valid('axes'):
+            port_value_dict.update({2: attr_roll.axes})
 
-            new_roll = Roll(graph, {'axes': attr_roll.soft_get('axes', None),
-                                    'shift': attr_roll.soft_get('shift', None)}).create_node()
-            rename_nodes([(attr_roll, original_name + '/to_be_removed'), (new_roll, original_name)])
+        new_roll = create_op_with_const_inputs(graph, op=Roll, port_value_dict=port_value_dict)
+        rename_nodes([(attr_roll, original_name + '/to_be_removed'), (new_roll, original_name)])
 
-            attr_roll.in_port(0).get_connection().set_destination(new_roll.in_port(0))
-            new_roll.in_port(1).connect(
-                Const(graph, {'name': original_name + '/shift', 'value': attr_roll.shift}).create_node().out_port(0))
-            if attr_roll.has_valid('axes'):
-                new_roll.in_port(2).connect(
-                    Const(graph, {'name': original_name + '/axes', 'value': attr_roll.axes}).create_node().out_port(0))
-
-            attr_roll.out_port(0).get_connection().set_source(new_roll.out_port(0))
-            graph.remove_node(attr_roll.id)
+        attr_roll.in_port(0).get_connection().set_destination(new_roll.in_port(0))
+        attr_roll.out_port(0).get_connection().set_source(new_roll.out_port(0))
+        graph.remove_node(attr_roll.id)
