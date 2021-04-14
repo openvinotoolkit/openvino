57dd717a6fe4c0977d6384e41510292391f596e8
mvafin <maxim.vafin@intel.com>
2021-04-14 12:14:52 +0300

    Implement setting inputs and outputs

--- ngraph/frontend/paddlepaddle/src/model.cpp
+++ ngraph/frontend/paddlepaddle/src/model.cpp
@@ -13,15 +13,15 @@ namespace ngraph {
 namespace frontend {
 
 class InputModelPDPD::InputModelPDPDImpl {
-    std::shared_ptr<paddle::framework::proto::ProgramDesc> fw_ptr;
-    std::vector<std::vector<std::shared_ptr<OpPlacePDPD>>> op_places;
-    std::vector<std::map<std::string, std::shared_ptr<VarPlacePDPD>>> var_places;
-    std::vector<Place::Ptr> inputs;
-    std::vector<Place::Ptr> outputs;
-    std::ifstream weights_stream;
-    bool weights_composed = false;
+    std::shared_ptr<paddle::framework::proto::ProgramDesc> m_fw_ptr;
+    std::vector<std::vector<std::shared_ptr<OpPlacePDPD>>> m_op_places;
+    std::vector<std::map<std::string, std::shared_ptr<VarPlacePDPD>>> m_var_places;
+    std::vector<Place::Ptr> m_inputs;
+    std::vector<Place::Ptr> m_outputs;
+    std::ifstream m_weights_stream;
+    bool m_weights_composed = false;
+    std::string m_path;
 public:
-    std::string path;
 
     InputModelPDPDImpl (const std::string& _path);
     std::vector<Place::Ptr> getInputs () const;
@@ -35,45 +35,45 @@ public:
     
     template<typename T>
     std::vector<T> readWeight(const std::string& name, int64_t tensor_length);
-    std::vector<std::shared_ptr<OpPlacePDPD>> getOpPlaces(int i) const { return op_places[i]; } 
-    std::map<std::string, std::shared_ptr<VarPlacePDPD>> getVarPlaces(int i) const { return var_places[i]; }
-    size_t getBlockNumber() const { return op_places.size(); }
+    std::vector<std::shared_ptr<OpPlacePDPD>> getOpPlaces(int i) const { return m_op_places[i]; } 
+    std::map<std::string, std::shared_ptr<VarPlacePDPD>> getVarPlaces(int i) const { return m_var_places[i]; }
+    size_t getBlockNumber() const { return m_op_places.size(); }
 };
 
-InputModelPDPD::InputModelPDPDImpl::InputModelPDPDImpl(const std::string& _path) : path(_path), fw_ptr{std::make_shared<paddle::framework::proto::ProgramDesc>()} {
+InputModelPDPD::InputModelPDPDImpl::InputModelPDPDImpl(const std::string& _path) : m_path(_path), m_fw_ptr{std::make_shared<paddle::framework::proto::ProgramDesc>()} {
     std::string ext = ".pdmodel";            
-    std::string model_file(path);
-    if (path.length() >= ext.length() && (0 == path.compare(path.length() - ext.length(), ext.length(), ext)))
+    std::string model_file(m_path);
+    if (m_path.length() >= ext.length() && (0 == m_path.compare(m_path.length() - ext.length(), ext.length(), ext)))
     {
-        weights_composed = true;
-        auto weights_file = path.replace(path.size() - ext.size(), ext.size(), ".pdiparams");
-        weights_stream = std::ifstream(weights_file, std::ios::binary);
-        if (!weights_stream || !weights_stream.is_open())
+        m_weights_composed = true;
+        auto weights_file = m_path.replace(m_path.size() - ext.size(), ext.size(), ".pdiparams");
+        m_weights_stream = std::ifstream(weights_file, std::ios::binary);
+        if (!m_weights_stream || !m_weights_stream.is_open())
         {
             std::cerr << "Model file cannot be opened" << std::endl;
         }
     } else {
-        weights_composed = false;
+        m_weights_composed = false;
         model_file += "/__model__";
     }
 
     std::ifstream pb_stream(model_file, std::ios::binary);
-    std::cout << "Model Parsed: " << fw_ptr->ParseFromIstream(&pb_stream) << std::endl;
+    std::cout << "Model Parsed: " << m_fw_ptr->ParseFromIstream(&pb_stream) << std::endl;
 
-    std::cout << "Blocks number: " << fw_ptr->blocks().size() << std::endl;
-    for (const auto& block : fw_ptr->blocks()) {
-        var_places.push_back(std::map<std::string, std::shared_ptr<VarPlacePDPD>>());
+    std::cout << "Blocks number: " << m_fw_ptr->blocks().size() << std::endl;
+    for (const auto& block : m_fw_ptr->blocks()) {
+        m_var_places.push_back(std::map<std::string, std::shared_ptr<VarPlacePDPD>>());
         for (int i = 0; i < block.vars().size(); i++) {
-            var_places.back()[block.vars()[i].name()] = std::make_shared<VarPlacePDPD>(VarPlacePDPD(&(block.vars()[i])));
+            m_var_places.back()[block.vars()[i].name()] = std::make_shared<VarPlacePDPD>(VarPlacePDPD(&(block.vars()[i])));
         }
 
-        op_places.push_back(std::vector<std::shared_ptr<OpPlacePDPD>>());
+        m_op_places.push_back(std::vector<std::shared_ptr<OpPlacePDPD>>());
         for (const auto& op : block.ops()) {
             auto op_place = std::make_shared<OpPlacePDPD>(OpPlacePDPD(&op));
             for (const auto &output : op.outputs()) {
                 std::vector<std::weak_ptr<VarPlacePDPD>> out_vars;
                 for (auto& var_name : output.arguments()) {
-                    auto& var = var_places.back().at(var_name);
+                    auto& var = m_var_places.back().at(var_name);
                     var->producing_ops.push_back(op_place);
                     out_vars.push_back(var);
                 }
@@ -83,7 +83,7 @@ InputModelPDPD::InputModelPDPDImpl::InputModelPDPDImpl(const std::string& _path)
             for (const auto &input : op.inputs()) {
                 std::vector<std::weak_ptr<VarPlacePDPD>> in_vars;
                 for (auto& var_name : input.arguments()) {
-                    auto& var = var_places.back().at(var_name);
+                    auto& var = m_var_places.back().at(var_name);
                     var->consuming_ops.push_back(op_place);
                     in_vars.push_back(var);
                 }
@@ -98,12 +98,12 @@ InputModelPDPD::InputModelPDPDImpl::InputModelPDPDImpl(const std::string& _path)
                 var_place->type = TYPE_MAP[dtype];
                 const auto& dims = tensor_desc.dims();
                 var_place->shape = ngraph::PartialShape(std::vector<ngraph::Dimension>(dims.begin(), dims.end()));
-                inputs.push_back(std::dynamic_pointer_cast<Place>(var_place));
+                m_inputs.push_back(std::dynamic_pointer_cast<Place>(var_place));
             } else if (op.type() == "fetch") {
                 auto var_place = op_place->inputs.at("X")[0].lock();
-                outputs.push_back(std::dynamic_pointer_cast<Place>(var_place));
+                m_outputs.push_back(std::dynamic_pointer_cast<Place>(var_place));
             }
-            op_places.back().push_back(op_place);
+            m_op_places.back().push_back(op_place);
         }
     }
 }
@@ -114,10 +114,10 @@ std::vector<T> InputModelPDPD::InputModelPDPDImpl::readWeight(const std::string&
 
     std::ifstream is;
     std::ifstream* stream_ptr;
-    if (weights_composed) {
-        stream_ptr = &weights_stream;
+    if (m_weights_composed) {
+        stream_ptr = &m_weights_stream;
     } else {
-        is = std::ifstream(path + "/" + name, std::ios::in | std::ifstream::binary);
+        is = std::ifstream(m_path + "/" + name, std::ios::in | std::ifstream::binary);
         if (!is || !is.is_open())
         {
             std::cout << "File not opened" << std::endl;
@@ -136,39 +136,46 @@ std::vector<T> InputModelPDPD::InputModelPDPDImpl::readWeight(const std::string&
 }
 
 std::vector<Place::Ptr> InputModelPDPD::InputModelPDPDImpl::getInputs () const {
-    return inputs;
+    return m_inputs;
 }
 
 std::vector<Place::Ptr> InputModelPDPD::InputModelPDPDImpl::getOutputs () const {
-    return outputs;
+    return m_outputs;
 }
 
 Place::Ptr InputModelPDPD::InputModelPDPDImpl::getPlaceByTensorName (const std::string& tensorName) const {
-    for (auto var_places_in_block : var_places) {
+    for (auto var_places_in_block : m_var_places) {
         if (var_places_in_block.count(tensorName))
             return var_places_in_block.at(tensorName);
     }
     return nullptr;
 }
 
-void InputModelPDPD::InputModelPDPDImpl::overrideAllOutputs (const std::vector<Place::Ptr>& outputs) {
-    NOT_IMPLEMENTED("overrideAllOutputs");
+void InputModelPDPD::InputModelPDPDImpl::overrideAllInputs (const std::vector<Place::Ptr>& inputs) {
+    // TODO: resolve for different kind of places
+    m_inputs = inputs;
 }
 
-void InputModelPDPD::InputModelPDPDImpl::overrideAllInputs (const std::vector<Place::Ptr>& inputs) {
-    NOT_IMPLEMENTED("overrideAllInputs");
+void InputModelPDPD::InputModelPDPDImpl::overrideAllOutputs (const std::vector<Place::Ptr>& outputs) {
+    // TODO: resolve for different kind of places
+    m_outputs = outputs;
 }
 
 void InputModelPDPD::InputModelPDPDImpl::extractSubgraph (const std::vector<Place::Ptr>& inputs, const std::vector<Place::Ptr>& outputs) {
-    NOT_IMPLEMENTED("extractSubgraph");
+    overrideAllInputs(inputs);
+    overrideAllOutputs(outputs);
 }
 
-void InputModelPDPD::InputModelPDPDImpl::setDefaultShape (Place::Ptr place, const ngraph::Shape&) {
+void InputModelPDPD::InputModelPDPDImpl::setDefaultShape (Place::Ptr place, const ngraph::Shape& shape) {
     NOT_IMPLEMENTED("setDefaultShape");
 }
 
 void InputModelPDPD::InputModelPDPDImpl::setPartialShape (Place::Ptr place, const ngraph::PartialShape& p_shape) {
-    NOT_IMPLEMENTED("setPartialShape");
+    auto var_place = std::dynamic_pointer_cast<VarPlacePDPD>(place);
+    if (var_place) {
+        var_place->shape = p_shape;
+    }
+    // TODO: resolve for port places
 }
 
 InputModelPDPD::InputModelPDPD (const std::string& _path) : _impl{std::make_shared<InputModelPDPDImpl>(_path)} {}
