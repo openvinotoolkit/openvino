cb4d32b26034b21b9e2ed04a5a6c9e604046f75e
Sergey Lyubimtsev <sergey.lyubimtsev@intel.com>
2021-04-16 19:39:42 +0300

    Define Apache Software License  license, add openvino to install_requires

--- tools/setup.py
+++ tools/setup.py
@@ -8,39 +8,41 @@ Use this script to create a wheel with OpenVINO™ Python* tools:
 
 $ python setup.py sdist bdist_wheel
 """
-
+import os
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as f:
+with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 with open('benchmark/requirements.txt') as f:
     required = f.read().splitlines()
+required.extend(['openvino'])
 
+pkgs = find_packages()
 NAMESPACE = 'openvino.tools'
-packages = find_packages()
 
 setup(
-    name="openvino-tools",
-    version="0.0.0",
+    name='openvino-tools',
+    version='0.0.0',
     author='Intel® Corporation',
+    license='OSI Approved :: Apache Software License',
     author_email='openvino_pushbot@intel.com',
     url='https://github.com/openvinotoolkit/openvino',
-    description="OpenVINO™ Python* tools package",
+    description='OpenVINO™ Python* tools package',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     entry_points={
         'console_scripts': [
             'benchmark_app = openvino.tools.benchmark.main:main'],
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
     ],
-    package_dir={''.join((NAMESPACE, '.', package)) : package.replace('.', '/')
-                 for package in packages},
-    packages=[''.join((NAMESPACE, '.', package)) for package in packages],
+    package_dir={''.join((NAMESPACE, '.', pkg)) : pkg.replace('.', '/')
+                 for pkg in pkgs},
+    packages=[''.join((NAMESPACE, '.', pkg)) for pkg in pkgs],
     install_requires=required,
-    python_requires=">=3.6",
+    python_requires='>=3.6',
 )
