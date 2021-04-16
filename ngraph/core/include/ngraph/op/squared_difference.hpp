1f9f25c4f62f1f6d4784e418ef17499467c9185a
Bartek Szmelczynski <bartosz.szmelczynski@intel.com>
2021-04-16 05:49:27 +0200

    Revise squared difference (#5091)
    * clean SquaredDifference op from legacy FusedOp operation
    
    * add backend and type_prop tests for squared_difference operation
    
    * apply requested changes
    
    * fix op name
    
    * revert date change
    
    * update squared_difference op info with ngraph RTTI
    
    * change test since squred_diff should be binop

--- ngraph/core/include/ngraph/op/squared_difference.hpp
+++ ngraph/core/include/ngraph/op/squared_difference.hpp
@@ -4,11 +4,7 @@
 
 #pragma once
 
-#include "ngraph/node.hpp"
-#include "ngraph/op/op.hpp"
-#include "ngraph/op/util/fused_op.hpp"
-
-NGRAPH_SUPPRESS_DEPRECATED_START
+#include "ngraph/op/util/binary_elementwise_arithmetic.hpp"
 
 namespace ngraph
 {
@@ -19,40 +15,30 @@ namespace ngraph
             /// \brief Calculates an element-wise squared difference between two tensors
             ///
             /// y[i] = (x1[i] - x2[i])^2
-            class NGRAPH_API SquaredDifference : public ngraph::op::util::FusedOp
+            class NGRAPH_API SquaredDifference : public util::BinaryElementwiseArithmetic
             {
             public:
-                static constexpr NodeTypeInfo type_info{"SquaredDifference", 0};
-                const NodeTypeInfo& get_type_info() const override { return type_info; }
-                SquaredDifference();
+                NGRAPH_RTTI_DECLARATION;
+
+                /// \brief Constrcuts an uninitialized squared difference operation
+                SquaredDifference()
+                    : util::BinaryElementwiseArithmetic(AutoBroadcastSpec::NUMPY)
+                {
+                }
                 /// \brief Constructs the squared difference operation.
                 ///
                 /// \param x1 First input tensor
                 /// \param x2 Second input tensor
                 /// \param auto_broadcast Auto broadcast specification
-                SquaredDifference(
-                    const Output<Node>& x1,
-                    const Output<Node>& x2,
-                    const AutoBroadcastSpec& auto_broadcast = AutoBroadcastType::NUMPY);
-
-                bool visit_attributes(AttributeVisitor& visitor) override;
-                virtual OutputVector decompose_op() const override;
+                SquaredDifference(const Output<Node>& x1,
+                                  const Output<Node>& x2,
+                                  const AutoBroadcastSpec& auto_broadcast =
+                                      AutoBroadcastSpec(AutoBroadcastType::NUMPY));
 
                 virtual std::shared_ptr<Node>
                     clone_with_new_inputs(const OutputVector& new_args) const override;
-
-                const AutoBroadcastSpec& get_autob() const override { return m_autobroadcast; }
-                void set_autob(const AutoBroadcastSpec& auto_broadcast)
-                {
-                    m_autobroadcast = auto_broadcast;
-                }
-
-            private:
-                AutoBroadcastSpec m_autobroadcast;
             };
         }
         using v0::SquaredDifference;
     } // namespace op
 } // namespace ngraph
-
-NGRAPH_SUPPRESS_DEPRECATED_END
--- ngraph/test/runtime/interpreter/evaluates_map.cpp
+++ ngraph/test/runtime/interpreter/evaluates_map.cpp
@@ -2114,7 +2114,7 @@ namespace
                                                   outputs[0]->get_data_ptr<T>(),
                                                   inputs[0]->get_shape(),
                                                   inputs[1]->get_shape(),
-                                                  ngraph::op::AutoBroadcastSpec::NUMPY);
+                                                  op->get_autob());
         return true;
     }
 
