0e403d539c37157c453bf8bfa33b0e279a9791c1
sadolini <svetlana.a.dolinina@intel.com>
2021-04-16 13:32:48 +0300

    fix pylint

--- model-optimizer/extensions/front/kaldi/replace_timeheightconvolution.py
+++ model-optimizer/extensions/front/kaldi/replace_timeheightconvolution.py
@@ -1,8 +1,6 @@
 # Copyright (C) 2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-import numpy as np
-
 from mo.front.common.partial_infer.utils import int64_array
 from mo.front.common.replacement import FrontReplacementOp
 from mo.graph.graph import Node, Graph
