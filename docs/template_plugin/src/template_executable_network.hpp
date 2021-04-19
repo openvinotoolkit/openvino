a9fbc030f208959ff4f94dd66380df60e89a2221
Anton Pankratv <anton.pankratov@intel.com>
2021-04-19 15:16:47 +0300

    Merged internal Infer Request implementation (#5125)

--- docs/template_plugin/src/template_executable_network.hpp
+++ docs/template_plugin/src/template_executable_network.hpp
@@ -36,9 +36,9 @@ public:
     // Methods from a base class ExecutableNetworkThreadSafeDefault
 
     void ExportImpl(std::ostream& model) override;
-    InferenceEngine::InferRequestInternal::Ptr CreateInferRequestImpl(InferenceEngine::InputsDataMap networkInputs,
+    InferenceEngine::IInferRequestInternal::Ptr CreateInferRequestImpl(InferenceEngine::InputsDataMap networkInputs,
                                                                       InferenceEngine::OutputsDataMap networkOutputs) override;
-    InferenceEngine::IInferRequest::Ptr CreateInferRequest() override;
+    InferenceEngine::IInferRequestInternal::Ptr CreateInferRequest() override;
     InferenceEngine::Parameter GetMetric(const std::string &name) const override;
     InferenceEngine::Parameter GetConfig(const std::string &name) const override;
 
