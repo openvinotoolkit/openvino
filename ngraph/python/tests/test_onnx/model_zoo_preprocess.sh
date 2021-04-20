dcf883151da7b1290a8721309991445030af59a1
Rafal Blaczkowski <rafal.blaczkowski@intel.com>
2021-04-20 19:49:33 +0200

    Update model preproces script

--- ngraph/python/tests/test_onnx/model_zoo_preprocess.sh
+++ ngraph/python/tests/test_onnx/model_zoo_preprocess.sh
@@ -5,8 +5,8 @@
 
 set -e
 
-# default ONNX Model Zoo commit hash ID
-#ONNX_SHA=d58213534f2a4d1c4b19ba62b3bb5f544353256e
+# default ONNX Model Zoo commit hash ID:
+ONNX_SHA=d58213534f2a4d1c4b19ba62b3bb5f544353256e
 
 MODELS_DIR="$HOME/.onnx/model_zoo"
 ENABLE_MSFT=false
@@ -25,7 +25,7 @@ function print_help {
     echo "    -f force update of a chosen model"
 }
 
-while getopts ":homfd:" opt; do
+while getopts "homfd:s:" opt; do
     case ${opt} in
         h )
             print_help
@@ -45,7 +45,6 @@ while getopts ":homfd:" opt; do
         s )
             ONNX_SHA="$OPTARG"
             ;;
-
         m )
             ENABLE_MSFT_MODELS=true
             ;;
@@ -80,12 +79,18 @@ function pull_and_postprocess_onnx_model_zoo() {
     mkdir -p test_data_set_0
     mv *.pb test_data_set_0/
 
-    # Save SHA of successfully post processed repository
-    git rev-parse HEAD > "$MODEL_ZOO_DIR/onnx_sha"
+    rm -f $MODEL_ZOO_DIR/executing_$ONNX_SHA
 }
 
 function update_onnx_models() {
+    while [[ -f $MODEL_ZOO_DIR/executing_$ONNX_SHA ]];
+        do
+            echo "Onnx Models update are currently executing - sleeping 5 minutes"
+            sleep 300
+        done
+
     if [[ ! -d $ONNX_MODELS_DIR ]] ; then
+        touch $MODEL_ZOO_DIR/executing_$ONNX_SHA
         echo "The ONNX Model Zoo repository doesn't exist on your filesystem then will be cloned"
         git clone https://github.com/onnx/models.git "$ONNX_MODELS_DIR"
         cd "$ONNX_MODELS_DIR"
@@ -100,15 +105,6 @@ function update_onnx_models() {
             echo "The ONNX Model Zoo repository doesn't exist then will be cloned"
             git clone https://github.com/onnx/models.git "$ONNX_MODELS_DIR"
         fi
-    fi
-
-    cd "$ONNX_MODELS_DIR"
-    export CURRENT_ONNX_MODELS_SHA=`head -n1 "$MODEL_ZOO_DIR/onnx_sha"  2> /dev/null 2>&1`
-    if [[ $ONNX_SHA = $CURRENT_ONNX_MODELS_SHA ]] ; then
-        echo "ONNX Model Zoo repository is in the right state"
-    else
-        printf "Current onnx model zoo state is: %s \nChecking out to expected onnx model zoo state: %s \n\n" "$CURRENT_ONNX_MODELS_SHA" "$ONNX_SHA"
-        pull_and_postprocess_onnx_model_zoo
     fi
 }
 
