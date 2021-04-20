7112e52d3297b529f4abdbc0683c01d77a4aafaf
Alexandra Sidorova <alexandra.sidorova@intel.com>
2021-04-20 17:28:04 +0300

    Fixed size

--- inference-engine/src/mkldnn_plugin/nodes/mkldnn_space_to_depth_node.cpp
+++ inference-engine/src/mkldnn_plugin/nodes/mkldnn_space_to_depth_node.cpp
@@ -150,9 +150,9 @@ void MKLDNNSpaceToDepthNode::createPrimitive() {
     PermuteParams params;
     params.data_size = getSelectedPrimitiveDescriptor()->getConfig().inConfs[0].desc.getPrecision().size();
     params.order.resize(reshapedRank, 0);
-    params.src_block_order.resize(nDims);
-    params.dst_block_order.resize(nDims);
-    params.dst_block_dims.resize(nDims);
+    params.src_block_order.resize(reshapedRank);
+    params.dst_block_order.resize(reshapedRank);
+    params.dst_block_dims.resize(reshapedRank);
     params.src_block_dims.resize(reshapedRank);
     params.src_block_dims[0] = srcDims[0];
 
@@ -223,7 +223,7 @@ void MKLDNNSpaceToDepthNode::createPrimitive() {
 
     std::iota(params.src_block_order.begin(), params.src_block_order.end(), 0);
     std::iota(params.dst_block_order.begin(), params.dst_block_order.end(), 0);
-    for (size_t i = 0; i < nDims; i++)
+    for (size_t i = 0; i < reshapedRank; i++)
         params.dst_block_dims[i] = params.src_block_dims[params.order[i]];
 
     permuteKernel = std::unique_ptr<PermuteKernel>(new PermuteKernel(params));
