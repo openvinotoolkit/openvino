c140e7053ccb7689eedb7ab6a4c4b8d98d46e0fe
sadolini <svetlana.a.dolinina@intel.com>
2021-04-20 12:11:07 +0300

    fix layout and kernel shapes for old convolutions

--- model-optimizer/extensions/front/kaldi/add_reshape_around_convolution.py
+++ model-optimizer/extensions/front/kaldi/add_reshape_around_convolution.py
@@ -49,7 +49,7 @@ class ReplaceConvolutionReshape(FrontReplacementPattern):
         dst_dtype = np.float32  # even if data_type=FP16 use float32 for shape values
 
         # create Reshape before convolution
-        # shape = [in_shape[0], t, patch_stride, in_shape[1]/(patch_stride*t)]
+        # shape = [in_shape[0], t= in_shape[1]/(patch_stride*t), patch_stride, C=1]
         i_shape = Shape(graph, {'name': node_name + '/Shape'}).create_node()
         shape = Cast(graph, {'name': node_name + '/to_float',
                              'dst_type': dst_dtype}).create_node()
@@ -59,7 +59,7 @@ class ReplaceConvolutionReshape(FrontReplacementPattern):
         N, H = node_to_get_shape_value_of_indices(shape, [0]), node_to_get_shape_value_of_indices(shape, [1])
 
         div = create_op_with_const_inputs(
-            graph, Div, {1: float32_array([node.patch_stride * node.kernel[2]])}, {'name': node_name + '/div_stride_h'})
+            graph, Div, {1: float32_array([node.patch_stride * node.kernel[1]])}, {'name': node_name + '/div_stride_h'})
         div.in_port(0).connect(H.out_port(0))
 
         # added if to avoid changes in old networks where channel dimension is 1 and transpose is not needed
@@ -74,10 +74,10 @@ class ReplaceConvolutionReshape(FrontReplacementPattern):
             sp_dim_2 = 2
 
         concat = create_op_with_const_inputs(graph, Concat, {sp_dim_2: float32_array([node.patch_stride]),
-                                                             sp_dim_1: float32_array([node.kernel[2]])},
+                                                             channel_dim: float32_array([node.kernel[1]])},
                                              {'name': node_name + '/concat_all_dims', 'in_ports_count': 4, 'axis': 0})
         concat.in_port(0).connect(N.out_port(0))
-        concat.in_port(channel_dim).connect(div.out_port(0))
+        concat.in_port(sp_dim_1).connect(div.out_port(0))
 
         reshape_pattern = Cast(graph, {'name': node_name + '/to_int', 'dst_type': np.int64}).create_node()
         concat.out_port(0).connect(reshape_pattern.in_port(0))
