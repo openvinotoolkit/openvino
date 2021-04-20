fe19089157c762b6ee58d66d6901ac2528032157
VGavrilov <vladimir.gavrilov@intel.com>
2021-04-20 15:36:14 +0300

    Tests for transformations were moved into unit_tests directory.

--- model-optimizer/unit_tests/extensions/front/tf/complex_abs_test.py
+++ model-optimizer/unit_tests/extensions/front/tf/complex_abs_test.py
@@ -9,7 +9,7 @@ import numpy as np
 from extensions.front.tf.complex_abs import ComplexAbs
 from mo.front.common.partial_infer.utils import int64_array
 from mo.utils.ir_engine.compare_graphs import compare_graphs
-from mo.utils.unittest.graph import build_graph
+from unit_tests.utils.graph import build_graph
 
 
 graph_node_attrs = {
--- model-optimizer/unit_tests/extensions/middle/TFRollToRoll_test.py
+++ model-optimizer/unit_tests/extensions/middle/TFRollToRoll_test.py
@@ -6,7 +6,7 @@ import unittest
 from extensions.middle.TFRollToRoll import TFRollToRoll
 from mo.front.common.partial_infer.utils import int64_array
 from mo.utils.ir_engine.compare_graphs import compare_graphs
-from mo.utils.unittest.graph import build_graph
+from unit_tests.utils.graph import build_graph
 
 
 graph_node_attrs = {
