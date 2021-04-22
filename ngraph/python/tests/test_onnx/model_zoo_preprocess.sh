2822e50814e78af4a5a2f6db9a716e07ad219037
Rafal Blaczkowski <rafal.blaczkowski@intel.com>
2021-04-22 07:58:11 +0200

    Enable parallel execution open vino onnx ci (#5332)
    * Separate workdir for parallel runs
    
    * Use random
    
    * Get back old working configuration
    
    * Change label
    
    * Debug parameters
    
    * Change models dir to SHA approach
    
    * Fix WORKDIR names
    
    * Change mountig models dirs
    
    * Update model preproces script
    
    * Improvements
    
    * Isolate all private variables
    
    * Fix model dir path
    
    * Update model preproces
    
    * Update node label
    
    * Remove debug printlns

--- ngraph/python/tests/test_onnx/model_zoo_preprocess.sh
+++ ngraph/python/tests/test_onnx/model_zoo_preprocess.sh
@@ -5,7 +5,7 @@
 
 set -e
 
-# provide ONNX Model Zoo commit hash ID to update:
+# default ONNX Model Zoo commit hash ID:
 ONNX_SHA=d58213534f2a4d1c4b19ba62b3bb5f544353256e
 
 MODELS_DIR="$HOME/.onnx/model_zoo"
@@ -20,11 +20,12 @@ function print_help {
     echo "    -d <DIR> set location of the models (for onnx model ZOO and MSFT models)"
     printf "    By default the models location is: %s\n" "$HOME/.onnx/model_zoo"
     echo "    -o update Onnx Model Zoo models"
+    echo "    -s Onnx Model Zoo commit SHA"
     echo "    -m update MSFT models"
     echo "    -f force update of a chosen model"
 }
 
-while getopts ":homfd:" opt; do
+while getopts "homfd:s:" opt; do
     case ${opt} in
         h )
             print_help
@@ -41,6 +42,9 @@ while getopts ":homfd:" opt; do
         o )
             ENABLE_ONNX_MODELS_ZOO=true
             ;;
+        s )
+            ONNX_SHA="$OPTARG"
+            ;;
         m )
             ENABLE_MSFT_MODELS=true
             ;;
@@ -52,7 +56,7 @@ done
 shift $((OPTIND -1))
 
 MODEL_ZOO_DIR="$MODELS_DIR/model_zoo"
-ONNX_MODELS_DIR="$MODEL_ZOO_DIR/onnx_model_zoo"
+ONNX_MODELS_DIR="$MODEL_ZOO_DIR/onnx_model_zoo_$ONNX_SHA"
 MSFT_MODELS_DIR="$MODEL_ZOO_DIR/MSFT"
 
 function pull_and_postprocess_onnx_model_zoo() {
@@ -75,12 +79,23 @@ function pull_and_postprocess_onnx_model_zoo() {
     mkdir -p test_data_set_0
     mv *.pb test_data_set_0/
 
-    # Save SHA of successfully post processed repository
-    git rev-parse HEAD > "$MODEL_ZOO_DIR/onnx_sha"
+    rm -f $MODEL_ZOO_DIR/executing_$ONNX_SHA
 }
 
 function update_onnx_models() {
+  if test `find $MODEL_ZOO_DIR/executing_$ONNX_SHA -mmin +60 2>/dev/null`;then
+        rm -rf $ONNX_MODELS_DIR
+        rm -f $MODEL_ZOO_DIR/executing_$ONNX_SHA
+  fi
+
+    while [[ -f $MODEL_ZOO_DIR/executing_$ONNX_SHA ]];
+        do
+            echo "Onnx Models update are currently executing - sleeping 5 minutes"
+            sleep 300
+        done
+
     if [[ ! -d $ONNX_MODELS_DIR ]] ; then
+        touch $MODEL_ZOO_DIR/executing_$ONNX_SHA
         echo "The ONNX Model Zoo repository doesn't exist on your filesystem then will be cloned"
         git clone https://github.com/onnx/models.git "$ONNX_MODELS_DIR"
         cd "$ONNX_MODELS_DIR"
@@ -95,15 +110,6 @@ function update_onnx_models() {
             echo "The ONNX Model Zoo repository doesn't exist then will be cloned"
             git clone https://github.com/onnx/models.git "$ONNX_MODELS_DIR"
         fi
-    fi
-
-    cd "$ONNX_MODELS_DIR"
-    export CURRENT_ONNX_MODELS_SHA=`head -n1 "$MODEL_ZOO_DIR/onnx_sha"  2> /dev/null 2>&1`
-    if [[ $ONNX_SHA = $CURRENT_ONNX_MODELS_SHA ]] ; then
-        echo "ONNX Model Zoo repository is in the right state"
-    else
-        printf "Current onnx model zoo state is: %s \nChecking out to expected onnx model zoo state: %s \n\n" "$CURRENT_ONNX_MODELS_SHA" "$ONNX_SHA"
-        pull_and_postprocess_onnx_model_zoo
     fi
 }
 
