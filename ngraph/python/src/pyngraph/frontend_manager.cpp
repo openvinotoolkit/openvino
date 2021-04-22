e6c0f0b67f073e0b0a42849001584c434da0b4a1
Michael Nosov <Mikhail.Nosov@intel.com>
2021-04-22 21:11:13 +0300

    FE Python bindings refactoring + full unit test coverage

--- ngraph/python/src/pyngraph/frontend_manager.cpp
+++ ngraph/python/src/pyngraph/frontend_manager.cpp
@@ -1,25 +1,240 @@
-//*****************************************************************************
-// Copyright 2017-2021 Intel Corporation
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//*****************************************************************************
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
+#include <pybind11/functional.h>
 
 #include "frontend_manager/frontend_manager.hpp"
-#include "pyngraph/function.hpp"
+#include <iostream>
+
+using namespace ngraph::frontend;
+
+//------------------ PLACE wrappers and trampolines -------------------
+// Base class for deriving on Python side, not derived from Place
+class PyPlace
+{
+public:
+    PyPlace() {}
+    virtual ~PyPlace() {}
+
+    virtual bool is_input() const {
+        PYBIND11_OVERLOAD_PURE(bool, PyPlace, is_input);
+    }
+
+    virtual bool is_output() const {
+        PYBIND11_OVERLOAD_PURE(bool, PyPlace, is_output);
+    }
+
+    virtual bool is_equal(std::shared_ptr<PyPlace> other) const {
+        PYBIND11_OVERLOAD_PURE(bool, PyPlace, is_equal, other);
+    }
+
+    virtual std::vector<std::string> get_names() const {
+        PYBIND11_OVERLOAD_PURE(std::vector<std::string>, PyPlace, get_names);
+    }
+};
+
+class InputModelWrapper;
+
+class PlaceWrapper : public Place
+{
+    std::shared_ptr<PyPlace> m_actual;
+    std::weak_ptr<const InputModelWrapper> m_model;
+public:
+    PlaceWrapper(std::shared_ptr<PyPlace> actual,
+                 std::shared_ptr<const InputModelWrapper> model): m_actual(actual), m_model(model) {}
+    ~PlaceWrapper() override {}
+
+    std::shared_ptr<PyPlace> getActual() { return m_actual; }
+    std::shared_ptr<const InputModelWrapper> getModel() { return m_model.lock(); }
+
+    bool isInput() const override {
+        return m_actual->is_input();
+    }
+
+    bool isOutput() const override {
+        return m_actual->is_output();
+    }
+
+    std::vector<std::string> getNames() const override {
+        return m_actual->get_names();
+    }
+
+    bool isEqual(Place::Ptr place) const override {
+        auto w = std::dynamic_pointer_cast<PlaceWrapper>(place);
+        if (!w) return false;
+        return m_actual->is_equal(w->m_actual);
+    }
+    friend class InputModelWrapper;
+};
+
+//------------------ PLACE wrappers and trampolines -------------------
+// Base class for deriving on Python side, not derived from InputModel
+class PyInputModel
+{
+public:
+    PyInputModel() {}
+    virtual ~PyInputModel() {}
+
+    virtual std::vector<std::shared_ptr<PyPlace>> get_inputs () const {
+        PYBIND11_OVERLOAD(std::vector<std::shared_ptr<PyPlace>>, PyInputModel, get_inputs);
+    }
+
+    virtual std::vector<std::shared_ptr<PyPlace>> get_outputs () const {
+        PYBIND11_OVERLOAD(std::vector<std::shared_ptr<PyPlace>>, PyInputModel, get_outputs);
+    }
+
+    virtual std::shared_ptr<PyPlace> get_place_by_tensor_name (const std::string& tensorName) const {
+        PYBIND11_OVERLOAD(std::shared_ptr<PyPlace>, PyInputModel, get_place_by_tensor_name, tensorName);
+    }
+
+    virtual void override_all_inputs (const std::vector<std::shared_ptr<PyPlace>>& inputs) {
+        PYBIND11_OVERLOAD(void, PyInputModel, override_all_inputs, inputs);
+    }
+
+    virtual void override_all_outputs (const std::vector<std::shared_ptr<PyPlace>>& outputs) {
+        PYBIND11_OVERLOAD(void, PyInputModel, override_all_outputs, outputs);
+    }
+
+    virtual void extract_subgraph(const std::vector<std::shared_ptr<PyPlace>>& inputs,
+                                  const std::vector<std::shared_ptr<PyPlace>>& outputs) {
+        PYBIND11_OVERLOAD(void, PyInputModel, extract_subgraph, inputs, outputs);
+    }
+
+    virtual void set_partial_shape(std::shared_ptr<PyPlace> place, const ngraph::PartialShape& shape) {
+        PYBIND11_OVERLOAD(void, PyInputModel, set_partial_shape, place, shape);
+    }
+};
+
+class FrontEndWrapper;
+class InputModelWrapper : public InputModel, public std::enable_shared_from_this<InputModelWrapper>
+{
+    std::shared_ptr<PyInputModel> m_actual;
+    std::weak_ptr<const FrontEndWrapper> m_frontEnd;
+public:
+    InputModelWrapper(std::shared_ptr<PyInputModel> actual,
+                      std::shared_ptr<const FrontEndWrapper> fe):
+            m_actual(actual), m_frontEnd(fe) {}
+    ~InputModelWrapper() override {}
+
+    std::shared_ptr<PyInputModel> getActual() { return m_actual; }
+    std::shared_ptr<const FrontEndWrapper> getFrontEnd() { return m_frontEnd.lock(); }
+
+    std::vector<Place::Ptr> getInputs() const override {
+        auto inputs = m_actual->get_inputs();
+        std::vector<Place::Ptr> res;
+        for (const auto& input : inputs) {
+            res.push_back(std::make_shared<PlaceWrapper>(input, shared_from_this()));
+        }
+        return res;
+    }
+
+    std::vector<Place::Ptr> getOutputs() const override {
+        auto outputs = m_actual->get_outputs();
+        std::vector<Place::Ptr> res;
+        for (const auto& output : outputs) {
+            res.push_back(std::make_shared<PlaceWrapper>(output, shared_from_this()));
+        }
+        return res;
+    }
+
+    Place::Ptr getPlaceByTensorName(const std::string& tensorName) const override {
+        auto place = m_actual->get_place_by_tensor_name(tensorName);
+        return std::make_shared<PlaceWrapper>(place, shared_from_this());
+    }
+
+    void setPartialShape(Place::Ptr place, const ngraph::PartialShape& newShape) override {
+        auto placeWrapper = std::dynamic_pointer_cast<PlaceWrapper>(place);
+        if (!placeWrapper || this != placeWrapper->getModel().get()) {
+            throw std::runtime_error("Invalid Place object to override");
+        }
+        m_actual->set_partial_shape(placeWrapper->m_actual, newShape);
+    }
+
+    void overrideAllInputs (const std::vector<Place::Ptr>& places) override {
+        std::vector<std::shared_ptr<PyPlace>> pyPlaces;
+        for (const auto& place : places) {
+            auto placeWrapper = std::dynamic_pointer_cast<PlaceWrapper>(place);
+            if (!placeWrapper || this != placeWrapper->getModel().get()) {
+                throw std::runtime_error("Invalid Place object to override");
+            }
+            pyPlaces.push_back(placeWrapper->m_actual);
+        }
+        m_actual->override_all_inputs(pyPlaces);
+    }
+
+    void overrideAllOutputs (const std::vector<Place::Ptr>& places) override {
+        std::vector<std::shared_ptr<PyPlace>> pyPlaces;
+        for (const auto& place : places) {
+            auto placeWrapper = std::dynamic_pointer_cast<PlaceWrapper>(place);
+            if (!placeWrapper || this != placeWrapper->getModel().get()) {
+                throw std::runtime_error("Invalid Place object to override");
+            }
+            pyPlaces.push_back(placeWrapper->m_actual);
+        }
+        m_actual->override_all_outputs(pyPlaces);
+    }
+
+    void extractSubgraph(const std::vector<Place::Ptr>& inputs, const std::vector<Place::Ptr>& outputs) override {
+        std::vector<std::shared_ptr<PyPlace>> pyInputs, pyOutputs;
+        for (const auto& input : inputs) {
+            auto placeWrapper = std::dynamic_pointer_cast<PlaceWrapper>(input);
+            if (!placeWrapper || this != placeWrapper->getModel().get()) {
+                throw std::runtime_error("Invalid Input Place object to extract");
+            }
+            pyInputs.push_back(placeWrapper->m_actual);
+        }
+        for (const auto& output : outputs) {
+            auto placeWrapper = std::dynamic_pointer_cast<PlaceWrapper>(output);
+            if (!placeWrapper || this != placeWrapper->getModel().get()) {
+                throw std::runtime_error("Invalid Output Place object to extract");
+            }
+            pyOutputs.push_back(placeWrapper->m_actual);
+        }
+        m_actual->extract_subgraph(pyInputs, pyOutputs);
+    }
+};
+
+// -------------- FRONTEND wrappers and trampolines -------------------
+// Base class for deriving on Python side, not derived from FrontEnd
+class PyFrontEnd
+{
+public:
+    PyFrontEnd() {}
+    virtual ~PyFrontEnd() {}
+
+    virtual std::shared_ptr<PyInputModel> load_from_file(const std::string& path) const {
+        PYBIND11_OVERLOAD_PURE(std::shared_ptr<PyInputModel>, PyFrontEnd, load_from_file, path);
+    }
+
+    virtual std::shared_ptr<ngraph::Function> do_convert(std::shared_ptr<PyInputModel> model) const {
+        PYBIND11_OVERLOAD_PURE(std::shared_ptr<ngraph::Function>, PyFrontEnd, do_convert, model);
+    }
+};
+
+class FrontEndWrapper : public FrontEnd, public std::enable_shared_from_this<FrontEndWrapper>
+{
+    std::shared_ptr<PyFrontEnd> m_actual;
+public:
+    FrontEndWrapper(std::shared_ptr<PyFrontEnd> actual): m_actual(actual) {}
+    ~FrontEndWrapper() override {}
+
+    InputModel::Ptr loadFromFile (const std::string& path) const override {
+        auto pyModel = m_actual->load_from_file(path);
+        return std::make_shared<InputModelWrapper>(pyModel, shared_from_this());
+    }
+
+    std::shared_ptr<ngraph::Function> convert (InputModel::Ptr model) const override {
+        auto mdlWrapper = std::dynamic_pointer_cast<InputModelWrapper>(model);
+        if (!mdlWrapper || this != mdlWrapper->getFrontEnd().get()) {
+            throw std::runtime_error("Invalid model to convert with this FrontEnd");
+        }
+        return m_actual->do_convert(mdlWrapper->getActual());
+    }
+};
 
 namespace py = pybind11;
 
@@ -33,6 +248,15 @@ void regclass_pyngraph_FrontEndManager(py::module m)
     fem.def(py::init<>());
 
     fem.def("availableFrontEnds", &ngraph::frontend::FrontEndManager::availableFrontEnds);
+    fem.def("registerFrontEnd", [](FrontEndManager& self,
+                                          const std::string& name,
+                                          std::function<std::shared_ptr<PyFrontEnd>(FrontEndCapabilities)> creator) {
+        self.registerFrontEnd(name, [=](FrontEndCapabilities fec) {
+            auto pyFE = creator(fec);
+            return std::make_shared<FrontEndWrapper>(pyFE);
+        });
+    });
+
     fem.def("loadByFramework",
             &ngraph::frontend::FrontEndManager::loadByFramework,
             py::arg("framework"),
@@ -41,43 +265,52 @@ void regclass_pyngraph_FrontEndManager(py::module m)
 
 void regclass_pyngraph_FrontEnd(py::module m)
 {
-    py::class_<ngraph::frontend::FrontEnd, std::shared_ptr<ngraph::frontend::FrontEnd>> fem(
+    py::class_<PyFrontEnd, std::shared_ptr<PyFrontEnd>> pyFE(
         m, "FrontEnd", py::dynamic_attr());
-    fem.doc() = "ngraph.impl.FrontEnd wraps ngraph::frontend::FrontEnd";
+    py::class_<FrontEnd, std::shared_ptr<FrontEnd>> wrapper(
+            m, "FrontEndWrapper", py::dynamic_attr());
+    pyFE.doc() = "ngraph.impl.FrontEnd wraps ngraph::frontend::FrontEnd";
+    pyFE.def(py::init<>());
 
-    fem.def("loadFromFile", &ngraph::frontend::FrontEnd::loadFromFile, py::arg("path"));
-    fem.def("convert",
-            static_cast<std::shared_ptr<ngraph::Function> (ngraph::frontend::FrontEnd::*)(
-                ngraph::frontend::InputModel::Ptr) const>(&ngraph::frontend::FrontEnd::convert));
-    fem.def("convert",
-            static_cast<std::shared_ptr<ngraph::Function> (ngraph::frontend::FrontEnd::*)(
-                std::shared_ptr<ngraph::Function>) const>(&ngraph::frontend::FrontEnd::convert));
+    wrapper.def("loadFromFile", &FrontEnd::loadFromFile, py::arg("path"));
+    wrapper.def("convert", [](FrontEnd& self,
+            InputModel::Ptr model) -> std::shared_ptr<ngraph::Function> {
+        return self.convert(model);
+    });
 }
 
 void regclass_pyngraph_Place(py::module m)
 {
-    py::class_<ngraph::frontend::Place, std::shared_ptr<ngraph::frontend::Place>> place(
-        m, "Place", py::dynamic_attr());
-    place.doc() = "ngraph.impl.Place wraps ngraph::frontend::Place";
+    py::class_<Place, std::shared_ptr<Place>> place(
+            m, "PlaceWrapper", py::dynamic_attr());
+    py::class_<PyPlace, std::shared_ptr<PyPlace>> pyPlace(
+            m, "Place", py::dynamic_attr());
+    pyPlace.doc() = "ngraph.impl.Place wraps ngraph::frontend::Place";
+    pyPlace.def(py::init<>());
 
     place.def("isInput", &ngraph::frontend::Place::isInput);
     place.def("isOutput", &ngraph::frontend::Place::isOutput);
     place.def("getNames", &ngraph::frontend::Place::getNames);
-    place.def("isEqual", &ngraph::frontend::Place::isEqual);
+    place.def("isEqual", [](Place& self, std::shared_ptr<Place> other) -> bool {
+        return self.isEqual(other);
+    });
 }
 
 void regclass_pyngraph_InputModel(py::module m)
 {
-    py::class_<ngraph::frontend::InputModel, std::shared_ptr<ngraph::frontend::InputModel>> im(
+    py::class_<InputModel, std::shared_ptr<InputModel>> im(
+            m, "InputModelWrapper", py::dynamic_attr());
+    py::class_<PyInputModel, std::shared_ptr<PyInputModel>> pyIM(
         m, "InputModel", py::dynamic_attr());
-    im.doc() = "ngraph.impl.InputModel wraps ngraph::frontend::InputModel";
-    im.def("extractSubgraph", &ngraph::frontend::InputModel::extractSubgraph);
-    im.def("getPlaceByTensorName", &ngraph::frontend::InputModel::getPlaceByTensorName);
-    im.def("setPartialShape", &ngraph::frontend::InputModel::setPartialShape);
-    im.def("getInputs", &ngraph::frontend::InputModel::getInputs);
-    im.def("getOutputs", &ngraph::frontend::InputModel::getOutputs);
-    im.def("overrideAllInputs", &ngraph::frontend::InputModel::overrideAllInputs);
-    im.def("overrideAllOutputs", &ngraph::frontend::InputModel::overrideAllOutputs);
+    pyIM.doc() = "ngraph.impl.InputModel wraps ngraph::frontend::InputModel";
+    pyIM.def(py::init<>());
+    im.def("extractSubgraph", &InputModel::extractSubgraph);
+    im.def("getPlaceByTensorName", &InputModel::getPlaceByTensorName);
+    im.def("setPartialShape", &InputModel::setPartialShape);
+    im.def("getInputs", &InputModel::getInputs);
+    im.def("getOutputs", &InputModel::getOutputs);
+    im.def("overrideAllInputs", &InputModel::overrideAllInputs);
+    im.def("overrideAllOutputs", &InputModel::overrideAllOutputs);
 }
 
 void regclass_pyngraph_FEC(py::module m)
@@ -98,4 +331,10 @@ void regclass_pyngraph_FEC(py::module m)
         [](const ngraph::frontend::FrontEndCapabilities& a,
            const ngraph::frontend::FrontEndCapabilities& b) { return a == b; },
         py::is_operator());
+
+    type.def("__str__", [](const ngraph::frontend::FrontEndCapabilities& self) -> std::string {
+        std::stringstream ss;
+        ss << self;
+        return ss.str();
+    });
 }
--- ngraph/python/tests/test_ngraph/test_frontendmanager.py
+++ ngraph/python/tests/test_ngraph/test_frontendmanager.py
@@ -1,14 +1,298 @@
-# Copyright (C) 2018-2021 Intel Corporation
+# Copyright (C) 2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from ngraph import FrontEndManager
+from ngraph import FrontEndManager, FrontEnd, FrontEndCapabilities, InputModel, Place, PartialShape
+import pytest
+
+class MockPlace(Place):
+    def __init__(self, _isInput, _isOutput, _names):
+        self.m_isInput = _isInput
+        self.m_isOutput = _isOutput
+        self.m_names = _names
+        self.m_eqPlace = None
+        self.m_isEqualCnt = 0
+        self.m_isInputCnt = 0
+        self.m_isOutputCnt = 0
+        self.m_getNamesCnt = 0
+        super(MockPlace, self).__init__()
+
+    def set_equal(self, place):
+        self.eqPlace = place
+
+    def is_input(self):
+        # print("MockPlace: Called isInput {}".format(self.m_isInput))
+        self.m_isInputCnt += 1
+        return self.m_isInput
+
+    def is_output(self):
+        # print("MockPlace: Called isOutput {}".format(self.m_isOutput))
+        self.m_isOutputCnt += 1
+        return self.m_isOutput
+
+    def get_names(self):
+        # print("MockPlace: Called getNames {}".format(self.m_names))
+        self.m_getNamesCnt += 1
+        return self.m_names
+
+    def is_equal(self, other):
+        self.m_isEqualCnt += 1
+        return self.eqPlace == other
+
+
+class MockInputModel(InputModel):
+    def __init__(self, inputPlace, outputPlace):
+        self.m_inputPlace = inputPlace
+        self.m_outputPlace = outputPlace
+        self.m_getInputsCnt = 0
+        self.m_getOutputsCnt = 0
+        self.m_overrideInputsCnt = 0
+        self.m_overrideOutputsCnt = 0
+        self.m_getPlaceByTensorNameCnt = 0
+        self.m_extractSubGraph = 0
+        self.m_setPartialShapeCnt = 0
+        self.m_partialShape = []
+        super(MockInputModel, self).__init__()
+
+    def get_inputs(self):
+        # print("Mock: Called get_inputs")
+        self.m_getInputsCnt += 1
+        return [self.m_inputPlace]
+
+    def get_outputs(self):
+        # print("Mock: Called get_outputs")
+        self.m_getOutputsCnt += 1
+        return [self.m_outputPlace]
+
+    def override_all_inputs(self, inputs):
+        # print("Mock: Called override_all_inputs")
+        self.m_overrideInputsCnt += 1
+        return
+
+    def override_all_outputs(self, outputs):
+        # print("Mock: Called override_all_outputs")
+        self.m_overrideOutputsCnt += 1
+        return
+
+    def extract_subgraph(self, inputs, outputs):
+        # print("Mock: Called extract_subgraph")
+        self.m_extractSubGraph += 1
+        return
+
+    def get_place_by_tensor_name(self, name):
+        # print("Mock: Called get_place_by_tensor_name {}".format(name))
+        self.m_getPlaceByTensorNameCnt += 1
+        if 'Input' in name:
+            return self.m_inputPlace
+        elif 'Output' in name:
+            return self.m_outputPlace
+        return None
+
+    def set_partial_shape(self, place, shape):
+        # print("Mock: Called set_partial_shape {}".format(name))
+        self.m_setPartialShapeCnt += 1
+        self.m_partialShape = shape
+
+
+class MockFrontEnd(FrontEnd):
+    def __init__(self, mdl):
+        self.mockModel = mdl
+        self.loadCnt = 0
+        self.convertCnt = 0
+        super(MockFrontEnd, self).__init__()
+
+    def load_from_file(self, path):
+        self.loadCnt += 1
+        # print("Mock: Called doLoadFromFile: {}".format(path))
+        return self.mockModel
+
+    def do_convert(self, model):
+        self.convertCnt += 1
+        # print("Mock: Called convert {}".format(model))
+        return None
+
+
+def setup(feName="mock"):
+    # -------------- Setup mock objects ----------------
+    mockInputPlace = MockPlace(True, False, ['nameInput1', 'nameInput2'])
+    mockInputPlace.set_equal(mockInputPlace)
+    mockOutputPlace = MockPlace(False, True, ['nameOutput1', 'nameOutput2', 'nameOutput3'])
+    mockOutputPlace.set_equal(mockOutputPlace)
+    mockModel = MockInputModel(mockInputPlace, mockOutputPlace)
+    mockFe = MockFrontEnd(mockModel)
+
+    def createMock(fec):
+        # print("Create Mock Called, caps = {}".format(fec))
+        assert fec == FrontEndCapabilities.WILDCARDS
+        return mockFe
+
+    fem = FrontEndManager()
+    fem.registerFrontEnd(feName, createMock)
+    frontEnds = fem.availableFrontEnds()
+    # print("Available frontends: {}".format(frontEnds))
+    assert feName in frontEnds
+    return fem, mockFe, mockModel, mockInputPlace, mockOutputPlace
+
 
 def test_frontendmanager():
-    fem = None
-    try:
-        fem = FrontEndManager()
-    except Exception:
-        assert False
+    fem, mockFe, mockModel, mockInputPlace, mockOutputPlace = setup()
 
-    frontEnds = fem.availableFrontEnds()
-    assert frontEnds is not None
+    fe = fem.loadByFramework(framework="mock", capabilities=FrontEndCapabilities.WILDCARDS)
+    assert fe is not None
+    model = fe.loadFromFile("abc.bin")
+    assert mockFe.loadCnt == 1
+
+
+def test_get_inputs():
+    fem, mockFe, mockModel, mockInputPlace, mockOutputPlace = setup()
+    fe = fem.loadByFramework(framework="mock", capabilities=FrontEndCapabilities.WILDCARDS)
+    model = fe.loadFromFile("abc.bin")
+
+    inputs = model.getInputs()
+    assert mockModel.m_getInputsCnt == 1
+    assert len(inputs) == 1
+    assert inputs[0].isInput()
+    assert mockInputPlace.m_isInputCnt == 1
+    assert not inputs[0].isOutput()
+    assert mockInputPlace.m_isOutputCnt == 1
+    assert len(inputs[0].getNames()) == 2
+    assert mockInputPlace.m_getNamesCnt == 1
+
+
+def test_get_outputs():
+    fem, mockFe, mockModel, mockInputPlace, mockOutputPlace = setup()
+    fe = fem.loadByFramework(framework="mock", capabilities=FrontEndCapabilities.WILDCARDS)
+    model = fe.loadFromFile("abc.bin")
+
+    outputs = model.getOutputs()
+    assert mockModel.m_getOutputsCnt == 1
+    assert len(outputs) == 1
+    assert outputs[0].isOutput()
+    assert mockOutputPlace.m_isOutputCnt == 1
+    assert not outputs[0].isInput()
+    assert mockOutputPlace.m_isInputCnt == 1
+    assert len(outputs[0].getNames()) == 3
+    assert mockOutputPlace.m_getNamesCnt == 1
+
+
+def test_is_equal():
+    fem, mockFe, mockModel, mockInputPlace, mockOutputPlace = setup()
+    fe = fem.loadByFramework(framework="mock", capabilities=FrontEndCapabilities.WILDCARDS)
+    model = fe.loadFromFile("abc.bin")
+
+    outputs = model.getOutputs()
+    inputs = model.getInputs()
+    assert inputs[0].isEqual(inputs[0])
+    assert mockInputPlace.m_isEqualCnt == 1
+    assert outputs[0].isEqual(outputs[0])
+    assert mockOutputPlace.m_isEqualCnt == 1
+    assert not inputs[0].isEqual(outputs[0])
+    assert mockInputPlace.m_isEqualCnt == 2
+    assert not outputs[0].isEqual(inputs[0])
+    assert mockOutputPlace.m_isEqualCnt == 2
+
+
+def test_override_inputs():
+    fem, mockFe, mockModel, mockInputPlace, mockOutputPlace = setup()
+    fe = fem.loadByFramework(framework="mock", capabilities=FrontEndCapabilities.WILDCARDS)
+    model = fe.loadFromFile("abc.bin")
+
+    inputs = model.getInputs()
+    model.overrideAllInputs(inputs)
+    assert mockModel.m_overrideInputsCnt == 1
+
+
+def test_override_outputs():
+    fem, mockFe, mockModel, mockInputPlace, mockOutputPlace = setup()
+    fe = fem.loadByFramework(framework="mock", capabilities=FrontEndCapabilities.WILDCARDS)
+    model = fe.loadFromFile("abc.bin")
+
+    outputs = model.getOutputs()
+    model.overrideAllOutputs(outputs)
+    assert mockModel.m_overrideOutputsCnt == 1
+
+
+def test_extract_subgraph():
+    fem, mockFe, mockModel, mockInputPlace, mockOutputPlace = setup()
+    fe = fem.loadByFramework(framework="mock", capabilities=FrontEndCapabilities.WILDCARDS)
+    model = fe.loadFromFile("abc.bin")
+
+    outputs = model.getOutputs()
+    inputs = model.getInputs()
+    model.extractSubgraph(inputs, outputs)
+    assert mockModel.m_extractSubGraph == 1
+
+
+def test_set_partial_shape():
+    fem, mockFe, mockModel, mockInputPlace, mockOutputPlace = setup()
+    fe = fem.loadByFramework(framework="mock", capabilities=FrontEndCapabilities.WILDCARDS)
+    model = fe.loadFromFile("abc.bin")
+
+    inputs = model.getInputs()
+    model.setPartialShape(inputs[0], PartialShape([1, 2, 3, 4]))
+    assert mockModel.m_setPartialShapeCnt == 1
+    assert mockModel.m_partialShape == PartialShape([1, 2, 3, 4])
+
+
+def test_get_place_by_tensor_name():
+    fem, mockFe, mockModel, mockInputPlace, mockOutputPlace = setup()
+    fe = fem.loadByFramework(framework="mock", capabilities=FrontEndCapabilities.WILDCARDS)
+    model = fe.loadFromFile("abc.bin")
+
+    place = model.getPlaceByTensorName('nameInput1')
+    assert mockModel.m_getPlaceByTensorNameCnt == 1
+    assert place.isInput()
+
+    place = model.getPlaceByTensorName('nameOutput2')
+    assert mockModel.m_getPlaceByTensorNameCnt == 2
+    assert place.isOutput()
+
+
+def test_convert():
+    fem, mockFe, mockModel, mockInputPlace, mockOutputPlace = setup()
+    fe = fem.loadByFramework(framework="mock", capabilities=FrontEndCapabilities.WILDCARDS)
+    model = fe.loadFromFile("abc.bin")
+
+    fe.convert(model)
+    assert mockFe.convertCnt == 1
+
+
+def test_error_cases():
+    fem1, mockFe1, mockModel1, mockInputPlace1, mockOutputPlace1 = setup("mock1")
+    fem2, mockFe2, mockModel2, mockInputPlace2, mockOutputPlace2 = setup("mock2")
+    fe1 = fem1.loadByFramework(framework="mock1", capabilities=FrontEndCapabilities.WILDCARDS)
+    fe2 = fem2.loadByFramework(framework="mock2", capabilities=FrontEndCapabilities.WILDCARDS)
+    model1 = fe1.loadFromFile("abc.bin")
+    model2 = fe2.loadFromFile("abc.bin")
+    inputs1 = model1.getInputs()
+    inputs2 = model2.getInputs()
+    outputs1 = model1.getOutputs()
+    outputs2 = model2.getOutputs()
+
+    with pytest.raises(RuntimeError) as excInfo:
+        fe1.convert(model2)
+    assert 'convert' in str(excInfo.value)
+
+    with pytest.raises(RuntimeError) as excInfo:
+        model1.overrideAllInputs(inputs2)
+    assert 'Place' in str(excInfo.value)
+
+    with pytest.raises(RuntimeError) as excInfo:
+        model1.overrideAllOutputs(inputs2)
+    assert 'Place' in str(excInfo.value)
+
+    with pytest.raises(RuntimeError) as excInfo:
+        model1.extractSubgraph(inputs1, outputs2)
+    assert 'Place' in str(excInfo.value)
+
+    with pytest.raises(RuntimeError) as excInfo:
+        model1.extractSubgraph(inputs2, outputs1)
+    assert 'Place' in str(excInfo.value)
+
+    with pytest.raises(RuntimeError) as excInfo:
+        model1.setPartialShape(inputs2[0], PartialShape([1, 2, 3, 4]))
+    assert 'Place' in str(excInfo.value)
+
+
+def test_frontend_caps():
+    capabilities = FrontEndCapabilities.WILDCARDS
+    assert str(capabilities)
