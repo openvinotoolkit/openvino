827706f72060b3d6c3a821ff24098fe67a071b85
Anton Voronov <anton.voronov@intel.com>
2021-04-22 12:00:03 +0300

    [CPU] int8 deconvolution support

--- inference-engine/src/mkldnn_plugin/mkldnn_plugin.cpp
+++ inference-engine/src/mkldnn_plugin/mkldnn_plugin.cpp
@@ -330,7 +330,8 @@ static void Transformation(CNNNetwork& clonedNetwork, const Config& conf) {
                 LayerTransformation::Params(params).setPrecisionsOnActivations({ ngraph::element::u8 }).setSupportAsymmetricQuantization(true))
             .addStandaloneCleanup<MultiplyToGroupConvolutionTransformation, ngraph::opset1::Multiply>(
                 LayerTransformation::Params(params).setPrecisionsOnActivations({ ngraph::element::u8 }))
-            .remove<ConvolutionBackpropDataTransformation, ngraph::opset1::ConvolutionBackpropData>());
+            .add<ConvolutionBackpropDataTransformation, ngraph::opset1::ConvolutionBackpropData>(
+                    LayerTransformation::Params(params).setSupportAsymmetricQuantization(false)));
 
         transformer.transform(nGraphFunc);
     }
