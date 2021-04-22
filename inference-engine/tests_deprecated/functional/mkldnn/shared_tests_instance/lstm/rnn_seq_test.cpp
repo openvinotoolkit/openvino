fa7fc1b60feb64c19cda75c4ceb66a2e27eda723
mandrono <maxim.andronov@intel.com>
2021-04-22 15:57:26 +0300

    remove deprecated test

--- inference-engine/tests_deprecated/functional/mkldnn/shared_tests_instance/lstm/rnn_seq_test.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2018-2021 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
-//
-
-#include "rnn_seq_test.hpp"
-
-RUN_CASE_CP_WITH_SUFFIX(CPU, _smoke, RNNSeqTest, workload);
