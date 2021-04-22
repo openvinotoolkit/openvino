0638425e88fc35e3b56601f15085d643d35fd50f
Efode, Irina <irina.efode@intel.com>
2021-04-22 11:11:12 +0300

    Init

--- inference-engine/tests/functional/plugin/conformance/test_runner/src/core_config.cpp
+++ inference-engine/tests/functional/plugin/conformance/test_runner/src/core_config.cpp
@@ -4,5 +4,15 @@
 
 #include "functional_test_utils/core_config.hpp"
 
+#include "conformance.hpp"
+
 void CoreConfiguration(LayerTestsUtils::LayerTestsCommon* test) {
+    std::shared_ptr<InferenceEngine::Core> core = PluginCache::get().ie();
+    auto availableDevices = core->GetAvailableDevices();
+    std::string targetDevice = std::string(ConformanceTests::targetDevice);
+    if (std::find(availableDevices.begin(), availableDevices.end(), targetDevice) == availableDevices.end()
+        && targetDevice == CommonTestUtils::DEVICE_TEMPLATE) {
+        core->RegisterPlugin(std::string("templatePlugin"),
+                             CommonTestUtils::DEVICE_TEMPLATE);
+    }
 }
--- inference-engine/tests/functional/plugin/conformance/test_runner/src/main.cpp
+++ inference-engine/tests/functional/plugin/conformance/test_runner/src/main.cpp
@@ -52,5 +52,5 @@ int main(int argc, char* argv[]) {
 
     ::testing::InitGoogleTest(&argc, argv);
     ::testing::AddGlobalTestEnvironment(new LayerTestsUtils::TestEnvironment);
-    return RUN_ALL_TESTS();;
+    return RUN_ALL_TESTS();
 }
