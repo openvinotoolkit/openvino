dad986e4e8b2baeb328aab81b1574f844709586d
VGavrilov <vladimir.gavrilov@intel.com>
2021-04-23 17:03:54 +0300

    Fixed types of constants in the transformation ComplexAbs.

--- model-optimizer/extensions/front/tf/complex_abs.py
+++ model-optimizer/extensions/front/tf/complex_abs.py
@@ -9,6 +9,7 @@ from mo.front.common.replacement import FrontReplacementSubgraph
 from mo.front.subgraph_matcher import SubgraphMatch
 from mo.front.tf.graph_utils import create_op_with_const_inputs
 from mo.graph.graph import Graph, rename_nodes
+from mo.middle.passes.convert_data_type import data_type_str_to_np
 
 
 class ComplexAbs(FrontReplacementSubgraph):
@@ -29,16 +30,18 @@ class ComplexAbs(FrontReplacementSubgraph):
         complex_abs = match['abs']
         complex_abs_name = complex_abs.soft_get('name', complex_abs.id)
 
-        pow0 = create_op_with_const_inputs(graph, Pow, {1: np.float32(2.0)},
+        power_type = data_type_str_to_np(graph.graph['cmd_params'].data_type)
+
+        pow0 = create_op_with_const_inputs(graph, Pow, {1: power_type(2.0)},
                                            {'name': complex_abs_name + '/real_part_squared'})
-        pow1 = create_op_with_const_inputs(graph, Pow, {1: np.float32(2.0)},
+        pow1 = create_op_with_const_inputs(graph, Pow, {1: power_type(2.0)},
                                            {'name': complex_abs_name + '/imag_part_squared'})
 
         cmp.in_port(0).get_connection().set_destination(pow0.in_port(0))
         cmp.in_port(1).get_connection().set_destination(pow1.in_port(0))
 
         add = Add(graph, {'name': complex_abs_name + '/squared_abs'}).create_node([pow0, pow1])
-        sqrt = create_op_with_const_inputs(graph, Pow, {1: np.float32(0.5)}, {})
+        sqrt = create_op_with_const_inputs(graph, Pow, {1: power_type(0.5)}, {})
         add.out_port(0).connect(sqrt.in_port(0))
 
         complex_abs.out_port(0).get_connection().set_source(sqrt.out_port(0))
