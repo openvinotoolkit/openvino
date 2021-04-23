a17004a08cf64383c184ed63fe64e39e64e33512
Ewa21 <ewa.tusien@intel.com>
2021-04-23 15:23:50 +0200

    Add test.

--- ngraph/test/onnx/onnx_import_quant.in.cpp
+++ ngraph/test/onnx/onnx_import_quant.in.cpp
@@ -123,6 +123,20 @@ NGRAPH_TEST(${BACKEND_NAME}, onnx_model_dequantize_linear)
     test_case.run();
 }
 
+NGRAPH_TEST(${BACKEND_NAME}, onnx_model_dequantize_linear_scalar_input)
+{
+    auto function = onnx_import::import_onnx_model(
+        file_util::path_join(SERIALIZED_ZOO, "onnx/dequantize_linear_scalar_input.prototxt"));
+
+    auto test_case = test::TestCase<TestEngine>(function);
+    test_case.add_input(std::vector<uint8_t>{0});              // x
+    test_case.add_input(std::vector<float>{2.0f});             // scale
+    test_case.add_input(std::vector<uint8_t>{128});            // zero_point
+
+    test_case.add_expected_output<float>(std::vector<float>{-256.0f});
+    test_case.run();
+}
+
 NGRAPH_TEST(${BACKEND_NAME}, onnx_model_dequantize_linear_scalar_zero_scale_uint8)
 {
     auto function = onnx_import::import_onnx_model(
