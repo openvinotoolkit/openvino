df786b1aa6fff75d578da3ceb5414d69a042f616
Tomasz Socha <tomasz.socha@intel.com>
2021-04-26 14:41:16 +0200

    Move compress tests

--- ngraph/test/onnx/onnx_import.in.cpp
+++ ngraph/test/onnx/onnx_import.in.cpp
@@ -24,7 +24,6 @@
 
 #include "core/null_node.hpp"
 #include "gtest/gtest.h"
-#include "onnx_editor/editor.hpp"
 #include "onnx_import/onnx.hpp"
 #include "onnx_import/onnx_utils.hpp"
 #include "default_opset.hpp"
@@ -4297,77 +4296,5 @@ NGRAPH_TEST(${BACKEND_NAME}, onnx_constant_fill_shape_attribute)
 
     auto test_case = test::TestCase<TestEngine>(function);
     test_case.add_expected_output<int32_t>(Shape{2, 3, 4}, std::vector<int32_t>(24, 5));
-    test_case.run();
-}
-
-NGRAPH_TEST(${BACKEND_NAME}, onnx_compress_axis_0)
-{
-    onnx_editor::ONNXModelEditor editor{
-        file_util::path_join(SERIALIZED_ZOO, "onnx/compress_0.prototxt")};
-
-    std::map<std::string, std::shared_ptr<ngraph::op::Constant>> in_vals;
-
-    in_vals.emplace("input", op::Constant::create(element::f32, Shape{3, 2}, {1., 2., 3., 4., 5., 6.}));
-    in_vals.emplace("condition", op::Constant::create(element::boolean, Shape{3}, {false, true, true}));
-    editor.set_input_values(in_vals);
-
-    const auto function = editor.get_function();
-    auto test_case = test::TestCase<TestEngine>(function);
-
-    test_case.add_expected_output<float>(Shape{2, 2}, {3., 4., 5., 6.});
-    test_case.run();
-}
-
-NGRAPH_TEST(${BACKEND_NAME}, onnx_compress_axis_1)
-{
-    onnx_editor::ONNXModelEditor editor{
-        file_util::path_join(SERIALIZED_ZOO, "onnx/compress_1.prototxt")};
-
-    std::map<std::string, std::shared_ptr<ngraph::op::Constant>> in_vals;
-
-    in_vals.emplace("input", op::Constant::create(element::f32, Shape{3, 2}, {1., 2., 3., 4., 5., 6.}));
-    in_vals.emplace("condition", op::Constant::create(element::boolean, Shape{2}, {false, true}));
-    editor.set_input_values(in_vals);
-
-    const auto function = editor.get_function();
-    auto test_case = test::TestCase<TestEngine>(function);
-
-    test_case.add_expected_output<float>(Shape{3, 1}, {2., 4., 6.});
-    test_case.run();
-}
-
-NGRAPH_TEST(${BACKEND_NAME}, onnx_compress_default_axis)
-{
-    onnx_editor::ONNXModelEditor editor{
-        file_util::path_join(SERIALIZED_ZOO, "onnx/compress_default_axis.prototxt")};
-
-    std::map<std::string, std::shared_ptr<ngraph::op::Constant>> in_vals;
-
-    in_vals.emplace("input", op::Constant::create(element::f32, Shape{3, 2}, {1., 2., 3., 4., 5., 6.}));
-    in_vals.emplace("condition", op::Constant::create(element::boolean, Shape{5}, {false, true, false, false, true}));
-    editor.set_input_values(in_vals);
-
-    const auto function = editor.get_function();
-    auto test_case = test::TestCase<TestEngine>(function);
-
-    test_case.add_expected_output<float>(Shape{2}, {2., 5.});
-    test_case.run();
-}
-
-NGRAPH_TEST(${BACKEND_NAME}, onnx_compress_negative_axis)
-{
-    onnx_editor::ONNXModelEditor editor{
-        file_util::path_join(SERIALIZED_ZOO, "onnx/compress_negative_axis.prototxt")};
-
-    std::map<std::string, std::shared_ptr<ngraph::op::Constant>> in_vals;
-
-    in_vals.emplace("input", op::Constant::create(element::f32, Shape{3, 2}, {1., 2., 3., 4., 5., 6.}));
-    in_vals.emplace("condition", op::Constant::create(element::boolean, Shape{2}, {false, true}));
-    editor.set_input_values(in_vals);
-
-    const auto function = editor.get_function();
-    auto test_case = test::TestCase<TestEngine>(function);
-
-    test_case.add_expected_output<float>(Shape{3, 1}, {2., 4., 6.});
     test_case.run();
 }
