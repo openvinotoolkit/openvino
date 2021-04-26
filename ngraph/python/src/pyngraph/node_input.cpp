5e4daed59fbce1ffd6264ab8a0e1dd7114e475c6
jiwaszki <jan.iwaszkiewicz@intel.com>
2021-04-26 10:14:04 +0200

    Add Input docs

--- ngraph/python/src/pyngraph/node_input.cpp
+++ ngraph/python/src/pyngraph/node_input.cpp
@@ -16,10 +16,64 @@ void regclass_pyngraph_Input(py::module m)
         m, "Input", py::dynamic_attr());
     input.doc() = "ngraph.impl.Input wraps ngraph::Input<Node>";
 
-    input.def("get_node", &ngraph::Input<ngraph::Node>::get_node);
-    input.def("get_index", &ngraph::Input<ngraph::Node>::get_index);
-    input.def("get_element_type", &ngraph::Input<ngraph::Node>::get_element_type);
-    input.def("get_shape", &ngraph::Input<ngraph::Node>::get_shape);
-    input.def("get_partial_shape", &ngraph::Input<ngraph::Node>::get_partial_shape);
-    input.def("get_source_output", &ngraph::Input<ngraph::Node>::get_source_output);
+    input.def("get_node",
+              &ngraph::Input<ngraph::Node>::get_node,
+              R"mydelimiter(
+                Get node referenced by this input handle.
+
+                Returns
+                ----------
+                get_node : Node
+                    Node object referenced by this input handle.
+              )mydelimiter");
+    input.def("get_index",
+              &ngraph::Input<ngraph::Node>::get_index,
+              R"mydelimiter(
+                The index of the input referred to by this input handle.
+
+                Returns
+                ----------
+                get_index : int
+                    Index value as integer.
+              )mydelimiter");
+    input.def("get_element_type",
+              &ngraph::Input<ngraph::Node>::get_element_type,
+              R"mydelimiter(
+                The element type of the input referred to by this input handle.
+
+                Returns
+                ----------
+                get_element_type : Type
+                    Type of the input.
+              )mydelimiter");
+    input.def("get_shape",
+              &ngraph::Input<ngraph::Node>::get_shape,
+              R"mydelimiter(
+                The shape of the input referred to by this input handle.
+
+                Returns
+                ----------
+                get_shape : Shape
+                    Shape of the input.
+              )mydelimiter");
+    input.def("get_partial_shape",
+              &ngraph::Input<ngraph::Node>::get_partial_shape,
+              R"mydelimiter(
+                The partial shape of the input referred to by this input handle.
+
+                Returns
+                ----------
+                get_partial_shape : PartialShape
+                    PartialShape of the input.
+              )mydelimiter");
+    input.def("get_source_output",
+              &ngraph::Input<ngraph::Node>::get_source_output,
+              R"mydelimiter(
+                A handle to the output that is connected to this input.
+
+                Returns
+                ----------
+                get_source_output : Output
+                    Output that is connected to the input.
+              )mydelimiter");
 }
