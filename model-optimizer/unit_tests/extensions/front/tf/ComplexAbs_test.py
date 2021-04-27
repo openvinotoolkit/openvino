e0cac506a82afe5f3a686148e84d81744b82e21f
VGavrilov <vladimir.gavrilov@intel.com>
2021-04-27 12:00:56 +0300

    Written tests for the transformation ComplexAbs.

--- model-optimizer/unit_tests/extensions/front/tf/ComplexAbs_test.py
+++ model-optimizer/unit_tests/extensions/front/tf/ComplexAbs_test.py
@@ -6,42 +6,36 @@ import unittest
 
 import numpy as np
 
-from extensions.front.tf.ComplexAbsAfterComplex import ComplexAbsAfterComplex
+from extensions.front.tf.ComplexAbs import ComplexAbs
 from mo.front.common.partial_infer.utils import int64_array
 from mo.utils.ir_engine.compare_graphs import compare_graphs
 from unit_tests.utils.graph import build_graph
 
 
 graph_node_attrs = {
-    'placeholder_0': {'shape': int64_array([3, 100, 100, 2]), 'type': 'Parameter', 'kind': 'op', 'op': 'Parameter'},
-    'placeholder_1': {'shape': int64_array([3, 100, 100, 2]), 'type': 'Parameter', 'kind': 'op', 'op': 'Parameter'},
-    'complex': {'kind': 'op', 'op': 'Complex'},
+    'placeholder': {'shape': int64_array([3, 100, 100, 2]), 'type': 'Parameter', 'kind': 'op', 'op': 'Parameter'},
     'complex_abs': {'kind': 'op', 'op': 'ComplexAbs'},
     'relu': {'type': 'ReLU', 'kind': 'op', 'op': 'ReLU'},
     'output': {'type': None, 'value': None, 'kind': 'op', 'op': 'Result'},
 }
 
 graph_edges = [
-    ('placeholder_0', 'complex', {'in': 0}),
-    ('placeholder_1', 'complex', {'in': 1}),
-    ('complex', 'complex_abs', {'in': 0}),
+    ('placeholder', 'complex_abs'),
     ('complex_abs', 'relu'),
     ('relu', 'output'),
 ]
 
 
 ref_graph_node_attrs = {
-    'placeholder_0': {'shape': int64_array([3, 100, 100, 2]), 'type': 'Parameter', 'kind': 'op', 'op': 'Parameter'},
-    'placeholder_1': {'shape': int64_array([3, 100, 100, 2]), 'type': 'Parameter', 'kind': 'op', 'op': 'Parameter'},
-    'pow0_const': {
+    'placeholder': {'shape': int64_array([3, 100, 100, 2]), 'type': 'Parameter', 'kind': 'op', 'op': 'Parameter'},
+    'pow2_const': {
         'type': 'Const', 'kind': 'op', 'op': 'Const', 'value': np.float32(2.0)
     },
-    'pow1_const': {
-        'type': 'Const', 'kind': 'op', 'op': 'Const', 'value': np.float32(2.0)
+    'pow2': {'type': 'Power', 'kind': 'op', 'op': 'Pow'},
+    'sum_axis': {
+        'type': 'Const', 'kind': 'op', 'op': 'Const', 'value': int64_array(-1)
     },
-    'pow0': {'type': 'Power', 'kind': 'op', 'op': 'Pow'},
-    'pow1': {'type': 'Power', 'kind': 'op', 'op': 'Pow'},
-    'add': {'type': 'Add', 'kind': 'op', 'op': 'Add'},
+    'sum': {'type': 'ReduceSum', 'kind': 'op', 'op': 'ReduceSum'},
     'sqrt_const': {
         'type': 'Const', 'kind': 'op', 'op': 'Const', 'value': np.float32(0.5)
     },
@@ -51,24 +45,22 @@ ref_graph_node_attrs = {
 }
 
 ref_graph_edges = [
-    ('placeholder_0', 'pow0', {'in': 0}),
-    ('placeholder_1', 'pow1', {'in': 0}),
-    ('pow0_const', 'pow0', {'in': 1}),
-    ('pow1_const', 'pow1', {'in': 1}),
-    ('pow0', 'add', {'in': 0}),
-    ('pow1', 'add', {'in': 1}),
-    ('add', 'sqrt', {'in': 0}),
+    ('placeholder', 'pow2', {'in': 0}),
+    ('pow2_const', 'pow2', {'in': 1}),
+    ('sum_axis', 'sum', {'in': 1}),
+    ('pow2', 'sum', {'in': 0}),
+    ('sum', 'sqrt', {'in': 0}),
     ('sqrt_const', 'sqrt', {'in': 1}),
     ('sqrt', 'relu'),
     ('relu', 'output'),
 ]
 
 
-class ComplexAbsAfterComplexTest(unittest.TestCase):
+class ComplexAbsTest(unittest.TestCase):
     def test_replacement(self):
         graph = build_graph(nodes_attrs=graph_node_attrs, edges=graph_edges)
         graph.stage = 'front'
-        ComplexAbsAfterComplex().find_and_replace_pattern(graph)
+        ComplexAbs().find_and_replace_pattern(graph)
         ref_graph = build_graph(nodes_attrs=ref_graph_node_attrs, edges=ref_graph_edges)
         (flag, resp) = compare_graphs(graph, ref_graph, 'output', check_op_attrs=True)
         self.assertTrue(flag, resp)
