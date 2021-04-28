f8a9b8310b13b7716870a6bbcf2d5695af2f2fa7
blesniewski <bartosz.lesniewski@intel.com>
2021-04-28 10:42:17 +0200

    move squeeze attribute test to a separate file

--- ngraph/test/visitors/op/squeeze.cpp
+++ ngraph/test/visitors/op/squeeze.cpp
@@ -7,9 +7,6 @@
 #include "ngraph/ngraph.hpp"
 #include "ngraph/op/util/attr_types.hpp"
 #include "ngraph/opsets/opset1.hpp"
-#include "ngraph/opsets/opset3.hpp"
-#include "ngraph/opsets/opset4.hpp"
-#include "ngraph/opsets/opset5.hpp"
 
 #include "util/visitor.hpp"
 
@@ -18,7 +15,7 @@ using namespace ngraph;
 using ngraph::test::NodeBuilder;
 using ngraph::test::ValueMap;
 
-TEST(attributes, no_attributes_squeeze_op)
+TEST(attributes, squeeze_op)
 {
     NodeBuilder::get_ops().register_factory<opset1::Squeeze>();
     const auto data_node = make_shared<op::Parameter>(element::f32, Shape{1});
