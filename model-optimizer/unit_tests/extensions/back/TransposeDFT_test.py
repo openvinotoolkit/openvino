d6aa6c43bb8635f9bbe4c517ec7ed8fbd0b5eb47
VGavrilov <vladimir.gavrilov@intel.com>
2021-04-28 12:20:27 +0300

    Tests for the transformation TransposeDFT were rewritten using helper functions.

--- model-optimizer/unit_tests/extensions/back/TransposeDFT_test.py
+++ model-optimizer/unit_tests/extensions/back/TransposeDFT_test.py
@@ -6,165 +6,64 @@ import unittest
 from extensions.back.TransposeDFT import TransposeDFT
 from mo.front.common.partial_infer.utils import int64_array
 from mo.utils.ir_engine.compare_graphs import compare_graphs
-from unit_tests.utils.graph import build_graph
-
+from unit_tests.utils.graph import build_graph, result, regular_op_with_shaped_data, valued_const_with_data, connect, \
+    regular_op_with_empty_data
 
 dft_graph_node_attrs = {
-    'placeholder': {'type': 'Parameter', 'kind': 'op', 'op': 'Parameter'},
-    'placeholder_data': {
-        'value': None,
-        'shape': int64_array([8, 2, 40, 56]),
-        'kind': 'data',
-        'data_type': None
-    },
-    'axes': {
-        'kind': 'op',
-        'op': 'Const',
-        'type': 'Const',
-        'value': int64_array([-2, -1]),
-        'shape': int64_array([2]),
-    },
-    'axes_data': {
-        'kind': 'data',
-        'value': int64_array([-2, -1]),
-        'shape': int64_array([2]),
-    },
-    'dft': {'kind': 'op', 'op': 'DFT', 'need_insert_transposes_for_dft': True},
-    'dft_data': {
-        'kind': 'data',
-        'shape': int64_array([8, 2, 40, 56]),
-        'value': None,
-    },
-    'abs': {'type': 'Abs', 'kind': 'op', 'op': 'Abs'},
-    'abs_data': {
-        'kind': 'data',
-        'shape': int64_array([8, 2, 40, 56]),
-        'value': None,
-    },
-    'output': {'kind': 'op', 'op': 'Result', 'type': 'Result'},
+    **regular_op_with_shaped_data('placeholder', [8, 2, 40, 56], {'type': 'Parameter', 'op': 'Parameter'}),
+    **valued_const_with_data('axes', int64_array([-2, -1])),
+    **regular_op_with_shaped_data('dft', [8, 2, 40, 56], {'op': 'DFT', 'need_insert_transposes_for_dft': True}),
+    **regular_op_with_shaped_data('abs', [8, 2, 40, 56], {'type': 'Abs', 'op': 'Abs'}),
+    **result(),
 }
 
 dft_graph_edges = [
-    ('placeholder', 'placeholder_data'),
-    ('axes', 'axes_data'),
-    ('placeholder_data', 'dft', {'in': 0}),
-    ('axes_data', 'dft', {'in': 1}),
-    ('dft', 'dft_data'),
-    ('dft_data', 'abs'),
-    ('abs', 'abs_data'),
-    ('abs_data', 'output'),
+    *connect('placeholder', '0:dft'),
+    *connect('axes', '1:dft'),
+    *connect('dft', 'abs'),
+    *connect('abs', 'output'),
 ]
 
 
 transposed_dft_graph_node_attrs = {
-    'placeholder': {'type': 'Parameter', 'kind': 'op', 'op': 'Parameter'},
-    'placeholder_data': {
-        'value': None,
-        'shape': int64_array([8, 2, 40, 56]),
-        'kind': 'data',
-        'data_type': None
-    },
-
-    'transpose_before': {'type': 'Transpose', 'kind': 'op', 'op': 'Transpose', 'need_shape_inference': True},
-    'transpose_before_data': {'value': None, 'shape': None, 'kind': 'data'},
-    'transpose_before_axis_const': {'type': 'Const', 'kind': 'op', 'op': 'Const', 'value': int64_array([0, 2, 3, 1])},
-    'transpose_before_axis_const_data': {'kind': 'data', 'value': int64_array([0, 2, 3, 1]), 'shape': None},
-    'transpose_after': {'type': 'Transpose', 'kind': 'op', 'op': 'Transpose', 'need_shape_inference': True},
-    'transpose_after_data': {'value': None, 'shape': None, 'kind': 'data'},
-    'transpose_after_axis_const': {'type': 'Const', 'kind': 'op', 'op': 'Const', 'value': int64_array([0, 3, 1, 2])},
-    'transpose_after_axis_const_data': {'kind': 'data', 'value': int64_array([0, 3, 1, 2])},
-
-    'dft_axes': {
-        'kind': 'op',
-        'op': 'Const',
-        'type': 'Const',
-        'value': int64_array([-2, -1]),
-        'shape': int64_array([2]),
-    },
-    'dft_axes_data': {
-        'kind': 'data',
-        'value': int64_array([-2, -1]),
-        'shape': int64_array([2]),
-    },
-    'dft': {'kind': 'op', 'op': 'DFT', 'need_insert_transposes_for_dft': True},
-    'dft_data': {
-        'kind': 'data',
-        'shape': int64_array([8, 2, 40, 56]),
-        'value': None,
-    },
-    'abs': {'type': 'Abs', 'kind': 'op', 'op': 'Abs'},
-    'abs_data': {
-        'kind': 'data',
-        'shape': int64_array([8, 2, 40, 56]),
-        'value': None,
-    },
-    'output': {'kind': 'op', 'op': 'Result', 'type': 'Result'},
+    **regular_op_with_shaped_data('placeholder', [8, 2, 40, 56], {'type': 'Parameter', 'op': 'Parameter'}),
+    **regular_op_with_empty_data('transpose_before',
+                                 {'type': 'Transpose', 'op': 'Transpose', 'need_shape_inference': True}),
+    **valued_const_with_data('transpose_before_axis_const', int64_array([0, 2, 3, 1])),
+    **regular_op_with_empty_data('transpose_after',
+                                 {'type': 'Transpose', 'op': 'Transpose', 'need_shape_inference': True}),
+    **valued_const_with_data('transpose_after_axis_const', int64_array([0, 3, 1, 2])),
+    **valued_const_with_data('dft_axes', int64_array([-2, -1])),
+    **regular_op_with_shaped_data('dft', [8, 2, 40, 56], {'op': 'DFT', 'need_insert_transposes_for_dft': True}),
+    **regular_op_with_shaped_data('abs', [8, 2, 40, 56], {'type': 'Abs', 'op': 'Abs'}),
+    **result(),
 }
 
 transposed_dft_graph_edges = [
-    ('placeholder', 'placeholder_data'),
-    ('dft_axes', 'dft_axes_data'),
-    ('transpose_before_axis_const', 'transpose_before_axis_const_data'),
-    ('transpose_after_axis_const', 'transpose_after_axis_const_data'),
-    ('transpose_before', 'transpose_before_data'),
-    ('transpose_after', 'transpose_after_data'),
-    ('transpose_before_axis_const_data', 'transpose_before', {'in': 1}),
-    ('transpose_after_axis_const_data', 'transpose_after', {'in': 1}),
-    ('placeholder_data', 'transpose_before', {'in': 0}),
-    ('transpose_before_data', 'dft', {'in': 0}),
-    ('dft_axes_data', 'dft', {'in': 1}),
-    ('dft', 'dft_data'),
-    ('dft_data', 'transpose_after', {'in': 0}),
-    ('transpose_after_data', 'abs'),
-    ('abs', 'abs_data'),
-    ('abs_data', 'output'),
+    *connect('placeholder', '0:transpose_before'),
+    *connect('transpose_before_axis_const', '1:transpose_before'),
+    *connect('transpose_before', '0:dft'),
+    *connect('dft_axes', '1:dft'),
+    *connect('dft', '0:transpose_after'),
+    *connect('transpose_after_axis_const', '1:transpose_after'),
+    *connect('transpose_after', 'abs'),
+    *connect('abs', 'output'),
 ]
 
 
 nontransposed_dft_graph_node_attrs = {
-    'placeholder': {'type': 'Parameter', 'kind': 'op', 'op': 'Parameter'},
-    'placeholder_data': {
-        'value': None,
-        'shape': int64_array([8, 40, 56, 2]),
-        'kind': 'data',
-        'data_type': None
-    },
-    'axes': {
-        'kind': 'op',
-        'op': 'Const',
-        'type': 'Const',
-        'value': int64_array([-2, -1]),
-        'shape': int64_array([2]),
-    },
-    'axes_data': {
-        'kind': 'data',
-        'value': int64_array([-2, -1]),
-        'shape': int64_array([2]),
-    },
-    'dft': {'kind': 'op', 'op': 'DFT'},
-    'dft_data': {
-        'kind': 'data',
-        'shape': int64_array([8, 40, 56, 2]),
-        'value': None,
-    },
-    'abs': {'type': 'Abs', 'kind': 'op', 'op': 'Abs'},
-    'abs_data': {
-        'kind': 'data',
-        'shape': int64_array([8, 40, 56, 2]),
-        'value': None,
-    },
-    'output': {'kind': 'op', 'op': 'Result', 'type': 'Result'},
+    **regular_op_with_shaped_data('placeholder', [8, 2, 40, 56], {'type': 'Parameter', 'op': 'Parameter'}),
+    **valued_const_with_data('axes', int64_array([-2, -1])),
+    **regular_op_with_shaped_data('dft', [8, 2, 40, 56], {'op': 'DFT'}),
+    **regular_op_with_shaped_data('abs', [8, 2, 40, 56], {'type': 'Abs', 'op': 'Abs'}),
+    **result(),
 }
 
 nontransposed_dft_graph_edges = [
-    ('placeholder', 'placeholder_data'),
-    ('axes', 'axes_data'),
-    ('placeholder_data', 'dft', {'in': 0}),
-    ('axes_data', 'dft', {'in': 1}),
-    ('dft', 'dft_data'),
-    ('dft_data', 'abs'),
-    ('abs', 'abs_data'),
-    ('abs_data', 'output'),
+    *connect('placeholder', '0:dft'),
+    *connect('axes', '1:dft'),
+    *connect('dft', 'abs'),
+    *connect('abs', 'output'),
 ]
 
 
