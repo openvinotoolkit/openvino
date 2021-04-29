2b7044b4910018b13a4cd19f86c6f29ddc63cdaa
pszmel <piotr.szmelczynski@intel.com>
2021-04-29 16:11:53 +0200

    create additional unit tests

--- ngraph/test/backend/prelu.in.cpp
+++ ngraph/test/backend/prelu.in.cpp
@@ -118,3 +118,84 @@ NGRAPH_TEST(${BACKEND_NAME}, prelu_negative_slope)
     test_case.add_expected_output<float>(vector<float>{1, 3, 1, 1, 0.5, 0});
     test_case.run();
 }
+
+NGRAPH_TEST(${BACKEND_NAME}, prelu_2d_same_shape) 
+{
+    Shape shape_a{2, 6};
+    Shape shape_b{2, 6};
+    auto A = make_shared<op::Parameter>(element::f32, shape_a);
+    auto B = make_shared<op::Parameter>(element::f32, shape_b);
+    auto f = make_shared<Function>(make_shared<op::PRelu>(A, B), ParameterVector{A, B});
+
+    std::vector<float> a{1, 2, -3, -4, 5, 6,
+                         1, 2, -3, -4, 5, 6};
+    std::vector<float> b{2, 2, 2, 2, 2, 2,
+                         1, 1, 4, 2, 1, 1};
+
+    auto test_case = test::TestCase<TestEngine>(f);
+    test_case.add_multiple_inputs<float>({a, b});
+    test_case.add_expected_output<float>(shape_a, {1, 2, -6, -8, 5, 6,
+                                                   1, 2, -12, -8, 5, 6});
+    test_case.run();
+}
+
+NGRAPH_TEST(${BACKEND_NAME}, prelu_2d_diff_shape) 
+{
+    Shape shape_a{2, 2, 2, 2};
+    Shape shape_b{2, 1, 2};
+    auto A = make_shared<op::Parameter>(element::f32, shape_a);
+    auto B = make_shared<op::Parameter>(element::f32, shape_b);
+    auto f = make_shared<Function>(make_shared<op::PRelu>(A, B), ParameterVector{A, B});
+
+    std::vector<float> a{1, 2, -3, -4, 1, 2, -3, -4, 1, 2, -3, -4, 1, 2, -3, -4};
+    std::vector<float> b{1, 2, 3, 4};
+
+    auto test_case = test::TestCase<TestEngine>(f);
+    test_case.add_multiple_inputs<float>({a, b});
+    test_case.add_expected_output<float>(shape_a, {1, 2, -3, -8, 1, 2, -9, -16, 1, 2, -3, -8, 1, 2, -9, -16});
+    test_case.run();
+}
+
+NGRAPH_TEST(${BACKEND_NAME}, prelu_3d_diff_shape) 
+{
+    Shape shape_a{2, 2, 6};
+    Shape shape_b{2, 1, 6};
+    auto A = make_shared<op::Parameter>(element::f32, shape_a);
+    auto B = make_shared<op::Parameter>(element::f32, shape_b);
+    auto f = make_shared<Function>(make_shared<op::PRelu>(A, B), ParameterVector{A, B});
+
+    std::vector<float> a{1, 2, -3, -4, -5, 6,
+                         -1, -2, -3, -4, -5, -6,
+                         1, 2, -3, -4, 5, 6,
+                         -2, 4, -6, -8, 10, 12};
+    std::vector<float> b{2, 1, 3, 4, 1, 7,
+                         1, 2, 3, 4, 5, 6};
+
+    auto test_case = test::TestCase<TestEngine>(f);
+    test_case.add_multiple_inputs<float>({a, b});
+    test_case.add_expected_output<float>(shape_a, {1, 2, -9, -16, -5, 6,
+                                                   -2, -2, -9, -16, -5, -42,
+                                                   1, 2, -9, -16, 5, 6,
+                                                   -2, 4, -18, -32, 10, 12});
+    test_case.run();
+}
+
+NGRAPH_TEST(${BACKEND_NAME}, prelu_3d_same_shape) 
+{
+    Shape shape_a{2, 3, 2};
+    Shape shape_b{2, 3, 2};
+    auto A = make_shared<op::Parameter>(element::f32, shape_a);
+    auto B = make_shared<op::Parameter>(element::f32, shape_b);
+    auto f = make_shared<Function>(make_shared<op::PRelu>(A, B), ParameterVector{A, B});
+
+    std::vector<float> a{1, 2, -3, -4, -5, 6,
+                         -1, -2, -3, -4,-5, -6,};
+    std::vector<float> b{2, 1, 3, 4, 1, 7,
+                         1, 2, 3, 4, 5, 6};
+
+    auto test_case = test::TestCase<TestEngine>(f);
+    test_case.add_multiple_inputs<float>({a, b});
+    test_case.add_expected_output<float>(shape_a, {1, 2, -9, -16, -5, 6,
+                                                   -1, -4, -9, -16, -25, -36});
+    test_case.run();
+}
