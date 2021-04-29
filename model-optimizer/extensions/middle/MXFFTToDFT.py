b6ba6318d4468eed3f1557149d23be68dfca2ba6
VGavrilov <vladimir.gavrilov@intel.com>
2021-04-29 14:11:32 +0300

    Fixed the conversion MXFFT to (I)DFT.

--- model-optimizer/extensions/middle/MXFFTToDFT.py
+++ model-optimizer/extensions/middle/MXFFTToDFT.py
@@ -9,6 +9,8 @@ from mo.front.common.replacement import FrontReplacementSubgraph
 from mo.front.tf.graph_utils import create_op_with_const_inputs
 from mo.graph.graph import Graph, rename_nodes
 from mo.ops.pad import Pad
+from mo.ops.squeeze import Squeeze
+from mo.ops.strided_slice import StridedSlice
 from mo.ops.unsqueeze import Unsqueeze
 
 
@@ -37,9 +39,32 @@ class MXFFTToDFT(FrontReplacementSubgraph):
                                                    {'name': mx_fft_name + '/Pad', 'mode': 'constant'},
                                                    unsqueeze_node)
 
-            op = IDFT if mx_fft.soft_get('is_inverse', False) else DFT
-            dft_node = create_op_with_const_inputs(graph, op, {1: int64_array([-1])}, {'in_ports_count': 2}, pad_node)
+            if not mx_fft.soft_get('is_inverse', False):
+                dft_node = create_op_with_const_inputs(graph, DFT, {1: int64_array([-1])}, {'in_ports_count': 2},
+                                                       pad_node)
 
-            mx_fft.in_port(0).get_connection().set_destination(unsqueeze_node.in_port(0))
-            mx_fft.out_port(0).get_connection().set_source(dft_node.in_port(0))
-            rename_nodes([(mx_fft, mx_fft_name + '/to_be_removed'), (dft_node, mx_fft_name)])
+                mx_fft.in_port(0).get_connection().set_destination(unsqueeze_node.in_port(0))
+                mx_fft.out_port(0).get_connection().set_source(dft_node.in_port(0))
+                rename_nodes([(mx_fft, mx_fft_name + '/to_be_removed'), (dft_node, mx_fft_name)])
+            else:
+                dft_node = create_op_with_const_inputs(graph, IDFT, {1: int64_array([-1])},
+                                                       {'name': mx_fft_name + '/IDFT', 'in_ports_count': 2},
+                                                       pad_node)
+                ss = create_op_with_const_inputs(graph, StridedSlice,
+                                                 {1: pads_begin,
+                                                  2: pads_end,
+                                                  3: int64_array(np.ones(dtype=np.int64))
+                                                  },
+                                                 {'name': mx_fft_name + '/real_part',
+                                                  'begin_mask': int64_array([1]),
+                                                  'end_mask': int64_array([1]),
+                                                  'new_axis_mask': int64_array([0]),
+                                                  'shrink_axis_mask': int64_array([0]),
+                                                  'ellipsis_mask': int64_array([0])
+                                                  },
+                                                 dft_node)
+                squeeze_node = create_op_with_const_inputs(graph, Squeeze, {1: int64_array([-1])}, {}, ss)
+
+                mx_fft.in_port(0).get_connection().set_destination(unsqueeze_node.in_port(0))
+                mx_fft.out_port(0).get_connection().set_source(squeeze_node.in_port(0))
+                rename_nodes([(mx_fft, mx_fft_name + '/to_be_removed'), (squeeze_node, mx_fft_name)])
