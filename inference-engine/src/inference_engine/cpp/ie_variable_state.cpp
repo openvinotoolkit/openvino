bea1afdeac1cad7ac86ea288defdee12ac076293
Ilya Lavrenov <ilya.lavrenov@intel.com>
2021-04-29 19:50:46 +0300

    Move all base wrapper classes from Plugin API to source folder (#5419)
    * Small refactoring in TEMPLATE plugin
    
    * Fixed compilation on Windows
    
    * Fixed code style
    
    * Hide CALL_STATUS_FNC helpers to private API
    
    * Moved some base classes to private place from plugin_api
    
    * Updates for VariableState creation
    
    * Take Jane's changes for Demension names
    
    * Revert "Take Jane's changes for Demension names"
    
    This reverts commit 9f6c8fa5a6460a9b70df15f2a201977a8b38aedd.
    
    * Removed ICNNNetwork include
    
    * removed more icnnnetwork includes
    
    * Added missed include with ie_input_info.hpp
    
    * Fixed GNA plugin to provide names w/o \0

--- inference-engine/src/inference_engine/cpp/ie_variable_state.cpp
+++ inference-engine/src/inference_engine/cpp/ie_variable_state.cpp
@@ -2,18 +2,30 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "ie_imemory_state.hpp"
 #include "cpp/ie_memory_state.hpp"
+#include "cpp_interfaces/interface/ie_ivariable_state_internal.hpp"
+#include "exception2status.hpp"
+
+#define VARIABLE_CALL_STATEMENT(...)                                                               \
+    if (_impl == nullptr) IE_THROW() << "VariableState was not initialized.";                      \
+    try {                                                                                          \
+        __VA_ARGS__;                                                                               \
+    } CATCH_IE_EXCEPTIONS catch (const std::exception& ex) {                                       \
+        IE_THROW() << ex.what();                                                                   \
+    } catch (...) {                                                                                \
+        IE_THROW(Unexpected);                                                                      \
+    }
 
 namespace InferenceEngine {
 
-IE_SUPPRESS_DEPRECATED_START
-
-VariableState::VariableState(IVariableState::Ptr pState, details::SharedObjectLoader::Ptr plg) : actual(pState), plugin(plg) {
-    if (actual == nullptr) {
-        IE_THROW() << "VariableState wrapper was not initialized.";
+VariableState::VariableState(const std::shared_ptr<IVariableStateInternal>& impl,
+                             const details::SharedObjectLoader::Ptr& so) : _impl(impl), _so(so) {
+    if (impl == nullptr) {
+        IE_THROW(NotAllocated) << "VariableState wrapper was not initialized.";
     }
 }
+
+IE_SUPPRESS_DEPRECATED_START
 
 Blob::CPtr VariableState::GetLastState() const {
     return GetState();
@@ -22,23 +34,19 @@ Blob::CPtr VariableState::GetLastState() const {
 IE_SUPPRESS_DEPRECATED_END
 
 void VariableState::Reset() {
-    CALL_STATUS_FNC_NO_ARGS(Reset);
+    VARIABLE_CALL_STATEMENT(_impl->Reset());
 }
 
 std::string VariableState::GetName() const {
-    char name[256];
-    CALL_STATUS_FNC(GetName, name, sizeof(name));
-    return name;
+    VARIABLE_CALL_STATEMENT(return _impl->GetName());
 }
 
 Blob::CPtr VariableState::GetState() const {
-    Blob::CPtr stateBlob;
-    CALL_STATUS_FNC(GetState, stateBlob);
-    return stateBlob;
+    VARIABLE_CALL_STATEMENT(return _impl->GetState());
 }
 
 void VariableState::SetState(Blob::Ptr state) {
-    CALL_STATUS_FNC(SetState, state);
+    VARIABLE_CALL_STATEMENT(_impl->SetState(state));
 }
 
 }  // namespace InferenceEngine
