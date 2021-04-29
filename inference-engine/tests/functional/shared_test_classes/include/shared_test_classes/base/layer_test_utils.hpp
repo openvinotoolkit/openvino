6dbd003ed8e37b82fe3ed2fa98faf660f6bd5272
Alexandra Sidorova <alexandra.sidorova@intel.com>
2021-04-29 19:08:22 +0300

    tried to fix nan and not float cases

--- inference-engine/tests/functional/shared_test_classes/include/shared_test_classes/base/layer_test_utils.hpp
+++ inference-engine/tests/functional/shared_test_classes/include/shared_test_classes/base/layer_test_utils.hpp
@@ -93,7 +93,7 @@ public:
 
             const auto max = std::max(CommonTestUtils::ie_abs(res), CommonTestUtils::ie_abs(ref));
             float diff = static_cast<float>(absoluteDifference) / static_cast<float>(max);
-            if (max == 0 || (diff > static_cast<float>(threshold))) {
+            if (std::isnan(res) || !std::is_floating_point<T>::value || max == 0 || (diff > static_cast<float>(threshold))) {
                 IE_THROW() << "Relative comparison of values expected: " << ref << " and actual: " << res
                                    << " at index " << i << " with threshold " << threshold
                                    << " failed";
