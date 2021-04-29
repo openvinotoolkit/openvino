92cac5dedc808f47b33b2f15ca71a66bd2caacb4
Szymon Durawa <szymon.durawa@intel.com>
2021-04-29 14:44:33 +0200

    Set default output_padding to all convolution_backprop_in interfaces.

--- ngraph/core/reference/include/ngraph/runtime/reference/convolution_backprop_data.hpp
+++ ngraph/core/reference/include/ngraph/runtime/reference/convolution_backprop_data.hpp
@@ -330,7 +330,7 @@ namespace ngraph
                                          const CoordinateDiff& forward_in_pad_bellow,
                                          const CoordinateDiff& forward_in_pad_above,
                                          const Strides& stride,
-                                         const CoordinateDiff& output_padding)
+                                         const CoordinateDiff& output_padding = {0, 0, 0})
             {
                 convolution_backprop_in(delta_in,
                                         filter,
