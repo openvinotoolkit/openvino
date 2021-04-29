8287e0873392bdba33cade334c28f122f179c509
Bartosz Lesniewski <bartosz.lesniewski@intel.com>
2021-04-29 05:58:03 +0200

    Fix Max and Average Pooling op behavior with ceil rounding (#5204)
    * Fix Max and Avg Pooling behavior with ceil rounding
    
    * Remove redundant code
    
    * Add backend test to check the problematic case

--- ngraph/test/backend/avg_pool.in.cpp
+++ ngraph/test/backend/avg_pool.in.cpp
@@ -102,6 +102,61 @@ NGRAPH_TEST(${BACKEND_NAME}, avg_pool_2d_pad)
     test_case.run();
 }
 
+NGRAPH_TEST(${BACKEND_NAME}, avg_pool_2d_ceil_stride_pad)
+{
+    Shape in_shape{1, 1, 1, 5};
+    Shape out_shape{1, 1, 1, 3};
+    const Strides& strides{1, 2};
+    const Shape& pads_begin{1, 1};
+    const Shape& pads_end{1, 1};
+    const Shape& kernel{3, 3};
+    const bool exclude_pad = true;
+    const op::RoundingType rounding_type = op::RoundingType::CEIL;
+    const op::PadType pad_type = op::PadType::EXPLICIT;
+
+    auto A = make_shared<op::Parameter>(element::f32, in_shape);
+    auto avgPool = make_shared<op::v1::AvgPool>(
+        A, strides, pads_begin, pads_end, kernel, exclude_pad, rounding_type, pad_type);
+    auto f = make_shared<Function>(avgPool, ParameterVector{A});
+
+
+    std::vector<float> a{1, 2, 3, 4, 5};
+    std::vector<float> result{1.5, 3, 4.5};
+
+    auto test_case = test::TestCase<TestEngine>(f);
+    test_case.add_input<float>({a});
+    test_case.add_expected_output<float>(out_shape, result);
+    test_case.run();
+}
+
+NGRAPH_TEST(${BACKEND_NAME}, avg_pool_2d_ceil_stride_pad_include_padding)
+{
+    Shape in_shape{1, 1, 1, 5};
+    Shape out_shape{1, 1, 1, 3};
+    const Strides& strides{1, 2};
+    const Shape& pads_begin{1, 1};
+    const Shape& pads_end{1, 1};
+    const Shape& kernel{3, 3};
+    const bool exclude_pad = false;
+    const op::RoundingType rounding_type = op::RoundingType::CEIL;
+    const op::PadType pad_type = op::PadType::EXPLICIT;
+
+    auto A = make_shared<op::Parameter>(element::f32, in_shape);
+    auto avgPool = make_shared<op::v1::AvgPool>(
+        A, strides, pads_begin, pads_end, kernel, exclude_pad, rounding_type, pad_type);
+    auto f = make_shared<Function>(avgPool, ParameterVector{A});
+
+
+    std::vector<float> a{2.5, 2, 12, 4, 5};
+    std::vector<float> result{0.5, 2, 1};
+
+    auto test_case = test::TestCase<TestEngine>(f);
+    test_case.add_input<float>({a});
+    test_case.add_expected_output<float>(out_shape, result);
+    test_case.run();
+}
+
+
 NGRAPH_TEST(${BACKEND_NAME}, avg_pool_2d_same_upper)
 {
     Shape in_shape{1, 1, 3, 3};
