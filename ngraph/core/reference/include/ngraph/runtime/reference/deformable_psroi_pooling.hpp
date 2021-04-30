a3b5291133d0c856d143ee44458df0331c2095a0
mitruska <katarzyna.mitrus@intel.com>
2021-04-30 10:31:41 +0200

    Code style (namespace comments)

--- ngraph/core/reference/include/ngraph/runtime/reference/deformable_psroi_pooling.hpp
+++ ngraph/core/reference/include/ngraph/runtime/reference/deformable_psroi_pooling.hpp
@@ -222,6 +222,6 @@ namespace ngraph
                     }
                 }
             }
-        }
-    }
-}
+        } // namespace reference
+    }     // namespace runtime
+} // namespace ngraph
