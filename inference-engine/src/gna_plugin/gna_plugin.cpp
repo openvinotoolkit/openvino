22e4566faa7bedf52ed15049155b89978b54db9f
Elizaveta Lobanova <elizaveta.lobanova@intel.com>
2021-04-30 15:42:27 +0300

    [GNA] Remove extra reshape before maxpool. Fix activation and maxpool reordering. (#5404)
    Fix convolution input transposition for Kaldi models with FakeQuantise layers.
    Fix floating point error in gnaFuncTests with debug logs.

--- inference-engine/src/gna_plugin/gna_plugin.cpp
+++ inference-engine/src/gna_plugin/gna_plugin.cpp
@@ -56,6 +56,8 @@
 #include <transformations/common_optimizations/relu_fake_quantize_fusion.hpp>
 #include <transformations/common_optimizations/add_fake_quantize_fusion.hpp>
 
+#include "transformations/remove_extra_reshapes.hpp"
+
 #if GNA_LIB_VER == 2
 #include <gna2-model-api.h>
 
@@ -663,6 +665,7 @@ void GNAPlugin::LoadNetwork(CNNNetwork & _network) {
         manager.register_pass<ngraph::pass::ConvertOpSet3ToOpSet2>();
         manager.register_pass<ngraph::pass::ConvertOpSet2ToOpSet1>();
         manager.register_pass<ngraph::pass::ConvertOpSet1ToLegacy>();
+        manager.register_pass<RemoveExtraReshapes>();
         // UnrollTI should be the last transformation in the transformation pipeline
         manager.register_pass<ngraph::pass::UnrollTensorIterator>();
 
