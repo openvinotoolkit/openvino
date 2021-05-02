81e9244b6fb956ac868e3726398b94f66994ecfe
Ilya Lavrenov <ilya.lavrenov@intel.com>
2021-05-02 14:04:22 +0300

    Fixes for TBB

--- inference-engine/cmake/ie_parallel.cmake
+++ inference-engine/cmake/ie_parallel.cmake
@@ -19,13 +19,11 @@ function(set_ie_threading_interface_for TARGET_NAME)
     if (THREADING STREQUAL "TBB" OR THREADING STREQUAL "TBB_AUTO" AND NOT TBB_FOUND)
         if(IEDevScripts_DIR)
             find_package(TBB COMPONENTS tbb tbbmalloc
-                        PATHS IEDevScripts_DIR
-                        NO_CMAKE_FIND_ROOT_PATH
-                        NO_DEFAULT_PATH)
+                         PATHS IEDevScripts_DIR
+                         NO_CMAKE_FIND_ROOT_PATH
+                         NO_DEFAULT_PATH)
         else()
-            find_package(TBB COMPONENTS tbb tbbmalloc
-                         PATHS ${TBBROOT}/cmake
-                               ${_tbb_cmake})
+            find_package(TBB COMPONENTS tbb tbbmalloc)
         endif()
         set("TBB_FOUND" ${TBB_FOUND} PARENT_SCOPE)
         set("TBB_IMPORTED_TARGETS" ${TBB_IMPORTED_TARGETS} PARENT_SCOPE)
