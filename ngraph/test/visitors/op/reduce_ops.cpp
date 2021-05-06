c93856e0c06026d6da4e2997c68744af1c5e36f6
ggalieroc <gabriele.galiero.casay@intel.com>
2021-05-06 18:01:16 +0200

    Add ngraph visitor tests for reduction operations

---
+++ ngraph/test/visitors/op/reduce_ops.cpp
@@ -0,0 +1,57 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "gtest/gtest.h"
+
+#include "ngraph/ngraph.hpp"
+#include "ngraph/op/util/attr_types.hpp"
+
+#include "util/visitor.hpp"
+
+using namespace std;
+using namespace ngraph;
+using ngraph::test::NodeBuilder;
+using ngraph::test::ValueMap;
+
+template<class T>
+class ReduceOpsAttrTest : public ::testing::Test
+{
+};
+
+TYPED_TEST_CASE_P(ReduceOpsAttrTest);
+
+TYPED_TEST_P(ReduceOpsAttrTest, reduce_ops)
+{
+    Shape in_shape{3, 4, 5};
+    element::Type in_et = element::f32;
+
+    Shape axes_shape{2};
+    element::Type axes_et = element::i64;
+
+    bool keep_dims = true;
+
+    NodeBuilder::get_ops().register_factory<TypeParam>();
+    auto data = make_shared<op::Parameter>(in_et, in_shape);
+    auto reduction_axes = make_shared<op::Parameter>(axes_et, axes_shape);
+    auto reduce_op = make_shared<TypeParam>(data, reduction_axes, keep_dims);
+
+    NodeBuilder builder(reduce_op);
+    auto g_reduce_op = as_type_ptr<TypeParam>(builder.create());
+    EXPECT_EQ(g_reduce_op->get_keep_dims(), reduce_op->get_keep_dims());
+}
+
+REGISTER_TYPED_TEST_CASE_P(
+    ReduceOpsAttrTest,
+    reduce_ops);
+
+using Types = ::testing::Types<
+    op::v1::ReduceLogicalAnd,
+    op::v1::ReduceLogicalOr,
+    op::v1::ReduceMax,
+    op::v1::ReduceMean,
+    op::v1::ReduceMin,
+    op::v1::ReduceProd,
+    op::v1::ReduceSum>;
+
+INSTANTIATE_TYPED_TEST_CASE_P(attributes, ReduceOpsAttrTest, Types);
