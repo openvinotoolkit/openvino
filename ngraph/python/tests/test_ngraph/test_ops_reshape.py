302578bdc3eec48aebfa073411ccc197dd373d92
Pavel Esir <pavel.esir@intel.com>
2021-05-12 12:11:27 +0300

    Add gather 7 to nG Python API and IE IR reader test (#5276)
    * nG Python api for Gather and IE IR reader tests
    
    * also included old IE IR reader test for Gather1
    
    * added a blank line before Gather
    
    * style corrections
    
    * applied review comments
    
    * removed blank line
    
    * removed xfailed for Gather7 with batch_dims since CPU was moved to nGraph (...allows to fallback node execution on ngraph evaluate() method in case if optimized implementation is absent)
    
    * added new visitor tests

--- ngraph/python/tests/test_ngraph/test_ops_reshape.py
+++ ngraph/python/tests/test_ngraph/test_ops_reshape.py
@@ -1,10 +1,10 @@
 # Copyright (C) 2018-2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+import ngraph as ng
 import numpy as np
 import pytest
 
-import ngraph as ng
 from tests.runtime import get_runtime
 from tests.test_ngraph.util import run_op_node, run_op_numeric_data
 
@@ -118,21 +118,6 @@ def test_broadcast_bidirectional():
 
     assert node.get_type_name() == "Broadcast"
     assert node.get_output_size() == 1
-
-
-def test_gather():
-    input_data = np.array(
-        [1.0, 1.1, 1.2, 2.0, 2.1, 2.2, 3.0, 3.1, 3.2], np.float32
-    ).reshape((3, 3))
-    input_indices = np.array([0, 2], np.int32).reshape(1, 2)
-    input_axes = np.array([1], np.int32)
-
-    expected = np.array([1.0, 1.2, 2.0, 2.2, 3.0, 3.2], dtype=np.float32).reshape(
-        (3, 1, 2)
-    )
-
-    result = run_op_node([input_data], ng.gather, input_indices, input_axes)
-    assert np.allclose(result, expected)
 
 
 def test_transpose():
