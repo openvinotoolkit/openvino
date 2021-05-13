cb3eb1c1c70f4fc522332e763e00bdcaa198824f
Shoujiang Ma <shoujiang.ma@intel.com>
2021-05-13 22:04:38 +0800

    [Auto Plugin] Auto plugin component in IE (#5366)
    * Implement AUTO plugin
    
    Usage:
    	1. -d AUTO
    	2. -d ""
    
    Signed-off-by: Shoujiang Ma <shoujiang.ma@intel.com>
    
    * Add tests for AUTO plugin
    
    Signed-off-by: Shoujiang Ma <shoujiang.ma@intel.com>
    
    * cleaned impl, that was incorrect from the async perspective, ansl also capturing the blobs in the constructor
    
    * Revert benchmark_app modification
    
    Signed-off-by: Shoujiang Ma <shoujiang.ma@intel.com>
    
    * Address reviewer's comments: need CI tests to verify
    
    Signed-off-by: Shoujiang Ma <shoujiang.ma@intel.com>
    
    Co-authored-by: myshevts <maxim.y.shevtsov@intel.com>

---
+++ inference-engine/src/auto_plugin/auto_plugin.hpp
@@ -0,0 +1,37 @@
+// Copyright (C) 2018-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <map>
+#include <vector>
+#include <string>
+#include <unordered_set>
+
+#include <cpp_interfaces/impl/ie_plugin_internal.hpp>
+#include <cpp_interfaces/interface/ie_internal_plugin_config.hpp>
+#include "auto_exec_network.hpp"
+
+namespace AutoPlugin {
+namespace IE = InferenceEngine;
+using ConfigType = std::map<std::string, std::string>;
+
+class AutoInferencePlugin : public IE::InferencePluginInternal {
+public:
+    AutoInferencePlugin();
+    ~AutoInferencePlugin() = default;
+    IE::ExecutableNetworkInternal::Ptr LoadExeNetworkImpl(const IE::CNNNetwork& network, const ConfigType& config) override;
+    IE::QueryNetworkResult QueryNetwork(const IE::CNNNetwork& network, const ConfigType& config) const override;
+    IE::Parameter GetMetric(const std::string& name, const std::map<std::string, IE::Parameter>& options) const override;
+    IE::Parameter GetConfig(const std::string& name, const std::map<std::string, IE::Parameter> & options) const override;
+    void SetConfig(const ConfigType& config) override;
+
+private:
+    std::vector<AutoPlugin::DeviceInformation> GetDeviceChoice(const ConfigType&  config) const;
+
+protected:
+    ConfigType GetSupportedConfig(const ConfigType& config, const AutoPlugin::DeviceName & deviceName) const;
+};
+
+}  // namespace AutoPlugin
