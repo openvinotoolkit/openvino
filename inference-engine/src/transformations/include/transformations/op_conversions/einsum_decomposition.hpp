4aebfdd9465a3ff0fc5c3ee6145e649f4cc4b7fd
Roman Kazantsev <roman.kazantsev@intel.com>
2021-05-13 18:14:06 +0300

    Fix code style #2
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- inference-engine/src/transformations/include/transformations/op_conversions/einsum_decomposition.hpp
+++ inference-engine/src/transformations/include/transformations/op_conversions/einsum_decomposition.hpp
@@ -4,26 +4,24 @@
 
 #pragma once
 
-#include <vector>
 #include <memory>
-
-#include <transformations_visibility.hpp>
-
 #include <ngraph/pass/graph_rewrite.hpp>
+#include <transformations_visibility.hpp>
+#include <vector>
 
 namespace ngraph {
-    namespace pass {
+namespace pass {
 
-        class TRANSFORMATIONS_API EinsumDecomposition;
+class TRANSFORMATIONS_API EinsumDecomposition;
 
-    }  // namespace pass
+}  // namespace pass
 }  // namespace ngraph
 
- /**
-  * @ingroup ie_transformation_common_api
-  * @brief EinsumDecomposition transformation decomposes Einsum-7 operation into a sub-graph with more simple operations:
-  * Transpose, Reshape, MatMul, ReduceSum and Unsqueeze
-  */
+/**
+ * @ingroup ie_transformation_common_api
+ * @brief EinsumDecomposition transformation decomposes Einsum-7 operation into a sub-graph with more simple operations:
+ *        Transpose, Reshape, MatMul, ReduceSum and Unsqueeze
+ */
 class ngraph::pass::EinsumDecomposition : public ngraph::pass::MatcherPass {
 public:
     NGRAPH_RTTI_DECLARATION;
