cde2acc0274db9056a36466bfdc6b8c6381dacd0
Szymon Durawa <szymon.durawa@intel.com>
2021-05-14 08:28:37 +0200

    Add ConvolutionBackpropDataLayerTest into TEST_P.

--- inference-engine/tests/functional/plugin/shared/include/single_layer_tests/convolution_backprop_data.hpp
+++ inference-engine/tests/functional/plugin/shared/include/single_layer_tests/convolution_backprop_data.hpp
@@ -2,13 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+// DEPRECATED, this file shall be removed when kmb_plugin will switch to use new API from "convolution_backprop.hpp" (#55568)
+
 #pragma once
 
-#include "shared_test_classes/single_layer/convolution_backprop.hpp"
+#include "shared_test_classes/single_layer/convolution_backprop_data.hpp"
 
 namespace LayerTestsDefinitions {
 
-TEST_P(ConvolutionBackpropLayerTest, CompareWithRefs) {
+TEST_P(ConvolutionBackpropDataLayerTest, CompareWithRefs) {
     Run();
 }
 
