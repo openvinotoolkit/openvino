0873626ed68398abbca25f72cb8c50b0bd418d23
azhogov <alexander.zhogov@intel.com>
2021-05-18 21:08:52 +0300

    Disable change in cpp, and enabled in yml due to strange issues

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
@@ -86,7 +86,7 @@ std::vector<std::string> disabledTestPatterns() {
 #endif
 #ifdef __APPLE__
         // TODO: Issue 55717
-        retVector.emplace_back(R"(.*smoke_LPT.*ReduceMinTransformation.*f32.*)");
+        //retVector.emplace_back(R"(.*smoke_LPT.*ReduceMinTransformation.*f32.*)");
 #endif
     if (!InferenceEngine::with_cpu_x86_avx512_core()) {
         // on platforms which do not support bfloat16, we are disabling bf16 tests since there are no bf16 primitives,
