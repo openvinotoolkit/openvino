af290ca959f943d0d118b2336fdb1aaf7f9b9b5d
Ilya Lavrenov <ilya.lavrenov@intel.com>
2021-05-19 10:43:58 +0300

    LPT fix for Windows
    * LPT fix for Windows
    
    * Remove inference_engine_transformations_EXPORTS

--- inference-engine/src/low_precision_transformations/include/low_precision/unsqueeze.hpp
+++ inference-engine/src/low_precision_transformations/include/low_precision/unsqueeze.hpp
@@ -11,7 +11,7 @@ namespace ngraph {
 namespace pass {
 namespace low_precision {
 
-class TRANSFORMATIONS_API UnsqueezeTransformation : public LayerTransformation {
+class LP_TRANSFORMATIONS_API UnsqueezeTransformation : public LayerTransformation {
 public:
     UnsqueezeTransformation(const Params& params);
     void registerMatcherIn(GraphRewrite& pass, TransformationContext& context) const override;
