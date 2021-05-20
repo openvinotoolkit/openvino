15bebe8249b17ba4fbdd611ecd582292946675ec
jiwaszki <jan.iwaszkiewicz@intel.com>
2021-05-20 16:31:26 +0200

    Add random image generation and model path getting

--- ngraph/python/examples/helpers.py
+++ ngraph/python/examples/helpers.py
@@ -1,44 +1,28 @@
-# Use case: user wants to run the inference on set of pictures
-# and store the results of the inference (e.g. in a database)
-# The Inference Queue allows him to run inference in parallel jobs.
+# Copyright (C) 2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+#
+"""Helper functions."""
 
 import numpy as np
 import os
-import time
-from openvino.inference_engine import IECore
-from openvino.inference_engine import TensorDesc
-from openvino.inference_engine import Blob
-from openvino.inference_engine import StatusCode
-from openvino.inference_engine import InferQueue
 
 
-def image_path(name):
-    path_to_repo = os.environ["DATA_PATH"]
-    path_to_img = os.path.join(path_to_repo, 'validation_set', '224x224', name)
-    return path_to_img
+def get_examples_path() -> str:
+    """Return examples absoulte path."""
+    return os.path.dirname(os.path.realpath(__file__))
 
 
-def read_image(name):
-    import cv2
-    n, c, h, w = (1, 3, 32, 32)
-    image = cv2.imread(image_path(name))
-    if image is None:
-        raise FileNotFoundError('Input image not found')
-
-    image = cv2.resize(image, (h, w)) / 255
-    image = image.transpose((2, 0, 1)).astype(np.float32)
-    image = image.reshape((n, c, h, w))
-    return image
+def get_example_model_path() -> str:
+    """Return example model path to xml file."""
+    return get_examples_path() + '/model/test_model_fp32.xml'
 
 
-def get_images():
-    images = []
+def get_example_weights_path() -> str:
+    """Return example model path to bin file."""
+    return get_examples_path() + '/model/test_model_fp32.bin'
 
-    images += [read_image('dog.bmp')]
-    images += [read_image('cat1.bmp')]
-    images += [read_image('cat2.bmp')]
-    images += [read_image('dog1.bmp')]
-    images += [read_image('dog2.bmp')]
-    images += [read_image('dog3.bmp')]
 
-    return images
+def generate_random_images(num=1) -> np.array:
+    """Generate `num` random images."""
+    return [np.array(np.random.rand(1, 3, 32, 32), dtype=np.float32)
+            for i in range(0, num)]
