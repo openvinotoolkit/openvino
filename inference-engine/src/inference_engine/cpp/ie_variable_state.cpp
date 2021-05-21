bb3ea830cce0129a6832144f83d71d2c80469716
Ilya Lavrenov <ilya.lavrenov@intel.com>
2021-05-21 20:50:16 +0300

    Returned C++ wrappers creation from interface API (#5729)

--- inference-engine/src/inference_engine/cpp/ie_variable_state.cpp
+++ inference-engine/src/inference_engine/cpp/ie_variable_state.cpp
@@ -4,6 +4,7 @@
 
 #include "details/ie_so_loader.h"
 #include "cpp/ie_memory_state.hpp"
+#include "ie_imemory_state.hpp"
 #include "cpp_interfaces/interface/ie_ivariable_state_internal.hpp"
 #include "exception2status.hpp"
 
@@ -27,25 +28,53 @@ VariableState::VariableState(const details::SharedObjectLoader& so,
 
 IE_SUPPRESS_DEPRECATED_START
 
+VariableState::VariableState(std::shared_ptr<IVariableState> state,
+                             std::shared_ptr<details::SharedObjectLoader> splg)
+    : _so(*splg), _impl(), actual(state) {
+    //  plg can be null, but not the actual
+    if (actual == nullptr)
+        IE_THROW() << "VariableState was not initialized.";
+}
+
 Blob::CPtr VariableState::GetLastState() const {
     return GetState();
 }
 
-IE_SUPPRESS_DEPRECATED_END
-
 void VariableState::Reset() {
+    if (actual) {
+        CALL_STATUS_FNC_NO_ARGS(Reset);
+        return;
+    }
+
     VARIABLE_CALL_STATEMENT(_impl->Reset());
 }
 
 std::string VariableState::GetName() const {
+    if (actual) {
+        char name[256];
+        CALL_STATUS_FNC(GetName, name, sizeof(name));
+        return name;
+    }
+
     VARIABLE_CALL_STATEMENT(return _impl->GetName());
 }
 
 Blob::CPtr VariableState::GetState() const {
+    if (actual) {
+        Blob::CPtr stateBlob;
+        CALL_STATUS_FNC(GetState, stateBlob);
+        return stateBlob;
+    }
+
     VARIABLE_CALL_STATEMENT(return _impl->GetState());
 }
 
 void VariableState::SetState(Blob::Ptr state) {
+    if (actual) {
+        CALL_STATUS_FNC(SetState, state);
+        return;
+    }
+
     VARIABLE_CALL_STATEMENT(_impl->SetState(state));
 }
 
