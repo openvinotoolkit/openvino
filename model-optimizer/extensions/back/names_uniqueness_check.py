23b45971790db81e2cabb9b6a124038040b332a7
Kruglov <yegor.kruglov@intel.com>
2021-05-21 11:28:40 +0300

    conversation resolving

--- model-optimizer/extensions/back/names_uniqueness_check.py
+++ model-optimizer/extensions/back/names_uniqueness_check.py
@@ -16,18 +16,18 @@ class NamesUniquenessCheck(BackReplacementPattern):
         return []
 
     def find_and_replace_pattern(self, graph: Graph):
-        names_dict = {}
-        not_unique_names = []
+        names_dict = dict()
+        not_unique_names = set()
         for node in graph.get_op_nodes():
             node_name = node.soft_get('name', node.id)
             if node_name not in names_dict.keys():
                 names_dict[node_name] = [node]
             else:
                 names_dict[node_name].append(node)
-                not_unique_names.append(node_name)
+                not_unique_names.add(node_name)
 
         if not_unique_names:
-            not_unique_names = set(not_unique_names)
             for name in not_unique_names:
                 for idx, node in enumerate(names_dict[name]):
-                    rename_node(node, name + '_{}'.format(idx))
+                    if node.op is not "Result":
+                        rename_node(node, name + '_{}'.format(idx))
