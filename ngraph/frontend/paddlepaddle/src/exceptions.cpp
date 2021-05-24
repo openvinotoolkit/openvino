6a688030d942b24e2c84bfa0d85e07f9e3a55125
Ivan Tikhonov <ivan.tikhonov@intel.com>
2021-05-24 18:29:35 +0300

    fix build

--- ngraph/frontend/paddlepaddle/src/exceptions.cpp
+++ ngraph/frontend/paddlepaddle/src/exceptions.cpp
@@ -18,15 +18,6 @@ namespace ngraph
                 ss << "While validating node '" << node.op_type() << '\'';
                 return ss.str();
             }
-
-            std::string OpConversionFailurePDPD::get_error_msg_prefix_pdpd(const Node* node)
-            {
-                std::stringstream ss;
-                ss << "While converting node '" << *node << "' with friendly_name '"
-                   << node->get_friendly_name() << '\'';
-                return ss.str();
-            }
-
         } // namespace pdpd
     }     // namespace frontend
 } // namespace ngraph
