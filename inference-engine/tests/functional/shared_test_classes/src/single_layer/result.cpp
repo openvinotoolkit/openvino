aacf296d8752d65fa88caeefc18276b57ecb79a7
blesniewski <bartosz.lesniewski@intel.com>
2021-05-24 11:25:29 +0200

    fix adding redundant nodes in slt

--- inference-engine/tests/functional/shared_test_classes/src/single_layer/result.cpp
+++ inference-engine/tests/functional/shared_test_classes/src/single_layer/result.cpp
@@ -28,10 +28,8 @@ void ResultLayerTest::SetUp() {
     std::tie(inputShape, inputPrecision, targetDevice, additionalConfig) = GetParam();
 
     auto ngPrc = FuncTestUtils::PrecisionUtils::convertIE2nGraphPrc(inputPrecision);
-    auto paramsIn = ngraph::builder::makeParams(ngPrc, {inputShape});
-    auto result_const = std::make_shared<ngraph::op::Constant>(ngPrc, ngraph::Shape{ 1 });
-    auto result = std::make_shared<ngraph::opset1::Result>(result_const);
-
-    function = std::make_shared<ngraph::Function>(result, paramsIn, "result");
+    auto params = ngraph::builder::makeParams(ngPrc, {inputShape});
+    const ngraph::ResultVector results{std::make_shared<ngraph::opset1::Result>(params[0])};
+    function = std::make_shared<ngraph::Function>(results, params, "result");
 }
 }  // namespace LayerTestsDefinitions
