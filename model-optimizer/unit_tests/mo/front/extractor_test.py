6444303067aca921382b127d1cd54f7cb7fb382a
achetver <anton.chetverikov@.intel.com>
2021-05-24 11:45:01 +0300

    Remove incorrectly added test

--- model-optimizer/unit_tests/mo/front/extractor_test.py
+++ model-optimizer/unit_tests/mo/front/extractor_test.py
@@ -672,24 +672,3 @@ class TestBoolToSrtFunction(unittest.TestCase):
         for attr in attrs:
             pool_1_node.bool_attr = attr[0]
             self.assertEqual(attr[1], bool_to_str(pool_1_node, 'bool_attr'))
-
-"""
-class TestAddInputOpFunction(unittest.TestCase):
-    nodes = {
-        'old_input': {'type': 'Parameter', 'kind': 'op', 'op': 'Parameter'},
-        'inp_data': {'kind': 'data', 'shape': shape + 1},
-        'conv_1': {'type': 'Convolution', 'kind': 'op', 'op': 'NotPlaceholder'},
-        'conv_data': {'kind': 'data', 'shape': shape, 'value': None},
-        'relu_1': {'type': 'ReLU', 'kind': 'op', 'op': 'NotPlaceholder'},
-    }
-    edges = [
-        ('old_input', 'inp_data'),
-        ('inp_data', 'conv_1'),
-        ('conv_1', 'conv_data'),
-        ('conv_data', 'relu_1'),
-    ]
-
-    def test_add_input_op_input_port_with_data(self):
-        graph = build_graph(self.nodes, self.edges)
-        pass
-"""
