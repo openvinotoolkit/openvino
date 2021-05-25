8d538986ed5a68353230c8973b69525b64e5d7e2
Egor Duplensky <egor.duplenskii@intel.com>
2021-05-25 21:20:30 +0300

    [CPU] Add bf16 support for ROI pooling (#5187)

--- inference-engine/src/mkldnn_plugin/nodes/mkldnn_roi_pooling_node.h
+++ inference-engine/src/mkldnn_plugin/nodes/mkldnn_roi_pooling_node.h
@@ -4,8 +4,9 @@
 
 #pragma once
 
-#include <ie_common.h>
 #include <mkldnn_node.h>
+#include <ie_common.h>
+
 #include <string>
 #include <memory>
 #include <vector>
@@ -22,12 +23,17 @@ struct jit_roi_pooling_params {
     int pooled_h;
     int pooled_w;
 
+    InferenceEngine::Precision src_prc;
+    InferenceEngine::Precision dst_prc;
+    int src_data_size;
+    int dst_data_size;
+
     Algorithm alg;
 };
 
 struct jit_roi_pooling_call_args {
-    const float *src;
-    float *dst;
+    const void *src;
+    void *dst;
 
     size_t kh;
     size_t kw;
@@ -68,15 +74,22 @@ public:
     void execute(mkldnn::stream strm) override;
     bool created() const override;
 
+private:
     static bool isSupportedOperation(const std::shared_ptr<ngraph::Node>& op, std::string& errorMessage) noexcept;
 
-private:
+    template<typename T> void execute();
+    template<typename T> struct ROIPoolingExecute;
+
+    InferenceEngine::Precision runtimePrecision;
+
+    size_t src_data_size;
+    size_t dst_data_size;
+
     int pooled_h = 0;
     int pooled_w = 0;
     float spatial_scale = 0;
 
     jit_roi_pooling_params jpp = {};
-
     std::shared_ptr<jit_uni_roi_pooling_kernel> roi_pooling_kernel = nullptr;
 
     std::string errorPrefix;
