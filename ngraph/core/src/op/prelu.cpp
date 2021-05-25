3cc95db51d62a44d09d285a0e830786949758c9d
Patryk Elszkowski <patryk.elszkowki@intel.com>
2021-05-25 12:45:37 +0200

    prelu new impl

--- ngraph/core/src/op/prelu.cpp
+++ ngraph/core/src/op/prelu.cpp
@@ -111,7 +111,8 @@ namespace prelu
                                   slope->get_data_ptr<ET>(),
                                   out->get_data_ptr<ET>(),
                                   arg->get_shape(),
-                                  slope->get_shape());
+                                  slope->get_shape(),
+                                  out->get_shape());
         return true;
     }
 
