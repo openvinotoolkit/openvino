26bfa6f0acddc18f7d666de671c8b7a504fc4dad
Ilya Lavrenov <ilya.lavrenov@intel.com>
2021-05-26 18:07:08 +0300

    Tests for dynamic preprocessing in SetBlob (#5798)
    * Corrected tests to match CVS-53713
    
    * Fixed tests configs
    
    * Skip tests on GPU
    
    * Commented condition inside main SetBlob because of MYRIAD
    
    * Adopted tests

--- docs/template_plugin/tests/functional/shared_tests_instances/hetero/synthetic.cpp
+++ docs/template_plugin/tests/functional/shared_tests_instances/hetero/synthetic.cpp
@@ -2,10 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "hetero/synthetic.hpp"
-
 #include <vector>
 
+#include "hetero/synthetic.hpp"
 #include "ngraph_functions/builders.hpp"
 #include "ngraph_functions/subgraph_builders.hpp"
 
@@ -13,12 +12,14 @@ namespace {
 using namespace HeteroTests;
 
 INSTANTIATE_TEST_CASE_P(smoke_SingleMajorNode, HeteroSyntheticTest,
-                        ::testing::Combine(::testing::Values(std::vector<PluginParameter> {{"TEMPLATE0", "templatePlugin"}, {"TEMPLATE1", "templatePlugin"}}),
-                                           ::testing::ValuesIn(HeteroTests::HeteroSyntheticTest::_singleMajorNodeFunctions)),
+                        ::testing::Combine(
+                                ::testing::Values(std::vector<PluginParameter>{{"TEMPLATE0", "templatePlugin"}, {"TEMPLATE1", "templatePlugin"}}),
+                                ::testing::ValuesIn(HeteroTests::HeteroSyntheticTest::_singleMajorNodeFunctions)),
                         HeteroSyntheticTest::getTestCaseName);
 
 INSTANTIATE_TEST_CASE_P(nightly_RandomMajorNodes, HeteroSyntheticTest,
-                        ::testing::Combine(::testing::Values(std::vector<PluginParameter> {{"TEMPLATE0", "templatePlugin"}, {"TEMPLATE1", "templatePlugin"}}),
-                                           ::testing::ValuesIn(HeteroTests::HeteroSyntheticTest::_randomMajorNodeFunctions)),
+                        ::testing::Combine(
+                                ::testing::Values(std::vector<PluginParameter>{{"TEMPLATE0", "templatePlugin"}, {"TEMPLATE1", "templatePlugin"}}),
+                                ::testing::ValuesIn(HeteroTests::HeteroSyntheticTest::_randomMajorNodeFunctions)),
                         HeteroSyntheticTest::getTestCaseName);
 }  // namespace
