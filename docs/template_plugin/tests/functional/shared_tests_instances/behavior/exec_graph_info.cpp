c71103501791f313038debe7aaa6575b84090b15
Ilya Lavrenov <ilya.lavrenov@intel.com>
2021-05-26 18:07:08 +0300

    Tests for dynamic preprocessing in SetBlob (#5798)
    * Corrected tests to match CVS-53713
    
    * Fixed tests configs
    
    * Skip tests on GPU
    
    * Commented condition inside main SetBlob because of MYRIAD
    
    * Adopted tests

--- docs/template_plugin/tests/functional/shared_tests_instances/behavior/exec_graph_info.cpp
+++ docs/template_plugin/tests/functional/shared_tests_instances/behavior/exec_graph_info.cpp
@@ -2,21 +2,28 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "behavior/exec_graph_info.hpp"
-
 #include <vector>
+
+#include "behavior/exec_graph_info.hpp"
 
 using namespace BehaviorTestsDefinitions;
 
 namespace {
 
-const std::vector<InferenceEngine::Precision> netPrecisions = {InferenceEngine::Precision::FP32, InferenceEngine::Precision::FP16};
+const std::vector<InferenceEngine::Precision> netPrecisions = {
+    InferenceEngine::Precision::FP32,
+    InferenceEngine::Precision::FP16
+};
 
-const std::vector<std::map<std::string, std::string>> configs = {{}};
+const std::vector<std::map<std::string, std::string>> configs = {
+    {}
+};
 
 INSTANTIATE_TEST_CASE_P(smoke_BehaviorTests, ExecGraphTests,
-                        ::testing::Combine(::testing::ValuesIn(netPrecisions), ::testing::Values(CommonTestUtils::DEVICE_TEMPLATE),
-                                           ::testing::ValuesIn(configs)),
+                        ::testing::Combine(
+                                ::testing::ValuesIn(netPrecisions),
+                                ::testing::Values(CommonTestUtils::DEVICE_TEMPLATE),
+                                ::testing::ValuesIn(configs)),
                         ExecGraphTests::getTestCaseName);
 
 }  // namespace
