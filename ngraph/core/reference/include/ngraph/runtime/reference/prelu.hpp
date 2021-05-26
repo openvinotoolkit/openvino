9bbebfcfa110875a021f0c0b918e3d65324d1602
Patryk Elszkowski <patryk.elszkowki@intel.com>
2021-05-26 10:53:31 +0200

    prelu new ref implementation and backend tests in separate file

--- ngraph/core/reference/include/ngraph/runtime/reference/prelu.hpp
+++ ngraph/core/reference/include/ngraph/runtime/reference/prelu.hpp
@@ -4,12 +4,10 @@
 
 #pragma once
 
-#include <cmath>
+#include <algorithm>
 #include <cstddef>
-#include <ngraph/op/util/attr_types.hpp>
+#include <ngraph/runtime/reference/autobroadcast_binop.hpp>
 #include <ngraph/shape.hpp>
-
-#include "ngraph/runtime/reference/autobroadcast_binop.hpp"
 
 namespace ngraph
 {
@@ -17,19 +15,36 @@ namespace ngraph
     {
         namespace reference
         {
+            bool shapes_match(const Shape& input, const Shape& output)
+            {
+                if (input.size() > output.size())
+                {
+                    return false;
+                }
+                const auto matching_size = [](size_t in_size, size_t out_size) {
+                    return in_size == out_size || in_size == 1;
+                };
+                return std::equal(input.rbegin(), input.rend(), output.rbegin(), matching_size);
+            }
             template <typename T>
             void prelu(const T* arg,
                        const T* slope,
                        T* out,
                        const Shape& arg_shape,
-                       const Shape& slope_shape)
+                       const Shape& slope_shape,
+                       const Shape& out_shape)
             {
-                int cnt = 0;
-                for (size_t i = 0; i < shape_size(arg_shape); ++i)
-                {
-                    out[i] =
-                        arg[i] < T(0) ? T(arg[i] * slope[cnt++ % shape_size(slope_shape)]) : arg[i];
-                }
+                NGRAPH_CHECK(shapes_match(arg_shape, out_shape) &&
+                                 shapes_match(slope_shape, out_shape),
+                             "PReLU has invalid input/output dims configuration.");
+
+                autobroadcast_binop(arg,
+                                    slope,
+                                    out,
+                                    arg_shape,
+                                    slope_shape,
+                                    op::AutoBroadcastType::NUMPY,
+                                    [](T x, T y) -> T { return x < T(0) ? T(x * y) : x; });
             }
         } // namespace reference
     }     // namespace runtime
