27b44a969f290f05f80360a53fa59c3af6e99a09
Ilya Lavrenov <ilya.lavrenov@intel.com>
2021-05-26 18:07:08 +0300

    Tests for dynamic preprocessing in SetBlob (#5798)
    * Corrected tests to match CVS-53713
    
    * Fixed tests configs
    
    * Skip tests on GPU
    
    * Commented condition inside main SetBlob because of MYRIAD
    
    * Adopted tests

--- docs/template_plugin/tests/functional/shared_tests_instances/behavior/config.cpp
+++ docs/template_plugin/tests/functional/shared_tests_instances/behavior/config.cpp
@@ -2,17 +2,19 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "behavior/config.hpp"
-
-#include <template/template_config.hpp>
-
 #include "multi-device/multi_device_config.hpp"
+
+#include "behavior/config.hpp"
+#include <template/template_config.hpp>
 
 using namespace BehaviorTestsDefinitions;
 
 namespace {
 
-const std::vector<InferenceEngine::Precision> netPrecisions = {InferenceEngine::Precision::FP32, InferenceEngine::Precision::FP16};
+const std::vector<InferenceEngine::Precision> netPrecisions = {
+    InferenceEngine::Precision::FP32,
+    InferenceEngine::Precision::FP16
+};
 
 const std::vector<std::map<std::string, std::string>> configs = {
     {{TEMPLATE_CONFIG_KEY(THROUGHPUT_STREAMS), InferenceEngine::PluginConfigParams::CPU_THROUGHPUT_AUTO}},
@@ -25,23 +27,32 @@ const std::vector<std::map<std::string, std::string>> inconfigs = {
 };
 
 INSTANTIATE_TEST_CASE_P(smoke_BehaviorTests, IncorrectConfigTests,
-                        ::testing::Combine(::testing::ValuesIn(netPrecisions), ::testing::Values(CommonTestUtils::DEVICE_TEMPLATE),
-                                           ::testing::ValuesIn(inconfigs)),
+                        ::testing::Combine(
+                                ::testing::ValuesIn(netPrecisions),
+                                ::testing::Values(CommonTestUtils::DEVICE_TEMPLATE),
+                                ::testing::ValuesIn(inconfigs)),
                         IncorrectConfigTests::getTestCaseName);
 
 INSTANTIATE_TEST_CASE_P(smoke_BehaviorTests, IncorrectConfigAPITests,
-                        ::testing::Combine(::testing::ValuesIn(netPrecisions), ::testing::Values(CommonTestUtils::DEVICE_TEMPLATE),
-                                           ::testing::ValuesIn(inconfigs)),
+                        ::testing::Combine(
+                                ::testing::ValuesIn(netPrecisions),
+                                ::testing::Values(CommonTestUtils::DEVICE_TEMPLATE),
+                                ::testing::ValuesIn(inconfigs)),
                         IncorrectConfigAPITests::getTestCaseName);
 
+
 INSTANTIATE_TEST_CASE_P(smoke_BehaviorTests, CorrectConfigAPITests,
-                        ::testing::Combine(::testing::ValuesIn(netPrecisions), ::testing::Values(CommonTestUtils::DEVICE_TEMPLATE),
-                                           ::testing::ValuesIn(configs)),
+                        ::testing::Combine(
+                                ::testing::ValuesIn(netPrecisions),
+                                ::testing::Values(CommonTestUtils::DEVICE_TEMPLATE),
+                                ::testing::ValuesIn(configs)),
                         CorrectConfigAPITests::getTestCaseName);
 
 INSTANTIATE_TEST_CASE_P(smoke_Multi_BehaviorTests, CorrectConfigTests,
-                        ::testing::Combine(::testing::ValuesIn(netPrecisions), ::testing::Values(CommonTestUtils::DEVICE_TEMPLATE),
-                                           ::testing::ValuesIn(configs)),
+                        ::testing::Combine(
+                                ::testing::ValuesIn(netPrecisions),
+                                ::testing::Values(CommonTestUtils::DEVICE_TEMPLATE),
+                                ::testing::ValuesIn(configs)),
                         CorrectConfigAPITests::getTestCaseName);
 
-}  // namespace
+} // namespace
