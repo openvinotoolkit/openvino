f24020b0962e07032d3ca6e7dd1fb6ad3d75b888
Ivan Tikhonov <ivan.tikhonov@intel.com>
2021-05-27 05:59:16 +0300

    update LowLatency functional tests

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/subgraph_tests/memory_LSTMCell.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/subgraph_tests/memory_LSTMCell.cpp
@@ -15,34 +15,31 @@ namespace SubgraphTestsDefinitions {
     };
 
     std::vector<size_t> input_sizes = {
-        80,
-        32,
-        64,
-        100,
-        25
+            80,
+            32,
+            64,
+            100,
+            25
     };
 
     std::vector<size_t> hidden_sizes = {
-        128,
-        200,
-        300,
-        24,
-        32,
+            128,
+            200,
+            300,
+            24,
+            32,
     };
 
     std::map<std::string, std::string> additional_config = {
-        {"GNA_COMPACT_MODE", "NO"},
-        {"GNA_DEVICE_MODE", "GNA_SW_EXACT"},
-        {"GNA_SCALE_FACTOR_0", "1638.4"},
     };
 
     INSTANTIATE_TEST_CASE_P(smoke_MemoryLSTMCellTest, MemoryLSTMCellTest,
-        ::testing::Combine(
-            ::testing::ValuesIn(transformation),
-            ::testing::Values(CommonTestUtils::DEVICE_GNA),
-            ::testing::Values(InferenceEngine::Precision::FP32),
-            ::testing::ValuesIn(input_sizes),
-            ::testing::ValuesIn(hidden_sizes),
-            ::testing::Values(additional_config)),
-        MemoryLSTMCellTest::getTestCaseName);
+                            ::testing::Combine(
+                                    ::testing::ValuesIn(transformation),
+                                    ::testing::Values(CommonTestUtils::DEVICE_CPU),
+                                    ::testing::Values(InferenceEngine::Precision::FP32),
+                                    ::testing::ValuesIn(input_sizes),
+                                    ::testing::ValuesIn(hidden_sizes),
+                                    ::testing::Values(additional_config)),
+                            MemoryLSTMCellTest::getTestCaseName);
 } // namespace SubgraphTestsDefinitions
