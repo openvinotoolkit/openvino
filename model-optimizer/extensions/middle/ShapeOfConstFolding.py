84e7ef276941b94fe944497613893e07ea5706f8
iimironov <iliya.mironov@intel.com>
2021-05-27 13:41:22 +0300

    Add ShapeOfConstFolding transform

---
+++ model-optimizer/extensions/middle/ShapeOfConstFolding.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2018-2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+import numpy as np
+
+from extensions.ops.activation_ops import SoftPlus
+from mo.middle.replacement import MiddleReplacementPattern
+from mo.front.subgraph_matcher import SubgraphMatch
+from mo.graph.graph import Graph, rename_nodes
+from mo.middle.pattern_match import check_value
+from mo.ops.const import Const
+
+
+class ShapeOfConstFolding(MiddleReplacementPattern):
+    """
+    TThe transformation calculates the value of the constant
+    Const() -> ShapeOf() --> Const()
+    """
+    enabled = True
+
+    def pattern(self):
+        return dict(
+            nodes=[
+                ('const', dict(op='Const')),
+                ('shape', dict(op='ShapeOf')),
+            ],
+            edges=[
+                ('const', 'shape', {})
+            ])
+
+    def replace_pattern(self, graph: Graph, match: [dict, SubgraphMatch]):
+        const = match['const']
+        shape = match['shape']
+
+        shape_name = shape.soft_get('name', shape.id)
+
+        shape_value = shape.in_port(0).data.get_value()
+        shape_const_node = Const(graph, {'name': shape_name + '/ExecutionConstValue',
+                                         'value': shape_value}).create_node()
+        shape.out_port(0).get_connection().set_source(shape_const_node.out_port(0))
+        rename_nodes([(shape, shape_name + '/TBD'), (shape_const_node, shape_name)])
