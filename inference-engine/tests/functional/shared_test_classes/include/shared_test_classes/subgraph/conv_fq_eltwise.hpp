505ede2d38b66eaab14653bc158f8565e1ac1d60
Elizaveta Lobanova <elizaveta.lobanova@intel.com>
2021-05-27 16:49:40 +0300

    [GNA] Avoid integers overflow during pwl calculation for FakeQuantize (#5841)
    * [GNA] Avoid integers overflow during pwl calculation for FakeQuantize
    
    * The similar fix for Relu

---
+++ inference-engine/tests/functional/shared_test_classes/include/shared_test_classes/subgraph/conv_fq_eltwise.hpp
@@ -0,0 +1,55 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <tuple>
+#include <vector>
+#include <string>
+#include <memory>
+
+#include "shared_test_classes/base/layer_test_utils.hpp"
+#include "ngraph_functions/builders.hpp"
+#include "ngraph_functions/utils/ngraph_helpers.hpp"
+
+namespace SubgraphTestsDefinitions {
+
+typedef std::tuple<
+        size_t,                           // levels
+        std::vector<float>                // input generator data: low, high, resolution
+> FqSpecificParams;
+
+typedef std::tuple<
+        std::vector<size_t>,                 // Kernel Shape
+        std::vector<size_t>,                 // Strides
+        size_t,                              // Input channels
+        size_t                               // Output channels
+> ConvParams;
+
+typedef std::tuple<
+        FqSpecificParams,
+        ConvParams,
+        InferenceEngine::Precision,        // Net precision
+        InferenceEngine::SizeVector,       // Input shapes
+        LayerTestsUtils::TargetDevice,     // Device name
+        std::map<std::string, std::string> // Additional backend configuration and alis name to it
+> ConvFqEltwiseTestParamsSet;
+
+class ConvFqEltwiseTest : public testing::WithParamInterface<ConvFqEltwiseTestParamsSet>,
+                   virtual public LayerTestsUtils::LayerTestsCommon {
+public:
+    static std::string getTestCaseName(testing::TestParamInfo<ConvFqEltwiseTestParamsSet> obj);
+
+protected:
+    void SetUp() override;
+    InferenceEngine::Blob::Ptr GenerateInput(const InferenceEngine::InputInfo &info) const override;
+
+protected:
+    float inputDataMin        = 0.0;
+    float inputDataMax        = 10.0;
+    float inputDataResolution = 1.0;
+    int32_t  seed = 1;
+};
+
+}  // namespace SubgraphTestsDefinitions
