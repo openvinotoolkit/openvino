f5700b5ddbad042b5c929c32849a218985955ccd
Anastasia Popova <anastasia.popova@intel.com>
2021-05-27 16:06:15 +0300

    Added attributes checks in unit test.

--- model-optimizer/mo/middle/passes/fusing/fuse_linear_ops.py
+++ model-optimizer/mo/middle/passes/fusing/fuse_linear_ops.py
@@ -115,12 +115,12 @@ def _fuse_mul(graph: Graph, node: Node, fuse_nodes: list, backward: bool = True)
         w_const_out_data = w_const.node.out_node(w_const.idx)
 
         # During this reconnection new data node name is copied from the data node
-        # outgoing from w_const port. Duplicate names of data nodes lead to appear
+        # outgoing from w_const port. Duplicate names of data nodes lead to appearing
         # of duplicate op node names after constant folding. So we should manually
         # set a unique name for the new data node.
         if fuse_node_in_data.soft_get('name') == w_const_out_data.soft_get('name') and \
                 fuse_node_in_data.soft_get('name', None) is not None:
-            fuse_node.in_node(weights_port.idx)['name'] = graph.unique_id()
+            fuse_node.in_node(weights_port.idx)['name'] = graph.unique_id(mul_name)
 
         # If we fuse in backward direction we should multiply biases if they exists
         if backward and len(fuse_node.in_ports()) == 3 and not fuse_node.in_port(2).disconnected() and \
