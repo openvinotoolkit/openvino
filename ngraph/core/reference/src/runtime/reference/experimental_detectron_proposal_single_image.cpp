c07855728c65a4391bf77c5da77536a6baca4953
VGavrilov <vladimir.gavrilov@intel.com>
2021-05-28 13:10:26 +0300

    Written the postprocessing function for the nGraph reference implementation of the operation ExperimentalDetectronGenerateProposalsSingleImage.

--- ngraph/core/reference/src/runtime/reference/experimental_detectron_proposal_single_image.cpp
+++ ngraph/core/reference/src/runtime/reference/experimental_detectron_proposal_single_image.cpp
@@ -38,69 +38,53 @@ namespace ngraph
                 const std::vector<float>& output_scores,
                 const Shape& output_rois_shape,
                 const Shape& output_scores_shape)
-//             void experimental_detectron_detection_output_postprocessing(
-//                 void* pboxes,
-//                 void* pclasses,
-//                 void* pscores,
-//                 const ngraph::element::Type output_type,
-//                 const std::vector<float>& output_boxes,
-//                 const std::vector<int32_t>& output_classes,
-//                 const std::vector<float>& output_scores,
-//                 const Shape& output_boxes_shape,
-//                 const Shape& output_classes_shape,
-//                 const Shape& output_scores_shape)
             {
-//                 size_t rois_num = output_boxes_shape[0];
-//
-//                 switch (output_type)
-//                 {
-//                 case element::Type_t::bf16:
-//                 {
-//                     bfloat16* boxes_ptr = reinterpret_cast<bfloat16*>(pboxes);
-//                     bfloat16* scores_ptr = reinterpret_cast<bfloat16*>(pscores);
-//                     for (size_t i = 0; i < rois_num; ++i)
-//                     {
-//                         boxes_ptr[4 * i + 0] = bfloat16(output_boxes[4 * i + 0]);
-//                         boxes_ptr[4 * i + 1] = bfloat16(output_boxes[4 * i + 1]);
-//                         boxes_ptr[4 * i + 2] = bfloat16(output_boxes[4 * i + 2]);
-//                         boxes_ptr[4 * i + 3] = bfloat16(output_boxes[4 * i + 3]);
-//                         scores_ptr[i] = bfloat16(output_scores[i]);
-//                     }
-//                 }
-//                 break;
-//                 case element::Type_t::f16:
-//                 {
-//                     float16* boxes_ptr = reinterpret_cast<float16*>(pboxes);
-//                     float16* scores_ptr = reinterpret_cast<float16*>(pscores);
-//                     for (size_t i = 0; i < rois_num; ++i)
-//                     {
-//                         boxes_ptr[4 * i + 0] = float16(output_boxes[4 * i + 0]);
-//                         boxes_ptr[4 * i + 1] = float16(output_boxes[4 * i + 1]);
-//                         boxes_ptr[4 * i + 2] = float16(output_boxes[4 * i + 2]);
-//                         boxes_ptr[4 * i + 3] = float16(output_boxes[4 * i + 3]);
-//                         scores_ptr[i] = float16(output_scores[i]);
-//                     }
-//                 }
-//                 break;
-//                 case element::Type_t::f32:
-//                 {
-//                     float* boxes_ptr = reinterpret_cast<float*>(pboxes);
-//                     float* scores_ptr = reinterpret_cast<float*>(pscores);
-//                     memcpy(boxes_ptr,
-//                            output_boxes.data(),
-//                            shape_size(output_boxes_shape) * sizeof(float));
-//                     memcpy(scores_ptr,
-//                            output_scores.data(),
-//                            shape_size(output_scores_shape) * sizeof(float));
-//                 }
-//                 break;
-//                 default:;
-//                 }
-//
-//                 int32_t* classes_ptr = reinterpret_cast<int32_t*>(pclasses);
-//                 memcpy(classes_ptr,
-//                        output_classes.data(),
-//                        shape_size(output_classes_shape) * sizeof(int32_t));
+                size_t rois_num = output_rois_shape[0];
+
+                switch (output_type)
+                {
+                case element::Type_t::bf16:
+                {
+                    bfloat16* rois_ptr = reinterpret_cast<bfloat16*>(prois);
+                    bfloat16* scores_ptr = reinterpret_cast<bfloat16*>(pscores);
+                    for (size_t i = 0; i < rois_num; ++i)
+                    {
+                        rois_ptr[4 * i + 0] = bfloat16(output_rois[4 * i + 0]);
+                        rois_ptr[4 * i + 1] = bfloat16(output_rois[4 * i + 1]);
+                        rois_ptr[4 * i + 2] = bfloat16(output_rois[4 * i + 2]);
+                        rois_ptr[4 * i + 3] = bfloat16(output_rois[4 * i + 3]);
+                        scores_ptr[i] = bfloat16(output_scores[i]);
+                    }
+                }
+                break;
+                case element::Type_t::f16:
+                {
+                    float16* rois_ptr = reinterpret_cast<float16*>(pboxes);
+                    float16* scores_ptr = reinterpret_cast<float16*>(pscores);
+                    for (size_t i = 0; i < rois_num; ++i)
+                    {
+                        rois_ptr[4 * i + 0] = float16(output_rois[4 * i + 0]);
+                        rois_ptr[4 * i + 1] = float16(output_rois[4 * i + 1]);
+                        rois_ptr[4 * i + 2] = float16(output_rois[4 * i + 2]);
+                        rois_ptr[4 * i + 3] = float16(output_rois[4 * i + 3]);
+                        scores_ptr[i] = float16(output_scores[i]);
+                    }
+                }
+                break;
+                case element::Type_t::f32:
+                {
+                    float* rois_ptr = reinterpret_cast<float*>(pboxes);
+                    float* scores_ptr = reinterpret_cast<float*>(pscores);
+                    memcpy(rois_ptr,
+                           output_rois.data(),
+                           shape_size(output_rois_shape) * sizeof(float));
+                    memcpy(scores_ptr,
+                           output_scores.data(),
+                           shape_size(output_scores_shape) * sizeof(float));
+                }
+                break;
+                default:;
+                }
             }
         } // namespace reference
     }     // namespace runtime
