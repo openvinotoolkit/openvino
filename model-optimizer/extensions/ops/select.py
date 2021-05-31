e105c3c443400c1687624ca205531e418d5e6283
Evgeny Lazarev <evgeny.lazarev@intel.com>
2021-05-31 10:16:30 +0300

    Updated Select shape inference function to use common shape broadcasting function supporting dynamism

--- model-optimizer/extensions/ops/select.py
+++ model-optimizer/extensions/ops/select.py
@@ -3,9 +3,9 @@
 
 import numpy as np
 
-from mo.front.common.partial_infer.utils import int64_array, broadcast_shape
 from mo.graph.graph import Node, Graph
 from mo.ops.op import Op
+from mo.utils.broadcasting import bi_directional_shape_broadcasting
 
 
 class Select(Op):
@@ -13,13 +13,13 @@ class Select(Op):
 
     def __init__(self, graph: Graph, attrs: dict):
         mandatory_props = {
-            'op': __class__.op,
-            'type': __class__.op,
+            'op': self.op,
+            'type': self.op,
             'version': 'opset1',
             'in_ports_count': 3,
             'out_ports_count': 1,
-            'infer': __class__.infer,
-            'type_infer': __class__.type_infer,
+            'infer': self.infer,
+            'type_infer': self.type_infer,
             'auto_broadcast': 'numpy'
         }
         super().__init__(graph, mandatory_props, attrs)
@@ -29,25 +29,26 @@ class Select(Op):
 
     @staticmethod
     def infer(node: Node):
-        assert len([port for port in node.in_ports().values() if not port.disconnected()]) == 3, "Select operation must have 3 inputs:" \
-                                          " \'condition\', \'then\' and \'else\' tensors"
+        assert len([port for port in node.in_ports().values() if not port.disconnected()]) == 3, \
+            "Select operation must have 3 inputs: \'condition\', \'then\' and \'else\' tensors"
 
         condition_value = node.in_port(0).data.get_value()
         resulting_tensors = [node.in_port(1).data.get_value(), node.in_port(2).data.get_value()]
 
         a_shape = node.in_port(1).data.get_shape()
         b_shape = node.in_port(2).data.get_shape()
-        node.out_port(0).data.set_shape(broadcast_shape(a_shape, b_shape))
+        node.out_port(0).data.set_shape(bi_directional_shape_broadcasting(a_shape, b_shape))
         # Case with unknown condition
         if condition_value is not None:
             output_value = np.where(condition_value, resulting_tensors[0], resulting_tensors[1])
             if condition_value.size != 1:
-                if np.any(output_value == None):
-                    # If any element of output value is None that means that we use the value from 'then' or 'else' tensor
-                    # which is not defined, this means that we cannot perform value propagation.
+                if np.any(output_value is None):
+                    # If any element of output value is None that means that we use the value from the 'then' or the
+                    # 'else' tensor which is not defined, this means that we cannot perform value propagation.
                     output_value = None
             else:
-                output_value = np.array(output_value, dtype=resulting_tensors[not np.bool(condition_value.item(0))].dtype)
+                output_value = np.array(output_value,
+                                        dtype=resulting_tensors[not np.bool(condition_value.item(0))].dtype)
 
             if output_value is not None:
                 node.out_port(0).data.set_value(np.array(output_value))
