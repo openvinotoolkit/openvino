9b023230b53c338acf0c9a7dc1aaf9fd8e84179a
Evgeny Lazarev <evgeny.lazarev@intel.com>
2021-05-31 10:57:43 +0300

    Fixed operation TFResize shape infer function to work correctly for case when model is converted with --disable_nhwc_to_nchw

--- model-optimizer/extensions/ops/TFResize.py
+++ model-optimizer/extensions/ops/TFResize.py
@@ -1,7 +1,6 @@
 # Copyright (C) 2018-2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from mo.front.common.layout import get_height_dim, get_width_dim
 from mo.front.common.partial_infer.utils import int64_array
 from mo.graph.graph import Node, Graph
 from mo.ops.op import Op
@@ -47,8 +46,7 @@ class TFResize(Op):
 
         output_shape = int64_array(input_shape.copy())
 
-        layout = node.graph.graph['layout']
-        output_shape[get_height_dim(layout, input_rank)] = new_sizes_value[0]
-        output_shape[get_width_dim(layout, input_rank)] = new_sizes_value[1]
+        output_shape[1] = new_sizes_value[0]
+        output_shape[2] = new_sizes_value[1]
 
         node.out_port(0).data.set_shape(output_shape)
