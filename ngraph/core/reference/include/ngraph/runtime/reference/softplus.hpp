91af8250567f843161f920be2b44f219a127efb4
DD <duncan.ding@intel.com>
2021-06-01 09:48:43 +0800

    change threshold to 20 instead of max limit of data type

--- ngraph/core/reference/include/ngraph/runtime/reference/softplus.hpp
+++ ngraph/core/reference/include/ngraph/runtime/reference/softplus.hpp
@@ -16,7 +16,7 @@ namespace ngraph
             template <typename T>
             void softplus(const T* arg, T* out, size_t count)
             {
-                const T threshold = static_cast<T>(std::log(std::numeric_limits<T>::max()));
+                const T threshold = 20.0;
 
                 for (size_t i = 0; i < count; i++)
                 {
