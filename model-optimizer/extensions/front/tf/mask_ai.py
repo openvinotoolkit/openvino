e91896ad9636c8bb380d5d78cfc12da0398c4646
Nam David <david.nam@intel.com>
2021-06-01 16:38:08 +0900

    [Model Optimizer] Add extension for MaskAI SRGAN

---
+++ model-optimizer/extensions/front/tf/mask_ai.py
@@ -0,0 +1,70 @@
+"""
+ Copyright (C) 2018-2020 Intel Corporation
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
+import numpy as np
+
+from mo.front.common.partial_infer.utils import int64_array
+from mo.front.common.replacement import FrontReplacementPattern
+from mo.front.tf.graph_utils import create_op_node_with_second_input
+from mo.graph.graph import Graph
+from mo.middle.pattern_match import apply_pattern
+from mo.ops.const import Const
+from mo.ops.reshape import Reshape
+
+MASK_EIP_PATTERN = {
+    'nodes': [
+        ('eip', dict(op='ExtractImagePatches')),
+        ('reshape', dict(op='Reshape')),
+        ('ss', dict(op='StridedSlice')),
+        ('mean', dict(op='ReduceMean')),
+        ('reshape_const', dict(op='Const')),
+        ('mean_const', dict(op='Const')),
+    ],
+    'edges': [
+        ('eip', 'reshape', {'out': 0, 'in': 0}),
+        ('reshape_const', 'reshape', {'out': 0, 'in': 1}),
+        ('reshape', 'ss', {'out': 0, 'in': 0}),
+        ('ss', 'mean', {'out': 0, 'in': 0}),
+        ('mean_const', 'mean', {'out': 0, 'in': 1}),
+    ]
+}
+
+class MaskTransformation(FrontReplacementPattern):
+    enabled = True
+
+    def find_and_replace_pattern(self, graph: Graph):
+        apply_pattern(graph, **MASK_EIP_PATTERN, action=self.optimize_eip)
+
+    @staticmethod
+    def optimize_eip(graph: Graph, match: dict):
+        reshape_const = match['reshape_const']
+        mean_const = match['mean_const']
+        if not np.array_equal(reshape_const.value, [1, -1, 3, 3, 1]):
+            return
+        if not np.array_equal(mean_const.value, [2, 3]):
+            return
+
+        mean = match['mean']
+        eip = match['eip']
+
+        mean.in_port(0).disconnect()
+        eip.out_port(0).get_connection().set_destination(mean.in_port(0))
+
+        mean.in_port(1).disconnect()
+        mean.in_port(1).connect(Const(graph, {'value': int64_array([-1])}).create_node().out_port(0))
+        mean.out_port(0).get_connection().insert_node(
+            create_op_node_with_second_input(graph, Reshape, int64_array([1, -1, 1, 1])))
+
+        graph.remove_nodes_from([match['ss'].id])
