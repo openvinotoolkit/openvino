94517ad0f438bfeef7c2b8c6b32633f0f57f529b
Sergey Lyubimtsev <sergey.lyubimtsev@intel.com>
2021-06-02 18:58:27 +0300

    blank line

--- inference-engine/ie_bridges/python/wheel/setup.py
+++ inference-engine/ie_bridges/python/wheel/setup.py
@@ -161,7 +161,7 @@ class PrepareLibs(build_clib):
         # additional blacklist filter, just to fix cmake install issues
         blacklist = ['.lib', '.pdb', '_debug.dll', '_debug.dylib']
         package_dir = os.path.join(get_package_dir(PY_INSTALL_CFG), WHEEL_LIBS_INSTALL_DIR)
-        
+
         for src_dir in src_dirs:
             local_base_dir = Path(src_dir)
             for file_path in local_base_dir.rglob('*'):
