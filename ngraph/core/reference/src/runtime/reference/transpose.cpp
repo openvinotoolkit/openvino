ae8d25be12572b435e7b4e486e744ef0bdfad17b
Roman Kazantsev <roman.kazantsev@intel.com>
2021-06-04 11:08:30 +0300

    Implement Einsum reference in nGraph interpreter (#5923)
    * Implement Einsum reference in nGraph interpreter
    
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>
    
    * Apply code-style patch and fix build issues
    
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>
    
    * Fix CI build
    
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>
    
    * Fix fast build and apply code review feedback
    
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>
    
    * Add visitor API tests, check for input type and reduce memory consumption
    
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>
    
    * Apply code style
    
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>
    
    * Add visitor API tests
    
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- ngraph/core/reference/src/runtime/reference/transpose.cpp
+++ ngraph/core/reference/src/runtime/reference/transpose.cpp
@@ -2,13 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#pragma once
-
 #include <cfenv>
 #include <cmath>
 #include <numeric>
 #include <vector>
 
+#include "ngraph/runtime/opt_kernel/reshape.hpp"
+#include "ngraph/runtime/reference/transpose.hpp"
 #include "ngraph/shape.hpp"
 
 namespace ngraph
@@ -22,7 +22,14 @@ namespace ngraph
                            const Shape& data_shape,
                            size_t element_size,
                            const int64_t* axes_order,
-                           Shape out_shape);
+                           Shape out_shape)
+            {
+                // To reuse opt_kernel::reshape axes order vector has to be converted to AxisVector
+                // Negative axes are not supported, it is validated by transpose evaluate method
+                std::vector<size_t> axis_vector(axes_order, axes_order + data_shape.size());
+                runtime::opt_kernel::reshape(
+                    data, out, data_shape, axis_vector, out_shape, element_size);
+            }
         } // namespace reference
     }     // namespace runtime
 } // namespace ngraph
