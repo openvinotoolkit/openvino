95afa50d94b7164968753194557888ecf16a9e69
DD <duncan.ding@intel.com>
2021-06-04 10:43:45 +0800

    Revert "[CPU] Fix for  CoreThreadingTestsWithIterations tests (#5892)"
    This reverts commit 115aa143ef8d8a0a17aed705db2f6d6e1dcb4164.

--- inference-engine/src/mkldnn_plugin/mkldnn_weights_cache.cpp
+++ inference-engine/src/mkldnn_plugin/mkldnn_weights_cache.cpp
@@ -43,7 +43,8 @@ MKLDNNWeightsSharing::MKLDNNSharedMemory::Ptr MKLDNNWeightsSharing::findOrCreate
     MKLDNNMemoryPtr newPtr;
 
     if (found == sharedWeights.end()
-        || !((ptr = found->second) && (newPtr = ptr->sharedMemory.lock()))) {
+        || !(ptr = found->second)
+        || ptr->sharedMemory.expired()) {
         newPtr = create();
         ptr = std::make_shared<MKLDNNMemoryInfo>(newPtr, valid);
         sharedWeights[key] = ptr;
@@ -59,15 +60,15 @@ MKLDNNWeightsSharing::MKLDNNSharedMemory::Ptr MKLDNNWeightsSharing::get(const st
     auto found = sharedWeights.find(key);
 
     MKLDNNMemoryInfo::Ptr ptr;
-    MKLDNNMemoryPtr newPtr;
 
     if (found == sharedWeights.end()
-        || !((ptr = found->second) && (newPtr = ptr->sharedMemory.lock())))
+        || !(ptr = found->second)
+        || ptr->sharedMemory.expired())
         IE_THROW() << "Unknown shared memory with key " << key;
 
     return std::make_shared<MKLDNNSharedMemory>(ptr->valid
                                                 ? std::unique_lock<std::mutex>(ptr->guard, std::defer_lock)
-                                                : std::unique_lock<std::mutex>(ptr->guard), ptr, newPtr);
+                                                : std::unique_lock<std::mutex>(ptr->guard), ptr);
 }
 
 NumaNodesWeights::NumaNodesWeights() {
