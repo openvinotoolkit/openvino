Fixed feature dim calculation in concat_convolutions().

model-optimizer/mo/middle/passes/fusing/fuse_grouped_conv.py
@@ -87,7 +87,9 @@ def concat_convolutions(graph: Graph, start_node: Node, last_node: Node):
     weights_value = np.array(weights_node.value)
     bias_value = np.array(bias_node.value) if has_biases else None
 
-    feature_dim = 3 if graph.graph['layout'] == 'NHWC' else 0
+    dims_indices = range(len(weights_node.shape))
+    permuted_indices = gconv.get_weights_permute.inv[dims_indices]
+    feature_dim = np.where(permuted_indices == 0)[0]
 
     for conv in conv_nodes[1:]:
         weights_value = np.concatenate((weights_value, conv.in_node(1).value), axis=feature_dim)