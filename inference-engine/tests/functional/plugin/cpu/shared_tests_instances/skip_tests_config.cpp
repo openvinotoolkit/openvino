5164f67a3629173308b8928ef12de7aa0dd2bb64
blesniewski <bartosz.lesniewski@intel.com>
2021-06-08 12:32:45 +0200

    Add and disable cpu instantiations of slts

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
@@ -69,6 +69,8 @@ std::vector<std::string> disabledTestPatterns() {
         R"(.*CoreThreading.*smoke_QueryNetwork.*targetDevice=AUTO_config.*)",
         // reference doesn't cover I8, U8 cases. Issue: 55842
         R"(.*Gather7LayerTest.*netPRC=I8.*)",
+        // TODO: 57562 No dynamic output shape support
+        R"(.*NonZeroLayerTest.*)"
     };
 #ifdef __APPLE__
         // TODO: Issue 55717
