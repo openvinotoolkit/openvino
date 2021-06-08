9d89df8c863f4c558d060dc0605a03a8a304a01c
ggalieroc <gabriele.galiero.casay@intel.com>
2021-06-08 11:34:44 +0200

    Added minor changes
     * Replace CoordinateTransform for CoordinateTransformBasic
     * Added constant expression to set keep_dims as false

--- ngraph/core/reference/include/ngraph/runtime/reference/product.hpp
+++ ngraph/core/reference/include/ngraph/runtime/reference/product.hpp
@@ -19,16 +19,18 @@ namespace ngraph
             template <typename T>
             void product(const T* arg, T* out, const Shape& in_shape, const AxisSet& reduction_axes)
             {
-                auto out_shape = reduce(in_shape, reduction_axes, false);
+                constexpr bool dont_keep_dims_in_output = false;
+                auto out_shape = reduce(in_shape, reduction_axes, dont_keep_dims_in_output);
                 std::fill(out, out + shape_size(out_shape), 1);
 
                 const auto in_strides = row_major_strides(in_shape);
                 const auto out_strides = row_major_strides(out_shape);
 
-                CoordinateTransform input_transform(in_shape);
+                CoordinateTransformBasic input_transform(in_shape);
                 for (const Coordinate& input_coord : input_transform)
                 {
-                    Coordinate output_coord = reduce(input_coord, reduction_axes, false);
+                    Coordinate output_coord =
+                        reduce(input_coord, reduction_axes, dont_keep_dims_in_output);
 
                     size_t in_idx = std::inner_product(
                         input_coord.begin(), input_coord.end(), in_strides.begin(), 0);
