dd0ecdd1cdf2a3887ab2e6f5747d03b7a15ecfce
Michael Nosov <mikhail.nosov@intel.com>
2021-06-08 19:12:22 +0300

    Added XML/BIN prints for 'ngraph-FE' serialization path

--- model-optimizer/mo/front_ng/serialize.py
+++ model-optimizer/mo/front_ng/serialize.py
@@ -3,6 +3,7 @@
 
 import argparse
 import os
+from mo.pipeline.common import get_ir_version
 
 
 def ngraph_emit_ir(nGraphFunction, argv: argparse.Namespace):
@@ -14,4 +15,7 @@ def ngraph_emit_ir(nGraphFunction, argv: argparse.Namespace):
     orig_model_name = os.path.normpath(os.path.join(output_dir, argv.model_name))
     network.serialize(orig_model_name + ".xml", orig_model_name + ".bin")
     print('[ SUCCESS ] Converted with nGraph Serializer')
+    print('[ SUCCESS ] Generated IR version {} model.'.format(get_ir_version(argv)))
+    print('[ SUCCESS ] XML file: {}.xml'.format(orig_model_name))
+    print('[ SUCCESS ] BIN file: {}.bin'.format(orig_model_name))
     return 0
