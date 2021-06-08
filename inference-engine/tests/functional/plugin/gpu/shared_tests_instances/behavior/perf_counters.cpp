1b7c581a48f5b35174bcd4edd6264850971e383e
Shoujiang Ma <shoujiang.ma@intel.com>
2021-06-08 17:11:58 +0800

    [AUTO plugin] AUTO plugin will ignore other plugins' configuration (#5979)
    * AUTO plugin will ignore other plugins' configuration
    
    Signed-off-by: Shoujiang Ma <shoujiang.ma@intel.com>
    
    * Update tests
    
    Signed-off-by: Shoujiang Ma <shoujiang.ma@intel.com>
    
    * Support PER_COUNT config which is needed in benchmark_app
    
    Signed-off-by: Shoujiang Ma <shoujiang.ma@intel.com>
    
    * Address reviewer comments: check config and throw exception for unsupported, but that begin with "AUTO_" will be ignored
    
    Signed-off-by: Shoujiang Ma <shoujiang.ma@intel.com>
    
    * Fix CI tests issue
    
    Signed-off-by: Shoujiang Ma <shoujiang.ma@intel.com>

--- inference-engine/tests/functional/plugin/gpu/shared_tests_instances/behavior/perf_counters.cpp
+++ inference-engine/tests/functional/plugin/gpu/shared_tests_instances/behavior/perf_counters.cpp
@@ -14,14 +14,6 @@ namespace {
             {{ MULTI_CONFIG_KEY(DEVICE_PRIORITIES) , CommonTestUtils::DEVICE_GPU}}
     };
 
-    const std::vector<std::map<std::string, std::string>> Autoconfigs = {
-            {{ AUTO_CONFIG_KEY(DEVICE_LIST) , CommonTestUtils::DEVICE_GPU}}
-    };
-
-    const std::vector<std::map<std::string, std::string>> auto_cpu_gpu_conf = {
-        {{InferenceEngine::KEY_AUTO_DEVICE_LIST , std::string(CommonTestUtils::DEVICE_CPU) + "," + CommonTestUtils::DEVICE_GPU}}
-    };
-
     INSTANTIATE_TEST_CASE_P(smoke_BehaviorTests, PerfCountersTest,
                             ::testing::Combine(
                                     ::testing::Values(InferenceEngine::Precision::FP32),
@@ -34,20 +26,6 @@ namespace {
                                     ::testing::Values(InferenceEngine::Precision::FP32),
                                     ::testing::Values(CommonTestUtils::DEVICE_MULTI),
                                     ::testing::ValuesIn(Multiconfigs)),
-                            PerfCountersTest::getTestCaseName);
-
-    INSTANTIATE_TEST_CASE_P(smoke_Auto_BehaviorTests, PerfCountersTest,
-                            ::testing::Combine(
-                                    ::testing::Values(InferenceEngine::Precision::FP32),
-                                    ::testing::Values(CommonTestUtils::DEVICE_AUTO),
-                                    ::testing::ValuesIn(Autoconfigs)),
-                            PerfCountersTest::getTestCaseName);
-
-    INSTANTIATE_TEST_CASE_P(smoke_AutoCG_BehaviorTests, PerfCountersTest,
-                            ::testing::Combine(
-                                ::testing::Values(InferenceEngine::Precision::FP32),
-                                ::testing::Values(CommonTestUtils::DEVICE_AUTO),
-                                ::testing::ValuesIn(auto_cpu_gpu_conf)),
                             PerfCountersTest::getTestCaseName);
 
 }  // namespace
