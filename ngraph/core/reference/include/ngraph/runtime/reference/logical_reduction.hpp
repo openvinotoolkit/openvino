e35fd86c3b100c6d4bed7a7ad106db40926c687b
ggalieroc <gabriele.galiero.casay@intel.com>
2021-06-08 12:05:15 +0200

    Added constant expression to set keep_dims as false

--- ngraph/core/reference/include/ngraph/runtime/reference/logical_reduction.hpp
+++ ngraph/core/reference/include/ngraph/runtime/reference/logical_reduction.hpp
@@ -21,7 +21,8 @@ namespace ngraph
                                                   const Shape& in_shape,
                                                   const AxisSet& reduction_axes)
             {
-                auto out_shape = reduce(in_shape, reduction_axes, false);
+                constexpr bool dont_keep_dims_in_output = false;
+                auto out_shape = reduce(in_shape, reduction_axes, dont_keep_dims_in_output);
                 std::fill(out, out + shape_size(out_shape), 1);
 
                 const auto in_strides = row_major_strides(in_shape);
@@ -30,7 +31,8 @@ namespace ngraph
                 CoordinateTransformBasic input_transform(in_shape);
                 for (const Coordinate& input_coord : input_transform)
                 {
-                    Coordinate output_coord = reduce(input_coord, reduction_axes, false);
+                    Coordinate output_coord =
+                        reduce(input_coord, reduction_axes, dont_keep_dims_in_output);
 
                     size_t in_idx = std::inner_product(
                         input_coord.begin(), input_coord.end(), in_strides.begin(), 0);
