b4d1439ea5f0ce6878b016f266d48a895eed9201
Zhang Yi3 <yi3.zhang@intel.com>
2021-06-09 10:16:27 +0800

    init support for matrix_nms

--- ngraph/core/reference/src/runtime/reference/matrix_nms.cpp
+++ ngraph/core/reference/src/runtime/reference/matrix_nms.cpp
@@ -7,6 +7,7 @@
 #include <cmath>
 #include <queue>
 #include <vector>
+#include <numeric>
 #include "ngraph/runtime/reference/matrix_nms.hpp"
 #include "ngraph/shape.hpp"
 
@@ -39,26 +40,61 @@ namespace ngraph
                     float x2 = 0.0f;
                 };
 
-                static float intersectionOverUnion(const Rectangle& boxI, const Rectangle& boxJ)
-                {
-                    float areaI = (boxI.y2 - boxI.y1) * (boxI.x2 - boxI.x1);
-                    float areaJ = (boxJ.y2 - boxJ.y1) * (boxJ.x2 - boxJ.x1);
+                template<typename T, bool gaussian>
+                struct decay_score;
 
-                    if (areaI <= 0.0f || areaJ <= 0.0f)
-                    {
-                        return 0.0f;
+                template<typename T>
+                struct decay_score<T, true> {
+                    T operator()(T iou, T max_iou, T sigma) {
+                        return std::exp((max_iou * max_iou - iou * iou) * sigma);
                     }
+                };
 
-                    float intersection_ymin = std::max(boxI.y1, boxJ.y1);
-                    float intersection_xmin = std::max(boxI.x1, boxJ.x1);
-                    float intersection_ymax = std::min(boxI.y2, boxJ.y2);
-                    float intersection_xmax = std::min(boxI.x2, boxJ.x2);
+                template<typename T>
+                struct decay_score<T, false> {
+                    T operator()(T iou, T max_iou, T sigma) {
+                        return (1. - iou) / (1. - max_iou);
+                    }
+                };
 
-                    float intersection_area =
-                        std::max(intersection_ymax - intersection_ymin, 0.0f) *
-                        std::max(intersection_xmax - intersection_xmin, 0.0f);
+                template<class T>
+                static inline T BBoxArea(const T *box, const bool normalized) {
+                    if (box[2] < box[0] || box[3] < box[1]) {
+                        // If coordinate values are is invalid
+                        // (e.g. xmax < xmin or ymax < ymin), return 0.
+                        return static_cast<T>(0.);
+                    } else {
+                        const T w = box[2] - box[0];
+                        const T h = box[3] - box[1];
+                        if (normalized) {
+                            return w * h;
+                        } else {
+                            // If coordinate values are not within range [0, 1].
+                            return (w + 1) * (h + 1);
+                        }
+                    }
+                }
 
-                    return intersection_area / (areaI + areaJ - intersection_area);
+
+                template<class T>
+                static inline T intersectionOverUnion(const T *box1, const T *box2,
+                                               const bool normalized) {
+                    if (box2[0] > box1[2] || box2[2] < box1[0] || box2[1] > box1[3] ||
+                        box2[3] < box1[1]) {
+                        return static_cast<T>(0.);
+                    } else {
+                        const T inter_xmin = std::max(box1[0], box2[0]);
+                        const T inter_ymin = std::max(box1[1], box2[1]);
+                        const T inter_xmax = std::min(box1[2], box2[2]);
+                        const T inter_ymax = std::min(box1[3], box2[3]);
+                        T norm = normalized ? static_cast<T>(0.) : static_cast<T>(1.);
+                        T inter_w = inter_xmax - inter_xmin + norm;
+                        T inter_h = inter_ymax - inter_ymin + norm;
+                        const T inter_area = inter_w * inter_h;
+                        const T bbox1_area = BBoxArea<T>(box1, normalized);
+                        const T bbox2_area = BBoxArea<T>(box2, normalized);
+                        return inter_area / (bbox1_area + bbox2_area - inter_area);
+                    }
                 }
 
                 struct SelectedIndex
@@ -125,6 +161,82 @@ namespace ngraph
                     float score = 0.0f;
                 };
             } // namespace
+            }
+
+            template<typename T, bool gaussian>
+            void nms_matrix(const T* boxes_data,
+                           const Shape& boxes_data_shape,
+                           const T* scores_data,
+                           const Shape& scores_data_shape,
+                           const T score_threshold, const T post_threshold,
+                           const float sigma, const int64_t top_k, const bool normalized,
+                           std::vector<int> *selected_indices,
+                           std::vector<T> *decayed_scores) {
+                int64_t num_boxes = static_cast<int64_t>(boxes_data_shape[1]);
+                int64_t box_size = static_cast<int64_t>(boxes_data_shape[2]);
+
+                auto score_ptr = scores_data;
+                auto bbox_ptr = boxes_data;
+
+                std::vector<int32_t> perm(num_boxes);
+                std::iota(perm.begin(), perm.end(), 0);
+                auto end = std::remove_if(perm.begin(), perm.end(),
+                                          [&score_ptr, score_threshold](int32_t idx) {
+                                              return score_ptr[idx] <= score_threshold;
+                                          });
+
+                auto sort_fn = [&score_ptr](int32_t lhs, int32_t rhs) {
+                    return score_ptr[lhs] > score_ptr[rhs];
+                };
+
+                int64_t num_pre = std::distance(perm.begin(), end);
+                if (num_pre <= 0) {
+                    return;
+                }
+                if (top_k > -1 && num_pre > top_k) {
+                    num_pre = top_k;
+                }
+                std::partial_sort(perm.begin(), perm.begin() + num_pre, end, sort_fn);
+
+                std::vector<T> iou_matrix((num_pre * (num_pre - 1)) >> 1);
+                std::vector<T> iou_max(num_pre);
+
+                iou_max[0] = 0.;
+                for (int64_t i = 1; i < num_pre; i++) {
+                    T max_iou = 0.;
+                    auto idx_a = perm[i];
+                    for (int64_t j = 0; j < i; j++) {
+                        auto idx_b = perm[j];
+                        auto iou = intersectionOverUnion<T>(bbox_ptr + idx_a * box_size,
+                                                     bbox_ptr + idx_b * box_size, normalized);
+                        max_iou = std::max(max_iou, iou);
+                        iou_matrix[i * (i - 1) / 2 + j] = iou;
+                    }
+                    iou_max[i] = max_iou;
+                }
+
+                if (score_ptr[perm[0]] > post_threshold) {
+                    selected_indices->push_back(perm[0]);
+                    decayed_scores->push_back(score_ptr[perm[0]]);
+                }
+
+                decay_score<T, gaussian> decay_fn;
+                for (int64_t i = 1; i < num_pre; i++) {
+                    T min_decay = 1.;
+                    for (int64_t j = 0; j < i; j++) {
+                        auto max_iou = iou_max[j];
+                        auto iou = iou_matrix[i * (i - 1) / 2 + j];
+                        auto decay = decay_fn(iou, max_iou, sigma);
+                        min_decay = std::min(min_decay, decay);
+                    }
+                    auto ds = min_decay * score_ptr[perm[i]];
+                    if (ds <= post_threshold) continue;
+                    selected_indices->push_back(perm[i]);
+                    decayed_scores->push_back(ds);
+                }
+            }
+
+
             void matrix_nms(const float* boxes_data,
                             const Shape& boxes_data_shape,
                             const float* scores_data,
@@ -144,9 +256,101 @@ namespace ngraph
                             const Shape& selected_indices_shape,
                             int64_t* valid_outputs)
             {
-                BoxInfo info;
-                intersectionOverUnion(Rectangle{}, Rectangle{});
-                *valid_outputs = 0;
+                // boxes shape: {num_batches, num_boxes, 4}
+                // scores shape: {num_batches, num_classes, num_boxes}
+                int64_t num_batches = static_cast<int64_t>(scores_data_shape[0]);
+                int64_t num_classes = static_cast<int64_t>(scores_data_shape[1]);
+                int64_t num_boxes = static_cast<int64_t>(boxes_data_shape[1]);
+                int64_t box_shape = static_cast<int64_t>(boxes_data_shape[2]);
+                std::vector<float> detections;
+                std::vector<int> indices;
+                std::vector<int> num_per_batch;
+                detections.reserve(6 * num_batches * num_classes * num_boxes);
+
+                std::vector<BoxInfo> filteredBoxes;
+                int64_t background_label = 0;
+                bool use_gaussian = false;
+                int64_t nms_top_k = 200;
+                int64_t keep_top_k = 200;
+                bool normalized = true;
+                float post_threshold = 0.0;
+                float gaussian_sigma = 0.2;
+                for (int64_t batch = 0; batch < num_batches; batch++)
+                {
+                    const float* boxesPtr = boxes_data + batch * num_boxes * 4;
+                    std::vector<int> all_indices;
+                    std::vector<float> all_scores;
+                    std::vector<float> all_classes;
+                    size_t num_det = 0;
+
+                    for (int64_t class_idx = 0; class_idx < num_classes; class_idx++)
+                    {
+
+                        std::vector<BoxInfo> candidate_boxes;
+                        candidate_boxes.reserve(num_boxes);
+                        if (class_idx == background_label) continue;
+                        const float* scoresPtr =
+                                scores_data + batch * (num_classes * num_boxes) + class_idx * num_boxes;
+                        if (use_gaussian) {
+                            nms_matrix<float, true>(boxesPtr, boxes_data_shape, scoresPtr, scores_data_shape, score_threshold, post_threshold,
+                                               gaussian_sigma, nms_top_k, normalized, &all_indices,
+                                               &all_scores);
+                        } else {
+                            nms_matrix<float, false>(boxesPtr, boxes_data_shape, scoresPtr, scores_data_shape, score_threshold,
+                                    post_threshold, gaussian_sigma, nms_top_k, normalized, &all_indices, &all_scores);
+                        }
+                        for (size_t i = 0; i < all_indices.size() - num_det; i++) {
+                            all_classes.push_back(static_cast<float >(class_idx));
+                        }
+                        num_det = all_indices.size();
+                    }
+
+                    if (num_det <= 0) {
+                        break;
+                    }
+
+                    if (keep_top_k > -1) {
+                        auto k = static_cast<size_t>(keep_top_k);
+                        if (num_det > k) num_det = k;
+                    }
+
+                    std::vector<int32_t> perm(all_indices.size());
+                    std::iota(perm.begin(), perm.end(), 0);
+
+                    std::partial_sort(perm.begin(), perm.begin() + num_det, perm.end(),
+                                      [&all_scores](int lhs, int rhs) {
+                                          return all_scores[lhs] > all_scores[rhs];
+                                      });
+                    std::cout << "First Batch " << batch << num_classes << std::endl;
+                    for (size_t i = 0; i < num_det; i++) {
+                        auto p = perm[i];
+                        auto idx = all_indices[p];
+                        auto cls = all_classes[p];
+                        auto score = all_scores[p];
+                        auto bbox = boxesPtr + idx * box_shape;
+
+                        indices.push_back(batch * num_boxes + idx);
+                        detections.push_back(cls);
+                        detections.push_back(score);
+                        for (int j = 0; j < box_shape; j++) {
+                            detections.push_back(bbox[j]);
+                        }
+                    }
+                }
+                std::cout << "Matrix NMS Nums " << indices.size() << std::endl;
+                size_t valid_nums = std::min(shape_size(selected_indices_shape), indices.size());
+                std::copy(indices.begin(), indices.begin() + valid_nums, selected_indices);
+                std::copy(indices.begin(), indices.begin() + valid_nums, selected_indices);
+                *valid_outputs = valid_nums;
+
+                for(size_t i = 0; i < detections.size() && i < shape_size(selected_indices_shape); i+=6)
+                {
+                    selected_scores[i] = detections[i+1];
+                }
+
+                *valid_outputs = valid_nums;
+
+
             }
 
             void matrix_nms_postprocessing(const HostTensorVector& outputs,
--- ngraph/test/backend/matrix_nms.in.cpp
+++ ngraph/test/backend/matrix_nms.in.cpp
@@ -677,3 +677,108 @@ NGRAPH_TEST(${BACKEND_NAME}, matrix_nms_by_IOU_and_scores_without_constants)
     EXPECT_EQ(expected_valid_outputs, valid_outputs_value);
 }
 */
+
+NGRAPH_TEST(${BACKEND_NAME}, matrix_nms_reference)
+{
+
+    std::vector<float> boxes_data = {
+            0.2369726, 0.28203848, 0.51645845, 0.539889,
+            0.4985257, 0.49854738, 0.70052814, 0.5731168,
+            0.24534181, 0.20871444, 0.6978695, 0.9007864,
+            0.3640467, 0.18057947, 0.88013, 0.70254624,
+            0.44733843, 0.08509488, 0.9813876, 0.51019615,
+            0.0800431, 0.4405484, 0.8949218, 0.7538091,
+            0.39023185, 0.2947409, 0.5822403, 0.6811248,
+            0.43342504, 0.47589797, 0.8638232, 0.5663469,
+            0.09810594, 0.34307447, 0.58732116, 0.63525164,
+            0.02561388, 0.3969397, 0.836488, 0.6961541,
+            0.38046527, 0.0203846, 0.8423643, 0.70654124,
+            0.32604268, 0.48462036, 0.93912554, 0.76133525,
+            0.28386533, 0.07562917, 0.72319627, 0.71275604,
+            0.33422744, 0.16976027, 0.5971304, 0.897465,
+            0.4521741, 0.43520954, 0.8457792, 0.96897304,
+            0.41536444, 0.22515537, 0.5683615, 0.5188405,
+            0.23329352, 0.3111817, 0.9340813, 0.9539974,
+            0.39396682, 0.0682839, 0.9657403, 0.89008236,
+            0.39679193, 0.35343063, 0.6144743, 0.7443275,
+            0.11305821, 0.47204477, 0.76243985, 0.62834394
+    };
+
+    std::vector<float> scores_data = {
+            0.59946257, 0.6581636, 0.48800406, 0.44284198, 0.60442245, 0.48058632,
+            0.4957834, 0.38717481, 0.48625258, 0.4617983,
+            0.40053746, 0.34183636, 0.5119959, 0.55715805, 0.39557755, 0.5194137,
+            0.5042166, 0.61282516, 0.5137474, 0.5382017,
+            0.66382104, 0.56581205, 0.5002907, 0.5758071, 0.6055713, 0.4621192,
+            0.5324077, 0.5875278, 0.48963425, 0.4790761,
+            0.33617902, 0.43418795, 0.49970928, 0.42419288, 0.3944287, 0.53788084,
+            0.46759236, 0.41247222, 0.5103657, 0.5209239
+    };
+
+    std::vector<int64_t> max_output_boxes_per_class_data = {40};
+    std::vector<float> iou_threshold_data = {0.4f};
+    std::vector<float> score_threshold_data = {0.01f};
+    const auto box_encoding = op::v8::MatrixNms::BoxEncodingType::CORNER;
+    const auto boxes_shape = Shape{2, 10, 4};
+    const auto scores_shape = Shape{2, 2, 10};
+
+    const auto boxes = make_shared<op::Parameter>(element::f32, boxes_shape);
+    const auto scores = make_shared<op::Parameter>(element::f32, scores_shape);
+    const auto max_output_boxes_per_class = make_shared<op::Parameter>(element::i64, Shape{1});
+    const auto score_threshold = make_shared<op::Parameter>(element::f32, Shape{1});
+    const auto iou_threshold = make_shared<op::Parameter>(element::f32, Shape{1});
+    const auto soft_nms_sigma = make_shared<op::Parameter>(element::f32, Shape{1});
+
+    auto nms = make_shared<op::v8::MatrixNms>(boxes,
+                                              scores,
+                                              max_output_boxes_per_class,
+                                              iou_threshold,
+                                              score_threshold,
+                                              soft_nms_sigma,
+                                              box_encoding,
+                                              false);
+    auto f = make_shared<Function>(nms,
+                                   ParameterVector{boxes,
+                                                   scores,
+                                                   max_output_boxes_per_class,
+                                                   iou_threshold,
+                                                   score_threshold,
+                                                   soft_nms_sigma});
+
+    auto backend = runtime::Backend::create("${BACKEND_NAME}");
+
+    auto selected_indeces = backend->create_tensor(element::i64, Shape{20, 3});
+    auto selected_scores = backend->create_tensor(element::f32, Shape{20, 3});
+    auto valid_outputs = backend->create_tensor(element::i64, Shape{1});
+
+    auto backend_boxes = backend->create_tensor(element::f32, boxes_shape);
+    auto backend_scores = backend->create_tensor(element::f32, scores_shape);
+    auto backend_max_output_boxes_per_class = backend->create_tensor(element::i64, {1});
+    auto backend_iou_threshold = backend->create_tensor(element::f32, {1});
+    auto backend_score_threshold = backend->create_tensor(element::f32, {1});
+    auto backend_soft_nms_sigma = backend->create_tensor(element::f32, {1});
+    copy_data(backend_boxes, boxes_data);
+    copy_data(backend_scores, scores_data);
+    copy_data(backend_max_output_boxes_per_class, max_output_boxes_per_class_data);
+    copy_data(backend_iou_threshold, iou_threshold_data);
+    copy_data(backend_score_threshold, score_threshold_data);
+    copy_data(backend_soft_nms_sigma, std::vector<float>(0.0));
+
+    auto handle = backend->compile(f);
+
+    handle->call({selected_indeces, selected_scores, valid_outputs},
+                 {backend_boxes,
+                  backend_scores,
+                  backend_max_output_boxes_per_class,
+                  backend_iou_threshold,
+                  backend_score_threshold,
+                  backend_soft_nms_sigma});
+
+    auto selected_indeces_value = read_vector<int64_t>(selected_indeces);
+    auto selected_scores_value = read_vector<float>(selected_scores);
+    auto valid_outputs_value = read_vector<int64_t>(valid_outputs);
+
+    for( auto& i : selected_indeces_value)
+        std::cout << i << endl;
+
+}
