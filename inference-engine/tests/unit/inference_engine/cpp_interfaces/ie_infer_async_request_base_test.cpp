0442766e0849de336ae8b09220b5ea765d8ee8cb
Ilya Churaev <ilya.churaev@intel.com>
2021-06-09 18:07:22 +0300

    Fixed build

--- inference-engine/tests/unit/inference_engine/cpp_interfaces/ie_infer_async_request_base_test.cpp
+++ inference-engine/tests/unit/inference_engine/cpp_interfaces/ie_infer_async_request_base_test.cpp
@@ -311,7 +311,7 @@ TEST_F(InferRequestTests, throwsIfGetBlobReturnNotOK) {
 }
 
 // SetShape
-TEST_F(InferRequestTests, canForwardSetBlob) {
+TEST_F(InferRequestTests, canForwardSetShape) {
     SizeVector shape;
     std::string name = "blob1";
 
