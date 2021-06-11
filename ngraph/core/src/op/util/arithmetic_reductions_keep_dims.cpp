9756b95dcf86d16940ee4a886cabdcd6790c4593
Gabriele Galiero Casay <gabriele.galiero.casay@intel.com>
2021-06-11 09:23:08 +0200

    Revise reduction operations base classes (#5609)
    * Revise reduction operations base class
    
    * Fix visitor tests for logical reduction ops
    
    * Remove node validation checks that break backward compatibility with tests
      * Check for integer element type of axes input
      * Check for unique elements provided in axes input
    
    * Set mvn fusion test to have axes input with element type integral for ReduceMean nodes
    
    * [LPT] Revise reduction operations base classes support in LPT
    
    * Add node validation checks for integer element type of axes input in reduction operations
    
    * Add end of line to files
    
    * Implement output shape inference of reduction ops as member function
    
    * Add ReduceBase class to have a common method to infer output shape
    
    Co-authored-by: Edward Shogulin <edward.shogulin@intel.com>

--- ngraph/core/src/op/util/arithmetic_reductions_keep_dims.cpp
+++ ngraph/core/src/op/util/arithmetic_reductions_keep_dims.cpp
@@ -32,61 +32,28 @@ bool ngraph::op::util::ArithmeticReductionKeepDims::visit_attributes(AttributeVi
 void op::util::ArithmeticReductionKeepDims::validate_and_infer_types()
 {
     NGRAPH_OP_SCOPE(v0_util_ArithmeticReductionKeepDims_validate_and_infer_types);
-    if (m_keep_dims)
-    {
-        auto input_shape = get_input_partial_shape(0);
-        auto input_rank = input_shape.rank();
-        PartialShape result_shape{PartialShape::dynamic()};
 
-        if (input_rank.is_static())
-            result_shape = PartialShape::dynamic(input_rank);
+    const element::Type& data_et = get_input_element_type(0);
+    const PartialShape& axes_shape = get_input_partial_shape(1);
+    const element::Type& axes_et = get_input_element_type(1);
 
-        const auto& axes = get_constant_from_source(input_value(1));
-        if (input_rank.is_static() && axes)
-        {
-            AxisSet reduction_axes;
-            auto reduction_axes_val = axes->cast_vector<int64_t>();
-            for (auto axis : reduction_axes_val)
-            {
-                try
-                {
-                    axis = normalize_axis(this, axis, input_rank);
-                }
-                catch (const ngraph_error&)
-                {
-                    NODE_VALIDATION_CHECK(this,
-                                          false,
-                                          "Reduction axis (",
-                                          axis,
-                                          ") is out of bounds ",
-                                          "(argument shape: ",
-                                          input_shape,
-                                          ", reduction axes: ",
-                                          reduction_axes,
-                                          ")");
-                }
-                reduction_axes.insert(axis);
-            }
+    NODE_VALIDATION_CHECK(this,
+                          data_et.is_real() || data_et.is_integral_number(),
+                          "Element type of data input must be numeric. Got: ",
+                          data_et);
 
-            std::vector<Dimension> dims;
-            for (int64_t i = 0; i < input_rank.get_length(); i++)
-            {
-                if (reduction_axes.count(i) == 0)
-                {
-                    dims.push_back(input_shape[i]);
-                }
-                else
-                {
-                    dims.emplace_back(Dimension{1});
-                }
-            }
-            result_shape = PartialShape(dims);
-        }
-        set_input_is_relevant_to_shape(1);
-        set_output_type(0, get_input_element_type(0), result_shape);
-    }
-    else
-    {
-        ArithmeticReduction::validate_and_infer_types();
-    }
+    NODE_VALIDATION_CHECK(this,
+                          axes_et.is_integral_number(),
+                          "Element type of axes input must be integer. Got: ",
+                          axes_et);
+
+    const Rank axes_rank = axes_shape.rank();
+    NODE_VALIDATION_CHECK(this,
+                          axes_rank.compatible(0) || axes_rank.compatible(1),
+                          "Axes input must be a scalar or 1D input. Got: ",
+                          axes_shape);
+
+    PartialShape result_shape = infer_reduction_output_shape(m_keep_dims);
+    set_input_is_relevant_to_shape(1);
+    set_output_type(0, data_et, result_shape);
 }
