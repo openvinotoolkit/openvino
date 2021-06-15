2de0cd3909610e75d9c52ba18eeb7d03bf0bf6bd
ggalieroc <gabriele.galiero.casay@intel.com>
2021-06-15 16:32:14 +0200

    Add const qualifier to local variables

--- ngraph/core/reference/include/ngraph/runtime/reference/mean.hpp
+++ ngraph/core/reference/include/ngraph/runtime/reference/mean.hpp
@@ -25,7 +25,7 @@ namespace ngraph
             void mean(const T* arg, T* out, const Shape& in_shape, const AxisSet& reduction_axes)
             {
                 constexpr bool dont_keep_dims_in_output = false;
-                auto out_shape = reduce(in_shape, reduction_axes, dont_keep_dims_in_output);
+                const auto out_shape = reduce(in_shape, reduction_axes, dont_keep_dims_in_output);
                 std::vector<T> cs(shape_size(out_shape), 0);
                 std::fill(out, out + shape_size(out_shape), 0);
 
@@ -37,12 +37,12 @@ namespace ngraph
 
                 for (const Coordinate& input_coord : input_transform)
                 {
-                    Coordinate output_coord =
+                    const Coordinate output_coord =
                         reduce(input_coord, reduction_axes, dont_keep_dims_in_output);
 
-                    size_t in_idx = std::inner_product(
+                    const size_t in_idx = std::inner_product(
                         input_coord.begin(), input_coord.end(), in_strides.begin(), 0);
-                    size_t out_idx = std::inner_product(
+                    const size_t out_idx = std::inner_product(
                         output_coord.begin(), output_coord.end(), out_strides.begin(), 0);
 
                     T x = arg[in_idx];
