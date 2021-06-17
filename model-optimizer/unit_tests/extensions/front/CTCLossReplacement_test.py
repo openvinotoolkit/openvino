ff1b43964323ca043bf78b5f9bd273b4cc3acb50
VGavrilov <vladimir.gavrilov@intel.com>
2021-06-17 11:25:11 +0300

    Written reading the operation CTCGreedyDecoder for ONNX (as a custom operation).

--- model-optimizer/unit_tests/extensions/front/CTCLossReplacement_test.py
+++ model-optimizer/unit_tests/extensions/front/CTCLossReplacement_test.py
@@ -5,7 +5,7 @@ import numpy as np
 import unittest
 from argparse import Namespace
 
-from extensions.front.tf.CTCLossReplacement import CTCLossReplacement
+from extensions.front.CTCLossReplacement import CTCLossReplacement
 from mo.front.common.partial_infer.utils import int64_array
 from mo.utils.ir_engine.compare_graphs import compare_graphs
 from unit_tests.utils.graph import build_graph, const
