497827d2ad80de59cf27f3960ec5b7856a67a2f6
ggalieroc <gabriele.galiero.casay@intel.com>
2021-06-21 13:13:24 +0200

    Refactored visitor unit test

--- ngraph/test/visitors/op/negative.cpp
+++ ngraph/test/visitors/op/negative.cpp
@@ -1,26 +1,13 @@
-// Copyright (C) 2018-2021 Intel Corporation
+// Copyright (C) 2021 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "gtest/gtest.h"
+#include "unary_ops.hpp"
 
-#include "ngraph/ngraph.hpp"
-#include "ngraph/op/util/attr_types.hpp"
-#include "ngraph/opsets/opset1.hpp"
-#include "util/visitor.hpp"
+using Types = ::testing::Types<UnaryOperatorType<ngraph::op::v0::Negative, element::f32>,
+                               UnaryOperatorType<ngraph::op::v0::Negative, element::i32>>;
 
-using namespace std;
-using namespace ngraph;
-using ngraph::test::NodeBuilder;
-
-TEST(attributes, negative_op)
-{
-    NodeBuilder::get_ops().register_factory<opset1::Negative>();
-    const auto A = make_shared<op::Parameter>(element::f32, Shape{5, 2});
-
-    const auto negative = make_shared<opset1::Negative>(A);
-    NodeBuilder builder(negative);
-
-    const auto expected_attr_count = 0;
-    EXPECT_EQ(builder.get_value_map_size(), expected_attr_count);
-}
+INSTANTIATE_TYPED_TEST_CASE_P(visitor_without_attribute,
+                              UnaryOperatorVisitor,
+                              Types,
+                              UnaryOperatorTypeName);
