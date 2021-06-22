d86d01284cca469be44d41518dfc96b84c935f70
Evgeny Lazarev <evgeny.lazarev@intel.com>
2021-06-22 12:50:11 +0300

    Fixed TF Rsqrt layer tests

--- tests/layer_tests/tensorflow_tests/test_tf_Rsqrt.py
+++ tests/layer_tests/tensorflow_tests/test_tf_Rsqrt.py
@@ -8,6 +8,8 @@ from common.tf_layer_test_class import CommonTFLayerTest
 
 
 class TestRsqrt(CommonTFLayerTest):
+    disable_input_layout_conversion = True
+
     def _prepare_input(self, inputs_dict):
         for input in inputs_dict.keys():
             inputs_dict[input] = np.random.randint(1, 256, inputs_dict[input]).astype(np.float32)
@@ -20,39 +22,24 @@ class TestRsqrt(CommonTFLayerTest):
             Input->Rsqrt       =>       Input->Power
 
         """
-
-        #
-        #   Create Tensorflow model
-        #
-
         import tensorflow as tf
 
         tf.compat.v1.reset_default_graph()
 
         # Create the graph and model
         with tf.compat.v1.Session() as sess:
-            shapes = shape.copy()
-            # reshaping
-            if len(shapes) >= 3:
-                shapes.append(shapes.pop(1))
-            input = tf.compat.v1.placeholder(tf.float32, shapes, 'Input')
+            input = tf.compat.v1.placeholder(tf.float32, shape, 'Input')
 
             tf.math.rsqrt(input, name='Operation')
 
             tf.compat.v1.global_variables_initializer()
             tf_net = sess.graph_def
 
-        #
-        #   Create reference IR net
-        #   Please, specify 'type': 'Input' for input node
-        #   Moreover, do not forget to validate ALL layer attributes!!!
-        #
-
         ref_net = None
 
         return tf_net, ref_net
 
-    test_data_precommit = [dict(shape=[1, 3, 50, 100, 224])]
+    test_data_precommit = [dict(shape=[1, 3, 5, 7, 9])]
 
     @pytest.mark.parametrize("params", test_data_precommit)
     @pytest.mark.precommit
@@ -61,10 +48,10 @@ class TestRsqrt(CommonTFLayerTest):
                    ie_device, precision, ir_version, temp_dir=temp_dir)
 
     test_data = [dict(shape=[1]),
-                 dict(shape=[1, 224]),
-                 pytest.param(dict(shape=[1, 3, 224]), marks=pytest.mark.xfail(reason="*-19053")),
-                 dict(shape=[1, 3, 100, 224]),
-                 dict(shape=[1, 3, 50, 100, 224])]
+                 dict(shape=[1, 3]),
+                 dict(shape=[1, 3, 5]),
+                 dict(shape=[1, 3, 5, 7]),
+                 dict(shape=[1, 3, 5, 7, 9])]
 
     @pytest.mark.parametrize("params", test_data)
     @pytest.mark.nightly
