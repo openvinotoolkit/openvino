bbfa8ff34a583dad748768fb1864789f6b07cdc5
Tomasz Socha <tomasz.socha@intel.com>
2021-06-22 15:16:41 +0200

    [nGraph] 🦒 Move onnx library to submodule

--- ngraph/cmake/external_onnx.cmake
+++ ngraph/cmake/external_onnx.cmake
@@ -2,31 +2,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-include(FetchContent)
-
-#------------------------------------------------------------------------------
-# ONNX.proto definition version
-#------------------------------------------------------------------------------
-
-set(ONNX_VERSION 1.8.1)
-
 #------------------------------------------------------------------------------
 # Download and install libonnx ...
 #------------------------------------------------------------------------------
 
-set(ONNX_GIT_REPO_URL https://github.com/onnx/onnx.git)
-set(ONNX_GIT_BRANCH rel-${ONNX_VERSION})
 set(NGRAPH_ONNX_NAMESPACE ngraph_onnx)
-set(ONNX_PATCH_FILE "${CMAKE_CURRENT_SOURCE_DIR}/cmake/patches/onnx_patch.diff")
-
-FetchContent_Declare(
-    ext_onnx
-    GIT_REPOSITORY ${ONNX_GIT_REPO_URL}
-    GIT_TAG ${ONNX_GIT_BRANCH}
-    GIT_SHALLOW TRUE
-    # apply patch to fix problems with symbols visibility for MSVC
-    PATCH_COMMAND git reset --hard HEAD && git apply --ignore-space-change --ignore-whitespace --verbose ${ONNX_PATCH_FILE}
-)
 
 macro(onnx_set_target_properties)
     target_include_directories(onnx SYSTEM PRIVATE "${Protobuf_INCLUDE_DIRS}")
@@ -57,19 +37,13 @@ macro(onnx_set_target_properties)
     endif()
 endmacro()
 
-FetchContent_GetProperties(ext_onnx)
-if(NOT ext_onnx_POPULATED)
-    FetchContent_Populate(ext_onnx)
-    set(ONNX_USE_PROTOBUF_SHARED_LIBS ${BUILD_SHARED_LIBS} CACHE BOOL "Use dynamic protobuf by ONNX library")
-    set(ONNX_NAMESPACE ${NGRAPH_ONNX_NAMESPACE})
-    set(ONNX_USE_LITE_PROTO ${NGRAPH_USE_PROTOBUF_LITE} CACHE BOOL "Use protobuf lite for ONNX library")
-    set(ONNX_ML ON CACHE BOOL "Use ONNX ML")
-    if(CMAKE_CROSSCOMPILING)
-        set(ONNX_CUSTOM_PROTOC_EXECUTABLE ${SYSTEM_PROTOC})
-    endif()
-
-    add_subdirectory(${ext_onnx_SOURCE_DIR} ${ext_onnx_BINARY_DIR} EXCLUDE_FROM_ALL)
-    onnx_set_target_properties()
-else()
-    onnx_set_target_properties()
+set(ONNX_USE_PROTOBUF_SHARED_LIBS ${BUILD_SHARED_LIBS} CACHE BOOL "Use dynamic protobuf by ONNX library")
+set(ONNX_NAMESPACE ${NGRAPH_ONNX_NAMESPACE})
+set(ONNX_USE_LITE_PROTO ${NGRAPH_USE_PROTOBUF_LITE} CACHE BOOL "Use protobuf lite for ONNX library")
+set(ONNX_ML ON CACHE BOOL "Use ONNX ML")
+if(CMAKE_CROSSCOMPILING)
+    set(ONNX_CUSTOM_PROTOC_EXECUTABLE ${SYSTEM_PROTOC})
 endif()
+
+add_subdirectory(${CMAKE_SOURCE_DIR}/thirdparty/onnx ${CMAKE_BINARY_DIR}/_deps/onnx EXCLUDE_FROM_ALL)
+onnx_set_target_properties()
