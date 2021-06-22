6cf87960b723a60b6287f0c3ec3eec7eb0f3f4b9
Evgeny Lazarev <evgeny.lazarev@intel.com>
2021-06-22 10:34:57 +0300

    Fixed TF Add layer tests

--- tests/layer_tests/common/tf_layer_test_class.py
+++ tests/layer_tests/common/tf_layer_test_class.py
@@ -17,6 +17,8 @@ def save_to_pb(tf_model, path_to_saved_tf_model):
 
 
 class CommonTFLayerTest(CommonLayerTest):
+    disable_input_layout_conversion = False
+
     def produce_model_path(self, framework_model, save_path):
         return save_to_pb(framework_model, save_path)
 
@@ -41,9 +43,9 @@ class CommonTFLayerTest(CommonLayerTest):
                 input = dict()
                 for key in inputs_dict.keys():
                     data = inputs_dict.get(key)
-                    if len(data.shape) == 4:        # reshaping for 4D tensors
+                    if not self.disable_input_layout_conversion and len(data.shape) == 4:
                         input[key+':0'] = data.transpose(0, 2, 3, 1)
-                    elif len(data.shape) == 5:      # reshaping for 5D tensors
+                    elif not self.disable_input_layout_conversion and len(data.shape) == 5:
                         input[key+':0'] = data.transpose(0, 2, 3, 4, 1)
                     else:
                         input[key+':0'] = data
@@ -52,10 +54,10 @@ class CommonTFLayerTest(CommonLayerTest):
                 result = dict()
                 for i, output in enumerate(outputs_list):
                     _tf_res = tf_res[i]
-                    if len(_tf_res.shape) == 4:      # reshaping for 4D tensors
-                        result[output] = _tf_res.transpose(0, 3, 1, 2) # 2, 0, 1
-                    elif len(_tf_res.shape) == 5:    # reshaping for 5D tensors
-                        result[output] = _tf_res.transpose(0, 4, 1, 2, 3)  # 3, 0, 1, 2
+                    if not self.disable_input_layout_conversion and len(_tf_res.shape) == 4:
+                        result[output] = _tf_res.transpose(0, 3, 1, 2)
+                    elif not self.disable_input_layout_conversion and len(_tf_res.shape) == 5:
+                        result[output] = _tf_res.transpose(0, 4, 1, 2, 3)
                     else:
                         result[output] = _tf_res
                 return result
