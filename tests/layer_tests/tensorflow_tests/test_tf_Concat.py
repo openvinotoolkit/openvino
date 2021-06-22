f9641d69d22b79978177a9ac97d74fa58aa06ac5
Evgeny Lazarev <evgeny.lazarev@intel.com>
2021-06-22 11:49:56 +0300

    Fixed TF Concat layer tests

--- tests/layer_tests/tensorflow_tests/test_tf_Concat.py
+++ tests/layer_tests/tensorflow_tests/test_tf_Concat.py
@@ -7,6 +7,8 @@ from common.tf_layer_test_class import CommonTFLayerTest
 
 
 class TestConcat(CommonTFLayerTest):
+    disable_input_layout_conversion = True
+
     def create_concat_net(self, shape, axis, ir_version):
         """
             Tensorflow net               IR net
@@ -14,49 +16,20 @@ class TestConcat(CommonTFLayerTest):
             Input->Concat        =>      Input->Concat
 
         """
-
-        #
-        #   Create Tensorflow model
-        #
-
         import tensorflow as tf
 
         tf.compat.v1.reset_default_graph()
 
         # Create the graph and model
         with tf.compat.v1.Session() as sess:
-
             ax = axis
+            x = tf.compat.v1.placeholder(tf.float32, shape, 'Input')
+            y = tf.compat.v1.placeholder(tf.float32, shape, 'Input')  # Input_1 in graph_def
 
-            input_shape_x = shape.copy()
-            # reshaping
-            if len(input_shape_x) >= 3:
-                input_shape_x.append(input_shape_x.pop(1))
-
-            # TODO: add concat with const inputs to check fusing (as in ONNX)
-
-            x = tf.compat.v1.placeholder(tf.float32, input_shape_x, 'Input')
-            y = tf.compat.v1.placeholder(tf.float32, input_shape_x, 'Input')  # Input_1 in graph_def
-
-            concat = tf.concat([x, y], axis=ax, name='Operation')
-            concat_shape = concat.shape.as_list()
+            tf.concat([x, y], axis=ax, name='Operation')
 
             tf.compat.v1.global_variables_initializer()
             tf_net = sess.graph_def
-
-        #
-        #   Create reference IR net
-        #   Please, specify 'type': 'Input' for input node
-        #   Moreover, do not forget to validate ALL layer attributes!!!
-        #
-
-        # Format axis to positive value
-        concat_ax = axis if axis >= 0 else axis + len(shape)
-        if len(shape) >= 3:
-            # Permute shape to (N,C,...) format and compute correct axis value
-            order = [0, len(concat_shape) - 1] + list(range(1, len(concat_shape) - 1))
-            concat_shape = [concat_shape[i] for i in order]
-            concat_ax = order.index(concat_ax)
 
         ref_net = None
 
@@ -73,8 +46,8 @@ class TestConcat(CommonTFLayerTest):
         self._test(*self.create_concat_net(**params, ir_version=ir_version),
                    ie_device, precision, ir_version, temp_dir=temp_dir)
 
-    test_data_2D = [dict(shape=[1, 224], axis=0),
-                    dict(shape=[1, 224], axis=-1)]
+    test_data_2D = [dict(shape=[1, 3], axis=0),
+                    dict(shape=[1, 3], axis=-1)]
 
     @pytest.mark.parametrize("params", test_data_2D)
     @pytest.mark.nightly
@@ -82,9 +55,10 @@ class TestConcat(CommonTFLayerTest):
         self._test(*self.create_concat_net(**params, ir_version=ir_version),
                    ie_device, precision, ir_version, temp_dir=temp_dir)
 
-    test_data_3D = [pytest.param(dict(shape=[1, 3, 224], axis=0), marks=pytest.mark.xfail(reason="*-19053")),
-                    pytest.param(dict(shape=[1, 3, 224], axis=-1), marks=pytest.mark.xfail(reason="*-19053")),
-                    pytest.param(dict(shape=[1, 3, 224], axis=2), marks=pytest.mark.xfail(reason="*-19053"))]
+    test_data_3D = [dict(shape=[1, 3, 5], axis=0),
+                    dict(shape=[1, 3, 5], axis=-1),
+                    dict(shape=[1, 3, 5], axis=2),
+    ]
 
     @pytest.mark.parametrize("params", test_data_3D)
     @pytest.mark.nightly
@@ -92,9 +66,9 @@ class TestConcat(CommonTFLayerTest):
         self._test(*self.create_concat_net(**params, ir_version=ir_version),
                    ie_device, precision, ir_version, temp_dir=temp_dir)
 
-    test_data_4D = [dict(shape=[1, 3, 100, 224], axis=0),
-                    dict(shape=[1, 3, 100, 224], axis=-1),
-                    dict(shape=[1, 3, 100, 224], axis=2)]
+    test_data_4D = [dict(shape=[1, 3, 5, 7], axis=0),
+                    dict(shape=[1, 3, 5, 7], axis=-1),
+                    dict(shape=[1, 3, 5, 7], axis=2)]
 
     @pytest.mark.parametrize("params", test_data_4D)
     @pytest.mark.nightly
@@ -103,9 +77,9 @@ class TestConcat(CommonTFLayerTest):
         self._test(*self.create_concat_net(**params, ir_version=ir_version),
                    ie_device, precision, ir_version, temp_dir=temp_dir)
 
-    test_data_5D = [dict(shape=[1, 3, 50, 100, 224], axis=0),
-                    dict(shape=[1, 3, 50, 100, 224], axis=-1),
-                    dict(shape=[1, 3, 50, 100, 224], axis=2)]
+    test_data_5D = [dict(shape=[1, 3, 5, 7, 9], axis=0),
+                    dict(shape=[1, 3, 5, 7, 9], axis=-1),
+                    dict(shape=[1, 3, 5, 7, 9], axis=2)]
 
     @pytest.mark.parametrize("params", test_data_5D)
     @pytest.mark.nightly
