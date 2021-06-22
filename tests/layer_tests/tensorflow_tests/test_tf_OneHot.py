dfdd110d09a35617f130bfb6729ac2d83dc75a1f
Evgeny Lazarev <evgeny.lazarev@intel.com>
2021-06-22 12:45:54 +0300

    Fixed TF OneHot layer tests

--- tests/layer_tests/tensorflow_tests/test_tf_OneHot.py
+++ tests/layer_tests/tensorflow_tests/test_tf_OneHot.py
@@ -1,13 +1,14 @@
 # Copyright (C) 2018-2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-import numpy as np
 import pytest
+
 from common.tf_layer_test_class import CommonTFLayerTest
-from tensorflow_tests.permutation_utils import permute_nchw_to_nhwc
 
 
 class TestOneHot(CommonTFLayerTest):
+    disable_input_layout_conversion = True
+
     @staticmethod
     def create_one_hot_net(shape, depth, on_value, off_value, axis, ir_version):
         """
@@ -24,41 +25,23 @@ class TestOneHot(CommonTFLayerTest):
             Input (> 3D)  -> Permute (NCHW -> NHWC) -> OneHot ->   Permute (NHWC -> NCHW)
         """
 
-        #
-        #   Create Tensorflow model
-        #
-
         import tensorflow as tf
 
         tf.compat.v1.reset_default_graph()
 
         # Create the graph and model
         with tf.compat.v1.Session() as sess:
-            # Permute NCHW -> NHWC for TF network creation
-            net_shape = permute_nchw_to_nhwc(shape)
-
-            indices = tf.compat.v1.placeholder(tf.int32, shape=net_shape, name='input_indices')
+            indices = tf.compat.v1.placeholder(tf.int32, shape=shape, name='input_indices')
 
             result = tf.one_hot(indices,
-                                 depth,
-                                 on_value,
-                                 off_value,
-                                 axis,
-                                 name='Operation')
+                                depth,
+                                on_value,
+                                off_value,
+                                axis,
+                                name='Operation')
 
             tf.compat.v1.global_variables_initializer()
             tf_net = sess.graph_def
-
-        #
-        #   Create reference IR net
-        #
-
-        if on_value is None:
-            on_value = 1.0
-        if off_value is None:
-            off_value = 0.0
-
-        axis = len(shape) if axis is None else axis
 
         ref_net = None
 
@@ -147,8 +130,6 @@ class TestOneHot(CommonTFLayerTest):
         # check for default on/off value, axis params
         dict(shape=[4, 5, 6, 7, 8], depth=9, on_value=None, off_value=None, axis=0),
         dict(shape=[4, 5, 6, 7, 8], depth=9, on_value=None, off_value=None, axis=1),
-        dict(shape=[4, 5, 6, 7, 8], depth=9, on_value=None, off_value=None, axis=2),
-        dict(shape=[4, 5, 6, 7, 8], depth=9, on_value=None, off_value=None, axis=3),
         dict(shape=[4, 5, 6, 7, 8], depth=9, on_value=None, off_value=None, axis=4),
     ]
 
