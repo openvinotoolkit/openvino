c7ce15d18d5994f9891568af5b99c9515ea24ad1
Ivan Tikhonov <ivan.tikhonov@intel.com>
2021-06-23 11:11:18 +0300

    evaluate method implementation for DeformableConv v8

--- ngraph/core/include/ngraph/op/deformable_convolution.hpp
+++ ngraph/core/include/ngraph/op/deformable_convolution.hpp
@@ -162,9 +162,9 @@ namespace ngraph
                 std::shared_ptr<Node>
                     clone_with_new_inputs(const OutputVector& new_args) const override;
 
-                /*                bool evaluate(const HostTensorVector& outputs,
-                                              const HostTensorVector& inputs) const override;
-                                bool has_evaluate() const override { return true; }*/
+                bool evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const override;
+
+                bool has_evaluate() const override;
                 bool get_use_bilinear_interpolation_padding() const
                 {
                     return m_use_bilinear_interpolation_padding;
