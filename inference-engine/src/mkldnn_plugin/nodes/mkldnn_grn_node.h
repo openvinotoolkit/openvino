af525153d2e8400a64e362f97a97a7f10f09a076
Egor Shulman <egor.shulman@intel.com>
2021-06-23 14:22:10 +0300

    [IE CPU] Reimplement extension nodes via MKLDNNNode API (#5784)

--- inference-engine/src/mkldnn_plugin/nodes/mkldnn_grn_node.h
+++ inference-engine/src/mkldnn_plugin/nodes/mkldnn_grn_node.h
@@ -9,9 +9,9 @@
 
 namespace MKLDNNPlugin {
 
-class MKLDNNMathNode : public MKLDNNNode {
+class MKLDNNGRNNode : public MKLDNNNode {
 public:
-    MKLDNNMathNode(const std::shared_ptr<ngraph::Node>& op, const mkldnn::engine& eng, MKLDNNWeightsSharing::Ptr &cache);
+    MKLDNNGRNNode(const std::shared_ptr<ngraph::Node>& op, const mkldnn::engine& eng, MKLDNNWeightsSharing::Ptr &cache);
 
     void getSupportedDescriptors() override {};
     void initSupportedPrimitiveDescriptors() override;
@@ -22,11 +22,7 @@ public:
     static bool isSupportedOperation(const std::shared_ptr<ngraph::Node>& op, std::string& errorMessage) noexcept;
 
 private:
-    static std::map<const ngraph::DiscreteTypeInfo, std::function<void(const std::shared_ptr<ngraph::Node>&, MKLDNNMathNode& node)>> initializers;
-
-    float alpha = 0.0f;
-    float beta = 0.0f;
-    float gamma = 0.0f;
+    float bias = 1.0f;
 
     std::string errorPrefix;
 };
