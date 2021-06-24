918c58fd5bc3f7637e4be2e91cfb6ed9335b2f83
Mingyu Kim <mingyu.kim@intel.com>
2021-06-24 13:21:27 +0900

    [CLDNN] Initial implementation of debug_configuration (#6012)

--- inference-engine/src/cldnn_engine/cldnn_program.cpp
+++ inference-engine/src/cldnn_engine/cldnn_program.cpp
@@ -6,6 +6,7 @@
 #include "ngraph/ops.hpp"
 #include "ngraph_ops/nms_ie_internal.hpp"
 #include "cldnn_itt.h"
+#include "cldnn/runtime/debug_configuration.hpp"
 
 using namespace InferenceEngine;
 using namespace InferenceEngine::details;
@@ -177,9 +178,16 @@ std::shared_ptr<cldnn::program> Program::BuildProgram(const std::vector<std::sha
                                                       bool createTopologyOnly) {
     OV_ITT_SCOPED_TASK(itt::domains::CLDNNPlugin, "Program::BuildProgram");
     cldnn::build_options options;
+    GPU_DEBUG_GET_INSTANCE(debug_config);
+
     if (!m_config.graph_dumps_dir.empty()) {
         options.set_option(cldnn::build_option::graph_dumps_dir(m_config.graph_dumps_dir));
     }
+
+    GPU_DEBUG_IF(!debug_config->dump_graphs.empty()) {
+        options.set_option(cldnn::build_option::graph_dumps_dir(debug_config->dump_graphs));
+    }
+
     options.set_option(cldnn::build_option::optimize_data(true));
     options.set_option(cldnn::build_option::tuning_config(m_config.tuningConfig));
 
