a59791cb87b5977a48f3166a4e756589282e3d91
Patryk Elszkowski <patryk.elszkowski@intel.com>
2021-06-25 14:37:15 +0200

    Cleanup reference implementation

--- ngraph/core/reference/src/runtime/reference/space_to_depth.cpp
+++ ngraph/core/reference/src/runtime/reference/space_to_depth.cpp
@@ -3,15 +3,13 @@
 //
 
 #include "ngraph/runtime/reference/space_to_depth.hpp"
-#include <cmath>
-#include <numeric>
+#include <vector>
 #include "ngraph/check.hpp"
 #include "ngraph/runtime/opt_kernel/reshape.hpp"
 
 namespace ngraph
 {
     namespace runtime
-
     {
         namespace reference
         {
@@ -23,6 +21,16 @@ namespace ngraph
                                 const op::SpaceToDepth::SpaceToDepthMode mode,
                                 const size_t elem_size)
             {
+                // SpaceToDepth run in tree steps:
+                // - disperse data from depth channel
+                // - rearrange data so as appropriate chunks of data where close to their
+                //   destination place
+                // - squeeze data from respective dimensions
+                //
+                // First and third step doesn't change input data in memory, it change only the
+                // shape of input. From data layout perspective firs and third step may be
+                // omit. The second operation have to be perform on input data with dispared
+                // shape (x').
                 const size_t n_dim = in_shape.at(0);
                 const size_t c_dim = in_shape.at(1);
                 const size_t spatial_dim_index = 2;
@@ -31,25 +39,20 @@ namespace ngraph
                 for (size_t i = spatial_dim_index; i < in_shape.size(); ++i)
                 {
                     NGRAPH_CHECK(block_size > 0 && in_shape.at(i) % block_size == 0,
-                                 "The dimension on position: ",
+                                 "SpaceToDepth: The dimension on position: ",
                                  i,
                                  " equal to: ",
                                  in_shape.at(i),
-                                 " must be a multiple of m_blocksize: ",
+                                 " must be a multiple of blocksize: ",
                                  block_size);
                 }
 
-                // First we have to disperse the data from spatial dimensions, then
-                // rearrange them so as appropriate chunks of data where close to their
-                // destination place. Finally squeeze data from respective dimensions.
                 Shape dispersed_shape{n_dim, c_dim};
                 for (size_t i = 0; i < spatial_dims; ++i)
                 {
                     dispersed_shape.push_back(in_shape.at(i + spatial_dim_index) / block_size);
                     dispersed_shape.push_back(block_size);
                 }
-                std::vector<size_t> plain_axes_order(in_shape.size());
-                std::iota(plain_axes_order.begin(), plain_axes_order.end(), 0);
 
                 // calculate axes to transpose
                 // [0, 3, 5, ..., spatial_dims + (spatial_dims + 1), 2, 4, ..., K + K])
@@ -66,29 +69,26 @@ namespace ngraph
                 switch (mode)
                 {
                 // x' = reshape(data, [N, C, D1/block_size, block_size, D2/block_size, block_size,
-                // ..., DK/block_size, block_size]) x'' = transpose(x', [0,  1, 3, 5, ..., K + (K +
-                // 1),  2, 4, ..., K + K]) y = reshape(x'', [N, C * (block_size ^ K), D1 /
-                // block_size, D2 / block_size, ..., DK
-                // /
-                // block_size])
+                //              ..., DK/block_size, block_size])
+                // x'' = transpose(x', [0,  1, 3, 5, ..., K + (K + 1),  2, 4, ..., K + K])
+                // y = reshape(x'', [N, C * (block_size ^ K), D1 / block_size, D2 / block_size, ...,
+                //             DK / block_size])
                 case op::SpaceToDepth::SpaceToDepthMode::DEPTH_FIRST:
                 {
                     axes_order.insert(axes_order.begin() + 1, 1);
                     break;
                 }
                 // x' = reshape(data, [N, C, D1/block_size, block_size, D2/block_size, block_size,
-                // ... , DK/block_size, block_size]) x'' = transpose(x',  [0,  3, 5, ..., K + (K +
-                // 1), 1,  2, 4, ..., K + K]) y = reshape(x'', [N, C * (block_size ^ K), D1 /
-                // block_size, D2 / block_size, ..., DK
-                // /
-                // block_size])
+                //              ... , DK/block_size, block_size])
+                // x'' = transpose(x',  [0,  3, 5, ..., K + (K + 1), 1,  2, 4, ..., K + K])
+                // y = reshape(x'', [N, C * (block_size ^ K), D1 / block_size, D2 / block_size, ...,
+                //             DK / block_size])
                 case op::SpaceToDepth::SpaceToDepthMode::BLOCKS_FIRST:
-                default:
                 {
                     axes_order.insert(axes_order.begin() + spatial_dims + 1, 1);
                 }
                 }
-                std::vector<char> transposed_data(shape_size(in_shape) * elem_size);
+
                 Shape post_transpose_shape(axes_order.size());
                 for (size_t axis_idx = 0; axis_idx < axes_order.size(); ++axis_idx)
                 {
