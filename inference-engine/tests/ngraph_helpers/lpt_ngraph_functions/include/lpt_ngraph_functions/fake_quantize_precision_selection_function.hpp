f78497c75342e8d3363982f1dff19559185aabbc
Vladislav Golubev <vladislav.golubev@intel.com>
2021-06-28 14:00:15 +0300

    [LPT] Plugin tests: added the ability to add test-cases with dynamic shapes

--- inference-engine/tests/ngraph_helpers/lpt_ngraph_functions/include/lpt_ngraph_functions/fake_quantize_precision_selection_function.hpp
+++ inference-engine/tests/ngraph_helpers/lpt_ngraph_functions/include/lpt_ngraph_functions/fake_quantize_precision_selection_function.hpp
@@ -35,7 +35,7 @@ public:
 
     static std::shared_ptr<ngraph::Function> getOriginal(
         const ngraph::element::Type precision,
-        const ngraph::Shape& inputShape,
+        const ngraph::PartialShape& inputShape,
         const ActualValues& values);
 
     static std::shared_ptr<ngraph::Function> getReference(
