c0202d5fa24745866dad70fb96a128ad9bd253ff
Maksim Kutakov <maksim.kutakov@intel.com>
2021-06-30 15:10:12 +0300

    MemDesc support in reference node

--- inference-engine/src/mkldnn_plugin/nodes/mkldnn_reference_node.cpp
+++ inference-engine/src/mkldnn_plugin/nodes/mkldnn_reference_node.cpp
@@ -6,6 +6,7 @@
 #include <ie_ngraph_utils.hpp>
 #include <mkldnn_extension_utils.h>
 #include <ngraph/runtime/host_tensor.hpp>
+#include "common/blocked_desc_creator.h"
 
 using namespace mkldnn;
 using namespace MKLDNNPlugin;
@@ -28,45 +29,32 @@ void MKLDNNReferenceNode::initSupportedPrimitiveDescriptors() {
     if (!supportedPrimitiveDescriptors.empty())
         return;
 
-    InferenceEngine::LayerConfig config;
-    for (size_t i = 0; i < inDims.size(); i++) {
-        InferenceEngine::DataConfig dataConfig;
-        dataConfig.inPlace = -1;
-        dataConfig.constant = false;
-
-        dataConfig.desc = MKLDNNMemoryDesc(inDims[i],
-                MKLDNNExtensionUtils::IEPrecisionToDataType(convertPrecision(ngraphOp->get_input_element_type(i))),
-                MKLDNNMemory::GetPlainFormat(inDims[i]));
-
-        config.inConfs.push_back(dataConfig);
+    std::vector<PortConfigurator> inputConfigurators;
+    inputConfigurators.reserve(inputShapes.size());
+    for (size_t i = 0; i < inputShapes.size(); i++) {
+        inputConfigurators.emplace_back(GeneralLayout::ncsp, convertPrecision(ngraphOp->get_input_element_type(i)), inputShapes[i]);
     }
 
-    for (size_t i = 0; i < outDims.size(); i++) {
-        InferenceEngine::DataConfig dataConfig;
-        dataConfig.inPlace = -1;
-        dataConfig.constant = false;
-
-        dataConfig.desc = MKLDNNMemoryDesc(outDims[i],
-                MKLDNNExtensionUtils::IEPrecisionToDataType(convertPrecision(ngraphOp->get_output_element_type(i))),
-                MKLDNNMemory::GetPlainFormat(outDims[i]));
-
-        config.outConfs.push_back(dataConfig);
+    std::vector<PortConfigurator> outputConfigurators;
+    outputConfigurators.reserve(inputShapes.size());
+    for (size_t i = 0; i < outputShapes.size(); i++) {
+        outputConfigurators.emplace_back(GeneralLayout::ncsp, convertPrecision(ngraphOp->get_output_element_type(i)), outputShapes[i]);
     }
 
-    supportedPrimitiveDescriptors.push_back({config, impl_desc_type::ref, memory::format_tag::undef});
+    addSupportedPrimDesc(inputConfigurators, outputConfigurators, impl_desc_type::ref);
 }
 
 void MKLDNNReferenceNode::createPrimitive() {}
 
 void MKLDNNReferenceNode::execute(mkldnn::stream strm) {
     ngraph::HostTensorVector inputs;
-    for (size_t i = 0; i < inDims.size(); i++) {
+    for (size_t i = 0; i < inputShapes.size(); i++) {
         void *srcDataPtr = getParentEdgesAtPort(i)[0]->getMemory().GetPtr();
         inputs.push_back(std::make_shared<ngraph::HostTensor>(ngraphOp->get_input_element_type(i), ngraphOp->get_input_shape(i), srcDataPtr));
     }
 
     ngraph::HostTensorVector outputs;
-    for (size_t i = 0; i < outDims.size(); i++) {
+    for (size_t i = 0; i < outputShapes.size(); i++) {
         void *dstDataPtr = getChildEdgesAtPort(i)[0]->getMemory().GetPtr();
         outputs.push_back(std::make_shared<ngraph::HostTensor>(ngraphOp->get_output_element_type(i), ngraphOp->get_output_shape(i), dstDataPtr));
     }
