2c072f9cde834c65360e5eff74121e37f711cc43
Luwei Zhou <luwei.zhou@intel.com>
2021-06-30 15:12:01 +0800

    Migrate to use the template test.

--- ngraph/test/visitors/op/erf.cpp
+++ ngraph/test/visitors/op/erf.cpp
@@ -2,25 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "gtest/gtest.h"
+#include "unary_ops.hpp"
 
-#include "ngraph/ngraph.hpp"
-#include "ngraph/op/util/attr_types.hpp"
-#include "ngraph/opsets/opset7.hpp"
-#include "util/visitor.hpp"
+using Type = ::testing::Types<UnaryOperatorType<ngraph::op::v0::Erf, element::f32>>;
 
-using namespace std;
-using namespace ngraph;
-using ngraph::test::NodeBuilder;
-
-TEST(attributes, erf_op)
-{
-    NodeBuilder::get_ops().register_factory<opset7::Erf>();
-    const auto A = make_shared<op::Parameter>(element::f32, Shape{3, 7});
-
-    const auto erf = make_shared<opset7::Erf>(A);
-    NodeBuilder builder(erf);
-
-    const auto expected_attr_count = 0;
-    EXPECT_EQ(builder.get_value_map_size(), expected_attr_count);
-}
+INSTANTIATE_TYPED_TEST_SUITE_P(visitor_without_atrribute,
+                               UnaryOperatorVisitor,
+                               Type,
+                               UnaryOperatorTypeName);
