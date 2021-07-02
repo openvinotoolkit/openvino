7968bca1daaf249bca47c87835c5e1f7cfd2011d
Evgeny Kotov <evgeny.kotov@intel.com>
2021-07-02 17:56:06 +0300

    remove debug

--- inference-engine/tests/unit/gna/ngraph/transformations/gna_convert_matmul_to_pointwise_convolution.cpp
+++ inference-engine/tests/unit/gna/ngraph/transformations/gna_convert_matmul_to_pointwise_convolution.cpp
@@ -429,4 +429,32 @@ TEST(TransformationTests, ConvertMatmulWithFqToPointWiseConvolutionTestNoAddNode
     ASSERT_TRUE(result.valid);
 }
 
+TEST(TransformationTests, ConvertMatmulWithFqToPointWiseConvolutionFqTestNoAdd) {
+    std::shared_ptr<ngraph::Function> func(nullptr), reference_func(nullptr);
+    {
+        Graph graph = createTransformedGraph<CreateFakeQuantize, CreateMatMul, CreateFakeQuantize>();
+
+        auto result = std::make_shared<ngraph::opset7::Result>(graph.output);
+        func = std::make_shared<ngraph::Function>(ngraph::ResultVector{result},
+                                                  ngraph::ParameterVector{graph.input_params});
+
+        ngraph::pass::Manager m;
+        m.register_pass<ngraph::pass::InitNodeInfo>();
+        m.register_pass<GNAPluginNS::ConvertMatmulWithFqToPointWiseConvolution>();
+        m.run_passes(func);
+        ASSERT_NO_THROW(check_rt_info(func));
+    }
+
+    {
+        Graph graph = createReferenceGraph(true /* addConstFakeQuantizeNode */, false /* insertAddNode */, true /* addOutFakeQuantizeNode */);
+        auto result = std::make_shared<ngraph::opset7::Result>(graph.output);
+        reference_func = std::make_shared<ngraph::Function>(ngraph::ResultVector{result},
+                                                  ngraph::ParameterVector{graph.input_params});
+    }
+
+    const FunctionsComparator func_comparator = FunctionsComparator::with_default().enable(FunctionsComparator::ATTRIBUTES);
+    const FunctionsComparator::Result result = func_comparator(func, reference_func);
+    ASSERT_TRUE(result.valid);
+}
+
 } // namespace testing
