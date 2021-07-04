0331bd34cefb5e02ddaa59b9b2caee3432388994
Gleb Kazantaev <gleb.kazantaev@intel.com>
2021-07-04 23:18:17 +0300

    Update tensor name logic

--- ngraph/core/src/graph_util.cpp
+++ ngraph/core/src/graph_util.cpp
@@ -905,17 +905,19 @@ bool ngraph::replace_output_update_name(Output<Node> output, const Output<Node>&
         if (has_result_output && !is_type<ngraph::op::Parameter>(replacement.get_node()))
         {
             replacement.get_node()->set_friendly_name(output.get_node()->get_friendly_name());
+            // Update output tensor name
+            NGRAPH_SUPPRESS_DEPRECATED_START
+            replacement.get_tensor().set_name(output.get_node()->get_friendly_name());
+            NGRAPH_SUPPRESS_DEPRECATED_END
         }
 
+        // Save replacement tensor names before replacement as they will be
+        // overrided by the output tensor names
         auto output_names = replacement.get_tensor_ptr()->get_names();
         output.replace(replacement);
 
-        // Union tensor names and set them to the replacement tensor
-        for (const auto& name : output.get_tensor_ptr()->get_names())
-        {
-            output_names.insert(name);
-        }
-        replacement.get_tensor_ptr()->set_names(output_names);
+        // Restore back original replacement tensor names
+        replacement.get_tensor().add_names(output_names);
 
         copy_runtime_info({replacement.get_node_shared_ptr(), output.get_node_shared_ptr()},
                           replacement.get_node_shared_ptr());
