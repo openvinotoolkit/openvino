88caaba6f5cc4ad492c59b4a2d77769447d7adaf
Nam David <david.nam@intel.com>
2021-07-06 23:41:47 +0900

    Revise data generators used in layout tests

--- inference-engine/tests/ie_test_utils/common_test_utils/data_utils.hpp
+++ inference-engine/tests/ie_test_utils/common_test_utils/data_utils.hpp
@@ -246,11 +246,11 @@ fill_data_random_float(InferenceEngine::Blob::Ptr &blob, const uint32_t range, i
     using dataType = typename InferenceEngine::PrecisionTrait<PRC>::value_type;
     std::default_random_engine random(seed);
     // 1/k is the resolution of the floating point numbers
-    std::uniform_int_distribution<int32_t> distribution(k * start_from, k * (start_from + range));
+    std::uniform_real_distribution<float> dist(start_from, range);
 
     auto *rawBlobDataPtr = blob->buffer().as<dataType *>();
     for (size_t i = 0; i < blob->size(); i++) {
-        auto value = static_cast<float>(distribution(random));
+        auto value = static_cast<float>(dist(random));
         value /= static_cast<float>(k);
         if (PRC == InferenceEngine::Precision::FP16) {
             rawBlobDataPtr[i] = static_cast<dataType>(ngraph::float16(value).to_bits());
