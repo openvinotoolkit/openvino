ebcbe46475b8b69aaa167138d909bc073753e188
blesniewski <bartosz.lesniewski@intel.com>
2021-07-06 14:59:32 +0200

    Add PriorBoxClustered to trusted ops list

--- inference-engine/tests/ie_test_utils/functional_test_utils/layer_tests_summary/utils/constants.py
+++ inference-engine/tests/ie_test_utils/functional_test_utils/layer_tests_summary/utils/constants.py
@@ -60,6 +60,7 @@ VERIFIED_OP_REFERENCES = [
     'NonMaxSuppression-4',
     'NonMaxSuppression-5',
     'NonZero-3',
+    'PriorBoxClustered-1',
     'PriorBox-1',
     'Proposal-1',
     'Proposal-4',
