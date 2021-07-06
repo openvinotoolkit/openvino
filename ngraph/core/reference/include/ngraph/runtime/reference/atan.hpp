964c62fcf34b5f9678aaffa11d04c7cb5b36c5af
song, bell <bell.song@intel.com>
2021-07-06 14:45:18 +0800

    revise atan op (#6288)
    * revise atan op
    
    * use parametrized vistor API
    
    * use new gtest macros
    
    * Update docs/ops/arithmetic/Atan_1.md
    
    Co-authored-by: Gabriele Galiero Casay <gabriele.galiero.casay@intel.com>
    
    * Update docs/ops/arithmetic/Atan_1.md
    
    Co-authored-by: Gabriele Galiero Casay <gabriele.galiero.casay@intel.com>
    
    * Update ngraph/core/src/op/atan.cpp
    
    Co-authored-by: Gabriele Galiero Casay <gabriele.galiero.casay@intel.com>
    
    * update doc to follow the rules
    
    * create type_prop for atan
    
    * drop op_eval to be covered in backend
    
    * add the missing type prop case
    
    * add integer type ref impl
    
    * fix clang issue
    
    Co-authored-by: Gabriele Galiero Casay <gabriele.galiero.casay@intel.com>

--- ngraph/core/reference/include/ngraph/runtime/reference/atan.hpp
+++ ngraph/core/reference/include/ngraph/runtime/reference/atan.hpp
@@ -13,12 +13,23 @@ namespace ngraph
     {
         namespace reference
         {
-            template <typename T>
+            template <typename T,
+                      typename std::enable_if<!std::is_integral<T>::value, bool>::type = true>
             void atan(const T* arg, T* out, size_t count)
             {
                 for (size_t i = 0; i < count; i++)
                 {
                     out[i] = std::atan(arg[i]);
+                }
+            }
+
+            template <typename T,
+                      typename std::enable_if<std::is_integral<T>::value, bool>::type = true>
+            void atan(const T* arg, T* out, size_t count)
+            {
+                for (size_t i = 0; i < count; i++)
+                {
+                    out[i] = std::roundl(std::atan(arg[i]));
                 }
             }
         } // namespace reference
