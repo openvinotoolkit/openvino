13b6d74475d6bb166b08bfc0c376322b6c99decb
Andrey Sapozhnikov <andrey.sapozhnikov@intel.com>
2021-07-07 15:50:55 +0300

    [GNA] Functional test returned to the skip list due to instability (#6492)

--- inference-engine/tests/functional/plugin/gna/shared_tests_instances/skip_tests_config.cpp
+++ inference-engine/tests/functional/plugin/gna/shared_tests_instances/skip_tests_config.cpp
@@ -9,28 +9,12 @@
 
 std::vector<std::string> disabledTestPatterns() {
     return {
-        ".*TensorNamesTest\\.CheckAddOutput.*",
         // TODO: FIX BUG 31661
         // TODO: support InferRequest in GNAPlugin
         ".*InferRequestTests\\.canRun3AsyncRequestsConsistentlyFromThreadsWithoutWait.*",
-        // TODO: FIX BUG 23740
-        ".*InferRequestTests\\.CanCreateTwoExeNetworks.*",
-        // TODO: FIX BUG 26702
-        ".*InferRequestTests\\.FailedAsyncInferWithNegativeTimeForWait.*",
         // TODO: FIX BUG 23741
         ".*InferRequestTests\\.canRun3SyncRequestsConsistentlyFromThreads.*",
-        // TODO: FIX BUG 23742
-        ".*InferRequestTests\\.canWaitWithotStartAsync.*",
-        // TODO: FIX BUG 23743
-        ".*InferRequestTests\\.returnDeviceBusyOnSetBlobAfterAsyncInfer.*",
-        ".*InferRequestTests\\.returnDeviceBusyOnGetBlobAfterAsyncInfer.*",
-        ".*InferRequestTests\\.returnDeviceBusyOnGetPerformanceCountAfterAsyncInfer.*",
-        ".*InferRequestTests\\.returnDeviceBusyOnStartInferAfterAsyncInfer.*",
-        ".*InferRequestTests\\.returnDeviceBusyOnGetUserDataAfterAsyncInfer.*",
-        ".*InferRequestTests\\.returnDeviceBusyOnSetUserDataAfterAsyncInfer.*",
-        // TODO: FIX BUG 31661
-        ".*InferRequestTests\\.canStartSeveralAsyncInsideCompletionCallbackNoSafeDtorWithoutWait.*",
-        // TODO: FIX BUG 31661
+        // TODO: FIX BUG 59041
         ".*Behavior.*CallbackThrowException.*",
         // TODO: FIX BUG 32210
         R"(.*ActivationLayerTest.CompareWithRefs/(Sigmoid|Tanh|Exp|Log).*)",
