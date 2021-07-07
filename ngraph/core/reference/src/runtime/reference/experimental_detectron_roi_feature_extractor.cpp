87950dffe3ad1fca99f9c2c9d46033ee5ab31ded
VGavrilov <vladimir.gavrilov@intel.com>
2021-07-07 09:55:16 +0300

    Some changes.

--- ngraph/core/reference/src/runtime/reference/experimental_detectron_roi_feature_extractor.cpp
+++ ngraph/core/reference/src/runtime/reference/experimental_detectron_roi_feature_extractor.cpp
@@ -122,19 +122,15 @@ namespace
             {
                 for (int64_t iy = 0; iy < iy_upper; iy++)
                 {
-                    std::cout << "                        iy: " << iy << "\n";
                     const T yy = roi_start_h + static_cast<T>(ph) * bin_size_h +
                                  static_cast<T>(static_cast<T>(iy) + static_cast<T>(0.5f)) * bin_size_h /
                                      static_cast<T>(roi_bin_grid_h);
-                    std::cout << "                        yy: " << yy << "\n";
-                    std::cout << "                        Loop with respect to ix in range(0, ix_upper)\n";
+
                     for (int64_t ix = 0; ix < ix_upper; ix++)
                     {
-                        std::cout << "                            ix: " << ix << "\n";
                         const T xx =
                             roi_start_w + static_cast<T>(pw) * bin_size_w +
                             static_cast<T>(static_cast<T>(ix) + static_cast<T>(0.5f)) * bin_size_w / static_cast<T>(roi_bin_grid_w);
-                        std::cout << "                            xx: " << xx << "\n";
 
                         T x = xx;
                         T y = yy;
