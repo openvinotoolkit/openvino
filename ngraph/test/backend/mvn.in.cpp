9e041eb66ea5e5be1bb95a41e60d1dcbe517151b
Szymon Durawa <szymon.durawa@intel.com>
2021-07-08 15:34:45 +0200

    Add visitor and backend tests.

--- ngraph/test/backend/mvn.in.cpp
+++ ngraph/test/backend/mvn.in.cpp
@@ -137,3 +137,85 @@ NGRAPH_TEST(${BACKEND_NAME}, evaluate_mvn_6_across_batch)
     // clang-format on
     test_case.run();
 }
+
+NGRAPH_TEST(${BACKEND_NAME}, evaluate_mvn_1_no_variance_no_across_channels)
+{
+    auto data = make_shared<op::Parameter>(element::f32, PartialShape{1, 3, 3, 3});
+    auto mvn = make_shared<op::v0::MVN>(data, false, false, 1e-9);
+    auto fun = make_shared<Function>(OutputVector{mvn}, ParameterVector{data});
+    auto test_case = test::TestCase<TestEngine>(fun);
+
+    test_case.add_input<float>({1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+
+    test_case.add_expected_output<float>({-4, -3, -2, -1, 0, 1, 2, 3, 4,
+                                          -4, -3, -2, -1, 0, 1, 2, 3, 4,
+                                          -4, -3, -2, -1, 0, 1, 2, 3, 4});
+    // clang-format on
+    test_case.run();
+}
+
+NGRAPH_TEST(${BACKEND_NAME}, evaluate_mvn_1_across_channels_no_variance)
+{
+    auto data = make_shared<op::Parameter>(element::f32, PartialShape{1, 3, 2, 2});
+    auto mvn = make_shared<op::v0::MVN>(data, true, false, 1e-9);
+    auto fun = make_shared<Function>(OutputVector{mvn}, ParameterVector{data});
+    auto test_case = test::TestCase<TestEngine>(fun);
+
+    // clang-format off
+    test_case.add_input<float>({1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3});
+
+    test_case.add_expected_output<float>({-3.25, -2.25, -1.25,
+                                          -0.25,  0.75, 1.75,
+                                           2.75,  3.75, 4.75,
+                                          -3.25, -2.25, -1.25});
+    // clang-format on
+    test_case.run();
+}
+
+NGRAPH_TEST(${BACKEND_NAME}, evaluate_mvn_1_variance_no_across_channels)
+{
+    auto data = make_shared<op::Parameter>(element::f32, PartialShape{1, 3, 3, 3});
+    auto mvn = make_shared<op::v0::MVN>(data, false, true, 1e-9);
+    auto fun = make_shared<Function>(OutputVector{mvn}, ParameterVector{data});
+    auto test_case = test::TestCase<TestEngine>(fun);
+
+    // clang-format off
+    test_case.add_input<float>({1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+
+    test_case.add_expected_output<float>({-1.5491934, -1.161895, -0.7745967,
+                                          -0.38729835, 0.,        0.38729835,
+                                           0.7745967,  1.161895,  1.5491934,
+                                          -1.5491934, -1.161895, -0.7745967,
+                                          -0.38729835, 0.,        0.38729835,
+                                           0.7745967,  1.161895,  1.5491934,
+                                          -1.5491934, -1.161895, -0.7745967,
+                                          -0.38729835, 0.,        0.38729835,
+                                           0.7745967,  1.161895,  1.5491934});
+    // clang-format on
+    test_case.run();
+}
+
+NGRAPH_TEST(${BACKEND_NAME}, evaluate_mvn_1_across_channels_with_variance)
+{
+    auto data = make_shared<op::Parameter>(element::f32, PartialShape{1, 3, 3, 3});
+
+    auto mvn = make_shared<op::v0::MVN>(data, true, true, 1e-9);
+    auto fun = make_shared<Function>(OutputVector{mvn}, ParameterVector{data});
+    auto test_case = test::TestCase<TestEngine>(fun);
+
+    // clang-format off
+    test_case.add_input<float>(
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+
+    test_case.add_expected_output<float>({-1.5491934, -1.161895, -0.7745967,
+                                          -0.38729835, 0.,        0.38729835,
+                                           0.7745967,  1.161895,  1.5491934,
+                                          -1.5491934, -1.161895, -0.7745967,
+                                          -0.38729835, 0.,        0.38729835,
+                                           0.7745967,  1.161895,  1.5491934,
+                                          -1.5491934, -1.161895, -0.7745967,
+                                          -0.38729835, 0.,        0.38729835,
+                                           0.7745967,  1.161895,  1.5491934});
+    // clang-format on
+    test_case.run();
+}
