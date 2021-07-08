033f24e966b66e8057326c0d0cf3a53c38093636
Patryk Elszkowski <patryk.elszkowski@intel.com>
2021-07-08 10:07:13 +0200

    fix backend test after refactoring

--- ngraph/test/backend/fake_quantize.in.cpp
+++ ngraph/test/backend/fake_quantize.in.cpp
@@ -36,11 +36,11 @@ namespace
     template <typename T>
     std::vector<T> iota_vector(size_t size, T first_value = {})
     {
-        std::vector<T> d;
+        std::vector<T> d(size);
         std::iota(begin(d), end(d), first_value);
         return d;
     }
-}
+} // namespace
 
 NGRAPH_TEST(${BACKEND_NAME}, fake_quantize)
 {
@@ -132,10 +132,10 @@ NGRAPH_TEST(${BACKEND_NAME}, fake_quantize_pdpd)
     size_t levels = 5;
     const auto broadcast = op::AutoBroadcastSpec(op::AutoBroadcastType::PDPD, 1);
     auto data = std::make_shared<op::Parameter>(element::f32, data_shape);
-    auto input_low = op::Constant::create(element::f32, Shape{2, 1, 1, 1, 1}, {5.f, 30.f});
-    auto input_high = op::Constant::create(element::f32, Shape{2, 1, 1, 1, 1}, {10.f, 40.f});
-    auto output_low = op::Constant::create(element::f32, Shape{2, 1, 1, 1, 1}, {0.f, 50.f});
-    auto output_high = op::Constant::create(element::f32, Shape{2, 1, 1, 1, 1}, {20.f, 70.f});
+    auto input_low = op::Constant::create(element::f32, Shape{2, 1, 1, 1}, {5.f, 30.f});
+    auto input_high = op::Constant::create(element::f32, Shape{2, 1, 1, 1}, {10.f, 40.f});
+    auto output_low = op::Constant::create(element::f32, Shape{2, 1, 1, 1}, {0.f, 50.f});
+    auto output_high = op::Constant::create(element::f32, Shape{2, 1, 1, 1}, {20.f, 70.f});
 
     auto quantize = std::make_shared<op::FakeQuantize>(
         data, input_low, input_high, output_low, output_high, levels, broadcast);
