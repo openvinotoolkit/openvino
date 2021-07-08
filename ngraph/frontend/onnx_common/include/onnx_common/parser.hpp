e03565662d55483974550a2a51bf7329f4eb9204
mbencer <mateusz.bencer@intel.com>
2021-07-08 12:17:29 +0200

    merged onnx common

--- ngraph/frontend/onnx_common/include/onnx_common/parser.hpp
+++ ngraph/frontend/onnx_common/include/onnx_common/parser.hpp
@@ -5,6 +5,7 @@
 #pragma once
 #include <fstream>
 #include <string>
+#include "onnx_import/utils/onnx_importer_visibility.hpp"
 
 namespace ONNX_NAMESPACE
 {
@@ -20,14 +21,14 @@ namespace ngraph
         /// \param   file_path    Path to the file containing an ONNX model.
         ///
         /// \return  The parsed in-memory representation of the ONNX model
-        ONNX_NAMESPACE::ModelProto parse_from_file(const std::string& file_path);
+        ONNX_IMPORTER_API ONNX_NAMESPACE::ModelProto parse_from_file(const std::string& file_path);
 
         /// \brief   Parses an ONNX model from a stream (representing for example a file)
         ///
         /// \param   model_stream  Path to the file containing an ONNX model.
         ///
         /// \return  The parsed in-memory representation of the ONNX model
-        ONNX_NAMESPACE::ModelProto parse_from_istream(std::istream& model_stream);
+        ONNX_IMPORTER_API ONNX_NAMESPACE::ModelProto parse_from_istream(std::istream& model_stream);
     } // namespace onnx_common
 
 } // namespace ngraph
