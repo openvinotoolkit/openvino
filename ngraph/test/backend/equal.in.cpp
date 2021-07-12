3aa834aa4803936e3631cfedf786372fdd697012
bszmelcz <bartosz.szmelczynski@intel.com>
2021-07-12 12:41:23 +0200

    add backend, visitors, serialize SLT tests

--- ngraph/test/backend/equal.in.cpp
+++ ngraph/test/backend/equal.in.cpp
@@ -33,17 +33,50 @@ using TestEngine = test::ENGINE_CLASS_NAME(${BACKEND_NAME});
 
 NGRAPH_TEST(${BACKEND_NAME}, equal_f32_no_broadcast)
 {
-    Shape shape{5};
-    auto A = make_shared<op::Parameter>(element::f32, shape);
-    auto B = make_shared<op::Parameter>(element::f32, shape);
-    auto f = make_shared<Function>(make_shared<op::Sinh>(A, B), ParameterVector{A, B});
-
-    const vector<float> a{1, 2, 3, 4, 5};
-    const vector<float> b{1, 6, 7, 4, 1};
-    const vector<char> expected{1, 0, 0, 1, 0};
+    Shape shape{4};
+    auto A = op::Constant::create(element::f32, shape, {-2.5f, 25.5f, 2.25f, 6.0f});
+    auto B = op::Constant::create(element::f32, shape, {10.0f, 5.0f, 2.25f, 10.0f});
+    auto f = make_shared<Function>(make_shared<op::v1::Equal>(A, B), ParameterVector{});
 
     auto test_case = test::TestCase<TestEngine>(f);
-    test_case.add_multiple_inputs<float>({a, b});
-    test_case.add_expected_output<float>(shape, {expected});
+    test_case.add_expected_output<bool>(shape, {false, false, true, false});
+    test_case.run();
+}
+
+NGRAPH_TEST(${BACKEND_NAME}, equal_f32_broadcast)
+{
+    Shape shape1{2, 1, 2, 1};
+    Shape shape2{1, 2, 1};
+    Shape shape_output{2, 1, 2, 1};
+    auto A = op::Constant::create(element::f32, shape1, {2.0f, 1.0f, 4.0f, 1.0f});
+    auto B = op::Constant::create(element::f32, shape2, {1.0f, 1.0f});
+    auto f = make_shared<Function>(make_shared<op::v1::Equal>(A, B), ParameterVector{});
+
+    auto test_case = test::TestCase<TestEngine>(f);
+    test_case.add_expected_output<bool>(shape_output, {false, true, false, true});
+    test_case.run();
+}
+
+NGRAPH_TEST(${BACKEND_NAME}, equal_f32_negative)
+{
+    Shape shape{3};
+    auto A = op::Constant::create(element::f32, shape, {-1.5f, -2.5f, -5.0f});
+    auto B = op::Constant::create(element::f32, shape, {1.5f, -4.5f, -5.0f});
+    auto f = make_shared<Function>(make_shared<op::v1::Equal>(A, B), ParameterVector{});
+
+    auto test_case = test::TestCase<TestEngine>(f);
+    test_case.add_expected_output<bool>(shape, {false, false, true});
+    test_case.run();
+}
+
+NGRAPH_TEST(${BACKEND_NAME}, equal_i32)
+{
+    Shape shape{3};
+    auto A = op::Constant::create(element::i32, shape, {1, 2, 3});
+    auto B = op::Constant::create(element::i32, shape, {4, 2, 1});
+    auto f = make_shared<Function>(make_shared<op::v1::Equal>(A, B), ParameterVector{});
+
+    auto test_case = test::TestCase<TestEngine>(f);
+    test_case.add_expected_output<uint8_t>(shape, {0, 1, 0});
     test_case.run();
 }
