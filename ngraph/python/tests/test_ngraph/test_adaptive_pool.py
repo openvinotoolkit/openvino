f70ebc99081b96ec1dc69477fcd9b5a61c624c89
Vafin, Maxim <maxim.vafin@intel.com>
2021-07-13 14:13:12 +0300

    Change ticket number

--- ngraph/python/tests/test_ngraph/test_adaptive_pool.py
+++ ngraph/python/tests/test_ngraph/test_adaptive_pool.py
@@ -1,10 +1,10 @@
 import ngraph as ng
 import numpy as np
-from tests import xfail_issue_56371
+from tests import xfail_issue_59935
 from tests.runtime import get_runtime
 
 
-@xfail_issue_56371
+@xfail_issue_59935
 def test_adaptive_avg_pool():
     runtime = get_runtime()
     input = np.reshape([0, 4, 1, 3, -2, -5, -2,
@@ -31,7 +31,7 @@ def test_adaptive_avg_pool():
     assert np.allclose(adaptive_pool_results, expected_results)
 
 
-@xfail_issue_56371
+@xfail_issue_59935
 def test_adaptive_max_pool():
     runtime = get_runtime()
     input = np.reshape([0, 4, 1, 3, -2, -5, -2,
