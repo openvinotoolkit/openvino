154b7fcca63751ebd475b86fe2ba342d37eb1711
Krzysztof Bruniecki <krzysztof.bruniecki@intel.com>
2021-07-13 16:53:29 +0200

    cpplint

--- inference-engine/src/gna_plugin/optimizer/gna_pass_manager.cpp
+++ inference-engine/src/gna_plugin/optimizer/gna_pass_manager.cpp
@@ -1193,7 +1193,6 @@ void InsertConcatAligningConvolutionFilterPass::run() {
                 size_t bytesOffset = (aligned64_offset / bytesPerConcatElement) * (quantized ? bytesPerConcatElement : 4);
 
                 if (0 == num_rows_padded && ALIGN(num_rows_in, 32) > 32) {
-
                     gnalog() << "Inserted Concat Aligning Layer between: " << prevLayer->name << " and " << l->name << std::endl;
 
                     // insert the filter
