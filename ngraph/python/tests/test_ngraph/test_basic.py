3a7b0b903cb5163133bb4e26049c968b5a18441f
Jan Iwaszkiewicz <jan.iwaszkiewicz@intel.com>
2021-07-13 11:49:11 +0200

    [nGraph] Return Node's operation version (#6589)
    * Add python test
    
    * expose node version to python api

--- ngraph/python/tests/test_ngraph/test_basic.py
+++ ngraph/python/tests/test_ngraph/test_basic.py
@@ -425,3 +425,10 @@ def test_sink_function_ctor():
     assert len(function.get_parameters()) == 1
     assert len(function.get_results()) == 1
     assert function.get_friendly_name() == "TestFunction"
+
+
+def test_node_version():
+    node = ng.add([1], [2])
+
+    assert node.get_version() == 1
+    assert node.version == 1
