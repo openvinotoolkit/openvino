4b65a143722ef7460d42c5ce429b96358751795b
vurusovs <vitaliy.urusovskij@intel.com>
2021-07-14 14:16:11 +0300

    Prepare separate runner in `utils`folder

--- tests/conditional_compilation/conftest.py
+++ tests/conditional_compilation/conftest.py
@@ -13,7 +13,7 @@ from inspect import getsourcefile
 from pathlib import Path
 
 # add ../lib to imports
-sys.path.insert(0, str((Path(getsourcefile(lambda: 0)) / ".." / ".." / "lib").resolve(strict=True)))
+sys.path.insert(0, str((Path(getsourcefile(lambda: 0)) / ".." / ".." / "utils").resolve(strict=True)))
 
 import yaml
 import pytest
