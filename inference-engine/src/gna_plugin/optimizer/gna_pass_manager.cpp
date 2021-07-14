57cb093863a8fd072439b517fe70c5222fce7c72
Dmitrii Khurtin <dmitrii.khurtin@intel.com>
2021-07-14 16:03:46 +0300

    fixed crash related with loading model with fq and sigmoid

--- inference-engine/src/gna_plugin/optimizer/gna_pass_manager.cpp
+++ inference-engine/src/gna_plugin/optimizer/gna_pass_manager.cpp
@@ -2245,7 +2245,7 @@ void MoveFakeQuantizeLayerIntoQuantParamsPass :: run() {
         }
 
         if (isFQFuseAllowed) {
-            getInputTo(prevData).clear();
+            getInputTo(prevData).erase(l->name);
         }
 
         // Connect all next layers after FQ to the layer that is before FQ
