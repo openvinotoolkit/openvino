ff3e883a71c8c14b55f9fd69407200ebf8d43181
Luo Cheng <cheng.luo@intel.com>
2021-07-14 12:06:39 +0800

    MulticlassNms/MatrixNms: ngraph python api (#6573)
    * nms python api
    
    * fix python code style
    
    * fix python code style
    
    * apply review comments
    
    * apply review comments

--- ngraph/python/src/ngraph/opset8/ops.py
+++ ngraph/python/src/ngraph/opset8/ops.py
@@ -99,3 +99,120 @@ def deformable_convolution(
             "bilinear_interpolation_pad": bilinear_interpolation_pad
         },
     )
+
+
+@nameable_op
+def multiclass_nms(
+    boxes: NodeInput,
+    scores: NodeInput,
+    sort_result_type: str = "none",
+    sort_result_across_batch: bool = False,
+    output_type: str = "i64",
+    iou_threshold: float = 0.0,
+    score_threshold: float = 0.0,
+    nms_top_k: int = -1,
+    keep_top_k: int = -1,
+    background_class: int = -1,
+    nms_eta: float = 1.0,
+    normalized: bool = True
+) -> Node:
+    """Return a node which performs MulticlassNms.
+
+    @param boxes: Tensor with box coordinates.
+    @param scores: Tensor with box scores.
+    @param sort_result_type: Specifies order of output elements, possible values:
+                             'class': sort selected boxes by class id (ascending)
+                             'score': sort selected boxes by score (descending)
+                             'none': do not guarantee the order.
+    @param sort_result_across_batch: Specifies whenever it is necessary to sort selected boxes
+                                     across batches or not
+    @param output_type: Specifies the output tensor type, possible values:
+                        'i64', 'i32'
+    @param iou_threshold: Specifies intersection over union threshold
+    @param score_threshold: Specifies minimum score to consider box for the processing
+    @param nms_top_k: Specifies maximum number of boxes to be selected per class, -1 meaning
+                      to keep all boxes
+    @param keep_top_k: Specifies maximum number of boxes to be selected per batch element, -1
+                       meaning to keep all boxes
+    @param background_class: Specifies the background class id, -1 meaning to keep all classes
+    @param nms_eta: Specifies eta parameter for adpative NMS, in close range [0, 1.0]
+    @param normalized: Specifies whether boxes are normalized or not
+    @return: The new node which performs MuticlassNms
+    """
+    inputs = as_nodes(boxes, scores)
+
+    attributes = {
+        "sort_result_type": sort_result_type,
+        "sort_result_across_batch": sort_result_across_batch,
+        "output_type": output_type,
+        "iou_threshold": iou_threshold,
+        "score_threshold": score_threshold,
+        "nms_top_k": nms_top_k,
+        "keep_top_k": keep_top_k,
+        "background_class": background_class,
+        "nms_eta": nms_eta,
+        "normalized": normalized
+    }
+
+    return _get_node_factory_opset8().create("MulticlassNms", inputs, attributes)
+
+
+@nameable_op
+def matrix_nms(
+    boxes: NodeInput,
+    scores: NodeInput,
+    sort_result_type: str = "none",
+    sort_result_across_batch: bool = False,
+    output_type: str = "i64",
+    score_threshold: float = 0.0,
+    nms_top_k: int = -1,
+    keep_top_k: int = -1,
+    background_class: int = -1,
+    decay_function: str = "linear",
+    gaussian_sigma: float = 2.0,
+    post_threshold: float = 0.0,
+    normalized: bool = True
+) -> Node:
+    """Return a node which performs MatrixNms.
+
+    @param boxes: Tensor with box coordinates.
+    @param scores: Tensor with box scores.
+    @param sort_result_type: Specifies order of output elements, possible values:
+                             'class': sort selected boxes by class id (ascending)
+                             'score': sort selected boxes by score (descending)
+                             'none': do not guarantee the order.
+    @param sort_result_across_batch: Specifies whenever it is necessary to sort selected boxes
+                                     across batches or not
+    @param output_type: Specifies the output tensor type, possible values:
+                        'i64', 'i32'
+    @param score_threshold: Specifies minimum score to consider box for the processing
+    @param nms_top_k: Specifies maximum number of boxes to be selected per class, -1 meaning
+                      to keep all boxes
+    @param keep_top_k: Specifies maximum number of boxes to be selected per batch element, -1
+                       meaning to keep all boxes
+    @param background_class: Specifies the background class id, -1 meaning to keep all classes
+    @param decay_function: Specifies decay function used to decay scores, possible values:
+                           'gaussian', 'linear'
+    @param gaussian_sigma: Specifies gaussian_sigma parameter for gaussian decay_function
+    @param post_threshold: Specifies threshold to filter out boxes with low confidence score
+                           after decaying
+    @param normalized: Specifies whether boxes are normalized or not
+    @return: The new node which performs MatrixNms
+    """
+    inputs = as_nodes(boxes, scores)
+
+    attributes = {
+        "sort_result_type": sort_result_type,
+        "sort_result_across_batch": sort_result_across_batch,
+        "output_type": output_type,
+        "score_threshold": score_threshold,
+        "nms_top_k": nms_top_k,
+        "keep_top_k": keep_top_k,
+        "background_class": background_class,
+        "decay_function": decay_function,
+        "gaussian_sigma": gaussian_sigma,
+        "post_threshold": post_threshold,
+        "normalized": normalized
+    }
+
+    return _get_node_factory_opset8().create("MatrixNms", inputs, attributes)
