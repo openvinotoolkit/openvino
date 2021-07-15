7a2c78f3bc489d425af9a2f9b1760a4bbba5bd71
mitruska <katarzyna.mitrus@intel.com>
2021-07-15 13:13:11 +0200

    Add bf16 type to cpu single layer tests

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/single_layer_tests/grn.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/single_layer_tests/grn.cpp
@@ -3,16 +3,17 @@
 //
 
 #include <vector>
+
 #include "single_layer_tests/grn.hpp"
 #include "common_test_utils/test_constants.hpp"
 
 using namespace LayerTestsDefinitions;
-using namespace ngraph::helpers;
 
 namespace {
 const std::vector<InferenceEngine::Precision> netPrecisions = {
+        InferenceEngine::Precision::BF16,
+        InferenceEngine::Precision::FP16,
         InferenceEngine::Precision::FP32,
-        InferenceEngine::Precision::FP16
 };
 
 const auto basicCases = ::testing::Combine(
@@ -30,7 +31,7 @@ const auto basicCases = ::testing::Combine(
     ::testing::Values(1e-6f, 0.33f, 1.1f, 2.25f, 100.25f),
     ::testing::Values(CommonTestUtils::DEVICE_CPU));
 
-INSTANTIATE_TEST_CASE_P(smoke_Grn_Basic, GrnLayerTest,
+INSTANTIATE_TEST_CASE_P(smoke_GRN_Basic, GrnLayerTest,
                         basicCases,
                         GrnLayerTest::getTestCaseName);
 }  // namespace
