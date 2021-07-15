974d36efd57d76e80fbb09caf648c35f90b1c121
pszmel <piotr.szmelczynski@intel.com>
2021-07-15 14:22:08 +0200

    create reference test for all supported types

--- docs/template_plugin/tests/functional/op_reference/sign.cpp
+++ docs/template_plugin/tests/functional/op_reference/sign.cpp
@@ -18,8 +18,8 @@ using namespace InferenceEngine;
 struct SignParams {
     template <class IT, class OT>
     SignParams(const ngraph::PartialShape& shape, const ngraph::element::Type& iType, const ngraph::element::Type& oType, const std::vector<IT>& iValues,
-                  const std::vector<OT>& oValues, size_t iSize = 0, size_t oSize = 0)
-        : pshape(shape), inType(iType), outType(oType), inputData(CreateBlob(iType, iValues, iSize)), refData(CreateBlob(oType, oValues, oSize)) {}
+                  const std::vector<OT>& oValues)
+        : pshape(shape), inType(iType), outType(oType), inputData(CreateBlob(iType, iValues)), refData(CreateBlob(oType, oValues)) {}
     ngraph::PartialShape pshape;
     ngraph::element::Type inType;
     ngraph::element::Type outType;
@@ -59,13 +59,18 @@ TEST_P(ReferenceSignLayerTest, CompareWithHardcodedRefs) {
 INSTANTIATE_TEST_SUITE_P(
     smoke_Sign_With_Hardcoded_Refs, ReferenceSignLayerTest,
     ::testing::Values(
-        // destination boolean
         SignParams(ngraph::PartialShape {6}, ngraph::element::f32, ngraph::element::f32,
                       std::vector<float> {1, -2, 0, -4.8f, 4.8f, -0.0f},
                       std::vector<float> {1, -1, 0, -1, 1, 0}),
-        SignParams(ngraph::PartialShape {6}, ngraph::element::i16, ngraph::element::i16,
-                      std::vector<int16_t> {1, -2, 0, -4, 4, -0},
-                      std::vector<int16_t> {1, -1, 0, -1, 1, 0}),
+        SignParams(ngraph::PartialShape {6}, ngraph::element::f16, ngraph::element::f16,
+                      std::vector<float16> {1, -2, 0, -4.8f, 4.8f, -0.0f},
+                      std::vector<float16> {1, -1, 0, -1, 1, 0}),
+        SignParams(ngraph::PartialShape {6}, ngraph::element::u64, ngraph::element::u64,
+                      std::vector<uint64_t> {1, 2, 0, 4, 4, 0},
+                      std::vector<uint64_t> {1, 1, 0, 1, 1, 0}),
+        SignParams(ngraph::PartialShape {6}, ngraph::element::u32, ngraph::element::u32,
+                      std::vector<uint32_t> {1, 2, 0, 4, 4, 0},
+                      std::vector<uint32_t> {1, 1, 0, 1, 1, 0}),
         SignParams(ngraph::PartialShape {6}, ngraph::element::i32, ngraph::element::i32,
                       std::vector<int32_t> {1, -2, 0, -4, 4, -0},
                       std::vector<int32_t> {1, -1, 0, -1, 1, 0}),
