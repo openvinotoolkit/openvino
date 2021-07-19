4e3aaa73630d0ad68718ea6b9b1a0f39ef0d10c5
Patryk Elszkowski <patryk.elszkowski@intel.com>
2021-07-19 10:48:15 +0200

    add template test for all supported types in Acosh

--- docs/template_plugin/tests/functional/op_reference/acosh.cpp
+++ docs/template_plugin/tests/functional/op_reference/acosh.cpp
@@ -82,14 +82,24 @@ TEST_P(ReferenceAcoshLayerTest, AcoshWithHardcodedRefs) {
 
 }  // namespace
 
-INSTANTIATE_TEST_SUITE_P(smoke_Acosh_With_Hardcoded_Refs, ReferenceAcoshLayerTest,
-                         ::testing::Values(Builder {}
-                                               .input({{8}, element::f32, std::vector<float> {1.f, 2.f, 3.f, 4.f, 5.f, 10.f, 100.f, 1000.f}})
-                                               .expected({{8}, element::f32, std::vector<float> {0., 1.317, 1.763, 2.063, 2.292, 2.993, 5.298, 7.6012}}),
-                                           Builder {}
-                                               .input({{8}, element::i32, std::vector<int32_t> {1, 2, 3, 4, 5, 10, 100, 1000}})
-                                               .expected({{8}, element::i32, std::vector<int32_t> {0, 1, 2, 2, 2, 3, 5, 8}}),
-                                           Builder {}
-                                               .input({{8}, element::u32, std::vector<uint32_t> {1, 2, 3, 4, 5, 10, 100, 1000}})
-                                               .expected({{8}, element::u32, std::vector<uint32_t> {0, 1, 2, 2, 2, 3, 5, 8}})),
-                         ReferenceAcoshLayerTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(
+    smoke_Acosh_With_Hardcoded_Refs, ReferenceAcoshLayerTest,
+    ::testing::Values(Builder {}
+                          .input({{8}, element::f16, std::vector<ngraph::float16> {1.f, 2.f, 3.f, 4.f, 5.f, 10.f, 100.f, 1000.f}})
+                          .expected({{8}, element::f16, std::vector<ngraph::float16> {0., 1.317, 1.763, 2.063, 2.292, 2.993, 5.298, 7.6012}}),
+                      Builder {}
+                          .input({{8}, element::f32, std::vector<float> {1.f, 2.f, 3.f, 4.f, 5.f, 10.f, 100.f, 1000.f}})
+                          .expected({{8}, element::f32, std::vector<float> {0., 1.317, 1.763, 2.063, 2.292, 2.993, 5.298, 7.6012}}),
+                      Builder {}
+                          .input({{8}, element::i32, std::vector<int32_t> {1, 2, 3, 4, 5, 10, 100, 1000}})
+                          .expected({{8}, element::i32, std::vector<int32_t> {0, 1, 2, 2, 2, 3, 5, 8}}),
+                      Builder {}
+                          .input({{8}, element::i64, std::vector<int64_t> {1, 2, 3, 4, 5, 10, 100, 1000}})
+                          .expected({{8}, element::i64, std::vector<int64_t> {0, 1, 2, 2, 2, 3, 5, 8}}),
+                      Builder {}
+                          .input({{8}, element::u32, std::vector<uint32_t> {1, 2, 3, 4, 5, 10, 100, 1000}})
+                          .expected({{8}, element::u32, std::vector<uint32_t> {0, 1, 2, 2, 2, 3, 5, 8}}),
+                      Builder {}
+                          .input({{8}, element::u64, std::vector<uint64_t> {1, 2, 3, 4, 5, 10, 100, 1000}})
+                          .expected({{8}, element::u64, std::vector<uint64_t> {0, 1, 2, 2, 2, 3, 5, 8}})),
+    ReferenceAcoshLayerTest::getTestCaseName);
