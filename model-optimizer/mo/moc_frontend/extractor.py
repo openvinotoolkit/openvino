5f6d234a85ad7ba65bc8ba95724fd7ce4b548166
Michael Nosov <mikhail.nosov@intel.com>
2021-07-19 16:17:57 +0300

    Fix review comments

--- model-optimizer/mo/moc_frontend/extractor.py
+++ model-optimizer/mo/moc_frontend/extractor.py
@@ -22,46 +22,39 @@ def decode_name_with_port(input_model: InputModel, node_name: str):
     found_nodes = []
     found_node_names = []
 
-    def add_found_node(found_nodes: list, node: Place):
-        """
-        Helper function to add Place node to list of found nodes. Adds only if node is not None
-        :param found_nodes List[Place]: list of found nodes
-        :param node Place: Place node to add to list
-        """
-        found_nodes.append(node) if node else None
-
     node = input_model.get_place_by_tensor_name(node_name)
-    found_node_names.append('Tensor:' + node_name)
-    add_found_node(found_nodes, node)
+    found_node_names.append('Tensor:' + node_name) if node else None
+    found_nodes.append(node) if node else None
 
     node = input_model.get_place_by_operation_name(node_name)
-    add_found_node(found_nodes, node)
-    found_node_names.append('Operation:' + node_name)
+    found_node_names.append('Operation:' + node_name) if node else None
+    found_nodes.append(node) if node else None
 
     regexp_post = r'(.+):(\d+)'
     match_post = re.search(regexp_post, node_name)
     if match_post:
-        found_node_names.append(match_post.group(1))
         node_post = input_model.get_place_by_operation_name(match_post.group(1))
-        add_found_node(found_nodes, node_post.get_output_port(
-            outputPortIndex=int(match_post.group(2)))) if node_post else None
+        node_post = node_post.get_output_port(
+            outputPortIndex=int(match_post.group(2))) if node_post else None
+        found_node_names.append(match_post.group(1)) if node_post else None
+        found_nodes.append(node_post) if node_post else None
 
     regexp_pre = r'(\d+):(.+)'
     match_pre = re.search(regexp_pre, node_name)
     if match_pre:
-        found_node_names.append(match_pre.group(2))
         node_pre = input_model.get_place_by_operation_name(match_pre.group(2))
-        add_found_node(found_nodes, node_pre.get_input_port(
-            inputPortIndex=int(match_pre.group(1)))) if node_pre else None
+        node_pre = node_pre.get_input_port(
+            inputPortIndex=int(match_pre.group(1))) if node_pre else None
+        found_node_names.append(match_pre.group(2)) if node_pre else None
+        found_nodes.append(node_pre) if node_pre else None
 
     if len(found_nodes) == 0:
         raise_no_node(node_name)
 
     # Check that there is no collision, all found places shall point to same data
-    if len(found_nodes) > 1:
-        all_equal = all([n.is_equal_data(found_nodes[0]) for n in found_nodes])
-        if not all_equal:
-            raise_node_name_collision(node_name, found_node_names)
+    all_equal = all([n.is_equal_data(found_nodes[0]) for n in found_nodes])
+    if not all_equal:
+        raise_node_name_collision(node_name, found_node_names)
 
     # TODO: ONNX specific (59408)
     # To comply with legacy behavior, for ONNX-only there shall be considered additional 2 possibilities
