e65e32cb5c8352a21a63baad0152a13af84d52d0
dkozykowski <dawid.kozykowski@intel.com>
2021-07-19 15:11:51 +0200

    migrate mod operation test o typed template test

--- ngraph/test/visitors/op/binary_ops.hpp
+++ ngraph/test/visitors/op/binary_ops.hpp
@@ -4,40 +4,20 @@
 
 #pragma once
 
-#include <vector>
 #include "gtest/gtest.h"
-#include "ngraph/ngraph.hpp"
-
-#include "ngraph/op/util/attr_types.hpp"
 #include "util/visitor.hpp"
 
-using namespace ngraph;
-using ngraph::test::NodeBuilder;
-template <typename T, element::Type_t ELEMENT_TYPE>
+template <typename T, ngraph::element::Type_t ELEMENT_TYPE>
 class BinaryOperatorType
 {
 public:
     using op_type = T;
-    static constexpr element::Type_t element_type = ELEMENT_TYPE;
+    static constexpr ngraph::element::Type_t element_type = ELEMENT_TYPE;
 };
+
 template <typename T>
 class BinaryOperatorVisitor : public testing::Test
 {
-};
-
-class BinaryOperatorTypeName
-{
-public:
-    template <typename T>
-    static std::string GetName(int)
-    {
-        using OP_Type = typename T::op_type;
-        constexpr element::Type precision(T::element_type);
-        const ngraph::Node::type_info_t typeinfo = OP_Type::get_type_info_static();
-        std::string op_name{typeinfo.name};
-        op_name.append("_");
-        return (op_name.append(precision.get_type_name()));
-    }
 };
 
 TYPED_TEST_SUITE_P(BinaryOperatorVisitor);
@@ -45,15 +25,21 @@ TYPED_TEST_SUITE_P(BinaryOperatorVisitor);
 TYPED_TEST_P(BinaryOperatorVisitor, No_Attribute_4D)
 {
     using OP_Type = typename TypeParam::op_type;
-    const element::Type_t element_type = TypeParam::element_type;
+    const ngraph::element::Type_t element_type = TypeParam::element_type;
 
-    NodeBuilder::get_ops().register_factory<OP_Type>();
-    const auto A = std::make_shared<op::Parameter>(element_type, PartialShape{2, 2, 2, 2});
+    ngraph::test::NodeBuilder::get_ops().register_factory<OP_Type>();
+    const auto A =
+        std::make_shared<ngraph::op::Parameter>(element_type, ngraph::PartialShape{1, 2, 3});
+    const auto B =
+        std::make_shared<ngraph::op::Parameter>(element_type, ngraph::PartialShape{3, 2, 1});
 
-    const auto op_func = std::make_shared<OP_Type>(A);
-    NodeBuilder builder(op_func);
-    const auto expected_attr_count = 0;
-    EXPECT_EQ(builder.get_value_map_size(), expected_attr_count);
+    auto auto_broadcast = ngraph::op::AutoBroadcastType::NUMPY;
+
+    const auto op_func = std::make_shared<OP_Type>(A, B, auto_broadcast);
+    ngraph::test::NodeBuilder builder(op_func);
+    const auto g_op_func = ngraph::as_type_ptr<OP_Type>(builder.create());
+
+    EXPECT_EQ(op_func->get_autob(), g_op_func->get_autob());
 }
 
 REGISTER_TYPED_TEST_SUITE_P(BinaryOperatorVisitor, No_Attribute_4D);
