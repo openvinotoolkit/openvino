06d79508cf8b7abc840d66dbef3bfca37f3c86e5
Artyom Anokhov <artyom.anokhov@intel.com>
2021-07-19 18:38:54 +0300

    Migrate Deployment Manager tool to Open Source (#6692)
    * Migrate Deployment Manager tool to Open Source
    
    * scripts/CMakeLists.txt: Added install rules for deployment manager

---
+++ scripts/deployment_manager/deployment_manager.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+ Copyright (c) 2018-2021 Intel Corporation
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
+
+import sys
+
+if sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] < 6):
+    exit("Python* 3.6 or higher is required to run the Deployment Manager.")
+
+if __name__ == '__main__':
+    from deployman.main import main
+    sys.exit(main())
