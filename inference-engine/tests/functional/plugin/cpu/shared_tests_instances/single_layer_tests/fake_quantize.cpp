258e50ebeddc193550fca32cb3fdc08be3300fa9
Patryk Elszkowski <patryk.elszkowski@intel.com>
2021-07-19 11:20:54 +0200

    FakeQuantize - reference implementation refactor (#6478)
    * new implementation of FakeQuantize
    
    * move FakeQuantize backend test to fake_quantize.in.cpp
    
    * enable some test for FakeQuantize
    
    * Add support for PDPD broadcasting and some backend tests
    
    * arm plugin expect less attributes in function call
    
    * try to fix arm plugin build
    
    * try to build arm plugin
    
    * start changing backend test
    
    * add check for attributes number in visitor test
    
    * fix backend test after refactoring
    
    * add default parameter value to runtime::reference::fake_quantize
    
    * Revert "add default parameter value to runtime::reference::fake_quantize"
    
    This reverts commit 9d2c00d9674d0cdddac33d097ff46f527abc7bd9.
    
    * add SLT for FakeQuantize
    
    * remove fesetround
    
    * change `v1` namesapce to `v0` in ref impl
    
    * add FakeQuantize-1 to VERIFIED_OP_REFERENCES
    
    * pass immutable reference to test functions

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/single_layer_tests/fake_quantize.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/single_layer_tests/fake_quantize.cpp
@@ -11,10 +11,18 @@ using namespace LayerTestsDefinitions;
 
 namespace {
 
-const std::vector<InferenceEngine::Precision> netPrecisions = {
-        InferenceEngine::Precision::FP32,
-        InferenceEngine::Precision::FP16
+const ngraph::op::AutoBroadcastSpec numpyBroadcast = ngraph::op::AutoBroadcastType::NUMPY;
+
+const ngraph::op::AutoBroadcastSpec noneBroadcast = ngraph::op::AutoBroadcastType::NONE;
+
+const std::vector<ngraph::op::AutoBroadcastSpec> broadcasts = {
+    {ngraph::op::AutoBroadcastType::NUMPY},
+    {ngraph::op::AutoBroadcastType::PDPD, -1},
 };
+
+const std::vector<InferenceEngine::Precision>
+    netPrecisions = {InferenceEngine::Precision::FP32,
+                     InferenceEngine::Precision::FP16};
 
 const std::vector<std::vector<size_t>> inputShapes = {{1, 1}, {2, 6}, {1, 1, 1}, {2, 6, 13},
                                                       {1, 1, 1, 1}, {3, 10, 5, 6}, {2, 8, 5, 18}, {2, 16, 3, 18}, {3, 49, 5, 6},
@@ -30,10 +38,11 @@ const auto fqParams = ::testing::Combine(
         ::testing::ValuesIn(levels),
         ::testing::ValuesIn(constShapes),
         ::testing::Values(fqArgs),
-        ::testing::Values(inputParams)
+        ::testing::Values(inputParams),
+        ::testing::ValuesIn(broadcasts)
 );
 
-INSTANTIATE_TEST_SUITE_P(smoke_FakeQuantize, FakeQuantizeLayerTest,
+INSTANTIATE_TEST_SUITE_P(smoke_FakeQuantize, FakeQuantizeLayerTestRevise,
                         ::testing::Combine(
                                 fqParams,
                                 ::testing::ValuesIn(netPrecisions),
@@ -44,27 +53,52 @@ INSTANTIATE_TEST_SUITE_P(smoke_FakeQuantize, FakeQuantizeLayerTest,
                                 ::testing::ValuesIn(inputShapes),
                                 ::testing::Values(CommonTestUtils::DEVICE_CPU),
                                 ::testing::Values(config)),
-                        FakeQuantizeLayerTest::getTestCaseName);
+                        FakeQuantizeLayerTestRevise::getTestCaseName);
+
+
+const std::vector<size_t> singleShape = {3, 4, 2, 5};
+const auto noneBroadcastFqParams = ::testing::Combine(
+        ::testing::ValuesIn(levels),
+        ::testing::Values(singleShape),
+        ::testing::Values(fqArgs),
+        ::testing::Values(inputParams),
+        ::testing::Values(noneBroadcast)
+);
+
+INSTANTIATE_TEST_SUITE_P(smoke_FakeQuantizeNoneBroadcast, FakeQuantizeLayerTestRevise,
+                        ::testing::Combine(
+                                noneBroadcastFqParams,
+                                ::testing::ValuesIn(netPrecisions),
+                                ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
+                                ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
+                                ::testing::Values(InferenceEngine::Layout::ANY),
+                                ::testing::Values(InferenceEngine::Layout::ANY),
+                                ::testing::Values(singleShape),
+                                ::testing::Values(CommonTestUtils::DEVICE_CPU),
+                                ::testing::Values(config)),
+                        FakeQuantizeLayerTestRevise::getTestCaseName);
 
 const std::vector<std::vector<size_t>> inputShapesPerChannel = {{11, 10, 22, 19}, {11, 10, 5, 6}};
 const std::vector<std::vector<size_t>> constShapesPerChannelAxis0 = {{11, 1, 1, 1}};
-const std::vector<std::vector<size_t>> constShapesPerChannelAxis1 = {{1, 10, 1, 1}};
+const std::vector<std::vector<size_t>> constShapesPerChannelAxis1 = {{1, 10, 1, 1}, {10, 1, 1}};
 
 const auto fqParamsPerChannelAxis0 = ::testing::Combine(
         ::testing::ValuesIn(levels),
         ::testing::ValuesIn(constShapesPerChannelAxis0),
         ::testing::Values(fqArgs),
-        ::testing::Values(inputParams)
+        ::testing::Values(inputParams),
+        ::testing::Values(numpyBroadcast)
 );
 
 const auto fqParamsPerChannelAxis1 = ::testing::Combine(
         ::testing::ValuesIn(levels),
         ::testing::ValuesIn(constShapesPerChannelAxis1),
         ::testing::Values(fqArgs),
-        ::testing::Values(inputParams)
+        ::testing::Values(inputParams),
+        ::testing::Values(numpyBroadcast)
 );
 
-INSTANTIATE_TEST_SUITE_P(smoke_FakeQuantizePerChannelAxis0, FakeQuantizeLayerTest,
+INSTANTIATE_TEST_SUITE_P(smoke_FakeQuantizePerChannelAxis0, FakeQuantizeLayerTestRevise,
                         ::testing::Combine(
                                 fqParamsPerChannelAxis0,
                                 ::testing::ValuesIn(netPrecisions),
@@ -75,9 +109,9 @@ INSTANTIATE_TEST_SUITE_P(smoke_FakeQuantizePerChannelAxis0, FakeQuantizeLayerTes
                                 ::testing::ValuesIn(inputShapesPerChannel),
                                 ::testing::Values(CommonTestUtils::DEVICE_CPU),
                                 ::testing::Values(config)),
-                        FakeQuantizeLayerTest::getTestCaseName);
+                        FakeQuantizeLayerTestRevise::getTestCaseName);
 
-INSTANTIATE_TEST_SUITE_P(smoke_FakeQuantizePerChannelAxis1, FakeQuantizeLayerTest,
+INSTANTIATE_TEST_SUITE_P(smoke_FakeQuantizePerChannelAxis1, FakeQuantizeLayerTestRevise,
                         ::testing::Combine(
                                 fqParamsPerChannelAxis1,
                                 ::testing::ValuesIn(netPrecisions),
@@ -88,7 +122,7 @@ INSTANTIATE_TEST_SUITE_P(smoke_FakeQuantizePerChannelAxis1, FakeQuantizeLayerTes
                                 ::testing::ValuesIn(inputShapesPerChannel),
                                 ::testing::Values(CommonTestUtils::DEVICE_CPU),
                                 ::testing::Values(config)),
-                        FakeQuantizeLayerTest::getTestCaseName);
+                        FakeQuantizeLayerTestRevise::getTestCaseName);
 
 const std::vector<std::vector<size_t>> inputShapesPerChannel2D = {{1, 10}};
 const std::vector<std::vector<size_t>> constShapesPerChannel2D = { {10}, {1, 10}, {1} };
@@ -96,10 +130,11 @@ const auto fqParamsPerChannel2D = ::testing::Combine(
     ::testing::ValuesIn(levels),
     ::testing::ValuesIn(constShapesPerChannel2D),
     ::testing::Values(fqArgs),
-    ::testing::Values(inputParams)
+    ::testing::Values(inputParams),
+    ::testing::Values(numpyBroadcast)
 );
 
-INSTANTIATE_TEST_SUITE_P(smoke_FakeQuantizePerChannel2D, FakeQuantizeLayerTest,
+INSTANTIATE_TEST_SUITE_P(smoke_FakeQuantizePerChannel2D, FakeQuantizeLayerTestRevise,
     ::testing::Combine(
         fqParamsPerChannel2D,
         ::testing::ValuesIn(netPrecisions),
@@ -110,6 +145,6 @@ INSTANTIATE_TEST_SUITE_P(smoke_FakeQuantizePerChannel2D, FakeQuantizeLayerTest,
         ::testing::ValuesIn(inputShapesPerChannel2D),
         ::testing::Values(CommonTestUtils::DEVICE_CPU),
         ::testing::Values(config)),
-    FakeQuantizeLayerTest::getTestCaseName);
+    FakeQuantizeLayerTestRevise::getTestCaseName);
 
 }  // namespace
