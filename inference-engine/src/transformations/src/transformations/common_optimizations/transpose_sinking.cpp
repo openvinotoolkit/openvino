openvinotoolkit/openvino

commit 606e79194d6c8cb8fdc145d1fe06f9cb856df9d4

Add transformations to optimize SR model (#5854)
* Add transformations to optimize SR model

* Add test for SplitSqueezeConcatFusion

* Add TransposeFuse tests

* Return TransposeOptimization renamed to TransposeToReshape

* Fix docstring

* Fix codestyle

* Fix build

* Fix GNA build

* Fix TransposeToReshape tests

* Fix test

* Temporarily disable cpu test

* Fix codestyle

* Fix test

* Fix test

* Enable SplitSqueezeConcatFusion

* Apply suggestions from code review

Co-authored-by: Gleb Kazantaev <gleb.nnstu@gmail.com>

* Apply review feedback

* Apply review feedback

* Update split_squeeze_concat_fusion.hpp

Co-authored-by: Gleb Kazantaev <gleb.nnstu@gmail.com>

inference-engine/src/transformations/src/transformations/common_optimizations/transpose_sinking.cpp
@@ -10,14 +10,15 @@
 #include <vector>
 
 #include <ngraph/opsets/opset6.hpp>
+#include <ngraph/opsets/opset7.hpp>
 #include <ngraph/rt_info.hpp>
 #include <ngraph/pattern/op/wrap_type.hpp>
 #include <numeric>
 
 NGRAPH_RTTI_DEFINITION(ngraph::pass::TransposeSinking, "TransposeSinking", 0);
-NGRAPH_RTTI_DEFINITION(ngraph::pass::TransposeOptimization, "TransposeOptimization", 0);
 NGRAPH_RTTI_DEFINITION(ngraph::pass::TransposeReduction, "TransposeReduction", 0);
 NGRAPH_RTTI_DEFINITION(ngraph::pass::TransposeFQReduction, "TransposeFQReduction", 0);
+NGRAPH_RTTI_DEFINITION(ngraph::pass::TransposeFuse, "TransposeFuse", 0);
 
 using namespace ngraph;
 
@@ -53,103 +54,6 @@ std::shared_ptr<ngraph::opset6::Constant> get_reversed_order_constant(const std:
 
     return std::make_shared<ngraph::opset6::Constant>(
             ngraph::element::i64, ngraph::Shape{reverse_order.size()}, reverse_order);
-}
-
-
-bool replace_transpose_with_reshape(const std::shared_ptr<Node>& transpose) {
-    auto data = transpose->input_value(0);
-    const auto input_shape = transpose->input(0).get_partial_shape();
-
-    const size_t input_shape_rank = input_shape.rank().get_length();
-
-    auto order = as_type_ptr<opset6::Constant>(transpose->input_value(1).get_node_shared_ptr());
-    if (!order || !ngraph::shape_size(order->get_shape())) {
-        return false;
-    }
-
-    const auto order_value = order->cast_vector<int64_t>();
-
-    // Check that transpose order without 1 dims has an ascending order
-    int64_t last_dim(-1);
-    for (size_t i = 0; i < input_shape_rank; ++i) {
-        if (input_shape[order_value[i]].is_dynamic() || input_shape[order_value[i]] != 1) {
-            if (order_value[i] < last_dim) {
-                return false;
-            }
-            last_dim = order_value[i];
-        }
-    }
-
-    // Transpose operation can be removed if original transpose order is sorted
-    // or dimension that changes their places equal to 1
-    using DimensionToPosition = struct {
-        Dimension dim;
-        size_t pos;
-    };
-    std::vector<DimensionToPosition> dims;
-    for (size_t i = 0; i < input_shape_rank; ++i) {
-        if (order_value[i] != static_cast<int64_t>(i)) {
-            dims.push_back({input_shape[order_value[i]], i});
-        }
-    }
-
-    // If number of dimensions != 1 to move equal to 0 we can remove this Transpose
-    if (count_if(dims.begin(), dims.end(), [](const DimensionToPosition& item) {
-        return !(item.dim.is_static() && item.dim.get_length() == 1);
-    }) == 0) {
-        return replace_output_update_name(transpose->output(0), transpose->input_value(0));
-    }
-
-    // Transpose can be replaced with Reshape in two ways:
-    // 1. Reshape with dims as Constant
-    // 2. Reshape with dims as input (ShapeOf->Gather)
-    //
-    // The first case is possible only if one or less dynamic dimensions changes their position
-    // For example: input_shape {?, 3, 1, ?} and order {0, 1, 3, 2} can be replaced with Reshape
-    // with Constant {0, 3, -1, 1} but if input_shape {?, 1, 1, ?} and order {1, 0, 3, 2} transpose
-    // cannot be replaced int the same way and in this case its only possible to use Gather(ShapeOf,
-    // order)
-
-    Output<Node> reshape_dim;
-    NodeVector new_ops;
-
-    if (count_if(dims.begin(), dims.end(), [](const DimensionToPosition& item) {
-        return item.dim.is_dynamic();
-    }) < 2) {
-        std::vector<int64_t> reshape_value(input_shape_rank, 0);
-        for (const auto& item : dims) {
-            reshape_value[item.pos] = item.dim.is_dynamic() ? -1 : item.dim.get_length();
-        }
-        reshape_dim =
-                opset3::Constant::create(element::i64, Shape{reshape_value.size()}, reshape_value);
-    } else {
-        auto shape_of = std::make_shared<opset3::ShapeOf>(data);
-        new_ops.push_back(shape_of);
-        reshape_dim = std::make_shared<opset3::Gather>(
-                shape_of, order, opset3::Constant::create(element::i64, Shape{1}, {0}));
-        new_ops.push_back(reshape_dim.get_node_shared_ptr());
-    }
-
-    auto reshape_op = std::make_shared<opset3::Reshape>(data, reshape_dim, true);
-    new_ops.push_back(reshape_op);
-
-    reshape_op->set_friendly_name(transpose->get_friendly_name());
-    copy_runtime_info(transpose, new_ops);
-    replace_node(transpose, reshape_op);
-    return true;
-}
-
-ngraph::pass::TransposeOptimization::TransposeOptimization() {
-    MATCHER_SCOPE(TransposeOptimization);
-
-    auto transpose_label = pattern::wrap_type<opset6::Transpose>(
-            {pattern::any_input(pattern::has_static_rank()), pattern::wrap_type<opset6::Constant>()});
-    ngraph::matcher_pass_callback matcher_pass_callback = [=](ngraph::pattern::Matcher &m) {
-        return replace_transpose_with_reshape(m.get_match_root());
-    };
-
-    auto m = std::make_shared<ngraph::pattern::Matcher>(transpose_label, matcher_name);
-    register_matcher(m, matcher_pass_callback);
 }
 
 ngraph::pass::TransposeReduction::TransposeReduction() {
@@ -269,5 +173,52 @@ ngraph::pass::TransposeFQReduction::TransposeFQReduction() {
     };
 
     auto m = std::make_shared<ngraph::pattern::Matcher>(reduce_or_squeeze_label, matcher_name);
+    register_matcher(m, matcher_pass_callback);
+}
+
+ngraph::pass::TransposeFuse::TransposeFuse() {
+    MATCHER_SCOPE(TransposeFuse);
+
+    auto transpose_1 = pattern::wrap_type<opset7::Transpose>({ pattern::any_input(), pattern::wrap_type<opset7::Constant>() }, pattern::consumers_count(1));
+    auto transpose_2 = pattern::wrap_type<opset7::Transpose>({ transpose_1, pattern::wrap_type<opset7::Constant>() });
+
+    ngraph::matcher_pass_callback matcher_pass_callback = [=](ngraph::pattern::Matcher& m) {
+        const auto& pattern_to_output = m.get_pattern_value_map();
+
+        auto transpose1 = pattern_to_output.at(transpose_1).get_node_shared_ptr();
+        auto transpose2 = pattern_to_output.at(transpose_2).get_node_shared_ptr();
+        auto input = transpose1->input_value(0);
+
+        auto transpose1_order = std::dynamic_pointer_cast<ngraph::opset7::Constant>(transpose1->get_input_node_shared_ptr(1));
+        auto transpose2_order = std::dynamic_pointer_cast<ngraph::opset7::Constant>(transpose2->get_input_node_shared_ptr(1));
+        if (!transpose1_order || !transpose2_order)
+            return false;
+
+        auto order1 = transpose1_order->cast_vector<int64_t>();
+        auto order2 = transpose2_order->cast_vector<int64_t>();
+        if (order1.size() != order2.size())
+            return false;
+
+        bool is_ordered = true;
+        for (size_t i = 0; i < order1.size(); i++) {
+            order2[i] = order1[order2[i]];
+            if (order2[i] != (int64_t)i)
+                is_ordered = false;
+        }
+
+        if (is_ordered) {
+            return ngraph::replace_output_update_name(transpose2->output(0), input);
+        } else {
+            auto new_order = ngraph::opset7::Constant::create(element::i64, {order2.size()}, order2);
+            auto new_transpose = register_new_node<ngraph::opset7::Transpose>(input, new_order);
+
+            ngraph::copy_runtime_info({ transpose1, transpose2 }, new_transpose);
+            ngraph::replace_node(transpose2, new_transpose);
+        }
+
+        return true;
+    };
+
+    auto m = std::make_shared<ngraph::pattern::Matcher>(transpose_2, matcher_name);
     register_matcher(m, matcher_pass_callback);
 }

inference-engine/src/transformations/include/transformations/common_optimizations/split_squeeze_concat_fusion.hpp
@@ -1,0 +1,33 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <vector>
+#include <memory>
+
+#include <transformations_visibility.hpp>
+
+#include <ngraph/ngraph.hpp>
+#include <ngraph/pass/graph_rewrite.hpp>
+#include "ngraph/pattern/matcher.hpp"
+
+namespace ngraph {
+namespace pass {
+
+class TRANSFORMATIONS_API SplitSqueezeConcatFusion;
+
+}  // namespace pass
+}  // namespace ngraph
+
+/**
+ * @ingroup ie_transformation_common_api
+ * @brief SplitSqueezeConcatFusion transformation replaces group of
+ * operations: Split -> Squeeze (multiple) -> Concat to Transpose -> Reshape ops.
+ */
+class ngraph::pass::SplitSqueezeConcatFusion : public ngraph::pass::MatcherPass {
+public:
+    NGRAPH_RTTI_DECLARATION;
+    SplitSqueezeConcatFusion();
+};

inference-engine/src/transformations/include/transformations/common_optimizations/transpose_sinking.hpp
@@ -17,22 +17,12 @@ namespace ngraph {
 namespace pass {
 
 class TRANSFORMATIONS_API TransposeSinking;
-class TRANSFORMATIONS_API TransposeOptimization;
 class TRANSFORMATIONS_API TransposeReduction;
 class TRANSFORMATIONS_API TransposeFQReduction;
+class TRANSFORMATIONS_API TransposeFuse;
 
 }  // namespace pass
 }  // namespace ngraph
-
-/**
- * @ingroup ie_transformation_common_api
- * @brief TransposeOptimization transformation replaces suitable Transposes with Reshape operation or optimises them out
- */
-class ngraph::pass::TransposeOptimization : public ngraph::pass::MatcherPass {
-public:
-    NGRAPH_RTTI_DECLARATION;
-    TransposeOptimization();
-};
 
 /**
  * @ingroup ie_transformation_common_api
@@ -56,6 +46,17 @@ public:
 
 /**
  * @ingroup ie_transformation_common_api
+ * @brief TransposeFuse transformation eliminates 2 consequtive Transposes if they result in no changes to input or fuses them
+ * to single Transpose if input gets changed
+ */
+class ngraph::pass::TransposeFuse : public ngraph::pass::MatcherPass {
+public:
+    NGRAPH_RTTI_DECLARATION;
+    TransposeFuse();
+};
+
+/**
+ * @ingroup ie_transformation_common_api
  * @brief TransposeSinking transformation sinks Transposes through known operations
  */
 class ngraph::pass::TransposeSinking: public ngraph::pass::GraphRewrite {
@@ -64,6 +65,6 @@ public:
     TransposeSinking() {
         add_matcher<ngraph::pass::TransposeFQReduction>();
         add_matcher<ngraph::pass::TransposeReduction>();
-        add_matcher<ngraph::pass::TransposeOptimization>();
+        add_matcher<ngraph::pass::TransposeFuse>();
     }
 };

inference-engine/src/transformations/include/transformations/common_optimizations/transpose_to_reshape.hpp
@@ -1,0 +1,32 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <vector>
+#include <memory>
+
+#include <transformations_visibility.hpp>
+
+#include <ngraph/ngraph.hpp>
+#include <ngraph/pass/graph_rewrite.hpp>
+#include "ngraph/pattern/matcher.hpp"
+
+namespace ngraph {
+namespace pass {
+
+class TRANSFORMATIONS_API TransposeToReshape;
+
+}  // namespace pass
+}  // namespace ngraph
+
+/**
+ * @ingroup ie_transformation_common_api
+ * @brief TransposeToReshape transformation replaces suitable Transposes with Reshape operation or optimizes them out
+ */
+class ngraph::pass::TransposeToReshape : public ngraph::pass::MatcherPass {
+public:
+    NGRAPH_RTTI_DECLARATION;
+    TransposeToReshape();
+};

inference-engine/src/transformations/src/transformations/common_optimizations/common_optimizations.cpp
@@ -41,6 +41,8 @@
 #include "transformations/common_optimizations/batch_to_space_fusion.hpp"
 #include "transformations/common_optimizations/dilated_convolution_converter.hpp"
 #include "transformations/common_optimizations/transpose_sinking.hpp"
+#include "transformations/common_optimizations/split_squeeze_concat_fusion.hpp"
+#include "transformations/common_optimizations/transpose_to_reshape.hpp"
 #include "transformations/op_conversions/bidirectional_sequences_decomposition.hpp"
 #include "transformations/op_conversions/convert_pad_to_group_conv.hpp"
 #include "transformations/op_conversions/convert_divide.hpp"
@@ -91,7 +93,13 @@ bool ngraph::pass::CommonOptimizations::run_on_function(std::shared_ptr<ngraph::
     manager.register_pass<ngraph::pass::ConstantFolding>();
     manager.register_pass<ngraph::pass::StridedSliceOptimization>(); // depends on CF
     manager.register_pass<ngraph::pass::BroadcastElementwiseFusion>();
-    manager.register_pass<ngraph::pass::TransposeSinking>();
+
+    auto transpose_sinking = manager.register_pass<ngraph::pass::GraphRewrite>();
+    transpose_sinking->add_matcher<ngraph::pass::TransposeSinking>();
+    // SplitSqueezeConcatFusion should work in same GraphRewrite as TransposesSinking,
+    // because it replaces pattern that may contain Transposes which must be optimized before
+    // the transformation and it also inserts Transpose that can be optimized by TransposeSinking
+    transpose_sinking->add_matcher<ngraph::pass::SplitSqueezeConcatFusion>();
 
     auto eliminations = manager.register_pass<ngraph::pass::GraphRewrite>();
     eliminations->add_matcher<ngraph::pass::EliminateUnsqueezeGather>();
@@ -119,6 +127,7 @@ bool ngraph::pass::CommonOptimizations::run_on_function(std::shared_ptr<ngraph::
     common_fusions->add_matcher<ngraph::pass::BatchToSpaceFusion>();
     common_fusions->add_matcher<ngraph::pass::DilatedConvolutionConverter>();
     common_fusions->add_matcher<ngraph::pass::GeluFusion>();
+    common_fusions->add_matcher<ngraph::pass::TransposeToReshape>();
     common_fusions->set_name("ngraph::pass::CommonFusions");
 
     manager.register_pass<ngraph::pass::ConvertPadToGroupConvolution, false>();

inference-engine/src/transformations/src/transformations/common_optimizations/split_squeeze_concat_fusion.cpp
@@ -1,0 +1,95 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "itt.hpp"
+#include "transformations/common_optimizations/split_squeeze_concat_fusion.hpp"
+
+#include <memory>
+#include <vector>
+#include <numeric>
+
+#include <ngraph/opsets/opset7.hpp>
+#include <ngraph/rt_info.hpp>
+#include <ngraph/pattern/op/wrap_type.hpp>
+
+NGRAPH_RTTI_DEFINITION(ngraph::pass::SplitSqueezeConcatFusion, "SplitSqueezeConcatFusion", 0);
+
+ngraph::pass::SplitSqueezeConcatFusion::SplitSqueezeConcatFusion() {
+    MATCHER_SCOPE(SplitSqueezeConcatFusion);
+    // Detect only concat, because we don't know how many inputs will go into concat
+    auto concat_pattern = ngraph::pattern::wrap_type<ngraph::opset7::Concat>();
+
+    ngraph::matcher_pass_callback callback = [=](ngraph::pattern::Matcher& m) {
+        const auto& pattern_to_output = m.get_pattern_value_map();
+        auto concat = std::dynamic_pointer_cast<ngraph::opset7::Concat>(pattern_to_output.at(concat_pattern).get_node_shared_ptr());
+        if (!concat) return false;
+
+        NodeVector nodes_to_delete{ concat };
+
+        int64_t axis_value = 0;
+        std::shared_ptr<ngraph::opset7::Split> split;
+
+        const auto& concat_inputs = concat->input_values();
+        if (concat_inputs.empty()) return false;
+        for (size_t i = 0; i < concat_inputs.size(); i++) {
+            auto squeeze = std::dynamic_pointer_cast<ngraph::opset7::Squeeze>(concat_inputs[i].get_node_shared_ptr());
+            if (!squeeze) return false;
+
+            nodes_to_delete.push_back(squeeze);
+
+            auto split_to_check = std::dynamic_pointer_cast<ngraph::opset7::Split>(squeeze->input_value(0).get_node_shared_ptr());
+            auto squeeze_axes = std::dynamic_pointer_cast<ngraph::opset7::Constant>(squeeze->input_value(1).get_node_shared_ptr());
+            if (!squeeze_axes || !split_to_check) return false;
+
+            auto squeeze_axes_vec = squeeze_axes->cast_vector<int64_t>();
+            if (squeeze_axes_vec.size() != 1) return false;
+
+            if (i == 0) {
+                axis_value = squeeze_axes_vec[0];
+                nodes_to_delete.push_back(split_to_check);
+                split = split_to_check;
+            } else if (axis_value != squeeze_axes_vec[0] || split_to_check != split) {
+                return false;
+            }
+
+            auto split_output = squeeze->input_value(0);
+            if (split_output.get_target_inputs().size() != 1 ||
+                split_output.get_index() != i)
+                return false;
+        }
+
+        if (split->get_num_splits() != concat_inputs.size()) return false;
+
+        auto split_axis = std::dynamic_pointer_cast<ngraph::opset7::Constant>(split->input_value(1).get_node_shared_ptr());
+        if (!split_axis) return false;
+
+        auto axis_vec = split_axis->cast_vector<int64_t>();
+        if (axis_vec.size() != 1 || axis_value != axis_vec[0])
+            return false;
+
+        auto input = split->input_value(0);
+
+        auto concat_axis = concat->get_axis();
+        auto rank = input.get_partial_shape().rank();
+        if (!rank.is_static())
+            return false;
+        std::vector<int64_t> order(rank.get_length());
+        std::iota(order.begin(), order.end(), 0);
+        order.erase(order.begin() + axis_value);
+        order.insert(order.begin() + concat_axis, axis_value);
+
+        auto transpose_order = ngraph::opset7::Constant::create(element::i64, { (size_t)rank.get_length() }, order);
+        auto transpose = register_new_node<ngraph::opset7::Transpose>(input, transpose_order);
+        auto shape_after = ngraph::opset7::Constant::create(element::i64, { (size_t)rank.get_length() - 1 }, concat->get_output_shape(0));
+        auto reshape = std::make_shared<ngraph::opset7::Reshape>(transpose, shape_after, false);
+
+        reshape->set_friendly_name(m.get_match_root()->get_friendly_name());
+        ngraph::copy_runtime_info(nodes_to_delete, { transpose, reshape });
+        ngraph::replace_node(m.get_match_root(), reshape);
+        return true;
+    };
+
+    auto m = std::make_shared<ngraph::pattern::Matcher>(concat_pattern, matcher_name);
+    register_matcher(m, callback);
+}

inference-engine/src/transformations/src/transformations/common_optimizations/transpose_to_reshape.cpp
@@ -1,0 +1,115 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "itt.hpp"
+#include "transformations/common_optimizations/transpose_to_reshape.hpp"
+#include "transformations/utils/utils.hpp"
+
+#include <memory>
+#include <vector>
+
+#include <ngraph/opsets/opset6.hpp>
+#include <ngraph/rt_info.hpp>
+#include <ngraph/pattern/op/wrap_type.hpp>
+#include <numeric>
+
+NGRAPH_RTTI_DEFINITION(ngraph::pass::TransposeToReshape, "TransposeToReshape", 0);
+
+using namespace ngraph;
+
+bool replace_transpose_with_reshape(const std::shared_ptr<Node>& transpose) {
+    auto data = transpose->input_value(0);
+    const auto input_shape = transpose->input(0).get_partial_shape();
+
+    const size_t input_shape_rank = input_shape.rank().get_length();
+
+    auto order = as_type_ptr<opset6::Constant>(transpose->input_value(1).get_node_shared_ptr());
+    if (!order || !ngraph::shape_size(order->get_shape())) {
+        return false;
+    }
+
+    const auto order_value = order->cast_vector<int64_t>();
+
+    // Check that transpose order without 1 dims has an ascending order
+    int64_t last_dim(-1);
+    for (size_t i = 0; i < input_shape_rank; ++i) {
+        if (input_shape[order_value[i]].is_dynamic() || input_shape[order_value[i]] != 1) {
+            if (order_value[i] < last_dim) {
+                return false;
+            }
+            last_dim = order_value[i];
+        }
+    }
+
+    // Transpose operation can be removed if original transpose order is sorted
+    // or dimension that changes their places equal to 1
+    using DimensionToPosition = struct {
+        Dimension dim;
+        size_t pos;
+    };
+    std::vector<DimensionToPosition> dims;
+    for (size_t i = 0; i < input_shape_rank; ++i) {
+        if (order_value[i] != static_cast<int64_t>(i)) {
+            dims.push_back({ input_shape[order_value[i]], i });
+        }
+    }
+
+    // If number of dimensions != 1 to move equal to 0 we can remove this Transpose
+    if (count_if(dims.begin(), dims.end(), [](const DimensionToPosition& item) {
+        return !(item.dim.is_static() && item.dim.get_length() == 1);
+        }) == 0) {
+        return replace_output_update_name(transpose->output(0), transpose->input_value(0));
+    }
+
+    // Transpose can be replaced with Reshape in two ways:
+    // 1. Reshape with dims as Constant
+    // 2. Reshape with dims as input (ShapeOf->Gather)
+    //
+    // The first case is possible only if one or less dynamic dimensions changes their position
+    // For example: input_shape {?, 3, 1, ?} and order {0, 1, 3, 2} can be replaced with Reshape
+    // with Constant {0, 3, -1, 1} but if input_shape {?, 1, 1, ?} and order {1, 0, 3, 2} transpose
+    // cannot be replaced int the same way and in this case its only possible to use Gather(ShapeOf,
+    // order)
+
+    Output<Node> reshape_dim;
+    NodeVector new_ops;
+
+    if (count_if(dims.begin(), dims.end(), [](const DimensionToPosition& item) {
+        return item.dim.is_dynamic();
+        }) < 2) {
+        std::vector<int64_t> reshape_value(input_shape_rank, 0);
+        for (const auto& item : dims) {
+            reshape_value[item.pos] = item.dim.is_dynamic() ? -1 : item.dim.get_length();
+        }
+        reshape_dim =
+            opset3::Constant::create(element::i64, Shape{ reshape_value.size() }, reshape_value);
+    } else {
+        auto shape_of = std::make_shared<opset3::ShapeOf>(data);
+        new_ops.push_back(shape_of);
+        reshape_dim = std::make_shared<opset3::Gather>(
+            shape_of, order, opset3::Constant::create(element::i64, Shape{ 1 }, { 0 }));
+        new_ops.push_back(reshape_dim.get_node_shared_ptr());
+    }
+
+    auto reshape_op = std::make_shared<opset3::Reshape>(data, reshape_dim, true);
+    new_ops.push_back(reshape_op);
+
+    reshape_op->set_friendly_name(transpose->get_friendly_name());
+    copy_runtime_info(transpose, new_ops);
+    replace_node(transpose, reshape_op);
+    return true;
+}
+
+ngraph::pass::TransposeToReshape::TransposeToReshape() {
+    MATCHER_SCOPE(TransposeToReshape);
+
+    auto transpose_label = pattern::wrap_type<opset6::Transpose>(
+        { pattern::any_input(pattern::has_static_rank()), pattern::wrap_type<opset6::Constant>() });
+    ngraph::matcher_pass_callback matcher_pass_callback = [=](ngraph::pattern::Matcher& m) {
+        return replace_transpose_with_reshape(m.get_match_root());
+    };
+
+    auto m = std::make_shared<ngraph::pattern::Matcher>(transpose_label, matcher_name);
+    register_matcher(m, matcher_pass_callback);
+}

inference-engine/tests/functional/inference_engine/transformations/algebraic_simplification.cpp
@@ -19,7 +19,7 @@
 #include <transformations/common_optimizations/algebraic_simplification.hpp>
 #include <transformations/utils/utils.hpp>
 #include <transformations/init_node_info.hpp>
-#include <transformations/common_optimizations/transpose_sinking.hpp>
+#include <transformations/common_optimizations/transpose_to_reshape.hpp>
 
 #include "common_test_utils/ngraph_test_utils.hpp"
 
@@ -312,7 +312,7 @@ TEST(algebraic_simplification, replace_transpose_with_reshape) {
 
         pass::Manager pass_manager;
         pass_manager.register_pass<pass::Validate>();
-        pass_manager.register_pass<pass::TransposeSinking>();
+        pass_manager.register_pass<pass::TransposeToReshape>();
         pass_manager.run_passes(optimized_f);
 
         auto ps = baseline_f->get_results()[0]->get_output_partial_shape(0);

commit 88156b03d738e6472f5d77b9c87bd9440c6a1a8c

Fix node name issue introduced by #5854

inference-engine/src/transformations/src/transformations/common_optimizations/transpose_sinking.cpp
@@ -212,8 +212,9 @@ ngraph::pass::TransposeFuse::TransposeFuse() {
             auto new_order = ngraph::opset7::Constant::create(element::i64, {order2.size()}, order2);
             auto new_transpose = register_new_node<ngraph::opset7::Transpose>(input, new_order);
 
+            new_transpose->set_friendly_name(m.get_match_root()->get_friendly_name());
             ngraph::copy_runtime_info({ transpose1, transpose2 }, new_transpose);
-            ngraph::replace_node(transpose2, new_transpose);
+            ngraph::replace_node(m.get_match_root(), new_transpose);
         }
 
         return true;