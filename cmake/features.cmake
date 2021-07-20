a63ef609d78e075817795328ae806b13fa599425
Ilya Lavrenov <ilya.lavrenov@intel.com>
2021-07-20 18:13:22 +0300

    Fixed usage of system protobuf (#6716)

--- cmake/features.cmake
+++ cmake/features.cmake
@@ -121,7 +121,6 @@ else()
 endif()
 
 ie_dependent_option(NGRAPH_ONNX_IMPORT_ENABLE "Enable ONNX importer" ON "protoc_available" OFF)
-ie_dependent_option(NGRAPH_ONNX_EDITOR_ENABLE "Enable ONNX Editor" ON "NGRAPH_ONNX_IMPORT_ENABLE" OFF)
 ie_dependent_option(NGRAPH_PDPD_FRONTEND_ENABLE "Enable PaddlePaddle FrontEnd" ON "protoc_available" OFF)
 ie_dependent_option(NGRAPH_USE_PROTOBUF_LITE "Compiles and links with protobuf-lite" OFF
     "NGRAPH_ONNX_IMPORT_ENABLE OR NGRAPH_PDPD_FRONTEND_ENABLE" OFF)
