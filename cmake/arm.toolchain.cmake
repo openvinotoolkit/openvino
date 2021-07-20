ba50e593c4670f5843ff22e340fe8ec9a0cfddc8
Aleksandr Voron <aleksandr.voron@intel.com>
2021-07-20 12:38:39 +0300

    Update arm.toolchain.cmake

--- cmake/arm.toolchain.cmake
+++ cmake/arm.toolchain.cmake
@@ -3,7 +3,7 @@
 #
 
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_SYSTEM_PROCESSOR armv6)
 
 set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
 set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
