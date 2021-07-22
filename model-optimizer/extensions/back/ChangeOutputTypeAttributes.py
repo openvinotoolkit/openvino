d9090eea27bf17142fb49b5e2bc2e960d72de2c4
Pavel Esir <pavel.esir@intel.com>
2021-07-22 11:04:06 +0300

    merged ChangeRangeOutputType.py, ChangeCastOutputType.py into a singe file

--- model-optimizer/extensions/back/ChangeOutputTypeAttributes.py
+++ model-optimizer/extensions/back/ChangeOutputTypeAttributes.py
@@ -7,14 +7,22 @@ import numpy as np
 
 from mo.back.replacement import BackReplacementPattern
 from mo.graph.graph import Graph
+from mo.graph.graph import Node
 from mo.middle.passes.convert_data_type import data_type_str_to_np
+from mo.utils.error import Error
+
+operations_with_data_type_attributes = {
+    'Cast': {'attr_name': 'dst_type', 'in_ports_to_check': (0,)},
+    'Range': {'attr_name': 'output_type', 'in_ports_to_check': (0, 1, 2)},
+}
 
 
 class ChangeOutputTypeAttributes(BackReplacementPattern):
     """
-    Change the Cast dst_type from fp64 to fp32 since not all plugins support fp64 data type.
-    Change the Cast dst_type from fp32 to fp16 when generating IR for fp16.
-    But leave fp32 if node returns shape value even if --data_type=FP16 (look extensions/back/MarkNodesWithShapeValues.py).
+    For operations that allow specify destination type by attribute this transformation
+    changes destination type from fp64 to fp32 since not all plugins support fp64 data type.
+    Changes destination type from fp32 to fp16 (and ensure that this is possible) when generating IR for fp16.
+    But leave fp32 if node returns shape value even if --data_type=FP16 (look extensions/back/MarkNodesWithShapeValues.py)
     """
     enabled = True
     force_shape_inference = True
@@ -27,17 +35,60 @@ class ChangeOutputTypeAttributes(BackReplacementPattern):
         return []
 
     def find_and_replace_pattern(self, graph: Graph):
-        for node in graph.get_op_nodes(op='Cast'):
-            if node.dst_type == np.float64:
-                log.warning('Change data type from {} to {} for node {}'.format(node.dst_type, np.float32, node.name))
-                node.dst_type = np.float32
+        ir_data_type = data_type_str_to_np(graph.graph['cmd_params'].data_type)
 
-            ir_data_type = data_type_str_to_np(node.graph.graph['cmd_params'].data_type)
-            if node.dst_type == np.float32 and ir_data_type == np.float16 and not node.has_and_set('returns_shape_value'):
-                log.warning('Change data type from {} to {} for node {}'.format(node.dst_type, ir_data_type, node.name))
-                node.dst_type = ir_data_type
-            elif node.has_and_set('returns_shape_value') and node.dst_type == np.float16:
-                # return back FP32 for all Convert nodes with shape values
-                log.warning('Change data type from {} to {} for node {} in ShapeOf subgraph'.
-                            format(node.dst_type, np.float32, node.name))
-                node.dst_type = np.float32
+        for op_name, op_map in operations_with_data_type_attributes.items():
+            dst_type = op_map['attr_name']
+            for node in graph.get_op_nodes(op=op_name):
+                node_name = node.soft_get('name', node.id)
+                assert node.has_valid(dst_type)
+
+                final_type = None
+                if node[dst_type] == np.float64:
+                    final_type = np.float32
+
+                if node[dst_type] in [np.float32, np.float64] and ir_data_type == np.float16 and \
+                        not node.has_and_set('returns_shape_value'):
+                    final_type = np.float16
+                elif node.has_and_set('returns_shape_value') and node.dst_type == np.float16:
+                    # return back FP32 for all nodes with shape values
+                    final_type = np.float32
+
+                if final_type is not None:
+                    node[dst_type] = final_type
+                    log.warning('Change data type from {} to {} for node {}'.format(node[dst_type], final_type,
+                                                                                    node_name))
+
+                if final_type == np.float16:
+                    assert_that_is_castable_to_fp16(node)
+
+
+def assert_that_is_castable_to_fp16(node: Node):
+    node_type = node.soft_get('type')
+    node_name = node.soft_get('name', node.id)
+
+    for i in operations_with_data_type_attributes[node_type]['in_ports_to_check']:
+        val = node.in_port(i).data.get_value()
+
+        if np.any(val > np.finfo(np.float16).max) or np.any(val < np.finfo(np.float16).min):
+            raise Error("Try to convert with --data_type=FP32 argument. "
+                        "This model can not be converted to FP16 precision, since "
+                        "'{}' node value {} exceeds FP16 allowed limits: [{}, {}]"
+                        .format(node_name, val, np.finfo(np.float16).min, np.finfo(np.float16).max))
+        # further this input values will be rewritten since force_shape_inference=True
+        node.in_port(i).data.set_value(val.astype(np.float16))
+
+    original_output = node.out_port(0).data.get_value()
+    node.infer(node)
+    casted_output = node.out_port(0).data.get_value()
+
+    if len(original_output) != len(casted_output):
+        raise Error("Try to convert with --data_type=FP32 argument. "
+                    "This model can not be converted to FP16 precision, since "
+                    "after '{}' node dtype to FP16 output shape {} differs from original {}."
+                    .format(node_name, len(casted_output), len(original_output)))
+
+    diff_count = np.count_nonzero(np.subtract(original_output, casted_output) > 1.e-4)
+    if diff_count > 0:
+        log.warning("{} elements of {} of Range node '{}' output differ from the original values while "
+                    "converting network to FP16 precision".format(diff_count, len(original_output), node_name))
