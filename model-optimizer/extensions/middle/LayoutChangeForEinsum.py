2aac2983ccb19b3db638691d5f4ad674cee32167
Roman Kazantsev <roman.kazantsev@intel.com>
2021-07-22 20:43:45 +0300

    Setup correct transformation dependencies for LayoutChangeForEinsum
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- model-optimizer/extensions/middle/LayoutChangeForEinsum.py
+++ model-optimizer/extensions/middle/LayoutChangeForEinsum.py
@@ -1,8 +1,8 @@
 # Copyright (C) 2018-2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from extensions.middle.InsertLayoutPropagationTransposes import InsertLayoutPropagationTranspose, \
-    is_input_data_in_correct_layout, is_output_data_in_correct_layout
+from extensions.middle.InsertLayoutPropagationTransposes import is_input_data_in_correct_layout, \
+    is_output_data_in_correct_layout
 from extensions.ops.einsum import Einsum
 from mo.graph.graph import Graph
 from mo.middle.replacement import MiddleReplacementPattern
@@ -24,10 +24,12 @@ class LayoutChangeForEinsum(MiddleReplacementPattern):
     graph_condition = [lambda graph: graph.graph['layout'] == 'NHWC']
 
     def run_after(self):
-        return [InsertLayoutPropagationTranspose]
+        from extensions.middle.MarkSubgraphsWithCorrectLayout import MarkSubGraphsWithCorrectLayout
+        return [MarkSubGraphsWithCorrectLayout]
 
     def run_before(self):
-        return []
+        from extensions.middle.InsertLayoutPropagationTransposes import InsertLayoutPropagationTranspose
+        return [InsertLayoutPropagationTranspose]
 
     def find_and_replace_pattern(self, graph: Graph):
         import extensions.middle.InsertLayoutPropagationTransposes as InsertTransposes
@@ -40,28 +42,30 @@ class LayoutChangeForEinsum(MiddleReplacementPattern):
             num_inputs = len(connected_in_ports)
 
             # check if correct_data_layout attribute is set for inputs and output
+            # this attribute can be set up within MarkSubgraphWithCorrectLayout transformation
+            # for example, when Einsum is located near to MatMul operation in a graph
             input_correct_layout_mask = []
             for input_ind in range(num_inputs):
                 input_correct_layout_mask.append(is_input_data_in_correct_layout(einsum, input_ind))
-            output_correct_layout_mask = is_output_data_in_correct_layout(einsum, 0)
+            is_output_layout_correct = is_output_data_in_correct_layout(einsum, 0)
 
-            # compute a mask of inputs of rank greater than 3 that are required original layout (NCHW)
-            # due to presence of ellipsis covering multiple tail dimensions in the corresponding input subscript
+            # compute a mask of which inputs/output are adjusted to the required layout
+            # if they are not adjusted, it means to require transpose
             input_ranks = [len(einsum.in_port(port_idx).data.get_shape()) for port_idx in range(num_inputs)]
             output_rank = len(einsum.out_port(0).data.get_shape())
-            permuted_equation, is_inputs_adjusted, is_output_adjusted = Einsum.adjust_equation_with_NCHW_layout(
+            permuted_equation, are_inputs_adjusted, is_output_adjusted = Einsum.adjust_equation_with_NCHW_layout(
                 einsum_name,
                 equation,
                 input_ranks,
-                output_rank, input_correct_layout_mask, output_correct_layout_mask)
-            assert len(is_inputs_adjusted) == num_inputs
+                output_rank, input_correct_layout_mask, is_output_layout_correct)
+            assert len(are_inputs_adjusted) == num_inputs
 
             # setup adjusted equation
             einsum.equation = permuted_equation
 
             # insert Transpose node to get NHWC layout back (for inputs) that is required due to specifics of equation
             for input_ind in range(num_inputs):
-                if not is_inputs_adjusted[input_ind]:
+                if not are_inputs_adjusted[input_ind]:
                     # that means Einsum can only accept input in NHWC layout
                     # so the inserted transpose before the Einsum will convert the layout to NHWC
                     InsertTransposes.insert_transpose(graph, einsum.in_port(input_ind), before_input=True)
