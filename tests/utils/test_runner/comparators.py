0beb23fd94242077bcb247d451cf8e0bc183515a
vurusovs <vitaliy.urusovskij@intel.com>
2021-07-23 19:03:37 +0300

    Add comparators.py int test_runner/

---
+++ tests/utils/test_runner/comparators.py
@@ -0,0 +1,72 @@
+import logging
+
+REFS_FACTOR = 1.2      # 120%
+
+
+class MetricsComparator:
+    executor = None
+
+    def __init__(self, values):
+        self.values = values
+
+    def raise_not_implemented(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def _set_executor(self, values: dict):
+        if self.executor:
+            return
+        else:
+            for executor in [TimeMetricsComparator, MemoryMetricsComparator]:
+                try:
+                    executor(self.values).compare_with(values)
+                    self.executor = executor(self.values)
+                    break
+                except:
+                    self.executor = None
+            if not self.executor:
+                self.raise_not_implemented()
+
+    def compare_with(self, values: dict):
+        self._set_executor(values)
+        getattr(self.executor, "compare_with", self.raise_not_implemented)(values)
+
+
+class TimeMetricsComparator:
+
+    def __init__(self, values):
+        self.status = 0
+        self.values = values
+
+    def compare_with(self, reference: dict):
+        """Compare values with provided reference"""
+        for step_name, references in reference.items():
+            for metric, reference_val in references.items():
+                if self.values[step_name][metric] > reference_val * REFS_FACTOR:
+                    logging.error("Comparison failed for '{}' step for '{}' metric. Reference: {}. Current values: {}"
+                                  .format(step_name, metric, reference_val, self.values[step_name][metric]))
+                    self.status = 1
+                else:
+                    logging.info("Comparison passed for '{}' step for '{}' metric. Reference: {}. Current values: {}"
+                                 .format(step_name, metric, reference_val, self.values[step_name][metric]))
+
+
+class MemoryMetricsComparator:
+
+    def __init__(self, values):
+        self.status = 0
+        self.values = values
+
+    def compare_with(self, reference: dict):
+        """Compare values with provided reference"""
+        for step_name, vm_records in reference.items():
+            for vm_metric, stat_metrics in vm_records.items():
+                for stat_metric_name, reference_val in stat_metrics.items():
+                    if self.values[step_name][vm_metric][stat_metric_name] > reference_val * REFS_FACTOR:
+                        logging.error(f"Comparison failed for '{step_name}' step for '{vm_metric}' for"
+                                      f" '{stat_metric_name}' metric. Reference: {reference_val}."
+                                      f" Current values: {self.values[step_name][vm_metric][stat_metric_name]}")
+                        self.status = 1
+                    else:
+                        logging.info(f"Comparison passed for '{step_name}' step for '{vm_metric}' for"
+                                     f" '{stat_metric_name}' metric. Reference: {reference_val}."
+                                     f" Current values: {self.values[step_name][vm_metric][stat_metric_name]}")
--- tests/utils/test_runner/test.py
+++ tests/utils/test_runner/test.py
@@ -21,8 +21,7 @@ import shutil
 
 from scripts.run_test import run_test
 from path_utils import expand_env_vars
-
-REFS_FACTOR = 1.2      # 120%
+from test_runner.comparators import MetricsComparator
 
 
 def test_timetest(instance, executable, niter, cl_cache_dir, model_cache_dir, test_info, temp_dir, validate_test_case,
@@ -72,16 +71,6 @@ def test_timetest(instance, executable, niter, cl_cache_dir, model_cache_dir, te
     test_info["raw_results"] = raw_stats
 
     # Compare with references
-    # TODO: implement additional comparison for memory values
-    comparison_status = 0
-    for step_name, references in instance["references"].items():
-        for metric, reference_val in references.items():
-            if aggr_stats[step_name][metric] > reference_val * REFS_FACTOR:
-                logging.error("Comparison failed for '{}' step for '{}' metric. Reference: {}. Current values: {}"
-                              .format(step_name, metric, reference_val, aggr_stats[step_name][metric]))
-                comparison_status = 1
-            else:
-                logging.info("Comparison passed for '{}' step for '{}' metric. Reference: {}. Current values: {}"
-                             .format(step_name, metric, reference_val, aggr_stats[step_name][metric]))
-
-    assert comparison_status == 0, "Comparison with references failed"
+    metrics_comparator = MetricsComparator(aggr_stats)
+    metrics_comparator.compare_with(instance["references"])
+    assert metrics_comparator.executor.status == 0, "Comparison with references failed"
