fc8b737ce9eaa002e595f77b3c6136e75bdda7e7
Roman Kazantsev <roman.kazantsev@intel.com>
2021-07-23 11:01:06 +0300

    Correct layout for Einsum inputs and output (#6696)
    * Fix recovery of output subscript in Einsum implicit mode
    
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>
    
    * Fix code style
    
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>
    
    * Correct layout adjustment for Einsum inputs and output
    
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>
    
    * Correct a comment in the unit-test
    
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>
    
    * Setup correct transformation dependencies for LayoutChangeForEinsum
    
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- model-optimizer/extensions/middle/LayoutChangeForEinsum.py
+++ model-optimizer/extensions/middle/LayoutChangeForEinsum.py
@@ -1,12 +1,14 @@
 # Copyright (C) 2018-2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+from extensions.middle.InsertLayoutPropagationTransposes import is_input_data_in_correct_layout, \
+    is_output_data_in_correct_layout
 from extensions.ops.einsum import Einsum
-from mo.back.replacement import BackReplacementPattern
 from mo.graph.graph import Graph
+from mo.middle.replacement import MiddleReplacementPattern
 
 
-class LayoutChangeForEinsum(BackReplacementPattern):
+class LayoutChangeForEinsum(MiddleReplacementPattern):
     """
     The transformation adjusts Einsum equation to NCHW layout.
     Subscripts for tensor of rank greater than three must be adjusted
@@ -19,7 +21,15 @@ class LayoutChangeForEinsum(BackReplacementPattern):
     """
     enabled = True
     force_shape_inference = True
-    graph_condition = [lambda graph: graph.graph['fw'] == 'tf']
+    graph_condition = [lambda graph: graph.graph['layout'] == 'NHWC']
+
+    def run_after(self):
+        from extensions.middle.MarkSubgraphsWithCorrectLayout import MarkSubGraphsWithCorrectLayout
+        return [MarkSubGraphsWithCorrectLayout]
+
+    def run_before(self):
+        from extensions.middle.InsertLayoutPropagationTransposes import InsertLayoutPropagationTranspose
+        return [InsertLayoutPropagationTranspose]
 
     def find_and_replace_pattern(self, graph: Graph):
         import extensions.middle.InsertLayoutPropagationTransposes as InsertTransposes
@@ -31,27 +41,35 @@ class LayoutChangeForEinsum(BackReplacementPattern):
             connected_in_ports = [port for port in einsum.in_ports().values() if not port.disconnected()]
             num_inputs = len(connected_in_ports)
 
-            # compute a mask of inputs of rank greater than 3 that are required original layout (NCHW)
-            # due to presence of ellipsis covering multiple tail dimensions in the corresponding input subscript
+            # check if correct_data_layout attribute is set for inputs and output
+            # this attribute can be set up within MarkSubgraphWithCorrectLayout transformation
+            # for example, when Einsum is located near to MatMul operation in a graph
+            input_correct_layout_mask = []
+            for input_ind in range(num_inputs):
+                input_correct_layout_mask.append(is_input_data_in_correct_layout(einsum, input_ind))
+            is_output_layout_correct = is_output_data_in_correct_layout(einsum, 0)
+
+            # compute a mask of which inputs/output are adjusted to the required layout
+            # if they are not adjusted, it means to require transpose
             input_ranks = [len(einsum.in_port(port_idx).data.get_shape()) for port_idx in range(num_inputs)]
             output_rank = len(einsum.out_port(0).data.get_shape())
-            permuted_equation, is_inputs_permuted, is_output_permuted = Einsum.adjust_equation_with_NCHW_layout(
+            permuted_equation, are_inputs_adjusted, is_output_adjusted = Einsum.adjust_equation_with_NCHW_layout(
                 einsum_name,
                 equation,
                 input_ranks,
-                output_rank)
-            assert len(is_inputs_permuted) == num_inputs
+                output_rank, input_correct_layout_mask, is_output_layout_correct)
+            assert len(are_inputs_adjusted) == num_inputs
 
             # setup adjusted equation
             einsum.equation = permuted_equation
 
             # insert Transpose node to get NHWC layout back (for inputs) that is required due to specifics of equation
             for input_ind in range(num_inputs):
-                if not is_inputs_permuted[input_ind]:
+                if not are_inputs_adjusted[input_ind]:
                     # that means Einsum can only accept input in NHWC layout
                     # so the inserted transpose before the Einsum will convert the layout to NHWC
                     InsertTransposes.insert_transpose(graph, einsum.in_port(input_ind), before_input=True)
-            if not is_output_permuted:
+            if not is_output_adjusted:
                 # that means Einsum can only generate output in NHWC layout
                 # so the inserted transpose followed after the output will convert the layout back into NCHW layout
                 InsertTransposes.insert_transpose(graph, einsum.out_port(0), before_input=False)
