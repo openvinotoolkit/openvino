d6f74781d76925ce77cc8fc6a9fdc644ceef5955
y <ilya.lavrenov@intel.com>
2021-07-26 00:20:26 +0300

    Fixed template containers for VPU

--- inference-engine/src/vpu/common/include/vpu/utils/containers.hpp
+++ inference-engine/src/vpu/common/include/vpu/utils/containers.hpp
@@ -11,24 +11,33 @@
 
 namespace vpu {
 
-template<class Key, class Value, template<class...> class Map>
-inline std::vector<Key> getKeys(const Map<Key, Value>& map) {
+template <template <typename, typename...> class Map,
+          typename Key,
+          typename Value,
+          typename... AddParams>
+inline std::vector<Key> getKeys(const Map<Key, Value, AddParams...>& map) {
     auto keys = std::vector<Key>{};
     keys.reserve(map.size());
     std::transform(map.cbegin(), map.cend(), std::back_inserter(keys), [](const std::pair<Key, Value>& entry) { return entry.first; });
     return keys;
 }
 
-template<class Key, class Value, template<class...> class Map>
-inline std::vector<Value> getValues(const Map<Key, Value>& map) {
+template <template <typename, typename...> class Map,
+          typename Key,
+          typename Value,
+          typename... AddParams>
+inline std::vector<Value> getValues(const Map<Key, Value, AddParams...>& map) {
     auto values = std::vector<Value>{};
     values.reserve(map.size());
     std::transform(map.cbegin(), map.cend(), std::back_inserter(values), [](const std::pair<Key, Value>& entry) { return entry.second; });
     return values;
 }
 
-template<class Key, class Value, template<class...> class Map>
-inline Map<Value, Key> inverse(const Map<Key, Value>& map) {
+template <template <typename, typename...> class Map,
+          typename Key,
+          typename Value,
+          typename... AddParams>
+inline Map<Value, Key> inverse(const Map<Key, Value, AddParams...>& map) {
     auto inverted = Map<Value, Key>{};
     for (const auto& entry : map) {
         const auto& insertion = inverted.emplace(entry.second, entry.first);
