9af44bff5b1602c540939026d7fbaa0bb13442fa
pszmel <piotr.szmelczynski@intel.com>
2021-07-26 14:13:18 +0200

    create util function for type_prop tests

--- ngraph/test/type_prop/logical_and.cpp
+++ ngraph/test/type_prop/logical_and.cpp
@@ -9,110 +9,55 @@
 using namespace std;
 using namespace ngraph;
 
+namespace {
+    void incorrect_init(const ngraph::element::Type& type, const std::string& err, const Shape& shape1 = {1, 3, 6}, const Shape& shape2 = {1, 3, 6}) {
+        auto input1 = make_shared<op::Parameter>(type, shape1);
+        auto input2 = make_shared<op::Parameter>(type, shape2);
+        try
+        {
+            auto logical_and = make_shared<op::v1::LogicalAnd>(input1, input2);
+        }
+        catch (const NodeValidationFailure& error)
+        {
+            EXPECT_HAS_SUBSTRING(error.what(), err);
+        }
+    }
+}
+
 TEST(type_prop, logical_and_incorrect_type_f32)
 {
-    auto input1 = make_shared<op::Parameter>(element::f32, Shape{1, 3, 6});
-    auto input2 = make_shared<op::Parameter>(element::f32, Shape{1, 3, 6});
-    try
-    {
-        auto logical_and = make_shared<op::v1::LogicalAnd>(input1, input2);
-    }
-    catch (const NodeValidationFailure& error)
-    {
-        EXPECT_HAS_SUBSTRING(error.what(),
-                             std::string("Operands for logical operators must have boolean element type but have element type f32"));
-    }
-
+    incorrect_init(element::f32, "Operands for logical operators must have boolean element type but have element type f32");
 }
 
 TEST(type_prop, logical_and_incorrect_type_f64)
 {
-    auto input1 = make_shared<op::Parameter>(element::f64, Shape{1, 3, 6});
-    auto input2 = make_shared<op::Parameter>(element::f64, Shape{1, 3, 6});
-    try
-    {
-        auto logical_and = make_shared<op::v1::LogicalAnd>(input1, input2);
-    }
-    catch (const NodeValidationFailure& error)
-    {
-        EXPECT_HAS_SUBSTRING(error.what(),
-                             std::string("Operands for logical operators must have boolean element type but have element type f64"));
-    }
+    incorrect_init(element::f64, "Operands for logical operators must have boolean element type but have element type f64");
 }
 
 TEST(type_prop, logical_and_incorrect_type_i32)
 {
-    auto input1 = make_shared<op::Parameter>(element::i32, Shape{1, 3, 6});
-    auto input2 = make_shared<op::Parameter>(element::i32, Shape{1, 3, 6});
-    try
-    {
-        auto logical_and = make_shared<op::v1::LogicalAnd>(input1, input2);
-    }
-    catch (const NodeValidationFailure& error)
-    {
-        EXPECT_HAS_SUBSTRING(error.what(),
-                             std::string("Operands for logical operators must have boolean element type but have element type i32"));
-    }
+    incorrect_init(element::i32, "Operands for logical operators must have boolean element type but have element type i32");
 }
 
 TEST(type_prop, logical_and_incorrect_type_i64)
 {
-    auto input1 = make_shared<op::Parameter>(element::i64, Shape{1, 3, 6});
-    auto input2 = make_shared<op::Parameter>(element::i64, Shape{1, 3, 6});
-    try
-    {
-        auto logical_and = make_shared<op::v1::LogicalAnd>(input1, input2);
-    }
-    catch (const NodeValidationFailure& error)
-    {
-        EXPECT_HAS_SUBSTRING(error.what(),
-                             std::string("Operands for logical operators must have boolean element type but have element type i64"));
-    }
+    incorrect_init(element::i64, "Operands for logical operators must have boolean element type but have element type i64");
 }
 
 TEST(type_prop, logical_and_incorrect_type_u32)
 {
-    auto input1 = make_shared<op::Parameter>(element::u32, Shape{1, 3, 6});
-    auto input2 = make_shared<op::Parameter>(element::u32, Shape{1, 3, 6});
-    try
-    {
-        auto logical_and = make_shared<op::v1::LogicalAnd>(input1, input2);
-    }
-    catch (const NodeValidationFailure& error)
-    {
-        EXPECT_HAS_SUBSTRING(error.what(),
-                             std::string("Operands for logical operators must have boolean element type but have element type u32"));
-    }
+    incorrect_init(element::u32, "Operands for logical operators must have boolean element type but have element type u32");
 }
 
 TEST(type_prop, logical_and_incorrect_type_u64)
 {
-    auto input1 = make_shared<op::Parameter>(element::u64, Shape{1, 3, 6});
-    auto input2 = make_shared<op::Parameter>(element::u64, Shape{1, 3, 6});
-    try
-    {
-        auto logical_and = make_shared<op::v1::LogicalAnd>(input1, input2);
-    }
-    catch (const NodeValidationFailure& error)
-    {
-        EXPECT_HAS_SUBSTRING(error.what(),
-                             std::string("Operands for logical operators must have boolean element type but have element type u64"));
-    }
+    incorrect_init(element::u64, "Operands for logical operators must have boolean element type but have element type u64");
+
 }
 
 TEST(type_prop, logical_and_incorrect_shape)
 {
-    auto input1 = make_shared<op::Parameter>(element::boolean, Shape{1, 3, 6});
-    auto input2 = make_shared<op::Parameter>(element::boolean, Shape{1, 2, 6});
-    try
-    {
-        auto logical_and = make_shared<op::v1::LogicalAnd>(input1, input2);
-    }
-    catch (const NodeValidationFailure& error)
-    {
-        EXPECT_HAS_SUBSTRING(error.what(),
-                             std::string("Argument shapes are inconsistent"));
-    }
+    incorrect_init(element::boolean, "Argument shapes are inconsistent", Shape {1, 3, 6}, Shape {1, 2, 3});
 }
 
 TEST(type_prop, logical_and_broadcast)
