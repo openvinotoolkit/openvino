3edc59b4d6764a8d88a747bdd83e17f875ce238a
Andrei Molotkov <andrei.molotkov@intel.com>
2021-07-26 10:02:21 +0300

    [GPU] Refactoring of pre_replace_deconv pass (#6218)

--- inference-engine/thirdparty/clDNN/src/program.cpp
+++ inference-engine/thirdparty/clDNN/src/program.cpp
@@ -834,13 +834,10 @@ void program_impl::swap_names(program_node& node1, program_node& node2) {
 }
 
 void program_impl::replace_all_usages(program_node& old_node, program_node& new_node) {
-    const std::list<program_node*> users(old_node.users);
-    auto itr = users.begin();
-    bool end = (itr == users.end());
-    while (!end) {
-        auto& usage = (*itr++);
-        end = (itr == users.end());
-        usage->replace_dependency(old_node, new_node);
+    auto itr = old_node.users.begin();
+    while (itr != old_node.users.end()) {
+        auto user = *(itr++);
+        user->replace_dependency(old_node, new_node);
     }
 }
 
