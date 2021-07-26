1dbfc6039d0d9e41203ede006515348248206ec1
Dawid Kożykowski <dawid.kozykowski@intel.com>
2021-07-26 14:46:31 +0200

    Refactor visitor tests for UnaryElementwiseArithmetic ops  (#6756)
    * remove namespace dependencies from header file
    
    * add RVO-exploit string concatenating
    
    * update acosh test
    
    * update erf test

--- ngraph/test/visitors/op/squeeze.cpp
+++ ngraph/test/visitors/op/squeeze.cpp
@@ -3,8 +3,9 @@
 //
 
 #include "unary_ops.hpp"
-using Types = ::testing::Types<UnaryOperatorType<ngraph::op::v0::Squeeze, element::f32>,
-                               UnaryOperatorType<ngraph::op::v0::Squeeze, element::f16>>;
+
+using Types = ::testing::Types<UnaryOperatorType<ngraph::op::v0::Squeeze, ngraph::element::f32>,
+                               UnaryOperatorType<ngraph::op::v0::Squeeze, ngraph::element::f16>>;
 
 INSTANTIATE_TYPED_TEST_SUITE_P(visitor_without_attribute,
                                UnaryOperatorVisitor,
