1331344217d449044b4e2fb679ee595d9db7d768
bszmelcz <bartosz.szmelczynski@intel.com>
2021-07-29 22:58:59 +0200

    refaactor template test for less_equal op

--- docs/template_plugin/tests/functional/op_reference/less_eq.cpp
+++ docs/template_plugin/tests/functional/op_reference/less_eq.cpp
@@ -8,108 +8,75 @@
 #include <ie_ngraph_utils.hpp>
 #include <ngraph/ngraph.hpp>
 #include <shared_test_classes/base/layer_test_utils.hpp>
-#include <tuple>
 
-#include "base_reference_test.hpp"
+#include "comparison.hpp"
 
 using namespace ngraph;
 using namespace InferenceEngine;
-using namespace reference_tests;
+using ComparisonTypes = ngraph::helpers::ComparisonTypes;
 
+namespace reference_tests {
+namespace ComparisonOpsRefTestDefinitions {
 namespace {
-struct LessEqualParams {
-    template <class IT, class OT>
-    LessEqualParams(const ngraph::PartialShape& input_shape1, const ngraph::PartialShape& input_shape2 , const ngraph::element::Type& iType,
-                const ngraph::element::Type& oType, const std::vector<IT>& iValues1, const std::vector<IT>& iValues2, const std::vector<OT>& oValues)
-        : pshape1(input_shape1), pshape2(input_shape2), inType(iType), outType(oType), inputData1(CreateBlob(iType, iValues1)),
-        inputData2(CreateBlob(iType, iValues2)), refData(CreateBlob(oType, oValues)) {}
-    ngraph::PartialShape pshape1;
-    ngraph::PartialShape pshape2;
-    ngraph::element::Type inType;
-    ngraph::element::Type outType;
-    InferenceEngine::Blob::Ptr inputData1;
-    InferenceEngine::Blob::Ptr inputData2;
-    InferenceEngine::Blob::Ptr refData;
-};
-
-class ReferenceLessEqualLayerTest : public testing::TestWithParam<LessEqualParams>, public CommonReferenceTest {
-public:
-    void SetUp() override {
-        auto params = GetParam();
-        function = CreateFunction(params.pshape1, params.pshape2, params.inType, params.outType);
-        inputData = {params.inputData1, params.inputData2};
-        refOutData = {params.refData};
-    }
-    static std::string getTestCaseName(const testing::TestParamInfo<LessEqualParams>& obj) {
-        auto param = obj.param;
-        std::ostringstream result;
-        result << "inpt_shape1=" << param.pshape1 << "_";
-        result << "inpt_shape2=" << param.pshape2 << "_";
-        result << "iType=" << param.inType << "_";
-        result << "oType=" << param.outType;
-        return result.str();
-    }
-
-private:
-    static std::shared_ptr<Function> CreateFunction(const PartialShape& input_shape1, const PartialShape& input_shape2, const element::Type& input_type,
-                                                    const element::Type& expected_output_type) {
-        const auto in = std::make_shared<op::Parameter>(input_type, input_shape1);
-        const auto in2 = std::make_shared<op::Parameter>(input_type, input_shape2);
-        const auto LessEqual = std::make_shared<op::v1::LessEqual>(in, in2);
-        return std::make_shared<Function>(NodeVector {LessEqual}, ParameterVector {in, in2});
-    }
-};
-
-TEST_P(ReferenceLessEqualLayerTest, CompareWithHardcodedRefs) {
+TEST_P(ReferenceComparisonLayerTest, LessEqualCompareWithHardcodedRefs) {
     Exec();
 }
 
 template <element::Type_t IN_ET>
-std::vector<LessEqualParams> generateLessEqualParams(const ngraph::element::Type& type) {
+std::vector<RefComparisonParams> generateComparisonParams(const element::Type& type) {
     using T = typename element_type_traits<IN_ET>::value_type;
-    std::vector<LessEqualParams> lessEqParams {
+    std::vector<RefComparisonParams> compParams {
         // 1D // 2D // 3D // 4D
-        LessEqualParams(ngraph::PartialShape {2, 2}, ngraph::PartialShape {2, 2}, type, ngraph::element::boolean,
-                std::vector<T> {0, 12, 23, 0},
-                std::vector<T> {0, 12, 23, 0},
-                std::vector<char> {1, 1, 1, 1}),
-        LessEqualParams(ngraph::PartialShape {2, 3}, ngraph::PartialShape {2, 3}, type, ngraph::element::boolean,
-                std::vector<T> {0, 6, 45, 1, 21, 21},
-                std::vector<T> {1, 18, 23, 1, 19, 21},
-                std::vector<char> {1, 1, 0, 1, 0, 1}),
-        LessEqualParams(ngraph::PartialShape {1}, ngraph::PartialShape {1},  type, ngraph::element::boolean,
-                std::vector<T> {53},
-                std::vector<T> {53},
-                std::vector<char> {1}),
-        LessEqualParams(ngraph::PartialShape {2, 4}, ngraph::PartialShape {2, 4}, type, ngraph::element::boolean,
-                std::vector<T> {0, 6, 25, 0, 11, 5, 11, 8},
-                std::vector<T> {1, 12, 23, 0, 10, 5, 13, 8},
-                std::vector<char> {1, 1, 0, 1, 0, 1, 1, 1}),
-        LessEqualParams(ngraph::PartialShape {3, 1, 2}, ngraph::PartialShape {1, 2, 1}, type, ngraph::element::boolean,
-                std::vector<T> {2, 1, 4, 1, 3, 1},
-                std::vector<T> {1, 1},
-                std::vector<char> {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1}),
-        LessEqualParams(ngraph::PartialShape {2, 1, 2, 1}, ngraph::PartialShape {1, 2, 1}, type, ngraph::element::boolean,
-                std::vector<T> {2, 1, 4, 1},
-                std::vector<T> {1, 1},
-                std::vector<char> {0, 1, 0, 1})};
-    return lessEqParams;
+        Builder {}
+            .compType(ComparisonTypes::LESS_EQUAL)
+            .input1({{2, 2}, type, std::vector<T> {0, 12, 23, 0}})
+            .input2({{2, 2}, type, std::vector<T> {0, 12, 23, 0}})
+            .expected({{2, 2}, element::boolean, std::vector<char> {1, 1, 1, 1}}),
+        Builder {}
+            .compType(ComparisonTypes::LESS_EQUAL)
+            .input1({{2, 3}, type, std::vector<T> {0, 6, 45, 1, 21, 21}})
+            .input2({{2, 3}, type, std::vector<T> {1, 18, 23, 1, 19, 21}})
+            .expected({{2, 3}, element::boolean, std::vector<char> {1, 1, 0, 1, 0, 1}}),
+        Builder {}
+            .compType(ComparisonTypes::LESS_EQUAL)
+            .input1({{1}, type, std::vector<T> {53}})
+            .input2({{1}, type, std::vector<T> {53}})
+            .expected({{1}, element::boolean, std::vector<char> {1}}),
+        Builder {}
+            .compType(ComparisonTypes::LESS_EQUAL)
+            .input1({{2, 4}, type, std::vector<T> {0, 12, 23, 0, 1, 5, 11, 8}})
+            .input2({{2, 4}, type, std::vector<T> {0, 12, 23, 0, 10, 5, 11, 8}})
+            .expected({{2, 4}, element::boolean, std::vector<char> {1, 1, 1, 1, 1, 1, 1, 1}}),
+        Builder {}
+            .compType(ComparisonTypes::LESS_EQUAL)
+            .input1({{3, 1, 2}, type, std::vector<T> {2, 1, 4, 1, 3, 1}})
+            .input2({{1, 2, 1}, type, std::vector<T> {1, 1}})
+            .expected({{3, 2, 2}, element::boolean, std::vector<char> {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1}}),
+        Builder {}
+            .compType(ComparisonTypes::LESS_EQUAL)
+            .input1({{2, 1, 2, 1}, type, std::vector<T> {2, 1, 4, 1}})
+            .input2({{1, 2, 1}, type, std::vector<T> {1, 1}})
+            .expected({{2, 1, 2, 1}, element::boolean, std::vector<char> {0, 1, 0, 1}})};
+    return compParams;
 }
 
-std::vector<LessEqualParams> generateLessEqualCombinedParams() {
-    const std::vector<std::vector<LessEqualParams>> LessEqualTypeParams {generateLessEqualParams<element::Type_t::f32>(ngraph::element::f32),
-                                                                         generateLessEqualParams<element::Type_t::f16>(ngraph::element::f16),
-                                                                         generateLessEqualParams<element::Type_t::i32>(ngraph::element::i32),
-                                                                         generateLessEqualParams<element::Type_t::u32>(ngraph::element::u32),
-                                                                         generateLessEqualParams<element::Type_t::u8>(ngraph::element::boolean)};
-    std::vector<LessEqualParams> combinedParams;
+std::vector<RefComparisonParams> generateComparisonCombinedParams() {
+    const std::vector<std::vector<RefComparisonParams>> compTypeParams {
+        generateComparisonParams<element::Type_t::f32>(element::f32),
+        generateComparisonParams<element::Type_t::f16>(element::f16),
+        generateComparisonParams<element::Type_t::i32>(element::i32),
+        generateComparisonParams<element::Type_t::u32>(element::u32),
+        generateComparisonParams<element::Type_t::u8>(element::boolean)};
+    std::vector<RefComparisonParams> combinedParams;
 
-    for (const auto& params : LessEqualTypeParams) {
+    for (const auto& params : compTypeParams) {
         combinedParams.insert(combinedParams.end(), params.begin(), params.end());
     }
     return combinedParams;
 }
 
-INSTANTIATE_TEST_SUITE_P(smoke_LessEqual_With_Hardcoded_Refs, ReferenceLessEqualLayerTest, ::testing::ValuesIn(generateLessEqualCombinedParams()),
-                                 ReferenceLessEqualLayerTest::getTestCaseName);
-}  // namespace
+} // namespace
+INSTANTIATE_TEST_SUITE_P(smoke_Comparison_With_Hardcoded_Refs, ReferenceComparisonLayerTest, ::testing::ValuesIn(generateComparisonCombinedParams()),
+                         ReferenceComparisonLayerTest::getTestCaseName);
+} // namespace ComparisonOpsRefTestDefinitions
+} // namespace reference_tests
