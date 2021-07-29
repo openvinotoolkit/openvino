openvinotoolkit/dldt

cover negative values for weight

inference-engine/tests/functional/plugin/cpu/single_layer_tests/convolution.cpp
@@ -84,8 +84,14 @@ protected:
         auto inputParams = ngraph::builder::makeParams(ngraph::element::f32, { inputShape });
         auto paramOuts = ngraph::helpers::convert2OutputVector(ngraph::helpers::castOps2Nodes<ngraph::op::Parameter>(inputParams));
 
+        size_t kernelShapeSize = convOutChannels * inputShape[1];
+        for (int i = 0; i < kernel.size(); i++) {
+            kernelShapeSize *= kernel[i];
+        }
+        std::vector<float> weights = generateRandom(kernelShapeSize);
+
         auto convolutionNode = ngraph::builder::makeConvolution(paramOuts.front(), ngPrc, kernel, stride, padBegin,
-            padEnd, dilation, padType, convOutChannels);
+            padEnd, dilation, padType, convOutChannels, false, weights);
 
         function = makeNgraphFunction(ngPrc, inputParams, convolutionNode, "Convolution");
     }

inference-engine/tests/functional/plugin/cpu/single_layer_tests/group_convolution.cpp
@@ -69,9 +69,16 @@ protected:
         auto params = ngraph::builder::makeParams(ngPrc, {inputShape});
         auto paramOuts = ngraph::helpers::convert2OutputVector(
                 ngraph::helpers::castOps2Nodes<ngraph::op::Parameter>(params));
+
+        size_t kernelShapeSize = convOutChannels * inputShape[1] / numGroups;
+        for (int i = 0; i < kernel.size(); i++) {
+            kernelShapeSize *= kernel[i];
+        }
+        std::vector<float> weights = generateRandom(kernelShapeSize);
+
         auto groupConv = std::dynamic_pointer_cast<ngraph::opset1::GroupConvolution>(
                 ngraph::builder::makeGroupConvolution(paramOuts[0], ngPrc, kernel, stride, padBegin,
-                                                      padEnd, dilation, padType, convOutChannels, numGroups));
+                                                      padEnd, dilation, padType, convOutChannels, numGroups, false, weights));
         function = makeNgraphFunction(ngPrc, params, groupConv, "groupConvolution");
     }
 };

inference-engine/tests/functional/shared_test_classes/src/single_layer/group_convolution.cpp
@@ -53,9 +53,14 @@ void GroupConvolutionLayerTest::SetUp() {
     auto params = ngraph::builder::makeParams(ngPrc, {inputShape});
     auto paramOuts = ngraph::helpers::convert2OutputVector(
             ngraph::helpers::castOps2Nodes<ngraph::op::Parameter>(params));
+    size_t kernelShapeSize = convOutChannels * inputShape[1] / numGroups;
+    for (int i = 0; i < kernel.size(); i++) {
+        kernelShapeSize *= kernel[i];
+    }
+    std::vector<float> weights = generateRandom(kernelShapeSize);
     auto groupConv = std::dynamic_pointer_cast<ngraph::opset1::GroupConvolution>(
             ngraph::builder::makeGroupConvolution(paramOuts[0], ngPrc, kernel, stride, padBegin,
-                                             padEnd, dilation, padType, convOutChannels, numGroups));
+                                             padEnd, dilation, padType, convOutChannels, numGroups, false, weights));
     ngraph::ResultVector results{std::make_shared<ngraph::opset1::Result>(groupConv)};
     function = std::make_shared<ngraph::Function>(results, params, "groupConvolution");
 }

openvinotoolkit/openvino

inference-engine/tests/functional/plugin/cpu/single_layer_tests/convolution.cpp
@@ -115,8 +115,14 @@ protected:
         auto inputParams = ngraph::builder::makeParams(ngraph::element::f32, { inputShape });
         auto paramOuts = ngraph::helpers::convert2OutputVector(ngraph::helpers::castOps2Nodes<ngraph::op::Parameter>(inputParams));
 
+        size_t kernelShapeSize = convOutChannels * inputShape[1];
+        for (int i = 0; i < kernel.size(); i++) {
+            kernelShapeSize *= kernel[i];
+        }
+        std::vector<float> weights = GenerateWeights(kernelShapeSize);
+
         auto convolutionNode = ngraph::builder::makeConvolution(paramOuts.front(), ngPrc, kernel, stride, padBegin,
-            padEnd, dilation, padType, convOutChannels);
+            padEnd, dilation, padType, convOutChannels, false, weights);
 
         function = makeNgraphFunction(ngPrc, inputParams, convolutionNode, "Convolution");
     }

inference-engine/tests/functional/plugin/cpu/single_layer_tests/group_convolution.cpp
@@ -100,9 +100,16 @@ protected:
         auto params = ngraph::builder::makeParams(ngPrc, {inputShape});
         auto paramOuts = ngraph::helpers::convert2OutputVector(
                 ngraph::helpers::castOps2Nodes<ngraph::op::Parameter>(params));
+
+        size_t kernelShapeSize = convOutChannels * inputShape[1] / numGroups;
+        for (int i = 0; i < kernel.size(); i++) {
+            kernelShapeSize *= kernel[i];
+        }
+        std::vector<float> weights = GenerateWeights(kernelShapeSize);
+
         auto groupConv = std::dynamic_pointer_cast<ngraph::opset1::GroupConvolution>(
                 ngraph::builder::makeGroupConvolution(paramOuts[0], ngPrc, kernel, stride, padBegin,
-                                                      padEnd, dilation, padType, convOutChannels, numGroups));
+                                                      padEnd, dilation, padType, convOutChannels, numGroups, false, weights));
         function = makeNgraphFunction(ngPrc, params, groupConv, "groupConvolution");
     }
 };

inference-engine/tests/functional/shared_test_classes/src/single_layer/group_convolution.cpp
@@ -53,9 +53,14 @@ void GroupConvolutionLayerTest::SetUp() {
     auto params = ngraph::builder::makeParams(ngPrc, {inputShape});
     auto paramOuts = ngraph::helpers::convert2OutputVector(
             ngraph::helpers::castOps2Nodes<ngraph::op::Parameter>(params));
+    size_t kernelShapeSize = convOutChannels * inputShape[1] / numGroups;
+    for (int i = 0; i < kernel.size(); i++) {
+        kernelShapeSize *= kernel[i];
+    }
+    std::vector<float> weights = GenerateWeights(kernelShapeSize);
     auto groupConv = std::dynamic_pointer_cast<ngraph::opset1::GroupConvolution>(
             ngraph::builder::makeGroupConvolution(paramOuts[0], ngPrc, kernel, stride, padBegin,
-                                             padEnd, dilation, padType, convOutChannels, numGroups));
+                                             padEnd, dilation, padType, convOutChannels, numGroups, false, weights));
     ngraph::ResultVector results{std::make_shared<ngraph::opset1::Result>(groupConv)};
     function = std::make_shared<ngraph::Function>(results, params, "groupConvolution");
 }