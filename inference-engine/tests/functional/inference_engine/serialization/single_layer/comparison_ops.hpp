b27a0670cf8429d27693bb6527ffc133c5258003
bszmelcz <bartosz.szmelczynski@intel.com>
2021-08-01 19:53:13 +0200

    add comparison hpp file and two examples

--- inference-engine/tests/functional/inference_engine/serialization/single_layer/comparison_ops.hpp
+++ inference-engine/tests/functional/inference_engine/serialization/single_layer/comparison_ops.hpp
@@ -2,33 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#pragma once
+
 #include "shared_test_classes/single_layer/comparison.hpp"
 
-using namespace LayerTestsDefinitions;
-using namespace LayerTestsDefinitions::ComparisonParams;
-
-static const std::map<std::vector<size_t>, std::vector<std::vector<size_t>>> inputShapes = {
-    {{1}, {{1}, {17}, {1, 1}, {2, 18}, {1, 1, 2}, {2, 2, 3}, {1, 1, 2, 3}}},
-    {{5}, {{1}, {1, 1}, {2, 5}, {1, 1, 1}, {2, 2, 5}}},
-    {{2, 200}, {{1}, {200}, {1, 200}, {2, 200}, {2, 2, 200}}},
-    {{1, 3, 20}, {{20}, {2, 1, 1}}},
-    {{2, 17, 3, 4}, {{4}, {1, 3, 4}, {2, 1, 3, 4}}},
-    {{2, 17, 3, 4}, {{4}, {1, 3, 4}, {141, 1, 3, 4}}},
-    {{2, 1, 1, 3, 1}, {{1}, {1, 3, 4}, {2, 1, 3, 4}, {1, 1, 1, 1, 1}}},
+struct ComparisionOpsData {
+    const std::map<std::vector<size_t>, std::vector<std::vector<size_t>>> inputShapes;
+    const std::vector<InferenceEngine::Precision> inputsPrecisions;
+    const std::vector<ngraph::helpers::InputLayerType> secondInputTypes;
+    const std::map<std::string, std::string> additional_config;
+    const ngraph::helpers::ComparisonTypes opType;
+    const InferenceEngine::Precision ieInputPrecision;
+    const InferenceEngine::Precision ieOutputPrecision;
+    const std::string deviceName;
 };
-
-static const std::vector<InferenceEngine::Precision> inputsPrecisions = {
-    InferenceEngine::Precision::FP64,
-    InferenceEngine::Precision::FP32,
-    InferenceEngine::Precision::FP16,
-    InferenceEngine::Precision::I32,
-    InferenceEngine::Precision::U32,
-    InferenceEngine::Precision::BOOL,
-};
-
-static const std::vector<ngraph::helpers::InputLayerType> secondInputTypes = {
-    ngraph::helpers::InputLayerType::CONSTANT,
-    ngraph::helpers::InputLayerType::PARAMETER,
-};
-
-static const std::map<std::string, std::string> additional_config = {};
--- inference-engine/tests/functional/inference_engine/serialization/single_layer/less_eq.cpp
+++ inference-engine/tests/functional/inference_engine/serialization/single_layer/less_eq.cpp
@@ -2,23 +2,57 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "comparison_ops.hpp"
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <vector>
+#include "shared_test_classes/single_layer/comparison.hpp"
+#include "common_test_utils/test_constants.hpp"
+
+using namespace LayerTestsDefinitions;
+using namespace LayerTestsDefinitions::ComparisonParams;
 
 namespace {
-TEST_P(ComparisonLayerTest, Serialize) {
-    Serialize();
+    TEST_P(ComparisonLayerTest, Serialize) {
+        Serialize();
     }
 
-const auto SerializeLessEqualTestParams = ::testing::Combine(
-    ::testing::ValuesIn(CommonTestUtils::combineParams(inputShapes)),
-    ::testing::ValuesIn(inputsPrecisions),
-    ::testing::Values(ngraph::helpers::ComparisonTypes::LESS_EQUAL),
-    ::testing::ValuesIn(secondInputTypes),
-    ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
-    ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
-    ::testing::Values(CommonTestUtils::DEVICE_CPU),
-    ::testing::Values(additional_config));
+const std::map<std::vector<size_t>, std::vector<std::vector<size_t>>> inputShapes = {
+    {{1}, {{1}, {17}, {1, 1}, {2, 18}, {1, 1, 2}, {2, 2, 3}, {1, 1, 2, 3}}},
+        {{5}, {{1}, {1, 1}, {2, 5}, {1, 1, 1}, {2, 2, 5}}},
+        {{2, 200}, {{1}, {200}, {1, 200}, {2, 200}, {2, 2, 200}}},
+        {{1, 3, 20}, {{20}, {2, 1, 1}}},
+        {{2, 17, 3, 4}, {{4}, {1, 3, 4}, {2, 1, 3, 4}}},
+        {{2, 1, 1, 3, 1}, {{1}, {1, 3, 4}, {2, 1, 3, 4}, {1, 1, 1, 1, 1}}},
+};
 
+const std::vector<InferenceEngine::Precision> inputsPrecisions = {
+        InferenceEngine::Precision::FP32,
+        InferenceEngine::Precision::FP16,
+        InferenceEngine::Precision::I32,
+        InferenceEngine::Precision::U32,
+        InferenceEngine::Precision::BOOL,
+};
 
-INSTANTIATE_TEST_SUITE_P(smoke_LessEqual, ComparisonLayerTest, SerializeLessEqualTestParams, ComparisonLayerTest::getTestCaseName);
-} // namespace
+const std::vector<ngraph::helpers::InputLayerType> secondInputTypes = {
+        ngraph::helpers::InputLayerType::CONSTANT,
+        ngraph::helpers::InputLayerType::PARAMETER,
+};
+
+const std::map<std::string, std::string> additional_config = {};
+
+const auto opType = ngraph::helpers::ComparisonTypes::LESS_EQUAL;
+
+const auto ComparisonTestParams = ::testing::Combine(
+        ::testing::ValuesIn(CommonTestUtils::combineParams(inputShapes)),
+        ::testing::ValuesIn(inputsPrecisions),
+        ::testing::Values(opType),
+        ::testing::ValuesIn(secondInputTypes),
+        ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
+        ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
+        ::testing::Values(CommonTestUtils::DEVICE_CPU),
+        ::testing::Values(additional_config));
+
+INSTANTIATE_TEST_SUITE_P(smoke_LessEqual, ComparisonLayerTest, ComparisonTestParams, ComparisonLayerTest::getTestCaseName);
+}  // namespace
