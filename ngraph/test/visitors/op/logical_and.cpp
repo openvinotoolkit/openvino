0b9d034fdc88055f1ebf6250dd01d551f0a1f293
pszmel <piotr.szmelczynski@intel.com>
2021-08-02 10:46:00 +0200

    refactor visitor test

--- ngraph/test/visitors/op/logical_and.cpp
+++ ngraph/test/visitors/op/logical_and.cpp
@@ -2,33 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "gtest/gtest.h"
-
-#include "ngraph/ngraph.hpp"
-#include "ngraph/op/util/attr_types.hpp"
+#include "binary_ops.hpp"
 #include "ngraph/opsets/opset1.hpp"
 
+using Type = ::testing::Types<BinaryOperatorType<ngraph::opset1::LogicalAnd, ngraph::element::boolean>>;
 
-#include "util/visitor.hpp"
-
-using namespace std;
-using namespace ngraph;
-using ngraph::test::NodeBuilder;
-using ngraph::test::ValueMap;
-
-TEST(attributes, logical_and_op)
-{
-    NodeBuilder::get_ops().register_factory<opset1::LogicalAnd>();
-    auto x1 = make_shared<op::Parameter>(element::boolean, Shape{200});
-    auto x2 = make_shared<op::Parameter>(element::boolean, Shape{200});
-
-    auto auto_broadcast = op::AutoBroadcastType::NUMPY;
-
-    auto logical_and = make_shared<opset1::LogicalAnd>(x1, x2, auto_broadcast);
-    NodeBuilder builder(logical_and);
-    auto g_logical_and = as_type_ptr<opset1::LogicalAnd>(builder.create());
-    const auto expected_attr_count = 1;
-
-    EXPECT_EQ(builder.get_value_map_size(), expected_attr_count);
-    EXPECT_EQ(g_logical_and->get_autob(), logical_and->get_autob());
-}
+INSTANTIATE_TYPED_TEST_SUITE_P(visitor_with_auto_broadcast,
+                               BinaryOperatorVisitor,
+                               Type,
+                               BinaryOperatorTypeName);
