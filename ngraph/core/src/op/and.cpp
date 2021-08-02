79cfec5cf403d837694e324e7e8daadc446345a4
pszmel <piotr.szmelczynski@intel.com>
2021-08-02 11:02:43 +0200

    update has_evaluate types

--- ngraph/core/src/op/and.cpp
+++ ngraph/core/src/op/and.cpp
@@ -82,13 +82,7 @@ bool op::v1::LogicalAnd::has_evaluate() const
     NGRAPH_OP_SCOPE(v1_LogicalAnd_has_evaluate);
     switch (get_input_element_type(0))
     {
-    case ngraph::element::boolean:
-    case ngraph::element::i32:
-    case ngraph::element::i64:
-    case ngraph::element::u32:
-    case ngraph::element::u64:
-    case ngraph::element::f16:
-    case ngraph::element::f32: return true;
+    case ngraph::element::boolean: return true;
     default: break;
     }
     return false;
