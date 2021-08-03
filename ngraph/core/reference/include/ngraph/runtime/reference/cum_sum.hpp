852aa6328ea266f2ba651050c1a3f567a8901d28
mitruska <katarzyna.mitrus@intel.com>
2021-08-03 19:12:48 +0200

    Refactor redundant copy

--- ngraph/core/reference/include/ngraph/runtime/reference/cum_sum.hpp
+++ ngraph/core/reference/include/ngraph/runtime/reference/cum_sum.hpp
@@ -45,8 +45,8 @@ namespace ngraph
                                  const T* data_ptr,
                                  const size_t axis_dim_size)
                 {
-                   const auto slices_count = shape_size(Shape(
-                        shape.begin(), shape.begin() + shape.size() - 1));
+                    const auto slices_count =
+                        shape_size(Shape(shape.begin(), shape.begin() + shape.size() - 1));
                     auto axis_dim_counter = 0;
                     for (auto i = 0; i < slices_count; ++i)
                     {
@@ -90,6 +90,7 @@ namespace ngraph
                     std::rotate(transposed_shape.begin() + axis,
                                 transposed_shape.begin() + axis + 1,
                                 transposed_shape.end());
+
                     reference::transpose(reinterpret_cast<const char*>(arg),
                                          reinterpret_cast<char*>(out),
                                          tensor_shape,
@@ -100,10 +101,10 @@ namespace ngraph
 
                 template <typename T, typename P>
                 void transpose_to_original(T* arg,
-                                            T* out,
-                                            const P axis,
-                                            const Shape& tensor_shape,
-                                            Shape& transposed_shape)
+                                           T* out,
+                                           const P axis,
+                                           const Shape& tensor_shape,
+                                           Shape& transposed_shape)
                 {
                     std::vector<int64_t> transposed_axes(tensor_shape.size());
                     std::iota(transposed_axes.begin(), transposed_axes.end(), 0);
@@ -111,7 +112,6 @@ namespace ngraph
                                 transposed_axes.end() - 1,
                                 transposed_axes.end());
 
-                    std::vector<T> transposed_output_data(shape_size(tensor_shape));
                     reference::transpose(reinterpret_cast<char*>(arg),
                                          reinterpret_cast<char*>(out),
                                          transposed_shape,
@@ -132,9 +132,10 @@ namespace ngraph
                 const auto rank = tensor_shape.size();
                 const auto axis = axis_tensor[0] >= 0 ? axis_tensor[0] : rank + axis_tensor[0];
                 const bool is_last_axis = axis == rank - 1;
+
+                std::vector<T> output_data(shape_size(tensor_shape), 0);
                 if (is_last_axis)
                 {
-                    std::vector<T> output_data(shape_size(tensor_shape), 0);
                     details::loop_cumsum(
                         output_data, exclusive, reverse, tensor_shape, arg, tensor_shape[axis]);
                     std::copy(begin(output_data), end(output_data), out);
@@ -143,14 +144,10 @@ namespace ngraph
                 {
                     Shape transposed_shape(tensor_shape);
                     details::transpose_to_last_axis(arg, out, axis, tensor_shape, transposed_shape);
-
-                    std::vector<T> output_data(shape_size(tensor_shape), 0);
                     details::loop_cumsum(
                         output_data, exclusive, reverse, transposed_shape, out, tensor_shape[axis]);
-
-                    std::vector<T> transposed_output_data(shape_size(tensor_shape));
-                    details::transpose_to_original(output_data.data(), transposed_output_data.data(), axis, tensor_shape, transposed_shape);
-                    std::copy(begin(transposed_output_data), end(transposed_output_data), out);
+                    details::transpose_to_original(
+                        output_data.data(), out, axis, tensor_shape, transposed_shape);
                 }
             }
         } // namespace reference
