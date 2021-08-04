60f7f13311fc4bb3c1c3a29772a00c27bd9c2d67
dkozykowski <dawid.kozykowski@intel.com>
2021-08-04 15:48:27 +0200

    fix formatting to match clang-format

--- ngraph/core/include/ngraph/op/or.hpp
+++ ngraph/core/include/ngraph/op/or.hpp
@@ -38,7 +38,7 @@ namespace ngraph
 
                 virtual std::shared_ptr<Node>
                     clone_with_new_inputs(const OutputVector& new_args) const override;
-                    
+
                 bool evaluate(const HostTensorVector& outputs,
                               const HostTensorVector& inputs) const override;
                 bool has_evaluate() const override;
