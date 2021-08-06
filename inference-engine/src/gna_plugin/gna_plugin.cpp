993c2bdf3a7f104ddacfacb04dec47196a8ff5c2
Evgeny Kotov <evgeny.kotov@intel.com>
2021-08-06 17:16:51 +0300

    check is node final non-functional for grouping; remove InsertIdentityToLSTMCellPass

--- inference-engine/src/gna_plugin/gna_plugin.cpp
+++ inference-engine/src/gna_plugin/gna_plugin.cpp
@@ -744,7 +744,6 @@ void GNAPlugin::LoadNetwork(CNNNetwork & _network) {
         passes->registerPass<RemoveConstPass>();
         passes->registerPass<UnrollTIPass>();
         passes->registerPass<RemoveConstPass>();
-        passes->registerPass<InsertIdentityToLSTMCellPass>();
         passes->registerPass<UnrollLSTMCellPass>();
         passes->registerPass<RemoveSingleInputConcatPass>();
 
