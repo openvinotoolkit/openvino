adfdc485f47d1443a46db82cb1e6cf5a94f47410
vkuznets <victor.kuznetsov@intel.com>
2021-08-06 17:26:23 +0300

    add memtest

---
+++ tests/memory_tests/README.md
@@ -0,0 +1,44 @@
+# Memory Tests
+
+This test suite contains pipelines, which are executables. 
+Memory tests measuring memory required for the use cases and fail when memory
+usage exceeds a pre-defined level.
+
+## Prerequisites
+
+To build the time tests, you need to have OpenVINO™ installed or build from source.
+
+## Measure Time
+
+To build and run the tests, open a terminal, set OpenVINO™ environment and run
+the commands below:
+
+1. Build tests:
+``` bash
+mkdir build && cd build
+cmake .. && make memory_tests
+```
+
+If you don't have OpenVINO™ installed you need to have the `build` folder, which
+is created when you configure and build OpenVINO™ from sources:
+
+``` bash
+cmake .. -DInferenceEngine_DIR=$(realpath ../../../build) && make memory_tests
+```
+For old versions of OpenVINO? from sources use `-DInferenceEngineDeveloperPackage_DIR`:
+``` bash
+cmake .. -DInferenceEngineDeveloperPackage_DIR=$(realpath ../../../build) && make memory_tests
+```
+
+2. Run test:
+``` bash
+./scripts/run_memorytest.py ../../bin/intel64/Release/memtest_infer -m model.xml -d CPU
+```
+
+2. Run several configurations using `pytest`:
+``` bash
+pytest ./test_runner/test.py --exe ../../bin/intel64/Release/memorytest_infer
+
+# For parse_stat testing:
+pytest ./scripts/run_memorytest.py
+```
--- tests/memory_tests/test_runner/conftest.py
+++ tests/memory_tests/test_runner/conftest.py
@@ -11,7 +11,7 @@ This plugin adds the following command-line options:
 
 * `--test_conf` - Path to test configuration file. Used to parametrize tests.
   Format: YAML file.
-* `--exe` - Path to a timetest binary to execute.
+* `--exe` - Path to a test binary to execute.
 * `--niter` - Number of times to run executable.
 """
 
@@ -29,20 +29,21 @@ from copy import deepcopy
 from pathlib import Path
 from jsonschema import validate, ValidationError
 
-UTILS_DIR = os.path.join(Path(__file__).parent.parent.parent, "utils")
+UTILS_DIR = Path(__file__).parent.parent
 sys.path.insert(0, str(UTILS_DIR))
 
 from plugins.conftest import *
-from path_utils import check_positive_int
+from scripts.run_test import check_positive_int
+from test_runner.utils import upload_data, metadata_from_manifest, query_timeline, DATABASES, DB_COLLECTIONS
 from platform_utils import get_os_name, get_os_version, get_cpu_info
-from utils import upload_data, metadata_from_manifest, DATABASES, DB_COLLECTIONS
+
 
 # -------------------- CLI options --------------------
 
 
 def pytest_addoption(parser):
     """Specify command-line options for all plugins"""
-    test_args_parser = parser.getgroup("timetest test run")
+    test_args_parser = parser.getgroup("test run")
     test_args_parser.addoption(
         "--test_conf",
         type=Path,
@@ -54,7 +55,7 @@ def pytest_addoption(parser):
         required=True,
         dest="executable",
         type=Path,
-        help="path to a timetest binary to execute"
+        help="path to a test binary to execute"
     )
     test_args_parser.addoption(
         "--niter",
@@ -62,7 +63,19 @@ def pytest_addoption(parser):
         help="number of iterations to run executable and aggregate results",
         default=3
     )
-    db_args_parser = parser.getgroup("timetest database use")
+    helpers_args_parser = parser.getgroup("test helpers")
+    helpers_args_parser.addoption(
+        "--dump_refs",
+        type=Path,
+        help="path to dump test config with references updated with statistics collected while run",
+    )
+    helpers_args_parser.addoption(
+        '--timeline_report',
+        type=Path,
+        # TODO:
+        help='path to build manifest to extract commit information'
+    )
+    db_args_parser = parser.getgroup("test database use")
     db_args_parser.addoption(
         '--db_submit',
         metavar="RUN_ID",
@@ -96,18 +109,14 @@ def pytest_addoption(parser):
         '--db_metadata',
         type=str,
         default=None,
-        help='path to JSON-formatted file to extract additional information')
+        help='path to JSON-formatted file to extract additional information'
+    )
     db_args_parser.addoption(
         '--manifest',
         type=Path,
         required=is_db_used,
-        help='path to build manifest to extract commit information')
-
-
-@pytest.fixture(scope="session")
-def test_conf(request):
-    """Fixture function for command-line option."""
-    return request.config.getoption('test_conf')
+        help='path to build manifest to extract commit information'
+    )
 
 
 @pytest.fixture(scope="session")
@@ -133,7 +142,7 @@ def cl_cache_dir(pytestconfig, instance):
     cache will be saved correctly. This behaviour is OS independent.
     More: https://github.com/intel/compute-runtime/blob/master/opencl/doc/FAQ.md#how-can-cl_cache-be-enabled
     """
-    if instance["device"]["name"] == "GPU":
+    if instance["instance"]["device"]["name"] == "GPU":
         cl_cache_dir = pytestconfig.invocation_dir / "cl_cache"
         # if cl_cache generation to a local `cl_cache` folder doesn't work, specify
         # `cl_cache_dir` environment variable in an attempt to fix it (Linux specific)
@@ -153,7 +162,7 @@ def model_cache_dir(pytestconfig, instance):
     """
     Generate directory to IE model cache before test run and clean up after run.
     """
-    if instance.get("use_model_cache"):
+    if instance["instance"].get("use_model_cache"):
         model_cache_dir = pytestconfig.invocation_dir / "models_cache"
         if model_cache_dir.exists():
             shutil.rmtree(model_cache_dir)
@@ -166,22 +175,7 @@ def model_cache_dir(pytestconfig, instance):
 
 
 @pytest.fixture(scope="function")
-def test_info(request, pytestconfig):
-    """Fixture for collecting timetests information.
-
-    Current fixture fills in `request` and `pytestconfig` global
-    fixtures with timetests information which will be used for
-    internal purposes.
-    """
-    setattr(request.node._request, "test_info", {"results": {},
-                                                 "raw_results": {},
-                                                 "db_info": {}})
-
-    yield request.node._request.test_info
-
-
-@pytest.fixture(scope="function")
-def validate_test_case(request, test_info):
+def validate_test_case(request):
     """Fixture for validating test case on correctness.
 
     Fixture checks current test case contains all fields required for
@@ -213,7 +207,7 @@ def validate_test_case(request, test_info):
     schema = json.loads(schema)
 
     try:
-        validate(instance=request.node.funcargs["instance"], schema=schema)
+        validate(instance=request.node.funcargs["instance"]["instance"], schema=schema)
     except ValidationError:
         request.config.option.db_submit = False
         raise
@@ -221,14 +215,14 @@ def validate_test_case(request, test_info):
 
 
 @pytest.fixture(scope="function")
-def prepare_db_info(request, test_info, executable, niter, manifest_metadata):
+def prepare_db_info(request, instance, executable, niter, manifest_metadata):
     """Fixture for preparing and validating data to submit to a database.
 
     Fixture prepares data and metadata to submit to a database. One of the steps
     is parsing of build information from build manifest. After preparation,
     it checks if data contains required properties.
     """
-    FIELDS_FOR_ID = ['run_id', 'timetest', 'model', 'device', 'niter']
+    FIELDS_FOR_ID = ['run_id', "test_exe", 'model', 'device', 'niter']
 
     run_id = request.config.getoption("db_submit")
     if not run_id:
@@ -239,25 +233,30 @@ def prepare_db_info(request, test_info, executable, niter, manifest_metadata):
     db_meta_path = request.config.getoption("db_metadata")
     if db_meta_path:
         with open(db_meta_path, "r") as db_meta_f:
-            test_info["db_info"].update(json.load(db_meta_f))
+            instance["db"].update(json.load(db_meta_f))
 
     # add test info
     info = {
-        # results will be added immediately before uploading to DB in `pytest_runtest_makereport`
+        # results will be added immediately before uploading to DB in `pytest_runtest_makereport`.
+        **instance["orig_instance"],  # TODO: think about use `instance` instead of `orig_instance`
         "run_id": run_id,
-        "timetest": str(executable.stem),
-        "model": request.node.funcargs["instance"]["model"],
-        "device": request.node.funcargs["instance"]["device"],
+        "test_exe": str(executable.stem),
         "niter": niter,
         "test_name": request.node.name,
-        "os": "_".join([str(item) for item in [get_os_name(), *get_os_version()]])
+        "os": "_".join([str(item) for item in [get_os_name(), *get_os_version()]]),
+        "cpu_info": get_cpu_info(),
+        "status": "not_finished",
+        "error_msg": "",
+        "results": {},
+        "raw_results": {},
+        "references": instance["instance"].get("references", {}),   # upload actual references that were used
     }
     info['_id'] = hashlib.sha256(
         ''.join([str(info[key]) for key in FIELDS_FOR_ID]).encode()).hexdigest()
-    test_info["db_info"].update(info)
+    instance["db"] = info
 
     # add manifest metadata
-    test_info["db_info"].update(manifest_metadata)
+    instance["db"].update(manifest_metadata)
 
     # validate db_info
     schema = """
@@ -282,21 +281,27 @@ def prepare_db_info(request, test_info, executable, niter, manifest_metadata):
                 "required": ["path", "name", "precision", "framework"]
             },
             "run_id": {"type": "string"},
-            "timetest": {"type": "string"},
+            "test_exe": {"type": "string"},
             "niter": {"type": "integer"},
             "test_name": {"type": "string"},
-            "results": {"type": "object"},
             "os": {"type": "string"},
+            "cpu_info": {"type": "string"},
+            "status": {"type": "string"},
+            "error_msg": {"type": "string"},
+            "results": {"type": "object"},
+            "raw_results": {"type": "object"},
+            "references": {"type": "object"},
             "_id": {"type": "string"}
         },
-        "required": ["device", "model", "run_id", "timetest", "niter", "test_name", "os", "_id"],
+        "required": ["device", "model", "run_id", "test_exe", "niter", "test_name", "os", "cpu_info", 
+                     "status", "error_msg", "results", "raw_results", "references", "_id"],
         "additionalProperties": true
     }
     """
     schema = json.loads(schema)
 
     try:
-        validate(instance=test_info["db_info"], schema=schema)
+        validate(instance=instance["db"], schema=schema)
     except ValidationError:
         request.config.option.db_submit = False
         raise
@@ -318,7 +323,7 @@ def manifest_metadata(request):
         {
             "type": "object",
             "properties": {
-                "product_type": {"enum": ["private_linux_ubuntu_18_04", "private_windows_vs2019"]},
+                "product_type": {"type": "string"},
                 "repo_url": {"type": "string"},
                 "commit_sha": {"type": "string"},
                 "commit_date": {"type": "string"},
@@ -340,6 +345,47 @@ def manifest_metadata(request):
     yield manifest_meta
 
 
+@pytest.fixture(scope="session", autouse=True)
+def prepare_timeline_report(pytestconfig): #records, args.db_url, args.db_collection, args.timeline_report
+    """ Create memcheck timeline HTML report for records.
+    """
+    yield
+    report_path = pytestconfig.getoption('timeline_report')
+    if report_path:
+        db_url = pytestconfig.getoption("db_url")
+        db_name = pytestconfig.getoption("db_name")
+        db_collection = pytestconfig.getoption("db_collection")
+
+        records = [rec["db"] for rec in pytestconfig.session_info]
+        records.sort(
+            key=lambda item: f"{item['status']}{item['device']['name']}{item['model']['name']}{item['test_name']}")
+        timelines = query_timeline(records, db_url, db_name, db_collection)
+        import jinja2  # pylint: disable=import-outside-toplevel
+        env = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(
+                searchpath=Path().absolute() / 'memcheck-template'),
+            autoescape=False)
+        template = env.get_template('timeline_report.html')
+        template.stream(records=records, timelines=timelines).dump(report_path)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def prepare_tconf_with_refs(pytestconfig):
+    """Fixture for preparing test config based on original test config
+    with timetests results saved as references.
+    """
+    yield
+    new_tconf_path = pytestconfig.getoption('dump_refs')
+    if new_tconf_path:
+        logging.info("Save new test config with test results as references to {}".format(new_tconf_path))
+        upd_cases = pytestconfig.orig_cases.copy()
+        for record in pytestconfig.session_info:
+            rec_i = upd_cases.index(record["orig_instance"])
+            upd_cases[rec_i]["references"] = record["results"]
+        with open(new_tconf_path, "w") as tconf:
+            yaml.safe_dump(upd_cases, tconf)
+
+
 def pytest_generate_tests(metafunc):
     """Pytest hook for test generation.
 
@@ -356,8 +402,6 @@ def pytest_generate_tests(metafunc):
         } for case in test_cases]
         metafunc.parametrize("instance", test_cases)
         setattr(metafunc.config, "session_info", test_cases)
-    # if test_cases:
-    #     metafunc.parametrize("instance", test_cases)
 
 
 def pytest_make_parametrize_id(config, val, argname):
@@ -372,7 +416,7 @@ def pytest_make_parametrize_id(config, val, argname):
             yield d
 
     keys = ["device", "model"]
-    values = {key: val[key] for key in keys}
+    values = {key: val["instance"][key] for key in keys}
     values = list(get_dict_values(values))
 
     return "-".join(["_".join([key, str(val)]) for key, val in zip(keys, values)])
@@ -384,30 +428,25 @@ def pytest_runtest_makereport(item, call):
 
     Submit tests' data to a database.
     """
-
     run_id = item.config.getoption("db_submit")
     if not run_id:
         yield
         return
 
-    data = item._request.test_info["db_info"].copy()
-    data["results"] = item._request.test_info["results"].copy()
-    data["raw_results"] = item._request.test_info["raw_results"].copy()
-    data["cpu_info"] = get_cpu_info()
-    data["status"] = "not_finished"
-    data["error_msg"] = ""
+    db_url = item.config.getoption("db_url")
+    db_name = item.config.getoption("db_name")
+    db_collection = item.config.getoption("db_collection")
 
+    instance = item.funcargs["instance"]  # alias
     report = (yield).get_result()
     if call.when in ["setup", "call"]:
         if call.when == "call":
             if not report.passed:
-                data["status"] = "failed"
-                data["error_msg"] = report.longrepr.reprcrash.message
+                instance["db"]["status"] = "failed"
+                instance["db"]["error_msg"] = report.longrepr.reprcrash.message
             else:
-                data["status"] = "passed"
-
-        db_url = item.config.getoption("db_url")
-        db_name = item.config.getoption("db_name")
-        db_collection = item.config.getoption("db_collection")
-        logging.info("Upload data to {}/{}.{}. Data: {}".format(db_url, db_name, db_collection, data))
-        upload_data(data, db_url, db_name, db_collection)
+                instance["db"]["status"] = "passed"
+        instance["db"]["results"] = instance["results"]
+        logging.info("Upload data to {}/{}.{}. Data: {}".format(db_url, db_name, db_collection, instance["db"]))
+        # TODO: upload to new DB (memcheck -> memory_tests)
+        # upload_data(data, db_url, db_name, db_collection)
