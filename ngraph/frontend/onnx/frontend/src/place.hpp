024f0152cff1870f8b269a30f1221a095d5cb036
Mateusz Bencer <mateusz.bencer@intel.com>
2021-08-06 13:33:42 +0200

    implementation of is_equal_data

--- ngraph/frontend/onnx/frontend/src/place.hpp
+++ ngraph/frontend/onnx/frontend/src/place.hpp
@@ -26,6 +26,10 @@ namespace ngraph
 
             bool is_equal(Place::Ptr another) const override;
 
+            bool is_equal_data(Place::Ptr another) const override;
+
+            Place::Ptr get_source_tensor() const override;
+
         private:
             onnx_editor::InputEdge m_edge;
             const std::shared_ptr<onnx_editor::ONNXModelEditor> m_editor;
@@ -44,6 +48,10 @@ namespace ngraph
             bool is_output() const override;
 
             bool is_equal(Place::Ptr another) const override;
+
+            bool is_equal_data(Place::Ptr another) const override;
+
+            Place::Ptr get_target_tensor() const override;
 
         private:
             onnx_editor::OutputEdge m_edge;
@@ -68,6 +76,8 @@ namespace ngraph
             bool is_output() const override;
 
             bool is_equal(Place::Ptr another) const override;
+
+            bool is_equal_data(Place::Ptr another) const override;
 
         private:
             std::string m_name;
