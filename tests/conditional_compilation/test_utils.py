0af76a75f0d86965459701f7817f2f9e9883386f
Koryachikhina, Anastasia <anastasia.koryachikhina@intel.com>
2021-08-06 16:02:22 +0300

    change built_target arg type

--- tests/conditional_compilation/test_utils.py
+++ tests/conditional_compilation/test_utils.py
@@ -71,18 +71,19 @@ def run_infer(model, out_file, install_dir):
     return return_code, output
 
 
-def make_build(openvino_root_dir, build_dir, install_dir, build_target=None, cmake_additional_args=None, log=None):
+def make_build(openvino_root_dir, build_dir, install_dir, build_target: dict = None, cmake_additional_args=None,
+               log=None):
     """Parametrized build and install OpenVINO package."""
     additional_args_line = " ".join(cmake_additional_args) + " " if cmake_additional_args else ""
-    sea_itt_lib_path = Path(openvino_root_dir / "thirdparty" / "itt_collector" / "sea_itt_lib")
-    build_target_arg_line = f"cmake --build {sea_itt_lib_path} --target  {build_target} && " if build_target else ""
+    build_target_arg_line = [f"cmake --build {build_target[target]} --target {target} && " for target in
+                             build_target.keys()] if build_target else ""
     nproc = multiprocessing.cpu_count()
     cmd = (
         f"cmake -DENABLE_PROFILING_ITT=ON -DCMAKE_BUILD_TYPE=Release "
         f"-DPYTHON_EXECUTABLE={sys.executable} {additional_args_line}"
         f"-S {openvino_root_dir} -B {build_dir} && "
         f"cmake --build {build_dir} -j{nproc} && "
-        f"{build_target_arg_line}"
+        f"{' '.join(build_target_arg_line)}"
         f"cmake --install {build_dir} --prefix {install_dir}"
     )
     return cmd_exec([cmd], shell=True, log=log)
