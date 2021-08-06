7ea6f655206e0c466d174d6fa9d0b706681f7542
CCR\ntyukaev <nikolay.tyukaev@intel.com>
2021-08-06 18:21:58 +0300

    update custom.js

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
     var collapsible_sections = $('div.collapsible-section');
-    $(collapsible_sections).each(function(item){
-        item.prepend('<summary>Click to expand</summary>');
-        item.wrap('<details></details>');
-    });
+    $(collapsible_sections).wrap('<details class="col-sect-details sphinx-bs dropdown card mb-3"></details>');
+    $('.col-sect-details').prepend('<summary class="summary-title card-header">Click to expand</summary>');
 });
