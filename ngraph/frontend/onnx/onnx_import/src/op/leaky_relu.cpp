4d3dcb8dbd5cac22fed0fa47179aacc1dfcfa73b
Mateusz Bencer <mateusz.bencer@intel.com>
2021-08-09 06:26:37 +0200

    Remove leaky relu alpha check (#6910)
    * remove leaky relu check
    
    * make alpha_node scalar

--- ngraph/frontend/onnx/onnx_import/src/op/leaky_relu.cpp
+++ ngraph/frontend/onnx/onnx_import/src/op/leaky_relu.cpp
@@ -21,11 +21,8 @@ namespace ngraph
                     auto data = node.get_ng_inputs().at(0);
                     double alpha = node.get_attribute_value<double>("alpha", 0.01);
 
-                    CHECK_VALID_NODE(
-                        node, alpha >= 0 && alpha <= 1, " alpha value should be in range (0,1)");
-
                     std::shared_ptr<ngraph::Node> alpha_node =
-                        default_opset::Constant::create(data.get_element_type(), Shape{}, {alpha});
+                        default_opset::Constant::create(data.get_element_type(), Shape{1}, {alpha});
                     return {std::make_shared<default_opset::PRelu>(data, alpha_node)};
                 }
 
