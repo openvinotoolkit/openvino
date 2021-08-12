2129dae657a00f8e51cdd46612bd9ed100d5e146
Mateusz Bencer <mateusz.bencer@intel.com>
2021-08-12 13:20:56 +0200

    changed name to onnx_experimental

--- ngraph/frontend/onnx/frontend/src/frontend.cpp
+++ ngraph/frontend/onnx/frontend/src/frontend.cpp
@@ -58,7 +58,7 @@ std::shared_ptr<ngraph::Function> FrontEndONNX::decode(InputModel::Ptr model) co
 
 std::string FrontEndONNX::get_name() const
 {
-    return "onnx";
+    return "onnx_experimental";
 }
 
 bool FrontEndONNX::supported_impl(const std::vector<std::shared_ptr<Variant>>& variants) const
