7bd83b754be4db3f0503aad91e026987b2e8b8f0
Ilya Churaev <ilya.churaev@intel.com>
2021-08-13 07:04:37 +0300

    Fixed code style

--- ngraph/core/include/openvino/core/variant.hpp
+++ ngraph/core/include/openvino/core/variant.hpp
@@ -11,98 +11,88 @@
 #include "ngraph/type.hpp"
 #include "openvino/core/core_visibility.hpp"
 
-namespace ngraph
-{
-    class Node;
+namespace ngraph {
+class Node;
 }
-namespace ov
-{
-    using VariantTypeInfo = ngraph::DiscreteTypeInfo;
+namespace ov {
+using VariantTypeInfo = ngraph::DiscreteTypeInfo;
 
-    class CORE_API Variant
-    {
-    public:
-        virtual ~Variant();
-        virtual const VariantTypeInfo& get_type_info() const = 0;
+class CORE_API Variant {
+public:
+    virtual ~Variant();
+    virtual const VariantTypeInfo& get_type_info() const = 0;
 
-        virtual bool is_copyable() const;
-        virtual std::shared_ptr<ov::Variant> init(const std::shared_ptr<ngraph::Node>& node);
-        virtual std::shared_ptr<ov::Variant> merge(const ngraph::NodeVector& nodes);
-        virtual std::string to_string() { return ""; }
-    };
+    virtual bool is_copyable() const;
+    virtual std::shared_ptr<ov::Variant> init(const std::shared_ptr<ngraph::Node>& node);
+    virtual std::shared_ptr<ov::Variant> merge(const ngraph::NodeVector& nodes);
+    virtual std::string to_string() {
+        return "";
+    }
+};
 
-    template <typename VT>
-    class VariantImpl : public Variant
-    {
-    public:
-        using value_type = VT;
+template <typename VT>
+class VariantImpl : public Variant {
+public:
+    using value_type = VT;
 
-        VariantImpl(const value_type& value)
-            : m_value(value)
-        {
-        }
+    VariantImpl(const value_type& value) : m_value(value) {}
 
-        const value_type& get() const { return m_value; }
-        value_type& get() { return m_value; }
-        void set(const value_type& value) { m_value = value; }
-
-    protected:
-        value_type m_value;
-    };
-
-    extern template class CORE_API VariantImpl<std::string>;
-    extern template class CORE_API VariantImpl<int64_t>;
-
-    template <typename VT>
-    class VariantWrapper
-    {
-    };
-
-    template <>
-    class CORE_API VariantWrapper<std::string> : public VariantImpl<std::string>
-    {
-    public:
-        static constexpr VariantTypeInfo type_info{"Variant::std::string", 0};
-        const VariantTypeInfo& get_type_info() const override { return type_info; }
-        VariantWrapper(const value_type& value)
-            : VariantImpl<value_type>(value)
-        {
-        }
-    };
-
-    template <>
-    class CORE_API VariantWrapper<int64_t> : public VariantImpl<int64_t>
-    {
-    public:
-        static constexpr VariantTypeInfo type_info{"Variant::int64_t", 0};
-        const VariantTypeInfo& get_type_info() const override { return type_info; }
-        VariantWrapper(const value_type& value)
-            : VariantImpl<value_type>(value)
-        {
-        }
-    };
-
-    template <typename T>
-    inline std::shared_ptr<Variant> make_variant(const T& p)
-    {
-        return std::dynamic_pointer_cast<VariantImpl<T>>(std::make_shared<VariantWrapper<T>>(p));
+    const value_type& get() const {
+        return m_value;
+    }
+    value_type& get() {
+        return m_value;
+    }
+    void set(const value_type& value) {
+        m_value = value;
     }
 
-    template <size_t N>
-    inline std::shared_ptr<Variant> make_variant(const char (&s)[N])
-    {
-        return std::dynamic_pointer_cast<VariantImpl<std::string>>(
-            std::make_shared<VariantWrapper<std::string>>(s));
+protected:
+    value_type m_value;
+};
+
+extern template class CORE_API VariantImpl<std::string>;
+extern template class CORE_API VariantImpl<int64_t>;
+
+template <typename VT>
+class VariantWrapper {};
+
+template <>
+class CORE_API VariantWrapper<std::string> : public VariantImpl<std::string> {
+public:
+    static constexpr VariantTypeInfo type_info{"Variant::std::string", 0};
+    const VariantTypeInfo& get_type_info() const override {
+        return type_info;
     }
+    VariantWrapper(const value_type& value) : VariantImpl<value_type>(value) {}
+};
+
+template <>
+class CORE_API VariantWrapper<int64_t> : public VariantImpl<int64_t> {
+public:
+    static constexpr VariantTypeInfo type_info{"Variant::int64_t", 0};
+    const VariantTypeInfo& get_type_info() const override {
+        return type_info;
+    }
+    VariantWrapper(const value_type& value) : VariantImpl<value_type>(value) {}
+};
+
+template <typename T>
+inline std::shared_ptr<Variant> make_variant(const T& p) {
+    return std::dynamic_pointer_cast<VariantImpl<T>>(std::make_shared<VariantWrapper<T>>(p));
+}
+
+template <size_t N>
+inline std::shared_ptr<Variant> make_variant(const char (&s)[N]) {
+    return std::dynamic_pointer_cast<VariantImpl<std::string>>(std::make_shared<VariantWrapper<std::string>>(s));
+}
 
 #if defined(ENABLE_UNICODE_PATH_SUPPORT) && defined(_WIN32)
-    template <size_t N>
-    inline std::shared_ptr<Variant> make_variant(const wchar_t (&s)[N])
-    {
-        return std::dynamic_pointer_cast<VariantImpl<std::wstring>>(
-            std::make_shared<VariantWrapper<std::wstring>>(s));
-    }
+template <size_t N>
+inline std::shared_ptr<Variant> make_variant(const wchar_t (&s)[N]) {
+    return std::dynamic_pointer_cast<VariantImpl<std::wstring>>(std::make_shared<VariantWrapper<std::wstring>>(s));
+}
 #endif
 
-    using RTMap = std::map<std::string, std::shared_ptr<Variant>>;
-} // namespace ov
+using RTMap = std::map<std::string, std::shared_ptr<Variant>>;
+}  // namespace ov
