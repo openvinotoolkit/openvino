0f26fcbc6f030a4549df549adefe696cce145676
mitruska <katarzyna.mitrus@intel.com>
2021-08-13 12:45:39 +0200

    new optimized cum_sum reference

--- ngraph/core/reference/include/ngraph/runtime/reference/cum_sum.hpp
+++ ngraph/core/reference/include/ngraph/runtime/reference/cum_sum.hpp
@@ -14,140 +14,38 @@
 #include "ngraph/runtime/reference/transpose.hpp"
 #include "utils/span.hpp"
 
-namespace ngraph
-{
-    namespace runtime
-    {
-        namespace reference
-        {
-            namespace details
-            {
-                template <typename InputIterator, typename OutputIterator>
-                void flat_cumsum(InputIterator it_input_begin,
-                                 InputIterator it_input_end,
-                                 OutputIterator it_output_begin,
-                                 const bool exclusive)
-                {
-                    if (exclusive)
-                    {
-                        --it_input_end;
-                        ++it_output_begin;
-                    }
-                    std::partial_sum(it_input_begin, it_input_end, it_output_begin);
-                }
-                template <typename T>
-                void loop_cumsum(std::vector<T>& output_data,
-                                 const bool exclusive,
-                                 const bool reverse,
-                                 const Shape& shape,
-                                 const T* data_ptr,
-                                 const size_t axis_dim_size)
-                {
-                    const auto slices_count = shape_size(span(shape).drop_back(1));
-                    auto axis_dim_counter = 0;
-                    for (auto i = 0; i < slices_count; ++i)
-                    {
-                        std::vector<T> input_data(data_ptr, data_ptr + axis_dim_size);
-                        std::vector<T> output_tmp_data(axis_dim_size, 0);
-                        if (reverse)
-                        {
-                            details::flat_cumsum(input_data.rbegin(),
-                                                 input_data.rend(),
-                                                 output_tmp_data.rbegin(),
-                                                 exclusive);
-                        }
-                        else
-                        {
-                            details::flat_cumsum(input_data.begin(),
-                                                 input_data.end(),
-                                                 output_tmp_data.begin(),
-                                                 exclusive);
-                        }
-                        std::move(output_tmp_data.begin(),
-                                  output_tmp_data.end(),
-                                  output_data.begin() + axis_dim_counter);
+namespace ngraph {
+namespace runtime {
+namespace reference {
 
-                        data_ptr += axis_dim_size;
-                        axis_dim_counter += axis_dim_size;
-                    }
-                }
+template <typename T, typename P>
+void cumsum(const T* arg,
+            const P* axis_tensor,
+            T* out,
+            const Shape& tensor_shape,
+            const bool exclusive,
+            const bool reverse) {
+    const auto rank = tensor_shape.size();
+    const auto axis = axis_tensor[0] >= 0 ? axis_tensor[0] : rank + axis_tensor[0];
 
-                template <typename T, typename P>
-                void transpose_to_last_axis(const T* arg,
-                                            T* out,
-                                            const P axis,
-                                            const Shape& tensor_shape,
-                                            Shape& transposed_shape)
-                {
-                    std::vector<int64_t> transposed_axes(tensor_shape.size());
-                    std::iota(transposed_axes.begin(), transposed_axes.end(), 0);
-                    std::rotate(transposed_axes.begin() + axis,
-                                transposed_axes.begin() + axis + 1,
-                                transposed_axes.end());
+    const auto slices_count = shape_size(Shape(tensor_shape.begin(), tensor_shape.begin() + axis));
+    const auto offset = shape_size(Shape(tensor_shape.begin() + axis + 1, tensor_shape.end()));
 
-                    std::rotate(transposed_shape.begin() + axis,
-                                transposed_shape.begin() + axis + 1,
-                                transposed_shape.end());
+    const auto axis_dim = tensor_shape[axis];
+    for (auto i = 0; i < slices_count; ++i) {
+        auto shift = exclusive ? offset : 0;
 
-                    reference::transpose(reinterpret_cast<const char*>(arg),
-                                         reinterpret_cast<char*>(out),
-                                         tensor_shape,
-                                         sizeof(T),
-                                         transposed_axes.data(),
-                                         transposed_shape);
-                }
-
-                template <typename T, typename P>
-                void transpose_to_original(T* arg,
-                                           T* out,
-                                           const P axis,
-                                           const Shape& tensor_shape,
-                                           const Shape& transposed_shape)
-                {
-                    std::vector<int64_t> transposed_axes(tensor_shape.size());
-                    std::iota(transposed_axes.begin(), transposed_axes.end(), 0);
-                    std::rotate(transposed_axes.begin() + axis,
-                                transposed_axes.end() - 1,
-                                transposed_axes.end());
-
-                    reference::transpose(reinterpret_cast<char*>(arg),
-                                         reinterpret_cast<char*>(out),
-                                         transposed_shape,
-                                         sizeof(T),
-                                         transposed_axes.data(),
-                                         tensor_shape);
-                }
-            } // namespace details
-
-            template <typename T, typename P>
-            void cumsum(const T* arg,
-                        const P* axis_tensor,
-                        T* out,
-                        const Shape& tensor_shape,
-                        const bool exclusive,
-                        const bool reverse)
-            {
-                const auto rank = tensor_shape.size();
-                const auto axis = axis_tensor[0] >= 0 ? axis_tensor[0] : rank + axis_tensor[0];
-                const bool is_last_axis = axis == rank - 1;
-
-                std::vector<T> output_data(shape_size(tensor_shape), 0);
-                if (is_last_axis)
-                {
-                    details::loop_cumsum(
-                        output_data, exclusive, reverse, tensor_shape, arg, tensor_shape[axis]);
-                    std::move(output_data.begin(), output_data.end(), out);
-                }
-                else
-                {
-                    Shape transposed_shape(tensor_shape);
-                    details::transpose_to_last_axis(arg, out, axis, tensor_shape, transposed_shape);
-                    details::loop_cumsum(
-                        output_data, exclusive, reverse, transposed_shape, out, tensor_shape[axis]);
-                    details::transpose_to_original(
-                        output_data.data(), out, axis, tensor_shape, transposed_shape);
-                }
+        for (auto o = 0; o < offset; ++o) {
+            auto sequence_start_idx = i * axis_dim * offset + o;
+            out[sequence_start_idx] = exclusive ? static_cast<T>(0) : arg[sequence_start_idx];
+            for (auto j = 1; j < axis_dim; ++j) {
+                auto element_idx = sequence_start_idx + j * offset;
+                auto in_idx = element_idx - shift;
+                out[element_idx] = out[element_idx - offset] + arg[in_idx];
             }
+        }
+    }
+}
 
 }  // namespace reference
 }  // namespace runtime
