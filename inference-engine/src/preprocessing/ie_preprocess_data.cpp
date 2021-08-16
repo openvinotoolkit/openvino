946f9f7ed1d4788c37d4a6230383fd53941f84be
Lyalin, Sergey <sergey.lyalin@intel.com>
2021-08-16 18:09:07 +0300

    Fixed wrong exception throwing

--- inference-engine/src/preprocessing/ie_preprocess_data.cpp
+++ inference-engine/src/preprocessing/ie_preprocess_data.cpp
@@ -72,10 +72,11 @@ void PreProcessData::execute(Blob::Ptr &preprocessedBlob, const PreProcessInfo &
 
     // Shape preprocessed tensor if needed; it is required here due to dynamic input shapes
     if (algorithm == NO_RESIZE) {
-        // Compete copy:
+        // Compete copy of shape
         preprocessedBlob->setShape(_userBlob->getTensorDesc().getDims());
     } else {
-        IE_THROW() << "Preprocessing for dynamically shaped inputs not yet supported. DEBUG: requires HW part detection here.";
+        //preprocessedBlob->getTensorDesc().
+        //IE_THROW() << "Preprocessing for dynamically shaped inputs not yet supported. DEBUG: requires HW part detection here.";
     }
 
     _preproc->preprocessWithGAPI(_userBlob, preprocessedBlob, algorithm, fmt, serial, batchSize);
