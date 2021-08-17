1234346693e1a4b50792d031948f5756e91ddb96
mitruska <katarzyna.mitrus@intel.com>
2021-08-17 13:19:59 +0200

    Remove FusedOp from v0::Gelu

--- ngraph/core/include/ngraph/op/gelu.hpp
+++ ngraph/core/include/ngraph/op/gelu.hpp
@@ -11,11 +11,10 @@
 
 namespace ngraph {
 namespace op {
-NGRAPH_SUPPRESS_DEPRECATED_START
 namespace v0 {
 /// \brief Gaussian Error Linear Unit
 /// f(x) = 0.5 * x * (1 + erf( x / sqrt(2) )
-class NGRAPH_API Gelu : public ngraph::op::util::FusedOp {
+class NGRAPH_API Gelu : public Op {
 public:
     static constexpr NodeTypeInfo type_info{"Gelu", 0};
     const NodeTypeInfo& get_type_info() const override {
@@ -28,15 +27,13 @@ public:
     Gelu(const Output<Node>& data);
 
     bool visit_attributes(AttributeVisitor& visitor) override;
-    virtual OutputVector decompose_op() const override;
 
-    void pre_validate_and_infer_types() override;
+    void validate_and_infer_types() override;
 
     virtual std::shared_ptr<Node> clone_with_new_inputs(const OutputVector& new_args) const override;
 };
 }  // namespace v0
 using v0::Gelu;
-NGRAPH_SUPPRESS_DEPRECATED_END
 
 /// \brief Specifies the approximation to calculate Gelu
 enum class GeluApproximationMode { TANH, ERF };
