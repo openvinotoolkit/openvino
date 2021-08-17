6ac585f88c2f893abf5c3b2faf75ba1c5f4632db
Anastasia Popova <anastasia.popova@intel.com>
2021-08-17 19:30:51 +0300

    Added comments, added names to consts.

--- ngraph/core/reference/include/ngraph/runtime/reference/random_uniform.hpp
+++ ngraph/core/reference/include/ngraph/runtime/reference/random_uniform.hpp
@@ -21,6 +21,11 @@ void random_uniform(const uint64_t* out_shape,
                     uint64_t seed,
                     uint64_t seed2);
 
+const uint32_t crush_resistance_const_lower_value = 0x9E3779B9;
+const uint32_t crush_resistance_const_upper_value = 0xBB67AE85;
+const uint64_t statistic_maximizing_multiplier_n = 0xD2511F53;
+const uint64_t statistic_maximizing_multiplier_counter = 0xCD9E8D57;
+
 }  // namespace reference
 }  // namespace runtime
 }  // namespace ngraph
--- ngraph/core/reference/src/runtime/reference/random_uniform.cpp
+++ ngraph/core/reference/src/runtime/reference/random_uniform.cpp
@@ -11,40 +11,62 @@
 namespace ngraph {
 namespace runtime {
 namespace reference {
+
+// Splits uint64 value into two uint32 values with right and left part of original value.
 std::pair<uint32_t, uint32_t> split_high_low(uint64_t value) {
     uint32_t low = static_cast<uint32_t>(value);
     uint32_t high = static_cast<uint32_t>(value >> 32);
     return {low, high};
 }
 
+// Concatenates two uint32 values into single uint64 values.
 uint64_t unite_high_low(uint32_t high, uint32_t low) {
     return (static_cast<uint64_t>(high) << 32) + low;
 }
 
+// Runs single "round" of Philox algorithm.
 void calculate_round(uint64_t key, uint64_t& counter, uint64_t& n) {
+    // Split key, counter and n into two uint32 values.
     auto counter_lr = split_high_low(counter);
     auto key_lr = split_high_low(key);
     auto n_lr = split_high_low(n);
-    auto prod0 = split_high_low(static_cast<uint64_t>(0xD2511F53) * n_lr.first);
-    auto prod1 = split_high_low(static_cast<uint64_t>(0xCD9E8D57) * counter_lr.first);
 
+    // Each round performs following updating for n and counter:
+    // left uint32 part = mullo(R, M)
+    // right uint32 part  = mulhi(R, M) xor k xor L
+    // mulhi(a, b) = floor((a * b) / 2^32)
+    // mullo(a, b) = (a * b) mod 2^32,
+    // where M - statistic_maximizing_multiplier const
+    auto prod0 = split_high_low(statistic_maximizing_multiplier_n * n_lr.first);
+    auto prod1 = split_high_low(statistic_maximizing_multiplier_counter * counter_lr.first);
     n_lr.first = prod1.second ^ n_lr.second ^ key_lr.first;
     n_lr.second = prod1.first;
     counter_lr.first = prod0.second ^ counter_lr.second ^ key_lr.second;
     counter_lr.second = prod0.first;
 
+    // Unite counter and n into uint64 values.
     counter = unite_high_low(counter_lr.second, counter_lr.first);
     n = unite_high_low(n_lr.second, n_lr.first);
 }
 
+// Increases key value.
 void raise_key(uint64_t& key) {
     auto key_lr = split_high_low(key);
-    key_lr.first += 0x9E3779B9;
-    key_lr.second += 0xBB67AE85;
+    key_lr.first += crush_resistance_const_lower_value;
+    key_lr.second += crush_resistance_const_upper_value;
     key = unite_high_low(key_lr.second, key_lr.first);
 }
 
+// Helper function for converting uint32 values to float32.
 float uint32_to_float(uint32_t x) {
+    // float32 is formatted as follows: sign(1 bit) exponent(8 bits) mantissa(23 bits). The value is interpreted
+    // The value is interpreted using following formula:
+    // (-1)^sign * 1, mantissa * 2 ^ (exponent - 127)
+    // Here we set the following values:
+    // sign = 0
+    // exponent = 127, for obtaining a zero exponent.
+    // mantissa = 23 right bits from generated uint32 random value.
+
     uint32_t x_uint32 = (static_cast<uint32_t>(127) << 23) | (x & 0x7fffffu);
 
     float x_float;
@@ -52,7 +74,16 @@ float uint32_to_float(uint32_t x) {
     return x_float - 1.0f;
 }
 
+// Helper function for converting uint32 values to float16.
 float16 uint32_to_float16(uint32_t x) {
+    // float16 is formatted as follows: sign(1 bit) exponent(5 bits) mantissa(10 bits). The value is interpreted
+    // The value is interpreted using following formula:
+    // (-1)^sign * 1, mantissa * 2 ^ (exponent - 15)
+    // Here we set the following values:
+    // sign = 0
+    // exponent = 15, for obtaining a zero exponent.
+    // mantissa = 10 right bits from generated uint32 random value.
+
     uint16_t x_uint16 = static_cast<uint16_t>(x);
     x_uint16 = (static_cast<uint16_t>(15) << 10) | (x_uint16 & 0x3ffu);
 
@@ -61,7 +92,16 @@ float16 uint32_to_float16(uint32_t x) {
     return x_float16 - static_cast<float16>(1);
 }
 
+// Helper function for converting uint32 values to double.
 double uint32_to_double(uint32_t x1, uint32_t x2) {
+    // float64 is formatted as follows: sign(1 bit) exponent(11 bits) mantissa(52 bits). The value is interpreted
+    // The value is interpreted using following formula:
+    // (-1)^sign * 1, mantissa * 2 ^ (exponent - 1023)
+    // Here we set the following values:
+    // sign = 0
+    // exponent = 1023, for obtaining a zero exponent.
+    // mantissa = 52 right bits from two concatenated uint32 values from random integer generator.
+
     uint64_t significant = ((static_cast<uint64_t>(x1) & 0xfffffu) << 32) | static_cast<uint64_t>(x2);
     uint64_t x_uint64 = ((static_cast<uint64_t>(1023) << 52) | significant);
 
@@ -70,11 +110,22 @@ double uint32_to_double(uint32_t x1, uint32_t x2) {
     return x_double - 1.0;
 }
 
+// Helper function for converting uint32 values to uint64.
 uint64_t uint32_to_uint64(uint32_t x1, uint32_t x2) {
+    // Concatenates x1 and x2 and casts to uint64_t.
     return (static_cast<uint64_t>(x2) << 32) | static_cast<uint64_t>(x1);
 }
 
+// Helper function for converting uint32 values to bfloat16.
 bfloat16 uint32_to_bfloat16(uint32_t x) {
+    // bfloat16 is formatted as follows: sign(1 bit) exponent(8 bits) mantissa(7 bits). The value is interpreted
+    // The value is interpreted using following formula:
+    // (-1)^sign * 1, mantissa * 2 ^ (exponent - 127)
+    // Here we set the following values:
+    // sign = 0
+    // exponent = 127, for obtaining a zero exponent.
+    // mantissa = 7 right bits from generated uint32 random value.
+
     uint16_t x_uint16 = static_cast<uint16_t>(x);
     x_uint16 = (static_cast<uint16_t>(127) << 7) | (x_uint16 & 0x7fu);
 
@@ -83,6 +134,7 @@ bfloat16 uint32_to_bfloat16(uint32_t x) {
     return x_bfloat16 - static_cast<bfloat16>(1);
 }
 
+// Runs Philox algorithm.
 void run_philox(uint64_t key, uint64_t counter, uint64_t n, size_t n_rounds, std::vector<uint32_t>& res) {
     for (size_t i = 0; i < n_rounds; i++) {
         calculate_round(key, counter, n);
@@ -97,6 +149,7 @@ void run_philox(uint64_t key, uint64_t counter, uint64_t n, size_t n_rounds, std
     res[3] = res2.second;
 }
 
+// Implementation of RandomUniform that uses Philox algorithm as inner random unsigned integer generator.
 void random_uniform(const uint64_t* out_shape,
                     const char* min_val,
                     const char* max_val,
@@ -117,59 +170,67 @@ void random_uniform(const uint64_t* out_shape,
     for (size_t i = 0; i < shape_count; i++) {
         elem_count *= out_shape[i];
     }
+    // Each run of Philox algorithm generates 4 uint32 values.
+    // If output_type is int32, f32, bf16, or f16 each value is converted to
+    // corresponding type so we have 4 result values. For f64 and i64 we use
+    // a pair of values for conversion, so we have 2 result values.
+    // Step indicates how many values we generate in one iteration.
     size_t step = elem_type.size() > 4 ? 2 : 4;
+
     for (size_t k = 0; k < elem_count; k += step) {
         // generate 4 random uint32 values using Philox algorithm
         std::vector<uint32_t> res(4);
         run_philox(key, counter, n, 10, res);
+
+        // convert values to corresponding output_type
         switch (elem_type) {
         case ngraph::element::Type_t::f32: {
-            float res_float[4];
-            std::transform(res.data(), res.data() + 4, res_float, uint32_to_float);
+            float res_float[step];
+            std::transform(res.data(), res.data() + step, res_float, uint32_to_float);
             float mn[1];
             float mx[1];
             memcpy(mn, min_val, elem_type.size());
             memcpy(mx, max_val, elem_type.size());
             // convert uint32 values to float32 and normalize to range
             // [min_val, max_val)
-            std::transform(res.data(), res.data() + 4, res_float, [&mn, &mx](uint32_t elem) {
+            std::transform(res.data(), res.data() + step, res_float, [&mn, &mx](uint32_t elem) {
                 return uint32_to_float(elem) * (mx[0] - mn[0]) + mn[0];
             });
 
-            memcpy(out + k * elem_type.size(), res_float, std::min((size_t)4, elem_count - k) * elem_type.size());
+            memcpy(out + k * elem_type.size(), res_float, std::min(step, elem_count - k) * elem_type.size());
             break;
         }
         case ngraph::element::Type_t::f16: {
-            float16 res_float16[4];
+            float16 res_float16[step];
             // convert uint32 values to float16 and normalize to range
             // [min_val, max_val)
-            std::transform(res.data(), res.data() + 4, res_float16, uint32_to_float16);
+            std::transform(res.data(), res.data() + step, res_float16, uint32_to_float16);
             float16 mn[1];
             float16 mx[1];
             memcpy(mn, min_val, elem_type.size());
             memcpy(mx, max_val, elem_type.size());
-            std::transform(res.data(), res.data() + 4, res_float16, [&mn, &mx](uint32_t elem) {
+            std::transform(res.data(), res.data() + step, res_float16, [&mn, &mx](uint32_t elem) {
                 return uint32_to_float16(elem) * (mx[0] - mn[0]) + mn[0];
             });
-            memcpy(out + k * elem_type.size(), res_float16, std::min((size_t)4, elem_count - k) * elem_type.size());
+            memcpy(out + k * elem_type.size(), res_float16, std::min(step, elem_count - k) * elem_type.size());
             break;
         }
         case ngraph::element::Type_t::bf16: {
-            bfloat16 res_bfloat16[4];
+            bfloat16 res_bfloat16[step];
             bfloat16 mn[1];
             bfloat16 mx[1];
             memcpy(mn, min_val, elem_type.size());
             memcpy(mx, max_val, elem_type.size());
             // convert uint32 values to bfloat16 and normalize to range
             // [min_val, max_val)
-            std::transform(res.data(), res.data() + 4, res_bfloat16, [&mn, &mx](uint32_t elem) {
+            std::transform(res.data(), res.data() + step, res_bfloat16, [&mn, &mx](uint32_t elem) {
                 return uint32_to_bfloat16(elem) * (mx[0] - mn[0]) + mn[0];
             });
-            memcpy(out + k * elem_type.size(), res_bfloat16, std::min((size_t)4, elem_count - k) * elem_type.size());
+            memcpy(out + k * elem_type.size(), res_bfloat16, std::min(step, elem_count - k) * elem_type.size());
             break;
         }
         case ngraph::element::Type_t::f64: {
-            double res_double[2];
+            double res_double[step];
             double mn[1];
             double mx[1];
             memcpy(mn, min_val, elem_type.size());
@@ -179,25 +240,25 @@ void random_uniform(const uint64_t* out_shape,
             // range [min_val, max_val)
             res_double[0] = uint32_to_double(res[0], res[1]) * (mx[0] - mn[0]) + mn[0];
             res_double[1] = uint32_to_double(res[2], res[3]) * (mx[0] - mn[0]) + mn[0];
-            memcpy(out + k * elem_type.size(), res_double, std::min((size_t)2, elem_count - k) * elem_type.size());
+            memcpy(out + k * elem_type.size(), res_double, std::min(step, elem_count - k) * elem_type.size());
             break;
         }
         case ngraph::element::Type_t::i32: {
-            int res_int[4];
+            int res_int[step];
             int mn[1];
             int mx[1];
             memcpy(mn, min_val, elem_type.size());
             memcpy(mx, max_val, elem_type.size());
             // convert uint32 values to int32 values and normalize to range
             // [min_val, max_val)
-            std::transform(res.data(), res.data() + 4, res_int, [&mn, &mx](uint32_t elem) {
+            std::transform(res.data(), res.data() + step, res_int, [&mn, &mx](uint32_t elem) {
                 return elem % (mx[0] - mn[0]) + mn[0];
             });
-            memcpy(out + k * elem_type.size(), res_int, std::min((size_t)4, elem_count - k) * elem_type.size());
+            memcpy(out + k * elem_type.size(), res_int, std::min(step, elem_count - k) * elem_type.size());
             break;
         }
         case ngraph::element::Type_t::i64: {
-            int64_t res_int64[2];
+            int64_t res_int64[step];
             int64_t mn[1];
             int64_t mx[1];
             memcpy(mn, min_val, elem_type.size());
@@ -206,7 +267,7 @@ void random_uniform(const uint64_t* out_shape,
             // range [min_val, max_val)
             res_int64[0] = uint32_to_uint64(res[0], res[1]) % (mx[0] - mn[0]) + mn[0];
             res_int64[1] = uint32_to_uint64(res[2], res[3]) % (mx[0] - mn[0]) + mn[0];
-            memcpy(out + k * elem_type.size(), res_int64, std::min((size_t)2, elem_count - k) * elem_type.size());
+            memcpy(out + k * elem_type.size(), res_int64, std::min(step, elem_count - k) * elem_type.size());
             break;
         }
         default:
