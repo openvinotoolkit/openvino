38ef7f826e897ae00a9a1fb54c892c63b05cda0c
Anna Likholat <anna.likholat@intel.com>
2021-08-17 16:31:55 +0300

    fix

--- ngraph/core/src/op/max_unpool.cpp
+++ ngraph/core/src/op/max_unpool.cpp
@@ -32,13 +32,54 @@ void op::v8::MaxPoolGrad::validate_and_infer_types() {
     outShape[1] = poolInpShape[1];  // and restore batch and channels
     set_output_type(0, get_input_element_type(0), outShape);
 }
-
+#include <typeinfo>
 bool op::v8::MaxPoolGrad::evaluate(const HostTensorVector& outputs,
                                const HostTensorVector& inputs) const
 {
     std::cout << "Unpool EVALUATE" << std::endl;
-    std::cout << inputs[0] << std::endl;
-    std::cout << inputs[1] << std::endl;
+    
+    const float* poolInp = static_cast<float*>(inputs[0]->get_data_ptr());
+    const float* poolOut = static_cast<float*>(inputs[1]->get_data_ptr());
+    const float* inp     = static_cast<float*>(inputs[2]->get_data_ptr());
+    float* out = static_cast<float*>(outputs[0]->get_data_ptr());
+
+    std::cout << *poolInp << std::endl;
+    std::cout << *poolOut << std::endl;
+    std::cout << *inp << std::endl;
+    std::cout << *out << std::endl;
+
+    // const float* poolInp = inputs[0]->cbuffer().as<float*>();
+    // const float* poolOut = inputs[1]->cbuffer().as<float*>();
+    // const float* inp     = inputs[2]->cbuffer().as<float*>();
+    // float* out = outputs[0]->buffer().as<float*>();
+
+    // std::vector<size_t> poolInpDims = inputs[0]->getTensorDesc().getDims();
+    // std::vector<size_t> poolOutDims = inputs[1]->getTensorDesc().getDims();
+    // std::vector<size_t> inpDims = inputs[2]->getTensorDesc().getDims();
+    // std::vector<size_t> outDims = outputs[0]->getTensorDesc().getDims();
+
+    // const size_t batch    = poolInpDims[0];
+    // const size_t channels = poolInpDims[1];
+    // const size_t height   = poolInpDims[2];
+    // const size_t width    = poolInpDims[3];
+    // const size_t outHeight = outDims[2];
+    // const size_t outWidth  = outDims[3];
+    // const size_t poolOutHeight = poolOutDims[2];
+    // const size_t poolOutWidth  = poolOutDims[3];
+    // InferenceEngine::parallel_for(batch*channels, [&](size_t d) {
+    //     for (int y = height - 1; y >= 0; --y) {
+    //         for (int x = width - 1; x >= 0; --x) {
+    //             int poolOutIdx = (d * poolOutHeight + y / 2) * poolOutWidth + x / 2;
+    //             int poolInpIdx = (d * height + y) * width + x;
+    //             int dstIdx = d * outHeight * outWidth + (y * width + x);
+    //             if (fabs(poolInp[poolInpIdx] - poolOut[poolOutIdx]) < 1e-6f) {
+    //                 out[dstIdx] = inp[poolOutIdx];
+    //             } else {
+    //                 out[dstIdx] = 0;
+    //             }
+    //         }
+    //     }
+    // });
     return true;
 }
 
