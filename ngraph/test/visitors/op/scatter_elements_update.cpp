0aba150b75b8d574f5c392e32cc17d7e3d443e12
blesniewski <bartosz.lesniewski@intel.com>
2021-08-17 11:50:12 +0200

    Add SSLT

--- ngraph/test/visitors/op/scatter_elements_update.cpp
+++ ngraph/test/visitors/op/scatter_elements_update.cpp
@@ -16,12 +16,12 @@ TEST(attributes, scatter_elements_update) {
     NodeBuilder::get_ops().register_factory<opset3::ScatterElementsUpdate>();
 
     auto data = std::make_shared<op::Parameter>(element::f32, Shape{2, 4, 5, 7});
-    auto indices = std::make_shared<op::Parameter>(element::i16, {2, 2, 2, 2});
-    auto updates = std::make_shared<op::Parameter>(element::f32, {2, 2, 2, 2});
-    auto axis = std::make_shared<op::Parameter>(element::i16, {});
+    auto indices = std::make_shared<op::Parameter>(element::i16, Shape{2, 2, 2, 2});
+    auto updates = std::make_shared<op::Parameter>(element::f32, Shape{2, 2, 2, 2});
+    auto axis = std::make_shared<op::Parameter>(element::i16, Shape{});
 
     auto scatter = std::make_shared<opset3::ScatterElementsUpdate>(data, indices, updates, axis);
-    NodeBuilder(scatter);
+    NodeBuilder builder(scatter);
 
     const auto expected_attr_count = 0;
     EXPECT_EQ(builder.get_value_map_size(), expected_attr_count);
