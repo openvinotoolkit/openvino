b5332f7e9042bad104a0f5953410e4f569701a83
achetver <Anton.Chetverikov@intel.com>
2021-08-17 15:36:07 +0300

    Fix incorrect comment

--- model-optimizer/extensions/front/tf/CTCGreedyDecoder_ext.py
+++ model-optimizer/extensions/front/tf/CTCGreedyDecoder_ext.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018-2021 Intel Corporationoutput_sparse_format = True
+# Copyright (C) 2018-2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
 from extensions.ops.ctc_greedy_decoder_seq_len import CTCGreedyDecoderSeqLenOp
@@ -13,7 +13,7 @@ class CTCCGreedyDecoderFrontExtractor(FrontExtractorOp):
     def extract(cls, node):
         attrs = {
             'merge_repeated': bool(node.pb.attr['merge_repeated'].b),
-            'output_sparse_format': True,  #
+            'output_sparse_format': True,  # Special argument for TF CTCGreedyDecoder replacement transformations
         }
         CTCGreedyDecoderSeqLenOp.update_node_stat(node, attrs)
         return cls.enabled
