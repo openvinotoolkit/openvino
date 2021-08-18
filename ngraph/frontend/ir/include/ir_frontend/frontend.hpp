58da119bf529353be585bfdb4db5507b157adfed
Gleb Kazantaev <gleb.nnstu@gmail.com>
2021-08-18 20:34:04 +0300

    IR FronteEnd

---
+++ ngraph/frontend/ir/include/ir_frontend/frontend.hpp
@@ -0,0 +1,87 @@
+// Copyright (C) 2018-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <ngraph/variant.hpp>
+
+#include <frontend_manager/frontend_manager.hpp>
+#include <pugixml.hpp>
+#include <inference_engine.hpp>
+
+#include "utility.hpp"
+
+namespace ngraph {
+namespace frontend {
+
+class IR_API FrontEndIR : public FrontEnd {
+public:
+    FrontEndIR() = default;
+
+    /// \brief Completely convert the remaining, not converted part of a function.
+    /// \param partiallyConverted partially converted nGraph function
+    /// \return fully converted nGraph function
+    std::shared_ptr<Function> convert(InputModel::Ptr model) const override;
+
+    /// \brief Gets name of this FrontEnd. Can be used by clients
+    /// if frontend is selected automatically by FrontEndManager::load_by_model
+    ///
+    /// \return IR frontend name.
+    std::string get_name() const override;
+
+//    InputModel::Ptr load(const pugi::xml_node & xml_node) const {
+//        return load_impl({make_variant(xml_node)});
+//    }
+
+protected:
+    /// \brief Check if FrontEndPDPD can recognize model from given parts
+    /// \param params Can be path to folder which contains __model__ file or path to
+    /// .pdmodel file
+    /// \return InputModel::Ptr
+    bool supported_impl(const std::vector<std::shared_ptr<Variant>>& variants) const override;
+
+    /// \brief Reads model from 1 or 2 given file names or 1 or 2 std::istream containing
+    /// model in protobuf format and weights
+    /// \param params Can contain path to folder with __model__ file or path to .pdmodel
+    /// file or 1 or 2 streams with model and weights
+    /// \return InputModel::Ptr
+    InputModel::Ptr load_impl(const std::vector<std::shared_ptr<Variant>>& params) const override;
+};
+
+}  // namespace frontend
+}  // namespace ngraph
+
+namespace ov {
+
+template <>
+class IR_API VariantWrapper<pugi::xml_node> : public VariantImpl<pugi::xml_node> {
+public:
+    static constexpr VariantTypeInfo type_info{"Variant::pugi::xml_node", 0};
+    const VariantTypeInfo& get_type_info() const override {
+        return type_info;
+    }
+    VariantWrapper(const value_type& value) : VariantImpl<value_type>(value) {}
+};
+
+template <>
+class IR_API VariantWrapper<InferenceEngine::Blob::CPtr> : public VariantImpl<InferenceEngine::Blob::CPtr> {
+public:
+    static constexpr VariantTypeInfo type_info{"Variant::Blob::CPtr", 0};
+    const VariantTypeInfo& get_type_info() const override {
+        return type_info;
+    }
+    VariantWrapper(const value_type& value) : VariantImpl<value_type>(value) {}
+};
+
+template <>
+class IR_API VariantWrapper<std::vector<InferenceEngine::IExtensionPtr>> : public VariantImpl<std::vector<InferenceEngine::IExtensionPtr>> {
+public:
+    static constexpr VariantTypeInfo type_info{"Variant::Extensions", 0};
+    const VariantTypeInfo& get_type_info() const override {
+        return type_info;
+    }
+    VariantWrapper(const value_type& value) : VariantImpl<value_type>(value) {}
+};
+
+}  // namespace ov
---
+++ ngraph/frontend/ir/src/frontend.cpp
@@ -0,0 +1,84 @@
+// Copyright (C) 2018-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <map>
+#include <vector>
+
+#include <ngraph/variant.hpp>
+
+#include <ir_frontend/frontend.hpp>
+#include <ir_frontend/model.hpp>
+#include <ir_frontend/utility.hpp>
+
+using namespace ngraph;
+
+namespace ngraph {
+namespace frontend {
+
+bool FrontEndIR::supported_impl(const std::vector<std::shared_ptr<Variant>>& variants) const {
+    // FrontEndIR can only load model specified by xml_node, weights_ptr and vector of extensions
+    if (variants.size() != 3) {
+        return false;
+    }
+
+    bool has_xml_node{false}, has_weights{false}, has_exts{false};
+
+    for (const auto & variant : variants) {
+        if (is_type<VariantWrapper<pugi::xml_node>>(variant)) {
+            has_xml_node = true;
+        } else if (is_type<VariantWrapper<InferenceEngine::Blob::CPtr>>(variant)) {
+            has_weights = true;
+        } else if (is_type<VariantWrapper<std::vector<InferenceEngine::IExtensionPtr>>>(variant)) {
+            has_exts = true;
+        }
+        return false;
+    }
+
+    return has_xml_node && has_weights && has_exts;
+}
+
+InputModel::Ptr FrontEndIR::load_impl(const std::vector<std::shared_ptr<Variant>>& variants) const {
+    pugi::xml_node root;
+    InferenceEngine::Blob::CPtr weights;
+    std::vector<InferenceEngine::IExtensionPtr> exts;
+
+    for (const auto & variant : variants) {
+        if (is_type<VariantWrapper<pugi::xml_node>>(variant)) {
+            root = as_type_ptr<VariantWrapper<pugi::xml_node>>(variant)->get();
+        } else if (is_type<VariantWrapper<InferenceEngine::Blob::CPtr>>(variant)) {
+            weights = as_type_ptr<VariantWrapper<InferenceEngine::Blob::CPtr>>(variant)->get();
+        } else if (is_type<VariantWrapper<std::vector<InferenceEngine::IExtensionPtr>>>(variant)) {
+            exts = as_type_ptr<VariantWrapper<std::vector<InferenceEngine::IExtensionPtr>>>(variant)->get();
+        }
+    }
+    return std::make_shared<InputModelIR>(root, weights, exts);
+}
+
+std::shared_ptr<ngraph::Function> FrontEndIR::convert(InputModel::Ptr model) const {
+    auto ir_model = std::dynamic_pointer_cast<InputModelIR>(model);
+    return ir_model->convert();
+}
+
+std::string FrontEndIR::get_name() const {
+    return "ir";
+}
+}  // namespace frontend
+}  // namespace ngraph
+
+extern "C" IR_API ngraph::frontend::FrontEndVersion GetAPIVersion() {
+    return OV_FRONTEND_API_VERSION;
+}
+
+extern "C" IR_API void* GetFrontEndData() {
+    frontend::FrontEndPluginInfo* res = new frontend::FrontEndPluginInfo();
+    res->m_name = "ir";
+    res->m_creator = []() {
+        return std::make_shared<frontend::FrontEndIR>();
+    };
+    return res;
+}
+
+constexpr VariantTypeInfo VariantWrapper<pugi::xml_node>::type_info;
+constexpr VariantTypeInfo VariantWrapper<InferenceEngine::Blob::CPtr>::type_info;
+constexpr VariantTypeInfo VariantWrapper<std::vector<InferenceEngine::IExtensionPtr>>::type_info;
