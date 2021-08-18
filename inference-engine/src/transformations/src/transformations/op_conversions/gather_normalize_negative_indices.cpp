6a3741c42c0b54366c4ae45139f0a85942335f05
Pavel Esir <pavel.esir@intel.com>
2021-08-18 16:03:35 +0300

    updated pattern matcher into GatherBase in negative indices resolver, so that it is triggered in all versions of operation

--- inference-engine/src/transformations/src/transformations/op_conversions/gather_normalize_negative_indices.cpp
+++ inference-engine/src/transformations/src/transformations/op_conversions/gather_normalize_negative_indices.cpp
@@ -18,11 +18,11 @@ ngraph::pass::GatherNegativeConstIndicesNormalize::GatherNegativeConstIndicesNor
     auto data_input = ngraph::pattern::any_input(pattern::has_static_rank());
     auto axis_input = ngraph::pattern::wrap_type<ngraph::opset7::Constant>();
     auto indices_input = ngraph::pattern::wrap_type<ngraph::opset7::Constant>();
-    auto gather_node = std::make_shared<ngraph::opset7::Gather>(data_input, indices_input, axis_input);
+    auto gather_node = ngraph::pattern::wrap_type<op::util::GatherBase>({data_input, indices_input, axis_input});
 
     ngraph::matcher_pass_callback callback = [=](ngraph::pattern::Matcher& m) {
         auto& pattern_to_output = m.get_pattern_value_map();
-        auto gather = std::dynamic_pointer_cast<ngraph::opset7::Gather>(pattern_to_output.at(gather_node).get_node_shared_ptr());
+        auto gather = std::dynamic_pointer_cast<ngraph::Node>(pattern_to_output.at(gather_node).get_node_shared_ptr());
         auto data = pattern_to_output.at(data_input);
         auto axis_constant = std::dynamic_pointer_cast<ngraph::opset7::Constant>(pattern_to_output.at(axis_input).get_node_shared_ptr());
         auto indices_constant = std::dynamic_pointer_cast<ngraph::opset7::Constant>(pattern_to_output.at(indices_input).get_node_shared_ptr());
@@ -63,11 +63,7 @@ ngraph::pass::GatherNegativeConstIndicesNormalize::GatherNegativeConstIndicesNor
             ngraph::opset7::Constant::create(input_type, Shape{}, {axis_value}), ngraph::opset7::Constant::create(input_type, Shape{}, {0}));
 
         auto add = std::make_shared<ngraph::opset7::Add>(input_gather, indices_constant);
-        auto gather_new = gather_node->copy_with_new_inputs({data, add, axis_constant});
-        gather_new->set_friendly_name(gather->get_friendly_name());
-
-        ngraph::copy_runtime_info(gather, {shape_of, input_gather, add, gather_new});
-        ngraph::replace_node(gather, gather_new);
+        gather->input(1).replace_source_output(add);
 
         return true;
     };
