2c97f1e9d10ac4ec5c239f53f0a4f1442a11a64f
pszmel <piotr.szmelczynski@intel.com>
2021-08-23 10:07:42 +0200

    update op_reference logical tests to take input as a vector

--- docs/template_plugin/tests/functional/op_reference/logical_not.cpp
+++ docs/template_plugin/tests/functional/op_reference/logical_not.cpp
@@ -24,7 +24,7 @@ std::vector<RefLogicalParams> generateLogicalParams() {
     std::vector<RefLogicalParams> logicalParams {
         Builder {}
             .opType(LogicalTypes::LOGICAL_NOT)
-            .input1({{2, 2}, element::boolean, std::vector<char> {true, false, true, false}})
+            .inputs({{{2, 2}, element::boolean, std::vector<char> {true, false, true, false}}})
             .expected({{2, 2}, element::boolean, std::vector<char> {false, true, false, true}})};
     return logicalParams;
 }
