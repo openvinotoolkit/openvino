185bffe576e63db50b913e00c294ab5a68c53b42
Ilya Churaev <ilya.churaev@intel.com>
2021-08-26 06:45:07 +0300

    Moved AxisSet, AxisVector, Coordinate, CoordinateDiff to ov (#7237)

--- ngraph/core/include/ngraph/axis_set.hpp
+++ ngraph/core/include/ngraph/axis_set.hpp
@@ -11,53 +11,8 @@
 
 #include "ngraph/attribute_adapter.hpp"
 #include "ngraph/ngraph_visibility.hpp"
+#include "openvino/core/axis_set.hpp"
 
 namespace ngraph {
-/// \brief A set of axes.
-class AxisSet : public std::set<size_t> {
-public:
-    NGRAPH_API AxisSet();
-
-    NGRAPH_API AxisSet(const std::initializer_list<size_t>& axes);
-
-    NGRAPH_API AxisSet(const std::set<size_t>& axes);
-
-    NGRAPH_API AxisSet(const std::vector<size_t>& axes);
-
-    NGRAPH_API AxisSet(const AxisSet& axes);
-
-    NGRAPH_API AxisSet& operator=(const AxisSet& v);
-
-    NGRAPH_API AxisSet& operator=(AxisSet&& v) noexcept;
-
-    NGRAPH_API std::vector<int64_t> to_vector() const;
-};
-
-NGRAPH_API
-std::ostream& operator<<(std::ostream& s, const AxisSet& axis_set);
+using ov::AxisSet;
 }  // namespace ngraph
-
-namespace ov {
-
-template <>
-class NGRAPH_API AttributeAdapter<ngraph::AxisSet> : public ValueAccessor<std::vector<int64_t>> {
-public:
-    AttributeAdapter(ngraph::AxisSet& value) : m_ref(value) {}
-
-    const std::vector<int64_t>& get() override;
-    void set(const std::vector<int64_t>& value) override;
-    static constexpr DiscreteTypeInfo type_info{"AttributeAdapter<AxisSet>", 0};
-    const DiscreteTypeInfo& get_type_info() const override {
-        return type_info;
-    }
-    operator ngraph::AxisSet&() {
-        return m_ref;
-    }
-
-protected:
-    ngraph::AxisSet& m_ref;
-    std::vector<int64_t> m_buffer;
-    bool m_buffer_valid{false};
-};
-
-}  // namespace ov
---
+++ ngraph/core/include/openvino/core/coordinate_diff.hpp
@@ -0,0 +1,56 @@
+// Copyright (C) 2018-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <cstddef>
+#include <ostream>
+#include <vector>
+
+#include "openvino/core/attribute_adapter.hpp"
+#include "openvino/core/core_visibility.hpp"
+
+namespace ov {
+/// \brief A difference (signed) of tensor element coordinates.
+class CoordinateDiff : public std::vector<std::ptrdiff_t> {
+public:
+    OPENVINO_API CoordinateDiff(const std::initializer_list<std::ptrdiff_t>& diffs);
+
+    OPENVINO_API CoordinateDiff(const std::vector<std::ptrdiff_t>& diffs);
+
+    OPENVINO_API CoordinateDiff(const CoordinateDiff& diffs);
+
+    OPENVINO_API explicit CoordinateDiff(size_t n, std::ptrdiff_t initial_value = 0);
+
+    template <class InputIterator>
+    CoordinateDiff(InputIterator first, InputIterator last) : std::vector<std::ptrdiff_t>(first, last) {}
+
+    OPENVINO_API ~CoordinateDiff();
+
+    OPENVINO_API CoordinateDiff();
+
+    OPENVINO_API CoordinateDiff& operator=(const CoordinateDiff& v);
+
+    OPENVINO_API CoordinateDiff& operator=(CoordinateDiff&& v) noexcept;
+};
+
+OPENVINO_API
+std::ostream& operator<<(std::ostream& s, const CoordinateDiff& coordinate_diff);
+
+template <>
+class OPENVINO_API AttributeAdapter<CoordinateDiff>
+    : public IndirectVectorValueAccessor<CoordinateDiff, std::vector<int64_t>>
+
+{
+public:
+    AttributeAdapter(CoordinateDiff& value)
+        : IndirectVectorValueAccessor<CoordinateDiff, std::vector<int64_t>>(value) {}
+
+    static constexpr DiscreteTypeInfo type_info{"AttributeAdapter<CoordinateDiff>", 0};
+    const DiscreteTypeInfo& get_type_info() const override {
+        return type_info;
+    }
+};
+
+}  // namespace ov
