df3a646423782ade8f10a82a376b8513300e749b
chenhuwa <chenhu.wang@intel.com>
2021-08-27 15:30:33 +0800

    optimize tails by apply load store emitters

--- inference-engine/src/mkldnn_plugin/nodes/mkldnn_interpolate_node.h
+++ inference-engine/src/mkldnn_plugin/nodes/mkldnn_interpolate_node.h
@@ -52,13 +52,13 @@ enum class InterpolateShapeCalcMode {
 struct jit_interpolate_config_params {
     InterpolateLayoutType layout;
     InterpolateMode mode;
-    mkldnn::memory::data_type src_dt;
-    mkldnn::memory::data_type dst_dt;
+    InferenceEngine::Precision src_prc;
+    InferenceEngine::Precision dst_prc;
     int src_data_size;
     int dst_data_size;
     int indices_size;
     int spatial_dim_size;
-    int ID, IH, IW, OD, OH, OW;
+    int C, ID, IH, IW, OD, OH, OW;
 };
 
 struct jit_interpolate_call_args {
