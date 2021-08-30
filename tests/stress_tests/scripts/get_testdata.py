6bc06d7c4c90fa8ec7a253b66cb2016c8fd5f050
Koryachikhina, Anastasia <anastasia.koryachikhina@intel.com>
2021-08-30 15:23:32 +0300

    requirements test

--- tests/stress_tests/scripts/get_testdata.py
+++ tests/stress_tests/scripts/get_testdata.py
@@ -134,7 +134,7 @@ def main():
         requirements = [
             omz_path / "tools" / "downloader" / "requirements.in",
             args.mo_tool.parent / "requirements.txt",
-            Path(abs_path("../scripts/")) / "requirements.txt",
+            #Path(abs_path("../scripts/")) / "requirements.txt",
             # args.mo_tool.parent / "requirements_dev.txt",
             omz_path / "tools" / "downloader" / "requirements-caffe2.in",
             omz_path / "tools" / "downloader" / "requirements-pytorch.in"
