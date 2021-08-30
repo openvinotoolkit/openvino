8568dde360cdee44cd469e2f76f68a9b8615ce4e
iimironov <iliya.mironov@intel.com>
2021-08-30 13:44:47 +0300

    Update range_like replacer

--- model-optimizer/extensions/front/mxnet/arange_like_replacer.py
+++ model-optimizer/extensions/front/mxnet/arange_like_replacer.py
@@ -10,6 +10,7 @@ from mo.front.common.replacement import FrontReplacementOp
 from mo.front.tf.graph_utils import create_op_with_const_inputs
 from mo.graph.graph import Graph
 from mo.ops.shape import Shape
+from mo.ops.reshape import Reshape
 
 
 class ArangeLikeReplacer(FrontReplacementOp):
@@ -20,17 +21,43 @@ class ArangeLikeReplacer(FrontReplacementOp):
         node = match['op']
         name = node.soft_get('name')
         axis = node.soft_get('axis')
+        shape_node = Shape(graph, {'name': name + '/shape'}).create_node()
+        range_node = create_op_with_const_inputs(graph, Range, {0: int64_array([0]),
+                                                                2: int64_array([1])},
+                                                 {'name': name + '/Range', 'output_type': np.int32})
+        shape_node.in_port(0).connect(node.in_port(0).get_source())
         if axis:
-            shape = Shape(graph, {'name': name + '/shape'}).create_node()
+            '''
+            Replace arange_like op to subgraph:
+            Shape - Gather - Range
+            '''
             gather_node = create_op_with_const_inputs(graph, Gather, {1: int64_array([axis]),
                                                                       2: int64_array(0)},
                                                       {'name': name + '/Gather'})
-            range_node = create_op_with_const_inputs(graph, Range, {0: int64_array([0]),
-                                                                    2: int64_array([1])},
-                                                     {'name': name + '/Range', 'output_type': np.int32})
-            shape.in_port(0).connect(node.in_port(0).get_source())
-            shape.out_port(0).connect(gather_node.in_port(0))
+            shape_node.out_port(0).connect(gather_node.in_port(0))
             gather_node.out_port(0).connect(range_node.in_port(1))
             node.out_port(0).get_connection().set_source(range_node.out_port(0))
             node.in_port(0).disconnect()
-            print("OK")
+        else:
+            '''
+            Replace arange_like op to subgraph:
+                    |
+                  Shape     
+                 /     \
+            Reshape     |
+               |        |
+             Range      | 
+                \      /
+                 Reshape
+                    |
+            '''
+
+            reshape_node1 = create_op_with_const_inputs(graph, Reshape, {1: int64_array([-1])},
+                                        {'name': name + '/Reshape_forward', 'output_type': np.int32})
+
+            reshape_node2 = Reshape(graph, {'name': name + '/Reshape_backward'}).create_node()
+            shape_node.out_port(0).connect(reshape_node1.in_port(0))
+            reshape_node1.out_port(0).connect(range_node.in_port(1))
+            range_node.out_port(0).connect(reshape_node2.in_port(0))
+            shape_node.out_port(0).connect(reshape_node2.in_port(1))
+            node.out_port(0).get_connection().set_source(reshape_node2.out_port(0))
