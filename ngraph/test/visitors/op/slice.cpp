e109c8eccef622a294a9b19ad3424cd07079022d
mitruska <katarzyna.mitrus@intel.com>
2021-08-30 08:38:28 +0200

    Add visitors test

--- ngraph/test/visitors/op/slice.cpp
+++ ngraph/test/visitors/op/slice.cpp
@@ -13,4 +13,30 @@ using namespace ngraph;
 using ngraph::test::NodeBuilder;
 
 TEST(attributes, slice_op_no_axes) {
+    NodeBuilder::get_ops().register_factory<opset8::Slice>();
+    const auto data = make_shared<op::Parameter>(element::f32, Shape{1, 3, 5, 4});
+    const auto start = make_shared<op::Parameter>(element::f32, Shape{4});
+    const auto stop = make_shared<op::Parameter>(element::f32, Shape{4});
+    const auto step = make_shared<op::Parameter>(element::f32, Shape{4});
+
+    const auto op = make_shared<opset8::Slice>(data, start, stop, step);
+    NodeBuilder builder(op);
+
+    const auto expected_attr_count = 0;
+    EXPECT_EQ(builder.get_value_map_size(), expected_attr_count);
+}
+
+TEST(attributes, slice_op_with_axes) {
+    NodeBuilder::get_ops().register_factory<opset8::Slice>();
+    const auto data = make_shared<op::Parameter>(element::f32, Shape{1, 3, 5, 4});
+    const auto start = make_shared<op::Parameter>(element::f32, Shape{4});
+    const auto stop = make_shared<op::Parameter>(element::f32, Shape{4});
+    const auto step = make_shared<op::Parameter>(element::f32, Shape{4});
+    const auto axes = make_shared<op::Parameter>(element::f32, Shape{4});
+
+    const auto op = make_shared<opset8::Slice>(data, start, stop, step, axes);
+    NodeBuilder builder(op);
+
+    const auto expected_attr_count = 0;
+    EXPECT_EQ(builder.get_value_map_size(), expected_attr_count);
 }
