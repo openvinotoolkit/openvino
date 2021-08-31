cc48e904ec563431427f629f498a18e0e778efa9
nsemaev <nikita.semaev@intel.com>
2021-08-31 12:56:01 +0300

    New version with Code Style checking

--- docs/template_plugin/tests/functional/op_reference/notequal.cpp
+++ docs/template_plugin/tests/functional/op_reference/notequal.cpp
@@ -15,64 +15,108 @@ using namespace ngraph;
 using namespace InferenceEngine;
 using ComparisonTypes = ngraph::helpers::ComparisonTypes;
 
+
 namespace reference_tests {
-namespace ComparisonOpsRefTestDefinitions {
-namespace {
-    TEST_P(ReferenceComparisonLayerTest, GreaterCompareWithHardcodedRefs) {
-        Exec();
-    }
+    namespace ComparisonOpsRefTestDefinitions {
+        namespace {
 
-    template <element::Type_t IN_ET>
-    std::vector<RefComparisonParams> generateComparisonParams(const element::Type& type) {
-        using T = typename element_type_traits<IN_ET>::value_type;
-        std::vector<RefComparisonParams> compParams {
-            Builder {}
-            .compType(ComparisonTypes::NOT_EQUAL)
-            .input1({{2, 2}, type, std::vector<T> {1, 0, 10, 999}})
-            .input2({{2, 2}, type, std::vector<T> {1, 0, 10, 999}})
-            .expected({{2, 2}, element::boolean, std::vector<char> {0, 0, 0, 0}}),
-            Builder {}
-            .compType(ComparisonTypes::NOT_EQUAL)
-            .input1({{2, 3}, type, std::vector<T> {0, 15, 45, 10, 5, 10}})
-            .input2({{2, 3}, type, std::vector<T> {1, 15, -45, 10, 50, 10}})
-            .expected({{2, 3}, element::boolean, std::vector<char> {1, 0, 1, 0, 1, 0}}),
-            Builder {}
-            .compType(ComparisonTypes::NOT_EQUAL)
-            .input1({{1}, type, std::vector<T> {23}})
-            .input2({{1}, type, std::vector<T> {23}})
-            .expected({{1}, element::boolean, std::vector<char> {0}}),
-            Builder {}
-            .compType(ComparisonTypes::NOT_EQUAL)
-            .input1({{1}, type, std::vector<T> {20}})
-            .input2({{1}, type, std::vector<T> {10}})
-            .expected({{1}, element::boolean, std::vector<char> {1}}),
-            Builder {}
-            .compType(ComparisonTypes::NOT_EQUAL)
-            .input1({{2, 4}, type, std::vector<T> {0, 12, 23, 0, 1, 5, 12, 8}})
-            .input2({{2, 4}, type, std::vector<T> {0, 12, 23, 0, 10, 5, 11, 8}})
-            .expected({{2, 4}, element::boolean, std::vector<char> {0, 0, 0, 0, 1, 0, 1, 0}});
-        return compParams;
-    }
+            template <element::Type_t IN_ET>
+            std::vector<RefComparisonParams> generateComparisonParams(const element::Type& type) {
+                using T = typename element_type_traits<IN_ET>::value_type;
+                std::vector<RefComparisonParams> compParams {
+                    // 1D // 2D // 3D // 4D
+                    Builder {}
+                    .compType(ComparisonTypes::NOT_EQUAL)
+                    .input1({{2, 2}, type, std::vector<T> {1, 0, 10, 999}})
+                    .input2({{2, 2}, type, std::vector<T> {1, 0, 10, 999}})
+                    .expected({{2, 2}, element::boolean, std::vector<char> {1, 1, 1, 1}}),
+                    Builder {}
+                    .compType(ComparisonTypes::NOT_EQUAL)
+                    .input1({{2, 3}, type, std::vector<T> {0, 15, 45, 10, 5, 10}})
+                    .input2({{2, 3}, type, std::vector<T> {1, 15, -45, 10, 50, 10}})
+                    .expected({{2, 3}, element::boolean, std::vector<char> {1, 0, 1, 0, 1, 0}}),
+                    Builder {}
+                    .compType(ComparisonTypes::NOT_EQUAL)
+                    .input1({{1}, type, std::vector<T> {20}})
+                    .input2({{1}, type, std::vector<T> {10}})
+                    .expected({{1}, element::boolean, std::vector<char> {1}}),
+                    Builder {}
+                    .compType(ComparisonTypes::NOT_EQUAL)
+                    .input1({{2, 4}, type, std::vector<T> {0, 12, 23, 0, 1, 5, 12, 8}})
+                    .input2({{2, 4}, type, std::vector<T> {0, 12, 23, 0, 10, 5, 11, 8}})
+                    .expected({{2, 4}, element::boolean, std::vector<char> {0, 0, 0, 0, 1, 0, 1, 0}}),
+                    Builder {}
+                    .compType(ComparisonTypes::NOT_EQUAL)
+                    .input1({{3, 1, 2}, type, std::vector<T> {2, 1, 4, 1, 3, 1}})
+                    .input2({{1, 2, 1}, type, std::vector<T> {1, 1}})
+                    .expected({{3, 2, 2}, element::boolean, std::vector<char> {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1}}),
+                    Builder {}
+                    .compType(ComparisonTypes::NOT_EQUAL)
+                    .input1({{2, 1, 2, 1}, type, std::vector<T> {2, 1, 4, 1}})
+                    .input2({{1, 2, 1}, type, std::vector<T> {1, 1}})
+                    .expected({{2, 1, 2, 1}, element::boolean, std::vector<char> {0, 1, 0, 1}})};
+                return compParams;
+            }
 
-    std::vector<RefComparisonParams> generateComparisonCombinedParams() {
-        const std::vector<std::vector<RefComparisonParams>> compTypeParams {
-            generateComparisonParams<element::Type_t::f32>(element::f32),
-            generateComparisonParams<element::Type_t::f16>(element::f16),
-            generateComparisonParams<element::Type_t::i32>(element::i32),
-            generateComparisonParams<element::Type_t::i64>(element::i64),
-            generateComparisonParams<element::Type_t::u32>(element::u32),
-            generateComparisonParams<element::Type_t::u64>(element::u64),
-            generateComparisonParams<element::Type_t::boolean>(element::boolean)};
-        std::vector<RefComparisonParams> combinedParams;
+            std::vector<RefComparisonParams> generateComparisonCombinedParams() {
+                const std::vector<std::vector<RefComparisonParams>> compTypeParams {
+                    generateComparisonParams<element::Type_t::f32>(element::f32),
+                    generateComparisonParams<element::Type_t::f16>(element::f16),
+                    generateComparisonParams<element::Type_t::i32>(element::i32),
+                    generateComparisonParams<element::Type_t::u32>(element::u32),
+                    generateComparisonParams<element::Type_t::u8>(element::boolean)};
+                std::vector<RefComparisonParams> combinedParams;
 
-        for (const auto& params : compTypeParams) {
-            combinedParams.insert(combinedParams.end(), params.begin(), params.end());
-        }
-        return combinedParams;
-    }
+                for (const auto& params : compTypeParams) {
+                    combinedParams.insert(combinedParams.end(), params.begin(), params.end());
+                }
+                return combinedParams;
+            }
 
-} // namespace
-INSTANTIATE_TEST_SUITE_P(smoke_Comparison_With_Hardcoded_Refs, ReferenceComparisonLayerTest, ::testing::ValuesIn(generateComparisonCombinedParams()),
-                         ReferenceComparisonLayerTest::getTestCaseName);
-} // namespace ComparisonOpsRefTestDefinitions
+            INSTANTIATE_TEST_SUITE_P(smoke_Comparison_With_Hardcoded_Refs, ReferenceComparisonLayerTest, ::testing::ValuesIn(generateComparisonCombinedParams()),
+                                     ReferenceComparisonLayerTest::getTestCaseName);
+
+            template <element::Type_t IN_ET>
+            std::vector<RefComparisonParams> generateNumericParams(const element::Type& type) {
+                using T = typename element_type_traits<IN_ET>::value_type;
+                std::vector<RefComparisonParams> compParams {
+                    Builder {}
+                    .compType(ComparisonTypes::NOT_EQUAL)
+                    .input1({{4}, type, std::vector<T> {-2.5f, 25.5f, 2.25f, NAN}})
+                    .input2({{4}, type, std::vector<T> {10.0f, 5.0f, 2.25f, 10.0f}})
+                    .expected({{4}, element::boolean, std::vector<char> {0, 0, 1, 0, }}),
+                    Builder {}
+                    .compType(ComparisonTypes::NOT_EQUAL)
+                    .input1({{2, 3}, type, std::vector<T> {0.0f, NAN, NAN, 1.0f, 21.0f, -INFINITY}})
+                    .input2({{2, 3}, type, std::vector<T> {1.0f, NAN, 23.0f, 1.0f, 19.0f, 21.0f}})
+                    .expected({{2, 3}, element::boolean, std::vector<char> {0, 0, 0, 1, 0, 0}}),
+                    Builder {}
+                    .compType(ComparisonTypes::NOT_EQUAL)
+                    .input1({{1}, type, std::vector<T> {INFINITY}})
+                    .input2({{1}, type, std::vector<T> {INFINITY}})
+                    .expected({{1}, element::boolean, std::vector<char> {1}}),
+                    Builder {}
+                    .compType(ComparisonTypes::NOT_EQUAL)
+                    .input1({{5}, type, std::vector<T> {-2.5f, 25.5f, 2.25f, INFINITY, 6.0f}})
+                    .input2({{5}, type, std::vector<T> {10.0f, 5.0f, 2.25f, 10.0f, -INFINITY}})
+                    .expected({{5}, element::boolean, std::vector<char> {0, 0, 1, 0, 0}})};
+                return compParams;
+            }
+
+            std::vector<RefComparisonParams> generateNumericCombinedParams() {
+                const std::vector<std::vector<RefComparisonParams>> compTypeParams {
+                    generateNumericParams<element::Type_t::f16>(element::f16),
+                    generateNumericParams<element::Type_t::f32>(element::f32)};
+                std::vector<RefComparisonParams> combinedParams;
+
+                for (const auto& params : compTypeParams) {
+                    combinedParams.insert(combinedParams.end(), params.begin(), params.end());
+                }
+                return combinedParams;
+            }
+
+            INSTANTIATE_TEST_SUITE_P(smoke_Numeric_With_Hardcoded_Refs, ReferenceComparisonLayerTest, ::testing::ValuesIn(generateNumericCombinedParams()),
+                                     ReferenceComparisonLayerTest::getTestCaseName);
+        } // namespace
+    } // namespace ComparisonOpsRefTestDefinitions
 } // namespace reference_tests
