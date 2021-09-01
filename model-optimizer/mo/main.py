54b9b23651eeb4dece72d4a48bb5aadc65930846
Michal Karzynski <michal.karzynski@intel.com>
2021-09-01 17:11:45 +0200

    Change the logic which Model Optimizer uses to load frontends

--- model-optimizer/mo/main.py
+++ model-optimizer/mo/main.py
@@ -11,7 +11,7 @@ import sys
 import traceback
 from collections import OrderedDict
 from copy import deepcopy
-from typing import List
+from typing import List, Optional
 
 import numpy as np
 
@@ -45,7 +45,7 @@ from mo.utils.version import get_version, get_simplified_mo_version, get_simplif
 from mo.utils.versions_checker import check_requirements  # pylint: disable=no-name-in-module
 
 # pylint: disable=no-name-in-module,import-error
-from ngraph.frontend import Frontend, FrontEndManager
+from ngraph.frontend import FrontEndManager, FrontEnd
 
 
 def replace_ext(name: str, old: str, new: str):
@@ -97,29 +97,33 @@ def print_argv(argv: argparse.Namespace, is_caffe: bool, is_tf: bool, is_mxnet: 
     print('\n'.join(lines), flush=True)
 
 
-def load_moc_frontends(argv: argparse.Namespace) -> (Frontend, List[str]):
+def load_moc_frontends(argv: argparse.Namespace) -> (Optional[FrontEnd], List[str]):
     fem = argv.feManager
-    available_moc_front_ends = []
     moc_front_end = None
 
-    # TODO: in future, check of 'use_legacy_frontend' in argv can be added here (issue 61973)
-    # use_new_frontend
-    force_use_legacy_frontend = False
+    # Read user flags:
+    use_legacy_frontend = argv.use_legacy_frontend
+    use_new_frontend = argv.use_new_frontend
 
-    if fem and not force_use_legacy_frontend:
-        available_moc_front_ends = fem.get_available_front_ends()
-        if argv.input_model:
-            if not argv.framework:
-                moc_front_end = fem.load_by_model(argv.input_model)
-                print('argv', argv)
+    if not fem or use_legacy_frontend:
+        return None, []
 
-                # skip onnx frontend as not fully supported yet (63050)
-                if moc_front_end and moc_front_end.get_name() == "onnx":
-                    moc_front_end = None
-                if moc_front_end:
-                    argv.framework = moc_front_end.get_name()
-            elif argv.framework in available_moc_front_ends:
-                moc_front_end = fem.load_by_framework(argv.framework)
+    available_moc_front_ends = fem.get_available_front_ends()
+
+    if argv.input_model and not argv.framework:
+        moc_front_end = fem.load_by_model(argv.input_model)
+        argv.framework = moc_front_end.get_name()
+    elif argv.framework in available_moc_front_ends:
+        moc_front_end = fem.load_by_framework(argv.framework)
+
+    # Set which frontend to use by default, values should be 'new' or 'legacy'
+    frontend_defaults = {
+        'onnx': 'legacy',
+        'paddle': 'new'
+    }
+    # Disable MOC frontend if default is set to legacy and no user override
+    if frontend_defaults[moc_front_end.get_name()] == 'legacy' and not use_new_frontend:
+        moc_front_end = None
 
     return moc_front_end, available_moc_front_ends
 
@@ -199,11 +203,11 @@ def prepare_ir(argv: argparse.Namespace):
     if argv.legacy_ir_generation and len(argv.transform) != 0:
         raise Error("--legacy_ir_generation and --transform keys can not be used at the same time.")
 
-    use_legacy_fe = argv.framework not in available_moc_front_ends
     # For C++ frontends there is no specific python installation requirements, thus check only generic ones
-    ret_code = check_requirements(framework=argv.framework if use_legacy_fe else None)
-    if ret_code:
-        raise Error('check_requirements exit with return code {}'.format(ret_code))
+    if not moc_front_end:
+        ret_code = check_requirements(framework=argv.framework)
+        if ret_code:
+            raise Error('check_requirements exit with return code {}'.format(ret_code))
 
     if is_tf and argv.tensorflow_use_custom_operations_config is not None:
         argv.transformations_config = argv.tensorflow_use_custom_operations_config
@@ -292,10 +296,11 @@ def prepare_ir(argv: argparse.Namespace):
     graph = None
     ngraph_function = None
 
-    if argv.framework not in available_moc_front_ends:
-        graph = unified_pipeline(argv)
-    else:
+    if moc_front_end:
         ngraph_function = moc_pipeline(argv, moc_front_end)
+    else:
+        graph = unified_pipeline(argv)
+
     return graph, ngraph_function
 
 
