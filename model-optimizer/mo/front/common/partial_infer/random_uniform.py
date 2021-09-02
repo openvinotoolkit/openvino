5d2c4ecdca4adfb33e48e1a1893c925cc0b5fc66
Anastasia Popova <anastasia.popova@intel.com>
2021-09-02 13:14:59 +0300

    Small fix.

--- model-optimizer/mo/front/common/partial_infer/random_uniform.py
+++
@@ -1,6 +0,0 @@
-# Copyright (C) 2018-2021 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-
-
-def tf_random_uniform_infer(node):
-    node.out_port(0).data.set_shape(node.in_port(0).data.get_value())
