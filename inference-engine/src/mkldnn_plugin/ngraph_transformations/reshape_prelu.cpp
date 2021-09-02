c121f7f5da7e489ffd18f36d10ba7b61ef784487
Vladislav Golubev <vladislav.golubev@intel.com>
2021-09-02 13:06:41 +0300

    [CPUTransformations] ReshapePrelu: dynamic shapes support

--- inference-engine/src/mkldnn_plugin/ngraph_transformations/reshape_prelu.cpp
+++ inference-engine/src/mkldnn_plugin/ngraph_transformations/reshape_prelu.cpp
@@ -12,27 +12,34 @@
 NGRAPH_RTTI_DEFINITION(MKLDNNPlugin::ReshapePRelu, "ReshapePRelu", 0);
 
 MKLDNNPlugin::ReshapePRelu::ReshapePRelu() {
-    auto prelu = ngraph::pattern::wrap_type<ngraph::opset1::PRelu>({ngraph::pattern::any_input(ngraph::pattern::has_static_shape()),
-                                                                    ngraph::pattern::any_input(ngraph::pattern::has_static_shape())});
+    auto input = ngraph::pattern::any_input(ngraph::pattern::has_static_rank());
+    auto slope_constant = ngraph::pattern::wrap_type<ngraph::opset1::Constant>();
+    auto prelu = ngraph::pattern::wrap_type<ngraph::opset1::PRelu>({ input, slope_constant });
 
     ngraph::matcher_pass_callback callback = [this](ngraph::pattern::Matcher& m) {
         auto prelu = std::dynamic_pointer_cast<ngraph::opset1::PRelu>(m.get_match_root());
-        if (!prelu || ngraph::shape_size(prelu->get_input_shape(1)) == 1 || prelu->get_input_shape(1).size() != 1) {
-            return false;
-        }
-        const auto prelu_shape = prelu->input_value(0).get_shape();
-        const auto slope_shape = prelu->input_value(1).get_shape();
-        ngraph::Shape new_shape(prelu_shape.size(), 1);
-        const auto slope_dim = slope_shape[0];
-        const auto channel_dim_idx = prelu_shape.size() > 1 ? 1 : 0;
-        if (slope_dim != prelu_shape[channel_dim_idx]) {
-            return false;
-        }
-        new_shape[channel_dim_idx] = slope_dim;
 
-        auto slope = ngraph::op::util::reshapeTo(prelu->input_value(1), new_shape);
-        auto new_prelu = std::make_shared<ngraph::opset1::PRelu>(prelu->input(0).get_source_output(), slope);
+        const auto slope_shape = prelu->input_value(1).get_shape();
+        const auto prelu_pshape = prelu->input_value(0).get_partial_shape();
+        const auto prelu_rank = prelu_pshape.rank();
+        if (!prelu || prelu_rank.is_dynamic() || prelu_rank.get_length() == 1 || ngraph::shape_size(slope_shape) == 1 || slope_shape.size() != 1) {
+            return false;
+        }
+
+        const auto slope_dim = slope_shape[0];
+        const auto channel_dim_idx = 1;
+        if (!prelu_pshape[channel_dim_idx].is_dynamic() && slope_dim != prelu_pshape[channel_dim_idx].get_length()) {
+            return false;
+        }
+
+        ngraph::Shape new_slope_shape(prelu_rank.get_length(), 1);
+        new_slope_shape[channel_dim_idx] = slope_dim;
+
+        auto reshape_constant = ngraph::opset1::Constant::create(ngraph::element::i64, ngraph::Shape{ new_slope_shape.size() }, new_slope_shape);
+        auto new_slope = ngraph::op::util::make_try_fold<ngraph::opset1::Reshape>(prelu->input_value(1), reshape_constant, true);
+        auto new_prelu = prelu->clone_with_new_inputs({ prelu->input_value(0), new_slope });
         new_prelu->set_friendly_name(prelu->get_friendly_name());
+
         ngraph::copy_runtime_info(prelu, new_prelu);
         ngraph::replace_node(prelu, new_prelu);
         return true;
