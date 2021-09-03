e67e22a5fed4aef525fea39a4359f373f85c5e28
mitruska <katarzyna.mitrus@intel.com>
2021-09-03 19:43:10 +0200

    Add more tests

--- ngraph/test/type_prop/slice.cpp
+++ ngraph/test/type_prop/slice.cpp
@@ -55,6 +55,23 @@ std::shared_ptr<Node> make_slice_op_param_inputs(const std::vector<PartialShape>
 }
 }  // namespace
 
+TEST(type_prop, slice_v8_basic_const_inputs_default_axes) {
+    PartialShape data_shape{10, 10, 10, 10, 10, 10, 10, 10};
+    PartialShape expected_out_shape{7, 4, 10, 10, 9, 9, 5, 10};
+
+    std::vector<int32_t> start_val{1, 1, -20, 9, 9, 9, 9, 20};
+    std::vector<int32_t> stop_val{8, 8, 20, -11, 0, -10, -11, -20};
+    std::vector<int32_t> step_val{1, 2, 1, -1, -1, -1, -2, -1};
+
+    element::Type_t et = element::i32;
+
+    std::vector<std::vector<int32_t>> input_vals{start_val, stop_val, step_val};
+    const auto op = make_slice_op_const_inputs(input_vals, data_shape, et);
+
+    EXPECT_EQ(op->get_element_type(), et);
+    EXPECT_EQ(op->get_output_partial_shape(0), expected_out_shape);
+}
+
 TEST(type_prop, slice_v8_basic_const_inputs) {
     PartialShape data_shape{10, 10, 10, 10, 10, 10, 10, 10};
     PartialShape expected_out_shape{7, 4, 10, 10, 9, 9, 5, 10};
@@ -75,18 +92,57 @@ TEST(type_prop, slice_v8_basic_const_inputs) {
     EXPECT_EQ(op->get_output_partial_shape(0), expected_out_shape);
 }
 
-TEST(type_prop, slice_v8_basic_const_inputs_default_axes) {
+TEST(type_prop, slice_v8_basic_const_inputs_unordered_axes) {
     PartialShape data_shape{10, 10, 10, 10, 10, 10, 10, 10};
-    PartialShape expected_out_shape{7, 4, 10, 10, 9, 9, 5, 10};
+    PartialShape expected_out_shape{4, 9, 7, 10, 10, 9, 5, 10};
 
     std::vector<int32_t> start_val{1, 1, -20, 9, 9, 9, 9, 20};
     std::vector<int32_t> stop_val{8, 8, 20, -11, 0, -10, -11, -20};
     std::vector<int32_t> step_val{1, 2, 1, -1, -1, -1, -2, -1};
 
+    std::vector<int32_t> axes_val{2, 0, 3, 7, 1, 5, 6, 4};
+
     element::Type_t et = element::i32;
 
-    std::vector<std::vector<int32_t>> input_vals{start_val, stop_val, step_val};
+    std::vector<std::vector<int32_t>> input_vals{start_val, stop_val, step_val, axes_val};
     const auto op = make_slice_op_const_inputs(input_vals, data_shape, et);
+
+    EXPECT_EQ(op->get_element_type(), et);
+    EXPECT_EQ(op->get_output_partial_shape(0), expected_out_shape);
+}
+
+TEST(type_prop, slice_v8_basic_param_inputs_default_axes) {
+    PartialShape data_shape{Dimension(0, 10),
+                            Dimension(1, 10),
+                            10,
+                            Dimension(3, 5),
+                            Dimension(-1, -1),
+                            Dimension(100, -1),
+                            Dimension(0, 8),
+                            Dimension(4, 8),
+                            16};
+    PartialShape expected_out_shape{Dimension(0, 10),
+                                    Dimension(0, 10),
+                                    Dimension(0, 10),
+                                    Dimension(0, 5),
+                                    Dimension(0, -1),
+                                    Dimension(0, -1),
+                                    Dimension(0, 8),
+                                    Dimension(4, 8),
+                                    16};
+
+    PartialShape start_shape{7};
+    PartialShape stop_shape{7};
+    PartialShape step_shape{7};
+
+    element::Type_t et = element::i32;
+
+    const auto data = make_shared<op::Parameter>(et, data_shape);
+    const auto start = make_shared<op::Parameter>(et, start_shape);
+    const auto stop = make_shared<op::Parameter>(et, stop_shape);
+    const auto step = make_shared<op::Parameter>(et, step_shape);
+
+    const auto op = make_shared<op::v8::Slice>(data, start, stop, step);
 
     EXPECT_EQ(op->get_element_type(), et);
     EXPECT_EQ(op->get_output_partial_shape(0), expected_out_shape);
