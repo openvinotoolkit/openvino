f82b8f5feb9a7f0005916a63d80f64d8f0fdf18b
Sergey Lyubimtsev <sergey.lyubimtsev@intel.com>
2021-09-08 12:12:30 +0300

    fix linter issue (mypy)

--- inference-engine/ie_bridges/python/src/openvino/__init__.py
+++ inference-engine/ie_bridges/python/src/openvino/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2018-2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
+from typing import Iterable
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)  # type: Iterable[str]
