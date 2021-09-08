f8ccde46c224dc425406444c928e5bc9f267f605
Vafin, Maxim <maxim.vafin@intel.com>
2021-09-09 00:07:25 +0300

    Implement partial conversion

--- ngraph/test/frontend/tensorflow/convert_model.cpp
+++ ngraph/test/frontend/tensorflow/convert_model.cpp
@@ -2,22 +2,22 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "basic_api.hpp"
+#include "convert_model.hpp"
 
 #include "tf_utils.hpp"
 
 using namespace ngraph;
 using namespace ngraph::frontend;
 
-using TFBasicTest = FrontEndBasicTest;
+using TFConvertModelTest = FrontEndConvertModelTest;
 
 static const std::vector<std::string> models{
     std::string("2in_2out/2in_2out.pb"),
 };
 
-INSTANTIATE_TEST_SUITE_P(TFBasicTest,
-                         FrontEndBasicTest,
+INSTANTIATE_TEST_SUITE_P(TFConvertModelTest,
+                         FrontEndConvertModelTest,
                          ::testing::Combine(::testing::Values(TF_FE),
                                             ::testing::Values(std::string(TEST_TENSORFLOW_MODELS_DIRNAME)),
                                             ::testing::ValuesIn(models)),
-                         FrontEndBasicTest::getTestCaseName);
+                         FrontEndConvertModelTest::getTestCaseName);
