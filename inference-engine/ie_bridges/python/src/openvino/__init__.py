205ca201036366146a1ebfc71082acdd9513b2cc
Sergey Lyubimtsev <sergey.lyubimtsev@intel.com>
2021-09-08 12:20:53 +0300

    ignore # mypy issue #1422

--- inference-engine/ie_bridges/python/src/openvino/__init__.py
+++ inference-engine/ie_bridges/python/src/openvino/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2018-2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-from typing import Iterable
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)  # type: Iterable[str]
+__path__ = __import__('pkgutil').extend_path(__path__, __name__) # type: ignore  # mypy issue #1422
