4cb096cd779d53bf1881d24ad71d708e049967c7
Anastasia Popova <anastasia.popova@intel.com>
2021-09-10 09:37:44 +0300

    Added const shape checks.

--- inference-engine/src/transformations/src/transformations/common_optimizations/random_uniform_fusion.cpp
+++ inference-engine/src/transformations/src/transformations/common_optimizations/random_uniform_fusion.cpp
@@ -28,26 +28,37 @@ ngraph::pass::RandomUniformMaxValFusion::RandomUniformMaxValFusion() {
 
     const auto convert_pattern = ngraph::pattern::wrap_type<opset8::Convert>({random_uniform_pattern});
     const auto random_uniform_or_convert_pattern =
-            std::make_shared<pattern::op::Or>(OutputVector{random_uniform_pattern, convert_pattern});
+        std::make_shared<pattern::op::Or>(OutputVector{random_uniform_pattern, convert_pattern});
 
-    const auto mul_pattern = ngraph::pattern::wrap_type<opset8::Multiply>({random_uniform_or_convert_pattern, mul_const_pattern});
+    const auto mul_pattern =
+        ngraph::pattern::wrap_type<opset8::Multiply>({random_uniform_or_convert_pattern, mul_const_pattern});
 
     ngraph::matcher_pass_callback callback = [=](pattern::Matcher& m) {
         const auto& pattern_map = m.get_pattern_value_map();
         const auto data = pattern_map.at(data_pattern);
         const auto random_uniform = pattern_map.at(random_uniform_pattern);
-        const auto mul_const = pattern_map.at(mul_const_pattern);
+        const auto mul_constant = pattern_map.at(mul_const_pattern);
         const auto ru = std::dynamic_pointer_cast<opset8::RandomUniform>(random_uniform.get_node_shared_ptr());
         if (!ru)
             return false;
 
+        const auto mul_const = std::dynamic_pointer_cast<opset8::Constant>(mul_constant.get_node_shared_ptr());
+        if (!mul_const)
+            return false;
+
+        auto const_shape = mul_const->get_shape();
+        size_t const_shape_size = shape_size(const_shape);
+
+        if (const_shape_size != 1 || const_shape.size() != 1)
+            return false;
+
         if (pattern_map.count(convert_pattern)) {
-            const auto mul = pattern_map.at(mul_pattern);
+            const auto& mul = pattern_map.at(mul_pattern);
             const auto ml = std::dynamic_pointer_cast<opset8::Multiply>(mul.get_node_shared_ptr());
             if (!ml)
                 return false;
 
-            const auto convert = pattern_map.at(convert_pattern);
+            const auto& convert = pattern_map.at(convert_pattern);
             const auto cvt = std::dynamic_pointer_cast<opset8::Convert>(convert.get_node_shared_ptr());
             if (!cvt)
                 return false;
@@ -60,7 +71,7 @@ ngraph::pass::RandomUniformMaxValFusion::RandomUniformMaxValFusion() {
             ngraph::replace_node(m.get_match_root(), new_ru_conv);
 
         } else {
-            const auto mul = pattern_map.at(mul_pattern);
+            const auto& mul = pattern_map.at(mul_pattern);
             const auto ml = std::dynamic_pointer_cast<opset8::Multiply>(mul.get_node_shared_ptr());
             if (!ml)
                 return false;
@@ -90,23 +101,35 @@ ngraph::pass::RandomUniformMinValFusion::RandomUniformMinValFusion() {
 
     const auto convert_pattern = ngraph::pattern::wrap_type<opset8::Convert>({random_uniform_pattern});
     const auto convert_or_random_uniform_pattern =
-            std::make_shared<pattern::op::Or>(OutputVector{convert_pattern, random_uniform_pattern});
-    const auto add_pattern = ngraph::pattern::wrap_type<opset8::Add>({convert_or_random_uniform_pattern, add_const_pattern});
+        std::make_shared<pattern::op::Or>(OutputVector{convert_pattern, random_uniform_pattern});
+    const auto add_pattern =
+        ngraph::pattern::wrap_type<opset8::Add>({convert_or_random_uniform_pattern, add_const_pattern});
 
     ngraph::matcher_pass_callback callback = [=](pattern::Matcher& m) {
         const auto& pattern_map = m.get_pattern_value_map();
         const auto data = pattern_map.at(data_pattern);
         const auto random_uniform = pattern_map.at(random_uniform_pattern);
-        const auto add_const = pattern_map.at(add_const_pattern);
+        const auto add_constant = pattern_map.at(add_const_pattern);
         const auto ru_max_input = pattern_map.at(ru_max_input_pattern);
         const auto ru_min_input = pattern_map.at(ru_min_input_pattern);
         const auto add = pattern_map.at(add_pattern);
         const auto ru = std::dynamic_pointer_cast<opset8::RandomUniform>(random_uniform.get_node_shared_ptr());
+
+        const auto add_const = std::dynamic_pointer_cast<opset8::Constant>(add_constant.get_node_shared_ptr());
+        if (!add_const)
+            return false;
+
+        auto const_shape = add_const->get_shape();
+        size_t const_shape_size = shape_size(const_shape);
+
+        if (const_shape_size != 1 || const_shape.size() != 1)
+            return false;
+
         if (!ru)
             return false;
 
         if (pattern_map.count(convert_pattern)) {
-            const auto convert = pattern_map.at(convert_pattern);
+            const auto& convert = pattern_map.at(convert_pattern);
             const auto cvt = std::dynamic_pointer_cast<opset8::Convert>(convert.get_node_shared_ptr());
             if (!cvt)
                 return false;
