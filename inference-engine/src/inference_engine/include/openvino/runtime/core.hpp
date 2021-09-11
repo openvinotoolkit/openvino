6e65b8f2e85ff5b035ce68f066a23e3a5862f4a1
apankratovantonp <anton.pankratov@intel.com>
2021-09-12 01:15:21 +0300

    added openvino runtime plugin

--- inference-engine/src/inference_engine/include/openvino/runtime/core.hpp
+++ inference-engine/src/inference_engine/include/openvino/runtime/core.hpp
@@ -16,7 +16,7 @@
 #include <vector>
 
 #include "common.hpp"
-#include "cpp/ie_executable_network.hpp"
+#include "executable_network.hpp"
 #include "ie_plugin_config.hpp"
 #include "ie_version.hpp"
 #include "remote_context.hpp"
@@ -117,9 +117,9 @@ public:
      * operation
      * @return An executable network reference
      */
-    ie::ExecutableNetwork compile_model(const std::shared_ptr<const ov::Function>& network,
-                                        const std::string& deviceName,
-                                        const ConfigMap& config = {});
+    ExecutableNetwork compile_model(const std::shared_ptr<const ov::Function>& network,
+                                    const std::string& deviceName,
+                                    const ConfigMap& config = {});
 
     /**
      * @brief Reads model and creates an executable network from IR or ONNX file
@@ -134,9 +134,9 @@ public:
      *
      * @return An executable network reference
      */
-    ie::ExecutableNetwork compile_model(const std::string& modelPath,
-                                        const std::string& deviceName,
-                                        const ConfigMap& config = {});
+    ExecutableNetwork compile_model(const std::string& modelPath,
+                                    const std::string& deviceName,
+                                    const ConfigMap& config = {});
 
     /**
      * @brief Creates an executable network from a network object within a specified remote context.
@@ -146,9 +146,9 @@ public:
      * operation
      * @return An executable network object
      */
-    ie::ExecutableNetwork compile_model(const std::shared_ptr<const ov::Function>& network,
-                                        const RemoteContext& context,
-                                        const ConfigMap& config = {});
+    ExecutableNetwork compile_model(const std::shared_ptr<const ov::Function>& network,
+                                    const RemoteContext& context,
+                                    const ConfigMap& config = {});
 
     /**
      * @brief Registers extension
@@ -164,9 +164,9 @@ public:
      * operation*
      * @return An executable network reference
      */
-    ie::ExecutableNetwork import_model(std::istream& networkModel,
-                                       const std::string& deviceName,
-                                       const ConfigMap& config = {});
+    ExecutableNetwork import_model(std::istream& networkModel,
+                                   const std::string& deviceName,
+                                   const ConfigMap& config = {});
 
     /**
      * @brief Creates an executable network from a previously exported network within a specified
@@ -178,9 +178,9 @@ public:
      * operation
      * @return An executable network reference
      */
-    ie::ExecutableNetwork import_model(std::istream& networkModel,
-                                       const RemoteContext& context,
-                                       const ConfigMap& config = {});
+    ExecutableNetwork import_model(std::istream& networkModel,
+                                   const RemoteContext& context,
+                                   const ConfigMap& config = {});
 
     /**
      * @brief Query device if it supports specified network with specified configuration
