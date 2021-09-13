183fedc4f89f06c2ccc66c613d98673f90b1c226
apankratovantonp <anton.pankratov@intel.com>
2021-09-14 02:00:02 +0300

    Removed ie plugin cpp wrapper

--- inference-engine/src/inference_engine/src/cpp/ie_plugin.hpp
+++ inference-engine/src/inference_engine/src/cpp/ie_plugin.hpp
@@ -20,109 +20,11 @@
 #include "so_ptr.hpp"
 #include "openvino/runtime/common.hpp"
 
-#if defined __GNUC__
-# pragma GCC diagnostic push
-# pragma GCC diagnostic ignored "-Wreturn-type"
-#endif
-
 #define PLUGIN_CALL_STATEMENT(...)                                                                \
-    if (!_ptr) IE_THROW() << "Wrapper used in the PLUGIN_CALL_STATEMENT was not initialized.";    \
+    if (!_impl) IE_THROW() << "Wrapper used in the PLUGIN_CALL_STATEMENT was not initialized.";   \
     try {                                                                                         \
         __VA_ARGS__;                                                                              \
     } catch(...) {::InferenceEngine::details::Rethrow();}
-
-namespace InferenceEngine {
-/**
- * @brief This class is a C++ API wrapper for IInferencePlugin.
- *
- * It can throw exceptions safely for the application, where it is properly handled.
- */
-class InferencePlugin : protected details::SOPointer<IInferencePlugin> {
-    using details::SOPointer<IInferencePlugin>::SOPointer;
-    friend class ICore;
-
-public:
-    void SetName(const std::string & deviceName) {
-        PLUGIN_CALL_STATEMENT(_ptr->SetName(deviceName));
-    }
-
-    void SetCore(std::weak_ptr<InferenceEngine::ICore> core) {
-        PLUGIN_CALL_STATEMENT(_ptr->SetCore(core));
-    }
-
-    const Version GetVersion() const {
-        PLUGIN_CALL_STATEMENT(return _ptr->GetVersion());
-    }
-
-    void AddExtension(const InferenceEngine::IExtensionPtr& extension) {
-        PLUGIN_CALL_STATEMENT(_ptr->AddExtension(extension));
-    }
-
-    void SetConfig(const std::map<std::string, std::string>& config) {
-        PLUGIN_CALL_STATEMENT(_ptr->SetConfig(config));
-    }
-
-    details::SOPointer<IExecutableNetworkInternal> LoadNetwork(const CNNNetwork& network, const std::map<std::string, std::string>& config) {
-        PLUGIN_CALL_STATEMENT(return {_so, _ptr->LoadNetwork(network, config)});
-    }
-
-    details::SOPointer<IExecutableNetworkInternal> LoadNetwork(const CNNNetwork& network,
-                                                               const std::shared_ptr<IRemoteContext>& context,
-                                                               const std::map<std::string, std::string>& config) {
-        PLUGIN_CALL_STATEMENT(return {_so, _ptr->LoadNetwork(network, config, context)});
-    }
-
-    details::SOPointer<IExecutableNetworkInternal> LoadNetwork(const std::string& modelPath, const std::map<std::string, std::string>& config) {
-        PLUGIN_CALL_STATEMENT(return {_so, _ptr->LoadNetwork(modelPath, config)});
-    }
-
-    QueryNetworkResult QueryNetwork(const CNNNetwork& network,
-                                    const std::map<std::string, std::string>& config) const {
-        QueryNetworkResult res;
-        PLUGIN_CALL_STATEMENT(res = _ptr->QueryNetwork(network, config));
-        if (res.rc != OK) IE_THROW() << res.resp.msg;
-        return res;
-    }
-
-    details::SOPointer<IExecutableNetworkInternal> ImportNetwork(const std::string& modelFileName,
-                                                                 const std::map<std::string, std::string>& config) {
-        PLUGIN_CALL_STATEMENT(return {_so, _ptr->ImportNetwork(modelFileName, config)});
-    }
-
-    details::SOPointer<IExecutableNetworkInternal> ImportNetwork(std::istream& networkModel,
-                                    const std::map<std::string, std::string>& config) {
-        PLUGIN_CALL_STATEMENT(return {_so, _ptr->ImportNetwork(networkModel, config)});
-    }
-
-    details::SOPointer<IExecutableNetworkInternal> ImportNetwork(std::istream& networkModel,
-                                                                 const std::shared_ptr<IRemoteContext>& context,
-                                                                 const std::map<std::string, std::string>& config) {
-        PLUGIN_CALL_STATEMENT(return {_so, _ptr->ImportNetwork(networkModel, context, config)});
-    }
-
-    Parameter GetMetric(const std::string& name, const std::map<std::string, Parameter>& options) const {
-        PLUGIN_CALL_STATEMENT(return _ptr->GetMetric(name, options));
-    }
-
-    details::SOPointer<IRemoteContext> CreateContext(const ParamMap& params) {
-        PLUGIN_CALL_STATEMENT(return {_so, _ptr->CreateContext(params)});
-    }
-
-    details::SOPointer<IRemoteContext> GetDefaultContext(const ParamMap& params) {
-        PLUGIN_CALL_STATEMENT(return {_so, _ptr->GetDefaultContext(params)});
-    }
-
-    Parameter GetConfig(const std::string& name, const std::map<std::string, Parameter>& options) const {
-        PLUGIN_CALL_STATEMENT(return _ptr->GetConfig(name, options));
-    }
-};
-}  // namespace InferenceEngine
-
-
-#if defined __GNUC__
-# pragma GCC diagnostic pop
-#endif
-
 namespace ov {
 namespace runtime {
 
@@ -133,86 +35,86 @@ namespace runtime {
  */
 struct InferencePlugin {
     std::shared_ptr<void> _so;
-    std::shared_ptr<ie::IInferencePlugin> _ptr;
+    std::shared_ptr<ie::IInferencePlugin> _impl;
 
     InferencePlugin(const std::shared_ptr<void>& so, const std::shared_ptr<ie::IInferencePlugin>& impl) :
         _so{so},
-        _ptr{impl} {
-        IE_ASSERT(_ptr != nullptr);
+        _impl{impl} {
+        if (_impl == nullptr) IE_THROW() << "InferencePlugin was not initialized";
     }
 
     void set_name(const std::string& deviceName) {
-        PLUGIN_CALL_STATEMENT(_ptr->SetName(deviceName));
+        PLUGIN_CALL_STATEMENT(_impl->SetName(deviceName));
     }
 
     void set_core(std::weak_ptr<ie::ICore> core) {
-        PLUGIN_CALL_STATEMENT(_ptr->SetCore(core));
+        PLUGIN_CALL_STATEMENT(_impl->SetCore(core));
     }
 
     const ie::Version get_version() const {
-        PLUGIN_CALL_STATEMENT(return _ptr->GetVersion());
+        PLUGIN_CALL_STATEMENT(return _impl->GetVersion());
     }
 
     void add_extension(const ie::IExtensionPtr& extension) {
-        PLUGIN_CALL_STATEMENT(_ptr->AddExtension(extension));
+        PLUGIN_CALL_STATEMENT(_impl->AddExtension(extension));
     }
 
     void set_config(const ConfigMap& config) {
-        PLUGIN_CALL_STATEMENT(_ptr->SetConfig(config));
+        PLUGIN_CALL_STATEMENT(_impl->SetConfig(config));
     }
 
     SoPtr<ie::IExecutableNetworkInternal> load_model(const ie::CNNNetwork& network, const ConfigMap& config) {
-        PLUGIN_CALL_STATEMENT(return {_so, _ptr->LoadNetwork(network, config)});
+        PLUGIN_CALL_STATEMENT(return {_so, _impl->LoadNetwork(network, config)});
     }
 
     SoPtr<ie::IExecutableNetworkInternal> load_model(const ie::CNNNetwork& network,
                                                                const std::shared_ptr<ie::IRemoteContext>& context,
                                                                const ConfigMap& config) {
-        PLUGIN_CALL_STATEMENT(return {_so, _ptr->LoadNetwork(network, config, context)});
+        PLUGIN_CALL_STATEMENT(return {_so, _impl->LoadNetwork(network, config, context)});
     }
 
     SoPtr<ie::IExecutableNetworkInternal> load_model(const std::string& modelPath, const ConfigMap& config) {
-        PLUGIN_CALL_STATEMENT(return {_so, _ptr->LoadNetwork(modelPath, config)});
+        PLUGIN_CALL_STATEMENT(return {_so, _impl->LoadNetwork(modelPath, config)});
     }
 
     ie::QueryNetworkResult query_model(const ie::CNNNetwork& network,
                                        const ConfigMap& config) const {
         ie::QueryNetworkResult res;
-        PLUGIN_CALL_STATEMENT(res = _ptr->QueryNetwork(network, config));
+        PLUGIN_CALL_STATEMENT(res = _impl->QueryNetwork(network, config));
         if (res.rc != ie::OK) IE_THROW() << res.resp.msg;
         return res;
     }
 
     SoPtr<ie::IExecutableNetworkInternal> import_model(const std::string& modelFileName,
                                                                  const ConfigMap& config) {
-        PLUGIN_CALL_STATEMENT(return {_so, _ptr->ImportNetwork(modelFileName, config)});
+        PLUGIN_CALL_STATEMENT(return {_so, _impl->ImportNetwork(modelFileName, config)});
     }
 
     SoPtr<ie::IExecutableNetworkInternal> import_model(std::istream& networkModel,
                                     const ConfigMap& config) {
-        PLUGIN_CALL_STATEMENT(return {_so, _ptr->ImportNetwork(networkModel, config)});
+        PLUGIN_CALL_STATEMENT(return {_so, _impl->ImportNetwork(networkModel, config)});
     }
 
     SoPtr<ie::IExecutableNetworkInternal> import_model(std::istream& networkModel,
                                                                  const std::shared_ptr<ie::IRemoteContext>& context,
                                                                  const ConfigMap& config) {
-        PLUGIN_CALL_STATEMENT(return {_so, _ptr->ImportNetwork(networkModel, context, config)});
+        PLUGIN_CALL_STATEMENT(return {_so, _impl->ImportNetwork(networkModel, context, config)});
     }
 
     ie::Parameter get_metric(const std::string& name, const ie::ParamMap& options) const {
-        PLUGIN_CALL_STATEMENT(return _ptr->GetMetric(name, options));
+        PLUGIN_CALL_STATEMENT(return _impl->GetMetric(name, options));
     }
 
     SoPtr<ie::IRemoteContext> create_context(const ie::ParamMap& params) {
-        PLUGIN_CALL_STATEMENT(return {_so, _ptr->CreateContext(params)});
+        PLUGIN_CALL_STATEMENT(return {_so, _impl->CreateContext(params)});
     }
 
     SoPtr<ie::IRemoteContext> get_default_context(const ie::ParamMap& params) {
-        PLUGIN_CALL_STATEMENT(return {_so, _ptr->GetDefaultContext(params)});
+        PLUGIN_CALL_STATEMENT(return {_so, _impl->GetDefaultContext(params)});
     }
 
     ie::Parameter get_config(const std::string& name, const ie::ParamMap& options) const {
-        PLUGIN_CALL_STATEMENT(return _ptr->GetConfig(name, options));
+        PLUGIN_CALL_STATEMENT(return _impl->GetConfig(name, options));
     }
 };
 
