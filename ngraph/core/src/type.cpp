6d150679bb25e86919dd19494c6ffb699069e0c8
Ilya Churaev <ilya.churaev@intel.com>
2021-09-15 14:17:56 +0300

    Introduced new RTTI macros for nGraph

--- ngraph/core/src/type.cpp
+++ ngraph/core/src/type.cpp
@@ -16,3 +16,49 @@ size_t std::hash<ngraph::DiscreteTypeInfo>::operator()(const ngraph::DiscreteTyp
     NGRAPH_SUPPRESS_DEPRECATED_END
 }
 }  // namespace std
+
+namespace ov {
+std::ostream& operator<<(std::ostream& s, const DiscreteTypeInfo& info) {
+    s << "DiscreteTypeInfo{name: " << info.name << ", version_id: " << info.version_id
+      << ", old_version: " << info.version << ", parent: ";
+    if (!info.parent)
+        s << info.parent;
+    else
+        s << *info.parent;
+
+    s << "}";
+    return s;
+}
+
+// parent is commented to fix type relaxed operations
+bool DiscreteTypeInfo::operator<(const DiscreteTypeInfo& b) const {
+    if (version_id == nullptr || b.version_id == nullptr)
+        return version < b.version ||
+               (version == b.version && strcmp(name, b.name) < 0);  // ||
+                                                                    // (version == b.version && strcmp(name, b.name) ==
+                                                                    // 0 && parent && b.parent && *parent < *b.parent);
+    else
+        return strcmp(version_id, b.version_id) < 0 ||
+               (strcmp(version_id, b.version_id) == 0 && strcmp(name, b.name) < 0);  // ||
+    // (strcmp(version_id, b.version_id) == 0 && strcmp(name, b.name) == 0 && parent && b.parent &&
+    //  *parent < *b.parent);
+}
+bool DiscreteTypeInfo::operator==(const DiscreteTypeInfo& b) const {
+    if (version_id == nullptr || b.version_id == nullptr)
+        return version == b.version && strcmp(name, b.name) == 0;  // && parent == b.parent;
+    else
+        return strcmp(version_id, b.version_id) == 0 && strcmp(name, b.name) == 0;  // && parent == b.parent;
+}
+bool DiscreteTypeInfo::operator<=(const DiscreteTypeInfo& b) const {
+    return *this == b || *this < b;
+}
+bool DiscreteTypeInfo::operator>(const DiscreteTypeInfo& b) const {
+    return !(*this <= b);
+}
+bool DiscreteTypeInfo::operator>=(const DiscreteTypeInfo& b) const {
+    return !(*this < b);
+}
+bool DiscreteTypeInfo::operator!=(const DiscreteTypeInfo& b) const {
+    return !(*this == b);
+}
+}  // namespace ov
