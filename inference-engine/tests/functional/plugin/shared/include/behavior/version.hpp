0547e3dd920a2f105c9c4fd4934f26b1c989dd91
Efode, Irina <irina.efode@intel.com>
2021-09-16 10:31:44 +0300

    vers

--- inference-engine/tests/functional/plugin/shared/include/behavior/version.hpp
+++ inference-engine/tests/functional/plugin/shared/include/behavior/version.hpp
@@ -18,16 +18,14 @@ using VersionTest = BehaviorTestsUtils::BehaviorTestsBasic;
 
 // Load unsupported network type to the Plugin
 TEST_P(VersionTest, pluginCurrentVersionIsCorrect) {
-    // Skip test according to plugin specific disabledTestPatterns() (if any)
-    SKIP_IF_CURRENT_TEST_IS_DISABLED()
     // Create CNNNetwork from ngrpah::Function
     InferenceEngine::CNNNetwork cnnNet(function);
-    std::string refError = "The plugin does not support";
     if (targetDevice.find(CommonTestUtils::DEVICE_AUTO) == std::string::npos &&
         targetDevice.find(CommonTestUtils::DEVICE_MULTI) == std::string::npos &&
         targetDevice.find(CommonTestUtils::DEVICE_HETERO) == std::string::npos) {
         std::map<std::string, InferenceEngine::Version> versions = ie->GetVersions(targetDevice);
         ASSERT_EQ(versions.size(), 1);
+        ASSERT_EQ(versions.begin()->first, targetDevice);
         auto version = versions.begin()->second;
         IE_SUPPRESS_DEPRECATED_START
         ASSERT_EQ(version.apiVersion.major, 2);
