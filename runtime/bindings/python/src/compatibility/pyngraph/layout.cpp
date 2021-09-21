43f6f1b3a9d0d1313353660c5bd2887b8e3305ec
pszmel <piotr.szmelczynski@intel.com>
2021-09-21 15:46:46 +0200

    Create binding for Layout class

---
+++ runtime/bindings/python/src/compatibility/pyngraph/layout.cpp
@@ -0,0 +1,37 @@
+#include "openvino/core/layout.hpp"
+
+#include "pyngraph/layout.hpp"
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/stl_bind.h>
+
+#include "ngraph/type.hpp"
+
+namespace py = pybind11;
+
+// using Names = std::unordered_map<std::string, std::int64_t>;
+// using IndexMap = std::unordered_map<std::int64_t, std::string>;
+
+// PYBIND11_MAKE_OPAQUE(Names, IndexMap);
+
+void regclass_pyngraph_Layout(py::module m) {
+    py::class_<ov::Layout, std::shared_ptr<ov::Layout>> layout(
+        m,
+        "Layout");
+    layout.doc() = "ngraph.impl.Layout wraps ov::Layout";
+
+    layout.def(py::init<>());
+    layout.def(py::init<const char*>());
+    layout.def(py::init<const std::string&>());
+
+    // operator overloading
+    layout.def(py::self == py::self);
+    layout.def(py::self != py::self);
+
+    layout.def("scalar", &ov::Layout::scalar);
+    layout.def("has_name", &ov::Layout::has_name);
+    layout.def("get_index_by_name", &ov::Layout::get_index_by_name);
+    layout.def("to_string", &ov::Layout::to_string);
+
+}
