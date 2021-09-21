fe351a5ca8801a6e78b34480a14432b535035694
yekruglov <yegor.kruglov@intel.com>
2021-09-21 11:20:35 +0300

    downgrade transformation update

--- inference-engine/src/transformations/src/transformations/op_conversions/convert_maxpool_downgrade.cpp
+++ inference-engine/src/transformations/src/transformations/op_conversions/convert_maxpool_downgrade.cpp
@@ -37,8 +37,9 @@ pass::ConvertMaxPool8ToMaxPool1::ConvertMaxPool8ToMaxPool1() {
                                                             maxpool_v8_node->get_auto_pad());
 
         maxpool_v1_node->set_friendly_name(maxpool_v8_node->get_friendly_name());
-        ngraph::copy_runtime_info(maxpool_v8_node, maxpool_v1_node);
         maxpool_v8_node->output(0).replace(maxpool_v1_node->output(0));
+        ngraph::copy_runtime_info(maxpool_v8_node, maxpool_v1_node);
+        maxpool_v8_node->clear_control_dependencies();
         return true;
     };
 
