db385569c295061ff82524289881a2ee61022c82
Ilya Lavrenov <ilya.lavrenov@intel.com>
2021-09-21 14:26:01 +0300

    Throw ov::Exception from ov::runtime (#7536)
    * Exceptions
    
    * Throw ov::Exception from ov::runtime
    
    * code style
    
    * Fixed function name

--- inference-engine/tests/functional/inference_engine/ov_executable_network_test.cpp
+++ inference-engine/tests/functional/inference_engine/ov_executable_network_test.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <gtest/gtest.h>
+#include <openvino/core/except.hpp>
 #include <openvino/runtime/executable_network.hpp>
 
 using namespace ::testing;
@@ -10,40 +11,40 @@ using namespace std;
 
 TEST(ExecutableNetworkOVTests, throwsOnUninitializedExportStream) {
     ov::runtime::ExecutableNetwork exec;
-    ASSERT_THROW(exec.export_model(std::cout), InferenceEngine::NotAllocated);
+    ASSERT_THROW(exec.export_model(std::cout), ov::Exception);
 }
 
 TEST(ExecutableNetworkOVTests, throwsOnUninitializedGetFunction) {
     ov::runtime::ExecutableNetwork exec;
-    ASSERT_THROW(exec.get_runtime_function(), InferenceEngine::NotAllocated);
+    ASSERT_THROW(exec.get_runtime_function(), ov::Exception);
 }
 
 TEST(ExecutableNetworkOVTests, throwsOnUninitializedGetParameters) {
     ov::runtime::ExecutableNetwork exec;
-    ASSERT_THROW(exec.get_parameters(), InferenceEngine::NotAllocated);
+    ASSERT_THROW(exec.get_parameters(), ov::Exception);
 }
 
 TEST(ExecutableNetworkOVTests, throwsOnUninitializedGetResults) {
     ov::runtime::ExecutableNetwork exec;
-    ASSERT_THROW(exec.get_results(), InferenceEngine::NotAllocated);
+    ASSERT_THROW(exec.get_results(), ov::Exception);
 }
 
 TEST(ExecutableNetworkOVTests, throwsOnUninitializedSetConfig) {
     ov::runtime::ExecutableNetwork exec;
-    ASSERT_THROW(exec.set_config({{}}), InferenceEngine::NotAllocated);
+    ASSERT_THROW(exec.set_config({{}}), ov::Exception);
 }
 
 TEST(ExecutableNetworkOVTests, throwsOnUninitializedGetConfig) {
     ov::runtime::ExecutableNetwork exec;
-    ASSERT_THROW(exec.get_config({}), InferenceEngine::NotAllocated);
+    ASSERT_THROW(exec.get_config({}), ov::Exception);
 }
 
 TEST(ExecutableNetworkOVTests, throwsOnUninitializedGetMetric) {
     ov::runtime::ExecutableNetwork exec;
-    ASSERT_THROW(exec.get_metric({}), InferenceEngine::NotAllocated);
+    ASSERT_THROW(exec.get_metric({}), ov::Exception);
 }
 
 TEST(ExecutableNetworkOVTests, throwsOnUninitializedGetContext) {
     ov::runtime::ExecutableNetwork exec;
-    ASSERT_THROW(exec.get_context(), InferenceEngine::NotAllocated);
+    ASSERT_THROW(exec.get_context(), ov::Exception);
 }
