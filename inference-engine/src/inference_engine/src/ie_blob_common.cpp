d4467e80d154117bcae5bcc39093746144ebaaa7
Anton Pankratv <anton.pankratov@intel.com>
2021-09-22 10:54:37 +0300

    Added range roi blob API (#7567)

--- inference-engine/src/inference_engine/src/ie_blob_common.cpp
+++ inference-engine/src/inference_engine/src/ie_blob_common.cpp
@@ -9,13 +9,26 @@
 #include "ie_blob.h"
 
 namespace InferenceEngine {
-
-Blob::Ptr Blob::createROI(const ROI&) const {
+Blob::Ptr Blob::createROI(const ROI& roi) const {
+    if (getTensorDesc().getLayout() == Layout::NCHW || getTensorDesc().getLayout() == Layout::NHWC) {
+        return createROI({roi.id, 0, roi.posY, roi.posX},
+                         {roi.id + 1, getTensorDesc().getDims()[1], roi.posY + roi.sizeY, roi.posX + roi.sizeX});
+    }
     IE_THROW(NotImplemented) << "createROI is not implemented for current type of Blob";
+}
+
+Blob::Ptr Blob::createROI(const std::vector<std::size_t>& begin, const std::vector<std::size_t>& end) const {
+    IE_THROW(NotImplemented) << "createROI is not implemented for current type of Blob or roi";
 }
 
 Blob::Ptr make_shared_blob(const Blob::Ptr& inputBlob, const ROI& roi) {
     return inputBlob->createROI(roi);
+}
+
+Blob::Ptr make_shared_blob(const Blob::Ptr& inputBlob,
+                           const std::vector<std::size_t>& begin,
+                           const std::vector<std::size_t>& end) {
+    return inputBlob->createROI(begin, end);
 }
 
 }  // namespace InferenceEngine
