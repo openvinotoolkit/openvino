bc2e881e15b40399b76604ad04b80f2cced9de29
akuporos <anastasia.kuporosova@intel.com>
2021-09-22 17:52:14 +0300

    fix setup.py building

--- runtime/bindings/python/src/compatibility/openvino/inference_engine/ie_api.py
+++ runtime/bindings/python/src/compatibility/openvino/inference_engine/ie_api.py
@@ -16,50 +16,51 @@ from openvino.pyopenvino import TBlobInt32
 from openvino.pyopenvino import TBlobInt64
 from openvino.pyopenvino import TBlobUint8
 from openvino.pyopenvino import TensorDesc
-from openvino.pyopenvino import TensorDesc
+
+from openvino.pyopenvino import InferRequest
 
 import numpy as np
 
 
-precision_map = {'FP32': np.float32,
-                 'FP64': np.float64,
-                 'FP16': np.int16,
-                 'BF16': np.int16,
-                 'I16': np.int16,
-                 'I8': np.int8,
-                 'BIN': np.int8,
-                 'I32': np.int32,
-                 'I64': np.int64,
-                 'U8': np.uint8,
-                 'BOOL': np.uint8,
-                 'U16': np.uint16,
-                 'U32': np.uint32,
-                 'U64': np.uint64}
+precision_map = {"FP32": np.float32,
+                 "FP64": np.float64,
+                 "FP16": np.int16,
+                 "BF16": np.int16,
+                 "I16": np.int16,
+                 "I8": np.int8,
+                 "BIN": np.int8,
+                 "I32": np.int32,
+                 "I64": np.int64,
+                 "U8": np.uint8,
+                 "BOOL": np.uint8,
+                 "U16": np.uint16,
+                 "U32": np.uint32,
+                 "U64": np.uint64}
 
 
-def normalize_inputs(py_dict: dict):
-    '''Normalize a dictionary of inputs to contiguous numpy arrays.'''
+def normalize_inputs(py_dict: dict) -> dict:
+    """Normalize a dictionary of inputs to contiguous numpy arrays."""
     return {k: (np.ascontiguousarray(v) if isinstance(v, np.ndarray) else v)
             for k, v in py_dict.items()}
 
-
-def infer(request, inputs: dict = {}):
-    results = request._infer(inputs=normalize_inputs(inputs))
+# flake8: noqa: D102
+def infer(request: InferRequest, inputs: dict = None) -> dict:
+    results = request._infer(inputs=normalize_inputs(inputs if inputs is not None else {}))
     return {name: (blob.buffer.copy()) for name, blob in results.items()}
 
-
-def get_result(request, name: str):
+# flake8: noqa: D102
+def get_result(request: InferRequest, name: str) -> np.ndarray:
     return request.get_blob(name).buffer.copy()
 
-
-def async_infer(request, inputs: dict = {}, userdata=None):
-    request._async_infer(inputs=normalize_inputs(inputs),
+# flake8: noqa: D102
+def async_infer(request: InferRequest, inputs: dict = None, userdata=None) -> None:  # type: ignore
+    request._async_infer(inputs=normalize_inputs(inputs if inputs is not None else {}),
                          userdata=userdata)
 
-
+# flake8: noqa: C901
 # Dispatch Blob types on Python side.
 class BlobWrapper:
-    def __new__(cls, tensor_desc: TensorDesc, arr: np.ndarray = None):
+    def __new__(cls, tensor_desc: TensorDesc, arr: np.ndarray = None):  # type: ignore
         arr_size = 0
         precision = ""
         if arr is not None and tensor_desc is not None:
@@ -67,6 +68,7 @@ class BlobWrapper:
             arr_size = np.prod(arr.shape)
             tensor_desc_size = np.prod(tensor_desc.dims)
             precision = tensor_desc.precision
+<<<<<<< HEAD:runtime/bindings/python/src/compatibility/openvino/inference_engine/ie_api.py
             if np.isfortran(arr):
                 arr = arr.ravel(order="F")
             else:
@@ -82,6 +84,26 @@ class BlobWrapper:
                 arr = np.ascontiguousarray(arr)
         elif arr is None and tensor_desc is not None:
             arr = np.empty(0, dtype=precision_map[precision])
+=======
+            if arr is not None:
+                arr = np.array(arr)  # Keeping array as numpy array
+                arr_size = int(np.prod(arr.shape))
+                if np.isfortran(arr):
+                    arr = arr.ravel(order="F")
+                else:
+                    arr = arr.ravel(order="C")
+                if arr_size != tensor_desc_size:
+                    raise AttributeError(f"Number of elements in provided numpy array "
+                                         f"{arr_size} and required by TensorDesc "
+                                         f"{tensor_desc_size} are not equal")
+                if arr.dtype != precision_map[precision]:
+                    raise ValueError(f"Data type {arr.dtype} of provided numpy array "
+                                     f"doesn't match to TensorDesc precision {precision}")
+                if not arr.flags["C_CONTIGUOUS"]:
+                    arr = np.ascontiguousarray(arr)
+            elif arr is None:
+                arr = np.empty(0, dtype=precision_map[precision])
+>>>>>>> 721515efd... fix setup.py building:runtime/bindings/python/src/openvino/ie_api.py
         else:
             raise AttributeError("TensorDesc can't be None")
 
@@ -108,9 +130,10 @@ class BlobWrapper:
         elif precision in ["U8", "BOOL"]:
             return TBlobUint8(tensor_desc, arr, arr_size)
         else:
-            raise AttributeError(f'Unsupported precision {precision} for Blob')
+            raise AttributeError(f"Unsupported precision {precision} for Blob")
 
-def blob_from_file(path_to_bin_file : str) -> BlobWrapper:
-    array = np.fromfile(path_to_bin_file,dtype=np.uint8)
+# flake8: noqa: D102
+def blob_from_file(path_to_bin_file: str) -> BlobWrapper:
+    array = np.fromfile(path_to_bin_file, dtype=np.uint8)
     tensor_desc = TensorDesc("U8", array.shape, "C")
-    return BlobWrapper(tensor_desc,array)
+    return BlobWrapper(tensor_desc, array)
