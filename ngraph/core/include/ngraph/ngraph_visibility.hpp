d0434c66b08466b65c09d8e69086a452e49829c3
y <ilya.lavrenov@intel.com>
2021-09-22 22:42:40 +0300

    Fixed code style

--- ngraph/core/include/ngraph/ngraph_visibility.hpp
+++ ngraph/core/include/ngraph/ngraph_visibility.hpp
@@ -5,6 +5,6 @@
 #include "ngraph/visibility.hpp"
 #include "openvino/core/core_visibility.hpp"
 
-#define NGRAPH_API   OPENVINO_API
-#define NGRAPH_API_C OPENVINO_API_C
+#define NGRAPH_API      OPENVINO_API
+#define NGRAPH_API_C    OPENVINO_API_C
 #define NGRAPH_EXTERN_C OPENVINO_EXTERN_C
