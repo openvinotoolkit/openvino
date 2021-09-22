5b10b4247e6ac543aec3c5c4c164e3842451db4c
mangguo <mang.guo@intel.com>
2021-09-22 04:12:57 -0400

    Add NopElimination pass to remove unnecessary convert layers

--- inference-engine/src/mkldnn_plugin/mkldnn_plugin.cpp
+++ inference-engine/src/mkldnn_plugin/mkldnn_plugin.cpp
@@ -29,6 +29,7 @@
 #include <transformations/common_optimizations/weights_dequantize_to_fake_quantize.hpp>
 #include "transformations/common_optimizations/convert_quantize_dequantize.hpp"
 #include <transformations/common_optimizations/softmax_fusion.hpp>
+#include <transformations/common_optimizations/nop_elimination.hpp>
 #include <transformations/op_conversions/convert_depth_to_space.hpp>
 #include <transformations/op_conversions/convert_shuffle_channels3.hpp>
 #include <transformations/op_conversions/convert_space_to_depth.hpp>
@@ -182,6 +183,7 @@ static void TransformationUpToCPUSpecificOpSet(std::shared_ptr<ngraph::Function>
             std::vector<ngraph::element::Type>{ ngraph::element::i8, ngraph::element::u8, ngraph::element::i4, ngraph::element::u4 });
     }
     manager.register_pass<ngraph::pass::ConvertPrecision>(precisions);
+    manager.register_pass<ngraph::pass::NopElimination>();
 
     auto pass_config = manager.get_pass_config();
 
