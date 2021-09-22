5c946eaabfbbea3226ff22cc9b624ffd811e20a7
y <ilya.lavrenov@intel.com>
2021-09-22 18:42:13 +0300

    Use jobs pool for PDPD model conversion as well

--- ngraph/test/frontend/paddlepaddle/test_models/gen_wrapper.py
+++ ngraph/test/frontend/paddlepaddle/test_models/gen_wrapper.py
@@ -3,7 +3,7 @@ import subprocess
 
 import sys
 
-print(sys.argv)
+# print(sys.argv)
 if len(sys.argv) < 4:
     print("Script, output folder and mark file must be specified as arguments")
     exit(1)
@@ -12,7 +12,7 @@ gen_script = sys.argv[1]
 out_folder = sys.argv[2]
 mark_file = sys.argv[3]
 
-print("Processing: {} ".format(gen_script))
+# print("Processing: {} ".format(gen_script))
 subprocess.run([sys.executable, gen_script, out_folder], env=os.environ)
 
 # Create mark file indicating that script was executed
