140ef1b0bb544eada9b50d89dcf4723e1775e75b
Ilya Lavrenov <ilya.lavrenov@intel.com>
2021-09-23 17:03:45 +0300

    Added version information to OpenVINO Core (#7600)
    * Added version information to OpenVINO Core
    
    * Fixed code style
    
    * Fixed Windows
    
    * Fixed code style
    
    * Fixed clang

--- ngraph/core/include/ngraph/ngraph_visibility.hpp
+++ ngraph/core/include/ngraph/ngraph_visibility.hpp
@@ -5,8 +5,10 @@
 #include "ngraph/visibility.hpp"
 #include "openvino/core/core_visibility.hpp"
 
-#define NGRAPH_API OPENVINO_API
+#define NGRAPH_API      OPENVINO_API
+#define NGRAPH_API_C    OPENVINO_API_C
+#define NGRAPH_EXTERN_C OPENVINO_EXTERN_C
 
-#ifdef ENABLE_UNICODE_PATH_SUPPORT
-#    define OPENVINO_ENABLE_UNICODE_PATH_SUPPORT
+#ifdef OPENVINO_ENABLE_UNICODE_PATH_SUPPORT
+#    define ENABLE_UNICODE_PATH_SUPPORT
 #endif
