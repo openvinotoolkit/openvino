f9b58f2539078c11f567ce547023de1c51a4377e
liubo-intel <bo4.liu@intel.com>
2021-09-23 18:47:48 +0800

    remove fp64 type check of exp

--- ngraph/frontend/paddlepaddle/src/op/exp.cpp
+++ ngraph/frontend/paddlepaddle/src/op/exp.cpp
@@ -11,7 +11,6 @@ namespace pdpd {
 namespace op {
 NamedOutputs exp(const NodeContext& node) {
     auto data = node.get_ng_input("X");
-    PDPD_OP_VALIDATION_CHECK(node, data.get_element_type() != ov::element::f64, "exp not support fp64 input type !");
     return node.default_single_output_mapping({std::make_shared<opset8::Exp>(data)}, {"Out"});
 }
 }  // namespace op
