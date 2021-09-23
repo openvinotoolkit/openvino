a2dcadfb82d4506c0793dec374682be8aebb079b
pszmel <piotr.szmelczynski@intel.com>
2021-09-23 12:53:42 +0200

    style fix

--- ngraph/core/include/openvino/core/preprocess/pre_post_process.hpp
+++ ngraph/core/include/openvino/core/preprocess/pre_post_process.hpp
@@ -5,9 +5,9 @@
 #pragma once
 
 #include "openvino/core/core_visibility.hpp"
-#include "openvino/core/preprocess/input_info.hpp"
-#include "openvino/core/node.hpp"
 #include "openvino/core/function.hpp"
+#include "openvino/core/node.hpp"
+#include "openvino/core/preprocess/input_info.hpp"
 
 namespace ov {
 
