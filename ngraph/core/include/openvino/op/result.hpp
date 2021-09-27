309e0dd6b81d1d54999f172bbd987d1dea4f8ec0
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2021-09-27 17:37:44 +0200

    Remove obsolete attribute from Result op
    m_needs_default_layout field had no effect to the Result operator state,
    even if set to true* by build_graph test.
    Its setter and getter were unused.
    
    * rather accidentaly considering implicit cast from const char*

--- ngraph/core/include/openvino/op/result.hpp
+++ ngraph/core/include/openvino/op/result.hpp
@@ -19,25 +19,16 @@ public:
     /// \brief Allows a value to be used as a function result.
     ///
     /// \param arg Node that produces the input tensor.
-    Result(const Output<Node>& arg, bool needs_default_layout = false);
+    Result(const Output<Node>& arg);
 
     bool visit_attributes(AttributeVisitor& visitor) override;
     void validate_and_infer_types() override;
 
     std::shared_ptr<Node> clone_with_new_inputs(const OutputVector& new_args) const override;
 
-    void set_needs_default_layout(bool val) {
-        m_needs_default_layout = val;
-    }
-    bool needs_default_layout() const {
-        return m_needs_default_layout;
-    }
     bool evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const override;
     bool has_evaluate() const override;
     bool constant_fold(OutputVector& output_values, const OutputVector& inputs_values) override;
-
-private:
-    bool m_needs_default_layout{false};
 };
 }  // namespace v0
 }  // namespace op
