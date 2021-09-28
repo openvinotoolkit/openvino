61be5d4af003562d497d5957196a8a16b9f51a14
Anna Likholat <anna.likholat@intel.com>
2021-09-28 17:51:30 +0300

    fix

--- ngraph/test/backend/max_unpool.in.cpp
+++ ngraph/test/backend/max_unpool.in.cpp
@@ -122,35 +122,3 @@ NGRAPH_TEST(${BACKEND_NAME}, max_unpool_2d_reshape_output) {
     test_case.add_expected_output<float>(pooling_shape, result);
     test_case.run();
 }
-
-NGRAPH_TEST(${BACKEND_NAME}, max_unpool_2d_kernel_3) {
-    Shape shape{1, 1, 7, 9};
-    const Strides& strides{2, 2};
-    const Shape& pads_begin{0, 0};
-    const Shape& pads_end{0, 0};
-    const Shape& kernel{3, 3};
-    const op::RoundingType rounding_type = op::RoundingType::FLOOR;
-    const op::PadType pad_type = op::PadType::NOTSET;
-
-    auto A = make_shared<op::Parameter>(element::f32, shape);
-    auto maxPool = make_shared<op::v1::MaxPool>(A, strides, pads_begin, pads_end, kernel, rounding_type, pad_type);
-    auto relu = std::make_shared<op::Relu>(maxPool);
-    auto maxUnpool = make_shared<op::v8::MaxUnpool>(A, maxPool, relu, A);
-    auto f = make_shared<Function>(maxUnpool, ParameterVector{A});
-
-    std::vector<float> a;
-    for (int i = 1; i < 64; ++i)
-        a.push_back(i);
-    std::vector<float> result{0, 0, 0, 0, 0, 0, 0, 0, 0,
-                              0, 0, 0, 0, 0, 0, 0, 0, 0,
-                              0, 0, 21, 0, 23, 0, 25, 0, 27,
-                              0, 0, 0, 0, 0, 0, 0, 0, 0,
-                              0, 0, 39, 0, 41, 0, 43, 0, 45,
-                              0, 0, 0, 0, 0, 0, 0, 0, 0,
-                              0, 0, 57, 0, 59, 0, 61, 0, 63};
-
-    auto test_case = test::TestCase<TestEngine>(f);
-    test_case.add_input<float>({a});
-    test_case.add_expected_output<float>(shape, result);
-    test_case.run();
-}
