584afd89051374b6dc3b9deb1554f9ae2531d628
Wang, Hongyu1 <hongyu1.wang@intel.com>
2021-09-28 10:41:12 +0800

    move sample_value calculation above switch

--- ngraph/core/reference/include/ngraph/runtime/reference/roi_align.hpp
+++ ngraph/core/reference/include/ngraph/runtime/reference/roi_align.hpp
@@ -158,24 +158,18 @@ void roi_align(const T* feature_maps,
                              pooling_points[sample_index + 3].first,
                              pooling_points[sample_index + 3].second})];
 
-                        switch (pooling_mode) {
-                        case ROIPoolingMode::MAX: {
-                            T sample_value = pooling_weights[sample_index] * sample_part_1 +
-                                             pooling_weights[sample_index + 1] * sample_part_2 +
-                                             pooling_weights[sample_index + 2] * sample_part_3 +
-                                             pooling_weights[sample_index + 3] * sample_part_4;
+                        T sample_value = pooling_weights[sample_index] * sample_part_1 +
+                                         pooling_weights[sample_index + 1] * sample_part_2 +
+                                         pooling_weights[sample_index + 2] * sample_part_3 +
+                                         pooling_weights[sample_index + 3] * sample_part_4;
 
+                        switch (pooling_mode) {
+                        case ROIPoolingMode::MAX:
                             pooled_value = sample_value > pooled_value ? sample_value : pooled_value;
                             break;
-                        }
                         case ROIPoolingMode::AVG:
-                        default: {
-                            T sample_value = pooling_weights[sample_index] * sample_part_1 +
-                                             pooling_weights[sample_index + 1] * sample_part_2 +
-                                             pooling_weights[sample_index + 2] * sample_part_3 +
-                                             pooling_weights[sample_index + 3] * sample_part_4;
+                        default:
                             pooled_value += sample_value / (num_samples_in_bin);
-                        }
                         }
                         sample_index += 4;
                     }
