7aab50943dc066e63b7c77a5b7b4227270195f73
Ilya Lavrenov <ilya.lavrenov@intel.com>
2021-09-29 20:51:23 +0300

    Enabled several CPU tests (#7724)

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
@@ -67,10 +67,6 @@ std::vector<std::string> disabledTestPatterns() {
         R"(.*BF16NetworkRestore1.*)",
         R"(.*MobileNet_ssd_with_branching.*)",
 
-        // TODO: 55656 AUTO plugin and QueryNetwork
-        R"(.*CoreThreading.*smoke_QueryNetwork.*targetDevice=AUTO_config.*)",
-        // Unsupported config KEY_ENFORCE_BF16 for AUTO plugin
-        R"(.*Behavior_Auto.*InferRequestSetBlobByType.*)",
         // TODO: 57562 No dynamic output shape support
         R"(.*NonZeroLayerTest.*)",
         // need to implement Export / Import
