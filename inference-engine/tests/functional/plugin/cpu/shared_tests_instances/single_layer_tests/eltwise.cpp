b96acdca6f2a24e77fbbe225d454514bda04a32b
Efode, Irina <irina.efode@intel.com>
2021-09-30 01:59:34 +0300

    eltwise

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/single_layer_tests/eltwise.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/single_layer_tests/eltwise.cpp
@@ -9,20 +9,25 @@
 using namespace LayerTestsDefinitions;
 
 namespace {
-std::vector<std::vector<std::vector<size_t>>> inShapes = {
-        {{2}},
-        {{2, 200}},
-        {{10, 200}},
-        {{1, 10, 100}},
-        {{4, 4, 16}},
-        {{1, 1, 1, 3}},
-        {{2, 17, 5, 4}, {1, 17, 1, 1}},
-        {{2, 17, 5, 1}, {1, 17, 1, 4}},
-        {{1, 2, 4}},
-        {{1, 4, 4}},
-        {{1, 4, 4, 1}},
-        {{1, 1, 1, 1, 1, 1, 3}},
-        {{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}}
+std::vector<std::pair<std::vector<ngraph::PartialShape>, std::vector<std::vector<ngraph::Shape>>>> inShapesStatic = {
+        {{}, {{{2}}}},
+        {{}, {{{2, 200}}}},
+        {{}, {{{10, 200}}}},
+        {{}, {{{1, 10, 100}}}},
+        {{}, {{{4, 4, 16}}}},
+        {{}, {{{1, 1, 1, 3}}}},
+        {{}, {{{2, 17, 5, 4}, {1, 17, 1, 1}}}},
+        {{}, {{{2, 17, 5, 1}, {1, 17, 1, 4}}}},
+        {{}, {{{1, 2, 4}}}},
+        {{}, {{{1, 4, 4}}}},
+        {{}, {{{1, 4, 4, 1}}}},
+        {{}, {{{1, 1, 1, 1, 1, 1, 3}}}},
+        {{}, {{{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}}}}
+};
+
+std::vector<std::pair<std::vector<ngraph::PartialShape>, std::vector<std::vector<ngraph::Shape>>>> inShapesDynamic = {
+        {{{ngraph::Dimension(1, 10), 200}, {ngraph::Dimension(1, 10), 200}},
+         {{{2, 200}, {2, 200}}}},
 };
 
 std::vector<InferenceEngine::Precision> netPrecisions = {
@@ -36,8 +41,16 @@ std::vector<ngraph::helpers::InputLayerType> secondaryInputTypes = {
         ngraph::helpers::InputLayerType::PARAMETER,
 };
 
+std::vector<ngraph::helpers::InputLayerType> secondaryInputTypesDynamic = {
+        ngraph::helpers::InputLayerType::PARAMETER,
+};
+
 std::vector<CommonTestUtils::OpType> opTypes = {
         CommonTestUtils::OpType::SCALAR,
+        CommonTestUtils::OpType::VECTOR,
+};
+
+std::vector<CommonTestUtils::OpType> opTypesDynamic = {
         CommonTestUtils::OpType::VECTOR,
 };
 
@@ -52,10 +65,16 @@ std::vector<ngraph::helpers::EltwiseTypes> eltwiseOpTypes = {
         ngraph::helpers::EltwiseTypes::MOD
 };
 
+std::vector<ngraph::helpers::EltwiseTypes> eltwiseOpTypesDynamic = {
+        ngraph::helpers::EltwiseTypes::ADD,
+        ngraph::helpers::EltwiseTypes::MULTIPLY,
+        ngraph::helpers::EltwiseTypes::SUBTRACT,
+};
+
 std::map<std::string, std::string> additional_config = {};
 
 const auto multiply_params = ::testing::Combine(
-        ::testing::ValuesIn(inShapes),
+        ::testing::ValuesIn(inShapesStatic),
         ::testing::ValuesIn(eltwiseOpTypes),
         ::testing::ValuesIn(secondaryInputTypes),
         ::testing::ValuesIn(opTypes),
@@ -66,13 +85,26 @@ const auto multiply_params = ::testing::Combine(
         ::testing::Values(CommonTestUtils::DEVICE_CPU),
         ::testing::Values(additional_config));
 
-INSTANTIATE_TEST_SUITE_P(smoke_CompareWithRefs, EltwiseLayerTest, multiply_params, EltwiseLayerTest::getTestCaseName);
+const auto multiply_params_dynamic = ::testing::Combine(
+        ::testing::ValuesIn(inShapesDynamic),
+        ::testing::ValuesIn(eltwiseOpTypesDynamic),
+        ::testing::ValuesIn(secondaryInputTypesDynamic),
+        ::testing::ValuesIn(opTypesDynamic),
+        ::testing::ValuesIn(netPrecisions),
+        ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
+        ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
+        ::testing::Values(InferenceEngine::Layout::ANY),
+        ::testing::Values(CommonTestUtils::DEVICE_CPU),
+        ::testing::Values(additional_config));
+
+INSTANTIATE_TEST_SUITE_P(smoke_CompareWithRefs_static, EltwiseLayerTest, multiply_params, EltwiseLayerTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_CompareWithRefs_dynamic, EltwiseLayerTest, multiply_params_dynamic, EltwiseLayerTest::getTestCaseName);
 
 
-std::vector<std::vector<std::vector<size_t>>> inShapesSingleThread = {
-        {{1, 2, 3, 4}},
-        {{2, 2, 2, 2}},
-        {{2, 1, 2, 1, 2, 2}}
+std::vector<std::pair<std::vector<ngraph::PartialShape>, std::vector<std::vector<ngraph::Shape>>>> inShapesSingleThread = {
+        {{}, {{{1, 2, 3, 4}}}},
+        {{}, {{{2, 2, 2, 2}}}},
+        {{}, {{{2, 1, 2, 1, 2, 2}}}}
 };
 
 std::vector<ngraph::helpers::EltwiseTypes> eltwiseOpTypesSingleThread = {
