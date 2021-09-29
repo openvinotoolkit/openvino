f0e74d24bb93259c998eec8512b6ba83a7cc120e
Mikhail Nosov <mikhail.nosov@intel.com>
2021-09-29 09:46:37 +0300

    Use 'u8' element type in tests (#7704)
    Use 'float32' output for 'compare with reference' tests to avoid IE precisions undesired conversions
    Added GPU preprocessing tests skeleton

--- docs/template_plugin/tests/functional/subgraph_reference/preprocess.cpp
+++ docs/template_plugin/tests/functional/subgraph_reference/preprocess.cpp
@@ -101,23 +101,44 @@ static RefPreprocessParams scale_then_mean() {
     return res;
 }
 
+static RefPreprocessParams convert_only() {
+    RefPreprocessParams res("convert_only");
+    res.function = []() {
+        auto f = create_simple_function(element::f32, Shape{1, 1, 2, 2});
+        f = PrePostProcessor().input(InputInfo()
+                .tensor(InputTensorInfo().set_element_type(element::i16))
+                .preprocess(PreProcessSteps()
+                .convert_element_type(element::f32)
+                .scale(3.f)
+                .convert_element_type(element::u8)
+                .convert_element_type(element::f32)))
+                        .build(f);
+        return f;
+    };
+    res.inputs.emplace_back(Shape{1, 1, 2, 2}, element::i16, std::vector<int16_t>{2, 3, 4, 5});
+    res.expected.emplace_back(Shape{1, 1, 2, 2}, element::f32, std::vector<float>{0., 1., 1., 1.});
+    return res;
+}
+
 static RefPreprocessParams convert_element_type_and_scale() {
     RefPreprocessParams res("convert_element_type_and_scale");
     res.function = []() {
-        auto f = create_simple_function(element::i8, Shape{1, 3, 2, 2});
+        auto f = create_simple_function(element::u8, Shape{1, 3, 2, 2});
         f = PrePostProcessor()
                 .input(InputInfo()
                                .tensor(InputTensorInfo().set_element_type(element::i16))
                                .preprocess(PreProcessSteps()
                                                    .convert_element_type(element::f32)
                                                    .scale(2.f)
-                                                   .convert_element_type(element::i8)))
+                                                   .convert_element_type(element::u8)))
                 .build(f);
         return f;
     };
 
-    res.inputs.emplace_back(Shape{1, 3, 2, 2}, element::i16, std::vector<int16_t>{2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 10000, 200});
-    res.expected.emplace_back(Shape{1, 3, 2, 2}, element::i8, std::vector<int8_t>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, (int8_t)5000, 100});
+    res.inputs.emplace_back(Shape{1, 3, 2, 2}, element::i16,
+                            std::vector<int16_t>{2, 3, 6, 8, 10, 12, 14, 16, 18, 20, 10000, 200});
+    res.expected.emplace_back(Shape{1, 3, 2, 2}, element::u8,
+                              std::vector<uint8_t>{1, 1, 3, 4, 5, 6, 7, 8, 9, 10, (uint8_t)5000, 100});
     return res;
 }
 
@@ -254,6 +275,7 @@ std::vector<RefPreprocessParams> allPreprocessTests() {
     return std::vector<RefPreprocessParams> {
         simple_mean_scale(),
         scale_then_mean(),
+        convert_only(),
         convert_element_type_and_scale(),
         tensor_element_type_and_scale(),
         custom_preprocessing(),
--- inference-engine/tests/functional/plugin/gpu/shared_tests_instances/skip_tests_config.cpp
+++ inference-engine/tests/functional/plugin/gpu/shared_tests_instances/skip_tests_config.cpp
@@ -71,6 +71,8 @@ std::vector<std::string> disabledTestPatterns() {
             R"(.*Behavior_Multi.*InferRequestSetBlobByType.*Batched.*)",
             R"(.*(Multi|Auto).*Behavior.*InferRequestIOBBlobTest.*canProcessDeallocatedOutputBlobAfterGetAndSetBlob.*)",
             // TODO: until issue is xxx-59670 is resolved
-            R"(.*Gather8LayerTest.*)"
+            R"(.*Gather8LayerTest.*)",
+            // TODO: Issue 66516
+            R"(.*smoke_PrePostProcess_GPU.*convert_element_type_and_mean.*)"
     };
 }
