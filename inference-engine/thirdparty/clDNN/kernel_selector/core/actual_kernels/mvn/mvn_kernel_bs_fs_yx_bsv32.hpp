946ebc3c9170db44a6c5796c5ffc8860fa4c0309
Jade Cho <jade.cho@intel.com>
2021-09-29 18:53:29 +0900

    [GPU] Merge mvn kernel updates
    + Support bs_fs_yx_bsv32_fsv32 format for i8 and u8 dtype.
    + Support bs_fs_yx_bfv32_fsv16 format for fp16 dtype.

--- inference-engine/thirdparty/clDNN/kernel_selector/core/actual_kernels/mvn/mvn_kernel_bs_fs_yx_bsv32.hpp
+++ inference-engine/thirdparty/clDNN/kernel_selector/core/actual_kernels/mvn/mvn_kernel_bs_fs_yx_bsv32.hpp
@@ -9,11 +9,11 @@
 #include <vector>
 
 namespace kernel_selector {
-class MVNKernel_b_fs_yx_fsv16_imad : public MVNKernelBase {
+class MVNKernel_bs_fs_yx_bsv32 : public MVNKernelBase {
 public:
     using Parent = MVNKernelBase;
-    MVNKernel_b_fs_yx_fsv16_imad() : MVNKernelBase("mvn_gpu_b_fs_yx_fsv16_imad") {}
-    virtual ~MVNKernel_b_fs_yx_fsv16_imad() {}
+    MVNKernel_bs_fs_yx_bsv32() : MVNKernelBase("mvn_gpu_b_fs_yx_fsv16_imad") {}
+    virtual ~MVNKernel_bs_fs_yx_bsv32() {}
 
     KernelsData GetKernelsData(const Params& params, const optional_params& options) const override;
     KernelsPriority GetKernelsPriority(const Params& params, const optional_params& options) const override;
@@ -29,7 +29,6 @@ protected:
     };
 
     bool Validate(const Params&, const optional_params&) const override;
-    DispatchData SetDefault(const mvn_params& params) const override;
     JitConstants GetJitConstants(const mvn_params& params, DispatchData dispatchData) const override;
     std::vector<FusedOpType> GetSupportedFusedOps() const override {
         return {
@@ -40,10 +39,11 @@ protected:
         };
     }
 
-    KernelsData GetMultiStageKernelsData(const mvn_params& params, const optional_params&) const;
-    MultiDispatchData SetDefaultForMulti(const mvn_params& params) const;
+    KernelsData GetMultiStageKernelsData(const mvn_params& params, const optional_params&, bool) const;
+    MultiDispatchData SetDefaultForMulti(const mvn_params& params, bool) const;
 
 private:
     Datatype GetAccumulatorType(const mvn_params& params) const;
+    std::vector<size_t> GetFinalKernelLws(const std::vector<size_t>& gws, uint64_t max_wg) const;
 };
 }  // namespace kernel_selector
