62ed238f5422c0d9652996544f74f2cab3d61cce
Gleb Kazantaev <gleb.kazantaev@intel.com>
2021-09-29 19:17:03 +0300

    RT Info Attributes Serialization/Deserialization (#7603)
    * Init rt info deserialization
    
    * Add RT Info attributes deserialization
    
    * Add RT Info attributes serialization
    
    * Add Serialization and Deserialization tests
    
    * Code style
    
    * Fix AttributeAdapter place
    
    * Extended rt_info section; added support for multi-field attrs; use version for attr serialization/deserialization
    
    * Align attributes and their usage
    
    * Fix LPT
    
    * Add missing #pragma one
    
    * Fix build
    
    * Change version delimiter to have valid xml
    
    * Fix PromitivesPriority
    
    * Fix tests
    
    * Fix test
    
    * Fix IR Reader; remove empty rt_info
    
    * More explicit attribute name and version representation

--- inference-engine/tests/functional/inference_engine/lp_transformations/convolution_backprop_data_transformation.cpp
+++ inference-engine/tests/functional/inference_engine/lp_transformations/convolution_backprop_data_transformation.cpp
@@ -284,7 +284,7 @@ const std::vector<ConvolutionBackpropDataTransformationTestValues> testValues = 
         {
             ngraph::element::u8,
             {{}, { { 128.f }, ngraph::element::f32, {}, false }, {}},
-            {{}, { { 2.f }, ngraph::element::f32, {1, 2, 1, 1}, true, 1ul, element::i8, false, { "DISABLED_CONSTANT_FOLDING" }  }, {}},
+            {{}, { { 2.f }, ngraph::element::f32, {1, 2, 1, 1}, true, 1ul, element::i8, false, { "disabled_constant_folding_0" }  }, {}},
             {{}, {}, {{ 0.0002f }, ngraph::element::f32, {}}},
             op::Constant::create(ngraph::element::i8, ngraph::Shape{}, std::vector<float>{ 2.f }),
             true
