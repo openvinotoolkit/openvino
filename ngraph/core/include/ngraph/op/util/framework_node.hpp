b1535336dc46541371a7a0f5a2b9a6f90479b3fc
Ilya Churaev <ilya.churaev@intel.com>
2021-09-29 07:32:21 +0300

    Moved FrameworkNode to nGraph

---
+++ ngraph/core/include/ngraph/op/util/framework_node.hpp
@@ -0,0 +1,17 @@
+// Copyright (C) 2018-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include "ngraph/op/op.hpp"
+#include "openvino/op/util/framework_node.hpp"
+
+namespace ngraph {
+namespace op {
+namespace util {
+using ov::op::util::FrameworkNode;
+using ov::op::util::FrameworkNodeAttrs;
+}  // namespace util
+}  // namespace op
+}  // namespace ngraph
