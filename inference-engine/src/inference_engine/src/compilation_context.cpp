bb9e9287fb273cbde2352b06758cb069f7c3f6dc
Gleb Kazantaev <gleb.kazantaev@intel.com>
2021-09-30 21:13:44 +0300

    Code style

--- inference-engine/src/inference_engine/src/compilation_context.cpp
+++ inference-engine/src/inference_engine/src/compilation_context.cpp
@@ -123,8 +123,7 @@ std::string NetworkCompilationContext::computeHash(const CNNNetwork& network,
             } else if (auto fNames =
                            std::dynamic_pointer_cast<ngraph::VariantWrapper<ngraph::FusedNames>>(rtMapData.second)) {
                 seed = hash_combine(seed, fNames->get().getNames());
-            } else if (auto prim = std::dynamic_pointer_cast<ov::PrimitivesPriority>(
-                           rtMapData.second)) {
+            } else if (auto prim = std::dynamic_pointer_cast<ov::PrimitivesPriority>(rtMapData.second)) {
                 seed = hash_combine(seed, prim->get());
             }
         }
