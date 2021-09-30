a20d58c787ee547da3d26c3d2f8af378edeafabe
Nam, David <david.nam@intel.com>
2021-09-30 13:45:32 +0900

    Apply ov namespace to reference test of arithmetic2 operations

--- docs/template_plugin/tests/functional/op_reference/sqrt.cpp
+++ docs/template_plugin/tests/functional/op_reference/sqrt.cpp
@@ -3,18 +3,11 @@
 //
 
 #include <gtest/gtest.h>
-
-#include <ie_core.hpp>
-#include <ie_ngraph_utils.hpp>
-#include <ngraph/ngraph.hpp>
-#include <shared_test_classes/base/layer_test_utils.hpp>
-#include <vector>
-
 #include "base_reference_test.hpp"
+#include "openvino/op/sqrt.hpp"
 
-using namespace ngraph;
+using namespace ov;
 using namespace reference_tests;
-using namespace InferenceEngine;
 
 namespace {
 
@@ -27,14 +20,14 @@ struct SqrtParams {
         : pshape(shape),
           inType(iType),
           outType(iType),
-          inputData(CreateBlob(iType, iValues)),
-          refData(CreateBlob(iType, oValues)) {}
+          inputData(CreateTensor(iType, iValues)),
+          refData(CreateTensor(iType, oValues)) {}
 
     PartialShape pshape;
     element::Type inType;
     element::Type outType;
-    Blob::Ptr inputData;
-    Blob::Ptr refData;
+    runtime::Tensor inputData;
+    runtime::Tensor refData;
 };
 
 class ReferenceSqrtLayerTest : public testing::TestWithParam<SqrtParams>, public CommonReferenceTest {
@@ -58,8 +51,8 @@ private:
     static std::shared_ptr<Function> CreateFunction(const PartialShape& input_shape,
                                                     const element::Type& input_type,
                                                     const element::Type& expected_output_type) {
-        const auto in = std::make_shared<op::Parameter>(input_type, input_shape);
-        const auto sqrt = std::make_shared<op::Sqrt>(in);
+        const auto in = std::make_shared<op::v0::Parameter>(input_type, input_shape);
+        const auto sqrt = std::make_shared<op::v0::Sqrt>(in);
         return std::make_shared<Function>(NodeVector{sqrt}, ParameterVector{in});
     }
 };
