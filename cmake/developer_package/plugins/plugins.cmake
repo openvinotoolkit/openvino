b04a6a8cd302e5180bd91935664473d90664977b
y <ilya.lavrenov@intel.com>
2021-10-02 18:41:12 +0300

    Updates for compiler flags

--- cmake/developer_package/plugins/plugins.cmake
+++ cmake/developer_package/plugins/plugins.cmake
@@ -71,7 +71,7 @@ function(ie_add_plugin)
             set_target_properties(${IE_PLUGIN_NAME} PROPERTIES COMPILE_PDB_NAME ${IE_PLUGIN_NAME})
         endif()
 
-        if(UNIX AND NOT CMAKE_CROSSCOMPILING)
+        if(CMAKE_COMPILER_IS_GNUCXX AND NOT CMAKE_CROSSCOMPILING)
             target_link_options(${IE_PLUGIN_NAME} PRIVATE -Wl,--unresolved-symbols=report-all)
         endif()
 
