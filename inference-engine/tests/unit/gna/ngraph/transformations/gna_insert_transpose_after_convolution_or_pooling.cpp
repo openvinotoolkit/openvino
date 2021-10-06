dda42ef18fc9a0d63460d120df796f9186e1c42c
Elizaveta Lobanova <elizaveta.lobanova@intel.com>
2021-10-06 11:12:10 +0300

    [GNA] Tests added

--- inference-engine/tests/unit/gna/ngraph/transformations/gna_insert_transpose_after_convolution_or_pooling.cpp
+++ inference-engine/tests/unit/gna/ngraph/transformations/gna_insert_transpose_after_convolution_or_pooling.cpp
@@ -246,4 +246,45 @@ TEST(TransformationTests, InsertTransposeAfterConvOrPoolTestInputRank3) {
     ASSERT_TRUE(result.valid);
 }
 
+std::shared_ptr<ngraph::Function> CreatePoolConvFunction(const ngraph::Shape& input_shape,
+                                                         const ngraph::Shape& pool_kernel_shape) {
+    auto input_params = std::make_shared<ngraph::opset7::Parameter>(ngraph::element::i64, input_shape);
+
+    auto max_pool_operation = std::make_shared<ngraph::opset7::MaxPool>(input_params, pool_kernel_shape,
+        ngraph::Shape{0, 0}, ngraph::Shape{0, 1}, pool_kernel_shape);
+
+    auto pool_out_shape = max_pool_operation->get_output_shape(0);
+    ngraph::Shape new_shape = {1, 1, 1, std::accumulate(std::begin(pool_out_shape), std::end(pool_out_shape),
+                                                        size_t{1}, std::multiplies<size_t>())};
+    auto new_shape_const = ngraph::opset7::Constant::create(ngraph::element::i64, ngraph::Shape{4}, new_shape);
+    auto reshape_operation = std::make_shared<ngraph::opset7::Reshape>(max_pool_operation, new_shape_const, true);
+
+    auto weights_next_convolution = ngraph::opset7::Constant::create(ngraph::element::i64, new_shape, {1});
+    auto next_convolution_operation = std::make_shared<ngraph::opset7::Convolution>(reshape_operation,
+        weights_next_convolution, ngraph::Strides{1, 1}, ngraph::CoordinateDiff{0, 0},
+        ngraph::CoordinateDiff{0, 1}, ngraph::Strides{1, 1});
+
+    auto result = std::make_shared<ngraph::opset7::Result>(next_convolution_operation);
+    return std::make_shared<ngraph::Function>(ngraph::ResultVector{result},
+                                              ngraph::ParameterVector{input_params});
+}
+
+TEST(TransformationTests, InsertTransposeAfterConvOrPoolTest1dOutput) {
+    std::shared_ptr<ngraph::Function> func(nullptr), reference_func(nullptr);
+
+    func = CreatePoolConvFunction(ngraph::Shape{1, 3, 1, 8}, ngraph::Strides{1, 8});
+
+    ngraph::pass::Manager m;
+    m.register_pass<ngraph::pass::InitNodeInfo>();
+    m.register_pass<GNAPluginNS::InsertTransposeAfterConvOrPool>();
+    m.run_passes(func);
+    ASSERT_NO_THROW(check_rt_info(func));
+
+    reference_func = CreatePoolConvFunction(ngraph::Shape{1, 3, 1, 8}, ngraph::Strides{1, 8});
+
+    const FunctionsComparator func_comparator = FunctionsComparator::with_default().enable(FunctionsComparator::ATTRIBUTES);
+    const FunctionsComparator::Result result = func_comparator(func, reference_func);
+    ASSERT_TRUE(result.valid);
+}
+
 } // namespace testing
