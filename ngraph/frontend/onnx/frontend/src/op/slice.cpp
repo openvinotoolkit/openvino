bba1fb5c46d0b4651f9c904355e8239f962e7a71
mitruska <katarzyna.mitrus@intel.com>
2021-10-07 11:16:42 +0200

    Support onnx Slice1-9, 10+

--- ngraph/frontend/onnx/frontend/src/op/slice.cpp
+++ ngraph/frontend/onnx/frontend/src/op/slice.cpp
@@ -2,20 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include <algorithm>
+#include "op/slice.hpp"
+
 #include <memory>
 #include <vector>
 
 #include "default_opset.hpp"
-#include "exceptions.hpp"
-#include "ngraph/node.hpp"
-#include "ngraph/op/constant.hpp"
-#include "ngraph/op/util/op_types.hpp"
-#include "ngraph/validation_util.hpp"
 #include "onnx_import/core/null_node.hpp"
-#include "op/gather.hpp"
-#include "utils/common.hpp"
-
 #include "openvino/opsets/opset8.hpp"
 
 namespace ngraph {
@@ -26,46 +19,27 @@ OutputVector slice(const Node& node) {
     using ngraph::op::is_null;
 
     OutputVector inputs{node.get_ng_inputs()};
-    const auto data = inputs.at(0);
-    const auto data_rank = data.get_partial_shape().rank();
-
-    auto starts = inputs.at(1);
-    auto ends = inputs.at(2);
-
-    // Slice is calculated over all axes as default
-    std::shared_ptr<default_opset::Constant> axes_const;
-    if (inputs.size() >= 4 && !is_null(inputs.at(3)))  // axes input provided
-    {
-        axes_const = ngraph::get_constant_from_source(inputs.at(3));
-        CHECK_VALID_NODE(node, axes_const != nullptr, "Axes input must be constant");
-    } else {
-        CHECK_VALID_NODE(node, data_rank.is_static(), "Data rank must be static when axes input is not provided");
-        const size_t data_rank_value = data_rank.get_length();
-        axes_const = default_opset::Constant::create(element::i64,
-                                                     {data_rank_value},
-                                                     common::get_monotonic_range<int64_t>(data_rank_value));
-    }
-    auto raw_axes_vec = axes_const->cast_vector<int64_t>();
-    std::vector<uint64_t> axes_vec = get_normalized_axes_vector(node, data_rank, raw_axes_vec);
-
-    const size_t slice_indices_length = *std::max_element(std::begin(axes_vec), std::end(axes_vec)) + 1;
-    const auto begin_end_mask = axes_to_mask(axes_vec, slice_indices_length);
+    const auto& data = inputs.at(0);
+    const auto& starts = inputs.at(1);
+    const auto& ends = inputs.at(2);
 
     Output<ngraph::Node> steps;
     if (inputs.size() == 5 && !is_null(inputs.at(4)))  // steps input provided
     {
         steps = inputs.at(4);
     } else {
-        steps = default_opset::Constant::create(element::i64,
-                                                {slice_indices_length},
-                                                std::vector<int64_t>(slice_indices_length, 1));
+        const auto& default_step = default_opset::Constant::create(starts.get_element_type(), {1}, {1});
+        steps =
+            std::make_shared<default_opset::Broadcast>(default_step, std::make_shared<default_opset::ShapeOf>(starts));
     }
 
-    starts = adjust_indices_if_needed(starts, axes_vec, slice_indices_length, 0);
-    ends = adjust_indices_if_needed(ends, axes_vec, slice_indices_length, 0);
-    steps = adjust_indices_if_needed(steps, axes_vec, slice_indices_length, 1);
-
-    return {std::make_shared<default_opset::StridedSlice>(data, starts, ends, steps, begin_end_mask, begin_end_mask)};
+    if (inputs.size() >= 4 && !is_null(inputs.at(3)))  // axes input provided
+    {
+        const auto& axes = inputs.at(3);
+        return {std::make_shared<ov::opset8::Slice>(data, starts, ends, steps, axes)};
+    } else {
+        return {std::make_shared<ov::opset8::Slice>(data, starts, ends, steps)};
+    }
 }
 }  // namespace set_10
 
@@ -75,22 +49,20 @@ OutputVector slice(const Node& node) {
     const auto starts_atr = node.get_attribute_value<std::vector<int64_t>>("starts");
     const auto ends_atr = node.get_attribute_value<std::vector<int64_t>>("ends");
 
-    const auto& starts =
-        std::make_shared<default_opset::Constant>(element::i64, Shape{starts_atr.size()}, starts_atr);
-    const auto& ends =
-        std::make_shared<default_opset::Constant>(element::i64, Shape{ends_atr.size()}, ends_atr);
+    const auto& starts = std::make_shared<default_opset::Constant>(element::i64, Shape{starts_atr.size()}, starts_atr);
+    const auto& ends = std::make_shared<default_opset::Constant>(element::i64, Shape{ends_atr.size()}, ends_atr);
 
     auto axes_atr = node.get_attribute_value<std::vector<int64_t>>("axes", std::vector<int64_t>());
 
     const auto& steps = default_opset::Constant::create(element::i64,
-                                        Shape{starts_atr.size()},
-                                        std::vector<int64_t>(starts_atr.size(), 1));
+                                                        Shape{starts_atr.size()},
+                                                        std::vector<int64_t>(starts_atr.size(), 1));
 
     if (axes_atr.empty()) {
-        return {std::make_shared<opset8::Slice>(data, starts, ends, steps)};
+        return {std::make_shared<ov::opset8::Slice>(data, starts, ends, steps)};
     } else {
         const auto& axes = std::make_shared<default_opset::Constant>(element::i64, Shape{axes_atr.size()}, axes_atr);
-        return {std::make_shared<opset8::Slice>(data, starts, ends, steps, axes)};
+        return {std::make_shared<ov::opset8::Slice>(data, starts, ends, steps, axes)};
     }
 }
 }  // namespace set_1
