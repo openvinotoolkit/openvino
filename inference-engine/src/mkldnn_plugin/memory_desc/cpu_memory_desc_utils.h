8815b2403f3ca003970fa9c0e53a1f8f29572d47
Maksim Kutakov <maksim.kutakov@intel.com>
2021-10-07 20:02:19 +0300

    MemDesc utils extension

--- inference-engine/src/mkldnn_plugin/memory_desc/cpu_memory_desc_utils.h
+++ inference-engine/src/mkldnn_plugin/memory_desc/cpu_memory_desc_utils.h
@@ -81,6 +81,14 @@ public:
      static std::shared_ptr<MemoryDesc> makeDummyDesc(const MemoryDesc& desc, Dim dummyVal = 64);
 
     /**
+    * @brief Makes a static dummy shape where all undefined values are replaced with the upper bound (if defined) or the parameter value
+    * @param desc MemoryDesc from which the new static shape is generated
+    * @param dummyVal Dim value to replace undefined dimensions
+    * @return a new Shape with dummy values instead of undefined dims
+    */
+    static Shape makeDummyShape(const Shape& shape, Dim dummyVal = 64);
+
+    /**
      * @brief Converts dim to string, undefined dim represented as ?
      * @param dim Dim to be converted
      * @return dim as string
