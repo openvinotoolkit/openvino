corrected shape calculation when condition is not elementwise equal, calculated calculation for TF, corrected calculation of shape when values are not set

model-optimizer/extensions/ops/select.py
@@ -3,7 +3,7 @@
 
 import numpy as np
 
-from mo.front.common.partial_infer.utils import compatible_shapes, dynamic_dimension, shape_array
+from mo.front.common.partial_infer.utils import compatible_shapes, dynamic_dimension, shape_array, is_fully_defined
 from mo.graph.graph import Node, Graph, Error
 from mo.ops.op import Op
 from mo.utils.broadcasting import bi_directional_shape_broadcasting, bi_directional_broadcasting
@@ -41,12 +41,31 @@ class Select(Op):
         a_shape = node.in_port(1).data.get_shape()
         b_shape = node.in_port(2).data.get_shape()
         broadcast_rule = node.soft_get('auto_broadcast', 'numpy')
+
         if broadcast_rule == 'numpy':
+            msg = "In Select node '{}' condition and then/else shapes must be broadcastable. " \
+                  "But instead got: cond_shape={}, then_shape={}, else_shape={}".format(
+                    node_name, condition_shape, a_shape, b_shape)
+
             output_shape = bi_directional_shape_broadcasting(a_shape, b_shape)
-            assert output_shape is not None, \
-                'In node \'{}\' for Select operation then/else values shapes must be broadcastable. ' \
-                'But instead got: then_shape={}, else_shape={}'.format(
-                    node_name, a_shape, b_shape)
+            assert output_shape is not None, msg
+
+            # if Select was created from TF Where operations then 1D condition must have the same size
+            # as 0-index dimension of output_shape. This condition is different from being numpy compatible
+            # but by adding ones to the end we can achieve numpy compatibility, as in transformation SelectBroadcast.py
+            if node.has_valid('format') and node['format'] == 'tf' and len(condition_shape) == 1:
+                # https://github.com/tensorflow/tensorflow/blob/master/tensorflow/python/ops/array_ops.py#L4596-L4598
+                msg_tf = "In Select node '{}' if 'condition' is a 1D tensor then it's size " \
+                         "must be matching with the first dimension of then/else branches. " \
+                         "But instead got: cond_shape={}, then_shape={}, else_shape={}".format(
+                            node_name, condition_shape, a_shape, b_shape)
+
+                assert condition_shape[0] == output_shape[0], msg_tf
+                condition_shape = np.concatenate((condition_shape, np.ones(len(output_shape))))
+
+            output_shape = bi_directional_shape_broadcasting(output_shape, condition_shape)
+            assert output_shape is not None, msg
+
         elif broadcast_rule == 'pdpd':
             # todo: add pdpd broadcasting rule
             # note that additionally to output_shape resulting_tensors must be broadcasted as well
@@ -61,8 +80,7 @@ class Select(Op):
         node.out_port(0).data.set_shape(output_shape)
 
         if condition_value is not None:
-            if (resulting_tensors[0] is None or resulting_tensors[1] is None) and np.all(np.ma.getmask(condition_value) == False):
-                assert np.all(condition_value == condition_value.item(0))
+            if is_fully_defined(condition_value) and np.all(condition_value == condition_value.item(0)):
                 # in some graphs Select condition is always True[False] and
                 # one of the branches is None (which is not selected)
                 # if we use np.where for such cases then dtype of output_value will be object (non numeric type)
@@ -77,14 +95,9 @@ class Select(Op):
                     raise Error("PDPD broadcasting rule is not implemented yet")
 
                 node.out_port(0).data.set_value(output_value)
-            else:
+            elif resulting_tensors[0] is not None and resulting_tensors[1] is not None:
                 output_value = np.ma.where(condition_value, resulting_tensors[0], resulting_tensors[1])
-                # If any element of output value is None that means that we use the value from the 'then' or the
-                # 'else' tensor which is not defined, this means that we cannot perform value propagation.
-                if np.any(output_value == None):
-                    return
-                output_value_dtype = resulting_tensors[0].dtype if resulting_tensors[0] is not None else resulting_tensors[1].dtype
-                node.out_port(0).data.set_value(output_value.astype(output_value_dtype))
+                node.out_port(0).data.set_value(output_value)
 
     @staticmethod
     def type_infer(node: Node):

model-optimizer/unit_tests/extensions/ops/select_test.py
@@ -6,7 +6,7 @@ import unittest
 import numpy as np
 
 from extensions.ops.select import Select
-from mo.front.common.partial_infer.utils import dynamic_dimension, shape_array
+from mo.front.common.partial_infer.utils import dynamic_dimension, shape_array, dynamic_dimension_value
 from mo.front.common.partial_infer.utils import strict_compare_tensors, int64_array
 from mo.graph.graph import Node
 from mo.utils.error import Error
@@ -19,7 +19,7 @@ class TestSelect(unittest.TestCase):
     @staticmethod
     def build_select_graph_and_infer(condition_value, then_value, else_value, out_value,
                                      condition_shape=None, then_shape=None, else_shape=None, out_shape=None,
-                                     auto_broadcast='numpy'):
+                                     auto_broadcast='numpy', fw_format=None):
         if then_value is not None:
             then_shape = int64_array(then_value.shape)
         if else_value is not None:
@@ -29,7 +29,7 @@ class TestSelect(unittest.TestCase):
             **valued_const_with_data('then', then_value, then_shape),
             **valued_const_with_data('else', else_value, else_shape),
             **valued_const_with_data('condition', condition_value, condition_shape),
-            **regular_op_with_empty_data('select', {'op': 'Select', 'auto_broadcast': auto_broadcast}),
+            **regular_op_with_empty_data('select', {'op': 'Select', 'auto_broadcast': auto_broadcast, 'format': fw_format}),
             **result('out'),
         }
         edges = [
@@ -167,6 +167,14 @@ class TestSelect(unittest.TestCase):
                                                       out_value=np.ones([2, 3, 4, 5], dtype=np.float))
         self.assertTrue(flag, msg)
 
+    # when output shape is broadcasted from condition, then, and else shapes
+    def test_select_broadcast_with_shape(self):
+        flag, msg = self.build_select_graph_and_infer(condition_shape=[2, 3, 4, 1], condition_value=None,
+                                                      then_shape=[1, 3, 1, 5], then_value=None,
+                                                      else_shape=[2, 1, 1, 5], else_value=None,
+                                                      out_shape=[2, 3, 4, 5], out_value=None)
+        self.assertTrue(flag, msg)
+
     def test_select_infer_assert_shapes(self):
         with self.assertRaisesRegex(AssertionError, "must be broadcastable"):
             self.build_select_graph_and_infer(condition_value=None, condition_shape=[2, 2],
@@ -174,57 +182,103 @@ class TestSelect(unittest.TestCase):
                                               else_value=None, else_shape=[3, 3],
                                               out_value=None, out_shape=[42, 42])
 
+    def test_select_infer_assert_condition_shapes_are_compatible(self):
+        with self.assertRaisesRegex(AssertionError, "must be broadcastable"):
+            self.build_select_graph_and_infer(condition_value=None, condition_shape=[42, 3],
+                                              then_value=None, then_shape=[1, 3],
+                                              else_value=None, else_shape=[3, 3],
+                                              out_value=None, out_shape=[3, 3])
+
     def test_select_infer_masked_1(self):
         flag, msg = self.build_select_graph_and_infer(condition_value=np.ma.array([True, True], mask=[1, 1]),
+                                                      #  condition_value = [dynamic_dimension, dynamic_dimension])
                                                       then_value=None, then_shape=[2],
                                                       else_value=np.zeros((2, 2), dtype=np.int64),
-                                                      out_value=np.ma.array([[1, 1], [1, 1]], mask=[[1, 1], [1, 1]]))
+                                                      out_value=None)
         self.assertTrue(flag, msg)
 
     def test_select_infer_masked_2(self):
         flag, msg = self.build_select_graph_and_infer(condition_value=np.ma.array([False, False], mask=[1, 1]),
+                                                      # condition_value = [dynamic_dimension, dynamic_dimension])
                                                       then_value=None, then_shape=[2],
                                                       else_value=np.zeros((2, 2), dtype=np.int64),
-                                                      out_value=np.ma.array([[1, 1], [1, 1]], mask=[[1, 1], [1, 1]]))
+                                                      out_value=None)
         self.assertTrue(flag, msg)
 
     def test_select_infer_masked_3(self):
         flag, msg = self.build_select_graph_and_infer(condition_value=np.ma.array([True, True], mask=[1, 1]),
+                                                      # condition_value = [dynamic_dimension, dynamic_dimension])
                                                       then_value=None, then_shape=[2],
-                                                      else_value=np.zeros((2, 2), dtype=np.int64), else_shape=[2],
-                                                      out_value=np.ma.array([[1, 1], [1, 1]], mask=[[1, 1], [1, 1]]))
+                                                      else_value=np.zeros((2, 2), dtype=np.int64),
+                                                      out_value=None)
         self.assertTrue(flag, msg)
 
     def test_select_infer_masked_4(self):
         flag, msg = self.build_select_graph_and_infer(condition_value=np.ma.array([True, False], mask=[0, 1]),
+                                                      #  condition_value = [True, dynamic_dimension])
                                                       then_value=np.ones((2, 2), dtype=np.int64),
                                                       else_value=np.zeros((2, 2), dtype=np.int64),
                                                       out_value=np.ma.array([[1, 42], [1, 42]], mask=[[0, 1], [0, 1]]))
+                                                      # out_value = [[1, dynamic_dimension], [1, dynamic_dimension]]
+        self.assertTrue(flag, msg)
+
+    def test_select_infer_masked_5(self):
+        flag, msg = self.build_select_graph_and_infer(condition_value=np.ma.array([False, True], mask=[0, 1]),
+                                                      #  condition_value = [True, dynamic_dimension])
+                                                      then_value=np.ones((2, 2), dtype=np.int64),
+                                                      else_value=np.zeros((2, 2), dtype=np.int64),
+                                                      out_value=np.ma.array([[0, 42], [0, 42]], mask=[[0, 1], [0, 1]]))
+                                                      # out_value = [[0, dynamic_dimension], [0, dynamic_dimension]]
+        self.assertTrue(flag, msg)
+
+    def test_select_infer_masked_6(self):
+        flag, msg = self.build_select_graph_and_infer(condition_value=np.ma.array([True, False], mask=[1, 0]),
+                                                      #  condition_value = [True, dynamic_dimension])
+                                                      then_value=np.ones((2, 2), dtype=np.int64),
+                                                      else_value=np.zeros((2, 2), dtype=np.int64),
+                                                      out_value=np.ma.array([[42, 0], [42, 0]], mask=[[1, 0], [1, 0]]))
+                                                      # out_value = [[dynamic_dimension, 0], [dynamic_dimension, 0]]
         self.assertTrue(flag, msg)
 
     def test_select_infer_no_broadcast_dynamic_then_else_shapes(self):
         flag, msg = self.build_select_graph_and_infer(condition_value=None, condition_shape=shape_array([100, 100]),
-                                                      then_value=None, then_shape=shape_array([100, dynamic_dimension]),
-                                                      else_value=None, else_shape=shape_array([dynamic_dimension, 100]),
+                                                      then_value=None, then_shape=shape_array([100, dynamic_dimension_value]),
+                                                      else_value=None, else_shape=shape_array([dynamic_dimension_value, 100]),
                                                       out_value=None, out_shape=shape_array([100, 100]),
                                                       auto_broadcast='none')
         self.assertTrue(flag, msg)
 
     def test_select_infer_no_broadcast_dynamic_then_else_shapes_2(self):
         flag, msg = self.build_select_graph_and_infer(condition_value=None, condition_shape=shape_array([100, 100]),
-                                                      then_value=None, then_shape=shape_array([dynamic_dimension, 100]),
-                                                      else_value=None, else_shape=shape_array([100, dynamic_dimension]),
+                                                      then_value=None, then_shape=shape_array([dynamic_dimension_value, 100]),
+                                                      else_value=None, else_shape=shape_array([100, dynamic_dimension_value]),
                                                       out_value=None, out_shape=shape_array([100, 100]),
                                                       auto_broadcast='none')
         self.assertTrue(flag, msg)
 
     def test_select_infer_no_broadcast_dynamic_shapes(self):
         flag, msg = self.build_select_graph_and_infer(condition_value=None, condition_shape=shape_array([100, 100]),
-                                                      then_value=None, then_shape=shape_array([100, dynamic_dimension]),
-                                                      else_value=None, else_shape=shape_array([dynamic_dimension, 100]),
+                                                      then_value=None, then_shape=shape_array([100, dynamic_dimension_value]),
+                                                      else_value=None, else_shape=shape_array([dynamic_dimension_value, 100]),
                                                       out_value=None, out_shape=shape_array([100, 100]),
                                                       auto_broadcast='none')
         self.assertTrue(flag, msg)
+
+    def test_select_infer_tf_condition(self):
+        flag, msg = self.build_select_graph_and_infer(condition_value=None, condition_shape=shape_array([100]),
+                                                      then_value=None, then_shape=shape_array([100, 20]),
+                                                      else_value=None, else_shape=shape_array([100, 20]),
+                                                      out_value=None, out_shape=shape_array([100, 20]),
+                                                      auto_broadcast='numpy', fw_format='tf')
+        self.assertTrue(flag, msg)
+
+    def test_select_infer_tf_condition_assert_raises(self):
+        with self.assertRaisesRegex(AssertionError, "if 'condition' is a 1D tensor then it's size"):
+            self.build_select_graph_and_infer(condition_value=None, condition_shape=shape_array([42]),
+                                                      then_value=None, then_shape=shape_array([100, 20]),
+                                                      else_value=None, else_shape=shape_array([100, 20]),
+                                                      out_value=None, out_shape=shape_array([100, 20]),
+                                                      auto_broadcast='numpy', fw_format='tf')
 
     def test_select_infer_assert_pdpd(self):
         with self.assertRaisesRegex(Error, "PDPD broadcasting rule is not implemented yet"):