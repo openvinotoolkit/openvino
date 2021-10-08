bb76ae779c8ab410c6c9e6f619e3693dfe1e7bc5
Sapozhnikov Andrey <andrey.sapozhnikov@intel.com>
2021-10-08 10:51:18 +0300

    [GNA] ActivationLayer tests in integer precision enabled

--- inference-engine/tests/functional/plugin/gna/shared_tests_instances/single_layer_tests/activation.cpp
+++ inference-engine/tests/functional/plugin/gna/shared_tests_instances/single_layer_tests/activation.cpp
@@ -6,6 +6,22 @@
 #include "single_layer_tests/activation.hpp"
 #include "common_test_utils/test_constants.hpp"
 
+namespace LayerTestsDefinitions {
+
+class ActivationLayerTestInteger : public ActivationLayerTest {
+protected:
+    void SetUp() override {
+            ActivationLayerTest::SetUp();
+            threshold = 1.0;
+    }
+};
+
+TEST_P(ActivationLayerTestInteger, CompareWithRefs) {
+        Run();
+}
+
+}  //  namespace LayerTestsDefinitions
+
 using namespace LayerTestsDefinitions;
 using namespace ngraph::helpers;
 namespace {
@@ -16,13 +32,16 @@ const std::vector<InferenceEngine::Precision> inputPrecisions = {
         InferenceEngine::Precision::U8
 };
 
+const std::vector<InferenceEngine::Precision> netPrecisionsIntegers = {
+        InferenceEngine::Precision::I16,
+        InferenceEngine::Precision::U8
+};
+
 const std::vector<InferenceEngine::Precision> netPrecisions = {
         // TODO: Issue:27391
         // InferenceEngine::Precision::FP32,
         // TODO: Issue:28036
         // InferenceEngine::Precision::FP16,
-        InferenceEngine::Precision::I16,
-        InferenceEngine::Precision::U8
 };
 
 const std::map<ActivationTypes, std::vector<std::vector<float>>> activationTypes = {
@@ -61,7 +80,21 @@ const auto basicCases = ::testing::Combine(
         ::testing::Values(CommonTestUtils::DEVICE_GNA)
 );
 
+const auto integerCases = ::testing::Combine(
+        ::testing::ValuesIn(CommonTestUtils::combineParams(activationTypes)),
+        ::testing::ValuesIn(netPrecisionsIntegers),
+        ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
+        ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
+        ::testing::Values(InferenceEngine::Layout::ANY),
+        ::testing::Values(InferenceEngine::Layout::ANY),
+        ::testing::ValuesIn(CommonTestUtils::combineParams(basic)),
+        ::testing::Values(CommonTestUtils::DEVICE_GNA)
+);
+
+
 
 INSTANTIATE_TEST_SUITE_P(smoke_Activation_Basic, ActivationLayerTest, basicCases, ActivationLayerTest::getTestCaseName);
+
+INSTANTIATE_TEST_SUITE_P(smoke_Activation_Basic, ActivationLayerTestInteger, integerCases, ActivationLayerTest::getTestCaseName);
 
 }  // namespace
--- inference-engine/tests/functional/plugin/gna/shared_tests_instances/skip_tests_config.cpp
+++ inference-engine/tests/functional/plugin/gna/shared_tests_instances/skip_tests_config.cpp
@@ -17,7 +17,6 @@ std::vector<std::string> disabledTestPatterns() {
         // TODO: FIX BUG 59041
         ".*Behavior.*CallbackThrowException.*",
         // TODO: FIX BUG 32210
-        R"(.*ActivationLayerTest.CompareWithRefs/(Sigmoid|Tanh|Exp|Log).*)",
         R"(.*ActivationFQSubgraph.*activation=(Exp|Log).*)",
         // TODO: Issue 68586
         R"(.*EltwiseActFqTest.*act=Log.*)",
