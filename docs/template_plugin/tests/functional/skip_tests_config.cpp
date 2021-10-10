fd1cbbbb062df9f6496ab459d05212a0663eb17b
y <ilya.lavrenov@intel.com>
2021-10-10 20:53:15 +0300

    Template plugin tests

--- docs/template_plugin/tests/functional/skip_tests_config.cpp
+++ docs/template_plugin/tests/functional/skip_tests_config.cpp
@@ -24,7 +24,7 @@ std::vector<std::string> disabledTestPatterns() {
         R"(.*ExecGraph.*)",
 
         // TODO: support import / export of precisions in template plugin
-        R"(.*smoke_(Auto|Multi)_BehaviorTests.*OVExecNetwork.ieImportExportedFunction.*)",
+        R"(.*smoke_(Hetero_|)BehaviorTests.*OVExecNetwork.ieImportExportedFunction.*)",
 
         // TODO: Round with f16 is not supported
         R"(.*smoke_Hetero_BehaviorTests.*OVExecNetwork.*readFromV10IR.*)",
