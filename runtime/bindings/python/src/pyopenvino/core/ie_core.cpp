df876af31c4b70b2f691dfa915648abf30dc1e9f
bszmelcz <bartosz.szmelczynski@intel.com>
2021-10-11 13:08:44 +0200

    refactor several core bindings

--- runtime/bindings/python/src/pyopenvino/core/ie_core.cpp
+++ runtime/bindings/python/src/pyopenvino/core/ie_core.cpp
@@ -22,28 +22,18 @@ std::string to_string(py::handle handle) {
 
 void regclass_Core(py::module m) {
     py::class_<ov::runtime::Core, std::shared_ptr<ov::runtime::Core>> cls(m, "Core");
-    cls.def(py::init());
-    cls.def(py::init<const std::string&>());
+    cls.def(py::init<const std::string&>(), py::arg("xml_config_file") = "");
 
-    cls.def(
-        "set_config",
-        [](ov::runtime::Core& self, const py::dict& config, const std::string& device_name) {
-            std::map<std::string, std::string> config_map;
-            for (auto item : config) {
-                config_map[to_string(item.first)] = to_string(item.second);
-            }
-            self.set_config(config_map, device_name);
-        },
-        py::arg("config"),
-        py::arg("device_name"));
+    cls.def("set_config",
+            (void (ov::runtime::Core::*)(const ConfigMap&, const std::string&)) & ov::runtime::Core::set_config,
+            py::arg("config"),
+            py::arg("device_name") = "");
 
     cls.def(
         "compile_model",
         (ov::runtime::ExecutableNetwork(
             ov::runtime::Core::*)(const std::shared_ptr<const ov::Function>&, const std::string&, const ConfigMap&)) &
             ov::runtime::Core::compile_model,
-        // overload_cast_<const std::shared_ptr<const ov::Function>&, const std::string&, const
-        // ConfigMap&>(&ov::runtime::Core::compile_model),
         py::arg("network"),
         py::arg("device_name"),
         py::arg("config") = py::dict());
@@ -57,10 +47,7 @@ void regclass_Core(py::module m) {
         },
         py::arg("extension_path"));
 
-    cls.def("get_versions",
-            (std::map<std::string, ov::ie::Version>(ov::runtime::Core::*)(const std::string&)) &
-                ov::runtime::Core::get_versions,
-            py::arg("device_name"));
+    cls.def("get_versions", &ov::runtime::Core::get_versions);
 
     cls.def(
         "read_model",
@@ -82,13 +69,11 @@ void regclass_Core(py::module m) {
         py::arg("model"),
         py::arg("weights"));
 
-    cls.def(
-        "read_model",
-        [](ov::runtime::Core& self, const std::string& model, const std::string& weights) {
-            return self.read_model(model, weights);
-        },
-        py::arg("model"),
-        py::arg("weights") = "");
+    cls.def("read_model",
+            (std::shared_ptr<ov::Function>(ov::runtime::Core::*)(const std::string&, const std::string&) const) &
+                ov::runtime::Core::read_model,
+            py::arg("model"),
+            py::arg("weights") = "");
 
     cls.def(
         "read_model",
@@ -116,26 +101,23 @@ void regclass_Core(py::module m) {
 
     cls.def(
         "get_config",
-        [](ov::runtime::Core& self, const std::string& device_name, const std::string& config_name) -> py::handle {
-            return Common::parse_parameter(self.get_config(device_name, config_name));
+        [](ov::runtime::Core& self, const std::string& device_name, const std::string& name) -> py::handle {
+            return Common::parse_parameter(self.get_config(device_name, name));
         },
         py::arg("device_name"),
-        py::arg("config_name"));
+        py::arg("name"));
 
     cls.def(
         "get_metric",
-        [](ov::runtime::Core& self, std::string device_name, std::string metric_name) -> py::handle {
-            return Common::parse_parameter(self.get_metric(device_name, metric_name));
+        [](ov::runtime::Core& self, const std::string device_name, const std::string name) -> py::handle {
+            return Common::parse_parameter(self.get_metric(device_name, name));
         },
         py::arg("device_name"),
-        py::arg("metric_name"));
+        py::arg("name"));
 
-    cls.def("register_plugin",
-            &ov::runtime::Core::register_plugin,
-            py::arg("plugin_name"),
-            py::arg("device_name") = py::str());
+    cls.def("register_plugin", &ov::runtime::Core::register_plugin, py::arg("plugin_name"), py::arg("device_name"));
 
-    cls.def("register_plugins", &ov::runtime::Core::register_plugins);
+    cls.def("register_plugins", &ov::runtime::Core::register_plugins, py::arg("xml_config_file"));
 
     cls.def("unload_plugin", &ov::runtime::Core::unload_plugin, py::arg("device_name"));
 
--- runtime/bindings/python/src/pyopenvino/core/ie_version.cpp
+++ runtime/bindings/python/src/pyopenvino/core/ie_version.cpp
@@ -2,24 +2,23 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "pyopenvino/core/ie_version.hpp"
+#include <pybind11/stl.h>
 
-#include <ie_version.hpp>
+#include "openvino/core/version.hpp"
 
 namespace py = pybind11;
 
 void regclass_Version(py::module m) {
-    py::class_<InferenceEngine::Version> cls(m, "Version");
+    py::class_<ov::Version> cls(m, "Version");
 
-    cls.def_readonly("build_number", &InferenceEngine::Version::buildNumber);
-    cls.def_readonly("description", &InferenceEngine::Version::description);
-    cls.def_readwrite("api_version", &InferenceEngine::Version::apiVersion);
+    cls.def_readonly("build_number", &ov::Version::buildNumber);
+    cls.def_readonly("description", &ov::Version::description);
 
-    cls.def_property_readonly("major", [](InferenceEngine::Version& self) {
-        return IE_VERSION_MAJOR;
+    cls.def_property_readonly("major", [](ov::Version& self) {
+        return OPENVINO_VERSION_MAJOR;
     });
 
-    cls.def_property_readonly("minor", [](InferenceEngine::Version& self) {
-        return IE_VERSION_MINOR;
+    cls.def_property_readonly("minor", [](ov::Version& self) {
+        return OPENVINO_VERSION_MINOR;
     });
 }
