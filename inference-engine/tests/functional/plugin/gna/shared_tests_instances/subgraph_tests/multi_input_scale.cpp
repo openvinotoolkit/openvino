3e9397e62d0fb96487f6e58540861a5f3dbf8c9f
Andrey <andrey.noskov@intel.com>
2021-10-11 13:48:26 +0300

    [GNA] added test multi_input_scale

---
+++ inference-engine/tests/functional/plugin/gna/shared_tests_instances/subgraph_tests/multi_input_scale.cpp
@@ -0,0 +1,31 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <subgraph_tests/multi_input_scale.hpp>
+#include "common_test_utils/test_constants.hpp"
+
+namespace SubgraphTestsDefinitions {
+namespace {
+std::vector<size_t> input = {
+    32,
+    15,
+    17,
+    10
+};
+
+std::map<std::string, std::string> additional_config = {
+    {"GNA_DEVICE_MODE", "GNA_SW_EXACT"},
+    {"GNA_SCALE_FACTOR_0", "2"},
+    {"GNA_SCALE_FACTOR_1", "2"}
+};
+} // namespace
+
+INSTANTIATE_TEST_SUITE_P(smoke_multiple_input_scale, MultipleInputScaleTest,
+    ::testing::Combine(
+        ::testing::Values(CommonTestUtils::DEVICE_GNA),
+        ::testing::Values(InferenceEngine::Precision::FP32),
+        ::testing::ValuesIn(input),
+        ::testing::Values(additional_config)),
+    MultipleInputScaleTest::getTestCaseName);
+} // namespace SubgraphTestsDefinitions
