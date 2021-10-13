5631358c46865b837d34931c6849e93970fb6dba
Li, Tingqian <tingqian.li@intel.com>
2021-10-13 13:16:25 +0800

    Add perf_test
    Signed-off-by: Li, Tingqian <tingqian.li@intel.com>

--- inference-engine/tests/unit/cpu/shape_inference_test.cpp
+++ inference-engine/tests/unit/cpu/shape_inference_test.cpp
@@ -41,95 +41,54 @@ TEST(StaticShapeInferenceTest, ConvolutionTest) {
     ASSERT_EQ(conv->get_pads_end(), (CoordinateDiff{1, 1}));
 }
 
-TEST(StaticShapeInferenceTest, ExperimentalDetectronROIFeatureExtractor) {
-    using namespace op::v0;
-    using namespace op::v6;
-
-    ExperimentalDetectronROIFeatureExtractor::Attributes attrs;
-    attrs.aligned = false;
-    attrs.output_size = 14;
-    attrs.sampling_ratio = 2;
-    attrs.pyramid_scales = {4, 8, 16, 32};
-
-    auto input = std::make_shared<Parameter>(element::f32, PartialShape{-1, -1});
-    auto pyramid_layer0 = std::make_shared<Parameter>(element::f32, PartialShape{1, -1, -1, -1});
-    auto pyramid_layer1 = std::make_shared<Parameter>(element::f32, PartialShape{1, -1, -1, -1});
-    auto pyramid_layer2 = std::make_shared<Parameter>(element::f32, PartialShape{1, -1, -1, -1});
-    auto pyramid_layer3 = std::make_shared<Parameter>(element::f32, PartialShape{1, -1, -1, -1});
-
-    auto roi = std::make_shared<ExperimentalDetectronROIFeatureExtractor>(
-        NodeVector{input, pyramid_layer0, pyramid_layer1, pyramid_layer2, pyramid_layer3},
-        attrs);
-
-    std::vector<PartialShape> input_shapes = {PartialShape{1000, 4},
-                                              PartialShape{1, 256, 200, 336},
-                                              PartialShape{1, 256, 100, 168},
-                                              PartialShape{1, 256, 50, 84},
-                                              PartialShape{1, 256, 25, 42}};
-    std::vector<PartialShape> output_shapes = {PartialShape{}, PartialShape{}};
-    shape_infer(roi.get(), input_shapes, output_shapes);
-
-    ASSERT_EQ(roi->get_output_element_type(0), element::f32);
-
-    EXPECT_EQ(output_shapes[0], (Shape{1000, 256, 14, 14}));
-    EXPECT_EQ(output_shapes[1], (Shape{1000, 4}));
-
-    std::vector<StaticShape> input_shapes2 = {StaticShape{1000, 4},
-                                              StaticShape{1, 256, 200, 336},
-                                              StaticShape{1, 256, 100, 168},
-                                              StaticShape{1, 256, 50, 84},
-                                              StaticShape{1, 256, 25, 42}};
-    std::vector<StaticShape> output_shapes2 = {StaticShape{}, StaticShape{}};
-    shape_infer(roi.get(), input_shapes2, output_shapes2);
-
-    ASSERT_EQ(roi->get_output_element_type(0), element::f32);
-
-    EXPECT_EQ(output_shapes2[0], (Shape{1000, 256, 14, 14}));
-    EXPECT_EQ(output_shapes2[1], (Shape{1000, 4}));
-}
-
-struct perf_counter {
+template <typename Callable>
+static void perf_test(Callable func) {
+    static int perf_test_N = std::getenv("PERFN") ? std::atoi(std::getenv("PERFN")) : 1;
     std::chrono::time_point<std::chrono::high_resolution_clock> before;
     std::chrono::time_point<std::chrono::high_resolution_clock> after;
     std::vector<std::chrono::nanoseconds::rep> diffs;
 
-    void clear() {
-        diffs.clear();
+    if (perf_test_N == 1) {
+        func();
+        return;
     }
-    void start() {
+
+    for (size_t i = 0; i < perf_test_N; ++i) {
         before = std::chrono::high_resolution_clock::now();
-    }
-    void stop() {
+        func();
         after = std::chrono::high_resolution_clock::now();
         auto diff = std::chrono::duration_cast<std::chrono::nanoseconds>(after - before).count();
         diffs.push_back(diff);
     }
 
-    std::chrono::nanoseconds::rep avg(int drop_percentage) {
-        std::sort(diffs.begin(), diffs.end());
-        auto skip = diffs.size() * drop_percentage / 2 / 100;
-        auto sum = std::accumulate(diffs.begin() + skip, diffs.end() - skip, 0);
-        return sum / (diffs.size() - 2 * skip);
-    }
-};
+    auto drop_percentage = 20;
+    std::sort(diffs.begin(), diffs.end());
+    auto skip = diffs.size() * drop_percentage / 2 / 100;
+    auto sum = std::accumulate(diffs.begin() + skip, diffs.end() - skip, 0);
+    auto avg = sum / (diffs.size() - 2 * skip);
 
-TEST(StaticShapeInferencePerfTest, ExperimentalDetectronROIFeatureExtractor) {
-    using namespace op::v0;
-    using namespace op::v6;
+    std::cout << " avg:" << avg << std::endl;
+}
 
-    ExperimentalDetectronROIFeatureExtractor::Attributes attrs;
+#define PERF_TEST(st) \
+    perf_test([&]() { \
+        st;           \
+    })
+
+TEST(StaticShapeInferenceTest, ExperimentalDetectronROIFeatureExtractor) {
+    op::v6::ExperimentalDetectronROIFeatureExtractor::Attributes attrs;
     attrs.aligned = false;
     attrs.output_size = 14;
     attrs.sampling_ratio = 2;
     attrs.pyramid_scales = {4, 8, 16, 32};
 
-    auto input = std::make_shared<Parameter>(element::f32, PartialShape{-1, -1});
-    auto pyramid_layer0 = std::make_shared<Parameter>(element::f32, PartialShape{1, -1, -1, -1});
-    auto pyramid_layer1 = std::make_shared<Parameter>(element::f32, PartialShape{1, -1, -1, -1});
-    auto pyramid_layer2 = std::make_shared<Parameter>(element::f32, PartialShape{1, -1, -1, -1});
-    auto pyramid_layer3 = std::make_shared<Parameter>(element::f32, PartialShape{1, -1, -1, -1});
+    auto input = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1});
+    auto pyramid_layer0 = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{1, -1, -1, -1});
+    auto pyramid_layer1 = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{1, -1, -1, -1});
+    auto pyramid_layer2 = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{1, -1, -1, -1});
+    auto pyramid_layer3 = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{1, -1, -1, -1});
 
-    auto roi = std::make_shared<ExperimentalDetectronROIFeatureExtractor>(
+    auto roi = std::make_shared<op::v6::ExperimentalDetectronROIFeatureExtractor>(
         NodeVector{input, pyramid_layer0, pyramid_layer1, pyramid_layer2, pyramid_layer3},
         attrs);
 
@@ -139,44 +98,12 @@ TEST(StaticShapeInferencePerfTest, ExperimentalDetectronROIFeatureExtractor) {
                                               PartialShape{1, 256, 50, 84},
                                               PartialShape{1, 256, 25, 42}};
     std::vector<PartialShape> output_shapes = {PartialShape{}, PartialShape{}};
-
-    perf_counter pc;
-    auto N = 500;
-    std::cout << "shape_infer(ExperimentalDetectronROIFeatureExtractor, PartialShape):" << std::endl;
-    pc.clear();
-    for (size_t i = 0; i < N; ++i) {
-        pc.start();
-        shape_infer(roi.get(), input_shapes, output_shapes);
-        pc.stop();
-    }
-    std::cout << " avg:" << pc.avg(10) << std::endl;
+    PERF_TEST(shape_infer(roi.get(), input_shapes, output_shapes));
 
     ASSERT_EQ(roi->get_output_element_type(0), element::f32);
 
     EXPECT_EQ(output_shapes[0], (Shape{1000, 256, 14, 14}));
     EXPECT_EQ(output_shapes[1], (Shape{1000, 4}));
-}
-
-
-TEST(StaticShapeInferencePerfTest, ExperimentalDetectronROIFeatureExtractor2) {
-    using namespace op::v0;
-    using namespace op::v6;
-
-    ExperimentalDetectronROIFeatureExtractor::Attributes attrs;
-    attrs.aligned = false;
-    attrs.output_size = 14;
-    attrs.sampling_ratio = 2;
-    attrs.pyramid_scales = {4, 8, 16, 32};
-
-    auto input = std::make_shared<Parameter>(element::f32, PartialShape{-1, -1});
-    auto pyramid_layer0 = std::make_shared<Parameter>(element::f32, PartialShape{1, -1, -1, -1});
-    auto pyramid_layer1 = std::make_shared<Parameter>(element::f32, PartialShape{1, -1, -1, -1});
-    auto pyramid_layer2 = std::make_shared<Parameter>(element::f32, PartialShape{1, -1, -1, -1});
-    auto pyramid_layer3 = std::make_shared<Parameter>(element::f32, PartialShape{1, -1, -1, -1});
-
-    auto roi = std::make_shared<ExperimentalDetectronROIFeatureExtractor>(
-        NodeVector{input, pyramid_layer0, pyramid_layer1, pyramid_layer2, pyramid_layer3},
-        attrs);
 
     std::vector<StaticShape> input_shapes2 = {StaticShape{1000, 4},
                                               StaticShape{1, 256, 200, 336},
@@ -184,24 +111,13 @@ TEST(StaticShapeInferencePerfTest, ExperimentalDetectronROIFeatureExtractor2) {
                                               StaticShape{1, 256, 50, 84},
                                               StaticShape{1, 256, 25, 42}};
     std::vector<StaticShape> output_shapes2 = {StaticShape{}, StaticShape{}};
-
-    perf_counter pc;
-    auto N = 500;
-    std::cout << "shape_infer(ExperimentalDetectronROIFeatureExtractor, StaticShape):" << std::endl;
-    pc.clear();
-    for (size_t i = 0; i < N; ++i) {
-        pc.start();
-        shape_infer(roi.get(), input_shapes2, output_shapes2);
-        pc.stop();
-    }
-    std::cout << " avg:" << pc.avg(10) << std::endl;
+    PERF_TEST(shape_infer(roi.get(), input_shapes2, output_shapes2));
 
     ASSERT_EQ(roi->get_output_element_type(0), element::f32);
 
     EXPECT_EQ(output_shapes2[0], (Shape{1000, 256, 14, 14}));
     EXPECT_EQ(output_shapes2[1], (Shape{1000, 4}));
 }
-
 
 #if 0
 TEST(StaticShapeInferenceTest, ConvolutionTimeTest) {
