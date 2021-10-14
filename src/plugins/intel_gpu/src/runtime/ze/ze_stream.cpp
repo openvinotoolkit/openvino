78e025013349109dc84a9970155356da256e8ac0
Andrei Gorbachev <andrei.gorbachev@intel.com>
2021-10-14 16:11:39 +0300

    update ze_stream

--- src/plugins/intel_gpu/src/runtime/ze/ze_stream.cpp
+++ src/plugins/intel_gpu/src/runtime/ze/ze_stream.cpp
@@ -98,211 +98,8 @@ void set_arguments_impl(ze_kernel_handle_t kernel,
                 break;
         }
     }
-
-
-//     using args_t = argument_desc::Types;
-//     using scalar_t = scalar_desc::Types;
-//     for (uint32_t i = 0; i < static_cast<uint32_t>(args.size()); i++) {
-//         cl_int status = CL_INVALID_ARG_VALUE;
-//         switch (args[i].t) {
-//             case args_t::INPUT:
-//                 if (args[i].index < data.inputs.size() && data.inputs[args[i].index]) {
-//                     const auto& input_mem = data.inputs[args[i].index];
-//                     if (input_mem) {
-//                         if (input_mem->get_layout().format.is_image_2d())
-//                             status = kernel.setArg(i, std::dynamic_pointer_cast<const ze::gpu_image2d>(input_mem)->get_buffer());
-//                         else if (memory_capabilities::is_usm_type(input_mem->get_allocation_type()))
-//                             status = kernel.setArgUsm(i, std::dynamic_pointer_cast<const ze::gpu_usm>(input_mem)->get_buffer());
-//                         else
-//                             status = kernel.setArg(i, std::dynamic_pointer_cast<const ze::gpu_buffer>(input_mem)->get_buffer());
-//                     }
-//                 }
-//                 break;
-//             case args_t::INPUT_OF_FUSED_PRIMITIVE:
-//                 if (args[i].index < data.fused_op_inputs.size() && data.fused_op_inputs[args[i].index]) {
-//                     const auto& input_mem = data.fused_op_inputs[args[i].index];
-//                     if (input_mem) {
-//                         if (memory_capabilities::is_usm_type(input_mem->get_allocation_type()))
-//                             status = kernel.setArgUsm(i, std::dynamic_pointer_cast<const ze::gpu_usm>(input_mem)->get_buffer());
-//                         else
-//                             status = kernel.setArg(i, std::dynamic_pointer_cast<const ze::gpu_buffer>(input_mem)->get_buffer());
-//                     }
-//                 }
-//                 break;
-//             case args_t::INTERNAL_BUFFER:
-//                 if (args[i].index < data.intermediates.size() && data.intermediates[args[i].index]) {
-//                     const auto& input_mem = data.intermediates[args[i].index];
-//                     if (input_mem) {
-//                         if (memory_capabilities::is_usm_type(input_mem->get_allocation_type()))
-//                             status = kernel.setArgUsm(i, std::dynamic_pointer_cast<const ze::gpu_usm>(input_mem)->get_buffer());
-//                         else
-//                             status = kernel.setArg(i, std::dynamic_pointer_cast<const ze::gpu_buffer>(input_mem)->get_buffer());
-//                     }
-//                 }
-//                 break;
-//             case args_t::OUTPUT:
-//                 if (data.output) {
-//                      if (data.output->get_layout().format.is_image_2d())
-//                         status = kernel.setArg(i, std::dynamic_pointer_cast<const ze::gpu_image2d>(data.output)->get_buffer());
-//                      else if (memory_capabilities::is_usm_type(data.output->get_allocation_type()))
-//                          status = kernel.setArgUsm(i, std::dynamic_pointer_cast<const ze::gpu_usm>(data.output)->get_buffer());
-//                      else
-//                         status = kernel.setArg(i, std::dynamic_pointer_cast<const ze::gpu_buffer>(data.output)->get_buffer());
-//                 }
-//                 break;
-//             case args_t::WEIGHTS:
-//                 if (data.weights) {
-//                     if (data.weights->get_layout().format.is_image_2d())
-//                         status = kernel.setArg(i, std::dynamic_pointer_cast<const ze::gpu_image2d>(data.weights)->get_buffer());
-//                     else if (memory_capabilities::is_usm_type(data.weights->get_allocation_type()))
-//                         status = kernel.setArgUsm(i, std::dynamic_pointer_cast<const ze::gpu_usm>(data.weights)->get_buffer());
-//                     else
-//                         status = kernel.setArg(i, std::dynamic_pointer_cast<const ze::gpu_buffer>(data.weights)->get_buffer());
-//                 }
-//                 break;
-//             case args_t::BIAS:
-//                 if (data.bias) {
-//                     if (memory_capabilities::is_usm_type(data.bias->get_allocation_type()))
-//                         status = kernel.setArgUsm(i, std::dynamic_pointer_cast<const ze::gpu_usm>(data.bias)->get_buffer());
-//                     else
-//                         status = kernel.setArg(i, std::dynamic_pointer_cast<const ze::gpu_buffer>(data.bias)->get_buffer());
-//                 }
-//                 break;
-//             case args_t::WEIGHTS_ZERO_POINTS:
-//                 if (data.weights_zero_points) {
-//                     if (memory_capabilities::is_usm_type(data.weights_zero_points->get_allocation_type()))
-//                         status = kernel.setArgUsm(
-//                             i,
-//                             std::dynamic_pointer_cast<const ze::gpu_usm>(data.weights_zero_points)->get_buffer());
-//                     else
-//                         status = kernel.setArg(
-//                             i,
-//                             std::dynamic_pointer_cast<const ze::gpu_buffer>(data.weights_zero_points)->get_buffer());
-//                 }
-//                 break;
-//             case args_t::ACTIVATIONS_ZERO_POINTS:
-//                 if (data.activations_zero_points) {
-//                     if (memory_capabilities::is_usm_type(data.activations_zero_points->get_allocation_type()))
-//                         status = kernel.setArgUsm(
-//                             i,
-//                             std::dynamic_pointer_cast<const ze::gpu_usm>(data.activations_zero_points)->get_buffer());
-//                     else
-//                         status = kernel.setArg(
-//                             i,
-//                             std::dynamic_pointer_cast<const ze::gpu_buffer>(data.activations_zero_points)->get_buffer());
-//                 }
-//                 break;
-//             case args_t::COMPENSATION:
-//                 if (data.compensation) {
-//                     if (memory_capabilities::is_usm_type(data.compensation->get_allocation_type()))
-//                         status = kernel.setArgUsm(
-//                                 i,
-//                                 std::dynamic_pointer_cast<const ze::gpu_usm>(data.compensation)->get_buffer());
-//                     else
-//                         status = kernel.setArg(
-//                                  i,
-//                                  std::dynamic_pointer_cast<const ze::gpu_buffer>(data.compensation)->get_buffer());
-//                 }
-//                 break;
-//             case args_t::SCALE_TABLE:
-//                 if (data.scale_table) {
-//                     if (memory_capabilities::is_usm_type(data.scale_table->get_allocation_type()))
-//                         status = kernel.setArgUsm(i, std::dynamic_pointer_cast<const ze::gpu_usm>(data.scale_table)->get_buffer());
-//                     else
-//                         status = kernel.setArg(i, std::dynamic_pointer_cast<const ze::gpu_buffer>(data.scale_table)->get_buffer());
-//                 }
-//                 break;
-//             case args_t::SLOPE:
-//                 if (data.slope) {
-//                     if (memory_capabilities::is_usm_type(data.slope->get_allocation_type()))
-//                         status = kernel.setArgUsm(i, std::dynamic_pointer_cast<const ze::gpu_usm>(data.slope)->get_buffer());
-//                     else
-//                         status = kernel.setArg(i, std::dynamic_pointer_cast<const ze::gpu_buffer>(data.slope)->get_buffer());
-//                 }
-//                 break;
-//             case args_t::SPLIT:
-//                 status = kernel.setArg(i, data.split);
-//                 break;
-//             case args_t::SCALAR:
-//                 if (data.scalars && args[i].index < data.scalars->size()) {
-//                     const auto& scalar = (*data.scalars)[args[i].index];
-//                     switch (scalar.t) {
-//                         case scalar_t::UINT8:
-//                             status = kernel.setArg(i, scalar.v.u8);
-//                             break;
-//                         case scalar_t::UINT16:
-//                             status = kernel.setArg(i, scalar.v.u16);
-//                             break;
-//                         case scalar_t::UINT32:
-//                             status = kernel.setArg(i, scalar.v.u32);
-//                             break;
-//                         case scalar_t::UINT64:
-//                             status = kernel.setArg(i, scalar.v.u64);
-//                             break;
-//                         case scalar_t::INT8:
-//                             status = kernel.setArg(i, scalar.v.s8);
-//                             break;
-//                         case scalar_t::INT16:
-//                             status = kernel.setArg(i, scalar.v.s16);
-//                             break;
-//                         case scalar_t::INT32:
-//                             status = kernel.setArg(i, scalar.v.s32);
-//                             break;
-//                         case scalar_t::INT64:
-//                             status = kernel.setArg(i, scalar.v.s64);
-//                             break;
-//                         case scalar_t::FLOAT32:
-//                             status = kernel.setArg(i, scalar.v.f32);
-//                             break;
-//                         case scalar_t::FLOAT64:
-//                             status = kernel.setArg(i, scalar.v.f64);
-//                             break;
-//                         default:
-//                             break;
-//                     }
-//                 }
-//                 break;
-//             case args_t::RECURRENT:  // RNN/LSTM/GRU layers
-//                 if (data.recurrent) {
-//                     if (data.recurrent->get_layout().format.is_image_2d())
-//                         status = kernel.setArg(i, dynamic_cast<const ze::gpu_image2d&>(*data.recurrent).get_buffer());
-//                     else if (memory_capabilities::is_usm_type(data.recurrent->get_allocation_type()))
-//                         status = kernel.setArgUsm(i, dynamic_cast<const ze::gpu_usm&>(*data.recurrent).get_buffer());
-//                     else
-//                         status = kernel.setArg(i, dynamic_cast<const ze::gpu_buffer&>(*data.recurrent).get_buffer());
-//                 }
-//                 break;
-//             case args_t::HIDDEN:  // RNN/LSTM/GRU layers
-//                 if (data.hidden) {
-//                     if (data.hidden->get_layout().format.is_image_2d())
-//                         status = kernel.setArg(i, dynamic_cast<const ze::gpu_image2d&>(*data.hidden).get_buffer());
-//                     else if (memory_capabilities::is_usm_type(data.hidden->get_allocation_type()))
-//                         status = kernel.setArgUsm(i, dynamic_cast<const ze::gpu_usm&>(*data.hidden).get_buffer());
-//                     else
-//                         status = kernel.setArg(i, dynamic_cast<const ze::gpu_buffer&>(*data.hidden).get_buffer());
-//                 }
-//                 break;
-//             case args_t::CELL:  // LSTMlayers
-//                 if (data.cell) {
-//                     if (data.cell->get_layout().format.is_image_2d())
-//                         status = kernel.setArg(i, dynamic_cast<const ze::gpu_image2d&>(*data.cell).get_buffer());
-//                     else if (memory_capabilities::is_usm_type(data.cell->get_allocation_type()))
-//                         status = kernel.setArgUsm(i, dynamic_cast<const ze::gpu_usm&>(*data.cell).get_buffer());
-//                     else
-//                         status = kernel.setArg(i, dynamic_cast<const ze::gpu_buffer&>(*data.cell).get_buffer());
-//                 }
-//                 break;
-//             default:
-//                 break;
-//         }
-
-//         if (status != CL_SUCCESS) {
-//             throw std::runtime_error("Error set arg " + std::to_string(i) + ", error code: " + std::to_string(status) + "\n");
-//         }
-//     }
 }
 }  // namespace
-
 ze_stream::ze_stream(const ze_engine& engine) : stream(engine.configuration().queue_type), _engine(engine) {
     auto context = engine.get_context();
     auto device = engine.get_device();
@@ -415,7 +212,7 @@ event::ptr ze_stream::enqueue_kernel(kernel& kernel,
                                     &local,
                                     set_output_event ? ret_ev : nullptr,
                                     dep_events_ptr == nullptr ? 0 : dep_events_ptr->size(),
-                                    &dep_events_ptr->at(0)));
+                                    dep_events_ptr == nullptr ? 0 : &dep_events_ptr->at(0)));
 
     return std::make_shared<ze_event>(ret_ev, ++_queue_counter);
     //return nullptr;
@@ -440,6 +237,7 @@ ze_event::ptr ze_stream::enqueue_marker(std::vector<ze_event::ptr> const& deps, 
     ZE_CHECK(zeEventCreate(_event_pool, &eventDesc, &hEvent));
 
     return std::make_shared<ze_event>(hEvent, false);
+
 /*
     if (sync_method  == sync_methods::events) {
         //cl::Event ret_ev;
