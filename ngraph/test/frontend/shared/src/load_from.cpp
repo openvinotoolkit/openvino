e9e44f2435b120766b92a5fb4271c2f0513facec
Ilya Lavrenov <ilya.lavrenov@intel.com>
2021-10-14 12:38:42 +0300

    FEM test (#7899)

--- ngraph/test/frontend/shared/src/load_from.cpp
+++ ngraph/test/frontend/shared/src/load_from.cpp
@@ -6,6 +6,7 @@
 
 #include <fstream>
 
+#include "openvino/core/variant.hpp"
 #include "utils.hpp"
 
 using namespace ngraph;
@@ -36,6 +37,25 @@ TEST_P(FrontEndLoadFromTest, testLoadFromFilePath) {
     ASSERT_NE(m_inputModel, nullptr);
 
     std::shared_ptr<ngraph::Function> function;
+    ASSERT_NO_THROW(function = m_frontEnd->convert(m_inputModel));
+    ASSERT_NE(function, nullptr);
+}
+
+TEST_P(FrontEndLoadFromTest, testLoadFromFilePathWithExplicitVariants) {
+    std::string model_path = FrontEndTestUtils::make_model_path(m_param.m_modelsPath + m_param.m_file);
+    std::vector<std::string> frontends;
+    FrontEnd::Ptr fe;
+    ASSERT_NO_THROW(frontends = m_fem.get_available_front_ends());
+
+    std::vector<std::shared_ptr<ov::Variant>> variants;
+    variants.emplace_back(ov::make_variant(model_path));
+    ASSERT_NO_THROW(m_frontEnd = m_fem.load_by_model(variants));
+    ASSERT_NE(m_frontEnd, nullptr);
+
+    ASSERT_NO_THROW(m_inputModel = m_frontEnd->load(variants));
+    ASSERT_NE(m_inputModel, nullptr);
+
+    std::shared_ptr<ov::Function> function;
     ASSERT_NO_THROW(function = m_frontEnd->convert(m_inputModel));
     ASSERT_NE(function, nullptr);
 }
