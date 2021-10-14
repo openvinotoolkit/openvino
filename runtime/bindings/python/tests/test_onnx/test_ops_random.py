b51fd7dd432a9e93fb483515b53019367aed46ff
Michal Karzynski <michal.karzynski@intel.com>
2021-10-14 12:22:11 +0200

    Add Python tests

--- runtime/bindings/python/tests/test_onnx/test_ops_random.py
+++ runtime/bindings/python/tests/test_onnx/test_ops_random.py
@@ -4,18 +4,50 @@
 import numpy as np
 import onnx
 import onnx.mapping
-import pytest
-from onnx.helper import make_graph, make_model, make_node, make_tensor_value_info
 
-from ngraph.exceptions import NgraphTypeError
-from tests.runtime import get_runtime
-from tests.test_onnx.utils import get_node_model, import_onnx_model, run_model, run_node
+from tests.test_onnx.utils import run_node
+
+
+def test_random_uniform():
+    low = 90.0
+    high = 100.0
+
+    node = onnx.helper.make_node(
+        "RandomUniform",
+        inputs=[],
+        outputs=["y"],
+        high=high,
+        low=low,
+        seed=10.0,
+        shape=(30, 30),
+    )
+
+    result = run_node(node, [])[0]
+
+    assert result.shape == (30, 30)
+    assert len(np.unique(result)) == 900
+    assert np.max(result) < high
+    assert np.min(result) < low
+    assert np.isclose(np.mean(result), np.mean(np.array([low, high])), rtol=0.001)
 
 
 def test_random_normal():
-    node = onnx.helper.make_node("RandomNormal", inputs=[], outputs=["y"], mean=100.0, scale=10.0, seed=5.0, shape=(30, 30))
+    mean = 100.0
+    scale = 10.0
+
+    node = onnx.helper.make_node(
+        "RandomNormal",
+        inputs=[],
+        outputs=["y"],
+        mean=mean,
+        scale=scale,
+        seed=10.0,
+        shape=(30, 30),
+    )
+
     result = run_node(node, [])[0]
-    from IPython import embed; embed()
 
     assert result.shape == (30, 30)
-    assert np.allclose(result, np.eye(shape[0], shape[1], k=shift))
+    assert len(np.unique(result)) == 900
+    assert np.allclose(np.mean(result), mean, rtol=0.05)
+    assert np.allclose(np.std(result), scale, rtol=0.05)
