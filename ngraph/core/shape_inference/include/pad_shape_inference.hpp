2aa6e1898eb3af189c2677b8f399401188c385c4
Li, Tingqian <tingqian.li@intel.com>
2021-10-15 16:36:39 +0800

    fix padding mode checks

--- ngraph/core/shape_inference/include/pad_shape_inference.hpp
+++ ngraph/core/shape_inference/include/pad_shape_inference.hpp
@@ -89,51 +89,28 @@ void shape_infer(Pad* op, const std::vector<T>& input_shapes, std::vector<T>& ou
                     if (op->m_pad_mode == op::PadMode::EDGE) {
                         NODE_VALIDATION_CHECK(op,
                                               begin == 0 || dim > 0,
-                                              "EDGE padding mode does not allow non-zero pad-begin (",
-                                              begin,
-                                              ") for zero dimension (",
-                                              dim,
-                                              ")");
+                                              "EDGE padding mode does not allow non-zero pad-begin for zero dimension");
                         NODE_VALIDATION_CHECK(op,
                                               end == 0 || dim > 0,
-                                              "EDGE padding mode does not allow non-zero pad-end (",
-                                              end,
-                                              ") for zero dimension (",
-                                              dim,
-                                              ")");
+                                              "EDGE padding mode does not allow non-zero pad-end for zero dimension");
                     }
                     if (op->m_pad_mode == op::PadMode::REFLECT) {
                         NODE_VALIDATION_CHECK(op,
                                               begin < dim,
-                                              "REFLECT padding mode does not allow pad-begin (",
-                                              begin,
-                                              ") > dimension (",
-                                              dim,
-                                              ") - 1.");
+                                              "REFLECT padding mode requires pad-begin to be less than dimension");
                         NODE_VALIDATION_CHECK(op,
                                               end < dim,
-                                              "REFLECT padding mode does not allow pad-end (",
-                                              end,
-                                              ") > dimension (",
-                                              dim,
-                                              ") - 1");
+                                              "REFLECT padding mode requires pad-end to be less than dimension");
                     }
-
                     if (op->m_pad_mode == op::PadMode::SYMMETRIC) {
-                        NODE_VALIDATION_CHECK(op,
-                                              begin <= dim,
-                                              "SYMMETRIC padding mode does not allow pad-begin (",
-                                              begin,
-                                              ") > dimension (",
-                                              dim,
-                                              ")");
-                        NODE_VALIDATION_CHECK(op,
-                                              end <= dim,
-                                              "SYMMETRIC padding mode does not allow pad-end (",
-                                              end,
-                                              ") > dimension (",
-                                              dim,
-                                              ")");
+                        NODE_VALIDATION_CHECK(
+                            op,
+                            begin <= dim,
+                            "SYMMETRIC padding mode requires pad-begin to be no greater than dimension");
+                        NODE_VALIDATION_CHECK(
+                            op,
+                            end <= dim,
+                            "SYMMETRIC padding mode requires pad-end to be no greater than dimension");
                     }
 
                     output_shape[i] = static_cast<size_t>(begin + dim + end);
