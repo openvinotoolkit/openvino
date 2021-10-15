4f956591b1bdc27d10c5bbcf570a5f569e3ad1b2
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2021-10-15 09:08:10 +0300

    [GPU] i8/u8 data types support for gather (#6116)

--- inference-engine/thirdparty/clDNN/src/impls/ocl/gather.cpp
+++ inference-engine/thirdparty/clDNN/src/impls/ocl/gather.cpp
@@ -74,12 +74,20 @@ attach_gather_impl::attach_gather_impl() {
         std::make_tuple(data_types::f32, format::bfyx),
         std::make_tuple(data_types::f16, format::bfyx),
         std::make_tuple(data_types::i32, format::bfyx),
+        std::make_tuple(data_types::i8, format::bfyx),
+        std::make_tuple(data_types::u8, format::bfyx),
+
         std::make_tuple(data_types::f32, format::bfzyx),
         std::make_tuple(data_types::f16, format::bfzyx),
         std::make_tuple(data_types::i32, format::bfzyx),
+        std::make_tuple(data_types::i8, format::bfzyx),
+        std::make_tuple(data_types::u8, format::bfzyx),
+
         std::make_tuple(data_types::f32, format::bfwzyx),
         std::make_tuple(data_types::f16, format::bfwzyx),
         std::make_tuple(data_types::i32, format::bfwzyx),
+        std::make_tuple(data_types::i8, format::bfwzyx),
+        std::make_tuple(data_types::u8, format::bfwzyx),
     });
 }
 
