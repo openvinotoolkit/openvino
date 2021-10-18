a43a2de7c3c64ca37df4e3cc84b2a5b449bdc238
sadolini <svetlana.a.dolinina@intel.com>
2021-10-18 11:08:02 +0300

    remove QueryState from GNAExecutableNetwork

--- inference-engine/src/gna_plugin/gna_executable_network.hpp
+++ inference-engine/src/gna_plugin/gna_executable_network.hpp
@@ -58,13 +58,6 @@ class GNAExecutableNetwork : public InferenceEngine::IExecutableNetworkInternal 
         return std::make_shared<GNAInferRequest>(plg, networkInputs, networkOutputs);
     }
 
-    INFERENCE_ENGINE_DEPRECATED("Use InferRequest::QueryState instead")
-    std::vector<InferenceEngine::IVariableStateInternal::Ptr>  QueryState() override {
-        IE_SUPPRESS_DEPRECATED_START
-        return plg->QueryState();
-        IE_SUPPRESS_DEPRECATED_END
-    }
-
     void Export(const std::string &modelFileName) override {
         plg->Export(modelFileName);
     }
