b95d1eba88a7c9b6b1b4924c557d5bd3b480bc32
Chetverikov <anton.chetverikov@intel.com>
2021-10-18 14:16:25 +0300

    Refactor tests

--- model-optimizer/unit_tests/extensions/ops/gathernd_test.py
+++ model-optimizer/unit_tests/extensions/ops/gathernd_test.py
@@ -86,7 +86,7 @@ inputs7 = {'data_data': {'shape': int64_array([2, 3, 4]), 'value': int64_array([
                                                                                       [[[0]],
                                                                                        [[2]],
                                                                                        [[2]]]])}}
-output7 = int64_array([[[2], [5], [11]], [[13], [19], [23]]])
+output7 = int64_array([[2], [5], [11], [13], [19], [23]])
 
 # test data for constant folding: gather elements, batch_dims = 2
 inputs8 = {'data_data': {'shape': int64_array([2, 3, 4, 2]),
@@ -103,12 +103,12 @@ inputs8 = {'data_data': {'shape': int64_array([2, 3, 4, 2]),
                                                   [[[2, 0], [1, 1], [3, 1]],
                                                    [[1, 1], [2, 0], [2, 0]],
                                                    [[0, 0], [3, 1], [3, 1]]]])}}
-output8 = int64_array([[[3, 8, 6],
-                        [10, 12, 13],
-                        [23, 24, 22]],
-                       [[29, 28, 32],
-                        [36, 37, 37],
-                        [41, 48, 48]]])
+output8 = int64_array([[3, 8, 6],
+                       [10, 12, 13],
+                       [23, 24, 22],
+                       [29, 28, 32],
+                       [36, 37, 37],
+                       [41, 48, 48]])
 
 # test data for partial infer: gather slices and batch_dims=2
 inputs9 = {'data_data': {'shape': shape_array([dynamic_dimension_value, 40, 4, 9]), 'value': None},
@@ -135,10 +135,10 @@ inputs_inv3 = {'data_data': {'shape': int64_array([10, 40, 20, 10, 2]), 'value':
                'indices_data': {'shape': int64_array([10, 40, 4]), 'value': None}}
 
 
-class TestGatherNDUpdate(unittest.TestCase):
+class TestGatherND_5(unittest.TestCase):
     def setUp(self):
         nodes_attributes['gathernd_node']['batch_dims'] = 0
-        nodes_attributes['gathernd_node']['version'] = 'opset8'
+        nodes_attributes['gathernd_node']['version'] = 'opset5'
 
     def test_partial_infer_gather_element(self):
         graph = build_graph(nodes_attributes, edges, inputs)
@@ -175,7 +175,7 @@ class TestGatherNDUpdate(unittest.TestCase):
         GatherND.infer(gathernd_node)
 
         # prepare reference results
-        ref_output_shape = int64_array([10, 40, 3, 5, 9])
+        ref_output_shape = int64_array([400, 3, 5, 9])
 
         # get the result
         res_output_shape = graph.node['output']['shape']
@@ -190,7 +190,7 @@ class TestGatherNDUpdate(unittest.TestCase):
         GatherND.infer(gathernd_node)
 
         # prepare reference results
-        ref_output_shape = int64_array([1, 64, 64, 1])
+        ref_output_shape = int64_array([4096, 1])
 
         # get the result
         res_output_shape = graph.node['output']['shape']
@@ -205,7 +205,7 @@ class TestGatherNDUpdate(unittest.TestCase):
         GatherND.infer(gathernd_node)
 
         # prepare reference results
-        ref_output_shape = shape_array([dynamic_dimension_value, 40, 3, 5, 9])
+        ref_output_shape = shape_array([dynamic_dimension_value, 3, 5, 9])
 
         # get the result
         res_output_shape = graph.node['output']['shape']
@@ -220,7 +220,7 @@ class TestGatherNDUpdate(unittest.TestCase):
         GatherND.infer(gathernd_node)
 
         # prepare reference results
-        ref_output_shape = shape_array([40, dynamic_dimension_value, 3, 5, 9])
+        ref_output_shape = shape_array([dynamic_dimension_value, 3, 5, 9])
 
         # get the result
         res_output_shape = graph.node['output']['shape']
@@ -235,7 +235,7 @@ class TestGatherNDUpdate(unittest.TestCase):
         GatherND.infer(gathernd_node)
 
         # prepare reference results
-        ref_output_shape = shape_array([40, 40, 3, 5, 9])
+        ref_output_shape = shape_array([dynamic_dimension_value, 3, 5, 9])
 
         # get the result
         res_output_shape = graph.node['output']['shape']
@@ -277,34 +277,8 @@ class TestGatherNDUpdate(unittest.TestCase):
         self.assertTrue(np.array_equal(output6, res_output_value),
                         'values do not match expected: {} and given: {}'.format(output6, res_output_value))
 
-    def test_infer6_opset_5(self):
-        nodes_attributes['gathernd_node']['batch_dims'] = 1
-        nodes_attributes['gathernd_node']['version'] = 'opset5'
-        graph = build_graph(nodes_attributes, edges, inputs6)
-        gathernd_node = Node(graph, 'gathernd_node')
-        GatherND.infer(gathernd_node)
-
-        # get the result
-        res_output_value = graph.node['output']['value']
-
-        self.assertTrue(np.array_equal(output6, res_output_value),
-                        'values do not match expected: {} and given: {}'.format(output6, res_output_value))
-
     def test_infer7(self):
         nodes_attributes['gathernd_node']['batch_dims'] = 2
-        graph = build_graph(nodes_attributes, edges, inputs7)
-        gathernd_node = Node(graph, 'gathernd_node')
-        GatherND.infer(gathernd_node)
-
-        # get the result
-        res_output_value = graph.node['output']['value']
-
-        self.assertTrue(np.array_equal(output7, res_output_value),
-                        'values do not match expected: {} and given: {}'.format(output7, res_output_value))
-
-    def test_infer7_opset_5(self):
-        nodes_attributes['gathernd_node']['batch_dims'] = 2
-        nodes_attributes['gathernd_node']['version'] = 'opset5'
         graph = build_graph(nodes_attributes, edges, inputs7)
         gathernd_node = Node(graph, 'gathernd_node')
         GatherND.infer(gathernd_node)
@@ -328,20 +302,6 @@ class TestGatherNDUpdate(unittest.TestCase):
         self.assertTrue(np.array_equal(output8, res_output_value),
                         'values do not match expected: {} and given: {}'.format(output8, res_output_value))
 
-    def test_infer8_opset_5(self):
-        nodes_attributes['gathernd_node']['batch_dims'] = 2
-        nodes_attributes['gathernd_node']['version'] = 'opset5'
-        graph = build_graph(nodes_attributes, edges, inputs8)
-        gathernd_node = Node(graph, 'gathernd_node')
-        GatherND.infer(gathernd_node)
-
-        # get the result
-        res_output_value = graph.node['output']['value']
-
-        output = output8.reshape([6, 3])
-        self.assertTrue(np.array_equal(output, res_output_value),
-                        'values do not match expected: {} and given: {}'.format(output, res_output_value))
-
     def test_infer9(self):
         nodes_attributes['gathernd_node']['batch_dims'] = 2
         graph = build_graph(nodes_attributes, edges, inputs8)
@@ -356,7 +316,6 @@ class TestGatherNDUpdate(unittest.TestCase):
 
     def test_infer9_opset_5(self):
         nodes_attributes['gathernd_node']['batch_dims'] = 2
-        nodes_attributes['gathernd_node']['version'] = 'opset5'
         graph = build_graph(nodes_attributes, edges, inputs8)
         gathernd_node = Node(graph, 'gathernd_node')
         GatherND.infer(gathernd_node)
@@ -384,3 +343,114 @@ class TestGatherNDUpdate(unittest.TestCase):
         graph = build_graph(nodes_attributes, edges, inputs_inv3)
         gathernd_node = Node(graph, 'gathernd_node')
         self.assertRaises(AssertionError, GatherND.infer, gathernd_node)
+
+
+    def test_partial_infer_gather_slice_batch_dims2_opset8(self):
+        nodes_attributes['gathernd_node']['batch_dims'] = 2
+        nodes_attributes['gathernd_node']['version'] = 'opset8'
+        graph = build_graph(nodes_attributes, edges, inputs2)
+        gathernd_node = Node(graph, 'gathernd_node')
+        GatherND.infer(gathernd_node)
+
+        # prepare reference results
+        ref_output_shape = int64_array([10, 40, 3, 5, 9])
+
+        # get the result
+        res_output_shape = graph.node['output']['shape']
+
+        self.assertTrue(np.array_equal(ref_output_shape, res_output_shape),
+                        'values do not match expected: {} and given: {}'.format(ref_output_shape, res_output_shape))
+
+    def test_partial_infer_gather_slice_batch_dims3_opset8(self):
+        nodes_attributes['gathernd_node']['batch_dims'] = 3
+        nodes_attributes['gathernd_node']['version'] = 'opset8'
+        graph = build_graph(nodes_attributes, edges, inputs3)
+        gathernd_node = Node(graph, 'gathernd_node')
+        GatherND.infer(gathernd_node)
+
+        # prepare reference results
+        ref_output_shape = int64_array([1, 64, 64, 1])
+
+        # get the result
+        res_output_shape = graph.node['output']['shape']
+
+        self.assertTrue(np.array_equal(ref_output_shape, res_output_shape),
+                        'values do not match expected: {} and given: {}'.format(ref_output_shape, res_output_shape))
+
+    def test_partial_infer_gather_slice_batch_dims2_dynamic1_opset8(self):
+        nodes_attributes['gathernd_node']['batch_dims'] = 2
+        nodes_attributes['gathernd_node']['version'] = 'opset8'
+        graph = build_graph(nodes_attributes, edges, inputs9)
+        gathernd_node = Node(graph, 'gathernd_node')
+        GatherND.infer(gathernd_node)
+
+        # prepare reference results
+        ref_output_shape = shape_array([dynamic_dimension_value, 40, 3, 5, 9])
+
+        # get the result
+        res_output_shape = graph.node['output']['shape']
+
+        self.assertTrue(strict_compare_tensors(ref_output_shape, res_output_shape),
+                        'values do not match expected: {} and given: {}'.format(ref_output_shape, res_output_shape))
+
+    def test_partial_infer_gather_slice_batch_dims2_dynamic2_opset8(self):
+        nodes_attributes['gathernd_node']['batch_dims'] = 2
+        nodes_attributes['gathernd_node']['version'] = 'opset8'
+        graph = build_graph(nodes_attributes, edges, inputs10)
+        gathernd_node = Node(graph, 'gathernd_node')
+        GatherND.infer(gathernd_node)
+
+        # prepare reference results
+        ref_output_shape = shape_array([40, dynamic_dimension_value, 3, 5, 9])
+
+        # get the result
+        res_output_shape = graph.node['output']['shape']
+
+        self.assertTrue(strict_compare_tensors(ref_output_shape, res_output_shape),
+                        'values do not match expected: {} and given: {}'.format(ref_output_shape, res_output_shape))
+
+    def test_partial_infer_gather_slice_batch_dims2_dynamic3_opset8(self):
+        nodes_attributes['gathernd_node']['batch_dims'] = 2
+        nodes_attributes['gathernd_node']['version'] = 'opset8'
+        graph = build_graph(nodes_attributes, edges, inputs11)
+        gathernd_node = Node(graph, 'gathernd_node')
+        GatherND.infer(gathernd_node)
+
+        # prepare reference results
+        ref_output_shape = shape_array([40, 40, 3, 5, 9])
+
+        # get the result
+        res_output_shape = graph.node['output']['shape']
+
+        self.assertTrue(strict_compare_tensors(ref_output_shape, res_output_shape),
+                        'values do not match expected: {} and given: {}'.format(ref_output_shape, res_output_shape))
+
+    def test_infer7_opset8(self):
+        nodes_attributes['gathernd_node']['batch_dims'] = 2
+        nodes_attributes['gathernd_node']['version'] = 'opset8'
+        graph = build_graph(nodes_attributes, edges, inputs7)
+        gathernd_node = Node(graph, 'gathernd_node')
+        GatherND.infer(gathernd_node)
+
+        # get the result
+        res_output_value = graph.node['output']['value']
+
+        output = output7.reshape([2, 3, 1])
+
+        self.assertTrue(np.array_equal(output, res_output_value),
+                        'values do not match expected: {} and given: {}'.format(output, res_output_value))
+
+    def test_infer8_opset8(self):
+        nodes_attributes['gathernd_node']['batch_dims'] = 2
+        nodes_attributes['gathernd_node']['version'] = 'opset8'
+        graph = build_graph(nodes_attributes, edges, inputs8)
+        gathernd_node = Node(graph, 'gathernd_node')
+        GatherND.infer(gathernd_node)
+
+        # get the result
+        res_output_value = graph.node['output']['value']
+
+        output = output8.reshape([2, 3, 3])
+
+        self.assertTrue(np.array_equal(output, res_output_value),
+                        'values do not match expected: {} and given: {}'.format(output, res_output_value))
