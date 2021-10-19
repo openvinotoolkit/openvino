8b42b6ad2fa324f02ab28e151a5a0fdc4aa48aa9
pszmel <piotr.szmelczynski@intel.com>
2021-10-19 15:17:19 +0200

    add doc() and args to Layout

--- runtime/bindings/python/src/pyopenvino/graph/layout.cpp
+++ runtime/bindings/python/src/pyopenvino/graph/layout.cpp
@@ -9,16 +9,17 @@ namespace py = pybind11;
 
 void regclass_graph_Layout(py::module m) {
     py::class_<ov::Layout, std::shared_ptr<ov::Layout>> layout(m, "Layout");
+    layout.doc() = "openvino.impl.Layout wraps ov::Layout";
 
     layout.def(py::init<>());
-    layout.def(py::init<const std::string&>());
+    layout.def(py::init<const std::string&>(), py::arg("layoutStr"));
 
     // operator overloading
     layout.def(py::self == py::self);
     layout.def(py::self != py::self);
 
     layout.def("scalar", &ov::Layout::scalar);
-    layout.def("has_name", &ov::Layout::has_name);
-    layout.def("get_index_by_name", &ov::Layout::get_index_by_name);
+    layout.def("has_name", &ov::Layout::has_name, py::arg("dimensionName"));
+    layout.def("get_index_by_name", &ov::Layout::get_index_by_name, py::arg("dimensionName"));
     layout.def("to_string", &ov::Layout::to_string);
 }
