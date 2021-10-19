3a5eefc897fcc75d54a4ba0770cb63365d249cdb
Vladislav Golubev <vladislav.golubev@intel.com>
2021-10-19 10:59:29 +0300

    [dynamism][CPU] MatMul transformations: dynamic shapes support (#7582)
    * [TESTS] CPUUnitTests: unitTestUtils added to CMakeLists
    
    * Reshape1D: functions moved to namespace
    
    * [nGraph] PartialShape extending
    
    * CPU specific transsformations for FullyConnected enabled to work with dynamic shapes
    
    * [Transformations] FC transformations: dynamic shapes support
    
    * [Transformations] FCBiasFusion: removed legacy check on bcast on weights
    
    * [TESTS] FC transformations: tests
    
    * [Transformations] SmartReshape: avoid tranformation utils methods
    
    * codestyle fix
    
    * codestyle fix
    
    * [TESTS] MatMulTransformation tests: compilation error fix
    
    * [CPU] FullyConnected: shape inference
    
    * postreview fixes
    
    * [CPU] FullyConnected: shape inference fix
    
    * [nGraph] PShape dimensions insertion fixed
    
    Co-authored-by: Stepyreva, Evgenya <evgenya.stepyreva@intel.com>

--- inference-engine/src/mkldnn_plugin/ngraph_transformations/op/fully_connected.hpp
+++ inference-engine/src/mkldnn_plugin/ngraph_transformations/op/fully_connected.hpp
@@ -19,13 +19,13 @@ public:
 
     FullyConnectedNode(const ngraph::Output<Node> &A,
                        const ngraph::Output<Node> &B,
-                       const ngraph::Shape &output_shape,
+                       const ngraph::Rank& output_rank,
                        const ngraph::element::Type output_type = ngraph::element::undefined);
 
     FullyConnectedNode(const ngraph::Output<Node> &A,
                        const ngraph::Output<Node> &B,
                        const ngraph::Output<Node> &C,
-                       const ngraph::Shape &output_shape,
+                       const ngraph::Rank& output_rank,
                        const ngraph::element::Type output_type = ngraph::element::undefined);
 
     bool visit_attributes(ngraph::AttributeVisitor &visitor) override;
@@ -34,13 +34,11 @@ public:
 
     std::shared_ptr<Node> clone_with_new_inputs(const ngraph::OutputVector& new_args) const override;
 
-    size_t get_out_size() const { return m_output_size; }
-
+    ngraph::Rank get_output_rank() const { return m_output_rank; }
     ngraph::element::Type get_output_type() const { return m_output_type; }
 
 private:
-    size_t m_output_size = 0;
-    ngraph::Shape m_output_shape = {};
+    ngraph::Rank m_output_rank;
     ngraph::element::Type m_output_type;
 };
 
