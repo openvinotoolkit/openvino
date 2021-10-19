70cd19446909279f08e7effcf743904306016d49
tomdol <tomasz.dolbniak@intel.com>
2021-10-19 16:12:52 +0200

    Merge of old& new bindings for offline_transformations

--- runtime/bindings/python/src/openvino/offline_transformations/__init__.py
+++ runtime/bindings/python/src/openvino/offline_transformations/__init__.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
 from openvino.pyopenvino.offline_transformations import apply_moc_transformations
 from openvino.pyopenvino.offline_transformations import apply_pot_transformations
 from openvino.pyopenvino.offline_transformations import apply_low_latency_transformation
