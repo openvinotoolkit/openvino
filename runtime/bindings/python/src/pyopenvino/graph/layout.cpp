4ecb0b20ad3307f8f9dd27e159b5f063ad1871aa
pszmel <piotr.szmelczynski@intel.com>
2021-10-19 15:35:26 +0200

    fix style

--- runtime/bindings/python/src/pyopenvino/graph/layout.cpp
+++ runtime/bindings/python/src/pyopenvino/graph/layout.cpp
@@ -12,14 +12,14 @@ void regclass_graph_Layout(py::module m) {
     layout.doc() = "openvino.impl.Layout wraps ov::Layout";
 
     layout.def(py::init<>());
-    layout.def(py::init<const std::string&>(), py::arg("layoutStr"));
+    layout.def(py::init<const std::string&>(), py::arg("layout_str"));
 
     // operator overloading
     layout.def(py::self == py::self);
     layout.def(py::self != py::self);
 
     layout.def("scalar", &ov::Layout::scalar);
-    layout.def("has_name", &ov::Layout::has_name, py::arg("dimensionName"));
-    layout.def("get_index_by_name", &ov::Layout::get_index_by_name, py::arg("dimensionName"));
+    layout.def("has_name", &ov::Layout::has_name, py::arg("dimension_name"));
+    layout.def("get_index_by_name", &ov::Layout::get_index_by_name, py::arg("dimension_name"));
     layout.def("to_string", &ov::Layout::to_string);
 }
