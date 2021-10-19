59fb6c2a4dd2316af4b43bbf90af0ed7bac42da7
Anton Pankratv <anton.pankratov@intel.com>
2021-10-19 04:53:33 +0300

    Forbid typed data access and roi tensor creation for little bit types (#8007)

--- ngraph/core/src/runtime/ov_tensor.cpp
+++ ngraph/core/src/runtime/ov_tensor.cpp
@@ -65,6 +65,9 @@ Tensor::Tensor(const element::Type element_type, const Shape& shape, void* host_
 }
 
 Tensor::Tensor(const Tensor& owner, const Coordinate& begin, const Coordinate& end) : _so{owner._so} {
+    OPENVINO_ASSERT(owner.get_element_type().bitwidth() >= 8,
+                    "ROI Tensor for types with bitwidths less then 8 bit is not implemented. Tensor type: ",
+                    owner.get_element_type());
     try {
         _impl = owner._impl->createROI(begin, end);
     } catch (const std::exception& ex) {
@@ -87,7 +90,10 @@ Shape Tensor::get_shape() const {
 }
 
 Strides Tensor::get_strides() const {
-    OV_TENSOR_STATEMENT(return _impl->getTensorDesc().getBlockingDesc().getStrides(););
+    OPENVINO_ASSERT(get_element_type().bitwidth() >= 8,
+                    "Could not get strides for types with bitwidths less then 8 bit. Tensor type: ",
+                    get_element_type());
+    OV_TENSOR_STATEMENT(return _impl->getTensorDesc().getBlockingDesc().getStrides());
 }
 
 size_t Tensor::get_size() const {
@@ -108,20 +114,18 @@ void* Tensor::data(const element::Type element_type) const {
 #undef TYPE_CHECK
     OPENVINO_ASSERT(host_accesable_implementation, "Tensor implementation type dose not contains host accessable data");
     if (element_type != element::undefined) {
-        OPENVINO_ASSERT(
-            element::fundamental_type_for(element_type) == element::fundamental_type_for(get_element_type()),
-            get_element_type(),
-            " tensor fundamental element type is ",
-            element::fundamental_type_for(get_element_type()),
-            ", but it casted to ",
-            element_type,
-            " with fundamental element type ",
-            element::fundamental_type_for(element_type));
+        OPENVINO_ASSERT(element_type == get_element_type(),
+                        "Tensor data with element type ",
+                        get_element_type(),
+                        ", is not representable as pointer to ",
+                        element_type);
     }
-    OV_TENSOR_STATEMENT({
-        return _impl->getTensorDesc().getBlockingDesc().getOffsetPadding() * get_element_type().size() +
-               InferenceEngine::as<InferenceEngine::MemoryBlob>(_impl)->rmap().as<uint8_t*>();
-    });
+    auto byte_offset = _impl->getTensorDesc().getBlockingDesc().getOffsetPadding() * get_element_type().size();
+    OPENVINO_ASSERT((get_element_type().bitwidth() >= 8) || (byte_offset == 0),
+                    "ROI access for types with bitwidths less then 8 bit is not implemented. Tensor type: ",
+                    get_element_type());
+    OV_TENSOR_STATEMENT(
+        { return byte_offset + InferenceEngine::as<InferenceEngine::MemoryBlob>(_impl)->rmap().as<uint8_t*>(); });
 }
 
 bool Tensor::operator!() const noexcept {
