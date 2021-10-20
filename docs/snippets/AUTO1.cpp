e6eb0242e0a9ec60346dd8ab2726e855e6ceb2a9
Yuan Hu <yuan2.hu@intel.com>
2021-10-21 00:44:58 +0800

    replace AUTO_DEVICE_LIST with MULTI_DEVICE_PRIORITY in test cases (#7932)
    * replace AUTO_DEVICE_LIST with MULTI_DEVICE_PRIORITY in test case
    
    Signed-off-by: Hu, Yuan2 <yuan2.hu@intel.com>
    
    * remove KEY_AUTO_DEVICE_LIST
    
    Signed-off-by: Hu, Yuan2 <yuan2.hu@intel.com>

--- docs/snippets/AUTO1.cpp
+++ docs/snippets/AUTO1.cpp
@@ -5,10 +5,10 @@ int main() {
     InferenceEngine::Core ie;
     InferenceEngine::CNNNetwork network = ie.ReadNetwork("sample.xml");
     // "AUTO" plugin is (globally) pre-configured with the explicit option:
-    ie.SetConfig({{"AUTO_DEVICE_LIST", "CPU,GPU"}}, "AUTO");
+    ie.SetConfig({{"MULTI_DEVICE_PRIORITIES", "CPU,GPU"}}, "AUTO");
     // the below 3 lines are equivalent (the first line leverages the pre-configured AUTO, while second and third explicitly pass the same settings)
     InferenceEngine::ExecutableNetwork exec0 = ie.LoadNetwork(network, "AUTO", {});
-    InferenceEngine::ExecutableNetwork exec1 = ie.LoadNetwork(network, "AUTO", {{"AUTO_DEVICE_LIST", "CPU,GPU"}});
+    InferenceEngine::ExecutableNetwork exec1 = ie.LoadNetwork(network, "AUTO", {{"MULTI_DEVICE_PRIORITIES", "CPU,GPU"}});
     InferenceEngine::ExecutableNetwork exec2 = ie.LoadNetwork(network, "AUTO:CPU,GPU");
 //! [part1]
 return 0;
