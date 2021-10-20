140feb09f8653e58f3db54b3dcaafa1ae5968b7a
sadolini <svetlana.a.dolinina@intel.com>
2021-10-20 15:08:15 +0300

    add subgraph instead of constant with fixed shape to allow model have undefined batch

--- model-optimizer/extensions/middle/ReverseV2ToReverseSequence.py
+++ model-optimizer/extensions/middle/ReverseV2ToReverseSequence.py
@@ -3,11 +3,15 @@
 
 import numpy as np
 
+from extensions.ops.elementwise import Mul
 from extensions.ops.reverse_sequence import ReverseSequence
-from mo.front.tf.graph_utils import create_op_node_with_second_input
 from mo.graph.graph import Graph, rename_node
 from mo.middle.replacement import MiddleReplacementPattern
+from mo.ops.broadcast import Broadcast
+from mo.ops.const import Const
+from mo.ops.shape import Shape
 from mo.utils.error import Error
+from mo.utils.shape import node_to_get_shape_value_of_indices
 
 
 class ReverseToReverseSequence(MiddleReplacementPattern):
@@ -44,14 +48,27 @@ class ReverseToReverseSequence(MiddleReplacementPattern):
         batch_axis = int(not seq_axis)
 
         # 1. For ReverseSequence 1-port input is seq_lengths => create this input node
-        seq_lengths = np.ones(input_data_shape[batch_axis]) * input_data_shape[seq_axis]
-
         reverse_name = reverse.soft_get('name',  reverse.id)
         rename_node(reverse, reverse_name + '/to_delete')
+
+        one_const = Const(graph, {'name': reverse_name + "/one",
+                                  'value': np.ones([1])}).create_node()
+        shape_node = Shape(graph, {'name': reverse_name + "/shape"}).create_node()
+        reverse.in_port(0).get_source().connect(shape_node.in_port(0))
+        batch_node = node_to_get_shape_value_of_indices(shape_node, [batch_axis])
+        seq_axis_node = node_to_get_shape_value_of_indices(shape_node, [seq_axis])
+        seq_length_shape_node = Mul(graph, {'name': reverse_name + "/mul"}).create_node()
+        seq_length_shape_node.in_port(0).connect(batch_node.out_port(0))
+        seq_length_shape_node.in_port(1).connect(seq_axis_node.out_port(0))
+        broadcast_node = Broadcast(graph, {'name': reverse_name + "/broadcast"}).create_node()
+        broadcast_node.in_port(0).connect(one_const.out_port(0))
+        broadcast_node.in_port(1).connect(seq_length_shape_node.out_port(0))
+
         # 2. Create new ReverseSequence node and reconnect all inputs/outputs to it
-        reverse_sequence = create_op_node_with_second_input(graph, ReverseSequence, seq_lengths,
-                                                            {'name':  reverse_name, 'seq_axis': seq_axis,
-                                                             'batch_axis': batch_axis})
+        reverse_sequence = ReverseSequence(graph, {'name':  reverse_name, 'seq_axis': seq_axis,
+                                                   'batch_axis': batch_axis}).create_node()
+        reverse_sequence.in_port(1).connect(broadcast_node.out_port(0))
+
         rename_node(reverse_sequence, reverse_name)
         reverse.in_port(0).get_connection().set_destination(reverse_sequence.in_port(0))
         reverse.out_port(0).get_connection().set_source(reverse_sequence.out_port(0))
