4733e0b4cfbc3db25ab01897467346695fa02ebe
Steve Yoo <steve.yoo@intel.com>
2021-10-20 11:21:10 +0900

    Remove test names and update test for types

--- docs/template_plugin/tests/functional/op_reference/constant.cpp
+++ docs/template_plugin/tests/functional/op_reference/constant.cpp
@@ -56,133 +56,260 @@ public:
         return result.str();
     }
 
-private:
+protected:
     using ReferenceType = std::tuple<std::shared_ptr<Function>, std::vector<ov::runtime::Tensor>, std::vector<ov::runtime::Tensor>>;
-    ReferenceType CreateFunction(const ParamType& params) {
+
+    virtual ReferenceType CreateFunction(const ParamType& params) {
         ReferenceType reference;
-        if (params.testcaseName == "tensor_2constant") {
-            auto A = op::v0::Constant::create(params.inType, params.inputShape.to_shape(), params.inputData.data());
-            auto B = op::v0::Constant::create(params.inType, params.inputShape.to_shape(), params.inputData.data());
-            auto function = std::make_shared<Function>(NodeVector{A, B}, ParameterVector{});
-            reference = {function, {}, {params.refData, params.refData}};
-        } else if (params.testcaseName == "tensor_constant_with_op") {
-            auto A = op::v0::Constant::create(params.inType, params.inputShape.to_shape(), params.inputData.data());
-            auto function = std::make_shared<Function>(std::make_shared<op::v0::Abs>(A), ParameterVector{});
-            reference = {function, {}, {params.refData}};
-        } else if (params.testcaseName == "constant_multi_use") {
-            const auto A = std::make_shared<op::v0::Constant>(
-                params.inType,
-                params.inputShape.to_shape(),
-                std::vector<std::string>{std::to_string(*reinterpret_cast<int*>(params.inputData.data()))});
-            auto function = std::make_shared<Function>(A, ParameterVector {});
-            reference = {function, {}, {params.refData}};
-        } else if (params.testcaseName == "constant_equality_bool") {
-            auto A = op::v0::Constant::create(params.inType, params.inputShape.to_shape(), params.inputData.data());
-            auto B = op::v0::Constant::create(params.inType, params.inputShape.to_shape(), {true, true, true, true});
-            auto function = std::make_shared<Function>(std::make_shared<op::v1::Equal>(A, B), ParameterVector{});
-            reference = {function, {}, {params.refData}};
-        } else {
-            auto A = op::v0::Constant::create(params.inType, params.inputShape.to_shape(), params.inputData.data());
-            auto function = std::make_shared<Function>(A, ParameterVector{});
-            reference = {function, {}, {params.refData}};
-        }
+        auto A = op::v0::Constant::create(params.inType, params.inputShape.to_shape(), params.inputData.data());
+        auto function = std::make_shared<Function>(A, ParameterVector{});
+        reference = {function, {}, {params.refData}};
+        return reference;
+    }
+};
+
+class ReferenceConstantLayerTest_2Constant : public ReferenceConstantLayerTest {
+protected:
+    ReferenceType CreateFunction(const ParamType& params) override {
+        ReferenceType reference;
+        auto A = op::v0::Constant::create(params.inType, params.inputShape.to_shape(), params.inputData.data());
+        auto B = op::v0::Constant::create(params.inType, params.inputShape.to_shape(), params.inputData.data());
+        auto function = std::make_shared<Function>(NodeVector{A, B}, ParameterVector{});
+        reference = {function, {}, {params.refData, params.refData}};
+        return reference;
+    }
+};
+
+class ReferenceConstantLayerTest_WithOp : public ReferenceConstantLayerTest {
+protected:
+    ReferenceType CreateFunction(const ParamType& params) override {
+        ReferenceType reference;
+        auto A = op::v0::Constant::create(params.inType, params.inputShape.to_shape(), params.inputData.data());
+        auto function = std::make_shared<Function>(std::make_shared<op::v0::Abs>(A), ParameterVector{});
+        reference = {function, {}, {params.refData}};
+        return reference;
+    }
+};
+
+class ReferenceConstantLayerTest_MultiUse : public ReferenceConstantLayerTest {
+protected:
+    ReferenceType CreateFunction(const ParamType& params) override {
+        ReferenceType reference;
+        const auto A = std::make_shared<op::v0::Constant>(
+            params.inType,
+            params.inputShape.to_shape(),
+            std::vector<std::string>{std::to_string(*reinterpret_cast<int*>(params.inputData.data()))});
+        auto function = std::make_shared<Function>(A, ParameterVector {});
+        reference = {function, {}, {params.refData}};
+        return reference;
+    }
+};
+
+class ReferenceConstantLayerTest_EqualityBool : public ReferenceConstantLayerTest {
+protected:
+    ReferenceType CreateFunction(const ParamType& params) override {
+        ReferenceType reference;
+        auto A = op::v0::Constant::create(params.inType, params.inputShape.to_shape(), params.inputData.data());
+        auto B = op::v0::Constant::create(params.inType, params.inputShape.to_shape(), {true, true, true, true});
+        auto function = std::make_shared<Function>(std::make_shared<op::v1::Equal>(A, B), ParameterVector{});
+        reference = {function, {}, {params.refData}};
         return reference;
     }
 };
 
 TEST_P(ReferenceConstantLayerTest, CompareWithHardcodedRefs) {
     Exec();
-    if (this->GetParam().testcaseName == "constant_multi_use") {
-        Infer();
-        Validate();
-    }
 }
 
-template <element::Type_t IET, element::Type_t OET>
-ConstantParams generateConstantParams(const PartialShape& staticShape,
-                                      const std::vector<typename element_type_traits<IET>::value_type>& input,
-                                      const std::vector<typename element_type_traits<OET>::value_type>& expected,
-                                      const std::string& test_name = "") {
-    return ConstantParams(staticShape, IET, OET, input, expected, test_name);
+TEST_P(ReferenceConstantLayerTest_2Constant, CompareWithHardcodedRefs) {
+    Exec();
+}
+
+TEST_P(ReferenceConstantLayerTest_WithOp, CompareWithHardcodedRefs) {
+    Exec();
+}
+
+TEST_P(ReferenceConstantLayerTest_MultiUse, CompareWithHardcodedRefs) {
+    Exec();
+    Infer();
+    Validate();
+}
+
+TEST_P(ReferenceConstantLayerTest_EqualityBool, CompareWithHardcodedRefs) {
+    Exec();
+}
+
+template <element::Type_t IN_ET>
+std::vector<ConstantParams> generateConstantParams() {
+    using T = typename element_type_traits<IN_ET>::value_type;
+    std::vector<ConstantParams> constantParams {
+        // tensor_constant
+        ConstantParams({2, 2, 2}, IN_ET, IN_ET,
+                       std::vector<T>{1, 2, 3, 4, 5, 6, 7, 8},
+                       std::vector<T>{1, 2, 3, 4, 5, 6, 7, 8},
+                       "tensor_constant"),
+    };
+    return constantParams;
+}
+
+std::vector<ConstantParams> generateConstantDefinedTypeParams() {
+    std::vector<ConstantParams> constantParams {
+        // scalar_constant_float32
+        ConstantParams({}, element::Type_t::f32, element::Type_t::f32,
+            std::vector<float>{4.75},
+            std::vector<float>{4.75f},
+            "scalar_constant_float32"),
+        // scalar_constant_int64
+        ConstantParams({}, element::Type_t::i64, element::Type_t::i64,
+            std::vector<int64_t>{0x4000000000000001},
+            std::vector<int64_t>{0x4000000000000001},
+            "scalar_constant_int64"),
+        // tensor_constant_float32
+        ConstantParams({2, 2}, element::Type_t::f32, element::Type_t::f32,
+            std::vector<float>{4.75, 4.5, -5.25, 0.0},
+            std::vector<float>{4.75f, 4.5f, -5.25f, 0.0f},
+            "tensor_constant_float32"),
+        // tensor_constant_int64
+        ConstantParams({2}, element::Type_t::i64, element::Type_t::i64,
+            std::vector<int64_t>{0x4000000000000001, 0x4000000000000002},
+            std::vector<int64_t>{0x4000000000000001, 0x4000000000000002},
+            "tensor_constant_int64"),
+        // constant_equality_u4_2x2x3
+        ConstantParams({2, 2, 3}, element::Type_t::u4, element::Type_t::u4,
+            std::vector<char>{0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xF, 0xF},
+            std::vector<char>{0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xF, 0xF},
+            "constant_equality_u4_2x2x3"),
+        // constant_equality_u4_1x3
+        ConstantParams({1, 3}, element::Type_t::u4, element::Type_t::u4,
+            std::vector<char>{0x1, 0x2, 0x3},
+            std::vector<char>{0x1, 0x2, 0x3},
+            "constant_equality_u4_1x3"),
+        // constant_equality_u1_1x10
+        ConstantParams({1, 10}, element::Type_t::u1, element::Type_t::u1,
+            std::vector<char>{0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0x1, 0x0, 0x0, 0x0},
+            std::vector<char>{0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0x1, 0x0, 0x0, 0x0},
+            "constant_equality_u1_1x10"),
+        // constant_equality_i4_2x2x3
+        ConstantParams({2, 2, 3}, element::Type_t::i4, element::Type_t::i4,
+            std::vector<char>{0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xF, 0xF},
+            std::vector<char>{0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xF, 0xF},
+            "constant_equality_i4_2x2x3"),
+        // constant_equality_i4_1x3
+        ConstantParams({1, 3}, element::Type_t::i4, element::Type_t::i4,
+            std::vector<char>{0x1, 0x2, 0x3},
+            std::vector<char>{0x1, 0x2, 0x3},
+            "constant_equality_i4_1x3"),
+    };
+    return constantParams;
 }
 
 std::vector<ConstantParams> generateConstantCombinedParams() {
-    const std::vector<ConstantParams> constantTypeParams {
-        generateConstantParams<element::Type_t::f32, element::Type_t::f32>(
-            {2, 2, 2},
-            {1, 2, 3, 4, 5, 6, 7, 8},
-            {1, 2, 3, 4, 5, 6, 7, 8},
-            "tensor_constant"),
-        generateConstantParams<element::Type_t::f32, element::Type_t::f32>(
-            {2, 2, 2},
-            {1, 2, 3, 4, 5, 6, 7, 8},
-            {1, 2, 3, 4, 5, 6, 7, 8},
-            "tensor_2constant"),
-        generateConstantParams<element::Type_t::f32, element::Type_t::f32>(
-            {2, 2, 2},
-            {-1, 2, 3, -4, 5, -6, -7, 8},
-            {1, 2, 3, 4, 5, 6, 7, 8},
-            "tensor_constant_with_op"),
-        generateConstantParams<element::Type_t::i32, element::Type_t::i32>(
-            {},
-            {388},
-            {388},
-            "constant_multi_use"),
-        generateConstantParams<element::Type_t::f32, element::Type_t::f32>(
-            {},
-            {4.75},
-            {4.75f},
-            "scalar_constant_float32"),
-        generateConstantParams<element::Type_t::i64, element::Type_t::i64>(
-            {},
-            {0x4000000000000001},
-            {0x4000000000000001},
-            "scalar_constant_int64"),
-        generateConstantParams<element::Type_t::f32, element::Type_t::f32>(
-            {2, 2},
-            {4.75, 4.5, -5.25, 0.0},
-            {4.75f, 4.5f, -5.25f, 0.0f},
-            "tensor_constant_float32"),
-        generateConstantParams<element::Type_t::i64, element::Type_t::i64>(
-            {2},
-            {0x4000000000000001, 0x4000000000000002},
-            {0x4000000000000001, 0x4000000000000002},
-            "tensor_constant_int64"),
-        generateConstantParams<element::Type_t::boolean, element::Type_t::boolean>(
-            {4},
-            {true, false, true, false},
-            {true, false, true, false},
-            "constant_equality_bool"),
-        generateConstantParams<element::Type_t::u4, element::Type_t::u4>(
-            {2, 2, 3},
-            {0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xF, 0xF},
-            {0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xF, 0xF},
-            "constant_equality_u4_2x2x3"),
-        generateConstantParams<element::Type_t::u4, element::Type_t::u4>(
-            {1, 3},
-            {0x1, 0x2, 0x3},
-            {0x1, 0x2, 0x3},
-            "constant_equality_u4_1x3"),
-        generateConstantParams<element::Type_t::u1, element::Type_t::u1>(
-            {1, 10},
-            {0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0x1, 0x0, 0x0, 0x0},
-            {0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0x1, 0x0, 0x0, 0x0},
-            "constant_equality_u1_1x10"),
-        generateConstantParams<element::Type_t::i4, element::Type_t::i4>(
-            {2, 2, 3},
-            {0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xF, 0xF},
-            {0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xF, 0xF},
-            "constant_equality_i4_2x2x3"),
-        generateConstantParams<element::Type_t::i4, element::Type_t::i4>(
-            {1, 3},
-            {0x1, 0x2, 0x3},
-            {0x1, 0x2, 0x3},
-            "constant_equality_i4_1x3"),
+    const std::vector<std::vector<ConstantParams>> constantTypeParams {
+        generateConstantParams<element::Type_t::i8>(),
+        generateConstantParams<element::Type_t::i16>(),
+        generateConstantParams<element::Type_t::i32>(),
+        generateConstantParams<element::Type_t::i64>(),
+        generateConstantParams<element::Type_t::u8>(),
+        generateConstantParams<element::Type_t::u16>(),
+        generateConstantParams<element::Type_t::u32>(),
+        generateConstantParams<element::Type_t::u64>(),
+        generateConstantParams<element::Type_t::f16>(),
+        generateConstantParams<element::Type_t::f32>(),
+        generateConstantDefinedTypeParams(),
     };
-    return constantTypeParams;
+    std::vector<ConstantParams> combinedParams;
+
+    for (const auto& params : constantTypeParams) {
+        combinedParams.insert(combinedParams.end(), params.begin(), params.end());
+    }
+    return combinedParams;
+}
+
+std::vector<ConstantParams> generateConstant2ConstantCombinedParams() {
+    const std::vector<std::vector<ConstantParams>> constantTypeParams {
+        generateConstantParams<element::Type_t::i8>(),
+        generateConstantParams<element::Type_t::i16>(),
+        generateConstantParams<element::Type_t::i32>(),
+        generateConstantParams<element::Type_t::i64>(),
+        generateConstantParams<element::Type_t::u8>(),
+        generateConstantParams<element::Type_t::u16>(),
+        generateConstantParams<element::Type_t::u32>(),
+        generateConstantParams<element::Type_t::u64>(),
+        generateConstantParams<element::Type_t::f16>(),
+        generateConstantParams<element::Type_t::f32>(),
+    };
+    std::vector<ConstantParams> combinedParams;
+
+    for (const auto& params : constantTypeParams) {
+        combinedParams.insert(combinedParams.end(), params.begin(), params.end());
+    }
+    return combinedParams;
+}
+
+template <element::Type_t IN_ET>
+std::vector<ConstantParams> generateConstantWithOpParams() {
+    using T = typename element_type_traits<IN_ET>::value_type;
+    std::vector<ConstantParams> constantParams {
+        // tensor_constant_with_op
+        ConstantParams({2, 2, 2}, IN_ET, IN_ET,
+                       std::vector<T>{-1, 2, 3, -4, 5, -6, -7, 8},
+                       std::vector<T>{1, 2, 3, 4, 5, 6, 7, 8},
+                       "tensor_constant_with_op"),
+    };
+    return constantParams;
+}
+
+std::vector<ConstantParams> generateConstantWithOpCombinedParams() {
+    const std::vector<std::vector<ConstantParams>> constantTypeParams {
+        generateConstantWithOpParams<element::Type_t::i8>(),
+        generateConstantWithOpParams<element::Type_t::i16>(),
+        generateConstantWithOpParams<element::Type_t::i32>(),
+        generateConstantWithOpParams<element::Type_t::i64>(),
+        generateConstantWithOpParams<element::Type_t::f16>(),
+        generateConstantWithOpParams<element::Type_t::f32>(),
+    };
+    std::vector<ConstantParams> combinedParams;
+
+    for (const auto& params : constantTypeParams) {
+        combinedParams.insert(combinedParams.end(), params.begin(), params.end());
+    }
+    return combinedParams;
+}
+
+std::vector<ConstantParams> generateConstantDefinedTypeMultiUseCombinedParams() {
+    const std::vector<ConstantParams> combinedParams {
+        // constant_multi_use
+        ConstantParams({}, element::Type_t::i32, element::Type_t::i32,
+            std::vector<int32_t>{388},
+            std::vector<int32_t>{388},
+            "constant_multi_use"),
+    };
+    return combinedParams;
+}
+
+std::vector<ConstantParams> generateConstantDefinedTypeEqualityBoolCombinedParams() {
+    const std::vector<ConstantParams> combinedParams {
+        // constant_equality_bool
+        ConstantParams({4}, element::Type_t::boolean, element::Type_t::boolean,
+            std::vector<char>{true, false, true, false},
+            std::vector<char>{true, false, true, false},
+            "constant_equality_bool"),
+    };
+    return combinedParams;
 }
 
 INSTANTIATE_TEST_SUITE_P(smoke_Constant_With_Hardcoded_Refs, ReferenceConstantLayerTest,
     testing::ValuesIn(generateConstantCombinedParams()), ReferenceConstantLayerTest::getTestCaseName);
 
+INSTANTIATE_TEST_SUITE_P(smoke_Constant_With_Hardcoded_Refs, ReferenceConstantLayerTest_2Constant,
+    testing::ValuesIn(generateConstant2ConstantCombinedParams()), ReferenceConstantLayerTest::getTestCaseName);
+
+INSTANTIATE_TEST_SUITE_P(smoke_Constant_With_Hardcoded_Refs, ReferenceConstantLayerTest_WithOp,
+    testing::ValuesIn(generateConstantWithOpCombinedParams()), ReferenceConstantLayerTest::getTestCaseName);
+
+INSTANTIATE_TEST_SUITE_P(smoke_Constant_With_Hardcoded_Refs, ReferenceConstantLayerTest_MultiUse,
+    testing::ValuesIn(generateConstantDefinedTypeMultiUseCombinedParams()), ReferenceConstantLayerTest::getTestCaseName);
+
+INSTANTIATE_TEST_SUITE_P(smoke_Constant_With_Hardcoded_Refs, ReferenceConstantLayerTest_EqualityBool,
+    testing::ValuesIn(generateConstantDefinedTypeEqualityBoolCombinedParams()), ReferenceConstantLayerTest::getTestCaseName);
 } // namespace
