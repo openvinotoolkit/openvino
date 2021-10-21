e92cc15f16f263f5adff70ba623c277474c8cd42
Zhang Yi3 <yi3.zhang@intel.com>
2021-10-21 10:53:10 +0800

    [ShapeInfer]Impl LSTM

---
+++ inference-engine/tests/unit/cpu/shape_inference_test/lstm_cell_shape_inference.cpp
@@ -0,0 +1,47 @@
+// Copyright (C) 2018-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <gtest/gtest.h>
+
+#include <openvino/core/coordinate_diff.hpp>
+#include <openvino/op/ops.hpp>
+#include <openvino/op/parameter.hpp>
+#include <lstm_cell_shape_inference.hpp>
+
+#include "utils/shape_inference/static_shape.hpp"
+
+using namespace ov;
+
+TEST(StaticShapeInferenceTest, LstmCellTest) {
+    const size_t batch_size = 2;
+    const size_t input_size = 3;
+    const size_t hidden_size = 3;
+    const size_t gates_count = 4;
+
+    const auto X = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{-1, -1});
+    const auto W = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{-1, -1});
+    const auto R = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{-1, -1});
+    const auto H_t = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{-1, -1});
+    const auto C_t = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{-1, -1});
+    const auto Bias = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{-1});
+    const auto lstm_cell = std::make_shared<op::v4::LSTMCell>(X, H_t, C_t, W, R, Bias, hidden_size);
+    std::vector<PartialShape> input_shapes = {PartialShape{batch_size, input_size},
+                                              PartialShape{batch_size, hidden_size},
+                                              PartialShape{batch_size, hidden_size},
+                                              PartialShape{gates_count * hidden_size, input_size},
+                                              PartialShape{gates_count * hidden_size, hidden_size},
+                                              PartialShape{gates_count * hidden_size}},
+                              output_shapes = {PartialShape{}, PartialShape{}};
+    shape_infer(lstm_cell.get(), input_shapes, output_shapes);
+    ASSERT_EQ(output_shapes[0], PartialShape({batch_size, hidden_size}));
+    ASSERT_EQ(output_shapes[1], PartialShape({batch_size, hidden_size}));
+    std::vector<StaticShape> static_input_shapes = {StaticShape{batch_size, input_size},
+                                                    StaticShape{batch_size, hidden_size},
+                                                    StaticShape{batch_size, hidden_size},
+                                                    StaticShape{gates_count * hidden_size, input_size},
+                                                    StaticShape{gates_count * hidden_size, hidden_size},
+                                                    StaticShape{gates_count * hidden_size}},
+                             static_output_shapes = {StaticShape{}, StaticShape{}};
+    shape_infer(lstm_cell.get(), static_input_shapes, static_output_shapes);
+}
---
+++ ngraph/core/shape_inference/include/lstm_cell_shape_inference.hpp
@@ -0,0 +1,158 @@
+// Copyright (C) 2018-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+#pragma once
+#include <rnn_cell_shape_inference.hpp>
+
+namespace ov {
+namespace op {
+namespace ShapeInferLSTM {
+template <class T1, class T2>
+void inline default_work(T1* op, T2& shape) {
+    NODE_VALIDATION_CHECK(op, false, "[LSTM]Can not infer shape based on input static shape");
+}
+
+template <class T1>
+void inline default_work(T1* op, PartialShape& shape) {
+    shape = PartialShape::dynamic();
+}
+
+template <class T1, class T2>
+void lstm_shape_infer(T1* op,
+                      const std::vector<T2>& input_shapes,
+                      std::vector<T2>& output_shapes,
+                      std::size_t gates_count) {
+    using DimType = typename std::iterator_traits<typename T2::iterator>::value_type;
+    // If rank is dynamic, then output_shape is undefined
+    for (const auto& input : input_shapes) {
+        if (input.rank().is_dynamic()) {
+            default_work(op, output_shapes[0]);
+            default_work(op, output_shapes[1]);
+            return;
+        }
+    }
+
+    const auto& x_pshape = input_shapes[0];
+    const auto& ht_pshape = input_shapes[1];
+    const auto& ct_pshape = input_shapes[2];
+    const auto& w_pshape = input_shapes[3];
+    const auto& r_pshape = input_shapes[4];
+    const auto& b_pshape = input_shapes[5];
+
+    // Prepare OutShape
+    auto& hidden_shape = output_shapes[0];
+    auto& cell_shape = output_shapes[1];
+    hidden_shape.resize(2);
+    cell_shape.resize(2);
+
+    // Check rnn common input
+    util::validate_input_rank(dynamic_cast<util::RNNCellBase*>(op),
+                              std::vector<T2>{x_pshape, ht_pshape, w_pshape, r_pshape, b_pshape});
+
+    // Check cell
+    NODE_VALIDATION_CHECK(op,
+                          (ct_pshape.rank().is_static()),
+                          "LSTMCell input tensor initial_cell_state shall have static rank.");
+    NODE_VALIDATION_CHECK(op,
+                          (ct_pshape.rank().get_length() == 2),
+                          "LSTMCell input tensor initial_cell_state shall have dimension 2D.");
+    // Check peepholes
+    if (input_shapes.size() == 7) {
+        const auto& p_pshape = input_shapes[6];
+        NODE_VALIDATION_CHECK(op, (p_pshape.rank().is_static()), "LSTMCell input tensor P shall have static rank.");
+        NODE_VALIDATION_CHECK(op,
+                              (p_pshape.rank().get_length() == 1),
+                              "LSTMCell input tensor P shall have dimension 1D.");
+    }
+
+    auto merged_batch_size = ht_pshape[0];
+    auto merged_hidden_size = ht_pshape[1];
+
+    // Merge batch_size dimension across all inputs to evaluate output[0] dimension
+    NODE_VALIDATION_CHECK(op,
+                          DimType::merge(merged_batch_size, merged_batch_size, ht_pshape[0]) &&
+                              DimType::merge(merged_batch_size, merged_batch_size, ct_pshape[0]) &&
+                              DimType::merge(merged_batch_size, merged_batch_size, x_pshape[0]),
+                          "Parameter batch_size not matched for X, initial_hidden_state or initial_cell_state "
+                          "inputs.");
+
+    // Merge hidden_size dimension across all inputs to evaluate output[1] dimension
+    NODE_VALIDATION_CHECK(op,
+                          DimType::merge(merged_hidden_size, merged_hidden_size, ht_pshape[1]) &&
+                              DimType::merge(merged_hidden_size, merged_hidden_size, ct_pshape[1]) &&
+                              DimType::merge(merged_hidden_size, merged_hidden_size, r_pshape[1]),
+                          "Parameter hidden_size not matched for R, initial_hidden_state and initial_cell_state "
+                          "inputs.");
+
+    // Validate hidden_size value for W, R and P inputs
+    if (merged_hidden_size.is_static()) {
+        if (w_pshape[0].is_static()) {
+            NODE_VALIDATION_CHECK(op,
+                                  w_pshape[0].compatible(merged_hidden_size * gates_count),
+                                  "Parameter hidden_size mistmatched in W input. Current value is: ",
+                                  w_pshape[0].get_length(),
+                                  ", expected: ",
+                                  merged_hidden_size.get_length() * gates_count,
+                                  ".");
+        }
+
+        if (r_pshape[0].is_static()) {
+            NODE_VALIDATION_CHECK(op,
+                                  r_pshape[0].compatible(merged_hidden_size * gates_count),
+                                  "Parameter hidden_size mistmatched in R input. Current value is: ",
+                                  r_pshape[0].get_length(),
+                                  ", expected: ",
+                                  merged_hidden_size.get_length() * gates_count,
+                                  ".");
+        }
+
+        if (b_pshape[0].is_static()) {
+            NODE_VALIDATION_CHECK(op,
+                                  b_pshape[0].compatible(merged_hidden_size * gates_count),
+                                  "Parameter hidden_size mistmatched in B input. Current value is: ",
+                                  b_pshape[0].get_length(),
+                                  ", expected: ",
+                                  merged_hidden_size.get_length() * gates_count,
+                                  ".");
+        }
+    }
+
+    hidden_shape[0] = merged_batch_size;
+    hidden_shape[1] = merged_hidden_size;
+    cell_shape[0] = merged_batch_size;
+    cell_shape[1] = merged_hidden_size;
+}
+
+}  // namespace ShapeInferLSTM
+
+namespace v0 {
+using ShapeInferLSTM::lstm_shape_infer;
+template <class T>
+void shape_infer(LSTMCell* op, const std::vector<T>& input_shapes, std::vector<T>& output_shapes) {
+    NODE_VALIDATION_CHECK(op, input_shapes.size() == 7 && output_shapes.size() == 2);
+    const auto& p_pshape = input_shapes[6];
+
+    lstm_shape_infer(op, input_shapes, output_shapes, op->s_gates_count);
+    const auto& hidden_size = output_shapes[0][1];
+    if (p_pshape[0].is_static() && output_shapes[0][0].is_static()) {
+        NODE_VALIDATION_CHECK(op,
+                              p_pshape[0].compatible(hidden_size * op->s_peepholes_count),
+                              "Parameter hidden_size mistmatched in P input. Current value is: ",
+                              p_pshape[0].get_length(),
+                              ", expected: ",
+                              hidden_size.get_length() * op->s_peepholes_count,
+                              ".");
+    }
+}
+}  // namespace v0
+
+namespace v4 {
+using ShapeInferLSTM::lstm_shape_infer;
+template <class T>
+void shape_infer(LSTMCell* op, const std::vector<T>& input_shapes, std::vector<T>& output_shapes) {
+    NODE_VALIDATION_CHECK(op, input_shapes.size() == 6 && output_shapes.size() == 2);
+    lstm_shape_infer(op, input_shapes, output_shapes, op->s_gates_count);
+}
+}  // namespace v4
+}  // namespace op
+}  // namespace ov
