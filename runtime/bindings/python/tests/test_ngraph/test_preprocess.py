73417e823eef36481636a7a3c3ab8d2c9211c7ee
pszmel <piotr.szmelczynski@intel.com>
2021-10-22 14:18:34 +0200

    update test

--- runtime/bindings/python/tests/test_ngraph/test_preprocess.py
+++ runtime/bindings/python/tests/test_ngraph/test_preprocess.py
@@ -35,12 +35,12 @@ def test_ngraph_preprocess_mean():
 
 def test_ngraph_preprocess_mean_scale_convert():
     shape = [2, 2]
-    param1 = ops.parameter(shape, dtype=np.float32, name="A")
-    param2 = ops.parameter(shape, dtype=np.float32, name="B")
+    param1 = ops.parameter(shape, dtype=np.int32, name="A")
+    param2 = ops.parameter(shape, dtype=np.int32, name="B")
     function = Function([param1, param2], [param1, param2], "TestFunction")
 
     def custom_preprocess(output: Output):
-        return Output(ops.abs(output))
+        return ops.abs(output).outputs()[0]
 
     function = PrePostProcessor() \
         .input(InputInfo(1)
@@ -54,20 +54,20 @@ def test_ngraph_preprocess_mean_scale_convert():
                ) \
         .input(InputInfo(0)
                .preprocess(PreProcessSteps()
+                           .convert_element_type(Type.f32)
                            .mean(1.)
                            .custom(custom_preprocess)
                            )
                ) \
         .build(function)
 
-    input_data1 = np.array([[0, 1], [2, -2]]).astype(np.int32)
-    input_data2 = np.array([[1, 3], [5, 7]]).astype(np.int32)
-    expected_output1 = np.array([[0, 2], [4, 6]]).astype(np.float32)
-    expected_output2 = np.array([[-0.5, 0], [0.5, -1.5]]).astype(np.float32)
+    input_data1 = np.array([[0,1], [2,-2]]).astype(np.int32)
+    input_data2 = np.array([[1,3], [5,7]]).astype(np.int32)
+    expected_output1 = np.array([[1,0], [1,3]]).astype(np.float32)
+    expected_output2 = np.array([[0,1], [2,3]]).astype(np.float32)
 
     runtime = get_runtime()
     computation = runtime.computation(function)
     [output1, output2] = computation(input_data1, input_data2)
-
     assert np.equal(output1, expected_output1).all()
     assert np.equal(output2, expected_output2).all()
