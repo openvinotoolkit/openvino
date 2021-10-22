744a3e64deb1afd749b30ed00b50e0f854310607
Anton Pankratv <anton.pankratov@intel.com>
2021-10-23 01:20:49 +0300

    Added syntetic hetero tests for dynamic shapes (#8050)

--- inference-engine/tests/functional/plugin/shared/include/hetero/synthetic.hpp
+++ inference-engine/tests/functional/plugin/shared/include/hetero/synthetic.hpp
@@ -19,8 +19,10 @@ struct PluginParameter {
 };
 
 struct FunctionParameter {
-    std::unordered_set<std::string>     _majorPluginNodeIds;
-    std::shared_ptr<ngraph::Function>   _function;
+    std::unordered_set<std::string>         _majorPluginNodeIds;
+    std::shared_ptr<ngraph::Function>       _function;
+    bool                                    _dynamic_batch;
+    uint32_t                                _seed;
 };
 
 using HeteroSyntheticTestParameters = std::tuple<
@@ -38,6 +40,14 @@ struct HeteroSyntheticTest : public testing::WithParamInterface<HeteroSyntheticT
     static std::string getTestCaseName(const ::testing::TestParamInfo<HeteroSyntheticTestParameters>& obj);
     static std::vector<FunctionParameter> _singleMajorNodeFunctions;
     static std::vector<FunctionParameter> _randomMajorNodeFunctions;
+    static std::vector<FunctionParameter> singleMajorNodeFunctions(
+        const std::vector<std::function<std::shared_ptr<ngraph::Function>()>>& builders, bool dynamic_batch = false);
+    static std::vector<FunctionParameter> randomMajorNodeFunctions(
+        const std::vector<std::function<std::shared_ptr<ngraph::Function>()>>& builders, bool dynamic_batch = false, uint32_t seed = 0);
+    static std::vector<FunctionParameter> withMajorNodesFunctions(
+        const std::function<std::shared_ptr<ngraph::Function>()>& builder,
+        const std::unordered_set<std::string>& majorNodes,
+        bool dynamic_batch = false);
     std::vector<std::string> _registredPlugins;
 };
 
