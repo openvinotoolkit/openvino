af5f67b549e3d040165cfa9b85263b96a53a9b37
pszmel <piotr.szmelczynski@intel.com>
2021-10-25 14:54:57 +0200

    create function that cast Node to Output<Node>

--- runtime/bindings/python/src/pyopenvino/graph/node_output.cpp
+++ runtime/bindings/python/src/pyopenvino/graph/node_output.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "openvino/core/node_output.hpp"
+#include "openvino/core/node.hpp"
 
 #include <pybind11/stl.h>
 
@@ -75,4 +76,7 @@ void regclass_graph_Output(py::module m) {
                 get_target_inputs : Set[Input]
                     Set of Inputs.
                )");
+    output.def("_cast_to_output", [](const std::shared_ptr<ov::Node>& node) {
+        return ov::Output<ov::Node>(node);
+    });
 }
