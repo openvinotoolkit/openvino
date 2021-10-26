74dc27037f69b23688de2867d6aaf6c9c7eb79e0
Hu, Yuan2 <yuan2.hu@intel.com>
2021-10-26 11:25:54 +0800

    fix CPU performance problem with loading by AutoPlugin
    Signed-off-by: Hu, Yuan2 <yuan2.hu@intel.com>

--- inference-engine/src/multi_device/multi_device_exec_network.cpp
+++ inference-engine/src/multi_device/multi_device_exec_network.cpp
@@ -191,7 +191,7 @@ MultiDeviceExecutableNetwork::MultiDeviceExecutableNetwork(const std::string&   
     _executor = InferenceEngine::ExecutorManager::getInstance()->getIdleCPUStreamsExecutor(
             IStreamsExecutor::Config{"AutoDeviceAsyncLoad",
             static_cast<int>(std::thread::hardware_concurrency()) /* max possible #streams*/,
-            1 /*single thread per stream*/,
+            0 /*single thread per stream*/,
             IStreamsExecutor::ThreadBindingType::NONE});
 
     for (auto& p : needLoadDevices) {
