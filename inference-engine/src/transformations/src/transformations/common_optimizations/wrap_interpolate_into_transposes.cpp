32d3ec078f1d0ccab57b9c0f4df0f1f9eb47dccd
vgavrilo <vladimir.gavrilov@intel.com>
2021-10-26 10:24:57 +0300

    Some fixes.

--- inference-engine/src/transformations/src/transformations/common_optimizations/wrap_interpolate_into_transposes.cpp
+++ inference-engine/src/transformations/src/transformations/common_optimizations/wrap_interpolate_into_transposes.cpp
@@ -24,12 +24,13 @@ bool transformation_is_applicable(const std::shared_ptr<ngraph::opset8::Interpol
     int64_t input_rank = interpolate->get_input_partial_shape(0).rank().get_length();
     if (input_rank < 4) return false;
 
-    if (!std::dynamic_pointer_cast<ngraph::opset8::Constant>(interpolate->input_value(3))) return false;
+    auto axes_node = std::dynamic_pointer_cast<ngraph::opset8::Constant>(interpolate->input_value(3).get_node_shared_ptr());
+    if (!axes_node) return false;
 
-    const auto axes = interpolate->get_axes();
+    const auto axes = axes_node->cast_vector<int64_t>();
     if (static_cast<int64_t>(axes.size()) > input_rank - 2) return false;
 
-    return std::any_of(axes.begin(), axes.end(), [](int64_t axis){ return axis != 0 && axis != 1});
+    return std::any_of(axes.begin(), axes.end(), [](int64_t axis){ return axis != 0 && axis != 1; });
 }
 
 std::vector<int64_t> reverse_permutation(const std::vector<int64_t>& perm) {
@@ -37,14 +38,13 @@ std::vector<int64_t> reverse_permutation(const std::vector<int64_t>& perm) {
 
     std::vector<int64_t> result(perm.size());
     for (int64_t i = 0; i < static_cast<int64_t>(perm.size()); ++i) {
-        result[perm[i]] = i
+        result[perm[i]] = i;
     }
 
     return result;
 }
 
-std::vector<int64_t> new_non_interpolated_axes(const std::vector<int64_t>& axes, int64_t input_rank)
-{
+std::vector<int64_t> new_non_interpolated_axes(const std::vector<int64_t>& axes, int64_t input_rank) {
     std::vector<int64_t> result {0, 1};
     for (int64_t i = 2 + static_cast<int64_t>(axes.size()); i < input_rank; ++i) {
         result.push_back(i);
@@ -77,7 +77,7 @@ std::vector<int64_t> build_transposition_for_axes(const std::vector<int64_t>& ax
 
     int64_t idx = 2;
     for (auto axis : old_interpolated_axes_set) {
-        result[a] = idx++;
+        result[axis] = idx++;
     }
 
     for (uint64_t i = 0; i < static_cast<uint64_t>(new_non_interpolated_axes_vector.size()); ++i) {
@@ -92,7 +92,7 @@ std::shared_ptr<ngraph::opset8::Constant> build_new_axes_node(const std::vector<
     for (uint64_t i = 0; i < static_cast<uint64_t>(axes.size()); ++i) {
         value[i] = perm[axes[i]];
     }
-    return opset8::Constant::create(element::i64, {axes.size()}, value);
+    return ngraph::opset8::Constant::create(ngraph::element::i64, {axes.size()}, value);
 }
 } // namespace
 
@@ -105,18 +105,23 @@ ngraph::pass::WrapInterpolateIntoTransposes::WrapInterpolateIntoTransposes() {
         auto interpolate = std::dynamic_pointer_cast<opset8::Interpolate>(m.get_match_root());
         if (!interpolate || !transformation_is_applicable(interpolate)) return false;
 
-        const auto axes = interpolate->get_axes();
+        auto axes_node = std::dynamic_pointer_cast<ngraph::opset8::Constant>(interpolate->input_value(3).get_node_shared_ptr());
+        if (!axes_node) return false;
+
+        const auto axes = axes_node->cast_vector<int64_t>();
         const int64_t input_rank = interpolate->get_input_partial_shape(0).rank().get_length();
         const auto first_perm = build_transposition_for_axes(axes, input_rank);
         const auto last_perm = reverse_permutation(first_perm);
 
-        auto first_transpose = std::make_shared<opset8::Transpose>(interpolate->input_value(0), first_perm);
+        auto first_transpose_perm = opset8::Constant::create(element::i64, {first_perm.size()}, first_perm);
+        auto first_transpose = std::make_shared<opset8::Transpose>(interpolate->input_value(0), first_transpose_perm);
         auto new_interpolate = std::make_shared<opset8::Interpolate>(first_transpose, interpolate->input_value(1), interpolate->input_value(2),
                                                                      build_new_axes_node(axes, first_perm), interpolate->get_attrs());
-        auto last_transpose = std::make_shared<opset8::Transpose>(new_interpolate, last_perm);
+        auto last_transpose_perm = opset8::Constant::create(element::i64, {last_perm.size()}, last_perm);
+        auto last_transpose = std::make_shared<opset8::Transpose>(new_interpolate, last_transpose_perm);
 
         last_transpose->set_friendly_name(interpolate->get_friendly_name());
-        copy_runtime_info({interpolate}, {first_transpose, new_interpolate, last_transpose});
+        copy_runtime_info(interpolate, {first_transpose_perm, first_transpose, new_interpolate, last_transpose_perm, last_transpose});
         replace_node(interpolate, last_transpose);
 
         return true;
