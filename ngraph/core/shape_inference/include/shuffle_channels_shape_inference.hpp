bf0ef0b40ae42ae3e7ee947e4525dea8672368c9
Luwei Zhou <luwei.zhou@intel.com>
2021-10-27 09:22:25 +0800

    Update based on the review comments.

--- ngraph/core/shape_inference/include/shuffle_channels_shape_inference.hpp
+++ ngraph/core/shape_inference/include/shuffle_channels_shape_inference.hpp
@@ -12,6 +12,7 @@ template <class T>
 void shape_infer(const ShuffleChannels* op, const std::vector<T>& input_shapes, std::vector<T>& output_shapes) {
     NODE_VALIDATION_CHECK(op, input_shapes.size() == 1 && output_shapes.size() == 1);
     const auto& input_shape = input_shapes[0];
+    NODE_VALIDATION_CHECK(op, op->m_group >= 1, "The 'group' parameter must be greater or equal to 1.");
     if (input_shape.is_static()) {
         auto input_rank = input_shape.size();
         NODE_VALIDATION_CHECK(op, input_rank >= 1, "The input tensor's shape is expected to be at least 1D.");
@@ -22,7 +23,6 @@ void shape_infer(const ShuffleChannels* op, const std::vector<T>& input_shapes, 
                               axis_zb < input_rank,
                               "The 'axis' parameter for ShuffleChannels has to point to one of the "
                               "input tensor's shape dimensions.");
-        NODE_VALIDATION_CHECK(op, op->m_group >= 1, "The 'group' parameter must be greater or equal to 1.");
 
         const auto channel_dim_size = input_shape[axis_zb].get_length();
         NODE_VALIDATION_CHECK(op,
