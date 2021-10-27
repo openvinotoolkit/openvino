e2cc10a01f056967b91b4462a2fbc36100f84876
Andrew Kwangwoong Park <andrew.kwangwoong.park@intel.com>
2021-10-27 17:51:21 +0900

    [GPU] Fix VA OD sample pipeline test failure with batch=64 (#8128)
    * Fix VA OD sample pipeline test failure with batch=64
    
    Signed-off-by: Andrew Kwangwoong Park <andrew.kwangwoong.park@intel.com>
    
    * Update cpu/gpu switching rule for maximum LWS of device capabilities
    
    Signed-off-by: Andrew Kwangwoong Park <andrew.kwangwoong.park@intel.com>
    
    * Add Validate function to compare batch size with device capabilities
    
    Signed-off-by: Andrew Kwangwoong Park <andrew.kwangwoong.park@intel.com>

--- inference-engine/thirdparty/clDNN/src/layout_optimizer.cpp
+++ inference-engine/thirdparty/clDNN/src/layout_optimizer.cpp
@@ -1081,11 +1081,14 @@ impl_types layout_optimizer::get_preferred_impl_type(program_node& node, format 
     if (!_forcing_map.empty() && _forcing_map.count(node.id()) != 0) {
         preferred_impl = _forcing_map.at(node.id()).second;
     } else if (node.is_type<detection_output>()) {
+        const auto& program = node.get_program();
+        const auto& device_info = program.get_engine().get_device_info();
+        const size_t lws_max = device_info.max_work_group_size;
         auto& detection_output_node = node.as<detection_output>();
         auto confidence_layout = detection_output_node.confidence().get_output_layout();
         auto prim = detection_output_node.get_primitive();
-        if (confidence_layout.size.batch[0] >= 4 && prim->confidence_threshold >= 0.1 && prim->top_k <= 400 &&
-            prim->num_classes >= 16 && confidence_layout.size.feature[0] > 10000)
+        if (confidence_layout.size.batch[0] <= lws_max && confidence_layout.size.batch[0] >= 4 && prim->confidence_threshold >= 0.1 &&
+            prim->top_k <= 400 && prim->num_classes >= 16 && confidence_layout.size.feature[0] > 10000)
             preferred_impl = impl_types::ocl;
         else
             preferred_impl = impl_types::cpu;
