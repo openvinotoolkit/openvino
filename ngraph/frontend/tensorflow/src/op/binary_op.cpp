6bb2bd27e7a4854af4e5c02e24504f0b86552364
Ivan Tikhonov <ivan.tikhonov@intel.com>
2021-10-29 00:11:01 +0300

    Delete redandant comments

--- ngraph/frontend/tensorflow/src/op/binary_op.cpp
+++ ngraph/frontend/tensorflow/src/op/binary_op.cpp
@@ -8,34 +8,11 @@
 using namespace std;
 using namespace ov::opset8;
 
-// Helper function to translate a binary op
-// Parameters:
-//
-//    TFNodeDecoder* op               - TF op being translated. Must have only two
-//    inputs.
-//    const std::vector<const ov::frontend::tf::detail::TensorWrapper*>& static_input_map - the static input
-//    map Builder::OpMap& ng_op_map  - The TF-to-nGraph op map. std::function<Output<Node>(Output<Node>,
-//    Output<Node>)>
-//    create_binary_op           - Function to construct the graph implementing
-//                                 the binary op, given the 2 ng_inputs to the
-//                                 binaryop
-// Example Usage:
-//
-// if (op->type_string() == "SquaredDifference") {
-//      TF_RETURN_IF_ERROR(TranslateBinaryOp(op, ng_op_map,
-//         [](Output<Node> ng_input1, Output<Node>
-//         ng_input2) {
-//           auto ng_diff = Output<Subtract>(input1,
-//           input2);
-//           return Output<Multiply>(ng_diff,ng_diff);
-//         }));
-//    }
-//
-
 namespace ov {
 namespace frontend {
 namespace tf {
 namespace op {
+
 OutputVector TranslateBinaryOp(const NodeContext& node,
                                const std::function<Output<Node>(Output<Node>&, Output<Node>&)>& create_binary_op) {
     auto ng_lhs = node.get_ng_input(0);
@@ -51,18 +28,6 @@ OutputVector TranslateFloorDivOp(const NodeContext& node) {
     };
     return TranslateBinaryOp(node, floordiv_fn);
 }
-
-// Helper function to translate a binary op in cases where there is a one-to-one
-// mapping from TensorFlow ops to nGraph ops.
-//
-// Example usage:
-//
-//  if (n->type_string == "Add") {
-//    TF_RETURN_IF_ERROR(TranslateBinaryOp<Add>(op,
-//    static_input_map,
-//    ng_op_map));
-//  }
-//
 
 template <typename T>
 OutputVector TranslateBinaryOp(const NodeContext& node) {
--- ngraph/frontend/tensorflow/src/op/unary_op.cpp
+++ ngraph/frontend/tensorflow/src/op/unary_op.cpp
@@ -13,29 +13,6 @@ namespace frontend {
 namespace tf {
 namespace op {
 
-// Helper function to translate a unary op.
-//
-// Parameters:
-//
-//    TFNodeDecoder* op                   - TF op being translated. Must have one input.
-//    const std::vector<const ov::frontend::tf::detail::TensorWrapper*>& static_input_map
-//                               - the static input map
-//    Builder::OpMap& ng_op_map  - The TF-to-nGraph op map.
-//
-//    std::function<Output<Node>(Output<Node>>
-//      create_unary_op           - Function to construct the graph implementing
-//                                 the unary op, given the input to the unop
-//                                 as an argument.
-//
-// Example usage:
-//
-//  if (n->type_string == "Square") {
-//    TF_RETURN_IF_ERROR(TranslateUnaryOp(n, static_input_map, ng_op_map,
-//                       [] (Output<Node> n) {
-//                           return
-//                           (Output<Multiply>(n,n));
-//                       });
-//  }
 OutputVector TranslateUnaryOp(const NodeContext& op,
                               const std::function<shared_ptr<Node>(Output<Node>)>& create_unary_op) {
     auto ng_input = op.get_ng_input(0);
@@ -44,16 +21,6 @@ OutputVector TranslateUnaryOp(const NodeContext& op,
     return {res};
 }
 
-// Helper function to translate a unary op in cases where there is a one-to-one
-// mapping from TensorFlow ops to nGraph ops.
-//
-// Example usage:
-//
-//  if (n->type_string == "Abs") {
-//    TF_RETURN_IF_ERROR(TranslateUnaryOp<op::Abs>(n, static_input_map,
-//    ng_op_map));
-//  }
-//
 template <typename T>
 OutputVector TranslateUnaryOp(const NodeContext& node) {
     return TranslateUnaryOp(node, [](Output<Node> n) {
