582b7832682364051fd0422d02362613fdc54578
Alexey Lebedev <alexey.lebedev@intel.com>
2021-10-28 15:19:05 +0300

    try to fix py code style

--- runtime/bindings/python/src/openvino/helpers/packing.py
+++ runtime/bindings/python/src/openvino/helpers/packing.py
@@ -5,24 +5,35 @@ import numpy as np
 from typing import Union
 from openvino.impl import Type, Shape
 
-def pack_data(array: np.ndarray, ov_type: Type) -> np.ndarray:
+
+def pack_data(array: np.ndarray, type: Type) -> np.ndarray:
     """
     Represents array values as u1,u4 or i4 openvino element type and packs them into uint8 numpy array.
-    If the number of elements in array is odd we pad them with zero value to be able to fit the bit sequence into the uint8 array.
-    Example: two uint8 values - [7, 8] can be represented as uint4 values and be packed into one int8 value - [120],
-             because [7, 8] bit representation is [0111, 1000] will be viewed as [01111000], which is bit representation of [120]
-    """
-    assert ov_type in [Type.u1, Type.u4, Type.i4]
 
-    minimum_regular_dtype = np.int8 if ov_type == Type.i4 else np.uint8
+    If the number of elements in array is odd we pad them with zero value to be able to fit the bit
+    sequence into the uint8 array.
+
+    Example: two uint8 values - [7, 8] can be represented as uint4 values and be packed into one int8
+             value - [120], because [7, 8] bit representation is [0111, 1000] will be viewed
+             as [01111000], which is bit representation of [120].
+
+    @param array: numpy array with values to pack.
+    @param type: openvino.element.Type to interpret the array values. Type must be be u1, u4 or i4.
+    """
+    assert type in [Type.u1, Type.u4, Type.i4], "Packing algorithm for the" \
+                                                "data types stored in 1, 2 or 4 bits"
+
+    minimum_regular_dtype = np.int8 if type == Type.i4 else np.uint8
     casted_to_regular_type = array.astype(dtype=minimum_regular_dtype, casting="unsafe")
     if not np.array_equal(casted_to_regular_type, array):
-        raise RuntimeError(f'The conversion of array "{array}" to dtype "{casted_to_regular_type}" results in rounding')
+        raise RuntimeError(f'The conversion of array "{array}" to dtype'
+                           f' "{casted_to_regular_type}" results in rounding')
 
     data_size = casted_to_regular_type.size
-    num_bits = ov_type.bitwidth
+    num_bits = type.bitwidth
 
-    assert num_bits < 8 and 8 % num_bits == 0, "Packing algorithm for the data types stored in 1, 2 or 4 bits"
+    assert num_bits < 8 and 8 % num_bits == 0, "Packing algorithm for the" \
+                                               "data types stored in 1, 2 or 4 bits"
     num_values_fitting_into_uint8 = 8 // num_bits
     pad = (-data_size) % num_values_fitting_into_uint8
 
@@ -37,22 +48,30 @@ def pack_data(array: np.ndarray, ov_type: Type) -> np.ndarray:
     return np.packbits(bit_order_big_flattened)
 
 
-def unpack_data(array: np.ndarray, ov_type: Type, shape: Union[list, Shape]) -> np.ndarray:
+def unpack_data(array: np.ndarray, type: Type, shape: Union[list, Shape]) -> np.ndarray:
     """
     Extracts openvino element type values from array bit representation
-    and saves them into uint8/int8 numpy array given shape.
-    Example: uint8 value [120] can be represented as two u4 values and be unpacked into [7, 8] because [120] bit
-             representation is [01111000] will be viewed as [0111, 1000], which is bit representation of [7, 8]
+                                    and saves them into uint8/int8 numpy array given shape.
+
+    Example: uint8 value [120] can be represented as two u4 values and be unpacked into [7, 8]
+             because [120] bit representation is [01111000] will be viewed as [0111, 1000],
+             which is bit representation of [7, 8].
+
+    @param array: numpy array to unpack.
+    @param type: openvino.element.Type to extract from array values. Type must be be u1, u4 or i4.
+    @param shape: the new shape for the unpacked array.
     """
+    assert type in [Type.u1, Type.u4, Type.i4], "Unpacking algorithm for the" \
+                                                "data types stored in 1, 2 or 4 bits"
     unpacked = np.unpackbits(array.view(np.uint8))
     shape = list(shape)
-    if ov_type.bitwidth == 1:
+    if type.bitwidth == 1:
         return np.resize(unpacked, shape)
     else:
-        unpacked = unpacked.reshape(-1, ov_type.bitwidth)
-        padding_shape = (unpacked.shape[0], 8 - ov_type.bitwidth)
+        unpacked = unpacked.reshape(-1, type.bitwidth)
+        padding_shape = (unpacked.shape[0], 8 - type.bitwidth)
         padding = np.ndarray(padding_shape, np.uint8)
-        if ov_type == Type.i4:
+        if type == Type.i4:
             for axis, bits in enumerate(unpacked):
                 if bits[0] == 1:
                     padding[axis] = np.ones((padding_shape[1],), np.uint8)
@@ -62,7 +81,7 @@ def unpack_data(array: np.ndarray, ov_type: Type, shape: Union[list, Shape]) -> 
             padding = np.zeros(padding_shape, np.uint8)
         padded = np.concatenate((padding, unpacked), 1)
         packed = np.packbits(padded, 1)
-        if ov_type == Type.i4:
+        if type == Type.i4:
             return np.resize(packed, shape).astype(dtype=np.int8)
         else:
             return np.resize(packed, shape)
