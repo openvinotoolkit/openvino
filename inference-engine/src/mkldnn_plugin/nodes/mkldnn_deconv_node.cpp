6416b73855053581dec347f15d2ceba658ced8c9
Maxim Andronov <maxim.andronov@intel.com>
2021-10-28 10:52:14 +0300

    [CPU] MVN, FQ, Convert dynamic nodes (#7817)

--- inference-engine/src/mkldnn_plugin/nodes/mkldnn_deconv_node.cpp
+++ inference-engine/src/mkldnn_plugin/nodes/mkldnn_deconv_node.cpp
@@ -18,6 +18,7 @@
 #include <nodes/common/cpu_memcpy.h>
 #include <memory_desc/cpu_memory_desc_utils.h>
 #include "memory_desc/dnnl_blocked_memory_desc.h"
+#include "utils/cpu_utils.hpp"
 
 using namespace mkldnn;
 using namespace MKLDNNPlugin;
@@ -273,7 +274,9 @@ void MKLDNNDeconvolutionNode::setPostOps(mkldnn::primitive_attr &attr) {
     for (auto &node : fusedWith) {
         auto* eltwiseNode = dynamic_cast<MKLDNNEltwiseNode *>(node.get());
         if (eltwiseNode) {
-            eltwiseNode->appendPostOps(ops);
+            // TODO [DS]: change to shape from memory
+            constexpr int align = 16;
+            eltwiseNode->appendPostOps(ops, getOutputShapeAtPort(0).getStaticDims(), align);
             continue;
         }
         auto* fakeQuantizeNode = dynamic_cast<MKLDNNFakeQuantizeNode *>(node.get());
