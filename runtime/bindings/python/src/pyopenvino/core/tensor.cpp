547094379b699613ff274c150546d219f69520a9
Alexey Lebedev <alexey.lebedev@intel.com>
2021-10-28 11:45:30 +0300

    Fix code style

--- runtime/bindings/python/src/pyopenvino/core/tensor.cpp
+++ runtime/bindings/python/src/pyopenvino/core/tensor.cpp
@@ -22,21 +22,19 @@ void regclass_Tensor(py::module m) {
         }),
         py::arg("array"));
 
-    cls.def(
-        py::init([](py::array& array, const ov::element::Type& ov_type, const ov::Shape& shape) {
-            return ov::runtime::Tensor(ov_type, shape, (void*)array.data());
-        }),
-        py::arg("array"),
-        py::arg("type"),
-        py::arg("shape"));
+    cls.def(py::init([](py::array& array, const ov::element::Type& ov_type, const ov::Shape& shape) {
+                return ov::runtime::Tensor(ov_type, shape, (void*)array.data());
+            }),
+            py::arg("array"),
+            py::arg("type"),
+            py::arg("shape"));
 
-    cls.def(
-        py::init([](py::array& array, const ov::element::Type& ov_type, const std::vector<size_t>& shape) {
-            return ov::runtime::Tensor(ov_type, shape, (void*)array.data());
-        }),
-        py::arg("array"),
-        py::arg("type"),
-        py::arg("shape"));
+    cls.def(py::init([](py::array& array, const ov::element::Type& ov_type, const std::vector<size_t>& shape) {
+                return ov::runtime::Tensor(ov_type, shape, (void*)array.data());
+            }),
+            py::arg("array"),
+            py::arg("type"),
+            py::arg("shape"));
 
     cls.def(py::init<const ov::element::Type, const ov::Shape>(), py::arg("type"), py::arg("shape"));
 
@@ -85,7 +83,7 @@ void regclass_Tensor(py::module m) {
     cls.def_property_readonly("byte_size", &ov::runtime::Tensor::get_byte_size);
 
     cls.def_property_readonly("data", [](ov::runtime::Tensor& self) {
-        if(self.get_element_type().bitwidth() < 8) {
+        if (self.get_element_type().bitwidth() < 8) {
             return py::array(Common::ov_type_to_dtype.at(self.get_element_type()),
                              self.get_byte_size(),
                              self.data(),
