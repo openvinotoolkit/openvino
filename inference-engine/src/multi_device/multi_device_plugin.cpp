5f33f3389520901a69b4484f5a2babcd86037179
Hu, Yuan2 <yuan2.hu@intel.com>
2021-10-28 16:00:58 +0800

    fix set config error
    Signed-off-by: Hu, Yuan2 <yuan2.hu@intel.com>

--- inference-engine/src/multi_device/multi_device_plugin.cpp
+++ inference-engine/src/multi_device/multi_device_plugin.cpp
@@ -165,6 +165,7 @@ void MultiDeviceInferencePlugin::SetConfig(const std::map<std::string, std::stri
     bool needPerfCounters = false;
     std::map<std::string, std::string> filterConfig;
     CheckConfig(config, needPerfCounters, filterConfig);
+
     for (auto && kvp : config) {
         const auto& name = kvp.first;
         _config[name] = kvp.second;
@@ -530,7 +531,6 @@ void MultiDeviceInferencePlugin::CheckConfig(const std::map<std::string, std::st
     const auto perf_hints_configs = PerfHintsConfig::SupportedKeys();
     for (auto&& kvp : config) {
         if (kvp.first.find("AUTO_") == 0) {
-            continue;
         } else if (kvp.first == PluginConfigParams::KEY_PERF_COUNT) {
             if (kvp.second == PluginConfigParams::YES) {
                 needPerfCounters = true;
@@ -544,7 +544,6 @@ void MultiDeviceInferencePlugin::CheckConfig(const std::map<std::string, std::st
         } else if (kvp.first == PluginConfigParams::KEY_EXCLUSIVE_ASYNC_REQUESTS) {
             if (kvp.second == PluginConfigParams::YES ||
                 kvp.second == PluginConfigParams::NO) {
-                continue;
             } else {
                 IE_THROW() << "Unsupported config value: " << kvp.second
                            << " for key: " << kvp.first;
@@ -562,6 +561,7 @@ void MultiDeviceInferencePlugin::CheckConfig(const std::map<std::string, std::st
         } else if (supported_configKeys.end() == std::find(supported_configKeys.begin(), supported_configKeys.end(), kvp.first)) {
             IE_THROW() << "Unsupported config key: " << kvp.first;
         }
+        _config[kvp.first] = kvp.second;
     }
 }
 
