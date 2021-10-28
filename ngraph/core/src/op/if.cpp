669aebdb268ff392931ad21ca0c02099fb9c6c4f
Ilya Churaev <ilya.churaev@intel.com>
2021-10-28 09:19:49 +0300

    Removed redundant evaluate methods

--- ngraph/core/src/op/if.cpp
+++ ngraph/core/src/op/if.cpp
@@ -220,35 +220,6 @@ op::v8::If::OutputMap op::v8::If::get_mapping_outputs_on_body_description(
     return outputs_map;
 }
 
-bool op::v8::If::evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const {
-    NGRAPH_OP_SCOPE(v8_If_evaluate);
-    ngraph::runtime::reference::if_reference(m_bodies, m_output_descriptions, m_input_descriptions, outputs, inputs);
-    return true;
-}
-
-bool op::v8::If::has_evaluate() const {
-    NGRAPH_OP_SCOPE(v8_If_has_evaluate);
-    return true;
-}
-
-void op::v8::If::set_input(const Output<Node>& value,
-                           const std::shared_ptr<v0::Parameter>& then_parameter,
-                           const std::shared_ptr<v0::Parameter>& else_parameter) {
-    NGRAPH_CHECK(then_parameter != nullptr || else_parameter != nullptr,
-                 "Missing parameters! Both parameters are nullptr!");
-    auto then_param_index = m_bodies[THEN_BODY_INDEX]->get_parameter_index(then_parameter);
-    auto else_param_index = m_bodies[ELSE_BODY_INDEX]->get_parameter_index(else_parameter);
-    NGRAPH_CHECK(then_parameter == nullptr || then_param_index != -1,
-                 "Missing parameter ",
-                 then_parameter->get_friendly_name(),
-                 " for \'then_body\'!");
-    NGRAPH_CHECK(else_parameter == nullptr || else_param_index != -1,
-                 "Missing parameter ",
-                 else_parameter->get_friendly_name(),
-                 " for \'else_body\'!");
-    set_invariant_inputs(value, {then_parameter, else_parameter});
-}
-
 Output<Node> op::v8::If::set_output(const std::shared_ptr<v0::Result>& then_result,
                                     const std::shared_ptr<v0::Result>& else_result) {
     NGRAPH_CHECK(then_result != nullptr, "Incorrect result in \"then_body\"! Result cant be \'nullptr\'");
