125ace076765a776f066c75a5c55177476a75ddd
mandrono <maxim.andronov@intel.com>
2021-10-29 17:13:15 +0300

    enable other nms test

--- inference-engine/tests/functional/inference_engine/serialization/single_layer/non_max_suppression.cpp
+++ inference-engine/tests/functional/inference_engine/serialization/single_layer/non_max_suppression.cpp
@@ -1,57 +1,58 @@
-// // Copyright (C) 2018-2021 Intel Corporation
-// // SPDX-License-Identifier: Apache-2.0
-// //
+// Copyright (C) 2018-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
 
-// #include <vector>
+#include <vector>
 
-// #include "shared_test_classes/single_layer/non_max_suppression.hpp"
+#include "shared_test_classes/single_layer/non_max_suppression.hpp"
 
-// using namespace ngraph;
-// using namespace LayerTestsDefinitions;
+using namespace ngraph;
+using namespace ov::test::subgraph;
 
-// namespace {
-//     TEST_P(NmsLayerTest, Serialize) {
-//         Serialize();
-//     }
+namespace {
+    TEST_P(NmsLayerTest, Serialize) {
+        serialize();
+    }
 
-//     const std::vector<InferenceEngine::Precision> netPrecisions = {
-//             InferenceEngine::Precision::FP32,
-//             InferenceEngine::Precision::FP16
-//     };
+    const std::vector<InferenceEngine::Precision> netPrecisions = {
+            InferenceEngine::Precision::FP32,
+            InferenceEngine::Precision::FP16
+    };
 
-//     /* ============= NO MAX SUPPRESSION ============= */
+    /* ============= NO MAX SUPPRESSION ============= */
 
-//     const std::vector<InputShapeParams> inShapeParams = {
-//         InputShapeParams{3, 100, 5},
-//         InputShapeParams{1, 10, 50},
-//         InputShapeParams{2, 50, 50}
-//     };
+    const std::vector<InputShapeParams> inShapeParams = {
+        InputShapeParams{std::vector<ov::Dimension>{}, std::vector<TargetShapeParams>{{3, 100, 5}}},
+        InputShapeParams{std::vector<ov::Dimension>{}, std::vector<TargetShapeParams>{{1, 10, 50}}},
+        InputShapeParams{std::vector<ov::Dimension>{}, std::vector<TargetShapeParams>{{2, 50, 50}}}
+    };
 
-//     const std::vector<int32_t> maxOutBoxPerClass = {5, 20};
-//     const std::vector<float> threshold = {0.3f, 0.7f};
-//     const std::vector<float> sigmaThreshold = {0.0f, 0.5f};
-//     const std::vector<ngraph::op::v5::NonMaxSuppression::BoxEncodingType> encodType = {op::v5::NonMaxSuppression::BoxEncodingType::CENTER,
-//                                                                                     op::v5::NonMaxSuppression::BoxEncodingType::CORNER};
-//     const std::vector<bool> sortResDesc = {true, false};
-//     const std::vector<element::Type> outType = {element::i32, element::i64};
+    const std::vector<int32_t> maxOutBoxPerClass = {5, 20};
+    const std::vector<float> threshold = {0.3f, 0.7f};
+    const std::vector<float> sigmaThreshold = {0.0f, 0.5f};
+    const std::vector<ngraph::op::v5::NonMaxSuppression::BoxEncodingType> encodType = {op::v5::NonMaxSuppression::BoxEncodingType::CENTER,
+                                                                                       op::v5::NonMaxSuppression::BoxEncodingType::CORNER};
+    const std::vector<bool> sortResDesc = {true, false};
+    const std::vector<element::Type> outType = {element::i32, element::i64};
 
-//     const auto inPrecisions = ::testing::Combine(
-//         ::testing::Values(InferenceEngine::Precision::FP32),
-//         ::testing::Values(InferenceEngine::Precision::I32),
-//         ::testing::Values(InferenceEngine::Precision::FP32));
+    const auto inPrecisions = ::testing::Combine(
+        ::testing::Values(ov::element::Type_t::f32),
+        ::testing::Values(ov::element::Type_t::i32),
+        ::testing::Values(ov::element::Type_t::f32));
 
-//     const auto nmsParams = ::testing::Combine(
-//             ::testing::ValuesIn(inShapeParams),
-//             inPrecisions,
-//             ::testing::ValuesIn(maxOutBoxPerClass),
-//             ::testing::ValuesIn(threshold), // IOU threshold
-//             ::testing::ValuesIn(threshold), // Score threshold
-//             ::testing::ValuesIn(sigmaThreshold),
-//             ::testing::ValuesIn(encodType),
-//             ::testing::ValuesIn(sortResDesc),
-//             ::testing::ValuesIn(outType),
-//             ::testing::Values(CommonTestUtils::DEVICE_CPU));
+    const auto nmsParams = ::testing::Combine(
+            ::testing::ValuesIn(inShapeParams),
+            inPrecisions,
+            ::testing::ValuesIn(maxOutBoxPerClass),
+            ::testing::Combine(::testing::ValuesIn(threshold),
+                               ::testing::ValuesIn(threshold),
+                               ::testing::ValuesIn(sigmaThreshold)),
+            ::testing::Values(ngraph::helpers::InputLayerType::CONSTANT),
+            ::testing::ValuesIn(encodType),
+            ::testing::ValuesIn(sortResDesc),
+            ::testing::ValuesIn(outType),
+            ::testing::Values(CommonTestUtils::DEVICE_CPU));
 
-//     INSTANTIATE_TEST_SUITE_P(smoke_NmsLayerTest, NmsLayerTest, nmsParams, NmsLayerTest::getTestCaseName);
-// }  // namespace
+    INSTANTIATE_TEST_SUITE_P(smoke_NmsLayerTest, NmsLayerTest, nmsParams, NmsLayerTest::getTestCaseName);
+}  // namespace
 
