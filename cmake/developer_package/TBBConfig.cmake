de17ea20213898abf021a7690554c0321dbe5edf
Ilya Lavrenov <ilya.lavrenov@intel.com>
2021-11-01 13:43:48 +0300

    Support of oneTBB via env TBBROOT (#8327)

--- cmake/developer_package/TBBConfig.cmake
+++ cmake/developer_package/TBBConfig.cmake
@@ -26,6 +26,7 @@ endif()
 find_package(TBB
     CONFIG
     PATHS ${TBBROOT}/cmake
+          ${TBBROOT}/lib/cmake/TBB # oneTBB case
           ${IEDevScripts_DIR}/${IE_OWN_TBB_CONFIG}
     NO_CMAKE_FIND_ROOT_PATH
     NO_DEFAULT_PATH
