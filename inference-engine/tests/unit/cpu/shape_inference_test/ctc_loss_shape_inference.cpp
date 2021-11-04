d65645f7a24445a5ca3cf3658c00f446d95f5a44
mangguo <mang.guo@intel.com>
2021-11-03 23:00:13 -0400

    Implement CTCLoss shape inference

---
+++ inference-engine/tests/unit/cpu/shape_inference_test/ctc_loss_shape_inference.cpp
@@ -0,0 +1,44 @@
+// Copyright (C) 2018-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <gtest/gtest.h>
+
+#include <ctc_loss_shape_inference.hpp>
+#include <openvino/core/coordinate_diff.hpp>
+#include <openvino/op/ctc_loss.hpp>
+#include <openvino/op/parameter.hpp>
+#include <utils/shape_inference/static_shape.hpp>
+
+using namespace ov;
+
+TEST(StaticShapeInferenceTest, CTCLossTest) {
+    const auto& logits = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1});
+    const auto& logit_length = std::make_shared<ov::op::v0::Parameter>(element::i32, PartialShape{-1});
+    const auto& labels = std::make_shared<ov::op::v0::Parameter>(element::i32, PartialShape{-1, -1});
+    const auto& label_length = std::make_shared<ov::op::v0::Parameter>(element::i32, PartialShape{-1});
+    const auto& blank_index = std::make_shared<ov::op::v0::Parameter>(element::i32, Shape{});
+
+    // create CTCLoss node
+    auto ctc_loss = std::make_shared<op::v4::CTCLoss>(logits, logit_length, labels, label_length, blank_index);
+
+    std::vector<PartialShape> input_shapes = {PartialShape{10, 120, 28},
+                                              PartialShape{10},
+                                              PartialShape{10, 120},
+                                              PartialShape{10},
+                                              Shape{}},
+                              output_shapes = {PartialShape{}};
+    shape_infer(ctc_loss.get(), input_shapes, output_shapes);
+
+    ASSERT_EQ(output_shapes[0], PartialShape({10}));
+
+    std::vector<StaticShape> static_input_shapes = {StaticShape{10, 120, 28},
+                                                    StaticShape{10},
+                                                    StaticShape{10, 120},
+                                                    StaticShape{10},
+                                                    Shape{}},
+                             static_output_shapes = {StaticShape{}};
+    shape_infer(ctc_loss.get(), static_input_shapes, static_output_shapes);
+
+    ASSERT_EQ(static_output_shapes[0], StaticShape({10}));
+}
