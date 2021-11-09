24ce4ada22e0aede599b919fe9fb80ebefa149bb
mangguo <mang.guo@intel.com>
2021-11-09 01:10:49 -0500

    Implement InterpolateV0 and InterpolateV4

---
+++ inference-engine/tests/unit/cpu/shape_inference_test/interpolate_shape_inference.cpp
@@ -0,0 +1,125 @@
+// Copyright (C) 2018-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <gtest/gtest.h>
+
+#include <interpolate_shape_inference.hpp>
+#include <openvino/op/constant.hpp>
+#include <openvino/op/interpolate.hpp>
+#include <openvino/op/parameter.hpp>
+#include <utils/shape_inference/shape_inference.hpp>
+#include <utils/shape_inference/static_shape.hpp>
+
+using namespace ov;
+
+using InterpolateMode = op::v4::Interpolate::InterpolateMode;
+using CoordinateTransformMode = op::v4::Interpolate::CoordinateTransformMode;
+using Nearest_mode = op::v4::Interpolate::NearestMode;
+using ShapeCalcMode = op::v4::Interpolate::ShapeCalcMode;
+
+static std::shared_ptr<op::v4::Interpolate> build_InterpolateV4() {
+    op::v4::Interpolate::InterpolateAttrs attrs;
+    attrs.mode = InterpolateMode::NEAREST;
+    attrs.shape_calculation_mode = ShapeCalcMode::SCALES;
+    attrs.coordinate_transformation_mode = CoordinateTransformMode::HALF_PIXEL;
+    attrs.nearest_mode = Nearest_mode::ROUND_PREFER_FLOOR;
+    attrs.antialias = false;
+    attrs.pads_begin = {0, 0, 0, 0};
+    attrs.pads_end = {0, 0, 0, 0};
+    attrs.cube_coeff = -0.75;
+
+    auto input_data = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1, -1});
+    auto sizes = std::make_shared<op::v0::Parameter>(element::i32, PartialShape::dynamic());
+    auto scales = std::make_shared<op::v0::Parameter>(element::f32, PartialShape::dynamic());
+    auto axes = std::make_shared<op::v0::Parameter>(element::i32, PartialShape::dynamic());
+
+    auto interpolate = std::make_shared<op::v4::Interpolate>(input_data, sizes, scales, axes, attrs);
+    return interpolate;
+}
+
+static std::shared_ptr<op::v0::Interpolate> build_InterpolateV0() {
+    ov::op::v0::Interpolate::Attributes attrs;
+    attrs.axes = {2, 3};
+    attrs.mode = "nearest";
+    attrs.align_corners = true;
+    attrs.antialias = false;
+    attrs.pads_begin = {0, 0, 0, 0};
+    attrs.pads_end = {0, 0, 0, 0};
+
+    auto input_data = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1, -1});
+    auto sizes = std::make_shared<op::v0::Parameter>(element::i32, PartialShape::dynamic());
+
+    auto interpolate_v0 = std::make_shared<op::v0::Interpolate>(input_data, sizes, attrs);
+    return interpolate_v0;
+}
+
+TEST(StaticShapeInferenceTest, InterpolateV4Test) {
+    auto interpolate = build_InterpolateV4();
+
+    int32_t sizes_val[] = {24, 160};
+    float scales_val[] = {2.0, 0.5};
+    int32_t axes_val[] = {2, 3};
+    std::map<size_t, std::shared_ptr<ngraph::runtime::HostTensor>> constant_data;
+    constant_data[1] = std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, Shape{2}, sizes_val);
+    constant_data[2] = std::make_shared<ngraph::runtime::HostTensor>(element::f32, Shape{2}, scales_val);
+    constant_data[3] = std::make_shared<ngraph::runtime::HostTensor>(element::i32, Shape{2}, axes_val);
+
+    std::vector<StaticShape> static_input_shapes = {StaticShape{1, 2, 48, 80},
+                                                    StaticShape{2},
+                                                    StaticShape{2},
+                                                    StaticShape{2}},
+                             static_output_shapes = {StaticShape{}};
+    std::vector<size_t> pads_begin;
+    std::vector<size_t> pads_end;
+    correct_pads_attr(interpolate.get(), pads_begin, pads_end, static_input_shapes);
+    shape_infer(interpolate.get(), pads_begin, pads_end, static_input_shapes, static_output_shapes, constant_data);
+    ASSERT_EQ(static_output_shapes[0], StaticShape({1, 2, 96, 40}));
+}
+
+TEST(StaticShapeInferenceTest, InterpolateV4MissingConstantTest) {
+    auto interpolate = build_InterpolateV4();
+
+    int32_t sizes_val[] = {24, 160};
+    float scales_val[] = {2.0, 0.5};
+    std::map<size_t, std::shared_ptr<ngraph::runtime::HostTensor>> constant_data;
+    constant_data[1] = std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, Shape{2}, sizes_val);
+    constant_data[2] = std::make_shared<ngraph::runtime::HostTensor>(element::f32, Shape{2}, scales_val);
+
+    std::vector<StaticShape> static_input_shapes = {StaticShape{1, 2, 48, 80},
+                                                    StaticShape{2},
+                                                    StaticShape{2},
+                                                    StaticShape{2}},
+                             static_output_shapes = {StaticShape{}};
+    std::vector<size_t> pads_begin;
+    std::vector<size_t> pads_end;
+    correct_pads_attr(interpolate.get(), pads_begin, pads_end, static_input_shapes);
+    EXPECT_THROW(
+        shape_infer(interpolate.get(), pads_begin, pads_end, static_input_shapes, static_output_shapes, constant_data),
+        NodeValidationFailure);
+}
+
+TEST(StaticShapeInferenceTest, InterpolateV0Test) {
+    auto interpolate = build_InterpolateV0();
+
+    int32_t sizes_val[] = {15, 30};
+    std::map<size_t, std::shared_ptr<ngraph::runtime::HostTensor>> constant_data;
+    constant_data[1] = std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, Shape{2}, sizes_val);
+
+    std::vector<StaticShape> static_input_shapes = {StaticShape{2, 2, 33, 65}, StaticShape{2}},
+                             static_output_shapes = {StaticShape{}};
+    std::vector<size_t> pads_begin;
+    std::vector<size_t> pads_end;
+    shape_infer(interpolate.get(), static_input_shapes, static_output_shapes, constant_data);
+    ASSERT_EQ(static_output_shapes[0], StaticShape({2, 2, 15, 30}));
+}
+
+TEST(StaticShapeInferenceTest, InterpolateV0MissingConstantTest) {
+    auto interpolate = build_InterpolateV0();
+
+    std::vector<StaticShape> static_input_shapes = {StaticShape{2, 2, 33, 65}, StaticShape{2}},
+                             static_output_shapes = {StaticShape{}};
+    std::vector<size_t> pads_begin;
+    std::vector<size_t> pads_end;
+    EXPECT_THROW(shape_infer(interpolate.get(), static_input_shapes, static_output_shapes), NodeValidationFailure);
+}
---
+++ ngraph/core/shape_inference/include/interpolate_shape_inference.hpp
@@ -0,0 +1,220 @@
+// Copyright (C) 2018-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <openvino/op/interpolate.hpp>
+
+#include "utils.hpp"
+
+namespace ov {
+namespace op {
+
+template <class T>
+inline void set_dim(T& dim) {
+    OPENVINO_UNREACHABLE("Cannot set Dimension to in-compatible type.");
+}
+
+template <>
+inline void set_dim<ov::Dimension>(ov::Dimension& dim) {
+    dim = ov::Dimension::dynamic();
+};
+
+template <class T>
+inline void set_low_upper_bound(T& dim, int64_t lower_bound, int64_t upper_bound) {
+    OPENVINO_UNREACHABLE("Cannot set Dimension to in-compatible type.");
+}
+
+template <>
+inline void set_low_upper_bound<ov::Dimension>(ov::Dimension& dim, int64_t lower_bound, int64_t upper_bound) {
+    dim = ov::Dimension(lower_bound, upper_bound);
+};
+
+namespace v0 {
+template <class T>
+void shape_infer(const Interpolate* op,
+                 const std::vector<T>& input_shapes,
+                 std::vector<T>& output_shapes,
+                 const std::map<size_t, std::shared_ptr<ngraph::runtime::HostTensor>>& constant_data = {}) {
+    NODE_VALIDATION_CHECK(op, input_shapes.size() == 2 && output_shapes.size() == 1);
+
+    const auto& input_shape = input_shapes[0];
+    const auto& target_spatial_shape = input_shapes[1];
+    auto& output_shape = output_shapes[0];
+    output_shape = input_shape;
+
+    auto attrs = op->get_attrs();
+
+    if (input_shape.rank().is_static()) {
+        // todo: add test case for input_shape.rank() is dynamic. If dynamic, can we set output_shape[axis]
+        // todo: test original code with proc-type, is input_shape.rank is dynamic, what's the output shape?
+        if (target_spatial_shape.rank().is_static()) {
+            NODE_VALIDATION_CHECK(
+                op,
+                target_spatial_shape[0].compatible(attrs.axes.size()),
+                "The target_spatial_shape must be compatible with the number of indices in *axes* attribute");
+        }
+        for (auto axis : attrs.axes) {
+            NODE_VALIDATION_CHECK(op, static_cast<int64_t>(axis) < input_shape.rank().get_length());
+        }
+
+        std::vector<int64_t> out_shape;
+        if (get_data_as_int64<T>(1, op, out_shape, constant_data)) {
+            size_t i = 0;
+            for (auto axis : attrs.axes) {
+                output_shape[axis] = out_shape[i++];
+            }
+        } else {
+            for (auto axis : attrs.axes) {
+                set_dim(output_shape[axis]);
+            }
+        }
+    }
+}
+}  // namespace v0
+
+namespace v4 {
+template <typename T>
+std::vector<T> correct_pad(const std::vector<T>& p, size_t rank) {
+    size_t pad_len = p.size();
+    if (pad_len == rank) {
+        return p;
+    }
+
+    std::vector<T> result;
+
+    if (pad_len > rank) {
+        result.insert(result.end(), p.begin(), p.begin() + rank);
+    } else {
+        result = p;
+        result.insert(result.end(), rank - pad_len, T{});
+    }
+
+    return result;
+}
+
+template <typename T>
+void correct_pads_attr(const Interpolate* op,
+                  std::vector<size_t>& pads_begin,
+                  std::vector<size_t>& pads_end,
+                  const std::vector<T>& input_shapes) {
+    auto attrs = op->get_attrs();
+    auto input_shape = input_shapes[0];
+    pads_begin = attrs.pads_begin;
+    pads_end = attrs.pads_end;
+
+    if (input_shape.rank().is_dynamic()) {
+        return;
+    }
+    const auto input_rank = input_shape.rank().get_length();
+
+    pads_begin = correct_pad(attrs.pads_begin, input_rank);
+    pads_end = correct_pad(attrs.pads_end, input_rank);
+}
+
+inline int64_t multiply_bound_and_scale(int64_t bound, float scale) {
+    if (bound == -1) {
+        return bound;
+    }
+    return static_cast<int64_t>(static_cast<float>(bound) * scale);
+}
+
+template <typename T>
+void infer_using_scales(T& output_shape,
+                        const std::vector<int64_t>& axes,
+                        const std::vector<float>& scales,
+                        const T& padded_input_shape) {
+    size_t i = 0;
+    static constexpr float epsilon = 1.0e-6f;
+    for (auto axis : axes) {
+        NGRAPH_CHECK(axis < padded_input_shape.rank().get_length());
+        const auto& current_dim = padded_input_shape[axis];
+        float multiplier = scales[i] + epsilon;
+        if (current_dim.is_static()) {
+            output_shape[axis] = multiply_bound_and_scale(current_dim.get_length(), multiplier);
+        } else {
+            int64_t new_lower_bound = multiply_bound_and_scale(current_dim.get_min_length(), multiplier);
+            int64_t new_upper_bound = multiply_bound_and_scale(current_dim.get_max_length(), multiplier);
+            set_low_upper_bound(output_shape[axis], new_lower_bound, new_upper_bound);
+        }
+        ++i;
+    }
+}
+
+template <typename T>
+void infer_using_shapes(T& output_shape, const std::vector<int64_t>& axes, const std::vector<int64_t>& sizes) {
+    size_t i = 0;
+    for (auto axis : axes) {
+        NGRAPH_CHECK(axis < output_shape.rank().get_length());
+        output_shape[axis] = sizes[i++];
+    }
+}
+
+template <class T>
+void shape_infer(const Interpolate* op,
+                 std::vector<size_t>& pads_begin,
+                 std::vector<size_t>& pads_end,
+                 const std::vector<T>& input_shapes,
+                 std::vector<T>& output_shapes,
+                 const std::map<size_t, std::shared_ptr<ngraph::runtime::HostTensor>>& constant_data = {}) {
+    NODE_VALIDATION_CHECK(op, (input_shapes.size() == 3 || input_shapes.size() == 4) && output_shapes.size() == 1);
+
+    const auto& input_shape = input_shapes[0];
+    auto& output_shape = output_shapes[0];
+    output_shape = input_shape;
+
+    if (input_shape.rank().is_static()) {
+        const auto input_rank = input_shape.rank().get_length();
+
+        // Get axes
+        std::vector<int64_t> axes;
+        if (input_shapes.size() == 4 && !(get_data_as_int64<T>(3, op, axes, constant_data))) {
+            for (size_t i = 0; i < input_rank; i++)
+                set_dim(output_shape[i]);
+            return;
+        } else if (input_shapes.size() == 3) {
+            axes.resize(input_rank);
+            std::iota(axes.begin(), axes.end(), 0);
+        }
+
+        // Get padded input shape
+        auto padded_input_shape = input_shape;
+        for (int64_t i = 0; i < input_rank; ++i) {
+            if (input_shape[i].is_static()) {
+                auto new_length = pads_begin[i] + pads_end[i] + input_shape[i].get_length();
+                padded_input_shape[i] = new_length;
+            }
+        }
+
+        output_shape = padded_input_shape;
+
+        auto attrs = op->get_attrs();
+
+        if (attrs.shape_calculation_mode == op->ShapeCalcMode::SCALES) {
+            std::vector<float> scales;
+            if (get_data_as_float<T>(2, op, scales, constant_data)) {
+                infer_using_scales(output_shape, axes, scales, padded_input_shape);
+            } else {
+                for (auto axis : axes) {
+                    NGRAPH_CHECK(axis < input_rank);
+                    set_dim(output_shape[axis]);
+                }
+            }
+        } else {
+            std::vector<int64_t> sizes;
+            if (get_data_as_int64<T>(1, op, sizes, constant_data)) {
+                infer_using_shapes(output_shape, axes, sizes);
+            } else {
+                for (auto axis : axes) {
+                    NGRAPH_CHECK(axis < input_rank);
+                    set_dim(output_shape[axis]);
+                }
+            }
+        }
+    }
+}
+
+}  // namespace v4
+}  // namespace op
+}  // namespace ov
