9b19350d5d6016a55b4f303a2889e080a453478b
Tomasz Dołbniak <tomasz.dolbniak@intel.com>
2021-11-09 10:53:34 +0100

    Update of ONNX submodule to v1.10.2 (#8383)
    * Update of ONNX submodule to v1.10.0
    
    * Update of ONNX to 1.10.2
    
    * Adaptation of the ONNX FE to ONNX 1.10
    
    * ConstantOfShape XFAIL removal
    
    * SCEL reference model updated to the new ONNX functions expansion behavior
    
    * UnXFAIL more ONNX tests
    
    * UnXFAIL even more ONNX tests

--- runtime/bindings/python/tests_compatibility/__init__.py
+++ runtime/bindings/python/tests_compatibility/__init__.py
@@ -114,8 +114,6 @@ xfail_issue_36538 = xfail_test(reason="RuntimeError: Check 'PartialShape::broadc
                                       "/openvino/ngraph/src/ngraph/op/util/elementwise_args.cpp:48:")
 xfail_issue_39658 = xfail_test(reason="RuntimeError: Tile operation has a form that is not supported."
                                       " z should be converted to TileIE operation.")
-xfail_issue_39659 = xfail_test(reason="RuntimeError: Broadcast operation has a form that is not supported."
-                                      " y should be converted to Tile operation.")
 xfail_issue_39662 = xfail_test(reason="RuntimeError: 'ScatterElementsUpdate' layer with name 'y' have "
                                       "indices value that points to non-existing output tensor element")
 
