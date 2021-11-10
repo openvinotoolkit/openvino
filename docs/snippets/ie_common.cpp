ec67af8fa64c0cab40f6a1da2389bd8eb63dc5e2
Ilya Churaev <ilya.churaev@intel.com>
2021-11-10 16:27:23 +0300

    Updated common migration pipeline (#8176)
    * Updated common migration pipeline
    
    * Fixed merge issue
    
    * Added new model and extended example
    
    * Fixed typo
    
    * Added v10-v11 comparison

--- docs/snippets/ie_common.cpp
+++ docs/snippets/ie_common.cpp
@@ -27,8 +27,29 @@ int main() {
     //! [ie:create_infer_request]
 
     //! [ie:get_input_tensor]
-    InferenceEngine::Blob::Ptr input_blob = infer_request.GetBlob(inputs.begin()->first);
-    // fill input blob
+    InferenceEngine::Blob::Ptr input_blob1 = infer_request.GetBlob(inputs.begin()->first);
+    // fill first blob
+    InferenceEngine::SizeVector dims1 = input_blob1->getTensorDesc().getDims();
+    InferenceEngine::MemoryBlob::Ptr minput1 = InferenceEngine::as<InferenceEngine::MemoryBlob>(input_blob1);
+    if (minput1) {
+        // locked memory holder should be alive all time while access to its
+        // buffer happens
+        auto minputHolder = minput1->wmap();
+        // Original I64 precision was converted to I32
+        auto data = minputHolder.as<InferenceEngine::PrecisionTrait<InferenceEngine::Precision::I32>::value_type*>();
+        // Fill data ...
+    }
+    InferenceEngine::Blob::Ptr input_blob2 = infer_request.GetBlob("data2");
+    // fill first blob
+    InferenceEngine::MemoryBlob::Ptr minput2 = InferenceEngine::as<InferenceEngine::MemoryBlob>(input_blob2);
+    if (minput2) {
+        // locked memory holder should be alive all time while access to its
+        // buffer happens
+        auto minputHolder = minput2->wmap();
+        // Original I64 precision was converted to I32
+        auto data = minputHolder.as<InferenceEngine::PrecisionTrait<InferenceEngine::Precision::I32>::value_type*>();
+        // Fill data ...
+    }
     //! [ie:get_input_tensor]
 
     //! [ie:inference]
@@ -37,7 +58,16 @@ int main() {
 
     //! [ie:get_output_tensor]
     InferenceEngine::Blob::Ptr output_blob = infer_request.GetBlob(outputs.begin()->first);
-    // process output data
+    InferenceEngine::MemoryBlob::Ptr moutput = InferenceEngine::as<InferenceEngine::MemoryBlob>(output_blob);
+    if (moutput) {
+        // locked memory holder should be alive all time while access to its
+        // buffer happens
+        auto minputHolder = moutput->rmap();
+        // Original I64 precision was converted to I32
+        auto data =
+            minputHolder.as<const InferenceEngine::PrecisionTrait<InferenceEngine::Precision::I32>::value_type*>();
+        // process output data
+    }
     //! [ie:get_output_tensor]
     return 0;
 }
