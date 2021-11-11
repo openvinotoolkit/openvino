6955c36cac60a235ad0257ff75248bb34e7a3855
Zhang Yi3 <yi3.zhang@intel.com>
2021-11-11 16:31:56 +0800

    [ShapeInfer]impl gather

--- ngraph/core/include/openvino/op/util/gather_base.hpp
+++ ngraph/core/include/openvino/op/util/gather_base.hpp
@@ -36,7 +36,7 @@ public:
     OPENVINO_SUPPRESS_DEPRECATED_END
 
     bool constant_fold(OutputVector& output_values, const OutputVector& inputs_values) override;
-
+    int64_t get_batch_dims() const;
 protected:
     int64_t m_batch_dims = 0;
 };
