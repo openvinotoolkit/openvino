858edad3d4144f3f56219b9b4b3282d0b3a09b62
Alexey Lebedev <alexey.lebedev@intel.com>
2021-11-11 20:40:53 +0300

    update statistics reporting

--- tools/benchmark_tool/openvino/tools/benchmark/utils/statistics_report.py
+++ tools/benchmark_tool/openvino/tools/benchmark/utils/statistics_report.py
@@ -58,49 +58,56 @@ class StatisticsReport:
 
             logger.info(f"Statistics report is stored to {f.name}")
 
-    def dump_performance_counters_request(self, f, perf_counts):
+    def dump_performance_counters_request(self, f, prof_info):
         total = 0
         total_cpu = 0
         f.write(self.csv_separator.join(['layerName', 'execStatus', 'layerType', 'execType', 'realTime (ms)', 'cpuTime (ms)\n']))
-        for k, v in sorted(perf_counts.items(), key=lambda x: x[1]['execution_index']):
-            f.write(self.csv_separator.join([k, v['status'], v['layer_type'], v['exec_type'], str(v['real_time']/1000.0), str(v['cpu_time']/1000.0)]))
+        for pi in prof_info:
+            f.write(self.csv_separator.join([pi.node_name, pi.status, pi.node_type, pi.exec_type, str(pi.real_time/1000.0), str(pi.cpu_time/1000.0)]))
             f.write('\n')
-            total += v['real_time']
-            total_cpu += v['cpu_time']
+            total += pi.real_time
+            total_cpu += pi.cpu_time
         f.write(self.csv_separator.join(['Total','','','',str(total/1000.0),str(total_cpu/1000.0)]))
         f.write('\n\n')
 
-    def dump_performance_counters(self, perf_counts):
+    def dump_performance_counters(self, prof_info):
         if self.config.report_type == '' or self.config.report_type == noCntReport:
             logger.info("Statistics collecting for performance counters was not requested. No reports are dumped.")
             return
 
-        if not perf_counts:
+        if not prof_info:
             logger.info('Performance counters are empty. No reports are dumped.')
             return
 
         filename = os.path.join(self.config.report_folder, f'benchmark_{self.config.report_type}_report.csv')
         with open(filename, 'w') as f:
             if self.config.report_type == detailedCntReport:
-                for pc in perf_counts:
-                    self.dump_performance_counters_request(f, pc)
+                for pi in prof_info:
+                    self.dump_performance_counters_request(f, pi)
             elif self.config.report_type == averageCntReport:
-                def get_average_performance_counters(perf_counts):
+                def get_average_performance_counters(prof_info):
                     performance_counters_avg = {}
                     ## iterate over each processed infer request and handle its PM data
-                    for i in range(0, len(perf_counts)):
+                    """for i in range(0, len(perf_counts)):
                         ## iterate over each layer from sorted vector and add required PM data to the per-layer maps
                         for k in perf_counts[0].keys():
                             if k not in performance_counters_avg.keys():
                                 performance_counters_avg[k] = perf_counts[i][k]
                             else:
                                 performance_counters_avg[k]['real_time'] += perf_counts[i][k]['real_time']
-                                performance_counters_avg[k]['cpu_time'] += perf_counts[i][k]['cpu_time']
-                    for _, v in performance_counters_avg.items():
-                        v['real_time'] /= len(perf_counts)
-                        v['cpu_time'] /= len(perf_counts)
+                                performance_counters_avg[k]['cpu_time'] += perf_counts[i][k]['cpu_time']"""
+                    for pi in prof_info:
+                        if pi.node_name not in performance_counters_avg.keys():
+                            performance_counters_avg[pi.node_name] = {'real_time': pi.real_time, 'cpu_time': pi.cpu_time}
+                        else:
+                            performance_counters_avg[pi.node_name]['real_time'] += pi.real_time
+                            performance_counters_avg[pi.node_name]['cpu_time'] += pi.cpu_time
+
+                    for v in performance_counters_avg.values():
+                        v['real_time'] /= len(prof_info)
+                        v['cpu_time'] /= len(prof_info)
                     return performance_counters_avg
-                self.dump_performance_counters_request(f, get_average_performance_counters(perf_counts))
+                self.dump_performance_counters_request(f, get_average_performance_counters(prof_info))
             else:
                 raise Exception('PM data can only be collected for average or detailed report types')
 
