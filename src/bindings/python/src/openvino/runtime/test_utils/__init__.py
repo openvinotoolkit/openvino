a445105a05e8f278a2e5fcbdf3d4947b06e1a31a
Anastasia Kuporosova <anastasia.kuporosova@intel.com>
2021-11-12 12:54:19 +0300

    Update __init__.py

--- src/bindings/python/src/openvino/runtime/test_utils/__init__.py
+++ src/bindings/python/src/openvino/runtime/test_utils/__init__.py
@@ -2,4 +2,4 @@
 # Copyright (C) 2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from openvino.pyopenvino.test_utils import compare_functions
+from openvino.test_utils import compare_functions
