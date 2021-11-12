5cdadb15db0a9985a502d3d4ee085e5505b6aa85
akuporos <anastasia.kuporosova@intel.com>
2021-11-12 03:54:46 +0300

    fix codestyle

--- src/inference/dev_api/src/pyopenvino/test_utils/test_utils.cpp
+++ src/inference/dev_api/src/pyopenvino/test_utils/test_utils.cpp
@@ -6,20 +6,20 @@
 
 #include <pybind11/pybind11.h>
 
-#include "common_test_utils/ngraph_test_utils.hpp"
 #include <string>
+
+#include "common_test_utils/ngraph_test_utils.hpp"
 
 namespace py = pybind11;
 
 void regmodule_test_utils(py::module m) {
-    py::module m_test_utils =
-            m.def_submodule("test_utils", "test_utils module");
+    py::module m_test_utils = m.def_submodule("test_utils", "test_utils module");
 
     m_test_utils.def(
-            "compare_functions",
-            [](std::shared_ptr<ov::Function> lhs, std::shared_ptr<ov::Function> rhs) {
-                return compare_functions(lhs, rhs, true, true, false, true, true);
-            },
-            py::arg("lhs"),
-            py::arg("rhs"));
+        "compare_functions",
+        [](std::shared_ptr<ov::Function> lhs, std::shared_ptr<ov::Function> rhs) {
+            return compare_functions(lhs, rhs, true, true, false, true, true);
+        },
+        py::arg("lhs"),
+        py::arg("rhs"));
 }
