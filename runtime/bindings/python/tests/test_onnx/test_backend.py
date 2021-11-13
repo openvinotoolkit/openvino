da708edba6d3d0f21744c1ddbda6b1df575fac02
Zhang Yi3 <yi3.zhang@intel.com>
2021-11-13 21:33:17 +0800

    remove useless xfail

--- runtime/bindings/python/tests/test_onnx/test_backend.py
+++ runtime/bindings/python/tests/test_onnx/test_backend.py
@@ -26,7 +26,6 @@ from tests import (
     xfail_issue_38724,
     xfail_issue_38734,
     xfail_issue_38735,
-    xfail_issue_39658,
     xfail_issue_39662,
     xfail_issue_44854,
     xfail_issue_44858,
