a5e15964d4bc2d5cc70c37de38efc09f5ef49284
meiyang-intel <yang.mei@intel.com>
2021-11-14 19:54:03 -0500

    Revert "support dynamic input for paddle elementwise ops"
    This reverts commit a30a93f5b80f0a88064112e498f83eb9b22aa4bd.
    use general opset in paddle elementwise op

--- src/frontends/paddlepaddle/src/op/elementwise_ops.cpp
+++ src/frontends/paddlepaddle/src/op/elementwise_ops.cpp
@@ -3,7 +3,7 @@
 //
 
 #include <map>
-#include <ngraph/opsets/opset6.hpp>
+#include "default_opset.hpp"
 #include <node_context.hpp>
 
 #include "openvino/opsets/opset6.hpp"
@@ -17,101 +17,64 @@ NamedOutputs elementwise_ops(const NodeContext& node) {
     auto x = node.get_ng_input("X");
     auto y = node.get_ng_input("Y");
 
-    auto axis_value = node.get_attribute<int>("axis");
-    if (axis_value == -1)
+    auto axis = node.get_attribute<int>("axis");
+
+    PDPD_OP_VALIDATION_CHECK(node, x.get_partial_shape().rank().is_static(), "elementwise_ops: X rank must be static!");
+    PDPD_OP_VALIDATION_CHECK(node, y.get_partial_shape().rank().is_static(), "elementwise_ops: Y rank must be static!");
+    int64_t x_rank = x.get_partial_shape().rank().get_length();
+    int64_t y_rank = y.get_partial_shape().rank().get_length();
+
+    if ((axis == -1) || (axis == x_rank - 1) || (x_rank == y_rank)) {
         return node.default_single_output_mapping({std::make_shared<T>(x, y)}, {"Out"});
-    else {
-        auto axis = ngraph::opset6::Constant::create(ngraph::element::i64, ov::Shape{}, {axis_value});
+    } else {
+        std::vector<int64_t> indices;
+        for (int64_t i = 0; i < axis; i++)
+            indices.push_back(i);
+        for (int64_t i = y_rank + axis; i < x_rank; i++)
+            indices.push_back(i);
 
-        auto x_shape = std::make_shared<ngraph::opset6::ShapeOf>(x);
-        auto x_rank = std::make_shared<ngraph::opset6::ShapeOf>(x_shape);
-        auto y_shape = std::make_shared<ngraph::opset6::ShapeOf>(y);
-        auto y_rank = std::make_shared<ngraph::opset6::ShapeOf>(y_shape);
-
-        auto const_0 = ngraph::opset6::Constant::create(ngraph::element::i64, ov::Shape{}, {0});
-        auto const_1 = ngraph::opset6::Constant::create(ngraph::element::i64, ov::Shape{}, {1});
-
-        // condition of when to broadcast
-        auto x_rank_sub_1 = std::make_shared<ngraph::opset6::Subtract>(x_rank, const_1);
-        auto axis_is_last = std::make_shared<ngraph::opset6::Equal>(axis, x_rank_sub_1);
-        auto rank_is_equal = std::make_shared<ngraph::opset6::Equal>(x_rank, y_rank);
-        auto not_broadcast = std::make_shared<ngraph::opset6::LogicalOr>(axis_is_last, rank_is_equal);
-        // if broadcast or not
-        auto if_op = std::make_shared<ngraph::op::v8::If>(not_broadcast);
-        auto element_type = y.get_element_type();
-        // not broadcast
-        auto then_x = std::make_shared<ngraph::op::Parameter>(element_type, PartialShape::dynamic());
-        auto then_y = std::make_shared<ngraph::op::Parameter>(element_type, PartialShape::dynamic());
-        auto then_res = std::make_shared<ngraph::op::Result>(then_y);
-        auto then_body =
-            std::make_shared<ngraph::Function>(ngraph::OutputVector{then_res}, ngraph::ParameterVector{then_x, then_y});
-        // broadcast
-        auto else_x = std::make_shared<ngraph::op::Parameter>(element_type, PartialShape::dynamic());
-        auto else_y = std::make_shared<ngraph::op::Parameter>(element_type, PartialShape::dynamic());
-        auto x_shape_else = std::make_shared<ngraph::opset6::ShapeOf>(else_x);
-        auto x_rank_else = std::make_shared<ngraph::opset6::ShapeOf>(x_shape_else);
-        auto y_shape_else = std::make_shared<ngraph::opset6::ShapeOf>(else_y);
-        auto y_rank_else = std::make_shared<ngraph::opset6::ShapeOf>(y_shape_else);
-
-        auto range0 = std::make_shared<ngraph::opset6::Range>(const_0, axis, const_1, ngraph::element::i64);
-        auto y_rank_add_axis = std::make_shared<ngraph::opset6::Add>(y_rank_else, axis);
-        auto y_rank_add_axis_scalar = std::make_shared<ngraph::opset6::Squeeze>(y_rank_add_axis);
-        auto x_rank_scalar = std::make_shared<ngraph::opset6::Squeeze>(x_rank_else);
-        auto range1 = std::make_shared<ngraph::opset6::Range>(y_rank_add_axis_scalar,
-                                                              x_rank_scalar,
-                                                              const_1,
-                                                              ngraph::element::i64);
-        auto indices = std::make_shared<ngraph::opset6::Concat>(ngraph::NodeVector{range0, range1}, 0);
-        auto y_broadcast = std::make_shared<ngraph::opset6::Unsqueeze>(else_y, indices);
-        auto else_res = std::make_shared<ngraph::op::Result>(y_broadcast);
-        auto else_body =
-            std::make_shared<ngraph::Function>(ngraph::OutputVector{else_res}, ngraph::ParameterVector{else_x, else_y});
-        // set input/output/body of if op
-        if_op->set_then_body(then_body);
-        if_op->set_else_body(else_body);
-        if_op->set_input(x, then_x, else_x);
-        if_op->set_input(y, then_y, else_y);
-        auto res_y = if_op->set_output(then_res, else_res);
-
-        return node.default_single_output_mapping({std::make_shared<T>(x, res_y)}, {"Out"});
+        auto indices_node =
+            default_opset::Constant::create(ngraph::element::i64, ngraph::Shape{indices.size()}, indices);
+        auto y_node = std::make_shared<default_opset::Unsqueeze>(y, indices_node);
+        return node.default_single_output_mapping({std::make_shared<T>(x, y_node)}, {"Out"});
     }
 }
 
 //
 NamedOutputs elementwise_add(const NodeContext& node_context) {
-    return elementwise_ops<ngraph::opset6::Add>(node_context);
+    return elementwise_ops<default_opset::Add>(node_context);
 }
 
 NamedOutputs elementwise_sub(const NodeContext& node_context) {
-    return elementwise_ops<ngraph::opset6::Subtract>(node_context);
+    return elementwise_ops<default_opset::Subtract>(node_context);
 }
 
 NamedOutputs elementwise_mul(const NodeContext& node_context) {
-    return elementwise_ops<ngraph::opset6::Multiply>(node_context);
+    return elementwise_ops<default_opset::Multiply>(node_context);
 }
 
 NamedOutputs elementwise_div(const NodeContext& node_context) {
-    return elementwise_ops<ngraph::opset6::Divide>(node_context);
+    return elementwise_ops<default_opset::Divide>(node_context);
 }
 
 NamedOutputs elementwise_min(const NodeContext& node_context) {
-    return elementwise_ops<ngraph::opset6::Minimum>(node_context);
+    return elementwise_ops<default_opset::Minimum>(node_context);
 }
 
 NamedOutputs elementwise_max(const NodeContext& node_context) {
-    return elementwise_ops<ngraph::opset6::Maximum>(node_context);
+    return elementwise_ops<default_opset::Maximum>(node_context);
 }
 
 NamedOutputs elementwise_pow(const NodeContext& node_context) {
-    return elementwise_ops<ngraph::opset6::Power>(node_context);
+    return elementwise_ops<default_opset::Power>(node_context);
 }
 
 NamedOutputs elementwise_equal(const NodeContext& node_context) {
-    return elementwise_ops<ngraph::opset6::Equal>(node_context);
+    return elementwise_ops<default_opset::Equal>(node_context);
 }
 
 NamedOutputs elementwise_greater_equal(const NodeContext& node_context) {
-    return elementwise_ops<ngraph::opset6::GreaterEqual>(node_context);
+    return elementwise_ops<default_opset::GreaterEqual>(node_context);
 }
 
 }  // namespace op
