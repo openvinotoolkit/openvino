11c46c0c1f3f428a67175bc39890077b86441431
bszmelcz <bartosz.szmelczynski@intel.com>
2021-11-15 12:16:02 +0100

    add read_model from buffer

--- runtime/bindings/python/tests/test_inference_engine/test_executable_network.py
+++ runtime/bindings/python/tests/test_inference_engine/test_executable_network.py
@@ -293,3 +293,30 @@ def test_infer_tensor_wrong_input_data(device):
     with pytest.raises(TypeError) as e:
         exec_net.infer_new_request({4.5: tensor})
     assert "Incompatible key type!" in str(e.value)
+
+
+def test_infer_numpy_model_from_buffer(device):
+    core = Core()
+    with open(test_net_bin, "rb") as f:
+        bin = f.read()
+    with open(test_net_xml, "rb") as f:
+        xml = f.read()
+    func = core.read_model(model=xml, weights=bin)
+    img = read_image()
+    exec_net = core.compile_model(func, device)
+    res = exec_net.infer_new_request({'data': img})
+    assert np.argmax(res) == 2
+
+
+def test_infer_tensor_model_from_buffer(device):
+    core = Core()
+    with open(test_net_bin, "rb") as f:
+        bin = f.read()
+    with open(test_net_xml, "rb") as f:
+        xml = f.read()
+    func = core.read_model(model=xml, weights=bin)
+    img = read_image()
+    tensor = Tensor(img)
+    exec_net = core.compile_model(func, device)
+    res = exec_net.infer_new_request({'data': tensor})
+    assert np.argmax(res) == 2
