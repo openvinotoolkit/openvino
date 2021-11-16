aa6b4209a82d6be264c7e5f5b599fdb59d792167
Roman Lyamin <roman.lyamin@intel.com>
2021-11-16 13:32:37 +0300

    [GPU] Add NV12toRGB/NV12toBGR operations

--- src/plugins/intel_gpu/src/plugin/ops/result.cpp
+++ src/plugins/intel_gpu/src/plugin/ops/result.cpp
@@ -40,6 +40,11 @@ static void CreateResultOp(Program& p, const std::shared_ptr<ngraph::op::v0::Res
     const auto outputDesc = outputData->getTensorDesc();
     const auto outputlayout = outputDesc.getLayout();
 
+    // if (ngraph::is_type<ngraph::op::v8::NV12toRGB>(prev) ||
+    //     ngraph::is_type<ngraph::op::v8::NV12toBGR>(prev)) {
+    //     outputlayout = NHWC;
+    // }
+
     // TODO: add precision check once there's an outputInfo object
     if (outputlayout != NCHW &&
         // TODO: change 6d case once new layout added in IE
@@ -59,7 +64,7 @@ static void CreateResultOp(Program& p, const std::shared_ptr<ngraph::op::v0::Res
 
     p.AddPrimitive(cldnn::reorder(outLayerName,
                                   outputID,
-                                  FormatFromLayout(outputData->getLayout()),
+                                  FormatFromLayout(outputData->getLayout()),//FormatFromLayout(outputlayout),
                                   DataTypeFromPrecision(precision),
                                   std::vector<float>(),
                                   cldnn::reorder_mean_mode::subtract,
