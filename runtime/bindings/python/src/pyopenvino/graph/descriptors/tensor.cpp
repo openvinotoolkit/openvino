bdc18e74ebaf7d32437c490a99b7a941a8b9fe9a
akuporos <anastasia.kuporosova@intel.com>
2021-11-17 00:14:54 +0300

    add documentation

--- runtime/bindings/python/src/pyopenvino/graph/descriptors/tensor.cpp
+++ runtime/bindings/python/src/pyopenvino/graph/descriptors/tensor.cpp
@@ -25,27 +25,88 @@ void regclass_graph_descriptor_Tensor(py::module m) {
                py::arg("shape"),
                py::arg("name"));
 
-    tensor.def("get_shape", &ov::descriptor::Tensor::get_shape);
+    tensor.def("get_shape",
+               &ov::descriptor::Tensor::get_shape,
+               R"(
+                Returns the shape description.
+
+                Returns
+                ----------
+                get_shape : Shape
+                   The shape description.
+             )");
 
     tensor.def("get_rt_info",
                (PyRTMap & (ov::descriptor::Tensor::*)()) & ov::descriptor::Tensor::get_rt_info,
-               py::return_value_policy::reference_internal);
+               py::return_value_policy::reference_internal,
+               R"(
+                Returns PyRTMap which is a dictionary of user defined runtime info.
 
-    tensor.def("size", &ov::descriptor::Tensor::size);
+                Returns
+                ----------
+                get_rt_info : PyRTMap
+                    A dictionary of user defined data.
+             )");
 
-    tensor.def("get_partial_shape", &ov::descriptor::Tensor::get_partial_shape);
+    tensor.def("size",
+               &ov::descriptor::Tensor::size,
+               R"(
+                Returns the size description
 
-    tensor.def("get_element_type", &ov::descriptor::Tensor::get_element_type);
+                Returns
+                ----------
+                size : size_t
+                    The size description.
+             )");
 
-    tensor.def("get_names", &ov::descriptor::Tensor::get_names);
+    tensor.def("get_partial_shape",
+               &ov::descriptor::Tensor::get_partial_shape,
+               R"(
+                Returns the partial shape description
 
-    tensor.def("set_names", &ov::descriptor::Tensor::set_names);
+                Returns
+                ----------
+                get_partial_shape : PartialShape
+                    PartialShape description.
+             )");
+
+    tensor.def("get_element_type",
+               &ov::descriptor::Tensor::get_element_type,
+               R"(
+                Returns the element type description
+
+                Returns
+                ----------
+                get_element_type : Type
+                    Type description
+             )");
+
+    tensor.def("get_names",
+               &ov::descriptor::Tensor::get_names,
+               R"(
+                Returns names
+
+                Returns
+                ----------
+                get_names : set
+                    Set of names
+             )");
+
+    tensor.def("set_names",
+               &ov::descriptor::Tensor::set_names,
+               py::arg("names"),
+               R"(
+                Set names for tensor
+
+                Parameters
+                ----------
+                names : set
+                    Set of names
+             )");
 
     tensor.def_property_readonly("shape", &ov::descriptor::Tensor::get_shape);
 
-    tensor.def_property_readonly("rt_info",
-                                 (PyRTMap & (ov::descriptor::Tensor::*)()) & ov::descriptor::Tensor::get_rt_info,
-                                 py::return_value_policy::reference_internal);
+    tensor.def_property_readonly("rt_info");
 
     tensor.def_property_readonly("size", &ov::descriptor::Tensor::size);
 
