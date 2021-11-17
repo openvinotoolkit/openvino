65b138ed08d1eff5cc764b8e03961e63d2e75c1b
VGavrilov <vladimir.gavrilov@intel.com>
2021-11-17 13:30:17 +0300

    Written a function that checks one of Concat node of the pattern.

--- inference-engine/src/transformations/src/transformations/common_optimizations/nearest_neighbor_umpsampling_fusion.cpp
+++ inference-engine/src/transformations/src/transformations/common_optimizations/nearest_neighbor_umpsampling_fusion.cpp
@@ -6,6 +6,7 @@
 #include "transformations/common_optimizations/nearest_neighbor_upsampling_fusion.hpp"
 #include "transformations/utils/utils.hpp"
 
+#include <algorithm>
 #include <memory>
 #include <vector>
 
@@ -14,41 +15,76 @@
 #include <ngraph/pattern/op/wrap_type.hpp>
 #include <ngraph/pattern/op/or.hpp>
 
+namespace {
+std::vector<size_t> get_scales_from_mul_const_shape(const Shape& s, uint64_t input_rank) {
+    if (input_rank < 4 || static_cast<uint64_t>(s.size()) != 2 + 2 * (input_rank - 2)) return {};
+
+    std::vector<size_t> expected_shape(2 + 2 * (input_rank - 2), static_cast<size_t>(1));
+    std::vector<size_t> scales(input_rank - 2);
+    for (uint64_t i = 1; i <= input_rank - 2; ++i) {
+        expected_shape[2 * i] = s[2 * i];
+        scales[i - 1] = s[2 * i];
+    }
+
+    if (s != expected_shape) return {};
+
+    return scales;
+}
+
+std::shared_ptr<ngraph::opset8::Unsqueeze> get_input_unsqueeze_for_concat_1(const std::shared_ptr<ngraph::opset8::Concat>& concat, const Shape& shape) {
+    size_t rank = shape.size();
+
+    const auto inputs = concat->input_values();
+    size_t num_of_input_values = inputs.size();
+
+    if (num_of_input_values != 2 + 2 * (rank - 2)) return nullptr;
+
+    const auto input0 = std::dynamic_pointer_cast<ngraph::opset8::Unsqueeze>(inputs[0].get_node_shared_ptr());
+    if (!input0) return nullptr;
+
+    const auto input0_axis = std::dynamic_pointer_cast<ngraph::opset8::Constant>(input0->input_value(1).get_node_shared_ptr());
+    if (!input0_axis || input0_axis->cast_vector<int64_t>() != std::vector<int64_t>{0}) return nullptr;
+
+    std::vector<int64_t> input_constants(num_of_input_values, 1);
+
+    for (size_t i = 1; i < num_of_input_values; ++i) {
+        const auto& current_input = std::dynamic_pointer_cast<ngraph::opset8::Unsqueeze>(inputs[i].get_node_shared_ptr());
+        if (!current_input) return nullptr;
+
+        const auto current_input_axis = std::dynamic_pointer_cast<ngraph::opset8::Constant>(current_input->input_value(1).get_node_shared_ptr());
+        if (!current_input_axis || current_input_axis->cast_vector<int64_t>() != std::vector<int64_t>{0}) return nullptr;
+
+        const auto unsqueezed_const = std::dynamic_pointer_cast<ngraph::opset8::Constant>(current_input->input_value(0).get_node_shared_ptr());
+        if (!unsqueezed_const) return nullptr;
+
+        const auto unsqueezed_const_value = unsqueezed_const->cast_vector<int64_t>();
+        if (unsqueezed_const_value.size() != 1) return nullptr;
+
+        input_constants[i] = unsqueezed_const_value[0];
+    }
+
+    std::vector<int64_t> expected_input_constants(num_of_input_values, 1);
+    for (size_t i = 1; i <= rank - 2; ++i) {
+        expected_input_constants[2 * (i - 1) + 1] = static_cast<int64_t>(shape[i]);
+    }
+    expected_input_constants.back() = static_cast<int64_t>(shape.back());
+
+    if (input_constants != expected_input_constants) return nullptr;
+
+    return input0;
+}
+}
+
 NGRAPH_RTTI_DEFINITION(ngraph::pass::NearestNeighborUpsamplingFusion, "NearestNeighborUpsamplingFusion", 0);
 
 ngraph::pass::NearestNeighborUpsamplingFusion::NearestNeighborUpsamplingFusion() {
     MATCHER_SCOPE(NearestNeighborUpsamplingFusion);
     auto input = ngraph::pattern::any_input();
-    auto shape_of = std::make_shared<ngraph::opset8::ShapeOf>(input);
-
-    auto slice_begin = pattern::wrap_type<ngraph::opset8::Constant>();
-    auto slice_end = pattern::wrap_type<ngraph::opset8::Constant>();
-    auto slice_stride = pattern::wrap_type<ngraph::opset8::Constant>();
-    auto strided_slice = ngraph::pattern::wrap_type<ngraph::opset8::StridedSlice>({shape_of, slice_begin, slice_end, slice_stride});
-
-    auto unsqueeze_1_axis = pattern::wrap_type<ngraph::opset8::Constant>();
-    auto unsqueeze_1 = ngraph::pattern::wrap_type<ngraph::opset8::Unsqueeze>({strided_slice, unsqueeze_1_axis});
-
-    auto unsqueeze_2_axis = pattern::wrap_type<ngraph::opset8::Constant>();
-    auto unsqueeze_2 = ngraph::pattern::wrap_type<ngraph::opset8::Unsqueeze>({strided_slice, unsqueeze_2_axis});
-
-    auto concat_1_h = pattern::wrap_type<ngraph::opset8::Constant>();
-    auto concat_1_one_1 = pattern::wrap_type<ngraph::opset8::Constant>();
-    auto concat_1_w = pattern::wrap_type<ngraph::opset8::Constant>();
-    auto concat_1_one_2 = pattern::wrap_type<ngraph::opset8::Constant>();
-    auto concat_1_c = pattern::wrap_type<ngraph::opset8::Constant>();
-    auto concat_1 = pattern::wrap_type<ngraph::opset8::Concat>({unsqueeze_1, concat_1_h, concat_1_one_1, concat_1_w, concat_1_one_2, concat_1_c});
-
-    auto concat_2_h = pattern::wrap_type<ngraph::opset8::Constant>();
-    auto concat_2_w = pattern::wrap_type<ngraph::opset8::Constant>();
-    auto concat_2_c = pattern::wrap_type<ngraph::opset8::Constant>();
-    auto concat_2 = pattern::wrap_type<ngraph::opset8::Concat>({unsqueeze_2, concat_2_h, concat_2_w, concat_2_c});
-
-    auto reshape_1 = pattern::wrap_type<ngraph::opset8::Reshape>({input, concat_1});
-
-    auto mul_const = pattern::wrap_type<ngraph::opset8::Constant>();
+    auto concat_1 = ngraph::pattern::wrap_type<ngraph::opset8::Concat>();
+    auto concat_2 = ngraph::pattern::wrap_type<ngraph::opset8::Concat>();
+    auto reshape_1 = ngraph::pattern::wrap_type<ngraph::opset8::Reshape>({input, concat_1});
+    auto mul_const = ngraph::pattern::wrap_type<ngraph::opset8::Constant>();
     auto mul = pattern::wrap_type<ngraph::opset8::Multiply>({reshape_1, mul_const});
-
     auto reshape_2 = pattern::wrap_type<ngraph::opset8::Reshape>({mul, concat_2});
 
     ngraph::matcher_pass_callback callback = [=](ngraph::pattern::Matcher& m) {
@@ -56,12 +92,29 @@ ngraph::pass::NearestNeighborUpsamplingFusion::NearestNeighborUpsamplingFusion()
 
         const auto reshape_2_node = std::dynamic_pointer_cast<ngraph::opset8::Reshape>(pattern_to_output.at(reshape_2).get_node_shared_ptr());
         const auto mul_node = std::dynamic_pointer_cast<ngraph::opset8::Multiply>(pattern_to_output.at(mul).get_node_shared_ptr());
-        const auto mul_const_node = std::dynamic_pointer_cast<ngraph::opset8::Constant>(pattern_to_output.at(mul_const).get_node_shared_ptr());
 
-        if (!reshape_2_node || !mul_node || !mul_const_node) return false;
+        if (!reshape_2_node || !mul_node || mul_node->get_input_partial_shape(1).is_dynamic()) return false;
+
+        const auto mul_const_node = std::dynamic_pointer_cast<ngraph::opset8::Constant>(mul_node->input_value(1).get_node_shared_ptr());
+        if (!mul_const_node) return false;
+
+        const auto reshape_1_node = std::dynamic_pointer_cast<ngraph::opset8::Reshape>(pattern_to_output.at(reshape_1).get_node_shared_ptr());
+        if (!reshape_1_node || reshape_1_node->get_input_partial_shape(0).is_dynamic()) return false;
+
+        uint64_t input_rank = static_cast<uint64_t>(reshape_1_node->get_input_partial_shape(0).rank().get_length());
+        const auto mul_const_shape = mul_const_node->get_output_shape(0);
+        const auto scales = get_scales_from_mul_const_shape(mul_const_shape, input_rank);
+        if (scales.empty()) return false;
 
         const auto mul_const_value = mul_const_node->cast_vector<float>();
-        if (mul_const_value != std::vector<float>{1.0f, 1.0f, 1.0f, 1.0f}) return false;
+        if (std::any_of(mul_const_value.begin(), mul_const_value.end(), [](float x){ return x != 1.0f; })) { return false; }
+
+        const auto concat_1_node = std::dynamic_pointer_cast<ngraph::opset8::Concat>(reshape_1_node->input_value(1).get_node_shared_ptr());
+        if (!concat_1_node) return false;
+
+        const auto unsqueeze_1 = get_input_unsqueeze_for_concat_1(concat_1_node, reshape_1_node->get_input_shape(0));
+        if (!unsqueeze_1) return false;
+
         return true;
     };
 
