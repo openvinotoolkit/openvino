432f52e3d59d21bc06ecabf05d8458e39a257375
Ilya Churaev <ilya.churaev@intel.com>
2021-11-17 17:44:52 +0300

    Fixed ONNX test utils

--- cmake/features.cmake
+++ cmake/features.cmake
@@ -160,21 +160,21 @@ ie_option(OV_IR_FRONTEND_ENABLE "Enable IR FrontEnd" ON)
 ie_dependent_option(OV_TF_FRONTEND_ENABLE "Enable TensorFlow FrontEnd" ON "protoc_available" OFF)
 ie_dependent_option(OV_USE_SYSTEM_PROTOBUF "Use system protobuf" OFF
     "OV_ONNX_FRONTEND_ENABLE OR OV_PDPD_FRONTEND_ENABLE OR OV_TF_FRONTEND_ENABLE" OFF)
-ie_dependent_option(OV_CORE_UNIT_TEST_ENABLE "Enables ngraph unit tests" ON "ENABLE_TESTS;NOT ANDROID" OFF)
+ie_dependent_option(OV_CORE_UNIT_TEST_ENABLE "Enables ov core unit tests" ON "ENABLE_TESTS;NOT ANDROID" OFF)
 ie_dependent_option(OV_CORE_TEST_BACKENDS_ENABLE "Control the building of unit tests using backends" ON
     "OV_CORE_UNIT_TEST_ENABLE" OFF)
 ie_option(OPENVINO_DEBUG_ENABLE "Enable output for OPENVINO_DEBUG statements" OFF)
 ie_option(ENABLE_REQUIREMENTS_INSTALL "Dynamic dependencies install" ON)
 
-# WA for ngraph python build on Windows debug
+# WA for openvino python build on Windows debug
 list(REMOVE_ITEM IE_OPTIONS OV_CORE_UNIT_TEST_ENABLE OV_CORE_TEST_BACKENDS_ENABLE)
 
 #
 # Process featues
 #
 
-if(NGRAPH_DEBUG_ENABLE)
-    add_definitions(-DNGRAPH_DEBUG_ENABLE)
+if(OPENVINO_DEBUG_ENABLE)
+    add_definitions(-DOPENVINO_DEBUG_ENABLE)
 endif()
 
 if (ENABLE_PROFILING_RAW)
