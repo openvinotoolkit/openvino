01e62f96b40e7b8f8066043b76ce14d28f2feae1
pszmel <piotr.szmelczynski@intel.com>
2021-11-18 21:31:29 +0100

    create test with model update tests with new PrePostProcessor init and build

--- runtime/bindings/python/tests/test_ngraph/test_preprocess.py
+++ runtime/bindings/python/tests/test_ngraph/test_preprocess.py
@@ -11,6 +11,7 @@ from openvino.impl.preprocess import PrePostProcessor, InputInfo, PreProcessStep
     PostProcessSteps, ResizeAlgorithm
 from openvino.impl import Function, Output, Type
 from openvino.utils.decorators import custom_preprocess_function
+from openvino import Core
 from tests.runtime import get_runtime
 
 
@@ -20,13 +21,13 @@ def test_ngraph_preprocess_mean():
     model = parameter_a
     function = Function(model, [parameter_a], "TestFunction")
 
-    function = PrePostProcessor()\
+    function = PrePostProcessor(function)\
         .input(InputInfo()
                .preprocess(PreProcessSteps()
                            .mean(1.)
                            )
                )\
-        .build(function)
+        .build()
 
     input_data = np.array([[1, 2], [3, 4]]).astype(np.float32)
     expected_output = np.array([[0, 1], [2, 3]]).astype(np.float32)
@@ -44,14 +45,14 @@ def test_ngraph_preprocess_mean_vector():
     function = Function(model, [parameter_a], "TestFunction")
     layout = ov.Layout("NCHW")
 
-    function = PrePostProcessor()\
+    function = PrePostProcessor(function)\
         .input(InputInfo()
                .tensor(InputTensorInfo().set_layout(layout))
                .preprocess(PreProcessSteps()
                            .mean([1., 2.])
                            )
                )\
-        .build(function)
+        .build()
 
     input_data = np.array([[1, 2], [3, 4]]).astype(np.float32)
     expected_output = np.array([[0, 0], [2, 2]]).astype(np.float32)
@@ -69,14 +70,14 @@ def test_ngraph_preprocess_scale_vector():
     function = Function(model, [parameter_a], "TestFunction")
     layout = ov.Layout("NCHW")
 
-    function = PrePostProcessor()\
+    function = PrePostProcessor(function)\
         .input(InputInfo()
                .tensor(InputTensorInfo().set_layout(layout))
                .preprocess(PreProcessSteps()
                            .scale([0.5, 2.])
                            )
                )\
-        .build(function)
+        .build()
 
     input_data = np.array([[1, 2], [3, 4]]).astype(np.float32)
     expected_output = np.array([[2, 1], [6, 2]]).astype(np.float32)
@@ -97,7 +98,7 @@ def test_ngraph_preprocess_mean_scale_convert():
     def custom_preprocess(output: Output):
         return ops.abs(output)
 
-    function = PrePostProcessor() \
+    function = PrePostProcessor(function) \
         .input(InputInfo(1)
                .tensor(InputTensorInfo()
                        .set_element_type(Type.i32))
@@ -114,7 +115,7 @@ def test_ngraph_preprocess_mean_scale_convert():
                            .custom(custom_preprocess)
                            )
                ) \
-        .build(function)
+        .build()
 
     input_data1 = np.array([[0, 1], [2, -2]]).astype(np.int32)
     input_data2 = np.array([[1, 3], [5, 7]]).astype(np.int32)
@@ -141,7 +142,7 @@ def test_ngraph_preprocess_output_postprocess():
     def custom_postprocess(output: Output):
         return ops.abs(output)
 
-    function = PrePostProcessor()\
+    function = PrePostProcessor(function)\
         .input(InputInfo()
                .tensor(InputTensorInfo().set_layout(layout1))
                .preprocess(PreProcessSteps()
@@ -154,7 +155,7 @@ def test_ngraph_preprocess_output_postprocess():
                                          .convert_layout(layout2)
                                          .convert_layout(layout3)
                                          .custom(custom_postprocess))) \
-        .build(function)
+        .build()
 
     input_data = np.array([[-1, -2], [-3, -4]]).astype(np.int32)
     expected_output = np.array([[2, 4], [4, 6]]).astype(np.float32)
@@ -173,7 +174,7 @@ def test_ngraph_preprocess_spatial_static_shape():
     layout = ov.Layout("CHW")
 
     color_format = ColorFormat.RGB
-    function = PrePostProcessor()\
+    function = PrePostProcessor(function)\
         .input(InputInfo()
                .tensor(InputTensorInfo()
                        .set_layout(layout)
@@ -190,7 +191,7 @@ def test_ngraph_preprocess_spatial_static_shape():
                         .set_layout(layout)
                         .set_element_type(Type.f32))
                 .network(OutputNetworkInfo().set_layout(layout))) \
-        .build(function)
+        .build()
 
     input_data = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]]).astype(np.int32)
     expected_output = np.array([[[0, 1], [2, 3]], [[3, 4], [5, 6]]]).astype(np.float32)
@@ -219,7 +220,7 @@ def test_ngraph_preprocess_steps(algorithm, color_format1, color_format2, is_fai
     function = Function(model, [parameter_a], "TestFunction")
     layout1 = ov.Layout("NCWH")
     layout2 = ov.Layout("NCHW")
-    custom_processor = PrePostProcessor()\
+    custom_processor = PrePostProcessor(function)\
         .input(InputInfo()
                .tensor(InputTensorInfo()
                        .set_layout(layout1)
@@ -231,10 +232,10 @@ def test_ngraph_preprocess_steps(algorithm, color_format1, color_format2, is_fai
                            .convert_color(color_format2)))
     if is_failing:
         with pytest.raises(RuntimeError) as e:
-            function = custom_processor.build(function)
+            function = custom_processor.build()
         assert "is not convertible to" in str(e.value)
     else:
-        function = custom_processor.build(function)
+        function = custom_processor.build()
         input_data = np.array([[[[1, 2, 3], [4, 5, 6], [7, 8, 9]]]]).astype(np.float32)
         expected_output = np.array([[[[0, 3, 6], [1, 4, 7], [2, 5, 8]]]]).astype(np.float32)
 
@@ -252,7 +253,7 @@ def test_ngraph_preprocess_postprocess_layout():
     layout1 = ov.Layout("NCWH")
     layout2 = ov.Layout("NCHW")
 
-    function = PrePostProcessor()\
+    function = PrePostProcessor(function)\
         .input(InputInfo()
                .tensor(InputTensorInfo()
                        .set_layout(layout1))
@@ -265,7 +266,7 @@ def test_ngraph_preprocess_postprocess_layout():
         .output(OutputInfo()
                 .postprocess(PostProcessSteps()
                              .convert_layout([0, 1, 2, 3]))) \
-        .build(function)
+        .build()
 
     input_data = np.array([[[[1, 2, 3], [4, 5, 6], [7, 8, 9]]]]).astype(np.float32)
     expected_output = np.array([[[[0, 3, 6], [1, 4, 7], [2, 5, 8]]]]).astype(np.float32)
@@ -283,7 +284,7 @@ def test_ngraph_preprocess_reverse_channels():
     function = Function(model, [parameter_a], "TestFunction")
     layout1 = ov.Layout("NCWH")
 
-    function = PrePostProcessor()\
+    function = PrePostProcessor(function)\
         .input(InputInfo()
                .tensor(InputTensorInfo()
                        .set_layout(layout1))
@@ -292,7 +293,7 @@ def test_ngraph_preprocess_reverse_channels():
                            .reverse_channels()
                            )
                ) \
-        .build(function)
+        .build()
 
     input_data = np.array([[[[1, 2], [3, 4]], [[5, 6], [7, 8]]]]).astype(np.float32)
     expected_output = np.array([[[[4, 5], [6, 7]], [[0, 1], [2, 3]]]]).astype(np.float32)
@@ -311,7 +312,7 @@ def test_ngraph_preprocess_resize_algorithm():
     resize_alg = ResizeAlgorithm.RESIZE_CUBIC
     layout1 = ov.Layout("NCWH")
 
-    function = PrePostProcessor()\
+    function = PrePostProcessor(function)\
         .input(InputInfo()
                .tensor(InputTensorInfo()
                        .set_layout(layout1))
@@ -319,7 +320,7 @@ def test_ngraph_preprocess_resize_algorithm():
                            .mean(1.)
                            .resize(resize_alg, 3, 3))
                )\
-        .build(function)
+        .build()
 
     input_data = np.array([[[[1, 2, 3], [4, 5, 6], [7, 8, 9]]]]).astype(np.float32)
     expected_output = np.array([[[[0, 1, 2], [3, 4, 5], [6, 7, 8]]]]).astype(np.float32)
@@ -327,4 +328,97 @@ def test_ngraph_preprocess_resize_algorithm():
     runtime = get_runtime()
     computation = runtime.computation(function)
     output = computation(input_data)
+    assert np.equal(output, expected_output).all()
+
+
+def test_ngraph_preprocess_model():
+    model = bytes(b"""<net name="add_model" version="10">
+    <layers>
+    <layer id="0" name="x" type="Parameter" version="opset1">
+        <data element_type="i32" shape="2,2,2"/>
+        <output>
+            <port id="0" precision="FP32">
+                <dim>2</dim>
+                <dim>2</dim>
+                <dim>2</dim>
+            </port>
+        </output>
+    </layer>
+    <layer id="1" name="y" type="Parameter" version="opset1">
+        <data element_type="i32" shape="2,2,2"/>
+        <output>
+            <port id="0" precision="FP32">
+                <dim>2</dim>
+                <dim>2</dim>
+                <dim>2</dim>
+            </port>
+        </output>
+    </layer>
+    <layer id="2" name="sum" type="Add" version="opset1">
+        <input>
+            <port id="0">
+                <dim>2</dim>
+                <dim>2</dim>
+                <dim>2</dim>
+            </port>
+            <port id="1">
+                <dim>2</dim>
+                <dim>2</dim>
+                <dim>2</dim>
+            </port>
+        </input>
+        <output>
+            <port id="2" precision="FP32">
+                <dim>2</dim>
+                <dim>2</dim>
+                <dim>2</dim>
+            </port>
+        </output>
+    </layer>
+    <layer id="3" name="sum/sink_port_0" type="Result" version="opset1">
+        <input>
+            <port id="0">
+                <dim>2</dim>
+                <dim>2</dim>
+                <dim>2</dim>
+            </port>
+        </input>
+    </layer>
+    </layers>
+    <edges>
+    <edge from-layer="0" from-port="0" to-layer="2" to-port="0"/>
+    <edge from-layer="1" from-port="0" to-layer="2" to-port="1"/>
+    <edge from-layer="2" from-port="2" to-layer="3" to-port="0"/>
+    </edges>
+</net>""")
+    core = Core()
+    function = core.read_model(model=model)
+
+    @custom_preprocess_function
+    def custom_preprocess(output: Output):
+        return ops.abs(output)
+
+    function = PrePostProcessor(function) \
+        .input(InputInfo(1)
+               .preprocess(PreProcessSteps()
+                           .convert_element_type(Type.f32)
+                           .scale(0.5)
+                           )
+               ) \
+        .input(InputInfo(0)
+               .preprocess(PreProcessSteps()
+                           .convert_element_type(Type.f32)
+                           .mean(5.))) \
+        .output(OutputInfo(0)
+                .postprocess(PostProcessSteps()
+                             .custom(custom_preprocess))) \
+        .build()
+
+    input_data = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]]).astype(np.float32)
+    expected_output = np.array([[[2, 1], [4, 7]], [[10, 13], [16, 19]]]).astype(np.float32)
+
+    runtime = get_runtime()
+    computation = runtime.computation(function)
+    output = computation(input_data, input_data)
+
     assert np.equal(output, expected_output).all()
