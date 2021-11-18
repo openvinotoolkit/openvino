268a511a8fb286570c72a0fcf578185d46d9bc27
Ivan Tikhonov <ivan.tikhonov@intel.com>
2021-11-18 17:33:37 +0300

    fix paddle frontend

--- ngraph/frontend/paddlepaddle/include/paddlepaddle_frontend/frontend.hpp
+++ ngraph/frontend/paddlepaddle/include/paddlepaddle_frontend/frontend.hpp
@@ -68,7 +68,7 @@ private:
     static std::shared_ptr<Function> convert_each_node(
         const std::shared_ptr<InputModelPDPD>& model,
         std::function<std::map<std::string, OutputVector>(const std::map<std::string, Output<Node>>&,
-                                                          const std::shared_ptr<OpPlacePDPD>&)> func) const;
+                                                          const std::shared_ptr<OpPlacePDPD>&)> func)1;
     std::shared_ptr<TelemetryExtension> m_telemetry;
     const std::string m_telemetry_category = "PADDLE_FE";
 };
