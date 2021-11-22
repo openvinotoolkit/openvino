b98bb250904783a71cf795a2e95a17634175d4a2
Egor Duplensky <egor.duplenskii@intel.com>
2021-11-22 22:24:30 +0300

    [CPU] [DEBUG CAPS] Implicitly enable perf counters for verbose mode
    Also refactor debug capabilities configuration.
    Now debug caps config parameters are the part of plugin configuration.

--- src/plugins/intel_cpu/src/mkldnn_node.h
+++ src/plugins/intel_cpu/src/mkldnn_node.h
@@ -576,6 +576,10 @@ public:
         return outputShapes[port];
     }
 
+    const std::vector<InferenceEngine::Blob::Ptr>& getInternalBlobs() const {
+        return internalBlobs;
+    }
+
     /**
     * @brief Return scales and shift if nodes can be executed as ScaleShift, else raise exception
     * If node has only scale or shift value, fill missing value with default values
@@ -668,7 +672,6 @@ protected:
     friend class MKLDNNEdge;
     friend class MKLDNNGraph;
     friend class MKLDNNGraphOptimizer;
-    friend class NodeDumper;
 
     void selectPreferPrimitiveDescriptor(const std::vector<impl_desc_type>& priority, bool ignoreConstInputs);
     bool isConfigDefined(const NodeConfig &config) const;
