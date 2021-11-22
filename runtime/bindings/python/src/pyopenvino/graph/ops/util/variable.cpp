6f46ce3d76e7787c84f2dd66e5757db811c22624
Artur Kulikowski <artur.kulikowski@intel.com>
2021-11-22 10:10:21 +0100

    Variable::get_info as a property instead of method

--- runtime/bindings/python/src/pyopenvino/graph/ops/util/variable.cpp
+++ runtime/bindings/python/src/pyopenvino/graph/ops/util/variable.cpp
@@ -10,10 +10,10 @@ namespace py = pybind11;
 
 void regclass_graph_op_util_Variable(py::module m) {
     py::class_<ov::op::util::VariableInfo> variable_info(m, "VariableInfo");
-    variable_info.doc() = "openvino.impl.op.util.Variable wraps ov::op::util::VariableInfo";
+    variable_info.doc() = "openvino.impl.op.util.VariableInfo wraps ov::op::util::VariableInfo";
 
     py::class_<ov::op::util::Variable, std::shared_ptr<ov::op::util::Variable>> variable(m, "Variable");
     variable.doc() = "openvino.impl.op.util.Variable wraps ov::op::util::Variable";
-    variable.def("get_info", &ov::op::util::Variable::get_info);
+    variable.def_property_readonly("get_info", &ov::op::util::Variable::get_info);
     variable.def("update", &ov::op::util::Variable::update, py::arg("variable_info"));
 }
