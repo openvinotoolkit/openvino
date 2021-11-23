a06e0463bf85f4aa3f65445df8f70ac73baf7f21
Artur Kulikowski <artur.kulikowski@intel.com>
2021-11-23 14:44:52 +0100

    Fix Variable test

--- runtime/bindings/python/tests/test_ngraph/test_ops_util_variable.py
+++ runtime/bindings/python/tests/test_ngraph/test_ops_util_variable.py
@@ -7,40 +7,40 @@ from openvino.impl.op.util import VariableInfo, Variable
 
 def test_info_as_property():
     info = VariableInfo()
-    info.shape = PartialShape([1])
-    info.type = Type.f32
-    info.id = "test_id"
+    info.data_shape = PartialShape([1])
+    info.data_type = Type.f32
+    info.variable_id = "test_id"
     variable = Variable(info)
-    assert variable.info.shape == info.shape
-    assert variable.info.type == info.type
-    assert variable.info.id == info.id
+    assert variable.info.data_shape == info.data_shape
+    assert variable.info.data_type == info.data_type
+    assert variable.info.variable_id == info.variable_id
 
 
 def test_get_info():
     info = VariableInfo()
-    info.shape = PartialShape([1])
-    info.type = Type.f32
-    info.id = "test_id"
+    info.data_shape = PartialShape([1])
+    info.data_type = Type.f32
+    info.variable_id = "test_id"
     variable = Variable(info)
-    assert variable.get_info().shape == info.shape
-    assert variable.get_info().type == info.type
-    assert variable.get_info().id == info.id
+    assert variable.get_info().data_shape == info.data_shape
+    assert variable.get_info().data_type == info.data_type
+    assert variable.get_info().variable_id == info.variable_id
 
 
 def test_info_update():
     info1 = VariableInfo()
-    info1.shape = PartialShape([1])
-    info1.type = Type.f32
-    info1.id = "test_id"
+    info1.data_shape = PartialShape([1])
+    info1.data_type = Type.f32
+    info1.variable_id = "test_id"
 
     variable = Variable(info1)
 
     info2 = VariableInfo()
-    info2.shape = PartialShape([2, 1])
-    info2.type = Type.i64
-    info2.id = "test_id2"
+    info2.data_shape = PartialShape([2, 1])
+    info2.data_type = Type.i64
+    info2.variable_id = "test_id2"
 
     variable.update(info2)
-    assert variable.info.shape == info2.shape
-    assert variable.info.type == info2.type
-    assert variable.info.id == info2.id
+    assert variable.info.data_shape == info2.data_shape
+    assert variable.info.data_type == info2.data_type
+    assert variable.info.variable_id == info2.variable_id
