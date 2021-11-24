efcd1c4086b8f6f94fc8cc4e8a20a6ccd7c821dd
Mateusz Tabaka <mateusz.tabaka@intel.com>
2021-11-24 04:53:08 +0000

    [VPU] filter out unused outputs in RNN layer handler
    Ticket: 60540

---
+++ inference-engine/tests/functional/plugin/myriad/subgraph_tests/lstm_transpose.cpp
@@ -0,0 +1,37 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <shared_test_classes/base/layer_test_utils.hpp>
+#include <ngraph/opsets/opset8.hpp>
+
+namespace {
+
+using namespace ngraph;
+
+class LSTMTranspose : public testing::WithParamInterface<std::string>, virtual public LayerTestsUtils::LayerTestsCommon {
+protected:
+    void SetUp() override {
+        targetDevice = GetParam();
+
+        auto X = std::make_shared<opset8::Parameter>(element::f32, Shape{1, 26, 64});
+        auto hidden_state = std::make_shared<opset8::Parameter>(element::f32, Shape{1, 1, 32});
+        auto cell_state = std::make_shared<opset8::Parameter>(element::f32, Shape{1, 1, 32});
+        auto seq_len = opset8::Constant::create(element::i32, Shape{1}, {26});
+        auto W = opset8::Constant::create(element::f32, Shape{1, 128, 64}, {1});
+        auto R = opset8::Constant::create(element::f32, Shape{1, 128, 32}, {1});
+        auto B = opset8::Constant::create(element::f32, Shape{1, 128}, {1});
+        auto lstm = std::make_shared<opset8::LSTMSequence>(X, hidden_state, cell_state, seq_len, W, R, B, 32, op::RecurrentSequenceDirection::FORWARD);
+        auto transpose = std::make_shared<opset8::Transpose>(lstm->output(0), opset8::Constant::create(element::i32, Shape{4}, {2, 0, 1, 3}));
+        function = std::make_shared<ngraph::Function>(transpose, ParameterVector{X, hidden_state, cell_state});
+    }
+};
+
+TEST_P(LSTMTranspose, CompareWithRefs) {
+    Run();
+}
+
+INSTANTIATE_TEST_SUITE_P(smoke_LSTMTranspose, LSTMTranspose,
+                         ::testing::Values(CommonTestUtils::DEVICE_MYRIAD));
+
+}  // namespace
