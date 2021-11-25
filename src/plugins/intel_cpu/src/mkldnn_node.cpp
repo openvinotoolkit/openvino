f27675165b3b3079b4c7ab42e935bced1d87cd8e
Nikolay Shchegolev <nikolay.shchegolev@intel.com>
2021-11-25 23:13:17 +0300

    [CPU] Dynamic shapes. RNN node.

--- src/plugins/intel_cpu/src/mkldnn_node.cpp
+++ src/plugins/intel_cpu/src/mkldnn_node.cpp
@@ -648,10 +648,6 @@ void MKLDNNNode::filterSupportedPrimitiveDescriptors() {
 }
 
 void MKLDNNNode::initDescriptor(const NodeConfig& config) {
-    auto* selectedPD = getSelectedPrimitiveDescriptor();
-    if (!selectedPD) {
-        return;
-    }
     std::vector<MemoryDescPtr> inDescs;
     for (const auto& inConf : config.inConfs)
         inDescs.emplace_back(inConf.desc);
@@ -662,6 +658,10 @@ void MKLDNNNode::initDescriptor(const NodeConfig& config) {
 
     AttrPtr attr = initPrimitiveAttr();
 
+    auto* selectedPD = getSelectedPrimitiveDescriptor();
+    if (!selectedPD) {
+        return;
+    }
     NodeConfig rightConfig = selectedPD->getConfig();
     size_t selected_count = 0;
     for (size_t j = 0; j < descs.size(); j++) {
@@ -1376,8 +1376,9 @@ bool MKLDNNNode::hasEmptyOutputTensors() const {
 
 bool MKLDNNNode::inputShapesDefined() const {
     for (size_t i = 0; i < getParentEdges().size(); i++) {
-        if (!getParentEdgesAtPort(i)[0]->getMemory().getDesc().isDefined())
+        if (!getParentEdgesAtPort(i)[0]->getMemory().getDesc().isDefined()) {
             return false;
+        }
     }
     return true;
 }
