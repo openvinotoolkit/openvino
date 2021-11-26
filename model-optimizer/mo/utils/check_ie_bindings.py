c3ccd88df6c775e2c375d0ff5852cb72a7f6f591
Andrei Kochin <andrei.kochin@intel.com>
2021-11-26 12:24:26 +0300

    [MO] simple change in ie bindings to avoid unnecessary warning

--- model-optimizer/mo/utils/check_ie_bindings.py
+++ model-optimizer/mo/utils/check_ie_bindings.py
@@ -102,7 +102,7 @@ def import_core_modules(silent: bool, path_to_module: str):
         return True
     except Exception as e:
         # Do not print a warning if module wasn't found or silent mode is on
-        if "No module named 'openvino'" not in str(e):
+        if "No module named 'openvino" not in str(e):
             print("[ WARNING ] Failed to import Inference Engine Python API in: {}".format(path_to_module))
             print("[ WARNING ] {}".format(e))
 
