6e7626e6e315f7904e1cfd9227e39e3ed9e20146
pszmel <piotr.szmelczynski@intel.com>
2021-11-26 01:29:18 +0100

    remove inferMap container

--- runtime/bindings/python/src/pyopenvino/core/containers.hpp
+++ runtime/bindings/python/src/pyopenvino/core/containers.hpp
@@ -19,10 +19,8 @@ namespace py = pybind11;
 namespace Containers {
     using TensorIndexMap = std::map<size_t, ov::runtime::Tensor>;
     using TensorNameMap = std::map<std::string, ov::runtime::Tensor>;
-    using InferMap = std::map<ov::Output<const ov::Node>, ov::runtime::Tensor>;
     using InferVec = std::vector<std::pair<ov::Output<const ov::Node>, ov::runtime::Tensor>>;
 
     void regclass_TensorIndexMap(py::module m);
     void regclass_TensorNameMap(py::module m);
-    void regclass_InferMap(py::module m);
 }
