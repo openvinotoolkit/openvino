53558176f3414f512c97b90ae79eed9a8942a20a
Vladimir Dudnik <vladimir.dudnik@intel.com>
2021-11-27 22:28:50 +0300

    OV2.0 API C++ hello_reashe_ssd sample

--- samples/cpp/hello_reshape_ssd/main.cpp
+++ samples/cpp/hello_reshape_ssd/main.cpp
@@ -2,214 +2,159 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include <format_reader_ptr.h>
-
-#include <inference_engine.hpp>
 #include <memory>
-#include <ngraph/ngraph.hpp>
-#include <samples/common.hpp>
+#include <sstream>
 #include <string>
 #include <vector>
 
-#include "reshape_ssd_extension.hpp"
+// clang-format off
+#include "ngraph/ngraph.hpp"
+#include "openvino/openvino.hpp"
 
-using namespace InferenceEngine;
+#include "samples/args_helper.hpp"
+#include "samples/common.hpp"
+#include "samples/slog.hpp"
+#include "format_reader_ptr.h"
+#include "reshape_ssd_extension.hpp"
+// clang-format on
+
+using namespace ov::preprocess;
 
 int main(int argc, char* argv[]) {
     try {
-        // ------------------------------ Parsing and validation of input arguments
-        // ---------------------------------
+        // -------- Get OpenVINO runtime version --------
+        slog::info << ov::get_openvino_version() << slog::endl;
+
+        // --------------------------- Parsing and validation of input arguments
         if (argc != 5) {
             std::cout << "Usage : " << argv[0] << " <path_to_model> <path_to_image> <device> <batch>" << std::endl;
             return EXIT_FAILURE;
         }
-        const std::string input_model{argv[1]};
-        const std::string input_image_path{argv[2]};
+        const std::string model_path{argv[1]};
+        const std::string image_path{argv[2]};
         const std::string device_name{argv[3]};
         const size_t batch_size{std::stoul(argv[4])};
         // -----------------------------------------------------------------------------------------------------
 
-        // --------------------------- Step 1. Initialize inference engine core
-        // -------------------------------------
-        Core ie;
+        // Step 1. Initialize inference engine core
+        ov::runtime::Core core;
+        // -----------------------------------------------------------------------------------------------------
 
-        IExtensionPtr inPlaceExtension;
-        if (device_name.find("CPU") != std::string::npos) {
-            inPlaceExtension = std::make_shared<InPlaceExtension>();
-            // register sample's custom kernel (CustomReLU)
-            ie.AddExtension(inPlaceExtension);
+        // Step 2. Read a model
+        slog::info << "Loading model files: " << model_path << slog::endl;
+        auto model = core.read_model(model_path);
+        printInputAndOutputsInfo(*model);
+
+        OPENVINO_ASSERT(model->get_parameters().size() == 1, "Sample supports models with 1 input only");
+        OPENVINO_ASSERT(model->get_results().size() == 1, "Sample supports models with 1 output only");
+
+        // SSD has an additional post-processing DetectionOutput layer that simplifies output filtering,
+        // try to find it.
+        ov::NodeVector ops = model->get_ops();
+        auto it = std::find_if(ops.begin(), ops.end(), [](std::shared_ptr<ov::Node> node) {
+            return node->get_type_info() == ngraph::op::DetectionOutput::get_type_info_static();
+        });
+        if (it == ops.end()) {
+            throw std::logic_error("model does not contain DetectionOutput layer");
         }
         // -----------------------------------------------------------------------------------------------------
 
-        // Step 2. Read a model in OpenVINO Intermediate Representation (.xml and
-        // .bin files) or ONNX (.onnx file) format
-        CNNNetwork network = ie.ReadNetwork(input_model);
+        // Step 3. Configure input & output
 
-        OutputsDataMap outputs_info(network.getOutputsInfo());
-        InputsDataMap inputs_info(network.getInputsInfo());
-        if (inputs_info.size() != 1 || outputs_info.size() != 1)
-            throw std::logic_error("Sample supports clean SSD network with one input and one output");
-
-        // --------------------------- Resize network to match image sizes and given
-        // batch----------------------
-        auto input_shapes = network.getInputShapes();
-        std::string input_name;
-        SizeVector input_shape;
-        std::tie(input_name, input_shape) = *input_shapes.begin();
-        FormatReader::ReaderPtr reader(input_image_path.c_str());
+        // Read input image without resize
+        FormatReader::ReaderPtr reader(image_path.c_str());
         if (reader.get() == nullptr) {
-            std::cout << "Image " + input_image_path + " cannot be read!" << std::endl;
+            std::cout << "Image " + image_path + " cannot be read!" << std::endl;
             return 1;
         }
-        size_t image_width, image_height;
-        image_width = reader->width();
-        image_height = reader->height();
-        input_shape[0] = batch_size;
-        input_shape[2] = image_height;
-        input_shape[3] = image_width;
-        input_shapes[input_name] = input_shape;
-        std::cout << "Resizing network to the image size = [" << image_height << "x" << image_width << "] "
-                  << "with batch = " << batch_size << std::endl;
-        network.reshape(input_shapes);
-        // -----------------------------------------------------------------------------------------------------
 
-        // --------------------------- Step 3. Configure input & output
-        // ---------------------------------------------
-        // --------------------------- Prepare input blobs
-        // -----------------------------------------------------
-        InputInfo::Ptr input_info;
-        std::tie(input_name, input_info) = *inputs_info.begin();
-        // Set input layout and precision
-        input_info->setLayout(Layout::NCHW);
-        input_info->setPrecision(Precision::U8);
-        // --------------------------- Prepare output blobs
-        // ----------------------------------------------------
-        DataPtr output_info;
-        std::string output_name;
-        std::tie(output_name, output_info) = *outputs_info.begin();
-        // SSD has an additional post-processing DetectionOutput layer
-        // that simplifies output filtering, try to find it.
-        if (auto ngraphFunction = network.getFunction()) {
-            for (const auto& op : ngraphFunction->get_ops()) {
-                if (op->get_type_info() == ngraph::op::DetectionOutput::get_type_info_static()) {
-                    if (output_info->getName() != op->get_friendly_name()) {
-                        throw std::logic_error("Detection output op does not produce a network output");
-                    }
-                    break;
-                }
+        std::shared_ptr<unsigned char> image_data = reader->getData();
+        size_t image_channels = 3;
+        size_t image_width = reader->width();
+        size_t image_height = reader->height();
+
+        // assume model layout NCHW
+        const ov::Layout model_layout{"NCHW"};
+
+        // reshape model to image size and batch size
+        ov::Shape tensor_shape = model->input().get_shape();
+
+        tensor_shape[ov::layout::batch_idx(model_layout)] = batch_size;
+        tensor_shape[ov::layout::channels_idx(model_layout)] = image_channels;
+        tensor_shape[ov::layout::height_idx(model_layout)] = image_height;
+        tensor_shape[ov::layout::width_idx(model_layout)] = image_width;
+
+        std::cout << "Reshape network to the image size = [" << image_height << "x" << image_width << "] "
+                  << "with batch = " << batch_size << std::endl;
+        model->reshape({{model->input().get_any_name(), tensor_shape}});
+        printInputAndOutputsInfo(*model);
+
+        // Step 4. Apply preprocessing
+        const ov::Layout tensor_layout{"NHWC"};
+
+        // clang-format off
+        model = PrePostProcessor(model).
+            // 1) InputInfo() with no args assumes a model has a single input
+            input(InputInfo().
+                // 2) Set input tensor information:
+                // - precision of tensor is supposed to be 'u8'
+                // - layout of data is 'NHWC'
+                // - set static spatial dimensions to input tensor to resize from
+                tensor(InputTensorInfo().
+                    set_element_type(ov::element::u8).
+                    set_layout(tensor_layout)).
+                // 3) Adding explicit preprocessing steps:
+                // - convert u8 to f32
+                // - convert layout to 'NCHW' (from 'NHWC' specified above at tensor layout)
+                preprocess(PreProcessSteps().
+                    convert_element_type(ov::element::f32).
+                    convert_layout("NCHW")).
+                // 4) Here we suppose model has 'NCHW' layout for input
+                network(InputNetworkInfo().
+                    set_layout("NCHW"))).
+            output(OutputInfo().
+                tensor(OutputTensorInfo().
+                    set_element_type(ov::element::f32))).
+        // 6) Apply preprocessing modifing the original 'model'
+        build();
+        // clang-format on
+
+        // Step 5. Loading a model to the device
+        // -------------------------------------------------
+        ov::runtime::ExecutableNetwork executable_network = core.compile_model(model, device_name);
+
+        // Step 6. Create an infer request
+        ov::runtime::InferRequest infer_request = executable_network.create_infer_request();
+
+        // Step 7. Prepare input
+        ov::runtime::Tensor input_tensor = infer_request.get_input_tensor();
+
+        // copy NHWC data from image to tensor with batch
+        unsigned char* image_data_ptr = image_data.get();
+        unsigned char* tensor_data_ptr = input_tensor.data<unsigned char>();
+        size_t image_size = image_width * image_height * image_channels;
+        for (size_t b = 0; b < batch_size; b++) {
+            for (size_t i = 0; i < image_size; i++) {
+                tensor_data_ptr[b * image_size + i] = image_data_ptr[i];
             }
         }
 
-        const SizeVector output_shape = output_info->getTensorDesc().getDims();
+        // Step 8. Do inference synchronously
+        infer_request.infer();
+
+        // Step 9. Process output
+        ov::runtime::Tensor output_tensor = infer_request.get_output_tensor();
+
+        ov::Shape output_shape = model->output().get_shape();
         const size_t max_proposal_count = output_shape[2];
         const size_t object_size = output_shape[3];
-        if (object_size != 7) {
-            throw std::logic_error("Output item should have 7 as a last dimension");
-        }
-        if (output_shape.size() != 4) {
-            throw std::logic_error("Incorrect output dimensions for SSD model");
-        }
-        if (output_info == nullptr) {
-            IE_THROW() << "[SAMPLES] internal error - output information is empty";
-        }
 
-        output_info->setPrecision(Precision::FP32);
-
-        auto dumpVec = [](const SizeVector& vec) -> std::string {
-            if (vec.empty())
-                return "[]";
-            std::stringstream oss;
-            oss << "[" << vec[0];
-            for (size_t i = 1; i < vec.size(); i++)
-                oss << "," << vec[i];
-            oss << "]";
-            return oss.str();
-        };
-        std::cout << "Resulting input shape = " << dumpVec(input_shape) << std::endl;
-        std::cout << "Resulting output shape = " << dumpVec(output_shape) << std::endl;
-        // -----------------------------------------------------------------------------------------------------
-
-        // --------------------------- Step 4. Loading a model to the device
-        // ------------------------------------------
-        ExecutableNetwork executable_network = ie.LoadNetwork(network, device_name);
-        // -----------------------------------------------------------------------------------------------------
-
-        // --------------------------- Step 5. Create an infer request
-        // -------------------------------------------------
-        InferRequest infer_request = executable_network.CreateInferRequest();
-        // -----------------------------------------------------------------------------------------------------
-
-        // --------------------------- Step 6. Prepare input
-        // --------------------------------------------------------
-        /** Collect images data ptrs **/
-        std::shared_ptr<unsigned char> image_data, original_image_data;
-        /** Store image data **/
-        std::shared_ptr<unsigned char> original_data(reader->getData());
-        std::shared_ptr<unsigned char> data_reader(
-            reader->getData(input_info->getTensorDesc().getDims()[3], input_info->getTensorDesc().getDims()[2]));
-        if (data_reader.get() != nullptr) {
-            original_image_data = original_data;
-            image_data = data_reader;
-        } else {
-            throw std::logic_error("Valid input images were not found!");
-        }
-
-        /** Creating input blob **/
-        Blob::Ptr image_input = infer_request.GetBlob(input_name);
-
-        /** Filling input tensor with images. First b channel, then g and r channels **/
-        MemoryBlob::Ptr mimage = as<MemoryBlob>(image_input);
-        if (!mimage) {
-            std::cout << "We expect image blob to be inherited from MemoryBlob, but by fact we were not able "
-                         "to cast imageInput to MemoryBlob"
-                      << std::endl;
-            return 1;
-        }
-        // locked memory holder should be alive all time while access to its buffer happens
-        auto minputHolder = mimage->wmap();
-
-        size_t num_channels = mimage->getTensorDesc().getDims()[1];
-        size_t image_size = mimage->getTensorDesc().getDims()[3] * mimage->getTensorDesc().getDims()[2];
-
-        unsigned char* data = minputHolder.as<unsigned char*>();
-
-        /** Iterate over all input images **/
-        for (size_t image_id = 0; image_id < batch_size; ++image_id) {
-            /** Iterate over all pixel in image (b,g,r) **/
-            for (size_t pid = 0; pid < image_size; pid++) {
-                /** Iterate over all channels **/
-                for (size_t ch = 0; ch < num_channels; ++ch) {
-                    /**          [images stride + channels stride + pixel id ] all in bytes            **/
-                    data[image_id * image_size * num_channels + ch * image_size + pid] =
-                        image_data.get()[pid * num_channels + ch];
-                }
-            }
-        }
-
-        // -----------------------------------------------------------------------------------------------------
-
-        // --------------------------- Step 7. Do inference
-        // --------------------------------------------------------
-        infer_request.Infer();
-        // -----------------------------------------------------------------------------------------------------
-
-        // --------------------------- Step 8. Process output
-        // ------------------------------------------------------
-        Blob::Ptr output = infer_request.GetBlob(output_name);
-        MemoryBlob::CPtr moutput = as<MemoryBlob>(output);
-        if (!moutput) {
-            throw std::logic_error("We expect output to be inherited from MemoryBlob, "
-                                   "but by fact we were not able to cast output to MemoryBlob");
-        }
-        // locked memory holder should be alive all time while access to its buffer
-        // happens
-        auto moutputHolder = moutput->rmap();
-        const float* detection = moutputHolder.as<const float*>();
+        const float* detection = output_tensor.data<const float>();
 
         std::vector<std::vector<int>> boxes(batch_size);
         std::vector<std::vector<int>> classes(batch_size);
 
-        /* Each detection has image_id that denotes processed image */
+        // Each detection has image_id that denotes processed image
         for (size_t cur_proposal = 0; cur_proposal < max_proposal_count; cur_proposal++) {
             auto image_id = static_cast<int>(detection[cur_proposal * object_size + 0]);
             if (image_id < 0) {
@@ -218,47 +163,46 @@ int main(int argc, char* argv[]) {
 
             float confidence = detection[cur_proposal * object_size + 2];
             auto label = static_cast<int>(detection[cur_proposal * object_size + 1]);
-            auto xmin = detection[cur_proposal * object_size + 3] * image_width;
-            auto ymin = detection[cur_proposal * object_size + 4] * image_height;
-            auto xmax = detection[cur_proposal * object_size + 5] * image_width;
-            auto ymax = detection[cur_proposal * object_size + 6] * image_height;
+            auto xmin = static_cast<int>(detection[cur_proposal * object_size + 3] * image_width);
+            auto ymin = static_cast<int>(detection[cur_proposal * object_size + 4] * image_height);
+            auto xmax = static_cast<int>(detection[cur_proposal * object_size + 5] * image_width);
+            auto ymax = static_cast<int>(detection[cur_proposal * object_size + 6] * image_height);
 
             if (confidence > 0.5f) {
-                /** Drawing only objects with >50% probability **/
+                // Drawing only objects with >50% probability
                 classes[image_id].push_back(label);
-                boxes[image_id].push_back(static_cast<int>(xmin));
-                boxes[image_id].push_back(static_cast<int>(ymin));
-                boxes[image_id].push_back(static_cast<int>(xmax - xmin));
-                boxes[image_id].push_back(static_cast<int>(ymax - ymin));
+                boxes[image_id].push_back(xmin);
+                boxes[image_id].push_back(ymin);
+                boxes[image_id].push_back(xmax - xmin);
+                boxes[image_id].push_back(ymax - ymin);
 
-                std::cout << "[" << cur_proposal << "," << label << "] element, prob = " << confidence << ", bbox = ("
+                std::cout << "[" << cur_proposal << "," << label << "] element, prob = " << confidence << "    ("
                           << xmin << "," << ymin << ")-(" << xmax << "," << ymax << ")"
-                          << ", batch id = " << image_id << std::endl;
+                          << " batch id = " << image_id;
+                std::cout << std::endl;
             }
         }
 
         for (size_t batch_id = 0; batch_id < batch_size; ++batch_id) {
-            addRectangles(original_image_data.get(),
+            addRectangles(image_data.get(),
                           image_height,
                           image_width,
                           boxes[batch_id],
                           classes[batch_id],
                           BBOX_THICKNESS);
-            const std::string image_path = "hello_reshape_ssd_output.bmp";
-            if (writeOutputBmp(image_path, original_image_data.get(), image_height, image_width)) {
+            std::stringstream os;
+            os << "hello_reshape_ssd_batch_" << batch_id << ".bmp";
+            const std::string image_path = os.str();
+            if (writeOutputBmp(image_path, image_data.get(), image_height, image_width)) {
                 std::cout << "The resulting image was saved in the file: " + image_path << std::endl;
             } else {
                 throw std::logic_error(std::string("Can't create a file: ") + image_path);
             }
         }
-        // -----------------------------------------------------------------------------------------------------
     } catch (const std::exception& ex) {
         std::cerr << ex.what() << std::endl;
         return EXIT_FAILURE;
     }
-    std::cout << std::endl
-              << "This sample is an API example, for any performance measurements "
-                 "please use the dedicated benchmark_app tool"
-              << std::endl;
+
     return EXIT_SUCCESS;
 }
