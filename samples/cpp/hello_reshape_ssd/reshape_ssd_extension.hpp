bdd319c557e69c02ddc4fbb2ae635e55a8e4927d
Vladimir Dudnik <vladimir.dudnik@intel.com>
2021-11-27 22:47:46 +0300

    clean header

--- samples/cpp/hello_reshape_ssd/reshape_ssd_extension.hpp
+++ samples/cpp/hello_reshape_ssd/reshape_ssd_extension.hpp
@@ -2,16 +2,5 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-//#include <algorithm>
-//#include <map>
-//#include <memory>
-//#include <string>
-//#include <vector>
-
-// clang-format off
-//#include "openvino/openvino.hpp"
-//#include "ngraph/ngraph.hpp"
-// clang-format on
-
-/* thickness of a line (in pixels) to be used for bounding boxes */
+// thickness of a line (in pixels) to be used for bounding boxes
 #define BBOX_THICKNESS 2
