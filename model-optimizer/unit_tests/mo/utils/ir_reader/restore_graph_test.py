0a0967c0344f9ecc8818109eecf08670d8556d1d
Roman Kazantsev <roman.kazantsev@intel.com>
2021-11-30 23:52:25 +0300

    Test MO and IR Reader on attacking inputs
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- model-optimizer/unit_tests/mo/utils/ir_reader/restore_graph_test.py
+++ model-optimizer/unit_tests/mo/utils/ir_reader/restore_graph_test.py
@@ -13,24 +13,38 @@ from mo.utils.ir_reader.restore_graph import restore_graph_from_ir
 class TestIRReader(unittest.TestCase):
     def setUp(self):
         self.xml_bomb = b'<?xml version="1.0"?>\n' \
-                   b'<!DOCTYPE lolz [\n' \
-                   b' <!ENTITY lol "lol">\n' \
-                   b' <!ELEMENT lolz (#PCDATA)>\n' \
-                   b' <!ENTITY lol1 "&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;">\n' \
-                   b' <!ENTITY lol2 "&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;">\n' \
-                   b' <!ENTITY lol3 "&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;">\n' \
-                   b' <!ENTITY lol4 "&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;">\n' \
-                   b' <!ENTITY lol5 "&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;">\n' \
-                   b' <!ENTITY lol6 "&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;">\n' \
-                   b' <!ENTITY lol7 "&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;">\n' \
-                   b' <!ENTITY lol8 "&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;">\n' \
-                   b' <!ENTITY lol9 "&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;">\n' \
-                   b']>\n' \
-                   b'<lolz>&lol9;</lolz>'
+                        b'<!DOCTYPE lolz [\n' \
+                        b' <!ENTITY lol "lol">\n' \
+                        b' <!ELEMENT lolz (#PCDATA)>\n' \
+                        b' <!ENTITY lol1 "&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;">\n' \
+                        b' <!ENTITY lol2 "&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;">\n' \
+                        b' <!ENTITY lol3 "&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;">\n' \
+                        b' <!ENTITY lol4 "&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;">\n' \
+                        b' <!ENTITY lol5 "&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;">\n' \
+                        b' <!ENTITY lol6 "&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;">\n' \
+                        b' <!ENTITY lol7 "&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;">\n' \
+                        b' <!ENTITY lol8 "&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;">\n' \
+                        b' <!ENTITY lol9 "&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;">\n' \
+                        b']>\n' \
+                        b'<lolz>&lol9;</lolz>'
+
+        self.xml_bomb2 = b'<?xml version="1.0"?>\n' \
+                         b'<!DOCTYPE foo [\n' \
+                         b'<!ELEMENT foo ANY>\n' \
+                         b'<!ENTITY xxe SYSTEM "file:///c:/boot.ini">\n' \
+                         b']>\n' \
+                         b'<foo>&xxe;</foo>\n'
 
     def test_read_xml_bomb(self):
         bomb_file = tempfile.NamedTemporaryFile(delete=False)
         bomb_file.write(self.xml_bomb)
+        bomb_file.close()
+        self.assertRaises(EntitiesForbidden, restore_graph_from_ir, bomb_file.name)
+        os.remove(bomb_file.name)
+
+    def test_read_xml_bomb2(self):
+        bomb_file = tempfile.NamedTemporaryFile(delete=False)
+        bomb_file.write(self.xml_bomb2)
         bomb_file.close()
         self.assertRaises(EntitiesForbidden, restore_graph_from_ir, bomb_file.name)
         os.remove(bomb_file.name)
