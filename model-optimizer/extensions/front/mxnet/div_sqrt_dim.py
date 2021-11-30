1035bec01f9a53a0011d59226c18918f4eb726af
yekruglov <yegor.kruglov@intel.com>
2021-11-30 19:38:53 +0300

    fixes in div_sqrt_dim

--- model-optimizer/extensions/front/mxnet/div_sqrt_dim.py
+++ model-optimizer/extensions/front/mxnet/div_sqrt_dim.py
@@ -1,12 +1,15 @@
 # Copyright (C) 2018-2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
+import numpy as np
 
+from extensions.front.PowerToEltwises import PowerToEltwises
+from extensions.ops.Cast import Cast
 from extensions.ops.elementwise import Div
 from mo.front.common.replacement import FrontReplacementOp
 from mo.graph.graph import Graph
 from mo.ops.power import AttributedPower
 from mo.ops.shape import Shape
-from mo.utils.shape import get_shape_values_by_range_idxs
+from mo.utils.shape import node_to_get_shape_value_of_indices
 
 
 class DivSqrtDim(FrontReplacementOp):
@@ -16,6 +19,9 @@ class DivSqrtDim(FrontReplacementOp):
     op = '_contrib_div_sqrt_dim'
     enabled = True
 
+    def run_before(self):
+        return [PowerToEltwises]
+
     def replace_sub_graph(self, graph: Graph, match: dict):
         div_sqrt = match['op']
         div_sqrt_name = div_sqrt.soft_get('name', div_sqrt.id)
@@ -24,14 +30,16 @@ class DivSqrtDim(FrontReplacementOp):
         shape_node.in_port(0).connect(div_sqrt.in_port(0).get_source())
         rank_node.in_port(0).connect(shape_node.out_port(0))
 
-        shape_values_node = get_shape_values_by_range_idxs(shape=shape_node, rank=rank_node, begin=-2,
-                                                           end=-1,
-                                                           include_begin=False, include_end=True)
+        shape_values_node = node_to_get_shape_value_of_indices(shape_node=shape_node, indices=[-1])
 
-        pow_node = AttributedPower(graph, dict(name=div_sqrt_name + '/Sqrt', power=0.5)).create_node()
+        pow_node = AttributedPower(graph, dict(name=div_sqrt_name + '/Sqrt',
+                                               power=np.array(0.5, dtype=np.float32))).create_node()
+        convert_pow_input = Cast(graph, dict(dst_type=np.float32,
+                                             name=shape_values_node.name + '/ConvertToFP32')).create_node()
         div_node = Div(graph, dict(name=div_sqrt_name + "/DivSqrt")).create_node()
 
-        pow_node.in_port(0).connect(shape_values_node.out_port(0))
+        shape_values_node.out_port(0).connect(convert_pow_input.in_port(0))
+        convert_pow_input.out_port(0).connect(pow_node.in_port(0))
         div_sqrt.in_port(0).get_connection().set_destination(div_node.in_port(0))
         div_node.in_port(1).connect(pow_node.out_port(0))
         div_sqrt.out_port(0).get_connection().set_source(div_node.out_port(0))
