21b3a7ac9f1682febc6ceab8173398f0856bf86b
jiwaszki <jan.iwaszkiewicz@intel.com>
2021-12-01 18:12:17 +0000

    pimpl implementation of class and refactoring

--- inference-engine/src/inference_engine/include/openvino/runtime/async_infer_queue.hpp
+++ inference-engine/src/inference_engine/include/openvino/runtime/async_infer_queue.hpp
@@ -34,36 +34,46 @@ namespace runtime {
  */
 class OPENVINO_RUNTIME_API AsyncInferQueue {
 public:
+    AsyncInferQueue() = delete;
+
     /**
-     * @brief Constructor
+     * @brief Basic constructor. Creates InferRequests based on given ExecutableNetwork.
+     * 
+     * @param net ExecutableNetwork object which will be base for creating of InferRequests.
+     * @param jobs Number of InferRequests to be created. If equals to @p 0 AsyncInferQueue
+     * will automatically try to choose number of jobs.
      */
-    AsyncInferQueue(ov::runtime::ExecutableNetwork net, size_t jobs = 0);
+    AsyncInferQueue(ExecutableNetwork &net, size_t jobs = 0);
 
-    ~AsyncInferQueue() {
-        _requests.clear();
-    }
+    // ~AsyncInferQueue() {
+    //     _requests.clear();
+    // }
 
     /**
-     * @brief Starts asynchronous inference on next avaiable job.
+     * @brief Starts asynchronous inference on next avaiable job. Blocking call if there are no free jobs.
+     * 
+     * @param userdata Data passed to InferRequest's callback. Can be anything that matches ov::Any.
      */
-    void start_async();
+    void start_async(const ov::Any userdata = nullptr);
 
     /**
-     * @brief Starts asynchronous inference with given input data.
+     * @brief Starts asynchronous inference with given input data. Blocking call if there are no free jobs.
      *
      * @param inputs Map containing integers (index) and Tensor pairs.
+     * @param userdata Data passed to InferRequest's callback. Can be anything that matches ov::Any.
      */
-    void start_async(std::map<size_t, ov::runtime::Tensor> inputs);
+    void start_async(std::map<size_t, ov::runtime::Tensor> inputs, const ov::Any userdata = nullptr);
 
     /**
-     * @brief Starts asynchronous inference with given input data.
+     * @brief Starts asynchronous inference with given input data. Blocking call if there are no free jobs.
      *
      * @param inputs Map containing strings (names) and Tensor pairs.
+     * @param userdata Data passed to InferRequest's callback. Can be anything that matches ov::Any.
      */
-    void start_async(std::map<std::string, ov::runtime::Tensor> inputs);
+    void start_async(std::map<std::string, ov::runtime::Tensor> inputs, const ov::Any userdata = nullptr);
 
     /**
-     * @brief Waits for any request to become avaiable.
+     * @brief Waits for any request to become avaiable. Blocking call if there are no free jobs.
      *
      * @note Waits for InferRequest while queue is running.
      * @return Returns @p true if at least one request is avaiable, otherwise @p false.
@@ -71,7 +81,8 @@ public:
     bool is_ready();
 
     /**
-     * @brief Waits for all jobs to be finished.
+     * @brief Waits for all jobs to be finished. This is a blocking call for caller thread until
+     * all jobs are finished.
      */
     void wait_all();
 
@@ -82,38 +93,31 @@ public:
      */
     size_t get_idle_id();
 
-    // Should this be a pointer?
-    // ov::runtime::InferRequest* get_idle_request();
-    // std::vector<ov::runtime::InferRequest*> get_requests();
-
     /**
      * @brief Set callback function that will be called on success or failure for all jobs.
      *
      * @param callback callback object which will be called on when inference finish.
      * @note Callback function differs from single InferRequest's set_callback function. There is
-     * a requirement to use InferRequest as an argument. This allows queue's internals 
-     * to pass underlaying request's object back to the user (for example: to get output tensors).
+     * a requirement to use InferRequest and Any as an argument. This allows queue's internals 
+     * to pass underlaying request's object back to the user (for example: to get output tensors),
+     * as well as providing additional data passing to the callback function.
+     * 
+     * It is NOT recommended to start another inference inside callback! (dodać o problemach itp)
      */
-    void set_callback(std::function<void(std::exception_ptr, ov::runtime::InferRequest&)> callback);
-
-protected:
-    /**
-     * @brief Gets id of the first idle job in queue and pops it of queue.
-     *
-     * @return Returns integer.
-     */
-    size_t pop_idle_id();
+    void set_callback(std::function<void(std::exception_ptr, ov::runtime::InferRequest&, const ov::Any&)> callback);
 
     /**
-     * @brief Set callback function that controls flow for the queue.
+     * @brief Get InferRequest under given id from jobs pool.
+     * 
+     * @param i Specific job id.
+     * 
+     * @return Returns InferRequest from pool.
      */
-    void set_default_callback();
+    InferRequest& operator[](size_t i);
 
-    std::vector<ov::runtime::InferRequest> _requests;
-    std::queue<size_t> _idle_handles;
-    std::mutex _mutex;
-    std::condition_variable _cv;
-    // std::vector<ov::Any> _user_ids;
+private:
+    class Impl;
+    std::unique_ptr<Impl, void (*)(Impl*)> m_pimpl; // = nullptr;
 };
 
 }  // namespace runtime
--- inference-engine/src/inference_engine/src/cpp/async_infer_queue.cpp
+++ inference-engine/src/inference_engine/src/cpp/async_infer_queue.cpp
@@ -15,120 +15,198 @@
 
 namespace ov {
 namespace runtime {
+class AsyncInferQueue::Impl {
+public:
+    Impl() = delete;
 
-AsyncInferQueue::AsyncInferQueue(ov::runtime::ExecutableNetwork net, size_t jobs) {
-    // Automatically set number of jobs
-    if (jobs <= 0) {
-        try {
-            auto parameter_value = net.get_metric(METRIC_KEY(SUPPORTED_METRICS));
-            auto supported_metrics = parameter_value.as<std::vector<std::string>>();
-            const std::string key = METRIC_KEY(OPTIMAL_NUMBER_OF_INFER_REQUESTS);
-            if (std::find(supported_metrics.begin(), supported_metrics.end(), key) != supported_metrics.end()) {
-                parameter_value = net.get_metric(key);
-                if (parameter_value.is<unsigned int>())
-                    jobs = parameter_value.as<unsigned int>();
-                else
-                    IE_THROW() << "Unsupported format for " << key << "!"
-                               << " Please specify number of infer requests directly!";
-            } else {
-                IE_THROW() << "Can't load network: " << key << " is not supported!"
-                           << " Please specify number of infer requests directly!";
+    Impl(ExecutableNetwork &net, size_t jobs) {
+        // Automatically set number of jobs
+        if (jobs == 0) {
+            try {
+                auto parameter_value = net.get_metric(METRIC_KEY(SUPPORTED_METRICS));
+                auto supported_metrics = parameter_value.as<std::vector<std::string>>();
+                const std::string key = METRIC_KEY(OPTIMAL_NUMBER_OF_INFER_REQUESTS);
+                if (std::find(supported_metrics.begin(), supported_metrics.end(), key) != supported_metrics.end()) {
+                    parameter_value = net.get_metric(key);
+                    if (parameter_value.is<unsigned int>())
+                        jobs = parameter_value.as<unsigned int>();
+                    else
+                        throw ov::Exception("Unsupported format for " + key +
+                                            "! Please specify number of jobs explicitly!");
+                } else {
+                    ov::Exception("Can't load model: " + key +
+                                  " is not supported! Please specify number of jobs explicitly!");
+                }
+            } catch (const std::exception& ex) {
+                ov::Exception(ex.what());
             }
-        } catch (const std::exception& ex) {
-            IE_THROW() << "Can't load network: " << ex.what() << " Please specify number of infer requests directly!";
         }
+
+        for (size_t handle = 0; handle < jobs; handle++) {
+            m_pool.push_back(net.create_infer_request());
+            m_idle_handles.push(handle);
+            m_userdata.push_back(nullptr);
+        }
+
+        set_default_callback();
     }
 
-    for (size_t handle = 0; handle < jobs; handle++) {
-        _requests.push_back(net.create_infer_request());
-        _idle_handles.push(handle);
-    }
+    InferRequest& get_request(size_t i);
 
-    set_default_callback();
+    size_t get_idle_id();
+    size_t pop_idle_id();
+
+    void start_async(const ov::Any userdata);
+    void start_async(std::map<size_t, ov::runtime::Tensor> &inputs, const ov::Any userdata);
+    void start_async(std::map<std::string, ov::runtime::Tensor> &inputs, const ov::Any userdata);
+
+    bool is_ready();
+    void wait_all();
+
+    void set_callback(std::function<void(std::exception_ptr, ov::runtime::InferRequest&, const ov::Any&)> callback);
+
+private:
+    void set_default_callback();
+
+    std::vector<ov::runtime::InferRequest> m_pool; // pool of jobs (InferRequests)
+    std::queue<size_t> m_idle_handles; // idle handles of requests
+    std::mutex m_mutex; 
+    std::condition_variable m_cv;
+    std::vector<ov::Any> m_userdata;
+};
+
+InferRequest& AsyncInferQueue::Impl::get_request(size_t i) {
+    return m_pool[i];
 }
 
-size_t AsyncInferQueue::get_idle_id() {
-    std::unique_lock<std::mutex> lock(_mutex);
-    _cv.wait(lock, [this] {
-        return !(_idle_handles.empty());
+size_t AsyncInferQueue::Impl::get_idle_id() {
+    std::unique_lock<std::mutex> lock(m_mutex);
+    m_cv.wait(lock, [this] {
+        return !m_idle_handles.empty();
     });
 
-    size_t request_id = _idle_handles.front();
+    size_t request_id = m_idle_handles.front();
 
     return request_id;
 }
 
-size_t AsyncInferQueue::pop_idle_id() {
+size_t AsyncInferQueue::Impl::pop_idle_id() {
     // Get idle request id and pop it from queue
     size_t idle_request_id = get_idle_id();
-    _idle_handles.pop();
+    std::unique_lock<std::mutex> lock(m_mutex);
+    m_idle_handles.pop();
 
     return idle_request_id;
 }
 
-void AsyncInferQueue::start_async() {
-    _requests[AsyncInferQueue::pop_idle_id()].start_async();
+void AsyncInferQueue::Impl::start_async(ov::Any userdata) {
+    auto request_id = pop_idle_id();
+    if (!userdata.empty()) {
+        m_userdata[request_id] = userdata;
+    }
+    m_pool[request_id].start_async();
 }
 
-void AsyncInferQueue::start_async(std::map<size_t, ov::runtime::Tensor> inputs) {
+void AsyncInferQueue::Impl::start_async(std::map<size_t, ov::runtime::Tensor> &inputs, ov::Any userdata) {
     size_t request_id = pop_idle_id();
-
-    for (auto const& t : inputs) {
-        _requests[request_id].set_input_tensor(t.first, t.second);
+    if (!userdata.empty()) {
+        m_userdata[request_id] = userdata;
     }
-    _requests[request_id].start_async();
+    for (auto const& t : inputs) {
+        m_pool[request_id].set_input_tensor(t.first, t.second);
+    }
+    m_pool[request_id].start_async();
 }
 
-void AsyncInferQueue::start_async(std::map<std::string, ov::runtime::Tensor> inputs) {
+void AsyncInferQueue::Impl::start_async(std::map<std::string, ov::runtime::Tensor> &inputs, ov::Any userdata) {
     size_t request_id = pop_idle_id();
-
-    for (auto const& t : inputs) {
-        _requests[request_id].set_tensor(t.first, t.second);
+    if (!userdata.empty()) {
+        m_userdata[request_id] = userdata;
     }
+    for (auto const& t : inputs) {
+        m_pool[request_id].set_tensor(t.first, t.second);
+    }
+    m_pool[request_id].start_async();
+}
 
-    _requests[request_id].start_async();
+bool AsyncInferQueue::Impl::is_ready() {
+    std::unique_lock<std::mutex> lock(m_mutex);
+    m_cv.wait(lock, [this] {
+        return !m_idle_handles.empty();
+    });
+
+    return !(m_idle_handles.empty());
+}
+
+void AsyncInferQueue::Impl::wait_all() {
+    std::unique_lock<std::mutex> lock(m_mutex);
+    m_cv.wait(lock, [this] {
+        return m_idle_handles.size() == m_pool.size();
+    });
+}
+
+void AsyncInferQueue::Impl::set_default_callback() {
+    for (size_t handle = 0; handle < m_pool.size(); handle++) {
+        m_pool[handle].set_callback([this, handle /* ... */](std::exception_ptr e) {
+            // Add idle handle to queue
+            std::unique_lock<std::mutex> lock(m_mutex);
+            m_idle_handles.push(handle);
+            // Notify locks
+            m_cv.notify_one();
+        });
+    }
+}
+
+void AsyncInferQueue::Impl::set_callback(
+    std::function<void(std::exception_ptr, ov::runtime::InferRequest&, const ov::Any&)> callback) {
+    for (size_t handle = 0; handle < m_pool.size(); handle++) {
+        m_pool[handle].set_callback([this, callback, handle /* ... */](std::exception_ptr e) {
+            callback(e, m_pool[handle], m_userdata[handle]);
+            // Add idle handle to queue
+            std::unique_lock<std::mutex> lock(m_mutex);
+            m_idle_handles.push(handle);
+            // Notify locks
+            m_cv.notify_one();
+        });
+    }
+}
+
+AsyncInferQueue::AsyncInferQueue(ExecutableNetwork &net, size_t jobs)
+    : m_pimpl{new Impl{net, jobs}, [](Impl* impl) {
+                  delete impl;
+              }} {}
+
+size_t AsyncInferQueue::get_idle_id() {
+    return m_pimpl->get_idle_id();
+}
+
+void AsyncInferQueue::start_async(const ov::Any userdata) {
+    m_pimpl->start_async(userdata);
+}
+
+void AsyncInferQueue::start_async(std::map<size_t, ov::runtime::Tensor> inputs, const ov::Any userdata) {
+    m_pimpl->start_async(inputs, userdata);
+}
+
+void AsyncInferQueue::start_async(std::map<std::string, ov::runtime::Tensor> inputs, const ov::Any userdata) {
+    m_pimpl->start_async(inputs, userdata);
 }
 
 bool AsyncInferQueue::is_ready() {
-    std::unique_lock<std::mutex> lock(_mutex);
-    _cv.wait(lock, [this] {
-        return !(_idle_handles.empty());
-    });
-
-    return !(_idle_handles.empty());
+    return m_pimpl->is_ready();
 }
 
 void AsyncInferQueue::wait_all() {
-    std::unique_lock<std::mutex> lock(_mutex);
-    _cv.wait(lock, [this] {
-        return _idle_handles.size() == _requests.size();
-    });
+    m_pimpl->wait_all();
 }
 
-// ov::runtime::InferRequest* get_idle_request();
-// std::vector<ov::runtime::InferRequest*> get_requests();
-
-void AsyncInferQueue::set_default_callback() {
-    for (size_t handle = 0; handle < _requests.size(); handle++) {
-        _requests[handle].set_callback([this, handle /* ... */](std::exception_ptr e) {
-            // Add idle handle to queue
-            _idle_handles.push(handle);
-            // Notify locks
-            _cv.notify_one();
-        });
-    }
+void AsyncInferQueue::set_callback(
+    std::function<void(std::exception_ptr, ov::runtime::InferRequest&, const ov::Any&)> callback) {
+    m_pimpl->set_callback(callback);
 }
 
-void AsyncInferQueue::set_callback(std::function<void(std::exception_ptr, ov::runtime::InferRequest&)> callback) {
-    for (size_t handle = 0; handle < _requests.size(); handle++) {
-        _requests[handle].set_callback([this, callback, handle /* ... */](std::exception_ptr e) {
-            callback(e, _requests[handle]);
-            // Add idle handle to queue
-            _idle_handles.push(handle);
-            // Notify locks
-            _cv.notify_one();
-        });
-    }
+InferRequest& AsyncInferQueue::operator[](size_t i) {
+    return m_pimpl->get_request(i);
 }
 
 }  // namespace runtime
