d2ad4ca8d8183f4d3590d69362ef4bebdcfed2cf
jiwaszki <jan.iwaszkiewicz@intel.com>
2021-12-01 18:12:46 +0000

    added new test scenarios for queue

--- inference-engine/tests/functional/inference_engine/ov_async_infer_queue.cpp
+++ inference-engine/tests/functional/inference_engine/ov_async_infer_queue.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
 
 #include <map>
 
@@ -11,6 +12,7 @@
 #include <openvino/core/type/element_type.hpp>
 
 #include <openvino/op/add.hpp>
+#include <openvino/op/multiply.hpp>
 #include <openvino/op/parameter.hpp>
 
 #include <openvino/runtime/core.hpp>
@@ -21,47 +23,84 @@
 
 using namespace ::testing;
 using namespace std;
-using namespace InferenceEngine;
-using namespace InferenceEngine::details;
 
-TEST(AsyncInferQueueOVTests, flowTest) {
-    ov::Shape input_shape{8};
-
-    auto arg0 = make_shared<ov::op::v0::Parameter>(ov::element::f32, input_shape);
-    auto arg1 = make_shared<ov::op::v0::Parameter>(ov::element::f32, input_shape);
+std::shared_ptr<ov::Function> create_add_model(ov::Shape shape) {
+    auto arg0 = make_shared<ov::op::v0::Parameter>(ov::element::f32, shape);
+    auto arg1 = make_shared<ov::op::v0::Parameter>(ov::element::f32, shape);
     auto add = make_shared<ov::op::v1::Add>(arg0, arg1);
-    ASSERT_EQ(add->input_value(0).get_node_shared_ptr(), arg0);
-    ASSERT_EQ(add->input_value(1).get_node_shared_ptr(), arg1);
 
-    auto model = make_shared<ov::Function>(add, ov::ParameterVector{arg0, arg1});
+    return make_shared<ov::Function>(add, ov::ParameterVector{arg0, arg1});
+}
 
+std::shared_ptr<ov::Function> create_mul_model(ov::Shape shape) {
+    auto arg0 = make_shared<ov::op::v0::Parameter>(ov::element::f32, shape);
+    auto arg1 = make_shared<ov::op::v0::Parameter>(ov::element::f32, shape);
+    auto mul = make_shared<ov::op::v1::Multiply>(arg0, arg1);
+
+    return make_shared<ov::Function>(mul, ov::ParameterVector{arg0, arg1});
+}
+
+TEST(AsyncInferQueueOVTests, BasicFlowTest) {
     ov::runtime::Core core;
-    ov::runtime::ExecutableNetwork net = core.compile_model(model, "CPU");
+    ov::runtime::ExecutableNetwork net =
+        core.compile_model(create_add_model({8}), "CPU");
 
-    ov::runtime::AsyncInferQueue my_first_queue(net, 6);
+    ov::runtime::AsyncInferQueue basic_queue(net);
 
     size_t counter = 0;
-    size_t *counter_ptr = &counter;
+    std::vector<ov::Any> callback_data;
 
-    auto f = [counter_ptr](std::exception_ptr e, ov::runtime::InferRequest &request) {
-        auto tensor = request.get_output_tensor();
-        float* data_ptr = reinterpret_cast<float*>(tensor.data());
-        vector<float> values(data_ptr, data_ptr + tensor.get_size());
-
-        *counter_ptr += 1;
-
-        for (auto i : values) {
-            std::cout << i << ' ';
-        }
-        std::cout << "request: " << &request << std::endl;
+    auto f = [&counter, &callback_data](std::exception_ptr e,
+                                        ov::runtime::InferRequest &request,
+                                        const ov::Any &data) {
+        counter += 1;
+        callback_data.push_back(data);
     };
 
-    my_first_queue.set_callback(f);
+    basic_queue.set_callback(f);
 
-    size_t num_of_runs = 16;
+    size_t num_of_runs = 4;
+
+    for (size_t i = 0; i < num_of_runs; i++) {
+        basic_queue.start_async(i);
+    }
+
+    basic_queue.wait_all();
+
+    std::vector<size_t> converted_data;
+    std::transform(callback_data.begin(), callback_data.end(),
+                   std::back_inserter(converted_data),
+                   [](ov::Any val) -> std::size_t { return val.as<size_t>(); });
+
+    ASSERT_EQ(counter, num_of_runs);
+    ASSERT_THAT(converted_data, UnorderedElementsAreArray({0, 1, 2, 3}));
+}
+
+TEST(AsyncInferQueueOVTests, AccessRequestInsideCallbackTest) {
+    size_t num_of_runs = 5;
+    ov::Shape input_shape{1};
+    std::vector<float> results;
+    std::vector<float> expected_results;
+
+    ov::runtime::Core core;
+    ov::runtime::ExecutableNetwork net =
+        core.compile_model(create_add_model(input_shape), "CPU");
+
+    ov::runtime::AsyncInferQueue basic_queue(net, 3);
+
+    auto f = [&results](std::exception_ptr e,
+                        ov::runtime::InferRequest &request, const ov::Any &data) {
+        float* data_ptr = request.get_output_tensor().data<float>();
+        results.push_back(data_ptr[0]);
+    };
+
+    basic_queue.set_callback(f);
 
     for (size_t i = 0; i < num_of_runs; i++) {
         std::vector<float> input_data(input_shape[0], i);
+
+        expected_results.push_back(i + i);
+
         auto t = ov::runtime::Tensor(ov::element::f32, input_shape);
         std::memcpy(t.data(), reinterpret_cast<void*>(&input_data[0]), t.get_byte_size());
 
@@ -69,14 +108,152 @@ TEST(AsyncInferQueueOVTests, flowTest) {
         my_map.insert({0, t});
         my_map.insert({1, t});
 
-        my_first_queue.start_async(my_map);
+        basic_queue.start_async(my_map);
     }
 
-    std::cout << "counter: " << counter << std::endl;
+    basic_queue.wait_all();
 
-    my_first_queue.wait_all();
+    ASSERT_THAT(results, UnorderedElementsAreArray(expected_results));
+}
 
-    std::cout << "counter: " << counter << std::endl;
+TEST(AsyncInferQueueOVTests, AccessRequestOutsideCallbackTest) {
+    size_t num_of_runs = 6;
+    ov::Shape input_shape{1};
+    std::vector<float> results;
+    std::vector<float> expected_results;
 
-    ASSERT_EQ(counter, num_of_runs);
+    ov::runtime::Core core;
+    ov::runtime::ExecutableNetwork net =
+        core.compile_model(create_add_model(input_shape), "CPU");
+
+    ov::runtime::AsyncInferQueue basic_queue(net, num_of_runs);
+
+    for (size_t i = 0; i < num_of_runs; i++) {
+        std::vector<float> input_data(input_shape[0], i);
+
+        expected_results.push_back(i + i);
+
+        auto t = ov::runtime::Tensor(ov::element::f32, input_shape);
+        std::memcpy(t.data(), reinterpret_cast<void*>(&input_data[0]), t.get_byte_size());
+
+        std::map<size_t, ov::runtime::Tensor> my_map;
+        my_map.insert({0, t});
+        my_map.insert({1, t});
+
+        basic_queue.start_async(my_map);
+    }
+
+    basic_queue.wait_all();
+
+    for (size_t i = 0; i < num_of_runs; i++) {
+        float* data_ptr = basic_queue[i].get_output_tensor().data<float>();
+        results.push_back(data_ptr[0]);
+    }
+
+    ASSERT_THAT(results, UnorderedElementsAreArray(expected_results));
+}
+
+TEST(AsyncInferQueueOVTests, AccessInferRequestInLoop) {
+    size_t num_of_runs = 4;
+    ov::Shape input_shape{1};
+    std::vector<float> results;
+    std::vector<float> expected_results;
+
+    ov::runtime::Core core;
+    ov::runtime::ExecutableNetwork net =
+        core.compile_model(create_mul_model(input_shape), "CPU");
+
+    ov::runtime::AsyncInferQueue basic_queue(net);
+
+    auto f = [&results](std::exception_ptr e,
+                        ov::runtime::InferRequest &request, const ov::Any &data) {
+        float *data_ptr = request.get_output_tensor().data<float>();
+        results.push_back(data_ptr[0]);
+    };
+
+    basic_queue.set_callback(f);
+
+    for (size_t i = 0; i < num_of_runs; i++) {
+        if (basic_queue.is_ready()) {
+            std::vector<float> input_data(input_shape[0], i);
+
+            expected_results.push_back(i * i);
+
+            auto t = ov::runtime::Tensor(ov::element::f32, input_shape);
+            std::memcpy(t.data(), reinterpret_cast<void*>(&input_data[0]), t.get_byte_size());
+
+            auto idle_id = basic_queue.get_idle_id();
+
+            basic_queue[idle_id].set_input_tensor(0, t);
+            basic_queue[idle_id].set_input_tensor(1, t);
+
+            basic_queue.start_async();
+        }
+    }
+
+    basic_queue.wait_all();
+
+    ASSERT_THAT(results, UnorderedElementsAreArray(expected_results));
+}
+
+TEST(AsyncInferQueueOVTests, ConnectAsyncInferQueuesTest) {
+    size_t num_of_runs = 8;
+    ov::Shape input_shape{1};
+    std::vector<float> results;
+    std::vector<float> expected_results;
+
+    ov::runtime::Core core;
+    ov::runtime::ExecutableNetwork add_net =
+        core.compile_model(create_add_model(input_shape), "CPU");
+
+   ov::runtime::ExecutableNetwork mul_net =
+        core.compile_model(create_mul_model(input_shape), "CPU");
+
+    ov::runtime::AsyncInferQueue add_queue(add_net, 4);
+    ov::runtime::AsyncInferQueue mul_queue(mul_net, 2);
+
+    auto f = [input_shape, &mul_queue](std::exception_ptr e,
+                        ov::runtime::InferRequest &request, const ov::Any &data) {
+        float *data_ptr = request.get_output_tensor().data<float>();
+        auto t = ov::runtime::Tensor(ov::element::f32, input_shape);
+        std::memcpy(t.data(), reinterpret_cast<void*>(&data_ptr[0]), t.get_byte_size());
+
+        std::map<size_t, ov::runtime::Tensor> tmp;
+        tmp.insert({0, t});
+        tmp.insert({1, t});
+
+        mul_queue.start_async(tmp);
+    };
+
+    add_queue.set_callback(f);
+
+    auto h = [&results](std::exception_ptr e,
+                        ov::runtime::InferRequest &request, const ov::Any &data) {
+        float *data_ptr = request.get_output_tensor().data<float>();
+        results.push_back(data_ptr[0]);
+    };
+
+    mul_queue.set_callback(h);
+
+    for (size_t i = 0; i < num_of_runs; i++) {
+        if (add_queue.is_ready()) {
+            std::vector<float> input_data(input_shape[0], i);
+
+            expected_results.push_back((i + i) * (i + i));
+
+            auto t = ov::runtime::Tensor(ov::element::f32, input_shape);
+            std::memcpy(t.data(), reinterpret_cast<void*>(&input_data[0]), t.get_byte_size());
+
+            std::map<size_t, ov::runtime::Tensor> my_map;
+            my_map.insert({0, t});
+            my_map.insert({1, t});
+
+            add_queue.start_async(my_map);
+        }
+    }
+
+    add_queue.wait_all();
+    mul_queue.wait_all();
+
+    ASSERT_THAT(results, UnorderedElementsAreArray(expected_results));
 }
