e67a46cc3ef3347bf7259e30da275bbd8811d807
Ilya Lavrenov <ilya.lavrenov@intel.com>
2021-12-03 18:48:43 +0300

    Start point for debian packages

--- cmake/developer_package/packaging.cmake
+++ cmake/developer_package/packaging.cmake
@@ -12,13 +12,13 @@ include(CPackComponent)
 #
 function(ie_cpack_set_library_dir)
     if(WIN32)
-        set(IE_CPACK_LIBRARY_PATH runtime/lib/${ARCH_FOLDER}/$<CONFIG> PARENT_SCOPE)
-        set(IE_CPACK_RUNTIME_PATH runtime/bin/${ARCH_FOLDER}/$<CONFIG> PARENT_SCOPE)
-        set(IE_CPACK_ARCHIVE_PATH runtime/lib/${ARCH_FOLDER}/$<CONFIG> PARENT_SCOPE)
+        set(IE_CPACK_LIBRARY_PATH lib/${ARCH_FOLDER}/$<CONFIG> PARENT_SCOPE)
+        set(IE_CPACK_RUNTIME_PATH bin/${ARCH_FOLDER}/$<CONFIG> PARENT_SCOPE)
+        set(IE_CPACK_ARCHIVE_PATH lib/${ARCH_FOLDER}/$<CONFIG> PARENT_SCOPE)
     else()
-        set(IE_CPACK_LIBRARY_PATH runtime/lib/${ARCH_FOLDER} PARENT_SCOPE)
-        set(IE_CPACK_RUNTIME_PATH runtime/lib/${ARCH_FOLDER} PARENT_SCOPE)
-        set(IE_CPACK_ARCHIVE_PATH runtime/lib/${ARCH_FOLDER} PARENT_SCOPE)
+        set(IE_CPACK_LIBRARY_PATH lib/${ARCH_FOLDER} PARENT_SCOPE)
+        set(IE_CPACK_RUNTIME_PATH lib/${ARCH_FOLDER} PARENT_SCOPE)
+        set(IE_CPACK_ARCHIVE_PATH lib/${ARCH_FOLDER} PARENT_SCOPE)
     endif()
 endfunction()
 
@@ -43,25 +43,30 @@ if(ENABLE_TESTS)
 endif()
 
 macro(ie_cpack)
-    set(CPACK_GENERATOR "TGZ")
-    set(CPACK_SOURCE_GENERATOR "")
-    set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "OpenVINO toolkit")
+    if(NOT DEFINED CPACK_GENERATOR)
+        set(CPACK_GENERATOR "TGZ")
+    endif()
+    set(CPACK_SOURCE_GENERATOR "") # not used
+    set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "OpenVINO™ Toolkit")
     set(CPACK_COMPONENT_UNSPECIFIED_REQUIRED OFF)
     set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY OFF)
-    set(CPACK_ARCHIVE_COMPONENT_INSTALL ON) # multiple components
     set(CPACK_PACKAGE_VENDOR "Intel Corporation")
+    set(CPACK_PACKAGE_CONTACT "openvino@intel.com")
     set(CPACK_VERBATIM_VARIABLES ON)
     set(CPACK_COMPONENTS_ALL ${ARGN})
+
+    # archive operations can be run in parallels since CMake 3.20
+    set(CPACK_THREADS 8)
+
     if (NOT DEFINED CPACK_STRIP_FILES)
         set(CPACK_STRIP_FILES ON)
     endif()
-    set(CPACK_THREADS 8)
 
-    string(REPLACE "/" "_" CPACK_PACKAGE_VERSION "${CI_BUILD_NUMBER}")
+    string(REPLACE "/" "_" CPACK_PACKAGE_VERSION "2022.1.1")
     if(WIN32)
-        set(CPACK_PACKAGE_NAME inference-engine_${CMAKE_BUILD_TYPE})
+        set(CPACK_PACKAGE_NAME openvino_${CMAKE_BUILD_TYPE})
     else()
-        set(CPACK_PACKAGE_NAME inference-engine)
+        set(CPACK_PACKAGE_NAME openvino)
     endif()
 
     foreach(ver IN LISTS MAJOR MINOR PATCH)
@@ -72,6 +77,35 @@ macro(ie_cpack)
 
     if(OS_FOLDER)
         set(CPACK_SYSTEM_NAME "${OS_FOLDER}")
+    endif()
+
+    # generator specific variables
+    if(CPACK_GENERATOR MATCHES "^(7Z|TBZ2|TGZ|TXZ|TZ|ZIP)$")
+        message(FATAL_ERROR "CPACK_GENERATORCPACK_GENERATORCPACK_GENERATOR")
+        # multiple packages are generated
+        set(CPACK_ARCHIVE_COMPONENT_INSTALL ON)
+    elseif(CPACK_GENERATOR STREQUAL "DEB")
+        # per component configuration
+        # list of components:
+        # - ngraph
+        # - core
+        # - core_c
+        # - ngraph_dev
+        # - core_dev
+        # - core_c_dev
+        # - licensing
+        # - docs
+        # - core_tools
+        # - [python .*]
+        # - (cpu|gpu|hetero)
+
+        # multiple packages are generated
+        set(CPACK_DEB_COMPONENT_INSTALL ON)
+        # automatically find dependencies for binaries
+        set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
+
+        # ngraph
+        set(CPACK_DEBIAN_ngraph_PACKAGE_NAME "openvino-ngraph")
     endif()
 
     include(CPack)
