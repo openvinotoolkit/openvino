12a73a2494382c81bff71bf1b6973a180702baa2
akuporos <anastasia.kuporosova@intel.com>
2021-12-03 17:16:56 +0300

    fix code style

--- src/bindings/python/src/pyopenvino/test_utils/test_utils.cpp
+++ src/bindings/python/src/pyopenvino/test_utils/test_utils.cpp
@@ -11,15 +11,16 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(test_utils_api, m) {
-    m.def("compare_functions",
-            [](std::shared_ptr<ov::Function> lhs, std::shared_ptr<ov::Function> rhs) {
-                const auto fc = FunctionsComparator::with_default()
-                        .enable(FunctionsComparator::ATTRIBUTES)
-                        .enable(FunctionsComparator::CONST_VALUES);
+    m.def(
+        "compare_functions",
+        [](std::shared_ptr<ov::Function> lhs, std::shared_ptr<ov::Function> rhs) {
+            const auto fc = FunctionsComparator::with_default()
+                                .enable(FunctionsComparator::ATTRIBUTES)
+                                .enable(FunctionsComparator::CONST_VALUES);
 
-                const auto results = fc.compare(lhs, rhs);
-                return std::make_pair(results.valid, results.message);
-            },
-            py::arg("lhs"),
-            py::arg("rhs"));
+            const auto results = fc.compare(lhs, rhs);
+            return std::make_pair(results.valid, results.message);
+        },
+        py::arg("lhs"),
+        py::arg("rhs"));
 }
