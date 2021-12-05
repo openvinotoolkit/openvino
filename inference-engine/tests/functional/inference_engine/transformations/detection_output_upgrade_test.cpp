4c1c0fd65433fde01a858f6aec8a41c3aac08dd9
Roman Kazantsev <roman.kazantsev@intel.com>
2021-12-05 20:01:24 +0300

    Use the same test suite name for test mixture
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- inference-engine/tests/functional/inference_engine/transformations/detection_output_upgrade_test.cpp
+++ inference-engine/tests/functional/inference_engine/transformations/detection_output_upgrade_test.cpp
@@ -49,7 +49,7 @@ void create_attributes_vectors(std::vector<opset1::DetectionOutput::Attributes>&
 }
 }  // namespace
 
-TEST_F(TransformationTests, DetectionOutput1ToDetectionOutput8) {
+TEST_F(TransformationTestsF, DetectionOutput1ToDetectionOutput8) {
     std::vector<opset1::DetectionOutput::Attributes> attrs_v1_vector;
     std::vector<opset8::DetectionOutput::Attributes> attrs_v8_vector;
     Dimension N = 5;
@@ -110,7 +110,7 @@ TEST_F(TransformationTests, DetectionOutput1ToDetectionOutput8) {
     }
 }
 
-TEST_F(TransformationTests, DetectionOutput1ToDetectionOutput8FiveArguments) {
+TEST_F(TransformationTestsF, DetectionOutput1ToDetectionOutput8FiveArguments) {
     // In this case num_classes attribute value is deduced using inputs shapes
     std::vector<opset1::DetectionOutput::Attributes> attrs_v1_vector;
     std::vector<opset8::DetectionOutput::Attributes> attrs_v8_vector;
