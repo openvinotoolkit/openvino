e21e805ed19e223b8a2b81619b1661c1644d1811
mangguo <mang.guo@intel.com>
2021-12-07 03:53:27 -0500

    Use shape_inference in test case

--- inference-engine/tests/unit/cpu/shape_inference_test/broadcast_base_shape_inference.cpp
+++ inference-engine/tests/unit/cpu/shape_inference_test/broadcast_base_shape_inference.cpp
@@ -21,26 +21,26 @@ TEST(StaticShapeInferenceTest, BroadcastBidirectionalTest) {
     int32_t target_shape_val[] = {1, 16, 50, 50};
     std::map<size_t, std::shared_ptr<ngraph::runtime::HostTensor>> constant_data;
     constant_data[1] =
-        std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, Shape{4}, target_shape_val);
+        std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, ov::Shape{4}, target_shape_val);
 
     std::vector<StaticShape> static_input_shapes = {StaticShape{16, 1, 1}, StaticShape{4}},
                              static_output_shapes = {StaticShape{}};
-    shape_infer(broadcast_v3.get(), static_input_shapes, static_output_shapes, constant_data);
+    shape_inference(broadcast_v3.get(), static_input_shapes, static_output_shapes, constant_data);
     ASSERT_EQ(static_output_shapes[0], StaticShape({1, 16, 50, 50}));
 
     static_input_shapes = {StaticShape{16, 1, 1}, StaticShape{4}};
     static_output_shapes = {StaticShape{}};
-    EXPECT_THROW(shape_infer(broadcast_v3.get(), static_input_shapes, static_output_shapes, {}), NodeValidationFailure);
+    EXPECT_THROW(shape_inference(broadcast_v3.get(), static_input_shapes, static_output_shapes, {}), NodeValidationFailure);
 }
 
 TEST(StaticShapeInferenceTest, BroadcastBidirectionalConstantTest) {
     auto input = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1, -1});
-    auto target_shape = std::make_shared<ov::op::v0::Constant>(element::i32, Shape{3}, std::vector<int32_t>{16, 1, 40});
+    auto target_shape = std::make_shared<ov::op::v0::Constant>(element::i32, ov::Shape{3}, std::vector<int32_t>{16, 1, 40});
     auto broadcast_v3 = std::make_shared<op::v3::Broadcast>(input, target_shape, op::BroadcastType::BIDIRECTIONAL);
 
     std::vector<StaticShape> static_input_shapes = {StaticShape{1, 16, 50, 1}, StaticShape{3}},
                              static_output_shapes = {StaticShape{}};
-    shape_infer(broadcast_v3.get(), static_input_shapes, static_output_shapes, {});
+    shape_inference(broadcast_v3.get(), static_input_shapes, static_output_shapes, {});
     ASSERT_EQ(static_output_shapes[0], StaticShape({1, 16, 50, 40}));
 }
 
@@ -53,27 +53,27 @@ TEST(StaticShapeInferenceTest, BroadcastPDPDTest) {
     int32_t target_shape_val[] = {2, 3, 6};
     std::map<size_t, std::shared_ptr<ngraph::runtime::HostTensor>> constant_data;
     constant_data[1] =
-        std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, Shape{3}, target_shape_val);
+        std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, ov::Shape{3}, target_shape_val);
 
     std::vector<StaticShape> static_input_shapes = {StaticShape{3, 1}, StaticShape{3}},
                              static_output_shapes = {StaticShape{}};
-    shape_infer(broadcast_v3.get(), static_input_shapes, static_output_shapes, constant_data);
+    shape_inference(broadcast_v3.get(), static_input_shapes, static_output_shapes, constant_data);
     ASSERT_EQ(static_output_shapes[0], StaticShape({2, 3, 6}));
 
     static_input_shapes = {StaticShape{3, 1}, StaticShape{3}};
     static_output_shapes = {StaticShape{}};
-    EXPECT_THROW(shape_infer(broadcast_v3.get(), static_input_shapes, static_output_shapes, {}), NodeValidationFailure);
+    EXPECT_THROW(shape_inference(broadcast_v3.get(), static_input_shapes, static_output_shapes, {}), NodeValidationFailure);
 }
 
 TEST(StaticShapeInferenceTest, BroadcastPDPDConstantTest) {
     auto input = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1});
-    auto target_shape = std::make_shared<ov::op::v0::Constant>(element::i32, Shape{3}, std::vector<int32_t>{2, 3, 6});
+    auto target_shape = std::make_shared<ov::op::v0::Constant>(element::i32, ov::Shape{3}, std::vector<int32_t>{2, 3, 6});
     auto broadcast_v3 =
         std::make_shared<op::v3::Broadcast>(input, target_shape, op::BroadcastModeSpec(op::BroadcastType::PDPD, 1));
 
     std::vector<StaticShape> static_input_shapes = {StaticShape{3, 1}, StaticShape{3}},
                              static_output_shapes = {StaticShape{}};
-    shape_infer(broadcast_v3.get(), static_input_shapes, static_output_shapes, {});
+    shape_inference(broadcast_v3.get(), static_input_shapes, static_output_shapes, {});
     ASSERT_EQ(static_output_shapes[0], StaticShape({2, 3, 6}));
 }
 
@@ -85,27 +85,27 @@ TEST(StaticShapeInferenceTest, BroadcastNumpyTest) {
     int32_t target_shape_val[] = {1, 16, 50, 50};
     std::map<size_t, std::shared_ptr<ngraph::runtime::HostTensor>> constant_data;
     constant_data[1] =
-        std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, Shape{4}, target_shape_val);
+        std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, ov::Shape{4}, target_shape_val);
 
     std::vector<StaticShape> static_input_shapes = {StaticShape{16, 1, 1}, StaticShape{4}},
                              static_output_shapes = {StaticShape{}};
-    shape_infer(broadcast_v3.get(), static_input_shapes, static_output_shapes, constant_data);
+    shape_inference(broadcast_v3.get(), static_input_shapes, static_output_shapes, constant_data);
     ASSERT_EQ(static_output_shapes[0], StaticShape({1, 16, 50, 50}));
 
     static_input_shapes = {StaticShape{16, 1, 1}, StaticShape{4}};
     static_output_shapes = {StaticShape{}};
-    EXPECT_THROW(shape_infer(broadcast_v3.get(), static_input_shapes, static_output_shapes, {}), NodeValidationFailure);
+    EXPECT_THROW(shape_inference(broadcast_v3.get(), static_input_shapes, static_output_shapes, {}), NodeValidationFailure);
 }
 
 TEST(StaticShapeInferenceTest, BroadcastNumpyConstantTest) {
     auto input = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1});
     auto target_shape =
-        std::make_shared<ov::op::v0::Constant>(element::i32, Shape{4}, std::vector<int32_t>{1, 16, 50, 50});
+        std::make_shared<ov::op::v0::Constant>(element::i32, ov::Shape{4}, std::vector<int32_t>{1, 16, 50, 50});
     auto broadcast_v3 = std::make_shared<op::v3::Broadcast>(input, target_shape, op::BroadcastType::NUMPY);
 
     std::vector<StaticShape> static_input_shapes = {StaticShape{16, 1, 1}, StaticShape{4}},
                              static_output_shapes = {StaticShape{}};
-    shape_infer(broadcast_v3.get(), static_input_shapes, static_output_shapes, {});
+    shape_inference(broadcast_v3.get(), static_input_shapes, static_output_shapes, {});
     ASSERT_EQ(static_output_shapes[0], StaticShape({1, 16, 50, 50}));
 }
 
@@ -120,32 +120,32 @@ TEST(StaticShapeInferenceTest, BroadcastExplicitTest) {
     int32_t axes_mapping_val[] = {1};
     std::map<size_t, std::shared_ptr<ngraph::runtime::HostTensor>> constant_data;
     constant_data[1] =
-        std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, Shape{4}, target_shape_val);
+        std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, ov::Shape{4}, target_shape_val);
     constant_data[2] =
-        std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, Shape{1}, axes_mapping_val);
+        std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, ov::Shape{1}, axes_mapping_val);
 
     std::vector<StaticShape> static_input_shapes = {StaticShape{16}, StaticShape{4}, StaticShape{1}};
     std::vector<StaticShape> static_output_shapes = {StaticShape{}};
-    shape_infer(broadcast_v3.get(), static_input_shapes, static_output_shapes, constant_data);
+    shape_inference(broadcast_v3.get(), static_input_shapes, static_output_shapes, constant_data);
     ASSERT_EQ(static_output_shapes[0], StaticShape({1, 16, 50, 50}));
 
     constant_data.erase(1);
-    EXPECT_THROW(shape_infer(broadcast_v3.get(), static_input_shapes, static_output_shapes, constant_data),
+    EXPECT_THROW(shape_inference(broadcast_v3.get(), static_input_shapes, static_output_shapes, constant_data),
                  NodeValidationFailure);
-    EXPECT_THROW(shape_infer(broadcast_v3.get(), static_input_shapes, static_output_shapes, {}), NodeValidationFailure);
+    EXPECT_THROW(shape_inference(broadcast_v3.get(), static_input_shapes, static_output_shapes, {}), NodeValidationFailure);
 }
 
 TEST(StaticShapeInferenceTest, BroadcastExplicitConstantTest) {
     auto input = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1});
     auto target_shape =
-        std::make_shared<ov::op::v0::Constant>(element::i32, Shape{4}, std::vector<int32_t>{1, 16, 50, 50});
-    auto axes_mapping = std::make_shared<ov::op::v0::Constant>(element::i32, Shape{1}, std::vector<int32_t>{1});
+        std::make_shared<ov::op::v0::Constant>(element::i32, ov::Shape{4}, std::vector<int32_t>{1, 16, 50, 50});
+    auto axes_mapping = std::make_shared<ov::op::v0::Constant>(element::i32, ov::Shape{1}, std::vector<int32_t>{1});
     auto broadcast_v3 =
         std::make_shared<op::v3::Broadcast>(input, target_shape, axes_mapping, op::BroadcastType::EXPLICIT);
 
     std::vector<StaticShape> static_input_shapes = {StaticShape{16}, StaticShape{4}, StaticShape{1}};
     std::vector<StaticShape> static_output_shapes = {StaticShape{}};
-    shape_infer(broadcast_v3.get(), static_input_shapes, static_output_shapes, {});
+    shape_inference(broadcast_v3.get(), static_input_shapes, static_output_shapes, {});
     ASSERT_EQ(static_output_shapes[0], StaticShape({1, 16, 50, 50}));
 }
 
@@ -160,16 +160,16 @@ TEST(StaticShapeInferenceTest, BroadcastV1PDPDTest) {
     int32_t target_shape_val[] = {2, 3, 6};
     std::map<size_t, std::shared_ptr<ngraph::runtime::HostTensor>> constant_data;
     constant_data[1] =
-        std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, Shape{3}, target_shape_val);
+        std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, ov::Shape{3}, target_shape_val);
 
     std::vector<StaticShape> static_input_shapes = {StaticShape{3, 1}, StaticShape{3}},
                              static_output_shapes = {StaticShape{}};
-    shape_infer(broadcast_v1.get(), static_input_shapes, static_output_shapes, constant_data);
+    shape_inference(broadcast_v1.get(), static_input_shapes, static_output_shapes, constant_data);
     ASSERT_EQ(static_output_shapes[0], StaticShape({2, 3, 6}));
 
     static_input_shapes = {StaticShape{3, 1}, StaticShape{3}};
     static_output_shapes = {StaticShape{}};
-    EXPECT_THROW(shape_infer(broadcast_v1.get(), static_input_shapes, static_output_shapes, {}), NodeValidationFailure);
+    EXPECT_THROW(shape_inference(broadcast_v1.get(), static_input_shapes, static_output_shapes, {}), NodeValidationFailure);
 }
 
 TEST(StaticShapeInferenceTest, BroadcastV1NumpyTest) {
@@ -180,16 +180,16 @@ TEST(StaticShapeInferenceTest, BroadcastV1NumpyTest) {
     int32_t target_shape_val[] = {2, 3, 6};
     std::map<size_t, std::shared_ptr<ngraph::runtime::HostTensor>> constant_data;
     constant_data[1] =
-        std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, Shape{3}, target_shape_val);
+        std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, ov::Shape{3}, target_shape_val);
 
     std::vector<StaticShape> static_input_shapes = {StaticShape{3, 1}, StaticShape{3}},
                              static_output_shapes = {StaticShape{}};
-    shape_infer(broadcast_v1.get(), static_input_shapes, static_output_shapes, constant_data);
+    shape_inference(broadcast_v1.get(), static_input_shapes, static_output_shapes, constant_data);
     ASSERT_EQ(static_output_shapes[0], StaticShape({2, 3, 6}));
 
     static_input_shapes = {StaticShape{3, 1}, StaticShape{3}};
     static_output_shapes = {StaticShape{}};
-    EXPECT_THROW(shape_infer(broadcast_v1.get(), static_input_shapes, static_output_shapes, {}), NodeValidationFailure);
+    EXPECT_THROW(shape_inference(broadcast_v1.get(), static_input_shapes, static_output_shapes, {}), NodeValidationFailure);
 }
 
 TEST(StaticShapeInferenceTest, BroadcastV1ExplicitTest) {
@@ -202,16 +202,16 @@ TEST(StaticShapeInferenceTest, BroadcastV1ExplicitTest) {
     int32_t axes_mapping_val[] = {1, 2};
     std::map<size_t, std::shared_ptr<ngraph::runtime::HostTensor>> constant_data;
     constant_data[1] =
-        std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, Shape{3}, target_shape_val);
+        std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, ov::Shape{3}, target_shape_val);
     constant_data[2] =
-        std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, Shape{2}, axes_mapping_val);
+        std::make_shared<ngraph::runtime::HostTensor>(ngraph::element::Type_t::i32, ov::Shape{2}, axes_mapping_val);
 
     std::vector<StaticShape> static_input_shapes = {StaticShape{3, 1}, StaticShape{3}, StaticShape{2}},
                              static_output_shapes = {StaticShape{}};
-    shape_infer(broadcast_v1.get(), static_input_shapes, static_output_shapes, constant_data);
+    shape_inference(broadcast_v1.get(), static_input_shapes, static_output_shapes, constant_data);
     ASSERT_EQ(static_output_shapes[0], StaticShape({2, 3, 1}));
 
     static_input_shapes = {StaticShape{3, 1}, StaticShape{3}, StaticShape{2}};
     static_output_shapes = {StaticShape{}};
-    EXPECT_THROW(shape_infer(broadcast_v1.get(), static_input_shapes, static_output_shapes, {}), NodeValidationFailure);
+    EXPECT_THROW(shape_inference(broadcast_v1.get(), static_input_shapes, static_output_shapes, {}), NodeValidationFailure);
 }
