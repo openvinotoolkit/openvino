d7dd6d9ca4265bf9c4a66b76f6bf78a93b53f8eb
intel-ava14 <wilson.seok@intel.com>
2021-12-08 18:23:49 +0900

    initial code commit

---
+++ docs/template_plugin/tests/functional/op_reference/lstm_sequence.cpp
@@ -0,0 +1,193 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <gtest/gtest.h>
+
+#include "openvino/opsets/opset8.hpp"
+#include "openvino/opsets/opset7.hpp"
+#include "openvino/opsets/opset1.hpp"
+#include "openvino/op/lstm_sequence.hpp"
+#include "base_reference_test.hpp"
+#include "ngraph_functions/utils/data_utils.hpp"
+
+using namespace reference_tests;
+using namespace ov;
+
+namespace {
+struct LSTMSequenceParams {
+    template <class T>
+    LSTMSequenceParams(
+        const size_t batchSize, const size_t inputSize, const size_t hiddenSize, const size_t seqLength,
+        const op::RecurrentSequenceDirection& lstm_direction,
+        const element::Type_t& iType,
+        const std::vector<T>& XValues, const std::vector<T>& H_tValues, const std::vector<T>& C_tValues,
+        const std::vector<int64_t>& S_tValues,
+        const std::vector<T>& WValues, const std::vector<T>& RValues, const std::vector<T>& BValues,
+        const std::vector<T>& YValues, const std::vector<T>& HoValues, const std::vector<T>& CoValues,
+        const std::string& testcaseName = "") :
+        batchSize(batchSize), inputSize(inputSize), hiddenSize(hiddenSize), seqLength(seqLength),
+        lstm_direction(lstm_direction), iType(iType), oType(iType),
+        testcaseName(testcaseName) {
+            numDirections = (lstm_direction == op::RecurrentSequenceDirection::BIDIRECTIONAL) ? 2 : 1;
+
+            Shape XShape = Shape{batchSize, seqLength, inputSize};
+            Shape H_tShape = Shape{batchSize, numDirections, hiddenSize};
+            Shape C_tShape = Shape{batchSize, numDirections, hiddenSize};
+            Shape S_tShape = Shape{batchSize};
+            Shape WShape = Shape{numDirections, 4 * hiddenSize, inputSize};
+            Shape RShape = Shape{numDirections, 4 * hiddenSize, hiddenSize};
+            Shape BShape = Shape{numDirections, 4 * hiddenSize};
+            Shape YShape = Shape{batchSize, numDirections, seqLength, hiddenSize};
+            Shape HoShape = Shape{batchSize, numDirections, hiddenSize};
+            Shape CoShape = Shape{batchSize, numDirections, hiddenSize};
+
+            X = Tensor(XShape, iType, XValues);
+            H_t = Tensor(H_tShape, iType, H_tValues);
+            C_t = Tensor(C_tShape, iType, C_tValues);
+            S_t = Tensor(S_tShape, element::Type_t::i64, S_tValues);
+            W = Tensor(WShape, iType, WValues);
+            R = Tensor(RShape, iType, RValues);
+            B = Tensor(BShape, iType, BValues);
+            Y = Tensor(YShape, oType, YValues);
+            Ho = Tensor(HoShape, oType, HoValues);
+            Co = Tensor(CoShape, oType, CoValues);
+        }
+
+    size_t batchSize;
+    size_t inputSize;
+    size_t hiddenSize;
+    size_t seqLength;
+    size_t numDirections;
+    op::RecurrentSequenceDirection lstm_direction;
+    element::Type_t iType;
+    element::Type_t oType;
+
+/*
+    PartialShape XShape;
+    PartialShape H_tShape;
+    PartialShape C_tShape;
+    Shape S_tShape;
+    Shape WShape;
+    Shape RShape;
+    Shape BShape;
+    Shape YShape;
+    Shape HoShape;
+    Shape CoShape;
+*/
+    Tensor X;
+    Tensor H_t;
+    Tensor C_t;
+    Tensor S_t;
+    Tensor W;
+    Tensor R;
+    Tensor B;
+    Tensor Y;
+    Tensor Ho;
+    Tensor Co;
+    std::string testcaseName;
+};
+
+class ReferenceLSTMSequenceTest : public testing::TestWithParam<LSTMSequenceParams>, public CommonReferenceTest {
+public:
+    void SetUp() override {
+        auto params = GetParam();
+        function = CreateFunction(params);
+        inputData = {params.X.data, params.H_t.data, params.C_t.data, params.S_t.data, params.W.data, params.R.data, params.B.data};
+        refOutData = {params.Y.data, params.Ho.data, params.Co.data};
+    }
+
+    static std::string getTestCaseName(const testing::TestParamInfo<LSTMSequenceParams>& obj) {
+        auto param = obj.param;
+        std::ostringstream result;
+        result << "iType=" << param.iType << "_";
+        result << "xShape=" << param.X.shape << "_";
+        result << "htShape=" << param.H_t.shape << "_";
+        result << "ctShape=" << param.C_t.shape << "_";
+        result << "stShape=" << param.S_t.shape << "_";
+        result << "wShape=" << param.W.shape << "_";
+        result << "rShape=" << param.R.shape << "_";
+        result << "bShape=" << param.B.shape << "_";
+        result << "YShape=" << param.Y.shape << "_";
+        result << "hoShape=" << param.Ho.shape << "_";
+        result << "coShape=" << param.Co.shape;
+        if (!param.testcaseName.empty())
+            result << "_" << param.testcaseName;
+
+        return result.str();
+    }
+
+private:
+    static std::shared_ptr<Function> CreateFunction(const LSTMSequenceParams& params) {
+        const auto X = std::make_shared<op::v0::Parameter>(params.X.type, params.X.shape);
+        const auto H_t = std::make_shared<op::v0::Parameter>(params.H_t.type, params.H_t.shape);
+        const auto C_t = std::make_shared<op::v0::Parameter>(params.C_t.type, params.C_t.shape);
+        const auto S_t = std::make_shared<op::v0::Parameter>(params.S_t.type, params.S_t.shape);
+        const auto W = std::make_shared<op::v0::Parameter>(params.W.type, params.W.shape);
+        const auto R = std::make_shared<op::v0::Parameter>(params.R.type, params.R.shape);
+        const auto B = std::make_shared<op::v0::Parameter>(params.B.type, params.B.shape);
+
+        const auto lstm_sequence =
+            std::make_shared<op::v5::LSTMSequence>(X,
+                                               H_t,
+                                               C_t,
+                                               S_t,
+                                               W,//op::util::convert_lstm_node_format(W, op::util::LSTMWeightsFormat::IOFC),
+                                               R,//op::util::convert_lstm_node_format(R, op::util::LSTMWeightsFormat::IOFC),
+                                               B,//op::util::convert_lstm_node_format(B, op::util::LSTMWeightsFormat::IOFC),
+                                               params.hiddenSize,
+                                               params.lstm_direction);
+
+        auto function = std::make_shared<Function>(lstm_sequence->outputs(), ParameterVector{X, H_t, C_t, S_t, W, R, B});
+        return function;
+    }
+};
+
+TEST_P(ReferenceLSTMSequenceTest, CompareWithRefs) {
+    Exec();
+}
+
+template <element::Type_t ET>
+std::vector<LSTMSequenceParams> generateParams() {
+    using T = typename element_type_traits<ET>::value_type;
+
+    //NGraphFunctions::Utils::generateVector<ET>;
+    //runtime::reference::lstm_sequence ;
+    std::vector<LSTMSequenceParams> params {
+        LSTMSequenceParams(
+            2, 3, 3, 2,
+            op::RecurrentSequenceDirection::FORWARD,
+            ET,
+            NGraphFunctions::Utils::generateVector<ET>((2 * 2 * 3)),
+            NGraphFunctions::Utils::generateVector<ET>((2 * 3)),
+            NGraphFunctions::Utils::generateVector<ET>((2 * 3)),
+            std::vector<int64_t>{2, 2},
+            NGraphFunctions::Utils::generateVector<ET>((4 * 3 * 3)),
+            NGraphFunctions::Utils::generateVector<ET>((4 * 3 * 3)),
+            NGraphFunctions::Utils::generateVector<ET>((4 * 3)),
+            std::vector<T>(2 * 2 * 3, 0.f),
+            std::vector<T>(2 * 3, 0.f),
+            std::vector<T>(2 * 3, 0.f)),
+    };
+    return params;
+}
+
+std::vector<LSTMSequenceParams> generateCombinedParams() {
+    const std::vector<std::vector<LSTMSequenceParams>> generatedParams {
+        //generateParams<element::Type_t::bf16>(),
+        //generateParams<element::Type_t::f16>(),
+        generateParams<element::Type_t::f32>(),
+        //generateParams<element::Type_t::f64>(),
+    };
+    std::vector<LSTMSequenceParams> combinedParams;
+
+    for (const auto& params : generatedParams) {
+        combinedParams.insert(combinedParams.end(), params.begin(), params.end());
+    }
+    return combinedParams;
+}
+
+INSTANTIATE_TEST_SUITE_P(smoke_LSTMSequence_With_Hardcoded_Refs, ReferenceLSTMSequenceTest,
+    testing::ValuesIn(generateCombinedParams()), ReferenceLSTMSequenceTest::getTestCaseName);
+
+} // namespace
