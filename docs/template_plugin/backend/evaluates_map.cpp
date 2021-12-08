d459b67a3b5e70d5934245d302d140ca082ef019
Steve Yoo <steve.yoo@intel.com>
2021-12-08 14:10:37 +0900

    Create LSTMCell-1

--- docs/template_plugin/backend/evaluates_map.cpp
+++ docs/template_plugin/backend/evaluates_map.cpp
@@ -1945,6 +1945,30 @@ bool evaluate(const shared_ptr<op::v0::RNNCell>& op, const HostTensorVector& out
 }
 
 template <element::Type_t ET>
+bool evaluate(const shared_ptr<op::v0::LSTMCell>& op, const HostTensorVector& outputs, const HostTensorVector& inputs) {
+    using T = typename element_type_traits<ET>::value_type;
+    runtime::reference::lstm_cell<T>(inputs[0]->get_data_ptr<ET>(),
+                                     inputs[0]->get_shape(),
+                                     inputs[1]->get_data_ptr<ET>(),
+                                     inputs[1]->get_shape(),
+                                     inputs[2]->get_data_ptr<ET>(),
+                                     inputs[2]->get_shape(),
+                                     inputs[3]->get_data_ptr<ET>(),
+                                     inputs[3]->get_shape(),
+                                     inputs[4]->get_data_ptr<ET>(),
+                                     inputs[4]->get_shape(),
+                                     inputs[5]->get_data_ptr<ET>(),
+                                     inputs[5]->get_shape(),
+                                     outputs[0]->get_data_ptr<ET>(),
+                                     outputs[1]->get_data_ptr<ET>(),
+                                     op->get_activations()[0],
+                                     op->get_activations()[1],
+                                     op->get_activations()[2],
+                                     op->get_clip());
+    return true;
+}
+
+template <element::Type_t ET>
 bool evaluate(const shared_ptr<op::v4::LSTMCell>& op, const HostTensorVector& outputs, const HostTensorVector& inputs) {
     using T = typename element_type_traits<ET>::value_type;
     runtime::reference::lstm_cell<T>(inputs[0]->get_data_ptr<ET>(),
