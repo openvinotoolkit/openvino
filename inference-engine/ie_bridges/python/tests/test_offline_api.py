7b51381d995f32085b19ee63b2bd07dac0aeb4d1
Ivan Tikhonov <ivan.tikhonov@intel.com>
2021-12-08 22:38:35 +0300

    fix python tests

--- inference-engine/ie_bridges/python/tests/test_offline_api.py
+++ inference-engine/ie_bridges/python/tests/test_offline_api.py
@@ -5,19 +5,19 @@ from openvino.inference_engine import IECore, IENetwork
 from openvino.offline_transformations import ApplyMOCTransformations, ApplyLowLatencyTransformation, \
     ApplyPruningTransformation, ApplyMakeStatefulTransformation
 
-import ngraph as ng
-from ngraph.impl.op import Parameter
-from ngraph.impl import Function, Shape, Type
-
+import openvino.runtime.opset8 as ops
+from openvino.runtime import Function, Shape, Type
 from conftest import model_path
-
 
 test_net_xml, test_net_bin = model_path()
 
+
 def get_test_cnnnetwork():
-    param = ng.parameter(Shape([1, 3, 22, 22]), name="parameter")
-    relu = ng.relu(param)
-    res = ng.result(relu, name='result')
+    param = ops.parameter(Shape([1, 3, 22, 22]))
+    param.get_output_tensor(0).set_names({'parameter'})
+    relu = ops.relu(param)
+    res = ops.result(relu, name='result')
+    res.get_output_tensor(0).set_names({'res1', 'res1a'})
     func = Function([res], [param], 'test')
     caps = Function.to_capsule(func)
 
@@ -30,7 +30,7 @@ def test_moc_transformations():
     net = get_test_cnnnetwork()
     ApplyMOCTransformations(net, False)
 
-    f = ng.function_from_cnn(net)
+    f = ops.function_from_cnn(net)
     assert f != None
     assert len(f.get_ops()) == 3
 
@@ -39,7 +39,7 @@ def test_low_latency_transformations():
     net = get_test_cnnnetwork()
     ApplyLowLatencyTransformation(net)
 
-    f = ng.function_from_cnn(net)
+    f = ops.function_from_cnn(net)
     assert f != None
     assert len(f.get_ops()) == 3
 
@@ -48,7 +48,7 @@ def test_make_stateful_transformations():
     net = get_test_cnnnetwork()
     ApplyMakeStatefulTransformation(net, {"parameter": "result"})
 
-    f = ng.function_from_cnn(net)
+    f = ops.function_from_cnn(net)
     assert f != None
     assert len(f.get_parameters()) == 0
     assert len(f.get_results()) == 0
@@ -58,6 +58,6 @@ def test_pruning_transformations():
     net = get_test_cnnnetwork()
     ApplyPruningTransformation(net)
 
-    f = ng.function_from_cnn(net)
+    f = ops.function_from_cnn(net)
     assert f != None
     assert len(f.get_ops()) == 3
