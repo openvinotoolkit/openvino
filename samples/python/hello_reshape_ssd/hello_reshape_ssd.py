ed42eb684417663bd83910a1b1f5c010f8ba1c81
Maxim Gordeev <maxim.gordeev@intel.com>
2021-12-13 11:41:41 +0300

    moved hello_reshape_ssd sample to new API 2.0

--- samples/python/hello_reshape_ssd/hello_reshape_ssd.py
+++ samples/python/hello_reshape_ssd/hello_reshape_ssd.py
@@ -9,7 +9,8 @@ import sys
 
 import cv2
 import numpy as np
-from openvino.inference_engine import IECore
+from openvino.runtime import Core, Tensor, Layout, Type, AsyncInferQueue, PartialShape
+from openvino.preprocess import PrePostProcessor, ResizeAlgorithm
 
 
 def parse_args() -> argparse.Namespace:
@@ -40,82 +41,89 @@ def main():
     log.basicConfig(format='[ %(levelname)s ] %(message)s', level=log.INFO, stream=sys.stdout)
     args = parse_args()
 
-    # ---------------------------Step 1. Initialize inference engine core--------------------------------------------------
-    log.info('Creating Inference Engine')
-    ie = IECore()
+    # ---------------------------Step 1. Initialize OpenVINO Runtime Core--------------------------------------------------
+    log.info('Creating OpenVINO Runtime Core')
+    core = Core()
 
     if args.extension and args.device == 'CPU':
         log.info(f'Loading the {args.device} extension: {args.extension}')
-        ie.add_extension(args.extension, args.device)
+        core.add_extension(args.extension, args.device)
 
     if args.config and args.device in ('GPU', 'MYRIAD', 'HDDL'):
         log.info(f'Loading the {args.device} configuration: {args.config}')
-        ie.set_config({'CONFIG_FILE': args.config}, args.device)
+        core.set_config({'CONFIG_FILE': args.config}, args.device)
 
     # ---------------------------Step 2. Read a model in OpenVINO Intermediate Representation or ONNX format---------------
     log.info(f'Reading the network: {args.model}')
     # (.xml and .bin files) or (.onnx file)
-    net = ie.read_network(model=args.model)
+    model = core.read_model(model=args.model)
 
-    if len(net.input_info) != 1:
+    if len(model.inputs) != 1:
         log.error('Sample supports only single input topologies')
         return -1
-    if len(net.outputs) != 1:
+    if model.get_output_size() != 1:
         log.error('Sample supports only single output topologies')
         return -1
 
-    # ---------------------------Step 3. Configure input & output----------------------------------------------------------
-    log.info('Configuring input and output blobs')
-    # Get names of input and output blobs
-    input_blob = next(iter(net.input_info))
-    out_blob = next(iter(net.outputs))
-
-    # Set input and output precision manually
-    net.input_info[input_blob].precision = 'U8'
-    net.outputs[out_blob].precision = 'FP32'
-
+    # ---------------------------Step 3. Configure input---------------------------------------------------------
     original_image = cv2.imread(args.input)
     image = original_image.copy()
-
+    
     # Change data layout from HWC to CHW
     image = image.transpose((2, 0, 1))
     # Add N dimension to transform to NCHW
     image = np.expand_dims(image, axis=0)
-
     log.info('Reshaping the network to the height and width of the input image')
-    log.info(f'Input shape before reshape: {net.input_info[input_blob].input_data.shape}')
-    net.reshape({input_blob: image.shape})
-    log.info(f'Input shape after reshape: {net.input_info[input_blob].input_data.shape}')
+    model.reshape({model.input().get_any_name():PartialShape(image.shape)})
+    h = model.input().shape[2]
+    w = model.input().shape[3]
+    
+    preproc = PrePostProcessor(model)
+    # 1) Set input tensor information:
+    # - input() provides information about a single model input
+    # - precision of tensor is supposed to be 'u8'
+    # - layout of data is 'NHWC'
+    # - set static spatial dimensions to input tensor to resize from
+    preproc.input() \
+           .tensor() \
+           .set_element_type(Type.u8) \
+           .set_layout(Layout('NHWC')) \
+           .set_spatial_static_shape(h,w) # noqa: ECE001, N400
+    # 2) Adding explicit preprocessing steps:
+    # - convert layout to 'NCHW' (from 'NHWC' specified above at tensor layout)
+    # - apply linear resize from tensor spatial dims to model spatial dims
+    preproc.input().preprocess().resize(ResizeAlgorithm.RESIZE_LINEAR)
+    # 3) Here we suppose model has 'NCHW' layout for input
+    preproc.input().model().set_layout(Layout("NCHW"))
+    # 4) Set output tensor information:
+    # - precision of tensor is supposed to be 'f32'
+    preproc.output().tensor().set_element_type(Type.f32)
+    # 5) Apply preprocessing modifing the original 'model'
+    model = preproc.build()
 
     # ---------------------------Step 4. Loading model to the device-------------------------------------------------------
     log.info('Loading the model to the plugin')
-    exec_net = ie.load_network(network=net, device_name=args.device)
+    compiled_model = core.compile_model(model=model, device_name=args.device)
 
     # ---------------------------Step 5. Create infer request--------------------------------------------------------------
-    # load_network() method of the IECore class with a specified number of requests (default 1) returns an ExecutableNetwork
-    # instance which stores infer requests. So you already created Infer requests in the previous step.
-
-    # ---------------------------Step 6. Prepare input---------------------------------------------------------------------
-    # This sample changes a network input layer shape instead of a image shape. See Step 4.
-
-    # ---------------------------Step 7. Do inference----------------------------------------------------------------------
     log.info('Starting inference in synchronous mode')
-    res = exec_net.infer(inputs={input_blob: image})
 
-    # ---------------------------Step 8. Process output--------------------------------------------------------------------
+    res = compiled_model.infer_new_request(inputs={0: image.transpose((0, 2, 3, 1))})
+
+    # ---------------------------Step 6. Process output--------------------------------------------------------------------
     # Generate a label list
     if args.labels:
         with open(args.labels, 'r') as f:
             labels = [line.split(',')[0].strip() for line in f]
 
-    res = res[out_blob]
+    predictions = next(iter(res.values()))
 
     output_image = original_image.copy()
     h, w, _ = output_image.shape
 
     # Change a shape of a numpy.ndarray with results ([1, 1, N, 7]) to get another one ([N, 7]),
     # where N is the number of detected bounding boxes
-    detections = res.reshape(-1, 7)
+    detections = predictions.reshape(-1, 7)
 
     for detection in detections:
         confidence = detection[2]
