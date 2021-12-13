0544649ce002e985fe81cdb5ec5ba0623a71af87
yekruglov <yegor.kruglov@intel.com>
2021-12-13 14:48:11 +0300

    Updated comments

--- src/bindings/python/src/compatibility/ngraph/opset8/ops.py
+++ src/bindings/python/src/compatibility/ngraph/opset8/ops.py
@@ -781,10 +781,12 @@ def detection_output(
 
 @nameable_op
 def softmax(data: NodeInput, axis: int, name: Optional[str] = None) -> Node:
-    """Apply softmax operation on each element of input tensor.
+    """
+    Apply softmax operation on each element of input tensor.
 
     @param data: The tensor providing input data.
-    @param axis: An axis along which Softmax should be calculated
+    @param axis: An axis along which Softmax should be calculated. Can be positive or negative.
+    @param name: Optional name for the node
     @return The new node with softmax operation applied on each element.
     """
     return _get_node_factory_opset8().create("Softmax", [as_node(data)], {"axis": axis})
