01cd33d731e845c5e9861175af21c432570398c2
Nam, David <david.nam@intel.com>
2021-12-13 16:43:19 +0900

    Fix ci issue regarding const value

--- src/core/src/op/read_value.cpp
+++ src/core/src/op/read_value.cpp
@@ -100,7 +100,7 @@ shared_ptr<Node> op::v6::ReadValue::clone_with_new_inputs(const OutputVector& ne
 
 bool op::v6::ReadValue::visit_attributes(AttributeVisitor& visitor) {
     NGRAPH_OP_SCOPE(v6_ReadValue_visit_attributes);
-    string& variable_id = m_variable->get_info().variable_id;
+    const string& variable_id = m_variable->get_info().variable_id;
     visitor.on_attribute("variable_id", const_cast<string&>(variable_id));
     return true;
 }
