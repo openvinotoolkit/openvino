c3d7c6f0360c1badaff072c6b754bbc9010671d3
Nam, David <david.nam@intel.com>
2021-12-13 16:43:19 +0900

    Fix ci issue regarding const value

--- src/core/src/op/assign.cpp
+++ src/core/src/op/assign.cpp
@@ -99,7 +99,7 @@ shared_ptr<Node> op::v6::Assign::clone_with_new_inputs(const OutputVector& new_a
 
 bool op::v6::Assign::visit_attributes(AttributeVisitor& visitor) {
     NGRAPH_OP_SCOPE(v6_Assign_visit_attributes);
-    string& variable_id = m_variable->get_info().variable_id;
+    const string& variable_id = m_variable->get_info().variable_id;
     visitor.on_attribute("variable_id", const_cast<string&>(variable_id));
     return true;
 }
