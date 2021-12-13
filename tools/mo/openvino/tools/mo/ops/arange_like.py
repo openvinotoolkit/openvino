a4afd25b34f83af5d06dc894057eb7b2632f02aa
yekruglov <yegor.kruglov@intel.com>
2021-12-13 12:46:33 +0300

    added op description

--- tools/mo/openvino/tools/mo/ops/arange_like.py
+++ tools/mo/openvino/tools/mo/ops/arange_like.py
@@ -5,6 +5,17 @@ from openvino.tools.mo.ops.op import Op
 
 
 class ArangeLikeOp(Op):
+    """
+    MXNet operation which returns a sequence of numbers. If axis attribute is None, the output has the
+    same shape as the input. Otherwise, the output is a 1D array with size of the specified axis.
+
+    Attributes:
+        start - Start of interval
+        step - Spacing between values
+        repeat - The repeating time of all elements. Now we can support only default value (= 1)
+        axis - Arange elements according to the size of a certain axis of input array. Defualt value is None
+
+    """
     op = 'arange_like'
 
     def __init__(self, graph: Graph, attrs: dict):
