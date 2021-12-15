3400b3c5d6a1b4c7963cc2c458dc655d4a19349a
evolosen <eugeny.volosenkov@intel.com>
2021-12-15 13:20:10 +0300

    fix tools/mo/openvino/tools/mo/middle/dequantize_linear_resolver.py

--- tools/mo/openvino/tools/mo/middle/dequantize_linear_resolver.py
+++ tools/mo/openvino/tools/mo/middle/dequantize_linear_resolver.py
@@ -3,14 +3,14 @@
 
 import numpy as np
 
-from extensions.ops.Cast import Cast
-from extensions.ops.elementwise import Mul, Sub
-from mo.front.common.partial_infer.utils import int64_array
-from mo.front.tf.graph_utils import create_op_with_const_inputs
-from mo.graph.graph import Graph, rename_nodes
-from mo.middle.passes.convert_data_type import data_type_str_to_np
-from mo.middle.replacement import MiddleReplacementPattern
-from mo.ops.reshape import Reshape
+from openvino.tools.mo.ops.Cast import Cast
+from openvino.tools.mo.ops.elementwise import Mul, Sub
+from openvino.tools.mo.front.common.partial_infer.utils import int64_array
+from openvino.tools.mo.front.tf.graph_utils import create_op_with_const_inputs
+from openvino.tools.mo.graph.graph import Graph, rename_nodes
+from openvino.tools.mo.middle.passes.convert_data_type import data_type_str_to_np
+from openvino.tools.mo.middle.replacement import MiddleReplacementPattern
+from openvino.tools.mo.ops.reshape import Reshape
 
 
 class DequantizeLinearResolver(MiddleReplacementPattern):
@@ -26,7 +26,7 @@ class DequantizeLinearResolver(MiddleReplacementPattern):
     graph_condition = [lambda graph: graph.graph['layout'] == 'NCHW']
 
     def run_after(self):
-        from extensions.middle.quantize_dequantize_linear_resolver import QuantizeDequantizeLinearResolver
+        from openvino.tools.mo.middle.quantize_dequantize_linear_resolver import QuantizeDequantizeLinearResolver
         return [QuantizeDequantizeLinearResolver]
 
     def find_and_replace_pattern(self, graph: Graph):
