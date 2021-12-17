917c206b293d6416d663d6c5e76efb6077e67c62
Ilya Churaev <ilya.churaev@intel.com>
2021-12-17 09:53:18 +0300

    Renamed frontends from *ov_frontend to ov_*_frontend

--- src/frontends/tensorflow/include/tensorflow_frontend/utility.hpp
+++ src/frontends/tensorflow/include/tensorflow_frontend/utility.hpp
@@ -10,11 +10,11 @@
 #    define TF_API
 #    define TF_C_API
 #else
-#    ifdef tensorflow_ov_frontend_EXPORTS
+#    ifdef ov_tensorflow_frontend_EXPORTS
 #        define TF_API   OPENVINO_CORE_EXPORTS
 #        define TF_C_API OPENVINO_EXTERN_C OPENVINO_CORE_EXPORTS
 #    else
 #        define TF_API   OPENVINO_CORE_IMPORTS
 #        define TF_C_API OPENVINO_EXTERN_C OPENVINO_CORE_IMPORTS
-#    endif  // tensorflow_ov_frontend_EXPORTS
+#    endif  // ov_tensorflow_frontend_EXPORTS
 #endif      // OPENVINO_STATIC_LIBRARY
