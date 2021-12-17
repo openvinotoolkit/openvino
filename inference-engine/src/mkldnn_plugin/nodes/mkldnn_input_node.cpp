4140f29d6f31c754942c7c276dc4d976f5c59014
Egor Duplensky <egor.duplenskii@intel.com>
2021-12-17 15:13:16 +0300

    [CPU] Fix sse4 uni_ methods (Normalize and Input nodes) (#9268)

--- inference-engine/src/mkldnn_plugin/nodes/mkldnn_input_node.cpp
+++ inference-engine/src/mkldnn_plugin/nodes/mkldnn_input_node.cpp
@@ -109,7 +109,7 @@ protected:
         uni_vmovdqu(b, a);                   // b = a
         uni_vmovdqu(c, a);                   // c = a
         uni_vpcmpeqd(b, b, zero);               // if (a == 0) b = 1 else b = 0
-        uni_vpand(c, mask);                  // c = a & 01111111100000000000000000000000
+        uni_vpand(c, c, mask);                  // c = a & 01111111100000000000000000000000
         uni_vpcmpeqd(c, c, zero);               // if (c == 0) c = 1 else c = 0
         uni_vtestps(b, c);                    // if ((!b & c) == 0) CF = 1 else CF = 0
     }
