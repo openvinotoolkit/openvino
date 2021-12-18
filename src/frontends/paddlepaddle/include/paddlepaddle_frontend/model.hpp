075c243c46a09ea058e8d87232e1d70e4f591c68
Ivan Tikhonov <ivan.tikhonov@intel.com>
2021-12-18 18:19:25 +0300

    JsonConfig/JsonTranformation/DecoderTransformation Extensions (#9042)
    * Squash commits: JsonConfig/JsonTranformation/DecoderTransformation extensions,
    Unit tests, pybindings
    
    * Call transformation_extensions in convert_partially method
    
    * fix codestyle
    
    * move json extensions to offline transformations
    
    * Fix unit tests
    
    * fix cmake files
    
    * Refactor JsonConfig extensions and FrontEnds
    
    * Update python bindings
    
    * Add missing files
    
    * correct mkl hash
    
    * fix conflicts with master branch
    
    * Resolve review comments
    
    * Revert local changes
    
    * code style
    
    * Add lib destinations
    
    * fix cmake
    
    * correct nlohmann_json hash
    
    * FunctionPass to ModelPass
    
    * fix unit tests and pyopenvino
    
    * fix pyopenvino
    
    * resolve review comments
    
    * revert local change
    
    * Fix dynamic Win build
    
    * Try to fix linux builds
    
    * Fix Lin build: set correct lib directory
    
    * Resolve review commets
    
    * code style
    
    * resolve review comments
    
    * ubuntu20 issue investigation: temporary disable LTO for frontends
    
    * ubuntu20 issue investigation: temporary disable LTO for unit tests
    
    * ubuntu20 issue investigation: temporary disable LTO for thirdparty and core parts
    
    * Revert "ubuntu20 issue investigation: temporary disable LTO for thirdparty and core parts"
    
    This reverts commit 5489072e4abce2656de82458470ff7f16e9885a9.
    
    * Revert "ubuntu20 issue investigation: temporary disable LTO for unit tests"
    
    This reverts commit 73bd3c6bac986f075f2c584f7fc7c076b3390548.
    
    * Revert "ubuntu20 issue investigation: temporary disable LTO for frontends"
    
    This reverts commit dce2ba4830134c732fd301b28533a079db354d23.
    
    * resolve review comments
    
    * try to disable lto for 3rd party json libs
    
    * fix missprint; codestyle

--- src/frontends/paddlepaddle/include/paddlepaddle_frontend/model.hpp
+++ src/frontends/paddlepaddle/include/paddlepaddle_frontend/model.hpp
@@ -4,8 +4,8 @@
 
 #pragma once
 
+#include <openvino/frontend/extension/telemetry.hpp>
 #include <openvino/frontend/manager.hpp>
-#include <openvino/frontend/telemetry_extension.hpp>
 
 #include "paddlepaddle_frontend/utility.hpp"
 
