3d3439d279c99c76604fc81479daf341419d98eb
mangguo <mang.guo@intel.com>
2021-12-21 21:40:43 -0500

    Fix CI failure

--- src/tests/unit/cpu/shape_inference_test/ctc_loss_shape_inference.cpp
+++ src/tests/unit/cpu/shape_inference_test/ctc_loss_shape_inference.cpp
@@ -17,7 +17,7 @@ TEST(StaticShapeInferenceTest, CTCLossTest) {
     const auto& logit_length = std::make_shared<ov::op::v0::Parameter>(element::i32, PartialShape{-1});
     const auto& labels = std::make_shared<ov::op::v0::Parameter>(element::i32, PartialShape{-1, -1});
     const auto& label_length = std::make_shared<ov::op::v0::Parameter>(element::i32, PartialShape{-1});
-    const auto& blank_index = std::make_shared<ov::op::v0::Parameter>(element::i32, Shape{});
+    const auto& blank_index = std::make_shared<ov::op::v0::Parameter>(element::i32, ov::Shape{});
 
     // create CTCLoss node
     auto ctc_loss = std::make_shared<op::v4::CTCLoss>(logits, logit_length, labels, label_length, blank_index);
@@ -26,7 +26,7 @@ TEST(StaticShapeInferenceTest, CTCLossTest) {
                                                     StaticShape{10},
                                                     StaticShape{10, 120},
                                                     StaticShape{10},
-                                                    Shape{}},
+                                                    ov::Shape{}},
                              static_output_shapes = {StaticShape{}};
     shape_inference(ctc_loss.get(), static_input_shapes, static_output_shapes);
 
