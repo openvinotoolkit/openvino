32798b78e7bb02d2cc46c498c9b09ec62eb846a9
Min, Byungil <byungil.min@intel.com>
2021-12-22 18:17:08 +0900

    Modify padding checking logic to enable new model (#9310)
    + Modified a way to add padding in prepare_padding
    + Changed condition of assertion for onednn padding
    
    Signed-off-by: Min, Byungil <byungil.min@intel.com>

--- inference-engine/thirdparty/clDNN/src/graph_optimizer/remove_redundant_reorders.cpp
+++ inference-engine/thirdparty/clDNN/src/graph_optimizer/remove_redundant_reorders.cpp
@@ -544,8 +544,8 @@ void remove_redundant_reorders::run(program& p) {
             n->set_preferred_impl_type(preferred_impl);
         }
 
-        // Validate fused layout when onednn is enable
-        if (n->get_preferred_impl_type() == impl_types::onednn && !lo.are_layouts_suitable_for_onednn(*n)) {
+        // Validate fused layout when onednn is enable in post_optimize_graph
+        if (!enable_reorder_fusing && n->get_preferred_impl_type() == impl_types::onednn && !lo.are_layouts_suitable_for_onednn(*n)) {
             throw std::runtime_error("Onednn doesnot support padded input or output");
         }
     }
