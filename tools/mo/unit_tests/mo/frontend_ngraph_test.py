185eb84890c90047f911b2be504ef369dcab59f7
Mateusz Bencer <mateusz.bencer@intel.com>
2021-12-22 09:46:22 +0100

    change test file name to *_actual

--- tools/mo/unit_tests/mo/frontend_ngraph_test.py
+++ tools/mo/unit_tests/mo/frontend_ngraph_test.py
@@ -64,7 +64,7 @@ def test_main_test():
 def test_mo_fallback_test():
     setup_env()
     args = [sys.executable, '-m', 'pytest',
-            os.path.join(os.path.dirname(__file__), 'utils/mo_fallback_test.py'), '-s']
+            os.path.join(os.path.dirname(__file__), 'utils/mo_fallback_test_actual.py'), '-s']
 
     status = subprocess.run(args, env=os.environ)
     assert not status.returncode
