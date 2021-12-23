4f0de1d26d94b0d4e5e1f4267844008dcd7b6bcd
Ilya Churaev <ilya.churaev@intel.com>
2021-12-23 07:19:34 +0300

    Added BWD macro for all new ops (#9356)

--- src/core/include/openvino/op/detection_output.hpp
+++ src/core/include/openvino/op/detection_output.hpp
@@ -70,6 +70,7 @@ public:
     using Attributes = op::util::DetectionOutputBase::AttributesBase;
 
     OPENVINO_OP("DetectionOutput", "opset8", op::util::DetectionOutputBase);
+    BWDCMP_RTTI_DECLARATION;
 
     DetectionOutput() = default;
     /// \brief Constructs a DetectionOutput operation
