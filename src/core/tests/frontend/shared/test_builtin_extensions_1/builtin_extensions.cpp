36c1807b75b7867e52d8ab69a2621fd9f86dfc89
Ivan Tikhonov <ivan.tikhonov@intel.com>
2021-12-24 08:15:42 +0300

    set different names for ConversionExtensions in unit tests

--- src/core/tests/frontend/shared/test_builtin_extensions_1/builtin_extensions.cpp
+++ src/core/tests/frontend/shared/test_builtin_extensions_1/builtin_extensions.cpp
@@ -12,8 +12,8 @@
 
 OPENVINO_CREATE_EXTENSIONS(std::vector<ov::Extension::Ptr>(
     {std::make_shared<TestExtension1>(),
-     std::make_shared<ov::frontend::ConversionExtension>("NewCustomOp", CustomTranslatorCommon_1),
-     std::make_shared<ov::frontend::ConversionExtension>("NewCustomOp", CustomTranslatorCommon_2),
-     std::make_shared<ov::frontend::onnx::ConversionExtension>("NewCustomOp", CustomTranslatorONNX),
-     std::make_shared<ov::frontend::paddle::ConversionExtension>("NewCustomOp", CustomTranslatorPaddle),
-     std::make_shared<ov::frontend::tensorflow::ConversionExtension>("NewCustomOp", CustomTranslatorTensorflow)}));
+     std::make_shared<ov::frontend::ConversionExtension>("NewCustomOp_1", CustomTranslatorCommon_1),
+     std::make_shared<ov::frontend::ConversionExtension>("NewCustomOp_2", CustomTranslatorCommon_2),
+     std::make_shared<ov::frontend::onnx::ConversionExtension>("NewCustomOp_3", CustomTranslatorONNX),
+     std::make_shared<ov::frontend::paddle::ConversionExtension>("NewCustomOp_4", CustomTranslatorPaddle),
+     std::make_shared<ov::frontend::tensorflow::ConversionExtension>("NewCustomOp_5", CustomTranslatorTensorflow)}));
