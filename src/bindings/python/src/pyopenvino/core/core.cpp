e13703008dbcfb8ac2021f4d98cc3ebe33429f70
AlexeyLebedev1 <alexey.lebedev@intel.com>
2021-12-27 11:40:54 +0300

    Fix code style

--- src/bindings/python/src/pyopenvino/core/core.cpp
+++ src/bindings/python/src/pyopenvino/core/core.cpp
@@ -132,11 +132,13 @@ void regclass_Core(py::module m) {
             py::arg("library_path"));
 
     cls.def("add_extension",
-            static_cast<void (ov::runtime::Core::*)(const std::shared_ptr<ov::Extension>&)>(&ov::runtime::Core::add_extension),
+            static_cast<void (ov::runtime::Core::*)(const std::shared_ptr<ov::Extension>&)>(
+                &ov::runtime::Core::add_extension),
             py::arg("extension"));
 
     cls.def("add_extension",
-            static_cast<void (ov::runtime::Core::*)(const std::vector<std::shared_ptr<ov::Extension>>&)>(&ov::runtime::Core::add_extension),
+            static_cast<void (ov::runtime::Core::*)(const std::vector<std::shared_ptr<ov::Extension>>&)>(
+                &ov::runtime::Core::add_extension),
             py::arg("extensions"));
 
     cls.def_property_readonly("available_devices", &ov::runtime::Core::get_available_devices);
