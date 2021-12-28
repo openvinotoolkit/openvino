8eeccabfa81ed64615108de52ce1c2b080ffa66a
Roman Kazantsev <roman.kazantsev@intel.com>
2021-12-28 13:54:35 +0300

    Use correct import
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- tools/mo/unit_tests/mo/utils/ir_reader/restore_graph_test.py
+++ tools/mo/unit_tests/mo/utils/ir_reader/restore_graph_test.py
@@ -4,7 +4,7 @@ import os
 import tempfile
 import unittest
 from defusedxml.common import EntitiesForbidden
-from mo.utils.ir_reader.restore_graph import restore_graph_from_ir
+from openvino.tools.mo.utils.ir_reader.restore_graph import restore_graph_from_ir
 
 
 class TestIRReader(unittest.TestCase):
