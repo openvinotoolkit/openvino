afa2afaa8e5e2abdde8c2c005685651bda95c7eb
mandrono <maxim.andronov@intel.com>
2021-12-29 11:06:45 +0300

    [CPU] Change mem size computation for CpuBlockedMemoryDesc

--- src/plugins/intel_cpu/src/memory_desc/cpu_blocked_memory_desc.cpp
+++ src/plugins/intel_cpu/src/memory_desc/cpu_blocked_memory_desc.cpp
@@ -119,9 +119,7 @@ bool CpuBlockedMemoryDesc::canComputeMemSizeZeroDims() const {
 size_t CpuBlockedMemoryDesc::getCurrentMemSizeImp() const {
     int64_t e_size = getOffsetPadding();  // size in bytes (from begin of data to last element)
     if (!getShape().hasZeroDims()) {
-        e_size += 1;
-        for (int j = 0; j < getBlockDims().size(); j++)
-            e_size += (getBlockDims()[j] - 1) * getStrides()[j];
+        e_size += getBlockDims()[0] * getStrides()[0];
     }
 
     e_size *= getPrecision() == InferenceEngine::Precision::BIN ? 1 : getPrecision().size();
