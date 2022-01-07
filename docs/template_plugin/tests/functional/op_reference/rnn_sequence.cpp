c28e407dc3866a0d31971b8d82f8d7a4a94475f8
intel-ava14 <wilson.seok@intel.com>
2022-01-08 03:43:18 +0900

    add initial op reference test of rnn_sequence

---
+++ docs/template_plugin/tests/functional/op_reference/rnn_sequence.cpp
@@ -0,0 +1,228 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <gtest/gtest.h>
+
+//#include "openvino/opsets/opset8.hpp"
+//#include "openvino/opsets/opset7.hpp"
+//#include "openvino/opsets/opset1.hpp"
+#include "openvino/op/rnn_sequence.hpp"
+#include "base_reference_test.hpp"
+#include "ngraph_functions/utils/data_utils.hpp"
+#include "ngraph/runtime/reference/sequences.hpp"
+
+
+using namespace reference_tests;
+using namespace ov;
+
+namespace {
+struct RNNSequenceParams {
+    template <class T>
+    RNNSequenceParams(
+        const size_t batchSize, const size_t inputSize, const size_t hiddenSize, const size_t seqLength,
+        const float clip, const op::RecurrentSequenceDirection& rnn_direction,
+        const element::Type_t& iType,
+        const std::vector<T>& XValues, const std::vector<T>& H_tValues, const std::vector<int64_t>& S_tValues,
+        const std::vector<T>& WValues, const std::vector<T>& RValues, const std::vector<T>& BValues,
+        const std::vector<T>& YValues, const std::vector<T>& HoValues,
+        const std::string& testcaseName = "") :
+        batchSize(batchSize), inputSize(inputSize), hiddenSize(hiddenSize), seqLength(seqLength),
+        clip(clip), rnn_direction(rnn_direction), iType(iType), oType(iType),
+        testcaseName(testcaseName) {
+            numDirections = (rnn_direction == op::RecurrentSequenceDirection::BIDIRECTIONAL) ? 2 : 1;
+
+            Shape XShape = Shape{batchSize, seqLength, inputSize};
+            Shape H_tShape = Shape{batchSize, numDirections, hiddenSize};
+            Shape S_tShape = Shape{batchSize};
+            Shape WShape = Shape{numDirections, hiddenSize, inputSize};
+            Shape RShape = Shape{numDirections, hiddenSize, hiddenSize};
+            Shape BShape = Shape{numDirections, hiddenSize};
+            Shape YShape = Shape{batchSize, numDirections, seqLength, hiddenSize};
+            Shape HoShape = Shape{batchSize, numDirections, hiddenSize};
+
+            X = Tensor(XShape, iType, XValues);
+            H_t = Tensor(H_tShape, iType, H_tValues);
+            S_t = Tensor(S_tShape, element::Type_t::i64, S_tValues);
+            W = Tensor(WShape, iType, WValues);
+            R = Tensor(RShape, iType, RValues);
+            B = Tensor(BShape, iType, BValues);
+            Y = Tensor(YShape, oType, YValues);
+            Ho = Tensor(HoShape, oType, HoValues);
+        }
+
+    size_t batchSize;
+    size_t inputSize;
+    size_t hiddenSize;
+    size_t seqLength;
+    size_t numDirections;
+    float clip;
+    op::RecurrentSequenceDirection rnn_direction;
+    element::Type_t iType;
+    element::Type_t oType;
+
+    Tensor X;
+    Tensor H_t;
+    Tensor S_t;
+    Tensor W;
+    Tensor R;
+    Tensor B;
+    Tensor Y;
+    Tensor Ho;
+    std::string testcaseName;
+};
+
+class ReferenceRNNSequenceTest : public testing::TestWithParam<RNNSequenceParams>, public CommonReferenceTest {
+public:
+    void SetUp() override {
+        auto params = GetParam();
+        function = CreateFunction(params);
+        inputData = {params.X.data, params.H_t.data, params.S_t.data, params.W.data, params.R.data, params.B.data};
+        refOutData = {params.Y.data, params.Ho.data};
+    }
+
+    static std::string getTestCaseName(const testing::TestParamInfo<RNNSequenceParams>& obj) {
+        auto param = obj.param;
+        std::ostringstream result;
+        result << "iType=" << param.iType << "_";
+        result << "xShape=" << param.X.shape << "_";
+        result << "htShape=" << param.H_t.shape << "_";
+        result << "stShape=" << param.S_t.shape << "_";
+        result << "wShape=" << param.W.shape << "_";
+        result << "rShape=" << param.R.shape << "_";
+        result << "bShape=" << param.B.shape << "_";
+        result << "YShape=" << param.Y.shape << "_";
+        result << "hoShape=" << param.Ho.shape << "_";
+        result << "clip=" << param.clip << "_";
+        result << "LSTMdirection=" << param.rnn_direction;
+        if (!param.testcaseName.empty())
+            result << "_" << param.testcaseName;
+
+        return result.str();
+    }
+
+private:
+    static std::shared_ptr<Model> CreateFunction(const RNNSequenceParams& params) {
+        const auto X = std::make_shared<op::v0::Parameter>(params.X.type, params.X.shape);
+        const auto H_t = std::make_shared<op::v0::Parameter>(params.H_t.type, params.H_t.shape);
+        const auto S_t = std::make_shared<op::v0::Parameter>(params.S_t.type, params.S_t.shape);
+        const auto W = std::make_shared<op::v0::Parameter>(params.W.type, params.W.shape);
+        const auto R = std::make_shared<op::v0::Parameter>(params.R.type, params.R.shape);
+        const auto B = std::make_shared<op::v0::Parameter>(params.B.type, params.B.shape);
+
+        const auto lstm_sequence =
+            std::make_shared<op::v5::RNNSequence>(X,
+                                               H_t,
+                                               S_t,
+                                               W,
+                                               R,
+                                               B,
+                                               params.hiddenSize,
+                                               params.rnn_direction,
+                                               std::vector<std::string>{"tanh"},
+                                               std::vector<float>{},
+                                               std::vector<float>{},
+                                               params.clip);
+
+        auto function = std::make_shared<Model>(lstm_sequence->outputs(), ParameterVector{X, H_t, S_t, W, R, B});
+        return function;
+    }
+};
+
+TEST_P(ReferenceRNNSequenceTest, CompareWithRefs) {
+    Exec();
+}
+
+template <element::Type_t ET>
+std::vector<RNNSequenceParams> generateParamsRuntimeRef(size_t batchSize,
+                                                         size_t inputSize,
+                                                         size_t hiddenSize,
+                                                         size_t seqLength,
+                                                         float clip,
+                                                         op::RecurrentSequenceDirection rnn_direction) {
+    using T = typename element_type_traits<ET>::value_type;
+
+    size_t numDirections = (rnn_direction == op::RecurrentSequenceDirection::BIDIRECTIONAL) ? 2 : 1;
+
+    Shape XShape = Shape{batchSize, seqLength, inputSize};
+    Shape H_tShape = Shape{batchSize, numDirections, hiddenSize};
+    Shape S_tShape = Shape{batchSize};
+    Shape WShape = Shape{numDirections, hiddenSize, inputSize};
+    Shape RShape = Shape{numDirections, hiddenSize, hiddenSize};
+    Shape BShape = Shape{numDirections, hiddenSize};
+    Shape YShape = Shape{batchSize, numDirections, seqLength, hiddenSize};
+    Shape HoShape = Shape{batchSize, numDirections, hiddenSize};
+
+    std::vector<T> X = NGraphFunctions::Utils::generateVector<ET>(shape_size(XShape));
+    std::vector<T> H = NGraphFunctions::Utils::generateVector<ET>(shape_size(H_tShape));
+    std::vector<int64_t> S = NGraphFunctions::Utils::generateVector<element::Type_t::i64>(shape_size(S_tShape), seqLength);
+    std::vector<T> W = NGraphFunctions::Utils::generateVector<ET>(shape_size(WShape));
+    std::vector<T> R = NGraphFunctions::Utils::generateVector<ET>(shape_size(RShape));
+    std::vector<T> B = NGraphFunctions::Utils::generateVector<ET>(shape_size(BShape));
+
+    std::string activation_f = "tanh";
+    std::vector<T> Y(shape_size(YShape));
+    std::vector<T> Ho(shape_size(HoShape));
+
+    ngraph::runtime::reference::rnn_sequence<T, int64_t>(reinterpret_cast<const char*>(X.data()),
+                   XShape,
+                   reinterpret_cast<const char*>(H.data()),
+                   H_tShape,
+                   reinterpret_cast<const char*>(S.data()),
+                   S_tShape,
+                   reinterpret_cast<const char*>(W.data()),
+                   WShape,
+                   reinterpret_cast<const char*>(R.data()),
+                   RShape,
+                   reinterpret_cast<const char*>(B.data()),
+                   BShape,
+                   reinterpret_cast<char*>(Y.data()),
+                   reinterpret_cast<char*>(Ho.data()),
+                   activation_f,
+                   clip,
+                   rnn_direction);
+
+    std::vector<RNNSequenceParams> params {
+        RNNSequenceParams(
+            batchSize, inputSize, hiddenSize, seqLength,
+            clip, rnn_direction,
+            ET,
+            X,
+            H,
+            S,
+            W,
+            R,
+            B,
+            Y,
+            Ho),
+    };
+    return params;
+}
+
+std::vector<RNNSequenceParams> generateCombinedParams() {
+    const std::vector<std::vector<RNNSequenceParams>> generatedParams {
+        generateParamsRuntimeRef<element::Type_t::bf16>(10, 10, 3, 2, 0.f, op::RecurrentSequenceDirection::FORWARD),
+        generateParamsRuntimeRef<element::Type_t::f16>(10, 10, 3, 2, 0.f, op::RecurrentSequenceDirection::FORWARD),
+        generateParamsRuntimeRef<element::Type_t::f32>(10, 10, 3, 2, 0.f, op::RecurrentSequenceDirection::FORWARD),
+        generateParamsRuntimeRef<element::Type_t::f64>(10, 10, 3, 2, 0.f, op::RecurrentSequenceDirection::FORWARD),
+        generateParamsRuntimeRef<element::Type_t::bf16>(10, 10, 3, 2, 3.5f, op::RecurrentSequenceDirection::REVERSE),
+        generateParamsRuntimeRef<element::Type_t::f16>(10, 10, 3, 2, 3.5f, op::RecurrentSequenceDirection::REVERSE),
+        generateParamsRuntimeRef<element::Type_t::f32>(10, 10, 3, 2, 3.5f, op::RecurrentSequenceDirection::REVERSE),
+        generateParamsRuntimeRef<element::Type_t::f64>(10, 10, 3, 2, 3.5f, op::RecurrentSequenceDirection::REVERSE),
+        generateParamsRuntimeRef<element::Type_t::bf16>(10, 10, 10, 10, 0.f, op::RecurrentSequenceDirection::BIDIRECTIONAL),
+        generateParamsRuntimeRef<element::Type_t::f16>(10, 10, 10, 10, 0.f, op::RecurrentSequenceDirection::BIDIRECTIONAL),
+        generateParamsRuntimeRef<element::Type_t::f32>(10, 10, 10, 10, 0.f, op::RecurrentSequenceDirection::BIDIRECTIONAL),
+        generateParamsRuntimeRef<element::Type_t::f64>(10, 10, 10, 10, 0.f, op::RecurrentSequenceDirection::BIDIRECTIONAL),
+    };
+    std::vector<RNNSequenceParams> combinedParams;
+
+    for (const auto& params : generatedParams) {
+        combinedParams.insert(combinedParams.end(), params.begin(), params.end());
+    }
+    return combinedParams;
+}
+
+INSTANTIATE_TEST_SUITE_P(smoke_RNNSequence_With_Hardcoded_Refs, ReferenceRNNSequenceTest,
+    testing::ValuesIn(generateCombinedParams()), ReferenceRNNSequenceTest::getTestCaseName);
+
+} // namespace
