00981ca56a03a8bad47b82de133d305447e2dda2
indira <indira.salyahova@intel.com>
2022-01-10 22:59:08 +0300

    fix: update pot patterns with transpose

--- tools/pot/openvino/tools/pot/graph/special_patterns.py
+++ tools/pot/openvino/tools/pot/graph/special_patterns.py
@@ -256,3 +256,52 @@ def create_equal_logicalnot_pattern():
     pattern.append_single_op('Equal', 'equal')
     pattern.append_single_op('LogicalNot', 'logicalnot')
     return pattern.set_name('equal_logicalnot').pattern
+
+
+@registry_ignore_patterns('inputs')
+def create_input_transpose_add_pattern():
+    pattern = PatternBuilder()
+    pattern.append_single_op('Parameter', 'input')
+    pattern.append_single_op('Transpose', 'transpose')
+    pattern.insert_add_const()
+    return pattern.set_name('input_transpose_add').pattern
+
+
+@registry_ignore_patterns('inputs')
+def create_input_transpose_mul_pattern():
+    pattern = PatternBuilder()
+    pattern.append_single_op('Parameter', 'input')
+    pattern.append_single_op('Transpose', 'transpose')
+    pattern.append_single_op('Multiply', 'multiply')
+    return pattern.set_name('input_transpose_multiply').pattern
+
+
+@registry_ignore_patterns('inputs')
+def create_input_transpose_scaleshift_pattern():
+    pattern = PatternBuilder()
+    pattern.append_single_op('Parameter', 'input')
+    pattern.append_single_op('Transpose', 'transpose')
+    pattern.insert_scaleshift()
+    return pattern.set_name('input_transpose_scale_shift').pattern
+
+
+@registry_ignore_patterns('inputs')
+def create_input_transpose_reverse_input_channel_scaleshift_pattern():
+    pattern = PatternBuilder()
+    pattern.append_single_op('Parameter', 'input')
+    pattern.append_single_op('Transpose', 'transpose')
+    pattern.insert_split()
+    pattern.append_single_op('Concat', 'concat')
+    pattern.insert_scaleshift()
+    return pattern.set_name('input_transpose_reverse_input_channels_scale_shift').pattern
+
+
+@registry_ignore_patterns('inputs')
+def create_input_transpose_reverse_input_channel_add_pattern():
+    pattern = PatternBuilder()
+    pattern.append_single_op('Parameter', 'input')
+    pattern.append_single_op('Transpose', 'transpose')
+    pattern.insert_split()
+    pattern.append_single_op('Concat', 'concat')
+    pattern.insert_add_const()
+    return pattern.set_name('input_transpose_reverse_input_channels_add').pattern
