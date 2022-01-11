dfae791a08f8e5b418b9641c3d04d550163406fe
AlexeyLebedev1 <alexey.lebedev@intel.com>
2022-01-11 11:32:14 +0300

    Add __eq__ for shape

--- src/bindings/python/src/pyopenvino/graph/shape.cpp
+++ src/bindings/python/src/pyopenvino/graph/shape.cpp
@@ -22,6 +22,12 @@ void regclass_graph_Shape(py::module m) {
     shape.def(py::init<const std::initializer_list<size_t>&>(), py::arg("axis_lengths"));
     shape.def(py::init<const std::vector<size_t>&>(), py::arg("axis_lengths"));
     shape.def(py::init<const ov::Shape&>(), py::arg("axis_lengths"));
+    shape.def(
+        "__eq__",
+        [](const ov::Shape& a, const ov::Shape& b) {
+            return a == b;
+        },
+        py::is_operator());
     shape.def("__len__", [](const ov::Shape& v) {
         return v.size();
     });
