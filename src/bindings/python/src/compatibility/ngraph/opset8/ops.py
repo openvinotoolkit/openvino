41c2535cc894d730a08b64a8588389888196a9f9
Anastasia Kuporosova <anastasia.kuporosova@intel.com>
2022-01-11 16:55:18 +0300

    [Python API] Move wheel folder to the python dir (#9125)
    * [Python API] Move wheel folder to the python dir
    
    * codestyle files
    
    * one more codestyle
    
    * align with master
    
    * ignore some flake8 comments

--- src/bindings/python/src/compatibility/ngraph/opset8/ops.py
+++ src/bindings/python/src/compatibility/ngraph/opset8/ops.py
@@ -781,8 +781,7 @@ def detection_output(
 
 @nameable_op
 def softmax(data: NodeInput, axis: int, name: Optional[str] = None) -> Node:
-    """
-    Apply softmax operation on each element of input tensor.
+    """Apply softmax operation on each element of input tensor.
 
     @param data: The tensor providing input data.
     @param axis: An axis along which Softmax should be calculated. Can be positive or negative.
