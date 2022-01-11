2c6078e96c2f5288b674a206b8e8e4891e31ce81
Edward Shogulin <edward.shogulin@intel.com>
2022-01-11 11:51:15 +0300

    [LPT] Documentation (developer guide) (#7444)
    * [LPT] Documentation
    
    * 1) ToC was removed 2) SVG => PNG temporary conversion
    
    * [LPT] Refactoring + developer guide
    
    * [LPT] attribute doxygen documentation was added
    
    * [LPT] Developer Guide to Reference API links were added
    
    * [LPT] comments fixes
    
    * [LPT] Reference API to Developer Guide links were added
    
    * [LPT] titles were changed
    
    * [LPT] comments fixes #2
    
    * [LPT] root document was moved to Plugin DG
    
    * [LPT] Documentation: image link quick fix
    
    * [LPT] Docummentation: PrecisionsAttribute description quick fix
    
    * fix comments from Karol
    
    * fixes
    
    * movement
    
    * directive was added
    
    * movement #2
    
    * LPT reference in Executable Network rollback
    
    * snippets were updated ini accordance with new API

--- src/common/low_precision_transformations/include/low_precision/avg_pool.hpp
+++ src/common/low_precision_transformations/include/low_precision/avg_pool.hpp
@@ -11,6 +11,14 @@ namespace ngraph {
 namespace pass {
 namespace low_precision {
 
+/**
+ * @ingroup ie_transformation_common_api
+ * @brief AvgPoolTransformation propagates dequantization operations through AvgPool operation.
+ *
+ * For more details about the transformation, refer to
+ * [AvgPoolTransformation](@ref openvino_docs_IE_DG_lpt_AvgPoolTransformation) page
+ * in the Inference Engine Developer Guide.
+ */
 class LP_TRANSFORMATIONS_API AvgPoolTransformation : public LayerTransformation {
 public:
     NGRAPH_RTTI_DECLARATION;
