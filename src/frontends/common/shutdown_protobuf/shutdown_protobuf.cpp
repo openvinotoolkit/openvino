7f384da491babb7a0ffb87f439da58a339e0c4b5
mei, yang <yang.mei@intel.com>
2022-01-12 18:07:51 +0800

    ShutdownProtobufLibrary when unload paddle frontend dynmaic library t… (#9442)
    * ShutdownProtobufLibrary when unload paddle frontend dynmaic library to fix probuf memory leak
    
    * ShutdownProtobufLibrary if the frontend libraries use protobuf
    
    * make shutdown_protobuf a library

---
+++ src/frontends/common/shutdown_protobuf/shutdown_protobuf.cpp
@@ -0,0 +1,27 @@
+#include <google/protobuf/stubs/common.h>
+
+#if defined(_WIN32)
+#    include <windows.h>
+BOOL WINAPI DllMain(HINSTANCE hinstDLL,  // handle to DLL module
+                    DWORD fdwReason,     // reason for calling function
+                    LPVOID lpReserved)   // reserved
+{
+    // Perform actions based on the reason for calling.
+    switch (fdwReason) {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+        break;
+
+    case DLL_PROCESS_DETACH:
+        google::protobuf::ShutdownProtobufLibrary();
+        break;
+    }
+    return TRUE;  // Successful DLL_PROCESS_ATTACH.
+}
+#elif defined(__linux__) || defined(__APPLE__)
+extern "C" __attribute__((destructor)) void library_unload();
+void library_unload() {
+    google::protobuf::ShutdownProtobufLibrary();
+}
+#endif
