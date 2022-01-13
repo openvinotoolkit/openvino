2fa42ad87256b5f259cfaeb429594bc5b36d9c01
Anastasia Kuporosova <anastasia.kuporosova@intel.com>
2022-01-13 15:52:34 +0300

    [Python API] Rename offline_transformations in the new api

--- src/bindings/python/setup.py
+++ src/bindings/python/setup.py
@@ -42,7 +42,7 @@ packages = [
     "openvino",
     # TODO: change the module name according to the description in 69196
     "openvino.frontend",
-    "openvino.offline_transformations_pybind",
+    "openvino.offline_transformations",
     "openvino.runtime.opset1",
     "openvino.runtime.opset2",
     "openvino.runtime.opset3",
--- src/bindings/python/src/pyopenvino/core/offline_transformations.cpp
+++ src/bindings/python/src/pyopenvino/core/offline_transformations.cpp
@@ -37,7 +37,7 @@ namespace py = pybind11;
 void regmodule_offline_transformations(py::module m) {
     // TODO: change the submodule name according to the description in 69196
     py::module m_offline_transformations =
-        m.def_submodule("offline_transformations_pybind", "Offline transformations module");
+        m.def_submodule("offline_transformations", "Offline transformations module");
 
     m_offline_transformations.def(
         "apply_moc_transformations",
