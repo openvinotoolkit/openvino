264fccab4c4328fcc0d44fe73d22d5472e5307f9
Mateusz Tabaka <mateusz.tabaka@intel.com>
2022-01-13 21:26:13 +0100

    tmp

--- src/tests/functional/shared_test_classes/src/subgraph/quantized_convolution_batch_norm.cpp
+++ src/tests/functional/shared_test_classes/src/subgraph/quantized_convolution_batch_norm.cpp
@@ -17,19 +17,19 @@ using namespace ngraph;
 void QuantizedConvolutionBatchNorm::SetUp() {
     targetDevice = GetParam();
 
-    auto parameter = std::make_shared<opset8::Parameter>(element::f32, Shape{1, 2, 3, 3});
+    auto parameter = std::make_shared<opset8::Parameter>(element::f32, Shape{1, 8, 3, 3});
     auto low_act = opset8::Constant::create(element::f32, Shape{}, {0});
     auto high_act = opset8::Constant::create(element::f32, Shape{}, {8.338173});
     std::shared_ptr<Node> activations = std::make_shared<opset8::FakeQuantize>(parameter, low_act, high_act, low_act, high_act, 256);
     auto low_weights = opset8::Constant::create(element::f32, Shape{}, {-0.72519057});
     auto high_weights = opset8::Constant::create(element::f32, Shape{}, {0.72519057});
-    std::shared_ptr<Node> weights = builder::makeConstant(element::f32, Shape{4, 2, 1, 1}, {}, true, 1.0f, -1.0f);
+    std::shared_ptr<Node> weights = builder::makeConstant(element::f32, Shape{8, 8, 1, 1}, {}, true, 1.0f, -1.0f);
     weights = std::make_shared<opset8::FakeQuantize>(weights, low_weights, high_weights, low_weights, high_weights, 255);
     auto conv = std::make_shared<opset8::Convolution>(activations, weights, Strides{1, 1}, CoordinateDiff{0, 0}, CoordinateDiff{0, 0}, Strides{1, 1});
-    auto gamma = builder::makeConstant(element::f32, Shape{4}, {}, true, 1.0f, 0.0f);
-    auto beta = builder::makeConstant(element::f32, Shape{4}, {}, true, 1.0f, 0.0f);
-    auto mean = builder::makeConstant(element::f32, Shape{4}, {}, true, 1.0f, 0.0f);
-    auto var = builder::makeConstant(element::f32, Shape{4}, {}, true, 1.0f, 0.0f);
+    auto gamma = builder::makeConstant(element::f32, Shape{8}, {}, true, 1.0f, 0.0f);
+    auto beta = builder::makeConstant(element::f32, Shape{8}, {}, true, 1.0f, 0.0f);
+    auto mean = builder::makeConstant(element::f32, Shape{8}, {}, true, 1.0f, 0.0f);
+    auto var = builder::makeConstant(element::f32, Shape{8}, {}, true, 1.0f, 0.0f);
 
     auto batch_norm = std::make_shared<opset8::BatchNormInference>(conv, gamma, beta, mean, var, 0.00001);
     function = std::make_shared<ngraph::Function>(batch_norm, ParameterVector{parameter});
