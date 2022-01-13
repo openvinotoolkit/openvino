9ff3b045e902a4b802d89c8274244ed9633836b4
Efode, Irina <irina.efode@intel.com>
2022-01-13 17:58:25 +0300

    [IE TESTS] Skip tests with Default device

--- src/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
+++ src/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
@@ -174,6 +174,9 @@ std::vector<std::string> disabledTestPatterns() {
         R"(smoke_ConversionLayerTest/ConversionLayerTest.CompareWithRefs.*UNSPECIFIED.*)",
         // Issue:
         R"(.*smoke_VariadicSplit4D_CPU_zero_dims.*)",
+        // Issue: 75022
+        R"(.*OVExecutableNetworkBaseTest.*LoadNetworkToDefaultDeviceNoThrow.*)",
+        R"(.*IEClassBasicTest.*smoke_LoadNetworkToDefaultDeviceNoThrow.*)",
     };
 
 #define FIX_62820 0
