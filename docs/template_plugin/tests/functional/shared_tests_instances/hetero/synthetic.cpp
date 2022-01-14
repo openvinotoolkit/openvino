5cb290ae6a9ca2872cdd1deedece4a1105c7388f
Anton Pankratov <anton.pankratov@intel.com>
2022-01-14 12:26:39 +0300

    Hetero creates single result node (#9572)

--- docs/template_plugin/tests/functional/shared_tests_instances/hetero/synthetic.cpp
+++ docs/template_plugin/tests/functional/shared_tests_instances/hetero/synthetic.cpp
@@ -14,6 +14,13 @@ using namespace HeteroTests;
 // this tests load plugin by library name: this is not available during static linkage
 #ifndef OPENVINO_STATIC_LIBRARY
 
+INSTANTIATE_TEST_SUITE_P(smoke_manyTargetInputs, HeteroSyntheticTest,
+                        ::testing::Combine(
+                                ::testing::Values(std::vector<PluginParameter>{{"TEMPLATE0", "ov_template_plugin"}, {"TEMPLATE1", "ov_template_plugin"}}),
+                                ::testing::ValuesIn(HeteroTests::HeteroSyntheticTest::withMajorNodesFunctions(
+                                        [] {return ngraph::builder::subgraph::makeConvPool2Relu2();}, {"Conv_1"}, true))),
+                        HeteroSyntheticTest::getTestCaseName);
+
 INSTANTIATE_TEST_SUITE_P(smoke_SingleMajorNode, HeteroSyntheticTest,
                         ::testing::Combine(
                                 ::testing::Values(std::vector<PluginParameter>{{"TEMPLATE0", "ov_template_plugin"}, {"TEMPLATE1", "ov_template_plugin"}}),
