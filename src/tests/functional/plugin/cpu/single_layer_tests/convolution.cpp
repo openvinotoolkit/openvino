a972da31f3253686f03cfc99e5d82e82949cd4b2
Luwei Zhou <luwei.zhou@intel.com>
2022-01-14 14:20:18 +0800

    [CPU] Enable Reorder node run time cache. (#9490)

--- src/tests/functional/plugin/cpu/single_layer_tests/convolution.cpp
+++ src/tests/functional/plugin/cpu/single_layer_tests/convolution.cpp
@@ -992,7 +992,9 @@ std::vector<InputShape> inputShapes_Reorder_2D = {
             // target static shapes
             {
                 { 1, 32, 39, 40 },
-                { 2, 32, 20, 20}
+                { 2, 32, 20, 20 },
+                { 1, 32, 39, 40 },
+                { 2, 32, 20, 20 }
             }
         }
 };
