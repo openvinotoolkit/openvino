b52cfb8b7233de79be72dee67d535597c8f3831a
liubo-intel <bo4.liu@intel.com>
2022-01-14 09:51:57 +0800

    Paddle FasterRCNN Ops Conversion: greater_than, less_than, gather, floor

---
+++ src/core/tests/frontend/paddle/test_models/gen_scripts/generate_gather.py
@@ -0,0 +1,108 @@
+#
+# gather paddle model generator
+#
+import numpy as np
+from save_model import saveModel
+import paddle
+import sys
+
+
+def gather1(name: str, x, y):
+    paddle.enable_static()
+
+    with paddle.static.program_guard(paddle.static.Program(), paddle.static.Program()):
+        data = paddle.static.data(name='x', shape=x.shape, dtype=x.dtype)
+        index = paddle.static.data(name='index', shape=y.shape, dtype=y.dtype)
+        out = paddle.gather(data, index)
+        if x.dtype == "int64":
+            out = paddle.cast(out, "float32")
+        out = paddle.cast(out, "float32")
+
+        cpu = paddle.static.cpu_places(1)
+        exe = paddle.static.Executor(cpu[0])
+        # startup program will call initializer to initialize the parameters.
+        exe.run(paddle.static.default_startup_program())
+
+        outs = exe.run(
+            feed={'x': x, 'index': y},
+            fetch_list=[out])
+
+        saveModel(name, exe, feedkeys=['x', 'index'], fetchlist=[out], inputs=[
+                  x, y], outputs=[outs[0]], target_dir=sys.argv[1])
+
+    return outs[0]
+
+# def gather2(name:str, x, y, z):
+#     paddle.enable_static()
+
+#     with paddle.static.program_guard(paddle.static.Program(), paddle.static.Program()):
+#         data = paddle.static.data(name='x', shape=x.shape, dtype = x.dtype)
+#         index = paddle.static.data(name='index', shape=y.shape, dtype = y.dtype)
+#         axis = paddle.static.data(name='axis', shape=z.shape, dtype = z.dtype)
+#         out = paddle.gather(data, index, axis)
+
+#         cpu = paddle.static.cpu_places(1)
+#         exe = paddle.static.Executor(cpu[0])
+#         # startup program will call initializer to initialize the parameters.
+#         exe.run(paddle.static.default_startup_program())
+
+#         outs = exe.run(
+#             feed={'x': x, 'index': y, 'axis': z},
+#             fetch_list=[out])
+
+#         saveModel(name, exe, feedkeys=['x', 'index', 'axis'], fetchlist=[out], inputs=[x, y, z], outputs=[outs[0]], target_dir=sys.argv[1])
+
+#     return outs[0]
+
+
+def main():
+    # For multi-dimension input
+    x_shape = (10, 20)
+    x_type = "float32"
+    index = [1, 3, 5]
+    index_type = "int32"
+
+    xnp = np.random.random(x_shape).astype(x_type)
+    index_np = np.array(index).astype(index_type)
+
+    gather1("gather1_multi_dimension", xnp, index_np)
+
+    # For one_dimension input
+    x_shape = (100)
+    x_type = "int64"
+    index = [1, 3, 5]
+    index_type = "int64"
+
+    xnp = np.random.random(x_shape).astype(x_type)
+    index_np = np.array(index).astype(index_type)
+
+    gather1("gather1_one_dimension", xnp, index_np)
+
+    # For one_dimension input2
+    x_shape = (100)
+    x_type = "int64"
+    index = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    index_type = "int64"
+
+    xnp = np.random.random(x_shape).astype(x_type)
+    index_np = np.array(index).astype(index_type)
+
+    gather1("gather1_one_dimension2", xnp, index_np)
+
+    # # For axis as input
+    # x_shape = (3, 88, 3)
+    # x_type = "float32"
+    # index = [1, 3, 5]
+    # index_type = "int32"
+    # axis = [0]
+    # axis_type = "int32"
+
+    # xnp = np.random.random(x_shape).astype(x_type)
+    # axis_np = np.array(axis).astype(axis_type)
+    # index_np = np.array(index).astype(index_type)
+
+    # gather2("gather2_multi_dimension", xnp, index_np, axis_np)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/frontends/paddle/src/op/gather.cpp
@@ -0,0 +1,28 @@
+// Copyright (C) 2018-2022 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "default_opset.hpp"
+#include "openvino/frontend/paddle/node_context.hpp"
+
+namespace ov {
+namespace frontend {
+namespace paddle {
+namespace op {
+NamedOutputs gather(const NodeContext& node) {
+    const auto data_node = node.get_input("X");
+    const auto index_node = node.get_input("Index");
+    Output<Node> axis_node;
+
+    PADDLE_OP_CHECK(node, !node.has_input("Axis"), "OpenVINO not support parameterized Axis input!");
+    const auto axis_value = node.get_attribute<int>("axis", 0);
+    axis_node = default_opset::Constant::create(element::i32, Shape{}, {axis_value});
+
+    return node.default_single_output_mapping(
+        {std::make_shared<default_opset::Gather>(data_node, index_node, axis_node)},
+        {"Out"});
+}
+}  // namespace op
+}  // namespace paddle
+}  // namespace frontend
+}  // namespace ov
