1a83e38092bfbf005a9a750d8eea025acf783cbc
Anton Pankratv <anton.pankratov@intel.com>
2022-01-20 02:28:38 +0300

    code stile

--- src/inference/src/cpp/ie_executable_network.cpp
+++ src/inference/src/cpp/ie_executable_network.cpp
@@ -9,8 +9,8 @@
 #include "cpp_interfaces/interface/ie_iexecutable_network_internal.hpp"
 #include "ie_common.h"
 #include "ie_executable_network_base.hpp"
-#include "ie_remote_context.hpp"
 #include "ie_plugin_config.hpp"
+#include "ie_remote_context.hpp"
 #include "openvino/core/except.hpp"
 #include "openvino/runtime/compiled_model.hpp"
 
