e895c1444fcb799d38d86110f0da924de0fd7a54
Malinin, Nikita <nikita.malinin@intel.com>
2022-01-21 14:44:13 +0300

    Disable mtcnn at sanity

--- tools/pot/tests/test_sanity.py
+++ tools/pot/tests/test_sanity.py
@@ -51,7 +51,8 @@ TEST_MODELS = [
     # {'drop_type': 'relative', 'max_iter_num': 1, 'accuracy_drop': 0.005, 'metrics': [
     #     {'name': 'accuracy@top1', 'baseline_value': 0.431}]}, 'GNA'),
 
-    ('mtcnn', 'caffe', 'DefaultQuantization', 'performance', 1, {'recall': 0.76, 'map': 0.6844}, {}, 'CPU'),
+    #TODO: Enable after the problem with the shapes will be fixed
+    # ('mtcnn', 'caffe', 'DefaultQuantization', 'performance', 1, {'recall': 0.76, 'map': 0.6844}, {}, 'CPU'),
 
     ('mtcnn', 'caffe', 'DefaultQuantization', 'performance', 2, {'recall': 0.8, 'map': 0.7445},
      {'use_fast_bias': False}, 'CPU')
