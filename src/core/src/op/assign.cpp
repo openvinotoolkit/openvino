dffb25addb323690efe2b793423f8a2dbcbd07c9
Nam, David <david.nam@intel.com>
2022-01-24 23:57:49 +0900

    Correct setting attribute

--- src/core/src/op/assign.cpp
+++ src/core/src/op/assign.cpp
@@ -57,7 +57,8 @@ shared_ptr<Node> op::v3::Assign::clone_with_new_inputs(const OutputVector& new_a
 
 bool op::v3::Assign::visit_attributes(AttributeVisitor& visitor) {
     NGRAPH_OP_SCOPE(v3_Assign_visit_attributes);
-    visitor.on_attribute("variable_id", m_variable_id);
+    const string& variable_id = get_variable_id();
+    visitor.on_attribute("variable_id", const_cast<string&>(variable_id));
     return true;
 }
 
@@ -88,7 +89,8 @@ shared_ptr<Node> op::v6::Assign::clone_with_new_inputs(const OutputVector& new_a
 
 bool op::v6::Assign::visit_attributes(AttributeVisitor& visitor) {
     NGRAPH_OP_SCOPE(v6_Assign_visit_attributes);
-    visitor.on_attribute("variable_id", m_variable);
+    const string& variable_id = get_variable_id();
+    visitor.on_attribute("variable_id", const_cast<string&>(variable_id));
     return true;
 }
 
