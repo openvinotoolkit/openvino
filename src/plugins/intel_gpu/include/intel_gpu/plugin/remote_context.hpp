975349b9085bbd915ec62649667e86debc209c24
Mikhail Letavin <mikhail.letavin@intel.com>
2022-01-26 19:19:29 +0300

    [GPU] Force shared surface cache cleanup in RemoteContext (#9816)

--- src/plugins/intel_gpu/include/intel_gpu/plugin/remote_context.hpp
+++ src/plugins/intel_gpu/include/intel_gpu/plugin/remote_context.hpp
@@ -476,6 +476,11 @@ public:
                                    const Config& config = {})
         : _impl(plugin, params, config) {}
 
+    ~TypedExecutionContext() {
+        shared_surf_reg.clear();
+        shared_obj_reg.clear();
+    }
+
     InferenceEngine::ParamMap getParams() const override { return _impl.getParams(); }
     std::string getDeviceName() const noexcept override { return _impl.getDeviceName(); }
 
