d45efced064cf44b4357097727c653fb05185a80
Anastasia Kuporosova <anastasia.kuporosova@intel.com>
2022-01-26 21:16:34 +0300

    [Python API] Align openvino/__init__ files (#9871)
    * [Python API] Align openvino/__init__ files
    
    * style fix

--- src/bindings/python/src/compatibility/openvino/inference_engine/__init__.py
+++ src/bindings/python/src/compatibility/openvino/inference_engine/__init__.py
@@ -5,7 +5,7 @@
 import os
 import sys
 
-if sys.platform == 'win32':
+if sys.platform == "win32":
     # Installer, yum, pip installs openvino dlls to the different directories
     # and those paths need to be visible to the openvino modules
     #
@@ -13,11 +13,11 @@ if sys.platform == 'win32':
     # add the location of openvino dlls to your system PATH.
     #
     # looking for the libs in the pip installation path by default.
-    openvino_libs = [os.path.join(os.path.dirname(__file__), '..', '..', 'openvino', 'libs')]
+    openvino_libs = [os.path.join(os.path.dirname(__file__), "..", "..", "openvino", "libs")]
     # setupvars.bat script set all libs paths to OPENVINO_LIB_PATHS environment variable.
-    openvino_libs_installer = os.getenv('OPENVINO_LIB_PATHS')
+    openvino_libs_installer = os.getenv("OPENVINO_LIB_PATHS")
     if openvino_libs_installer:
-        openvino_libs.extend(openvino_libs_installer.split(';'))
+        openvino_libs.extend(openvino_libs_installer.split(";"))
     for lib in openvino_libs:
         lib_path = os.path.join(os.path.dirname(__file__), lib)
         if os.path.isdir(lib_path):
@@ -25,9 +25,9 @@ if sys.platform == 'win32':
             if (3, 8) <= sys.version_info:
                 os.add_dll_directory(os.path.abspath(lib_path))
             else:
-                os.environ['PATH'] = os.path.abspath(lib_path) + ';' + os.environ['PATH']
+                os.environ["PATH"] = os.path.abspath(lib_path) + ";" + os.environ["PATH"]
 
 from .ie_api import *
 
-__all__ = ['IENetwork', 'TensorDesc', 'IECore', 'Blob', 'PreProcessInfo', 'get_version']
+__all__ = ["IENetwork", "TensorDesc", "IECore", "Blob", "PreProcessInfo", "get_version"]
 __version__ = get_version()  # type: ignore
