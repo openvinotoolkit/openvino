9c77042bf81cf7bf68b78eddb56688820a7eeaa9
Yaroslav Torzuk <yaroslav.torzuk2@altran.com>
2022-01-27 13:49:38 +0200

    Integration of jit_uni_nms_proposal_kernel into Proposal

--- src/plugins/intel_cpu/src/nodes/proposal.cpp
+++ src/plugins/intel_cpu/src/nodes/proposal.cpp
@@ -10,6 +10,8 @@
 #include "proposal.h"
 
 using namespace InferenceEngine;
+using namespace dnnl::impl::cpu;
+using namespace dnnl::impl::cpu::x64;
 
 namespace ov {
 namespace intel_cpu {
@@ -72,6 +74,25 @@ static std::vector<float> generate_anchors(proposal_conf &conf) {
         }
     }
     return anchors;
+}
+
+void Proposal::createPrimitive() {
+    jit_uni_nms_proposal_kernel::jit_nms_conf jcp { conf.post_nms_topn_, conf.nms_thresh_, conf.coordinates_offset };
+    std::unique_ptr<jit_uni_nms_proposal_kernel> nms_kernel;
+    if (mayiuse(avx512_core)) {
+        nms_kernel.reset(new jit_uni_nms_proposal_kernel_impl<avx512_core> { jcp });
+    } else if (mayiuse(x64::avx2)) {
+        nms_kernel.reset(new jit_uni_nms_proposal_kernel_impl<x64::avx2> { jcp });
+    } else if (mayiuse(sse41)) {
+        nms_kernel.reset(new jit_uni_nms_proposal_kernel_impl<sse41> { jcp });
+    } else {
+        DEBUG_LOG("Unable to create JIT version of Proposal due to unsupported ISA."
+            " Non-JIT version of proposal will be executed.");
+    }
+    if (nms_kernel) {
+        nms_kernel->create_ker();
+        nms_kernel_ = std::move(nms_kernel);
+    }
 }
 
 bool Proposal::isSupportedOperation(const std::shared_ptr<const ngraph::Node>& op, std::string& errorMessage) noexcept {
@@ -162,6 +183,104 @@ void Proposal::executeDynamicImpl(dnnl::stream strm) {
     execute(strm);
 }
 
+void Proposal::executeImpl(const float *input0, const float *input1, std::vector<size_t> dims0,
+                           std::array<float, 4> img_info, const float *anchors, int *roi_indices, float *output0,
+                           float *output1, proposal_conf &conf) {
+    // Prepare memory
+    const float *p_bottom_item = input0;
+    const float *p_d_anchor_item = input1;
+
+    float *p_roi_item = output0;
+    float *p_prob_item = output1;
+    auto store_prob = p_prob_item != nullptr;
+
+    // bottom shape: (2 x num_anchors) x H x W
+    const int bottom_H = dims0[2];
+    const int bottom_W = dims0[3];
+
+    // input image height & width
+    const float img_H = img_info[conf.swap_xy ? 1 : 0];
+    const float img_W = img_info[conf.swap_xy ? 0 : 1];
+
+    // scale factor for height & width
+    const float scale_H = img_info[2];
+    const float scale_W = img_info[3];
+
+    // minimum box width & height
+    const float min_box_H = conf.min_size_ * scale_H;
+    const float min_box_W = conf.min_size_ * scale_W;
+
+    // number of all proposals = num_anchors * H * W
+    const int num_proposals = conf.anchors_shape_0 * bottom_H * bottom_W;
+
+    // number of top-n proposals before NMS
+    const int pre_nms_topn = std::min<int>(num_proposals, conf.pre_nms_topn_);
+
+    // number of final RoIs
+    std::size_t num_rois = 0;
+
+    // enumerate all proposals
+    //   num_proposals = num_anchors * H * W
+    //   (x1, y1, x2, y2, score) for each proposal
+    // NOTE: for bottom, only foreground scores are passed
+    struct ProposalBox {
+        float x0;
+        float y0;
+        float x1;
+        float y1;
+        float score;
+    };
+    std::vector<ProposalBox> proposals_(num_proposals);
+    const int unpacked_boxes_buffer_size = store_prob ? 5 * pre_nms_topn : 4 * pre_nms_topn;
+    std::vector<float> unpacked_boxes(unpacked_boxes_buffer_size);
+    std::vector<int> is_dead(pre_nms_topn, 0);
+
+    // Execute
+    int nn = dims0[0];
+    for (int n = 0; n < nn; ++n) {
+        enumerate_proposals_cpu(p_bottom_item + num_proposals + n * num_proposals * 2,
+                                p_d_anchor_item + n * num_proposals * 4,
+                                anchors, reinterpret_cast<float *>(&proposals_[0]),
+                                conf.anchors_shape_0, bottom_H, bottom_W, img_H, img_W,
+                                min_box_H, min_box_W, conf.feat_stride_,
+                                conf.box_coordinate_scale_, conf.box_size_scale_,
+                                conf.coordinates_offset, conf.initial_clip, conf.swap_xy, conf.clip_before_nms);
+        std::partial_sort(proposals_.begin(), proposals_.begin() + pre_nms_topn, proposals_.end(),
+                          [](const ProposalBox &struct1, const ProposalBox &struct2) {
+                              return (struct1.score > struct2.score);
+                          });
+
+        unpack_boxes(reinterpret_cast<float *>(&proposals_[0]), &unpacked_boxes[0], pre_nms_topn, store_prob);
+        if (n > 0)
+            std::fill(is_dead.begin(), is_dead.end(), 0);
+#ifdef __GNUC__
+        if (__builtin_expect(static_cast<bool>(nms_kernel_), true)) {
+#else
+        if (nms_kernel_) {
+#endif
+            jit_uni_nms_proposal_kernel::jit_nms_call_args args {
+                pre_nms_topn,
+                is_dead.data(),
+                unpacked_boxes.data(),
+                &unpacked_boxes[2 * pre_nms_topn],
+                &unpacked_boxes[pre_nms_topn],
+                &unpacked_boxes[3 * pre_nms_topn],
+                roi_indices,
+                &num_rois
+            };
+            nms_kernel_->operator()(&args);
+        } else {
+            nms_cpu(pre_nms_topn, &is_dead[0], &unpacked_boxes[0], roi_indices, &num_rois, 0, conf.nms_thresh_,
+                conf.post_nms_topn_, conf.coordinates_offset);
+        }
+
+        float* p_probs = store_prob ? p_prob_item + n * conf.post_nms_topn_ : nullptr;
+        retrieve_rois_cpu(num_rois, n, pre_nms_topn, &unpacked_boxes[0], roi_indices,
+                          p_roi_item + n * conf.post_nms_topn_ * 5,
+                          conf.post_nms_topn_, conf.normalize_, img_H, img_W, conf.clip_after_nms, p_probs);
+    }
+}
+
 void Proposal::execute(dnnl::stream strm) {
     try {
         const float* probabilitiesData = reinterpret_cast<const float *>(getParentEdgeAt(PROBABILITIES_IN_IDX)->getMemoryPtr()->GetPtr());
@@ -189,8 +308,8 @@ void Proposal::execute(dnnl::stream strm) {
             IE_THROW() << "Proposal operation image info input must have non negative scales.";
         }
 
-        InferenceEngine::Extensions::Cpu::XARCH::proposal_exec(probabilitiesData, anchorsData, inProbDims,
-                {imgHeight, imgWidth, scaleHeight, scaleWidth}, anchors.data(), roi_indices.data(), outRoiData, outProbData, conf);
+        executeImpl(probabilitiesData, anchorsData, inProbDims, { imgHeight, imgWidth, scaleHeight, scaleWidth },
+            anchors.data(), roi_indices.data(), outRoiData, outProbData, conf);
     } catch (const InferenceEngine::Exception& e) {
         std::string errorMsg = e.what();
         IE_THROW() << errorMsg;
