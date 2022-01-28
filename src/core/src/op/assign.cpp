22d395a3454a6deb6e422ce17aeca05663de8462
Nam, David <david.nam@intel.com>
2022-01-28 21:02:37 +0900

    Use member variable for variable_id in assign-3, read_value-3

--- src/core/src/op/assign.cpp
+++ src/core/src/op/assign.cpp
@@ -57,8 +57,7 @@ shared_ptr<Node> op::v3::Assign::clone_with_new_inputs(const OutputVector& new_a
 
 bool op::v3::Assign::visit_attributes(AttributeVisitor& visitor) {
     NGRAPH_OP_SCOPE(v3_Assign_visit_attributes);
-    const string& variable_id = get_variable_id();
-    visitor.on_attribute("variable_id", const_cast<string&>(variable_id));
+    visitor.on_attribute("variable_id", m_variable_id);
     return true;
 }
 
