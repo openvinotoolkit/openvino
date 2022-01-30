9393ffaff694a836f2e4fbdaec222688dc16d893
Anton Pankratv <anton.pankratov@intel.com>
2022-01-30 19:13:28 +0300

    fied build

--- src/tests/functional/plugin/myriad/shared_tests_instances/behavior/ov_infer_request/infer_request_dynamic.cpp
+++ src/tests/functional/plugin/myriad/shared_tests_instances/behavior/ov_infer_request/infer_request_dynamic.cpp
@@ -23,7 +23,7 @@ const std::vector<ov::AnyMap> configs = {
 };
 
 const std::vector<ov::AnyMap> HeteroConfigs = {
-    {ov::device::priorities(CommonTestUtils::DEVICE_MYRIAD, CommonTestUtils::CPU)}
+    {ov::device::priorities(CommonTestUtils::DEVICE_MYRIAD, CommonTestUtils::DEVICE_CPU)}
 };
 
 std::shared_ptr<ngraph::Function> getFunction1() {
