7417568db29678d8593dd1cd50b26635a31e78a3
Anton Dudchenko <anton.dudchenko@intel.com>
2022-01-31 11:43:01 +0300

    Remove IE_MAIN_SOURCE_DIR dependencies

--- src/plugins/intel_myriad/myriad_dependencies.cmake
+++ src/plugins/intel_myriad/myriad_dependencies.cmake
@@ -162,7 +162,7 @@ endif()
 add_library(vpu_custom_kernels INTERFACE)
 
 function(add_vpu_compile_custom_kernels)
-    set(SRC_DIR "${IE_MAIN_SOURCE_DIR}/src/vpu/custom_kernels")
+    set(SRC_DIR "${OpenVINO_SOURCE_DIR}/src/plugins/intel_myriad/custom_kernels")
     set(DST_DIR "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/vpu_custom_kernels")
 
     file(MAKE_DIRECTORY "${DST_DIR}")
