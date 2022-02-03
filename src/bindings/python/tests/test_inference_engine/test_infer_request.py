f733f73104c20a9bbef761cceb319466dde7ab35
AlexeyLebedev1 <alexey.lebedev@intel.com>
2022-02-03 13:13:22 +0300

    Fix is_ready and add tests

--- src/bindings/python/tests/test_inference_engine/test_infer_request.py
+++ src/bindings/python/tests/test_inference_engine/test_infer_request.py
@@ -277,12 +277,22 @@ def test_infer_queue(device):
 
     img = read_image()
     infer_queue.set_callback(callback)
-    assert infer_queue.is_ready
     for i in range(jobs):
         infer_queue.start_async({"data": img}, i)
     infer_queue.wait_all()
     assert all(job["finished"] for job in jobs_done)
     assert all(job["latency"] > 0 for job in jobs_done)
+
+
+def test_infer_queue_is_ready(device):
+    core = Core()
+    model = core.read_model(test_net_xml, test_net_bin)
+    compiled = core.compile_model(model, device)
+    infer_queue = AsyncInferQueue(compiled, 1)
+    assert infer_queue.is_ready()
+    infer_queue.start_async()
+    assert not infer_queue.is_ready()
+    infer_queue.wait_all()
 
 
 def test_infer_queue_fail_on_cpp_model(device):
@@ -298,7 +308,6 @@ def test_infer_queue_fail_on_cpp_model(device):
 
     img = read_image()
     infer_queue.set_callback(callback)
-    assert infer_queue.is_ready
 
     with pytest.raises(RuntimeError) as e:
         for _ in range(jobs):
@@ -321,7 +330,6 @@ def test_infer_queue_fail_on_py_model(device):
 
     img = read_image()
     infer_queue.set_callback(callback)
-    assert infer_queue.is_ready
 
     with pytest.raises(TypeError) as e:
         for _ in range(jobs):
@@ -434,7 +442,6 @@ def test_results_async_infer(device):
 
     img = read_image()
     infer_queue.set_callback(callback)
-    assert infer_queue.is_ready
     for i in range(jobs):
         infer_queue.start_async({"data": img}, i)
     infer_queue.wait_all()
