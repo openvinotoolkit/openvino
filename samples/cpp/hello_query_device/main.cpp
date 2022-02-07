cb75841f39a71284b1039a0b403a46b66d166a9b
Anton Pankratv <anton.pankratov@intel.com>
2022-02-08 02:06:04 +0300

    fixed hello_query_device

--- samples/cpp/hello_query_device/main.cpp
+++ samples/cpp/hello_query_device/main.cpp
@@ -24,20 +24,7 @@
 void print_any_value(const ov::Any& value) {
     if (value.empty()) {
         slog::info << "EMPTY VALUE" << slog::endl;
-    } else if (value.is<bool>()) {
-        slog::info << std::boolalpha << value.as<bool>() << std::noboolalpha << slog::endl;
-    } else if (value.is<int>()) {
-        slog::info << value.as<int>() << slog::endl;
-    } else if (value.is<unsigned int>()) {
-        slog::info << value.as<unsigned int>() << slog::endl;
-    } else if (value.is<uint64_t>()) {
-        slog::info << value.as<uint64_t>() << slog::endl;
-    } else if (value.is<float>()) {
-        slog::info << value.as<float>() << slog::endl;
-    } else if (value.is<std::string>()) {
-        std::string stringValue = value.as<std::string>();
-        slog::info << (stringValue.empty() ? "\"\"" : stringValue) << slog::endl;
-    } else if (value.is<std::vector<std::string>>()) {
+    } if (value.is<std::vector<std::string>>()) {
         slog::info << value.as<std::vector<std::string>>() << slog::endl;
     } else if (value.is<std::vector<int>>()) {
         slog::info << value.as<std::vector<int>>() << slog::endl;
@@ -53,17 +40,6 @@ void print_any_value(const ov::Any& value) {
         slog::info << std::get<2>(values);
         slog::info << " }";
         slog::info << slog::endl;
-    } else if (value.is<InferenceEngine::Metrics::DeviceType>()) {
-        auto v = value.as<InferenceEngine::Metrics::DeviceType>();
-        slog::info << v << slog::endl;
-    } else if (value.is<std::map<InferenceEngine::Precision, float>>()) {
-        auto values = value.as<std::map<InferenceEngine::Precision, float>>();
-        slog::info << "{ ";
-        for (auto& kv : values) {
-            slog::info << kv.first << ": " << kv.second << "; ";
-        }
-        slog::info << " }";
-        slog::info << slog::endl;
     } else if (value.is<std::tuple<unsigned int, unsigned int>>()) {
         auto values = value.as<std::tuple<unsigned int, unsigned int>>();
         slog::info << "{ ";
@@ -72,14 +48,8 @@ void print_any_value(const ov::Any& value) {
         slog::info << " }";
         slog::info << slog::endl;
     } else {
-        std::stringstream strm;
-        value.print(strm);
-        auto str = strm.str();
-        if (str.empty()) {
-            std::cout << "UNSUPPORTED TYPE" << std::endl;
-        } else {
-            std::cout << str << std::endl;
-        }
+        std::string stringValue = value.as<std::string>();
+        slog::info << (stringValue.empty() ? "\"\"" : stringValue) << slog::endl;
     }
 }
 
