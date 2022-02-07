9543054d3ead2d4567328cda307606e9b8397594
Dmitry Kurtaev <dmitry.kurtaev@intel.com>
2022-02-07 11:15:27 +0300

    Fix ONNX import

---
+++ src/frontends/onnx/frontend/src/op/sparse_conv.cpp
@@ -0,0 +1,26 @@
+// Copyright (C) 2018-2022 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "ngraph/node.hpp"
+#include "onnx_import/core/node.hpp"
+#include "ngraph/opsets/opset1.hpp"
+
+namespace ngraph {
+namespace onnx_import {
+namespace op {
+namespace set_1 {
+OutputVector sparse_conv(const Node& node) {
+    return {std::make_shared<ngraph::opset1::SparseConv>(node.get_ng_inputs().at(0),
+                                                         node.get_ng_inputs().at(1),
+                                                         node.get_ng_inputs().at(2),
+                                                         node.get_ng_inputs().at(3),
+                                                         node.get_ng_inputs().at(4))};
+}
+}  // namespace set_1
+
+}  // namespace op
+
+}  // namespace onnx_import
+
+}  // namespace ngraph
--- src/frontends/onnx/frontend/src/op/sparse_conv.hpp
+++ src/frontends/onnx/frontend/src/op/sparse_conv.hpp
@@ -4,7 +4,6 @@
 
 #pragma once
 
-#include "default_opset.hpp"
 #include "ngraph/node.hpp"
 #include "onnx_import/core/node.hpp"
 
@@ -12,13 +11,7 @@ namespace ngraph {
 namespace onnx_import {
 namespace op {
 namespace set_1 {
-// inline OutputVector sparse_conv(const Node& node) {
-//     return {std::make_shared<ngraph::opset1::SparseConv>(node.get_ng_inputs().at(0),
-//                                                          node.get_ng_inputs().at(1),
-//                                                          node.get_ng_inputs().at(2),
-//                                                          node.get_ng_inputs().at(3),
-//                                                          node.get_ng_inputs().at(4))};
-// }
+OutputVector sparse_conv(const Node& node);
 }  // namespace set_1
 
 }  // namespace op
