13fecedc7f01d64de09bdb2ff8b076141864b4ba
Mateusz Tabaka <mateusz.tabaka@intel.com>
2022-02-09 18:47:07 +0100

    move to moc transformations

--- src/common/transformations/src/transformations/common_optimizations/moc_transformations.cpp
+++ src/common/transformations/src/transformations/common_optimizations/moc_transformations.cpp
@@ -58,6 +58,7 @@
 #include <transformations/common_optimizations/nearest_neighbor_upsampling_fusion.hpp>
 #include <transformations/common_optimizations/ric_fusion.hpp>
 #include <transformations/common_optimizations/matmul_multiply_fusion.hpp>
+#include "transformations/common_optimizations/align_eltwise_input_ranks.hpp"
 
 NGRAPH_RTTI_DEFINITION(ngraph::pass::MOCTransformations, "MOCTransformations", 0);
 
@@ -178,6 +179,8 @@ bool ngraph::pass::MOCTransformations::run_on_model(const std::shared_ptr<ngraph
     multiply_fusions->set_name("ngraph::pass::MultiplyFusions");
 
     manager.register_pass<ngraph::pass::ReverseInputChannelsFusion>();
+
+    manager.register_pass<ngraph::pass::AlignEltwiseInputRanks>();
 
     manager.register_pass<ngraph::pass::ConstantFolding>();
 
