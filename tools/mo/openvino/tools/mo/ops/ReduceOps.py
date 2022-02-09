23fcf318fa7779c880ad7cf570418f8d2b841c74
Chetverikov <anton.chetverikov@intel.com>
2022-02-09 13:48:15 +0300

    remove redundant input

--- tools/mo/openvino/tools/mo/ops/ReduceOps.py
+++ tools/mo/openvino/tools/mo/ops/ReduceOps.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2018-2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-import numpy
+
 import numpy as np
 
 from openvino.tools.mo.front.common.partial_infer.utils import int64_array, is_fully_defined
