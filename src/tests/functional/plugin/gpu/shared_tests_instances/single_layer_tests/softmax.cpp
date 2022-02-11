4423fa3bf863cc08a8404455bb0a95483bc6b589
Oleksandr Poluektov <opoluektov@lohika.com>
2022-02-11 17:55:22 +0200

    [GPU] Add Softmax single layer test that checks 5D case
    Since some Softmax kernel code contains ifdef on 5-dimensional case,
    a test case is needed that covers this functionality.

--- src/tests/functional/plugin/gpu/shared_tests_instances/single_layer_tests/softmax.cpp
+++ src/tests/functional/plugin/gpu/shared_tests_instances/single_layer_tests/softmax.cpp
@@ -67,4 +67,29 @@ INSTANTIATE_TEST_SUITE_P(
         SoftMaxLayerTest::getTestCaseName
 );
 
+const std::vector<ov::Shape> inputShapes5D = {
+    {1, 100, 1, 1, 1},
+    {1, 3, 4, 3, 4},
+    {2, 3, 4, 5, 6},
+};
+
+const std::vector<int64_t> axis5D = {-4, -3, -2, -1, 0, 1, 2, 3, 4};
+
+const auto params5D = testing::Combine(
+    testing::ValuesIn(netPrecisions),
+    ::testing::Values(ov::element::undefined),
+    ::testing::Values(ov::element::undefined),
+    testing::ValuesIn(ov::test::static_shapes_to_test_representation(inputShapes5D)),
+    testing::ValuesIn(axis5D),
+    testing::Values(CommonTestUtils::DEVICE_GPU),
+    testing::Values(ov::AnyMap())
+);
+
+INSTANTIATE_TEST_SUITE_P(
+        smoke_SoftMax5D,
+        SoftMaxLayerTest,
+        params5D,
+        SoftMaxLayerTest::getTestCaseName
+);
+
 }  // namespace
