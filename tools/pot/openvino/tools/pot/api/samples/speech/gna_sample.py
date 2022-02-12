6de749f482b30b67eb2d2bb8506ce462c4baccb2
Nikita Malinin <nikita.malinin@intel.com>
2022-02-12 18:57:35 +0300

    [POT] Update IEEngine for SW API support (#10304)
    * Update IEEngine for SW API support
    
    * Change Engine for GNA sample
    
    * Change stacks into reshape

--- tools/pot/openvino/tools/pot/api/samples/speech/gna_sample.py
+++ tools/pot/openvino/tools/pot/api/samples/speech/gna_sample.py
@@ -8,7 +8,7 @@ from openvino.tools.pot import load_model, save_model, create_pipeline
 from openvino.tools.pot.utils.logger import init_logger
 from openvino.tools.pot.api.samples.utils.argument_parser import get_common_argparser
 from openvino.tools.pot.api.samples.speech.data_loader import ArkDataLoader
-from openvino.tools.pot.api.samples.speech.utils import ArkEngine
+from openvino.tools.pot.engines.simplified_engine import SimplifiedEngine
 
 
 def parse_args():
@@ -111,7 +111,7 @@ def get_configs(args):
 def optimize_model(args):
     model_config, engine_config, dataset_config, algorithms = get_configs(args)
     data_loader = ArkDataLoader(dataset_config)
-    engine = ArkEngine(config=engine_config, data_loader=data_loader)
+    engine = SimplifiedEngine(config=engine_config, data_loader=data_loader)
     pipeline = create_pipeline(algorithms, engine)
 
     model = load_model(model_config, target_device='GNA')
