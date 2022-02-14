809f10f9483dbfa1b3348771ad6f5c1f69b77c1d
intel-ava14 <wilson.seok@intel.com>
2022-02-15 01:07:40 +0900

    add op imple check tests

--- src/tests/functional/plugin/shared/src/single_layer_tests/op_impl_check/single_op_graph.cpp
+++ src/tests/functional/plugin/shared/src/single_layer_tests/op_impl_check/single_op_graph.cpp
@@ -68,6 +68,74 @@ std::shared_ptr<ov::Model> generateDeformableConvolutionBase(const std::shared_p
     ngraph::ResultVector results{std::make_shared<ngraph::opset1::Result>(deformableConvolutionNode)};
     return std::make_shared<ngraph::Function>(results, params, "DeformableConvolutionBaseGraph");
 }
+
+std::shared_ptr<ov::Model> generateDetectionOutputBase(const std::shared_ptr<ov::op::Op> &node) {
+    const auto params = ngraph::builder::makeDynamicParams(ov::element::f32, {{2, 8},
+                                                                              {2, 6},
+                                                                              {2, 1, 8}});
+    ov::op::v0::DetectionOutput::Attributes attrs;
+    ov::op::v8::DetectionOutput::Attributes attrs_v8;
+    attrs.num_classes = 3;
+    attrs_v8.background_label_id = attrs.background_label_id = -1;
+    attrs_v8.top_k = attrs.top_k = -1;
+    attrs_v8.variance_encoded_in_target = attrs.variance_encoded_in_target = true;
+    attrs_v8.keep_top_k = attrs.keep_top_k = {2};
+    attrs_v8.code_type = attrs.code_type = "caffe.PriorBoxParameter.CORNER";
+    attrs_v8.share_location = attrs.share_location = true;
+    attrs_v8.nms_threshold = attrs.nms_threshold = 0.5;
+    attrs_v8.confidence_threshold = attrs.confidence_threshold = 0.3;
+    attrs_v8.clip_after_nms = attrs.clip_after_nms = false;
+    attrs_v8.clip_before_nms = attrs.clip_before_nms = true;
+    attrs_v8.decrease_label_id = attrs.decrease_label_id = false;
+    attrs_v8.normalized = attrs.normalized = true;
+    attrs_v8.input_height = attrs.input_height = 0;
+    attrs_v8.input_width = attrs.input_width = 0;
+    attrs_v8.objectness_score = attrs.objectness_score = 0;
+
+    std::shared_ptr<ov::Node> DetectionOutputNode;
+    if (ov::is_type<ov::op::v0::DetectionOutput>(node)) {
+        DetectionOutputNode = std::make_shared<ov::op::v0::DetectionOutput>(params[0], params[1], params[2], attrs);
+    } else if (ov::is_type<ov::op::v8::DetectionOutput>(node)) {
+        DetectionOutputNode = std::make_shared<ov::op::v8::DetectionOutput>(params[0], params[1], params[2], attrs_v8);
+    } else {
+        return nullptr;
+    }
+
+    ngraph::ResultVector results{std::make_shared<ngraph::opset1::Result>(DetectionOutputNode)};
+    return std::make_shared<ngraph::Function>(results, params, "DetectionOutputBaseGraph");
+}
+
+std::shared_ptr<ov::Model> generateEmbeddingBagOffsetsBase(const std::shared_ptr<ov::op::Op> &node) {
+    const auto params = ngraph::builder::makeDynamicParams(ov::element::f32, {{5, 2}});
+    const auto indices = ngraph::builder::makeConstant<int32_t>(ov::element::i32, {4}, {}, true);
+    const auto offsets = ngraph::builder::makeConstant<int32_t>(ov::element::i32, {3}, {}, true);
+    const auto default_index = ngraph::builder::makeConstant<int32_t>(ov::element::i32, ov::Shape(), std::vector<int32_t>{0});
+
+    std::shared_ptr<ov::Node> EmbeddingBagOffsetsSumNode;
+    if (ov::is_type<ov::op::v3::EmbeddingBagOffsetsSum>(node)) {
+        EmbeddingBagOffsetsSumNode = std::make_shared<ov::op::v3::EmbeddingBagOffsetsSum>(params[0], indices, offsets, default_index);
+    } else {
+        return nullptr;
+    }
+
+    ngraph::ResultVector results{std::make_shared<ngraph::opset1::Result>(EmbeddingBagOffsetsSumNode)};
+    return std::make_shared<ngraph::Function>(results, params, "EmbeddingBagOffsetsBaseGraph");
+}
+
+std::shared_ptr<ov::Model> generateEmbeddingBagPackedBase(const std::shared_ptr<ov::op::Op> &node) {
+    const auto params = ngraph::builder::makeDynamicParams(ov::element::f32, {{5, 2}});
+    const auto indices = ngraph::builder::makeConstant<int32_t>(ov::element::i32, {2, 3}, {}, true);
+
+    std::shared_ptr<ov::Node> EmbeddingBagPackedSumNode;
+    if (ov::is_type<ov::op::v3::EmbeddingBagPackedSum>(node)) {
+        EmbeddingBagPackedSumNode = std::make_shared<ov::op::v3::EmbeddingBagPackedSum>(params[0], indices);
+    } else {
+        return nullptr;
+    }
+
+    ngraph::ResultVector results{std::make_shared<ngraph::opset1::Result>(EmbeddingBagPackedSumNode)};
+    return std::make_shared<ngraph::Function>(results, params, "EmbeddingBagPackedBaseGraph");
+}
 } // namespace
 
 template <typename T>
@@ -77,6 +145,12 @@ std::shared_ptr<ov::Model> generateGraph() {
         return generateBinaryEltwise(node);
     } else if (ov::is_type<ov::op::util::DeformableConvolutionBase>(node)) {
         return generateDeformableConvolutionBase(node);
+    } else if (ov::is_type<ov::op::util::DetectionOutputBase>(node)) {
+        return generateDetectionOutputBase(node);
+    } else if (ov::is_type<ov::op::util::EmbeddingBagOffsetsBase>(node)) {
+        return generateEmbeddingBagOffsetsBase(node);
+    } else if (ov::is_type<ov::op::util::EmbeddingBagPackedBase>(node)) {
+        return generateEmbeddingBagPackedBase(node);
     }
     return generate(node);
 }
