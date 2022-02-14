f3d706e253fbce8da70ad7e30ad55985fe9ece8a
Anton Pankratv <anton.pankratov@intel.com>
2022-02-14 17:54:29 +0300

    removed dbg

--- samples/cpp/benchmark_app/main.cpp
+++ samples/cpp/benchmark_app/main.cpp
@@ -206,7 +206,6 @@ int main(int argc, char* argv[]) {
                     std::stringstream strm;
                     strm << ov::hint::PerformanceMode::THROUGHPUT;
                     FLAGS_hint = strm.str();
-                    dbg(FLAGS_hint);
                 }
             }
         }
