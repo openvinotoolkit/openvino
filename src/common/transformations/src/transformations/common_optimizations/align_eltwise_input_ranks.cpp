abfaee71a33eb34639760ff8a35f9dd7ab6e7307
Mateusz Tabaka <mateusz.tabaka@intel.com>
2022-02-14 09:06:22 +0000

    use single wrap_type

--- src/common/transformations/src/transformations/common_optimizations/align_eltwise_input_ranks.cpp
+++ src/common/transformations/src/transformations/common_optimizations/align_eltwise_input_ranks.cpp
@@ -6,30 +6,20 @@
 
 #include <ngraph/opsets/opset8.hpp>
 #include <ngraph/pattern/op/wrap_type.hpp>
-#include <ngraph/pattern/op/or.hpp>
 
 NGRAPH_RTTI_DEFINITION(ngraph::pass::AlignEltwiseInputRanks, "AlignEltwiseInputRanks", 0);
 
 ngraph::pass::AlignEltwiseInputRanks::AlignEltwiseInputRanks() {
-    auto input_pattern = pattern::any_input();
-    auto const_pattern = pattern::wrap_type<opset8::Constant>();
     auto eltwise_pattern = pattern::wrap_type<opset8::SquaredDifference,
                                               op::util::BinaryElementwiseComparison,
                                               op::util::BinaryElementwiseLogical,
-                                              op::util::BinaryElementwiseArithmetic>({input_pattern, const_pattern}, pattern::has_static_rank());
-
-    auto input_low_pattern = pattern::wrap_type<opset8::Constant>();
-    auto input_high_pattern = pattern::wrap_type<opset8::Constant>();
-    auto output_low_pattern = pattern::wrap_type<opset8::Constant>();
-    auto output_high_pattern = pattern::wrap_type<opset8::Constant>();
-    auto fq_pattern = pattern::wrap_type<opset8::FakeQuantize>({input_pattern, input_low_pattern, input_high_pattern,
-                                                                output_low_pattern, output_high_pattern}, pattern::has_static_rank());
-    auto eltwise_or_fq = std::make_shared<pattern::op::Or>(OutputVector{eltwise_pattern, fq_pattern});
+                                              op::util::BinaryElementwiseArithmetic,
+                                              opset8::FakeQuantize>(pattern::has_static_rank());
 
     matcher_pass_callback callback = [=](pattern::Matcher& m) {
         auto node = m.get_match_root();
 
-        auto fq = dynamic_cast<opset8::FakeQuantize*>(node.get());
+        auto fq = as_type<opset8::FakeQuantize>(node.get());
         if (fq) {
             if (fq->get_auto_broadcast() != ngraph::op::AutoBroadcastType::NUMPY) {
                 return false;
@@ -38,17 +28,21 @@ ngraph::pass::AlignEltwiseInputRanks::AlignEltwiseInputRanks() {
             return false;
         }
 
+        // NormalizeL2 and Multiply can be fused to NormalizeIE.
+        // NormalizeIE has an attribute called channel_shared, which is set
+        // based on Multiply's constant input rank - it's true if the rank is 1.
+        // So we skip extending Multiply's constant input rank here.
         if (ov::is_type<opset8::Multiply>(node)) {
-            const auto& map = m.get_pattern_value_map();
-            const auto& mul_input = map.at(input_pattern);
-            if (ov::is_type<opset8::NormalizeL2>(mul_input.get_node()))
+            auto inputs = node->input_values();
+            if (std::any_of(inputs.begin(), inputs.end(),
+                [] (const Output<Node>& input) -> bool { return ov::is_type<opset8::NormalizeL2>(input.get_node()); }))
                 return false;
         }
 
         const auto rank = node->get_output_partial_shape(0).size();
 
         for (size_t i = 0; i < node->get_input_size(); i++) {
-            auto const_node = dynamic_cast<op::Constant*>(node->get_input_node_ptr(i));
+            auto const_node = as_type<op::Constant>(node->get_input_node_ptr(i));
             if (const_node == nullptr)
                 continue;
             const auto& const_shape = const_node->get_shape();
@@ -64,6 +58,6 @@ ngraph::pass::AlignEltwiseInputRanks::AlignEltwiseInputRanks() {
         return false;
     };
 
-    auto m = std::make_shared<pattern::Matcher>(eltwise_or_fq, "AlignEltwiseInputRanks");
+    auto m = std::make_shared<pattern::Matcher>(eltwise_pattern, "AlignEltwiseInputRanks");
     this->register_matcher(m, callback);
 }
