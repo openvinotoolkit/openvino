6044baa505aeef438fb7aea0a382505d12fd3716
Anton Pankratv <anton.pankratov@intel.com>
2022-02-14 19:37:32 +0300

    simplified code

--- samples/cpp/benchmark_app/main.cpp
+++ samples/cpp/benchmark_app/main.cpp
@@ -195,6 +195,7 @@ int main(int argc, char* argv[]) {
             slog::info << "GPU extensions is loaded " << ext << slog::endl;
         }
 
+        ov::hint::PerformanceMode ov_perf_hint = ov::hint::PerformanceMode::UNDEFINED;
         if (FLAGS_hint.empty()) {
             for (auto& device : devices) {
                 auto supported_properties = core.get_property(device, ov::supported_properties);
@@ -203,9 +204,7 @@ int main(int argc, char* argv[]) {
                     slog::warn << "-hint default value is determined as " << ov::hint::PerformanceMode::THROUGHPUT
                                << " automatically for " << device
                                << " device. For more detailed information look at README." << slog::endl;
-                    std::stringstream strm;
-                    strm << ov::hint::PerformanceMode::THROUGHPUT;
-                    FLAGS_hint = strm.str();
+                    ov_perf_hint = ov::hint::PerformanceMode::THROUGHPUT;
                 }
             }
         }
@@ -217,17 +216,10 @@ int main(int argc, char* argv[]) {
         // ----------------- 3. Setting device configuration
         // -----------------------------------------------------------
         next_step();
-        ov::hint::PerformanceMode ov_perf_hint = ov::hint::PerformanceMode::UNDEFINED;
-        {
-            std::stringstream tstream;
-            tstream << ov::hint::PerformanceMode::THROUGHPUT;
-            std::stringstream lstream;
-            lstream << ov::hint::PerformanceMode::LATENCY;
-            if (FLAGS_hint == "throughput" || FLAGS_hint == "tput" || FLAGS_hint == tstream.str())
-                ov_perf_hint = ov::hint::PerformanceMode::THROUGHPUT;
-            else if (FLAGS_hint == "latency" || FLAGS_hint == lstream.str())
-                ov_perf_hint = ov::hint::PerformanceMode::LATENCY;
-        }
+        if (FLAGS_hint == "throughput" || FLAGS_hint == "tput")
+            ov_perf_hint = ov::hint::PerformanceMode::THROUGHPUT;
+        else if (FLAGS_hint == "latency")
+            ov_perf_hint = ov::hint::PerformanceMode::LATENCY;
 
         auto getDeviceTypeFromName = [](std::string device) -> std::string {
             return device.substr(0, device.find_first_of(".("));
