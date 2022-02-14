b51f451d43469dc0322d9dd991ad9cb63e7bc207
Anastasia Kuporosova <anastasia.kuporosova@intel.com>
2022-02-14 11:24:29 +0300

    [Python API] Update python test installation (#10283)

--- src/bindings/python/tests_compatibility/test_onnx/test_backend.py
+++ src/bindings/python/tests_compatibility/test_onnx/test_backend.py
@@ -114,11 +114,9 @@ tests_expected_to_fail = [
     (
         xfail_issue_39662,
         "OnnxBackendNodeModelTest.test_scatter_elements_with_negative_indices_cpu",
-        "OnnxBackendNodeModelTest.test_gather_negative_indices_cpu",
     ),
     (
         xfail_issue_38091,
-        "OnnxBackendNodeModelTest.test_gather_negative_indices_cpu",
         "OnnxBackendNodeModelTest.test_dynamicquantizelinear_cpu",
         "OnnxBackendNodeModelTest.test_dynamicquantizelinear_expanded_cpu",
     ),
