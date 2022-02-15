323fc06fd21ba0a4f70e4b119930551ebf124f43
Derbasov, Maksim <maksim.derbasov@intel.com>
2022-02-15 19:13:21 +0300

    Fix for ngraph tests generateVector

--- src/tests/ngraph_helpers/ngraph_functions/include/ngraph_functions/utils/data_utils.hpp
+++ src/tests/ngraph_helpers/ngraph_functions/include/ngraph_functions/utils/data_utils.hpp
@@ -4,23 +4,22 @@
 
 #pragma once
 
-#include <vector>
-#include <random>
-#include <chrono>
-
 #include <ngraph/type/element_type.hpp>
+#include <random>
+#include <vector>
+
 #include "ngraph_helpers.hpp"
+
 
 namespace NGraphFunctions {
 namespace Utils {
 
-
-template<ngraph::element::Type_t dType>
-std::vector<typename ngraph::helpers::nGraphTypesTrait<dType>::value_type> inline
-generateVector(size_t vec_len,
-               typename ngraph::helpers::nGraphTypesTrait<dType>::value_type upTo = 10,
-               typename ngraph::helpers::nGraphTypesTrait<dType>::value_type startFrom = 1,
-               int32_t seed = 1) {
+template <ngraph::element::Type_t dType>
+std::vector<typename ngraph::helpers::nGraphTypesTrait<dType>::value_type> inline generateVector(
+    size_t vec_len,
+    typename ngraph::helpers::nGraphTypesTrait<dType>::value_type upTo = 10,
+    typename ngraph::helpers::nGraphTypesTrait<dType>::value_type startFrom = 1,
+    int32_t seed = 1) {
     using dataType = typename ngraph::helpers::nGraphTypesTrait<dType>::value_type;
     std::vector<dataType> res(vec_len);
 
@@ -34,7 +33,13 @@ generateVector(size_t vec_len,
         for (size_t i = 1; i < vec_len - 1; i++) {
             res[i] = static_cast<dataType>(dist(gen));
         }
-        return res;
+
+    } else if (std::is_same<dataType, bool>::value) {
+        std::bernoulli_distribution dist;
+        for (size_t i = 0; i < vec_len; i++) {
+            res[i] = dist(gen);
+        }
+
     } else {
         // chose values between this range to avoid type overrun (e.g. in case of I8 precision)
         std::uniform_int_distribution<long> dist(static_cast<long>(startFrom), static_cast<long>(upTo));
@@ -44,12 +49,14 @@ generateVector(size_t vec_len,
         for (size_t i = 1; i < vec_len - 1; i++) {
             res[i] = static_cast<dataType>(dist(gen));
         }
-        return res;
     }
+    return res;
 }
 
-std::vector<ngraph::float16> inline
-generateF16Vector(size_t vec_len, ngraph::float16 upTo = 10, ngraph::float16 startFrom = 1, int32_t seed = 1) {
+std::vector<ngraph::float16> inline generateF16Vector(size_t vec_len,
+                                                      ngraph::float16 upTo = 10,
+                                                      ngraph::float16 startFrom = 1,
+                                                      int32_t seed = 1) {
     std::vector<ngraph::float16> res(vec_len);
     std::mt19937 gen(seed);
     // chose values between this range to avoid type overrun (e.g. in case of I8 precision)
@@ -63,13 +70,16 @@ generateF16Vector(size_t vec_len, ngraph::float16 upTo = 10, ngraph::float16 sta
     return res;
 }
 
-std::vector<ngraph::bfloat16> inline
-generateBF16Vector(size_t vec_len, ngraph::bfloat16 upTo = 10, ngraph::bfloat16 startFrom = 1, int32_t seed = 1) {
+std::vector<ngraph::bfloat16> inline generateBF16Vector(size_t vec_len,
+                                                        ngraph::bfloat16 upTo = 10,
+                                                        ngraph::bfloat16 startFrom = 1,
+                                                        int32_t seed = 1) {
     std::vector<ngraph::bfloat16> res(vec_len);
 
     std::mt19937 gen(seed);
     // chose values between this range to avoid type overrun (e.g. in case of I8 precision)
-    std::uniform_real_distribution<float> dist(startFrom, upTo);;
+    std::uniform_real_distribution<float> dist(startFrom, upTo);
+    ;
     // explicitly include data range borders to avoid missing the corner values while data generation
     res[0] = startFrom;
     res[vec_len - 1] = upTo;
@@ -79,11 +89,11 @@ generateBF16Vector(size_t vec_len, ngraph::bfloat16 upTo = 10, ngraph::bfloat16 
     return res;
 }
 
-template<typename fromType, typename toType>
-std::vector<toType> castVector(const std::vector<fromType> &vec) {
+template <typename fromType, typename toType>
+std::vector<toType> castVector(const std::vector<fromType>& vec) {
     std::vector<toType> resVec;
     resVec.reserve(vec.size());
-    for (const auto &el : vec) {
+    for (const auto& el : vec) {
         resVec.push_back(static_cast<toType>(el));
     }
     return resVec;
