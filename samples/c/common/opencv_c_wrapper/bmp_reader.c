3d82df10b4900cf80442137c62b5061b97037d43
antonrom23 <anton.romanov@intel.com>
2022-02-16 14:05:16 +0300

    Delete goto

--- samples/c/common/opencv_c_wrapper/bmp_reader.c
+++ samples/c/common/opencv_c_wrapper/bmp_reader.c
@@ -4,6 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define CLEANUP_AND_RETURN(x)        \
+    if (x && !image && !image->data) \
+        free(image->data);           \
+    if (!input)                      \
+        fclose(input);               \
+    return x;
+
 int readBmpImage(const char* fileName, BitMap* image) {
     size_t cnt;
     int status = 0;
@@ -11,8 +18,7 @@ int readBmpImage(const char* fileName, BitMap* image) {
 
     if (NULL == fileName || NULL == image) {
         printf("[BMP] bad arguments\n");
-        status = -1;
-        goto Exit;
+        CLEANUP_AND_RETURN(-1);
     }
 
     memset(image, 0, sizeof(BitMap));
@@ -20,49 +26,42 @@ int readBmpImage(const char* fileName, BitMap* image) {
     input = fopen(fileName, "rb");
     if (input == NULL) {
         printf("[BMP] file %s is not opened\n", fileName);
-        status = 1;
-        goto Exit;
+        CLEANUP_AND_RETURN(-1);
     }
 
     cnt = fread(&image->header.type, sizeof(image->header.type), sizeof(unsigned char), input);
     if (cnt != sizeof(image->header.type)) {
         printf("[BMP] file read error\n");
-        status = 2;
-        goto Exit;
+        CLEANUP_AND_RETURN(-2);
     }
 
     if (image->header.type != 'M' * 256 + 'B') {
         printf("[BMP] file is not bmp type\n");
-        status = 2;
-        goto Exit;
+        CLEANUP_AND_RETURN(2);
     }
 
     cnt = fread(&image->header.size, sizeof(image->header.size), sizeof(unsigned char), input);
     if (cnt != sizeof(image->header.size)) {
         printf("[BMP] file read error\n");
-        status = 2;
-        goto Exit;
+        CLEANUP_AND_RETURN(2);
     }
 
     cnt = fread(&image->header.reserved, sizeof(image->header.reserved), sizeof(unsigned char), input);
     if (cnt != sizeof(image->header.reserved)) {
         printf("[BMP] file read error\n");
-        status = 2;
-        goto Exit;
+        CLEANUP_AND_RETURN(2);
     }
 
     cnt = fread(&image->header.offset, sizeof(image->header.offset), sizeof(unsigned char), input);
     if (cnt != sizeof(image->header.offset)) {
         printf("[BMP] file read error\n");
-        status = 2;
-        goto Exit;
+        CLEANUP_AND_RETURN(2);
     }
 
     cnt = fread(&image->infoHeader, sizeof(BmpInfoHeader), sizeof(unsigned char), input);
     if (cnt != sizeof(image->header.offset)) {
         printf("[BMP] file read error\n");
-        status = 2;
-        goto Exit;
+        CLEANUP_AND_RETURN(2);
     }
 
     image->width = image->infoHeader.width;
@@ -70,14 +69,12 @@ int readBmpImage(const char* fileName, BitMap* image) {
 
     if (image->infoHeader.bits != 24) {
         printf("[BMP] 24bpp only supported. But input has: %d\n", image->infoHeader.bits);
-        status = 3;
-        goto Exit;
+        CLEANUP_AND_RETURN(3);
     }
 
     if (image->infoHeader.compression != 0) {
         printf("[BMP] compression not supported\n");
-        status = 4;
-        goto Exit;
+        CLEANUP_AND_RETURN(4);
     }
 
     int padSize = image->width & 3;
@@ -88,14 +85,12 @@ int readBmpImage(const char* fileName, BitMap* image) {
     image->data = malloc(sizeof(char) * size);
     if (NULL == image->data) {
         printf("[BMP] memory allocation failed\n");
-        status = 5;
-        goto Exit;
+        CLEANUP_AND_RETURN(5);
     }
 
     if (0 != fseek(input, image->header.offset, SEEK_SET)) {
         printf("[BMP] file seek error\n");
-        status = 2;
-        goto Exit;
+        CLEANUP_AND_RETURN(2);
     }
 
     // reading by rows in invert vertically
@@ -106,25 +101,14 @@ int readBmpImage(const char* fileName, BitMap* image) {
         cnt = fread(image->data + row_size * storeAt, row_size, sizeof(unsigned char), input);
         if (cnt != row_size) {
             printf("[BMP] file read error\n");
-            status = 2;
-            goto Exit;
+            CLEANUP_AND_RETURN(2);
         }
 
         cnt = fread(pad, padSize, sizeof(unsigned char), input);
         if (cnt != padSize) {
             printf("[BMP] file read error\n");
-            status = 2;
-            goto Exit;
+            CLEANUP_AND_RETURN(2);
         }
-    }
-
-Exit:
-    if (0 != status && NULL != image && NULL != image->data) {
-        free(image->data);
-    }
-
-    if (NULL != input) {
-        fclose(input);
     }
 
     return status;
