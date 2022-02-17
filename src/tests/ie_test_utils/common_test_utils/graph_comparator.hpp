3062fe1b9e350c796b5f311a5922a795ab4a3dfe
Smirnov Grigorii <grigorii.smirnov@intel.com>
2022-02-17 15:36:43 +0300

    add / in cmake and remove set_tensor_name(s)

--- src/tests/ie_test_utils/common_test_utils/graph_comparator.hpp
+++ src/tests/ie_test_utils/common_test_utils/graph_comparator.hpp
@@ -104,10 +104,6 @@ inline std::pair<bool, std::string> compare_functions(const std::shared_ptr<ngra
 
 void check_rt_info(const std::shared_ptr<ngraph::Function>& f);
 
-void set_tensor_name(ngraph::Output<ngraph::Node> output, const std::string& name);
-
-void set_tensor_names(ngraph::Output<ngraph::Node> output, const std::unordered_set<std::string>& names);
-
 namespace ngraph {
 namespace pass {
 class InjectionPass : public ov::pass::ModelPass {
