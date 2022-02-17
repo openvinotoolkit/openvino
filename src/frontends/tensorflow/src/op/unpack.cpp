937e1470430caf2078a27b2665f03bde93554f46
Maxim Vafin <maxim.vafin@intel.com>
2022-02-17 19:53:39 +0300

    Fix Unpack translator in TF FE

--- src/frontends/tensorflow/src/op/unpack.cpp
+++ src/frontends/tensorflow/src/op/unpack.cpp
@@ -19,9 +19,16 @@ OutputVector translate_unpack_op(const NodeContext& node) {
     auto num = node.get_attribute<int64_t>("num");
 
     auto axis_const = make_shared<Constant>(element::i64, Shape{}, axis);
-    auto res = make_shared<Split>(input, axis_const, num);
-    set_node_name(node.get_name(), res);
-    return res->outputs();
+    auto split = make_shared<Split>(input, axis_const, num);
+    set_node_name(node.get_name(), split);
+    OutputVector res;
+    int i = 0;
+    for (auto out : split->outputs()) {
+        auto squeezed_res = make_shared<Squeeze>(out, axis_const);
+        set_node_name(node.get_name() + "/squeeze_" + to_string(i++), squeezed_res);
+        res.push_back(squeezed_res);
+    }
+    return res;
 }
 }  // namespace op
 }  // namespace tensorflow
