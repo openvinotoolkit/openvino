e093a639ab5037bf51c1dfd76ff11967ec4a2901
Pavel Esir <pavel.esir@intel.com>
2022-02-18 22:40:28 +0300

    fix ConvertGroupedStridedSlice.py for XLNet (#10496)

--- tools/mo/openvino/tools/mo/middle/ConvertGroupedStridedSlice.py
+++ tools/mo/openvino/tools/mo/middle/ConvertGroupedStridedSlice.py
@@ -91,6 +91,10 @@ class ConvertGroupedStridedSlice(MiddleReplacementPattern):
 
             sorted_out_nodes = sorted(out_nodes, key=lambda n: list(n.slices))
             out_nodes = unique_by(sorted_out_nodes, strided_slices_equality)
+            # if there is only one StridedSlice out_node with unique 'slices',
+            # there is nothing to optimize, continue to the next data node
+            if len(out_nodes) <= 1:
+                continue
 
             for node in out_nodes:
                 if len(node.slices) != len(out_nodes[0].slices):
