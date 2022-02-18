9bf0713f44f2a82571c2aa15dcddc17c42fc944b
Efode, Irina <irina.efode@intel.com>
2022-02-18 10:18:01 +0300

    tmp

--- src/tests/functional/plugin/shared/src/behavior/infer_request/set_io_blob_precision.cpp
+++ src/tests/functional/plugin/shared/src/behavior/infer_request/set_io_blob_precision.cpp
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "set_io_blob_precision.hpp"
+#include "behavior/infer_request/set_io_blob_precision.hpp"
 #include "common_test_utils/test_constants.hpp"
 #include "ngraph_functions/builders.hpp"
 
@@ -114,28 +114,7 @@ void SetBlobTest::SetUp() {
 }
 
 TEST_P(SetBlobTest, CompareWithRefs) {
-Run();
+    Run();
 }
 
 } // namespace BehaviorTestsDefinitions
-
-using namespace BehaviorTestsDefinitions;
-
-const std::vector<Precision> precisionSet = {
-    Precision::U8,   Precision::I8,
-    Precision::U16,  Precision::I16,
-    Precision::U32,  Precision::I32,
-    Precision::U64,  Precision::I64,
-    Precision::BF16, Precision::FP16,
-    Precision::FP32, Precision::FP64,
-    Precision::BOOL
-};
-
-const std::vector<setType> typeSet = {setType::INPUT, setType::OUTPUT, setType::BOTH};
-
-const auto params = ::testing::Combine(::testing::ValuesIn(precisionSet),
-                                       ::testing::ValuesIn(precisionSet),
-                                       ::testing::ValuesIn(typeSet),
-                                       ::testing::Values(CommonTestUtils::DEVICE_CPU));
-
-INSTANTIATE_TEST_SUITE_P(smoke_SetBlobCPU, SetBlobTest, params, SetBlobTest::getTestCaseName);
