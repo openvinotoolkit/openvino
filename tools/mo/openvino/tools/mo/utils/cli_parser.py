0a3273d843947ef439a328b159e572a3687aa2db
Ivan Tikhonov <ivan.tikhonov@intel.com>
2022-02-18 19:42:49 +0300

    Update --transform help for MakeStateful transformation

--- tools/mo/openvino/tools/mo/utils/cli_parser.py
+++ tools/mo/openvino/tools/mo/utils/cli_parser.py
@@ -365,8 +365,10 @@ def get_common_cli_parser(parser: argparse.ArgumentParser = None):
                                    'Usage: "--transform transformation_name1[args],transformation_name2..." ' +
                                    'where [args] is key=value pairs separated by semicolon. ' +
                                    'Examples: "--transform LowLatency2" or ' +
-                                   '          "--transform LowLatency2[use_const_initializer=False]" ' +
-                                   'Available transformations: "LowLatency2"',
+                                   '          "--transform LowLatency2[use_const_initializer=False]" or ' +
+                                   '          "--transform MakeStateful[param_res_names='
+                                   '{\'input_name_1\':\'output_name_1\',\'input_name_2\':\'output_name_2\'}]" ' +
+                                   'Available transformations: "LowLatency2", "MakeStateful"',
                               default="")
     common_group.add_argument('--disable_fusing',
                               help='Turn off fusing of linear operations to Convolution',
