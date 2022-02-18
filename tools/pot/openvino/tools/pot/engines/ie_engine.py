5942c7b95f48ef2e1ff17df381bc5421a80b7a4b
Malinin, Nikita <nikita.malinin@intel.com>
2022-02-18 16:35:15 +0300

    IEEngine fix for multiply-output nets

--- tools/pot/openvino/tools/pot/engines/ie_engine.py
+++ tools/pot/openvino/tools/pot/engines/ie_engine.py
@@ -144,7 +144,7 @@ class IEEngine(Engine):
     @staticmethod
     def postprocess_output(outputs, _metadata):
         """ Processes raw model output using the image metadata obtained during data loading """
-        return outputs
+        return list(outputs.values())
 
     def _reset(self):
         """ Resets collected statistics """
@@ -182,14 +182,12 @@ class IEEngine(Engine):
                                      annotations=batch_annotations)
 
         # Postprocess network output
-        outputs = process_raw_output(predictions)
-        output = outputs[self._output_layers[0]]
-        outputs[self._output_layers[0]] = self.postprocess_output(output, batch_meta)
+        processed_outputs = process_raw_output(predictions)
+        outputs = {name: data for name, data in processed_outputs.items() if name in self._output_layers}
+        logits = self.postprocess_output(outputs, batch_meta)
 
         # Update metrics
         if batch_annotations:
-            # TODO: Create some kind of an order for the correct metric calculation
-            logits = [outputs[name] for name in self._output_layers]  # output_layers are in a random order
             self._update_metrics(output=logits, annotations=batch_annotations,
                                  need_metrics_per_sample=need_metrics_per_sample)
 
