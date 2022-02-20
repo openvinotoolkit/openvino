e1d84e23eafe4c0414b7f7711f5eaba4129784ad
indira <indira.salyahova@intel.com>
2022-02-20 10:36:44 +0300

    use scipy to take most frequent shape

--- tools/pot/openvino/tools/pot/algorithms/quantization/utils.py
+++ tools/pot/openvino/tools/pot/algorithms/quantization/utils.py
@@ -4,6 +4,8 @@
 from copy import deepcopy
 from pathlib import Path
 
+from scipy import stats
+
 from .range_estimator import get_range_estimator_config
 from ...api.engine import Engine
 from ...configs.hardware_config import HardwareConfig
@@ -338,7 +340,7 @@ def get_stat_name_by_config(config, stat_type):
 
 
 def get_input_shape_for_bias(activations_statistics, input_node_name):
-    input_shape = list(activations_statistics[input_node_name]['shape'][0])
+    input_shape = stats.mode(activations_statistics[input_node_name]['shape'])[0][0]
     if len(input_shape) > 1:
         input_shape[0] = 1
     return input_shape
