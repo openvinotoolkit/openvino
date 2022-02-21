69d8efb890708e2190f6c9ab4808ddce07f166a1
Roman Kazantsev <roman.kazantsev@intel.com>
2022-02-21 17:57:42 +0300

    [MO] Print information about new API 2.0
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- tools/mo/openvino/tools/mo/utils/get_ov_update_message.py
+++ tools/mo/openvino/tools/mo/utils/get_ov_update_message.py
@@ -14,3 +14,12 @@ def get_ov_update_message():
     link = 'https://software.intel.com/content/www/us/en/develop/tools/openvino-toolkit/download.html?cid=other&source=prod&campid=ww_2022_bu_IOTG_OpenVINO-2022-1&content=upg_all&medium=organic'
 
     return msg_fmt.format(link) if current_date >= expected_update_date else None
+
+
+def get_ov_api20_message():
+    link = "https://docs.openvino.ai/latest/documentation.html"
+    message = '[ INFO ] Starting from 2022.1 release the Model Optimizer generates IR in a format compatible with OpenVINO(TM) API 2.0. ' \
+              'OpenVINO(TM) API 2.0 provides better alignment with original frameworks such as model inputs and outputs format. ' \
+              'For more information about OpenVINO(TM) API 2.0 please follow: {}'. format(link)
+
+    return message
