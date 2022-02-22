42e2054e5c0df2b75cd99e6af43cd3a737192c2e
Andrew Kwangwoong Park <andrew.kwangwoong.park@intel.com>
2022-02-22 11:35:04 +0900

    [GPU] Fix permute performance degradation (#10559)
    * [GPU] Fix permute performance degradation
    
    Signed-off-by: Andrew Kwangwoong Park <andrew.kwangwoong.park@intel.com>
    
    * add description for update
    
    Signed-off-by: Andrew Kwangwoong Park <andrew.kwangwoong.park@intel.com>

--- src/plugins/intel_gpu/src/graph/include/permute_inst.h
+++ src/plugins/intel_gpu/src/graph/include/permute_inst.h
@@ -24,6 +24,18 @@ public:
 
     program_node& input() const { return get_dependency(0); }
     std::vector<uint16_t> get_permute_order() const { return get_primitive()->permute_order; }
+    bool is_rotating_except_batch() const {
+        // Target transform: Rotate feature dim to back to be taken as inner-most axis
+        // ex) 0(b), 4(f), 1(z), 2(y), 3(x)
+        // ex) 0(b), 3(f), 1(y), 2(x)
+        auto& order = get_primitive()->permute_order;
+        if ((int32_t) order[1] != order.size() - 1) return false;
+        if ((int32_t) order[0] != 0) return false;
+        for (int32_t i = 2; i < (int32_t) order.size(); ++i) {
+            if ((int32_t)order[i] !=  (i - 1)) return false;
+        }
+        return true;
+    }
 };
 
 using permute_node = typed_program_node<permute>;
