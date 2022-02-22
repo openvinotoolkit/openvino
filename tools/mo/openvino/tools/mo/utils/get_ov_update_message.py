773a2a2614b1d09747be5abf9b17bca192402f37
Roman Kazantsev <roman.kazantsev@intel.com>
2022-02-22 13:25:11 +0300

    Apply feedback
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- tools/mo/openvino/tools/mo/utils/get_ov_update_message.py
+++ tools/mo/openvino/tools/mo/utils/get_ov_update_message.py
@@ -18,9 +18,9 @@ def get_ov_update_message():
 
 def get_ov_api20_message():
     link = "https://docs.openvino.ai"
-    message = '[ INFO ] The model was converted to IR v11, the latest model format that aligns best with the source DL framework. ' \
-              'While IR v11 is backwards compatible with OpenVINO Inference Engine API v1.0, please use API v2.0 (as of 2022.1) ' \
-              'to take advantage of the latest improvements in IR v11.\n' \
+    message = '[ INFO ] The model was converted to IR v11, the latest model format that corresponds to the source DL framework ' \
+              'input/output format. While IR v11 is backwards compatible with OpenVINO Inference Engine API v1.0, ' \
+              'please use API v2.0 (as of 2022.1) to take advantage of the latest improvements in IR v11.\n' \
               'Find more information about API v2.0 and IR v11 at {}'.format(link)
 
     return message
