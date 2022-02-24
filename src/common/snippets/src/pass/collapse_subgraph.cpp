8ecd1e18dfe966d14c38a016be19c2aea8098a1b
Alexander Zhogov <alexander.zhogov@intel.com>
2022-02-24 09:52:43 +0300

    Revert "[Snippets] Check for cyclic dependencies during ternary merge. (#10374)"
    This reverts commit 6500ec775d2a0f38c85fbcd972a2049e130b5383.

--- src/common/snippets/src/pass/collapse_subgraph.cpp
+++ src/common/snippets/src/pass/collapse_subgraph.cpp
@@ -372,12 +372,9 @@ TokenizeSnippets::TokenizeSnippets() {
 
                             auto internal = input_body_parameters[i];
                             auto internal_consumers = internal->outputs();
+
                             if (auto to_replace_with = ov::as_type_ptr<op::Subgraph>(subgraph->get_input_node_shared_ptr(i))) {
-                                // todo: In principle, we can still attach the node to the subgraph if cyclic dependency is introduced during ternary merge.
-                                //  Need to support.
-                                if (cyclicDependencyIsIntoduced(to_replace_with, currentTopoBounds))
-                                    return abort_with_strategy("Attempt to perform recurrent merge for cyclic-dependent subgraphs. Aborting.");
-                                for (const auto& output : internal_consumers) {
+                                 for (auto output : internal_consumers) {
                                      for (auto consumer : output.get_target_inputs()) {
                                          auto other_body = clones[subgraph->get_input_node_shared_ptr(i)];
                                          auto other_body_result = other_body->get_results()[consumer.get_source_output().get_index()];
