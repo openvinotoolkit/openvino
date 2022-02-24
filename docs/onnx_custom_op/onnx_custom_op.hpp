e3bee0b266aab73ad2728bb917786d9f8dac2cdf
Ilya Churaev <ilya.churaev@intel.com>
2022-02-24 19:41:47 +0300

    Remove onnx_custom_op doc (#10638)
    * Remove onnx_custom_op doc
    
    * Remove test
    
    * Fixed tests

--- docs/onnx_custom_op/onnx_custom_op.hpp
+++
@@ -1,8 +0,0 @@
-// Copyright (C) 2018-2022 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
-//
-
-#pragma once
-
-void register_custom_relu_operator();
-void unregister_custom_relu_operator();
