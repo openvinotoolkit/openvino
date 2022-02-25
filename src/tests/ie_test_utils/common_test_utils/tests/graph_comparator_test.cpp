a97131ea1e3d43f4585b5db4a373538ddf3e7b19
Smirnov Grigorii <grigorii.smirnov@intel.com>
2022-02-25 11:09:15 +0300

    move accuracy tests to graph_comparator.cpp

--- src/tests/ie_test_utils/common_test_utils/tests/graph_comparator_test.cpp
+++ src/tests/ie_test_utils/common_test_utils/tests/graph_comparator_test.cpp
@@ -513,3 +513,78 @@ TEST(GraphComparatorTests, CheckSinksNegative) {
     auto res = comparator.compare(function, function_ref);
     ASSERT_FALSE(res.valid) << res.message;
 }
+
+TEST(GraphComparatorTests, CheckAccuracyPositive) {
+    FunctionsComparator comparator(FunctionsComparator::no_default());
+    std::shared_ptr<ov::Model> function, function_ref;
+    {
+        auto input = std::make_shared<ov::opset8::Parameter>(ngraph::element::i64, ov::Shape{1});
+        auto constant = ov::opset8::Constant::create(ngraph::element::i64, {1}, {0});
+        auto add = std::make_shared<ov::opset8::Add>(input, constant);
+        function_ref = std::make_shared<ngraph::Function>(ngraph::NodeVector{ add}, ngraph::ParameterVector{ input });
+    }
+    {
+        auto input = std::make_shared<ov::opset8::Parameter>(ngraph::element::i64, ov::Shape{1});
+        auto constant = ov::opset8::Constant::create(ngraph::element::i64, {1}, {0});
+        auto add = std::make_shared<ov::opset8::Add>(input, constant);
+        function = std::make_shared<ngraph::Function>(ngraph::NodeVector{ add}, ngraph::ParameterVector{ input });
+    }
+    comparator.enable(FunctionsComparator::ACCURACY);
+    auto res = comparator.compare(function, function_ref);
+    ASSERT_TRUE(res.valid) << res.message;
+}
+
+TEST(GraphComparatorTests, CheckAccuracyNegative) {
+    FunctionsComparator comparator(FunctionsComparator::no_default());
+    std::shared_ptr<ov::Model> function, function_ref;
+    {
+        auto input = std::make_shared<ov::opset8::Parameter>(ngraph::element::i64, ov::Shape{1});
+        auto constant = ov::opset8::Constant::create(ngraph::element::i64, {1}, {12});
+        auto add = std::make_shared<ov::opset8::Add>(input, constant);
+        function_ref = std::make_shared<ngraph::Function>(ngraph::NodeVector{ add}, ngraph::ParameterVector{ input });
+    }
+    {
+        auto input = std::make_shared<ov::opset8::Parameter>(ngraph::element::i64, ov::Shape{1});
+        auto constant = ov::opset8::Constant::create(ngraph::element::i64, {1}, {200});
+        auto add = std::make_shared<ov::opset8::Add>(input, constant);
+        function = std::make_shared<ngraph::Function>(ngraph::NodeVector{ add}, ngraph::ParameterVector{ input });
+    }
+    comparator.enable(FunctionsComparator::ACCURACY);
+    auto res = comparator.compare(function, function_ref);
+    ASSERT_FALSE(res.valid) << res.message;
+}
+
+TEST(GraphComparatorTests, CheckAccuracyNotEnabled) {
+    FunctionsComparator comparator(FunctionsComparator::no_default());
+    std::shared_ptr<ov::Model> function, function_ref;
+    {
+        auto input = std::make_shared<ov::opset8::Parameter>(ov::element::f32, ov::Shape{ 1, 3, 12, 12 });
+        auto const_weights = ov::opset8::Constant::create(ov::element::f16,
+                                                          ov::Shape{ 1, 3, 3, 3 },
+                                                          { 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+        auto convert_ins1 = std::make_shared<ov::opset8::Convert>(const_weights, ov::element::f32);
+        auto conv = std::make_shared<ov::opset8::Convolution>(input,
+                                                              convert_ins1,
+                                                              ov::Strides{ 1, 1 },
+                                                              ov::CoordinateDiff{ 1, 1 },
+                                                              ov::CoordinateDiff{ 1, 1 },
+                                                              ov::Strides{ 1, 1 });
+        function_ref = std::make_shared<ngraph::Function>(ngraph::NodeVector{ conv }, ngraph::ParameterVector{ input });
+    }
+    {
+        auto input = std::make_shared<ov::opset8::Parameter>(ov::element::f32, ov::Shape{ 1, 3, 12, 12 });
+        auto const_weights = ov::opset8::Constant::create(ov::element::f16,
+                                                          ov::Shape{ 1, 3, 3, 3 },
+                                                          { 1, 9, 3, 4, 5, 6, 7, 8, 9, 1, 12, 3, 9, 5, 0, 7, 8, 9, 1, 2, 12, 4, 9, 6, 7, 8, 9 });
+        auto convert_ins1 = std::make_shared<ov::opset8::Convert>(const_weights, ov::element::f32);
+        auto conv = std::make_shared<ov::opset8::Convolution>(input,
+                                                              convert_ins1,
+                                                              ov::Strides{ 1, 1 },
+                                                              ov::CoordinateDiff{ 1, 1 },
+                                                              ov::CoordinateDiff{ 1, 1 },
+                                                              ov::Strides{ 1, 1 });
+        function = std::make_shared<ngraph::Function>(ngraph::NodeVector{ conv }, ngraph::ParameterVector{ input });
+    }
+    auto res = comparator.compare(function, function_ref);
+    ASSERT_TRUE(res.valid) << res.message;
+}
