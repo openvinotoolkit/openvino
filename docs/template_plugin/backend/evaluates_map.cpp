dc3fc34bfb4cf6419f50148805fc83f00614436a
Nikita Semaev <nikita.semaev@intel.com>
2022-02-25 19:04:22 +0300

    Setting the correct value for selected_scores_type NMS-5

--- docs/template_plugin/backend/evaluates_map.cpp
+++ docs/template_plugin/backend/evaluates_map.cpp
@@ -857,7 +857,7 @@ bool evaluate(const shared_ptr<op::v5::NonMaxSuppression>& op,
                                             &valid_outputs,
                                             info.sort_result_descending);
 
-    auto selected_scores_type = element::f32;
+    auto selected_scores_type = (outputs.size() < 3) ? element::f32 : outputs[1]->get_element_type();
 
     runtime::reference::nms5_postprocessing(outputs,
                                             info.output_type,
