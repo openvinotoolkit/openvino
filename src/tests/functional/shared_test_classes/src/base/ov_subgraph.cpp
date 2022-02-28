6ab53f34fa3f0d608f8ac56544841315e7639af0
Nikita Semaev <nikita.semaev@intel.com>
2022-02-28 14:50:04 +0300

    Removed the static for the vector

--- src/tests/functional/shared_test_classes/src/base/ov_subgraph.cpp
+++ src/tests/functional/shared_test_classes/src/base/ov_subgraph.cpp
@@ -239,7 +239,7 @@ std::vector<ov::Tensor> SubgraphBaseTest::calculate_refs() {
 
     auto functionToProcess = ov::clone_model(*functionRefs);
     //TODO: remove this conversions as soon as function interpreter fully support bf16 and f16
-    static precisions_array precisions = {
+    precisions_array precisions = {
             { ngraph::element::bf16, ngraph::element::f32 }
     };
     auto convert_added = false;
