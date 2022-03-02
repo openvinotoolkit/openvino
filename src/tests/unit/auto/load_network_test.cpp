b96e0256ea22b834c4fe8c04149ebb420e154a84
Wang, Yang <yang4.wang@intel.com>
2022-03-02 14:47:40 +0800

    Add test case for compile_model.
    Signed-off-by: Wang, Yang <yang4.wang@intel.com>

--- src/tests/unit/auto/load_network_test.cpp
+++ src/tests/unit/auto/load_network_test.cpp
@@ -7,6 +7,8 @@
 
 #include <common_test_utils/test_constants.hpp>
 #include <ie_core.hpp>
+#include "openvino/runtime/core.hpp"
+#include "openvino/runtime/compiled_model.hpp"
 #include <ie_metric_helpers.hpp>
 #include <multi-device/multi_device_config.hpp>
 #include <ngraph_functions/subgraph_builders.hpp>
@@ -33,6 +35,7 @@ TEST(LoadNetworkToDefaultDeviceTest, LoadNetwork) {
     outfile << content;
     outfile.close();
     Core ie(pluginXML);
+    ov::Core ie_core(pluginXML);
     std::remove(pluginXML.c_str());
     std::string mockEngineName("mock_engine");
     std::string libraryName = CommonTestUtils::pre + mockEngineName + IE_BUILD_POSTFIX + CommonTestUtils::ext;
@@ -50,7 +53,7 @@ TEST(LoadNetworkToDefaultDeviceTest, LoadNetwork) {
 
     auto mockIExeNet = std::make_shared<MockIExecutableNetworkInternal>();
     EXPECT_CALL(*mockIExeNet, GetOutputsInfo())
-        .WillOnce([&]() -> ConstOutputsDataMap {
+        .WillRepeatedly([&]() -> ConstOutputsDataMap {
             ConstOutputsDataMap outputMap;
             for (const auto& output : actualCnnNetwork.getOutputsInfo()) {
                 outputMap.emplace(output.first, output.second);
@@ -59,7 +62,7 @@ TEST(LoadNetworkToDefaultDeviceTest, LoadNetwork) {
         });
 
     EXPECT_CALL(*mockIExeNet, GetInputsInfo())
-        .WillOnce([&]() -> ConstInputsDataMap {
+        .WillRepeatedly([&]() -> ConstInputsDataMap {
             ConstInputsDataMap inputMap;
             for (const auto& input : actualCnnNetwork.getInputsInfo()) {
                 inputMap.emplace(input.first, input.second);
@@ -67,12 +70,63 @@ TEST(LoadNetworkToDefaultDeviceTest, LoadNetwork) {
             return inputMap;
         });
 
-    EXPECT_CALL(*plugin, LoadExeNetworkImpl(_, _)).Times(1)
-        .WillOnce([&](const InferenceEngine::CNNNetwork&,
+    EXPECT_CALL(*plugin, LoadExeNetworkImpl(_, _)).Times(2)
+        .WillRepeatedly([&](const InferenceEngine::CNNNetwork&,
                       const std::map<std::string, std::string>&) -> InferenceEngine::IExecutableNetworkInternal::Ptr {
             return mockIExeNet;
         });
 
-
     ASSERT_NO_THROW(ie.LoadNetwork(actualCnnNetwork));
+    ASSERT_NO_THROW(ie.LoadNetwork(actualCnnNetwork, CommonTestUtils::DEVICE_AUTO));
+}
+
+TEST(CompileModelToDefaultDeviceTest, compileModel) {
+    std::string pluginXML{"mock_engine_valid.xml"};
+    std::string content{"<ie><plugins><plugin name=\"AUTO\" location=\"libmock_engine.so\"></plugin></plugins></ie>"};
+    std::ofstream outfile(pluginXML);
+    outfile << content;
+    outfile.close();
+    ov::Core ie(pluginXML);
+    std::remove(pluginXML.c_str());
+    std::string mockEngineName("mock_engine");
+    std::string libraryName = CommonTestUtils::pre + mockEngineName + IE_BUILD_POSTFIX + CommonTestUtils::ext;
+    std::shared_ptr<void> sharedObjectLoader = ov::util::load_shared_object(libraryName.c_str());
+    std::function<void(IInferencePlugin*)> injectProxyEngine(
+        reinterpret_cast<void (*)(IInferencePlugin*)>(ov::util::get_symbol(sharedObjectLoader, "InjectProxyEngine")));
+
+    auto* origin_plugin = new MockMultiDeviceInferencePlugin();
+    auto plugin  = std::shared_ptr<MockMultiDeviceInferencePlugin>(origin_plugin);
+    injectProxyEngine(origin_plugin);
+
+    InferenceEngine::CNNNetwork actualCnnNetwork;
+    std::shared_ptr<ngraph::Function> actualNetwork = ngraph::builder::subgraph::makeSplitConvConcat();
+    ASSERT_NO_THROW(actualCnnNetwork = InferenceEngine::CNNNetwork(actualNetwork));
+
+    auto mockIExeNet = std::make_shared<MockIExecutableNetworkInternal>();
+    EXPECT_CALL(*mockIExeNet, GetOutputsInfo())
+        .WillRepeatedly([&]() -> ConstOutputsDataMap {
+            ConstOutputsDataMap outputMap;
+            for (const auto& output : actualCnnNetwork.getOutputsInfo()) {
+                outputMap.emplace(output.first, output.second);
+            }
+            return outputMap;
+        });
+
+    EXPECT_CALL(*mockIExeNet, GetInputsInfo())
+        .WillRepeatedly([&]() -> ConstInputsDataMap {
+            ConstInputsDataMap inputMap;
+            for (const auto& input : actualCnnNetwork.getInputsInfo()) {
+                inputMap.emplace(input.first, input.second);
+            }
+            return inputMap;
+        });
+
+    EXPECT_CALL(*plugin, LoadExeNetworkImpl(_, _)).Times(2)
+        .WillRepeatedly([&](const InferenceEngine::CNNNetwork&,
+                      const std::map<std::string, std::string>&) -> InferenceEngine::IExecutableNetworkInternal::Ptr {
+            return mockIExeNet;
+        });
+
+    ASSERT_NO_THROW(ie.compile_model(actualNetwork));
+    ASSERT_NO_THROW(ie.compile_model(actualNetwork, CommonTestUtils::DEVICE_AUTO));
 }
