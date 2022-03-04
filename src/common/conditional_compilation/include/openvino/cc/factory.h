25dfb2a05dd0caa1b61da92e398a2a4590e943d9
Ilya Churaev <ilya.churaev@intel.com>
2022-03-04 18:40:18 +0300

    Enabled clang-format for cc and itt libs (#10793)

--- src/common/conditional_compilation/include/openvino/cc/factory.h
+++ src/common/conditional_compilation/include/openvino/cc/factory.h
@@ -3,19 +3,20 @@
 //
 
 #pragma once
-#include "selective_build.h"
-#include <string>
 #include <functional>
+#include <string>
 #include <type_traits>
 #include <unordered_map>
+
+#include "selective_build.h"
 
 namespace openvino {
 namespace cc {
 
-template<typename Key, typename T>
+template <typename Key, typename T>
 class Factory;
 
-template<typename Key, typename T, typename ...Args>
+template <typename Key, typename T, typename... Args>
 class Factory<Key, T(Args...)> {
     Factory(Factory const&) = delete;
     Factory& operator=(Factory const&) = delete;
@@ -23,27 +24,25 @@ class Factory<Key, T(Args...)> {
 public:
     using builder_t = std::function<T(Args...)>;
 
-    Factory(const std::string & name)
-        : name(name) {}
+    Factory(const std::string& name) : name(name) {}
 
 #ifdef SELECTIVE_BUILD
-    #define registerNodeIfRequired(Module, Name, key, Impl)       \
-        OV_PP_EXPAND(OV_PP_CAT(registerImpl, OV_CC_SCOPE_IS_ENABLED(OV_PP_CAT3(Module, _, Name)))<Impl>(key))
-    #define createNodeIfRegistered(Module, key, ...) createImpl(key, __VA_ARGS__)
+#    define registerNodeIfRequired(Module, Name, key, Impl) \
+        OV_PP_EXPAND(OV_PP_CAT(registerImpl, OV_CC_SCOPE_IS_ENABLED(OV_PP_CAT3(Module, _, Name))) < Impl > (key))
+#    define createNodeIfRegistered(Module, key, ...) createImpl(key, __VA_ARGS__)
 
-    template<typename Impl>
-    void registerImpl0(const Key &) {
-    }
+    template <typename Impl>
+    void registerImpl0(const Key&) {}
 
-    template<typename Impl>
-    void registerImpl1(const Key & key) {
+    template <typename Impl>
+    void registerImpl1(const Key& key) {
         builders[key] = [](Args... args) -> T {
-            Impl *impl = new Impl(args...);
+            Impl* impl = new Impl(args...);
             return static_cast<T>(impl);
         };
     }
 
-    T createImpl(const Key & key, Args... args) {
+    T createImpl(const Key& key, Args... args) {
         auto builder = builders.find(key);
         if (builder != builders.end()) {
             return builder->second(args...);
@@ -52,21 +51,22 @@ public:
     }
 
 #elif defined(SELECTIVE_BUILD_ANALYZER)
-    #define registerNodeIfRequired(Module, Name, key, Impl) registerImpl<OV_PP_CAT(FACTORY_, Module), Impl>(key, OV_PP_TOSTRING(Name))
-    #define createNodeIfRegistered(Module, key, ...) createImpl<OV_PP_CAT(FACTORY_, Module)>(key, __VA_ARGS__)
+#    define registerNodeIfRequired(Module, Name, key, Impl) \
+        registerImpl<OV_PP_CAT(FACTORY_, Module), Impl>(key, OV_PP_TOSTRING(Name))
+#    define createNodeIfRegistered(Module, key, ...) createImpl<OV_PP_CAT(FACTORY_, Module)>(key, __VA_ARGS__)
 
-    template<openvino::itt::domain_t(*domain)(), typename Impl>
-    void registerImpl(const Key & key, const char *typeName) {
+    template <openvino::itt::domain_t (*domain)(), typename Impl>
+    void registerImpl(const Key& key, const char* typeName) {
         const std::string task_name = "REG$" + name + "$" + to_string(key) + "$" + typeName;
         openvino::itt::ScopedTask<domain> task(openvino::itt::handle(task_name));
         builders[key] = [](Args... args) -> T {
-            Impl *impl = new Impl(args...);
+            Impl* impl = new Impl(args...);
             return static_cast<T>(impl);
         };
     }
 
-    template<openvino::itt::domain_t(*domain)()>
-    T createImpl(const Key & key, Args... args) {
+    template <openvino::itt::domain_t (*domain)()>
+    T createImpl(const Key& key, Args... args) {
         auto builder = builders.find(key);
         if (builder != builders.end()) {
             const std::string task_name = "CREATE$" + name + "$" + to_string(key);
@@ -78,18 +78,18 @@ public:
 
 #else
 
-    #define registerNodeIfRequired(Module, Name, key, Impl) registerImpl<Impl>(key)
-    #define createNodeIfRegistered(Module, key, ...) createImpl(key, __VA_ARGS__)
+#    define registerNodeIfRequired(Module, Name, key, Impl) registerImpl<Impl>(key)
+#    define createNodeIfRegistered(Module, key, ...)        createImpl(key, __VA_ARGS__)
 
-    template<typename Impl>
-    void registerImpl(const Key & key) {
+    template <typename Impl>
+    void registerImpl(const Key& key) {
         builders[key] = [](Args... args) -> T {
-            Impl *impl = new Impl(args...);
+            Impl* impl = new Impl(args...);
             return static_cast<T>(impl);
         };
     }
 
-    T createImpl(const Key & key, Args... args) {
+    T createImpl(const Key& key, Args... args) {
         auto builder = builders.find(key);
         if (builder != builders.end()) {
             return builder->second(args...);
@@ -98,8 +98,8 @@ public:
     }
 #endif
 
-    template<typename Fn>
-    void foreach(Fn fn) const {
+    template <typename Fn>
+    void foreach (Fn fn) const {
         for (auto itm : builders)
             fn(itm);
     }
@@ -109,18 +109,16 @@ public:
     }
 
 private:
-    const std::string & to_string(const std::string & str) const noexcept {
+    const std::string& to_string(const std::string& str) const noexcept {
         return str;
     }
 
-    template<typename V,
-            typename std::enable_if<std::is_enum<V>::value, bool>::type = true>
+    template <typename V, typename std::enable_if<std::is_enum<V>::value, bool>::type = true>
     std::string to_string(V val) const {
         return std::to_string(static_cast<int>(val));
     }
 
-    template<typename V,
-            typename std::enable_if<!std::is_enum<V>::value, bool>::type = true>
+    template <typename V, typename std::enable_if<!std::is_enum<V>::value, bool>::type = true>
     std::string to_string(V val) const {
         return std::to_string(val);
     }
