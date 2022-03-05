85db974b33a92ee74d924e2ce41401e590316077
vgavrilo <vladimir.gavrilov@intel.com>
2022-03-05 12:47:45 +0300

    Deleted 'using namespace ngraph' from cpp-files of nGraph shells of operations RDFT and IRDFT.

--- src/core/src/op/irdft.cpp
+++ src/core/src/op/irdft.cpp
@@ -22,37 +22,36 @@
 #include "itt.hpp"
 
 using namespace std;
-using namespace ngraph;
 
-BWDCMP_RTTI_DEFINITION(op::v9::IRDFT);
+BWDCMP_RTTI_DEFINITION(ov::op::v9::IRDFT);
 
-op::v9::IRDFT::IRDFT(const Output<Node>& data, const Output<Node>& axes) : FFTBase(data, axes) {
+ov::op::v9::IRDFT::IRDFT(const Output<Node>& data, const Output<Node>& axes) : FFTBase(data, axes) {
     constructor_validate_and_infer_types();
 }
 
-op::v9::IRDFT::IRDFT(const Output<Node>& data, const Output<Node>& axes, const Output<Node>& signal_size)
+ov::op::v9::IRDFT::IRDFT(const Output<Node>& data, const Output<Node>& axes, const Output<Node>& signal_size)
     : FFTBase(data, axes, signal_size) {
     constructor_validate_and_infer_types();
 }
 
-bool op::v9::IRDFT::visit_attributes(AttributeVisitor& visitor) {
+bool ov::op::v9::IRDFT::visit_attributes(AttributeVisitor& visitor) {
     NGRAPH_OP_SCOPE(v9_IRDFT_visit_attributes);
     return true;
 }
 
-std::shared_ptr<Node> op::v9::IRDFT::clone_with_new_inputs(const OutputVector& new_args) const {
+std::shared_ptr<ov::Node> ov::op::v9::IRDFT::clone_with_new_inputs(const OutputVector& new_args) const {
     NGRAPH_OP_SCOPE(v9_IRDFT_clone_with_new_inputs);
     check_new_args_count(this, new_args);
     NODE_VALIDATION_CHECK(this, new_args.size() == 2 || new_args.size() == 3, "Number of inputs must be 2 or 3");
 
     if (new_args.size() == 2) {
-        return std::make_shared<op::v9::IRDFT>(new_args.at(0), new_args.at(1));
+        return std::make_shared<ov::op::v9::IRDFT>(new_args.at(0), new_args.at(1));
     }
 
-    return std::make_shared<op::v9::IRDFT>(new_args.at(0), new_args.at(1), new_args.at(2));
+    return std::make_shared<ov::op::v9::IRDFT>(new_args.at(0), new_args.at(1), new_args.at(2));
 }
 
-void op::v9::IRDFT::validate_and_infer_types() {
+void ov::op::v9::IRDFT::validate_and_infer_types() {
     NGRAPH_OP_SCOPE(v9_IRDFT_validate_and_infer_types);
 
     validate_types();
--- src/core/src/op/rdft.cpp
+++ src/core/src/op/rdft.cpp
@@ -22,37 +22,36 @@
 #include "rdft_shape_inference.hpp"
 
 using namespace std;
-using namespace ngraph;
 
-BWDCMP_RTTI_DEFINITION(op::v9::RDFT);
+BWDCMP_RTTI_DEFINITION(ov::op::v9::RDFT);
 
-op::v9::RDFT::RDFT(const Output<Node>& data, const Output<Node>& axes) : FFTBase(data, axes) {
+ov::op::v9::RDFT::RDFT(const Output<Node>& data, const Output<Node>& axes) : FFTBase(data, axes) {
     constructor_validate_and_infer_types();
 }
 
-op::v9::RDFT::RDFT(const Output<Node>& data, const Output<Node>& axes, const Output<Node>& signal_size)
+ov::op::v9::RDFT::RDFT(const Output<Node>& data, const Output<Node>& axes, const Output<Node>& signal_size)
     : FFTBase(data, axes, signal_size) {
     constructor_validate_and_infer_types();
 }
 
-bool op::v9::RDFT::visit_attributes(AttributeVisitor& visitor) {
+bool ov::op::v9::RDFT::visit_attributes(AttributeVisitor& visitor) {
     NGRAPH_OP_SCOPE(v9_RDFT_visit_attributes);
     return true;
 }
 
-std::shared_ptr<Node> op::v9::RDFT::clone_with_new_inputs(const OutputVector& new_args) const {
+std::shared_ptr<ov::Node> ov::op::v9::RDFT::clone_with_new_inputs(const OutputVector& new_args) const {
     NGRAPH_OP_SCOPE(v9_RDFT_clone_with_new_inputs);
     check_new_args_count(this, new_args);
     NODE_VALIDATION_CHECK(this, new_args.size() == 2 || new_args.size() == 3, "Number of inputs must be 2 or 3");
 
     if (new_args.size() == 2) {
-        return std::make_shared<op::v9::RDFT>(new_args.at(0), new_args.at(1));
+        return std::make_shared<ov::op::v9::RDFT>(new_args.at(0), new_args.at(1));
     }
 
-    return std::make_shared<op::v9::RDFT>(new_args.at(0), new_args.at(1), new_args.at(2));
+    return std::make_shared<ov::op::v9::RDFT>(new_args.at(0), new_args.at(1), new_args.at(2));
 }
 
-void op::v9::RDFT::validate_and_infer_types() {
+void ov::op::v9::RDFT::validate_and_infer_types() {
     NGRAPH_OP_SCOPE(v9_RDFT_validate_and_infer_types);
 
     validate_types();
