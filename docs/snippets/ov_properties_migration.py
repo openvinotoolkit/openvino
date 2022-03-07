59b07a945e6c927c3a5ffe8beeabb5bfdff94a87
p-wysocki <przemyslaw.wysocki@intel.com>
2022-03-07 15:09:47 +0100

    Bugfixes

--- docs/snippets/ov_properties_migration.py
+++ docs/snippets/ov_properties_migration.py
@@ -3,40 +3,43 @@
 #
 
 from openvino.runtime import Core
-import openvino
 
+
+core = Core()
 
 # ! [core_set_property]
-Core.set_property(device_name='CPU', properties={'PERF_COUNT': True})
+core.set_property(device_name="CPU", properties={"PERF_COUNT": "YES"})
 # ! [core_set_property]
 
-model = Core.read_model('sample.xml')
+#help(core.read_model)
+model = core.read_model("/home/pwysocki/openvino/docs/snippets/sparsity_example_02_sparsity_tf.xml")
+
 # ! [core_compile_model]
-compiled_model = Core.compile_model(model=model, device_name='MULTI', config=
+compiled_model = core.compile_model(model=model, device_name="MULTI", config=
     {
-        'MULTI_DEVICE_PRIORITIES': ['GPU, CPU'],
-        'PERFORMANCE_HINT': 'THROUGHPUT',
-        'INFERENCE_PRECISION_HINT': openvino.Type.f32
+        "MULTI_DEVICE_PRIORITIES": "GPU,CPU",
+        "PERFORMANCE_HINT": "THROUGHPUT",
+        "INFERENCE_PRECISION_HINT": "f32"
     })
 # ! [core_compile_model]
 
 # ! [compiled_model_set_property]
 # turn CPU off for multi-device execution
-compiled_model.set_property(properties={'MULTI_DEVICE_PRIORITIES': 'GPU'})
+compiled_model.set_property(properties={"MULTI_DEVICE_PRIORITIES": "GPU"})
 # ! [compiled_model_set_property]
 
 # ! [core_get_rw_property]
-num_streams = Core.get_property('CPU', 'NUM_STREAMS')
+num_streams = core.get_property("CPU", "NUM_STREAMS")
 # ! [core_get_rw_property]
 
 # ! [core_get_ro_property]
-full_device_name = Core.get_property('CPU', 'FULL_DEVICE_NAME')
+full_device_name = core.get_property("CPU", "FULL_DEVICE_NAME")
 # ! [core_get_ro_property]
 
 # ! [compiled_model_get_rw_property]
-perf_model = compiled_model.get_property('PERFORMANCE_HINT')
+perf_model = compiled_model.get_property("PERFORMANCE_HINT")
 # ! [compiled_model_get_rw_property]
 
 # ! [compiled_model_get_ro_property]
-nireq = compiled_model.get_property('OPTIMAL_NUMBER_OF_INFER_REQUESTS')
+nireq = compiled_model.get_property("OPTIMAL_NUMBER_OF_INFER_REQUESTS")
 # ! [compiled_model_get_ro_property]
