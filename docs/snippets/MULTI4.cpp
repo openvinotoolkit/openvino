4075c0d3f7eb2ef0e29a104b46b9a657a5648995
Hu, Yuan2 <yuan2.hu@intel.com>
2022-03-07 09:34:57 +0800

    update snippets ov::enableProfile
    Signed-off-by: Hu, Yuan2 <yuan2.hu@intel.com>

--- docs/snippets/MULTI4.cpp
+++ docs/snippets/MULTI4.cpp
@@ -1,8 +1,8 @@
 #include <openvino/openvino.hpp>
 
 int main() {
-const ov::AnyMap hddl_config  = {{ ov::enable_profiling.name(), InferenceEngine::PluginConfigParams::YES}};
-const ov::AnyMap gpu_config = {{ ov::enable_profiling.name(), InferenceEngine::PluginConfigParams::YES}};
+const ov::AnyMap hddl_config  = ov::enable_profiling(true);
+const ov::AnyMap gpu_config = ov::enable_profiling(true);
 //! [part4]
 // configure the HDDL device first
 ov::Core core;
