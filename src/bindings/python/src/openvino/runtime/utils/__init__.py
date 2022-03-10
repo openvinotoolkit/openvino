33d1d62b8509575595debf122f3b88c831bb4b08
Anastasia Kuporosova <anastasia.kuporosova@intel.com>
2022-03-10 22:06:28 +0300

    [Python API] move util under utils

--- src/bindings/python/src/openvino/runtime/utils/__init__.py
+++ src/bindings/python/src/openvino/runtime/utils/__init__.py
@@ -2,3 +2,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
 """Generic utilities. Factor related functions out to separate files."""
+
+from openvino.pyopenvino import util
+
+numpy_to_c = util.numpy_to_c
+get_constant_from_source = util.get_constant_from_source
