a9d251ab896a5daac301accc66f6ee5e92cb38aa
vgavrilo <vladimir.gavrilov@intel.com>
2022-03-10 15:57:24 +0300

    Common part of RDFT and IRDFT shape validation moved into the separate file.

--- src/core/shape_inference/include/irdft_shape_inference.hpp
+++ src/core/shape_inference/include/irdft_shape_inference.hpp
@@ -5,6 +5,7 @@
 #include <openvino/op/irdft.hpp>
 
 #include "openvino/core/axis_vector.hpp"
+#include "rfft_common_validation.hpp"
 #include "utils.hpp"
 
 namespace ov {
@@ -24,70 +25,7 @@ void irdft_shape_infer(const ov::op::v9::IRDFT* op,
     std::vector<int64_t> axes;
     bool axes_are_known = get_data_as_int64<T>(1, op, axes, constant_data);
 
-    if (input_shape.rank().is_static()) {
-        const auto input_rank = input_shape.size();
-        NODE_VALIDATION_CHECK(op,
-                              input_rank >= 2,
-                              "The input rank must be greater or equal to 2. Got input rank: ",
-                              input_rank);
-
-        NODE_VALIDATION_CHECK(op,
-                              input_shape[input_rank - 1].compatible(2),
-                              "The last dimension of input data must be 2. Got: ",
-                              input_shape[input_rank - 1]);
-
-        if (axes_shape.is_static()) {
-            NODE_VALIDATION_CHECK(op,
-                                  input_rank >= static_cast<int64_t>(axes_shape[0].get_length() + 1),
-                                  "The input rank must be greater than number of IRDFT op axes. Got "
-                                  "input rank: ",
-                                  input_rank,
-                                  ", number of axes: ",
-                                  axes_shape[0].get_length());
-        }
-
-        // IRDFT operation supports negative axes to transform. More precisely, according to
-        // the IRDFT operation specification, axes should be integers from -(r - 1) to (r - 2)
-        // inclusively, where r = rank(data). A negative axis 'a' is interpreted as an axis
-        // 'r - 1 + a'. The reason is the following: real input tensor of the shape
-        // [n_0, ..., n_{r - 1}, 2] is interpreted as a complex tensor with the shape
-        // [n_0, ..., n_{r - 1}].
-        if (axes_shape.rank().is_static() && axes_are_known) {
-            ov::AxisSet axes_set;
-            for (int64_t& axis : axes) {
-                if (axis < 0) {
-                    axis += input_rank - 1;
-                }
-                axes_set.insert(static_cast<size_t>(axis));
-            }
-
-            NODE_VALIDATION_CHECK(op, axes.size() == axes_set.size(), "IRDFT op axes must be unique.");
-
-            NODE_VALIDATION_CHECK(op,
-                                  std::find(axes.begin(), axes.end(), input_rank - 1) == axes.end(),
-                                  "IRDFT op axes cannot contain the last axis.");
-        }
-    }
-
-    NODE_VALIDATION_CHECK(op, axes_shape.rank().compatible(1), "IRDFT op axes input must be 1D tensor.");
-
-    if (input_shapes.size() == 3) {
-        const auto& signal_size_shape = input_shapes[2];
-        NODE_VALIDATION_CHECK(op,
-                              signal_size_shape.rank().compatible(1),
-                              "IRDFT op signal size input must be 1D tensor. Got signal: ",
-                              signal_size_shape);
-
-        if (axes_shape.is_static() && signal_size_shape.is_static()) {
-            NODE_VALIDATION_CHECK(op,
-                                  axes_shape[0].compatible(signal_size_shape[0]),
-                                  "Sizes of inputs 'axes' and 'signal_size' must be equal. Got "
-                                  "size of 'axes': ",
-                                  axes_shape[0],
-                                  "size of 'signal_size': ",
-                                  signal_size_shape[0]);
-        }
-    }
+    rfft_common_validation::shape_validation(op, input_shapes, axes, axes_are_known, rfft_common_validation::RFFTKind::Inverse);
 
     if (input_shape.rank().is_dynamic()) {
         output_shape = ov::PartialShape::dynamic();
