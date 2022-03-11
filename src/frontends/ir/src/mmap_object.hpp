503df3c0f3793c60f4ceae7f623365286da7aa63
Michael Nosov <mikhail.nosov@intel.com>
2022-03-11 13:38:09 +0300

    Windows mmap implementation + unicode

--- src/frontends/ir/src/mmap_object.hpp
+++ src/frontends/ir/src/mmap_object.hpp
@@ -15,20 +15,12 @@
 
 namespace ov {
 
-/**
- * @brief Loads a library with the name specified.
- * @param path Full or relative path to the plugin library
- * @return Reference to shared object
- */
 std::shared_ptr<ngraph::runtime::AlignedBuffer> load_mmap_object(const std::string& path);
 
 #ifdef OPENVINO_ENABLE_UNICODE_PATH_SUPPORT
-/**
- * @brief Loads a library with the wide char name specified.
- * @param path Full or relative path to the plugin library
- * @return Reference to shared object
- */
-// std::shared_ptr<void> load_mmap_object(const std::wstring& path);
+
+std::shared_ptr<ngraph::runtime::AlignedBuffer> load_mmap_object(const std::wstring& path);
+
 #endif  // OPENVINO_ENABLE_UNICODE_PATH_SUPPORT
 
 }  // namespace ov
--- src/frontends/ir/src/os/win/win_mmap_object.cpp
+++ src/frontends/ir/src/os/win/win_mmap_object.cpp
@@ -2,35 +2,29 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include <dlfcn.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <unistd.h>
-
-#include <iostream>
-#include <sstream>
-
 #include "mmap_object.hpp"
+
+#include <windows.h>
+
 #include "ngraph/runtime/shared_buffer.hpp"
 #include "openvino/util/file_util.hpp"
 
 namespace ov {
 
 class HandleHolder {
-    int m_handle = -1;
+    HANDLE m_handle = INVALID_HANDLE_VALUE;
     void reset() {
-        if (m_handle != -1) {
-            close(m_handle);
-            m_handle = -1;
+        if (m_handle != INVALID_HANDLE_VALUE) {
+            ::CloseHandle(m_handle);
+            m_handle = INVALID_HANDLE_VALUE;
         }
     }
 
 public:
-    explicit HandleHolder(int handle = -1) : m_handle(handle) {}
+    explicit HandleHolder(HANDLE handle = INVALID_HANDLE_VALUE) : m_handle(handle) {}
     HandleHolder(const HandleHolder&) = delete;
     HandleHolder(HandleHolder&& other) noexcept : m_handle(other.m_handle) {
-        other.m_handle = -1;
+        other.m_handle = INVALID_HANDLE_VALUE;
     }
     HandleHolder& operator=(const HandleHolder&) = delete;
     HandleHolder& operator=(HandleHolder&& other) noexcept {
@@ -39,7 +33,7 @@ public:
         }
         reset();
         m_handle = other.m_handle;
-        other.m_handle = -1;
+        other.m_handle = INVALID_HANDLE_VALUE;
         return *this;
     }
 
@@ -47,39 +41,34 @@ public:
         reset();
     }
 
-    int get() const noexcept {
+    HANDLE get() const noexcept {
         return m_handle;
     }
 };
 
 class MapHolder {
-    void* m_data = MAP_FAILED;
-    size_t m_size = 0;
-    HandleHolder m_handle;
-
 public:
     MapHolder() = default;
 
-    void set(const std::string& path) {
-        int prot = PROT_READ;
-        int mode = O_RDONLY;
-        struct stat sb = {};
-        m_handle = HandleHolder(open(path.c_str(), mode));
-        OPENVINO_ASSERT(m_handle.get() != -1,
-                        "Can not open file ",
-                        path,
-                        " for mapping. Ensure that file exists and has appropriate permissions");
-        OPENVINO_ASSERT(fstat(m_handle.get(), &sb) != -1, "Can not get file size for ", path);
-        m_size = sb.st_size;
-        m_data = mmap(nullptr, m_size, prot, MAP_PRIVATE, m_handle.get(), 0);
-        OPENVINO_ASSERT(m_data != MAP_FAILED, "Can not create file mapping for ", path);
-    }
-
     ~MapHolder() {
-        if (m_data != MAP_FAILED) {
-            munmap(m_data, m_size);
+        if (m_data) {
+            ::UnmapViewOfFile(m_data);
         }
     }
+
+    void set(const std::string& path) {
+        auto h = ::CreateFileA(path.c_str(), GENERIC_READ, FILE_SHARE_READ,
+                               0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+        map(path, h);
+    }
+
+#ifdef OPENVINO_ENABLE_UNICODE_PATH_SUPPORT
+    void set(const std::wstring& path) {
+        auto h = ::CreateFileW(path.c_str(), GENERIC_READ, FILE_SHARE_READ,
+                               0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+        map(ov::util::wstring_to_string(path), h);
+    }
+#endif
 
     char* data() noexcept {
         return static_cast<char*>(m_data);
@@ -87,6 +76,49 @@ public:
     size_t size() const noexcept {
         return m_size;
     }
+
+private:
+
+    void map(const std::string& path, HANDLE h) {
+        OPENVINO_ASSERT(h != INVALID_HANDLE_VALUE,
+                        "Can not open file ",
+                        path,
+                        " for mapping. Ensure that file exists and has appropriate permissions");
+        m_handle = HandleHolder(h);
+        SYSTEM_INFO SystemInfo;
+        GetSystemInfo(&SystemInfo);
+        const int64_t page_size = SystemInfo.dwAllocationGranularity;
+
+        DWORD file_mode = GENERIC_READ;
+        DWORD map_mode = FILE_MAP_READ;
+        DWORD access = PAGE_READONLY;
+
+        LARGE_INTEGER file_size_large;
+        OPENVINO_ASSERT(::GetFileSizeEx(m_handle.get(), &file_size_large) != 0, "Can not get file size for ", path);
+
+        m_size = static_cast<uint64_t>(file_size_large.QuadPart);
+        std::cout << "Size: " << m_size << "\n";
+        m_mapping = HandleHolder(::CreateFileMapping(m_handle.get(), 0, access,
+            m_size >> 32,
+            m_size & 0xffffffff,
+            0));
+        OPENVINO_ASSERT(m_mapping.get() != INVALID_HANDLE_VALUE, "Can not create file mapping for ", path);
+
+        m_data = ::MapViewOfFile(
+            m_mapping.get(),
+            map_mode,
+            0, //offset_align >> 32,
+            0, //offset_align & 0xffffffff,
+            m_size);
+        OPENVINO_ASSERT(m_data, "Can not create map view for ", path);
+    }
+
+private:
+    void* m_data = NULL;
+    size_t m_size = 0;
+    HandleHolder m_handle;
+    HandleHolder m_mapping;
+
 };
 
 std::shared_ptr<ngraph::runtime::AlignedBuffer> load_mmap_object(const std::string& path) {
@@ -96,5 +128,17 @@ std::shared_ptr<ngraph::runtime::AlignedBuffer> load_mmap_object(const std::stri
                                                                                        holder->size(),
                                                                                        holder);
 }
+
+#ifdef OPENVINO_ENABLE_UNICODE_PATH_SUPPORT
+
+std::shared_ptr<ngraph::runtime::AlignedBuffer> load_mmap_object(const std::wstring& path) {
+    auto holder = std::make_shared<MapHolder>();
+    holder->set(path);
+    return std::make_shared<ngraph::runtime::SharedBuffer<std::shared_ptr<MapHolder>>>(holder->data(),
+                                                                                       holder->size(),
+                                                                                       holder);
+}
+
+#endif
 
 }  // namespace ov
