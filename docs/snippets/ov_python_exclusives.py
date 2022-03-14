c71452ba31a8fa7b32c2c8005cadca813e020ab2
jiwaszki <jan.iwaszkiewicz@intel.com>
2022-03-14 17:02:57 +0000

    Apply comments

--- docs/snippets/ov_python_exclusives.py
+++ docs/snippets/ov_python_exclusives.py
@@ -13,7 +13,7 @@ compiled_model = ov.compile_model("model.xml")
 core = ov.Core()
 
 input_a = ov.opset8.parameter([8])
-res = ov.opset8.relu(input_a)
+res = ov.opset8.absolute(input_a)
 model = ov.Model(res, [input_a])
 compiled = core.compile_model(model, "CPU")
 
@@ -27,26 +27,26 @@ print(compiled.outputs)
 #! [tensor_basics]
 data_float64 = np.ones(shape=(2,8))
 
-t = ov.Tensor(data_float64)
-assert t.element_type == ov.Type.f64
+tensor = ov.Tensor(data_float64)
+assert tensor.element_type == ov.Type.f64
 
 data_int32 = np.ones(shape=(2,8), dtype=np.int32)
 
-t = ov.Tensor(data_int32)
-assert t.element_type == ov.Type.i32
+tensor = ov.Tensor(data_int32)
+assert tensor.element_type == ov.Type.i32
 #! [tensor_basics]
 
 #! [tensor_shared_mode]
 data_to_share = np.ones(shape=(2,8))
 
-t_shared = ov.Tensor(data_to_share, shared_memory=True)
+shared_tensor = ov.Tensor(data_to_share, shared_memory=True)
 
 # Editing of the numpy array affects Tensor's data
 data_to_share[0][2] = 6.0
-assert t_shared.data[0][2] == 6.0
+assert shared_tensor.data[0][2] == 6.0
 
 # Editing of Tensor's data affects the numpy array
-t_shared.data[0][2] = 0.6
+shared_tensor.data[0][2] = 0.6
 assert data_to_share[0][2] == 0.6
 #! [tensor_shared_mode]
 
@@ -54,15 +54,15 @@ assert data_to_share[0][2] == 0.6
 data_to_share = np.ones(shape=(2,8))
 
 # Specify slice of memory and the shape
-t_shared = ov.Tensor(data_to_share[1][:] , shape=ov.Shape([8]))
+shared_tensor = ov.Tensor(data_to_share[1][:] , shape=ov.Shape([8]))
 
 # Editing of the numpy array affects Tensor's data
 data_to_share[1][:] = 2
-assert np.array_equal(t_shared.data, data_to_share[1][:])
+assert np.array_equal(shared_tensor.data, data_to_share[1][:])
 #! [tensor_slice_mode]
 
-infer_request = compiled_model.create_infer_request()
-data = np.ones(shape=(2,8))
+infer_request = compiled.create_infer_request()
+data = np.random.randint(-5, 3 + 1, size=(8))
 
 #! [passing_numpy_array]
 # Passing inputs data in form of a dictionary
@@ -70,6 +70,17 @@ infer_request.infer(inputs={0: data})
 # Passing inputs data in form of a list
 infer_request.infer(inputs=[data])
 #! [passing_numpy_array]
+
+#! [getting_results]
+# Get output tensor
+results = infer_request.get_output_tensor().data
+
+# Get tensor with CompiledModel's output node
+results = infer_request.get_tensor(compiled.outputs[0]).data
+
+# Get all results with special helper property
+results = list(infer_request.results.values())
+#! [getting_results]
 
 #! [sync_infer]
 # Simple call to InferRequest
