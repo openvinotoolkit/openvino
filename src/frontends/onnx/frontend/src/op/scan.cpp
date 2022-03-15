700d2813b07718673aa1b11a97bf752a70bf14ce
mitruska <katarzyna.mitrus@intel.com>
2022-03-15 22:40:01 +0100

    Code refactor

--- src/frontends/onnx/frontend/src/op/scan.cpp
+++ src/frontends/onnx/frontend/src/op/scan.cpp
@@ -44,65 +44,73 @@ OutputVector scan(const Node& node) {
         node.get_attribute_value<std::vector<int64_t>>("scan_output_directions",
                                                        std::vector<int64_t>(num_scan_outputs, 0));
 
-    for (size_t i = 0; i < body_inputs.size(); i++) {
+    for (size_t i = 0; i < num_initial_values; ++i) {
         body_inputs[i]->set_element_type(ng_inputs[i].get_element_type());
-        if (i < num_initial_values) {
-            body_inputs[i]->set_partial_shape(ng_inputs[i].get_partial_shape());
-            body_inputs[i]->validate_and_infer_types();
-            continue;
-        }
-
-        auto axis_val = scan_input_axes[i - num_initial_values];
-        auto shape = ng_inputs[i].get_partial_shape();
-        if (shape.rank().is_static()) {
-            shape[axis_val] = 1;
-        }
-        body_inputs[i]->set_partial_shape(shape);
+        body_inputs[i]->set_partial_shape(ng_inputs[i].get_partial_shape());
         body_inputs[i]->validate_and_infer_types();
+    }
 
-        auto input_consumers = body_inputs[i]->output(0).get_target_inputs();
-        auto axis = default_opset::Constant::create(element::i64, Shape{1}, {axis_val});
-        auto squeeze = std::make_shared<default_opset::Squeeze>(body_inputs[i], axis);
+    // Single slice of TensorIterator sliced input has the same rank as the input,
+    // but in ONNX Scan the body scan_input can has one dimension less,
+    // so the parameter needs to have aligned rank with 1 at sliced axis,
+    // and then squeezed to restore original shape.
+
+    // Body inputs shape alignment
+    for (size_t i = 0; i < num_scan_inputs; ++i) {
+        const auto in_idx = num_initial_values + i;
+        const auto axis = scan_input_axes[i];
+        const auto axis_node = default_opset::Constant::create(element::i64, Shape{1}, {axis});
+
+        auto shape = ng_inputs[in_idx].get_partial_shape();
+        if (shape.rank().is_static()) {
+            shape[axis] = 1;
+        }
+        body_inputs[in_idx]->set_partial_shape(shape);
+        body_inputs[in_idx]->validate_and_infer_types();
+
+        auto input_consumers = body_inputs[in_idx]->output(0).get_target_inputs();
+        auto squeeze = std::make_shared<default_opset::Squeeze>(body_inputs[in_idx], axis_node);
         for (auto& input : input_consumers) {
             input.replace_source_output(squeeze);
         }
     }
-
-    for (size_t i = num_initial_values; i < body_outputs.size(); i++) {
-        auto axis_val = scan_output_axes[i - num_initial_values];
-        auto axis = default_opset::Constant::create(element::i64, Shape{1}, {axis_val});
-        body_outputs[i] = std::make_shared<default_opset::Unsqueeze>(body_outputs[i], axis);
+    // Body outputs shape alignment
+    for (size_t i = 0; i < num_scan_outputs; ++i) {
+        const auto out_idx = num_initial_values + i;
+        const auto axis = scan_output_axes[i];
+        const auto axis_node = default_opset::Constant::create(element::i64, Shape{1}, {axis});
+        body_outputs[out_idx] = std::make_shared<default_opset::Unsqueeze>(body_outputs[out_idx], axis_node);
     }
 
-    auto ti_body = std::make_shared<Function>(body_outputs, body_inputs);
-
+    // TensorIterator setup
     auto tensor_iterator = std::make_shared<default_opset::TensorIterator>();
+    auto ti_body = std::make_shared<Function>(body_outputs, body_inputs);
     tensor_iterator->set_function(ti_body);
 
-    OutputVector outputs;
-    const auto& ti_body_results = ti_body->get_results();
-    for (size_t i = 0; i < num_initial_values; i++) {
-        tensor_iterator->set_merged_input(body_inputs[i], ng_inputs[i], ti_body_results[i]);
-        outputs.push_back(tensor_iterator->get_iter_value(ti_body_results[i], -1));
-    }
-
-    for (size_t i = num_initial_values; i < num_initial_values + num_scan_inputs; i++) {
-        auto direction = scan_input_directions[i - num_initial_values];
-        auto axis = scan_input_axes[i - num_initial_values];
-        if (direction == 0) {
-            tensor_iterator->set_sliced_input(body_inputs[i], ng_inputs[i], 0, 1, 1, -1, axis);
-        } else {
-            tensor_iterator->set_sliced_input(body_inputs[i], ng_inputs[i], -1, -1, 1, 0, axis);
+    // Set slicing for Scan (TensorIterator) inputs
+    for (size_t i = 0; i < num_scan_inputs; ++i) {
+        const auto in_idx = num_initial_values + i;
+        const auto axis = scan_input_axes[i];
+        if (scan_input_directions[i]) {  // reverse direction
+            tensor_iterator->set_sliced_input(body_inputs[in_idx], ng_inputs[in_idx], -1, -1, 1, 0, axis);
+        } else {  // forward direction
+            tensor_iterator->set_sliced_input(body_inputs[in_idx], ng_inputs[in_idx], 0, 1, 1, -1, axis);
         }
     }
 
-    for (size_t i = num_initial_values; i < ti_body_results.size(); i++) {
-        auto direction = scan_output_directions[i - num_initial_values];
-        auto axis = scan_output_axes[i - num_initial_values];
-        if (direction == 0) {
-            outputs.push_back(tensor_iterator->get_concatenated_slices(ti_body_results[i], 0, 1, 1, -1, axis));
-        } else {
-            outputs.push_back(tensor_iterator->get_concatenated_slices(ti_body_results[i], -1, -1, 1, 0, axis));
+    // Set Scan (TensorIterator) outputs
+    OutputVector outputs;
+    for (size_t i = 0; i < num_initial_values; ++i) {
+        tensor_iterator->set_merged_input(body_inputs[i], ng_inputs[i], body_outputs[i]);
+        outputs.push_back(tensor_iterator->get_iter_value(body_outputs[i], -1));
+    }
+    for (size_t i = 0; i < num_scan_outputs; ++i) {
+        const auto out_idx = num_initial_values + i;
+        const auto axis = scan_output_axes[i];
+        if (scan_output_directions[i]) {  // reverse direction
+            outputs.push_back(tensor_iterator->get_concatenated_slices(body_outputs[out_idx], -1, -1, 1, 0, axis));
+        } else {  // forward direction
+            outputs.push_back(tensor_iterator->get_concatenated_slices(body_outputs[out_idx], 0, 1, 1, -1, axis));
         }
     }
 
