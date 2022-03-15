43f57bf203d41902154eaae547ba455592ba7a61
Sergey Lyalin <sergey.lyalin@intel.com>
2022-03-15 04:15:37 +0300

    Rework of Extensibility Intro, adopted examples to missing OPENVINO_FRAMEWORK_MAP

--- docs/snippets/ov_extensions.cpp
+++ docs/snippets/ov_extensions.cpp
@@ -2,18 +2,43 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 #include <openvino/openvino.hpp>
+//! [add_extension]
 #include <openvino/core/op_extension.hpp>
+//! [add_extension]
+//! [add_frontend_extension]
+#include <openvino/frontend/extension.hpp>
+//! [add_frontend_extension]
+
 #include <identity.hpp>
+
 
 int main() {
 {
 //! [add_extension]
 ov::Core core;
-// Use operation type to add operation extension 
+
+// Use operation type to add operation extension
 core.add_extension<TemplateExtension::Identity>();
-// or you can add operation extension to this method
+
+// or you can add operation extension object which is equivalent form
 core.add_extension(ov::OpExtension<TemplateExtension::Identity>());
 //! [add_extension]
+}
+{
+ov::Core core;
+
+//! [add_frontend_extension]
+// Register mapping for new frontends: FW's "TemplateIdentity" operation to TemplateExtension::Identity
+core.add_extension(ov::frontend::OpExtension<TemplateExtension::Identity>("TemplateIdentity"));
+
+// Register more sophisticated mapping with decomposition
+core.add_extension(ov::frontend::ConversionExtension(
+        "TemplateIdentity",
+        [](const ov::frontend::NodeContext& context) {
+            // Arbitrary decomposition code here
+            return ov::OutputVector{ std::make_shared<TemplateExtension::Identity>(context.get_input(0)) };
+        }));
+//! [add_frontend_extension]
 }
 {
 //! [add_extension_lib]
--- docs/template_extension/new/ov_extension.cpp
+++ docs/template_extension/new/ov_extension.cpp
@@ -4,6 +4,7 @@
 
 #include <openvino/core/extension.hpp>
 #include <openvino/core/op_extension.hpp>
+#include <openvino/frontend/extension.hpp>
 
 #include "identity.hpp"
 
@@ -11,7 +12,8 @@
 //! [ov_extension:entry_point]
 OPENVINO_CREATE_EXTENSIONS(
     std::vector<ov::Extension::Ptr>({
-        std::make_shared<ov::OpExtension<TemplateExtension::Identity>>()
+        std::make_shared<ov::OpExtension<TemplateExtension::Identity>>(),
+        std::make_shared<ov::frontend::OpExtension<TemplateExtension::Identity>>("TemplateIdentity")
     }));
 //! [ov_extension:entry_point]
 // clang-format on
