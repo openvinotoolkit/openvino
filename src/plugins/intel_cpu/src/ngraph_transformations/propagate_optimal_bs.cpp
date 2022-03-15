adbccd471b5b52d173464e44f62b1738f35ecb43
Vladislav Golubev <vladislav.golubev@intel.com>
2022-03-15 15:34:37 +0300

    added check on static input shapes in PropagateOptimalBS transformation

--- src/plugins/intel_cpu/src/ngraph_transformations/propagate_optimal_bs.cpp
+++ src/plugins/intel_cpu/src/ngraph_transformations/propagate_optimal_bs.cpp
@@ -20,17 +20,19 @@ ov::intel_cpu::PropagateOptimalBS::PropagateOptimalBS() {
         const auto node = m.get_match_root();
         if (has_optimal_bs(node) || is_type<ngraph::opset1::Result>(node))
             return false;
+
+        // TODO: do we really need it?
         if (is_type<ngraph::opset1::Reshape>(node) || is_type<ngraph::opset1::Concat>(node) ||
             is_type<ngraph::opset1::Split>(node) || is_type<ngraph::opset1::VariadicSplit>(node))
             return false;
+
         const auto& pshape = m.get_match_value().get_partial_shape();
         if (pshape.size() == 0)
             return false;
 
         auto set_parent_opt_bs = [&node](const std::shared_ptr<ov::Node>& parent) {
-            if (!has_optimal_bs(parent)) {
+            if (!has_optimal_bs(parent))
                 return false;
-            }
             const auto parent_bs = get_optimal_bs(parent);
             set_optimal_bs(node, parent_bs);
             return true;
@@ -39,13 +41,10 @@ ov::intel_cpu::PropagateOptimalBS::PropagateOptimalBS() {
         const auto node_batch = pshape[0].get_length();
         for (const auto& input : node->input_values()) {
             const auto& input_ps = input.get_partial_shape();
-            if (input_ps.rank().is_dynamic() || input_ps.size() == 0 ||
-                input_ps[0].is_dynamic() || input_ps[0].get_length() != node_batch)
+            if (input_ps.is_dynamic() || input_ps.size() == 0 || input_ps[0].get_length() != node_batch)
                 continue;
-
-            if (set_parent_opt_bs(input.get_node_shared_ptr())) {
+            if (set_parent_opt_bs(input.get_node_shared_ptr()))
                 break;
-            }
         }
 
         return false;
