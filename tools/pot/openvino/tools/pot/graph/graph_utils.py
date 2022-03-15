733dbd3743e7dbad4aa7aa29ac00f1ded1dfe728
AlexeyLebedev1 <alexey.lebedev@intel.com>
2022-03-15 09:48:38 +0300

    revert pot

--- tools/pot/openvino/tools/pot/graph/graph_utils.py
+++ tools/pot/openvino/tools/pot/graph/graph_utils.py
@@ -7,7 +7,8 @@ from copy import deepcopy
 from openvino.tools.mo.graph.graph import Graph
 from openvino.tools.mo.utils.ir_reader.restore_graph import restore_graph_from_ir, save_restored_graph
 from openvino.tools.mo.utils.logger import init_logger
-from openvino.runtime import Core, serialize  # pylint: disable=E0401,E0611
+from openvino.runtime import Core  # pylint: disable=E0401,E0611
+from openvino.runtime.passes import Manager # pylint: disable=E0401,E0611
 try:
     from openvino.offline_transformations import apply_pot_transformations # pylint: disable=import-error,no-name-in-module
 except ImportError:
@@ -18,6 +19,7 @@ from ..utils.logger import stdout_redirect
 
 init_logger('ERROR', False)
 core = Core()
+pass_manager = Manager()
 
 
 def load_graph(model_config, target_device='ANY'):
@@ -35,7 +37,9 @@ def load_graph(model_config, target_device='ANY'):
         apply_pot_transformations(model, target_device.encode('utf-8'))
         bin_path = serialized_bin_path
         xml_path = serialized_xml_path
-        serialize(model=model, model_path=xml_path, weights_path=bin_path)
+        # TODO: replace by openvino.runtime.serialize
+        pass_manager.register_pass(pass_name="Serialize", xml_path=xml_path, bin_path=bin_path)
+        pass_manager.run_passes(model)
 
     if not os.path.exists(xml_path):
         raise RuntimeError('Input model xml should link to an existing file. Please, provide a correct path.')
