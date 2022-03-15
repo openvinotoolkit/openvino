5d83300327cbac1b95a75637b630c15f71f1588b
Gleb Kazantaev <gleb.nnstu@gmail.com>
2022-03-15 12:22:00 +0300

    Update tests

--- src/bindings/python/tests/test_transformations/test_transformations_api.py
+++ src/bindings/python/tests/test_transformations/test_transformations_api.py
@@ -8,53 +8,6 @@ from openvino.runtime.passes import Manager, ModelPass, Matcher, MatcherPass, Wr
 from openvino.runtime.utils import replace_node, check_test_pass
 
 
-# Simple: for Extensions. Without any classes and inheritance.
-def pattern_replacement():
-    param = WrapType("Parameter")
-    relu = WrapType("Relu", param.output(0))
-
-    def callback(m: Matcher) -> bool:
-        print("SimpleReplacement - Applied!")
-        root = m.get_match_root()
-
-        # Just to check that capturing works and we can
-        # link pattern nodes with matched graph nodes.
-        assert (relu in m.get_pattern_value_map())
-
-        new_relu = opset8.relu(root.input_value(0))  # ot root.input(0).get_source_output()
-        replace_node(root, new_relu)
-        return True
-
-    return Matcher(relu, "SimpleReplacement"), callback
-
-
-# Hard: for POT. Includes all MatcherPass capabilities!
-class PatternReplacement(MatcherPass):
-    def __init__(self):
-        MatcherPass.__init__(self)
-
-        relu = WrapType("Relu")
-
-        def callback(m: Matcher) -> bool:
-            print("PatternReplacement - Applied!")
-            root = m.get_match_root()
-            new_relu = opset8.relu(root.input(0).get_source_output())
-
-            # Use new operation for additional matching
-            # self.register_new_node(new_relu)
-
-            # Input->Relu->Result => Input->Relu->Relu->Result
-            root.input(0).replace_source_output(new_relu.output(0))
-            return True
-
-        self.register_matcher(Matcher(relu, "PatternReplacement"), callback)
-
-
-class MyModelPass(ModelPass):
-    def run_on_model(self, model):
-        print("ModelPass - Applied!")
-
-
 def get_test_function():
     param = opset8.parameter(PartialShape([1, 3, 22, 22]), name="parameter")
     relu = opset8.relu(param.output(0))
@@ -62,18 +15,81 @@ def get_test_function():
     return Model([res], [param], "test")
 
 
-def test_transformations_api():
-    check_test_pass(MyModelPass())
+def test_simple_pattern_replacement():
+    # Simple: for Extensions. Without any classes and inheritance.
+    def pattern_replacement():
+        param = WrapType("Parameter")
+        relu = WrapType("Relu", param.output(0))
+
+        def callback(m: Matcher) -> bool:
+            root = m.get_match_root()
+
+            # Just to check that capturing works and we can
+            # link pattern nodes with matched graph nodes.
+            assert relu in m.get_pattern_value_map()
+
+            new_relu = opset8.relu(root.input_value(0))  # ot root.input(0).get_source_output()
+            replace_node(root, new_relu)
+            return True
+
+        return Matcher(relu, "SimpleReplacement"), callback
 
     m = Manager()
-    #m.register_pass(MatcherPass(*pattern_replacement()))
-    #m.register_pass(PatternReplacement())
-    p = MyModelPass()
-    m.register_pass(p)
+    m.register_pass(MatcherPass(*pattern_replacement()))
     m.run_passes(get_test_function())
 
-    # p = MyModelPass()
-    # p.run_on_model(get_test_function())
+
+def test_matcher_pass():
+    # Hard: for POT. Includes all MatcherPass capabilities!
+    class PatternReplacement(MatcherPass):
+        def __init__(self):
+            MatcherPass.__init__(self)
+            self.function_changed = False
+
+            relu = WrapType("Relu")
+
+            def callback(m: Matcher) -> bool:
+                self.applied = True
+                root = m.get_match_root()
+                new_relu = opset8.relu(root.input(0).get_source_output())
+
+                # For testing purpose
+                self.function_changes = True
+
+                # Use new operation for additional matching
+                # self.register_new_node(new_relu)
+
+                # Input->Relu->Result => Input->Relu->Relu->Result
+                root.input(0).replace_source_output(new_relu.output(0))
+                return True
+
+            self.register_matcher(Matcher(relu, "PatternReplacement"), callback)
+
+    m = Manager()
+    # check that register pass returns pass instance
+    p = m.register_pass(PatternReplacement())
+    m.run_passes(get_test_function())
+
+    assert p.function_changed
 
 
-test_transformations_api()
+def test_model_pass():
+    class MyModelPass(ModelPass):
+        def __init__(self):
+            super().__init__(self)
+            self.function_changed = False
+
+        def run_on_model(self, model):
+            for op in model.get_ops():
+                if op.get_type_info().name == 'Relu':
+                    self.function_changed = True
+
+    m = Manager()
+    p = m.register_pass(MyModelPass())
+    m.run_passes(get_test_function())
+
+    assert p.function_changed
+
+test_simple_pattern_replacement()
+test_matcher_pass()
+test_model_pass()
