706cd61fcb5598c2f1f40ace25980e4ec3cdc6a4
sadolini <svetlana.a.dolinina@intel.com>
2022-03-15 13:49:43 +0300

    fix code style

--- src/common/transformations/src/transformations/common_optimizations/mark_precision_sensitive_divides.cpp
+++ src/common/transformations/src/transformations/common_optimizations/mark_precision_sensitive_divides.cpp
@@ -51,7 +51,7 @@ bool ov::pass::MarkPrecisionSensitiveDivides::run_on_model(const std::shared_ptr
             if (visited.count(input_node_ptr.get()))
                 continue;
 
-             if (auto sub_graph_node = std::dynamic_pointer_cast<ngraph::op::util::MultiSubGraphOp>(input_node_ptr)) {
+            if (auto sub_graph_node = std::dynamic_pointer_cast<ngraph::op::util::MultiSubGraphOp>(input_node_ptr)) {
                 size_t sub_graphs_num = sub_graph_node->get_internal_subgraphs_size();
                 for (size_t sub_graph_ind = 0; sub_graph_ind < sub_graphs_num; ++sub_graph_ind) {
                     auto sub_graph = sub_graph_node->get_function(sub_graph_ind);
