79dead6e189becfb8e98017ba36349832ad182eb
Ilya Lavrenov <ilya.lavrenov@intel.com>
2022-03-20 10:44:33 +0300

    Updated multi code snippets (#11037)

--- docs/snippets/MULTI2.cpp
+++ docs/snippets/MULTI2.cpp
@@ -4,13 +4,14 @@ int main() {
 //! [part2]
 ov::Core core;
 std::shared_ptr<ov::Model> model = core.read_model("sample.xml");
-std::string allDevices = "MULTI:";
 std::vector<std::string> availableDevices = core.get_available_devices();
+std::string all_devices;
 for (auto && device : availableDevices) {
-    allDevices += device;
-    allDevices += ((device == availableDevices[availableDevices.size()-1]) ? "" : ",");
+    all_devices += device;
+    all_devices += ((device == availableDevices[availableDevices.size()-1]) ? "" : ",");
 }
-ov::CompiledModel compileModel = core.compile_model(model, allDevices);
+ov::CompiledModel compileModel = core.compile_model(model, "MULTI",
+    ov::device::priorities(all_devices));
 //! [part2]
 return 0;
 }
