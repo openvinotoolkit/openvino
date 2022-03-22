8cf23a19fbcf313f8bcdca58e7b5113c03558183
Elizaveta Lobanova <elizaveta.lobanova@intel.com>
2022-03-22 12:02:15 +0300

    Fixed handling of transpose after convolution

--- src/plugins/intel_gna/optimizer/gna_pass_manager.cpp
+++ src/plugins/intel_gna/optimizer/gna_pass_manager.cpp
@@ -685,21 +685,40 @@ void RemovePermutationsNHWCToNCHWPass::run() {
             data->setLayout(layout);
         };
 
-        auto input_to = getInputTo(pattern_start->outData[0]);
-        IE_ASSERT(!input_to.empty());
-        auto current_layer = input_to.begin()->second;
-        setTransposedOrder(current_layer->input());
-        std::function<void(CNNLayerPtr)> propogateNHWCOrderRecursive =
-            [pattern_end, &propogateNHWCOrderRecursive, &setTransposedOrder](CNNLayerPtr current_layer) {
-            if (current_layer == pattern_end) return;
-            for (size_t i = 0; i < current_layer->outData.size(); ++i) {
-                setTransposedOrder(current_layer->outData[i]);
-                auto input_to = getInputTo(current_layer->outData[i]);
-                IE_ASSERT(!input_to.empty());
-                propogateNHWCOrderRecursive(input_to.begin()->second);
+        std::function<std::list<InferenceEngine::DataPtr>(CNNLayerPtr, const std::list<InferenceEngine::DataPtr>&)> getPathBetweenTransposes =
+            [pattern_start, pattern_end, &getPathBetweenTransposes](CNNLayerPtr current_layer, const std::list<InferenceEngine::DataPtr>& path) {
+            if (current_layer == pattern_end) {
+                // the pattern end has been found, return the full path
+                return path;
             }
+
+            // transpose is reached, the pattern end hasn't been found
+            if (current_layer != pattern_start &&
+                (LayerInfo(current_layer).isPermute() || LayerInfo(current_layer).isPermuteViaReshape())) {
+                return std::list<InferenceEngine::DataPtr>();
+            }
+
+            auto new_path(path);
+            std::list<InferenceEngine::DataPtr> mergedChildPath;
+            for (const auto& output : current_layer->outData) {
+                new_path.push_back(output);
+                for (const auto& input : getInputTo(output)) {
+                    auto childPath = getPathBetweenTransposes(input.second, new_path);
+                    // only the branch with the pattern end will return not empty list
+                    if (!childPath.empty()) {
+                        mergedChildPath.insert(std::end(mergedChildPath), std::begin(childPath), std::end(childPath));
+                        break;
+                    }
+                }
+            }
+
+            return mergedChildPath;
         };
-        propogateNHWCOrderRecursive(current_layer);
+
+        auto path = getPathBetweenTransposes(pattern_start, std::list<InferenceEngine::DataPtr>());
+        for (const auto& data : path) {
+            setTransposedOrder(data);
+        }
 
         if ((LayerInfo(pattern_start).isPermute() || LayerInfo(pattern_start).isPermuteViaReshape()) &&
          !getInputTo(pattern_start->outData.front()).empty()) {
