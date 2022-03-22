be1a29bb566de7ca934d0c7f9aedef9ff5c1e894
Evgenya Stepyreva <evgenya.stepyreva@intel.com>
2022-03-22 15:16:33 +0300

    Reshape documentation (#10901) (#11108)
    * Reshape documentation
    
    * Converting Model : reshape metrined, Supported Devices: no shape inference mentioning
    
    * demos removed

--- docs/snippets/ShapeInference.cpp
+++ docs/snippets/ShapeInference.cpp
@@ -1,44 +1,66 @@
-#include <ie_core.hpp>
+#include <openvino/runtime/core.hpp>
+#include <openvino/core/layout.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 
-
 int main() {
-int batch_size = 1;
-//! [part0]
-    InferenceEngine::Core core;
-    // ------------- 0. Read IR and image ----------------------------------------------
-    InferenceEngine::CNNNetwork network = core.ReadNetwork("path/to/IR/xml");
+ov::Core core;
+auto model = core.read_model("path/to/model");
+
+//! [picture_snippet]
+    model->reshape({8, 3, 448, 448});
+//! [picture_snippet]
+
+size_t new_batch = 8;
+//! [set_batch]
+    // Mark up batch in the layout of the input(s) and reset batch to the new value
+    model->get_parameters()[0]->set_layout("N...");
+    ov::set_batch(model, new_batch);
+//! [set_batch]
+
+//! [spatial_reshape]
+    // Read an image and adjust models single input for image to fit
     cv::Mat image = cv::imread("path/to/image");
-    // ---------------------------------------------------------------------------------
+    model->reshape({1, 3, image.rows, image.cols});
+//! [spatial_reshape]
 
-    // ------------- 1. Collect the map of input names and shapes from IR---------------
-    auto input_shapes = network.getInputShapes();
-    // ---------------------------------------------------------------------------------
+//! [obj_to_shape]
+    std::map<ov::Output<ov::Node>, ov::PartialShape> port_to_shape;
+    for (const ov::Output<ov::Node>& input : model->inputs()) {
+        ov::PartialShape shape = input.get_partial_shape();
+        // Modify shape to fit your needs
+        // ...
+        port_to_shape[input] = shape;
+    }
+    model->reshape(port_to_shape);
+//! [obj_to_shape]
 
-    // ------------- 2. Set new input shapes -------------------------------------------
-    std::string input_name;
-    InferenceEngine::SizeVector input_shape;
-    std::tie(input_name, input_shape) = *input_shapes.begin(); // let's consider first input only
-    input_shape[0] = batch_size; // set batch size to the first input dimension
-    input_shape[2] = image.rows; // changes input height to the image one
-    input_shape[3] = image.cols; // changes input width to the image one
-    input_shapes[input_name] = input_shape;
-    // ---------------------------------------------------------------------------------
+//! [idx_to_shape]
+    size_t i = 0;
+    std::map<size_t, ov::PartialShape> idx_to_shape;
+    for (const ov::Output<ov::Node>& input : model->inputs()) {
+        ov::PartialShape shape = input.get_partial_shape();
+        // Modify shape to fit your needs
+        // ...
+        idx_to_shape[i++] = shape;
+    }
+    model->reshape(idx_to_shape);
+//! [idx_to_shape]
 
-    // ------------- 3. Call reshape ---------------------------------------------------
-    network.reshape(input_shapes);
-    // ---------------------------------------------------------------------------------
-
-    //...
-
-    // ------------- 4. Loading model to the device ------------------------------------
-    std::string device = "CPU";
-    InferenceEngine::ExecutableNetwork executable_network = core.LoadNetwork(network, device);
-    // ---------------------------------------------------------------------------------
-
-//! [part0]
+//! [name_to_shape]
+    std::map<std::string, ov::PartialShape> name_to_shape;
+    for (const ov::Output<ov::Node>& input : model->inputs()) {
+        ov::PartialShape shape = input.get_partial_shape();
+        // input may have no name, in such case use map based on input index or port instead
+        if (!input.get_names().empty()) {
+        // Modify shape to fit your needs
+        // ...
+            name_to_shape[input.get_any_name()] = shape;
+        }
+    }
+    model->reshape(name_to_shape);
+//! [name_to_shape]
 
 return 0;
 }
