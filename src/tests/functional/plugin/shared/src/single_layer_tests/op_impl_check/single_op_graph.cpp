81dd8de729d4b612dacee46373bdac76459827b8
intel-ava14 <wilson.seok@intel.com>
2022-03-24 07:07:36 +0900

    modify Constant function

--- src/tests/functional/plugin/shared/src/single_layer_tests/op_impl_check/single_op_graph.cpp
+++ src/tests/functional/plugin/shared/src/single_layer_tests/op_impl_check/single_op_graph.cpp
@@ -178,10 +178,9 @@ std::shared_ptr<ov::Model> generate(const std::shared_ptr<ov::op::v0::Concat> &n
 
 std::shared_ptr<ov::Model> generate(const std::shared_ptr<ov::op::v0::Constant> &node) {
     const auto params = ngraph::builder::makeDynamicParams(ov::element::f32, {{2, 2}});
-    const auto A = ov::op::v0::Constant::create(ov::element::f32, {2, 2}, {1, 2, 3, 4});
+    const auto A = std::make_shared<ov::op::v0::Constant>(ov::element::f32, ov::Shape{2, 2}, 2.0);
     const auto eltwiseNode = std::make_shared<ov::op::v1::Add>(params.at(0), A);
-    ov::ResultVector results{std::make_shared<ov::op::v0::Result>(eltwiseNode)};
-    return std::make_shared<ov::Model>(results, params, "ConstantGraph");
+    return std::make_shared<ov::Model>(A, params, "ConstantGraph");
 }
 
 std::shared_ptr<ov::Model> generate(const std::shared_ptr<ov::op::v0::Convert> &node) {
