ab60ff1109287e75d5c78e962cf4421cbc014280
AlexeyLebedev1 <alexey.lebedev@intel.com>
2022-03-23 09:50:40 +0300

    Fix doc style

--- src/bindings/python/src/openvino/helpers/packing.py
+++ src/bindings/python/src/openvino/helpers/packing.py
@@ -7,15 +7,19 @@ from openvino.runtime import Type, Shape
 
 
 def pack_data(array: np.ndarray, type: Type) -> np.ndarray:
-    """
-    Represent array values as u1,u4 or i4 openvino element type and pack them into uint8 numpy array.
+    """Represent array values as u1,u4 or i4 openvino element type and pack them into uint8 numpy array.
+
     If the number of elements in array is odd we pad them with zero value to be able to fit the bit
     sequence into the uint8 array.
+
     Example: two uint8 values - [7, 8] can be represented as uint4 values and be packed into one int8
              value - [120], because [7, 8] bit representation is [0111, 1000] will be viewed
              as [01111000], which is bit representation of [120].
-    @param array: numpy array with values to pack.
-    @param type: openvino.runtime.Type to interpret the array values. Type must be u1, u4 or i4.
+
+    :param array: numpy array with values to pack.
+    :type array: numpy array
+    :param type: Type to interpret the array values. Type must be u1, u4 or i4.
+    :type type: openvino.runtime.Type
     """
     assert type in [Type.u1, Type.u4, Type.i4], "Packing algorithm for the" \
                                                 "data types stored in 1, 2 or 4 bits"
@@ -46,14 +50,18 @@ def pack_data(array: np.ndarray, type: Type) -> np.ndarray:
 
 
 def unpack_data(array: np.ndarray, type: Type, shape: Union[list, Shape]) -> np.ndarray:
-    """
-    Extract openvino element type values from array into new uint8/int8 array given shape.
+    """Extract openvino element type values from array into new uint8/int8 array given shape.
+
     Example: uint8 value [120] can be represented as two u4 values and be unpacked into [7, 8]
              because [120] bit representation is [01111000] will be viewed as [0111, 1000],
              which is bit representation of [7, 8].
-    @param array: numpy array to unpack.
-    @param type: openvino.element.Type to extract from array values. Type must be u1, u4 or i4.
-    @param shape: the new shape for the unpacked array.
+
+    :param array: numpy array to unpack.
+    :type array: numpy array
+    :param type: Type to extract from array values. Type must be u1, u4 or i4.
+    :type type: openvino.runtime.Type
+    :param shape: the new shape for the unpacked array.
+    :type shape: Union[list, openvino.runtime.Shape]
     """
     assert type in [Type.u1, Type.u4, Type.i4], "Unpacking algorithm for the" \
                                                 "data types stored in 1, 2 or 4 bits"
