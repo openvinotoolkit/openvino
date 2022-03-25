a7f0cdd4ee9fa94f7b07ea0f56e1498a3c27f891
si-eun-kim <sieun.kim@intel.com>
2022-03-25 18:02:42 +0900

    draft pr for planar and fsv16

--- src/plugins/intel_gpu/src/graph/impls/ocl/scatter_nd_update.cpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl/scatter_nd_update.cpp
@@ -61,6 +61,21 @@ attach_scatter_nd_update_impl::attach_scatter_nd_update_impl() {
         std::make_tuple(data_types::f32, format::bfwzyx),
         std::make_tuple(data_types::f16, format::bfwzyx),
         std::make_tuple(data_types::i32, format::bfwzyx),
+        std::make_tuple(data_types::i8,  format::b_fs_yx_fsv16),
+        std::make_tuple(data_types::f16, format::b_fs_yx_fsv16),
+        std::make_tuple(data_types::f32, format::b_fs_yx_fsv16),
+        std::make_tuple(data_types::i8,  format::b_fs_zyx_fsv16),
+        std::make_tuple(data_types::f16, format::b_fs_zyx_fsv16),
+        std::make_tuple(data_types::f32, format::b_fs_zyx_fsv16),
+        std::make_tuple(data_types::i8,  format::b_fs_yx_fsv32),
+        std::make_tuple(data_types::i8,  format::b_fs_zyx_fsv32),
+        std::make_tuple(data_types::i8,  format::fs_b_yx_fsv32),
+        std::make_tuple(data_types::f16, format::bs_fs_yx_bsv32_fsv16),
+        std::make_tuple(data_types::f32, format::bs_fs_yx_bsv32_fsv16),
+        std::make_tuple(data_types::f16, format::bs_fs_zyx_bsv32_fsv16),
+        std::make_tuple(data_types::f32, format::bs_fs_zyx_bsv32_fsv16),
+        std::make_tuple(data_types::i8,  format::bs_fs_yx_bsv32_fsv32),
+        std::make_tuple(data_types::i8,  format::bs_fs_zyx_bsv32_fsv32),
     });
 }
 
