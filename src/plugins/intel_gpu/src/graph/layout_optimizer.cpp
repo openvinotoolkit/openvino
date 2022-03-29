57d4fd845596c71a3faac9ae19cab4c23ccf707d
hyunback <hyunback.kim@intel.com>
2022-03-30 08:14:31 +0900

    Update fisrt conv conditions.
    Signed-off-by: hyunback <hyunback.kim@intel.com>

--- src/plugins/intel_gpu/src/graph/layout_optimizer.cpp
+++ src/plugins/intel_gpu/src/graph/layout_optimizer.cpp
@@ -322,6 +322,14 @@ bool layout_optimizer::can_fuse_reorder(program_node& prev, program_node& next, 
             (fmt_next == format::bs_fs_yx_bsv32_fsv16 && (prev_output_layout.feature() == 3 || prev_output_layout.feature() == 4))))
             return true;
 
+        // Remove reorder to support blocked input for first convolution
+        if (next.is_type<convolution>() && needs_onednn_small_ic_to_blocked(fmt_next, prev_output_layout, next.as<convolution>()) &&
+            ((prev_output_layout.data_type == data_types::f16 && prev_output_layout.batch() < 8 && fmt_prev == format::b_fs_yx_fsv2) ||
+             (prev_output_layout.data_type == data_types::f16 && prev_output_layout.batch() >= 8 && fmt_prev == format::bs_fs_yx_bsv8_fsv2) ||
+             (data_type_traits::is_i8_u8(prev_output_layout.data_type) && prev_output_layout.batch() < 8 && fmt_prev == format::b_fs_yx_fsv4) ||
+             (data_type_traits::is_i8_u8(prev_output_layout.data_type) && prev_output_layout.batch() >= 8 && fmt_prev == format::bs_fs_yx_bsv8_fsv4)))
+            return true;
+
         // Remove Reorder for Convolution: b_fs_yx_fsv32 (i8/u8) -> b_fs_yx_fsv16 (fp32/fp16)
         //                                 b_fs_yx_fsv16 (fp32/fp16) -> b_fs_yx_fsv32 (i8/u8)
         if (next.is_type<convolution>()) {
