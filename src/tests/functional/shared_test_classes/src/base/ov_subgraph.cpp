01b701349d8f39b3f9423ef43b6b5068f0000686
Gleb Kazantaev <gleb.kazantaev@intel.com>
2022-03-29 21:08:55 +0300

    Move tensor utils to common utils (#11306)

--- src/tests/functional/shared_test_classes/src/base/ov_subgraph.cpp
+++ src/tests/functional/shared_test_classes/src/base/ov_subgraph.cpp
@@ -19,7 +19,7 @@
 
 #include "common_test_utils/file_utils.hpp"
 #include "common_test_utils/crash_handler.hpp"
-#include "functional_test_utils/ov_tensor_utils.hpp"
+#include <common_test_utils/ov_tensor_utils.hpp>
 #include "functional_test_utils/skip_tests_config.hpp"
 
 #include "shared_test_classes/base/ov_subgraph.hpp"
