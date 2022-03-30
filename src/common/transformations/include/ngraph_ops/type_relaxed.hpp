08a69eeb37d40b8c62e8443f8a32879eed3f4c91
Vladislav Golubev <vladislav.golubev@intel.com>
2022-03-30 09:28:42 +0300

    Added rt_info initialization for cloned node

--- src/common/transformations/include/ngraph_ops/type_relaxed.hpp
+++ src/common/transformations/include/ngraph_ops/type_relaxed.hpp
@@ -327,6 +327,7 @@ std::shared_ptr<Node> TypeRelaxed<BaseOp>::clone_with_new_inputs(const OutputVec
     // clean rt_info and friendly name (that mustn't be cloned due to semantic of this method)
     new_node->set_friendly_name("");
     new_node->get_rt_info() = {};
+    init_rt_info(*new_node);
     return new_node;
 }
 
