f0c02fb5922ddb8d46357e2499feab7483d50784
Maxim Andronov <maxim.andronov@intel.com>
2022-03-30 10:20:52 +0300

    [CPU] Fix Parameter -> Result model for dynamic case (#10764)

--- src/tests/functional/plugin/cpu/shared_tests_instances/subgraph_tests/parameter_result.cpp
+++ src/tests/functional/plugin/cpu/shared_tests_instances/subgraph_tests/parameter_result.cpp
@@ -8,7 +8,23 @@
 using namespace SubgraphTestsDefinitions;
 
 namespace {
-    INSTANTIATE_TEST_SUITE_P(smoke_Check, ParameterResultSubgraphTest,
-                            ::testing::Values(CommonTestUtils::DEVICE_CPU),
-                            ParameterResultSubgraphTest::getTestCaseName);
+
+INSTANTIATE_TEST_SUITE_P(smoke_Check, ParameterResultSubgraphTestLegacyApi,
+                        ::testing::Combine(
+                            ::testing::Values(ov::test::InputShape{{1, 3, 10, 10}, {}}),
+                            ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+                        ParameterResultSubgraphTestBase::getTestCaseName);
+
+const std::vector<ov::test::InputShape> inputShapes = {
+    ov::test::InputShape{{1, 3, 10, 10}, {{ 1, 3, 10, 10}, { 1, 3, 10, 10}}},
+    ov::test::InputShape{{-1, -1, -1, -1}, {{ 1, 3, 10, 10}, { 2, 5, 3, 10}, { 1, 3, 10, 10}, { 1, 3, 10, 10}}},
+    ov::test::InputShape{{{1, 10}, {1, 10}, {1, 10}, {1, 10}}, {{ 1, 3, 10, 10}, { 2, 5, 3, 10}, { 1, 3, 10, 10}, { 1, 3, 10, 10}}},
+};
+
+INSTANTIATE_TEST_SUITE_P(smoke_Check, ParameterResultSubgraphTest,
+                        ::testing::Combine(
+                            ::testing::ValuesIn(inputShapes),
+                            ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+                        ParameterResultSubgraphTestBase::getTestCaseName);
+
 }  // namespace
