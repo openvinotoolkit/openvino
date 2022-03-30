32ea8a03e313a22e8865e783ccf85d702c61d9ed
hyunback <hyunback.kim@intel.com>
2022-03-31 00:41:37 +0900

    Test for CI.
    Signed-off-by: hyunback <hyunback.kim@intel.com>

--- src/plugins/intel_gpu/src/graph/impls/onednn/utils.cpp
+++ src/plugins/intel_gpu/src/graph/impls/onednn/utils.cpp
@@ -131,6 +131,7 @@ void combine_bf_with_first_spatial_dim(cldnn::layout& l) {
     l.size.spatial[last_spatial_dim_idx] = 1;
 }
 
+/* test */
 int64_t get_f_offset(cldnn::layout l, dnnl::memory::desc desc) {
     int64_t offset = 0;
     auto f_padding = l.data_padding.lower_size().feature[0];
