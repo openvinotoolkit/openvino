84dc51d27bd5fd249c78789d54785db539a37ba0
Nikita Semaev <nikita.semaev@intel.com>
2022-03-31 17:10:20 +0300

    Rollback changes that are related to range

--- src/tests/ie_test_utils/common_test_utils/data_utils.hpp
+++ src/tests/ie_test_utils/common_test_utils/data_utils.hpp
@@ -194,14 +194,14 @@ void inline
 fill_data_random(T *pointer, std::size_t size, const uint32_t range = 10, int32_t start_from = 0, const int32_t k = 1,
                  const int seed = 1) {
     testing::internal::Random random(seed);
-    random.Generate(range * k);
+    random.Generate(range);
 
     if (start_from < 0 && !std::is_signed<T>::value) {
         start_from = 0;
     }
 
     for (std::size_t i = 0; i < size; i++) {
-        pointer[i] = static_cast<T>(start_from + static_cast<T>(random.Generate(range * k)) / k);
+        pointer[i] = static_cast<T>(start_from + static_cast<T>(random.Generate(range)) / k);
     }
 }
 
