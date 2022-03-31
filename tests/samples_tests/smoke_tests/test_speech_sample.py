0c7b6c57b77a6bbdb0bebdbdaae6587e1c2e6b90
Maxim Gordeev <maxim.gordeev@intel.com>
2022-03-31 10:09:31 +0300

    [IE Samples] New command line parameters format for speech sample (#11051)
    * New command line parameters format for speech sample
    
    * fixed notes
    
    * changed format for scale factor
    
    * changed format for scale factor in tests
    
    * added more variants, when name is directy specified for i/o/r like it is done for sf
    
    * removed nthreads flag
    
    * fixed notes
    
    * changed output params
    
    * updated tests with new format
    
    Co-authored-by: Alexander Zhogov <alexander.zhogov@intel.com>

--- tests/samples_tests/smoke_tests/test_speech_sample.py
+++ tests/samples_tests/smoke_tests/test_speech_sample.py
@@ -15,6 +15,7 @@ import pytest
 import sys
 import logging as log
 from common.samples_common_test_clas import SamplesCommonTestClass
+from common.samples_common_test_clas import Environment
 from common.samples_common_test_clas import get_tests
 from common.common_utils import parse_avg_err
 
@@ -27,8 +28,21 @@ test_data = get_tests(cmd_params={'i': [os.path.join('ark', 'dev93_10.ark')],
                                            'o': ['res_output.ark'],
                                            'r': [os.path.join('ark', 'dev93_scores_10.ark')],
                                            'qb': [8, 16],
-                                           'sf': ["Parameter:2175.43", "2175.43"],
+                                           'sf': ["2175.43"],
                                            'q': ["static", "user"],
+                                           'd': ['GNA_SW_EXACT']},
+                               use_device=False
+                               )
+							   
+new_format_test_data = get_tests(cmd_params={'i': ['Parameter=' + os.path.join(Environment.env['test_data'], 'ark', 'dev93_10.ark')],
+                                           'm': [os.path.join('wsj', 'FP32', 'wsj_dnn5b.xml')],
+                                           'layout': ["[NC]"],
+                                           'bs': [1],
+                                           'o': ['affinetransform14/Fused_Add_:0=' + os.path.join(Environment.env['test_data'], 'res_output.ark')],
+                                           'r': ['affinetransform14/Fused_Add_:0=' + os.path.join(Environment.env['test_data'], 'ark', 'dev93_scores_10.ark')],
+                                           'qb': [8],
+                                           'sf': ["Parameter=2175.43"],
+                                           'q': ["static"],
                                            'd': ['GNA_SW_EXACT']},
                                use_device=False
                                )
@@ -43,7 +57,14 @@ class TestSpeechSample(SamplesCommonTestClass):
     @pytest.mark.parametrize("param", test_data)
     def test_speech_sample_nthreads(self, param):
         stdout = self._test(param).split('\n')
-        assert os.path.isfile(param['o']), "Ark file after infer was not found"
+
+        avg_error = parse_avg_err(stdout)
+        log.info('Average scores diff: {}'.format(avg_error))
+        assert avg_error <= self.threshold
+		
+    @pytest.mark.parametrize("param", new_format_test_data)
+    def test_speech_sample_new_format(self, param):
+        stdout = self._test(param, complete_path=False).split('\n')
 
         avg_error = parse_avg_err(stdout)
         log.info('Average scores diff: {}'.format(avg_error))
