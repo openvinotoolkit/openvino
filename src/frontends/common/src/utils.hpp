5a2f7d4b8908c458492b74789af72a73b3e85c86
Mikhail Nosov <mikhail.nosov@intel.com>
2022-03-31 22:23:40 +0300

    Frontend exception safety (#11368)
    * Frontend exception safety
    
    Every call to frontend's API (except Places) can throw exception. If during exception handling, FrontEndManager is destroyed and calls 'dlclose' for plugin - call stack will be corrupted and crash will occur.
    
    Solution is to wrap 'plugins' calls with try/catch and throw new exception in 'openvino' context
    
    TODO: currently "Place" objects don't have 'actual' wrappers, so exception in 'place' objects will potentially cause such crash (if exception handler destroys FrontEndManager). Workaround for user would be to try/catch any calls of Place API on their side.
    We're not expecting users to use Place API directly, so this workaround looks acceptable
    
    * Add check for exception message
    
    * Keep type of frontend exception during rethrow
    
    * IR FE tests: don't expect InferenceEngine::exception as it be not propagated as is by FrontEndManager

--- src/frontends/common/src/utils.hpp
+++ src/frontends/common/src/utils.hpp
@@ -6,6 +6,41 @@
 
 #include "openvino/frontend/visibility.hpp"
 
+#define RETHROW_FRONTEND_EXCEPTION(Type) \
+    catch (const Type& ex) {             \
+        throw Type(ex);                  \
+    }
+
+#define FRONTEND_CALL_STATEMENT(MESSAGE, ...)                       \
+    try {                                                           \
+        __VA_ARGS__;                                                \
+    }                                                               \
+    RETHROW_FRONTEND_EXCEPTION(ov::frontend::GeneralFailure)        \
+    RETHROW_FRONTEND_EXCEPTION(ov::frontend::OpValidationFailure)   \
+    RETHROW_FRONTEND_EXCEPTION(ov::frontend::InitializationFailure) \
+    RETHROW_FRONTEND_EXCEPTION(ov::frontend::OpConversionFailure)   \
+    RETHROW_FRONTEND_EXCEPTION(ov::frontend::NotImplementedFailure) \
+    RETHROW_FRONTEND_EXCEPTION(ov::AssertFailure)                   \
+    RETHROW_FRONTEND_EXCEPTION(ov::Exception)                       \
+    catch (...) {                                                   \
+        OPENVINO_ASSERT(false, (MESSAGE));                          \
+    }
+
+#define FRONTEND_RETURN_STATEMENT(MESSAGE, FUNCTION)                \
+    try {                                                           \
+        return FUNCTION;                                            \
+    }                                                               \
+    RETHROW_FRONTEND_EXCEPTION(ov::frontend::GeneralFailure)        \
+    RETHROW_FRONTEND_EXCEPTION(ov::frontend::OpValidationFailure)   \
+    RETHROW_FRONTEND_EXCEPTION(ov::frontend::InitializationFailure) \
+    RETHROW_FRONTEND_EXCEPTION(ov::frontend::OpConversionFailure)   \
+    RETHROW_FRONTEND_EXCEPTION(ov::frontend::NotImplementedFailure) \
+    RETHROW_FRONTEND_EXCEPTION(ov::AssertFailure)                   \
+    RETHROW_FRONTEND_EXCEPTION(ov::Exception)                       \
+    catch (...) {                                                   \
+        OPENVINO_ASSERT(false, (MESSAGE));                          \
+    }
+
 namespace ov {
 namespace frontend {
 std::string get_frontend_library_path();
