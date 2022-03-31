3686e68d6ccf05a1f5ba422cd293e4c765b336ea
Yury Gaydaychuk <yury.gaydaychuk@intel.com>
2022-03-31 17:05:42 +0300

    clean

--- src/tests/functional/plugin/cpu/single_layer_tests/eye.cpp
+++ src/tests/functional/plugin/cpu/single_layer_tests/eye.cpp
@@ -2,15 +2,19 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include <functional_test_utils/ov_tensor_utils.hpp>
-#include "shared_test_classes/base/ov_subgraph.hpp"
+// #include <functional_test_utils/ov_tensor_utils.hpp>
+// TODO: Remove ?
 #include "test_utils/cpu_test_utils.hpp"
 #include "ngraph_functions/builders.hpp"
 #include "ngraph_functions/utils/ngraph_helpers.hpp"
 #include <ngraph/opsets/opset9.hpp>
 
+#include "shared_test_classes/base/ov_subgraph.hpp"
+#include <common_test_utils/ov_tensor_utils.hpp>
+
 using namespace InferenceEngine;
 using namespace CPUTestUtils;
+using namespace ngraph::helpers;
 using namespace ov::test;
 
 namespace CPULayerTestsDefinitions {
@@ -81,11 +85,6 @@ protected:
         std::tie(pooledVector, inputShape) = adaPoolParams;
 
         init_input_shapes(inputShape);
-        if (!isStatic) {
-            for (auto &target : targetStaticShapes) {
-                target.push_back({pooledVector.size()});
-            }
-        }
 
         selectedType = std::string("unknown_FP32");
         if (netPrecision == ElementType::bf16) {
@@ -95,7 +94,7 @@ protected:
     }
 
     std::shared_ptr<ngraph::Function> createFunction(bool secondInputConst) {
-        // auto params = ngraph::builder::makeDynamicParams(ngraph::element::f32, { inputDynamicShapes[0] });
+        // auto params = ngraph::builder::makeDynamicParams(ngraph::element::i32, { inputDynamicShapes[0] });
         // params.front()->set_friendly_name("ParamsInput");
         // std::shared_ptr<ov::Node> secondInput;
         // if (secondInputConst) {
@@ -106,63 +105,21 @@ protected:
         //     params.push_back(pooledParam);
         //     secondInput = pooledParam;
         // }
-
-        // auto adapoolMax = std::make_shared<ngraph::opset8::AdaptiveMaxPool>(params[0], secondInput, ngraph::element::i32);
-        // adapoolMax->get_rt_info() = getCPUInfo();
-        // auto adapoolAvg = std::make_shared<ngraph::opset8::AdaptiveAvgPool>(params[0], secondInput);
-        // adapoolAvg->get_rt_info() = getCPUInfo();
-
-        // auto function = (mode == "max" ? std::make_shared<ngraph::Function>(adapoolMax->outputs(), params, "AdaPoolMax") :
-        //             std::make_shared<ngraph::Function>(adapoolAvg->outputs(), params, "AdaPoolAvg"));
-        //
         auto rowsPar = std::make_shared<ngraph::opset5::Parameter>(ngraph::element::i32, ngraph::Shape{});
         rowsPar->set_friendly_name("rows");
         auto colsPar = std::make_shared<ngraph::opset5::Parameter>(ngraph::element::i32, ngraph::Shape{});
         colsPar->set_friendly_name("cols");
-        auto eyelike = std::make_shared<ngraph::op::v9::Eye>(rowsPar, colsPar, ngraph::element::i32);
+        auto diagPar = std::make_shared<ngraph::opset5::Parameter>(ngraph::element::i32, ngraph::Shape{});
+        diagPar->set_friendly_name("diagInd");
+        auto eyelike = std::make_shared<ngraph::op::v9::Eye>(rowsPar, colsPar, diagPar, ngraph::element::i32);
         eyelike->get_rt_info() = getCPUInfo();
 
-        auto function2 = std::make_shared<ngraph::Function>(eyelike->outputs(), ngraph::ParameterVector{rowsPar, colsPar}, "EyeLike");
+        auto function2 = std::make_shared<ngraph::Function>(eyelike->outputs(),
+            ngraph::ParameterVector{rowsPar, colsPar, diagPar}, "EyeLike");
 
         //
         return function2;
     }
-
-    //    void SetUp() override {
-//        CPULayerTestsDefinitions::RangeLayerTestParams basicParamsSet;
-//        CPUSpecificParams cpuParams;
-//        std::tie(basicParamsSet, cpuParams) = this->GetParam();
-//        std::tie(inFmts, outFmts, priority, selectedType) = cpuParams;
-//        CPULayerTestsDefinitions::RangeSpecificParams rangeParams;
-//        std::pair<std::vector<ngraph::PartialShape>, std::vector<std::vector<ngraph::Shape>>> shapes;
-//        std::tie(rangeParams, inPrc, targetDevice) = basicParamsSet;
-//        std::tuple<float, float, float> rangeInputs;
-//
-//        std::tie(shapes, rangeInputs, outPrc) = rangeParams;
-//        targetStaticShapes = shapes.second;
-//        inputDynamicShapes = shapes.first;
-//
-//        start = std::get<0>(rangeInputs);
-//        stop = std::get<1>(rangeInputs);
-//        step = std::get<2>(rangeInputs);
-//        auto ngOutPr = FuncTestUtils::PrecisionUtils::convertIE2nGraphPrc(outPrc);
-//        auto ngNetPrc = FuncTestUtils::PrecisionUtils::convertIE2nGraphPrc(inPrc);
-//        auto startPar = std::make_shared<ngraph::opset5::Parameter>(ngNetPrc, ngraph::Shape{});
-//        auto stopPar = std::make_shared<ngraph::opset5::Parameter>(ngNetPrc, ngraph::Shape{});
-//        auto stepPar = std::make_shared<ngraph::opset5::Parameter>(ngNetPrc, ngraph::Shape{});
-//        auto range = std::make_shared<ngraph::opset4::Range>(startPar, stopPar, stepPar, ngOutPr);
-//        range->get_rt_info() = getCPUInfo();
-//        selectedType = std::string("ref_any_") + (inPrc == outPrc ? inPrc.name() : "FP32");
-//        startPar->set_friendly_name("start");
-//        stopPar->set_friendly_name("stop");
-//        stepPar->set_friendly_name("step");
-//
-//        const ngraph::ResultVector results{std::make_shared<ngraph::opset3::Result>(range)};
-//        function = std::make_shared<ngraph::Function>(results, ngraph::ParameterVector {
-//            startPar, stopPar, stepPar}, "Range");
-//        functionRefs = ngraph::clone_function(*function);
-//    }
-//};
 
     void init_ref_function(std::shared_ptr<ov::Model> &funcRef, const std::vector<ov::Shape>& targetInputStaticShapes) override {
         if (function->get_parameters().size() == 2) {
@@ -176,15 +133,7 @@ protected:
         const auto& funcInputs = function->inputs();
         for (int i = 0; i < funcInputs.size(); ++i) {
             const auto& funcInput = funcInputs[i];
-            ov::Tensor tensor;
-
-            if (false && i == 1) {
-                tensor = ov::Tensor(funcInput.get_element_type(), targetInputStaticShapes[i]);
-                auto *dataPtr = tensor.data<int32_t>();
-                dataPtr[i] = 3;
-            } else {
-                    tensor = ov::test::utils::create_and_fill_tensor(funcInput.get_element_type(), targetInputStaticShapes[i], 1, 3);
-            }
+            ov::Tensor tensor = ov::test::utils::create_and_fill_tensor(funcInput.get_element_type(), targetInputStaticShapes[i], 1, 3);
             inputs.insert({funcInput.get_node_shared_ptr(), tensor});
         }
     }
@@ -202,68 +151,21 @@ TEST_P(EyeLikeLayerCPUTest, CompareWithRefs) {
 namespace {
 
 /* CPU PARAMS */
-std::vector<CPUSpecificParams> filterCPUInfoForDevice(std::string dims = "3D", std::string modeStr = "max") {
+std::vector<CPUSpecificParams> filterCPUInfoForDevice(std::string dims = "", std::string modeStr = "") {
         std::vector<CPUSpecificParams> resCPUParams;
         resCPUParams.push_back(CPUSpecificParams{{}, {}, {}, {}});
         return resCPUParams;
-//     if (modeStr == "max") {
-//         if (dims == "5D") {
-//             resCPUParams.push_back(CPUSpecificParams{{ncdhw, x}, {ncdhw}, {}, {}});  // i.e. two equal output layouts
-//             resCPUParams.push_back(CPUSpecificParams{{ndhwc, x}, {ndhwc, ncdhw}, {}, {}});
-//             if (with_cpu_x86_avx512f()) {
-//                 resCPUParams.push_back(CPUSpecificParams{{nCdhw16c, x}, {nCdhw16c, ncdhw}, {}, {}});
-//             } else if (with_cpu_x86_avx2() || with_cpu_x86_sse42()) {
-//                 resCPUParams.push_back(CPUSpecificParams{{nCdhw8c, x}, {nCdhw8c, ncdhw}, {}, {}});
-//             }
-//         } else if (dims == "4D") {
-//             resCPUParams.push_back(CPUSpecificParams{{nchw, x}, {nchw}, {}, {}});  // i.e. two equal output layouts
-//             resCPUParams.push_back(CPUSpecificParams{{nhwc, x}, {nhwc, nchw}, {}, {}});
-//             if (with_cpu_x86_avx512f()) {
-//                 resCPUParams.push_back(CPUSpecificParams{{nChw16c, x}, {nChw16c, nchw}, {}, {}});
-//             } else if (with_cpu_x86_avx2() || with_cpu_x86_sse42()) {
-//                 resCPUParams.push_back(CPUSpecificParams{{nChw8c, x}, {nChw8c, nchw}, {}, {}});
-//             }
-//         } else {
-//             resCPUParams.push_back(CPUSpecificParams{{ncw, x}, {ncw}, {}, {}});  // i.e. two equal output layouts
-//             resCPUParams.push_back(CPUSpecificParams{{nwc, x}, {nwc, ncw}, {}, {}});
-//             if (with_cpu_x86_avx512f()) {
-//                 resCPUParams.push_back(CPUSpecificParams{{nCw16c, x}, {nCw16c, ncw}, {}, {}});
-//             } else if (with_cpu_x86_avx2() || with_cpu_x86_sse42()) {
-//                 resCPUParams.push_back(CPUSpecificParams{{nCw8c, x}, {nCw8c, ncw}, {}, {}});
-//             }
-//         }
-//     } else {
-//         if (dims == "5D") {
-//             resCPUParams.push_back(CPUSpecificParams{{ncdhw, x}, {ncdhw}, {}, {}});
-//             resCPUParams.push_back(CPUSpecificParams{{ndhwc, x}, {ndhwc}, {}, {}});
-//             if (with_cpu_x86_avx512f()) {
-//                 resCPUParams.push_back(CPUSpecificParams{{nCdhw16c, x}, {nCdhw16c}, {}, {}});
-//             } else if (with_cpu_x86_avx2() || with_cpu_x86_sse42()) {
-//                 resCPUParams.push_back(CPUSpecificParams{{nCdhw8c, x}, {nCdhw8c}, {}, {}});
-//             }
-//         } else if (dims == "4D") {
-//             resCPUParams.push_back(CPUSpecificParams{{nchw, x}, {nchw}, {}, {}});
-//             resCPUParams.push_back(CPUSpecificParams{{nhwc, x}, {nhwc}, {}, {}});
-//             if (with_cpu_x86_avx512f()) {
-//                 resCPUParams.push_back(CPUSpecificParams{{nChw16c, x}, {nChw16c}, {}, {}});
-//             } else if (with_cpu_x86_avx2() || with_cpu_x86_sse42()) {
-//                 resCPUParams.push_back(CPUSpecificParams{{nChw8c, x}, {nChw8c}, {}, {}});
-//             }
-//         } else {
-//             resCPUParams.push_back(CPUSpecificParams{{ncw, x}, {ncw}, {}, {}});
-//             resCPUParams.push_back(CPUSpecificParams{{nwc, x}, {nwc}, {}, {}});
-//             if (with_cpu_x86_avx512f()) {
-//                 resCPUParams.push_back(CPUSpecificParams{{nCw16c, x}, {nCw16c}, {}, {}});
-//             } else if (with_cpu_x86_avx2() || with_cpu_x86_sse42()) {
-//                 resCPUParams.push_back(CPUSpecificParams{{nCw8c, x}, {nCw8c}, {}, {}});
-//             }
-//         }
-//     }
-//     return resCPUParams;
 }
 
 const std::vector<ElementType> netPrecisions = {
         ElementType::i32
+};
+
+std::vector<std::vector<ov::Shape>> staticInput3DShapeVector = {{{}, {}, {}}};
+
+std::vector<std::pair<std::vector<ngraph::PartialShape>, std::vector<std::vector<ngraph::Shape>>>> inShapesDynamic = {
+       {{ngraph::PartialShape(), ngraph::PartialShape(), ngraph::PartialShape()},
+        {{ngraph::Shape{}, ngraph::Shape{}}, {ngraph::Shape{}, ngraph::Shape{}}}}
 };
 
 const std::vector<std::vector<int>> pooled3DVector = {
@@ -271,126 +173,21 @@ const std::vector<std::vector<int>> pooled3DVector = {
         { 3 },
         { 5 }
 };
-const std::vector<std::vector<int>> pooled4DVector = {
-        { 1, 1 },
-        { 3, 5 },
-        { 5, 5 }
-};
 
-const std::vector<std::vector<int>> pooled5DVector = {
-        { 1, 1, 1 },
-        { 3, 5, 1 },
-        { 3, 5, 3 },
-};
-
-std::vector<std::vector<ov::Shape>> staticInput3DShapeVector = {{{}, {}}};
-
-const std::vector<std::vector<InputShape>> input3DShapeVector = {
-        {
-                {{{-1, 17, -1}, {{1, 17, 3}, {3, 17, 5}, {3, 17, 5}}}},
-                {{{{1, 10}, 20, {1, 10}}, {{1, 20, 5}, {2, 20, 4}, {3, 20, 6}}}}
-        }
-};
-
-std::vector<std::vector<ov::Shape>> staticInput4DShapeVector = {{{1, 3, 1, 1}, {3, 17, 5, 2}}};
-
-const std::vector<std::vector<InputShape>> input4DShapeVector = {
-        {
-                {{{-1, 3, -1, -1}, {{1, 3, 1, 1}, {3, 3, 5, 2}, {3, 3, 5, 2}}}},
-                {{{{1, 10}, 3, {1, 10}, {1, 10}}, {{2, 3, 10, 6}, {3, 3, 6, 5}, {3, 3, 6, 5}}}}
-        }
-};
-
-std::vector<std::vector<ov::Shape>> staticInput5DShapeVector = {{{ 1, 17, 2, 5, 2}, {3, 17, 4, 5, 4}}};
-
-const std::vector<std::vector<InputShape>> input5DShapeVector = {
-        {
-                {{{-1, 17, -1, -1, -1}, {{1, 17, 2, 5, 2}, {3, 17, 4, 5, 4}, {3, 17, 4, 5, 4}}}},
-                {{{{1, 10}, 3, {1, 10}, {1, 10}, {1, 10}}, {{3, 3, 2, 5, 2}, {1, 3, 4, 5, 4}, {1, 3, 4, 5, 4}}}}
-        }
-};
-
-const auto adaPool3DParams = ::testing::Combine(
+const auto staticParams = ::testing::Combine(
         ::testing::ValuesIn(pooled3DVector),         // output spatial shape
-        ::testing::ValuesIn(input3DShapeVector)      // feature map shape
-);
-
-const auto adaPool4DParams = ::testing::Combine(
-        ::testing::ValuesIn(pooled4DVector),         // output spatial shape
-        ::testing::ValuesIn(input4DShapeVector)     // feature map shape
-);
-
-const auto adaPool5DParams = ::testing::Combine(
-        ::testing::ValuesIn(pooled5DVector),         // output spatial shape
-        ::testing::ValuesIn(input5DShapeVector)     // feature map shape
-);
-
-std::vector<std::pair<std::vector<ngraph::PartialShape>, std::vector<std::vector<ngraph::Shape>>>> inShapesDynamic = {
-       {{ngraph::PartialShape(), ngraph::PartialShape()},
-        {{ngraph::Shape{}, ngraph::Shape{}}, {ngraph::Shape{}, ngraph::Shape{}}}}
-};
-
-const auto staticAdaPool3DParams = ::testing::Combine(
-        ::testing::ValuesIn(pooled3DVector),         // output spatial shape
-        ::testing::ValuesIn(static_shapes_to_test_representation(staticInput3DShapeVector))      // feature map shape
-);
-
-const auto staticAdaPool4DParams = ::testing::Combine(
-        ::testing::ValuesIn(pooled4DVector),         // output spatial shape
-        ::testing::ValuesIn(static_shapes_to_test_representation(staticInput4DShapeVector))     // feature map shape
-);
-
-const auto staticAdaPool5DParams = ::testing::Combine(
-        ::testing::ValuesIn(pooled5DVector),         // output spatial shape
-        ::testing::ValuesIn(static_shapes_to_test_representation(staticInput5DShapeVector))     // feature map shape
+        ::testing::ValuesIn(static_shapes_to_test_representation(staticInput3DShapeVector))
 );
 
 INSTANTIATE_TEST_SUITE_P(x_smoke_StaticAdaPoolAvg3DLayoutTest, EyeLikeLayerCPUTest,
                          ::testing::Combine(
                                  ::testing::Combine(
-                                         staticAdaPool3DParams,
+                                         staticParams,
                                          ::testing::Values("avg"),
                                          ::testing::Values(true),
                                          ::testing::ValuesIn(netPrecisions),
                                          ::testing::Values(CommonTestUtils::DEVICE_CPU)),
-                                 ::testing::ValuesIn(filterCPUInfoForDevice("3D", "avg"))),
+                                 ::testing::ValuesIn(filterCPUInfoForDevice("", ""))),
                          EyeLikeLayerCPUTest::getTestCaseName);
-
-// in 1-channel cases  {..., 1, 1, 1} shape cannot be correctly resolved on oneDnn level, so it was removed from instances
-
-const std::vector<std::vector<InputShape>> input3DShape1Channel = {
-        {
-                {{{-1, -1, -1}, {{1, 1, 2}, {1, 1, 2}, {1, 1, 2}}}},
-                {{{{1, 10}, {1, 10}, {1, 10}}, {{1, 1, 2}, {2, 1, 2}, {2, 1, 2}}}}
-        }
-};
-
-const std::vector<std::vector<InputShape>> input4DShape1Channel = {
-        {
-                {{{-1, -1, -1, -1}, {{1, 1, 1, 2}, {2, 1, 2, 1}, {2, 1, 2, 1}}}},
-                {{{{1, 10}, {1, 10}, {1, 10}, {1, 10}}, {{1, 1, 1, 2}, {1, 1, 1, 2}, {2, 1, 2, 1}}}}
-        }
-};
-
-const std::vector<std::vector<InputShape>> input5DShape1Channel = {
-        {
-                {{{-1, -1, -1, -1, -1}, {{1, 1, 1, 1, 2}, {1, 1, 1, 1, 2}, {2, 1, 1, 2, 1}}}},
-                {{{{1, 10}, {1, 10}, {1, 10}, {1, 10}, {1, 10}}, {{1, 1, 1, 1, 2}, {1, 1, 1, 1, 2}, {2, 1, 1, 2, 1}}}}
-        }
-};
-
-// INSTANTIATE_TEST_SUITE_P(x_smoke_AdaPool_1ch_Avg3DLayoutTest, EyeLikeLayerCPUTest,
-//                          ::testing::Combine(
-//                                  ::testing::Combine(
-//                                          ::testing::Combine(
-//                                                  ::testing::ValuesIn(std::vector<std::vector<int>> {
-//                                                          {1}, {2}}),
-//                                                  ::testing::ValuesIn(input3DShape1Channel)),
-//                                          ::testing::Values("avg"),
-//                                          ::testing::Values(true),
-//                                          ::testing::ValuesIn(netPrecisions),
-//                                          ::testing::Values(CommonTestUtils::DEVICE_CPU)),
-//                                  ::testing::Values(CPUSpecificParams{{}, {}, {}, {}})),
-//                          EyeLikeLayerCPUTest::getTestCaseName);
 } // namespace
 } // namespace CPULayerTestsDefinitions
