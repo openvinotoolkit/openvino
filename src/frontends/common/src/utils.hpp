774252765aeb577cc1e0eca74241971af4a9dc6b
Mikhail Nosov <mikhail.nosov@intel.com>
2022-03-31 16:33:50 +0300

    Frontend exception safety
    Every call to frontend's API (except Places) can throw exception. If during exception handling, FrontEndManager is destroyed and calls 'dlclose' for plugin - call stack will be corrupted and crash will occur.
    
    Solution is to wrap 'plugins' calls with try/catch and throw new exception in 'openvino' context
    
    TODO: currently "Place" objects don't have 'actual' wrappers, so exception in 'place' objects will potentially cause such crash (if exception handler destroys FrontEndManager). Workaround for user would be to try/catch any calls of Place API on their side.
    We're not expecting users to use Place API directly, so this workaround looks acceptable

--- src/frontends/common/src/utils.hpp
+++ src/frontends/common/src/utils.hpp
@@ -6,6 +6,24 @@
 
 #include "openvino/frontend/visibility.hpp"
 
+#define FRONTEND_CALL_STATEMENT(MESSAGE, ...) \
+    try {                                     \
+        __VA_ARGS__;                          \
+    } catch (const std::exception& ex) {      \
+        throw ex;                             \
+    } catch (...) {                           \
+        OPENVINO_ASSERT(false, (MESSAGE));    \
+    }
+
+#define FRONTEND_RETURN_STATEMENT(MESSAGE, FUNCTION) \
+    try {                                            \
+        return FUNCTION;                             \
+    } catch (const std::exception& ex) {             \
+        throw ex;                                    \
+    } catch (...) {                                  \
+        OPENVINO_ASSERT(false, (MESSAGE));           \
+    }
+
 namespace ov {
 namespace frontend {
 std::string get_frontend_library_path();
