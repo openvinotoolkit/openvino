1d3e021898f4565d709b65698afa8dc96e671674
Yury Gaydaychuk <yury.gaydaychuk@intel.com>
2022-03-31 17:37:11 +0300

    clean

--- src/tests/functional/plugin/cpu/single_layer_tests/eye.cpp
+++ src/tests/functional/plugin/cpu/single_layer_tests/eye.cpp
@@ -19,41 +19,30 @@ using namespace ov::test;
 
 namespace CPULayerTestsDefinitions {
 namespace {
-    std::vector<int> pooledSpatialShape;
-    std::string mode;
     std::vector<InputShape> inputShape;
 }  // namespace
 
-using AdaPoolSpecificParams = std::tuple<
-        std::vector<int>,        // pooled vector
-        std::vector<InputShape>>;      // feature map shape
-
-using AdaPoolLayerTestParams = std::tuple<
-        AdaPoolSpecificParams,
-        std::string,         // mode
-        bool,                // second Input is Constant
+using EyeLayerTestParams = std::tuple<
+        std::vector<InputShape>,
         ElementType,         // Net precision
         TargetDevice>;       // Device name
 
 using EyeLikeLayerCPUTestParamsSet = std::tuple<
-        CPULayerTestsDefinitions::AdaPoolLayerTestParams,
+        CPULayerTestsDefinitions::EyeLayerTestParams,
         CPUSpecificParams>;
 
 class EyeLikeLayerCPUTest : public testing::WithParamInterface<EyeLikeLayerCPUTestParamsSet>,
                             virtual public SubgraphBaseTest, public CPUTestsBase {
 public:
     static std::string getTestCaseName(testing::TestParamInfo<EyeLikeLayerCPUTestParamsSet> obj) {
-        CPULayerTestsDefinitions::AdaPoolLayerTestParams basicParamsSet;
+        CPULayerTestsDefinitions::EyeLayerTestParams basicParamsSet;
         CPUSpecificParams cpuParams;
         std::tie(basicParamsSet, cpuParams) = obj.param;
         std::string td;
         ElementType netPr;
-        bool isStatic;
-        AdaPoolSpecificParams adaPar;
-        std::tie(adaPar, mode, isStatic, netPr, td) = basicParamsSet;
-        std::tie(pooledSpatialShape, inputShape) = adaPar;
+        std::tie(inputShape, netPr, td) = basicParamsSet;
         std::ostringstream result;
-        result << "AdaPoolTest_";
+        result << "EyeTest_";
         result << "IS=(";
         for (const auto& shape : inputShape) {
             result << CommonTestUtils::partialShape2str({shape.first}) << "_";
@@ -64,47 +53,28 @@ public:
                 result << CommonTestUtils::vec2str(item) << "_";
             }
         }
-        result << "OS=" << CommonTestUtils::vec2str(pooledSpatialShape) << "(spat.)_";
         result << netPr << "_";
-        result << mode << "_";
         result << CPUTestsBase::getTestCaseName(cpuParams) << "_";
         result << std::to_string(obj.index);
         return result.str();
     }
 protected:
     void SetUp() override {
-        CPULayerTestsDefinitions::AdaPoolLayerTestParams basicParamsSet;
+        CPULayerTestsDefinitions::EyeLayerTestParams basicParamsSet;
         CPUSpecificParams cpuParams;
         std::tie(basicParamsSet, cpuParams) = this->GetParam();
         std::tie(inFmts, outFmts, priority, selectedType) = cpuParams;
 
-        CPULayerTestsDefinitions::AdaPoolSpecificParams adaPoolParams;
         ElementType netPrecision;
-        bool isStatic;
-        std::tie(adaPoolParams, mode, isStatic, netPrecision, targetDevice) = basicParamsSet;
-        std::tie(pooledVector, inputShape) = adaPoolParams;
+        std::tie(inputShape, netPrecision, targetDevice) = basicParamsSet;
 
         init_input_shapes(inputShape);
 
-        selectedType = std::string("unknown_FP32");
-        if (netPrecision == ElementType::bf16) {
-            rel_threshold = 1e-2;
-        }
-        function = createFunction(isStatic);
+        selectedType = std::string("unknown_I32");
+        function = createFunction(true);
     }
 
     std::shared_ptr<ngraph::Function> createFunction(bool secondInputConst) {
-        // auto params = ngraph::builder::makeDynamicParams(ngraph::element::i32, { inputDynamicShapes[0] });
-        // params.front()->set_friendly_name("ParamsInput");
-        // std::shared_ptr<ov::Node> secondInput;
-        // if (secondInputConst) {
-        //     secondInput = ngraph::op::Constant::create(ngraph::element::i32, ngraph::Shape{pooledVector.size()}, pooledVector);
-        // } else {
-        //     auto pooledParam = std::make_shared<ngraph::opset1::Parameter>(ngraph::element::i32, ngraph::Shape{pooledVector.size()});
-        //     pooledParam->set_friendly_name("ParamSecondInput");
-        //     params.push_back(pooledParam);
-        //     secondInput = pooledParam;
-        // }
         auto rowsPar = std::make_shared<ngraph::opset5::Parameter>(ngraph::element::i32, ngraph::Shape{});
         rowsPar->set_friendly_name("rows");
         auto colsPar = std::make_shared<ngraph::opset5::Parameter>(ngraph::element::i32, ngraph::Shape{});
@@ -121,13 +91,6 @@ protected:
         return function2;
     }
 
-    void init_ref_function(std::shared_ptr<ov::Model> &funcRef, const std::vector<ov::Shape>& targetInputStaticShapes) override {
-        if (function->get_parameters().size() == 2) {
-            funcRef = createFunction(true);
-        }
-        ngraph::helpers::resize_function(funcRef, targetInputStaticShapes);
-    }
-
     void generate_inputs(const std::vector<ov::Shape>& targetInputStaticShapes) override {
         inputs.clear();
         const auto& funcInputs = function->inputs();
@@ -137,9 +100,6 @@ protected:
             inputs.insert({funcInput.get_node_shared_ptr(), tensor});
         }
     }
-
-private:
-    std::vector<int> pooledVector;
 };
 
 TEST_P(EyeLikeLayerCPUTest, CompareWithRefs) {
@@ -168,23 +128,10 @@ std::vector<std::pair<std::vector<ngraph::PartialShape>, std::vector<std::vector
         {{ngraph::Shape{}, ngraph::Shape{}}, {ngraph::Shape{}, ngraph::Shape{}}}}
 };
 
-const std::vector<std::vector<int>> pooled3DVector = {
-        { 1 },
-        { 3 },
-        { 5 }
-};
-
-const auto staticParams = ::testing::Combine(
-        ::testing::ValuesIn(pooled3DVector),         // output spatial shape
-        ::testing::ValuesIn(static_shapes_to_test_representation(staticInput3DShapeVector))
-);
-
-INSTANTIATE_TEST_SUITE_P(x_smoke_StaticAdaPoolAvg3DLayoutTest, EyeLikeLayerCPUTest,
+INSTANTIATE_TEST_SUITE_P(x_smoke_EyeTest, EyeLikeLayerCPUTest,
                          ::testing::Combine(
                                  ::testing::Combine(
-                                         staticParams,
-                                         ::testing::Values("avg"),
-                                         ::testing::Values(true),
+                                         ::testing::ValuesIn(static_shapes_to_test_representation(staticInput3DShapeVector)),
                                          ::testing::ValuesIn(netPrecisions),
                                          ::testing::Values(CommonTestUtils::DEVICE_CPU)),
                                  ::testing::ValuesIn(filterCPUInfoForDevice("", ""))),
