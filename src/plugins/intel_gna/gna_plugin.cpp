1e085d3f11cac39b89f1ec026db4eb4c5571d8d7
Anton Grishin <anton.grishin@intel.com>
2022-04-01 11:04:11 +0300

    completly remove bidirectional_sequences_decomposition

--- src/plugins/intel_gna/gna_plugin.cpp
+++ src/plugins/intel_gna/gna_plugin.cpp
@@ -75,7 +75,6 @@
 #include "transformations/op_conversions/lstm_cell_decomposition.hpp"
 #include "transformations/op_conversions/gru_cell_decomposition.hpp"
 #include "transformations/op_conversions/convert_sequences_to_tensor_iterator.hpp"
-#include "transformations/op_conversions/bidirectional_sequences_decomposition.hpp"
 #include "transformations/remove_single_input_concat.hpp"
 #include "transformations/remove_converts.hpp"
 #include "transformations/broadcast_const.hpp"
