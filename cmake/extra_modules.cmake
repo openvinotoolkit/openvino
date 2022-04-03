89eebc8ea5a5dd02d930df3d6597b0321033e716
Ilya Lavrenov <ilya.lavrenov@intel.com>
2022-04-03 12:35:32 +0300

    handle system gflags in developer package

--- cmake/extra_modules.cmake
+++ cmake/extra_modules.cmake
@@ -10,10 +10,15 @@ function(ie_generate_dev_package_config)
     foreach(component IN LISTS openvino_export_components)
         # export all targets with prefix and use them during extra modules build
         export(TARGETS ${${component}} NAMESPACE IE::
-            APPEND FILE "${CMAKE_BINARY_DIR}/${component}_dev_targets.cmake")
+               APPEND FILE "${CMAKE_BINARY_DIR}/${component}_dev_targets.cmake")
         list(APPEND all_dev_targets ${${component}})
     endforeach()
     add_custom_target(ie_dev_targets DEPENDS ${all_dev_targets})
+
+    # if we've found system gflags
+    if(gflags_DIR)
+        set(gflags_BINARY_DIR "${gflags_DIR}")
+    endif()
 
     configure_package_config_file("${OpenVINO_SOURCE_DIR}/cmake/templates/InferenceEngineDeveloperPackageConfig.cmake.in"
                                   "${CMAKE_BINARY_DIR}/InferenceEngineDeveloperPackageConfig.cmake"
@@ -33,14 +38,19 @@ function(ov_generate_dev_package_config)
     set(all_dev_targets ov_runtime_libraries)
     foreach(component IN LISTS openvino_export_components)
         string(FIND "${component}" "_legacy" index)
-        if (index EQUAL -1)
+        if(index EQUAL -1)
             # export all targets with prefix and use them during extra modules build
             export(TARGETS ${${component}} NAMESPACE openvino::
-                APPEND FILE "${CMAKE_BINARY_DIR}/ov_${component}_dev_targets.cmake")
+                   APPEND FILE "${CMAKE_BINARY_DIR}/ov_${component}_dev_targets.cmake")
             list(APPEND all_dev_targets ${${component}})
         endif()
     endforeach()
     add_custom_target(ov_dev_targets DEPENDS ${all_dev_targets})
+
+    # if we've found system gflags
+    if(gflags_DIR)
+        set(gflags_BINARY_DIR "${gflags_DIR}")
+    endif()
 
     configure_package_config_file("${OpenVINO_SOURCE_DIR}/cmake/templates/OpenVINODeveloperPackageConfig.cmake.in"
                                   "${CMAKE_BINARY_DIR}/OpenVINODeveloperPackageConfig.cmake"
