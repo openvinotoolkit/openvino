dee1088d3e47a5d7155f97eb3d5b10fe5177806a
Roman Kazantsev <roman.kazantsev@intel.com>
2022-04-04 12:30:31 +0300

    [MO] Support TensorFlow Grouped Conv2DBackpropInput (#11420)
    * [MO] Support TensorFlow Grouped Conv2DBackpropInput
    
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>
    
    * Correct computation of group number for ConvBackpropInput operation
    
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>
    
    * Fix get_conv_backprop_groups function
    
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>
    
    * Add unit-tests for Deconvolution shape inference
    
    Signed-off-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- tools/mo/openvino/tools/mo/front/tf/deconv_ext.py
+++ tools/mo/openvino/tools/mo/front/tf/deconv_ext.py
@@ -1,9 +1,11 @@
 # Copyright (C) 2018-2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from openvino.tools.mo.front.common.partial_infer.utils import convert_deconv_tf_padding_to_str, int64_array
+from openvino.tools.mo.front.common.partial_infer.utils import convert_deconv_tf_padding_to_str, int64_array, \
+    dynamic_dimension
 from openvino.tools.mo.front.extractor import FrontExtractorOp
-from openvino.tools.mo.front.tf.extractors.utils import tf_data_format_spatial, tf_data_format_channel, tf_data_format_batch, \
+from openvino.tools.mo.front.tf.extractors.utils import tf_data_format_spatial, tf_data_format_channel, \
+    tf_data_format_batch, \
     tf_int_list
 from openvino.tools.mo.ops.deconvolution import Deconvolution
 from openvino.tools.mo.ops.op import PermuteAttrs
@@ -17,6 +19,7 @@ class Conv2DBackpropInputFrontExtractor(FrontExtractorOp):
     def extract(cls, node):
         attrs = tf_create_attrs(node, 3, 2)
         attrs.update({'op': cls.op,
+                      'get_group': get_conv_backprop_groups,
                       'get_weights_permute': PermuteAttrs.Permutation(perm=int64_array([3, 2, 0, 1]),
                                                                       inv=int64_array([2, 3, 1, 0])),
                       'swap_0_and_2_inputs': True,
@@ -36,6 +39,7 @@ class Conv3DBackpropInputV2InputFrontExtractor(FrontExtractorOp):
     def extract(cls, node):
         attrs = tf_create_attrs(node, 4, 3)
         attrs.update({'op': cls.op,
+                      'get_group': get_conv_backprop_groups,
                       'get_weights_permute': PermuteAttrs.Permutation(perm=int64_array([4, 3, 0, 1, 2]),
                                                                       inv=int64_array([2, 3, 4, 1, 0])),
                       'swap_0_and_2_inputs': True,
@@ -69,3 +73,20 @@ def tf_create_attrs(node, input_feature_channel, output_feature_channel):
         'input_feature_channel': input_feature_channel,
         'output_feature_channel': output_feature_channel,
     }
+
+
+def get_conv_backprop_groups(node):
+    # output shape is required input for TensorFlow ConvBackpropInput operation and contains output shape values
+    # in the form [batch_size, output_height, output_width, output_channel], so that
+    # groups number = output_channel // kernel_out_channels, where
+    # kernel shape is given as [kernel_height, kernel_width, kernel_out_channels, in_channels]
+    output_shape = node.in_port(2).data.get_value()
+    kernel_shape = node.in_port(1).data.get_shape()
+    if node.has_and_set('group'):
+        return node.group
+    elif output_shape is not None and kernel_shape is not None \
+            and output_shape[node.channel_dims[0]] is not dynamic_dimension \
+            and kernel_shape[node.output_feature_channel] is not dynamic_dimension:
+        return output_shape[node.channel_dims] // kernel_shape[node.output_feature_channel]
+    else:
+        return 1
