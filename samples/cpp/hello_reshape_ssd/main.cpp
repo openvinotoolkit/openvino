395d50ddff8fe73f2666f7eed1621566402d5ad4
Maxim Gordeev <maxim.gordeev@intel.com>
2022-04-05 13:14:52 +0300

    [IE Samples] Changed condition for hello_reshape_ssd according moving of layer in model

--- samples/cpp/hello_reshape_ssd/main.cpp
+++ samples/cpp/hello_reshape_ssd/main.cpp
@@ -9,6 +9,7 @@
 // clang-format off
 #include "openvino/openvino.hpp"
 #include "ngraph/ngraph.hpp"
+#include "ngraph/opsets/opset8.hpp"
 
 #include "format_reader_ptr.h"
 #include "samples/args_helper.hpp"
@@ -53,7 +54,7 @@ int main(int argc, char* argv[]) {
         // try to find it.
         ov::NodeVector ops = model->get_ops();
         auto it = std::find_if(ops.begin(), ops.end(), [](std::shared_ptr<ov::Node> node) {
-            return node->get_type_info() == ngraph::op::DetectionOutput::get_type_info_static();
+            return node->get_type_info() == ngraph::opset8::DetectionOutput::get_type_info_static();
         });
         if (it == ops.end()) {
             throw std::logic_error("model does not contain DetectionOutput layer");
