ecd78da757e079f4bdde09955f993ac9a22ea138
Angel2014fly1 <101121237+Angel2014fly1@users.noreply.github.com>
2022-04-23 20:35:50 +0100

    Config
    For get model of the cars

---
+++ .github/github_org_control/config.py
@@ -0,0 +1,8 @@
+# Copyright © 2019 by Spectrico
+# Licensed under the MIT License
+
+model_file = "model-weights-spectrico-mmr-mobilenet-128x128-344FF72B.pb"  # path to the car make and model classifier
+label_file = "labels.txt"   # path to the text file, containing list with the supported makes and models
+input_layer = "input_1"
+output_layer = "softmax/Softmax"
+classifier_input_size = (128, 128)  # input size of the classifier
