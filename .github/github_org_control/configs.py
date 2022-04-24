0429b36377085acf83a8dceabce09856899b1dd0
Angel2014fly1 <101121237+Angel2014fly1@users.noreply.github.com>
2022-04-24 09:36:43 +0100

    Update configs.py

--- .github/github_org_control/configs.py
+++ .github/github_org_control/configs.py
@@ -115,13 +115,6 @@ def _test():
     print("Config.properties:", cfg.properties)
     print("cfg.PROXIES:", cfg.PROXIES)
 
-# Author's' Jurijus Pacalovas
-def config_model(self):
-    model_file = "model-weights-spectrico-mmr-mobilenet-128x128-344FF72B.pb"  # path to the car make and model classifier
-    label_file = "labels.txt"   # path to the text file, containing list with the supported makes and models
-    input_layer = "input_1"
-    output_layer = "softmax/Softmax"
-    classifier_input_size = (128, 128)  # input size of the classifier
 
 if __name__ == "__main__":
     _test()
