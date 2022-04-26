a5dcd0238198377589cdf4ddd2733ea6105456db
mitruska <katarzyna.mitrus@intel.com>
2022-04-26 23:15:48 +0200

    Add template plugin tests for dynamic shapes

--- docs/template_plugin/tests/functional/op_reference/eye.cpp
+++ docs/template_plugin/tests/functional/op_reference/eye.cpp
@@ -18,13 +18,15 @@ struct EyeParams {
               const reference_tests::Tensor& diagonal_index,
               const element::Type& output_type,
               const reference_tests::Tensor& expected_tensor,
-              const std::string& test_case_name)
+              const std::string& test_case_name,
+              bool is_dyn_shape_test = false)
               : num_rows(num_rows),
               num_columns(num_columns),
               diagonal_index(diagonal_index),
               output_type(output_type),
               expected_tensor(expected_tensor),
-              test_case_name(test_case_name) {}
+              test_case_name(test_case_name),
+              set_dynamic_shape(is_dyn_shape_test) {}
 
     reference_tests::Tensor num_rows;
     reference_tests::Tensor num_columns;
@@ -32,6 +34,7 @@ struct EyeParams {
     element::Type output_type;
     reference_tests::Tensor expected_tensor;
     std::string test_case_name;
+    bool set_dynamic_shape = false;
 };
 
 struct EyeBatchShapeParams {
@@ -66,24 +69,25 @@ public:
         function = CreateFunction(params.num_rows,
                                   params.num_columns,
                                   params.diagonal_index,
-                                  params.output_type);
+                                  params.output_type,
+                                  params.set_dynamic_shape);
         inputData = {params.num_rows.data, params.num_columns.data, params.diagonal_index.data};
         refOutData = {params.expected_tensor.data};
     }
 
     static std::string getTestCaseName(const testing::TestParamInfo<EyeParams>& obj) {
-        auto param = obj.param;
-        return param.test_case_name;
+        return obj.param.test_case_name + (obj.param.set_dynamic_shape ? "_dyn_shape_inputs" : "");
     }
 
 private:
     static std::shared_ptr<Model> CreateFunction(const reference_tests::Tensor& num_rows,
                                                  const reference_tests::Tensor& num_columns,
                                                  const reference_tests::Tensor& diagonal_index,
-                                                 const element::Type& output_type) {
-        const auto in1 = std::make_shared<op::v0::Parameter>(num_rows.type, num_rows.shape);
-        const auto in2 = std::make_shared<op::v0::Parameter>(num_columns.type, num_columns.shape);
-        const auto in3 = std::make_shared<op::v0::Parameter>(diagonal_index.type, diagonal_index.shape);
+                                                 const element::Type& output_type,
+                                                 bool set_dynamic_shape = false) {
+        const auto in1 = std::make_shared<op::v0::Parameter>(num_rows.type, set_dynamic_shape ? PartialShape::dynamic() : num_rows.shape);
+        const auto in2 = std::make_shared<op::v0::Parameter>(num_columns.type, set_dynamic_shape ? PartialShape::dynamic() : num_columns.shape);
+        const auto in3 = std::make_shared<op::v0::Parameter>(diagonal_index.type, set_dynamic_shape ? PartialShape::dynamic() : diagonal_index.shape);
         const auto Eye = std::make_shared<op::v9::Eye>(in1, in2, in3, output_type);
         return std::make_shared<Model>(NodeVector{Eye}, ParameterVector{in1, in2, in3});
     }
@@ -130,42 +134,55 @@ TEST_P(ReferenceEyeBatchShapeLayerTest, EyeRectangleBatchShapeWithHardcodedRefs)
     Exec();
 }
 
+std::vector<EyeParams> generateEyeParams(bool is_dyn_shape_test = false) {
+    std::vector<EyeParams> test_params {
+        EyeParams(reference_tests::Tensor{{1}, element::i32, std::vector<int32_t>{3}},
+                    reference_tests::Tensor{{1}, element::i32, std::vector<int32_t>{2}},
+                    reference_tests::Tensor{{1}, element::i32, std::vector<int32_t>{0}},
+                    element::Type_t::f32,
+                    reference_tests::Tensor{{3, 2}, element::f32, std::vector<float>{1, 0,
+                                                                                    0, 1,
+                                                                                    0, 0}},
+                    "float32_default_3x2", is_dyn_shape_test),
+        EyeParams(reference_tests::Tensor{{}, element::i32, std::vector<int32_t>{2}},
+                    reference_tests::Tensor{{}, element::i32, std::vector<int32_t>{4}},
+                    reference_tests::Tensor{{}, element::i32, std::vector<int32_t>{2}},
+                    element::Type_t::i8,
+                    reference_tests::Tensor{{2, 4}, element::i8, std::vector<int8_t>{0, 0, 1, 0,
+                                                                                    0, 0, 0, 1}},
+                    "int8_diag=2_2x4", is_dyn_shape_test),
+        EyeParams(reference_tests::Tensor{{1}, element::i32, std::vector<int32_t>{4}},
+                    reference_tests::Tensor{{1}, element::i32, std::vector<int32_t>{2}},
+                    reference_tests::Tensor{{}, element::i32, std::vector<int32_t>{-3}},
+                    element::Type_t::i64,
+                    reference_tests::Tensor{{4, 2}, element::i64, std::vector<int64_t>{0, 0,
+                                                                                        0, 0,
+                                                                                        0, 0,
+                                                                                        1, 0}},
+                    "int64_diag=-3_4x2", is_dyn_shape_test),
+        EyeParams(reference_tests::Tensor{{}, element::i32, std::vector<int32_t>{1}},
+                    reference_tests::Tensor{{}, element::i32, std::vector<int32_t>{4}},
+                    reference_tests::Tensor{{}, element::i32, std::vector<int32_t>{10}},
+                    element::Type_t::i8,
+                    reference_tests::Tensor{{1, 4}, element::i8, std::vector<int8_t>{0, 0, 0, 0}},
+                    "int8_empty_1x4", is_dyn_shape_test)};
+    return test_params;
+}
+
+std::vector<EyeParams> generateEyeCombinedParams() {
+    std::vector<EyeParams> combined_params = generateEyeParams(false);
+    std::vector<EyeParams> dyn_shape_params = generateEyeParams(true);
+    combined_params.insert(combined_params.end(), dyn_shape_params.begin(), dyn_shape_params.end());
+    return combined_params;
+}
+
 }  // namespace
 
 INSTANTIATE_TEST_SUITE_P(
         smoke_Eye_With_Hardcoded_Refs,
         ReferenceEyeLayerTest,
-        ::testing::Values(
-                EyeParams(reference_tests::Tensor{{1}, element::i32, std::vector<int32_t>{3}},
-                          reference_tests::Tensor{{1}, element::i32, std::vector<int32_t>{2}},
-                          reference_tests::Tensor{{1}, element::i32, std::vector<int32_t>{0}},
-                          element::Type_t::f32,
-                          reference_tests::Tensor{{3, 2}, element::f32, std::vector<float>{1, 0,
-                                                                                           0, 1,
-                                                                                           0, 0}},
-                          "float32_default_3x2"),
-                EyeParams(reference_tests::Tensor{{}, element::i32, std::vector<int32_t>{2}},
-                          reference_tests::Tensor{{}, element::i32, std::vector<int32_t>{4}},
-                          reference_tests::Tensor{{}, element::i32, std::vector<int32_t>{2}},
-                          element::Type_t::i8,
-                          reference_tests::Tensor{{2, 4}, element::i8, std::vector<int8_t>{0, 0, 1, 0,
-                                                                                           0, 0, 0, 1}},
-                          "int8_diag=2_2x4"),
-                EyeParams(reference_tests::Tensor{{1}, element::i32, std::vector<int32_t>{4}},
-                          reference_tests::Tensor{{1}, element::i32, std::vector<int32_t>{2}},
-                          reference_tests::Tensor{{}, element::i32, std::vector<int32_t>{-3}},
-                          element::Type_t::i64,
-                          reference_tests::Tensor{{4, 2}, element::i64, std::vector<int64_t>{0, 0,
-                                                                                             0, 0,
-                                                                                             0, 0,
-                                                                                             1, 0}},
-                          "int64_diag=-3_4x2"),
-                EyeParams(reference_tests::Tensor{{}, element::i32, std::vector<int32_t>{1}},
-                          reference_tests::Tensor{{}, element::i32, std::vector<int32_t>{4}},
-                          reference_tests::Tensor{{}, element::i32, std::vector<int32_t>{10}},
-                          element::Type_t::i8,
-                          reference_tests::Tensor{{1, 4}, element::i8, std::vector<int8_t>{0, 0, 0, 0}},
-                          "int8_empty_1x4")),
+        // Generate params with static and dynamic shapes
+        ::testing::ValuesIn(generateEyeCombinedParams()),
         ReferenceEyeLayerTest::getTestCaseName);
 
 INSTANTIATE_TEST_SUITE_P(
@@ -212,4 +229,4 @@ INSTANTIATE_TEST_SUITE_P(
                                     "uint8_1x3x2x2")),
         ReferenceEyeBatchShapeLayerTest::getTestCaseName);
 
-}  // namespace reference_tests 
+}  // namespace reference_tests
