70979cf13f43cec54b934fd4005535cbf89d3b0c
bszmelcz <bartosz.szmelczynski@intel.com>
2022-04-26 11:05:52 +0200

    fix wrong imports

--- src/bindings/python/src/openvino/runtime/opset9/ops.py
+++ src/bindings/python/src/openvino/runtime/opset9/ops.py
@@ -5,10 +5,15 @@
 from functools import partial
 from typing import Optional
 
-from ngraph.impl import Node
-from ngraph.opset_utils import _get_node_factory
-from ngraph.utils.decorators import nameable_op
-from ngraph.utils.types import NodeInput, as_nodes
+import numpy as np
+from openvino.runtime import Node
+from openvino.runtime.opset_utils import _get_node_factory
+from openvino.runtime.utils.decorators import nameable_op
+from openvino.runtime.utils.types import (
+    NodeInput,
+    as_nodes,
+    as_node
+)
 
 
 _get_node_factory_opset9 = partial(_get_node_factory, "opset9")
