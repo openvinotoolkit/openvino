a7e35e7430e914bd4776f38623da7b984160a3ab
p-wysocki <przemyslaw.wysocki@intel.com>
2022-05-02 10:27:19 +0200

    Remove unnecessary TF NMS update

--- tools/mo/openvino/tools/mo/front/tf/non_max_suppression_ext.py
+++ tools/mo/openvino/tools/mo/front/tf/non_max_suppression_ext.py
@@ -59,18 +59,3 @@ class NonMaxSuppressionV5Extractor(FrontExtractorOp):
         attrs = {'sort_result_descending': 1, 'box_encoding': 'corner', 'output_type': np.int32}
         NonMaxSuppression.update_node_stat(node, attrs)
         return cls.enabled
-
-
-class NonMaxSuppressionV9Extractor(FrontExtractorOp):
-    op = 'NonMaxSuppressionV9'
-    enabled = True
-
-    @classmethod
-    def extract(cls, node):
-        pad_to_max_output_size = node.pb.attr["pad_to_max_output_size:"].b
-        if not pad_to_max_output_size:
-            log.warning('The attribute "pad_to_max_output_size" of node {} is equal to False which is not supported. '
-                        'Forcing it to be equal to True'.format(node.soft_get('name')))
-        attrs = {'sort_result_descending': 1, 'box_encoding': 'corner', 'output_type': np.int64}
-        NonMaxSuppression.update_node_stat(node, attrs)
-        return cls.enabled
