6687e9fcdccd9fc0d49bcb9316d1c1d5aaa20769
Helena <helena.kloosterman@intel.com>
2022-05-11 21:13:43 +0200

    Update date in MO update message

--- tools/mo/openvino/tools/mo/utils/get_ov_update_message.py
+++ tools/mo/openvino/tools/mo/utils/get_ov_update_message.py
@@ -3,15 +3,15 @@
 
 import datetime
 
-msg_fmt = 'It\'s been a while, check for a new version of ' + \
-          'Intel(R) Distribution of OpenVINO(TM) toolkit here {0} or on the GitHub*'
+msg_fmt = 'Check for a new version of Intel(R) Distribution of OpenVINO(TM) toolkit here {0} ' \
+          'or on https://github.com/openvinotoolkit/openvino'
 
 
 def get_ov_update_message():
-    expected_update_date = datetime.date(year=2022, month=4, day=30)
+    expected_update_date = datetime.date(year=2022, month=10, day=31)
     current_date = datetime.date.today()
 
-    link = 'https://software.intel.com/content/www/us/en/develop/tools/openvino-toolkit/download.html?cid=other&source=prod&campid=ww_2022_bu_IOTG_OpenVINO-2022-1&content=upg_all&medium=organic'
+    link = 'https://software.intel.com/content/www/us/en/develop/tools/openvino-toolkit/download.html?cid=other&source=prod&campid=ww_2022_bu_IOTG_OpenVINO-2022-2&content=upg_all&medium=organic'
 
     return msg_fmt.format(link) if current_date >= expected_update_date else None
 
