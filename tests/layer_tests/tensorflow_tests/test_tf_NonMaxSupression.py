773f0bd0623b578eb43c625186b4814b155e72da
p-wysocki <przemyslaw.wysocki@intel.com>
2022-05-11 10:05:17 +0200

    Add tests for soft NMS

--- tests/layer_tests/tensorflow_tests/test_tf_NonMaxSupression.py
+++ tests/layer_tests/tensorflow_tests/test_tf_NonMaxSupression.py
@@ -42,21 +42,49 @@ class TestNonMaxSupression(CommonTFLayerTest):
         ref_net = None
         return tf_net, ref_net
 
+    def create_nms_net_with_scores(self, test_params: dict):
+
+        tf.compat.v1.reset_default_graph()
+        with tf.compat.v1.Session() as sess:
+
+            # parametrized inputs
+            number_of_boxes = test_params["number_of_boxes"]
+            max_output_size = tf.constant(test_params["max_output_size"])
+            iou_threshold = tf.constant(test_params["iou_threshold"])
+            score_threshold = tf.constant(test_params["score_threshold"])
+            soft_nms_sigma = tf.constant(test_params["soft_nms_sigma"])
+
+            # inputs to be generated
+            boxes = tf.compat.v1.placeholder(tf.float32, [number_of_boxes, 4], 'Input')
+
+            # randomize boxes' confidence scores
+            np.random.seed(42)
+            scores = np.random.uniform(low=0.2, high=1.0, size=[number_of_boxes])
+
+            _ = tf.image.non_max_suppression_with_scores(boxes, scores, max_output_size,
+                                             iou_threshold, score_threshold, soft_nms_sigma, name="NMS")
+            tf_net = sess.graph_def
+
+        ref_net = None
+        return tf_net, ref_net
+
     test_params = [
             (
                 {
                     "number_of_boxes": 50,
                     "max_output_size": 5,
                     "iou_threshold": 0.7,
-                    "score_threshold": 0.8
+                    "score_threshold": 0.8,
+                    "soft_nms_sigma": 0.1
                 }
             ),
             (
                 {
                     "number_of_boxes": 50,
-                    "max_output_size": 10,
+                    "max_output_size": 9,
                     "iou_threshold": 0.7,
                     "score_threshold": 0.7,
+                    "soft_nms_sigma": 0.4
                 }
             ),
             (
@@ -64,14 +92,24 @@ class TestNonMaxSupression(CommonTFLayerTest):
                     "number_of_boxes": 50,
                     "max_output_size": 3,
                     "iou_threshold": 0.3,
-                    "score_threshold": 0.8
+                    "score_threshold": 0.8,
+                    "soft_nms_sigma": 0.7
                 }
             )
         ]
 
     @pytest.mark.parametrize("test_params", test_params)
     @pytest.mark.nightly
+    @pytest.mark.precommit
     def test_NonMaxSupression(self, test_params, ie_device, precision, ir_version, temp_dir,
                               use_new_frontend, api_2):
         self._test(*self.create_nms_net(test_params), ie_device, precision,
+                   ir_version, temp_dir, api_2, use_new_frontend)
+
+    @pytest.mark.parametrize("test_params", test_params)
+    @pytest.mark.nightly
+    @pytest.mark.precommit
+    def test_NonMaxSupressionWithScores(self, test_params, ie_device, precision, ir_version, temp_dir,
+                                        use_new_frontend, api_2):
+        self._test(*self.create_nms_net_with_scores(test_params), ie_device, precision,
                    ir_version, temp_dir, api_2, use_new_frontend)
