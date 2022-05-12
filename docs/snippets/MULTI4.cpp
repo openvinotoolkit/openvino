4eb888c9033ea72dcbd18e0d077d4238c96c638e
Karol Blaszczak <karol.blaszczak@intel.com>
2022-05-12 09:52:27 +0200

    Docs multiplugin page-wide tabs merge (#11461)
    * Update multi_device.md
    
    * druga runda
    
    * runda trzecia
    
    11
    
    * Update docs/OV_Runtime_UG/multi_device.md
    
    * Update docs/OV_Runtime_UG/multi_device.md
    
    * Update docs/OV_Runtime_UG/multi_device.md
    
    * Update docs/OV_Runtime_UG/multi_device.md
    
    * Update docs/OV_Runtime_UG/multi_device.md
    
    * Update docs/OV_Runtime_UG/multi_device.md
    
    * Update docs/OV_Runtime_UG/multi_device.md
    
    * Update docs/OV_Runtime_UG/multi_device.md
    
    * Update docs/OV_Runtime_UG/supported_plugins/Device_Plugins.md
    
    * correct post review
    
    * align the property table
    
    * Update docs/OV_Runtime_UG/auto_device_selection.md
    
    * Update docs/OV_Runtime_UG/multi_device.md
    
    Co-authored-by: Yuan Xu <yuan1.xu@intel.com>
    
    * Update docs/OV_Runtime_UG/multi_device.md
    
    Co-authored-by: Yuan Xu <yuan1.xu@intel.com>
    
    * Update docs/OV_Runtime_UG/multi_device.md
    
    Co-authored-by: Yuan Xu <yuan1.xu@intel.com>
    
    * Update docs/OV_Runtime_UG/multi_device.md
    
    Co-authored-by: Yuan Xu <yuan1.xu@intel.com>
    
    * Update docs/OV_Runtime_UG/multi_device.md
    
    Co-authored-by: Yuan Xu <yuan1.xu@intel.com>
    
    * Update docs/OV_Runtime_UG/multi_device.md
    
    Co-authored-by: Yuan Xu <yuan1.xu@intel.com>
    
    * Update docs/OV_Runtime_UG/supported_plugins/Device_Plugins.md
    
    Co-authored-by: Yuan Xu <yuan1.xu@intel.com>
    
    * Update docs/OV_Runtime_UG/supported_plugins/Device_Plugins.md
    
    Co-authored-by: Yuan Xu <yuan1.xu@intel.com>
    
    * Update docs/OV_Runtime_UG/supported_plugins/Device_Plugins.md
    
    Co-authored-by: Yuan Xu <yuan1.xu@intel.com>
    
    * Update docs/OV_Runtime_UG/supported_plugins/Device_Plugins.md
    
    Co-authored-by: Yuan Xu <yuan1.xu@intel.com>
    
    Co-authored-by: Yuan Xu <yuan1.xu@intel.com>

--- docs/snippets/MULTI4.cpp
+++ docs/snippets/MULTI4.cpp
@@ -3,18 +3,19 @@
 int main() {
 ov::AnyMap hddl_config, gpu_config;
 //! [part4]
-// configure the HDDL device first
 ov::Core core;
-std::shared_ptr<ov::Model> model = core.read_model("sample.xml");
-// compile the modle on the multi-device,
-// while specifying the configuration (devices along with priorities
-// and the configuration of devices):
-ov::CompiledModel compileModel = core.compile_model(model, "MULTI",
-    ov::device::priorities("HDDL", "GPU"),
-    ov::device::properties("HDDL", hddl_config),
-    ov::device::properties("GPU", gpu_config));
 
-// query the optimal number of requests:
+// Read a network in IR, PaddlePaddle, or ONNX format:
+std::shared_ptr<ov::Model> model = core.read_model("sample.xml");
+
+// When compiling the model on MULTI, configure GPU and HDDL 
+// (devices, priorities, and device configurations):
+ov::CompiledModel compiled_model = core.compile_model(model, "MULTI",
+    ov::device::priorities("HDDL", "GPU"),
+    ov::device::properties("GPU", gpu_config),
+    ov::device::properties("HDDL", myriad_config));
+
+// Optionally, query the optimal number of requests:
 uint32_t nireq = compileModel.get_property(ov::optimal_number_of_infer_requests);
 //! [part4]
 return 0;
