8886d0fde7a98d09b6a8c02b2136a57ca9336b53
Chen Xu <chen.xu@intel.com>
2022-05-20 11:05:17 +0800

    [CPU] Fix shape mismatching in fusing per channel (#11162)
    * Fix shape mismatching in fusing per channel
    
    * channelAxis data type changes to int

--- src/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
+++ src/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
@@ -83,8 +83,6 @@ std::vector<std::string> disabledTestPatterns() {
         R"(.*(Auto|Multi).*Behavior.*IncorrectConfigTests.*CanNotLoadNetworkWithIncorrectConfig.*)",
         R"(.*OVExecutableNetworkBaseTest.*(CanGetInputsInfoAndCheck|CanSetConfigToExecNet).*)",
         R"(.*Behavior.*CorrectConfigCheck.*(canSetConfigAndCheckGetConfig|canSetConfigTwiceAndCheckGetConfig).*CPU_BIND_THREAD=YES.*)",
-        // Issue: 62846 Here shape[1] is not the channel dimension size
-        R"(.*smoke_Reduce.*KeepNoDims.*(_axes=\((0.*|.*1.*)).*Fused=.*PerChannel.*)",
         // Issue: 72021 Unreasonable abs_threshold for comparing bf16 results
         R"(.*smoke_Reduce.*type=(Prod|Min).*netPRC=(BF|bf)16.*)",
         // TODO: 56520 Accuracy mismatch
