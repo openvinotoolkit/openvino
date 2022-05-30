f068a183a32b5a5b726ce68105f9d66ef5b4c7e6
jiwaszki <jan.iwaszkiewicz@intel.com>
2022-05-30 15:28:15 +0000

    Add SoftSign op as default operator for onnx frontend

--- src/frontends/onnx/frontend/src/op/softsign.cpp
+++ src/frontends/onnx/frontend/src/op/softsign.cpp
@@ -15,19 +15,9 @@ namespace onnx_import {
 namespace op {
 namespace set_1 {
 OutputVector softsign(const Node& node) {
-    auto data = node.get_ng_inputs().at(0);
-
-    std::shared_ptr<ngraph::Node> one_node = default_opset::Constant::create(data.get_element_type(), Shape{}, {1});
-    auto abs_data = std::make_shared<default_opset::Abs>(data);
-    auto data_plus_one_node = std::make_shared<default_opset::Add>(abs_data, one_node);
-
-    return {std::make_shared<default_opset::Divide>(data, data_plus_one_node)};
+    return {std::make_shared<default_opset::SoftSign>(node.get_ng_inputs().at(0))};
 }
-
 }  // namespace set_1
-
 }  // namespace op
-
 }  // namespace onnx_import
-
 }  // namespace ngraph
