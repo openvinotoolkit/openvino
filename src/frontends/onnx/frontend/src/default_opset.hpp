6eff7dec19defcb1880c47ed3949ec78437470b6
jiwaszki <jan.iwaszkiewicz@intel.com>
2022-05-31 09:43:30 +0000

    Revert "Change default opset for onnx frontend"
    This reverts commit 19991e192392debe23f1f5c6fe412077c212eaa8.

--- src/frontends/onnx/frontend/src/default_opset.hpp
+++ src/frontends/onnx/frontend/src/default_opset.hpp
@@ -2,10 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "ngraph/opsets/opset9.hpp"
+#include "ngraph/opsets/opset8.hpp"
 
 namespace ngraph {
 namespace onnx_import {
-namespace default_opset = ngraph::opset9;
+namespace default_opset = ngraph::opset8;
 }
 }  // namespace ngraph
