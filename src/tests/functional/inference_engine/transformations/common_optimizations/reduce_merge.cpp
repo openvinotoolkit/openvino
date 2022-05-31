e5592bbde5001b413161f44fc707bacbc9e24ea0
Artur Kulikowski <artur.kulikowski@intel.com>
2022-05-31 19:00:52 +0200

    Compare the produced function with expected instead of counting ops

--- src/tests/functional/inference_engine/transformations/common_optimizations/reduce_merge.cpp
+++ src/tests/functional/inference_engine/transformations/common_optimizations/reduce_merge.cpp
@@ -13,8 +13,7 @@
 
 using namespace ngraph;
 
-TEST(ReduceMerge, ReduceL1) {
-    std::shared_ptr<Function> f;
+TEST_F(TransformationTestsF, ReduceMergeReduceL1) {
     {
         Shape shape{1};
         auto type = element::f32;
@@ -22,18 +21,22 @@ TEST(ReduceMerge, ReduceL1) {
         auto reduce1_axes = op::Constant::create(element::i64, Shape{1}, {0});
         auto reduce1 = std::make_shared<opset9::ReduceL1>(A, reduce1_axes, true);
         auto reduce2_axis = op::Constant::create(element::i64, Shape{1}, {0});
-        f = std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceL1>(reduce1, reduce2_axis, true)},
+        function =
+            std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceL1>(reduce1, reduce2_axis, true)},
                                        ParameterVector{A});
+        manager.register_pass<pass::ReduceMerge>();
     }
-    pass::Manager pass_manager;
-    pass_manager.register_pass<pass::ReduceMerge>();
-    pass_manager.run_passes(f);
-    ASSERT_TRUE(count_ops_of_type<opset9::ReduceL1>(f) == 1);
-    ASSERT_TRUE(count_ops_of_type<opset9::Concat>(f) == 1);
+    {
+        auto data = std::make_shared<op::Parameter>(element::f32, Shape{1});
+        auto axis1 = op::Constant::create(element::i64, Shape{1}, {0});
+        auto axis2 = op::Constant::create(element::i64, Shape{1}, {0});
+        auto concat = std::make_shared<opset9::Concat>(OutputVector{axis1, axis2}, 0);
+        auto reduce = std::make_shared<opset9::ReduceL1>(data, concat, true);
+        function_ref = std::make_shared<Function>(OutputVector{reduce}, ParameterVector{data});
+    }
 }
 
-TEST(ReduceMerge, ReduceL2) {
-    std::shared_ptr<Function> f;
+TEST_F(TransformationTestsF, ReduceMergeReduceL2) {
     {
         Shape shape{1};
         auto type = element::f32;
@@ -41,156 +44,191 @@ TEST(ReduceMerge, ReduceL2) {
         auto reduce1_axes = op::Constant::create(element::i64, Shape{1}, {0});
         auto reduce1 = std::make_shared<opset9::ReduceL2>(A, reduce1_axes, true);
         auto reduce2_axis = op::Constant::create(element::i64, Shape{1}, {0});
-        f = std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceL2>(reduce1, reduce2_axis, true)},
+        function =
+            std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceL2>(reduce1, reduce2_axis, true)},
                                        ParameterVector{A});
+        manager.register_pass<pass::ReduceMerge>();
     }
-    pass::Manager pass_manager;
-    pass_manager.register_pass<pass::ReduceMerge>();
-    pass_manager.run_passes(f);
-    ASSERT_TRUE(count_ops_of_type<opset9::ReduceL2>(f) == 1);
-    ASSERT_TRUE(count_ops_of_type<opset9::Concat>(f) == 1);
+    {
+        auto data = std::make_shared<op::Parameter>(element::f32, Shape{1});
+        auto axis1 = op::Constant::create(element::i64, Shape{1}, {0});
+        auto axis2 = op::Constant::create(element::i64, Shape{1}, {0});
+        auto concat = std::make_shared<opset9::Concat>(OutputVector{axis1, axis2}, 0);
+        auto reduce = std::make_shared<opset9::ReduceL2>(data, concat, true);
+        function_ref = std::make_shared<Function>(OutputVector{reduce}, ParameterVector{data});
+    }
 }
 
-TEST(ReduceMerge, ReduceLogicalAnd) {
-    std::shared_ptr<Function> f;
+TEST_F(TransformationTestsF, ReduceMergeReduceLogicalAnd) {
     {
         auto A = std::make_shared<op::Parameter>(element::boolean, Shape{1});
         auto reduce1_axis = op::Constant::create(element::i64, Shape{1}, {0});
         auto reduce1 = std::make_shared<opset9::ReduceLogicalAnd>(A, reduce1_axis, true);
         auto reduce2_axis = op::Constant::create(element::i64, Shape{1}, {0});
-        f = std::make_shared<Function>(
+        function = std::make_shared<Function>(
             OutputVector{std::make_shared<opset9::ReduceLogicalAnd>(reduce1, reduce2_axis, true)},
             ParameterVector{A});
+        manager.register_pass<pass::ReduceMerge>();
     }
-    pass::Manager pass_manager;
-    pass_manager.register_pass<pass::ReduceMerge>();
-    pass_manager.run_passes(f);
-    ASSERT_TRUE(count_ops_of_type<opset9::ReduceLogicalAnd>(f) == 1);
-    ASSERT_TRUE(count_ops_of_type<opset9::Concat>(f) == 1);
+    {
+        auto data = std::make_shared<op::Parameter>(element::boolean, Shape{1});
+        auto axis1 = op::Constant::create(element::i64, Shape{1}, {0});
+        auto axis2 = op::Constant::create(element::i64, Shape{1}, {0});
+        auto concat = std::make_shared<opset9::Concat>(OutputVector{axis1, axis2}, 0);
+        auto reduce = std::make_shared<opset9::ReduceLogicalAnd>(data, concat, true);
+        function_ref = std::make_shared<Function>(OutputVector{reduce}, ParameterVector{data});
+    }
 }
 
-TEST(ReduceMerge, ReduceLogicalOr) {
-    std::shared_ptr<Function> f;
+TEST_F(TransformationTestsF, ReduceMergeReduceLogicalOr) {
     {
         auto A = std::make_shared<op::Parameter>(element::boolean, Shape{1});
         auto reduce1_axes = op::Constant::create(element::i64, Shape{1}, {0});
         auto reduce1 = std::make_shared<opset9::ReduceLogicalOr>(A, reduce1_axes, true);
         auto reduce2_axis = op::Constant::create(element::i64, Shape{1}, {0});
-        f = std::make_shared<Function>(
+        function = std::make_shared<Function>(
             OutputVector{std::make_shared<opset9::ReduceLogicalOr>(reduce1, reduce2_axis, true)},
             ParameterVector{A});
+        manager.register_pass<pass::ReduceMerge>();
     }
-    pass::Manager pass_manager;
-    pass_manager.register_pass<pass::ReduceMerge>();
-    pass_manager.run_passes(f);
-    ASSERT_TRUE(count_ops_of_type<opset9::ReduceLogicalOr>(f) == 1);
-    ASSERT_TRUE(count_ops_of_type<opset9::Concat>(f) == 1);
+    {
+        auto data = std::make_shared<op::Parameter>(element::boolean, Shape{1});
+        auto axis1 = op::Constant::create(element::i64, Shape{1}, {0});
+        auto axis2 = op::Constant::create(element::i64, Shape{1}, {0});
+        auto concat = std::make_shared<opset9::Concat>(OutputVector{axis1, axis2}, 0);
+        auto reduce = std::make_shared<opset9::ReduceLogicalOr>(data, concat, true);
+        function_ref = std::make_shared<Function>(OutputVector{reduce}, ParameterVector{data});
+    }
 }
 
-TEST(ReduceMerge, ReduceMax) {
-    std::shared_ptr<Function> f;
+TEST_F(TransformationTestsF, ReduceMergeReduceMax) {
     {
         auto A = std::make_shared<op::Parameter>(element::i64, Shape{1});
         auto reduce1_axes = op::Constant::create(element::i64, Shape{1}, {0});
         auto reduce1 = std::make_shared<opset9::ReduceMax>(A, reduce1_axes, true);
         auto reduce2_axis = op::Constant::create(element::i64, Shape{1}, {0});
-        f = std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceMax>(reduce1, reduce2_axis, true)},
+        function =
+            std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceMax>(reduce1, reduce2_axis, true)},
                                        ParameterVector{A});
+        manager.register_pass<pass::ReduceMerge>();
     }
-    pass::Manager pass_manager;
-    pass_manager.register_pass<pass::ReduceMerge>();
-    pass_manager.run_passes(f);
-    ASSERT_TRUE(count_ops_of_type<opset9::ReduceMax>(f) == 1);
-    ASSERT_TRUE(count_ops_of_type<opset9::Concat>(f) == 1);
+    {
+        auto data = std::make_shared<op::Parameter>(element::i64, Shape{1});
+        auto axis1 = op::Constant::create(element::i64, Shape{1}, {0});
+        auto axis2 = op::Constant::create(element::i64, Shape{1}, {0});
+        auto concat = std::make_shared<opset9::Concat>(OutputVector{axis1, axis2}, 0);
+        auto reduce = std::make_shared<opset9::ReduceMax>(data, concat, true);
+        function_ref = std::make_shared<Function>(OutputVector{reduce}, ParameterVector{data});
+    }
 }
 
-TEST(ReduceMerge, ReduceMean) {
-    std::shared_ptr<Function> f;
+TEST_F(TransformationTestsF, ReduceMergeReduceMean) {
     {
         auto A = std::make_shared<op::Parameter>(element::i64, Shape{1});
         auto reduce1_axes = op::Constant::create(element::i64, Shape{1}, {0});
         auto reduce1 = std::make_shared<opset9::ReduceMean>(A, reduce1_axes, true);
         auto reduce2_axis = op::Constant::create(element::i64, Shape{1}, {0});
-        f = std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceMean>(reduce1, reduce2_axis, true)},
+        function =
+            std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceMean>(reduce1, reduce2_axis, true)},
                                        ParameterVector{A});
+        manager.register_pass<pass::ReduceMerge>();
     }
-    pass::Manager pass_manager;
-    pass_manager.register_pass<pass::ReduceMerge>();
-    pass_manager.run_passes(f);
-    ASSERT_TRUE(count_ops_of_type<opset9::ReduceMean>(f) == 1);
-    ASSERT_TRUE(count_ops_of_type<opset9::Concat>(f) == 1);
+    {
+        auto data = std::make_shared<op::Parameter>(element::i64, Shape{1});
+        auto axis1 = op::Constant::create(element::i64, Shape{1}, {0});
+        auto axis2 = op::Constant::create(element::i64, Shape{1}, {0});
+        auto concat = std::make_shared<opset9::Concat>(OutputVector{axis1, axis2}, 0);
+        auto reduce = std::make_shared<opset9::ReduceMean>(data, concat, true);
+        function_ref = std::make_shared<Function>(OutputVector{reduce}, ParameterVector{data});
+    }
 }
 
-TEST(ReduceMerge, ReduceMin) {
-    std::shared_ptr<Function> f;
+TEST_F(TransformationTestsF, ReduceMergeReduceMin) {
     {
         auto A = std::make_shared<op::Parameter>(element::i64, Shape{1});
         auto reduce1_axes = op::Constant::create(element::i64, Shape{1}, {0});
         auto reduce1 = std::make_shared<opset9::ReduceMin>(A, reduce1_axes, true);
         auto reduce2_axis = op::Constant::create(element::i64, Shape{1}, {0});
-        f = std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceMin>(reduce1, reduce2_axis, true)},
+        function =
+            std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceMin>(reduce1, reduce2_axis, true)},
                                        ParameterVector{A});
+        manager.register_pass<pass::ReduceMerge>();
     }
-    pass::Manager pass_manager;
-    pass_manager.register_pass<pass::ReduceMerge>();
-    pass_manager.run_passes(f);
-    ASSERT_TRUE(count_ops_of_type<opset9::ReduceMin>(f) == 1);
-    ASSERT_TRUE(count_ops_of_type<opset9::Concat>(f) == 1);
+    {
+        auto data = std::make_shared<op::Parameter>(element::i64, Shape{1});
+        auto axis1 = op::Constant::create(element::i64, Shape{1}, {0});
+        auto axis2 = op::Constant::create(element::i64, Shape{1}, {0});
+        auto concat = std::make_shared<opset9::Concat>(OutputVector{axis1, axis2}, 0);
+        auto reduce = std::make_shared<opset9::ReduceMin>(data, concat, true);
+        function_ref = std::make_shared<Function>(OutputVector{reduce}, ParameterVector{data});
+    }
 }
 
-TEST(ReduceMerge, ReduceProd) {
-    std::shared_ptr<Function> f;
+TEST_F(TransformationTestsF, ReduceProd) {
     {
         auto A = std::make_shared<op::Parameter>(element::i64, Shape{1});
         auto reduce1_axes = op::Constant::create(element::i64, Shape{1}, {0});
         auto reduce1 = std::make_shared<opset9::ReduceProd>(A, reduce1_axes, true);
         auto reduce2_axis = op::Constant::create(element::i64, Shape{1}, {0});
-        f = std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceProd>(reduce1, reduce2_axis, true)},
+        function =
+            std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceProd>(reduce1, reduce2_axis, true)},
                                        ParameterVector{A});
+        manager.register_pass<pass::ReduceMerge>();
     }
-    pass::Manager pass_manager;
-    pass_manager.register_pass<pass::ReduceMerge>();
-    pass_manager.run_passes(f);
-    ASSERT_TRUE(count_ops_of_type<opset9::ReduceProd>(f) == 1);
-    ASSERT_TRUE(count_ops_of_type<opset9::Concat>(f) == 1);
+    {
+        auto data = std::make_shared<op::Parameter>(element::i64, Shape{1});
+        auto axis1 = op::Constant::create(element::i64, Shape{1}, {0});
+        auto axis2 = op::Constant::create(element::i64, Shape{1}, {0});
+        auto concat = std::make_shared<opset9::Concat>(OutputVector{axis1, axis2}, 0);
+        auto reduce = std::make_shared<opset9::ReduceProd>(data, concat, true);
+        function_ref = std::make_shared<Function>(OutputVector{reduce}, ParameterVector{data});
+    }
 }
 
-TEST(ReduceMerge, ReduceSum) {
-    std::shared_ptr<Function> f;
+TEST_F(TransformationTestsF, ReduceMergeReduceSum) {
     {
         auto A = std::make_shared<op::Parameter>(element::i64, Shape{1});
         auto reduce1_axes = op::Constant::create(element::i64, Shape{1}, {0});
         auto reduce1 = std::make_shared<opset9::ReduceSum>(A, reduce1_axes, true);
         auto reduce2_axis = op::Constant::create(element::i64, Shape{1}, {0});
-        f = std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceSum>(reduce1, reduce2_axis, true)},
+        function =
+            std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceSum>(reduce1, reduce2_axis, true)},
                                        ParameterVector{A});
+        manager.register_pass<pass::ReduceMerge>();
     }
-    pass::Manager pass_manager;
-    pass_manager.register_pass<pass::ReduceMerge>();
-    pass_manager.run_passes(f);
-    ASSERT_TRUE(count_ops_of_type<opset9::ReduceSum>(f) == 1);
-    ASSERT_TRUE(count_ops_of_type<opset9::Concat>(f) == 1);
+    {
+        auto data = std::make_shared<op::Parameter>(element::i64, Shape{1});
+        auto axis1 = op::Constant::create(element::i64, Shape{1}, {0});
+        auto axis2 = op::Constant::create(element::i64, Shape{1}, {0});
+        auto concat = std::make_shared<opset9::Concat>(OutputVector{axis1, axis2}, 0);
+        auto reduce = std::make_shared<opset9::ReduceSum>(data, concat, true);
+        function_ref = std::make_shared<Function>(OutputVector{reduce}, ParameterVector{data});
+    }
 }
 
-TEST(ReduceMerge, NoReduceDifferentKeepDims) {
-    std::shared_ptr<Function> f;
+TEST_F(TransformationTestsF, ReduceMergeNoReduceDiffKeepDims) {
     {
         auto A = std::make_shared<op::Parameter>(element::i64, Shape{1});
         auto reduce1_axes = op::Constant::create(element::i64, Shape{1}, {0});
         auto reduce1 = std::make_shared<opset9::ReduceL1>(A, reduce1_axes, true);
         auto reduce2_axis = op::Constant::create(element::i64, Shape{1}, {0});
-        f = std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceL1>(reduce1, reduce2_axis, false)},
+        function =
+            std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceL1>(reduce1, reduce2_axis, false)},
                                        ParameterVector{A});
+        manager.register_pass<pass::ReduceMerge>();
     }
-    pass::Manager pass_manager;
-    pass_manager.register_pass<pass::ReduceMerge>();
-    pass_manager.run_passes(f);
-    ASSERT_TRUE(count_ops_of_type<opset9::ReduceL1>(f) == 2);
-    ASSERT_TRUE(count_ops_of_type<opset9::Concat>(f) == 0);
+    {
+        auto data = std::make_shared<op::Parameter>(element::i64, Shape{1});
+        auto reduce1_axes = op::Constant::create(element::i64, Shape{1}, {0});
+        auto reduce1 = std::make_shared<opset9::ReduceL1>(data, reduce1_axes, true);
+        auto reduce2_axis = op::Constant::create(element::i64, Shape{1}, {0});
+        function_ref =
+            std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceL1>(reduce1, reduce2_axis, false)},
+                                       ParameterVector{data});
+    }
 }
 
-TEST(ReduceMerge, NoReduceMergeInvalidAxes) {
-    std::shared_ptr<Function> f;
+TEST_F(TransformationTestsF, ReduceMergeNoReduceMergeInvalidAxes) {
     {
         Shape shape{2, 1};
         auto type = element::f32;
@@ -198,12 +236,18 @@ TEST(ReduceMerge, NoReduceMergeInvalidAxes) {
         auto reduce1_axis = op::Constant::create(element::i64, Shape{1}, {0});
         auto reduce1 = std::make_shared<opset9::ReduceL2>(A, reduce1_axis, false);
         auto reduce2_axis = op::Constant::create(element::i64, Shape{1}, {0});
-        f = std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceL2>(reduce1, reduce2_axis, false)},
+        function =
+            std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceL2>(reduce1, reduce2_axis, false)},
                                        ParameterVector{A});
+        manager.register_pass<pass::ReduceMerge>();
     }
-    pass::Manager pass_manager;
-    pass_manager.register_pass<pass::ReduceMerge>();
-    pass_manager.run_passes(f);
-    ASSERT_TRUE(count_ops_of_type<opset9::ReduceL2>(f) == 2);
-    ASSERT_TRUE(count_ops_of_type<opset9::Concat>(f) == 0);
+    {
+        auto data = std::make_shared<op::Parameter>(element::f32, Shape{2, 1});
+        auto reduce1_axis = op::Constant::create(element::i64, Shape{1}, {0});
+        auto reduce1 = std::make_shared<opset9::ReduceL2>(data, reduce1_axis, false);
+        auto reduce2_axis = op::Constant::create(element::i64, Shape{1}, {0});
+        function_ref =
+            std::make_shared<Function>(OutputVector{std::make_shared<opset9::ReduceL2>(reduce1, reduce2_axis, false)},
+                                       ParameterVector{data});
+    }
 }
