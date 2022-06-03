55a4ed6b33de477dae28d741b5b247a491854606
Artur Kulikowski <artur.kulikowski@intel.com>
2022-06-03 13:38:48 +0200

    std::dynamic_pointer_cast -> ov::as_type_ptr

--- src/common/transformations/src/transformations/common_optimizations/reduce_merge.cpp
+++ src/common/transformations/src/transformations/common_optimizations/reduce_merge.cpp
@@ -25,12 +25,12 @@ std::shared_ptr<Node> create_pattern() {
 
 template <typename T>
 bool fuse_reduce_operations(const std::shared_ptr<Node>& node) {
-    const auto bottom_reduce = std::dynamic_pointer_cast<T>(node);
+    const auto bottom_reduce = as_type_ptr<T>(node);
     if (!bottom_reduce) {
         return false;
     }
 
-    const auto top_reduce = std::dynamic_pointer_cast<T>(bottom_reduce->get_input_node_shared_ptr(0));
+    const auto top_reduce = as_type_ptr<T>(bottom_reduce->get_input_node_shared_ptr(0));
     if (!top_reduce) {
         return false;
     }
