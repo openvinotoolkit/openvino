9ee4a11cc7020b26a4c15905e284cb8b52c60a86
Andrii Voskoboinyk <avoskoboinyk@lohika.com>
2022-06-07 02:02:11 +0300

    Remove my previous changes and add dynamic shapes / repeatable shapes into the correct file

--- src/tests/functional/plugin/cpu/shared_tests_instances/single_layer_tests/nonzero.cpp
+++ src/tests/functional/plugin/cpu/shared_tests_instances/single_layer_tests/nonzero.cpp
@@ -18,6 +18,119 @@ namespace {
         {{}, {{2, 4, 4, 2, 1000}}},
     };
 
+    std::vector<ov::test::InputShape> inShapesDynamic = {
+        {
+            // dynamic
+            {-1},
+            // target
+            {
+                {2},
+                {6},
+                {1},
+                {2}
+            }
+        },
+        {
+            // dynamic
+            {{1, 10}},
+            // target
+            {
+                {2},
+                {6},
+                {4},
+                {6}
+            }
+        },
+        {
+            // dynamic
+            {-1, -1},
+            // target
+            {
+                {4, 24},
+                {8, 32},
+                {4, 24},
+                {16, 48}
+            }
+        },
+        {
+            // dynamic
+            {{1, 50}, -1},
+            // target
+            {
+                {1, 16},
+                {8, 32},
+                {4, 16},
+                {8, 32}
+            }
+        },
+        {
+            // dynamic
+            {-1, -1, -1},
+            // target
+            {
+                {4, 24, 6},
+                {8, 32, 10},
+                {4, 24, 6},
+                {16, 48, 12}
+            }
+        },
+        {
+            // dynamic
+            {-1, {1, 50}, {1, 40}},
+            // target
+            {
+                {1, 16, 8},
+                {8, 32, 14},
+                {4, 16, 10},
+                {8, 32, 14}
+            }
+        },
+        {
+            // dynamic
+            {-1, -1, -1, -1},
+            // target
+            {
+                {4, 24, 6, 4},
+                {8, 32, 10, 2},
+                {4, 24, 6, 4},
+                {16, 48, 12, 2}
+            }
+        },
+        {
+            // dynamic
+            {-1, {1, 50}, {1, 20}, -1},
+            // target
+            {
+                {1, 16, 8, 8},
+                {8, 32, 14, 6},
+                {4, 16, 10, 8},
+                {8, 32, 14, 6},
+            }
+        },
+            {
+            // dynamic
+            {-1, -1, -1, -1, -1},
+            // target
+            {
+                {4, 24, 5, 6, 4},
+                {8, 32, 9, 10, 2},
+                {4, 24, 5, 6, 4},
+                {16, 48, 9, 12, 2}
+            }
+        },
+        {
+            // dynamic
+            {-1, {1, 50}, -1, {1, 30}, -1},
+            // target
+            {
+                {1, 16, 1, 8, 8},
+                {8, 32, 5, 14, 6},
+                {4, 16, 9, 10, 8},
+                {8, 32, 5, 14, 6},
+            }
+        }
+    };
+
     const std::vector<InferenceEngine::Precision> inputPrecisions = {
         InferenceEngine::Precision::I32,
         InferenceEngine::Precision::FP16,
@@ -26,11 +139,19 @@ namespace {
 
     ConfigMap config;
 
-    INSTANTIATE_TEST_SUITE_P(smoke_nonzero, NonZeroLayerTest,
+    INSTANTIATE_TEST_SUITE_P(smoke_nonzeroStatic, NonZeroLayerTest,
                             ::testing::Combine(
                                 ::testing::ValuesIn(inShapes),
                                 ::testing::ValuesIn(inputPrecisions),
                                 ::testing::Values(CommonTestUtils::DEVICE_CPU),
                                 ::testing::Values(config)),
                             NonZeroLayerTest::getTestCaseName);
-} // namespace
+
+    INSTANTIATE_TEST_SUITE_P(smoke_nonzeroDynamic, NonZeroLayerTest,
+                            ::testing::Combine(
+                                ::testing::ValuesIn(inShapesDynamic),
+                                ::testing::ValuesIn(inputPrecisions),
+                                ::testing::Values(CommonTestUtils::DEVICE_CPU),
+                                ::testing::Values(config)),
+                            NonZeroLayerTest::getTestCaseName);
+    } // namespace
