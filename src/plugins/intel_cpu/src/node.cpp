12704c82e41c14c82c3d66ec56a0368730538ad0
Luo Cheng <cheng.luo@intel.com>
2022-06-07 15:59:40 +0800

    disable avx512 brgconv

--- src/plugins/intel_cpu/src/node.cpp
+++ src/plugins/intel_cpu/src/node.cpp
@@ -875,8 +875,9 @@ const std::vector<impl_desc_type>& Node::getPrimitivesPriority() {
             impl_desc_type::jit_avx512_amx_dw,
             impl_desc_type::jit_avx512_amx_1x1,
             impl_desc_type::jit_avx512_amx,
-            impl_desc_type::brgconv_avx512_1x1,
-            impl_desc_type::brgconv_avx512,
+            // Brgconv kernels disabled in order to prevent perf degradations on non AMX HW
+            // impl_desc_type::brgconv_avx512_1x1,
+            // impl_desc_type::brgconv_avx512,
             impl_desc_type::jit_uni_dw,
             impl_desc_type::jit_uni_1x1,
             impl_desc_type::jit_uni,
