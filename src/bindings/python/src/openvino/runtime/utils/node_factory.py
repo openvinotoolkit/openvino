ee8c455345cba4e560a22e2d8b1193657e62caa0
bszmelcz <bartosz.szmelczynski@intel.com>
2022-06-09 17:50:30 +0200

    revert questionable changes

--- src/bindings/python/src/openvino/runtime/utils/node_factory.py
+++ src/bindings/python/src/openvino/runtime/utils/node_factory.py
@@ -26,6 +26,72 @@ class NodeFactory(object):
         """
         self.factory = _NodeFactory(opset_version)
 
+    def create(
+        self,
+        op_type_name: str,
+        arguments: Optional[List[Union[Node, Output]]] = None,
+        attributes: Optional[Dict[str, Any]] = None,
+    ) -> Node:
+        """Create node object from provided description.
+
+        The user does not have to provide all node's attributes, but only required ones.
+
+        :param      op_type_name:  The operator type name.
+        :param      arguments:     The operator arguments.
+        :param      attributes:    The operator attributes.
+
+        :return:   Node object representing requested operator with attributes set.
+        """
+        if arguments is None and attributes is None:
+            node = self.factory.create(op_type_name)
+            node._attr_cache = {}
+            node._attr_cache_valid = False
+            return node
+
+        if arguments is None and attributes is not None:
+            raise UserInputError(f'Error: cannot create "{op_type_name}" op without arguments.')
+
+        if attributes is None:
+            attributes = {}
+
+        assert arguments is not None
+
+        arguments = self._arguments_as_outputs(arguments)
+        node = self.factory.create(op_type_name, arguments, attributes)
+
+        # Currently we don't support any attribute getters & setters for TensorIterator node.
+        if node.get_type_name() == "TensorIterator":
+            return node
+
+        """Set getters and setters for each node's attribute.
+            node.get_attribute_name()
+            node.set_attribute_name()
+           For compound (with more than one level of nesting) attributes of form ie.:
+           node.class_member_name.some_metric.attr_name:
+            node.get_some_metric_attr_name()
+            node.set_some_metric_attr_name()
+           Please see test_dyn_attributes.py for more usage examples.
+        """
+        all_attributes = node.get_attributes()
+        for attr_name in all_attributes.keys():
+            setattr(
+                node,
+                self._normalize_attr_name_getter(attr_name),
+                partial(NodeFactory._get_node_attr_value, node, attr_name),
+            )
+            setattr(
+                node,
+                self._normalize_attr_name_setter(attr_name),
+                partial(NodeFactory._set_node_attr_value, node, attr_name),
+            )
+
+        # Setup helper members for caching attribute values.
+        # The cache would be lazily populated at first access attempt.
+        node._attr_cache = {}
+        node._attr_cache_valid = False
+
+        return node
+
     @staticmethod
     def _arguments_as_outputs(arguments: List[Union[Node, Output]]) -> List[Output]:
         outputs = []
@@ -94,69 +160,3 @@ class NodeFactory(object):
         :return:   The appropriate setter function name.
         """
         return cls._normalize_attr_name(attr_name, "set_")
-
-    def create(
-        self,
-        op_type_name: str,
-        arguments: Optional[List[Union[Node, Output]]] = None,
-        attributes: Optional[Dict[str, Any]] = None,
-    ) -> Node:
-        """Create node object from provided description.
-
-        The user does not have to provide all node's attributes, but only required ones.
-
-        :param      op_type_name:  The operator type name.
-        :param      arguments:     The operator arguments.
-        :param      attributes:    The operator attributes.
-
-        :return:   Node object representing requested operator with attributes set.
-        """
-        if arguments is None and attributes is None:
-            node = self.factory.create(op_type_name)
-            node._attr_cache = {}
-            node._attr_cache_valid = False
-            return node
-
-        if arguments is None and attributes is not None:
-            raise UserInputError(f'Error: cannot create "{op_type_name}" op without arguments.')
-
-        if attributes is None:
-            attributes = {}
-
-        assert arguments is not None
-
-        arguments = self._arguments_as_outputs(arguments)
-        node = self.factory.create(op_type_name, arguments, attributes)
-
-        # Currently we don't support any attribute getters & setters for TensorIterator node.
-        if node.get_type_name() == "TensorIterator":
-            return node
-
-        """Set getters and setters for each node's attribute.
-            node.get_attribute_name()
-            node.set_attribute_name()
-           For compound (with more than one level of nesting) attributes of form ie.:
-           node.class_member_name.some_metric.attr_name:
-            node.get_some_metric_attr_name()
-            node.set_some_metric_attr_name()
-           Please see test_dyn_attributes.py for more usage examples.
-        """
-        all_attributes = node.get_attributes()
-        for attr_name in all_attributes.keys():
-            setattr(
-                node,
-                self._normalize_attr_name_getter(attr_name),
-                partial(NodeFactory._get_node_attr_value, node, attr_name),
-            )
-            setattr(
-                node,
-                self._normalize_attr_name_setter(attr_name),
-                partial(NodeFactory._set_node_attr_value, node, attr_name),
-            )
-
-        # Setup helper members for caching attribute values.
-        # The cache would be lazily populated at first access attempt.
-        node._attr_cache = {}
-        node._attr_cache_valid = False
-
-        return node
