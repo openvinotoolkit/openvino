5f4458ffd879baf02622dd64ba1efc027f15b40f
Sungeun Kim <sungeun.kim@intel.com>
2022-06-14 22:04:29 +0900

    add zyx_fsv32 format for onednn deconv (#11873)

--- src/plugins/intel_gpu/src/graph/impls/onednn/deconvolution_onednn.cpp
+++ src/plugins/intel_gpu/src/graph/impls/onednn/deconvolution_onednn.cpp
@@ -183,6 +183,11 @@ attach_deconvolution_onednn::attach_deconvolution_onednn() {
         std::make_tuple(data_types::u8, format::b_fs_yx_fsv32),
         std::make_tuple(data_types::i8, format::b_fs_yx_fsv32),
 
+        std::make_tuple(data_types::f32, format::b_fs_zyx_fsv32),
+        std::make_tuple(data_types::f16, format::b_fs_zyx_fsv32),
+        std::make_tuple(data_types::u8, format::b_fs_zyx_fsv32),
+        std::make_tuple(data_types::i8, format::b_fs_zyx_fsv32),
+
         std::make_tuple(data_types::f32, format::bs_fs_yx_bsv16_fsv16),
         std::make_tuple(data_types::f16, format::bs_fs_yx_bsv16_fsv16),
         std::make_tuple(data_types::u8, format::bs_fs_yx_bsv16_fsv16),
