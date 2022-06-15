da8a7e4fbd3ade2baee13f3f1b371815bd0beb9b
fishbell <bell.song@intel.com>
2022-06-15 09:48:10 -0400

    fix bug
    Signed-off-by: fishbell <bell.song@intel.com>
    
    typo
    
    Signed-off-by: fishbell <bell.song@intel.com>

--- src/plugins/auto/infer_request.cpp
+++ src/plugins/auto/infer_request.cpp
@@ -88,10 +88,37 @@ void MultiDeviceInferRequest::SetBlobsToAnotherRequest(const SoIInferRequestInte
     }
 }
 
+void MultiDeviceInferRequest::SetBlob(const std::string& name, const InferenceEngine::Blob::Ptr& blob) {
+    if (_sharedRequest)
+        _sharedRequest->SetBlob(name, blob);
+    else
+        IInferRequestInternal::SetBlob(name, blob);
+}
+
+void MultiDeviceInferRequest::SetBlob(const std::string& name, const Blob::Ptr& blob, const PreProcessInfo& info) {
+    if (_sharedRequest)
+        _sharedRequest->SetBlob(name, blob, info);
+    else
+        IInferRequestInternal::SetBlob(name, blob, info);
+}
+
+InferenceEngine::Blob::Ptr MultiDeviceInferRequest::GetBlob(const std::string& name) {
+    if (_sharedRequest)
+        return _sharedRequest->GetBlob(name);
+    else
+        return IInferRequestInternal::GetBlob(name);
+}
+
 std::map<std::string, InferenceEngine::InferenceEngineProfileInfo> MultiDeviceInferRequest::GetPerformanceCounts() const {
     if (_sharedRequest)
         return _sharedRequest->GetPerformanceCounts();
     return _perfMap;
+}
+
+std::vector<std::shared_ptr<InferenceEngine::IVariableStateInternal>> MultiDeviceInferRequest::QueryState() {
+    if (_sharedRequest)
+        return _sharedRequest->QueryState();
+    IE_THROW(NotImplemented);
 }
 
 void MultiDeviceInferRequest::InferImpl() {
