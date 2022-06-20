afcec969da23a554ada0b8262c6a03e397d023ec
Dariusz Trawinski <dariusz.trawinski@intel.com>
2022-06-20 14:35:00 +0200

    adjust RH8 dependencies

--- scripts/install_dependencies/install_openvino_dependencies.sh
+++ scripts/install_dependencies/install_openvino_dependencies.sh
@@ -145,12 +145,7 @@ elif [ "$os" == "rhel8" ] ; then
     pkgs_python=(python3 python3-devel python3-setuptools python3-pip)
     pkgs_dev=(gcc gcc-c++ make glibc libstdc++ libgcc cmake pkg-config gflags-devel.i686 zlib-devel.i686 curl sudo)
     pkgs_myriad=()
-    pkgs_opencv_opt=(
-        gstreamer1
-        gstreamer1-plugins-bad-free
-        gstreamer1-plugins-good
-        gstreamer1-plugins-ugly-free
-    )
+    pkgs_opencv_opt=()
     extra_repos+=(https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm)
 
 else
