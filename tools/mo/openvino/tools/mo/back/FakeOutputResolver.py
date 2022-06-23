103e04b6b586d7569ba513ebd87c76da85841273
Artur Kulikowski <artur.kulikowski@intel.com>
2022-06-23 10:05:06 +0200

    Fixed FakeOutputResolver to avoid renaming correctly named nodes (#11938)
    * fixed FakeOutputResolver to avoid renaming correctly named nodes
    
    * fixed failed mo_args test: process reverse_input_channels through eltwise with constant with shape=[]
    
    * changed fix to more accuarate to avoid possible issues
    
    * Remove unnecessary iterating over producer outputs
    
    Co-authored-by: sadolini <svetlana.a.dolinina@intel.com>

--- tools/mo/openvino/tools/mo/back/FakeOutputResolver.py
+++ tools/mo/openvino/tools/mo/back/FakeOutputResolver.py
@@ -21,32 +21,22 @@ class FakeOutputResolver(BackReplacementPattern):
             name = fake_output.soft_get('name', fake_output.id)
 
             producer = fake_output.in_port(0).get_source().node
-            producer_outputs = 0
-            for port in producer.out_ports().values():
-                if not port.disconnected():
-                    producer_outputs += 1
-            if producer_outputs != 1:
-                # At this stage we don't know the type of output, so we rely on MO transformation which updates the
-                # Const type for elementwise operations in case of input data types mismatch
-                add = create_op_with_const_inputs(graph, Add, {1: int64_array(0)}, {'can_be_fused': False})
-                rename_nodes([(fake_output, name + '/TBD'), (add, name)])
 
-                prev_op_in_port = fake_output.in_port(0).get_connection().get_source()
-                # Get tensor names incoming to FakeOutput
-                tensor_names = prev_op_in_port.get_tensor_names()
+            # At this stage we don't know the type of output, so we rely on MO transformation which updates the
+            # Const type for elementwise operations in case of input data types mismatch
+            add = create_op_with_const_inputs(graph, Add, {1: int64_array(0)}, {'can_be_fused': False})
+            rename_nodes([(fake_output, name + '/TBD'), (add, name)])
 
-                # Remove tensor info from data node
-                prev_op_in_port.remove_tensor_names()
+            prev_op_in_port = fake_output.in_port(0).get_connection().get_source()
+            # Get tensor names incoming to FakeOutput
+            tensor_names = prev_op_in_port.get_tensor_names()
 
-                fake_output.in_port(0).get_connection().set_destination(add.in_port(0))
-                fake_output.out_port(0).get_connection().set_source(add.out_port(0))
+            # Remove tensor info from data node
+            prev_op_in_port.remove_tensor_names()
 
-                # Move tensor names to Add op, which replaces FakeOutput
-                if len(tensor_names) > 0:
-                    add.out_port(0).add_tensor_names(tensor_names)
+            fake_output.in_port(0).get_connection().set_destination(add.in_port(0))
+            fake_output.out_port(0).get_connection().set_source(add.out_port(0))
 
-            else:
-                result_in_port = fake_output.out_port(0).get_destination()
-                result_in_port.disconnect()
-                fake_output.in_port(0).get_connection().set_destination(result_in_port)
-                rename_nodes([(fake_output, name + '/TBD'), (producer, name)])
+            # Move tensor names to Add op, which replaces FakeOutput
+            if len(tensor_names) > 0:
+                add.out_port(0).add_tensor_names(tensor_names)
