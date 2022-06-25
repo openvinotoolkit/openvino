1070a17fdc5530031fb6f54705dc67d4f62f7997
guozhong wang <guozhong.wang@intel.com>
2022-06-25 16:37:43 +0800

    add testcase for plugin properties should not be revised by compile_m… (#11842)
    * add testcase for plugin properties should not be revised by compile_model
    
    * rename smoke_cpuCompileModelBehaviorTests to smoke_gpuCompileModelBehaviorTests
    
    * remove property EXCLUSIVE_ASYNC_REQUESTS in ov2.0 test
    
    * add testcase for plugin properties should not be revised by loadNetwork

--- src/tests/functional/plugin/gpu/shared_tests_instances/behavior/ov_plugin/properties_tests.cpp
+++ src/tests/functional/plugin/gpu/shared_tests_instances/behavior/ov_plugin/properties_tests.cpp
@@ -36,4 +36,41 @@ INSTANTIATE_TEST_SUITE_P(smoke_AutoMultiBehaviorTests, OVPropertiesTests,
                 ::testing::Values(CommonTestUtils::DEVICE_AUTO, CommonTestUtils::DEVICE_MULTI),
                 ::testing::ValuesIn(auto_multi_properties)),
         OVPropertiesTests::getTestCaseName);
+
+const std::vector<ov::AnyMap> gpu_plugin_properties = {
+    {ov::hint::performance_mode(ov::hint::PerformanceMode::THROUGHPUT),
+     ov::hint::num_requests(2),
+     ov::enable_profiling(false)}};
+const std::vector<ov::AnyMap> gpu_compileModel_properties = {
+    {ov::hint::performance_mode(ov::hint::PerformanceMode::LATENCY),
+     ov::hint::num_requests(10),
+     ov::enable_profiling(true)}};
+
+INSTANTIATE_TEST_SUITE_P(smoke_gpuCompileModelBehaviorTests,
+                         OVSetPropComplieModleGetPropTests,
+                         ::testing::Combine(::testing::Values(CommonTestUtils::DEVICE_GPU),
+                                            ::testing::ValuesIn(gpu_plugin_properties),
+                                            ::testing::ValuesIn(gpu_compileModel_properties)),
+                         OVSetPropComplieModleGetPropTests::getTestCaseName);
+
+const std::vector<ov::AnyMap> auto_multi_plugin_properties = {
+    {ov::device::priorities(CommonTestUtils::DEVICE_GPU),
+     ov::hint::performance_mode(ov::hint::PerformanceMode::THROUGHPUT),
+     ov::hint::num_requests(2),
+     ov::hint::allow_auto_batching(false),
+     ov::enable_profiling(false)}};
+const std::vector<ov::AnyMap> auto_multi_compileModel_properties = {
+    {ov::device::priorities(CommonTestUtils::DEVICE_GPU),
+     ov::hint::performance_mode(ov::hint::PerformanceMode::LATENCY),
+     ov::hint::num_requests(10),
+     ov::hint::allow_auto_batching(true),
+     ov::enable_profiling(true)}};
+
+INSTANTIATE_TEST_SUITE_P(smoke_AutoMultiCompileModelBehaviorTests,
+                         OVSetPropComplieModleGetPropTests,
+                         ::testing::Combine(::testing::Values(CommonTestUtils::DEVICE_AUTO,
+                                                              CommonTestUtils::DEVICE_MULTI),
+                                            ::testing::ValuesIn(auto_multi_plugin_properties),
+                                            ::testing::ValuesIn(auto_multi_compileModel_properties)),
+                         OVSetPropComplieModleGetPropTests::getTestCaseName);
 } // namespace
