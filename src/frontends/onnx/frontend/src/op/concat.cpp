3aa8a21701832f3891e68c4efd178a6c239c38f5
mitruska <katarzyna.mitrus@intel.com>
2022-06-27 18:24:18 +0200

    Use is_filsafe_node to check empty tensors

--- src/frontends/onnx/frontend/src/op/concat.cpp
+++ src/frontends/onnx/frontend/src/op/concat.cpp
@@ -2,14 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "ngraph/op/concat.hpp"
+#include "op/concat.hpp"
 
 #include <cstdint>
 
 #include "default_opset.hpp"
-#include "exceptions.hpp"
-#include "ngraph/validation_util.hpp"
-#include "op/concat.hpp"
+#include "utils/common.hpp"
 
 namespace ngraph {
 namespace onnx_import {
@@ -20,7 +18,7 @@ OutputVector concat(const Node& node) {
     std::int64_t axis = node.get_attribute_value<std::int64_t>("axis");
     OutputVector valid_inputs;
     std::copy_if(inputs.begin(), inputs.end(), std::back_inserter(valid_inputs), [](ov::Output<ov::Node>& in) -> bool {
-        return !(in.get_partial_shape().same_scheme(PartialShape(Shape{})));
+        return !common::is_failsafe_node(in.get_node_shared_ptr());
     });
     return {std::make_shared<default_opset::Concat>(valid_inputs, axis)};
 }
