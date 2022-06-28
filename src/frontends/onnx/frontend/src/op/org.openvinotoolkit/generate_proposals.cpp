a65d4b1ca2d47a3ef011151e929552957902a2a6
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2022-06-28 21:48:42 +0200

    Create ONNX FrontEnd GenerateProposals op

---
+++ src/frontends/onnx/frontend/src/op/org.openvinotoolkit/generate_proposals.cpp
@@ -0,0 +1,45 @@
+// Copyright (C) 2022 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "generate_proposals.hpp"
+
+#include "default_opset.hpp"
+
+namespace ngraph {
+namespace onnx_import {
+namespace op {
+namespace set_1 {
+OutputVector generate_proposals(const Node& node) {
+    const auto inputs = node.get_ng_inputs();
+    OPENVINO_ASSERT(inputs.size() == 4, "GenerateProposals operator expects 4 inputs, got ", inputs.size());
+
+    const auto& scores = inputs[0];
+    const auto& deltas = inputs[1];
+    const auto& im_info = inputs[2];
+    const auto& anchors = inputs[3];
+
+    ov::op::v9::GenerateProposals::Attributes attrs;
+    attrs.min_size = node.get_attribute_value<float>("min_size", 1.f);
+    attrs.nms_threshold = node.get_attribute_value<float>("nms_thresh", 0.7f);
+    attrs.pre_nms_count = node.get_attribute_value<int64_t>("pre_nms_topN", 6000);
+    attrs.post_nms_count = node.get_attribute_value<int64_t>("post_nms_topN", 300);
+    attrs.normalized = !node.get_attribute_value<int64_t>("legacy_plus_one", true);
+
+    const auto zero = default_opset::Constant::create(element::i64, Shape{1}, {0});
+    const auto scores_shape = std::make_shared<default_opset::ShapeOf>(scores);
+    const auto anchors_shape = std::make_shared<default_opset::ShapeOf>(anchors);
+    const auto scores_shape_tail = default_opset::Constant::create(element::i64, Shape{2}, {2, 3});
+    const auto new_anchors_shape_front = std::make_shared<default_opset::Gather>(scores_shape, scores_shape_tail, zero);
+    const auto new_anchors_shape =
+        std::make_shared<default_opset::Concat>(OutputVector{new_anchors_shape_front, anchors_shape}, 0);
+    const auto new_anchors = std::make_shared<default_opset::Broadcast>(anchors, new_anchors_shape);
+
+    const auto gs = std::make_shared<ov::op::v9::GenerateProposals>(im_info, new_anchors, deltas, scores, attrs);
+
+    return {gs->output(0), gs->output(1)};
+}
+}  // namespace set_1
+}  // namespace op
+}  // namespace onnx_import
+}  // namespace ngraph
