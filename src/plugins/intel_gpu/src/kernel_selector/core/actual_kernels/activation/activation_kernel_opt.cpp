201684f059143816181e452ad7e5ae98921383a9
Felix Dohyun Kim <tuxedcat@gmail.com>
2022-06-30 14:20:40 +0900

    Make opt kernel support bfwzyx

--- src/plugins/intel_gpu/src/kernel_selector/core/actual_kernels/activation/activation_kernel_opt.cpp
+++ src/plugins/intel_gpu/src/kernel_selector/core/actual_kernels/activation/activation_kernel_opt.cpp
@@ -110,11 +110,20 @@ JitConstants ActivationKernelOpt::GetJitConstants(const activation_params& param
                              "x / OUTPUT_SIZE_X % OUTPUT_SIZE_Y",
                              "x % OUTPUT_SIZE_X"};
             } else if (params.inputs[0].GetDims().size() == 5) {
-                idx_order = {"x / (OUTPUT_SIZE_X * OUTPUT_SIZE_Y * OUTPUT_SIZE_Z* OUTPUT_FEATURE_NUM)",
+                idx_order = {"x / (OUTPUT_SIZE_X * OUTPUT_SIZE_Y * OUTPUT_SIZE_Z * OUTPUT_FEATURE_NUM)",
                              "x / (OUTPUT_SIZE_X * OUTPUT_SIZE_Y * OUTPUT_SIZE_Z) % OUTPUT_FEATURE_NUM",
                              "x / (OUTPUT_SIZE_X * OUTPUT_SIZE_Y) % OUTPUT_SIZE_Z",
                              "x / OUTPUT_SIZE_X % OUTPUT_SIZE_Y",
                              "x % OUTPUT_SIZE_X"};
+            } else if (params.inputs[0].GetDims().size() == 6) {
+                idx_order = {"x / (OUTPUT_SIZE_X * OUTPUT_SIZE_Y * OUTPUT_SIZE_Z * OUTPUT_SIZE_W * OUTPUT_FEATURE_NUM)",
+                             "x / (OUTPUT_SIZE_X * OUTPUT_SIZE_Y * OUTPUT_SIZE_Z * OUTPUT_SIZE_W) % OUTPUT_FEATURE_NUM",
+                             "x / (OUTPUT_SIZE_X * OUTPUT_SIZE_Y * OUTPUT_SIZE_Z) % OUTPUT_SIZE_W",
+                             "x / (OUTPUT_SIZE_X * OUTPUT_SIZE_Y) % OUTPUT_SIZE_Z",
+                             "x / OUTPUT_SIZE_X % OUTPUT_SIZE_Y",
+                             "x % OUTPUT_SIZE_X"};
+            } else {
+                IE_THROW() << "Unknown dimension";
             }
         } else {
             if (params.inputs[0].GetDims().size() <= 4) {
@@ -123,11 +132,20 @@ JitConstants ActivationKernelOpt::GetJitConstants(const activation_params& param
                              "(x + i) / OUTPUT_SIZE_X % OUTPUT_SIZE_Y",
                              "(x + i) % OUTPUT_SIZE_X"};
             } else if (params.inputs[0].GetDims().size() == 5) {
-                idx_order = {"(x + i) / (OUTPUT_SIZE_X * OUTPUT_SIZE_Y * OUTPUT_SIZE_Z* OUTPUT_FEATURE_NUM)",
+                idx_order = {"(x + i) / (OUTPUT_SIZE_X * OUTPUT_SIZE_Y * OUTPUT_SIZE_Z * OUTPUT_FEATURE_NUM)",
                              "(x + i) / (OUTPUT_SIZE_X * OUTPUT_SIZE_Y * OUTPUT_SIZE_Z) % OUTPUT_FEATURE_NUM",
                              "(x + i) / (OUTPUT_SIZE_X * OUTPUT_SIZE_Y) % OUTPUT_SIZE_Z",
                              "(x + i) / OUTPUT_SIZE_X % OUTPUT_SIZE_Y",
                              "(x + i) % OUTPUT_SIZE_X"};
+            } else if (params.inputs[0].GetDims().size() == 6) {
+                idx_order = {"(x + i) / (OUTPUT_SIZE_X * OUTPUT_SIZE_Y * OUTPUT_SIZE_Z * OUTPUT_SIZE_W * OUTPUT_FEATURE_NUM)",
+                             "(x + i) / (OUTPUT_SIZE_X * OUTPUT_SIZE_Y * OUTPUT_SIZE_Z * OUTPUT_SIZE_W) % OUTPUT_FEATURE_NUM",
+                             "(x + i) / (OUTPUT_SIZE_X * OUTPUT_SIZE_Y * OUTPUT_SIZE_Z) % OUTPUT_SIZE_W",
+                             "(x + i) / (OUTPUT_SIZE_X * OUTPUT_SIZE_Y) % OUTPUT_SIZE_Z",
+                             "(x + i) / OUTPUT_SIZE_X % OUTPUT_SIZE_Y",
+                             "(x + i) % OUTPUT_SIZE_X"};
+            } else {
+                IE_THROW() << "Unknown dimension";
             }
         }
         FusedOpsConfiguration conf_vector = {"_VECTOR",
