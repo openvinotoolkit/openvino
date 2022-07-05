609029e46072cf1bf3fd6388d8aca44afb3efe0f
Tingqian Li <tingqian.li@intel.com>
2022-07-05 15:50:35 +0800

    [Transformation] transform ConvertLike into Convert when applicable (#7543)
    * [Transformation] transform ConvertLike into Convert when applicable
    
    Signed-off-by: Li, Tingqian <tingqian.li@intel.com>
    
    * Remove xfail markup for onnx castlike cpu tests
    
    Signed-off-by: Li, Tingqian <tingqian.li@intel.com>
    
    * remove un-used xfail
    
    Signed-off-by: Li, Tingqian <tingqian.li@intel.com>
    
    * Enable ConvertLike cpu functional test
    
    Signed-off-by: Li, Tingqian <tingqian.li@intel.com>
    
    * remove unnecessary headers
    
    * move to common transformation
    
    Signed-off-by: Li, Tingqian <tingqian.li@intel.com>
    
    * remove test
    
    * fix pytest CI issue
    
    * minor fix after rebase
    
    * fix clang format check
    
    * fix failed tests

--- src/bindings/python/tests_compatibility/__init__.py
+++ src/bindings/python/tests_compatibility/__init__.py
@@ -130,7 +130,6 @@ xfail_issue_63043 = xfail_test(reason="Recurrent node expects constants as W, R,
 xfail_issue_63044 = xfail_test(reason="ONNX opset 14 operation: Trilu")
 
 skip_rng_tests = pytest.mark.skip(reason="Tests use random number generator with no seed.")
-xfail_issue_63136 = xfail_test(reason="Unsupported operation: CastLike")
 xfail_issue_63137 = xfail_test(reason="Unsupported operations: OptionalHasElement, OptionalGetElement")
 xfail_issue_63138 = xfail_test(reason="Missing ONNX Shape-15 support")
 
