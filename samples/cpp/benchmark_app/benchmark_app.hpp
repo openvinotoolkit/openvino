0f105d71271ffce1c92a31f416e27264c3b7de49
Chenhu Wang <chenhu.wang@intel.com>
2022-07-05 15:50:16 +0800

    [CPU] General denormals optimization (#11883)
    * FTZ_and_DAZ_set_for_cpu
    
    * remove DAZ
    
    * fix
    
    * extract to utils
    
    * ie core part changes to add do as property and benchmark_app enable do
    
    * enable brgcov from Luocheng patch
    
    * add debug info
    
    * enable_brgemm_on_avx512
    
    * add python binding
    
    * dlb test
    
    * FTZ_and_DAZ_set_for_cpu
    
    * remove DAZ
    
    * fix
    
    * extract to utils
    
    * ie core part changes to add do as property and benchmark_app enable do
    
    * enable brgcov from Luocheng patch
    
    * add debug info
    
    * enable_brgemm_on_avx512
    
    * add python binding
    
    * dlb test
    
    * revert test code
    
    * revert test code

--- samples/cpp/benchmark_app/benchmark_app.hpp
+++ samples/cpp/benchmark_app/benchmark_app.hpp
@@ -230,6 +230,8 @@ static constexpr char inference_only_message[] =
     " To enable full mode for static models pass \"false\" value to this argument:"
     " ex. \"-inference_only=false\".\n";
 
+static const char denormals_optimization_message[] = "Optional. Denormals is optimized by treating as zero";
+
 static constexpr char cpu_experimental_message[] =
     "Optional. Enable experimental setting for CPU plugin. Setting should be 'brgconv' etc.";
 
@@ -366,6 +368,9 @@ DEFINE_string(imean, "", input_image_mean_message);
 /// @brief Define flag for inference only mode <br>
 DEFINE_bool(inference_only, true, inference_only_message);
 
+/// @brief Define flag for denormals handling mode <br>
+DEFINE_bool(dopt, false, denormals_optimization_message);
+
 /// @brief Define flag for using experimental setting for CPU plugin <br>
 DEFINE_string(cpu_experimental, "", cpu_experimental_message);
 
@@ -408,7 +413,7 @@ static void show_usage() {
     std::cout << "    -use_device_mem           " << use_device_mem_message << std::endl;
 #endif
     std::cout << std::endl << "  Statistics dumping options:" << std::endl;
-    std::cout << "    -report_type \"<type>\"     " << report_type_message << std::endl;
+    std::cout << "    -report_type \"<type>\"   " << report_type_message << std::endl;
     std::cout << "    -report_folder            " << report_folder_message << std::endl;
     std::cout << "    -json_stats               " << json_stats_message << std::endl;
     std::cout << "    -exec_graph_path          " << exec_graph_path_message << std::endl;
@@ -417,10 +422,11 @@ static void show_usage() {
     std::cout << "    -dump_config              " << dump_config_message << std::endl;
     std::cout << "    -load_config              " << load_config_message << std::endl;
     std::cout << "    -infer_precision \"<element type>\"" << inference_precision_message << std::endl;
-    std::cout << "    -ip                          <value>     " << inputs_precision_message << std::endl;
-    std::cout << "    -op                          <value>     " << outputs_precision_message << std::endl;
-    std::cout << "    -iop                        \"<value>\"    " << iop_message << std::endl;
-    std::cout << "    -iscale                    " << input_image_scale_message << std::endl;
-    std::cout << "    -imean                     " << input_image_mean_message << std::endl;
-    std::cout << "    -inference_only              " << inference_only_message << std::endl;
+    std::cout << "    -ip                       <value>     " << inputs_precision_message << std::endl;
+    std::cout << "    -op                       <value>     " << outputs_precision_message << std::endl;
+    std::cout << "    -iop                      \"<value>\"    " << iop_message << std::endl;
+    std::cout << "    -iscale                   " << input_image_scale_message << std::endl;
+    std::cout << "    -imean                    " << input_image_mean_message << std::endl;
+    std::cout << "    -inference_only           " << inference_only_message << std::endl;
+    std::cout << "    -dopt                     " << denormals_optimization_message << std::endl;
 }
