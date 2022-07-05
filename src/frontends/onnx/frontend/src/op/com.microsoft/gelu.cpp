c9eba19c0e94a25eeae78036413bf90ec9bbdec7
MyGInt <73644272+MyGInt@users.noreply.github.com>
2022-07-05 17:45:14 +0800

    Update gelu.cpp

--- src/frontends/onnx/frontend/src/op/com.microsoft/gelu.cpp
+++ src/frontends/onnx/frontend/src/op/com.microsoft/gelu.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "default_opset.hpp"
+#include "op/com.microsoft/bias_gelu.hpp"
 
 namespace ngraph {
 namespace onnx_import {
@@ -10,7 +11,7 @@ namespace op {
 namespace set_1 {
 OutputVector gelu(const Node& node) {
     auto nodes = node.get_ng_inputs();
-    NGRAPH_CHECK(nodes.size() == 1, "Gelu takes 1 input. Provided： " + std::to_string(nodes.size()));
+    NGRAPH_CHECK(nodes.size() == 1, "BiasGelu takes 1 input. Provided： " + std::to_string(nodes.size()));
     return {std::make_shared<default_opset::Gelu>(nodes.at(0))};
 }
 }  // namespace set_1
