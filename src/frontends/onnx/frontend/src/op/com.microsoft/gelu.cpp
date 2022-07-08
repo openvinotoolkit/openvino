271287f8b97bb9716bf1c11b05a9cdc70ef338db
MyGInt <73644272+MyGInt@users.noreply.github.com>
2022-07-08 16:44:03 +0800

    Update gelu.cpp

--- src/frontends/onnx/frontend/src/op/com.microsoft/gelu.cpp
+++ src/frontends/onnx/frontend/src/op/com.microsoft/gelu.cpp
@@ -3,19 +3,19 @@
 //
 
 #include "op/com.microsoft/gelu.hpp"
+
 #include "default_opset.hpp"
 
 namespace ngraph {
 namespace onnx_import {
 namespace op {
 namespace set_1 {
-OutputVector gelu(const Node &node) {
-  auto nodes = node.get_ng_inputs();
-  NGRAPH_CHECK(nodes.size() == 1,
-               "Gelu takes 1 input. Provided:" + std::to_string(nodes.size()));
-  return {std::make_shared<default_opset::Gelu>(nodes.at(0))};
+OutputVector gelu(const Node& node) {
+    auto nodes = node.get_ng_inputs();
+    NGRAPH_CHECK(nodes.size() == 1, "Gelu takes 1 input. Provided: " + std::to_string(nodes.size()));
+    return {std::make_shared<default_opset::Gelu>(nodes.at(0))};
 }
-} // namespace set_1
-} // namespace op
-} // namespace onnx_import
-} // namespace ngraph
+}  // namespace set_1
+}  // namespace op
+}  // namespace onnx_import
+}  // namespace ngraph
