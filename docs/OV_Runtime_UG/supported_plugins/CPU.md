d7a90cf7cc168a1aa634d08b1d5415376bb4c28a
chenhuwa <chenhu.wang@intel.com>
2022-07-12 23:18:49 +0800

    doc update

--- docs/OV_Runtime_UG/supported_plugins/CPU.md
+++ docs/OV_Runtime_UG/supported_plugins/CPU.md
@@ -231,6 +231,7 @@ All parameters must be set before calling `ov::Core::compile_model()` in order t
 - `ov::num_streams`
 - `ov::affinity`
 - `ov::inference_num_threads`
+- `ov::denormals_optimization`
 
 
 ### Read-only properties
---
+++ docs/snippets/ov_denormals.cpp
@@ -0,0 +1,25 @@
+// Copyright (C) 2022 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+#include <openvino/runtime/core.hpp>
+
+int main() {
+    try {
+        std::string modelPath = "modelWithDenormals.xml";
+        std::string device = "CPU";
+        ov::AnyMap config;
+        //! [ov:denormals_optimization:part0]
+        ov::Core core;                                              // Step 1: create ov::Core object
+        core.set_property(ov::denormals_optimization("YES"));       // Step 1b: Enable denormals optimization
+        auto model = core.read_model(modelPath);                    // Step 2: Read Model
+        //...                                                       // Step 3: Prepare inputs/outputs
+        //...                                                       // Step 4: Set device configuration
+        auto compiled = core.compile_model(model, device, config);  // Step 5: LoadNetwork
+        //! [ov:denormals_optimization:part0]
+            if (!compiled) {
+                throw std::runtime_error("error");
+            }
+    } catch (...) {
+    }
+    return 0;
+}
