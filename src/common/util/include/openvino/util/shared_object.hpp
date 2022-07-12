fffaeda5189c79ba357fb0aa9de5090f62a90cf0
Raasz, Pawel <pawel.raasz@intel.com>
2022-07-12 07:52:24 +0000

    Remove not used defer closer of shared object

--- src/common/util/include/openvino/util/shared_object.hpp
+++ src/common/util/include/openvino/util/shared_object.hpp
@@ -66,51 +66,5 @@ std::shared_ptr<void> load_shared_object(const wchar_t* path,
  * @throws Exception if the function is not found
  */
 void* get_symbol(const std::shared_ptr<void>& shared_object, const char* symbolName);
-
-/**
- * \brief Close shared object (library) with defer.
- *
- * Can be used as custom deleter in smart pointer.
- */
-class SharedObjectDeferCloser {
-public:
-    SharedObjectDeferCloser();  //!< Ctor
-
-    /**
-     * @brief Close shared object (library) with defer.
-     *
-     * Starts thread which complete closing the shared object.
-     *
-     * @param shared_object pointer to shared_object.
-     */
-    void operator()(void* shared_object) const;
-
-private:
-    static int m_count;                       //!< Number of libraries opened.
-    static std::mutex m_count_mutex;          //!< Mutex to guard access to counter.
-    static std::once_flag m_atexit_register;  //!< Flag to register wait for lib close at application exit.
-
-    /**
-     * @brief Close the library with delay.
-     *
-     * @param shared_object pointer to shared object (library).
-     */
-    static inline void lib_delayed_close(void* shared_object);
-
-    /**
-     * @brief Check if any library is open.
-     *
-     * @return True when any library open, otherwise false.
-     */
-    static inline bool any_open();
-
-    /**
-     * @brief Waits until all libraries are closed.
-     *
-     * Function waits until all threads which should close libraries will end.
-     */
-    static inline void wait_all_closed();
-};
-
 }  // namespace util
 }  // namespace ov
