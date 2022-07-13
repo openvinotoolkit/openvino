f5bf7d5df6dd949d2404c3af697b64a1d6ce81e9
Ryan Metcalfe <ryan.metcalfe@intel.com>
2022-07-13 08:46:28 -0700

    samples/classification_results.h: code style fixes

--- samples/cpp/common/utils/include/samples/classification_results.h
+++ samples/cpp/common/utils/include/samples/classification_results.h
@@ -148,21 +148,18 @@ public:
             std::cout << std::endl << std::endl;
             printHeader();
 
-            for (size_t id = image_id * _nTop, cnt = 0; id < (image_id + 1) * _nTop; ++cnt, ++id) 
-            {
+            for (size_t id = image_id * _nTop, cnt = 0; id < (image_id + 1) * _nTop; ++cnt, ++id) {
                 std::cout.precision(7);
                 // Getting probability for resulting class
                 const auto index = _results.at(id) + image_id * (_outTensor.get_size() / _batchSize);
             
                 ov::element::Type output_type = _outTensor.get_element_type();
-                if (output_type == ov::element::f16)
-                {
+                if (output_type == ov::element::f16) {
                     const auto result = _outTensor.data<ov::float16>()[index];
                     std::cout << std::setw(static_cast<int>(_classidStr.length())) << std::left << _results.at(id) << " ";
                     std::cout << std::left << std::setw(static_cast<int>(_probabilityStr.length())) << std::fixed << result;
                 }
-                else if (output_type == ov::element::f32)
-                {
+                else if (output_type == ov::element::f32) {
                     const auto result2 = _outTensor.data<const float>()[index];
                     std::cout << std::setw(static_cast<int>(_classidStr.length())) << std::left << _results.at(id) << " ";
                     std::cout << std::left << std::setw(static_cast<int>(_probabilityStr.length())) << std::fixed << result2;
