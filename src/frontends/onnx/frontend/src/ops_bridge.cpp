ca5372308c0b3695a2c5a80565d14a9706acc5d9
sheng.gui@intel.com <guisheng315@sina.com>
2022-07-14 20:35:49 +0800

    [ONNX] Add operator com.microsoft.Fusedgemm support into frontend/onnx (#11878)

--- src/frontends/onnx/frontend/src/ops_bridge.cpp
+++ src/frontends/onnx/frontend/src/ops_bridge.cpp
@@ -35,7 +35,11 @@
 #include "op/com.microsoft/attention.hpp"
 #include "op/com.microsoft/bias_gelu.hpp"
 #include "op/com.microsoft/embed_layer_normalization.hpp"
+<<<<<<< HEAD
 #include "op/com.microsoft/gelu.hpp"
+=======
+#include "op/com.microsoft/fusedgemm.hpp"
+>>>>>>> b67aeb881... [ONNX] Add operator com.microsoft.Fusedgemm support into frontend/onnx (#11878)
 #include "op/com.microsoft/skip_layer_normalization.hpp"
 #include "op/compress.hpp"
 #include "op/concat.hpp"
@@ -502,8 +506,12 @@ OperatorsBridge::OperatorsBridge() {
 
     REGISTER_OPERATOR_WITH_DOMAIN(MICROSOFT_DOMAIN, "Attention", 1, attention);
     REGISTER_OPERATOR_WITH_DOMAIN(MICROSOFT_DOMAIN, "BiasGelu", 1, bias_gelu);
+<<<<<<< HEAD
     REGISTER_OPERATOR_WITH_DOMAIN(MICROSOFT_DOMAIN, "Gelu", 1, gelu);
 
+=======
+    REGISTER_OPERATOR_WITH_DOMAIN(MICROSOFT_DOMAIN, "FusedGemm", 1, fusedgemm);
+>>>>>>> b67aeb881... [ONNX] Add operator com.microsoft.Fusedgemm support into frontend/onnx (#11878)
     REGISTER_OPERATOR_WITH_DOMAIN(MICROSOFT_DOMAIN, "EmbedLayerNormalization", 1, embed_layer_normalization);
     REGISTER_OPERATOR_WITH_DOMAIN(MICROSOFT_DOMAIN, "SkipLayerNormalization", 1, skip_layer_normalization);
 }
