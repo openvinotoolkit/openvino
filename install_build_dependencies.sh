9c69551c551f3981c445107b56eb6f3283e62632
Ilya Lavrenov <ilya.lavrenov@intel.com>
2022-07-16 03:28:50 +0800

    Try to restore

--- install_build_dependencies.sh
+++ install_build_dependencies.sh
@@ -21,7 +21,7 @@ yes_or_no() {
 
 # install dependencies
 if [ -f /etc/lsb-release ]; then
-    # Ubuntu 18.04, 20.04, 21.10, 22.04
+    # Ubuntu
     host_cpu=$(uname -m)
     if [ "$host_cpu" = "x86_64" ]; then
         x86_64_specific_packages="gcc-multilib g++-multilib"
@@ -30,8 +30,6 @@ if [ -f /etc/lsb-release ]; then
     fi
 
     sudo -E apt update
-
-    # build step tools
     sudo -E apt-get install -y \
             build-essential \
             cmake \
@@ -53,13 +51,11 @@ if [ -f /etc/lsb-release ]; then
             libtool \
             autoconf \
             shellcheck \
-            `# python and wheel packages` \
             patchelf \
             libenchant1c2a \
             python3-pip \
             python3-enchant \
             python3-setuptools \
-            `# openvino` \
             libcairo2-dev \
             libpango1.0-dev \
             libglib2.0-dev \
@@ -72,38 +68,17 @@ if [ -f /etc/lsb-release ]; then
             libusb-1.0-0-dev \
             libtbb-dev \
             libtinfo5 \
-            libopenblas-dev \
-            `# samples` \
-            pkg-config \
-            libgflags-dev \
-            zlib1g-dev \
-            `# myriad` \
-            libusb-1.0-0-dev \
-            `# hddl` \
-            libudev1 \
-            libusb-1.0-0 \
-            `# cl_compiler` \
-            libtinfo5
-    # hddl
-    if apt-cache search --names-only '^libjson-c3'| grep -q libjson-c3; then
-        # ubuntu 18.04
-        sudo -E apt-get install -y \
-            libjson-c3 \
-            libboost-filesystem1.65.1 \
-            libboost-program-options1.65.1 \
-            libboost-system1.65.1
-    elif apt-cache search --names-only '^libjson-c4'| grep -q libjson-c4; then
-        # ubuntu 20.04
-        sudo -E apt-get install -y \
-            libjson-c4 \
-            libboost-filesystem1.71.0 \
-            libboost-program-options1.71.0
+            libopenblas-dev
+    if apt-cache search --names-only '^libjson-c2'| grep -q libjson-c2; then
+        sudo -E apt-get install -y libjson-c2
+    else
+        sudo -E apt-get install -y libjson-c3
     fi
-    # for python3-enchant
-    if apt-cache search --names-only 'libenchant1c2a'| grep -q libenchant1c2a; then
-        sudo -E apt-get install -y libenchant1c2a
+    if apt-cache search --names-only '^libpng12-dev'| grep -q libpng12; then
+        sudo -E apt-get install -y libpng12-dev
+    else
+        sudo -E apt-get install -y libpng-dev
     fi
-    # samples
     if apt-cache search --names-only '^nlohmann-json3-dev'| grep -q nlohmann-json3; then
         sudo -E apt-get install -y nlohmann-json3-dev
     else
@@ -179,16 +154,37 @@ elif [ -f /etc/os-release ] && grep -q "raspbian" /etc/os-release; then
     sudo -E apt update
     sudo -E apt-get install -y \
             build-essential \
+            curl \
             wget \
             libssl-dev \
             ca-certificates \
             git \
+            libboost-regex-dev \
+            libgtk2.0-dev \
             pkg-config \
             libgflags-dev \
             zlib1g-dev \
             nlohmann-json-dev \
             unzip \
-            libusb-1.0-0-dev
+            automake \
+            libtool \
+            autoconf \
+            libcairo2-dev \
+            libpango1.0-dev \
+            libglib2.0-dev \
+            libgtk2.0-dev \
+            libswscale-dev \
+            libavcodec-dev \
+            libavformat-dev \
+            libgstreamer1.0-0 \
+            gstreamer1.0-plugins-base \
+            libusb-1.0-0-dev \
+            libopenblas-dev
+    if apt-cache search --names-only '^libpng12-dev'| grep -q libpng12; then
+        sudo -E apt-get install -y libpng12-dev
+    else
+        sudo -E apt-get install -y libpng-dev
+    fi
 else
     echo "Unknown OS, please install build dependencies manually"
 fi
