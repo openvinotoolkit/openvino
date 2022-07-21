d92832245d0e10d3a3c9063cdfe191792e5778b3
Ilya Lavrenov <ilya.lavrenov@intel.com>
2022-07-22 02:34:46 +0400

    Fixed WIndows backslash paths (#12250)

--- src/cmake/install_tbb.cmake
+++ src/cmake/install_tbb.cmake
@@ -78,6 +78,9 @@ if(THREADING MATCHES "^(TBB|TBB_AUTO)$" AND
                 endif()
             endforeach()
         endforeach()
+
+        # remember TBBROOT path or system one
+        set(IE_TBB_DIR_INSTALL "${TBB_DIR}")
     elseif(tbb_downloaded)
         if(WIN32)
             install(DIRECTORY "${TBB}/bin"
