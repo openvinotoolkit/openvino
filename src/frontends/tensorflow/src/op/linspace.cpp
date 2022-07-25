46d2ca22cc76243a57d94a70abe268cbc6548a0c
Kazantsev, Roman <roman.kazantsev@intel.com>
2022-07-25 15:03:59 +0400

    Apply code-review feedback
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/frontends/tensorflow/src/op/linspace.cpp
+++ src/frontends/tensorflow/src/op/linspace.cpp
@@ -27,17 +27,17 @@ OutputVector translate_linspace_op(const NodeContext& node) {
     auto num = node.get_input(2);
 
     TENSORFLOW_OP_VALIDATION(node, start.get_partial_shape().rank().is_static(), "Input rank must be static.");
-    int64_t rank = start.get_partial_shape().rank().get_length();
+    int64_t start_rank = start.get_partial_shape().rank().get_length();
 
     // retrieve axis from Constant node and compute a range of axes except given axis
     // for unsqueezing start and delta tensors
     std::vector<int64_t> axis;
     std::vector<int64_t> except_axis_range;
-    if (num_inputs > 3 && rank > 0) {
+    if (num_inputs > 3 && start_rank > 0) {
         get_const_input(node, 3, &axis);
         TENSORFLOW_OP_VALIDATION(node, axis.size() == 1, "Axis must be a scalar for LinSpace operation.");
-        axis[0] = axis[0] >= 0 ? axis[0] : rank + 1 + axis[0];
-        for (int64_t dim_ind = 0; dim_ind < rank + 1; ++dim_ind) {
+        axis[0] = axis[0] >= 0 ? axis[0] : start_rank + 1 + axis[0];
+        for (int64_t dim_ind = 0; dim_ind < start_rank + 1; ++dim_ind) {
             if (dim_ind != axis[0]) {
                 except_axis_range.push_back(dim_ind);
             }
@@ -45,18 +45,18 @@ OutputVector translate_linspace_op(const NodeContext& node) {
     }
 
     TENSORFLOW_OP_VALIDATION(node,
-                             axis.empty() && rank == 0 || axis.size() == 1 && rank > 0,
+                             axis.empty() && start_rank == 0 || axis.size() == 1 && start_rank > 0,
                              "Axis must be used only if input for LinSpace operation is ND tensor.");
 
-    auto one = make_shared<ConvertLike>(make_shared<Constant>(element::i64, Shape{}, 1), num);
+    auto one = make_shared<Constant>(num.get_element_type(), Shape{}, 1);
     auto num_minus_1 = make_shared<ConvertLike>(make_shared<Subtract>(num, one), start);
     auto delta = make_shared<Divide>(make_shared<Subtract>(stop, start), num_minus_1);
 
-    auto zero = make_shared<ConvertLike>(make_shared<Constant>(element::i64, Shape{}, 0), num);
-    auto range_0_num_minus_1 = make_shared<ConvertLike>(make_shared<Range>(zero, num, one, element::i64), start);
+    auto zero = make_shared<Constant>(num.get_element_type(), Shape{}, 0);
+    auto range_0_num_minus_1 = make_shared<ConvertLike>(make_shared<Range>(zero, num, one, num.get_element_type()), start);
 
     // convert a case with scalar inputs
-    if (axis.empty() && rank == 0) {
+    if (axis.empty() && start_rank == 0) {
         auto delta_mul_range = make_shared<Multiply>(delta, range_0_num_minus_1);
         auto result = make_shared<Add>(start, delta_mul_range);
         set_node_name(node.get_name(), result);
