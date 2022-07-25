046468408613cf9b8a85eed87c592a9c79db7d4b
chenhuwa <chenhu.wang@intel.com>
2022-07-25 22:54:35 +0800

    proofreading for grammatical and stylistic corrections

--- src/inference/include/ie/cpu/cpu_config.hpp
+++ src/inference/include/ie/cpu/cpu_config.hpp
@@ -29,11 +29,11 @@ namespace CPUConfigParams {
 /**
  * @brief The name for defining if denormals is optimized on CPU whenever it is possible
  *
- * This option let cpu plugin know if denormals is optimized where it see performance benefits from
- * getting ride of denormals computation.
- * Such option do not guarantee accuracy of the model, the accuracy in this mode should be
- * verified separately by the user and basing on performance and accuracy results it should be
- * user's decision to use this option or not to use
+ * This option lets CPU plugin determine whether denormals are optimized where it expects
+ * performance benefits from getting rid of denormals computation.
+ * Such option does not guarantee accuracy of the model, the accuracy in this mode should be
+ * verified separately by the user. Basing on performance and accuracy results, it should be
+ * user's decision to use this option or not.
  * It is passed to Core::SetConfig(), this option should be used with values:
  * PluginConfigParams::YES or PluginConfigParams::NO
  * If not set explicitly, the behavior is kept in runtime enviroment where compile_model is called.
