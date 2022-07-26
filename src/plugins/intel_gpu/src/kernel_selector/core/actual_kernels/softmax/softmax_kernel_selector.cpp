204f2759fff5a941127ca9483120444e126c67a0
Oleksii Khovan <okhovan@lohika.com>
2022-07-26 13:13:27 +0300

    add kernel for all axes case;

--- src/plugins/intel_gpu/src/kernel_selector/core/actual_kernels/softmax/softmax_kernel_selector.cpp
+++ src/plugins/intel_gpu/src/kernel_selector/core/actual_kernels/softmax/softmax_kernel_selector.cpp
@@ -7,6 +7,7 @@
 #include "softmax_kernel_bf.h"
 #include "softmax_kernel_fb.h"
 #include "softmax_kernel_items_class_optimized.h"
+#include "softmax_kernel_blocked_all_axis.h"
 
 namespace kernel_selector {
 
@@ -15,6 +16,7 @@ softmax_kernel_selector::softmax_kernel_selector() {
     Attach<SoftmaxKernel_bf>();
     Attach<SoftmaxKernel_fb>();
     Attach<SoftmaxKerneItemsClassOptimized>();
+    Attach<SoftmaxKernelBlockedAllAxis>();
 }
 
 KernelsData softmax_kernel_selector::GetBestKernels(const Params& params, const optional_params& options) const {
