e4a046549ff4dee01c066f5fc41a92a37e205003
Ilya Churaev <ilya.churaev@intel.com>
2022-07-26 05:05:32 +0200

    Renamed shutdown method and added to legacy C++ API
    (cherry picked from commit a8395bd2073728b07c419ad019e2c7fcdaa54951)

--- src/inference/src/openvino_shutdown.cpp
+++ src/inference/src/openvino_shutdown.cpp
@@ -2,9 +2,14 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include "ie_core.hpp"
 #include "openvino/frontend/manager.hpp"
 #include "openvino/runtime/core.hpp"
 
-void ov::openvino_shutdown() {
+void ov::shutdown() {
     frontend::FrontEndManager::shutdown();
+}
+
+void InferenceEngine::shutdown() {
+    ov::shutdown();
 }
