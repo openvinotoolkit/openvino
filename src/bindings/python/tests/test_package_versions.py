1117f43b6d369358b6fb9867c5d972654b0ae33c
Roboreptile <piotr.krzeminski@intel.com>
2022-07-26 14:46:49 +0000

    [PYTHON] Update setup.py to include correct packages, code quality improvements

--- src/bindings/python/tests/test_package_versions.py
+++ src/bindings/python/tests/test_package_versions.py
@@ -2,34 +2,15 @@
 # Copyright (C) 2018-2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-import pytest
-
-import openvino                         as ov
-import openvino.preprocess              as ov_pre
-import openvino.runtime                 as ov_run
-import openvino.frontend                as ov_front
+import openvino.preprocess as ov_pre
+import openvino.runtime as ov_run
+import openvino.frontend as ov_front
 import openvino.offline_transformations as ov_off_transf
-import openvino.inference_engine        as ov_ie
-import openvino.pyopenvino              as ov_py
-import openvino.utils                   as ov_utils
-import openvino.tools                   as ov_tools
-
-# == Version rules for all packages ==
-# Refer to ../docs/code_examples.md
-# openvino                         - namespace - no version methods
-# openvino.utils                   - not a package - no version provided
-# openvino.tools                   - namespace - no version methods
-# openvino.pyopenvino              - pybind, provides only get_version method
-# openvino.inference_engine        - \/ \/
-# openvino.offline_transformations - Remaining entries are proper packages
-# openvino.frontend                - with__version__ attribute and
-# openvino.preprocess              - get_version method provided
-# openvino.runtime                 - /\ /\
-# ====================================
+import openvino.pyopenvino as ov_py
 
 
-def test_get_version_exists_and_match():
-    packages = [ov_run, ov_front, ov_pre, ov_off_transf, ov_ie, ov_py]
+def test_get_version_match():
+    packages = [ov_run, ov_front, ov_pre, ov_off_transf, ov_py]
     versions = set()
 
     for package in packages:
@@ -38,8 +19,8 @@ def test_get_version_exists_and_match():
     assert len(versions) == 1
 
 
-def test_dunder_version_exists_and_match():
-    packages = [ov_run, ov_front, ov_pre, ov_off_transf, ov_ie]
+def test_dunder_version_match():
+    packages = [ov_run, ov_front, ov_pre, ov_off_transf]
     versions = set()
 
     for package in packages:
