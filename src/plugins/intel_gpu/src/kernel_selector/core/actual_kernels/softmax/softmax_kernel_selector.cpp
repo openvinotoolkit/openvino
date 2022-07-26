fc20a5ef6a8c77174579c32b26dde2e00b8c99bd
Oleksii Khovan <okhovan@lohika.com>
2022-07-26 13:17:11 +0300

    add kernel for single axis case

--- src/plugins/intel_gpu/src/kernel_selector/core/actual_kernels/softmax/softmax_kernel_selector.cpp
+++ src/plugins/intel_gpu/src/kernel_selector/core/actual_kernels/softmax/softmax_kernel_selector.cpp
@@ -7,6 +7,7 @@
 #include "softmax_kernel_bf.h"
 #include "softmax_kernel_fb.h"
 #include "softmax_kernel_items_class_optimized.h"
+#include "softmax_kernel_blocked_single_axis.h"
 #include "softmax_kernel_blocked_all_axis.h"
 
 namespace kernel_selector {
@@ -16,7 +17,9 @@ softmax_kernel_selector::softmax_kernel_selector() {
     Attach<SoftmaxKernel_bf>();
     Attach<SoftmaxKernel_fb>();
     Attach<SoftmaxKerneItemsClassOptimized>();
+    Attach<SoftmaxKernelBlockedSingleAxis>();
     Attach<SoftmaxKernelBlockedAllAxis>();
+    Attach<SoftmaxKernelBlockedSingleAxis>();
 }
 
 KernelsData softmax_kernel_selector::GetBestKernels(const Params& params, const optional_params& options) const {
