be34871521b8b41274ba738fb01175edd13073b2
Roboreptile <piotr.krzeminski@intel.com>
2022-07-26 15:33:41 +0000

    [PYTHON] Fix pylinter errors

--- src/bindings/python/tests/__init__.py
+++ src/bindings/python/tests/__init__.py
@@ -133,4 +133,3 @@ xfail_issue_82039 = xfail_test(reason="Unsupported data type Optional, RuntimeEr
 
 xfail_issue_86909 = xfail_test(reason="ssd_resnet34 - AssertionError: zoo models results mismatch")
 xfail_issue_86911 = xfail_test(reason="LSTM_Seq_len_unpacked - AssertionError: zoo models results mismatch")
-
