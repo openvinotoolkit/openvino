01011cccb4e8a7fd707b200b564bb51fe50e52e2
Roboreptile <piotr.krzeminski@intel.com>
2022-07-27 15:17:29 +0000

    [PYTHON] Reverse get_version removal, add flake8 ignore note

--- src/bindings/python/src/compatibility/openvino/inference_engine/__init__.py
+++ src/bindings/python/src/compatibility/openvino/inference_engine/__init__.py
@@ -30,3 +30,4 @@ if sys.platform == "win32":
 from .ie_api import *
 
 __all__ = ["IENetwork", "TensorDesc", "IECore", "Blob", "PreProcessInfo", "get_version"]
+__version__ = get_version()  # type: ignore
