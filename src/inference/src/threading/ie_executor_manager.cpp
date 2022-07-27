8efa5dbd7bc77f4f073f4263a65cbaf82a731ba4
Sun Xiaoxia <xiaoxia.sun@intel.com>
2022-07-27 16:49:53 +0800

    Xiaoxia/onetbb old version (#12303)
    * support oneTBB old version
    
    * fix oneTBB version mismatch issues
    
    * fix clang issue
    
    * add 'tbb' path to setupvars.sh and OpenVINOConfig.cmake.in
    
    * Update scripts/setupvars/setupvars.sh
    
    Co-authored-by: Ilya Lavrenov <ilya.lavrenov@intel.com>
    Co-authored-by: Shen, Wanglei <wanglei.shen@intel.com>

--- src/inference/src/threading/ie_executor_manager.cpp
+++ src/inference/src/threading/ie_executor_manager.cpp
@@ -64,6 +64,9 @@ void ExecutorManagerImpl::setTbbFlag(bool flag) {
         if (!tbbTaskScheduler) {
 #    if (TBB_INTERFACE_VERSION < 12000)
             tbbTaskScheduler = std::make_shared<tbb::task_scheduler_init>();
+#    elif (TBB_INTERFACE_VERSION < 12060)
+            tbbTaskScheduler =
+                std::make_shared<oneapi::tbb::task_scheduler_handle>(oneapi::tbb::task_scheduler_handle::get());
 #    else
             tbbTaskScheduler = std::make_shared<oneapi::tbb::task_scheduler_handle>(tbb::attach{});
 #    endif
