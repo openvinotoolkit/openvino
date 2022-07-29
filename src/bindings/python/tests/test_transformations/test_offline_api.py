de0630e6df09d3dd1909f3f833f0362ec2a65e0f
bszmelcz <bartosz.szmelczynski@intel.com>
2022-07-29 14:39:55 +0200

    expose ConvertSeqToTensorIterator transformation

--- src/bindings/python/tests/test_transformations/test_offline_api.py
+++ src/bindings/python/tests/test_transformations/test_offline_api.py
@@ -12,6 +12,7 @@ from openvino.offline_transformations import (
     apply_pruning_transformation,
     apply_make_stateful_transformation,
     compress_model_transformation,
+    convert_sequence_to_tensor_iterator_transformation,
 )
 
 from openvino.runtime import Model, PartialShape, Core
@@ -25,6 +26,70 @@ def get_test_model():
     res = ov.opset8.result(relu, name="result")
     res.get_output_tensor(0).set_names({"result"})
     return Model([res], [param], "test")
+
+
+def get_lstm_sequence_model():
+    parameter_x = ov.opset9.parameter([1, 2, 16], name="X")
+    parameter_y = ov.opset9.parameter([1, 1, 128], name="Y")
+    parameter_z = ov.opset9.parameter([1, 1, 128], name="Z")
+    seq_lengths = ov.opset9.constant(np.array([2]), dtype=np.int32)
+
+    w_val = np.zeros(shape=[1, 512, 16])
+    r_val = np.zeros(shape=[1, 512, 128])
+    b_val = np.zeros(shape=[1, 512])
+
+    const_w = ov.opset9.constant(value=w_val, dtype=np.float32)
+    const_r = ov.opset9.constant(value=r_val, dtype=np.float32)
+    const_b = ov.opset9.constant(value=b_val, dtype=np.float32)
+
+    lstm_sequence = ov.opset9.lstm_sequence(parameter_x, parameter_y, parameter_z, seq_lengths, const_w, const_r, const_b, 128, "FORWARD")
+    y_out = ov.opset9.result(lstm_sequence.output(0))
+    Ho = ov.opset9.result(lstm_sequence.output(1))
+    Co = ov.opset9.result(lstm_sequence.output(2))
+
+    model = Model([y_out, Ho, Co], [parameter_x, parameter_y, parameter_z])
+    return model
+
+
+def get_rnn_sequence_model():
+    parameter_x = ov.opset9.parameter([1, 2, 16], name="X")
+    parameter_y = ov.opset9.parameter([1, 1, 128], name="Y")
+    seq_lengths = ov.opset9.constant(np.array([2]), dtype=np.int32)
+
+    w_val = np.zeros(shape=[1, 128, 16])
+    r_val = np.zeros(shape=[1, 128, 128])
+    b_val = np.zeros(shape=[1, 128])
+
+    const_w = ov.opset9.constant(value=w_val, dtype=np.float32)
+    const_r = ov.opset9.constant(value=r_val, dtype=np.float32)
+    const_b = ov.opset9.constant(value=b_val, dtype=np.float32)
+
+    rnn_sequence = ov.opset9.rnn_sequence(parameter_x, parameter_y, seq_lengths, const_w, const_r, const_b, 128, "FORWARD")
+    y_out = ov.opset9.result(rnn_sequence.output(0))
+    model = Model([y_out], [parameter_x, parameter_y])
+
+    return model
+
+
+def get_gru_sequence_model():
+    parameter_x = ov.opset9.parameter([1, 2, 16], name="X")
+    parameter_y = ov.opset9.parameter([1, 1, 128], name="Y")
+    seq_lengths = ov.opset9.constant(np.array([2]), dtype=np.int32)
+
+    w_val = np.zeros(shape=[1, 384, 16])
+    r_val = np.zeros(shape=[1, 384, 128])
+    b_val = np.zeros(shape=[1, 384])
+
+    const_w = ov.opset9.constant(value=w_val, dtype=np.float32)
+    const_r = ov.opset9.constant(value=r_val, dtype=np.float32)
+    const_b = ov.opset9.constant(value=b_val, dtype=np.float32)
+
+    gru_sequence = ov.opset9.gru_sequence(parameter_x, parameter_y, seq_lengths, const_w, const_r, const_b, 128, "FORWARD")
+    y_out = ov.opset9.result(gru_sequence.output(0))
+    Ho = ov.opset9.result(gru_sequence.output(1))
+    model = Model([y_out, Ho], [parameter_x, parameter_y])
+
+    return model
 
 
 def test_moc_transformations():
@@ -177,3 +242,42 @@ def test_version_ir_v11():
 
     os.remove(xml_path)
     os.remove(bin_path)
+
+
+def test_convert_lstm_to_tensor_iterator():
+    model = get_lstm_sequence_model()
+    ops_types = [op.get_type_name() for op in model.get_ops()]
+    # assert that LSTM sequence is present in the model
+    assert "TensorIterator" not in ops_types
+    assert "LSTMSequence" in ops_types
+    # assert that LSTM sequence got transformed into TensorIterator
+    convert_sequence_to_tensor_iterator_transformation(model)
+    ops_types = [op.get_type_name() for op in model.get_ops()]
+    assert "LSTMSequence" not in ops_types
+    assert "TensorIterator" in ops_types
+
+
+def test_convert_rnn_to_tensor_iterator():
+    model = get_rnn_sequence_model()
+    ops_types = [op.get_type_name() for op in model.get_ops()]
+    # assert that RNN sequence is present in the model
+    assert "TensorIterator" not in ops_types
+    assert "RNNSequence" in ops_types
+    convert_sequence_to_tensor_iterator_transformation(model)
+    ops_types = [op.get_type_name() for op in model.get_ops()]
+    # assert that RNN sequence got transformed into TensorIterator
+    assert "RNNSequence" not in ops_types
+    assert "TensorIterator" in ops_types
+
+
+def test_convert_gru_to_tensor_iterator():
+    model = get_gru_sequence_model()
+    ops_types = [op.get_type_name() for op in model.get_ops()]
+    # assert that GRU sequence is present in the model
+    assert "TensorIterator" not in ops_types
+    assert "GRUSequence" in ops_types
+    convert_sequence_to_tensor_iterator_transformation(model)
+    ops_types = [op.get_type_name() for op in model.get_ops()]
+    # assert that GRU sequence got transformed into TensorIterator
+    assert "GRUSequence" not in ops_types
+    assert "TensorIterator" in ops_types
