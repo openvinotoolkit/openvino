f34d1c0b315854f9349ae9e7a9a949f14cee07da
Kazantsev, Roman <roman.kazantsev@intel.com>
2022-08-01 14:40:05 +0400

    Fix build issue
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/frontends/tests/frontend/shared/src/telemetry.cpp
+++ src/frontends/tests/frontend/shared/src/telemetry.cpp
@@ -81,7 +81,6 @@ TEST_P(FrontEndTelemetryTest, TestTelemetryMock) {
         EXPECT_NO_THROW(m_frontEnd->add_extension(telemetry_extension));
         m_inputModel = m_frontEnd->load(m_param.m_modelName);
         function = m_frontEnd->decode(m_inputModel);
-        bool is_found = false;
         for (const auto m_expected_events : m_param.m_expected_events) {
             is_found = false;
             is_found = (m_test_telemetry.m_event_cnt == m_expected_events.size()) &&
