f9a73217ce2afc20e8cabe00bbd05c8ded1361fe
Mateusz Tabaka <mateusz.tabaka@intel.com>
2022-08-02 04:40:23 +0000

    fix DefaultConfigurationTest test

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/configuration_tests/configuration_tests.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/configuration_tests/configuration_tests.cpp
@@ -6,19 +6,27 @@
 #include "configuration_tests/configuration_tests.hpp"
 #include "threading/ie_istreams_executor.hpp"
 #include "ie_plugin_config.hpp"
+#include "ie_system_conf.h"
 
 namespace {
+InferenceEngine::Parameter getDefaultBindThreadParameter() {
+    if (InferenceEngine::getAvailableCoresTypes().size() > 1) {
+        return InferenceEngine::Parameter{std::string{CONFIG_VALUE(HYBRID_AWARE)}};
+    } else {
 #if (defined(__APPLE__) || defined(_WIN32))
-auto defaultBindThreadParameter = InferenceEngine::Parameter{std::string{CONFIG_VALUE(NUMA)}};
+        return InferenceEngine::Parameter{std::string{CONFIG_VALUE(NUMA)}};
 #else
-auto defaultBindThreadParameter = InferenceEngine::Parameter{std::string{CONFIG_VALUE(YES)}};
+        return InferenceEngine::Parameter{std::string{CONFIG_VALUE(YES)}};
 #endif
+    }
+}
+
 INSTANTIATE_TEST_CASE_P(
     smoke_Basic,
     DefaultConfigurationTest,
     ::testing::Combine(
         ::testing::Values("CPU"),
-        ::testing::Values(DefaultParameter{CONFIG_KEY(CPU_BIND_THREAD), defaultBindThreadParameter})),
+        ::testing::Values(DefaultParameter{CONFIG_KEY(CPU_BIND_THREAD), getDefaultBindThreadParameter()})),
     DefaultConfigurationTest::getTestCaseName);
 
 }  // namespace
