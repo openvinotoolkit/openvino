729b7515f3d8e464dc630ee2026dd73628005652
Kim,SungEun <sungeun.kim@intel.com>
2022-08-03 05:26:23 +0900

    add double blocked formats to onednn concat

--- src/plugins/intel_gpu/src/graph/impls/onednn/concatenation_onednn.cpp
+++ src/plugins/intel_gpu/src/graph/impls/onednn/concatenation_onednn.cpp
@@ -82,8 +82,13 @@ attach_concatenation_onednn::attach_concatenation_onednn() {
         MAKE_TUPLE4(bs_fs_yx_bsv4_fsv4,     f32, f16, u8, i8),
         MAKE_TUPLE4(bs_fs_yx_bsv8_fsv4,     f32, f16, u8, i8),
         MAKE_TUPLE4(bs_fs_yx_bsv16_fsv16,   f32, f16, u8, i8),
+        MAKE_TUPLE4(bs_fs_yx_bsv16_fsv32,   f32, f16, u8, i8),
         MAKE_TUPLE4(bs_fs_yx_bsv32_fsv16,   f32, f16, u8, i8),
         MAKE_TUPLE4(bs_fs_yx_bsv32_fsv32,   f32, f16, u8, i8),
+        MAKE_TUPLE4(bs_fs_zyx_bsv16_fsv16,  f32, f16, u8, i8),
+        MAKE_TUPLE4(bs_fs_zyx_bsv16_fsv32,  f32, f16, u8, i8),
+        MAKE_TUPLE4(bs_fs_zyx_bsv32_fsv16,  f32, f16, u8, i8),
+        MAKE_TUPLE4(bs_fs_zyx_bsv32_fsv32,  f32, f16, u8, i8),
     });
 }
 
