67dcb05251de8539e3655519455228524226425c
Andrew Kwangwoong Park <andrew.park@intel.com>
2022-08-02 13:34:15 +0900

    [GPU] Remove dependency to typed_program_node from calc_output_layout (#12378)
    Signed-off-by: Andrew Park <andrew.park@intel.com>

--- src/plugins/intel_gpu/src/graph/tile.cpp
+++ src/plugins/intel_gpu/src/graph/tile.cpp
@@ -15,12 +15,12 @@ primitive_type_id tile::type_id() {
     return &instance;
 }
 
-layout tile_inst::calc_output_layout(tile_node const& node) {
-    assert(static_cast<bool>(node.get_primitive()->output_data_type) == false &&
+layout tile_inst::calc_output_layout(tile_node const& node, kernel_impl_params const& impl_param) {
+    assert(static_cast<bool>(impl_param.desc->output_data_type) == false &&
            "Output data type forcing is not supported for tile_node!");
-    auto desc = node.get_primitive();
+    auto desc = impl_param.typed_desc<tile>();
 
-    auto input_layout = node.input().get_output_layout();
+    auto input_layout = impl_param.input_layouts[0];
     auto input_format = input_layout.format;
     return layout{input_layout.data_type, input_format, desc->out_shape};
 }
