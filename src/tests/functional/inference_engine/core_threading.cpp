5ef6140b63f121f857564a8c6874201dc44d8c3d
fishbell <bell.song@intel.com>
2022-08-03 04:55:01 -0400

    debug only
    Signed-off-by: fishbell <bell.song@intel.com>

--- src/tests/functional/inference_engine/core_threading.cpp
+++ src/tests/functional/inference_engine/core_threading.cpp
@@ -130,7 +130,7 @@ TEST_F(CoreThreadingTests, RegisterPlugins) {
 
 // tested function: GetAvailableDevices, UnregisterPlugin
 // TODO: some initialization (e.g. thread/dlopen) sporadically fails during such stress-test scenario
-TEST_F(CoreThreadingTests, DISABLED_GetAvailableDevices) {
+TEST_F(CoreThreadingTests, GetAvailableDevices) {
     InferenceEngine::Core ie;
     runParallel([&] () {
         std::vector<std::string> devices = ie.GetAvailableDevices();
@@ -145,7 +145,7 @@ TEST_F(CoreThreadingTests, DISABLED_GetAvailableDevices) {
                 ASSERT_STR_CONTAINS(ex.what(), "name is not registered in the");
             }
         }
-    }, 30);
+    }, 100, 12);
 }
 
 // tested function: ReadNetwork, AddExtension
