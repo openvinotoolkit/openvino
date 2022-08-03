e8bc643f819aefacbbcec4ccf8e6541cc552a38f
Kim,SungEun <sungeun.kim@intel.com>
2022-08-04 03:37:33 +0900

    add bs_fs_yx_bsv16_fsv16/bs_fs_yx_bsv16_fsv32 to depth_to_space

--- src/plugins/intel_gpu/src/graph/impls/ocl/depth_to_space.cpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl/depth_to_space.cpp
@@ -51,30 +51,14 @@ namespace detail {
 
 attach_depth_to_space_impl::attach_depth_to_space_impl() {
     implementation_map<depth_to_space>::add(impl_types::ocl, depth_to_space_impl::create, {
-        std::make_tuple(data_types::f32, format::bfyx),
-        std::make_tuple(data_types::f16, format::bfyx),
-        std::make_tuple(data_types::u8, format::bfyx),
-        std::make_tuple(data_types::i8, format::bfyx),
-        std::make_tuple(data_types::f32, format::bfzyx),
-        std::make_tuple(data_types::f16, format::bfzyx),
-        std::make_tuple(data_types::u8, format::bfzyx),
-        std::make_tuple(data_types::i8, format::bfzyx),
-        std::make_tuple(data_types::f32, format::b_fs_yx_fsv16),
-        std::make_tuple(data_types::f16, format::b_fs_yx_fsv16),
-        std::make_tuple(data_types::u8, format::b_fs_yx_fsv16),
-        std::make_tuple(data_types::i8, format::b_fs_yx_fsv16),
-        std::make_tuple(data_types::f32, format::b_fs_yx_fsv32),
-        std::make_tuple(data_types::f16, format::b_fs_yx_fsv32),
-        std::make_tuple(data_types::u8, format::b_fs_yx_fsv32),
-        std::make_tuple(data_types::i8, format::b_fs_yx_fsv32),
-        std::make_tuple(data_types::f32, format::bs_fs_yx_bsv32_fsv16),
-        std::make_tuple(data_types::f16, format::bs_fs_yx_bsv32_fsv16),
-        std::make_tuple(data_types::u8, format::bs_fs_yx_bsv32_fsv16),
-        std::make_tuple(data_types::i8, format::bs_fs_yx_bsv32_fsv16),
-        std::make_tuple(data_types::f32, format::bs_fs_yx_bsv32_fsv32),
-        std::make_tuple(data_types::f16, format::bs_fs_yx_bsv32_fsv32),
-        std::make_tuple(data_types::u8, format::bs_fs_yx_bsv32_fsv32),
-        std::make_tuple(data_types::i8, format::bs_fs_yx_bsv32_fsv32),
+        MAKE_TUPLE4(bfyx,                   f32, f16, u8, i8),
+        MAKE_TUPLE4(bfzyx,                  f32, f16, u8, i8),
+        MAKE_TUPLE4(b_fs_yx_fsv16,          f32, f16, u8, i8),
+        MAKE_TUPLE4(b_fs_yx_fsv32,          f32, f16, u8, i8),
+        MAKE_TUPLE4(bs_fs_yx_bsv16_fsv16,   f32, f16, u8, i8),
+        MAKE_TUPLE4(bs_fs_yx_bsv16_fsv32,   f32, f16, u8, i8),
+        MAKE_TUPLE4(bs_fs_yx_bsv32_fsv16,   f32, f16, u8, i8),
+        MAKE_TUPLE4(bs_fs_yx_bsv32_fsv32,   f32, f16, u8, i8),
     });
 }
 
