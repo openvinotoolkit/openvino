7eb0dfd08ce281a15a93ecf8a814f7d1fc7ea092
Anastasia Popova <anastasia.popova@intel.com>
2022-08-03 12:00:30 +0200

    Updated date for update message. (#12405)
    * Updated date for update message.
    
    * Year update.

--- tools/mo/openvino/tools/mo/utils/get_ov_update_message.py
+++ tools/mo/openvino/tools/mo/utils/get_ov_update_message.py
@@ -8,10 +8,10 @@ msg_fmt = 'It\'s been a while, check for a new version of ' + \
 
 
 def get_ov_update_message():
-    expected_update_date = datetime.date(year=2022, month=4, day=30)
+    expected_update_date = datetime.date(year=2023, month=2, day=1)
     current_date = datetime.date.today()
 
-    link = 'https://software.intel.com/content/www/us/en/develop/tools/openvino-toolkit/download.html?cid=other&source=prod&campid=ww_2022_bu_IOTG_OpenVINO-2022-1&content=upg_all&medium=organic'
+    link = 'https://software.intel.com/content/www/us/en/develop/tools/openvino-toolkit/download.html?cid=other&source=prod&campid=ww_2023_bu_IOTG_OpenVINO-2022-1&content=upg_all&medium=organic'
 
     return msg_fmt.format(link) if current_date >= expected_update_date else None
 
