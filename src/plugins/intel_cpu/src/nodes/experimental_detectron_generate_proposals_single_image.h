62c65222f9aafc3a5e5b0e65b66fca7c64f308e9
Yaroslav Torzuk <yaroslav.torzuk2@altran.com>
2022-08-08 17:40:44 +0200

    Integration of jit_uni_nms_proposal_kernel into ExperimentalDetectronGenerateProposalsSingleImage

--- src/plugins/intel_cpu/src/nodes/experimental_detectron_generate_proposals_single_image.h
+++ src/plugins/intel_cpu/src/nodes/experimental_detectron_generate_proposals_single_image.h
@@ -6,6 +6,7 @@
 
 #include <ie_common.h>
 #include <node.h>
+#include "kernels/jit_uni_nms_proposal_kernel.hpp"
 
 namespace ov {
 namespace intel_cpu {
@@ -23,6 +24,7 @@ public:
 
     bool needShapeInfer() const override;
     bool needPrepareParams() const override;
+    void createPrimitive() override;
     void executeDynamicImpl(dnnl::stream strm) override { execute(strm); }
     static bool isSupportedOperation(const std::shared_ptr<const ngraph::Node>& op, std::string& errorMessage) noexcept;
 
@@ -47,6 +49,7 @@ private:
     float coordinates_offset;
 
     std::vector<int> roi_indices_;
+    std::unique_ptr<jit_uni_nms_proposal_kernel> nms_kernel_ {};
 };
 
 }   // namespace node
