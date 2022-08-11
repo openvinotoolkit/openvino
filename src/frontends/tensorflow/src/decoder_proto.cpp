833234fc484de17620cc5764f04329ad9fe91373
Maxim Vafin <maxim.vafin@intel.com>
2022-08-11 17:33:03 +0200

    Apply code style

--- src/frontends/tensorflow/src/decoder_proto.cpp
+++ src/frontends/tensorflow/src/decoder_proto.cpp
@@ -4,10 +4,9 @@
 
 #include "decoder_proto.hpp"
 
-#include "openvino/frontend/tensorflow/node_context.hpp"
-
 #include "attr_value.pb.h"
 #include "node_def.pb.h"
+#include "openvino/frontend/tensorflow/node_context.hpp"
 #include "types.pb.h"
 
 namespace ov {
