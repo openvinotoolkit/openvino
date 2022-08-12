437f90bac54f2402c6f1cd8caa3a52c0bd8cd149
mitruska <katarzyna.mitrus@intel.com>
2022-08-12 13:00:12 +0200

    Imports and code refactor

--- tools/mo/openvino/tools/mo/front/tf/GRUBlockCellReplacement.py
+++ tools/mo/openvino/tools/mo/front/tf/GRUBlockCellReplacement.py
@@ -1,12 +1,12 @@
 # Copyright (C) 2018-2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from openvino.tools.mo.front.common.replacement import FrontReplacementPattern
 from openvino.tools.mo.front.common.partial_infer.utils import int64_array
+from openvino.tools.mo.front.common.replacement import FrontReplacementPattern
 from openvino.tools.mo.front.tf.graph_utils import create_op_node_with_second_input
 from openvino.tools.mo.graph.graph import Graph, rename_node
-from openvino.tools.mo.ops.GRUCell import GRUCell
 from openvino.tools.mo.ops.concat import Concat
+from openvino.tools.mo.ops.GRUCell import GRUCell
 from openvino.tools.mo.ops.split import AttributedSplit
 from openvino.tools.mo.ops.transpose import Transpose
 
@@ -34,11 +34,8 @@ class GRUBlockCellToGRUCell(FrontReplacementPattern):
             # W_rz -> W_r, W_z
             tf_gru_block_cell.in_port(2).get_connection().set_destination(split_rz_w.in_port(0))
 
-            concat_zrh_w = Concat(graph, {'name': original_name + '/Concat_W_zrh',
-                                                   'axis': 1}).create_node()
-            concat_zrh_w.add_input_port(0)
-            concat_zrh_w.add_input_port(1)
-            concat_zrh_w.add_input_port(2)
+            concat_zrh_w = Concat(graph, {'name': original_name + '/Concat_W_zrh', 'in_ports_count': 3,
+                                          'axis': 1}).create_node()
 
             # W_r -> r at 1
             split_rz_w.out_port(0).connect(concat_zrh_w.in_port(1))
@@ -58,11 +55,8 @@ class GRUBlockCellToGRUCell(FrontReplacementPattern):
             # B_rz -> B_r, B_z
             tf_gru_block_cell.in_port(4).get_connection().set_destination(split_rz_b.in_port(0))
 
-            concat_zrh_b = Concat(graph, {'name': original_name + '/Concat_B_zrh',
+            concat_zrh_b = Concat(graph, {'name': original_name + '/Concat_B_zrh', 'in_ports_count': 3,
                                                    'axis': 0}).create_node()
-            concat_zrh_b.add_input_port(0)
-            concat_zrh_b.add_input_port(1)
-            concat_zrh_b.add_input_port(2)
 
             # B_r -> r at 1
             split_rz_b.out_port(0).connect(concat_zrh_b.in_port(1))
@@ -75,7 +69,7 @@ class GRUBlockCellToGRUCell(FrontReplacementPattern):
 
             permute_order = int64_array([1, 0])
             transpose_w = create_op_node_with_second_input(graph, Transpose, permute_order,
-                                                          dict(name=original_name + 'Transpose_W'), concat_zrh_w)
+                                                          dict(name=original_name + '/Transpose_W'), concat_zrh_w)
 
             transpose_w.out_port(0).connect(new_gru_cell.in_port(2))
             concat_zrh_b.out_port(0).connect(new_gru_cell.in_port(3))
