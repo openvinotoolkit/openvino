519e97ac7bd42d35fc895e3cc48bfdd67e3549db
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2022-08-15 12:59:54 +0400

    [GPU] Fixed invalid access to input layout in impl map (#12533)

--- src/plugins/intel_gpu/src/graph/include/primitive_type_base.h
+++ src/plugins/intel_gpu/src/graph/include/primitive_type_base.h
@@ -22,17 +22,12 @@ template <class PType>
 struct primitive_type_base : primitive_type {
     std::shared_ptr<cldnn::program_node> create_node(program& program,
                                                      const std::shared_ptr<primitive> prim) const override {
-        if (prim->type != this)
-            throw std::invalid_argument("primitive_type_base::create_node: primitive type mismatch");
-
+        OPENVINO_ASSERT(prim->type == this, "[GPU] primitive_type_base::create_node: primitive type mismatch");
         return std::make_shared<typed_program_node<PType>>(std::static_pointer_cast<PType>(prim), program);
     }
 
-    std::shared_ptr<cldnn::primitive_inst> create_instance(network& network,
-                                                           const cldnn::program_node& node) const override {
-        if (node.type() != this)
-            throw std::invalid_argument("primitive_type_base::create_instance: primitive type mismatch");
-
+    std::shared_ptr<cldnn::primitive_inst> create_instance(network& network, const cldnn::program_node& node) const override {
+        OPENVINO_ASSERT(node.type() == this, "[GPU] primitive_type_base::create_instance: primitive type mismatch");
         return std::make_shared<typed_primitive_inst<PType>>(network, node);
     }
 
@@ -42,11 +37,9 @@ struct primitive_type_base : primitive_type {
     }
 
     std::unique_ptr<primitive_impl> choose_impl(const cldnn::program_node& node, const kernel_impl_params& runtime_params) const override {
-        if (node.type() != this)
-            throw std::invalid_argument("primitive_type_base::choose_impl: primitive type mismatch");
+        OPENVINO_ASSERT(node.type() == this, "[GPU] primitive_type_base::choose_impl: primitive type mismatch");
         auto factory = implementation_map<PType>::get(runtime_params, node.get_preferred_impl_type());
-        auto impl = std::unique_ptr<primitive_impl>(factory(node, runtime_params));
-        return impl;
+        return std::unique_ptr<primitive_impl>(factory(node, runtime_params));
     }
 
     bool does_an_implementation_exist(const cldnn::program_node& node) const override {
@@ -54,10 +47,8 @@ struct primitive_type_base : primitive_type {
     }
 
     bool does_an_implementation_exist(const cldnn::program_node& node, const kernel_impl_params& impl_param) const override {
-        if (node.type() != this)
-            throw std::invalid_argument("primitive_type_base::does_an_implementation_exist: primitive type mismatch");
-
-        return implementation_map<PType>::check(node, impl_param);
+        OPENVINO_ASSERT(node.type() == this, "[GPU] primitive_type_base::does_an_implementation_exist: primitive type mismatch");
+        return implementation_map<PType>::check(impl_param, node.get_preferred_impl_type());
     }
 
     bool does_possible_implementation_exist(const cldnn::program_node& node) const override {
@@ -65,22 +56,17 @@ struct primitive_type_base : primitive_type {
     }
 
     bool does_possible_implementation_exist(const cldnn::program_node& node, const kernel_impl_params& impl_param) const override {
-        if (node.type() != this)
-            throw std::invalid_argument("primitive_type_base::does_possible_implementation_exist: primitive type mismatch");
-        return implementation_map<PType>::check_io_eq(node, impl_param);
+        OPENVINO_ASSERT(node.type() == this, "[GPU] primitive_type_base::does_possible_implementation_exist: primitive type mismatch");
+        return implementation_map<PType>::check_io_eq(impl_param, node.get_preferred_impl_type());
     }
 
     cldnn::layout calc_output_layout(const cldnn::program_node& node, const kernel_impl_params& impl_param) const override {
-        if (node.type() != this)
-            throw std::invalid_argument("primitive_type_base::calc_output_layout: primitive type mismatch");
-
+        OPENVINO_ASSERT(node.type() == this, "[GPU] primitive_type_base::calc_output_layout: primitive type mismatch");
         return typed_primitive_inst<PType>::calc_output_layout(node, impl_param);
     }
 
     std::string to_string(const cldnn::program_node& node) const override {
-        if (node.type() != this)
-            throw std::invalid_argument("primitive_type_base::to_string: primitive type mismatch");
-
+        OPENVINO_ASSERT(node.type() == this, "[GPU] primitive_type_base::to_string: primitive type mismatch");
         return typed_primitive_inst<PType>::to_string(node);
     }
 };
