73eeee940cee596b715df6237733039c3f3b692d
Wang, Yang <yang4.wang@intel.com>
2022-08-17 14:30:53 +0800

    Update.
    Signed-off-by: Wang, Yang <yang4.wang@intel.com>

--- src/tests/functional/plugin/shared/src/behavior/ov_plugin/properties_tests.cpp
+++ src/tests/functional/plugin/shared/src/behavior/ov_plugin/properties_tests.cpp
@@ -146,12 +146,12 @@ TEST_P(OVSetPropComplieModleGetPropTests, SetPropertyComplieModelGetProperty) {
 }
 
 TEST_P(OVSetPropComplieModleWihtIncorrectPropTests, SetPropertyComplieModelWithIncorrectProperty) {
-    OV_ASSERT_NO_THROW(core->set_property(device_name, properties));
-    ASSERT_THROW(core->compile_model(model, device_name, compileModelProperties), ov::Exception);
+    OV_ASSERT_NO_THROW(core->set_property(target_device, properties));
+    ASSERT_THROW(core->compile_model(model, target_device, compileModelProperties), ov::Exception);
 }
 
 TEST_P(OVSetPropComplieModleWihtIncorrectPropTests, CanNotCompileModelWithIncorrectProperties) {
-    ASSERT_THROW(core->compile_model(model, device_name, properties), ov::Exception);
+    ASSERT_THROW(core->compile_model(model, target_device, properties), ov::Exception);
 }
 
 }  // namespace behavior
