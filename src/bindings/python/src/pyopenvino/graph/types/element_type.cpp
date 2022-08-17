61791e6cf4b0fb028e2c9832696e5a88ba5f3012
jiwaszki <jan.iwaszkiewicz@intel.com>
2022-08-17 21:40:16 +0000

    Adjust __repr__ for Type

--- src/bindings/python/src/pyopenvino/graph/types/element_type.cpp
+++ src/bindings/python/src/pyopenvino/graph/types/element_type.cpp
@@ -51,13 +51,12 @@ void regclass_graph_Type(py::module m) {
 
     type.def("__hash__", &ov::element::Type::hash);
     type.def("__repr__", [](const ov::element::Type& self) {
-        std::string bitwidth = std::to_string(self.bitwidth());
-        if (self == ov::element::undefined) {
-            return "<Type: '" + self.c_type_string() + "'>";
-        } else if (self.is_signed()) {
+        if (self == ov::element::f32 || self == ov::element::f64) {
+            std::string bitwidth = std::to_string(self.bitwidth());
             return "<Type: '" + self.c_type_string() + bitwidth + "'>";
         }
-        return "<Type: 'u" + self.c_type_string() + bitwidth + "'>";
+
+        return "<Type: '" + self.c_type_string() + "'>";
     });
     type.def(
         "__eq__",
