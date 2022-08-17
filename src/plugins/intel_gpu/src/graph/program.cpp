4fb2d48269543339b67a3c66de7f1f456669824a
ozhydkov-lohika <ozhydkov@lohika.com>
2022-08-17 14:35:47 +0300

    add tile blocked layouts support

--- src/plugins/intel_gpu/src/graph/program.cpp
+++ src/plugins/intel_gpu/src/graph/program.cpp
@@ -1451,6 +1451,7 @@ void program::set_layout_optimizer_attributes(layout_optimizer& lo) {
             prim.type() != cldnn::generate_proposals::type_id() &&
             prim.type() != cldnn::reverse::type_id() &&
             prim.type() != cldnn::reorg_yolo::type_id() &&
+            prim.type() != cldnn::tile::type_id() &&
             prim.type() != cldnn::scatter_elements_update::type_id() &&
             prim.type() != cldnn::experimental_detectron_detection_output::type_id()) {
             can_use_fsv16 = false;
@@ -1494,6 +1495,7 @@ void program::set_layout_optimizer_attributes(layout_optimizer& lo) {
             prim.type() != cldnn::generate_proposals::type_id() &&
             prim.type() != cldnn::reverse::type_id() &&
             prim.type() != cldnn::reorg_yolo::type_id() &&
+            prim.type() != cldnn::tile::type_id() &&
             prim.type() != cldnn::scatter_elements_update::type_id() &&
             prim.type() != cldnn::experimental_detectron_detection_output::type_id() &&
             prim.type() != cldnn::deconvolution::type_id()) {
