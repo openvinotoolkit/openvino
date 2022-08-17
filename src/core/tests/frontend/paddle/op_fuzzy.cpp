37f8188a4b9726afe645f3c7b274ba233a991dc7
Asthestarsfalll <1186454801@qq.com>
2022-08-17 17:10:12 +0800

    add more test cases and support boolen

--- src/core/tests/frontend/paddle/op_fuzzy.cpp
+++ src/core/tests/frontend/paddle/op_fuzzy.cpp
@@ -411,6 +411,8 @@ static const std::vector<std::string> models{
     std::string("where_index_2"),
     std::string("where_index_3"),
     std::string("where_index_4"),
+    std::string("where_index_5"),
+    std::string("where_index_6"),
     // Temporily disable them until root caused to secure CI stable.
     // CVS-66703 to track this.
     // std::string("yolo_box_clip_box"),
--- src/core/tests/frontend/paddle/test_models/gen_scripts/generate_where_index.py
+++ src/core/tests/frontend/paddle/test_models/gen_scripts/generate_where_index.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 #
-# where_index paddle model generator
+# where paddle model generator
 #
 import numpy as np
 from save_model import saveModel
@@ -14,7 +14,7 @@ paddle.enable_static()
 
 
 def where_index_ref(x, name=None):
-    helper = LayerHelper('p_norm', **locals())
+    helper = LayerHelper('where_index', **locals())
     out = helper.create_variable_for_type_inference(dtype=helper.input_dtype())
     helper.append_op(type='where_index',
                      inputs={'Condition': x},
@@ -22,11 +22,15 @@ def where_index_ref(x, name=None):
     return out
 
 
-def where_index(name: str, x):
+def where_index(name: str, x, force_boolean=False):
     with paddle.static.program_guard(paddle.static.Program(), paddle.static.Program()):
         node_x = paddle.static.data(name='x', shape=x.shape, dtype=x.dtype)
+        if force_boolean:
+            node_x_bl = paddle.fluid.layers.cast(node_x, "bool")
+            out = where_index_ref(node_x_bl)
+        else:
+            out = where_index_ref(node_x)
 
-        out = where_index_ref(node_x)
         cpu = paddle.static.cpu_places(1)
         exe = paddle.static.Executor(cpu[0])
 
@@ -48,19 +52,24 @@ def main():
 
     # case of float32
     datatype = "float32"
-    condition = (np.random.randint(
-        0, 5, size=[8, 3, 2]) * 1.1).astype(datatype)
+    condition = (np.random.randint(0, 5, size=[8, 3, 2]) * 1.1).astype(datatype)
     paddle_out = where_index("where_index_2", condition)
 
     # case of dimension 4
-    condition = (np.random.randint(
-        -1, 2, size=[8, 3, 2, 6]) * 1.1).astype(datatype)
+    condition = (np.random.randint(0, 5, size=[8, 3, 2, 6]) * 1.1).astype(datatype)
     paddle_out = where_index("where_index_3", condition)
 
     # case of dimension 5
     condition = (np.random.randint(0, 5, size=[4, 6, 8, 2, 5]) * 1.1).astype(datatype)
     paddle_out = where_index("where_index_4", condition)
 
+    # case of rank 1
+    condition = np.ones(10).astype(datatype)
+    paddle_out = where_index("where_index_5", condition, force_boolean=True)
+
+    # case of rank 1 and boolean zeros
+    condition = np.array([1, 0, 1]).astype(datatype)
+    paddle_out = where_index("where_index_6", condition, force_boolean=True)
 
 if __name__ == "__main__":
     main()
