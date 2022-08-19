2af8a44830ae9ce0025158a7fbda206845f1eb8d
Roman Kazantsev <roman.kazantsev@intel.com>
2022-08-19 11:44:41 +0300

    [TF FE] Implement translators for ExtractImagePatches and MatrixDiag (#12593)
    * [TF FE] Implement translators for ExtractImagePatches and MatrixDiag
    
    It allows to convert Inpaint model and infer it correctly
    
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>
    
    * Apply code-review feedback: correct comments, use set
    
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Maxim Vafin <maxim.vafin@intel.com>
    
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>
    Co-authored-by: Maxim Vafin <maxim.vafin@intel.com>

--- src/frontends/tensorflow/src/op/placeholder.cpp
+++ src/frontends/tensorflow/src/op/placeholder.cpp
@@ -14,13 +14,25 @@ namespace tensorflow {
 namespace op {
 
 OutputVector translate_placeholder_op(const NodeContext& node) {
-    auto ng_et = node.get_attribute<ov::element::Type>("dtype");
-    auto ng_shape = node.get_attribute<ov::PartialShape>("shape", ov::PartialShape());
+    auto tf_dtype = node.get_attribute<ov::element::Type>("dtype");
+    auto tf_shape = node.get_attribute<ov::PartialShape>("shape", ov::PartialShape::dynamic());
 
-    auto res = std::make_shared<Parameter>(ng_et, ng_shape);
+    auto res = std::make_shared<Parameter>(tf_dtype, tf_shape);
     set_node_name(node.get_name(), res);
     return res->outputs();
 }
+
+OutputVector translate_placeholder_with_default_op(const NodeContext& node) {
+    // For parity with legacy frontend, it creates a constant node with the default value
+    // As a rule, PlaceholderWithDefault is mainly used for is_training variables in the model
+    TENSORFLOW_OP_VALIDATION(node,
+                             node.get_input_size() > 0,
+                             "PlaceholderWithDefault must have at least one input that is the default value.");
+    auto input = node.get_input(0);
+    set_out_name(node.get_name(), input);
+    return {input};
+}
+
 }  // namespace op
 }  // namespace tensorflow
 }  // namespace frontend
