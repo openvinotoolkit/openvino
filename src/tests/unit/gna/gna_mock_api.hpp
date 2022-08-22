94bca6210d0286247a59b519e0bf4450d83bc2f9
Nadezhda Ageeva <nadezhda.ageeva@intel.com>
2022-08-22 16:34:24 +0400

    Commits from PR11438 (#12337)
    Draft version of test
    
    Added i8 test
    
    Some fixes
    
    Fix static build issue

--- src/tests/unit/gna/gna_mock_api.hpp
+++ src/tests/unit/gna/gna_mock_api.hpp
@@ -4,6 +4,8 @@
 
 #pragma once
 #include <gmock/gmock-generated-function-mockers.h>
+#include <gna2-instrumentation-api.h>
+#include <gna2-model-export-api.h>
 
 #if defined(_WIN32)
     #ifdef libGNAStubs_EXPORTS
@@ -24,7 +26,38 @@ public:
         uint32_t sizeRequested,
         uint32_t * sizeGranted,
         void ** memoryAddress));
+
+    MOCK_METHOD1(Gna2DeviceOpen, Gna2Status(
+        uint32_t deviceIndex));
+
+    MOCK_METHOD3(Gna2ModelCreate, Gna2Status(
+        uint32_t deviceIndex,
+        struct Gna2Model const * model,
+        uint32_t * modelId));
+
+    MOCK_METHOD2(Gna2RequestConfigCreate, Gna2Status(
+        uint32_t modelId,
+        uint32_t * requestConfigId));
+
     MOCK_METHOD2(Gna2RequestWait, Gna2Status(
         uint32_t requestId,
         uint32_t timeoutMilliseconds));
+
+    MOCK_METHOD2(Gna2DeviceGetVersion, Gna2Status(
+        uint32_t deviceIndex,
+        enum Gna2DeviceVersion * deviceVersion));
+
+    MOCK_METHOD4(Gna2InstrumentationConfigCreate, Gna2Status(
+        uint32_t numberOfInstrumentationPoints,
+        enum Gna2InstrumentationPoint* selectedInstrumentationPoints,
+        uint64_t * results,
+        uint32_t * instrumentationConfigId));
+
+    MOCK_METHOD2(Gna2InstrumentationConfigAssignToRequestConfig, Gna2Status(
+        uint32_t instrumentationConfigId,
+        uint32_t requestConfigId));
+
+    MOCK_METHOD2(Gna2GetLibraryVersion, Gna2Status(
+        char* versionBuffer,
+        uint32_t versionBufferSize));
 };
