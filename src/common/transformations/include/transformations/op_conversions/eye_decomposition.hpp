bc9d60e3124cdae3e7a2586997af30959e8e76bb
Raasz, Pawel <pawel.raasz@intel.com>
2022-08-22 08:34:20 +0000

    Refactor eye decomposition: - match style of other transformations - add NodeRegister class to make and collect created nodes - use `NodeRegister` in transformation for copy runtime info - use `NodeRegister` in `MatcherPass` to replace new `register_new_node`

--- src/common/transformations/include/transformations/op_conversions/eye_decomposition.hpp
+++ src/common/transformations/include/transformations/op_conversions/eye_decomposition.hpp
@@ -10,9 +10,9 @@
 namespace ov {
 namespace pass {
 
-    class TRANSFORMATIONS_API EyeDecomposition;
-}
-}
+class TRANSFORMATIONS_API EyeDecomposition;
+}  // namespace pass
+}  // namespace ov
 
 /**
  * @ingroup ie_transformation_common_api
@@ -23,32 +23,4 @@ class ov::pass::EyeDecomposition : public MatcherPass {
 public:
     OPENVINO_RTTI("EyeDecomposition", "0");
     EyeDecomposition();
-
-protected:
-    /**
-     * @brief Make eye model which generate eye matrix.
-     *
-     * If 'k' is outside the eye dimension then result matrix will be filled with zeros.
-     *
-     * @param height  Height of eye
-     * @param width   Width of eye
-     * @param k       Eye diagonal shift.
-     * @param dtype   Data type of eye.
-     *
-     * @return Pointer to decomposed eye model.
-     */
-    std::shared_ptr<Node> make_eye_model(const Output<Node>& height,
-                                         const Output<Node>& width,
-                                         const Output<Node>& k,
-                                         element::Type dtype);
-
-    /**
-     * @brief Make eye model as basic 2D eye replicated as spcified in batch size.
-     *
-     * @param eye    Eye model
-     * @param batch  1-D tensor which defines leading batch dimensions of output eye shape.
-     *
-     * @return Pointer to decomposed eye model.
-     */
-    std::shared_ptr<Node> make_eye_batches(const Output<Node>& eye, const Output<Node>& batch);
 };
