77599851c1e0d702a9cca17ab7da3f9fd4288ba6
Kazantsev, Roman <roman.kazantsev@intel.com>
2022-08-23 20:28:58 +0400

    Align TF FE tranlator for Resize with legacy frontend
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/frontends/tensorflow/src/op/interpolate.cpp
+++ src/frontends/tensorflow/src/op/interpolate.cpp
@@ -16,21 +16,43 @@ ov::OutputVector translate_interpolate_op(const NodeContext& node) {
     default_op_checks(node, 2, {"ResizeBilinear", "ResizeNearestNeighbor"});
     auto images = node.get_input(0);
     auto size = node.get_input(1);
+    auto op_name = node.get_name();
+    auto op_type = node.get_op_type();
 
     // retrieve optional attribute
     auto tf_align_corners = node.get_attribute<bool>("align_corners", false);
+    auto tf_half_pixel_centers = node.get_attribute<bool>("half_pixel_centers", false);
+
+    TENSORFLOW_OP_VALIDATION(node,
+                             !tf_half_pixel_centers || (tf_half_pixel_centers && !tf_align_corners),
+                             "If half_pixel_centers attribute of the node" + op_name + " with op " + op_type +
+                                 " is True, the attribute align_corners must be False.");
 
     // prepare attributes for OpenVINO Interpolate operation
     Interpolate::InterpolateAttrs interpolate_attrs;
-    interpolate_attrs.mode = Interpolate::InterpolateMode::LINEAR;
     interpolate_attrs.shape_calculation_mode = Interpolate::ShapeCalcMode::SIZES;
-    if (tf_align_corners) {
-        interpolate_attrs.coordinate_transformation_mode = Interpolate::CoordinateTransformMode::ALIGN_CORNERS;
+    if (op_type == "ResizeNearestNeighbor") {
+        interpolate_attrs.mode = Interpolate::InterpolateMode::NEAREST;
+        interpolate_attrs.nearest_mode = Interpolate::NearestMode::FLOOR;
+    } else if (op_type == "ResizeBilinear") {
+        interpolate_attrs.mode = Interpolate::InterpolateMode::LINEAR;
+        interpolate_attrs.nearest_mode = Interpolate::NearestMode::ROUND_PREFER_FLOOR;
     }
 
-    if (node.get_op_type() == "ResizeNearestNeighbor") {
-        interpolate_attrs.mode = Interpolate::InterpolateMode::NEAREST;
-        interpolate_attrs.nearest_mode = Interpolate::NearestMode::ROUND_PREFER_FLOOR;
+    if (tf_align_corners) {
+        interpolate_attrs.coordinate_transformation_mode = Interpolate::CoordinateTransformMode::ALIGN_CORNERS;
+        if (interpolate_attrs.mode == Interpolate::InterpolateMode::NEAREST) {
+            interpolate_attrs.nearest_mode = Interpolate::NearestMode::ROUND_PREFER_CEIL;
+        }
+    } else if (tf_half_pixel_centers) {
+        if (interpolate_attrs.mode == Interpolate::InterpolateMode::NEAREST) {
+            interpolate_attrs.coordinate_transformation_mode =
+                Interpolate::CoordinateTransformMode::TF_HALF_PIXEL_FOR_NN;
+        } else {
+            interpolate_attrs.coordinate_transformation_mode = Interpolate::CoordinateTransformMode::HALF_PIXEL;
+        }
+    } else {
+        interpolate_attrs.coordinate_transformation_mode = Interpolate::CoordinateTransformMode::ASYMMETRIC;
     }
 
     // prepare scales input
