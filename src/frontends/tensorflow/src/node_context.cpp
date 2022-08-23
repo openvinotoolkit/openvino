55d71a26f2ddc28c4a6ac45190fc540888d9671f
Maxim Vafin <maxim.vafin@intel.com>
2022-08-23 15:11:14 +0200

    Improve error logging

---
+++ src/frontends/tensorflow/src/node_context.cpp
@@ -0,0 +1,20 @@
+// Copyright (C) 2018-2022 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "openvino/frontend/tensorflow/node_context.hpp"
+
+using namespace ov::frontend::tensorflow;
+
+ ov::Any NodeContext::apply_additional_conversion_rules(const ov::Any& data,
+                                                     const std::type_info& type_info) const override {
+    if (data.is<EmptyList>(){
+        if (type_info == typeid(std::vector<int64_t>)) {
+            return std::vector<int64_t>();
+        } else {
+            FRONT_END_GENERAL_CHECK(false, "Could not decode empty list attribute. Provided type is not known.");
+        }
+    }
+    // no conversion rules found
+    return data;
+}
