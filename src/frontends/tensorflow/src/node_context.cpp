55ea90777c5a64a2c8d1346eceaa3386a905ab88
Maxim Vafin <maxim.vafin@intel.com>
2022-08-23 15:15:31 +0200

    Improve formatting

--- src/frontends/tensorflow/src/node_context.cpp
+++ src/frontends/tensorflow/src/node_context.cpp
@@ -7,12 +7,15 @@
 using namespace ov::frontend::tensorflow;
 
  ov::Any NodeContext::apply_additional_conversion_rules(const ov::Any& data,
-                                                     const std::type_info& type_info) const override {
+                                                       const std::type_info& type_info) const override {
     if (data.is<EmptyList>(){
         if (type_info == typeid(std::vector<int64_t>)) {
             return std::vector<int64_t>();
         } else {
-            FRONT_END_GENERAL_CHECK(false, "Could not decode empty list attribute. Provided type is not known.");
+            FRONT_END_GENERAL_CHECK(false,
+                                    "Could not decode empty list attribute for ",
+                                    get_name(),
+                                    " node. Provided type is not known.");
         }
     }
     // no conversion rules found
