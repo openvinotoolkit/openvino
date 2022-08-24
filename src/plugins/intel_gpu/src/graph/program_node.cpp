ce486009aed7bccab2d23c74df6afd079c4929ea
hyunback <hyunback.kim@intel.com>
2022-08-24 17:30:57 +0900

    Add comment
    Signed-off-by: hyunback <hyunback.kim@intel.com>

--- src/plugins/intel_gpu/src/graph/program_node.cpp
+++ src/plugins/intel_gpu/src/graph/program_node.cpp
@@ -964,6 +964,7 @@ void program_node::init_onednn_primitive_attributes() {
             } else {
                 if (in.spatial(0) > 1 || in.spatial(1) > 1 || in.batch() > 1)
                     throw std::runtime_error("Unsupported eltwise mode for fused onednn op");
+                // convolution using post-op output scales can only be int8/uint8
                 if (idx == 0 && !has_out_scales(attrs) && !is_type<pooling>() && !is_type<reduce>() &&
                     !(is_type<convolution>() && data_type_traits::is_floating_point(output_layout.data_type))) {
                     int mask = in.count() > 1 ? 2 : 0;
