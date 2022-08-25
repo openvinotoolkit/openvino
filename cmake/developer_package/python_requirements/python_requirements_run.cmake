2bbbcf11d7770de76467d9a7dade56a4253ebe48
Ilya Lavrenov <ilya.lavrenov@intel.com>
2022-08-25 18:15:56 +0800

    Fixes

--- cmake/developer_package/python_requirements/python_requirements_run.cmake
+++ cmake/developer_package/python_requirements/python_requirements_run.cmake
@@ -5,7 +5,6 @@
 
 execute_process(
     COMMAND "${PYTHON_EXECUTABLE}" -m pip install --upgrade pip
-    COMMAND "${PYTHON_EXECUTABLE}" -m pip install -r "${REQUIREMENTS_FILE}"
     RESULT_VARIABLE result_code
     OUTPUT_VARIABLE output_var
     ERROR_VARIABLE error_var)
@@ -13,5 +12,18 @@ execute_process(
 if(result_code EQUAL 0)
     file(WRITE "${OUTPUT_FILE}" "${output_var}")
 else()
-    message(FATAL_ERROR "pip install command failed: ${error_var}")
+    message(WARNING "Failed to upgrade pip: ${error_var} ${output_var}")
+endif()
+
+execute_process(
+    COMMAND "${PYTHON_EXECUTABLE}" -m pip install --upgrade pip
+    COMMAND "${PYTHON_EXECUTABLE}" -m pip install -r "${REQUIREMENTS_FILE}"
+    RESULT_VARIABLE result_code
+    OUTPUT_VARIABLE output_var
+    ERROR_VARIABLE error_var)
+
+if(result_code EQUAL 0)
+    file(APPEND "${OUTPUT_FILE}" "${output_var}")
+else()
+    message(WARNING "Failed to install ${REQUIREMENTS_FILE}: ${error_var} ${output_var}")
 endif()
