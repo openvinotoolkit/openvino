1a5016d9fb1b7409e8d48be6a8ba7371b707a8a1
Lyamin-Roman <roman.lyamin@intel.com>
2022-08-26 00:36:13 +0900

    [GPU] fully_connected/gemm shape infer support

--- src/plugins/intel_gpu/src/graph/fully_connected.cpp
+++ src/plugins/intel_gpu/src/graph/fully_connected.cpp
@@ -9,6 +9,8 @@
 #include "json_object.h"
 #include <string>
 
+#include "matmul_shape_inference.hpp"
+
 namespace cldnn {
 primitive_type_id fully_connected::type_id() {
     static primitive_type_base<fully_connected> instance;
@@ -39,7 +41,7 @@ bool is_batch_after_spatial(const std::string order) {
     return false;
 }
 
-format::type get_preferred_format(const fully_connected_node& node, const kernel_impl_params& impl_param) {
+format::type get_preferred_format(const kernel_impl_params& impl_param) {
     auto input_layout = impl_param.get_input_layout();
 
     // for 3d output we have to chose bfyx format
@@ -105,9 +107,39 @@ layout fully_connected_inst::calc_output_layout(fully_connected_node const& node
     if (desc->input_size == 3) {
         output_size = tensor(input_layout.batch(), input_layout.feature(), 1, weights_layout.batch());
     }
-    format output_format = get_preferred_format(node, impl_param);
+    format output_format = get_preferred_format(impl_param);
 
     return layout(output_type, output_format, output_size);
+}
+
+template<typename ShapeType>
+std::vector<layout> fully_connected_inst::calc_output_layouts(fully_connected_node const& /*node*/, const kernel_impl_params& impl_param) {
+    auto desc = impl_param.typed_desc<fully_connected>();
+    auto input_layout = impl_param.get_input_layout();
+    auto weights_layout = *impl_param.weights_layout;
+
+    auto default_out_dt = data_type_traits::is_floating_point(input_layout.data_type) ? input_layout.data_type : data_types::f32;
+    auto output_type = desc->output_data_type.value_or(default_out_dt);
+
+    if (impl_param.has_fused_primitives()) {
+        output_type = impl_param.get_fused_output_layout().data_type;
+    }
+
+    ov::op::v0::MatMul op;
+    std::vector<ShapeType> output_shapes = {ShapeType()};
+    std::vector<ShapeType> input_shapes = {
+        input_layout.get<ShapeType>(),
+        weights_layout.get<ShapeType>()
+    };
+
+    ov::op::v0::shape_infer(&op, input_shapes, output_shapes);
+
+    bool is_static = input_layout.is_static() && weights_layout.is_static();
+
+    format::type output_format = is_static ? get_preferred_format(impl_param) :
+                                             input_layout.format.value;
+
+    return { layout{output_shapes[0], output_type, output_format} };
 }
 
 std::string fully_connected_inst::to_string(fully_connected_node const& node) {
