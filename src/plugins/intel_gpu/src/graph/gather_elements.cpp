07d010797951c7afbac43a021f31282f0ca4b28b
Roman Lyamin <Roman.Lyamin@intel.com>
2022-08-25 09:38:53 +0400

    [GPU] GatherElements shape infer (#12517)

--- src/plugins/intel_gpu/src/graph/gather_elements.cpp
+++ src/plugins/intel_gpu/src/graph/gather_elements.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "gather_elements_inst.h"
+#include "gather_elements_shape_inference.hpp"
 
 #include "primitive_type_base.h"
 #include "intel_gpu/runtime/error_handler.hpp"
@@ -30,6 +31,31 @@ layout gather_elements_inst::calc_output_layout(gather_elements_node const& node
     auto output_format = op->output_format;
     // calculate initial output shape
     return layout(output_type, output_format, output_shape);
+}
+
+template<typename ShapeType>
+std::vector<layout> gather_elements_inst::calc_output_layouts(gather_elements_node const& /*node*/, const kernel_impl_params& impl_param) {
+    auto desc = impl_param.typed_desc<gather_elements>();
+    auto input_layout = impl_param.get_input_layout(0);
+
+    auto output_type = input_layout.data_type;
+    if (impl_param.has_fused_primitives()) {
+        output_type = impl_param.get_fused_output_layout().data_type;
+    }
+
+    ov::op::v6::GatherElements op;
+    op.set_axis(desc->axis);
+
+    std::vector<ShapeType> output_shapes = {ShapeType()};
+    std::vector<ShapeType> input_shapes = {
+        impl_param.get_input_layout(0).get<ShapeType>(),
+        impl_param.get_input_layout(1).get<ShapeType>()
+    };
+    ov::op::v6::shape_infer(&op, input_shapes, output_shapes);
+
+    format output_format = format::adjust_to_rank(input_layout.format, output_shapes[0].size());
+
+    return { layout{output_shapes[0], output_type, output_format} };
 }
 
 std::string gather_elements_inst::to_string(gather_elements_node const& node) {
