753506bcdf8c44e421a0836a592664a0411abe96
Ilya Churaev <ilya.churaev@intel.com>
2022-08-30 14:06:57 +0400

    Enabled in-place evaluate for identity (#12813)

--- docs/template_extension/new/identity.cpp
+++ docs/template_extension/new/identity.cpp
@@ -37,6 +37,8 @@ bool Identity::visit_attributes(ov::AttributeVisitor& visitor) {
 bool Identity::evaluate(ov::TensorVector& outputs, const ov::TensorVector& inputs) const {
     auto in = inputs[0];
     auto out = outputs[0];
+    if (out.data() == in.data())  // Nothing to do
+        return true;
     out.set_shape(in.get_shape());
     memcpy(out.data(), in.data(), in.get_byte_size());
     return true;
