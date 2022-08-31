7f665502b622eb07b85168f400f3fafceb096142
Andrew Kwangwoong Park <andrew.park@intel.com>
2022-09-01 08:29:41 +0900

    [GPU] Baseline InferRequest changes for dynamic shapes support (#12415)

--- src/plugins/intel_gpu/src/plugin/async_infer_request_legacy.cpp
+++ src/plugins/intel_gpu/src/plugin/async_infer_request_legacy.cpp
@@ -2,17 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "intel_gpu/plugin/async_infer_request.hpp"
+#include "intel_gpu/plugin/async_infer_request_legacy.hpp"
 #include "intel_gpu/plugin/itt.hpp"
 #include <memory>
 
 namespace ov {
 namespace intel_gpu {
 
-AsyncInferRequest::AsyncInferRequest(const InferRequest::Ptr &inferRequest,
-                                     const InferenceEngine::ITaskExecutor::Ptr& taskExecutor,
-                                     const InferenceEngine::ITaskExecutor::Ptr& waitExecutor,
-                                     const InferenceEngine::ITaskExecutor::Ptr& callbackExecutor)
+AsyncInferRequestLegacy::AsyncInferRequestLegacy(const InferRequestLegacy::Ptr &inferRequest,
+                                                 const InferenceEngine::ITaskExecutor::Ptr& taskExecutor,
+                                                 const InferenceEngine::ITaskExecutor::Ptr& waitExecutor,
+                                                 const InferenceEngine::ITaskExecutor::Ptr& callbackExecutor)
     : AsyncInferRequestThreadSafeDefault(inferRequest, taskExecutor, callbackExecutor), _inferRequest(inferRequest), _waitExecutor(waitExecutor) {
     _pipeline = {};
 
@@ -21,6 +21,7 @@ AsyncInferRequest::AsyncInferRequest(const InferRequest::Ptr &inferRequest,
                     [this] {
                         OV_ITT_SCOPED_TASK(itt::domains::intel_gpu_plugin, "AsyncInferRequest::PreprocessingAndStartPipeline");
                         _inferRequest->setup_stream_graph();
+                        _inferRequest->preprocess();
                         _inferRequest->enqueue();
                         _inferRequest->wait();
         } });
@@ -33,23 +34,25 @@ AsyncInferRequest::AsyncInferRequest(const InferRequest::Ptr &inferRequest,
     }
 }
 
-void AsyncInferRequest::Infer_ThreadUnsafe() {
+void AsyncInferRequestLegacy::Infer_ThreadUnsafe() {
     if (_inferRequest->use_external_queue()) {
         _inferRequest->setup_stream_graph();
+        _inferRequest->preprocess_notify();
         _inferRequest->enqueue_notify();
     }
     Parent::Infer_ThreadUnsafe();
 }
 
-void AsyncInferRequest::StartAsync_ThreadUnsafe() {
+void AsyncInferRequestLegacy::StartAsync_ThreadUnsafe() {
     if (_inferRequest->use_external_queue()) {
         _inferRequest->setup_stream_graph();
+        _inferRequest->preprocess_notify();
         _inferRequest->enqueue_notify();
     }
     Parent::StartAsync_ThreadUnsafe();
 }
 
-AsyncInferRequest::~AsyncInferRequest() {
+AsyncInferRequestLegacy::~AsyncInferRequestLegacy() {
     StopAndWait();
 }
 
