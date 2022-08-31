81039ebe555d7ce5f6bbe092460e0193e2bfae82
p-wysocki <przemyslaw.wysocki@intel.com>
2022-08-31 17:39:06 +0200

    Revert "Fix ONNX compatibility and numpy warnings"
    This reverts commit cd8a0e6b8e68b1f5653a7204f6c6940ce1c38a47.

--- src/bindings/python/tests/test_graph/test_utils.py
+++ src/bindings/python/tests/test_graph/test_utils.py
@@ -8,8 +8,9 @@ from openvino.runtime import Shape
 
 
 def test_get_constant_from_source_success():
-    input1 = ov.opset8.parameter(Shape([5, 5]), dtype=int, name="input_1")
-    input2 = ov.opset8.parameter(Shape([25]), dtype=int, name="input_2")
+    dtype = np.int
+    input1 = ov.opset8.parameter(Shape([5, 5]), dtype=dtype, name="input_1")
+    input2 = ov.opset8.parameter(Shape([25]), dtype=dtype, name="input_2")
     shape_of = ov.opset8.shape_of(input2, name="shape_of")
     reshape = ov.opset8.reshape(input1, shape_of, special_zero=True)
     folded_const = ov.utils.get_constant_from_source(reshape.input(1).get_source_output())
@@ -19,8 +20,9 @@ def test_get_constant_from_source_success():
 
 
 def test_get_constant_from_source_failed():
-    input1 = ov.opset8.parameter(Shape([5, 5]), dtype=int, name="input_1")
-    input2 = ov.opset8.parameter(Shape([1]), dtype=int, name="input_2")
+    dtype = np.int
+    input1 = ov.opset8.parameter(Shape([5, 5]), dtype=dtype, name="input_1")
+    input2 = ov.opset8.parameter(Shape([1]), dtype=dtype, name="input_2")
     reshape = ov.opset8.reshape(input1, input2, special_zero=True)
     folded_const = ov.utils.get_constant_from_source(reshape.input(1).get_source_output())
 
