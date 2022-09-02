7134814919574bd3712b3d97320ef386bb7b892b
vitaliy.mostovy <vitaliy.mostovy@altran.com>
2022-09-02 10:59:23 +0300

    [CPU] Refactor MultiClassNms operation

--- src/plugins/intel_cpu/src/nodes/multiclass_nms.hpp
+++ src/plugins/intel_cpu/src/nodes/multiclass_nms.hpp
@@ -7,17 +7,9 @@
 #include <ie_common.h>
 #include <node.h>
 
-#include <string>
-
 namespace ov {
 namespace intel_cpu {
 namespace node {
-
-enum class MulticlassNmsSortResultType {
-    CLASSID,  // sort selected boxes by class id (ascending) in each batch element
-    SCORE,    // sort selected boxes by score (descending) in each batch element
-    NONE      // do not guarantee the order in each batch element
-};
 
 class MultiClassNms : public Node {
 public:
@@ -38,76 +30,112 @@ public:
 
 private:
     // input (port Num)
-    const size_t NMS_BOXES = 0;
-    const size_t NMS_SCORES = 1;
-    const size_t NMS_ROISNUM = 2;
+    constexpr static size_t NMS_BOXES = 0;
+    constexpr static size_t NMS_SCORES = 1;
+    constexpr static size_t NMS_ROISNUM = 2;
 
     // output (port Num)
-    const size_t NMS_SELECTEDOUTPUTS = 0;
-    const size_t NMS_SELECTEDINDICES = 1;
-    const size_t NMS_SELECTEDNUM = 2;
+    constexpr static size_t NMS_SELECTEDOUTPUTS = 0;
+    constexpr static size_t NMS_SELECTEDINDICES = 1;
+    constexpr static size_t NMS_SELECTEDNUM = 2;
 
-    bool m_sortResultAcrossBatch = false;
-    MulticlassNmsSortResultType m_sortResultType = MulticlassNmsSortResultType::NONE;
+    std::string err_str_prefix_;
 
-    size_t m_numBatches = 0;
-    size_t m_numBoxes = 0;
-    size_t m_numClasses = 0;
-    size_t m_maxBoxesPerBatch = 0;
+private:
+    // Attributes
+    int attr_nms_top_k_ = 0;
+    float attr_iou_threshold_ = 0.0f;
+    float attr_score_threshold_ = 0.0f;
+    int attr_background_class_ = 0;
+    int attr_keep_top_k_ = 0;
+    float attr_nms_eta_ = 0.0f;
+    bool attr_normalized_ = true;
+    bool attr_sort_result_across_batch_ = false;
+    enum class SortResultType { CLASSID, SCORE, NONE };
+    SortResultType attr_sort_result_ = SortResultType::NONE;
 
-    int m_nmsRealTopk = 0;
-    int m_nmsTopK = 0;
-    float m_iouThreshold = 0.0f;
-    float m_scoreThreshold = 0.0f;
-
-    int32_t m_backgroundClass = 0;
-    int32_t m_keepTopK = 0;
-    float m_nmsEta = 0.0f;
-    bool m_normalized = true;
-
-    std::string m_errorPrefix;
-
-    std::vector<std::vector<size_t>> m_numFiltBox; // number of rois after nms for each class in each image
-    std::vector<size_t> m_numBoxOffset;
-    const std::string m_inType = "input", m_outType = "output";
-
-    struct filteredBoxes {
+    struct Box {
         float score;
-        int batch_index;
-        int class_index;
-        int box_index;
-        filteredBoxes() = default;
-        filteredBoxes(float _score, int _batch_index, int _class_index, int _box_index)
-            : score(_score), batch_index(_batch_index), class_index(_class_index), box_index(_box_index) {}
+        int batch_idx;
+        int class_idx;
+        int box_idx;
     };
 
-    struct boxInfo {
-        float score;
-        int idx;
-        int suppress_begin_index;
+    struct Buffer {
+        Box* boxes;
+        size_t size;
     };
 
-    std::vector<filteredBoxes> m_filtBoxes; // rois after nms for each class in each image
+    class Inputs {
+    public:
+        virtual ~Inputs() = default;
 
-    void checkPrecision(const InferenceEngine::Precision prec, const std::vector<InferenceEngine::Precision> precList, const std::string name,
-                        const std::string type);
+        enum class Format { first, second };
+        virtual Format format() const = 0;
+
+        virtual void prepareParams(const Node& node, const std::string& err_str) = 0;
+
+        virtual const float* boxes(const int batch_idx, const int class_idx, const float* boxes, const int* roisnum) const = 0;
+        virtual const float* scores(const int batch_idx, const int class_idx, const float* scores, const int* roisnum) const = 0;
+        virtual int num_input_boxes(const int batch_idx, const int* roisnum) const = 0;
+
+        virtual void locate(const Box& box, const int* roisnum, int& input_box_index, int& input_box_offset) = 0;
+
+        size_t num_batches() const { return num_batches_; }
+        size_t num_boxes() const { return num_boxes_; }
+        size_t num_classes() const { return num_classes_; }
+
+    protected:
+        size_t num_batches_ = 0;
+        size_t num_boxes_ = 0;
+        size_t num_classes_ = 0;
+    };
+    class InputsFormatFirst;
+    class InputsFormatSecond;
+    std::unique_ptr<Inputs> inputs_;
+
+    class Workbuffers {
+    public:
+        Workbuffers(size_t num_batches, size_t num_classes, size_t num_boxes);
+
+        std::vector<Box> boxes; // rois after nms for each class in each image
+        std::vector<std::vector<size_t>> num_boxes_per_batch_and_class; // number of rois after nms for each class in each image
+        std::vector<int> num_boxes_per_batch;
+
+        Box* thread_workspace_for(int batch_idx, int class_idx) {
+            return boxes.data() + batch_idx * num_classes_ * num_boxes_ + class_idx * num_boxes_;
+        }
+
+        Buffer flatten_all();
+        Buffer flatten_within_batch(int batch_idx);
+        Buffer flatten_batches();
+
+    private:
+        const size_t num_batches_;
+        const size_t num_classes_;
+        const size_t num_boxes_;
+    };
+    std::unique_ptr<Workbuffers> workbuffers_;
+
+    class OutputsLayout {
+    public:
+        void prepareParams(size_t num_classes, size_t num_boxes, int nms_top_k, int keep_top_k, int background_class);
+        int boxes_per_batch() const { return boxes_per_batch_; }
+    private:
+        int boxes_per_batch_ = 0;
+    };
+    OutputsLayout output_layout_;
 
     float intersectionOverUnion(const float* boxesI, const float* boxesJ, const bool normalized);
+    void multiclass_nms(const float* boxes, const float* scores, const int* roisnum);
 
-    void nmsWithEta(const float* boxes, const float* scores, const int* roisnum, const InferenceEngine::SizeVector& boxesStrides,
-                    const InferenceEngine::SizeVector& scoresStrides, const InferenceEngine::SizeVector& roisnumStrides, const bool shared);
+    static void partial_sort_by_score(Buffer buffer, int mid_size);
+    static void parallel_sort_by_score(Buffer buffer);
+    static void parallel_sort_by_score_across_batch(Buffer buffer);
+    static void parallel_sort_by_class(Buffer buffer);
+    static void parallel_sort_by_class_across_batch(Buffer buffer);
 
-    void nmsWithoutEta(const float* boxes, const float* scores, const int* roisnum, const InferenceEngine::SizeVector& boxesStrides,
-                       const InferenceEngine::SizeVector& scoresStrides, const InferenceEngine::SizeVector& roisnumStrides, const bool shared);
-
-    const float* slice_class(const int batch_idx,
-                            const int class_idx,
-                            const float* dataPtr,
-                            const InferenceEngine::SizeVector& dataStrides,
-                            const bool is_boxes,
-                            const int* roisnum,
-                            const InferenceEngine::SizeVector& roisnumStrides,
-                            const bool shared);
+    void fill_outputs(Buffer rois, const float* input_boxes, const int* input_roisnum);
 };
 
 }   // namespace node
