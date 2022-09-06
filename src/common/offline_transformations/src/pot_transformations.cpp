26bdd12808dc76ae161cc6ac0de2640961da5ad9
Liubov Talamanova <liubov.talamanova@intel.com>
2022-09-06 11:13:11 +0200

    [POT] Unroll GRUCell for GNA

--- src/common/offline_transformations/src/pot_transformations.cpp
+++ src/common/offline_transformations/src/pot_transformations.cpp
@@ -6,6 +6,9 @@
 
 #include <ngraph/pass/manager.hpp>
 
+#include <transformations/op_conversions/bidirectional_sequences_decomposition.hpp>
+#include <transformations/op_conversions/convert_sequences_to_tensor_iterator.hpp>
+#include <transformations/op_conversions/gru_cell_decomposition.hpp>
 #include <transformations/op_conversions/lstm_cell_decomposition.hpp>
 
 #include "pot_transformations.hpp"
@@ -14,6 +17,9 @@ bool ngraph::pass::POTTransformations::run_on_model(const std::shared_ptr<ngraph
     ngraph::pass::Manager manager(get_pass_config());
     if (m_device == "GNA") {
         manager.register_pass<ngraph::pass::LSTMCellDecomposition>();
+        manager.register_pass<ngraph::pass::BidirectionalGRUSequenceDecomposition>();
+        manager.register_pass<ngraph::pass::ConvertGRUSequenceToTensorIterator>();
+        manager.register_pass<ngraph::pass::GRUCellDecomposition>();
     }
     manager.run_passes(f);
     return false;
