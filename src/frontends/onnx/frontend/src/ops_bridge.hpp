a7e1f86bd51b856fbc06c8fb2bebc51829b6fc05
Mateusz Tabaka <mateusz.tabaka@intel.com>
2022-09-07 14:15:24 +0200

    [ONNX] Add support for Trilu operator
    Ticket: 91096

--- src/frontends/onnx/frontend/src/ops_bridge.hpp
+++ src/frontends/onnx/frontend/src/ops_bridge.hpp
@@ -25,12 +25,6 @@ struct UnknownDomain : ngraph_error {
     explicit UnknownDomain(const std::string& domain) : ngraph_error{domain} {}
 };
 
-struct UnsupportedVersion : ngraph_error {
-    UnsupportedVersion(const std::string& name, std::int64_t version, const std::string& domain)
-        : ngraph_error{"Unsupported operator version: " + (domain.empty() ? "" : domain + ".") + name + ":" +
-                       std::to_string(version)} {}
-};
-
 }  // namespace error
 
 class OperatorsBridge {
