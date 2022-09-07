cfbe141b0c2da06a75d7a442ad52a6e33bff0eec
Tetiana Gubanova <tgubanova@lohika.com>
2022-09-07 08:20:14 +0300

    Add blocked layouts support to bucketize (#12079)

--- src/plugins/intel_gpu/src/graph/program.cpp
+++ src/plugins/intel_gpu/src/graph/program.cpp
@@ -1419,7 +1419,8 @@ void program::set_layout_optimizer_attributes(layout_optimizer& lo) {
             prim.type() != cldnn::scatter_nd_update::type_id() &&
             prim.type() != cldnn::broadcast::type_id() &&
             prim.type() != cldnn::non_max_suppression::type_id() &&
-            prim.type() != cldnn::roi_align::type_id()) {
+            prim.type() != cldnn::roi_align::type_id() &&
+            prim.type() != cldnn::bucketize::type_id()) {
             can_use_fsv16 = false;
         }
 
@@ -1448,7 +1449,8 @@ void program::set_layout_optimizer_attributes(layout_optimizer& lo) {
             prim.type() != cldnn::broadcast::type_id() &&
             prim.type() != cldnn::quantize::type_id() &&
             prim.type() != cldnn::non_max_suppression::type_id() &&
-            prim.type() != cldnn::roi_align::type_id()) {
+            prim.type() != cldnn::roi_align::type_id() &&
+            prim.type() != cldnn::bucketize::type_id()) {
             can_use_bs_fs_yx_bsv16_fsv16 = false;
         }
     }
