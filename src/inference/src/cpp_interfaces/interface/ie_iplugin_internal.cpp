649dd61d8657491eeff0de1010cacfbe8a644e74
Nadezhda Ageeva <nadezhda.ageeva@intel.com>
2022-09-09 16:14:38 +0900

    [GPU] mark constants as supported on QN. Fetch common part for CPU and GPU.

--- src/inference/src/cpp_interfaces/interface/ie_iplugin_internal.cpp
+++ src/inference/src/cpp_interfaces/interface/ie_iplugin_internal.cpp
@@ -330,6 +330,88 @@ std::unordered_set<std::string> IInferencePlugin::GetRemovedNodes(
     return result;
 }
 
+std::unordered_set<std::string> IInferencePlugin::GetSupportedNodes(const CNNNetwork& network,
+    std::function<void(std::shared_ptr<ov::Model>&)> transform,
+    std::function<bool(const std::shared_ptr<ngraph::Node>)> is_node_supported) const {
+    auto model = network.getFunction();
+    if (model == nullptr) {
+        IE_THROW() << "Only ngraph-based models are supported!";
+    }
+
+    // Collect original operation names
+    std::unordered_set<std::string> original_ops;
+    for (auto&& node : model->get_ops()) {
+        original_ops.emplace(node->get_friendly_name());
+    }
+
+    auto cloned_network = InferenceEngine::details::cloneNetwork(network);
+    auto transformed_model = cloned_network.getFunction();
+    transform(transformed_model);
+    auto ops = transformed_model->get_ordered_ops();
+
+    // Mark removed nodes as supported
+    std::unordered_set<std::string> supported = GetRemovedNodes(model, transformed_model);;
+    std::unordered_set<std::string> unsupported;
+
+    for (auto&& op : ops) {
+        bool is_supported = false;
+        bool is_checked = false;
+        if (InferenceEngine::details::contains(original_ops, op->get_friendly_name())) {
+            is_supported = is_node_supported(op);
+            is_checked = true;
+            if (is_supported) {
+                supported.emplace(op->get_friendly_name());
+            } else {
+                unsupported.emplace(op->get_friendly_name());
+            }
+        }
+
+        for (auto&& fusedLayerName : ngraph::getFusedNamesVector(op)) {
+            if (InferenceEngine::details::contains(original_ops, fusedLayerName)) {
+                if (!is_checked) {
+                    is_supported = is_node_supported(op);
+                    is_checked = true;
+                }
+                if (is_supported) {
+                    supported.emplace(fusedLayerName);
+                } else {
+                    unsupported.emplace(fusedLayerName);
+                }
+            }
+        }
+    }
+    for (auto&& unsupportedNode : unsupported) {
+        supported.erase(unsupportedNode);
+    }
+    for (auto&& node : model->get_ops()) {
+        if (InferenceEngine::details::contains(supported, node->get_friendly_name())) {
+            for (auto&& inputNodeOutput : node->input_values()) {
+                if (ngraph::op::is_constant(inputNodeOutput.get_node()) || ngraph::op::is_parameter(inputNodeOutput.get_node())) {
+                    supported.emplace(inputNodeOutput.get_node()->get_friendly_name());
+                }
+            }
+            for (auto&& outputs : node->outputs()) {
+                for (auto&& outputNodeInput : outputs.get_target_inputs()) {
+                    if (ngraph::op::is_output(outputNodeInput.get_node())) {
+                        supported.emplace(outputNodeInput.get_node()->get_friendly_name());
+                    }
+                }
+            }
+        }
+
+        if (ngraph::op::is_constant(node) || ngraph::op::is_parameter(node)) {
+            if (!InferenceEngine::details::contains(supported, node->output(0).get_target_inputs().begin()->get_node()->get_friendly_name())) {
+                supported.erase(node->get_friendly_name());
+            }
+        } else if (ngraph::op::is_output(node)) {
+            if (!InferenceEngine::details::contains(supported, node->input_values().begin()->get_node()->get_friendly_name())) {
+                supported.erase(node->get_friendly_name());
+            }
+        }
+    }
+    return supported;
+}
+
 void SetExeNetworkInfo(const std::shared_ptr<IExecutableNetworkInternal>& exeNetwork,
                        const std::shared_ptr<const ov::Model>& function,
                        bool new_api) {
