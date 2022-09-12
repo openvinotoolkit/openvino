11d5383610c1b4a895fdee54758c493913a3359e
Ilya Churaev <ilya.churaev@intel.com>
2022-09-12 11:46:20 +0400

    Update src/tests/functional/inference_engine/lp_transformations/disable_convert_on_const_path_transformation.cpp
    Co-authored-by: Vladislav Golubev <vladislav.golubev@intel.com>

--- src/tests/functional/inference_engine/lp_transformations/disable_convert_on_const_path_transformation.cpp
+++ src/tests/functional/inference_engine/lp_transformations/disable_convert_on_const_path_transformation.cpp
@@ -151,7 +151,7 @@ const std::vector<DisableConvertOnConstPathTransformationValues> testValues = {
             },
             {
                 { ngraph::element::f32, false },
-                { {128.f}, element::f32, {}, false, 1ul, element::u8, true, {},
+                { {127.f}, element::f32, {}, false, 1ul, element::i8, true, {},
                   {{ov::pass::DisableConstantFolding::get_type_info_static(), ov::pass::DisableConstantFolding()} } },
                 { {0.03f}, element::f32, {}, false }
             },
