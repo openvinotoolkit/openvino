ab66441d7ca1fe612aac6de6a803d452ea54ff2c
Kazantsev, Roman <roman.kazantsev@intel.com>
2022-09-12 19:15:29 +0400

    Recover apply_additional_conversion_rules

--- src/frontends/tensorflow/include/openvino/frontend/tensorflow/node_context.hpp
+++ src/frontends/tensorflow/include/openvino/frontend/tensorflow/node_context.hpp
@@ -52,6 +52,8 @@ public:
     }
 
 private:
+    ov::Any apply_additional_conversion_rules(const ov::Any& data, const std::type_info& type_info) const override;
+
     std::shared_ptr<DecoderBase> m_decoder;
     const OutputVector& m_inputs;
 };
