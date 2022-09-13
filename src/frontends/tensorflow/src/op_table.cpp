3e6800ef5f881079bf7034cc20bf05bef5b83c5e
Roman Kazantsev <roman.kazantsev@intel.com>
2022-09-13 10:28:36 +0300

    [TF FE] Support Wide&Deep family models by TensorFlow FrontEnd (#13004)
    * [TF FE] Support Wide&Deep family models by TensorFlow FrontEnd
    
    It adds support for operations SparseToDense, transfomations for SparseReshape and
    fusing transformation for EmbeddingSegmentSum operation
    
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>
    
    * Apply code style for sparse_fill_empty_rows
    
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>
    
    * Complete transformation for embedding_segments_feature_fusing
    
    * Fix signatures of constructors of internal ops: SparseFillEmptyRows, etc.
    
    * Add override for validate_and_infer_types methods of the internal ops
    
    * Remove unused variable in Unique
    
    * Revert incorrect changes for default type in case graph cutting
    
    * Make InternalOperation class and apply code feedback
    
    * Revert changes for pipeline.py
    
    * Remove apply_middle_transformation from frontend.hpp
    
    * Recover apply_additional_conversion_rules
    
    * Eliminate a number of constructors for internal operations
    
    * Improve code readibility for embedding_segments_feature_fusing transformation
    
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>
    
    * Fix build issue: returning reference to temporary
    
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/frontends/tensorflow/src/op_table.cpp
+++ src/frontends/tensorflow/src/op_table.cpp
@@ -89,12 +89,14 @@ OP_CONVERTER(translate_roll_op);
 OP_CONVERTER(translate_round_op);
 OP_CONVERTER(translate_rsqrt_op);
 OP_CONVERTER(translate_scatter_nd_op);
+OP_CONVERTER(translate_sparse_to_dense_op);
 OP_CONVERTER(translate_select_op);
 OP_CONVERTER(translate_shape_op);
 OP_CONVERTER(translate_size_op);
 OP_CONVERTER(translate_slice_op);
 OP_CONVERTER(translate_softmax_op);
 OP_CONVERTER(translate_space_to_depth_op);
+OP_CONVERTER(translate_sparse_reshape_op);
 OP_CONVERTER(translate_split_op);
 OP_CONVERTER(translate_split_v_op);
 OP_CONVERTER(translate_square_op);
@@ -109,6 +111,11 @@ OP_CONVERTER(translate_unpack_op);
 OP_CONVERTER(translate_where_op);
 OP_CONVERTER(translate_x_div_y_op);
 OP_CONVERTER(translate_zeros_like_op);
+
+// Translators for internal operations
+OP_CONVERTER(translate_sparse_fill_empty_rows_op);
+OP_CONVERTER(translate_sparse_segment_sum_op);
+OP_CONVERTER(translate_unique_op);
 
 const std::map<std::string, CreatorFunction> get_supported_ops() {
     return {
@@ -258,6 +265,7 @@ const std::map<std::string, CreatorFunction> get_supported_ops() {
         {"Round", translate_round_op},
         {"Rsqrt", translate_rsqrt_op},
         {"ScatterNd", translate_scatter_nd_op},
+        {"SparseToDense", translate_sparse_to_dense_op},
         {"Select", translate_select_op},
         {"SelectV2", translate_select_op},
         {"Shape", translate_shape_op},
@@ -266,6 +274,7 @@ const std::map<std::string, CreatorFunction> get_supported_ops() {
         {"Snapshot", translate_identity_op},
         {"Softmax", translate_softmax_op},
         {"SpaceToDepth", translate_space_to_depth_op},
+        {"SparseReshape", translate_sparse_reshape_op},
         {"Split", translate_split_op},
         {"SplitV", translate_split_v_op},
         {"StopGradient", translate_identity_op},
@@ -282,6 +291,11 @@ const std::map<std::string, CreatorFunction> get_supported_ops() {
         {"Where", translate_where_op},
         {"Xdivy", translate_x_div_y_op},
         {"ZerosLike", translate_zeros_like_op},
+
+        // Translators for internal operations
+        {"SparseFillEmptyRows", translate_sparse_fill_empty_rows_op},
+        {"SparseSegmentSum", translate_sparse_segment_sum_op},
+        {"Unique", translate_unique_op},
     };
 };
 }  // namespace op
