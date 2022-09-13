1713d964da855470d36c8d56b74d192d13476d92
Kim, Mingyu <mingyu.kim@intel.com>
2022-09-13 23:46:00 +0900

    Revert "[GPU] Softmax blocked layouts support (#12467)"
    This reverts commit 0145865301ac07619b53ed0969a04bdca3490f78.

--- src/plugins/intel_gpu/src/graph/impls/ocl/softmax.cpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl/softmax.cpp
@@ -69,25 +69,16 @@ struct softmax_impl : typed_primitive_impl_ocl<softmax> {
 namespace detail {
 
 attach_softmax_impl::attach_softmax_impl() {
-    auto types = {data_types::f16, data_types::f32};
-    auto formats = {
-            format::bfyx,
-            format::yxfb,
-            format::b_fs_yx_fsv16,
-            format::b_fs_yx_fsv32,
-            format::bs_fs_yx_bsv16_fsv16,
-            format::bs_fs_yx_bsv32_fsv16,
-            format::bs_fs_yx_bsv32_fsv32,
-            format::bfzyx,
-            format::b_fs_zyx_fsv16,
-            format::b_fs_zyx_fsv32,
-            format::bs_fs_zyx_bsv16_fsv32,
-            format::bs_fs_zyx_bsv16_fsv16,
-            format::bs_fs_zyx_bsv32_fsv32,
-            format::bs_fs_zyx_bsv32_fsv16
-    };
-
-    implementation_map<softmax>::add(impl_types::ocl, softmax_impl::create, types, formats);
+    implementation_map<softmax>::add(impl_types::ocl, softmax_impl::create, {
+        std::make_tuple(data_types::f32, format::yxfb),
+        std::make_tuple(data_types::f16, format::yxfb),
+        std::make_tuple(data_types::f32, format::bfyx),
+        std::make_tuple(data_types::f16, format::bfyx),
+        std::make_tuple(data_types::f32, format::byxf),
+        std::make_tuple(data_types::f16, format::byxf),
+        std::make_tuple(data_types::f32, format::bfzyx),
+        std::make_tuple(data_types::f16, format::bfzyx),
+    });
 }
 
 }  // namespace detail
