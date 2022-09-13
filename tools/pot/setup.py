ee07eeb2282dee09e9d0e645f1c63648c215e49d
Mateusz <mateusz.mikolajczyk@intel.com>
2022-09-13 07:36:08 +0000

    Bump pandas requirement version

--- tools/pot/setup.py
+++ tools/pot/setup.py
@@ -73,7 +73,7 @@ INSTALL_REQUIRES = [
     "networkx<2.8.1;python_version>'3.6'",
     "tqdm>=4.54.1",
     "texttable~=1.6.3",
-    "pandas~=1.1.5",
+    "pandas~=1.3.5",
     "openvino-telemetry>=2022.1.0"
 ]
 
