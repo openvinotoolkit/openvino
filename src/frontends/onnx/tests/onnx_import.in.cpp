5b73ed8c370643c9c7b0334d742df4aaf882f279
Vlad Zilkov <vladislav.zilkov@intel.com>
2022-09-14 07:10:37 +0000

    Deleted comments, edited test float -> bfloat

--- src/frontends/onnx/tests/onnx_import.in.cpp
+++ src/frontends/onnx/tests/onnx_import.in.cpp
@@ -5622,23 +5622,6 @@ NGRAPH_TEST(${BACKEND_NAME}, castlike_int8_to_uint16) {
         test_case.run();
     }
 
-    // [ RUN      ] INTERPRETER.castlike_int8_to_uint16
-    // >>>>>>>>>>>>>>>  === castlike_int8_to_uint16 test
-    // [       OK ] INTERPRETER.castlike_int8_to_uint16 (39 ms)
-
-    // [ RUN      ] IE_CPU.castlike_int8_to_uint16
-    // >>>>>>>>>>>>>>>  === castlike_int8_to_uint16 test
-    // 65535 is not close to 0 at index 0
-    // 65534 is not close to 0 at index 1
-
-    // /home/vzilkov/openvino/src/tests/engines_util/../engines_util/test_case.hpp:190: Failure
-    // Value of: res
-    //   Actual: false (65535 is not close to 0 at index 0
-    // 65534 is not close to 0 at index 1
-    // )
-    // Expected: true
-    // [  FAILED  ] IE_CPU.castlike_int8_to_uint16 (75 ms)
-
 NGRAPH_TEST(${BACKEND_NAME}, castlike_float64_to_int64) {
     auto function = onnx_import::import_onnx_model(
         file_util::path_join(CommonTestUtils::getExecutableDirectory(), SERIALIZED_ZOO, "onnx/castlike_float64_to_int64.onnx"));
@@ -5691,18 +5674,18 @@ NGRAPH_TEST(${BACKEND_NAME}, castlike_float64_to_int32) {
     test_case.run();
 }
 
-// NGRAPH_TEST(${BACKEND_NAME}, castlike_float32_to_bfloat16) {
-//     auto function = onnx_import::import_onnx_model(
-//         file_util::path_join(CommonTestUtils::getExecutableDirectory(), SERIALIZED_ZOO, "onnx/castlike_float32_to_bfloat16.onnx"));
+NGRAPH_TEST(${BACKEND_NAME}, DISABLED_castlike_float32_to_bfloat16) {
+    auto function = onnx_import::import_onnx_model(
+        file_util::path_join(CommonTestUtils::getExecutableDirectory(), SERIALIZED_ZOO, "onnx/castlike_float32_to_bfloat16.onnx"));
     
-//     auto test_case = test::TestCase(function, s_device);
+    auto test_case = test::TestCase(function, s_device);
     
-//     test_case.add_input<float>(Shape{3,4},std::vector<float>{1.5, 2.4, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
-//     test_case.add_input<bfloat16>(Shape{3,4},{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
-//     test_case.add_expected_output<bfloat16>(std::vector<bfloat16>{1.5, 2.4, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
+    test_case.add_input<float>(Shape{3,4},std::vector<float>{121.5, 122.7, 3, 4, 5, 6, 7, 8.8, 9, 10, 11, 12});
+    test_case.add_input<bfloat16>(Shape{3,4},{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
+    test_case.add_expected_output<bfloat16>(std::vector<bfloat16>{121.5, 122.7, 3, 4, 5, 6, 7, 8.8, 9, 10, 11, 12});
     
-//     test_case.run();
-// }
+    test_case.run();
+}
 
 NGRAPH_TEST(${BACKEND_NAME}, DISABLED_castlike_bfloat16_to_float32) {
     auto function = onnx_import::import_onnx_model(
@@ -5716,20 +5699,3 @@ NGRAPH_TEST(${BACKEND_NAME}, DISABLED_castlike_bfloat16_to_float32) {
     
     test_case.run();
 }
-
-//double -> float32
-
-/**
- * castlike_float16_to_uint32
- * castlike_float16_to_int64
- * castlike_int8_to_uint16      // trouble with IE_CPU.castlike_int8_to_uint16, INTERPRETER.castlike_int8_to_uint16 is OK
- * castlike_float64_to_int64
- * castlike_int8_to_float16
- * castlike_int32_to_float64
- * castlike_float64_to_int32
- * castlike_float32_to_bfloat16 <--
- * castlike_bfloat16_to_float32
- * 
- * add double, bfloat, string, bool
-**/
-// create a BUG in jira, assign to CPU team (auto, component field ... dmitry gorohov)
