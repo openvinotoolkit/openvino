189880a79de478e87f3894c99e0de0015315fb37
Kazantsev, Roman <roman.kazantsev@intel.com>
2022-09-14 11:20:59 +0400

    Put only destructor into cpp

--- src/frontends/tensorflow/src/extension/conversion.cpp
+++ src/frontends/tensorflow/src/extension/conversion.cpp
@@ -6,12 +6,4 @@
 
 using namespace ov::frontend::tensorflow;
 
-ConversionExtension::ConversionExtension(const std::string& op_type, const ov::frontend::CreatorFunction& converter)
-    : ConversionExtensionBase(op_type),
-      m_converter(converter) {}
-
-const ov::frontend::CreatorFunction& ConversionExtension::get_converter() const {
-    return m_converter;
-}
-
 ConversionExtension ::~ConversionExtension() = default;
