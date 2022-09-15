72c3bf222bfef6b1f5a795f6a1234cc2789b533e
yanlan song <bell.song@intel.com>
2022-09-15 16:47:11 +0800

    fix coredump when quit benchmark_app (#13026)
    * fix coredump when quit benchmark_app
    
    Signed-off-by: fishbell <bell.song@intel.com>
    
    * enable tests
    
    Signed-off-by: fishbell <bell.song@intel.com>
    
    * add macro to handle CPU not built
    
    Signed-off-by: fishbell <bell.song@intel.com>
    
    Signed-off-by: fishbell <bell.song@intel.com>

--- src/plugins/auto/multi_schedule.cpp
+++ src/plugins/auto/multi_schedule.cpp
@@ -311,10 +311,21 @@ IInferPtr MultiSchedule::CreateInferRequest() {
         } catch(...) {
             LOG_INFO("query perf hint from passthrough network failed");
         }
-        if (_multiSContext->_batchingDisabled || perfmode != CONFIG_VALUE(THROUGHPUT))
+        if (_multiSContext->_batchingDisabled || perfmode != CONFIG_VALUE(THROUGHPUT)) {
             syncRequestImpl->setPointerToSo(_passthroughExeNet._so);
+        } else {
+            auto so = _passthroughExeNet._ptr->GetPointerToSo();
+            // Get the _so from passthrough executable network when batch plugin is disable.
+            if (!so)
+                so = _passthroughExeNet._so;
+            syncRequestImpl->setPointerToSo(so);
+        }
+    } else if (_multiSContext->_bindBuffer) {
+        auto sharedRequest = std::static_pointer_cast<MultiDeviceInferRequest>(syncRequestImpl)->GetSharedRequest();
+        if (sharedRequest._ptr->getPointerToSo())
+             syncRequestImpl->setPointerToSo(sharedRequest._ptr->getPointerToSo());
         else
-            syncRequestImpl->setPointerToSo(_passthroughExeNet._ptr->GetPointerToSo());
+            syncRequestImpl->setPointerToSo(sharedRequest._so);
     }
     return std::make_shared<AsyncInferRequest>(shared_from_this(),
                                                syncRequestImpl,
