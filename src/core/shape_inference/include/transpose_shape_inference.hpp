f543ff0c060fceb46d683eea3d6b13e7b4a654db
Raasz, Pawel <pawel.raasz@intel.com>
2022-09-19 12:32:11 +0200

    Update names for shapes: input -> input_shape order -> order_shape

--- src/core/shape_inference/include/transpose_shape_inference.hpp
+++ src/core/shape_inference/include/transpose_shape_inference.hpp
@@ -28,14 +28,15 @@ void shape_infer(const Transpose* op,
                  const std::vector<T>& input_shapes,
                  std::vector<T>& output_shapes,
                  const std::map<size_t, std::shared_ptr<ngraph::runtime::HostTensor>>& constant_data = {}) {
-    const auto& input = input_shapes[TransposeIn::ARG];
-    const auto& order = input_shapes[TransposeIn::ORDER];
+    const auto& input_shape = input_shapes[TransposeIn::ARG];
+    const auto& order_shape = input_shapes[TransposeIn::ORDER];
     auto& output = output_shapes[TransposeOut::ARG_T];
 
     std::vector<int64_t> axes;
 
     if (get_data_as_int64<T>(op::TransposeIn::ORDER, op, axes, constant_data)) {
-        const auto out_rank_size = input.rank().is_static() ? input.rank().get_length() : order[0].get_length();
+        const auto out_rank_size =
+            input_shape.rank().is_static() ? input_shape.rank().get_length() : order_shape[0].get_length();
         const auto max_dim_num = out_rank_size - 1;
 
         if (axes.empty()) {
@@ -44,21 +45,21 @@ void shape_infer(const Transpose* op,
         } else {
             NODE_VALIDATION_CHECK(
                 op,
-                input.rank().is_dynamic() ||
+                input_shape.rank().is_dynamic() ||
                     (std::set<size_t>(axes.cbegin(), axes.cend()).size() == out_rank_size &&
                      std::all_of(axes.cbegin(), axes.cend(), cmp::Between<int64_t, cmp::LOWER>(0, out_rank_size))),
                 "Permutation ",
                 AxisVector(axes.begin(), axes.end()),
                 " is not valid for input shape ",
-                input);
+                input_shape);
         }
 
         output.resize(out_rank_size);
-        std::transform(axes.cbegin(), axes.cend(), output.begin(), [&input](const size_t axis) {
-            return input[axis];
+        std::transform(axes.cbegin(), axes.cend(), output.begin(), [&input_shape](const size_t axis) {
+            return input_shape[axis];
         });
     } else {
-        output = ov::PartialShape::dynamic(input.rank());
+        output = ov::PartialShape::dynamic(input_shape.rank());
     }
 }
 }  // namespace v1
