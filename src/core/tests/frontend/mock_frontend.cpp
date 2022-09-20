d7a6268494f9134d26a8eec8e85e999cb85f7e9f
Pawel Raasz <pawel.raasz@intel.com>
2022-09-20 10:56:46 +0200

    Enable mock fe tests in python api 2.0 tests (#12978)
    * Building python frontend tests
    
    * Enable mock tests in new python API:
    - Fix mock_py frontend library name
    - Set same destination for mock_py frontend as others frontends
    - Fix importing mock_py frontend
    - Minor changes in mock_py tests
    
    * Fix flake8 issue
    
    * Fix linking issue on windows
    
    * Remove DISABLE_PYBIND11 option
    
    * Build mock_py always as shared library
    - put mock_py into ov::frontend namespace
    - minor re-factor of mock_py
    
    * Add mock c API fix warning on windows
    
    * Fix installation of mock_py frontend
    
    * Install mock_py frontend library in tests
    
    * More detailed error message in python test
    when cannot import frontend
    
    * Restore IMPLEMENT_OPENVINO_API for windows build
    of pybind_mock_frontend
    
    * Set env paths for python API test on Azure CI
    - skip tensorflow conversion ext test when not installed
    
    * Correct skip tensorflow test decorator
    
    * Add C API macro for mock1 frontend
    
    * Ignore E999 error reported by flake8
    
    * Fix conversion extension name for tf imports
    
    * Fix flake8 issues
    
    * Remove python3.8 path from LD_LIBRARY_PATH
    on debian CI
    
    Co-authored-by: Ilya Lavrenov <ilya.lavrenov@intel.com>

--- src/core/tests/frontend/mock_frontend.cpp
+++ src/core/tests/frontend/mock_frontend.cpp
@@ -8,12 +8,7 @@
 #include "openvino/frontend/visibility.hpp"
 #include "openvino/opsets/opset8.hpp"
 
-// Defined if we are building the plugin DLL (instead of using it)
-#ifdef openvino_mock1_frontend_EXPORTS
-#    define MOCK_API OPENVINO_CORE_EXPORTS
-#else
-#    define MOCK_API OPENVINO_CORE_IMPORTS
-#endif  // openvino_mock1_frontend_EXPORTS
+#define MOCK_C_API OPENVINO_EXTERN_C OPENVINO_CORE_EXPORTS
 
 using namespace ngraph;
 using namespace ov::frontend;
@@ -198,14 +193,14 @@ public:
     }
 };
 
-extern "C" MOCK_API FrontEndVersion GetAPIVersion();
-extern "C" MOCK_API void* GetFrontEndData();
+MOCK_C_API FrontEndVersion GetAPIVersion();
+MOCK_C_API void* GetFrontEndData();
 
-extern "C" MOCK_API FrontEndVersion GetAPIVersion() {
+MOCK_C_API FrontEndVersion GetAPIVersion() {
     return OV_FRONTEND_API_VERSION;
 }
 
-extern "C" MOCK_API void* GetFrontEndData() {
+MOCK_C_API void* GetFrontEndData() {
     auto* res = new FrontEndPluginInfo();
     res->m_name = "mock1";
     res->m_creator = []() {
