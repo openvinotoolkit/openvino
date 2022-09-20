ef21f5154f545d89e2b8d795fe2fea20eb4f867c
Raasz, Pawel <pawel.raasz@intel.com>
2022-09-20 07:30:28 +0200

    Not fill output shape for dynamic range

--- src/core/shape_inference/include/transpose_shape_inference.hpp
+++ src/core/shape_inference/include/transpose_shape_inference.hpp
@@ -29,14 +29,12 @@ void shape_infer(const Transpose* op,
                  std::vector<T>& output_shapes,
                  const std::map<size_t, std::shared_ptr<ngraph::runtime::HostTensor>>& constant_data = {}) {
     const auto& input_shape = input_shapes[TransposeIn::ARG];
-    const auto& order_shape = input_shapes[TransposeIn::ORDER];
     auto& output = output_shapes[TransposeOut::ARG_T];
 
     std::vector<int64_t> axes;
 
-    if (get_data_as_int64<T>(op::TransposeIn::ORDER, op, axes, constant_data)) {
-        const auto out_rank_size =
-            input_shape.rank().is_static() ? input_shape.rank().get_length() : order_shape[0].get_length();
+    if (get_data_as_int64<T>(op::TransposeIn::ORDER, op, axes, constant_data) && input_shape.rank().is_static()) {
+        const auto out_rank_size = input_shape.rank().get_length();
         const auto max_dim_num = out_rank_size - 1;
 
         if (axes.empty()) {
@@ -45,9 +43,8 @@ void shape_infer(const Transpose* op,
         } else {
             NODE_VALIDATION_CHECK(
                 op,
-                input_shape.rank().is_dynamic() ||
-                    (std::set<size_t>(axes.cbegin(), axes.cend()).size() == out_rank_size &&
-                     std::all_of(axes.cbegin(), axes.cend(), cmp::Between<int64_t, cmp::LOWER>(0, out_rank_size))),
+                (std::set<size_t>(axes.cbegin(), axes.cend()).size() == out_rank_size &&
+                 std::all_of(axes.cbegin(), axes.cend(), cmp::Between<int64_t, cmp::LOWER>(0, out_rank_size))),
                 "Permutation ",
                 AxisVector(axes.begin(), axes.end()),
                 " is not valid for input shape ",
