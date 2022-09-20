ed31a46b7bcabd0c3a3c79aa2b142d718f785cb4
River Li <river.li@intel.com>
2022-09-20 05:59:27 -0700

    [CAPI] improve property implement to achieve better compatibility (#12887)
    * Solve some code style issues
    
    Change-Id: I06c31ad9513463c758db96dbe33031983a8fd7a5
    
    * Refine ov_dimension with C style structure
    
    Use C style definition to replace opaque C++ handle:
     1). Improve partial shape
     2). Improve dimension/rank
     3). Improve shape
    
    Change-Id: I680fd3e76c63d0c06d9d8b07c67af0440fb15bb7
    
    * Some minor update
    
    Change-Id: I237490a765e6ecd6f3896b1bd75d5f962304f43b
    
    * add partial_shape_is_dynamic and update doc
    
    Change-Id: Ief358f27de8386e35485d08847c8243d1490157f
    
    * Rename property and add basic test cases
    
    Change-Id: Ifbed0bbf7a5e836d2cab186763d7e5f8f2cb7d44
    
    * address reviewer's comments
    
    Change-Id: I9a5d1ede6d086906f3777c3d29237986a505db52
    
    * Fix compile error
    
    Change-Id: I72267a8dda32dd8f43c1ea1522d07b392052358f
    
    * Use ov_any_t to replace ov_property_value_t
    
    Change-Id: Ia4062d0da0941c901990bf5f16497ae3c8f2eb0f
    
    * update doc for ov_model
    
    Change-Id: I80183394f16dd1e07ff998993e26f8b903f2402e
    
    * Property key apply string variables replace enum variable
    
    Change-Id: I0e1699862de3913ccdc06ded5b8f8ac5a2bf8773
    
    * Fix compiling error in windows
    
    Change-Id: I4f9c7c746e218db94491c5f4bc5aeda1b3fde62b
    
    * Change init/deinit method to create/free, remove unnecessary methods
    
    Change-Id: Ibc5de039da94e92db494583ab1ff1a63e0586c73
    
    * Pass by value for single C structure data
    
    Change-Id: I289eb56e2ba1ab90942c8c801c585ab0f792952f
    
    * fix dllexport issue in windows
    
    Change-Id: I1632375e5e651fdfa076cdab2d8d1c3e83986549
    
    * Fix static build error
    
    Change-Id: I6633fe4887edad0b3a8b7e10730fb9d36a6ac0cd
    
    * Use pass by value for all transparent structure
    
    Change-Id: I652f6e70b420ef7c5af5cff3f5d482911cf0d75a
    
    * Unify parameter order in  C APIs, input first and then output parameters
    
    Change-Id: Ib89c77d81c314e3a81bfcec9917630abbc896f5e
    
    * Rename preprocess api to be more readable
    
    Change-Id: I0a8ecd005e73e4ba8e90a8f25bedf0739fe31836
    
    * Simplfy property implement for set and get property
    
    Change-Id: Icad41606368cef9520b1623f6207784c295f1b9f
    
    * check string type property issue
    
    Change-Id: Ie8ee6995dda401f1db0cc098e061ffcfd24de92a
    
    * Fix memory leak
    
    Change-Id: Ife8980736be320314eca6fb3f3605e019663e1da
    
    * Check string property data
    
    Change-Id: I542a136ff874cb345c67f7d3c1dbc74981a909df
    
    * Fix issues caused by rebase
    
    Change-Id: I3d675a16cf76980a6414e369a83464a46dec1e49
    
    * Remove some redundant code
    
    Change-Id: Iab6bf7f355ec7a7424d884e620319ed9db80ecf5
    
    * Remove property type from interface
    
    ov_core_set_property will have 2 cases:
    
    1) enum type:  <const char* property_key, ov_property_xxx_e property_value>
    2) other: <const char* property_key, const char* property_value>
    
    Change-Id: Id7dec1ac40948d45c4f8baee687652aee92ea75d
    
    * Remove redundant code
    
    Change-Id: I66865d3313e305b0109cb63bd049772a01c0688f
    
    * Remove exception log and update propery size argument
    
    Change-Id: Ia1a60c07c9adf08d175d94a7d5e4298adb27bf80
    
    * Apply string for enum property
    
    Change-Id: I591b1db453a52b5ea9000e0cc3e11edf0ef5f790
    
    * Add test case for MULTI_DEVICE_PRIORITIES
    
    Change-Id: I59e4307564a2560724b4dacdb83c3ed45ea1ce8d
    
    * Fix test issues and add more negative test case
    
    Change-Id: Ib849fb8c098e487a537c1b1ab881f5e905871f73

--- src/bindings/c/src/ov_property.cpp
+++ src/bindings/c/src/ov_property.cpp
@@ -5,22 +5,27 @@
 
 #include "common.h"
 
-ov_status_e ov_properties_create(ov_properties_t* property, size_t size) {
-    if (!property || size < 0) {
-        return ov_status_e::INVALID_C_PARAM;
-    }
-    try {
-        std::unique_ptr<ov_property_t> _property(new ov_property_t[size]);
-        property->list = _property.release();
-        property->size = size;
-    }
-    CATCH_OV_EXCEPTIONS
-    return ov_status_e::OK;
-}
+// Read-only property key
+const char* ov_property_key_supported_properties = "SUPPORTED_PROPERTIES";
+const char* ov_property_key_available_devices = "AVAILABLE_DEVICES";
+const char* ov_property_key_optimal_number_of_infer_requests = "OPTIMAL_NUMBER_OF_INFER_REQUESTS";
+const char* ov_property_key_range_for_async_infer_requests = "RANGE_FOR_ASYNC_INFER_REQUESTS";
+const char* ov_property_key_range_for_streams = "RANGE_FOR_STREAMS";
+const char* ov_property_key_device_full_name = "FULL_DEVICE_NAME";
+const char* ov_property_key_device_capabilities = "OPTIMIZATION_CAPABILITIES";
+const char* ov_property_key_model_name = "NETWORK_NAME";
+const char* ov_property_key_optimal_batch_size = "OPTIMAL_BATCH_SIZE";
+const char* ov_property_key_max_batch_size = "MAX_BATCH_SIZE";
 
-void ov_properties_free(ov_properties_t* properties) {
-    if (properties && properties->list) {
-        delete[] properties->list;
-        properties->size = 0;
-    }
-}
+// Read-write property key
+const char* ov_property_key_cache_dir = "CACHE_DIR";
+const char* ov_property_key_num_streams = "NUM_STREAMS";
+const char* ov_property_key_affinity = "AFFINITY";
+const char* ov_property_key_inference_num_threads = "INFERENCE_NUM_THREADS";
+const char* ov_property_key_hint_performance_mode = "PERFORMANCE_HINT";
+const char* ov_property_key_hint_inference_precision = "INFERENCE_PRECISION_HINT";
+const char* ov_property_key_hint_num_requests = "PERFORMANCE_HINT_NUM_REQUESTS";
+const char* ov_property_key_hint_model_priority = "MODEL_PRIORITY";
+const char* ov_property_key_log_level = "LOG_LEVEL";
+const char* ov_property_key_enable_profiling = "PERF_COUNT";
+const char* ov_property_key_device_priorities = "MULTI_DEVICE_PRIORITIES";
