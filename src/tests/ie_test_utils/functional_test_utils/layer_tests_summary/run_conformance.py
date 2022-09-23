3527c6716994c2851a1164a9d6d378c0032b9316
Irina Efode <irina.efode@intel.com>
2022-09-23 16:36:58 +0200

    dd

--- src/tests/ie_test_utils/functional_test_utils/layer_tests_summary/run_conformance.py
+++ src/tests/ie_test_utils/functional_test_utils/layer_tests_summary/run_conformance.py
@@ -1,12 +1,14 @@
 from asyncio import subprocess
+from queue import Empty
 from git import Repo
 from argparse import ArgumentParser
 from utils import utils
 from glob import glob
 from subprocess import Popen
 from shutil import copytree
-# from summarize import create_summary
+from summarize import create_summary
 from merge_xmls import merge_xml
+from pathlib import Path
 
 import xml.etree.ElementTree as ET
 
@@ -14,32 +16,47 @@ import os
 
 logger = utils.get_logger('ConformanceRunner')
 
+OPENVINO_NAME = 'openvino'
+
 OMZ_REPO_URL = "https://github.com/openvinotoolkit/open_model_zoo.git"
 OMZ_REPO_BRANCH = "master"
 
-GTEST_PARALLEL_URL = "https://github.com/asomsiko/gtest-parallel.git"
-GTEST_PARALLEL_BRANCH = "limit_log_name"
+GTEST_PARALLEL_URL = "https://github.com/intel-innersource/frameworks.ai.openvino.ci.infrastructure.git"
+GTEST_PARALLEL_BRANCH = "master"
 
 API_CONFORMANCE_BIN_NAME = "apiConformanceTests"
 OP_CONFORMANCE_BIN_NAME = "conformanceTests"
 SUBGRAPH_DUMPER_BIN_NAME = "subgraphsDumper"
+
+def get_ov_path():
+    ov_dir = None
+    try:
+        ov_dir = os.environ['INTEL_OPENVINO_DIR']
+    except:
+        ov_dir = os.path.abspath(os.getcwd())[:os.path.abspath(os.getcwd()).find(OPENVINO_NAME) + len(OPENVINO_NAME)]
+        ov_dir = os.path.join(ov_dir, 'bin')
+        get_latest_dir = lambda path: sorted(Path(ov_dir).iterdir(), key=os.path.getmtime)[0]
+        ov_dir = os.path.join(get_latest_dir(ov_dir))
+        ov_dir = os.path.join(get_latest_dir(ov_dir))
+    return ov_dir
+
 
 
 def parse_arguments():
     parser = ArgumentParser()
 
     models_path_help = "Path to directory/ies contains models to dump subgraph (default way is download OMZ). If `--d=False` specify Conformance IRs directory"
-    device_help = "Specify target device"
+    device_help = "Specify target device. Default value is CPU"
     ov_help = "OV binary files path"
     working_dir_help = "Specify working directory"
     type_help = "Specify conformance type: OP or API"
     dump_conformance_help = "Set 'True' if you want to create Conformance IRs from custom models. Default value is 'False'"
 
-    parser.add_argument("-m", "--models_path", help=models_path_help, type=str, required=True)
-    parser.add_argument("-d", "--device", help= device_help, type=str, required=True)
-    parser.add_argument("-ov", "--ov_binaries", help=ov_help, type=str, required=True)
+    parser.add_argument("-m", "--models_path", help=models_path_help, type=str, required=False, default=".")
+    parser.add_argument("-d", "--device", help= device_help, type=str, required=False, default="CPU")
+    parser.add_argument("-ov", "--ov_binaries", help=ov_help, type=str, required=False, default=get_ov_path())
     parser.add_argument("-w", "--working_dir", help=working_dir_help, type=str, required=False, default='.')
-    parser.add_argument("-t", "--type", help=type_help, type=str, required=True, default="OP")
+    parser.add_argument("-t", "--type", help=type_help, type=str, required=False, default="OP")
     parser.add_argument("-s", "--dump_conformance", help=dump_conformance_help, type=bool, required=False, default=False)
 
     return parser.parse_args()
@@ -66,7 +83,7 @@ class Conformance:
             branch = repo.git.checkout(version)
             repo.submodule_update(recursive=True)
             logger.info(f'Repo {https_url} is on {version}')
-        return repo.working_dir
+        return repo_path
 
     def _convert_models(self):
         omz_tools_path = os.path.join(self._omz_path, "tools", "model_tools")
@@ -78,10 +95,10 @@ class Conformance:
         command = f'python3 -m venv .env3;'\
             f'source .env3/bin/activate;'\
             f'pip3 install -e "{mo_path}/.[caffe,kaldi,mxnet,onnx,pytorch,tensorflow2]";'\
-            f'pip3 install "{omz_tools_path}/."'\
-            f'omz_converter --all --download_dir={original_model_path} --output_dir={converted_model_path};'\
+            f'pip3 install "{omz_tools_path}/.[paddle,pytorch,tensorflow]"'\
+            f'omz_converter --name=vgg16 --download_dir={original_model_path} --output_dir={converted_model_path};'\
             f'deactivate;'
-        process = Popen(command, stdout=subprocess.PIPE, shell=True)
+        process = Popen(command, shell=True)
         out, err = process.communicate()
         if err is None:
             for line in str(out).split('\n'):
@@ -105,7 +122,7 @@ class Conformance:
             os.mkdir(conformance_ir_path)
         logger.info(f"Stating model dumping from {self._model_path}")
         cmd = f'{subgraph_dumper_path} --input_folders={self._model_path} --output_folder={conformance_ir_path}'
-        process = Popen(cmd, stdout=subprocess.PIPE, shell=True)
+        process = Popen(cmd, shell=True)
         out, err = process.communicate()
         if err is None:
             for line in str(out).split('\n'):
@@ -116,8 +133,17 @@ class Conformance:
             raise Exception("Process failed on step: 'Subgraph dumping'")
         self._model_path = conformance_ir_path
 
+    def _prepare_filelist(self):
+        xmls = Path(self._model_path).rglob("*.xml")
+        filelist_path = os.path.join(self._model_path, "conformance_ir.lst")
+        with open(filelist_path, 'w') as file:
+            for xml in xmls:
+                file.write(str(xml) + '\n')
+            file.close()
+        return filelist_path
+
     def run_conformance(self):
-        gtest_parallel_path = os.path.join(self.__download_repo(GTEST_PARALLEL_URL, GTEST_PARALLEL_BRANCH), "gtest_parallel.py")
+        gtest_parallel_path = os.path.join(self.__download_repo(GTEST_PARALLEL_URL, GTEST_PARALLEL_BRANCH), "thirdparty", "gtest-parallel", "gtest_parallel.py")
         worker_num = os.cpu_count()
         conformance_path = None
         if self._type == "OP":
@@ -128,16 +154,19 @@ class Conformance:
         logs_dir = os.path.join(self._working_dir, f'{self._device}_logs')
         report_dir = os.path.join(self._working_dir, 'report')
         parallel_report_dir = os.path.join(report_dir, 'parallel')
+        conformance_filelist_path = self._prepare_filelist()
         if not os.path.exists(report_dir):
             os.mkdir(report_dir)
         if not os.path.exists(logs_dir):
             os.mkdir(logs_dir)
-        cmd = f'python3 {gtest_parallel_path}  {conformance_path} -w {worker_num} -d {logs_dir} '\
-            f'-- --input_folders={self._model_path} --device={self._device} --report_unique_name --output_folder={parallel_report_dir}'
+        
+        cmd = f'python3 {gtest_parallel_path}  {conformance_path} -w {worker_num} -d {logs_dir} -- ' \
+            f'--device {self._device} --input_folders {conformance_filelist_path} --report_unique_name --output_folder {parallel_report_dir}'
         logger.info(f"Stating conformance: {cmd}")
-        process = Popen(cmd, stdout=subprocess.PIPE, shell=True)
+        process = Popen(cmd, shell=True)
         out, err = process.communicate()
         if err is None:
+            pass
             for line in str(out).split('\n'):
                 logger.info(line)
         else:
@@ -146,25 +175,34 @@ class Conformance:
         final_report_name = f'report_{self._type}'
         merge_xml([parallel_report_dir], report_dir, final_report_name, self._type)
         logger.info(f"Conformance is successful. XML reportwas saved to {report_dir}")
-        return (os.path.join(report_dir, final_report_name), report_dir)
+        return (os.path.join(report_dir, final_report_name + ".xml"), report_dir)
 
-    # def summarize(self, xml_report_path:os.path, report_dir: os.path):
-    #     summary_root = ET.parse(xml_report_path)
-    #     create_summary(summary_root, report_dir)
-    #     copytree("./template/", report_dir)
-    #     logger.info(f"Report was saved to {report_dir}/report.html")
+    def summarize(self, xml_report_path:os.path, report_dir: os.path):
+        summary_root = ET.parse(xml_report_path).getroot()
+        create_summary(summary_root, report_dir, "", "", False, True)
+        copytree("template/", os.path.join(report_dir, "template"))
+        logger.info(f"Report was saved to {report_dir}/report.html")
 
     def start_pipeline(self, dump_models: bool):
+        logger.info(f"[ARGUMENTS] --device = {self._device}")
+        logger.info(f"[ARGUMENTS] --ov_binaries = {self._ov_binaries}")
+        logger.info(f"[ARGUMENTS] --models_path = {self._model_path}")
+        logger.info(f"[ARGUMENTS] --working_dir = {self._working_dir}")
+        logger.info(f"[ARGUMENTS] --type = {self._type}")
+        logger.info(f"[ARGUMENTS] --dump_conformance = {dump_models}")
+
         if dump_models:
-            if not os.path.exists(self._model_path):
+            if self._model_path == ".":
                 self._model_path = self.download_and_convert_models()
             self.dump_subgraph()
         if not os.path.isdir(self._model_path):
             raise Exception(f"Directory {self._model_path} does not exist")
         xml_report, report_dir = self.run_conformance()
-        # self.summarize(xml_report, report_dir)
+        self.summarize(xml_report, report_dir)
         
 if __name__ == "__main__":
     args = parse_arguments()
     conformance = Conformance(args.device, args.models_path, args.ov_binaries, args.type, args.working_dir)
     conformance.start_pipeline(args.dump_conformance)
+
+ 
