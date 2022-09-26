946e22cf8c00155d2c366df52ddb1d71e1190f03
Tomasz Adamowicz <tomasz.adamowicz@intel.com>
2022-09-26 11:24:20 +0200

    [GNA] Add UT for OV & GNA ver. export/import from model

--- src/plugins/intel_gna/gna_model_serial.hpp
+++ src/plugins/intel_gna/gna_model_serial.hpp
@@ -17,6 +17,15 @@
 #include "gna_device_allocation.hpp"
 
 /**
+ * @brief helper class for GNAGraph serialization tasks
+ */
+class GNAVersionSerializer {
+public:
+    void Export(std::ostream& os) const;
+    std::string Import(std::istream& is) const;
+};
+
+/**
  * @brief implements serialization tasks for GNAGraph
  */
 class GNAModelSerial {
@@ -31,7 +40,7 @@ private:
     TranspositionInfoMap inputs_transpose_info_;
     TranspositionInfoMap outputs_transpose_info_;
     GNAPluginNS::HeaderLatest::ModelHeader model_header_;
-    ov::log::Level log_level_;
+    GNAVersionSerializer version_;
 
     void ImportInputs(std::istream &is, void* basePtr, GNAPluginNS::GnaInputs &inputs);
 
@@ -48,20 +57,17 @@ private:
     void AppendTensorNameIfNeeded(GNAPluginNS::GnaDesc& nodeDesc) const;
 
  public:
-    GNAModelSerial(Gna2Model* model, MemoryType& states_holder, ov::log::Level log_level = ov::log::Level::NO)
+    GNAModelSerial(Gna2Model* model, MemoryType& states_holder)
          : gna2model_(model),
-           pstates_(&states_holder),
-           log_level_(log_level) {
+           pstates_(&states_holder) {
     }
 
     GNAModelSerial(Gna2Model* model,
                    GNAPluginNS::GnaInputs& inputs,
-                   GNAPluginNS::GnaOutputs& outputs,
-                   ov::log::Level log_level = ov::log::Level::NO)
+                   GNAPluginNS::GnaOutputs& outputs)
         : gna2model_(model),
           inputs_(inputs),
-          outputs_(outputs),
-          log_level_(log_level) {
+          outputs_(outputs) {
     }
 
     void setHeader(GNAPluginNS::HeaderLatest::ModelHeader header) {
@@ -112,7 +118,8 @@ private:
                 GNAPluginNS::GnaInputs &inputs,
                 GNAPluginNS::GnaOutputs &outputs,
                 TranspositionInfoMap& inputstranspositionInfo,
-                TranspositionInfoMap& outputstranspositionInfo);
+                TranspositionInfoMap& outputstranspositionInfo,
+                std::string& modelLibVersion);
 
     /**
      * save gna graph to an outpus stream
