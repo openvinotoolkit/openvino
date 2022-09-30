b09225f6157b0c3ca4d9da3c6e5ea4ccfb882460
Ilya Churaev <ilya.churaev@intel.com>
2022-09-30 09:59:09 +0400

    Moved plugin to standalone folder and fixed build

--- cmake/extra_modules.cmake
+++ cmake/extra_modules.cmake
@@ -35,13 +35,15 @@ function(ov_generate_dev_package_config)
     find_package(OpenCV QUIET)
 
     foreach(component IN LISTS openvino_export_components)
-        string(FIND "${component}" "_legacy" index)
-        if(index EQUAL -1)
-            # export all targets with prefix and use them during extra modules build
-            export(TARGETS ${${component}} NAMESPACE openvino::
-                   APPEND FILE "${CMAKE_BINARY_DIR}/ov_${component}_dev_targets.cmake")
-            list(APPEND all_dev_targets ${${component}})
-        endif()
+        # TODO: remove legacy targets from tests
+        # string(FIND "${component}" "_legacy" index)
+        # if(index EQUAL -1)
+
+        # export all targets with prefix and use them during extra modules build
+        export(TARGETS ${${component}} NAMESPACE openvino::
+               APPEND FILE "${CMAKE_BINARY_DIR}/ov_${component}_dev_targets.cmake")
+        list(APPEND all_dev_targets ${${component}})
+        # endif()
     endforeach()
     add_custom_target(ov_dev_targets DEPENDS ${all_dev_targets})
 
