64101938a12c7b05472a1ccbacbec869ae6164b1
Mateusz Tabaka <mateusz.tabaka@intel.com>
2022-10-03 08:35:43 +0200

    Use Output<Node> of If inputs to replace its body parameters in UnrollIf (#13287)
    Fixes case when input node to a an If operator has multiple outputs itself.
    
    Ticket: 93116

--- src/common/transformations/src/transformations/control_flow/unroll_if.cpp
+++ src/common/transformations/src/transformations/control_flow/unroll_if.cpp
@@ -37,7 +37,7 @@ bool ngraph::pass::UnrollIf::run_on_model(const std::shared_ptr<ngraph::Function
         for (const auto& input_descr : input_descriptions) {
             auto in_data = if_node->input_value(input_descr->m_input_index);
             auto& param = body->get_parameters()[input_descr->m_body_parameter_index];
-            ngraph::replace_node(param, in_data.get_node_shared_ptr());
+            ngraph::replace_node(param, {in_data});
         }
         for (const auto& output_desc : output_descriptions) {
             std::shared_ptr<opset8::Result> result = body->get_results()[output_desc->m_body_value_index];
