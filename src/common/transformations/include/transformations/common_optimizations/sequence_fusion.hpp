5447a69e2c1c167e1cb4ed850e7b1d7ae67f2a7f
Ivan Tikhonov <ivan.tikhonov@intel.com>
2022-10-03 09:36:54 +0300

    SequenceFusion transformation (#12845)
    * SequenceFusion transformation and tests
    
    * Enable SequenceFusion transformation in MOC
    
    * add missed includes
    
    * fix type
    
    * fix build, apply review comments
    
    * fix build
    
    * fix build
    
    * fix build again
    
    * use ov namespace in has_result_consumers function
    
    * fix win build
    
    * try to fix win build
    
    * investigate issue on win platform
    
    * investigate issue on win platform
    
    * investigate issue on win
    
    * issue on win platform
    
    * remove the transformation from MOC
    
    * fix LSTMCell fusion, simplify transformation implementation, fix copying tensor and friendly names
    
    * clean up
    
    * add support for LSTMCell v0, resolve review comments, enable additional tests

---
+++ src/common/transformations/include/transformations/common_optimizations/sequence_fusion.hpp
@@ -0,0 +1,34 @@
+// Copyright (C) 2018-2022 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <memory>
+#include <openvino/pass/graph_rewrite.hpp>
+#include <transformations_visibility.hpp>
+#include <vector>
+
+namespace ov {
+namespace pass {
+
+class TRANSFORMATIONS_API SequenceFusion;
+
+}  // namespace pass
+}  // namespace ov
+
+/**
+ * @ingroup ie_transformation_common_api
+ * @brief SequenceFusion transformation replaces a chain of Cells
+ * operations with single Sequence op.
+ *
+ * Supported cells: GRUCell, LSTMCell, RNNCell, AUGRUCell
+ * Prerequisites: the source of W,R,B inputs must be the same or
+ * it can be different Constants with the same type, shape and value.
+ */
+
+class ov::pass::SequenceFusion : public ov::pass::MatcherPass {
+public:
+    OPENVINO_RTTI("SequenceFusion", "0");
+    SequenceFusion();
+};
