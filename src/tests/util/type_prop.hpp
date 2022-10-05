c33186976b968d73c897333ef431b59424f3b2da
Pawel Raasz <pawel.raasz@intel.com>
2022-10-05 15:21:36 +0200

    Review opset1 matmul shape inference aspects (#13333)
    * Add label propagate test for shape inference
    
    * Update MatMul tests for StaticShape
    
    * MatMul label propagation, reshape input with label
    
    * Apply clang-format to matmul shape inference tests

--- src/tests/util/type_prop.hpp
+++ src/tests/util/type_prop.hpp
@@ -5,6 +5,7 @@
 #pragma once
 
 #include "gtest/gtest.h"
+#include "openvino/core/partial_shape.hpp"
 
 #define EXPECT_HAS_SUBSTRING(haystack, needle) EXPECT_PRED_FORMAT2(testing::IsSubstring, needle, haystack)
 
@@ -14,3 +15,7 @@ struct PrintToDummyParamName {
         return "dummy" + std::to_string(info.index);
     }
 };
+
+std::vector<size_t> get_shape_labels(const ov::PartialShape& p_shape);
+
+void set_shape_labels(ov::PartialShape& p_shape, const std::vector<size_t>& labels);
