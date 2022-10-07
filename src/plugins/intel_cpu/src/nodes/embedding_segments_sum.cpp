f074cf96c1f5708b6670be562f08526a5d3fb937
dmitrygo <dmitry.gorokhov@intel.com>
2022-10-07 01:20:59 -0700

    Fixed EmbeddingSegmentsSum initialization with dynamic num_segments input

--- src/plugins/intel_cpu/src/nodes/embedding_segments_sum.cpp
+++ src/plugins/intel_cpu/src/nodes/embedding_segments_sum.cpp
@@ -91,6 +91,12 @@ void EmbeddingSegmentsSum::initFromInputs() {
         numSegments_ = reinterpret_cast<const int *>(getParentEdgeAt(NUM_SEGMENTS_IDX)->getMemoryPtr()->GetPtr())[0];
     }
 
+    if (getChildEdgesAtPort(0)[0]->getMemoryPtr()->GetShape().isDynamic()) {
+        auto newDims = getParentEdgesAtPort(EMB_TABLE_IDX)[0]->getMemory().getStaticDims();
+        newDims[0] = numSegments_;
+        redefineOutputMemory({newDims});
+    }
+
     if (getParentEdges().size() > DEFAULT_INDEX_IDX) {
         defaultIndices_ = reinterpret_cast<const int *>(getParentEdgeAt(DEFAULT_INDEX_IDX)->getMemoryPtr()->GetPtr());
     }
@@ -138,14 +144,13 @@ bool EmbeddingSegmentsSum::isExecutable() const {
 
 void EmbeddingSegmentsSum::execute(dnnl::stream strm) {
     const auto *srcData = reinterpret_cast<const uint8_t *>(getParentEdgeAt(0)->getMemoryPtr()->GetPtr());
-    auto *dstData = reinterpret_cast<uint8_t *>(getChildEdgeAt(0)->getMemoryPtr()->GetPtr());
     const uint8_t* weightsData = nullptr;
     if (_withWeights)
         weightsData = reinterpret_cast<const uint8_t *>(getParentEdgeAt(PER_SAMPLE_WEIGHTS_IDX)->getMemoryPtr()->GetPtr());
 
     const auto &inputMem  = getParentEdgeAt(0)->getMemory();
-    EmbeddingBagSum::execute(srcData, weightsData, dstData, inputMem .getDesc().getPrecision(),
-                                       inputMem .getStaticDims(), getChildEdgesAtPort(0)[0]->getMemory().GetShape().getStaticDims());
+    EmbeddingBagSum::execute(srcData, weightsData, inputMem.getDesc().getPrecision(),
+                                       inputMem.getStaticDims(), getChildEdgesAtPort(0)[0]->getMemoryPtr());
 }
 
 bool EmbeddingSegmentsSum::created() const {
