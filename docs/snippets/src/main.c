6e86ef3957a845dc1b45e3153cf3660d1542e93a
xuejun <Xuejun.Zhai@intel.com>
2022-10-09 10:20:35 +0800

    [C API DOC] C API user guide pipeline clear annotates
    Signed-off-by: xuejun <Xuejun.Zhai@intel.com>

--- docs/snippets/src/main.c
+++ docs/snippets/src/main.c
@@ -9,26 +9,26 @@
 int main() {
 
 //! [part1]
-ov_core_t* core = NULL; // need to free by ov_core_free(core)
+ov_core_t* core = NULL;
 ov_core_create(&core);
 //! [part1]
 
 const char* device_name = "CPU";
 {
 //! [part2_1]
-ov_compiled_model_t* compiled_model = NULL; // need to free by ov_compiled_model_free(compiled_model)
+ov_compiled_model_t* compiled_model = NULL;
 ov_core_compile_model_from_file(core, "model.xml", device_name, 0, &compiled_model);
 //! [part2_1]
 }
 {
 //! [part2_2]
-ov_compiled_model_t* compiled_model = NULL; // need to free by ov_compiled_model_free(compiled_model)
+ov_compiled_model_t* compiled_model = NULL;
 ov_core_compile_model_from_file(core, "model.onnx", device_name, 0, &compiled_model);
 //! [part2_2]
 }
 {
 //! [part2_3]
-ov_compiled_model_t* compiled_model = NULL; // need to free by ov_compiled_model_free(compiled_model)
+ov_compiled_model_t* compiled_model = NULL;
 ov_core_compile_model_from_file(core, "model.pdmodel", device_name, 0, &compiled_model);
 //! [part2_3]
 }
@@ -37,13 +37,13 @@ ov_core_compile_model_from_file(core, "model.pdmodel", device_name, 0, &compiled
 // Construct a model
 ov_model_t* model = NULL; // need to free by ov_model_free(model)
 ov_core_read_model(core, "model.xml", NULL, &model);
-ov_compiled_model_t* compiled_model = NULL; // need to free by ov_compiled_model_free(compiled_model)
+ov_compiled_model_t* compiled_model = NULL;
 ov_core_compile_model(core, model, device_name, 0, &compiled_model);
 //! [part2_4]
 
 
 //! [part3]
-ov_infer_request_t* infer_request = NULL; // need to free by ov_infer_request_free(infer_request)
+ov_infer_request_t* infer_request = NULL;
 ov_compiled_model_create_infer_request(compiled_model, &infer_request);
 //! [part3]
 
@@ -51,7 +51,7 @@ ov_shape_t input_shape;
 void* img_data = NULL;
 ov_element_type_e input_type = U8;
 //! [part4]
-ov_tensor_t* tensor = NULL; // need to free by ov_tensor_free(tensor)
+ov_tensor_t* tensor = NULL;
 // Create tensor from external memory
 ov_tensor_create_from_host_ptr(input_type, input_shape, img_data, &tensor);
 // Set input tensor for model with one input
@@ -63,7 +63,7 @@ ov_infer_request_infer(infer_request);
 //! [part5]
 
 //! [part6]
-ov_tensor_t* output_tensor = NULL; // need to free by ov_tensor_free(output_tensor)
+ov_tensor_t* output_tensor = NULL;
 // Get output tensor by tensor index
 ov_infer_request_get_output_tensor_by_index(infer_request, 0, &output_tensor);
 //! [part6]
