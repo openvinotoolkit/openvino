b2bf90da8dd092698c50cc4c33e0e9b4f61afe59
Mateusz <mateusz.mikolajczyk@intel.com>
2022-10-10 12:40:39 +0000

    Fix typo in test dim

--- src/core/tests/type_prop/is_inf.cpp
+++ src/core/tests/type_prop/is_inf.cpp
@@ -10,22 +10,22 @@ using namespace std;
 using namespace ov;
 
 TEST(type_prop, is_inf_default) {
-    const auto data = make_shared<opset10::Parameter>(element::f32, PartialShape{1, 64, 244, 244});
+    const auto data = make_shared<opset10::Parameter>(element::f32, PartialShape{1, 64, 256, 256});
     const auto is_inf = make_shared<opset10::IsInf>(data, opset10::IsInf::Attributes{});
 
     EXPECT_EQ(is_inf->get_element_type(), element::boolean)
         << "The output element type of IsInf should always be boolean";
-    EXPECT_TRUE(is_inf->get_output_partial_shape(0).same_scheme(PartialShape{1, 64, 224, 224}))
+    EXPECT_TRUE(is_inf->get_output_partial_shape(0).same_scheme(PartialShape{1, 64, 256, 256}))
         << "The output shape of IsInf is incorrect, it should be the same as shape of input data";
 }
 
 TEST(type_prop, is_inf_dynamic_batch) {
-    const auto data = make_shared<opset10::Parameter>(element::f32, PartialShape{Dimension::dynamic(), 64, 244, 244});
+    const auto data = make_shared<opset10::Parameter>(element::f32, PartialShape{Dimension::dynamic(), 64, 256, 256});
     const auto is_inf = make_shared<opset10::IsInf>(data, opset10::IsInf::Attributes{});
 
     EXPECT_EQ(is_inf->get_element_type(), element::boolean)
         << "The output element type of IsInf should always be boolean";
-    EXPECT_TRUE(is_inf->get_output_partial_shape(0).same_scheme(PartialShape{Dimension::dynamic(), 64, 224, 224}))
+    EXPECT_TRUE(is_inf->get_output_partial_shape(0).same_scheme(PartialShape{Dimension::dynamic(), 64, 256, 256}))
         << "The output shape of IsInf is incorrect, it should be the same as shape of input data";
 }
 
@@ -40,31 +40,31 @@ TEST(type_prop, is_inf_scalar) {
 }
 
 TEST(type_prop, is_inf_bfloat16) {
-    const auto data = make_shared<opset10::Parameter>(element::bf16, PartialShape{1, 64, 244, 244});
+    const auto data = make_shared<opset10::Parameter>(element::bf16, PartialShape{1, 64, 256, 256});
     const auto is_inf = make_shared<opset10::IsInf>(data, opset10::IsInf::Attributes{});
 
     EXPECT_EQ(is_inf->get_element_type(), element::boolean)
         << "The output element type of IsInf should always be boolean";
-    EXPECT_TRUE(is_inf->get_output_partial_shape(0).same_scheme(PartialShape{1, 64, 244, 244}))
+    EXPECT_TRUE(is_inf->get_output_partial_shape(0).same_scheme(PartialShape{1, 64, 256, 256}))
         << "The output shape of IsInf is incorrect, it should be the same as shape of input data";
 }
 
 TEST(type_prop, is_inf_float16) {
-    const auto data = make_shared<opset10::Parameter>(element::f16, PartialShape{1, 64, 244, 244});
+    const auto data = make_shared<opset10::Parameter>(element::f16, PartialShape{1, 64, 256, 256});
     const auto is_inf = make_shared<opset10::IsInf>(data, opset10::IsInf::Attributes{});
 
     EXPECT_EQ(is_inf->get_element_type(), element::boolean)
         << "The output element type of IsInf should always be boolean";
-    EXPECT_TRUE(is_inf->get_output_partial_shape(0).same_scheme(PartialShape{1, 64, 244, 244}))
+    EXPECT_TRUE(is_inf->get_output_partial_shape(0).same_scheme(PartialShape{1, 64, 256, 256}))
         << "The output shape of IsInf is incorrect, it should be the same as shape of input data";
 }
 
 TEST(type_prop, is_inf_float64) {
-    const auto data = make_shared<opset10::Parameter>(element::f64, PartialShape{1, 64, 244, 244});
+    const auto data = make_shared<opset10::Parameter>(element::f64, PartialShape{1, 64, 256, 256});
     const auto is_inf = make_shared<opset10::IsInf>(data, opset10::IsInf::Attributes{});
 
     EXPECT_EQ(is_inf->get_element_type(), element::boolean)
         << "The output element type of IsInf should always be boolean";
-    EXPECT_TRUE(is_inf->get_output_partial_shape(0).same_scheme(PartialShape{1, 64, 244, 244}))
+    EXPECT_TRUE(is_inf->get_output_partial_shape(0).same_scheme(PartialShape{1, 64, 256, 256}))
         << "The output shape of IsInf is incorrect, it should be the same as shape of input data";
 }
