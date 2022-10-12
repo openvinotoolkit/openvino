a40c201be2f65819f2ef2f50dbb0eee151c42905
Ilya Churaev <ilya.churaev@intel.com>
2022-10-12 15:35:17 +0400

    Added test to new meta format

--- src/frontends/ir/tests/meta_data_tests.cpp
+++ src/frontends/ir/tests/meta_data_tests.cpp
@@ -73,6 +73,66 @@ public:
 </net>
 )V0G0N";
 
+    std::string ir_with_new_meta = R"V0G0N(
+<net name="Network" version="11">
+    <layers>
+        <layer name="in1" type="Parameter" id="0" version="opset1">
+            <data element_type="f32" shape="1,3,22,22"/>
+            <output>
+                <port id="0" precision="FP32">
+                    <dim>1</dim>
+                    <dim>3</dim>
+                    <dim>22</dim>
+                    <dim>22</dim>
+                </port>
+            </output>
+        </layer>
+        <layer name="activation" id="1" type="ReLU" version="opset1">
+            <input>
+                <port id="1" precision="FP32">
+                    <dim>1</dim>
+                    <dim>3</dim>
+                    <dim>22</dim>
+                    <dim>22</dim>
+                </port>
+            </input>
+            <output>
+                <port id="2" precision="FP32">
+                    <dim>1</dim>
+                    <dim>3</dim>
+                    <dim>22</dim>
+                    <dim>22</dim>
+                </port>
+            </output>
+        </layer>
+        <layer name="output" type="Result" id="2" version="opset1">
+            <input>
+                <port id="0" precision="FP32">
+                    <dim>1</dim>
+                    <dim>3</dim>
+                    <dim>22</dim>
+                    <dim>22</dim>
+                </port>
+            </input>
+        </layer>
+    </layers>
+    <edges>
+        <edge from-layer="1" from-port="2" to-layer="2" to-port="0"/>
+        <edge from-layer="0" from-port="0" to-layer="1" to-port="1"/>
+    </edges>
+    <rt_info>
+        <MO_version value="TestVersion" />
+        <Runtime_version value="TestVersion" />
+        <conversion_parameters>
+            <input_shape value="[1, 3, 22, 22]" />
+            <transform value="" />
+            <use_new_frontend value="False" />
+        </conversion_parameters>
+        <optimization />
+    </rt_info>
+</net>
+)V0G0N";
+
     std::string ir_without_meta = R"V0G0N(
 <net name="Network" version="11">
     <layers>
@@ -138,6 +198,8 @@ TEST_F(MetaData, get_meta_data_as_map_from_model_without_info) {
 
     auto& rt_info = model->get_rt_info();
     auto it = rt_info.find("meta_data");
+    EXPECT_EQ(it, rt_info.end());
+    it = rt_info.find("MO_version");
     EXPECT_EQ(it, rt_info.end());
     ov::AnyMap meta;
     ASSERT_THROW(meta = model->get_rt_info<ov::AnyMap>("meta_data"), ov::Exception);
@@ -216,6 +278,38 @@ TEST_F(MetaData, get_meta_data_from_removed_file) {
     EXPECT_NO_THROW(cli_map = model->get_rt_info<ov::AnyMap>("conversion_parameters"));
 
     it = cli_map.find("input_shape");
+    ASSERT_NE(it, cli_map.end());
+    EXPECT_TRUE(it->second.is<std::string>());
+    EXPECT_EQ(it->second.as<std::string>(), "[1, 3, 22, 22]");
+
+    it = cli_map.find("transform");
+    ASSERT_NE(it, cli_map.end());
+    EXPECT_TRUE(it->second.is<std::string>());
+    EXPECT_EQ(it->second.as<std::string>(), "");
+
+    it = cli_map.find("use_new_frontend");
+    ASSERT_NE(it, cli_map.end());
+    EXPECT_TRUE(it->second.is<std::string>());
+    EXPECT_EQ(it->second.as<std::string>(), "False");
+}
+
+TEST_F(MetaData, get_meta_data_as_map_from_new_format) {
+    auto model = core.read_model(ir_with_new_meta, ov::Tensor());
+
+    auto& rt_info = model->get_rt_info();
+    ASSERT_TRUE(!rt_info.empty());
+    std::string version;
+    EXPECT_NO_THROW(version = model->get_rt_info<std::string>("MO_version"));
+    EXPECT_EQ(version, "TestVersion");
+    version = "";
+
+    EXPECT_NO_THROW(version = model->get_rt_info<std::string>("Runtime_version"));
+    EXPECT_EQ(version, "TestVersion");
+
+    ov::AnyMap cli_map;
+    EXPECT_NO_THROW(cli_map = model->get_rt_info<ov::AnyMap>("conversion_parameters"));
+
+    auto it = cli_map.find("input_shape");
     ASSERT_NE(it, cli_map.end());
     EXPECT_TRUE(it->second.is<std::string>());
     EXPECT_EQ(it->second.as<std::string>(), "[1, 3, 22, 22]");
