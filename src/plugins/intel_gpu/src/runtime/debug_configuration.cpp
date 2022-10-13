09aa452fd6d28289c5ab597ab6d1138adbdceb7f
hyunback kim <hyunback.kim@intel.com>
2022-10-13 14:47:03 +0900

    [GPU] Fix endless loop issue in onednn optimization post-opt (#13413)
    * [GPU] Fix endless loop issue in onednn optimization post-opt
    * Reproduced in yolo_v5s.int8
    * Add OV_GPU_DisableOnednnOptPostOps in debug_configuration

--- src/plugins/intel_gpu/src/runtime/debug_configuration.cpp
+++ src/plugins/intel_gpu/src/runtime/debug_configuration.cpp
@@ -105,6 +105,7 @@ static void print_help_messages() {
     message_list.emplace_back("OV_GPU_PrintMultiKernelPerf", "Print execution time of each kernel in multi-kernel primitimive");
     message_list.emplace_back("OV_GPU_DisableUsm", "Disable usm usage");
     message_list.emplace_back("OV_GPU_DisableOnednn", "Disable onednn for discrete GPU (no effect for integrated GPU)");
+    message_list.emplace_back("OV_GPU_DisableOnednnOptPostOps", "Disable onednn optimize post operators");
     message_list.emplace_back("OV_GPU_DumpProfilingData", "Enables dump of extended profiling information to specified directory."
                               " Note: Performance impact may be significant as this option enforces host side sync after each primitive");
     message_list.emplace_back("OV_GPU_DumpGraphs", "Dump optimized graph");
@@ -143,6 +144,7 @@ debug_configuration::debug_configuration()
         , print_multi_kernel_perf(0)
         , disable_usm(0)
         , disable_onednn(0)
+        , disable_onednn_opt_post_ops(0)
         , dump_profiling_data(std::string(""))
         , dump_graphs(std::string())
         , dump_sources(std::string())
@@ -167,6 +169,7 @@ debug_configuration::debug_configuration()
     get_gpu_debug_env_var("DumpLayersDstOnly", dump_layers_dst_only);
     get_gpu_debug_env_var("DumpLayersResult", dump_layers_result);
     get_gpu_debug_env_var("DisableOnednn", disable_onednn);
+    get_gpu_debug_env_var("DisableOnednnOptPostOps", disable_onednn_opt_post_ops);
     get_gpu_debug_env_var("DumpProfilingData", dump_profiling_data);
     get_gpu_debug_env_var("DryRunPath", dry_run_path);
     get_gpu_debug_env_var("BaseBatchForMemEstimation", base_batch_for_memory_estimation);
