798f9621bd66400b5365ad86f39f50d28b9e2211
Krivoruchko <georgy.krivoruchko@intel.com>
2022-10-18 14:44:05 +0400

    Modified Add test of TensorFlow to support AddV2 operation

--- tests/layer_tests/tensorflow_tests/test_tf_Add.py
+++ tests/layer_tests/tensorflow_tests/test_tf_Add.py
@@ -7,13 +7,22 @@ import pytest
 from common.tf_layer_test_class import CommonTFLayerTest
 from common.utils.tf_utils import permute_nchw_to_nhwc
 
+# Testing operation Add and AddV2
+# Documentation: https://www.tensorflow.org/api_docs/python/tf/raw_ops/Add
+#                https://www.tensorflow.org/api_docs/python/tf/raw_ops/AddV2
 
 class TestAdd(CommonTFLayerTest):
-    def create_add_placeholder_const_net(self, x_shape, y_shape, ir_version, use_new_frontend):
+    # x_shape - first argument, should be an array (shape)
+    # y_shape - second argument, should be an array (shape). Might be None if y_value is passed
+    # ir_version - common parameter
+    # use_new_frontend - common parameter
+    # y_value - fills y_shape by chosen value, uses randint instead. x_value isn't used because it isn't necessary right
+    # use_addv2 - force to use AddV2 operation
+    def create_add_placeholder_const_net(self, x_shape, y_shape, ir_version, use_new_frontend, y_value = None, use_addv2 = False):
         """
             Tensorflow net                  IR net
 
-            Placeholder->Add       =>       Placeholder->Add
+            Placeholder->Add       =>       Placeholder->Add/AddV2
                          /                               /
             Const-------/                   Const-------/
 
@@ -32,23 +41,23 @@ class TestAdd(CommonTFLayerTest):
             tf_y_shape = permute_nchw_to_nhwc(tf_y_shape, use_new_frontend)
 
             x = tf.compat.v1.placeholder(tf.float32, tf_x_shape, 'Input')
-            constant_value = np.random.randint(-256, 256, tf_y_shape).astype(np.float32)
-            if (constant_value == 0).all():
-                # Avoid elimination of the layer from IR
-                constant_value = constant_value + 1
+            if y_value is None:
+                constant_value = np.random.randint(-256, 256, tf_y_shape).astype(np.float32)
+                if (constant_value == 0).all():
+                    # Avoid elimination of the layer from IR
+                    constant_value = constant_value + 1
+            else:
+                constant_value = np.full(tf_y_shape, y_value, dtype=np.float32)
             y = tf.constant(constant_value)
 
-            add = tf.add(x, y, name="Operation")
+            if use_addv2 == False:
+                add = tf.raw_ops.Add(x=x, y=y, name="Operation")
+            else:
+                add = tf.raw_ops.AddV2(x=x, y=y, name="Operation")
             add_shape = add.shape.as_list()
 
             tf.compat.v1.global_variables_initializer()
             tf_net = sess.graph_def
-
-        #
-        #   Create reference IR net
-        #   Please, specify 'type': 'Input' for input node
-        #   Moreover, do not forget to validate ALL layer attributes!!!
-        #
 
         ref_net = None
 
@@ -58,7 +67,7 @@ class TestAdd(CommonTFLayerTest):
 
     test_data_1D = [
         dict(x_shape=[1], y_shape=[1]),
-        pytest.param(dict(x_shape=[3], y_shape=[3]), marks=pytest.mark.xfail(reason="*-19180"))
+        pytest.param(dict(x_shape=[3], y_shape=[3]), marks=pytest.mark.xfail(reason="19180"))
     ]
 
     @pytest.mark.parametrize("params", test_data_1D)
@@ -67,6 +76,20 @@ class TestAdd(CommonTFLayerTest):
                                       use_new_frontend, use_old_api):
         self._test(*self.create_add_placeholder_const_net(**params, ir_version=ir_version,
                                                           use_new_frontend=use_new_frontend),
+                   ie_device, precision, ir_version, temp_dir=temp_dir,
+                   use_new_frontend=use_new_frontend, use_old_api=use_old_api)
+
+    # Extend test data by specific V2 cases
+    test_data_1D_v2 = test_data_1D.copy() + [
+        dict(x_shape=[1], y_shape=[1], y_value=np.inf),
+        dict(x_shape=[1], y_shape=[1], y_value=np.NINF)
+    ]
+    @pytest.mark.parametrize("params", test_data_1D_v2)
+    @pytest.mark.nightly
+    def test_addv2_placeholder_const_1D(self, params, ie_device, precision, ir_version, temp_dir,
+                                      use_new_frontend, use_old_api):
+        self._test(*self.create_add_placeholder_const_net(**params, ir_version=ir_version,
+                                                          use_new_frontend=use_new_frontend, use_addv2 = True),
                    ie_device, precision, ir_version, temp_dir=temp_dir,
                    use_new_frontend=use_new_frontend, use_old_api=use_old_api)
 
@@ -84,6 +107,20 @@ class TestAdd(CommonTFLayerTest):
                                       use_new_frontend, use_old_api):
         self._test(*self.create_add_placeholder_const_net(**params, ir_version=ir_version,
                                                           use_new_frontend=use_new_frontend),
+                   ie_device, precision, ir_version, temp_dir=temp_dir,
+                   use_new_frontend=use_new_frontend, use_old_api=use_old_api)
+
+    # Extend test data by specific V2 cases
+    test_data_2D_v2 = test_data_2D.copy() + [
+        dict(x_shape=[1, 3], y_shape=[1, 3], y_value=np.inf),
+        dict(x_shape=[1, 3], y_shape=[1, 3], y_value=np.NINF)
+    ]
+    @pytest.mark.parametrize("params", test_data_2D_v2)
+    @pytest.mark.nightly
+    def test_addv2_placeholder_const_2D(self, params, ie_device, precision, ir_version, temp_dir,
+                                      use_new_frontend, use_old_api):
+        self._test(*self.create_add_placeholder_const_net(**params, ir_version=ir_version,
+                                                          use_new_frontend=use_new_frontend, use_addv2 = True),
                    ie_device, precision, ir_version, temp_dir=temp_dir,
                    use_new_frontend=use_new_frontend, use_old_api=use_old_api)
 
@@ -107,6 +144,20 @@ class TestAdd(CommonTFLayerTest):
                    ie_device, precision, ir_version, temp_dir=temp_dir,
                    use_new_frontend=use_new_frontend, use_old_api=use_old_api)
 
+    # Extend test data by specific V2 cases
+    test_data_3D_v2 = test_data_3D.copy() + [
+        dict(x_shape=[1, 1, 1], y_shape=[1, 1, 1], y_value=np.inf),
+        dict(x_shape=[1, 1, 1], y_shape=[1, 1, 1], y_value=np.NINF)
+    ]
+    @pytest.mark.parametrize("params", test_data_3D_v2)
+    @pytest.mark.nightly
+    def test_addv2_placeholder_const_3D(self, params, ie_device, precision, ir_version, temp_dir,
+                                      use_new_frontend, use_old_api):
+        self._test(*self.create_add_placeholder_const_net(**params, ir_version=ir_version,
+                                                          use_new_frontend=use_new_frontend, use_addv2 = True),
+                   ie_device, precision, ir_version, temp_dir=temp_dir,
+                   use_new_frontend=use_new_frontend, use_old_api=use_old_api)
+
     test_data_4D = [
         dict(x_shape=[1, 1, 1, 1], y_shape=[1, 1, 1, 1]),
         dict(x_shape=[1, 3, 1, 1], y_shape=[1, 3, 1, 1]),
@@ -122,6 +173,20 @@ class TestAdd(CommonTFLayerTest):
                                       use_new_frontend, use_old_api):
         self._test(*self.create_add_placeholder_const_net(**params, ir_version=ir_version,
                                                           use_new_frontend=use_new_frontend),
+                   ie_device, precision, ir_version, temp_dir=temp_dir,
+                   use_new_frontend=use_new_frontend, use_old_api=use_old_api)
+
+    # Extend test data by specific V2 cases
+    test_data_4D_v2 = test_data_4D.copy() + [
+        dict(x_shape=[1, 1, 1, 1], y_shape=[1, 1, 1, 1], y_value=np.inf),
+        dict(x_shape=[1, 1, 1, 1], y_shape=[1, 1, 1, 1], y_value=np.NINF)
+    ]
+    @pytest.mark.parametrize("params", test_data_4D_v2)
+    @pytest.mark.nightly
+    def test_addv2_placeholder_const_4D(self, params, ie_device, precision, ir_version, temp_dir,
+                                      use_new_frontend, use_old_api):
+        self._test(*self.create_add_placeholder_const_net(**params, ir_version=ir_version,
+                                                          use_new_frontend=use_new_frontend, use_addv2 = True),
                    ie_device, precision, ir_version, temp_dir=temp_dir,
                    use_new_frontend=use_new_frontend, use_old_api=use_old_api)
 
@@ -143,6 +208,20 @@ class TestAdd(CommonTFLayerTest):
                    ie_device, precision, ir_version=ir_version, temp_dir=temp_dir,
                    use_new_frontend=use_new_frontend, use_old_api=use_old_api)
 
+    # Extend test data by specific V2 cases
+    test_data_5D_v2 = test_data_5D.copy() + [
+        dict(x_shape=[1, 1, 1, 1, 1], y_shape=[1, 1, 1, 1, 1], y_value=np.inf),
+        dict(x_shape=[1, 1, 1, 1, 1], y_shape=[1, 1, 1, 1, 1], y_value=np.NINF)
+    ]
+    @pytest.mark.parametrize("params", test_data_5D_v2)
+    @pytest.mark.nightly
+    def test_addv2_placeholder_const_5D(self, params, ie_device, precision, ir_version, temp_dir,
+                                      use_new_frontend, use_old_api):
+        self._test(*self.create_add_placeholder_const_net(**params, ir_version=ir_version,
+                                                          use_new_frontend=use_new_frontend, use_addv2 = True),
+                   ie_device, precision, ir_version, temp_dir=temp_dir,
+                   use_new_frontend=use_new_frontend, use_old_api=use_old_api)
+
     ###############################################################################################
     #                                                                                             #
     #                                       Broadcast cases                                       #
@@ -162,6 +241,20 @@ class TestAdd(CommonTFLayerTest):
                    ie_device, precision, ir_version=ir_version, temp_dir=temp_dir,
                    use_new_frontend=use_new_frontend, use_old_api=use_old_api)
 
+    # Extend test data by specific V2 cases
+    test_data_broadcast_1D_v2 = test_data_broadcast_1D.copy() + [
+        dict(x_shape=[3], y_shape=[1], y_value=np.inf),
+        dict(x_shape=[3], y_shape=[1], y_value=np.NINF)
+    ]
+    @pytest.mark.parametrize("params", test_data_broadcast_1D_v2)
+    @pytest.mark.nightly
+    def test_addv2_placeholder_const_broadcast_1D(self, params, ie_device, precision, ir_version, temp_dir,
+                                      use_new_frontend, use_old_api):
+        self._test(*self.create_add_placeholder_const_net(**params, ir_version=ir_version,
+                                                          use_new_frontend=use_new_frontend, use_addv2 = True),
+                   ie_device, precision, ir_version, temp_dir=temp_dir,
+                   use_new_frontend=use_new_frontend, use_old_api=use_old_api)
+
     test_data_broadcast_2D = [
         dict(x_shape=[1, 1], y_shape=[1]),
         dict(x_shape=[1, 3], y_shape=[1]),
@@ -178,6 +271,20 @@ class TestAdd(CommonTFLayerTest):
         self._test(*self.create_add_placeholder_const_net(**params, ir_version=ir_version,
                                                           use_new_frontend=use_new_frontend),
                    ie_device, precision, ir_version=ir_version, temp_dir=temp_dir,
+                   use_new_frontend=use_new_frontend, use_old_api=use_old_api)
+
+    # Extend test data by specific V2 cases
+    test_data_broadcast_2D_v2 = test_data_broadcast_2D.copy() + [
+        dict(x_shape=[1, 1], y_shape=[1], y_value=np.inf),
+        dict(x_shape=[1, 1], y_shape=[1], y_value=np.NINF)
+    ]
+    @pytest.mark.parametrize("params", test_data_broadcast_2D_v2)
+    @pytest.mark.nightly
+    def test_addv2_placeholder_const_broadcast_2D(self, params, ie_device, precision, ir_version, temp_dir,
+                                      use_new_frontend, use_old_api):
+        self._test(*self.create_add_placeholder_const_net(**params, ir_version=ir_version,
+                                                          use_new_frontend=use_new_frontend, use_addv2 = True),
+                   ie_device, precision, ir_version, temp_dir=temp_dir,
                    use_new_frontend=use_new_frontend, use_old_api=use_old_api)
 
     test_data_broadcast_3D = [
@@ -205,6 +312,20 @@ class TestAdd(CommonTFLayerTest):
                    ie_device, precision, ir_version=ir_version, temp_dir=temp_dir,
                    use_new_frontend=use_new_frontend, use_old_api=use_old_api)
 
+    # Extend test data by specific V2 cases
+    test_data_broadcast_3D_v2 = test_data_broadcast_3D.copy() + [
+        dict(x_shape=[1, 1, 1], y_shape=[1], y_value=np.inf),
+        dict(x_shape=[1, 1, 1], y_shape=[1], y_value=np.NINF)
+    ]
+    @pytest.mark.parametrize("params", test_data_broadcast_3D_v2)
+    @pytest.mark.nightly
+    def test_addv2_placeholder_const_broadcast_3D(self, params, ie_device, precision, ir_version, temp_dir,
+                                      use_new_frontend, use_old_api):
+        self._test(*self.create_add_placeholder_const_net(**params, ir_version=ir_version,
+                                                          use_new_frontend=use_new_frontend, use_addv2 = True),
+                   ie_device, precision, ir_version, temp_dir=temp_dir,
+                   use_new_frontend=use_new_frontend, use_old_api=use_old_api)
+
     test_data_broadcast_4D = [
         dict(x_shape=[1, 1, 1, 1], y_shape=[1]),
         dict(x_shape=[1, 3, 1, 1], y_shape=[1]),
@@ -226,6 +347,20 @@ class TestAdd(CommonTFLayerTest):
         self._test(*self.create_add_placeholder_const_net(**params, ir_version=ir_version,
                                                           use_new_frontend=use_new_frontend),
                    ie_device, precision, ir_version=ir_version, temp_dir=temp_dir,
+                   use_new_frontend=use_new_frontend, use_old_api=use_old_api)
+
+    # Extend test data by specific V2 cases
+    test_data_broadcast_4D_v2 = test_data_broadcast_4D.copy() + [
+        dict(x_shape=[1, 1, 1, 1], y_shape=[1], y_value=np.inf),
+        dict(x_shape=[1, 1, 1, 1], y_shape=[1], y_value=np.NINF)
+    ]
+    @pytest.mark.parametrize("params", test_data_broadcast_4D_v2)
+    @pytest.mark.nightly
+    def test_addv2_placeholder_const_broadcast_4D(self, params, ie_device, precision, ir_version, temp_dir,
+                                      use_new_frontend, use_old_api):
+        self._test(*self.create_add_placeholder_const_net(**params, ir_version=ir_version,
+                                                          use_new_frontend=use_new_frontend, use_addv2 = True),
+                   ie_device, precision, ir_version, temp_dir=temp_dir,
                    use_new_frontend=use_new_frontend, use_old_api=use_old_api)
 
     test_data_broadcast_5D = [
@@ -251,3 +386,17 @@ class TestAdd(CommonTFLayerTest):
                    ie_device, precision,
                    ir_version=ir_version, temp_dir=temp_dir, use_new_frontend=use_new_frontend,
                    use_old_api=use_old_api)
+
+    # Extend test data by specific V2 cases
+    test_data_broadcast_5D_v2 = test_data_broadcast_5D.copy() + [
+        dict(x_shape=[1, 1, 1, 1, 1], y_shape=[1], y_value=np.inf),
+        dict(x_shape=[1, 1, 1, 1, 1], y_shape=[1], y_value=np.NINF)
+    ]
+    @pytest.mark.parametrize("params", test_data_broadcast_5D_v2)
+    @pytest.mark.nightly
+    def test_addv2_placeholder_const_broadcast_5D(self, params, ie_device, precision, ir_version, temp_dir,
+                                      use_new_frontend, use_old_api):
+        self._test(*self.create_add_placeholder_const_net(**params, ir_version=ir_version,
+                                                          use_new_frontend=use_new_frontend, use_addv2 = True),
+                   ie_device, precision, ir_version, temp_dir=temp_dir,
+                   use_new_frontend=use_new_frontend, use_old_api=use_old_api)
