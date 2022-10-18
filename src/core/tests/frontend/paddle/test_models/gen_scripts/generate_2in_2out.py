c992435d97f462f1467b50777d6981aae5b2f831
mei, yang <yang.mei@intel.com>
2022-10-18 15:14:03 +0800

    Meiyang/paddle 2.3 (#13272)
    * Migrate to PDPD 2.3
    
    * update supported paddle version to 2.3.2
    
    * update paddle test with paddle new static API on 2.3.2
    
    * fix issue of different output shape with paddle's
    
    * fix clang format issue
    
    * fix compiling issue
    
    * integrate paddle static save model api to original save model api
    
    Co-authored-by: Ilya Lavrenov <ilya.lavrenov@intel.com>
    Co-authored-by: Alina Kladieva <alina.kladieva@intel.com>

--- src/core/tests/frontend/paddle/test_models/gen_scripts/generate_2in_2out.py
+++ src/core/tests/frontend/paddle/test_models/gen_scripts/generate_2in_2out.py
@@ -6,6 +6,7 @@ from paddle import fluid
 import numpy as np
 import os
 import sys
+from save_model import saveModel
 
 
 paddle.enable_static()
@@ -38,5 +39,8 @@ inp_dict = {'inputX1': inp_blob1, 'inputX2': inp_blob2}
 var = [relu3a, relu3b]
 res_paddle = exe.run(fluid.default_main_program(), fetch_list=var, feed=inp_dict)
 
-fluid.io.save_inference_model(os.path.join(sys.argv[1], "2in_2out"), list(inp_dict.keys()), var, exe,
-                              model_filename="2in_2out.pdmodel", params_filename="2in_2out.pdiparams")
+saveModel("2in_2out", exe, feedkeys=[x1, x2],
+          fetchlist=var,
+          inputs=[inp_blob1, inp_blob2],
+          outputs=[res_paddle[0], res_paddle[1]], target_dir=sys.argv[1],
+          use_static_api=True)
