e99689a5d7a458f3b3b66c64536385aa5b1c5923
Anastasiia Pnevskaia <anastasia.popova@intel.com>
2022-10-18 11:57:19 +0200

    Use new API.

--- src/tests/functional/inference_engine/transformations/const_folding_for_mvn.cpp
+++ src/tests/functional/inference_engine/transformations/const_folding_for_mvn.cpp
@@ -5,7 +5,7 @@
 #include <gtest/gtest.h>
 
 #include "common_test_utils/test_common.hpp"
-#include "common_test_utils/ngraph_test_utils.hpp"
+#include "common_test_utils/graph_comparator.hpp"
 
 #include "openvino/opsets/opset10.hpp"
 #include "openvino/pass/constant_folding.hpp"
@@ -15,29 +15,29 @@
 
 using namespace testing;
 using namespace std;
-using namespace ngraph;
+using namespace ov;
 
 
 TEST(TransformationTests, ConstFoldingMVN) {
     shared_ptr<ov::Model> fun(nullptr);
     {
-        const auto in = make_shared<op::Parameter>(element::f32, Shape{6});
-        const auto mvn_in = make_shared<op::Constant>(element::f32, Shape{6}, vector<float>(6, 0.0));
-        const auto axes = make_shared<op::Constant>(element::i32, Shape{1}, vector<int> {0});
-        auto mvn = make_shared<op::v6::MVN>(mvn_in, axes, false, 1e-9, op::MVNEpsMode::OUTSIDE_SQRT);
-        auto add = make_shared<op::v1::Add>(in, mvn);
+        const auto in = make_shared<opset10::Parameter>(element::f32, Shape{6});
+        const auto mvn_in = make_shared<opset10::Constant>(element::f32, Shape{6}, vector<float>(6, 0.0));
+        const auto axes = make_shared<opset10::Constant>(element::i32, Shape{1}, vector<int> {0});
+        auto mvn = make_shared<opset10::MVN>(mvn_in, axes, false, 1e-9, op::MVNEpsMode::OUTSIDE_SQRT);
+        auto add = make_shared<opset10::Add>(in, mvn);
 
         fun = make_shared<ov::Model>(NodeVector {add}, ParameterVector {in});
 
-        ngraph::pass::Manager manager;
+        ov::pass::Manager manager;
         manager.register_pass<ov::pass::ConstantFolding>();
         manager.run_passes(fun);
     }
     shared_ptr<ov::Model> f_ref(nullptr);
     {
-        const auto in = make_shared<op::Parameter>(element::f32, Shape{6});
-        const auto mvn_const = make_shared<op::Constant>(element::f32, Shape{6}, vector<float>(6, 0.0));
-        auto add = make_shared<op::v1::Add>(in, mvn_const);
+        const auto in = make_shared<opset10::Parameter>(element::f32, Shape{6});
+        const auto mvn_const = make_shared<opset10::Constant>(element::f32, Shape{6}, vector<float>(6, 0.0));
+        auto add = make_shared<opset10::Add>(in, mvn_const);
 
         f_ref = make_shared<ov::Model>(NodeVector {add}, ParameterVector {in});
     }
