d146f945cbb1c78e8046d7b1330ee7b61fde18aa
sikorsl1 <leonard.sikorski@intel.com>
2022-10-18 06:34:43 +0000

    Changes requested by reviewer

--- src/core/src/op/is_nan.cpp
+++ src/core/src/op/is_nan.cpp
@@ -5,7 +5,6 @@
 #include "openvino/op/is_nan.hpp"
 
 #include "itt.hpp"
-#include "ngraph/runtime/host_tensor.hpp"
 
 ov::op::v10::IsNaN::IsNaN(const Output<Node>& data) : op::Op{{data}} {
     constructor_validate_and_infer_types();
--- src/core/tests/type_prop/is_nan.cpp
+++ src/core/tests/type_prop/is_nan.cpp
@@ -11,16 +11,14 @@ using namespace ov;
 using namespace ov::opset10;
 
 TEST(type_prop, isnan_output_shape) {
-    const auto data = make_shared<Parameter>(ov::element::f16, Shape{4, 2});
+    const auto data = make_shared<Parameter>(element::f16, Shape{4, 2});
     const auto isnan = make_shared<IsNaN>(data);
 
-    EXPECT_TRUE(isnan->get_output_partial_shape(0).same_scheme(PartialShape{4, 2}))
-        << "The output shape of IsNaN is incorrect";
-    ASSERT_EQ(isnan->get_shape(), (Shape{4, 2})) << "The output shape of IsNaN is incorrect";
+    EXPECT_EQ(isnan->get_output_partial_shape(0), (PartialShape{4, 2})) << "The output shape of IsNaN is incorrect";
 }
 
 TEST(type_prop, isnan_sample_dynamic_batch) {
-    const auto data = make_shared<Parameter>(ov::element::f16, PartialShape{Dimension::dynamic(), 21, 37});
+    const auto data = make_shared<Parameter>(element::f16, PartialShape{Dimension::dynamic(), 21, 37});
     const auto isnan = make_shared<IsNaN>(data);
 
     EXPECT_EQ(isnan->get_output_partial_shape(0), (PartialShape{Dimension::dynamic(), 21, 37}))
@@ -28,7 +26,7 @@ TEST(type_prop, isnan_sample_dynamic_batch) {
 }
 
 TEST(type_prop, isnan_sample_dynamic_shape) {
-    const auto data = make_shared<Parameter>(ov::element::f16, PartialShape::dynamic(5));
+    const auto data = make_shared<Parameter>(element::f16, PartialShape::dynamic(5));
     const auto isnan = make_shared<IsNaN>(data);
 
     EXPECT_EQ(isnan->get_output_partial_shape(0), (PartialShape::dynamic(5)))
@@ -36,7 +34,7 @@ TEST(type_prop, isnan_sample_dynamic_shape) {
 }
 
 TEST(type_prop, isnan_sample_interval_dimension) {
-    const auto data = make_shared<Parameter>(ov::element::f16, (PartialShape{Dimension(2, 4), 73, 12}));
+    const auto data = make_shared<Parameter>(element::f16, (PartialShape{Dimension(2, 4), 73, 12}));
     const auto isnan = make_shared<IsNaN>(data);
 
     EXPECT_EQ(isnan->get_output_partial_shape(0), (PartialShape{Dimension(2, 4), 73, 12}))
@@ -44,7 +42,27 @@ TEST(type_prop, isnan_sample_interval_dimension) {
 }
 
 TEST(type_prop, isnan_output_type) {
-    const auto data = make_shared<Parameter>(ov::element::f16, Shape{4, 2});
+    const auto data = make_shared<Parameter>(element::f16, Shape{4, 2});
+    const auto isnan = make_shared<IsNaN>(data);
+
+    EXPECT_EQ(isnan->get_element_type(), ov::element::boolean) << "The output element type of IsNaN is not a boolean.";
+}
+
+TEST(type_prop, isnan_bad_input_type) {
+    auto data = make_shared<Parameter>(element::i64, Shape{1, 2, 3});
+    try {
+        auto isnan = make_shared<op::v10::IsNaN>(data);
+        FAIL() << "IsNaN invalid input type not detected";
+    } catch (const ov::AssertFailure& error) {
+        const auto exp_msg = "The element type of the input tensor must be a floating point number.";
+        EXPECT_HAS_SUBSTRING(error.what(), exp_msg);
+    } catch (...) {
+        FAIL() << "Deduced type check failed for unexpected reason";
+    }
+}
+
+TEST(type_prop, isnan_dynamic_input_type) {
+    const auto data = make_shared<Parameter>(element::dynamic, Shape{3, 2, 1});
     const auto isnan = make_shared<IsNaN>(data);
 
     EXPECT_EQ(isnan->get_element_type(), ov::element::boolean) << "The output element type of IsNaN is not a boolean.";
