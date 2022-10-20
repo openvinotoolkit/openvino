d194499cd00f4adf7786e3848e62abd35d66627c
Mateusz Mikolajczyk <mateusz.mikolajczyk@intel.com>
2022-10-20 19:59:01 +0200

    IsInf operator (#13383)

--- src/bindings/python/tests/test_graph/test_create_op.py
+++ src/bindings/python/tests/test_graph/test_create_op.py
@@ -2201,6 +2201,55 @@ def test_interpolate_opset10(dtype, expected_shape, shape_calculation_mode):
     assert list(node.get_output_shape(0)) == expected_shape
 
 
+def test_is_inf_opset10_default():
+    input_shape = [2, 2, 2, 2]
+    input_node = ov.parameter(input_shape, dtype=np.float, name="InputData")
+    node = ov_opset10.is_inf(input_node)
+
+    assert node.get_type_name() == "IsInf"
+    assert node.get_output_size() == 1
+    assert list(node.get_output_shape(0)) == input_shape
+
+    node_attributes = node.get_attributes()
+    assert node_attributes["detect_positive"] is True
+    assert node_attributes["detect_negative"] is True
+
+
+def test_is_inf_opset10_custom_attribute():
+    input_shape = [2, 2, 2]
+    input_node = ov.parameter(input_shape, dtype=np.float, name="InputData")
+    attributes = {
+        "detect_positive": False,
+    }
+    node = ov_opset10.is_inf(input_node, attributes)
+
+    assert node.get_type_name() == "IsInf"
+    assert node.get_output_size() == 1
+    assert list(node.get_output_shape(0)) == input_shape
+
+    node_attributes = node.get_attributes()
+    assert node_attributes["detect_positive"] is False
+    assert node_attributes["detect_negative"] is True
+
+
+def test_is_inf_opset10_custom_all_attributes():
+    input_shape = [2, 2, 2]
+    input_node = ov.parameter(input_shape, dtype=np.float, name="InputData")
+    attributes = {
+        "detect_negative": False,
+        "detect_positive": True,
+    }
+    node = ov_opset10.is_inf(input_node, attributes)
+
+    assert node.get_type_name() == "IsInf"
+    assert node.get_output_size() == 1
+    assert list(node.get_output_shape(0)) == input_shape
+
+    node_attributes = node.get_attributes()
+    assert node_attributes["detect_positive"] is True
+    assert node_attributes["detect_negative"] is False
+
+
 def test_in_nan_opset10():
     input_shape = [1, 2, 3, 4]
     input_node = ov.parameter(input_shape, np.float, name="InputData")
