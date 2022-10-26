056b3188b869d6357a6063a8ef7f3e478aed704a
Sergey Shlyapnikov <sergey.shlyapnikov@intel.com>
2022-10-26 12:12:06 +0000

    [GPU] Add default context for each device

--- src/plugins/intel_gpu/include/intel_gpu/plugin/device_config.hpp
+++ src/plugins/intel_gpu/include/intel_gpu/plugin/device_config.hpp
@@ -63,6 +63,9 @@ struct Config {
     static bool isNewApiProperty(std::string property);
     static std::string ConvertPropertyToLegacy(const std::string& key, const std::string& value);
 
+    bool operator==(const Config& other) const;
+    bool operator!=(const Config& other) const { return !(*this == other); }
+
     std::string device_id;
     uint16_t throughput_streams;
     bool useProfiling;
