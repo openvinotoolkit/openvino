898f3f736cf53fdc5c9da93b7bf0f47f6a2796b2
Yan, Xiping <xiping.yan@intel.com>
2022-10-26 15:52:31 +0800

    fix macro define issue.

--- src/common/conditional_compilation/include/openvino/cc/pass/itt.hpp
+++ src/common/conditional_compilation/include/openvino/cc/pass/itt.hpp
@@ -45,21 +45,21 @@ OV_CC_DOMAINS(ov_pass);
 
 #    define RUN_ON_MODEL_SCOPE(region) MATCHER_SCOPE_(ov_pass, OV_PP_CAT(region, _run_on_model))
 
-#    define ADD_MATCHER_1(nspace, region, ...) add_matcher<nspace::region>(__VA_ARGS__);
-#    define ADD_MATCHER_0(nspace, region, ...)
-#    define ADD_MATCHER_SCOPE_WITHOUT_OBJ(nspace, region, ...)                          \
-        OV_PP_CAT(ADD_MATCHER_, OV_CC_SCOPE_IS_ENABLED(OV_PP_CAT3(ov_pass, _, region))) \
-        (nspace, region)
-#    define ADD_MATCHER_NO_OBJ_(obj, region, ...) obj->add_matcher<region>(__VA_ARGS__);
-#    define ADD_MATCHER_NO_OBJ_(obj, region, ...)
-#    define ADD_MATCHER_SCOPE_WITHOUT_NSPACE(obj, region, ...)                                 \
+#    define ADD_MATCHER_NO_OBJ_1(nspace, region, ...) add_matcher<nspace::region>(__VA_ARGS__);
+#    define ADD_MATCHER_NO_OBJ_0(nspace, region, ...)
+#    define ADD_MATCHER_SCOPE_WITHOUT_OBJ(nspace, region, ...)                                 \
         OV_PP_CAT(ADD_MATCHER_NO_OBJ_, OV_CC_SCOPE_IS_ENABLED(OV_PP_CAT3(ov_pass, _, region))) \
-        (obj, region)
+        (nspace, region, __VA_ARGS__)
+#    define ADD_MATCHER_NO_NSPACE_1(obj, region, ...) obj->add_matcher<region>(__VA_ARGS__);
+#    define ADD_MATCHER_NO_NSPACE_0(obj, region, ...)
+#    define ADD_MATCHER_SCOPE_WITHOUT_NSPACE(obj, region, ...)                                    \
+        OV_PP_CAT(ADD_MATCHER_NO_NSPACE_, OV_CC_SCOPE_IS_ENABLED(OV_PP_CAT3(ov_pass, _, region))) \
+        (obj, region, __VA_ARGS__)
 #    define ADD_MATCHER_NO_OBJ_NSPACE_1(region, ...) add_matcher<region>(__VA_ARGS__);
 #    define ADD_MATCHER_NO_OBJ_NSPACE_0(region, ...)
 #    define ADD_MATCHER_SCOPE_WITHOUT_OBJ_NSPACE(region, ...)                                         \
         OV_PP_CAT(ADD_MATCHER_NO_OBJ_NSPACE_, OV_CC_SCOPE_IS_ENABLED(OV_PP_CAT3(ov_pass, _, region))) \
-        (region)
+        (region, __VA_ARGS__)
 #    define ADD_MATCHER_OBJ_1(obj, nspace, region, ...) obj->add_matcher<nspace::region>(__VA_ARGS__);
 #    define ADD_MATCHER_OBJ_0(obj, nspace, region, ...)
 #    define ADD_MATCHER_SCOPE(obj, nspace, region, ...)                                     \
