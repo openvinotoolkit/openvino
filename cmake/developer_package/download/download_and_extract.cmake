453408cdc1b5146e18661b455448ebb8b59f020e
pkaczor <piotr.kaczor@intel.com>
2022-10-26 18:19:11 +0200

    Revert "Update GNA library version (#13188)"
    This reverts commit 17e702776b5023525da943d45394012c609d702e.

--- cmake/developer_package/download/download_and_extract.cmake
+++ cmake/developer_package/download/download_and_extract.cmake
@@ -5,13 +5,9 @@
 include(download/extract)
 include(download/download_and_check)
 
-function (GetNameAndUrlToDownload name url archive_name_unified archive_name_win archive_name_lin archive_name_mac archive_name_android USE_NEW_LOCATION)
+function (GetNameAndUrlToDownload name url archive_name_unified archive_name_win archive_name_lin archive_name_mac archive_name_android)
   if (archive_name_unified)
-      if (USE_NEW_LOCATION)
-        set (${url} "${archive_name_unified}" PARENT_SCOPE)
-      else()
-        set (${url} "thirdparty/unified/${archive_name_unified}" PARENT_SCOPE)
-      endif()
+    set (${url} "thirdparty/unified/${archive_name_unified}" PARENT_SCOPE)
     set (${name} ${archive_name_unified} PARENT_SCOPE)
   else()
     if(archive_name_lin)
@@ -47,26 +43,25 @@ function (DownloadAndExtractPlatformSpecific
   result_path
   folder
   sha256
-  files_to_extract
-  USE_NEW_LOCATION)
+  files_to_extract)
 
-  GetNameAndUrlToDownload(archive_name RELATIVE_URL ${archive_name_unified} ${archive_name_win} ${archive_name_lin} ${archive_name_mac} ${archive_name_android} ${USE_NEW_LOCATION})
+  GetNameAndUrlToDownload(archive_name RELATIVE_URL ${archive_name_unified} ${archive_name_win} ${archive_name_lin} ${archive_name_mac} ${archive_name_android} )
   if (NOT archive_name OR NOT RELATIVE_URL)
     return()
   endif()
-  CheckOrDownloadAndExtract(${component} ${RELATIVE_URL} ${archive_name} ${unpacked_path} result_path2 ${folder} TRUE FALSE TRUE ${sha256} ${files_to_extract} ${USE_NEW_LOCATION})
+  CheckOrDownloadAndExtract(${component} ${RELATIVE_URL} ${archive_name} ${unpacked_path} result_path2 ${folder} TRUE FALSE TRUE ${sha256} ${files_to_extract})
   set (${result_path} ${result_path2} PARENT_SCOPE)
 
 endfunction(DownloadAndExtractPlatformSpecific)
 
 #download from common folder
-function (DownloadAndExtract component archive_name unpacked_path result_path folder sha256 files_to_extract USE_NEW_LOCATION)
+function (DownloadAndExtract component archive_name unpacked_path result_path folder sha256 files_to_extract)
   set (RELATIVE_URL  "${archive_name}")
   set(fattal TRUE)
-  CheckOrDownloadAndExtract(${component} ${RELATIVE_URL} ${archive_name} ${unpacked_path} result_path2 ${folder} ${fattal} result TRUE ${sha256} ${files_to_extract} ${USE_NEW_LOCATION})
+  CheckOrDownloadAndExtract(${component} ${RELATIVE_URL} ${archive_name} ${unpacked_path} result_path2 ${folder} ${fattal} result TRUE ${sha256} ${files_to_extract})
 
   if (NOT ${result})
-    DownloadAndExtractPlatformSpecific(${component} ${archive_name} ${archive_name} ${archive_name} ${unpacked_path} ${result_path2} ${folder} ${sha256} ${files_to_extract} ${USE_NEW_LOCATION})
+    DownloadAndExtractPlatformSpecific(${component} ${archive_name} ${archive_name} ${archive_name} ${unpacked_path} ${result_path2} ${folder} ${sha256} ${files_to_extract})
   endif()
 
   set (${result_path} ${result_path2} PARENT_SCOPE)
@@ -153,7 +148,7 @@ function (DownloadOrExtractInternal URL archive_path unpacked_path folder fattal
 
 endfunction(DownloadOrExtractInternal)
 
-function (CheckOrDownloadAndExtract component RELATIVE_URL archive_name unpacked_path result_path folder fattal resultExt use_alternatives sha256 files_to_extract USE_NEW_LOCATION)
+function (CheckOrDownloadAndExtract component RELATIVE_URL archive_name unpacked_path result_path folder fattal resultExt use_alternatives sha256 files_to_extract)
   set (archive_path ${TEMP}/download/${archive_name})
   set (status "ON")
 
@@ -161,8 +156,6 @@ function (CheckOrDownloadAndExtract component RELATIVE_URL archive_name unpacked
     set(URL "${IE_PATH_TO_DEPS}/${RELATIVE_URL}")
   elseif(DEFINED ENV{IE_PATH_TO_DEPS})
     set(URL "$ENV{IE_PATH_TO_DEPS}/${RELATIVE_URL}")
-  elseif(USE_NEW_LOCATION)
-    set(URL "https://storage.openvinotoolkit.org/dependencies/${RELATIVE_URL}")
   else()
     set(URL "https://download.01.org/opencv/master/openvinotoolkit/${RELATIVE_URL}")
   endif()
