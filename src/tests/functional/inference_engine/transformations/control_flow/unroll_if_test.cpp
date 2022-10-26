7a3901b4ffa6734b3c9642517188f3ada92cfc22
Ivan <ivan.tikhonov@intel.com>
2022-10-26 13:31:58 +0400

    Unroll nested if

--- src/tests/functional/inference_engine/transformations/control_flow/unroll_if_test.cpp
+++ src/tests/functional/inference_engine/transformations/control_flow/unroll_if_test.cpp
@@ -18,33 +18,48 @@
 
 using namespace testing;
 
+std::shared_ptr<ov::Model> get_then_body() {
+    auto Xt = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
+    auto Yt = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
+    auto add_op = std::make_shared<ngraph::opset1::Add>(Xt, Yt);
+    auto then_op_result = std::make_shared<ngraph::opset1::Result>(add_op);
+    auto then_body = std::make_shared<ngraph::Function>(ngraph::OutputVector{ then_op_result }, ngraph::ParameterVector{ Xt, Yt });
+    return then_body;
+}
+
+std::shared_ptr<ov::Model> get_else_body() {
+    auto Xe = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
+    auto Ye = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
+    auto mul_op = std::make_shared<ngraph::opset1::Multiply>(Xe, Ye);
+    auto else_op_result = std::make_shared<ngraph::opset1::Result>(mul_op);
+    auto else_body = std::make_shared<ngraph::Function>(ngraph::OutputVector{ else_op_result }, ngraph::ParameterVector{ Xe, Ye });
+    return else_body;
+}
+
+std::shared_ptr<ov::Model> create_if_model(bool condition) {
+    auto X = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
+    auto Y = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
+    auto cond = std::make_shared<ngraph::opset1::Constant>(ngraph::element::boolean, ngraph::Shape{ 1 }, condition);
+    auto if_op = std::make_shared<ngraph::opset8::If>(cond);
+    const auto& then_body = get_then_body();
+    const auto& else_body = get_else_body();
+
+    if_op->set_then_body(then_body);
+    if_op->set_else_body(else_body);
+    auto then_p = then_body->get_parameters();
+    auto else_p = else_body->get_parameters();
+    if_op->set_input(X, then_p[0], else_p[0]);
+    if_op->set_input(Y, then_p[1], else_p[1]);
+    if_op->set_output(then_body->get_results()[0], else_body->get_results()[0]);
+    auto if_result = std::make_shared<ngraph::opset1::Result>(if_op);
+
+    return std::make_shared<ngraph::Function>(ngraph::NodeVector{ if_result }, ngraph::ParameterVector{ X, Y });
+}
+
 TEST(TransformationTests, UnrollIfCondIsTrue) {
     std::shared_ptr<ngraph::Function> f(nullptr), f_ref(nullptr);
     {
-        auto X = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
-        auto Y = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
-        auto cond = std::make_shared<ngraph::opset1::Constant>(ngraph::element::boolean, ngraph::Shape{ 1 }, true);
-        auto if_op = std::make_shared<ngraph::opset8::If>(cond);
-        auto Xt = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
-        auto Yt = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
-        auto add_op = std::make_shared<ngraph::opset1::Add>(Xt, Yt);
-        auto then_op_result = std::make_shared<ngraph::opset1::Result>(add_op);
-        auto then_body = std::make_shared<ngraph::Function>(ngraph::OutputVector{ then_op_result }, ngraph::ParameterVector{ Xt, Yt });
-
-        auto Xe = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
-        auto Ye = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
-        auto mul_op = std::make_shared<ngraph::opset1::Multiply>(Xe, Ye);
-        auto else_op_result = std::make_shared<ngraph::opset1::Result>(mul_op);
-        auto else_body = std::make_shared<ngraph::Function>(ngraph::OutputVector{ else_op_result }, ngraph::ParameterVector{ Xe, Ye });
-
-        if_op->set_then_body(then_body);
-        if_op->set_else_body(else_body);
-        if_op->set_input(X, Xt, Xe);
-        if_op->set_input(Y, Yt, Ye);
-        if_op->set_output(then_op_result, else_op_result);
-        auto if_result = std::make_shared<ngraph::opset1::Result>(if_op);
-
-        f = std::make_shared<ngraph::Function>(ngraph::NodeVector{ if_result }, ngraph::ParameterVector{ X, Y });
+        f = create_if_model(true);
 
         ngraph::pass::Manager manager;
         manager.register_pass<ngraph::pass::InitNodeInfo>();
@@ -55,11 +70,7 @@ TEST(TransformationTests, UnrollIfCondIsTrue) {
     }
 
     {
-        auto X = std::make_shared<ngraph::opset1::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
-        auto Y = std::make_shared<ngraph::opset1::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
-        auto add_op = std::make_shared<ngraph::opset1::Add>(X, Y);
-        auto if_result = std::make_shared<ngraph::opset1::Result>(add_op);
-        f_ref = std::make_shared<ngraph::Function>(ngraph::NodeVector{ if_result }, ngraph::ParameterVector{ X, Y });
+        f_ref = get_then_body();
     }
 
     auto res = compare_functions(f, f_ref);
@@ -69,30 +80,7 @@ TEST(TransformationTests, UnrollIfCondIsTrue) {
 TEST(TransformationTests, UnrollIfCondIsFalse) {
     std::shared_ptr<ngraph::Function> f(nullptr), f_ref(nullptr);
     {
-        auto X = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
-        auto Y = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
-        auto cond = std::make_shared<ngraph::opset1::Constant>(ngraph::element::boolean, ngraph::Shape{ 1 },  false);
-        auto if_op = std::make_shared<ngraph::opset8::If>(cond);
-        auto Xt = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
-        auto Yt = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
-        auto add_op = std::make_shared<ngraph::opset1::Add>(Xt, Yt);
-        auto then_op_result = std::make_shared<ngraph::opset1::Result>(add_op);
-        auto then_body = std::make_shared<ngraph::Function>(ngraph::OutputVector{ then_op_result }, ngraph::ParameterVector{ Xt, Yt });
-
-        auto Xe = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
-        auto Ye = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
-        auto mul_op = std::make_shared<ngraph::opset1::Multiply>(Xe, Ye);
-        auto else_op_result = std::make_shared<ngraph::opset1::Result>(mul_op);
-        auto else_body = std::make_shared<ngraph::Function>(ngraph::OutputVector{ else_op_result }, ngraph::ParameterVector{ Xe, Ye });
-
-        if_op->set_then_body(then_body);
-        if_op->set_else_body(else_body);
-        if_op->set_input(X, Xt, Xe);
-        if_op->set_input(Y, Yt, Ye);
-        if_op->set_output(then_op_result, else_op_result);
-        auto if_result = std::make_shared<ngraph::opset1::Result>(if_op);
-
-        f = std::make_shared<ngraph::Function>(ngraph::NodeVector{ if_result }, ngraph::ParameterVector{ X, Y });
+        f = create_if_model(false);
 
         ngraph::pass::Manager manager;
         manager.register_pass<ngraph::pass::InitNodeInfo>();
@@ -103,11 +91,7 @@ TEST(TransformationTests, UnrollIfCondIsFalse) {
     }
 
     {
-        auto X = std::make_shared<ngraph::opset1::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
-        auto Y = std::make_shared<ngraph::opset1::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
-        auto mul_op = std::make_shared<ngraph::opset1::Multiply>(X, Y);
-        auto if_result = std::make_shared<ngraph::opset1::Result>(mul_op);
-        f_ref = std::make_shared<ngraph::Function>(ngraph::NodeVector{ if_result }, ngraph::ParameterVector{ X, Y });
+        f_ref = get_else_body();
     }
 
     auto res = compare_functions(f, f_ref);
@@ -122,24 +106,18 @@ TEST(TransformationTests, UnrollIfWithSplitInput) {
         auto split = std::make_shared<ngraph::opset6::Split>(X, ngraph::opset6::Constant::create(ngraph::element::i32, ngraph::Shape{}, {0}), 2);
         auto cond = std::make_shared<ngraph::opset1::Constant>(ngraph::element::boolean, ngraph::Shape{ 1 }, false);
         auto if_op = std::make_shared<ngraph::opset8::If>(cond);
-        auto Xt = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 1, 3 });
-        auto Yt = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
-        auto add_op = std::make_shared<ngraph::opset1::Add>(Xt, Yt);
-        auto then_op_result = std::make_shared<ngraph::opset1::Result>(add_op);
-        auto then_body = std::make_shared<ngraph::Function>(ngraph::OutputVector{ then_op_result }, ngraph::ParameterVector{ Xt, Yt });
-
-        auto Xe = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 1, 3 });
-        auto Ye = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
-        auto mul_op = std::make_shared<ngraph::opset1::Multiply>(Xe, Ye);
-        auto else_op_result = std::make_shared<ngraph::opset1::Result>(mul_op);
-        auto else_body = std::make_shared<ngraph::Function>(ngraph::OutputVector{ else_op_result }, ngraph::ParameterVector{ Xe, Ye });
+        const auto& then_body = get_then_body();
+        const auto& else_body = get_else_body();
 
         if_op->set_then_body(then_body);
         if_op->set_else_body(else_body);
-        if_op->set_input(split->output(0), Xt, nullptr);
-        if_op->set_input(split->output(1), nullptr, Xe);
-        if_op->set_input(Y, Yt, Ye);
-        if_op->set_output(then_op_result, else_op_result);
+        auto then_p = then_body->get_parameters();
+        auto else_p = else_body->get_parameters();
+
+        if_op->set_input(split->output(0), then_p[0], nullptr);
+        if_op->set_input(split->output(1), nullptr, else_p[0]);
+        if_op->set_input(Y, then_p[1], else_p[1]);
+        if_op->set_output(then_body->get_results()[0], else_body->get_results()[0]);
         auto if_result = std::make_shared<ngraph::opset1::Result>(if_op);
 
         f = std::make_shared<ngraph::Function>(ngraph::NodeVector{ if_result }, ngraph::ParameterVector{ X, Y });
@@ -159,6 +137,80 @@ TEST(TransformationTests, UnrollIfWithSplitInput) {
         auto mul_op = std::make_shared<ngraph::opset1::Multiply>(split->output(1), Y);
         auto if_result = std::make_shared<ngraph::opset1::Result>(mul_op);
         f_ref = std::make_shared<ngraph::Function>(ngraph::NodeVector{ if_result }, ngraph::ParameterVector{ X, Y });
+    }
+
+    auto res = compare_functions(f, f_ref);
+    ASSERT_TRUE(res.first) << res.second;
+}
+
+TEST(TransformationTests, UnrollNestedIfThenBody) {
+    std::shared_ptr<ngraph::Function> f(nullptr), f_ref(nullptr);
+    {
+        auto X = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
+        auto Y = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
+
+        auto then_body = create_if_model(true);
+        auto else_body = create_if_model(false);
+
+        auto cond = std::make_shared<ngraph::opset1::Constant>(ngraph::element::boolean, ngraph::Shape{ 1 }, true);
+        auto if_op = std::make_shared<ngraph::opset8::If>(cond);
+
+        if_op->set_then_body(then_body);
+        if_op->set_else_body(else_body);
+        auto then_p = then_body->get_parameters();
+        auto else_p = else_body->get_parameters();
+        if_op->set_input(X, then_p[0], else_p[0]);
+        if_op->set_input(Y, then_p[1], else_p[1]);
+        if_op->set_output(then_body->get_results()[0], else_body->get_results()[0]);
+        auto if_result = std::make_shared<ngraph::opset1::Result>(if_op);
+
+        f = std::make_shared<ngraph::Function>(ngraph::NodeVector{ if_result }, ngraph::ParameterVector{ X, Y });
+        ngraph::pass::Manager manager;
+        manager.register_pass<ngraph::pass::InitNodeInfo>();
+        manager.register_pass<ngraph::pass::UnrollIf>();
+        manager.run_passes(f);
+        ASSERT_NO_THROW(check_rt_info(f));
+    }
+
+    {
+        f_ref = get_then_body();
+    }
+
+    auto res = compare_functions(f, f_ref);
+    ASSERT_TRUE(res.first) << res.second;
+}
+
+TEST(TransformationTests, UnrollNestedIfThenBody) {
+    std::shared_ptr<ngraph::Function> f(nullptr), f_ref(nullptr);
+    {
+        auto X = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
+        auto Y = std::make_shared<ngraph::opset6::Parameter>(ngraph::element::f32, ngraph::Shape{ 3 });
+
+        auto then_body = create_if_model(true);
+        auto else_body = create_if_model(false);
+
+        auto cond = std::make_shared<ngraph::opset1::Constant>(ngraph::element::boolean, ngraph::Shape{ 1 }, true);
+        auto if_op = std::make_shared<ngraph::opset8::If>(cond);
+
+        if_op->set_then_body(then_body);
+        if_op->set_else_body(else_body);
+        auto then_p = then_body->get_parameters();
+        auto else_p = else_body->get_parameters();
+        if_op->set_input(X, then_p[0], else_p[0]);
+        if_op->set_input(Y, then_p[1], else_p[1]);
+        if_op->set_output(then_body->get_results()[0], else_body->get_results()[0]);
+        auto if_result = std::make_shared<ngraph::opset1::Result>(if_op);
+
+        f = std::make_shared<ngraph::Function>(ngraph::NodeVector{ if_result }, ngraph::ParameterVector{ X, Y });
+        ngraph::pass::Manager manager;
+        manager.register_pass<ngraph::pass::InitNodeInfo>();
+        manager.register_pass<ngraph::pass::UnrollIf>();
+        manager.run_passes(f);
+        ASSERT_NO_THROW(check_rt_info(f));
+    }
+
+    {
+        f_ref = get_then_body();
     }
 
     auto res = compare_functions(f, f_ref);
