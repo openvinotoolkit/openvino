4ec5499c5d46e868c25ef33ab5dcb0c5b5c1e6ef
Luo Cheng <cheng.luo@intel.com>
2022-10-27 23:36:24 +0800

    [FrontEnd] add control flow, basic tensor array support for paddle faster_rcnn model (#10684)
    * support control flow, tensor array
    
    * fix clang error
    
    * support ppdet2.3 model
    
    * 1. code clean; 2. more comments inlined.
    
    * fix after clone ov::Model no more in need.
    
    * support dynamic shape; more comments
    
    * only process same rank
    
    * remove unused function
    
    * simplify the loop logic
    
    * fix review comments
    
    * support shape{1} {}
    
    * disable FoldSubgraphEmptyInputs because loop in loop
    
    * fix review comments
    
    * remove scalar{}->shape{1} testcase
    
    * allow re-infer shape when backedge changes input shape
    
    * fix condition shape to {1}
    
    * support output rank is not same or dynamic
    
    * fix refactor error
    
    * apply review comments
    
    * fix win warnings
    
    * remove TransformEliminateConvert
    
    Co-authored-by: jialipen <cecilia.peng@intel.com>
    Co-authored-by: Evgenya Stepyreva <evgenya.stepyreva@intel.com>

---
+++ src/frontends/paddle/src/internal/op/tensorarray_write.hpp
@@ -0,0 +1,32 @@
+// Copyright (C) 2018-2022 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include "openvino/op/op.hpp"
+
+namespace ov {
+namespace op {
+namespace internal {
+class TensorArrayWrite : public Op {
+public:
+    OPENVINO_OP("TensorArrayWrite", "internal");
+    BWDCMP_RTTI_DECLARATION;
+
+    TensorArrayWrite() = default;
+
+    TensorArrayWrite(const Output<Node>& input, const Output<Node>& index);
+
+    void validate_and_infer_types() override;
+
+    bool visit_attributes(AttributeVisitor& visitor) override;
+
+    std::shared_ptr<Node> clone_with_new_inputs(const OutputVector& new_args) const override;
+
+private:
+};
+
+}  // namespace internal
+}  // namespace op
+}  // namespace ov
