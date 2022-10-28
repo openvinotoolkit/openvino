677303346ede84e6321749fa9a56a362e09bdd8f
Maksim Kutakov <maksim.kutakov@intel.com>
2022-10-28 14:08:03 +0200

    Eye Fix

--- src/plugins/intel_cpu/src/nodes/eye.cpp
+++ src/plugins/intel_cpu/src/nodes/eye.cpp
@@ -49,7 +49,7 @@ private:
 } // namespace
 
 Eye::Eye(const std::shared_ptr<ov::Node>& op, const dnnl::engine& eng,
-         WeightsSharing::Ptr &cache) : Node(op, eng, cache, DefaultShapeInferFactory(op, 0x00)) {
+         WeightsSharing::Ptr &cache) : Node(op, eng, cache, EyeShapeInferFactory(op)) {
     std::string errorMessage;
     if (!isSupportedOperation(op, errorMessage)) {
             IE_THROW(NotImplemented) << errorMessage;
