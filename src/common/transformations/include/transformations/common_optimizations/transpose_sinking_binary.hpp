91eff6ec969c4da54e2178e2203b1d4f582e027d
Evgeny Kotov <evgeny.kotov@intel.com>
2022-10-28 16:19:39 +0200

    remove unneeded transformations

--- src/common/transformations/include/transformations/common_optimizations/transpose_sinking_binary.hpp
+++ src/common/transformations/include/transformations/common_optimizations/transpose_sinking_binary.hpp
@@ -11,47 +11,12 @@
 namespace ov {
 namespace pass {
 
-class TRANSFORMATIONS_API TransposeSinkingBinaryForward;
-class TRANSFORMATIONS_API TransposeSinkingBinaryBackward;
-class TRANSFORMATIONS_API TransposeSinkingConcatForward;
-class TRANSFORMATIONS_API TransposeSinkingConcatBackward;
-class TRANSFORMATIONS_API TransposeSinkingSplitForward;
 class TRANSFORMATIONS_API TransposeSinkingSplitBackward;
 class TRANSFORMATIONS_API TransposeSinkingElementwiseForward;
 class TRANSFORMATIONS_API TransposeSinkingElementwiseBackward;
 
 }  // namespace pass
 }  // namespace ngraph
-
-class ov::pass::TransposeSinkingBinaryForward : public ov::pass::MatcherPass {
-public:
-    OPENVINO_RTTI("ov::pass::TransposeSinkingBinaryForward", "0");
-    TransposeSinkingBinaryForward();
-};
-
-class ov::pass::TransposeSinkingBinaryBackward : public ov::pass::MatcherPass {
-public:
-    OPENVINO_RTTI("ov::pass::TransposeSinkingBinaryBackward", "0");
-    TransposeSinkingBinaryBackward();
-};
-
-class ov::pass::TransposeSinkingConcatForward : public ov::pass::MatcherPass {
-public:
-    OPENVINO_RTTI("ov::pass::TransposeSinkingConcatForward", "0");
-    TransposeSinkingConcatForward();
-};
-
-class ov::pass::TransposeSinkingConcatBackward : public ov::pass::MatcherPass {
-public:
-    OPENVINO_RTTI("ov::pass::TransposeSinkingConcatBackward", "0");
-    TransposeSinkingConcatBackward();
-};
-
-class ov::pass::TransposeSinkingSplitForward : public ov::pass::MatcherPass {
-public:
-    OPENVINO_RTTI("ov::pass::TransposeSinkingSplitForward", "0");
-    TransposeSinkingSplitForward();
-};
 
 class ov::pass::TransposeSinkingSplitBackward : public ov::pass::MatcherPass {
 public:
