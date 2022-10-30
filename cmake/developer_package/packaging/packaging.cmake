719a4e34ef25cb504b1643e8478328d0d260fbc0
Ilya Lavrenov <ilya.lavrenov@intel.com>
2022-10-30 16:44:13 +0400

    Reverted changes to install only SOVERSION library

--- cmake/developer_package/packaging/packaging.cmake
+++ cmake/developer_package/packaging/packaging.cmake
@@ -100,11 +100,19 @@ endif()
 #
 function(ov_install_with_name file component)
     if((APPLE AND file MATCHES "^[^\.]+\.[0-9]+${CMAKE_SHARED_LIBRARY_SUFFIX}$") OR
-                (file MATCHES "^.*\.${CMAKE_SHARED_LIBRARY_SUFFIX}(\.[0-9]+)+$"))
+                (file MATCHES "^.*\.${CMAKE_SHARED_LIBRARY_SUFFIX}\.[0-9]+$"))
+        if(IS_SYMLINK "${file}")
+            message("Original ${file}")
+            get_filename_component(actual_name "${file}" NAME)
+            get_filename_component(file "${file}" REALPATH)
+            set(install_rename RENAME "${actual_name}")
+        endif()
+
         install(FILES "${file}"
                 DESTINATION runtime/3rdparty/${component}/lib
                 COMPONENT ${component}
-                EXCLUDE_FROM_ALL)
+                EXCLUDE_FROM_ALL
+                ${install_rename})
 
         set("${component}_INSTALLED" ON PARENT_SCOPE)
     endif()
