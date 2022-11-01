c0bb6309f9223e7f05440a3dd288cb4dbddbeec7
Andrei Kochin <andrei.kochin@intel.com>
2022-11-01 13:57:33 +0400

    [MO Tests] Replace Caffe2 backend with ONNX RT

--- tests/layer_tests/onnx_tests/test_prelu.py
+++ tests/layer_tests/onnx_tests/test_prelu.py
@@ -4,12 +4,12 @@
 import numpy as np
 import pytest
 from common.layer_test_class import check_ir_version
-from common.onnx_layer_test_class import Caffe2OnnxLayerTest
+from common.onnx_layer_test_class import OnnxRuntimeLayerTest
 
 from unit_tests.utils.graph import build_graph
 
 
-class TestPRelu(Caffe2OnnxLayerTest):
+class TestPRelu(OnnxRuntimeLayerTest):
     def create_net(self, shape, slope_shape, precision, ir_version, opset=None):
         """
             ONNX net                     IR net
