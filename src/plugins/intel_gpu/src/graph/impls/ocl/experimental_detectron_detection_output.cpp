1193ae49e91f68a50fbbdd055f972328c283cda9
OlehKravchyshyn <86965088+OlehKravchyshyn@users.noreply.github.com>
2022-11-01 02:50:10 +0200

    [GPU] add blocked format to experimental detectron detection output (#12561)
    * [GPU] add blocked format to experimental detectron detection output
    
    * [GPU] add this kernel in whitelist of program.cpp
    
    Co-authored-by: ozhydkov-lohika <ozhydkov@lohika.com>

--- src/plugins/intel_gpu/src/graph/impls/ocl/experimental_detectron_detection_output.cpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl/experimental_detectron_detection_output.cpp
@@ -70,10 +70,17 @@ public:
 
 namespace detail {
 attach_experimental_detectron_detection_output_impl::attach_experimental_detectron_detection_output_impl() {
-    implementation_map<experimental_detectron_detection_output>::add(
-        impl_types::ocl,
-        experimental_detectron_detection_output_impl::create,
-        {std::make_tuple(data_types::f16, format::bfyx), std::make_tuple(data_types::f32, format::bfyx)});
+  const std::vector<data_types> types {data_types::f16, data_types::f32};
+  const std::vector<format::type> formats = {format::bfyx,
+                        format::b_fs_yx_fsv16,
+                        format::b_fs_yx_fsv32,
+                        format::bs_fs_yx_bsv16_fsv16,
+                        format::bs_fs_yx_bsv32_fsv32,
+                        format::bs_fs_yx_bsv32_fsv16};
+
+  implementation_map<experimental_detectron_detection_output>::add(
+      impl_types::ocl, experimental_detectron_detection_output_impl::create,
+      types, formats);
 }
 }  // namespace detail
 }  // namespace ocl
