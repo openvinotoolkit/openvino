bed140b0ff6e641df0968204be57318309efa284
Alexandra Sidorova <alexandra.sidorova@intel.com>
2022-11-02 17:45:54 +0300

    [Snippets] Fixed input and output original precisions in Subgraph (#13451)

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/snippets/add.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/snippets/add.cpp
@@ -13,30 +13,52 @@ namespace snippets {
 namespace {
 
 INSTANTIATE_TEST_SUITE_P(smoke_Snippets_Eltwise, Add,
-                     ::testing::Combine(
-                             ::testing::Values(ov::Shape {1, 42, 16, 64}),
-                             ::testing::Values(ov::Shape {1, 42, 16,  1}),
-                             ::testing::Values(1), // one node - Add
-                             ::testing::Values(0), // SnippetsMarkSkipped disables tokenization for eltwise chains after inputs
-                             ::testing::Values(CommonTestUtils::DEVICE_CPU)),
-                     Add::getTestCaseName);
+        ::testing::Combine(
+                ::testing::Values(ov::Shape {1, 42, 16, 64}),
+                ::testing::Values(ov::Shape {1, 42, 16,  1}),
+                ::testing::Values(ov::element::f32),
+                ::testing::Values(1), // one node - Add
+                ::testing::Values(0), // SnippetsMarkSkipped disables tokenization for eltwise chains after inputs
+                ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+        Add::getTestCaseName);
 
 INSTANTIATE_TEST_SUITE_P(smoke_Snippets_Eltwise, AddSinh,
         ::testing::Combine(
         ::testing::Values(ov::Shape {1, 42, 16, 64}),
         ::testing::Values(ov::Shape {1, 42, 16,  1}),
-        ::testing::Values(3), // Add + 2 converts after inputs
+        ::testing::Values(ov::element::f32),
+        ::testing::Values(3), // Add + 2 sinh after inputs
         ::testing::Values(1), // Subgraph is created, since the inputs are followed by converts
         ::testing::Values(CommonTestUtils::DEVICE_CPU)),
                          AddSinh::getTestCaseName);
 
 INSTANTIATE_TEST_SUITE_P(smoke_Snippets_Eltwise, AddSinhConst,
-                     ::testing::Combine(
-                             ::testing::Values(ov::Shape {1, 42, 16, 64}),
-                             ::testing::Values(2), // Add + 2 converts after inputs
-                             ::testing::Values(1), // Subgraph is created, since the inputs are followed by converts
-                             ::testing::Values(CommonTestUtils::DEVICE_CPU)),
-                     AddSinhConst::getTestCaseName);
+        ::testing::Combine(
+                ::testing::Values(ov::Shape {1, 42, 16, 64}),
+                ::testing::Values(ov::element::f32),
+                ::testing::Values(2), // Add + sinh after inputs
+                ::testing::Values(1), // Subgraph is created, since the inputs are followed by converts
+                ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+        AddSinhConst::getTestCaseName);
+
+INSTANTIATE_TEST_SUITE_P(smoke_Snippets_Eltwise, AddRollConst,
+        ::testing::Combine(
+                ::testing::Values(ov::Shape {1, 42, 16, 64}),
+                ::testing::Values(ov::element::f32),
+                ::testing::Values(2), // Add + roll after inputs
+                ::testing::Values(1), // Subgraph is created, since the inputs are followed by converts
+                ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+        AddRollConst::getTestCaseName);
+
+INSTANTIATE_TEST_SUITE_P(smoke_Snippets_Eltwise_BF16, AddRollConst,
+        ::testing::Combine(
+                ::testing::Values(ov::Shape {1, 42, 16, 64}),
+                ::testing::Values(ov::element::bf16),
+                ::testing::Values(3), // Add + reorder + roll after inputs
+                ::testing::Values(1), // Subgraph is created, since the inputs are followed by converts
+                ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+        AddRollConst::getTestCaseName);
+
 
 }  // namespace
 } // namespace snippets
--- src/tests/functional/plugin/shared/src/snippets/add.cpp
+++ src/tests/functional/plugin/shared/src/snippets/add.cpp
@@ -12,13 +12,15 @@ namespace snippets {
 
 std::string Add::getTestCaseName(testing::TestParamInfo<ov::test::snippets::AddParams> obj) {
     ov::Shape inputShapes0, inputShapes1, newInputShapes;
+    ov::element::Type type;
     std::string targetDevice;
     size_t num_nodes, num_subgraphs;
-    std::tie(inputShapes0, inputShapes1, num_nodes, num_subgraphs, targetDevice) = obj.param;
+    std::tie(inputShapes0, inputShapes1, type, num_nodes, num_subgraphs, targetDevice) = obj.param;
 
     std::ostringstream result;
     result << "IS[0]=" << CommonTestUtils::vec2str(inputShapes0) << "_";
     result << "IS[1]=" << CommonTestUtils::vec2str(inputShapes1) << "_";
+    result << "T=" << type << "_";
     result << "#N=" << num_nodes << "_";
     result << "#S=" << num_subgraphs << "_";
     result << "targetDevice=" << targetDevice;
@@ -27,30 +29,36 @@ std::string Add::getTestCaseName(testing::TestParamInfo<ov::test::snippets::AddP
 
 void Add::SetUp() {
     ov::Shape inputShape0, inputShape1;
-    std::tie(inputShape0, inputShape1, ref_num_nodes, ref_num_subgraphs, targetDevice) = this->GetParam();
+    ov::element::Type type;
+    std::tie(inputShape0, inputShape1, type, ref_num_nodes, ref_num_subgraphs, targetDevice) = this->GetParam();
     init_input_shapes({{{}, {inputShape0, }}, {{}, {inputShape1, }}});
 
     auto f = ov::test::snippets::AddFunction({inputShape0, inputShape1});
     function = f.getOriginal();
+    setInferenceType(type);
 }
 
 void AddSinh::SetUp() {
     ov::Shape inputShape0, inputShape1;
-    std::tie(inputShape0, inputShape1, ref_num_nodes, ref_num_subgraphs, targetDevice) = this->GetParam();
+    ov::element::Type type;
+    std::tie(inputShape0, inputShape1, type, ref_num_nodes, ref_num_subgraphs, targetDevice) = this->GetParam();
     init_input_shapes({{{}, {inputShape0, }}, {{}, {inputShape1, }}});
 
     auto f = ov::test::snippets::AddSinhFunction({inputShape0, inputShape1});
     function = f.getOriginal();
+    setInferenceType(type);
 }
 
 std::string AddSinhConst::getTestCaseName(testing::TestParamInfo<ov::test::snippets::AddConstParams> obj) {
     ov::Shape inputShapes, newInputShapes;
+    ov::element::Type type;
     std::string targetDevice;
     size_t num_nodes, num_subgraphs;
-    std::tie(inputShapes, num_nodes, num_subgraphs, targetDevice) = obj.param;
+    std::tie(inputShapes, type, num_nodes, num_subgraphs, targetDevice) = obj.param;
 
     std::ostringstream result;
     result << "IS[0]=" << CommonTestUtils::vec2str(inputShapes) << "_";
+    result << "T=" << type << "_";
     result << "#N=" << num_nodes << "_";
     result << "#S=" << num_subgraphs << "_";
     result << "targetDevice=" << targetDevice;
@@ -59,11 +67,24 @@ std::string AddSinhConst::getTestCaseName(testing::TestParamInfo<ov::test::snipp
 
 void AddSinhConst::SetUp() {
     ov::Shape inputShape;
-    std::tie(inputShape, ref_num_nodes, ref_num_subgraphs, targetDevice) = this->GetParam();
+    ov::element::Type type;
+    std::tie(inputShape, type, ref_num_nodes, ref_num_subgraphs, targetDevice) = this->GetParam();
     init_input_shapes({{{}, {inputShape, }}});
 
     auto f = ov::test::snippets::AddSinhConstFunction({inputShape});
     function = f.getOriginal();
+    setInferenceType(type);
+}
+
+void AddRollConst::SetUp() {
+    ov::Shape inputShape;
+    ov::element::Type type;
+    std::tie(inputShape, type, ref_num_nodes, ref_num_subgraphs, targetDevice) = this->GetParam();
+    init_input_shapes({{{}, {inputShape, }}});
+
+    auto f = ov::test::snippets::AddRollConstFunction({inputShape});
+    function = f.getOriginal();
+    setInferenceType(type);
 }
 
 TEST_P(Add, CompareWithRefImpl) {
@@ -80,6 +101,12 @@ TEST_P(AddSinhConst, CompareWithRefImpl) {
     run();
     validateNumSubgraphs();
 }
+
+TEST_P(AddRollConst, CompareWithRefImpl) {
+    run();
+    validateNumSubgraphs();
+}
+
 
 } // namespace snippets
 } // namespace test
