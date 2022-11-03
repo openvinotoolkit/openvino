3288d59cff4a7b1d63eda484b2af31e575b4a750
Marcin Kacprzak <marcin.kacprzak@intel.com>
2022-11-03 08:10:45 +0100

    Adjusted enum names to fit GNA 3.5

--- src/plugins/intel_gna/src/gna_device.hpp
+++ src/plugins/intel_gna/src/gna_device.hpp
@@ -52,8 +52,8 @@ class GNADeviceHelper : public GNAPluginNS::GNADevice {
 
     static const uint32_t TotalGna2InstrumentationPoints = 2;
     Gna2InstrumentationPoint gna2InstrumentationPoints[TotalGna2InstrumentationPoints] = {
-        Gna2InstrumentationPointHwTotalCycles,
-        Gna2InstrumentationPointHwStallCycles };
+        Gna2InstrumentationPointHwTotal,
+        Gna2InstrumentationPointHwStall};
 
     uint64_t instrumentationResults[TotalGna2InstrumentationPoints] = {};
     uint64_t instrumentationTotal[TotalGna2InstrumentationPoints] = {};
