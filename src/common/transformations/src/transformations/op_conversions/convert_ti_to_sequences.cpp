1265ba02c9fc7938e24b88e9d050ea67e01e130f
Ivan <ivan.tikhonov@intel.com>
2022-11-04 14:22:40 +0400

    fix warning, enable bf16, f64 ti tests

--- src/common/transformations/src/transformations/op_conversions/convert_ti_to_sequences.cpp
+++ src/common/transformations/src/transformations/op_conversions/convert_ti_to_sequences.cpp
@@ -106,11 +106,11 @@ bool convertTensorIteratorToSequence(const std::shared_ptr<ngraph::opset5::Tenso
     auto shape_of = std::make_shared<ngraph::opset5::ShapeOf>(X);
     auto batch_dimension =
         std::make_shared<ngraph::opset5::Gather>(shape_of,
-                                                 ngraph::opset5::Constant::create(ngraph::element::i64, {1}, {0}),
+                                                 ngraph::opset5::Constant::create(ngraph::element::i64, {1}, {batch_dim}),
                                                  ngraph::opset5::Constant::create(ngraph::element::i64, {}, {0}));
     auto seq_len_dim =
         std::make_shared<ngraph::opset5::Gather>(shape_of,
-                                                 ngraph::opset5::Constant::create(ngraph::element::i64, {1}, {1}),
+                                                 ngraph::opset5::Constant::create(ngraph::element::i64, {1}, {slice_axis}),
                                                  ngraph::opset5::Constant::create(ngraph::element::i64, {}, {0}));
     auto seq_lengths = std::make_shared<ngraph::opset5::Broadcast>(seq_len_dim, batch_dimension);
     auto axis_0 = ngraph::opset5::Constant::create(ngraph::element::i64, ngraph::Shape{1}, {0});
