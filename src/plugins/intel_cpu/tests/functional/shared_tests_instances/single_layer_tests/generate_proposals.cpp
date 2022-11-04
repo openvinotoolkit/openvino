b2b63fa33c57993f4db5e47e304d53d7f3a216f9
Andrii Voskoboinyk <avoskoboinyk@lohika.com>
2022-11-04 19:02:39 +0200

    Update test cases
    # Conflicts:
    #	src/plugins/intel_cpu/tests/functional/shared_tests_instances/single_layer_tests/generate_proposals.cpp

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/single_layer_tests/generate_proposals.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/single_layer_tests/generate_proposals.cpp
@@ -17,52 +17,98 @@ const std::vector<float> nms_threshold = { 0.699999988079071 };
 const std::vector<int64_t> post_nms_count = { 6 };
 const std::vector<int64_t> pre_nms_count = { 1000 };
 
+ov::Tensor create_and_fill_im_info_tensor(
+        const ov::element::Type element_type,
+        const ov::Shape& shape,
+        const uint32_t range,
+        const int32_t start_from,
+        const int32_t resolution,
+        const int seed) {
+    auto tensor = ov::Tensor{element_type, shape};
+
+    return tensor;
+}
+
+ov::Tensor create_and_fill_anchors_tensor(
+        const ov::element::Type element_type,
+        const ov::Shape& shape,
+        const uint32_t range,
+        const int32_t start_from,
+        const int32_t resolution,
+        const int seed) {
+    auto tensor = ov::Tensor{element_type, shape};
+
+    return tensor;
+}
+
+ov::Tensor create_and_fill_deltas_tensor(
+        const ov::element::Type element_type,
+        const ov::Shape& shape,
+        const uint32_t range,
+        const int32_t start_from,
+        const int32_t resolution,
+        const int seed) {
+    auto tensor = ov::Tensor{element_type, shape};
+
+    return tensor;
+}
+
+ov::Tensor create_and_fill_scores_tensor(
+        const ov::element::Type element_type,
+        const ov::Shape& shape,
+        const uint32_t range,
+        const int32_t start_from,
+        const int32_t resolution,
+        const int seed) {
+    auto tensor = ov::Tensor{element_type, shape};
+
+    return tensor;
+}
+
 const std::vector<std::pair<std::string, std::vector<ov::Tensor>>> inputTensors0 = {
     {
         "anchor_9",
         {
-            // 3
-            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 3}, 255, 0, 1, 17),
-            // 2 x 6 x 3 x 4 = 144
+            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 3}, 1080, 360, 1, 17),
             ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 6, 9, 4}, 255, 0, 1, 21),
-            // 2 x 36 x 2 x 6 = 144 * 2
-            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 36, 2, 6}, 255, 0, 1, 11),
-            // {2 x 9 x 2 x 6} = 36 * 2
-            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 9, 2, 6}, 255, 0, 1, 8)
-
-
+            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 36, 2, 6}, 10, 0, 1, 11),
+            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 9, 2, 6}, 10, 0, 10, 8)
         }
     },
 };
 
 const std::vector<std::pair<std::string, std::vector<ov::Tensor>>> inputTensors1 = {
     {
-        "anchor_16",
+        "anchor_11",
         {
-            // 3
-            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 3}, 255, 0, 1, 17),
-            // 2 x 6 x 3 x 4 = 144
-            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 6, 16, 4}, 255, 0, 1, 21),
-            // 2 x 64 x 2 x 6 = 144 * 2
-            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 64, 2, 6}, 255, 0, 1, 11),
-            // {2 x 16 x 2 x 6} = 36 * 2
-            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 16, 2, 6}, 255, 0, 1, 8)
+            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 3}, 1080, 360, 1, 17),
+            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 6, 11, 4}, 255, 0, 1, 21),
+            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 44, 2, 6}, 10, 0, 1, 11),
+            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 11, 2, 6}, 10, 0, 10, 8)
         }
     },
 };
 
 const std::vector<std::pair<std::string, std::vector<ov::Tensor>>> inputTensors2 = {
     {
+        "anchor_16",
+        {
+            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 3}, 1080, 360, 1, 17),
+            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 6, 16, 4}, 255, 0, 1, 21),
+            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 64, 2, 6}, 10, 0, 1, 11),
+            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 16, 2, 6}, 10, 0, 10, 8)
+        }
+    },
+};
+
+const std::vector<std::pair<std::string, std::vector<ov::Tensor>>> inputTensors3 = {
+    {
         "anchor_256",
         {
-            // 3
-            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 3}, 255, 0, 1, 17),
-            // 2 x 6 x 3 x 4 = 144
+            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 3}, 1080, 360, 1, 17),
             ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 6, 256, 4}, 255, 0, 1, 21),
-            // 2 x 1024 x 2 x 6 = 144 * 2
-            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 1024, 2, 6}, 255, 0, 1, 11),
-            // {2 x 256 x 2 x 6} = 36 * 2
-            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 256, 2, 6}, 255, 0, 1, 8)
+            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 1024, 2, 6}, 10, 0, 1, 11),
+            ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{2, 256, 2, 6}, 100, 0, 100, 8)
         }
     },
 };
@@ -84,6 +130,18 @@ const std::vector<std::vector<InputShape>> staticInputShape1 = {
     static_shapes_to_test_representation(
         {
             {2, 3},
+            {2, 6, 11, 4},
+            {2, 44, 2, 6},
+            {2, 11, 2, 6}
+        }
+    ),
+};
+
+const std::vector<std::vector<InputShape>> staticInputShape2 = {
+    // im_info / anchors / deltas / scores
+    static_shapes_to_test_representation(
+        {
+            {2, 3},
             {2, 6, 16, 4},
             {2, 64, 2, 6},
             {2, 16, 2, 6}
@@ -91,7 +149,7 @@ const std::vector<std::vector<InputShape>> staticInputShape1 = {
     ),
 };
 
-const std::vector<std::vector<InputShape>> staticInputShape2 = {
+const std::vector<std::vector<InputShape>> staticInputShape3 = {
     // im_info / anchors / deltas / scores
     static_shapes_to_test_representation(
         {
@@ -104,7 +162,7 @@ const std::vector<std::vector<InputShape>> staticInputShape2 = {
 };
 
 INSTANTIATE_TEST_SUITE_P(
-    smoke_GenerateProposalsLayerTest_staticInputShape0,
+    smoke_staticInputShape0,
     GenerateProposalsLayerTest,
     ::testing::Combine(
         ::testing::ValuesIn(staticInputShape0),
@@ -119,23 +177,23 @@ INSTANTIATE_TEST_SUITE_P(
     GenerateProposalsLayerTest::getTestCaseName);
 
 INSTANTIATE_TEST_SUITE_P(
-        smoke_GenerateProposalsLayerTest_staticInputShape1,
-        GenerateProposalsLayerTest,
-        ::testing::Combine(
-                ::testing::ValuesIn(staticInputShape1),
-                ::testing::ValuesIn(min_size),
-                ::testing::ValuesIn(nms_threshold),
-                ::testing::ValuesIn(post_nms_count),
-                ::testing::ValuesIn(pre_nms_count),
-                ::testing::ValuesIn({true, false}),
-                ::testing::ValuesIn(inputTensors1),
-                ::testing::Values(ov::element::Type_t::f32),
-                ::testing::Values(ov::element::Type_t::i32),
-                ::testing::Values(CommonTestUtils::DEVICE_CPU)),
-        GenerateProposalsLayerTest::getTestCaseName);
+    smoke_staticInputShape1,
+    GenerateProposalsLayerTest,
+    ::testing::Combine(
+        ::testing::ValuesIn(staticInputShape1),
+        ::testing::ValuesIn(min_size),
+        ::testing::ValuesIn(nms_threshold),
+        ::testing::ValuesIn(post_nms_count),
+        ::testing::ValuesIn(pre_nms_count),
+        ::testing::ValuesIn({true, false}),
+        ::testing::ValuesIn(inputTensors1),
+        ::testing::Values(ov::element::Type_t::f32),
+        ::testing::Values(ov::element::Type_t::i32),
+        ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+    GenerateProposalsLayerTest::getTestCaseName);
 
 INSTANTIATE_TEST_SUITE_P(
-    smoke_GenerateProposalsLayerTest_staticInputShape2,
+    smoke_staticInputShape2,
     GenerateProposalsLayerTest,
     ::testing::Combine(
         ::testing::ValuesIn(staticInputShape2),
@@ -150,6 +208,21 @@ INSTANTIATE_TEST_SUITE_P(
         ::testing::Values(CommonTestUtils::DEVICE_CPU)),
     GenerateProposalsLayerTest::getTestCaseName);
 
+INSTANTIATE_TEST_SUITE_P(
+    smoke_staticInputShape3,
+    GenerateProposalsLayerTest,
+    ::testing::Combine(
+        ::testing::ValuesIn(staticInputShape3),
+        ::testing::ValuesIn(min_size),
+        ::testing::ValuesIn(nms_threshold),
+        ::testing::ValuesIn(post_nms_count),
+        ::testing::ValuesIn(pre_nms_count),
+        ::testing::ValuesIn(inputTensors3),
+        ::testing::Values(ov::element::Type_t::f32),
+        ::testing::Values(ov::element::Type_t::i32),
+        ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+    GenerateProposalsLayerTest::getTestCaseName);
+
 struct GenerateProposalsBenchmarkTest : ov::test::BenchmarkLayerTest<GenerateProposalsLayerTest> {};
 
 TEST_P(GenerateProposalsBenchmarkTest, GenerateProposals_Benchmark) {
@@ -158,7 +231,7 @@ TEST_P(GenerateProposalsBenchmarkTest, GenerateProposals_Benchmark) {
 }
 
 INSTANTIATE_TEST_SUITE_P(
-    smoke_GenerateProposalsBenchmarkTest_staticInputShape0,
+    smoke_staticInputShape0,
     GenerateProposalsBenchmarkTest,
     ::testing::Combine(
         ::testing::ValuesIn(staticInputShape0),
@@ -170,10 +243,10 @@ INSTANTIATE_TEST_SUITE_P(
         ::testing::Values(ov::element::Type_t::f32),
         ::testing::Values(ov::element::Type_t::i32),
         ::testing::Values(CommonTestUtils::DEVICE_CPU)),
-    GenerateProposalsBenchmarkTest::getTestCaseName);
+    GenerateProposalsLayerTest::getTestCaseName);
 
 INSTANTIATE_TEST_SUITE_P(
-    smoke_GenerateProposalsBenchmarkTest_staticInputShape1,
+    smoke_staticInputShape1,
     GenerateProposalsBenchmarkTest,
     ::testing::Combine(
         ::testing::ValuesIn(staticInputShape1),
@@ -185,21 +258,36 @@ INSTANTIATE_TEST_SUITE_P(
         ::testing::Values(ov::element::Type_t::f32),
         ::testing::Values(ov::element::Type_t::i32),
         ::testing::Values(CommonTestUtils::DEVICE_CPU)),
-    GenerateProposalsBenchmarkTest::getTestCaseName);
+    GenerateProposalsLayerTest::getTestCaseName);
 
 INSTANTIATE_TEST_SUITE_P(
-        smoke_GenerateProposalsBenchmarkTest_staticInputShape2,
-        GenerateProposalsBenchmarkTest,
-        ::testing::Combine(
-                ::testing::ValuesIn(staticInputShape2),
-                ::testing::ValuesIn(min_size),
-                ::testing::ValuesIn(nms_threshold),
-                ::testing::ValuesIn(post_nms_count),
-                ::testing::ValuesIn(pre_nms_count),
-                ::testing::ValuesIn(inputTensors2),
-                ::testing::Values(ov::element::Type_t::f32),
-                ::testing::Values(ov::element::Type_t::i32),
-                ::testing::Values(CommonTestUtils::DEVICE_CPU)),
-        GenerateProposalsBenchmarkTest::getTestCaseName);
+    smoke_staticInputShape2,
+    GenerateProposalsBenchmarkTest,
+    ::testing::Combine(
+        ::testing::ValuesIn(staticInputShape2),
+        ::testing::ValuesIn(min_size),
+        ::testing::ValuesIn(nms_threshold),
+        ::testing::ValuesIn(post_nms_count),
+        ::testing::ValuesIn(pre_nms_count),
+        ::testing::ValuesIn(inputTensors2),
+        ::testing::Values(ov::element::Type_t::f32),
+        ::testing::Values(ov::element::Type_t::i32),
+        ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+    GenerateProposalsLayerTest::getTestCaseName);
+
+INSTANTIATE_TEST_SUITE_P(
+    smoke_staticInputShape3,
+    GenerateProposalsBenchmarkTest,
+    ::testing::Combine(
+        ::testing::ValuesIn(staticInputShape3),
+        ::testing::ValuesIn(min_size),
+        ::testing::ValuesIn(nms_threshold),
+        ::testing::ValuesIn(post_nms_count),
+        ::testing::ValuesIn(pre_nms_count),
+        ::testing::ValuesIn(inputTensors3),
+        ::testing::Values(ov::element::Type_t::f32),
+        ::testing::Values(ov::element::Type_t::i32),
+        ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+    GenerateProposalsLayerTest::getTestCaseName);
 
 } // namespace
