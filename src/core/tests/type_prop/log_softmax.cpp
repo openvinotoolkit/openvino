fb747ca24293b9b5b02549b25b458874c2310f89
Ilya Churaev <ilya.churaev@intel.com>
2022-11-07 15:11:12 +0400

    Fixed code style

--- src/core/tests/type_prop/log_softmax.cpp
+++ src/core/tests/type_prop/log_softmax.cpp
@@ -9,28 +9,21 @@
 using namespace std;
 using namespace ngraph;
 
-TEST(type_prop, log_softmax)
-{
+TEST(type_prop, log_softmax) {
     auto data = make_shared<op::Parameter>(element::f32, Shape{1, 3, 6});
     auto log_softmax_func = make_shared<op::v5::LogSoftmax>(data, 1);
     EXPECT_EQ(log_softmax_func->get_element_type(), element::f32);
     EXPECT_EQ(log_softmax_func->get_shape(), (Shape{1, 3, 6}));
 }
 
-TEST(type_prop, log_softmax_incorrect_axis)
-{
+TEST(type_prop, log_softmax_incorrect_axis) {
     const auto data = make_shared<op::Parameter>(element::f32, Shape{1, 3, 6});
 
-    try
-    {
+    try {
         auto log_softmax_func = make_shared<op::v5::LogSoftmax>(data, 3);
         FAIL() << "LogSoftmax node was created with incorrect axis.";
-    }
-    catch (const NodeValidationFailure& error)
-    {
-        EXPECT_HAS_SUBSTRING(
-            error.what(),
-            std::string("Reduction axis (3) is out of bounds"));
+    } catch (const NodeValidationFailure& error) {
+        EXPECT_HAS_SUBSTRING(error.what(), std::string("Reduction axis (3) is out of bounds"));
     }
 }
 
@@ -41,7 +34,7 @@ TEST(type_prop, log_softmax_incorrect_axis)
 //     EXPECT_EQ(log_softmax_func->get_element_type(), element::f32);
 //     ASSERT_TRUE(log_softmax_func->get_output_partial_shape(0).same_scheme(
 //         (PartialShape{1, Dimension::dynamic(), 6})));
-// 
+//
 //     // rank unknown
 //     auto log_softmax_partial = make_shared<op::v5::LogSoftmax>(
 //         make_shared<op::Parameter>(element::f32, PartialShape::dynamic()));
@@ -49,12 +42,10 @@ TEST(type_prop, log_softmax_incorrect_axis)
 //         log_softmax_partial->get_output_partial_shape(0).same_scheme(PartialShape::dynamic()));
 // }
 
-TEST(type_prop, log_softmax_partial_static_rank)
-{
+TEST(type_prop, log_softmax_partial_static_rank) {
     auto data = make_shared<op::Parameter>(element::f32, PartialShape{1, Dimension::dynamic(), 6});
     auto log_softmax_func = make_shared<op::v5::LogSoftmax>(data, 1);
     EXPECT_EQ(log_softmax_func->get_element_type(), element::f32);
-    ASSERT_TRUE(log_softmax_func->get_output_partial_shape(0).same_scheme(
-        (PartialShape{1, Dimension::dynamic(), 6})));
+    ASSERT_TRUE(log_softmax_func->get_output_partial_shape(0).same_scheme((PartialShape{1, Dimension::dynamic(), 6})));
     ASSERT_TRUE(log_softmax_func->get_output_partial_shape(0).rank().is_static());
 }
