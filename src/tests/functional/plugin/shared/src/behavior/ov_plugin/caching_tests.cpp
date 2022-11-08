4289eb48b6f272d9e8c34fb3b52af6ccb047c5c1
Eddy Kim <eddy.kim@intel.com>
2022-11-08 11:21:36 +0900

    setenv is disabled in windows

--- src/tests/functional/plugin/shared/src/behavior/ov_plugin/caching_tests.cpp
+++ src/tests/functional/plugin/shared/src/behavior/ov_plugin/caching_tests.cpp
@@ -182,7 +182,9 @@ void CompileModelCacheTestBase::run() {
         init_input_shapes(static_shapes_to_test_representation(inShapes));
     }
     if ((targetDevice.find("GPU") != std::string::npos)) {
+#if !defined(_WIN32) && !defined(_WIN64)
         setenv("OV_GPU_MODEL_CACHING", "1", 1);
+#endif
     }
     if ((targetDevice.find("AUTO") == std::string::npos) && !importExportSupported(*core)) {
         GTEST_COUT << "Plugin doesn't support import and export - skipping test" << std::endl;
@@ -221,7 +223,9 @@ void CompileModelCacheTestBase::run() {
         compare(originalOutputs, get_plugin_outputs());
     }
     if ((targetDevice.find("GPU") != std::string::npos)) {
+#if !defined(_WIN32) && !defined(_WIN64)
         setenv("OV_GPU_MODEL_CACHING", "", 1);
+#endif
     }
 }
 
