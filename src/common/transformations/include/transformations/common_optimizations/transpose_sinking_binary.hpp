604ebc0deb404b4ece29242b1d103a30f33e7faa
Evgeny Kotov <evgeny.kotov@intel.com>
2022-11-08 17:26:59 +0100

    decomposite transformations into different files

--- src/common/transformations/include/transformations/common_optimizations/transpose_sinking_binary.hpp
+++ src/common/transformations/include/transformations/common_optimizations/transpose_sinking_binary.hpp
@@ -11,12 +11,8 @@
 namespace ov {
 namespace pass {
 
-class TRANSFORMATIONS_API TransposeSinkingSplitBackward;
 class TRANSFORMATIONS_API TransposeSinkingBinaryElementwiseForward;
-class TRANSFORMATIONS_API TransposeSinkingConcatForward;
-class TRANSFORMATIONS_API TransposeSinkingSplitForward;
 class TRANSFORMATIONS_API TransposeSinkingBinaryElementwiseBackward;
-class TRANSFORMATIONS_API TransposeSinkingConcatBackward;
 
 }  // namespace pass
 }  // namespace ov
@@ -27,32 +23,8 @@ public:
     TransposeSinkingBinaryElementwiseForward();
 };
 
-class ov::pass::TransposeSinkingConcatForward : public ov::pass::MatcherPass {
-public:
-    OPENVINO_RTTI("ov::pass::TransposeSinkingConcatForward", "0");
-    TransposeSinkingConcatForward();
-};
-
-class ov::pass::TransposeSinkingSplitForward : public ov::pass::MatcherPass {
-public:
-    OPENVINO_RTTI("ov::pass::TransposeSinkingSplitForward", "0");
-    TransposeSinkingSplitForward();
-};
-
 class ov::pass::TransposeSinkingBinaryElementwiseBackward : public ov::pass::MatcherPass {
 public:
     OPENVINO_RTTI("ov::pass::TransposeSinkingBinaryElementwiseBackward", "0");
     TransposeSinkingBinaryElementwiseBackward();
-};
-
-class ov::pass::TransposeSinkingConcatBackward : public ov::pass::MatcherPass {
-public:
-    OPENVINO_RTTI("ov::pass::TransposeSinkingConcatBackward", "0");
-    TransposeSinkingConcatBackward();
-};
-
-class ov::pass::TransposeSinkingSplitBackward : public ov::pass::MatcherPass {
-public:
-    OPENVINO_RTTI("ov::pass::TransposeSinkingSplitBackward", "0");
-    TransposeSinkingSplitBackward();
 };
