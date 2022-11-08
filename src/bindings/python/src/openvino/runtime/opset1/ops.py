ec037fbb00b2a8d0af6226fb13bf8cf0a4405e1c
p-wysocki <przemyslaw.wysocki@intel.com>
2022-11-08 02:47:34 +0100

    Fix linter 2

--- src/bindings/python/src/openvino/runtime/opset1/ops.py
+++ src/bindings/python/src/openvino/runtime/opset1/ops.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2018-2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
+# mypy: ignore-errors
 
 """Factory functions for all openvino ops."""
 from typing import List, Optional, Union
