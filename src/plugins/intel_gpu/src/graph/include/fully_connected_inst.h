2155a98661c90a78805e4ed0a730b7badd110b32
Eddy Kim <eddy.kim@intel.com>
2022-11-09 22:16:37 +0900

    reference to node and primitive are removed

--- src/plugins/intel_gpu/src/graph/include/fully_connected_inst.h
+++ src/plugins/intel_gpu/src/graph/include/fully_connected_inst.h
@@ -54,11 +54,11 @@ public:
     typed_primitive_inst(network& network, fully_connected_node const& node);
 
     memory::ptr weights_memory() const {
-        return _node->is_dynamic() && _impl_params->reordered_weights != nullptr ? _impl_params->reordered_weights : dep_memory_ptr(1);
+        return is_dynamic() && _impl_params->reordered_weights != nullptr ? _impl_params->reordered_weights : dep_memory_ptr(1);
     }
     memory::ptr bias_memory() const { return dep_memory_ptr(2); }
 
-    bool bias_term() const { return !argument->bias.empty(); }
+    bool bias_term() const { return _impl_params->bias_layout.has_value(); }
 };
 
 using fully_connected_inst = typed_primitive_inst<fully_connected>;
