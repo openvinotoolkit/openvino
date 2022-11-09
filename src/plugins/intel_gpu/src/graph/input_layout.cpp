6b59b3690bed6b4dc72402c94e2fec801f86fee7
Eddy Kim <eddy.kim@intel.com>
2022-11-09 17:25:02 +0900

    fixed to use get_node_output_layout()

--- src/plugins/intel_gpu/src/graph/input_layout.cpp
+++ src/plugins/intel_gpu/src/graph/input_layout.cpp
@@ -41,7 +41,9 @@ input_layout_inst::typed_primitive_inst(network& network, input_layout_node cons
 }
 
 void input_layout_inst::set_data(memory::ptr mem) {
-    check_memory_to_set(*mem, _impl_params->output_layout);
+    auto ol = get_node_output_layout();
+
+    check_memory_to_set(*mem, ol);
 
     if (mem->is_allocated_by(get_network().get_engine())) {
         OPENVINO_ASSERT(!_outputs.empty(), "[GPU] Can't set data for empty input memory");
