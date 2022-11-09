0689e1f5eb32a5e580d1ff3b263e43dd99ac38f7
Evgeny Kotov <evgeny.kotov@intel.com>
2022-11-09 18:18:06 +0100

    fix ngraph::pass::TransposeFuse use element type the same as in fusing nodes

--- src/common/transformations/src/transformations/common_optimizations/transpose_sinking.cpp
+++ src/common/transformations/src/transformations/common_optimizations/transpose_sinking.cpp
@@ -302,10 +302,14 @@ ngraph::pass::TransposeFuse::TransposeFuse() {
                 is_ordered = false;
         }
 
+        auto transpose_oder_type = transpose1_order->get_element_type();
+        if (transpose_oder_type != transpose2_order->get_element_type())
+            transpose_oder_type = element::i64;
+
         if (is_ordered) {
             return ngraph::replace_output_update_name(transpose2->output(0), input);
         } else {
-            auto new_order = ngraph::opset7::Constant::create(element::i64, {order2.size()}, order2);
+            auto new_order = ngraph::opset7::Constant::create(transpose_oder_type, {order2.size()}, order2);
             auto new_transpose = register_new_node<ngraph::opset7::Transpose>(input, new_order);
 
             new_transpose->set_friendly_name(m.get_match_root()->get_friendly_name());
