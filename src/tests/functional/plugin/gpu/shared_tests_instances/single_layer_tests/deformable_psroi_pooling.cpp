1b2aff95a1302ad7d0e7290baff9529d2005f55c
mdovbnia-lohika <mdovbnia@lohika.com>
2022-11-10 08:37:12 +0200

    [GPU] Added blocked layouts support for roi_pooling operations  (#12983)
    * [GPU] Added unit tests for roi polling operations.
    
    * [GPU] Added blocked layouts support for roi_pooling operations
    
    * [GPU] Added single layer tests for roi polling operations.
    
    * [GPU] Fix attach roi pooling impl, remov const from formats and types
    
    * [GPU] Fix roi polling kernel, bilinear pooling operation
    
    * Fix, whenever output_width and output_hight are equal to 1, which cause a dividing to 0.
    * Fix, for proper calculation y and x.
    
    Co-authored-by: Mykhailo Hnap <mhnap@lohika.com>

---
+++ src/tests/functional/plugin/gpu/shared_tests_instances/single_layer_tests/deformable_psroi_pooling.cpp
@@ -0,0 +1,54 @@
+// Copyright (C) 2022 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "single_layer_tests/deformable_psroi_pooling.hpp"
+
+#include <vector>
+
+#include "common_test_utils/test_constants.hpp"
+
+using namespace LayerTestsDefinitions;
+
+namespace {
+
+const auto params = testing::Combine(
+    testing::ValuesIn(std::vector<std::vector<size_t>>{{3, 8, 16, 16}, {1, 8, 67, 32}}),  // data input shape
+    testing::Values(std::vector<size_t>{10, 5}),                                          // rois input shape
+    // Empty offsets shape means test without optional third input
+    testing::ValuesIn(std::vector<std::vector<size_t>>{{}, {10, 2, 2, 2}}),                // offsets input shape
+    testing::Values(2),                                                                    // output_dim
+    testing::Values(2),                                                                    // group_size
+    testing::ValuesIn(std::vector<float>{1.0, 0.5, 0.0625}),                               // spatial_scale
+    testing::ValuesIn(std::vector<std::vector<int64_t>>{{1, 1}, {2, 2}, {3, 3}, {2, 3}}),  // spatial_bins_x_y
+    testing::ValuesIn(std::vector<float>{0.0, 0.01, 0.5}),                                 // trans_std
+    testing::Values(2));                                                                   // part_size
+
+INSTANTIATE_TEST_SUITE_P(smoke_DeformablePSROIPooling,
+                         DeformablePSROIPoolingLayerTest,
+                         testing::Combine(params,
+                                          testing::Values(InferenceEngine::Precision::FP32,
+                                                          InferenceEngine::Precision::FP16),
+                                          testing::Values(CommonTestUtils::DEVICE_GPU)),
+                         DeformablePSROIPoolingLayerTest::getTestCaseName);
+
+const auto params_advanced =
+    testing::Combine(testing::ValuesIn(std::vector<std::vector<size_t>>{{2, 441, 63, 38}}),  // data input shape
+                     testing::Values(std::vector<size_t>{30, 5}),                            // rois input shape
+                     testing::Values(std::vector<size_t>{30, 2, 3, 3}),                      // offsets input shape
+                     testing::Values(49),                                                    // output_dim
+                     testing::Values(3),                                                     // group_size
+                     testing::ValuesIn(std::vector<float>{0.0625}),                          // spatial_scale
+                     testing::ValuesIn(std::vector<std::vector<int64_t>>{{4, 4}}),           // spatial_bins_x_y
+                     testing::ValuesIn(std::vector<float>{0.1}),                             // trans_std
+                     testing::Values(3));                                                    // part_size
+
+INSTANTIATE_TEST_SUITE_P(smoke_DeformablePSROIPooling_advanced,
+                         DeformablePSROIPoolingLayerTest,
+                         testing::Combine(params_advanced,
+                                          testing::Values(InferenceEngine::Precision::FP32,
+                                                          InferenceEngine::Precision::FP16),
+                                          testing::Values(CommonTestUtils::DEVICE_GPU)),
+                         DeformablePSROIPoolingLayerTest::getTestCaseName);
+
+}  // namespace
