73a573ffbfbb43bdcf9357cd63b04e1941ca010d
Yan, Xiping <xiping.yan@intel.com>
2022-11-15 14:13:19 +0800

    Optimize ADD_MATCHER_FOR_THIS. -#    define ADD_MATCHER_FOR_THIS(region, ...)        add_matcher<region>(__VA_ARGS__); +#    define ADD_MATCHER_FOR_THIS(region, ...)        ADD_MATCHER(this, region, __VA_ARGS__)
    Signed-off-by: Yan, Xiping <xiping.yan@intel.com>

--- src/common/conditional_compilation/include/openvino/cc/pass/itt.hpp
+++ src/common/conditional_compilation/include/openvino/cc/pass/itt.hpp
@@ -21,8 +21,8 @@ OV_CC_DOMAINS(ov_pass);
 #    define MATCHER_SCOPE(region)         const std::string matcher_name(OV_PP_TOSTRING(region))
 #    define RUN_ON_MODEL_SCOPE(region)    OV_SCOPE(ov_pass, OV_PP_CAT(region, _run_on_model))
 
-#    define ADD_MATCHER_FOR_THIS(region, ...)        add_matcher<region>(__VA_ARGS__);
 #    define ADD_MATCHER(obj, region, ...)            obj->add_matcher<region>(__VA_ARGS__);
+#    define ADD_MATCHER_FOR_THIS(region, ...)        ADD_MATCHER(this, region, __VA_ARGS__)
 #    define REGISTER_PASS(obj, region, ...)          obj.register_pass<region>(__VA_ARGS__);
 #    define REGISTER_DISABLED_PASS(obj, region, ...) obj.register_pass<region, false>(__VA_ARGS__);
 #elif defined(SELECTIVE_BUILD)
@@ -39,16 +39,12 @@ OV_CC_DOMAINS(ov_pass);
 
 #    define RUN_ON_MODEL_SCOPE(region) MATCHER_SCOPE_(ov_pass, OV_PP_CAT(region, _run_on_model))
 
-#    define ADD_MATCHER_NO_OBJ_1(region, ...) add_matcher<region>(__VA_ARGS__);
-#    define ADD_MATCHER_NO_OBJ_0(region, ...)
-#    define ADD_MATCHER_FOR_THIS(region, ...)                                                  \
-        OV_PP_CAT(ADD_MATCHER_NO_OBJ_, OV_CC_SCOPE_IS_ENABLED(OV_PP_CAT3(ov_pass, _, region))) \
-        (region, __VA_ARGS__)
 #    define ADD_MATCHER_OBJ_1(obj, region, ...) obj->add_matcher<region>(__VA_ARGS__);
 #    define ADD_MATCHER_OBJ_0(obj, region, ...)
 #    define ADD_MATCHER(obj, region, ...)                                                   \
         OV_PP_CAT(ADD_MATCHER_OBJ_, OV_CC_SCOPE_IS_ENABLED(OV_PP_CAT3(ov_pass, _, region))) \
         (obj, region, __VA_ARGS__)
+#    define ADD_MATCHER_FOR_THIS(region, ...) ADD_MATCHER(this, region, __VA_ARGS__)
 
 #    define REGISTER_PASS_1(obj, region, ...) obj.register_pass<region>(__VA_ARGS__);
 #    define REGISTER_PASS_0(obj, region, ...)
@@ -81,8 +77,8 @@ OV_CC_DOMAINS(ov_pass);
 #    define RUN_ON_FUNCTION_SCOPE(region)
 #    define RUN_ON_MODEL_SCOPE(region)
 
-#    define ADD_MATCHER_FOR_THIS(region, ...)        add_matcher<region>(__VA_ARGS__);
 #    define ADD_MATCHER(obj, region, ...)            obj->add_matcher<region>(__VA_ARGS__);
+#    define ADD_MATCHER_FOR_THIS(region, ...)        ADD_MATCHER(this, region, __VA_ARGS__)
 #    define REGISTER_PASS(obj, region, ...)          obj.register_pass<region>(__VA_ARGS__);
 #    define REGISTER_DISABLED_PASS(obj, region, ...) obj.register_pass<region, false>(__VA_ARGS__);
 #endif
