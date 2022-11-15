c16a6dfa53592272a741336d8d13497bd331a5e7
Mateusz Tabaka <mateusz.tabaka@intel.com>
2022-11-15 07:22:51 +0000

    tests for v0::ShapeOf

--- src/tests/functional/inference_engine/transformations/smart_reshape/shape_of_const_folding.cpp
+++ src/tests/functional/inference_engine/transformations/smart_reshape/shape_of_const_folding.cpp
@@ -5,6 +5,7 @@
 #include <gtest/gtest.h>
 
 #include "common_test_utils/ngraph_test_utils.hpp"
+#include "openvino/op/shape_of.hpp"
 #include "openvino/opsets/opset10.hpp"
 #include "transformations/smart_reshape/shape_of_const_folding.hpp"
 
@@ -15,14 +16,29 @@ TEST_F(TransformationTestsF, ShapeOfConstFolding) {
     {
         auto input = std::make_shared<opset10::Constant>(element::f32, Shape{10, 20}, 1);
         auto shape_of = std::make_shared<opset10::ShapeOf>(input);
-        function = std::make_shared<ngraph::Function>(shape_of, ParameterVector{});
+        model = std::make_shared<Model>(shape_of, ParameterVector{});
     }
 
     manager.register_pass<ov::pass::ShapeOfConstFolding>();
 
     {
         auto input = std::make_shared<opset10::Constant>(element::i64, Shape{2}, std::vector<int64_t>{10, 20});
-        function_ref = std::make_shared<ngraph::Function>(input, ParameterVector{});
+        model_ref = std::make_shared<Model>(input, ParameterVector{});
+    }
+}
+
+TEST_F(TransformationTestsF, v0ShapeOfConstFolding) {
+    {
+        auto input = std::make_shared<opset10::Constant>(element::f32, Shape{10, 20}, 1);
+        auto shape_of = std::make_shared<op::v0::ShapeOf>(input);
+        model = std::make_shared<Model>(shape_of, ParameterVector{});
+    }
+
+    manager.register_pass<ov::pass::ShapeOfConstFolding>();
+
+    {
+        auto input = std::make_shared<opset10::Constant>(element::i64, Shape{2}, std::vector<int64_t>{10, 20});
+        model_ref = std::make_shared<Model>(input, ParameterVector{});
     }
 }
 
@@ -30,7 +46,7 @@ TEST_F(TransformationTestsF, ShapeOfConstFoldingNegative) {
     {
         auto input = std::make_shared<opset10::Parameter>(element::f32, Shape{10, 20});
         auto shape_of = std::make_shared<opset10::ShapeOf>(input);
-        function = std::make_shared<ngraph::Function>(shape_of, ParameterVector{input});
+        model = std::make_shared<Model>(shape_of, ParameterVector{input});
     }
 
     manager.register_pass<ov::pass::ShapeOfConstFolding>();
