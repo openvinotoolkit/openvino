e559d64c67a660e364c8c90092453385dbbe17e8
sgolebiewski-intel <101244613+sgolebiewski-intel@users.noreply.github.com>
2022-11-15 14:23:22 +0100

    Update doxyrest.py

--- docs/doxyrest-sphinx/doxyrest.py
+++ docs/doxyrest-sphinx/doxyrest.py
@@ -150,7 +150,7 @@ def visit_scrollbox(self, node):
             + "".join(c for c in str(node["height"]) if c.isdigit()) + "px; "
             + (("width: " + "".join(c for c in str(node["width"]) if c.isdigit()) ) if "width" in node is not None else "")
             + (("px; " if node["width"].find("px") != -1 else "%;") if "width" in node is not None else "")
-            + ( ("border-left: solid "+"".join(c for c in str(node["bar"]) if c.isdigit())+ "px "+"".join(str(node["bar-color"]))+"; ") if "bar" in node is not None else "")
+            + ( ("border-left:solid "+"".join(c for c in str(node["bar"]) if c.isdigit())+ "px " + (("".join(str(node["bar-color"]))) if "bar-color" in node is not None else "#dee2e6") +"; ") if "bar" in node is not None else "")
             + "overflow-y: scroll; "
         )
         attrs["class"] = ("scrollbox sortable-table" if "sortable" in node is not None else "scrollbox")
