9283f74141f400f688b8418d7723c542d4b250b4
sikorsl1 <leonard.sikorski@intel.com>
2022-11-16 06:30:52 +0000

    Changes requested by a reviewer

--- src/frontends/pytorch/src/op/select.cpp
+++ src/frontends/pytorch/src/op/select.cpp
@@ -12,19 +12,18 @@ namespace pytorch {
 namespace op {
 
 OutputVector translate_select(NodeContext& context) {
-    auto const_1 = opset8::Constant::create(element::i32, Shape{1}, {1});
-    auto const_minus_1 = opset8::Constant::create(element::i32, Shape{1}, {-1});
-    auto const_0 = opset8::Constant::create(element::i32, Shape{1}, {0});
+    auto const_1 =  context.mark_node(opset8::Constant::create(element::i32, Shape{1}, {1}));
+    auto const_minus_1 = context.mark_node(opset8::Constant::create(element::i32, Shape{1}, {-1}));
+    auto const_0 = context.mark_node(opset8::Constant::create(element::i32, Shape{1}, {0}));
     auto input_tensor = context.get_input(0);
-    auto dim = std::make_shared<opset8::Reshape>(context.get_input(1), const_1, false);
-    auto start = std::make_shared<opset8::Reshape>(context.get_input(2), const_1, false);
+    auto dim = context.mark_node(std::make_shared<opset8::Reshape>(context.get_input(1), const_1, false));
+    auto start = context.mark_node(std::make_shared<opset8::Reshape>(context.get_input(2), const_1, false));
 
-    auto less = std::make_shared<opset8::Less>(start, const_0);
-    auto const_1_signed = std::make_shared<opset8::Select>(less, const_minus_1, const_1);
-    auto stop = std::make_shared<opset8::Add>(start, const_1_signed);
+    auto less = context.mark_node(std::make_shared<opset8::Less>(start, const_0));
+    auto const_1_signed = context.mark_node(std::make_shared<opset8::Select>(less, const_minus_1, const_1));
+    auto stop = context.mark_node(std::make_shared<opset8::Add>(start, const_1_signed));
 
-    auto slice_node = std::make_shared<opset8::Slice>(input_tensor, start, stop, const_1_signed, dim);
-    context.mark_node(slice_node);
+    auto slice_node = context.mark_node(std::make_shared<opset8::Slice>(input_tensor, start, stop, const_1_signed, dim));
 
     return {context.mark_node(std::make_shared<opset8::Squeeze>(slice_node, dim))};
 };
