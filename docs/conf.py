de257dce0dd5e100a04a59f9c40c7c9ac2291b43
Ilya Churaev <ilya.churaev@intel.com>
2022-11-16 18:04:21 +0400

    Try to cache results

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db',
-                    '.DS_Store', 'openvino/inference-engine']
+                    '.DS_Store', 'openvino/src']
 
 
 panels_add_bootstrap_css = False
