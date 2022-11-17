026185b3a9764aa7790ec36914c3db2f2204752a
Kazantsev, Roman <roman.kazantsev@intel.com>
2022-11-17 14:56:46 +0400

    Better to cut by outputs of operation creating the cycle

--- src/frontends/tensorflow/src/input_model.cpp
+++ src/frontends/tensorflow/src/input_model.cpp
@@ -171,6 +171,7 @@ std::vector<std::shared_ptr<OpPlace>> InputModel::InputModelTFImpl::get_op_place
 std::vector<std::shared_ptr<OpPlace>> InputModel::InputModelTFImpl::determine_cut_nodes() const {
     std::vector<std::shared_ptr<OpPlace>> topologically_sorted_ops;
     std::stack<std::shared_ptr<OpPlace>> ops_to_do;
+    std::unordered_set<std::string> ops_names_to_do;
     std::unordered_set<std::shared_ptr<OpPlace>> ops_done;
 
     for (const auto& output_place : m_outputs) {
@@ -184,6 +185,9 @@ std::vector<std::shared_ptr<OpPlace>> InputModel::InputModelTFImpl::determine_cu
                                 "Custom specified output is incorrect: " + output_place_name);
         auto output_operation_place = m_op_places_map.at(operation_name);
         ops_to_do.push(output_operation_place);
+        FRONT_END_GENERAL_CHECK(output_operation_place->get_names().size() > 0,
+                                "TensorPlace must have at least one name.");
+        ops_names_to_do.insert(output_operation_place->get_names()[0]);
     }
 
     // the traversing algorithm to compute topologically sorted nodes is taken from topological_sort in
@@ -197,14 +201,6 @@ std::vector<std::shared_ptr<OpPlace>> InputModel::InputModelTFImpl::determine_cu
             auto input_count = current_operation_decoder->get_input_size();
             const auto& curr_op_type = current_operation_decoder->get_op_type();
 
-            // break the cycle
-            // otherwise, the operations nodes in the cycle will be added to ops_to_do infinitely
-            if (curr_op_type == "NextIteration") {
-                topologically_sorted_ops.push_back(current_operation_place);
-                ops_to_do.pop();
-                ops_done.insert(current_operation_place);
-                continue;
-            }
             for (size_t input_port_idx = 0; input_port_idx < input_count; ++input_port_idx) {
                 std::string producer_name;
                 size_t producer_output_port_idx;
@@ -250,9 +246,14 @@ std::vector<std::shared_ptr<OpPlace>> InputModel::InputModelTFImpl::determine_cu
                     is_input |= tensor_place->is_input();
                 }
 
-                if (!is_input && ops_done.count(producer_operation_place) == 0) {
+                // in case presence of NextIteration in the graph (or cycle created by other operation),
+                // we break the cycle by outputs from the NextIteration operation
+                // otherwise, the operations nodes in the cycle will be added to ops_to_do infinitely
+                if (!is_input && ops_done.count(producer_operation_place) == 0 &&
+                    ops_names_to_do.count(producer_name) == 0) {
                     can_add = false;
                     ops_to_do.push(producer_operation_place);
+                    ops_names_to_do.insert(producer_name);
                 }
             }
 
