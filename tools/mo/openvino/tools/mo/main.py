c56cd93a28d4e999ad25036c15b8c911ba319142
Anastasiia Pnevskaia <anastasia.popova@intel.com>
2022-11-18 20:08:35 +0100

    Pass MO meta data to RT info (#13633)
    * Moved MO meta data to RT info.
    
    * Undo wrong change.
    
    * Reformat code.
    
    * Code reformat.
    
    * Corrected error message.
    
    * Test correction.
    
    * Test fix.
    
    * Updated MO IR Reader.
    
    * Added testing of meta data for MO IR reader.
    
    * Code corrections.
    
    * Default params removed from meta data.
    
    * Test corrected, fixed paths cropping.
    
    * Small fix.
    
    * Renamed legacy_path to legacy_frontend.
    
    * Fixed error.

--- tools/mo/openvino/tools/mo/main.py
+++ tools/mo/openvino/tools/mo/main.py
@@ -13,13 +13,12 @@ except ImportError:
 
 from openvino.tools.mo.convert import convert
 from openvino.tools.mo.pipeline.common import get_ir_version
-from openvino.tools.mo.utils.cli_parser import get_model_name_from_args, get_meta_info
+from openvino.tools.mo.utils.cli_parser import get_model_name_from_args
 from openvino.tools.mo.utils.logger import init_logger
 from openvino.tools.mo.utils.error import Error, FrameworkError
 import traceback
 from openvino.tools.mo.utils.get_ov_update_message import get_ov_update_message, get_ov_api20_message
 from openvino.tools.mo.utils.model_analysis import AnalysisResults
-from openvino.tools.mo.back.ie_ir_ver_2.emitter import append_ir_info
 
 # pylint: disable=no-name-in-module,import-error
 from openvino.frontend import FrontEndManager
@@ -80,9 +79,6 @@ def main(cli_parser: argparse.ArgumentParser, framework=None):
     model_path = model_path_no_ext + '.xml'
 
     serialize(ngraph_function, model_path.encode('utf-8'), model_path.replace('.xml', '.bin').encode('utf-8'))
-
-    # add meta information to IR
-    append_ir_info(file=model_path_no_ext, meta_info=get_meta_info(argv))
 
     # generate .mapping file
     path_to_mapping = model_path_no_ext + ".mapping"
