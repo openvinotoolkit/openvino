92ecb241395e816a060308f0a25c8864d8293c31
Oleksii Khovan <okhovan@lohika.com>
2022-11-21 11:53:40 +0100

    fix fake_aling unit tests - no more forced Weights transpose

--- src/plugins/intel_gpu/tests/fake_alignment/fc_fake_alignment_test.cpp
+++ src/plugins/intel_gpu/tests/fake_alignment/fc_fake_alignment_test.cpp
@@ -63,21 +63,21 @@ INSTANTIATE_TEST_SUITE_P(smoke, fully_connected_fake_align_test,
     testing::ValuesIn(std::vector<fc_fake_align_params>{
         {
             layout{ov::PartialShape{0, 1024}, data_types::i8, format::bfyx, padding{{1,1,1,1}, 0}},    // input_layout
-            layout{ov::PartialShape{1000, 1024}, data_types::i8, format::bfyx},                        // weight layout
+            layout{ov::PartialShape{1024, 1000}, data_types::i8, format::bfyx},                        // weight layout
             data_types::f16,
             layout{ov::PartialShape{0, 1024}, data_types::i8, format::bfyx, padding{{1,1,1,1}, 0}},    // fake_aligned input layout
             layout{ov::PartialShape{0, 1000}, data_types::f16, format::bfyx}                           // fake_aligned output layout
         },
         {
             layout{ov::PartialShape{11, 1024}, data_types::i8, format::bfyx, padding{{1,1,1,1}, 0}},   // input_layout
-            layout{ov::PartialShape{1000, 1024}, data_types::i8, format::bfyx},                        // weight layout
+            layout{ov::PartialShape{1024, 1000}, data_types::i8, format::bfyx},                        // weight layout
             data_types::f16,
             layout{ov::PartialShape{16, 1024}, data_types::i8, format::bfyx, padding{{1,1,1,1}, 0}},   // fake_aligned input layout
             layout{ov::PartialShape{16, 1000}, data_types::f16, format::bfyx}                          // fake_aligned output layout
         },
         {
             layout{ov::PartialShape{133, 511}, data_types::i8, format::bfyx, padding{{1,1,1,1}, 0}},   // input_layout
-            layout{ov::PartialShape{800, 511}, data_types::i8, format::bfyx},                        // weight layout
+            layout{ov::PartialShape{511, 800}, data_types::i8, format::bfyx},                        // weight layout
             data_types::f16,
             layout{ov::PartialShape{144, 511}, data_types::i8, format::bfyx, padding{{1,1,1,1}, 0}},   // fake_aligned input layout
             layout{ov::PartialShape{144, 800}, data_types::f16, format::bfyx}                          // fake_aligned output layout
