d481782cb140cb39784d7830aaf94105b07006ec
eaidova <ekaterina.aidova@intel.com>
2022-11-22 08:48:12 -0500

    cleanup

--- src/frontends/pytorch/src/op/pad.cpp
+++ src/frontends/pytorch/src/op/pad.cpp
@@ -17,52 +17,43 @@ OutputVector translate_pad(NodeContext& context) {
     auto paddings = context.const_input<std::vector<int64_t>>(1);
     std::string mode = "constant";
     double value = 0;
-    auto shape = context.mark_node(std::make_shared<opset8::ShapeOf>(data, element::i64));
-    auto rank = context.mark_node(std::make_shared<opset8::ShapeOf>(shape, element::i64));
+    auto shape = context.mark_node(std::make_shared<opset8::ShapeOf>(data, element::i32));
+    auto rank = context.mark_node(std::make_shared<opset8::ShapeOf>(shape, element::i32));
     auto reduced_rank = context.mark_node(std::make_shared<opset8::Squeeze>(rank));
-    auto zero = context.mark_node(opset8::Constant::create(element::i64, Shape{}, {0}));
-    auto pad_begins = context.mark_node(std::make_shared<opset8::Broadcast>(zero, rank));
-    auto pad_ends = context.mark_node(std::make_shared<opset8::Broadcast>(zero, rank));
-    std::vector<int64_t> pad_b(paddings.size() / 2, 0);
-    std::vector<int64_t> pad_e(paddings.size() / 2, 0);
-    std::vector<int64_t> neg_indicies(paddings.size() / 2, 0);
+    auto zero = context.mark_node(opset8::Constant::create(element::i32, Shape{}, {0}));
+    auto pad_size_half = paddings.size() / 2;
+    std::vector<int64_t> pad_b(pad_size_half, 0);
+    std::vector<int64_t> pad_e(pad_size_half, 0);
     auto pad_mode = ov::op::PadMode::CONSTANT;
-    int pad_size = static_cast<int>(paddings.size() / 2);
-    for (int i = 0; i < paddings.size() / 2; i++) {
-        pad_b.push_back(paddings[paddings.size() - (2 * i + 2)]);
-        pad_e.push_back(paddings[paddings.size() - (2 * i + 1)]);
-        neg_indicies.push_back(pad_size - i);
-
+    for (int i = 0; i < pad_size_half; i++) {
+        pad_b[i] = paddings[paddings.size() - 2 - 2 * i];
+        pad_e[i] = paddings[paddings.size() - 1 - 2 * i];
     }
-    auto neg_id_shape = context.mark_node(opset8::Constant::create(element::i64, Shape({1}), std::vector<size_t>({neg_indicies.size()})));
-    auto neg_id_node = context.mark_node(opset8::Constant::create(element::i64, Shape({neg_indicies.size()}), neg_indicies));
-    auto end_indicies = context.mark_node(std::make_shared<opset8::Broadcast>(reduced_rank, neg_id_shape));
-    auto indicies = context.mark_node(std::make_shared<opset8::Subtract>(end_indicies, neg_id_node));
-    pad_begins = context.mark_node(std::make_shared<opset8::ScatterUpdate>(pad_begins, indicies,
-            context.mark_node(opset8::Constant::create(element::i64, Shape({pad_b.size()}), pad_b)),
-            context.mark_node(opset8::Constant::create(element::i64, Shape{}, {-1}))));
-    pad_ends = context.mark_node(std::make_shared<opset8::ScatterUpdate>(pad_ends,
-            indicies,
-            context.mark_node(opset8::Constant::create(element::i64, Shape({pad_e.size()}), pad_e)),
-            context.mark_node(opset8::Constant::create(element::i64, Shape{}, {-1}))));
+    auto pads_begin_short = context.mark_node(opset8::Constant::create(element::i32, Shape{pad_size_half}, pad_b));
+    auto pads_end_short = context.mark_node(opset8::Constant::create(element::i32, Shape{pad_size_half}, pad_e));
+    auto pads_short_len = context.mark_node(opset8::Constant::create(element::i32, Shape{1}, {pad_size_half}));
+    auto pads_diff = context.mark_node(std::make_shared<opset8::Subtract>(rank, pads_short_len));
+    auto pads_remaining = context.mark_node(std::make_shared<opset8::Broadcast>(zero, pads_diff));
+    auto pads_begins =
+        context.mark_node(std::make_shared<opset8::Concat>(NodeVector{pads_remaining, pads_begin_short}, 0));
+    auto pads_ends = context.mark_node(std::make_shared<opset8::Concat>(NodeVector{pads_remaining, pads_end_short}, 0));
     if (!context.input_is_none(2)) {
-            mode = context.const_input<std::string>(2);
-        }
+        mode = context.const_input<std::string>(2);
+    }
     if (mode == "circular") {
         int64_t pad_l;
         int64_t pad_r;
-        auto ndim = paddings.size() / 2;
         auto pad_last_id = paddings.size();
         auto cur = data.get_node_shared_ptr();
         auto step = context.mark_node(opset8::Constant::create(element::i64, Shape{1}, {1}));
-        for (auto i = 0; i < ndim; i++) {
+        for (auto i = 0; i < pad_size_half; i++) {
             ov::NodeVector tensors;
             pad_r = paddings[pad_last_id - (2 * i + 1)];
             pad_l = paddings[pad_last_id - (2 * i + 2)];
             auto axes = context.mark_node(opset8::Constant::create(element::i64, Shape{1}, {2 + i}));
             if (pad_l > 0) {
-                auto start = context.mark_node(
-                    context.mark_node(opset8::Constant::create(element::i64, Shape{1}, {-1 * pad_l})));
+                auto start =
+                    context.mark_node(context.mark_node(opset8::Constant::create(element::i64, Shape{1}, {-pad_l})));
                 auto end = context.mark_node(std::make_shared<opset8::Gather>(
                     shape,
                     context.mark_node(opset8::Constant::create(element::i64, Shape{1}, {2 + i})),
@@ -73,9 +64,9 @@ OutputVector translate_pad(NodeContext& context) {
             }
             if (pad_l < 0 || pad_r < 0) {
                 auto start = context.mark_node(
-                    context.mark_node(opset8::Constant::create(element::i64, Shape{1}, {-1 * pad_l > 0 ? -1 * pad_l : 0})));
+                    context.mark_node(opset8::Constant::create(element::i64, Shape{1}, {-pad_l > 0 ? -pad_l : 0})));
                 auto end = context.mark_node(
-                    context.mark_node(opset8::Constant::create(element::i64, Shape{1}, {-1 * (-1 * pad_r > 0 ? -1 * pad_r : 0)})));
+                    context.mark_node(opset8::Constant::create(element::i64, Shape{1}, {-pad_r > 0 ? pad_r : 0})));
                 auto middle = context.mark_node(std::make_shared<opset8::Slice>(cur, start, end, step, axes));
                 tensors.push_back(middle);
             } else {
@@ -87,8 +78,8 @@ OutputVector translate_pad(NodeContext& context) {
                 auto right = context.mark_node(std::make_shared<opset8::Slice>(cur, start, end, step, axes));
                 tensors.push_back(right);
             }
-            if (tensors.size()){
-            cur = context.mark_node(std::make_shared<opset8::Concat>(tensors, 2 + i));
+            if (tensors.size()) {
+                cur = context.mark_node(std::make_shared<opset8::Concat>(tensors, 2 + i));
             }
         }
         return {cur};
@@ -107,8 +98,8 @@ OutputVector translate_pad(NodeContext& context) {
     }
     return {context.mark_node(std::make_shared<opset8::Pad>(
         data,
-        pad_begins,
-        pad_ends,
+        pads_begins,
+        pads_ends,
         context.mark_node(std::make_shared<opset8::Constant>(element::f32, Shape({}), value)),
         pad_mode))};
 }
