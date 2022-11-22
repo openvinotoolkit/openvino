26e99a407c0fa1faeb254c8bf8dbf52eff42c2d6
Oleg Pipikin <oleg.pipikin@intel.com>
2022-11-22 04:46:19 +0100

    Move transformation and lp transformations tests from IeFuncTests and IeUnitTests (#14096)
    * Move transformation and lp transformations tests from IeFuncTests and IeUnitTests
    
    * Enable tests in Azure CI
    
    Co-authored-by: Ilya Churaev <ilya.churaev@intel.com>

---
+++ src/common/transformations/tests/skip_tests_config.cpp
@@ -0,0 +1,15 @@
+// Copyright (C) 2018-2022 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <vector>
+#include <string>
+
+#include "functional_test_utils/skip_tests_config.hpp"
+
+std::vector<std::string> disabledTestPatterns() {
+    return {
+        // TODO: task 32568, enable after supporting constants outputs in plugins
+        ".*TransformationTests\\.ConstFoldingPriorBox.*",
+    };
+}
