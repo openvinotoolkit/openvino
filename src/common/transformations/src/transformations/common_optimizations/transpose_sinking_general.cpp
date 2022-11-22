15d83757795e9a46bbe4606198c88ee0cea5da42
Evgeny Kotov <evgeny.kotov@intel.com>
2022-11-22 14:50:44 +0100

    remove comment

--- src/common/transformations/src/transformations/common_optimizations/transpose_sinking_general.cpp
+++ src/common/transformations/src/transformations/common_optimizations/transpose_sinking_general.cpp
@@ -51,9 +51,5 @@ bool ov::pass::TransposeSinkingGeneral::run_on_model(const std::shared_ptr<ov::M
         manager.run_passes(f);
     }
 
-    // Returning value is false because pass::Manager always apply Validation pass
-    // if function was changed. This helps to avoid excess Validations after applying
-    // this pass. In future when we will return more meaningful status code it will be
-    // replaced with real status reported by manager.run_passes() method call.
     return false;
 }
