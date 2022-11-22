d4731a4baf96a0655ca7c7fc31a89a7b32e9f881
sikorsl1 <leonard.sikorski@intel.com>
2022-11-22 10:34:27 +0000

    Enable aten::reshape_as operator and add layer test

---
+++ tests/layer_tests/pytorch_tests/test_reshape_as.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2018-2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+from pytorch_layer_test_class import PytorchLayerTest
+import numpy as np
+import torch
+
+
+@pytest.mark.parametrize('input_tesnors', ((np.ones((3, 6)), np.ones((2, 9))),
+                                           (np.ones((2, 2, 3)), np.ones((6, 2))),
+                                           (np.ones((6, 2)), np.ones((2, 2, 3)))))
+class TestSelect(PytorchLayerTest):
+
+    def _prepare_input(self):
+        return self.input_tesnors
+
+    def create_model(self):
+
+        class aten_select(torch.nn.Module):
+
+            def forward(self, input_tensor, shape_tensor):
+                return input_tensor.reshape_as(shape_tensor)
+
+        ref_net = None
+
+        return aten_select(), ref_net, "aten::reshape_as"
+
+    @pytest.mark.nightly
+    def test_pow(self, ie_device, precision, ir_version, input_tesnors):
+        self.input_tesnors = input_tesnors
+        self._test(*self.create_model(), 'CPU', precision, ir_version)
