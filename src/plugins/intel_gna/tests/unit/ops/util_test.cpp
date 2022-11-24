08bd491d135f51ef433d66f803bf759bfa06d301
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2022-11-24 13:54:01 +0100

    Update src/plugins/intel_gna/tests/unit/ops/util_test.cpp
    Co-authored-by: Nadezhda Ageeva <nkogteva@gmail.com>

--- src/plugins/intel_gna/tests/unit/ops/util_test.cpp
+++ src/plugins/intel_gna/tests/unit/ops/util_test.cpp
@@ -149,7 +149,7 @@ INSTANTIATE_TEST_SUITE_P(smoke_ops_util_is_elwise_add,
                             ::testing::Values(true)),
                          GnaOpsUtilIsEltwiseAddTest::get_test_name);
 
-INSTANTIATE_TEST_SUITE_P(smoke_ops_util_not_elwise_add,
+INSTANTIATE_TEST_SUITE_P(smoke_ops_util_not_eltwise_add,
                          GnaOpsUtilIsEltwiseAddTest,
                          ::testing::Combine(
                             ::testing::ValuesIn(elwise_add_nodes_false),
