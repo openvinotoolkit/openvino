b6f32ca385c3a91106ac3218f0e7d8313bc9cefb
Artur Kulikowski <artur.kulikowski@intel.com>
2022-11-24 15:18:16 +0100

    Add default visibility to class LegacyOpExtension

--- src/core/dev_api/legacy_op_extension.hpp
+++ src/core/dev_api/legacy_op_extension.hpp
@@ -9,5 +9,5 @@
 namespace ov {
 
 /** @brief Class to distinguish legacy extension. */
-class LegacyOpExtension : public BaseOpExtension {};
+class OPENVINO_API LegacyOpExtension : public BaseOpExtension {};
 }  // namespace ov
