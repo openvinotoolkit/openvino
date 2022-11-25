03fa573d0d6f41ad945e3c2a0c205c6a48e789a3
Eddy Kim <eddy.kim@intel.com>
2022-11-25 16:37:30 +0900

    [GPU] Removing OneDNN patch file for GPU model caching (#14214)
    * updated not to use default c-tor for onednn desc
    
    * onednn patch file is removed

--- src/plugins/intel_gpu/src/graph/impls/onednn/pooling_onednn.cpp
+++ src/plugins/intel_gpu/src/graph/impls/onednn/pooling_onednn.cpp
@@ -77,7 +77,10 @@ public:
     void load(BinaryInputBuffer& ib) override {
         parent::load(ib);
 
-        _desc = std::make_shared<dnnl::pooling_forward::desc>();
+        const char dummy_mem[sizeof(dnnl::pooling_forward::desc)] = {};
+        const dnnl::pooling_forward::desc *dummy_opdesc
+            = reinterpret_cast<const dnnl::pooling_forward::desc *>(&dummy_mem[0]);
+        _desc = std::make_shared<dnnl::pooling_forward::desc>(std::move(*dummy_opdesc));
         ib >> make_data(&_desc->data, sizeof(dnnl_pooling_desc_t));
 
         std::vector<uint8_t> prim_cache;
