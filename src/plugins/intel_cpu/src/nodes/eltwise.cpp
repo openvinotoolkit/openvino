3134aa5bb79af152028408a44d1a2efa8e33e497
Nikolay Shchegolev <nikolay.shchegolev@intel.com>
2022-11-29 15:27:39 +0400

    [CPU] New operations: IsFinite, IsInf, IsNaN.

--- src/plugins/intel_cpu/src/nodes/eltwise.cpp
+++ src/plugins/intel_cpu/src/nodes/eltwise.cpp
@@ -940,6 +940,18 @@ const std::map<const ngraph::DiscreteTypeInfo, Eltwise::Initializer> Eltwise::in
     {ngraph::op::v1::NotEqual::get_type_info_static(), [](const std::shared_ptr<ngraph::Node>& op, Eltwise& node) {
         node.algorithm = Algorithm::EltwiseNotEqual;
     }},
+    {ov::op::v10::IsFinite::get_type_info_static(), [](const std::shared_ptr<ov::Node>& op, Eltwise& node) {
+        node.algorithm = Algorithm::EltwiseIsFinite;
+    }},
+    {ov::op::v10::IsInf::get_type_info_static(), [](const std::shared_ptr<ov::Node>& op, Eltwise& node) {
+        node.algorithm = Algorithm::EltwiseIsInf;
+        const auto& attributes = ov::as_type_ptr<ov::op::v10::IsInf>(op)->get_attributes();
+        node.detectNegative = attributes.detect_negative;
+        node.detectPositive = attributes.detect_positive;
+    }},
+    {ov::op::v10::IsNaN::get_type_info_static(), [](const std::shared_ptr<ov::Node>& op, Eltwise& node) {
+        node.algorithm = Algorithm::EltwiseIsNaN;
+    }},
     {ngraph::op::v1::Greater::get_type_info_static(), [](const std::shared_ptr<ngraph::Node>& op, Eltwise& node) {
         node.algorithm = Algorithm::EltwiseGreater;
     }},
@@ -1562,6 +1574,11 @@ public:
                     case Algorithm::EltwisePrelu:             *dst_ptr_f = src_f[0] > 0 ? src_f[0] : src_f[0] * src_f[1]; break;
                     case Algorithm::EltwiseErf:               *dst_ptr_f = std::erf(src_f[0]); break;
                     case Algorithm::EltwiseSoftSign:          *dst_ptr_f = src_f[0] / (1 + std::fabs(src_f[0])); break;
+                    case Algorithm::EltwiseIsFinite:          *dst_ptr_f = std::isfinite(src_f[0]); break;
+                    case Algorithm::EltwiseIsInf:
+                        *dst_ptr_f = std::isinf(src_f[0]) && (_opData.detectNegative && (src_f[0] < 0) || _opData.detectPositive && (src_f[0] > 0));
+                        break;
+                    case Algorithm::EltwiseIsNaN:             *dst_ptr_f = std::isnan(src_f[0]); break;
                     default: IE_THROW() << "Unsupported operation type for Eltwise executor";
                 }
             }
@@ -1593,7 +1610,9 @@ bool Eltwise::EltwiseData::operator==(const EltwiseData &rhs) const noexcept {
            onednnAlgorithm == rhs.onednnAlgorithm &&
            alpha == rhs.alpha &&
            beta == rhs.beta &&
-           gamma == rhs.gamma;
+           gamma == rhs.gamma &&
+           detectNegative == rhs.detectNegative &&
+           detectPositive == rhs.detectPositive;
 }
 
 static Eltwise::executorPtr buildExecutor(const EltwiseKey& key) {
@@ -1646,6 +1665,9 @@ Eltwise::Eltwise(const std::shared_ptr<ngraph::Node>& op, const dnnl::engine& en
 
 size_t Eltwise::getOpInputsNum() const {
     switch (getAlgorithm()) {
+        case Algorithm::EltwiseIsFinite:
+        case Algorithm::EltwiseIsInf:
+        case Algorithm::EltwiseIsNaN:
         case Algorithm::EltwiseRelu:
         case Algorithm::EltwiseGelu:
         case Algorithm::EltwiseElu:
@@ -1729,6 +1751,7 @@ void Eltwise::initSupportedPrimitiveDescriptors() {
 
     // if dim rank is greater than the maximum possible, we should use the reference execution
     canUseOptimizedImpl = mayiuse(x64::sse41) && getInputShapeAtPort(0).getRank() <= MAX_ELTWISE_DIM_RANK;
+    canUseOptimizedImpl &= !one_of(getAlgorithm(), Algorithm::EltwiseIsFinite, Algorithm::EltwiseIsInf, Algorithm::EltwiseIsNaN);
 
     if (!canUseOptimizedImpl && !fusedWith.empty()) {
         IE_THROW(Unexpected) << "Eltwise node with name '" << getName() << "' uses reference impl, but unexpectedly fused with other ops";
@@ -2007,7 +2030,7 @@ void Eltwise::prepareParams() {
         if (node->getType() == Type::Eltwise) {
             if (auto eltwise = std::dynamic_pointer_cast<Eltwise>(node)) {
                 key.eltwise_data.push_back({eltwise->getAlgorithm(), eltwise->getOneDnnAlgorithm(), eltwise->getAlpha(),
-                                            eltwise->getBeta(), eltwise->getGamma()});
+                                            eltwise->getBeta(), eltwise->getGamma(), eltwise->getNegative(), eltwise->getPositive()});
             }
         } else if (node->getType() == Type::FakeQuantize) {
             node->appendPostOps(key.postOps, {}, fqDataPtrs);
--- src/plugins/intel_cpu/src/nodes/eltwise.h
+++ src/plugins/intel_cpu/src/nodes/eltwise.h
@@ -74,6 +74,8 @@ public:
         float alpha;
         float beta;
         float gamma;
+        bool detectNegative;
+        bool detectPositive;
 
         bool operator==(const EltwiseData& rhs) const noexcept;
     };
@@ -108,6 +110,8 @@ public:
     float getAlpha() const { return alpha; }
     float getBeta() const { return beta; }
     float getGamma() const { return gamma; }
+    bool getNegative() const { return detectNegative; }
+    bool getPositive() const { return detectPositive; }
 
     dnnl::algorithm getOneDnnAlgorithm() const { return onednnAlgorithm; }
 
@@ -150,6 +154,9 @@ private:
     float alpha = 0;
     float beta = 0;
     float gamma = 0;
+
+    bool detectNegative = true;
+    bool detectPositive = true;
 
     std::vector<float> scales = {};
     std::vector<float> shifts = {};
