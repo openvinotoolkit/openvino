310a56f892d9a6861b865065b44f2847c737588f
sikorsl1 <leonard.sikorski@intel.com>
2022-12-01 10:29:36 +0000

    Add aten::norm operator and layer test

---
+++ src/frontends/pytorch/src/op/norm.cpp
@@ -0,0 +1,36 @@
+// Copyright (C) 2018-2022 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "openvino/frontend/pytorch/node_context.hpp"
+#include "openvino/opsets/opset8.hpp"
+#include "utils.hpp"
+
+namespace ov {
+namespace frontend {
+namespace pytorch {
+namespace op {
+
+OutputVector translate_norm(NodeContext& context) {
+    auto input_tensor = context.get_input(0);
+    auto p = context.const_input<int64_t>(1);
+    auto dim = context.get_input(2);
+    auto keep_dim = context.const_input<bool>(3);
+
+    std::shared_ptr<ov::op::util::ArithmeticReductionKeepDims> reduce;
+
+    FRONT_END_OP_CONVERSION_CHECK(p == 1 || p == 2, "OpenVino export only p-norms with p of 1 or 2");
+
+    if (p == 1) {
+        reduce = std::make_shared<opset8::ReduceL1>(input_tensor, dim, keep_dim);
+    } else if (p == 2) {
+        reduce = std::make_shared<opset8::ReduceL2>(input_tensor, dim, keep_dim);
+    }
+
+    return {context.mark_node(reduce)};
+};
+
+}  // namespace op
+}  // namespace pytorch
+}  // namespace frontend
+}  // namespace ov
