7f4c9709981fd105dd2f10abcc36ade4e6433d0d
Maxim Vafin <maxim.vafin@intel.com>
2022-12-02 18:22:21 +0100

    Add activations and eltwise support

--- src/common/transformations/src/transformations/common_optimizations/reverse_shape_and_type_infer.cpp
+++ src/common/transformations/src/transformations/common_optimizations/reverse_shape_and_type_infer.cpp
@@ -66,8 +66,19 @@ bool ov::pass::ReverseShapeAndTypeInfer::run_on_model(const std::shared_ptr<ngra
         auto output_shape = op->get_output_partial_shape(0);
         auto output_type = op->get_output_element_type(0);
         std::cout << op->get_type_name() << std::endl;
-        if (std::dynamic_pointer_cast<Convolution>(op) || std::dynamic_pointer_cast<GroupConvolutionBackpropData>(op) ||
-            std::dynamic_pointer_cast<ConvolutionBackpropData>(op) || std::dynamic_pointer_cast<GroupConvolution>(op)) {
+        if (const auto& param = std::dynamic_pointer_cast<Parameter>(op)) {
+            if (param->get_partial_shape().rank().is_dynamic()) {
+                param->set_partial_shape(output_shape);
+                is_changed = true;
+            }
+            if (param->get_element_type().is_dynamic()) {
+                param->set_element_type(output_type);
+                is_changed = true;
+            }
+        } else if (std::dynamic_pointer_cast<Convolution>(op) ||
+                   std::dynamic_pointer_cast<GroupConvolutionBackpropData>(op) ||
+                   std::dynamic_pointer_cast<ConvolutionBackpropData>(op) ||
+                   std::dynamic_pointer_cast<GroupConvolution>(op)) {
             is_changed |= inherit_output_rank(op, {0, 1});
             is_changed |= inherit_output_type(op, {0, 1});
         } else if (std::dynamic_pointer_cast<DeformableConvolution>(op)) {
@@ -87,15 +98,31 @@ bool ov::pass::ReverseShapeAndTypeInfer::run_on_model(const std::shared_ptr<ngra
                 }
             }
             is_changed |= inherit_output_type(op, {0});
-        } else if (const auto& param = std::dynamic_pointer_cast<Parameter>(op)) {
-            if (param->get_partial_shape().rank().is_dynamic()) {
-                param->set_partial_shape(output_shape);
-                is_changed = true;
+        } else if (std::dynamic_pointer_cast<op::util::UnaryElementwiseArithmetic>(op)) {
+            is_changed |= inherit_output_shape(op, {0});
+            is_changed |= inherit_output_type(op, {0});
+        } else if (const auto& eltwise = std::dynamic_pointer_cast<op::util::BinaryElementwiseArithmetic>(op)) {
+            auto interval = Dimension(0, output_shape.rank().get_length());
+            if (output_shape.rank().is_static()) {
+                auto in0_rank = op->get_input_partial_shape(0).rank();
+                auto in1_rank = op->get_input_partial_shape(1).rank();
+                if (in0_rank.is_dynamic() && in1_rank.is_static()) {
+                    if (eltwise->get_autob() == ov::op::AutoBroadcastType::NONE)
+                        op->get_input_tensor(0).set_partial_shape(output_shape);
+                    else if (in1_rank.get_length() < output_shape.rank().get_length())
+                        op->get_input_tensor(0).set_partial_shape(PartialShape::dynamic(output_shape.rank()));
+                    else
+                        op->get_input_tensor(0).set_partial_shape(PartialShape::dynamic(interval));
+                } else if (in1_rank.is_dynamic() && in0_rank.is_static()) {                    
+                    if (eltwise->get_autob() == ov::op::AutoBroadcastType::NONE)
+                        op->get_input_tensor(1).set_partial_shape(output_shape);
+                    else if (in0_rank.get_length() < output_shape.rank().get_length())
+                        op->get_input_tensor(1).set_partial_shape(PartialShape::dynamic(output_shape.rank()));
+                    else
+                        op->get_input_tensor(1).set_partial_shape(PartialShape::dynamic(interval));
+                }
             }
-            if (param->get_element_type().is_dynamic()) {
-                param->set_element_type(output_type);
-                is_changed = true;
-            }
+            is_changed |= inherit_output_type(op, {0, 1});
         }
     }
     return is_changed;
