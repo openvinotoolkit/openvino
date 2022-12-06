bc39e2e63bc31e9afd6783e100dd6e50bd21a50f
Vladislav Golubev <vladislav.golubev@intel.com>
2022-12-06 09:49:54 +0100

    Revert "CPU skip tests config corrected to avoid BF16 tests for avx512 platforms without bf16 support"
    This reverts commit 3a805746ebd1d1218f09b7ecaa6008939611ea96.

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/skip_tests_config.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/skip_tests_config.cpp
@@ -211,7 +211,7 @@ std::vector<std::string> disabledTestPatterns() {
     retVector.emplace_back(R"(.*LoadNetworkCompiledKernelsCacheTest.*CanCreateCacheDirAndDumpBinariesUnicodePath.*)");
 #endif
 
-    if (!InferenceEngine::with_cpu_x86_bfloat16()) {
+    if (!InferenceEngine::with_cpu_x86_avx512_core()) {
         // on platforms which do not support bfloat16, we are disabling bf16 tests since there are no bf16 primitives,
         // tests are useless on such platforms
         retVector.emplace_back(R"(.*(BF|bf)16.*)");
