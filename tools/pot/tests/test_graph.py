4ace6da3bfc59434dcfccf4d8939f2ccec892684
Liubov Talamanova <liubov.talamanova@intel.com>
2022-12-07 10:05:03 +0000

    [POT] Update test references (#14318)
    * Update references
    
    * Update GNA references
    
    * Rebase branch

--- tools/pot/tests/test_graph.py
+++ tools/pot/tests/test_graph.py
@@ -259,14 +259,14 @@ def test_multibranch_propagation_without_fq_moving(tmp_path, models, model_name,
 
 MODELS_WITH_LSTM = [
     ('lstm_example', 'pytorch', {
-        'LSTM_15/TensorIterator/22/variable_1':
-            ['Assign_304'],
-        'LSTM_15/TensorIterator/24/variable_2':
-            ['Assign_311'],
-        'LSTM_19/TensorIterator/22/variable_1':
-            ['Assign_333'],
-        'LSTM_19/TensorIterator/24/variable_2':
-            ['Assign_340']
+        'ReadValue_2474':
+            ['Assign_2475'],
+        'ReadValue_2430':
+            ['Assign_2431'],
+        'ReadValue_2440':
+            ['Assign_2441'],
+        'ReadValue_2464':
+            ['Assign_2465']
     })
 ]
 
