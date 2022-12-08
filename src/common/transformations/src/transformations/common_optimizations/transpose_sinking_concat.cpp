48f6baaea783ffbc7a19d3f55e9df349f97f65dd
Evgeny Kotov <evgeny.kotov@intel.com>
2022-12-08 17:28:07 +0100

    fix names

--- src/common/transformations/src/transformations/common_optimizations/transpose_sinking_concat.cpp
+++ src/common/transformations/src/transformations/common_optimizations/transpose_sinking_concat.cpp
@@ -58,12 +58,12 @@ ov::pass::TransposeSinkingConcatBackward::TransposeSinkingConcatBackward() {
 
     auto transpose_const_label = wrap_type<Constant>(consumers_count(1));
 
-    auto IfSinkingEnable = [](const Output<Node>& output) -> bool {
+    auto IfSinkingEnabled = [](const Output<Node>& output) -> bool {
         static auto consumers_check = consumers_count(1);
         return consumers_check(output) && is_sinking_node(output.get_node_shared_ptr());
     };
 
-    auto transpose_label = wrap_type<Transpose>({main_node_label, transpose_const_label}, IfSinkingEnable);
+    auto transpose_label = wrap_type<Transpose>({main_node_label, transpose_const_label}, IfSinkingEnabled);
 
     matcher_pass_callback matcher_pass_callback = [=](Matcher& m) {
         const auto& pattern_to_output = m.get_pattern_value_map();
