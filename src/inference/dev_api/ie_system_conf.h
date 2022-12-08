31fb27240592c46964ab57d0b40833d1add6c831
Shen, Wanglei <wanglei.shen@intel.com>
2022-12-09 00:24:09 +0800

    fix code style issue

--- src/inference/dev_api/ie_system_conf.h
+++ src/inference/dev_api/ie_system_conf.h
@@ -137,6 +137,7 @@ INFERENCE_ENGINE_API_CPP(bool) with_cpu_x86_avx512_core_amx();
  * @brief This enum contains columns of CPU map. Below is the structure of CPU map
  *
  * Proc ID | Socket ID | HW Core ID | Phy Core of Pcores | Logic Core of Pcores | ID of Ecore Group | Used
+ *
  */
 enum CpuMapColumn : int {
     CPU_MAP_SOCKET = 0,     
