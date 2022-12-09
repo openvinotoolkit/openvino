323001b7d30d6f583ebc8f31fe98c077ff23f2fc
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2022-12-09 19:15:49 +0100

    Add evaluate to CumSum core op

--- src/core/include/openvino/op/cum_sum.hpp
+++ src/core/include/openvino/op/cum_sum.hpp
@@ -38,6 +38,11 @@ public:
 
     std::shared_ptr<Node> clone_with_new_inputs(const OutputVector& new_args) const override;
 
+    OPENVINO_SUPPRESS_DEPRECATED_START
+    bool evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const override;
+    OPENVINO_SUPPRESS_DEPRECATED_END
+    bool has_evaluate() const override;
+
     bool visit_attributes(AttributeVisitor& visitor) override;
     void validate_and_infer_types() override;
 
--- src/core/src/op/cum_sum.cpp
+++ src/core/src/op/cum_sum.cpp
@@ -2,16 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "ngraph/op/cum_sum.hpp"
+#include "openvino/op/cum_sum.hpp"
 
 #include "itt.hpp"
-#include "ngraph/attribute_visitor.hpp"
 #include "ngraph/graph_util.hpp"
-#include "ngraph/op/broadcast.hpp"
-#include "ngraph/op/constant.hpp"
+#include "ngraph/runtime/reference/cum_sum.hpp"
+#include "ngraph/validation_util.hpp"
+#include "openvino/core/attribute_visitor.hpp"
+#include "openvino/op/constant.hpp"
 
 using namespace std;
-using namespace ngraph;
+using namespace ov;
 
 op::v0::CumSum::CumSum(const Output<Node>& arg, const Output<Node>& axis, const bool exclusive, const bool reverse)
     : Op({arg, axis}),
@@ -21,7 +22,7 @@ op::v0::CumSum::CumSum(const Output<Node>& arg, const Output<Node>& axis, const 
 }
 
 op::v0::CumSum::CumSum(const Output<Node>& arg, const bool exclusive, const bool reverse)
-    : Op({arg, op::v0::Constant::create(element::i32, ov::Shape{}, {0})}),
+    : Op({arg, op::v0::Constant::create(element::i32, Shape{}, {0})}),
       m_exclusive(exclusive),
       m_reverse(reverse) {
     constructor_validate_and_infer_types();
@@ -58,8 +59,89 @@ shared_ptr<Node> op::v0::CumSum::clone_with_new_inputs(const OutputVector& new_a
     }
 }
 
-NGRAPH_SUPPRESS_DEPRECATED_START
+OPENVINO_SUPPRESS_DEPRECATED_START
 shared_ptr<Node> op::v0::CumSum::get_default_value() const {
     return ngraph::make_constant_from_string("0", get_element_type(), get_shape());
 }
-NGRAPH_SUPPRESS_DEPRECATED_END
+OPENVINO_SUPPRESS_DEPRECATED_END
+
+namespace {
+template <element::Type_t DATA_ET, element::Type_t AXIS_ET>
+bool evaluate_cum_sum(const HostTensorPtr& output,
+                      const HostTensorPtr& data,
+                      const HostTensorPtr& axis,
+                      const bool exclusive,
+                      const bool reverse) {
+    using data_t = fundamental_type_for<DATA_ET>;
+    using axis_t = fundamental_type_for<AXIS_ET>;
+    ngraph::runtime::reference::cumsum<data_t, axis_t>(data->get_data_ptr<data_t>(),
+                                                       axis->get_data_ptr<axis_t>(),
+                                                       output->get_data_ptr<data_t>(),
+                                                       data->get_shape(),
+                                                       exclusive,
+                                                       reverse);
+    return true;
+}
+
+#define CUM_SUM_TYPE_CASE(a, ...)                                          \
+    case element::Type_t::a: {                                             \
+        OV_OP_SCOPE(OV_PP_CAT4(evaluate_cum_sum, _, a, AXIS_ET));          \
+        return evaluate_cum_sum<element::Type_t::a, AXIS_ET>(__VA_ARGS__); \
+    }
+
+template <element::Type_t AXIS_ET>
+bool evaluate(const HostTensorPtr& output,
+              const HostTensorPtr& data,
+              const HostTensorPtr& axis,
+              const bool exclusive,
+              const bool reverse) {
+    switch (data->get_element_type()) {
+        CUM_SUM_TYPE_CASE(bf16, output, data, axis, exclusive, reverse);
+        CUM_SUM_TYPE_CASE(f16, output, data, axis, exclusive, reverse);
+        CUM_SUM_TYPE_CASE(f32, output, data, axis, exclusive, reverse);
+        CUM_SUM_TYPE_CASE(f64, output, data, axis, exclusive, reverse);
+        CUM_SUM_TYPE_CASE(i4, output, data, axis, exclusive, reverse);
+        CUM_SUM_TYPE_CASE(i8, output, data, axis, exclusive, reverse);
+        CUM_SUM_TYPE_CASE(i16, output, data, axis, exclusive, reverse);
+        CUM_SUM_TYPE_CASE(i32, output, data, axis, exclusive, reverse);
+        CUM_SUM_TYPE_CASE(i64, output, data, axis, exclusive, reverse);
+        CUM_SUM_TYPE_CASE(u1, output, data, axis, exclusive, reverse);
+        CUM_SUM_TYPE_CASE(u4, output, data, axis, exclusive, reverse);
+        CUM_SUM_TYPE_CASE(u8, output, data, axis, exclusive, reverse);
+        CUM_SUM_TYPE_CASE(u16, output, data, axis, exclusive, reverse);
+        CUM_SUM_TYPE_CASE(u32, output, data, axis, exclusive, reverse);
+        CUM_SUM_TYPE_CASE(u64, output, data, axis, exclusive, reverse);
+    default:
+        return false;
+    }
+}
+
+bool evaluate_cum_sum(const HostTensorPtr& output,
+                      const HostTensorPtr& data,
+                      const HostTensorPtr& axis,
+                      const bool exclusive,
+                      const bool reverse) {
+    auto rc = true;
+    switch (axis->get_element_type()) {
+        NGRAPH_TYPE_CASE(evaluate_cum_sum, i32, output, data, axis, exclusive, reverse);
+        NGRAPH_TYPE_CASE(evaluate_cum_sum, i64, output, data, axis, exclusive, reverse);
+    default:
+        return false;
+    }
+    return rc;
+}
+}  // namespace
+
+bool op::v0::CumSum::evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const {
+    OV_OP_SCOPE(v0_CumSum_evaluate);
+    OPENVINO_ASSERT(ngraph::validate_host_tensor_vector(inputs, 2), "Invalid CumSum input TensorVector.");
+    OPENVINO_ASSERT(ngraph::validate_host_tensor_vector(outputs, 1), "Invalid CumSum output TensorVector.");
+
+    return evaluate_cum_sum(outputs[0], inputs[0], inputs[1], is_exclusive(), is_reverse());
+}
+
+bool op::v0::CumSum::has_evaluate() const {
+    OV_OP_SCOPE(v0_CumSum_has_evaluate);
+    const auto& input_1_element_type = get_input_element_type(1);
+    return input_1_element_type == element::i32 || input_1_element_type == element::i64;
+}
