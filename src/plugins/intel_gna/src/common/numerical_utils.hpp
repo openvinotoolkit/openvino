ed44fe4882d36511010b456451215925dd4c4fa0
Marcin Kusmierski <marcin.kusmierski@intel.com>
2022-12-14 13:45:26 +0100

    [GNA] Refactor numerical_utils and usage of its method in plugin
    * refactor numerical utils use template methods for conversion and comparision

--- src/plugins/intel_gna/src/common/numerical_utils.hpp
+++ src/plugins/intel_gna/src/common/numerical_utils.hpp
@@ -11,19 +11,21 @@ namespace ov {
 namespace intel_gna {
 namespace common {
 
-#define FLOAT_TO_INT8(a)  static_cast<int8_t>(((a) < 0) ? ((a)-0.5f) : ((a) + 0.5f))
-#define FLOAT_TO_INT16(a) static_cast<int16_t>(((a) < 0) ? ((a)-0.5f) : ((a) + 0.5f))
-#define FLOAT_TO_INT32(a) static_cast<int32_t>(((a) < 0) ? ((a)-0.5f) : ((a) + 0.5f))
-#define FLOAT_TO_INT64(a) static_cast<int64_t>(((a) < 0) ? ((a)-0.5f) : ((a) + 0.5f))
-
-/**
- * @brief Compares two float values and returns if they are equal
- * @param p1 First float value
- * @param p2 Second float value
- * @return Returns true if two float values are equal
- */
-inline bool fp32eq(float p1, float p2, float accuracy = 0.00001f) {
-    return (std::abs(p1 - p2) <= accuracy * std::min(std::abs(p1), std::abs(p2)));
+template <typename T>
+inline T FloatToInteger(float a) {
+    return static_cast<T>((a < 0.0f) ? (a - 0.5f) : (a + 0.5f));
+}
+inline int8_t FloatToInt8(float a) {
+    return FloatToInteger<int8_t>(a);
+}
+inline int16_t FloatToInt16(float a) {
+    return FloatToInteger<int16_t>(a);
+}
+inline int32_t FloatToInt32(float a) {
+    return FloatToInteger<int32_t>(a);
+}
+inline int64_t FloatToInt64(float a) {
+    return FloatToInteger<int64_t>(a);
 }
 
 /**
