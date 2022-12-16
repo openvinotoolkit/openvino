c72b2833047004621498465133985d515e34da15
Pawel Raasz <pawel.raasz@intel.com>
2022-12-16 11:37:47 +0100

    Review logical ops for shape inference aspects (#14393)
    * Review dims and labels propagation for logical not
    
    * Review dims and labels propagation
    for logical and, or, xor
    
    * Remove duplicated tests
    
    * Expand logical ops tests by numpy broadcast
    and inputs order
    
    * Review template shape infer of logical ops
     - add static shape inference test
     - add default ctor test
    
    * Default ctor test for LogicalNot op

---
+++ src/plugins/intel_cpu/tests/unit/shape_inference_test/bel_shape_inference_test.cpp
@@ -0,0 +1,107 @@
+// Copyright (C) 2022 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "common_test_utils/test_assertions.hpp"
+#include "gmock/gmock.h"
+#include "openvino/op/parameter.hpp"
+#include "utils.hpp"
+
+using namespace ov;
+using namespace ov::intel_cpu;
+using namespace testing;
+
+template <class TOp>
+class BELStaticShapeInferenceTest : public OpStaticShapeInferenceTest<TOp> {
+protected:
+    void SetUp() override {
+        this->output_shapes = ShapeVector(1);
+    }
+
+    element::Type dtype{element::boolean};
+};
+
+TYPED_TEST_SUITE_P(BELStaticShapeInferenceTest);
+
+TYPED_TEST_P(BELStaticShapeInferenceTest, broadcast_none) {
+    const auto a = std::make_shared<op::v0::Parameter>(this->dtype, PartialShape{-1, -1, -1, -1});
+    const auto b = std::make_shared<op::v0::Parameter>(this->dtype, PartialShape{-1, -1, -1, -1});
+    const auto op = this->make_op(a, b, op::AutoBroadcastType::NONE);
+
+    this->input_shapes = {StaticShape{3, 4, 7, 5}, StaticShape{3, 4, 7, 5}};
+
+    shape_inference(op.get(), this->input_shapes, this->output_shapes);
+
+    ASSERT_EQ(this->output_shapes.front(), StaticShape({3, 4, 7, 5}));
+}
+
+TYPED_TEST_P(BELStaticShapeInferenceTest, broadcast_none_incompatible_shapes) {
+    const auto a = std::make_shared<op::v0::Parameter>(this->dtype, PartialShape{-1, -1, -1, -1});
+    const auto b = std::make_shared<op::v0::Parameter>(this->dtype, PartialShape{-1, -1, -1, -1});
+    const auto op = this->make_op(a, b, op::AutoBroadcastType::NONE);
+
+    this->input_shapes = {StaticShape{3, 4, 6, 5}, StaticShape{3, 1, 6, 1}};
+
+    OV_EXPECT_THROW(shape_inference(op.get(), this->input_shapes, this->output_shapes),
+                    NodeValidationFailure,
+                    HasSubstr("Argument shapes are inconsistent."))
+}
+
+TYPED_TEST_P(BELStaticShapeInferenceTest, broadcast_numpy_equal_rank) {
+    const auto a = std::make_shared<op::v0::Parameter>(this->dtype, PartialShape{-1, -1, -1, -1});
+    const auto b = std::make_shared<op::v0::Parameter>(this->dtype, PartialShape{-1, -1, -1, -1});
+    const auto op = this->make_op(a, b);
+
+    this->input_shapes = {StaticShape{3, 1, 1, 5}, StaticShape{3, 1, 6, 1}};
+
+    shape_inference(op.get(), this->input_shapes, this->output_shapes);
+
+    ASSERT_EQ(this->output_shapes.front(), StaticShape({3, 1, 6, 5}));
+}
+
+TYPED_TEST_P(BELStaticShapeInferenceTest, broadcast_numpy_a_rank_higher) {
+    const auto a = std::make_shared<op::v0::Parameter>(this->dtype, PartialShape{-1, -1, -1, -1});
+    const auto b = std::make_shared<op::v0::Parameter>(this->dtype, PartialShape{-1, -1, -1});
+    const auto op = this->make_op(a, b);
+
+    this->input_shapes = {StaticShape{6, 5, 1, 8}, StaticShape{5, 6, 1}},
+
+    shape_inference(op.get(), this->input_shapes, this->output_shapes);
+
+    ASSERT_EQ(this->output_shapes.front(), StaticShape({6, 5, 6, 8}));
+}
+
+TYPED_TEST_P(BELStaticShapeInferenceTest, broadcast_numpy_b_rank_higher) {
+    const auto a = std::make_shared<op::v0::Parameter>(this->dtype, PartialShape{-1, -1, -1});
+    const auto b = std::make_shared<op::v0::Parameter>(this->dtype, PartialShape{-1, -1, -1, -1});
+    const auto op = this->make_op(a, b);
+
+    this->input_shapes = {StaticShape{5, 6, 1}, StaticShape{6, 5, 1, 8}},
+
+    shape_inference(op.get(), this->input_shapes, this->output_shapes);
+
+    ASSERT_EQ(this->output_shapes.front(), StaticShape({6, 5, 6, 8}));
+}
+
+TYPED_TEST_P(BELStaticShapeInferenceTest, broadcast_numpy_incompatible_shapes) {
+    const auto a = std::make_shared<op::v0::Parameter>(this->dtype, PartialShape{-1, -1, -1, -1});
+    const auto b = std::make_shared<op::v0::Parameter>(this->dtype, PartialShape{-1, -1, -1, -1});
+    const auto op = this->make_op(a, b);
+
+    this->input_shapes = {StaticShape{3, 4, 6, 6}, StaticShape{2, 4, 6, 6}};
+
+    OV_EXPECT_THROW(shape_inference(op.get(), this->input_shapes, this->output_shapes),
+                    NodeValidationFailure,
+                    HasSubstr("Argument shapes are inconsistent."))
+}
+
+REGISTER_TYPED_TEST_SUITE_P(BELStaticShapeInferenceTest,
+                            broadcast_none,
+                            broadcast_none_incompatible_shapes,
+                            broadcast_numpy_equal_rank,
+                            broadcast_numpy_a_rank_higher,
+                            broadcast_numpy_b_rank_higher,
+                            broadcast_numpy_incompatible_shapes);
+
+using BinaryLogicOpTypes = Types<op::v1::LogicalAnd, op::v1::LogicalOr, op::v1::LogicalXor>;
+INSTANTIATE_TYPED_TEST_SUITE_P(shape_inference, BELStaticShapeInferenceTest, BinaryLogicOpTypes);
