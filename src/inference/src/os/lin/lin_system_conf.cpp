f3c1c37f987b0bca4c706cc91decd4cdf255617d
Shen, Wanglei <wanglei.shen@intel.com>
2022-12-17 18:16:34 +0800

    remove changes for linux

--- src/inference/src/os/lin/lin_system_conf.cpp
+++ src/inference/src/os/lin/lin_system_conf.cpp
@@ -3,8 +3,6 @@
 //
 
 #include <sched.h>
-#include <string.h>
-#include <unistd.h>
 
 #include <fstream>
 #include <iostream>
@@ -23,12 +21,6 @@ struct CPU {
     int _processors = 0;
     int _sockets = 0;
     int _cores = 0;
-
-    int _node = 0;
-    int _p_cores = 0;
-    int _e_cores = 0;
-    int _phy_cores = 0;
-    int _proc = 0;
 
     CPU() {
         std::ifstream cpuinfo("/proc/cpuinfo");
@@ -61,131 +53,6 @@ struct CPU {
         if (_cores == 0) {
             _cores = _processors;
         }
-
-        /**
-         * New method to get CPU infomation and CPU map. Below is the structure of CPU map and two sample.
-         *  1. Four processors of two Pcore
-         *  2. Four processors of four Ecores shared L2 cache
-         *
-         *  Proc ID | Socket ID | HW Core ID | Phy Core of Pcores | Logic Core of Pcores | ID of Ecore Group | Used
-         *     0         1            1                0                      1                   0             0
-         *     1         1            1                1                      0                   0             0
-         *     2         1            2                0                      2                   0             0
-         *     3         1            2                2                      0                   0             0
-         *     4         1            3                0                      0                   1             0
-         *     5         1            4                0                      0                   1             0
-         *     6         1            5                0                      0                   1             0
-         *     7         1            6                0                      0                   1             0
-         */
-        _proc = sysconf(_SC_NPROCESSORS_ONLN);
-        int _cpu_mapping[_proc][CPU_MAP_USED_PROC + 1];
-        memset(_cpu_mapping, 0, _proc * (CPU_MAP_USED_PROC + 1) * sizeof(int));
-
-        auto updateProcMapping = [&](const int nproc) {
-            if (0 == _cpu_mapping[nproc][CPU_MAP_CORE]) {
-                int core_1 = 0;
-                int core_2 = 0;
-                std::string::size_type pos = 0;
-                std::string::size_type endpos = 0;
-                std::string sub_str = "";
-
-                std::ifstream cache_1_file("/sys/devices/system/cpu/cpu" + std::to_string(nproc) +
-                                           "/cache/index0/shared_cpu_list");
-                std::ifstream cache_2_file("/sys/devices/system/cpu/cpu" + std::to_string(nproc) +
-                                           "/cache/index2/shared_cpu_list");
-                std::string cache_1_info;
-                std::getline(cache_1_file, cache_1_info);
-                std::string cache_2_info;
-                std::getline(cache_2_file, cache_2_info);
-
-                if (((endpos = cache_1_info.find(',', pos)) != std::string::npos) ||
-                    ((endpos = cache_1_info.find('-', pos)) != std::string::npos)) {
-                    sub_str = cache_1_info.substr(pos, endpos);
-                    core_1 = std::stoi(sub_str);
-                    sub_str = cache_1_info.substr(endpos + 1);
-                    core_2 = std::stoi(sub_str);
-
-                    _phy_cores++;
-                    _p_cores++;
-                    _cpu_mapping[core_1][CPU_MAP_CORE] = _phy_cores;
-                    _cpu_mapping[core_2][CPU_MAP_CORE] = _phy_cores;
-
-                    /**
-                     * Processor 0 need to handle system interception on Linux. So use second processor as physical core
-                     * and first processor as logic core
-                     */
-                    _cpu_mapping[core_1][CPU_MAP_LOG_CORE] = _p_cores;
-                    _cpu_mapping[core_2][CPU_MAP_PHY_CORE] = _p_cores;
-
-                } else if ((endpos = cache_2_info.find('-', pos)) != std::string::npos) {
-                    sub_str = cache_2_info.substr(pos, endpos);
-                    core_1 = std::stoi(sub_str);
-                    sub_str = cache_2_info.substr(endpos + 1);
-                    core_2 = std::stoi(sub_str);
-
-                    _e_cores++;
-                    for (int m = core_1; m <= core_2; m++) {
-                        _phy_cores++;
-                        _cpu_mapping[m][CPU_MAP_CORE] = _phy_cores;
-                        _cpu_mapping[m][CPU_MAP_SMALL_CORE] = _e_cores;
-                    }
-
-                } else {
-                    core_1 = std::stoi(cache_1_info);
-
-                    _p_cores++;
-                    _phy_cores++;
-                    _cpu_mapping[core_1][CPU_MAP_CORE] = _phy_cores;
-                    _cpu_mapping[core_2][CPU_MAP_PHY_CORE] = _p_cores;
-                }
-            }
-            return;
-        };
-
-        for (int n = 0; n < _proc; n++) {
-            if (0 == _cpu_mapping[n][CPU_MAP_SOCKET]) {
-                std::ifstream cache_3_file("/sys/devices/system/cpu/cpu" + std::to_string(n) +
-                                           "/cache/index3/shared_cpu_list");
-                std::string cache_3_info;
-                std::getline(cache_3_file, cache_3_info);
-
-                std::string::size_type pos = 0;
-                std::string::size_type endpos = 0;
-                std::string sub_str;
-
-                int core_1;
-                int core_2;
-
-                _node++;
-                while (1) {
-                    if ((endpos = cache_3_info.find('-', pos)) != std::string::npos) {
-                        sub_str = cache_3_info.substr(pos, endpos);
-                        core_1 = std::stoi(sub_str);
-                        sub_str = cache_3_info.substr(endpos + 1);
-                        core_2 = std::stoi(sub_str);
-
-                        for (int m = core_1; m <= core_2; m++) {
-                            _cpu_mapping[m][CPU_MAP_SOCKET] = _node;
-                            updateProcMapping(m);
-                        }
-
-                    } else if (pos != std::string::npos) {
-                        sub_str = cache_3_info.substr(pos);
-                        core_1 = std::stoi(sub_str);
-                        _cpu_mapping[core_1][CPU_MAP_SOCKET] = _node;
-                        updateProcMapping(core_1);
-                        endpos = pos;
-                    }
-
-                    if ((pos = cache_3_info.find(',', endpos)) != std::string::npos) {
-                        pos++;
-                    } else {
-                        break;
-                    }
-                }
-            }
-        }
-        /**********************/
     }
 };
 static CPU cpu;
