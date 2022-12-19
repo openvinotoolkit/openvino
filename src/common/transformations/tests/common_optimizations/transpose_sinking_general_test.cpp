c134347bf3969b21a626f1870f231c1043f1b97f
Evgeny Kotov <evgeny.kotov@intel.com>
2022-12-19 18:48:38 +0100

    remove unused headers

--- src/common/transformations/tests/common_optimizations/transpose_sinking_general_test.cpp
+++ src/common/transformations/tests/common_optimizations/transpose_sinking_general_test.cpp
@@ -8,11 +8,6 @@
 #include <openvino/pass/manager.hpp>
 #include <transformations/common_optimizations/transpose_sinking_general.hpp>
 #include <transformations/init_node_info.hpp>
-#include "common_test_utils/ngraph_test_utils.hpp"
-
-#include "ngraph/pass/visualize_tree.hpp" // DEBUG
-
-#include <functional>
 
 #include "common_test_utils/ngraph_test_utils.hpp"
 #include "gtest/gtest.h"
