3ab1efcb91c45fb5d03cd56e93be5f008e491d75
Pavel Esir <pavel.esir@intel.com>
2022-12-20 14:59:14 +0100

    shortened specifying MO args

--- tools/mo/unit_tests/mo/utils/convert_impl_tmp_irs_cleanup_test.py
+++ tools/mo/unit_tests/mo/utils/convert_impl_tmp_irs_cleanup_test.py
@@ -7,11 +7,11 @@ from unittest.mock import patch
 
 from openvino.tools.mo.convert import convert_model
 from openvino.tools.mo.utils.error import Error
-from unit_tests.utils.utils import base_args_config
 
 
 class TestConvertImplTmpIrsCleanup(unittest.TestCase):
-    test_model_file = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "moc_tf_fe/test_models/mul_with_unknown_rank_y.pbtxt") 
+    test_model_file = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir,
+                                   "moc_tf_fe/test_models/mul_with_unknown_rank_y.pbtxt")
 
     @staticmethod
     def are_tmp_files_left(orig_model_name):
@@ -23,36 +23,24 @@ class TestConvertImplTmpIrsCleanup(unittest.TestCase):
 
     def test_tmp_irs_cleanup_convert_impl_1(self):
         with patch("openvino.tools.mo.back.offline_transformations.apply_offline_transformations") as emit_ir_func:
-            emit_ir_func.side_effect = Error("Error during offline_transformation")
+            emit_ir_func.side_effect = Error('offline transformations step has failed')
 
-            args = base_args_config()
-            args.input_model = self.test_model_file
-            args.input = "x[3],y[1 3]"
-            args.input_model_is_text = True
-
-            self.assertRaises(Error, convert_model, **vars(args))
+            params = {'input_model': self.test_model_file, 'input_model_is_text': True, 'input': 'x[3],y[1 3]'}
+            self.assertRaisesRegex(Error, 'offline transformations step has failed',  convert_model, **params)
             self.assertFalse(self.are_tmp_files_left(self.test_model_file))
 
     def test_tmp_irs_cleanup_convert_impl_2(self):
         with patch("openvino.tools.mo.back.ie_ir_ver_2.emitter.add_net_rt_info") as emit_ir_func:
-            emit_ir_func.side_effect = Error("Error during tmp emitting")
+            emit_ir_func.side_effect = Error('emitting tmp IR has failed')
 
-            args = base_args_config()
-            args.input_model = self.test_model_file
-            args.input = "x[3],y[1 3]"
-            args.input_model_is_text = True
-
-            self.assertRaises(Error, convert_model, **vars(args))
+            params = {'input_model': self.test_model_file, 'input_model_is_text': True, 'input': 'x[3],y[1 3]'}
+            self.assertRaisesRegex(Error, 'emitting tmp IR has failed', convert_model, **params)
             self.assertFalse(self.are_tmp_files_left(self.test_model_file))
 
     def test_tmp_irs_cleanup_convert_impl_3(self):
         with patch("openvino.tools.mo.convert_impl.read_model") as emit_ir_func:
-            emit_ir_func.side_effect = Exception("Error during FEM read_model")
+            emit_ir_func.side_effect = Exception('FEM read_model has failed')
 
-            args = base_args_config()
-            args.input_model = self.test_model_file
-            args.input = "x[3],y[1 3]"
-            args.input_model_is_text = True
-
-            self.assertRaises(Exception, convert_model, **vars(args))
+            params = {'input_model': self.test_model_file, 'input_model_is_text': True, 'input': 'x[3],y[1 3]'}
+            self.assertRaisesRegex(Error, 'FEM read_model has failed', convert_model, **params)
             self.assertFalse(self.are_tmp_files_left(self.test_model_file))
