3858925be9c3faac8fae9420ef7f14f2bc0affc1
mitruska <katarzyna.mitrus@intel.com>
2022-12-20 13:58:56 +0100

    Use get_node_input_partial_shapes

--- src/core/src/op/select.cpp
+++ src/core/src/op/select.cpp
@@ -43,10 +43,9 @@ void op::v1::Select::validate_and_infer_types() {
                           element::Type::merge(result_et, get_input_element_type(1), get_input_element_type(2)),
                           "Argument 1 and 2 element types must match.");
 
-    std::vector<ov::PartialShape> output_shapes = {ov::PartialShape{}};
-    const std::vector<ov::PartialShape> input_shapes = {get_input_partial_shape(0),
-                                                        get_input_partial_shape(1),
-                                                        get_input_partial_shape(2)};
+    const auto input_shapes = get_node_input_partial_shapes(*this);
+    std::vector<ov::PartialShape> output_shapes{ov::PartialShape::dynamic()};
+
     shape_infer(this, input_shapes, output_shapes);
     set_output_type(0, result_et, output_shapes[0]);
 }
