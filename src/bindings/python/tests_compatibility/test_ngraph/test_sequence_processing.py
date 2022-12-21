f98ed2593b31d19838f6646a10876227f019383d
Przemyslaw Wysocki <przemyslaw.wysocki@intel.com>
2022-12-21 14:44:20 +0100

    [PyOV] Make graph tests hardware agnostic - part 4 (#14705)

--- src/bindings/python/tests_compatibility/test_ngraph/test_sequence_processing.py
+++ src/bindings/python/tests_compatibility/test_ngraph/test_sequence_processing.py
@@ -4,17 +4,13 @@
 import numpy as np
 
 import ngraph as ng
-from tests_compatibility.runtime import get_runtime
-from tests_compatibility.test_ngraph.util import run_op_node
+from ngraph.impl import Type
 
 
 def test_onehot():
-    runtime = get_runtime()
     param = ng.parameter([3], dtype=np.int32)
     model = ng.one_hot(param, 3, 1, 0, 0)
-    computation = runtime.computation(model, param)
-
-    expected = np.eye(3)[np.array([1, 0, 2])]
-    input_data = np.array([1, 0, 2], dtype=np.int32)
-    result = computation(input_data)
-    assert np.allclose(result, expected)
+    assert model.get_output_size() == 1
+    assert model.get_type_name() == "OneHot"
+    assert list(model.get_output_shape(0)) == [3, 3]
+    assert model.get_output_element_type(0) == Type.i64
