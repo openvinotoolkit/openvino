6971add819d24907c45feae8edaa4d04da1f7c60
rnugmano <ruslan.nugmanov@intel.com>
2022-12-21 12:01:47 +0100

    clean-ups

--- tests/layer_tests/common/tf_layer_test_class.py
+++ tests/layer_tests/common/tf_layer_test_class.py
@@ -40,7 +40,6 @@ def save_to_pb(tf_model, path_to_saved_tf_model):
 
 def save_pb_to_tflite(pb_model):
     import tensorflow as tf
-    from pathlib import Path
 
     graph_summary = summarize_graph(pb_model)
     inputs = [k for k in graph_summary['inputs'].keys()]
@@ -49,7 +48,7 @@ def save_pb_to_tflite(pb_model):
     converter = tf.compat.v1.lite.TFLiteConverter.from_frozen_graph(pb_model, inputs, outputs)
     tflite_model = converter.convert()
 
-    tflite_model_path = Path(pb_model).parent / 'model.tflite'
+    tflite_model_path = os.path.join(os.path.dirname(pb_model), 'model.tflite')
     with tf.io.gfile.GFile(tflite_model_path, 'wb') as f:
         f.write(tflite_model)
 
@@ -57,6 +56,16 @@ def save_pb_to_tflite(pb_model):
 
 
 class CommonTFLayerTest(CommonLayerTest):
+    def prepare_tf_inputs(self, inputs_dict):
+        input = dict()
+        for key in inputs_dict.keys():
+            data = inputs_dict.get(key)
+            if self.use_old_api or self.use_new_frontend:
+                key += ':0'
+            input[key] = transpose_nchw_to_nhwc(data, self.use_new_frontend, self.use_old_api)
+
+        return input
+
     def produce_model_path(self, framework_model, save_path):
         if not getattr(self, 'tflite', False):
             return save_to_pb(framework_model, save_path)
@@ -64,59 +73,65 @@ class CommonTFLayerTest(CommonLayerTest):
             pb_model = save_to_pb(framework_model, save_path)
             return save_pb_to_tflite(pb_model)
 
-    def get_framework_results(self, inputs_dict, model_path):
-        # Evaluate model via Tensorflow and IE
-        # Load the Tensorflow model
+    def get_tf_results(self, inputs_dict, model_path):
         import tensorflow as tf
+        from tensorflow.python.platform import gfile
+
+        graph_summary = summarize_graph(model_path=model_path)
+        outputs_list = graph_summary["outputs"]
+
+        tf.compat.v1.reset_default_graph()
+
+        with tf.compat.v1.Session() as sess:
+            with gfile.FastGFile(model_path, 'rb') as f:
+                graph_def = tf.compat.v1.GraphDef()
+                graph_def.ParseFromString(f.read())
+                sess.graph.as_default()
+                tf.compat.v1.import_graph_def(graph_def, name='')
+
+                tf_res = sess.run([out + ":0" for out in outputs_list], inputs_dict)
+
+                result = dict()
+                for i, output in enumerate(outputs_list):
+                    _tf_res = tf_res[i]
+                    result[output] = transpose_nhwc_to_nchw(_tf_res, self.use_new_frontend,
+                                                            self.use_old_api)
+                return result
+
+    def get_tflite_results(self, inputs_dict, model_path):
+        import tensorflow as tf
+        interpreter = tf.compat.v1.lite.Interpreter(model_path=model_path)
+        interpreter.allocate_tensors()
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
+
+        input_name_to_id_mapping = {input['name']: input['index'] for input in input_details}
+
+        for layer, data in inputs_dict.items():
+            tensor_index = input_name_to_id_mapping[layer]
+            tensor_id = next(i for i, tensor in enumerate(input_details) if tensor['index'] == tensor_index)
+            interpreter.set_tensor(input_details[tensor_id]['index'], data)
+
+        interpreter.invoke()
+        tf_result = dict()
+        for output in output_details:
+            tf_result[output['name']] = interpreter.get_tensor(output['index'])
+
+        result = dict()
+        for out in tf_result.keys():
+            _tf_res = tf_result[out]
+            result[out] = transpose_nhwc_to_nchw(_tf_res, self.use_new_frontend,
+                                                 self.use_old_api)
+
+        return tf_result
+
+    def get_framework_results(self, inputs_dict, model_path):
+        # prepare inputs
+        input = self.prepare_tf_inputs(inputs_dict)
 
         if not getattr(self, 'tflite', False):
             # get results from tensorflow
-            from tensorflow.python.platform import gfile
-
-            graph_summary = summarize_graph(model_path=model_path)
-            outputs_list = graph_summary["outputs"]
-
-            tf.compat.v1.reset_default_graph()
-
-            with tf.compat.v1.Session() as sess:
-                with gfile.FastGFile(model_path, 'rb') as f:
-                    graph_def = tf.compat.v1.GraphDef()
-                    graph_def.ParseFromString(f.read())
-                    sess.graph.as_default()
-                    tf.compat.v1.import_graph_def(graph_def, name='')
-
-                    input = dict()
-                    for key in inputs_dict.keys():
-                        data = inputs_dict.get(key)
-                        if self.use_old_api or self.use_new_frontend:
-                            key += ':0'
-                        input[key] = transpose_nchw_to_nhwc(data, self.use_new_frontend, self.use_old_api)
-
-                    tf_res = sess.run([out + ":0" for out in outputs_list], input)
-
-                    result = dict()
-                    for i, output in enumerate(outputs_list):
-                        _tf_res = tf_res[i]
-                        result[output] = transpose_nhwc_to_nchw(_tf_res, self.use_new_frontend,
-                                                                self.use_old_api)
-                    return result
+            return self.get_tf_results(input, model_path)
         else:
             # get results from tflite
-            interpreter = tf.compat.v1.lite.Interpreter(model_path=str(model_path))
-            interpreter.allocate_tensors()
-            input_details = interpreter.get_input_details()
-            output_details = interpreter.get_output_details()
-
-            input_name_to_id_mapping = {input['name']: input['index'] for input in input_details}
-
-            for layer, data in inputs_dict.items():
-                tensor_index = input_name_to_id_mapping[layer]
-                tensor_id = next(i for i, tensor in enumerate(input_details) if tensor['index'] == tensor_index)
-                interpreter.set_tensor(input_details[tensor_id]['index'], data)
-
-            interpreter.invoke()
-            result = dict()
-            for output in output_details:
-                result[output['name']] = interpreter.get_tensor(output['index'])
-
-            return result
+            return self.get_tflite_results(input, model_path)
