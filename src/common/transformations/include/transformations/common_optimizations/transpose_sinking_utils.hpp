cfc16562c79d709c14df4db3fe810b841866a845
Evgeny Kotov <evgeny.kotov@intel.com>
2022-12-23 18:02:46 +0100

    fix function descriptions

--- src/common/transformations/include/transformations/common_optimizations/transpose_sinking_utils.hpp
+++ src/common/transformations/include/transformations/common_optimizations/transpose_sinking_utils.hpp
@@ -29,66 +29,65 @@ struct TransposeInputsInfo {
 };
 
 /**
- * @brief Find node first input that is a transpose operation and return filled TransposeInputsInfo
+ * @brief Finds node first input that is a transpose operation and returns filled TransposeInputsInfo
  * for it
  */
 TransposeInputsInfo GetFirstTransposeInput(std::shared_ptr<ov::Node>);
 
 /**
- * @brief Check if @arg has any input node that is a transpose operation
+ * @brief Checks if @arg has any input node that is a transpose operation
  */
 bool IfNodeHasTransposeInputs(const ov::Output<ov::Node>&);
 
 /**
- * @brief Reverse order of transpose operation. Do it in a such way that if we had couple following one after
+ * @brief Reverses order of transpose operation. Do it in a such way that if we had couple following one after
  * another transposes (one would be reversed version of another) we will have no transpose as a result of that
  * couple of transposes.
  */
 ov::AxisVector ReverseTransposeOrder(const ov::AxisVector&);
 
 /**
- * @brief Swap @args output tensor names
+ * @brief Swaps @args output tensor names
  */
 void SwapOutputNames(ov::Output<ov::Node>, ov::Output<ov::Node>);
 
 /**
- * @brief Swap @args friendly names
+ * @brief Swaps @args friendly names
  */
 void SwapFriendlyNames(std::shared_ptr<ov::Node>, std::shared_ptr<ov::Node>);
 
 /**
- * @brief Swap @args output tensor names and friendly names
+ * @brief Swaps @args output tensor names and friendly names
  */
 void SwapNames(std::shared_ptr<ov::Node>, std::shared_ptr<ov::Node>);
 
 /**
- * @brief Clone @arg node with the same inputs as original. Reconnect all output consumers to a cloned node
+ * @brief Clones @arg node with the same inputs as original. Reconnects all output consumers to a cloned node
  * except consumers specified as @args consumers.
  */
 std::shared_ptr<ov::Node> CloneNodeWithoutConsumers(std::shared_ptr<ov::Node> node, const ov::NodeVector& consumers);
 
 namespace sink_forward {
-// insert input reversed transposes, remove first input tranpose
 /**
- * @brief Insert reversed transposed on @args main_node inputs. Remove input transpose specified in @arg
+ * @brief Inserts reversed transposed on @args main_node inputs. Removes input transpose specified in @arg
  * transpose_input_info
  */
 void UpdateInputTransposes(std::shared_ptr<ov::Node> main_node, TransposeInputsInfo& transpose_input_info);
 
 /**
- * @brief Remove @arg input node
+ * @brief Removes @arg input node
  */
 void RemoveInputNode(std::shared_ptr<ov::Node>, size_t input_idx);
 
 /**
- * @brief Insert transposes on each main_node output with the order specified in @arg transpose_input_info
+ * @brief Inserts transposes on each main_node output with the order specified in @arg transpose_input_info
  */
 ov::NodeVector InsertOutputTransposes(std::shared_ptr<ov::Node> main_node, TransposeInputsInfo& transpose_input_info);
 }  // namespace sink_forward
 
 namespace sink_backward {
 /**
- * @brief Insert transposes on each input of @arg main_node with the order specified in @arg transpose_const
+ * @brief Inserts transposes on each input of @arg main_node with the order specified in @arg transpose_const
  */
 ov::NodeVector InsertTransposeBeforeNode(std::shared_ptr<ov::Node> main_node,
                                          std::shared_ptr<ov::opset9::Constant> transpose_const);
