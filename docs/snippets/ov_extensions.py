2374f82d20f96e7ebddda7adec543befb59d6513
Mateusz Tabaka <mateusz.tabaka@intel.com>
2022-12-23 07:44:25 +0100

    Update frontend extension docs with python examples (#14778)
    Ticket: 98857

--- docs/snippets/ov_extensions.py
+++ docs/snippets/ov_extensions.py
@@ -4,6 +4,11 @@
 
 import openvino.runtime as ov
 
+#! [py_frontend_extension_ThresholdedReLU_header]
+import openvino.runtime.opset8 as ops
+from openvino.frontend import ConversionExtension
+#! [py_frontend_extension_ThresholdedReLU_header]
+
 #! [add_extension]
 # Not implemented
 #! [add_extension]
@@ -15,6 +20,21 @@ import openvino.runtime as ov
 #! [add_extension_lib]
 core = ov.Core()
 # Load extensions library to ov.Core
-core.add_extension("openvino_template_extension.so")
+core.add_extension("libopenvino_template_extension.so")
 #! [add_extension_lib]
 
+#! [py_frontend_extension_MyRelu]
+from openvino.frontend import OpExtension
+core.add_extension(OpExtension("Relu", "MyRelu"))
+#! [py_frontend_extension_MyRelu]
+
+#! [py_frontend_extension_ThresholdedReLU]
+def conversion(node):
+    input_node = node.get_input(0)
+    input_type = input_node.get_element_type()
+    greater = ops.greater(input_node, ops.constant([node.get_attribute("alpha")], input_type))
+    casted = ops.convert(greater, input_type.get_type_name())
+    return ops.multiply(input_node, casted).outputs()
+
+core.add_extension(ConversionExtension("ThresholdedRelu", conversion))
+#! [py_frontend_extension_ThresholdedReLU]
