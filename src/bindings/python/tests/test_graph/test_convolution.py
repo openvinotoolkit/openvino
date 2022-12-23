73d9903856974b2788ebdd357449bd638d1e0a0f
Przemyslaw Wysocki <przemyslaw.wysocki@intel.com>
2022-12-23 11:06:00 +0100

    [PyOV] Make graph tests hardware agnostic - part 5 (#14743)

--- src/bindings/python/tests/test_graph/test_convolution.py
+++ src/bindings/python/tests/test_graph/test_convolution.py
@@ -40,26 +40,9 @@ def test_convolution_backprop_data():
     data_node = ov.parameter(shape=data_shape)
     filter_node = ov.parameter(shape=filter_shape)
     output_shape_node = ov.constant(np.array(output_spatial_shape, dtype=np.int64))
-    expected_shape = [1, 1, 9, 9]
 
     deconvolution = ov.convolution_backprop_data(data_node, filter_node, strides, output_shape_node)
     assert deconvolution.get_type_name() == "ConvolutionBackpropData"
     assert deconvolution.get_output_size() == 1
-    assert list(deconvolution.get_output_shape(0)) == expected_shape
+    assert list(deconvolution.get_output_shape(0)) == [1, 1, 9, 9]
     assert deconvolution.get_output_element_type(0) == Type.f32
-
-
-def test_convolution_v1():
-    input_tensor = ov.parameter((1, 1, 16, 16), name="input_tensor", dtype=np.float32)
-    filters = ov.parameter((1, 1, 3, 3), name="filters", dtype=np.float32)
-    strides = np.array([1, 1])
-    pads_begin = np.array([0, 0])
-    pads_end = np.array([0, 0])
-    dilations = np.array([1, 1])
-    expected_shape = [1, 1, 14, 14]
-
-    node = ov.convolution(input_tensor, filters, strides, pads_begin, pads_end, dilations)
-    assert node.get_type_name() == "Convolution"
-    assert node.get_output_size() == 1
-    assert list(node.get_output_shape(0)) == expected_shape
-    assert node.get_output_element_type(0) == Type.f32
--- src/bindings/python/tests/test_graph/test_ops.py
+++ src/bindings/python/tests/test_graph/test_ops.py
@@ -4,176 +4,42 @@
 # flake8: noqa
 
 import numpy as np
+import pytest
 
 import openvino.runtime.opset8 as ov
 from openvino.runtime import AxisSet, Shape, Type
 from openvino.runtime.op import Constant, Parameter
 
-
-def binary_op(op_str, a, b):
-
-    if op_str == "+":
-        return a + b
-    elif op_str == "Add":
-        return ov.add(a, b)
-    elif op_str == "-":
-        return a - b
-    elif op_str == "Sub":
-        return ov.subtract(a, b)
-    elif op_str == "*":
-        return a * b
-    elif op_str == "Mul":
-        return ov.multiply(a, b)
-    elif op_str == "/":
-        return a / b
-    elif op_str == "Div":
-        return ov.divide(a, b)
-    elif op_str == "Equal":
-        return ov.equal(a, b)
-    elif op_str == "Greater":
-        return ov.greater(a, b)
-    elif op_str == "GreaterEq":
-        return ov.greater_equal(a, b)
-    elif op_str == "Less":
-        return ov.less(a, b)
-    elif op_str == "LessEq":
-        return ov.less_equal(a, b)
-    elif op_str == "Maximum":
-        return ov.maximum(a, b)
-    elif op_str == "Minimum":
-        return ov.minimum(a, b)
-    elif op_str == "NotEqual":
-        return ov.not_equal(a, b)
-    elif op_str == "Power":
-        return ov.power(a, b)
-
-
-def binary_op_ref(op_str, a, b):
-
-    if op_str == "+" or op_str == "Add":
-        return a + b
-    elif op_str == "-" or op_str == "Sub":
-        return a - b
-    elif op_str == "*" or op_str == "Mul":
-        return a * b
-    elif op_str == "/" or op_str == "Div":
-        return a / b
-    elif op_str == "Dot":
-        return np.dot(a, b)
-    elif op_str == "Equal":
-        return np.equal(a, b)
-    elif op_str == "Greater":
-        return np.greater(a, b)
-    elif op_str == "GreaterEq":
-        return np.greater_equal(a, b)
-    elif op_str == "Less":
-        return np.less(a, b)
-    elif op_str == "LessEq":
-        return np.less_equal(a, b)
-    elif op_str == "Maximum":
-        return np.maximum(a, b)
-    elif op_str == "Minimum":
-        return np.minimum(a, b)
-    elif op_str == "NotEqual":
-        return np.not_equal(a, b)
-    elif op_str == "Power":
-        return np.power(a, b)
-
-
-def binary_op_exec(op_str, expected_ov_str=None):
-    if not expected_ov_str:
-        expected_ov_str = op_str
-
+@pytest.mark.parametrize(("ov_op", "expected_ov_str", "expected_type"), [
+    (lambda a, b: a + b, "Add", Type.f32),
+    (ov.add, "Add", Type.f32),
+    (lambda a, b: a - b, "Subtract", Type.f32),
+    (ov.subtract, "Subtract", Type.f32),
+    (lambda a, b: a * b, "Multiply", Type.f32),
+    (ov.multiply, "Multiply", Type.f32),
+    (lambda a, b: a / b, "Divide", Type.f32),
+    (ov.divide, "Divide", Type.f32),
+    (ov.maximum, "Maximum", Type.f32),
+    (ov.minimum, "Minimum", Type.f32),
+    (ov.power, "Power", Type.f32),
+    (ov.equal, "Equal", Type.boolean),
+    (ov.greater, "Greater", Type.boolean),
+    (ov.greater_equal, "GreaterEqual", Type.boolean),
+    (ov.less, "Less", Type.boolean),
+    (ov.less_equal, "LessEqual", Type.boolean),
+    (ov.not_equal, "NotEqual", Type.boolean),
+])
+def test_binary_op(ov_op, expected_ov_str, expected_type):
     element_type = Type.f32
     shape = Shape([2, 2])
     A = Parameter(element_type, shape)
     B = Parameter(element_type, shape)
-    node = binary_op(op_str, A, B)
+    node = ov_op(A, B)
 
     assert node.get_type_name() == expected_ov_str
     assert node.get_output_size() == 1
     assert list(node.get_output_shape(0)) == [2, 2]
-    assert node.get_output_element_type(0) == Type.f32
-
-
-def binary_op_comparison(op_str, expected_ov_str=None):
-    if not expected_ov_str:
-        expected_ov_str = op_str
-
-    element_type = Type.f32
-    shape = Shape([2, 2])
-    A = Parameter(element_type, shape)
-    B = Parameter(element_type, shape)
-    node = binary_op(op_str, A, B)
-
-    assert node.get_type_name() == expected_ov_str
-    assert node.get_output_size() == 1
-    assert list(node.get_output_shape(0)) == [2, 2]
-    assert node.get_output_element_type(0) == Type.boolean
-
-
-def test_add():
-    binary_op_exec("+", "Add")
-
-
-def test_add_op():
-    binary_op_exec("Add")
-
-
-def test_sub():
-    binary_op_exec("-", "Subtract")
-
-
-def test_sub_op():
-    binary_op_exec("Sub", "Subtract")
-
-
-def test_mul():
-    binary_op_exec("*", "Multiply")
-
-
-def test_mul_op():
-    binary_op_exec("Mul", "Multiply")
-
-
-def test_div():
-    binary_op_exec("/", "Divide")
-
-
-def test_div_op():
-    binary_op_exec("Div", "Divide")
-
-
-def test_maximum():
-    binary_op_exec("Maximum")
-
-
-def test_minimum():
-    binary_op_exec("Minimum")
-
-
-def test_power():
-    binary_op_exec("Power")
-
-
-def test_greater():
-    binary_op_comparison("Greater")
-
-
-def test_greater_eq():
-    binary_op_comparison("GreaterEq", "GreaterEqual")
-
-
-def test_less():
-    binary_op_comparison("Less")
-
-
-def test_less_eq():
-    binary_op_comparison("LessEq", "LessEqual")
-
-
-def test_not_equal():
-    binary_op_comparison("NotEqual")
+    assert node.get_output_element_type(0) == expected_type
 
 
 def test_add_with_mul():
@@ -191,230 +57,39 @@ def test_add_with_mul():
     assert node.get_output_element_type(0) == Type.f32
 
 
-def unary_op(op_str, a):
-    if op_str == "Abs":
-        return ov.abs(a)
-    elif op_str == "Acos":
-        return ov.acos(a)
-    elif op_str == "Acosh":
-        return ov.acosh(a)
-    elif op_str == "Asin":
-        return ov.asin(a)
-    elif op_str == "Asinh":
-        return ov.asinh(a)
-    elif op_str == "Atan":
-        return ov.atan(a)
-    elif op_str == "Atanh":
-        return ov.atanh(a)
-    elif op_str == "Ceiling":
-        return ov.ceiling(a)
-    elif op_str == "Cos":
-        return ov.cos(a)
-    elif op_str == "Cosh":
-        return ov.cosh(a)
-    elif op_str == "Floor":
-        return ov.floor(a)
-    elif op_str == "log":
-        return ov.log(a)
-    elif op_str == "exp":
-        return ov.exp(a)
-    elif op_str == "negative":
-        return ov.negative(a)
-    elif op_str == "Sign":
-        return ov.sign(a)
-    elif op_str == "Sin":
-        return ov.sin(a)
-    elif op_str == "Sinh":
-        return ov.sinh(a)
-    elif op_str == "Sqrt":
-        return ov.sqrt(a)
-    elif op_str == "Tan":
-        return ov.tan(a)
-    elif op_str == "Tanh":
-        return ov.tanh(a)
-
-
-def unary_op_ref(op_str, a):
-    if op_str == "Abs":
-        return np.abs(a)
-    elif op_str == "Acos":
-        return np.arccos(a)
-    elif op_str == "Acosh":
-        return np.arccosh(a)
-    elif op_str == "Asin":
-        return np.arcsin(a)
-    elif op_str == "Asinh":
-        return np.arcsinh(a)
-    elif op_str == "Atan":
-        return np.arctan(a)
-    elif op_str == "Atanh":
-        return np.arctanh(a)
-    elif op_str == "Ceiling":
-        return np.ceil(a)
-    elif op_str == "Cos":
-        return np.cos(a)
-    elif op_str == "Cosh":
-        return np.cosh(a)
-    elif op_str == "Floor":
-        return np.floor(a)
-    elif op_str == "log":
-        return np.log(a)
-    elif op_str == "exp":
-        return np.exp(a)
-    elif op_str == "negative":
-        return np.negative(a)
-    elif op_str == "Reverse":
-        return np.fliplr(a)
-    elif op_str == "Sign":
-        return np.sign(a)
-    elif op_str == "Sin":
-        return np.sin(a)
-    elif op_str == "Sinh":
-        return np.sinh(a)
-    elif op_str == "Sqrt":
-        return np.sqrt(a)
-    elif op_str == "Tan":
-        return np.tan(a)
-    elif op_str == "Tanh":
-        return np.tanh(a)
-
-
-def unary_op_exec(op_str, input_list, expected_ov_str=None):
-    """
-    input_list needs to have deep length of 4
-    """
-    if not expected_ov_str:
-        expected_ov_str = op_str
+@pytest.mark.parametrize(("ov_op", "expected_ov_str"), [
+    (ov.abs, "Abs"),
+    (ov.acos, "Acos"),
+    (ov.acosh, "Acosh"),
+    (ov.asin, "Asin"),
+    (ov.asinh, "Asinh"),
+    (ov.atan, "Atan"),
+    (ov.atanh, "Atanh"),
+    (ov.ceiling, "Ceiling"),
+    (ov.cos, "Cos"),
+    (ov.cosh, "Cosh"),
+    (ov.floor, "Floor"),
+    (ov.log, "Log"),
+    (ov.exp, "Exp"),
+    (ov.negative, "Negative"),
+    (ov.sign, "Sign"),
+    (ov.sin, "Sin"),
+    (ov.sinh, "Sinh"),
+    (ov.sqrt, "Sqrt"),
+    (ov.tan, "Tan"),
+    (ov.tanh, "Tanh"),
+])
+def test_unary_op(ov_op, expected_ov_str):
 
     element_type = Type.f32
-    shape = Shape(np.array(input_list).shape)
+    shape = Shape([4])
     A = Parameter(element_type, shape)
-    node = unary_op(op_str, A)
+    node = ov_op(A)
 
     assert node.get_type_name() == expected_ov_str
     assert node.get_output_size() == 1
     assert list(node.get_output_shape(0)) == list(shape)
     assert node.get_output_element_type(0) == Type.f32
-
-
-def test_abs():
-    input_list = [-1, 0, 1, 2]
-    op_str = "Abs"
-    unary_op_exec(op_str, input_list)
-
-
-def test_acos():
-    input_list = [-1, 0, 0.5, 1]
-    op_str = "Acos"
-    unary_op_exec(op_str, input_list)
-
-
-def test_acosh():
-    input_list = [2., 3., 1.5, 1.0]
-    op_str = "Acosh"
-    unary_op_exec(op_str, input_list)
-
-
-def test_asin():
-    input_list = [-1, 0, 0.5, 1]
-    op_str = "Asin"
-    unary_op_exec(op_str, input_list)
-
-
-def test_asinh():
-    input_list = [-1, 0, 0.5, 1]
-    op_str = "Asinh"
-    unary_op_exec(op_str, input_list)
-
-
-def test_atan():
-    input_list = [-1, 0, 0.5, 1]
-    op_str = "Atan"
-    unary_op_exec(op_str, input_list)
-
-
-def test_atanh():
-    input_list = [-1, 0, 0.5, 1]
-    op_str = "Atanh"
-    unary_op_exec(op_str, input_list)
-
-
-def test_ceiling():
-    input_list = [0.5, 0, 0.4, 0.5]
-    op_str = "Ceiling"
-    unary_op_exec(op_str, input_list)
-
-
-def test_cos():
-    input_list = [0, 0.7, 1.7, 3.4]
-    op_str = "Cos"
-    unary_op_exec(op_str, input_list)
-
-
-def test_cosh():
-    input_list = [-1, 0.0, 0.5, 1]
-    op_str = "Cosh"
-    unary_op_exec(op_str, input_list)
-
-
-def test_floor():
-    input_list = [-0.5, 0, 0.4, 0.5]
-    op_str = "Floor"
-    unary_op_exec(op_str, input_list)
-
-
-def test_log():
-    input_list = [1, 2, 3, 4]
-    op_str = "log"
-    unary_op_exec(op_str, input_list, "Log")
-
-
-def test_exp():
-    input_list = [-1, 0, 1, 2]
-    op_str = "exp"
-    unary_op_exec(op_str, input_list, "Exp")
-
-
-def test_negative():
-    input_list = [-1, 0, 1, 2]
-    op_str = "negative"
-    unary_op_exec(op_str, input_list, "Negative")
-
-
-def test_sign():
-    input_list = [-1, 0, 0.5, 1]
-    op_str = "Sign"
-    unary_op_exec(op_str, input_list)
-
-
-def test_sin():
-    input_list = [0, 0.7, 1.7, 3.4]
-    op_str = "Sin"
-    unary_op_exec(op_str, input_list)
-
-
-def test_sinh():
-    input_list = [-1, 0.0, 0.5, 1]
-    op_str = "Sinh"
-    unary_op_exec(op_str, input_list)
-
-
-def test_sqrt():
-    input_list = [0.0, 0.5, 1, 2]
-    op_str = "Sqrt"
-    unary_op_exec(op_str, input_list)
-
-
-def test_tan():
-    input_list = [-np.pi / 4, 0, np.pi / 8, np.pi / 8]
-    op_str = "Tan"
-    unary_op_exec(op_str, input_list)
-
-
-def test_tanh():
-    input_list = [-1, 0, 0.5, 1]
-    op_str = "Tanh"
-    unary_op_exec(op_str, input_list)
 
 
 def test_reshape():
@@ -439,25 +114,12 @@ def test_broadcast():
     assert node.get_output_element_type(0) == element_type
 
 
-def test_constant():
-    element_type = Type.f32
-    node = Constant(element_type, Shape([3, 3]), list(range(9)))
-    assert node.get_type_name() == "Constant"
-    assert node.get_output_size() == 1
-    assert list(node.get_output_shape(0)) == [3, 3]
-    assert node.get_output_element_type(0) == element_type
-
-
-def test_constant_opset_ov_type():
-    node = ov.constant(np.arange(9).reshape(3, 3), Type.f32)
-    assert node.get_type_name() == "Constant"
-    assert node.get_output_size() == 1
-    assert list(node.get_output_shape(0)) == [3, 3]
-    assert node.get_output_element_type(0) == Type.f32
-
-
-def test_constant_opset_numpy_type():
-    node = ov.constant(np.arange(9).reshape(3, 3), np.float32)
+@pytest.mark.parametrize("node", [
+    Constant(Type.f32, Shape([3, 3]), list(range(9))),
+    ov.constant(np.arange(9).reshape(3, 3), Type.f32),
+    ov.constant(np.arange(9).reshape(3, 3), np.float32)
+])
+def test_constant(node):
     assert node.get_type_name() == "Constant"
     assert node.get_output_size() == 1
     assert list(node.get_output_shape(0)) == [3, 3]
