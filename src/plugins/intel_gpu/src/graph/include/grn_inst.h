fb6308c0b09f28dfafdfb7b3e34762ea8a0f37f4
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2022-12-28 09:12:46 +0400

    [GPU] new shape infer for several trivial primitives (#14803)
    * [GPU] new shape infer for several primitives
    
    * [GPU] Make forwatd_input0_shape method tempalte

--- src/plugins/intel_gpu/src/graph/include/grn_inst.h
+++ src/plugins/intel_gpu/src/graph/include/grn_inst.h
@@ -19,10 +19,13 @@ class typed_primitive_inst<grn> : public typed_primitive_inst_base<grn> {
     using parent::parent;
 
 public:
+    template<typename ShapeType>
+    static std::vector<layout> calc_output_layouts(grn_node const& /*node*/, const kernel_impl_params& impl_param) {
+        return forward_input0_shape<ShapeType>(impl_param);
+    }
     static layout calc_output_layout(grn_node const& node, kernel_impl_params const& impl_param);
     static std::string to_string(grn_node const& node);
 
-public:
     typed_primitive_inst(network& network, grn_node const& node);
 };
 
