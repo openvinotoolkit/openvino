bdcb58dcb9069d54f768abd9203907d3286ba9f9
eaidova <ekaterina.aidova@intel.com>
2022-12-30 09:11:45 +0400

    fix code style

--- src/frontends/pytorch/src/op/repeat.cpp
+++ src/frontends/pytorch/src/op/repeat.cpp
@@ -17,7 +17,8 @@ OutputVector translate_repeat(NodeContext& context) {
     auto one = context.mark_node(opset8::Constant::create(element::i64, Shape{}, {1}));
     auto sizes_shape = context.mark_node(std::make_shared<opset8::ShapeOf>(repeats, element::i64));
     auto expand_shape = context.mark_node(std::make_shared<opset8::Broadcast>(one, sizes_shape));
-    auto expanded_input = context.mark_node(std::make_shared<opset8::Broadcast>(x, expand_shape, ov::op::BroadcastType::BIDIRECTIONAL));
+    auto expanded_input =
+        context.mark_node(std::make_shared<opset8::Broadcast>(x, expand_shape, ov::op::BroadcastType::BIDIRECTIONAL));
     return {context.mark_node(std::make_shared<opset8::Tile>(expanded_input, repeats))};
 };
 
