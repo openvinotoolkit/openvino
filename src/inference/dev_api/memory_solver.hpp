6c9e26782367c128b65a2702301bb833287c9e6c
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2023-01-03 18:05:29 +0100

    Revert "Enabled dumping"
    This reverts commit fe72b7421bcc4deb3bdf82952e7d7bb5dd0fcdd3.

--- src/inference/dev_api/memory_solver.hpp
+++ src/inference/dev_api/memory_solver.hpp
@@ -11,8 +11,6 @@
 #include <ie_common.h>
 #include <stdint.h>
 
-#include <fstream>
-#include <iomanip>
 #include <algorithm>
 #include <map>
 #include <vector>
@@ -170,8 +168,7 @@ public:
             _min_required = std::max(_min_required, box.id + box.size);
             _offsets[id] = box.id;  // TODO: move to constructor (use .insert instead of [])
         }
-        dumpOffsets();
-        dumpTimeline();
+
         return _min_required;
     }
 
@@ -224,43 +221,6 @@ private:
 
             _top_depth = std::max(_top_depth, top_depth);
             _depth = std::max(_depth, depth);
-        }
-    }
-
-    void dumpOffsets() {
-        std::ofstream dump_file("memory_boxes_offsets.txt", std::ios::out);
-        uint8_t scale = 64;
-        std::sort(_boxes.begin(), _boxes.end(),
-            [](const Box& box_l, const Box& box_r){ return (box_l.id == box_r.id) ? box_l.start < box_r.start : box_l.id < box_r.id;});
-
-        for (const Box& box : _boxes) {
-            dump_file << std::left << std::setw(5) << box.id
-            << " "
-            << std::left << std::setw(10) << "{" + std::to_string(box.start) + "," + std::to_string(box.finish) + "}"
-            << " "
-            << std::left << std::setw(5) << box.size
-            << " "
-            << std::left << std::string(box.id / scale, ' ')
-            << std::left << std::string(box.size / scale, 'X')
-            << std::endl;
-        }
-    }
-
-    void dumpTimeline() {
-        std::ofstream dump_file("memory_boxes_timeline.txt", std::ios::out);
-        std::sort(_boxes.begin(), _boxes.end(),
-            [](const Box& box_l, const Box& box_r){ return (box_l.id == box_r.id) ? box_l.start < box_r.start : box_l.id < box_r.id;});
-
-        for (const Box& box : _boxes) {
-            dump_file << std::left << std::setw(5) << box.id
-            << " "
-            << std::left << std::setw(10) << "{" + std::to_string(box.start) + "," + std::to_string(box.finish) + "}"
-            << " "
-            << std::left << std::setw(5) << box.size
-            << " "
-            << std::left << std::string(box.start, ' ')
-            << std::left << std::string(box.finish - box.start + 1, 'X')
-            << std::endl;
         }
     }
 };
