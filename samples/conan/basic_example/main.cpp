d8ce76c096d0e2a8f043f556ec19f184b5d30a53
Artur Kulikowski <artur.kulikowski@intel.com>
2023-01-05 13:11:53 +0100

    Fix clang format

--- samples/conan/basic_example/main.cpp
+++ samples/conan/basic_example/main.cpp
@@ -5,16 +5,13 @@
 #include <iostream>
 #include <openvino/openvino.hpp>
 
-int main(int argc, char *argv[])
-{
-    try
-    {
+int main(int argc, char* argv[]) {
+    try {
         // -------- Get OpenVINO runtime version --------
         std::cout << ov::get_openvino_version() << std::endl;
 
         // -------- Parsing and validation of input arguments --------
-        if (argc != 2)
-        {
+        if (argc != 2) {
             std::cout << "Usage : " << argv[0] << " <path_to_model>" << std::endl;
             return EXIT_FAILURE;
         }
@@ -30,19 +27,15 @@ int main(int argc, char *argv[])
 
         // -------- Step 3. Print inputs and outputs --------
         std::cout << "Inputs:" << std::endl;
-        for (const auto &input : model->inputs())
-        {
+        for (const auto& input : model->inputs()) {
             std::cout << "name: " << input.get_any_name() << " type:" << input.get_element_type() << std::endl;
         }
 
         std::cout << "Outputs:" << std::endl;
-        for (const auto &output : model->outputs())
-        {
+        for (const auto& output : model->outputs()) {
             std::cout << "name: " << output.get_any_name() << " type:" << output.get_element_type() << std::endl;
         }
-    }
-    catch (const std::exception &ex)
-    {
+    } catch (const std::exception& ex) {
         std::cerr << ex.what() << std::endl;
         return EXIT_FAILURE;
     }
