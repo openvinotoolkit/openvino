dcd41cc65146484d3d1fcfe4acb9d72ec30bea5f
sbalandi <sofya.balandina@intel.com>
2023-01-06 14:37:59 +0000

    Add worksheets

--- src/tests/ie_test_utils/functional_test_utils/layer_tests_summary/analyze_confomance_fails.py
+++ src/tests/ie_test_utils/functional_test_utils/layer_tests_summary/analyze_confomance_fails.py
@@ -6,7 +6,7 @@ import re
 import shutil
 import zipfile
 import argparse
-import xlsxwriter
+import openpyxl
 
 from utils import utils
 
@@ -16,7 +16,6 @@ JOB_PREFIX_LINUX = 'ie-tests-linux_'
 NEW_LOG_DIR = 'ie_logs'
 
 SW_PLUGINS = {'HETERO': '1', 'AUTO': '2', 'BATCH': '3', 'MULTI': '4'}
-OLD_API_REG_EXP = '(ie_plugin/|ie_executable_network/|ie_infer_request/|conformance_query_model)'
 
 logger = utils.get_logger('AnalyzerConformanceLog')
 
@@ -71,11 +70,6 @@ class AnalyzerConformanceLog:
 
     def collect_tests_result(self, exclude_from_log):
         logger.info(f'Collecting results')
-        analyzed_test = None
-        analyzed_number = None
-        in_run_stage = False
-        error_msg = ''
-        device = ''
         for root, dirs, files in os.walk(self.local_log_dir, topdown=False):
             for file_name in files:
                 file_name_match = re.match(f'apiconformancetests_(\w*)_(dlb|omz)-\d.log', file_name)
@@ -89,19 +83,25 @@ class AnalyzerConformanceLog:
                         device = file_name_match.group(1)
                         self.analyzed_hw_devices.add(device)
 
+                        test_name = None
+                        test_number = None
+                        in_run_stage = False
+                        error_msg = ''
+                        test_info_by_device = None
+
                         for line in lines:
-                            if '[ RUN      ]' in line:
+                            if utils.RUN in line:
                                 in_run_stage = True
                                 error_msg = ''
                                 # if run stage exists, it is because gtest decided to show log as test fails
-                                self.logs_info[analyzed_test][device]['pass'] = False
+                                test_info_by_device['pass'] = False
                                 continue
 
                             # it is result, we got to the end of run stage
-                            if '[  FAILED  ]' in line:
+                            if utils.TEST_STATUS['failed'] in line:
                                 in_run_stage = False
                                 if error_msg:
-                                    self.logs_info[analyzed_test][device]['err_info'] = error_msg
+                                    test_info_by_device['err_info'] = error_msg
                                     error_msg = None
                                 continue
                             
@@ -120,93 +120,123 @@ class AnalyzerConformanceLog:
                             if test_match:
                                 in_run_stage = False
                                 # analyzed failed test without RUN stage, it can be crashed tests
-                                if analyzed_test and device and error_msg and analyzed_test in self.logs_info:
-                                    error_msg = error_msg.replace(self.logs_info[analyzed_test][device]['name'], '')
+                                if error_msg and test_info_by_device:
+                                    error_msg = error_msg.replace(test_info_by_device['name'], '')
                                     error_msg = re.sub('\[\d*\/\d*\]', '', error_msg)
                                     error_msg = re.sub('\(\d* ms\)', '', error_msg)
                                     if exclude_from_log:
                                         error_msg = error_msg.replace(exclude_from_log, '')
-                                    self.logs_info[analyzed_test][device]['err_info'] = error_msg
+                                    test_info_by_device['err_info'] = error_msg
 
                                 error_msg = None
 
                                 # start analyze new test
-                                if analyzed_number != test_match.group(1):
-                                    analyzed_number = test_match.group(1)
-                                    analyzed_test = test_match.group(2)
-                                    analyzed_test = re.sub(device.upper(), '[HWDevice]', analyzed_test)
+                                if test_number != test_match.group(1):
+                                    test_number = test_match.group(1)
+                                    test_name = test_match.group(2)
+                                    test_name = re.sub(device.upper(), '[HWDevice]', test_name)
+                                    test_group = test_name.split('/')[0] if len(test_name.split('/')) > 1 else test_name.split('.')[0]
+                                    # cover case ov_infer_request_1/2/...
+                                    if 'ov_infer_request_' in test_group:
+                                        test_group = 'ov_infer_request'
+                                    # cover case conformance_query_model_[ops]
+                                    if 'conformance_query_model_' in test_group:
+                                        test_group = 'conformance_query_model'
 
-                                    self.logs_info.setdefault(analyzed_test, {})
-                                    self.logs_info[analyzed_test][device] = {'name': test_match.group(2), 'pass': True, 'err_info': ''}
+                                    self.logs_info.setdefault(test_group, {})
+                                    self.logs_info[test_group].setdefault(test_name, {})
+                                    self.logs_info[test_group][test_name][device] = {'name': test_match.group(2), 'pass': True, 'err_info': ''}
+                                    test_info_by_device = self.logs_info[test_group][test_name][device]
 
                     except Exception as e:
                         logger.error(f'Analyzing of {file_name} FAIL: {e}')
 
-
     def create_exel(self, expected_devices, exclude_sw_plugins):
         logger.info(f'Creating exel file with results')
-
-        workbook = xlsxwriter.Workbook(os.path.join(self.local_log_dir, 'apiConformanceFails.xlsx'))
-        worksheet = workbook.add_worksheet()
-
-        worksheet.write('A1', 'Test')
-        worksheet.write('B1', 'Device')
 
         if not expected_devices:
             expected_devices = self.analyzed_hw_devices
 
-        column = 1
-        for dev in expected_devices:
-            worksheet.write(1, column, dev.upper())
-            worksheet.write(2, column, 'Status')
-            column += 1
-            worksheet.write(2, column, 'Error')
-            column += 1
+        workbook = openpyxl.workbook.Workbook()
 
-        cell_format_fail = workbook.add_format({'bg_color': '#FF8B8B', 'font_color': '#9C0006'})
-        cell_format_pass = workbook.add_format({'bg_color': '#C6E0B4', 'font_color': '#006100'})
-        cell_format_not_run = workbook.add_format({'bg_color': '#D9D9D9'})
+        fail_font = openpyxl.styles.Font(color="9C0006")
+        fail_fill = openpyxl.styles.PatternFill(start_color="FF8B8B", end_color="FF8B8B", fill_type = "solid")
 
-        row = 3
-        column = 0
+        pass_font = openpyxl.styles.Font(color="006100")
+        pass_fill = openpyxl.styles.PatternFill(start_color="C6E0B4", end_color="C6E0B4", fill_type = "solid")
 
-        for test_name, devices_info in self.logs_info.items():
-            if exclude_sw_plugins and re.search(exclude_sw_plugins, test_name):
-                continue
-            if  re.search(OLD_API_REG_EXP, test_name) and not args.old_api:
-                continue
+        not_run_font = openpyxl.styles.Font(color="000000")
+        not_run_fill = openpyxl.styles.PatternFill(start_color="D9D9D9", end_color="D9D9D9", fill_type = "solid")
 
-            # if test pass on all devices, it is not need to analyze it here
-            all_pass = all([dev.lower() in devices_info and devices_info[dev.lower()]['pass'] for dev in expected_devices])
-            if all_pass:
-                continue
 
-            worksheet.write(row, column, test_name)
-            column += 1
+        for group_name, group_info in self.logs_info.items():
+            sheet_name = group_name if len(group_name) < 30 else group_name[0:30]
+            worksheet = workbook.create_sheet(sheet_name)
+
+            worksheet['A1'] = 'Test'
+            worksheet['B1'] = 'Device'
+            worksheet['B1'].alignment = openpyxl.styles.Alignment(horizontal='center')
+            worksheet.merge_cells(start_row=1, start_column=2, end_row=1, end_column=(len(expected_devices)*2 + 1))
+
+            column = 2
             for dev in expected_devices:
-                status = 'Not run'
-                error_msg = ''
-                format = cell_format_not_run
-                if dev.lower() in devices_info:
-                    if devices_info[dev.lower()]['pass']:
-                        format = cell_format_pass
-                        status = 'Pass'
-                    else:
-                        status = 'Fail'
-                        error_msg = devices_info[dev.lower()]['err_info']
-                        format = cell_format_fail
-
-                worksheet.write(row, column, status, format)
+                worksheet.cell(2, column, dev.upper())
+                worksheet.cell(2, column).alignment = openpyxl.styles.Alignment(horizontal='center')
+                worksheet.cell(2, column).font = openpyxl.styles.Font(bold=True)
+                worksheet.merge_cells(start_row=2, start_column=column, end_row=2, end_column=(column + 1))
+                worksheet.cell(3, column, 'Status')
                 column += 1
-                worksheet.write(row, column, error_msg)
-
+                worksheet.cell(3, column, 'Error')
                 column += 1
 
-            column = 0
-            row += 1
+            row = 4
+            column = 1
 
+            for test_name, devices_info in group_info.items():
+                if exclude_sw_plugins and re.search(exclude_sw_plugins, test_name):
+                    continue
+                if  re.search(OLD_API_REG_EXP, test_name) and not args.old_api:
+                    continue
 
-        workbook.close()
+                # if test pass on all devices, it is not need to analyze it here
+                all_pass = all([dev.lower() in devices_info and devices_info[dev.lower()]['pass'] for dev in expected_devices])
+                if all_pass:
+                    continue
+
+                worksheet.cell(row, column, test_name)
+                column += 1
+                for dev in expected_devices:
+                    status = 'Not run'
+                    error_msg = ''
+                    font = not_run_font
+                    fill = not_run_fill
+                    if dev.lower() in devices_info:
+                        if devices_info[dev.lower()]['pass']:
+                            font = pass_font
+                            fill = pass_fill
+                            status = 'Pass'
+                        else:
+                            status = 'Fail'
+                            error_msg = devices_info[dev.lower()]['err_info']
+                            font = fail_font
+                            fill = fail_fill
+
+                    worksheet.cell(row, column, status)
+                    worksheet.cell(row, column).fill = fill
+                    worksheet.cell(row, column).font = font
+                    worksheet
+                    column += 1
+                    worksheet.cell(row, column, error_msg)
+
+                    column += 1
+                column = 1
+                row += 1
+
+        for worksheet in workbook.worksheets:
+            if worksheet.max_row <= 3:
+                workbook.remove(worksheet)
+
+        workbook.save(os.path.join(self.local_log_dir, 'apiConformanceFails.xlsx'))
 
 
 if __name__ == '__main__':
@@ -262,7 +292,6 @@ Example2 - exclude all from AUTO/HETERO/MULTI/BATCH: ALL')
     if args.exclude_sw_plugins:
         if 'ALL' in args.exclude_sw_plugins:
             exclude_sw_plugins = '(' + '|'.join(SW_PLUGINS.keys()) + '|\/[1234]$)'
-            print(exclude_sw_plugins)
         else:
             exclude_sw_plugins = '(' + '|'.join(args.exclude_sw_plugins)
             sw_plugins_numbers = ''.join([SW_PLUGINS[plugin.upper()] for plugin in args.exclude_sw_plugins if plugin.upper() in SW_PLUGINS])
