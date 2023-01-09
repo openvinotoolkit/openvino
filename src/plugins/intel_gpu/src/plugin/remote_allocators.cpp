f57024d5892dafb91a97b99e9c273383b072ce95
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2023-01-09 15:57:10 +0400

    [GPU] Fixes after rebase

--- src/plugins/intel_gpu/src/plugin/remote_allocators.cpp
+++ src/plugins/intel_gpu/src/plugin/remote_allocators.cpp
@@ -6,7 +6,6 @@
 #include "intel_gpu/plugin/remote_context.hpp"
 #include "intel_gpu/plugin/remote_allocators.hpp"
 #include "intel_gpu/plugin/remote_blob.hpp"
-#include "intel_gpu/plugin/itt.hpp"
 #include "intel_gpu/plugin/plugin.hpp"
 #include "intel_gpu/runtime/device_query.hpp"
 
