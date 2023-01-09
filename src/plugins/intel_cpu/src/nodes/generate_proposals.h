29b8c9e7afaa3870b489f25abc59ae36a6362c31
Tingqian Li <tingqian.li@intel.com>
2023-01-09 19:28:48 +0800

    [CPU] Add GraphContext & EnforeceBF16 for sub-graphs (#14695)

--- src/plugins/intel_cpu/src/nodes/generate_proposals.h
+++ src/plugins/intel_cpu/src/nodes/generate_proposals.h
@@ -13,8 +13,7 @@ namespace node {
 
 class GenerateProposals : public Node {
 public:
-    GenerateProposals(const std::shared_ptr<ngraph::Node>& op,
-        const dnnl::engine& eng, WeightsSharing::Ptr &cache);
+    GenerateProposals(const std::shared_ptr<ngraph::Node>& op, const GraphContext::CPtr context);
 
     void getSupportedDescriptors() override {};
     void initSupportedPrimitiveDescriptors() override;
