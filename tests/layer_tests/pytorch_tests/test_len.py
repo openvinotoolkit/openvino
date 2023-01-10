e2c428beb6b88a8e1ddc33d9655f510aa681af59
sikorsl1 <leonard.sikorski@intel.com>
2023-01-10 10:44:53 +0000

    Add test case to layer test

--- tests/layer_tests/pytorch_tests/test_len.py
+++ tests/layer_tests/pytorch_tests/test_len.py
@@ -13,7 +13,7 @@ class TestLen(PytorchLayerTest):
 
     def _prepare_input(self):
         input_tensor = self.input_tensor*10
-        return (input_tensor.astype(np.int64), )
+        return (input_tensor.astype(np.int64),)
 
     def create_model(self):
         class aten_len(torch.nn.Module):
@@ -25,7 +25,24 @@ class TestLen(PytorchLayerTest):
 
         return aten_len(), ref_net, "aten::len"
 
+    def create_model_int_list(self):
+        class aten_len(torch.nn.Module):
+
+            def forward(self, input_tensor):
+                int_list = input_tensor.size()
+                return torch.as_tensor(len(int_list), dtype=torch.int)
+
+        ref_net = None
+
+        return aten_len(), ref_net, "aten::len"
+
     @pytest.mark.nightly
     def test_len(self, ie_device, precision, ir_version, input_tensor):
         self.input_tensor = input_tensor
         self._test(*self.create_model(), ie_device, precision, ir_version)
+
+    @pytest.mark.nightly
+    def test_len_int_list(self, ie_device, precision, ir_version, input_tensor):
+        self.input_tensor = input_tensor
+        self._test(*self.create_model_int_list(),
+                   ie_device, precision, ir_version)
