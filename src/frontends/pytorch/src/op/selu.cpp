d6068b9a53e0b5e535d6728009b199ed4fea3f8e
eaidova <ekaterina.aidova@intel.com>
2023-01-10 20:32:53 +0400

    execute in fp64 and convert back to initial precision

--- src/frontends/pytorch/src/op/selu.cpp
+++ src/frontends/pytorch/src/op/selu.cpp
@@ -13,9 +13,11 @@ namespace op {
 
 OutputVector translate_selu(NodeContext& context) {
     auto x = context.get_input(0);
-    auto alpha = context.mark_node(opset8::Constant::create(element::f32, Shape{}, {1.6732632423543772848170429916717}));
-    auto lambda = context.mark_node(opset8::Constant::create(element::f32, Shape{}, {1.0507009873554804934193349852946}));
-    return {context.mark_node(std::make_shared<opset8::Selu>(x, alpha, lambda))};
+    auto alpha = context.mark_node(opset8::Constant::create(element::f64, Shape{}, {1.6732632423543772848170429916717}));
+    auto lambda = context.mark_node(opset8::Constant::create(element::f64, Shape{}, {1.0507009873554804934193349852946}));
+    auto x_f64 = context.mark_node(std::make_shared<opset8::Convert>(x, element::f64));
+    auto res = context.mark_node(std::make_shared<opset8::Selu>(x, alpha, lambda));
+    return {context.mark_node(std::make_shared<opset8::ConvertLike>(res, x)};
 };
 
 }  // namespace op
