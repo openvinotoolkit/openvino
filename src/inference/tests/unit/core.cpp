292811fbaa667de028d099360d1bd4a51e0de9b7
vurusovs <vitaliy.urusovskij@intel.com>
2023-01-11 18:02:00 +0400

    ClangFormat

--- src/inference/tests/unit/core.cpp
+++ src/inference/tests/unit/core.cpp
@@ -5,16 +5,19 @@
 #include "openvino/runtime/core.hpp"
 
 #include <gtest/gtest.h>
+
 #include <fstream>
 
-#include "common_test_utils/test_assertions.hpp"
 #include "common_test_utils/file_utils.hpp"
+#include "common_test_utils/test_assertions.hpp"
 #include "openvino/util/file_util.hpp"
 
 TEST(CoreTests, ThrowOnRegisterPluginTwice) {
     ov::Core core;
     core.register_plugin("test_plugin", "TEST_DEVICE");
-    OV_EXPECT_THROW(core.register_plugin("test_plugin", "TEST_DEVICE"), ov::Exception, ::testing::HasSubstr("Device with \"TEST_DEVICE\"  is already registered in the OpenVINO Runtime"));
+    OV_EXPECT_THROW(core.register_plugin("test_plugin", "TEST_DEVICE"),
+                    ov::Exception,
+                    ::testing::HasSubstr("Device with \"TEST_DEVICE\"  is already registered in the OpenVINO Runtime"));
 }
 
 TEST(CoreTests, ThrowOnRegisterPluginsTwice) {
@@ -26,10 +29,11 @@ TEST(CoreTests, ThrowOnRegisterPluginsTwice) {
         file << "<ie><plugins><plugin location=\"libtest_plugin.so\" name=\"TEST_DEVICE\"></plugin></plugins></ie>";
         file.flush();
         file.close();
-
         return pluginsXML;
     };
 
     core.register_plugins(getPluginXml());
-    OV_EXPECT_THROW(core.register_plugins(getPluginXml()), ov::Exception, ::testing::HasSubstr("Device with \"TEST_DEVICE\"  is already registered in the OpenVINO Runtime"));
+    OV_EXPECT_THROW(core.register_plugins(getPluginXml()),
+                    ov::Exception,
+                    ::testing::HasSubstr("Device with \"TEST_DEVICE\"  is already registered in the OpenVINO Runtime"));
 }
