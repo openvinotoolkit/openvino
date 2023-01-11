10ed57c07a438f98de561d1c1af1f3e7c569ff54
eaidova <ekaterina.aidova@intel.com>
2023-01-11 17:11:41 +0400

    convert constants to input type

--- src/frontends/pytorch/src/op/selu.cpp
+++ src/frontends/pytorch/src/op/selu.cpp
@@ -13,11 +13,13 @@ namespace op {
 
 OutputVector translate_selu(NodeContext& context) {
     auto x = context.get_input(0);
-    auto alpha = context.mark_node(opset8::Constant::create(element::f64, Shape{}, {1.6732632423543772848170429916717}));
-    auto lambda = context.mark_node(opset8::Constant::create(element::f64, Shape{}, {1.0507009873554804934193349852946}));
-    auto x_f64 = context.mark_node(std::make_shared<opset8::Convert>(x, element::f64));
-    auto res = context.mark_node(std::make_shared<opset8::Selu>(x, alpha, lambda));
-    return {context.mark_node(std::make_shared<opset8::ConvertLike>(res, x)};
+    auto alpha =
+        context.mark_node(opset8::Constant::create(element::f64, Shape{}, {1.6732632423543772848170429916717}));
+    auto lambda =
+        context.mark_node(opset8::Constant::create(element::f64, Shape{}, {1.0507009873554804934193349852946}));
+    alpha = context.mark_node(std::make_shared<opset8::ConvertLike>(alpha, x));
+    lambda = context.mark_node(std::make_shared<opset8::ConvertLike>(lambda, x));
+    return {context.mark_node(std::make_shared<opset8::Selu>(x, alpha, lambda))};
 };
 
 }  // namespace op
