7bdf90b96bfd1140503f664acf28a0c4ebf7a588
Alexandra Sidorova <alexandra.sidorova@intel.com>
2023-01-11 15:27:13 +0400

    Reverted shape for ConvEltwise test

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/snippets/conv_eltwise.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/snippets/conv_eltwise.cpp
@@ -10,26 +10,26 @@ namespace test {
 namespace snippets {
 namespace {
 
-    ov::Shape convInputShape {1, 2, 16, 16};
-    INSTANTIATE_TEST_SUITE_P(smoke_Snippets_ConvAdd, ConvEltwise,
-            ::testing::Combine(
-            ::testing::Values(convInputShape),
-            ::testing::Values(convInputShape),
-            ::testing::Values(std::shared_ptr<ov::Node> (std::make_shared<ov::op::v1::Add>())), // non-tokenizable
-            ::testing::Values(6), // num nodes = 6: Convert + Convolution + 4 Reorders on Convs in&outs
-            ::testing::Values(0), // num subgraphs = 0: No subgraph since all ops eltwises fused into Convolution
-            ::testing::Values(CommonTestUtils::DEVICE_CPU)),
-            ConvEltwise::getTestCaseName);
+ov::Shape convInputShape {1, 10, 16, 16};
+INSTANTIATE_TEST_SUITE_P(smoke_Snippets_ConvAdd, ConvEltwise,
+        ::testing::Combine(
+        ::testing::Values(convInputShape),
+        ::testing::Values(convInputShape),
+        ::testing::Values(std::shared_ptr<ov::Node> (std::make_shared<ov::op::v1::Add>())), // non-tokenizable
+        ::testing::Values(6), // num nodes = 6: Convert + Convolution + 4 Reorders on Convs in&outs
+        ::testing::Values(0), // num subgraphs = 0: No subgraph since all ops eltwises fused into Convolution
+        ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+        ConvEltwise::getTestCaseName);
 
-    INSTANTIATE_TEST_SUITE_P(smoke_Snippets_ConvMul, ConvEltwise,
-            ::testing::Combine(
-            ::testing::Values(convInputShape),
-            ::testing::Values(convInputShape),
-            ::testing::Values(std::shared_ptr<ov::Node> (std::make_shared<ov::op::v1::Multiply>())), // fully-tokenizable
-            ::testing::Values(7), //num nodes = 7: Convert + Convolution + Subgraph + Reorders
-            ::testing::Values(1), // num subgraphs = 0: Mul (2 inputs) can't be fused into Conv => Subgraph is created
-            ::testing::Values(CommonTestUtils::DEVICE_CPU)),
-            ConvEltwise::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_Snippets_ConvMul, ConvEltwise,
+        ::testing::Combine(
+        ::testing::Values(convInputShape),
+        ::testing::Values(convInputShape),
+        ::testing::Values(std::shared_ptr<ov::Node> (std::make_shared<ov::op::v1::Multiply>())), // fully-tokenizable
+        ::testing::Values(7), //num nodes = 7: Convert + Convolution + Subgraph + Reorders
+        ::testing::Values(1), // num subgraphs = 1: Mul (2 inputs) can't be fused into Conv => Subgraph is created
+        ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+        ConvEltwise::getTestCaseName);
 }  // namespace
 } // namespace snippets
 } // namespace test
