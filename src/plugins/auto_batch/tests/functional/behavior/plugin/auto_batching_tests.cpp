086a0d8ef7f0f340a4e301c3ed8c4580413d3389
River,Li <river.li@intel.com>
2023-01-12 14:45:09 +0800

    Fix CI build issues

--- src/plugins/auto_batch/tests/functional/behavior/plugin/auto_batching_tests.cpp
+++ src/plugins/auto_batch/tests/functional/behavior/plugin/auto_batching_tests.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2018-2022 Intel Corporation
+// Copyright (C) 2018-2023 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
 #include "auto_batching_tests.hpp"
@@ -14,73 +14,70 @@ const std::vector<bool> get_vs_set{true, false};
 const std::vector<size_t> num_streams{1, 2};
 const std::vector<size_t> num_requests{1, 3, 8, 9, 16, 64};
 const std::vector<size_t> num_batch{1, 4, 8, 16, 32, 64, 128, 256};
-INSTANTIATE_TEST_SUITE_P(smoke_AutoBatching_CPU, AutoBatching_Test,
-        ::testing::Combine(
-                ::testing::Values(CommonTestUtils::DEVICE_CPU),
-                ::testing::ValuesIn(get_vs_set),
-                ::testing::ValuesIn(num_streams),
-                ::testing::ValuesIn(num_requests),
-                ::testing::ValuesIn(num_batch)),
+INSTANTIATE_TEST_SUITE_P(smoke_AutoBatching_CPU,
+                         AutoBatching_Test,
+                         ::testing::Combine(::testing::Values(CommonTestUtils::DEVICE_CPU),
+                                            ::testing::ValuesIn(get_vs_set),
+                                            ::testing::ValuesIn(num_streams),
+                                            ::testing::ValuesIn(num_requests),
+                                            ::testing::ValuesIn(num_batch)),
                          AutoBatching_Test::getTestCaseName);
 
-INSTANTIATE_TEST_SUITE_P(smoke_AutoBatching_CPU, AutoBatching_Test_DetectionOutput,
-                         ::testing::Combine(
-                                 ::testing::Values(CommonTestUtils::DEVICE_CPU),
-                                 ::testing::ValuesIn(get_vs_set),
-                                 ::testing::ValuesIn(num_streams),
-                                 ::testing::ValuesIn(num_requests),
-                                 ::testing::ValuesIn(num_batch)),
+INSTANTIATE_TEST_SUITE_P(smoke_AutoBatching_CPU,
+                         AutoBatching_Test_DetectionOutput,
+                         ::testing::Combine(::testing::Values(CommonTestUtils::DEVICE_CPU),
+                                            ::testing::ValuesIn(get_vs_set),
+                                            ::testing::ValuesIn(num_streams),
+                                            ::testing::ValuesIn(num_requests),
+                                            ::testing::ValuesIn(num_batch)),
                          AutoBatching_Test_DetectionOutput::getTestCaseName);
 
 #ifdef HAVE_GPU_PLUGIN
-const std::vector<size_t> gpu_num_streams{ 2 };
-const std::vector<bool>   gpu_get_vs_set{ true, false };
-const std::vector<size_t> gpu_num_requests{ 1, 8, 16, 64 };
-const std::vector<size_t> gpu_num_batch{ 1, 8, 32, 256 };
+const std::vector<size_t> gpu_num_streams{2};
+const std::vector<bool> gpu_get_vs_set{true, false};
+const std::vector<size_t> gpu_num_requests{1, 8, 16, 64};
+const std::vector<size_t> gpu_num_batch{1, 8, 32, 256};
 
-INSTANTIATE_TEST_SUITE_P(smoke_AutoBatching_GPU, AutoBatching_Test,
-                         ::testing::Combine(
-                                 ::testing::Values(CommonTestUtils::DEVICE_GPU),
-                                 ::testing::ValuesIn(gpu_get_vs_set),
-                                 ::testing::ValuesIn(gpu_num_streams),
-                                 ::testing::ValuesIn(gpu_num_requests),
-                                 ::testing::ValuesIn(gpu_num_batch)),
+INSTANTIATE_TEST_SUITE_P(smoke_AutoBatching_GPU,
+                         AutoBatching_Test,
+                         ::testing::Combine(::testing::Values(CommonTestUtils::DEVICE_GPU),
+                                            ::testing::ValuesIn(gpu_get_vs_set),
+                                            ::testing::ValuesIn(gpu_num_streams),
+                                            ::testing::ValuesIn(gpu_num_requests),
+                                            ::testing::ValuesIn(gpu_num_batch)),
                          AutoBatching_Test::getTestCaseName);
 
-INSTANTIATE_TEST_SUITE_P(smoke_AutoBatching_GPU, AutoBatching_Test_DetectionOutput,
-                         ::testing::Combine(
-                                 ::testing::Values(CommonTestUtils::DEVICE_GPU),
-                                 ::testing::ValuesIn(gpu_get_vs_set),
-                                 ::testing::ValuesIn(gpu_num_streams),
-                                 ::testing::ValuesIn(gpu_num_requests),
-                                 ::testing::ValuesIn(gpu_num_batch)),
+INSTANTIATE_TEST_SUITE_P(smoke_AutoBatching_GPU,
+                         AutoBatching_Test_DetectionOutput,
+                         ::testing::Combine(::testing::Values(CommonTestUtils::DEVICE_GPU),
+                                            ::testing::ValuesIn(gpu_get_vs_set),
+                                            ::testing::ValuesIn(gpu_num_streams),
+                                            ::testing::ValuesIn(gpu_num_requests),
+                                            ::testing::ValuesIn(gpu_num_batch)),
                          AutoBatching_Test_DetectionOutput::getTestCaseName);
 
-INSTANTIATE_TEST_SUITE_P(
-        smoke_AutoBatching_GPU,
-        DefaultConfigurationTest,
-        ::testing::Combine(
-                ::testing::Values(std::string(CommonTestUtils::DEVICE_BATCH) + ":" + CommonTestUtils::DEVICE_GPU),
-                ::testing::Values(DefaultParameter{CONFIG_KEY(AUTO_BATCH_TIMEOUT),
-                                                   InferenceEngine::Parameter{"1000"}})),
-        DefaultConfigurationTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_AutoBatching_GPU,
+                         DefaultConfigurationTest,
+                         ::testing::Combine(::testing::Values(std::string(CommonTestUtils::DEVICE_BATCH) + ":" +
+                                                              CommonTestUtils::DEVICE_GPU),
+                                            ::testing::Values(DefaultParameter{CONFIG_KEY(AUTO_BATCH_TIMEOUT),
+                                                                               InferenceEngine::Parameter{"1000"}})),
+                         DefaultConfigurationTest::getTestCaseName);
 
-INSTANTIATE_TEST_SUITE_P(
-        smoke_AutoBatching_GPU_2_0_string,
-        DefaultConfigurationTest,
-        ::testing::Combine(
-                ::testing::Values(std::string(CommonTestUtils::DEVICE_BATCH) + ":" + CommonTestUtils::DEVICE_GPU),
-                ::testing::Values(DefaultParameter{ov::auto_batch_timeout.name(),
-                                                   InferenceEngine::Parameter{"1000"}})),
-        DefaultConfigurationTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_AutoBatching_GPU_2_0_string,
+                         DefaultConfigurationTest,
+                         ::testing::Combine(::testing::Values(std::string(CommonTestUtils::DEVICE_BATCH) + ":" +
+                                                              CommonTestUtils::DEVICE_GPU),
+                                            ::testing::Values(DefaultParameter{ov::auto_batch_timeout.name(),
+                                                                               InferenceEngine::Parameter{"1000"}})),
+                         DefaultConfigurationTest::getTestCaseName);
 
-INSTANTIATE_TEST_SUITE_P(
-        smoke_AutoBatching_GPU_2_0_uint,
-        DefaultConfigurationTest,
-        ::testing::Combine(
-                ::testing::Values(std::string(CommonTestUtils::DEVICE_BATCH) + ":" + CommonTestUtils::DEVICE_GPU),
-                ::testing::Values(DefaultParameter{ov::auto_batch_timeout.name(),
-                                                   InferenceEngine::Parameter{1000}})),
-        DefaultConfigurationTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_AutoBatching_GPU_2_0_uint,
+                         DefaultConfigurationTest,
+                         ::testing::Combine(::testing::Values(std::string(CommonTestUtils::DEVICE_BATCH) + ":" +
+                                                              CommonTestUtils::DEVICE_GPU),
+                                            ::testing::Values(DefaultParameter{ov::auto_batch_timeout.name(),
+                                                                               InferenceEngine::Parameter{1000}})),
+                         DefaultConfigurationTest::getTestCaseName);
 #endif
 }  // namespace
