10fc30dcf794e5acb5a22d0cc7abab789bc6ed66
Ahn, Paul Y <paul.y.ahn@intel.com>
2023-01-16 11:40:00 +0900

    [GPU] update compilation queue (#14797)

--- src/plugins/intel_gpu/src/graph/compilation_context.cpp
+++ src/plugins/intel_gpu/src/graph/compilation_context.cpp
@@ -7,20 +7,55 @@
 #include "kernel_selector/kernel_base.h"
 
 namespace cldnn {
+class CompilationTaskQueue {
+    using CompilationTaskData = std::pair<size_t, ICompilationContext::Task>;
+
+public:
+    void push_task(size_t task_key, ICompilationContext::Task&& task) {
+        std::lock_guard<std::mutex> lock(_mutex);
+        if (_queue_keymap.find(task_key) == _queue_keymap.end()) {
+            auto insert_it = _queue.insert(_queue.end(), {task_key, task});
+            _queue_keymap.insert({task_key, insert_it});
+        }
+    }
+
+    bool pop_front_task(size_t& task_key, ICompilationContext::Task& task) {
+        std::lock_guard<std::mutex> lock(_mutex);
+        if (!_queue.empty()) {
+            auto front = _queue.front();
+            task = front.second;
+            task_key = front.first;
+            _queue.pop_front();
+            return true;
+        }
+        return false;
+    }
+
+    void erase_task_key(size_t removed_key) {
+        std::lock_guard<std::mutex> lock(_mutex);
+        if (_queue_keymap.find(removed_key) != _queue_keymap.end()) {
+            _queue_keymap.erase(removed_key);
+        }
+    }
+
+private:
+    std::deque<CompilationTaskData> _queue;
+    std::unordered_map<size_t, std::deque<CompilationTaskData>::iterator> _queue_keymap;
+    std::mutex _mutex;
+};
 
 class CompilationContext : public ICompilationContext {
 public:
-    using data_type = std::pair<size_t, ICompilationContext::Task>;
-
     CompilationContext(cldnn::engine& engine, const ExecutionConfig& config, size_t program_id) {
         _kernels_cache = cldnn::make_unique<kernels_cache>(engine, config, program_id, nullptr, kernel_selector::KernelBase::get_db().get_batch_header_str());
         _worker = std::thread([this](){
             while (!_stop_compilation) {
                 CompilationContext::Task task;
-                bool success = get_front_task(task);
+                size_t task_key;
+                bool success = _queue.pop_front_task(task_key, task);
                 if (success) {
                     task(*_kernels_cache);
-                    erase_front_task();
+                    _queue.erase_task_key(task_key);
                 } else {
                     std::chrono::milliseconds ms{1};
                     std::this_thread::sleep_for(ms);
@@ -30,12 +65,7 @@ public:
     }
 
     void push_task(size_t key, ICompilationContext::Task&& task) override {
-        std::lock_guard<std::mutex> lock(_mutex);
-        auto iter = _compile_queue_keymap.find(key);
-        if (iter == _compile_queue_keymap.end()) {
-            auto insert_it = _compile_queue.insert(_compile_queue.end(), {key, task});
-            _compile_queue_keymap.insert({key, insert_it});
-        }
+        _queue.push_task(key, std::move(task));
     }
 
     void cancel() noexcept override {
@@ -47,31 +77,11 @@ public:
     ~CompilationContext() noexcept { cancel(); }
 
 private:
-    bool get_front_task(ICompilationContext::Task& task) {
-        std::lock_guard<std::mutex> lock(_mutex);
-        if (!_compile_queue.empty()) {
-            auto front = _compile_queue.front();
-            task = front.second;
-            return true;
-        }
-        return false;
-    }
-
-    void erase_front_task() {
-        std::lock_guard<std::mutex> lock(_mutex);
-        auto front = _compile_queue.front();
-        _compile_queue_keymap.erase(front.first);
-        _compile_queue.pop_front();
-    }
-
-private:
     std::unique_ptr<kernels_cache> _kernels_cache;
     std::thread _worker;
     std::atomic_bool _stop_compilation{false};
 
-    std::deque<data_type> _compile_queue;
-    std::unordered_map<size_t, std::deque<data_type>::iterator> _compile_queue_keymap;
-    std::mutex _mutex;
+    CompilationTaskQueue _queue;
 };
 
 std::unique_ptr<ICompilationContext> ICompilationContext::create(cldnn::engine& engine, const ExecutionConfig& config, size_t program_id) {
