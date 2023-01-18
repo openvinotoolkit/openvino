c5683bc608ea6cb71edf855ca0edc6a60f197eb3
Sergey Shlyapnikov <sergey.shlyapnikov@intel.com>
2023-01-18 23:16:37 +0400

    [GPU] Perform memory transfer from usm_host to usm_device only for dGPU

--- src/plugins/intel_gpu/src/graph/network.cpp
+++ src/plugins/intel_gpu/src/graph/network.cpp
@@ -1197,6 +1197,9 @@ void network::transfer_memory_to_device(std::shared_ptr<primitive_inst> instance
     if (!get_engine().supports_allocation(allocation_type::usm_device))
         return;
 
+    if (get_engine().get_device_info().dev_type != device_type::discrete_gpu)
+        return;
+
     if (alloc_type == allocation_type::usm_host || alloc_type == allocation_type::usm_shared) {
         // Allocate and transfer memory
         auto device_mem = inst_mem.get_engine()->allocate_memory(inst_mem.get_layout(), allocation_type::usm_device, false);
