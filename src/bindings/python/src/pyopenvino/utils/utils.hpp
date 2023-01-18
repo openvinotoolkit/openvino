a111b66d2793b11752db144e7e8f1636cb7ab7ce
Maxim Vafin <maxim.vafin@intel.com>
2023-01-18 10:03:05 +0100

    Apply suggestions from code review
    Co-authored-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- src/bindings/python/src/pyopenvino/utils/utils.hpp
+++ src/bindings/python/src/pyopenvino/utils/utils.hpp
@@ -9,17 +9,16 @@
 #include "openvino/core/any.hpp"
 #include "openvino/core/type/element_type.hpp"
 #include "openvino/runtime/properties.hpp"
-#include "openvino/frontend/decoder.hpp"
 
 namespace py = pybind11;
 
 namespace Common {
 namespace utils {
-py::object from_ov_any(const ov::Any& any);
+    py::object from_ov_any(const ov::Any& any);
 
-std::map<std::string, ov::Any> properties_to_any_map(const std::map<std::string, py::object>& properties);
+    std::map<std::string, ov::Any> properties_to_any_map(const std::map<std::string, py::object>& properties);
 
-std::string convert_path_to_string(const py::object& path);
+    std::string convert_path_to_string(const py::object& path);
 
     void deprecation_warning(const std::string& function_name, const std::string& version = std::string(), const std::string& message = std::string());
 
