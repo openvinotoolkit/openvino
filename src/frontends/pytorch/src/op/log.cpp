4bcf972983a92a7513577aebe0ba5bcc5ad58f78
eaidova <ekaterina.aidova@intel.com>
2023-01-18 21:09:50 +0400

    apply review comments

--- src/frontends/pytorch/src/op/log.cpp
+++ src/frontends/pytorch/src/op/log.cpp
@@ -4,7 +4,6 @@
 
 #include "openvino/frontend/pytorch/node_context.hpp"
 #include "openvino/opsets/opset10.hpp"
-#include "utils.hpp"
 
 namespace ov {
 namespace frontend {
@@ -12,22 +11,18 @@ namespace pytorch {
 namespace op {
 
 OutputVector translate_log(NodeContext& context) {
+    // torch.log returns a tensor with the natural logarithm of the elements of input.
     auto x = context.get_input(0);
-    if (x.get_element_type().is_integral()) {
-        x = context.mark_node(std::make_shared<opset10::Convert>(x, element::f32));
-    }
+    x = context.mark_node(std::make_shared<opset10::Convert>(x, element::f32));
     auto log = context.mark_node(std::make_shared<opset10::Log>(x));
     return {log};
 };
 
 OutputVector translate_log2(NodeContext& context) {
+    // torch.log2 returns a tensor with the logarithm to the base 2 of the elements of input.
     auto x = context.get_input(0);
     auto two = context.mark_node(opset10::Constant::create(element::f32, Shape{}, {2}));
-    if (x.get_element_type().is_integral()) {
-        x = context.mark_node(std::make_shared<opset10::Convert>(x, element::f32));
-    } else {
-        two = context.mark_node(std::make_shared<opset10::ConvertLike>(two, x));
-    }
+    x = context.mark_node(std::make_shared<opset10::Convert>(x, element::f32));
     auto log2 = context.mark_node(std::make_shared<opset10::Log>(two));
     auto log = context.mark_node(std::make_shared<opset10::Log>(x));
     auto res = context.mark_node(std::make_shared<opset10::Divide>(log, log2));
