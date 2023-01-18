81b606290d38f4ba448046d88844fb623475014f
Vladislav Golubev <vladislav.golubev@intel.com>
2023-01-18 13:10:28 +0100

    Layer limitations replaced with common checks

--- src/plugins/intel_cpu/src/ngraph_transformations/propagate_optimal_bs.cpp
+++ src/plugins/intel_cpu/src/ngraph_transformations/propagate_optimal_bs.cpp
@@ -18,16 +18,7 @@ ov::intel_cpu::PropagateOptimalBS::PropagateOptimalBS() {
 
     ov::matcher_pass_callback callback = [=](ov::pass::pattern::Matcher& m) {
         const auto node = m.get_match_root();
-        if (has_optimal_bs(node) || is_type<ov::opset1::Result>(node))
-            return false;
-
-        // TODO: do we really need it?
-        if (is_type<ov::opset1::Reshape>(node) || is_type<ov::opset1::Concat>(node) ||
-            is_type<ov::opset1::Split>(node) || is_type<ov::opset1::VariadicSplit>(node))
-            return false;
-
-        const auto& pshape = m.get_match_value().get_partial_shape();
-        if (pshape.size() == 0)
+        if (has_optimal_bs(node) || is_type<ov::opset1::Result>(node) || node->get_input_size() == 0)
             return false;
 
         auto set_parent_opt_bs = [&node](const std::shared_ptr<ov::Node>& parent) {
@@ -38,10 +29,15 @@ ov::intel_cpu::PropagateOptimalBS::PropagateOptimalBS() {
             return true;
         };
 
-        const auto node_batch = pshape[0].get_length();
+        const size_t batch_dim = 0;
+        const auto& pshape = m.get_match_value().get_partial_shape();
+        if (pshape.size() <= batch_dim)
+            return false;
+
+        const auto node_batch = pshape[batch_dim].get_length();
         for (const auto& input : node->input_values()) {
             const auto& input_ps = input.get_partial_shape();
-            if (input_ps.is_dynamic() || input_ps.size() == 0 || input_ps[0].get_length() != node_batch)
+            if (input_ps.is_dynamic() || input_ps.size() <= batch_dim || input_ps[batch_dim].get_length() != node_batch)
                 continue;
             if (set_parent_opt_bs(input.get_node_shared_ptr()))
                 break;
