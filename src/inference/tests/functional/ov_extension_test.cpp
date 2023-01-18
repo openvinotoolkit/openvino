52535c864049b50fa3152b35f05d89ef37220522
Artur Kulikowski <artur.kulikowski@intel.com>
2023-01-18 08:19:37 +0100

    Remove test

--- src/inference/tests/functional/ov_extension_test.cpp
+++ src/inference/tests/functional/ov_extension_test.cpp
@@ -190,11 +190,6 @@ std::string getIncorrectExtensionPath() {
                                               std::string("incorrect") + IE_BUILD_POSTFIX);
 }
 
-std::string getOVExtensionRelativePath() {
-    return ov::util::make_plugin_library_name(std::string("."),
-                                              std::string("openvino_template_extension") + IE_BUILD_POSTFIX);
-}
-
 }  // namespace
 
 class CustomOldIdentity : public ngraph::op::Op {
@@ -358,9 +353,4 @@ TEST_F(OVExtensionTests, load_old_extension) {
 TEST_F(OVExtensionTests, load_incorrect_extension) {
     EXPECT_THROW(core.add_extension(getIncorrectExtensionPath()), ov::Exception);
 }
-
-TEST_F(OVExtensionTests, load_relative_path) {
-    EXPECT_NO_THROW(core.add_extension(getOVExtensionRelativePath()));
-}
-
 #endif  // defined(ENABLE_OV_IR_FRONTEND)
