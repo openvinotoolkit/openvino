4fce85436348870fb35bdc30a576f4f3f117c96f
eaidova <ekaterina.aidova@intel.com>
2023-01-19 12:30:39 +0400

    [WIP]: poc: integrate pt fe to mo.convert_model

--- tools/mo/openvino/tools/mo/convert_impl.py
+++ tools/mo/openvino/tools/mo/convert_impl.py
@@ -668,12 +668,8 @@ def to_torch_tensor(tensor):
         raise Error("Unexpected type of example_input. Supported types torch.Tensor, np.array or ov.Tensor. "
                     "Got {}".format(type(tensor)))
 
-
-def convert_pytorch_to_onnx(model, input_shape, opset_version, example_inputs, output_dir):
-    import io
+def prepare_torch_inputs(example_inputs, input_shape):
     import torch
-
-    input_names = None
     if example_inputs is not None:
         inputs = example_inputs
         if isinstance(inputs, list):
@@ -694,12 +690,20 @@ def convert_pytorch_to_onnx(model, input_shape, opset_version, example_inputs, o
             inputs = to_torch_tensor(inputs)
     elif input_shape is not None:
         inputs = []
-        for shape_idx, shape in enumerate(input_shape):
+        for _, shape in enumerate(input_shape):
             static_shape = get_static_shape(shape, dynamic_value=1)
             inputs.append(torch.zeros(static_shape))
         inputs = tuple(inputs)
     else:
         raise Error("Please provide input_shape or example_input for converting PyTorch model.")
+    return inputs
+
+def convert_pytorch_to_onnx(model, input_shape, opset_version, example_inputs, output_dir):
+    import io
+    import torch
+
+    input_names = None
+    inputs = prepare_torch_inputs(example_inputs, input_shape)
 
     dynamic_dims_dict = {}
     if input_shape is not None and input_names is None:
@@ -726,6 +730,51 @@ def convert_pytorch_to_onnx(model, input_shape, opset_version, example_inputs, o
                       model_onnx,
                       **additional_params)
     return model_onnx
+
+
+def convert_pytorch_pytorch_frontend(model, input_shape, example_inputs):
+    import torch
+    try:
+        from openvino.frontend import FrontEndManager
+        from openvino.frontend.pytorch.decoder import TorchScriptPythonDecoder, pt_to_ov_type_map
+        fe_manager = FrontEndManager()
+        fe = fe_manager.load_by_framework('pytorch')
+    except Exception as e:
+        print("PyTorch frontend loading failed")
+        raise e
+    inputs = prepare_torch_inputs(example_inputs, input_shape)
+    if isinstance(inputs, dict):
+        example_inputs_list = list(inputs.values())
+    else:
+        example_inputs_list = inputs
+    model.eval()
+    if isinstance(model, torch.nn.Module):
+        try:
+            scripted = torch.jit.script(model)
+        except:
+            try:
+                scripted = torch.jit.trace(model, example_inputs_list)
+            except Exception as e:
+                print('Both traicing and scripting failed')
+                raise e
+    else:
+        scripted = model
+    f_model = torch.jit.freeze(scripted)
+    decoder = TorchScriptPythonDecoder(f_model)
+    im = fe.load(decoder)
+    om = fe.convert(im)
+    provide_shapes = input_shape is not None
+    if isinstance(example_inputs_list, torch.Tensor):
+        example_inputs_list = [example_inputs_list]
+    for idx, input_data in enumerate(example_inputs_list):
+        pt_dtype = input_data.dtype if isinstance(input_data, torch.Tensor) else type(input_data)
+        dtype = pt_to_ov_type_map.get(str(pt_dtype))
+
+        om.inputs[idx].get_node().set_element_type(dtype)
+        shape = [-1] * len(input_data.shape) if not provide_shapes else input_shape[idx]
+        om.inputs[idx].get_node().set_partial_shape(PartialShape(shape))
+    om.validate_nodes_and_infer_types()
+    return om
 
 
 def parse_input_shapes(argv):
@@ -938,15 +987,35 @@ def _convert(cli_parser: argparse.ArgumentParser, framework, args):
         inp_model_is_object = input_model_is_object(args)
         if inp_model_is_object:
             model_framework = check_model_object(args)
-            if model_framework == "pytorch" and not os.environ.get('USE_PYTORCH_FRONTEND'):
-
-                opset_version = None
-                if 'onnx_opset_version' in args and args['onnx_opset_version'] is not None:
-                    opset_version = args['onnx_opset_version']
-
+            if model_framework == "pytorch":
+                use_pt_fe = os.environ.get('USE_PYTORCH_FRONTEND') or args["use_new_frontend"]
                 example_inputs = None
                 if 'example_input' in args and args['example_input'] is not None:
                     example_inputs = args['example_input']
+                if not use_pt_fe:
+                    opset_version = None
+                    if 'onnx_opset_version' in args and args['onnx_opset_version'] is not None:
+                        opset_version = args['onnx_opset_version']
+
+                    out_dir = args['output_dir'] if 'output_dir' in args else None
+
+                    model_onnx = convert_pytorch_to_onnx(args['input_model'],
+                                                        parse_input_shapes(args),
+                                                        opset_version,
+                                                        example_inputs,
+                                                        out_dir)
+
+                    args['input_model'] = model_onnx
+                    if os.environ.get('SAVE_TO_BYTES_IO_ONNX_MODEL'):
+                        args['use_legacy_frontend'] = True
+                    args['example_input'] = None
+                    args['onnx_opset_version'] = None
+
+                    try:
+                        ov_model = _convert(**args)
+                    except Exception as e:
+                        remove_tmp_onnx_model(out_dir)
+                        raise e
 
                 out_dir = args['output_dir'] if 'output_dir' in args else None
 
@@ -969,6 +1038,9 @@ def _convert(cli_parser: argparse.ArgumentParser, framework, args):
                     raise e
 
                 remove_tmp_onnx_model(out_dir)
+                return ov_model, argv
+            else:
+                ov_model = convert_pytorch_pytorch_frontend(args['input_model'], parse_input_shapes(args), example_inputs)
                 return ov_model, argv
 
         # Initialize logger with 'ERROR' as default level to be able to form nice messages
