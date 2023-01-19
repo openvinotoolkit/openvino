9c2b18fe1c33d6e74f24bea35c833f21fc1ba79f
eaidova <ekaterina.aidova@intel.com>
2023-01-19 13:20:56 +0400

    add transformation for prim::min and TupleConstruct + ListConstruct

--- src/frontends/pytorch/src/frontend.cpp
+++ src/frontends/pytorch/src/frontend.cpp
@@ -15,6 +15,7 @@
 #include "transforms/aten_cat_replacer.hpp"
 #include "transforms/aten_getitem_replacer.hpp"
 #include "transforms/max_prim_list_construct_replacer.hpp"
+#include "transforms/min_prim_list_construct_replacer.hpp"
 #include "transforms/prim_list_unpack_replacer.hpp"
 #include "transforms/prim_tuple_construct_replacer.hpp"
 
@@ -87,6 +88,7 @@ void FrontEnd::normalize(const std::shared_ptr<ov::Model>& model) const {
     manager.register_pass<ov::frontend::pytorch::pass::PrimListUnpackReplacer>();
     manager.register_pass<ov::frontend::pytorch::pass::AtenGetItemReplacer>();
     manager.register_pass<ov::frontend::pytorch::pass::MaxPrimListConstructReplacer>();
+    manager.register_pass<ov::frontend::pytorch::pass::MinPrimListConstructReplacer>();
     manager.register_pass<ov::frontend::pytorch::pass::DecomposeTupleResults>();
     manager.register_pass<ov::pass::ConstantFolding>();
 
