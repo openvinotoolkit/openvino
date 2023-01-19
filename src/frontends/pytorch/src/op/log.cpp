e1e29ef5f8a766ad13efbf69ab16ee3723f6ce11
eaidova <ekaterina.aidova@intel.com>
2023-01-19 16:16:50 +0400

    upd headers

--- src/frontends/pytorch/src/op/log.cpp
+++ src/frontends/pytorch/src/op/log.cpp
@@ -2,8 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include "openvino/op/log.hpp"
+
 #include "openvino/frontend/pytorch/node_context.hpp"
-#include "openvino/opsets/opset10.hpp"
+#include "openvino/op/constant.hpp"
+#include "openvino/op/convert.hpp"
+#include "openvino/op/divide.hpp"
 
 namespace ov {
 namespace frontend {
@@ -13,19 +17,19 @@ namespace op {
 OutputVector translate_log(NodeContext& context) {
     // torch.log returns a tensor with the natural logarithm of the elements of input.
     auto x = context.get_input(0);
-    x = context.mark_node(std::make_shared<opset10::Convert>(x, element::f32));
-    auto log = context.mark_node(std::make_shared<opset10::Log>(x));
+    x = context.mark_node(std::make_shared<ov::op::v0::Convert>(x, element::f32));
+    auto log = context.mark_node(std::make_shared<ov::op::v0::Log>(x));
     return {log};
 };
 
 OutputVector translate_log2(NodeContext& context) {
     // torch.log2 returns a tensor with the logarithm to the base 2 of the elements of input.
     auto x = context.get_input(0);
-    auto two = context.mark_node(opset10::Constant::create(element::f32, Shape{}, {2}));
-    x = context.mark_node(std::make_shared<opset10::Convert>(x, element::f32));
-    auto log2 = context.mark_node(std::make_shared<opset10::Log>(two));
-    auto log = context.mark_node(std::make_shared<opset10::Log>(x));
-    auto res = context.mark_node(std::make_shared<opset10::Divide>(log, log2));
+    auto two = context.mark_node(ov::op::v0::Constant::create(element::f32, Shape{}, {2}));
+    x = context.mark_node(std::make_shared<ov::op::v0::Convert>(x, element::f32));
+    auto log2 = context.mark_node(std::make_shared<ov::op::v0::Log>(two));
+    auto log = context.mark_node(std::make_shared<ov::op::v0::Log>(x));
+    auto res = context.mark_node(std::make_shared<ov::op::v1::Divide>(log, log2));
     return {res};
 };
 
