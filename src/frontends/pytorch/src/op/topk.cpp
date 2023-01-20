5982081a26d6480673c459f31146f1aa7d689b3f
bszmelcz <bartosz.szmelczynski@intel.com>
2023-01-20 14:02:29 +0100

    swithc include statements

--- src/frontends/pytorch/src/op/topk.cpp
+++ src/frontends/pytorch/src/op/topk.cpp
@@ -2,9 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "openvino/frontend/pytorch/node_context.hpp"
 #include "openvino/op/topk.hpp"
 #include "openvino/op/convert.hpp"
+#include "openvino/frontend/pytorch/node_context.hpp"
 
 namespace ov {
 namespace frontend {
