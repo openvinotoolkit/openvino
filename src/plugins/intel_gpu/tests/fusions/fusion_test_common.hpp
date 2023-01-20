499f32913ca2143a73cf81cb5fdbf2005814aa99
tuxedcat <tuxedcat@gmail.com>
2023-01-20 11:00:36 +0900

    fix fusion test

--- src/plugins/intel_gpu/tests/fusions/fusion_test_common.hpp
+++ src/plugins/intel_gpu/tests/fusions/fusion_test_common.hpp
@@ -34,6 +34,10 @@ public:
         cfg_fused.set_property(ov::intel_gpu::optimize_data(true));
         cfg_not_fused.set_property(ov::intel_gpu::optimize_data(false));
         cfg_not_fused.set_property(ov::intel_gpu::allow_static_input_reorder(true));
+
+        cfg_fused.set_property(ov::intel_gpu::queue_type(QueueTypes::in_order));
+        cfg_not_fused.set_property(ov::intel_gpu::queue_type(QueueTypes::in_order));
+        engine.create_onednn_engine(cfg_fused);
     }
 
     void compare(network& not_fused, network& fused, T& p, bool count_reorder = false) {
