94eca966dbc75766f521bee387f914760a3290ab
Ilya Lavrenov <ilya.lavrenov@intel.com>
2023-01-23 23:40:36 +0400

    Fixed crash in CPU tests

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/subgraph_tests/conv_eltwise_fusion.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/subgraph_tests/conv_eltwise_fusion.cpp
@@ -13,16 +13,13 @@ namespace {
     const std::vector<ngraph::element::Type> types{ngraph::element::f32, ngraph::element::f16};
 
 #define MUL(X) std::tuple<ngraph::NodeTypeInfo, int64_t>(ngraph::opset4::Multiply::get_type_info_static(), X)
-// TODO: return the line below
-// #define ADD(X) std::tuple<ngraph::NodeTypeInfo, int64_t>(ngraph::opset4::Add::get_type_info_static(), X)
-// instead of
-#define ADD(X) MUL(X)
+#define ADD(X) std::tuple<ngraph::NodeTypeInfo, int64_t>(ngraph::opset4::Add::get_type_info_static(), X)
 #define IN std::vector<std::tuple<ngraph::NodeTypeInfo, int64_t>>
 
     INSTANTIATE_TEST_SUITE_P(smoke_Convolution_1D, ConvEltwiseFusion,
                             ::testing::Combine(
                                     ::testing::Values(ngraph::opset4::Convolution::get_type_info_static()),
-                                    ::testing::ValuesIn(IN({MUL(4), ADD(5)})),
+                                    ::testing::ValuesIn(IN({ /*MUL(4),*/ ADD(5)})),
                                     ::testing::Values(ngraph::Shape{1, 8, 64}),
                                     ::testing::Values(ngraph::Shape{64, 8, 1}),
                                     ::testing::Values(ngraph::Shape{64, 1}),
@@ -33,7 +30,7 @@ namespace {
     INSTANTIATE_TEST_SUITE_P(smoke_GroupConvolution_1D, ConvEltwiseFusion,
                             ::testing::Combine(
                                     ::testing::Values(ngraph::opset4::GroupConvolution::get_type_info_static()),
-                                    ::testing::ValuesIn(IN({MUL(4), ADD(5)})),
+                                    ::testing::ValuesIn(IN({ /*MUL(4),*/ ADD(5)})),
                                     ::testing::Values(ngraph::Shape{1, 12, 5}),
                                     ::testing::Values(ngraph::Shape{4, 5, 3, 2}),
                                     ::testing::Values(ngraph::Shape{20, 1}),
@@ -44,7 +41,7 @@ namespace {
     INSTANTIATE_TEST_SUITE_P(smoke_ConvolutionBackpropData_1D, ConvEltwiseFusion,
                             ::testing::Combine(
                                     ::testing::Values(ngraph::opset4::ConvolutionBackpropData::get_type_info_static()),
-                                    ::testing::ValuesIn(IN({MUL(4), ADD(5)})),
+                                    ::testing::ValuesIn(IN({ /*MUL(4),*/ ADD(5)})),
                                     ::testing::Values(ngraph::Shape{1, 12, 64}),
                                     ::testing::Values(ngraph::Shape{12, 20, 1}),
                                     ::testing::Values(ngraph::Shape{20, 1}),
@@ -55,7 +52,7 @@ namespace {
     INSTANTIATE_TEST_SUITE_P(smoke_GroupConvolutionBackpropData_1D, ConvEltwiseFusion,
                             ::testing::Combine(
                                     ::testing::Values(ngraph::opset4::GroupConvolutionBackpropData::get_type_info_static()),
-                                    ::testing::ValuesIn(IN({MUL(4), ADD(5)})),
+                                    ::testing::ValuesIn(IN({ /*MUL(4),*/ ADD(5)})),
                                     ::testing::Values(ngraph::Shape{1, 12, 64}),
                                     ::testing::Values(ngraph::Shape{4, 3, 5, 1}),
                                     ::testing::Values(ngraph::Shape{1, 20, 1}),
@@ -74,7 +71,7 @@ namespace {
     INSTANTIATE_TEST_SUITE_P(smoke_Convolution_2D, ConvEltwiseFusion,
                             ::testing::Combine(
                                     ::testing::Values(ngraph::opset4::Convolution::get_type_info_static()),
-                                    ::testing::ValuesIn(IN({MUL(4), ADD(5)})),
+                                    ::testing::ValuesIn(IN({ /*MUL(4),*/ ADD(5)})),
                                     ::testing::Values(ngraph::Shape{1, 3, 64, 64}),
                                     ::testing::Values(ngraph::Shape{20, 3, 1, 1}),
                                     ::testing::ValuesIn(const_shapes_2d),
@@ -85,7 +82,7 @@ namespace {
     INSTANTIATE_TEST_SUITE_P(smoke_GroupConvolution_2D, ConvEltwiseFusion,
                             ::testing::Combine(
                                     ::testing::Values(ngraph::opset4::GroupConvolution::get_type_info_static()),
-                                    ::testing::ValuesIn(IN({MUL(4), ADD(5)})),
+                                    ::testing::ValuesIn(IN({ /*MUL(4),*/ ADD(5)})),
                                     ::testing::Values(ngraph::Shape{1, 12, 64, 64}),
                                     ::testing::Values(ngraph::Shape{4, 5, 3, 1, 2}),
                                     ::testing::ValuesIn(const_shapes_2d),
@@ -96,7 +93,7 @@ namespace {
     INSTANTIATE_TEST_SUITE_P(smoke_ConvolutionBackpropData_2D, ConvEltwiseFusion,
                             ::testing::Combine(
                                     ::testing::Values(ngraph::opset4::ConvolutionBackpropData::get_type_info_static()),
-                                    ::testing::ValuesIn(IN({MUL(4), ADD(5)})),
+                                    ::testing::ValuesIn(IN({ /*MUL(4),*/ ADD(5)})),
                                     ::testing::Values(ngraph::Shape{1, 3, 64, 64}),
                                     ::testing::Values(ngraph::Shape{3, 20, 3, 3}),
                                     ::testing::ValuesIn(const_shapes_2d),
@@ -107,7 +104,7 @@ namespace {
     INSTANTIATE_TEST_SUITE_P(smoke_GroupConvolutionBackpropData_2D, ConvEltwiseFusion,
                             ::testing::Combine(
                                     ::testing::Values(ngraph::opset4::GroupConvolutionBackpropData::get_type_info_static()),
-                                    ::testing::ValuesIn(IN({MUL(4), ADD(5)})),
+                                    ::testing::ValuesIn(IN({ /*MUL(4),*/ ADD(5)})),
                                     ::testing::Values(ngraph::Shape{1, 12, 64, 64}),
                                     ::testing::Values(ngraph::Shape{4, 3, 5, 1, 1}),
                                     ::testing::ValuesIn(const_shapes_2d),
