040a2ad19358b6a635e40cad6f82b60339100927
Mateusz Bencer <mateusz.bencer@intel.com>
2023-01-23 15:00:22 +0000

    handle 1D case

--- src/frontends/onnx/frontend/src/op/dft.cpp
+++ src/frontends/onnx/frontend/src/op/dft.cpp
@@ -13,7 +13,21 @@ namespace op {
 namespace set_1 {
 OutputVector dft(const Node& node) {
     const OutputVector ng_inputs{node.get_ng_inputs()};
-    const auto data = ng_inputs.at(0);
+    ov::Output<ov::Node> data = ng_inputs.at(0);
+
+    if (data.get_partial_shape().rank().is_static()) {
+        const auto length = data.get_partial_shape().rank().get_length();
+        const auto last_axis_pos = length - 1;
+        const auto last_dim = data.get_partial_shape()[last_axis_pos];
+        if (last_dim.is_static()) {
+            if (last_dim.get_length() == 1) {
+                ov::Output<ov::Node> imag_part = default_opset::Constant::create(data.get_element_type(), {}, {0});
+                imag_part = std::make_shared<default_opset::Broadcast>(imag_part,
+                                                                       std::make_shared<default_opset::ShapeOf>(data));
+                data = std::make_shared<default_opset::Concat>(OutputVector{data, imag_part}, last_axis_pos);
+            }
+        }
+    }
 
     const auto dft_length_provided = ng_inputs.size() > 1;
     const auto axis = node.get_attribute_value<int64_t>("axis", 1);
