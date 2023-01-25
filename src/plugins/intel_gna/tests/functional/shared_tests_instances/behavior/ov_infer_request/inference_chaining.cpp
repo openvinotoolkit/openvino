0680ea1dd61a476299bb3d99404d6f257c40fb2f
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2023-01-25 14:58:46 +0100

    [GNA] Excluded EXACT mode from the test (#15239)
    * Excluded EXACT mode from the test
    
    * Update src/plugins/intel_gna/tests/functional/shared_tests_instances/behavior/ov_infer_request/inference_chaining.cpp

--- src/plugins/intel_gna/tests/functional/shared_tests_instances/behavior/ov_infer_request/inference_chaining.cpp
+++ src/plugins/intel_gna/tests/functional/shared_tests_instances/behavior/ov_infer_request/inference_chaining.cpp
@@ -8,22 +8,14 @@
 using namespace ov::test::behavior;
 namespace {
 
+// GNA_SW_EXACT mode excluded from the tests because without quantization we got the bad accuracy for this test
 const std::vector<ov::AnyMap> device_modes {
-    {{"GNA_DEVICE_MODE", "GNA_SW_FP32"},
-    {"GNA_DEVICE_MODE", "GNA_SW_EXACT"}}
-};
-
-const std::vector<ov::AnyMap> configs = {
-    {{"GNA_DEVICE_MODE", "GNA_SW_EXACT"},
-     {"GNA_SCALE_FACTOR_0", "1"},
-     {"GNA_SCALE_FACTOR_1", "1"},
-     {"GNA_SCALE_FACTOR_2", "1"},
-     {"GNA_SCALE_FACTOR_3", "1"}}
+    {{"GNA_DEVICE_MODE", "GNA_SW_FP32"}}
 };
 
 INSTANTIATE_TEST_SUITE_P(smoke_BehaviorTests, OVInferenceChaining,
                         ::testing::Combine(
                                 ::testing::Values(CommonTestUtils::DEVICE_GNA),
-                                ::testing::ValuesIn(configs)),
+                                ::testing::ValuesIn(device_modes)),
                         OVInferenceChaining::getTestCaseName);
 }  // namespace
