0583c614722b6f275df17af807f7f50767507725
sikorsl1 <leonard.sikorski@intel.com>
2023-01-26 09:40:04 +0000

    Working version of aten::unfold

--- tests/layer_tests/pytorch_tests/test_unfold.py
+++ tests/layer_tests/pytorch_tests/test_unfold.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018-2022 Intel Corporation
+# Copyright (C) 2018-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
 import pytest
@@ -7,18 +7,11 @@ import numpy as np
 import torch
 
 
-# @pytest.mark.parametrize('dimension', (0, 1, 2))
-# @pytest.mark.parametrize('size', (1, 2))
-# @pytest.mark.parametrize('step', (1, 2))
-# @pytest.mark.parametrize('input_tensor', (np.random.randn(2, 2, 5).astype(np.float32),
-#                                           np.random.randn(3, 3, 3, 3).astype(np.float32)))
-@pytest.mark.parametrize('step', (1,))
-@pytest.mark.parametrize('size', (2,))
-@pytest.mark.parametrize('dimension', (2,))
-@pytest.mark.parametrize('input_tensor', (np.array([[[1, 2, 3, 4, 5],
-                                                     [11, 12, 13, 14, 15]],
-                                                    [[21, 22, 23, 24, 25],
-                                                     [31, 32, 33, 34, 35]]]).astype(np.float32),))
+@pytest.mark.parametrize('dimension', (0, 1, 2))
+@pytest.mark.parametrize('size', (1, 2))
+@pytest.mark.parametrize('step', (1, 2))
+@pytest.mark.parametrize('input_tensor', (np.random.randn(2, 2, 5).astype(np.float32),
+                                          np.random.randn(3, 3, 3, 3).astype(np.float32)))
 class TestUnfold(PytorchLayerTest):
 
     def _prepare_input(self):
