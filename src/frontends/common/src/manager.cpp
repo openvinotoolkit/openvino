925bbc9d4f2c0dcea4b4d5582270efc03767a31d
vurusovs <vitaliy.urusovskij@intel.com>
2023-01-27 15:30:15 +0400

    Remove "OV_FRONTEND_PATH" from `FrontEndManager`

--- src/frontends/common/src/manager.cpp
+++ src/frontends/common/src/manager.cpp
@@ -207,24 +207,9 @@ private:
     }
 
     void search_all_plugins() {
-        auto search_from_dir = [&](const std::string& dir) {
-            if (!dir.empty()) {
-                find_plugins(dir, m_plugins);
-            }
-        };
-        std::string env_path = ov::util::getenv_string("OV_FRONTEND_PATH");
-        if (!env_path.empty()) {
-            size_t start = 0;
-            auto sep_pos = env_path.find(PathSeparator, start);
-            while (sep_pos != std::string::npos) {
-                search_from_dir(env_path.substr(start, sep_pos - start));
-                start = sep_pos + 1;
-                sep_pos = env_path.find(PathSeparator, start);
-            }
-            search_from_dir(env_path.substr(start, sep_pos));
-        } else {
-            search_from_dir(get_frontend_library_path());
-        }
+        auto fe_lib_dir = get_frontend_library_path();
+        if (!fe_lib_dir.empty())
+            find_plugins(fe_lib_dir, m_plugins);
     }
 };
 
