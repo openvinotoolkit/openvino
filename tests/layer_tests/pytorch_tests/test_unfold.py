a2c2d651f5f94de869233bd763f6e8115b142ddf
sikorsl1 <leonard.sikorski@intel.com>
2023-01-27 11:56:13 +0000

    Changes requested by a reviewer

--- tests/layer_tests/pytorch_tests/test_unfold.py
+++ tests/layer_tests/pytorch_tests/test_unfold.py
@@ -1,17 +1,19 @@
 # Copyright (C) 2018-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-import pytest
-from pytorch_layer_test_class import PytorchLayerTest
 import numpy as np
+import pytest
 import torch
+
+from pytorch_layer_test_class import PytorchLayerTest
 
 
 @pytest.mark.parametrize('dimension', (0, 1, 2))
 @pytest.mark.parametrize('size', (1, 2))
-@pytest.mark.parametrize('step', (1, 2))
+@pytest.mark.parametrize('step', (1, 2, 3, 4))
 @pytest.mark.parametrize('input_tensor', (np.random.randn(2, 2, 5).astype(np.float32),
-                                          np.random.randn(3, 3, 3, 3).astype(np.float32)))
+                                          np.random.randn(3, 3, 3, 3).astype(np.float32),
+                                          np.random.randn(2, 3, 4, 5).astype(np.float32)))
 class TestUnfold(PytorchLayerTest):
 
     def _prepare_input(self):
@@ -34,6 +36,7 @@ class TestUnfold(PytorchLayerTest):
         return aten_unfold(dimension, size, step), ref_net, "aten::unfold"
 
     @pytest.mark.nightly
+    @pytest.mark.precommit
     def test_unfold(self, ie_device, precision, ir_version, dimension, size, step, input_tensor):
         self.input_tensor = input_tensor
         self._test(*self.create_model(dimension, size, step),
