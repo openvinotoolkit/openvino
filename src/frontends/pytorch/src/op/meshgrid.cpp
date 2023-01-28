47922ad4425b5225352c6f2868c756117c4ec405
Mateusz Mikolajczyk <mateusz.mikolajczyk@intel.com>
2023-01-28 08:08:25 +0100

    [PT FE]: Add aten::meshgrid + prim::ListUnpack transformation (#15195)
    * Add meshgrid listunpack transformation
    
    * Add case when indexing is not specified
    
    * Fix typos
    
    * Fix problem with 1 input execution & missing runtime_info
    
    * Fix issue with meshgrid placed in loop body
    
    * Add tests to precommit
    
    * Apply sugestions from review
    
    * Fix input 0
    
    * Improve indexing attribute read

---
+++ src/frontends/pytorch/src/op/meshgrid.cpp
@@ -0,0 +1,29 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "openvino/frontend/pytorch/node_context.hpp"
+#include "pt_framework_node.hpp"
+
+namespace ov {
+namespace frontend {
+namespace pytorch {
+namespace op {
+
+OutputVector translate_meshgrid(NodeContext& context) {
+    OutputVector inputs{context.get_input(0)};
+    std::string indexing = "ij";
+    if (!context.input_is_none(1)) {
+        indexing = context.const_input<std::string>(1);
+    }
+    auto node = std::make_shared<PtFrameworkNode>(context.get_decoder(), inputs);
+    auto attrs = node->get_attrs();
+    attrs["indexing"] = indexing;
+    node->set_attrs(attrs);
+    return {context.mark_node(node)};
+};
+
+}  // namespace op
+}  // namespace pytorch
+}  // namespace frontend
+}  // namespace ov
