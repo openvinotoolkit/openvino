74b97024d4bacb0a25e4533a0a3f83b2ecc83ed9
sikorsl1 <leonard.sikorski@intel.com>
2023-01-30 08:08:37 +0000

    Cover cases with non constant repeats

--- src/frontends/pytorch/src/op/repeat_interleave.cpp
+++ src/frontends/pytorch/src/op/repeat_interleave.cpp
@@ -27,54 +27,86 @@ OutputVector translate_repeat_interleave(NodeContext& context) {
     // constants
     auto const_0 = context.mark_node(opset10::Constant::create(element::i64, Shape{}, {0}));
     auto const_1 = context.mark_node(opset10::Constant::create(element::i64, Shape{}, {1}));
+    auto const_1_list = context.mark_node(opset10::Constant::create(element::i64, Shape{1}, {1}));
     auto const_neg_1 = context.mark_node(opset10::Constant::create(element::i64, Shape{1}, {-1}));
 
     // inputs
     auto input = context.get_input(0);
-    auto repeats = context.const_input<std::vector<int64_t>>(1);
-    FRONT_END_OP_CONVERSION_CHECK(repeats.size() >= 1, "repeats should contain at least 1 element");
-
-    auto const_repeats =
-        context.mark_node(opset10::Constant::create(element::i64, Shape{2}, {repeats.at(0), (int64_t)1}));
     std::shared_ptr<ov::Node> result;
 
-    if (context.input_is_none(2)) {
-        if (repeats.size() == 1) {
+    try {
+        // repeats is Constant
+        auto repeats = context.const_input<std::vector<int64_t>>(1);
+        FRONT_END_OP_CONVERSION_CHECK(repeats.size() >= 1, "repeats should contain at least 1 element");
+        auto const_repeats =
+            context.mark_node(opset10::Constant::create(element::i64, Shape{2}, {repeats.at(0), (int64_t)1}));
+
+        if (context.input_is_none(2)) {
+            if (repeats.size() == 1) {
+                // case (repeats=number, dim=None)
+                auto flat_shape = context.mark_node(opset10::Constant::create(element::i64, Shape{2}, {1, -1}));
+                auto reshape = context.mark_node(std::make_shared<opset10::Reshape>(input, flat_shape, false));
+                auto tile = context.mark_node(std::make_shared<opset10::Tile>(reshape, const_repeats));
+                auto shape_perm = context.mark_node(opset10::Constant::create(element::i64, Shape{2}, {1, 0}));
+                auto transpose = context.mark_node(std::make_shared<opset10::Transpose>(tile, shape_perm));
+                result = std::make_shared<opset10::Reshape>(transpose, const_neg_1, false);
+            } else {
+                // case (repeats=tensor, dim=None)
+                auto flat_shape = context.mark_node(opset10::Constant::create(element::i64, Shape{1}, {-1}));
+                auto reshape = context.mark_node(std::make_shared<opset10::Reshape>(input, flat_shape, false));
+                OutputVector all_indices = generate_indices_from_repeats_tensor(repeats, context);
+                auto concat = context.mark_node(std::make_shared<opset10::Concat>(all_indices, 0));
+                result = std::make_shared<opset10::Gather>(reshape, concat, const_0);
+            }
+        } else {
+            auto const_dim = context.get_input(2);
+            if (repeats.size() == 1) {
+                // case (repeats=number, dim=number)
+                auto input_shape = context.mark_node(std::make_shared<opset10::ShapeOf>(input, element::i64));
+                auto input_dim_size =
+                    context.mark_node(std::make_shared<opset10::Gather>(input_shape, const_dim, const_0));
+                auto range =
+                    context.mark_node(std::make_shared<opset10::Range>(const_0, input_dim_size, const_1, element::i64));
+                auto range_unsqeezed = context.mark_node(std::make_shared<opset10::Unsqueeze>(range, const_0));
+                auto tile = context.mark_node(std::make_shared<opset10::Tile>(range_unsqeezed, const_repeats));
+                auto shape_perm =
+                    context.mark_node(context.mark_node(opset10::Constant::create(element::i64, Shape{2}, {1, 0})));
+                auto transpose = context.mark_node(std::make_shared<opset10::Transpose>(tile, shape_perm));
+                auto flatten = context.mark_node(std::make_shared<opset10::Reshape>(transpose, const_neg_1, false));
+                result = std::make_shared<opset10::Gather>(input, flatten, const_dim);
+            } else {
+                // case (repeats=tensor, dim=number)
+                OutputVector all_indices = generate_indices_from_repeats_tensor(repeats, context);
+                auto concat = context.mark_node(std::make_shared<opset10::Concat>(all_indices, 0));
+                result = std::make_shared<opset10::Gather>(input, concat, const_dim);
+            }
+        }
+    } catch (...) {
+        // repeats is not Constant
+        auto repeats_input = std::make_shared<opset10::Reshape>(context.get_input(1), const_1_list, false);
+        auto repeats = std::make_shared<opset10::Concat>(OutputVector{repeats_input, const_1_list}, 0);
+        if (context.input_is_none(2)) {
             // case (repeats=number, dim=None)
             auto flat_shape = context.mark_node(opset10::Constant::create(element::i64, Shape{2}, {1, -1}));
             auto reshape = context.mark_node(std::make_shared<opset10::Reshape>(input, flat_shape, false));
-            auto tile = context.mark_node(std::make_shared<opset10::Tile>(reshape, const_repeats));
+            auto tile = context.mark_node(std::make_shared<opset10::Tile>(reshape, repeats));
             auto shape_perm = context.mark_node(opset10::Constant::create(element::i64, Shape{2}, {1, 0}));
             auto transpose = context.mark_node(std::make_shared<opset10::Transpose>(tile, shape_perm));
             result = std::make_shared<opset10::Reshape>(transpose, const_neg_1, false);
         } else {
-            // case (repeats=tensor, dim=None)
-            auto flat_shape = context.mark_node(opset10::Constant::create(element::i64, Shape{1}, {-1}));
-            auto reshape = context.mark_node(std::make_shared<opset10::Reshape>(input, flat_shape, false));
-            OutputVector all_indices = generate_indices_from_repeats_tensor(repeats, context);
-            auto concat = context.mark_node(std::make_shared<opset10::Concat>(all_indices, 0));
-            result = std::make_shared<opset10::Gather>(reshape, concat, const_0);
-        }
-    } else {
-        auto const_dim = context.get_input(2);
-        if (repeats.size() == 1) {
             // case (repeats=number, dim=number)
+            auto const_dim = context.get_input(2);
             auto input_shape = context.mark_node(std::make_shared<opset10::ShapeOf>(input, element::i64));
             auto input_dim_size = context.mark_node(std::make_shared<opset10::Gather>(input_shape, const_dim, const_0));
             auto range =
                 context.mark_node(std::make_shared<opset10::Range>(const_0, input_dim_size, const_1, element::i64));
             auto range_unsqeezed = context.mark_node(std::make_shared<opset10::Unsqueeze>(range, const_0));
-            auto tile = context.mark_node(std::make_shared<opset10::Tile>(range_unsqeezed, const_repeats));
+            auto tile = context.mark_node(std::make_shared<opset10::Tile>(range_unsqeezed, repeats));
             auto shape_perm =
                 context.mark_node(context.mark_node(opset10::Constant::create(element::i64, Shape{2}, {1, 0})));
             auto transpose = context.mark_node(std::make_shared<opset10::Transpose>(tile, shape_perm));
             auto flatten = context.mark_node(std::make_shared<opset10::Reshape>(transpose, const_neg_1, false));
             result = std::make_shared<opset10::Gather>(input, flatten, const_dim);
-        } else {
-            // case (repeats=tensor, dim=number)
-            OutputVector all_indices = generate_indices_from_repeats_tensor(repeats, context);
-            auto concat = context.mark_node(std::make_shared<opset10::Concat>(all_indices, 0));
-            result = std::make_shared<opset10::Gather>(input, concat, const_dim);
         }
     }
 
--- tests/layer_tests/pytorch_tests/test_repeat_interleave.py
+++ tests/layer_tests/pytorch_tests/test_repeat_interleave.py
@@ -12,15 +12,16 @@ import torch
                                         {'repeats': 2, 'dim': 2},
                                         {'repeats': [2, 3], 'dim': 1},
                                         {'repeats': [3, 2, 1], 'dim': 3},
+                                        {'repeats': [3, 2, 1], 'dim': 3},
                                         {'repeats': 2, 'dim': None},
                                         {'repeats': [random.randint(1, 5) for _ in range(36)], 'dim': None}))
-class TestRepeatInterleave(PytorchLayerTest):
+class TestRepeatInterleaveConstRepeats(PytorchLayerTest):
 
     def _prepare_input(self):
-        return (np.random.randn(2, 2, 3, 3), )
+        return (np.random.randn(2, 2, 3, 3),)
 
-    def create_model(self, repeats, dim):
-        class aten_repeat_interleave(torch.nn.Module):
+    def create_model_const_repeat(self, repeats, dim):
+        class aten_repeat_interleave_const_repeat(torch.nn.Module):
 
             def __init__(self) -> None:
                 super().__init__()
@@ -32,12 +33,44 @@ class TestRepeatInterleave(PytorchLayerTest):
 
         ref_net = None
 
-        return aten_repeat_interleave(), ref_net, "aten::repeat_interleave"
+        return aten_repeat_interleave_const_repeat(), ref_net, "aten::repeat_interleave"
 
     @pytest.mark.nightly
     @pytest.mark.precommit
-    def test_repeat_interleave(self, ie_device, precision, ir_version, input_data):
+    def test_repeat_interleave_const_repeats(self, ie_device, precision, ir_version, input_data):
         repeats = input_data['repeats']
         dim = input_data['dim']
-        self._test(*self.create_model(repeats, dim),
+        self._test(*self.create_model_const_repeat(repeats, dim),
+                   ie_device, precision, ir_version)
+        
+@pytest.mark.parametrize('input_data', ({'repeats': np.array([1]), 'dim': 0},
+                                        {'repeats': np.array(1), 'dim': 1},
+                                        {'repeats': np.array([2]), 'dim': 2},
+                                        {'repeats': np.array(2), 'dim': 1},
+                                        {'repeats': np.array([3]), 'dim': None}))
+class TestRepeatInterleaveNonConstRepeats(PytorchLayerTest):
+
+    def _prepare_input(self):
+        return (np.random.randn(2, 2, 3, 3), self.repeats)
+
+    def create_model_non_const_repeat(self, dim):
+        class aten_repeat_interleave_non_const_repeat(torch.nn.Module):
+
+            def __init__(self) -> None:
+                super().__init__()
+                self.dim = dim
+
+            def forward(self, input_tensor, repeats):
+                return input_tensor.repeat_interleave(repeats, self.dim)
+
+        ref_net = None
+
+        return aten_repeat_interleave_non_const_repeat(), ref_net, "aten::repeat_interleave"
+
+    @pytest.mark.nightly
+    @pytest.mark.precommit
+    def test_repeat_interleave_non_const_repeats(self, ie_device, precision, ir_version, input_data):
+        self.repeats = input_data['repeats']
+        dim = input_data['dim']
+        self._test(*self.create_model_non_const_repeat(dim),
                    ie_device, precision, ir_version)
