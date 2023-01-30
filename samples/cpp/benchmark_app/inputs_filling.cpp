f1d4c94b3bfc082d2a1ecfd08eff21e7d9d5368c
Zlobin Vladimir <vladimir.zlobin@intel.com>
2023-01-30 18:31:12 +0400

    benchmark_app: fix FP16 image type (#15393)

--- samples/cpp/benchmark_app/inputs_filling.cpp
+++ samples/cpp/benchmark_app/inputs_filling.cpp
@@ -216,12 +216,12 @@ ov::Tensor get_image_tensor(const std::vector<std::string>& files,
                                                inputInfo.first,
                                                filenames_used);
     } else if (type == ov::element::f16) {
-        return create_tensor_from_image<short>(files,
-                                               inputId,
-                                               batchSize,
-                                               inputInfo.second,
-                                               inputInfo.first,
-                                               filenames_used);
+        return create_tensor_from_image<ov::float16>(files,
+                                                     inputId,
+                                                     batchSize,
+                                                     inputInfo.second,
+                                                     inputInfo.first,
+                                                     filenames_used);
     } else if (type == ov::element::f64) {
         return create_tensor_from_image<double>(files,
                                                 inputId,
