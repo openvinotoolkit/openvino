d67eb6f169deeecc1a614cdf3c491509b4698a60
eaidova <ekaterina.aidova@intel.com>
2023-01-31 09:11:22 +0400

    fix tests

--- tests/layer_tests/pytorch_tests/test_size.py
+++ tests/layer_tests/pytorch_tests/test_size.py
@@ -11,7 +11,7 @@ class TestSize(PytorchLayerTest):
         import numpy as np
         return (np.random.randn(*input_shape).astype(np.float32),)
 
-    def create_model(self, case):
+    def create_model(self):
         import torch
 
         class aten_size(torch.nn.Module):
@@ -22,10 +22,9 @@ class TestSize(PytorchLayerTest):
 
         op = aten_size()
 
-        return op(), ref_net, "aten::size"
+        return op, ref_net, "aten::size"
     @pytest.mark.nightly
     @pytest.mark.precommit
     @pytest.mark.parametrize("input_shape", [[1,], [1, 2], [1, 2, 3], [1, 2, 3, 4], [1, 2, 3, 4, 5]])
-    @pytest.mark.parametrize("case", ["size", "size_with_getitem"])
-    def test_silu(self, input_shape, case, ie_device, precision, ir_version):
-        self._test(*self.create_model(case), ie_device, precision, ir_version, kwargs_to_prepare_input={"input_shape": input_shape})
+    def test_size(self, input_shape, ie_device, precision, ir_version):
+        self._test(*self.create_model(), ie_device, precision, ir_version, kwargs_to_prepare_input={"input_shape": input_shape})
