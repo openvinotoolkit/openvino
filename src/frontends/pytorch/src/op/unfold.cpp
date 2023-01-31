5c861b96adf74b35e70680dddef6a23eac31afa3
sikorsl1 <leonard.sikorski@intel.com>
2023-01-31 10:16:16 +0000

    Remove redundant Convert nodes and use int32

--- src/frontends/pytorch/src/op/unfold.cpp
+++ src/frontends/pytorch/src/op/unfold.cpp
@@ -14,25 +14,24 @@ namespace op {
 
 OutputVector translate_unfold(NodeContext& context) {
     // constants
-    auto const_0 = context.mark_node(Constant::create(element::i64, Shape{}, {0}));
-    auto const_1 = context.mark_node(Constant::create(element::i64, Shape{}, {1}));
-    auto const_0_list = context.mark_node(Constant::create(element::i64, Shape{1}, {0}));
-    auto const_1_list = context.mark_node(Constant::create(element::i64, Shape{1}, {1}));
-    auto const_neg_1_list = context.mark_node(Constant::create(element::i64, Shape{1}, {-1}));
+    auto const_0 = context.mark_node(Constant::create(element::i32, Shape{}, {0}));
+    auto const_1 = context.mark_node(Constant::create(element::i32, Shape{}, {1}));
+    auto const_0_list = context.mark_node(Constant::create(element::i32, Shape{1}, {0}));
+    auto const_1_list = context.mark_node(Constant::create(element::i32, Shape{1}, {1}));
+    auto const_neg_1_list = context.mark_node(Constant::create(element::i32, Shape{1}, {-1}));
 
     // get inputs and prepare auxiliary nodes
     auto input = context.get_input(0);
-    auto input_shape = context.mark_node(std::make_shared<ShapeOf>(input));
-    auto input_rank = context.mark_node(std::make_shared<ShapeOf>(input_shape));
+    auto input_shape = context.mark_node(std::make_shared<ShapeOf>(input, element::i32));
+    auto input_rank = context.mark_node(std::make_shared<ShapeOf>(input_shape, element::i32));
 
-    auto dimension_input = context.mark_node(std::make_shared<Unsqueeze>(context.get_input(1), const_0));
-    auto dimension = context.mark_node(std::make_shared<Convert>(dimension_input, element::i64));
+    auto dimension = context.mark_node(std::make_shared<Unsqueeze>(context.get_input(1), const_0));
     auto dimension_plus_1 = context.mark_node(std::make_shared<Add>(dimension, const_1_list));
 
-    auto size_scalar = context.mark_node(std::make_shared<Convert>(context.get_input(2), element::i64));
+    auto size_scalar = context.get_input(2);
     auto size_list = context.mark_node(std::make_shared<Reshape>(size_scalar, const_1_list, false));
 
-    auto step = context.mark_node(std::make_shared<Convert>(context.get_input(3), element::i64));
+    auto step = context.get_input(3);
 
     // calculate number of "slices"
     auto sizedim = context.mark_node(std::make_shared<Gather>(input_shape, dimension, const_0));
@@ -45,14 +44,14 @@ OutputVector translate_unfold(NodeContext& context) {
     // [0,1,...,size-1, 0+step, 1+step,..., size-1+step,..., (slices_count-1)*step,..., size-1+(slices_count-1)*step]
     // for example [0, 1, 2, 3, 2, 3, 4, 5, 4, 5, 6, 7] (size=4, step=2, slices_count=3)
     auto start_indices_unscaled =
-        context.mark_node(std::make_shared<Range>(const_0, slices_count_scalar, const_1, element::i64));
+        context.mark_node(std::make_shared<Range>(const_0, slices_count_scalar, const_1, element::i32));
     auto start_indices = context.mark_node(std::make_shared<Multiply>(start_indices_unscaled, step));
     auto unsqueeze_indices = context.mark_node(std::make_shared<Unsqueeze>(start_indices, const_0));
     auto repeats_for_tile = context.mark_node(std::make_shared<Concat>(OutputVector{size_list, const_1_list}, 0));
     auto tile = context.mark_node(std::make_shared<Tile>(unsqueeze_indices, repeats_for_tile));
-    auto shape_perm = context.mark_node(context.mark_node(Constant::create(element::i64, Shape{2}, {1, 0})));
+    auto shape_perm = context.mark_node(context.mark_node(Constant::create(element::i32, Shape{2}, {1, 0})));
     auto transpose_indices = context.mark_node(std::make_shared<Transpose>(tile, shape_perm));
-    auto range_zero_to_size = context.mark_node(std::make_shared<Range>(const_0, size_scalar, const_1, element::i64));
+    auto range_zero_to_size = context.mark_node(std::make_shared<Range>(const_0, size_scalar, const_1, element::i32));
     auto correct_indices = context.mark_node(std::make_shared<Add>(transpose_indices, range_zero_to_size));
     auto reshape_indices = context.mark_node(std::make_shared<Reshape>(correct_indices, const_neg_1_list, false));
 
@@ -71,9 +70,9 @@ OutputVector translate_unfold(NodeContext& context) {
     auto dimension_plus_1_scalar = context.mark_node(std::make_shared<Reshape>(dimension_plus_1, const_1, false));
     auto dimension_plus_2_scalar = std::make_shared<Add>(dimension_plus_1_scalar, const_1);
     auto perm_begin =
-        context.mark_node(std::make_shared<Range>(const_0, dimension_plus_1_scalar, const_1, element::i64));
+        context.mark_node(std::make_shared<Range>(const_0, dimension_plus_1_scalar, const_1, element::i32));
     auto perm_end =
-        context.mark_node(std::make_shared<Range>(dimension_plus_2_scalar, rank_plus_1_scalar, const_1, element::i64));
+        context.mark_node(std::make_shared<Range>(dimension_plus_2_scalar, rank_plus_1_scalar, const_1, element::i32));
     auto perm = context.mark_node(std::make_shared<Concat>(OutputVector{perm_begin, perm_end, dimension_plus_1}, 0));
     auto transpose = context.mark_node(std::make_shared<Transpose>(reshape, perm));
 
