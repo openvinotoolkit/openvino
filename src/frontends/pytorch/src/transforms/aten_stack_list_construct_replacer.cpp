b419f7418378b33cb5d15405a36e90fd73fae26e
bszmelcz <bartosz.szmelczynski@intel.com>
2023-02-01 13:57:00 +0100

    add comments to the code

--- src/frontends/pytorch/src/transforms/aten_stack_list_construct_replacer.cpp
+++ src/frontends/pytorch/src/transforms/aten_stack_list_construct_replacer.cpp
@@ -21,6 +21,7 @@ AtenStackListConstructReplacer::AtenStackListConstructReplacer() {
     auto list_construct = ov::pass::pattern::wrap_type<ov::op::util::FrameworkNode>();
     auto axis = ov::pass::pattern::wrap_type<opset10::Constant>();
 
+    // We search for a pattern: ListConstruct -> aten::stack <- Constant
     auto stack = ov::pass::pattern::wrap_type<ov::op::util::FrameworkNode>({list_construct, axis});
 
     ov::matcher_pass_callback callback = [=](ov::pass::pattern::Matcher& m) {
@@ -33,16 +34,18 @@ AtenStackListConstructReplacer::AtenStackListConstructReplacer() {
         auto axis_node = pattern_map.at(axis).get_node_shared_ptr();
         auto axis_const = std::dynamic_pointer_cast<opset10::Constant>(axis_node);
         auto axis = axis_const->cast_vector<int64_t>();
-
+        // Check if ListConstruct is an input
         if (auto list_construct_node = cast_fw_node(input_node, "prim::ListConstruct")) {
             const auto& list_inputs = list_construct_node->input_values();
             OutputVector node_vector;
             auto zero = opset10::Constant::create(element::i32, Shape{}, {0});
+            // Iterate over values in ListConstruct
             for (const auto& list_input : list_inputs) {
                 auto node = concat_list_construct(list_input.get_node_shared_ptr());
                 auto unsqueezed_node = std::make_shared<opset10::Unsqueeze>(node, axis_const);
                 node_vector.push_back(unsqueezed_node);
             }
+            // Concat vectors on provided axis
             auto concat = std::make_shared<opset10::Concat>(node_vector, axis[0]);
 
             copy_runtime_info({stack, input_node}, concat);
