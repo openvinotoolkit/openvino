9edaef5ae6f4fa7e5415d3c053a17db6a415a2a4
Roman Kazantsev <roman.kazantsev@intel.com>
2023-02-02 18:28:31 +0400

    [TF FE] Refactor ExtractImagePatches and add tests (#15456)
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/frontends/tensorflow_common/src/op/extract_image_patches.cpp
+++ src/frontends/tensorflow_common/src/op/extract_image_patches.cpp
@@ -16,41 +16,36 @@ namespace tensorflow {
 namespace op {
 
 OutputVector translate_extract_image_patches_op(const NodeContext& node) {
-    TENSORFLOW_OP_VALIDATION(node, node.get_input_size() >= 0, "ExtractImagePatches must have at least one input.");
+    default_op_checks(node, 1, {"ExtractImagePatches"});
     auto images = node.get_input(0);
 
     // retrieve attributes for ExtractImagePatches
     auto tf_ksizes = node.get_attribute<std::vector<int64_t>>("ksizes");
     auto tf_strides = node.get_attribute<std::vector<int64_t>>("strides");
     auto tf_rates = node.get_attribute<std::vector<int64_t>>("rates");
-    auto tf_padding_type = node.get_attribute<std::string>("padding");
-    ov::op::PadType auto_pad = convert_tf_padding(node, tf_padding_type);
+    auto padding = node.get_attribute<std::string>("padding");
+    ov::op::PadType auto_pad = convert_tf_padding(node, padding);
     TENSORFLOW_OP_VALIDATION(node,
                              auto_pad == ov::op::PadType::SAME_UPPER || auto_pad == ov::op::PadType::VALID,
-                             "Only SAME_UPPER and VALID padding modes are supported for ExtractImagePatches.");
+                             "[TensorFlow Frontend] Inconsistent model: only SAME and VALID padding modes are "
+                             "supported for ExtractImagePatches.");
 
-    // prepare attributes for OpenVINO ExtractImagePatches
+    // prepare attributes for opset ExtractImagePatches
     Shape sizes(2);
     Shape rates(2);
     Strides strides(2);
-
-    // layout for this operation is always NHWC
-    bool is_nhwc = true;
-    convert_nhwc_to_hw(is_nhwc, tf_ksizes, sizes);
-    convert_nhwc_to_hw(is_nhwc, tf_strides, strides);
-    convert_nhwc_to_hw(is_nhwc, tf_rates, rates);
+    convert_nhwc_to_hw(true, tf_ksizes, sizes);
+    convert_nhwc_to_hw(true, tf_strides, strides);
+    convert_nhwc_to_hw(true, tf_rates, rates);
 
     // prepare input to ExtractImagePatches
-    convert_nhwc_to_nchw(is_nhwc, images);
+    convert_nhwc_to_nchw(true, images);
 
-    auto extract_image_patches = make_shared<ExtractImagePatches>(images, sizes, strides, rates, auto_pad);
+    Output<Node> extract_image_patches = make_shared<ExtractImagePatches>(images, sizes, strides, rates, auto_pad);
+    convert_nchw_to_nhwc(true, extract_image_patches);
 
-    // prepare output to return the original layout NHWC
-    auto extract_image_patches_output = extract_image_patches->output(0);
-    convert_nchw_to_nhwc(is_nhwc, extract_image_patches_output);
-
-    set_node_name(node.get_name(), extract_image_patches_output.get_node_shared_ptr());
-    return {extract_image_patches_output};
+    set_node_name(node.get_name(), extract_image_patches.get_node_shared_ptr());
+    return {extract_image_patches};
 }
 }  // namespace op
 }  // namespace tensorflow
