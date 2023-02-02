fe3d797cf6b4d1747f535b0e8366cbf173c62e9e
eaidova <ekaterina.aidova@intel.com>
2023-02-02 18:39:05 +0400

    [PT FE]: support prim::min, prim::ListConstruct as output and fix aten::getitem for prim::ListConstruct

--- src/frontends/pytorch/src/frontend.cpp
+++ src/frontends/pytorch/src/frontend.cpp
@@ -16,10 +16,10 @@
 #include "transforms/aten_cat_replacer.hpp"
 #include "transforms/aten_getitem_replacer.hpp"
 #include "transforms/listconstruct_reshape_replacer.hpp"
-#include "transforms/max_prim_list_construct_replacer.hpp"
+#include "transforms/min_max_prim_list_construct_replacer.hpp"
 #include "transforms/prim_list_construct_pad.hpp"
+#include "transforms/prim_list_tuple_construct_replacer.hpp"
 #include "transforms/prim_list_unpack_replacer.hpp"
-#include "transforms/prim_tuple_construct_replacer.hpp"
 
 namespace ov {
 namespace frontend {
@@ -88,10 +88,11 @@ void FrontEnd::normalize(const std::shared_ptr<ov::Model>& model) const {
     manager.register_pass<ov::frontend::pytorch::pass::AppendListUnpackReplacer>();
     manager.register_pass<ov::frontend::pytorch::pass::PrimListUnpackReplacer>();
     manager.register_pass<ov::frontend::pytorch::pass::AtenGetItemReplacer>();
-    manager.register_pass<ov::frontend::pytorch::pass::MaxPrimListConstructReplacer>();
     manager.register_pass<ov::frontend::pytorch::pass::ListConstructReshapeReplacer>();
     manager.register_pass<ov::frontend::pytorch::pass::PrimListConstructPadReplacer>();
-    manager.register_pass<ov::frontend::pytorch::pass::DecomposeTupleResults>();
+    manager.register_pass<ov::frontend::pytorch::pass::MinMaxPrimListConstructReplacer>();
+    manager.register_pass<ov::frontend::pytorch::pass::DecomposeListTupleResults>();
+    manager.register_pass<ov::pass::ConstantFolding>();
 
     manager.run_passes(model);
 
