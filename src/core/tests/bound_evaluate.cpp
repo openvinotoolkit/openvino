d2940bccc498bcaee012957b6eae55fd2d120a57
Raasz, Pawel <pawel.raasz@intel.com>
2023-02-02 15:15:23 +0100

    Correct code style

--- src/core/tests/bound_evaluate.cpp
+++ src/core/tests/bound_evaluate.cpp
@@ -2,9 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include "bound_evaluate.hpp"
+
 #include <gtest/gtest.h>
 
-#include "bound_evaluate.hpp"
 #include "openvino/op/util/framework_node.hpp"
 #include "openvino/opsets/opset10.hpp"
 #include "type_prop.hpp"
