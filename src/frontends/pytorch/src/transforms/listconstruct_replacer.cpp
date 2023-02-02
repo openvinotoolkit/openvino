92d792b0be721c2a2bc79fc1c30862a7c005675b
Maxim Vafin <maxim.vafin@intel.com>
2023-02-02 11:50:36 +0100

    Add AdaptiveAvgPool

--- src/frontends/pytorch/src/transforms/listconstruct_replacer.cpp
+++ src/frontends/pytorch/src/transforms/listconstruct_replacer.cpp
@@ -5,6 +5,7 @@
 #include "listconstruct_replacer.hpp"
 
 #include "openvino/core/rt_info.hpp"
+#include "openvino/op/adaptive_avg_pool.hpp"
 #include "openvino/op/broadcast.hpp"
 #include "openvino/op/concat.hpp"
 #include "openvino/op/constant.hpp"
@@ -33,8 +34,9 @@ ListConstructReplacer::ListConstructReplacer() {
     auto reshape_op = pattern::wrap_type<v1::Reshape>({pattern::any_input(), list_construct});
     auto roll_op = pattern::wrap_type<v7::Roll>({pattern::any_input(), list_construct, pattern::any_input()});
     auto broadcast_op = pattern::wrap_type<v1::Broadcast>({pattern::any_input(), list_construct});
+    auto adapool_op = pattern::wrap_type<v8::AdaptiveAvgPool>({pattern::any_input(), list_construct});
 
-    auto lc_pattern = std::make_shared<pattern::op::Or>(OutputVector{reshape_op, roll_op, broadcast_op});
+    auto lc_pattern = std::make_shared<pattern::op::Or>(OutputVector{reshape_op, roll_op, broadcast_op, adapool_op});
 
     ov::matcher_pass_callback callback = [=](pattern::Matcher& m) {
         auto& pattern_map = m.get_pattern_value_map();
