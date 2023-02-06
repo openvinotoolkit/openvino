f062cd602f241751df3925c1a4d8e8552e32353a
Roman Kazantsev <roman.kazantsev@intel.com>
2023-02-06 12:49:29 +0400

    [TF FE] Add layer test for Pack (#15518)
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/frontends/tensorflow_common/src/op/pack.cpp
+++ src/frontends/tensorflow_common/src/op/pack.cpp
@@ -14,18 +14,21 @@ namespace tensorflow {
 namespace op {
 
 OutputVector translate_pack_op(const NodeContext& node) {
-    auto axis = node.get_attribute<int64_t>("axis");
+    default_op_checks(node, 1, {"Pack", "PACK"});
+    auto num_size = static_cast<int>(node.get_input_size());
+
+    auto axis = node.get_attribute<int64_t>("axis", 0);
     auto axis_const = make_shared<Constant>(element::i64, Shape{}, axis);
 
     OutputVector concat_inputs;
-    for (size_t i = 0; i < node.get_input_size(); ++i) {
-        auto in = node.get_input(static_cast<int>(i));
+    for (int ind = 0; ind < num_size; ++ind) {
+        auto in = node.get_input(ind);
         concat_inputs.push_back(make_shared<Unsqueeze>(in, axis_const));
     }
 
-    auto res = make_shared<Concat>(concat_inputs, axis);
-    set_node_name(node.get_name(), res);
-    return res->outputs();
+    auto pack = make_shared<Concat>(concat_inputs, axis);
+    set_node_name(node.get_name(), pack);
+    return {pack};
 }
 }  // namespace op
 }  // namespace tensorflow
