feeec953bd6b55274fed550ae3f4716e94b8e952
mitruska <katarzyna.mitrus@intel.com>
2023-02-06 13:28:05 +0100

    Tests refactor

--- src/plugins/intel_cpu/tests/unit/shape_inference_test/ctc_greedy_decoder_shape_inference_test.cpp
+++ src/plugins/intel_cpu/tests/unit/shape_inference_test/ctc_greedy_decoder_shape_inference_test.cpp
@@ -13,50 +13,50 @@ using namespace ov;
 using namespace ov::intel_cpu;
 using namespace testing;
 
-class CTCGreedyDecoderStaticShapeInferenceTest : public OpStaticShapeInferenceTest<op::v0::CTCGreedyDecoder> {};
+class CTCGreedyDecoderV0StaticShapeInferenceTest : public OpStaticShapeInferenceTest<op::v0::CTCGreedyDecoder> {
+    void SetUp() override {
+        output_shapes.resize(1);
+    }
+};
 
-TEST_F(CTCGreedyDecoderStaticShapeInferenceTest, ctc_greedy_decoder) {
+TEST_F(CTCGreedyDecoderV0StaticShapeInferenceTest, basic) {
     auto data = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1});
     auto seq_mask = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{-1, -1});
     op = make_op(data, seq_mask, false);
 
     input_shapes = {StaticShape{100, 3, 1200}, StaticShape{100, 3}};
-    output_shapes = {StaticShape{}};
 
     shape_inference(op.get(), input_shapes, output_shapes);
     EXPECT_EQ(output_shapes[0], StaticShape({3, 100, 1, 1}));
 }
 
-TEST_F(CTCGreedyDecoderStaticShapeInferenceTest, ctc_greedy_decoder_default_ctor) {
+TEST_F(CTCGreedyDecoderV0StaticShapeInferenceTest, decoder_default_ctor) {
     op = make_op();
 
     input_shapes = {StaticShape{100, 3, 1200}, StaticShape{100, 3}};
-    output_shapes = {StaticShape{}};
 
     shape_infer(op.get(), input_shapes, output_shapes);
     EXPECT_EQ(output_shapes[0], StaticShape({3, 100, 1, 1}));
 }
 
-TEST_F(CTCGreedyDecoderStaticShapeInferenceTest, ctc_greedy_decoder_incompatible_batch) {
+TEST_F(CTCGreedyDecoderV0StaticShapeInferenceTest, incompatible_batch) {
     auto data = std::make_shared<op::v0::Parameter>(element::f32, PartialShape::dynamic());
     auto seq_mask = std::make_shared<op::v0::Parameter>(element::i32, PartialShape::dynamic());
     op = make_op(data, seq_mask, false);
 
     input_shapes = {StaticShape{10, 3, 1200}, StaticShape{100, 3}};
-    output_shapes = {StaticShape{}};
 
     OV_EXPECT_THROW(shape_inference(op.get(), input_shapes, output_shapes),
                     NodeValidationFailure,
                     HasSubstr("The first dimensions of input tensors must match"))
 }
 
-TEST_F(CTCGreedyDecoderStaticShapeInferenceTest, ctc_greedy_decoder_incompatible_t_dim) {
+TEST_F(CTCGreedyDecoderV0StaticShapeInferenceTest, incompatible_t_dim) {
     auto data = std::make_shared<op::v0::Parameter>(element::f32, PartialShape::dynamic());
     auto seq_mask = std::make_shared<op::v0::Parameter>(element::i32, PartialShape::dynamic());
     op = make_op(data, seq_mask, false);
 
     input_shapes = {StaticShape{100, 3, 1200}, StaticShape{100, 5}};
-    output_shapes = {StaticShape{}};
 
     OV_EXPECT_THROW(shape_inference(op.get(), input_shapes, output_shapes),
                     NodeValidationFailure,
