cf3f93e4754abd17cfdca1a958e97f10014588ad
Shen, Wanglei <wanglei.shen@intel.com>
2023-02-07 21:16:32 +0800

    move CPU mapping parser API to specific header file

---
+++ src/inference/include/ie/streams_executor.hpp
@@ -0,0 +1,90 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+/**
+ * @brief A header file that provides a set minimal required Streams Executor API.
+ * @file streams_executor.hpp
+ */
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace InferenceEngine {
+/**
+ * @enum column_of_cpu_mapping_table
+ * @brief This enum contains defination of each columns in CPU mapping table which use processor id as index.
+ *
+ * GROUP_ID is generated according to the following rules.
+ *  1. If one MAIN_CORE_PROC and one HYPER_THREADING_PROC are based on same Performance-cores, they are in one group.
+ *  2. If some EFFICIENT_CORE_PROC share one L2 cachle, they are in one group.
+ *  3. There are no duplicate group IDs in the system
+ *
+ * Below is the example of CPU mapping table.
+ *  1. Four processors of two Pcore
+ *  2. Four processors of four Ecores shared L2 cache
+ *
+ *  PROCESSOR_ID | SOCKET_ID | CORE_ID | CORE_TYPE | GROUP_ID | Used
+ *       0             0          0          2          0        0
+ *       1             0          0          1          0        0
+ *       2             0          1          2          1        0
+ *       3             0          1          1          1        0
+ *       4             0          2          3          2        0
+ *       5             0          3          3          2        0
+ *       6             0          4          3          2        0
+ *       7             0          5          3          2        0
+ */
+typedef enum {
+    CPU_MAP_PROCESSOR_ID = 0,  //!< column for processor id of the processor
+    CPU_MAP_SOCKET_ID = 1,     //!< column for socket id of the processor
+    CPU_MAP_CORE_ID = 2,       //!< column for hardware core id of the processor
+    CPU_MAP_CORE_TYPE = 3,     //!< column for CPU core type corresponding to the processor
+    CPU_MAP_GROUP_ID = 4,      //!< column for group id to the processor. Processors in one group have dependency.
+    CPU_MAP_USED_FLAG = 5,     //!< column for resource management of the processor
+    CPU_MAP_TABLE_SIZE = 6
+} column_of_cpu_mapping_table;
+
+#ifdef __linux__
+/**
+ * @brief      Parse processors infomation on Linux
+ * @ingroup    ie_dev_api_system_conf
+ * @param[in]  _processors total number for processors in system.
+ * @param[in]  _system_info_table system information for this platform.
+ * @param[out] _sockets total number for sockets in system
+ * @param[out] _cores total number for physical CPU cores in system
+ * @param[out] _proc_type_table summary table of number of processors per type
+ * @param[out] _cpu_mapping_table CPU mapping table for each processor
+ * @return
+ */
+void parse_processor_info_linux(const int _processors,
+                                const std::vector<std::vector<std::string>> _system_info_table,
+                                int& _sockets,
+                                int& _cores,
+                                std::vector<std::vector<int>>& _proc_type_table,
+                                std::vector<std::vector<int>>& _cpu_mapping_table);
+#endif
+
+#if (defined(_WIN32) || defined(_WIN64))
+/**
+ * @brief      Parse processors infomation on Windows
+ * @ingroup    ie_dev_api_system_conf
+ * @param[in]  base_ptr buffer object pointer of Windows system infomation
+ * @param[in]  len buffer object length of Windows system infomation
+ * @param[in]  _processors total number for processors in system.
+ * @param[out] _sockets total number for sockets in system
+ * @param[out] _cores total number for physical CPU cores in system
+ * @param[out] _proc_type_table summary table of number of processors per type
+ * @param[out] _cpu_mapping_table CPU mapping table for each processor
+ * @return
+ */
+void parse_processor_info_win(const char* base_ptr,
+                              const unsigned long len,
+                              const int _processors,
+                              int& _sockets,
+                              int& _cores,
+                              std::vector<std::vector<int>>& _proc_type_table,
+                              std::vector<std::vector<int>>& _cpu_mapping_table);
+#endif
+
+}  // namespace InferenceEngine
