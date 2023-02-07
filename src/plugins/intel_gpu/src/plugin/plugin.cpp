f7bb1eda3e3cce2893c79f01daba315b83f1f3d1
Mingyu Kim <mingyu.kim@intel.com>
2023-02-07 15:22:45 +0900

    [GPU] Show num_ccs for RANGE_FOR_STREAMS (#15525)

--- src/plugins/intel_gpu/src/plugin/plugin.cpp
+++ src/plugins/intel_gpu/src/plugin/plugin.cpp
@@ -500,7 +500,7 @@ Parameter Plugin::GetMetric(const std::string& name, const std::map<std::string,
         std::tuple<unsigned int, unsigned int, unsigned int> range = std::make_tuple(1, 2, 1);
         IE_SET_METRIC_RETURN(RANGE_FOR_ASYNC_INFER_REQUESTS, range);
     } else if (name == ov::range_for_streams) {
-        std::tuple<unsigned int, unsigned int> range = std::make_tuple(1, 2);
+        std::tuple<unsigned int, unsigned int> range = std::make_tuple(1, device_info.num_ccs == 1 ? 2 : device_info.num_ccs);
         IE_SET_METRIC_RETURN(RANGE_FOR_STREAMS, range);
     } else if (name == GPU_METRIC_KEY(MEMORY_STATISTICS) ||
                name == ov::intel_gpu::memory_statistics) {
