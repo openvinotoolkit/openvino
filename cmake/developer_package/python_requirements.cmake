76d043ae8291b4725bee27181fc72950227827f0
p-wysocki <przemyslaw.wysocki@intel.com>
2023-02-13 14:47:57 +0100

    Add dependency resolver to cmake

--- cmake/developer_package/python_requirements.cmake
+++ cmake/developer_package/python_requirements.cmake
@@ -97,8 +97,46 @@ function(ov_check_pip_packages)
 
     if(PYTHONINTERP_FOUND)
         execute_process(
-            COMMAND ${PYTHON_EXECUTABLE} -c "import pkg_resources ;
-            pkg_resources.require('onnx==1.12.0')"
+            COMMAND ${PYTHON_EXECUTABLE} -c "
+import pkg_resources ;
+import re ;
+import os ;
+constraints = {} ;
+constraints_path = None ;
+requirements = [] ;
+with open('${ARG_REQUIREMENTS_FILE}') as f:
+    raw_requirements = f.readlines() ;
+for line in raw_requirements:
+    if line.startswith('-c'):
+        constraints_path = os.path.join(os.path.dirname('${ARG_REQUIREMENTS_FILE}'), line.split(' ')[1][:-1]) ;
+if constraints_path: 
+    with open(constraints_path) as f:
+        raw_constraints = f.readlines() ;
+    for line in raw_constraints:
+        if line.startswith('#') or line=='\\n':
+            continue ;
+        line = line.replace('\\n', '') ;
+        package, delimiter, constraint = re.split('(~|=|<|>|;)', line, maxsplit=1) ;
+        if constraints.get(package) is None:
+            constraints[package] = [delimiter + constraint] ;
+        else:
+            constraints[package].extend([delimiter + constraint]) ;
+    for line in raw_requirements:
+        if line.startswith(('#', '-c')):
+            continue ;
+        line = line.replace('\\n', '') ;
+        if re.search('\\W', line):
+            requirements.append(line) ;
+        else:
+            constraint = constraints.get(line) ;
+            if constraint:
+                for marker in constraint: 
+                    requirements.append(line+marker) ;
+            else:
+                requirements.append(line) ;
+else:
+    requirements = raw_requirements ;
+pkg_resources.require(requirements)"
             RESULT_VARIABLE EXIT_CODE
             OUTPUT_VARIABLE OUTPUT_TEXT
             ERROR_VARIABLE ERROR_TEXT)
