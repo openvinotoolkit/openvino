a3d3ce75b6574b8171b2d9fccf368bd7138fab4a
Taylor Yeonbok Lee <taylor.lee@intel.com>
2023-02-15 06:30:32 +0900

    Fixed quantize unittest error

--- src/plugins/intel_gpu/src/graph/impls/ocl/quantize.cpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl/quantize.cpp
@@ -84,6 +84,7 @@ public:
             quantize_params.inputs.push_back(convert_data_tensor(impl_param.input_layouts[i]));
         }
 
+        quantize_params.is_shape_agnostic = impl_param.is_dynamic();
         auto& kernel_selector = kernel_selector::quantize_kernel_selector::Instance();
         auto best_kernel = kernel_selector.get_best_kernel(quantize_params, quantize_optional_params);
 
@@ -94,7 +95,6 @@ public:
         auto quantize_params = get_default_params<kernel_selector::quantize_params>(impl_param);
         const auto& output_layout = impl_param.get_output_layout();
         quantize_params.packed_binary_output = output_layout.data_type == data_types::bin;
-        quantize_params.is_shape_agnostic = true;
         (_kernel_data.update_dispatch_data_func)(quantize_params, _kernel_data);
         set_skip_kernels();
     }
