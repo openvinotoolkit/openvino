fb114e7fee9e859f4b419c5913e9429173094b27
hyunback <hyunback.kim@intel.com>
2023-02-14 09:09:05 +0000

    [GPU] Add oneDNN FC preferred_format to bfyx
    oneDNN FC always run bfyx.
    
    Signed-off-by: hyunback <hyunback.kim@intel.com>

--- src/plugins/intel_gpu/src/graph/layout_optimizer.cpp
+++ src/plugins/intel_gpu/src/graph/layout_optimizer.cpp
@@ -1757,6 +1757,10 @@ format layout_optimizer::get_preferred_format(program_node& node) {
         // Set default format for issue 92967/98750
         // TODO: will remove when arg_max_min_ref supports blocked format
         expected = format::get_default_format(node.get_input_layouts()[0].get_rank(), false, false);
+    } else if (node.is_type<fully_connected>()) {
+        if (use_onednn_impls) {
+            expected = format::bfyx;
+        }
     }
 
     if (allow_new_shape_infer && node.get_preferred_input_fmt() != format::any) {
