ca2ea4d759d186eeca80aa6cac976d29e8f6122a
mhy-666 <2367290636@qq.com>
2023-02-15 08:17:02 +0000

    fix

--- src/frontends/paddle/src/op/prelu.cpp
+++ src/frontends/paddle/src/op/prelu.cpp
@@ -9,17 +9,18 @@ namespace ov {
 namespace frontend {
 namespace paddle {
 namespace op {
-NamedOutputs cross(const NodeContext& node) {
-    auto x = node.get_input("X");
-    auto weight = node.get_input("Alpha");
+NamedOutputs cross(const NodeContext &node) {
+  auto x = node.get_input("X");
+  auto weight = node.get_input("Alpha");
 
-    auto data_format  = node.get_attribute<std::string>("data_format", 'NCHW');
-    
-    auto node_prelu = std::make_shared<default_opset::PRelu>(x, weight);
+  auto data_format = node.get_attribute<std::string>("data_format", 'NCHW');
 
-    return node_prelu;
+  auto node_prelu = std::make_shared<default_opset::PRelu>(x, weight);
 
-}  // namespace op
-}  // namespace paddle
-}  // namespace frontend
-}  // namespace ov
+  return node_prelu;
+}
+
+} // namespace op
+} // namespace paddle
+} // namespace frontend
+} // namespace ov
