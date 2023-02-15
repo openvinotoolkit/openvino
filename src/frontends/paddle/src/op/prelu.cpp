236e688edbb5f2ce57d659a93f0f4e7af97adc13
mhy-666 <2367290636@qq.com>
2023-02-15 11:50:07 +0000

    fix

--- src/frontends/paddle/src/op/prelu.cpp
+++ src/frontends/paddle/src/op/prelu.cpp
@@ -2,8 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "default_opset.hpp"
 #include "openvino/frontend/paddle/node_context.hpp"
+#include "openvino/opsets/opset6.hpp"
 
 namespace ov {
 namespace frontend {
