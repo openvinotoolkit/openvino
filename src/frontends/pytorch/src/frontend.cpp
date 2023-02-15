d8b27ffcd8b823220aafa5dc24021cb336af6ec7
Ekaterina Aidova <ekaterina.aidova@intel.com>
2023-02-15 11:19:15 +0400

    [MO][PT FE] POC Integrate PT FE into MO convert_model (#15197)
    * [WIP]: poc: integrate pt fe to mo.convert_model
    
    * temporaly resolve names using signature inspection
    
    * fix logic
    
    * move to moc frontend
    
    * add rank and data type setting
    
    * add input signature
    
    * apply comments
    
    * move after convert processing
    
    * support work without example_input, provide parameters description
    
    * ignore extensions on fe side
    
    * upd tests
    
    * more tests
    
    * add notes about extensions support and removing onnx path
    
    * remove debug prints
    
    * refactoring
    
    * update BOM

--- src/frontends/pytorch/src/frontend.cpp
+++ src/frontends/pytorch/src/frontend.cpp
@@ -119,7 +119,9 @@ void FrontEnd::normalize(const std::shared_ptr<ov::Model>& model) const {
 }
 
 void FrontEnd::add_extension(const std::shared_ptr<ov::Extension>& extension) {
-    FRONT_END_NOT_IMPLEMENTED(add_extension);
+    // Extension loading mechanism is not implemented, any extensions will be ignored
+    // see CVS-98766 for tracking progress
+    return;
 }
 
 bool FrontEnd::supported_impl(const std::vector<ov::Any>& variants) const {
