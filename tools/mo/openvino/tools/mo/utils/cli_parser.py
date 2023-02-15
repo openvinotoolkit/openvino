efb7840487a8cc27b0bd80c1ab53baee7f879937
Ekaterina Aidova <ekaterina.aidova@intel.com>
2023-02-15 11:19:15 +0400

    [MO][PT FE] POC Integrate PT FE into MO convert_model (#15197)
    * [WIP]: poc: integrate pt fe to mo.convert_model
    
    * temporaly resolve names using signature inspection
    
    * fix logic
    
    * move to moc frontend
    
    * add rank and data type setting
    
    * add input signature
    
    * apply comments
    
    * move after convert processing
    
    * support work without example_input, provide parameters description
    
    * ignore extensions on fe side
    
    * upd tests
    
    * more tests
    
    * add notes about extensions support and removing onnx path
    
    * remove debug prints
    
    * refactoring
    
    * update BOM

--- tools/mo/openvino/tools/mo/utils/cli_parser.py
+++ tools/mo/openvino/tools/mo/utils/cli_parser.py
@@ -704,7 +704,11 @@ mo_convert_params = {
     'example_input': ParamDescription('Sample of model input in original framework. '
                                        'For PyTorch it can be torch.Tensor.', '', '', None),
     'onnx_opset_version': ParamDescription('Version of ONNX opset that is used for converting from PyTorch to ONNX.',
-                                           '', '', None)
+                                           '', '', None),
+    'input_signature': ParamDescription('PyTorch model forward method input signature, ' 
+                                        'will be detected automatically for torch.nn.Module based model instances, '
+                                        'for for scripted models may requires to set manually. Example of usage: for forward method defined as'
+                                        ' def forward(self, x, y), it will be ["x", "y"]', '', '', None)
     }
 }
 
