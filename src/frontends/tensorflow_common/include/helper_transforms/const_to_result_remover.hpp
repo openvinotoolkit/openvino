7510642aeab65378245a5a21172e95cccbb1d73c
Roman Kazantsev <roman.kazantsev@intel.com>
2023-02-16 08:49:16 +0100

    [TF FE] Add Const to Result removing transformation (#15739)
    * [TF FE] Add Const to Result removing transformation
    
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>
    
    * Add test model
    
    * Optimize ConstToResultRemover
    
    ---------
    
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/frontends/tensorflow_common/include/helper_transforms/const_to_result_remover.hpp
+++ src/frontends/tensorflow_common/include/helper_transforms/const_to_result_remover.hpp
@@ -11,11 +11,14 @@ namespace frontend {
 namespace tensorflow {
 namespace pass {
 
-// This transformation removes isolated subgraph Unsupported constant going to the Result node
-class UnsupportedConstToResultRemover : public ov::pass::ModelPass {
+// This transformation removes isolated subgraph Constant going to the Result node
+// It can be case that TensorFlow can remain training artifacts in the form
+// of multiple Constant nodes storing training parameter values
+// We need to remove them because separate sub-graphs can solidly affect performance
+class ConstToResultRemover : public ov::pass::ModelPass {
 public:
     OPENVINO_RTTI("ov::frontend::tensorflow::pass::UnsupportedConstToResultRemover");
-    UnsupportedConstToResultRemover() {}
+    ConstToResultRemover() {}
 
     bool run_on_model(const std::shared_ptr<ov::Model>& m) override;
 };
