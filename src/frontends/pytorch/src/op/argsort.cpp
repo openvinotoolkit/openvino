54f12425b4c4d6b89c1810b0ef174630e85a8fbe
Roboreptile <piotr.krzeminski@intel.com>
2023-02-16 17:29:38 +0000

    [PT FE] Fix formatting

--- src/frontends/pytorch/src/op/argsort.cpp
+++ src/frontends/pytorch/src/op/argsort.cpp
@@ -2,13 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 #include "openvino/frontend/pytorch/node_context.hpp"
-#include "openvino/op/shape_of.hpp"
 #include "openvino/op/convert.hpp"
-#include "openvino/op/topk.hpp"
 #include "openvino/op/gather.hpp"
 #include "openvino/op/reshape.hpp"
+#include "openvino/op/shape_of.hpp"
+#include "openvino/op/topk.hpp"
 #include "utils.hpp"
-
 
 namespace ov {
 namespace frontend {
@@ -21,28 +20,29 @@ OutputVector translate_argsort(NodeContext& context) {
     auto const input_tensor = context.get_input(0);
 
     int64_t dim{-1};
-    if(!context.input_is_none(1)) {
+    if (!context.input_is_none(1)) {
         dim = context.const_input<int>(1);
     }
-    
+
     bool descending{false};
-    if(!context.input_is_none(2)) {
+    if (!context.input_is_none(2)) {
         descending = context.const_input<bool>(2);
     }
     auto mode = descending ? TopKMode::MAX : TopKMode::MIN;
 
     bool stable{false};
-    if(!context.input_is_none(3)) {
+    if (!context.input_is_none(3)) {
         stable = context.const_input<bool>(3);
     }
 
     auto shape = context.mark_node(std::make_shared<v0::ShapeOf>(input_tensor));
 
     ov::Output<ov::Node> output_indices;
-    if(dim == -1) {
+    if (dim == -1) {
         auto k = numel(context, shape);
         auto flattened_input_tensor = context.mark_node(std::make_shared<v1::Reshape>(input_tensor, k, false));
-        auto flattened_topk = context.mark_node(std::make_shared<v3::TopK>(flattened_input_tensor, k, -1, mode, TopKSortType::NONE));
+        auto flattened_topk =
+            context.mark_node(std::make_shared<v3::TopK>(flattened_input_tensor, k, -1, mode, TopKSortType::NONE));
         output_indices = context.mark_node(std::make_shared<v1::Reshape>(flattened_topk->output(1), shape));
     } else {
         auto k = context.mark_node(std::make_shared<v8::Gather>(shape, Shape{(uint64_t)dim}, Shape{0}));
