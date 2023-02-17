627b3a3b8fb77f03ed70a0d8873208dfaab7c1f9
Lyamin-Roman <roman.lyamin@intel.com>
2023-02-18 01:16:11 +0900

    [GPU] Added shape agnostic kernel for Tile

--- src/plugins/intel_gpu/src/graph/impls/ocl/gather_elements.cpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl/gather_elements.cpp
@@ -54,9 +54,9 @@ struct gather_elements_impl : typed_primitive_impl_ocl<gather_elements> {
         return make_unique<gather_elements_impl>(*this);
     }
 
-    static kernel_params_t get_kernel_params(const kernel_impl_params& impl_param) {
+    static kernel_params_t get_kernel_params(const kernel_impl_params& impl_param, bool is_shape_agnostic = false) {
         const auto& primitive = impl_param.typed_desc<gather_elements>();
-        auto params = get_default_params<kernel_selector::gather_elements_params>(impl_param);
+        auto params = get_default_params<kernel_selector::gather_elements_params>(impl_param, is_shape_agnostic);
         auto optional_params = get_default_optional_params<kernel_selector::gather_elements_optional_params>(impl_param.get_program());
 
         size_t rank = impl_param.get_output_layout().get_rank();
@@ -67,7 +67,7 @@ struct gather_elements_impl : typed_primitive_impl_ocl<gather_elements> {
     }
 
     void update_dispatch_data(const kernel_impl_params& impl_param) override {
-       auto kernel_params = get_kernel_params(impl_param);
+       auto kernel_params = get_kernel_params(impl_param, true);
        (_kernel_data.update_dispatch_data_func)(kernel_params.first, _kernel_data);
        update_kernels_list_to_skip();
     }
