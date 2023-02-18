6ad8053d578a962a6a958cfd783200f6bcf9166f
Oleg Pipikin <oleg.pipikin@intel.com>
2023-02-18 09:42:44 +0100

    Remove WA for myriad plugin (#15786)

--- src/tests/ie_test_utils/functional_test_utils/src/plugin_cache.cpp
+++ src/tests/ie_test_utils/functional_test_utils/src/plugin_cache.cpp
@@ -33,10 +33,6 @@ PluginCache &PluginCache::get() {
 }
 
 std::shared_ptr<InferenceEngine::Core> PluginCache::ie(const std::string &deviceToCheck) {
-    // TODO: Remove it after fixing issue 69529
-    // w/a for myriad (cann't store 2 caches simultaneously)
-    ov::test::utils::PluginCache::get().reset();
-
     std::lock_guard<std::mutex> lock(g_mtx);
     if (std::getenv("DISABLE_PLUGIN_CACHE") != nullptr) {
 #ifndef NDEBUG
