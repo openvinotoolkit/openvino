90ac86b0a63e67e7d83a35983b7573e312df43f5
Mykhailo Hnap <mykhailo.hnap@capgemini.com>
2023-02-20 17:48:16 +0200

    [GPU] Run clang format for comparison single layer tests.

--- src/tests/functional/plugin/gpu/shared_tests_instances/single_layer_tests/comparison.cpp
+++ src/tests/functional/plugin/gpu/shared_tests_instances/single_layer_tests/comparison.cpp
@@ -2,8 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include <vector>
 #include "single_layer_tests/comparison.hpp"
+
+#include <vector>
+
 #include "common_test_utils/test_constants.hpp"
 
 using namespace LayerTestsDefinitions;
@@ -11,34 +13,34 @@ using namespace LayerTestsDefinitions::ComparisonParams;
 
 namespace {
 
-std::map<std::vector<size_t>, std::vector<std::vector<size_t >>> inputShapes = {
-        {{1}, {{1}, {17}, {1, 1}, {2, 18}, {1, 1, 2}, {2, 2, 3}, {1, 1, 2, 3}}},
-        {{5}, {{1}, {1, 1}, {2, 5}, {1, 1, 1}, {2, 2, 5}}},
-        {{2, 200}, {{1}, {200}, {1, 200}, {2, 200}, {2, 2, 200}}},
-        {{1, 3, 20}, {{20}, {2, 1, 1}}},
-        {{2, 17, 3, 4}, {{4}, {1, 3, 4}, {2, 1, 3, 4}}},
-        {{2, 1, 1, 3, 1}, {{1}, {1, 3, 4}, {2, 1, 3, 4}, {1, 1, 1, 1, 1}}},
+const std::map<std::vector<size_t>, std::vector<std::vector<size_t>>> inputShapes = {
+    {{1}, {{1}, {17}, {1, 1}, {2, 18}, {1, 1, 2}, {2, 2, 3}, {1, 1, 2, 3}}},
+    {{5}, {{1}, {1, 1}, {2, 5}, {1, 1, 1}, {2, 2, 5}}},
+    {{2, 200}, {{1}, {200}, {1, 200}, {2, 200}, {2, 2, 200}}},
+    {{1, 3, 20}, {{20}, {2, 1, 1}}},
+    {{2, 17, 3, 4}, {{4}, {1, 3, 4}, {2, 1, 3, 4}}},
+    {{2, 1, 1, 3, 1}, {{1}, {1, 3, 4}, {2, 1, 3, 4}, {1, 1, 1, 1, 1}}},
 };
 
-std::vector<InferenceEngine::Precision> inputsPrecisions = {
-        InferenceEngine::Precision::FP32,
+const std::vector<InferenceEngine::Precision> inputsPrecisions = {
+    InferenceEngine::Precision::FP32,
 };
 
-std::vector<ngraph::helpers::ComparisonTypes> comparisonOpTypes = {
-        ngraph::helpers::ComparisonTypes::EQUAL,
-        ngraph::helpers::ComparisonTypes::NOT_EQUAL,
-        ngraph::helpers::ComparisonTypes::GREATER,
-        ngraph::helpers::ComparisonTypes::GREATER_EQUAL,
-        ngraph::helpers::ComparisonTypes::LESS,
-        ngraph::helpers::ComparisonTypes::LESS_EQUAL,
+const std::vector<ngraph::helpers::ComparisonTypes> comparisonOpTypes = {
+    ngraph::helpers::ComparisonTypes::EQUAL,
+    ngraph::helpers::ComparisonTypes::NOT_EQUAL,
+    ngraph::helpers::ComparisonTypes::GREATER,
+    ngraph::helpers::ComparisonTypes::GREATER_EQUAL,
+    ngraph::helpers::ComparisonTypes::LESS,
+    ngraph::helpers::ComparisonTypes::LESS_EQUAL,
 };
 
-std::vector<ngraph::helpers::InputLayerType> secondInputTypes = {
-        ngraph::helpers::InputLayerType::CONSTANT,
-        ngraph::helpers::InputLayerType::PARAMETER,
+const std::vector<ngraph::helpers::InputLayerType> secondInputTypes = {
+    ngraph::helpers::InputLayerType::CONSTANT,
+    ngraph::helpers::InputLayerType::PARAMETER,
 };
 
-std::map<std::string, std::string> additional_config = {};
+const std::map<std::string, std::string> additional_config = {};
 
 INSTANTIATE_TEST_SUITE_P(smoke_CompareWithRefs,
                          ComparisonLayerTest,
