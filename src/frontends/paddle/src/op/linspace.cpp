fd16dfc3f723dadcd347819d2c58e37be1c19607
Patrick-Star125 <2327994230@qq.com>
2023-02-22 13:32:26 +0000

    adjust op test

--- src/frontends/paddle/src/op/linspace.cpp
+++ src/frontends/paddle/src/op/linspace.cpp
@@ -11,23 +11,21 @@ namespace frontend {
 namespace paddle {
 namespace op {
 NamedOutputs linspace(const NodeContext& node) {
-    // The operation is simple that generates a range [start, ..., stop]
-    // with num elements staying in the same distance between each other
     auto start = node.get_input("Start");
     auto stop = node.get_input("Stop");
     auto num = node.get_input("Num");
 
     // compute delta value, i.e. distance between neighbor values of the result
-    auto const_one = std::make_shared<default_opset::Constant>(num.get_element_type(), Shape{}, 1);
+    auto const_one = std::make_shared<default_opset::Constant>(start.get_element_type(), Shape{}, 1);
     Output<Node> num_minus_one = std::make_shared<default_opset::Subtract>(num, const_one);
     num_minus_one = std::make_shared<default_opset::Convert>(num_minus_one, start.get_element_type());
     Output<Node> delta = std::make_shared<default_opset::Subtract>(stop, start);
     delta = std::make_shared<default_opset::Divide>(delta, num_minus_one);
 
     // generate a range of numbers [0, 1, ..., num)
-    // to have exact numbers of elements equal to num
-    auto const_zero = std::make_shared<default_opset::Constant>(num.get_element_type(), Shape{}, 0);
-    auto range0_n = std::make_shared<default_opset::Range>(const_zero, num, const_one, start.get_element_type());
+    auto const_zero = std::make_shared<default_opset::Constant>(start.get_element_type(), Shape{}, 0); 
+    auto const_num = std::make_shared<default_opset::Squeeze>(num);
+    auto range0_n = std::make_shared<default_opset::Range>(const_zero, const_num, const_one, start.get_element_type());
 
     // compute the result
     Output<Node> linspace = std::make_shared<default_opset::Multiply>(range0_n, delta);
