c2f42ce617a17acc467bcbbe97f8137d72ffdb6f
Anastasia Kuporosova <anastasia.kuporosova@intel.com>
2023-02-22 10:43:46 +0100

    Update src/bindings/python/tests/test_graph/test_manager.py

--- src/bindings/python/tests/test_graph/test_manager.py
+++ src/bindings/python/tests/test_graph/test_manager.py
@@ -8,7 +8,7 @@ import os
 import numpy as np
 import pytest
 
-import openvino.runtime.opset8 as ops
+import openvino.runtime.opset10 as ops
 from openvino.runtime import Core, Model
 from openvino.runtime.passes import Manager, Serialize, ConstantFolding, Version
 from tests.test_graph.util import count_ops_of_type
