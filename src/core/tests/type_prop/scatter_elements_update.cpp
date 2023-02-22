b186dafd616c6196a79bacdb43e510805716845d
Raasz, Pawel <pawel.raasz@intel.com>
2023-02-22 16:07:53 +0100

    Add upper, lower and label evaluate

--- src/core/tests/type_prop/scatter_elements_update.cpp
+++ src/core/tests/type_prop/scatter_elements_update.cpp
@@ -52,8 +52,8 @@ TEST(type_prop, scatter_elements_update_data_has_interval_dimensions) {
     set_shape_labels(data_shape, 10);
 
     const auto data = make_shared<op::Parameter>(element::i64, data_shape);
-    const auto indices = make_shared<op::Parameter>(element::i16, PartialShape{1, 2, 2, {2, 3}};);
-    const auto updates = make_shared<op::Parameter>(element::i64, PartialShape{{0, 2}, -1, 2, -1};);
+    const auto indices = make_shared<op::Parameter>(element::i16, PartialShape{1, 2, 2, {2, 3}});
+    const auto updates = make_shared<op::Parameter>(element::i64, PartialShape{{0, 2}, -1, 2, -1});
     const auto axis = make_shared<op::Parameter>(element::i16, PartialShape::dynamic());
 
     const auto scatter = make_shared<op::v3::ScatterElementsUpdate>(data, indices, updates, axis);
@@ -95,6 +95,23 @@ TEST(type_prop, scatter_elements_update_default_ctor) {
     EXPECT_EQ(scatter->get_output_element_type(0), element::f32);
     EXPECT_EQ(scatter->get_output_partial_shape(0), PartialShape({2, 5, 5, 6}));
     EXPECT_THAT(get_shape_labels(scatter->get_output_partial_shape(0)), Each(ov::no_label));
+}
+
+TEST(type_prop, scatter_elements_update_preserve_partial_values_and_labels_via_evaluates_bounds) {
+    const auto data = op::Constant::create(element::i64, Shape{4}, {2, 3, 15, 4});
+    const auto indices = op::Constant::create(element::i64, Shape{2}, {3, 0});
+    auto updates_shape = PartialShape{{10, 20}, {3, 4}};
+    set_shape_labels(updates_shape, 20);
+    const auto axis = make_shared<op::Constant>(element::i16, Shape{}, 0);
+
+    const auto shape_of_u = std::make_shared<op::ShapeOf>(std::make_shared<op::Parameter>(element::i64, updates_shape));
+    const auto scatter = make_shared<op::v3::ScatterElementsUpdate>(data, indices, shape_of_u, axis);
+
+    auto param = std::make_shared<op::Parameter>(element::f32, PartialShape{1});
+    auto bc = std::make_shared<op::v3::Broadcast>(param, scatter, op::BroadcastType::BIDIRECTIONAL);
+
+    EXPECT_EQ(bc->get_output_partial_shape(0), PartialShape({{3, 4}, 3, 15, {10, 20}}));
+    EXPECT_THAT(get_shape_labels(bc->get_output_partial_shape(0)), ElementsAre(21, ov::no_label, ov::no_label, 20));
 }
 
 TEST(type_prop, scatter_elements_update_axis_validation) {
