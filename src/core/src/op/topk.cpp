5061ac2f33e71181d74efc822ebf2ac3576a32a2
tomdol <tomasz.dolbniak@intel.com>
2023-02-23 13:39:48 +0100

    Delegating constructor calls and initialization cleanup

--- src/core/src/op/topk.cpp
+++ src/core/src/op/topk.cpp
@@ -347,16 +347,7 @@ op::v3::TopK::TopK(const Output<Node>& data,
                    const Mode mode,
                    const SortType sort,
                    const element::Type& index_element_type)
-    : op::v1::TopK{} {
-    set_arguments(OutputVector{data, k});
-    m_axis = axis;
-    m_normalized_axis = UNKNOWN_NORMALIZED_AXIS;
-    m_mode = mode;
-    m_sort = sort;
-    m_index_element_type = index_element_type;
-    ov::mark_as_precision_sensitive(input(1));
-    constructor_validate_and_infer_types();
-}
+    : op::v1::TopK{data, k, axis, mode, sort, index_element_type} {}
 
 bool ngraph::op::v3::TopK::visit_attributes(AttributeVisitor& visitor) {
     OV_OP_SCOPE(v3_TopK_visit_attributes);
@@ -498,17 +489,8 @@ ov::op::v11::TopK::TopK(const Output<Node>& data,
                         const SortType sort,
                         const element::Type& index_element_type,
                         const bool stable)
-    : op::v3::TopK{} {
-    set_arguments(OutputVector{data, k});
-    m_axis = axis;
-    m_normalized_axis = UNKNOWN_NORMALIZED_AXIS;
-    m_mode = mode;
-    m_sort = sort;
-    m_index_element_type = index_element_type;
-    m_stable = stable;
-    ov::mark_as_precision_sensitive(input(1));
-    constructor_validate_and_infer_types();
-}
+    : op::v3::TopK{data, k, axis, mode, sort, index_element_type},
+      m_stable{stable} {}
 
 bool ov::op::v11::TopK::visit_attributes(AttributeVisitor& visitor) {
     OV_OP_SCOPE(v11_TopK_visit_attributes);
