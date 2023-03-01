f4ff3b6b76d06c562c0e6cf772fd6077e4ae4eca
Katarzyna Mitrus <katarzyna.mitrus@intel.com>
2023-03-01 08:13:24 +0100

    [ShapeInference] DeformablePSROIPooling shape infer  (#15766)
    * Add shape infer function
    
    * Update shape_infer and usage
    
    * Add setters
    
    * Register shape_infer for CPU
    
    * Tests
    
    * Style
    
    * Add cast for dim type
    
    * Add precision
    
    * Update input size check
    
    * Move setters to cpp

--- src/core/tests/type_prop/deformable_psroi_pooling.cpp
+++ src/core/tests/type_prop/deformable_psroi_pooling.cpp
@@ -8,6 +8,50 @@
 
 using namespace std;
 using namespace ngraph;
+using namespace testing;
+
+TEST(type_prop, deformable_psroi_pooling_default_ctor) {
+    const int64_t output_dim = 48;
+    const int64_t group_size = 2;
+
+    const auto rois_dim = 30;
+
+    auto input_data = make_shared<op::Parameter>(element::f32, PartialShape{2, 4, 64, 56});
+    auto input_coords = make_shared<op::Parameter>(element::f32, PartialShape{rois_dim, 5});
+
+    auto op = make_shared<op::v1::DeformablePSROIPooling>();
+
+    op->set_arguments(OutputVector{input_data, input_coords});
+    op->set_output_dim(output_dim);
+    op->set_group_size(group_size);
+
+    op->validate_and_infer_types();
+
+    const PartialShape expected_output{rois_dim, output_dim, group_size, group_size};
+    EXPECT_EQ(op->get_output_partial_shape(0), expected_output);
+}
+
+TEST(type_prop, deformable_psroi_pooling_interval_labels) {
+    const float spatial_scale = 0.05f;
+    const int64_t output_dim = 48;
+    const int64_t group_size = 2;
+
+    const auto rois_dim = Dimension(15, 30);
+
+    auto input_data = make_shared<op::Parameter>(element::f32, PartialShape{2, 4, 64, 56});
+
+    auto coords_shape = PartialShape{rois_dim, 5};
+    set_shape_labels(coords_shape, 20);
+    auto input_coords = make_shared<op::Parameter>(element::f32, coords_shape);
+
+    auto op =
+        make_shared<op::v1::DeformablePSROIPooling>(input_data, input_coords, output_dim, spatial_scale, group_size);
+
+    const PartialShape expected_output{rois_dim, output_dim, group_size, group_size};
+    EXPECT_EQ(op->get_output_partial_shape(0), expected_output);
+    EXPECT_THAT(get_shape_labels(op->get_output_partial_shape(0)),
+                ElementsAre(20, ov::no_label, ov::no_label, ov::no_label));
+}
 
 TEST(type_prop, deformable_psroi_pooling_no_offsets_group_size_3) {
     const float spatial_scale = 0.0625;
