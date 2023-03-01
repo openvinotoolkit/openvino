3a2c5eefd77cd6932d2aa3ac13686b04c79edfea
Mateusz Bencer <mateusz.bencer@intel.com>
2023-03-01 09:36:35 +0100

    fixed and in legacy opset

--- src/frontends/onnx/frontend/src/op/and.hpp
+++ src/frontends/onnx/frontend/src/op/and.hpp
@@ -10,21 +10,21 @@
 #include "ngraph/node.hpp"
 #include "ngraph/op/and.hpp"
 #include "onnx_import/core/node.hpp"
+#include "utils/common.hpp"
 
 namespace ngraph {
 namespace onnx_import {
 namespace op {
 namespace set_1 {
 inline OutputVector logical_and(const Node& node) {
-    CHECK_VALID_NODE(node,
-                     !node.has_attribute("axis") && !node.has_attribute("broadcast"),
-                     "Legacy broadcast mode of And operator is not supported");
-    return {std::make_shared<default_opset::LogicalAnd>(node.get_ng_inputs().at(0), node.get_ng_inputs().at(1))};
+    return common::handle_opset6_binary_op<default_opset::LogicalAnd>(node);
 }
 }  // namespace set_1
 
 namespace set_7 {
-using set_1::logical_and;
+inline OutputVector logical_and(const Node& node) {
+    return {std::make_shared<default_opset::LogicalAnd>(node.get_ng_inputs().at(0), node.get_ng_inputs().at(1))};
+}
 }  // namespace set_7
 
 }  // namespace op
