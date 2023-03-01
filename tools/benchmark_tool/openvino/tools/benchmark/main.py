2e7b08291a992ed0b6a1f99f055a783b44d5a6b1
haiqi <haiqi.pan@intel.com>
2023-03-01 14:28:13 +0800

    add if property_name == 'AFFINITY'

--- tools/benchmark_tool/openvino/tools/benchmark/main.py
+++ tools/benchmark_tool/openvino/tools/benchmark/main.py
@@ -277,12 +277,13 @@ def main():
                 return
 
             def set_nthreads_pin(property_name, property_value):
-                if property_name in supported_properties or device_name == AUTO_DEVICE_NAME:
-                    # create nthreads/pin primary property for HW device or AUTO if -d is AUTO directly.
+                if property_name == "AFFINITY":
                     if property_value == "YES":
                         property_value = "CORE"
                     elif property_value == "NO":
                         property_value = "NONE"
+                if property_name in supported_properties or device_name == AUTO_DEVICE_NAME:
+                    # create nthreads/pin primary property for HW device or AUTO if -d is AUTO directly.
                     config[device][property_name] = property_value
                 elif if_auto or if_multi:
                     # Create secondary property of -nthreads/-pin only for CPU if CPU device appears in the devices
