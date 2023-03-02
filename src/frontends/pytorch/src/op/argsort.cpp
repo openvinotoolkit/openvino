7ef652b6601733c84e54b68e9f1386ec1bb4ebf9
Roboreptile <piotr.krzeminski@intel.com>
2023-03-02 01:37:43 +0000

    [PT FE] Use utils sort function to implement argsort

--- src/frontends/pytorch/src/op/argsort.cpp
+++ src/frontends/pytorch/src/op/argsort.cpp
@@ -2,21 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 #include "openvino/frontend/pytorch/node_context.hpp"
-#include "openvino/opsets/opset10.hpp"
+#include "utils.hpp"
 
 namespace ov {
 namespace frontend {
 namespace pytorch {
 namespace op {
 
-using namespace opset10;
-using namespace ov::op;
-
 OutputVector translate_argsort(NodeContext& context) {
     const auto input_tensor = context.get_input(0);
-    bool stable;
+    bool stable, descending;
     int64_t dim;
-    bool descending;
 
     if (context.get_input_size() == 4) {
         stable = context.const_input<bool>(1);
@@ -27,17 +23,9 @@ OutputVector translate_argsort(NodeContext& context) {
         dim = context.const_input<int64_t>(1);
         descending = context.const_input<bool>(2);
     }
-    auto mode = descending ? TopKMode::MAX : TopKMode::MIN;
 
-    auto zero_axis = context.mark_node(Constant::create(element::i32, Shape{1}, {0}));
-    auto dim_axis = context.mark_node(Constant::create(element::i64, Shape{1}, {dim}));
-    auto shape = context.mark_node(std::make_shared<ShapeOf>(input_tensor));
-    auto k_values_node = context.mark_node(std::make_shared<Gather>(shape, dim_axis, zero_axis));
-    auto k_values = context.mark_node(std::make_shared<Squeeze>(k_values_node));
-    auto topk = context.mark_node(
-        std::make_shared<TopK>(input_tensor, k_values, dim, mode, TopKSortType::SORT_VALUES, element::i64));
-
-    return {topk->output(1)};
+    auto sort = sort_elements(context, input_tensor, stable, dim, descending);
+    return {sort->output(1)};
 };
 
 }  // namespace op
