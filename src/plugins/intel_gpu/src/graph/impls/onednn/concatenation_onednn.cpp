ff882d20e2a836e9175ac2634f6f1f5dfdbfa43f
Gorbachev <andrei.gorbachev@intel.com>
2023-03-02 18:32:52 +0000

    2nd part of fixed warnings

--- src/plugins/intel_gpu/src/graph/impls/onednn/concatenation_onednn.cpp
+++ src/plugins/intel_gpu/src/graph/impls/onednn/concatenation_onednn.cpp
@@ -34,8 +34,8 @@ protected:
         int input_idx = DNNL_ARG_MULTIPLE_SRC;
         for (size_t i = 0; i < instance.inputs_memory_count(); i++) {
             auto& input = instance.input_memory(i);
-            auto offset = onednn::get_f_offset(instance.get_input_layout(), _pd.dnnl::primitive_desc_base::src_desc(i));
-            args.insert({input_idx++, input.get_onednn_memory(_pd.dnnl::primitive_desc_base::src_desc(i), offset)});
+            auto offset = onednn::get_f_offset(instance.get_input_layout(), _pd.dnnl::primitive_desc_base::src_desc(static_cast<uint8_t>(i)));
+            args.insert({input_idx++, input.get_onednn_memory(_pd.dnnl::primitive_desc_base::src_desc(static_cast<uint8_t>(i)), offset)});
         }
 
         {
