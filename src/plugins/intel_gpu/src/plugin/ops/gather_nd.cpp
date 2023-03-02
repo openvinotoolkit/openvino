ed2214a3f9e36cc46fbd24966a8157f7233feaa5
Gorbachev <andrei.gorbachev@intel.com>
2023-03-02 14:28:57 +0000

    fixed part of warnings

--- src/plugins/intel_gpu/src/plugin/ops/gather_nd.cpp
+++ src/plugins/intel_gpu/src/plugin/ops/gather_nd.cpp
@@ -18,16 +18,18 @@ static void CreateGatherNDOp(Program& p, const std::shared_ptr<ngraph::op::v5::G
     auto inputs = p.GetInputInfo(op);
     std::string layerName = layer_type_name_ID(op);
 
-    auto input_rank = static_cast<uint8_t>(op->get_input_partial_shape(0).size());
-    auto indices_rank = static_cast<uint8_t>(op->get_input_partial_shape(1).size());
+    auto input_rank = op->get_input_partial_shape(0).size();
+    auto indices_rank = op->get_input_partial_shape(1).size();
     auto batch_dims = static_cast<uint8_t>(op->get_batch_dims());
+    auto batch_merged_output = true;
 
     auto primitive = cldnn::gather_nd(layerName,
                                       inputs[0],
                                       inputs[1],
                                       input_rank,
                                       indices_rank,
-                                      batch_dims);
+                                      batch_dims,
+                                      batch_merged_output);
 
     p.add_primitive(*op, primitive);
 }
