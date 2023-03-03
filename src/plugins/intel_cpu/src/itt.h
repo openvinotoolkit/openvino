13767a55a0fdcd30c3bee13e1aaa9471b51c9c8c
Fang Xu <fang.xu@intel.com>
2023-03-03 13:25:39 +0530

    [CPU] Enable conditional compilation for graph optimizer (#15478)

--- src/plugins/intel_cpu/src/itt.h
+++ src/plugins/intel_cpu/src/itt.h
@@ -27,10 +27,15 @@ OV_ITT_DOMAIN(intel_cpu_LT);
 
 #if defined(SELECTIVE_BUILD_ANALYZER)
 #    define CPU_LPT_SCOPE(region) OV_SCOPE(intel_cpu, region)
+#    define CPU_GRAPH_OPTIMIZER_SCOPE(region) OV_SCOPE(intel_cpu, region)
 #elif defined(SELECTIVE_BUILD)
 #    define CPU_LPT_SCOPE(region)                                          \
         if (OV_CC_SCOPE_IS_ENABLED(OV_PP_CAT3(intel_cpu, _, region)) == 0) \
         throw ov::Exception(std::string(OV_PP_TOSTRING(OV_PP_CAT3(ov_op, _, region))) + " is disabled!")
+#    define CPU_GRAPH_OPTIMIZER_SCOPE(region)                              \
+        if (OV_CC_SCOPE_IS_ENABLED(OV_PP_CAT3(intel_cpu, _, region)) == 0) \
+        throw ov::Exception(std::string(OV_PP_TOSTRING(OV_PP_CAT3(intel_cpu, _, region))) + " is disabled!")
 #else
 #    define CPU_LPT_SCOPE(region) OV_ITT_SCOPED_TASK(ov::intel_cpu::itt::domains::intel_cpu, OV_PP_TOSTRING(region))
+#    define CPU_GRAPH_OPTIMIZER_SCOPE(region)
 #endif
