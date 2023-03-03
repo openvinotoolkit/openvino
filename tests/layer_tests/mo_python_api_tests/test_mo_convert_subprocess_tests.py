497f2d577e2380ac2d8ed813d59e548701e360c0
Anastasiia Pnevskaia <anastasia.popova@intel.com>
2023-03-03 06:59:30 +0100

    convert_model() legacy extensions fix (#15742)
    * Fixed legacy extensions passing to MO tool.
    
    * Added tests.
    
    * Corrected test.
    
    * Add debug print.
    
    * Moved tests to layer tests.
    
    * Added comment.
    
    * Moved legacy ext tests to separate file. Fixed tmp .pb file cleaning.
    
    * Small correction.
    
    * Run MO Python API tests directory in CI.
    
    * Small fix.
    
    * Fix for case of splitted output.
    
    * Corrected imports.
    
    * Corrected imports.
    
    * Added run of legacy extensions tests from subprocess.

---
+++ tests/layer_tests/mo_python_api_tests/test_mo_convert_subprocess_tests.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2018-2023 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import subprocess
+import sys
+import unittest
+
+
+class TestSubprocessMoConvert(unittest.TestCase):
+    def test_mo_convert(self):
+        args = [sys.executable, '-m', 'pytest',
+                os.path.join(os.path.dirname(__file__), 'mo_convert_legacy_extensions_test_actual.py'), '-s']
+
+        status = subprocess.run(args, env=os.environ)
+        assert not status.returncode
