03d8b8eea0051d4972afb09251ea1a0da1d81748
Ekaterina Aidova <ekaterina.aidova@intel.com>
2023-03-04 13:17:09 +0400

    [PT FE]: fix arange dtype if it is not provided (#16084)

--- src/frontends/pytorch/src/op/arange.cpp
+++ src/frontends/pytorch/src/op/arange.cpp
@@ -7,6 +7,7 @@
 #include "openvino/op/convert.hpp"
 #include "openvino/op/convert_like.hpp"
 #include "openvino/op/range.hpp"
+#include "pt_framework_node.hpp"
 #include "utils.hpp"
 
 namespace ov {
@@ -19,6 +20,7 @@ using namespace ov::op;
 OutputVector translate_arange(NodeContext& context) {
     auto zero = context.mark_node(v0::Constant::create(element::i32, Shape{}, {0}));
     auto one = context.mark_node(v0::Constant::create(element::i32, Shape{}, {1}));
+    int dtype_port = -1;
     auto dtype = element::f32;
     bool dtype_applied = false;
     auto num_inputs = context.get_input_size();
@@ -41,31 +43,37 @@ OutputVector translate_arange(NodeContext& context) {
         // aten::arange(Scalar end, ScalarType dtype, Layout, Device, bool pin_memory)
         end = context.get_input(0);
         out_tensor = end;
-        if (!context.input_is_none(1)) {
-            dtype = convert_dtype(context.const_input<int64_t>(1));
-            dtype_applied = true;
-        }
+        dtype_port = 1;
     } else if (num_inputs == 6) {
         // aten::arange(Scalar start, Scalar end, ScalarType dtype, Layout, Device, bool pin_memory)
         start = context.get_input(0);
         end = context.get_input(1);
         out_tensor = end;
-        if (!context.input_is_none(2)) {
-            dtype = convert_dtype(context.const_input<int64_t>(2));
-            dtype_applied = true;
-        }
+        dtype_port = 2;
+        dtype_applied = true;
     } else if (num_inputs == 7) {
         // aten::arange(Scalar start, Scalar end, Scalar step, ScalarType dtype, Layout, Device, bool pin_memory)
         start = context.get_input(0);
         end = context.get_input(1);
         step = context.get_input(2);
         out_tensor = end;
-        if (!context.input_is_none(3)) {
-            dtype = convert_dtype(context.const_input<int64_t>(3));
-            dtype_applied = true;
-        }
+        dtype_port = 3;
+        dtype_applied = true;
     } else {
         FRONT_END_OP_CONVERSION_CHECK(false, "Not expected number of inputs for ", context.get_op_type());
+    }
+    if (dtype_port >= 0 && !context.input_is_none(dtype_port)) {
+        if (std::dynamic_pointer_cast<v0::Constant>(
+                context.get_input_from_visible_context(dtype_port).get_node_shared_ptr())) {
+            dtype = convert_dtype(context.const_input<int64_t>(dtype_port));
+            dtype_applied = true;
+        } else if (const auto& fw_node =
+                       cast_fw_node(context.get_input(dtype_port).get_node_shared_ptr(), "prim::dtype")) {
+            out_tensor = fw_node->input_value(0);
+            dtype_applied = false;
+        } else {
+            FRONT_END_OP_CONVERSION_CHECK(false, "Couldn't get dtype input");
+        }
     }
     auto r_end = context.mark_node(std::make_shared<v0::Convert>(end, dtype));
     auto r_start = context.mark_node(std::make_shared<v0::Convert>(start, dtype));
