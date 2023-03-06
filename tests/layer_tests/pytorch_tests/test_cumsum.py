3adce5a05497e26d05680e721e74de5c9974bc87
eaidova <ekaterina.aidova@intel.com>
2023-03-06 09:44:42 +0400

    handle out and prim::dtype

--- tests/layer_tests/pytorch_tests/test_cumsum.py
+++ tests/layer_tests/pytorch_tests/test_cumsum.py
@@ -7,11 +7,18 @@ from pytorch_layer_test_class import PytorchLayerTest
 
 
 class TestCumSum(PytorchLayerTest):
-    def _prepare_input(self):
+    def _prepare_input(self, out=False, out_dtype=None):
         import numpy as np
-        return (np.random.randn(1, 3, 224, 224).astype(np.float32),)
+        x = np.random.randn(1, 3, 224, 224).astype(np.float32)
+        if not out:
+            return (x, )
+        y =  np.random.randn(1, 3, 224, 224).astype(np.float32)
+        if out_dtype is not None:
+            y = y.astype(out_dtype)
+        return (x, y)
 
-    def create_model(self, axis, dtype_str):
+
+    def create_model(self, axis, dtype_str, out, dtype_from_input):
         import torch
 
         dtypes = {
@@ -26,26 +33,41 @@ class TestCumSum(PytorchLayerTest):
         dtype = dtypes.get(dtype_str)
 
         class aten_cumsum(torch.nn.Module):
-            def __init__(self, axis, dtype):
+            def __init__(self, axis, dtype, out=False, dtype_from_input=False):
                 super(aten_cumsum, self).__init__()
                 self.axis = axis
                 self.dtype = dtype
+                if dtype_from_input:
+                    self.forward_out = self.forward_out_prim_dtype
+                if out:
+                    self.forward =  self.forward_out
                 if self.dtype is not None:
-                    self.forward = self.forward_dtype
+                    if not dtype_from_input: 
+                        self.forward = self.forward_dtype if not out else self.forward_out_dtype
 
             def forward(self, x):
                 return torch.cumsum(x, self.axis)
             
             def forward_dtype(self, x):
                 return torch.cumsum(x, self.axis, dtype=self.dtype)
+            
+            def forward_out(self, x, y):
+                return y, torch.cumsum(x, self.axis, out=y)
+
+            def forward_out_dtype(self, x, y):
+                return y, torch.cumsum(x, self.axis, dtype=self.dtype, out=y)
+
+            def forward_out_prim_dtype(self, x, y):
+                return y, torch.cumsum(x, self.axis, dtype=y.dtype, out=y)
 
         ref_net = None
 
-        return aten_cumsum(axis, dtype), ref_net, "aten::cumsum"
+        return aten_cumsum(axis, dtype, out, dtype_from_input), ref_net, "aten::cumsum"
 
     @pytest.mark.parametrize("axis", [0, 1, 2, 3, -1, -2, -3, -4])
     @pytest.mark.parametrize("dtype", [None, "float32", "float64", "int32", "int64", "int8"])
+    @pytest.mark.parametrize("out,dtype_from_input", [(False, False), (True, False), (True, True)])
     @pytest.mark.nightly
     @pytest.mark.precommit
-    def test_cumsum(self, axis, dtype, ie_device, precision, ir_version):
-        self._test(*self.create_model(axis, dtype), ie_device, precision, ir_version)
+    def test_cumsum(self, axis, dtype, out, dtype_from_input, ie_device, precision, ir_version):
+        self._test(*self.create_model(axis, dtype, out, dtype_from_input), ie_device, precision, ir_version, kwargs_to_prepare_input={"out": out, "out_dtype": dtype})
