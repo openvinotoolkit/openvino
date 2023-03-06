052765193324e7d21dd843b9a07ec705bc623586
Shen, Wanglei <wanglei.shen@intel.com>
2023-03-06 14:21:33 +0800

    fix code style issue

--- src/inference/include/ie/cpu/cpu_config.hpp
+++ src/inference/include/ie/cpu/cpu_config.hpp
@@ -46,19 +46,21 @@ DECLARE_CPU_CONFIG_KEY(SPARSE_WEIGHTS_DECOMPRESSION_RATE);
  * @brief The name for defining processor type used for CPU inference.
  *
  *  - UNDEFINED: default setting may vary by platform and performance hints
- *  - ALL_CORE: all processors can be used. If hyper threading is enabled, both processors ofoneperformance-corewill be used.
+ *  - ALL_CORE: all processors can be used. If hyper threading is enabled, both processors ofoneperformance-corewill be
+ *              used.
  *  - PHY_CORE_ONLY: only one processor can be used per CPU core even with hyper threading enabled.
- *  - P_CORE_ONLY: only processors of performance-cores can be used. If hyper threading is enabled, both processors of one performance-core will be used.
+ *  - P_CORE_ONLY: only processors of performance-cores can be used. If hyper threading is enabled, both processors of
+ *                 one performance-core will be used.
  *  - E_CORE_ONLY: only processors of efficient-cores can be used.
  *  - PHY_P_CORE_ONLY: only one processor can be used per performance-core even with hyper threading enabled.
  */
 DECLARE_CPU_CONFIG_KEY(PROCESSOR_TYPE);
-DECLARE_CPU_CONFIG_VALUE(UNDEFINED);   
-DECLARE_CPU_CONFIG_VALUE(ALL_CORE);   
-DECLARE_CPU_CONFIG_VALUE(PHY_CORE_ONLY);   
-DECLARE_CPU_CONFIG_VALUE(P_CORE_ONLY);   
-DECLARE_CPU_CONFIG_VALUE(E_CORE_ONLY);   
-DECLARE_CPU_CONFIG_VALUE(PHY_P_CORE_ONLY);   
+DECLARE_CPU_CONFIG_VALUE(UNDEFINED);
+DECLARE_CPU_CONFIG_VALUE(ALL_CORE);
+DECLARE_CPU_CONFIG_VALUE(PHY_CORE_ONLY);
+DECLARE_CPU_CONFIG_VALUE(P_CORE_ONLY);
+DECLARE_CPU_CONFIG_VALUE(E_CORE_ONLY);
+DECLARE_CPU_CONFIG_VALUE(PHY_P_CORE_ONLY);
 
 }  // namespace CPUConfigParams
 }  // namespace InferenceEngine
