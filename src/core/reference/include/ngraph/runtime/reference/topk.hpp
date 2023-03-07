31f1f5384b4633730de7421455cc75be7adc685f
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2023-03-07 14:32:55 +0100

    Stabilize ascending comparison of ref impl

--- src/core/reference/include/ngraph/runtime/reference/topk.hpp
+++ src/core/reference/include/ngraph/runtime/reference/topk.hpp
@@ -16,7 +16,7 @@ namespace runtime {
 namespace reference {
 // Had to split out these two functions. They used to be lambda expressions but
 // MSVC had difficulty compiling. This way is more explicit.
-template <typename T, typename U>
+template <bool D, typename T, typename U>
 inline bool compare_max(const std::tuple<T, U>& a, const std::tuple<T, U>& b) {
 // this is intentional to be able to compare floats directly
 // without using relative or absolute tolerance
@@ -30,12 +30,11 @@ inline bool compare_max(const std::tuple<T, U>& a, const std::tuple<T, U>& b) {
 #if defined(__GNUC__)
 #    pragma GCC diagnostic pop
 #endif
-    return a > b;
-}
 
-template <typename T, typename U>
-inline bool compare_min(const std::tuple<T, U>& a, const std::tuple<T, U>& b) {
-    return a < b;
+    if (D)
+        return a > b;
+    else
+        return a < b;
 }
 
 template <typename T, typename U>
@@ -87,9 +86,9 @@ void topk(const T* arg,
         }
         // Sort the temp vector
         if (compute_max) {
-            nth_element(workspace.begin(), workspace.begin() + k, workspace.end(), compare_max<T, U>);
+            nth_element(workspace.begin(), workspace.begin() + k, workspace.end(), compare_max<true, T, U>);
         } else {
-            nth_element(workspace.begin(), workspace.begin() + k, workspace.end(), compare_min<T, U>);
+            nth_element(workspace.begin(), workspace.begin() + k, workspace.end(), compare_max<false, T, U>);
         }
         // Write temp vector to output
         switch (sort) {
@@ -100,9 +99,9 @@ void topk(const T* arg,
             break;
         case op::v1::TopK::SortType::SORT_VALUES:
             if (compute_max)
-                std::sort(workspace.begin(), workspace.begin() + k, compare_max<T, U>);
+                std::sort(workspace.begin(), workspace.begin() + k, compare_max<true, T, U>);
             else
-                std::sort(workspace.begin(), workspace.begin() + k, compare_min<T, U>);
+                std::sort(workspace.begin(), workspace.begin() + k, compare_max<false, T, U>);
         }
         for (size_t j = 0; j < k; j++) {
             tuple<T, U> entry = workspace[j];
