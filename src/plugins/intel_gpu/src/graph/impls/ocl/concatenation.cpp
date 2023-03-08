5f410db302c7631ea2ea6c0a06e01af210bbf818
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2023-03-08 17:11:57 +0400

    [GPU] Shape agnostic concat kernel

--- src/plugins/intel_gpu/src/graph/impls/ocl/concatenation.cpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl/concatenation.cpp
@@ -72,11 +72,38 @@ public:
 
         return {params, optional_params};
     }
+
+    void update_dispatch_data(const kernel_impl_params& impl_param) override {
+        auto kernel_params = get_kernel_params(impl_param);
+        (_kernel_data.update_dispatch_data_func)(kernel_params.first, _kernel_data);
+        update_kernels_list_to_skip();
+    }
 };
 
 namespace detail {
 
 attach_concatenation_impl::attach_concatenation_impl() {
+    auto dyn_types = {
+        data_types::i8,
+        data_types::u8,
+        data_types::f16,
+        data_types::f32,
+        data_types::i32,
+        data_types::i64
+    };
+
+    auto dyn_formats = {
+        format::bfyx,
+        format::bfzyx,
+        format::bfwzyx
+    };
+
+    implementation_map<concatenation>::add(impl_types::ocl,
+                                           shape_types::dynamic_shape,
+                                           typed_primitive_impl_ocl<concatenation>::create<concatenation_impl>,
+                                           dyn_types,
+                                           dyn_formats);
+
     implementation_map<concatenation>::add(impl_types::ocl, typed_primitive_impl_ocl<concatenation>::create<concatenation_impl>, {
         std::make_tuple(data_types::f32, format::yxfb),
         std::make_tuple(data_types::f16, format::yxfb),
