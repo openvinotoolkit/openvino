925728657e897fb4c63ee43035402ee1d9ec22a5
ynimmaga <yamini.nimmagadda@intel.com>
2023-03-08 14:27:29 -0800

    Added Torchscript Backend

---
+++ src/bindings/python/src/openvino/frontend/pytorch/torchdynamo/backends.py
@@ -0,0 +1,60 @@
+import logging
+from functools import partial
+
+import torch
+from torch._dynamo.backends.common import aot_autograd, mem_efficient_fusion_kwargs
+from torch._dynamo.backends.common import fake_tensor_unsupported
+from torch._dynamo.backends.registry import register_backend
+
+from openvino.runtime import Core, Type, PartialShape
+from openvino.frontend import FrontEndManager
+from openvino.frontend.pytorch.decoder import TorchScriptPythonDecoder
+
+log = logging.getLogger(__name__)
+
+@register_backend
+@fake_tensor_unsupported
+def openvino(subgraph, example_inputs):
+    return ts_openvino(subgraph, example_inputs)
+
+def ts_openvino(subgraph, example_inputs):
+    try:
+        model = torch.jit.script(subgraph)
+        model.eval()
+        fr_model = torch.jit.freeze(model)
+        
+        core = Core()
+        fe_manager = FrontEndManager()
+        fe = fe_manager.load_by_framework('pytorch')
+        dtype_mapping = {
+            torch.float32: Type.f32,
+            torch.float16: Type.f16,
+            torch.int64: Type.i64,
+            torch.int32: Type.i32,
+            torch.uint8: Type.u8,
+            torch.int8: Type.i8,
+            torch.bool: Type.boolean
+        }
+        decoder = TorchScriptPythonDecoder(model)
+
+        im = fe.load(decoder)
+        om = fe.convert(im)
+
+        for idx, input_data in enumerate(example_inputs):
+            om.inputs[idx].get_node().set_element_type(dtype_mapping[input_data.dtype])
+            om.inputs[idx].get_node().set_partial_shape(PartialShape(list(input_data.shape)))
+        om.validate_nodes_and_infer_types()
+
+        compiled_model = core.compile_model(om, 'CPU')
+
+        def _call(*args):
+            ov_inputs = [a.detach().cpu().numpy() for a in args]
+            try:
+                res = compiled_model(ov_inputs)
+            except Exception as e:
+                return subgraph.model.forward(*args)
+            result = [torch.from_numpy(res[out]) for out in compiled_model.outputs]
+            return result
+        return _call
+    except Exception as e:
+        return subgraph
