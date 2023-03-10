a35354218651efab990703743a882bffe641125a
Ilya Churaev <ilya.churaev@intel.com>
2023-03-10 13:19:19 +0400

    Changed samples throw exception

--- samples/cpp/benchmark_app/remote_tensors_filling.cpp
+++ samples/cpp/benchmark_app/remote_tensors_filling.cpp
@@ -61,7 +61,7 @@ void fill_buffer(void* inputBuffer, size_t elementsNum, const ov::element::Type&
     } else if (type == ov::element::boolean) {
         fill_buffer_random<uint8_t, uint32_t>(inputBuffer, elementsNum, 0, 1);
     } else {
-        throw ov::Exception("Requested type is not supported");
+        OPENVINO_THROW("Requested type is not supported");
     }
 }
 
@@ -124,7 +124,7 @@ std::map<std::string, ov::TensorVector> get_remote_input_tensors(
     }
     return remoteTensors;
 #else
-    throw ov::Exception("Device memory requested for GPU device, but OpenCL was not linked");
+    OPENVINO_THROW("Device memory requested for GPU device, but OpenCL was not linked");
 #endif
 }
 
@@ -183,7 +183,7 @@ std::map<std::string, ov::Tensor> get_remote_output_tensors(const ov::CompiledMo
 
     return outputTensors;
 #else
-    throw ov::Exception("Device memory requested for GPU device, but OpenCL was not linked");
+    OPENVINO_THROW("Device memory requested for GPU device, but OpenCL was not linked");
 #endif
 }
 }  // namespace gpu
