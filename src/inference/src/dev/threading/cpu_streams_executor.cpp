e8e2f49ebf2b5da3888ebae460707e25c3b5f502
xufang <fang.xu@intel.com>
2023-03-11 16:29:05 +0800

    update prebuilt tbbbind static library for linux
    cmake 3.23.2
    centos7
    
    https://github.com/open-mpi/hwloc/archive/refs/tags/hwloc-2.8.0.tar.gz
    ./autogen.sh
    ./configure --enable-static --disable-io --disable-libudev --disable-libxml2 --disable-cairo CFLAGS="-fPIE"
    make -j$(proc)
    make install prefix=$(pwd)/install
    
    https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.7.0.tar.gz
    sed -i "s/APPLE\s*OR\s*NOT\s*BUILD_SHARED_LIBS/APPLE/g" CMakeLists.txt
    export HWLOC_DIR="${hwloc_root_dir}/hwloc-hwloc-2.8.0/install"
    export PKG_CONFIG_PATH="${HWLOC_DIR}/lib/pkgconfig"
    export CXXFLAGS="-I ${HWLOC_DIR}/include -L ${HWLOC_DIR}/lib"
    ~/cmake-3.23.2-linux-x86_64/bin/cmake -DTBB_TEST=OFF -DTBB_BUILD=OFF -DTBBMALLOC_BUILD=OFF -DBUILD_SHARED_LIBS=OFF
    make -j$(nproc)

--- src/inference/src/dev/threading/cpu_streams_executor.cpp
+++ src/inference/src/dev/threading/cpu_streams_executor.cpp
@@ -133,9 +133,13 @@ struct CPUStreamsExecutor::Impl {
                             ? (small_core ? small_core_offset : (logic_core ? 0 : 1))
                             : 0;
 
+#    ifdef _WIN32
                     _taskArena.reset(new custom::task_arena{custom::task_arena::constraints{}
                                                                 .set_core_type(selected_core_type)
                                                                 .set_max_concurrency(max_concurrency)});
+#    else
+                    _taskArena.reset(new custom::task_arena{max_concurrency});
+#    endif
                     CpuSet processMask;
                     int ncpus = 0;
                     std::tie(processMask, ncpus) = get_process_mask();
