690b2110d4ca515ecb9eca53752b472dac30bb7a
Yan, Xiping <xiping.yan@intel.com>
2023-03-14 08:33:25 +0800

    Update comments, gave more clear explanation.
    Signed-off-by: Yan, Xiping <xiping.yan@intel.com>

--- src/plugins/intel_cpu/src/nodes/matmul.cpp
+++ src/plugins/intel_cpu/src/nodes/matmul.cpp
@@ -633,9 +633,10 @@ void MatMul::prepareParams() {
                 break;
             }
             if (!itpd.next_impl()) {
-                // implType(AMX) will be not in list when input dynamic shape for AMX.
-                // Bcause AMX will be available when it assumes batch 16 for dynamic shape in compile model.
-                // However if we only pass batch 1 in runtime, AMX will be not available(Performance is low).
+                // In case of dynamic shapes an implementation type chosen as optimal for a primitive_desc with
+                // undefined input shapes, is not necessarily available for the primitive_desc with defined shape.
+                // Example: brgemm_avx512_amx (Intel Sapphire Rapids Platform) is available for a primitive with
+                // undefined input shapes but not available for primitive_desc with input batch 1.
                 prim_desc = itpd_first.get();
                 break;
             }
