06f393f9fa25cd76717d02b5cfa08545d6b57045
PiotrKrzem <piotr.krzeminski@intel.com>
2023-03-15 23:29:50 +0100

    [PT FE] Fix tests for zeros

--- tests/layer_tests/pytorch_tests/test_zeros.py
+++ tests/layer_tests/pytorch_tests/test_zeros.py
@@ -5,31 +5,28 @@ import torch
 from pytorch_layer_test_class import PytorchLayerTest
 
 class aten_zeros(torch.nn.Module):
-    def __init__(self, dtype) -> None:
+    def __init__(self, size, dtype) -> None:
         torch.nn.Module.__init__(self)
+        self.size = size
         self.dtype = dtype
 
-    def forward(self, input_tensor):
-        return torch.zeros(input_tensor, 
-            dtype = self.dtype,
-            layout = torch.strided,
-            device = None,
-            requires_grad = False
+    def forward(self, x):
+        return torch.zeros([self.size], 
+            dtype = self.dtype
         )
     
 class TestChunk(PytorchLayerTest):
     def _prepare_input(self):
-        return (np.random.randint(100),)
+        return (None,)
     
     @pytest.mark.parametrize("dtype", [
         torch.int32,
         torch.int16,
-        torch.uint16,
         torch.bool,
         torch.float32,
     ])
     @pytest.mark.nightly
     @pytest.mark.precommit
     def test_zeros(self, dtype, ie_device, precision, ir_version):
-        self._test(aten_zeros(dim, chunks), None, ["aten::zeros"], 
+        self._test(aten_zeros(np.random.randint(100), dtype), None, ["aten::zeros"], 
                 ie_device, precision, ir_version)
