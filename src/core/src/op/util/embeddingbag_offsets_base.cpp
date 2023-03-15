f0c153858bfcd582794bd28565d4e31310d394d2
Katarzyna Mitrus <katarzyna.mitrus@intel.com>
2023-03-15 11:12:57 +0100

    [ShapeInference] EmbeddingBag-Offsets/Packed-Sum shape infer improvements (#16072)
    * shape_infer
    
    * Register EmbeddingBagPackedSum shape_nfer for CPU
    
    * Tests
    
    * Merge shapes to preserve 2rd input info
    
    * More label tests
    
    * Add emb_table size check
    
    * rename shape infer file
    
    * Add more tests
    
    * Update constexpr
    
    * Use OV_EXPECT_THROW
    
    * Style
    
    * Reuse emb_table for dynamic rank
    
    * Add common util to calculate emb output shape
    
    * Update embd shape infer to use common util
    
    * Update embedding shape infer util

--- src/core/src/op/util/embeddingbag_offsets_base.cpp
+++ src/core/src/op/util/embeddingbag_offsets_base.cpp
@@ -79,16 +79,9 @@ void ov::op::util::EmbeddingBagOffsetsBase::validate_and_infer_types() {
                               ")");
     }
 
-    element::Type result_et = get_input_element_type(EMB_TABLE);
-
-    std::vector<PartialShape> result_shapes = {PartialShape::dynamic()};
-    std::vector<PartialShape> input_shapes;
-    for (size_t i = 0; i < get_input_size(); i++)
-        input_shapes.push_back(get_input_partial_shape(i));
-
-    shape_infer(this, input_shapes, result_shapes);
-
-    set_output_type(0, result_et, result_shapes[0]);
+    const auto& result_et = get_input_element_type(EMB_TABLE);
+    const auto input_shapes = get_node_input_partial_shapes(*this);
+    set_output_type(0, result_et, shape_infer(this, input_shapes)[0]);
 }
 
 bool ov::op::util::EmbeddingBagOffsetsBase::visit_attributes(AttributeVisitor& visitor) {
