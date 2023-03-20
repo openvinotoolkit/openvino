ad629e93959717ff99020805631474fe9b226bf4
p-wysocki <przemyslaw.wysocki@intel.com>
2023-03-20 12:25:17 +0100

    Minor fix

--- src/bindings/python/tests/test_runtime/test_infer_request.py
+++ src/bindings/python/tests/test_runtime/test_infer_request.py
@@ -1092,11 +1092,10 @@ def test_mixed_scalar_infer(device, shared_flag, input_data):
 ])
 def test_mixed_dynamic_infer(device, shared_flag, input_data):
 
+    core = Core()
     if device == "CPU":
         if "Intel" not in core.get_property(device, "FULL_DEVICE_NAME"):
             pytest.skip("Arm Plugin doesn't support inputs having dynamic shapes.")
-
-    core = Core()
     param0 = ops.parameter([], np.float32, name="data0")
     param1 = ops.parameter(["?"], np.float32, name="data1")
     add = ops.add(param0, param1, name="add")
