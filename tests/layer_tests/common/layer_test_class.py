95606daceea92141aa9db94862d4a28f31511705
Pavel Esir <pavel.esir@intel.com>
2023-03-20 15:01:23 +0100

    fix layer_test_class.py

--- tests/layer_tests/common/layer_test_class.py
+++ tests/layer_tests/common/layer_test_class.py
@@ -40,10 +40,11 @@ class CommonLayerTest:
         os.environ['MO_ENABLED_TRANSFORMS'] = enabled_transforms
         os.environ['MO_DISABLED_TRANSFORMS'] = disabled_transforms
 
+        compress_to_fp16 = False if 'data_type' == 'FP32' else False
         mo_params = {self.input_model_key: model_path,
                      "output_dir": temp_dir,
-                     "data_type": precision, "model_name": 'model'
-                     }
+                     "compress_to_fp16": compress_to_fp16,
+                     "model_name": 'model'}
 
         if 'input_shapes' in kwargs and len(kwargs['input_shapes']):
             input_shapes_str = []
