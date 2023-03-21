a0cb3fb27e884e12aa79c472fef4850a1887f307
Mateusz Bencer <mateusz.bencer@intel.com>
2023-03-21 17:49:20 +0100

    tensor iterator version

--- src/frontends/onnx/frontend/src/op/stft.cpp
+++ src/frontends/onnx/frontend/src/op/stft.cpp
@@ -19,7 +19,7 @@ OutputVector stft(const Node& node) {
     const auto tensor_iterator = std::make_shared<default_opset::TensorIterator>();
     const OutputVector ng_inputs{node.get_ng_inputs()};
     const auto& signal = ng_inputs.at(0);
-    const auto signal_param = std::make_shared<default_opset::Parameter>();
+    std::cout << "signal: " << signal << "\n";
     const auto dft_length_provided = ng_inputs.size() > 3 && !ngraph::op::is_null(ng_inputs[3]);
     const auto onesided = node.get_attribute_value<int64_t>("onesided", 0);
     const int64_t axis = 1;
@@ -27,16 +27,25 @@ OutputVector stft(const Node& node) {
     const auto& frame_step_node = ng_inputs.at(1);
     CHECK_VALID_NODE(node,
                     ngraph::op::is_constant(frame_step_node.get_node_shared_ptr()),
-                    "Non-constant frame_step input is not supported.");
+                    "Non-constant frame_step input is not supported."); // TODO: CHECK IF SCALAR
     const auto frame_step = ov::as_type_ptr<default_opset::Constant>(frame_step_node.get_node_shared_ptr())->cast_vector<int64_t>()[0];
+    auto signal_param_shape = signal.get_partial_shape();
+    CHECK_VALID_NODE(node,
+                    signal_param_shape.rank().is_static() && signal_param_shape.rank().get_length() > axis && signal_param_shape[axis].is_static(),
+                    "Non-constant frame_step input is not supported."); // TODO: CHECK IF SCALAR
+    std::cout << "shape before: " << signal_param_shape << "\n";
+    signal_param_shape[axis] /= frame_step;
+    std::cout << "shape after: " << signal_param_shape << "\n";
+    const auto signal_param = std::make_shared<default_opset::Parameter>(signal.get_element_type(), signal_param_shape);
 
     const auto dft = dft::make_dft(signal_param, dft_length_provided ? ng_inputs.at(1) : std::make_shared<NullNode>(), axis, false, onesided == 1);
-    const auto body = std::make_shared<ov::Model>(OutputVector{dft}, ParameterVector{signal_param});
+    const auto dft_res = std::make_shared<default_opset::Result>(dft);
+    const auto body = std::make_shared<ov::Model>(ResultVector{dft_res}, ParameterVector{signal_param});
 
     tensor_iterator->set_body(body);
     tensor_iterator->set_sliced_input(signal_param, signal, 0, 1, frame_step, -1, axis);
 
-    return {tensor_iterator};
+    return {tensor_iterator->get_concatenated_slices(dft_res, 0, 1, frame_step, -1, axis)};
 }
 
 }  // namespace set_17
