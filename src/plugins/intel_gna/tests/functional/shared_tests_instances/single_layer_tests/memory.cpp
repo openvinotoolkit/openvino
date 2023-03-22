877067e6a725dd9816d8e7980c974d13d9e5dae3
Maciej Kwapulinski <maciej.kwapulinski@intel.com>
2023-03-22 11:37:21 +0100

    relative threshold for smoke_MemoryTest suite adjusted for GNA

--- src/plugins/intel_gna/tests/functional/shared_tests_instances/single_layer_tests/memory.cpp
+++ src/plugins/intel_gna/tests/functional/shared_tests_instances/single_layer_tests/memory.cpp
@@ -10,6 +10,24 @@ using namespace LayerTestsDefinitions;
 
 namespace {
 
+class MemoryTestGna : public MemoryTest {
+    using Super = MemoryTest;
+
+protected:
+    void SetUp() override {
+        // 'smoke_MemoryTest' suite does not fit well to GNA quantization.
+        // Here input values get multiplied for each iteration present in test.
+        // This makes scale computation impossible based on first layer.
+        // Manual setting for relative threshold has thus been used.
+        threshold = 0.936032;
+        Super::SetUp();
+    }
+};
+
+TEST_P(MemoryTestGna, CompareWithRefs) {
+    Run();
+}
+
 std::vector<ngraph::helpers::MemoryTransformation> transformation{
     ngraph::helpers::MemoryTransformation::NONE,
     ngraph::helpers::MemoryTransformation::LOW_LATENCY_V2,
@@ -23,7 +41,7 @@ const std::vector<InferenceEngine::Precision> inputPrecisions = {InferenceEngine
 const std::vector<int64_t> iterationCount{1, 3, 4, 10};
 
 INSTANTIATE_TEST_SUITE_P(smoke_MemoryTest,
-                         MemoryTest,
+                         MemoryTestGna,
                          ::testing::Combine(::testing::ValuesIn(transformation),
                                             ::testing::ValuesIn(iterationCount),
                                             ::testing::ValuesIn(inShapes),
