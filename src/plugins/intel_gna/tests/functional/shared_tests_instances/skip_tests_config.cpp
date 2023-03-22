903d4b948899a021eaa0b6f8d96a15f9757e0f41
Maciej Kwapulinski <maciej.kwapulinski@intel.com>
2023-03-22 11:38:50 +0100

    smoke_MemoryTest suite enabled

--- src/plugins/intel_gna/tests/functional/shared_tests_instances/skip_tests_config.cpp
+++ src/plugins/intel_gna/tests/functional/shared_tests_instances/skip_tests_config.cpp
@@ -43,12 +43,6 @@ std::vector<std::string> disabledTestPatterns() {
         R"(.*importExportedIENetworkConstantResultOnly.*)",
         R"(.*importExportedIENetworkParameterResultOnly.*)",
 
-        // TODO: Issue 57368 (accuracy)
-        R"(.*smoke_MemoryTest.*LOW_LATENCY.*IS=\(1.10\).*)",
-        R"(.*smoke_MemoryTest.*iteration_count=3.*IS=\(1.10\).*)",
-        R"(.*smoke_MemoryTest.*iteration_count=4.*IS=\(1.10\).*)",
-        R"(.*smoke_MemoryTest.*iteration_count=10.*IS=\(1.10\).*)",
-        R"(.*smoke_MemoryTest.*LOW_LATENCY.*iteration_count=10.*IS=\(1.2\).*)",
         // Not implemented yet
         R"(.*Behavior.*ExecutableNetworkBaseTest.*(canSetConfigToExecNet|canSetConfigToExecNetWithIncorrectConfig).*)",
         R"(.*Behavior.*ExecutableNetworkBaseTest.*(CheckExecGraphInfoBeforeExecution|CheckExecGraphInfoAfterExecution|CheckExecGraphInfoSerialization).*)",
