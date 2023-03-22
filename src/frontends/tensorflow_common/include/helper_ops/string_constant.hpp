e33d7d7553dc91ae13820562c1e005fe7d938d5b
Krivoruchko <georgy.krivoruchko@intel.com>
2023-03-22 02:47:08 -0700

    Aligned behavior of StringConstant with UnsupportedConstant

--- src/frontends/tensorflow_common/include/helper_ops/string_constant.hpp
+++ src/frontends/tensorflow_common/include/helper_ops/string_constant.hpp
@@ -36,7 +36,7 @@ public:
     }
 
     void validate_and_infer_types() override {
-        set_output_type(0, ov::element::undefined, ov::PartialShape::dynamic());
+        set_output_type(0, ov::element::dynamic, ov::PartialShape::dynamic());
     }
 
     ov::Any get_data() {
