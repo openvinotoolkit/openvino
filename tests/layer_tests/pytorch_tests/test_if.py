620909f68f89d5df5c4dc5487db0e250292a85ca
Maxim Vafin <maxim.vafin@intel.com>
2023-03-23 10:49:43 +0100

    Fix test

--- tests/layer_tests/pytorch_tests/test_if.py
+++ tests/layer_tests/pytorch_tests/test_if.py
@@ -7,15 +7,6 @@ import numpy as np
 from pytorch_layer_test_class import PytorchLayerTest
 
 
-def not_yet_supported(value):
-    return pytest.param(
-        value,
-        marks=pytest.mark.xfail(
-            reason="Failed due to aten::sargsort not yet supporting stable sorting. Ticket 105242"
-        ),
-    )
-
-
 class TestIf(PytorchLayerTest):
     def _prepare_input(self):
         return (np.random.randn(1, 3, 224, 224).astype(np.float32), self.y)
@@ -30,16 +21,17 @@ class TestIf(PytorchLayerTest):
 
             def forward(self, x, y):
                 if y > 0:
-                    return x.new_empty((0, 10), dtype=torch.uint8)
-                return torch.zeros(x.shape[:2], dtype=torch.bool)
+                    res = x.new_empty((0, 10), dtype=torch.uint8)
+                else:
+                    res = torch.zeros(x.shape[:2], dtype=torch.bool)
+                return res.to(torch.bool)
 
         ref_net = None
 
         return prim_if(), ref_net, "prim::If"
 
     @pytest.mark.parametrize("y", [np.array(1),
-                                   # only the branch with upgraded type is supported, ticket 106627
-                                   not_yet_supported(np.array(-1))
+                                   np.array(-1)
                                    ])
     @pytest.mark.nightly
     @pytest.mark.precommit
