b0753b792547ea6c91867c601e563b7e9b1c5d16
Mateusz <mateusz.mikolajczyk@intel.com>
2023-03-24 13:26:46 +0000

    Make NodeContext const

--- src/frontends/pytorch/src/op/deform_conv.cpp
+++ src/frontends/pytorch/src/op/deform_conv.cpp
@@ -15,7 +15,7 @@ namespace op {
 
 using namespace ov::op;
 
-OutputVector translate_deform_conv(NodeContext& context) {
+OutputVector translate_deform_conv(const NodeContext& context) {
     // torchvision::deform_conv2d(Tensor input, Tensor weight, Tensor offset,
     //                            Tensor mask, Tensor bias, int64_t stride_h, int64_t stride_w,
     //                            int64_t pad_h, int64_t pad_w, int64_t dilation_h, int64_t dilation_w,
