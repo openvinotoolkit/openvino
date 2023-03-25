d4800ef788f1476d73784ab9a4c8aedff92ffb61
Shen, Wanglei <wanglei.shen@intel.com>
2023-03-26 02:07:19 +0800

    update header file

--- src/plugins/intel_cpu/src/cpu_streams_calculation.hpp
+++ src/plugins/intel_cpu/src/cpu_streams_calculation.hpp
@@ -13,7 +13,6 @@
 
 namespace ov {
 namespace intel_cpu {
-
 /**
  * @brief      Generate streams information table according to processors type table.
  * @param[in]  input_streams is the targeted number of streams set by user via ov::num_streams or hints.
