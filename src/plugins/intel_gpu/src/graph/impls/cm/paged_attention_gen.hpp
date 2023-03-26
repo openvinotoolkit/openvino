799d619e9900d41e4d476ac6b8388d9569663cdb
Wangwang Wang <wangwang.wang@intel.com>
2023-03-27 01:08:18 +0800

    Configure BLOCK_SG_M/N for different platforms

--- src/plugins/intel_gpu/src/graph/impls/cm/paged_attention_gen.hpp
+++ src/plugins/intel_gpu/src/graph/impls/cm/paged_attention_gen.hpp
@@ -31,8 +31,13 @@ constexpr auto get_pa_build_options() {
 #define PA_KV_CACHE_BLOCK_SIZE       16
 #define PA_KV_CACHE_BLOCK_SIZE_XATTN 256
 
+#ifdef CM_HAS_LSC_UNTYPED_2D
+constexpr uint32_t BLOCK_SG_M = 64;
+constexpr uint32_t BLOCK_SG_N = 32;
+#else
 constexpr uint32_t BLOCK_SG_M = 32;
 constexpr uint32_t BLOCK_SG_N = 16;
+#endif
 constexpr uint32_t SG_M = 4;
 constexpr uint32_t SG_N = 8;
 constexpr uint32_t BLOCK_WG_M = BLOCK_SG_M * SG_M;
