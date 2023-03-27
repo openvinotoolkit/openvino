440ba8205e07e614f0124d115dcf2bfa3e352529
mitruska <katarzyna.mitrus@intel.com>
2023-03-27 23:03:52 +0200

    Revert interpolate-11 core evaluate support

--- src/core/include/openvino/op/interpolate.hpp
+++ src/core/include/openvino/op/interpolate.hpp
@@ -212,15 +212,10 @@ public:
 
     void validate_and_infer_types() override;
 
-    OPENVINO_SUPPRESS_DEPRECATED_START
-    bool evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const override;
-    OPENVINO_SUPPRESS_DEPRECATED_END
-    bool has_evaluate() const override;
-
     std::shared_ptr<Node> clone_with_new_inputs(const OutputVector& new_args) const override;
-
-private:
-    bool evaluate_interpolate(const HostTensorVector& outputs, const HostTensorVector& inputs) const;
+    bool has_evaluate() const override {
+        return false;
+    }
 };
 }  // namespace v11
 }  // namespace op
