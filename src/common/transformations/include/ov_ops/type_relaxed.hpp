833e7b7d5f9b9fba6feb84eb4983e456ee678159
Zhai, Xuejun <xuejun.zhai@intel.com>
2023-03-27 10:40:10 +0800

    Revert "Remove set_element_type() from TemporaryReplaceOutputType class"
    This reverts commit e0b8027742676a7ccf3d8ba9e5e158cba94e3af2.

--- src/common/transformations/include/ov_ops/type_relaxed.hpp
+++ src/common/transformations/include/ov_ops/type_relaxed.hpp
@@ -8,7 +8,6 @@
 #include <memory>
 #include <mutex>
 #include <openvino/op/convert.hpp>
-#include <openvino/op/parameter.hpp>
 #include <string>
 #include <transformations_visibility.hpp>
 #include <vector>
@@ -159,10 +158,9 @@ public:
     TemporaryReplaceOutputType(Output<Node> output, element::Type tmp_type) : m_output(output) {
         // save original element type in order to restore it in the destructor
         orig_type = m_output.get_element_type();
-        if (auto parameter = dynamic_cast<ov::op::v0::Parameter*>(m_output.get_node())) {
-            parameter->set_element_type(tmp_type);
-            parameter->validate_and_infer_types();
-        }
+        OPENVINO_SUPPRESS_DEPRECATED_START
+        m_output.get_tensor().set_element_type(tmp_type);
+        OPENVINO_SUPPRESS_DEPRECATED_END
     }
 
     /// Return the output port that was used in the constructor
@@ -172,10 +170,9 @@ public:
 
     /// Restores the original element type for the output
     ~TemporaryReplaceOutputType() {
-        if (auto parameter = dynamic_cast<ov::op::v0::Parameter*>(m_output.get_node())) {
-            parameter->set_element_type(orig_type);
-            parameter->validate_and_infer_types();
-        }
+        OPENVINO_SUPPRESS_DEPRECATED_START
+        m_output.get_tensor().set_element_type(orig_type);
+        OPENVINO_SUPPRESS_DEPRECATED_END
     }
 };
 
