85c0be859579ce95a7c26a3bfe4f919ea6e2555d
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2023-03-27 12:28:35 +0200

    m_subgraph_cpu_map -> m_input_output_subgraphs

--- src/plugins/intel_gna/src/gna_plugin.hpp
+++ src/plugins/intel_gna/src/gna_plugin.hpp
@@ -52,10 +52,10 @@ protected:
 
     uint32_t activeLayerIndex = 0xffffffff;
     // TODO: transpose_inputs_info and transpose_outputs_info should be moved to GNAModelSerial class when ngraph
-    // migration is finished. Those filed are needed to support the exported models <= 2.8.
+    // migration is finished. Those structures are needed to support the exported models <= 2.8.
     pre_post_processing::TranspositionInfoMap transpose_inputs_info;
     pre_post_processing::TranspositionInfoMap transpose_outputs_info;
-    PrePostProcessModels m_subgraph_cpu_map;
+    PrePostProcessModels m_input_output_subgraphs;
 
     uint32_t dnn_dump_write_index = 0;
     intel_dnn_number_type_t output_type = kDnnInt;
