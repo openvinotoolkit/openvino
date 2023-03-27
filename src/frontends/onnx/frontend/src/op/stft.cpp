65967db734d2a746ce314f243c235cd06680808a
Mateusz Bencer <mateusz.bencer@intel.com>
2023-03-27 11:20:11 +0200

    support of batch>1

--- src/frontends/onnx/frontend/src/op/stft.cpp
+++ src/frontends/onnx/frontend/src/op/stft.cpp
@@ -32,9 +32,10 @@ OutputVector stft(const Node& node) {
     const auto signal_param_shape = signal.get_partial_shape();
     CHECK_VALID_NODE(node,
                      signal_param_shape.rank().is_static() && signal_param_shape.rank().get_length() > axis &&
-                         signal_param_shape[axis].is_static() && (signal_param_shape.cend()-1)->is_static(),
-                     "Shape of DFT axis and the last dimension must be known.");  // TODO: CHECK IF SCALAR
+                         signal_param_shape[axis].is_static() && (signal_param_shape.cend()-1)->is_static() && signal_param_shape[0].is_static(),
+                     "Shape of DFT axis, batch and the last dimension must be known.");  // TODO: CHECK IF SCALAR
 
+    const int64_t batch_size = signal_param_shape[0].get_length();
     int64_t frame_length = signal_param_shape[axis].get_length() / frame_step; // default value
     if(dft_length_provided) {
         const auto& frame_length_node = ng_inputs[3];
@@ -44,26 +45,30 @@ OutputVector stft(const Node& node) {
         frame_length = ov::as_type_ptr<default_opset::Constant>(frame_length_node.get_node_shared_ptr())->cast_vector<int64_t>()[0];
     }
     const int64_t nstfts = std::floor((signal_param_shape[axis].get_length() - frame_length) / frame_step) + 1;
-    //std::cout << "nstfts: " << nstfts  << ", len: " << signal_param_shape[axis].get_length() << ", frame_length: " << frame_length << ", frame_step: " << frame_step << std::endl;
+    //std::cout << "nstfts: " << nstfts << ", shape: " << signal_param_shape << ", len: " << signal_param_shape[axis].get_length() << ", frame_length: " << frame_length << ", frame_step: " << frame_step << std::endl;
     const auto axis_const = default_opset::Constant::create(element::i64, {}, {axis});
     const auto zero_const = default_opset::Constant::create(element::i64, {}, {0});
     const auto is_complex_signal = (signal_param_shape.cend()-1)->get_length() == 2;
-    ov::OutputVector concatenated_dft;
-    for (int i = 0; i < nstfts; ++i) {
-        std::vector<int64_t> indices(frame_length);
-        std::iota(std::begin(indices), std::end(indices), i * frame_step);
-        const auto indices_const = default_opset::Constant::create(element::i64, Shape{indices.size()}, indices);
-        const auto gather = std::make_shared<default_opset::Reshape>(std::make_shared<default_opset::Gather>(signal, indices_const, axis_const), is_complex_signal ? default_opset::Constant::create(element::i64, {2},  {-1, 2}) : default_opset::Constant::create(element::i64, {1},  {-1}), false);
-        //std::cout << "fft input shape: " << gather->get_output_partial_shape(0) << std::endl;
-        const auto dft = dft::make_dft(gather,
-                      dft_length_provided ? ng_inputs.at(1) : std::make_shared<NullNode>(),
-                      0,
-                      false,
-                      onesided == 1);
-        concatenated_dft.push_back(std::make_shared<default_opset::Unsqueeze>(dft, zero_const));
-        //std::cout << "fft output shape: " << concatenated_dft.back().get_partial_shape() << std::endl;
+    const auto step = default_opset::Constant::create(element::i64, Shape{2}, {1,1});
+    ov::OutputVector all_signals;
+    for (int64_t batch = 0; batch < batch_size; ++batch) {
+        ov::OutputVector signals_in_batch;
+        for (int64_t sig_idx = 0; sig_idx < nstfts; ++sig_idx) {
+            const auto start = default_opset::Constant::create(element::i64, Shape{2}, std::vector<int64_t>{batch, sig_idx*frame_step});
+            const auto stop = default_opset::Constant::create(element::i64, Shape{2}, std::vector<int64_t>{batch+1, sig_idx*frame_step+frame_length});
+            const auto slice_axes = default_opset::Constant::create(element::i64, Shape{2}, std::vector<int64_t>{0, axis});
+            const auto slice = std::make_shared<default_opset::Slice>(signal, start, stop, step, slice_axes);
+            const auto flatten_slice = std::make_shared<default_opset::Reshape>(slice, is_complex_signal ? default_opset::Constant::create(element::i64, {2},  {-1, 2}) : default_opset::Constant::create(element::i64, {1},  {-1}), false);
+            const auto dft = dft::make_dft(flatten_slice,
+                        dft_length_provided ? ng_inputs.at(1) : std::make_shared<NullNode>(),
+                        0,
+                        false,
+                        onesided == 1);
+            signals_in_batch.push_back(std::make_shared<default_opset::Unsqueeze>(dft, zero_const));
+        }
+        all_signals.push_back(std::make_shared<default_opset::Unsqueeze>(std::make_shared<default_opset::Concat>(signals_in_batch, 0), zero_const));
     }
-    return {std::make_shared<default_opset::Unsqueeze>(std::make_shared<default_opset::Concat>(concatenated_dft, 0), zero_const)}; // TODO: Try to optimize this with previous Unsqueeze
+    return {std::make_shared<default_opset::Concat>(all_signals, 0)};
 }
 
 }  // namespace set_17
