9fc863c57e63141dc400fa573dd2a578929b9445
Irina Efode <irina.efode@intel.com>
2023-03-28 22:06:26 +0400

    Trigger linux build

--- src/tests/functional/plugin/conformance/test_runner/conformance_infra/src/main.cpp
+++ src/tests/functional/plugin/conformance/test_runner/conformance_infra/src/main.cpp
@@ -22,6 +22,7 @@
 using namespace ov::test::conformance;
 
 int main(int argc, char* argv[]) {
+    // just trigger linux build 
     // Workaround for Gtest + Gflag
     std::vector<char*> argv_gflags_vec;
     int argc_gflags = 0;
