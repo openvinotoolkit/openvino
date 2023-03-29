f1f14e74b01ae5ee641accf5fec6f6e4784f1f8f
Katarzyna Mitrus <katarzyna.mitrus@intel.com>
2023-03-29 07:11:56 +0200

    [Interpolate-11] Reference implementation for Interpolate-11 (#16342)
    * Reference impl for interpolate-11 init
    
    * ND support init
    
    * Tests clean up
    
    * Add evaluate method for Interpolate-11
    
    * New version tests init
    
    * Type parametrized tests
    
    * Tests duplication clean up and reusage of v4 test cases
    
    * Add clipping to the type bounds
    
    * Style fix
    
    * Add float type tests
    
    * Fix default ports values
    
    * Commented code clean up
    
    * Add passing cube_coeff param
    
    * Tests clean up
    
    * Add separate namespace
    
    * Adjust variable names
    
    * Adjust function name
    
    * Use vectors instead of raw ptrs
    
    * update func to static inline
    
    * Adjust types
    
    * Add Interpolate-11 to template plugin evaluates map
    
    * Revert interpolate-11 core evaluate support
    
    * Use const ref to filter
    
    * Use static cast
    
    * Update link

--- src/plugins/template/backend/evaluates_map.cpp
+++ src/plugins/template/backend/evaluates_map.cpp
@@ -91,6 +91,7 @@
 #include <ngraph/runtime/reference/utils/nms_common.hpp>
 
 #include "backend.hpp"
+#include "interpolate_shape_inference.hpp"
 #include "ngraph/ops.hpp"
 #include "ngraph/runtime/reference/convert_color_nv12.hpp"
 #include "ov_ops/augru_cell.hpp"
@@ -4145,6 +4146,195 @@ bool evaluate(const shared_ptr<op::v0::Interpolate>& op,
     default:;
     }
     return true;
+}
+
+namespace eval {
+namespace interpolate {
+// The helpers below are similar to the internal utils used in evaluate method of v4::Intepolate core op
+// Those functions can be unified and moved to a common place
+std::vector<int64_t> get_axes_vector(const ngraph::HostTensorVector& args,
+                                     size_t default_size,
+                                     size_t axes_port,
+                                     size_t max_num_of_ports) {
+    size_t num_of_inputs = args.size();
+    std::vector<int64_t> axes;
+
+    if (num_of_inputs == max_num_of_ports) {
+        auto axes_arg = args[axes_port];
+        size_t num_of_axes = args[axes_port]->get_shape()[0];
+        axes.reserve(num_of_axes);
+
+        if (axes_arg->get_element_type() == ov::element::i64) {
+            int64_t* axes_ptr = axes_arg->get_data_ptr<int64_t>();
+            axes.insert(axes.end(), axes_ptr, axes_ptr + num_of_axes);
+        } else if (axes_arg->get_element_type() == ov::element::i32) {
+            int32_t* axes_ptr = axes_arg->get_data_ptr<int32_t>();
+            for (size_t i = 0; i < num_of_axes; ++i)
+                axes.push_back(axes_ptr[i]);
+        } else {
+            OPENVINO_ASSERT(false, "Failed to process ", axes_arg->get_element_type());
+        }
+    } else {
+        for (size_t i = 0; i < default_size; ++i) {
+            axes.push_back(i);
+        }
+    }
+
+    return axes;
+}
+
+std::vector<int64_t> get_target_shape_vector(const ngraph::HostTensorVector& args,
+                                             size_t num_of_axes,
+                                             size_t target_shape_port = 1) {
+    std::vector<int64_t> target_shape;
+    target_shape.reserve(num_of_axes);
+
+    auto target_shape_arg = args[target_shape_port];
+    if (target_shape_arg->get_element_type() == ov::element::i64) {
+        int64_t* target_shape_ptr = target_shape_arg->get_data_ptr<int64_t>();
+        target_shape.insert(target_shape.end(), target_shape_ptr, target_shape_ptr + num_of_axes);
+    } else if (target_shape_arg->get_element_type() == ov::element::i32) {
+        int32_t* target_shape_ptr = target_shape_arg->get_data_ptr<int32_t>();
+        for (size_t i = 0; i < num_of_axes; ++i)
+            target_shape.push_back(target_shape_ptr[i]);
+    } else {
+        OPENVINO_ASSERT(false, "Failed to process ", target_shape_arg->get_element_type());
+    }
+
+    return target_shape;
+}
+
+std::vector<float> get_scales_vector(const ngraph::HostTensorVector& args,
+                                     const ov::Shape& input_shape,
+                                     const ov::op::util::InterpolateBase::InterpolateAttrs& attrs,
+                                     std::vector<int64_t> axes,
+                                     size_t scales_port) {
+    std::vector<float> scales;
+    size_t num_of_axes = axes.size();
+    if (attrs.shape_calculation_mode == ov::op::util::InterpolateBase::ShapeCalcMode::SCALES) {
+        float* scales_ptr = args[scales_port]->get_data_ptr<float>();
+        scales.insert(scales.end(), scales_ptr, scales_ptr + num_of_axes);
+    } else {
+        auto target_shape = get_target_shape_vector(args, num_of_axes);
+        for (size_t i = 0; i < num_of_axes; ++i) {
+            size_t axis = axes[i];
+            float scale = static_cast<float>(target_shape[i]) / static_cast<float>(input_shape[axis]);
+            scales.push_back(scale);
+        }
+    }
+    return scales;
+}
+
+static void pad_input_data(const uint8_t* data_ptr,
+                           uint8_t* padded_data_ptr,
+                           size_t type_size,
+                           const ov::Shape& input_shape,
+                           const ov::Shape& padded_input_shape,
+                           const std::vector<size_t>& pads_begin) {
+    NGRAPH_SUPPRESS_DEPRECATED_START
+    ngraph::CoordinateTransform input_transform(input_shape);
+    ngraph::CoordinateTransform padded_transform(padded_input_shape);
+
+    for (const ngraph::Coordinate& input_coord : input_transform) {
+        auto padded_coord = input_coord;
+        size_t i = 0;
+        for (size_t pad : pads_begin) {
+            padded_coord[i] += pad;
+            ++i;
+        }
+        uint8_t* dst_ptr = padded_data_ptr + type_size * padded_transform.index(padded_coord);
+        const uint8_t* src_ptr = data_ptr + type_size * input_transform.index(input_coord);
+        memcpy(dst_ptr, src_ptr, type_size);
+    }
+    NGRAPH_SUPPRESS_DEPRECATED_END
+}
+
+namespace v11 {
+bool evaluate_interpolate(const shared_ptr<op::v11::Interpolate>& op,
+                          const HostTensorVector& outputs,
+                          const HostTensorVector& inputs) {
+    using namespace ov::op;
+
+    constexpr size_t data_port = 0;
+    constexpr size_t scales_sizes_port = 1;
+    constexpr size_t axes_port = 2;
+    constexpr size_t max_num_of_ports = 3;
+
+    element::Type input_et = inputs[0]->get_element_type();
+    size_t type_size = input_et.size();
+
+    ov::PartialShape input_shape{inputs[data_port]->get_shape()};
+    auto m_attrs = op->get_attrs();
+    util::correct_pads_attr(op.get(), m_attrs.pads_begin, m_attrs.pads_end, std::vector<PartialShape>{input_shape});
+
+    ov::Shape padded_input_shape;
+    for (size_t i = 0; i < input_shape.size(); ++i) {
+        padded_input_shape.emplace_back(m_attrs.pads_begin[i] + m_attrs.pads_end[i] + input_shape[i].get_length());
+    }
+
+    auto axes = get_axes_vector(inputs, inputs[1]->get_shape().size(), axes_port, max_num_of_ports);
+    auto scales = get_scales_vector(inputs, padded_input_shape, m_attrs, axes, scales_sizes_port);
+
+    ov::PartialShape output_shape{padded_input_shape};
+    if (m_attrs.shape_calculation_mode == util::InterpolateBase::ShapeCalcMode::SCALES) {
+        util::infer_using_scales(output_shape, axes, scales);
+    } else {
+        auto sizes = get_target_shape_vector(inputs, axes.size(), scales_sizes_port);
+        for (size_t i = 0; i < sizes.size(); ++i) {
+            output_shape[axes[i]] = Dimension(sizes[i]);
+        }
+    }
+
+    ov::Shape out_shape = output_shape.to_shape();
+    outputs[0]->set_shape(out_shape);
+    outputs[0]->set_element_type(input_et);
+
+    size_t bytes_in_padded_input = shape_size(padded_input_shape) * type_size;
+    std::vector<uint8_t> padded_input_data(bytes_in_padded_input, 0);
+
+    const uint8_t* data_ptr = inputs[0]->get_data_ptr<uint8_t>();
+    uint8_t* padded_data_ptr = padded_input_data.data();
+
+    pad_input_data(data_ptr,
+                   padded_data_ptr,
+                   type_size,
+                   input_shape.to_shape(),
+                   padded_input_shape,
+                   m_attrs.pads_begin);
+
+    switch (input_et) {
+    case element::Type_t::f32:
+        ngraph::runtime::reference::interpolate<float>(reinterpret_cast<float*>(padded_data_ptr),
+                                                       padded_input_shape,
+                                                       scales,
+                                                       axes,
+                                                       outputs[0]->get_data_ptr<float>(),
+                                                       out_shape,
+                                                       m_attrs);
+        break;
+    case element::Type_t::u8:
+        ngraph::runtime::reference::interpolate<uint8_t>(reinterpret_cast<uint8_t*>(padded_data_ptr),
+                                                         padded_input_shape,
+                                                         scales,
+                                                         axes,
+                                                         outputs[0]->get_data_ptr<uint8_t>(),
+                                                         out_shape,
+                                                         m_attrs);
+        break;
+    default:;
+    }
+
+    return true;
+}
+}  // namespace v11
+}  // namespace interpolate
+}  // namespace eval
+
+template <element::Type_t ET>
+bool evaluate(const shared_ptr<op::v11::Interpolate>& op,
+              const HostTensorVector& outputs,
+              const HostTensorVector& inputs) {
+    return eval::interpolate::v11::evaluate_interpolate(op, outputs, inputs);
 }
 
 template <element::Type_t ET>
