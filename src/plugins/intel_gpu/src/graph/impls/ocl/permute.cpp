f13e5c2d79c0535e9d83fddbb271087482288fe1
Lyamin-Roman <roman.lyamin@intel.com>
2023-04-01 04:14:54 +0900

    [GPU] Added GatherND dynamic support and changed logic for empty tensor support

--- src/plugins/intel_gpu/src/graph/impls/ocl/permute.cpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl/permute.cpp
@@ -65,7 +65,6 @@ struct permute_impl : typed_primitive_impl_ocl<permute> {
     void update_dispatch_data(const kernel_impl_params& impl_param) override {
         auto kernel_params = get_kernel_params(impl_param, true);
         (_kernel_data.update_dispatch_data_func)(kernel_params.first, _kernel_data);
-        update_kernels_list_to_skip();
     }
 };
 
