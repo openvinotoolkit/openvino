9fe31249ef7228c130c79bfe29ae6b417eb3c576
sunxiaoxia2022 <xiaoxia.sun@intel.com>
2023-04-01 20:02:11 +0800

    change useHyperThreading init value

--- src/plugins/intel_cpu/src/config.h
+++ src/plugins/intel_cpu/src/config.h
@@ -51,8 +51,8 @@ struct Config {
     InferenceEngine::IStreamsExecutor::Config streamExecutorConfig;
     InferenceEngine::PerfHintsConfig  perfHintsConfig;
     ov::hint::SchedulingCoreType schedulingCoreType = ov::hint::SchedulingCoreType::ANY_CORE;
-    bool useHyperThreading = false;
-    bool changedHyperThreading = true;
+    bool useHyperThreading = true;
+    bool changedHyperThreading = false;
 #if defined(OPENVINO_ARCH_X86) || defined(OPENVINO_ARCH_X86_64)
     LPTransformsMode lpTransformsMode = LPTransformsMode::On;
     bool enforceBF16 = true;
