d1563d5aa66cc8d5fe5c25a3f97545b5aaa5bc1f
Kazantsev, Roman <roman.kazantsev@intel.com>
2023-04-02 00:18:27 +0400

    Switch off no sorted case

--- tests/layer_tests/tensorflow_tests/test_tf_TopKV2.py
+++ tests/layer_tests/tensorflow_tests/test_tf_TopKV2.py
@@ -38,8 +38,9 @@ class TestTopKV2(CommonTFLayerTest):
              is_second_output=False),
         dict(input_shape=[4, 12], input_type=tf.float32, k=10, sorted=True, is_first_output=True,
              is_second_output=True),
-        dict(input_shape=[5, 10], input_type=tf.int32, k=8, sorted=False, is_first_output=True,
-             is_second_output=True),
+        # Expect stable mode implementation for sort_type=indices in OpenVINO. See 101503
+        pytest.param(dict(input_shape=[5, 10], input_type=tf.int32, k=8, sorted=False, is_first_output=True,
+                          is_second_output=True), marks=pytest.mark.xfail(reason="101503")),
     ]
 
     @pytest.mark.parametrize("params", test_basic)
