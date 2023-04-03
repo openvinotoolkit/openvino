66cc30356fea58a760a3715553d311814fc86ceb
Ivan <ivan.tikhonov@intel.com>
2023-04-03 15:55:17 +0400

    disable constant folding for shapeOf subgraph only

--- src/common/transformations/src/transformations/transpose_sinking/ts_general.cpp
+++ src/common/transformations/src/transformations/transpose_sinking/ts_general.cpp
@@ -22,6 +22,7 @@
 #include "transformations/transpose_sinking/ts_unary.hpp"
 #include "transformations/transpose_sinking/ts_unsqueeze.hpp"
 #include "transformations/utils/utils.hpp"
+#include "transformations/common_optimizations/disable_shapeof_constant_folding.hpp"
 
 using namespace ov::pass::transpose_sinking;
 
@@ -61,13 +62,17 @@ bool TSGeneral::run_on_model(const std::shared_ptr<ov::Model>& f) {
     RUN_ON_FUNCTION_SCOPE(TSGeneral);
     {
         Manager manager(get_pass_config());
+        manager.register_pass<DisableShapeOfConstantFolding>();
         manager.register_pass<TSGeneralForward>();
+        manager.register_pass<ConstantFolding>();
         manager.run_passes(f);
     }
 
     {
         Manager manager(get_pass_config());
+        manager.register_pass<DisableShapeOfConstantFolding>();
         manager.register_pass<TSGeneralBackward>();
+        manager.register_pass<ConstantFolding>();
         manager.run_passes(f);
     }
 
