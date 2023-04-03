4e500609dc192e4d7636eac8df7bc43eaba63bbe
Maxim Vafin <maxim.vafin@intel.com>
2023-04-03 17:11:44 +0200

    Remove usage of Any from CreatorFunction

--- src/frontends/tensorflow/include/openvino/frontend/tensorflow/node_context.hpp
+++ src/frontends/tensorflow/include/openvino/frontend/tensorflow/node_context.hpp
@@ -71,21 +71,26 @@ private:
 using CreatorFunctionIndexed = std::function<ov::OutputVector(const ov::frontend::tensorflow::NodeContext&)>;
 using CreatorFunctionNamedAndIndexed = std::function<NamedOutputVector(const ov::frontend::tensorflow::NodeContext&)>;
 
-struct CreatorFunction {
+class CreatorFunction {
+public:
     CreatorFunction() = default;
-    CreatorFunction(CreatorFunctionIndexed _func) : func(_func) {}
-    CreatorFunction(CreatorFunctionNamedAndIndexed _func) : func(_func) {}
+    CreatorFunction(CreatorFunctionIndexed _func) : func_indexed(_func) {}
+    CreatorFunction(CreatorFunctionNamedAndIndexed _func) : func_named_and_indexed(_func) {}
 
     NamedOutputVector operator()(const ov::frontend::tensorflow::NodeContext& node) const {
-        if (func.is<CreatorFunctionIndexed>()) {
-            auto outputs = func.as<CreatorFunctionIndexed>()(node);
+        if (func_indexed) {
+            auto outputs = func_indexed(node);
             return NamedOutputVector(outputs.begin(), outputs.end());
+        } else if (func_named_and_indexed) {
+            return func_named_and_indexed(node);
         } else {
-            return func.as<CreatorFunctionNamedAndIndexed>()(node);
+            FRONT_END_GENERAL_CHECK(false, "No conversion function exist in this CreatorFunction");
         }
     }
 
-    Any func;
+private:
+    CreatorFunctionIndexed func_indexed = nullptr;
+    CreatorFunctionNamedAndIndexed func_named_and_indexed = nullptr;
 };
 
 using TranslatorDictionaryType = std::map<std::string, CreatorFunction>;
