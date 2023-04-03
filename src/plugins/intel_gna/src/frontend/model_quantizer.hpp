99468049a347d90b1771f48002a44111cf701ce6
Denis Orlov <denis.orlov@intel.com>
2023-04-03 17:46:50 +0100

    [GNA] Fix compilation

--- src/plugins/intel_gna/src/frontend/model_quantizer.hpp
+++ src/plugins/intel_gna/src/frontend/model_quantizer.hpp
@@ -148,7 +148,7 @@ private:
                 inf_loop_count++;
             } else {
                 if (inf_loop_count > 0 &&
-                    (inf_loop_pattern.size() > 0 && inf_loop_history.size() % inf_loop_pattern.size() == 0 ||
+                    ((inf_loop_pattern.size() > 0) && (inf_loop_history.size() % inf_loop_pattern.size() == 0) ||
                      sf.allLayersProcessed())) {
                     int32_t history_shift = 0;
                     int32_t pattern_shift = 0;
