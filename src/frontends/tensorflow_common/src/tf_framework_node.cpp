1e93667324911d5101318ad2608a483a2716a52e
Kazantsev, Roman <roman.kazantsev@intel.com>
2023-04-03 20:59:38 +0400

    Correct Key for exception message

--- src/frontends/tensorflow_common/src/tf_framework_node.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2018-2023 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
-//
-
-#include "tf_framework_node.hpp"
-
-const std::string ov::frontend::tensorflow::FrameworkNode::failed_conversion_key = "failed_conversion_message";
