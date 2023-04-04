0014b8152bc150df12fac0977c288cfc488d138d
Maciej Kwapulinski <maciej.kwapulinski@intel.com>
2023-04-04 12:39:23 +0200

    RR3 comments applied

--- src/plugins/intel_gna/tests/functional/shared_tests_instances/single_layer_tests/memory.cpp
+++ src/plugins/intel_gna/tests/functional/shared_tests_instances/single_layer_tests/memory.cpp
@@ -17,7 +17,6 @@ using namespace opset11;
 namespace {
 
 class MemoryTestGna : public MemoryTest {
-    using Super = MemoryTest;
 
 public:
     InferenceEngine::Blob::Ptr GenerateInput(const InferenceEngine::InputInfo& info) const override {
@@ -27,38 +26,38 @@ public:
     }
 
 protected:
-    void CreateCommonFunc() override {
-        auto param = builder::makeParams(ngPrc, {inputShape});
-        const auto variable_info = targetDevice == CommonTestUtils::DEVICE_GPU
-                                       ? VariableInfo{Shape{inputShape}, ngPrc, "v0"}
-                                       : VariableInfo{PartialShape::dynamic(), element::dynamic, "v0"};
-        auto variable = std::make_shared<Variable>(variable_info);
-
-        auto min55 = ngraph::builder::makeConstant<float>(ngPrc, {}, {-55.001678466796875f});
-        auto max55 = ngraph::builder::makeConstant<float>(ngPrc, {}, {55.0f});
-
-        auto min270 = ngraph::builder::makeConstant<float>(ngPrc, {}, {-270.00823974609375f});
-        auto max270 = ngraph::builder::makeConstant<float>(ngPrc, {}, {270.0f});
-
-        auto min325 = ngraph::builder::makeConstant<float>(ngPrc, {}, {-325.0099182128906f});
-        auto max325 = ngraph::builder::makeConstant<float>(ngPrc, {}, {325.0f});
-
-        auto fq_from_Par225 = std::make_shared<FakeQuantize>(param.at(0), min55, max55, min55, max55, 65536);
-
-        auto read_value = CreateReadValueOp(fq_from_Par225, variable);
-
-        auto fq_from_ReadVal = std::make_shared<FakeQuantize>(read_value, min270, max270, min270, max270, 65536);
-
-        auto add = std::make_shared<Add>(fq_from_ReadVal, fq_from_Par225);
-
-        auto fq_from_add = std::make_shared<FakeQuantize>(add, min325, max325, min325, max325, 65536);
-
-        auto assign = CreateAssignOp(fq_from_add, variable);
-        auto res = std::make_shared<Result>(fq_from_add);
-
-        function = std::make_shared<Function>(ResultVector{res}, SinkVector{assign}, param, "TestMemory");
-    }
+    void CreateCommonFunc() override;
 };
+
+void MemoryTestGna::CreateCommonFunc() {
+    auto param = builder::makeParams(ngPrc, {inputShape});
+    const auto variable_info = VariableInfo{PartialShape::dynamic(), element::dynamic, "v0"};
+    auto variable = std::make_shared<Variable>(variable_info);
+
+    auto min55 = ngraph::builder::makeConstant<float>(ngPrc, {}, {-55.001678466796875f});
+    auto max55 = ngraph::builder::makeConstant<float>(ngPrc, {}, {55.0f});
+
+    auto min270 = ngraph::builder::makeConstant<float>(ngPrc, {}, {-270.00823974609375f});
+    auto max270 = ngraph::builder::makeConstant<float>(ngPrc, {}, {270.0f});
+
+    auto min325 = ngraph::builder::makeConstant<float>(ngPrc, {}, {-325.0099182128906f});
+    auto max325 = ngraph::builder::makeConstant<float>(ngPrc, {}, {325.0f});
+
+    auto fq_from_Par225 = std::make_shared<FakeQuantize>(param.at(0), min55, max55, min55, max55, 65536);
+
+    auto read_value = CreateReadValueOp(fq_from_Par225, variable);
+
+    auto fq_from_ReadVal = std::make_shared<FakeQuantize>(read_value, min270, max270, min270, max270, 65536);
+
+    auto add = std::make_shared<Add>(fq_from_ReadVal, fq_from_Par225);
+
+    auto fq_from_add = std::make_shared<FakeQuantize>(add, min325, max325, min325, max325, 65536);
+
+    auto assign = CreateAssignOp(fq_from_add, variable);
+    auto res = std::make_shared<Result>(fq_from_add);
+
+    function = std::make_shared<Function>(ResultVector{res}, SinkVector{assign}, param, "TestMemory");
+}
 
 TEST_P(MemoryTestGna, CompareWithRefs) {
     Run();
