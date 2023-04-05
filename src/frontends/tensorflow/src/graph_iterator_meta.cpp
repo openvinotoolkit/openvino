0fc7dd3dde23d7e48fc58c018f81553fff388184
Krivoruchko <georgy.krivoruchko@intel.com>
2023-04-05 02:41:03 -0700

    Fixing CI

--- src/frontends/tensorflow/src/graph_iterator_meta.cpp
+++ src/frontends/tensorflow/src/graph_iterator_meta.cpp
@@ -2,19 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include "graph_iterator_meta.hpp"
+
 #include <stdlib.h>
 
 #include <fstream>
 #include <string>
 
-#include "graph_iterator_meta.hpp"
 #include "openvino/core/type/element_type.hpp"
 #include "tensor_bundle.pb.h"
 #include "trackable_object_graph.pb.h"
-
-#ifdef ENABLE_SNAPPY_COMPRESSION
-#    include "snappy.h"
-#endif
 
 namespace ov {
 namespace frontend {
