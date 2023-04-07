132b65797749d048824a09ce80cfebdb4e033786
Roman Lyamin <Roman.Lyamin@intel.com>
2023-04-07 09:19:05 +0400

    [GPU] Added GatherND dynamic support and changed logic for empty tensor support (#16690)

--- src/plugins/intel_gpu/src/graph/impls/ocl/gather_elements.cpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl/gather_elements.cpp
@@ -69,7 +69,6 @@ struct gather_elements_impl : typed_primitive_impl_ocl<gather_elements> {
     void update_dispatch_data(const kernel_impl_params& impl_param) override {
        auto kernel_params = get_kernel_params(impl_param, true);
        (_kernel_data.update_dispatch_data_func)(kernel_params.first, _kernel_data);
-       update_kernels_list_to_skip();
     }
 };
 
