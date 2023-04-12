d0985e3ba3aaff4adaf6c86c52e2880072b4b4be
Roboreptile <piotr.krzeminski@intel.com>
2023-04-12 23:17:04 +0000

    [PT FE] aten::all implementation with tests

---
+++ src/frontends/pytorch/src/op/all.cpp
@@ -0,0 +1,45 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+#include "openvino/frontend/pytorch/node_context.hpp"
+#include "openvino/opsets/opset10.hpp"
+#include "utils.hpp"
+
+namespace ov {
+namespace frontend {
+namespace pytorch {
+namespace op {
+
+OutputVector translate_all(const NodeContext& context) {
+    num_inputs_check(context, 1, 3);
+    const auto input_tensor = context.get_input(0);
+    bool keep_dims;
+    ov::Output<ov::Node> axes;
+    element::Type output_dtype;
+
+    auto elem_type = input_tensor.get_element_type();
+    if (elem_type == element::u8) {
+        // if (context.get_input_type(0).is<type::Dict>()) {
+        output_dtype = element::u8;
+    } else {
+        output_dtype = element::boolean;
+    }
+
+    if (context.get_input_size() == 1) {
+        keep_dims = false;
+        axes = get_axes_range(context, 0);
+    } else {
+        int64_t dim = context.const_input<int64_t>(1);
+        keep_dims = !context.input_is_none(2) ? context.const_input<bool>(2) : false;
+        axes = context.mark_node(opset10::Constant::create(element::i64, Shape{1}, {dim}));
+    }
+
+    const auto all_nonzero = context.mark_node(std::make_shared<opset10::ReduceProd>(input_tensor, axes, keep_dims));
+
+    return {context.mark_node(std::make_shared<opset10::Convert>(all_nonzero, output_dtype))};
+};
+
+}  // namespace op
+}  // namespace pytorch
+}  // namespace frontend
+}  // namespace ov
