fd141579a78a4a5ecef5eec07c3c47057983faa9
Ilya Lavrenov <ilya.lavrenov@intel.com>
2023-04-13 06:31:27 +0800

    ARM32 support

--- cmake/arm.toolchain.cmake
+++ cmake/arm.toolchain.cmake
@@ -5,8 +5,8 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR armv7l)
 
-set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc-13)
-set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++-13)
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
 set(CMAKE_STRIP arm-linux-gnueabihf-strip)
 set(PKG_CONFIG_EXECUTABLE arm-linux-gnueabihf-pkg-config CACHE PATH "Path to ARM pkg-config")
 
