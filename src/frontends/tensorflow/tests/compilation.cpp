97ee143ae8702351e3c21d59a024ed8a924362b1
Mateusz Tabaka <mateusz.tabaka@intel.com>
2023-04-13 10:42:17 +0200

    [TF frontend] Add test for Split->Conv->Concat scenario (#16816)

--- src/frontends/tensorflow/tests/compilation.cpp
+++ src/frontends/tensorflow/tests/compilation.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include <exec_graph_info.hpp>
 #include <openvino/frontend/manager.hpp>
 #include <openvino/openvino.hpp>
 
@@ -41,4 +42,26 @@ TEST_F(CompileModelsTests, NgramCompilation) {
     EXPECT_EQ(runtime_model->get_ordered_ops().size(), 4);
     EXPECT_EQ(runtime_model->get_parameters().size(), 2);
     EXPECT_EQ(runtime_model->get_results().size(), 1);
+}
+
+TEST_F(CompileModelsTests, ModelWithSplitConvConcat) {
+    {
+        auto model = convert_model("split_conv_concat/split_conv_concat.pbtxt");
+        ov::Core core;
+        ov::CompiledModel compiled_model = core.compile_model(model, "CPU");
+        const auto runtime_model = compiled_model.get_runtime_model();
+        auto get_layer_type = [](const std::shared_ptr<ov::Node>& node) {
+            return node->get_rt_info().at(ExecGraphInfoSerialization::LAYER_TYPE).as<std::string>();
+        };
+        const auto ops = runtime_model->get_ops();
+        EXPECT_EQ(0, std::count_if(ops.begin(), ops.end(), [&](const std::shared_ptr<ov::Node>& node) {
+                      return get_layer_type(node) == "Split";
+                  }));
+        EXPECT_EQ(2, std::count_if(ops.begin(), ops.end(), [&](const std::shared_ptr<ov::Node>& node) {
+                      return get_layer_type(node) == "Convolution";
+                  }));
+        EXPECT_EQ(0, std::count_if(ops.begin(), ops.end(), [&](const std::shared_ptr<ov::Node>& node) {
+                      return get_layer_type(node) == "Concat";
+                  }));
+    }
 }
