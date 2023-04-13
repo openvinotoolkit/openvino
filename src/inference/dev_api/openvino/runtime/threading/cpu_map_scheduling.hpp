2625bd24bb01254c47164f2dab294204ed783bb3
Shen, Wanglei <wanglei.shen@intel.com>
2023-04-13 14:25:31 +0800

    inital function

--- src/inference/dev_api/openvino/runtime/threading/cpu_map_scheduling.hpp
+++ src/inference/dev_api/openvino/runtime/threading/cpu_map_scheduling.hpp
@@ -35,4 +35,16 @@ std::vector<std::vector<int>> apply_hyper_threading(bool input_type,
                                                     const bool input_changed,
                                                     const std::vector<std::vector<int>>& proc_type_table);
 
+/**
+ * @brief      Update CPU pinning property according to platform and user setting
+ * @param[in]  input_pinning indicates value of property enable_cpu_pinning.
+ * @param[in]  input_changed indicates if value is set by user.
+ * @param[in]  stream_info_table indicates the stream details that will be created..
+ * @param[in]  original_proc_type_table indicates CPU details on this platform
+ * @return     updated proc_type_table which removed unmatched processors
+ */
+bool update_cpu_pinning(const bool input_pinning,
+                        const bool input_changed,
+                        const std::vector<std::vector<int>>& stream_info_table,
+                        const std::vector<std::vector<int>>& original_proc_type_table);
 }  // namespace ov
--- src/inference/src/dev/threading/cpu_map_scheduling.cpp
+++ src/inference/src/dev/threading/cpu_map_scheduling.cpp
@@ -57,4 +57,32 @@ std::vector<std::vector<int>> apply_hyper_threading(bool input_value,
     return result_table;
 }
 
+bool update_cpu_pinning(const bool input_pinning,
+                        const bool input_changed,
+                        const std::vector<std::vector<int>>& stream_info_table,
+                        const std::vector<std::vector<int>>& original_proc_type_table) {
+#if (defined(_WIN32) || defined(_WIN64))
+
+    return false;
+
+#endif
+
+#ifdef __linux__
+
+    if (original_proc_type_table[0][HYPER_THREADING_PROC] == 0) {
+        return false;
+    } else if (!input_changed) {
+        for (auto& row : stream_info_table) {
+            if ((row[PROC_TYPE] == ALL_PROC) || (row[PROC_TYPE] == MAIN_CORE_PROC) || (row[PROC_TYPE] == HYPER_THREADING_PROC)) {
+                return false;
+            }
+        }
+        return true;
+    } else {
+        return input_pinning;
+    }
+
+#endif
+}
+
 }  // namespace ov
