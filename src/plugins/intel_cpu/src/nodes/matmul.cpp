fcc95cd05674863008eb64b332f95db95636a785
Li,Tingqian <tingqian.li@intel.com>
2023-04-19 00:13:25 +0200

    Refine WA

--- src/plugins/intel_cpu/src/nodes/matmul.cpp
+++ src/plugins/intel_cpu/src/nodes/matmul.cpp
@@ -208,9 +208,9 @@ bool MatMul::canFuse(const NodePtr& node) const {
     // WA for CVS-84056: oneDNN brgemm impl has problem with per-OC binary-postOps for MatMul with 6D inputs
     if (impl::cpu::x64::mayiuse(impl::cpu::x64::avx512_core)) {
         if (auto* eltwiseNode = dynamic_cast<Eltwise*>(node.get())) {
-            if (eltwiseNode->getBroadcastingPolicy() != Eltwise::BroadcastingPolicy::PerTensor) {
+            if (eltwiseNode->getBroadcastingPolicy() == Eltwise::BroadcastingPolicy::PerChannel) {
                 auto rank = getInputShapeAtPort(0).getRank();
-                if (rank == 6) {
+                if (rank > 4) {
                     DEBUG_LOG("skip fusing non-perTensor Eltwise:", eltwiseNode->getName(), " into 6D MatMul:", getName());
                     return false;
                 }
