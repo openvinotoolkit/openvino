bd5ac624bb9ffb70165f86e0d859975cbbe0f014
sunxiaoxia2022 <xiaoxia.sun@intel.com>
2023-04-20 20:31:30 +0800

    fix build issue

--- src/plugins/intel_cpu/src/cpu_map_scheduling.hpp
+++ src/plugins/intel_cpu/src/cpu_map_scheduling.hpp
@@ -23,9 +23,8 @@ namespace intel_cpu {
  * @param[in]  proc_type_table candidate processors available at this time
  * @return     updated proc_type_table which removed unmatched processors
  */
-OPENVINO_RUNTIME_API std::vector<std::vector<int>> apply_scheduling_core_type(
-    const ov::hint::SchedulingCoreType input_type,
-    const std::vector<std::vector<int>>& proc_type_table);
+std::vector<std::vector<int>> apply_scheduling_core_type(const ov::hint::SchedulingCoreType input_type,
+                                                         const std::vector<std::vector<int>>& proc_type_table);
 
 /**
  * @brief      Limit available CPU resource in processors type table according to hyper threading property
@@ -34,8 +33,9 @@ OPENVINO_RUNTIME_API std::vector<std::vector<int>> apply_scheduling_core_type(
  * @param[in]  proc_type_table candidate processors available at this time
  * @return     updated proc_type_table which removed unmatched processors
  */
-OPENVINO_RUNTIME_API std::vector<std::vector<int>>
-apply_hyper_threading(bool& input_type, const bool input_changed, const std::vector<std::vector<int>>& proc_type_table);
+std::vector<std::vector<int>> apply_hyper_threading(bool& input_type,
+                                                    const bool input_changed,
+                                                    const std::vector<std::vector<int>>& proc_type_table);
 
 /**
  * @brief      whether pinning cpu cores according to enableCpuPinning property
@@ -46,11 +46,11 @@ apply_hyper_threading(bool& input_type, const bool input_changed, const std::vec
  * @param[in]  proc_type_table candidate processors available at this time
  * @return     whether pinning threads to cpu cores
  */
-OPENVINO_RUNTIME_API bool get_cpu_pinning(bool& input_value,
-                                          const bool input_changed,
-                                          const int num_streams,
-                                          const threading::IStreamsExecutor::ThreadBindingType bind_type,
-                                          const std::vector<std::vector<int>>& proc_type_table);
+bool get_cpu_pinning(bool& input_value,
+                     const bool input_changed,
+                     const int num_streams,
+                     const threading::IStreamsExecutor::ThreadBindingType bind_type,
+                     const std::vector<std::vector<int>>& proc_type_table);
 
 }  // namespace intel_cpu
 }  // namespace ov
