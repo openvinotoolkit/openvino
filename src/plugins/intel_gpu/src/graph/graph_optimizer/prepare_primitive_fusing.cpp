97f4d8a76f8b356f43742b28e0acae195fc1b1e3
hyunback <hyunback.kim@intel.com>
2023-04-24 09:42:43 +0000

    Follow up code-reivew
    Only disable Prelu fusion in Fc, gemm
     - check additional data input
     Add unit test
     - It run non fusion network, no seg fault.
    
    Signed-off-by: hyunback <hyunback.kim@intel.com>

--- src/plugins/intel_gpu/src/graph/graph_optimizer/prepare_primitive_fusing.cpp
+++ src/plugins/intel_gpu/src/graph/graph_optimizer/prepare_primitive_fusing.cpp
@@ -705,7 +705,8 @@ void prepare_primitive_fusing::fuse_simple_primitives(program &p) {
             if (_lo.get_optimization_attributes().use_onednn_impls) {
                 if (input.is_type<reshape>() || input.is_type<concatenation>())
                     return;
-                if (activation_func == cldnn::activation_func::relu_negative_slope &&
+                auto additional_params_input = activation_node.get_primitive()->additional_params_input;
+                if (activation_func == cldnn::activation_func::relu_negative_slope && !additional_params_input.empty() &&
                     (input.is_type<fully_connected>() || input.is_type<gemm>())) {
                     // prelu fusion is not implemented in oneDNN3.1 (CVS-108233)
                     return;
