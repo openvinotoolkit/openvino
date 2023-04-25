7e734cf626ab23e3e57d07c735e1d4665dab808a
PiotrKrzem <piotr.krzeminski@intel.com>
2023-04-25 13:16:04 +0200

    [PT FE] Apply suggested optimizations

--- src/frontends/pytorch/src/op/all.cpp
+++ src/frontends/pytorch/src/op/all.cpp
@@ -19,15 +19,16 @@ OutputVector translate_all(const NodeContext& context) {
         output_dtype = element::u8;
     }
 
-    bool keep_dims;
+    bool keep_dims = false;
     ov::Output<ov::Node> axes;
     if (context.get_input_size() == 1) {
-        keep_dims = false;
         axes = get_axes_range(context, 0);
     } else {
         const auto dim = context.const_input<int64_t>(1);
-        keep_dims = !context.input_is_none(2) ? context.const_input<bool>(2) : false;
         axes = context.mark_node(opset10::Constant::create(element::i64, Shape{1}, {dim}));
+        if (!context.input_is_none(2)) {
+            keep_dims = context.const_input<bool>(2);
+        }
     }
 
     const auto all_nonzero = context.mark_node(std::make_shared<opset10::ReduceProd>(input_tensor, axes, keep_dims));
