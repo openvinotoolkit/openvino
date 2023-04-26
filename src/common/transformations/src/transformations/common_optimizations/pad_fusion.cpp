1d152a1877bb58c3066df202b5fd006c227cf2f8
Maxim Vafin <maxim.vafin@intel.com>
2023-04-26 17:45:19 +0200

    Do not fuse Pad with negative pad values

--- src/common/transformations/src/transformations/common_optimizations/pad_fusion.cpp
+++ src/common/transformations/src/transformations/common_optimizations/pad_fusion.cpp
@@ -45,11 +45,18 @@ static bool can_be_fused(const std::shared_ptr<opset5::Pad>& pad,
     if (node->get_pads_end().size() != shape_size(pads_end->get_shape()) - 2)
         return false;
 
-    auto pads_begin_val = pads_begin->cast_vector<size_t>();
-    auto pads_end_val = pads_end->cast_vector<size_t>();
+    auto pads_begin_val = pads_begin->cast_vector<int64_t>();
+    auto pads_end_val = pads_end->cast_vector<int64_t>();
     for (size_t i = 0; i < 2; i++) {
         if (pads_begin_val[i] != 0 || pads_end_val[i] != 0)
             return false;
+    }
+    auto pred = [](int64_t a) {
+        return a < 0;
+    };
+    if (std::any_of(pads_begin_val.begin(), pads_begin_val.end(), pred) ||
+        std::any_of(pads_end_val.begin(), pads_end_val.end(), pred)) {
+        return false;
     }
 
     return true;
