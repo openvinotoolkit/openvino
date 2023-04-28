1da3a866832bb5b8f0b39d5ad69b9a5243f77b88
PuQing <me@puqing.work>
2023-04-28 15:10:01 +0800

    add input dtype check

--- src/frontends/paddle/src/op/softshrink.cpp
+++ src/frontends/paddle/src/op/softshrink.cpp
@@ -12,39 +12,31 @@ namespace op {
 NamedOutputs softshrink(const NodeContext& node) {
     auto data = node.get_input("X");
     const float lambda = node.get_attribute<float>("lambda", 0.5f);
-
     const auto input_element_type = data.get_element_type();
+    PADDLE_OP_CHECK(node, lambda >= 0, "Softshrink op lambda must be non-negative.");
+    PADDLE_OP_CHECK(node, input_element_type.is_signed(), "Softshrink op input must be signed type.");
+
     std::shared_ptr<ngraph::Node> output;
     const auto positive_lambda = default_opset::Constant::create(input_element_type, Shape{}, {lambda});
+    const auto negative_lambda = default_opset::Constant::create(input_element_type, Shape{}, {-lambda});
     std::shared_ptr<ngraph::Node> negative_node = std::make_shared<default_opset::Subtract>(data, positive_lambda);
+    std::shared_ptr<ngraph::Node> positive_node = std::make_shared<default_opset::Add>(data, positive_lambda);
+
     std::shared_ptr<ngraph::Node> zero_node = default_opset::Constant::create(input_element_type, Shape{}, {0});
-    if (input_element_type.is_signed()) {
-        std::shared_ptr<default_opset::Constant> negative_lambda =
-            default_opset::Constant::create(input_element_type, Shape{}, {-lambda});
 
-        // Create masks for values below negative lambda and above positive lambda
-        std::shared_ptr<ngraph::Node> values_below_neg_lambda =
-            std::make_shared<default_opset::Less>(data, negative_lambda);
-        std::shared_ptr<ngraph::Node> values_above_pos_lambda =
-            std::make_shared<default_opset::Greater>(data, positive_lambda);
+    // Create masks for values below negative lambda and above positive lambda
+    std::shared_ptr<ngraph::Node> values_below_neg_lambda =
+        std::make_shared<default_opset::Less>(data, negative_lambda);
+    std::shared_ptr<ngraph::Node> values_above_pos_lambda =
+        std::make_shared<default_opset::Greater>(data, positive_lambda);
 
-        std::shared_ptr<ngraph::Node> positive_node = std::make_shared<default_opset::Add>(data, positive_lambda);
+    output = std::make_shared<default_opset::Select>(values_above_pos_lambda, negative_node, data);
+    output = std::make_shared<default_opset::Select>(values_below_neg_lambda, positive_node, output);
 
-        output = std::make_shared<default_opset::Select>(values_above_pos_lambda, negative_node, data);
-        output = std::make_shared<default_opset::Select>(values_below_neg_lambda, positive_node, output);
+    std::shared_ptr<ngraph::Node> zero_mask =
+        std::make_shared<default_opset::LogicalOr>(values_below_neg_lambda, values_above_pos_lambda);
 
-        std::shared_ptr<ngraph::Node> zero_mask =
-            std::make_shared<default_opset::LogicalOr>(values_below_neg_lambda, values_above_pos_lambda);
-
-        output = std::make_shared<default_opset::Select>(zero_mask, output, zero_node);
-    } else {
-        // Passing -lambd to unsigned type constant will cause an overflow.
-        // For unsigned types the lowest possible value is 0. So we just need to compare with lambda.
-        std::shared_ptr<ngraph::Node> values_above_pos_lambda =
-            std::make_shared<default_opset::Greater>(data, positive_lambda);
-
-        output = std::make_shared<default_opset::Select>(values_above_pos_lambda, negative_node, zero_node);
-    }
+    output = std::make_shared<default_opset::Select>(zero_mask, output, zero_node);
 
     return node.default_single_output_mapping({output}, {"Out"});
 }
