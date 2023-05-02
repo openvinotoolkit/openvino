1300908e5e91c57ab0655e0768e93c2411345a75
Vishniakov Nikolai <nikolai.vishniakov@intel.com>
2023-05-02 16:11:17 +0200

    Add ESLint to openvinojs-node

--- src/bindings/js/node/lib/index.ts
+++ src/bindings/js/node/lib/index.ts
@@ -1,20 +1,25 @@
 import loadModel from './node-model';
-import createModule from 'openvinojs-common'
+import createModule from 'openvinojs-common';
 
 import type { ovNodeModule } from './types';
 
+/* eslint-disable @typescript-eslint/no-var-requires */
 const addon: ovNodeModule = require('../build/Release/ov_node_addon.node');
 
-export default
-  createModule('node', loadModel, getVersionString, getDescriptionString);
+export default createModule(
+  'node',
+  loadModel,
+  getVersionString,
+  getDescriptionString
+);
 export { addon };
 
-
 async function getVersionString(): Promise<string> {
-    const str= "Version";
-    return str;
+  const str= 'Version';
+
+  return str;
 }
 
 async function getDescriptionString(): Promise<string> {
-    return addon.getDescriptionString();
+  return addon.getDescriptionString();
 }
--- src/bindings/js/node/lib/node-model.ts
+++ src/bindings/js/node/lib/node-model.ts
@@ -1,44 +1,61 @@
 import { Tensor, Shape } from 'openvinojs-common';
 
 import type { ovNodeModule, NodeModel, NodeTensor } from './types';
-import type { ITensor, IShape, IModel, PrecisionSupportedType } from 'openvinojs-common';
+import type {
+  ITensor,
+  IShape,
+  IModel,
+  PrecisionSupportedType,
+} from 'openvinojs-common';
 
+/* eslint-disable @typescript-eslint/no-var-requires */
 const ovNode: ovNodeModule = require('../build/Release/ov_node_addon.node');
 
-export default async function loadModel(xmlPath: string, binPath: string): Promise<IModel> {
-    if (typeof xmlPath !== 'string' || typeof binPath !== 'string')
-        throw new Error('Parameters \'xmlPath\' and \'binPath\' should be string');
+const DEFAULT_TENSOR_PRECISION = 'f32';
 
-    const model = new ovNode.Model().read_model(xmlPath).compile("CPU");
-    return new CommonModel(ovNode, model);
+export default async function loadModel(xmlPath: string, binPath: string)
+: Promise<IModel> {
+  if (typeof xmlPath !== 'string' || typeof binPath !== 'string')
+    throw new Error('Parameters \'xmlPath\' and \'binPath\' should be string');
+
+  const model = new ovNode.Model().read_model(xmlPath).compile('CPU');
+
+  return new CommonModel(ovNode, model);
 }
 
 class CommonModel implements IModel {
-    #ovNode: ovNodeModule;
-    #nodeModel: NodeModel;
+  #ovNode: ovNodeModule;
+  #nodeModel: NodeModel;
 
-    constructor(ovNode: ovNodeModule, nodeModel: NodeModel) {
-        this.#ovNode = ovNode;
-        this.#nodeModel = nodeModel;
-    }
+  constructor(ovNode: ovNodeModule, nodeModel: NodeModel) {
+    this.#ovNode = ovNode;
+    this.#nodeModel = nodeModel;
+  }
 
-    async infer(tensorOrDataArray: ITensor | number[], shape: IShape): Promise<ITensor> {
-        const tensor_data = tensorOrDataArray instanceof Array
-            ? Float32Array.from(tensorOrDataArray) : tensorOrDataArray.data;
-        const precision = tensorOrDataArray instanceof Tensor
-            ? tensorOrDataArray.precision : "f32";
+  async infer(tensorOrDataArray: ITensor | number[], shape: IShape)
+  : Promise<ITensor> {
+    const tensorData = tensorOrDataArray instanceof Array
+      ? Float32Array.from(tensorOrDataArray)
+      : tensorOrDataArray.data;
+    const precision = tensorOrDataArray instanceof Tensor
+      ? tensorOrDataArray.precision
+      : DEFAULT_TENSOR_PRECISION;
 
-        const nodeTensor = new this.#ovNode.Tensor(precision, shape.data, tensor_data);
-        const output = this.#nodeModel.infer(nodeTensor);
+    const nodeTensor = new this.#ovNode.Tensor(
+      precision,
+      shape.data,
+      tensorData,
+    );
+    const output = this.#nodeModel.infer(nodeTensor);
 
-        return parseNodeTensor(output);
-
-    }
+    return parseNodeTensor(output);
+  }
 }
 
 function parseNodeTensor(nodeTensor: NodeTensor): Tensor {
-    const precision = nodeTensor.getPrecision() as PrecisionSupportedType;
-    const data = nodeTensor.data;
-    const shape = new Shape(nodeTensor.getShape().getData());
-    return new Tensor(precision, data, shape);
+  const precision = nodeTensor.getPrecision() as PrecisionSupportedType;
+  const data = nodeTensor.data;
+  const shape = new Shape(nodeTensor.getShape().getData());
+
+  return new Tensor(precision, data, shape);
 }
