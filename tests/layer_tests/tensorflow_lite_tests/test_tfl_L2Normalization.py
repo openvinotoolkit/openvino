d68ec4605c7dcf871fd0e4a356bdebcd8fb8c3f4
rnugmano <ruslan.nugmanov@intel.com>
2023-05-02 09:39:32 +0200

    tfl l2_normalization

---
+++ tests/layer_tests/tensorflow_lite_tests/test_tfl_L2Normalization.py
@@ -0,0 +1,56 @@
+import itertools
+
+import pytest
+import tensorflow as tf
+
+from common.tflite_layer_test_class import TFLiteLayerTest
+from common.utils.tflite_utils import data_generators, additional_test_params
+
+test_ops = [
+    {'op_name': ['L2_NORMALIZATION'], 'op_func': tf.math.l2_normalize},
+]
+
+test_params = [
+    {'shape': [1]},
+    {'shape': [1, 22]},
+    {'shape': [1, 1, 8]},
+    {'shape': [1, 22, 22, 8]},
+]
+
+
+test_data = list(itertools.product(test_ops, test_params))
+for i, (parameters, shapes) in enumerate(test_data):
+    parameters.update(shapes)
+    test_data[i] = parameters.copy()
+
+test_data = list(itertools.product(test_data, additional_test_params[0]))
+for i, (parameters, additional_test_params[0]) in enumerate(test_data):
+    parameters.update(additional_test_params[0])
+    test_data[i] = parameters.copy()
+
+
+class TestTFLiteExpandDimsLayerTest(TFLiteLayerTest):
+    inputs = ["Input"]
+    outputs = ["L2_Normalization"]
+
+    def _prepare_input(self, inputs_dict, generator=None):
+        if generator is None:
+            return super()._prepare_input(inputs_dict)
+        return data_generators[generator](inputs_dict)
+
+    def make_model(self, params):
+        assert len(set(params.keys()).intersection({'op_name', 'op_func', 'shape'})) == 3, \
+            'Unexpected parameters for test: ' + ','.join(params.keys())
+        self.allowed_ops = params['op_name']
+        tf.compat.v1.reset_default_graph()
+        with tf.compat.v1.Session() as sess:
+            place_holder = tf.compat.v1.placeholder(params.get('dtype', tf.float32), params['shape'],
+                                                    name=self.inputs[0])
+            params['op_func'](place_holder, axis=params['axis'], name=self.outputs[0])
+            net = sess.graph_def
+        return net
+
+    @pytest.mark.parametrize("params", test_data)
+    @pytest.mark.nightly
+    def test_l2_normalization(self, params, ie_device, precision, temp_dir):
+        self._test(ie_device, precision, temp_dir, params)
