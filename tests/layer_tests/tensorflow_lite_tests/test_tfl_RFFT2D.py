093947634070b85fbfc66833ddc37635237f66c8
rnugmano <ruslan.nugmanov@intel.com>
2023-05-04 14:55:31 +0200

    tfl rfft2d fix

--- tests/layer_tests/tensorflow_lite_tests/test_tfl_RFFT2D.py
+++ tests/layer_tests/tensorflow_lite_tests/test_tfl_RFFT2D.py
@@ -7,17 +7,13 @@ from common.tflite_layer_test_class import TFLiteLayerTest
 from common.utils.tflite_utils import data_generators
 
 test_ops = [
-    {'op_name': ['RFFT2D'], 'op_func': tf.signal.rfft2d},
+    {'op_name': ['COMPLEX_ABS', 'RESHAPE', 'RFFT2D'], 'op_func': tf.signal.rfft2d},
 ]
 
 test_params = [
-    {'shape': [8, 8], 'fft_length': None},
-    {'shape': [3, 8, 8], 'fft_length': [4, 4]},
-    {'shape': [3, 8, 8], 'fft_length': [4, 8]},
-    {'shape': [3, 8, 8], 'fft_length': [8, 4]},
-    {'shape': [3, 8, 8], 'fft_length': [1, 16]},
+    {'shape': [96, 1, 512], 'fft_length': [1, 512], 'reshape_to': [96, 257]},
+    {'shape': [5, 1, 16], 'fft_length': [1, 16], 'reshape_to': [5, 9]},
 ]
-
 
 test_data = list(itertools.product(test_ops, test_params))
 for i, (parameters, shapes) in enumerate(test_data):
@@ -27,7 +23,7 @@ for i, (parameters, shapes) in enumerate(test_data):
 
 class TestTFLiteRFFT2DLayerTest(TFLiteLayerTest):
     inputs = ["Input"]
-    outputs = ["RFFT2D"]
+    outputs = ['RFFT2D_1']
 
     def _prepare_input(self, inputs_dict, generator=None):
         if generator is None:
@@ -35,14 +31,17 @@ class TestTFLiteRFFT2DLayerTest(TFLiteLayerTest):
         return data_generators[generator](inputs_dict)
 
     def make_model(self, params):
-        assert len(set(params.keys()).intersection({'op_name', 'op_func', 'shape', 'fft_length'})) == 4, \
+        assert len(set(params.keys()).intersection({'op_name', 'op_func', 'shape', 'fft_length', 'reshape_to'})) == 5, \
             'Unexpected parameters for test: ' + ','.join(params.keys())
         self.allowed_ops = params['op_name']
         tf.compat.v1.reset_default_graph()
         with tf.compat.v1.Session() as sess:
             placeholder = tf.compat.v1.placeholder(params.get('dtype', tf.float32), params['shape'],
                                                    name=self.inputs[0])
-            params['op_func'](placeholder, fft_length=params["fft_length"], name=self.outputs[0])
+            rfft_2d = params['op_func'](placeholder, fft_length=params["fft_length"])
+            reshape = tf.reshape(rfft_2d, params['reshape_to'])
+            out = tf.raw_ops.ComplexAbs(x=reshape, Tout=tf.float32, name='RFFT2D')
+
             net = sess.graph_def
         return net
 
