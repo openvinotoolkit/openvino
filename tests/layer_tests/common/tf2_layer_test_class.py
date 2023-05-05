304ff954ff89f2bd7aa96ae078c7519bfa649f1b
Kazantsev, Roman <roman.kazantsev@intel.com>
2023-05-05 18:32:45 +0400

    Use old path of tf2 layer testing for legacy frontend

--- tests/layer_tests/common/tf2_layer_test_class.py
+++ tests/layer_tests/common/tf2_layer_test_class.py
@@ -36,7 +36,39 @@ class CommonTF2LayerTest(CommonLayerTest):
 
     def get_tf2_keras_results(self, inputs_dict, model_path):
         import tensorflow as tf
-        imported = tf.saved_model.load(model_path)
-        f = imported.signatures["serving_default"]
-        tf_outputs = f(**inputs_dict)
-        return tf_outputs
+
+        result = dict()
+        if self.use_new_frontend:
+            imported = tf.saved_model.load(model_path)
+            f = imported.signatures["serving_default"]
+            result = f(**inputs_dict)
+        else:
+            # load a model
+            loaded_model = tf.keras.models.load_model(model_path, custom_objects=None)
+            # prepare input
+            input_for_model = []
+            # order inputs based on input names in tests
+            # since TF2 Keras model accepts a list of tensors for prediction
+            for input_name in sorted(inputs_dict):
+                input_value = inputs_dict[input_name]
+                input_for_model.append(input_value)
+            if len(input_for_model) == 1:
+                input_for_model = input_for_model[0]
+
+            # infer by original framework and complete a dictionary with reference results
+            tf_res_list = loaded_model(input_for_model)
+            if tf.is_tensor(tf_res_list):
+                tf_res_list = [tf_res_list]
+            else:
+                # in this case tf_res_list is a list of the single tuple of outputs
+                tf_res_list = tf_res_list[0]
+
+            for ind, tf_res in enumerate(tf_res_list):
+                if ind == 0:
+                    output = "Identity"
+                else:
+                    output = "Identity_{}".format(ind)
+                tf_res = tf_res.numpy()
+                result[output] = tf_res
+
+        return result
