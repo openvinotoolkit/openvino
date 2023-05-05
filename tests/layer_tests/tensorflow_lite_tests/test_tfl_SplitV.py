0a935f7b83adb57dd95be323a5f0914048e67b2e
rnugmano <ruslan.nugmanov@intel.com>
2023-05-05 13:48:40 +0200

    tfl split_v

---
+++ tests/layer_tests/tensorflow_lite_tests/test_tfl_SplitV.py
@@ -0,0 +1,57 @@
+import itertools
+
+import pytest
+import tensorflow as tf
+
+from common.tflite_layer_test_class import TFLiteLayerTest
+from common.utils.tflite_utils import data_generators
+
+test_ops = [
+    {'op_name': ['SPLIT_V'], 'op_func': tf.raw_ops.SplitV},
+]
+
+test_params = [
+    {'shape': [3], 'num_or_size_splits': [0, 2, 0, 1], 'axis': 0},
+    {'shape': [2, 3, 9], 'num_or_size_splits': [1, 2, 3, -1, 1], 'axis': 2},
+    {'shape': [3, 9, 5, 4], 'num_or_size_splits': [1, 2, 0, -1, 2, 0], 'axis': -3},
+]
+
+
+test_data = list(itertools.product(test_ops, test_params))
+for i, (parameters, shapes) in enumerate(test_data):
+    parameters.update(shapes)
+    test_data[i] = parameters.copy()
+
+
+class TestTFLiteSplitLayerTest(TFLiteLayerTest):
+    inputs = ["Input"]
+    outputs = ["SplitV"]
+
+    def _prepare_input(self, inputs_dict, generator=None):
+        if generator is None:
+            return super()._prepare_input(inputs_dict)
+        return data_generators[generator](inputs_dict)
+
+    def make_model(self, params):
+        assert len(set(params.keys()).intersection({'op_name', 'op_func', 'shape', 'num_or_size_splits',
+                                                    'axis'})) == 5, \
+            'Unexpected parameters for test: ' + ','.join(params.keys())
+        self.allowed_ops = params['op_name']
+        tf.compat.v1.reset_default_graph()
+        with tf.compat.v1.Session() as sess:
+            placeholder = tf.compat.v1.placeholder(tf.float32, params['shape'], self.inputs[0])
+            size_splits = tf.constant(params['num_or_size_splits'], dtype=tf.int32)
+            axis = tf.constant(params['axis'], dtype=tf.int32)
+            num_split = len(params['num_or_size_splits'])
+
+            params['op_func'](value=placeholder, size_splits=size_splits, axis=axis,
+                              num_split=num_split, name=self.outputs[0])
+            net = sess.graph_def
+        return net
+
+    @pytest.mark.parametrize("params", test_data)
+    @pytest.mark.nightly
+    def test_split_v(self, params, ie_device, precision, temp_dir):
+        if 0 in params['num_or_size_splits']:
+            pytest.skip("CVS-110040")
+        self._test(ie_device, precision, temp_dir, params)
