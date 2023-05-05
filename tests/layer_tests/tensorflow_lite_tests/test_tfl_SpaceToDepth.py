87726f13c5ea4575f312709b1088c785b9c76b68
rnugmano <ruslan.nugmanov@intel.com>
2023-05-05 11:06:40 +0200

    tfl space_to_depth

---
+++ tests/layer_tests/tensorflow_lite_tests/test_tfl_SpaceToDepth.py
@@ -0,0 +1,49 @@
+import itertools
+
+import pytest
+import tensorflow as tf
+
+from common.tflite_layer_test_class import TFLiteLayerTest
+from common.utils.tflite_utils import data_generators
+
+test_ops = [
+    {'op_name': 'SPACE_TO_DEPTH', 'op_func': tf.nn.space_to_depth},
+]
+
+test_params = [
+    {'shape': [8, 10, 10, 16], 'block_size': 2, 'data_format': 'NHWC'},
+    {'shape': [24, 10, 10, 50], 'block_size': 5, 'data_format': 'NHWC'},
+]
+
+
+test_data = list(itertools.product(test_ops, test_params))
+for i, (parameters, shapes) in enumerate(test_data):
+    parameters.update(shapes)
+    test_data[i] = parameters.copy()
+
+
+class TestTFLiteSpaceToDepthLayerTest(TFLiteLayerTest):
+    inputs = ["Input"]
+    outputs = ["SpaceToDepth"]
+
+    def _prepare_input(self, inputs_dict, generator=None):
+        if generator is None:
+            return super()._prepare_input(inputs_dict)
+        return data_generators[generator](inputs_dict)
+
+    def make_model(self, params):
+        assert len(set(params.keys()).intersection({'op_name', 'op_func', 'shape', 'block_size', 'data_format'})) == 5, \
+            'Unexpected parameters for test: ' + ','.join(params.keys())
+        self.allowed_ops = [params['op_name']]
+        tf.compat.v1.reset_default_graph()
+        with tf.compat.v1.Session() as sess:
+            place_holder = tf.compat.v1.placeholder(params.get('dtype', tf.float32), params['shape'],
+                                                    name=self.inputs[0])
+            params['op_func'](place_holder, params['block_size'], params['data_format'], name=self.outputs[0])
+            net = sess.graph_def
+        return net
+
+    @pytest.mark.parametrize("params", test_data)
+    @pytest.mark.nightly
+    def test_space_to_depth(self, params, ie_device, precision, temp_dir):
+        self._test(ie_device, precision, temp_dir, params)
