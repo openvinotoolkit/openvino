f25a2ec6b7b1c4890a9942d7401ab3d186b6d1f7
rnugmano <ruslan.nugmanov@intel.com>
2023-05-05 09:54:22 +0200

    tfl segment_sum

---
+++ tests/layer_tests/tensorflow_lite_tests/test_tfl_SegmentSum.py
@@ -0,0 +1,52 @@
+import itertools
+
+import pytest
+import tensorflow as tf
+
+from common.tflite_layer_test_class import TFLiteLayerTest
+from common.utils.tflite_utils import data_generators
+
+test_ops = [
+    {'op_name': ['SEGMENT_SUM'], 'op_func': tf.math.segment_sum},
+]
+
+test_params = [
+    {'shape': [2, 3, 1, 2, 2], 'segment_ids': [0, 1, 2, 3, 4]},
+    {'shape': [2, 3, 1, 2], 'segment_ids': [-1, -2, -3, -4]},
+    {'shape': [2, 3], 'segment_ids': [0, 1]},
+    {'shape': [2], 'segment_ids': [-1]},
+]
+
+
+test_data = list(itertools.product(test_ops, test_params))
+for i, (parameters, shapes) in enumerate(test_data):
+    parameters.update(shapes)
+    test_data[i] = parameters.copy()
+
+
+class TestTFLiteSegmentSumLayerTest(TFLiteLayerTest):
+    inputs = ["Input"]
+    outputs = ["SegmentSum"]
+
+    def _prepare_input(self, inputs_dict, generator=None):
+        if generator is None:
+            return super()._prepare_input(inputs_dict)
+        return data_generators[generator](inputs_dict)
+
+    def make_model(self, params):
+        assert len(set(params.keys()).intersection({'op_name', 'op_func', 'shape', 'segment_ids'})) == 4, \
+            'Unexpected parameters for test: ' + ','.join(params.keys())
+        self.allowed_ops = params['op_name']
+        tf.compat.v1.reset_default_graph()
+        with tf.compat.v1.Session() as sess:
+            place_holder = tf.compat.v1.placeholder(params.get('dtype', tf.float32), params['shape'],
+                                                    name=self.inputs[0])
+            constant = tf.constant(params['segment_ids'], dtype=tf.int32)
+            params['op_func'](place_holder, constant, name=self.outputs[0])
+            net = sess.graph_def
+        return net
+
+    @pytest.mark.parametrize("params", test_data)
+    @pytest.mark.nightly
+    def test_segment_sum(self, params, ie_device, precision, temp_dir):
+        self._test(ie_device, precision, temp_dir, params)
