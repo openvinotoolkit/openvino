740c43e65477f40d28234688173e60d648144128
rnugmano <ruslan.nugmanov@intel.com>
2023-05-08 12:08:30 +0200

    tfl unpack

--- tests/layer_tests/tensorflow_lite_tests/test_tfl_Unpack.py
+++ tests/layer_tests/tensorflow_lite_tests/test_tfl_Unpack.py
@@ -12,12 +12,16 @@ np.random.seed(42)
 
 
 test_ops = [
-    {'op_name': 'UNIQUE', 'op_func': tf.unique},
+    {'op_name': 'UNPACK', 'op_func': tf.unstack},
 ]
 
 test_params = [
-    {'shape': [15]},
-    {'shape': [1]}
+    {'shape': [3, 4, 3], 'axis': 0},
+    {'shape': [3, 4, 3], 'axis': -1},
+    {'shape': [3, 4, 3, 5], 'axis': -2},
+    {'shape': [3, 4, 3, 5], 'axis': 2},
+    {'shape': [1], 'axis': -1},
+    {'shape': [1], 'axis': 0}
 ]
 
 test_data = list(itertools.product(test_ops, test_params))
@@ -26,9 +30,9 @@ for i, (parameters, shapes) in enumerate(test_data):
     test_data[i] = parameters.copy()
 
 
-class TestTFLiteUniqueLayerTest(TFLiteLayerTest):
+class TestTFLiteUnpackLayerTest(TFLiteLayerTest):
     inputs = ["Input"]
-    outputs = ["Unqiue"]
+    outputs = ["Unpack"]
 
     def _prepare_input(self, inputs_dict, generator=None):
         if generator is None:
@@ -36,18 +40,18 @@ class TestTFLiteUniqueLayerTest(TFLiteLayerTest):
         return data_generators[generator](inputs_dict)
 
     def make_model(self, params):
-        assert len(set(params.keys()).intersection({'op_name', 'op_func', 'shape'})) == 3, \
+        assert len(set(params.keys()).intersection({'op_name', 'op_func', 'shape', 'axis'})) == 4, \
             'Unexpected parameters for test: ' + ','.join(params.keys())
         self.allowed_ops = [params['op_name']]
         tf.compat.v1.reset_default_graph()
         with tf.compat.v1.Session() as sess:
-            placeholder = tf.compat.v1.placeholder(params.get('dtype', tf.float32), params['shape'],
-                                                   name=self.inputs[0])
-            params['op_func'](placeholder, name=self.outputs[0])
+            place_holder = tf.compat.v1.placeholder(params.get('dtype', tf.float32), params['shape'],
+                                                    name=self.inputs[0])
+            params['op_func'](place_holder, axis=params['axis'], name=self.outputs[0])
             net = sess.graph_def
         return net
 
     @pytest.mark.parametrize("params", test_data)
     @pytest.mark.nightly
-    def test_unique(self, params, ie_device, precision, temp_dir):
+    def test_unpack(self, params, ie_device, precision, temp_dir):
         self._test(ie_device, precision, temp_dir, params)
