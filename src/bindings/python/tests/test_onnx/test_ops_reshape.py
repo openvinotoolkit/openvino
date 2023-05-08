c05d82a805745a182e3356ab4b1966145da60643
mitruska <katarzyna.mitrus@intel.com>
2023-05-08 21:42:06 +0200

    Add missing new line

--- src/bindings/python/tests/test_onnx/test_ops_reshape.py
+++ src/bindings/python/tests/test_onnx/test_ops_reshape.py
@@ -218,6 +218,7 @@ def test_concat():
             graph_results = run_node(node, np.array(values, dtype=np.int32))
             assert np.array_equal(graph_results, [expected_output])
 
+
 def test_squeeze():
     data = np.arange(6, dtype=np.int32).reshape([1, 2, 1, 3, 1])
     expected_output = data.reshape([1, 2, 3, 1])
