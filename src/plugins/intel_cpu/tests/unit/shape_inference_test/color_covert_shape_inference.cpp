6eb76176d302d0bde2d155b03c9a1754d6c07160
Hu Yuan2 <yuan2.hu@intel.com>
2023-05-10 13:57:33 +0800

    add test case for color convert
    Signed-off-by: Hu Yuan2 <yuan2.hu@intel.com>

---
+++ src/plugins/intel_cpu/tests/unit/shape_inference_test/color_covert_shape_inference.cpp
@@ -0,0 +1,89 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <gtest/gtest.h>
+
+#include "utils.hpp"
+
+using namespace ov;
+using namespace ov::intel_cpu;
+
+TEST(StaticShapeInferenceTest, ColorConvertNV12toBGR) {
+    auto data = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1, -1});
+    auto op = std::make_shared<op::v8::NV12toBGR>(data);
+    std::vector<StaticShape> static_input_shapes = {StaticShape{1, 720, 640, 1}};
+    std::vector<StaticShape> static_output_shapes = {StaticShape{}};
+    shape_inference(op.get(), static_input_shapes, static_output_shapes);
+    ASSERT_EQ(static_output_shapes[0], StaticShape({1, 480, 640, 3}));
+}
+
+TEST(StaticShapeInferenceTest, ColorConvertNV12toBGRMutliPlane) {
+    auto dataY = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1, -1});
+    auto dataUV = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1, -1});
+    auto op = std::make_shared<op::v8::NV12toBGR>(dataY, dataUV);
+    std::vector<StaticShape> static_input_shapes = {StaticShape{1, 480, 640, 1}, StaticShape{1, 240, 320, 2}};
+    std::vector<StaticShape> static_output_shapes = {StaticShape{}};
+    shape_inference(op.get(), static_input_shapes, static_output_shapes);
+    ASSERT_EQ(static_output_shapes[0], StaticShape({1, 480, 640, 3}));
+}
+
+TEST(StaticShapeInferenceTest, ColorConvertNV12toRGB) {
+    auto data = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1, -1});
+    auto op = std::make_shared<op::v8::NV12toRGB>(data);
+    std::vector<StaticShape> static_input_shapes = {StaticShape{1, 720, 640, 1}};
+    std::vector<StaticShape> static_output_shapes = {StaticShape{}};
+    shape_inference(op.get(), static_input_shapes, static_output_shapes);
+    ASSERT_EQ(static_output_shapes[0], StaticShape({1, 480, 640, 3}));
+}
+
+TEST(StaticShapeInferenceTest, ColorConvertNV12toRGBMutliPlane) {
+    auto dataY = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1, -1});
+    auto dataUV = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1, -1});
+    auto op = std::make_shared<op::v8::NV12toRGB>(dataY, dataUV);
+    std::vector<StaticShape> static_input_shapes = {StaticShape{1, 480, 640, 1}, StaticShape{1, 240, 320, 2}};
+    std::vector<StaticShape> static_output_shapes = {StaticShape{}};
+    shape_inference(op.get(), static_input_shapes, static_output_shapes);
+    ASSERT_EQ(static_output_shapes[0], StaticShape({1, 480, 640, 3}));
+}
+
+TEST(StaticShapeInferenceTest, ColorConvertI420toBGR) {
+    auto data = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1, -1});
+    auto op = std::make_shared<op::v8::I420toBGR>(data);
+    std::vector<StaticShape> static_input_shapes = {StaticShape{1, 720, 640, 1}};
+    std::vector<StaticShape> static_output_shapes = {StaticShape{}};
+    shape_inference(op.get(), static_input_shapes, static_output_shapes);
+    ASSERT_EQ(static_output_shapes[0], StaticShape({1, 480, 640, 3}));
+}
+
+TEST(StaticShapeInferenceTest, ColorConvertI420toBGRMutliPlane) {
+    auto dataY = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1, -1});
+    auto dataU = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1, -1});
+    auto dataV = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1, -1});
+    auto op = std::make_shared<op::v8::I420toBGR>(dataY, dataU, dataV);
+    std::vector<StaticShape> static_input_shapes = {StaticShape{1, 480, 640, 1}, StaticShape{1, 240, 320, 1}, StaticShape{1, 240, 320, 1}};
+    std::vector<StaticShape> static_output_shapes = {StaticShape{}};
+    shape_inference(op.get(), static_input_shapes, static_output_shapes);
+    ASSERT_EQ(static_output_shapes[0], StaticShape({1, 480, 640, 3}));
+}
+
+TEST(StaticShapeInferenceTest, ColorConvertI420toRGB) {
+    auto data = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1, -1});
+    auto op = std::make_shared<op::v8::I420toRGB>(data);
+    std::vector<StaticShape> static_input_shapes = {StaticShape{1, 720, 640, 1}};
+    std::vector<StaticShape> static_output_shapes = {StaticShape{}};
+    shape_inference(op.get(), static_input_shapes, static_output_shapes);
+    ASSERT_EQ(static_output_shapes[0], StaticShape({1, 480, 640, 3}));
+}
+
+TEST(StaticShapeInferenceTest, ColorConvertI420toRGBMutliPlane) {
+    auto dataY = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1, -1});
+    auto dataU = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1, -1});
+    auto dataV = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1, -1, -1});
+    auto op = std::make_shared<op::v8::I420toRGB>(dataY, dataU, dataV);
+    std::vector<StaticShape> static_input_shapes = {StaticShape{1, 480, 640, 1}, StaticShape{1, 240, 320, 1}, StaticShape{1, 240, 320, 1}};
+    std::vector<StaticShape> static_output_shapes = {StaticShape{}};
+    shape_inference(op.get(), static_input_shapes, static_output_shapes);
+    ASSERT_EQ(static_output_shapes[0], StaticShape({1, 480, 640, 3}));
+}
+
