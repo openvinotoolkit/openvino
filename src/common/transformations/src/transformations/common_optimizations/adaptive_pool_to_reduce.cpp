c1b8c00adadb552b5a37b3890696d7e758a26fe0
Maxim Vafin <maxim.vafin@intel.com>
2023-05-11 13:01:53 +0200

    Add tests and apply feedback

--- src/common/transformations/src/transformations/common_optimizations/adaptive_pool_to_reduce.cpp
+++ src/common/transformations/src/transformations/common_optimizations/adaptive_pool_to_reduce.cpp
@@ -9,24 +9,26 @@
 
 #include "itt.hpp"
 #include "openvino/core/rt_info.hpp"
-#include "openvino/opsets/opset11.hpp"
-#include "openvino/pass/pattern/op/or.hpp"
+#include "openvino/op/adaptive_avg_pool.hpp"
+#include "openvino/op/adaptive_max_pool.hpp"
+#include "openvino/op/constant.hpp"
+#include "openvino/op/reduce_max.hpp"
+#include "openvino/op/reduce_mean.hpp"
 #include "openvino/pass/pattern/op/wrap_type.hpp"
 #include "transformations/utils/utils.hpp"
+
+using namespace ov::op;
 
 ov::pass::AdaptivePoolToReduce::AdaptivePoolToReduce() {
     MATCHER_SCOPE(AdaptivePoolToReduce);
     auto data_pattern = pattern::any_input();
-    auto out_spatial_shape = pattern::wrap_type<opset11::Constant>();
-    auto a_avg_pool = pattern::wrap_type<opset11::AdaptiveAvgPool>({data_pattern, out_spatial_shape});
-    auto a_max_pool = pattern::wrap_type<opset11::AdaptiveMaxPool>({data_pattern, out_spatial_shape});
-    auto adaptive_pool = std::make_shared<pattern::op::Or>(OutputVector{a_avg_pool, a_max_pool});
+    auto out_spatial_shape = pattern::wrap_type<v0::Constant>();
+    auto a_pool = pattern::wrap_type<v8::AdaptiveAvgPool, v8::AdaptiveMaxPool>({data_pattern, out_spatial_shape});
 
     ov::matcher_pass_callback callback = [=](pattern::Matcher& m) {
-        const auto& pattern_map = m.get_pattern_value_map();
+        const auto& pattern_map = m.get_pattern_map();
 
-        const auto& spatial_shape_c =
-            std::dynamic_pointer_cast<opset11::Constant>(pattern_map.at(out_spatial_shape).get_node_shared_ptr());
+        const auto& spatial_shape_c = std::dynamic_pointer_cast<v0::Constant>(pattern_map.at(out_spatial_shape));
         auto spatial_shape = spatial_shape_c->cast_vector<int64_t>();
         // Verify that all dimensions in adaptive pool shape are 1
         for (auto& s : spatial_shape) {
@@ -36,26 +38,28 @@ ov::pass::AdaptivePoolToReduce::AdaptivePoolToReduce() {
 
         auto axes = std::vector<int64_t>(spatial_shape.size(), 0);
         std::iota(axes.begin(), axes.end(), 2);
-        auto axes_const = opset11::Constant::create(element::i64, {spatial_shape.size()}, axes);
-        if (pattern_map.count(a_avg_pool)) {
-            const auto adaptive_avg_pool = pattern_map.at(a_avg_pool).get_node_shared_ptr();
-            auto res_node = std::make_shared<opset11::ReduceMean>(adaptive_avg_pool->input_value(0), axes_const, true);
+        auto axes_const = v0::Constant::create(element::i64, {spatial_shape.size()}, axes);
+        const auto adaptive_pool = pattern_map.at(a_pool);
+        if (const auto adaptive_avg_pool = std::dynamic_pointer_cast<v8::AdaptiveAvgPool>(adaptive_pool)) {
+            auto res_node = std::make_shared<v1::ReduceMean>(adaptive_avg_pool->input_value(0), axes_const, true);
             replace_node(adaptive_avg_pool, res_node);
             res_node->set_friendly_name(adaptive_avg_pool->get_friendly_name());
             copy_runtime_info(adaptive_avg_pool, res_node);
-        } else if (pattern_map.count(a_max_pool)) {
-            const auto adaptive_max_pool = pattern_map.at(a_max_pool).get_node_shared_ptr();
-            auto res_node = std::make_shared<opset11::ReduceMax>(adaptive_max_pool->input_value(0), axes_const, true);
-            replace_node(adaptive_max_pool, res_node);
+        } else if (const auto adaptive_max_pool = std::dynamic_pointer_cast<v8::AdaptiveMaxPool>(adaptive_pool)) {
+            if (adaptive_max_pool->output(1).get_target_inputs().size() != 0) {
+                // If indexes are used we can't replace it
+                return false;
+            }
+            auto res_node = std::make_shared<v1::ReduceMax>(adaptive_max_pool->input_value(0), axes_const, true);
+            adaptive_max_pool->output(0).replace(res_node);
             res_node->set_friendly_name(adaptive_max_pool->get_friendly_name());
             copy_runtime_info(adaptive_max_pool, res_node);
         } else {
             return false;
         }
-
         return true;
     };
 
-    auto m = std::make_shared<ov::pass::pattern::Matcher>(adaptive_pool, matcher_name);
+    auto m = std::make_shared<ov::pass::pattern::Matcher>(a_pool, matcher_name);
     this->register_matcher(m, callback);
 }
