24a613f9e14a0a4542b585a728390361ec020050
Vishniakov Nikolai <nikolai.vishniakov@intel.com>
2023-05-11 16:28:42 +0200

    Align samples with updated API

--- samples/js/samples/classification.mjs
+++ samples/js/samples/classification.mjs
@@ -8,8 +8,7 @@ const layout = 'NHWC';
 export default async function(openvinojs) {
   const model = await openvinojs.loadModel(modelPath, shape, layout);
   const inputTensor = await getArrayByImgPath(imgPath);
-  const shapeObj = new openvinojs.Shape(shape);
-  const outputTensor = await model.infer(inputTensor, shapeObj);
+  const outputTensor = await model.infer(inputTensor, shape);
 
   printShape(outputTensor.shape);
   await printClass(outputTensor);
