ba31f8d77d1bba51ececcb7a3975a4af188198a2
panhaiqi <haiqi.pan@intel.com>
2023-05-11 14:01:32 +0800

    remove ... in iplugin_wrapper.cpp

--- src/inference/src/dev/iplugin_wrapper.cpp
+++ src/inference/src/dev/iplugin_wrapper.cpp
@@ -59,7 +59,7 @@ void IPluginWrapper::set_property(const ov::AnyMap& properties) {
 ov::Any IPluginWrapper::get_property(const std::string& name, const ov::AnyMap& arguments) const {
     try {
         return m_old_plugin->GetConfig(name, arguments);
-    } catch (...) {
+    } catch (const ov::Exception&) {
         return m_old_plugin->GetMetric(name, arguments);
     }
 }
