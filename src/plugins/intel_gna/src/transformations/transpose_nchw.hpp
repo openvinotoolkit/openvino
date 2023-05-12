2fbe2fae581bf32a04f08821f3cfb7095e039957
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2023-05-12 13:53:45 +0200

    Squashed commit of the following: GNAConv+ MaxPool
    commit 51e7254eab13c9f94e1ddea1142d3060e1f55528
    Author: Mikhail Ryzhov <mikhail.ryzhov@intel.com>
    Date:   Tue Apr 18 18:09:46 2023 +0200
    
        reset json schema
    
    commit 6d87c4070399036bc09d8622b6789f286dc1c877
    Author: Mikhail Ryzhov <mikhail.ryzhov@intel.com>
    Date:   Tue Apr 18 18:02:53 2023 +0200
    
        Exclude wrong changes
    
    commit 30dd9aef8662458b991e4db92f1cc2c19af0beb7
    Author: Mikhail Ryzhov <mikhail.ryzhov@intel.com>
    Date:   Tue Apr 18 17:56:54 2023 +0200
    
        clang format
    
    commit dad7151b37c3119165359ac093c28c6396f337af
    Author: Mikhail Ryzhov <mikhail.ryzhov@intel.com>
    Date:   Tue Apr 18 17:47:17 2023 +0200
    
        Init commit

---
+++ src/plugins/intel_gna/src/transformations/transpose_nchw.hpp
@@ -0,0 +1,36 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <ngraph/pass/graph_rewrite.hpp>
+
+namespace ov {
+namespace intel_gna {
+namespace pass {
+
+/**
+ * @brief TODO
+ */
+class SubstituteGNAConvolution : public ngraph::pass::MatcherPass {
+public:
+    NGRAPH_RTTI_DECLARATION;
+    SubstituteGNAConvolution();
+};
+
+class SubstituteGNAMaxPool : public ngraph::pass::MatcherPass {
+public:
+    NGRAPH_RTTI_DECLARATION;
+    SubstituteGNAMaxPool();
+};
+
+class TransposeNCHW : public ngraph::pass::FunctionPass {
+public:
+    NGRAPH_RTTI_DECLARATION;
+    bool run_on_model(const std::shared_ptr<ngraph::Function>& f) override;
+};
+
+}  // namespace pass
+}  // namespace intel_gna
+}  // namespace ov
