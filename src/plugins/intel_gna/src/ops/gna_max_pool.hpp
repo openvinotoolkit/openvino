22878a7280fa3810fd841180d75c5c72d52ed29c
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2023-05-12 13:53:45 +0200

    Squashed commit of the following: GNAConv+ MaxPool
    commit 51e7254eab13c9f94e1ddea1142d3060e1f55528
    Author: Mikhail Ryzhov <mikhail.ryzhov@intel.com>
    Date:   Tue Apr 18 18:09:46 2023 +0200
    
        reset json schema
    
    commit 6d87c4070399036bc09d8622b6789f286dc1c877
    Author: Mikhail Ryzhov <mikhail.ryzhov@intel.com>
    Date:   Tue Apr 18 18:02:53 2023 +0200
    
        Exclude wrong changes
    
    commit 30dd9aef8662458b991e4db92f1cc2c19af0beb7
    Author: Mikhail Ryzhov <mikhail.ryzhov@intel.com>
    Date:   Tue Apr 18 17:56:54 2023 +0200
    
        clang format
    
    commit dad7151b37c3119165359ac093c28c6396f337af
    Author: Mikhail Ryzhov <mikhail.ryzhov@intel.com>
    Date:   Tue Apr 18 17:47:17 2023 +0200
    
        Init commit

---
+++ src/plugins/intel_gna/src/ops/gna_max_pool.hpp
@@ -0,0 +1,108 @@
+// Copyright (C) 2018-2022 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <limits>
+
+#include "ngraph/node.hpp"
+#include "openvino/op/op.hpp"
+#include "openvino/op/util/attr_types.hpp"
+#include "openvino/op/util/max_pool_base.hpp"
+
+namespace ov {
+namespace intel_gna {
+namespace op {
+/// \brief Batched max pooling operation.
+class GNAMaxPool : public ov::op::Op {
+public:
+    NGRAPH_RTTI_DECLARATION;
+
+    /// \brief Constructs a batched max pooling operation.
+    GNAMaxPool() = default;
+
+    /// \brief Constructs a batched max pooling operation.
+    ///
+    /// \param arg The node producing the input data batch tensor.
+    /// \param strides The strides.
+    /// \param pads_begin The beginning of padding shape.
+    /// \param pads_end The end of padding shape.
+    /// \param kernel The kernel shape.
+    /// \param rounding_type Whether to use ceiling or floor rounding type while
+    /// computing output shape.
+    /// \param auto_pad The pad type for automatically computing padding sizes.
+    GNAMaxPool(const ngraph::Output<ngraph::Node>& arg,
+               const ngraph::Strides& strides,
+               const ngraph::Shape& pads_begin,
+               const ngraph::Shape& pads_end,
+               const ngraph::Shape& kernel,
+               const ov::op::RoundingType rounding_type = ov::op::RoundingType::FLOOR,
+               const ov::op::PadType auto_pad = ov::op::PadType::EXPLICIT);
+
+    void validate_and_infer_types() override;
+    bool visit_attributes(ov::AttributeVisitor& visitor) override;
+
+    /// \return The kernel shape.
+    const ngraph::Shape& get_kernel() const {
+        return m_kernel;
+    }
+    void set_kernel(const ngraph::Shape& kernel) {
+        m_kernel = kernel;
+    }
+    /// \return The strides.
+    const ngraph::Strides& get_strides() const {
+        return m_strides;
+    }
+    void set_strides(const ngraph::Strides& strides) {
+        m_strides = strides;
+    }
+    /// \return The beginning of padding shape.
+    const ngraph::Shape& get_pads_begin() const {
+        return m_pads_begin;
+    }
+    void set_pads_begin(const ngraph::Shape& pads_begin) {
+        m_pads_begin = pads_begin;
+    }
+    /// \return The end of padding shape.
+    const ngraph::Shape& get_pads_end() const {
+        return m_pads_end;
+    }
+    void set_adding_above(const ngraph::Shape& pads_end) {
+        m_pads_end = pads_end;
+    }
+    /// \return The pad type for pooling.
+    ov::op::PadType get_auto_pad() const {
+        return m_auto_pad;
+    }
+    void set_auto_pad(const ov::op::PadType auto_pad) {
+        m_auto_pad = auto_pad;
+    }
+    /// \return The ceiling mode being used for output shape computations
+    ov::op::RoundingType get_rounding_type() const {
+        return m_rounding_type;
+    }
+    void set_rounding_type(ov::op::RoundingType rounding_type) {
+        m_rounding_type = rounding_type;
+    }
+
+    std::shared_ptr<ngraph::Node> clone_with_new_inputs(const ov::OutputVector& new_args) const override;
+
+protected:
+    bool update_auto_padding(const ov::PartialShape& in_shape,
+                             const ngraph::Strides& filter_dilations,
+                             ngraph::Shape& new_pads_end,
+                             ngraph::Shape& new_pads_begin) const;
+
+    ov::PartialShape infer_output_shape(const ngraph::Strides& dilations);
+
+    ngraph::Shape m_kernel;
+    ngraph::Strides m_strides;
+    ngraph::Shape m_pads_begin;
+    ngraph::Shape m_pads_end;
+    ov::op::PadType m_auto_pad;
+    ov::op::RoundingType m_rounding_type;
+};
+}  // namespace op
+}  // namespace intel_gna
+}  // namespace ov
