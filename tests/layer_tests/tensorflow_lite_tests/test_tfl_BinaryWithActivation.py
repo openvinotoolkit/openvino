df58c163d88501cbb5c40ca446ee4b76f4d399ee
Ruslan Nugmanov <ruslan.nugmanov@intel.com>
2023-05-15 13:03:09 +0200

    Layer test tflite (#17410)
    * tfl argvalue test
    
    * tfl add_n test
    
    * tfl batch to space and space to batch
    
    * tfl batch to space and space to batch
    
    * tfl broadcast
    
    * tfl avgpool
    
    * tfl concat
    
    * tfl conv2d and depthtospace
    
    * tfl depthwise_conv2d
    
    * tfl broadcast_args
    
    * tfl fill
    
    * tfl floordiv
    
    * tfl fully_connected
    
    * tfl fully_connected
    
    * tfl gather
    
    * tfl gather_nd
    
    * tfl l2_normalization
    
    * tfl matrix_diag
    
    * tfl maxpool2d
    
    * tfl mirror_pad
    
    * tfl one_hot
    
    * tfl pack
    
    * tfl pad
    
    * tfl range
    
    * tfl relu
    
    * tfl reshape
    
    * tfl resize_bilinear
    
    * tfl resize
    
    * tfl reverse
    
    * tfl tfft2d
    
    * tfl scatter_nd
    
    * tfl rfft2d fix
    
    * tfl segment_sum
    
    * tfl select
    
    * tfl select_v2
    
    * tfl slice
    
    * tfl space_to_depth
    
    * tfl split
    
    * tfl split_v
    
    * tfl strided_slice
    
    * tfl tile
    
    * tfl topk_v2
    
    * tfl transpose
    
    * tfl transpose_conv
    
    * tfl fix conv tests
    
    * tfl unique + refactoring
    
    * tfl unpack
    
    * refactoring
    
    * tfl review - reformat params
    
    * tfl review - fixes
    
    * tfl review - fixes
    
    * tfl review - xfails
    
    * tfl review - remove redundant tickets
    
    * tfl review - xfail for pad

--- tests/layer_tests/tensorflow_lite_tests/test_tfl_BinaryWithActivation.py
+++ tests/layer_tests/tensorflow_lite_tests/test_tfl_BinaryWithActivation.py
@@ -1,10 +1,9 @@
-import itertools
-
-import tensorflow as tf
 import pytest
+import tensorflow as tf
 
 from common.tflite_layer_test_class import TFLiteLayerTest
-from common.utils.tflite_utils import data_generators, additional_test_params, activation_helper
+from common.utils.tflite_utils import additional_test_params, activation_helper
+from common.utils.tflite_utils import parametrize_tests
 
 test_ops = [
     {'op_name': 'ADD', 'op_func': tf.math.add},
@@ -18,26 +17,13 @@ test_params = [
     {'shape': [2, 10]}
 ]
 
-
-test_data = list(itertools.product(test_ops, test_params))
-for i, (parameters, shapes) in enumerate(test_data):
-    parameters.update(shapes)
-    test_data[i] = parameters.copy()
-
-test_data = list(itertools.product(test_data, additional_test_params[1]))
-for i, (parameters, additional_test_params[1]) in enumerate(test_data):
-    parameters.update(additional_test_params[1])
-    test_data[i] = parameters.copy()
+test_data = parametrize_tests(test_ops, test_params)
+test_data = parametrize_tests(test_data, additional_test_params[1])
 
 
 class TestTFLiteBinaryWithActivationLayerTest(TFLiteLayerTest):
     inputs = ["Input_0", "Input_1"]
     outputs = ["BinaryOperation"]
-
-    def _prepare_input(self, inputs_dict, generator=None):
-        if generator is None:
-            return super()._prepare_input(inputs_dict)
-        return data_generators[generator](inputs_dict)
 
     def make_model(self, params):
         assert len(set(params.keys()).intersection({'op_name', 'op_func', 'shape', 'activation'})) == 4, \
@@ -45,14 +31,12 @@ class TestTFLiteBinaryWithActivationLayerTest(TFLiteLayerTest):
         self.allowed_ops = [params['op_name']]
         tf.compat.v1.reset_default_graph()
         with tf.compat.v1.Session() as sess:
-            in0 = tf.compat.v1.placeholder(params.get('dtype', tf.float32), params['shape'],
-                                           name=TestTFLiteBinaryWithActivationLayerTest.inputs[0])
-            in1 = tf.compat.v1.placeholder(params.get('dtype', tf.float32), params['shape'],
-                                           name=TestTFLiteBinaryWithActivationLayerTest.inputs[1])
-            bin_op_name = TestTFLiteBinaryWithActivationLayerTest.outputs[0] if not params['activation'] else \
-            TestTFLiteBinaryWithActivationLayerTest.outputs[0] + "/op"
+            in0 = tf.compat.v1.placeholder(params.get('dtype', tf.float32), params['shape'], name=self.inputs[0])
+            in1 = tf.compat.v1.placeholder(params.get('dtype', tf.float32), params['shape'], name=self.inputs[1])
+            bin_op_name = self.outputs[0] if not params['activation'] else \
+                self.outputs[0] + "/op"
             op = params['op_func'](in0, in1, name=bin_op_name)
-            op = activation_helper(op, params['activation'], TestTFLiteBinaryWithActivationLayerTest.outputs[0])
+            op = activation_helper(op, params['activation'], self.outputs[0])
 
             net = sess.graph_def
         return net
