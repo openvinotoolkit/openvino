245acea92f1164b8a8e3ed11f4c601c0e00a00ee
Zhai, Xuejun <xuejun.zhai@intel.com>
2023-05-15 11:02:11 +0800

    Using mock engine
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>

--- src/bindings/c/tests/test_model_repo.hpp
+++ src/bindings/c/tests/test_model_repo.hpp
@@ -12,6 +12,7 @@
 #include "openvino/core/visibility.hpp"
 #include "openvino/pass/manager.hpp"
 #include "openvino/util/file_util.hpp"
+#include <common_test_utils/file_utils.hpp>
 
 namespace TestDataHelpers {
 
@@ -81,14 +82,21 @@ inline std::string generate_test_xml_file() {
 
     plugin_xml_file << "<ie>\n";
     plugin_xml_file << "    <plugins>\n";
-    plugin_xml_file << "        <plugin location=\""
-                    << ov::util::from_file_path(ov::util::get_compiled_plugin_path(libraryname))
-                    << "\" name=\"CUSTOM\">\n";
+    plugin_xml_file << "        <plugin location=\"";
+    plugin_xml_file << CommonTestUtils::getExecutableDirectory();
+    plugin_xml_file << ov::util::FileTraits<char>::file_separator;
+    plugin_xml_file << ov::util::FileTraits<char>::library_prefix();
+    plugin_xml_file << "mock_engine";
+    plugin_xml_file << IE_BUILD_POSTFIX;
+    plugin_xml_file << ov::util::FileTraits<char>::dot_symbol;
+    plugin_xml_file << ov::util::FileTraits<char>::library_ext();
+    plugin_xml_file << "\" name=\"CUSTOM\">\n";
     plugin_xml_file << "        </plugin>\n";
     plugin_xml_file << "    </plugins>\n";
     plugin_xml_file << "</ie>\n";
 
     // Close the file
+    plugin_xml_file.flush();
     plugin_xml_file.close();
     return plugin_xml;
 }
