f34cbce395cf22d123f482fe6fc0113010427538
almilosz <alicja.miloszewska@intel.com>
2023-05-16 14:28:58 +0200

    Fix eslint warnings from common_api_example.js

--- samples/js/node/common_api_example.js
+++ samples/js/node/common_api_example.js
@@ -1,38 +1,35 @@
-
-const { default: nodeAddon } = require('openvinojs-node')
-
+const { default: nodeAddon } = require('openvinojs-node');
 
 const math = require('./lib/helpers.js');
+const cv = require('opencv.js');
 const imagenetClassesMap = require('../assets/imagenet_classes_map.json');
 const Jimp = require('jimp');
 
+run();
 
-async function onRuntimeInitialized()
+async function run()
 {
-    const {loadModel, Shape, Tensor,  getDescriptionString} = nodeAddon;
-    console.log(await getDescriptionString());
-    const model = await loadModel('../assets/models/v3-small_224_1.0_float.xml', '../assets/models/v3-small_224_1.0_float.bin');
+  const {loadModel, Shape, Tensor, getDescriptionString} = nodeAddon;
+  console.log(await getDescriptionString());
+  const model = await loadModel(
+    '../assets/models/v3-small_224_1.0_float.xml',
+    '../assets/models/v3-small_224_1.0_float.bin',
+  );
 
-    const img_path =  '../assets/images/coco224x224.jpg';
-    const jimpSrc = await Jimp.read(img_path);
-    const src = cv.matFromImageData(jimpSrc.bitmap);
-    cv.cvtColor(src, src, cv.COLOR_RGBA2RGB);
+  const imgPath = '../assets/images/coco224x224.jpg';
+  const jimpSrc = await Jimp.read(imgPath);
+  const src = cv.matFromImageData(jimpSrc.bitmap);
+  cv.cvtColor(src, src, cv.COLOR_RGBA2RGB);
 
-    //create tensor
-    const tensor_data = new Float32Array(src.data);
-    const shape = new Shape([1, 224, 224, 3]);
-    const tensor = new Tensor("f32", tensor_data, shape);
+  //create tensor
+  const tensorData = new Float32Array(src.data);
+  const shape = new Shape([1, 224, 224, 3]);
+  const tensor = new Tensor('f32', tensorData, shape);
 
-    /*   ---Perform inference---   */
-    const output =  await model.infer(tensor, shape);
+  /*   ---Perform inference---   */
+  const output = await model.infer(tensor, shape);
 
-    //show the results
-    console.log("Result: " + imagenetClassesMap[math.argMax(output.data)]);
-    console.log(math.argMax(output.data)); 
+  //show the results
+  console.log('Result: ' + imagenetClassesMap[math.argMax(output.data)]);
+  console.log(math.argMax(output.data));
 }
-
-
-Module = {
-    onRuntimeInitialized
-};
-cv = require('opencv.js');
