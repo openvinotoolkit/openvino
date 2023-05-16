f041564294aa1451dbb06b69c2e67cbd05e0f0f4
Shen, Wanglei <wanglei.shen@intel.com>
2023-05-16 14:50:59 +0800

    keep WA of streams = 1 for ARM Linux

--- src/plugins/intel_cpu/src/config.cpp
+++ src/plugins/intel_cpu/src/config.cpp
@@ -271,7 +271,7 @@ void Config::readProperties(const std::map<std::string, std::string> &prop) {
         streamExecutorConfig._streams_changed = true;
     }
 
-#if (defined(OPENVINO_ARCH_ARM) || defined(OPENVINO_ARCH_ARM64))
+#if defined(OPENVINO_ARCH_ARM) || defined(OPENVINO_ARCH_ARM64)
     // TODO: multi-stream execution has functional issues on ARM target
     streamExecutorConfig._streams = 1;
 #endif
