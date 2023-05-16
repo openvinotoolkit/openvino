6d951d045149e4f8763ccc2ac32f42e49476473e
River Li <river.li@intel.com>
2023-05-16 16:37:58 +0800

    [AUTO_BATCH]Create separated auto_batch plugin testcase (#15051)
    * Create separated auto_batch plugin testcase
    
    * Add test sample into azure
    
    * Move to auto_plugin directory
    
    * Fix CI build issues
    
    * move batch test cases from gpu/cpu/template plugin to auto batch plugin
    
    * Check OpenCL to decide whether enable auto_batch gpu test cases
    
    * Revert "move batch test cases from gpu/cpu/template plugin to auto batch plugin"
    
    This reverts commit 9f4f2ce1afcdcc0d076cb3a2d0113bb2ba8a55a1.
    
    * Add functional tests for auto_batch
    
    * Runtime check gpu available to decide whether run gpu test cases
    
    * Remove HW plugins from functional test
    
    1. Apply Template plugin for auto_batch functional test
    2. Remove unnecessary code
    
    * Restore some original tests
    
    * Apply new api property replace old config
    
    * Solve warning suppressions issue
    
    * Fix CI build error
    
    * Solve CI failure issues
    
    * Fix getOutputsFromFunctionWithSeveralOutputs bug
    
    ---------
    
    Co-authored-by: Chen Peter <peter.chen@intel.com>

--- src/tests/functional/plugin/shared/include/base/ov_behavior_test_utils.hpp
+++ src/tests/functional/plugin/shared/include/base/ov_behavior_test_utils.hpp
@@ -49,7 +49,7 @@ private:
                                                                         new CommonTestUtils::CrashHandler(CommonTestUtils::CONFORMANCE_TYPE::api));
 
 protected:
-    size_t k = 1;
+    double k = 1.0;
     std::string target_device = "";
     ov::test::utils::ov_entity api_entity = ov::test::utils::ov_entity::undefined;
     ov::test::utils::ApiSummary& api_summary = ov::test::utils::ApiSummary::getInstance();
@@ -62,7 +62,7 @@ public:
     void SetUp() override {
         set_api_entity();
         auto test_name = this->GetFullTestName();
-        k = test_name.find("_mandatory") != std::string::npos || test_name.find("mandatory_") != std::string::npos ? 1 : 0;
+        k = test_name.find("_mandatory") != std::string::npos || test_name.find("mandatory_") != std::string::npos ? 1.0 : 0.0;
         std::cout << "[ CONFORMANCE ] Influence coefficient: " << k << std::endl;
         api_summary.updateStat(api_entity, target_device, ov::test::utils::PassRate::Statuses::CRASHED, k);
         crashHandler->StartTimer();
