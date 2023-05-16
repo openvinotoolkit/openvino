26da5034914b27cf84babecb4d978c93f7ad6cdf
Tatiana Savina <tatiana.savina@intel.com>
2023-05-16 12:05:32 +0200

    snippet comment

--- docs/snippets/compile_model.cpp
+++ docs/snippets/compile_model.cpp
@@ -2,6 +2,8 @@
 {
 //! [compile_model]
 
+#include <openvino/runtime/core.hpp>
+
 ov::Core.compile_model(device, modelPath, properties).export_model(compiled_blob);
 
 //!  [compile_model]
--- docs/snippets/compile_model.py
+++ docs/snippets/compile_model.py
@@ -1,5 +1,7 @@
 #! [compile_model]
 
+from openvino.runtime import Core
+
 ov.Core().compile_model(device, modelPath, properties).export_model(compiled_blob)
 
 #! [compile_model]
