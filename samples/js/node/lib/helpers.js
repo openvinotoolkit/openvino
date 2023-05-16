1127e18b45c5d99b076426f60de00d4a9e74faa8
Alicja Miloszewska <alicja.miloszewska@intel.com>
2023-05-16 08:06:34 +0200

    Update samples/js/node/lib/helpers.js
    Co-authored-by: Vishniakov Nikolai <nikolai.vishniakov@intel.com>

--- samples/js/node/lib/helpers.js
+++ samples/js/node/lib/helpers.js
@@ -5,12 +5,11 @@ module.exports = {
     let max = arr[0];
     let maxIndex = 0;
 
-    for (let i = 1; i < arr.length; i++) {
+    for (let i = 1; i < arr.length; i++)
       if (arr[i] > max) {
         maxIndex = i;
         max = arr[i];
       }
-    }
 
     return maxIndex;
   },
