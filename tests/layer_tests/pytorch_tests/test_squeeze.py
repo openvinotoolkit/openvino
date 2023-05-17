687d95daac9cbc02ab56245f82dc6cb70a9c2bf7
Mateusz <mateusz.mikolajczyk@intel.com>
2023-05-17 14:43:46 +0000

    Fix squeeze compatibility

--- tests/layer_tests/pytorch_tests/test_squeeze.py
+++ tests/layer_tests/pytorch_tests/test_squeeze.py
@@ -9,7 +9,8 @@ from pytorch_layer_test_class import PytorchLayerTest
 class TestSqueeze(PytorchLayerTest):
     def _prepare_input(self):
         import numpy as np
-        return (np.random.randn(1, 10).astype(np.float32),)
+
+        return (np.random.randn(1, 1, 32).astype(np.float32),)
 
     def create_model(self, dim):
         import torch
@@ -28,8 +29,8 @@ class TestSqueeze(PytorchLayerTest):
 
         return aten_squeeze(dim), ref_net, "aten::squeeze"
 
-    @pytest.mark.parametrize("dim,dynamic_shapes", [(-2, True), (0, True), (None, False)])
+    @pytest.mark.parametrize("dim", [-2, -1, 0, None, [-1, -2], 1, 2])
     @pytest.mark.nightly
     @pytest.mark.precommit
-    def test_squeeze(self, dim, ie_device, precision, ir_version, dynamic_shapes):
-        self._test(*self.create_model(dim), ie_device, precision, ir_version, dynamic_shapes=dynamic_shapes)
+    def test_squeeze(self, dim, ie_device, precision, ir_version):
+        self._test(*self.create_model(dim), ie_device, precision, ir_version, dynamic_shapes=False)
