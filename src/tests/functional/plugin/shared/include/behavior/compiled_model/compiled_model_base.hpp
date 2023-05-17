c4d464620916077ce4a338db40528a9d5b3685ab
panhaiqi <haiqi.pan@intel.com>
2023-05-17 17:40:15 +0800

    add test case

--- src/tests/functional/plugin/shared/include/behavior/compiled_model/compiled_model_base.hpp
+++ src/tests/functional/plugin/shared/include/behavior/compiled_model/compiled_model_base.hpp
@@ -160,11 +160,156 @@ TEST_P(OVCompiledModelBaseTest, canCompileModelFromMemory) {
     EXPECT_NO_THROW(auto execNet = core->compile_model(model, ov::Tensor(), target_device, configuration));
 }
 
+TEST_P(OVCompiledModelBaseTest, canCompileModelwithBrace) {
+ std::string model = R"V0G0N(
+        <net name="Network" version="10">
+            <layers>
+                <layer name="in1" type="Parameter" id="0" version="opset8">
+                    <data element_type="f16" shape="1,3,22,22"/>
+                    <output>
+                        <port id="0" precision="FP16" names="data1">
+                            <dim>1</dim>
+                            <dim>3</dim>
+                            <dim>22</dim>
+                            <dim>22</dim>
+                        </port>
+                    </output>
+                </layer>
+                <layer name="in2" type="Parameter" id="1" version="opset8">
+                    <data element_type="f16" shape="1,3,22,22"/>
+                    <output>
+                        <port id="0" precision="FP16" names="data2">
+                            <dim>1</dim>
+                            <dim>3</dim>
+                            <dim>22</dim>
+                            <dim>22</dim>
+                        </port>
+                    </output>
+                </layer>
+                <layer name="concat" id="2" type="Concat" version="opset8">
+                    <input>
+                        <port id="0" precision="FP16">
+                            <dim>1</dim>
+                            <dim>3</dim>
+                            <dim>22</dim>
+                            <dim>22</dim>
+                        </port>
+                        <port id="1"  precision="FP16">
+                            <dim>1</dim>
+                            <dim>3</dim>
+                            <dim>22</dim>
+                            <dim>22</dim>
+                        </port>
+                    </input>
+                    <output>
+                        <port id="2" precision="FP16" names="r">
+                            <dim>1</dim>
+                            <dim>6</dim>
+                            <dim>22</dim>
+                            <dim>22</dim>
+                        </port>
+                    </output>
+                </layer>
+                <layer name="output" type="Result" id="3" version="opset8">
+                    <input>
+                        <port id="0" precision="FP16">
+                            <dim>1</dim>
+                            <dim>6</dim>
+                            <dim>22</dim>
+                            <dim>22</dim>
+                        </port>
+                    </input>
+                </layer>
+            </layers>
+            <edges>
+                <edge from-layer="0" from-port="0" to-layer="2" to-port="0"/>
+                <edge from-layer="1" from-port="0" to-layer="2" to-port="1"/>
+                <edge from-layer="2" from-port="2" to-layer="3" to-port="0"/>
+            </edges>
+        </net>
+        )V0G0N";
+    ov::CompiledModel compiled_model = ov::Core{}.compile_model(model, target_device);
+    EXPECT_NO_THROW(compiled_model.get_property(ov::optimal_number_of_infer_requests));
+}
+
 TEST(OVCompiledModelBaseTest, canCompileModelToDefaultDevice) {
     std::shared_ptr<ov::Core> core = utils::PluginCache::get().core();
     std::shared_ptr<ov::Model> function = ngraph::builder::subgraph::makeSingleConcatWithConstant();
     EXPECT_NO_THROW(auto execNet = core->compile_model(function));
 }
+
+// TEST(OVCompiledModelBaseTest, canCompilewithBracket){
+//      std::string model = R"V0G0N(
+//         <net name="Network" version="10">
+//             <layers>
+//                 <layer name="in1" type="Parameter" id="0" version="opset8">
+//                     <data element_type="f16" shape="1,3,22,22"/>
+//                     <output>
+//                         <port id="0" precision="FP16" names="data1">
+//                             <dim>1</dim>
+//                             <dim>3</dim>
+//                             <dim>22</dim>
+//                             <dim>22</dim>
+//                         </port>
+//                     </output>
+//                 </layer>
+//                 <layer name="in2" type="Parameter" id="1" version="opset8">
+//                     <data element_type="f16" shape="1,3,22,22"/>
+//                     <output>
+//                         <port id="0" precision="FP16" names="data2">
+//                             <dim>1</dim>
+//                             <dim>3</dim>
+//                             <dim>22</dim>
+//                             <dim>22</dim>
+//                         </port>
+//                     </output>
+//                 </layer>
+//                 <layer name="concat" id="2" type="Concat" version="opset8">
+//                     <input>
+//                         <port id="0" precision="FP16">
+//                             <dim>1</dim>
+//                             <dim>3</dim>
+//                             <dim>22</dim>
+//                             <dim>22</dim>
+//                         </port>
+//                         <port id="1"  precision="FP16">
+//                             <dim>1</dim>
+//                             <dim>3</dim>
+//                             <dim>22</dim>
+//                             <dim>22</dim>
+//                         </port>
+//                     </input>
+//                     <output>
+//                         <port id="2" precision="FP16" names="r">
+//                             <dim>1</dim>
+//                             <dim>6</dim>
+//                             <dim>22</dim>
+//                             <dim>22</dim>
+//                         </port>
+//                     </output>
+//                 </layer>
+//                 <layer name="output" type="Result" id="3" version="opset8">
+//                     <input>
+//                         <port id="0" precision="FP16">
+//                             <dim>1</dim>
+//                             <dim>6</dim>
+//                             <dim>22</dim>
+//                             <dim>22</dim>
+//                         </port>
+//                     </input>
+//                 </layer>
+//             </layers>
+//             <edges>
+//                 <edge from-layer="0" from-port="0" to-layer="2" to-port="0"/>
+//                 <edge from-layer="1" from-port="0" to-layer="2" to-port="1"/>
+//                 <edge from-layer="2" from-port="2" to-layer="3" to-port="0"/>
+//             </edges>
+//         </net>
+//         )V0G0N";
+//     ov::CompiledModel compiled_model = ov::Core{}.compile_model(model, "CPU");
+//     EXPECT_NO_THROW(compiled_model.get_property(ov::optimal_number_of_infer_requests));
+
+// }
 
 TEST_P(OVCompiledModelBaseTest, canCompileModelAndCreateInferRequest) {
     auto execNet = core->compile_model(function, target_device, configuration);
