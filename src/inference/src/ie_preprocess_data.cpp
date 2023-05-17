d883fe8e191edf0c01875e7b7d387781329da390
Ilya Churaev <ilya.churaev@intel.com>
2023-05-17 13:56:15 +0400

    Remove preprocessing library

--- src/inference/src/ie_preprocess_data.cpp
+++ src/inference/src/ie_preprocess_data.cpp
@@ -2,10 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "ie_preprocess_gapi.hpp"
 #include "ie_system_conf.h"
 #include "ie_preprocess_data.hpp"
-#include "ie_preprocess_itt.hpp"
 
 #include "debug.h"
 #include <ie_input_info.hpp>
@@ -22,12 +20,6 @@ class PreProcessData : public IPreProcessData {
      * @brief ROI blob.
      */
     Blob::Ptr _userBlob = nullptr;
-
-    /**
-     * @brief Pointer-to-implementation (PIMPL) hiding preprocessing implementation details.
-     * BEWARE! Will be shared among copies!
-     */
-    std::shared_ptr<PreprocEngine> _preproc;
 
 public:
     void setRoiBlob(const Blob::Ptr &blob) override;
@@ -53,26 +45,11 @@ Blob::Ptr PreProcessData::getRoiBlob() const {
 
 void PreProcessData::execute(Blob::Ptr &preprocessedBlob, const PreProcessInfo &info, bool serial,
         int batchSize) {
-    OV_ITT_SCOPED_TASK(itt::domains::IEPreproc, "Preprocessing");
-
-    auto algorithm = info.getResizeAlgorithm();
-    auto fmt = info.getColorFormat();
-
-    if (_userBlob == nullptr || preprocessedBlob == nullptr) {
-        IE_THROW() << "Input pre-processing is called with null " << (_userBlob == nullptr ? "_userBlob" : "preprocessedBlob");
-    }
-
-    batchSize = PreprocEngine::getCorrectBatchSize(batchSize, _userBlob);
-
-    if (!_preproc) {
-        _preproc.reset(new PreprocEngine);
-    }
-
-    _preproc->preprocessWithGAPI(_userBlob, preprocessedBlob, algorithm, fmt, serial, batchSize);
+    IE_THROW() << "GAPI preprocess was removed!";
 }
 
 void PreProcessData::isApplicable(const Blob::Ptr &src, const Blob::Ptr &dst) {
-    PreprocEngine::checkApplicabilityGAPI(src, dst);
+    IE_THROW() << "GAPI preprocess was removed!";
 }
 
 }  // namespace InferenceEngine
