f8f9c2f3ebc0b4dc25c0630c3ecd366079be2f8e
Zhai, Xuejun <xuejun.zhai@intel.com>
2023-05-18 15:49:33 +0800

    [C API UNIT TEST] Fix error in ie_core_register_plugins
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>

--- src/bindings/c/tests/test_model_repo.hpp
+++ src/bindings/c/tests/test_model_repo.hpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <random>
 
+#include "common_test_utils/file_utils.hpp"
 #include "ngraph_functions/builders.hpp"
 #include "ngraph_functions/subgraph_builders.hpp"
 #include "openvino/core/visibility.hpp"
@@ -80,7 +81,15 @@ inline std::string generate_test_xml_file() {
 
     plugin_xml_file << "<ie>\n";
     plugin_xml_file << "    <plugins>\n";
-    plugin_xml_file << "        <plugin location=\"" << libraryname << "\" name=\"CUSTOM\">\n";
+    plugin_xml_file << "        <plugin location=\"";
+    plugin_xml_file << CommonTestUtils::getExecutableDirectory();
+    plugin_xml_file << ov::util::FileTraits<char>::file_separator;
+    plugin_xml_file << ov::util::FileTraits<char>::library_prefix();
+    plugin_xml_file << "mock_engine";
+    plugin_xml_file << IE_BUILD_POSTFIX;
+    plugin_xml_file << ov::util::FileTraits<char>::dot_symbol;
+    plugin_xml_file << ov::util::FileTraits<char>::library_ext();
+    plugin_xml_file << "\" name=\"CUSTOM\">\n";
     plugin_xml_file << "        </plugin>\n";
     plugin_xml_file << "    </plugins>\n";
     plugin_xml_file << "</ie>\n";
