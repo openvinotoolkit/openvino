0511f028da0c174dc102dae1f64cd88a969538e5
xufang <fang.xu@intel.com>
2023-05-18 17:57:46 +0800

    update tbb2020 package for windows
    add tbb binaries versioning
    
    tbb2020.3.3: https://github.com/oneapi-src/oneTBB/releases/tag/v2020.3.3

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -101,10 +101,10 @@ function(ov_download_tbb)
     if(WIN32 AND X86_64)
         # TODO: add target_path to be platform specific as well, to avoid following if
         RESOLVE_DEPENDENCY(TBB
-                ARCHIVE_WIN "tbb2020_617e9a71_win.zip"
+                ARCHIVE_WIN "tbb2020_81e4471_win.zip"
                 TARGET_PATH "${TEMP}/tbb"
                 ENVIRONMENT "TBBROOT"
-                SHA256 "01cac3cc48705bd52b83a6e1fa1ed95c708928be76160f5b9c5c37f954d56df4"
+                SHA256 "0255892d90c4acbb61089ed2e50e625016b32a887ce6e7311562ba503b6961dc"
                 USE_NEW_LOCATION TRUE)
     elseif(ANDROID AND X86_64)
         RESOLVE_DEPENDENCY(TBB
