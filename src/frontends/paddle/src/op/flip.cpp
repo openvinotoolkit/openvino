d549f63e5e7e7ae41c65768603f29215a9f9d3b3
Asthestarsfalll <1186454801@qq.com>
2023-05-18 18:40:19 +0800

    unify flip and reverse

--- src/frontends/paddle/src/op/flip.cpp
+++ src/frontends/paddle/src/op/flip.cpp
@@ -2,32 +2,14 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "default_opset.hpp"
-#include "openvino/frontend/paddle/node_context.hpp"
+#include "reverse_op.hpp"
 
 namespace ov {
 namespace frontend {
 namespace paddle {
 namespace op {
 NamedOutputs flip(const NodeContext& node) {
-    const auto data_node = node.get_input("X");
-    const auto axes = node.get_attribute<std::vector<int32_t>>("axis");
-    auto axes_length = axes.size();
-    const auto starts =
-        default_opset::Constant::create(element::i32,
-                                        {axes_length},
-                                        std::vector<int32_t>(axes_length, std::numeric_limits<int32_t>::max()));
-    const auto stops =
-        default_opset::Constant::create(element::i32,
-                                        {axes_length},
-                                        std::vector<int32_t>(axes_length, std::numeric_limits<int32_t>::min()));
-    const auto steps =
-        default_opset::Constant::create(element::i32, {axes_length}, std::vector<int32_t>(axes_length, -1));
-    const auto axes_node = default_opset::Constant::create(element::i32, {axes_length}, axes);
-
-    return node.default_single_output_mapping(
-        {std::make_shared<default_opset::Slice>(data_node, starts, stops, steps, axes_node)},
-        {"Out"});
+    return reverse_op(node);
 }
 }  // namespace op
 }  // namespace paddle
