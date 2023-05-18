14972f1c0430c273a800989e39dfcfba8501e46c
xuchen-intel <chen.xu@intel.com>
2023-05-19 00:06:13 +0800

    Extend bf16 optimization to int8

--- src/plugins/intel_cpu/src/nodes/reduce.cpp
+++ src/plugins/intel_cpu/src/nodes/reduce.cpp
@@ -1823,8 +1823,7 @@ void Reduce::initSupportedPrimitiveDescriptors() {
 
     precision_change = input_prec != output_prec;
     support_split = algorithm != Algorithm::ReduceL2 && algorithm != Algorithm::ReduceLogSumExp &&
-                    algorithm != Algorithm::ReduceSumSquare &&
-                    (!precision_change || (input_prec == Precision::BF16 && output_prec == Precision::FP32));
+                    algorithm != Algorithm::ReduceSumSquare;
 
     src_data_size = input_prec.size();
     dst_data_size = output_prec.size();
