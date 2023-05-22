4999e3f4cf0e692c0ba78478657467c4a095baca
Ilya Churaev <ilya.churaev@intel.com>
2023-05-22 10:24:45 +0400

    Added IN_OV_LIBRARY define

--- src/core/include/ngraph/attribute_adapter.hpp
+++ src/core/include/ngraph/attribute_adapter.hpp
@@ -4,7 +4,9 @@
 
 #pragma once
 
-#warning("The nGraph API is deprecated and will be removed in the 2024.0 release. For instructions on transitioning to the new API, please refer to https://docs.openvino.ai/latest/openvino_2_0_transition_guide.html")
+#ifndef IN_OV_LIBRARY
+#    warning("The nGraph API is deprecated and will be removed in the 2024.0 release. For instructions on transitioning to the new API, please refer to https://docs.openvino.ai/latest/openvino_2_0_transition_guide.html")
+#endif
 
 #include <string>
 #include <type_traits>
