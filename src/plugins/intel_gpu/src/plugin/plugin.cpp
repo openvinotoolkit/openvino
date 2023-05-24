0a4d2d7d183a1cb07e15625bc18fcc757d996d34
Ilya Churaev <ilya.churaev@intel.com>
2023-05-24 09:02:47 +0400

    Revert GPU plugin under proxy

--- src/plugins/intel_gpu/src/plugin/plugin.cpp
+++ src/plugins/intel_gpu/src/plugin/plugin.cpp
@@ -138,7 +138,7 @@ InferenceEngine::CNNNetwork Plugin::clone_and_transform_model(const InferenceEng
 }
 
 Plugin::Plugin() : m_default_contexts({}) {
-    _pluginName = "Intel_GPU";
+    _pluginName = "GPU";
     register_primitives();
     // try loading gpu engine and get info from it
     {
