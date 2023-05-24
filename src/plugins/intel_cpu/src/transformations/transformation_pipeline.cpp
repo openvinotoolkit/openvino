c2fedcf2f4feb0f7560bca9ef4a4008ca2747e7b
Pavel Esir <pavel.esir@intel.com>
2023-05-24 12:52:56 +0200

    added TODOs with ticket numbers

--- src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
+++ src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
@@ -198,6 +198,7 @@ void Transformations::PreLpt(const std::vector<ov::element::Type>& defaultPrecis
     ov::pass::Manager manager;
     manager.set_per_pass_validation(false);
     CPU_REGISTER_PASS_COMMON(manager, ov::pass::InitNodeInfo);
+    // todo: should be uncommented when xxx-105060 is ready
     CPU_REGISTER_PASS_COMMON(manager, ov::pass::KeepDecompressionsInFP32);
 
     const bool useLpt = !defaultPrecisions.empty();
@@ -257,6 +258,9 @@ void Transformations::PreLpt(const std::vector<ov::element::Type>& defaultPrecis
     }
     CPU_REGISTER_PASS_COMMON(manager, ov::pass::Validate);
     CPU_REGISTER_PASS_COMMON(manager, ov::pass::RefConvertI64ToI32);
+
+    // todo: remove this conversion line when xxx-105060 is ready and activate conversion at the end of the pipeline
+    // CPU_REGISTER_PASS_COMMON(manager, ov::pass::ConvertPrecision, precisions, type_to_fuse);
 
     CPU_REGISTER_PASS_COMMON(manager, ov::pass::EliminateConvert);
     CPU_REGISTER_PASS_COMMON(manager, SwapConvertTranspose);
@@ -455,6 +459,7 @@ void Transformations::PreLpt(const std::vector<ov::element::Type>& defaultPrecis
             ov::pass::ConvertQuantizeDequantize);
     }
 
+    // todo: should be uncommented when xxx-105060 is ready
     CPU_REGISTER_PASS_COMMON(manager, ov::pass::ConvertPrecision, precisions, type_to_fuse);
 
     manager.run_passes(model);
