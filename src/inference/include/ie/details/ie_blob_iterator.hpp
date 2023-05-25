725b4a8f2d2f79eb3b6fe07f4946b34bdb33ff2b
Ilya Churaev <ilya.churaev@intel.com>
2023-05-25 07:59:45 +0400

    Fixed snippets and throw only first error

--- src/inference/include/ie/details/ie_blob_iterator.hpp
+++ src/inference/include/ie/details/ie_blob_iterator.hpp
@@ -10,7 +10,8 @@
 
 #pragma once
 
-#ifndef IN_OV_COMPONENT
+#if !defined(IN_OV_COMPONENT) && !defined(IE_LEGACY_HEADER_INCLUDED)
+#    define IE_LEGACY_HEADER_INCLUDED
 #    ifdef _MSC_VER
 #        pragma message( \
             "The Inference Engine API is deprecated and will be removed in the 2024.0 release. For instructions on transitioning to the new API, please refer to https://docs.openvino.ai/latest/openvino_2_0_transition_guide.html")
