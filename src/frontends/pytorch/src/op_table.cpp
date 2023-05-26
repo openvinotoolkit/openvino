b2aaa10ef69017559b14645b5e99cb6132cdda06
Ekaterina Aidova <ekaterina.aidova@intel.com>
2023-05-26 19:27:05 +0400

    [PT FE]: support aten::unflatten (#17736)
    * [PT FE]: support aten::unflatten
    
    * Update src/frontends/pytorch/src/utils.cpp
    
    Co-authored-by: Maxim Vafin <maxim.vafin@intel.com>
    
    ---------
    
    Co-authored-by: Maxim Vafin <maxim.vafin@intel.com>

--- src/frontends/pytorch/src/op_table.cpp
+++ src/frontends/pytorch/src/op_table.cpp
@@ -126,6 +126,7 @@ OP_CONVERTER(translate_topk);
 OP_CONVERTER(translate_transpose);
 OP_CONVERTER(translate_tril);
 OP_CONVERTER(translate_triu);
+OP_CONVERTER(translate_unflatten);
 OP_CONVERTER(translate_unfold);
 OP_CONVERTER(translate_upsample_bicubic2d);
 OP_CONVERTER(translate_upsample_bilinear2d);
@@ -338,6 +339,7 @@ const std::map<std::string, CreatorFunction> get_supported_ops() {
         {"aten::triu", op::translate_triu},
         {"aten::type_as",
          op::translate_1to1_match_2_inputs<opset10::ConvertLike>},  // TODO: overflow semantics is different
+        {"aten::unflatten", op::translate_unflatten},
         {"aten::unfold", op::translate_unfold},
         {"aten::unsqueeze", op::translate_1to1_match_2_inputs<opset10::Unsqueeze>},
         {"aten::unsqueeze_", op::inplace_op<op::translate_1to1_match_2_inputs<opset10::Unsqueeze>>},
