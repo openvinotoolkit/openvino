35b453adb7faa75685f2b71a4e2b90f1af03cd39
mitruska <katarzyna.mitrus@intel.com>
2023-05-26 15:07:36 +0200

    Update ov scope name

--- src/core/src/op/util/pad_base.cpp
+++ src/core/src/op/util/pad_base.cpp
@@ -66,13 +66,13 @@ CoordinateDiff ov::op::util::PadBase::get_pads_end() const {
 }
 
 bool ov::op::util::PadBase::visit_attributes(AttributeVisitor& visitor) {
-    OV_OP_SCOPE(v1_Pad_visit_attributes);
+    OV_OP_SCOPE(util_PadBase_visit_attributes);
     visitor.on_attribute("pad_mode", m_pad_mode);
     return true;
 }
 
 void ov::op::util::PadBase::validate_and_infer_types() {
-    OV_OP_SCOPE(v1_Pad_validate_and_infer_types);
+    OV_OP_SCOPE(util_PadBase_validate_and_infer_types);
     element::Type result_et = element::dynamic;
 
     const auto& arg_element_type = get_input_element_type(0);
@@ -117,7 +117,7 @@ void ov::op::util::PadBase::validate_and_infer_types() {
 }
 
 shared_ptr<Node> ov::op::util::PadBase::clone_with_new_inputs(const OutputVector& new_args) const {
-    OV_OP_SCOPE(v1_Pad_clone_with_new_inputs);
+    OV_OP_SCOPE(util_PadBase_clone_with_new_inputs);
     check_new_args_count(this, new_args);
     if (get_input_size() == 4) {
         return make_shared<v1::Pad>(new_args.at(0), new_args.at(1), new_args.at(2), new_args.at(3), m_pad_mode);
@@ -170,27 +170,27 @@ bool ov::op::util::PadBase::evaluate_pad(const HostTensorVector& outputs, const 
 }
 
 bool ov::op::util::PadBase::evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const {
-    OV_OP_SCOPE(v1_Pad_evaluate);
+    OV_OP_SCOPE(util_PadBase_evaluate);
     return evaluate_pad(outputs, inputs);
 }
 
 bool ov::op::util::PadBase::has_evaluate() const {
-    OV_OP_SCOPE(v1_Pad_has_evaluate);
+    OV_OP_SCOPE(util_PadBase_has_evaluate);
     return true;
 }
 
 bool ov::op::util::PadBase::evaluate_lower(ov::TensorVector& output_values) const {
-    OV_OP_SCOPE(v1_Pad_evaluate_lower);
+    OV_OP_SCOPE(util_PadBase_evaluate_lower);
     return ov::have_node_inputs_bounds_set(this, 1, 2) && ov::default_lower_bound_evaluator(this, output_values);
 }
 
 bool ov::op::util::PadBase::evaluate_upper(ov::TensorVector& output_values) const {
-    OV_OP_SCOPE(v1_Pad_evaluate_upper);
+    OV_OP_SCOPE(util_PadBase_evaluate_upper);
     return ov::have_node_inputs_bounds_set(this, 1, 2) && ov::default_upper_bound_evaluator(this, output_values);
 }
 
 bool ov::op::util::PadBase::evaluate_label(ov::TensorLabelVector& output_labels) const {
-    OV_OP_SCOPE(v1_Pad_evaluate_label);
+    OV_OP_SCOPE(util_PadBase_evaluate_label);
     OPENVINO_SUPPRESS_DEPRECATED_START
     return ov::default_label_evaluator(this, output_labels);
     OPENVINO_SUPPRESS_DEPRECATED_END
