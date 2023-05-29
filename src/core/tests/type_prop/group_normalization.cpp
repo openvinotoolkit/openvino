3517249de0b40f601ae57b0433f75160841b6521
tomdol <tomasz.dolbniak@intel.com>
2023-05-29 16:03:22 +0200

    More tests

--- src/core/tests/type_prop/group_normalization.cpp
+++ src/core/tests/type_prop/group_normalization.cpp
@@ -21,6 +21,17 @@ TEST(type_prop, group_normalization_basic) {
     EXPECT_EQ(gn->get_shape(), (Shape{1, 12, 6, 6}));
 }
 
+TEST(type_prop, group_normalization_labels) {
+    auto data_shape = PartialShape{1, 12, 6, 6};
+    set_shape_labels(data_shape, 43);
+    const auto data = std::make_shared<opset12::Parameter>(element::f32, data_shape);
+    const auto scale = std::make_shared<opset12::Parameter>(element::f32, Shape{12});
+    const auto bias = std::make_shared<opset12::Parameter>(element::f32, Shape{12});
+
+    const auto gn = std::make_shared<opset12::GroupNormalization>(data, scale, bias, 4, 0.00001f);
+    EXPECT_THAT(get_shape_labels(gn->get_output_partial_shape(0)), ElementsAre(43, 44, 45, 46));
+}
+
 TEST(type_prop, group_normalization_dynamic_channels) {
     const auto data = std::make_shared<opset12::Parameter>(element::f32, PartialShape{1, -1, 6, 6});
     const auto scale = std::make_shared<opset12::Parameter>(element::f32, Shape{12});
@@ -69,6 +80,16 @@ TEST(type_prop, group_normalization_dynamic_everything) {
     const auto gn = std::make_shared<opset12::GroupNormalization>(data, scale, bias, 7, 0.00001f);
     EXPECT_EQ(gn->get_element_type(), element::f16);
     EXPECT_EQ(gn->get_output_partial_shape(0), (PartialShape{3, -1, 10, 10}));
+}
+
+TEST(type_prop, group_normalization_dynamic_intervals) {
+    const auto data = std::make_shared<opset12::Parameter>(element::f32, PartialShape{2, Dimension{10, 20}, 6, 6});
+    const auto scale = std::make_shared<opset12::Parameter>(element::f32, PartialShape{Dimension{10, 20}});
+    const auto bias = std::make_shared<opset12::Parameter>(element::f32, PartialShape{Dimension{10, 20}});
+
+    const auto gn = std::make_shared<opset12::GroupNormalization>(data, scale, bias, 2, 0.00001f);
+    EXPECT_EQ(gn->get_element_type(), element::f32);
+    EXPECT_EQ(gn->get_output_partial_shape(0), (PartialShape{2, Dimension{10, 20}, 6, 6}));
 }
 
 TEST(type_prop, group_normalization_incorrect_scale_shape) {
