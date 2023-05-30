9b43aed6968d9a5d76d2c596a460ac7d99f0ed2e
Anton Voronov <anton.voronov@intel.com>
2023-05-30 20:12:42 +0000

    code examples small changes

--- docs/snippets/cpu/ov_execution_mode.cpp
+++ docs/snippets/cpu/ov_execution_mode.cpp
@@ -7,7 +7,10 @@
 int main() {
     //! [ov:execution_mode:part0]
     ov::Core core;
+    // in case of Accuracy
     core.set_property("CPU", ov::hint::execution_mode(ov::hint::ExecutionMode::ACCURACY));
+    // in case of Performance
+    core.set_property("CPU", ov::hint::execution_mode(ov::hint::ExecutionMode::PERFORMANCE));
     //! [ov:execution_mode:part0]
 
     return 0;
