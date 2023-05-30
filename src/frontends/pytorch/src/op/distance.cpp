16eed2c27e116e1b01197ba4edb28825cce13b25
eaidova <ekaterina.aidova@intel.com>
2023-05-30 13:19:54 +0400

    p and eps non-const

--- src/frontends/pytorch/src/op/distance.cpp
+++ src/frontends/pytorch/src/op/distance.cpp
@@ -3,7 +3,10 @@
 //
 
 #include "openvino/frontend/pytorch/node_context.hpp"
+#include "openvino/op/add.hpp"
 #include "openvino/op/constant.hpp"
+#include "openvino/op/convert.hpp"
+#include "openvino/op/divide.hpp"
 #include "openvino/op/power.hpp"
 #include "openvino/op/reduce_sum.hpp"
 #include "openvino/op/shape_of.hpp"
@@ -23,18 +26,18 @@ namespace {
 Output<Node> pairwise_distance(const NodeContext& context,
                                Output<Node> x,
                                Output<Node> y,
-                               float p,
-                               float eps,
+                               Output<Node> p,
+                               Output<Node> eps,
                                bool keepdim) {
-    auto p_node = context.mark_node(v0::Constant::create(element::f32, Shape{}, {p}));
-    double inv_p = 1 / (p + eps);
-    auto inv_p_node = context.mark_node(v0::Constant::create(element::f32, Shape{}, {inv_p}));
+    auto one = context.mark_node(v0::Constant::create(element::f32, Shape{}, {1}));
+    auto p_plus_eps = context.mark_node(std::make_shared<v1::Add>(p, eps));
+    auto inv_p = context.mark_node(std::make_shared<v1::Divide>(one, p_plus_eps));
     auto minus_one = context.mark_node(v0::Constant::create(element::i32, Shape{1}, {-1}));
     align_eltwise_input_types(context, x, y, true);
     auto x_y_diff = context.mark_node(std::make_shared<v1::Subtract>(x, y));
-    auto x_y_diff_in_p_power = context.mark_node(std::make_shared<v1::Power>(x_y_diff, p_node));
+    auto x_y_diff_in_p_power = context.mark_node(std::make_shared<v1::Power>(x_y_diff, p));
     auto summation = context.mark_node(std::make_shared<v1::ReduceSum>(x_y_diff_in_p_power, minus_one, keepdim));
-    auto summation_in_inv_p = context.mark_node(std::make_shared<v1::Power>(summation, inv_p_node));
+    auto summation_in_inv_p = context.mark_node(std::make_shared<v1::Power>(summation, inv_p));
     return summation_in_inv_p;
 }
 };  // namespace
@@ -45,9 +48,10 @@ OutputVector translate_cdist(const NodeContext& context) {
     num_inputs_check(context, 2, 4);
     auto x = context.get_input(0);
     auto y = context.get_input(1);
-    float p = 2.0;
+    Output<Node> p = context.mark_node(v0::Constant::create(element::f32, Shape{}, {2.0}));
     if (!context.input_is_none(2)) {
-        p = context.const_input<float>(2);
+        p = context.get_input(2);
+        p = context.mark_node(std::make_shared<v0::Convert>(p, element::f32));
     }
     auto input_shape = context.mark_node(std::make_shared<v3::ShapeOf>(x, element::i32));
     auto input_rank = context.mark_node(std::make_shared<v3::ShapeOf>(input_shape, element::i32));
@@ -57,7 +61,8 @@ OutputVector translate_cdist(const NodeContext& context) {
     auto y_unsqueeze_ax = context.mark_node(std::make_shared<v1::Subtract>(input_rank, two));
     auto x_unsqueeze = context.mark_node(std::make_shared<v0::Unsqueeze>(x, x_unsqueeze_ax));
     auto y_unsqueeze = context.mark_node(std::make_shared<v0::Unsqueeze>(y, y_unsqueeze_ax));
-    auto result = pairwise_distance(context, x_unsqueeze, y_unsqueeze, p, 1e-06, false);
+    auto eps = context.mark_node(v0::Constant::create(element::f32, Shape{}, {1e-06}));
+    auto result = pairwise_distance(context, x_unsqueeze, y_unsqueeze, p, eps, false);
     return {result};
 };
 
@@ -66,14 +71,16 @@ OutputVector translate_pairwise_distance(const NodeContext& context) {
     num_inputs_check(context, 2, 5);
     auto x = context.get_input(0);
     auto y = context.get_input(1);
-    float p = 2.0;
-    float eps = 1e-06;
+    Output<Node> p = context.mark_node(v0::Constant::create(element::f32, Shape{}, {2.0}));
+    Output<Node> eps = context.mark_node(v0::Constant::create(element::f32, Shape{}, {1e-06}));
     bool keepdims = false;
     if (!context.input_is_none(2)) {
-        p = context.const_input<float>(2);
+        p = context.get_input(2);
+        p = context.mark_node(std::make_shared<v0::Convert>(p, element::f32));
     }
     if (!context.input_is_none(3)) {
-        eps = context.const_input<float>(3);
+        eps = context.get_input(3);
+        eps = context.mark_node(std::make_shared<v0::Convert>(eps, element::f32));
     }
     if (!context.input_is_none(4)) {
         keepdims = context.const_input<bool>(4);
