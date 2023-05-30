5a49a3550e8630d94261e85d8fe673f95d72d4e8
tomdol <tomasz.dolbniak@intel.com>
2023-05-30 14:22:11 +0200

    attributes visitor tests

--- src/core/include/openvino/opsets/opset12_tbl.hpp
+++ src/core/include/openvino/opsets/opset12_tbl.hpp
@@ -205,4 +205,5 @@ _OPENVINO_OP_REG(Unique, ov::op::v10)
 _OPENVINO_OP_REG(Interpolate, ov::op::v11)
 _OPENVINO_OP_REG(TopK, ov::op::v11)
 
+// New operations added in opset12
 _OPENVINO_OP_REG(GroupNormalization, ov::op::v12)
