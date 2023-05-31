239523e376e364920e5db4c725aee0f31e612bd1
tomdol <tomasz.dolbniak@intel.com>
2023-05-31 13:17:32 +0200

    Opset registration

--- src/tests/functional/plugin/shared/src/single_layer_tests/op_impl_check/single_op_graph.cpp
+++ src/tests/functional/plugin/shared/src/single_layer_tests/op_impl_check/single_op_graph.cpp
@@ -1893,6 +1893,7 @@ OpGenerator getOpGeneratorMap() {
 #include "openvino/opsets/opset9_tbl.hpp"
 #include "openvino/opsets/opset10_tbl.hpp"
 #include "openvino/opsets/opset11_tbl.hpp"
+#include "openvino/opsets/opset12_tbl.hpp"
 #undef _OPENVINO_OP_REG
     };
     return opGeneratorMap;
