6c4829348241382978f07441ea3269f6dfa35628
Zhang Yi3 <yi3.zhang@intel.com>
2023-05-31 14:26:24 +0800

    update gemm api

--- src/plugins/intel_cpu/src/gemm/ov_cpu_gemm.cpp
+++ src/plugins/intel_cpu/src/gemm/ov_cpu_gemm.cpp
@@ -59,6 +59,35 @@ void ov_sgemm_compute(const char* transa,
                       float* C,
                       const int64_t ldc) {
     // C = alpha*op( A )op( B ) + beta * C
+    std::vector<MLAS_SGEMM_DATA_PARAMS> data(1);
+    data[0].BIsPacked = false;
+    data[0].A = A;
+    data[0].lda = lda;
+    data[0].B = B;
+    data[0].ldb = ldb;
+    data[0].C = C;
+    data[0].ldc = ldc;
+    data[0].alpha = alpha;
+    data[0].beta = beta;
+    auto _transa = *transa == 'N' ? CblasNoTrans : CblasTrans;
+    auto _transb = *transb == 'N' ? CblasNoTrans : CblasTrans;
+    MlasGemmBatch(_transa, _transb, M, N, K, data.data(), 1, nullptr);
+}
+
+void ov_sgemm_pack_compute(const char* transa,
+                      const char* transb,
+                      const int64_t M,
+                      const int64_t N,
+                      const int64_t K,
+                      const float alpha,
+                      const float* A,
+                      const int64_t lda,
+                      const float* B,
+                      const int64_t ldb,
+                      const float beta,
+                      float* C,
+                      const int64_t ldc) {
+    // C = alpha*op( A )op( B ) + beta * C
     ov::cpu::ThreadPool threadPool;
     std::vector<MLAS_SGEMM_DATA_PARAMS> data(1);
     data[0].BIsPacked = true;
