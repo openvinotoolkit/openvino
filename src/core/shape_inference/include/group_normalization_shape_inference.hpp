216451962d9dbaa649e772b35038aaa46750e133
tomdol <tomasz.dolbniak@intel.com>
2023-05-31 13:06:05 +0200

    Cleanup and more tests

--- src/core/shape_inference/include/group_normalization_shape_inference.hpp
+++ src/core/shape_inference/include/group_normalization_shape_inference.hpp
@@ -10,39 +10,29 @@ namespace ov {
 namespace op {
 namespace v12 {
 template <class TShape>
-std::vector<TShape> shape_infer(const GroupNormalization* op,
-                                const std::vector<TShape>& input_shapes,
-                                const std::map<size_t, HostTensorPtr>& constant_data = {}) {
+std::vector<TShape> shape_infer(const GroupNormalization* op, const std::vector<TShape>& input_shapes) {
     const auto& inputs_count = input_shapes.size();
     NODE_VALIDATION_CHECK(op, (inputs_count == 3));
-    TShape output_shape;
 
-    const auto data_partial_shape = input_shapes[0];
-    const auto data_rank = data_partial_shape.rank();
-    const auto scale_partial_shape = input_shapes[1];
-    const auto bias_partial_shape = input_shapes[2];
+    const auto& data_shape = input_shapes[0];
+    const auto& data_rank = data_shape.rank();
+    const auto& scale_shape = input_shapes[1];
+    const auto& bias_shape = input_shapes[2];
 
     NODE_VALIDATION_CHECK(op, op->get_num_groups() > 0, "The number of groups needs to be a positive integer value");
 
-    NODE_VALIDATION_CHECK(op,
-                          scale_partial_shape.rank().compatible(Dimension{1}),
-                          "The scale input is required to be 1D");
-    NODE_VALIDATION_CHECK(op,
-                          bias_partial_shape.rank().compatible(Dimension{1}),
-                          "The bias input is required to be 1D");
-
-    NODE_VALIDATION_CHECK(op,
-                          data_rank.is_dynamic() || data_rank.get_length() >= 2,
-                          "The input tensor is required to be at least 2D");
+    NODE_VALIDATION_CHECK(op, scale_shape.rank().compatible(1), "The scale input is required to be 1D");
+    NODE_VALIDATION_CHECK(op, bias_shape.rank().compatible(1), "The bias input is required to be 1D");
 
     if (data_rank.is_static()) {
-        const auto channels_dim = data_partial_shape[1];
-        NODE_VALIDATION_CHECK(
-            op,
-            scale_partial_shape.rank().is_dynamic() || channels_dim.compatible(scale_partial_shape[0]),
-            "The scale input shape needs to match the channel dimension in the data input");
+        NODE_VALIDATION_CHECK(op, data_rank.get_length() >= 2, "The input tensor is required to be at least 2D");
+
+        const auto& channels_dim = data_shape[1];
         NODE_VALIDATION_CHECK(op,
-                              bias_partial_shape.rank().is_dynamic() || channels_dim.compatible(bias_partial_shape[0]),
+                              scale_shape.rank().is_dynamic() || channels_dim.compatible(scale_shape[0]),
+                              "The scale input shape needs to match the channel dimension in the data input");
+        NODE_VALIDATION_CHECK(op,
+                              bias_shape.rank().is_dynamic() || channels_dim.compatible(bias_shape[0]),
                               "The bias input shape needs to match the channel dimension in the data input");
 
         NODE_VALIDATION_CHECK(op,
@@ -55,7 +45,7 @@ std::vector<TShape> shape_infer(const GroupNormalization* op,
     }
 
     NODE_VALIDATION_CHECK(op,
-                          scale_partial_shape.compatible(bias_partial_shape),
+                          scale_shape.compatible(bias_shape),
                           "The shapes of both scale and bias inputs need to match");
 
     return {input_shapes[0]};
@@ -64,9 +54,8 @@ std::vector<TShape> shape_infer(const GroupNormalization* op,
 template <class TShape>
 void shape_infer(const GroupNormalization* op,
                  const std::vector<TShape>& input_shapes,
-                 std::vector<TShape>& output_shapes,
-                 const std::map<size_t, HostTensorPtr>& constant_data = {}) {
-    output_shapes = shape_infer(op, input_shapes, constant_data);
+                 std::vector<TShape>& output_shapes) {
+    output_shapes = shape_infer(op, input_shapes);
 }
 }  // namespace v12
 }  // namespace op
