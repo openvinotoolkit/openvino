a8e2cf090d674de1d5657bd9d5b63f30d8cb4cbe
Lyamin-Roman <roman.lyamin@intel.com>
2023-06-01 21:20:24 +0900

    [GPU] Additional condition for remove_redundant_reorders

--- src/plugins/intel_gpu/src/graph/graph_optimizer/remove_redundant_reorders.cpp
+++ src/plugins/intel_gpu/src/graph/graph_optimizer/remove_redundant_reorders.cpp
@@ -275,7 +275,8 @@ void remove_redundant_reorders::run(program& p) {
             !r_node.get_primitive()->subtract_per_feature.empty() ||
             no_output_optimization ||
             r_node.has_fused_primitives() ||
-            r_node.get_primitive()->has_surface_input())
+            r_node.get_primitive()->has_surface_input() ||
+            r_node.get_primitive()->transposed)
             continue;
 
         auto o_layout = r_node.get_output_layout();
