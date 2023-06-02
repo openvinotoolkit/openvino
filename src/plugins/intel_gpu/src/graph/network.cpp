81fb4a46a16c614cc18e847449b42097189b7f27
Taylor Yeonbok Lee <taylor.lee@intel.com>
2023-06-02 12:39:28 -0700

    [GPU] Enable runtime buffer fusing for dynamic shape (#17668)
    * Initial impl for runtime buffer fusing
    Passing unittest with static kernel
    
    * pass unittest with dynamic impl
    
    * Refactor allocate_output
    
    * Separate header of buffer fusing
    
    * Refactored buffer fusing :: matcher/optimize
    
    * More cleanup
    
    * Fix crash in dolly
    
    * Reset can_be_optimized of primitive_inst when it is not
    
    * Fix empty tensor : Primitive with empty data should be skipped
    
    * Fix issue in dynamic padding : Static kernel should not contain dynamic padding dims
    Fix missing reset of update_shape_done_by_other flag
    
    * Not to add cache with emtpy kernel for optimized out inst
    
    * Fix corner case error in buffer fusing
    - Shapes of some preds may not be changed, but still needed to do update_impl because 1) paddings are changed 2) output memory should be updated
    - optimizable impl should not be added to the cache
    
    * Allowing reorder & permute_ref to be optimized concat predecessor
    
    * Some more fixes :
    runtime buffer fusing is available only when all preds/concat are dynamic
    runtime buffer fusing is to be executed only if the node is dynamic
    
    * Fix allocate_output parameter called by get_estimated_device_mem_usage according to the new change
    
    * Fixed error in cascaded concatt
    
    * Need to reinterprete even though the size is same

--- src/plugins/intel_gpu/src/graph/network.cpp
+++ src/plugins/intel_gpu/src/graph/network.cpp
@@ -1083,9 +1083,35 @@ void network::build_insts_deps() {
 
 void network::build_exec_order() {
     GPU_DEBUG_DEFINE_MEM_LOGGER("build_exec_order");
-    for (auto& node : _program->get_processing_order()) {
-        if (!node->is_type<data>() && !(node->is_type<mutable_data>() && node->get_dependencies().empty())) {
-            add_to_exec_order(node->id());
+    if (!_is_dynamic) {
+        for (auto& node : _program->get_processing_order()) {
+            if (!node->is_type<data>() && !(node->is_type<mutable_data>() && node->get_dependencies().empty())) {
+                add_to_exec_order(node->id());
+            }
+        }
+    } else {
+        auto is_runtime_optimized_concat = [&](const program_node* node) {
+            return (node->is_dynamic() && node->is_type<concatenation>() && node->can_be_optimized());
+        };
+        auto is_allowed_pred_for_runtime_optimized_concat = [&](const program_node* node) {
+            return (!node->is_type<data>() && !(node->is_type<mutable_data>() && node->get_dependencies().empty()) &&
+                    node->get_users().size() == 1 && is_runtime_optimized_concat(node->get_users().front()));
+        };
+        for (auto& node : _program->get_processing_order()) {
+            if (!node->is_type<data>() && !(node->is_type<mutable_data>() && node->get_dependencies().empty())) {
+                if (is_allowed_pred_for_runtime_optimized_concat(node)) {
+                    continue;
+                } else if (is_runtime_optimized_concat(node)) {
+                    // For in-place concat applied at runtime, we need to do update_shape for all other predecessors of the concat user.
+                    // i.e., We need to make sure that all the preds of them are already updated too.
+                    for (auto dep : node->get_dependencies()) {
+                        if (!dep.first->is_type<data>()) {
+                            add_to_exec_order(dep.first->id());
+                        }
+                    }
+                }
+                add_to_exec_order(node->id());
+            }
         }
     }
 }
