3616d797ac23a7ebb30a8af8344987bc281f7153
Mykhailo Hnap <mykhailo.hnap@capgemini.com>
2023-06-06 12:41:53 +0300

    Revert "Quick fix for add_intermediate_node to consider dep_idx of multiple output"
    This reverts commit 2dfd2aaefdf32067a7469505b35f7096632ac5f2.

--- src/plugins/intel_gpu/src/graph/include/program_node.h
+++ src/plugins/intel_gpu/src/graph/include/program_node.h
@@ -175,7 +175,7 @@ public:
     }
 
     // replaces idx-th dependency of 'this' with 'new_dep', calls program::remove_if_dangling(old_dep)
-    void replace_dependency(size_t idx, program_node& new_dep, bool remove_if_dangling = true, size_t new_dep_idx = 0);
+    void replace_dependency(size_t idx, program_node& new_dep, bool remove_if_dangling = true);
     // searches for 'old_dep' in dependencies list of 'this' and replaces it with 'new_dep', calls
     // program::remove_if_dangling(old_dep)
     void replace_dependency(program_node const& old_dep, program_node& new_dep, bool remove_if_dangling = true);
