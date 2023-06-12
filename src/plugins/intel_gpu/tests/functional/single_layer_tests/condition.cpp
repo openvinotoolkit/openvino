aac941877abf9a8759003bb46227c08e82dde2f7
Ahn, Paul Y <paul.y.ahn@intel.com>
2023-06-12 10:37:12 +0900

    Add InnerBodyGenerator

--- src/plugins/intel_gpu/tests/functional/single_layer_tests/condition.cpp
+++ src/plugins/intel_gpu/tests/functional/single_layer_tests/condition.cpp
@@ -20,6 +20,16 @@ namespace LayerTestsDefinitions {
 
 class InnerBodyGenerator {
 public:
+    using ptr = std::shared_ptr<InnerBodyGenerator>;
+
+enum InnerBodyType {
+    Type01 = 1,
+    Type02 = 2,
+    Type03 = 3,
+    Type04 = 4
+};
+
+public:
     InnerBodyGenerator() { }
 
     virtual std::shared_ptr<ngraph::Function> get_function() { return _func; }
@@ -28,63 +38,210 @@ public:
 
     virtual void create_body(ngraph::Shape input_shape) {
         _func = generate(input_shape);
-        _param = _func->get_parameters().front();
+        _param = (_func->get_parameters().size() > 0)? _func->get_parameters().front() : nullptr;
         _result = _func->get_results().front();
     }
 
 protected:
     virtual std::shared_ptr<ngraph::Function> generate(ngraph::Shape input_shape) = 0;
 
-
     std::shared_ptr<ngraph::Function> _func;
     std::shared_ptr<ngraph::opset9::Parameter> _param;
     std::shared_ptr<ngraph::opset1::Result> _result;
 };
 
-class EltwsieSumInnerBodyGenerator : public InnerBodyGenerator {
-public:
-    EltwsieSumInnerBodyGenerator() = default;
+
+class InnerBodyType01 : public InnerBodyGenerator {
 protected:
     std::shared_ptr<ngraph::Function> generate(ngraph::Shape input_shape) override {
-        auto constant   = std::make_shared<ngraph::opset9::Constant>(ngraph::element::f32, ngraph::Shape{}, 10.0f);
-        constant->set_friendly_name("body1_const");
-        auto data     = std::make_shared<ngraph::opset9::Parameter>(ngraph::element::f32, input_shape);
-        data->set_friendly_name("body1_data");
-        auto sum      = std::make_shared<ngraph::opset9::Multiply>(data, constant);
-        sum->set_friendly_name("body1_sum");
-        auto result = std::make_shared<ngraph::opset1::Result>(sum);
+        auto constant    = std::make_shared<ngraph::opset9::Constant>(ngraph::element::f32, input_shape, 2.0f);
+        constant->set_friendly_name("body1_constant");
+        auto result = std::make_shared<ngraph::opset1::Result>(constant);
         result->set_friendly_name("body1_result");
         auto body          = std::make_shared<ngraph::Function>(
             ngraph::OutputVector {result},
-            ngraph::ParameterVector{data});
+            ngraph::ParameterVector{},
+            "eltwise_mul");
         return body;
     }
 };
 
-class EltwiseMulInnerBodyGenerator : public InnerBodyGenerator {
-public:
-    EltwiseMulInnerBodyGenerator() = default;
+class InnerBodyType02 : public InnerBodyGenerator {
 protected:
     std::shared_ptr<ngraph::Function> generate(ngraph::Shape input_shape) override {
-        auto scale    = std::make_shared<ngraph::opset9::Constant>(ngraph::element::f32, ngraph::Shape{}, 2.0f);
-        scale->set_friendly_name("body2_scale");
+        auto constant   = std::make_shared<ngraph::opset9::Constant>(ngraph::element::f32, ngraph::Shape{}, 10.0f);
+        constant->set_friendly_name("body2_const");
         auto data     = std::make_shared<ngraph::opset9::Parameter>(ngraph::element::f32, input_shape);
         data->set_friendly_name("body2_data");
-        auto mul      = std::make_shared<ngraph::opset9::Multiply>(data, scale);
-        data->set_friendly_name("body2_mul");
-        auto result = std::make_shared<ngraph::opset1::Result>(mul);
-        data->set_friendly_name("body2_result");
+        auto sum      = std::make_shared<ngraph::opset9::Multiply>(data, constant);
+        sum->set_friendly_name("body2_sum");
+        auto result = std::make_shared<ngraph::opset1::Result>(sum);
+        result->set_friendly_name("body2_result");
         auto body          = std::make_shared<ngraph::Function>(
             ngraph::OutputVector {result},
-            ngraph::ParameterVector{data});
+            ngraph::ParameterVector{data},
+            "eltwise_sum");
         return body;
     }
 };
+
+class InnerBodyType03 : public InnerBodyGenerator {
+protected:
+    std::shared_ptr<ngraph::Function> generate(ngraph::Shape input_shape) override {
+        auto constant    = std::make_shared<ngraph::opset9::Constant>(ngraph::element::f32, ngraph::Shape{}, 2.0f);
+        constant->set_friendly_name("body3_constant");
+        auto data     = std::make_shared<ngraph::opset9::Parameter>(ngraph::element::f32, input_shape);
+        data->set_friendly_name("body3_data");
+        auto add      = std::make_shared<ngraph::opset9::Add>(data, constant);
+        add->set_friendly_name("body3_add");
+        auto result = std::make_shared<ngraph::opset1::Result>(add);
+        result->set_friendly_name("body3_result");
+        auto body          = std::make_shared<ngraph::Function>(
+            ngraph::OutputVector {result},
+            ngraph::ParameterVector{data},
+            "eltwise_sum");
+        return body;
+    }
+};
+
+class InnerBodyType04 : public InnerBodyGenerator {
+protected:
+    std::shared_ptr<ngraph::Function> generate(ngraph::Shape input_shape) override {
+        auto scale    = std::make_shared<ngraph::opset9::Constant>(ngraph::element::f32, ngraph::Shape{}, 2.0f);
+        scale->set_friendly_name("body4_scale");
+        auto data     = std::make_shared<ngraph::opset9::Parameter>(ngraph::element::f32, input_shape);
+        data->set_friendly_name("body4_data");
+        auto mul      = std::make_shared<ngraph::opset9::Multiply>(data, scale);
+        mul->set_friendly_name("body4_mul");
+        auto pooling = ngraph::builder::makePooling(mul, {1, 2}, {}, {}, {1, 2},
+                                                    ov::op::RoundingType::FLOOR,
+                                                    ov::op::PadType::AUTO,
+                                                    false,
+                                                    ngraph::helpers::PoolingTypes::AVG);
+        pooling->set_friendly_name("body4_pool");
+        auto result = std::make_shared<ngraph::opset1::Result>(pooling);
+        result->set_friendly_name("body4_result");
+        auto body          = std::make_shared<ngraph::Function>(
+            ngraph::OutputVector {result},
+            ngraph::ParameterVector{data},
+            "eltwise_mul_pooling");
+        return body;
+    }
+};
+
+static std::shared_ptr<InnerBodyGenerator> get_inner_body_generator(InnerBodyGenerator::InnerBodyType type) {
+    std::shared_ptr<InnerBodyGenerator> generator_ptr;
+    switch (type) {
+        case InnerBodyGenerator::InnerBodyType::Type01:
+        {
+            return std::make_shared<InnerBodyType01>();
+        }
+        case InnerBodyGenerator::InnerBodyType::Type02:
+        {
+            return std::make_shared<InnerBodyType02>();
+        }
+        case InnerBodyGenerator::InnerBodyType::Type03:
+        {
+            return std::make_shared<InnerBodyType03>();
+        }
+        case InnerBodyGenerator::InnerBodyType::Type04:
+        {
+            return std::make_shared<InnerBodyType04>();
+        }
+        default:
+        {
+            OPENVINO_ASSERT(false, "Not supported type");
+        }
+    }
+}
+
+class TestModelGenerator {
+public:
+    enum CondTypes {
+        CONSTANT,
+        PARAM,
+        NODE
+    };
+
+public:
+    TestModelGenerator(InnerBodyGenerator::InnerBodyType then_body_type,
+                        InnerBodyGenerator::InnerBodyType else_body_type,
+                        CondTypes cond_type,
+                        ngraph::Shape input_shape) {
+                            body_then_generator = get_inner_body_generator(then_body_type);
+                            body_else_generator = get_inner_body_generator(else_body_type);
+                            body_then_generator->create_body(input_shape);
+                            body_else_generator->create_body(input_shape);
+
+                            ngraph::ParameterVector params{};
+                            auto exec_cond = create_cond_execution(cond_type, params, ngraph::element::boolean);
+                            auto data = create_condition_input(params, ngraph::element::f32, input_shape);
+                            auto cond = std::make_shared<ngraph::opset9::If>(exec_cond);
+                            cond->set_else_body(body_else_generator->get_function());
+                            cond->set_then_body(body_then_generator->get_function());
+                            cond->set_input(data, body_then_generator->get_input(), body_else_generator->get_input());
+                            cond->set_output(body_then_generator->get_result(), body_else_generator->get_result());
+                            function = std::make_shared<ngraph::Function>(ngraph::OutputVector {cond}, params);
+                        }
+    std::shared_ptr<ngraph::Function> get_function() { return function; }
+
+private:
+    std::shared_ptr<ngraph::Node> create_condition_input(ngraph::ParameterVector& params,
+        const ngraph::element::Type prc, const ngraph::Shape shape,
+        int value = 0, bool is_static = false) {
+        if (is_static)
+            return std::make_shared<ngraph::opset9::Constant>(prc, shape, value);
+
+        auto input = std::make_shared<ngraph::opset9::Parameter>(prc, shape);
+        params.push_back(input);
+        return input;
+    }
+
+    std::shared_ptr<ngraph::Node> create_cond_execution(CondTypes cond_type,
+                                                        ngraph::ParameterVector& params,
+                                                        const ngraph::element::Type prc = ngraph::element::u8,
+                                                        const ngraph::Shape shape = ngraph::Shape{},
+                                                        int value = 0) {
+        std::shared_ptr<ngraph::Node> if_cond;
+        switch (cond_type) {
+            case CondTypes::CONSTANT:
+            {
+                if_cond = create_condition_input(params, prc, shape, value, true);
+                break;
+            }
+            case CondTypes::PARAM:
+            {
+                if_cond = create_condition_input(params, prc, shape);
+                break;
+            }
+            case CondTypes::NODE:
+            {
+                auto const_cond = create_condition_input(params, prc, ngraph::Shape{}, 10, true);
+                const_cond->set_friendly_name("const_cond");
+                auto param_cond = create_condition_input(params, prc, shape);
+                param_cond->set_friendly_name("param_cond");
+                auto if_cond = std::make_shared<ngraph::opset3::GreaterEqual>(param_cond, const_cond);
+                if_cond->set_friendly_name("if_cond");
+                break;
+            }
+            default:
+            {
+                OPENVINO_ASSERT(false, "Not supported type");
+            }
+        }
+        return if_cond;
+    }
+
+private:
+    std::shared_ptr<ngraph::Function> function;
+    InnerBodyGenerator::ptr body_then_generator;
+    InnerBodyGenerator::ptr body_else_generator;
+};
+
 using ConditionParams = typename std::tuple<
         InferenceEngine::SizeVector,
         InferenceEngine::Precision,
         std::string>;
-
 
 class ConditionLayerGPUTest : public testing::WithParamInterface<ConditionParams>,
                         virtual public LayerTestsUtils::LayerTestsCommon {
@@ -97,34 +254,11 @@ protected:
     void SetUp() override {
         targetDevice = CommonTestUtils::DEVICE_GPU;
         const auto ngShape = ngraph::Shape{3, 2};
-        const auto scalarShape = ngraph::Shape{};
-        ngraph::ParameterVector params{};
-        auto create_condition_input = [&params] (ngraph::element::Type prc, const ngraph::Shape &shape, int value = 0, bool is_static = false)
-                -> std::shared_ptr<ngraph::Node> {
-            if (is_static)
-                return std::make_shared<ngraph::opset9::Constant>(prc, shape, value);
-
-            auto input = std::make_shared<ngraph::opset9::Parameter>(prc, shape);
-            params.push_back(input);
-            return input;
-        };
-
-        // body else
-        EltwsieSumInnerBodyGenerator body_else_generator;
-        body_else_generator.create_body(ngShape);
-
-        // body then
-        EltwiseMulInnerBodyGenerator body_then_generator;
-        body_then_generator.create_body(ngShape);
-
-        auto exec_cond = create_condition_input(ngraph::element::boolean, scalarShape);
-        auto data = create_condition_input(ngraph::element::f32, ngShape);
-        auto cond = std::make_shared<ngraph::opset9::If>(exec_cond);
-        cond->set_else_body(body_else_generator.get_function());
-        cond->set_then_body(body_then_generator.get_function());
-        cond->set_input(data, body_then_generator.get_input(), body_else_generator.get_input());
-        cond->set_output(body_then_generator.get_result(), body_else_generator.get_result());
-        function = std::make_shared<ngraph::Function>(ngraph::OutputVector {cond, data}, params);
+        TestModelGenerator model_generator(InnerBodyGenerator::InnerBodyType::Type02,
+                                            InnerBodyGenerator::InnerBodyType::Type03,
+                                            TestModelGenerator::CondTypes::PARAM,
+                                            ngShape);
+        function = model_generator.get_function();
     }
 
     InferenceEngine::Blob::Ptr GenerateInput(const InferenceEngine::InputInfo &info) const override {
@@ -154,6 +288,7 @@ TEST_P(ConditionLayerGPUTest, CompareWithRefs) {
     SKIP_IF_CURRENT_TEST_IS_DISABLED();
     Run();
 }
+
 std::vector<InferenceEngine::Precision> netPrecisions = {
     InferenceEngine::Precision::FP32
 };
@@ -168,12 +303,4 @@ INSTANTIATE_TEST_SUITE_P(smoke_ConditionGPUTest, ConditionLayerGPUTest,
                     testing::ValuesIn(netPrecisions),
                     testing::Values<std::string>(CommonTestUtils::DEVICE_GPU)),
                 ConditionLayerGPUTest::getTestCaseName);
-
-////////////////////////////////////////////////////////////////////////////////
-/// Dynamic shape
-////////////////////////////////////////////////////////////////////////////////
-
-
-
-
 }   // namespace LayerTestsDefinitions
