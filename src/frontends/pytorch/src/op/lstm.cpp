1c2a1e2c39c51e536e4fc797ebbe99ae37bf80ca
Roboreptile <piotr.krzeminski@intel.com>
2023-06-12 00:41:02 +0000

    [PT FE] Add updated LSTM impl

--- src/frontends/pytorch/src/op/lstm.cpp
+++ src/frontends/pytorch/src/op/lstm.cpp
@@ -11,28 +11,127 @@ namespace pytorch {
 namespace op {
 
 OutputVector translate_lstm(const NodeContext& context) {
-    num_inputs_check(context, 1, 3);
-    const auto input_tensor = context.get_input(0);
+    num_inputs_check(context, 9, 9);
+    const auto data = context.get_input(0);
+    ov::Output<ov::Node> hx;
+    ov::Output<ov::Node> params;
+    bool has_biases;
+    int num_layers;
+    bool bidirectional;
 
-    element::Type output_dtype = element::boolean;
-    if (input_tensor.get_element_type() == element::u8) {
-        output_dtype = element::u8;
-    }
-
-    bool keep_dims = false;
-    ov::Output<ov::Node> axes;
-    if (context.get_input_size() == 1) {
-        axes = get_axes_range(context, 0);
+    if (context.get_input_type(6) == element::boolean) {
+        /* Data of shape either [batch, seq, features] or [seq, features, batch]
+        aten::lstm(
+            Tensor data
+            Tuple[Tensor, Tensor] hx,   (state[0], state[1])
+            List[Tensor] params,        (_flat_weights)
+            bool has_biases,
+            int num_layers,
+            IGNORED float dropout,
+            IGNORED bool train,
+            bool bidirectional,
+            bool batch_first
+        ) -> output, hidden1, hidden2, ...
+        */
+        hx = context.get_input(1);
+        params = context.get_input(2);
+        has_biases = context.const_input<bool>(3);
+        num_layers = context.const_input<int>(4);
+        bidirectional = context.const_input<bool>(7);
+        const auto batch_first = context.const_input<bool>(8);
     } else {
-        const auto dim = context.const_input<int64_t>(1);
-        axes = context.mark_node(opset10::Constant::create(element::i64, Shape{1}, {dim}));
-        if (!context.input_is_none(2)) {
-            keep_dims = context.const_input<bool>(2);
+        /* Data of shape [seq, features], split seq by batch_sizes
+        aten::lstm(
+            Tensor data
+            Tensor batch_sizes
+            Tuple[Tensor, Tensor] hx,   (state[0], state[1])
+            List[Tensor] params,        (_flat_weights)
+            bool has_biases,
+            int num_layers,
+            IGNORED float dropout,
+            IGNORED bool train,
+            bool bidirectional
+        ) -> output, hidden1, hidden2, ...
+        */
+        const auto batch_sizes = context.get_input(1);
+        hx = context.get_input(2);
+        params = context.get_input(3);
+        has_biases = context.const_input<bool>(4);
+        num_layers = context.const_input<int>(5);
+        bidirectional = context.const_input<bool>(8);
+    }
+    const auto neg_one_1d = context.mark_node(opset10::Constant::create(element::i64, Shape{1}, {-1}));
+    const auto zero_1d = context.mark_node(opset10::Constant::create(element::i64, Shape{1}, {0}));
+    const auto one_1d = context.mark_node(opset10::Constant::create(element::i64, Shape{1}, {1}));
+    const auto two_1d = context.mark_node(opset10::Constant::create(element::i64, Shape{1}, {2}));
+    const auto three_1d = context.mark_node(opset10::Constant::create(element::i64, Shape{1}, {3}));
+    const auto four_1d = context.mark_node(opset10::Constant::create(element::i64, Shape{1}, {4}));
+
+    const auto input_shape = context.mark_node(std::make_shared<opset10::ShapeOf>(data));
+    const auto input_size = context.mark_node(std::make_shared<opset10::Gather>(input_shape, neg_one_1d));
+    const auto input_size_i64 = context.mark_node(std::make_shared<opset10::Convert>(input_size, element::i64));
+
+    const auto h0 = context.mark_node(std::make_shared<opset10::Gather>(hx, zero_1d));
+    const auto c0 = context.mark_node(std::make_shared<opset10::Gather>(hx, one_1d));
+
+    ov::Output<ov::Node> Wi, Wh, bi, bh, buffer, lstm_buffer, slice_start, slice_end;
+    ov::Output<ov::Node> it, ft, gt, ot, ct = c0, ht = h0;
+    // TODO bidirectional <backwards>, input for multilayer fix, projection matrix?
+    int layer_offset = 0;
+    for (int direction = 0; direction <= bidirectional; direction++) {
+        for (int layer = 0; layer < num_layers; layer++) {
+            slice_start = context.mark_node(std::make_shared<opset10::Broadcast>(zero_1d, one_1d));
+            slice_end = context.mark_node(std::make_shared<opset10::Add>(slice_start, input_size_i64));
+            if (has_biases) {
+                Wi = context.mark_node(std::make_shared<opset10::Gather>(params, layer_offset++));
+                Wh = context.mark_node(std::make_shared<opset10::Gather>(params, layer_offset++));
+                bi = context.mark_node(std::make_shared<opset10::Gather>(params, layer_offset++));
+                bh = context.mark_node(std::make_shared<opset10::Gather>(params, layer_offset++));
+
+                buffer = context.mark_node(std::make_shared<opset10::Multiply>(Wi, data));
+                lstm_buffer = context.mark_node(std::make_shared<opset10::Multiply>(Wh, ht));
+                lstm_buffer = context.mark_node(std::make_shared<opset10::Add>(lstm_buffer, buffer));
+                lstm_buffer = context.mark_node(std::make_shared<opset10::Add>(lstm_buffer, bi));
+                lstm_buffer = context.mark_node(std::make_shared<opset10::Add>(lstm_buffer, bh));
+            } else {
+                Wi = context.mark_node(std::make_shared<opset10::Gather>(params, layer_offset++));
+                Wh = context.mark_node(std::make_shared<opset10::Gather>(params, layer_offset++));
+
+                buffer = context.mark_node(std::make_shared<opset10::Multiply>(Wi, data));
+                lstm_buffer = context.mark_node(std::make_shared<opset10::Multiply>(Wh, ht));
+                lstm_buffer = context.mark_node(std::make_shared<opset10::Add>(lstm_buffer, buffer));
+            }
+            it = context.mark_node(std::make_shared<opset10::Slice>(lstm_buffer, slice_start, slice_end));
+            it = context.mark_node(std::make_shared<opset10::Sigmoid>(it));
+            slice_start = context.mark_node(std::make_shared<opset10::Add>(slice_start, input_size_i64));
+            slice_end = context.mark_node(std::make_shared<opset10::Add>(slice_end, input_size_i64));
+
+            ft = context.mark_node(std::make_shared<opset10::Slice>(lstm_buffer, slice_start, slice_end));
+            ft = context.mark_node(std::make_shared<opset10::Sigmoid>(ft));
+            slice_start = context.mark_node(std::make_shared<opset10::Add>(slice_start, input_size_i64));
+            slice_end = context.mark_node(std::make_shared<opset10::Add>(slice_end, input_size_i64));
+
+            gt = context.mark_node(std::make_shared<opset10::Slice>(lstm_buffer, slice_start, slice_end));
+            gt = context.mark_node(std::make_shared<opset10::Tanh>(gt));
+            slice_start = context.mark_node(std::make_shared<opset10::Add>(slice_start, input_size_i64));
+            slice_end = context.mark_node(std::make_shared<opset10::Add>(slice_end, input_size_i64));
+
+            ot = context.mark_node(std::make_shared<opset10::Slice>(lstm_buffer, slice_start, slice_end));
+            ot = context.mark_node(std::make_shared<opset10::Sigmoid>(ot));
+            slice_start = context.mark_node(std::make_shared<opset10::Add>(slice_start, input_size_i64));
+            slice_end = context.mark_node(std::make_shared<opset10::Add>(slice_end, input_size_i64));
+
+            buffer = context.mark_node(std::make_shared<opset10::Multiply>(ft, ct));
+            ct = context.mark_node(std::make_shared<opset10::Multiply>(it, gt));
+            ct = context.mark_node(std::make_shared<opset10::Add>(buffer, ct));
+            slice_start = context.mark_node(std::make_shared<opset10::Add>(slice_start, input_size_i64));
+            slice_end = context.mark_node(std::make_shared<opset10::Add>(slice_end, input_size_i64));
+
+            buffer = context.mark_node(std::make_shared<opset10::Tanh>(ct));
+            ot = context.mark_node(std::make_shared<opset10::Multiply>(ot, buffer));
         }
     }
-
-    const auto all_nonzero = context.mark_node(std::make_shared<opset10::ReduceProd>(input_tensor, axes, keep_dims));
-    return {context.mark_node(std::make_shared<opset10::Convert>(all_nonzero, output_dtype))};
+    return {it, ht, ct};
 };
 
 }  // namespace op
--- tests/layer_tests/pytorch_tests/test_lstm.py
+++ tests/layer_tests/pytorch_tests/test_lstm.py
@@ -8,29 +8,57 @@ import torch
 from pytorch_layer_test_class import PytorchLayerTest
 
 class aten_lstm(torch.nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, hx, params, num_layers, has_biases, bidirectional) -> None:
         torch.nn.Module.__init__(self)
+        self.hx = hx
+        self.params = params, 
+        self.num_layers = layers, 
+        self.has_biases = has_biases, 
+        self.bidirectional = bidirectional
 
     def forward(self, input_tensor):
-        return torch.all(input_tensor)
+        return torch._VF.lstm(input_tensor, 
+                              self.hx,
+                              self.params,
+                              self.has_biases,
+                              self.num_layers,
+                              False,
+                              False,
+                              self.bidirectional)
 
 class TestLSTM(PytorchLayerTest):
     def _prepare_input(self):
         return (self.input_tensor,)
 
-    @pytest.mark.parametrize("input_tensor", [
-        np.eye(5,5),
-        np.zeros((5, 5)),
-        np.zeros((9,8)) + 1,
-        np.random.randint(0, 2, (5, 9, 7)),
-        np.random.randint(0, 2, (10, 13, 11)),
-        np.random.randint(0, 2, (8, 7, 6, 5, 4)),
-        np.random.randint(0, 2, (11, 11), dtype=np.uint8),
-        np.random.randint(0, 2, (7, 7), dtype=np.uint8),
+    @pytest.mark.parametrize("input_shape", [
+        np.random.randint(0, 2, (2))
+    ])
+    @pytest.mark.parametrize("num_layers", [
+        np.random.randint(1, 4)
+    ])
+    @pytest.mark.parametrize("has_biases", [
+        True, False
+    ])
+    @pytest.mark.parametrize("bidirectional", [
+        True, False
+    ])
+    @pytest.mark.parametrize("projection", [
+        True, False
     ])
     @pytest.mark.nightly
     @pytest.mark.precommit
-    def test_all_noparams(self, input_tensor, ie_device, precision, ir_version):
-        self.input_tensor = input_tensor
-        self._test(aten_lstm(), None, "aten::lstm", 
+    def test_all_noparams(self, input_shape, num_layers, has_biases, bidirectional, projection, ie_device, precision, ir_version):
+        self.input_tensor = np.random.rand(*input_shape)
+
+        hx = [np.random.rand(*input_shape), np.random.rand(*input_shape)]
+        params = []
+        for i in range(num_layers):
+            for j in range(int(bidirectional) + 1):
+                m = 5 if projection else 4
+                params.append(np.random.rand(input_shape[0] * m, input_shape[1]))
+                params.append(np.random.rand(input_shape[0] * m, input_shape[1]))
+                if has_biases:
+                    params.append(np.random.rand(input_shape[0] * m, 1))
+                    params.append(np.random.rand(input_shape[0] * m, 1))
+        self._test(aten_lstm(hx, params, num_layers, has_biases, bidirectional), None, "aten::lstm", 
                 ie_device, precision, ir_version, trace_model=True, freeze_model=False)
