d93bee4bd1617008b964f509ee3347495de39333
Alexander Nesterov <nesterov.alexander@outlook.com>
2023-06-13 18:34:26 +0200

    update unit transforamtion test

--- src/plugins/intel_cpu/tests/unit/ngraph_transformations/arm/mnv6_power_decomposition.cpp
+++ src/plugins/intel_cpu/tests/unit/ngraph_transformations/arm/mnv6_power_decomposition.cpp
@@ -10,48 +10,53 @@
 #include <ngraph/function.hpp>
 #include <transformations/cpu_opset/arm/pass/mvn6_power_decomposition.hpp>
 #include <transformations/init_node_info.hpp>
-#include <transformations/utils/utils.hpp>
 #include <ngraph/pass/manager.hpp>
 #include <ov_ops/type_relaxed.hpp>
+#include <utility>
+
+#include "common_test_utils/ngraph_test_utils.hpp"
 
 using namespace testing;
 using namespace ov::intel_cpu;
 
-TEST(TransformationTests, MVN6PowerDecompositionWithSquarePower) {
-    std::shared_ptr<ov::Model> m(nullptr);
-    auto input = std::make_shared<ov::opset8::Parameter>(ov::element::f32, ov::Shape{20});
-    auto k = ov::opset8::Constant::create(ov::element::f32, ov::Shape{1}, {2});
-    auto b = std::make_shared<ov::opset8::Power>(input, k);
-
-    m = std::make_shared<ov::Model>(ov::OutputVector{b}, ov::ParameterVector{input});
-    {
-        ov::pass::Manager manager;
-        manager.register_pass<MVN6PowerDecomposition>();
-        manager.run_passes(m);
-    }
-
-    auto list_of_ops = m->get_ordered_ops();
-    ASSERT_STRCASEEQ(list_of_ops[0]->get_type_info().name, "Parameter");
-    ASSERT_STRCASEEQ(list_of_ops[1]->get_type_info().name, "Multiply");
-    ASSERT_STRCASEEQ(list_of_ops[2]->get_type_info().name, "Result");
+static std::shared_ptr<ov::Model> createInitGraph(const std::shared_ptr<ngraph::opset1::Parameter>& param, float power_value) {
+    auto power_const = ngraph::opset1::Constant::create(ngraph::element::f32, ngraph::Shape{1}, {power_value});
+    auto reduce = std::make_shared<ov::opset8::Power>(param, power_const);
+    return std::make_shared<ngraph::Function>(ngraph::NodeVector{ reduce }, ngraph::ParameterVector{ param });
 }
 
-TEST(TransformationTests, MVN6PowerDecompositionWithoutSquarePower) {
-    std::shared_ptr<ov::Model> m(nullptr);
-    auto input = std::make_shared<ov::opset8::Parameter>(ov::element::f32, ov::Shape{20});
-    auto k = ov::opset8::Constant::create(ov::element::f32, ov::Shape{1}, {3});
-    auto b = std::make_shared<ov::opset8::Power>(input, k);
+static std::shared_ptr<ov::Model> createRefGraph(const ov::Shape& param_shape) {
+    auto param = std::make_shared<ngraph::opset1::Parameter>(ngraph::element::f32, param_shape);
+    auto multiply = std::make_shared<ngraph::opset1::Multiply>(param, param);
+    return std::make_shared<ngraph::Function>(ngraph::NodeVector{ multiply }, ngraph::ParameterVector{ param });
+}
 
-    m = std::make_shared<ov::Model>(ov::OutputVector{b}, ov::ParameterVector{input});
-    {
-        ov::pass::Manager manager;
-        manager.register_pass<MVN6PowerDecomposition>();
-        manager.run_passes(m);
-    }
+static void registerAndRunPass(std::shared_ptr<ov::Model> function) {
+    ov::pass::Manager manager;
+    manager.register_pass<MVN6PowerDecomposition>();
+    manager.run_passes(std::move(function));
+}
 
-    auto list_of_ops = m->get_ordered_ops();
-    ASSERT_STRCASEEQ(list_of_ops[0]->get_type_info().name, "Parameter");
-    ASSERT_STRCASEEQ(list_of_ops[1]->get_type_info().name, "Constant");
-    ASSERT_STRCASEEQ(list_of_ops[2]->get_type_info().name, "Power");
-    ASSERT_STRCASEEQ(list_of_ops[3]->get_type_info().name, "Result");
+static ngraph::Shape param_shape = ngraph::Shape{2, 19, 2, 9};
+
+TEST(MVN6PowerDecompositionTest, CheckMVN6PowerDecompositionIsApplied) {
+    auto param = std::make_shared<ngraph::opset1::Parameter>(ngraph::element::f32, param_shape);
+    auto function = createInitGraph(param, 2);
+    auto function_ref = createRefGraph(param_shape);
+
+    registerAndRunPass(function);
+
+    auto res = compare_functions(function, function_ref);
+    ASSERT_TRUE(res.first) << res.second;
+}
+
+TEST(MVN6PowerDecompositionTest, CheckMVN6PowerDecompositionIsNotApplied) {
+    auto param = std::make_shared<ngraph::opset1::Parameter>(ngraph::element::f32, param_shape);
+    auto function = createInitGraph(param, 3);
+    auto function_ref = createInitGraph(param, 3);
+
+    registerAndRunPass(function);
+
+    auto res = compare_functions(function, function_ref);
+    ASSERT_TRUE(res.first) << res.second;
 }
