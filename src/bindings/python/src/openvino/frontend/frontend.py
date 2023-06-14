f185114a0bc5629c3e6c5ba2030d40c3b6e88a28
Anastasia Kuporosova <anastasia.kuporosova@intel.com>
2023-06-14 23:00:48 +0000

    fix fe tests

---
+++ src/bindings/python/src/openvino/frontend/frontend.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2018-2023 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import Union
+
+from openvino._pyopenvino import FrontEnd as FrontEndBase
+from openvino._pyopenvino import FrontEndManager as FrontEndManagerBase
+from openvino._pyopenvino import InputModel
+from openvino.runtime import Model
+
+
+class FrontEnd(FrontEndBase):
+    def __init__(self, fe: FrontEndBase) -> None:
+        super().__init__(fe)
+
+    def convert(self, model: Union[Model, InputModel]) -> Model:
+        converted_model = super().convert(model)
+        if isinstance(model, InputModel):
+            return Model(converted_model)
+        return converted_model
+    
+    def convert_partially(self, model: InputModel) -> Model:
+        return Model(super().convert_partially(model))
+
+    def decode(self, model: InputModel) -> Model:
+        return Model(super().decode(model))
+    
+    def normalize(self, model: Model) -> Model:
+        return Model(super().normalize(model))
+
+
+class FrontEndManager(FrontEndManagerBase):
+    def load_by_framework(self, framework: str) -> FrontEnd:
+        fe = super().load_by_framework(framework)
+        if fe is not None:
+            return FrontEnd(fe)
+        return fe
+
+    def load_by_model(self, model_path: str) -> FrontEnd:
+        fe = super().load_by_model(model_path)
+        if fe is not None:
+            return FrontEnd(fe)
+        return fe
