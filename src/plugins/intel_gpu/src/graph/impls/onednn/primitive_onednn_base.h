fa9aef94a3e7c1f519ca50fb3dc46693f21a1ad9
Kim,SungEun <sungeun.kim@intel.com>
2023-06-14 21:22:38 +0900

    Revert "[GPU] add WA to avoid hang issue. (#16724)"
    This reverts commit fef04e468a8ae7352a1a8d34e0afc3f9364eb125.

--- src/plugins/intel_gpu/src/graph/impls/onednn/primitive_onednn_base.h
+++ src/plugins/intel_gpu/src/graph/impls/onednn/primitive_onednn_base.h
@@ -8,7 +8,6 @@
 
 #include "primitive_inst.h"
 #include "intel_gpu/graph/serialization/binary_buffer.hpp"
-#include "intel_gpu/plugin/common_utils.hpp"
 #include "intel_gpu/runtime/memory.hpp"
 #include "to_string_utils.h"
 #include "register.hpp"
@@ -483,10 +482,6 @@ protected:
             try {
                 _prim.execute(stream.get_onednn_stream(), _args[net_id]);
             } catch (dnnl::error& err) {
-                /// WA: Force exit. Any opencl api call can be hang after CL_OUT_OF_RESOURCES.
-                if (err.status == dnnl_status_t::dnnl_out_of_memory) {
-                    ov::intel_gpu::ForceExit();
-                }
                 throw;    // rethrowing dnnl::error if not out_of_memory
             }
 
