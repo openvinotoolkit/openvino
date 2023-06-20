aedc5df53331bbb6265f5330784026026ee6e3a6
Anastasiia Pnevskaia <anastasiia.pnevskaia@intel.com>
2023-06-20 21:47:32 +0200

    Minor fix.

---
+++ tools/mo/openvino/tools/mo/utils/logger.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2018-2023 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+
+from openvino.tools.ovc.logger import init_logger, LvlFormatter, TagFilter, get_logger_state, restore_logger_state, \
+    progress_bar, progress_printer  # pylint: disable=no-name-in-module,import-error
