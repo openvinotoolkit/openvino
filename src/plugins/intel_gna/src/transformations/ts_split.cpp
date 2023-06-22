51279002ece007f7c1f01b4af483992d214c794c
Evgeny Kotov <evgeny.kotov@intel.com>
2023-06-22 18:51:42 +0200

    code review fixes

--- src/plugins/intel_gna/src/transformations/ts_split.cpp
+++ src/plugins/intel_gna/src/transformations/ts_split.cpp
@@ -48,30 +48,32 @@ TSSplitBackward::TSSplitBackward() {
         auto& split_node_label_output = pattern_to_output.at(split_node_label);
         auto split_node = as_type_ptr<Split>(split_node_label_output.get_node_shared_ptr());
 
+        ov::AxisVector gather_ids = {};
         std::vector<AxisVector> gather_indices_vecs;
         for (size_t output_idx = 0; output_idx < split_node->get_output_size(); ++output_idx) {
             for (auto& input : split_node->get_output_target_inputs(output_idx)) {
                 auto transpose = ov::as_type_ptr<Transpose>(input.get_node()->shared_from_this());
 
-                if (transpose && !Limitations::is_transpose_supported(transpose)) {
-                    auto transpose_const = ov::as_type_ptr<Constant>(transpose->get_input_node_shared_ptr(1));
-                    if (!transpose_const)
-                        return false;
-                    auto gather_indices_value =
-                        make_gather_indexes_from_transpose_axes(transpose->get_input_shape(0),
-                                                                transpose_const->get_axis_vector_val());
-                    gather_indices_vecs.push_back(gather_indices_value);
+                ov::Shape transpose_shape = split_node->get_output_shape(output_idx);
+                ov::AxisVector transpose_order(transpose_shape.size());
+
+                if (transpose) {
+                    std::shared_ptr<Constant> transpose_const =
+                        ov::as_type_ptr<Constant>(transpose->get_input_node_shared_ptr(1));
+                    transpose_order = transpose_const->get_axis_vector_val();
+                    transpose_shape = transpose->get_input_shape(0);
                 } else {
-                    const Shape& input_shape = input.get_shape();
-                    const size_t input_dims = std::accumulate(input_shape.begin(),
-                                                              input_shape.end(),
-                                                              1,
-                                                              std::multiplies<Shape::value_type>());
-                    std::vector<size_t> indices(input_dims);
-                    std::iota(indices.begin(), indices.end(), 1);
-                    gather_indices_vecs.push_back(indices);
-                    continue;
+                    std::iota(transpose_order.begin(), transpose_order.end(), 0);
                 }
+
+                ov::AxisVector slice_ids =
+                    graph_utils::make_gather_indexes_from_transpose_axes(transpose_shape, transpose_order);
+                // shift slice indexes and insert at the end of the gather indexes vector
+                size_t id = gather_ids.size();
+                std::for_each(slice_ids.begin(), slice_ids.end(), [&id](size_t& i) {
+                    i += id;
+                });
+                gather_ids.insert(gather_ids.end(), slice_ids.begin(), slice_ids.end());
             }
         }
 
@@ -87,20 +89,8 @@ TSSplitBackward::TSSplitBackward() {
 
         ov::copy_runtime_info(split_node, {reshape_input, reshape_input_const});
 
-        std::vector<size_t> gather_indices_value;
-        {
-            size_t shift = 0;
-            for (const auto& indices : gather_indices_vecs) {
-                for (size_t i = 0; i < indices.size(); ++i) {
-                    gather_indices_value.push_back(indices[i] + shift);
-                }
-                shift += indices.size();
-            }
-        }
-
         auto gather_axis = std::make_shared<Constant>(ov::element::i64, ov::Shape{}, 1);
-        auto gather_indices =
-            std::make_shared<Constant>(ov::element::i64, ov::Shape{gather_indices_value.size()}, gather_indices_value);
+        auto gather_indices = std::make_shared<Constant>(ov::element::i64, ov::Shape{gather_ids.size()}, gather_ids);
         auto gather = std::make_shared<Gather>(reshape_input, gather_indices, gather_axis);
 
         auto split_axis_new = std::make_shared<Constant>(ov::element::i64, ov::Shape{}, 1);
@@ -111,7 +101,7 @@ TSSplitBackward::TSSplitBackward() {
         for (size_t output_idx = 0; output_idx < split_node->get_output_size(); ++output_idx) {
             for (auto& input : split_node->get_output_target_inputs(output_idx)) {
                 auto transpose = ov::as_type_ptr<Transpose>(input.get_node()->shared_from_this());
-                if (transpose && !Limitations::is_transpose_supported(transpose)) {
+                if (transpose) {
                     auto reshape_output_const_new =
                         std::make_shared<Constant>(ov::element::i64,
                                                    ov::Shape{transpose->get_output_shape(0).size()},
