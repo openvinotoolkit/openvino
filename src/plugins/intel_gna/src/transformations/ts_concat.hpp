5d55d92e7169ecd7832a71351c3f59ab30cc0cd3
Evgeny Kotov <evgeny.kotov@intel.com>
2023-06-22 13:34:39 +0200

    code review fixes

--- src/plugins/intel_gna/src/transformations/ts_concat.hpp
+++ src/plugins/intel_gna/src/transformations/ts_concat.hpp
@@ -14,8 +14,8 @@ namespace pass {
  * @brief This transformation is part of Transpose/Gather sinking group of transformations.
  * This transformation moves Transpose through Concat layer. It changes Concat axis.
  * Currently GNA plugin has restrictions working with Concat layers.
- * It doesn't support all type of Concat layers. This transformation allows to remove Transpose layer
- * on the Concat input with adding Gather layer on the output with Concat axis being supported.
+ * It doesn't support all types of Concat layers. This transformation allows to remove Transpose layer
+ * on the Concat input and replace it with Gather layer on the output ensuring supported Concat axis.
  * Substitute graph from
  *    Any#1 ... Any#K ... Any#N
  *      |         |         |
