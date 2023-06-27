aff07542fb781ec9dfff38c3d6c5cad7ff6fca3e
Evgeny Kotov <evgeny.kotov@intel.com>
2023-06-27 19:20:29 +0200

    Revert "code review fixes -> move has_one_consumer to graph_utils"
    This reverts commit f1a24041083b0da3c5ca34417e1789fd54e24dab.

--- src/plugins/intel_gna/src/transformations/rotate_inputs.cpp
+++ src/plugins/intel_gna/src/transformations/rotate_inputs.cpp
@@ -13,17 +13,22 @@
 using namespace ov::opset11;
 using namespace ov::pass;
 using namespace ov::intel_gna::pass;
-using namespace ov::intel_gna::graph_utils;
 
 namespace {
+
+inline bool has_one_consumer(const std::shared_ptr<ov::Node>& node) {
+    return node->output(0).get_target_inputs().size() == 1;
+}
+
 inline bool is_skip_operation(const std::shared_ptr<ov::Node>& node) {
     return (std::dynamic_pointer_cast<Reshape>(node) != nullptr ||
             std::dynamic_pointer_cast<Transpose>(node) != nullptr ||
             std::dynamic_pointer_cast<Squeeze>(node) != nullptr ||
             std::dynamic_pointer_cast<Unsqueeze>(node) != nullptr ||
             std::dynamic_pointer_cast<FakeQuantize>(node) != nullptr) &&
-           has_n_consumers(node, 1);
+           has_one_consumer(node);
 }
+
 }  // namespace
 
 InsertConvolutionTransposeHW::InsertConvolutionTransposeHW() {
