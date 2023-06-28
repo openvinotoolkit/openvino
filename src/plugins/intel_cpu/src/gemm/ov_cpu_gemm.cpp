f6970959f84cb699117121256eed9646a36717f5
yi3.zhang@intel.com <yi3.zhang@intel.com>
2023-06-28 19:04:02 +0800

    check nullptr in threadpool

--- src/plugins/intel_cpu/src/gemm/ov_cpu_gemm.cpp
+++ src/plugins/intel_cpu/src/gemm/ov_cpu_gemm.cpp
@@ -21,16 +21,23 @@ public:
     size_t threadNum;
 };
 size_t DegreeOfParallelism(ThreadPool* tp) {
-    return tp->threadNum;
+    // threadpool nullptr means single threaded
+    return tp ? tp->threadNum : 1;
 }
 void TrySimpleParallelFor(ThreadPool* tp, const std::ptrdiff_t total, const std::function<void(std::ptrdiff_t)>& fn) {
-    ov::parallel_nt(tp->threadNum, [&](const size_t ithr, const size_t nthr) {
-        std::ptrdiff_t start = 0, end = 0;
-        ov::splitter(total, nthr, ithr, start, end);
-        for (std::ptrdiff_t i = start; i < end; i++) {
+    if (tp == nullptr) {
+        for(std::ptrdiff_t i = 0; i < total; i++) {
             fn(i);
         }
-    });
+    } else {
+        ov::parallel_nt(tp->threadNum, [&](const size_t ithr, const size_t nthr) {
+            std::ptrdiff_t start = 0, end = 0;
+            ov::splitter(total, nthr, ithr, start, end);
+            for (std::ptrdiff_t i = start; i < end; i++) {
+                fn(i);
+            }
+        });
+    }
 }
 size_t getCacheSize(int level, bool perCore) {
     return dnnl::utils::get_cache_size(level, perCore);
