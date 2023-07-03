28a574bf4c461e89b43a2b1dcb12051f8882e69d
Alexandra Sidorova <alexandra.sidorova@intel.com>
2023-07-03 10:34:31 +0400

    Applied Vladislav comments

--- src/plugins/intel_cpu/tests/unit/ngraph_transformations/swap_convert_transpose.cpp
+++ src/plugins/intel_cpu/tests/unit/ngraph_transformations/swap_convert_transpose.cpp
@@ -20,7 +20,7 @@ public:
 };
 
 TEST_F(SwapConvertTransposeTest, SwapConvertTranspose) {
-    const ngraph::Shape shape{1, 224, 224, 3};
+    const ov::Shape shape{1, 224, 224, 3};
     const std::vector<int64_t> input_order = {0, 3, 1, 2};
     const ov::element::Type in_type = ov::element::u8;
     const ov::element::Type out_type = ov::element::f32;
@@ -31,17 +31,17 @@ TEST_F(SwapConvertTransposeTest, SwapConvertTranspose) {
 
         auto convert = std::make_shared<ov::op::v0::Convert>(input, out_type);
 
-        auto transpose_const = std::make_shared<ov::op::v0::Constant>(ngraph::element::i64, ngraph::Shape{input_order.size()}, input_order);
+        auto transpose_const = std::make_shared<ov::op::v0::Constant>(ov::element::i64, ov::Shape{input_order.size()}, input_order);
         auto transpose = std::make_shared<ov::op::v1::Transpose>(convert, transpose_const);
         transpose->set_friendly_name(transpose_name);
 
-        function = std::make_shared<ngraph::Function>(ngraph::NodeVector{transpose}, ngraph::ParameterVector{input});
+        function = std::make_shared<ov::Model>(ov::NodeVector{transpose}, ov::ParameterVector{input});
         manager.register_pass<ov::intel_cpu::SwapConvertTranspose>();
     }
     {
         auto input = std::make_shared<ov::op::v0::Parameter>(in_type, shape);
 
-        auto transpose_const = std::make_shared<ov::op::v0::Constant>(ngraph::element::i64, ngraph::Shape{input_order.size()}, input_order);
+        auto transpose_const = std::make_shared<ov::op::v0::Constant>(ov::element::i64, ov::Shape{input_order.size()}, input_order);
         auto transpose = std::make_shared<ov::op::v1::Transpose>(input, transpose_const);
 
         auto convert = std::make_shared<ov::op::v0::Convert>(transpose, out_type);
@@ -49,6 +49,31 @@ TEST_F(SwapConvertTransposeTest, SwapConvertTranspose) {
         transpose->set_friendly_name(transpose_name + "_original");
         convert->set_friendly_name(transpose_name);
 
-        function_ref = std::make_shared<ngraph::Function>(ngraph::NodeVector{convert}, ngraph::ParameterVector{input});
+        function_ref = std::make_shared<ov::Model>(ov::NodeVector{convert}, ov::ParameterVector{input});
+    }
+}
+
+TEST_F(SwapConvertTransposeTest, SwapConvertTransposeImpossible) {
+    const ov::Shape shape{1, 224, 224, 3};
+    const std::vector<int64_t> input_order = {0, 3, 1, 2};
+    const ov::element::Type in_type = ov::element::u8;
+    const ov::element::Type out_type = ov::element::f32;
+    const std::string transpose_name = "Transpose";
+
+    {
+        auto input = std::make_shared<ov::op::v0::Parameter>(in_type, shape);
+
+        auto convert = std::make_shared<ov::op::v0::Convert>(input, out_type);
+
+        auto transpose0_const = std::make_shared<ov::op::v0::Constant>(ov::element::i64, ov::Shape{input_order.size()}, input_order);
+        auto transpose0 = std::make_shared<ov::op::v1::Transpose>(convert, transpose0_const);
+        transpose0->set_friendly_name(transpose_name + "_0");
+
+        auto transpose1_const = std::make_shared<ov::op::v0::Constant>(ov::element::i64, ov::Shape{input_order.size()}, input_order);
+        auto transpose1 = std::make_shared<ov::op::v1::Transpose>(convert, transpose1_const);
+        transpose1->set_friendly_name(transpose_name + "_1");
+
+        function = std::make_shared<ov::Model>(ov::NodeVector{transpose0, transpose1}, ov::ParameterVector{input});
+        manager.register_pass<ov::intel_cpu::SwapConvertTranspose>();
     }
 }
