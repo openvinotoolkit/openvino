3c9eaa0c08761719dd1747996a8f27848e9d6a7c
p-wysocki <przemyslaw.wysocki@intel.com>
2023-07-03 12:08:45 +0200

    Fix PT FE issue

--- src/bindings/python/tests/test_torchvision_to_ov/test_preprocessor.py
+++ src/bindings/python/tests/test_torchvision_to_ov/test_preprocessor.py
@@ -32,7 +32,8 @@ class Convnet(torch.nn.Module):
 
 def _infer_pipelines(test_input, preprocess_pipeline, input_channels=3):
     torch_model = Convnet(input_channels)
-    ov_model = mo.convert_model(torch_model, example_input=Tensor(test_input))
+    example_input = Tensor(np.expand_dims(test_input, axis=0).astype(np.float32))
+    ov_model = mo.convert_model(torch_model, example_input=example_input)
     core = Core()
 
     ov_model = PreprocessConverter.from_torchvision(
@@ -193,7 +194,7 @@ def test_grayscale_num_output_channels():
     test_input = np.random.randint(255, size=(224, 224, 3), dtype=np.uint8)
     preprocess_pipeline = transforms.Compose([transforms.ToTensor(), transforms.Grayscale(3)])
     torch_result, ov_result = _infer_pipelines(test_input, preprocess_pipeline)
-    assert np.max(np.absolute(torch_result - ov_result)) < 1e-04
+    assert np.max(np.absolute(torch_result - ov_result)) < 2e-04
 
 
 def test_pipeline_1():
@@ -223,7 +224,7 @@ def test_pipeline_2():
         ],
     )
     torch_result, ov_result = _infer_pipelines(test_input, preprocess_pipeline)
-    assert np.max(np.absolute(torch_result - ov_result)) < 2e-03
+    assert np.max(np.absolute(torch_result - ov_result)) < 3e-03
 
 
 def test_pipeline_3():
