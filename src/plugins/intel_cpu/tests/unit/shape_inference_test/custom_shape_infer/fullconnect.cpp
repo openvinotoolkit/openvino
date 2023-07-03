b7b2b33167e7db5420726b46cf9ddac9cc14c8a1
HU Yuan2 <yuan2.hu@intel.com>
2023-07-03 17:46:31 +0800

    use namespace ov instead of ngraph
    Signed-off-by: HU Yuan2 <yuan2.hu@intel.com>

--- src/plugins/intel_cpu/tests/unit/shape_inference_test/custom_shape_infer/fullconnect.cpp
+++ src/plugins/intel_cpu/tests/unit/shape_inference_test/custom_shape_infer/fullconnect.cpp
@@ -16,7 +16,7 @@ using namespace ov::intel_cpu;
 TEST(CpuShapeInfer, FC_InputSize_2) {
     auto activate = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1 });
     auto weight = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{5, 6});
-    auto op = std::make_shared<ov::intel_cpu::FullyConnectedNode>(activate, weight, ngraph::Rank(5), element::f32);
+    auto op = std::make_shared<ov::intel_cpu::FullyConnectedNode>(activate, weight, ov::Rank(5), element::f32);
     std::vector<StaticShape> static_input_shapes = {StaticShape{720, 640}, {5, 6}};
     std::vector<StaticShape> static_output_shapes = {StaticShape{1, 1, 1, 720, 5}};
     unit_test::cpu_test_shape_infer(op.get(), static_input_shapes, static_output_shapes);
@@ -26,7 +26,7 @@ TEST(CpuShapeInfer, FC_InputSize_3) {
     auto activate = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1});
     auto weight = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{5, 6});
     auto channel = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{5});
-    auto op = std::make_shared<ov::intel_cpu::FullyConnectedNode>(activate, weight, channel, ngraph::Rank(5), element::f32);
+    auto op = std::make_shared<ov::intel_cpu::FullyConnectedNode>(activate, weight, channel, ov::Rank(5), element::f32);
     std::vector<StaticShape> static_input_shapes = {StaticShape{720, 640}, {5, 6}, {5}};
     std::vector<StaticShape> static_output_shapes = {StaticShape{1, 1, 1, 720, 5}};
     unit_test::cpu_test_shape_infer(op.get(), static_input_shapes, static_output_shapes);
