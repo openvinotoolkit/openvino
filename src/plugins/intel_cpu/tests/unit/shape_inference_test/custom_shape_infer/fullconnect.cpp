603f00b259d535f4de85694464d0fa4f20148a22
HU Yuan2 <yuan2.hu@intel.com>
2023-07-04 07:39:54 +0800

    fix conflict with PR17082
    Signed-off-by: HU Yuan2 <yuan2.hu@intel.com>

--- src/plugins/intel_cpu/tests/unit/shape_inference_test/custom_shape_infer/fullconnect.cpp
+++ src/plugins/intel_cpu/tests/unit/shape_inference_test/custom_shape_infer/fullconnect.cpp
@@ -22,15 +22,6 @@ TEST(CpuShapeInfer, FC_InputSize_2) {
     unit_test::cpu_test_shape_infer(op.get(), static_input_shapes, static_output_shapes);
 }
 
-TEST(CpuShapeInfer, FC_InputSize_3) {
-    auto activate = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{-1, -1});
-    auto weight = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{5, 6});
-    auto channel = std::make_shared<ov::op::v0::Parameter>(element::f32, PartialShape{5});
-    auto op = std::make_shared<ov::intel_cpu::FullyConnectedNode>(activate, weight, channel, ov::Rank(5), element::f32);
-    std::vector<StaticShape> static_input_shapes = {StaticShape{720, 640}, {5, 6}, {5}};
-    std::vector<StaticShape> static_output_shapes = {StaticShape{1, 1, 1, 720, 5}};
-    unit_test::cpu_test_shape_infer(op.get(), static_input_shapes, static_output_shapes);
-}
 } // namespace cpu_shape_infer
 } // namespace unit_test
 } // namespace intel_cpu
