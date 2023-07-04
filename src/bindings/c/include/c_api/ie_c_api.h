b21478c96db66c91719c0fbf824adf2c9af54110
panhaiqi <haiqi.pan@intel.com>
2023-07-04 14:30:30 +0800

    move declaration to ie_c_api.h

--- src/bindings/c/include/c_api/ie_c_api.h
+++ src/bindings/c/include/c_api/ie_c_api.h
@@ -33,6 +33,11 @@
 #    define INFERENCE_ENGINE_C_API_EXTERN
 #endif
 
+#define IE_1_0_DEPRECATED                                                                                    \
+    OPENVINO_DEPRECATED("The Inference Engine API is deprecated and will be removed in the 2024.0 release. " \
+                        "For instructions on transitioning to the new API, please refer to "                 \
+                        "https://docs.openvino.ai/latest/openvino_2_0_transition_guide.html")
+
 #if defined(OPENVINO_STATIC_LIBRARY) || defined(__GNUC__) && (__GNUC__ < 4)
 #    define INFERENCE_ENGINE_C_API(...) INFERENCE_ENGINE_C_API_EXTERN __VA_ARGS__ IE_1_0_DEPRECATED
 #    define IE_NODISCARD
