1c7fc3e0f2f2d876250c81743ab72dda8fcd6bf5
almilosz <alicja.miloszewska@intel.com>
2023-07-05 19:37:34 +0200

    Add tests for Input<const Node>

--- src/bindings/js/node/tests/basic.test.js
+++ src/bindings/js/node/tests/basic.test.js
@@ -91,3 +91,39 @@ describe('Output class for ov::Output<const ov::Node>', () => {
         expect(compiledModel.output(0).getShape().getData()).toEqual([1, 10]);
     });
 });
+
+describe('Input class for ov::Input<const ov::Node>', () => {
+    const core = new ov.Core();
+    const model = core.read_model(test_xml);
+    const compiledModel = core.compile_model(model, 'CPU');
+
+    test('CompiledModel.input() method', () => {
+        // TO_DO check if object is an instance of a value/class
+        expect(compiledModel.input() && typeof compiledModel.input() === 'object').toBe(true)
+    });
+
+    test('CompiledModel.inputs property', () => {
+        expect(compiledModel.inputs.length).toEqual(1);     
+    });
+
+    test('CompiledModel.input().ToString() method', () => {
+        //test for a model with one output
+        expect(compiledModel.input().toString()).toEqual("data");
+    });
+
+    test('CompiledModel.input(idx: number).ToString() method', () => {
+        expect(compiledModel.input(0).toString()).toEqual("data");
+    });
+
+    test('CompiledModel.input(tensorName: string).ToString() method', () => {
+        expect(compiledModel.input("data").toString()).toEqual("data");
+    });
+
+    test('Input.shape property with dimensions', () => {
+        expect(compiledModel.input(0).shape).toEqual([1, 3, 32, 32]);
+    });
+
+    test('Input.getShape() method', () => {
+        expect(compiledModel.input(0).getShape().getData()).toEqual([1, 3, 32, 32]);
+    });
+});
