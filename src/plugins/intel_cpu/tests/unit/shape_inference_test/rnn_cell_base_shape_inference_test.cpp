37766c0b83a95c841b4df3d013e5637aa122ce06
mitruska <katarzyna.mitrus@intel.com>
2023-07-05 16:07:52 +0200

    Update RNN shape infer tests

--- src/plugins/intel_cpu/tests/unit/shape_inference_test/rnn_cell_base_shape_inference_test.cpp
+++ src/plugins/intel_cpu/tests/unit/shape_inference_test/rnn_cell_base_shape_inference_test.cpp
@@ -9,28 +9,34 @@
 using namespace ov;
 using namespace ov::intel_cpu;
 
+class RNNCellV0StaticShapeInferenceTest : public OpStaticShapeInferenceTest<op::v0::RNNCell> {
+protected:
+    void SetUp() override {
+        this->output_shapes = ShapeVector(1);
+    }
+};
 
-TEST(StaticShapeInferenceTest, RNNCellTest_default_ctor) {
+TEST_F(RNNCellV0StaticShapeInferenceTest, default_ctor) {
     constexpr size_t batch_size = 2;
     constexpr size_t input_size = 3;
     constexpr size_t hidden_size = 5;
     constexpr size_t gates_count = 1;
 
     // Default `B` input is created as Constant by RNNCell contructor
-    const auto gru = std::make_shared<op::v0::RNNCell>();
+    const auto gru = make_op();
 
-    std::vector<StaticShape> static_input_shapes{StaticShape{batch_size, input_size},                  // X
-                                                 StaticShape{batch_size, hidden_size},                 // H_t
-                                                 StaticShape{gates_count * hidden_size, input_size},   // W
-                                                 StaticShape{gates_count * hidden_size, hidden_size},  // R
-                                                 StaticShape{gates_count * hidden_size}};              // B
+    input_shapes = {StaticShape{batch_size, input_size},                  // X
+                    StaticShape{batch_size, hidden_size},                 // H_t
+                    StaticShape{gates_count * hidden_size, input_size},   // W
+                    StaticShape{gates_count * hidden_size, hidden_size},  // R
+                    StaticShape{gates_count * hidden_size}};              // B
 
-    std::vector<StaticShape> static_output_shapes;
-    shape_inference(gru.get(), static_input_shapes, static_output_shapes);
-    EXPECT_EQ(static_output_shapes[0], StaticShape({batch_size, hidden_size}));
+    std::vector<StaticShape> output_shapes;
+    shape_inference(gru.get(), input_shapes, output_shapes);
+    EXPECT_EQ(output_shapes[0], StaticShape({batch_size, hidden_size}));
 }
 
-TEST(StaticShapeInferenceTest, RNNCellTest_default_bias) {
+TEST_F(RNNCellV0StaticShapeInferenceTest, default_bias) {
     constexpr size_t batch_size = 2;
     constexpr size_t input_size = 3;
     constexpr size_t hidden_size = 5;
@@ -42,20 +48,20 @@ TEST(StaticShapeInferenceTest, RNNCellTest_default_bias) {
     const auto R = std::make_shared<op::v0::Parameter>(element::f32, PartialShape::dynamic(2));
 
     // Default `B` input is created as Constant by RNNCell contructor
-    const auto gru = std::make_shared<op::v0::RNNCell>(X, H_t, W, R, hidden_size);
+    const auto gru = make_op(X, H_t, W, R, hidden_size);
 
-    std::vector<StaticShape> static_input_shapes{StaticShape{batch_size, input_size},                  // X
-                                                 StaticShape{batch_size, hidden_size},                 // H_t
-                                                 StaticShape{gates_count * hidden_size, input_size},   // W
-                                                 StaticShape{gates_count * hidden_size, hidden_size},  // R
-                                                 StaticShape{gates_count * hidden_size}};              // B
+    input_shapes = {StaticShape{batch_size, input_size},                  // X
+                    StaticShape{batch_size, hidden_size},                 // H_t
+                    StaticShape{gates_count * hidden_size, input_size},   // W
+                    StaticShape{gates_count * hidden_size, hidden_size},  // R
+                    StaticShape{gates_count * hidden_size}};              // B
 
-    std::vector<StaticShape> static_output_shapes;
-    shape_inference(gru.get(), static_input_shapes, static_output_shapes);
-    EXPECT_EQ(static_output_shapes[0], StaticShape({batch_size, hidden_size}));
+    std::vector<StaticShape> output_shapes;
+    shape_inference(gru.get(), input_shapes, output_shapes);
+    EXPECT_EQ(output_shapes[0], StaticShape({batch_size, hidden_size}));
 }
 
-TEST(StaticShapeInferenceTest, RNNCellTest_with_bias) {
+TEST_F(RNNCellV0StaticShapeInferenceTest, with_bias) {
     constexpr size_t batch_size = 2;
     constexpr size_t input_size = 3;
     constexpr size_t hidden_size = 5;
@@ -66,20 +72,19 @@ TEST(StaticShapeInferenceTest, RNNCellTest_with_bias) {
     const auto W = std::make_shared<op::v0::Parameter>(element::f32, PartialShape::dynamic(2));
     const auto R = std::make_shared<op::v0::Parameter>(element::f32, PartialShape::dynamic(2));
     const auto B = std::make_shared<op::v0::Parameter>(element::f32, PartialShape::dynamic(1));
-    const auto gru = std::make_shared<op::v0::RNNCell>(X, H_t, W, R, B, hidden_size);
+    const auto gru = make_op(X, H_t, W, R, B, hidden_size);
 
-    std::vector<StaticShape> static_input_shapes{StaticShape{batch_size, input_size},                  // X
-                                                 StaticShape{batch_size, hidden_size},                 // H_t
-                                                 StaticShape{gates_count * hidden_size, input_size},   // W
-                                                 StaticShape{gates_count * hidden_size, hidden_size},  // R
-                                                 StaticShape{gates_count * hidden_size}};              // B
+    input_shapes = {StaticShape{batch_size, input_size},                  // X
+                    StaticShape{batch_size, hidden_size},                 // H_t
+                    StaticShape{gates_count * hidden_size, input_size},   // W
+                    StaticShape{gates_count * hidden_size, hidden_size},  // R
+                    StaticShape{gates_count * hidden_size}};              // B
 
-    std::vector<StaticShape> static_output_shapes;
-    shape_inference(gru.get(), static_input_shapes, static_output_shapes);
-    EXPECT_EQ(static_output_shapes[0], StaticShape({batch_size, hidden_size}));
+    shape_inference(gru.get(), input_shapes, output_shapes);
+    EXPECT_EQ(output_shapes[0], StaticShape({batch_size, hidden_size}));
 }
 
-TEST(StaticShapeInferenceTest, RNNCellTest_dynamic_rank_inputs) {
+TEST_F(RNNCellV0StaticShapeInferenceTest, dynamic_rank_inputs) {
     constexpr size_t batch_size = 2;
     constexpr size_t input_size = 3;
     constexpr size_t hidden_size = 5;
@@ -91,15 +96,15 @@ TEST(StaticShapeInferenceTest, RNNCellTest_dynamic_rank_inputs) {
     const auto R = std::make_shared<op::v0::Parameter>(element::f32, PartialShape::dynamic());
     const auto B = std::make_shared<op::v0::Parameter>(element::f32, PartialShape::dynamic());
 
-    const auto gru = std::make_shared<op::v0::RNNCell>(X, H_t, W, R, B, hidden_size);
+    const auto gru = make_op(X, H_t, W, R, B, hidden_size);
 
-    std::vector<StaticShape> static_input_shapes{StaticShape{batch_size, input_size},                  // X
-                                                 StaticShape{batch_size, hidden_size},                 // H_t
-                                                 StaticShape{gates_count * hidden_size, input_size},   // W
-                                                 StaticShape{gates_count * hidden_size, hidden_size},  // R
-                                                 StaticShape{gates_count * hidden_size}};              // B
+    input_shapes = {StaticShape{batch_size, input_size},                  // X
+                    StaticShape{batch_size, hidden_size},                 // H_t
+                    StaticShape{gates_count * hidden_size, input_size},   // W
+                    StaticShape{gates_count * hidden_size, hidden_size},  // R
+                    StaticShape{gates_count * hidden_size}};              // B
 
-    std::vector<StaticShape> static_output_shapes;
-    shape_inference(gru.get(), static_input_shapes, static_output_shapes);
-    EXPECT_EQ(static_output_shapes[0], StaticShape({batch_size, hidden_size}));
+    std::vector<StaticShape> output_shapes;
+    shape_inference(gru.get(), input_shapes, output_shapes);
+    EXPECT_EQ(output_shapes[0], StaticShape({batch_size, hidden_size}));
 }
