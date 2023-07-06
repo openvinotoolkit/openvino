6db0a3b4fd70b7fbc54ef40549bc883ea5002b8a
mitruska <katarzyna.mitrus@intel.com>
2023-07-06 18:58:13 +0200

    Add NodeRegistry

--- src/common/transformations/src/transformations/op_conversions/group_normalization_decomposition.cpp
+++ src/common/transformations/src/transformations/op_conversions/group_normalization_decomposition.cpp
@@ -25,42 +25,43 @@ namespace {
 // before normalization.
 // If data shape is [N,C,H,W], the function returns
 // [N * num_groups, C // num_groups, H, W]
-std::shared_ptr<Node> create_group_norm_shape(const Output<Node>& data, size_t num_groups) {
+std::shared_ptr<Node> create_group_norm_shape(NodeRegistry& reg, const Output<Node>& data, size_t num_groups) {
     const auto& pshape = data.get_partial_shape();
     NGRAPH_CHECK(pshape.rank().is_static());
     size_t rank_size = pshape.rank().get_length();
     NGRAPH_CHECK(rank_size >= 3, "3-D and above tensors supported only");
 
-    auto shape = std::make_shared<ShapeOf>(data);
+    auto shape = reg.make<ShapeOf>(data);
 
-    const auto axis_node = Constant::create(element::i64, Shape{}, {0});
-    const auto split = std::make_shared<Split>(shape, axis_node, rank_size);
+    const auto axis_node = reg.add(Constant::create(element::i64, Shape{}, {0}));
+    const auto split = reg.make<Split>(shape, axis_node, rank_size);
     auto splits = split->outputs();
-    auto num_groups_const = Constant::create(element::i64, Shape{1}, {num_groups});
+    auto num_groups_const = reg.add(Constant::create(element::i64, Shape{1}, {num_groups}));
     // The 4D shape: [N * num_groups, C // num_groups, H, W] is created
     // instead of 5D shape: [N, num_groups, C // num_groups, H, W].
     // The reason is the lack of support for 5D MVN input by some plugins.
-    OutputVector new_shape{std::make_shared<Multiply>(splits[0], num_groups_const),
-                           std::make_shared<Divide>(splits[1], num_groups_const)};
+    OutputVector new_shape{reg.make<Multiply>(splits[0], num_groups_const),
+                           reg.make<Divide>(splits[1], num_groups_const)};
 
     for (size_t i = 2; i < rank_size; i++) {
         new_shape.push_back(splits[i]);
     }
-    return std::make_shared<Concat>(new_shape, 0);
+    return reg.make<Concat>(new_shape, 0);
 }
 
-Output<Node> reshape_channel_shaped_node_to_nchw(const Output<Node>& node, const Output<Node>& expected_rank) {
-    // Prepare tail shape (rank = conv.rank - 2): [1, 1, 1, 1, ... ]
-    const auto one_const = Constant::create(element::i64, Shape{1}, {1});
-    const auto two_const = Constant::create(element::i64, Shape{1}, {2});
-    const auto tail_shape_rank = std::make_shared<Subtract>(expected_rank, two_const);
-    const auto tail_shape = std::make_shared<Broadcast>(one_const, tail_shape_rank);
+Output<Node> reshape_channel_shaped_node_to_nchw(NodeRegistry& reg,
+                                                 const Output<Node>& node,
+                                                 const Output<Node>& expected_rank) {
+    const auto one_const = reg.add(Constant::create(element::i64, Shape{1}, {1}));
+    const auto two_const = reg.add(Constant::create(element::i64, Shape{1}, {2}));
+    const auto tail_shape_rank = reg.make<Subtract>(expected_rank, two_const);
+    const auto tail_shape = reg.make<Broadcast>(one_const, tail_shape_rank);
 
     // Construct new bias shape: [1, C, 1, 1, ... ]
-    const auto C_dim = std::make_shared<ShapeOf>(node);
-    const auto new_shape = std::make_shared<Concat>(OutputVector{one_const, C_dim, tail_shape}, 0);
+    const auto C_dim = reg.make<ShapeOf>(node);
+    const auto new_shape = reg.make<Concat>(OutputVector{one_const, C_dim, tail_shape}, 0);
 
-    return std::make_shared<Reshape>(node, new_shape, false);
+    return reg.make<Reshape>(node, new_shape, false);
 }
 
 template <typename T>
@@ -79,20 +80,23 @@ std::vector<T> get_monotonic_range(T end_value, T start_value = T{0}, T step = T
     return range;
 }
 
-std::shared_ptr<Node> get_monotonic_range_along_node_rank(const Output<Node>& value,
-                                                                  int64_t start_value,
-                                                                  int64_t step) {
+std::shared_ptr<Node> get_monotonic_range_along_node_rank(NodeRegistry& reg,
+                                                          const Output<Node>& value,
+                                                          int64_t start_value,
+                                                          int64_t step) {
     if (value.get_partial_shape().rank().is_static()) {
         const auto range_value =
             get_monotonic_range<int64_t>(value.get_partial_shape().rank().get_length(), start_value, step);
-        return Constant::create(element::i64, {range_value.size()}, range_value);
+        return reg.add(Constant::create(element::i64, {range_value.size()}, range_value));
     }
 
-    const auto value_shape = std::make_shared<ShapeOf>(value);
-    return std::make_shared<Range>(Constant::create(element::i64, {}, {start_value}),
-                                                  std::make_shared<ShapeOf>(value_shape),
-                                                  Constant::create(element::i64, {}, {step}),
-                                                  element::i64);
+    const auto value_shape = reg.make<ShapeOf>(value);
+    return reg.make<Range>(reg.add(Constant::create(element::i64, {}, {start_value})),
+                           reg.make<ShapeOf>(value_shape),
+                           reg.add(Constant::create(element::i64, {}, {step})),
+                           element::i64
+
+    );
 }
 
 }  // namespace
@@ -103,7 +107,7 @@ ov::pass::GroupNormalizationDecomposition::GroupNormalizationDecomposition() {
     auto group_norm_pattern = pattern::wrap_type<GroupNormalization>();
 
     matcher_pass_callback callback = [=](pattern::Matcher& matcher) {
-        NodeRegistry rg;
+        NodeRegistry reg;
 
         auto group_norm_node = std::dynamic_pointer_cast<GroupNormalization>(matcher.get_match_root());
         if (!group_norm_node || transformation_callback(group_norm_node) || group_norm_node->get_input_size() != 3) {
@@ -114,41 +118,29 @@ ov::pass::GroupNormalizationDecomposition::GroupNormalizationDecomposition() {
         auto scale = group_norm_node->input_value(1);
         auto bias = group_norm_node->input_value(2);
 
-        size_t num_groups = static_cast<size_t>(group_norm_node->get_num_groups()); // Negative values are checked by op validation
+        size_t num_groups =
+            static_cast<size_t>(group_norm_node->get_num_groups());  // Negative values are checked by op validation
         float eps = group_norm_node->get_epsilon();
 
-        auto data_shape_node = std::make_shared<ShapeOf>(data);
-        auto data_reshaped = std::make_shared<Reshape>(data, create_group_norm_shape(data, num_groups), true);
-        const auto reduction_axes = get_monotonic_range_along_node_rank(data_reshaped, 1, 1);
+        auto data_shape_node = reg.make<ShapeOf>(data);
+        auto data_reshaped = reg.make<Reshape>(data, create_group_norm_shape(reg, data, num_groups), true);
+        const auto reduction_axes = get_monotonic_range_along_node_rank(reg, data_reshaped, 1, 1);
 
-        auto mvn = std::make_shared<MVN>(data_reshaped, reduction_axes, true, eps, op::MVNEpsMode::INSIDE_SQRT);
-        std::shared_ptr<Node> result = std::make_shared<Reshape>(mvn, data_shape_node, true);
+        auto mvn = reg.make<MVN>(data_reshaped, reduction_axes, true, eps, op::MVNEpsMode::INSIDE_SQRT);
+        std::shared_ptr<Node> result = reg.make<Reshape>(mvn, data_shape_node, true);
 
         const auto& scale_shape = scale.get_partial_shape();
-
         const auto& bias_shape = bias.get_partial_shape();
 
         if (!scale_shape.rank().is_static() || !bias_shape.rank().is_static()) {
             return false;
         }
 
-        auto scale_rank = scale_shape.rank().get_length();
-        auto bias_rank = bias_shape.rank().get_length();
+        const auto data_rank = reg.make<ShapeOf>(data_shape_node);
+        result = reg.make<Multiply>(result, reshape_channel_shaped_node_to_nchw(reg, scale, data_rank));
+        result = reg.make<Add>(result, reshape_channel_shaped_node_to_nchw(reg, bias, data_rank));
 
-        const auto data_rank = std::make_shared<ShapeOf>(data_shape_node);
-
-        if (scale_rank == 1) {
-            result = std::make_shared<Multiply>(result, reshape_channel_shaped_node_to_nchw(scale, data_rank));
-        } else {
-            result = std::make_shared<Multiply>(result, scale);
-        }
-        if (bias_rank == 1) {
-            result = std::make_shared<Add>(result, reshape_channel_shaped_node_to_nchw(bias, data_rank));
-        } else {
-            result = std::make_shared<Add>(result, bias);
-        }
-
-        copy_runtime_info(group_norm_node, rg.get());
+        copy_runtime_info(group_norm_node, reg.get());
         replace_node(group_norm_node, result);
 
         return true;
