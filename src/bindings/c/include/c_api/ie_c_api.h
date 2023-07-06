fd328f68cd8317a81c47f3ab728ee2b490cd2c5f
panhaiqi <haiqi.pan@intel.com>
2023-07-06 09:51:47 +0800

    add ov_deprecated_no_errors back

--- src/bindings/c/include/c_api/ie_c_api.h
+++ src/bindings/c/include/c_api/ie_c_api.h
@@ -36,6 +36,16 @@
 #define IE_1_0_DEPRECATED \
     OPENVINO_DEPRECATED("The Inference Engine API is deprecated and will be removed in the 2024.0 release. ")
 
+#if !defined(IN_OV_COMPONENT) && !defined(C_API_LEGACY_HEADER_INCLUDED)
+#    define C_API_LEGACY_HEADER_INCLUDED
+#    ifdef _MSC_VER
+#        pragma message(
+            "The legacy C API is deprecated and will be removed in the 2024.0 release. For instructions on transitioning to the new API, please refer to https://docs.openvino.ai/latest/openvino_2_0_transition_guide.html")
+#    else
+#        warning("The legacy C API is deprecated and will be removed in the 2024.0 release. For instructions on transitioning to the new API, please refer to https://docs.openvino.ai/latest/openvino_2_0_transition_guide.html")
+#    endif
+#endif
+
 #if defined(OPENVINO_STATIC_LIBRARY) || defined(__GNUC__) && (__GNUC__ < 4)
 #    define INFERENCE_ENGINE_C_API(...) INFERENCE_ENGINE_C_API_EXTERN __VA_ARGS__ IE_1_0_DEPRECATED
 #    define IE_NODISCARD
@@ -58,16 +68,6 @@
 
 #ifndef INFERENCE_ENGINE_C_API_CALLBACK
 #    define INFERENCE_ENGINE_C_API_CALLBACK
-#endif
-
-#if !defined(IN_OV_COMPONENT) && !defined(C_API_LEGACY_HEADER_INCLUDED)
-#    define C_API_LEGACY_HEADER_INCLUDED
-#    ifdef _MSC_VER
-#        pragma message(
-            "The legacy C API is deprecated and will be removed in the 2024.0 release. For instructions on transitioning to the new API, please refer to https://docs.openvino.ai/latest/openvino_2_0_transition_guide.html")
-#    else
-#        warning("The legacy C API is deprecated and will be removed in the 2024.0 release. For instructions on transitioning to the new API, please refer to https://docs.openvino.ai/latest/openvino_2_0_transition_guide.html")
-#    endif
 #endif
 
 typedef struct ie_core ie_core_t;
