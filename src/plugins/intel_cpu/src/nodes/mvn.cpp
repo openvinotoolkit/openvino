5c8f6859d51a05d666cead1e4d1243fe7616c6fe
Aleksandr Voron <aleksandr.voron@intel.com>
2023-07-07 10:50:15 +0200

    mvn node - avoid fp32 force

--- src/plugins/intel_cpu/src/nodes/mvn.cpp
+++ src/plugins/intel_cpu/src/nodes/mvn.cpp
@@ -1179,12 +1179,12 @@ void MVN::initSupportedPrimitiveDescriptors() {
     if (!fusedWith.empty()) {
         outputPrecision = fusedWith[fusedWith.size() - 1]->getOriginalOutputPrecisionAtPort(0);
     }
-
+#if defined(OPENVINO_ARCH_X86) || defined(OPENVINO_ARCH_X86_64)
     // ref with float planar and no fusion
     if (!mayiuse(cpu::x64::sse41)) {
         inputPrecision = outputPrecision = Precision::FP32;
     }
-
+#endif
     // TODO [DS]: inplace
     bool canBeInplace = !isDynamicNode() && (inputPrecision.size() == outputPrecision.size()) &&
                         (getParentEdgeAt(0)->getParent()->getChildEdges().size() == 1) &&
