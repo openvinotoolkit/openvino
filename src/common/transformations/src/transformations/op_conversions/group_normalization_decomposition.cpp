4ac58e404671daa1d4ba6b424729a2ca0580b6bb
mitruska <katarzyna.mitrus@intel.com>
2023-07-07 15:41:19 +0200

    Use Unsqueeze for bias and scale adjustment

--- src/common/transformations/src/transformations/op_conversions/group_normalization_decomposition.cpp
+++ src/common/transformations/src/transformations/op_conversions/group_normalization_decomposition.cpp
@@ -25,14 +25,10 @@ namespace {
 // before normalization.
 // If data shape is [N,C,H,W], the function returns
 // [N * num_groups, C // num_groups, H, W]
-std::shared_ptr<Node> create_group_norm_shape(NodeRegistry& reg, const Output<Node>& data, size_t num_groups) {
-    const auto& pshape = data.get_partial_shape();
-    NGRAPH_CHECK(pshape.rank().is_static());
-    size_t rank_size = pshape.rank().get_length();
-    NGRAPH_CHECK(rank_size >= 3, "3-D and above tensors supported only");
-
-    auto shape = reg.make<ShapeOf>(data);
-
+std::shared_ptr<Node> create_group_norm_shape(NodeRegistry& reg,
+                                              const Output<Node>& shape,
+                                              size_t num_groups,
+                                              size_t rank_size) {
     const auto axis_node = reg.add(Constant::create(element::i64, Shape{}, {0}));
     const auto split = reg.make<Split>(shape, axis_node, rank_size);
     auto splits = split->outputs();
@@ -43,33 +39,16 @@ std::shared_ptr<Node> create_group_norm_shape(NodeRegistry& reg, const Output<No
     OutputVector new_shape{reg.make<Multiply>(splits[0], num_groups_const),
                            reg.make<Divide>(splits[1], num_groups_const)};
 
-    for (size_t i = 2; i < rank_size; i++) {
+    for (size_t i = 2; i < rank_size; ++i) {
         new_shape.push_back(splits[i]);
     }
     return reg.make<Concat>(new_shape, 0);
 }
 
-Output<Node> reshape_channel_shaped_node_to_nchw(NodeRegistry& reg,
-                                                 const Output<Node>& node,
-                                                 const Output<Node>& expected_rank) {
-    const auto one_const = reg.add(Constant::create(element::i64, Shape{1}, {1}));
-    const auto two_const = reg.add(Constant::create(element::i64, Shape{1}, {2}));
-    const auto tail_shape_rank = reg.make<Subtract>(expected_rank, two_const);
-    const auto tail_shape = reg.make<Broadcast>(one_const, tail_shape_rank);
-
-    // Construct new bias shape: [1, C, 1, 1, ... ]
-    const auto C_dim = reg.make<ShapeOf>(node);
-    const auto new_shape = reg.make<Concat>(OutputVector{one_const, C_dim, tail_shape}, 0);
-
-    return reg.make<Reshape>(node, new_shape, false);
-}
-
 template <typename T>
-std::vector<T> get_monotonic_range(T end_value, T start_value = T{0}, T step = T{1}) {
+std::vector<T> get_monotonic_range(T start_value, T end_value, T step = T{1}) {
     auto value_count = static_cast<std::size_t>(std::floor((end_value - start_value) / step));
-
     std::vector<T> range(value_count);
-
     // Calculate initial value (one step below starting value)
     size_t n = start_value - step;
     // Generate a vector of values by adding step to previous value
@@ -81,24 +60,12 @@ std::vector<T> get_monotonic_range(T end_value, T start_value = T{0}, T step = T
 }
 
 std::shared_ptr<Node> get_monotonic_range_along_node_rank(NodeRegistry& reg,
-                                                          const Output<Node>& value,
-                                                          int64_t start_value,
+                                                          int64_t start,
+                                                          int64_t stop,
                                                           int64_t step) {
-    if (value.get_partial_shape().rank().is_static()) {
-        const auto range_value =
-            get_monotonic_range<int64_t>(value.get_partial_shape().rank().get_length(), start_value, step);
-        return reg.add(Constant::create(element::i64, {range_value.size()}, range_value));
-    }
-
-    const auto value_shape = reg.make<ShapeOf>(value);
-    return reg.make<Range>(reg.add(Constant::create(element::i64, {}, {start_value})),
-                           reg.make<ShapeOf>(value_shape),
-                           reg.add(Constant::create(element::i64, {}, {step})),
-                           element::i64
-
-    );
+    const auto range_value = get_monotonic_range<int64_t>(start, stop, step);
+    return reg.add(Constant::create(element::i64, {range_value.size()}, range_value));
 }
-
 }  // namespace
 
 ov::pass::GroupNormalizationDecomposition::GroupNormalizationDecomposition() {
@@ -122,23 +89,27 @@ ov::pass::GroupNormalizationDecomposition::GroupNormalizationDecomposition() {
             static_cast<size_t>(group_norm_node->get_num_groups());  // Negative values are checked by op validation
         auto eps = group_norm_node->get_epsilon();
 
+        const auto& data_rank = data.get_partial_shape().rank();
+        if (data_rank.is_dynamic() || data_rank.get_length() < 3) {
+            return false;
+        }
+        const auto data_rank_size = data_rank.get_length();
         auto data_shape_node = reg.make<ShapeOf>(data);
-        auto data_reshaped = reg.make<Reshape>(data, create_group_norm_shape(reg, data, num_groups), true);
-        const auto reduction_axes = get_monotonic_range_along_node_rank(reg, data_reshaped, 1, 1);
+        auto data_reshaped = reg.make<Reshape>(
+            data,
+            create_group_norm_shape(reg, data_shape_node, num_groups, static_cast<size_t>(data_rank_size)),
+            true);
+        const auto reduction_axes = get_monotonic_range_along_node_rank(reg, 1, data_rank_size + 1, 1);
 
         auto mvn = reg.make<MVN>(data_reshaped, reduction_axes, true, eps, op::MVNEpsMode::INSIDE_SQRT);
         std::shared_ptr<Node> result = reg.make<Reshape>(mvn, data_shape_node, true);
 
-        const auto& scale_shape = scale.get_partial_shape();
-        const auto& bias_shape = bias.get_partial_shape();
+        // Unsqueeze scale and bias to shape: [C, 1, 1, ... ]
+        const auto unsqueeze_axes = get_monotonic_range_along_node_rank(reg, 1, data_rank_size - 1, 1);
+        result = reg.make<Multiply>(result, reg.make<Unsqueeze>(scale, unsqueeze_axes));
+        result = reg.make<Add>(result, reg.make<Unsqueeze>(bias, unsqueeze_axes));
 
-        if (!scale_shape.rank().is_static() || !bias_shape.rank().is_static()) {
-            return false;
-        }
-
-        const auto data_rank = reg.make<ShapeOf>(data_shape_node);
-        result = reg.make<Multiply>(result, reshape_channel_shaped_node_to_nchw(reg, scale, data_rank));
-        result = reg.make<Add>(result, reshape_channel_shaped_node_to_nchw(reg, bias, data_rank));
+        result->set_friendly_name(group_norm_node->get_friendly_name());
 
         copy_runtime_info(group_norm_node, reg.get());
         replace_node(group_norm_node, result);
