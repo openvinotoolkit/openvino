41030fab43ae3012b817c8a3b39bf378ebf709f6
Pavel Durandin <pavel.durandin@intel.com>
2023-07-07 11:04:19 +0400

    Update compile_graph.cpp

--- src/plugins/intel_gpu/src/graph/graph_optimizer/compile_graph.cpp
+++ src/plugins/intel_gpu/src/graph/graph_optimizer/compile_graph.cpp
@@ -52,12 +52,12 @@ void compile_graph::run(program& p) {
         // TODO: Remove this WA once we have shape agnostic reshape kernel
         if (node->is_type<reshape>() && node->is_dynamic() && !node->can_be_optimized())
             can_select_impl = false;
-        
+
         // TODO: Remove this WA once we have shape agnostic conv kernl with specified auto_pad attributes
         if (node->is_type<convolution>() && node->is_dynamic() && !node->as<convolution>().use_explicit_padding()) {
             can_select_impl = false;
         }
-        
+
         // TODO: need to come up with better handling of unsupported shape agnostic cases
         // e.g. process exceptions from choose_impl() and ignore those for dynamic parameters
         if (node->is_type<fully_connected>() && node->is_dynamic() && node->get_output_pshape().size() > 3)
