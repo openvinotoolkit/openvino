f35e9fa37a35927810626d15e0fb7d8c346c0b52
Ahn, Paul Y <paul.y.ahn@intel.com>
2023-07-10 14:08:43 +0900

    [GPU] disable blocked format for dynamic shape model(#18448)

--- src/plugins/intel_gpu/src/graph/graph_optimizer/select_preferred_formats.cpp
+++ src/plugins/intel_gpu/src/graph/graph_optimizer/select_preferred_formats.cpp
@@ -32,6 +32,7 @@ void select_preferred_formats::run(program& p) {
         return;
 
 #ifdef ENABLE_ONEDNN_FOR_GPU
+    bool allow_new_shape_infer = p.get_config().get_property(ov::intel_gpu::allow_new_shape_infer);
     engine.create_onednn_engine(p.get_config());
     for (auto n : p.get_processing_order()) {
         if (n->is_input() || !_lo.are_data_types_suitable_for_onednn(*n)) {
@@ -39,12 +40,14 @@ void select_preferred_formats::run(program& p) {
         }
         // Onednn primitive descriptor creation may fail, for example, due to asymmetric weight.
         try {
-            if (n->is_type<convolution>()) {
+            // Do not call select_preferred_formats_for_onednn for dynamic shape
+            // because currently gpu plugin doesn't fully support blocked format for dynamic shape
+            if (n->is_type<convolution>() && !allow_new_shape_infer) {
                 auto prim_desc = onednn::get_convolution_primitive_descriptor(*n->get_kernel_impl_params(),
                                                                               dnnl::primitive_attr(),
                                                                               dnnl::memory::format_tag::any);
                 _lo.select_preferred_formats_for_onednn(*n, *prim_desc);
-            } else if (n->is_type<deconvolution>()) {
+            } else if (n->is_type<deconvolution>() && !allow_new_shape_infer) {
                 auto prim_desc = onednn::get_deconvolution_primitive_descriptor(*n->get_kernel_impl_params(),
                                                                                 dnnl::primitive_attr(),
                                                                                 dnnl::memory::format_tag::any);
