9d45c3c51ced8874f7e955ce9df81194cbd32a1f
River.Li <river.li@intel.com>
2023-07-10 21:22:09 +0800

    Recover smoke_Transpose(2|4|5|6)D/TransposeLayerTest.CompareWithRefs due to fixing is merged

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/skip_tests_config.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/skip_tests_config.cpp
@@ -204,7 +204,6 @@ std::vector<std::string> disabledTestPatterns() {
         R"(.*smoke_IsOp/ComparisonLayerTest.*)",
         // Issue: 113703 - input/output port's name maybe changed after transformation in plugin api 2.0
         R"(.*smoke_If/SimpleIfTest.*Cond=0.*)",
-        R"(.*smoke_Transpose(2|4|5|6)D/TransposeLayerTest.CompareWithRefs.*)",
         // Issue: 113704 - Layout information maybe incorrect when covert tensor to blob
         R"(.*smoke_.*BehaviorTests/InferRequestPreprocessConversionTest.*)",
         // Issue: JIT choose error issue, don't know why not choose jit_avx512_BF16
