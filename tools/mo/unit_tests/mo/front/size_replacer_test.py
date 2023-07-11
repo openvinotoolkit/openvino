3c4ddd7a6024b1a272b90c9a1abdcc5fbcc839b6
PRATHAM-SPS <ingawalepratham@gmail.com>
2023-07-11 23:15:57 +0530

    replaced generator functionality in size_replacer_test.py

--- tools/mo/unit_tests/mo/front/size_replacer_test.py
+++ tools/mo/unit_tests/mo/front/size_replacer_test.py
@@ -4,7 +4,6 @@
 import unittest
 
 import numpy as np
-from generator import generator, generate
 
 from openvino.tools.mo.front.SizeReplacer import SizeFrontReplacer
 from openvino.tools.mo.front.common.partial_infer.utils import int64_array
@@ -23,29 +22,29 @@ nodes = lambda output_type: {
 }
 
 
-@generator
 class SizeReplacerTest(unittest.TestCase):
+    def test_size_replacer(self):
+        test_cases =[np.int32, np.int64]
+        for idx, (output_type) in enumerate(test_cases):
+            with self.subTest(test_cases=idx):
+                graph = build_graph(nodes_attrs=nodes(output_type), edges=[
+                    *connect('input', 'size'),
+                    *connect('size', 'output'),
+                ], nodes_with_edges_only=True)
+                SizeFrontReplacer().find_and_replace_pattern(graph)
 
-    @generate(np.int32, np.int64)
-    def test_size_replacer(self, output_type):
-        graph = build_graph(nodes_attrs=nodes(output_type), edges=[
-            *connect('input', 'size'),
-            *connect('size', 'output'),
-        ], nodes_with_edges_only=True)
-        SizeFrontReplacer().find_and_replace_pattern(graph)
+                graph_ref = build_graph(nodes_attrs=nodes(output_type), edges=[
+                    *connect('input', 'shape'),
+                    *connect('shape', '0:reduce'),
+                    *connect('zero', '1:reduce'),
+                    *connect('reduce', 'output'),
+                ], nodes_with_edges_only=True)
 
-        graph_ref = build_graph(nodes_attrs=nodes(output_type), edges=[
-            *connect('input', 'shape'),
-            *connect('shape', '0:reduce'),
-            *connect('zero', '1:reduce'),
-            *connect('reduce', 'output'),
-        ], nodes_with_edges_only=True)
-
-        (flag, resp) = compare_graphs(graph, graph_ref, 'output', check_op_attrs=True)
-        self.assertTrue(flag, resp)
-        self.assertEqual(graph.get_op_nodes(type='ReduceProd')[0]['name'], 'my_size',
-                         'Name is not inherited from original node for SizeReplacer')
-        print(output_type)
+                (flag, resp) = compare_graphs(graph, graph_ref, 'output', check_op_attrs=True)
+                self.assertTrue(flag, resp)
+                self.assertEqual(graph.get_op_nodes(type='ReduceProd')[0]['name'], 'my_size',
+                                'Name is not inherited from original node for SizeReplacer')
+                print(output_type)
 
     def test_size_replacer_assertion(self):
         graph = build_graph(nodes_attrs=nodes(None), edges=[
