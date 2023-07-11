118ed1b30b9953e736f68136f258d3250be21228
almilosz <alicja.miloszewska@intel.com>
2023-07-11 11:11:57 +0200

    Change double to single quotes

--- src/bindings/js/node/tests/basic.test.js
+++ src/bindings/js/node/tests/basic.test.js
@@ -2,13 +2,13 @@ const ov = require('../build/Release/ov_node_addon.node');
 const path = require('path');
 
 function getModelPath(isFP16=false){
-    basePath = "../../python/tests/"
+    basePath = '../../python/tests/'
     if (isFP16){
-        test_xml = path.join(basePath, "test_utils", "utils", "test_model_fp16.xml")
-        test_bin = path.join(basePath, "test_utils", "utils", "test_model_fp16.bin")
+        test_xml = path.join(basePath, 'test_utils', 'utils', 'test_model_fp16.xml')
+        test_bin = path.join(basePath, 'test_utils', 'utils', 'test_model_fp16.bin')
     } else {
-        test_xml = path.join(basePath, "test_utils", "utils", "test_model_fp32.xml")
-        test_bin = path.join(basePath, "test_utils", "utils", "test_model_fp32.bin")
+        test_xml = path.join(basePath, 'test_utils', 'utils', 'test_model_fp32.xml')
+        test_bin = path.join(basePath, 'test_utils', 'utils', 'test_model_fp32.bin')
     }
     return (test_xml, test_bin)
 }
@@ -31,30 +31,30 @@ describe('Output class', () => {
         expect(obj.output() && typeof obj.output() === 'object').toBe(true);
         expect(obj.outputs.length).toEqual(1);  
         //test for a obj with one output
-        expect(obj.output().toString()).toEqual("fc_out");
-        expect(obj.output(0).toString()).toEqual("fc_out");
-        expect(obj.output("fc_out").toString()).toEqual("fc_out");
+        expect(obj.output().toString()).toEqual('fc_out');
+        expect(obj.output(0).toString()).toEqual('fc_out');
+        expect(obj.output('fc_out').toString()).toEqual('fc_out');
         expect(obj.output(0).shape).toEqual([1, 10]);
         expect(obj.output(0).getShape().getData()).toEqual([1, 10]);
-        expect(obj.output().getAnyName()).toEqual("fc_out");
+        expect(obj.output().getAnyName()).toEqual('fc_out');
     });
 
     test('Ouput<ov::Node>.setNames() method', () => {
-        model.output().setNames(["bTestName", "cTestName"]);
-        expect(model.output().getAnyName()).toEqual("bTestName");
+        model.output().setNames(['bTestName', 'cTestName']);
+        expect(model.output().getAnyName()).toEqual('bTestName');
     });
 
     test('Ouput<ov::Node>.addNames() method', () => {
-        model.output().addNames(["aTestName"]);
-        expect(model.output().getAnyName()).toEqual("aTestName");
+        model.output().addNames(['aTestName']);
+        expect(model.output().getAnyName()).toEqual('aTestName');
     });
 
     test('Ouput<const ov::Node>.setNames() method', () => {
-        expect(() => compiledModel.output().setNames(["bTestName", "cTestName"])).toThrow(TypeError);
+        expect(() => compiledModel.output().setNames(['bTestName', 'cTestName'])).toThrow(TypeError);
     });
 
     test('Ouput<const ov::Node>.addNames() method', () => {
-        expect(() => compiledModel.output().addNames(["aTestName"])).toThrow(TypeError);
+        expect(() => compiledModel.output().addNames(['aTestName'])).toThrow(TypeError);
     });
 
 });
@@ -75,15 +75,15 @@ describe('Input class for ov::Input<const ov::Node>', () => {
 
     test('CompiledModel.input().ToString() method', () => {
         //test for a model with one output
-        expect(compiledModel.input().toString()).toEqual("data");
+        expect(compiledModel.input().toString()).toEqual('data');
     });
 
     test('CompiledModel.input(idx: number).ToString() method', () => {
-        expect(compiledModel.input(0).toString()).toEqual("data");
+        expect(compiledModel.input(0).toString()).toEqual('data');
     });
 
     test('CompiledModel.input(tensorName: string).ToString() method', () => {
-        expect(compiledModel.input("data").toString()).toEqual("data");
+        expect(compiledModel.input('data').toString()).toEqual('data');
     });
 
     test('Input.shape property with dimensions', () => {
