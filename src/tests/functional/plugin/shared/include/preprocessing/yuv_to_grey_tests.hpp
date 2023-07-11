f9fc11424aefc88082546b80ba5729d4f474dd31
Pawel Raasz <pawel.raasz@intel.com>
2023-07-11 12:41:24 +0200

    Grey preprocessing yuv nv12 i420 (#18239)
    * Add two plane YOV to Grey conversion
    
    * Add i420 to grey conversion
    
    * Add yuv to grey conversion for GPU
    
    * Fix cmakes
    
    * Remove static from local function
    
    * Remove opencv dependency from tests
    
    * Put grey_from_yuv_single_plane into namespace

---
+++ src/tests/functional/plugin/shared/include/preprocessing/yuv_to_grey_tests.hpp
@@ -0,0 +1,36 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include <openvino/core/preprocess/resize_algorithm.hpp>
+
+#include "shared_test_classes/base/ov_subgraph.hpp"
+
+using TParams = std::tuple<std::string>;
+
+namespace ov {
+namespace preprocess {
+class PreprocessingYUV2GreyTest : public testing::WithParamInterface<TParams>, public ov::test::SubgraphBaseTest {
+public:
+    static std::string getTestCaseName(const testing::TestParamInfo<TParams>& obj);
+
+protected:
+    void SetUp() override;
+    void run() override;
+    ov::TensorVector calculate_refs() override;
+
+    size_t get_full_height();
+    std::shared_ptr<ov::Model> build_test_model(const element::Type_t et, const Shape& shape);
+    void set_test_model_color_conversion(ColorFormat from, ColorFormat to);
+
+    ov::TensorVector ref_out_data;
+    size_t width, height;
+    int b_step;
+};
+
+}  // namespace preprocess
+}  // namespace ov
