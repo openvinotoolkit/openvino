e88530cabb808b205eca00ca550be58c5886d353
Vladislav Golubev <vladislav.golubev@intel.com>
2023-07-11 11:04:18 +0200

    Added MHAWithExtractedReshape test cases to cpu plugin tests

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/snippets/mha.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/snippets/mha.cpp
@@ -243,6 +243,23 @@ INSTANTIATE_TEST_SUITE_P(smoke_Snippets_MHATransposedB, MHATransposedB,
                                  ::testing::Values(std::map<std::string, std::string>{})),
                          MHA::getTestCaseName);
 
+const std::vector<std::vector<ov::PartialShape>> inputShapesExtractedReshape = {
+//     {{400, 196, 80}, {400, 80, 196}, {400, 14, 14, 14, 1}, {400, 14, 14, 1, 14}, {400, 196, 80}},
+    {{20, 16, 10}, {20, 10, 16}, {20, 4, 4, 4, 1}, {20, 4, 4, 1, 4}, {20, 16, 10}}
+};
+
+INSTANTIATE_TEST_SUITE_P(smoke_Snippets_MHAWithExtractedReshape, MHAWithExtractedReshape,
+                         ::testing::Combine(
+                                 ::testing::ValuesIn(inputShapesExtractedReshape),
+                                 ::testing::Values(std::vector<element::Type>{}),
+                                 ::testing::Values(ov::element::f32),
+                                 ::testing::ValuesIn({true}), // False is not supported for graph builder in tests
+                                 ::testing::Values(3), // Extracted Add + Extracted Reshape + MHA
+                                 ::testing::Values(2), // Extracted Add + MHA
+                                 ::testing::Values(CommonTestUtils::DEVICE_CPU),
+                                 ::testing::Values(std::map<std::string, std::string>{})),
+                         MHA::getTestCaseName);
+
 } // namespace
 } // namespace snippets
 } // namespace test
