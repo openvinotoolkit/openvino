4b3374a90409fd832fb6c677be76532eb7ebcc73
mitruska <katarzyna.mitrus@intel.com>
2023-07-11 13:29:21 +0200

    Use versions instead of opset

--- src/common/transformations/src/transformations/op_conversions/group_normalization_decomposition.cpp
+++ src/common/transformations/src/transformations/op_conversions/group_normalization_decomposition.cpp
@@ -17,8 +17,8 @@
 
 using namespace std;
 using namespace ov;
+using namespace op;
 using namespace ov::pass;
-using namespace ov::opset12;
 
 namespace {
 // This function creates a shape to which we need to reshape the input
@@ -29,36 +29,36 @@ std::shared_ptr<Node> create_group_norm_shape(NodeRegistry& reg,
                                               const Output<Node>& shape,
                                               size_t num_groups,
                                               size_t rank_size) {
-    const auto axis_node = reg.add(Constant::create(element::i64, Shape{}, {0}));
-    const auto split = reg.make<Split>(shape, axis_node, rank_size);
+    const auto axis_node = reg.add(v0::Constant::create(element::i64, Shape{}, {0}));
+    const auto split = reg.make<v1::Split>(shape, axis_node, rank_size);
     auto splits = split->outputs();
-    auto num_groups_const = reg.add(Constant::create(element::i64, Shape{1}, {num_groups}));
+    auto num_groups_const = reg.add(v0::Constant::create(element::i64, Shape{1}, {num_groups}));
     // The 4D shape: [N * num_groups, C // num_groups, H, W] is created
     // instead of 5D shape: [N, num_groups, C // num_groups, H, W].
     // The reason is the lack of support for 5D MVN input by some plugins.
-    OutputVector new_shape{reg.make<Multiply>(splits[0], num_groups_const),
-                           reg.make<Divide>(splits[1], num_groups_const)};
+    OutputVector new_shape{reg.make<v1::Multiply>(splits[0], num_groups_const),
+                           reg.make<v1::Divide>(splits[1], num_groups_const)};
 
     std::move(splits.begin() + 2, splits.end(), std::back_inserter(new_shape));
-    return reg.make<Concat>(new_shape, 0);
+    return reg.make<v0::Concat>(new_shape, 0);
 }
 
 std::shared_ptr<Node> get_range(NodeRegistry& reg, int64_t start, int64_t stop) {
     std::vector<int64_t> range_values(stop - start);
     std::iota(range_values.begin(), range_values.end(), start);
-    return reg.add(Constant::create(element::i64, {range_values.size()}, range_values));
+    return reg.add(v0::Constant::create(element::i64, {range_values.size()}, range_values));
 }
 }  // namespace
 
 ov::pass::GroupNormalizationDecomposition::GroupNormalizationDecomposition() {
     MATCHER_SCOPE(GroupNormalizationDecomposition);
 
-    auto group_norm_pattern = pattern::wrap_type<GroupNormalization>();
+    auto group_norm_pattern = pattern::wrap_type<v12::GroupNormalization>();
 
     matcher_pass_callback callback = [=](pattern::Matcher& matcher) {
         NodeRegistry reg;
 
-        const auto group_norm_node = std::dynamic_pointer_cast<GroupNormalization>(matcher.get_match_root());
+        const auto group_norm_node = std::dynamic_pointer_cast<v12::GroupNormalization>(matcher.get_match_root());
         if (!group_norm_node || transformation_callback(group_norm_node) || group_norm_node->get_input_size() != 3) {
             return false;
         }
@@ -76,20 +76,20 @@ ov::pass::GroupNormalizationDecomposition::GroupNormalizationDecomposition() {
             return false;
         }
         const auto data_rank_size = data_rank.get_length();
-        const auto data_shape_node = reg.make<ShapeOf>(data);
-        const auto data_reshaped = reg.make<Reshape>(
+        const auto data_shape_node = reg.make<v3::ShapeOf>(data);
+        const auto data_reshaped = reg.make<v1::Reshape>(
             data,
             create_group_norm_shape(reg, data_shape_node, num_groups, static_cast<size_t>(data_rank_size)),
             true);
         const auto reduction_axes = get_range(reg, 1, data_rank_size);
 
-        const auto mvn = reg.make<MVN>(data_reshaped, reduction_axes, true, eps, op::MVNEpsMode::INSIDE_SQRT);
-        std::shared_ptr<Node> result = reg.make<Reshape>(mvn, data_shape_node, true);
+        const auto mvn = reg.make<v6::MVN>(data_reshaped, reduction_axes, true, eps, op::MVNEpsMode::INSIDE_SQRT);
+        std::shared_ptr<Node> result = reg.make<v1::Reshape>(mvn, data_shape_node, true);
 
         // Unsqueeze scale and bias to shape: [C, 1, 1, ... ]
         const auto unsqueeze_axes = get_range(reg, 1, data_rank_size - 1);
-        result = reg.make<Multiply>(result, reg.make<Unsqueeze>(scale, unsqueeze_axes));
-        result = reg.make<Add>(result, reg.make<Unsqueeze>(bias, unsqueeze_axes));
+        result = reg.make<v1::Multiply>(result, reg.make<v0::Unsqueeze>(scale, unsqueeze_axes));
+        result = reg.make<v1::Add>(result, reg.make<v0::Unsqueeze>(bias, unsqueeze_axes));
 
         result->set_friendly_name(group_norm_node->get_friendly_name());
 
