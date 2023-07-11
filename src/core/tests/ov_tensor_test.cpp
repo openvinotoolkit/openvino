7e7f0866b81b2f57634a2137186521a41e17aa1f
Ilya Churaev <ilyachur@gmail.com>
2023-07-11 16:15:47 +0400

    Allow cast only if both element types are real or not

--- src/core/tests/ov_tensor_test.cpp
+++ src/core/tests/ov_tensor_test.cpp
@@ -42,7 +42,7 @@ TEST_F(OVTensorTest, canCreateTensor) {
     ASSERT_EQ(byteStrides(ov::Strides({6, 2, 1}), t.get_element_type()), t.get_strides());
     ASSERT_EQ(ov::element::f32.size() * totalSize, t.get_byte_size());
     ASSERT_THROW(t.data(ov::element::i64), ov::Exception);
-    ASSERT_NO_THROW(t.data<std::int32_t>());
+    ASSERT_THROW(t.data<std::int32_t>(), ov::Exception);
 }
 
 TEST_F(OVTensorTest, createTensorFromPort) {
@@ -72,8 +72,8 @@ TEST_F(OVTensorTest, canAccessF16Tensor) {
     EXPECT_NO_THROW(t.data(ov::element::f16));
     EXPECT_NO_THROW(t.data<ov::float16>());
     EXPECT_NO_THROW(t.data<ov::bfloat16>());
-    EXPECT_NO_THROW(t.data<std::uint16_t>());
-    EXPECT_NO_THROW(t.data<std::int16_t>());
+    EXPECT_THROW(t.data<std::uint16_t>(), ov::Exception);
+    EXPECT_THROW(t.data<std::int16_t>(), ov::Exception);
 }
 
 TEST_F(OVTensorTest, emptySize) {
