8293f983d70e266a9a87bf60d42914dc402c6001
Anastasia Kuporosova <anastasia.kuporosova@intel.com>
2023-07-12 08:58:24 +0000

    py wrap for clone_model

---
+++ src/bindings/python/src/openvino/runtime/utils/util.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018-2023 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+from openvino._pyopenvino.util import clone_model as clone_model_base
+
+def clone_model(model):
+    from openvino.runtime import Model
+    return Model(clone_model_base(model))
