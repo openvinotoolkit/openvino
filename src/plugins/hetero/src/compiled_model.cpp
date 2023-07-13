909a2bdbf19d8948b016dd308483d7cb33b803d4
vurusovs <vitaliy.urusovskij@intel.com>
2023-07-13 13:05:10 +0400

    Fix CompiledModel `ov::device::properties` issue

--- src/plugins/hetero/src/compiled_model.cpp
+++ src/plugins/hetero/src/compiled_model.cpp
@@ -587,10 +587,16 @@ ov::Any ov::hetero::CompiledModel::get_property(const std::string& name) const {
         return to_string_vector(m_cfg.get_supported());
     } else if (ov::device::properties == name) {
         ov::AnyMap all_devices = {};
-        for (const auto& comp_model_desc : m_compiled_submodels)
-            if (all_devices.count(comp_model_desc.device) == 0)
-                all_devices[comp_model_desc.device] =
+        for (const auto& comp_model_desc : m_compiled_submodels) {
+            ov::AnyMap device_properties = {};
+            if (all_devices.count(comp_model_desc.device) == 0) {
+                auto device_supported_props =
                     comp_model_desc.compiled_model->get_property(ov::supported_properties.name());
+                for (auto&& property_name : device_supported_props.as<std::vector<ov::PropertyName>>())
+                    device_properties[property_name] = comp_model_desc.compiled_model->get_property(property_name);
+                all_devices[comp_model_desc.device] = device_properties;
+            }
+        }
         return all_devices;
     } else if (ov::model_name == name) {
         return decltype(ov::model_name)::value_type(m_name);
