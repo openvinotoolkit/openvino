e2ff77a2d8f77ab4a19a7ed8bac75c608eb4a6e2
Vishniakov Nikolai <nikolai.vishniakov@intel.com>
2023-07-13 19:33:53 +0200

    Add helpers functions

--- samples/js/node/notebooks/helpers.js
+++ samples/js/node/notebooks/helpers.js
@@ -10,6 +10,8 @@ exports.getImageData = getImageData;
 exports.displayArrayAsImage = displayArrayAsImage;
 exports.transform = transform;
 exports.downloadFile = downloadFile;
+exports.extractValues = extractValues;
+exports.setShape = setShape;
 
 function arrayToImageData(array, width, height) {
   return createImageData(new Uint8ClampedArray(array), width, height);
@@ -124,3 +126,187 @@ function downloadFile(url, filename, destination) {
     });
   });
 }
+
+function sum(array) {
+  return array.reduce((acc, val) => acc+val, 0);
+}
+
+function mul(array) {
+  return array.reduce((acc, val) => acc*val, 1);
+}
+
+function setShape(flatArray, shape) {
+  console.log({ shape, l: flatArray.length })
+
+  if (mul(shape) !== flatArray.length)
+    throw new Error('Shape doesn\'t according to array length');
+
+  return createMultidimensionArray(flatArray, shape, 0);
+}
+
+function createMultidimensionArray(flatArray, shape, offset) {
+  const currentDim = shape[0];
+  const remainingShape = shape.slice(1);
+  const currentArray = [];
+
+  if (remainingShape.length === 0) {
+    for (let i = 0; i < currentDim; i++)
+      currentArray.push(flatArray[offset + i]);
+  }
+  else {
+    const innerArrayLength = mul(shape) / currentDim;
+
+    for (let i = 0; i < currentDim; i++) {
+      const innerArray = createMultidimensionArray(flatArray, remainingShape, offset + i*innerArrayLength);
+      currentArray.push(innerArray);
+    }
+  }
+
+  return currentArray;
+}
+
+function extractValues(arrOrVal, collector = []) {
+  if (arrOrVal[Symbol.iterator] && arrOrVal.map) {
+    arrOrVal.map(v => extractValues(v, collector));
+  }
+  else {
+    collector.push(arrOrVal);
+  }
+
+  return collector;
+}
+
+function isIterableArray(arr) {
+  return arr[Symbol.iterator] && arr.map;
+}
+
+function eachInner(arrOrValue, fn) {
+  return isIterableArray(arrOrValue)
+    ? arrOrValue.map(e => eachInner(e, fn))
+    : fn(arrOrValue);
+}
+
+function exp(arr) {
+  return eachInner(arr, Math.exp);
+}
+
+function sumRows(arr) {
+  return arr.map(row => sum(row));
+}
+
+function reshape(arr, newShape) {
+  const flat = extractValues(arr);
+
+  return setShape(flat, newShape);
+}
+
+function getShape(arr, acc = []) {
+  if (isIterableArray(arr)) {
+    acc.push(arr.length);
+    getShape(arr[0], acc);
+  }
+
+  return acc;
+}
+
+function matrixMultiplication(matrix1, matrix2) {
+  const rows1 = matrix1.length;
+  const cols1 = matrix1[0].length;
+  const rows2 = matrix2.length;
+  const cols2 = matrix2[0].length;
+
+  if (cols1 !== rows2)
+    throw new Error('Number of columns in the first matrix must match the number of rows in the second matrix.');
+
+  const result = [];
+
+  for (let i = 0; i < rows1; i++) {
+    result[i] = [];
+    for (let j = 0; j < cols2; j++) {
+      let sum = 0;
+      for (let k = 0; k < cols1; k++) {
+        sum += matrix1[i][k] * matrix2[k][j];
+      }
+      result[i][j] = sum;
+    }
+  }
+
+  return result;
+}
+
+function findMax(arr) {
+  let max = -Infinity;
+  let index = -1;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < max) continue;
+
+    max = arr[i];
+    index = i;
+  }
+
+  return { value: max, index };
+}
+
+function argMax(arr) {
+  return findMax(arr).index;
+}
+
+
+function triu(matrix, k = 0) {
+  const numRows = matrix.length;
+  const numCols = matrix[0].length;
+
+  const result = [];
+
+  for (let i = 0; i < numRows; i++) {
+    result[i] = [];
+    for (let j = 0; j < numCols; j++) {
+      if (i <= j - k) {
+        result[i][j] = matrix[i][j];
+      } else {
+        result[i][j] = 0;
+      }
+    }
+  }
+
+  return result;
+}
+
+function tril(matrix, k = 0) {
+  const numRows = matrix.length;
+  const numCols = matrix[0].length;
+
+  const result = [];
+
+  for (let i = 0; i < numRows; i++) {
+    result[i] = [];
+    for (let j = 0; j < numCols; j++) {
+      if (i >= j - k) {
+        result[i][j] = matrix[i][j];
+      } else {
+        result[i][j] = 0;
+      }
+    }
+  }
+
+  return result;
+}
+
+function arange(count) {
+  const arr = new Array(count);
+
+  for (let i = 0; i < count; i++) arr[i] = i;
+
+  return arr;
+}
+
+exports.exp = exp;
+exports.sumRows = sumRows;
+exports.reshape = reshape;
+exports.getShape = getShape;
+exports.argMax = argMax;
+exports.triu = triu;
+exports.tril = tril;
+exports.arange = arange;
+exports.matrixMultiplication = matrixMultiplication;
