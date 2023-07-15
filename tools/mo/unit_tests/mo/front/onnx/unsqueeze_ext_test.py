ecf7b803d329dee9ea0b961646738bf7e7290e7d
PRATHAM-SPS <ingawalepratham@gmail.com>
2023-07-15 23:17:26 +0530

    replaced generator functionality in unsqueeze_ext_test.py

--- tools/mo/unit_tests/mo/front/onnx/unsqueeze_ext_test.py
+++ tools/mo/unit_tests/mo/front/onnx/unsqueeze_ext_test.py
@@ -5,7 +5,6 @@ import unittest
 
 import numpy as np
 import onnx
-from generator import generator, generate
 
 from openvino.tools.mo.front.onnx.unsqueeze_ext import UnsqueezeFrontExtractor
 from openvino.tools.mo.ops.op import Op
@@ -13,7 +12,6 @@ from openvino.tools.mo.ops.unsqueeze import Unsqueeze
 from unit_tests.utils.extractors import PB
 
 
-@generator
 class TestUnsqueezeONNXExt(unittest.TestCase):
     @staticmethod
     def _create_unsqueeze_node(axes):
@@ -38,17 +36,19 @@ class TestUnsqueezeONNXExt(unittest.TestCase):
     def setUpClass(cls):
         Op.registered_ops['Unsqueeze'] = Unsqueeze
 
-    @generate(*[[0, 1, 2, 3], [1], []])
-    def test_unsqueeze_ext(self, axes):
-        node = self._create_unsqueeze_node(axes)
-        UnsqueezeFrontExtractor.extract(node)
+    def test_unsqueeze_ext(self):
+        test_cases=[[0, 1, 2, 3], [1], []]
+        for idx, (axes) in enumerate(test_cases):
+            with self.subTest(test_cases=idx):
+                node = self._create_unsqueeze_node(axes)
+                UnsqueezeFrontExtractor.extract(node)
 
-        exp_res = {
-            'expand_axis': axes,
-        }
+                exp_res = {
+                    'expand_axis': axes,
+                }
 
-        for key in exp_res.keys():
-            if type(node[key]) in [list, np.ndarray]:
-                self.assertTrue(np.array_equal(np.array(node[key]), np.array(exp_res[key])))
-            else:
-                self.assertEqual(node[key], exp_res[key])
+                for key in exp_res.keys():
+                    if type(node[key]) in [list, np.ndarray]:
+                        self.assertTrue(np.array_equal(np.array(node[key]), np.array(exp_res[key])))
+                    else:
+                        self.assertEqual(node[key], exp_res[key])
