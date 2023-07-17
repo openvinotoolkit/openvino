402316aacb74f98d01043b5cd3990f71e56be8a5
wilson-seok <wilson.seok@intel.com>
2023-07-18 00:29:52 +0900

    add func test cases

--- src/plugins/intel_gpu/tests/functional/single_layer_tests/dynamic/prior_box.cpp
+++ src/plugins/intel_gpu/tests/functional/single_layer_tests/dynamic/prior_box.cpp
@@ -19,12 +19,17 @@ using namespace ov::test;
 using ElementType = ov::element::Type_t;
 
 namespace GPULayerTestsDefinitions {
+enum class priorbox_type {
+    V0,
+    V8,
+    Clustered
+};
 typedef std::tuple<
         InputShape,
         InputShape,
-        ElementType                // Net precision
+        ElementType,                // Net precision
+        priorbox_type
 > PriorBoxLayerGPUTestParamsSet;
-
 class PriorBoxLayerGPUTest : public testing::WithParamInterface<PriorBoxLayerGPUTestParamsSet>,
                             virtual public SubgraphBaseTest {
 public:
@@ -32,10 +37,19 @@ public:
         InputShape input1Shape;
         InputShape input2Shape;
         ElementType netPrecision;
-        std::tie(input1Shape, input1Shape, netPrecision) = obj.param;
+        priorbox_type priorboxType;
+        std::tie(input1Shape, input1Shape, netPrecision, priorboxType) = obj.param;
 
         std::ostringstream result;
-        result << "PriorBoxTest_";
+        switch (priorboxType) {
+            case priorbox_type::Clustered:
+                result << "PriorBoxClusteredTest_";
+            case priorbox_type::V0:
+                result << "PriorBoxV0Test_";
+            case priorbox_type::V8:
+            default:
+                result << "PriorBoxV8Test_";
+        }
         result << std::to_string(obj.index) << "_";
         result << "netPrec=" << netPrecision << "_";
         result << "I1S=";
@@ -61,7 +75,8 @@ protected:
         auto netPrecision = ElementType::undefined;
         InputShape input1Shape;
         InputShape input2Shape;
-        std::tie(input1Shape, input2Shape, netPrecision) = this->GetParam();
+        priorbox_type priorboxType;
+        std::tie(input1Shape, input2Shape, netPrecision, priorboxType) = this->GetParam();
 
         init_input_shapes({input1Shape, input2Shape});
 
@@ -83,6 +98,65 @@ protected:
         auto stridedSliceOp2 = ngraph::builder::makeStridedSlice(shapeOfOp2, beginInput, endInput, strideInput, element::i32,
                                                                 {0}, {1}, {0}, {0}, {0});
 
+        switch (priorboxType) {
+            case priorbox_type::Clustered: {
+                ngraph::op::v0::PriorBoxClustered::Attributes attributes_clustered;
+
+                attributes_clustered.widths = {86, 13, 57, 39, 68, 34, 142, 50, 23};
+                attributes_clustered.heights = {44, 10, 30, 19, 94, 32, 61, 53, 17};
+                attributes_clustered.variances = {0.1, 0.1, 0.2, 0.2};
+                attributes_clustered.step = 16;
+                attributes_clustered.step_widths = 0;
+                attributes_clustered.step_heights = 0;
+                attributes_clustered.offset = 0.5;
+                attributes_clustered.clip = false;
+
+                auto priorBoxOp = std::make_shared<ngraph::op::v0::PriorBoxClustered>(stridedSliceOp1, stridedSliceOp2, attributes_clustered);
+
+                ngraph::ResultVector results{std::make_shared<ngraph::opset1::Result>(priorBoxOp)};
+                function = std::make_shared <ngraph::Function>(results, functionParams, "PriorBoxV0Function");
+                break;
+            }
+            case priorbox_type::V0: {
+                ngraph::op::v0::PriorBox::Attributes attributes_v0;
+
+                attributes_v0.min_size = {64};
+                attributes_v0.max_size = {300};
+                attributes_v0.aspect_ratio = {2};
+                attributes_v0.variance = {0.1, 0.1, 0.2, 0.2};
+                attributes_v0.step = 16;
+                attributes_v0.offset = 0.5;
+                attributes_v0.clip = false;
+                attributes_v0.flip = true;
+                attributes_v0.scale_all_sizes = true;
+
+                auto priorBoxOp = std::make_shared<ngraph::op::v0::PriorBox>(stridedSliceOp1, stridedSliceOp2, attributes_v0);
+
+                ngraph::ResultVector results{std::make_shared<ngraph::opset1::Result>(priorBoxOp)};
+                function = std::make_shared <ngraph::Function>(results, functionParams, "PriorBoxV0Function");
+                break;
+            }
+            case priorbox_type::V8:
+            default: {
+                ngraph::op::v8::PriorBox::Attributes attributes_v8;
+
+                attributes_v8.min_size = {64};
+                attributes_v8.max_size = {300};
+                attributes_v8.aspect_ratio = {2};
+                attributes_v8.variance = {0.1, 0.1, 0.2, 0.2};
+                attributes_v8.step = 16;
+                attributes_v8.offset = 0.5;
+                attributes_v8.clip = false;
+                attributes_v8.flip = true;
+                attributes_v8.scale_all_sizes = true;
+                attributes_v8.min_max_aspect_ratios_order = true;
+
+                auto priorBoxOp = std::make_shared<ngraph::op::v8::PriorBox>(stridedSliceOp1, stridedSliceOp2, attributes_v8);
+
+                ngraph::ResultVector results{std::make_shared<ngraph::opset1::Result>(priorBoxOp)};
+                function = std::make_shared <ngraph::Function>(results, functionParams, "PriorBoxV8Function");
+            }
+        }
         ngraph::op::v8::PriorBox::Attributes attributes;
         attributes.min_size = {64};
         attributes.max_size = {300};
@@ -114,6 +188,12 @@ const std::vector<ElementType> netPrecisions = {
         ElementType::f32,
 };
 
+const std::vector<priorbox_type> mode = {
+        priorbox_type::V0,
+        priorbox_type::V8,
+        priorbox_type::Clustered
+};
+
 std::vector<ov::test::InputShape> inShapesDynamic = {
         {
             {1, 3, -1, -1},
@@ -135,7 +215,8 @@ INSTANTIATE_TEST_SUITE_P(smoke_prior_box_dynamic,
     ::testing::Combine(
         ::testing::ValuesIn(inShapesDynamic),
         ::testing::ValuesIn(imgShapesDynamic),
-        ::testing::ValuesIn(netPrecisions)),
+        ::testing::ValuesIn(netPrecisions),
+        ::testing::ValuesIn(mode)),
     PriorBoxLayerGPUTest::getTestCaseName);
 } // namespace
 
