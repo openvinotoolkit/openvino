ed3a957ceec08abc7c2a54e40d30fdf0430e3a1d
jiwaszki <jan.iwaszkiewicz@intel.com>
2023-07-17 15:17:00 +0000

    Testing wrong solution

--- src/bindings/python/src/openvino/__init__.py
+++ src/bindings/python/src/openvino/__init__.py
@@ -2,48 +2,45 @@
 # Copyright (C) 2018-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore # mypy issue #1422
-
-import warnings
-
 # Required for Windows OS platforms
-from .utils import _add_openvino_libs_to_search_path
+from openvino.utils import _add_openvino_libs_to_search_path
 
 _add_openvino_libs_to_search_path()
 
 # Import openvino.runtime and most important classes
-from . import runtime
+from openvino import runtime
 
-from .runtime import get_version
+from openvino.runtime import get_version
 __version__ = get_version()
 
-from .runtime import Model
-from .runtime import Core
-from .runtime import AsyncInferQueue
-from .runtime import Type
-from .runtime import PartialShape
-from .runtime import Shape
-from .runtime import Strides
-from .runtime import Layout
-from .runtime import Tensor
+from openvino.runtime import Model
+from openvino.runtime import Core
+from openvino.runtime import AsyncInferQueue
+from openvino.runtime import Type
+from openvino.runtime import PartialShape
+from openvino.runtime import Shape
+from openvino.runtime import Strides
+from openvino.runtime import Layout
+from openvino.runtime import Tensor
 
-from .runtime import get_batch
-from .runtime import set_batch
-from .runtime import serialize
-from .runtime import shutdown
-from .runtime import tensor_from_file
-from .runtime import compile_model
+from openvino.runtime import get_batch
+from openvino.runtime import set_batch
+from openvino.runtime import serialize
+from openvino.runtime import shutdown
+from openvino.runtime import tensor_from_file
+from openvino.runtime import compile_model
 
 # TODO should opsets go here?
 
 # Import all additional modules
-from . import frontend
-from . import helpers
-from . import preprocess
+from openvino import frontend as frontend
+from openvino import helpers as helpers
+from openvino import preprocess as preprocess
 
 # Try to import openvino.tools
 try:
-    from . import tools
-    from .tools.mo import convert_model  # TODO , InputCutInfo, LayoutMap
+    from openvino import tools
+    from openvino.tools.mo import convert_model, InputCutInfo, LayoutMap
 except ImportError:
+    import warnings
     warnings.warn("openvino.tools module could not be found!", ImportWarning, stacklevel=2)
