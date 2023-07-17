d4645a1a65fa0f0fe0d7b67e3f6b4be736caa233
almilosz <alicja.miloszewska@intel.com>
2023-07-17 11:46:17 +0200

    Add  infer({input0: tensor})

--- src/bindings/js/node/src/infer_request.cpp
+++ src/bindings/js/node/src/infer_request.cpp
@@ -49,48 +49,20 @@ Napi::Value InferRequestWrap::set_input_tensor(const Napi::CallbackInfo& info) {
 Napi::Value InferRequestWrap::infer(const Napi::CallbackInfo& info) {
     if (info.Length() == 0)
         _infer_request.infer();
-    else if (info.Length() == 1) {
-        const auto elem = info[0];
+    else if (info.Length() == 1 && info[0].IsObject()) {
+        const auto inputs = info[0].As<Napi::Object>();
+        auto keys = inputs.GetPropertyNames();
 
-        if (elem.IsArray()) {
-            auto array = elem.As<Napi::Array>();
-            size_t i = 0;
+        for (size_t i = 0; i < keys.Length(); i++) {
+            auto tensor_name = static_cast<Napi::Value>(keys[i]).ToString().Utf8Value();
+            auto tensor_obj = inputs.Get(tensor_name).As<Napi::Object>();
 
-            // TO_DO
-            Napi::Value arrayItem = array[i];
-            if (!arrayItem.IsObject()) {
-                throw std::invalid_argument(std::string("Passed array must contain objects."));
-            }
-            Napi::Object obj = arrayItem.As<Napi::Object>();
+            auto* tensorWrap = Napi::ObjectWrap<TensorWrap>::Unwrap(tensor_obj);
+            auto tensor = tensorWrap->get_tensor();
 
-            auto* tensorWrap = Napi::ObjectWrap<TensorWrap>::Unwrap(obj);
-            ov::Tensor tensor = tensorWrap->get_tensor();
-
-            _infer_request.set_input_tensor(tensor);
-            _infer_request.infer();
-
-        } else if (elem.IsObject()) {
-            // elem z js = {"input:0": tensor}
-            Napi::Object obj = elem.As<Napi::Object>();
-
-            // for (const auto& it : obj) {
-            //     if(input.IsString()){
-            //         std::cout<< (it.first).As<String>().Utf8Value()<< " ";
-            //     }
-            // }
-
-            auto keys = obj.GetPropertyNames();
-            for (size_t i = 0; i < keys.Length(); i++) {
-                auto tensor_name = static_cast<Napi::Value>(keys[i]).ToString().Utf8Value();
-                auto tensor_obj = obj.Get(tensor_name).As<Napi::Object>();
-
-                auto* tensorWrap = Napi::ObjectWrap<TensorWrap>::Unwrap(tensor_obj);
-                ov::Tensor t = tensorWrap->get_tensor();
-
-                _infer_request.set_tensor(tensor_name, t);
-            }
-            _infer_request.infer();
+            _infer_request.set_tensor(tensor_name, tensor);
         }
+        _infer_request.infer();
     } else {
         reportError(info.Env(), "Inference error.");
     }
