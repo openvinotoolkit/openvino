bf02d28734d150fd724bce0f06b9a3eacddf7eaf
Jade Cho <jade.cho@intel.com>
2023-07-18 11:27:21 +0900

    [GPU] Copy from usm_host memory to cl_mem memory. (#18555)

--- src/plugins/intel_gpu/src/runtime/ocl/ocl_memory.cpp
+++ src/plugins/intel_gpu/src/runtime/ocl/ocl_memory.cpp
@@ -89,16 +89,32 @@ shared_mem_params gpu_buffer::get_internal_params() const {
 }
 
 event::ptr gpu_buffer::copy_from(stream& stream, const memory& other, bool blocking) {
-    auto& cl_stream = downcast<ocl_stream>(stream);
-    auto& mem_inst = downcast<const gpu_buffer>(other);
-    auto ev = stream.create_base_event();
-    cl::Event& ev_ocl = downcast<ocl_event>(ev.get())->get();
-    cl_stream.get_cl_queue().enqueueCopyBuffer(mem_inst.get_buffer(), get_buffer(), 0, 0, other.size(), nullptr, &ev_ocl);
+    switch (other.get_allocation_type()) {
+    case allocation_type::usm_host:
+    case allocation_type::usm_shared:
+        {
+            // If other is gpu_usm, down cast to gpu_buffer is not possible.
+            // But it can read as host ptr if it's allocation type is either usm_host or usm_shared.
+            auto& mem_inst = downcast<const gpu_usm>(other);
+            return copy_from(stream, mem_inst.buffer_ptr(), blocking);
+        }
+    case allocation_type::cl_mem:
+        {
+            auto& cl_stream = downcast<ocl_stream>(stream);
+            auto& mem_inst = downcast<const gpu_buffer>(other);
+            auto ev = stream.create_base_event();
+            cl::Event& ev_ocl = downcast<ocl_event>(ev.get())->get();
+            cl_stream.get_cl_queue().enqueueCopyBuffer(mem_inst.get_buffer(), get_buffer(), 0, 0, other.size(), nullptr, &ev_ocl);
 
-    if (blocking)
-        ev->wait();
+            if (blocking)
+                ev->wait();
 
-    return ev;
+            return ev;
+        }
+    case allocation_type::usm_device:
+    default:
+        throw std::runtime_error("Unsupported allocation_type.");
+    }
 }
 
 event::ptr gpu_buffer::copy_from(stream& stream, const void* host_ptr, bool blocking) {
