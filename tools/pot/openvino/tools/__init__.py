371fe8254829b636a70fdeb5c391610d4096f1dc
jiwaszki <jan.iwaszkiewicz@intel.com>
2023-07-19 15:32:40 +0000

    mo modules mo problems

--- tools/pot/openvino/tools/__init__.py
+++ tools/pot/openvino/tools/__init__.py
@@ -1,3 +1,8 @@
-# Copyright (C) 2020-2022 Intel Corporation
+# Copyright (C) 2018-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+try:
+    import openvino.tools.mo
+except ImportError:
+    import warnings
+    warnings.warn("openvino.tools.mo module could not be found!", ImportWarning, stacklevel=2)
