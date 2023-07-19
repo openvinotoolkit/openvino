f65be2b94ad46b50ff9c9067a3923be573b68d7c
Nesterov Alexander <nesterov.alexander@outlook.com>
2023-07-19 08:01:31 +0200

    [ARM CPU] ACL TBB scheduler (#17445)

--- src/plugins/intel_cpu/src/plugin.cpp
+++ src/plugins/intel_cpu/src/plugin.cpp
@@ -38,6 +38,11 @@
 #include <cpu/x64/cpu_isa_traits.hpp>
 #include <itt.h>
 
+#if defined(OV_CPU_WITH_ACL)
+#include "nodes/executors/acl/acl_ie_scheduler.hpp"
+#include "arm_compute/runtime/CPP/CPPScheduler.h"
+#endif
+
 using namespace InferenceEngine;
 
 #define IE_CPU_PLUGIN_THROW(...) IE_THROW(__VA_ARGS__) << "CPU plugin: "
@@ -142,6 +147,10 @@ Engine::Engine() :
     specialSetup(new CPUSpecialSetup) {
     _pluginName = "CPU";
     extensionManager->AddExtension(std::make_shared<Extension>());
+#if defined(OV_CPU_WITH_ACL)
+    acl_scheduler = std::make_unique<ACLScheduler>();
+    arm_compute::Scheduler::set(acl_scheduler);
+#endif
 }
 
 Engine::~Engine() {
