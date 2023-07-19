1d2c179d34b01e72485eccbad392d5a4a52a4965
jiwaszki <jan.iwaszkiewicz@intel.com>
2023-07-19 12:52:57 +0000

    Testing test test

--- tools/mo/openvino/__init__.py
+++ tools/mo/openvino/__init__.py
@@ -10,9 +10,6 @@ _add_openvino_libs_to_search_path()
 # Import openvino.runtime and most important classes
 from openvino import runtime
 
-from openvino.runtime import get_version
-__version__ = get_version()
-
 from openvino.runtime import Model
 from openvino.runtime import Core
 from openvino.runtime import AsyncInferQueue
@@ -29,18 +26,16 @@ from openvino.runtime import serialize
 from openvino.runtime import shutdown
 from openvino.runtime import tensor_from_file
 from openvino.runtime import compile_model
-
-# TODO should opsets go here?
+from openvino.runtime import get_version
+# Set version for openvino package
+__version__ = get_version()
 
 # Import all additional modules
 from openvino import frontend as frontend
 from openvino import helpers as helpers
 from openvino import preprocess as preprocess
 
-# Try to import openvino.tools
-try:
-    from openvino import tools
-    from openvino.tools.mo import convert_model  # TODO , InputCutInfo, LayoutMap
-except ImportError:
-    import warnings
-    warnings.warn("openvino.tools module could not be found!", ImportWarning, stacklevel=2)
+# Import openvino.tools
+from openvino import tools
+# Model Conversion API - ovc should reside in the main namespace
+from openvino.tools.ovc import convert_model, InputCutInfo, LayoutMap
