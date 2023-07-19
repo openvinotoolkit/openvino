6ea700610bb169dad54e9913b3f6e2678fdb6eb8
Roboreptile <piotr.krzeminski@intel.com>
2023-07-19 11:43:31 +0000

    [PT FE] Fix conversion errors for aten::quantize_per_channel

--- src/frontends/pytorch/src/utils_quantize.cpp
+++ src/frontends/pytorch/src/utils_quantize.cpp
@@ -5,13 +5,17 @@
 #include "utils_quantize.hpp"
 
 #include "openvino/frontend/pytorch/node_context.hpp"
+#include "openvino/op/add.hpp"
 #include "openvino/op/broadcast.hpp"
 #include "openvino/op/convert.hpp"
 #include "openvino/op/convert_like.hpp"
 #include "openvino/op/fake_quantize.hpp"
+#include "openvino/op/less.hpp"
 #include "openvino/op/multiply.hpp"
 #include "openvino/op/reshape.hpp"
 #include "openvino/op/scatter_elements_update.hpp"
+#include "openvino/op/select.hpp"
+#include "openvino/op/shape_of.hpp"
 #include "openvino/op/subtract.hpp"
 
 namespace ov {
@@ -67,9 +71,9 @@ ov::Output<ov::Node> quantize(const NodeContext& context,
         const auto input_convert = context.mark_node(std::make_shared<v0::Convert>(input, element::f32));
         const auto scales_convert = context.mark_node(std::make_shared<v0::Convert>(scale, element::f32));
         const auto zero_points_convert = context.mark_node(std::make_shared<v0::Convert>(zero_point, element::f32));
-        const auto axis_convert = context.mark_node(std::make_shared<v0::Convert>(zero_point, element::i32));
+        const auto axis_convert = context.mark_node(std::make_shared<v0::Convert>(axis, element::i32));
 
-        const auto neg_one = context.mark_node(v0::Constant::create(element::i32, Shape{}, {-1}));
+        const auto neg_one = context.mark_node(v0::Constant::create(element::i32, Shape{1}, {-1}));
         const auto zero = context.mark_node(v0::Constant::create(element::i32, Shape{}, {0}));
         const auto one = context.mark_node(v0::Constant::create(element::i32, Shape{}, {1}));
 
@@ -88,8 +92,9 @@ ov::Output<ov::Node> quantize(const NodeContext& context,
         const auto out_low = context.mark_node(v0::Constant::create(element::f32, Shape{}, {out_low_i64}));
         const auto out_high = context.mark_node(v0::Constant::create(element::f32, Shape{}, {out_high_i64}));
 
-        const auto rank = std::get<1>(get_shape_rank(context, input_convert));
+        const auto rank = std::get<1>(get_shape_rank(context, input_convert, false, element::i32));
         const auto ones = context.mark_node(std::make_shared<v3::Broadcast>(one, rank));
+
         const auto normalized_axis = normalize_axis(context, axis_convert, input_convert);
         const auto new_shape =
             context.mark_node(std::make_shared<v3::ScatterElementsUpdate>(ones, normalized_axis, neg_one, zero));
@@ -105,13 +110,14 @@ ov::Output<ov::Node> quantize(const NodeContext& context,
         const auto bound_high = context.mark_node(std::make_shared<v1::Multiply>(scale_bc, out_high_normalized));
 
         const auto quantized_input = context.mark_node(
-            std::make_shared<v0::FakeQuantize>(input_convert, out_low, out_high, bound_low, bound_high, levels));
+            std::make_shared<v0::FakeQuantize>(input_convert, bound_low, bound_high, bound_low, bound_high, levels));
 
         return context.mark_node(std::make_shared<QuantizedPtNode>(quantization_type,
                                                                    context,
                                                                    quantized_input,
                                                                    scale_bc,
                                                                    zero_point_bc,
+                                                                   axis_convert,
                                                                    dtype));
     }
     FRONT_END_OP_CONVERSION_CHECK(false, "Got unknown quantization method in quantize.");
