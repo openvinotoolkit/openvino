cd8fa5b313c155d41233744f371c72ff16056535
Mateusz <mateusz.mikolajczyk@intel.com>
2023-07-19 13:17:34 +0000

    Add requested checks

--- src/frontends/pytorch/src/op/quantized_linear.cpp
+++ src/frontends/pytorch/src/op/quantized_linear.cpp
@@ -18,8 +18,18 @@ OutputVector translate_quantized_linear(const NodeContext& context) {
     // int Y_zero_point_i) -> Tensor Y"
     num_inputs_check(context, 4, 4);
     auto x = context.get_input(0);
-    auto packed_params_input = context.get_input(1).get_node();
-    auto packed_params = packed_params_input->inputs();
+    auto packed_params_node =
+        std::dynamic_pointer_cast<ov::op::util::FrameworkNode>(context.get_input(1).get_node_shared_ptr());
+    FRONT_END_OP_CONVERSION_CHECK(packed_params_node, "Packed params input node type is required to be FrameworkNode.");
+    const auto& attrs = packed_params_node->get_attrs();
+    FRONT_END_OP_CONVERSION_CHECK((attrs.find(PtFrameworkNode::op_type_key) != attrs.end()),
+                                  "Packed params input node does not contain information about op type.");
+    FRONT_END_OP_CONVERSION_CHECK((attrs.at(PtFrameworkNode::op_type_key) == "prim::GetAttr"),
+                                  "Incorrect packed params input node operator type, expected prim::GetAttr.");
+
+    auto packed_params = packed_params_node->inputs();
+    FRONT_END_OP_CONVERSION_CHECK(packed_params.size() == 2,
+                                  "Packed parameters for quantized linear should contain 2 items.");
     auto weights = packed_params[0].get_source_output();
     auto bias = packed_params[1].get_source_output();
 
@@ -27,7 +37,7 @@ OutputVector translate_quantized_linear(const NodeContext& context) {
     linear = context.mark_node(std::make_shared<ov::op::v1::Add>(linear, bias));
     auto scale = context.get_input(2);
     auto zero_point = context.get_input(3);
-    return {context.mark_output(quantize(context, linear, scale, zero_point, context.get_input(0)))};
+    return {context.mark_output(quantize(context, linear, scale, zero_point, x))};
 };
 
 }  // namespace op
