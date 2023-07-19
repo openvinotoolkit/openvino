5b4f444b08b4ff4ce4bdd1957fb2f72f4569ad3a
Evgenya Stepyreva <evgenya.stepyreva@intel.com>
2023-07-19 11:47:52 +0400

    PR Comments adressed

--- src/common/transformations/src/transformations/common_optimizations/shared_ops_optimization.cpp
+++ src/common/transformations/src/transformations/common_optimizations/shared_ops_optimization.cpp
@@ -18,23 +18,22 @@ using namespace std;
 using namespace ov;
 using namespace ov::op;
 
-bool shared_node_optimization(
-    const std::shared_ptr<ov::Model>& model,
-    const std::unordered_map<ov::Node::type_info_t, bool (*)(const Node*, const Node*)>& rules) {
+bool shared_node_optimization(const shared_ptr<Model>& model,
+                              const unordered_map<Node::type_info_t, bool (*)(const Node*, const Node*)>& rules) {
     bool rewritten = false;
 
     for (const auto& op : model->get_ordered_ops()) {
         // Recursively apply transformation for sub-graph based operations
-        if (auto sub_graph_node = std::dynamic_pointer_cast<ov::op::util::SubGraphOp>(op)) {
+        if (auto sub_graph_node = dynamic_pointer_cast<util::SubGraphOp>(op)) {
             if (auto sub_graph = sub_graph_node->get_function()) {
-                rewritten = shared_node_optimization(sub_graph, rules) || rewritten;
+                rewritten |= shared_node_optimization(sub_graph, rules);
             }
         }
         for (auto& output : op->outputs()) {
             const auto& target_inputs = output.get_target_inputs();
             if (target_inputs.size() <= 1)
                 continue;  // nothing to optimize
-            std::unordered_map<ov::Node::type_info_t, std::vector<ov::Node*>> type_to_node;
+            unordered_map<Node::type_info_t, vector<Node*>> type_to_node;
             for (const auto& input : target_inputs) {
                 auto node = input.get_node();
                 if (node && rules.count(node->get_type_info()))
@@ -49,7 +48,7 @@ bool shared_node_optimization(
                 const auto& root_op = shared_nodes[0];
                 for (const auto& child_op : shared_nodes) {
                     if (root_op->get_instance_id() != child_op->get_instance_id() && are_equal(root_op, child_op)) {
-                        rewritten = replace_output_update_name(child_op->output(0), root_op->output(0)) || rewritten;
+                        rewritten |= replace_output_update_name(child_op->output(0), root_op->output(0));
                     }
                 }
             }
@@ -66,44 +65,38 @@ bool inputs_from_same_source_or_equal_constants(const Node* lhs, const Node* rhs
         if (lhs->input_value(i) == rhs->input_value(i))
             continue;
         OPENVINO_SUPPRESS_DEPRECATED_START
-        auto lhs_constant = ov::get_constant_from_source(lhs->input_value(i));
-        auto rhs_constant = ov::get_constant_from_source(rhs->input_value(i));
+        auto lhs_constant = get_constant_from_source(lhs->input_value(i));
+        auto rhs_constant = get_constant_from_source(rhs->input_value(i));
         OPENVINO_SUPPRESS_DEPRECATED_END
         if (!lhs_constant || !rhs_constant)
             return false;
         if (lhs_constant->get_shape() != rhs_constant->get_shape() ||
-            ov::shape_size(lhs_constant->get_shape()) > 10)  // protection for not comparing huge constants
+            shape_size(lhs_constant->get_shape()) > 10)  // protection for not comparing huge constants
             return false;
         if (lhs_constant->get_element_type() != rhs_constant->get_element_type())
             return false;
-        if (lhs_constant->get_element_type().is_integral()) {
-            if (lhs_constant->cast_vector<int64_t>() != rhs_constant->cast_vector<int64_t>())
-                return false;
-        } else if (lhs_constant->get_element_type().is_real()) {
-            if (lhs_constant->cast_vector<double>() != rhs_constant->cast_vector<double>())
-                return false;
-        } else {
+        size_t bytes = shape_size(lhs_constant->get_shape()) * lhs_constant->get_element_type().size();
+        if (!memcmp(lhs_constant->get_data_ptr(), rhs_constant->get_data_ptr(), bytes))
             return false;
-        }
     }
     return true;
 }
 
 bool concats_are_equal(const Node* lhs, const Node* rhs) {
-    const auto* lhs_concat = as_type<const v0::Concat>(lhs);
+    const auto lhs_concat = as_type<const v0::Concat>(lhs);
     if (!lhs_concat)
         return false;
-    const auto* rhs_concat = as_type<const v0::Concat>(rhs);
+    const auto rhs_concat = as_type<const v0::Concat>(rhs);
     if (!rhs_concat)
         return false;
     return lhs_concat->get_axis() == rhs_concat->get_axis() && inputs_from_same_source_or_equal_constants(lhs, rhs);
 }
 
 bool gather_elements_are_equal(const Node* lhs, const Node* rhs) {
-    const auto* lhs_gather_elements = as_type<const v6::GatherElements>(lhs);
+    const auto lhs_gather_elements = as_type<const v6::GatherElements>(lhs);
     if (!lhs_gather_elements)
         return false;
-    const auto* rhs_gather_elements = as_type<const v6::GatherElements>(rhs);
+    const auto rhs_gather_elements = as_type<const v6::GatherElements>(rhs);
     if (!rhs_gather_elements)
         return false;
     return lhs_gather_elements->get_axis() == rhs_gather_elements->get_axis() &&
@@ -111,22 +104,22 @@ bool gather_elements_are_equal(const Node* lhs, const Node* rhs) {
 }
 
 bool reshapes_are_equal(const Node* lhs, const Node* rhs) {
-    const auto* lhs_reshape = as_type<const v1::Reshape>(lhs);
+    const auto lhs_reshape = as_type<const v1::Reshape>(lhs);
     if (!lhs_reshape)
         return false;
-    const auto* rhs_reshape = as_type<const v1::Reshape>(rhs);
+    const auto rhs_reshape = as_type<const v1::Reshape>(rhs);
     if (!rhs_reshape)
         return false;
     return lhs_reshape->get_special_zero() == rhs_reshape->get_special_zero() &&
            inputs_from_same_source_or_equal_constants(lhs, rhs);
 }
 
-bool ov::pass::SharedOpOptimization::run_on_model(const std::shared_ptr<ov::Model>& model) {
+bool pass::SharedOpOptimization::run_on_model(const shared_ptr<Model>& model) {
     RUN_ON_FUNCTION_SCOPE(SharedOpOptimization);
-#define RECORD(op, func) \
-    { op::get_type_info_static(), func }
+#define RECORD(operation, func) \
+    { operation::get_type_info_static(), func }
 
-    const std::unordered_map<ov::Node::type_info_t, bool (*)(const Node*, const Node*)> rules = {
+    const unordered_map<Node::type_info_t, bool (*)(const Node*, const Node*)> rules = {
         // no attributes
         RECORD(v8::Slice, inputs_from_same_source_or_equal_constants),
         RECORD(v0::Tile, inputs_from_same_source_or_equal_constants),
--- src/common/transformations/tests/common_optimizations/shared_ops_optimization.cpp
+++ src/common/transformations/tests/common_optimizations/shared_ops_optimization.cpp
@@ -10,6 +10,8 @@
 #include "openvino/op/parameter.hpp"
 #include "openvino/op/result.hpp"
 #include "openvino/op/slice.hpp"
+#include "openvino/op/tile.hpp"
+#include "openvino/op/transpose.hpp"
 
 using namespace ov;
 using namespace ov::op;
@@ -52,5 +54,182 @@ TEST_F(TransformationTestsF, SharedSlice) {
 
         auto result = std::make_shared<v0::Result>(concat);
         model_ref = std::make_shared<ov::Model>(ResultVector{result}, ParameterVector{data});
+    }
+}
+
+TEST_F(TransformationTestsF, SharedRecursively) {
+    auto make_slice = [](const Output<Node>& out,
+                         const int64_t& start,
+                         const int64_t& stop,
+                         const int64_t& step,
+                         const int64_t& axis) {
+        return std::make_shared<v8::Slice>(out,
+                                           v0::Constant::create(element::i64, Shape{1}, {start}),
+                                           v0::Constant::create(element::i64, Shape{1}, {stop}),
+                                           v0::Constant::create(element::i64, Shape{1}, {step}),
+                                           v0::Constant::create(element::i64, Shape{1}, {axis}));
+    };
+
+    auto make_tile = [](const Output<Node>& out, const std::vector<int64_t>& repeats) {
+        return std::make_shared<v0::Tile>(out, v0::Constant::create(element::i64, Shape{repeats.size()}, repeats));
+    };
+
+    auto make_transpose = [](const Output<Node>& out, const std::vector<int64_t>& order) {
+        return std::make_shared<v1::Transpose>(out, v0::Constant::create(element::i64, Shape{order.size()}, order));
+    };
+
+    {
+        auto data = std::make_shared<v0::Parameter>(element::f32, PartialShape{-1, -1, -1, -1});
+
+        auto slice_0 = make_slice(data, 1, 2, 3, 3);
+        auto slice_1 = make_slice(data, 1, 2, 3, 3);
+        auto slice_2 = make_slice(data, 1, 3, 3, 3);
+
+        auto tile_0_0 = make_tile(slice_0, {1, 2, 3, 4});
+        auto transpose_0_0 = make_transpose(slice_0, {0, 2, 3, 1});
+        auto tile_0_1 = make_tile(slice_0, {1, 2, 3, 4});
+        auto transpose_0_1 = make_transpose(slice_0, {0, 2, 3, 1});
+        auto tile_0_2 = make_tile(slice_0, {1, 2, 3, 4});
+        auto transpose_0_2 = make_transpose(slice_0, {0, 2, 3, 1});
+
+        auto tile_1_0 = make_tile(slice_1, {1, 2, 3, 4});
+        auto transpose_1_0 = make_transpose(slice_1, {0, 2, 3, 1});
+        auto tile_1_1 = make_tile(slice_1, {1, 2, 3, 4});
+        auto transpose_1_1 = make_transpose(slice_1, {0, 2, 3, 1});
+        auto tile_1_2 = make_tile(slice_1, {1, 2, 3, 4});
+        auto transpose_1_2 = make_transpose(slice_1, {0, 2, 3, 1});
+
+        auto tile_2_0 = make_tile(slice_2, {1, 2, 3, 4});
+        auto transpose_2_0 = make_transpose(slice_2, {0, 2, 3, 1});
+        auto tile_2_1 = make_tile(slice_2, {1, 2, 3, 4});
+        auto transpose_2_1 = make_transpose(slice_2, {0, 2, 3, 1});
+        auto tile_2_2 = make_tile(slice_2, {1, 2, 3, 4});
+        auto transpose_2_2 = make_transpose(slice_2, {0, 2, 3, 1});
+
+        auto concat = std::make_shared<v0::Concat>(
+            OutputVector{// source from slice 0
+                         tile_0_0,
+                         transpose_0_0,
+                         tile_0_1,
+                         transpose_0_1,
+                         tile_0_2,
+                         transpose_0_2,
+                         // source from slice 1
+                         tile_1_0,
+                         transpose_1_0,
+                         tile_1_1,
+                         transpose_1_1,
+                         tile_1_2,
+                         transpose_1_2,
+                         // source from slice 2
+                         tile_2_0,
+                         transpose_2_0,
+                         tile_2_1,
+                         transpose_2_1,
+                         tile_2_2,
+                         transpose_2_2},
+            0);
+
+        auto result = std::make_shared<v0::Result>(concat);
+        model = std::make_shared<ov::Model>(ResultVector{result}, ParameterVector{data});
+        manager.register_pass<ov::pass::SharedOpOptimization>();
+    }
+    {
+        auto data = std::make_shared<v0::Parameter>(element::f32, PartialShape{-1, -1, -1, -1});
+
+        auto slice_0 = make_slice(data, 1, 2, 3, 3);
+        auto slice_2 = make_slice(data, 1, 3, 3, 3);
+
+        auto tile_0_0 = make_tile(slice_0, {1, 2, 3, 4});
+        auto transpose_0_0 = make_transpose(slice_0, {0, 2, 3, 1});
+
+        auto tile_2_0 = make_tile(slice_2, {1, 2, 3, 4});
+        auto transpose_2_0 = make_transpose(slice_2, {0, 2, 3, 1});
+
+        auto concat = std::make_shared<v0::Concat>(
+            OutputVector{// source from slice 0
+                         tile_0_0,
+                         transpose_0_0,
+                         tile_0_0,
+                         transpose_0_0,
+                         tile_0_0,
+                         transpose_0_0,
+                         // source from slice 0
+                         tile_0_0,
+                         transpose_0_0,
+                         tile_0_0,
+                         transpose_0_0,
+                         tile_0_0,
+                         transpose_0_0,
+                         // source from slice 2
+                         tile_2_0,
+                         transpose_2_0,
+                         tile_2_0,
+                         transpose_2_0,
+                         tile_2_0,
+                         transpose_2_0},
+            0);
+
+        auto result = std::make_shared<v0::Result>(concat);
+        model_ref = std::make_shared<ov::Model>(ResultVector{result}, ParameterVector{data});
+    }
+}
+
+TEST_F(TransformationTestsF, SharedConcat) {
+    {
+        auto pre_constant_0 = v0::Constant::create(element::f32, Shape{4}, std::vector<float>{3.14, 42, 0, 14});
+        auto pre_constant_1 = v0::Constant::create(element::f32, Shape{4}, std::vector<float>{3.14, 42, 0, 14});
+        auto data = std::make_shared<v0::Parameter>(element::f32, PartialShape{-1});
+        auto post_constant = v0::Constant::create(element::f32, Shape{1}, std::vector<float>{3.14});
+
+        auto concat_0 = std::make_shared<v0::Concat>(OutputVector{pre_constant_0, data, post_constant}, 0);
+        auto concat_1 = std::make_shared<v0::Concat>(OutputVector{pre_constant_1, data, post_constant}, 0);
+
+        auto concat = std::make_shared<v0::Concat>(OutputVector{concat_0, concat_1}, 0);
+        auto result = std::make_shared<v0::Result>(concat);
+        model = std::make_shared<ov::Model>(ResultVector{result}, ParameterVector{data});
+        manager.register_pass<ov::pass::SharedOpOptimization>();
+    }
+    {
+        auto pre_constant_0 = v0::Constant::create(element::f32, Shape{4}, std::vector<float>{3.14, 42, 0, 14});
+        auto data = std::make_shared<v0::Parameter>(element::f32, PartialShape{-1});
+        auto post_constant = v0::Constant::create(element::f32, Shape{1}, std::vector<float>{3.14});
+
+        auto concat_0 = std::make_shared<v0::Concat>(OutputVector{pre_constant_0, data, post_constant}, 0);
+
+        auto concat = std::make_shared<v0::Concat>(OutputVector{concat_0, concat_0}, 0);
+        auto result = std::make_shared<v0::Result>(concat);
+
+        model_ref = std::make_shared<ov::Model>(ResultVector{result}, ParameterVector{data});
+    }
+}
+
+TEST_F(TransformationTestsF, SharedConcatCheckOpWithResultIsntReplaced) {
+    {
+        auto pre_constant_0 = v0::Constant::create(element::f32, Shape{4}, std::vector<float>{3.14, 42, 0, 14});
+        auto pre_constant_1 = v0::Constant::create(element::f32, Shape{4}, std::vector<float>{3.14, 42, 0, 14});
+        auto data = std::make_shared<v0::Parameter>(element::f32, PartialShape{-1});
+        auto post_constant = v0::Constant::create(element::f32, Shape{1}, std::vector<float>{3.14});
+
+        auto concat_0 = std::make_shared<v0::Concat>(OutputVector{pre_constant_0, data, post_constant}, 0);
+        auto concat_1 = std::make_shared<v0::Concat>(OutputVector{pre_constant_1, data, post_constant}, 0);
+
+        auto result_0 = std::make_shared<v0::Result>(concat_0);
+        auto result_1 = std::make_shared<v0::Result>(concat_1);
+        model = std::make_shared<ov::Model>(ResultVector{result_0, result_1}, ParameterVector{data});
+        manager.register_pass<ov::pass::SharedOpOptimization>();
+    }
+    {
+        auto pre_constant_0 = v0::Constant::create(element::f32, Shape{4}, std::vector<float>{3.14, 42, 0, 14});
+        auto pre_constant_1 = v0::Constant::create(element::f32, Shape{4}, std::vector<float>{3.14, 42, 0, 14});
+        auto data = std::make_shared<v0::Parameter>(element::f32, PartialShape{-1});
+        auto post_constant = v0::Constant::create(element::f32, Shape{1}, std::vector<float>{3.14});
+
+        auto concat_0 = std::make_shared<v0::Concat>(OutputVector{pre_constant_0, data, post_constant}, 0);
+        auto concat_1 = std::make_shared<v0::Concat>(OutputVector{pre_constant_1, data, post_constant}, 0);
+
+        auto result_0 = std::make_shared<v0::Result>(concat_0);
+        auto result_1 = std::make_shared<v0::Result>(concat_1);
+        model_ref = std::make_shared<ov::Model>(ResultVector{result_0, result_1}, ParameterVector{data});
     }
 }
