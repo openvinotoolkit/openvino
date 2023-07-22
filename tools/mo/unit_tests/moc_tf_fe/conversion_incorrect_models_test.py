e69a9851703181439f234a0b4b8fee6761606b08
PRATHAM-SPS <ingawalepratham@gmail.com>
2023-07-22 11:46:18 +0530

    replaced generator functionality in conversion_incorrect_models_test.py

--- tools/mo/unit_tests/moc_tf_fe/conversion_incorrect_models_test.py
+++ tools/mo/unit_tests/moc_tf_fe/conversion_incorrect_models_test.py
@@ -4,101 +4,82 @@
 import tempfile
 import unittest
 import os
-from generator import generator, generate
 
 from openvino.tools.mo.convert import convert_model
 
-@generator
 class TestMoFreezePlaceholderTFFE(unittest.TestCase):
-    @generate(
-        *[
+    def test_conversion_fake_pb_model(self):
+        test_cases = [
             # the default frontend
-            (
-                    False, False, None
-            ),
-            (
-                    False, False, "tf"
-            ),
+            (False, False, None),
+            (False, False, "tf"),
             # new frontend
-            (
-                    True, False, None
-            ),
-            (
-                    True, False, "tf"
-            ),
-        ],
-    )
-    def test_conversion_fake_pb_model(self, use_new_frontend, use_legacy_frontend, framework):
-        with self.assertRaisesRegex(Exception,
-                                    "Internal error or inconsistent input model: the frontend supports frozen formats"
-                                    " \(.pb and .pbtxt\), SavedModel and MetaGraph \(.meta\), and v1 checkpoints."):
-            path = os.path.dirname(__file__)
-            input_model = os.path.join(path, "test_models", "fake.pb")
+            (True, False, None),
+            (True, False, "tf"),
+        ]
 
-            convert_model(input_model,
-                          use_new_frontend=use_new_frontend, use_legacy_frontend=use_legacy_frontend,
-                          framework=framework)
+        for idx, (use_new_frontend, use_legacy_frontend, framework) in enumerate(test_cases):
+            with self.subTest(idx=idx, use_new_frontend=use_new_frontend,
+                              use_legacy_frontend=use_legacy_frontend, framework=framework):
+                with self.assertRaisesRegex(Exception, "Internal error or inconsistent input model: the frontend supports frozen formats (.pb and .pbtxt), SavedModel and MetaGraph (.meta), and v1 checkpoints."):
+                    path = os.path.dirname(__file__)
+                    input_model = os.path.join(path, "test_models", "fake.pb")
 
-    @generate(
-        *[
+                    convert_model(input_model,
+                                  use_new_frontend=use_new_frontend, use_legacy_frontend=use_legacy_frontend,
+                                  framework=framework)
+
+    def test_conversion_empty_model(self):
+        test_cases = [
             # the default frontend
-            (
-                    False, False, None,
-                    r"Framework name can not be deduced from the given options"
-            ),
-            (
-                    False, False, "tf",
-                    r"Internal error or inconsistent input model: the frontend supports frozen formats"
-                    " \(.pb and .pbtxt\), SavedModel and MetaGraph \(.meta\), and v1 checkpoints."
-            ),
-            # new frontend
-            (
-                    True, False, None,
-                    r"Option \-\-use_new_frontend is specified but the Model Optimizer is unable to find new frontend"
-            ),
-            (
-                    True, False, "tf",
-                    r"Internal error or inconsistent input model: the frontend supports frozen formats"
-                    " \(.pb and .pbtxt\), SavedModel and MetaGraph \(.meta\), and v1 checkpoints."
-            ),
-        ],
-    )
-    def test_conversion_empty_model(self, use_new_frontend, use_legacy_frontend, framework, exp_reg_exp):
-        with tempfile.NamedTemporaryFile(mode='w') as tmp, self.assertRaisesRegex(Exception,
-                                                                                  exp_reg_exp):
-            tmp.write("")
-            convert_model(tmp.name,
-                          use_new_frontend=use_new_frontend, use_legacy_frontend=use_legacy_frontend,
-                          framework=framework)
+            (False, False, None,
+             r"Framework name can not be deduced from the given options"),
 
-    @generate(
-        *[
-            # the default frontend
-            (
-                    False, False, None,
-                    r"Framework name can not be deduced from the given options"
-            ),
-            (
-                    False, False, "tf",
-                    r"Internal error or inconsistent input model: the frontend supports frozen formats"
-                    " \(.pb and .pbtxt\), SavedModel and MetaGraph \(.meta\), and v1 checkpoints."
-            ),
+            (False, False, "tf",
+             r"Internal error or inconsistent input model: the frontend supports frozen formats (.pb and .pbtxt), SavedModel and MetaGraph (.meta), and v1 checkpoints."),
+
             # new frontend
-            (
-                    True, False, None,
-                    r"Option \-\-use_new_frontend is specified but the Model Optimizer is unable to find new frontend"
-            ),
-            (
-                    True, False, "tf",
-                    r"Internal error or inconsistent input model: the frontend supports frozen formats"
-                    " \(.pb and .pbtxt\), SavedModel and MetaGraph \(.meta\), and v1 checkpoints."
-            ),
-        ],
-    )
-    def test_conversion_fake_model_with_no_ext(self, use_new_frontend, use_legacy_frontend, framework, exp_reg_exp):
-        with tempfile.NamedTemporaryFile(mode='w') as tmp, self.assertRaisesRegex(Exception,
-                                                                                  exp_reg_exp):
-            tmp.write("1212234\n12312")
-            convert_model(tmp.name,
-                          use_new_frontend=use_new_frontend, use_legacy_frontend=use_legacy_frontend,
-                          framework=framework)
+            (True, False, None,
+             r"Option --use_new_frontend is specified but the Model Optimizer is unable to find new frontend"),
+
+            (True, False, "tf",
+             r"Internal error or inconsistent input model: the frontend supports frozen formats (.pb and .pbtxt), SavedModel and MetaGraph (.meta), and v1 checkpoints."),
+        ]
+
+        for idx, (use_new_frontend, use_legacy_frontend, framework, exp_reg_exp) in enumerate(test_cases):
+            with self.subTest(idx=idx, use_new_frontend=use_new_frontend,
+                              use_legacy_frontend=use_legacy_frontend, framework=framework,
+                              exp_reg_exp=exp_reg_exp):
+                with tempfile.NamedTemporaryFile(mode='w') as tmp:
+                    tmp.write("")
+                    tmp.flush()
+                    with self.assertRaisesRegex(Exception, exp_reg_exp):
+                        convert_model(tmp.name,
+                                      use_new_frontend=use_new_frontend, use_legacy_frontend=use_legacy_frontend,
+                                      framework=framework)
+
+    def test_conversion_fake_model_with_no_ext(self):
+        test_cases = [
+            # the default frontend
+            (False, False, None,
+             r"Framework name can not be deduced from the given options"),
+            (False, False, "tf",
+             r"Internal error or inconsistent input model: the frontend supports frozen formats (.pb and .pbtxt), SavedModel and MetaGraph (.meta), and v1 checkpoints."),
+            # new frontend
+            (True, False, None,
+             r"Option --use_new_frontend is specified but the Model Optimizer is unable to find new frontend"),
+            (True, False, "tf",
+             r"Internal error or inconsistent input model: the frontend supports frozen formats (.pb and .pbtxt), SavedModel and MetaGraph (.meta), and v1 checkpoints."),
+        ]
+
+        for idx, (use_new_frontend, use_legacy_frontend, framework, exp_reg_exp) in enumerate(test_cases):
+            with self.subTest(idx=idx, use_new_frontend=use_new_frontend,
+                              use_legacy_frontend=use_legacy_frontend, framework=framework,
+                              exp_reg_exp=exp_reg_exp):
+                with tempfile.NamedTemporaryFile(mode='w') as tmp:
+                    tmp.write("1212234\n12312")
+                    tmp.flush()
+                    with self.assertRaisesRegex(Exception, exp_reg_exp):
+                        convert_model(tmp.name,
+                                      use_new_frontend=use_new_frontend, use_legacy_frontend=use_legacy_frontend,
+                                      framework=framework)
