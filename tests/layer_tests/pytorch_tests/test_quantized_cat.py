00f50b9489fe60e7edb5e0ce1499b526b404fea6
Mateusz <mateusz.mikolajczyk@intel.com>
2023-07-24 12:52:09 +0000

    Format

--- tests/layer_tests/pytorch_tests/test_quantized_cat.py
+++ tests/layer_tests/pytorch_tests/test_quantized_cat.py
@@ -1,10 +1,11 @@
 # Copyright (C) 2018-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+import numpy as np
 import pytest
 import torch
-
 from pytorch_layer_test_class import PytorchLayerTest
+
 
 class aten_quantized_cat(torch.nn.Module):
     def __init__(self, scale, zero_point):
@@ -13,7 +14,7 @@ class aten_quantized_cat(torch.nn.Module):
         self.zero_point = int(zero_point)
 
     def forward(self, x):
-        x = torch.quantize_per_tensor(x, 1., 0, torch.quint8)
+        x = torch.quantize_per_tensor(x, 1.0, 0, torch.quint8)
         return torch.dequantize(torch.ops.quantized.cat([x, x], 1, self.scale, self.zero_point))
 
 
@@ -22,8 +23,9 @@ class aten_append_quantized_cat(torch.nn.Module):
         super().__init__()
         self.scale = float(scale)
         self.zero_point = int(zero_point)
+
     def forward(self, x):
-        x = torch.quantize_per_tensor(x, 1., 0, torch.quint8)
+        x = torch.quantize_per_tensor(x, 1.0, 0, torch.quint8)
         list = []
         list.append(x)
         list.append(x)
@@ -35,8 +37,9 @@ class aten_loop_append_quantized_cat(torch.nn.Module):
         super().__init__()
         self.scale = float(scale)
         self.zero_point = int(zero_point)
+
     def forward(self, x):
-        x = torch.quantize_per_tensor(x, 1., 0, torch.quint8)
+        x = torch.quantize_per_tensor(x, 1.0, 0, torch.quint8)
         list = []
         for i in range(3):
             list.append(x)
@@ -48,8 +51,9 @@ class aten_add_quantized_cat(torch.nn.Module):
         super().__init__()
         self.scale = float(scale)
         self.zero_point = int(zero_point)
+
     def forward(self, x):
-        x = torch.quantize_per_tensor(x, 1., 0, torch.quint8)
+        x = torch.quantize_per_tensor(x, 1.0, 0, torch.quint8)
         list = [x, x]
         list2 = list + [x, x]
         return torch.dequantize(torch.ops.quantized.cat(list2, 1, self.scale, self.zero_point))
@@ -57,7 +61,6 @@ class aten_add_quantized_cat(torch.nn.Module):
 
 class TestQuantizedCat(PytorchLayerTest):
     def _prepare_input(self):
-        import numpy as np
         return (np.random.randn(2, 1, 3).astype(np.float32),)
 
     @pytest.mark.parametrize("scale", [1, 0.3, 1.3])
@@ -65,30 +68,69 @@ class TestQuantizedCat(PytorchLayerTest):
     @pytest.mark.nightly
     @pytest.mark.precommit
     def test_quantized_cat(self, scale, zero_point, ie_device, precision, ir_version):
-        self._test(aten_quantized_cat(scale, zero_point), None, ["quantized::cat", "prim::ListConstruct"],
-                   ie_device, precision, ir_version, quantized_ops=True, freeze_model=False, quant_size=scale)
+        self._test(
+            aten_quantized_cat(scale, zero_point),
+            None,
+            ["quantized::cat", "prim::ListConstruct"],
+            ie_device,
+            precision,
+            ir_version,
+            quantized_ops=True,
+            freeze_model=False,
+            quant_size=scale,
+        )
 
     @pytest.mark.parametrize("scale", [1, 0.3, 1.3])
     @pytest.mark.parametrize("zero_point", [0, 1])
     @pytest.mark.nightly
     @pytest.mark.precommit
     def test_append_quantized_cat(self, scale, zero_point, ie_device, precision, ir_version):
-        self._test(aten_append_quantized_cat(scale, zero_point), None, ["quantized::cat", "aten::append", "prim::ListConstruct"],
-                   ie_device, precision, ir_version, quantized_ops=True, freeze_model=False, quant_size=scale)
+        self._test(
+            aten_append_quantized_cat(scale, zero_point),
+            None,
+            ["quantized::cat", "aten::append", "prim::ListConstruct"],
+            ie_device,
+            precision,
+            ir_version,
+            quantized_ops=True,
+            freeze_model=False,
+            quant_size=scale,
+        )
 
     @pytest.mark.parametrize("scale", [1, 0.3, 1.3])
     @pytest.mark.parametrize("zero_point", [0, 1])
     @pytest.mark.nightly
     @pytest.mark.precommit
-    @pytest.mark.xfail(reason="Transformation RemoveMultiSubGraphOpDanglingParamsResults doesn't support removing unused merged inputs, ticket 112833.")
+    @pytest.mark.xfail(
+        reason="Transformation RemoveMultiSubGraphOpDanglingParamsResults doesn't support removing unused merged inputs, ticket 112833."
+    )
     def test_loop_append_quantized_cat(self, scale, zero_point, ie_device, precision, ir_version):
-        self._test(aten_loop_append_quantized_cat(scale, zero_point), None, ["quantized::cat", "aten::append", "prim::ListConstruct", "prim::Loop"],
-                   ie_device, precision, ir_version, quantized_ops=True, freeze_model=False, quant_size=scale)
+        self._test(
+            aten_loop_append_quantized_cat(scale, zero_point),
+            None,
+            ["quantized::cat", "aten::append", "prim::ListConstruct", "prim::Loop"],
+            ie_device,
+            precision,
+            ir_version,
+            quantized_ops=True,
+            freeze_model=False,
+            quant_size=scale,
+        )
 
     @pytest.mark.parametrize("scale", [1, 0.3, 1.3])
     @pytest.mark.parametrize("zero_point", [0, 1])
     @pytest.mark.nightly
     @pytest.mark.precommit
     def test_add_quantized_cat(self, scale, zero_point, ie_device, precision, ir_version):
-        self._test(aten_add_quantized_cat(scale, zero_point), None, ["quantized::cat", "aten::add", "prim::ListConstruct"],
-                   ie_device, precision, ir_version, trace_model=False, quantized_ops=True, freeze_model=False, quant_size=scale)
+        self._test(
+            aten_add_quantized_cat(scale, zero_point),
+            None,
+            ["quantized::cat", "aten::add", "prim::ListConstruct"],
+            ie_device,
+            precision,
+            ir_version,
+            trace_model=False,
+            quantized_ops=True,
+            freeze_model=False,
+            quant_size=scale,
+        )
