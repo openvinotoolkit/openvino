ceb74e0c2374a31c1334746da6552ee896702df8
Alexander Nesterov <nesterov.alexander@outlook.com>
2023-07-24 15:04:15 +0200

    Revert "[ARM CPU] ACL TBB scheduler (#17445)"
    This reverts commit 510f578aab5f6aebb7891bdfb4440e993db9455a.

--- src/plugins/intel_cpu/src/config.cpp
+++ src/plugins/intel_cpu/src/config.cpp
@@ -256,6 +256,12 @@ void Config::readProperties(const std::map<std::string, std::string> &prop) {
         streamExecutorConfig._streams_changed = true;
     }
 
+#if defined(OPENVINO_ARCH_ARM) || defined(OPENVINO_ARCH_ARM64)
+    // TODO: multi-stream execution has functional issues on ARM target
+    streamExecutorConfig._streams = 1;
+    streamExecutorConfig._streams_changed = true;
+#endif
+
     CPU_DEBUG_CAP_ENABLE(applyDebugCapsProperties());
     updateProperties();
 }
