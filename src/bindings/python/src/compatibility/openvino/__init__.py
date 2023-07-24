93abd04224b0c11209b73be4c7be0b189ff9bf4d
jiwaszki <jan.iwaszkiewicz@intel.com>
2023-07-24 09:16:04 +0000

    Fix package imports

--- src/bindings/python/src/compatibility/openvino/__init__.py
+++ src/bindings/python/src/compatibility/openvino/__init__.py
@@ -2,6 +2,8 @@
 # Copyright (C) 2018-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
 try:
     # Import all modules
     from openvino import runtime as runtime
