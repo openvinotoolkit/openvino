a1ab897d8bf66bb7701483ea0c4b6b7466117b3c
mitruska <katarzyna.mitrus@intel.com>
2023-07-24 19:01:32 +0200

    Use OV_EXPECT_THROW

--- src/core/tests/type_prop/detection_output.cpp
+++ src/core/tests/type_prop/detection_output.cpp
@@ -6,6 +6,7 @@
 
 #include <memory>
 
+#include "common_test_utils/test_assertions.hpp"
 #include "common_test_utils/type_prop.hpp"
 #include "gtest/gtest.h"
 #include "ngraph/ngraph.hpp"
@@ -1145,113 +1146,87 @@ TEST(type_prop_layers, detection_output_v8_default_ctor) {
 }
 
 TEST(type_prop_layers, detection_output_v8_incompatible_num_prior_boxes_normalized_true_shareloc_true) {
-    try {
-        op::DetectionOutputAttrs attrs;
-        attrs.keep_top_k = {-1};
-        attrs.normalized = true;      // If true, prior_box_size = 4, otherwise prior_box_size = 5
-        attrs.share_location = true;  // If true, num_loc_classes = 1, otherwise num_loc_classes = num_classes
-        auto op =
-            create_detection_output_v8(Shape{4, 6 * 1 * 4},  // [N, num_prior_boxes * num_loc_classes * 4]
-                                       Shape{4, 8 * 16},     // [N, num_prior_boxes * num_classes]
-                                       Shape{4, 2, 8 * 4},  // [priors_batch_size, 2, num_prior_boxes * prior_box_size]`
-                                       attrs,
-                                       element::f32);
-        FAIL() << "Exception expected";
-    } catch (const NodeValidationFailure& error) {
-        EXPECT_HAS_SUBSTRING(error.what(),
-                             std::string("The second dimension of the first input (box logits) is not compatible. "
-                                         "Current value: 24, expected value: 32"));
-    } catch (...) {
-        FAIL() << "Unknown exception was thrown";
-    }
+    op::DetectionOutputAttrs attrs;
+    attrs.keep_top_k = {-1};
+    attrs.normalized = true;      // If true, prior_box_size = 4, otherwise prior_box_size = 5
+    attrs.share_location = true;  // If true, num_loc_classes = 1, otherwise num_loc_classes = num_classes
+
+    OV_EXPECT_THROW(auto op = create_detection_output_v8(
+                        Shape{4, 6 * 1 * 4},  // [N, num_prior_boxes * num_loc_classes * 4]
+                        Shape{4, 8 * 16},     // [N, num_prior_boxes * num_classes]
+                        Shape{4, 2, 8 * 4},   // [priors_batch_size, 2, num_prior_boxes * prior_box_size]`
+                        attrs,
+                        element::f32),
+                    NodeValidationFailure,
+                    HasSubstr("The second dimension of the first input (box logits) is not compatible. Current value: "
+                              "24, expected value: 32"));
 }
 
 TEST(type_prop_layers, detection_output_v8_incompatible_num_prior_boxes_normalized_false_shareloc_true) {
-    try {
-        op::DetectionOutputAttrs attrs;
-        attrs.keep_top_k = {-1};
-        attrs.normalized = false;     // If true, prior_box_size = 4, otherwise prior_box_size = 5
-        attrs.share_location = true;  // If true, num_loc_classes = 1, otherwise num_loc_classes = num_classes
-        auto op =
-            create_detection_output_v8(Shape{4, 6 * 1 * 4},  // [N, num_prior_boxes * num_loc_classes * 4]
-                                       Shape{4, 8 * 16},     // [N, num_prior_boxes * num_classes]
-                                       Shape{4, 2, 8 * 5},  // [priors_batch_size, 2, num_prior_boxes * prior_box_size]`
-                                       attrs,
-                                       element::f32);
-        FAIL() << "Exception expected";
-    } catch (const NodeValidationFailure& error) {
-        EXPECT_HAS_SUBSTRING(error.what(),
-                             std::string("The second dimension of the first input (box logits) is not compatible. "
-                                         "Current value: 24, expected value: 32"));
-    } catch (...) {
-        FAIL() << "Unknown exception was thrown";
-    }
+    op::DetectionOutputAttrs attrs;
+    attrs.keep_top_k = {-1};
+    attrs.normalized = false;     // If true, prior_box_size = 4, otherwise prior_box_size = 5
+    attrs.share_location = true;  // If true, num_loc_classes = 1, otherwise num_loc_classes = num_classes
+
+    OV_EXPECT_THROW(auto op = create_detection_output_v8(
+                        Shape{4, 6 * 1 * 4},  // [N, num_prior_boxes * num_loc_classes * 4]
+                        Shape{4, 8 * 16},     // [N, num_prior_boxes * num_classes]
+                        Shape{4, 2, 8 * 5},   // [priors_batch_size, 2, num_prior_boxes * prior_box_size]`
+                        attrs,
+                        element::f32),
+                    NodeValidationFailure,
+                    HasSubstr("The second dimension of the first input (box logits) is not compatible. Current value: "
+                              "24, expected value: 32"));
 }
 
 TEST(type_prop_layers, detection_output_v8_incompatible_num_prior_boxes_normalized_false_shareloc_false) {
-    try {
-        op::DetectionOutputAttrs attrs;
-        attrs.keep_top_k = {-1};
-        attrs.normalized = false;      // If true, prior_box_size = 4, otherwise prior_box_size = 5
-        attrs.share_location = false;  // If true, num_loc_classes = 1, otherwise num_loc_classes = num_classes
-        auto op =
-            create_detection_output_v8(Shape{4, 6 * 16 * 4},  // [N, num_prior_boxes * num_loc_classes * 4]
-                                       Shape{4, 8 * 16},      // [N, num_prior_boxes * num_classes]
-                                       Shape{4, 2, 8 * 5},  // [priors_batch_size, 2, num_prior_boxes * prior_box_size]`
-                                       attrs,
-                                       element::f32);
-        FAIL() << "Exception expected";
-    } catch (const NodeValidationFailure& error) {
-        EXPECT_HAS_SUBSTRING(error.what(),
-                             std::string("The second dimension of the first input (box logits) is not compatible. "
-                                         "Current value: 384, expected value: 512"));
-    } catch (...) {
-        FAIL() << "Unknown exception was thrown";
-    }
+    op::DetectionOutputAttrs attrs;
+    attrs.keep_top_k = {-1};
+    attrs.normalized = false;      // If true, prior_box_size = 4, otherwise prior_box_size = 5
+    attrs.share_location = false;  // If true, num_loc_classes = 1, otherwise num_loc_classes = num_classes
+
+    OV_EXPECT_THROW(auto op = create_detection_output_v8(
+                        Shape{4, 6 * 16 * 4},  // [N, num_prior_boxes * num_loc_classes * 4]
+                        Shape{4, 8 * 16},      // [N, num_prior_boxes * num_classes]
+                        Shape{4, 2, 8 * 5},    // [priors_batch_size, 2, num_prior_boxes * prior_box_size]`
+                        attrs,
+                        element::f32),
+                    NodeValidationFailure,
+                    HasSubstr("The second dimension of the first input (box logits) is not compatible. Current value: "
+                              "384, expected value: 512"));
 }
 
 TEST(type_prop_layers, detection_output_v8_incompatible_num_prior_boxes_normalized_true_shareloc_false) {
-    try {
-        op::DetectionOutputAttrs attrs;
-        attrs.keep_top_k = {-1};
-        attrs.normalized = true;       // If true, prior_box_size = 4, otherwise prior_box_size = 5
-        attrs.share_location = false;  // If true, num_loc_classes = 1, otherwise num_loc_classes = num_classes
-        auto op =
-            create_detection_output_v8(Shape{4, 6 * 16 * 4},  // [N, num_prior_boxes * num_loc_classes * 4]
-                                       Shape{4, 8 * 16},      // [N, num_prior_boxes * num_classes]
-                                       Shape{4, 2, 8 * 4},  // [priors_batch_size, 2, num_prior_boxes * prior_box_size]`
-                                       attrs,
-                                       element::f32);
-        FAIL() << "Exception expected";
-    } catch (const NodeValidationFailure& error) {
-        EXPECT_HAS_SUBSTRING(error.what(),
-                             std::string("The second dimension of the first input (box logits) is not compatible. "
-                                         "Current value: 384, expected value: 512"));
-    } catch (...) {
-        FAIL() << "Unknown exception was thrown";
-    }
+    op::DetectionOutputAttrs attrs;
+    attrs.keep_top_k = {-1};
+    attrs.normalized = true;       // If true, prior_box_size = 4, otherwise prior_box_size = 5
+    attrs.share_location = false;  // If true, num_loc_classes = 1, otherwise num_loc_classes = num_classes
+    OV_EXPECT_THROW(auto op = create_detection_output_v8(
+                        Shape{4, 6 * 16 * 4},  // [N, num_prior_boxes * num_loc_classes * 4]
+                        Shape{4, 8 * 16},      // [N, num_prior_boxes * num_classes]
+                        Shape{4, 2, 8 * 4},    // [priors_batch_size, 2, num_prior_boxes * prior_box_size]`
+                        attrs,
+                        element::f32),
+                    NodeValidationFailure,
+                    HasSubstr("The second dimension of the first input (box logits) is not compatible. Current value: "
+                              "384, expected value: 512"));
 }
 
 TEST(type_prop_layers, detection_output_v8_incompatible_dynamic_num_prior_boxes_normalized_true_shareloc_true) {
-    try {
-        op::DetectionOutputAttrs attrs;
-        attrs.keep_top_k = {-1};
-        attrs.normalized = true;      // If true, prior_box_size = 4, otherwise prior_box_size = 5
-        attrs.share_location = true;  // If true, num_loc_classes = 1, otherwise num_loc_classes = num_classes
-        auto op = create_detection_output_v8(
-            PartialShape{4, {16, 24}},  // [N, num_prior_boxes * num_loc_classes * 4]
-            PartialShape{4, 8 * 16},    // [N, num_prior_boxes * num_classes]
-            PartialShape{4, 2, 8 * 4},  // [priors_batch_size, 2, num_prior_boxes * prior_box_size]`
-            attrs,
-            element::f32);
-        FAIL() << "Exception expected";
-    } catch (const NodeValidationFailure& error) {
-        EXPECT_HAS_SUBSTRING(error.what(),
-                             std::string("The second dimension of the first input (box logits) is not compatible. "
-                                         "Current value: 16..24, expected value: 32"));
-    } catch (...) {
-        FAIL() << "Unknown exception was thrown";
-    }
+    op::DetectionOutputAttrs attrs;
+    attrs.keep_top_k = {-1};
+    attrs.normalized = true;      // If true, prior_box_size = 4, otherwise prior_box_size = 5
+    attrs.share_location = true;  // If true, num_loc_classes = 1, otherwise num_loc_classes = num_classes
+
+    OV_EXPECT_THROW(auto op = create_detection_output_v8(
+                        PartialShape{4, {16, 24}},  // [N, num_prior_boxes * num_loc_classes * 4]
+                        PartialShape{4, 8 * 16},    // [N, num_prior_boxes * num_classes]
+                        PartialShape{4, 2, 8 * 4},  // [priors_batch_size, 2, num_prior_boxes * prior_box_size]`
+                        attrs,
+                        element::f32),
+                    NodeValidationFailure,
+                    HasSubstr("The second dimension of the first input (box logits) is not compatible. Current value: "
+                              "16..24, expected value: 32"));
 }
 
 TEST(type_prop_layers, detection_output_v8_dynamic_range_num_prior_boxes_normalized_true_shareloc_true) {
