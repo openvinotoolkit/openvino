00469a2f5d83a9e2239331aad0db585e6b5af371
Raasz, Pawel <pawel.raasz@intel.com>
2023-07-25 10:25:27 +0000

    Remove commented test code

--- src/plugins/intel_cpu/tests/unit/shape_inference_test/region_yolo_shape_inference_test.cpp
+++ src/plugins/intel_cpu/tests/unit/shape_inference_test/region_yolo_shape_inference_test.cpp
@@ -58,38 +58,3 @@ TEST_F(StaticShapeRegionYoloTest, data_shape_not_compatible_rank_4) {
                     NodeValidationFailure,
                     HasSubstr("Input must be a tensor of rank 4, but got"));
 }
-
-// TEST_F(StaticShapeRegionYoloTest, h_dim_not_div_by_stride) {
-//     const auto data = std::make_shared<op::v0::Parameter>(element::f32, PartialShape::dynamic());
-//     op = make_op(data, 2);
-
-//     OV_EXPECT_THROW(shape_inference(op.get(), ShapeVector{{2, 20, 11, 24}}, output_shapes),
-//                     NodeValidationFailure,
-//                     HasSubstr("H and W should be divisible by stride"));
-// }
-
-// #include <region_yolo_shape_inference.hpp>
-
-// #include "utils.hpp"
-
-// using namespace ov;
-// using namespace ov::intel_cpu;
-// using namespace std;
-
-// TEST(StaticShapeInferenceTest, RegionYoloV0) {
-//     auto inputs = make_shared<op::v0::Parameter>(element::f32, ov::PartialShape{-1, -1, -1, -1});
-//     auto op = make_shared<op::v0::RegionYolo>(inputs, 0, 0, 0, true, std::vector<int64_t>{}, 0, 1);
-
-//     check_static_shape(op.get(), {StaticShape{1, 125, 13, 13}}, {StaticShape{1 * 125, 13, 13}});
-// }
-
-// TEST(StaticShapeInferenceTest, RegionYoloV0Dynamic) {
-//     auto inputs = make_shared<op::v0::Parameter>(element::f32,
-//                                                  ov::PartialShape{{1, 11}, {2, 12}, ov::Dimension::dynamic(), {4,
-//                                                  14}});
-//     auto op = make_shared<op::v0::RegionYolo>(inputs, 4, 80, 5, true, std::vector<int64_t>{}, 1, 3);
-
-//     EXPECT_EQ(op->get_output_partial_shape(0), ov::PartialShape({{1, 11}, ov::Dimension::dynamic()}));
-
-//     check_static_shape(op.get(), {StaticShape{10, 125, 13, 13}}, {StaticShape{10, 125 * 13 * 13}});
-// }
