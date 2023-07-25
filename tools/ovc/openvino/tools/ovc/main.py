9e8e77194187859f83026a5bcf7c0f4e6b22c875
Pavel Esir <pavel.esir@intel.com>
2023-07-25 15:26:40 +0200

    revert back serialize and compress_model but into main instead of moc_emit_ir

--- tools/ovc/openvino/tools/ovc/main.py
+++ tools/ovc/openvino/tools/ovc/main.py
@@ -12,7 +12,7 @@ from openvino.tools.ovc.convert_impl import _convert
 from openvino.tools.ovc.utils import get_ir_version
 
 # pylint: disable=no-name-in-module,import-error
-from openvino.runtime import save_model
+from openvino.runtime import serialize
 
 
 def main():
@@ -25,7 +25,12 @@ def main():
     model_path_no_ext = os.path.normpath(os.path.join(output_dir, argv.output_model))
     model_path = model_path_no_ext + '.xml'
 
-    save_model(ngraph_function, model_path.encode('utf-8'), argv.compress_to_fp16)
+    # TODO: replace compress_model + serialize with save_model
+    if argv.compress_to_fp16:
+        from openvino.tools.ovc.moc_frontend.offline_transformations import compress_model
+        compress_model(ngraph_function)
+
+    serialize(ngraph_function, model_path.encode('utf-8'), model_path.replace('.xml', '.bin').encode('utf-8'))
 
     print('[ SUCCESS ] XML file: {}'.format(model_path))
     print('[ SUCCESS ] BIN file: {}'.format(model_path.replace('.xml', '.bin')))
