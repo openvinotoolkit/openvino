ceda3cc264101ec4c5091d742736fd61657db6dc
Pavel Esir <pavel.esir@intel.com>
2023-07-25 13:09:58 +0200

    user save_model in ovc cli tool

--- tools/ovc/openvino/tools/ovc/main.py
+++ tools/ovc/openvino/tools/ovc/main.py
@@ -12,7 +12,7 @@ from openvino.tools.ovc.convert_impl import _convert
 from openvino.tools.ovc.utils import get_ir_version
 
 # pylint: disable=no-name-in-module,import-error
-from openvino.runtime import serialize
+from openvino.runtime import save_model
 
 
 def main():
@@ -25,7 +25,7 @@ def main():
     model_path_no_ext = os.path.normpath(os.path.join(output_dir, argv.output_model))
     model_path = model_path_no_ext + '.xml'
 
-    serialize(ngraph_function, model_path.encode('utf-8'), model_path.replace('.xml', '.bin').encode('utf-8'))
+    save_model(ngraph_function, model_path.encode('utf-8'), argv.compress_to_fp16)
 
     print('[ SUCCESS ] XML file: {}'.format(model_path))
     print('[ SUCCESS ] BIN file: {}'.format(model_path.replace('.xml', '.bin')))
