c0dbc5b304c3b84a0791ecdf8ca442dc765f9ee8
Zhai, Xuejun <xuejun.zhai@intel.com>
2023-07-26 10:58:52 +0800

    [INTEL CPU] disable compareAutoBatchingToSingleBatch test with batchsize(4) & req(64) for ARM32
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/skip_tests_config.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/skip_tests_config.cpp
@@ -205,6 +205,10 @@ std::vector<std::string> disabledTestPatterns() {
 #if defined(OPENVINO_ARCH_ARM)
     // TODO: rounding errors
     retVector.emplace_back(R"(.*iv_secondaryInputType=PARAMETER_opType=VECTOR_NetType=i32.*)");
+    retVector.emplace_back(R"(.*compareAutoBatchingToSingleBatch/CPU_get_blob_batch_size_4_num_streams_1_num_req_64*)");
+    retVector.emplace_back(R"(.*compareAutoBatchingToSingleBatch/CPU_get_blob_batch_size_4_num_streams_2_num_req_64*)");
+    retVector.emplace_back(R"(.*compareAutoBatchingToSingleBatch/CPU_set_blob_batch_size_4_num_streams_1_num_req_64*)");
+    retVector.emplace_back(R"(.*compareAutoBatchingToSingleBatch/CPU_set_blob_batch_size_4_num_streams_2_num_req_64*)");
 #endif
 
 #if !defined(OPENVINO_ARCH_X86_64)
