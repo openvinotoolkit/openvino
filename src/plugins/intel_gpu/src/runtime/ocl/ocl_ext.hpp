b7f41dc26fed3c6fe46c671aa4a54a5c3f44e48e
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2023-07-27 19:12:55 +0400

    build fix

--- src/plugins/intel_gpu/src/runtime/ocl/ocl_ext.hpp
+++ src/plugins/intel_gpu/src/runtime/ocl/ocl_ext.hpp
@@ -223,10 +223,12 @@ clEnqueueMemFillINTEL_fn)(
 
 // for C++ wrappers
 using uuid_array = std::array<cl_uchar, CL_UUID_SIZE_KHR>;
+using luid_array = std::array<cl_uchar, CL_LUID_SIZE_KHR>;
 
 namespace cl {
 namespace detail {
 CL_HPP_DECLARE_PARAM_TRAITS_(cl_device_info, CL_DEVICE_UUID_KHR, uuid_array)
+CL_HPP_DECLARE_PARAM_TRAITS_(cl_device_info, CL_DEVICE_LUID_KHR, luid_array)
 }  // namespace detail
 }  // namespace cl
 
