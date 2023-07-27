729f779d97326c759849799a8030f1cf9860c16c
jiwaszki <jan.iwaszkiewicz@intel.com>
2023-07-27 12:42:32 +0000

    Remove MO from tools inits

--- tools/mo/openvino/tools/__init__.py
+++ tools/mo/openvino/tools/__init__.py
@@ -2,9 +2,3 @@
 # SPDX-License-Identifier: Apache-2.0
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
-
-try:
-    import openvino.tools.mo
-except ImportError:
-    import warnings
-    warnings.warn("openvino.tools.mo module could not be found!", ImportWarning, stacklevel=2)
