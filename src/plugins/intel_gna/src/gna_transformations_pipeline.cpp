b20478234361c5c0f838be06f7e42b9016b84cf5
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2023-07-28 19:56:10 +0200

    node_id crash fix

--- src/plugins/intel_gna/src/gna_transformations_pipeline.cpp
+++ src/plugins/intel_gna/src/gna_transformations_pipeline.cpp
@@ -104,7 +104,6 @@ void TransformationsPipeline::apply(const std::shared_ptr<ov::Model>& model,
     manager.register_pass<ov::pass::InitNodeInfo>();
     // In OV API 2.0(IRv10) default convertion to fp32 (inputs, outputs and weights) is disabled
     // and we need to run the ConvertPrecision transformation to support old networks.
-    manager.register_pass<ov::pass::Serialize>("model.xml", "model.bin");
     manager.register_pass<ov::pass::ConvertPrecision>(precisions_map{{ngraph::element::f16, ngraph::element::f32}});
     manager.register_pass<ov::pass::ConvertMVN1ToMVN6>();
     manager.register_pass<ov::intel_gna::pass::DecomposeMVN>();
