4d07d5262b3bc5d7dd9782f09bc2f177690a11b0
jiwaszki <jan.iwaszkiewicz@intel.com>
2023-07-31 13:18:31 +0000

    [PyOV] Add Range-4 as part of opset12

--- src/bindings/python/src/openvino/runtime/opset12/ops.py
+++ src/bindings/python/src/openvino/runtime/opset12/ops.py
@@ -15,6 +15,7 @@ from openvino.runtime.utils.types import (
     as_node,
 )
 
+_get_node_factory_opset4 = partial(_get_node_factory, "opset4")
 _get_node_factory_opset12 = partial(_get_node_factory, "opset12")
 
 
@@ -116,5 +117,31 @@ def group_normalization(
         {
             "num_groups": num_groups,
             "epsilon": epsilon,
+        },
+    )
+
+
+@nameable_op
+def range(
+    start: Node,
+    stop: NodeInput,
+    step: NodeInput,
+    output_type: str,
+    name: Optional[str] = None,
+) -> Node:
+    """Return a node which produces the Range operation.
+
+    :param start:       The start value of the generated range.
+    :param stop:        The stop value of the generated range.
+    :param step:        The step value for the generated range.
+    :param output_type: The output tensor type.
+    :param name:        Optional name for output node.
+    :return: Range node
+    """
+    return _get_node_factory_opset4().create(
+        "Range",
+        as_nodes(start, stop, step),
+        {
+            "output_type": output_type,
         },
     )
