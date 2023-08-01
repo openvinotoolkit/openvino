9b8a3e8682b296b3c54a047b5e8b4b27b4d9cb95
Oleg Pipikin <oleg.pipikin@intel.com>
2023-08-01 21:26:17 +0400

    Refactor engines utils. Delete the target

--- src/frontends/onnx/tests/onnx_utils.hpp
+++ src/frontends/onnx/tests/onnx_utils.hpp
@@ -6,4 +6,21 @@
 
 #include <string>
 
+#include "openvino/runtime/core.hpp"
+
 static const std::string ONNX_FE = "onnx";
+
+inline std::string backend_name_to_device(const std::string& backend_name) {
+    if (backend_name == "INTERPRETER")
+        return "TEMPLATE";
+    if (backend_name == "IE_CPU")
+        return "CPU";
+    if (backend_name == "IE_GPU")
+        return "GPU";
+    OPENVINO_THROW("Unsupported backend name");
+}
+
+inline std::shared_ptr<ov::Model> function_from_ir(const std::string& xml_path, const std::string& bin_path = {}) {
+    ov::Core c;
+    return c.read_model(xml_path, bin_path);
+}
