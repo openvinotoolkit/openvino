ecb71e16f4d8749b1bb483ef2af5c042bbad63bd
Mateusz <mateusz.mikolajczyk@intel.com>
2023-08-04 10:22:33 +0000

    Add linspace tests

--- tests/layer_tests/pytorch_tests/test_linspace.py
+++ tests/layer_tests/pytorch_tests/test_linspace.py
@@ -1,41 +1,20 @@
 # Copyright (C) 2018-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+import numpy as np
 import pytest
-
+import torch
 from pytorch_layer_test_class import PytorchLayerTest
 
 
 class TestLinspace(PytorchLayerTest):
-    def _prepare_input(self, end, start=None, steps=None, dtype="int64", ref_dtype=None):
-        import numpy as np
+    def _prepare_input(self, start, end, steps, dtype=None, ref_dtype=None):
+        inputs = [np.array(start).astype(dtype), np.array(end).astype(dtype), np.array(steps).astype("int32")]
+        if ref_dtype:
+            inputs.append(np.zeros(1).astype(ref_dtype))
+        return inputs
 
-        if start is None and steps is None:
-            return (
-                (np.array(end).astype(dtype),)
-                if not ref_dtype
-                else (np.array(end).astype(dtype), np.zeros(1).astype(ref_dtype))
-            )
-        if steps is None:
-            return (
-                (np.array(start).astype(dtype), np.array(end).astype(dtype))
-                if not ref_dtype
-                else (np.array(start).astype(dtype), np.array(end).astype(dtype), np.zeros(1).astype(ref_dtype))
-            )
-        return (
-            (np.array(start).astype(dtype), np.array(end).astype(dtype), np.array(steps).astype(dtype))
-            if not ref_dtype
-            else (
-                np.array(start).astype(dtype),
-                np.array(end).astype(dtype),
-                np.array(steps).astype("int32"),
-                np.zeros(1).astype(ref_dtype),
-            )
-        )
-
-    def create_model(self, dtype=None, num_inputs=1, use_out=False, ref_dtype=False):
-        import torch
-
+    def create_model(self, dtype=None, use_out=False, ref_dtype=False):
         dtype_map = {
             "float32": torch.float32,
             "float64": torch.float64,
@@ -45,41 +24,34 @@ class TestLinspace(PytorchLayerTest):
             "int8": torch.int8,
         }
 
-        class aten_linspace_start_end_steps_dtype(torch.nn.Module):
+        class aten_linspace_dtype(torch.nn.Module):
             def __init__(self, dtype) -> None:
-                super(aten_linspace_start_end_steps_dtype, self).__init__()
+                super().__init__()
                 self.dtype = dtype
 
-            def forward(self, x, y, z):
-                return torch.linspace(start=x, end=y, steps=z, dtype=self.dtype)
+            def forward(self, start, end, steps):
+                return torch.linspace(start=start, end=end, steps=steps, dtype=self.dtype)
 
-        class aten_linspace_start_end_steps_out(torch.nn.Module):
+        class aten_linspace_out(torch.nn.Module):
             def __init__(self, out) -> None:
-                super(aten_linspace_start_end_steps_out, self).__init__()
+                super().__init__()
                 # Size of empty tensor needs to be of equal or larger size than linspace steps
                 self.out = torch.empty(25, dtype=out)
 
-            def forward(self, x, y, z):
-                return torch.linspace(start=x, end=y, steps=z, out=self.out)
+            def forward(self, start, end, steps):
+                return torch.linspace(start=start, end=end, steps=steps, out=self.out)
 
-        class aten_linspace_start_end_steps_prim_dtype(torch.nn.Module):
-            def forward(self, x, y, z, d):
-                return torch.linspace(start=x, end=y, steps=z, dtype=d.dtype)
+        class aten_linspace_prim_dtype(torch.nn.Module):
+            def forward(self, start, end, steps, d):
+                return torch.linspace(start=start, end=end, steps=steps, dtype=d.dtype)
 
-        model_classes = {
-            3: (
-                aten_linspace_start_end_steps_dtype,
-                aten_linspace_start_end_steps_out,
-                aten_linspace_start_end_steps_prim_dtype,
-            )
-        }
         dtype = dtype_map.get(dtype)
         if ref_dtype:
-            model_class = model_classes[num_inputs][2]()
-        elif not use_out or dtype is None:
-            model_class = model_classes[num_inputs][0](dtype)
+            model_class = aten_linspace_prim_dtype()
+        elif not use_out:
+            model_class = aten_linspace_dtype(dtype)
         else:
-            model_class = model_classes[num_inputs][1](dtype)
+            model_class = aten_linspace_out(dtype)
 
         ref_net = None
 
@@ -87,45 +59,13 @@ class TestLinspace(PytorchLayerTest):
 
     @pytest.mark.nightly
     @pytest.mark.precommit
-    @pytest.mark.parametrize(
-        "dtype",
-        [
-            # None,
-            # "float32",
-            # "float64",
-            "int32",
-            "int64",
-            "int8",
-        ],
-    )
-    @pytest.mark.parametrize("start,end,steps", [(0, 1, 1), (5, 7, 1), (-2, 1, 5), (1, -5, 7), (1, 10, 2), (-1, -5, 2)])
-    def test_linspace_start_end_steps(self, dtype, end, start, steps, ie_device, precision, ir_version):
-        self._test(
-            *self.create_model(dtype, 3),
-            ie_device,
-            precision,
-            ir_version,
-            kwargs_to_prepare_input={"end": end, "start": start, "steps": steps, "dtype": dtype}
-        )
-
-    @pytest.mark.nightly
-    @pytest.mark.precommit
-    @pytest.mark.parametrize(
-        "dtype",
-        [
-            "float32",
-            "float64",
-            "int32",
-            "int64",
-            "int8",
-        ],
-    )
+    @pytest.mark.parametrize("dtype", ["float32", "float64", "int32", "int64", "int8"])
     @pytest.mark.parametrize(
         "start,end,steps", [(0, 1, 5), (-2, 1, 5), (1, -5, 7), (1, 10, 2), (-1, -5, 2), (-1, -5, 1)]
     )
-    def test_linspace_start_end_steps_with_prim_dtype(self, dtype, end, start, steps, ie_device, precision, ir_version):
+    def test_linspace_with_prim_dtype(self, dtype, end, start, steps, ie_device, precision, ir_version):
         self._test(
-            *self.create_model(dtype, 3, ref_dtype=True),
+            *self.create_model(dtype, ref_dtype=True),
             ie_device,
             precision,
             ir_version,
@@ -134,27 +74,14 @@ class TestLinspace(PytorchLayerTest):
 
     @pytest.mark.nightly
     @pytest.mark.precommit
-    @pytest.mark.parametrize(
-        "dtype",
-        [
-            # None,
-            "float32",
-            "float64",
-            "int32",
-            "int64",
-            "int8",
-            "uin8",
-        ],
-    )
+    @pytest.mark.parametrize("dtype", [None, "float32", "float64", "int32", "int64", "int8", "uin8"])
     @pytest.mark.parametrize(
         "start,end,steps", [(0, 1, 5), (-2, 1, 5), (1, -5, 7), (1, 10, 2), (-1, -5, 2), (-1, -5, 1)]
     )
     @pytest.mark.parametrize("use_out", [False, True])
-    def test_linspace_start_end_steps_with_out(
-        self, dtype, use_out, end, start, steps, ie_device, precision, ir_version
-    ):
+    def test_linspace_with_out(self, dtype, use_out, end, start, steps, ie_device, precision, ir_version):
         self._test(
-            *self.create_model(dtype=dtype, num_inputs=3, use_out=use_out),
+            *self.create_model(dtype=dtype, use_out=use_out),
             ie_device,
             precision,
             ir_version,
