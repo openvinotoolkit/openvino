bae6849ecc988059ab535aff10640af5c3f06603
Ekaterina Aidova <ekaterina.aidova@intel.com>
2023-08-04 12:23:08 +0400

    fix tests

--- tests/layer_tests/pytorch_tests/test_reshape_as.py
+++ tests/layer_tests/pytorch_tests/test_reshape_as.py
@@ -15,18 +15,25 @@ class TestReshapeAs(PytorchLayerTest):
 
     def create_model(self, op):
         class aten_reshape_as(torch.nn.Module):
+            def __init__(self, op) -> None:
+                super().__init__()
+                if op == "view_as":
+                    self.forward = self.forward_view
 
             def forward(self, input_tensor, shape_tensor):
                 return input_tensor.reshape_as(shape_tensor)
 
+            def forward_view(self, input_tensor, shape_tensor):
+                return input_tensor.view_as(shape_tensor)
+
         ref_net = None
 
-        return aten_reshape_as(), ref_net, "aten::{op}"
+        return aten_reshape_as(op), ref_net, f"aten::{op}"
 
     @pytest.mark.nightly
     @pytest.mark.precommit
     @pytest.mark.parametrize("op", ["reshape_as", "view_as"])
-    @pytest.mark.parametrize('input_tesnor_shapes',( ((3, 6), (2, 9)), ((2, 2, 3), (6, 2)), ((6, 2), (2, 2, 3))))
+    @pytest.mark.parametrize('input_tensor_shapes',( ((3, 6), (2, 9)), ((2, 2, 3), (6, 2)), ((6, 2), (2, 2, 3))))
     def test_reshape_as(self, op, input_tensor_shapes, ie_device, precision, ir_version):
         self._test(*self.create_model(op), ie_device, precision, ir_version, 
                    kwargs_to_prepare_input={"shape1": input_tensor_shapes[0], "shape2": input_tensor_shapes[1]})
