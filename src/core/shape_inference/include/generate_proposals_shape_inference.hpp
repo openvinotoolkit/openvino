ab5e1ae3a83ceea07ffb91057c13cc40f0f4b5c7
Katarzyna Mitrus <katarzyna.mitrus@intel.com>
2023-08-07 16:42:48 +0200

    [ShapeInference] GenerateProposals - shape infer review and tests (#18943)
    * type_prop tests
    
    * add set_attrs
    
    * Update to use shape infer check

--- src/core/shape_inference/include/generate_proposals_shape_inference.hpp
+++ src/core/shape_inference/include/generate_proposals_shape_inference.hpp
@@ -22,106 +22,120 @@ std::vector<TRShape> shape_infer(const GenerateProposals* op, const std::vector<
     const auto& scores_shape = input_shapes[3];
     const auto im_info_shape_rank = im_info_shape.rank();
     auto num_batches = Dimension::dynamic();
-    NODE_VALIDATION_CHECK(op,
-                          im_info_shape_rank.compatible(2),
-                          "The 'input_im_info' input is expected to be a 2D. Got: ",
-                          im_info_shape);
+    NODE_SHAPE_INFER_CHECK(op,
+                           input_shapes,
+                           im_info_shape_rank.compatible(2),
+                           "The 'input_im_info' input is expected to be a 2D. Got: ",
+                           im_info_shape);
 
     if (im_info_shape_rank.is_static()) {
-        NODE_VALIDATION_CHECK(op,
-                              (im_info_shape[1].compatible(3) || im_info_shape[1].compatible(4)),
-                              "The 'input_im_info' shape[1] is expected to be a compatible with [3] or [4]. Got: ",
-                              im_info_shape);
+        NODE_SHAPE_INFER_CHECK(op,
+                               input_shapes,
+                               (im_info_shape[1].compatible(3) || im_info_shape[1].compatible(4)),
+                               "The 'input_im_info' shape[1] is expected to be a compatible with [3] or [4]. Got: ",
+                               im_info_shape);
         Dimension::merge(num_batches, im_info_shape[0], num_batches);
     }
 
     const auto anchors_shape_rank = anchors_shape.rank();
-    NODE_VALIDATION_CHECK(op,
-                          anchors_shape_rank.compatible(4),
-                          "The 'input_anchors' input is expected to be a 4D. Got: ",
-                          anchors_shape);
+    NODE_SHAPE_INFER_CHECK(op,
+                           input_shapes,
+                           anchors_shape_rank.compatible(4),
+                           "The 'input_anchors' input is expected to be a 4D. Got: ",
+                           anchors_shape);
     if (anchors_shape_rank.is_static()) {
-        NODE_VALIDATION_CHECK(op,
-                              anchors_shape[3].compatible(4),
-                              "The fourth dimension of 'input_anchors' should be compatible with 4. Got: ",
-                              anchors_shape[3]);
+        NODE_SHAPE_INFER_CHECK(op,
+                               input_shapes,
+                               anchors_shape[3].compatible(4),
+                               "The fourth dimension of 'input_anchors' should be compatible with 4. Got: ",
+                               anchors_shape[3]);
     }
     const auto deltas_shape_rank = deltas_shape.rank();
     const auto scores_shape_rank = scores_shape.rank();
 
-    NODE_VALIDATION_CHECK(op,
-                          deltas_shape_rank.compatible(4),
-                          "The 'input_deltas' input is expected to be a 4D. Got: ",
-                          deltas_shape);
-    NODE_VALIDATION_CHECK(op,
-                          scores_shape_rank.compatible(4),
-                          "The 'input_scores' input is expected to be a 4D. Got: ",
-                          scores_shape);
+    NODE_SHAPE_INFER_CHECK(op,
+                           input_shapes,
+                           deltas_shape_rank.compatible(4),
+                           "The 'input_deltas' input is expected to be a 4D. Got: ",
+                           deltas_shape);
+    NODE_SHAPE_INFER_CHECK(op,
+                           input_shapes,
+                           scores_shape_rank.compatible(4),
+                           "The 'input_scores' input is expected to be a 4D. Got: ",
+                           scores_shape);
     if (deltas_shape_rank.is_static())
         Dimension::merge(num_batches, deltas_shape[0], num_batches);
     if (scores_shape_rank.is_static())
         Dimension::merge(num_batches, scores_shape[0], num_batches);
 
     if (deltas_shape_rank.is_static() && scores_shape_rank.is_static()) {
-        NODE_VALIDATION_CHECK(op,
-                              deltas_shape[0].compatible(scores_shape[0]),
-                              "Batch for inputs 'input_deltas' and 'input_scores' should be "
-                              "equal. Got: ",
-                              deltas_shape[0],
-                              scores_shape[0]);
+        NODE_SHAPE_INFER_CHECK(op,
+                               input_shapes,
+                               deltas_shape[0].compatible(scores_shape[0]),
+                               "Batch for inputs 'input_deltas' and 'input_scores' should be "
+                               "equal. Got: ",
+                               deltas_shape[0],
+                               scores_shape[0]);
 
-        NODE_VALIDATION_CHECK(op,
-                              deltas_shape[1].compatible(scores_shape[1] * 4),
-                              "Anchor number for inputs 'input_deltas' and 'input_scores' should be "
-                              "equal. Got: ",
-                              deltas_shape[1] / 4,
-                              scores_shape[1]);
+        NODE_SHAPE_INFER_CHECK(op,
+                               input_shapes,
+                               deltas_shape[1].compatible(scores_shape[1] * 4),
+                               "Anchor number for inputs 'input_deltas' and 'input_scores' should be "
+                               "equal. Got: ",
+                               deltas_shape[1] / 4,
+                               scores_shape[1]);
 
-        NODE_VALIDATION_CHECK(op,
-                              deltas_shape[2].compatible(scores_shape[2]),
-                              "Heights for inputs 'input_deltas' and 'input_scores' should be "
-                              "equal. Got: ",
-                              deltas_shape[2],
-                              scores_shape[2]);
+        NODE_SHAPE_INFER_CHECK(op,
+                               input_shapes,
+                               deltas_shape[2].compatible(scores_shape[2]),
+                               "Heights for inputs 'input_deltas' and 'input_scores' should be "
+                               "equal. Got: ",
+                               deltas_shape[2],
+                               scores_shape[2]);
 
-        NODE_VALIDATION_CHECK(op,
-                              deltas_shape[3].compatible(scores_shape[3]),
-                              "Width for inputs 'input_deltas' and 'input_scores' should be "
-                              "equal. Got: ",
-                              deltas_shape[3],
-                              scores_shape[3]);
+        NODE_SHAPE_INFER_CHECK(op,
+                               input_shapes,
+                               deltas_shape[3].compatible(scores_shape[3]),
+                               "Width for inputs 'input_deltas' and 'input_scores' should be "
+                               "equal. Got: ",
+                               deltas_shape[3],
+                               scores_shape[3]);
 
         if (im_info_shape_rank.is_static()) {
-            NODE_VALIDATION_CHECK(op,
-                                  deltas_shape[0].compatible(im_info_shape[0]),
-                                  "Batch for inputs 'im_info' and 'input_deltas' should be "
-                                  "equal. Got: ",
-                                  deltas_shape[0],
-                                  im_info_shape[0]);
+            NODE_SHAPE_INFER_CHECK(op,
+                                   input_shapes,
+                                   deltas_shape[0].compatible(im_info_shape[0]),
+                                   "Batch for inputs 'im_info' and 'input_deltas' should be "
+                                   "equal. Got: ",
+                                   deltas_shape[0],
+                                   im_info_shape[0]);
         }
     }
 
     if (scores_shape_rank.is_static() && anchors_shape_rank.is_static()) {
-        NODE_VALIDATION_CHECK(op,
-                              anchors_shape[0].compatible(scores_shape[2]),
-                              "Heights for inputs 'input_anchors' and 'input_scores' should be "
-                              "equal. Got: ",
-                              anchors_shape[0],
-                              scores_shape[2]);
+        NODE_SHAPE_INFER_CHECK(op,
+                               input_shapes,
+                               anchors_shape[0].compatible(scores_shape[2]),
+                               "Heights for inputs 'input_anchors' and 'input_scores' should be "
+                               "equal. Got: ",
+                               anchors_shape[0],
+                               scores_shape[2]);
 
-        NODE_VALIDATION_CHECK(op,
-                              anchors_shape[1].compatible(scores_shape[3]),
-                              "Width for inputs 'input_anchors' and 'input_scores' should be "
-                              "equal. Got: ",
-                              anchors_shape[1],
-                              scores_shape[3]);
+        NODE_SHAPE_INFER_CHECK(op,
+                               input_shapes,
+                               anchors_shape[1].compatible(scores_shape[3]),
+                               "Width for inputs 'input_anchors' and 'input_scores' should be "
+                               "equal. Got: ",
+                               anchors_shape[1],
+                               scores_shape[3]);
 
-        NODE_VALIDATION_CHECK(op,
-                              anchors_shape[2].compatible(scores_shape[1]),
-                              "Anchor number for inputs 'input_anchors' and 'input_scores' should be "
-                              "equal. Got: ",
-                              anchors_shape[2],
-                              scores_shape[1]);
+        NODE_SHAPE_INFER_CHECK(op,
+                               input_shapes,
+                               anchors_shape[2].compatible(scores_shape[1]),
+                               "Anchor number for inputs 'input_anchors' and 'input_scores' should be "
+                               "equal. Got: ",
+                               anchors_shape[2],
+                               scores_shape[1]);
     }
 
     auto num_rois = Dimension(0, (num_batches * op->get_attrs().post_nms_count).get_max_length());
--- src/core/tests/type_prop/generate_proposals.cpp
+++ src/core/tests/type_prop/generate_proposals.cpp
@@ -9,9 +9,52 @@
 #include "ngraph/ngraph.hpp"
 
 using namespace ngraph;
+using namespace testing;
 
 using GenerateProposals = op::v9::GenerateProposals;
 using Attrs = op::v9::GenerateProposals::Attributes;
+
+TEST(type_prop, generate_proposals_default_ctor) {
+    struct ShapesAndAttrs {
+        PartialShape im_info_shape;
+        PartialShape anchors_shape;
+        PartialShape deltas_shape;
+        PartialShape scores_shape;
+        size_t post_nms_count;
+        PartialShape expected_shape_0;
+        PartialShape expected_shape_1;
+        PartialShape expected_shape_2;
+    };
+
+    const auto dyn_dim = Dimension::dynamic();
+    const auto dyn_shape = PartialShape::dynamic();
+
+    ShapesAndAttrs s =
+        {{1, 3}, {200, 336, 3, 4}, {1, 12, 200, 336}, {1, 3, 200, 336}, 1000, {{0, 1000}, 4}, {{0, 1000}}, {1}};
+    Attrs attrs;
+    attrs.min_size = 0.0f;
+    attrs.nms_threshold = 0.699999988079071f;
+    attrs.post_nms_count = static_cast<int64_t>(s.post_nms_count);
+    attrs.pre_nms_count = 1000;
+
+    auto im_info = std::make_shared<op::Parameter>(element::f32, s.im_info_shape);
+    auto anchors = std::make_shared<op::Parameter>(element::f32, s.anchors_shape);
+    auto deltas = std::make_shared<op::Parameter>(element::f32, s.deltas_shape);
+    auto scores = std::make_shared<op::Parameter>(element::f32, s.scores_shape);
+
+    auto proposals = std::make_shared<GenerateProposals>();
+    proposals->set_arguments(OutputVector{im_info, anchors, deltas, scores});
+    proposals->set_attrs(attrs);
+    proposals->validate_and_infer_types();
+
+    EXPECT_EQ(proposals->get_output_size(), 3);
+    EXPECT_EQ(proposals->get_output_element_type(0), element::f32);
+    EXPECT_EQ(proposals->get_output_element_type(1), element::f32);
+    EXPECT_EQ(proposals->get_output_element_type(2), element::i64);
+    EXPECT_EQ(proposals->get_output_partial_shape(0), s.expected_shape_0);
+    EXPECT_EQ(proposals->get_output_partial_shape(1), s.expected_shape_1);
+    EXPECT_EQ(proposals->get_output_partial_shape(2), s.expected_shape_2);
+}
 
 TEST(type_prop, generate_proposals) {
     Attrs attrs;
@@ -114,6 +157,30 @@ TEST(type_prop, generate_proposals_dynamic) {
 
     std::vector<ShapesAndAttrs> shapes = {
         {{1, 3}, {200, 336, 3, 4}, {1, 12, 200, 336}, {1, 3, 200, 336}, 1000, {{0, 1000}, 4}, {{0, 1000}}, {1}},
+        {{{2, 4}, 3},
+         {200, 336, 3, 4},
+         {{2, 4}, 12, 200, 336},
+         {{2, 4}, 3, 200, 336},
+         1000,
+         {{0, 4000}, 4},
+         {{0, 4000}},
+         {{2, 4}}},
+        {{{2, 8}, 3},
+         {200, 336, 3, 4},
+         {{2, 6}, 12, 200, 336},
+         {{6, 8}, 3, 200, 336},
+         1000,
+         {{0, 6000}, 4},
+         {{0, 6000}},
+         {{6}}},
+        {{dyn_dim, 3},
+         {200, 336, 3, 4},
+         {dyn_dim, 12, 200, 336},
+         {dyn_dim, 3, 200, 336},
+         1000,
+         {dyn_dim, 4},
+         {dyn_dim},
+         {dyn_dim}},
         {{2, 3}, {200, 336, 3, 4}, {2, 12, 200, 336}, dyn_shape, 500, {{0, 1000}, 4}, {{0, 1000}}, {2}},
         {{1, 3}, {200, 336, 3, 4}, dyn_shape, {1, 3, 200, 336}, 700, {{0, 700}, 4}, {{0, 700}}, {1}},
         {{2, 3}, {200, 336, 3, 4}, dyn_shape, dyn_shape, 300, {{0, 600}, 4}, {{0, 600}}, {2}},
@@ -197,12 +264,26 @@ TEST(type_prop, generate_proposals_dynamic) {
          {{1}}},
     };
 
-    for (const auto& s : shapes) {
+    for (auto& s : shapes) {
         Attrs attrs;
         attrs.min_size = 0.0f;
         attrs.nms_threshold = 0.699999988079071f;
         attrs.post_nms_count = static_cast<int64_t>(s.post_nms_count);
         attrs.pre_nms_count = 1000;
+
+        if (s.im_info_shape.rank().is_static())
+            set_shape_labels(s.im_info_shape, 10);
+        if (s.anchors_shape.rank().is_static())
+            set_shape_labels(s.anchors_shape, 20);
+        if (s.deltas_shape.rank().is_static())
+            set_shape_labels(s.deltas_shape, 30);
+        if (s.scores_shape.rank().is_static())
+            set_shape_labels(s.scores_shape, 40);
+
+        ov::label_t expected_batch_label =
+            s.im_info_shape.rank().is_static()
+                ? 10
+                : s.deltas_shape.rank().is_static() ? 30 : s.scores_shape.rank().is_static() ? 40 : ov::no_label;
 
         auto im_info = std::make_shared<op::Parameter>(element::f32, s.im_info_shape);
         auto anchors = std::make_shared<op::Parameter>(element::f32, s.anchors_shape);
@@ -211,11 +292,15 @@ TEST(type_prop, generate_proposals_dynamic) {
 
         auto proposals = std::make_shared<GenerateProposals>(im_info, anchors, deltas, scores, attrs);
 
-        ASSERT_EQ(proposals->get_output_element_type(0), element::f32);
-        ASSERT_EQ(proposals->get_output_element_type(1), element::f32);
-        ASSERT_EQ(proposals->get_output_element_type(2), element::i64);
+        EXPECT_EQ(proposals->get_output_size(), 3);
+        EXPECT_EQ(proposals->get_output_element_type(0), element::f32);
+        EXPECT_EQ(proposals->get_output_element_type(1), element::f32);
+        EXPECT_EQ(proposals->get_output_element_type(2), element::i64);
         EXPECT_EQ(proposals->get_output_partial_shape(0), s.expected_shape_0);
         EXPECT_EQ(proposals->get_output_partial_shape(1), s.expected_shape_1);
         EXPECT_EQ(proposals->get_output_partial_shape(2), s.expected_shape_2);
+        EXPECT_THAT(get_shape_labels(proposals->get_output_partial_shape(0)), ElementsAre(ov::no_label, ov::no_label));
+        EXPECT_THAT(get_shape_labels(proposals->get_output_partial_shape(1)), ElementsAre(ov::no_label));
+        EXPECT_THAT(get_shape_labels(proposals->get_output_partial_shape(2)), ElementsAre(expected_batch_label));
     }
 }
