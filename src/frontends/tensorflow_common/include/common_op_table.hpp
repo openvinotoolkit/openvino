d603b96878338ef0f3e88116c409d41bc39b6e39
Roman Kazantsev <roman.kazantsev@intel.com>
2023-08-07 17:50:30 +0400

    [TF FE][TF Hub] Support DivNoNan and EnsureShape operations (#19007)
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/frontends/tensorflow_common/include/common_op_table.hpp
+++ src/frontends/tensorflow_common/include/common_op_table.hpp
@@ -55,6 +55,7 @@ OP_CONVERTER(translate_cumsum_op);
 OP_CONVERTER(translate_crop_and_resize_op);
 OP_CONVERTER(translate_depth_to_space_op);
 OP_CONVERTER(translate_depthwise_conv_2d_native_op);
+OP_CONVERTER(translate_div_no_nan_op);
 OP_CONVERTER(translate_dynamic_partition_op);
 OP_CONVERTER(translate_einsum_op);
 OP_CONVERTER(translate_elu_op);
