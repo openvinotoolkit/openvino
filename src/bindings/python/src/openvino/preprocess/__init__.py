7092676f70aacb4553182ab49d38ef6d798b2eb6
p-wysocki <przemyslaw.wysocki@intel.com>
2023-08-07 09:13:31 +0200

    Add upper bound

--- src/bindings/python/src/openvino/preprocess/__init__.py
+++ src/bindings/python/src/openvino/preprocess/__init__.py
@@ -28,3 +28,5 @@ from openvino._pyopenvino.preprocess import PreProcessSteps
 from openvino._pyopenvino.preprocess import PostProcessSteps
 from openvino._pyopenvino.preprocess import ColorFormat
 from openvino._pyopenvino.preprocess import ResizeAlgorithm
+
+from .torchvision import PreprocessConverter
