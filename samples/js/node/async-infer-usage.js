a2439ad74e759db2741609d96671d29da21a956e
almilosz <alicja.miloszewska@intel.com>
2023-08-08 12:44:50 +0200

    Expose asyncInfer()

--- samples/js/node/async-infer-usage.js
+++ samples/js/node/async-infer-usage.js
@@ -9,15 +9,25 @@ const model = core.readModel(modelPath);
 const compiledModel = core.compileModel(model, 'CPU');
 
 const inferRequest = compiledModel.createInferRequest();
+const tensorData = new Float32Array([1.6041]);
 
-inferRequest.asyncInfer(inputData[0], (err, result) => {
-  if (err) throw err; // Something went wrong
+const promisifiedAsyncInfer = util.promisify(ov.asyncInfer);
+promisifiedAsyncInfer(inferRequest, [tensorData]).then((output) => {
+    console.log("Output of the inference: ", output)
+})
+.catch((err) => {
+    console.log("Error: ", err);
+})
 
-  processResult(result); // result: { [outputTensorName: string]: Tensor }
-});
 
-const promises = inputData.map(i => {
-  const promisifiedAsyncInfer = util.promisify(inferRequest.asyncInfer); // : asyncInfer({ [inputName: string]: Tensor }) => Promise<{ [outputName: string]: Tensor }>
+// inferRequest.asyncInfer(inputData[0], (err, result) => {
+//   if (err) throw err; // Something went wrong
 
-  return promisifiedAsyncInfer(i); // : Promise<{ [outputName: string]: Tensor }>
-});
+//   processResult(result); // result: { [outputTensorName: string]: Tensor }
+// });
+
+// const promises = inputData.map(i => {
+//   const promisifiedAsyncInfer = util.promisify(inferRequest.asyncInfer); // : asyncInfer({ [inputName: string]: Tensor }) => Promise<{ [outputName: string]: Tensor }>
+
+//   return promisifiedAsyncInfer(i); // : Promise<{ [outputName: string]: Tensor }>
+// });
