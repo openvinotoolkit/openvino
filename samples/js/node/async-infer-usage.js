a3302a1343c5c24386dc40fbfeae9a9af5766b50
almilosz <alicja.miloszewska@intel.com>
2023-08-08 13:22:56 +0200

    Update the sample

--- samples/js/node/async-infer-usage.js
+++ samples/js/node/async-infer-usage.js
@@ -1,33 +1,31 @@
 const { addon: ov } = require('openvinojs-node');
 const util = require('node:util');
 
-const modelPath = './model.xml';
-const inputData = []; // { [inputName: string]: Tensor }[]
+const modelPath = '../assets/models/v3-small_224_1.0_float.xml';
+const inputData = [
+  Float32Array.from({length: 150528}, () => Math.random() ),
+  Float32Array.from({length: 150528}, () => Math.random() )];
+  // e.g. [tensorData1: TypedArray, tensorData2: TypedArray, ...]
 
 const core = new ov.Core();
 const model = core.readModel(modelPath);
 const compiledModel = core.compileModel(model, 'CPU');
 
 const inferRequest = compiledModel.createInferRequest();
-const tensorData = new Float32Array([1.6041]);
 
-const promisifiedAsyncInfer = util.promisify(ov.asyncInfer);
-promisifiedAsyncInfer(inferRequest, [tensorData]).then((output) => {
-    console.log("Output of the inference: ", output)
-})
-.catch((err) => {
-    console.log("Error: ", err);
-})
+const promises = inputData.map(i => {
+  // : asyncInfer({ inferRequest: InferRequest, [inputName: string]: Tensor })
+  // => Promise<{ [outputName: string]: Tensor }>
+  const promisifiedAsyncInfer = util.promisify(ov.asyncInfer);
+  
+  // : Promise<{ [outputName: string]: Tensor }>
+  return promisifiedAsyncInfer(inferRequest, [i]);
+  
+});
 
-
-// inferRequest.asyncInfer(inputData[0], (err, result) => {
-//   if (err) throw err; // Something went wrong
-
-//   processResult(result); // result: { [outputTensorName: string]: Tensor }
-// });
-
-// const promises = inputData.map(i => {
-//   const promisifiedAsyncInfer = util.promisify(inferRequest.asyncInfer); // : asyncInfer({ [inputName: string]: Tensor }) => Promise<{ [outputName: string]: Tensor }>
-
-//   return promisifiedAsyncInfer(i); // : Promise<{ [outputName: string]: Tensor }>
-// });
+Promise.all(promises).then(outputs => {
+  for (const i in outputs)
+    console.log(outputs[i]);
+}).catch((err) => {
+  console.log('Error: ', err);
+});
