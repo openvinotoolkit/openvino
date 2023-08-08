0fc78ce091804e9556d1b7ad8b786c77fc388134
Roman Kazantsev <roman.kazantsev@intel.com>
2023-08-08 13:49:52 +0400

    [TF FE][TF Hub] Support AdjustContrastv2 operation (#19029)
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/frontends/tensorflow/src/op_table.cpp
+++ src/frontends/tensorflow/src/op_table.cpp
@@ -115,6 +115,7 @@ const std::map<std::string, CreatorFunction> get_supported_ops() {
 
         // Separate translators:
         {"AddN", CreatorFunction(translate_add_n_op)},
+        {"AdjustContrastv2", CreatorFunction(translate_adjust_contrast_op)},
         {"ArgMax", CreatorFunction(translate_arg_max_op)},
         {"ArgMin", CreatorFunction(translate_arg_min_op)},
         {"Assert", CreatorFunction(translate_no_op)},
