968424a5c36c23340d8b29a05d564dba893cd096
Hu Yuan2 <yuan2.hu@intel.com>
2023-08-09 13:15:25 +0800

    revert test case
    Signed-off-by: Hu Yuan2 <yuan2.hu@intel.com>

--- tests/stress_tests/memleaks_tests/tests.cpp
+++ tests/stress_tests/memleaks_tests/tests.cpp
@@ -193,64 +193,6 @@ TEST_P(MemLeaksTestSuite, inference_with_streams) {
     test_runner(test_params.numthreads, test);
 }
 
-using MultiThreadLeak = MemLeaksTestSuite;
-TEST_P(MultiThreadLeak, inference_in_multi_thread) {
-    auto test_params = GetParam();
-    long vmsize = 0;
-    long vmpeak = 0;
-    long vmrss = 0;
-    long vmhwm = 0;
-    long last_vmsize = 0;
-    long last_vmpeak = 0;
-    long last_vmrss = 0;
-    long last_vmhwm = 0;
-    int loop_iters = test_params.numiters;
-    if (loop_iters < 10) {
-        GTEST_SKIP() << "Skipping the test case, loop niter < 10";
-    }
-    ov::Core core;
-    for (int m = 0; m < test_params.models.size(); m++) {
-        auto model = core.read_model(test_params.models[m]["full_path"]);
-        auto compile_model = core.compile_model(model, test_params.device);
-        int peak_count = 0;
-        std::vector<long> memory_data;
-        memory_data.reserve(loop_iters * 4);
-        for (int z = 0; z < loop_iters; z++) {
-            {
-                std::vector<std::thread> threads;
-                for (int i = 0; i < test_params.numthreads; i++) {
-                    threads.push_back(std::thread([&compile_model](){
-                                auto infer_request = compile_model.create_infer_request();
-                                infer_request.infer();
-                                }));
-                }
-                for (int i = 0; i < test_params.numthreads; i++) {
-                    threads[i].join();
-                }
-            }
-            getVmValues(vmsize, vmpeak, vmrss, vmhwm);
-            memory_data.push_back(vmsize);
-            memory_data.push_back(vmpeak);
-            memory_data.push_back(vmrss);
-            memory_data.push_back(vmhwm);
-            if (vmpeak > last_vmpeak) {
-                last_vmpeak = vmpeak;
-                peak_count++;
-            }
-        }
-        if (peak_count >  loop_iters * 2 / 3) {
-            for(int i = 0, y = 0; i < loop_iters * 4; i = i + 4, y++) {
-                std::cout << "loop:" << y << " vmsize:" << memory_data[i] <<
-                    " vmpeak:" << memory_data[i+1] <<
-                    " vmrss:" << memory_data[i+2] <<
-                    " vmhwm:" << memory_data[i+3] << std::endl;
-            }
-        }
-        EXPECT_LE(peak_count, loop_iters * 2 / 3);
-    }
-}
-
-
 // tests_pipelines/tests_pipelines.cpp
 
 INSTANTIATE_TEST_SUITE_P(MemLeaksTests, MemLeaksTestSuiteNoModel, ::testing::ValuesIn(generateTestsParamsMemLeaks()),
@@ -260,7 +202,4 @@ INSTANTIATE_TEST_SUITE_P(MemLeaksTests, MemLeaksTestSuiteNoDevice, ::testing::Va
                          getTestCaseNameMemLeaks);
 
 INSTANTIATE_TEST_SUITE_P(MemLeaksTests, MemLeaksTestSuite, ::testing::ValuesIn(generateTestsParamsMemLeaks()),
-                         getTestCaseNameMemLeaks);
-
-INSTANTIATE_TEST_SUITE_P(MemLeaksTests, MultiThreadLeak, ::testing::ValuesIn(generateTestsParamsMemLeaks()),
                          getTestCaseNameMemLeaks);
