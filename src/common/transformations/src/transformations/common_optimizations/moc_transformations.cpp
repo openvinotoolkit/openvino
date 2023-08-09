669eec44b7df4a1ce11b91267c430ed456bbd24a
Anastasiia Pnevskaia <anastasiia.pnevskaia@intel.com>
2023-08-09 14:33:50 +0200

    Added validate_nodes_and_infer_types() pass at the end of MOC pipeline.

--- src/common/transformations/src/transformations/common_optimizations/moc_transformations.cpp
+++ src/common/transformations/src/transformations/common_optimizations/moc_transformations.cpp
@@ -254,13 +254,9 @@ bool ov::pass::MOCTransformations::run_on_model(const std::shared_ptr<ngraph::Fu
         // Restore original shapes to the nGraph Function
         for (auto&& param : f->get_parameters()) {
             param->set_partial_shape(input_shapes.at(param.get()));
-        }
-        f->validate_nodes_and_infer_types();
-    } else {
-        for (auto&& result : f->get_results()) {
-            result->validate_and_infer_types();
-        }
+        }   
     }
+    f->validate_nodes_and_infer_types();
 
     return false;
 }
