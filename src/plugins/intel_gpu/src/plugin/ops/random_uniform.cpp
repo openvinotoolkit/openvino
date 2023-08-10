f4e1423798d793e150e5130d9817132880d86dcf
Sergey Shlyapnikov <sergey.shlyapnikov@intel.com>
2023-08-10 09:43:08 +0400

    [GPU] RandomUniform new shape inference for dynamism support (#19087)

--- src/plugins/intel_gpu/src/plugin/ops/random_uniform.cpp
+++ src/plugins/intel_gpu/src/plugin/ops/random_uniform.cpp
@@ -15,17 +15,33 @@ namespace {
 
 void CreateRandomUniformOp(Program &p, const std::shared_ptr<ngraph::op::v8::RandomUniform> &op) {
     auto inputs = p.GetInputInfo(op);
-    auto output_shape = op->get_output_shape(0);
-    cldnn::format outputFormat = cldnn::format::get_default_format(output_shape.size());
+    auto input_pshape = op->get_input_partial_shape(0);
+    auto output_pshape = op->get_output_partial_shape(0);
 
-    auto random_uniform_prim = cldnn::random_uniform(layer_type_name_ID(op),
-                                                     inputs,
-                                                     cldnn::element_type_to_data_type(op->get_out_type()),
-                                                     op->get_global_seed(),
-                                                     op->get_op_seed(),
-                                                     tensor_from_dims(output_shape),
-                                                     outputFormat);
-    p.add_primitive(*op, random_uniform_prim);
+    OPENVINO_ASSERT(input_pshape.is_static(), "[GPU] Dynamic input of RandomUniform leads to dynamic output rank, but GPU doesn't support it yet");
+
+    if (output_pshape.is_static() && !p.use_new_shape_infer()) {
+        auto output_shape = output_pshape.get_shape();
+        // Extend to 4D shape
+        output_shape.insert(output_shape.end(), 4 - output_shape.size(), 1ul);
+
+        auto random_uniform_prim = cldnn::random_uniform(layer_type_name_ID(op),
+                                                         inputs,
+                                                         cldnn::element_type_to_data_type(op->get_out_type()),
+                                                         op->get_global_seed(),
+                                                         op->get_op_seed(),
+                                                         output_shape);
+        p.add_primitive(*op, random_uniform_prim);
+    } else {
+        OPENVINO_ASSERT(input_pshape.size() == 1, "[GPU] RandomUniform expects 1D input, got ", input_pshape.size());
+
+        auto random_uniform_prim = cldnn::random_uniform(layer_type_name_ID(op),
+                                                         inputs,
+                                                         cldnn::element_type_to_data_type(op->get_out_type()),
+                                                         op->get_global_seed(),
+                                                         op->get_op_seed());
+        p.add_primitive(*op, random_uniform_prim);
+    }
 }
 
 } // namespace
