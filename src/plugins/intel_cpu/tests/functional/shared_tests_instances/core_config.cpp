412443d1976301ff3d1cbd42cf47749c16554844
Aleksandr Voron <aleksandr.voron@intel.com>
2023-08-14 12:49:29 +0200

    set prec in configuration for fp16 capable machines

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/core_config.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/core_config.cpp
@@ -11,10 +11,12 @@ void CoreConfiguration(LayerTestsUtils::LayerTestsCommon* test) {
     if (!configuration.count(InferenceEngine::PluginConfigParams::KEY_ENFORCE_BF16)) {
         configuration.insert({InferenceEngine::PluginConfigParams::KEY_ENFORCE_BF16, InferenceEngine::PluginConfigParams::NO});
     }
-    //force fp32 inference precision if it is not configured specially
-    if (!configuration.count(ov::hint::inference_precision.name())) {
-        configuration.insert({ov::hint::inference_precision.name(), ov::element::f32.to_string()});
-    }
+    #if defined(OV_CPU_ARM_ENABLE_FP16)
+        //force fp32 inference precision if it is not configured specially
+        if (!configuration.count(ov::hint::inference_precision.name())) {
+            configuration.insert({ov::hint::inference_precision.name(), ov::element::f32.to_string()});
+        }
+    #endif
 }
 
 namespace ov {
@@ -26,10 +28,12 @@ void core_configuration(ov::test::SubgraphBaseTest* test) {
             test->configuration.insert({InferenceEngine::PluginConfigParams::KEY_ENFORCE_BF16, InferenceEngine::PluginConfigParams::NO});
         }
     #endif
-    //force fp32 inference precision if it is not configured specially
-    if (!test->configuration.count(ov::hint::inference_precision.name())) {
-        test->configuration.insert({ov::hint::inference_precision.name(), ov::element::f32.to_string()});
-    }
+    #if defined(OV_CPU_ARM_ENABLE_FP16)
+        //force fp32 inference precision if it is not configured specially
+        if (!test->configuration.count(ov::hint::inference_precision.name())) {
+            test->configuration.insert({ov::hint::inference_precision.name(), ov::element::f32.to_string()});
+        }
+    #endif
 }
 
 } // namespace test
