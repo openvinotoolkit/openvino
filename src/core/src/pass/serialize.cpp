8000da70697380e855e6bfa1eaf0c09b692b737b
Pavel Esir <pavel.esir@gmail.com>
2023-08-15 09:48:23 +0200

    change fixme placement in serialize.cpp

--- src/core/src/pass/serialize.cpp
+++ src/core/src/pass/serialize.cpp
@@ -157,8 +157,6 @@ private:
                                                   size_t size,
                                                   ov::element::Type src_type,
                                                   size_t* compressed_size) {
-        // FIXME: This is SLOW stub implemenation!
-
         if (src_type == ov::element::f32) {
             *compressed_size = size / 2;
             auto new_ptr = std::unique_ptr<char[]>(new char[size / 2]);
@@ -166,11 +164,13 @@ private:
             auto src_data = reinterpret_cast<const float*>(ptr);
             ngraph::runtime::reference::convert_from_f32_to_f16_with_clamp(src_data, dst_data, size / 4);
             return new_ptr;
-        } else if (src_type == ov::element::f64) {
+        } else if (src_type == ov::element::f64) { 
             *compressed_size = size / 4;
             auto new_ptr = std::unique_ptr<char[]>(new char[size / 4]);
             auto dst_data = reinterpret_cast<ov::float16*>(new_ptr.get());
             auto src_data = reinterpret_cast<const float*>(ptr);
+            
+            // FIXME: This is SLOW stub implemenation!
             //#pragma omp parallel for
             for (size_t i = 0; i < size / 8; ++i) {
                 // if abs value is smaller than the smallest positive fp16, but not zero
