4a8e521ff71f9f5a139ffdee5188f433a3660f28
suryasidd <surya.siddharth.pemmaraju@intel.com>
2023-08-17 12:35:41 -0700

    Removed redundant functions

--- src/bindings/python/src/openvino/torch/__init__.py
+++ src/bindings/python/src/openvino/torch/__init__.py
@@ -2,8 +2,4 @@
 # Copyright (C) 2018-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from openvino.utils import _add_openvino_libs_to_search_path
-
-_add_openvino_libs_to_search_path()
-
 from openvino.frontend.pytorch.torchdynamo import backend
