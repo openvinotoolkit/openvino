2b9b96dcce23c08ee96094c80ccc49485232c43b
suryasidd <surya.siddharth.pemmaraju@intel.com>
2023-08-16 17:34:38 -0700

    Fixed flake8 issues

--- src/bindings/python/src/openvino/torch/__init__.py
+++ src/bindings/python/src/openvino/torch/__init__.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018-2023 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
 from openvino.utils import _add_openvino_libs_to_search_path
 
 _add_openvino_libs_to_search_path()
