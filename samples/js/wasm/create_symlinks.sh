7b84465abac7a06a4ee9fc09b74b215db91c8a52
Vishniakov Nikolai <nikolai.vishniakov@intel.com>
2023-08-18 18:56:42 +0200

    Cleanup ./samples/js/ directory

--- samples/js/wasm/create_symlinks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ln -svf ../make_inference.js ./browser
-ln -svf ../../assets ./browser
-ln -svf ../node_modules ./browser
-ln -svf ../../samples ./browser
-ln -svf ../../common ./browser
