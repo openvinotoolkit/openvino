b8f61a93b67e3a6ac7b5144dec8fcfd1933ef4e5
Ilya Churaev <ilyachur@gmail.com>
2023-08-22 13:30:02 +0400

    Remove legacy headers and namespaces from C-F type prop tests

--- src/core/tests/type_prop/divide.cpp
+++ src/core/tests/type_prop/divide.cpp
@@ -4,6 +4,6 @@
 
 #include "arithmetic_ops.hpp"
 
-using Type = ::testing::Types<ngraph::op::v1::Divide>;
+using Type = ::testing::Types<ov::op::v1::Divide>;
 
 INSTANTIATE_TYPED_TEST_SUITE_P(type_prop_divide, ArithmeticOperator, Type);
--- src/core/tests/type_prop/floor_mod.cpp
+++ src/core/tests/type_prop/floor_mod.cpp
@@ -4,6 +4,6 @@
 
 #include "arithmetic_ops.hpp"
 
-using Type = ::testing::Types<ngraph::op::FloorMod>;
+using Type = ::testing::Types<ov::op::v1::FloorMod>;
 
 INSTANTIATE_TYPED_TEST_SUITE_P(type_prop_floormod, ArithmeticOperator, Type);
