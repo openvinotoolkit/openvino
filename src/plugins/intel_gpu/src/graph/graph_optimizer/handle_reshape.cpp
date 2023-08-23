6214fdb44fe79569c7027ec7be9dea47b1a8dbb4
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2023-08-23 12:25:04 +0200

    [GPU] Fix reshape optimization (#19270)

--- src/plugins/intel_gpu/src/graph/graph_optimizer/handle_reshape.cpp
+++ src/plugins/intel_gpu/src/graph/graph_optimizer/handle_reshape.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include "intel_gpu/runtime/internal_properties.hpp"
 #include "pass_manager.h"
 #include "program_helpers.h"
 #include "reshape_inst.h"
@@ -63,8 +64,20 @@ void handle_reshape::run(program& p) {
                 return;
 
             auto& out_node = node.get_users().front();
-            if (out_node->is_type<reshape>())
-                p.extract_and_remove(node);
+
+            if (!out_node->is_type<reshape>())
+                return;
+
+            const auto& out_reshape = out_node->as<reshape>();
+            // In case of new shape infer we should not shrink reshapes chain if first reshape changes input rank, e.g.
+            // [a, b] -> reshape1 -> [a1, b1, c1] -> reshape2 -> [a2, b2, 0] and any of the reshapes has special_zero=true
+            // Configuration above will fail if we remove reshape1 node as attempt to handle special zero will fail due to small rank of input
+            if (p.get_config().get_property(ov::intel_gpu::allow_new_shape_infer) &&
+                out_node->get_output_pshape().size() != node.get_input_pshape().size() &&
+                (out_reshape.get_primitive()->special_zero || node.get_primitive()->special_zero))
+                return;
+
+            p.extract_and_remove(node);
         });
     }
 
