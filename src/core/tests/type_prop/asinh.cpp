2f52d14b1f0c01b11f056adc9d661e2d49fda28e
Ilya Churaev <ilya.churaev@intel.com>
2023-08-24 06:55:21 +0400

    Removed legacy headers from some core tests (#19328)
    * Removed legacy headers from some core tests
    
    * Fixed build

--- src/core/tests/type_prop/asinh.cpp
+++ src/core/tests/type_prop/asinh.cpp
@@ -4,6 +4,6 @@
 
 #include "unary_ops.hpp"
 
-using Type = ::testing::Types<ngraph::op::Asinh>;
+using Type = ::testing::Types<ov::op::v3::Asinh>;
 
 INSTANTIATE_TYPED_TEST_SUITE_P(type_prop_asinh, UnaryOperator, Type);
