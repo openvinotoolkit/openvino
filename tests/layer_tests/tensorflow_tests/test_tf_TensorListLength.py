d35706fafcf44cbde3dabd6e832e5463e6a2979a
Kazantsev, Roman <roman.kazantsev@intel.com>
2023-08-24 15:24:08 +0400

    Add test with empty tensor list

--- tests/layer_tests/tensorflow_tests/test_tf_TensorListLength.py
+++ tests/layer_tests/tensorflow_tests/test_tf_TensorListLength.py
@@ -43,3 +43,42 @@ class TestTensorListLength(CommonTFLayerTest):
         self._test(*self.create_tensor_list_length(**params),
                    ie_device, precision, ir_version, temp_dir=temp_dir,
                    use_new_frontend=use_new_frontend, use_old_api=use_old_api)
+
+
+class TestTensorListLengthEmptyList(CommonTFLayerTest):
+    def _prepare_input(self, inputs_info):
+        #assert 'tensor_list_size' in inputs_info
+        inputs_data = {}
+        inputs_data['tensor_list_size'] = np.array([self.tensor_list_size], dtype=np.int32)
+        return inputs_data
+
+    def create_tensor_list_length_empty_list(self, tensor_list_size, element_shape):
+        self.tensor_list_size = tensor_list_size
+        tf.compat.v1.reset_default_graph()
+        # Create the graph and model
+        with tf.compat.v1.Session() as sess:
+            tensor_list_size = tf.compat.v1.placeholder(tf.int32, [1], 'tensor_list_size')
+            tf_element_shape = tf.constant(element_shape, dtype=tf.int32)
+            tensor_shape = tf.concat([tensor_list_size, tf_element_shape], 0)
+            tensor = tf.broadcast_to(tf.constant(0.0, dtype=tf.float32), tensor_shape)
+            tensor_list = tf.raw_ops.TensorListFromTensor(tensor=tensor,
+                                                          element_shape=tf_element_shape)
+            tf.raw_ops.TensorListLength(input_handle=tensor_list)
+            tf.compat.v1.global_variables_initializer()
+            tf_net = sess.graph_def
+
+        return tf_net, None
+
+    test_data_tensor_list_length_empty_list = [
+        dict(tensor_list_size=0, element_shape=[]),
+        dict(tensor_list_size=0, element_shape=[2, 3]),
+    ]
+
+    @pytest.mark.parametrize("params", test_data_tensor_list_length_empty_list)
+    @pytest.mark.precommit_tf_fe
+    @pytest.mark.nightly
+    def test_tensor_list_length_empty_list(self, params, ie_device, precision, ir_version, temp_dir,
+                                           use_new_frontend, use_old_api):
+        self._test(*self.create_tensor_list_length_empty_list(**params),
+                   ie_device, precision, ir_version, temp_dir=temp_dir,
+                   use_new_frontend=use_new_frontend, use_old_api=use_old_api)
