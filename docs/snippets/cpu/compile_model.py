ff8f1f0763366430dbedeb9bf514e8a7bb87048d
Jan Iwaszkiewicz <jan.iwaszkiewicz@intel.com>
2023-08-28 10:02:19 +0200

    CPU snippets update (#134)
    * snippets CPU 1/6
    
    * snippets CPU 2/6
    
    * snippets CPU 3/6
    
    * snippets CPU 4/6
    
    * snippets CPU 5/6
    
    * snippets CPU 6/6
    
    * make  module TODO: REMEMBER ABOUT EXPORTING PYTONPATH ON CIs ETC
    
    * Add static model creation in snippets for CPU

--- docs/snippets/cpu/compile_model.py
+++ docs/snippets/cpu/compile_model.py
@@ -1,17 +1,18 @@
 # Copyright (C) 2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+from snippets import get_model
+
+model = get_model()
 
 #! [compile_model_default]
-from openvino.runtime import Core
+import openvino as ov
 
-core = Core()
-model = core.read_model("model.xml")
+core = ov.Core()
 compiled_model = core.compile_model(model, "CPU")
 #! [compile_model_default]
 
 #! [compile_model_multi]
-core = Core()
-model = core.read_model("model.xml")
+core = ov.Core()
 compiled_model = core.compile_model(model, "MULTI:CPU,GPU.0")
 #! [compile_model_multi]
