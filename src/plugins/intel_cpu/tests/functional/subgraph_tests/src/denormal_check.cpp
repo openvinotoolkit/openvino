1e119c3f4ef36a5c46212f1faeec41def44782cb
Gorokhov Dmitriy <dmitry.gorokhov@intel.com>
2023-08-29 09:53:05 +0400

    [CPU] Fixed has_subnormals behavior for negative zero values (#19360)

--- src/plugins/intel_cpu/tests/functional/subgraph_tests/src/denormal_check.cpp
+++ src/plugins/intel_cpu/tests/functional/subgraph_tests/src/denormal_check.cpp
@@ -43,10 +43,11 @@ void validate() override {
     ASSERT_FALSE(actualOutputs.empty());
     auto& outTensor = actualOutputs.front();
     ASSERT_EQ(ov::element::f32, outTensor.get_element_type()) << "Unexpected element type";
-    const uint32_t* data = reinterpret_cast<const uint32_t*>(outTensor.data());
+    const float* data = reinterpret_cast<const float*>(outTensor.data());
     bool hasDenormals = false;
     for (size_t i = 0; i < outTensor.get_size(); ++i) {
-        if (data[i] && (data[i] & (0xff << 23)) == 0) {
+        if (std::abs(data[i]) >= std::numeric_limits<float>::denorm_min() &&
+            std::abs(data[i]) < std::numeric_limits<float>::min()) {
             hasDenormals = true;
         }
     }
@@ -108,4 +109,5 @@ TEST_F(DenormalNullifyCheck, smoke_CPU_Denormal_Check) {
         run();
     }
 }
+
 }// namespace SubgraphTestsDefinitions
