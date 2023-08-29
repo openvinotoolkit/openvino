f2136a158afb73ae22c9808c35f8f49990a4f4c5
PRATHAM-SPS <ingawalepratham@gmail.com>
2023-08-29 22:17:16 +0530

    pytest:- MatMulNormalizer_test.py

--- tools/mo/unit_tests/mo/back/MatMulNormalizer_test.py
+++ tools/mo/unit_tests/mo/back/MatMulNormalizer_test.py
@@ -5,6 +5,7 @@ import unittest
 from argparse import Namespace
 
 import numpy as np
+import pytest
 
 from openvino.tools.mo.back.MatMulNormalizer import SmartReshape_HC_Reshape_MatMul, PullTransposeThroughFQUp
 from openvino.tools.mo.ops.MatMul import MatMul
@@ -18,89 +19,84 @@ from unit_tests.utils.graph import build_graph, regular_op_with_shaped_data, val
 from unit_tests.utils.graph import regular_op_with_empty_data as op_with_empty_data
 
 
-class SmartReshape_HC_Reshape_MatMulTest(unittest.TestCase):
-    def test_reshape_on_the_A_input(self):
-        test_cases = [
+class TestSmartReshape_HC_Reshape_MatMulTest():
+    @pytest.mark.parametrize("in1_shape, in2_shape, reshape_pattern, transpose_a, transpose_b, updated_pattern",
+        [
             ([1, 20, 30], [30, 40], [20, -1], False, False, [-1, 30]),
             ([1, 20, 30], [40, 30], [20, -1], False, True, [-1, 30]),
             ([1, 30, 20], [30, 40], [-1, 20], True, False, [30, -1]),
             ([1, 30, 20], [40, 30], [-1, 20], True, True, [30, -1]),
         ]
+    )
+    def test_reshape_on_the_A_input(self,
+                                    in1_shape, in2_shape, reshape_pattern, transpose_a, transpose_b, updated_pattern):
+        nodes = {
+            **regular_op_with_shaped_data('in_1', in1_shape, dict(type='Parameter', op='Parameter')),
+            **regular_op_with_shaped_data('in_2', in2_shape, dict(type='Parameter', op='Parameter')),
+            **valued_const_with_data('dim', int64_array(reshape_pattern)),
+            **op_with_empty_data('reshape',
+                                 dict(type='Reshape', op='Reshape', infer=Reshape.infer, need_shape_inference=True)),
+            **op_with_empty_data('matmul',
+                                 dict(type='MatMul', op='MatMul', infer=MatMul.infer, need_shape_inference=True,
+                                      transpose_a=transpose_a, transpose_b=transpose_b, dim_attrs={})),
+            **result(),
+        }
+        edges = [
+            *connect('in_1:0', '0:reshape'),
+            *connect('dim:0', '1:reshape'),
+            *connect('reshape:0', '0:matmul'),
+            *connect('in_2:0', '1:matmul'),
+            *connect('matmul:0', 'output'),
+        ]
+        graph = build_graph(nodes_attrs=nodes, edges=edges, cli=Namespace(static_shape=True))
+        graph.clean_up()
+        SmartReshape_HC_Reshape_MatMul().find_and_replace_pattern(graph)
+        graph.clean_up()
 
-        for idx, (in1_shape, in2_shape, reshape_pattern, transpose_a, transpose_b,
-                  updated_pattern) in enumerate(test_cases):
-            with self.subTest(test_case=idx):
-                nodes = {
-                    **regular_op_with_shaped_data('in_1', in1_shape, dict(type='Parameter', op='Parameter')),
-                    **regular_op_with_shaped_data('in_2', in2_shape, dict(type='Parameter', op='Parameter')),
-                    **valued_const_with_data('dim', int64_array(reshape_pattern)),
-                    **op_with_empty_data('reshape',
-                                         dict(type='Reshape', op='Reshape', infer=Reshape.infer,
-                                              need_shape_inference=True)),
-                    **op_with_empty_data('matmul',
-                                         dict(type='MatMul', op='MatMul', infer=MatMul.infer, need_shape_inference=True,
-                                              transpose_a=transpose_a, transpose_b=transpose_b, dim_attrs={})),
-                    **result(),
-                }
-                edges = [
-                    *connect('in_1:0', '0:reshape'),
-                    *connect('dim:0', '1:reshape'),
-                    *connect('reshape:0', '0:matmul'),
-                    *connect('in_2:0', '1:matmul'),
-                    *connect('matmul:0', 'output'),
-                ]
-                graph = build_graph(nodes_attrs=nodes, edges=edges, cli=Namespace(static_shape=True))
-                graph.clean_up()
-                SmartReshape_HC_Reshape_MatMul().find_and_replace_pattern(graph)
-                graph.clean_up()
+        graph_ref = build_graph(nodes_attrs=nodes, edges=edges, update_attributes={
+            'dim': {'value': int64_array(updated_pattern)}, 'dim_d': {'value': int64_array(updated_pattern)}})
+        graph_ref.clean_up()
 
-                graph_ref = build_graph(nodes_attrs=nodes, edges=edges, update_attributes={
-                    'dim': {'value': int64_array(updated_pattern)}, 'dim_d': {'value': int64_array(updated_pattern)}})
-                graph_ref.clean_up()
+        (flag, resp) = compare_graphs(graph, graph_ref, 'output', check_op_attrs=True)
+        assert flag, resp
 
-                (flag, resp) = compare_graphs(graph, graph_ref, 'output', check_op_attrs=True)
-                self.assertTrue(flag, resp)
-
-    def test_reshape_on_the_B_input(self):
-        test_cases=[
+    @pytest.mark.parametrize("in1_shape, in2_shape, reshape_pattern, transpose_a, transpose_b, updated_pattern",[
         ([20, 30], [1, 30, 40], [-1, 40], False, False, [30, -1]),
         ([20, 30], [1, 40, 30], [40, -1], False, True, [-1, 30]),
         ([30, 20], [1, 30, 40], [-1, 40], True, False, [30, -1]),
         ([30, 20], [1, 40, 30], [40, -1], True, True, [-1, 30]),
-    ]
-        for idx ,(in1_shape, in2_shape, reshape_pattern, transpose_a,
-                  transpose_b, updated_pattern) in enumerate(test_cases):
-            with self.subTest(test_cases=idx):
-                nodes = {
-                    **regular_op_with_shaped_data('in_1', in1_shape, dict(type='Parameter', op='Parameter')),
-                    **regular_op_with_shaped_data('in_2', in2_shape, dict(type='Parameter', op='Parameter')),
-                    **valued_const_with_data('dim', int64_array(reshape_pattern)),
-                    **op_with_empty_data('reshape',
-                                        dict(type='Reshape', op='Reshape', infer=Reshape.infer,
-                                             need_shape_inference=True)),
-                    **op_with_empty_data('matmul',
-                                        dict(type='MatMul', op='MatMul', infer=MatMul.infer, need_shape_inference=True,
-                                            transpose_a=transpose_a, transpose_b=transpose_b, dim_attrs={})),
-                    **result(),
-                }
-                edges = [
-                    *connect('in_1:0', '0:matmul'),
-                    *connect('in_2:0', '0:reshape'),
-                    *connect('dim:0', '1:reshape'),
-                    *connect('reshape:0', '1:matmul'),
-                    *connect('matmul:0', 'output'),
-                ]
-                graph = build_graph(nodes_attrs=nodes, edges=edges, cli=Namespace(static_shape=True))
-                graph.clean_up()
-                SmartReshape_HC_Reshape_MatMul().find_and_replace_pattern(graph)
-                graph.clean_up()
+    ])
+    def test_reshape_on_the_B_input(self,
+                                    in1_shape, in2_shape, reshape_pattern, transpose_a, transpose_b, updated_pattern):
+        nodes = {
+            **regular_op_with_shaped_data('in_1', in1_shape, dict(type='Parameter', op='Parameter')),
+            **regular_op_with_shaped_data('in_2', in2_shape, dict(type='Parameter', op='Parameter')),
+            **valued_const_with_data('dim', int64_array(reshape_pattern)),
+            **op_with_empty_data('reshape',
+                                 dict(type='Reshape', op='Reshape', infer=Reshape.infer, need_shape_inference=True)),
+            **op_with_empty_data('matmul',
+                                 dict(type='MatMul', op='MatMul', infer=MatMul.infer, need_shape_inference=True,
+                                      transpose_a=transpose_a, transpose_b=transpose_b, dim_attrs={})),
+            **result(),
+        }
+        edges = [
+            *connect('in_1:0', '0:matmul'),
+            *connect('in_2:0', '0:reshape'),
+            *connect('dim:0', '1:reshape'),
+            *connect('reshape:0', '1:matmul'),
+            *connect('matmul:0', 'output'),
+        ]
+        graph = build_graph(nodes_attrs=nodes, edges=edges, cli=Namespace(static_shape=True))
+        graph.clean_up()
+        SmartReshape_HC_Reshape_MatMul().find_and_replace_pattern(graph)
+        graph.clean_up()
 
-                graph_ref = build_graph(nodes_attrs=nodes, edges=edges, update_attributes={
-                    'dim': {'value': int64_array(updated_pattern)}, 'dim_d': {'value': int64_array(updated_pattern)}})
-                graph_ref.clean_up()
+        graph_ref = build_graph(nodes_attrs=nodes, edges=edges, update_attributes={
+            'dim': {'value': int64_array(updated_pattern)}, 'dim_d': {'value': int64_array(updated_pattern)}})
+        graph_ref.clean_up()
 
-                (flag, resp) = compare_graphs(graph, graph_ref, 'output', check_op_attrs=True)
-                self.assertTrue(flag, resp)
+        (flag, resp) = compare_graphs(graph, graph_ref, 'output', check_op_attrs=True)
+        assert flag, resp
 
 
 class FQTransposePullerTest(unittest.TestCase):
@@ -110,11 +106,9 @@ class FQTransposePullerTest(unittest.TestCase):
             **valued_const_with_data('ih', np.array([[[[255]]]])),
             **valued_const_with_data('ol', np.array([[[[0]]]])),
             **valued_const_with_data('oh', np.array([[[[255]]]])),
-            **regular_op_with_shaped_data('FQ', fq_shape, dict(type='FakeQuantize',
-                                            op='FakeQuantize', infer=FakeQuantize.infer)),
+            **regular_op_with_shaped_data('FQ', fq_shape, dict(type='FakeQuantize', op='FakeQuantize', infer=FakeQuantize.infer)),
             **valued_const_with_data('order', int64_array([0, 2, 3, 1])),
-            **regular_op_with_shaped_data('transpose', transpose_shape, dict(type='Transpose',
-                                            op='Transpose', infer=Transpose.infer)),
+            **regular_op_with_shaped_data('transpose', transpose_shape, dict(type='Transpose', op='Transpose', infer=Transpose.infer)),
             **regular_op_with_shaped_data('relu', fq_shape, dict(type='Relu', op='Relu')),
 
             **result(),
