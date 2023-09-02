dfd9d43a23bb219f9d67913abb48c9268bc50cda
fishbell <bell.song@intel.com>
2023-09-02 15:26:22 -0400

    clean up
    Signed-off-by: fishbell <bell.song@intel.com>

--- src/tests/functional/plugin/auto/shared_tests_instances/skip_tests_config.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2018-2021 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
-//
-#include "functional_test_utils/skip_tests_config.hpp"
-
-std::vector<std::string> disabledTestPatterns() {
-    return {};
-//    return {R"(.*(AutoInferConsistency.CompareWithDirectHW).*)"};
-}
