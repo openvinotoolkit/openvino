24eebbfc2243bf39ea66b4e5901b5c4dc8cd3ad3
fishbell <bell.song@intel.com>
2023-09-03 16:01:01 -0400

    fix windows build failure
    Signed-off-by: fishbell <bell.song@intel.com>

--- src/plugins/auto/tests/functional/shared_tests_instances/behavior/plugin/core_threading_tests.cpp
+++ src/plugins/auto/tests/functional/shared_tests_instances/behavior/plugin/core_threading_tests.cpp
@@ -8,11 +8,14 @@
 #if __GLIBC_MINOR__  >= 34
     #define ENABLETESTMULTI
 #endif
+#else
+    #define ENABLETESTMULTI
 #endif
 
 namespace {
 
 const Params params[] = {
+    std::tuple<Device, Config>{ ov::test::utils::DEVICE_CPU, {{ CONFIG_KEY(PERF_COUNT), CONFIG_VALUE(YES) }}},
 #ifdef ENABLETESTMULTI
     std::tuple<Device, Config>{ ov::test::utils::DEVICE_MULTI, {{ MULTI_CONFIG_KEY(DEVICE_PRIORITIES) , ov::test::utils::DEVICE_CPU }}},
     std::tuple<Device, Config>{ ov::test::utils::DEVICE_AUTO, {{ MULTI_CONFIG_KEY(DEVICE_PRIORITIES) , ov::test::utils::DEVICE_CPU }}},
@@ -20,11 +23,11 @@ const Params params[] = {
 };
 }  // namespace
 
-//TBD INSTANTIATE_TEST_SUITE_P(smoke_Auto_BehaviorTests, CoreThreadingTests, testing::ValuesIn(params), CoreThreadingTests::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_Auto_BehaviorTests, CoreThreadingTests, testing::ValuesIn(params), CoreThreadingTests::getTestCaseName);
 
-/*INSTANTIATE_TEST_SUITE_P(smoke_Auto_BehaviorTests, CoreThreadingTestsWithIterations,
+INSTANTIATE_TEST_SUITE_P(smoke_Auto_BehaviorTests, CoreThreadingTestsWithIterations,
     testing::Combine(testing::ValuesIn(params),
                      testing::Values(4),
                      testing::Values(50),
                      testing::Values(ModelClass::Default)),
-    CoreThreadingTestsWithIterations::getTestCaseName);*/
+    CoreThreadingTestsWithIterations::getTestCaseName);
