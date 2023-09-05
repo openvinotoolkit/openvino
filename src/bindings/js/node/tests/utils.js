eba21b7cc476119b13d520b8d7c40c34a4ae7a61
almilosz <alicja.miloszewska@intel.com>
2023-09-05 08:49:58 +0200

    Update getModelPath()

--- src/bindings/js/node/tests/utils.js
+++ src/bindings/js/node/tests/utils.js
@@ -6,14 +6,7 @@ const path = require('path');
 
 function getModelPath(isFP16=false) {
   const basePath = '../../python/tests/';
-  let testXml;
-  if (isFP16) {
-    testXml = path.join(basePath, 'test_utils', 'utils', 'test_model_fp16.xml');
-  } else {
-    testXml = path.join(basePath, 'test_utils', 'utils', 'test_model_fp32.xml');
-  }
-
-  return testXml;
+  return path.join(basePath, 'test_utils', 'utils', `test_model_fp${isFP16 ? 16 : 32}.xml`);
 }
 
 module.exports = {
