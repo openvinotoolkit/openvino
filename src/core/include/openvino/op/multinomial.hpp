ee27c7030f397703bd360dc7bddda8f92f85c59c
Roboreptile <piotr.krzeminski@intel.com>
2023-09-06 12:40:33 +0000

    [Ref] Add core & reference implementation reusing other ops

---
+++ src/core/include/openvino/op/multinomial.hpp
@@ -0,0 +1,89 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include "openvino/op/op.hpp"
+
+namespace ov {
+namespace op {
+namespace v13 {
+/// \brief Multinomial operation over the input tensor.
+///
+/// \ingroup ov_ops_cpp_api
+class OPENVINO_API Multinomial : public Op {
+public:
+    OPENVINO_OP("Multinomial", "opset13");
+    Multinomial();
+    /// \param input The input tensor to be normalized
+    /// \param num_samples The tensor containing samples count. Defines output shape of the network
+    /// \param output_type The data type of the output
+    /// \param replacement Determines sampling with replacement
+    /// \param log_probs Determines whether the input contains probabilities or log probabilities
+    /// \param global_seed The seed used in ----
+    /// \param op_seed The seed used in ----
+    Multinomial(const Output<Node>& input,
+                       const Output<Node>& num_samples,
+                       const ngraph::element::Type_t output_type,
+                       const bool replacement,
+                       const bool log_probs,
+                       const int64_t global_seed,
+                       const int64_t op_seed);
+
+    bool visit_attributes(AttributeVisitor& visitor) override;
+
+    void validate_and_infer_types() override;
+
+    ngraph::element::Type_t get_output_type() const {
+        return m_output_type;
+    }
+
+    void set_output_type(ngraph::element::Type_t output_type) {
+        m_output_type = output_type;
+    }
+
+    bool get_replacement() const {
+        return m_replacement;
+    }
+
+    void set_replacement(bool replacement) {
+        m_replacement = replacement;
+    }
+
+    bool get_log_probs() const {
+        return m_log_probs;
+    }
+
+    void set_log_probs(bool log_probs) {
+        m_log_probs = log_probs;
+    }
+
+    int64_t get_global_seed() const {
+        return m_global_seed;
+    }
+
+    void set_global_seed(int64_t global_seed) {
+        m_global_seed = global_seed;
+    }
+
+    int64_t get_op_seed() const {
+        return m_op_seed;
+    }
+
+    void set_op_seed(int64_t op_seed) {
+        m_op_seed = op_seed;
+    }
+
+    std::shared_ptr<Node> clone_with_new_inputs(const OutputVector& new_args) const override;
+
+private:
+    ngraph::element::Type_t m_output_type;
+    bool m_replacement;
+    bool m_log_probs;
+    int64_t m_global_seed;
+    int64_t m_op_seed;
+};
+}  // namespace v13
+}  // namespace op
+}  // namespace ov
--- src/core/reference/include/openvino/reference/multinomial.hpp
+++ src/core/reference/include/openvino/reference/multinomial.hpp
@@ -7,13 +7,81 @@
 #include <cmath>
 #include <cstddef>
 
+#include <openvino/reference/min.hpp>
+#include <openvino/reference/max.hpp>
+#include <openvino/reference/random_uniform.hpp>
+#include <ngraph/shape.hpp>
+
 namespace ov {
 namespace reference {
-template <typename T>
-void multinomial(const T* input, T* output, ov::element::Type output_type, bool replacement, bool log_probs, int64_t global_seed = 0, int64_t op_seed = 0) {
-    for (size_t i = 0; i < count; i++) {
-        out[i] = arg[i] < T(0) ? T(alpha * (std::exp(arg[i]) - 1.0)) : arg[i];
+
+namespace {
+    template <typename T>
+    void prefix_sum_with_normalize(const T* input,
+            const T* output,
+            const Shape& input_output_shape,
+            T* min,
+            T* max) {
+        max[0] -= min[0];
+        for(uint64_t i = 1; i<input_output_shape.at(0); ++i) {
+                output[i] = input[i] + output[i-1];
+                output[i] = (output[i] - min[i]) / max[i];
+        }
     }
 }
+
+template <typename T>
+void multinomial(const T* input,
+    const Shape& input_shape,
+    const T* num_samples, 
+    const Shape& num_samples_shape,
+    const T* output,
+    const Shape& output_shape,
+    const bool replacement, // TODO
+    const bool log_probs,   // TODO
+    const int64_t global_seed = 0, 
+    const int64_t op_seed = 0) {
+
+        float one = 1.0f;
+        float zero = 0.0f;
+        size_t count = num_samples_shape.at(0);
+        std::pair<uint64_t, uint64_t> initial_state(0, 0);
+
+        float* uniform_samples = malloc(sizeof(float) * count);
+        random_uniform(*count, *zero, *one, uniform_samples, *one, ov::element::f32, global_seed, op_seed, initial_state);
+
+        ov::AxisSet input_axes();
+        for(uint64_t i = 0; input_shape.size(); i++) {
+            axes.push_back(i);
+        }
+        T* min_value = malloc(sizeof(T));
+        min<T>(input, min_value, input_shape, ov::AxisSet(axes));
+        T* max_value = malloc(sizeof(T));
+        max<T>(input, max_value, input_shape, ov::AxisSet(axes));
+        T* cdf = calloc(count, sizeof(T));
+        prefix_sum_with_normalize<T>(input, cdf, input_shape, min_value, max_value);
+
+        for(size_t i = 0; i < count; ++i) {
+            float random_nr = uniform_samples[i];
+            for (size_t j = 0; j < count; ++j) {
+                if (j + 1 < count) {
+                    if (cdf[j] == cdf[j+1]) {
+                        continue;
+                    }
+                    if (std::abs((float)cdf[j] - random_nr) < std::abs((float)cdf[j + 1] - random_nr)) {
+                        output[i] = j;
+                        break;
+                    }
+                } else {
+                    output[i] = j;
+                }
+            }
+        }
+
+        free(uniform_samples);
+        free (min_value);
+        free (max_value);
+        free (cdf);
+    }
 }  // namespace reference
 }  // namespace ov
