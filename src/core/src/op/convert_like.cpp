0ae80291f5f3576ffbb835e09ebe7bd9baf8bfcb
Raasz, Pawel <pawel.raasz@intel.com>
2023-09-06 09:41:32 +0000

    Migrate ops evaluate

--- src/core/src/op/convert_like.cpp
+++ src/core/src/op/convert_like.cpp
@@ -2,38 +2,39 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "ngraph/op/convert_like.hpp"
+#include "openvino/op/convert_like.hpp"
 
 #include <memory>
 
 #include "itt.hpp"
-#include "ngraph/op/constant.hpp"
-#include "ngraph/op/convert.hpp"
+#include "openvino/op/constant.hpp"
+#include "openvino/op/convert.hpp"
 
-using namespace std;
-using namespace ngraph;
+namespace ov {
+namespace op {
+namespace v1 {
 
-op::v1::ConvertLike::ConvertLike(const Output<Node>& data, const Output<Node>& like) : Op({data, like}) {
+ConvertLike::ConvertLike(const Output<Node>& data, const Output<Node>& like) : Op({data, like}) {
     constructor_validate_and_infer_types();
 }
 
-void op::v1::ConvertLike::validate_and_infer_types() {
+void ConvertLike::validate_and_infer_types() {
     OV_OP_SCOPE(v1_ConvertLike_validate_and_infer_types);
     set_output_type(0, get_input_element_type(1), get_input_partial_shape(0));
 }
 
-bool op::v1::ConvertLike::visit_attributes(AttributeVisitor& visitor) {
+bool ConvertLike::visit_attributes(AttributeVisitor& visitor) {
     OV_OP_SCOPE(v1_ConvertLike_visit_attributes);
     return true;
 }
 
-shared_ptr<Node> op::v1::ConvertLike::clone_with_new_inputs(const OutputVector& new_args) const {
+std::shared_ptr<Node> ConvertLike::clone_with_new_inputs(const OutputVector& new_args) const {
     OV_OP_SCOPE(v1_ConvertLike_clone_with_new_inputs);
     check_new_args_count(this, new_args);
-    return make_shared<ConvertLike>(new_args.at(0), new_args.at(1));
+    return std::make_shared<ConvertLike>(new_args.at(0), new_args.at(1));
 }
 
-bool op::v1::ConvertLike::constant_fold(OutputVector& output_values, const OutputVector& input_values) {
+bool ConvertLike::constant_fold(OutputVector& output_values, const OutputVector& input_values) {
     OV_OP_SCOPE(v1_ConvertLike_constant_fold);
     if (is_const_fold_disabled()) {
         return false;
@@ -45,3 +46,7 @@ bool op::v1::ConvertLike::constant_fold(OutputVector& output_values, const Outpu
     }
     return false;
 }
+
+}  // namespace v1
+}  // namespace op
+}  // namespace ov
