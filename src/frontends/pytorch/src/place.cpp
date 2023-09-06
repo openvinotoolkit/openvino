900f1714e24808b7d9aa0382ffa8999d48bd9a40
Maxim Vafin <maxim.vafin@intel.com>
2023-09-06 09:21:05 +0200

    Implement override_all_inputs, override_all_outputs

--- src/frontends/pytorch/src/place.cpp
+++ src/frontends/pytorch/src/place.cpp
@@ -20,19 +20,19 @@ Place::Place(const ov::frontend::InputModel& input_model, size_t tensor_index)
       m_is_output(false) {
     const auto im = dynamic_cast<const ov::frontend::pytorch::InputModel*>(&m_input_model);
     FRONT_END_GENERAL_CHECK(im, "PyTorch Place requires PyTorch InputModel class.");
-    const auto& inputs = im->m_model_decoder->inputs();
-    const auto& outputs = im->m_model_decoder->outputs();
+    auto decoder = im->get_decoder();
+    const auto& inputs = decoder->inputs();
+    const auto& outputs = decoder->outputs();
     auto in_it = std::find(inputs.begin(), inputs.end(), tensor_index);
     if (in_it != inputs.end()) {
         m_is_input = true;
-        const auto& signature_name =
-            im->m_model_decoder->get_input_signature_name(std::distance(inputs.begin(), in_it));
+        const auto& signature_name = decoder->get_input_signature_name(std::distance(inputs.begin(), in_it));
         m_names.push_back(signature_name);
     }
     auto out_it = std::find(outputs.begin(), outputs.end(), tensor_index);
     if (out_it != outputs.end()) {
         m_is_output = true;
-        const auto& debug_name = im->m_model_decoder->get_output_debug_name(std::distance(outputs.begin(), out_it));
+        const auto& debug_name = decoder->get_output_debug_name(std::distance(outputs.begin(), out_it));
         m_names.push_back(debug_name);
     }
     if (m_is_input && m_is_output) {
