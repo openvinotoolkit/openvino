b92a7564d2d7b4f84d3c75e67453dc00cfd13fb6
Andrew Park <andrew.park@intel.com>
2023-09-08 00:14:56 +0900

    [GPU] Fix canonicalization for fused dep's shape
    Signed-off-by: Andrew Park <andrew.park@intel.com>

--- src/plugins/intel_gpu/src/graph/impls/ocl/kernel_selector_helper.h
+++ src/plugins/intel_gpu/src/graph/impls/ocl/kernel_selector_helper.h
@@ -263,7 +263,9 @@ inline kernel_impl_params canonicalize_fused_shapes(const kernel_impl_params& im
                 auto dep_shape = dep_layout.get_partial_shape();
 
                 if (!broadcastable(dep_shape, out_pshape, use_new_shape_infer)) {
-                    dep_layout.set_partial_shape(extend_shape_to_rank_from_begin(dep_shape, out_pshape.size()));
+                    // In particular, as in the case of 1D tensor,
+                    // the shape should be expanded by rank from the end to avoid unintentional extension
+                    dep_layout.set_partial_shape(extend_shape_to_rank_from_end(dep_shape, out_pshape.size()));
                 }
             }
         }
