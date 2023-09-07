6f2d6847daf2a23ebc142c9a528e87fac0378fba
Oleg Pipikin <oleg.pipikin@intel.com>
2023-09-07 21:12:36 +0400

    Refactor ComparisonLayerTest

---
+++ src/tests/functional/shared_test_classes/include/shared_test_classes/single_op/comparison.hpp
@@ -0,0 +1,39 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+
+#include <map>
+
+#include "gtest/gtest.h"
+#include "shared_test_classes/base/ov_subgraph.hpp"
+#include "common_test_utils/test_constants.hpp"
+#include "ngraph_functions/utils/ngraph_helpers.hpp"
+
+namespace ov {
+namespace test {
+
+typedef std::tuple<
+    std::vector<InputShape>,             // Input shapes tuple
+    ov::element::Type,                   // NG Inputs precision
+    ngraph::helpers::ComparisonTypes,    // Comparison op type
+    ngraph::helpers::InputLayerType,     // Second input type
+    ov::element::Type,                   // IE in precision
+    ov::element::Type,                   // IE out precision
+    std::string,                         // Device name
+    std::map<std::string, std::string>   // Additional network configuration
+> ComparisonTestParams;
+
+class ComparisonLayerTest : public testing::WithParamInterface<ComparisonTestParams>,
+    virtual public ov::test::SubgraphBaseTest {
+    ngraph::helpers::ComparisonTypes comparison_op_type;
+protected:
+    void SetUp() override;
+    void generate_inputs(const std::vector<ov::Shape>& targetInputStaticShapes) override;
+public:
+    static std::string getTestCaseName(const testing::TestParamInfo<ComparisonTestParams> &obj);
+};
+} // namespace test
+} // namespace ov
