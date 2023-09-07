908ae1d94dbc10856e18670238feb8083d391ba3
Raasz, Pawel <pawel.raasz@intel.com>
2023-09-07 11:40:22 +0200

    Hide helper functions from public API

--- src/core/src/shape_util.cpp
+++ src/core/src/shape_util.cpp
@@ -77,6 +77,30 @@ PartialShape ngraph::inject_pairs(const PartialShape& shape,
 }
 
 namespace ov {
+template <class TContainer, class TAxes>
+TContainer reduce_container(const TContainer& input, const TAxes& axes) {
+    TContainer result;
+    const auto input_size = input.size();
+    result.reserve(input_size);
+
+    for (size_t axis = 0; axis < input_size; ++axis) {
+        if (std::find(axes.begin(), axes.end(), axis) == axes.end()) {
+            result.emplace_back(input[axis]);
+        }
+    }
+
+    return result;
+}
+
+template <class TContainer, class TAxes>
+TContainer replace_container(const TContainer& input, const TAxes& axes) {
+    auto result = input;
+    for (auto&& axis : axes) {
+        result[axis] = 1;
+    }
+    return result;
+}
+
 namespace util {
 Shape make_dynamic_shape() {
     return Shape{0, std::numeric_limits<size_t>::max()};
