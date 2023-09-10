7dc3fdc0a99ebfad3e2f521623d1c02a682f408c
Roman Kazantsev <roman.kazantsev@intel.com>
2023-09-10 07:46:01 +0400

    [TF FE][TF Hub] Use ConcreteFunc input and output signatures (#19690)
    * [TF Hub][TF FE] Preserve outputs of ConcreteFunction from signature and their names
    
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>
    
    * Fix naming and complete TODO
    
    * Apply code-review: extra assert to check input_signature
    
    * Fix inputs for fw
    
    * Fix input data preparation and import convert_model
    
    * Correct variable detection among all inputs
    
    * Handle special input and output signature
    
    * Fix adjust_saved_model_names
    
    ---------
    
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/bindings/python/src/pyopenvino/frontend/tensorflow/graph_iterator.hpp
+++ src/bindings/python/src/pyopenvino/frontend/tensorflow/graph_iterator.hpp
@@ -5,8 +5,9 @@
 #pragma once
 
 #include <pybind11/pybind11.h>
-#include "openvino/frontend/graph_iterator.hpp"
+
 #include "openvino/frontend/decoder.hpp"
+#include "openvino/frontend/graph_iterator.hpp"
 
 namespace py = pybind11;
 
@@ -14,9 +15,10 @@ namespace py = pybind11;
 class PyGraphIterator : public ov::frontend::tensorflow::GraphIterator {
     /* Inherit the constructors */
     using ov::frontend::tensorflow::GraphIterator::GraphIterator;
+    using map_str_to_str = std::map<std::string, std::string>;
 
     /// \brief Get a number of operation nodes in the graph
-    size_t size() const override{
+    size_t size() const override {
         PYBIND11_OVERRIDE_PURE(size_t, GraphIterator, size);
     }
 
@@ -30,7 +32,8 @@ class PyGraphIterator : public ov::frontend::tensorflow::GraphIterator {
         next_impl();
     }
 
-    /// Implementation of next method, it is needed to be in separate method to avoid shadowing of Python "next" operator.
+    /// Implementation of next method, it is needed to be in separate method to avoid shadowing of Python "next"
+    /// operator.
     void next_impl() {
         PYBIND11_OVERRIDE_PURE(void, GraphIterator, next_impl);
     }
@@ -41,25 +44,34 @@ class PyGraphIterator : public ov::frontend::tensorflow::GraphIterator {
     }
 
     /// \brief Return a pointer to a decoder of the current node
-    std::shared_ptr<ov::frontend::DecoderBase> get_decoder() const override{
+    std::shared_ptr<ov::frontend::DecoderBase> get_decoder() const override {
         PYBIND11_OVERRIDE_PURE(std::shared_ptr<ov::frontend::DecoderBase>, GraphIterator, get_decoder);
     }
 
     /// \brief Checks if the main model graph contains a function of the requested name in the library
     /// Returns GraphIterator to this function and nullptr, if it does not exist
-    std::shared_ptr<GraphIterator> get_body_graph_iterator(const std::string& func_name) const override{
+    std::shared_ptr<GraphIterator> get_body_graph_iterator(const std::string& func_name) const override {
         PYBIND11_OVERRIDE_PURE(std::shared_ptr<GraphIterator>, GraphIterator, get_body_graph_iterator, func_name);
     }
 
     /// \brief Returns a vector of input names in the original order
-    std::vector<std::string> get_input_names() const override{
+    std::vector<std::string> get_input_names() const override {
         PYBIND11_OVERRIDE_PURE(std::vector<std::string>, GraphIterator, get_input_names);
-
     }
 
     /// \brief Returns a vector of output names in the original order
-    std::vector<std::string> get_output_names() const override{
+    std::vector<std::string> get_output_names() const override {
         PYBIND11_OVERRIDE_PURE(std::vector<std::string>, GraphIterator, get_output_names);
+    }
+
+    /// \brief Returns a map from internal tensor name to (user-defined) external name for inputs
+    map_str_to_str get_input_names_map() const override {
+        PYBIND11_OVERRIDE_PURE(map_str_to_str, GraphIterator, get_input_names_map);
+    }
+
+    /// \brief Returns a map from internal tensor name to (user-defined) external name for outputs
+    map_str_to_str get_output_names_map() const override {
+        PYBIND11_OVERRIDE_PURE(map_str_to_str, GraphIterator, get_output_names_map);
     }
 };
 
