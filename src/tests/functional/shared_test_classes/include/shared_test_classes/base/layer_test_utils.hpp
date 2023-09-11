1a9468611f1602b9c83940987633c6fda8fcc42f
Ilya Churaev <ilyachur@gmail.com>
2023-09-11 16:13:40 +0400

    Fixed more issues

--- src/tests/functional/shared_test_classes/include/shared_test_classes/base/layer_test_utils.hpp
+++ src/tests/functional/shared_test_classes/include/shared_test_classes/base/layer_test_utils.hpp
@@ -11,6 +11,8 @@
 #include <tuple>
 #include <gtest/gtest.h>
 #include <ngraph/node.hpp>
+#include <ngraph/opsets/opset1.hpp>
+#include <ngraph/opsets/opset2.hpp>
 #include <ngraph/opsets/opset3.hpp>
 #include <ngraph/opsets/opset4.hpp>
 #include <ngraph/opsets/opset5.hpp>
