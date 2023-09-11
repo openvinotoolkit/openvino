d1eb0d3241efa0c066cda3046835ef666485f82b
Vishniakov Nikolai <nikolai.vishniakov@intel.com>
2023-09-11 15:18:45 +0200

    Wrap tensor constructor body in try block
    Co-authored-by: Alicja Miloszewska <alicja.miloszewska@intel.com>

--- src/bindings/js/node/src/tensor.cpp
+++ src/bindings/js/node/src/tensor.cpp
@@ -6,33 +6,35 @@
 #include <iostream>
 
 TensorWrap::TensorWrap(const Napi::CallbackInfo& info) : Napi::ObjectWrap<TensorWrap>(info) {
-    if (info.Length() == 2) {
-        const auto type = js_to_cpp<ov::element::Type_t>(info, 0, {napi_string});
-        const auto shape_vec =
+    try {
+        if (info.Length() == 0) {
+            ;  // default contructor takes 0 args
+        } else if (info.Length() == 2) {
+            const auto type = js_to_cpp<ov::element::Type_t>(info, 0, {napi_string});
+            const auto shape_vec =
                 js_to_cpp<std::vector<size_t>>(info, 1, {napi_int32_array, napi_uint32_array, js_array});
-        const auto& shape = ov::Shape(shape_vec);
+            const auto& shape = ov::Shape(shape_vec);
 
-        this->_tensor = ov::Tensor(type, shape);
-    }
-    else if (info.Length() != 3 && info.Length() != 0)  // default contructor takes 0 args
-        reportError(info.Env(), "Invalid number of arguments for Tensor constructor.");
-    else if (info.Length() == 3) {
-        if (!info[2].IsTypedArray()) {
-            reportError(info.Env(), "Third argument of a tensor must be of type TypedArray.");
-            return;
-        }
-        try {
+            this->_tensor = ov::Tensor(type, shape);
+        } else if (info.Length() == 3) {
+            if (!info[2].IsTypedArray()) {
+                reportError(info.Env(), "Third argument of a tensor must be of type TypedArray.");
+                return;
+            }
             const auto type = js_to_cpp<ov::element::Type_t>(info, 0, {napi_string});
             const auto shape_vec =
                 js_to_cpp<std::vector<size_t>>(info, 1, {napi_int32_array, napi_uint32_array, js_array});
             const auto& shape = ov::Shape(shape_vec);
             const auto data = info[2].As<Napi::TypedArray>();
+
             this->_tensor = cast_to_tensor(data, shape, type);
-        } catch (std::invalid_argument& e) {
-            reportError(info.Env(), std::string("Invalid tensor argument. ") + e.what());
-        } catch (std::exception& e) {
-            reportError(info.Env(), e.what());
+        } else {
+            reportError(info.Env(), "Invalid number of arguments for Tensor constructor.");
         }
+    } catch (std::invalid_argument& e) {
+        reportError(info.Env(), std::string("Invalid tensor argument. ") + e.what());
+    } catch (std::exception& e) {
+        reportError(info.Env(), e.what());
     }
 }
 
