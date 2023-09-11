z71258847/openvino_sycl_poc

[GPU] Compression support for FC

src/plugins/intel_gpu/include/intel_gpu/primitives/fully_connected.hpp
@@ -76,10 +76,42 @@ struct fully_connected : public primitive_base<fully_connected> {
           weights_rank(weights_rank)
     {}
 
+    /// @brief Constructs fully connected compressed layer.
+    /// @param id This primitive id.
+    /// @param input Input primitive id.
+    /// @param weights Primitive id containing weights data.
+    /// @param bias Primitive id containing bias data.
+    /// @param compression_scale Primitive id containing scale factors for weights decompression.
+    /// @param compression_zero_point Primitive id containing zero points for weights decompression.
+    fully_connected(const primitive_id& id,
+                    const input_info& input,
+                    const primitive_id& weights,
+                    const primitive_id& bias,
+                    const primitive_id& compression_scale,
+                    const primitive_id& compression_zero_point,
+                    const data_types data_type,
+                    const padding& output_padding = padding(),
+                    const size_t input_size = 2,
+                    const size_t weights_rank = 2)
+        : primitive_base(id, { input }, {output_padding}, {optional_data_type{data_type}}),
+          weights(weights),
+          bias(bias),
+          compressed_weights(true),
+          compression_scale(compression_scale),
+          compression_zero_point(compression_zero_point),
+          input_size(input_size),
+          weights_rank(weights_rank)
+    {}
+
     /// @brief Primitive id containing weights data.
     primitive_id weights;
     /// @brief Primitive id containing bias data.
     primitive_id bias;
+
+    bool compressed_weights = false;
+    primitive_id compression_scale = "";
+    primitive_id compression_zero_point = "";
+
     /// @brief Primitive dimension size.
     size_t input_size = 2;
     /// @brief Primitive weights rank.
@@ -90,6 +122,9 @@ struct fully_connected : public primitive_base<fully_connected> {
         seed = hash_combine(seed, input_size);
         seed = hash_combine(seed, weights_rank);
         seed = hash_combine(seed, bias.empty());
+        seed = hash_combine(seed, compressed_weights);
+        seed = hash_combine(seed, !compression_scale.empty());
+        seed = hash_combine(seed, !compression_zero_point.empty());
         return seed;
     }
 
@@ -108,6 +143,9 @@ struct fully_connected : public primitive_base<fully_connected> {
         primitive_base<fully_connected>::save(ob);
         ob << weights;
         ob << bias;
+        ob << compressed_weights;
+        ob << compression_scale;
+        ob << compression_zero_point;
         ob << input_size;
         ob << weights_rank;
     }
@@ -116,6 +154,9 @@ struct fully_connected : public primitive_base<fully_connected> {
         primitive_base<fully_connected>::load(ib);
         ib >> weights;
         ib >> bias;
+        ib >> compressed_weights;
+        ib >> compression_scale;
+        ib >> compression_zero_point;
         ib >> input_size;
         ib >> weights_rank;
     }
@@ -127,6 +168,12 @@ protected:
 
         if (!bias.empty())
             ret.push_back(bias);
+
+        if (!compression_scale.empty())
+            ret.push_back(compression_scale);
+
+        if (!compression_zero_point.empty())
+            ret.push_back(compression_zero_point);
 
         return ret;
     }

src/plugins/intel_gpu/src/graph/fully_connected.cpp
@@ -214,6 +214,11 @@ std::string fully_connected_inst::to_string(fully_connected_node const& node) {
     json_composite fc_info;
     fc_info.add("weights id", weights_id);
     fc_info.add("bias id", bias_id);
+    fc_info.add("compressed weights", desc->compressed_weights ? "true" : "false");
+    if (desc->compressed_weights) {
+        fc_info.add("compression scale id", desc->compression_scale);
+        fc_info.add("compression zp id", desc->compression_zero_point);
+    }
 
     node_info->add("fully connected info", fc_info);
     node_info->dump(primitive_description);

src/plugins/intel_gpu/src/graph/graph_optimizer/prepare_primitive_fusing.cpp
@@ -466,6 +466,11 @@ void prepare_primitive_fusing::fuse_bias(program &p) {
                                                                        desc->output_paddings[0],
                                                                        desc->input_size);
 
+            if (desc->compressed_weights) {
+                fc_with_bias_prim->compressed_weights = true;
+                fc_with_bias_prim->compression_scale = desc->compression_scale;
+                fc_with_bias_prim->compression_zero_point = desc->compression_zero_point;
+            }
             auto& new_fc_node = p.get_or_create(fc_with_bias_prim);
             fuse_bias_f(fc, new_fc_node, bias_node, eltw_node);
         }

src/plugins/intel_gpu/src/plugin/transformations/convert_fc_to_compressed.cpp
@@ -1,0 +1,106 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "convert_fc_to_compressed.hpp"
+
+#include "intel_gpu/op/fully_connected.hpp"
+#include "intel_gpu/op/fully_connected_compressed.hpp"
+
+#include "openvino/op/subtract.hpp"
+#include "openvino/op/matmul.hpp"
+#include "openvino/op/convert.hpp"
+#include "openvino/op/transpose.hpp"
+#include "openvino/op/reshape.hpp"
+#include "openvino/core/rt_info.hpp"
+#include "openvino/pass/pattern/op/wrap_type.hpp"
+#include "openvino/pass/pattern/op/or.hpp"
+#include "transformations/utils/utils.hpp"
+
+namespace ov {
+namespace intel_gpu {
+
+ConvertFullyConnectedToFullyConnectedCompressed::ConvertFullyConnectedToFullyConnectedCompressed() {
+    using namespace ov::pass::pattern;
+
+    auto weights_m = wrap_type<ov::op::v0::Constant>(consumers_count(1));
+    auto convert_m = wrap_type<ov::op::v0::Convert>({weights_m});
+
+    auto sub_const_m = wrap_type<ov::op::v0::Constant>(consumers_count(1));
+    auto subtract_m = wrap_type<ov::op::v1::Subtract>({convert_m, sub_const_m});
+
+    auto mul_const_m = wrap_type<ov::op::v0::Constant>(consumers_count(1));
+    auto mul_with_sub_m = wrap_type<ov::op::v1::Multiply>({subtract_m, mul_const_m});
+    auto mul_no_sub_m = wrap_type<ov::op::v1::Multiply>({convert_m, mul_const_m});
+    auto mul_m = std::make_shared<ov::pass::pattern::op::Or>(OutputVector{mul_with_sub_m, mul_no_sub_m});
+
+    auto transpose_const_m = wrap_type<ov::op::v0::Constant>();
+    auto transpose_m = wrap_type<ov::op::v1::Transpose>({mul_m, transpose_const_m});
+    auto weights_input_m = std::make_shared<ov::pass::pattern::op::Or>(ov::OutputVector{mul_m, transpose_m});
+
+    auto data_m = any_input();
+    auto fully_connected_m = wrap_type<op::FullyConnected>({data_m, weights_input_m});
+
+    ov::matcher_pass_callback callback = [=](ov::pass::pattern::Matcher& m) {
+        const auto& pattern_map = m.get_pattern_value_map();
+        OPENVINO_ASSERT(pattern_map.count(fully_connected_m));
+        OPENVINO_ASSERT(pattern_map.count(mul_const_m));
+        OPENVINO_ASSERT(pattern_map.count(weights_m));
+        OPENVINO_ASSERT(pattern_map.count(convert_m));
+        auto fc = std::dynamic_pointer_cast<op::FullyConnected>(pattern_map.at(fully_connected_m).get_node_shared_ptr());
+        if (!fc || transformation_callback(fc)) {
+            return false;
+        }
+
+        const auto& fc_input_a = fc->get_input_node_shared_ptr(0);
+        const auto& scale = pattern_map.at(mul_const_m).get_node_shared_ptr();
+        std::shared_ptr<ov::Node> optional_shift = nullptr;
+
+        ov::NodeVector nodes_to_copy_info{pattern_map.at(fully_connected_m).get_node_shared_ptr(),
+                                          pattern_map.at(convert_m).get_node_shared_ptr()};
+        if (pattern_map.count(mul_no_sub_m)) {
+            nodes_to_copy_info.push_back(pattern_map.at(mul_no_sub_m).get_node_shared_ptr());
+        }
+        if (pattern_map.count(mul_with_sub_m)) {
+            nodes_to_copy_info.push_back(pattern_map.at(mul_with_sub_m).get_node_shared_ptr());
+        }
+
+        const bool with_zero_point = pattern_map.count(subtract_m) > 0;
+        if (with_zero_point) {
+            optional_shift = pattern_map.at(sub_const_m).get_node_shared_ptr();
+            nodes_to_copy_info.push_back(subtract_m);
+        }
+
+        std::shared_ptr<ov::Node> fc_input_b = pattern_map.at(weights_m).get_node_shared_ptr();
+        if (pattern_map.count(transpose_m)) {
+            const auto& transpose = pattern_map.at(transpose_m).get_node_shared_ptr();
+            const auto& transpose_const = pattern_map.at(transpose_const_m).get_node_shared_ptr();
+            fc_input_b = transpose->clone_with_new_inputs({ fc_input_b->output(0), transpose_const });
+        }
+
+        std::shared_ptr<ov::Node> new_fc = nullptr;
+        if (with_zero_point) {
+            new_fc = std::make_shared<op::FullyConnectedCompressed>(fc_input_a,
+                                                                    fc_input_b,
+                                                                    scale,
+                                                                    optional_shift,
+                                                                    fc->get_output_type());
+        } else {
+            new_fc = std::make_shared<op::FullyConnectedCompressed>(fc_input_a,
+                                                                    fc_input_b,
+                                                                    scale,
+                                                                    fc->get_output_type());
+        }
+
+        new_fc->set_friendly_name(fc->get_friendly_name());
+        ov::copy_runtime_info(nodes_to_copy_info, new_fc);
+        ov::replace_node(fc, new_fc);
+        return true;
+    };
+
+    auto m = std::make_shared<ov::pass::pattern::Matcher>(fully_connected_m);
+    this->register_matcher(m, callback);
+}
+
+}  // namespace intel_gpu
+}  // namespace ov

src/plugins/intel_gpu/src/plugin/transformations/op/fully_connected_compressed.cpp
@@ -1,0 +1,51 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "intel_gpu/op/fully_connected_compressed.hpp"
+
+namespace ov {
+namespace intel_gpu {
+namespace op {
+
+FullyConnectedCompressed::FullyConnectedCompressed(const ov::Output<Node>& A,
+                                                   const ov::Output<Node>& B,
+                                                   const ov::Output<Node>& scale,
+                                                   const ov::Output<Node>& shift,
+                                                   const ov::element::Type output_type)
+    : FullyConnected(A, B, output_type) {
+    set_argument(2, scale);
+    set_argument(3, shift);
+    validate_and_infer_types();
+}
+
+FullyConnectedCompressed::FullyConnectedCompressed(const ov::Output<Node>& A,
+                                                   const ov::Output<Node>& B,
+                                                   const ov::Output<Node>& scale,
+                                                   const ov::element::Type output_type)
+    : FullyConnected(A, B, output_type) {
+    set_argument(2, scale);
+    validate_and_infer_types();
+}
+
+std::shared_ptr<ov::Node> FullyConnectedCompressed::clone_with_new_inputs(const ov::OutputVector& new_args) const {
+    check_new_args_count(this, new_args);
+
+    if (new_args.size() == 3)
+        return std::make_shared<FullyConnectedCompressed>(new_args.at(0),
+                                                          new_args.at(1),
+                                                          new_args.at(2),
+                                                          m_output_type);
+    else if (new_args.size() == 4)
+        return std::make_shared<FullyConnectedCompressed>(new_args.at(0),
+                                                          new_args.at(1),
+                                                          new_args.at(2),
+                                                          new_args.at(3),
+                                                          m_output_type);
+    else
+        OPENVINO_THROW("Unexpected inputs count for FullyConnectedCompressed op: ", new_args.size());
+}
+
+}  // namespace op
+}  // namespace intel_gpu
+}  // namespace ov

openvinotoolkit/openvino

src/plugins/intel_gpu/include/intel_gpu/primitives/fully_connected.hpp
@@ -76,10 +76,42 @@ struct fully_connected : public primitive_base<fully_connected> {
           weights_rank(weights_rank)
     {}
 
+    /// @brief Constructs fully connected compressed layer.
+    /// @param id This primitive id.
+    /// @param input Input primitive id.
+    /// @param weights Primitive id containing weights data.
+    /// @param bias Primitive id containing bias data.
+    /// @param compression_scale Primitive id containing scale factors for weights decompression.
+    /// @param compression_zero_point Primitive id containing zero points for weights decompression.
+    fully_connected(const primitive_id& id,
+                    const input_info& input,
+                    const primitive_id& weights,
+                    const primitive_id& bias,
+                    const primitive_id& decompression_scale,
+                    const primitive_id& decompression_zero_point,
+                    const data_types data_type,
+                    const padding& output_padding = padding(),
+                    const size_t input_size = 2,
+                    const size_t weights_rank = 2)
+        : primitive_base(id, { input }, {output_padding}, {optional_data_type{data_type}}),
+          weights(weights),
+          bias(bias),
+          compressed_weights(true),
+          decompression_scale(decompression_scale),
+          decompression_zero_point(decompression_zero_point),
+          input_size(input_size),
+          weights_rank(weights_rank)
+    {}
+
     /// @brief Primitive id containing weights data.
     primitive_id weights;
     /// @brief Primitive id containing bias data.
     primitive_id bias;
+
+    bool compressed_weights = false;
+    primitive_id decompression_scale = "";
+    primitive_id decompression_zero_point = "";
+
     /// @brief Primitive dimension size.
     size_t input_size = 2;
     /// @brief Primitive weights rank.
@@ -90,6 +122,9 @@ struct fully_connected : public primitive_base<fully_connected> {
         seed = hash_combine(seed, input_size);
         seed = hash_combine(seed, weights_rank);
         seed = hash_combine(seed, bias.empty());
+        seed = hash_combine(seed, compressed_weights);
+        seed = hash_combine(seed, !decompression_scale.empty());
+        seed = hash_combine(seed, !decompression_zero_point.empty());
         return seed;
     }
 
@@ -108,6 +143,9 @@ struct fully_connected : public primitive_base<fully_connected> {
         primitive_base<fully_connected>::save(ob);
         ob << weights;
         ob << bias;
+        ob << compressed_weights;
+        ob << decompression_scale;
+        ob << decompression_zero_point;
         ob << input_size;
         ob << weights_rank;
     }
@@ -116,6 +154,9 @@ struct fully_connected : public primitive_base<fully_connected> {
         primitive_base<fully_connected>::load(ib);
         ib >> weights;
         ib >> bias;
+        ib >> compressed_weights;
+        ib >> decompression_scale;
+        ib >> decompression_zero_point;
         ib >> input_size;
         ib >> weights_rank;
     }
@@ -127,6 +168,12 @@ protected:
 
         if (!bias.empty())
             ret.push_back(bias);
+
+        if (!decompression_scale.empty())
+            ret.push_back(decompression_scale);
+
+        if (!decompression_zero_point.empty())
+            ret.push_back(decompression_zero_point);
 
         return ret;
     }

src/plugins/intel_gpu/src/graph/fully_connected.cpp
@@ -217,6 +217,11 @@ std::string fully_connected_inst::to_string(fully_connected_node const& node) {
     json_composite fc_info;
     fc_info.add("weights id", weights_id);
     fc_info.add("bias id", bias_id);
+    fc_info.add("compressed weights", desc->compressed_weights ? "true" : "false");
+    if (desc->compressed_weights) {
+        fc_info.add("decompression scale id", desc->decompression_scale);
+        fc_info.add("decompression zp id", desc->decompression_zero_point);
+    }
 
     node_info->add("fully connected info", fc_info);
     node_info->dump(primitive_description);

src/plugins/intel_gpu/src/graph/graph_optimizer/prepare_primitive_fusing.cpp
@@ -466,6 +466,11 @@ void prepare_primitive_fusing::fuse_bias(program &p) {
                                                                        desc->output_paddings[0],
                                                                        desc->input_size);
 
+            if (desc->compressed_weights) {
+                fc_with_bias_prim->compressed_weights = true;
+                fc_with_bias_prim->decompression_scale = desc->decompression_scale;
+                fc_with_bias_prim->decompression_zero_point = desc->decompression_zero_point;
+            }
             auto& new_fc_node = p.get_or_create(fc_with_bias_prim);
             fuse_bias_f(fc, new_fc_node, bias_node, eltw_node);
         }

src/plugins/intel_gpu/src/plugin/transformations/convert_fc_to_compressed.cpp
@@ -1,0 +1,106 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "convert_fc_to_compressed.hpp"
+
+#include "intel_gpu/op/fully_connected.hpp"
+#include "intel_gpu/op/fully_connected_compressed.hpp"
+
+#include "openvino/op/subtract.hpp"
+#include "openvino/op/matmul.hpp"
+#include "openvino/op/convert.hpp"
+#include "openvino/op/transpose.hpp"
+#include "openvino/op/reshape.hpp"
+#include "openvino/core/rt_info.hpp"
+#include "openvino/pass/pattern/op/wrap_type.hpp"
+#include "openvino/pass/pattern/op/or.hpp"
+#include "transformations/utils/utils.hpp"
+
+namespace ov {
+namespace intel_gpu {
+
+ConvertFullyConnectedToFullyConnectedCompressed::ConvertFullyConnectedToFullyConnectedCompressed() {
+    using namespace ov::pass::pattern;
+
+    auto weights_m = wrap_type<ov::op::v0::Constant>(consumers_count(1));
+    auto convert_m = wrap_type<ov::op::v0::Convert>({weights_m});
+
+    auto sub_const_m = wrap_type<ov::op::v0::Constant>(consumers_count(1));
+    auto subtract_m = wrap_type<ov::op::v1::Subtract>({convert_m, sub_const_m});
+
+    auto mul_const_m = wrap_type<ov::op::v0::Constant>(consumers_count(1));
+    auto mul_with_sub_m = wrap_type<ov::op::v1::Multiply>({subtract_m, mul_const_m});
+    auto mul_no_sub_m = wrap_type<ov::op::v1::Multiply>({convert_m, mul_const_m});
+    auto mul_m = std::make_shared<ov::pass::pattern::op::Or>(OutputVector{mul_with_sub_m, mul_no_sub_m});
+
+    auto transpose_const_m = wrap_type<ov::op::v0::Constant>();
+    auto transpose_m = wrap_type<ov::op::v1::Transpose>({mul_m, transpose_const_m});
+    auto weights_input_m = std::make_shared<ov::pass::pattern::op::Or>(ov::OutputVector{mul_m, transpose_m});
+
+    auto data_m = any_input();
+    auto fully_connected_m = wrap_type<op::FullyConnected>({data_m, weights_input_m});
+
+    ov::matcher_pass_callback callback = [=](ov::pass::pattern::Matcher& m) {
+        const auto& pattern_map = m.get_pattern_value_map();
+        OPENVINO_ASSERT(pattern_map.count(fully_connected_m));
+        OPENVINO_ASSERT(pattern_map.count(mul_const_m));
+        OPENVINO_ASSERT(pattern_map.count(weights_m));
+        OPENVINO_ASSERT(pattern_map.count(convert_m));
+        auto fc = std::dynamic_pointer_cast<op::FullyConnected>(pattern_map.at(fully_connected_m).get_node_shared_ptr());
+        if (!fc || transformation_callback(fc)) {
+            return false;
+        }
+
+        const auto& fc_input_a = fc->get_input_node_shared_ptr(0);
+        const auto& scale = pattern_map.at(mul_const_m).get_node_shared_ptr();
+        std::shared_ptr<ov::Node> optional_zero_point = nullptr;
+
+        ov::NodeVector nodes_to_copy_info{pattern_map.at(fully_connected_m).get_node_shared_ptr(),
+                                          pattern_map.at(convert_m).get_node_shared_ptr()};
+        if (pattern_map.count(mul_no_sub_m)) {
+            nodes_to_copy_info.push_back(pattern_map.at(mul_no_sub_m).get_node_shared_ptr());
+        }
+        if (pattern_map.count(mul_with_sub_m)) {
+            nodes_to_copy_info.push_back(pattern_map.at(mul_with_sub_m).get_node_shared_ptr());
+        }
+
+        const bool with_zero_point = pattern_map.count(subtract_m) > 0;
+        if (with_zero_point) {
+            optional_zero_point = pattern_map.at(sub_const_m).get_node_shared_ptr();
+            nodes_to_copy_info.push_back(subtract_m);
+        }
+
+        std::shared_ptr<ov::Node> fc_input_b = pattern_map.at(weights_m).get_node_shared_ptr();
+        if (pattern_map.count(transpose_m)) {
+            const auto& transpose = pattern_map.at(transpose_m).get_node_shared_ptr();
+            const auto& transpose_const = pattern_map.at(transpose_const_m).get_node_shared_ptr();
+            fc_input_b = transpose->clone_with_new_inputs({ fc_input_b->output(0), transpose_const });
+        }
+
+        std::shared_ptr<ov::Node> new_fc = nullptr;
+        if (with_zero_point) {
+            new_fc = std::make_shared<op::FullyConnectedCompressed>(fc_input_a,
+                                                                    fc_input_b,
+                                                                    scale,
+                                                                    optional_zero_point,
+                                                                    fc->get_output_type());
+        } else {
+            new_fc = std::make_shared<op::FullyConnectedCompressed>(fc_input_a,
+                                                                    fc_input_b,
+                                                                    scale,
+                                                                    fc->get_output_type());
+        }
+
+        new_fc->set_friendly_name(fc->get_friendly_name());
+        ov::copy_runtime_info(nodes_to_copy_info, new_fc);
+        ov::replace_node(fc, new_fc);
+        return true;
+    };
+
+    auto m = std::make_shared<ov::pass::pattern::Matcher>(fully_connected_m);
+    this->register_matcher(m, callback);
+}
+
+}  // namespace intel_gpu
+}  // namespace ov

src/plugins/intel_gpu/src/plugin/transformations/op/fully_connected_compressed.cpp
@@ -1,0 +1,51 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "intel_gpu/op/fully_connected_compressed.hpp"
+
+namespace ov {
+namespace intel_gpu {
+namespace op {
+
+FullyConnectedCompressed::FullyConnectedCompressed(const ov::Output<Node>& A,
+                                                   const ov::Output<Node>& B,
+                                                   const ov::Output<Node>& decompression_scale,
+                                                   const ov::Output<Node>& decompression_zero_point,
+                                                   const ov::element::Type output_type)
+    : FullyConnected(A, B, output_type) {
+    set_argument(2, decompression_scale);
+    set_argument(3, decompression_zero_point);
+    validate_and_infer_types();
+}
+
+FullyConnectedCompressed::FullyConnectedCompressed(const ov::Output<Node>& A,
+                                                   const ov::Output<Node>& B,
+                                                   const ov::Output<Node>& decompression_scale,
+                                                   const ov::element::Type output_type)
+    : FullyConnected(A, B, output_type) {
+    set_argument(2, decompression_scale);
+    validate_and_infer_types();
+}
+
+std::shared_ptr<ov::Node> FullyConnectedCompressed::clone_with_new_inputs(const ov::OutputVector& new_args) const {
+    check_new_args_count(this, new_args);
+
+    if (new_args.size() == 3)
+        return std::make_shared<FullyConnectedCompressed>(new_args.at(0),
+                                                          new_args.at(1),
+                                                          new_args.at(2),
+                                                          m_output_type);
+    else if (new_args.size() == 4)
+        return std::make_shared<FullyConnectedCompressed>(new_args.at(0),
+                                                          new_args.at(1),
+                                                          new_args.at(2),
+                                                          new_args.at(3),
+                                                          m_output_type);
+    else
+        OPENVINO_THROW("Unexpected inputs count for FullyConnectedCompressed op: ", new_args.size());
+}
+
+}  // namespace op
+}  // namespace intel_gpu
+}  // namespace ov