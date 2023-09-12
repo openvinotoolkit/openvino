398431a8bc4f5926fa5741db9fe585570e93f857
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2023-09-12 11:13:59 +0400

    [GPU] 2.0 plugin api impl (#18920)

--- src/plugins/intel_gpu/include/intel_gpu/plugin/compiled_model.hpp
+++ src/plugins/intel_gpu/include/intel_gpu/plugin/compiled_model.hpp
@@ -4,51 +4,70 @@
 
 #pragma once
 
+#include "openvino/runtime/icompiled_model.hpp"
+#include "intel_gpu/plugin/graph.hpp"
+#include "intel_gpu/plugin/plugin.hpp"
+#include "intel_gpu/plugin/remote_context.hpp"
+#include "intel_gpu/runtime/execution_config.hpp"
+
 #include <vector>
 #include <map>
 #include <set>
 #include <memory>
 #include <string>
 #include <utility>
-#include "ie_blob.h"
-#include "cpp/ie_cnn_network.h"
-#include <cpp_interfaces/impl/ie_executable_network_thread_safe_default.hpp>
-#include "intel_gpu/plugin/graph.hpp"
-#include "intel_gpu/plugin/remote_context.hpp"
-#include "intel_gpu/runtime/execution_config.hpp"
 
 namespace ov {
 namespace intel_gpu {
 
-class CompiledModel : public InferenceEngine::ExecutableNetworkThreadSafeDefault {
+class CompiledModel : public ov::ICompiledModel {
 public:
-    typedef std::shared_ptr<CompiledModel> Ptr;
+    using Ptr = std::shared_ptr<CompiledModel>;
 
-    CompiledModel(InferenceEngine::CNNNetwork &network, InferenceEngine::RemoteContext::Ptr context, const ExecutionConfig& config,
-                  InferenceEngine::InputsDataMap* inputs = nullptr, InferenceEngine::OutputsDataMap* outputs = nullptr);
-    CompiledModel(cldnn::BinaryInputBuffer& ib, InferenceEngine::RemoteContext::Ptr context, const ExecutionConfig& config,
-                  InferenceEngine::InputsDataMap* inputs = nullptr, InferenceEngine::OutputsDataMap* outputs = nullptr);
+    CompiledModel(std::shared_ptr<ov::Model> model,
+                  const std::shared_ptr<const ov::IPlugin>& plugin,
+                  RemoteContextImpl::Ptr context,
+                  const ExecutionConfig& config);
+    CompiledModel(cldnn::BinaryInputBuffer ib,
+                  const std::shared_ptr<const ov::IPlugin>& plugin,
+                  RemoteContextImpl::Ptr context,
+                  const ExecutionConfig& config);
 
-    void Export(std::ostream& networkModel) override;
-    std::shared_ptr<ngraph::Function> GetExecGraphInfo() override;
-    InferenceEngine::IInferRequestInternal::Ptr CreateInferRequest() override;
-    InferenceEngine::IInferRequestInternal::Ptr CreateInferRequestImpl(InferenceEngine::InputsDataMap networkInputs,
-                                                                       InferenceEngine::OutputsDataMap networkOutputs) override;
-    InferenceEngine::IInferRequestInternal::Ptr CreateInferRequestImpl(const std::vector<std::shared_ptr<const ov::Node>>& inputs,
-                                                                       const std::vector<std::shared_ptr<const ov::Node>>& outputs) override;
-    template <class T>
-    InferenceEngine::IInferRequestInternal::Ptr GetInferRequestImpl(const std::vector<std::shared_ptr<const ov::Node>>& inputs,
-                                                                    const std::vector<std::shared_ptr<const ov::Node>>& outputs);
-    InferenceEngine::Parameter GetMetric(const std::string &name) const override;
-    InferenceEngine::Parameter GetConfig(const std::string &name) const override;
-    std::shared_ptr<InferenceEngine::RemoteContext> GetContext() const override;
+    std::shared_ptr<ov::IAsyncInferRequest> create_infer_request() const override;
+    std::shared_ptr<ov::ISyncInferRequest> create_sync_infer_request() const override;
 
-    std::vector<std::shared_ptr<Graph>> m_graphs;
-    InferenceEngine::RemoteContext::Ptr m_context;
+    void export_model(std::ostream& model) const override;
+
+    std::shared_ptr<const ov::Model> get_runtime_model() const override;
+
+    ov::Any get_property(const std::string& name) const override;
+
+    void set_property(const ov::AnyMap& properties) override {
+        OPENVINO_ASSERT_HELPER(::ov::NotImplemented,
+                               "",
+                               false,
+                               "Not Implemented: ",
+                               "CompiledModel::set_property is not supported by this plugin!");
+    };
+
+    const std::vector<ov::Output<const ov::Node>>& outputs() const override { return m_outputs; }
+    const std::vector<ov::Output<const ov::Node>>& inputs() const override { return m_inputs;}
+
+    bool is_new_api() const { return std::static_pointer_cast<const ov::intel_gpu::Plugin>(get_plugin())->is_new_api(); }
+    RemoteContextImpl::Ptr get_context_impl() const {return m_context; }
+    const std::vector<std::shared_ptr<Graph>>& get_graphs() const;
+    std::shared_ptr<Graph> get_graph(size_t n) const;
+
+private:
+    RemoteContextImpl::Ptr m_context;
     ExecutionConfig m_config;
-    InferenceEngine::ITaskExecutor::Ptr m_taskExecutor;
-    InferenceEngine::ITaskExecutor::Ptr m_waitExecutor;
-    InferenceEngine::CNNNetwork m_network;
+    std::shared_ptr<ov::threading::ITaskExecutor> m_wait_executor;
+    std::shared_ptr<ov::Model> m_model;
+    std::string m_model_name;
+    std::vector<ov::Output<const ov::Node>> m_inputs;
+    std::vector<ov::Output<const ov::Node>> m_outputs;
+    std::vector<std::shared_ptr<Graph>> m_graphs;
+    bool m_loaded_from_cache;
 };
 
 }  // namespace intel_gpu
