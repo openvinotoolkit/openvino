6d3356e033bd60f4c40a0060d5704340f92c7c97
Ilya Lavrenov <ilya.lavrenov@intel.com>
2023-09-13 18:31:35 +0400

    Replaced several cmake utilities with new ov_ prefix

--- cmake/developer_package/faster_build.cmake
+++ cmake/developer_package/faster_build.cmake
@@ -4,23 +4,23 @@
 
 include(CMakeParseArguments)
 
-function(ie_faster_build TARGET_NAME)
+function(ov_build_target_faster TARGET_NAME)
     if(NOT ENABLE_FASTER_BUILD)
         return()
     endif()
 
-    cmake_parse_arguments(IE_FASTER_BUILD "UNITY" "" "PCH" ${ARGN})
+    cmake_parse_arguments(ov_build_target_faster "UNITY" "" "PCH" ${ARGN})
 
     if(IE_FASTER_BUILD_UNITY)
-        set_target_properties(${TARGET_NAME}
-            PROPERTIES
-                UNITY_BUILD ON
-        )
+        set_target_properties(${TARGET_NAME} PROPERTIES UNITY_BUILD ${IE_FASTER_BUILD_UNITY})
     endif()
 
-    if(IE_FASTER_BUILD_PCH)
-        target_precompile_headers(${TARGET_NAME}
-            ${IE_FASTER_BUILD_PCH}
-        )
+    if(OV_FASTER_BUILD_PCH)
+        target_precompile_headers(${TARGET_NAME} ${OV_FASTER_BUILD_PCH})
     endif()
+endfunction()
+
+function(ie_faster_build)
+    message(WARNING "ie_faster_build is deprecated, use ov_build_target_faster instead")
+    ov_build_target_faster(${ARGV})
 endfunction()
