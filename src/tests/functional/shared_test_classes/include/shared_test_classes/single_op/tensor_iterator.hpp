10189ee7dd1b7fef557b5c9eb118b933b3d9f274
vurusovs <vitaliy.urusovskij@intel.com>
2023-09-15 12:43:50 +0400

    `TensorIteratorTest` to API2.0

---
+++ src/tests/functional/shared_test_classes/include/shared_test_classes/single_op/tensor_iterator.hpp
@@ -0,0 +1,40 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <tuple>
+#include <string>
+#include <vector>
+#include <memory>
+#include <ngraph/op/util/attr_types.hpp>
+#include "shared_test_classes/base/ov_subgraph.hpp"
+#include "ngraph_functions/utils/ngraph_helpers.hpp"
+
+namespace ov {
+namespace test {
+using TensorIteratorParams = typename std::tuple<
+        bool,                                     // using unroll tensor iterator transformation
+        size_t,                                   // seq_lengths
+        size_t,                                   // batch
+        size_t,                                   // hidden size
+        // todo: fix. input size hardcoded to 10 due to limitation (10 args) of gtests Combine() func.
+        //size_t,                                 // input size
+        size_t,                                   // sequence axis
+        float,                                    // clip
+        ngraph::helpers::TensorIteratorBody,      // body type
+        ov::op::RecurrentSequenceDirection,       // direction
+        ov::element::Type,                        // Model type
+        ov::test::TargetDevice>;                  // Device name
+
+class TensorIteratorTest : public testing::WithParamInterface<TensorIteratorParams>,
+                           virtual public ov::test::SubgraphBaseTest {
+public:
+    static std::string getTestCaseName(const testing::TestParamInfo<TensorIteratorParams> &obj);
+
+protected:
+    void SetUp() override;
+};
+}  // namespace test
+}  // namespace ov
