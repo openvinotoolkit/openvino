a1ce9174c751448933c0ac63f578e98a1bc65ed1
p-wysocki <przemyslaw.wysocki@intel.com>
2023-09-15 10:57:09 +0200

    Add init check

--- src/bindings/python/src/openvino/__init__.py
+++ src/bindings/python/src/openvino/__init__.py
@@ -57,6 +57,6 @@ except ImportError:
 # Tools
 try:
     # Model Conversion API - ovc should reside in the main namespace
-    from openvino.tools.ovc import convert_model
+    from openvino.tools.ovc import convert_model, InputCutInfo
 except ImportError:
     pass
