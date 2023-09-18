d9d1f53e0fcbe7508d699b70899a9a909a22111d
bstankix <bartoszx.stankiewicz@intel.com>
2023-09-18 10:25:53 +0200

    Bugfix requirements 2

---
+++ docs/openvino_custom_sphinx_sitemap/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(
+    name="openvino_custom_sphinx_sitemap",
+    version="0.0.1",
+    install_requires=['sphinx>=4.5.0', 'sphinx-sitemap>=2.2.0'],
+    packages=['openvino_custom_sphinx_sitemap'],
+)
