3f96f2f6402c1062ac07b31c2dc4fc5d6214861e
Zhai, Xuejun <xuejun.zhai@intel.com>
2023-09-19 16:14:26 +0800

    Fix format issue
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>

--- src/core/include/openvino/core/core_visibility.hpp
+++ src/core/include/openvino/core/core_visibility.hpp
@@ -48,7 +48,8 @@ namespace ov {}  // namespace ov
 #    pragma warning(disable : 4275)
 #endif
 
-#if defined(OPENVINO_STATIC_LIBRARY) || defined(USE_STATIC_IE) // defined if we are building or calling NGRAPH as a static library
+#if defined(OPENVINO_STATIC_LIBRARY) || \
+    defined(USE_STATIC_IE)  // defined if we are building or calling NGRAPH as a static library
 #    define OPENVINO_API
 #    define OPENVINO_API_C(...) __VA_ARGS__
 #else
--- src/inference/src/dev/iremote_context.cpp
+++ src/inference/src/dev/iremote_context.cpp
@@ -2,10 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "openvino/core/node.hpp"
-
 #include "openvino/runtime/iremote_context.hpp"
 
+#include "openvino/core/node.hpp"
 #include "openvino/runtime/make_tensor.hpp"
 
 ov::SoPtr<ov::ITensor> ov::IRemoteContext::create_host_tensor(const ov::element::Type type, const ov::Shape& shape) {
