96ec80b8a89dd574a122b5e0faaaf6c71e480b10
Aleksandr Voron <aleksandr.voron@intel.com>
2023-09-19 17:26:42 +0200

    removed useless include from filter_cpu_info.hpp

--- src/plugins/intel_cpu/tests/functional/test_utils/filter_cpu_info.hpp
+++ src/plugins/intel_cpu/tests/functional/test_utils/filter_cpu_info.hpp
@@ -4,13 +4,6 @@
 
 #pragma once
 
-#include <string>
-#include "ie_system_conf.h"
-#include "shared_test_classes/base/layer_test_utils.hpp"
-#include <exec_graph_info.hpp>
-#include <openvino/runtime/compiled_model.hpp>
-#include "ie_system_conf.h"
-
 namespace CPUTestUtils {
 std::vector<CPUSpecificParams> filterCPUInfo(const std::vector<CPUSpecificParams>& CPUParams);
 std::vector<CPUSpecificParams> filterCPUInfoForArch(const std::vector<CPUSpecificParams>& CPUParams);
