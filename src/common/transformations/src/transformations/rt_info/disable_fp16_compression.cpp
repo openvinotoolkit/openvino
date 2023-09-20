7d2968cd199a0b350b264fd5e1ccec85818a34c7
Anton Voronov <anton.voronov@intel.com>
2023-09-20 12:57:05 +0000

    static build: fixed ov::Model serialization

--- src/common/transformations/src/transformations/rt_info/disable_fp16_compression.cpp
+++ src/common/transformations/src/transformations/rt_info/disable_fp16_compression.cpp
@@ -5,7 +5,10 @@
 #include "transformations/rt_info/disable_fp16_compression.hpp"
 
 namespace {
-const std::string& postponed_fp16_compression_tag = "postponed_fp16_compression";
+static const std::string& get_postponed_fp16_compression_tag() {
+    static const std::string postponed_fp16_compression_tag("postponed_fp16_compression");
+    return postponed_fp16_compression_tag;
+}
 }
 
 void ov::disable_fp16_compression(const std::shared_ptr<Node>& node) {
@@ -24,13 +27,13 @@ bool ov::fp16_compression_is_disabled(const std::shared_ptr<const Node>& node) {
 }
 
 void ov::postpone_fp16_compression(ov::RTMap& rt_info) {
-    rt_info[postponed_fp16_compression_tag] = true;
+    rt_info[get_postponed_fp16_compression_tag()] = true;
 }
 
 bool ov::is_fp16_compression_postponed(const ov::RTMap& rt_info) {
-    return rt_info.count(postponed_fp16_compression_tag);
+    return rt_info.count(get_postponed_fp16_compression_tag());
 }
 
 void ov::do_not_postpone_fp16_compression(ov::RTMap& rt_info) {
-    rt_info.erase(postponed_fp16_compression_tag);
+    rt_info.erase(get_postponed_fp16_compression_tag());
 }
