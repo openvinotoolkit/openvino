3580886e92256634a4d1615b16eac1111ea0f89c
PRATHAM-SPS <ingawalepratham@gmail.com>
2023-09-20 13:57:36 +0530

    pytest:- utils_test.py

--- tools/mo/unit_tests/mo/front/common/partial_infer/utils_test.py
+++ tools/mo/unit_tests/mo/front/common/partial_infer/utils_test.py
@@ -4,6 +4,7 @@
 import unittest
 
 import numpy as np
+import pytest
 
 from openvino.tools.mo.front.common.partial_infer.utils import int64_array, mo_array, is_fully_defined, \
     dynamic_dimension_value, dynamic_dimension, shape_array, compatible_shapes, shape_delete, shape_insert, \
@@ -25,44 +26,40 @@ def gen_masked_array(array, masked_indices):
     return res
 
 
-class IsFullyDefinedTest(unittest.TestCase):
-    def test_is_fully_defined(self):
-        test_cases=[(None, False),
-                    (int64_array([2, 3, 5, 7]), True),  # int64 array with valid values
-                    (np.array([2, 3, 5, 7]), True),  # any numpy array with valid values
-                    (np.array([2, dynamic_dimension_value]), True),  # array with dynamic dimension value is fully defined!
-                    (shape_array([2, dynamic_dimension_value, 5]), False),  # masked array with at least one masked element
-                    (shape_array([2, 4, 5]), True),  # masked array with no masked elements is fully defined
-                    (dynamic_dimension, False),  # dynamic dimension is not fully defined
-                    (dynamic_dimension_value, True),  # dynamic dimension value is fully defined
-                    ((dynamic_dimension_value, dynamic_dimension_value), True),  # list with dynamic dimension values is
-                    # fully defined
-                    ((dynamic_dimension, 1), False),  # tuple with dynamic dimension is not fully defined
-                    ([dynamic_dimension, 1], False),  # list with dynamic dimension is not fully defined
-                    ]
-        for idx, (data, result) in enumerate(test_cases):
-            with self.subTest(test_cases=idx):
-                self.assertEqual(is_fully_defined(data), result)
+class TestIsFullyDefinedTest():
+    @pytest.mark.parametrize("data, result",[(None, False),
+                (int64_array([2, 3, 5, 7]), True),  # int64 array with valid values
+                (np.array([2, 3, 5, 7]), True),  # any numpy array with valid values
+                (np.array([2, dynamic_dimension_value]), True),  # array with dynamic dimension value is fully defined!
+                (shape_array([2, dynamic_dimension_value, 5]), False),  # masked array with at least one masked element
+                (shape_array([2, 4, 5]), True),  # masked array with no masked elements is fully defined
+                (dynamic_dimension, False),  # dynamic dimension is not fully defined
+                (dynamic_dimension_value, True),  # dynamic dimension value is fully defined
+                ((dynamic_dimension_value, dynamic_dimension_value), True),  # list with dynamic dimension values is
+                # fully defined
+                ((dynamic_dimension, 1), False),  # tuple with dynamic dimension is not fully defined
+                ([dynamic_dimension, 1], False),  # list with dynamic dimension is not fully defined
+                ])
+    def test_is_fully_defined(self, data, result):
+        assert is_fully_defined(data) == result
 
 
-class ShapeArrayTest(unittest.TestCase):
-    def test_shape_array(self):
-        test_cases= [([1], shape_array([1]), True),
+class TestShapeArrayTest():
+    @pytest.mark.parametrize("data, ref, result",[([1], shape_array([1]), True),
                 # if we provide a list with dynamic_dimension_value then it is converted to dynamic dimension
                 ([dynamic_dimension_value, 5], gen_masked_array([1, 5], [0]), True),
                 # if we provide a list with dynamic_dimension then the generated shape array still have it
                 ([7, dynamic_dimension], gen_masked_array([7, 1], [1]), True),
                 # negative test to make sure that np.ma.allequal works properly
                 ([2], gen_masked_array([1], []), False),
-                ]
-        for idx, (data, ref, result) in enumerate(test_cases):
-            with self.subTest(test_cases=idx):
-                self.assertEqual(strict_compare_tensors(shape_array(data), ref), result)
+                ])
+    def test_shape_array(self, data, ref, result):
+        assert strict_compare_tensors(shape_array(data), ref) == result
 
 
-class CompareShapesTest(unittest.TestCase):
-    def test_compare_shapes(self):
-        test_cases=[(gen_masked_array([1, 2, 3], []), gen_masked_array([1, 2, 3], []), True),
+class TestCompareShapesTest():
+    @pytest.mark.parametrize("input1, input2, result",[(gen_masked_array([1, 2, 3], []),
+                                                        gen_masked_array([1, 2, 3], []), True),
                 (gen_masked_array([4, 2, 3], []), gen_masked_array([1, 2, 3], []), False),
                 (gen_masked_array([1, 2], []), gen_masked_array([1, 2, 3], []), False),
                 (gen_masked_array([1, 2, 3], []), gen_masked_array([1, 2], []), False),
@@ -74,15 +71,14 @@ class CompareShapesTest(unittest.TestCase):
                 (np.array([1, 2]), gen_masked_array([1, 5, 3], [1]), False),
                 (np.array([1, 2]), np.array([1, 2]), True),
                 (np.array([1, 2]), np.array([3, 2]), False),
-                ]
-        for idx, (input1, input2, result) in enumerate(test_cases):
-            with self.subTest(test_cases=idx):
-                self.assertEqual(compatible_shapes(input1, input2), result)
+                ])
+    def test_compare_shapes(self, input1, input2, result):
+        assert compatible_shapes(input1, input2) == result
 
 
-class ShapeDeleteTest(unittest.TestCase):
-    def test_shape_delete(self):
-        test_cases= [(gen_masked_array([1, 2, 3], []), [], gen_masked_array([1, 2, 3], [])),
+class TestShapeDeleteTest():
+    @pytest.mark.parametrize("shape, indices, result",[(gen_masked_array([1, 2, 3], []), [],
+                                                        gen_masked_array([1, 2, 3], [])),
                 # [1, d, 3] -> [d, 3]. Indices input is a list
                 (gen_masked_array([1, 2, 3], [1]), [0], gen_masked_array([2, 3], [0])),
                 # [1, d, 3] -> [d, 3]. Indices input is a numpy array
@@ -103,19 +99,18 @@ class ShapeDeleteTest(unittest.TestCase):
                 (np.array([1, 2, 3, 4]), [1], [1, 3, 4]),  # [1, 2, 3, 4] -> [1, 3, 4]. Input is a regular arrays
                 (np.array([1, 2, 3, 4]), [-1, -3], [1, 3]),  # [1, 2, 3, 4] -> [1, 3]. Negative indices
                 (np.array([1, 2, 3, 4]), -2, [1, 2, 4]),  # [1, 2, 3, 4] -> [1, 2, 4]. Negative index
-                ]
-        for idx, (shape, indices, result) in enumerate(test_cases):
-            with self.subTest(test_cases=idx):
-                self.assertTrue(strict_compare_tensors(shape_delete(shape, indices), result))
+                ])
+    def test_shape_delete(self, shape, indices, result):
+        assert strict_compare_tensors(shape_delete(shape, indices), result)
 
     def test_shape_delete_raise_exception(self):
-        with self.assertRaisesRegex(Error, '.*Incorrect parameter type.*'):
+        with pytest.raises(Error, match ='.*Incorrect parameter type.*'):
             shape_delete(gen_masked_array([1, 2, 3], []), {})
 
 
-class ShapeInsertTest(unittest.TestCase):
-    def test_shape_insert(self):
-        test_cases =[(gen_masked_array([1, 2, 3], []), 1, [5], gen_masked_array([1, 5, 2, 3], [])),
+class TestShapeInsertTest():
+    @pytest.mark.parametrize("shape, pos, values, result",[(gen_masked_array([1, 2, 3], []), 1, [5],
+                                                            gen_masked_array([1, 5, 2, 3], [])),
                 (gen_masked_array([1, 2, 3], [1]), 1, [5], gen_masked_array([1, 5, 2, 3], [2])),
                 (gen_masked_array([1, 2, 3], [1]), 1, [dynamic_dimension], gen_masked_array([1, 5, 2, 3], [1, 2])),
                 (gen_masked_array([1, 2, 3], [1]), 0, [dynamic_dimension], gen_masked_array([5, 1, 2, 3], [0, 2])),
@@ -125,32 +120,27 @@ class ShapeInsertTest(unittest.TestCase):
                 (gen_masked_array([1, 2, 3], [1]), 3, [dynamic_dimension, dynamic_dimension],
                  gen_masked_array([1, 2, 3, 5, 6], [1, 3, 4])),
                 (gen_masked_array([1], [0]), 0, [7, dynamic_dimension], gen_masked_array([7, 5, 2], [1, 2])),
-                ]
-        for idx, (shape, pos, values, result) in enumerate(test_cases):
-            with self.subTest(test_cases=idx):
-                self.assertTrue(strict_compare_tensors(shape_insert(shape, pos, values), result))
+                ])
+    def test_shape_insert(self, shape, pos, values, result):
+        assert strict_compare_tensors(shape_insert(shape, pos, values), result)
 
     def test_shape_insert_raise_exception(self):
-        with self.assertRaisesRegex(Error, '.*Incorrect parameter type.*'):
+        with pytest.raises(Error, match='.*Incorrect parameter type.*'):
             shape_insert(gen_masked_array([1, 2, 3], []), 2, {})
 
 
-class mo_array_test(unittest.TestCase):
-    def test_mo_array_positive(self):
-        test_cases=[(mo_array([2, 3, 5, 7]), np.array([2, 3, 5, 7])),
+class Testmo_array_test():
+    @pytest.mark.parametrize("data, result",[(mo_array([2, 3, 5, 7]), np.array([2, 3, 5, 7])),
                 (mo_array([2., 3., 5., 7.], dtype=np.float64), np.array([2., 3., 5., 7.])),
                 (mo_array([2., 3., 5., 7.]), np.array([2., 3., 5., 7.], dtype=np.float32)),
-                ]
-        for idx, (data, result) in enumerate(test_cases):
-            with self.subTest(test_cases=idx):
-                self.assertEqual(data.dtype, result.dtype)
+                ])
+    def test_mo_array_positive(self, data, result):
+        assert data.dtype == result.dtype
 
-    def test_mo_array_negative(self):
-        test_cases=[(mo_array([2., 3., 5., 7.]), np.array([2., 3., 5., 7.])),
-                ]
-        for idx, (data, result) in enumerate(test_cases):
-            with self.subTest(test_cases=idx):
-                self.assertNotEqual(data.dtype, result.dtype)
+    @pytest.mark.parametrize("data, result",[(mo_array([2., 3., 5., 7.]), np.array([2., 3., 5., 7.])),
+                ])
+    def test_mo_array_negative(self, data, result):
+        assert data.dtype != result.dtype
 
 
 class clarify_partial_shape_test(unittest.TestCase):
