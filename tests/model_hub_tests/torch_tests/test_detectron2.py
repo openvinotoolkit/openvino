eedea9a1f0c63056304bb52113e4588cb55a8235
Maxim Vafin <maxim.vafin@intel.com>
2023-09-20 12:51:17 +0200

    Temporarily disable det2 models

--- tests/model_hub_tests/torch_tests/test_detectron2.py
+++ tests/model_hub_tests/torch_tests/test_detectron2.py
@@ -82,7 +82,7 @@ class TestDetectron2ConvertModel(TestConvertModel):
 
     @pytest.mark.parametrize("name,type,mark,reason",
                              get_models_list(os.path.join(os.path.dirname(__file__), "detectron2_precommit")))
-    @pytest.mark.precommit
+    #@pytest.mark.precommit
     def test_detectron2_precommit(self, name, type, mark, reason, ie_device):
         self.run(name, None, ie_device)
 
