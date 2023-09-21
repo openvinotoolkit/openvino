d4ed0f716347e3e34aeafbd508723f6ef43f38f1
Zhai, Xuejun <xuejun.zhai@intel.com>
2023-09-21 14:03:15 +0800

    [C API] Fix BOOLEAN confict between Windows.h & ov_element_type_e in OV
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>

--- src/bindings/c/include/openvino/c/openvino.h
+++ src/bindings/c/include/openvino/c/openvino.h
@@ -15,6 +15,12 @@
  **/
 #pragma once
 
+#ifdef _WINDOWS_
+#pragma message("The BOOLEAN define in ov_element_type_e conflict with Windows.h BOOLEAN define. Here redifine the BOOLEAN of Windows.h to WIN_BOOLEAN & the BOOLEAN of ov_element_type_e to OV_BOOLEAN.")
+typedef BOOLEAN WIN_BOOLEAN;
+#define BOOLEAN OV_BOOLEAN
+#endif
+
 #include "openvino/c/auto/properties.h"
 #include "openvino/c/ov_common.h"
 #include "openvino/c/ov_compiled_model.h"
