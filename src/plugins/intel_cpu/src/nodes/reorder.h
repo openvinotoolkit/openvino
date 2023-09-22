97963abcc65110082465cc81386df66f5dc45cd0
Egor Duplenskii <egor.duplensky@gmail.com>
2023-09-22 07:32:12 +0200

    [CPU][ARM][FP16] Use Transpose executor for layout Reorder (#19227)

--- src/plugins/intel_cpu/src/nodes/reorder.h
+++ src/plugins/intel_cpu/src/nodes/reorder.h
@@ -9,6 +9,7 @@
 #include <string>
 #include <memory>
 #include <vector>
+#include "nodes/executors/transpose.hpp"
 #include <utils/general_utils.h>
 
 namespace ov {
@@ -83,6 +84,10 @@ private:
     void optimizedNspc2Ncsp();
     void optimizedNcsp2Nspc();
     void createReorderPrimitive(const dnnl::memory::desc &srcDesc, void* srcPtr, const dnnl::memory::desc &dstDesc, void* dstPtr);
+#if defined(OV_CPU_ARM_ENABLE_FP16)
+    void prepareReorderAsTranspose(MemoryDescPtr parentDesc, MemoryDescPtr childDesc);
+    TransposeExecutorPtr transposeExecutor;
+#endif
 };
 
 }   // namespace node
