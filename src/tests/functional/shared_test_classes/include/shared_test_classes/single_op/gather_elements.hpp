655742542ce818573bcc3d528ec1675c198807f7
Oleg Pipikin <oleg.pipikin@intel.com>
2023-09-25 09:29:57 +0200

    Refactor GatherLayerTest, GatherElementsLayerTest (#20020)
    * Refactor GatherLayerTest
    
    * Refactor GatherElementsLayerTest

---
+++ src/tests/functional/shared_test_classes/include/shared_test_classes/single_op/gather_elements.hpp
@@ -0,0 +1,34 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <string>
+#include <tuple>
+#include <vector>
+
+#include "shared_test_classes/base/ov_subgraph.hpp"
+
+namespace ov {
+namespace test {
+typedef std::tuple<
+        std::vector<InputShape>,  // Input shapes
+        ov::Shape,                // Indices shape
+        int,                      // Axis
+        ov::element::Type,        // Model type
+        ov::element::Type,        // Indices type
+        std::string               // Device name
+> GatherElementsParams;
+
+class GatherElementsLayerTest : public testing::WithParamInterface<GatherElementsParams>,
+                          virtual public ov::test::SubgraphBaseTest {
+public:
+    static std::string getTestCaseName(const testing::TestParamInfo<GatherElementsParams>& obj);
+
+protected:
+    void SetUp() override;
+};
+
+}  // namespace test
+}  // namespace ov
