45fe02dd37a5380ba465241d73e7028a59b7c567
PRATHAM-SPS <ingawalepratham@gmail.com>
2023-09-25 23:01:56 +0530

    pytest:- dft_signal_size_canonicalization_test.py

--- tools/mo/unit_tests/mo/ops/dft_signal_size_canonicalization_test.py
+++ tools/mo/unit_tests/mo/ops/dft_signal_size_canonicalization_test.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2018-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-import unittest
+import pytest
 
 import numpy as np
 
@@ -9,33 +9,31 @@ from openvino.tools.mo.ops.dft import FFTBase
 from openvino.tools.mo.front.common.partial_infer.utils import int64_array
 
 
-class DFTSignalSizeCanonicalizationTest(unittest.TestCase):
-    def test_canonicalization(self):
-        test_cases=[
-            (int64_array([-1, 77]), int64_array([1, 2]), int64_array([2, 180, 180, 2]), int64_array([180, 77])),
-            (int64_array([390, 87]), int64_array([2, 0]), int64_array([2, 180, 180, 2]), int64_array([390, 87])),
-            (int64_array([600, -1, 40]),
-            int64_array([3, 0, 1]),
-            int64_array([7, 50, 130, 400, 2]),
-            int64_array([600, 7, 40])),
-            (int64_array([-1, 16, -1]),
-            int64_array([3, 0, 2]),
-            int64_array([7, 50, 130, 400, 2]),
-            int64_array([400, 16, 130])),
-            (int64_array([16, -1, -1]),
-            int64_array([3, 0, 2]),
-            int64_array([7, 50, 130, 400, 2]),
-            int64_array([16, 7, 130])),
-            (int64_array([-1, -1, 16]),
-            int64_array([3, 0, 2]),
-            int64_array([7, 50, 130, 400, 2]),
-            int64_array([400, 7, 16])),
-            (int64_array([-1, -1, -1]),
-            int64_array([3, 0, 2]),
-            int64_array([7, 50, 130, 400, 2]),
-            int64_array([400, 7, 130])),
-        ]
-        for idx, (signal_size, axes, input_shape, expected_result) in enumerate(test_cases):
-            with self.subTest(test_cases=idx):
-                canonicalized_signal_size = FFTBase.canonicalize_signal_size(signal_size, axes, input_shape)
-                self.assertTrue(np.array_equal(canonicalized_signal_size, expected_result))
+class TestDFTSignalSizeCanonicalizationTest():
+    @pytest.mark.parametrize("signal_size, axes, input_shape, expected_result",[
+        (int64_array([-1, 77]), int64_array([1, 2]), int64_array([2, 180, 180, 2]), int64_array([180, 77])),
+        (int64_array([390, 87]), int64_array([2, 0]), int64_array([2, 180, 180, 2]), int64_array([390, 87])),
+        (int64_array([600, -1, 40]),
+         int64_array([3, 0, 1]),
+         int64_array([7, 50, 130, 400, 2]),
+         int64_array([600, 7, 40])),
+        (int64_array([-1, 16, -1]),
+         int64_array([3, 0, 2]),
+         int64_array([7, 50, 130, 400, 2]),
+         int64_array([400, 16, 130])),
+        (int64_array([16, -1, -1]),
+         int64_array([3, 0, 2]),
+         int64_array([7, 50, 130, 400, 2]),
+         int64_array([16, 7, 130])),
+        (int64_array([-1, -1, 16]),
+         int64_array([3, 0, 2]),
+         int64_array([7, 50, 130, 400, 2]),
+         int64_array([400, 7, 16])),
+        (int64_array([-1, -1, -1]),
+         int64_array([3, 0, 2]),
+         int64_array([7, 50, 130, 400, 2]),
+         int64_array([400, 7, 130])),
+    ])
+    def test_canonicalization(self, signal_size, axes, input_shape, expected_result):
+        canonicalized_signal_size = FFTBase.canonicalize_signal_size(signal_size, axes, input_shape)
+        assert np.array_equal(canonicalized_signal_size, expected_result)
