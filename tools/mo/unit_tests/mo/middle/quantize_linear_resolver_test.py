1b5e4d28b7c81cecfcfe4111cc7c81b95a41b750
PRATHAM-SPS <ingawalepratham@gmail.com>
2023-09-25 19:13:16 +0530

    pytest:- quantize_linear_resolver_test.py

--- tools/mo/unit_tests/mo/middle/quantize_linear_resolver_test.py
+++ tools/mo/unit_tests/mo/middle/quantize_linear_resolver_test.py
@@ -9,6 +9,7 @@ from openvino.tools.mo.middle.quantize_linear_resolver import QuantizeLinearReso
 from openvino.tools.mo.front.common.partial_infer.utils import int64_array
 from openvino.tools.mo.utils.ir_engine.compare_graphs import compare_graphs
 from unit_tests.utils.graph import build_graph
+import pytest
 
 nodes1_attributes = {
     'input': {'kind': 'op', 'op': 'AnyOp'},
@@ -246,9 +247,9 @@ class TestQuantizeLinearResolver(unittest.TestCase):
         self.assertTrue(flag, resp)
 
 
-class TestQuantizeWithAxis(unittest.TestCase):
-    def test_quantize_with_axis(self):
-        test_cases=[(int64_array([1, 3, 4, 4]), np.array([2, 3, 4, 5], dtype=np.float32),
+class TestQuantizeWithAxis():
+    @pytest.mark.parametrize("input_shape, scale_param_value, zero_param_value,target_shape, in_low, in_high, out_low, out_high, axis",
+                             [(int64_array([1, 3, 4, 4]), np.array([2, 3, 4, 5], dtype=np.float32),
                  np.array([2, 3, 4, 5], dtype=np.uint8), int64_array([1, 1, 4, 1]),
                  np.array([-2., -3., -4., -5.]), np.array([253., 252., 251., 250.]),
                  0, 255, 2),
@@ -288,84 +289,81 @@ class TestQuantizeWithAxis(unittest.TestCase):
                  np.array([2, 3, 4, 5], dtype=np.int8), int64_array([1, 1, 4, 1]),
                  np.array([-130., -131., -132., -133.]), np.array([125., 124., 123., 122.]),
                  -128.0, 127.0, -2),
-                ]
-        for idx, (input_shape, scale_param_value, zero_param_value,
-                                target_shape, in_low, in_high, out_low, out_high, axis) in enumerate(test_cases):
-            with self.subTest(test_cases=idx):
-                graph = build_graph(nodes1_attributes,
-                                    [('input', 'input_data'),
-                                    ('input_data', 'quantize'),
-                                    ('scale_param_q', 'scale_param_q_data'),
-                                    ('scale_param_q_data', 'quantize'),
-                                    ('zerop_param_q', 'zerop_param_q_data'),
-                                    ('zerop_param_q_data', 'quantize'),
-                                    ('quantize', 'quantize_data'),
-                                    ('quantize_data', 'out'),
-                                    ('out', 'out_data'),
-                                    ('out_data', 'result'),
-                                    ],
-                                    {
-                                        'quantize': {'axis': axis},
-                                        'input': {'shape': input_shape},
-                                        'input_data': {'shape': input_shape},
-                                        'scale_param_q': {'shape': scale_param_value.shape, 'value': scale_param_value},
-                                        'scale_param_q_data': {'shape': scale_param_value.shape,
-                                                               'value': scale_param_value},
-                                        'zerop_param_q': {'shape': zero_param_value.shape, 'value': zero_param_value},
-                                        'zerop_param_q_data': {'shape': zero_param_value.shape,
-                                                               'value': zero_param_value},
-                                    }, nodes_with_edges_only=True)
+                ])
+    def test_quantize_with_axis(self, input_shape, scale_param_value, zero_param_value,
+                                target_shape, in_low, in_high, out_low, out_high, axis):
+        graph = build_graph(nodes1_attributes,
+                            [('input', 'input_data'),
+                             ('input_data', 'quantize'),
+                             ('scale_param_q', 'scale_param_q_data'),
+                             ('scale_param_q_data', 'quantize'),
+                             ('zerop_param_q', 'zerop_param_q_data'),
+                             ('zerop_param_q_data', 'quantize'),
+                             ('quantize', 'quantize_data'),
+                             ('quantize_data', 'out'),
+                             ('out', 'out_data'),
+                             ('out_data', 'result'),
+                             ],
+                            {
+                                'quantize': {'axis': axis},
+                                'input': {'shape': input_shape},
+                                'input_data': {'shape': input_shape},
+                                'scale_param_q': {'shape': scale_param_value.shape, 'value': scale_param_value},
+                                'scale_param_q_data': {'shape': scale_param_value.shape, 'value': scale_param_value},
+                                'zerop_param_q': {'shape': zero_param_value.shape, 'value': zero_param_value},
+                                'zerop_param_q_data': {'shape': zero_param_value.shape, 'value': zero_param_value},
+                            }, nodes_with_edges_only=True)
 
-                graph_ref = build_graph(nodes_ref_attributes,
-                                        [('input', 'input_data'),
-                                        ('input_data', 'f_quantize'),
-                                        ('scale_param_q', 'scale_param_q_data'),
-                                        ('scale_param_q_data', 'mul1', {'out': 0}),
-                                        ('in_low', 'in_low_data'),
-                                        ('in_low_data', 'mul1'),
-                                        ('mul1', 'mul1_data'),
-                                        ('mul1_data', 'high_reshape'),
-                                        ('high_reshape_const', 'high_reshape_const_data'),
-                                        ('high_reshape_const_data', 'high_reshape'),
-                                        ('high_reshape', 'high_reshape_data'),
-                                        ('high_reshape_data', 'f_quantize'),
+        graph_ref = build_graph(nodes_ref_attributes,
+                                [('input', 'input_data'),
+                                 ('input_data', 'f_quantize'),
+                                 ('scale_param_q', 'scale_param_q_data'),
+                                 ('scale_param_q_data', 'mul1', {'out': 0}),
+                                 ('in_low', 'in_low_data'),
+                                 ('in_low_data', 'mul1'),
+                                 ('mul1', 'mul1_data'),
+                                 ('mul1_data', 'high_reshape'),
+                                 ('high_reshape_const', 'high_reshape_const_data'),
+                                 ('high_reshape_const_data', 'high_reshape'),
+                                 ('high_reshape', 'high_reshape_data'),
+                                 ('high_reshape_data', 'f_quantize'),
 
-                                        ('f_quantize', 'f_quantize_data'),
-                                        ('scale_param_q_data', 'mul2', {'out': 0}),
-                                        ('in_high', 'in_high_data'),
-                                        ('in_high_data', 'mul2'),
-                                        ('mul2', 'mul2_data'),
-                                        ('mul2_data', 'low_reshape'),
-                                        ('low_reshape', 'low_reshape_data'),
-                                        ('low_reshape_data', 'f_quantize'),
-                                        ('low_reshape_const', 'low_reshape_const_data'),
-                                        ('low_reshape_const_data', 'low_reshape'),
-                                        ('out_low', 'out_low_data'),
-                                        ('out_low_data', 'f_quantize'),
+                                 ('f_quantize', 'f_quantize_data'),
+                                 ('scale_param_q_data', 'mul2', {'out': 0}),
+                                 ('in_high', 'in_high_data'),
+                                 ('in_high_data', 'mul2'),
+                                 ('mul2', 'mul2_data'),
+                                 ('mul2_data', 'low_reshape'),
+                                 ('low_reshape', 'low_reshape_data'),
+                                 ('low_reshape_data', 'f_quantize'),
+                                 ('low_reshape_const', 'low_reshape_const_data'),
+                                 ('low_reshape_const_data', 'low_reshape'),
+                                 ('out_low', 'out_low_data'),
+                                 ('out_low_data', 'f_quantize'),
 
-                                        ('out_high', 'out_high_data'),
-                                        ('out_high_data', 'f_quantize'),
-                                        ('f_quantize_data', 'cast'),
-                                        ('cast', 'cast_data'),
-                                        ('cast_data', 'out'),
-                                        ('out', 'out_data'),
-                                        ('out_data', 'result'),
-                                        ],
-                                        {'in_low': {'shape': in_low.shape, 'value': in_low},
-                                        'in_low_data': {'shape': in_low.shape, 'value': in_low},
-                                        'in_high': {'shape': in_high.shape, 'value': in_high},
-                                        'in_high_data': {'shape': in_high.shape, 'value': in_high},
-                                        'out_low': {'shape': np.array([]), 'value': out_low},
-                                        'out_low_data': {'shape': np.array([]), 'value': out_low},
-                                        'out_high': {'shape': np.array([]), 'value': out_high},
-                                        'out_high_data': {'shape': np.array([]), 'value': out_high},
-                                        'cast': {'dst_type': zero_param_value.dtype},
-                                        'low_reshape_const_data': {'shape': target_shape.shape, 'value': target_shape},
-                                        'high_reshape_const_data': {'shape': target_shape.shape, 'value': target_shape},
-                                        }, nodes_with_edges_only=True)
+                                 ('out_high', 'out_high_data'),
+                                 ('out_high_data', 'f_quantize'),
+                                 ('f_quantize_data', 'cast'),
+                                 ('cast', 'cast_data'),
+                                 ('cast_data', 'out'),
+                                 ('out', 'out_data'),
+                                 ('out_data', 'result'),
+                                 ],
+                                {'in_low': {'shape': in_low.shape, 'value': in_low},
+                                 'in_low_data': {'shape': in_low.shape, 'value': in_low},
+                                 'in_high': {'shape': in_high.shape, 'value': in_high},
+                                 'in_high_data': {'shape': in_high.shape, 'value': in_high},
+                                 'out_low': {'shape': np.array([]), 'value': out_low},
+                                 'out_low_data': {'shape': np.array([]), 'value': out_low},
+                                 'out_high': {'shape': np.array([]), 'value': out_high},
+                                 'out_high_data': {'shape': np.array([]), 'value': out_high},
+                                 'cast': {'dst_type': zero_param_value.dtype},
+                                 'low_reshape_const_data': {'shape': target_shape.shape, 'value': target_shape},
+                                 'high_reshape_const_data': {'shape': target_shape.shape, 'value': target_shape},
+                                 }, nodes_with_edges_only=True)
 
-                graph.stage = 'middle'
-                QuantizeLinearResolver().find_and_replace_pattern(graph)
+        graph.stage = 'middle'
+        QuantizeLinearResolver().find_and_replace_pattern(graph)
 
-                (flag, resp) = compare_graphs(graph, graph_ref, 'result', check_op_attrs=True)
-                self.assertTrue(flag, resp)
+        (flag, resp) = compare_graphs(graph, graph_ref, 'result', check_op_attrs=True)
+        assert flag, resp
