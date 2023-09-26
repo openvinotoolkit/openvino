667d5a1bf750d2187ad918abdc9dc06233bf0db3
Raasz, Pawel <pawel.raasz@intel.com>
2023-09-26 09:00:00 +0200

    Migrate ReduceLogicalOr to new API

--- src/core/reference/include/openvino/reference/logical_reduction.hpp
+++ src/core/reference/include/openvino/reference/logical_reduction.hpp
@@ -8,6 +8,8 @@
 #include <numeric>
 
 #include "ngraph/shape_util.hpp"
+#include "openvino/core/shape_util.hpp"
+#include "openvino/reference/utils/coordinate_index.hpp"
 #include "openvino/reference/utils/coordinate_transform.hpp"
 
 namespace ov {
@@ -36,26 +38,24 @@ static inline void reduce_logical_and(const char* arg,
         out[out_idx] = out[out_idx] && arg[in_idx];
     }
 }
+OPENVINO_SUPPRESS_DEPRECATED_END
 
 static inline void reduce_logical_or(const char* arg, char* out, const Shape& in_shape, const AxisSet& reduction_axes) {
-    const auto out_shape = ngraph::reduce(in_shape, reduction_axes, false);
+    const auto out_shape = ov::util::reduce(in_shape, reduction_axes);
     std::fill(out, out + shape_size(out_shape), 0);
 
     const auto in_strides = row_major_strides(in_shape);
     const auto out_strides = row_major_strides(out_shape);
 
     CoordinateTransformBasic input_transform(in_shape);
-    for (const Coordinate& input_coord : input_transform) {
-        const Coordinate output_coord = ngraph::reduce(input_coord, reduction_axes, false);
+    for (const auto& in_coord : input_transform) {
+        const auto out_coord = ov::util::reduce(in_coord, reduction_axes);
 
-        const size_t in_idx =
-            std::inner_product(input_coord.begin(), input_coord.end(), in_strides.begin(), uint64_t(0));
-        const size_t out_idx =
-            std::inner_product(output_coord.begin(), output_coord.end(), out_strides.begin(), uint64_t(0));
+        const auto in_idx = ov::coordinate_offset(in_coord, in_strides);
+        const auto out_idx = ov::coordinate_offset(out_coord, out_strides);
 
         out[out_idx] = out[out_idx] || arg[in_idx];
     }
 }
-OPENVINO_SUPPRESS_DEPRECATED_END
 }  // namespace reference
 }  // namespace ov
