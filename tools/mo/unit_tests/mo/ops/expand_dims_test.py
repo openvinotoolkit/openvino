9a4b426998be8b098af642c2f69611d04f842cbb
PRATHAM-SPS <ingawalepratham@gmail.com>
2023-09-26 16:44:20 +0530

    pytest:- expand_dims_test.py

--- tools/mo/unit_tests/mo/ops/expand_dims_test.py
+++ tools/mo/unit_tests/mo/ops/expand_dims_test.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2018-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-import unittest
+import pytest
 
 import numpy as np
 
@@ -27,69 +27,62 @@ nodes_attributes = {
     }
 }
 
-class ExpandDimsOp(unittest.TestCase):
-    def test_expand_dims_infer(self):
-        test_cases=[(0, [1, 2, 3, 224, 224]),
+class TestExpandDimsOp():
+    @pytest.mark.parametrize("axis, ref_out_shape",[(0, [1, 2, 3, 224, 224]),
                 (1, [2, 1, 3, 224, 224]),
                 (2, [2, 3, 1, 224, 224]),
                 (3, [2, 3, 224, 1, 224]),
                 (4, [2, 3, 224, 224, 1]),
-                ]
-        for idx, (axis, ref_out_shape) in enumerate(test_cases):
-            with self.subTest(test_cases=idx):
-                graph = build_graph(nodes_attributes,
-                                    [('data_1', 'expand_dims'),
-                                    ('expand_dims', 'data_2')],
-                                    {'expand_dims': {'expand_axis': axis}})
-                expand_dims_node = Node(graph, 'expand_dims')
+                ])
+    def test_expand_dims_infer(self, axis, ref_out_shape):
+        graph = build_graph(nodes_attributes,
+                            [('data_1', 'expand_dims'),
+                             ('expand_dims', 'data_2')],
+                            {'expand_dims': {'expand_axis': axis}})
+        expand_dims_node = Node(graph, 'expand_dims')
 
-                ExpandDims.infer(expand_dims_node)
+        ExpandDims.infer(expand_dims_node)
 
-                self.assertTrue(np.array_equal(expand_dims_node.out_node().shape, np.array(ref_out_shape)))
+        assert np.array_equal(expand_dims_node.out_node().shape, np.array(ref_out_shape))
 
 
-class ExpandDimsOpDynamicDims(unittest.TestCase):
-    def test_expand_dims_infer(self):
-        test_cases=[(0, [1, 2, 3, dynamic_dimension_value, 224]),
+class TestExpandDimsOpDynamicDims():
+    @pytest.mark.parametrize("axis, ref_out_shape",[(0, [1, 2, 3, dynamic_dimension_value, 224]),
                 (1, [2, 1, 3, dynamic_dimension_value, 224]),
                 (2, [2, 3, 1, dynamic_dimension_value, 224]),
                 (3, [2, 3, dynamic_dimension_value, 1, 224]),
                 (4, [2, 3, dynamic_dimension_value, 224, 1]),
-                ]
-        for idx, (axis, ref_out_shape) in enumerate(test_cases):
-            with self.subTest(test_cases=idx):
-                graph = build_graph(nodes_attributes,
-                                    [('data_1', 'expand_dims'),
-                                    ('expand_dims', 'data_2')],
-                                    {'expand_dims': {'expand_axis': axis}})
-                Node(graph, 'data_1').shape = shape_array([2, 3, dynamic_dimension_value, 224])
-                expand_dims_node = Node(graph, 'expand_dims')
+                ])
+    def test_expand_dims_infer(self, axis, ref_out_shape):
+        graph = build_graph(nodes_attributes,
+                            [('data_1', 'expand_dims'),
+                             ('expand_dims', 'data_2')],
+                            {'expand_dims': {'expand_axis': axis}})
+        Node(graph, 'data_1').shape = shape_array([2, 3, dynamic_dimension_value, 224])
+        expand_dims_node = Node(graph, 'expand_dims')
 
-                ExpandDims.infer(expand_dims_node)
+        ExpandDims.infer(expand_dims_node)
 
-                self.assertTrue(strict_compare_tensors(expand_dims_node.out_node().shape, shape_array(ref_out_shape)))
+        assert strict_compare_tensors(expand_dims_node.out_node().shape, shape_array(ref_out_shape))
 
 
-class ExpandDimsOpValueInfer(unittest.TestCase):
-    def test_expand_dims_infer_value(self):
-        test_cases=[(0, [2, 3, 224, 224], [1, 2, 3, 224, 224]),
+class TestExpandDimsOpValueInfer():
+    @pytest.mark.parametrize("axis, in_shape, ref_out_shape",[(0, [2, 3, 224, 224], [1, 2, 3, 224, 224]),
                 (1, [2, 3, 224, 224], [2, 1, 3, 224, 224]),
                 (2, [2, 3, 224, 224], [2, 3, 1, 224, 224]),
                 (3, [2, 3, 224, 224], [2, 3, 224, 1, 224]),
                 (4, [2, 3, 224, 224], [2, 3, 224, 224, 1]),
-                ]
-        for idx, (axis, in_shape, ref_out_shape) in enumerate(test_cases):
-            with self.subTest(test_cases=idx):
-                in_value = np.random.rand(*in_shape)
-                graph = build_graph(nodes_attributes,
-                                    [('data_1', 'expand_dims'),
-                                    ('expand_dims', 'data_2')],
-                                    {'data_1': {'value': in_value},
-                                    'expand_dims': {'expand_axis': axis}})
-                expand_dims_node = Node(graph, 'expand_dims')
+                ])
+    def test_expand_dims_infer_value(self, axis, in_shape, ref_out_shape):
+        in_value = np.random.rand(*in_shape)
+        graph = build_graph(nodes_attributes,
+                            [('data_1', 'expand_dims'),
+                             ('expand_dims', 'data_2')],
+                            {'data_1': {'value': in_value},
+                             'expand_dims': {'expand_axis': axis}})
+        expand_dims_node = Node(graph, 'expand_dims')
 
-                ExpandDims.infer(expand_dims_node)
+        ExpandDims.infer(expand_dims_node)
 
-                self.assertTrue(np.array_equal(expand_dims_node.out_node().shape, np.array(ref_out_shape)))
-                self.assertTrue(np.array_equal(expand_dims_node.out_node().value,
-                                               np.array(in_value.reshape(ref_out_shape))))
+        assert np.array_equal(expand_dims_node.out_node().shape, np.array(ref_out_shape))
+        assert np.array_equal(expand_dims_node.out_node().value, np.array(in_value.reshape(ref_out_shape)))
