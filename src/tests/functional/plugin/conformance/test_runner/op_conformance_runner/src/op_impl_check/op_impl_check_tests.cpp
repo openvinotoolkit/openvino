9e5b669111b2a2d939929199c025418da8e524a7
Irina Efode <irina.efode@intel.com>
2023-09-26 23:01:27 +0400

    [CONFORMANCE] Provide plugin impl status based on QM instead of compile_model (#20036)

--- src/tests/functional/plugin/conformance/test_runner/op_conformance_runner/src/op_impl_check/op_impl_check_tests.cpp
+++ src/tests/functional/plugin/conformance/test_runner/op_conformance_runner/src/op_impl_check/op_impl_check_tests.cpp
@@ -2,8 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "op_impl_check/op_impl_check_compile_model.hpp"
-#include "op_impl_check/op_impl_check_query_model.hpp"
+#include "op_impl_check/op_impl_check_test.hpp"
 #include "op_impl_check/single_op_graph.hpp"
 #include "conformance.hpp"
 
