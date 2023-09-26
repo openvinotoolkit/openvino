54ba3994eb043db3a7347361f2b2703c3d9ba5e4
PRATHAM-SPS <ingawalepratham@gmail.com>
2023-09-26 20:09:29 +0530

    pytest:- ReduceOps_test.py

--- tools/mo/unit_tests/mo/ops/ReduceOps_test.py
+++ tools/mo/unit_tests/mo/ops/ReduceOps_test.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2018-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-import unittest
+import pytest
 
 import numpy as np
 
@@ -20,9 +20,8 @@ nodes_attributes = {
 }
 
 
-class ReduceLpTest(unittest.TestCase):
-    def test_reduce_lp(self):
-        test_cases=[
+class TestReduceLpTest():
+    @pytest.mark.parametrize("shape, axes, keepdims, p",[
         ([3, 2, 2], [0], True, 1),
         ([3, 2, 2], [0], True, 2),
         ([3, 2, 2], [1], True, 2),
@@ -31,61 +30,58 @@ class ReduceLpTest(unittest.TestCase):
         ([3, 2, 2], [0], False, 2),
         ([3, 2, 2], [1], False, 2),
         ([3, 2, 2], [2], False, 2),
-    ]
-        for idx, (shape, axes, keepdims, p) in enumerate(test_cases):
-            with self.subTest(test_cases=idx):
-                data = np.reshape(np.arange(1, np.prod(shape) + 1, dtype=np.float32), shape)
-                reduced = np.power(np.sum(a=np.abs(np.power(data, p)), axis=tuple(axes), keepdims=keepdims), 1 / p)
-                axis = int64_array(axes)
-                p = int64_array(p)
-                graph = build_graph(nodes_attributes,
-                                    [*connect('data', '0:reduce_lp'),
-                                    *connect('axis', '1:reduce_lp'),
-                                    *connect('reduce_lp', '0:identity'),
-                                    ('identity', 'identity_d', {'out': 0}),
-                                    ('identity_d', 'output')
-                                    ],
-                                    {'data_d': {'value': data, 'shape': data.shape},
-                                    'axis_d': {'value': axis, 'shape': axis.shape},
-                                    'reduce_lp': {'keep_dims': keepdims}},
-                                    nodes_with_edges_only=True)
+    ])
+    def test_reduce_lp(self, shape, axes, keepdims, p):
+        data = np.reshape(np.arange(1, np.prod(shape) + 1, dtype=np.float32), shape)
+        reduced = np.power(np.sum(a=np.abs(np.power(data, p)), axis=tuple(axes), keepdims=keepdims), 1 / p)
+        axis = int64_array(axes)
+        p = int64_array(p)
+        graph = build_graph(nodes_attributes,
+                            [*connect('data', '0:reduce_lp'),
+                             *connect('axis', '1:reduce_lp'),
+                             *connect('reduce_lp', '0:identity'),
+                             ('identity', 'identity_d', {'out': 0}),
+                             ('identity_d', 'output')
+                             ],
+                            {'data_d': {'value': data, 'shape': data.shape},
+                             'axis_d': {'value': axis, 'shape': axis.shape},
+                             'reduce_lp': {'keep_dims': keepdims}},
+                            nodes_with_edges_only=True)
 
-                reduce_node = Node(graph, 'reduce_lp')
-                reduce_node.op = reduce_node.type = 'ReduceL' + str(p)
-                reduce_infer(reduce_node)
-                self.assertTrue(np.array_equal(reduce_node.out_port(0).data.get_value(), reduced))
+        reduce_node = Node(graph, 'reduce_lp')
+        reduce_node.op = reduce_node.type = 'ReduceL' + str(p)
+        reduce_infer(reduce_node)
+        assert np.array_equal(reduce_node.out_port(0).data.get_value(), reduced)
 
-    def test_reduce_dynamic(self):
-        test_cases=[
+    @pytest.mark.parametrize("shape, axes, keepdims, p",[
         ([3, 2, 2], [0], True, 1),
         ([3, 2, 2], [2], False, 2),
         ([3, 2, 2], [0, 2], False, 2),
-    ]
-        for idx, (shape, axes, keepdims, p) in enumerate(test_cases):
-            with self.subTest(test_cases=idx):
-                false_mask = np.zeros(shape)
-                false_mask[0][1][1] = True
-                data = np.ma.masked_array(np.ones(shape), mask=false_mask)
-                assert not is_fully_defined(data)
-                reduced_tensor = np.sum(data, axis=tuple(axes), keepdims=keepdims)
-                # create an array of all masked elements which is the expected result of
-                # the reduce of the tensor with dynamic values
-                fully_undefined = np.ma.masked_array(reduced_tensor, mask=np.ones(reduced_tensor.shape))
-                axis = int64_array(axes)
-                p = int64_array(p)
-                graph = build_graph(nodes_attributes,
-                                    [*connect('data', '0:reduce_lp'),
-                                    *connect('axis', '1:reduce_lp'),
-                                    *connect('reduce_lp', '0:identity'),
-                                    ('identity', 'identity_d', {'out': 0}),
-                                    ('identity_d', 'output')
-                                    ],
-                                    {'data_d': {'value': data, 'shape': data.shape},
-                                    'axis_d': {'value': axis, 'shape': axis.shape},
-                                    'reduce_lp': {'keep_dims': keepdims}},
-                                    nodes_with_edges_only=True)
+    ])
+    def test_reduce_dynamic(self, shape, axes, keepdims, p):
+        false_mask = np.zeros(shape)
+        false_mask[0][1][1] = True
+        data = np.ma.masked_array(np.ones(shape), mask=false_mask)
+        assert not is_fully_defined(data)
+        reduced_tensor = np.sum(data, axis=tuple(axes), keepdims=keepdims)
+        # create an array of all masked elements which is the expected result of the reduce of the tensor with dynamic
+        # values
+        fully_undefined = np.ma.masked_array(reduced_tensor, mask=np.ones(reduced_tensor.shape))
+        axis = int64_array(axes)
+        p = int64_array(p)
+        graph = build_graph(nodes_attributes,
+                            [*connect('data', '0:reduce_lp'),
+                             *connect('axis', '1:reduce_lp'),
+                             *connect('reduce_lp', '0:identity'),
+                             ('identity', 'identity_d', {'out': 0}),
+                             ('identity_d', 'output')
+                             ],
+                            {'data_d': {'value': data, 'shape': data.shape},
+                             'axis_d': {'value': axis, 'shape': axis.shape},
+                             'reduce_lp': {'keep_dims': keepdims}},
+                            nodes_with_edges_only=True)
 
-                reduce_node = Node(graph, 'reduce_lp')
-                reduce_node.op = reduce_node.type = 'ReduceL' + str(p)
-                reduce_infer(reduce_node)
-                self.assertTrue(strict_compare_tensors(reduce_node.out_port(0).data.get_value(), fully_undefined))
+        reduce_node = Node(graph, 'reduce_lp')
+        reduce_node.op = reduce_node.type = 'ReduceL' + str(p)
+        reduce_infer(reduce_node)
+        assert strict_compare_tensors(reduce_node.out_port(0).data.get_value(), fully_undefined)
