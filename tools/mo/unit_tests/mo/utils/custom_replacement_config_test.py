d0b50b98df123d22c3e4a61ed95a411fa2358943
PRATHAM-SPS <ingawalepratham@gmail.com>
2023-09-27 19:52:28 +0530

    missing 1 required positional argument

--- tools/mo/unit_tests/mo/utils/custom_replacement_config_test.py
+++ tools/mo/unit_tests/mo/utils/custom_replacement_config_test.py
@@ -3,7 +3,7 @@
 
 import os
 from fnmatch import fnmatch
-from generator import generator, generate
+import pytest
 
 from openvino.tools.mo.utils.custom_replacement_config import load_and_validate_json_config
 from openvino.tools.mo.utils.error import Error
@@ -21,7 +21,6 @@ def get_json_configs(mo_root_dir):
                 config_files_list.append((os.path.join(path, name),))
     return config_files_list
 
-@generator
 class TestSchema(UnitTestWithMockedTelemetry):
     base_dir = get_mo_root_dir()
     schema_file = os.path.join(base_dir, 'mo', 'utils', 'schema.json')
@@ -29,12 +28,14 @@ class TestSchema(UnitTestWithMockedTelemetry):
     test_json1 = '[{"id": "", "match_kind": "general", "custom_attributes": {}}]'
     test_json2 = '[{"id": "someid", "match_kind": "abc", "custom_attributes": {}}]'
 
-    @generate(*transformation_configs)
+    @pytest.mark.parametrize("transformation_config",transformation_configs)
     def test_schema_file(self, transformation_config):
-        self.assertTrue(load_and_validate_json_config(transformation_config))
+        assert load_and_validate_json_config(transformation_config)
 
     def test_schema_id_empty(self):
-        self.assertRaises(Error, load_and_validate_json_config, self.test_json1)
+        with pytest.raises(Error):
+            load_and_validate_json_config(self.test_json1)
 
     def test_schema_match_kind_wrong(self):
-        self.assertRaises(Error, load_and_validate_json_config, self.test_json2)
+        with pytest.raises(Error):
+            load_and_validate_json_config(self.test_json2)
