378478e6ad0e65d5233658649489f3e7eb02e4c2
PRATHAM-SPS <ingawalepratham@gmail.com>
2023-09-27 19:08:05 +0530

    pytest:- common_test.py

--- tools/mo/unit_tests/mo/pipeline/common_test.py
+++ tools/mo/unit_tests/mo/pipeline/common_test.py
@@ -2,36 +2,96 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import unittest
+import pytest
 from openvino.tools.mo.graph.graph import Node
 from openvino.tools.mo.pipeline.common import determined_sort, get_fw_tensor_debug_info, get_sorted_outputs
 from unit_tests.utils.graph import build_graph_with_edge_attrs
 
-class TestTopologicalSort(unittest.TestCase):
-    def test_determined_topological_sort_1(self):
-        edges = [('A', 'Ad', {'out': 0}),
-                 ('Ad', 'B', {'in': 0}),
-                 ('B', 'Bd', {'out': 0}),
-                 ('Bd', 'C', {'in': 0}),
-                 ('C', 'Cd', {'out': 0}),
-                 ('Cd', 'D', {'in': 0}),
-                 ('D', 'Dd', {'out': 0}),
-                 ('Dd', 'E', {'in': 0}),
-                 ('E', 'Ed', {'out': 0}),
-                 ('Ed', 'I', {'in': 0}),
-                 ('Cd', 'F', {'in': 0}),
-                 ('F', 'Fd', {'out': 0}),
-                 ('Fd', 'G', {'in': 0}),
-                 ('G', 'Gd', {'out': 0}),
-                 ('Gd', 'I', {'in': 1}),
-                 ('Cd', 'H', {'in': 0}),
-                 ('H', 'Hd', {'out': 0}),
-                 ('Hd', 'I', {'in': 2}),
-                 ('I', 'Id', {'out': 0}),
-                 ('Id', 'J', {'in': 0}),
-                 ('J', 'Jd', {'out': 0}),
-                 ('Jd', 'K', {'in': 0}),
-                 ('K', 'Kd', {'out': 0})]
 
+class TestTopologicalSort():
+    @pytest.mark.parametrize( "edges",[
+        [('A', 'Ad', {'out': 0}),
+         ('Ad', 'B', {'in': 0}),
+         ('B', 'Bd', {'out': 0}),
+         ('Bd', 'C', {'in': 0}),
+         ('C', 'Cd', {'out': 0}),
+         ('Cd', 'D', {'in': 0}),
+         ('D', 'Dd', {'out': 0}),
+         ('Dd', 'E', {'in': 0}),
+         ('E', 'Ed', {'out': 0}),
+         ('Ed', 'I', {'in': 0}),
+         ('Cd', 'F', {'in': 0}),
+         ('F', 'Fd', {'out': 0}),
+         ('Fd', 'G', {'in': 0}),
+         ('G', 'Gd', {'out': 0}),
+         ('Gd', 'I', {'in': 1}),
+         ('Cd', 'H', {'in': 0}),
+         ('H', 'Hd', {'out': 0}),
+         ('Hd', 'I', {'in': 2}),
+         ('I', 'Id', {'out': 0}),
+         ('Id', 'J', {'in': 0}),
+         ('J', 'Jd', {'out': 0}),
+         ('Jd', 'K', {'in': 0}),
+         ('K', 'Kd', {'out': 0})],
+
+        [('A', 'Ad', {'out': 0}),
+         ('Ad', 'B', {'in': 0}),
+         ('B', 'Bd', {'out': 0}),
+         ('Bd', 'C', {'in': 0}),
+         ('C', 'Cd', {'out': 0}),
+         ('Cd', 'D', {'in': 0}),
+         ('D', 'Dd', {'out': 0}),
+         ('Dd', 'E', {'in': 0}),
+         ('E', 'Ed', {'out': 0}),
+         ('Ed', 'I', {'in': 0}),
+         ('Cd', 'F', {'in': 0}),
+         ('F', 'Fd', {'out': 0}),
+         ('Fd', 'G', {'in': 0}),
+         ('G', 'Gd', {'out': 0}),
+         ('Gd', 'I', {'in': 1}),
+         ('Cd', 'H', {'in': 0}),
+         ('H', 'Hd', {'out': 0}),
+         ('Hd', 'I', {'in': 2}),
+         ('I', 'Id', {'out': 0}),
+         ('Id', 'J', {'in': 0}),
+         ('J', 'Jd', {'out': 0}),
+         ('Jd', 'K', {'in': 0}),
+         ('K', 'Kd', {'out': 0}),
+         ('Ad', 'E', {'in': 1}),
+         ('Bd', 'K', {'in': 1}),
+         ('Hd', 'J', {'in': 1})],
+
+        [('A', 'Ad', {'out': 0}),
+         ('Ad', 'B', {'in': 0}),
+         ('B', 'Bd', {'out': 0}),
+         ('Bd', 'C', {'in': 0}),
+         ('C', 'Cd', {'out': 0}),
+         ('Cd', 'D', {'in': 0}),
+         ('D', 'Dd', {'out': 0}),
+         ('Dd', 'E', {'in': 0}),
+         ('E', 'Ed', {'out': 0}),
+         ('Ed', 'I', {'in': 0}),
+         ('Cd', 'F', {'in': 0}),
+         ('F', 'Fd', {'out': 0}),
+         ('Fd', 'G', {'in': 0}),
+         ('G', 'Gd', {'out': 0}),
+         ('Gd', 'I', {'in': 1}),
+         ('Cd', 'H', {'in': 0}),
+         ('H', 'Hd', {'out': 0}),
+         ('Hd', 'I', {'in': 2}),
+         ('I', 'Id', {'out': 0}),
+         ('Id', 'J', {'in': 0}),
+         ('J', 'Jd', {'out': 0}),
+         ('Jd', 'K', {'in': 0}),
+         ('K', 'Kd', {'out': 0}),
+         ('Ad', 'E', {'in': 1}),
+         ('Bd', 'K', {'in': 1}),
+         ('Hd', 'J', {'in': 1}),
+         ('Dd', 'F', {'in': 1}),
+         ('Fd', 'H', {'in': 1}),
+         ('Gd', 'H', {'in': 0})]]
+    )
+    def test_determined_topological_sort(self, edges):
         nodes = {'A': {'type': 'Identity', 'kind': 'op'},
                  'B': {'type': 'Identity', 'kind': 'op'},
                  'C': {'type': 'Identity', 'kind': 'op'},
@@ -58,134 +118,11 @@ class TestTopologicalSort(unittest.TestCase):
 
         graph = build_graph_with_edge_attrs(nodes, edges)
         outputs = [Node(graph, 'Kd')]
-        op_order, data_order = determined_sort(outputs)
-        expected_op_order = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']
-        expected_data_order = ['Ad', 'Bd', 'Cd', 'Dd', 'Ed', 'Fd', 'Gd', 'Hd', 'Id', 'Jd', 'Kd']
-        self.assertListEqual(op_order, expected_op_order)
-        self.assertListEqual(data_order, expected_data_order)
+        for i in range(100):
+            op_order, data_order = determined_sort(outputs)
+            assert op_order == ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']
+            assert data_order == ['Ad', 'Bd', 'Cd', 'Dd', 'Ed', 'Fd', 'Gd', 'Hd', 'Id', 'Jd', 'Kd']
 
-    def test_determined_topological_sort_2(self):
-        edges = [('A', 'Ad', {'out': 0}),
-                 ('Ad', 'B', {'in': 0}),
-                 ('B', 'Bd', {'out': 0}),
-                 ('Bd', 'C', {'in': 0}),
-                 ('C', 'Cd', {'out': 0}),
-                 ('Cd', 'D', {'in': 0}),
-                 ('D', 'Dd', {'out': 0}),
-                 ('Dd', 'E', {'in': 0}),
-                 ('E', 'Ed', {'out': 0}),
-                 ('Ed', 'I', {'in': 0}),
-                 ('Cd', 'F', {'in': 0}),
-                 ('F', 'Fd', {'out': 0}),
-                 ('Fd', 'G', {'in': 0}),
-                 ('G', 'Gd', {'out': 0}),
-                 ('Gd', 'I', {'in': 1}),
-                 ('Cd', 'H', {'in': 0}),
-                 ('H', 'Hd', {'out': 0}),
-                 ('Hd', 'I', {'in': 2}),
-                 ('I', 'Id', {'out': 0}),
-                 ('Id', 'J', {'in': 0}),
-                 ('J', 'Jd', {'out': 0}),
-                 ('Jd', 'K', {'in': 0}),
-                 ('K', 'Kd', {'out': 0}),
-                 ('Ad', 'E', {'in': 1}),
-                 ('Bd', 'K', {'in': 1}),
-                 ('Hd', 'J', {'in': 1})]
-
-        nodes = {'A': {'type': 'Identity', 'kind': 'op'},
-                 'B': {'type': 'Identity', 'kind': 'op'},
-                 'C': {'type': 'Identity', 'kind': 'op'},
-                 'D': {'type': 'Identity', 'kind': 'op'},
-                 'E': {'type': 'Identity', 'kind': 'op'},
-                 'F': {'type': 'Identity', 'kind': 'op'},
-                 'G': {'type': 'Identity', 'kind': 'op'},
-                 'H': {'type': 'Identity', 'kind': 'op'},
-                 'I': {'type': 'Identity', 'kind': 'op'},
-                 'J': {'type': 'Identity', 'kind': 'op'},
-                 'K': {'type': 'Identity', 'kind': 'op'},
-                 'Ad': {'value': None, 'kind': 'data'},
-                 'Bd': {'value': None, 'kind': 'data'},
-                 'Cd': {'value': None, 'kind': 'data'},
-                 'Dd': {'value': None, 'kind': 'data'},
-                 'Ed': {'value': None, 'kind': 'data'},
-                 'Fd': {'value': None, 'kind': 'data'},
-                 'Gd': {'value': None, 'kind': 'data'},
-                 'Hd': {'value': None, 'kind': 'data'},
-                 'Id': {'value': None, 'kind': 'data'},
-                 'Jd': {'value': None, 'kind': 'data'},
-                 'Kd': {'value': None, 'kind': 'data'},
-                 }
-
-        graph = build_graph_with_edge_attrs(nodes, edges)
-        outputs = [Node(graph, 'Kd')]
-        op_order, data_order = determined_sort(outputs)
-        expected_op_order = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']
-        expected_data_order = ['Ad', 'Bd', 'Cd', 'Dd', 'Ed', 'Fd', 'Gd', 'Hd', 'Id', 'Jd', 'Kd']
-        self.assertListEqual(op_order, expected_op_order)
-        self.assertListEqual(data_order, expected_data_order)
-
-    def test_determined_topological_sort_3(self):
-        edges = [('A', 'Ad', {'out': 0}),
-                 ('Ad', 'B', {'in': 0}),
-                 ('B', 'Bd', {'out': 0}),
-                 ('Bd', 'C', {'in': 0}),
-                 ('C', 'Cd', {'out': 0}),
-                 ('Cd', 'D', {'in': 0}),
-                 ('D', 'Dd', {'out': 0}),
-                 ('Dd', 'E', {'in': 0}),
-                 ('E', 'Ed', {'out': 0}),
-                 ('Ed', 'I', {'in': 0}),
-                 ('Cd', 'F', {'in': 0}),
-                 ('F', 'Fd', {'out': 0}),
-                 ('Fd', 'G', {'in': 0}),
-                 ('G', 'Gd', {'out': 0}),
-                 ('Gd', 'I', {'in': 1}),
-                 ('Cd', 'H', {'in': 0}),
-                 ('H', 'Hd', {'out': 0}),
-                 ('Hd', 'I', {'in': 2}),
-                 ('I', 'Id', {'out': 0}),
-                 ('Id', 'J', {'in': 0}),
-                 ('J', 'Jd', {'out': 0}),
-                 ('Jd', 'K', {'in': 0}),
-                 ('K', 'Kd', {'out': 0}),
-                 ('Ad', 'E', {'in': 1}),
-                 ('Bd', 'K', {'in': 1}),
-                 ('Hd', 'J', {'in': 1}),
-                 ('Dd', 'F', {'in': 1}),
-                 ('Fd', 'H', {'in': 1}),
-                 ('Gd', 'H', {'in': 0})]
-
-        nodes = {'A': {'type': 'Identity', 'kind': 'op'},
-                 'B': {'type': 'Identity', 'kind': 'op'},
-                 'C': {'type': 'Identity', 'kind': 'op'},
-                 'D': {'type': 'Identity', 'kind': 'op'},
-                 'E': {'type': 'Identity', 'kind': 'op'},
-                 'F': {'type': 'Identity', 'kind': 'op'},
-                 'G': {'type': 'Identity', 'kind': 'op'},
-                 'H': {'type': 'Identity', 'kind': 'op'},
-                 'I': {'type': 'Identity', 'kind': 'op'},
-                 'J': {'type': 'Identity', 'kind': 'op'},
-                 'K': {'type': 'Identity', 'kind': 'op'},
-                 'Ad': {'value': None, 'kind': 'data'},
-                 'Bd': {'value': None, 'kind': 'data'},
-                 'Cd': {'value': None, 'kind': 'data'},
-                 'Dd': {'value': None, 'kind': 'data'},
-                 'Ed': {'value': None, 'kind': 'data'},
-                 'Fd': {'value': None, 'kind': 'data'},
-                 'Gd': {'value': None, 'kind': 'data'},
-                 'Hd': {'value': None, 'kind': 'data'},
-                 'Id': {'value': None, 'kind': 'data'},
-                 'Jd': {'value': None, 'kind': 'data'},
-                 'Kd': {'value': None, 'kind': 'data'},
-                 }
-
-        graph = build_graph_with_edge_attrs(nodes, edges)
-        outputs = [Node(graph, 'Kd')]
-        op_order, data_order = determined_sort(outputs)
-        expected_op_order = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']
-        expected_data_order = ['Ad', 'Bd', 'Cd', 'Dd', 'Ed', 'Fd', 'Gd', 'Hd', 'Id', 'Jd', 'Kd']
-        self.assertListEqual(op_order, expected_op_order)
-        self.assertListEqual(data_order, expected_data_order)
 
 class TestGetFWTensorName(unittest.TestCase):
     def test_get_fw_tensor_debug_info(self):
