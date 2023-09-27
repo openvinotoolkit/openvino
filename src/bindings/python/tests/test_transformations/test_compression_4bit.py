583924d04e5c52b6c0852b0617e22f72eccdaad4
Andrei Anufriev <andrey.anufriev@intel.com>
2023-09-27 08:48:33 +0200

    NF4 test refactoring.

--- src/bindings/python/tests/test_transformations/test_compression_4bit.py
+++ src/bindings/python/tests/test_transformations/test_compression_4bit.py
@@ -15,9 +15,9 @@ import pytest
     (ov.Type.f64, np.float64),
     (ov.Type.f16, np.float16),
 ])
-def test_float_convert(ov_type, numpy_dtype):
-    data = np.linspace(-1.0, 1.0, num=41, dtype=numpy_dtype)
-    
+def test_float_to_nf4_convert(ov_type, numpy_dtype):
+    data = np.linspace(-1.5, 1.5, num=41, dtype=numpy_dtype)
+
     compressed_const = opset.constant(data, dtype=ov.Type.nf4, name="nf4_constant")
     convert = opset.convert(compressed_const, data.dtype)
     parameter = opset.parameter(ov.PartialShape([-1]), ov_type)
@@ -27,15 +27,15 @@ def test_float_convert(ov_type, numpy_dtype):
     compiled = ov.compile_model(model)
     tensor = np.zeros(data.shape, dtype=numpy_dtype)
     result = compiled(tensor)[0]
-    
+
     uniq = []
-    for val in result:
-        if not val in uniq:
-            uniq.append(val)
+    for res_val in result:
+        if res_val not in uniq:
+            uniq.append(res_val)
     uniq = np.array(uniq)
 
     assert len(uniq) == 16
-    
+
     target = [-1.0, -0.6961928009986877, -0.5250730514526367,
               -0.39491748809814453, -0.28444138169288635,
               -0.18477343022823334, -0.09105003625154495,
@@ -44,7 +44,7 @@ def test_float_convert(ov_type, numpy_dtype):
               0.44070982933044434, 0.5626170039176941,
               0.7229568362236023, 1.0]
     target = np.array(target)
-    
+
     diff = np.max(np.abs(target - uniq))
-    
+
     assert diff < 0.001
