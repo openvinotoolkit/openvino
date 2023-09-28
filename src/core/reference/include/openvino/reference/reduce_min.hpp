66732c59901976a511c9798e2b3a60bf08df58cd
Raasz, Pawel <pawel.raasz@intel.com>
2023-09-28 08:25:04 +0200

    Use `coordinate_offset` helper in refs impl

--- src/core/reference/include/openvino/reference/reduce_min.hpp
+++ src/core/reference/include/openvino/reference/reduce_min.hpp
@@ -9,6 +9,7 @@
 #include <numeric>
 
 #include "openvino/core/shape_util.hpp"
+#include "openvino/reference/utils/coordinate_index.hpp"
 #include "openvino/reference/utils/coordinate_transform.hpp"
 
 namespace ov {
@@ -34,11 +35,9 @@ void reduce_min(const T* in, T* out, const Shape& in_shape, const AxisSet& reduc
 
     CoordinateTransformBasic input_transform(in_shape);
     for (const auto& in_coord : input_transform) {
-        constexpr uint64_t init_value = 0;
         const auto out_coord = util::reduce(in_coord, reduction_axes);
-
-        const auto in_idx = std::inner_product(in_coord.begin(), in_coord.end(), in_strides.begin(), init_value);
-        const auto out_idx = std::inner_product(out_coord.begin(), out_coord.end(), out_strides.begin(), init_value);
+        const auto in_idx = coordinate_offset(in_coord, in_strides);
+        const auto out_idx = coordinate_offset(out_coord, out_strides);
 
         out[out_idx] = std::min(out[out_idx], in[in_idx]);
     }
