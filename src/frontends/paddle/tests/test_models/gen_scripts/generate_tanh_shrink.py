d6145ae46f0b6ba799a76d012c61fa51bcc7f48f
Asthestarsfalll <1186454801@qq.com>
2023-09-28 08:07:48 +0800

    update for unittests

--- src/frontends/paddle/tests/test_models/gen_scripts/generate_tanh_shrink.py
+++ src/frontends/paddle/tests/test_models/gen_scripts/generate_tanh_shrink.py
@@ -14,45 +14,41 @@ def tanh_shrink(name: str, x):
 
     paddle.enable_static()
 
-    node_x = paddle.static.data(name="x", shape=x.shape, dtype=x.dtype)
-    out = paddle.nn.functional.tanhshrink(node_x)
+    with paddle.static.program_guard(paddle.static.Program(), paddle.static.Program()):
+        node_x = paddle.static.data(name='x', shape=x.shape, dtype=x.dtype)
+        out = paddle.nn.functional.tanhshrink(node_x)
 
-    cpu = paddle.static.cpu_places(1)
-    exe = paddle.static.Executor(cpu[0])
-    # startup program will call initializer to initialize the parameters.
-    exe.run(paddle.static.default_startup_program())
+        cpu = paddle.static.cpu_places(1)
+        exe = paddle.static.Executor(cpu[0])
+        # startup program will call initializer to initialize the parameters.
+        exe.run(paddle.static.default_startup_program())
 
-    outs = exe.run(feed={"x": x}, fetch_list=[out])
+        outs = exe.run(feed={'x': x}, fetch_list=[out])
 
-    saveModel(
-        name,
-        exe,
-        feedkeys=["x"],
-        fetchlist=[out],
-        inputs=[x],
-        outputs=[outs[0]],
-        target_dir=sys.argv[1],
-    )
+        saveModel(
+            name,
+            exe,
+            feedkeys=['x'],
+            fetchlist=[out],
+            inputs=[x],
+            outputs=[outs[0]],
+            target_dir=sys.argv[1],
+        )
 
     return outs[0]
 
 
 def main():
-    data = np.random.randn(2, 3, 4).astype(np.float32)
+    data = np.random.uniform(10, 20, [2, 3, 4]).astype(np.float32)
     tanh_shrink("tanh_shrink_1", data)
 
-    data = np.random.randn(2, 3, 4).astype(np.float64)
+    data = np.random.uniform(-10, 20, [4, 3, 2]).astype(np.float32)
     tanh_shrink("tanh_shrink_2", data)
 
-    data = np.random.randn(2, 3, 4).astype(np.float16)
-    tanh_shrink("tanh_shrink_3", data)
-
-    data = np.random.randn(2, 3, 4).astype(np.int32)
-    tanh_shrink("tanh_shrink_4", data)
-
-    data = np.random.randn(2, 3, 4).astype(np.int64)
-    tanh_shrink("tanh_shrink_5", data)
-
+    # data = np.random.uniform(10, 20, [2, 3, 4]).astype(np.float16) // only on gpu
+    # tanh_shrink("tanh_shrink_3", data)
 
 if __name__ == "__main__":
     main()
+
+
