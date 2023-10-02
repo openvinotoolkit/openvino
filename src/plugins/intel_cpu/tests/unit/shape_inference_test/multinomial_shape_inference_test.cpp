efb1f8629c38b225769d746462d5f98fcad576e3
Roboreptile <piotr.krzeminski@intel.com>
2023-10-02 15:39:11 +0000

    [Ref] Add multiple shape inference tests

--- src/plugins/intel_cpu/tests/unit/shape_inference_test/multinomial_shape_inference_test.cpp
+++ src/plugins/intel_cpu/tests/unit/shape_inference_test/multinomial_shape_inference_test.cpp
@@ -11,11 +11,12 @@
 using namespace ov;
 using namespace ov::intel_cpu;
 
-TEST(StaticShapeInferenceTest, MultinomialDefaultShapeInferenceTest) {
+TEST(StaticShapeInferenceTest, MultinomialStaticShapeInferenceTest1D) {
     auto probs = std::make_shared<op::v0::Parameter>(element::f32, Shape{4});
-    auto num_elements = std::make_shared<op::v0::Parameter>(element::f32, Shape{1});
-    auto multinomial = std::make_shared<op::v13::Multinomial>(probs, num_elements, element::i32, false, false, 0, 0);
-    // Test Static Shape
+    auto num_samples = std::make_shared<op::v0::Parameter>(element::f32, Shape{1});
+    auto multinomial = std::make_shared<op::v13::Multinomial>(probs, num_samples, element::i32, false, false, 0, 0);
+
+    // Test Static Shape 1D input
     std::vector<StaticShape> static_input_shapes = {StaticShape{4}, StaticShape{1}};
     int32_t num_elements_val = 2;
     auto const_data =
@@ -23,4 +24,104 @@ TEST(StaticShapeInferenceTest, MultinomialDefaultShapeInferenceTest) {
     auto acc = make_tensor_accessor(const_data);
     auto static_output_shapes = shape_infer(multinomial.get(), static_input_shapes, acc);
     ASSERT_EQ(static_output_shapes[0], StaticShape({2}));
+}
+
+TEST(StaticShapeInferenceTest, MultinomialStaticShapeInferenceTest2D) {
+    auto probs = std::make_shared<op::v0::Parameter>(element::f32, Shape{4, 4});
+    auto num_samples = std::make_shared<op::v0::Parameter>(element::f32, Shape{1});
+    auto multinomial = std::make_shared<op::v13::Multinomial>(probs, num_samples, element::i32, false, false, 0, 0);
+
+    // Test Static Shape 2D input
+    std::vector<StaticShape> static_input_shapes = {StaticShape{4, 4}, StaticShape{1}};
+    int32_t num_elements_val = 2;
+    auto const_data =
+        std::map<size_t, HostTensorPtr>{{1, std::make_shared<HostTensor>(element::i32, Shape{1}, &num_elements_val)}};
+    auto acc = make_tensor_accessor(const_data);
+    auto static_output_shapes = shape_infer(multinomial.get(), static_input_shapes, acc);
+    ASSERT_EQ(static_output_shapes[0], StaticShape({4, 2}));
+}
+
+TEST(StaticShapeInferenceTest, MultinomialDynamicShapeInferenceTestAllDimKnown1D) {
+    auto probs = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{3});
+    auto num_samples = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{1});
+    auto multinomial = std::make_shared<op::v13::Multinomial>(probs, num_samples, element::i32, false, false, 0, 0);
+
+    // Test Partial Shape 1D input
+    std::vector<PartialShape> partial_input_shapes = {PartialShape{4}, PartialShape{1}};
+    int32_t num_elements_val = 2;
+    auto const_data =
+        std::map<size_t, HostTensorPtr>{{1, std::make_shared<HostTensor>(element::i32, Shape{1}, &num_elements_val)}};
+    auto acc = make_tensor_accessor(const_data);
+    auto partial_output_shapes = shape_infer(multinomial.get(), partial_input_shapes, acc);
+    ASSERT_EQ(partial_output_shapes[0], PartialShape({2}));
+}
+
+TEST(StaticShapeInferenceTest, MultinomialDynamicShapeInferenceTestAllDimKnown2D) {
+    auto probs = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{2, 3});
+    auto num_samples = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{1});
+    auto multinomial = std::make_shared<op::v13::Multinomial>(probs, num_samples, element::i32, false, false, 0, 0);
+
+    // Test Partial Shape 2D input
+    std::vector<PartialShape> partial_input_shapes = {PartialShape{4}, PartialShape{1}};
+    int32_t num_elements_val = 2;
+    auto const_data =
+        std::map<size_t, HostTensorPtr>{{1, std::make_shared<HostTensor>(element::i32, Shape{1}, &num_elements_val)}};
+    auto acc = make_tensor_accessor(const_data);
+    auto partial_output_shapes = shape_infer(multinomial.get(), partial_input_shapes, acc);
+    ASSERT_EQ(partial_output_shapes[0], PartialShape({2, 2}));
+}
+
+TEST(StaticShapeInferenceTest, MultinomialDynamicShapeInferenceTestDynamicNumSamples1D) {
+    auto probs = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{4});
+    auto num_samples = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{-1});
+    auto multinomial = std::make_shared<op::v13::Multinomial>(probs, num_samples, element::i32, false, false, 0, 0);
+
+    // Test Partial Shape 1D input, unknown num_samples
+    std::vector<PartialShape> partial_input_shapes = {PartialShape{4}, PartialShape{-1}};
+    auto partial_output_shapes = shape_infer(multinomial.get(), partial_input_shapes, make_tensor_accessor());
+    ASSERT_EQ(partial_output_shapes[0], PartialShape({-1}));
+}
+
+TEST(StaticShapeInferenceTest, MultinomialDynamicShapeInferenceTestDynamicNumSamples2D) {
+    auto probs = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{4, 4});
+    auto num_samples = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{-1});
+    auto multinomial = std::make_shared<op::v13::Multinomial>(probs, num_samples, element::i32, false, false, 0, 0);
+
+    // Test Partial Shape 2D input, unknown num_samples
+    std::vector<PartialShape> partial_input_shapes = {PartialShape{4}, PartialShape{-1}};
+    auto partial_output_shapes = shape_infer(multinomial.get(), partial_input_shapes, make_tensor_accessor());
+    ASSERT_EQ(partial_output_shapes[0], PartialShape({4, -1}));
+}
+
+TEST(StaticShapeInferenceTest, MultinomialDynamicShapeInferenceTestDynamicProbsDynamicNumSamples1D) {
+    auto probs = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{-1});
+    auto num_samples = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{-1});
+    auto multinomial = std::make_shared<op::v13::Multinomial>(probs, num_samples, element::i32, false, false, 0, 0);
+
+    // Test Partial Shape 1D input, unknown num_samples and probs shape
+    std::vector<PartialShape> partial_input_shapes = {PartialShape{4}, PartialShape{-1}};
+    auto partial_output_shapes = shape_infer(multinomial.get(), partial_input_shapes, make_tensor_accessor());
+    ASSERT_EQ(partial_output_shapes[0], PartialShape({-1}));
+}
+
+TEST(StaticShapeInferenceTest, MultinomialDynamicShapeInferenceTestDynamicProbsDynamicNumSamples2D) {
+    auto probs = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{-1, -1});
+    auto num_samples = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{-1});
+    auto multinomial = std::make_shared<op::v13::Multinomial>(probs, num_samples, element::i32, false, false, 0, 0);
+
+    // Test Partial Shape 2D input, unknown num_samples and probs shape
+    std::vector<PartialShape> partial_input_shapes = {PartialShape{4}, PartialShape{-1}};
+    auto partial_output_shapes = shape_infer(multinomial.get(), partial_input_shapes, make_tensor_accessor());
+    ASSERT_EQ(partial_output_shapes[0], PartialShape({-1, -1}));
+}
+
+TEST(StaticShapeInferenceTest, MultinomialDynamicShapeInferenceTestDynamicProbsDynamicNumSamplesDynamicRank) {
+    auto probs = std::make_shared<op::v0::Parameter>(element::f32, PartialShape::dynamic());
+    auto num_samples = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{-1});
+    auto multinomial = std::make_shared<op::v13::Multinomial>(probs, num_samples, element::i32, false, false, 0, 0);
+
+    // Test Partial Shape dynamic input, unknown num_samples and probs shape
+    std::vector<PartialShape> partial_input_shapes = {PartialShape{4}, PartialShape{-1}};
+    auto partial_output_shapes = shape_infer(multinomial.get(), partial_input_shapes, make_tensor_accessor());
+    ASSERT_EQ(partial_output_shapes[0], PartialShape::dynamic());
 }
