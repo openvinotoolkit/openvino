06b542f31117a7a30188d2c49b1c57b17dbd23d4
Mateusz <mateusz.mikolajczyk@intel.com>
2023-10-03 14:04:13 +0000

    change reference

--- src/core/reference/include/openvino/reference/bitwise_and.hpp
+++ src/core/reference/include/openvino/reference/bitwise_and.hpp
@@ -11,18 +11,6 @@
 
 namespace ov {
 namespace reference {
-namespace func {
-// Check for char datatype used by ov::element::boolean
-template <class T, typename std::enable_if<std::is_same<typename std::decay<T>::type, char>::value>::type* = nullptr>
-T bitwise_and(const T arg0, const T arg1) {
-    return static_cast<T>(std::bit_and<bool>()(arg0, arg1));
-}
-
-template <class T, typename std::enable_if<!std::is_same<typename std::decay<T>::type, char>::value>::type* = nullptr>
-T bitwise_and(const T arg0, const T arg1) {
-    return static_cast<T>(std::bit_and<T>()(arg0, arg1));
-}
-}  // namespace func
 /**
  * @brief Reference implementation of binary elementwise bitwise AND operator.
  *
@@ -33,14 +21,34 @@ T bitwise_and(const T arg0, const T arg1) {
  * @param arg_shape1      Input 1 shape.
  * @param broadcast_spec  Broadcast specification mode.
  */
-template <class T>
+template <class T, typename std::enable_if<std::is_same<typename std::decay<T>::type, char>::value>::type* = nullptr>
+// Check for char datatype used by ov::element::boolean
 void bitwise_and(const T* arg0,
                  const T* arg1,
                  T* out,
                  const Shape& arg0_shape,
                  const Shape& arg1_shape,
                  const op::AutoBroadcastSpec& broadcast_spec) {
-    autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, func::bitwise_and<T>);
+    autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, std::bit_and<bool>());
+}
+/**
+ * @brief Reference implementation of binary elementwise bitwise AND operator.
+ *
+ * @param arg0            Pointer to input 0 data.
+ * @param arg1            Pointer to input 1 data.
+ * @param out             Pointer to output data.
+ * @param arg_shape0      Input 0 shape.
+ * @param arg_shape1      Input 1 shape.
+ * @param broadcast_spec  Broadcast specification mode.
+ */
+template <class T, typename std::enable_if<!std::is_same<typename std::decay<T>::type, char>::value>::type* = nullptr>
+void bitwise_and(const T* arg0,
+                 const T* arg1,
+                 T* out,
+                 const Shape& arg0_shape,
+                 const Shape& arg1_shape,
+                 const op::AutoBroadcastSpec& broadcast_spec) {
+    autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, std::bit_and<T>());
 }
 }  // namespace reference
 }  // namespace ov
--- src/core/reference/include/openvino/reference/bitwise_xor.hpp
+++ src/core/reference/include/openvino/reference/bitwise_xor.hpp
@@ -11,18 +11,6 @@
 
 namespace ov {
 namespace reference {
-namespace func {
-// Check for char datatype used by ov::element::boolean
-template <class T, typename std::enable_if<std::is_same<typename std::decay<T>::type, char>::value>::type* = nullptr>
-T bitwise_xor(const T arg0, const T arg1) {
-    return static_cast<T>(std::bit_xor<bool>()(arg0, arg1));
-}
-
-template <class T, typename std::enable_if<!std::is_same<typename std::decay<T>::type, char>::value>::type* = nullptr>
-T bitwise_xor(const T arg0, const T arg1) {
-    return static_cast<T>(std::bit_xor<T>()(arg0, arg1));
-}
-}  // namespace func
 /**
  * @brief Reference implementation of binary elementwise bitwise XOR operator.
  *
@@ -33,14 +21,34 @@ T bitwise_xor(const T arg0, const T arg1) {
  * @param arg_shape1      Input 1 shape.
  * @param broadcast_spec  Broadcast specification mode.
  */
-template <class T>
+template <class T, typename std::enable_if<std::is_same<typename std::decay<T>::type, char>::value>::type* = nullptr>
+// Check for char datatype used by ov::element::boolean
 void bitwise_xor(const T* arg0,
                  const T* arg1,
                  T* out,
                  const Shape& arg0_shape,
                  const Shape& arg1_shape,
                  const op::AutoBroadcastSpec& broadcast_spec) {
-    autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, func::bitwise_xor<T>);
+    autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, std::bit_xor<bool>());
+}
+/**
+ * @brief Reference implementation of binary elementwise bitwise XOR operator.
+ *
+ * @param arg0            Pointer to input 0 data.
+ * @param arg1            Pointer to input 1 data.
+ * @param out             Pointer to output data.
+ * @param arg_shape0      Input 0 shape.
+ * @param arg_shape1      Input 1 shape.
+ * @param broadcast_spec  Broadcast specification mode.
+ */
+template <class T, typename std::enable_if<!std::is_same<typename std::decay<T>::type, char>::value>::type* = nullptr>
+void bitwise_xor(const T* arg0,
+                 const T* arg1,
+                 T* out,
+                 const Shape& arg0_shape,
+                 const Shape& arg1_shape,
+                 const op::AutoBroadcastSpec& broadcast_spec) {
+    autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, std::bit_xor<T>());
 }
 }  // namespace reference
 }  // namespace ov
