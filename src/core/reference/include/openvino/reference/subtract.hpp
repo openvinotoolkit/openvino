83540c9fa3e4380a1908adfcb803faaaa49101ac
Pawel Raasz <pawel.raasz@intel.com>
2023-10-03 10:55:20 +0200

    [core]Api 2.0/migrate Subtract op to new API (#20108)
    * Migrate Subtract to new API
    
    * Sync has_evaluate precision with evaluate

--- src/core/reference/include/openvino/reference/subtract.hpp
+++ src/core/reference/include/openvino/reference/subtract.hpp
@@ -4,31 +4,36 @@
 
 #pragma once
 
-#include <cstddef>
+#include <algorithm>
+#include <functional>
 
-#include "ngraph/op/util/attr_types.hpp"
-#include "ngraph/shape.hpp"
 #include "openvino/reference/autobroadcast_binop.hpp"
 
 namespace ov {
 namespace reference {
-template <typename T>
+template <class T>
 void subtract(const T* arg0, const T* arg1, T* out, size_t count) {
-    for (size_t i = 0; i < count; i++) {
-        out[i] = arg0[i] - arg1[i];
-    }
+    std::transform(arg0, std::next(arg0, count), arg1, out, std::minus<T>());
 }
 
-template <typename T>
+/**
+ * @brief Reference implementation of binary elementwise Subtract operator.
+ *
+ * @param arg0            Pointer to input 0 data.
+ * @param arg1            Pointer to input 1 data.
+ * @param out             Pointer to output data.
+ * @param arg_shape0      Input 0 shape.
+ * @param arg_shape1      Input 1 shape.
+ * @param broadcast_spec  Broadcast specification mode.
+ */
+template <class T>
 void subtract(const T* arg0,
               const T* arg1,
               T* out,
               const Shape& arg0_shape,
               const Shape& arg1_shape,
               const op::AutoBroadcastSpec& broadcast_spec) {
-    autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, [](T x, T y) -> T {
-        return x - y;
-    });
+    autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, std::minus<T>());
 }
 }  // namespace reference
 }  // namespace ov
