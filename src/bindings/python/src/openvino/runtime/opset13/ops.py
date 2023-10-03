603abe92cc3768e7b9e32cd7ed3f67655c727389
mitruska <katarzyna.mitrus@intel.com>
2023-10-03 09:49:17 +0200

    Add nms_rotated op to py API

--- src/bindings/python/src/openvino/runtime/opset13/ops.py
+++ src/bindings/python/src/openvino/runtime/opset13/ops.py
@@ -19,3 +19,42 @@ _get_node_factory_opset13 = partial(_get_node_factory, "opset13")
 
 
 # -------------------------------------------- ops ------------------------------------------------
+
+
+@nameable_op
+def nms_rotated(
+    boxes: NodeInput,
+    scores: NodeInput,
+    max_output_boxes_per_class: NodeInput,
+    iou_threshold: NodeInput,
+    score_threshold: NodeInput,
+    sort_result_descending: Optional[bool] = True,
+    output_type: Optional[str] = "i64",
+    clockwise: Optional[bool] = True,
+    name: Optional[str] = None,
+) -> Node:
+    """Return a node which performs NMSRotated.
+
+    :param boxes: Tensor with box coordinates [x_ctr, y_ctr, width, height, angle_radians].
+    :param scores: Tensor with box scores.
+    :param max_output_boxes_per_class: Tensor Specifying maximum number of boxes
+                                        to be selected per class.
+    :param iou_threshold: Tensor specifying intersection over union threshold
+    :param score_threshold: Tensor specifying minimum score to consider box for the processing.
+    :param box_encoding: Format of boxes data encoding.
+    :param sort_result_descending: Flag that specifies whenever it is necessary to sort selected
+                                   boxes across batches or not.
+    :param output_type: Output element type.
+    :param clockwise: Flag that specifies direction of the box rotation.
+    :return: The new node which performs NMSRotated
+    """
+
+    inputs = as_nodes(boxes, scores, max_output_boxes_per_class, iou_threshold, score_threshold)
+
+    attributes = {
+        "sort_result_descending": sort_result_descending,
+        "output_type": output_type,
+        "clockwise": clockwise
+    }
+
+    return _get_node_factory_opset13().create("NMSRotated", inputs, attributes)
