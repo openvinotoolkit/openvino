55eee75c880bdad465e854543b27b83aea76bd4b
Ilya Churaev <ilyachur@gmail.com>
2023-10-04 11:03:54 +0400

    Moved migrated classes to ov::test namespace

--- src/tests/functional/shared_test_classes/include/shared_test_classes/subgraph/preprocess.hpp
+++ src/tests/functional/shared_test_classes/include/shared_test_classes/subgraph/preprocess.hpp
@@ -4,29 +4,30 @@
 
 #pragma once
 
-#include <tuple>
-#include <string>
-#include <vector>
 #include <memory>
+#include <string>
+#include <tuple>
+#include <vector>
 
-#include "shared_test_classes/base/ov_subgraph.hpp"
 #include "ngraph_functions/builders.hpp"
 #include "ngraph_functions/preprocess/preprocess_builders.hpp"
 #include "ngraph_functions/utils/ngraph_helpers.hpp"
+#include "shared_test_classes/base/ov_subgraph.hpp"
 
-namespace SubgraphTestsDefinitions {
+namespace ov {
+namespace test {
 
-using preprocessParamsTuple = std::tuple<
-        ov::builder::preprocess::preprocess_func,  // Function with preprocessing
-        std::string>;                              // Device name
+using preprocessParamsTuple = std::tuple<ov::builder::preprocess::preprocess_func,  // Function with preprocessing
+                                         std::string>;                              // Device name
 
 class PrePostProcessTest : public testing::WithParamInterface<preprocessParamsTuple>,
                            virtual public ov::test::SubgraphBaseTest {
 public:
-    static std::string getTestCaseName(const testing::TestParamInfo<preprocessParamsTuple> &obj);
+    static std::string getTestCaseName(const testing::TestParamInfo<preprocessParamsTuple>& obj);
 
 protected:
     void SetUp() override;
 };
 
-}  // namespace SubgraphTestsDefinitions
+}  // namespace test
+}  // namespace ov
--- src/tests/functional/shared_test_classes/src/subgraph/preprocess.cpp
+++ src/tests/functional/shared_test_classes/src/subgraph/preprocess.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "shared_test_classes/subgraph/preprocess.hpp"
+
 #include "ngraph_functions/preprocess/preprocess_builders.hpp"
 #include "openvino/core/preprocess/pre_post_process.hpp"
 
@@ -10,9 +11,10 @@ using namespace ov;
 using namespace ov::preprocess;
 using namespace ov::builder::preprocess;
 
-namespace SubgraphTestsDefinitions {
-std::string PrePostProcessTest::getTestCaseName(
-        const testing::TestParamInfo<preprocessParamsTuple> &obj) {
+namespace ov {
+namespace test {
+
+std::string PrePostProcessTest::getTestCaseName(const testing::TestParamInfo<preprocessParamsTuple>& obj) {
     std::string targetName;
     preprocess_func func;
 
@@ -29,7 +31,7 @@ void PrePostProcessTest::SetUp() {
     std::tie(func, targetDevice) = GetParam();
     function = func.m_function();
     rel_threshold = func.m_accuracy;
-    functionRefs = ngraph::clone_function(*function);
+    functionRefs = function->clone();
     abs_threshold = func.m_accuracy;
     if (func.m_shapes.empty()) {
         for (const auto& input : function->inputs()) {
@@ -43,4 +45,5 @@ TEST_P(PrePostProcessTest, CompareWithRefs) {
     run();
 }
 
-}  // namespace SubgraphTestsDefinitions
+}  // namespace test
+}  // namespace ov
