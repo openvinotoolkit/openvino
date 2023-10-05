90b283ef969a0043c835fb1f431018443c1b87b5
Ilya Churaev <ilyachur@gmail.com>
2023-10-05 13:11:01 +0400

    More fixs

--- src/core/src/op/elu.cpp
+++ src/core/src/op/elu.cpp
@@ -6,27 +6,25 @@
 
 #include "itt.hpp"
 
-using namespace ov;
-
-op::v0::Elu::Elu(const Output<Node>& data, const double alpha)
+ov::op::v0::Elu::Elu(const Output<Node>& data, const double alpha)
     : util::UnaryElementwiseArithmetic(data),
       m_alpha{alpha} {
     constructor_validate_and_infer_types();
 }
 
-bool op::v0::Elu::visit_attributes(AttributeVisitor& visitor) {
+bool ov::op::v0::Elu::visit_attributes(AttributeVisitor& visitor) {
     OV_OP_SCOPE(v0_Elu_visit_attributes);
     visitor.on_attribute("alpha", m_alpha);
     return true;
 }
 
-void op::v0::Elu::validate_and_infer_types() {
+void ov::op::v0::Elu::validate_and_infer_types() {
     OV_OP_SCOPE(v0_Elu_validate_and_infer_types);
     set_output_size(1);
     set_output_type(0, get_input_element_type(0), get_input_partial_shape(0));
 }
 
-std::shared_ptr<Node> op::v0::Elu::clone_with_new_inputs(const OutputVector& new_args) const {
+std::shared_ptr<ov::Node> ov::op::v0::Elu::clone_with_new_inputs(const OutputVector& new_args) const {
     OV_OP_SCOPE(v0_Elu_clone_with_new_inputs);
     check_new_args_count(this, new_args);
     return std::make_shared<Elu>(new_args.at(0), m_alpha);
