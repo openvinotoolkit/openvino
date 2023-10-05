fbb84975e13defa2d2376e262d6951a692c02923
sgolebiewski-intel <sebastianx.golebiewski@intel.com>
2023-10-05 07:20:09 +0200

    Direct Github link to a specific notebook

--- docs/nbdoc/nbdoc.py
+++ docs/nbdoc/nbdoc.py
@@ -19,6 +19,7 @@ from consts import (
     no_binder_template,
     repo_directory,
     repo_name,
+    repo_branch,
     repo_owner,
     rst_template,
     section_names,
@@ -99,6 +100,7 @@ class NbProcessor:
             "owner": repo_owner,
             "repo": repo_name,
             "folder": repo_directory,
+            "branch": repo_branch,
         }
         self.colab_data = {
             "owner": repo_owner,
