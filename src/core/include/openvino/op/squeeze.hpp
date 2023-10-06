46a50f6c2ab03dfe1382b224392442a5c7c4572c
Raasz, Pawel <pawel.raasz@intel.com>
2023-10-06 07:34:00 +0200

    Migrate Squeeze to new API

--- src/core/include/openvino/op/squeeze.hpp
+++ src/core/include/openvino/op/squeeze.hpp
@@ -22,9 +22,7 @@ public:
 
     bool visit_attributes(AttributeVisitor& visitor) override;
     void validate_and_infer_types() override;
-    OPENVINO_SUPPRESS_DEPRECATED_START
-    bool evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const override;
-    OPENVINO_SUPPRESS_DEPRECATED_END
+    bool evaluate(TensorVector& outputs, const TensorVector& inputs) const override;
     bool has_evaluate() const override;
     bool evaluate_lower(TensorVector& outputs) const override;
     bool evaluate_upper(TensorVector& outputs) const override;
