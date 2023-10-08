8f029a983c3a501895ab9b240db4e35d717c0063
HU Yuan2 <yuan2.hu@intel.com>
2023-10-08 14:53:59 +0800

    revert test case order in matmul
    Signed-off-by: HU Yuan2 <yuan2.hu@intel.com>

--- src/plugins/intel_cpu/tests/unit/shape_inference_test/custom_shape_infer/matmul.cpp
+++ src/plugins/intel_cpu/tests/unit/shape_inference_test/custom_shape_infer/matmul.cpp
@@ -127,17 +127,16 @@ TEST_P(CPUMatMulTest, transpose_inputs_a_b) {
 /** \brief Use transpose order -> output shape dimensions shall be as transpose order. */
 INSTANTIATE_TEST_SUITE_P(CpuShapeInfer,
                          CPUMatMulTest,
-                         // only support rankA = rankB
                          Values(make_tuple(StaticShape({1}), StaticShape({1})),
-                                make_tuple(StaticShape({3}), StaticShape({3})),
-                                make_tuple(StaticShape({5, 2}), StaticShape({2, 6})),
-                                make_tuple(StaticShape({10, 8, 9, 2}), StaticShape({10, 8, 2, 8})),
-                                make_tuple(StaticShape({3, 1, 4, 3, 4}), StaticShape({3, 2, 1, 4, 1})),
                                 make_tuple(StaticShape({1}), StaticShape({1, 3})),
                                 make_tuple(StaticShape({1}), StaticShape({1, 1, 3})),
                                 make_tuple(StaticShape({3, 1}), StaticShape({1})),
                                 make_tuple(StaticShape({3, 2, 1}), StaticShape({1})),
-                                make_tuple(StaticShape({2, 1, 2}), StaticShape({2, 6}))),
+                                make_tuple(StaticShape({3}), StaticShape({3})),
+                                make_tuple(StaticShape({5, 2}), StaticShape({2, 6})),
+                                make_tuple(StaticShape({2, 1, 2}), StaticShape({2, 6})),
+                                make_tuple(StaticShape({10, 8, 9, 2}), StaticShape({10, 8, 2, 8})),
+                                make_tuple(StaticShape({3, 1, 4, 3, 4}), StaticShape({3, 2, 1, 4, 1}))),
                          CPUMatMulTest::getTestCaseName);
 
 } // namespace cpu_shape_infer
