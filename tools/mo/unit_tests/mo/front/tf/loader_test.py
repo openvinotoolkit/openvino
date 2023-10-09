0773b2f3271406b504c45cb0804f7699b944428c
PRATHAM-SPS <ingawalepratham@gmail.com>
2023-10-09 22:08:43 +0530

    revertback:- loader_test.py

--- tools/mo/unit_tests/mo/front/tf/loader_test.py
+++ tools/mo/unit_tests/mo/front/tf/loader_test.py
@@ -5,20 +5,17 @@ import unittest.mock
 from io import StringIO
 from unittest.mock import Mock, MagicMock
 
+from generator import generate, generator
 
 from openvino.tools.mo.front.tf.loader import load_tf_graph_def
 
 
+@generator
 class TestLoader(unittest.TestCase):
-    def test_helper_print_ckpt(self):
-        test_cases = [
-            ('/path/to/somewhere/my_checkpoint.ckpt', '/path/to/somewhere/my_meta_graph.meta'),
-            # Add more test cases as needed
-        ]
-
-        for idx, (path, meta_graph_file) in enumerate(test_cases):
-            with self.subTest(test_case=f"Test case {idx+1}"):
-                with unittest.mock.patch('sys.stdout', new_callable=StringIO) as out:
-                    mock = Mock(__bool__=MagicMock(side_effect=Exception()))
-                    self.assertRaises(Exception, load_tf_graph_def, path, meta_graph_file=mock)
-                    self.assertRegex(out.getvalue(), r'\[ WARNING ] The value for the --input_model command line parameter ends with "\.ckpt"')
+    @generate('/path/to/somewhere/my_checkpoint.ckpt', '/path/to/somewhere/my_meta_graph.meta')
+    @unittest.mock.patch('sys.stdout', new_callable=StringIO)
+    def test_helper_print_ckpt(self, path, out):
+        mock = Mock(__bool__=MagicMock(side_effect=Exception()))
+        self.assertRaises(Exception, load_tf_graph_def, path, meta_graph_file=mock)
+        self.assertRegex(out.getvalue(),
+                         r'\[ WARNING ] The value for the --input_model command line parameter ends with "\.ckpt"')
