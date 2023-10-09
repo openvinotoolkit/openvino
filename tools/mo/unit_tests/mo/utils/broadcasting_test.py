0a0f0543e66df9a4ac81c136c65a8283eb522ff0
PRATHAM-SPS <ingawalepratham@gmail.com>
2023-10-09 14:07:10 +0530

    pytest:- broadcasting_test.py

--- tools/mo/unit_tests/mo/utils/broadcasting_test.py
+++ tools/mo/unit_tests/mo/utils/broadcasting_test.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2018-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-import unittest
+import pytest
 
 import numpy as np
 
@@ -10,36 +10,30 @@ from openvino.tools.mo.utils.broadcasting import uni_directional_broadcasting, u
     bi_directional_shape_broadcasting
 
 
-class TestingBroadcasting(unittest.TestCase):
-    def test_uni_directional_broadcasting(self):
-        test_cases = [
-            ([], [20, 30, 10], [20, 30, 10]),
-            ([1], [20, 30, 10], [20, 30, 10]),
-            ([1, 1, 10], [20, 30, 10], [20, 30, 10]),
-            ([20, 1, 10], [20, 30, 10], [20, 30, 10]),
-            ([20, 30, 10], [20, 30, 10], [20, 30, 10]),
-            ([20, 30, 10], [5, 7, 20, 30, 10], [5, 7, 20, 30, 10]),
-            ([1, 2], [20, 3, 10, 2], [20, 3, 10, 2]),
-            ([1, 1], [1], None),
-            ([5, 10], [1, 10], None),
-        ]
+class TestingBroadcasting():
+    @pytest.mark.parametrize("input_shape, target_shape, expected_shape",[([], [20, 30, 10], [20, 30, 10]),
+                ([1], [20, 30, 10], [20, 30, 10]),
+                ([1, 1, 10], [20, 30, 10], [20, 30, 10]),
+                ([20, 1, 10], [20, 30, 10], [20, 30, 10]),
+                ([20, 30, 10], [20, 30, 10], [20, 30, 10]),
+                ([20, 30, 10], [5, 7, 20, 30, 10], [5, 7, 20, 30, 10]),
+                ([1, 2], [20, 3, 10, 2], [20, 3, 10, 2]),
+                ([1, 1], [1], None),
+                ([5, 10], [1, 10], None),
+                ])
+    def test_uni_directional_broadcasting(self, input_shape, target_shape, expected_shape):
+        assert np.array_equal(uni_directional_shape_broadcasting(input_shape, target_shape), expected_shape)
 
-        for idx, (input_shape, target_shape, expected_shape) in enumerate(test_cases):
-            with self.subTest(test_cases=idx):
-                self.assertTrue(np.array_equal(uni_directional_shape_broadcasting(input_shape, target_shape),
-                                               expected_shape))
+        input_value = np.array(np.random.rand(*input_shape))
+        if expected_shape is not None:
+            expected_value = np.broadcast_to(input_value, int64_array(target_shape))
+            assert np.array_equal(uni_directional_broadcasting(input_value, int64_array(target_shape)),
+                                           expected_value)
+        else:
+            with pytest.raises(Exception,match = '.*cannot be uni-directionally broadcasted.*'):
+                uni_directional_broadcasting(input_value, int64_array(target_shape))
 
-                input_value = np.array(np.random.rand(*input_shape))
-                if expected_shape is not None:
-                    expected_value = np.broadcast_to(input_value, int64_array(target_shape))
-                    self.assertTrue(np.array_equal(uni_directional_broadcasting(input_value, int64_array(target_shape)),
-                                                expected_value))
-                else:
-                    with self.assertRaisesRegex(Exception, '.*cannot be uni-directionally broadcasted.*'):
-                        uni_directional_broadcasting(input_value, int64_array(target_shape))
-
-    def test_uni_directional_shape_broadcasting(self):
-        test_cases=[([], [20, 30, 10], [20, 30, 10]),
+    @pytest.mark.parametrize("input_shape, target_shape, expected_shape",[([], [20, 30, 10], [20, 30, 10]),
                 ([1], [20, 30, 10], [20, 30, 10]),
                 ([1, 1, 10], [20, 30, 10], [20, 30, 10]),
                 ([20, 1, 10], [20, 30, 10], [20, 30, 10]),
@@ -58,18 +52,15 @@ class TestingBroadcasting(unittest.TestCase):
                 (shape_array([1]), shape_array([dynamic_dimension_value]), shape_array([dynamic_dimension_value])),
                 (shape_array([dynamic_dimension_value]), shape_array([6]), shape_array([6])),
                 (shape_array([6]), shape_array([dynamic_dimension_value]), shape_array([6])),
-                ]
-        for idx, (input_shape, target_shape, expected_shape) in enumerate(test_cases):
-            with self.subTest(test_cases=idx):
-                result = uni_directional_shape_broadcasting(input_shape, target_shape)
-                if expected_shape is None:
-                    self.assertIsNone(result)
-                else:
-                    self.assertTrue(strict_compare_tensors(result, expected_shape))
+                ])
+    def test_uni_directional_shape_broadcasting(self, input_shape, target_shape, expected_shape):
+        result = uni_directional_shape_broadcasting(input_shape, target_shape)
+        if expected_shape is None:
+            assert result is None
+        else:
+            assert strict_compare_tensors(result, expected_shape)
 
-
-    def test_bi_directional_shape_broadcasting(self):
-        test_cases=[([], [20, 30, 10], [20, 30, 10]),
+    @pytest.mark.parametrize("input_shape, target_shape, expected_shape",[([], [20, 30, 10], [20, 30, 10]),
                 ([1], [20, 30, 10], [20, 30, 10]),
                 ([1, 1, 10], [20, 30, 10], [20, 30, 10]),
                 ([20, 1, 10], [20, 30, 10], [20, 30, 10]),
@@ -88,11 +79,10 @@ class TestingBroadcasting(unittest.TestCase):
                 (shape_array([1]), shape_array([dynamic_dimension_value]), shape_array([dynamic_dimension_value])),
                 (shape_array([dynamic_dimension_value]), shape_array([6]), shape_array([6])),
                 (shape_array([6]), shape_array([dynamic_dimension_value]), shape_array([6])),
-                ]
-        for idx, (input_shape, target_shape, expected_shape) in enumerate(test_cases):
-            with self.subTest(test_cases=idx):
-                result = bi_directional_shape_broadcasting(input_shape, target_shape)
-                if expected_shape is None:
-                    self.assertIsNone(result)
-                else:
-                    self.assertTrue(strict_compare_tensors(result, expected_shape))
+                ])
+    def test_bi_directional_shape_broadcasting(self, input_shape, target_shape, expected_shape):
+        result = bi_directional_shape_broadcasting(input_shape, target_shape)
+        if expected_shape is None:
+            assert result is None
+        else:
+            assert strict_compare_tensors(result, expected_shape)
