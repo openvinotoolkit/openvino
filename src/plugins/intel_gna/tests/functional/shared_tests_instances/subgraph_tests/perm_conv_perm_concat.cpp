4e9bc67e4e84fbd546ccb3b67778179acd9ecf62
Ilya Churaev <ilyachur@gmail.com>
2023-10-10 14:56:33 +0400

    Migrate subgraph tests till quatized subgraph

--- src/plugins/intel_gna/tests/functional/shared_tests_instances/subgraph_tests/perm_conv_perm_concat.cpp
+++ src/plugins/intel_gna/tests/functional/shared_tests_instances/subgraph_tests/perm_conv_perm_concat.cpp
@@ -6,14 +6,13 @@
 
 #include <vector>
 
-#include "common_test_utils/test_constants.hpp"
 namespace {
-std::vector<std::array<size_t, 4>> input_shapes{
+std::vector<ov::Shape> input_shapes{
     {1, 1, 7, 32},
     {1, 1, 8, 16},
 };
 
-std::vector<std::array<size_t, 2>> kernel_shapes{
+std::vector<ov::Shape> kernel_shapes{
     {1, 3},
     {1, 5},
 };
@@ -23,16 +22,17 @@ std::vector<size_t> output_channels{
     64,
 };
 
-std::vector<InferenceEngine::Precision> netPrecisions = {
-    InferenceEngine::Precision::FP32,
-    InferenceEngine::Precision::FP16,
+std::vector<ov::element::Type> netPrecisions = {
+    ov::element::f32,
+    ov::element::f16,
 };
 
-std::vector<std::map<std::string, std::string>> configs = {{{"GNA_DEVICE_MODE", "GNA_SW_EXACT"}},
-                                                           {{"GNA_DEVICE_MODE", "GNA_SW_FP32"}}};
+std::vector<ov::AnyMap> configs = {{{"GNA_DEVICE_MODE", "GNA_SW_EXACT"}}, {{"GNA_DEVICE_MODE", "GNA_SW_FP32"}}};
 }  // namespace
 
-namespace SubgraphTestsDefinitions {
+namespace ov {
+namespace test {
+
 INSTANTIATE_TEST_SUITE_P(smoke_basic,
                          PermConvPermConcat,
                          ::testing::Combine(::testing::ValuesIn(netPrecisions),
@@ -42,4 +42,6 @@ INSTANTIATE_TEST_SUITE_P(smoke_basic,
                                             ::testing::ValuesIn(output_channels),
                                             ::testing::ValuesIn(configs)),
                          PermConvPermConcat::getTestCaseName);
-}  // namespace SubgraphTestsDefinitions
+
+}
+}  // namespace ov
