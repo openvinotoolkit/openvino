6e256a5d4de1247b4a5dddbaefd5e9b70e5ac7d9
Kazantsev, Roman <roman.kazantsev@intel.com>
2023-10-10 14:46:19 +0400

    Fix GatherTree translator

--- src/frontends/tensorflow_common/src/op/gather_tree.cpp
+++ src/frontends/tensorflow_common/src/op/gather_tree.cpp
@@ -15,7 +15,7 @@ namespace tensorflow {
 namespace op {
 
 OutputVector translate_gather_tree_op(const NodeContext& node) {
-    default_op_checks(node, 4, {"GatherTree"});
+    default_op_checks(node, 4, {"GatherTree", "Addons>GatherTree"});
     auto step_ids = node.get_input(0);
     auto parent_ids = node.get_input(1);
     auto max_sequence_lengths = node.get_input(2);
