34c58087a6a91dfd0773f41cadfc569ed9726634
Oleg Pipikin <oleg.pipikin@intel.com>
2023-10-10 11:25:57 +0200

    Refactor BinaryConvolutionLayerTest and BucketizeLayerTest (#19654)
    * Refactor BinaryConvolutionLayerTest
    
    * Refactor BucketizeLayerTest

---
+++ src/tests/functional/shared_test_classes/include/shared_test_classes/single_op/binary_convolution.hpp
@@ -0,0 +1,42 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <tuple>
+#include <vector>
+#include <string>
+#include <memory>
+
+#include "shared_test_classes/base/ov_subgraph.hpp"
+
+namespace ov {
+namespace test {
+
+using binConvSpecificParams = std::tuple<
+    std::vector<size_t>,            // Kernel size
+    std::vector<size_t>,            // Strides
+    std::vector<ptrdiff_t>,         // Pads begin
+    std::vector<ptrdiff_t>,         // Pads end
+    std::vector<size_t>,            // Dilations
+    size_t,                         // Num Output channels
+    ov::op::PadType,                // Padding type
+    float>;                         // Padding value
+
+using binaryConvolutionTestParamsSet = std::tuple<
+    binConvSpecificParams,          //
+    ov::element::Type,              // Model Type
+    std::vector<InputShape>,        // Input shape
+    std::string>;                   // Device name
+
+class BinaryConvolutionLayerTest : public testing::WithParamInterface<binaryConvolutionTestParamsSet>,
+                                   virtual public ov::test::SubgraphBaseTest {
+public:
+    static std::string getTestCaseName(const testing::TestParamInfo<binaryConvolutionTestParamsSet>& obj);
+protected:
+    void SetUp() override;
+};
+
+} // namespace test
+} // namespace ov
---
+++ src/tests/functional/shared_test_classes/include/shared_test_classes/single_op/bucketize.hpp
@@ -0,0 +1,33 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <tuple>
+#include <string>
+
+#include "shared_test_classes/base/ov_subgraph.hpp"
+
+namespace ov {
+namespace test {
+
+using bucketizeParamsTuple = std::tuple<
+    std::vector<InputShape>,        // data shape, bucket shape
+    bool,                           // Right edge of interval
+    ov::element::Type,              // Data input precision
+    ov::element::Type,              // Buckets input precision
+    ov::element::Type,              // Output precision
+    std::string>;                   // Device name
+
+class BucketizeLayerTest : public testing::WithParamInterface<bucketizeParamsTuple>,
+                           virtual public ov::test::SubgraphBaseTest {
+public:
+    static std::string getTestCaseName(const testing::TestParamInfo<bucketizeParamsTuple>& obj);
+protected:
+    void SetUp() override;
+};
+
+
+} // namespace test
+} // namespace ov
