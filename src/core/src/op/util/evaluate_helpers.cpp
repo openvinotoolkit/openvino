edd607976f29d9c878be32cf6534635520fb2a25
Pawel Raasz <pawel.raasz@intel.com>
2023-10-11 11:50:29 +0200

    [core]Migrate Eye to new API (#20258)
    * Migrate Eye to new API
    
    * Fix `matrix_offset` initialization
    
    * get_tensors_shapes -> get_tensors_partial_shapes

--- src/core/src/op/util/evaluate_helpers.cpp
+++ src/core/src/op/util/evaluate_helpers.cpp
@@ -4,6 +4,8 @@
 
 #include "ngraph/op/util/evaluate_helpers.hpp"
 
+#include "openvino/op/util/evaluate_helpers.hpp"
+
 namespace ngraph {
 AxisSet get_normalized_axes_from_tensor(const HostTensorPtr tensor,
                                         const ngraph::Rank& rank,
@@ -15,3 +17,18 @@ AxisSet get_normalized_axes_from_tensor(const HostTensorPtr tensor,
     return AxisSet{normalized_axes};
 }
 }  // namespace ngraph
+
+namespace ov {
+namespace op {
+namespace util {
+std::vector<PartialShape> get_tensors_partial_shapes(const TensorVector& tensors) {
+    std::vector<PartialShape> shapes;
+    shapes.reserve(tensors.size());
+    for (const auto& t : tensors) {
+        shapes.emplace_back(t.get_shape());
+    }
+    return shapes;
+}
+}  // namespace util
+}  // namespace op
+}  // namespace ov
