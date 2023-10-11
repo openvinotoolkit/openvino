4e4d29e42f65381db2954c14cf63762e2efedbdc
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2023-10-11 21:44:41 +0200

    [Template plugin] Unify headers inclusion (#20326)
    * Use precise op version - Parameter
    
    * Use precise op version - Constant
    
    * Use precise op version - Result
    
    * Use precise op version - Add, Concat, Subtract
    
    * Use precise op version - NMS
    
    * Use precise op version - TopK, Broadcast
    
    * Use precise op version
    
    * Include openvino headers in quotes
    
    * Use precise op version - LSTMCell
    
    * Fix code style

--- src/plugins/template/tests/functional/op_reference/matrix_nms.cpp
+++ src/plugins/template/tests/functional/op_reference/matrix_nms.cpp
@@ -2,18 +2,19 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include "openvino/op/matrix_nms.hpp"
+
 #include <gtest/gtest.h>
 
 #include "base_reference_test.hpp"
-#include "openvino/opsets/opset1.hpp"
-#include "openvino/opsets/opset8.hpp"
+#include "openvino/op/parameter.hpp"
 
 using namespace reference_tests;
 using namespace ov;
 
 namespace {
 struct MatrixNmsParams {
-    MatrixNmsParams(const opset8::MatrixNms::Attributes& attrs,
+    MatrixNmsParams(const op::v8::MatrixNms::Attributes& attrs,
                     const reference_tests::Tensor& boxes,
                     const reference_tests::Tensor& scores,
                     const reference_tests::Tensor& expectedSelectedScores,
@@ -28,7 +29,7 @@ struct MatrixNmsParams {
           expectedValidOutputs(expectedValidOutputs),
           testcaseName(testcaseName) {}
 
-    opset8::MatrixNms::Attributes attrs;
+    op::v8::MatrixNms::Attributes attrs;
     reference_tests::Tensor boxes;
     reference_tests::Tensor scores;
     reference_tests::Tensor expectedSelectedScores;
@@ -71,9 +72,9 @@ public:
 
 private:
     static std::shared_ptr<Model> CreateFunction(const MatrixNmsParams& params) {
-        const auto boxes = std::make_shared<opset1::Parameter>(params.boxes.type, PartialShape::dynamic());
-        const auto scores = std::make_shared<opset1::Parameter>(params.scores.type, PartialShape::dynamic());
-        const auto nms = std::make_shared<opset8::MatrixNms>(boxes, scores, params.attrs);
+        const auto boxes = std::make_shared<op::v0::Parameter>(params.boxes.type, PartialShape::dynamic());
+        const auto scores = std::make_shared<op::v0::Parameter>(params.scores.type, PartialShape::dynamic());
+        const auto nms = std::make_shared<op::v8::MatrixNms>(boxes, scores, params.attrs);
         const auto f = std::make_shared<Model>(nms->outputs(), ParameterVector{boxes, scores});
         return f;
     }
@@ -91,14 +92,14 @@ std::vector<MatrixNmsParams> generateParams() {
     std::vector<MatrixNmsParams> params{
         MatrixNmsParams(
             {
-                opset8::MatrixNms::SortResultType::SCORE,  // sort_result_type
+                op::v8::MatrixNms::SortResultType::SCORE,  // sort_result_type
                 false,                                     // sort_result_across_batch
                 ET_IND,                                    // output_type
                 0.0f,                                      // score_threshold
                 3,                                         // nms_top_k
                 -1,                                        // keep_top_k
                 0,                                         // background_class
-                opset8::MatrixNms::DecayFunction::LINEAR,  // decay_function
+                op::v8::MatrixNms::DecayFunction::LINEAR,  // decay_function
                 2.0f,                                      // gaussian_sigma
                 0.0f,                                      // post_threshold
                 true,                                      // normalized
@@ -135,14 +136,14 @@ std::vector<MatrixNmsParams> generateParams() {
             "matrix_nms_output_type_i64"),
         MatrixNmsParams(
             {
-                opset8::MatrixNms::SortResultType::SCORE,  // sort_result_type
+                op::v8::MatrixNms::SortResultType::SCORE,  // sort_result_type
                 false,                                     // sort_result_across_batch
                 ET_IND,                                    // output_type
                 0.0f,                                      // score_threshold
                 3,                                         // nms_top_k
                 -1,                                        // keep_top_k
                 0,                                         // background_class
-                opset8::MatrixNms::DecayFunction::LINEAR,  // decay_function
+                op::v8::MatrixNms::DecayFunction::LINEAR,  // decay_function
                 2.0f,                                      // gaussian_sigma
                 0.0f,                                      // post_threshold
                 true,                                      // normalized
@@ -179,14 +180,14 @@ std::vector<MatrixNmsParams> generateParams() {
             "matrix_nms_output_type_i32"),
         MatrixNmsParams(
             {
-                opset8::MatrixNms::SortResultType::SCORE,    // sort_result_type
+                op::v8::MatrixNms::SortResultType::SCORE,    // sort_result_type
                 false,                                       // sort_result_across_batch
                 ET_IND,                                      // output_type
                 0.0f,                                        // score_threshold
                 3,                                           // nms_top_k
                 -1,                                          // keep_top_k
                 0,                                           // background_class
-                opset8::MatrixNms::DecayFunction::GAUSSIAN,  // decay_function
+                op::v8::MatrixNms::DecayFunction::GAUSSIAN,  // decay_function
                 2.0f,                                        // gaussian_sigma
                 0.0f,                                        // post_threshold
                 true,                                        // normalized
@@ -223,14 +224,14 @@ std::vector<MatrixNmsParams> generateParams() {
             "matrix_nms_gaussian"),
         MatrixNmsParams(
             {
-                opset8::MatrixNms::SortResultType::SCORE,  // sort_result_type
+                op::v8::MatrixNms::SortResultType::SCORE,  // sort_result_type
                 false,                                     // sort_result_across_batch
                 ET_IND,                                    // output_type
                 0.0f,                                      // score_threshold
                 3,                                         // nms_top_k
                 -1,                                        // keep_top_k
                 0,                                         // background_class
-                opset8::MatrixNms::DecayFunction::LINEAR,  // decay_function
+                op::v8::MatrixNms::DecayFunction::LINEAR,  // decay_function
                 2.0f,                                      // gaussian_sigma
                 0.0f,                                      // post_threshold
                 true,                                      // normalized
@@ -258,14 +259,14 @@ std::vector<MatrixNmsParams> generateParams() {
             "matrix_nms_two_batches_two_classes"),
         MatrixNmsParams(
             {
-                opset8::MatrixNms::SortResultType::SCORE,  // sort_result_type
+                op::v8::MatrixNms::SortResultType::SCORE,  // sort_result_type
                 true,                                      // sort_result_across_batch
                 ET_IND,                                    // output_type
                 0.0f,                                      // score_threshold
                 3,                                         // nms_top_k
                 -1,                                        // keep_top_k
                 -1,                                        // background_class
-                opset8::MatrixNms::DecayFunction::LINEAR,  // decay_function
+                op::v8::MatrixNms::DecayFunction::LINEAR,  // decay_function
                 2.0f,                                      // gaussian_sigma
                 0.5f,                                      // post_threshold
                 true,                                      // normalized
@@ -296,14 +297,14 @@ std::vector<MatrixNmsParams> generateParams() {
             "matrix_nms_two_batches_two_classes_by_score_cross_batch"),
         MatrixNmsParams(
             {
-                opset8::MatrixNms::SortResultType::CLASSID,  // sort_result_type
+                op::v8::MatrixNms::SortResultType::CLASSID,  // sort_result_type
                 true,                                        // sort_result_across_batch
                 ET_IND,                                      // output_type
                 0.0f,                                        // score_threshold
                 3,                                           // nms_top_k
                 -1,                                          // keep_top_k
                 -1,                                          // background_class
-                opset8::MatrixNms::DecayFunction::LINEAR,    // decay_function
+                op::v8::MatrixNms::DecayFunction::LINEAR,    // decay_function
                 2.0f,                                        // gaussian_sigma
                 0.5f,                                        // post_threshold
                 true,                                        // normalized
@@ -334,14 +335,14 @@ std::vector<MatrixNmsParams> generateParams() {
             "matrix_nms_two_batches_two_classes_by_classid_cross_batch"),
         MatrixNmsParams(
             {
-                opset8::MatrixNms::SortResultType::CLASSID,  // sort_result_type
+                op::v8::MatrixNms::SortResultType::CLASSID,  // sort_result_type
                 false,                                       // sort_result_across_batch
                 ET_IND,                                      // output_type
                 0.0f,                                        // score_threshold
                 3,                                           // nms_top_k
                 3,                                           // keep_top_k
                 0,                                           // background_class
-                opset8::MatrixNms::DecayFunction::LINEAR,    // decay_function
+                op::v8::MatrixNms::DecayFunction::LINEAR,    // decay_function
                 2.0f,                                        // gaussian_sigma
                 0.0f,                                        // post_threshold
                 true,                                        // normalized
@@ -369,14 +370,14 @@ std::vector<MatrixNmsParams> generateParams() {
             "matrix_nms_by_keep_top_k"),
         MatrixNmsParams(
             {
-                opset8::MatrixNms::SortResultType::SCORE,  // sort_result_type
+                op::v8::MatrixNms::SortResultType::SCORE,  // sort_result_type
                 false,                                     // sort_result_across_batch
                 ET_IND,                                    // output_type
                 0.0f,                                      // score_threshold
                 3,                                         // nms_top_k
                 -1,                                        // keep_top_k
                 -1,                                        // background_class
-                opset8::MatrixNms::DecayFunction::LINEAR,  // decay_function
+                op::v8::MatrixNms::DecayFunction::LINEAR,  // decay_function
                 2.0f,                                      // gaussian_sigma
                 0.0f,                                      // post_threshold
                 true,                                      // normalized
@@ -400,14 +401,14 @@ std::vector<MatrixNmsParams> generateParams() {
             "matrix_nms_background"),
         MatrixNmsParams(
             {
-                opset8::MatrixNms::SortResultType::SCORE,  // sort_result_type
+                op::v8::MatrixNms::SortResultType::SCORE,  // sort_result_type
                 false,                                     // sort_result_across_batch
                 ET_IND,                                    // output_type
                 0.0f,                                      // score_threshold
                 3,                                         // nms_top_k
                 -1,                                        // keep_top_k
                 -1,                                        // background_class
-                opset8::MatrixNms::DecayFunction::LINEAR,  // decay_function
+                op::v8::MatrixNms::DecayFunction::LINEAR,  // decay_function
                 2.0f,                                      // gaussian_sigma
                 0.0f,                                      // post_threshold
                 true,                                      // normalized
@@ -442,14 +443,14 @@ std::vector<MatrixNmsParams> generateParams() {
             "matrix_nms_flipped_coordinates"),
         MatrixNmsParams(
             {
-                opset8::MatrixNms::SortResultType::SCORE,  // sort_result_type
+                op::v8::MatrixNms::SortResultType::SCORE,  // sort_result_type
                 false,                                     // sort_result_across_batch
                 ET_IND,                                    // output_type
                 0.0f,                                      // score_threshold
                 3,                                         // nms_top_k
                 -1,                                        // keep_top_k
                 -1,                                        // background_class
-                opset8::MatrixNms::DecayFunction::LINEAR,  // decay_function
+                op::v8::MatrixNms::DecayFunction::LINEAR,  // decay_function
                 2.0f,                                      // gaussian_sigma
                 0.8f,                                      // post_threshold
                 true,                                      // normalized
@@ -468,14 +469,14 @@ std::vector<MatrixNmsParams> generateParams() {
             "matrix_nms_post_threshold"),
         MatrixNmsParams(
             {
-                opset8::MatrixNms::SortResultType::SCORE,  // sort_result_type
+                op::v8::MatrixNms::SortResultType::SCORE,  // sort_result_type
                 false,                                     // sort_result_across_batch
                 ET_IND,                                    // output_type
                 0.0f,                                      // score_threshold
                 3,                                         // nms_top_k
                 -1,                                        // keep_top_k
                 -1,                                        // background_class
-                opset8::MatrixNms::DecayFunction::LINEAR,  // decay_function
+                op::v8::MatrixNms::DecayFunction::LINEAR,  // decay_function
                 2.0f,                                      // gaussian_sigma
                 0.3f,                                      // post_threshold
                 true,                                      // normalized
@@ -497,14 +498,14 @@ std::vector<MatrixNmsParams> generateParams() {
             "matrix_nms_identical_boxes"),
         MatrixNmsParams(
             {
-                opset8::MatrixNms::SortResultType::SCORE,  // sort_result_type
+                op::v8::MatrixNms::SortResultType::SCORE,  // sort_result_type
                 false,                                     // sort_result_across_batch
                 ET_IND,                                    // output_type
                 0.0f,                                      // score_threshold
                 2,                                         // nms_top_k
                 -1,                                        // keep_top_k
                 -1,                                        // background_class
-                opset8::MatrixNms::DecayFunction::LINEAR,  // decay_function
+                op::v8::MatrixNms::DecayFunction::LINEAR,  // decay_function
                 2.0f,                                      // gaussian_sigma
                 0.0f,                                      // post_threshold
                 true,                                      // normalized
@@ -523,14 +524,14 @@ std::vector<MatrixNmsParams> generateParams() {
             "matrix_nms_nms_top_k"),
         MatrixNmsParams(
             {
-                opset8::MatrixNms::SortResultType::SCORE,  // sort_result_type
+                op::v8::MatrixNms::SortResultType::SCORE,  // sort_result_type
                 false,                                     // sort_result_across_batch
                 ET_IND,                                    // output_type
                 0.0f,                                      // score_threshold
                 3,                                         // nms_top_k
                 -1,                                        // keep_top_k
                 -1,                                        // background_class
-                opset8::MatrixNms::DecayFunction::LINEAR,  // decay_function
+                op::v8::MatrixNms::DecayFunction::LINEAR,  // decay_function
                 2.0f,                                      // gaussian_sigma
                 0.0f,                                      // post_threshold
                 true,                                      // normalized
@@ -545,14 +546,14 @@ std::vector<MatrixNmsParams> generateParams() {
             "matrix_nms_single_box"),
         MatrixNmsParams(
             {
-                opset8::MatrixNms::SortResultType::SCORE,  // sort_result_type
+                op::v8::MatrixNms::SortResultType::SCORE,  // sort_result_type
                 false,                                     // sort_result_across_batch
                 ET_IND,                                    // output_type
                 2.0f,                                      // score_threshold
                 3,                                         // nms_top_k
                 -1,                                        // keep_top_k
                 -1,                                        // background_class
-                opset8::MatrixNms::DecayFunction::LINEAR,  // decay_function
+                op::v8::MatrixNms::DecayFunction::LINEAR,  // decay_function
                 2.0f,                                      // gaussian_sigma
                 0.0f,                                      // post_threshold
                 true,                                      // normalized
