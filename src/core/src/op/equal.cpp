5ba6938ebb7ad0d15a87d9806315ae86446d4bb0
Raasz, Pawel <pawel.raasz@intel.com>
2023-10-12 13:11:00 +0200

    Migrate Equal to new API

--- src/core/src/op/equal.cpp
+++ src/core/src/op/equal.cpp
@@ -2,183 +2,160 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "ngraph/op/equal.hpp"
+#include "openvino/op/equal.hpp"
 
 #include "bound_evaluate.hpp"
+#include "element_visitor.hpp"
 #include "itt.hpp"
-#include "ngraph/op/constant.hpp"
-#include "ngraph/op/less_eq.hpp"
-#include "ngraph/runtime/host_tensor.hpp"
-#include "openvino/op/ops.hpp"
+#include "openvino/op/less_eq.hpp"
+#include "openvino/op/logical_and.hpp"
+#include "openvino/op/logical_or.hpp"
 #include "openvino/reference/equal.hpp"
+#include "utils.hpp"
 
-using namespace std;
-using namespace ngraph;
-
-OPENVINO_SUPPRESS_DEPRECATED_START
+namespace ov {
+namespace op {
 namespace equal {
 namespace {
-template <element::Type_t ET>
-bool evaluate(const HostTensorPtr& arg0,
-              const HostTensorPtr& arg1,
-              const HostTensorPtr& out,
-              const op::AutoBroadcastSpec& broadcast_spec) {
-    ov::reference::equal(arg0->get_data_ptr<ET>(),
-                         arg1->get_data_ptr<ET>(),
-                         out->get_data_ptr<element::Type_t::boolean>(),
-                         arg0->get_shape(),
-                         arg1->get_shape(),
-                         broadcast_spec);
-    return true;
-}
 
-bool evaluate_equal(const HostTensorPtr& arg0,
-                    const HostTensorPtr& arg1,
-                    const HostTensorPtr& out,
-                    const op::AutoBroadcastSpec& broadcast_spec) {
-    bool rc = true;
-    out->set_broadcast(broadcast_spec, arg0, arg1, element::boolean);
-    switch (arg0->get_element_type()) {
-        OPENVINO_TYPE_CASE(evaluate_equal, boolean, arg0, arg1, out, broadcast_spec);
-        OPENVINO_TYPE_CASE(evaluate_equal, i4, arg0, arg1, out, broadcast_spec);
-        OPENVINO_TYPE_CASE(evaluate_equal, i8, arg0, arg1, out, broadcast_spec);
-        OPENVINO_TYPE_CASE(evaluate_equal, i16, arg0, arg1, out, broadcast_spec);
-        OPENVINO_TYPE_CASE(evaluate_equal, i32, arg0, arg1, out, broadcast_spec);
-        OPENVINO_TYPE_CASE(evaluate_equal, i64, arg0, arg1, out, broadcast_spec);
-        OPENVINO_TYPE_CASE(evaluate_equal, u4, arg0, arg1, out, broadcast_spec);
-        OPENVINO_TYPE_CASE(evaluate_equal, u8, arg0, arg1, out, broadcast_spec);
-        OPENVINO_TYPE_CASE(evaluate_equal, u16, arg0, arg1, out, broadcast_spec);
-        OPENVINO_TYPE_CASE(evaluate_equal, u32, arg0, arg1, out, broadcast_spec);
-        OPENVINO_TYPE_CASE(evaluate_equal, u64, arg0, arg1, out, broadcast_spec);
-        OPENVINO_TYPE_CASE(evaluate_equal, bf16, arg0, arg1, out, broadcast_spec);
-        OPENVINO_TYPE_CASE(evaluate_equal, f16, arg0, arg1, out, broadcast_spec);
-        OPENVINO_TYPE_CASE(evaluate_equal, f32, arg0, arg1, out, broadcast_spec);
-        OPENVINO_TYPE_CASE(evaluate_equal, f64, arg0, arg1, out, broadcast_spec);
-    default:
-        rc = false;
-        break;
-    }
-    return rc;
-}
-
-ov::Tensor equal_tensor(const ov::Tensor& lhs, const ov::Tensor& rhs) {
-    auto equal = op::v1::Equal(std::make_shared<op::v0::Parameter>(lhs.get_element_type(), lhs.get_shape()),
-                               std::make_shared<op::v0::Parameter>(rhs.get_element_type(), rhs.get_shape()),
-                               op::AutoBroadcastType::NUMPY);
-    auto outs = ov::TensorVector{{equal.get_output_element_type(0), equal.get_output_shape(0)}};
-    equal.evaluate(outs, ov::TensorVector{lhs, rhs});
+Tensor equal_tensor(const Tensor& lhs, const Tensor& rhs) {
+    const auto eq = v1::Equal();
+    auto outs = TensorVector{{element::boolean, Shape{}}};
+    eq.evaluate(outs, {lhs, rhs});
     return outs.front();
 }
 
-ov::Tensor less_equal_tensor(const ov::Tensor& lhs, const ov::Tensor& rhs) {
-    auto equal = op::v1::LessEqual(std::make_shared<op::v0::Parameter>(lhs.get_element_type(), lhs.get_shape()),
-                                   std::make_shared<op::v0::Parameter>(rhs.get_element_type(), rhs.get_shape()),
-                                   op::AutoBroadcastType::NUMPY);
-    auto outs = ov::TensorVector{{equal.get_output_element_type(0), equal.get_output_shape(0)}};
-    equal.evaluate(outs, ov::TensorVector{lhs, rhs});
+Tensor less_equal_tensor(const Tensor& lhs, const Tensor& rhs) {
+    const auto less_eq = v1::LessEqual();
+    auto outs = TensorVector{{element::boolean, Shape{}}};
+    less_eq.evaluate(outs, {lhs, rhs});
     return outs.front();
 }
 
-ov::Tensor and_tensor(const ov::Tensor& lhs, const ov::Tensor& rhs) {
-    auto logical_and =
-        ov::op::v1::LogicalAnd(std::make_shared<op::v0::Parameter>(lhs.get_element_type(), lhs.get_shape()),
-                               std::make_shared<op::v0::Parameter>(rhs.get_element_type(), rhs.get_shape()),
-                               op::AutoBroadcastType::NUMPY);
-    auto outs = ov::TensorVector{{logical_and.get_output_element_type(0), logical_and.get_output_shape(0)}};
-    logical_and.evaluate(outs, ov::TensorVector{lhs, rhs});
+Tensor and_tensor(const Tensor& lhs, const Tensor& rhs) {
+    const auto logical_and = v1::LogicalAnd();
+    auto outs = TensorVector{{element::boolean, Shape{}}};
+    logical_and.evaluate(outs, {lhs, rhs});
     return outs.front();
 }
 
-ov::Tensor or_tensor(const ov::Tensor& lhs, const ov::Tensor& rhs) {
-    auto logical_or =
-        ov::op::v1::LogicalOr(std::make_shared<op::v0::Parameter>(lhs.get_element_type(), lhs.get_shape()),
-                              std::make_shared<op::v0::Parameter>(rhs.get_element_type(), rhs.get_shape()),
-                              op::AutoBroadcastType::NUMPY);
-    auto outs = ov::TensorVector{{logical_or.get_output_element_type(0), logical_or.get_output_shape(0)}};
-    logical_or.evaluate(outs, ov::TensorVector{lhs, rhs});
+Tensor or_tensor(const Tensor& lhs, const Tensor& rhs) {
+    const auto logical_or = v1::LogicalOr();
+    auto outs = TensorVector{{element::boolean, Shape{}}};
+    logical_or.evaluate(outs, {lhs, rhs});
     return outs.front();
 }
 
-void all_equal(const ov::TensorVector tensors, ov::Tensor& output_value) {
-    OPENVINO_ASSERT(tensors.size() >= 2, "Unexpected number of tensors in all_equal helper");
-    auto& tensor = tensors[0];
-    output_value = equal_tensor(tensor, tensors[1]);
-    for (size_t i = 2; i < tensors.size(); ++i) {
-        output_value = and_tensor(output_value, equal_tensor(tensor, tensors[i]));
+void all_equal(const TensorVector tensors, Tensor& output_value) {
+    auto& first_tensor = tensors[0];
+    for (size_t i = 1; i < tensors.size(); ++i) {
+        output_value = and_tensor(output_value, equal_tensor(first_tensor, tensors[i]));
     }
 }
 
-ov::Tensor within_interval(const ov::Tensor& lower, const ov::Tensor& upper, const ov::Tensor& subject_to_check) {
-    auto lower_check = less_equal_tensor(lower, subject_to_check);
-    auto upper_check = less_equal_tensor(subject_to_check, upper);
+Tensor within_interval(const Tensor& lower, const Tensor& upper, const Tensor& subject_to_check) {
+    const auto lower_check = less_equal_tensor(lower, subject_to_check);
+    const auto upper_check = less_equal_tensor(subject_to_check, upper);
     return and_tensor(lower_check, upper_check);
 }
-
 }  // namespace
+
+struct Evaluate : public element::NoAction<bool> {
+    using element::NoAction<bool>::visit;
+
+    template <element::Type_t ET, class T = fundamental_type_for<ET>>
+    static result_type visit(const Tensor& arg0,
+                             const Tensor& arg1,
+                             Tensor& out,
+                             const Shape& shape0,
+                             const Shape& shape1,
+                             const op::AutoBroadcastSpec& broadcast_spec) {
+        reference::equal(arg0.data<const T>(),
+                         arg1.data<const T>(),
+                         out.data<fundamental_type_for<element::boolean>>(),
+                         shape0,
+                         shape1,
+                         broadcast_spec);
+        return true;
+    }
+};
 }  // namespace equal
 
 //------------------------------- v1 -------------------------------------------
-op::v1::Equal::Equal(const Output<Node>& arg0, const Output<Node>& arg1, const AutoBroadcastSpec& auto_broadcast)
+namespace v1 {
+Equal::Equal(const Output<Node>& arg0, const Output<Node>& arg1, const AutoBroadcastSpec& auto_broadcast)
     : BinaryElementwiseComparison(arg0, arg1, auto_broadcast) {
     constructor_validate_and_infer_types();
 }
 
-shared_ptr<Node> op::v1::Equal::clone_with_new_inputs(const OutputVector& new_args) const {
+std::shared_ptr<Node> Equal::clone_with_new_inputs(const OutputVector& new_args) const {
     OV_OP_SCOPE(v1_Equal_clone_with_new_inputs);
     check_new_args_count(this, new_args);
-    return make_shared<op::v1::Equal>(new_args.at(0), new_args.at(1), this->get_autob());
+    return std::make_shared<Equal>(new_args.at(0), new_args.at(1), get_autob());
 }
 
-bool op::v1::Equal::evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const {
+bool Equal::evaluate(TensorVector& outputs, const TensorVector& inputs) const {
     OV_OP_SCOPE(v1_Equal_evaluate);
-    return equal::evaluate_equal(inputs[0], inputs[1], outputs[0], get_autob());
+
+    outputs[0].set_shape(ov::op::infer_broadcast_shape(this, inputs));
+    using namespace ov::element;
+    return IfTypeOf<boolean, bf16, f16, f32, f64, i4, i8, i16, i32, i64, u4, u8, u16, u32, u64>::apply<equal::Evaluate>(
+        inputs[0].get_element_type(),
+        inputs[0],
+        inputs[1],
+        outputs[0],
+        inputs[0].get_shape(),
+        inputs[1].get_shape(),
+        get_autob());
 }
 
-bool op::v1::Equal::evaluate_lower(ov::TensorVector& output_values) const {
+bool Equal::evaluate_lower(TensorVector& output_values) const {
     if (get_input_tensor(0).has_and_set_bound() && get_input_tensor(1).has_and_set_bound())
         return default_upper_bound_evaluator(this, output_values);
     // ll == lu == rl == ru     -> {true}
     // else                     -> {false}
     const auto &lhs = get_input_tensor(0), &rhs = get_input_tensor(1);
-    auto lhs_lower = lhs.get_lower_value(), lhs_upper = lhs.get_upper_value();
-    auto rhs_lower = rhs.get_lower_value(), rhs_upper = rhs.get_upper_value();
+    const auto &lhs_lower = lhs.get_lower_value(), &lhs_upper = lhs.get_upper_value();
+    const auto &rhs_lower = rhs.get_lower_value(), &rhs_upper = rhs.get_upper_value();
     equal::all_equal({lhs_lower, lhs_upper, rhs_lower, rhs_upper}, output_values[0]);
     return true;
 }
 
-bool op::v1::Equal::evaluate_upper(ov::TensorVector& output_values) const {
+bool Equal::evaluate_upper(TensorVector& output_values) const {
     const auto &lhs = get_input_tensor(0), &rhs = get_input_tensor(1);
-    auto lhs_lower = lhs.get_lower_value(), lhs_upper = lhs.get_upper_value();
-    auto rhs_lower = rhs.get_lower_value(), rhs_upper = rhs.get_upper_value();
+    const auto &lhs_lower = lhs.get_lower_value(), &lhs_upper = lhs.get_upper_value();
+    const auto &rhs_lower = rhs.get_lower_value(), &rhs_upper = rhs.get_upper_value();
     // check for intersection:
     // ll <= rl <= lu or ll <= ru <= lu
-    auto rl_check = equal::within_interval(lhs_lower, lhs_upper, rhs_lower);
-    auto ru_check = equal::within_interval(lhs_lower, lhs_upper, rhs_upper);
+    const auto rl_check = equal::within_interval(lhs_lower, lhs_upper, rhs_lower);
+    const auto ru_check = equal::within_interval(lhs_lower, lhs_upper, rhs_upper);
     output_values[0] = equal::or_tensor(rl_check, ru_check);
     return true;
 }
 
-bool op::v1::Equal::has_evaluate() const {
+bool Equal::has_evaluate() const {
     OV_OP_SCOPE(v1_Equal_has_evaluate);
     switch (get_input_element_type(0)) {
-    case ngraph::element::boolean:
-    case ngraph::element::i8:
-    case ngraph::element::u8:
-    case ngraph::element::i32:
-    case ngraph::element::i64:
-    case ngraph::element::u32:
-    case ngraph::element::u64:
-    case ngraph::element::f16:
-    case ngraph::element::f32:
+    case element::boolean:
+    case element::f16:
+    case element::f32:
+    case element::i8:
+    case element::i32:
+    case element::i64:
+    case element::u8:
+    case element::u32:
+    case element::u64:
         return true;
     default:
-        break;
+        return false;
     }
-    return false;
 }
 
-bool op::v1::Equal::visit_attributes(AttributeVisitor& visitor) {
+bool Equal::visit_attributes(AttributeVisitor& visitor) {
     OV_OP_SCOPE(v1_Equal_visit_attributes);
     BinaryElementwiseComparison::visit_attributes(visitor);
     return true;
 }
+}  // namespace v1
+}  // namespace op
+}  // namespace ov
