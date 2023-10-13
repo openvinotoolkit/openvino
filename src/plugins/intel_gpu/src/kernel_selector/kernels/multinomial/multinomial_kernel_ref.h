e13a7a63411d0f51b7bc6e376e5fcef464fa3393
Yaroslav Torzuk <yaroslav.torzuk2@altran.com>
2023-10-13 12:41:32 +0200

    Add kernel selector for multinomial

---
+++ src/plugins/intel_gpu/src/kernel_selector/kernels/multinomial/multinomial_kernel_ref.h
@@ -0,0 +1,30 @@
+// Copyright (C) 2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+#pragma once
+#include "kernel_base_opencl.h"
+#include "kernel_selector_params.h"
+
+namespace kernel_selector {
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// GroupNormalizationParams
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+struct multinomial_params : public base_params {
+    multinomial_params() : base_params(KernelType::MULTINOMIAL) {}
+
+    ov::element::Type_t output_data_type {};
+    bool with_replacement {};
+    bool log_probs {};
+
+    ParamsKey GetParamsKey() const override {
+        return base_params::GetParamsKey();
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// multinomial_optional_params
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+struct multinomial_optional_params : optional_params {
+    multinomial_optional_params() : optional_params(KernelType::MULTINOMIAL) {}
+};
+} // namespace kernel_selector
---
+++ src/plugins/intel_gpu/src/kernel_selector/kernels/multinomial/multinomial_kernel_selector.cpp
@@ -0,0 +1,18 @@
+// Copyright (C) 2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+#include "multinomial_kernel_selector.h"
+#include "multinomial_kernel_ref.h"
+
+namespace kernel_selector {
+
+multinomial_kernel_selector::multinomial_kernel_selector() {
+//    Attach<MultinomialKernelRef>();
+}
+
+KernelsData multinomial_kernel_selector::GetBestKernels(const Params &params,
+                                                                const optional_params &options) const {
+    return GetNaiveBestKernel(params, options, KernelType::MULTINOMIAL);
+}
+
+}  // namespace kernel_selector
