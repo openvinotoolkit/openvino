a1592c63cf9c7f9c820cccfb7fa3f9813bf8c1f2
Andrei Gorbachev <andrei.gorbachev@intel.com>
2023-10-13 10:36:47 +0100

    [GPU] Refactor CTCGreedyDecoderSeqLen, CTCGreedyDecoder, CTCLoss (#20432)
    * CTCGreedyDecoderSeqLen, CTCGreedyDecoder
    
    * CTCLoss
    
    ---------
    
    Co-authored-by: Pavel Durandin <pavel.durandin@intel.com>

--- src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/ctc_greedy_decoder_seq_len.cpp
+++ src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/ctc_greedy_decoder_seq_len.cpp
@@ -3,30 +3,28 @@
 //
 
 #include <vector>
-#include "single_layer_tests/ctc_greedy_decoder_seq_len.hpp"
+#include "single_op_tests/ctc_greedy_decoder_seq_len.hpp"
 #include "common_test_utils/test_constants.hpp"
 
-using namespace LayerTestsDefinitions;
-using namespace ngraph::helpers;
-
 namespace {
+using ov::test::CTCGreedyDecoderSeqLenLayerTest;
 
-std::vector<std::vector<size_t>> inputShape{{1, 1, 1}, {1, 6, 10}, {3, 3, 16}, {5, 3, 55}};
+std::vector<std::vector<ov::Shape>> inputShape{{{1, 1, 1}}, {{1, 6, 10}}, {{3, 3, 16}}, {{5, 3, 55}}};
 
-const std::vector<InferenceEngine::Precision> probPrecisions = {
-    InferenceEngine::Precision::FP32,
-    InferenceEngine::Precision::FP16
+const std::vector<ov::element::Type> probPrecisions = {
+    ov::element::f32,
+    ov::element::f16
 };
-const std::vector<InferenceEngine::Precision> idxPrecisions = {
-    InferenceEngine::Precision::I32,
-    InferenceEngine::Precision::I64
+const std::vector<ov::element::Type> idxPrecisions = {
+    ov::element::i32,
+    ov::element::i64
 };
 
 std::vector<bool> mergeRepeated{true, false};
 
 INSTANTIATE_TEST_SUITE_P(smoke_set1,
                          CTCGreedyDecoderSeqLenLayerTest,
-                         ::testing::Combine(::testing::ValuesIn(inputShape),
+                         ::testing::Combine(::testing::ValuesIn(ov::test::static_shapes_to_test_representation(inputShape)),
                                             ::testing::Values(10),
                                             ::testing::ValuesIn(probPrecisions),
                                             ::testing::ValuesIn(idxPrecisions),
@@ -37,8 +35,8 @@ INSTANTIATE_TEST_SUITE_P(smoke_set1,
 
 INSTANTIATE_TEST_SUITE_P(smoke_set2,
                          CTCGreedyDecoderSeqLenLayerTest,
-                         ::testing::Combine(::testing::ValuesIn(std::vector<std::vector<size_t>>{{2, 8, 11},
-                                                                                                 {4, 10, 55}}),
+                         ::testing::Combine(::testing::ValuesIn(ov::test::static_shapes_to_test_representation(std::vector<std::vector<ov::Shape>>{{{2, 8, 11}},
+                                                                                                 {{4, 10, 55}}})),
                                             ::testing::ValuesIn(std::vector<int>{5, 100}),
                                             ::testing::ValuesIn(probPrecisions),
                                             ::testing::ValuesIn(idxPrecisions),
