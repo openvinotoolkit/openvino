14f6b0ee552438d57cf19ece38cf91691ad2d7f1
Pawel Raasz <pawel.raasz@intel.com>
2023-10-16 06:16:43 +0200

    Migrate Split operator to new API (#20263)

--- src/core/reference/include/openvino/reference/split.hpp
+++ src/core/reference/include/openvino/reference/split.hpp
@@ -4,17 +4,28 @@
 
 #pragma once
 
-#include <cmath>
+#include <cstddef>
 
-#include "openvino/reference/slice.hpp"
+#include "openvino/core/shape.hpp"
 
 namespace ov {
 namespace reference {
+
+/**
+ * @brief Reference implementation of Split operator.
+ *
+ * @param data        Pointer to input data.
+ * @param data_shape  Input data shape.
+ * @param elem_size   Size of single element type.
+ * @param axis        Axis used for split input data.
+ * @param num_splits  Number of splits
+ * @param out_data    Pointer to output data pointers (must have size of num_splits)
+ */
 void split(const char* data,
            const Shape& data_shape,
            size_t elem_size,
            int64_t axis,
            size_t num_splits,
            char** out_data);
-}
+}  // namespace reference
 }  // namespace ov
--- src/core/reference/src/op/split.cpp
+++ src/core/reference/src/op/split.cpp
@@ -6,35 +6,43 @@
 
 #include <stdio.h>
 
-#include <cmath>
+#include <iterator>
 
-using namespace ov;
+#include "openvino/core/coordinate.hpp"
+#include "openvino/reference/slice.hpp"
 
-void reference::split(const char* data,
-                      const Shape& data_shape,
-                      size_t elem_size,
-                      int64_t axis,
-                      size_t num_splits,
-                      char** out_data) {
+namespace ov {
+namespace reference {
+
+void split(const char* data,
+           const Shape& data_shape,
+           const size_t elem_size,
+           const int64_t axis,
+           const size_t num_splits,
+           char** out_data) {
     const size_t part_length = data_shape.at(axis) / num_splits;
 
-    Shape output_shape = data_shape;
-    output_shape.at(axis) = part_length;
+    auto output_shape = data_shape;
+    output_shape[axis] = part_length;
 
-    std::vector<size_t> lower_bounds(data_shape.size(), 0);
-    std::vector<size_t> upper_bounds = data_shape;
-    upper_bounds.at(axis) = part_length;
+    Coordinate lower_bounds(data_shape.size(), 0);
+    Coordinate upper_bounds = output_shape;
+    auto& lb_at_axis = lower_bounds[axis];
+    auto& ub_at_axis = upper_bounds[axis];
 
-    for (size_t i = 0; i < num_splits; ++i) {
+    const auto out_last = std::next(out_data, num_splits);
+    for (auto out_first = out_data; out_first != out_last; ++out_first) {
         reference::slice(data,
-                         out_data[i],
+                         *out_first,
                          data_shape,
                          lower_bounds,
                          upper_bounds,
                          Strides(lower_bounds.size(), 1),
                          output_shape,
                          elem_size);
-        lower_bounds.at(axis) += part_length;
-        upper_bounds.at(axis) += part_length;
+        lb_at_axis += part_length;
+        ub_at_axis += part_length;
     }
 }
+}  // namespace reference
+}  // namespace ov
