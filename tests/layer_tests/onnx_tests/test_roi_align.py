9ede33a962ddb2d92f83e9e5fd209fa636078420
Andrei Kashchikhin <andrey.kashchikhin@intel.com>
2023-10-16 11:36:32 +0100

    skip tests on mac arm

--- tests/layer_tests/onnx_tests/test_roi_align.py
+++ tests/layer_tests/onnx_tests/test_roi_align.py
@@ -136,6 +136,8 @@ class TestROIAlign(OnnxRuntimeLayerTest):
     @pytest.mark.nightly
     @pytest.mark.precommit
     @pytest.mark.xfail(condition=platform.system() == 'Windows', reason="Ticket - 122731")
+    @pytest.mark.xfail(condition=platform.system() == 'Darwin' and platform.machine() == 'arm64',
+                       reason='Ticket - 122846')
     def test_roi_alignv10(self, params, ie_device, precision, ir_version, temp_dir, use_old_api):
         # TODO: ticket for investigating GPU failures: CVS-86300
         if ie_device != "GPU":
