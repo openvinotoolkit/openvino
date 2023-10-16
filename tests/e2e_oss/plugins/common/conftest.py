544af1fd2a30d955096aaeb3f09cf4308eb85474
rnugmano <ruslan.nugmanov@intel.com>
2023-10-16 13:04:56 +0400

    check imports

--- tests/e2e_oss/plugins/common/conftest.py
+++ tests/e2e_oss/plugins/common/conftest.py
@@ -32,7 +32,7 @@ import shutil
 import pytest
 from jsonschema import validate, ValidationError
 
-from e2e_oss._utils.test_utils import copy_files_by_pattern, log_timestamp
+from tests.e2e_oss._utils.test_utils import copy_files_by_pattern, log_timestamp
 from utils.e2e.env_tools import Environment
 
 
