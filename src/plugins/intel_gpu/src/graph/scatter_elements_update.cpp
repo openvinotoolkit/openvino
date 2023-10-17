0a5277ba4b116055bd9d83668f59b496c04d1638
wilson-seok <wilson.seok@intel.com>
2023-10-17 19:31:16 +0900

    output shape is same with input shape in dynamic case

--- src/plugins/intel_gpu/src/graph/scatter_elements_update.cpp
+++ src/plugins/intel_gpu/src/graph/scatter_elements_update.cpp
@@ -18,11 +18,11 @@ layout scatter_elements_update_inst::calc_output_layout(scatter_elements_update_
     auto desc = impl_param.typed_desc<scatter_elements_update>();
 
     const int32_t axis = desc->axis;
-    const size_t input_number_of_dims = impl_param.get_input_layout().get_tensor().sizes().size();
+    const size_t input_number_of_dims = impl_param.get_input_layout().get_partial_shape().size();
 
     auto input_layout = impl_param.get_input_layout();
 
-    auto output_shape = input_layout.get_tensor();
+    auto output_shape = input_layout.get_partial_shape();
     auto input_format = input_layout.format;
     auto output_type = input_layout.data_type;
 
@@ -33,34 +33,8 @@ layout scatter_elements_update_inst::calc_output_layout(scatter_elements_update_
     if (static_cast<size_t>(axis) < 0 || static_cast<size_t>(axis) >= input_number_of_dims)
         CLDNN_ERROR_MESSAGE(desc->id, "Incorrect axis value for ScatterElementsUpdate: Axis must be positive and less than the input tensor dimension.");
 
-    return layout{output_type, input_format, output_shape};
+    return layout{output_shape, output_type, input_format};
 }
-
-template<typename ShapeType>
-std::vector<layout> scatter_elements_update_inst::calc_output_layouts(scatter_elements_update_node const& /*node*/, const kernel_impl_params& impl_param) {
-    auto input0_layout = impl_param.get_input_layout(0);
-    auto input1_layout = impl_param.get_input_layout(1);
-    auto input2_layout = impl_param.get_input_layout(2);
-    // axis should be scalar or 1D tensor
-    auto input3_layout = layout{ov::PartialShape{1}, data_types::i64, format::bfyx};
-
-    std::vector<ShapeType> input_shapes = {
-        input0_layout.get<ShapeType>(),     // inputs_shape
-        input1_layout.get<ShapeType>(),     // indices_shape,
-        input2_layout.get<ShapeType>(),     // updates_shape,
-        input3_layout.get<ShapeType>(),     // axis_shape,
-    };
-
-    std::vector<ShapeType> output_shapes = {ShapeType()};
-
-    ov::op::v3::ScatterElementsUpdate op;
-    output_shapes = shape_infer(&op, input_shapes);
-
-    return { layout{output_shapes[0], input0_layout.data_type, input0_layout.format} };
-}
-
-template std::vector<layout>
-scatter_elements_update_inst::calc_output_layouts<ov::PartialShape>(scatter_elements_update_node const& node, const kernel_impl_params& impl_param);
 
 std::string scatter_elements_update_inst::to_string(scatter_elements_update_node const& node) {
     auto desc = node.get_primitive();
