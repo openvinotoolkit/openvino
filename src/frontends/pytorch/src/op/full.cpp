a0483d17bc5146abbbe3176933589726d2af8393
Ekaterina Aidova <ekaterina.aidova@intel.com>
2023-10-18 12:58:54 +0400

    [PT FE]: support aten::fill_diagonal_, aten::fill (#20395)
    * [PT FE]: support aten::fill_diagonal_, aten::fill
    
    * remove xfail
    
    * Update src/frontends/pytorch/src/op/full.cpp
    
    Co-authored-by: Maxim Vafin <maxim.vafin@intel.com>
    
    * Update tests/model_hub_tests/torch_tests/test_hf_transformers.py
    
    ---------
    
    Co-authored-by: Maxim Vafin <maxim.vafin@intel.com>

--- src/frontends/pytorch/src/op/full.cpp
+++ src/frontends/pytorch/src/op/full.cpp
@@ -3,10 +3,19 @@
 //
 
 #include "openvino/frontend/pytorch/node_context.hpp"
+#include "openvino/op/add.hpp"
 #include "openvino/op/broadcast.hpp"
 #include "openvino/op/constant.hpp"
 #include "openvino/op/convert_like.hpp"
+#include "openvino/op/divide.hpp"
+#include "openvino/op/gather.hpp"
+#include "openvino/op/multiply.hpp"
+#include "openvino/op/power.hpp"
+#include "openvino/op/range.hpp"
+#include "openvino/op/reshape.hpp"
+#include "openvino/op/scatter_elements_update.hpp"
 #include "openvino/op/shape_of.hpp"
+#include "openvino/op/squeeze.hpp"
 #include "utils.hpp"
 
 namespace ov {
@@ -71,12 +80,17 @@ OutputVector translate_full_like(const NodeContext& context) {
     return {base_translate_full_with_convertlike(context, sizes, value, out)};
 };
 
-OutputVector translate_fill_(const NodeContext& context) {
-    num_inputs_check(context, 2, 2);
+OutputVector translate_fill(const NodeContext& context) {
+    num_inputs_check(context, 2, 3);
     auto input = context.get_input(0);
     auto value = context.get_input(1);
     auto sizes = context.mark_node(std::make_shared<v3::ShapeOf>(input, element::i32));
-    return {base_translate_full_with_convertlike(context, sizes, value, input)};
+    auto out = context.input_is_none(2) ? input : context.get_input(2);
+    auto result = base_translate_full_with_convertlike(context, sizes, value, out);
+    if (!context.input_is_none(2)) {
+        context.mutate_input(2, result);
+    }
+    return {result};
 };
 
 OutputVector translate_new_full(const NodeContext& context) {
@@ -187,6 +201,67 @@ OutputVector translate_empty(const NodeContext& context) {
     }
     return {empty};
 };
+
+OutputVector translate_fill_diagonal(const NodeContext& context) {
+    // aten::fill_diagonal_(Tensor(a!) self, Scalar fill_value, bool wrap=False) -> Tensor(a!)
+    // realization inspired by numpy:
+    // https://github.com/numpy/numpy/blob/c236e694d222ae6b812cb8dab54471bc4c912f0f/numpy/lib/_index_tricks_impl.py#L787-L918
+    num_inputs_check(context, 3, 3);
+    auto input_tensor = context.get_input(0);
+    auto fill_value = context.get_input(1);
+    auto input_shape = context.mark_node(std::make_shared<v3::ShapeOf>(input_tensor, element::i32));
+    auto input_rank = input_tensor.get_partial_shape().rank();
+    auto const_one = context.mark_node(v0::Constant::create(element::i32, Shape{1}, {1}));
+    auto const_zero = context.mark_node(v0::Constant::create(element::i32, Shape{1}, {0}));
+    auto const_one_s = context.mark_node(v0::Constant::create(element::i32, Shape{}, {1}));
+    auto const_zero_s = context.mark_node(v0::Constant::create(element::i32, Shape{}, {0}));
+    auto const_neg_one = context.mark_node(v0::Constant::create(element::i32, Shape{1}, {-1}));
+    if (input_rank.is_dynamic() || input_rank.get_length() < 2) {
+        FRONT_END_OP_CONVERSION_CHECK(false, "aten::fill_diagonal_ required tensor with static rank >= 2 ");
+    }
+    auto flatten_input = context.mark_node(std::make_shared<v1::Reshape>(input_tensor, const_neg_one, false));
+    auto wrap = context.const_input<bool>(2);
+    Output<Node> step;
+    // default value for end - number of elements in input tensor
+    Output<Node> end;
+    auto flatten_shape = context.mark_node(std::make_shared<v3::ShapeOf>(flatten_input, element::i32));
+    end = context.mark_node(std::make_shared<v8::Gather>(flatten_shape, const_neg_one, const_zero));
+    auto last_dim = context.mark_node(std::make_shared<v8::Gather>(input_shape, const_neg_one, const_zero));
+    if (input_rank.get_length() == 2) {
+        // step = a.shape[1] + 1
+        step = context.mark_node(std::make_shared<v1::Add>(last_dim, const_one_s));
+        if (!wrap) {
+            // if not wrap. and non squared matrix, do not fill tail by cutting end to square
+            end = context.mark_node(std::make_shared<v1::Multiply>(last_dim, last_dim));
+        }
+    } else {
+        // step = 1 + (cumprod(a.shape[:-1])).sum()
+        // cumprod operation is not supported by ov, but with condition that >2D tensors supported only if all dims
+        // equals cumprod can be represented as finite geometric serial and its sum can be found by formula
+        // b0 * (bn * q - 1) / (q - 1), where in this particual case q = b0, bn = b0 ^ n
+        auto rank_minus_one =
+            context.mark_node(v0::Constant::create(element::i32, Shape{}, {input_rank.get_length() - 1}));
+        auto dim_power = context.mark_node(std::make_shared<v1::Power>(last_dim, rank_minus_one));
+        auto dim_power_minus_one = context.mark_node(std::make_shared<v1::Add>(dim_power, const_neg_one));
+        auto dim_minus_one = context.mark_node(std::make_shared<v1::Add>(last_dim, const_neg_one));
+        auto q = context.mark_node(std::make_shared<v1::Divide>(dim_power_minus_one, dim_minus_one, true));
+        auto cumprod_sum = context.mark_node(std::make_shared<v1::Multiply>(last_dim, q));
+        step = context.mark_node(std::make_shared<v1::Add>(const_one_s, cumprod_sum));
+        // wrap parameter is not applicable in this case as supported only equal dims on pytorch side
+    }
+    step = context.mark_node(std::make_shared<v0::Squeeze>(step, const_zero));
+    end = context.mark_node(std::make_shared<v0::Squeeze>(end, const_zero));
+    auto indices = context.mark_node(std::make_shared<v4::Range>(const_zero_s, end, step, element::i32));
+    auto indices_shape = context.mark_node(std::make_shared<v3::ShapeOf>(indices, element::i32));
+    fill_value = context.mark_node(std::make_shared<v1::ConvertLike>(fill_value, input_tensor));
+    fill_value = context.mark_node(std::make_shared<v1::Broadcast>(fill_value, indices_shape));
+    // fill values
+    auto filled_tensor =
+        context.mark_node(std::make_shared<v12::ScatterElementsUpdate>(flatten_input, indices, fill_value, const_zero));
+    // reshape back to original shape
+    filled_tensor = context.mark_node(std::make_shared<v1::Reshape>(filled_tensor, input_shape, false));
+    return {filled_tensor};
+}
 }  // namespace op
 }  // namespace pytorch
 }  // namespace frontend
