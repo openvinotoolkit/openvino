e70e312e7f637279d1b01647b800306e9438c143
Pawel Raasz <pawel.raasz@intel.com>
2023-10-18 11:09:27 +0200

    [core]Migrate BatchToSpace to new API (#20450)
    * Migrate BatchToSpace to new API
    
    * Remove not required suppression macro

--- src/core/include/openvino/op/batch_to_space.hpp
+++ src/core/include/openvino/op/batch_to_space.hpp
@@ -37,9 +37,7 @@ public:
                  const Output<Node>& block_shape,
                  const Output<Node>& crops_begin,
                  const Output<Node>& crops_end);
-    OPENVINO_SUPPRESS_DEPRECATED_START
-    bool evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const override;
-    OPENVINO_SUPPRESS_DEPRECATED_END
+    bool evaluate(TensorVector& outputs, const TensorVector& inputs) const override;
     bool has_evaluate() const override;
 
     void validate_and_infer_types() override;
