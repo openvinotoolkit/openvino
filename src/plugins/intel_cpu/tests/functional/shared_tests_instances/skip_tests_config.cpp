5708b01d8cb2a651362fd3943ddef949c9a7f4b4
Nikolai Shchegolev <nikolay.shchegolev@intel.com>
2023-10-18 15:19:36 +0400

    [CPU] NMSRotated operation implementation.

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/skip_tests_config.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/skip_tests_config.cpp
@@ -197,6 +197,8 @@ std::vector<std::string> disabledTestPatterns() {
         R"(.*RDFTLayerTest.*SignalSize=().*)",
         // Issue: 123815 (Tests are sensintive to available thread count on testing machines)
         R"(.*smoke_Snippets_MHA_.?D_SplitDimensionM.*)",
+        // Issue: 122356
+        R"(.*NmsRotatedLayerTestCPU.*(SortDesc=True|Clockwise=False).*)",
     };
 
 #if defined(OPENVINO_ARCH_X86)
