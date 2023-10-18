3fb3ee0e12d7305d6bf90f34f79956220e5972d0
andrei-cv <andrei.gorbachev@intel.com>
2023-10-18 16:32:55 +0400

    temp chenges

--- src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/gru_sequence.cpp
+++ src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/gru_sequence.cpp
@@ -4,64 +4,63 @@
 
 #include <vector>
 #include <ngraph/op/util/attr_types.hpp>
-#include "single_layer_tests/gru_sequence.hpp"
+#include "single_op_tests/gru_sequence.hpp"
 #include "common_test_utils/test_constants.hpp"
-
-using namespace LayerTestsDefinitions;
+#include "common_test_utils/test_enums.hpp"
 
 namespace {
-    std::vector<ngraph::helpers::SequenceTestsMode> mode{ngraph::helpers::SequenceTestsMode::CONVERT_TO_TI_MAX_SEQ_LEN_CONST,
-                                                         ngraph::helpers::SequenceTestsMode::CONVERT_TO_TI_RAND_SEQ_LEN_CONST,
-                                                         ngraph::helpers::SequenceTestsMode::CONVERT_TO_TI_RAND_SEQ_LEN_PARAM,
-                                                         ngraph::helpers::SequenceTestsMode::PURE_SEQ_RAND_SEQ_LEN_CONST,
-                                                         ngraph::helpers::SequenceTestsMode::PURE_SEQ_RAND_SEQ_LEN_PARAM,
-                                                         ngraph::helpers::SequenceTestsMode::PURE_SEQ};
+using ov::test::GRUSequenceTest;
+using ov::test::utils::InputLayerType;
+using ov::test::utils::SequenceTestsMode;
+
+    std::vector<SequenceTestsMode> mode{SequenceTestsMode::CONVERT_TO_TI_MAX_SEQ_LEN_CONST,
+                                        SequenceTestsMode::CONVERT_TO_TI_RAND_SEQ_LEN_CONST,
+                                        SequenceTestsMode::CONVERT_TO_TI_RAND_SEQ_LEN_PARAM,
+                                        SequenceTestsMode::PURE_SEQ_RAND_SEQ_LEN_CONST,
+                                        SequenceTestsMode::PURE_SEQ_RAND_SEQ_LEN_PARAM,
+                                        SequenceTestsMode::PURE_SEQ};
     // output values increase rapidly without clip, so use only seq_lengths = 2
     std::vector<size_t> seq_lengths_zero_clip{2};
     std::vector<size_t> seq_lengths_clip_non_zero{20};
     std::vector<size_t> batch{10};
     std::vector<size_t> hidden_size{1, 10};
-    // std::vector<size_t> input_size{10};
+    std::vector<std::vector<ov::Shape>> input_size{{{10}}};
     std::vector<std::vector<std::string>> activations = {{"relu", "tanh"}, {"tanh", "sigmoid"}, {"sigmoid", "tanh"},
                                                          {"tanh", "relu"}};
     std::vector<bool> linear_before_reset = {true, false};
     std::vector<float> clip{0.f};
     std::vector<float> clip_non_zeros{0.7f};
-    std::vector<ngraph::op::RecurrentSequenceDirection> direction = {ngraph::op::RecurrentSequenceDirection::FORWARD,
-                                                                     ngraph::op::RecurrentSequenceDirection::REVERSE,
-                                                                     ngraph::op::RecurrentSequenceDirection::BIDIRECTIONAL
+    std::vector<ov::op::RecurrentSequenceDirection> direction = {ov::op::RecurrentSequenceDirection::FORWARD,
+                                                                     ov::op::RecurrentSequenceDirection::REVERSE,
+                                                                     ov::op::RecurrentSequenceDirection::BIDIRECTIONAL
     };
-    std::vector<InferenceEngine::Precision> netPrecisions = {InferenceEngine::Precision::FP32,
-                                                             InferenceEngine::Precision::FP16};
+    std::vector<ov::element::Type> netPrecisions = {ov::element::f32,
+                                                    ov::element::f16};
 
     INSTANTIATE_TEST_SUITE_P(GRUSequenceCommonZeroClip, GRUSequenceTest,
                             ::testing::Combine(
                                     ::testing::ValuesIn(mode),
-                                    ::testing::ValuesIn(seq_lengths_zero_clip),
-                                    ::testing::ValuesIn(batch),
-                                    ::testing::ValuesIn(hidden_size),
-                                    // ::testing::ValuesIn(input_size), // hardcoded to 10 due to Combine supports up to 10 args
+                                    ::testing::ValuesIn(ov::test::static_shapes_to_test_representation(input_size)),
+                                    // hardcoded to 10 due to Combine supports up to 10 args
                                     ::testing::ValuesIn(activations),
                                     ::testing::ValuesIn(clip),
                                     ::testing::ValuesIn(linear_before_reset),
                                     ::testing::ValuesIn(direction),
-                                    ::testing::Values(ngraph::helpers::InputLayerType::CONSTANT),
+                                    ::testing::Values(InputLayerType::CONSTANT),
                                     ::testing::ValuesIn(netPrecisions),
                                     ::testing::Values(ov::test::utils::DEVICE_GPU)),
                             GRUSequenceTest::getTestCaseName);
 
     INSTANTIATE_TEST_SUITE_P(GRUSequenceCommonZeroClipNonConstantWRB, GRUSequenceTest,
                             ::testing::Combine(
-                                    ::testing::Values(ngraph::helpers::SequenceTestsMode::PURE_SEQ),
-                                    ::testing::ValuesIn(seq_lengths_zero_clip),
-                                    ::testing::ValuesIn(batch),
-                                    ::testing::ValuesIn(hidden_size),
-                                    // ::testing::ValuesIn(input_size), // hardcoded to 10 due to Combine supports up to 10 args
+                                    ::testing::Values(SequenceTestsMode::PURE_SEQ),
+                                    ::testing::ValuesIn(ov::test::static_shapes_to_test_representation(input_size)),
+                                    // hardcoded to 10 due to Combine supports up to 10 args
                                     ::testing::ValuesIn(activations),
                                     ::testing::ValuesIn(clip),
                                     ::testing::ValuesIn(linear_before_reset),
                                     ::testing::ValuesIn(direction),
-                                    ::testing::Values(ngraph::helpers::InputLayerType::PARAMETER),
+                                    ::testing::Values(InputLayerType::PARAMETER),
                                     ::testing::ValuesIn(netPrecisions),
                                     ::testing::Values(ov::test::utils::DEVICE_GPU)),
                             GRUSequenceTest::getTestCaseName);
@@ -69,15 +68,13 @@ namespace {
     INSTANTIATE_TEST_SUITE_P(GRUSequenceCommonClip, GRUSequenceTest,
                             ::testing::Combine(
                                     ::testing::ValuesIn(mode),
-                                    ::testing::ValuesIn(seq_lengths_clip_non_zero),
-                                    ::testing::ValuesIn(batch),
-                                    ::testing::ValuesIn(hidden_size),
-                                    // ::testing::ValuesIn(input_size),  // hardcoded to 10 due to Combine supports up to 10 args
+                                    ::testing::ValuesIn(ov::test::static_shapes_to_test_representation(input_size)),
+                                    // hardcoded to 10 due to Combine supports up to 10 args
                                     ::testing::ValuesIn(activations),
                                     ::testing::ValuesIn(clip_non_zeros),
                                     ::testing::ValuesIn(linear_before_reset),
                                     ::testing::ValuesIn(direction),
-                                    ::testing::Values(ngraph::helpers::InputLayerType::CONSTANT),
+                                    ::testing::Values(InputLayerType::CONSTANT),
                                     ::testing::ValuesIn(netPrecisions),
                                     ::testing::Values(ov::test::utils::DEVICE_GPU)),
                             GRUSequenceTest::getTestCaseName);
