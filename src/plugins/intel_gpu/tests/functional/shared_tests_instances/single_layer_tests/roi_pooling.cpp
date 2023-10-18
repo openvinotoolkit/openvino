fae7f60e78b145da84ed636fdb7bbe1c1a5f8e0b
andrei-cv <andrei.gorbachev@intel.com>
2023-10-18 18:51:06 +0400

    roi_pooling

--- src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/roi_pooling.cpp
+++ src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/roi_pooling.cpp
@@ -2,65 +2,70 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "single_layer_tests/roi_pooling.hpp"
-
-#include <vector>
-
+#include "single_op_tests/roi_pooling.hpp"
 #include "common_test_utils/test_constants.hpp"
 
-using namespace LayerTestsDefinitions;
-
 namespace {
+using ov::test::ROIPoolingLayerTest;
 
-const std::vector<std::vector<size_t>> inShapes = {
-    {1, 3, 8, 8},
-    {3, 4, 50, 50},
+const std::vector<ov::Shape> inShapes = {
+    {{1, 3, 8, 8}},
+    {{3, 4, 50, 50}},
 };
 
-const std::vector<std::vector<size_t>> pooledShapes_max = {
-    {1, 1},
-    {2, 2},
-    {3, 3},
-    {6, 6},
+const std::vector<ov::Shape> pooledShapes_max = {
+    {{1, 1}},
+    {{2, 2}},
+    {{3, 3}},
+    {{6, 6}},
 };
 
-const std::vector<std::vector<size_t>> pooledShapes_bilinear = {
-    {1, 1},
-    {2, 2},
-    {3, 3},
-    {6, 6},
+const std::vector<ov::Shape> pooledShapes_bilinear = {
+    {{1, 1}},
+    {{2, 2}},
+    {{3, 3}},
+    {{6, 6}},
 };
 
-const std::vector<std::vector<size_t>> coordShapes = {
-    {1, 5},
-    {3, 5},
-    {5, 5},
+const std::vector<ov::Shape> coordShapes = {
+    {{1, 5}},
+    {{3, 5}},
+    {{5, 5}},
 };
 
-const std::vector<InferenceEngine::Precision> netPRCs = {
-    InferenceEngine::Precision::FP16,
-    InferenceEngine::Precision::FP32,
+const std::vector<ov::element::Type> netPRCs = {
+    ov::element::f16,
+    ov::element::f32,
 };
 
 const std::vector<float> spatial_scales = {0.625f, 1.f};
 
-const auto params_max = testing::Combine(testing::ValuesIn(inShapes),
-                                         testing::ValuesIn(coordShapes),
+auto input_shapes = [](const std::vector<ov::Shape>& in1, const std::vector<ov::Shape>& in2) {
+    std::vector<std::vector<ov::test::InputShape>> res;
+    for (const auto& sh1 : in1)
+        for (const auto& sh2 : in2)
+            res.push_back(ov::test::static_shapes_to_test_representation({sh1, sh2}));
+    return res;
+}(inShapes, coordShapes);
+
+const auto params_max = testing::Combine(testing::ValuesIn(input_shapes),
                                          testing::ValuesIn(pooledShapes_max),
                                          testing::ValuesIn(spatial_scales),
-                                         testing::Values(ngraph::helpers::ROIPoolingTypes::ROI_MAX),
+                                         testing::Values(ov::test::utils::ROIPoolingTypes::ROI_MAX),
                                          testing::ValuesIn(netPRCs),
                                          testing::Values(ov::test::utils::DEVICE_GPU));
 
-const auto params_bilinear = testing::Combine(testing::ValuesIn(inShapes),
-                                              testing::ValuesIn(coordShapes),
+const auto params_bilinear = testing::Combine(testing::ValuesIn(input_shapes),
                                               testing::ValuesIn(pooledShapes_bilinear),
                                               testing::Values(spatial_scales[1]),
-                                              testing::Values(ngraph::helpers::ROIPoolingTypes::ROI_BILINEAR),
+                                              testing::Values(ov::test::utils::ROIPoolingTypes::ROI_BILINEAR),
                                               testing::ValuesIn(netPRCs),
                                               testing::Values(ov::test::utils::DEVICE_GPU));
 
-INSTANTIATE_TEST_SUITE_P(smoke_ROIPooling_max, ROIPoolingLayerTest, params_max, ROIPoolingLayerTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_ROIPooling_max,
+                         ROIPoolingLayerTest,
+                         params_max,
+                         ROIPoolingLayerTest::getTestCaseName);
 
 INSTANTIATE_TEST_SUITE_P(smoke_ROIPooling_bilinear,
                          ROIPoolingLayerTest,
