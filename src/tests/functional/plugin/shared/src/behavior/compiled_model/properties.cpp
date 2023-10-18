1317d0773c18c7659d9e49962712fc9cb5a20242
panhaiqi <haiqi.pan@intel.com>
2023-10-18 20:11:17 +0800

    remove failed case CanCompileModelWithEmptyProperties, CanLoadNetworkWithCustomLocale and LoadNetworkWithBigDeviceIDThrows to test

--- src/tests/functional/plugin/shared/src/behavior/compiled_model/properties.cpp
+++ src/tests/functional/plugin/shared/src/behavior/compiled_model/properties.cpp
@@ -351,8 +351,6 @@ TEST_P(OVCompileModelGetExecutionDeviceTests, CanGetExecutionDeviceInfo) {
         ASSERT_FALSE(property.empty());
 }
 
-
-
 }  // namespace behavior
 }  // namespace test
 }  // namespace ov
