76510d457776799ac535b22e7e3e7fe30d22dc8c
Roman Kazantsev <roman.kazantsev@intel.com>
2023-10-18 09:19:49 +0400

    Update tests/layer_tests/tensorflow_tests/test_tf_ToBool.py

--- tests/layer_tests/tensorflow_tests/test_tf_ToBool.py
+++ tests/layer_tests/tensorflow_tests/test_tf_ToBool.py
@@ -38,6 +38,6 @@ class TestToBool(CommonTFLayerTest):
     @pytest.mark.nightly
     def test_div_no_nan_basic(self, params, ie_device, precision, ir_version, temp_dir,
                               use_new_frontend, use_old_api):
-        self._test(*self.create_div_no_nan_net(**params),
+        self._test(*self.create_tobool_net(**params),
                    ie_device, precision, ir_version, temp_dir=temp_dir,
                    use_new_frontend=use_new_frontend, use_old_api=use_old_api)
