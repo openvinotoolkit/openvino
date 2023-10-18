e766fa4645b89300d04c81de8ac8cfc1924f93f7
Ilya Churaev <ilyachur@gmail.com>
2023-10-18 13:45:14 +0400

    Fixed comment

--- src/core/src/runtime/aligned_buffer.cpp
+++ src/core/src/runtime/aligned_buffer.cpp
@@ -75,7 +75,7 @@ AlignedBuffer::AlignedBuffer(size_t byte_size, size_t alignment) : m_byte_size(b
     size_t allocation_size = m_byte_size + alignment;
     m_allocated_buffer = new char[allocation_size];
     m_aligned_buffer = m_allocated_buffer;
-    size_t mod = (alignment != 0) ? size_t(m_aligned_buffer) % alignment : 0;
+    size_t mod = (alignment != 0) ? reinterpret_cast<size_t>(m_aligned_buffer) % alignment : 0;
 
     if (mod != 0) {
         m_aligned_buffer += (alignment - mod);
