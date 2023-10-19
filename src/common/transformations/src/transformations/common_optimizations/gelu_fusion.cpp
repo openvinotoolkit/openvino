d0dd1202272d7676e19fc012f3b78242e80d5901
Evgeny Kotov <evgeny.kotov@intel.com>
2023-10-19 11:35:26 +0200

    fix windows build warnings

--- src/common/transformations/src/transformations/common_optimizations/gelu_fusion.cpp
+++ src/common/transformations/src/transformations/common_optimizations/gelu_fusion.cpp
@@ -306,7 +306,7 @@ ov::pass::GeluFusionWithTanh::GeluFusionWithTanh() {
         bool valid_constant_values =
             op::util::has_constant_value<float>(pow_constant_value, 3.0f) &&
             op::util::has_constant_value<float>(mul_0_constant_value, 0.044715f, 0.001f) &&
-            op::util::has_constant_value<float>(mul_1_constant_value, std::sqrt(2.0f / M_PI), 0.01f) &&
+            op::util::has_constant_value<double>(mul_1_constant_value, std::sqrt(2.0 / M_PI), 0.01) &&
             op::util::has_constant_value<float>(mul_2_constant_value, 0.5f) &&
             op::util::has_constant_value<float>(add_1_constant_value, 1.0f);
 
@@ -378,7 +378,7 @@ ov::pass::GeluFusionWithTanhNoPower::GeluFusionWithTanhNoPower() {
 
         bool valid_constant_values = op::util::has_constant_value<float>(const1_value, 0.044715f, 0.001f) &&
                                      op::util::has_constant_value<float>(const2_value, 1.0f) &&
-                                     op::util::has_constant_value<float>(const3_value, std::sqrt(2.0f / M_PI), 0.01f) &&
+                                     op::util::has_constant_value<double>(const3_value, std::sqrt(2.0 / M_PI), 0.01) &&
                                      op::util::has_constant_value<float>(const4_value, 1.0f) &&
                                      op::util::has_constant_value<float>(const5_value, 0.5f);
 
