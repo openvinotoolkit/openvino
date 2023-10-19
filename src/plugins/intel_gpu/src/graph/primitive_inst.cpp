fe12874d3fa3bc4e8ea24d2c1be3481e75ee9dd5
wilson-seok <wilson.seok@intel.com>
2023-10-19 21:24:32 +0900

    only dynamic shape case takes check_allocatable

--- src/plugins/intel_gpu/src/graph/primitive_inst.cpp
+++ src/plugins/intel_gpu/src/graph/primitive_inst.cpp
@@ -975,7 +975,6 @@ primitive_inst::primitive_inst(network& network, program_node const& node, bool 
     , _outputs({memory::ptr()})
     , _reordered_weights_cache(network.get_weights_cache_capacity())
     , _output_changed(false)
-    , _mem_allocated(allocate_memory)
     , _is_dynamic(node.is_dynamic() || node.generates_dynamic_output())
     , _type(node.type())
     , _id(node.id())
@@ -990,6 +989,12 @@ primitive_inst::primitive_inst(network& network, program_node const& node, bool 
     , _can_share_buffer(node.can_share_buffer())
     , _is_constant(node.is_constant())
     , _needs_completion_event(is_any_user_cpu(node.get_users()) || node.is_output()) {
+    // When dynamic shape node has huge upper boundary which causes bigger mem size than system max allocable mem size, do not allocate in build time.
+    auto output_layout = node.get_output_layout();
+    if (node.is_dynamic() && (!network.get_engine().check_allocatable(output_layout, allocation_type::usm_host))) {
+        allocate_memory = false;
+    }
+    _mem_allocated = allocate_memory;
     if (allocate_memory) {
         // In case when output is mutable_data primitive, and other users dependencies are only used for
         // suychronization, The output memory of such primitive will be fused with mutable_data
@@ -1012,14 +1017,7 @@ primitive_inst::primitive_inst(network& network, program_node const& node, bool 
                 if (user->is_type<mutable_data>())
                     _outputs[0] = user->as<mutable_data>().get_attached_memory_ptr();
         } else {
-            auto& e = network.get_engine();
-            auto output_layout = node.get_output_layout();
-            // When dynamic shape node has huge upper boundary which causes bigger mem size than system max allocable mem size, return false.
-            if (!e.check_allocatable(output_layout, e.get_lockable_preferred_memory_allocation_type(output_layout.format.is_image_2d()))) {
-                _mem_allocated = false;
-            } else {
-                _outputs = allocate_outputs();
-            }
+            _outputs = allocate_outputs();
         }
     }
     if (_impl) {
