dc6067975f8299fd6146b9aaf06b9a26f658fd30
Pavel Esir <pavel.esir@intel.com>
2023-10-20 13:29:16 +0200

    style fix

--- src/common/transformations/src/transformations/init_node_info.cpp
+++ src/common/transformations/src/transformations/init_node_info.cpp
@@ -10,8 +10,8 @@
 #include "itt.hpp"
 #include "openvino/op/util/sub_graph_base.hpp"
 #include "transformations/rt_info/fused_names_attribute.hpp"
-#include "transformations/rt_info/primitives_priority_attribute.hpp"
 #include "transformations/rt_info/disable_fp16_compression.hpp"
+#include "transformations/rt_info/primitives_priority_attribute.hpp"
 
 bool ov::pass::InitNodeInfo::run_on_model(const std::shared_ptr<ov::Model>& f) {
     RUN_ON_FUNCTION_SCOPE(InitNodeInfo);
@@ -27,7 +27,6 @@ bool ov::pass::InitNodeInfo::run_on_model(const std::shared_ptr<ov::Model>& f) {
         rtInfo.emplace(FusedNames::get_type_info_static(), FusedNames{node->get_friendly_name()});
         if (ov::fp16_compression_is_disabled(node))
             rtInfo.emplace(ov::DisableFP16Compression::get_type_info_static(), DisableFP16Compression{});
-
     }
     return false;
 }
