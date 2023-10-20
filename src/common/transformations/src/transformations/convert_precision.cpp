0100a5c0c9ca0956066b7e6f93df73916524265c
Ahn, Paul Y <paul.y.ahn@intel.com>
2023-10-20 16:58:25 +0900

    do not test with this commit

--- src/common/transformations/src/transformations/convert_precision.cpp
+++ src/common/transformations/src/transformations/convert_precision.cpp
@@ -440,7 +440,7 @@ bool ov::pass::ConvertPrecision::run_on_model(const std::shared_ptr<ov::Model>& 
     if (m_keep_precision_sensitive_in_fp32) {
         pass::Manager manager(get_pass_config());
         manager.register_pass<pass::EnableDecompressionConvertConstantFolding>();
-        // manager.register_pass<pass::ConstantFolding>();
+        manager.register_pass<pass::ConstantFolding>();
         manager.run_passes(f);
     }
 
