5cba915c3f116b036e7f197eab98bac9352d5a20
Raasz, Pawel <pawel.raasz@intel.com>
2023-10-20 15:11:38 +0200

    Migrate Sqrt operator to new API

--- src/core/src/op/sqrt.cpp
+++ src/core/src/op/sqrt.cpp
@@ -2,80 +2,71 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "ngraph/op/sqrt.hpp"
+#include "openvino/op/sqrt.hpp"
 
+#include "element_visitor.hpp"
 #include "itt.hpp"
-#include "ngraph/op/add.hpp"
-#include "ngraph/op/divide.hpp"
-#include "ngraph/runtime/host_tensor.hpp"
 #include "openvino/reference/sqrt.hpp"
 
-using namespace std;
-using namespace ngraph;
+namespace ov {
+namespace op {
+namespace sqrt {
+struct Evaluate : element::NoAction<bool> {
+    using element::NoAction<bool>::visit;
 
-op::Sqrt::Sqrt(const Output<Node>& arg) : UnaryElementwiseArithmetic(arg) {
+    template <element::Type_t ET, class T = fundamental_type_for<ET>>
+    static result_type visit(const Tensor& arg0, Tensor& out, const size_t count) {
+        reference::sqrt(arg0.data<const T>(), out.data<T>(), count);
+        return true;
+    }
+};
+}  // namespace sqrt
+
+namespace v0 {
+Sqrt::Sqrt(const Output<Node>& arg) : UnaryElementwiseArithmetic(arg) {
     constructor_validate_and_infer_types();
 }
 
-bool ngraph::op::v0::Sqrt::visit_attributes(AttributeVisitor& visitor) {
+bool Sqrt::visit_attributes(AttributeVisitor& visitor) {
     OV_OP_SCOPE(v0_Sqrt_visit_attrinutes);
     return true;
 }
 
-shared_ptr<Node> op::Sqrt::clone_with_new_inputs(const OutputVector& new_args) const {
+std::shared_ptr<Node> Sqrt::clone_with_new_inputs(const OutputVector& new_args) const {
     OV_OP_SCOPE(v0_Sqrt_clone_with_new_inputs);
     check_new_args_count(this, new_args);
-    return make_shared<Sqrt>(new_args.at(0));
+    return std::make_shared<Sqrt>(new_args.at(0));
 }
 
-OPENVINO_SUPPRESS_DEPRECATED_START
-namespace sqrtop {
-namespace {
-template <element::Type_t ET>
-inline bool evaluate(const HostTensorPtr& arg0, const HostTensorPtr& out, const size_t count) {
-    using T = typename element_type_traits<ET>::value_type;
-    ov::reference::sqrt<T>(arg0->get_data_ptr<ET>(), out->get_data_ptr<ET>(), count);
-    return true;
-}
-
-bool evaluate_sqrt(const HostTensorPtr& arg0, const HostTensorPtr& out, const size_t count) {
-    bool rc = true;
-    out->set_unary(arg0);
-    switch (arg0->get_element_type()) {
-        OPENVINO_TYPE_CASE(evaluate_sqrt, i32, arg0, out, count);
-        OPENVINO_TYPE_CASE(evaluate_sqrt, i64, arg0, out, count);
-        OPENVINO_TYPE_CASE(evaluate_sqrt, u32, arg0, out, count);
-        OPENVINO_TYPE_CASE(evaluate_sqrt, u64, arg0, out, count);
-        OPENVINO_TYPE_CASE(evaluate_sqrt, f16, arg0, out, count);
-        OPENVINO_TYPE_CASE(evaluate_sqrt, f32, arg0, out, count);
-        OPENVINO_TYPE_CASE(evaluate_sqrt, f64, arg0, out, count);
-    default:
-        rc = false;
-        break;
-    }
-    return rc;
-}
-}  // namespace
-}  // namespace sqrtop
-
-bool op::Sqrt::evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const {
+bool Sqrt::evaluate(TensorVector& outputs, const TensorVector& inputs) const {
     OV_OP_SCOPE(v0_Sqrt_evaluate);
-    return sqrtop::evaluate_sqrt(inputs[0], outputs[0], shape_size(inputs[0]->get_shape()));
+    OPENVINO_ASSERT(outputs.size() == 1);
+    OPENVINO_ASSERT(inputs.size() == 1);
+
+    const auto& in_shape = inputs[0].get_shape();
+    outputs[0].set_shape(in_shape);
+    using namespace ov::element;
+    return IfTypeOf<f16, f32, f64, i32, i64, u32, u64>::apply<sqrt::Evaluate>(inputs[0].get_element_type(),
+                                                                              inputs[0],
+                                                                              outputs[0],
+                                                                              shape_size(in_shape));
 }
 
-bool op::Sqrt::has_evaluate() const {
+bool Sqrt::has_evaluate() const {
     OV_OP_SCOPE(v0_Sqrt_has_evaluate);
     switch (get_input_element_type(0)) {
-    case ngraph::element::i32:
-    case ngraph::element::i64:
-    case ngraph::element::u32:
-    case ngraph::element::u64:
-    case ngraph::element::f16:
-    case ngraph::element::f32:
-    case ngraph::element::f64:
+    case element::f16:
+    case element::f32:
+    case element::f64:
+    case element::i32:
+    case element::i64:
+    case element::u32:
+    case element::u64:
         return true;
     default:
-        break;
+        return false;
     }
-    return false;
 }
+}  // namespace v0
+}  // namespace op
+}  // namespace ov
