62b60d5cb49370b69f32dfc89208c67e54c6406a
Ilya Lavrenov <ilya.lavrenov@intel.com>
2023-10-20 21:01:20 +0800

    Moved all tensorflow proto files to a folder with ov prefix to avoid ODR conflicts

--- src/frontends/tensorflow/src/op/var_handle.cpp
+++ src/frontends/tensorflow/src/op/var_handle.cpp
@@ -10,7 +10,7 @@
 #include "ngraph/runtime/shared_buffer.hpp"
 #include "openvino/opsets/opset8.hpp"
 #include "openvino/util/mmap_object.hpp"
-#include "tensor_bundle.pb.h"
+#include "ov_tensorflow/tensor_bundle.pb.h"
 
 using namespace std;
 using namespace ov::opset8;
@@ -26,7 +26,7 @@ template <typename T>
 static std::shared_ptr<ov::Node> read_variable(std::shared_ptr<VariablesIndex> var_index,
                                                const ov::element::Type ov_type,
                                                const ov::Shape shape,
-                                               const ::ov_tensorflow::BundleEntryProto& entry,
+                                               const ::tensorflow::BundleEntryProto& entry,
                                                const NodeContext& node) {
     google::protobuf::int64 size = 1;
     for (uint64_t i = 0; i < shape.size(); ++i) {
@@ -95,7 +95,7 @@ OutputVector translate_varhandle_op(const NodeContext& node) {
 
         TENSORFLOW_OP_VALIDATION(node, result, "[TensorFlow Frontend] Internal error: Cannot find requested variable.");
 
-        ::ov_tensorflow::BundleEntryProto entry;
+        ::tensorflow::BundleEntryProto entry;
         TENSORFLOW_OP_VALIDATION(node,
                                  entry.ParseFromArray(entry_data, static_cast<int>(entry_size)),
                                  "[TensorFlow Frontend] Internal error: Cannot get read bundle entry.");
