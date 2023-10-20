1f67b8b7268f9c5ae891e5bb9c2d6d20b35ff0a1
siddhant-0707 <chauhanjoy10@gmail.com>
2023-10-20 13:16:56 +0530

    fix

--- tests/layer_tests/pytorch_tests/test_log.py
+++ tests/layer_tests/pytorch_tests/test_log.py
@@ -19,6 +19,7 @@ class TestLog(PytorchLayerTest):
             "log2": torch.log2,
             "log2_": torch.log2_,
             "log10": torch.log10,
+            "log10": torch.log10_,
             "log1p": torch.log1p,
             "log1p_": torch.log1p_
         }
@@ -47,6 +48,7 @@ class TestLog(PytorchLayerTest):
                              ["log2", "int32"], 
                              ["log2_", "float32"],
                              ["log10", "float32"],
+                             ["log10_", "float32"],
                              ["log1p", "float32"], 
                              ["log1p", "int32"], 
                              ["log1p_", "float32"]])
