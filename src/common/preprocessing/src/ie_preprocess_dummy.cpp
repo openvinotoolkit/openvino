aeff4043c1a56cad7576839114442c3e58431e76
Pedro Henrique Linhares <pedrolmota@gmail.com>
2023-10-22 22:50:28 -0300

    Fixes build for preprocessing

---
+++ src/common/preprocessing/src/ie_preprocess_dummy.cpp
@@ -0,0 +1,8 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+//  dummy.cpp
+//  openvino
+//
+
+#include "ie_preprocess_dummy.hpp"
---
+++ src/common/preprocessing/src/ie_preprocess_dummy.hpp
@@ -0,0 +1,13 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+//  dummy.hpp
+//  openvino
+//
+
+#ifndef dummy_hpp
+#define dummy_hpp
+
+#include <stdio.h>
+
+#endif /* dummy_hpp */
