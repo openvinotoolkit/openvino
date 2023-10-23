8e09fc180070f0f9d27045f5891d6873a1b2e9e3
Nikolai Shchegolev <nikolay.shchegolev@intel.com>
2023-10-23 13:08:48 +0400

    Fixes as per comments 2.

--- src/plugins/intel_cpu/src/nodes/reference.cpp
+++ src/plugins/intel_cpu/src/nodes/reference.cpp
@@ -116,9 +116,7 @@ ov::TensorVector Reference::prepareInputs() const {
         if (std::any_of(shape.begin(), shape.end(), [](const size_t dim) { return dim == 0lu; } )) {
             inputs.push_back(ov::Tensor(ovCoreNode->get_input_element_type(i), shape));
         } else {
-            if (!srcDataPtr) {
-                THROW_CPU_NODE_ERR("has empty input data on port ", i);
-            }
+            CPU_NODE_ASSERT(srcDataPtr, "has empty input data on port ", i);
             inputs.push_back(ov::Tensor(ovCoreNode->get_input_element_type(i), shape, srcDataPtr));
         }
     }
@@ -135,9 +133,7 @@ ov::TensorVector Reference::prepareOutputs() const {
         if (std::any_of(shape.begin(), shape.end(), [](const size_t dim) { return dim == 0lu; } )) {
             outputs.push_back(ov::Tensor(ovCoreNode->get_output_element_type(i), shape));
         } else {
-            if (!dstDataPtr) {
-                THROW_CPU_NODE_ERR("has empty output data on port ", i);
-            }
+            CPU_NODE_ASSERT(dstDataPtr, "has empty output data on port ", i);
             outputs.push_back(ov::Tensor(ovCoreNode->get_output_element_type(i), shape, dstDataPtr));
         }
     }
