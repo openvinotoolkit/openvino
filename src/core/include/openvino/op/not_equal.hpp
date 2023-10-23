f9c7e2e5d21c2850a793c492f945d8f1ae4a7a07
Raasz, Pawel <pawel.raasz@intel.com>
2023-10-23 11:20:54 +0200

    Migrate NotEqual operator to new API

--- src/core/include/openvino/op/not_equal.hpp
+++ src/core/include/openvino/op/not_equal.hpp
@@ -27,9 +27,7 @@ public:
 
     std::shared_ptr<Node> clone_with_new_inputs(const OutputVector& new_args) const override;
 
-    OPENVINO_SUPPRESS_DEPRECATED_START
-    bool evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const override;
-    OPENVINO_SUPPRESS_DEPRECATED_END
+    bool evaluate(TensorVector& outputs, const TensorVector& inputs) const override;
     bool has_evaluate() const override;
     bool visit_attributes(AttributeVisitor& visitor) override;
 };
