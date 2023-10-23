fb1eff2b0e32ecf4f05578b914230c27b2aa6416
jiwaszki <jan.iwaszkiewicz@intel.com>
2023-10-23 19:24:36 +0200

    Extending pyproject of OV, adding custom PDM plugins

---
+++ src/bindings/python/pdm-custom-plugins/pdm_pip_install.py
@@ -0,0 +1,48 @@
+import platform
+
+from pdm.cli.commands.install import Command as BaseCommand
+from pdm.core import Core
+
+
+class PipInstallCommand(BaseCommand):
+    """
+    This class is used to run the ``pdm install`` command in a custom mode.
+
+    The command will use the python version of the currently active environment.
+    """
+
+    description = BaseCommand.__doc__  # TODO: extend docs from Base
+    name = "pip-install"  # TODO: consider changing to "install" to override original one
+
+    def handle(self, project, options):
+        # Pass project to the function to reuse options available for Project class.
+        # For style refer to: https://github.com/pdm-project/pdm/blob/main/src/pdm/termui.py
+        def note(project, style: str, message: str) -> None:
+            if not project.is_global:
+                project.core.ui.echo(message, style=style, err=True)
+
+        # TODO: think about enable_write_lockfile to sync with pip-lock
+        # project.enable_write_lockfile = True  # set --no-lock if False
+        options.no_self = True  # set --no-self if True
+
+        # Get current environment version and apply to current install session.
+        project.pyproject.metadata["requires-python"] = f"=={platform.python_version()}"
+        note(project, "warning", f"Python version has been overriden by the user.")
+
+        note(
+            project,
+            "info",
+            f"Custom installation with Python version set to: {project.pyproject.metadata.get('requires-python', '')}",
+        )
+
+        # Probably not needed?
+        # if options.groups:
+        #     if ":all" in options.groups:
+        #         options.groups += list(project.iter_groups())
+
+        # Run `pdm install` itself.
+        super().handle(project, options)
+
+
+def register(core: Core) -> None:
+    core.register_command(PipInstallCommand)
