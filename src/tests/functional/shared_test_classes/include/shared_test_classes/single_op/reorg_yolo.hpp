18801d982c5d41439f096abad55bdda76161bb1b
vurusovs <vitaliy.urusovskij@intel.com>
2023-10-23 11:04:14 +0400

    `ReorgYolo` to API2.0

---
+++ src/tests/functional/shared_test_classes/include/shared_test_classes/single_op/reorg_yolo.hpp
@@ -0,0 +1,31 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <tuple>
+#include <string>
+#include <vector>
+
+#include "shared_test_classes/base/ov_subgraph.hpp"
+
+namespace ov {
+namespace test {
+using ReorgYoloParamsTuple = typename std::tuple<
+        std::vector<size_t>,         // Input shape
+        size_t,                         // Stride
+        ov::element::Type,              // Model type
+        ov::test::TargetDevice          // Device name
+>;
+
+class ReorgYoloLayerTest : public testing::WithParamInterface<ReorgYoloParamsTuple>,
+                            virtual public ov::test::SubgraphBaseStaticTest {
+public:
+    static std::string getTestCaseName(const testing::TestParamInfo<ReorgYoloParamsTuple> &obj);
+
+protected:
+    void SetUp() override;
+};
+}  // namespace test
+}  // namespace ov
