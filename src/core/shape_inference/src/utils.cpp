cd42d5043395b2f008281217a52b7a39ad8d7932
Pawel Raasz <pawel.raasz@intel.com>
2023-10-24 14:48:09 +0200

    [core]Migrate Minimum operator to new API (#20597)
    * Migrate Minimum op to new API
    
    * Refactor evaluates to reduce binary size
    - add infer_broadcast_shape, get shapes from tensors reduce OV_ASSERT
    - refactor Evaluate structures to reduce binary size
    
    ---------
    
    Co-authored-by: Michal Lukaszewski <michal.lukaszewski@intel.com>

--- src/core/shape_inference/src/utils.cpp
+++ src/core/shape_inference/src/utils.cpp
@@ -5,12 +5,17 @@
 #include "utils.hpp"
 
 #include "eltwise_shape_inference.hpp"
+#include "openvino/core/validation_util.hpp"
 
 namespace ov {
 namespace op {
 
 ov::Shape infer_broadcast_shape(const ov::Node* const op, const ov::Shape& first, const ov::Shape& second) {
     return eltwise_shape_infer(op, std::vector<ov::PartialShape>{first, second}).front().to_shape();
+}
+
+ov::Shape infer_broadcast_shape(const ov::Node* const op, const ov::TensorVector& inputs) {
+    return eltwise_shape_infer(op, ov::util::get_tensors_partial_shapes(inputs)).front().to_shape();
 }
 }  // namespace op
 }  // namespace ov
