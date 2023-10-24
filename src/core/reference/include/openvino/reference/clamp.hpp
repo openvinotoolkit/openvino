9892f885b2169353f13f5f55f19e1e86f89a7bbe
Raasz, Pawel <pawel.raasz@intel.com>
2023-10-24 16:00:19 +0200

    Migrate Clamp operator to new API

--- src/core/reference/include/openvino/reference/clamp.hpp
+++ src/core/reference/include/openvino/reference/clamp.hpp
@@ -9,9 +9,22 @@
 
 namespace ov {
 namespace reference {
+namespace func {
+template <class T>
+const T& clamp(const T& v, const T& min, const T& max) {
+    if (v < min) {
+        return min;
+    } else if (v > max) {
+        return max;
+    } else {
+        return v;
+    }
+}
+}  // namespace func
+
 template <typename T>
-void clamp(const T* arg, T* out, T min, T max, size_t count) {
-    for (size_t i = 0; i < count; i++) {
+void clamp(const T* arg, T* out, const T min, const T max, const size_t count) {
+    for (size_t i = 0; i < count; ++i) {
         if (arg[i] < min) {
             out[i] = min;
         } else if (arg[i] > max) {
