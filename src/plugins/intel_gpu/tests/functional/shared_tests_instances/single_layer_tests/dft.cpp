ab74cc1300690b4067b1be3417e95966efb2cc3e
andrei-cv <andrei.gorbachev@intel.com>
2023-10-25 19:17:29 +0400

    dft

--- src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/dft.cpp
+++ src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/dft.cpp
@@ -32,58 +32,58 @@ const auto combine = [](const std::vector<std::vector<ov::Shape>>& inputShapes,
 
 INSTANTIATE_TEST_SUITE_P(smoke_DFT_2d,
                          DFTLayerTest,
-                         combine({{{10, 2}}},   // input shapes
+                         combine({{{10, 2}}}, // input shapes
                                  {{0}},       // axes
                                  {{}, {3}}),  // signal sizes
                          DFTLayerTest::getTestCaseName);
 
-// INSTANTIATE_TEST_SUITE_P(smoke_DFT_3d,
-//                          DFTLayerTest,
-//                          combine({{{10, 4, 2}}},    // input shapes
-//                                  {{0, 1}},        // axes
-//                                  {{}, {3, 10}}),  // signal sizes
-//                          DFTLayerTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_DFT_3d,
+                         DFTLayerTest,
+                         combine({{{10, 4, 2}}},  // input shapes
+                                 {{0, 1}},        // axes
+                                 {{}, {3, 10}}),  // signal sizes
+                         DFTLayerTest::getTestCaseName);
 
-// INSTANTIATE_TEST_SUITE_P(smoke_DFT_4d,
-//                          DFTLayerTest,
-//                          combine({{{10, 4, 8, 2}}},    // input shapes
-//                                  {{0, 1, 2}},        // axes
-//                                  {{}, {3, 10, 8}}),  // signal sizes
-//                          DFTLayerTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_DFT_4d,
+                         DFTLayerTest,
+                         combine({{{10, 4, 8, 2}}},  // input shapes
+                                 {{0, 1, 2}},        // axes
+                                 {{}, {3, 10, 8}}),  // signal sizes
+                         DFTLayerTest::getTestCaseName);
 
-// INSTANTIATE_TEST_SUITE_P(smoke_DFT_4d_negative_reversed_axes,
-//                          DFTLayerTest,
-//                          combine({{{10, 4, 8, 2}}},    // input shapes
-//                                  {{-1, -2, -3}},     // axes
-//                                  {{}, {8, 10, 3}}),  // signal sizes
-//                          DFTLayerTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_DFT_4d_negative_reversed_axes,
+                         DFTLayerTest,
+                         combine({{{10, 4, 8, 2}}},  // input shapes
+                                 {{-1, -2, -3}},     // axes
+                                 {{}, {8, 10, 3}}),  // signal sizes
+                         DFTLayerTest::getTestCaseName);
 
-// INSTANTIATE_TEST_SUITE_P(smoke_DFT_4d_single_axis,
-//                          DFTLayerTest,
-//                          combine({{{10, 4, 8, 2}}},        // input shapes
-//                                  {{0}, {1}, {2}},        // axes
-//                                  {{}, {1}, {5}, {20}}),  // signal sizes
-//                          DFTLayerTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_DFT_4d_single_axis,
+                         DFTLayerTest,
+                         combine({{{10, 4, 8, 2}}},      // input shapes
+                                 {{0}, {1}, {2}},        // axes
+                                 {{}, {1}, {5}, {20}}),  // signal sizes
+                         DFTLayerTest::getTestCaseName);
 
-// INSTANTIATE_TEST_SUITE_P(smoke_DFT_5d,
-//                          DFTLayerTest,
-//                          combine({{{10, 4, 8, 2, 2}}},    // input shapes
-//                                  {{0, 1, 2, 3}},        // axes
-//                                  {{}, {3, 10, 8, 6}}),  // signal sizes
-//                          DFTLayerTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_DFT_5d,
+                         DFTLayerTest,
+                         combine({{{10, 4, 8, 2, 2}}},  // input shapes
+                                 {{0, 1, 2, 3}},        // axes
+                                 {{}, {3, 10, 8, 6}}),  // signal sizes
+                         DFTLayerTest::getTestCaseName);
 
-// INSTANTIATE_TEST_SUITE_P(smoke_DFT_6d,
-//                          DFTLayerTest,
-//                          combine({{{10, 4, 8, 2, 5, 2}}},    // input shapes
-//                                  {{0, 1, 2, 3, 4}},        // axes
-//                                  {{}, {3, 10, 8, 6, 2}}),  // signal sizes
-//                          DFTLayerTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_DFT_6d,
+                         DFTLayerTest,
+                         combine({{{10, 4, 8, 2, 5, 2}}},  // input shapes
+                                 {{0, 1, 2, 3, 4}},        // axes
+                                 {{}, {3, 10, 8, 6, 2}}),  // signal sizes
+                         DFTLayerTest::getTestCaseName);
 
-// INSTANTIATE_TEST_SUITE_P(smoke_DFT_6d_zero,
-//                          DFTLayerTest,
-//                          combine({{{10, 4, 8, 2, 5, 2}}},  // input shapes
-//                                  {{}},                   // axes
-//                                  {{}}),                  // signal sizes
-//                          DFTLayerTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_DFT_6d_zero,
+                         DFTLayerTest,
+                         combine({{{10, 4, 8, 2, 5, 2}}}, // input shapes
+                                 {{}},                    // axes
+                                 {{}}),                   // signal sizes
+                         DFTLayerTest::getTestCaseName);
 
 }  // namespace
