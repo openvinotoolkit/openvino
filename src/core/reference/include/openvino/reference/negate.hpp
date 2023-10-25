93f8d448540f44b5edd3f2dc9983fd2526857687
Pawel Raasz <pawel.raasz@intel.com>
2023-10-25 10:39:50 +0200

    [core]Migrate Negative operator to new API (#20609)
    * Migrate Negative operator to new API
    
    * Remove `visit_attributes` is same as base
    
    * Use std::negate instead of lambda
    
    ---------
    
    Co-authored-by: Michal Lukaszewski <michal.lukaszewski@intel.com>

--- src/core/reference/include/openvino/reference/negate.hpp
+++ src/core/reference/include/openvino/reference/negate.hpp
@@ -4,15 +4,22 @@
 
 #pragma once
 
-#include <cstddef>
+#include <algorithm>
+#include <functional>
 
 namespace ov {
 namespace reference {
+
+/**
+ * @brief Reference implementation of Negative operator.
+ *
+ * @param arg    Pointer to input data.
+ * @param out    Pointer to output data.
+ * @param count  Number of elements in input buffer.
+ */
 template <typename T>
-void negate(const T* arg, T* out, size_t count) {
-    for (size_t i = 0; i < count; i++) {
-        out[i] = -arg[i];
-    }
+void negate(const T* arg, T* out, const size_t count) {
+    std::transform(arg, std::next(arg, count), out, std::negate<T>());
 }
 }  // namespace reference
 }  // namespace ov
