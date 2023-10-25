c4f5104f689b251da78eea3a69946e18d0a10767
Raasz, Pawel <pawel.raasz@intel.com>
2023-10-25 07:30:30 +0200

    Remove not used function add comment

--- src/core/reference/include/openvino/reference/clamp.hpp
+++ src/core/reference/include/openvino/reference/clamp.hpp
@@ -9,19 +9,16 @@
 
 namespace ov {
 namespace reference {
-namespace func {
-template <class T>
-const T& clamp(const T& v, const T& min, const T& max) {
-    if (v < min) {
-        return min;
-    } else if (v > max) {
-        return max;
-    } else {
-        return v;
-    }
-}
-}  // namespace func
 
+/**
+ * @brief Reference implementation of Clamp operator.
+ *
+ * @param arg    Pointer to input data.
+ * @param out    Pointer to output data.
+ * @param min    Minimum value used to clamp input data.
+ * @param max    Maximum value used to clamp input data.
+ * @param count  Number of elements in input buffer.
+ */
 template <typename T>
 void clamp(const T* arg, T* out, const T min, const T max, const size_t count) {
     for (size_t i = 0; i < count; ++i) {
