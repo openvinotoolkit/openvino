520deeeaeaea7191d56951d3cb451a4eb95a22fe
siddhant-0707 <chauhanjoy10@gmail.com>
2023-10-25 13:11:23 +0530

    Update is_nonzero.cpp

--- src/frontends/pytorch/src/op/is_nonzero.cpp
+++ src/frontends/pytorch/src/op/is_nonzero.cpp
@@ -4,6 +4,7 @@
 
 #include "openvino/frontend/pytorch/node_context.hpp"
 #include "openvino/op/constant.hpp"
+#include "openvino/op/convert_like.hpp"
 #include "openvino/op/not_equal.hpp"
 #include "pt_framework_node.hpp"
 #include "utils.hpp"
@@ -21,7 +22,7 @@ OutputVector translate_is_nonzero(const NodeContext& context) {
 
     Output<Node> zero_tensor = context.mark_node(v0::Constant::create(element::boolean, Shape{1}, {false}));
 
-    align_eltwise_input_types(context, input, zero_tensor);
+    zero_tensor = context.mark_node(std::make_shared<v1::ConvertLike>(zero_tensor, input));
     auto result = context.mark_node(std::make_shared<v1::NotEqual>(input, zero_tensor));
 
     return {result};
