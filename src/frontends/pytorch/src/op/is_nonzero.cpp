70d1baa39d56846704e1d9c3d710910f14ea0aea
siddhant-0707 <chauhanjoy10@gmail.com>
2023-10-25 13:14:33 +0530

    Update is_nonzero.cpp

--- src/frontends/pytorch/src/op/is_nonzero.cpp
+++ src/frontends/pytorch/src/op/is_nonzero.cpp
@@ -20,7 +20,7 @@ OutputVector translate_is_nonzero(const NodeContext& context) {
     num_inputs_check(context, 1, 1);
     auto input = context.get_input(0);
 
-    Output<Node> zero_tensor = context.mark_node(v0::Constant::create(element::boolean, Shape{1}, {false}));
+    auto zero_tensor = context.mark_node(v0::Constant::create(element::boolean, Shape{1}, {false}));
 
     zero_tensor = context.mark_node(std::make_shared<v1::ConvertLike>(zero_tensor, input));
     auto result = context.mark_node(std::make_shared<v1::NotEqual>(input, zero_tensor));
