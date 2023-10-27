094bb08dc2eef764ba123b5c4979ae2967b73493
Pavel Esir <pavel.esir@intel.com>
2023-10-28 00:41:37 +0200

    keep disable_fp16_compression rt_info (#20625)
    * keep disable_fp16_compression rt_info
    
    * style fix
    
    * style fix 2
    
    * cleanup init_node_info.cpp; redefining a class for rt_info in Serialize
    
    * move rt_info refreshing inside serialize.cpp
    
    * rename rt_info name in IR
    
    * add rt_info serialize test
    
    * add ticket number
    
    * updated comments
    
    * code style fix
    
    ---------
    
    Co-authored-by: Andrei Kochin <andrei.kochin@intel.com>

--- src/core/src/pass/serialize.cpp
+++ src/core/src/pass/serialize.cpp
@@ -1208,6 +1208,14 @@ void serializeFunc(std::ostream& xml_file,
 namespace ov {
 bool pass::Serialize::run_on_model(const std::shared_ptr<ov::Model>& model) {
     RUN_ON_FUNCTION_SCOPE(Serialize);
+
+    // TODO xxx-105807: if rt_info is set in python api as a string ['precise_0'] = '',
+    //  we need to convert value to a class in order to have rt_info in the IR. The code below will convert
+    // ['precise_0'] = '' into => rt_info['precise_0'] = DisableFP16Compression{}
+    for (auto& node : model->get_ops())
+        if (fp16_compression_is_disabled(node))
+            disable_fp16_compression(node);
+
     if (m_xmlFile && m_binFile) {
         serializeFunc(*m_xmlFile, *m_binFile, model, m_version, m_custom_opsets);
     } else {
