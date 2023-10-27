047e1211cbf37523a25aaacb5e1081f4f65c0b1d
Maksim Kutakov <maksim.kutakov@intel.com>
2023-10-27 17:58:12 +0200

    Add state as input test

--- src/plugins/intel_cpu/tests/functional/subgraph_tests/src/stateful_model.cpp
+++ src/plugins/intel_cpu/tests/functional/subgraph_tests/src/stateful_model.cpp
@@ -11,6 +11,29 @@ using namespace InferenceEngine;
 using namespace ov::test;
 
 namespace SubgraphTestsDefinitions {
+
+//     ┌────────┐     ┌───────┐
+//     │ Param1 │     │ Const │
+//     └───┬────┘     └───┬───┘
+//         │              │
+//         │         ┌────┴──────┐
+//  .......│.........│ ReadValue │
+//  .      │         └────┬─────┬┘
+//  .      │              │     │
+//  .      │   ┌─────┐    │     │
+//  .      └───┤ Add ├────┘     │
+//  .          └──┬──┘          │
+//  .             │             │
+//  .             │             │
+//  . ┌────────┐  │    ┌─────┐  │
+//  ..│ Assign ├──┴────┤ Add ├──┘
+//    └────────┘       └──┬──┘
+//                        │
+//                        │
+//                   ┌────┴──────┐
+//                   │  Result   │
+//                   └───────────┘
+
 class StaticShapeStatefulModel : public SubgraphBaseTest {
 public:
     static constexpr ov::element::Type_t testPrc = ov::element::Type_t::f32;
@@ -108,6 +131,38 @@ TEST_F(StaticShapeStatefulModel, smoke_Run_Stateful_Static) {
     run_test();
 }
 
+
+// ┌─────────┐    ┌───────────┐
+// │ Param1  │    │ ReadValue │..
+// └───┬──┬──┘    └─────┬─────┘ .
+//     │  │             │       .
+//     │  │             │       .
+//     │  └──┬─────┬────┘       .
+//     │     │ Add │            .
+//     │     └──┬──┘            .
+//     │        │               .
+//     │    ┌───┴────┐          .
+//     └────┤ Concat │          .
+//          └────────┘          .
+//             / \              .
+//            /   \             .
+//   ┌────────┐   ┌────────┐    .
+//   │ Result │   │ Assign │.....
+//   └────────┘   └────────┘
+
+static void float_compare(const float* expected_res, const float* actual_res, size_t size) {
+    constexpr float rel_diff_threshold = 1e-4f;
+    for (size_t i = 0; i < size; ++i) {
+        const float expected_val = expected_res[i];
+        const float actual_val = actual_res[i];
+        if (0.f == expected_val) {
+            ASSERT_TRUE(abs(actual_val) < rel_diff_threshold);
+        } else {
+            ASSERT_TRUE(abs(actual_val / expected_val - 1.f) < rel_diff_threshold);
+        }
+    }
+}
+
 class DynamicShapeStatefulModel : public SubgraphBaseTest {
 public:
     static constexpr ov::element::Type_t testPrc = ov::element::Type_t::f32;
@@ -199,12 +254,7 @@ public:
             ASSERT_EQ(expected_res.size(), outputTensor.get_shape().front());
             auto actual_res = outputTensor.data<ov::element_type_traits<testPrc>::value_type>();
 
-            constexpr float rel_diff_threshold = 1e-4f;
-            for (size_t i = 0; i < expected_res.size(); ++i) {
-                const float expected_val = expected_res[i];
-                const float actual_val = actual_res[i];
-                ASSERT_TRUE(abs(actual_val - expected_val) / abs(expected_val) < rel_diff_threshold);
-            }
+            float_compare(expected_res.data(), actual_res, expected_res.size());
 
             auto states = inferRequest.query_state();
             ASSERT_FALSE(states.empty());
@@ -213,11 +263,7 @@ public:
             ASSERT_EQ(mstate.get_shape().front(), vec_state.size());
             auto actual_state = mstate.data<ov::element_type_traits<testPrc>::value_type>();
 
-            for (size_t i = 0; i < vec_state.size(); ++i) {
-                const float expected_state_val = vec_state[i];
-                const float actual_state_val = actual_state[i];
-                ASSERT_TRUE(abs(expected_state_val - actual_state_val) / abs(expected_state_val) < rel_diff_threshold);
-            }
+            float_compare(vec_state.data(), actual_state, vec_state.size());
         }
     }
 
@@ -229,6 +275,182 @@ public:
 };
 
 TEST_F(DynamicShapeStatefulModel, smoke_Run_Stateful_Dynamic) {
+    prepare();
+    run_test();
+    reset_state();
+    run_test();
+}
+
+//     ┌────────┐    ┌────────┐
+//     │ Param1 │    │ Param2 │
+//     └────────┘    └────────┘
+//             \     /
+//            ┌───────┐
+//            │  Add1 │
+//            └───┬───┘
+//            /   │
+//    ┌─────────┐ │  ┌───────┐
+//    │ Result1 │ │  │ Const │
+//    └───┬─────┘ │  └───────┘
+//        │       │  /
+//        │    ┌──┴───┐
+//        │    │ Add2 │
+//        │    └──┬───┘
+//        │       │
+//        │       │  ┌───────────┐
+//        └───────┼─►│ ReadValue │..
+//                │  └───────────┘ .
+//                │   /            .
+//            ┌───┴────┐            .
+//            │ Concat │            .
+//            └────────┘            .
+//             /     \             .
+//    ┌─────────┐    ┌────────┐    .
+//    │ Result2 │    │ Assign │.....
+//    └─────────┘    └────────┘
+
+class DynamicShapeStatefulModelStateAsInp : public SubgraphBaseTest {
+public:
+    static constexpr ov::element::Type_t testPrc = ov::element::Type_t::f32;
+
+public:
+    void SetUp() override {
+        targetDevice = ov::test::utils::DEVICE_CPU;
+        ov::element::Type netPrc = testPrc;
+        const_val = 42.0f;
+
+        const ov::Shape inpShape = {1, 1};
+        const InputShape input_shape = {{1, -1}, {{1, 1}, {1, 2}, {1, 4}, {1, 8}, {1, 16}}};
+        init_input_shapes({input_shape, input_shape});
+
+        auto param1 = std::make_shared<ov::op::v0::Parameter>(netPrc, inputDynamicShapes[0]);
+        auto param2 = std::make_shared<ov::op::v0::Parameter>(netPrc, inputDynamicShapes[1]);
+        auto init_param = std::make_shared<ov::op::v0::Parameter>(netPrc, ov::PartialShape{1, -1});
+
+        // The ReadValue/Assign operations must be used in pairs in the model.
+        // For each such a pair, its own variable object must be created.
+        const std::string variable_name("variable0");
+        auto variable = std::make_shared<ov::op::util::Variable>(
+            ov::op::util::VariableInfo{ov::PartialShape::dynamic(), ov::element::dynamic, variable_name});
+
+        // Creating ov::Model
+        auto read = std::make_shared<ov::op::v6::ReadValue>(init_param, variable);
+        std::vector<std::shared_ptr<ov::Node>> args = {param1, param2, read};
+        auto add1 = ngraph::builder::makeEltwise(param1, param2, ngraph::helpers::EltwiseTypes::ADD);
+        auto add_const = ov::op::v0::Constant::create(netPrc, ov::Shape{1, 1}, {const_val});
+        auto add2 = ngraph::builder::makeEltwise(add1, add_const, ngraph::helpers::EltwiseTypes::ADD);
+        constexpr int concat_axis = 1;
+        auto concat = std::make_shared<ov::op::v0::Concat>(ov::NodeVector{add2, read}, concat_axis);
+        auto assign = std::make_shared<ov::op::v6::Assign>(concat, variable);
+        auto res1 = std::make_shared<ov::op::v0::Result>(add1);
+        auto res2 = std::make_shared<ov::op::v0::Result>(concat);
+        function = std::make_shared<ov::Model>(
+            ov::ResultVector({res1, res2}),
+            ov::SinkVector({assign}),
+            ov::ParameterVector({param1, param2, init_param}));
+    }
+
+    const std::vector<float>& get_inputs() const {
+        static const std::vector<float> input_vals =
+            {2.44f, 8.06f, 0.59f, 5.21f, 0.29f, 3.33f, 0.36f, 1.75f, 3.52f, 5.46f, 4.55f, 7.13f, 7.35f, 4.81f, 4.24f, 3.60f};
+        return input_vals;
+    }
+
+    std::tuple<std::vector<float>, std::vector<float>>
+    calc_refs(const ov::Shape& inp_shape, std::vector<float>& vec_state) {
+        auto size = inp_shape[1];
+        auto& input_vals = get_inputs();
+        std::vector<float> input1(input_vals.begin(), input_vals.begin() + size);
+        std::vector<float> input2(input_vals.begin(), input_vals.begin() + size);
+        std::vector<float> result1(input1.size(), 0.f);
+        for (size_t i = 0; i < input1.size(); ++i) {
+            result1[i] = input1[i] + input2[i];
+        }
+
+        std::vector<float> result2 = result1;
+        for (size_t i = 0; i < result2.size(); ++i) {
+            result2[i] += const_val;
+        }
+        result2.insert(result2.end(), vec_state.begin(), vec_state.end());
+        vec_state = result2;
+        return {result1, result2};
+    }
+
+    void prepare() {
+        compile_model();
+        inferRequest = compiledModel.create_infer_request();
+        ASSERT_TRUE(inferRequest);
+    }
+
+    void run_test() {
+        std::vector<float> vec_state = {0.f};
+
+        auto states = inferRequest.query_state();
+        ASSERT_FALSE(states.empty());
+        auto init_tensor = ov::runtime::Tensor{testPrc, ov::Shape{1, 1}};
+        auto init_data = init_tensor.data<ov::element_type_traits<testPrc>::value_type>();
+        init_data[0] = vec_state[0];
+        states.front().set_state(init_tensor);
+
+        auto& input_vals = get_inputs();
+
+        for (auto&& shapes : targetStaticShapes) {
+            inputs.clear();
+            auto &input_shape = shapes.front();
+            const auto& funcInputs = function->inputs();
+            for (auto&& funcInput : funcInputs) {
+                auto tensor = ov::runtime::Tensor{testPrc, input_shape};
+                auto input_data = tensor.data<ov::element_type_traits<testPrc>::value_type>();
+                for (size_t i = 0; i < input_shape[1]; ++i) {
+                    input_data[i] = input_vals[i];
+                }
+                inputs.insert({funcInput.get_node_shared_ptr(), tensor});
+            }
+
+            for (const auto& input : inputs) {
+                inferRequest.set_tensor(input.first, input.second);
+            }
+            auto outputTensor1 = inferRequest.get_output_tensor(0);
+            auto outputTensor2 = inferRequest.get_output_tensor(1);
+            ASSERT_TRUE(outputTensor1);
+            ASSERT_TRUE(outputTensor2);
+            inferRequest.infer();
+            std::vector<float> result1;
+            std::vector<float> result2;
+            std::tie(result1, result2) = calc_refs(input_shape, vec_state);
+            ASSERT_EQ(result1.size(), outputTensor1.get_shape()[1]);
+            ASSERT_EQ(result2.size(), outputTensor2.get_shape()[1]);
+            auto actual_res1 = outputTensor1.data<ov::element_type_traits<testPrc>::value_type>();
+            auto actual_res2 = outputTensor2.data<ov::element_type_traits<testPrc>::value_type>();
+
+            float_compare(result1.data(), actual_res1, result1.size());
+            float_compare(result2.data(), actual_res2, result2.size());
+
+            auto states = inferRequest.query_state();
+            ASSERT_FALSE(states.empty());
+            auto mstate = states.front().get_state();
+            ASSERT_TRUE(mstate);
+            ASSERT_EQ(mstate.get_shape()[1], vec_state.size());
+            auto actual_state = mstate.data<ov::element_type_traits<testPrc>::value_type>();
+
+            float_compare(vec_state.data(), actual_state, vec_state.size());
+
+            states.front().set_state(outputTensor1);
+            vec_state = result1;
+        }
+    }
+
+    void reset_state() {
+        for (auto&& state : inferRequest.query_state()) {
+            state.reset();
+        }
+    }
+
+private:
+    float const_val = 0.0f;
+};
+
+TEST_F(DynamicShapeStatefulModelStateAsInp, smoke_Run_Stateful_Dynamic_State_As_Inp) {
     prepare();
     run_test();
     reset_state();
