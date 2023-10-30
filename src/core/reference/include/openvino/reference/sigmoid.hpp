e3816a1759417fb2503bad04b17aaaf41758a150
Raasz, Pawel <pawel.raasz@intel.com>
2023-10-30 13:46:07 +0000

    Migrate Sigmoid operator to new API

--- src/core/reference/include/openvino/reference/sigmoid.hpp
+++ src/core/reference/include/openvino/reference/sigmoid.hpp
@@ -8,24 +8,26 @@
 #include <cstddef>
 #include <type_traits>
 
+#include "openvino/reference/utils/type_util.hpp"
+
 namespace ov {
 namespace reference {
-template <typename T, typename std::enable_if<std::is_integral<T>::value, bool>::type = true>
-void sigmoid(const T* arg, T* out, size_t count) {
-    T exp_value;
-    for (size_t i = 0; i < count; i++) {
-        exp_value = static_cast<T>(std::exp(-static_cast<typename std::make_signed<T>::type>(arg[i])));
-        out[i] = static_cast<T>(1 / (1 + exp_value));
-    }
+namespace func {
+template <class T, typename std::enable_if<std::is_integral<T>::value>::type* = nullptr>
+T sigmoid(const T value) {
+    const auto exp_value = static_cast<T>(std::exp(-static_cast<typename std::make_signed<T>::type>(value)));
+    return 1 / (1 + exp_value);
 }
 
-template <typename T, typename std::enable_if<!std::is_integral<T>::value, bool>::type = true>
-void sigmoid(const T* arg, T* out, size_t count) {
-    T exp_value;
-    for (size_t i = 0; i < count; i++) {
-        exp_value = static_cast<T>(std::exp(-arg[i]));
-        out[i] = static_cast<T>(1 / (1 + exp_value));
-    }
+template <class T, typename std::enable_if<ov::is_floating_point<T>()>::type* = nullptr>
+T sigmoid(const T value) {
+    return 1 / (1 + std::exp(-value));
+}
+}  // namespace func
+
+template <class T>
+void sigmoid(const T* arg, T* out, const size_t count) {
+    std::transform(arg, arg + count, out, func::sigmoid<T>);
 }
 }  // namespace reference
 }  // namespace ov
