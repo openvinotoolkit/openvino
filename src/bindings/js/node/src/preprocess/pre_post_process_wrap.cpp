95168853caf651aea30862badad3619c44948f05
almilosz <alicja.miloszewska@intel.com>
2023-10-30 12:09:44 +0100

    Remove unaligned methods

--- src/bindings/js/node/src/preprocess/pre_post_process_wrap.cpp
+++ src/bindings/js/node/src/preprocess/pre_post_process_wrap.cpp
@@ -17,15 +17,10 @@ PrePostProcessorWrap::PrePostProcessorWrap(const Napi::CallbackInfo& info)
 }
 
 Napi::Function PrePostProcessorWrap::GetClassConstructor(Napi::Env env) {
-    return DefineClass(env,
-                       "PrePostProcessorWrap",
-                       {InstanceMethod("setInputTensorShape", &PrePostProcessorWrap::set_input_tensor_shape),
-                        InstanceMethod("preprocessResizeAlgorithm", &PrePostProcessorWrap::preprocess_resize_input),
-                        InstanceMethod("setInputTensorLayout", &PrePostProcessorWrap::set_input_tensor_layout),
-                        InstanceMethod("setInputModelLayout", &PrePostProcessorWrap::set_input_model_layout),
-                        InstanceMethod("setInputElementType", &PrePostProcessorWrap::set_input_element_type),
-                        InstanceMethod("input", &PrePostProcessorWrap::input),
-                        InstanceMethod("build", &PrePostProcessorWrap::build)});
+    return DefineClass(
+        env,
+        "PrePostProcessorWrap",
+        {InstanceMethod("input", &PrePostProcessorWrap::input), InstanceMethod("build", &PrePostProcessorWrap::build)});
 }
 
 Napi::Object PrePostProcessorWrap::Init(Napi::Env env, Napi::Object exports) {
@@ -37,56 +32,6 @@ Napi::Object PrePostProcessorWrap::Init(Napi::Env env, Napi::Object exports) {
 
     exports.Set("PrePostProcessor", func);
     return exports;
-}
-
-Napi::Value PrePostProcessorWrap::set_input_tensor_shape(const Napi::CallbackInfo& info) {
-    auto shape = js_to_cpp<ov::Shape>(info, 0, {napi_int32_array, js_array});
-    _ppp->input().tensor().set_shape(shape);
-    return info.This();
-}
-
-Napi::Value PrePostProcessorWrap::preprocess_resize_input(const Napi::CallbackInfo& info) {
-    if (info.Length() > 1 || (info.Length() == 1 && !info[0].IsString())) {
-        reportError(info.Env(), "Invalid number of arguments for `preprocess_resize_input`.");
-        return Napi::Value();
-    }
-
-    auto algorithm = (info.Length() == 0) ? "RESIZE_LINEAR" : info[0].ToString().Utf8Value();
-
-    if (algorithm == "RESIZE_CUBIC") {
-        _ppp->input().preprocess().resize(ov::preprocess::ResizeAlgorithm::RESIZE_CUBIC);
-    } else if (algorithm == "RESIZE_NEAREST") {
-        _ppp->input().preprocess().resize(ov::preprocess::ResizeAlgorithm::RESIZE_NEAREST);
-    } else if (algorithm == "RESIZE_LINEAR") {
-        _ppp->input().preprocess().resize(ov::preprocess::ResizeAlgorithm::RESIZE_LINEAR);
-    }
-
-    return info.This();
-}
-
-Napi::Value PrePostProcessorWrap::set_input_tensor_layout(const Napi::CallbackInfo& info) {
-    auto layout = js_to_cpp<ov::Layout>(info, 0, {napi_string});
-    _ppp->input().tensor().set_layout(layout);
-    return info.This();
-}
-
-Napi::Value PrePostProcessorWrap::set_input_model_layout(const Napi::CallbackInfo& info) {
-    auto layout = js_to_cpp<ov::Layout>(info, 0, {napi_string});
-    _ppp->input().model().set_layout(layout);
-    return info.This();
-}
-
-Napi::Value PrePostProcessorWrap::set_input_element_type(const Napi::CallbackInfo& info) {
-    if (info.Length() == 2 && info[0].IsNumber()) {
-        auto idx = info[0].As<Napi::Number>().Int32Value();
-        auto type = js_to_cpp<ov::element::Type_t>(info, 1, {napi_string});
-
-        _ppp->input(idx).tensor().set_element_type(type);
-        return info.This();
-    } else {
-        reportError(info.Env(), "Invalid number of arguments or it type -> " + std::to_string(info.Length()));
-        return Napi::Value();
-    }
 }
 
 Napi::Value PrePostProcessorWrap::input(const Napi::CallbackInfo& info) {
