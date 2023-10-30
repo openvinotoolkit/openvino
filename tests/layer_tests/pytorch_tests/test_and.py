474fe1a74c9c74d5af35f77c76e211e24a34734f
Mateusz Mikolajczyk <mateusz.mikolajczyk@intel.com>
2023-10-30 14:11:14 +0100

    [Opset13][PT FE] Update torch bitwise operators (#20339)
    * Add opset-13 bitwise implementation
    
    * Improvements in test
    
    * Add transformation BitwiseOps->LogicalOps for bool
    
    * Improve existing tests to better tests dtypes
    
    * Disable transformatiions for supported bitwise ops
    
    * Improvebitwise test inputs
    
    * Update src/common/transformations/src/transformations/op_conversions/convert_bitwise_to_logical_bool.cpp
    
    Co-authored-by: Katarzyna Mitrus <katarzyna.mitrus@intel.com>
    
    * Update src/common/transformations/src/transformations/op_conversions/convert_bitwise_to_logical_bool.cpp
    
    Co-authored-by: Katarzyna Mitrus <katarzyna.mitrus@intel.com>
    
    * Update src/common/transformations/src/transformations/op_conversions/convert_bitwise_to_logical_bool.cpp
    
    Co-authored-by: Katarzyna Mitrus <katarzyna.mitrus@intel.com>
    
    * Update src/common/transformations/src/transformations/op_conversions/convert_bitwise_to_logical_bool.cpp
    
    Co-authored-by: Katarzyna Mitrus <katarzyna.mitrus@intel.com>
    
    * Update to REGISETR_PASS
    
    ---------
    
    Co-authored-by: Katarzyna Mitrus <katarzyna.mitrus@intel.com>

--- tests/layer_tests/pytorch_tests/test_and.py
+++ tests/layer_tests/pytorch_tests/test_and.py
@@ -9,13 +9,11 @@ from pytorch_layer_test_class import PytorchLayerTest
 
 
 class TestAnd(PytorchLayerTest):
-
     def _prepare_input(self):
         return self.input_data
 
     def create_model_tensor_input(self):
         class aten_and_tensor(torch.nn.Module):
-
             def __init__(self) -> None:
                 super().__init__()
 
@@ -25,10 +23,9 @@ class TestAnd(PytorchLayerTest):
         ref_net = None
 
         return aten_and_tensor(), ref_net, "aten::__and__"
-    
+
     def create_model_bool_input(self):
         class aten_and_bool(torch.nn.Module):
-
             def __init__(self) -> None:
                 super().__init__()
 
@@ -39,18 +36,43 @@ class TestAnd(PytorchLayerTest):
 
         return aten_and_bool(), ref_net, "aten::__and__"
 
+    def create_model_int_input(self):
+        class aten_and_int(torch.nn.Module):
+            def __init__(self) -> None:
+                super().__init__()
+
+            def forward(self, int_a: int, int_b: int):
+                return int_a & int_b
+
+        ref_net = None
+
+        return aten_and_int(), ref_net, "aten::__and__"
+
     @pytest.mark.nightly
     @pytest.mark.precommit
     def test_and_tensor(self, ie_device, precision, ir_version):
-        self.input_data = (np.array([True, False, False], dtype=np.bool_), np.array(
-            [True, True, False], dtype=np.bool_))
-        self._test(*self.create_model_tensor_input(),
-                   ie_device, precision, ir_version)
+        self.input_data = (
+            np.array([True, False, False], dtype=np.bool_),
+            np.array([True, True, False], dtype=np.bool_),
+        )
+        self._test(*self.create_model_tensor_input(), ie_device, precision, ir_version)
 
     @pytest.mark.nightly
     @pytest.mark.precommit
     def test_and_bool(self, ie_device, precision, ir_version):
-        self.input_data = (np.array(True, dtype=np.bool_),
-                           np.array(True, dtype=np.bool_))
-        self._test(*self.create_model_bool_input(),
-                   ie_device, precision, ir_version)
+        self.input_data = (np.array(True, dtype=np.bool_), np.array(True, dtype=np.bool_))
+        self._test(*self.create_model_bool_input(), ie_device, precision, ir_version)
+
+    @pytest.mark.nightly
+    @pytest.mark.precommit
+    def test_and_int(self, ie_device, precision, ir_version):
+        self.input_data = (np.array(3, dtype=np.int32), np.array(4, dtype=np.int32))
+        self._test(*self.create_model_int_input(), ie_device, precision, ir_version)
+
+    @pytest.mark.nightly
+    @pytest.mark.precommit
+    def test_and_tensor(self, ie_device, precision, ir_version):
+        self.input_data = (np.array([3, 5, 8], dtype=np.int32), np.array([7, 11, 2], dtype=np.int32))
+        self._test(
+            *self.create_model_tensor_input(), ie_device, precision, ir_version, freeze_model=False, trace_model=True
+        )
--- tests/layer_tests/pytorch_tests/test_or.py
+++ tests/layer_tests/pytorch_tests/test_or.py
@@ -1,29 +1,78 @@
 # Copyright (C) 2018-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+import numpy as np
 import pytest
+import torch
+
 from pytorch_layer_test_class import PytorchLayerTest
 
 
-class TestLog(PytorchLayerTest):
+class TestOr(PytorchLayerTest):
     def _prepare_input(self):
-        import numpy as np
-        return (np.random.randint(0, 255, (20, 30, 40, 50)),)
+        return self.input_data
 
-    def create_model(self):
-        import torch
+    def create_model_tensor_input(self):
+        class aten_or_tensor(torch.nn.Module):
+            def __init__(self) -> None:
+                super().__init__()
 
-        class aten_or(torch.nn.Module):
-            def forward(self, x):
-                res = torch.ByteTensor(x.size()).zero_()
-                res[:, :, :, 1:] = res[:, :, :, 1:] | (x[:, :, :, 1:] != x[:, :, :, :-1])
-                res[:, :, :, :-1] = res[:, :, :, :-1] | (x[:, :, :, 1:] != x[:, :, :, :-1])
-                return res.float()
+            def forward(self, tensor_a, tensor_b):
+                return tensor_a | tensor_b
 
-        return aten_or(), None, "aten::__or__"
+        ref_net = None
+
+        return aten_or_tensor(), ref_net, "aten::__or__"
+
+    def create_model_bool_input(self):
+        class aten_or_bool(torch.nn.Module):
+            def __init__(self) -> None:
+                super().__init__()
+
+            def forward(self, bool_a: bool, bool_b: bool):
+                return bool_a | bool_b
+
+        ref_net = None
+
+        return aten_or_bool(), ref_net, "aten::__or__"
+
+    def create_model_int_input(self):
+        class aten_or_int(torch.nn.Module):
+            def __init__(self) -> None:
+                super().__init__()
+
+            def forward(self, int_a: int, int_b: int):
+                return int_a | int_b
+
+        ref_net = None
+
+        return aten_or_int(), ref_net, "aten::__or__"
 
     @pytest.mark.nightly
     @pytest.mark.precommit
-    def test_or(self, ie_device, precision, ir_version):
-        self._test(*self.create_model(), ie_device, precision, ir_version,
-                   dynamic_shapes=False, trace_model=True, use_convert_model=True)
+    def test_or_tensor(self, ie_device, precision, ir_version):
+        self.input_data = (
+            np.array([True, False, False], dtype=np.bool_),
+            np.array([True, True, False], dtype=np.bool_),
+        )
+        self._test(*self.create_model_tensor_input(), ie_device, precision, ir_version)
+
+    @pytest.mark.nightly
+    @pytest.mark.precommit
+    def test_or_bool(self, ie_device, precision, ir_version):
+        self.input_data = (np.array(True, dtype=np.bool_), np.array(True, dtype=np.bool_))
+        self._test(*self.create_model_bool_input(), ie_device, precision, ir_version)
+
+    @pytest.mark.nightly
+    @pytest.mark.precommit
+    def test_or_int(self, ie_device, precision, ir_version):
+        self.input_data = (np.array(3, dtype=np.int32), np.array(4, dtype=np.int32))
+        self._test(*self.create_model_int_input(), ie_device, precision, ir_version)
+
+    @pytest.mark.nightly
+    @pytest.mark.precommit
+    def test_or_tensor(self, ie_device, precision, ir_version):
+        self.input_data = (np.array([3, 5, 8], dtype=np.int32), np.array([7, 11, 2], dtype=np.int32))
+        self._test(
+            *self.create_model_tensor_input(), ie_device, precision, ir_version, freeze_model=False, trace_model=True
+        )
