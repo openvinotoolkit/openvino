b1547f6ff781b4e168a0a277015fafdc197372d0
Kim,SungEun <sungeun.kim@intel.com>
2023-10-31 18:10:38 +0900

    move the logic to impl/convolution

--- src/plugins/intel_gpu/src/plugin/ops/convolution.cpp
+++ src/plugins/intel_gpu/src/plugin/ops/convolution.cpp
@@ -78,13 +78,6 @@ static void CreateConvolutionOp(ProgramBuilder& p, const std::shared_ptr<ov::op:
     auto pads_end = op->get_pads_end();
     auto auto_pad = op->get_auto_pad();
 
-    if (!op->is_dynamic()) {
-        // Extend 1d vectors to 2d as 1d can't be handled properly by the graph optimizer for now
-        strides.resize(std::max<size_t>(2, strides.size()), 1);
-        dilations.resize(std::max<size_t>(2, strides.size()), 1);
-        pads_begin.resize(std::max<size_t>(2, pads_begin.size()), 0);
-        pads_end.resize(std::max<size_t>(2, pads_end.size()), 0);
-    }
     auto convPrim = cldnn::convolution(layerName,
                                        inputs[0],
                                        weights,
