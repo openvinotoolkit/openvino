d227349f7ba2ca57abd404afe9984058d5a0b409
Roman Kazantsev <roman.kazantsev@intel.com>
2023-10-31 12:46:36 +0400

    [TF FE] Switch off TF1 While support totally (#20774)
    * [TF FE] Switch off TF1 While support totally
    
    This is a total switch off due to GPU limitation
    
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>
    
    * Need additional fallback in Enter to avoid shapes problem
    
    * Disable tests with While op
    
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>
    
    * Disable layer test for TF1 While
    
    * Remove extra spaces
    
    ---------
    
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/frontends/tensorflow/src/op/loop_cond.cpp
+++ src/frontends/tensorflow/src/op/loop_cond.cpp
@@ -22,6 +22,9 @@ OutputVector translate_loop_cond_op(const NodeContext& node) {
     default_op_checks(node, 1, {"LoopCond"});
     auto input = node.get_input(0);
 
+    // TODO 123651: remove this fallback to the legacy FE once GPU fixes dynamism for Loop operation
+    TENSORFLOW_OP_VALIDATION(node, false, "Fallback to legacy FE: Switch off TF1 While support due to GPU limitation");
+
     auto loop_cond_node = make_shared<LoopCond>(input, node.get_decoder());
     set_node_name(node.get_name(), loop_cond_node);
 
