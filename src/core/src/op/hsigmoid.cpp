0d824b16386074a5180aa9b77154d9774dc83ee9
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2023-11-02 12:04:10 +0100

    Use ov::Tensor
    instaed of ngraph::HostTensor

--- src/core/src/op/hsigmoid.cpp
+++ src/core/src/op/hsigmoid.cpp
@@ -2,11 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "ngraph/op/hsigmoid.hpp"
+#include "openvino/op/hsigmoid.hpp"
 
+#include "element_visitor.hpp"
 #include "itt.hpp"
-#include "ngraph/runtime/host_tensor.hpp"  // tbr
-#include "ngraph/validation_util.hpp"      // tbr
 #include "openvino/reference/hsigmoid.hpp"
 
 namespace ov {
@@ -21,38 +20,33 @@ std::shared_ptr<Node> HSigmoid::clone_with_new_inputs(const OutputVector& new_ar
     return std::make_shared<HSigmoid>(new_args.at(0));
 }
 
-OPENVINO_SUPPRESS_DEPRECATED_START
+namespace hsigmoid {
 namespace {
-template <element::Type_t ET>
-inline bool evaluate(const ngraph::HostTensorPtr& arg, const ngraph::HostTensorPtr& out, const size_t count) {
-    using T = typename element_type_traits<ET>::value_type;
+struct Evaluate : element::NoAction<bool> {
+    using element::NoAction<bool>::visit;
 
-    ov::reference::hsigmoid<T>(arg->get_data_ptr<ET>(), out->get_data_ptr<ET>(), count);
-    return true;
-}
-
-bool evaluate_hsigmoid(const ngraph::HostTensorPtr& arg, const ngraph::HostTensorPtr& out) {
-    bool rc = true;
-    size_t count = shape_size(arg->get_shape());
-    out->set_unary(arg);
-
-    switch (arg->get_element_type()) {
-        OPENVINO_TYPE_CASE(evaluate_hsigmoid, bf16, arg, out, count);
-        OPENVINO_TYPE_CASE(evaluate_hsigmoid, f16, arg, out, count);
-        OPENVINO_TYPE_CASE(evaluate_hsigmoid, f32, arg, out, count);
-    default:
-        rc = false;
-        break;
+    template <element::Type_t ET, class T = fundamental_type_for<ET>>
+    static result_type visit(const Tensor& in, Tensor& out, const size_t count) {
+        ov::reference::hsigmoid(in.data<const T>(), out.data<T>(), count);
+        return true;
     }
-    return rc;
-}
+};
 }  // namespace
+}  // namespace hsigmoid
 
-bool HSigmoid::evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const {
+bool HSigmoid::evaluate(TensorVector& outputs, const TensorVector& inputs) const {
     OV_OP_SCOPE(v5_HSigmoid_evaluate);
     OPENVINO_ASSERT(outputs.size() == 1);
     OPENVINO_ASSERT(inputs.size() == 1);
-    return evaluate_hsigmoid(inputs[0], outputs[0]);
+
+    const auto& input_shape = inputs[0].get_shape();
+    const auto count = shape_size(input_shape);
+    outputs[0].set_shape(input_shape);
+    using namespace ov::element;
+    return IfTypeOf<bf16, f16, f32>::apply<hsigmoid::Evaluate>(inputs[0].get_element_type(),
+                                                               inputs[0],
+                                                               outputs[0],
+                                                               count);
 }
 
 bool HSigmoid::has_evaluate() const {
