e4da423662d0bfe327c4a708e57561466b8c7b41
Raasz, Pawel <pawel.raasz@intel.com>
2023-11-02 08:34:27 +0100

    Migrate FloorMod operator to new API

--- src/core/reference/include/openvino/reference/floor_mod.hpp
+++ src/core/reference/include/openvino/reference/floor_mod.hpp
@@ -8,9 +8,31 @@
 #include <cstddef>
 
 #include "openvino/reference/autobroadcast_binop.hpp"
+#include "openvino/reference/mod.hpp"
 
 namespace ov {
 namespace reference {
+namespace func {
+
+template <class T, typename std::enable_if<std::is_unsigned<T>::value>::type* = nullptr>
+constexpr T floor_mod(const T x, const T y) {
+    return mod(x, y);
+}
+
+template <class T, typename std::enable_if<std::is_integral<T>::value && std::is_signed<T>::value>::type* = nullptr>
+T floor_mod(const T x, const T y) {
+    // Cast to double is needed for integer input,
+    // otherwise std::floor will act like std::trunc
+    const auto floor_div = static_cast<T>(std::floor(x / static_cast<double>(y)));
+    return x - (floor_div * y);
+}
+
+template <class T, typename std::enable_if<ov::is_floating_point<T>()>::type* = nullptr>
+T floor_mod(const T x, const T y) {
+    return x - (std::floor(x / y) * y);
+}
+}  // namespace func
+
 template <typename T>
 void floor_mod(const T* arg0,
                const T* arg1,
@@ -18,12 +40,7 @@ void floor_mod(const T* arg0,
                const Shape& arg0_shape,
                const Shape& arg1_shape,
                const op::AutoBroadcastSpec& broadcast_spec) {
-    autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, [](T x, T y) -> T {
-        // Cast to double is needed for integer input,
-        // otherwise std::floor will act like std::trunc
-        const double divisor = static_cast<double>(y);
-        return static_cast<T>(x - y * std::floor(x / divisor));
-    });
+    autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, func::floor_mod<T>);
 }
 }  // namespace reference
 }  // namespace ov
