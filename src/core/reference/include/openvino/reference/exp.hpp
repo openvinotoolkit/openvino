f6b52d648cc1c288dc4458688d5ffb388cb82e3f
Raasz, Pawel <pawel.raasz@intel.com>
2023-11-06 11:26:48 +0000

    Migrate Exp operator to new API

--- src/core/reference/include/openvino/reference/exp.hpp
+++ src/core/reference/include/openvino/reference/exp.hpp
@@ -9,11 +9,19 @@
 
 namespace ov {
 namespace reference {
-template <typename T>
+
+/**
+ * @brief Reference implementation of Exp operator.
+ *
+ * @param arg    Pointer to input data.
+ * @param out    Pointer to output data.
+ * @param count  Number of elements in input buffer.
+ */
+template <class T>
 void exp(const T* arg, T* out, size_t count) {
-    for (size_t i = 0; i < count; i++) {
-        out[i] = static_cast<T>(std::exp(arg[i]));
-    }
+    std::transform(arg, arg + count, out, [](const T v) {
+        return static_cast<T>(std::exp(v));
+    });
 }
 }  // namespace reference
 }  // namespace ov
