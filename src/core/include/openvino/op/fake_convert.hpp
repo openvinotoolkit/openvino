c75b12e71d9db0d15857e425fcc0a36dff57783e
mitruska <katarzyna.mitrus@intel.com>
2023-11-07 14:13:06 +0100

    Update op hpp

--- src/core/include/openvino/op/fake_convert.hpp
+++ src/core/include/openvino/op/fake_convert.hpp
@@ -9,29 +9,37 @@
 namespace ov {
 namespace op {
 namespace v13 {
-    /// \ingroup ov_ops_cpp_api
-    class OPENVINO_API FakeConvert : public Op {
-    public:
-        OPENVINO_OP("FakeConvert", "opset13");
+/// \ingroup ov_ops_cpp_api
+class OPENVINO_API FakeConvert : public Op {
+public:
+    OPENVINO_OP("FakeConvert", "opset13");
 
-        FakeConvert() = default;
-        FakeConvert(const ov::Output<ov::Node>& arg,
-                   const ov::Output<ov::Node>& scale,
-                   const ov::Output<ov::Node>& shift,
-                   const std::string& destination_type,
-                   bool apply_scale);
+    FakeConvert() = default;
+    FakeConvert(const ov::Output<ov::Node>& arg,
+                const ov::Output<ov::Node>& scale,
+                const ov::Output<ov::Node>& shift,
+                const std::string& destination_type = "F8E4M3",
+                bool apply_scale = true);
 
-        void validate_and_infer_types() override;
-        std::shared_ptr<ov::Node> clone_with_new_inputs(const ov::OutputVector& new_args) const override;
-        bool visit_attributes(ov::AttributeVisitor& visitor) override;
-        bool has_evaluate() const override;
+    void validate_and_infer_types() override;
+    std::shared_ptr<ov::Node> clone_with_new_inputs(const ov::OutputVector& new_args) const override;
+    bool visit_attributes(ov::AttributeVisitor& visitor) override;
+    bool has_evaluate() const override;
 
-    private:
-        void validate() const;
-        std::string m_destination_type = "F8E4M3";
-        bool m_apply_scale = false;
-        static const std::vector<std::string> m_valid_types;
-    };
+    bool get_apply_scale() const {
+        return m_apply_scale;
+    }
+
+    const std::string& destination_type() const {
+        return m_destination_type;
+    }
+
+private:
+    void validate() const;
+    std::string m_destination_type = "F8E4M3";
+    bool m_apply_scale = false;
+    static const std::vector<std::string> m_valid_types;
+};
 }  // namespace v13
 }  // namespace op
 }  // namespace ov
