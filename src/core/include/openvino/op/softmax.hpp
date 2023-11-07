1603f95bb8810aa9c887c34712601c56c0d2a286
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2023-11-07 08:26:58 +0100

    [core] Migrate Softmax operator to new API (#20894)
    * Drop ngraph remains
    
    * Use ov::Tensor
    
    instaed of ngraph::HostTensor

--- src/core/include/openvino/op/softmax.hpp
+++ src/core/include/openvino/op/softmax.hpp
@@ -38,9 +38,7 @@ public:
     void set_axis(const size_t axis) {
         m_axis = axis;
     }
-    OPENVINO_SUPPRESS_DEPRECATED_START
-    bool evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const override;
-    OPENVINO_SUPPRESS_DEPRECATED_END
+    bool evaluate(TensorVector& outputs, const TensorVector& inputs) const override;
     bool has_evaluate() const override;
 
 private:
@@ -77,9 +75,7 @@ public:
     void set_axis(const int64_t& axis) {
         m_axis = axis;
     }
-    OPENVINO_SUPPRESS_DEPRECATED_START
-    bool evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const override;
-    OPENVINO_SUPPRESS_DEPRECATED_END
+    bool evaluate(TensorVector& outputs, const TensorVector& inputs) const override;
     bool has_evaluate() const override;
 
 private:
