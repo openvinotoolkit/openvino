13e1e85d40f50bd74d7456b4f0a01d2a3cb94072
vurusovs <vitaliy.urusovskij@intel.com>
2023-11-08 16:25:41 +0400

    Restore ng::helpers::QuantGranularity for BWD CMP

--- src/tests/ov_helpers/ov_models/include/ov_models/utils/ov_helpers.hpp
+++ src/tests/ov_helpers/ov_models/include/ov_models/utils/ov_helpers.hpp
@@ -79,7 +79,12 @@ using ov::test::utils::ConversionTypes;
 using ov::test::utils::LogicalTypes;
 using ov::test::utils::SqueezeOpType;
 using ov::test::utils::MinMaxOpType;
-using ov::test::utils::QuantizationGranularity;
+
+enum QuantizationGranularity {
+    Pertensor,
+    Perchannel
+};
+
 using ov::test::utils::TensorIteratorBody;
 using ov::test::utils::ReductionType;
 using ov::test::utils::DFTOpType;
@@ -90,6 +95,23 @@ using ov::test::utils::MemoryTransformation;
 // clang-format on
 
 bool is_tensor_iterator_exist(const std::shared_ptr<ngraph::Function>& func);
+
+inline std::string quantizationGranularityToString(const QuantizationGranularity& granularity) {
+    static std::map<QuantizationGranularity, std::string> names = {
+        {Pertensor, "Pertensor"},
+        {Perchannel, "Perchannel"},
+    };
+
+    auto i = names.find(granularity);
+    if (i != names.end())
+        return i->second;
+    else
+        throw std::runtime_error("Unsupported QuantizationGranularity type");
+}
+
+inline std::ostream& operator<<(std::ostream& out, const QuantizationGranularity& granularity) {
+    return out << quantizationGranularityToString(granularity);
+}
 
 ngraph::OutputVector convert2OutputVector(const std::vector<std::shared_ptr<ngraph::Node>>& nodes);
 
