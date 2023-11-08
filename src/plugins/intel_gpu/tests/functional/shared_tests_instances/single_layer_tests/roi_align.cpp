cb5453c03b55e33df0e1c9d322f3211d51622efa
Andrei Gorbachev <andrei.gorbachev@intel.com>
2023-11-08 09:42:44 +0000

    [GPU] Refactor (#20938)
    * maxmin
    
    * mvn
    
    * normalize_l2 and fix mvn
    
    * prior_box_clustered
    
    * prior_box
    
    * pad
    
    * roi_align
    
    * scatter_update
    
    * select
    
    * shape_of
    
    * shuffle_channels
    
    * space_to_batch
    
    * space_to_depth
    
    * split
    
    * squeeze_unsqueeze
    
    * tile
    
    * transpose
    
    * variadic_split
    
    * scatter_nd_update

--- src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/roi_align.cpp
+++ src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/roi_align.cpp
@@ -1,16 +1,15 @@
 // Copyright (C) 2018-2023 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
-#include "single_layer_tests/roi_align.hpp"
-
-#include <vector>
-
+#include "single_op_tests/roi_align.hpp"
 #include "common_test_utils/test_constants.hpp"
 
-using namespace LayerTestsDefinitions;
+namespace {
+using ov::test::ROIAlignLayerTest;
+using ov::test::ROIAlignV9LayerTest;
 
-const std::vector<InferenceEngine::Precision> netPRCs = {
-    InferenceEngine::Precision::FP32
+const std::vector<ov::element::Type> netPRCs = {
+    ov::element::f32
     // There is no possibility to test ROIAlign in fp16 precision,
     // because on edge cases where in fp32 version ROI value is
     // a little bit smaller than the nearest integer value,
@@ -19,14 +18,15 @@ const std::vector<InferenceEngine::Precision> netPRCs = {
     // in fp32 and fp16 precisions.
     // In real AI applications this problem is solved by precision-aware training.
 
-    // InferenceEngine::Precision::FP16
+    // ov::element::f16
 };
 
 INSTANTIATE_TEST_SUITE_P(smoke_TestsROIAlign_average,
                          ROIAlignLayerTest,
-                         ::testing::Combine(::testing::ValuesIn(std::vector<std::vector<size_t>>{{3, 8, 16, 16},
-                                                                                                 {2, 1, 16, 16},
-                                                                                                 {2, 1, 8, 16}}),
+                         ::testing::Combine(::testing::ValuesIn(ov::test::static_shapes_to_test_representation(
+                                                            std::vector<std::vector<ov::Shape>>{{{3, 8, 16, 16}},
+                                                                                                {{2, 1, 16, 16}},
+                                                                                                {{2, 1, 8, 16}}})),
                                             ::testing::Values(std::vector<size_t>{2, 4}),
                                             ::testing::Values(2),
                                             ::testing::Values(2),
@@ -39,9 +39,10 @@ INSTANTIATE_TEST_SUITE_P(smoke_TestsROIAlign_average,
 
 INSTANTIATE_TEST_SUITE_P(smoke_TestsROIAlign_max,
                          ROIAlignLayerTest,
-                         ::testing::Combine(::testing::ValuesIn(std::vector<std::vector<size_t>>{{2, 8, 20, 20},
-                                                                                                 {2, 1, 20, 20},
-                                                                                                 {2, 1, 10, 20}}),
+                         ::testing::Combine(::testing::ValuesIn(ov::test::static_shapes_to_test_representation(
+                                                            std::vector<std::vector<ov::Shape>>{{{2, 8, 20, 20}},
+                                                                                                {{2, 1, 20, 20}},
+                                                                                                {{2, 1, 10, 20}}})),
                                             ::testing::Values(std::vector<size_t>{2, 4}),
                                             ::testing::Values(2),
                                             ::testing::Values(2),
@@ -54,11 +55,12 @@ INSTANTIATE_TEST_SUITE_P(smoke_TestsROIAlign_max,
 
 INSTANTIATE_TEST_SUITE_P(smoke_TestsROIAlign_avg_asym,
                          ROIAlignV9LayerTest,
-                         ::testing::Combine(::testing::ValuesIn(std::vector<std::vector<size_t>>{{2, 1, 8, 8},
-                                                                                                 {2, 8, 20, 20},
-                                                                                                 {2, 1, 20, 20},
-                                                                                                 {2, 1, 10, 20}}),
-                                            ::testing::Values(std::vector<size_t>{2, 4}),
+                         ::testing::Combine(::testing::ValuesIn(ov::test::static_shapes_to_test_representation(
+                                                            std::vector<std::vector<ov::Shape>>{{{2, 1, 8, 8}},
+                                                                                                {{2, 8, 20, 20}},
+                                                                                                {{2, 1, 20, 20}},
+                                                                                                {{2, 1, 10, 20}}})),
+                                            ::testing::Values(ov::Shape{2, 4}),
                                             ::testing::Values(2),
                                             ::testing::Values(2),
                                             ::testing::ValuesIn(std::vector<float>{1, 0.625}),
@@ -71,11 +73,12 @@ INSTANTIATE_TEST_SUITE_P(smoke_TestsROIAlign_avg_asym,
 
 INSTANTIATE_TEST_SUITE_P(smoke_TestsROIAlign_avg_hpfn,
                          ROIAlignV9LayerTest,
-                         ::testing::Combine(::testing::ValuesIn(std::vector<std::vector<size_t>>{{2, 1, 8, 8},
-                                                                                                 {2, 8, 20, 20},
-                                                                                                 {2, 1, 20, 20},
-                                                                                                 {2, 1, 10, 20}}),
-                                            ::testing::Values(std::vector<size_t>{2, 4}),
+                         ::testing::Combine(::testing::ValuesIn(ov::test::static_shapes_to_test_representation(
+                                                            std::vector<std::vector<ov::Shape>>{{{2, 1, 8, 8}},
+                                                                                                {{2, 8, 20, 20}},
+                                                                                                {{2, 1, 20, 20}},
+                                                                                                {{2, 1, 10, 20}}})),
+                                            ::testing::Values(ov::Shape{2, 4}),
                                             ::testing::Values(2),
                                             ::testing::Values(2),
                                             ::testing::ValuesIn(std::vector<float>{1, 0.625}),
@@ -88,11 +91,12 @@ INSTANTIATE_TEST_SUITE_P(smoke_TestsROIAlign_avg_hpfn,
 
 INSTANTIATE_TEST_SUITE_P(smoke_TestsROIAlign_max_hp,
                          ROIAlignV9LayerTest,
-                         ::testing::Combine(::testing::ValuesIn(std::vector<std::vector<size_t>>{{2, 1, 8, 8},
-                                                                                                 {2, 8, 20, 20},
-                                                                                                 {2, 1, 20, 20},
-                                                                                                 {2, 1, 10, 20}}),
-                                            ::testing::Values(std::vector<size_t>{2, 4}),
+                         ::testing::Combine(::testing::ValuesIn(ov::test::static_shapes_to_test_representation(
+                                                            std::vector<std::vector<ov::Shape>>{{{2, 1, 8, 8}},
+                                                                                                {{2, 8, 20, 20}},
+                                                                                                {{2, 1, 20, 20}},
+                                                                                                {{2, 1, 10, 20}}})),
+                                            ::testing::Values(ov::Shape{2, 4}),
                                             ::testing::Values(2),
                                             ::testing::Values(2),
                                             ::testing::ValuesIn(std::vector<float>{1, 0.625}),
@@ -102,3 +106,4 @@ INSTANTIATE_TEST_SUITE_P(smoke_TestsROIAlign_max_hp,
                                             ::testing::ValuesIn(netPRCs),
                                             ::testing::Values(ov::test::utils::DEVICE_GPU)),
                          ROIAlignV9LayerTest::getTestCaseName);
+}  // namespace
--- src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/shape_of.cpp
+++ src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/shape_of.cpp
@@ -2,30 +2,28 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include <vector>
-
-#include "single_layer_tests/shape_of.hpp"
+#include "single_op_tests/shape_of.hpp"
 #include "common_test_utils/test_constants.hpp"
 
-using namespace LayerTestsDefinitions;
-
 namespace {
-const std::vector<InferenceEngine::Precision> model_precisions = {
-        InferenceEngine::Precision::FP32,
-        InferenceEngine::Precision::I32
+using ov::test::ShapeOfLayerTest;
+
+const std::vector<ov::element::Type> model_precisions = {
+        ov::element::f32,
+        ov::element::i32
 };
 
-const std::vector<InferenceEngine::SizeVector> input_shapes = {
-    std::vector<size_t>({1, 2, 3, 4, 5}),
-    std::vector<size_t>({1, 2, 3, 4}),
-    std::vector<size_t>({1, 2})
+const std::vector<std::vector<ov::Shape>> input_shapes = {
+    {{1, 2, 3, 4, 5}},
+    {{1, 2, 3, 4}},
+    {{1, 2}}
 };
 
 INSTANTIATE_TEST_SUITE_P(smoke_Check, ShapeOfLayerTest,
                         ::testing::Combine(
                                 ::testing::ValuesIn(model_precisions),
-                                ::testing::Values(InferenceEngine::Precision::I64),
-                                ::testing::ValuesIn(input_shapes),
+                                ::testing::Values(ov::element::i64),
+                                ::testing::ValuesIn(ov::test::static_shapes_to_test_representation(input_shapes)),
                                 ::testing::Values(ov::test::utils::DEVICE_GPU)),
                         ShapeOfLayerTest::getTestCaseName);
 }  // namespace
