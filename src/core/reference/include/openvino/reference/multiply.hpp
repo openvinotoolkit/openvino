06e38b81fb894be1a97125f1b6979d57918adf59
Pawel Raasz <pawel.raasz@intel.com>
2023-11-08 10:53:20 +0100

    [core]Migrate Multiply operator to new API (#20853)
    * Migrate Multiply operator to new API
    
    * Add comment explain use of custom multiply
    
    * Update custom multiply comment
    
    Co-authored-by: Tomasz Jankowski <tomasz1.jankowski@intel.com>
    
    ---------
    
    Co-authored-by: Tomasz Jankowski <tomasz1.jankowski@intel.com>

--- src/core/reference/include/openvino/reference/multiply.hpp
+++ src/core/reference/include/openvino/reference/multiply.hpp
@@ -4,21 +4,36 @@
 
 #pragma once
 
+#include <algorithm>
 #include <cstddef>
 
-#include "openvino/core/shape.hpp"
-#include "openvino/op/util/attr_types.hpp"
 #include "openvino/reference/autobroadcast_binop.hpp"
 
 namespace ov {
 namespace reference {
+namespace func {
+// Usage of custom function instead of std::multiplies gives smaller binary size.
+template <class T>
+constexpr T multiply(const T a, const T b) {
+    return a * b;
+}
+}  // namespace func
+
 template <typename T>
-void multiply(const T* arg0, const T* arg1, T* out, size_t count) {
-    for (size_t i = 0; i < count; i++) {
-        out[i] = arg0[i] * arg1[i];
-    }
+void multiply(const T* arg0, const T* arg1, T* out, const size_t count) {
+    std::transform(arg0, arg0 + count, arg1, out, func::multiply<T>);
 }
 
+/**
+ * @brief Reference implementation of binary elementwise Multiply operator.
+ *
+ * @param arg0            Pointer to input 0 data.
+ * @param arg1            Pointer to input 1 data.
+ * @param out             Pointer to output data.
+ * @param arg_shape0      Input 0 shape.
+ * @param arg_shape1      Input 1 shape.
+ * @param broadcast_spec  Broadcast specification mode.
+ */
 template <typename T>
 void multiply(const T* arg0,
               const T* arg1,
@@ -26,9 +41,7 @@ void multiply(const T* arg0,
               const Shape& arg0_shape,
               const Shape& arg1_shape,
               const op::AutoBroadcastSpec& broadcast_spec) {
-    autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, [](T x, T y) -> T {
-        return x * y;
-    });
+    autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, func::multiply<T>);
 }
 }  // namespace reference
 }  // namespace ov
