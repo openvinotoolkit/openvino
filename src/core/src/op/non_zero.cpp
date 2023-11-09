738ab3cabdf67e5d1076d40dc252402aa41b40eb
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2023-11-09 12:21:36 +0100

    Drop ngraph remains

--- src/core/src/op/non_zero.cpp
+++ src/core/src/op/non_zero.cpp
@@ -2,56 +2,55 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "ngraph/op/non_zero.hpp"
+#include "openvino/op/non_zero.hpp"
 
-#include <ngraph/validation_util.hpp>
 #include <numeric>
 
 #include "itt.hpp"
-#include "ngraph/op/op.hpp"
-#include "ngraph/runtime/host_tensor.hpp"
-#include "ngraph/type/element_type_traits.hpp"
+#include "ngraph/runtime/host_tensor.hpp"  // tbr
+#include "ngraph/validation_util.hpp"      // tbr
+#include "openvino/core/type/element_type_traits.hpp"
 #include "openvino/reference/non_zero.hpp"
 
 using namespace ngraph;
-using namespace std;
 
-op::v3::NonZero::NonZero(const Output<Node>& arg) : Op({arg}) {
+namespace ov {
+namespace op {
+namespace v3 {
+NonZero::NonZero(const Output<Node>& arg) : Op({arg}) {
     constructor_validate_and_infer_types();
 }
 
-op::v3::NonZero::NonZero(const Output<Node>& arg, const std::string& output_type)
+NonZero::NonZero(const Output<Node>& arg, const std::string& output_type)
     : Op({arg}),
       m_output_type(EnumNames<element::Type_t>::as_enum(output_type)) {
     constructor_validate_and_infer_types();
 }
 
-op::v3::NonZero::NonZero(const Output<Node>& arg, const element::Type& output_type)
-    : Op({arg}),
-      m_output_type(output_type) {
+NonZero::NonZero(const Output<Node>& arg, const element::Type& output_type) : Op({arg}), m_output_type(output_type) {
     constructor_validate_and_infer_types();
 }
 
-bool ngraph::op::v3::NonZero::visit_attributes(AttributeVisitor& visitor) {
+bool NonZero::visit_attributes(AttributeVisitor& visitor) {
     OV_OP_SCOPE(v3_NonZero_visit_attributes);
     visitor.on_attribute("output_type", m_output_type);
     return true;
 }
 
-void op::v3::NonZero::validate_and_infer_types() {
+void NonZero::validate_and_infer_types() {
     OV_OP_SCOPE(v3_NonZero_validate_and_infer_types);
 
     NODE_VALIDATION_CHECK(this,
                           m_output_type == element::i64 || m_output_type == element::i32,
                           "Output type must be i32 or i64");
     // For scalar non-zero value case, onnx test case expects output shape {1, 1}
-    const ov::PartialShape& input_shape = get_input_partial_shape(0);
+    const PartialShape& input_shape = get_input_partial_shape(0);
     if (input_shape.rank().compatible(0)) {
-        set_output_type(0, m_output_type, ov::PartialShape{Dimension::dynamic(), Dimension::dynamic()});
+        set_output_type(0, m_output_type, PartialShape{Dimension::dynamic(), Dimension::dynamic()});
     } else {
         const Dimension dim =
-            std::accumulate(begin(input_shape), end(input_shape), Dimension(0, 1), std::multiplies<Dimension>());
-        set_output_type(0, m_output_type, ov::PartialShape{input_shape.rank(), dim});
+            std::accumulate(std::begin(input_shape), std::end(input_shape), Dimension(0, 1), std::multiplies<Dimension>());
+        set_output_type(0, m_output_type, PartialShape{input_shape.rank(), dim});
     }
 
     set_input_is_relevant_to_shape(0);
@@ -74,10 +73,10 @@ void op::v3::NonZero::validate_and_infer_types() {
     OPENVINO_SUPPRESS_DEPRECATED_END
 }
 
-shared_ptr<Node> op::v3::NonZero::clone_with_new_inputs(const OutputVector& new_args) const {
+std::shared_ptr<Node> NonZero::clone_with_new_inputs(const OutputVector& new_args) const {
     OV_OP_SCOPE(v3_NonZero_clone_with_new_inputs);
     check_new_args_count(this, new_args);
-    return make_shared<v3::NonZero>(new_args.at(0), m_output_type);
+    return std::make_shared<v3::NonZero>(new_args.at(0), m_output_type);
 }
 
 OPENVINO_SUPPRESS_DEPRECATED_START
@@ -88,20 +87,20 @@ bool evaluate_nonzero_execute(const HostTensorPtr& input, const HostTensorPtr& o
     using IN_T = typename element_type_traits<INPUT_ET>::value_type;
     using OUT_T = typename element_type_traits<OUT_ET>::value_type;
 
-    ov::Shape input_shape = input->get_shape();
+    Shape input_shape = input->get_shape();
     size_t input_rank = input_shape.size();
 
-    size_t non_zero_count = ov::reference::non_zero_get_count<IN_T>(input->get_data_ptr<INPUT_ET>(), input_shape);
+    size_t non_zero_count = reference::non_zero_get_count<IN_T>(input->get_data_ptr<INPUT_ET>(), input_shape);
 
-    ov::Shape out_shape;
+    Shape out_shape;
     if (input_rank == 0 && non_zero_count > 0) {
-        out_shape = ov::Shape{1, 1};
+        out_shape = Shape{1, 1};
     } else {
-        out_shape = ov::Shape{input_rank, non_zero_count};
+        out_shape = Shape{input_rank, non_zero_count};
     }
 
     output->set_shape(out_shape);
-    ov::reference::non_zero<IN_T, OUT_T>(input->get_data_ptr<INPUT_ET>(), output->get_data_ptr<OUT_ET>(), input_shape);
+    reference::non_zero<IN_T, OUT_T>(input->get_data_ptr<INPUT_ET>(), output->get_data_ptr<OUT_ET>(), input_shape);
 
     return true;
 }
@@ -152,29 +151,31 @@ bool evaluate_nonzero(const HostTensorPtr& input, const HostTensorPtr& output) {
 }  // namespace
 }  // namespace nonzero
 
-bool op::v3::NonZero::evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const {
+bool NonZero::evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) const {
     OV_OP_SCOPE(v3_NonZero_evaluate);
     return nonzero::evaluate_nonzero(inputs[0], outputs[0]);
 }
 
-bool op::v3::NonZero::has_evaluate() const {
+bool NonZero::has_evaluate() const {
     OV_OP_SCOPE(v3_NonZero_has_evaluate);
     switch (get_input_element_type(0)) {
-    case ngraph::element::i8:
-    case ngraph::element::i16:
-    case ngraph::element::i32:
-    case ngraph::element::i64:
-    case ngraph::element::u8:
-    case ngraph::element::u16:
-    case ngraph::element::u32:
-    case ngraph::element::u64:
-    case ngraph::element::bf16:
-    case ngraph::element::f16:
-    case ngraph::element::f32:
-    case ngraph::element::f64:
+    case element::i8:
+    case element::i16:
+    case element::i32:
+    case element::i64:
+    case element::u8:
+    case element::u16:
+    case element::u32:
+    case element::u64:
+    case element::bf16:
+    case element::f16:
+    case element::f32:
+    case element::f64:
         return true;
     default:
-        break;
+        return false;
     }
-    return false;
 }
+}  // namespace v3
+}  // namespace op
+}  // namespace ov
