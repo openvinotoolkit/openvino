6a3c0dc80d611a2607675fc5b0ee711d5e2995ad
Alina Kladieva <alina.kladieva@intel.com>
2023-11-09 08:38:58 +0100

    Smart CI POC (#19825)
    * Try using a custom action directly from repo
    
    * Run smart CI under ubuntu-latest
    
    * Set output + add a sample step
    
    * Update linux.yml
    
    * Add components.yml
    
    * Add some conditions
    
    * Just to check if reference to "needs" work in job context
    
    * Update linux.yml
    
    * More example cases
    
    * Dummy change to CPU
    
    * Fix typo
    
    * Fix SAMPLES_AFFECTED variable
    
    * Use more correct dependents key
    
    * Fighting with messy GHA conditions
    
    * No brackets and no double quotes in conditions
    
    * Revert "Dummy change to CPU"
    
    This reverts commit 4eae09e5b51816a63f77c868276e8edc4869f816.
    
    * Use refactored action
    
    * Move action implementation to openvino repo
    
    * Extend components.yml config
    
    * Update labeler.yml
    
    * Dummy change to TF FE
    
    * Fix indentation
    
    * Add missing needs
    
    * Add missing records
    
    * Allow missing records for components in validation
    
    * install_openvino_dependencies as a separate step for Python_Unit_Tests
    
    * Improve config validation
    
    * Revert "Dummy change to TF FE"
    
    This reverts commit 01190864d1001c21ccc6d228ba445f4d94d8fc62.
    
    * Dummy change to model hub tests
    
    * Update CPU component config
    
    * Dummy change to Python API
    
    * Dummy change to Python API
    
    * Revert "Dummy change to Python API"
    
    This reverts commit 3fce0bb3fbdbdd0fcb70d65bf6cae0d06be1a881.
    
    * Dummy change to Python API
    
    * Simplify conditions. Cover "no components changed" case
    
    * Update components.yml
    
    * Update .gitignore
    
    * Revert "Dummy change to Python API"
    
    This reverts commit e57ea9852c6bcfd6091ff1db3ae7c8e285a9b9f4.
    
    * Fix dependencies scopes
    
    * Add simple unit tests for smart ci functionality
    
    * Revert "Dummy change to model hub tests"
    
    This reverts commit c3d6837e224117eb70083297aabd6a978aa1632d.
    
    * Use ghapi module with permissive license
    
    * Cover install_build_dependencies.sh script by labeler
    
    * More labels
    
    * Use ghapi. Apply review comments
    
    * Enable dot files to be matched by labeler
    
    * Warning instead of error in artifacts upload where smart ci is enabled
    
    * Fix master merge
    
    * Fix condition for TF FE common tests
    
    * Fix condition for Pytorch FE tests
    
    * Remove condition for pytorch model tests
    
    * Allow any label as a component
    
    * Refactor tests log handling
    
    * Allow any defined label as a component
    
    * Rearrange config structure. Fill the config with actual data
    
    * Run full scope on changes to non-matching files
    
    * Add missing conditions
    
    ---------
    
    Co-authored-by: Ilya Lavrenov <ilya.lavrenov@intel.com>

---
+++ .github/actions/smart-ci/smart_ci_test.py
@@ -0,0 +1,198 @@
+import logging
+import sys
+import unittest
+from smart_ci import ComponentConfig
+
+log = logging.getLogger()
+log.level = logging.DEBUG
+
+
+def log_handler(func):
+    def wrapper(*args, **kwargs):
+        stream_handler = logging.StreamHandler(sys.stdout)
+        log.addHandler(stream_handler)
+        result = func(*args, **kwargs)
+        log.removeHandler(stream_handler)
+        return result
+    return wrapper
+
+
+class TestComponentConfig(unittest.TestCase):
+    def setUp(self):
+        self.all_possible_components = {'comp1', 'comp2', 'comp3', 'comp4'}
+        ComponentConfig.ScopeKeys = {'build', 'revalidate', '_scope_1', '_scope_2', '_scope_3'}
+
+    @log_handler
+    def validate(self, config_data: dict, changed_components: set, expected_result: dict):
+        log.info(f"{self._testMethodName}:")
+        config = ComponentConfig(config_data, {}, self.all_possible_components)
+        result = config.get_affected_components(changed_components)
+        self.assertEqual(expected_result, result)
+
+    def test_no_changed_components(self):
+        config_data = {
+            'comp1': {'build': {}, 'revalidate': {}},
+            'comp2': {'build': {}, 'revalidate': {}},
+            'comp3': {'build': {}, 'revalidate': {}},
+            'comp4': {'build': {}, 'revalidate': {}},
+        }
+        changed_components = set()
+        expected_result = {
+            'comp1': ComponentConfig.FullScope,
+            'comp2': ComponentConfig.FullScope,
+            'comp3': ComponentConfig.FullScope,
+            'comp4': ComponentConfig.FullScope,
+        }
+        self.validate(config_data, changed_components, expected_result)
+
+    def test_all_components_changed(self):
+        config_data = {
+            'comp1': {'build': {}, 'revalidate': {}},
+            'comp2': {'build': {}, 'revalidate': {}},
+            'comp3': {'build': {}, 'revalidate': {}},
+            'comp4': {'build': {}, 'revalidate': {}},
+        }
+        changed_components = {'comp1', 'comp2', 'comp3', 'comp4'}
+        expected_result = {
+            'comp1': ComponentConfig.FullScope,
+            'comp2': ComponentConfig.FullScope,
+            'comp3': ComponentConfig.FullScope,
+            'comp4': ComponentConfig.FullScope,
+        }
+        self.validate(config_data, changed_components, expected_result)
+
+    def test_changed_component_not_defined(self):
+        config_data = {
+            'comp2': {'build': {}, 'revalidate': {}},
+            'comp3': {'build': {}, 'revalidate': {}},
+            'comp4': {'build': {}, 'revalidate': {}},
+        }
+        changed_components = {'comp1'}
+        expected_result = {
+            'comp1': ComponentConfig.FullScope,
+            'comp2': ComponentConfig.FullScope,
+            'comp3': ComponentConfig.FullScope,
+            'comp4': ComponentConfig.FullScope,
+        }
+        self.validate(config_data, changed_components, expected_result)
+
+    def test_component_changed_no_scope_keys(self):
+        config_data = {
+            'comp1': {},
+            'comp2': {},
+            'comp3': {},
+            'comp4': {},
+        }
+        changed_components = {'comp1'}
+        expected_result = {
+            'comp1': ComponentConfig.FullScope,
+            'comp2': ComponentConfig.FullScope,
+            'comp3': ComponentConfig.FullScope,
+            'comp4': ComponentConfig.FullScope,
+        }
+        self.validate(config_data, changed_components, expected_result)
+
+    def test_one_component_changed_dependents_empty(self):
+        config_data = {
+            'comp1': {'build': {}, 'revalidate': {}},
+            'comp2': {'build': {}, 'revalidate': {}},
+            'comp3': {'build': {}, 'revalidate': {}},
+            'comp4': {'build': {}, 'revalidate': {}},
+        }
+        changed_components = {'comp1'}
+        expected_result = {
+            'comp1': ComponentConfig.FullScope,
+        }
+        self.validate(config_data, changed_components, expected_result)
+
+    def test_not_changed_dependent_component(self):
+        config_data = {
+            'comp1': {'build': {'comp2'}, 'revalidate': {}},
+            'comp2': {'build': {}, 'revalidate': {}},
+            'comp3': {'build': {}, 'revalidate': {}},
+            'comp4': {'build': {}, 'revalidate': {}},
+        }
+        changed_components = {'comp1'}
+        expected_result = {
+            'comp1': ComponentConfig.FullScope,
+            'comp2': {'build'}
+        }
+        self.validate(config_data, changed_components, expected_result)
+
+    def test_changed_dependent_component(self):
+        config_data = {
+            'comp1': {'build': {'comp2'}, 'revalidate': {}},
+            'comp2': {'build': {}, 'revalidate': {}},
+            'comp3': {'build': {}, 'revalidate': {}},
+            'comp4': {'build': {}, 'revalidate': {}},
+        }
+        changed_components = {'comp1', 'comp2'}
+        expected_result = {
+            'comp1': ComponentConfig.FullScope,
+            'comp2': ComponentConfig.FullScope
+        }
+        self.validate(config_data, changed_components, expected_result)
+
+    def test_dependent_component_multiple_parents(self):
+        config_data = {
+            'comp1': {'_scope_1': {'comp2'}, 'revalidate': {}},
+            'comp2': {'build': {}, 'revalidate': {}},
+            'comp3': {'build': {}, '_scope_2': {'comp2'}, '_scope_3': {'comp2'}},
+            'comp4': {'build': {}, 'revalidate': {}},
+        }
+        changed_components = {'comp1', 'comp3'}
+        expected_result = {
+            'comp1': ComponentConfig.FullScope,
+            'comp2': {'_scope_1', '_scope_2', '_scope_3'},
+            'comp3': ComponentConfig.FullScope
+        }
+        self.validate(config_data, changed_components, expected_result)
+
+    def test_dependent_component_empty_scopes(self):
+        config_data = {
+            'comp1': {'build': {}, 'revalidate': {'comp2'}},
+            'comp2': {},
+            'comp3': {},
+            'comp4': {},
+        }
+        changed_components = {'comp1', 'comp3'}
+        expected_result = {
+            'comp1': ComponentConfig.FullScope,
+            'comp2': ComponentConfig.FullScope,
+            'comp3': ComponentConfig.FullScope,
+            'comp4': ComponentConfig.FullScope
+        }
+        self.validate(config_data, changed_components, expected_result)
+
+    def test_changed_component_empty_dependencies(self):
+        config_data = {
+            'comp1': {'build': {}, 'revalidate': {}},
+            'comp2': {'build': {}, 'revalidate': {}},
+            'comp3': {'build': {}, 'revalidate': {}},
+            'comp4': {'build': {}, 'revalidate': {}},
+        }
+        changed_components = {'comp1'}
+        expected_result = {
+            'comp1': ComponentConfig.FullScope,
+        }
+        self.validate(config_data, changed_components, expected_result)
+
+    def test_multiple_dependents(self):
+        config_data = {
+            'comp1': {'build': {'comp2'}, 'revalidate': {'comp3'}},
+            'comp2': {'build': {}, 'revalidate': {}},
+            'comp3': {'build': {'comp4'}, 'revalidate': {}},
+            'comp4': {'build': {}, 'revalidate': {}},
+        }
+        changed_components = {'comp1'}
+        expected_result = {
+            'comp1': ComponentConfig.FullScope,
+            'comp2': {'build'},
+            'comp3': ComponentConfig.FullScope,
+            # We don't consider dependencies of dependencies affected, so comp4 is not expected here
+        }
+        self.validate(config_data, changed_components, expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
