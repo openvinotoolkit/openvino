8b61e3c6423ad6feb264f31dd65c0f3e83059573
Kazantsev, Roman <roman.kazantsev@intel.com>
2023-11-12 23:08:54 +0400

    Refactor Round, Square, and unary operation translators
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/frontends/tensorflow_common/src/op/round.cpp
+++ src/frontends/tensorflow_common/src/op/round.cpp
@@ -2,11 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include "openvino/op/round.hpp"
+
 #include "common_op_table.hpp"
-#include "openvino/opsets/opset8.hpp"
 
 using namespace std;
-using namespace ov::opset8;
+using namespace ov::op;
 
 namespace ov {
 namespace frontend {
@@ -14,11 +15,13 @@ namespace tensorflow {
 namespace op {
 
 OutputVector translate_round_op(const NodeContext& node) {
+    default_op_checks(node, 1, {"Round", "ROUND"});
+
     auto input = node.get_input(0);
     // using default round mode "half_to_even" in openvino,
     // as TF has only that mode
-    auto round_mode = Round::RoundMode::HALF_TO_EVEN;
-    auto res = make_shared<Round>(input, round_mode);
+    auto round_mode = v5::Round::RoundMode::HALF_TO_EVEN;
+    auto res = make_shared<v5::Round>(input, round_mode);
     set_node_name(node.get_name(), res);
     return res->outputs();
 }
