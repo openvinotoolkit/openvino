617e5573795641dcd2a4d91950fb3e9c744e189a
Krivoruchko <georgy.krivoruchko@intel.com>
2023-11-14 03:23:22 -0800

    Fix for making intermediate dirs

--- src/frontends/tensorflow_lite/tests/test_models/gen_scripts/generate_pose_detector.py
+++ src/frontends/tensorflow_lite/tests/test_models/gen_scripts/generate_pose_detector.py
@@ -15,7 +15,7 @@ tflite_model_path = os.path.join(path_to_model_dir, tflite_file_name)
 task_file_name = 'pose_landmarker_lite.task'
 task_file_path = os.path.join(path_to_model_dir, task_file_name)
 if not os.path.exists(path_to_model_dir):
-    os.mkdir(path_to_model_dir)
+    os.makedirs(path_to_model_dir)
 if not os.path.exists(tflite_model_path):
     if not os.path.exists(task_file_path):
         urllib.request.urlretrieve("https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/latest/pose_landmarker_lite.task", task_file_path)
