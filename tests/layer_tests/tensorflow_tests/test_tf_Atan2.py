457d1955d4afc28f9dcaaa681c83ed1f2d18910a
rghvsh <116428320+rghvsh@users.noreply.github.com>
2023-11-14 22:49:28 +0530

    Create test_tf_Atan2.py
    Adds test for Atan2.

---
+++ tests/layer_tests/tensorflow_tests/test_tf_Atan2.py
@@ -0,0 +1,48 @@
+# Copyright (C) 2018-2023 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+import numpy as np
+import pytest
+import tensorflow as tf
+from common.tf_layer_test_class import CommonTFLayerTest
+
+
+class TestAtan2(CommonTFLayerTest):
+    def _prepare_input(self, inputs_info):
+        assert 'x' in inputs_info
+        assert 'y' in inputs_info
+        x_shape = inputs_info['x']
+        y_shape = inputs_info['y']
+        inputs_data = {}
+        inputs_data['x'] = np.random.rand(x.shape).astype(self.input_type)
+        inputs_data['y'] = np.random.rand(y.shape).astype(self.input_type)
+        return inputs_data
+
+    def create_atan2_net(self, input_shape, input_type):
+        self.input_type = input_type
+        tf.compat.v1.reset_default_graph()
+        # Create the graph and model
+        with tf.compat.v1.Session() as sess:
+            x = tf.compat.v1.placeholder(input_type, input_shape, 'x')
+            y = tf.compat.v1.placeholder(input_type, input_shape, 'y')
+            tf.math.atan2(y=y, x=x)
+            tf.compat.v1.global_variables_initializer()
+            tf_net = sess.graph_def
+
+        return tf_net, None
+
+    test_data_basic = [
+        dict(input_shape=[10, 20], input_type=np.float32),
+        dict(input_shape=[8, 5], input_type=np.float32),
+        dict(input_shape=[5, 3], input_type=np.int32),
+        dict(input_shape=[6, 4], input_type=np.int32),
+    ]
+
+    @pytest.mark.parametrize("params", test_data_basic)
+    @pytest.mark.precommit_tf_fe
+    @pytest.mark.nightly
+    def test_atan2_basic(self, params, ie_device, precision, ir_version, temp_dir,
+                               use_new_frontend, use_old_api):
+        self._test(*self.create_atan2_net(**params),
+                   ie_device, precision, ir_version, temp_dir=temp_dir,
+                   use_new_frontend=use_new_frontend, use_old_api=use_old_api)
