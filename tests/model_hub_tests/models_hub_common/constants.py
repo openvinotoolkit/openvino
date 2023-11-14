369831caef55efe1f2d87889d3997f93c53ab6b8
Maxim Vafin <maxim.vafin@intel.com>
2023-11-14 11:43:06 +0100

    [PT FE] Use system model cache when running torch hub tests locally (#21047)
    * Use system model cache when running torch hub tests locally
    
    * Update tests/model_hub_tests/torch_tests/test_torchvision_models.py
    
    * Update tests/model_hub_tests/torch_tests/test_torchvision_models.py

--- tests/model_hub_tests/models_hub_common/constants.py
+++ tests/model_hub_tests/models_hub_common/constants.py
@@ -9,9 +9,9 @@ tf_hub_cache_dir = os.environ.get('TFHUB_CACHE_DIR',
                                   os.path.join(tempfile.gettempdir(), "tfhub_modules"))
 os.environ['TFHUB_CACHE_DIR'] = tf_hub_cache_dir
 
-hf_hub_cache_dir = os.environ.get('HUGGINGFACE_HUB_CACHE',
-                                  tempfile.gettempdir())
-os.environ['HUGGINGFACE_HUB_CACHE'] = hf_hub_cache_dir
+hf_hub_cache_dir = tempfile.gettempdir()
+if os.environ.get('USE_SYSTEM_CACHE', 'True') == 'False':
+    os.environ['HUGGINGFACE_HUB_CACHE'] = hf_hub_cache_dir
 
 # supported_devices : CPU, GPU, GNA
 test_device = os.environ.get('TEST_DEVICE', 'CPU;GPU').split(';')
