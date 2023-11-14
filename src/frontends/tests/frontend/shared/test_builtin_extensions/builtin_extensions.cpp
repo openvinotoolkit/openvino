6974ab967ec89c9aac7ec2ced0daf22cad0194ed
Raasz, Pawel <pawel.raasz@intel.com>
2023-11-14 06:06:53 +0000

    Remove unused function from build if Paddle FE is disabled to avoid build failures `-Werror=unused-function`

--- src/frontends/tests/frontend/shared/test_builtin_extensions/builtin_extensions.cpp
+++ src/frontends/tests/frontend/shared/test_builtin_extensions/builtin_extensions.cpp
@@ -67,6 +67,7 @@ ov::OutputVector ReluToSwishTranslator(const ov::frontend::NodeContext& node) {
     return {std::make_shared<ov::opset8::Swish>(node.get_input(0))};
 }
 
+#ifdef ENABLE_OV_PADDLE_FRONTEND
 std::map<std::string, ov::OutputVector> ReluToSwishTranslatorPDPD(const ov::frontend::NodeContext& node) {
     return {{"Out", {std::make_shared<ov::opset8::Swish>(node.get_input("X"))}}};
 }
@@ -81,7 +82,7 @@ std::map<std::string, ov::OutputVector> Relu6ToReluTranslatorPaddle(const ov::fr
     ret["Out"] = {relu};
     return ret;
 }
-
+#endif
 }  // namespace
 
 class CustomElu : public ov::op::Op {
