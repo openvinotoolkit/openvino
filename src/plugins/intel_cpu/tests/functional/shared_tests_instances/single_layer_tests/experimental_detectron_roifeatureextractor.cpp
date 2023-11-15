8aa07267f149d29be4df4886253e8fda1f3725a0
Oleg Pipikin <oleg.pipikin@intel.com>
2023-11-15 13:16:37 +0100

    Refactor ExperimentalDetectron tests (#20846)
    * Refactor ExperimentalDetectronDetectionOutputLayerTest
    
    * Refactor ExperimentalDetectronGenerateProposalsSingleImageLayerTest
    
    * Refactor ExperimentalDetectronPriorGridGeneratorLayerTest
    
    * ExperimentalDetectronROIFeatureExtractorLayerTest
    
    * Refactor ExperimentalDetectronTopKROIsLayerTest
    
    * Apply comments
    
    * Aplly comments

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/single_layer_tests/experimental_detectron_roifeatureextractor.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/single_layer_tests/experimental_detectron_roifeatureextractor.cpp
@@ -3,12 +3,9 @@
 //
 
 #include <vector>
-#include "single_layer_tests/experimental_detectron_roifeatureextractor.hpp"
-
-using namespace ov::test;
-using namespace ov::test::subgraph;
-
+#include "single_op_tests/experimental_detectron_roifeatureextractor.hpp"
 namespace {
+using ov::test::ExperimentalDetectronROIFeatureExtractorLayerTest;
 
 const std::vector<int64_t> outputSize = {7, 14};
 const std::vector<int64_t> samplingRatio = {1, 2, 3};
@@ -19,10 +16,10 @@ const std::vector<std::vector<int64_t>> pyramidScales = {
         {2, 4, 8, 16}
 };
 
-const std::vector<std::vector<InputShape>> staticInputShape = {
-        static_shapes_to_test_representation({{1000, 4}, {1, 8, 200, 336}, {1, 8, 100, 168}, {1, 8, 50, 84}, {1, 8, 25, 42}}),
-        static_shapes_to_test_representation({{1000, 4}, {1, 16, 200, 336}, {1, 16, 100, 168}, {1, 16, 50, 84}, {1, 16, 25, 42}}),
-        static_shapes_to_test_representation({{1200, 4}, {1, 8, 200, 42}, {1, 8, 100, 336}, {1, 8, 50, 168}, {1, 8, 25, 84}})
+const std::vector<std::vector<ov::test::InputShape>> staticInputShape = {
+        ov::test::static_shapes_to_test_representation({{1000, 4}, {1, 8, 200, 336}, {1, 8, 100, 168}, {1, 8, 50, 84}, {1, 8, 25, 42}}),
+        ov::test::static_shapes_to_test_representation({{1000, 4}, {1, 16, 200, 336}, {1, 16, 100, 168}, {1, 16, 50, 84}, {1, 16, 25, 42}}),
+        ov::test::static_shapes_to_test_representation({{1200, 4}, {1, 8, 200, 42}, {1, 8, 100, 336}, {1, 8, 50, 168}, {1, 8, 25, 84}})
 };
 
 INSTANTIATE_TEST_SUITE_P(smoke_ExperimentalROI_static, ExperimentalDetectronROIFeatureExtractorLayerTest,
@@ -36,7 +33,7 @@ INSTANTIATE_TEST_SUITE_P(smoke_ExperimentalROI_static, ExperimentalDetectronROIF
                                  ::testing::Values(ov::test::utils::DEVICE_CPU)),
                          ExperimentalDetectronROIFeatureExtractorLayerTest::getTestCaseName);
 
-const std::vector<std::vector<InputShape>> dynamicInputShape = {
+const std::vector<std::vector<ov::test::InputShape>> dynamicInputShape = {
         {
                 {
                         {{-1, 4}, {{1000, 4}, {1500, 4}, {2000, 4}}},
