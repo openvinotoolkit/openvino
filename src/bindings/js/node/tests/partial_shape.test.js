4977d4c268c9bcbf88943ebbc306d58ab01e4221
Vishniakov Nikolai <nikolai.vishniakov@intel.com>
2023-11-15 11:55:28 +0100

    Add tests for PartialShape class

--- src/bindings/js/node/tests/partial_shape.test.js
+++ src/bindings/js/node/tests/partial_shape.test.js
@@ -7,7 +7,7 @@ const assert = require('assert');
 const { describe, it } = require('node:test');
 
 const staticShape = '1, 3, 224, 224';
-const dynamicShape = '1, -1, 1..3, 224';
+const dynamicShape = '?, -1, 1..3, 224';
 
 describe('PartialShape', () => {
   it('Should detect static shape', () => {
@@ -22,11 +22,27 @@ describe('PartialShape', () => {
     assert.strictEqual(partialShape.isStatic(), false);
   });
 
-  it('Should return shape as string', () => {
-    const partialShape1 = new ov.PartialShape(staticShape);
-    const partialShape2 = new ov.PartialShape(dynamicShape);
+  it('Should return shape as string for static shape', () => {
+    const partialShape = new ov.PartialShape(staticShape);
 
-    assert.strictEqual(partialShape1.toString(), `[1,3,224,224]`);
-    assert.strictEqual(partialShape2.toString(), `[1,?,1..3,224]`);
+    assert.strictEqual(partialShape.toString(), '[1,3,224,224]');
+  });
+
+  it('Should return shape as string for dynamic shape', () => {
+    const partialShape = new ov.PartialShape(dynamicShape);
+
+    assert.strictEqual(partialShape.toString(), '[?,?,1..3,224]');
+  });
+
+  it('Should return array with dimensions for dynamic shape', () => {
+    const partialShape = new ov.PartialShape(staticShape);
+
+    assert.deepStrictEqual(partialShape.getDimensions(), [1,3,224,224]);
+  });
+
+  it('Should return array with dimensions for dynamic shape', () => {
+    const partialShape = new ov.PartialShape(dynamicShape);
+
+    assert.deepStrictEqual(partialShape.getDimensions(), [-1,-1,[1,3],224]);
   });
 });
