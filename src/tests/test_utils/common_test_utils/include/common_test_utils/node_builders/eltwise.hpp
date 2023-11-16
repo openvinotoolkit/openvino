ea68222b4b59fdf2fa37029a3ecd04f3ddc0ea8f
Oleg Pipikin <oleg.pipikin@intel.com>
2023-11-16 18:35:16 +0400

    Refactor make_eltwise builder

---
+++ src/tests/test_utils/common_test_utils/include/common_test_utils/node_builders/eltwise.hpp
@@ -0,0 +1,16 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "openvino/core/node.hpp"
+#include "common_test_utils/test_enums.hpp"
+
+namespace ov {
+namespace test {
+namespace utils {
+std::shared_ptr<ov::Node> makeEltwise(const ov::Output<Node>& in0,
+                                      const ov::Output<Node>& in1,
+                                      ov::test::utils::EltwiseTypes eltwise_type);
+} // namespace utils
+} // namespace test
+} // namespace ov
