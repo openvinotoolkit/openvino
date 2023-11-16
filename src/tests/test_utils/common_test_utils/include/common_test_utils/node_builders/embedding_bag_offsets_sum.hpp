1699f8ba63f859447140c351cb212e0ea10a706b
Oleg Pipikin <oleg.pipikin@intel.com>
2023-11-16 18:51:26 +0400

    Refactor make_embedding_bag_offsets_sum

---
+++ src/tests/test_utils/common_test_utils/include/common_test_utils/node_builders/embedding_bag_offsets_sum.hpp
@@ -0,0 +1,21 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "openvino/core/node.hpp"
+#include "common_test_utils/test_enums.hpp"
+
+namespace ov {
+namespace test {
+namespace utils {
+std::shared_ptr<ov::Node> make_embedding_bag_offsets_sum(const element::Type& data_type,
+                                                         const ov::element::Type& indices_type,
+                                                         const ov::Output<Node>& emb_table_node,
+                                                         const std::vector<size_t>& indices,
+                                                         const std::vector<size_t>& offsets,
+                                                         size_t default_index,
+                                                         bool with_weights,
+                                                         bool with_default_index);
+} // namespace utils
+} // namespace test
+} // namespace ov
