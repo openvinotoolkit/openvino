331446d4ee608ee8f67b12c26d6c413baab55481
Piotr Krzemiński <piotr.krzeminski@intel.com>
2023-11-16 09:27:24 +0100

    Update src/plugins/intel_cpu/src/shape_inference/custom/multinomial.hpp
    Co-authored-by: Mateusz Mikolajczyk <mateusz.mikolajczyk@intel.com>

--- src/plugins/intel_cpu/src/shape_inference/custom/multinomial.hpp
+++ src/plugins/intel_cpu/src/shape_inference/custom/multinomial.hpp
@@ -26,7 +26,7 @@ public:
 
 class MultinomialShapeInferFactory : public ShapeInferFactory {
 public:
-    explicit MultinomialShapeInferFactory(const std::shared_ptr<ov::Node>& op) : m_op(op){};
+    explicit MultinomialShapeInferFactory(const std::shared_ptr<ov::Node>& op) : m_op(op) {}
     ShapeInferPtr makeShapeInfer() const override;
 
 private:
