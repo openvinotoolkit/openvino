988a3692cf44baf25d31512f606e603d267564bd
Alexander Suslov <alexander.suslov@intel.com>
2023-11-16 16:34:33 +0400

    DOCS Update optimization docs with ov.save_model() instead of ov.serialize() (#20711)
    * introduced ov.save_model(...) to the ptq code examples
    
    * replied to comments
    
    * fixed rendering
    
    ---------
    
    Co-authored-by: Karol Blaszczak <karol.blaszczak@intel.com>

--- docs/optimization_guide/nncf/ptq/code/ptq_torch.py
+++ docs/optimization_guide/nncf/ptq/code/ptq_torch.py
@@ -40,5 +40,5 @@ model_int8 = ov.compile_model(ov_quantized_model)
 res = model_int8(input_fp32)
 
 # save the model
-ov.serialize(ov_quantized_model, "quantized_model.xml")
+ov.save_model(ov_quantized_model, "quantized_model.xml")
 #! [inference]
