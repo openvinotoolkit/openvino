121a22351895ae58920c26e254606afcf0e1b46c
rghvsh <116428320+rghvsh@users.noreply.github.com>
2023-11-17 18:01:36 +0530

    Update atan2.cpp

--- src/frontends/tensorflow_common/src/op/atan2.cpp
+++ src/frontends/tensorflow_common/src/op/atan2.cpp
@@ -23,50 +23,46 @@ namespace ov {
 namespace frontend {
 namespace tensorflow {
 namespace op {
-
-OutputVector translate_div_no_nan_op(const NodeContext& node) {
+OutputVector translate_Atan2_op(const NodeContext& node) {
     default_op_checks(node, 2, {"Atan2"});
     auto y = node.get_input(0);
     auto x = node.get_input(1);
-    
-    // handle the first condition : x>0 
+
+    // handle the first condition : x>0
     auto div_y_x = make_shared<Divide>(y, x);
     auto atan = make_shared<Atan>(div_y_x);
     auto const_zero = create_same_type_const_scalar<int32_t>(x, 0);
     auto is_x_positive = make_shared<Greater>(x, const_zero);
-    auto result = make_shared<Select>(is_x_positive, atan, result);;
+    auto result = make_shared<Select>(is_x_positive, atan, result);
 
-    // handle the second condition : x<0 && y>=0 
-    auto const_pi = create_same_type_const_scalar<double>(x, std::atan(1.0)*4);
-    auto x_negative = make_shared<Less>(x, const_zero);
+    // handle the second condition : x<0 && y>=0
+    auto const_pi = create_same_type_const_scalar<double>(x, std::atan(1.0) * 4);
+    auto is_x_negative = make_shared<Less>(x, const_zero);
     auto y_non_negative = make_shared<GreaterEqual>(y, const_zero);
-    auto cond1 = make_shared<LogicalAnd>(x_negative, y_non_negative);
-    auto arctan_y_x_plus_pi = make_shared<Add>(arctan_y_x, const_pi)
-    auto result = make_shared<Select>(cond1, arctan_y_x_plus_pi, result);
+    auto cond1 = make_shared<LogicalAnd>(is_x_negative, y_non_negative);
+    auto atan_y_x_plus_pi = make_shared<Add>(atan, const_pi);
+    auto result = make_shared<Select>(cond1, atan_y_x_plus_pi, result);
 
-    // handle the third consition : x<0 && y<0 
-    auto y_negative = make_shared<Less>(y, const_zero);
-    auto cond2 = make_shared<LogicalAnd>(x_negative, y_negative);
-    auto arctan_y_x_minus_pi = make_shared<Subtract>(arctan_y_x, const_pi);
-    auto result = make_shared<Select>(cond2, arctan_y_x_minus_pi, result);
+    // handle the third consition : x<0 && y<0
+    auto is_y_negative = make_shared<Less>(y, const_zero);
+    auto cond2 = make_shared<LogicalAnd>(is_x_negative, is_y_negative);
+    auto atan_y_x_minus_pi = make_shared<Subtract>(atan, const_pi);
+    auto result = make_shared<Select>(cond2, atan_y_x_minus_pi, result);
 
     // handle the fourth condition : x=0 && y>0
     auto is_x_zero = make_shared<Equal>(x, const_zero);
-    auto is_y_negative = make_shared<Less>(y, const_zero);
-    auto cond3 = make_shared<LogicalAnd>(is_x_zero, is_y_negative);
+    auto is_y_positive = make_shared<Greater>(y, const_zero);
+    auto cond3 = make_shared<LogicalAnd>(is_x_zero, is_y_positive);
     auto const_two = create_same_type_const_scalar<int32_t>(x, 2);
     auto pi_div_two = make_shared<Divide>(const_pi, const_two);
     auto result = make_shared<Select>(cond3, pi_div_two, result);
 
     // handle the fifth condition : x=0 && y<0
-    auto is_x_zero = make_shared<Equal>(x, const_zero);
-    auto is_y_negative = make_shared<Less>(y, const_zero);
     auto cond4 = make_shared<LogicalAnd>(is_x_zero, is_y_negative);
     auto const_minus_two = create_same_type_const_scalar<int32_t>(x, -2);
     auto pi_div_minus_two = make_shared<Divide>(const_pi, const_minus_two);
     auto result = make_shared<Select>(cond4, pi_div_two, result);
 
-       
     set_node_name(node.get_name(), result);
     return result->outputs();
 }
@@ -75,3 +71,5 @@ OutputVector translate_div_no_nan_op(const NodeContext& node) {
 }  // namespace tensorflow
 }  // namespace frontend
 }  // namespace ov
+
+                
