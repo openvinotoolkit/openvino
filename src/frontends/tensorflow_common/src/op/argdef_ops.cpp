8afe77dbb6eef2bb8561bdd83825bb31a0b78997
Roman Kazantsev <roman.kazantsev@intel.com>
2023-11-17 14:26:28 +0400

    [TF FE] Speed up compilation - part 1 (#21144)
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/frontends/tensorflow_common/src/op/argdef_ops.cpp
+++ src/frontends/tensorflow_common/src/op/argdef_ops.cpp
@@ -3,10 +3,12 @@
 //
 
 #include "common_op_table.hpp"
-#include "openvino/opsets/opset10.hpp"
+#include "openvino/op/parameter.hpp"
+#include "openvino/op/result.hpp"
 
 using namespace std;
-using namespace ov::opset10;
+using namespace ov;
+using namespace ov::op;
 
 namespace ov {
 namespace frontend {
@@ -14,16 +16,16 @@ namespace tensorflow {
 namespace op {
 OutputVector translate_input_arg_op(const NodeContext& node) {
     default_op_checks(node, 0, {"input_arg"});
-    auto param_type = node.get_attribute<ov::element::Type>("type");
+    auto param_type = node.get_attribute<element::Type>("type");
 
-    auto param = std::make_shared<Parameter>(param_type, ov::PartialShape::dynamic());
+    auto param = std::make_shared<v0::Parameter>(param_type, PartialShape::dynamic());
     set_node_name(node.get_name(), param);
     return param->outputs();
 }
 
 OutputVector translate_output_arg_op(const NodeContext& node) {
     default_op_checks(node, 1, {"output_arg"});
-    auto result = std::make_shared<Result>(node.get_input(0));
+    auto result = std::make_shared<v0::Result>(node.get_input(0));
     set_node_name(node.get_name(), result);
     return result->outputs();
 }
