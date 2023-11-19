5867d9912326e9f6e706b2ff494b7aa3119c1371
Oleg Pipikin <oleg.pipikin@intel.com>
2023-11-19 22:11:06 +0400

    Refactor make_fully_connected

---
+++ src/tests/test_utils/common_test_utils/include/common_test_utils/node_builders/fully_connected.hpp
@@ -0,0 +1,20 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "openvino/core/node.hpp"
+#include "common_test_utils/test_enums.hpp"
+
+namespace ov {
+namespace test {
+namespace utils {
+std::shared_ptr<ov::Node> make_fully_connected(const ov::Output<Node>& in,
+                                               const ov::element::Type& type,
+                                               const size_t output_size,
+                                               bool  addBias = true,
+                                               const ov::Shape& weights_shape = {},
+                                               const std::vector<float>& weights = {},
+                                               const std::vector<float>& bias_weights = {});
+}  // namespace utils
+}  // namespace test
+}  // namespace ov
