0727c417107392bfd80669eb6224a95731b25a9d
Oleg Pipikin <oleg.pipikin@intel.com>
2023-11-20 20:12:51 +0400

    Refactor ConvReshapeAct

--- src/plugins/intel_gna/tests/functional/shared_tests_instances/subgraph_tests/reshape_permute_conv_permute_reshape_act.cpp
+++ src/plugins/intel_gna/tests/functional/shared_tests_instances/subgraph_tests/reshape_permute_conv_permute_reshape_act.cpp
@@ -8,6 +8,9 @@
 
 #include "common_test_utils/test_constants.hpp"
 
+namespace {
+using ov::test::ConvReshapeAct;
+
 std::vector<std::array<size_t, 4>> input_shapes{
     {1, 1, 166, 2},
     {1, 1, 144, 2},
@@ -26,23 +29,22 @@ std::vector<size_t> output_channels{
     4,
 };
 
-std::vector<InferenceEngine::Precision> netPrecisions = {
-    InferenceEngine::Precision::FP32,
-    InferenceEngine::Precision::FP16,
+std::vector<ov::element::Type> model_types = {
+    ov::element::f32,
+    ov::element::f16,
 };
 
 std::vector<std::map<std::string, std::string>> configs = {
     {{"GNA_DEVICE_MODE", "GNA_SW_EXACT"}, {"GNA_SCALE_FACTOR_0", "2340"}},
     {{"GNA_DEVICE_MODE", "GNA_SW_FP32"}}};
 
-namespace SubgraphTestsDefinitions {
 INSTANTIATE_TEST_SUITE_P(smoke_basic,
                          ConvReshapeAct,
-                         ::testing::Combine(::testing::ValuesIn(netPrecisions),
+                         ::testing::Combine(::testing::ValuesIn(model_types),
                                             ::testing::Values(ov::test::utils::DEVICE_GNA),
                                             ::testing::ValuesIn(input_shapes),
                                             ::testing::ValuesIn(kernel_shapes),
                                             ::testing::ValuesIn(output_channels),
                                             ::testing::ValuesIn(configs)),
                          ConvReshapeAct::getTestCaseName);
-}  // namespace SubgraphTestsDefinitions
+}  // namespace
