662f82b0e3b51b9b332b5202db02d3f772bf5685
Nam, David <david.nam@intel.com>
2023-11-21 11:58:50 +0900

    [GPU] Fix constant dimension in case of node from if-op internal body

--- src/plugins/intel_gpu/src/plugin/ops/constant.cpp
+++ src/plugins/intel_gpu/src/plugin/ops/constant.cpp
@@ -219,6 +219,8 @@ static void CreateConstantOp(ProgramBuilder& p, const std::shared_ptr<ov::op::v0
             // To pass check_memory_to_set in input_layout::set_data for this case, Set constDims to [N, 1, 1, 1]
             // when constDims is one dim and user op is Loop or TensorIterator.
             consts[op].needsBatchInterpretation = constDims.size() == 1;
+        } else if (ov::is_type<ov::op::v0::Result>(outOp)) {
+            consts[op].needsBatchInterpretation = constDims.size() == 1;
         }
     }
 
