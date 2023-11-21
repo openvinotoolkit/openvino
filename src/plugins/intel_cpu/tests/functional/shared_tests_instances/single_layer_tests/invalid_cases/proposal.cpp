6c08fc0dd58e2bcf7e75447cb910e077dcd2ffd3
Oleg Pipikin <oleg.pipikin@intel.com>
2023-11-21 08:28:49 +0100

    Refactor negative ProposalBehTest (#21190)
    Co-authored-by: Ilya Lavrenov <ilya.lavrenov@intel.com>

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/single_layer_tests/invalid_cases/proposal.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/single_layer_tests/invalid_cases/proposal.cpp
@@ -2,25 +2,23 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include <vector>
 #include "single_layer_tests/invalid_cases/proposal.hpp"
 
-using namespace ngraph::helpers;
-using namespace LayerTestsDefinitions;
-using namespace BehaviorTestsDefinitions;
+#include <vector>
 
 namespace {
+using ov::test::ProposalBehTest;
 
 /* ============= Proposal ============= */
-const std::vector<base_size_type> base_size_ = {16};
-const std::vector<pre_nms_topn_type> pre_nms_topn_ = {100};
-const std::vector<post_nms_topn_type> post_nms_topn_ = {100};
-const std::vector<nms_thresh_type> nms_thresh_ = {0.7f};
-const std::vector<min_size_type> min_size_ = {1};
-const std::vector<ratio_type> ratio_ = {{1.0f, 2.0f}};
-const std::vector<scale_type> scale_ = {{1.2f, 1.5f}};
-const std::vector<clip_before_nms_type> clip_before_nms_ = {false};
-const std::vector<clip_after_nms_type> clip_after_nms_ = {false};
+const std::vector<size_t> base_size_ = {16};
+const std::vector<size_t> pre_nms_topn_ = {100};
+const std::vector<size_t> post_nms_topn_ = {100};
+const std::vector<float> nms_thresh_ = {0.7f};
+const std::vector<size_t> min_size_ = {1};
+const std::vector<std::vector<float>> ratio_ = {{1.0f, 2.0f}};
+const std::vector<std::vector<float>> scale_ = {{1.2f, 1.5f}};
+const std::vector<bool> clip_before_nms_ = {false};
+const std::vector<bool> clip_after_nms_ = {false};
 const std::vector<std::vector<float>> img_info_invalid = {{0.f, 225.f, 1.f},
                                                           {225.f, -1.f, 1.f},
                                                           {225.f, NAN, 1.f},
@@ -29,27 +27,24 @@ const std::vector<std::vector<float>> img_info_invalid = {{0.f, 225.f, 1.f},
                                                           {225.f, 100.f, INFINITY}};
 
 // empty string corresponds to Caffe framework
-const std::vector<framework_type> framework_ = {""};
+const std::vector<std::string> framework_ = {""};
 
-const auto proposalParams = ::testing::Combine(
-        ::testing::ValuesIn(base_size_),
-        ::testing::ValuesIn(pre_nms_topn_),
-        ::testing::ValuesIn(post_nms_topn_),
-        ::testing::ValuesIn(nms_thresh_),
-        ::testing::ValuesIn(min_size_),
-        ::testing::ValuesIn(ratio_),
-        ::testing::ValuesIn(scale_),
-        ::testing::ValuesIn(clip_before_nms_),
-        ::testing::ValuesIn(clip_after_nms_),
-        ::testing::ValuesIn(framework_)
-);
+const auto proposalParams = ::testing::Combine(::testing::ValuesIn(base_size_),
+                                               ::testing::ValuesIn(pre_nms_topn_),
+                                               ::testing::ValuesIn(post_nms_topn_),
+                                               ::testing::ValuesIn(nms_thresh_),
+                                               ::testing::ValuesIn(min_size_),
+                                               ::testing::ValuesIn(ratio_),
+                                               ::testing::ValuesIn(scale_),
+                                               ::testing::ValuesIn(clip_before_nms_),
+                                               ::testing::ValuesIn(clip_after_nms_),
+                                               ::testing::ValuesIn(framework_));
 
-INSTANTIATE_TEST_SUITE_P(invalid, ProposalBehTest,
-                        ::testing::Combine(
-                                proposalParams,
-                                ::testing::ValuesIn(img_info_invalid),
-                                ::testing::Values(ov::test::utils::DEVICE_CPU)),
-                        ProposalBehTest::getTestCaseName
-);
+INSTANTIATE_TEST_SUITE_P(invalid,
+                         ProposalBehTest,
+                         ::testing::Combine(proposalParams,
+                                            ::testing::ValuesIn(img_info_invalid),
+                                            ::testing::Values(ov::test::utils::DEVICE_CPU)),
+                         ProposalBehTest::getTestCaseName);
 
 }  // namespace
