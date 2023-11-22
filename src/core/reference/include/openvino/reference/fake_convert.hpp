90a2011edd85dd62b3e2264713e89a9ab1cf2e17
mitruska <katarzyna.mitrus@intel.com>
2023-11-22 18:29:41 +0100

    Add doxygen text

--- src/core/reference/include/openvino/reference/fake_convert.hpp
+++ src/core/reference/include/openvino/reference/fake_convert.hpp
@@ -20,8 +20,8 @@ namespace func {
 /**
  * @brief Emulation of conversion fp16 value to f8e5m2 format
  *
- * @param arg       Pointer to the input data.
- * @param out       Pointer to the otuput data.
+ * @param arg_f       Pointer to the input data.
+ * @param out_f       Pointer to the otuput data.
  * @param count     Number of elements in the data input.
  */
 static void emulate_f8e5m2_on_fp16(const float16* const arg_f, float16* out_f, size_t count) {
@@ -72,8 +72,8 @@ static void emulate_f8e5m2_on_fp16(const float16* const arg_f, float16* out_f, s
 /**
  * @brief Emulation of conversion fp16 value to f8e4m3 format
  *
- * @param arg       Pointer to the input data.
- * @param out       Pointer to the otuput data.
+ * @param arg_f       Pointer to the input data.
+ * @param out_f       Pointer to the otuput data.
  * @param count     Number of elements in the data input.
  *
  * Exponent denormal values 0 -7
@@ -156,6 +156,18 @@ static void emulate_f8e4m3_on_fp16(const float16* arg_f, float16* out_f, size_t 
     }
 }
 
+/**
+ * @brief Apply scale and shift for the data input.
+ * The scale_shape and shift_shape should be equal and numpy-broadcastable to the data_shape.
+ *
+ * @param data                   Pointer to the input data.
+ * @param scale                  Pointer to the input scale.
+ * @param shift                  Pointer to the input shift.
+ * @param data_shape             Shape of the input data.
+ * @param scale_shape            Shape of the input scale.
+ * @param shift_shape            Shape of the input shift.
+ * @param invert                 Flag denoting applying scale before (if false) or after conversion (if true).
+ */
 template <typename T>
 void apply_scale_shift(T* out,
                        const T* data,
@@ -231,6 +243,14 @@ void apply_scale_shift(T* out,
 }
 }  // namespace func
 
+/**
+ * @brief Call conversion of fp16 value to the desired destination type
+ *
+ * @param arg                  Pointer to the input data.
+ * @param out                  Pointer to the otuput data.
+ * @param count                Number of elements in the data input.
+ * @param destination_type     Name of the destination type.
+ */
 static void apply_conversion(const float16* data,
                              float16* out,
                              size_t element_count,
@@ -244,6 +264,22 @@ static void apply_conversion(const float16* data,
     }
 }
 
+/**
+ * @brief Reference implementation of the FakeConvert operation specialized for float16 type.
+ * It emulates format specified by "destination_type" on the input type.
+ * FakeConvert performs element-wise quantization of floating-point input values into a set of values corresponding to a
+ * target low-precision floating-point type.
+ *
+ *
+ * @param data                   Pointer to the input data.
+ * @param scale                  Pointer to the input scale.
+ * @param shift                  Pointer to the input shift.
+ * @param out                    Pointer to the output data.
+ * @param data_shape             Shape of the input data.
+ * @param scale_shape            Shape of the input scale.
+ * @param shift_shape            Shape of the input shift.
+ * @param destination_type       Name of the destination type.
+ */
 template <typename T, typename std::enable_if<std::is_same<T, float16>::value, bool>::type = true>
 void fake_convert(const T* data,
                   const T* scale,
@@ -254,25 +290,27 @@ void fake_convert(const T* data,
                   const Shape& shift_shape,
                   const std::string& destination_type) {
     const size_t element_count = shape_size(data_shape);
-    reference::func::apply_scale_shift<float16>(out,
-                                                                data,
-                                                                scale,
-                                                                shift,
-                                                                data_shape,
-                                                                scale_shape,
-                                                                shift_shape,
-                                                                false);
+    reference::func::apply_scale_shift<float16>(out, data, scale, shift, data_shape, scale_shape, shift_shape, false);
     apply_conversion(out, out, element_count, destination_type);
-    reference::func::apply_scale_shift<float16>(out,
-                                                                out,
-                                                                scale,
-                                                                shift,
-                                                                data_shape,
-                                                                scale_shape,
-                                                                shift_shape,
-                                                                true);
+    reference::func::apply_scale_shift<float16>(out, out, scale, shift, data_shape, scale_shape, shift_shape, true);
 }
 
+/**
+ * @brief Reference implementation of the FakeConvert operation for floating-point types, with middle conversion
+ * to float16. It emulates format specified by "destination_type" on the input type. FakeConvert performs
+ * element-wise quantization of floating-point input values into a set of values corresponding to a target low-precision
+ * floating-point type.
+ *
+ *
+ * @param data                   Pointer to the input data.
+ * @param scale                  Pointer to the input scale.
+ * @param shift                  Pointer to the input shift.
+ * @param out                    Pointer to the output data.
+ * @param data_shape             Shape of the input data.
+ * @param scale_shape            Shape of the input scale.
+ * @param shift_shape            Shape of the input shift.
+ * @param destination_type       Name of the destination type.
+ */
 template <typename T, typename std::enable_if<!std::is_same<T, float16>::value, bool>::type = true>
 void fake_convert(const T* data,
                   const T* scale,
@@ -283,14 +321,7 @@ void fake_convert(const T* data,
                   const Shape& shift_shape,
                   const std::string& destination_type) {
     const size_t element_count = shape_size(data_shape);
-    reference::func::apply_scale_shift<T>(out,
-                                                          data,
-                                                          scale,
-                                                          shift,
-                                                          data_shape,
-                                                          scale_shape,
-                                                          shift_shape,
-                                                          false);
+    reference::func::apply_scale_shift<T>(out, data, scale, shift, data_shape, scale_shape, shift_shape, false);
 
     std::vector<ov::float16> tmp_fp16;
     tmp_fp16.reserve(element_count);
@@ -298,16 +329,12 @@ void fake_convert(const T* data,
     apply_conversion(tmp_fp16.data(), tmp_fp16.data(), element_count, destination_type);
     reference::convert(tmp_fp16.data(), out, element_count);
 
-    reference::func::apply_scale_shift<T>(out,
-                                                          out,
-                                                          scale,
-                                                          shift,
-                                                          data_shape,
-                                                          scale_shape,
-                                                          shift_shape,
-                                                          true);
+    reference::func::apply_scale_shift<T>(out, out, scale, shift, data_shape, scale_shape, shift_shape, true);
 }
 
+/**
+ * @brief Reference implementation of the FakeConvert operation, with default `shift` input.
+ */
 template <typename T>
 void fake_convert(const T* data,
                   const T* scale,
