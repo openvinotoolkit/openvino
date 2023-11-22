d44edd71a4c3d281c49adfddd9cdf995e4a8ff5f
Min, Byungil <byungil.min@intel.com>
2023-11-22 20:47:16 +0900

    [GPU] Update space_to_depth to use nGraph shape inference (#21135)
    * [GPU] Update space_to_depth to use nGraph shape inference
    
    + Added calc_output_layouts to use shape_infer
    + Added relevant test-cases for static and dynamic
    + Modified typo of filename
    
    Signed-off-by: Min, Byungil <byungil.min@intel.com>

--- src/plugins/intel_gpu/src/plugin/ops/space_to_depth.cpp
+++ src/plugins/intel_gpu/src/plugin/ops/space_to_depth.cpp
@@ -11,23 +11,13 @@
 
 namespace ov {
 namespace intel_gpu {
-
-static cldnn::space_to_depth::depth_mode GetDepthMode(ov::op::v0::SpaceToDepth::SpaceToDepthMode mode) {
-    switch (mode) {
-        case ov::op::v0::SpaceToDepth::SpaceToDepthMode::BLOCKS_FIRST: return cldnn::space_to_depth::blocks_first;
-        case ov::op::v0::SpaceToDepth::SpaceToDepthMode::DEPTH_FIRST: return cldnn::space_to_depth::depth_first;
-        default: OPENVINO_THROW("[GPU] Unsupported SpaceToDepthMode value: ", static_cast<int>(mode));
-    }
-    return cldnn::space_to_depth::blocks_first;
-}
-
 static void CreateSpaceToDepthOp(ProgramBuilder& p, const std::shared_ptr<ov::op::v0::SpaceToDepth>& op) {
     validate_inputs_count(op, {1});
     auto inputs = p.GetInputInfo(op);
     std::string layerName = layer_type_name_ID(op);
     auto spaceToDepthPrim = cldnn::space_to_depth(layerName,
                                                   inputs[0],
-                                                  GetDepthMode(op->get_mode()),
+                                                  op->get_mode(),
                                                   op->get_block_size());
 
     p.add_primitive(*op, spaceToDepthPrim);
