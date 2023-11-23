400bd39a1834696d253c75d38beb76479772373c
Anastasia Kuporosova <anastasia.kuporosova@intel.com>
2023-11-23 11:44:15 +0000

    [PyOV] Update imports in py api tests

--- src/bindings/python/tests/test_transformations/test_replacement_api.py
+++ src/bindings/python/tests/test_transformations/test_replacement_api.py
@@ -2,35 +2,36 @@
 # Copyright (C) 2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from openvino.runtime import Model, PartialShape, opset8
+from openvino import Model, PartialShape
+from openvino.runtime import opset13 as ops
 from openvino.runtime.utils import replace_node, replace_output_update_name
 
 
 def get_relu_model():
     # Parameter->Relu->Result
-    param = opset8.parameter(PartialShape([1, 3, 22, 22]), name="parameter")
-    relu = opset8.relu(param.output(0))
-    res = opset8.result(relu.output(0), name="result")
+    param = ops.parameter(PartialShape([1, 3, 22, 22]), name="parameter")
+    relu = ops.relu(param.output(0))
+    res = ops.result(relu.output(0), name="result")
     return Model([res], [param], "test")
 
 
 def test_output_replace():
-    param = opset8.parameter(PartialShape([1, 3, 22, 22]), name="parameter")
-    relu = opset8.relu(param.output(0))
-    res = opset8.result(relu.output(0), name="result")
+    param = ops.parameter(PartialShape([1, 3, 22, 22]), name="parameter")
+    relu = ops.relu(param.output(0))
+    res = ops.result(relu.output(0), name="result")
 
-    exp = opset8.exp(param.output(0))
+    exp = ops.exp(param.output(0))
     relu.output(0).replace(exp.output(0))
 
     assert res.input_value(0).get_node() == exp
 
 
 def test_replace_source_output():
-    param = opset8.parameter(PartialShape([1, 3, 22, 22]), name="parameter")
-    relu = opset8.relu(param.output(0))
-    res = opset8.result(relu.output(0), name="result")
+    param = ops.parameter(PartialShape([1, 3, 22, 22]), name="parameter")
+    relu = ops.relu(param.output(0))
+    res = ops.result(relu.output(0), name="result")
 
-    exp = opset8.exp(param.output(0))
+    exp = ops.exp(param.output(0))
     res.input(0).replace_source_output(exp.output(0))
 
     assert len(exp.output(0).get_target_inputs()) == 1
@@ -40,21 +41,21 @@ def test_replace_source_output():
 
 
 def test_replace_node():
-    param = opset8.parameter(PartialShape([1, 3, 22, 22]), name="parameter")
-    relu = opset8.relu(param.output(0))
-    res = opset8.result(relu.output(0), name="result")
+    param = ops.parameter(PartialShape([1, 3, 22, 22]), name="parameter")
+    relu = ops.relu(param.output(0))
+    res = ops.result(relu.output(0), name="result")
 
-    exp = opset8.exp(param.output(0))
+    exp = ops.exp(param.output(0))
     replace_node(relu, exp)
 
     assert res.input_value(0).get_node() == exp
 
 
 def test_replace_output_update_name():
-    param = opset8.parameter(PartialShape([1, 3, 22, 22]), name="parameter")
-    relu = opset8.relu(param.output(0))
-    exp = opset8.exp(relu.output(0))
-    res = opset8.result(exp.output(0), name="result")
+    param = ops.parameter(PartialShape([1, 3, 22, 22]), name="parameter")
+    relu = ops.relu(param.output(0))
+    exp = ops.exp(relu.output(0))
+    res = ops.result(exp.output(0), name="result")
 
     replace_output_update_name(exp.output(0), exp.input_value(0))
 
