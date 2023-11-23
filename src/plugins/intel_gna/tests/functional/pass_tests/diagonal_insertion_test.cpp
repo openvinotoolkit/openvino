02d551d465a7d080ec118398bbef26670196341e
Kazantsev, Roman <roman.kazantsev@intel.com>
2023-11-23 18:35:40 +0400

    Fix code-style
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/plugins/intel_gna/tests/functional/pass_tests/diagonal_insertion_test.cpp
+++ src/plugins/intel_gna/tests/functional/pass_tests/diagonal_insertion_test.cpp
@@ -27,8 +27,8 @@ using namespace ngraph::op;
 using namespace ngraph::opset9;
 using namespace std;
 
-using DiagonalInsertionTestParams = tuple<map<std::string, std::string>,   // Configuration
-                                          vector<vector<float>>            // FakeQuantize min/max params
+using DiagonalInsertionTestParams = tuple<map<std::string, std::string>,  // Configuration
+                                          vector<vector<float>>           // FakeQuantize min/max params
                                           >;
 
 constexpr uint16_t fq_levels = numeric_limits<uint16_t>::max();
