b29481cf442ee6e51df91a4bd861c13788693369
Tomasz Adamowicz <tomasz.adamowicz@intel.com>
2023-11-23 14:50:49 +0100

    [GNA] Rem usage of thread_local

--- src/plugins/intel_gna/src/backend/gna_limitations.hpp
+++ src/plugins/intel_gna/src/backend/gna_limitations.hpp
@@ -163,12 +163,14 @@ public:
 
 class Limitations {
 public:
+    ~Limitations() {}
     /**
      * @brief Create instance of the Limitations class. Due to Limitations being a singleton, multiple instances of the
      * plugin with different compilation targets cannot exist at the same time
      * @param compile_target GNA compile target
      */
     static void init(const target::DeviceVersion& compile_target);
+    static void deinit();
 
     /**
      * @brief Returns the instance of Limitations object. Requires an Init call before the first usage
@@ -309,14 +311,19 @@ private:
     bool m_use_only_16bit_conv_weights = false;
     size_t m_mem_alignment = 0;
     std::shared_ptr<cnn2d::AbstractValidator> m_cnn_validator;
-    static thread_local std::shared_ptr<Limitations> k_instance;
+
+    static std::unordered_map<std::thread::id, std::shared_ptr<Limitations>> kInstances;
+    static std::mutex kInstancesMtx;
 };
 
 inline std::shared_ptr<Limitations> Limitations::get_instance() {
-    if (!k_instance) {
+    std::lock_guard<std::mutex> lock(kInstancesMtx);
+    auto thread_id = std::this_thread::get_id();
+    auto iter = kInstances.find(thread_id);
+    if (iter == kInstances.end() || !iter->second) {
         THROW_GNA_EXCEPTION << "Limitations instance is not initialized.\n";
     }
-    return k_instance;
+    return iter->second;
 }
 
 inline bool Limitations::is_crop_affined_offset(size_t numberOfElements) const {
