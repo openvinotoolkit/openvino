a7a35c1e935eee13157abe92c344cb2094fbf1ec
Anastasia Kuporosova <anastasia.kuporosova@intel.com>
2023-11-27 11:51:01 +0400

    [PyOV] Replace legacy assert (#21268)
    * [PyOV] Replace legacy assert
    
    * codestyle
    
    * apply comments
    
    * Update src/bindings/python/src/pyopenvino/graph/dict_attribute_visitor.cpp

--- src/bindings/python/src/pyopenvino/graph/dict_attribute_visitor.hpp
+++ src/bindings/python/src/pyopenvino/graph/dict_attribute_visitor.hpp
@@ -86,10 +86,10 @@ public:
 
     template <typename T>
     T get_attribute(const std::string& name) {
-        NGRAPH_CHECK(m_attributes.contains(name),
-                     "Couldn't find attribute \"",
-                     name,
-                     "\" in serialized node attribute dictionary.");
+        OPENVINO_ASSERT(m_attributes.contains(name),
+                        "Couldn't find attribute \"",
+                        name,
+                        "\" in serialized node attribute dictionary.");
         return m_attributes[name.c_str()].cast<T>();
     }
 
