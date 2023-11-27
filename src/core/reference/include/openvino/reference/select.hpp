35e7284bd5fa7e847e075f715d2f9217ac6417ad
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2023-11-27 12:11:24 +0100

    Revert "Reduce binary size"
    This reverts commit 4f69f983570918e307da62b04cb675ead6b98235.

--- src/core/reference/include/openvino/reference/select.hpp
+++ src/core/reference/include/openvino/reference/select.hpp
@@ -11,13 +11,6 @@
 
 namespace ov {
 namespace reference {
-namespace func {
-template <class T>
-T select(char s, T x, T y) {
-    return s ? x : y;
-}
-}  // namespace func
-
 template <typename T>
 void select(const char* arg0,
             const T* arg1,
@@ -41,7 +34,17 @@ void select(const char* arg0,
             const Shape& arg1_shape,
             const Shape& arg2_shape,
             const op::AutoBroadcastSpec& broadcast_spec) {
-    autobroadcast_select(arg0, arg1, arg2, out, arg0_shape, arg1_shape, arg2_shape, broadcast_spec, func::select<T>);
+    autobroadcast_select(arg0,
+                         arg1,
+                         arg2,
+                         out,
+                         arg0_shape,
+                         arg1_shape,
+                         arg2_shape,
+                         broadcast_spec,
+                         [](char s, T x, T y) -> T {
+                             return static_cast<T>(s ? x : y);
+                         });
 }
 }  // namespace reference
 }  // namespace ov
