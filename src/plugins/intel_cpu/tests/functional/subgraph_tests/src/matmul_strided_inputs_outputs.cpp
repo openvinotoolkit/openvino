e1f846ea178ae9c4da92ec44c76ab8c19b329529
River.Li <river.li@intel.com>
2023-11-28 11:34:38 +0800

    [CPU Tests] migrate matmul test cases to be api 2.0

--- src/plugins/intel_cpu/tests/functional/subgraph_tests/src/matmul_strided_inputs_outputs.cpp
+++ src/plugins/intel_cpu/tests/functional/subgraph_tests/src/matmul_strided_inputs_outputs.cpp
@@ -2,39 +2,37 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "test_utils/cpu_test_utils.hpp"
 #include "ov_models/builders.hpp"
+#include "shared_test_classes/base/ov_subgraph.hpp"
+#include "test_utils/cpu_test_utils.hpp"
 
-using namespace ngraph;
-using namespace InferenceEngine;
 using namespace CPUTestUtils;
 
-namespace SubgraphTestsDefinitions {
+namespace ov {
+namespace test {
 
-using MatmulStridedInputsOutputsTestParams = Precision;
+using MatmulStridedInputsOutputsTestParams = ov::element::Type;
 
 class MatmulStridedInputsOutputsTest : public testing::WithParamInterface<MatmulStridedInputsOutputsTestParams>,
                                        public CPUTestsBase,
-                                       virtual public LayerTestsUtils::LayerTestsCommon {
+                                       virtual public SubgraphBaseStaticTest {
 public:
     static std::string getTestCaseName(testing::TestParamInfo<MatmulStridedInputsOutputsTestParams> obj) {
-        Precision netPrecision;
+        ov::element::Type netPrecision;
         netPrecision = obj.param;
 
         std::ostringstream result;
-        result << "netPRC=" << netPrecision.name() << "_";
+        result << "netPRC=" << netPrecision.to_string() << "_";
 
         return result.str();
     }
 
 protected:
     void SetUp() override {
-        targetDevice = ov::test::utils::DEVICE_CPU;
-        Precision netPrecision;
-        netPrecision = this->GetParam();
-        const auto ngPrec = FuncTestUtils::PrecisionUtils::convertIE2nGraphPrc(netPrecision);
+        targetDevice = utils::DEVICE_CPU;
+        const auto ngPrec = this->GetParam();
 
-        SizeVector splitShape{1, 2, 1, 16};
+        ov::Shape splitShape{1, 2, 1, 16};
         ov::ParameterVector splitInputParams {std::make_shared<ov::op::v0::Parameter>(ngPrec, ov::Shape(splitShape))};
         auto split_axis_op = std::make_shared<ov::op::v0::Constant>(ov::element::Type_t::i64, ov::Shape{}, std::vector<int64_t>{1});
         auto split = std::make_shared<ov::op::v1::Split>(splitInputParams[0], split_axis_op, 2);
@@ -42,19 +40,20 @@ protected:
         std::vector<ov::Shape> concatShapes{{1, 1, 8, 8}, {1, 1, 8, 8}};
         ov::ParameterVector concatInputParams {std::make_shared<ov::op::v0::Parameter>(ngPrec, concatShapes[0]),
                                                std::make_shared<ov::op::v0::Parameter>(ngPrec, concatShapes[1])};
-        const auto concatOutputNodes = helpers::convert2OutputVector(helpers::castOps2Nodes<op::Parameter>(concatInputParams));
+        const auto concatOutputNodes = ov::test::utils::convert2OutputVector(
+            ov::test::utils::castOps2Nodes<ov::op::v0::Parameter>(concatInputParams));
         const auto concat = std::make_shared<ov::op::v0::Concat>(concatOutputNodes, 2);
 
         const auto matMul1 = std::make_shared<ov::op::v0::MatMul>(split->output(0), concat, false, false);
 
-        SizeVector matmulShape{1, 1, 16, 8};
+        ov::Shape matmulShape{1, 1, 16, 8};
         ov::ParameterVector matmulInputParams {std::make_shared<ov::op::v0::Parameter>(ngPrec, ov::Shape(matmulShape))};
 
         const auto matMul2 = std::make_shared<ov::op::v0::MatMul>(split->output(1), matmulInputParams[0], false, false);
 
         const auto concatMatMuls = std::make_shared<ov::op::v0::Concat>(ov::NodeVector{matMul1, matMul2}, 2 /* 3rd axis */);
 
-        ngraph::ParameterVector inputParams = {splitInputParams[0], concatInputParams[0], concatInputParams[1], matmulInputParams[0]};
+        ov::ParameterVector inputParams = {splitInputParams[0], concatInputParams[0], concatInputParams[1], matmulInputParams[0]};
         function = makeNgraphFunction(ngPrec, inputParams, concatMatMuls, "MatmulStridedInputsOutputs");
     }
 };
@@ -84,16 +83,17 @@ protected:
 */
 
 TEST_P(MatmulStridedInputsOutputsTest, CompareWithRefs) {
-    Run();
+    run();
 }
 
 namespace {
 
-INSTANTIATE_TEST_SUITE_P(smoke_Check, MatmulStridedInputsOutputsTest,
-                         ::testing::Values(Precision::FP32,
-                                           Precision::BF16),
+INSTANTIATE_TEST_SUITE_P(smoke_Check,
+                         MatmulStridedInputsOutputsTest,
+                         ::testing::Values(ov::element::f32, ov::element::bf16),
                          MatmulStridedInputsOutputsTest::getTestCaseName);
 
-} // namespace
+}  // namespace
 
-} // namespace SubgraphTestsDefinitions
+}  // namespace test
+}  // namespace ov
