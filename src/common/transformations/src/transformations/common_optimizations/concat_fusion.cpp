6ba4ce641978c300324d37b4b32e86510c77bb68
Maxim Vafin <maxim.vafin@intel.com>
2023-11-28 10:34:27 +0100

    Add transformation to fuse Concat sequence (#21310)
    * Add transformation to fuse Concat sequence
    
    * Update src/common/transformations/src/transformations/common_optimizations/concat_fusion.cpp

---
+++ src/common/transformations/src/transformations/common_optimizations/concat_fusion.cpp
@@ -0,0 +1,60 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "transformations/common_optimizations/concat_fusion.hpp"
+
+#include <memory>
+#include <vector>
+
+#include "itt.hpp"
+#include "openvino/core/rt_info.hpp"
+#include "openvino/op/concat.hpp"
+#include "openvino/pass/pattern/op/wrap_type.hpp"
+#include "transformations/utils/utils.hpp"
+
+using namespace ov::op;
+
+ov::pass::ConcatFusion::ConcatFusion() {
+    MATCHER_SCOPE(ConcatFusion);
+    auto has_same_axis_concat_input = [](const Output<Node>& output) {
+        const auto& concat = std::dynamic_pointer_cast<v0::Concat>(output.get_node_shared_ptr());
+        const auto axis = concat->get_axis();
+        auto is_aplicable = false;
+        for (auto input : concat->input_values()) {
+            const auto inp_concat = std::dynamic_pointer_cast<v0::Concat>(input.get_node_shared_ptr());
+            if (inp_concat && inp_concat->get_axis() == axis) {
+                is_aplicable = true;
+            }
+        }
+        return is_aplicable;
+    };
+    auto concat_pattern = pattern::wrap_type<v0::Concat>(has_same_axis_concat_input);
+
+    ov::matcher_pass_callback callback = [=](pattern::Matcher& m) {
+        const auto& pattern_map = m.get_pattern_map();
+
+        const auto& concat = std::dynamic_pointer_cast<v0::Concat>(pattern_map.at(concat_pattern));
+        const auto axis = concat->get_axis();
+
+        OutputVector new_inputs;
+        for (auto input : concat->input_values()) {
+            const auto inp_concat = std::dynamic_pointer_cast<v0::Concat>(input.get_node_shared_ptr());
+            if (inp_concat && inp_concat->get_axis() == axis) {
+                const auto inp_concat_inps = inp_concat->input_values();
+                new_inputs.insert(new_inputs.end(), inp_concat_inps.begin(), inp_concat_inps.end());
+            } else {
+                new_inputs.push_back(input);
+            }
+        }
+
+        auto new_concat = std::make_shared<v0::Concat>(new_inputs, axis);
+        replace_node(concat, new_concat);
+        new_concat->set_friendly_name(concat->get_friendly_name());
+        copy_runtime_info(concat, new_concat);
+        return true;
+    };
+
+    auto m = std::make_shared<ov::pass::pattern::Matcher>(concat_pattern, matcher_name);
+    this->register_matcher(m, callback);
+}
