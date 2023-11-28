a093c9f9cb4d42ecbf2c683aea402ae5582df06e
Kazantsev, Roman <roman.kazantsev@intel.com>
2023-11-28 12:02:58 +0400

    Recover init.py file

--- tools/ovc/openvino/__init__.py
+++ tools/ovc/openvino/__init__.py
@@ -19,8 +19,37 @@ except ImportError:
 # # It is not compared with init files from openvino-dev package.
 # #
 # Import all public modules
+from openvino import runtime as runtime
+from openvino import frontend as frontend
+from openvino import helpers as helpers
+from openvino import preprocess as preprocess
+from openvino import utils as utils
+from openvino.runtime import properties as properties
 
 # Import most important classes and functions from openvino.runtime
+from openvino.runtime import Model
+from openvino.runtime import Core
+from openvino.runtime import CompiledModel
+from openvino.runtime import InferRequest
+from openvino.runtime import AsyncInferQueue
+
+from openvino.runtime import Dimension
+from openvino.runtime import Strides
+from openvino.runtime import PartialShape
+from openvino.runtime import Shape
+from openvino.runtime import Layout
+from openvino.runtime import Type
+from openvino.runtime import Tensor
+from openvino.runtime import OVAny
+
+from openvino.runtime import compile_model
+from openvino.runtime import get_batch
+from openvino.runtime import set_batch
+from openvino.runtime import serialize
+from openvino.runtime import shutdown
+from openvino.runtime import tensor_from_file
+from openvino.runtime import save_model
+from openvino.runtime import layout_helpers
 
 # Set version for openvino package
 from openvino.runtime import get_version
