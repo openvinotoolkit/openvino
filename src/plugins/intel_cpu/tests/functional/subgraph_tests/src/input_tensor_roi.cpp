db9ec812f0c9051257f056e728546d1bc1d62293
Zhai, Xuejun <xuejun.zhai@intel.com>
2023-11-29 21:37:07 +0800

    [CPU Plugin][Func Test] Upgrade InputTensorROI to API 2.0
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>

--- src/plugins/intel_cpu/tests/functional/subgraph_tests/src/input_tensor_roi.cpp
+++ src/plugins/intel_cpu/tests/functional/subgraph_tests/src/input_tensor_roi.cpp
@@ -2,16 +2,14 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include "functional_test_utils/ov_plugin_cache.hpp"
 #include "ov_models/builders.hpp"
 #include "test_utils/cpu_test_utils.hpp"
-#include "functional_test_utils/ov_plugin_cache.hpp"
 
-using namespace ngraph;
-using namespace ngraph::op;
-using namespace InferenceEngine;
 using namespace CPUTestUtils;
 
-namespace SubgraphTestsDefinitions {
+namespace ov {
+namespace test {
 
 struct InputTensorROIParamType {
     ov::PartialShape shape;
@@ -30,24 +28,23 @@ public:
     }
 
 protected:
-    std::shared_ptr<ov::Model>
-    create_test_function(element::Type type,
-                        const ov::PartialShape & shape,
-                        const ov::Layout & layout) {
+    std::shared_ptr<ov::Model> create_test_function(element::Type type,
+                                                    const ov::PartialShape& shape,
+                                                    const ov::Layout& layout) {
         ResultVector res;
         ParameterVector params;
 
-        auto param = std::make_shared<opset8::Parameter>(type, shape);
+        auto param = std::make_shared<ov::op::v0::Parameter>(type, shape);
         param->set_friendly_name("input_0");
         param->get_output_tensor(0).set_names({"tensor_input_0"});
         param->set_layout(layout);
 
-        auto constant = opset8::Constant::create(type, {1}, {1});
+        auto constant = ov::op::v0::Constant::create(type, {1}, {1});
 
-        auto add = std::make_shared<opset8::Add>(param, constant);
+        auto add = std::make_shared<ov::op::v1::Add>(param, constant);
         add->set_friendly_name("Add");
 
-        auto result = std::make_shared<opset8::Result>(add);
+        auto result = std::make_shared<ov::op::v0::Result>(add);
         result->set_friendly_name("result_0");
         result->get_output_tensor(0).set_names({"tensor_output_0"});
 
@@ -57,7 +54,7 @@ protected:
         return std::make_shared<ov::Model>(res, params);
     }
 
-    template<typename T>
+    template <typename T>
     void Run() {
         std::shared_ptr<ov::Core> ie = ov::test::utils::PluginCache::get().core();
 
@@ -70,14 +67,14 @@ protected:
         ov::InferRequest req = compiled_model.create_infer_request();
 
         // Create input tensor
-        auto input_shape = Shape{ 1, 4, 4, 4 };
+        auto input_shape = Shape{1, 4, 4, 4};
         auto input_shape_size = ov::shape_size(input_shape);
         std::vector<T> data(input_shape_size);
         std::iota(data.begin(), data.end(), 0);
         auto input_tensor = ov::Tensor(GetParam().type, input_shape, &data[0]);
 
         // Set ROI
-        auto roi = ov::Tensor(input_tensor, { 0, 1, 1, 1 }, { 1, 3, 3, 3 });
+        auto roi = ov::Tensor(input_tensor, {0, 1, 1, 1}, {1, 3, 3, 3});
         req.set_tensor("tensor_input_0", roi);
 
         // Infer
@@ -99,24 +96,24 @@ protected:
 
 TEST_P(InputTensorROI, SetInputTensorROI) {
     switch (GetParam().type) {
-        case ov::element::Type_t::f32: {
-            Run<float>();
-            break;
-        }
-        case ov::element::Type_t::u8: {
-            Run<uint8_t>();
-            break;
-        }
-        default:
-            break;
+    case ov::element::Type_t::f32: {
+        Run<float>();
+        break;
+    }
+    case ov::element::Type_t::u8: {
+        Run<uint8_t>();
+        break;
+    }
+    default:
+        break;
     }
 }
 
 static InputTensorROI::ParamType InputTensorROIParams[] = {
-    { ov::PartialShape{ 1, 2, 2, 2 }, element::f32, "NCHW" },
-    { ov::PartialShape{ 1, 2, ov::Dimension::dynamic(), ov::Dimension::dynamic() }, element::f32, "NCHW" },
-    { ov::PartialShape{ 1, 2, 2, 2 }, element::u8, "NCHW" },
-    { ov::PartialShape{ 1, 2, ov::Dimension::dynamic(), ov::Dimension::dynamic() }, element::u8, "NCHW" },
+    {ov::PartialShape{1, 2, 2, 2}, element::f32, "NCHW"},
+    {ov::PartialShape{1, 2, ov::Dimension::dynamic(), ov::Dimension::dynamic()}, element::f32, "NCHW"},
+    {ov::PartialShape{1, 2, 2, 2}, element::u8, "NCHW"},
+    {ov::PartialShape{1, 2, ov::Dimension::dynamic(), ov::Dimension::dynamic()}, element::u8, "NCHW"},
 };
 
 INSTANTIATE_TEST_SUITE_P(smoke_InputTensorROI,
@@ -124,4 +121,5 @@ INSTANTIATE_TEST_SUITE_P(smoke_InputTensorROI,
                          ::testing::ValuesIn(InputTensorROIParams),
                          InputTensorROI::getTestCaseName);
 
-} // namespace SubgraphTestsDefinitions
+}  // namespace test
+}  // namespace ov
