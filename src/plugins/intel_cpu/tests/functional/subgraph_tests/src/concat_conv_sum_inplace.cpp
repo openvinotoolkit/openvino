ff4afde9c3ce303ed594171cb7f37a01112e52f4
Xuejun Zhai <xuejun.zhai@intel.com>
2023-11-29 18:31:11 +0800

    Xuejun/concat related tests 2.0 (#21334)
    * [CPU Plugin][Func Test] Upgrade ConcatConstantInPlaceTest to API 2.0
    
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>
    
    * [CPU Plugin][Func Test] Upgrade ReLuConcatConvSumInPlaceTest to API 2.0
    
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>
    
    * [CPU Plugin][Func Test] Upgrade ConcatReorderInPlaceTest to API 2.0
    
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>
    
    * [CPU Plugin][Func Test] fix enforce_bf16
    
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>
    
    * Update concat_conv_sum_inplace.cpp
    
    ---------
    
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>

--- src/plugins/intel_cpu/tests/functional/subgraph_tests/src/concat_conv_sum_inplace.cpp
+++ src/plugins/intel_cpu/tests/functional/subgraph_tests/src/concat_conv_sum_inplace.cpp
@@ -2,15 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "test_utils/cpu_test_utils.hpp"
-#include "shared_test_classes/base/layer_test_utils.hpp"
-#include "ov_models/utils/ov_helpers.hpp"
+#include "common_test_utils/node_builders/convolution.hpp"
 #include "ov_models/builders.hpp"
+#include "ov_models/utils/ov_helpers.hpp"
+#include "shared_test_classes/base/ov_subgraph.hpp"
+#include "test_utils/cpu_test_utils.hpp"
 
 using namespace CPUTestUtils;
-using namespace InferenceEngine;
 
-namespace SubgraphTestsDefinitions {
+namespace ov {
+namespace test {
 // Subgraph:
 /*
  *          paramter1         parameter2
@@ -36,46 +37,56 @@ namespace SubgraphTestsDefinitions {
  *                      Result
  */
 
-class ReLuConcatConvSumInPlaceTest : virtual public LayerTestsUtils::LayerTestsCommon {
+class ReLuConcatConvSumInPlaceTest : virtual public SubgraphBaseStaticTest {
 public:
     void SetUp() override {
-        const std::vector<size_t> inputShape = {1, 64, 12, 12};
-        const InferenceEngine::SizeVector kernel = {1, 1};
-        const InferenceEngine::SizeVector stride = {1, 1};
-        const InferenceEngine::SizeVector dilation = {1, 1};
+        const ov::Shape inputShape = {1, 64, 12, 12};
+        const std::vector<size_t> kernel = {1, 1};
+        const std::vector<size_t> stride = {1, 1};
+        const std::vector<size_t> dilation = {1, 1};
         const std::vector<ptrdiff_t> padBegin = {0, 0};
         const std::vector<ptrdiff_t> padEnd = {0, 0};
         const size_t convOutChannels = 64;
         const auto targetFormat = with_cpu_x86_avx512_core() ? nChw16c : nChw8c;
 
-
-        ov::ParameterVector inputParams{std::make_shared<ov::op::v0::Parameter>(ov::element::f32, ov::Shape(inputShape)),
-                                        std::make_shared<ov::op::v0::Parameter>(ov::element::f32, ov::Shape(inputShape))};
-        auto Relu1 = std::make_shared<ngraph::opset3::Relu>(inputParams[0]);
+        ov::ParameterVector inputParams{std::make_shared<ov::op::v0::Parameter>(ov::element::f32, inputShape),
+                                        std::make_shared<ov::op::v0::Parameter>(ov::element::f32, inputShape)};
+        auto Relu1 = std::make_shared<ov::op::v0::Relu>(inputParams[0]);
         Relu1->get_rt_info() = CPUTestsBase::makeCPUInfo({targetFormat}, {targetFormat}, {});
-        auto Relu2 = std::make_shared<ngraph::opset3::Relu>(inputParams[1]);
+        auto Relu2 = std::make_shared<ov::op::v0::Relu>(inputParams[1]);
         Relu2->get_rt_info() = CPUTestsBase::makeCPUInfo({targetFormat}, {targetFormat}, {});
 
         auto concat = std::make_shared<ov::op::v0::Concat>(ov::NodeVector{Relu1, Relu2}, 1);
 
-        auto conv = ngraph::builder::makeConvolution(concat, ngraph::element::f32, kernel, stride, padBegin,
-                                                     padEnd, dilation, ngraph::op::PadType::AUTO, convOutChannels);
-        auto bias = ngraph::builder::makeConstant<float>(ngraph::element::Type_t::f32, ngraph::Shape({1, convOutChannels, 1, 1}), {}, true);
-        auto convBiasAdd = std::make_shared<ngraph::opset3::Add>(conv, bias);
+        auto conv = ov::test::utils::make_convolution(concat,
+                                                      ov::element::f32,
+                                                      kernel,
+                                                      stride,
+                                                      padBegin,
+                                                      padEnd,
+                                                      dilation,
+                                                      ov::op::PadType::AUTO,
+                                                      convOutChannels);
+        auto bias = ngraph::builder::makeConstant<float>(ov::element::Type_t::f32,
+                                                         ov::Shape({1, convOutChannels, 1, 1}),
+                                                         {},
+                                                         true);
+        auto convBiasAdd = std::make_shared<ov::op::v1::Add>(conv, bias);
 
-        auto sum = std::make_shared<ngraph::opset3::Add>(convBiasAdd, Relu1);
+        auto sum = std::make_shared<ov::op::v1::Add>(convBiasAdd, Relu1);
 
-        auto Relu3 = std::make_shared<ngraph::opset3::Relu>(sum);
+        auto Relu3 = std::make_shared<ov::op::v0::Relu>(sum);
 
-        ngraph::ResultVector results{std::make_shared<ngraph::opset3::Result>(Relu3)};
-        function = std::make_shared<ngraph::Function>(results, inputParams, "ConcatConvSumInPlace");
+        ov::ResultVector results{std::make_shared<ov::op::v0::Result>(Relu3)};
+        function = std::make_shared<ov::Model>(results, inputParams, "ConcatConvSumInPlace");
         targetDevice = ov::test::utils::DEVICE_CPU;
     }
 };
 
 namespace {
-    TEST_F(ReLuConcatConvSumInPlaceTest, smoke_ReLuConcatConvSumInPlace_CPU) {
-        Run();
-    }
-} // namespace
-} // namespace SubgraphTestsDefinitions
+TEST_F(ReLuConcatConvSumInPlaceTest, smoke_ReLuConcatConvSumInPlace_CPU) {
+    run();
+}
+}  // namespace
+}  // namespace test
+}  // namespace ov
