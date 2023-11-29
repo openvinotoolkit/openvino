8627c45ba99f787b2787f2a327f5c5e31ebab278
Maxim Vafin <maxim.vafin@intel.com>
2023-11-29 20:10:09 +0100

    Update src/frontends/pytorch/src/op/rand.cpp

--- src/frontends/pytorch/src/op/rand.cpp
+++ src/frontends/pytorch/src/op/rand.cpp
@@ -26,10 +26,10 @@ using namespace ov::op;
 
 namespace {
 OutputVector make_random_normal(const NodeContext& context,
-                                Output<Node> sizes,
+                                const Output<Node>& sizes,
                                 element::Type target_type,
-                                Output<Node> scale_const,
-                                Output<Node> mean_const) {
+                                const Output<Node>& scale_const,
+                                const Output<Node>& mean_const) {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<uint64_t> distrib(0, 9999);
