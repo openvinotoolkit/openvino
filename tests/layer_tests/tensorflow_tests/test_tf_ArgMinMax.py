c9aaecfa2c46087d06fc0290ccce15f20b2ca7fd
Kazantsev, Roman <roman.kazantsev@intel.com>
2023-11-29 13:19:18 +0400

    [TF FE] Correct ArgMinMax translators and add tests
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- tests/layer_tests/tensorflow_tests/test_tf_ArgMinMax.py
+++ tests/layer_tests/tensorflow_tests/test_tf_ArgMinMax.py
@@ -3,44 +3,33 @@
 
 import numpy as np
 import pytest
-
+import tensorflow as tf
 from common.tf_layer_test_class import CommonTFLayerTest
+
 
 # Testing operation ArgMin, ArgMax (Initial Implementation)
 # Documentation: https://www.tensorflow.org/api_docs/python/tf/raw_ops/ArgMin
 #                https://www.tensorflow.org/api_docs/python/tf/raw_ops/ArgMax
 
 class TestArgMinMax(CommonTFLayerTest):
-    # input_shape - should be an array
-    # dimension - dimension to be used, for vector should be 0
-    # op_type - type of testing operation
-    # ir_version - common parameter
-    # use_new_frontend - common parameter
-    def create_argminmax_placeholder_const_net(self, input_shape, dimension, op_type, ir_version, use_new_frontend):
-        """
-            Tensorflow net                  IR net
+    def _prepare_input(self, inputs_info):
+        assert 'input' in inputs_info
+        input_shape = inputs_info['input']
+        inputs_data = {}
+        rng = np.random.default_rng()
+        inputs_data['input'] = rng.integers(-8, 8, input_shape).astype(self.input_type)
+        return inputs_data
 
-            Placeholder->op_type    =>       Placeholder->TopK->Squeeze
-                         /                               /     /
-            Const-------/                   Const-------/-----/
-
-        """
-
-        import tensorflow as tf
-
+    def create_argmin_max_net(self, input_shape, dimension, input_type, output_type, op_type):
+        self.input_type = input_type
         tf.compat.v1.reset_default_graph()
 
         # Create the graph and model
         with tf.compat.v1.Session() as sess:
-            op_type_to_tf = {
-                'ArgMax': tf.raw_ops.ArgMax,
-                'ArgMin': tf.raw_ops.ArgMin,
-            }
-            tf_input_shape = input_shape.copy()
-            tf_input = tf.compat.v1.placeholder(tf.float32, tf_input_shape, 'Input')
+            tf_input = tf.compat.v1.placeholder(input_type, input_shape, 'input')
             tf_dimension = tf.constant(dimension)
 
-            op_type_to_tf[op_type](input = tf_input, dimension = tf_dimension)
+            op_type(input=tf_input, dimension=tf_dimension, output_type=output_type)
 
             tf.compat.v1.global_variables_initializer()
             tf_net = sess.graph_def
@@ -50,21 +39,21 @@ class TestArgMinMax(CommonTFLayerTest):
         return tf_net, ref_net
 
     test_data = [
-        dict(input_shape=[5], dimension=0),            #Simple test of vector
-        pytest.param(
-            dict(input_shape=[2, 3], dimension=1),     #Simple test
-            marks=pytest.mark.precommit_tf_fe),
-        dict(input_shape=[2, 3, 3, 4], dimension=2),   #Simple test with possible nchw/nhcw
+        dict(input_shape=[20], dimension=0),
+        dict(input_shape=[20, 30], dimension=1),
+        dict(input_shape=[2, 30, 3, 4], dimension=2),
     ]
 
     @pytest.mark.parametrize("params", test_data)
-    @pytest.mark.parametrize("op_type", ['ArgMin', 'ArgMax'])
+    @pytest.mark.parametrize("input_type", [np.float32, np.int32])
+    @pytest.mark.parametrize("output_type", [tf.int32, tf.int64])
+    @pytest.mark.parametrize("op_type", [tf.raw_ops.ArgMax, tf.raw_ops.ArgMin])
     @pytest.mark.precommit
+    @pytest.mark.precommit_tf_fe
     @pytest.mark.nightly
-    def test_argminmax_placeholder_const(self, params, op_type, ie_device, precision, ir_version, temp_dir,
-                                      use_new_frontend, use_old_api):
-        self._test(*self.create_argminmax_placeholder_const_net(**params, op_type=op_type,
-                                                                ir_version=ir_version,
-                                                                use_new_frontend=use_new_frontend),
+    def test_argmin_max_net(self, params, input_type, output_type, op_type, ie_device, precision, ir_version, temp_dir,
+                            use_new_frontend, use_old_api):
+        self._test(*self.create_argmin_max_net(**params, input_type=input_type,
+                                               output_type=output_type, op_type=op_type),
                    ie_device, precision, ir_version, temp_dir=temp_dir,
                    use_new_frontend=use_new_frontend, use_old_api=use_old_api)
