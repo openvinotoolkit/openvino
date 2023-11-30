405d97e4a521f0983a6cadb8f5476321e3d95796
Maksim Kutakov <maksim.kutakov@intel.com>
2023-11-30 11:07:43 +0100

    [CPU] Add optimized memory management for SDPA KV cache (#21242)

---
+++ src/plugins/intel_cpu/src/nodes/common/arbitrary_order_desc_creator.cpp
@@ -0,0 +1,39 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "arbitrary_order_desc_creator.h"
+
+namespace ov {
+namespace intel_cpu {
+
+ArbitraryOrderDescCreator::ArbitraryOrderDescCreator(VectorDims order) :
+    m_order(std::move(order)) {
+    OPENVINO_ASSERT(std::adjacent_find(m_order.begin(), m_order.end()) == m_order.end(),
+        "Can't construct ArbitraryOrderDescCreator, order vector contains repetitive elements",
+        vec2str(m_order));
+}
+
+CpuBlockedMemoryDesc
+ArbitraryOrderDescCreator::createDesc(const ov::element::Type& precision, const Shape& srcShape) const {
+    auto&& dims = srcShape.getDims();
+    OPENVINO_ASSERT(dims.size() == m_order.size(),
+        "Couldn't create a tensor descriptor, shape and order size mismatch. Shape: ",
+        vec2str(dims),
+        " order: ",
+        vec2str(m_order));
+
+    VectorDims blkDims(dims.size());
+    for (size_t i = 0; i < dims.size(); ++i) {
+        blkDims[i] = dims[m_order[i]];
+    }
+
+    return CpuBlockedMemoryDesc(precision, srcShape, blkDims, m_order);
+}
+
+size_t ArbitraryOrderDescCreator::getMinimalRank() const {
+    return m_order.size();
+}
+
+}   // namespace intel_cpu
+}   // namespace ov
---
+++ src/plugins/intel_cpu/src/transformations/cpu_opset/common/pass/stateful_sdp_fusion.hpp
@@ -0,0 +1,18 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <ngraph/pass/graph_rewrite.hpp>
+
+namespace ov {
+namespace intel_cpu {
+class StatefulSDPFusion : public ov::pass::MatcherPass {
+public:
+    OPENVINO_RTTI("StatefulSDPFusion", "0");
+    StatefulSDPFusion();
+};
+
+}   // namespace intel_cpu
+}   // namespace ov
