5da52511e4a17f94f072c7de21d12f678e4a6717
Kazantsev, Roman <roman.kazantsev@intel.com>
2023-11-30 14:04:09 +0400

    Fix build issue
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/core/src/op/constant.cpp
+++ src/core/src/op/constant.cpp
@@ -76,7 +76,7 @@ Constant::Constant(const std::shared_ptr<ngraph::runtime::Tensor>& tensor) {
             tensor->get_size_in_bytes(),
             tensor);
     } else {
-        auto tensor_type = std::string(typeid(*tensor.get()).name());
+        auto tensor_type = std::string(typeid(*tensor).name());
         OPENVINO_ASSERT(m_element_type != ov::element::string,
                         "Creation of string constant from a tensor " + tensor_type + " is not supported");
         constructor_validate_and_infer_types();
