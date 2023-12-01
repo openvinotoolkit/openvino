b64d602f7e7ab67be084624618807de5f5416185
Kelvin Choi <kelvin.choi@intel.com>
2023-12-01 20:04:50 +0900

    [GPU] Support negative indice for static (#20909)

--- src/plugins/intel_gpu/src/plugin/ops/gather.cpp
+++ src/plugins/intel_gpu/src/plugin/ops/gather.cpp
@@ -106,7 +106,12 @@ void CreateGatherOpBase(ProgramBuilder& p, const std::shared_ptr<T>& op, const i
 
         // Set tensors for crop shape and offset
         ov::Shape start_offset(input_shape.size());
-        start_offset[0] = static_cast<size_t>(result);
+        int32_t new_offset0 = static_cast<int32_t>(result);
+        if (support_neg_ind && new_offset0 < 0) {
+            new_offset0 += static_cast<int32_t>(start_offset.size());
+        }
+
+        start_offset[0] = static_cast<size_t>(new_offset0);
         auto offsetTensor = tensor_from_dims(start_offset, 0);
         auto outTensor = tensor_from_dims(out_shape, 1);
 
