abbb2b45ef3219fa34a5e6450b12f41f2b577cca
andsonder <changlu@keter.top>
2023-12-02 06:30:06 +0000

    fix grid_sample error when using dynamic shape

--- src/frontends/paddle/src/op/grid_sampler.cpp
+++ src/frontends/paddle/src/op/grid_sampler.cpp
@@ -4,6 +4,7 @@
 
 #include "default_opset.hpp"
 #include "openvino/frontend/paddle/node_context.hpp"
+#include "openvino/op/grid_sample.hpp"
 
 namespace ov {
 namespace frontend {
@@ -12,15 +13,15 @@ namespace op {
 NamedOutputs grid_sampler(const NodeContext& node) {
     auto data = node.get_input("X");
     auto grid = node.get_input("Grid");
-    default_opset::GridSample::Attributes attributes{};
+    ov::op::v9::GridSample::Attributes attributes{};
 
     attributes.align_corners = node.get_attribute<bool>("align_corners", 1);
-    attributes.mode = ov::EnumNames<default_opset::GridSample::InterpolationMode>::as_enum(
+    attributes.mode = ov::EnumNames<ov::op::v9::GridSample::InterpolationMode>::as_enum(
         node.get_attribute<std::string>("mode", "bilinear"));
-    attributes.padding_mode = ov::EnumNames<default_opset::GridSample::PaddingMode>::as_enum(
+    attributes.padding_mode = ov::EnumNames<ov::op::v9::GridSample::PaddingMode>::as_enum(
         node.get_attribute<std::string>("padding_mode", "zeros"));
 
-    return node.default_single_output_mapping({std::make_shared<default_opset::GridSample>(data, grid, attributes)},
+    return node.default_single_output_mapping({std::make_shared<ov::op::v9::GridSample>(data, grid, attributes)},
                                               {"Output"});
 }
 }  // namespace op
