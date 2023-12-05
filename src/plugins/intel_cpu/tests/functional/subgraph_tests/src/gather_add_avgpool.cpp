aa1c9a1ac25a09988d6278e089ef25dc92470802
Xuejun Zhai <xuejun.zhai@intel.com>
2023-12-05 14:24:55 +0800

    Upgrade CPU func tests to 2.o (#21357)
    * [CPU Plugin][Func Test] Upgrade AddConvertToReorderTest to API 2.0
    
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>
    
    * [CPU Plugin][Func Test] Upgrade AlignMatMulInputRanksTest to API 2.0
    
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>
    
    * [CPU Plugin][Func Test] Upgrade GatherAddAvgpool to API 2.0
    
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>
    
    * [CPU Plugin][Func Test] Upgrade InputNoReorderEltwiseBF16 to API 2.0
    
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>
    
    * [CPU Plugin][Func Test] Fix review comments
    
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>
    
    ---------
    
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>

--- src/plugins/intel_cpu/tests/functional/subgraph_tests/src/gather_add_avgpool.cpp
+++ src/plugins/intel_cpu/tests/functional/subgraph_tests/src/gather_add_avgpool.cpp
@@ -2,12 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "shared_test_classes/base/layer_test_utils.hpp"
 #include <ngraph/opsets/opset8.hpp>
-#include <exec_graph_info.hpp>
 
+#include "openvino/runtime/exec_model_info.hpp"
+#include "shared_test_classes/base/ov_subgraph.hpp"
 
-namespace SubgraphTestsDefinitions {
+namespace ov {
+namespace test {
 
 using namespace ngraph;
 
@@ -25,30 +26,33 @@ using namespace ngraph;
    input's precision if its child has Subgraph consumers.
    Same scenario happens when we have Eltwise instead of Subgraph - to be addressed in #78939.
 */
-class GatherAddAvgpool : virtual public LayerTestsUtils::LayerTestsCommon {
+class GatherAddAvgpool : virtual public SubgraphBaseStaticTest {
 protected:
     void SetUp() override {
         targetDevice = ov::test::utils::DEVICE_CPU;
-        inPrc = InferenceEngine::Precision::U8;
-        outPrc = InferenceEngine::Precision::FP32;
-        auto type = element::f32;
-        auto param = std::make_shared<opset8::Parameter>(type, Shape{1, 3, 64, 64});
-        auto gather = std::make_shared<opset8::Gather>(param,
-                                                       op::Constant::create(element::i32, Shape{3}, {2, 1, 0}),
-                                                       op::Constant::create(element::i32, Shape{1}, {1}));
-        auto add = std::make_shared<opset8::Add>(gather, op::Constant::create(type, Shape{1, 3, 1, 1}, {3}));
-        auto avgpool = std::make_shared<opset8::AvgPool>(add, Strides{1, 1}, Shape{0, 0}, Shape{0, 0}, Shape{2, 2}, false);
-        function = std::make_shared<Function>(avgpool, ParameterVector{param});
+        inType = ov::element::u8;
+        outType = ov::element::f32;
+        auto type = ov::element::f32;
+        auto param = std::make_shared<ov::op::v0::Parameter>(type, Shape{1, 3, 64, 64});
+        auto gather =
+            std::make_shared<ov::op::v8::Gather>(param,
+                                                 ov::op::v0::Constant::create(element::i32, Shape{3}, {2, 1, 0}),
+                                                 ov::op::v0::Constant::create(element::i32, Shape{1}, {1}));
+        auto add =
+            std::make_shared<ov::op::v1::Add>(gather, ov::op::v0::Constant::create(type, Shape{1, 3, 1, 1}, {3}));
+        auto avgpool =
+            std::make_shared<ov::op::v1::AvgPool>(add, Strides{1, 1}, Shape{0, 0}, Shape{0, 0}, Shape{2, 2}, false);
+        function = std::make_shared<ov::Model>(avgpool, ov::ParameterVector{param});
     }
 
     void TearDown() override {
-        auto exec_model = executableNetwork.GetExecGraphInfo().getFunction();
+        auto exec_model = compiledModel.get_runtime_model();
 
         int eltwise_nodes_found = 0;
         int pool_nodes_found = 0;
         for (const auto& n : exec_model->get_ordered_ops()) {
-            auto layer_type = n->get_rt_info().at(ExecGraphInfoSerialization::LAYER_TYPE).as<std::string>();
-            auto output_layout = n->get_rt_info().at(ExecGraphInfoSerialization::OUTPUT_LAYOUTS).as<std::string>();
+            auto layer_type = n->get_rt_info().at(ov::exec_model_info::LAYER_TYPE).as<std::string>();
+            auto output_layout = n->get_rt_info().at(ov::exec_model_info::OUTPUT_LAYOUTS).as<std::string>();
             if (layer_type == "Subgraph") {
                 eltwise_nodes_found++;
                 ASSERT_EQ("abcd", output_layout);
@@ -63,7 +67,8 @@ protected:
 };
 
 TEST_F(GatherAddAvgpool, smoke_CompareWithRefs) {
-    Run();
+    run();
 }
 
-} // namespace SubgraphTestsDefinitions
+}  // namespace test
+}  // namespace ov
