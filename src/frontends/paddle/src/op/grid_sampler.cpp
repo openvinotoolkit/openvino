e159bc9ae1efc50063e14116fe0b6d620a7e7a8e
andsonder <changlu@keter.top>
2023-12-05 05:20:26 +0000

    recover grid_sampler

--- src/frontends/paddle/src/op/grid_sampler.cpp
+++ src/frontends/paddle/src/op/grid_sampler.cpp
@@ -18,17 +18,11 @@ NamedOutputs grid_sampler(const NodeContext& node) {
     auto grid = node.get_input("Grid");
     default_opset::GridSample::Attributes attributes{};
 
-    const std::unordered_map<std::string, v9::GridSample::InterpolationMode> grid_sample_mode_map{
-        {"bilinear", v9::GridSample::InterpolationMode::BILINEAR},
-        {"nearest", v9::GridSample::InterpolationMode::NEAREST}};
-    const std::unordered_map<std::string, v9::GridSample::PaddingMode> grid_sample_padding_mode_map{
-        {"zeros", v9::GridSample::PaddingMode::ZEROS},
-        {"border", v9::GridSample::PaddingMode::BORDER},
-        {"reflection", v9::GridSample::PaddingMode::REFLECTION}};
-
-    attributes.align_corners = node.get_attribute<bool>("align_corners", true);
-    attributes.mode = grid_sample_mode_map.at(node.get_attribute<std::string>("mode", "bilinear"));
-    attributes.padding_mode = grid_sample_padding_mode_map.at(node.get_attribute<std::string>("padding_mode", "zeros"));
+    attributes.align_corners = node.get_attribute<bool>("align_corners", 1);
+    attributes.mode = ov::EnumNames<default_opset::GridSample::InterpolationMode>::as_enum(
+        node.get_attribute<std::string>("mode", "bilinear"));
+    attributes.padding_mode = ov::EnumNames<default_opset::GridSample::PaddingMode>::as_enum(
+        node.get_attribute<std::string>("padding_mode", "zeros"));
 
     return node.default_single_output_mapping({std::make_shared<default_opset::GridSample>(data, grid, attributes)},
                                               {"Output"});
