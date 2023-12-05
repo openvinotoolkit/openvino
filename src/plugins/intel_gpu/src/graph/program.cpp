9a836b14b461604fa96b185b30225024a274e82e
Eddy Kim <eddy.kim@intel.com>
2023-12-05 21:36:12 +0900

    added get_allocating_order() that is cached for faster loading

--- src/plugins/intel_gpu/src/graph/program.cpp
+++ src/plugins/intel_gpu/src/graph/program.cpp
@@ -740,6 +740,47 @@ program::nodes_ordering& program::get_processing_order() { return processing_ord
 
 const program::nodes_ordering& program::get_processing_order() const { return processing_order; }
 
+const std::vector<primitive_id>& program::get_allocating_order(bool forced_update) {
+    if (!forced_update && allocating_order.size() > 0)
+        return allocating_order;
+
+    std::vector<std::shared_ptr<program_node>> nodes_to_allocate{};
+    auto& po = get_processing_order();
+    for (auto node : po) {
+        nodes_to_allocate.push_back(get_node_ptr(node->id()));
+    }
+
+    std::sort(nodes_to_allocate.begin(),
+            nodes_to_allocate.end(),
+            [&po](std::shared_ptr<program_node> const& lhs, std::shared_ptr<program_node> const& rhs) {
+                    auto lhs_layout = lhs->get_output_layout();
+                    auto rhs_layout = rhs->get_output_layout();
+                    if (lhs_layout.is_dynamic() && lhs_layout.has_upper_bound()) {
+                        lhs_layout.set_tensor(lhs_layout.get_tensor());
+                    }
+                    if (rhs_layout.is_dynamic() && rhs_layout.has_upper_bound()) {
+                        rhs_layout.set_tensor(rhs_layout.get_tensor());
+                    }
+
+                    if (rhs_layout.is_dynamic() && !rhs_layout.has_upper_bound() && lhs_layout.is_dynamic() && !lhs_layout.has_upper_bound()) {
+                        return po.get_processing_number(lhs.get()) < po.get_processing_number(rhs.get());
+                    }
+
+                    if (rhs_layout.is_dynamic())
+                        return true;
+                    if (lhs_layout.is_dynamic())
+                        return false;
+
+                    return (lhs_layout.bytes_count() > rhs_layout.bytes_count());
+            });
+
+    for (auto const& node : nodes_to_allocate) {
+        allocating_order.emplace_back(node->id());
+    }
+
+    return allocating_order;
+}
+
 void program::prepare_memory_dependencies() {
     if (!_config.get_property(ov::intel_gpu::enable_memory_pool))
         return;
@@ -1787,6 +1828,11 @@ void program::save(cldnn::BinaryOutputBuffer& ob) const {
         ob << p_info.is_cpu;
         ob << p_info.exec_id;
     }
+
+    ob << allocating_order.size();
+    for (auto const& node_id : allocating_order) {
+        ob << node_id;
+    }
 }
 
 void program::load(cldnn::BinaryInputBuffer& ib) {
@@ -1853,6 +1899,7 @@ void program::load(cldnn::BinaryInputBuffer& ib) {
 
     ib >> _is_body_program;
     ib >> _can_be_optimized;
+    _loaded_from_cache = true;
 
     get_processing_order().load(ib, *this);
 
@@ -1921,5 +1968,14 @@ void program::load(cldnn::BinaryInputBuffer& ib) {
         primitive_info p_info(original_id, type_id, c_dependencies, c_users, c_fused_ids,
                               output_layout, layout_str, kernel_id, runtime_precision, is_cpu, exec_id);
         prim_info.emplace_back(p_info);
+    }
+
+    size_t allocating_order_size;
+    ib >> allocating_order_size;
+    allocating_order.clear();
+    for (size_t i = 0; i < allocating_order_size; i++) {
+        primitive_id node_id;
+        ib >> node_id;
+        allocating_order.emplace_back(node_id);
     }
 }
