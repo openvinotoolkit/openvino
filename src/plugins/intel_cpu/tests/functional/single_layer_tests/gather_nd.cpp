c1c273b74ae0775c0701eb63d66fa460b106a6ce
Xuejun Zhai <xuejun.zhai@intel.com>
2023-12-07 21:19:46 +0800

    [CPU tests] migrate single layer test cases to be API 2.0 - part 4 (#21488)
    * [CPU tests] migrate single layer test cases to be API 2.0 - part 4
    
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>
    
    * [CPU tests] fix test error
    
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>
    
    * [CPU Func Test] revert single_layer_tests/group_convolution_backprop_data.cpp change
    
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>
    
    ---------
    
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>

--- src/plugins/intel_cpu/tests/functional/single_layer_tests/gather_nd.cpp
+++ src/plugins/intel_cpu/tests/functional/single_layer_tests/gather_nd.cpp
@@ -2,23 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include <shared_test_classes/single_layer/gather_nd.hpp>
 #include "shared_test_classes/base/ov_subgraph.hpp"
-#include "ov_models/builders.hpp"
 
-using namespace InferenceEngine;
-using namespace ov;
-using namespace test;
+namespace ov {
+namespace test {
 
-namespace CPULayerTestsDefinitions {
-
-using GatherNDLayerCPUTestParamSet = std::tuple<
-        InputShape,                                     // Input shapes
-        std::pair<Shape, std::vector<int>>,             // Indexes shape and values
-        ElementType,                                    // Input element type
-        ElementType,                                    // Indices element type
-        int                                             // Batch dims
->;
+using GatherNDLayerCPUTestParamSet = std::tuple<InputShape,                          // Input shapes
+                                                std::pair<Shape, std::vector<int>>,  // Indexes shape and values
+                                                ElementType,                         // Input element type
+                                                ElementType,                         // Indices element type
+                                                int                                  // Batch dims
+                                                >;
 
 class GatherNDLayerCPUTest : public testing::WithParamInterface<GatherNDLayerCPUTestParamSet>,
                              virtual public SubgraphBaseTest {
@@ -42,7 +36,7 @@ public:
         results << "BD=" << batchDims << "_";
 
         return results.str();
-}
+    }
 
 protected:
     void SetUp() override {
@@ -59,10 +53,10 @@ protected:
         for (auto&& shape : inputDynamicShapes) {
             params.push_back(std::make_shared<ov::op::v0::Parameter>(dataElementType, shape));
         }
-        auto indexes_node = ngraph::opset3::Constant::create(idxElementType, indexes.first, indexes.second);
-        auto gather_nd = std::make_shared<ngraph::opset5::GatherND>(params[0], indexes_node, batchDims);
-        ngraph::ResultVector results{std::make_shared<ngraph::opset3::Result>(gather_nd)};
-        function = std::make_shared<ngraph::Function>(results, params, "gatherND");
+        auto indexes_node = ov::op::v0::Constant::create(idxElementType, indexes.first, indexes.second);
+        auto gather_nd = std::make_shared<ov::op::v5::GatherND>(params[0], indexes_node, batchDims);
+        ov::ResultVector results{std::make_shared<ov::op::v0::Result>(gather_nd)};
+        function = std::make_shared<ov::Model>(results, params, "gatherND");
     }
 };
 
@@ -88,10 +82,10 @@ protected:
         for (auto&& shape : inputDynamicShapes) {
             params.push_back(std::make_shared<ov::op::v0::Parameter>(dataElementType, shape));
         }
-        auto indexes_node = ngraph::opset3::Constant::create(idxElementType, indexes.first, indexes.second);
-        auto gather_nd = std::make_shared<ngraph::opset8::GatherND>(params[0], indexes_node, batchDims);
-        ngraph::ResultVector results{std::make_shared<ngraph::opset3::Result>(gather_nd)};
-        function = std::make_shared<ngraph::Function>(results, params, "gatherND");
+        auto indexes_node = ov::op::v0::Constant::create(idxElementType, indexes.first, indexes.second);
+        auto gather_nd = std::make_shared<ov::op::v8::GatherND>(params[0], indexes_node, batchDims);
+        ov::ResultVector results{std::make_shared<ov::op::v0::Result>(gather_nd)};
+        function = std::make_shared<ov::Model>(results, params, "gatherND");
     }
 };
 
@@ -105,91 +99,100 @@ TEST_P(GatherND8LayerCPUTest, CompareWithRefs) {
 
 namespace {
 
-const std::vector<ElementType> inputPrecisions = {
-        ElementType::f32,
-        ElementType::bf16,
-        ElementType::i8
-};
+const std::vector<ElementType> inputPrecisions = {ElementType::f32, ElementType::bf16, ElementType::i8};
 
-const std::vector<ElementType> indexesPrecisions = {
-        ElementType::i32
-};
+const std::vector<ElementType> indexesPrecisions = {ElementType::i32};
 
 const std::vector<InputShape> inputShapesDynamicBD_0 = {
-        {{-1, -1, -1},                                       // dynamic
-         {{5, 10, 5}, {4, 12, 4}, {4, 12, 4}, {5, 5, 5}}},   // target
+    {{-1, -1, -1},                                      // dynamic
+     {{5, 10, 5}, {4, 12, 4}, {4, 12, 4}, {5, 5, 5}}},  // target
 
-        {{-1, 5, -1, -1},                                    // dynamic
-         {{8, 5, 5, 5}, {5, 5, 8, 4}, {4, 5, 4, 5}}},        // target
+    {{-1, 5, -1, -1},                              // dynamic
+     {{8, 5, 5, 5}, {5, 5, 8, 4}, {4, 5, 4, 5}}},  // target
 
-        {{{4, 10}, {5, 10}, {5, 10}, {5, 10}, {5, 10}},           // dynamic
-         {{4, 5, 5, 5, 5}, {4, 5, 5, 8, 5}, {10, 8, 5, 5, 5}}},   // target
+    {{{4, 10}, {5, 10}, {5, 10}, {5, 10}, {5, 10}},          // dynamic
+     {{4, 5, 5, 5, 5}, {4, 5, 5, 8, 5}, {10, 8, 5, 5, 5}}},  // target
 };
 
 const std::vector<std::pair<Shape, std::vector<int>>> indexesShapesBD_0 = {
-        std::pair<Shape, std::vector<int>>{{2, 2}, {3, 3, 2, 1}},
-        std::pair<Shape, std::vector<int>>{{1, 2, 3}, {0, 1, 1, 1, 0, 2}},
-        std::pair<Shape, std::vector<int>>{{2, 1, 1, 2}, {0, 2, 1, 1}},
+    std::pair<Shape, std::vector<int>>{{2, 2}, {3, 3, 2, 1}},
+    std::pair<Shape, std::vector<int>>{{1, 2, 3}, {0, 1, 1, 1, 0, 2}},
+    std::pair<Shape, std::vector<int>>{{2, 1, 1, 2}, {0, 2, 1, 1}},
 };
 
-const auto subset_BD0 = ::testing::Combine(
-        ::testing::ValuesIn(inputShapesDynamicBD_0),
-        ::testing::ValuesIn(indexesShapesBD_0),
-        ::testing::ValuesIn(inputPrecisions),
-        ::testing::ValuesIn(indexesPrecisions),
-        ::testing::Values(0));
+const auto subset_BD0 = ::testing::Combine(::testing::ValuesIn(inputShapesDynamicBD_0),
+                                           ::testing::ValuesIn(indexesShapesBD_0),
+                                           ::testing::ValuesIn(inputPrecisions),
+                                           ::testing::ValuesIn(indexesPrecisions),
+                                           ::testing::Values(0));
 
-INSTANTIATE_TEST_SUITE_P(smoke_GatherND5DynamicBD_0, GatherNDLayerCPUTest, subset_BD0, GatherNDLayerCPUTest::getTestCaseName);
-INSTANTIATE_TEST_SUITE_P(smoke_GatherND8DynamicBD_0, GatherND8LayerCPUTest, subset_BD0, GatherNDLayerCPUTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_GatherND5DynamicBD_0,
+                         GatherNDLayerCPUTest,
+                         subset_BD0,
+                         GatherNDLayerCPUTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_GatherND8DynamicBD_0,
+                         GatherND8LayerCPUTest,
+                         subset_BD0,
+                         GatherNDLayerCPUTest::getTestCaseName);
 
 const std::vector<InputShape> inputShapesDynamicBD_1 = {
-        {{3, -1, -1},                                            // dynamic
-         {{3, 10, 5}, {3, 10, 5}, {3, 12, 8}, {3, 8, 8}}},       // target
+    {{3, -1, -1},                                       // dynamic
+     {{3, 10, 5}, {3, 10, 5}, {3, 12, 8}, {3, 8, 8}}},  // target
 
-        {{3, {5, 10}, {5, 10}, {5, 10}, {5, 10}},                 // dynamic
-         {{3, 5, 5, 5, 5}, {3, 8, 10, 10, 10}, {3, 8, 6, 8, 7}}}, // target
+    {{3, {5, 10}, {5, 10}, {5, 10}, {5, 10}},                  // dynamic
+     {{3, 5, 5, 5, 5}, {3, 8, 10, 10, 10}, {3, 8, 6, 8, 7}}},  // target
 };
 
 const std::vector<std::pair<Shape, std::vector<int>>> indexesShapesBD_1 = {
-        std::pair<Shape, std::vector<int>>{{3, 2}, {0, 1, 2, 1, 0, 0}},
-        std::pair<Shape, std::vector<int>>{{3, 2, 2}, {0, 1, 1, 1, 0, 2, 0, 1, 1, 1, 0, 2}},
-        std::pair<Shape, std::vector<int>>{{3, 1, 1, 2}, {0, 2, 1, 1, 0, 2}},
+    std::pair<Shape, std::vector<int>>{{3, 2}, {0, 1, 2, 1, 0, 0}},
+    std::pair<Shape, std::vector<int>>{{3, 2, 2}, {0, 1, 1, 1, 0, 2, 0, 1, 1, 1, 0, 2}},
+    std::pair<Shape, std::vector<int>>{{3, 1, 1, 2}, {0, 2, 1, 1, 0, 2}},
 };
 
-const auto subset_BD1 = ::testing::Combine(
-        ::testing::ValuesIn(inputShapesDynamicBD_1),
-        ::testing::ValuesIn(indexesShapesBD_1),
-        ::testing::ValuesIn(inputPrecisions),
-        ::testing::ValuesIn(indexesPrecisions),
-        ::testing::Values(0));
+const auto subset_BD1 = ::testing::Combine(::testing::ValuesIn(inputShapesDynamicBD_1),
+                                           ::testing::ValuesIn(indexesShapesBD_1),
+                                           ::testing::ValuesIn(inputPrecisions),
+                                           ::testing::ValuesIn(indexesPrecisions),
+                                           ::testing::Values(0));
 
-INSTANTIATE_TEST_SUITE_P(smoke_GatherND5DynamicBD_1, GatherNDLayerCPUTest, subset_BD1, GatherNDLayerCPUTest::getTestCaseName);
-INSTANTIATE_TEST_SUITE_P(smoke_GatherND8DynamicBD_1, GatherND8LayerCPUTest, subset_BD1, GatherNDLayerCPUTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_GatherND5DynamicBD_1,
+                         GatherNDLayerCPUTest,
+                         subset_BD1,
+                         GatherNDLayerCPUTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_GatherND8DynamicBD_1,
+                         GatherND8LayerCPUTest,
+                         subset_BD1,
+                         GatherNDLayerCPUTest::getTestCaseName);
 
 const std::vector<InputShape> inputShapesDynamicBD_2 = {
-        {{2, 2, -1, -1, -1},                                                       // dynamic
-         {{2, 2, 5, 6, 5}, {2, 2, 2, 3, 3}, {2, 2, 2, 3, 3}, {2, 2, 7, 2, 3}}},    // target
+    {{2, 2, -1, -1, -1},                                                     // dynamic
+     {{2, 2, 5, 6, 5}, {2, 2, 2, 3, 3}, {2, 2, 2, 3, 3}, {2, 2, 7, 2, 3}}},  // target
 
-        {{2, 2, {5, 10}, {5, 10}, {5, 10}},                                        // dynamic
-         {{2, 2, 5, 5, 5}, {2, 2, 10, 10, 5}, {2, 2, 7, 8, 7}}},                   // target
+    {{2, 2, {5, 10}, {5, 10}, {5, 10}},                       // dynamic
+     {{2, 2, 5, 5, 5}, {2, 2, 10, 10, 5}, {2, 2, 7, 8, 7}}},  // target
 };
 
 const std::vector<std::pair<Shape, std::vector<int>>> indexesShapesBD_2 = {
-        std::pair<Shape, std::vector<int>>{{2, 2, 3}, {0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0}},
-        std::pair<Shape, std::vector<int>>{{2, 2, 2, 3}, {0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0,
-                                                                0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0}},
+    std::pair<Shape, std::vector<int>>{{2, 2, 3}, {0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0}},
+    std::pair<Shape, std::vector<int>>{{2, 2, 2, 3},
+                                       {0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0}},
 };
 
-const auto subset_BD2 = ::testing::Combine(
-        ::testing::ValuesIn(inputShapesDynamicBD_2),
-        ::testing::ValuesIn(indexesShapesBD_2),
-        ::testing::ValuesIn(inputPrecisions),
-        ::testing::ValuesIn(indexesPrecisions),
-        ::testing::Values(0));
+const auto subset_BD2 = ::testing::Combine(::testing::ValuesIn(inputShapesDynamicBD_2),
+                                           ::testing::ValuesIn(indexesShapesBD_2),
+                                           ::testing::ValuesIn(inputPrecisions),
+                                           ::testing::ValuesIn(indexesPrecisions),
+                                           ::testing::Values(0));
 
-INSTANTIATE_TEST_SUITE_P(smoke_GatherND5DynamicBD_2, GatherNDLayerCPUTest, subset_BD2, GatherNDLayerCPUTest::getTestCaseName);
-INSTANTIATE_TEST_SUITE_P(smoke_GatherND8DynamicBD_2, GatherND8LayerCPUTest, subset_BD2, GatherNDLayerCPUTest::getTestCaseName);
-
+INSTANTIATE_TEST_SUITE_P(smoke_GatherND5DynamicBD_2,
+                         GatherNDLayerCPUTest,
+                         subset_BD2,
+                         GatherNDLayerCPUTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_GatherND8DynamicBD_2,
+                         GatherND8LayerCPUTest,
+                         subset_BD2,
+                         GatherNDLayerCPUTest::getTestCaseName);
 
 }  // namespace
-} // namespace CPULayerTestsDefinitions
+}  // namespace test
+}  // namespace ov
--- src/plugins/intel_cpu/tests/functional/single_layer_tests/grn.cpp
+++ src/plugins/intel_cpu/tests/functional/single_layer_tests/grn.cpp
@@ -2,41 +2,33 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "shared_test_classes/base/ov_subgraph.hpp"
 #include "ov_models/builders.hpp"
+#include "shared_test_classes/base/ov_subgraph.hpp"
 #include "test_utils/cpu_test_utils.hpp"
 
 using namespace CPUTestUtils;
-using namespace ov::test;
+namespace ov {
+namespace test {
 
-namespace CPULayerTestsDefinitions {
-
-using GRNCPUTestParams = typename std::tuple<
-        ov::element::Type,             // Network precision
-        InferenceEngine::Precision,    // Input precision
-        InferenceEngine::Precision,    // Output precision
-        InferenceEngine::Layout,       // Input layout
-        InferenceEngine::Layout,       // Output layout
-        InputShape,                    // Input shape
-        float,                         // Bias
-        std::string>;                  // Device name
+using GRNCPUTestParams = typename std::tuple<ov::element::Type,  // Network type
+                                             ov::element::Type,  // Input type
+                                             ov::element::Type,  // Output type
+                                             InputShape,         // Input shape
+                                             float,              // Bias
+                                             std::string>;       // Device name
 
 class GRNLayerCPUTest : public testing::WithParamInterface<GRNCPUTestParams>,
-                        virtual public SubgraphBaseTest, public CPUTestsBase {
+                        virtual public SubgraphBaseTest,
+                        public CPUTestsBase {
 public:
     static std::string getTestCaseName(testing::TestParamInfo<GRNCPUTestParams> obj) {
         ov::element::Type netPrecision;
-        InferenceEngine::Precision inPrc, outPrc;
-        InferenceEngine::Layout inLayout, outLayout;
+        ov::element::Type inPrc, outPrc;
         InputShape inputShape;
         float bias;
         std::string targetDevice;
 
-        std::tie(netPrecision, inPrc, outPrc,
-                 inLayout, outLayout,
-                 inputShape,
-                 bias,
-                 targetDevice) = obj.param;
+        std::tie(netPrecision, inPrc, outPrc, inputShape, bias, targetDevice) = obj.param;
 
         std::ostringstream result;
         result << "IS=" << ov::test::utils::partialShape2str({inputShape.first}) << "_";
@@ -45,11 +37,9 @@ public:
             result << ov::test::utils::vec2str(item) << "_";
         }
         result << "netPRC=" << netPrecision.get_type_name() << "_";
-        result << "inPRC=" << inPrc.name() << "_";
-        result << "outPRC=" << outPrc.name() << "_";
-        result << "inL=" << inLayout << "_";
-        result << "outL=" << outLayout << "_";
-        result << "bias="   << bias << "_";
+        result << "inPRC=" << inPrc.get_type_name() << "_";
+        result << "outPRC=" << outPrc.get_type_name() << "_";
+        result << "bias=" << bias << "_";
         result << "trgDev=" << targetDevice;
 
         return result.str();
@@ -58,12 +48,11 @@ public:
 protected:
     void SetUp() override {
         ov::element::Type netPrecision;
-        InferenceEngine::Precision inPrc, outPrc;
-        InferenceEngine::Layout inLayout, outLayout;
+        ov::element::Type inPrc, outPrc;
         InputShape inputShape;
         float bias;
 
-        std::tie(netPrecision, inPrc, outPrc, inLayout, outLayout, inputShape, bias, targetDevice) = GetParam();
+        std::tie(netPrecision, inPrc, outPrc, inputShape, bias, targetDevice) = GetParam();
 
         init_input_shapes({inputShape});
 
@@ -71,9 +60,9 @@ protected:
         for (auto&& shape : inputDynamicShapes)
             paramsIn.push_back(std::make_shared<ov::op::v0::Parameter>(netPrecision, shape));
 
-        const auto grn = std::make_shared<ngraph::opset1::GRN>(paramsIn[0], bias);
-        const ngraph::ResultVector results{std::make_shared<ngraph::opset1::Result>(grn)};
-        function = std::make_shared<ngraph::Function>(results, paramsIn, "Grn");
+        const auto grn = std::make_shared<ov::op::v0::GRN>(paramsIn[0], bias);
+        const ov::ResultVector results{std::make_shared<ov::op::v0::Result>(grn)};
+        function = std::make_shared<ov::Model>(results, paramsIn, "Grn");
     }
 };
 
@@ -83,43 +72,36 @@ TEST_P(GRNLayerCPUTest, CompareWithRefs) {
 
 namespace {
 
-const std::vector<ov::element::Type> netPrecisions = {
-        ov::element::bf16,
-        ov::element::f16,
-        ov::element::f32
-};
+const std::vector<ov::element::Type> netPrecisions = {ov::element::bf16, ov::element::f16, ov::element::f32};
 
 const std::vector<float> biases = {1e-6f, 0.33f, 1.1f, 2.25f, 100.25f};
 
 const std::vector<InputShape> dataInputStaticShapes = {{{}, {{16, 24}}}, {{}, {{3, 16, 24}}}, {{}, {{1, 3, 30, 30}}}};
 
-const std::vector<InputShape> dataInputDynamicShapes =
-    {{{-1, -1}, {{5, 17}, {10, 3}}}, {{3, {10, 12}, -1}, {{3, 12, 25}, {3, 10, 10}}},
-     {{2, -1, -1, {5, 10}}, {{2, 17, 20, 7}, {2, 10, 12, 5}}}};
+const std::vector<InputShape> dataInputDynamicShapes = {{{-1, -1}, {{5, 17}, {10, 3}}},
+                                                        {{3, {10, 12}, -1}, {{3, 12, 25}, {3, 10, 10}}},
+                                                        {{2, -1, -1, {5, 10}}, {{2, 17, 20, 7}, {2, 10, 12, 5}}}};
 
-INSTANTIATE_TEST_SUITE_P(smoke_GRNCPUStatic, GRNLayerCPUTest,
-                        ::testing::Combine(
-                            ::testing::ValuesIn(netPrecisions),
-                            ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
-                            ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
-                            ::testing::Values(InferenceEngine::Layout::ANY),
-                            ::testing::Values(InferenceEngine::Layout::ANY),
-                            ::testing::ValuesIn(dataInputStaticShapes),
-                            ::testing::ValuesIn(biases),
-                            ::testing::Values(ov::test::utils::DEVICE_CPU)),
-                        GRNLayerCPUTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_GRNCPUStatic,
+                         GRNLayerCPUTest,
+                         ::testing::Combine(::testing::ValuesIn(netPrecisions),
+                                            ::testing::Values(ov::element::undefined),
+                                            ::testing::Values(ov::element::undefined),
+                                            ::testing::ValuesIn(dataInputStaticShapes),
+                                            ::testing::ValuesIn(biases),
+                                            ::testing::Values(ov::test::utils::DEVICE_CPU)),
+                         GRNLayerCPUTest::getTestCaseName);
 
-INSTANTIATE_TEST_SUITE_P(smoke_GRNCPUDynamic, GRNLayerCPUTest,
-                        ::testing::Combine(
-                            ::testing::ValuesIn(netPrecisions),
-                            ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
-                            ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
-                            ::testing::Values(InferenceEngine::Layout::ANY),
-                            ::testing::Values(InferenceEngine::Layout::ANY),
-                            ::testing::ValuesIn(dataInputDynamicShapes),
-                            ::testing::ValuesIn(biases),
-                            ::testing::Values(ov::test::utils::DEVICE_CPU)),
-                        GRNLayerCPUTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_GRNCPUDynamic,
+                         GRNLayerCPUTest,
+                         ::testing::Combine(::testing::ValuesIn(netPrecisions),
+                                            ::testing::Values(ov::element::undefined),
+                                            ::testing::Values(ov::element::undefined),
+                                            ::testing::ValuesIn(dataInputDynamicShapes),
+                                            ::testing::ValuesIn(biases),
+                                            ::testing::Values(ov::test::utils::DEVICE_CPU)),
+                         GRNLayerCPUTest::getTestCaseName);
 
-} // namespace
-} // namespace CPULayerTestsDefinitions
+}  // namespace
+}  // namespace test
+}  // namespace ov
