697903b383f69353b91052c4c1b6d812b8a3ba0f
Anastasia Kuporosova <anastasia.kuporosova@intel.com>
2023-12-07 14:08:07 +0100

    Update src/bindings/python/tests/test_runtime/test_async_infer_request.py

--- src/bindings/python/tests/test_runtime/test_async_infer_request.py
+++ src/bindings/python/tests/test_runtime/test_async_infer_request.py
@@ -362,34 +362,3 @@ def test_compiled_model_share_memory(device, share_inputs, share_outputs, is_pos
         assert results[0].flags["OWNDATA"] is True
 
 
-@pytest.mark.parametrize("share_inputs", [True, False])
-@pytest.mark.parametrize("share_outputs", [True, False])
-@pytest.mark.parametrize("is_positional", [True, False])
-def test_infer_request_share_memory(device, share_inputs, share_outputs, is_positional):
-    _, request, _, input_data = abs_model_with_data(
-        device, Type.f32, np.float32)
-
-    if is_positional:
-        results = request.infer(
-            input_data, share_inputs=share_inputs, share_outputs=share_outputs)
-    else:
-        results = request.infer(input_data, share_inputs, share_outputs)
-
-    assert np.array_equal(results[0], np.abs(input_data))
-
-    in_tensor_shares = np.shares_memory(
-        request.get_input_tensor(0).data, input_data)
-
-    if share_inputs:
-        assert in_tensor_shares
-    else:
-        assert not in_tensor_shares
-
-    out_tensor_shares = np.shares_memory(
-        request.get_output_tensor(0).data, results[0])
-    if share_outputs:
-        assert out_tensor_shares
-        assert results[0].flags["OWNDATA"] is False
-    else:
-        assert not out_tensor_shares
-        assert results[0].flags["OWNDATA"] is True
