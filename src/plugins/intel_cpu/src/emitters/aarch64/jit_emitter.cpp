73c8fbb4275db487e7cd711dd6e245f3d700567d
eshoguli <edward.shogulin@intel.com>
2023-12-07 01:31:22 +0100

    context saving & loading

--- src/plugins/intel_cpu/src/emitters/aarch64/jit_emitter.cpp
+++ src/plugins/intel_cpu/src/emitters/aarch64/jit_emitter.cpp
@@ -14,15 +14,11 @@ namespace intel_cpu {
 namespace aarch64 {
 
 const std::vector<uint32_t> jit_emitter::save_gpr_regs = {
-    0, 1, 2, 3, 4, 5, 6, 7,
-    8, 9, 10, 11, 12, 13, 14, 15,
-    16, 17, 18, 31
+    9, 10
 };
 
 const std::vector<uint32_t> jit_emitter::save_v_regs = {
-    0, 1, 2, 3, 4, 5, 6, 7,
-    16, 17, 18, 19, 20, 21, 22, 23,
-    24, 25, 26, 27, 28, 29, 30, 31
+    10, 11, 16, 17, 18, 19, 20, 21, 22, 23
 };
 
 void jit_emitter::emit_code(const std::vector<size_t> &in_idxs,
