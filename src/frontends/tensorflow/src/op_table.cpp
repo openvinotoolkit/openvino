5e4400388b3c86d26fc49f15854fb41896de8b18
Ahmad Chalhoub <84783887+ahmadchalhoub@users.noreply.github.com>
2023-12-07 11:31:41 -0500

    support MulNoNan for TF (#21459)
    * support MulNoNan for TF
    
    * update test_tf_MulNoNan
    
    * Update src/frontends/tensorflow_common/src/op/mul_no_nan.cpp
    
    * update zeros const method
    
    * Update src/frontends/tensorflow_common/src/op/mul_no_nan.cpp
    
    * Update src/frontends/tensorflow_common/src/op/mul_no_nan.cpp
    
    * Update src/frontends/tensorflow_common/src/op/mul_no_nan.cpp
    
    * Update src/frontends/tensorflow_common/src/op/mul_no_nan.cpp
    
    * Update src/frontends/tensorflow_common/src/op/mul_no_nan.cpp
    
    * Update tests/layer_tests/tensorflow_tests/test_tf_MulNoNan.py
    
    ---------
    
    Co-authored-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- src/frontends/tensorflow/src/op_table.cpp
+++ src/frontends/tensorflow/src/op_table.cpp
@@ -232,6 +232,7 @@ const std::map<std::string, CreatorFunction> get_supported_ops() {
         {"MaxPoolWithArgmax", CreatorFunction(translate_max_pool_with_argmax)},
         {"Merge", CreatorFunction(translate_merge_op)},
         {"MirrorPad", CreatorFunction(translate_mirror_pad_op)},
+        {"MulNoNan", CreatorFunction(translate_mul_no_nan_op)},
         {"MutableHashTable", CreatorFunction(translate_hash_table_op)},
         {"MutableHashTableV2", CreatorFunction(translate_hash_table_op)},
         {"NonMaxSuppression", CreatorFunction(translate_non_max_suppression_op)},
