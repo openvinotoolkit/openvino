7dabfcd22444af72e21a9599406a4fc6b7168856
Sergey Shlyapnikov <sergey.shlyapnikov@intel.com>
2023-12-07 20:04:35 +0400

    [GPU] Improve fake alignment logic and fix missed zero point value

--- src/plugins/intel_gpu/src/graph/fully_connected.cpp
+++ src/plugins/intel_gpu/src/graph/fully_connected.cpp
@@ -177,12 +177,15 @@ kernel_impl_params fully_connected_inst::get_fake_aligned_params(kernel_impl_par
     if (orig_input_layout.format == format::bfyx && orig_output_layout.format == format::bfyx) {
         auto updated_param = orig_impl_param;
         auto input_shape = orig_input_layout.get_partial_shape().to_shape();
-        auto input_row_idx = input_shape.size() - 2;
         auto output_shape = orig_output_layout.get_partial_shape().to_shape();
-        auto output_row_idx = output_shape.size() - 2;
+
+        auto batch_size = std::accumulate(input_shape.begin(),
+                                          input_shape.end() - 1,
+                                          size_t{1},
+                                          std::multiplies<size_t>());
 
         // Vector by matrix multiplication sometimes works slower if we align it
-        if (input_shape[input_row_idx] == 1 && output_shape[output_row_idx] == 1 && input_shape[input_shape.size() - 1] >= 1024) {
+        if (batch_size == 1 && input_shape.back() >= 1024) {
             return std::move(orig_impl_param);
         }
 
@@ -190,12 +193,15 @@ kernel_impl_params fully_connected_inst::get_fake_aligned_params(kernel_impl_par
         if (orig_impl_param.dev_type == cldnn::device_type::integrated_gpu) {
             auto weights_layout_dt = orig_impl_param.weights_layout.value().data_type;
             auto is_4bit = weights_layout_dt == data_types::i4 || weights_layout_dt == data_types::u4;
-            auto is_extra_alignment_needed = output_shape[output_row_idx] >= 256;
+            auto is_extra_alignment_needed = batch_size >= 256;
             fake_align_base = is_4bit && is_extra_alignment_needed ? 64 : 16;
         }
 
-        input_shape[input_row_idx] = align_to(input_shape[input_row_idx], fake_align_base);
-        output_shape[output_row_idx] = align_to(output_shape[output_row_idx], fake_align_base);
+        std::fill(input_shape.begin(), input_shape.end() - 1, 1);
+        std::fill(output_shape.begin(), output_shape.end() - 1, 1);
+
+        input_shape[0] = align_to(batch_size, fake_align_base);
+        output_shape[0] = align_to(batch_size, fake_align_base);
 
         updated_param.input_layouts[0] = layout(ov::PartialShape(input_shape),
                                                 orig_input_layout.data_type,
