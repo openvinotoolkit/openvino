c86a1a404264d349c3209cd54f52bb0446eb7b8d
Georgy Krivoruchko <georgy.krivoruchko@intel.com>
2023-12-08 23:06:15 -0800

    [ONNX] Preparing environment for moving ONNX tests to API2.0 (#21528)
    * Added common functions for onnx tests
    * Removed test cases which are non-related to ONNX
    Extended convert_model by extension
    * Extended test utils by convert_partially
    * Added model convertion using istream as input
    * Moved common functionality to a ov namespace
    * Removed unused function

--- src/frontends/onnx/onnx_common/include/onnx_common/parser.hpp
+++ src/frontends/onnx/onnx_common/include/onnx_common/parser.hpp
@@ -3,24 +3,23 @@
 //
 
 #pragma once
+#include <onnx/onnx_pb.h>
+
 #include <fstream>
 #include <string>
 
-/// \ingroup ngraph_cpp_api
-namespace ONNX_NAMESPACE {
-class ModelProto;
-}
-
-namespace ngraph {
+namespace ov {
+namespace frontend {
 namespace onnx_common {
+using namespace ::ONNX_NAMESPACE;
 /// \brief   Parses an ONNX model from a file located on a storage device.
 ///
 /// \param   file_path    Path to the file containing an ONNX model.
 ///
 /// \return  The parsed in-memory representation of the ONNX model
-ONNX_NAMESPACE::ModelProto parse_from_file(const std::string& file_path);
+ModelProto parse_from_file(const std::string& file_path);
 #if defined(OPENVINO_ENABLE_UNICODE_PATH_SUPPORT) && defined(_WIN32)
-ONNX_NAMESPACE::ModelProto parse_from_file(const std::wstring& file_path);
+ModelProto parse_from_file(const std::wstring& file_path);
 #endif
 
 /// \brief   Parses an ONNX model from a stream (representing for example a file)
@@ -28,7 +27,13 @@ ONNX_NAMESPACE::ModelProto parse_from_file(const std::wstring& file_path);
 /// \param   model_stream  Path to the file containing an ONNX model.
 ///
 /// \return  The parsed in-memory representation of the ONNX model
-ONNX_NAMESPACE::ModelProto parse_from_istream(std::istream& model_stream);
+ModelProto parse_from_istream(std::istream& model_stream);
 }  // namespace onnx_common
+}  // namespace frontend
+}  // namespace ov
 
+namespace ngraph {
+namespace onnx_common {
+using namespace ov::frontend::onnx_common;
+}
 }  // namespace ngraph
