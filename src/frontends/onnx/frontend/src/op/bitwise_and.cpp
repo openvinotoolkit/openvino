de75fb5fbc285a7f488c6d494e0b8adbc5f77561
rghvsh <116428320+rghvsh@users.noreply.github.com>
2023-12-11 13:22:20 +0530

    Update bitwise_and.cpp

--- src/frontends/onnx/frontend/src/op/bitwise_and.cpp
+++ src/frontends/onnx/frontend/src/op/bitwise_and.cpp
@@ -1,1 +1,30 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
 
+
+#include "op/bitwise_and.hpp"
+#include "default_opset.hpp"
+
+OPENVINO_SUPPRESS_DEPRECATED_START
+using namespace ov::op;
+
+namespace ngraph {
+namespace onnx_import {
+namespace op {
+namespace set_1 {
+OutputVector bitwise_and(const Node& node) {
+    const Output<ngraph::Node> a = node.get_ng_inputs().at(0);
+    const Output<ngraph::Node> b = node.get_ng_inputs().at(1);
+
+     return {std::make_shared<v13::BitwiseAnd>(a, b)};
+
+}
+
+}  // namespace set_1
+
+}  // namespace op
+
+}  // namespace onnx_import
+
+}  // namespace ngraph
