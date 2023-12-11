4b8e03f99f044c7788b8952d64f82f205add0c4b
vurusovs <vitaliy.urusovskij@intel.com>
2023-12-11 15:16:47 +0400

    Fix

--- src/common/low_precision_transformations/tests/space_to_batch_transformation.cpp
+++ src/common/low_precision_transformations/tests/space_to_batch_transformation.cpp
@@ -11,7 +11,6 @@
 
 #include <transformations/utils/utils.hpp>
 #include <transformations/init_node_info.hpp>
-#include <ov/opsets/opset2.hpp>
 #include <low_precision/space_to_batch.hpp>
 
 #include "common_test_utils/ov_test_utils.hpp"
