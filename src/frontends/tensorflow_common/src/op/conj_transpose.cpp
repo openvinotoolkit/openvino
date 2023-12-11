27833f348a691c4c243ecd38120c36460402c959
Abdulrahman Adel Ibrahim <57441828+Abdulrahman-Adel@users.noreply.github.com>
2023-12-11 22:44:35 +0000

    conj transpose

---
+++ src/frontends/tensorflow_common/src/op/conj_transpose.cpp
@@ -0,0 +1,56 @@
+#include "helper_ops/complex_type_mark.hpp"
+#include "openvino/op/transpose.hpp"
+#include "openvino/op/convert.hpp"
+#include "openvino/op/multiply.hpp"
+#include "openvino/op/concat.hpp"
+#include "common_op_table.hpp"
+
+using namespace std;
+using namespace ov::op;
+
+namespace ov {
+namespace frontend {
+namespace tensorflow {
+namespace op {
+
+OutputVector translate_conj_transpose_op(const NodeContext& node){
+	default_op_checks(node, 1, {"conj_transpose"});
+
+	auto op_type = node.get_op_type();
+	auto x = node.get_input(0);
+	auto perm = node.get_input(1);
+	
+	
+	auto complex_type_mark = as_type_ptr<ComplexTypeMark>(x.get_node_shared_ptr());
+    TENSORFLOW_OP_VALIDATION(
+            node,
+            complex_type_mark,
+            "[TensorFlow Frontend] internal error: ComplexTypeMark is not set at the input of " + op_type);
+    auto data = complex_type_mark->input_value(0);
+
+
+    auto real_index = make_shared<v0::Constant>(element::i32, Shape{}, 0);
+	auto imag_index = make_shared<v0::Constant>(element::i32, Shape{}, 1);
+
+    auto gather_axis = make_shared<v0::Constant>(element::i32, Shape{1}, -1);
+	
+	auto real = make_shared<v8::Gather>(data, real_index, gather_axis);
+    auto imag = make_shared<v8::Gather>(data, imag_index, gather_axis);
+
+	
+	auto const_minus_one = make_shared<v0::Constant>(element::i32, Shape{}, -1);
+	auto imag = make_shared<v1::Multiply>(imag, const_minus_one);
+	
+
+	auto conj_tensor = make_shared<v0::Concat>(OutputVector{real, imag}, -1)->output(0);
+
+	auto conj_transpose = make_shared<v1::Transpose>(conj_tensor, perm);
+
+	set_node_name(node.get_name(), conj_transpose);
+	return {conj_transpose};
+    }
+}
+}
+}
+}
+	
