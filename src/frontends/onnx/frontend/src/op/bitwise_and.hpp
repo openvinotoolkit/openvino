2e862848f7d09e3f662fd55c8319b0d308d71a87
rghvsh <116428320+rghvsh@users.noreply.github.com>
2023-12-11 13:29:45 +0530

    Update bitwise_and.hpp

--- src/frontends/onnx/frontend/src/op/bitwise_and.hpp
+++ src/frontends/onnx/frontend/src/op/bitwise_and.hpp
@@ -7,8 +7,6 @@
 #include "openvino/core/deprecated.hpp"
 OPENVINO_SUPPRESS_DEPRECATED_START
 
-#include <memory>
-
 #include "ngraph/node.hpp"
 #include "onnx_import/core/node.hpp"
 
@@ -19,7 +17,6 @@ namespace set_1 {
 OutputVector bitwise_and(const Node& node);
 
 }  // namespace set_1
-
 }  // namespace op
 
 }  // namespace onnx_import
