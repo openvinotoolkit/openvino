5c9fe4f8362abb1b4db659addbba9f5ee03cbe82
Maxim Vafin <maxim.vafin@intel.com>
2023-12-11 12:04:10 +0100

    Fix constant type in aten::square

--- src/frontends/pytorch/src/op/square.cpp
+++ src/frontends/pytorch/src/op/square.cpp
@@ -5,6 +5,7 @@
 #include "openvino/frontend/pytorch/node_context.hpp"
 #include "openvino/op/constant.hpp"
 #include "openvino/op/power.hpp"
+#include "openvino/op/convert_like.hpp"
 #include "utils.hpp"
 
 namespace ov {
@@ -17,7 +18,8 @@ using namespace ov::op;
 OutputVector translate_square(const NodeContext& context) {
     num_inputs_check(context, 1, 1);
     auto input_0 = context.get_input(0);
-    auto const_2 = context.mark_node(v0::Constant::create(input_0.get_element_type(), Shape{1}, {2}));
+    auto const_2 = context.mark_node(v0::Constant::create(element::i32, Shape{1}, {2}));
+    const_2 = context.mark_node(std::make_shared<v1::ConvertLike>(const_2, input_0));
     return {context.mark_node(std::make_shared<v1::Power>(input_0, const_2))};
 };
 
