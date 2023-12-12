6e98c2658d7ca09a5ad1aa2b78fe74aa78b52e7c
Ahmad <chalhoah@gmail.com>
2023-12-12 00:21:24 -0500

    fix code style, use space 4

--- src/frontends/pytorch/src/op/erfc.cpp
+++ src/frontends/pytorch/src/op/erfc.cpp
@@ -4,7 +4,6 @@
 
 #include "openvino/frontend/pytorch/node_context.hpp"
 #include "openvino/op/convert.hpp"
-#include "openvino/op/convert_like.hpp"
 #include "openvino/op/erf.hpp"
 #include "openvino/op/subtract.hpp"
 #include "utils.hpp"
@@ -18,28 +17,26 @@ namespace pytorch {
 namespace op {
 
 OutputVector translate_erfc(const NodeContext& context) {
-  // aten::erf(Tensor self) -> Tensor
-  // aten::erf.out(Tensor self, Tensor(!a) out) -> Tensor(!a)
-  num_inputs_check(context, 1, 2);
-  auto x = context.get_input(0);
+    // aten::erf(Tensor self) -> Tensor
+    // aten::erf.out(Tensor self, Tensor(!a) out) -> Tensor(!a)
+    num_inputs_check(context, 1, 2);
+    auto x = context.get_input(0);
 
-  // create 'ones' to use to calculate complementary of Erf output
-  auto ones =
-      context.mark_node(make_shared<v0::Constant>(element::f32, Shape{}, 1.0f))
-          ->output(0);
+    // create 'ones' to use to calculate complementary of Erf output
+    auto ones = context.mark_node(make_shared<v0::Constant>(element::f32, Shape{}, 1.0f))->output(0);
 
-  // align data types of input 'x' and ones
-  align_eltwise_input_types(context, x, ones);
+    // align data types of input 'x' and ones
+    align_eltwise_input_types(context, x, ones);
 
-  // apply Erf to the input tensor 'x'
-  auto y = context.mark_node(make_shared<v0::Erf>(x));
+    // apply Erf to the input tensor 'x'
+    auto y = context.mark_node(make_shared<v0::Erf>(x));
 
-  y = context.mark_node(make_shared<v1::Subtract>(ones, y));
+    y = context.mark_node(make_shared<v1::Subtract>(ones, y));
 
-  if (!context.input_is_none(1)) {
-    context.mutate_input(1, y);
-  }
-  return {y};
+    if (!context.input_is_none(1)) {
+        context.mutate_input(1, y);
+    }
+    return {y};
 };
 
 }  // namespace op
