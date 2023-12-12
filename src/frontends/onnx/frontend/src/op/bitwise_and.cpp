9458460d90bf34a4c632ada3dad432755fa978f7
rghvsh <116428320+rghvsh@users.noreply.github.com>
2023-12-12 22:05:44 +0530

    Update bitwise_and.cpp
    Corrects code style

--- src/frontends/onnx/frontend/src/op/bitwise_and.cpp
+++ src/frontends/onnx/frontend/src/op/bitwise_and.cpp
@@ -10,20 +10,24 @@ OPENVINO_SUPPRESS_DEPRECATED_START
 using namespace ov::op;
 
 namespace ngraph {
-namespace onnx_import {
-namespace op {
-namespace set_1 {
-OutputVector bitwise_and(const Node& node) {
-    const auto inputs = node.get_ng_inputs();
-    OPENVINO_ASSERT(inputs.size() == 2);   
-    return {std::make_shared<v13::BitwiseAnd>(inputs[0], inputs[1])};
+	namespace onnx_import {
+		namespace op {
+			namespace set_1 {
+				OutputVector bitwise_and(const Node& node) {
+					const Output<ngraph::Node> a = node.get_ng_inputs().at(0);
+					const Output<ngraph::Node> b = node.get_ng_inputs().at(1);
 
-}
+					NGRAPH_CHECK(a.get_element_type() != ov::element::bf16 && b.get_element_type() != ov::element::bf16,
+						"The input data bfloat16 isn't supported in opset 12");
 
-}  // namespace set_1
+					return { std::make_shared<v13::BitwiseAnd>(a, b) };
 
-}  // namespace op
+				}
 
-}  // namespace onnx_import
+			}  // namespace set_1
+
+		}  // namespace op
+
+	}  // namespace onnx_import
 
 }  // namespace ngraph
