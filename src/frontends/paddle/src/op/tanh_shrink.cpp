e141eff748e63e0661b503c304d112af6c695323
Asthestarsfalll <72954905+Asthestarsfalll@users.noreply.github.com>
2023-12-12 12:49:49 +0800

    【Hackathon 5th No.97】add paddle tanh_shrink op  (#20067)
    * Add paddle tanh_shrink conversion
    
    * update
    
    * update
    
    * restore CMakeLists.txt and waitting for PD test build enabled
    
    * update for unittests
    
    * remove code
    
    * Add paddle tanh_shrink conversion

---
+++ src/frontends/paddle/src/op/tanh_shrink.cpp
@@ -0,0 +1,21 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "default_opset.hpp"
+#include "openvino/frontend/paddle/node_context.hpp"
+
+namespace ov {
+namespace frontend {
+namespace paddle {
+namespace op {
+NamedOutputs tanh_shrink(const NodeContext& node) {
+    const auto x = node.get_input("X");
+    const auto tanh = std::make_shared<default_opset::Tanh>(x);
+    return node.default_single_output_mapping({std::make_shared<default_opset::Subtract>(x, tanh)}, {"Out"});
+}
+
+}  // namespace op
+}  // namespace paddle
+}  // namespace frontend
+}  // namespace ov
