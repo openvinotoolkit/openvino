4d432d17579aa8b28c168b1fad6abd80ea1991ca
Piotr Krzemiński <piotr.krzeminski@intel.com>
2023-12-12 12:36:48 +0100

    Update src/common/transformations/src/transformations/convert_precision.cpp
    Co-authored-by: Mateusz Tabaka <mateusz.tabaka@intel.com>

--- src/common/transformations/src/transformations/convert_precision.cpp
+++ src/common/transformations/src/transformations/convert_precision.cpp
@@ -856,7 +856,6 @@ bool fuse_type_to_multinomial_v13(const std::shared_ptr<ov::Node>& node, const p
 
     return update_type(0, node, precisions, [&](const element::Type& type) {
         multinomial->set_convert_type(ov::element::i32);
-        multinomial->set_output_type(0, type, multinomial->get_output_partial_shape(0));
     });
 }
 
