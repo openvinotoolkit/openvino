eb0344588222b281c3e63d11c1e786e3fd47aa78
sunxiaoxia2022 <xiaoxia.sun@intel.com>
2023-12-13 14:14:08 +0800

    restore the properties 1.0

--- src/inference/dev_api/openvino/runtime/threading/istreams_executor.hpp
+++ src/inference/dev_api/openvino/runtime/threading/istreams_executor.hpp
@@ -110,6 +110,8 @@ public:
          * @return configured values
          */
         static Config make_default_multi_threaded(const Config& initial, const bool fp_intesive = true);
+        static int get_default_num_streams(
+            const bool enable_hyper_thread = true);  // no network specifics considered (only CPU's caps);
         static void update_hybrid_custom_threads(Config& config);
 
         /**
@@ -136,6 +138,7 @@ public:
         int _threads_per_stream_big = 0;    //!< Threads per stream in big cores
         int _threads_per_stream_small = 0;  //!< Threads per stream in small cores
         int _small_core_offset = 0;         //!< Calculate small core start offset when binding cpu cores
+        bool _enable_hyper_thread = true;   //!< enable hyper thread
         enum PreferredCoreType {
             ANY,
             LITTLE,
