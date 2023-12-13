5623da1e956f4b83229c4ac914e03fc220769ee1
Zhai, Xuejun <xuejun.zhai@intel.com>
2023-12-13 16:09:33 +0800

    [CPU tests] fix enforce bf16 error
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/core_config.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/core_config.cpp
@@ -23,11 +23,6 @@ namespace ov {
 namespace test {
 
 void core_configuration(ov::test::SubgraphBaseTest* test) {
-    #if defined(OPENVINO_ARCH_X86) || defined(OPENVINO_ARCH_X86_64)
-        if (!test->configuration.count(InferenceEngine::PluginConfigParams::KEY_ENFORCE_BF16)) {
-            test->configuration.insert({InferenceEngine::PluginConfigParams::KEY_ENFORCE_BF16, InferenceEngine::PluginConfigParams::NO});
-        }
-    #endif
     #if defined(OV_CPU_ARM_ENABLE_FP16)
         //force fp32 inference precision if it is not configured specially
         if (!test->configuration.count(ov::hint::inference_precision.name())) {
