1a87177ece767422cbd2538c26e2c99665e84151
rghvsh <116428320+rghvsh@users.noreply.github.com>
2023-12-14 19:38:31 +0530

    Update bitwise_and.cpp

--- src/frontends/onnx/frontend/src/op/bitwise_and.cpp
+++ src/frontends/onnx/frontend/src/op/bitwise_and.cpp
@@ -2,23 +2,23 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "default_opset.hpp"
 #include "op/bitwise_and.hpp"
-
 OPENVINO_SUPPRESS_DEPRECATED_START
+
+#include "default_opset.hpp"
+
 using namespace ov::op;
 
 namespace ngraph {
 namespace onnx_import {
 namespace op {
 namespace set_1 {
-OutputVector bitwise_and(const Node &node) {
-  const auto inputs = node.get_ng_inputs();
-  OPENVINO_ASSERT(inputs.size() == 2);
-  return {std::make_shared<v13::BitwiseAnd>(inputs[0], inputs[1])};
+OutputVector bitwise_and(const Node& node) {
+    const auto inputs = node.get_ng_inputs();
+    OPENVINO_ASSERT(inputs.size() == 2);
+    return {std::make_shared<v13::BitwiseAnd>(inputs[0], inputs[1])};
 }
-} // namespace set_1
-} // namespace op
-} // namespace onnx_import
-} // namespace ngraph
-OPENVINO_SUPPRESS_DEPRECATED_END
+}  // namespace set_1
+}  // namespace op
+}  // namespace onnx_import
+}  // namespace ngraph
