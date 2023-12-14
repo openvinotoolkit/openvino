3b3a5154cf99b5c6707968bfcf53012c9852bc9e
Oleg Pipikin <oleg.pipikin@intel.com>
2023-12-14 18:12:12 +0400

    Apply comments

--- src/plugins/intel_cpu/src/compiled_model.h
+++ src/plugins/intel_cpu/src/compiled_model.h
@@ -38,7 +38,8 @@ public:
     ov::Any get_property(const std::string& name) const override;
 
     void set_property(const ov::AnyMap& properties) override {
-        OPENVINO_THROW_NOT_IMPLEMENTED("It's not possible to set property of compiled model. Set property for entire Core before compiling the model");
+        OPENVINO_THROW_NOT_IMPLEMENTED("It's not possible to set property of an already compiled model. "
+                                       "Set property to Core::compile_model during compilation");
     };
 
 private:
