b1840aac198dadbfcd42f9796711056c55d2a8b5
Tikhonov Ivan <ivan.tikhonov@intel.com>
2023-12-14 16:17:23 +0330

    Add reset_state method to Python API

--- src/bindings/python/src/pyopenvino/core/infer_request.cpp
+++ src/bindings/python/src/pyopenvino/core/infer_request.cpp
@@ -613,6 +613,19 @@ void regclass_InferRequest(py::module m) {
         )");
 
     cls.def(
+            "reset_state",
+            [](InferRequestWrapper& self) {
+                return self.m_request.reset_state();
+            },
+            py::call_guard<py::gil_scoped_release>(),
+            R"(
+            Resets all internal variable states for relevant infer request to
+            a value specified as default for the corresponding `ReadValue` node
+
+            GIL is released while running this function.
+        )");
+
+    cls.def(
         "get_compiled_model",
         [](InferRequestWrapper& self) {
             return self.m_request.get_compiled_model();
