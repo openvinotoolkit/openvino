7d2f895887d5b1584fc27fa1efd1609c1f5377f8
Zhai, Xuejun <xuejun.zhai@intel.com>
2023-12-14 19:38:46 +0800

    [CPU tests] fix review comments
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/core_config.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/core_config.cpp
@@ -23,7 +23,7 @@ namespace ov {
 namespace test {
 
 void core_configuration(ov::test::SubgraphBaseTest* test) {
-    #if defined(OV_CPU_ARM_ENABLE_FP16)
+    #if defined(OV_CPU_ARM_ENABLE_FP16) || defined(OPENVINO_ARCH_X86) || defined(OPENVINO_ARCH_X86_64)
         //force fp32 inference precision if it is not configured specially
         if (!test->configuration.count(ov::hint::inference_precision.name())) {
             test->configuration.insert({ov::hint::inference_precision.name(), ov::element::f32.to_string()});
