d98abe30a56f90b3ddf0c8d49c8ce14150ab2bf7
Vitaliy Urusovskij <vitaliy.urusovskij@intel.com>
2023-12-20 01:03:33 +0400

    Shared `onnx` tests to API2.0 (#21726)

--- src/tests/functional/plugin/shared/include/onnx/quantized_models_tests.hpp
+++ src/tests/functional/plugin/shared/include/onnx/quantized_models_tests.hpp
@@ -5,19 +5,19 @@
 #pragma once
 
 #include <string>
-#include "shared_test_classes/base/layer_test_utils.hpp"
+#include "shared_test_classes/base/ov_subgraph.hpp"
 
 namespace ONNXTestsDefinitions {
 
 class QuantizedModelsTests : public testing::WithParamInterface<std::string>,
-                            virtual public LayerTestsUtils::LayerTestsCommon {
+                             virtual public ov::test::SubgraphBaseStaticTest {
 public:
     static std::string getTestCaseName(const testing::TestParamInfo<std::string>& obj);
 
 protected:
     void SetUp() override;
-    using LayerInputTypes = std::unordered_map<std::string, std::vector<ngraph::element::Type_t>>;
-    void runModel(const char* model, const LayerInputTypes& expected_layer_input_types, float thr);
+    using LayerInputTypes = std::unordered_map<std::string, std::vector<ov::element::Type>>;
+    void run_model(const char* model, const LayerInputTypes& expected_layer_input_types, float thr);
 };
 
 } // namespace ONNXTestsDefinitions
