537abc5edd3d0c7581ede3fc36f39908fea4b8da
Samane <samaneahangar93@gmail.com>
2023-12-22 03:53:31 +0100

    Update div.cpp

--- src/frontends/tensorflow_common/src/op/div.cpp
+++ src/frontends/tensorflow_common/src/op/div.cpp
@@ -12,16 +12,14 @@ namespace ov {
 namespace frontend {
 namespace tensorflow {
 namespace op {
-OutputVector translate_div_op(const NodeContext& node) {
+OutputVector translate_binary_op(const NodeContext& node,
+                                 const std::function<shared_ptr<v1::Divide>(Output<Node>&, Output<Node>&)>& Div) {
     default_op_checks(node, 2, {"Div"});
     auto x = node.get_input(0);
     auto y = node.get_input(1);
-
-    // compute Division
-    auto div = make_shared<v1::Divide>(x, y);
-
-    set_node_name(node.get_name(), div);
-    return div->outputs();
+    auto result = Div(x, y);
+    set_node_name(node.get_name(), result);
+    return {result};
 }
 }  // namespace op
 }  // namespace tensorflow
