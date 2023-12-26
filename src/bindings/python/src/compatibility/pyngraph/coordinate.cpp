8ee814829256c3592ccc1be1c247715a9239bf0e
vurusovs <vitaliy.urusovskij@intel.com>
2023-12-26 16:30:15 +0400

    Fix pyngraph

--- src/bindings/python/src/compatibility/pyngraph/coordinate.cpp
+++ src/bindings/python/src/compatibility/pyngraph/coordinate.cpp
@@ -7,6 +7,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
+#include "openvino/core/shape.hpp"
 #include "pyngraph/coordinate.hpp"
 
 namespace py = pybind11;
@@ -15,7 +16,7 @@ void regclass_pyngraph_Coordinate(py::module m) {
     py::class_<ov::Coordinate, std::shared_ptr<ov::Coordinate>> coordinate(m, "Coordinate", py::module_local());
     coordinate.doc() = "ngraph.impl.Coordinate wraps ov::Coordinate";
     coordinate.def(py::init<const std::initializer_list<size_t>&>());
-    coordinate.def(py::init<const ngraph::Shape&>());
+    coordinate.def(py::init<const ov::Shape&>());
     coordinate.def(py::init<const std::vector<size_t>&>());
     coordinate.def(py::init<const ov::Coordinate&>());
 }
