13c3d2e4b5bb15edc5b12dbe0644afca3b0bb513
Anastasiia Pnevskaia <anastasiia.pnevskaia@intel.com>
2024-01-08 17:59:55 +0100

    Removed commented code.

--- src/frontends/tensorflow_common/src/utils.cpp
+++ src/frontends/tensorflow_common/src/utils.cpp
@@ -28,11 +28,6 @@ void set_node_name(const string& node_name, const shared_ptr<Node>& node) {
     }
     for (size_t idx = 0; idx < outputs.size(); ++idx) {
         set_out_name({node_name + ":" + to_string(idx)}, outputs[idx]);
-        // if (node_name.find(':') == std::string::npos) {
-        //     set_out_name({node_name + ":" + to_string(idx)}, outputs[idx]);
-        // } else {
-        //     set_out_name({node_name}, outputs[idx]);
-        // }
     }
 }
 
