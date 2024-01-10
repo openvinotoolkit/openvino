b7950a907fbad5aa743c7317bcf44fc149ca74d1
Tikhonov Ivan <ivan.tikhonov@intel.com>
2024-01-10 13:49:04 +0330

    fix merge conflicts

--- src/plugins/intel_gpu/src/plugin/transformations_pipeline.cpp
+++ src/plugins/intel_gpu/src/plugin/transformations_pipeline.cpp
@@ -106,7 +106,6 @@
 #include "transformations/op_conversions/convert_prior_box_v8_to_v0.hpp"
 #include "transformations/op_conversions/convert_shapeof3.hpp"
 #include "transformations/op_conversions/convert_topk11_downgrade.hpp"
-#include "transformations/op_conversions/group_normalization_decomposition.hpp"
 #include "transformations/op_conversions/eye_decomposition.hpp"
 #include "transformations/op_conversions/convert_pad12_downgrade.hpp"
 #include "transformations/convert_precision.hpp"
@@ -138,7 +137,6 @@
 #include "low_precision/recurrent_cell.hpp"
 
 #include "intel_gpu/runtime/itt.hpp"
-#include "transformations/op_conversions/convert_slice_to_strided_slice.hpp"
 
 namespace {
 template<typename T>
@@ -546,8 +544,6 @@ void TransformationsPipeline::apply(std::shared_ptr<ov::Model> func) {
         pass_config->disable<ov::pass::ConvertGather8ToGather7>();
         pass_config->disable<ov::pass::ConvertGather7ToGather1>();
         pass_config->disable<ov::pass::ConvertTopK11ToTopK3>();
-        pass_config->disable<ov::pass::GroupNormalizationDecomposition>();
-        pass_config->disable<ov::pass::SliceToStridedSlice>();
         pass_config->enable<ov::pass::ConvertInterpolate1ToInterpolate4>();
 
         if (enableInt8) {
