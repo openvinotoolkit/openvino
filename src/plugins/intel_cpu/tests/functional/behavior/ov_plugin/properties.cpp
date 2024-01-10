9ccef7931bd9a41299dce77dc7fdc28c2d4fa3b5
Yuan Hu <yuan2.hu@intel.com>
2024-01-10 13:51:50 +0800

    [CPU][apiConformance] Support LOG_LEVEL property (#22007)

--- src/plugins/intel_cpu/tests/functional/behavior/ov_plugin/properties.cpp
+++ src/plugins/intel_cpu/tests/functional/behavior/ov_plugin/properties.cpp
@@ -2,9 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include <gmock/gmock-matchers.h>
 #include <gtest/gtest.h>
 
 #include "test_utils/properties_test.hpp"
+#include "common_test_utils/test_assertions.hpp"
 #include "openvino/runtime/properties.hpp"
 #include "openvino/runtime/core.hpp"
 #include "openvino/core/type/element_type.hpp"
@@ -45,6 +47,7 @@ TEST_F(OVClassConfigTestCPU, smoke_PluginAllSupportedPropertiesAreAvailable) {
         RW_property(ov::hint::enable_hyper_threading.name()),
         RW_property(ov::device::id.name()),
         RW_property(ov::intel_cpu::denormals_optimization.name()),
+        RW_property(ov::log::level.name()),
         RW_property(ov::intel_cpu::sparse_weights_decompression_rate.name()),
     };
 
@@ -281,6 +284,37 @@ TEST_F(OVClassConfigTestCPU, smoke_PluginSetConfigExecutionModeAndInferencePreci
     ASSERT_NO_THROW(ie.set_property("CPU", ov::hint::inference_precision(bf16_if_can_be_emulated)));
     expect_execution_mode(ov::hint::ExecutionMode::ACCURACY);
     expect_inference_precision(bf16_if_can_be_emulated);
+}
+
+TEST_F(OVClassConfigTestCPU, smoke_PluginSetConfigLogLevel) {
+    ov::Core ie;
+    //check default value
+    ov::Any value;
+    ASSERT_NO_THROW(value = ie.get_property("CPU", ov::log::level));
+    ASSERT_EQ(value.as<ov::log::Level>(), ov::log::Level::NO);
+
+    //check set and get
+    const std::vector<ov::log::Level> logLevels = {
+        ov::log::Level::ERR,
+        ov::log::Level::NO,
+        ov::log::Level::WARNING,
+        ov::log::Level::INFO,
+        ov::log::Level::DEBUG,
+        ov::log::Level::TRACE};
+
+    for (unsigned int i = 0; i < logLevels.size(); i++) {
+        ASSERT_NO_THROW(ie.set_property("CPU", ov::log::level(logLevels[i])));
+        ASSERT_NO_THROW(value = ie.get_property("CPU", ov::log::level));
+        ASSERT_EQ(value.as<ov::log::Level>(), logLevels[i]);
+    }
+
+    // check throwing message
+    auto property = ov::PropertyName(ov::log::level.name(), ov::PropertyMutability::RW);
+    const std::string expect_message = std::string("Wrong value DUMMY VALUE for property key ")  +
+        ov::log::level.name() + ". Expected only ov::log::Level::NO/ERR/WARNING/INFO/DEBUG/TRACE.";
+    OV_EXPECT_THROW(ie.set_property("CPU", {{property, "DUMMY VALUE"}}),
+            ov::Exception,
+            testing::HasSubstr(expect_message));
 }
 
 } // namespace
