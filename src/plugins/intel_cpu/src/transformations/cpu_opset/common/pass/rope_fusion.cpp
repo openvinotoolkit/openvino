4f8326aff802308cc5e5f0d919188a052699b13b
Luwei Zhou <luwei.zhou@intel.com>
2024-01-11 18:00:17 +0800

    Update RoPE RT info.

--- src/plugins/intel_cpu/src/transformations/cpu_opset/common/pass/rope_fusion.cpp
+++ src/plugins/intel_cpu/src/transformations/cpu_opset/common/pass/rope_fusion.cpp
@@ -86,6 +86,11 @@ ov::intel_cpu::RoPEFusionGPTNEOX::RoPEFusionGPTNEOX() {
         auto old_node = root;
         auto new_node = std::make_shared<RoPENode>(new_args, config);
         new_node->set_friendly_name(old_node->get_friendly_name());
+        const auto& rotate_node = pattern_map.at(x_rotate_half).get_node_shared_ptr();
+        const auto& mul_cos_node = pattern_map.at(mul_cos).get_node_shared_ptr();;
+        const auto& mul_sin_node = pattern_map.at(mul_sin).get_node_shared_ptr();;
+        const auto& res_node = pattern_map.at(result).get_node_shared_ptr();;
+        ov::copy_runtime_info({rotate_node, mul_cos_node, mul_sin_node, res_node}, new_node);
         ov::replace_node(old_node, new_node);
 
         // this new node may match following additional matchers
@@ -223,6 +228,7 @@ ov::intel_cpu::RoPEFusionIOSlicing::RoPEFusionIOSlicing() {
         // remove slice & concat
         rope_node->set_argument(0, pattern_map.at(data));
         rope_node->set_friendly_name(root->get_friendly_name());
+
         ov::replace_node(root, rope_node);
 
         rope_node->validate_and_infer_types();
@@ -447,6 +453,10 @@ ov::intel_cpu::RoPEFusionGPTJ::RoPEFusionGPTJ() {
 
         auto new_node = std::make_shared<RoPENode>(new_args, config);
         new_node->set_friendly_name(old_node->get_friendly_name());
+
+        const auto& concat_node = pattern_map.at(stack_1182).get_node_shared_ptr();
+        const auto& flat_concat_node = pattern_map.at(flatten_Concat_1197).get_node_shared_ptr();;
+        ov::copy_runtime_info({concat_node, flat_concat_node}, new_node);
         ov::replace_node(old_node, new_node);
         return true;
     };
@@ -572,6 +582,10 @@ ov::intel_cpu::RoPEFusionChatGLM::RoPEFusionChatGLM(int split_output_id) {
 
         auto new_node = std::make_shared<RoPENode>(new_args, config);
         new_node->set_friendly_name(old_node->get_friendly_name());
+        const auto& sub_node = pattern_map.at(sub_Subtract_469).get_node_shared_ptr();
+        const auto& add_node = pattern_map.at(add_Add_476).get_node_shared_ptr();;
+        const auto& concat_node = pattern_map.at(stack_481).get_node_shared_ptr();;
+        ov::copy_runtime_info({sub_node, add_node, concat_node}, new_node);
         ov::replace_node(old_node, new_node);
         return true;
     };
@@ -711,6 +725,11 @@ ov::intel_cpu::RoPEFusionQwen::RoPEFusionQwen(int split_output_id) {
         auto old_node = root;
         auto new_node = std::make_shared<RoPENode>(new_args, config);
         new_node->set_friendly_name(old_node->get_friendly_name());
+        const auto& rotary_emb_cos_node = pattern_map.at(rotary_emb_cos).get_node_shared_ptr();
+        const auto& rotary_emb_sin_node = pattern_map.at(rotary_emb_sin).get_node_shared_ptr();;
+        const auto& cacat_node = pattern_map.at(cat_Concat_593).get_node_shared_ptr();;
+        const auto& multiply_node = pattern_map.at(Multiply_567524).get_node_shared_ptr();;
+        ov::copy_runtime_info({rotary_emb_cos_node, rotary_emb_sin_node, cacat_node, multiply_node}, new_node);
         ov::replace_node(old_node, new_node);
         return true;
     };
