6bc2ce2780a072af32883dd2e357342c427bd710
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2024-01-11 19:59:56 +0100

    Remove deprecated functions

--- src/core/include/ngraph/validation_util.hpp
+++ src/core/include/ngraph/validation_util.hpp
@@ -27,19 +27,6 @@ namespace ngraph {
 
 NGRAPH_API_DEPRECATED
 NGRAPH_API
-PartialShape infer_windowed_reduction_output_shape(const Node* node,
-                                                   const PartialShape& data_shape,
-                                                   const Strides& data_dilation,
-                                                   const CoordinateDiff& data_padding_below,
-                                                   const CoordinateDiff& data_padding_above,
-                                                   const PartialShape& window_shape,
-                                                   const Strides& window_strides,
-                                                   const Strides& window_dilation,
-                                                   bool is_window_all_in_padding_allowed,
-                                                   bool ceil_mode = false);
-
-NGRAPH_API_DEPRECATED
-NGRAPH_API
 std::tuple<element::Type, PartialShape, PartialShape> infer_batch_norm_forward(const Node* node,
                                                                                element::Type input_element_type,
                                                                                element::Type gamma_element_type,
@@ -52,27 +39,4 @@ std::tuple<element::Type, PartialShape, PartialShape> infer_batch_norm_forward(c
                                                                                const PartialShape& mean_shape,
                                                                                const PartialShape& variance_shape);
 
-/// \brief Apply auto padding to padding_above and padding_below inputs
-///        if all needed informations are known.
-///
-/// \param image_shape       The shape of input image.
-/// \param filter_shape      The shape of filter input.
-/// \param filter_strides    The strides of applied padding.
-/// \param filter_dilations  The dilations of applied padding.
-/// \param pad_type          The type of padding. Auto padding is applied only
-///                          for SAME_UPPER and SAME_LOWER mode.
-/// \param padding_above     The beginning of padding shape.
-/// \param end               The beginning of padding shape.
-///
-/// \return true if auto padding was applied successfully (all needed informations such as
-///         spatial dims are known), false otherwise.
-NGRAPH_API_DEPRECATED
-NGRAPH_API
-bool try_apply_auto_padding(const PartialShape& image_shape,
-                            const Shape& filter_shape,
-                            const Strides& filter_strides,
-                            const Strides& filter_dilations,
-                            const op::PadType pad_type,
-                            CoordinateDiff& padding_above,
-                            CoordinateDiff& padding_below);
 }  // namespace ngraph
