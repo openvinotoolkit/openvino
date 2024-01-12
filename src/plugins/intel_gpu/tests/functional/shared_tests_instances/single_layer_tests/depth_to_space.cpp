074cab5b639010f5e7da5189fa5791f5c47b4f2b
Raasz, Pawel <pawel.raasz@intel.com>
2024-01-12 12:22:01 +0000

    Fix GPU test building

--- src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/depth_to_space.cpp
+++ src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/depth_to_space.cpp
@@ -3,10 +3,10 @@
 //
 
 #include <vector>
-#include <ngraph/opsets/opset3.hpp>
 
-#include "single_op_tests/depth_to_space.hpp"
 #include "common_test_utils/test_constants.hpp"
+#include "openvino/opsets/opset3.hpp"
+#include "single_op_tests/depth_to_space.hpp"
 
 namespace {
 using ov::test::DepthToSpaceLayerTest;
