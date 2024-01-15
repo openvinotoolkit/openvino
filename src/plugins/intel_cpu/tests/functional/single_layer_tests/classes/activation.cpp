490d67858509ec067b881c35262c5dde68a4bbd6
liubo-intel <bo4.liu@intel.com>
2024-01-15 11:23:12 +0800

    use std::numeric_limits function to replace macro NAN in test cases

--- src/plugins/intel_cpu/tests/functional/single_layer_tests/classes/activation.cpp
+++ src/plugins/intel_cpu/tests/functional/single_layer_tests/classes/activation.cpp
@@ -86,7 +86,7 @@ void ActivationLayerCPUTest::generate_inputs(const std::vector<ov::Shape>& targe
             // cover Sign NAN test case
             if ((activationType == utils::ActivationTypes::Sign) && targetInputStaticShapes[i].size() == 8 &&
                 funcInput.get_element_type() == ov::element::f32) {
-                static_cast<float*>(tensor.data())[0] = NAN;
+                static_cast<float*>(tensor.data())[0] = std::numeric_limits<float>::quiet_NaN();
             }
         } else {
             tensor = ov::test::utils::create_and_fill_tensor(funcInput.get_element_type(), targetInputStaticShapes[i]);
