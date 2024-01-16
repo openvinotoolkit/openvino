39fcb7e561b4a5a84a9030203cab9917271ced20
andrei-cv <andrei.gorbachev@intel.com>
2024-01-16 17:49:44 +0400

    code style

--- src/tests/test_utils/common_test_utils/src/data_utils.cpp
+++ src/tests/test_utils/common_test_utils/src/data_utils.cpp
@@ -322,7 +322,8 @@ void fill_psroi(ov::Tensor& tensor,
                            spatialScale,               \
                            spatialBinsX,               \
                            spatialBinsY,               \
-                           mode); break;
+                           mode);                      \
+        break;
 
     auto element_type = tensor.get_element_type();
     switch (element_type) {
