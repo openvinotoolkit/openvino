f0d5cfc76ffb1c79e1fcc04f6ac775227a97114f
Krivoruchko <georgy.krivoruchko@intel.com>
2024-01-15 22:29:36 -0800

    Fixed code style

--- src/frontends/onnx/frontend/include/onnx_import/core/node.hpp
+++ src/frontends/onnx/frontend/include/onnx_import/core/node.hpp
@@ -8,7 +8,7 @@
 #    define NGRAPH_LEGACY_HEADER_INCLUDED
 #    ifdef _MSC_VER
 #        pragma message( \
-                "The nGraph API is deprecated and will be removed in the 2024.0 release. For instructions on transitioning to the new API, please refer to https://docs.openvino.ai/latest/openvino_2_0_transition_guide.html")
+            "The nGraph API is deprecated and will be removed in the 2024.0 release. For instructions on transitioning to the new API, please refer to https://docs.openvino.ai/latest/openvino_2_0_transition_guide.html")
 #    else
 #        warning("The nGraph API is deprecated and will be removed in the 2024.0 release. For instructions on transitioning to the new API, please refer to https://docs.openvino.ai/latest/openvino_2_0_transition_guide.html")
 #    endif
