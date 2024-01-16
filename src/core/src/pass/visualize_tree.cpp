b3c2c386dc527f5b877680fbe1215b6a3e05968f
Katarzyna Mitrus <katarzyna.mitrus@intel.com>
2024-01-16 15:26:43 +0100

    [FP8] Implementation of FP8 element types (ov::element::f8e4m3 and ov::element::f8e5m2) (#21608)
    * FP8 element types init
    
    * Remove redundant union
    
    * Replace using ov
    
    * Update fundamental types
    
    * Update class name check
    
    * Update tests
    
    * Remove redundant sign
    
    * Expose f8 types in Python
    
    * Add to py to dtype map
    
    * Style alignment
    
    * Align python style
    
    * Update test values
    
    * Remove constexpr from_bits to fix warning
    
    * Add trivially construcitble asserts and common constrexpr
    
    * Align python tests opset
    
    * Update f8e4m3 <-> float conversion
    
    * f8e5m2 class update
    
    * Add f8e5m2 unit test
    
    * Add to string conversion tests
    
    * Rename class f8e4m3 -> float8_e4m3
    
    * Rename f8e5m2 -> float8_e5m2
    
    * Remove size() and to_string from float8
    - size() can be replaced by compile time sizeof
    - to_string can be replaced by std::to_string()
    
    * float8 E5M2 remove unused constexpr value
    
    * Fix union initialization and ncc style rules
    
    * Fix test issues
    
    * Use NaN from std::numeric_limits instead macro
    - minor refactor of float8_e4m3
    
    * Update nf4 usage in element_type.cpp
    
    * Sync openvino.style with master
    
    * Update f8e5m2 test
    
    ---------
    
    Co-authored-by: Raasz, Pawel <pawel.raasz@intel.com>

--- src/core/src/pass/visualize_tree.cpp
+++ src/core/src/pass/visualize_tree.cpp
@@ -376,6 +376,8 @@ static std::string get_value(const std::shared_ptr<ov::op::v0::Constant>& consta
     case ov::element::Type_t::u4:
     case ov::element::Type_t::nf4:
     case ov::element::Type_t::i4:
+    case ov::element::Type_t::f8e4m3:
+    case ov::element::Type_t::f8e5m2:
         ss << constant->get_output_element_type(0).get_type_name() << " value";
         break;
     case ov::element::Type_t::bf16:
