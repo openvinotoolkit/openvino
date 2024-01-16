d24c0ecc771d92dc323a60bc910295ec1bc33da6
Mingyu Kim <mingyu.kim@intel.com>
2024-01-16 14:24:53 +0900

    [GPU] Reclaim memory for dynamic shape (#22133)
    * [GPU] Reclaim memory for dynamic shape

--- src/plugins/intel_gpu/src/graph/include/primitive_inst.h
+++ src/plugins/intel_gpu/src/graph/include/primitive_inst.h
@@ -368,7 +368,7 @@ protected:
     bool _is_constant = false;
     bool _needs_completion_event = false;
 
-    size_t max_output_layout_size = 0;
+    size_t _max_output_layout_count = 0;
     std::vector<size_t> max_intermediates_memory_sizes;
 
     std::vector<memory::ptr> allocate_outputs(kernel_impl_params* updated_params = nullptr, bool reset_mem = true, bool runtime_alloc = false);
