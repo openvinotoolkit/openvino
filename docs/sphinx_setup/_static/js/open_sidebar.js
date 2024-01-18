17280c027a4aa230c1b8989f9bf000d1d56614ed
Andrzej Kopytko <andrzejx.kopytko@intel.com>
2024-01-18 13:09:26 +0100

    [Docs]One-banner (#21628)

--- docs/sphinx_setup/_static/js/open_sidebar.js
+++ docs/sphinx_setup/_static/js/open_sidebar.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-    const elems = $( "ul.bd-sidenav > li > input" )
-    console.log(elems)
+    const elems = $( "ul.bd-sidenav > li > input" );
     for(let i = 0; i < elems.length; i++){
         elems[i].setAttribute("checked", "checked");
     }
