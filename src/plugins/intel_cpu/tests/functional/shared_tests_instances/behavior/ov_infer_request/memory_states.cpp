d0795c8798c054f3807a882663b09e54014ed306
Haiqi Pan <haiqi.pan@intel.com>
2024-01-19 19:18:52 +0800

    [API 2.0] Add memory_states test cases (#21627)
    * add memory_states test case
    
    * add memory_states.cpp
    
    * fix m_cached_output_names.count(tensor_name) failed
    
    * add inferreq_smoke_VariableState_SetState
    
    * add inferreq_smoke_VariableState_Reset
    
    * add inferreq_smoke_VariableState_2infers_set
    
    * add inferreq_smoke_VariableState_2infers
    
    * fix name style
    
    * Update src/tests/functional/plugin/shared/src/behavior/ov_infer_request/memory_states.cpp
    
    Co-authored-by: Pawel Raasz <pawel.raasz@intel.com>
    
    * move SKIP_IF_CURRENT_TEST_IS_DISABLED before std::tie
    
    * Update src/plugins/intel_cpu/tests/functional/shared_tests_instances/behavior/ov_infer_request/memory_states.cpp
    
    Co-authored-by: Vitaliy Urusovskij <vitaliy.urusovskij@intel.com>
    
    * Update src/tests/functional/plugin/shared/include/behavior/ov_infer_request/memory_states.hpp
    
    Co-authored-by: Vitaliy Urusovskij <vitaliy.urusovskij@intel.com>
    
    * Update src/tests/functional/plugin/shared/src/behavior/ov_infer_request/memory_states.cpp
    
    Co-authored-by: Vitaliy Urusovskij <vitaliy.urusovskij@intel.com>
    
    * fix comment
    
    * use vector to replace array
    
    * Update src/tests/functional/plugin/shared/src/behavior/ov_infer_request/memory_states.cpp
    
    Co-authored-by: Pawel Raasz <pawel.raasz@intel.com>
    
    * avoid temporary vector creation
    
    * fix element
    
    * fix element
    
    * remove test in arm
    
    ---------
    
    Co-authored-by: Pawel Raasz <pawel.raasz@intel.com>
    Co-authored-by: Vitaliy Urusovskij <vitaliy.urusovskij@intel.com>
    Co-authored-by: Chen Peter <peter.chen@intel.com>

---
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/behavior/ov_infer_request/memory_states.cpp
@@ -0,0 +1,25 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "behavior/ov_infer_request/memory_states.hpp"
+
+using namespace ov::test::behavior;
+using namespace ov;
+
+namespace {
+std::vector<memoryStateParams> memoryStateTestCases = {
+    memoryStateParams(OVInferRequestVariableStateTest::get_network(),
+                      {"c_1-3", "r_1-3"},
+                      ov::test::utils::DEVICE_CPU,
+                      {}),
+    memoryStateParams(OVInferRequestVariableStateTest::get_network(),
+                      {"c_1-3", "r_1-3"},
+                      ov::test::utils::DEVICE_HETERO,
+                      {{MULTI_CONFIG_KEY(DEVICE_PRIORITIES), ov::test::utils::DEVICE_CPU}})};
+
+INSTANTIATE_TEST_SUITE_P(smoke_VariableState,
+                         OVInferRequestVariableStateTest,
+                         ::testing::ValuesIn(memoryStateTestCases),
+                         OVInferRequestVariableStateTest::getTestCaseName);
+}  // namespace
