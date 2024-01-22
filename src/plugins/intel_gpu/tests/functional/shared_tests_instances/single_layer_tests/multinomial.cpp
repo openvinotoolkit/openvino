b6e07168e3c2cfc78c68226b3b3a98ba17fe1937
andrei-cv <andrei.gorbachev@intel.com>
2024-01-23 01:50:46 +0400

    Revert "add multinomial"
    This reverts commit 971f7e9431cd04a07f3ebb35b3a59d0b66c676d2.

--- src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/multinomial.cpp
+++ src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/multinomial.cpp
@@ -1,36 +1,25 @@
 // Copyright (C) 2023 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
-#include "single_op_tests/multinomial.hpp"
+#include "single_layer_tests/multinomial.hpp"
+
+using namespace ov::test::subgraph;
 
 namespace {
-using ov::test::MultinomialLayerTest;
 
 const std::vector<ov::test::ElementType> netPrecisions = {
     ov::element::f16,
     ov::element::f32,
 };
 
-std::vector<float> probs_1x32_f32_log =
-    {3.0f, 6.0f, 10.0f, 0.0f, 3.0f, 0.0f, 10.0f, 6.0f, 6.0f, 10.0f, 0.0f, 3.0f, 10.0f, 6.0f, 3.0f, 0.0f,
-     3.0f, 6.0f, 10.0f, 0.0f, 3.0f, 0.0f, 10.0f, 6.0f, 6.0f, 10.0f, 0.0f, 3.0f, 10.0f, 6.0f, 3.0f, 0.0f};
+const std::vector<ov::Shape> inputShapes = {
+    {1, 32},
+    {2, 28},
+};
 
-std::vector<float> probs_2x28_f32_log =
-    {3.0f, 6.0f, 10.0f, 0.0f, 3.0f, 0.0f, 10.0f, 6.0f, 6.0f, 10.0f, 0.0f, 3.0f, 10.0f, 6.0f,
-     10.0f, 0.0f, 3.0f, 0.0f, 10.0f, 6.0f, 6.0f, 10.0f, 0.0f, 3.0f, 10.0f, 6.0f, 3.0f, 0.0f,
-     3.0f, 6.0f, 10.0f, 0.0f, 3.0f, 0.0f, 10.0f, 6.0f, 6.0f, 10.0f, 0.0f, 3.0f, 10.0f, 6.0f,
-     10.0f, 0.0f, 3.0f, 0.0f, 10.0f, 6.0f, 6.0f, 10.0f, 0.0f, 3.0f, 10.0f, 6.0f, 3.0f, 0.0f};
-
-std::vector<int64_t> num_samples_2_i64 = {2};
-std::vector<int64_t> num_samples_4_i64 = {4};
-
-const std::vector<ov::Tensor> inputTensors = {
-                    ov::Tensor(ov::element::f32, {1, 32}, probs_1x32_f32_log.data()),
-                    ov::Tensor(ov::element::f32, {2, 28}, probs_2x28_f32_log.data())};
-
-const std::vector<ov::Tensor> numSamples = {
-                    ov::Tensor(ov::element::i64, {1}, num_samples_2_i64.data()),
-                    ov::Tensor(ov::element::i64, {1}, num_samples_4_i64.data())};
+const std::vector<int64_t> numSamples = {
+    2, 4,
+};
 
 const std::vector<bool> withReplacement = {
     false,
@@ -44,14 +33,16 @@ const std::vector<bool> logProbes = {
 
 INSTANTIATE_TEST_SUITE_P(
     smoke_Multinomial,
-    MultinomialLayerTest,
-    testing::Combine(testing::Values("static"),
-                     testing::ValuesIn(inputTensors),
+    MultinomialTest,
+    testing::Combine(testing::ValuesIn(netPrecisions),
+                     ::testing::Values(ov::element::undefined),
+                     ::testing::Values(ov::element::undefined),
+                     testing::ValuesIn(ov::test::static_shapes_to_test_representation(inputShapes)),
                      testing::ValuesIn(numSamples),
                      testing::Values(ov::element::i64),
                      testing::ValuesIn(withReplacement),
                      testing::ValuesIn(logProbes),
-                     testing::Values(std::pair<uint64_t, uint64_t>{0, 2}),
-                     testing::Values(ov::test::utils::DEVICE_GPU)),
-                     MultinomialLayerTest::getTestCaseName);
+                     testing::Values(ov::test::utils::DEVICE_GPU),
+                     testing::Values(ov::AnyMap())),
+                     MultinomialTest::getTestCaseName);
 } // anonymous namespace
