57a7629204be881a2e0623c2f3b72e779a23c18c
himanshugupta11002 <72141497+himanshugupta11002@users.noreply.github.com>
2024-01-23 20:28:16 +0530

    Update matrix_band_part.cpp

--- src/frontends/tensorflow_common/src/op/matrix_band_part.cpp
+++ src/frontends/tensorflow_common/src/op/matrix_band_part.cpp
@@ -14,63 +14,56 @@ namespace frontend {
 namespace tensorflow {
 namespace op {
 
-OutputVector translate_matrix_band_part_op(const NodeContext& node) {
-    default_op_checks(node, 1, {"MatrixBandPart", "MATRIX_BAND_PART"});
-
+OutputVector matrix_band_part(const NodeContext& node) {
+    // Input tensor and parameters
     auto input = node.get_input(0);
+
+    // Get the shape of the input tensor
     auto input_shape = make_shared<v3::ShapeOf>(input);
-    auto last_dim = make_shared<v1::StridedSlice>(
-        input_shape,
-        make_shared<v0::Constant>(element::i64, Shape{1}, std::vector<int64_t>{-2}),
-        make_shared<v0::Constant>(element::i64, Shape{1}, std::vector<int64_t>{-1}),
-        make_shared<v0::Constant>(element::i64, Shape{1}, std::vector<int64_t>{1}),
-        std::vector<int64_t>({0}),
-        std::vector<int64_t>({0}));
 
-    auto m = make_shared<v1::StridedSlice>(
-        last_dim,
-        make_shared<v0::Constant>(element::i64, Shape{1}, std::vector<int64_t>{0}),
-        make_shared<v0::Constant>(element::i64, Shape{1}, std::vector<int64_t>{1}),
-        make_shared<v0::Constant>(element::i64, Shape{1}, std::vector<int64_t>{1}),
-        std::vector<int64_t>({0}),
-        std::vector<int64_t>({0}));
+    // Find sizes m and n of the last two dimensions
+    auto m = make_shared<v1::StridedSlice>(input_shape,
+                                           make_shared<v0::Constant>(element::i64, Shape{1}, {-2}),
+                                           make_shared<v0::Constant>(element::i64, Shape{1}, {-1}),
+                                           make_shared<v0::Constant>(element::i64, Shape{1}, {1}),
+                                           std::vector<int64_t>({0}),
+                                           std::vector<int64_t>({1}));
 
-    auto n = make_shared<v1::StridedSlice>(
-        last_dim,
-        make_shared<v0::Constant>(element::i64, Shape{1}, std::vector<int64_t>{1}),
-        make_shared<v0::Constant>(element::i64, Shape{1}, std::vector<int64_t>{2}),
-        make_shared<v0::Constant>(element::i64, Shape{1}, std::vector<int64_t>{1}),
-        std::vector<int64_t>({0}),
-        std::vector<int64_t>({0}));
+    auto n = make_shared<v1::StridedSlice>(input_shape,
+                                           make_shared<v0::Constant>(element::i64, Shape{1}, {-1}),
+                                           make_shared<v0::Constant>(element::i64, Shape{1}, {}),
+                                           make_shared<v0::Constant>(element::i64, Shape{1}, {1}),
+                                           std::vector<int64_t>({0}),
+                                           std::vector<int64_t>({1}));
 
-    auto range_m = make_shared<v0::Range>(make_shared<v0::Constant>(element::i64, Shape{1}, std::vector<int64_t>{0}),
-                                          m,
-                                          make_shared<v0::Constant>(element::i64, Shape{1}, std::vector<int64_t>{1}));
+    // Generate ranges [0, m) and [0, n)
+    auto range_m = make_shared<v1::Range>(m);
+    auto range_n = make_shared<v1::Range>(n);
 
-    auto range_n = make_shared<v0::Range>(make_shared<v0::Constant>(element::i64, Shape{1}, std::vector<int64_t>{0}),
-                                          n,
-                                          make_shared<v0::Constant>(element::i64, Shape{1}, std::vector<int64_t>{1}));
+    // Unsqueeze ranges to have tensors of shapes [m, 1] and [1, n]
+    auto unsqueeze_range_m = make_shared<v0::Unsqueeze>(range_m, make_shared<v0::Constant>(element::i64, Shape{1}, {1}));
+    auto unsqueeze_range_n = make_shared<v0::Unsqueeze>(range_n, make_shared<v0::Constant>(element::i64, Shape{1}, {0}));
 
-    auto unsqueeze_range_m = make_shared<v0::Unsqueeze>(range_m, make_shared<v0::Constant>(element::i64, Shape{1}, std::vector<int64_t>{1}));
-    auto unsqueeze_range_n = make_shared<v0::Unsqueeze>(range_n, make_shared<v0::Constant>(element::i64, Shape{1}, std::vector<int64_t>{0}));
+    // Create indicator function using logical operations
+    auto in_band_rows = make_shared<v1::LogicalAnd>(make_shared<v1::LessEqual>(unsqueeze_range_m, m),
+                                                    make_shared<v1::GreaterEqual>(unsqueeze_range_m, make_shared<v0::Constant>(element::i64, Shape{}, {0})));
 
-    auto in_band_indicator = make_shared<v1::LessEqual>(unsqueeze_range_m, unsqueeze_range_n);
+    auto in_band_cols = make_shared<v1::LogicalAnd>(make_shared<v1::LessEqual>(unsqueeze_range_n, n),
+                                                    make_shared<v1::GreaterEqual>(unsqueeze_range_n, make_shared<v0::Constant>(element::i64, Shape{}, {0})));
 
-    auto unsqueeze_in_band_indicator = make_shared<v0::Unsqueeze>(in_band_indicator, last_dim);
+    auto in_band_indicator = make_shared<v1::LogicalAnd>(in_band_rows, in_band_cols);
 
-    auto zero_padding = make_shared<v0::Concat>(
-        OutputVector({make_shared<v0::Constant>(input.get_element_type(), Shape{1}, std::vector<int64_t>{0})}),
-        0);
+    // Unsqueeze the indicator function to have a tensor of shape [1,..,1, m, n]
+    auto unsqueeze_indicator = make_shared<v0::Unsqueeze>(in_band_indicator, make_shared<v0::Constant>(element::i64, Shape{}, {1}));
 
-    auto band_part = make_shared<v1::Select>(unsqueeze_in_band_indicator, input, zero_padding);
+    // Extract or modify the elements based on the indicator function
+    auto result = make_shared<v3::Where>(unsqueeze_indicator, input, make_shared<v0::Constant>(input->get_element_type(), Shape{}, {0}));
 
-    auto new_shape = make_shared<v0::Concat>(OutputVector({input_shape, last_dim}), 0);
-
-    auto reshaped_band_part = make_shared<v1::Reshape>(band_part, new_shape, false);
-
-    set_node_name(node.get_name(), reshaped_band_part);
-    return {reshaped_band_part};
+    // Return the result
+    set_node_name(node.get_name(), result);
+    return {result};
 }
+
 
 }  // namespace op
 }  // namespace tensorflow
