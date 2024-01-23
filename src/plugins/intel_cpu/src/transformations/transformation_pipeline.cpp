935c825935dfb23eb43b004c251ecda4b220dd65
dmitrygo <dmitry.gorokhov@intel.com>
2024-01-23 16:50:58 +0400

    [CPU] Enable bf16->fp32 ConvertPrecision pass for any HW

--- src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
+++ src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
@@ -282,9 +282,7 @@ void Transformations::PreLpt(const std::vector<ov::element::Type>& defaultPrecis
             {ov::element::u4,      ov::element::u8}
         };
 
-        // @todo should we always convert to f32 regardless of hardware support, as it is done for f16?
-        if (!hasHardwareSupport(ov::element::bf16))
-            map.insert({ov::element::bf16, ov::element::f32});
+        map.insert({ov::element::bf16, ov::element::f32});
 #if defined(OV_CPU_ARM_ENABLE_FP16)
         if (inferencePrecision != ov::element::f16)
             map.insert({ov::element::f16, ov::element::f32});
