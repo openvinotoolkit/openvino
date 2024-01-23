df88e6a290ccff2daa8dafa1f6a379917c81ab32
Kazantsev, Roman <roman.kazantsev@intel.com>
2024-01-24 00:09:12 +0400

    Make assign_variable_op backward-compatible

--- src/frontends/tensorflow/src/op/assign_variable_op.cpp
+++ src/frontends/tensorflow/src/op/assign_variable_op.cpp
@@ -17,23 +17,20 @@ namespace tensorflow {
 namespace op {
 OutputVector translate_assignvariable_op(const NodeContext& node) {
     default_op_checks(node, 2, {"AssignVariableOp"});
-    auto ref = as_type_ptr<Variable>(node.get_input_by_reference(0).get_node_shared_ptr());
-    TENSORFLOW_OP_VALIDATION(
-        node,
-        ref,
-        "[TensorFlow Frontend] internal error: AssignVariableOp operation expects variable by the first input");
+    if (auto ref = as_type_ptr<Variable>(node.get_input_by_reference(0).get_node_shared_ptr())) {
+        // create new variable operation node that will represent the same variable
+        // but it will be initialized with value
+        auto value = node.get_input(1);
+        auto new_ref = make_shared<Variable>(*ref, value);
 
-    // create new variable operation node that will represent the same variable but it will be initialized with value
-    auto value = node.get_input(1);
-    auto new_ref = make_shared<Variable>(*ref, value);
-
-    // since this operation produces new state of the variable
-    // it needs to update variables map
-    auto variables_state_map = node.get_variable_state_map();
-    TENSORFLOW_OP_VALIDATION(node,
-                             variables_state_map,
-                             "[TensorFlow Frontend] internal error: variable state map is nullptr");
-    variables_state_map->update_variable_state_map_for_node(node.get_name(), new_ref);
+        // since this operation produces new state of the variable
+        // it needs to update variables map
+        auto variables_state_map = node.get_variable_state_map();
+        TENSORFLOW_OP_VALIDATION(node,
+                                 variables_state_map,
+                                 "[TensorFlow Frontend] internal error: variable state map is nullptr");
+        variables_state_map->update_variable_state_map_for_node(node.get_name(), new_ref);
+    }
 
     return {};
 }
