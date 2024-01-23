2b419c00fcfe9a9efd9c51032d47411de68448df
Krivoruchko <georgy.krivoruchko@intel.com>
2024-01-23 06:29:57 -0800

    Fixed code style

--- src/frontends/onnx/frontend/src/op/matmul.hpp
+++ src/frontends/onnx/frontend/src/op/matmul.hpp
@@ -7,8 +7,8 @@
 #include "openvino/core/deprecated.hpp"
 OPENVINO_SUPPRESS_DEPRECATED_START
 
-#include "openvino/op/matmul.hpp"
 #include "onnx_import/core/node.hpp"
+#include "openvino/op/matmul.hpp"
 
 namespace ngraph {
 namespace onnx_import {
