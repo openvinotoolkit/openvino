e5d0e41865bbb861d048e7980cc3fc7539f2feb5
Anastasiia Pnevskaia <anastasiia.pnevskaia@intel.com>
2024-01-24 12:16:47 +0100

    Minor fix.

--- src/frontends/tensorflow_common/include/helper_ops/keep_in_graph_op.hpp
+++ src/frontends/tensorflow_common/include/helper_ops/keep_in_graph_op.hpp
@@ -16,8 +16,6 @@ class OPENVINO_API KeepInGraphOp : public InternalOperation {
 public:
     OPENVINO_OP("KeepInGraphOp", "ov::frontend::tensorflow", InternalOperation);
 
-    KeepInGraphOp() = default;
-
     KeepInGraphOp(const std::string& op_type_name, const std::shared_ptr<DecoderBase>& decoder = nullptr)
         : InternalOperation(decoder, OutputVector{}, 1, op_type_name) {}
 };
