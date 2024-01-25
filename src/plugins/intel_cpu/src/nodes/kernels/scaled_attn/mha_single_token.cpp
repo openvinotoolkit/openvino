c88ad07889bb1396d2fa4a26d1470d0b5e58fe1b
Luo Cheng <cheng.luo@intel.com>
2024-01-25 15:59:14 +0800

    remove profiler

--- src/plugins/intel_cpu/src/nodes/kernels/scaled_attn/mha_single_token.cpp
+++ src/plugins/intel_cpu/src/nodes/kernels/scaled_attn/mha_single_token.cpp
@@ -18,7 +18,6 @@
 #include "mha_single_token.hpp"
 #include "common.hpp"
 #include "softmax_kernel.hpp"
-#include "utils/profiler.hpp"
 
 namespace ov {
 namespace Extensions {
@@ -604,7 +603,6 @@ static void mha_single_token_kernel(const ov::intel_cpu::PlainTensor& query,
                              const ov::intel_cpu::PlainTensor& past_k_scale_zp,
                              const ov::intel_cpu::PlainTensor& past_v_scale_zp,
                              ov::intel_cpu::PlainTensor& head_sum) {
-    PROFILE(_prof, "1tok_q*k");
     ov::intel_cpu::PlainTensor causal_mask;
     bool select_nfltmax_at_0 = false;
     auto B = query.size(0);
@@ -637,7 +635,6 @@ static void mha_single_token_kernel(const ov::intel_cpu::PlainTensor& query,
         });
     }
 #endif
-    _prof = ov::intel_cpu::profilerManagerInstance.startProfile("1tok_raw");
     parallel_nt_static(nthr, [&](const size_t ithr, const size_t nthr) {
         size_t start{0}, end{0};
         splitter(B * h_group_num * kv_len, nthr, ithr, start, end);
@@ -685,7 +682,6 @@ static void mha_single_token_kernel(const ov::intel_cpu::PlainTensor& query,
         }
     });
 
-    _prof = ov::intel_cpu::profilerManagerInstance.startProfile("1tok_softmax");
     parallel_for3d(B, H, q_len, [&](size_t b, size_t h, size_t pq) {
         // apply attention mask & sofmax
         auto ncausal = auto_causal ? (kv_len - q_len + pq + 1) : kv_len;
@@ -705,7 +701,6 @@ static void mha_single_token_kernel(const ov::intel_cpu::PlainTensor& query,
     });
 
     // attn_w * V
-    _prof = ov::intel_cpu::profilerManagerInstance.startProfile("1tok_k*v");
     buf_attn_score.resize<float>({static_cast<size_t>(nthr), B, q_len, H, S});
     // buf_attn_w {B, H, q_len, kv_len}
     parallel_nt_static(nthr, [&](const size_t ithr, const size_t nthr) {
