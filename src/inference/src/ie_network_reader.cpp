d0efa28587c6b8fba80e88a9cd8bd1d253eaffa3
panhaiqi <haiqi.pan@intel.com>
2024-01-25 10:22:56 +0800

    remove file_utils.h in ie_network_reader.cpp

--- src/inference/src/ie_network_reader.cpp
+++ src/inference/src/ie_network_reader.cpp
@@ -14,7 +14,7 @@
 #include "cnn_network_ngraph_impl.hpp"
 #include "cpp/ie_cnn_network.h"
 #include "dev/converter_utils.hpp"
-#include "file_utils.h"
+#include "openvino/util/file_util.hpp"
 #include "ie_api.h"
 #include "ie_common.h"
 #include "ie_icnn_network.hpp"
