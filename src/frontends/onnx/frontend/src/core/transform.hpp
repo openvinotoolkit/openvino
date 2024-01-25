bed8457525eb2550b2abc83e5a1e9dd87cf17160
Georgy Krivoruchko <georgy.krivoruchko@intel.com>
2024-01-24 22:26:30 -0800

    [ONNX] Extended ONNX Frontend with Function Gelu-20 (#22395)

--- src/frontends/onnx/frontend/src/core/transform.hpp
+++ src/frontends/onnx/frontend/src/core/transform.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2018-2023 Intel Corporation
+// Copyright (C) 2018-2024 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
 
@@ -14,7 +14,6 @@ static const std::vector<std::string> onnx_functions_to_expand = {"AffineGrid",
                                                                   "Bernoulli",
                                                                   "Celu",
                                                                   "CenterCropPad",
-                                                                  "Gelu",
                                                                   "NegativeLogLikelihoodLoss",
                                                                   "SoftmaxCrossEntropyLoss",
                                                                   "LayerNormalization"};
