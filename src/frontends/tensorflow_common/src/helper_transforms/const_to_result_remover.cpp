4232d0d44d1d4dcdf87224f01c2d6126c0b88909
Roman Kazantsev <roman.kazantsev@intel.com>
2024-01-25 21:13:45 +0400

    [TF FE] Avoid use of heavy opset header in transformations (#22407)
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/frontends/tensorflow_common/src/helper_transforms/const_to_result_remover.cpp
+++ src/frontends/tensorflow_common/src/helper_transforms/const_to_result_remover.cpp
@@ -5,10 +5,10 @@
 #include "helper_transforms/const_to_result_remover.hpp"
 
 #include "helper_ops/unsupported_constant.hpp"
-#include "openvino/opsets/opset10.hpp"
+#include "openvino/op/constant.hpp"
 
 using namespace std;
-using namespace ov::opset10;
+using namespace ov::op;
 
 namespace ov {
 namespace frontend {
@@ -23,7 +23,7 @@ bool ConstToResultRemover::run_on_model(const std::shared_ptr<ov::Model>& m) {
     // also, find isolated Constant->Result sub-graphs to remove
     for (const auto& result : m->get_results()) {
         auto unsupported_const = as_type_ptr<UnsupportedConstant>(result->get_input_node_shared_ptr(0));
-        auto const_node = as_type_ptr<Constant>(result->get_input_node_shared_ptr(0));
+        auto const_node = as_type_ptr<v0::Constant>(result->get_input_node_shared_ptr(0));
         if (unsupported_const || const_node) {
             results_to_remove.push_back(result);
         }
