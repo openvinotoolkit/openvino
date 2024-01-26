9d31e02fe2a9959aaa1274f973c597902b7904e1
Zhai, Xuejun <xuejun.zhai@intel.com>
2024-01-26 09:52:51 +0800

    Remove unused
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>

--- src/common/transformations/src/transformations/control_flow/unroll_if.cpp
+++ src/common/transformations/src/transformations/control_flow/unroll_if.cpp
@@ -54,7 +54,6 @@ bool ov::pass::UnrollIf::run_on_model(const std::shared_ptr<ov::Model>& f) {
         }
         for (const auto& output_desc : output_descriptions) {
             std::shared_ptr<ov::op::v0::Result> result = body->get_results()[output_desc->m_body_value_index];
-            const auto& in_value = result->input_value(0);
             for (const auto& input : if_node->output(output_desc->m_output_index).get_target_inputs()) {
                 input.replace_source_output(result->get_input_source_output(0));
             }
