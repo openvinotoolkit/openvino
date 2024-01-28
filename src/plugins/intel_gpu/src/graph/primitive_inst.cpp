dae937db74a483f5c334ac2badce80c6eec135a0
Taylor Yeonbok Lee <taylor.lee@intel.com>
2024-01-28 10:24:30 +0900

    Fix crash when _dynamic_impl is nullptr (#22471)

--- src/plugins/intel_gpu/src/graph/primitive_inst.cpp
+++ src/plugins/intel_gpu/src/graph/primitive_inst.cpp
@@ -822,8 +822,8 @@ bool primitive_inst::update_impl() {
                             // In the case of gemm, if current dynamic impl is not gemm_ref and newly chosen impl is gemm_ref,
                             // the newly chosen impl is not added to the impl cache for beffer performance.
                             if (_node->is_type<gemm>() &&
-                                _dynamic_impl->get_kernel_name().find("gemm_ref") == std::string::npos &&
-                                impl->get_kernel_name().find("gemm_ref") != std::string::npos) {
+                                    (_node->get_selected_impl() && _node->get_selected_impl()->get_kernel_name().find("gemm_ref") == std::string::npos) &&
+                                    impl->get_kernel_name().find("gemm_ref") != std::string::npos) {
                                 return;
                             }
                             if (impl->get_kernels_source().size() > 0) {
