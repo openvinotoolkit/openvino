90a258d4daaad2c2a172dcac0f323034b7f17ae0
Evgenya Nugmanova <evgeniia.nugmanova@intel.com>
2024-01-29 17:48:07 +0400

    Update src/frontends/onnx/frontend/src/op/layer_normalization.cpp

--- src/frontends/onnx/frontend/src/op/layer_normalization.cpp
+++ src/frontends/onnx/frontend/src/op/layer_normalization.cpp
@@ -38,7 +38,7 @@ ov::OutputVector layer_normalization(const Node& node) {
     const auto inputs = node.get_ng_inputs();
     const auto num_inputs = inputs.size();
     CHECK_VALID_NODE(node,
-                     num_inputs >= 2 && num_inputs <= 3,
+                     num_inputs == 2 || num_inputs == 3,
                      "LayerNormalization expects 2 or 3 input tensors. Got: ", num_inputs);
 
     const auto& X = inputs.at(0);
