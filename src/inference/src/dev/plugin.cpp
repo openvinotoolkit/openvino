71b53cbb49cc528b801d385ea84f8944057e16d1
River.Li <river.li@intel.com>
2024-01-30 20:49:42 +0800

    Fix build warning

--- src/inference/src/dev/plugin.cpp
+++ src/inference/src/dev/plugin.cpp
@@ -16,9 +16,9 @@
     OPENVINO_ASSERT(m_ptr != nullptr, "OpenVINO Runtime Plugin was not initialized."); \
     try {                                                                              \
         __VA_ARGS__;                                                                   \
-    } catch (const ov::NotImplemented& ex) {                                           \
+    } catch (const ov::NotImplemented&) {                                              \
         OPENVINO_NOT_IMPLEMENTED;                                                      \
-    } catch (const InferenceEngine::NotImplemented& ex) {                              \
+    } catch (const InferenceEngine::NotImplemented&) {                                 \
         OPENVINO_NOT_IMPLEMENTED;                                                      \
     } catch (const std::exception& ex) {                                               \
         OPENVINO_THROW(ex.what());                                                     \
