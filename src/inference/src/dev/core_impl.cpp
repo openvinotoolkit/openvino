81115f0d9da658508000dbedf130312ecfef11ca
wgzintel <guozhong.wang@intel.com>
2024-01-30 13:02:54 +0800

    clang format

--- src/inference/src/dev/core_impl.cpp
+++ src/inference/src/dev/core_impl.cpp
@@ -899,8 +899,8 @@ ov::SupportedOpsMap ov::CoreImpl::query_model(const std::shared_ptr<const ov::Mo
     OV_ITT_SCOPED_TASK(ov::itt::domains::OV, "Core::query_model");
     auto parsed = parseDeviceNameIntoConfig(device_name, config);
     auto plugin = get_plugin(parsed._deviceName);
-    // if plugin does not explicitly support cache_dir, and if plugin is not virtual, 
-    // we need to remove it from config
+    // if plugin does not explicitly support cache_dir, and if plugin is not virtual, we need to remove
+    // it from config
     if (!util::contains(plugin.get_property(ov::supported_properties), ov::cache_dir) &&
             !is_virtual_device(plugin.get_name())) {
             parsed._config.erase(ov::cache_dir.name());
