0cc22dc8467c0c078f7a5939e423d2a5e56e398a
Jorge Ragde <ragdejorge@gmail.com>
2024-01-31 11:09:14 -0800

    [TF FE] Support Conj operation (#21947)
    * Conjugate
    
    * Moved common logic into make_conj helper.
    
    * Update src/frontends/tensorflow_common/src/op/conj_transpose.cpp
    
    * Moved helper to conj_transpose
    
    * Applied helper to both conj and conj_transpose
    
    * Deleted conj.cpp
    
    * Update src/frontends/tensorflow_common/src/op/conj_transpose.cpp
    
    Co-authored-by: Roman Kazantsev <roman.kazantsev@intel.com>
    
    * Update src/frontends/tensorflow_common/src/op/conj_transpose.cpp
    
    Co-authored-by: Roman Kazantsev <roman.kazantsev@intel.com>
    
    * Removed additional Shape:: scope resolution from get_conj helper
    
    * Added Conj and ConjugateTranspose to supported ops
    
    * Update src/frontends/tensorflow/src/op_table.cpp
    
    Change "Conjugate" to "Conj"
    
    Co-authored-by: Roman Kazantsev <roman.kazantsev@intel.com>
    
    * Apply suggestions from code review
    
    * Removed perms call and moved test data directly to parametrize macro
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Changed input types from float32 to complex64
    
    * Changed input type back to np.float32 and removed real tensor test
    
    * Update src/frontends/tensorflow_common/src/op/conj_transpose.cpp
    
    ---------
    
    Co-authored-by: Roman Kazantsev <roman.kazantsev@intel.com>
    Co-authored-by: Michal Lukaszewski <michal.lukaszewski@intel.com>

--- src/frontends/tensorflow_common/src/op/conj_transpose.cpp
+++ src/frontends/tensorflow_common/src/op/conj_transpose.cpp
@@ -14,10 +14,47 @@
 using namespace std;
 using namespace ov::op;
 
+std::shared_ptr<ov::op::v0::Concat> get_conj_ptr(const ov::Output<ov::Node>& node) {
+    auto real_index = make_shared<ov::op::v0::Constant>(ov::element::i32, ov::Shape{1}, 0);
+    auto imag_index = make_shared<v0::Constant>(ov::element::i32, ov::Shape{1}, 1);
+
+    auto gather_axis = make_shared<v0::Constant>(ov::element::i32, ov::Shape{1}, -1);
+
+    auto real = make_shared<v8::Gather>(node, real_index, gather_axis)->output(0);
+    auto imag = make_shared<v8::Gather>(node, imag_index, gather_axis)->output(0);
+
+    imag = make_shared<v0::Negative>(imag);
+
+    auto conj = make_shared<v0::Concat>(ov::OutputVector{real, imag}, -1);
+    return conj;
+}
+
 namespace ov {
 namespace frontend {
 namespace tensorflow {
 namespace op {
+
+OutputVector translate_conj_op(const NodeContext& node) {
+    default_op_checks(node, 1, {"Conj"}, true);
+
+    auto x = node.get_input(0);
+
+    auto complex_type_mark = as_type_ptr<ComplexTypeMark>(x.get_node_shared_ptr());
+
+    std::shared_ptr<Node> conj{x.get_node_shared_ptr()};
+    if (complex_type_mark) {
+        element::Type complex_part_type = complex_type_mark->get_complex_part_type();
+        auto x = complex_type_mark->input_value(0);
+        auto conj = get_conj_ptr(x);
+
+        set_node_name(node.get_name(), conj);
+        auto complex_conj = make_shared<ComplexTypeMark>(conj, complex_part_type);
+        return {complex_conj->output(0)};
+    }
+
+    set_node_name(node.get_name(), conj);
+    return {conj};
+}
 
 OutputVector translate_conj_transpose_op(const NodeContext& node) {
     default_op_checks(node, 2, {"ConjugateTranspose"}, true);
@@ -29,18 +66,7 @@ OutputVector translate_conj_transpose_op(const NodeContext& node) {
     if (complex_type_mark) {
         element::Type complex_part_type = complex_type_mark->get_complex_part_type();
         auto x = complex_type_mark->input_value(0);
-
-        auto real_index = make_shared<v0::Constant>(element::i32, Shape{1}, 0);
-        auto imag_index = make_shared<v0::Constant>(element::i32, Shape{1}, 1);
-
-        auto gather_axis = make_shared<v0::Constant>(element::i32, Shape{1}, -1);
-
-        auto real = make_shared<v8::Gather>(x, real_index, gather_axis)->output(0);
-        auto imag = make_shared<v8::Gather>(x, imag_index, gather_axis)->output(0);
-
-        imag = make_shared<v0::Negative>(imag);
-
-        auto conj_tensor = make_shared<v0::Concat>(OutputVector{real, imag}, -1)->output(0);
+        auto conj_tensor = get_conj_ptr(x);
 
         OutputVector concat_inputs;
         concat_inputs.push_back(perm);
