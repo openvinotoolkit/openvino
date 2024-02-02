c29bc760b176bce1202dc51261bfcd49eb459f88
mitruska <katarzyna.mitrus@intel.com>
2024-02-02 20:20:08 +0100

    Comments cleanup

--- src/plugins/template/tests/functional/op_reference/base_reference_test.cpp
+++ src/plugins/template/tests/functional/op_reference/base_reference_test.cpp
@@ -79,30 +79,6 @@ void CommonReferenceTest::Validate() {
     }
 }
 
-// void compare_str(const ov::Tensor& expected, const ov::Tensor& actual) {
-//     const auto& expected_shape = expected.get_shape();
-//     const auto& actual_shape = actual.get_shape();
-//     if (expected_shape != actual_shape) {
-//         std::ostringstream out_stream;
-//         out_stream << "Expected and actual shape are different: " << expected_shape << " " << actual_shape;
-//         throw std::runtime_error(out_stream.str());
-//     }
-
-//     ASSERT_EQ(expected.get_element_type(), ov::element::string);
-//     ASSERT_EQ(actual.get_element_type(), ov::element::string);
-
-//     const auto expected_const = ov::op::v0::Constant(expected);
-//     const auto result_const = ov::op::v0::Constant(actual);
-
-//     EXPECT_EQ(expected_const.get_value_strings(), result_const.get_value_strings());
-
-//     // DEBUG // TO BE REMOVED
-//     for (size_t i = 0; i != shape_size(actual_shape); ++i) {
-//         std::cout << "result_const[i]: " << i << " " << result_const.convert_value_to_string(i) << std::endl;
-//         std::cout << "expected_const[i]: " << i << " " << expected_const.convert_value_to_string(i) << std::endl;
-//     }
-// }
-
 void CommonReferenceTest::ValidateBlobs(const ov::Tensor& refBlob,
                                         const ov::Tensor& outBlob,
                                         const size_t blob_idx,
