ca02fbcc97b57811c425517d8571e25b22120ca4
Katarzyna Mitrus <katarzyna.mitrus@intel.com>
2024-02-02 10:55:21 +0100

    [FP8] Enable FP8 Constant constructor with std::vector<ov::float8_e4m3/ov::float_e5m2> (#22555)
    ### Details:
    - This PR enable FP8 Constant constructor with
    `std::vector<ov::float8_e4m3/ov::float_e5m2>`
    - It was possible to create Constant passing `std::vector<float>` or
    data pointer,
    but not with `std::vector<ov::float8_e4m3/ov::float_e5m2>` (now enabled)
    
    ### Tickets:
     - 131208

--- src/core/include/openvino/op/constant.hpp
+++ src/core/include/openvino/op/constant.hpp
@@ -754,7 +754,8 @@ private:
               typename StorageDataType = fundamental_type_for<Type>,
               typename std::enable_if<Type == element::Type_t::nf4 &&
                                           (std::is_floating_point<T>::value || std::is_same<T, bfloat16>::value ||
-                                           std::is_same<T, float16>::value),
+                                           std::is_same<T, float16>::value || std::is_same<T, float8_e4m3>::value ||
+                                           std::is_same<T, float8_e5m2>::value),
                                       bool>::type = true>
     void write_buffer(const std::vector<T>& source) {
         auto p = get_data_ptr_nc<Type>();
