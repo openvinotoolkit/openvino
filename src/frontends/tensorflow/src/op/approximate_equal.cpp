dad195883b2ab734ebdcb5e69d769d8dc29cd125
Roman Kazantsev <roman.kazantsev@intel.com>
2024-02-04 10:37:48 +0400

    Apply suggestions from code review

--- src/frontends/tensorflow/src/op/approximate_equal.cpp
+++ src/frontends/tensorflow/src/op/approximate_equal.cpp
@@ -1,3 +1,7 @@
+// Copyright (C) 2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
 #include "common_op_table.hpp"
 #include "openvino/op/abs.hpp"
 #include "openvino/op/constant.hpp"
