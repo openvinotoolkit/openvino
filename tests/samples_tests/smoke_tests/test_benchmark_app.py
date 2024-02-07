9c91ebbc17c4d707d9f8d6a48a9f0c7f52a51b24
Wovchena <vladimir.zlobin@intel.com>
2024-02-07 20:50:21 +0400

    simplify

--- tests/samples_tests/smoke_tests/test_benchmark_app.py
+++ tests/samples_tests/smoke_tests/test_benchmark_app.py
@@ -12,12 +12,8 @@
 """
 import os
 import pytest
-import logging as log
-import sys
 from common.samples_common_test_class import SamplesCommonTestClass
 from common.samples_common_test_class import get_tests
-
-log.basicConfig(format="[ %(levelname)s ] %(message)s", level=log.INFO, stream=sys.stdout)
 
 test_data_fp32_async = get_tests \
     (cmd_params={'i': [os.path.join('227x227', 'dog.bmp')],
@@ -50,42 +46,8 @@ class TestBenchmarkApp(SamplesCommonTestClass):
         cls.sample_name = 'benchmark_app'
         super().setup_class()
 
-    @pytest.mark.parametrize("param", test_data_fp32_async)
-    def test_benchmark_app_sample_fp32_async(self, param):
-        _check_output(self, param)
-
-    @pytest.mark.parametrize("param", test_data_fp32_sync)
-    def test_benchmark_app_fp32_sync(self, param):
-        raise ''
-        _check_output(self, param)
-
-
-def _check_output(self, param):
-    """
-    Benchmark_app has functional and accuracy testing.
-    For accuracy the test checks if 'FPS' 'Latency' in output. If both exist - the est passed
-    """
-
-    # Run _test function, that returns stdout or 0.
-    stdout = self._test(param)
-    if not stdout:
-        return 0
-    stdout = stdout.split('\n')
-    is_ok = False
-    flag = 0
-    for line in stdout:
-        if 'FPS' in line:
-            is_ok = True
-    if is_ok == False:
-        flag = 1
-        log.error("No FPS in output")
-
-    is_ok = False
-    for line in stdout:
-        if 'Latency' in line:
-            is_ok = True
-    if is_ok == False:
-        flag = 1
-        log.error("No Latency in output")
-    assert flag == 0, "Wrong output of this sample"
-    log.info('Accuracy passed')
+    @pytest.mark.parametrize("param", [test_data_fp32_async, test_data_fp32_sync])
+    def test_benchmark_app(self, param):
+        stdout = self._test(param)
+        assert "FPS" in stdout
+        assert "Latency" in stdout
