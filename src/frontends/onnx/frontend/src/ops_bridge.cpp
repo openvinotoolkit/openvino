876a8ed08ec927bd27911b13e1d2252bc4d36729
Luca Tam <tutordimatematica.ing@gmail.com>
2024-02-07 23:44:29 +0100

    registered function in ops_bridge

--- src/frontends/onnx/frontend/src/ops_bridge.cpp
+++ src/frontends/onnx/frontend/src/ops_bridge.cpp
@@ -479,6 +479,9 @@ OperatorsBridge::OperatorsBridge() {
     REGISTER_OPERATOR("ReduceLogSumExp", 1, reduce_log_sum_exp);
     REGISTER_OPERATOR("ReduceL1", 1, reduce_l1);
     REGISTER_OPERATOR("ReduceL2", 1, reduce_l2);
+    REGISTER_OPERATOR("ReduceL2", 11, reduce_l2);
+    REGISTER_OPERATOR("ReduceL2", 13, reduce_l2);
+    REGISTER_OPERATOR("ReduceL2", 18, reduce_l2);
     REGISTER_OPERATOR("ReduceMax", 1, reduce_max);
     REGISTER_OPERATOR("ReduceMean", 1, reduce_mean);
     REGISTER_OPERATOR("ReduceMin", 1, reduce_min);
