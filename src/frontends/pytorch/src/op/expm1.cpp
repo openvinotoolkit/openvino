99e3e4e4b1ab9deeff57b8c2f17f7772c697a12d
Luca Tam <tutordimatematica.ing@gmail.com>
2024-02-07 18:11:58 +0100

    changed from opset_7 -> v0, input for num_input_check changed to 1(max)

--- src/frontends/pytorch/src/op/expm1.cpp
+++ src/frontends/pytorch/src/op/expm1.cpp
@@ -5,7 +5,7 @@
 #include "openvino/op/constant.hpp"
 #include "openvino/op/convert.hpp"
 #include "openvino/op/subtract.hpp"
-#include "openvino/opsets/opset7.hpp"
+#include "openvino/op/exp.hpp"
 #include "utils.hpp"
 
 namespace ov {
@@ -17,12 +17,12 @@ using namespace ov::op;
 
 
 OutputVector translate_expm1(const NodeContext& context) {
-    num_inputs_check(context, 1, false);
+    num_inputs_check(context, 1, 1);
     // "aten::expm1(Tensor self) -> Tensor"
 
     auto input = context.mark_node(std::make_shared<v0::Convert>(context.get_input(0), element::f32));
 
-    auto exp = context.mark_node(std::make_shared<opset7::Exp>(input));
+    auto exp = context.mark_node(std::make_shared<v0::Exp>(input));
     auto const_1 = v0::Constant::create(element::f32, Shape{}, {1});
     auto expm1 = context.mark_node(std::make_shared<v1::Subtract>(exp, const_1));
     return {expm1};
