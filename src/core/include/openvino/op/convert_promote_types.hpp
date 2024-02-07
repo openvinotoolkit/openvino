132a4ba9500439d1278c28bc5ffdff7e7dda20e2
Mateusz <mateusz.mikolajczyk@intel.com>
2024-02-07 19:49:59 +0100

    Improve ConvertPromoteTypes docstring

--- src/core/include/openvino/op/convert_promote_types.hpp
+++ src/core/include/openvino/op/convert_promote_types.hpp
@@ -9,22 +9,21 @@
 namespace ov {
 namespace op {
 namespace v14 {
-/// \brief Elementwise type alignment conversion operation.
+/// \brief Elementwise operation that promote and convert input types to one common datatype.
 /// \ingroup ov_ops_cpp_api
 class OPENVINO_API ConvertPromoteTypes : public Op {
 public:
     OPENVINO_OP("ConvertPromoteTypes", "opset14", op::Op);
 
-    /// \brief Constructs a type alignment and conversion operation.
+    /// \brief Constructs operation that promote and convert input types to one common datatype.
     ConvertPromoteTypes() = default;
-    /// \brief Constructs a type alignment and conversion operation.
-    /// \param input_0  Node with datatype to be aligned.
-    /// \param input_1  Node with datatype to be aligned.
+    /// \brief Constructs operation that promote and convert input types to one common datatype.
+    /// \param input_0  Node with datatype to be promoted.
+    /// \param input_1  Node with datatype to be promoted.
     /// \param promote_unsafe  Bool attribute whether to allow promotions that might result in bit-widening,
     /// precision loss and undefined behaviors.
-    /// \param pytorch_scalar_promotion  Bool attribute whether to align scalars using PyTorch-like rules.
-    /// \param u64_integer_promotion_target  Element type attribute to select alignment target for u64 and signed
-    /// integers.
+    /// \param pytorch_scalar_promotion  Bool attribute whether to promote scalar input to type provided by non-scalar input when number format is matching.
+    /// \param u64_integer_promotion_target  Element type attribute to select promotion result for u64 and signed integers.
     ConvertPromoteTypes(const Output<Node>& input_0,
                       const Output<Node>& input_1,
                       const bool promote_unsafe = false,
@@ -35,16 +34,22 @@ public:
 
     std::shared_ptr<Node> clone_with_new_inputs(const OutputVector& new_args) const override;
 
+    /// \brief Get bool attribute whether to promote scalar input to type provided by non-scalar input when number format is matching.
     bool get_pytorch_scalar_promotion() const;
 
+    /// \brief Set bool attribute whether to promote scalar input to type provided by non-scalar input when number format is matching.
     void set_pytorch_scalar_promotion(bool pytorch_scalar_promotion);
 
+    /// \brief Get bool attribute whether to allow promotions that might result in bit-widening, precision loss and undefined behaviors.
     bool get_promote_unsafe() const;
 
+    /// \brief Set bool attribute whether to allow promotions that might result in bit-widening, precision loss and undefined behaviors.
     void set_promote_unsafe(bool promote_unsafe);
 
+    /// \brief Get element type attribute to select promotion result for u64 and signed integers.
     const element::Type& get_u64_integer_promotion_target() const;
 
+    /// \brief Set element type attribute to select promotion result for u64 and signed integers.
     void set_u64_integer_promotion_target(const element::Type& u64_integer_promotion_target);
 
 private:
