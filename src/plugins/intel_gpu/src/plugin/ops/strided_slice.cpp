ec8657281aca4387be5259863247dc91b1f7b25d
Taylor Yeonbok Lee <taylor.lee@intel.com>
2024-02-09 17:23:43 +0900

    [GPU] Fix crash of strided_slice when input shape data is INT_MAX (#22743)
    ### Details:
    - Fix crash in the case when original constant of end data is
    i32::INT_MAX and it is converted to a normal bound of int64_t shape
    type.
    
    
    ### Tickets:
     - *ticket-id*

--- src/plugins/intel_gpu/src/plugin/ops/strided_slice.cpp
+++ src/plugins/intel_gpu/src/plugin/ops/strided_slice.cpp
@@ -12,6 +12,8 @@
 #include "intel_gpu/primitives/reshape.hpp"
 #include "intel_gpu/primitives/crop.hpp"
 
+#include <climits>
+
 namespace ov {
 namespace intel_gpu {
 
@@ -23,10 +25,30 @@ static void CreateStridedSliceOp(ProgramBuilder& p, const std::shared_ptr<ov::op
     auto output_pshape = op->get_output_partial_shape(0);
     auto input_pshape = op->get_input_partial_shape(0);
 
+    auto convert_max_val = [](int64_t casted_val, ov::element::Type type) {
+        // if the original const is int32_t and the value is max,
+        // it should be kept to set the shape as unbounded dynamic shape.
+        if ((type == ov::element::i32 && casted_val == INT_MAX) || (type == ov::element::u32 && casted_val == UINT_MAX))
+            return static_cast<int64_t>(LONG_MAX);
+        return casted_val;
+    };
+
     auto begin_constant = std::dynamic_pointer_cast<ov::op::v0::Constant>(op->input_value(1).get_node_shared_ptr());
-    std::vector<int64_t> begin = begin_constant ? begin_constant->cast_vector<int64_t>() : std::vector<int64_t>{};
+    std::vector<int64_t> begin;
+    if (begin_constant) {
+        auto const_vals = begin_constant->cast_vector<int64_t>();
+        for (auto val : const_vals) {
+            begin.push_back(convert_max_val(val, begin_constant->get_element_type()));
+        }
+    }
     auto end_constant = std::dynamic_pointer_cast<ov::op::v0::Constant>(op->input_value(2).get_node_shared_ptr());
-    std::vector<int64_t> end = end_constant ? end_constant->cast_vector<int64_t>() : std::vector<int64_t>{};
+    std::vector<int64_t> end;
+    if (end_constant) {
+       auto const_vals = end_constant->cast_vector<int64_t>();
+        for (auto val : const_vals) {
+            end.push_back(convert_max_val(val, end_constant->get_element_type()));
+        }
+    }
     auto stride_constant = std::dynamic_pointer_cast<ov::op::v0::Constant>(op->input_value(3).get_node_shared_ptr());
     std::vector<int64_t> strides = stride_constant ? stride_constant->cast_vector<int64_t>() : std::vector<int64_t>{};
 
