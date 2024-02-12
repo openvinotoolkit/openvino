6ff87ccdd4691d424c21f635166295e697c6fd59
sbalandi <sofya.balandina@intel.com>
2024-02-12 19:23:56 +0100

    fix comment

--- src/tests/functional/plugin/conformance/subgraphs_dumper/src/matchers/subgraph/read_value_assign.cpp
+++ src/tests/functional/plugin/conformance/subgraphs_dumper/src/matchers/subgraph/read_value_assign.cpp
@@ -9,25 +9,28 @@
 #include "matchers/subgraph/read_value_assign.hpp"
 #include "utils/model.hpp"
 
+#include "openvino/pass/manager.hpp"
+#include "openvino/pass/serialize.hpp"
+
 using namespace ov::tools::subgraph_dumper;
 
 std::vector<ReadValueAssignExtractor::ExtractedPattern>
 ReadValueAssignExtractor::extract(const std::shared_ptr<ov::Model> &model) {
-    struct PairChecker {
+    struct ReadValuePairs {
         int cnt_assign = 0;
         int cnt_read_val = 0;
         std::shared_ptr<ov::Node> rv;
         std::string variable_id;
     };
-    std::map<ov::op::util::Variable*, PairChecker>  pairs;
+    std::map<ov::op::util::Variable::Ptr, ReadValuePairs>  pairs;
     for (auto& node : model->get_ordered_ops()) {
         if (const auto& assign = std::dynamic_pointer_cast<ov::op::util::AssignBase>(node)) {
-            pairs[assign->get_variable().get()].cnt_assign++;
-            pairs[assign->get_variable().get()].variable_id = assign->get_variable()->get_info().variable_id;
+            pairs[assign->get_variable()].cnt_assign++;
+            pairs[assign->get_variable()].variable_id = assign->get_variable()->get_info().variable_id;
         } else if (const auto& read_value = std::dynamic_pointer_cast<ov::op::util::ReadValueBase>(node)) {
-            pairs[read_value->get_variable().get()].cnt_read_val++;
-            pairs[read_value->get_variable().get()].rv = read_value;
-            pairs[read_value->get_variable().get()].variable_id = read_value->get_variable()->get_info().variable_id;
+            pairs[read_value->get_variable()].cnt_read_val++;
+            pairs[read_value->get_variable()].rv = read_value;
+            pairs[read_value->get_variable()].variable_id = read_value->get_variable()->get_info().variable_id;
         }
     }
 
@@ -38,32 +41,30 @@ ReadValueAssignExtractor::extract(const std::shared_ptr<ov::Model> &model) {
             std::cout << "Check operations with id: " << pair.second.variable_id << std::endl;
             continue;
         }
-        // node, amount of analyzed outputs, amount of analyzed target inputs of output
-        std::vector<std::tuple<std::shared_ptr<ov::Node>, size_t, size_t>> bfs_queue;
-        bfs_queue.push_back({pair.second.rv, 0, 0});
+        // Breadth first search will find all nodes from ReadValue to Assign from pair
+        NodeVector bfs_queue;
+        bfs_queue.push_back(pair.second.rv);
 
         std::vector<std::shared_ptr<ov::Node>> all_extracted_nodes;
-
         while (bfs_queue.size() != 0) {
-            auto& node_element = bfs_queue.front();
-            auto node = std::get<0>(node_element);
+            auto node = bfs_queue.front();
             all_extracted_nodes.push_back(node);
             if (const auto& assign = std::dynamic_pointer_cast<ov::op::util::AssignBase>(node)) {
-                if (pairs[assign->get_variable().get()].rv &&
-                    pairs[assign->get_variable().get()].rv->get_friendly_name() == pair.second.rv->get_friendly_name()) {
+                if (assign->get_variable()->get_info().variable_id == pair.second.variable_id) {
                     break;
                 }
             }
 
             for (size_t i = 0; i < node->outputs().size(); i++) {
                 for (auto& out_node : node->get_output_target_inputs(i)) {
-                    bfs_queue.push_back({out_node.get_node()->shared_from_this() , 0, 0});
+                    bfs_queue.push_back(out_node.get_node()->shared_from_this());
                 }
             }
 
             bfs_queue.erase(bfs_queue.begin());
         }
 
+        // Reduce nodes, cross through nodes from Assign to ReadValue and remove all not essential
         NodeVector extracted_nodes;
         for (auto it = all_extracted_nodes.rbegin(); it != all_extracted_nodes.rend(); ++it) {
             auto temp_node = it->get();
@@ -71,9 +72,8 @@ ReadValueAssignExtractor::extract(const std::shared_ptr<ov::Model> &model) {
             std::string temp_node_name = temp_node->get_friendly_name();
             for (size_t i = 0; i < temp_node->outputs().size(); i++) {
                 auto target_inputs = temp_node->output(i).get_target_inputs();
-                for (auto& node : extracted_nodes) {
-                    std::string analyzed_node = node->get_friendly_name();
-                    for (auto& input : node->inputs()) {
+                for (auto& extr_node : extracted_nodes) {
+                    for (auto& input : extr_node->inputs()) {
                         if (target_inputs.count(input)) {
                             in_model = true;
                             break;
@@ -97,6 +97,5 @@ ReadValueAssignExtractor::extract(const std::shared_ptr<ov::Model> &model) {
             std::cout << "[ WARNING ] Impossible to generate network and add to GraphCache: " << e.what() << std::endl;
         }
     }
-
     return matched_patterns;
 }
