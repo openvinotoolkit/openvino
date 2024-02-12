0a4a49d30329fec7fedb1e4422027ab7e7a2f6ed
Viswanath <2100032168cser@gmail.com>
2024-02-12 11:20:52 +0530

    Commit message describing the changes

--- src/frontends/tensorflow/src/op/approximate_equal.cpp
+++ src/frontends/tensorflow/src/op/approximate_equal.cpp
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 #include "common_op_table.hpp"
-#include "openvino/op/abs.hpp"
+#include "openvino/op/abs.hpp"  
 #include "openvino/op/constant.hpp"
 #include "openvino/op/less.hpp"
 #include "openvino/op/select.hpp"
@@ -19,8 +19,6 @@ namespace op {
 OutputVector translate_approximate_equal_op(const NodeContext& node) {
     // Perform default checks for the operation
     default_op_checks(node, 2, {"ApproximateEqual"});
-
-    // Extract necessary attributes and inputs
     
     auto tolerance = node.get_attribute<float>("tolerance", 1e-5f);
     // Access inputs directly
@@ -28,10 +26,10 @@ OutputVector translate_approximate_equal_op(const NodeContext& node) {
     auto y = node.get_input(1);
 
     // Implement the logic for ApproximateEqual
-    auto difference = make_shared<Subtract>(x, y);
-    auto absolute = make_shared<Abs>(difference);
-    auto tolerance_constant = create_same_type_const_scalar(x, tolerance);
-     auto less = make_shared<Less>(absolute, tolerance_constant);
+    auto difference = make_shared<ov::op::v1::Subtract>(x, y);
+    auto absolute = make_shared<ov::op::v0::Abs>(difference);  
+    auto tolerance_constant = make_shared<ov::op::v0::Constant>(x.get_element_type(), Shape{}, vector<float>{tolerance});
+    auto less = make_shared<ov::op::v1::Less>(absolute, tolerance_constant);
 
     // Create and return the corresponding OpenVINO operation
     set_node_name(node.get_name(), less);
