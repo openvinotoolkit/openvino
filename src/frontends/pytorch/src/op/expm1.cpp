0db886c4d6b9efc1dc40f7ee61284d669b0d0697
Luca Tam <tutordimatematica.ing@gmail.com>
2024-02-12 23:07:27 +0100

    fix style

--- src/frontends/pytorch/src/op/expm1.cpp
+++ src/frontends/pytorch/src/op/expm1.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2018-2023 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
+//
 
 #include "openvino/frontend/pytorch/node_context.hpp"
 #include "openvino/op/constant.hpp"
@@ -17,20 +18,20 @@ using namespace ov::op;
 
 OutputVector translate_expm1(const NodeContext& context) {
     num_inputs_check(context, 1, 2);
-    // "aten::expm1(Tensor self) -> Tensor"
-
+    // aten::expm1(Tensor self) -> Tensor
+    // aten::expm1(Tensor self, Tensor out) -> Tensor
     auto input = context.mark_node(std::make_shared<v0::Convert>(context.get_input(0), element::f32));
 
     auto exp = context.mark_node(std::make_shared<v0::Exp>(input));
     auto const_1 = context.mark_node(v0::Constant::create(element::f32, Shape{}, {1}));
     auto expm1 = context.mark_node(std::make_shared<v1::Subtract>(exp, const_1));
-    if (!context.input_is_none(1)){
+    if (!context.input_is_none(1)) {
         context.mutate_input(1, expm1);
     }
     return {expm1};
 }
 
-} // namespace op
-} // namespace pytorch
-} // namespace frontend
-} // namespace ov
+}  // namespace op
+}  // namespace pytorch
+}  // namespace frontend
+}  // namespace ov
