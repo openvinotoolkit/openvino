7b16b31607197a24e092135898ba92dc4383868a
Aryan Pandey <92007507+Aryan8912@users.noreply.github.com>
2024-02-13 08:11:37 +0530

    Update matmul.cpp

--- src/frontends/pytorch/src/op/matmul.cpp
+++ src/frontends/pytorch/src/op/matmul.cpp
@@ -1,6 +1,7 @@
 #include "openvino/frontend/pytorch/node_context.hpp"
-#include "openvino/op/matmul.hpp"  // Updated header for MatMul
+#include "openvino/op/matmul.hpp" 
 #include "pt_framework_node.hpp"
+#include "openvino/op/convert.hpp" 
 #include "utils.hpp"
 
 namespace ov {
@@ -10,15 +11,16 @@ namespace op {
 using namespace ov::op;
 
 OutputVector translate_mv(const NodeContext& context) {
-    num_inputs_check(context, 2, 3);  // Corrected number of inputs for matrix multiplication
+    num_inputs_check(context, 2, 3);  
 
     auto matrix = context.mark_node(std::make_shared<v0::Convert>(context.get_input(0), element::f32));
     auto vector = context.mark_node(std::make_shared<v0::Convert>(context.get_input(1), element::f32));
 
-    // Updated operation to MatMul for matrix multiplication
+
     auto result = context.mark_node(std::make_shared<v1::MatMul>(matrix, vector));
 
-    // Check for the out parameter
+
+
     if (!context.input_is_none(2)) {
         context.mutate_input(2, result);
     }
