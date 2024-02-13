ae3597d36bad76d2348f3ab4cecee486f31edb4c
Vladislav Golubev <vladislav.golubev@intel.com>
2024-02-13 09:29:20 +0100

    [CPU] Fixed descriptor creation bug for 3D quantized deconvolution (#22731)
    ### Details:
     - *Added supported ranks in `canBeExecutedInInt8`*
     - *`getSupportedDescriptors`: added 3D case handling*
     - *Added 3D ConvolutionBackpropData test cases to LPT plugin tests*
    
    ### Tickets:
     - *CVS-132070*

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/low_precision_transformations/convolution_backprop_data_transformation.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/low_precision_transformations/convolution_backprop_data_transformation.cpp
@@ -100,21 +100,56 @@ const std::vector<LayerTestsDefinitions::ConvolutionBackpropDataTransformationPa
     }
 };
 
-const std::vector<std::pair<ov::PartialShape, bool>> inputShapes = {
+const std::vector<std::pair<ov::PartialShape, bool>> inputShapes_4D = {
     {{ 1, 8, 16, 16 }, true}
 };
 
-const std::vector<ov::Shape> outputShapes = {
+const std::vector<ov::Shape> outputShapes_4D = {
     { 16, 16 }
 };
 
-INSTANTIATE_TEST_SUITE_P(smoke_LPT, ConvolutionBackpropDataTransformation,
+INSTANTIATE_TEST_SUITE_P(smoke_LPT_4D, ConvolutionBackpropDataTransformation,
     ::testing::Combine(
         ::testing::ValuesIn(netPrecisions),
-        ::testing::ValuesIn(inputShapes),
-        ::testing::ValuesIn(outputShapes),
+        ::testing::ValuesIn(inputShapes_4D),
+        ::testing::ValuesIn(outputShapes_4D),
         ::testing::Values(ov::test::utils::DEVICE_CPU),
         ::testing::ValuesIn(trasformationParamValues),
         ::testing::ValuesIn(params)),
+    ConvolutionBackpropDataTransformation::getTestCaseName);
+
+const std::vector<std::pair<ov::PartialShape, bool>> inputShapes_3D = {
+    {{ 1, 8, 16 }, true}
+};
+
+const std::vector<ov::Shape> outputShapes_3D = {
+    { 16 }
+};
+
+const std::vector<LayerTestsDefinitions::ConvolutionBackpropDataTransformationParam> params_3D = {
+    // FQ on weights
+    {
+        {256ul, ov::Shape{1, 1, 1}, { 0.f }, { 25.5f }, { 0.f }, { 25.5f }},
+        {255ul, ov::Shape{1, 1, 1}, { -12.7f }, { 12.7f }, { -12.7f }, { 12.7f }},
+        "convolutionBackpropData_original",
+        "u8"
+    },
+    // Qdq on weights
+    {
+        {256ul, ov::Shape{1, 1, 1}, { 0.f }, { 255.f }, { 0.f }, { 25.5f }},
+        {{ov::element::f32}, {}, { {4.f}, ov::element::f32, {}, false }},
+        "convolutionBackpropData_original",
+        "u8"
+    },
+};
+
+INSTANTIATE_TEST_SUITE_P(smoke_LPT_3D, ConvolutionBackpropDataTransformation,
+    ::testing::Combine(
+        ::testing::ValuesIn(netPrecisions),
+        ::testing::ValuesIn(inputShapes_3D),
+        ::testing::ValuesIn(outputShapes_3D),
+        ::testing::Values(ov::test::utils::DEVICE_CPU),
+        ::testing::ValuesIn(trasformationParamValues),
+        ::testing::ValuesIn(params_3D)),
     ConvolutionBackpropDataTransformation::getTestCaseName);
 }  // namespace
