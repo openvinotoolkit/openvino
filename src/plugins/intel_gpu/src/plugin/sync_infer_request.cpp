36f467a49d175e473bc14f2732aadc35215ac39b
andrei-cv <andrei.gorbachev@intel.com>
2024-02-13 20:07:35 +0400

    fix

--- src/plugins/intel_gpu/src/plugin/sync_infer_request.cpp
+++ src/plugins/intel_gpu/src/plugin/sync_infer_request.cpp
@@ -659,7 +659,7 @@ std::vector<cldnn::event::ptr> SyncInferRequest::prepare_input(const std::string
         if (convert_needed) {
             m_plugin_inputs[name] = { create_device_tensor(pshape,
                                                            cldnn::element_type_to_data_type(element_type),
-                                                           true), TensorOwner::PLUGIN };
+                                                           false), TensorOwner::PLUGIN };
         } else {
             m_plugin_inputs[name] = user_tensor_wrapper;
         }
