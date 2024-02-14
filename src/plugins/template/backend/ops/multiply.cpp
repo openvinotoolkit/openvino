610d4c20db89dc275642493494eb815c84c2c164
Luca Tam <tutordimatematica.ing@gmail.com>
2024-02-14 19:30:18 +0100

    removed duplicated types

--- src/plugins/template/backend/ops/multiply.cpp
+++ src/plugins/template/backend/ops/multiply.cpp
@@ -20,38 +20,14 @@ bool evaluate_node<ov::op::v1::Multiply>(std::shared_ptr<ov::Node> node,
     auto element_type = node->get_output_element_type(0);
 
     switch (element_type) {
-    case ov::element::boolean:
-        return evaluate<ov::element::boolean>(ov::as_type_ptr<ov::op::v1::Multiply>(node), outputs, inputs);
-    case ov::element::bf16:
-        return evaluate<ov::element::bf16>(ov::as_type_ptr<ov::op::v1::Multiply>(node), outputs, inputs);
-    case ov::element::f16:
-        return evaluate<ov::element::f16>(ov::as_type_ptr<ov::op::v1::Multiply>(node), outputs, inputs);
-    case ov::element::f64:
-        return evaluate<ov::element::f64>(ov::as_type_ptr<ov::op::v1::Multiply>(node), outputs, inputs);
-    case ov::element::f32:
-        return evaluate<ov::element::f32>(ov::as_type_ptr<ov::op::v1::Multiply>(node), outputs, inputs);
-    case ov::element::i4:
-        return evaluate<ov::element::i4>(ov::as_type_ptr<ov::op::v1::Multiply>(node), outputs, inputs);
     case ov::element::i8:
         return evaluate<ov::element::i8>(ov::as_type_ptr<ov::op::v1::Multiply>(node), outputs, inputs);
     case ov::element::i16:
         return evaluate<ov::element::i16>(ov::as_type_ptr<ov::op::v1::Multiply>(node), outputs, inputs);
-    case ov::element::i32:
-        return evaluate<ov::element::i32>(ov::as_type_ptr<ov::op::v1::Multiply>(node), outputs, inputs);
-    case ov::element::i64:
-        return evaluate<ov::element::i64>(ov::as_type_ptr<ov::op::v1::Multiply>(node), outputs, inputs);
-    case ov::element::u1:
-        return evaluate<ov::element::u1>(ov::as_type_ptr<ov::op::v1::Multiply>(node), outputs, inputs);
-    case ov::element::u4:
-        return evaluate<ov::element::u4>(ov::as_type_ptr<ov::op::v1::Multiply>(node), outputs, inputs);
     case ov::element::u8:
         return evaluate<ov::element::u8>(ov::as_type_ptr<ov::op::v1::Multiply>(node), outputs, inputs);
     case ov::element::u16:
         return evaluate<ov::element::u16>(ov::as_type_ptr<ov::op::v1::Multiply>(node), outputs, inputs);
-    case ov::element::u32:
-        return evaluate<ov::element::u32>(ov::as_type_ptr<ov::op::v1::Multiply>(node), outputs, inputs);
-    case ov::element::u64:
-        return evaluate<ov::element::u64>(ov::as_type_ptr<ov::op::v1::Multiply>(node), outputs, inputs);
     default:
         OPENVINO_THROW("Unhandled data type ", node->get_element_type().get_type_name(), " in evaluate_node()");
     }
