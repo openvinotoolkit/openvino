4d8fb9b6f646ba47fc1f1274cb2c90360af1ef19
Aleksandr Voron <aleksandr.voron@intel.com>
2024-02-14 15:35:48 +0100

    init

--- src/plugins/intel_cpu/src/config.cpp
+++ src/plugins/intel_cpu/src/config.cpp
@@ -343,10 +343,7 @@ void Config::readProperties(const ov::AnyMap& prop, const ModelType modelType) {
         if (executionMode == ov::hint::ExecutionMode::PERFORMANCE) {
             inferencePrecision = ov::element::f32;
 #if defined(OV_CPU_ARM_ENABLE_FP16)
-            // fp16 precision is used as default precision on ARM for non-convolution networks
-            // fp16 ACL convolution is slower than fp32
-            if (modelType != ModelType::CNN)
-                inferencePrecision = ov::element::f16;
+            inferencePrecision = ov::element::f16;
 #else
             if (mayiuse(avx512_core_bf16))
                 inferencePrecision = ov::element::bf16;
