8e16059678f3463a65e60c3722d184867c1d5390
p-wysocki <przemyslaw.wysocki@intel.com>
2024-02-15 14:02:43 +0100

    Add Python tests

--- src/bindings/python/tests/test_graph/test_pooling.py
+++ src/bindings/python/tests/test_graph/test_pooling.py
@@ -5,7 +5,7 @@
 import numpy as np
 import pytest
 
-import openvino.runtime.opset8 as ov
+import openvino.runtime.opset14 as ov
 from openvino import Type
 
 
@@ -32,6 +32,66 @@ def test_avg_pool_2d(ndarray_1x1x4x4):
     assert node.get_output_element_type(0) == Type.f32
 
 
+def test_avg_pooling_3d_ceil_torch():
+    data = np.arange(0, 25, dtype=np.float32).reshape(1, 1, 5, 5)
+    param = ov.parameter(list(data.shape))
+    kernel_shape = [2, 2]
+    strides = [2, 2]
+    pads_begin = [1, 1]
+    pads_end = [1, 1]
+
+    node = ov.avg_pool(param, strides, pads_begin, pads_end, kernel_shape, False, "ceil_torch")
+    assert node.get_type_name() == "AvgPool"
+    assert node.get_output_size() == 1
+    assert list(node.get_output_shape(0)) == [1, 1, 3, 3]
+    assert node.get_output_element_type(0) == Type.f32
+
+
+def test_avg_pool_2d_ceil_torch():
+    input_data = np.arange(0, 243, dtype=np.float32).reshape(1, 3, 9, 9)
+    param = ov.parameter(input_data.shape, name="A", dtype=np.float32)
+    kernel_shape = [2, 2]
+    strides = [2, 2]
+    pads_begin = [1, 1]
+    pads_end = [1, 1]
+
+    node = ov.avg_pool(param, strides, pads_begin, pads_end, kernel_shape, False, "ceil_torch")
+    assert node.get_type_name() == "AvgPool"
+    assert node.get_output_size() == 1
+    assert list(node.get_output_shape(0)) == [1, 3, 5, 5]
+    assert node.get_output_element_type(0) == Type.f32
+
+
+def test_avg_pool_3x3_kernel_ceil_torch_strides():
+    input_data = np.arange(0, 300, dtype=np.float32).reshape(1, 3, 10, 10)
+    param = ov.parameter(input_data.shape, name="A", dtype=np.float32)
+    kernel_shape = [3, 3]
+    strides = [2, 2]
+    pads_begin = [0, 0]
+    pads_end = [0, 0]
+
+    node = ov.avg_pool(param, strides, pads_begin, pads_end, kernel_shape, False, "ceil_torch")
+    assert node.get_type_name() == "AvgPool"
+    assert node.get_output_size() == 1
+    assert list(node.get_output_shape(0)) == [1, 3, 5, 5]
+    assert node.get_output_element_type(0) == Type.f32
+
+
+def test_avg_pool_3x3_kernel_ceil_torch_no_strides():
+    input_data = np.arange(0, 300, dtype=np.float32).reshape(1, 3, 10, 10)
+    param = ov.parameter(input_data.shape, name="A", dtype=np.float32)
+    kernel_shape = [3, 3]
+    strides = [1, 1]
+    pads_begin = [0, 0]
+    pads_end = [0, 0]
+
+    node = ov.avg_pool(param, strides, pads_begin, pads_end, kernel_shape, False, "ceil_torch")
+    assert node.get_type_name() == "AvgPool"
+    assert node.get_output_size() == 1
+    assert list(node.get_output_shape(0)) == [1, 3, 8, 8]
+    assert node.get_output_element_type(0) == Type.f32
+
+
 def test_avg_pooling_3d(ndarray_1x1x4x4):
     data = ndarray_1x1x4x4
     data = np.broadcast_to(data, (1, 1, 4, 4, 4))
@@ -43,7 +103,7 @@ def test_avg_pooling_3d(ndarray_1x1x4x4):
     pads_end = [0] * spatial_dim_count
     exclude_pad = True
 
-    node = ov.avg_pool(param, strides, pads_begin, pads_end, kernel_shape, exclude_pad)
+    node = ov.avg_pool(param, strides, pads_begin, pads_end, kernel_shape, exclude_pad, "ceil_torch")
     assert node.get_type_name() == "AvgPool"
     assert node.get_output_size() == 1
     assert list(node.get_output_shape(0)) == [1, 1, 2, 2, 2]
@@ -263,5 +323,98 @@ def test_max_pool_same_lower_auto_pads():
     assert node.get_output_size() == 2
     assert list(node.get_output_shape(0)) == [1, 1, 4, 4]
     assert list(node.get_output_shape(1)) == [1, 1, 4, 4]
+    assert node.get_output_element_type(0) == Type.f32
+    assert node.get_output_element_type(1) == Type.i32
+
+
+def test_max_pool_strides_ceil_torch():
+    data = np.arange(0.5, 75, dtype=np.float32).reshape((1, 3, 5, 5))
+    strides = [2, 2]
+    dilations = [1, 1]
+    pads_begin = [1, 1]
+    pads_end = [1, 1]
+    kernel_shape = [2, 2]
+    rounding_type = "ceil_torch"
+    auto_pad = None
+    index_et = "i32"
+
+    data_node = ov.parameter(data.shape, name="A", dtype=np.float32)
+    node = ov.max_pool(
+        data_node,
+        strides,
+        dilations,
+        pads_begin,
+        pads_end,
+        kernel_shape,
+        rounding_type,
+        auto_pad,
+        index_et,
+    )
+    assert node.get_type_name() == "MaxPool"
+    assert node.get_output_size() == 2
+    assert list(node.get_output_shape(0)) == [1, 3, 3, 3]
+    assert list(node.get_output_shape(1)) == [1, 3, 3, 3]
+    assert node.get_output_element_type(0) == Type.f32
+    assert node.get_output_element_type(1) == Type.i32
+
+
+def test_max_pool_same_lower_auto_pads_ceil_torch():
+    data = np.arange(0.5, 16, dtype=np.float32).reshape((1, 1, 4, 4))
+    strides = [1, 1]
+    dilations = [1, 1]
+    pads_begin = [0, 0]
+    pads_end = [0, 0]
+    kernel_shape = [2, 2]
+    auto_pad = "same_lower"
+    rounding_type = "ceil_torch"
+    index_et = "i32"
+
+    data_node = ov.parameter(data.shape, name="A", dtype=np.float32)
+    node = ov.max_pool(
+        data_node,
+        strides,
+        dilations,
+        pads_begin,
+        pads_end,
+        kernel_shape,
+        rounding_type,
+        auto_pad,
+        index_et,
+    )
+    assert node.get_type_name() == "MaxPool"
+    assert node.get_output_size() == 2
+    assert list(node.get_output_shape(0)) == [1, 1, 4, 4]
+    assert list(node.get_output_shape(1)) == [1, 1, 4, 4]
+    assert node.get_output_element_type(0) == Type.f32
+    assert node.get_output_element_type(1) == Type.i32
+
+
+def test_max_pool_kernel_shape3x3_ceil_torch():
+    data = np.arange(0.5, 300, dtype=np.float32).reshape((1, 3, 10, 10))
+    strides = [1, 1]
+    dilations = [1, 1]
+    pads_begin = [0, 0]
+    pads_end = [0, 0]
+    kernel_shape = [3, 3]
+    rounding_type = "ceil_torch"
+    auto_pad = None
+    index_et = "i32"
+
+    data_node = ov.parameter(data.shape, name="A", dtype=np.float32)
+    node = ov.max_pool(
+        data_node,
+        strides,
+        dilations,
+        pads_begin,
+        pads_end,
+        kernel_shape,
+        rounding_type,
+        auto_pad,
+        index_et,
+    )
+    assert node.get_type_name() == "MaxPool"
+    assert node.get_output_size() == 2
+    assert list(node.get_output_shape(0)) == [1, 3, 8, 8]
+    assert list(node.get_output_shape(1)) == [1, 3, 8, 8]
     assert node.get_output_element_type(0) == Type.f32
     assert node.get_output_element_type(1) == Type.i32
