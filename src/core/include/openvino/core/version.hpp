6378e0a3b17331a38a32cdf915d6ac17868fe38e
Ilya Lavrenov <ilya.lavrenov@intel.com>
2024-02-15 17:27:33 +0400

    Updated OpenVINO version to 2024.1 on master (#22857)

--- src/core/include/openvino/core/version.hpp
+++ src/core/include/openvino/core/version.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2018-2023 Intel Corporation
+// Copyright (C) 2018-2024 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
 #pragma once
@@ -20,7 +20,7 @@
  */
 
 #define OPENVINO_VERSION_MAJOR 2024
-#define OPENVINO_VERSION_MINOR 0
+#define OPENVINO_VERSION_MINOR 1
 #define OPENVINO_VERSION_PATCH 0
 
 namespace ov {
