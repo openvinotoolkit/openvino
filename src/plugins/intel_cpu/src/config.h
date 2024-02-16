d90fa754c418194d5b23bdd981aec261ab57dc13
rafalstaroszczyk1 <83882627+rafalstaroszczyk1@users.noreply.github.com>
2024-02-16 09:14:15 +0100

    Update config.h

--- src/plugins/intel_cpu/src/config.h
+++ src/plugins/intel_cpu/src/config.h
@@ -55,7 +55,7 @@ struct Config {
     std::string dumpToDot = {};
     std::string device_id = {};
     float fcSparseWeiDecompressionRate = 1.0f;
-    uint64_t fcDynamicQuantizationGroupSize = 0;
+    uint64_t fcDynamicQuantizationGroupSize = 32;
     ov::element::Type kvCachePrecision = ov::element::f16;
 #if defined(OPENVINO_ARCH_X86_64)
     size_t rtCacheCapacity = 5000ul;
