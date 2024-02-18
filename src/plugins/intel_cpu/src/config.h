ae2a248daf13aa7ef9844d66f2b4a1863bfcd035
Shen, Wanglei <wanglei.shen@intel.com>
2024-02-18 21:57:34 +0800

    update default setting

--- src/plugins/intel_cpu/src/config.h
+++ src/plugins/intel_cpu/src/config.h
@@ -78,7 +78,7 @@ struct Config {
     ov::hint::SchedulingCoreType schedulingCoreType = ov::hint::SchedulingCoreType::ANY_CORE;
     bool enableHyperThreading = true;
     bool changedHyperThreading = false;
-    Config::MaxThreadsPerStream hintMaxThreadsPerStream = Config::MaxThreadsPerStream::AUTO;
+    Config::MaxThreadsPerStream hintMaxThreadsPerStream = Config::MaxThreadsPerStream::PER_SOCKET;
 #if defined(OPENVINO_ARCH_X86) || defined(OPENVINO_ARCH_X86_64)
     LPTransformsMode lpTransformsMode = LPTransformsMode::On;
 #else
