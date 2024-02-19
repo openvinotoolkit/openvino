0a3daa0e411e0ca1f356b6f89988c12aafe628b6
LucaTamSapienza <tutordimatematica.ing@gmail.com>
2024-02-19 23:17:52 +0100

    fixing test for input type

--- tests/layer_tests/pytorch_tests/test_expm1.py
+++ tests/layer_tests/pytorch_tests/test_expm1.py
@@ -34,10 +34,10 @@ class TestExpm1(PytorchLayerTest):
                     self.forward = self.forward_default
 
             def forward_default(self, x):
-                return torch.expm1(x.type(self.dtype))
+                return torch.expm1(x.type(self.dtype)).type(torch.float32)
 
             def forward_out(self, x, y):
-                return torch.expm1(x.type(self.dtype), out=y), y
+                return torch.expm1(x.type(self.dtype).type(torch.float32), out=y), y.type(torch.float32)
 
         model_class = aten_expm1(mode, dtype)
 
