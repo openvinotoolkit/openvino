5c3b29fbbd88afd13867ce98b864850a68e61486
p-wysocki <przemyslaw.wysocki@intel.com>
2024-02-20 13:39:06 +0100

    Update template plugin tests

--- src/plugins/template/backend/ops/avg_pool.cpp
+++ src/plugins/template/backend/ops/avg_pool.cpp
@@ -96,37 +96,37 @@ bool evaluate_node<ov::op::v14::AvgPool>(std::shared_ptr<ov::Node> node,
 
     switch (element_type) {
     case ov::element::boolean:
-        return evaluate<ov::element::boolean>(ov::as_type_ptr<ov::op::v1::AvgPool>(node), outputs, inputs);
+        return evaluate<ov::element::boolean>(ov::as_type_ptr<ov::op::v14::AvgPool>(node), outputs, inputs);
     case ov::element::bf16:
-        return evaluate<ov::element::bf16>(ov::as_type_ptr<ov::op::v1::AvgPool>(node), outputs, inputs);
+        return evaluate<ov::element::bf16>(ov::as_type_ptr<ov::op::v14::AvgPool>(node), outputs, inputs);
     case ov::element::f16:
-        return evaluate<ov::element::f16>(ov::as_type_ptr<ov::op::v1::AvgPool>(node), outputs, inputs);
+        return evaluate<ov::element::f16>(ov::as_type_ptr<ov::op::v14::AvgPool>(node), outputs, inputs);
     case ov::element::f64:
-        return evaluate<ov::element::f64>(ov::as_type_ptr<ov::op::v1::AvgPool>(node), outputs, inputs);
+        return evaluate<ov::element::f64>(ov::as_type_ptr<ov::op::v14::AvgPool>(node), outputs, inputs);
     case ov::element::f32:
-        return evaluate<ov::element::f32>(ov::as_type_ptr<ov::op::v1::AvgPool>(node), outputs, inputs);
+        return evaluate<ov::element::f32>(ov::as_type_ptr<ov::op::v14::AvgPool>(node), outputs, inputs);
     case ov::element::i4:
-        return evaluate<ov::element::i4>(ov::as_type_ptr<ov::op::v1::AvgPool>(node), outputs, inputs);
+        return evaluate<ov::element::i4>(ov::as_type_ptr<ov::op::v14::AvgPool>(node), outputs, inputs);
     case ov::element::i8:
-        return evaluate<ov::element::i8>(ov::as_type_ptr<ov::op::v1::AvgPool>(node), outputs, inputs);
+        return evaluate<ov::element::i8>(ov::as_type_ptr<ov::op::v14::AvgPool>(node), outputs, inputs);
     case ov::element::i16:
-        return evaluate<ov::element::i16>(ov::as_type_ptr<ov::op::v1::AvgPool>(node), outputs, inputs);
+        return evaluate<ov::element::i16>(ov::as_type_ptr<ov::op::v14::AvgPool>(node), outputs, inputs);
     case ov::element::i32:
-        return evaluate<ov::element::i32>(ov::as_type_ptr<ov::op::v1::AvgPool>(node), outputs, inputs);
+        return evaluate<ov::element::i32>(ov::as_type_ptr<ov::op::v14::AvgPool>(node), outputs, inputs);
     case ov::element::i64:
-        return evaluate<ov::element::i64>(ov::as_type_ptr<ov::op::v1::AvgPool>(node), outputs, inputs);
+        return evaluate<ov::element::i64>(ov::as_type_ptr<ov::op::v14::AvgPool>(node), outputs, inputs);
     case ov::element::u1:
-        return evaluate<ov::element::u1>(ov::as_type_ptr<ov::op::v1::AvgPool>(node), outputs, inputs);
+        return evaluate<ov::element::u1>(ov::as_type_ptr<ov::op::v14::AvgPool>(node), outputs, inputs);
     case ov::element::u4:
-        return evaluate<ov::element::u4>(ov::as_type_ptr<ov::op::v1::AvgPool>(node), outputs, inputs);
+        return evaluate<ov::element::u4>(ov::as_type_ptr<ov::op::v14::AvgPool>(node), outputs, inputs);
     case ov::element::u8:
-        return evaluate<ov::element::u8>(ov::as_type_ptr<ov::op::v1::AvgPool>(node), outputs, inputs);
+        return evaluate<ov::element::u8>(ov::as_type_ptr<ov::op::v14::AvgPool>(node), outputs, inputs);
     case ov::element::u16:
-        return evaluate<ov::element::u16>(ov::as_type_ptr<ov::op::v1::AvgPool>(node), outputs, inputs);
+        return evaluate<ov::element::u16>(ov::as_type_ptr<ov::op::v14::AvgPool>(node), outputs, inputs);
     case ov::element::u32:
-        return evaluate<ov::element::u32>(ov::as_type_ptr<ov::op::v1::AvgPool>(node), outputs, inputs);
+        return evaluate<ov::element::u32>(ov::as_type_ptr<ov::op::v14::AvgPool>(node), outputs, inputs);
     case ov::element::u64:
-        return evaluate<ov::element::u64>(ov::as_type_ptr<ov::op::v1::AvgPool>(node), outputs, inputs);
+        return evaluate<ov::element::u64>(ov::as_type_ptr<ov::op::v14::AvgPool>(node), outputs, inputs);
     default:
         OPENVINO_THROW("Unhandled data type ", node->get_element_type().get_type_name(), " in evaluate_node()");
     }
