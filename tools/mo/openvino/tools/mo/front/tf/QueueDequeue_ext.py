7e7087bc3c7721d17dcd21efaa00121f3daeba17
Jacek Pawlak <jacek.pawlak@intel.com>
2024-02-20 18:01:21 +0100

    Updated copyright year to 2024 (#22958)
    [CVS-132992](https://jira.devtools.intel.com/browse/CVS-132992)

--- tools/mo/openvino/tools/mo/front/tf/QueueDequeue_ext.py
+++ tools/mo/openvino/tools/mo/front/tf/QueueDequeue_ext.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018-2023 Intel Corporation
+# Copyright (C) 2018-2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 from openvino.tools.mo.front.extractor import FrontExtractorOp
 from openvino.tools.mo.front.tf.extractors.utils import tf_dtype_extractor, tf_tensor_shape
