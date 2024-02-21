d1c7256869e208b10cc6dadc3448a3de98e6b105
Pavel Esir <pavel.esir@intel.com>
2024-02-21 10:59:01 +0100

    check dtype as well

--- src/frontends/pytorch/src/op/mul.cpp
+++ src/frontends/pytorch/src/op/mul.cpp
@@ -17,8 +17,13 @@ using namespace ov::op;
 OutputVector translate_mul_common(const NodeContext& context, bool inplace) {
     auto lhs = context.get_input(0);
     auto rhs = context.get_input(1);
+    auto lhs_dtype = simplified_type_interpret(context.get_input_type(0));
+    auto rhs_dtype = simplified_type_interpret(context.get_input_type(1));
 
-    if (lhs.get_element_type() == ov::element::boolean && rhs.get_element_type() == ov::element::boolean) {
+    auto left_is_bool = lhs.get_element_type() == ov::element::boolean || (lhs_dtype.is<element::Type>() && lhs_dtype.as<element::Type>() == element::boolean);
+    auto right_is_bool = rhs.get_element_type() == ov::element::boolean || (rhs_dtype.is<element::Type>() && rhs_dtype.as<element::Type>() == element::boolean);
+    
+    if (left_is_bool && right_is_bool) {
         // if input types are boolean then aten::mul meand logical Add operation
         if (inplace)
             return op::inplace_translate_1to1_match_2_inputs_align_types<v1::LogicalAnd>(context);
