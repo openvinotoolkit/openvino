c56890424252a7347921ea004f14ec920a82dfad
siddhant-0707 <chauhanjoy10@gmail.com>
2024-02-22 20:35:50 +0530

    Update random_uniform_like.cpp

--- src/frontends/onnx/frontend/src/op/random_uniform_like.cpp
+++ src/frontends/onnx/frontend/src/op/random_uniform_like.cpp
@@ -1,10 +1,11 @@
-// Copyright (C) 2018-2023 Intel Corporation
+// Copyright (C) 2018-2024 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
 
 #include "op/random_uniform_like.hpp"
 
 #include "exceptions.hpp"
+#include "openvino/op/convert.hpp"
 #include "openvino/op/random_uniform.hpp"
 #include "openvino/op/shape_of.hpp"
 #include "utils/common.hpp"
@@ -21,6 +22,13 @@ ov::OutputVector random_uniform_like(const ov::frontend::onnx::Node& node) {
     ov::OutputVector inputs{node.get_ov_inputs()};
     const auto input = inputs.at(0);
 
+    const auto high_const = node.get_attribute_as_constant<float>("high", 1.0f);
+    const auto low_const = node.get_attribute_as_constant<float>("low", 0.0f);
+    const auto seed = node.get_attribute_value<float>("seed", 0.f);
+
+    const uint64_t global_seed = 0;
+    const auto seed_uint64 = static_cast<uint64_t>(seed * 1000);
+
     ov::element::Type target_type;
     if (node.has_attribute("dtype")) {
         const auto dtype = node.get_attribute_value<int64_t>("dtype");
@@ -29,18 +37,20 @@ ov::OutputVector random_uniform_like(const ov::frontend::onnx::Node& node) {
         target_type = input.get_element_type();
     }
 
+    std::shared_ptr<ov::Node> high_convert =
+        target_type == ov::element::f32
+            ? std::static_pointer_cast<ov::Node>(high_const)
+            : std::static_pointer_cast<ov::Node>(std::make_shared<v0::Convert>(high_const, target_type));
+    std::shared_ptr<ov::Node> low_convert =
+        target_type == ov::element::f32
+            ? std::static_pointer_cast<ov::Node>(low_const)
+            : std::static_pointer_cast<ov::Node>(std::make_shared<v0::Convert>(low_const, target_type));
+
     const auto target_shape = std::make_shared<v3::ShapeOf>(input);
 
-    const auto high_const = node.get_attribute_as_constant<float>("high", 1.0f);
-    const auto low_const = node.get_attribute_as_constant<float>("low", 0.0f);
-    const auto seed = node.get_attribute_value<float>("seed", 0.f);
-
-    const uint64_t global_seed = 0;
-    const auto seed_uint64 = static_cast<uint64_t>(seed * 1000);
-
     return {std::make_shared<v8::RandomUniform>(target_shape,
-                                                low_const,
-                                                high_const,
+                                                low_convert,
+                                                high_convert,
                                                 target_type,
                                                 global_seed,
                                                 seed_uint64)};
