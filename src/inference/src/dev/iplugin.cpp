0bd14090f1114f19de02ce35a3479a7aa12be860
Wang Wangwang <wangwang.wang@intel.com>
2024-02-22 16:26:56 +0800

    Add check to the model to make sure Assign and ReadValue in pairs in query model (#23002)
    ### Details:
    - *Check the ops to make sure Assign and ReadValue in pairs on the
    network*
    
    ### Tickets:
     - *95344*

--- src/inference/src/dev/iplugin.cpp
+++ src/inference/src/dev/iplugin.cpp
@@ -132,16 +132,50 @@ std::unordered_set<std::string> ov::get_supported_nodes(
         return !supported.count(node->input_values().begin()->get_node()->get_friendly_name());
     };
 
+    auto remove_op_from_supported = [&](const std::shared_ptr<ov::Node>& node) {
+        auto names = get_names_set(node);
+        for (auto& name : get_names_set(node)) {
+            supported.erase(name);
+        }
+    };
+
+    auto check_pairs = [](std::map<std::string, int> pair_checker) {
+        return std::all_of(pair_checker.begin(), pair_checker.end(), [](const std::pair<std::string, int>& val) {
+            return val.second == 2;
+        });
+    };
+
+    // Check the ops to make sure Assign and ReadValue operations in pairs on the network
+    std::map<std::string, int> pair_checker;
+    for (auto&& op : ops) {
+        if (supported.count(op->get_friendly_name())) {
+            if (const auto& assign = std::dynamic_pointer_cast<ov::op::util::VariableExtension>(op)) {
+                if (pair_checker.count(assign->get_variable_id()) == 0) {
+                    pair_checker[assign->get_variable_id()] = 1;
+                } else {
+                    pair_checker[assign->get_variable_id()]++;
+                }
+            }
+        }
+    }
+
+    if (!check_pairs(pair_checker)) {
+        for (auto& op : ops) {
+            if (const auto& assign = std::dynamic_pointer_cast<ov::op::util::VariableExtension>(op)) {
+                if (pair_checker[assign->get_variable_id()] == 1) {
+                    remove_op_from_supported(op);
+                }
+            }
+        }
+    }
+
     // Walk over transformed model for special handing of Parameters/Constants/Results
     for (auto&& op : ops) {
         // Mark Constants and all fused names as unsupported if they are have no
         // supported consumers/sources
         if (ov::op::util::is_constant(op)) {
             if (has_all_consumers_unsupported(op)) {
-                auto names = get_names_set(op);
-                for (auto& name : get_names_set(op)) {
-                    supported.erase(name);
-                }
+                remove_op_from_supported(op);
             }
         }
     }
