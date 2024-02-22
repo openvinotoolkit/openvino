264935ddb249ba696255ace055d7675fc06254c6
Andrei Kashchikhin <andrey.kashchikhin@intel.com>
2024-02-22 13:20:50 +0000

    try with shared git status

--- cmake/developer_package/compile_flags/sanitizer.cmake
+++ cmake/developer_package/compile_flags/sanitizer.cmake
@@ -18,11 +18,19 @@ if (ENABLE_SANITIZER)
         endif()
     elseif(CMAKE_COMPILER_IS_GNUCXX OR OV_COMPILER_IS_CLANG)
         set(SANITIZER_COMPILER_FLAGS "${SANITIZER_COMPILER_FLAGS} -fsanitize=address -fsanitize-blacklist=${PROJECT_SOURCE_DIR}/tests/asan/ignore.txt")
+        if(BUILD_SHARED_LIBS)
+            set(SANITIZER_COMPILER_FLAGS "${SANITIZER_COMPILER_FLAGS} -shared-libasan")
+        endif()
+
         check_cxx_compiler_flag("-fsanitize-recover=address" SANITIZE_RECOVER_ADDRESS_SUPPORTED)
         if (SANITIZE_RECOVER_ADDRESS_SUPPORTED)
             set(SANITIZER_COMPILER_FLAGS "${SANITIZER_COMPILER_FLAGS} -fsanitize-recover=address")
         endif()
+
         set(SANITIZER_LINKER_FLAGS "${SANITIZER_LINKER_FLAGS} -fsanitize=address -fsanitize-blacklist=${PROJECT_SOURCE_DIR}/tests/asan/ignore.txt")
+        if(BUILD_SHARED_LIBS)
+            set(SANITIZER_LINKER_FLAGS "${SANITIZER_LINKER_FLAGS} -shared-libasan")
+        endif()
     else()
         message(WARNING "Unsupported CXX compiler ${CMAKE_CXX_COMPILER_ID}")
     endif()
@@ -90,6 +98,13 @@ if(DEFINED SANITIZER_COMPILER_FLAGS)
         # prevent unloading libraries at runtime, so sanitizer can resolve their symbols
         if(NOT OV_COMPILER_IS_APPLECLANG)
             set(SANITIZER_LINKER_FLAGS "${SANITIZER_LINKER_FLAGS} -Wl,-z,nodelete")
+
+            # clang does not provide rpath if -shared-libasan is used
+            # https://stackoverflow.com/questions/68571138/asan-dynamic-runtime-is-missing-on-ubuntu-18, https://bugs.llvm.org/show_bug.cgi?id=51271
+            if(BUILD_SHARED_LIBS)
+                set(SANITIZER_LINKER_FLAGS "${SANITIZER_LINKER_FLAGS},-rpath=$(dirname $($CXX --print-file-name libclang_rt.asan-x86_64.so))")
+            endif()
+
             if(OV_COMPILER_IS_CLANG AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 8.0)
                 set(SANITIZER_LINKER_FLAGS "${SANITIZER_LINKER_FLAGS} -fuse-ld=lld")
             endif()
