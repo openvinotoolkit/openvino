78580d2a4f25ab04eda5e2f8cc071b58cde59a44
Vitaliy Urusovskij <vitaliy.urusovskij@intel.com>
2024-02-22 13:41:21 +0400

    [2024.0] Additional cleaning of `InferenceEngine` (#22959) (#22996)

--- src/core/src/op/util/framework_node.cpp
+++ src/core/src/op/util/framework_node.cpp
@@ -132,7 +132,7 @@ void ov::op::util::FrameworkNode::validate_and_infer_types() {
             out << "Input descriptor for " << get_friendly_name() << " node has been changed:" << std::endl;
             out << "Before: " << std::get<0>(m_inputs_desc[i]) << ", " << std::get<1>(m_inputs_desc[i]) << std::endl;
             out << "After:  " << input_pshape << ", " << input_type << std::endl;
-            out << "Please specify InferenceEngine Extensions to support this case.";
+            out << "Please specify OpenVINO Extensions to support this case.";
             return out.str();
         };
 
