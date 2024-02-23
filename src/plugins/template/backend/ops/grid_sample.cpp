b9b4297379b4fd616973731a52d6f403e09f12b3
Raasz, Pawel <pawel.raasz@intel.com>
2024-02-23 15:00:47 +0100

    GridSample in template plugin supports more precisions

--- src/plugins/template/backend/ops/grid_sample.cpp
+++ src/plugins/template/backend/ops/grid_sample.cpp
@@ -6,18 +6,48 @@
 
 #include "evaluate_node.hpp"
 
-template <ov::element::Type_t T>
+template <ov::element::Type_t DATA_ET>
 bool evaluate(const std::shared_ptr<ov::op::v9::GridSample>& op,
               ov::TensorVector& outputs,
               const ov::TensorVector& inputs) {
-    using ET = typename ov::element_type_traits<T>::value_type;
+    using DT = ov::fundamental_type_for<DATA_ET>;
     const auto& attributes = op->get_attributes();
-    ov::element::Type grid_et = op->get_input_element_type(1);
-    switch (grid_et) {
+
+    switch (op->get_input_element_type(1)) {
+    case ov::element::f16:
+        ov::reference::grid_sample(outputs[0].data<DT>(),
+                                   inputs[0].data<const DT>(),
+                                   inputs[1].data<const ov::fundamental_type_for<ov::element::f16>>(),
+                                   inputs[0].get_shape(),
+                                   inputs[1].get_shape(),
+                                   attributes.align_corners,
+                                   attributes.mode,
+                                   attributes.padding_mode);
+        break;
+    case ov::element::bf16:
+        ov::reference::grid_sample(outputs[0].data<DT>(),
+                                   inputs[0].data<const DT>(),
+                                   inputs[1].data<const ov::fundamental_type_for<ov::element::bf16>>(),
+                                   inputs[0].get_shape(),
+                                   inputs[1].get_shape(),
+                                   attributes.align_corners,
+                                   attributes.mode,
+                                   attributes.padding_mode);
+        break;
     case ov::element::f32:
-        ov::reference::grid_sample(outputs[0].data<ET>(),
-                                   inputs[0].data<ET>(),
-                                   inputs[1].data<float>(),
+        ov::reference::grid_sample(outputs[0].data<DT>(),
+                                   inputs[0].data<const DT>(),
+                                   inputs[1].data<const ov::fundamental_type_for<ov::element::f32>>(),
+                                   inputs[0].get_shape(),
+                                   inputs[1].get_shape(),
+                                   attributes.align_corners,
+                                   attributes.mode,
+                                   attributes.padding_mode);
+        break;
+    case ov::element::f64:
+        ov::reference::grid_sample(outputs[0].data<DT>(),
+                                   inputs[0].data<const DT>(),
+                                   inputs[1].data<const ov::fundamental_type_for<ov::element::f64>>(),
                                    inputs[0].get_shape(),
                                    inputs[1].get_shape(),
                                    attributes.align_corners,
@@ -34,23 +64,17 @@ template <>
 bool evaluate_node<ov::op::v9::GridSample>(std::shared_ptr<ov::Node> node,
                                            ov::TensorVector& outputs,
                                            const ov::TensorVector& inputs) {
-    auto element_type = node->get_output_element_type(0);
-    if (ov::is_type<ov::op::v1::Select>(node) || ov::is_type<ov::op::util::BinaryElementwiseComparison>(node))
-        element_type = node->get_input_element_type(1);
-
-    switch (element_type) {
+    switch (node->get_output_element_type(0)) {
     case ov::element::boolean:
         return evaluate<ov::element::boolean>(ov::as_type_ptr<ov::op::v9::GridSample>(node), outputs, inputs);
     case ov::element::bf16:
         return evaluate<ov::element::bf16>(ov::as_type_ptr<ov::op::v9::GridSample>(node), outputs, inputs);
     case ov::element::f16:
         return evaluate<ov::element::f16>(ov::as_type_ptr<ov::op::v9::GridSample>(node), outputs, inputs);
-    case ov::element::f64:
-        return evaluate<ov::element::f64>(ov::as_type_ptr<ov::op::v9::GridSample>(node), outputs, inputs);
     case ov::element::f32:
         return evaluate<ov::element::f32>(ov::as_type_ptr<ov::op::v9::GridSample>(node), outputs, inputs);
-    case ov::element::i4:
-        return evaluate<ov::element::i4>(ov::as_type_ptr<ov::op::v9::GridSample>(node), outputs, inputs);
+    case ov::element::f64:
+        return evaluate<ov::element::f64>(ov::as_type_ptr<ov::op::v9::GridSample>(node), outputs, inputs);
     case ov::element::i8:
         return evaluate<ov::element::i8>(ov::as_type_ptr<ov::op::v9::GridSample>(node), outputs, inputs);
     case ov::element::i16:
@@ -59,10 +83,6 @@ bool evaluate_node<ov::op::v9::GridSample>(std::shared_ptr<ov::Node> node,
         return evaluate<ov::element::i32>(ov::as_type_ptr<ov::op::v9::GridSample>(node), outputs, inputs);
     case ov::element::i64:
         return evaluate<ov::element::i64>(ov::as_type_ptr<ov::op::v9::GridSample>(node), outputs, inputs);
-    case ov::element::u1:
-        return evaluate<ov::element::u1>(ov::as_type_ptr<ov::op::v9::GridSample>(node), outputs, inputs);
-    case ov::element::u4:
-        return evaluate<ov::element::u4>(ov::as_type_ptr<ov::op::v9::GridSample>(node), outputs, inputs);
     case ov::element::u8:
         return evaluate<ov::element::u8>(ov::as_type_ptr<ov::op::v9::GridSample>(node), outputs, inputs);
     case ov::element::u16:
