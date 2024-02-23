b89cf2b66b6908e6241783981ac00dbfc35226eb
Kazantsev, Roman <roman.kazantsev@intel.com>
2024-02-23 14:08:39 +0400

    Use different type for shape
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- tests/layer_tests/tensorflow_tests/test_tf_RaggedTensorToTensor.py
+++ tests/layer_tests/tensorflow_tests/test_tf_RaggedTensorToTensor.py
@@ -22,7 +22,7 @@ class TestRaggedTensorToTensor(CommonTFLayerTest):
             inputs_data['values:0'] = rng.integers(0, 8, values_shape).astype(self.values_type)
         return inputs_data
 
-    def create_ragged_tensor_to_tensor_net(self, shape, values_shape, values_type, default_value,
+    def create_ragged_tensor_to_tensor_net(self, shape_type, shape_value, values_shape, values_type, default_value,
                                            row_partition_tensors, row_partition_types):
         self.values_type = values_type
         tf.compat.v1.reset_default_graph()
@@ -30,6 +30,7 @@ class TestRaggedTensorToTensor(CommonTFLayerTest):
         # Create the graph and model
         with tf.compat.v1.Session() as sess:
             values = tf.compat.v1.placeholder(values_type, values_shape, 'values')
+            shape = tf.constant(shape_value, dtype=shape_type)
             default_value = tf.constant(default_value, dtype=values_type)
             tf.raw_ops.RaggedTensorToTensor(shape=shape, values=values, default_value=default_value,
                                             row_partition_tensors=row_partition_tensors,
@@ -39,7 +40,8 @@ class TestRaggedTensorToTensor(CommonTFLayerTest):
 
         return tf_net, None
 
-    @pytest.mark.parametrize('shape', [[4, 8], [-1, 64]])
+    @pytest.mark.parametrize('shape_type', [np.int32, np.int64])
+    @pytest.mark.parametrize('shape_value', [[4, 8], [-1, 64]])
     @pytest.mark.parametrize('values_shape', [[40], [100]])
     @pytest.mark.parametrize('values_type', [np.float32, np.int32, np.int64])
     @pytest.mark.parametrize('default_value', [-1, 0])
@@ -48,11 +50,12 @@ class TestRaggedTensorToTensor(CommonTFLayerTest):
     @pytest.mark.precommit_tf_fe
     @pytest.mark.nightly
     @pytest.mark.xfail(reason='132675 - Add support of RaggedTensorToTensor')
-    def test_ragged_tensor_to_tensor(self, shape, values_shape, values_type, default_value,
+    def test_ragged_tensor_to_tensor(self, shape_type, shape_value, values_shape, values_type, default_value,
                                      row_partition_tensors, row_partition_types,
                                      ie_device, precision, ir_version, temp_dir, use_legacy_frontend):
-        self._test(*self.create_ragged_tensor_to_tensor_net(shape=shape, values_shape=values_shape,
-                                                            values_type=values_type, default_value=default_value,
+        self._test(*self.create_ragged_tensor_to_tensor_net(shape_type=shape_type, shape_value=shape_value,
+                                                            values_shape=values_shape, values_type=values_type,
+                                                            default_value=default_value,
                                                             row_partition_tensors=row_partition_tensors,
                                                             row_partition_types=row_partition_types),
                    ie_device, precision, ir_version, temp_dir=temp_dir,
