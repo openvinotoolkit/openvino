0a062acfa09cd3fcab33f207757c9df4c46683a9
Roman Kazantsev <roman.kazantsev@intel.com>
2024-02-24 23:47:50 +0400

    Update tests/layer_tests/tensorflow_tests/test_tf_angle.py

--- tests/layer_tests/tensorflow_tests/test_tf_angle.py
+++ tests/layer_tests/tensorflow_tests/test_tf_angle.py
@@ -39,7 +39,7 @@ class TestAngle(CommonTFLayerTest):
     @pytest.mark.parametrize("params", test_data_basic)
     @pytest.mark.precommit_tf_fe
     @pytest.mark.nightly
-    def test_atan2_basic(self, params, ie_device, precision, ir_version, temp_dir,
+    def test_angle(self, params, ie_device, precision, ir_version, temp_dir,
                                use_new_frontend, use_old_api):
         self._test(*self.create_atan2_net(**params),
                    ie_device, precision, ir_version, temp_dir=temp_dir,
