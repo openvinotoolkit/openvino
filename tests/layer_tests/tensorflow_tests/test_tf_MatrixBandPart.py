01b0c4382c83b3d353320e9c95be06fc9c7acc32
Kazantsev, Roman <roman.kazantsev@intel.com>
2024-02-26 17:42:31 +0400

    Extend layer tests for MatrixBandPart
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- tests/layer_tests/tensorflow_tests/test_tf_MatrixBandPart.py
+++ tests/layer_tests/tensorflow_tests/test_tf_MatrixBandPart.py
@@ -6,33 +6,36 @@ import pytest
 import tensorflow as tf
 from common.tf_layer_test_class import CommonTFLayerTest
 
+rng = np.random.default_rng()
+
+
 class TestMatrixBandPart(CommonTFLayerTest):
     def _prepare_input(self, inputs_info):
         assert 'input:0' in inputs_info
         input_shape = inputs_info['input:0']
         inputs_data = {}
-        inputs_data['input:0'] = np.random.randint(-50, 50, input_shape).astype(np.float32)
+        inputs_data['input:0'] = rng.integers(-8, 8, input_shape).astype(self.input_type)
         return inputs_data
 
-    def create_matrix_band_part_net(self, input_shape):
+    def create_matrix_band_part_net(self, input_shape, input_type, num_lower, num_upper):
+        self.input_type = input_type
         tf.compat.v1.reset_default_graph()
         with tf.compat.v1.Session() as sess:
-            input_tensor = tf.compat.v1.placeholder(tf.float32, input_shape, 'input')
-            tf.raw_ops.MatrixBandPart(input=input_tensor, num_lower=-1, num_upper=0)
+            input_tensor = tf.compat.v1.placeholder(input_type, input_shape, 'input')
+            tf.raw_ops.MatrixBandPart(input=input_tensor, num_lower=num_lower, num_upper=num_upper)
             tf.compat.v1.global_variables_initializer()
             tf_net = sess.graph_def
         return tf_net, None
 
-    test_data_basic = [
-        dict(input_shape=[5, 5]),
-        dict(input_shape=[3, 4, 4]),
-        dict(input_shape=[2, 3, 5, 5]),
-    ]
-
-    @pytest.mark.parametrize("params", test_data_basic)
+    @pytest.mark.parametrize('input_shape', [[5, 5], [3, 4, 4], [1, 2, 5, 5], [3, 5, 4]])
+    @pytest.mark.parametrize('input_type', [np.float32, np.int32])
+    @pytest.mark.parametrize('num_lower', [-4, -1, 0, 1, 4])
+    @pytest.mark.parametrize('num_upper', [-4, -1, 0, 1, 4])
     @pytest.mark.precommit_tf_fe
     @pytest.mark.nightly
-    def test_matrix_band_part_basic(self, params, ie_device, precision, ir_version, temp_dir, use_legacy_frontend):
-        self._test(*self.create_matrix_band_part_net(**params),
+    def test_matrix_band_part_basic(self, input_shape, input_type, num_lower, num_upper,
+                                    ie_device, precision, ir_version, temp_dir,
+                                    use_legacy_frontend):
+        self._test(*self.create_matrix_band_part_net(input_shape, input_type, num_lower, num_upper),
                    ie_device, precision, ir_version, temp_dir=temp_dir,
                    use_legacy_frontend=use_legacy_frontend)
