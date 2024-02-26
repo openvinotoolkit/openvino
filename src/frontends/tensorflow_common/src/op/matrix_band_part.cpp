ec23766a2b7002b20df56eb81f200bf7dd6fdc93
Kazantsev, Roman <roman.kazantsev@intel.com>
2024-02-26 12:52:27 +0400

    Fix translator
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/frontends/tensorflow_common/src/op/matrix_band_part.cpp
+++ src/frontends/tensorflow_common/src/op/matrix_band_part.cpp
@@ -1,12 +1,19 @@
 // Copyright (C) 2024 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-#include "openvino/op/logical_and.hpp"
-#include "openvino/op/less_equal.hpp"
-#include "openvino/op/unsqueeze.hpp"
 #include "openvino/op/constant.hpp"
-#include "openvino/op/concat.hpp"
+#include "openvino/op/convert.hpp"
+#include "openvino/op/less.hpp"
+#include "openvino/op/less_eq.hpp"
+#include "openvino/op/logical_and.hpp"
+#include "openvino/op/logical_or.hpp"
+#include "openvino/op/range.hpp"
 #include "openvino/op/reshape.hpp"
+#include "openvino/op/select.hpp"
+#include "openvino/op/shape_of.hpp"
+#include "openvino/op/slice.hpp"
+#include "openvino/op/subtract.hpp"
+#include "openvino/op/unsqueeze.hpp"
 #include "utils.hpp"
 
 using namespace std;
@@ -19,73 +26,60 @@ namespace op {
 
 OutputVector translate_matrix_band_part_op(const NodeContext& node) {
     default_op_checks(node, 3, {"MatrixBandPart"});
+
     // Input tensor and parameters
     auto input = node.get_input(0);
-    
     auto num_lower = node.get_input(1);
     auto num_upper = node.get_input(2);
 
-    // Get the shape of the input tensor
-    auto input_shape = make_shared<v3::ShapeOf>(input);
+    // create scalar auxiliary constants
+    auto const_zero = make_shared<v0::Constant>(element::i64, Shape{}, 0);
+    auto const_one = make_shared<v0::Constant>(element::i64, Shape{}, 1);
+    auto const_two = make_shared<v0::Constant>(element::i64, Shape{}, 2);
 
-    // Find sizes m and n of the last two dimensions
-    auto m = make_shared<v1::StridedSlice>(input_shape,
-                                           make_shared<v0::Constant>(element::i64, Shape{1}, {-2}),
-                                           make_shared<v0::Constant>(element::i64, Shape{1}, {-1}),
-                                           make_shared<v0::Constant>(element::i64, Shape{1}, {1}),
-                                           std::vector<int64_t>({0}),
-                                           std::vector<int64_t>({1}));
+    // input has a shape [I, J, K, ..., M, N]
+    // compute sizes of two last dimensions of M and N
+    auto input_shape = make_shared<v3::ShapeOf>(input, element::i64);
+    auto input_rank = make_shared<v3::ShapeOf>(input_shape, element::i64);
+    auto input_rank_minus_one = make_shared<v1::Subtract>(input_rank, const_one);
+    auto input_rank_minus_two = make_shared<v1::Subtract>(input_rank, const_two);
+    auto slice_step = make_shared<v0::Constant>(element::i64, Shape{1}, 1);
+    auto slice_axis = make_shared<v0::Constant>(element::i64, Shape{1}, 0);
+    auto m = make_shared<v8::Slice>(input_shape, input_rank_minus_two, input_rank_minus_one, slice_step, slice_axis)
+                 ->output(0);
+    auto n = make_shared<v8::Slice>(input_shape, input_rank_minus_one, input_rank, slice_step, slice_axis)->output(0);
 
-    auto n = make_shared<v1::StridedSlice>(input_shape,
-                                           make_shared<v0::Constant>(element::i64, Shape{1}, {-1}),
-                                           make_shared<v0::Constant>(element::i64, Shape{1}, {}),
-                                           make_shared<v0::Constant>(element::i64, Shape{1}, {1}),
-                                           std::vector<int64_t>({0}),
-                                           std::vector<int64_t>({1}));
+    // generate ranges [0, M) and [0, N)
+    auto scalar_shape = make_shared<v0::Constant>(element::i64, Shape{0}, vector<int64_t>{});
+    m = make_shared<v1::Reshape>(m, scalar_shape, false);
+    n = make_shared<v1::Reshape>(n, scalar_shape, false);
+    auto range_m = make_shared<v4::Range>(const_zero, m, const_one, element::i64)->output(0);
+    auto range_n = make_shared<v4::Range>(const_zero, n, const_one, element::i64)->output(0);
+    range_m = make_shared<v0::Unsqueeze>(range_m, const_one);
+    range_n = make_shared<v0::Unsqueeze>(range_n, const_zero);
 
-    // Generate ranges [0, m) and [0, n)
-    auto range_m = make_shared<v1::Range>(m);
-    auto range_n = make_shared<v1::Range>(n);
+    // adjust num_lower and num_upper to have them of type i64
+    // the same as M and N
+    // it is needed for in_band computation
+    num_lower = make_shared<v0::Convert>(num_lower, element::i64);
+    num_upper = make_shared<v0::Convert>(num_upper, element::i64);
 
-    // Unsqueeze ranges to have tensors of shapes [m, 1] and [1, n]
-    auto unsqueeze_range_m = make_shared<v0::Unsqueeze>(range_m, make_shared<v0::Constant>(element::i64, Shape{1}, {1}));
-    auto unsqueeze_range_n = make_shared<v0::Unsqueeze>(range_n, make_shared<v0::Constant>(element::i64, Shape{1}, {0}));
+    // compute in_band(m, n) = (num_lower < 0 || (m-n) <= num_lower)) && (num_upper < 0 || (n-m) <= num_upper)
+    auto num_lower_less_zero = make_shared<v1::Less>(num_lower, const_zero);
+    auto i_minus_j = make_shared<v1::Subtract>(range_m, range_n);
+    auto i_minus_j_less_eq_num_lower = make_shared<v1::LessEqual>(i_minus_j, num_lower);
+    auto num_upper_less_zero = make_shared<v1::Less>(num_upper, const_zero);
+    auto j_minus_i = make_shared<v1::Subtract>(range_n, range_m);
+    auto j_minus_i_less_eq_num_upper = make_shared<v1::LessEqual>(j_minus_i, num_upper);
+    auto in_band1 = make_shared<v1::LogicalOr>(num_lower_less_zero, i_minus_j_less_eq_num_lower);
+    auto in_band2 = make_shared<v1::LogicalOr>(num_upper_less_zero, j_minus_i_less_eq_num_upper);
+    auto in_band = make_shared<v1::LogicalAnd>(in_band1, in_band2);
 
-    // Create indicator function using logical operations
-    // Create indicator function using logical operations
-    auto zero = make_shared<v1::Constant>(element::i64, Shape{}, {0});
+    // create zero constant of the same type as input
+    auto zero = create_same_type_const_scalar<int32_t>(input, 0);
 
-    auto in_band_rows = make_shared<v1::LogicalAnd>(
-        make_shared<v1::LogicalOr>(
-        make_shared<v1::Less>(num_lower, zero),
-        make_shared<v1::LessEqual>(make_shared<v1::Subtract>(m, n), zero)
-        ),
-        make_shared<v1::LogicalOr>(
-        make_shared<v1::Less>(num_upper, zero),
-        make_shared<v1::LessEqual>(make_shared<v1::Subtract>(n, m), zero)
-        )
-    );
+    auto result = make_shared<v1::Select>(in_band, input, zero);
 
-    auto in_band_cols = make_shared<v1::LogicalAnd>(
-        make_shared<v1::LogicalOr>(
-            make_shared<v1::Less>(num_lower, zero),
-            make_shared<v1::LessEqual>(make_shared<v1::Subtract>(m, n), zero)
-        ),
-        make_shared<v1::LogicalOr>(
-            make_shared<v1::Less>(num_upper, zero),
-            make_shared<v1::LessEqual>(make_shared<v1::Subtract>(n, m), zero)
-        )
-    );
-
-    auto in_band_indicator = make_shared<v1::LogicalAnd>(in_band_rows, in_band_cols);
-
-    // Unsqueeze the indicator function to have a tensor of shape [1,..,1, m, n]
-    auto unsqueeze_indicator = make_shared<v0::Unsqueeze>(in_band_indicator, make_shared<v0::Constant>(element::i64, Shape{}, {1}));
-
-    // Extract or modify the elements based on the indicator function
-    auto result = make_shared<v3::Where>(unsqueeze_indicator, input, make_shared<v0::Constant>(input->get_element_type(), Shape{}, {0}));
-
-    // Return the result
     set_node_name(node.get_name(), result);
     return {result};
 }
