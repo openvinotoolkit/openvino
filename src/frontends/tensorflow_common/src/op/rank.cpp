5dafc03efef2804f6480ad100359a54fc9b035c2
Rinne <AsakusaRinne@gmail.com>
2024-02-27 08:36:11 +0800

    feat: add complex support for rank op.

--- src/frontends/tensorflow_common/src/op/rank.cpp
+++ src/frontends/tensorflow_common/src/op/rank.cpp
@@ -3,8 +3,11 @@
 //
 
 #include "common_op_table.hpp"
+#include "helper_ops/complex_type_mark.hpp"
+#include "openvino/op/constant.hpp"
 #include "openvino/op/shape_of.hpp"
 #include "openvino/op/squeeze.hpp"
+#include "openvino/util/log.hpp"
 
 using namespace std;
 using namespace ov::op;
@@ -14,17 +17,38 @@ namespace frontend {
 namespace tensorflow {
 namespace op {
 
-ov::OutputVector translate_rank_op(const NodeContext& node) {
-    default_op_checks(node, 1, {"Rank"});
-    auto input = node.get_input(0);
-    auto input_shape = make_shared<v3::ShapeOf>(input, ov::element::i32);
-    auto unsqueeze_input_rank = make_shared<v3::ShapeOf>(input_shape, ov::element::i32);
+ov::OutputVector translate_rank_op(const NodeContext &node) {
+  OPENVINO_DEBUG << "Translating Rank...............";
+  default_op_checks(node, 1, {"Rank"}, true);
+  auto input = node.get_input(0);
+  auto complex_type_mark =
+      as_type_ptr<ComplexTypeMark>(input.get_node_shared_ptr());
+  auto input_shape = make_shared<v3::ShapeOf>(input, ov::element::i32);
+  if (complex_type_mark) {
+    element::Type complex_part_type =
+        complex_type_mark->get_complex_part_type();
+    input = complex_type_mark->input_value(0);
+
+    auto unsqueeze_input_rank =
+        make_shared<v3::ShapeOf>(input_shape, ov::element::i32);
     auto input_rank = make_shared<v0::Squeeze>(unsqueeze_input_rank);
+    input_rank = make_shared<v0::Squeeze>(
+        input_rank,
+        make_shared<v0::Constant>(ov::element::i32, Shape{1},
+                                  input_shape->get_input_size() - 1));
     set_node_name(node.get_name(), input_rank);
-    return {input_rank};
+    auto complex_rank =
+        make_shared<ComplexTypeMark>(input_rank, complex_part_type);
+    return {complex_rank->output(0)};
+  }
+  auto unsqueeze_input_rank =
+      make_shared<v3::ShapeOf>(input_shape, ov::element::i32);
+  auto input_rank = make_shared<v0::Squeeze>(unsqueeze_input_rank);
+  set_node_name(node.get_name(), input_rank);
+  return {input_rank};
 }
 
-}  // namespace op
-}  // namespace tensorflow
-}  // namespace frontend
-}  // namespace ov
+} // namespace op
+} // namespace tensorflow
+} // namespace frontend
+} // namespace ov
