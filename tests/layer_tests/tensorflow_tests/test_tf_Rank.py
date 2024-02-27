90208e0125bf7e9108a090c4acc9d6b1a9f6f09f
Rinne <AsakusaRinne@gmail.com>
2024-02-28 01:46:34 +0800

    feat: support complex type for rank op.

--- tests/layer_tests/tensorflow_tests/test_tf_Rank.py
+++ tests/layer_tests/tensorflow_tests/test_tf_Rank.py
@@ -7,38 +7,36 @@ import tensorflow as tf
 from common.tf_layer_test_class import CommonTFLayerTest
 
 
-# class TestRank(CommonTFLayerTest):
-#     def create_rank_net(self, input_shape, input_type):
-#         tf.compat.v1.reset_default_graph()
-#         # Create the graph and model
-#         with tf.compat.v1.Session() as sess:
-#             input = tf.compat.v1.placeholder(input_type, input_shape, 'input')
-#             tf.raw_ops.Rank(input=input)
-#             tf.compat.v1.global_variables_initializer()
-#             tf_net = sess.graph_def
+class TestRank(CommonTFLayerTest):
+    def create_rank_net(self, input_shape, input_type):
+        tf.compat.v1.reset_default_graph()
+        # Create the graph and model
+        with tf.compat.v1.Session() as sess:
+            input = tf.compat.v1.placeholder(input_type, input_shape, 'input')
+            tf.raw_ops.Rank(input=input)
+            tf.compat.v1.global_variables_initializer()
+            tf_net = sess.graph_def
 
-#         return tf_net, None
+        return tf_net, None
 
-#     test_data_basic = [
-#         dict(input_shape=[], input_type=tf.float32),
-#         dict(input_shape=[1], input_type=tf.float32),
-#         dict(input_shape=[2, 6], input_type=tf.int32),
-#         dict(input_shape=[3, 4, 5, 6], input_type=tf.float32)
-#     ]
+    test_data_basic = [
+        dict(input_shape=[], input_type=tf.float32),
+        dict(input_shape=[1], input_type=tf.float32),
+        dict(input_shape=[2, 6], input_type=tf.int32),
+        dict(input_shape=[3, 4, 5, 6], input_type=tf.float32)
+    ]
 
-#     @pytest.mark.parametrize("params", test_data_basic)
-#     @pytest.mark.precommit_tf_fe
-#     @pytest.mark.nightly
-#     def test_rank_basic(self, params, ie_device, precision, ir_version, temp_dir,
-#                         use_legacy_frontend):
-#         self._test(*self.create_rank_net(**params),
-#                    ie_device, precision, ir_version, temp_dir=temp_dir,
-#                    use_legacy_frontend=use_legacy_frontend)
+    @pytest.mark.parametrize("params", test_data_basic)
+    @pytest.mark.precommit_tf_fe
+    @pytest.mark.nightly
+    def test_rank_basic(self, params, ie_device, precision, ir_version, temp_dir,
+                        use_legacy_frontend):
+        self._test(*self.create_rank_net(**params),
+                   ie_device, precision, ir_version, temp_dir=temp_dir,
+                   use_legacy_frontend=use_legacy_frontend)
 
 class TestComplexRank(CommonTFLayerTest):
     def _prepare_input(self, inputs_info):
-        print(inputs_info)
-        raise NotImplementedError
         rng = np.random.default_rng()
         assert 'param_real:0' in inputs_info
         assert 'param_imag:0' in inputs_info
@@ -53,22 +51,20 @@ class TestComplexRank(CommonTFLayerTest):
         tf.compat.v1.reset_default_graph()
         # Create the graph and model
         with tf.compat.v1.Session() as sess:
-            input_real = tf.compat.v1.placeholder(np.float32, input_shape, 'param_real')
-            input_imag = tf.compat.v1.placeholder(np.float32, input_shape, 'param_imag')
+            input_real = tf.compat.v1.placeholder(tf.float32, input_shape, 'param_real')
+            input_imag = tf.compat.v1.placeholder(tf.float32, input_shape, 'param_imag')
             input = tf.raw_ops.Complex(real=input_real, imag=input_imag)
-            rank = tf.raw_ops.Rank(input=input)
-            # real = tf.raw_ops.Real(input=rank)
-            # imag = tf.raw_ops.Imag(input=rank)
+            tf.raw_ops.Rank(input=input)
             tf.compat.v1.global_variables_initializer()
             tf_net = sess.graph_def
 
         return tf_net, None
 
     test_data_basic = [
-        # dict(input_shape=[]),
-        # dict(input_shape=[1]),
+        dict(input_shape=[]),
+        dict(input_shape=[1]),
         dict(input_shape=[2, 6]),
-        # dict(input_shape=[3, 4, 5, 6])
+        dict(input_shape=[3, 4, 5, 6])
     ]
 
     @pytest.mark.parametrize("params", test_data_basic)
