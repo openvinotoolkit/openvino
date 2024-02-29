051f22f0a209637d666acadecbc0bf71636ac9e3
sbalandi <sofya.balandina@intel.com>
2024-02-29 13:58:53 +0100

    [api conformance] Fix "No  devices" for sw plugings when creating new core

--- src/tests/test_utils/common_test_utils/include/common_test_utils/ov_plugin_cache.hpp
+++ src/tests/test_utils/common_test_utils/include/common_test_utils/ov_plugin_cache.hpp
@@ -23,11 +23,11 @@ extern std::unordered_set<std::string> available_devices;
 
 void register_plugin(ov::Core& ov_core) noexcept;
 void register_template_plugin(ov::Core& ov_core) noexcept;
-ov::Core create_core(const std::string& in_target_device = target_device);
+ov::Core create_core(const std::string& in_target_device = std::string());
 
 class PluginCache {
 public:
-    std::shared_ptr<ov::Core> core(const std::string& in_target_device = target_device);
+    std::shared_ptr<ov::Core> core(const std::string& in_target_device = std::string());
 
     static PluginCache& get();
     void reset();
