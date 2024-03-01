7ef8e794763ea0ebe85794248b33ce366f091388
Rinne <liu_yaohui1998@126.com>
2024-03-01 19:08:52 +0800

    [TF FE] Add complex type support for rank op (#23118)
    ### Details:
     - Add complex type support for rank op
     - Add corresponding test of it
    
    ### Tickets:
     - I think I don't have a ticket id
    
    ### Related issues
    
    #22956
    
    ---------
    
    Co-authored-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- src/frontends/tensorflow_common/src/op/rank.cpp
+++ src/frontends/tensorflow_common/src/op/rank.cpp
@@ -3,8 +3,11 @@
 //
 
 #include "common_op_table.hpp"
+#include "helper_ops/complex_type_mark.hpp"
+#include "openvino/op/constant.hpp"
 #include "openvino/op/shape_of.hpp"
 #include "openvino/op/squeeze.hpp"
+#include "openvino/op/subtract.hpp"
 
 using namespace std;
 using namespace ov::op;
@@ -15,8 +18,21 @@ namespace tensorflow {
 namespace op {
 
 ov::OutputVector translate_rank_op(const NodeContext& node) {
-    default_op_checks(node, 1, {"Rank"});
+    default_op_checks(node, 1, {"Rank"}, true);
     auto input = node.get_input(0);
+    auto complex_type_mark = as_type_ptr<ComplexTypeMark>(input.get_node_shared_ptr());
+    if (complex_type_mark) {
+        input = complex_type_mark->input_value(0);
+        auto input_shape = make_shared<v3::ShapeOf>(input, ov::element::i32);
+
+        auto unsqueeze_input_rank = make_shared<v3::ShapeOf>(input_shape, ov::element::i32);
+        auto input_rank_with_complex = make_shared<v0::Squeeze>(unsqueeze_input_rank);
+        // eliminate the extra dimension
+        auto input_rank =
+            make_shared<v1::Subtract>(input_rank_with_complex, make_shared<v0::Constant>(ov::element::i32, Shape{}, 1));
+        set_node_name(node.get_name(), input_rank);
+        return {input_rank->output(0)};
+    }
     auto input_shape = make_shared<v3::ShapeOf>(input, ov::element::i32);
     auto unsqueeze_input_rank = make_shared<v3::ShapeOf>(input_shape, ov::element::i32);
     auto input_rank = make_shared<v0::Squeeze>(unsqueeze_input_rank);
