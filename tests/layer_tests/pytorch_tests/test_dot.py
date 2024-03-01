cb851aa918441112e9892b3ee3319ba44445546e
LucaTamSapienza <tutordimatematica.ing@gmail.com>
2024-03-01 19:59:37 +0100

    added test for out param

--- tests/layer_tests/pytorch_tests/test_dot.py
+++ tests/layer_tests/pytorch_tests/test_dot.py
@@ -6,12 +6,15 @@ import pytest
 from pytorch_layer_test_class import PytorchLayerTest
 
 class TestDot(PytorchLayerTest):
-    def _prepare_input(self, inputs, dtype):
+    def _prepare_input(self, inputs, dtype, out=False):
         import numpy as np
-        return ((np.array(inputs[0]).astype(dtype)),
-                (np.array(inputs[1]).astype(dtype)))
+        x = np.array(inputs[0]).astype(dtype)
+        y = np.array(inputs[1]).astype(dtype)
+        if not out:
+            return (x, y)
+        return (x, y, np.array(0).astype(dtype))
 
-    def create_model(self, dtype):
+    def create_model(self, mode, dtype):
         import torch
 
         dtype_map = {
@@ -24,32 +27,39 @@ class TestDot(PytorchLayerTest):
         }
 
         class aten_dot(torch.nn.Module):
-            def __init__(self, dtype):
+            def __init__(self, mode, dtype):
                 super().__init__()
                 self.dtype = dtype
+                if mode =="out":
+                    self.forward = self.forward_out
+                else:
+                    self.forward = self.forward_default
 
-            def forward(self, tensor1, tensor2):
+            def forward_default(self, tensor1, tensor2):
                 return torch.dot(tensor1.to(self.dtype), tensor2.to(self.dtype))
 
+            def forward_out(self, tensor1, tensor2, y):
+                return torch.dot(tensor1.to(self.dtype), tensor2.to(self.dtype), out=y), y
+
         dtype = dtype_map.get(dtype)
-        
+
         ref_net = None
 
-        return aten_dot(dtype), ref_net, "aten::dot"
+        return aten_dot(mode, dtype), ref_net, "aten::dot"
 
     @pytest.mark.nightly
     @pytest.mark.precommit
-    @pytest.mark.parametrize("dtype", ["float32", "float64", "int32", "int64", "int8"])
+    @pytest.mark.parametrize("mode, dtype", [
+        ("", "float32"), ("", "float64"), ("", "int32"), ("", "int64"), ("", "int8"),
+        ("out", "float32"), ("out", "float64"), ("out", "int32"), ("out", "int64"), ("out", "int8")])
     @pytest.mark.parametrize(
         "inputs", [([0, 1, 2, 3, 4], [5, 6, 7, 8, 9]), ([1, 2, 3], [4, 5, 6]), ([1, 1, 1], [1, 1, 1])]
     )
-    def test_dot(self, dtype, inputs, ie_device, precision, ir_version):
-        self.dtype = dtype
-        self.inputs = inputs
+    def test_dot(self, mode, dtype, inputs, ie_device, precision, ir_version):
         self._test(
-            *self.create_model(dtype),
+            *self.create_model(mode, dtype),
             ie_device,
             precision,
             ir_version,
-            kwargs_to_prepare_input={"inputs": inputs, "dtype": dtype}
+            kwargs_to_prepare_input={"inputs": inputs, "dtype": dtype, "out": mode == "out"}
         )
