6d4f5ede4751d0e4e9dcebf3ecfee2a5716ef274
parthmahajan <mahajanparth0@gmail.com>
2024-03-04 18:05:34 +0530

    sorted params in conftest to support pytest-xdist

--- tests/layer_tests/ovc_python_api_tests/conftest.py
+++ tests/layer_tests/ovc_python_api_tests/conftest.py
@@ -2,12 +2,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import inspect
-
 from common.layer_test_class import get_params
 
 
 def pytest_generate_tests(metafunc):
     test_gen_attrs_names = list(inspect.signature(get_params).parameters)
     params = get_params()
-
-    metafunc.parametrize(test_gen_attrs_names, params, scope="function")
+    params=sorted(params)
+    metafunc.parametrize(test_gen_attrs_names, params, scope="module")
