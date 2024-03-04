b1989cc337df23d35446854b28ab2e3eda06daba
Katarzyna Mitrus <katarzyna.mitrus@intel.com>
2024-03-04 10:53:09 +0100

    [Ref] Concat op reference implementation code improvements (#23048)
    ### Details:
    - Concat op reference implementation code improvements, removal of code
    duplication
    - Leftovers from PR:
    https://github.com/openvinotoolkit/openvino/pull/22686
    - Reduce Concat template code, pass element_type to distinguish copy
    method for string
     (element::Type_t::undefined as default to keep compatibility)
    - ~Corresponding update of places where reference::concat is used
    (including one gpu file)~ (reverted)
     
    (*First approach
    (https://github.com/openvinotoolkit/openvino/pull/23048/commits/6308f9d1cfe7e8de5848f7e8e04fb2907b129066)
    was to simply introduce common template to reuse the Concat reference
    code, but it still results in generation of string and char
    specializations of the whole template function*
    *Current approach is to use the same function and choose the copy method
    inside, based on the element type*)
    
    ### Tickets:
     -Related to 131838

--- src/core/src/op/concat.cpp
+++ src/core/src/op/concat.cpp
@@ -52,43 +52,35 @@ std::shared_ptr<Node> Concat::clone_with_new_inputs(const OutputVector& new_args
     return std::make_shared<Concat>(new_args, m_axis);
 }
 
-template <typename T>
-void evaluate_concat(const Concat* node, TensorVector& outputs, const TensorVector& inputs) {
-    const auto inputs_count = inputs.size();
-    std::vector<Shape> arg_shapes;
-    std::vector<PartialShape> input_shapes;
-    arg_shapes.reserve(inputs_count);
-    input_shapes.reserve(inputs_count);
-
-    std::vector<const T*> arg_bufs(inputs_count);
-    auto arg_buf = arg_bufs.begin();
-    for (auto& input : inputs) {
-        *arg_buf = static_cast<const T*>(input.data());
-        ++arg_buf;
-        const auto& input_shape = input.get_shape();
-        arg_shapes.emplace_back(input_shape);
-        input_shapes.emplace_back(input_shape);
-    }
-
-    const auto& out_shape = shape_infer(node, input_shapes).front().to_shape();
-    outputs.front().set_shape(out_shape);
-    reference::concat(arg_bufs,
-                      static_cast<T*>(outputs.front().data()),
-                      arg_shapes,
-                      out_shape,
-                      ov::util::normalize(node->get_axis(), out_shape.size()),
-                      outputs.front().get_element_type().size());
-}
-
 bool Concat::evaluate(TensorVector& outputs, const TensorVector& inputs) const {
     OV_OP_SCOPE(v0_Concat_evaluate);
     OPENVINO_ASSERT(outputs.size() == 1);
 
-    if (outputs.front().get_element_type() == ov::element::string) {
-        evaluate_concat<std::string>(this, outputs, inputs);
-    } else {
-        evaluate_concat<char>(this, outputs, inputs);
+    const auto inputs_count = inputs.size();
+    std::vector<Shape> arg_shapes;
+    std::vector<PartialShape> input_shapes;
+    std::vector<const char*> arg_bufs;
+    arg_shapes.reserve(inputs_count);
+    input_shapes.reserve(inputs_count);
+    arg_bufs.reserve(inputs_count);
+
+    for (auto& input : inputs) {
+        const auto& input_shape = input.get_shape();
+        arg_shapes.emplace_back(input_shape);
+        input_shapes.emplace_back(input_shape);
+        arg_bufs.emplace_back(static_cast<const char*>(input.data()));
     }
+
+    const auto& out_shape = shape_infer(this, input_shapes).front().to_shape();
+    outputs.front().set_shape(out_shape);
+    const auto elem_type = outputs.front().get_element_type();
+    reference::concat(arg_bufs,
+                      static_cast<char*>(outputs.front().data()),
+                      arg_shapes,
+                      out_shape,
+                      ov::util::normalize(this->get_axis(), out_shape.size()),
+                      elem_type.size(),
+                      elem_type);
 
     return true;
 }
