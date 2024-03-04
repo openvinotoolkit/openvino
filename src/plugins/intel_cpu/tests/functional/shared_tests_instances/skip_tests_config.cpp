376802bbd8bc6b60c275b60797ae5c8a77b96b51
Vitaliy Urusovskij <vitaliy.urusovskij@intel.com>
2024-03-04 09:20:45 +0400

    Enable skipped tests (#23168)
    ### Tickets:
    - [CVS-55937](https://jira.devtools.intel.com/browse/CVS-55937)
    - [CVS-123427](https://jira.devtools.intel.com/browse/CVS-123427)

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/skip_tests_config.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/skip_tests_config.cpp
@@ -184,8 +184,6 @@ std::vector<std::string> disabledTestPatterns() {
         R"(.*RandomUniformLayerTestCPU.*OutPrc=i64.*)",
         // Issue: 123321
         R"(.*smoke_RNNSequenceCommonZeroClip/RNNSequenceTest.Inference.*hidden_size=10.*relu.*)",
-        // Issue: 123427
-        R"(.*RDFTLayerTest.*SignalSize=().*)",
         // Issue: 123815 (Tests are sensintive to available thread count on testing machines)
         R"(.*smoke_Snippets_MHA_.?D_SplitDimensionM.*)",
         // Issue: 122356
