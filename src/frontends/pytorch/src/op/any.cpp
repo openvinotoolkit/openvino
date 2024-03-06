2f2f7b96cd0e4e094ea0c02bf54ed3b5e681d21d
Mustafa Cavus <mustafa.cavus@intel.com>
2024-03-06 11:42:43 -0800

    Code style fix src/frontends/pytorch/src/op/any.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontends/pytorch/src/op/any.cpp
+++ src/frontends/pytorch/src/op/any.cpp
@@ -3,8 +3,6 @@
 //
 
 #include "openvino/frontend/pytorch/node_context.hpp"
-#include "openvino/op/reduce_logical_or.hpp"
-#include "openvino/op/reshape.hpp"
 #include "openvino/op/constant.hpp"
 #include "openvino/op/not_equal.hpp"
 #include "utils.hpp"
