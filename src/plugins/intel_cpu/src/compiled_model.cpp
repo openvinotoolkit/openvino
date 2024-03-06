79c7f5552618a2e5e74e36a7a184048acf1d22e5
sunxiaoxia2022 <xiaoxia.sun@intel.com>
2024-03-06 10:02:02 +0800

    fix sync issue

--- src/plugins/intel_cpu/src/compiled_model.cpp
+++ src/plugins/intel_cpu/src/compiled_model.cpp
@@ -51,11 +51,13 @@ CompiledModel::CompiledModel(const std::shared_ptr<ov::Model>& model,
     if (!core)
         OPENVINO_THROW("Unable to get API version. Core is unavailable");
 
+    ov::threading::IStreamsExecutor::Ptr stream_executor;
     if (cfg.exclusiveAsyncRequests) {
         // special case when all InferRequests are muxed into a single queue
         m_task_executor = m_plugin->get_executor_manager()->get_executor("CPU");
     } else {
-        m_task_executor = m_plugin->get_executor_manager()->get_idle_cpu_streams_executor(m_cfg.streamExecutorConfig);
+        stream_executor = m_plugin->get_executor_manager()->get_idle_cpu_streams_executor(m_cfg.streamExecutorConfig);
+        m_task_executor = stream_executor;
     }
     if (0 != m_cfg.streamExecutorConfig.get_streams()) {
         m_callback_executor = m_plugin->get_executor_manager()->get_idle_cpu_streams_executor(
@@ -89,6 +91,18 @@ CompiledModel::CompiledModel(const std::shared_ptr<ov::Model>& model,
         } while (!all_graphs_ready());
     } else {
         CompiledModel::get_graph();
+    }
+    // init sub stream threads of executor
+    if (m_cfg.streamExecutorConfig.get_sub_stream_mode() ==
+        IStreamsExecutor::Config::SubStreamsMode::SUB_STREAMS_FOR_SOCKET) {
+        std::atomic<int> nodes_remain(m_cfg.streamExecutorConfig.get_sub_streams());
+        stream_executor->run_sub_stream(
+            [&]() {
+                nodes_remain--;
+            },
+            0);
+        while (nodes_remain.load() > 0) {
+        }
     }
 }
 
