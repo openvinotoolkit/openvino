50aef33699c4fe66d2179baafc7615a3201544c7
Maxim Vafin <maxim.vafin@intel.com>
2024-03-07 14:07:46 +0100

    [PT FE] Support elu, celu, mish, shuffle ops

--- src/frontends/pytorch/src/utils.cpp
+++ src/frontends/pytorch/src/utils.cpp
@@ -154,7 +154,8 @@ const std::unordered_map<int64_t, element::Type> TORCH_TO_OV_TYPE{
     {11, element::boolean},
     {12, element::i8},  // quantized i8
     {13, element::u8},  // quantized u8
-    {14, element::i32}  // quantized i32
+    {14, element::i32}, // quantized i32
+    {15, element::bf16}
 };
 
 const std::unordered_map<std::string, ov::op::PadType> TORCH_AUTO_PAD_TO_OV{{"valid", ov::op::PadType::VALID},
