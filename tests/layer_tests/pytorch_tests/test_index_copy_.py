4ca0b7b476664472a8081c9e91100f3f9117ec17
Rinne <AsakusaRinne@gmail.com>
2024-03-09 15:56:39 +0800

    fix: error of some cases in index_copy_.

--- tests/layer_tests/pytorch_tests/test_index_copy_.py
+++ tests/layer_tests/pytorch_tests/test_index_copy_.py
@@ -9,7 +9,7 @@ import torch
 from pytorch_layer_test_class import PytorchLayerTest
 
 
-class TestIndexCopy_SingleIndex(PytorchLayerTest):
+class TestIndexCopy(PytorchLayerTest):
     def _prepare_input(self):
         return (self.input_tensor, self.values)
 
@@ -31,23 +31,47 @@ class TestIndexCopy_SingleIndex(PytorchLayerTest):
     @pytest.mark.parametrize(
         "input_data",
         (
-            # {
-            #     "input_shape": [5],
-            #     "dim": 0, 
-            #     "values": np.array([1, 2, 3]).astype(np.float32), 
-            #     "index": torch.tensor([0, 2, 1], dtype=torch.long)
-            # },
-            # {
-            #     "input_shape": [3, 3],
-            #     "dim": 0, 
-            #     "values": np.array([[10, 11, 12], [-1, -2, -3]]).astype(np.float32),
-            #     "index": torch.tensor([2, 0], dtype=torch.long)
-            # },
+            {
+                "input_shape": [1],
+                "dim": 0, 
+                "values_shape": [1],
+                "index": torch.tensor([0], dtype=torch.long)
+            },
+            {
+                "input_shape": [10],
+                "dim": 0, 
+                "values_shape": [5],
+                "index": torch.tensor([2, 3, 6, 7, 1], dtype=torch.long)
+            },
             {
                 "input_shape": [3, 3],
                 "dim": 0, 
-                "values": np.array([[7, 8], [-1, -2], [0, 5]]).astype(np.float32),
+                "values_shape": [2, 3],
+                "index": torch.tensor([2, 0], dtype=torch.long)
+            },
+            {
+                "input_shape": [4, 3, 5],
+                "dim": 1, 
+                "values_shape": [4, 2, 5],
                 "index": torch.tensor([1, 0], dtype=torch.long)
+            },
+            {
+                "input_shape": [5, 6, 7, 8],
+                "dim": -2, 
+                "values_shape": [5, 6, 4, 8],
+                "index": torch.tensor([5, 0, 6, 3], dtype=torch.long)
+            },
+            {
+                "input_shape": [5, 6, 7, 8],
+                "dim": -3, 
+                "values_shape": [5, 3, 7, 8],
+                "index": torch.tensor([2, 0, 1], dtype=torch.long)
+            },
+            {
+                "input_shape": [5, 6, 7, 8],
+                "dim": 3, 
+                "values_shape": [5, 6, 7, 5],
+                "index": torch.tensor([2, 6, 0, 4, 1], dtype=torch.long)
             },
         ),
     )
@@ -55,7 +79,7 @@ class TestIndexCopy_SingleIndex(PytorchLayerTest):
     @pytest.mark.precommit
     def test_index_copy_single_index(self, ie_device, precision, ir_version, input_data):
         self.input_tensor = np.random.randn(*input_data["input_shape"]).astype(np.float32)
-        self.values = input_data["values"]
+        self.values = np.random.randn(*input_data["values_shape"]).astype(np.float32)
         index = input_data["index"]
         dim = input_data["dim"]
         self._test(*self.create_model(dim, index), ie_device, precision, ir_version)
