fedfc4f21b1e7e6771a60e8541603653459c0377
Vitaliy Urusovskij <vitaliy.urusovskij@intel.com>
2024-03-11 00:25:20 +0400

    Get rid of `ov_models` (#23084)

--- src/tests/ov_helpers/ov_models/include/ov_models/preprocess/preprocess_builders.hpp
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2018-2023 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
-//
-
-// TODO (vurusovs): delete after migration of NVidia and NPU
-
-#pragma once
-
-#include "common_test_utils/subgraph_builders/preprocess_builders.hpp"
--- src/tests/ov_helpers/ov_models/src/preprocess_builders.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2018-2023 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
-//
-
-// TODO (vurusovs): delete after migration of NVidia and NPU
-
-#include "ov_models/preprocess/preprocess_builders.hpp"
