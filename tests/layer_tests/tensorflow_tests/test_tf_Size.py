22cd04e5b342f780e90c4c6c39f8e839f8a722cc
LucaTamSapienza <tutordimatematica.ing@gmail.com>
2024-03-11 02:06:24 +0100

    Solved test since doesn't handle properly Complex tensor

--- tests/layer_tests/tensorflow_tests/test_tf_Size.py
+++ tests/layer_tests/tensorflow_tests/test_tf_Size.py
@@ -51,32 +51,29 @@ class TestComplexSize(CommonTFLayerTest):
         assert 'param_imag:0' in inputs_info
         param_real_shape_1 = inputs_info['param_real:0']
         param_imag_shape_1 = inputs_info['param_imag:0']
-        input_type = self.input_type
         inputs_data = {}
-        inputs_data['param_real:0'] = 4 * rng.random(param_real_shape_1).astype(input_type) - 2
-        inputs_data['param_imag:0'] = 4 * rng.random(param_imag_shape_1).astype(input_type) - 2
+        inputs_data['param_real:0'] = 4 * rng.random(param_real_shape_1).astype(np.float32) - 2
+        inputs_data['param_imag:0'] = 4 * rng.random(param_imag_shape_1).astype(np.float32) - 2
         return inputs_data
 
-    def create_complex_size_net(self, input_shape, input_type, out_type):
-        self.input_type = input_type
+    def create_complex_size_net(self, input_shape):
         tf.compat.v1.reset_default_graph()
         # Create the graph and model
         with tf.compat.v1.Session() as sess:
-            param_real = tf.compat.v1.placeholder(input_type, input_shape, 'param_real')
-            param_imag = tf.compat.v1.placeholder(input_type, input_shape, 'param_imag')
+            param_real = tf.compat.v1.placeholder(np.float32, input_shape, 'param_real')
+            param_imag = tf.compat.v1.placeholder(np.float32, input_shape, 'param_imag')
             complex = tf.raw_ops.Complex(real=param_real, imag=param_imag)
-            size = tf.raw_ops.Size(input=complex, out_type=out_type)
-            #real = tf.raw_ops.Real(input=size)
-            #img = tf.raw_ops.Imag(input=size)
+            tf.raw_ops.Size(input=complex)
             tf.compat.v1.global_variables_initializer()
             tf_net = sess.graph_def
 
         return tf_net, None
 
     test_data_basic = [
-        dict(input_shape=[2, 6], input_type=np.float32, out_type=tf.int32),
-        dict(input_shape=[2, 4, 5], input_type=np.int32, out_type=tf.int64),
-        dict(input_shape=[1], input_type=np.float64, out_type=tf.int32)
+        dict(input_shape=[]),
+        dict(input_shape=[1]),
+        dict(input_shape=[2, 6]),
+        dict(input_shape=[2, 4, 5]),
     ]
 
     @pytest.mark.parametrize("params", test_data_basic)
