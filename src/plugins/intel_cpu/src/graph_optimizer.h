f56802de40feddafdfcb9cd4c75dacd0d30de22f
Vladislav Golubev <vladislav.golubev@intel.com>
2024-03-11 11:19:05 +0100

    Added detailed examples

--- src/plugins/intel_cpu/src/graph_optimizer.h
+++ src/plugins/intel_cpu/src/graph_optimizer.h
@@ -63,12 +63,24 @@ private:
     // Method merges Transpose -> Reshape(optional) -> Reorder sequences which do opposite permutation to each other.
     // Reverse order Reorder -> Reshape(optional) -> Transpose is supported too.
     // Reshape support has the following limitations:
-    // - direct order: Only reshape which separates one of the dimension on 2 consecutive ones is supported
+    // - direct order: Only reshape which split one of the dimension into 2 consecutive ones is supported
     // - reverse order: Only reshape which fuses 2 consecutive dimensions into one is supported
-    // Example:
-    //      chain [physical layout: NCHW, logical layout: NCHW] -> Transpose(order=0312) -> [physical layout: NWCH, logical layout: NCHW] ->
-    //      Reorder(nchw->nhwc) -> [physical layout: NCHW, logical layout: NHWC] can be replaced with Reorder(nchw->nhwc; isOptimized=true)
-    //      which will just reinterprets layout without physical change of the memory.
+    // Examples:
+    // 1. Direct order, no Reshape node.
+    //    Before: [N,C,H,W]abcd==>Transpose(0312)==>[N,W,C,H]abcd==>Reorder(abcd->acdb)==>[N,W,C,H]acdb
+    //    [N,C,H,W]abcd is equivalent to the [N,W,C,H]acdb, so the Transpose and Reorder can be fused into single optimized Reorder:
+    //    After:  [N,C,H,W]abcd==>Reorder(abcd->acdb, isOptimized=true)==>[N,W,C,H]acdb
+    // 2. Reverse order, no Reshape node.
+    //    Before: [N,W,C,H]acdb==>Reorder(acdb->abcd)==>[N,W,C,H]abcd==>Transpose(0231)==>[N,C,H,W]abcd
+    //    [N,W,C,H]acdb is equivalent to the [N,C,H,W]abcd, so the Transpose and Reorder can be fused into single optimized Reorder:
+    //    After:  [N,W,C,H]acdb==>Reorder(acdb->abcd, isOptimized=true)==>[N,C,H,W]abcd
+    // 3. Direct order with Reshape node (L = H x w).
+    //    Before: [N,L,C]abc==>Transpose(021)==>[N,C,L]abc==>Reshape==>[N,C,H,W]abcd==>Reoder(abcd->acdb)==>[N,C,H,W]acdb
+    //    After:  [N,L,C]abc==>Reorder(abc->acdb, isOptimized=true)==>[N,C,H,W]acdb
+    // 4. Reverse order with Reshape node (L = H x W).
+    //    Before: [N,C,H,W]acdb==>Reorder(acdb->abcd)==>[N,C,H,W]abcd==>Reshape==>[N,C,L]abc==>Transpose(021)==>[N,L,C]abc
+    //    After:  [N,C,H,W]acdb==>Reorder(acdb->abc, isOptimized=true)==>[N,L,C]abc
+    // Note: in some cases (inplace conflicts or transpose with blocked input and non-blocked output) the merged Reorder can not be optimized.
     void mergeTransposeReshapeReorder(Graph& graph,
                                       const NodePtr& transposeNode,
                                       const NodePtr& reshapeNode,
