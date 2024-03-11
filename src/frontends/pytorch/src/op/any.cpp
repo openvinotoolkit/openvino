c1544e14cf916fb795a324e30031e5a6d5c91ffd
Cavus Mustafa <mustafa.cavus@intel.com>
2024-03-11 10:56:40 -0700

    Simplified aten.any translation

--- src/frontends/pytorch/src/op/any.cpp
+++ src/frontends/pytorch/src/op/any.cpp
@@ -18,16 +18,9 @@ namespace op {
 OutputVector translate_any_fx(const NodeContext& context) {
     num_inputs_check(context, 1, 1);
     auto x = context.get_input(0);
-    auto num_inputs = context.get_input_size();
     bool keep_dims = false;
 
-    auto const_minus_one = context.mark_node(ov::op::v0::Constant::create(element::i32, Shape{1}, {-1}));
-    auto flatten_source = context.mark_node(std::make_shared<ov::op::v1::Reshape>(x, const_minus_one, false));
-    auto const_zero = context.mark_node(ov::op::v0::Constant::create(element::i32, Shape{1}, {0}));
-    auto zero = context.mark_node(ov::op::v0::Constant::create(element::i32, Shape{}, {0}));
-    auto one = context.mark_node(ov::op::v0::Constant::create(element::i32, Shape{}, {1}));
-    auto rank = std::get<1>(get_shape_rank(context, x, true));
-    auto axes = context.mark_node(std::make_shared<ov::op::v0::Range>(zero, rank, one));
+    auto axes = get_axes_range(context, 0);
     auto any = context.mark_node(std::make_shared<ov::op::v1::ReduceLogicalOr>(x, axes, keep_dims));
     return {any};
 };
