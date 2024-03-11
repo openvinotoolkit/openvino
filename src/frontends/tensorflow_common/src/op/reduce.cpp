34ac7b725b831ee4f3bec4f87348cdfb687340b1
MonalSD <monaldhankar@gmail.com>
2024-03-11 16:42:50 +0530

    Support complex tensors for Prod operations

--- src/frontends/tensorflow_common/src/op/reduce.cpp
+++ src/frontends/tensorflow_common/src/op/reduce.cpp
@@ -22,13 +22,28 @@ namespace op {
 
 template <typename T>
 OutputVector translate_direct_reduce_op(const NodeContext& node) {
-    default_op_checks(node, 2, {"Any", "All", "EuclideanNorm", "Max", "Mean", "Min", "Prod", "Sum"});
+    default_op_checks(node, 2, {"Any", "All", "EuclideanNorm", "Max", "Mean", "Min", "Prod", "Sum"}, true);
     auto input = node.get_input(0);
     auto axis = node.get_input(1);
     auto keep_dims = node.get_attribute<bool>("keep_dims", false);
-    auto reduce_op = make_shared<T>(input, axis, keep_dims);
-    set_node_name(node.get_name(), reduce_op);
-    return {reduce_op};
+    auto bool is_input_complex =
+        input->get_data_type() == DataType::Complex64 || input->get_data_type() == DataType::Complex128;
+    if (is_input_complex) {
+        auto complex_type_mark = input->get_attribute<ComplexTypeMark>("ComplexTypeMark");
+        auto output_shape = input->get_shape();
+        output_shape.push_back(2);  // Adding auxillary dimension
+
+        auto output_tensor = make_shared<Tensor>(DataType::Float32, output_shape);
+        output_tensor->set_attribute("ComplexTypeMark", complex_type_mark);
+
+        auto reduce_op = make_shared<T>(input, axis, keep_dims);
+        set_node_name(node.get_name(), reduce_op);
+        return {reduce_op, output_tensor};
+    } else {
+        auto reduce_op = make_shared<T>(input, axis, keep_dims);
+        set_node_name(node.get_name(), reduce_op);
+        return {reduce_op};
+    }
 }
 
 template OutputVector translate_direct_reduce_op<v1::ReduceLogicalOr>(const NodeContext& node);
