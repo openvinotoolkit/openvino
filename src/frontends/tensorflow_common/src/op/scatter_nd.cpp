aaa143ac2444678c03e720455546fe48399495da
MonalSD <monaldhankar@gmail.com>
2024-03-12 10:10:01 +0530

    Addressing changes in ScatterNd

--- src/frontends/tensorflow_common/src/op/scatter_nd.cpp
+++ src/frontends/tensorflow_common/src/op/scatter_nd.cpp
@@ -20,16 +20,10 @@ OutputVector translate_scatter_nd_op(const NodeContext& node) {
     auto updates = node.get_input(1);
     auto shape = node.get_input(2);
 
-    auto complex_type_mark_input_indices = as_type_ptr<ComplexTypeMark>(input_indices.get_node_shared_ptr());
     auto complex_type_mark_updates = as_type_ptr<ComplexTypeMark>(updates.get_node_shared_ptr());
-    auto complex_type_mark_shape = as_type_ptr<ComplexTypeMark>(shape.get_node_shared_ptr());
-    auto complex_type_inputs =
-        (complex_type_mark_input_indices || complex_type_mark_updates || complex_type_mark_shape) ? true : false;
 
-    if (complex_type_inputs) {
-        input_indices = complex_type_mark_input_indices->input_value(0);
+    if (complex_type_mark_updates) {
         updates = complex_type_mark_updates->input_value(0);
-        shape = complex_type_mark_shape->input_value(0);
     }
 
     auto input_data = create_same_type_const<int32_t>(updates, vector<int32_t>{0}, Shape{1});
@@ -42,7 +36,7 @@ OutputVector translate_scatter_nd_op(const NodeContext& node) {
             make_shared<ComplexTypeMark>(scatter_nd, complex_type_mark_updates->get_complex_part_type());
         return {complex_scatter_nd};
     }
-
+    
     return {scatter_nd};
 }
 }  // namespace op
