b20ca333a5210fefd28cf228f28becfaa39f4e59
MonalSD <monaldhankar@gmail.com>
2024-03-12 10:10:01 +0530

    Addressing changes in ScatterNd

--- tests/layer_tests/tensorflow_tests/test_tf_ScatterND.py
+++ tests/layer_tests/tensorflow_tests/test_tf_ScatterND.py
@@ -5,7 +5,7 @@ import platform
 
 import pytest
 from common.tf_layer_test_class import CommonTFLayerTest
-
+import numpy as np
 
 class TestTFScatterND(CommonTFLayerTest):
     def create_tf_scatternd_placeholder_const_net(self, x_shape, indices, updates, ir_version,
@@ -79,44 +79,55 @@ class TestComplexScatterND(CommonTFLayerTest):
                                                   use_legacy_frontend):
         import tensorflow as tf
         tf.compat.v1.reset_default_graph()
-
         # Create the graph and model
         with tf.compat.v1.Session() as sess:
-            x = tf.compat.v1.placeholder(tf.complex64, x_shape, 'Input')
-            tf_indices = tf.constant(indices)
-            tf_updates = tf.constant(updates, dtype=tf.complex64)
-            scatter_nd = tf.scatter_nd(tf_indices, tf_updates, tf.shape(x), name="Operation")
-            res = tf.add(x, scatter_nd)
+            x = tf.compat.v1.placeholder(tf.float32, x_shape, 'Input')
+            tf_indices = tf.compat.v1.placeholder(np.int32, [None], 'indices')
+            updates_real = tf.compat.v1.placeholder(np.float32, [None], 'updates_real')
+            updates_imag = tf.compat.v1.placeholder(np.float32, [None], 'updates_imag')
+            updates = tf.raw_ops.Complex(real=updates_real,imag=updates_imag)
+
+            scatter_nd = tf.raw_ops.ScatterNd(indices, updates, tf.shape(x), name="Operation")
+            res = tf.add(x_shape, scatter_nd, name="Operation")
+            real = tf.raw_ops.Real(input=res)
+            img = tf.raw_ops.Imag(input=res)
             tf.compat.v1.global_variables_initializer()
             tf_net = sess.graph_def
-        ref_net = None
-        return tf_net, ref_net
 
-    test_data = [
-        pytest.param(
-            dict(x_shape=[8], indices=[[4], [3], [1], [7]], updates=[9j, 10j, 11j, 12j]),
-            marks=pytest.mark.precommit),
-        pytest.param(dict(x_shape=[4, 4, 4], indices=[[0], [2]], updates= \
-            [[[[5j, 5j, 5j, 5j], [6j, 6j, 6j, 6j], [7j, 7j, 7j, 7j], [8j, 8j, 8j, 8j]], \
-             [[1j, 1j, 1j, 1j], [2j, 2j, 2j, 2j], [3j, 3j, 3j, 3j], [4j, 4j, 4j, 4j]]]])),
-        pytest.param(dict(x_shape=[2, 2], indices=[[0]], updates=[[5j, 3j]])),
-        pytest.param(dict(x_shape=[2, 2], indices=[[1, 1]], updates=[5j])),
-        dict(x_shape=[1], indices=[[0]], updates=[3j]),
-        dict(x_shape=[20], indices=[[0], [6], [9], [19], [13]],
-             updates=[3j, 7j, -12j, 4j, -99j]),
-        dict(x_shape=[4, 2], indices=[[1], [2]], updates=[[9j, 14j], [-76j, 0j]]),
-        dict(x_shape=[4, 4, 4], indices=[[0], [1], [3]], updates=[
-            [[5j, 1j, 5j, 13j], [8j, 6j, 6j, 8j], [7j, 0j, 0j, 7j], [8j, 8j, 8j, 8j]],
-            [[0j, 0j, 0j, 0j], [1j, 2j, 3j, 4j], [5j, 6j, 7j, 8j], [9j, 10j, 11j, 12j]],
-            [[5j, 5j, 5j, 5j], [6j, 6j, 6j, 6j], [7j, 7j, 7j, 7j], [8j, 8j, 8j, 8j]]]),
-        dict(x_shape=[2, 2, 2], indices=[[1, 1, 1], [0, 1, 0]], updates=[9j, 6.3j]),
-        pytest.param(dict(x_shape=[2, 2, 2], indices=[[0, 0], [0, 1]], updates=[[6.7j, 9j], [45j, 8.3j]]),
-                     marks=pytest.mark.precommit_tf_fe),
-        dict(x_shape=[2, 2, 2], indices=[[1]], updates=[[[6.7j, 9j], [45j, 8.3j]]]),
+        return tf_net, None
+    
+    def prepare_input(self, inputs_info):
+        rng = np.random.default_rng()
 
+        assert 'indices' in inputs_info
+        assert 'updates_real' in inputs_info
+        assert 'updates_imag' in inputs_info
+        assert 'x_shape' in inputs_info
+
+        indices_shape = inputs_info['indices']
+        updates_real_shape = inputs_info['updates_real']
+        updates_imag_shape = inputs_info['updates_imag']
+        x_shape = inputs_info['x_shape']
+
+        inputs_data = {}
+        inputs_data['indices'] = rng.integers(0, 10, indices_shape).astype(np.int32)  # Example range (0, 10), adjust as needed
+        inputs_data['updates_real'] = 4 * rng.random(updates_real_shape).astype(np.float32) - 2
+        inputs_data['updates_imag'] = 4 * rng.random(updates_imag_shape).astype(np.float32) - 2
+        inputs_data['x_shape'] = rng.integers(0, 10, indices_shape).astype(np.int32)
+
+        return inputs_data
+    
+    test_data_basic = [
+        dict(input_shape=[]),
+        dict(input_shape=[2]),
+        dict(input_shape=[1, 3]),
+        dict(input_shape=[2, 3, 4]),
+        dict(input_shape=[3, 4, 5, 6]),
     ]
 
-    @pytest.mark.parametrize("params", test_data)
+ 
+    @pytest.mark.parametrize("params", test_data_basic)
+    @pytest.mark.precommit_tf_fe
     @pytest.mark.nightly
     def test_tf_scatter_nd(self, params, ie_device, precision, ir_version, temp_dir,
                            use_legacy_frontend):
