7cd8b3ec5e19ef4429ef98570f438ea75cee62ea
Vladislav Golubev <vladislav.golubev@intel.com>
2024-03-13 11:11:34 +0100

    Skip the transformation if bcast can't be casted

--- src/common/transformations/src/transformations/common_optimizations/broadcast_transition.cpp
+++ src/common/transformations/src/transformations/common_optimizations/broadcast_transition.cpp
@@ -35,11 +35,8 @@ ov::pass::BroadcastTransition::BroadcastTransition() {
         }
 
         const auto bcast = ov::as_type_ptr<ov::op::util::BroadcastBase>(pattern_map.at(bcast_m).get_node_shared_ptr());
-        OPENVINO_ASSERT(bcast,
-                        "The matched node can't be casted to BroadcastBase: ",
-                        pattern_map.at(bcast_m).get_node_shared_ptr());
-        const auto& bcast_type = bcast->get_broadcast_spec().m_type;
-        if (bcast_type != ov::op::BroadcastType::NUMPY && bcast_type != ov::op::BroadcastType::BIDIRECTIONAL) {
+        if (!bcast || (bcast->get_broadcast_spec().m_type != ov::op::BroadcastType::NUMPY &&
+                       bcast->get_broadcast_spec().m_type != ov::op::BroadcastType::BIDIRECTIONAL)) {
             return false;
         }
 
