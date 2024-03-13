98a20998535641dd87aebb475476bdf6d50db71f
MonalSD <monaldhankar@gmail.com>
2024-03-13 23:10:31 +0530

    Support complex tensors for OnesLike operation

--- src/frontends/tensorflow_common/src/op/ones_like.cpp
+++ src/frontends/tensorflow_common/src/op/ones_like.cpp
@@ -21,6 +21,11 @@ namespace op {
 OutputVector translate_ones_like_op(const NodeContext& node) {
     default_op_checks(node, 1, {"OnesLike"});
     auto x = node.get_input(0);
+    auto complex_type_mark_x = as_type_ptr<ComplexTypeMark>(x.get_node_shared_ptr());
+    if (complex_type_mark_x) {
+        x = complex_type_mark_x->input_value(0);
+    }
+
     Output<Node> shape_of = make_shared<v3::ShapeOf>(x, element::i32);
     auto one_const = create_same_type_const_scalar<int32_t>(x, 1);
 
@@ -35,11 +40,17 @@ OutputVector translate_ones_like_op(const NodeContext& node) {
     // remove extra dimension by squeezing
     auto zero_dim_ind = make_shared<v0::Constant>(element::i32, Shape{1}, 0);
     ones_like = make_shared<v0::Squeeze>(ones_like, zero_dim_ind);
-
     set_node_name(node.get_name(), ones_like.get_node_shared_ptr());
+
+    if (complex_type_mark_x) {
+        auto complex_ones_like = make_shared<ComplexTypeMark>(ones_like, complex_type_mark_x->get_complex_part_type());
+        return {
+            complex_ones_like
+        }
+    }
+
     return {ones_like};
 }
-
 }  // namespace op
 }  // namespace tensorflow
 }  // namespace frontend
