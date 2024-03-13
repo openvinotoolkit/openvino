699868019342bdfbabb73a95fe933ced0e24ef95
Vishniakov Nikolai <nikolai.vishniakov@intel.com>
2024-03-13 19:54:25 +0100

    Add tests

--- src/bindings/js/node/tests/core.test.js
+++ src/bindings/js/node/tests/core.test.js
@@ -17,3 +17,21 @@ it('Core.setProperty()', () => {
 
   assert.equal(cacheDir, tmpDir);
 });
+
+it('Core.setProperty(\'CPU\')', () => {    
+  const tmpDir = '/home/nvishnya/tmp123';
+
+  core.setProperty('CPU', { 'CACHE_DIR': tmpDir });
+
+  const cacheDir = core.getProperty('CPU', 'CACHE_DIR');
+
+  assert.equal(cacheDir, tmpDir);
+});
+
+it('Core.getProperty(\'CPU\', \'SUPPORTED_PROPERTIES\') is Array', () => {    
+  const tmpDir = '/home/nvishnya/tmp123';
+
+  const supportedPropertiesArray = core.getProperty('CPU', 'SUPPORTED_PROPERTIES');
+
+  assert.ok(Array.isArray(supportedPropertiesArray), tmpDir);
+});
