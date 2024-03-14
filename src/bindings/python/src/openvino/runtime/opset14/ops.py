bafe9c859802d163763e1a43eaab178b62976630
p-wysocki <przemyslaw.wysocki@intel.com>
2024-03-14 14:00:37 +0100

    Linter fix

--- src/bindings/python/src/openvino/runtime/opset14/ops.py
+++ src/bindings/python/src/openvino/runtime/opset14/ops.py
@@ -11,7 +11,7 @@ from openvino.runtime import Node, Type
 from openvino.runtime.opset_utils import _get_node_factory
 from openvino.runtime.utils.types import TensorShape
 from openvino.runtime.utils.decorators import nameable_op
-from openvino.runtime.utils.types import NodeInput, as_nodes
+from openvino.runtime.utils.types import NodeInput, as_node, as_nodes
 
 _get_node_factory_opset14 = partial(_get_node_factory, "opset14")
 
