d5f30cb18276229be2d1ae8f285f91a30aec8094
MonalSD <monaldhankar@gmail.com>
2024-03-14 18:38:40 +0530

    Addressed the changes

--- src/frontends/tensorflow_common/src/op/reciprocal.cpp
+++ src/frontends/tensorflow_common/src/op/reciprocal.cpp
@@ -21,9 +21,9 @@ OutputVector translate_reciprocal_op(const NodeContext& node) {
     auto x = node.get_input(0);
     auto complex_type_mark_x = as_type_ptr<ComplexTypeMark>(x.get_node_shared_ptr());
     auto minus_one = make_shared<v0::Constant>(element::i32, Shape{1}, -1);
-    auto two = make_shared<v0::Constant>(element::i32, Shape{1}, 2);
     if (complex_type_mark_x) {
         x = complex_type_mark_x->input_value(0);
+        auto two = create_same_type_const_scalar<int32_t>(x, 2);
         auto gather_index_real = make_shared<v0::Constant>(element::i32, Shape{1}, 0);
         auto gather_index_imag = make_shared<v0::Constant>(element::i32, Shape{1}, 1);
         auto x_real = make_shared<v8::Gather>(x, gather_index_real, minus_one)->output(0);
@@ -31,7 +31,7 @@ OutputVector translate_reciprocal_op(const NodeContext& node) {
 
         // Compute (a^2+b^2)
         auto real_squared_norm = make_shared<v1::Power>(x_real, two);
-        auto img_squared_norm = make_shared<v1::Multiply>(x_imag, two);
+        auto img_squared_norm = make_shared<v1::Power>(x_imag, two);
         auto squared_norm = make_shared<v1::Add>(real_squared_norm, img_squared_norm);
 
         // compute 1/(a+bi) = (a-bi)/(a^2+b^2)
