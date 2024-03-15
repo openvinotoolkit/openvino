d3a3c573ff7f5b034cb40b87b12fc4d26e0baf48
Piotr Kowalczyk <piotr.kowalczyk@intel.com>
2024-03-15 10:46:07 +0000

    [Op][ROIAlignRotated]: Review comments.

--- src/core/src/op/roi_align_rotated.cpp
+++ src/core/src/op/roi_align_rotated.cpp
@@ -6,30 +6,28 @@
 
 #include "itt.hpp"
 
-using namespace std;
-
 namespace ov {
 namespace op {
 namespace v14 {
-op::v14::ROIAlignRotated::ROIAlignRotated(const Output<Node>& input,
-                                          const Output<Node>& rois,
-                                          const Output<Node>& batch_indices,
-                                          const int pooled_h,
-                                          const int pooled_w,
-                                          const int sampling_ratio,
-                                          const float spatial_scale,
-                                          const bool clockwise_mode)
+ROIAlignRotated::ROIAlignRotated(const Output<Node>& input,
+                                 const Output<Node>& rois,
+                                 const Output<Node>& batch_indices,
+                                 const int pooled_h,
+                                 const int pooled_w,
+                                 const int sampling_ratio,
+                                 const float spatial_scale,
+                                 const bool clockwise_mode)
     : ROIAlignBase{input, rois, batch_indices, pooled_h, pooled_w, sampling_ratio, spatial_scale},
       m_clockwise_mode{clockwise_mode} {
     constructor_validate_and_infer_types();
 }
 
-void op::v14::ROIAlignRotated::validate_and_infer_types() {
+void ROIAlignRotated::validate_and_infer_types() {
     OV_OP_SCOPE(v14_ROIAlignRotated_validate_and_infer_types);
     ROIAlignBase::validate_and_infer_types();
 }
 
-bool op::v14::ROIAlignRotated::visit_attributes(AttributeVisitor& visitor) {
+bool ROIAlignRotated::visit_attributes(AttributeVisitor& visitor) {
     OV_OP_SCOPE(v14_ROIAlignRotated_visit_attributes);
     ROIAlignBase::visit_attributes(visitor);
     visitor.on_attribute("clockwise_mode", m_clockwise_mode);
@@ -37,10 +35,10 @@ bool op::v14::ROIAlignRotated::visit_attributes(AttributeVisitor& visitor) {
     return true;
 }
 
-shared_ptr<Node> op::v14::ROIAlignRotated::clone_with_new_inputs(const OutputVector& new_args) const {
+std::shared_ptr<Node> ROIAlignRotated::clone_with_new_inputs(const OutputVector& new_args) const {
     OV_OP_SCOPE(v14_ROIAlignRotated_clone_with_new_inputs);
     check_new_args_count(this, new_args);
-    return make_shared<ROIAlignRotated>(new_args.at(0),
+    return std::make_shared<ROIAlignRotated>(new_args.at(0),
                                         new_args.at(1),
                                         new_args.at(2),
                                         get_pooled_h(),
@@ -49,4 +47,6 @@ shared_ptr<Node> op::v14::ROIAlignRotated::clone_with_new_inputs(const OutputVec
                                         get_spatial_scale(),
                                         get_clockwise_mode());
 }
+}  // namespace v14
+}  // namespace op
 }  // namespace ov
