8191af0499905caff6ab2b4c65d50deaae9f7b84
MonalSD <monaldhankar@gmail.com>
2024-03-15 19:02:11 +0530

    Addressed changes in ones_like

--- src/frontends/tensorflow_common/src/op/ones_like.cpp
+++ src/frontends/tensorflow_common/src/op/ones_like.cpp
@@ -3,9 +3,11 @@
 //
 
 #include "common_op_table.hpp"
+#include "helper_ops/complex_type_mark.hpp"
 #include "openvino/op/broadcast.hpp"
 #include "openvino/op/concat.hpp"
 #include "openvino/op/constant.hpp"
+#include "openvino/op/gather.hpp"
 #include "openvino/op/shape_of.hpp"
 #include "openvino/op/squeeze.hpp"
 #include "utils.hpp"
@@ -24,50 +26,37 @@ OutputVector translate_ones_like_op(const NodeContext& node) {
     auto complex_type_mark_x = as_type_ptr<ComplexTypeMark>(x.get_node_shared_ptr());
     if (complex_type_mark_x) {
         x = complex_type_mark_x->input_value(0);
-        auto gather_index_real = make_shared<v0::Constant>(element::i32, Shape{}, 0);
+        auto gather_index_real = make_shared<v0::Constant>(element::i32, Shape{1}, 0);
         auto minus_one = make_shared<v0::Constant>(element::i32, Shape{1}, -1);
         auto x_real = make_shared<v8::Gather>(x, gather_index_real, minus_one)->output(0);
-
         Output<Node> shape_of_real = make_shared<v3::ShapeOf>(x_real, element::i32);
+
         auto one_const = create_same_type_const_scalar<int32_t>(x_real, 1);
-        auto one_dim = make_shared<v0::Constant>(element::i32, Shape{1}, 1);
-        shape_of_real = make_shared<v0::Concat>(OutputVector{one_dim, shape_of_real}, 0);
-        // create a tensor of zeros of shape with extra dimension
-        Output<Node> ones_like_one = make_shared<v3::Broadcast>(one_const, shape_of_real);
-        // remove extra dimension by squeezing
-        auto zero_dim_ind = make_shared<v0::Constant>(element::i32, Shape{1}, 0);
-        ones_like_one = make_shared<v0::Squeeze>(ones_like_one, zero_dim_ind);
+        Output<Node> ones_like = make_shared<v3::Broadcast>(one_const, shape_of_real);
 
-        // For zeroes:
         auto zero_const = create_same_type_const_scalar<int32_t>(x_real, 0);
-        // create a tensor of zeros of shape with extra dimension
-        Output<Node> ones_like_zero = make_shared<v3::Broadcast>(zero_const, shape_of_real);
-        // remove extra dimension by squeezing
-        ones_like_zero = make_shared<v0::Squeeze>(ones_like_zero, zero_dim_ind);
+        Output<Node> zeros_like = make_shared<v3::Broadcast>(zero_const, shape_of_real);
+        auto result = make_shared<v0::Concat>(OutputVector{ones_like, zeros_like}, -1);
+        set_node_name(node.get_name(), result);
 
-        auto ones_like_output = make_shared<v0::Concat>(OutputVector{ones_like_one, ones_like_zero}, -1);
-        auto ones_like_output_complex =
-            make_shared<ComplexTypeMark>(ones_like_output, ones_like_complex->get_complex_part_type())
-                set_node_name(node.get_name(), ones_like_output.get_node_shared_ptr());
-        return {ones_like_output_complex};
-    } else {
-        Output<Node> shape_of = make_shared<v3::ShapeOf>(x, element::i32);
-        auto one_const = create_same_type_const_scalar<int32_t>(x, 1);
+        auto ones_like_complex = make_shared<ComplexTypeMark>(result, complex_type_mark_x->get_complex_part_type());
 
-        // in case of x to be scalar, we need handle it more specifically
-        // since Broadcast supports only broadcasting to rank greater 0
-        // we have to introduce extra dimension for input scalar case
-        auto one_dim = make_shared<v0::Constant>(element::i32, Shape{1}, 1);
-        shape_of = make_shared<v0::Concat>(OutputVector{one_dim, shape_of}, 0);
-
-        // create a tensor of zeros of shape with extra dimension
-        Output<Node> ones_like = make_shared<v3::Broadcast>(one_const, shape_of);
-        // remove extra dimension by squeezing
-        auto zero_dim_ind = make_shared<v0::Constant>(element::i32, Shape{1}, 0);
-        ones_like = make_shared<v0::Squeeze>(ones_like, zero_dim_ind);
-        set_node_name(node.get_name(), ones_like.get_node_shared_ptr());
-        return {ones_like};
+        return {ones_like_complex};
     }
+
+    Output<Node> shape_of = make_shared<v3::ShapeOf>(x, element::i32);
+    auto one_const = create_same_type_const_scalar<int32_t>(x, 1);
+
+    // in case of x to be scalar, we need handle it more specifically
+    // since Broadcast supports only broadcasting to rank greater 0
+    // we have to introduce extra dimension for input scalar case
+    auto one_dim = make_shared<v0::Constant>(element::i32, Shape{1}, 1);
+    shape_of = make_shared<v0::Concat>(OutputVector{one_dim, shape_of}, 0);
+
+    // create a tensor of zeros of shape with extra dimension
+    Output<Node> ones_like = make_shared<v3::Broadcast>(one_const, shape_of);
+    set_node_name(node.get_name(), ones_like.get_node_shared_ptr());
+    return {ones_like};
 }
 }  // namespace op
 }  // namespace tensorflow
