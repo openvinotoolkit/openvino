2c660b04a1a31474bf26b5c357bfe20d734efc32
Roman Kazantsev <roman.kazantsev@intel.com>
2024-03-15 13:36:16 +0400

    Update tests/layer_tests/tensorflow_tests/test_tf_Bincount.py

--- tests/layer_tests/tensorflow_tests/test_tf_Bincount.py
+++ tests/layer_tests/tensorflow_tests/test_tf_Bincount.py
@@ -59,6 +59,7 @@ class TestBincount(CommonTFLayerTest):
 
     @pytest.mark.parametrize("params", test_data)
     @pytest.mark.precommit_tf_fe
+    @pytest.mark.nightly
     def test_bincount(self, params, ie_device, precision, ir_version, temp_dir):
         self._test(*self.create_bincount_net(**params),
                    ie_device, precision, ir_version, temp_dir=temp_dir)
