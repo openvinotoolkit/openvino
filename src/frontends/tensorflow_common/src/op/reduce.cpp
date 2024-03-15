57fb48145a676c33cc306f7ca8874074b6732817
MonalSD <monaldhankar@gmail.com>
2024-03-15 11:46:34 +0530

    Translator for prod_op operation

--- src/frontends/tensorflow_common/src/op/reduce.cpp
+++ src/frontends/tensorflow_common/src/op/reduce.cpp
@@ -26,23 +26,31 @@ OutputVector translate_direct_reduce_op(const NodeContext& node) {
     auto input = node.get_input(0);
     auto axis = node.get_input(1);
     auto keep_dims = node.get_attribute<bool>("keep_dims", false);
-    auto bool is_input_complex =
-        input->get_data_type() == DataType::Complex64 || input->get_data_type() == DataType::Complex128;
-    if (is_input_complex) {
-        auto complex_type_mark = input->get_attribute<ComplexTypeMark>("ComplexTypeMark");
+    auto reduce_op = make_shared<T>(input, axis, keep_dims);
+    set_node_name(node.get_name(), reduce_op);
+    return {reduce_op};
+}
+
+OutputVector translate_prod_op(const NodeContext& node) {
+    default_op_checks(node, 2, {"Prod"}, true);
+    auto input = node.get_input(0);
+    auto axis = node.get_input(1);
+    auto keep_dims = node.get_attribute<bool>("keep_dims", false);
+    auto complex_type_input = as_type_ptr<ComplexTypeMark>(input.get_node_shared_ptr());
+    if (complex_type_input) {
+        input = complex_type_input->input_value(0);
         auto output_shape = input->get_shape();
         output_shape.push_back(2);  // Adding auxillary dimension
-
-        auto output_tensor = make_shared<Tensor>(DataType::Float32, output_shape);
-        output_tensor->set_attribute("ComplexTypeMark", complex_type_mark);
-
-        auto reduce_op = make_shared<T>(input, axis, keep_dims);
-        set_node_name(node.get_name(), reduce_op);
-        return {reduce_op, output_tensor};
+        auto output_tensor = make_shared<Tensor>(element::f32, output_shape);
+        output_tensor->set_attribute("ComplexTypeMark", complex_type_input);
+        auto prod_op = make_shared<v1::ReduceProd>(input, axis, keep_dims);
+        auto complex_result = make_shared<ComplexTypeMark>(prod_op, complex_type_input->get_complex_part_type());
+        set_node_name(node.get_name(), prod_op);
+        return {complex_result};
     } else {
-        auto reduce_op = make_shared<T>(input, axis, keep_dims);
-        set_node_name(node.get_name(), reduce_op);
-        return {reduce_op};
+        auto prod_op = make_shared<v1::ReduceProd>(input, axis, keep_dims);
+        set_node_name(node.get_name(), prod_op);
+        return {prod_op};
     }
 }
 
