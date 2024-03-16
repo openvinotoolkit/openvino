1e55f9c9f55a5f7039b9d2a9b37d282df75f271e
MonalSD <monaldhankar@gmail.com>
2024-03-16 19:16:22 +0530

    Support complex tensors for ReverseSequence operations

--- src/frontends/tensorflow_common/src/op/reverse_sequence.cpp
+++ src/frontends/tensorflow_common/src/op/reverse_sequence.cpp
@@ -5,6 +5,8 @@
 #include "openvino/op/reverse_sequence.hpp"
 
 #include "common_op_table.hpp"
+#include "helper_ops/complex_type_mark.hpp"
+#include "openvino/op/add.hpp"
 
 using namespace std;
 using namespace ov::op;
@@ -14,13 +16,28 @@ namespace frontend {
 namespace tensorflow {
 namespace op {
 OutputVector translate_reverse_sequence_op(const NodeContext& node) {
-    default_op_checks(node, 2, {"ReverseSequence"});
+    default_op_checks(node, 2, {"ReverseSequence"}, true);
     auto input = node.get_input(0);
     auto seq_lengths = node.get_input(1);
 
     // retrieve attributes
     auto seq_dim = node.get_attribute<int64_t>("seq_dim");
     auto batch_dim = node.get_attribute<int64_t>("batch_dim", 0);
+
+    auto complex_type_mark = as_type_ptr<ComplexTypeMark>(input.get_node_shared_ptr());
+    if (complex_type_mark) {
+        auto const_one = make_shared<v0::Constant>(element::i32, Shape{1}, 1);
+        auto updated_seq_dim = make_shared<v1::Add>(seq_dim, const_one);
+        auto updated_batch_dim = make_shared<v1::Add>(batch_dim, const_one);
+
+        auto reverse_sequence =
+            make_shared<v0::ReverseSequence>(input, seq_lengths, updated_batch_dim, updated_seq_dim);
+        set_node_name(node.get_name(), reverse_sequence);
+
+        auto complex_reverse =
+            make_shared<ComplexTypeMark>(reverse_sequence, complex_type_mark->get_complex_part_type());
+        return {complex_reverse};
+    }
 
     auto reverse_sequence = make_shared<v0::ReverseSequence>(input, seq_lengths, batch_dim, seq_dim);
     set_node_name(node.get_name(), reverse_sequence);
