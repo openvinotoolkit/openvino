f7409c7d4efef6f7383733e3acf8f2fc2f5ba415
rghvsh <116428320+rghvsh@users.noreply.github.com>
2024-03-17 23:09:04 +0530

    Update test_atan2.py

--- tests/layer_tests/pytorch_tests/test_atan2.py
+++ tests/layer_tests/pytorch_tests/test_atan2.py
@@ -9,32 +9,61 @@ import numpy as np
 from pytorch_layer_test_class import PytorchLayerTest
 
 class TestAtan2(PytorchLayerTest):
-    def _prepare_input(self, x, y, dtype=None):
-        inputs_data = {}
-        inputs_data['y'] = np.array(y).astype(dtype) - np.array(y).astype(dtype)
-        inputs_data['x'] = np.array(x).astype(dtype) - np.array(x).astype(dtype)
-        return inputs_data
+    def _prepare_input(self, y, x, ref_dtype=None):
+        inputs = [np.array(y).astype(ref_dtype) - np.array(y).astype(ref_dtype), np.array(x).astype(ref_dtype) - np.array(x).astype(ref_dtype)]
+        if ref_dtype:
+            inputs.append(np.zeros(1).astype(ref_dtype))
+        return inputs
 
-    def create_model(self, input_type):
-        class aten_atan2(torch.nn.Module):
-            def __init__(self):
+    def create_model(self, dtype=None, use_out=False):
+        dtype_map = {
+            "float32": torch.float32,
+            "float64": torch.float64,
+            "int64": torch.int64,
+            "int32": torch.int32,
+            "uint8": torch.uint8,
+            "int8": torch.int8,
+            }
+    
+        class aten_atan2_out(torch.nn.Module):
+            def __init__(self, out) -> None:
                 super().__init__()
-                self.input_type = input_type
+                self.out = torch.empty(25, dtype=out)
+    
+            def forward(self, y, x):
+                return torch.atan2(input = y, other = x, out=self.out)
 
-            def forward(self, input_tensor, other_tensor):
-                return torch.atan2(input_tensor.to(self.input_type), other_tensor.to(self.input_type))
+        class aten_atan2(torch.nn.Module):
+            def __init__(self) -> None:
+                super().__init__()
+    
+            def forward(self, y, x):
+                return torch.atan2(input = y, other = x)
+        
+        dtype = dtype_map.get(dtype)
 
+        if out_use:
+            model_class = aten_atan2_out(dtype)
+        else:
+            model_class = aten_atan2()
+                
+    
         ref_net = None
-
-        return aten_atan2(), ref_net, "aten::atan2"
-
-    @pytest.mark.parametrize(("input_type"), [
-        (torch.float16),
-        (torch.int32),
-        (torch.float64),
-        (torch.float32),
-    ])
+    
+        return model_class, ref_net, "aten::atan2"
+        
     @pytest.mark.nightly
     @pytest.mark.precommit
-    def test_atan2(self, input_type, ie_device, precision, ir_version):
-        self._test(*self.create_model(input_type), ie_device, precision, ir_version, use_convert_model=True)
+    @pytest.mark.parametrize("dtype", [None, "float32", "float64", "int32", "int64", "int8", "uin8"])
+    @pytest.mark.parametrize(
+        "start,end,steps", [(0, 1), (0, 0), (1, -5), (1, 10), (-1, -5), (-1, -5), (1.25, -5.5)]
+    )
+    @pytest.mark.parametrize("use_out", [False, True])
+    def test_linspace_with_out(self, dtype, use_out, y, x, ie_device, precision, ir_version):
+        self._test(
+            *self.create_model(dtype=dtype, use_out=use_out),
+            ie_device,
+            precision,
+            ir_version,
+            kwargs_to_prepare_input={"y": y, "x": x}
+        )
