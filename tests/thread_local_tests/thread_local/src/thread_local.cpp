0b43a64ba4a284ca8197dc6e5e779e840468ec8e
xufang <fang.xu@intel.com>
2024-03-18 09:09:35 +0800

    apply formatting

--- tests/thread_local_tests/thread_local/src/thread_local.cpp
+++ tests/thread_local_tests/thread_local/src/thread_local.cpp
@@ -1,25 +1,26 @@
 // Copyright (C) 2018-2024 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
-#include <string>
-#include <iostream>
-#include <future>
-#include "openvino/openvino.hpp"
-#include "openvino/runtime/properties.hpp"
-#include "openvino/core/model.hpp"
-#include "openvino/core/type/element_type.hpp"
-#include "openvino/core/node_vector.hpp"
-#include "openvino/op/concat.hpp"
-#include "openvino/op/constant.hpp"
-#include "openvino/op/split.hpp"
-#include "openvino/op/reshape.hpp"
 #include "thread_local.hpp"
 
-std::shared_ptr<ov::Model> make_split_concat(std::vector<size_t> inputShape = {1, 4, 24, 24},
-                                           ov::element::Type_t type = ov::element::Type_t::f32);
+#include <future>
+#include <iostream>
+#include <string>
 
-void core_get_property_test(const std::string& target_device)
-{
+#include "openvino/core/model.hpp"
+#include "openvino/core/node_vector.hpp"
+#include "openvino/core/type/element_type.hpp"
+#include "openvino/op/concat.hpp"
+#include "openvino/op/constant.hpp"
+#include "openvino/op/reshape.hpp"
+#include "openvino/op/split.hpp"
+#include "openvino/openvino.hpp"
+#include "openvino/runtime/properties.hpp"
+
+std::shared_ptr<ov::Model> make_split_concat(std::vector<size_t> inputShape = {1, 4, 24, 24},
+                                             ov::element::Type_t type = ov::element::Type_t::f32);
+
+void core_get_property_test(const std::string& target_device) {
     std::promise<void> call_finish_promise;
     auto call_finish_future = call_finish_promise.get_future();
     std::promise<void> thread_exit_promise;
@@ -27,22 +28,20 @@ void core_get_property_test(const std::string& target_device)
     std::thread sub_thread;
     {
         ov::Core ie;
-        sub_thread = std::thread([&]
-                                 {
+        sub_thread = std::thread([&] {
             ie.get_property(target_device, ov::supported_properties);
             call_finish_promise.set_value();
-            thread_exit_future.get(); });
+            thread_exit_future.get();
+        });
         call_finish_future.get();
     }
     thread_exit_promise.set_value();
-    if (sub_thread.joinable())
-    {
+    if (sub_thread.joinable()) {
         sub_thread.join();
     }
 }
 
-std::shared_ptr<ov::Model> make_split_concat(std::vector<size_t> inputShape, ov::element::Type_t type)
-{
+std::shared_ptr<ov::Model> make_split_concat(std::vector<size_t> inputShape, ov::element::Type_t type) {
     auto param1 = std::make_shared<ov::op::v0::Parameter>(type, ov::Shape{inputShape});
     param1->set_friendly_name("Param1");
     param1->output(0).get_tensor().set_names({"data1"});
@@ -62,8 +61,7 @@ std::shared_ptr<ov::Model> make_split_concat(std::vector<size_t> inputShape, ov:
     return model_ptr;
 }
 
-void core_infer_test(const std::string& target_device)
-{
+void core_infer_test(const std::string& target_device) {
     std::promise<void> call_finish_promise;
     std::future<void> call_finish_future = call_finish_promise.get_future();
     std::promise<void> thread_exit_promise;
@@ -72,19 +70,18 @@ void core_infer_test(const std::string& target_device)
     auto actualNetwork = make_split_concat();
     {
         ov::Core ie;
-        sub_thread = std::thread([&]
-                                 {
+        sub_thread = std::thread([&] {
             auto net = ie.compile_model(actualNetwork, target_device);
             auto infer_req = net.create_infer_request();
             ie.get_property(target_device, ov::supported_properties);
             infer_req.infer();
             call_finish_promise.set_value();
-            thread_exit_future.get(); });
+            thread_exit_future.get();
+        });
         call_finish_future.get();
     }
     thread_exit_promise.set_value();
-    if (sub_thread.joinable())
-    {
+    if (sub_thread.joinable()) {
         sub_thread.join();
     }
 }
--- tests/thread_local_tests/thread_local_test/tests.cpp
+++ tests/thread_local_tests/thread_local_test/tests.cpp
@@ -2,24 +2,22 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 #include <gtest/gtest.h>
-#include <thread>
-#include <future>
-#include <windows.h>
 #include <psapi.h>
 #include <tlhelp32.h>
+#include <windows.h>
+
+#include <future>
+#include <thread>
 
 typedef void (*TestFunc)(const std::string&);
 
-class ThreadLocalTest : public ::testing::Test, public ::testing::WithParamInterface<std::string>
-{
+class ThreadLocalTest : public ::testing::Test, public ::testing::WithParamInterface<std::string> {
 public:
-    void SetUp()
-    {
+    void SetUp() {
         target_device = GetParam();
     }
 
-    static std::string getTestCaseName(testing::TestParamInfo<std::string> obj)
-    {
+    static std::string getTestCaseName(testing::TestParamInfo<std::string> obj) {
         return obj.param;
     }
 
@@ -27,79 +25,71 @@ public:
     std::string target_device = "";
 };
 
-TEST(LoadLibraryTest, load_free_library)
-{
+TEST(LoadLibraryTest, load_free_library) {
     std::promise<void> free_promise;
     std::future<void> free_future = free_promise.get_future();
     std::promise<void> thread_exit_promise;
     std::future<void> thread_exit_future = thread_exit_promise.get_future();
     auto shared_object = LoadLibraryA("openvino.dll");
 
-    std::thread sub_thread = std::thread([&]
-                                         {
+    std::thread sub_thread = std::thread([&] {
         free_promise.set_value();
-        thread_exit_future.get(); });
+        thread_exit_future.get();
+    });
     free_future.get();
     FreeLibrary(shared_object);
 
     thread_exit_promise.set_value();
-    if (sub_thread.joinable())
-    {
+    if (sub_thread.joinable()) {
         sub_thread.join();
     }
 }
 
-TEST_P(ThreadLocalTest, get_property_test)
-{
+TEST_P(ThreadLocalTest, get_property_test) {
     auto shared_object = LoadLibraryA("ov_thread_local.dll");
     auto procAddr = reinterpret_cast<TestFunc>(GetProcAddress(shared_object, "core_get_property_test"));
     procAddr(target_device);
     FreeLibrary(shared_object);
 }
 
-TEST_P(ThreadLocalTest, infer_test)
-{
+TEST_P(ThreadLocalTest, infer_test) {
     auto shared_object = LoadLibraryA("ov_thread_local.dll");
     auto procAddr = reinterpret_cast<TestFunc>(GetProcAddress(shared_object, "core_infer_test"));
     procAddr(target_device);
     FreeLibrary(shared_object);
 }
 
-void process_sub_thread(const std::string& func_name, const std::string& target_device)
-{
+void process_sub_thread(const std::string& func_name, const std::string& target_device) {
     auto shared_object = LoadLibraryA("ov_thread_local.dll");
     auto procAddr = reinterpret_cast<TestFunc>(GetProcAddress(shared_object, func_name.c_str()));
     std::promise<void> free_promise;
     std::future<void> free_future = free_promise.get_future();
     std::promise<void> thread_exit_promise;
     std::future<void> thread_exit_future = thread_exit_promise.get_future();
-    std::thread sub_thread = std::thread([&]
-                                         {
+    std::thread sub_thread = std::thread([&] {
         procAddr(target_device);
         free_promise.set_value();
-        thread_exit_future.get(); });
+        thread_exit_future.get();
+    });
 
     free_future.get();
     FreeLibrary(shared_object);
 
     thread_exit_promise.set_value();
-    if (sub_thread.joinable())
-    {
+    if (sub_thread.joinable()) {
         sub_thread.join();
     }
 }
 
-TEST_P(ThreadLocalTest, get_property_test_subthread)
-{
+TEST_P(ThreadLocalTest, get_property_test_subthread) {
     process_sub_thread("core_get_property_test", target_device);
 }
 
-TEST_P(ThreadLocalTest, infer_test_subthread)
-{
+TEST_P(ThreadLocalTest, infer_test_subthread) {
     process_sub_thread("core_infer_test", target_device);
 }
 
-
 INSTANTIATE_TEST_SUITE_P(OV_ThreadLocalTests,
-                         ThreadLocalTest, ::testing::Values("CPU", "GPU"),
+                         ThreadLocalTest,
+                         ::testing::Values("CPU", "GPU"),
                          ThreadLocalTest::getTestCaseName);
