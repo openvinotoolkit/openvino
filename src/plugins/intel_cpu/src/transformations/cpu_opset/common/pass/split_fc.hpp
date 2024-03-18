d5c4f9b3620b6352042d4df36738c5d91ce1de18
Xiuchuan Zhai <xiuchuan.zhai@intel.com>
2024-03-18 09:28:11 +0100

    fix comments

--- src/plugins/intel_cpu/src/transformations/cpu_opset/common/pass/split_fc.hpp
+++ src/plugins/intel_cpu/src/transformations/cpu_opset/common/pass/split_fc.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2018-2023 Intel Corporation
+// Copyright (C) 2018-2024 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
 
@@ -8,6 +8,68 @@
 
 namespace ov {
 namespace intel_cpu {
+
+/*
+ * Description:
+ *      SplitFC detects FC CPU operation with and without compressed weighted.
+ *      And then split the FC into serveral small FCs according to sub stream number.
+ *      The goal is that the executor can dispatch the splited FCs to different numa node in the system.
+ *      As a result, the splited FCs can be executed at the parallel level.
+ *
+ * Before:
+ *
+ *             +-------+                         +-------+
+ *             |   X   |                         |   W   |
+ *             |       |                         |       |
+ *             |       |                         |       |
+ *             +-------+                         +-------+
+ *                 |                                 |
+ *                 |                                 |
+ * +---------------v---------------------------------v--------------+
+ * |                                                                |
+ * |                        FullyConnected                          |
+ * |                                                                |
+ * +------------------------------+---------------------------------+
+ *                                |
+ *                                | Output
+ *                                v
+ *
+ * After:
+ *
+ *            +-------+                           +-------+
+ *            |   X   |                           |   W   |
+ *            |       |                           |       |
+ *            |       |                           |       |
+ *            +---+---+                           +---+---+
+ *                |                                   |
+ *                |                                   |
+ *                |                           +-------v-------+
+ *                |                           |               |
+ *                |                           | VariadicSplit |
+ *                |                           |               |
+ *                |                           +--+---------+--+
+ *                |                              |         |
+ *                |     +------------------------+         |
+ *                |     |                                  |
+ *            +---------|------------------------+         |
+ *            |         |                        |         |
+ * +----------v---------v---------+  +-----------v---------v--------+
+ * |                              |  |                              |
+ * |        FullyConnected        |  |        FullyConnected        |
+ * |                              |  |                              |
+ * +--------------+---------------+  +--------------+---------------+
+ *                |                                 |
+ *                | Output                          | Output
+ *                |                                 |
+ * +--------------v---------------------------------v---------------+
+ * |                                                                |
+ * |                            Concat                              |
+ * |                                                                |
+ * +-------------------------------+--------------------------------+
+ *                                 |
+ *                                 |
+ *                                 v
+ */
 
 class SplitFC: public ov::pass::MatcherPass {
 public:
