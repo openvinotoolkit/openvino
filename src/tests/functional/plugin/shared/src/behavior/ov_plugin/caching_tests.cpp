b8381f0f81c35bff79e2f9f376082f6602ddb570
Wang, Yang <yang4.wang@intel.com>
2024-03-18 10:39:26 +0800

    Update.

--- src/tests/functional/plugin/shared/src/behavior/ov_plugin/caching_tests.cpp
+++ src/tests/functional/plugin/shared/src/behavior/ov_plugin/caching_tests.cpp
@@ -242,8 +242,8 @@ void CompileModelCacheTestBase::run() {
         GTEST_FAIL() << "Can't compile network without cache for " << m_functionName << " with precision " << m_precision.get_type_name() << std::endl;
     }
     auto originalOutputs = get_plugin_outputs();
-    size_t blobSizeInitial = -1;
-    size_t blobSizeAfterwards = -1;
+    size_t blobCountInitial = -1;
+    size_t blobCountAfterwards = -1;
     for (int i = 0; i < 2; i++) {
         // Step 2: Load with cache. Export or import shall not throw
         compiledModel = {}; // Destroy network object
@@ -251,28 +251,24 @@ void CompileModelCacheTestBase::run() {
         {
             core->set_property(ov::cache_dir(m_cacheFolderName));
             ASSERT_NO_THROW(compiledModel = core->compile_model(function, targetDevice, configuration));
-            if (targetDevice.find("AUTO") == std::string::npos) {
-                // Apply check only for HW plugins
-                ASSERT_EQ(i != 0, compiledModel.get_property(ov::loaded_from_cache));
-            } else {
-                while (true) {
-                    auto exeDevices = compiledModel.get_property(ov::execution_devices);
-                    if (exeDevices.size() == 1 && exeDevices.front().find("(CPU)") != std::string::npos)
-                        continue;
-                    break;
-                }
+            ASSERT_EQ(i != 0, compiledModel.get_property(ov::loaded_from_cache));
+            while (targetDevice.find("AUTO") != std::string::npos) {
+                auto exeDevices = compiledModel.get_property(ov::execution_devices);
+                if (exeDevices.size() == 1 && exeDevices.front().find("(CPU)") != std::string::npos)
+                    continue;
+                break;
             }
             generate_inputs(targetStaticShapes.front());
             ASSERT_NO_THROW(infer());
         }
         if (i == 0) {
             // blob size should be greater than 0 initially
-            blobSizeInitial = ov::test::utils::listFilesWithExt(m_cacheFolderName, "blob").size();
-            ASSERT_GT(blobSizeInitial, 0);
+            blobCountInitial = ov::test::utils::listFilesWithExt(m_cacheFolderName, "blob").size();
+            ASSERT_GT(blobCountInitial, 0);
         } else {
             // cache is created and reused
-            blobSizeAfterwards = ov::test::utils::listFilesWithExt(m_cacheFolderName, "blob").size();
-            ASSERT_EQ(blobSizeInitial, blobSizeAfterwards);
+            blobCountAfterwards = ov::test::utils::listFilesWithExt(m_cacheFolderName, "blob").size();
+            ASSERT_EQ(blobCountInitial, blobCountAfterwards);
         }
         compare(originalOutputs, get_plugin_outputs());
     }
