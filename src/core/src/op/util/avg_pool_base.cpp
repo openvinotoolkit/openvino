0fa32444523ffe467e6cdc7d1321ad0e35c32d08
Vladislav Denisov <vlad.roix@yandex.ru>
2024-03-18 19:51:01 +0400

    [Core] Changed nesting style for namespace and approach of initialization constructor params.

--- src/core/src/op/util/avg_pool_base.cpp
+++ src/core/src/op/util/avg_pool_base.cpp
@@ -6,26 +6,30 @@
 
 #include "itt.hpp"
 
-ov::op::util::AvgPoolBase::AvgPoolBase(const Output<Node>& arg,
-                                       const Strides& strides,
-                                       const Shape& pads_begin,
-                                       const Shape& pads_end,
-                                       const Shape& kernel,
-                                       bool exclude_pad,
-                                       RoundingType rounding_type,
-                                       PadType auto_pad)
-    : Op({arg}),
-      m_kernel(kernel),
-      m_strides(strides),
-      m_pads_begin(pads_begin),
-      m_pads_end(pads_end),
-      m_exclude_pad(exclude_pad),
-      m_auto_pad(auto_pad),
-      m_rounding_type(rounding_type) {
+namespace ov {
+namespace op {
+namespace util {
+
+AvgPoolBase::AvgPoolBase(const Output<Node>& arg,
+                         const Strides& strides,
+                         const Shape& pads_begin,
+                         const Shape& pads_end,
+                         const Shape& kernel,
+                         bool exclude_pad,
+                         RoundingType rounding_type,
+                         PadType auto_pad)
+    : Op{{arg}},
+      m_kernel{kernel},
+      m_strides{strides},
+      m_pads_begin{pads_begin},
+      m_pads_end{pads_end},
+      m_exclude_pad{exclude_pad},
+      m_auto_pad{auto_pad},
+      m_rounding_type{rounding_type} {
     constructor_validate_and_infer_types();
 }
 
-void ov::op::util::AvgPoolBase::validate_and_infer_types() {
+void AvgPoolBase::validate_and_infer_types() {
     OV_OP_SCOPE(util_AvgPoolBase_validate_and_infer_types);
 
     if (m_strides.empty()) {
@@ -41,7 +45,7 @@ void ov::op::util::AvgPoolBase::validate_and_infer_types() {
     }
 }
 
-bool ov::op::util::AvgPoolBase::visit_attributes(AttributeVisitor& visitor) {
+bool AvgPoolBase::visit_attributes(AttributeVisitor& visitor) {
     OV_OP_SCOPE(util_AvgPoolBase_visit_attributes);
     visitor.on_attribute("kernel", m_kernel);
     visitor.on_attribute("strides", m_strides);
@@ -53,58 +57,62 @@ bool ov::op::util::AvgPoolBase::visit_attributes(AttributeVisitor& visitor) {
     return true;
 }
 
-const ov::Shape& ov::op::util::AvgPoolBase::get_kernel() const {
+const Shape& AvgPoolBase::get_kernel() const {
     return m_kernel;
 }
 
-void ov::op::util::AvgPoolBase::set_kernel(const Shape& kernel) {
+void AvgPoolBase::set_kernel(const Shape& kernel) {
     m_kernel = kernel;
 }
 
-const ov::Strides& ov::op::util::AvgPoolBase::get_strides() const {
+const Strides& AvgPoolBase::get_strides() const {
     return m_strides;
 
 }
-void ov::op::util::AvgPoolBase::set_strides(const Strides& strides) {
+void AvgPoolBase::set_strides(const Strides& strides) {
     m_strides = strides;
 }
 
-const ov::Shape& ov::op::util::AvgPoolBase::get_pads_begin() const {
+const Shape& AvgPoolBase::get_pads_begin() const {
     return m_pads_begin;
 }
 
-void ov::op::util::AvgPoolBase::set_pads_begin(const Shape& pads_begin) {
+void AvgPoolBase::set_pads_begin(const Shape& pads_begin) {
     m_pads_begin = pads_begin;
 }
 
-const ov::Shape& ov::op::util::AvgPoolBase::get_pads_end() const {
+const Shape& AvgPoolBase::get_pads_end() const {
     return m_pads_end;
 }
 
-void ov::op::util::AvgPoolBase::set_pads_end(const Shape& pads_end) {
+void AvgPoolBase::set_pads_end(const Shape& pads_end) {
     m_pads_end = pads_end;
 }
 
-bool ov::op::util::AvgPoolBase::get_exclude_pad() const {
+bool AvgPoolBase::get_exclude_pad() const {
     return m_exclude_pad;
 }
 
-void ov::op::util::AvgPoolBase::set_exclude_pad(bool exclude_pad) {
+void AvgPoolBase::set_exclude_pad(bool exclude_pad) {
     m_exclude_pad = exclude_pad;
 }
 
-const ov::op::PadType& ov::op::util::AvgPoolBase::get_auto_pad() const {
+const PadType& AvgPoolBase::get_auto_pad() const {
     return m_auto_pad;
 }
 
-void ov::op::util::AvgPoolBase::set_auto_pad(const PadType& auto_pad) {
+void AvgPoolBase::set_auto_pad(const PadType& auto_pad) {
     m_auto_pad = auto_pad;
 }
 
-ov::op::RoundingType ov::op::util::AvgPoolBase::get_rounding_type() const {
+RoundingType AvgPoolBase::get_rounding_type() const {
     return m_rounding_type;
 }
 
-void ov::op::util::AvgPoolBase::set_rounding_type(RoundingType rounding_type) {
+void AvgPoolBase::set_rounding_type(RoundingType rounding_type) {
     m_rounding_type = rounding_type;
 }
+
+}  // namespace util
+}  // namespace op
+}  // namespace ov
