6a3b6b5b0c9b810431044a06bd5eff1248463ee8
Roboreptile <piotr.krzeminski@intel.com>
2024-03-18 09:14:26 +0000

    [FIX][DEBUG] Show both tensor comparisons

--- src/tests/functional/shared_test_classes/src/single_op/inverse.cpp
+++ src/tests/functional/shared_test_classes/src/single_op/inverse.cpp
@@ -99,6 +99,28 @@ void InverseLayerTest::compare(const std::vector<ov::Tensor>& expected, const st
         std::cout << '\n';
     }
 
+    if(t2.get_shape().size() == 3) {
+        for (size_t i = 0; i < t2.get_shape()[0]; ++i) {
+            for (size_t x = 0; x < t2.get_shape()[1]; ++x) {
+                for (size_t y = 0; y < t2.get_shape()[2]; ++y) {
+                    float val = ((float*)t2.data())[i * t2.get_shape()[0] + x * t2.get_shape()[1] + y];
+                    std::cout << val << ' ';
+                }
+                std::cout << '\n';
+            }
+            std::cout << '\n';
+        }
+    } else if (t2.get_shape().size() == 2) {
+        for (size_t x = 0; x < t2.get_shape()[0]; ++x) {
+            for (size_t y = 0; y < t2.get_shape()[1]; ++y) {
+                float val = ((float*)t2.data())[x * t2.get_shape()[0] + y];
+                std::cout << val << ' ';
+            }
+            std::cout << '\n';
+        }
+        std::cout << '\n';
+    }
+
     if (expected[0].get_element_type() == ov::element::bf16) {
         ov::test::utils::compare(t1, t2, 1.0f, 1.2f);
     } else {
