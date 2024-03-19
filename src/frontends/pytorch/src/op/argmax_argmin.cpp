aab368c818fc7598bfef2c62c4c95d06f458ad79
Cavus Mustafa <mustafa.cavus@intel.com>
2024-03-19 14:42:43 -0700

    Revert removing convert from argmax_argmin translation

--- src/frontends/pytorch/src/op/argmax_argmin.cpp
+++ src/frontends/pytorch/src/op/argmax_argmin.cpp
@@ -31,10 +31,10 @@ OutputVector create_argmax_argmin_op(const NodeContext& context, TopKMode mode) 
     }
     if (!context.input_is_none(1)) {
         auto axis = context.const_input<int64_t>(1);
-        indices =
+        auto topk =
             context
-                .mark_node(std::make_shared<v11::TopK>(input, k, axis, mode, TopKSortType::NONE, element::i64, true))
-                ->output(1);
+                .mark_node(std::make_shared<v11::TopK>(input, k, axis, mode, TopKSortType::NONE, element::i32, true));
+        indices = context.mark_node(std::make_shared<v0::Convert>(topk->output(1), element::i64));
         if (!keep_dims) {
             auto axis_to_remove = context.mark_node(v0::Constant::create(element::i32, Shape{}, {axis}));
             indices = context.mark_node(std::make_shared<v0::Squeeze>(indices, axis_to_remove));
@@ -43,11 +43,11 @@ OutputVector create_argmax_argmin_op(const NodeContext& context, TopKMode mode) 
         int64_t axis = 0;
         auto minus_one = context.mark_node(v0::Constant::create(element::i32, Shape{1}, {-1}));
         auto flatten_input = context.mark_node(std::make_shared<v1::Reshape>(input, minus_one, false));
-        indices =
+        auto topk =
             context
                 .mark_node(
-                    std::make_shared<v11::TopK>(flatten_input, k, axis, mode, TopKSortType::NONE, element::i64, true))
-                ->output(1);
+                    std::make_shared<v11::TopK>(flatten_input, k, axis, mode, TopKSortType::NONE, element::i32, true));
+        indices = context.mark_node(std::make_shared<v0::Convert>(topk->output(1), element::i64));
         if (keep_dims) {
             auto input_shape = context.mark_node(std::make_shared<v3::ShapeOf>(input, element::i32));
             auto input_rank = context.mark_node(std::make_shared<v3::ShapeOf>(input_shape, element::i32));
