e8b8c55d95795bbaf0eb7528899d11e0813295f8
Maxim Vafin <maxim.vafin@intel.com>
2024-03-19 11:21:51 +0100

    Fix style

--- src/bindings/python/src/openvino/frontend/pytorch/module_extension.py
+++ src/bindings/python/src/openvino/frontend/pytorch/module_extension.py
@@ -1,11 +1,8 @@
-
 # Copyright (C) 2018-2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
 # flake8: noqa
 # mypy: ignore-errors
-
-import typing
 
 class ModuleExtension:
     def __init__(self, module, target_op, evaluate=None, convert=None):
