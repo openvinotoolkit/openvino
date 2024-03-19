4f28e1924ca2ba8d12b5d638b78ad6e0f75addb5
Cavus Mustafa <mustafa.cavus@intel.com>
2024-03-19 00:07:02 -0700

    Enable TorchFX unit test for new_ones

--- tests/layer_tests/pytorch_tests/test_full.py
+++ tests/layer_tests/pytorch_tests/test_full.py
@@ -636,6 +636,7 @@ class TestNewOnes(PytorchLayerTest):
     @pytest.mark.nightly
     @pytest.mark.precommit
     @pytest.mark.precommit_torch_export
+    @pytest.mark.precommit_fx_backend
     def test_new_ones(self, shape, input_dtype, ie_device, precision, ir_version):
         self._test(*self.create_model(shape), ie_device, precision, ir_version,
                    kwargs_to_prepare_input={'input_dtype': input_dtype}, use_convert_model=True)
@@ -645,6 +646,7 @@ class TestNewOnes(PytorchLayerTest):
     @pytest.mark.parametrize("dtype", ["bool", "uint8", "int8", "int32", "int64", "float32", "float64"])
     @pytest.mark.nightly
     @pytest.mark.precommit_torch_export
+    @pytest.mark.precommit_fx_backend
     def test_new_ones_with_dtype(self, shape, dtype, input_dtype, ie_device, precision, ir_version):
         self._test(*self.create_model(shape, dtype=dtype, used_dtype=True), ie_device, precision, ir_version,
                    kwargs_to_prepare_input={'input_dtype': input_dtype}, use_convert_model=True)
