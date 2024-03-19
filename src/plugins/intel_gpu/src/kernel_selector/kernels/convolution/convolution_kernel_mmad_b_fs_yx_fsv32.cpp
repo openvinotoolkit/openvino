325ccbc8ddb98362419e5424171404a6795656cf
Durandin, Pavel <pavel.durandin@intel.com>
2024-03-19 15:07:35 +0400

    [GPU] LNL simd fix

--- src/plugins/intel_gpu/src/kernel_selector/kernels/convolution/convolution_kernel_mmad_b_fs_yx_fsv32.cpp
+++ src/plugins/intel_gpu/src/kernel_selector/kernels/convolution/convolution_kernel_mmad_b_fs_yx_fsv32.cpp
@@ -61,6 +61,9 @@ bool ConvolutionKernel_mmad_b_fs_yx_fsv32::Validate(const Params& p) const {
         && !params.HasCompensation()) {
         return false;
     }
+    
+    if (!IsSIMDSizeSupported(params.engineInfo, 8))
+        return false;
 
     if (params.groups > 1)
         return false;
