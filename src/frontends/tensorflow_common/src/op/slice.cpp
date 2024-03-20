f81afc470def9a00e694a1437bb4a566a9d380d0
ysrastogi <97824759+ysrastogi@users.noreply.github.com>
2024-03-20 20:00:25 +0530

    Update src/frontends/tensorflow_common/src/op/slice.cpp
    Co-authored-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- src/frontends/tensorflow_common/src/op/slice.cpp
+++ src/frontends/tensorflow_common/src/op/slice.cpp
@@ -24,7 +24,7 @@ namespace tensorflow {
 namespace op {
 
 OutputVector translate_slice_op(const NodeContext& node) {
-    default_op_checks(node, 3, {"Slice"});
+    default_op_checks(node, 3, {"Slice"}, true);
     auto input = node.get_input(0);
     auto complex_type_mark = as_type_ptr<ComplexTypeMark>(input.get_node_shared_ptr());
     auto start = node.get_input(1);
