fcb8d9f3603a16741ea89d6c59498c5b692b85f0
Aleksandr Voron <aleksandr.voron@intel.com>
2024-03-20 18:01:47 +0100

    update inferencePrecision selection

--- src/plugins/intel_cpu/src/config.cpp
+++ src/plugins/intel_cpu/src/config.cpp
@@ -362,9 +362,14 @@ void Config::readProperties(const ov::AnyMap& prop, const ModelType modelType) {
     // when both execution_mode and inference_precision are specified
     if (!inferencePrecisionSetExplicitly) {
         if (executionMode == ov::hint::ExecutionMode::PERFORMANCE) {
-            inferencePrecision = hasHardwareSupport(ov::element::f16) ? ov::element::f16 : ov::element::f32;
-        if (mayiuse(avx512_core_bf16))
-            inferencePrecision = ov::element::bf16;
+            inferencePrecision = ov::element::f32;
+#if defined(OPENVINO_ARCH_ARM) || defined(OPENVINO_ARCH_ARM64)
+            if (hasHardwareSupport(ov::element::f16)) {
+                inferencePrecision = ov::element::f16;
+            }
+#endif
+            if (mayiuse(avx512_core_bf16))
+                inferencePrecision = ov::element::bf16;
         } else {
             inferencePrecision = ov::element::f32;
         }
