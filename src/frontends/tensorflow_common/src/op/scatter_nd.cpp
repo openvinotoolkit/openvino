153c3d59b60f4ebcb03386fe9ce446a66bc1dbbd
MonalSD <monaldhankar@gmail.com>
2024-03-24 00:05:15 +0530

    Addressing changes

--- src/frontends/tensorflow_common/src/op/scatter_nd.cpp
+++ src/frontends/tensorflow_common/src/op/scatter_nd.cpp
@@ -3,7 +3,9 @@
 //
 
 #include "common_op_table.hpp"
+#include "helper_ops/complex_type_mark.hpp"
 #include "openvino/op/broadcast.hpp"
+#include "openvino/op/concat.hpp"
 #include "openvino/op/scatter_nd_update.hpp"
 #include "utils.hpp"
 
@@ -23,25 +25,27 @@ OutputVector translate_scatter_nd_op(const NodeContext& node) {
 
     if (complex_type_mark_updates) {
         updates = complex_type_mark_updates->input_value(0);
-    }
-    // Add two auxiliary dimensions to the shape tensor
-    Shape shape_dims = shape->get_shape();
-    shape_dims.insert(shape_dims.begin(), 1);
-    shape_dims.insert(shape_dims.begin(), 1);
-    auto updated_shape = make_shared<v3::Constant>(shape_dims, shape->get_data_type());
+        // Add two auxiliary dimensions to the shape tensor
+        Shape shape_dims = shape.get_shape();
+        shape_dims.insert(shape_dims.begin(), 2);
+        auto aux_shape = create_same_type_const<int32_t>(shape, std::vector<int32_t>{2}, Shape{1});
+        auto updated_shape = make_shared<v0::Concat>(OutputVector{aux_shape, shape}, 0);
 
-    auto input_data = create_same_type_const<int32_t>(updates, vector<int32_t>{0}, Shape{1});
-    auto broadcast = make_shared<v3::Broadcast>(input_data, updated_shape);
+        auto input_data = create_same_type_const<int32_t>(updates, vector<int32_t>{0}, Shape{1});
+        auto broadcast = make_shared<v3::Broadcast>(input_data, updated_shape);
 
-    auto scatter_nd = make_shared<v3::ScatterNDUpdate>(broadcast, input_indices, updates);
-    set_node_name(node.get_name(), scatter_nd);
+        auto scatter_nd = make_shared<v3::ScatterNDUpdate>(broadcast, input_indices, updates);
+        set_node_name(node.get_name(), scatter_nd);
 
-    if (complex_type_inputs) {
         auto complex_scatter_nd =
             make_shared<ComplexTypeMark>(scatter_nd, complex_type_mark_updates->get_complex_part_type());
         return {complex_scatter_nd};
     }
 
+    auto input_data = create_same_type_const<int32_t>(updates, vector<int32_t>{0}, Shape{1});
+    auto broadcast = make_shared<v3::Broadcast>(input_data, shape);
+    auto scatter_nd = make_shared<v3::ScatterNDUpdate>(broadcast, input_indices, updates);
+    set_node_name(node.get_name(), scatter_nd);
     return {scatter_nd};
 }
 }  // namespace op
