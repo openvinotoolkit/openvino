9613e80b975965c0460e5e85d1e191169b0ae8e2
Aleksandr Voron <aleksandr.voron@intel.com>
2024-03-25 11:22:00 +0100

    [CPU][ARM] Enable both f16 and f32 kernels for aarch64 and introduce runtime f16 support check (#22992)
    Inherited from https://github.com/openvinotoolkit/openvino/pull/22437
    
    ---------
    
    Co-authored-by: Ilya Lavrenov <ilya.lavrenov@intel.com>

--- src/plugins/intel_cpu/tests/functional/custom/behavior/ov_plugin/properties.cpp
+++ src/plugins/intel_cpu/tests/functional/custom/behavior/ov_plugin/properties.cpp
@@ -5,6 +5,7 @@
 #include <gmock/gmock-matchers.h>
 #include <gtest/gtest.h>
 
+#include "utils/precision_support.h"
 #include "utils/properties_test.hpp"
 #include "common_test_utils/test_assertions.hpp"
 #include "openvino/runtime/properties.hpp"
@@ -208,8 +209,8 @@ TEST_F(OVClassConfigTestCPU, smoke_PluginSetConfigAffinityCore) {
     ASSERT_EQ(false, value);
 }
 
-#if defined(OV_CPU_ARM_ENABLE_FP16)
-    const auto expected_precision_for_performance_mode = ov::element::f16;
+#if defined(OPENVINO_ARCH_ARM) || defined(OPENVINO_ARCH_ARM64)
+    const auto expected_precision_for_performance_mode = ov::intel_cpu::hasHardwareSupport(ov::element::f16) ? ov::element::f16 : ov::element::f32;
 #else
     const auto expected_precision_for_performance_mode = ov::with_cpu_x86_bfloat16() ? ov::element::bf16 : ov::element::f32;
 #endif
