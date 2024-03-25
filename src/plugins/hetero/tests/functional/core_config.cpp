2595b52bd2174343475aa042cd37e1f30f7a326f
Zhai, Xuejun <xuejun.zhai@intel.com>
2024-03-25 10:49:42 +0800

    Move OVHeteroSyntheticTest test from CPU to HERTRO
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>

---
+++ src/plugins/hetero/tests/functional/core_config.cpp
@@ -0,0 +1,13 @@
+// Copyright (C) 2018-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "shared_test_classes/base/ov_subgraph.hpp"
+
+namespace ov {
+namespace test {
+
+void core_configuration(ov::test::SubgraphBaseTest* test) {}
+
+}  // namespace test
+}  // namespace ov
