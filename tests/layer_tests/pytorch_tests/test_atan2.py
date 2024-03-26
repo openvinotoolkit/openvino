30f642cb0a3af5eb163503584c6fdc3237bbb05c
rghvsh <116428320+rghvsh@users.noreply.github.com>
2024-03-26 16:50:58 +0530

    Update test_atan2.py
    add different types for x and y

--- tests/layer_tests/pytorch_tests/test_atan2.py
+++ tests/layer_tests/pytorch_tests/test_atan2.py
@@ -9,16 +9,17 @@ import numpy as np
 from pytorch_layer_test_class import PytorchLayerTest
 
 class TestAtan2(PytorchLayerTest):
-    def _prepare_input(self, y, x, dtype):
-        inputs = [np.array(y).astype(dtype), np.array(x).astype(dtype)]
+    def _prepare_input(self, y, x, dtype1=None, dtype2=None):
+        inputs = [np.array(y).astype(dtype1), np.array(x).astype(dtype2)]
         return inputs
 
-    def create_model(self, dtype=None, use_out=False):
+    def create_model(self, dtype1=None, dtype2=None use_out=False):
         dtype_map = {
             "float32": torch.float32,
             "float64": torch.float64,
             "int64": torch.int64,
             "int32": torch.int32,
+            "int16": torch.int16,
             "uint8": torch.uint8,
             "int8": torch.int8,
             }
@@ -38,10 +39,11 @@ class TestAtan2(PytorchLayerTest):
             def forward(self, y, x):
                 return torch.atan2(input = y, other = x)
         
-        dtype = dtype_map.get(dtype)
+        dtype1 = dtype_map.get(dtype1)
+        dtype2 = dtype_map.get(dtype2)
 
         if use_out:
-            model_class = aten_atan2_out(dtype)
+            model_class = aten_atan2_out(dtype1)
         else:
             model_class = aten_atan2()
                 
@@ -52,12 +54,12 @@ class TestAtan2(PytorchLayerTest):
         
     @pytest.mark.nightly
     @pytest.mark.precommit
-    @pytest.mark.parametrize("dtype", [None, "float32", "float64", "int32", "int64", "int8", "uint8"])
+    @pytest.mark.parametrize("dtype1, dtype2", [(None, None), ("float32", "int32"), ("float64", "float64"), ("int32", "float64"), ("int64", "int16"), ("int8", "int8"), ("uint8", "uint8")])
     @pytest.mark.parametrize(
         "y, x", [(0, 1.5), (0, 0), (1.25, -5), (1, 10), (-1, -5.5), (-1, -5), (1.25, -5.5), (1.9, 2.9), [10, 9.9]]
     )
     @pytest.mark.parametrize("use_out", [False, True])
-    def test_atan2_with_out(self, dtype, use_out, y, x, ie_device, precision, ir_version):
+    def test_atan2_with_out(self, dtype1, dtype2, use_out, y, x, ie_device, precision, ir_version):
         self._test(
             *self.create_model(dtype=dtype, use_out=use_out),
             ie_device,
