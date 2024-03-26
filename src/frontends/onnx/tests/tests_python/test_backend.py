c719c036604fb6be36abee1087e037a62d4e0ab4
LucaTamSapienza <tutordimatematica.ing@gmail.com>
2024-03-26 22:03:55 +0100

    removed OnnxBackendNodeModelTest.test_reduce_l2_empty_set_cpu

--- src/frontends/onnx/tests/tests_python/test_backend.py
+++ src/frontends/onnx/tests/tests_python/test_backend.py
@@ -701,7 +701,6 @@ tests_expected_to_fail = [
     (
         xfail_issue_125493,
         "OnnxBackendNodeModelTest.test_reduce_l1_empty_set_cpu",
-        "OnnxBackendNodeModelTest.test_reduce_l2_empty_set_cpu",
         "OnnxBackendNodeModelTest.test_reduce_log_sum_empty_set_cpu",
         "OnnxBackendNodeModelTest.test_reduce_log_sum_exp_empty_set_cpu",
         "OnnxBackendNodeModelTest.test_reduce_min_empty_set_cpu",
