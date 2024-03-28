139eada879d081fcba8cd5f8dbf115f5386efaba
Evgeniia Nugmanova <evgeniia.nugmanova@intel.com>
2024-03-28 13:32:27 +0400

    Broader equivalence application for two Symbols

--- src/core/src/symbol.cpp
+++ src/core/src/symbol.cpp
@@ -22,10 +22,10 @@ bool ov::symbol::are_equal(const std::shared_ptr<Symbol>& lhs, const std::shared
 void ov::symbol::set_equal(const std::shared_ptr<Symbol>& lhs, const std::shared_ptr<Symbol>& rhs) {
     if (lhs == nullptr || rhs == nullptr)
         return;
-    auto lhs_root = ov::symbol::ancestor_of(lhs), rhs_root = ov::symbol::ancestor_of(rhs);
+    std::shared_ptr<ov::Symbol> lhs_root = ov::symbol::ancestor_of(lhs), rhs_root = ov::symbol::ancestor_of(rhs);
     if (lhs_root.get() == rhs_root.get())
         return;  // already are equal
-    lhs->m_parent = rhs;
+    lhs_root->m_parent = rhs_root;
 }
 
 std::shared_ptr<ov::Symbol> ov::Symbol::parent() {
