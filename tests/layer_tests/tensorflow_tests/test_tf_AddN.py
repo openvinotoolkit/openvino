d9f59b1955a8d0e4894626ec9e6f1652de3be4fa
Roman Kazantsev <roman.kazantsev@intel.com>
2024-03-28 18:53:25 +0400

    Update tests/layer_tests/tensorflow_tests/test_tf_AddN.py

--- tests/layer_tests/tensorflow_tests/test_tf_AddN.py
+++ tests/layer_tests/tensorflow_tests/test_tf_AddN.py
@@ -96,7 +96,7 @@ class TestComplexAddN(CommonTFLayerTest):
     ]
 
     @pytest.mark.parametrize("params", test_data)
-    @pytest.mark.precommit_tf_fe
+    @pytest.mark.precommit
     @pytest.mark.nightly
     def test_complex_addn(self, params, ie_device, precision, ir_version, temp_dir,
                           use_legacy_frontend):
