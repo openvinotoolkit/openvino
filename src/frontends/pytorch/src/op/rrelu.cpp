e34ff86d14aae4a57bf3013c27d40410508ec660
Payasam Rushikeswar Reddy <126307570+Increshi@users.noreply.github.com>
2024-03-29 19:10:44 +0530

    Update rrelu.cpp
    RRELu operation is completed

--- src/frontends/pytorch/src/op/rrelu.cpp
+++ src/frontends/pytorch/src/op/rrelu.cpp
@@ -14,16 +14,33 @@ namespace op {
 using namespace ov::op;
 
 OutputVector translate_rrelu_fx(const NodeContext& context) {
-    num_inputs_check(context, 1, 3);
-    auto x = context.get_input(0);
-    //float default_negative_slope = 1e-2f;
-    Output<Node> negative_slope = ov::op::v0::Constant::create(element::f32, Shape{1}, {default_negative_slope});
-    if (context.get_input_size() == 1) {
-        negative_slope = context.mark_node(std::make_shared<ov::op::v1::ConvertLike>(negative_slope, x));
-    } else {
-        negative_slope = context.get_input(1);
-    }
-    return {context.mark_node(std::make_shared<v0::PRelu>(x, negative_slope))};
+     num_inputs_check(context, 1, 3);
+     auto x = context.get_input(0);
+     float default_lower = 1/8.0;
+     float default_upper=1/3.0;
+     Output<Node> lower = ov::op::v0::Constant::create(element::f32, Shape{1}, {default_lower});
+     Output<Node> upper = ov::op::v0::Constant::create(element::f32, Shape{1}, {default_upper});
+     if (context.get_input_size() == 1) {
+        lower = context.mark_node(std::make_shared<ov::op::v1::ConvertLike>(lower, x));
+        upper = context.mark_node(std::make_shared<ov::op::v1::ConvertLike>(upper, x));
+         
+     } else {
+        lower = context.get_input(1);
+        upper = context.get_input(2);
+     }
+    Output<Node> lower_plus_upper = std::make_shared<ov::op::v1::Add>(lower, upper);
+    Output<Node> average = std::make_shared<ov::op::v1::Divide>(lower_plus_upper, ov::op::v0::Constant::create(element::f32, Shape{1}, {2.0f}));
+
+    average = context.mark_node(average);
+
+    return {context.mark_node(std::make_shared<v0::PRelu>(x, average))};
+    
+     
+     
+     
+     
+    
+     
 };
 
 }  // namespace op
