0335341b7ca24b4b03a2f6f01dec6d39d49d34b8
panhaiqi <haiqi.pan@intel.com>
2024-04-01 15:38:45 +0800

    change shape size

--- src/bindings/python/tests/test_graph/test_preprocess.py
+++ src/bindings/python/tests/test_graph/test_preprocess.py
@@ -730,28 +730,11 @@ def test_graph_set_layout_by_layout_class_thow_exception():
     assert "Layout name is invalid" in str(e.value)
 
 def test_pad_vector_constant_layout():
-    shape = [1, 3, 200, 200]
-    parameter_a = ops.parameter(shape, dtype=np.float32, name="A")
+    shape = [1, 3, 224, 224]
+    parameter_a = ops.parameter(shape, dtype=np.float32, name="RGB_input")
     model = parameter_a
     model = Model(model, [parameter_a], "TestModel")
     ppp = PrePostProcessor(model)
-    ppp.input().tensor().set_shape({1, 3, 199, 199})
+    ppp.input().tensor().set_shape({1, 3, 223, 223})
     ppp.input().preprocess().pad({0, 0, 0, 0}, {0, 0, 1, 1}, 0, PaddingMode.CONSTANT)
     assert ppp.build()
-
-
-def test_pad_vector_out_of_range():
-    shape = [1, 3, 5, 5]
-    parameter_a = ops.parameter(shape, dtype=np.float32, name="A")
-    model = parameter_a
-    model = Model(model, [parameter_a], "TestModel")
-    ppp = PrePostProcessor(model)
-    assert not ppp.input().preprocess().pad({0, 0, -2, 0}, {0, 0, -4, 1}, 0, PaddingMode.CONSTANT)
-
-def test_pad_vector_dim_mismatch():
-    shape = [1, 3, 5, 5]
-    parameter_a = ops.parameter(shape, dtype=np.float32, name="A")
-    model = parameter_a
-    model = Model(model, [parameter_a], "TestModel")
-    ppp = PrePostProcessor(model)
-    assert not ppp.input().preprocess().pad({0, 0, 2, 0, 1}, {0, 0, 4, 1, 1}, 0, PaddingMode.CONSTANT)
