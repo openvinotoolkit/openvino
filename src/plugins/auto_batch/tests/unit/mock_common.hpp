71894be7bc7d5609385e6b29cc3262c440679cbb
Nikolai Shchegolev <nikolay.shchegolev@intel.com>
2024-04-01 10:06:20 +0400

    Fixes as per comments

--- src/plugins/auto_batch/tests/unit/mock_common.hpp
+++ src/plugins/auto_batch/tests/unit/mock_common.hpp
@@ -44,14 +44,6 @@ public:
                 import_model,
                 (std::istream&, const ov::SoPtr<ov::IRemoteContext>&, const ov::AnyMap&),
                 (const, override));
-    MOCK_METHOD(std::shared_ptr<ov::ICompiledModel>,
-                import_model,
-                (const ov::Any&, const ov::AnyMap&),
-                (const, override));
-    MOCK_METHOD(std::shared_ptr<ov::ICompiledModel>,
-                import_model,
-                (const ov::Any&, const ov::SoPtr<ov::IRemoteContext>&, const ov::AnyMap&),
-                (const, override));
     MOCK_METHOD(ov::SupportedOpsMap,
                 query_model,
                 (const std::shared_ptr<const ov::Model>&, const ov::AnyMap&),
