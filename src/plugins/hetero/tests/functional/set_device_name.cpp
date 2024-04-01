1a998f9d81e08c809fb2be03df65c228cfa65c08
Xuejun Zhai <xuejun.zhai@intel.com>
2024-04-01 09:27:51 +0800

    Update src/plugins/hetero/tests/functional/set_device_name.cpp
    Co-authored-by: Pawel Raasz <pawel.raasz@intel.com>

--- src/plugins/hetero/tests/functional/set_device_name.cpp
+++ src/plugins/hetero/tests/functional/set_device_name.cpp
@@ -9,9 +9,7 @@
 namespace ov {
 namespace test {
 void set_device_suffix(const std::string& suffix) {
-    if (!suffix.empty()) {
-        throw std::runtime_error("The suffix can't be used for TEMPLATE device!");
-    }
+    OPENVINO_THROW(suffix.empty(), "The suffix can't be used for TEMPLATE device!");
 }
 }  // namespace test
 }  // namespace ov
