1a66e72f45a775d55a17c89dd8995989abb1f60b
Georgy Krivoruchko <georgy.krivoruchko@intel.com>
2024-04-02 13:19:39 +0400

    Update reduce.hpp: code style

--- src/frontends/onnx/frontend/src/op/reduce.hpp
+++ src/frontends/onnx/frontend/src/op/reduce.hpp
@@ -67,11 +67,9 @@ ov::OutputVector reduce_sum_square(const ov::frontend::onnx::Node& node);
 namespace set_13 {
 ov::OutputVector reduce_sum_square(const ov::frontend::onnx::Node& node);
 }  // namespace set_13
-
 namespace set_18 {
 ov::OutputVector reduce_sum_square(const ov::frontend::onnx::Node& node);
 }  // namespace set_18
-
 
 }  // namespace op
 }  // namespace onnx
