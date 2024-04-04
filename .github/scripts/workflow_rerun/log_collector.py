e9f03a37923b1b2400af1fd5677366706d40f113
Andrei Kashchikhin <andrey.kashchikhin@intel.com>
2024-04-04 11:17:47 +0100

    add rerunner

---
+++ .github/scripts/workflow_rerun/log_collector.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+import requests
+from github.WorkflowRun import WorkflowRun
+from workflow_rerun.constants import GITHUB_TOKEN, LOGGER
+
+
+def collect_logs_for_run(run: WorkflowRun,
+                         log_archive_path: Path) -> Path:
+    """
+    Collects log archive for a pipeline
+    """
+    with open(log_archive_path, 'wb') as log_archive:
+        LOGGER.info(f'STARTED LOG COLLECTION FOR {run.id} IN {log_archive_path}')
+        # PyGitHub does not expose the "/repos/{owner}/{repo}/actions/runs/{run_id}/logs" endpoint so we have to use requests
+        log_archive.write(requests.get(run.logs_url, headers={'Authorization': f'Bearer {GITHUB_TOKEN}'}).content)
+        LOGGER.info(f'COLLECTED LOGS FOR {run.id} IN {log_archive_path}')
+
+    return log_archive_path
