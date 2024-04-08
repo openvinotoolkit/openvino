9dfcc3e54f3f66ab11f42e80aa030595a6f3e4b6
Roman Kazantsev <roman.kazantsev@intel.com>
2024-04-08 08:46:24 +0400

    Apply suggestions from code review

--- src/frontends/tensorflow_common/src/op/matrix_inverse.cpp
+++ src/frontends/tensorflow_common/src/op/matrix_inverse.cpp
@@ -14,20 +14,14 @@ namespace frontend {
 namespace tensorflow {
 namespace op {
 OutputVector translate_matrix_inverse_op(const NodeContext& node) {
-    /*
-    For matrix inverse translation, the goal is to convert the TensorFlow input tensor
-    into an OpenVINO OutputVector that the Inverse operation can accept.
-    */
-    // a function that performs standard or default checks on a specific operation within the OpenVINO pipeline
-    default_op_checks(node, 1, {"MatrixInverse"}); // copied from issue #22964
-    // Retrieve the input tensor
+    default_op_checks(node, 1, {"MatrixInverse"});
+    // retrieve the input tensor
     auto input = node.get_input(0);
 
-    // Handle optional 'adjoint' attribute (default is False)
+    // handle optional 'adjoint' attribute (default is false)
     bool adjoint = node.get_attribute<bool>("adjoint", false);
 
 
-    // Create the OpenVINO equivalent operation (Inverse from opset-14)
     auto inverse_op = make_shared<ov::op::v14::Inverse>(input, false);
     set_node_name(node.get_name(), inverse_op);
 
