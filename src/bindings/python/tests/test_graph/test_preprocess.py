1a0d577303713c1b3b7d2210e0467e02b5ea9c94
panhaiqi <haiqi.pan@intel.com>
2024-04-08 17:52:15 +0800

    check model.get_output_shape(0)

--- src/bindings/python/tests/test_graph/test_preprocess.py
+++ src/bindings/python/tests/test_graph/test_preprocess.py
@@ -740,6 +740,7 @@ def test_pad_vector_constant_layout():
     ppp.input().tensor().set_shape([1, 3, 199, 199])
     ppp.input().preprocess().pad([0, 0, 0, 0], [0, 0, 1, 1], 0, PaddingMode.CONSTANT)
     assert ppp.build()
+    assert list(model.get_output_shape(0)) == shape
 
 
 def test_pad_vector_out_of_range():
@@ -749,6 +750,7 @@ def test_pad_vector_out_of_range():
     model = Model(model, [parameter_a], "TestModel")
     ppp = PrePostProcessor(model)
     assert not ppp.input().preprocess().pad([0, 0, -2, 0], [0, 0, -4, 1], 0, PaddingMode.CONSTANT)
+    assert list(model.get_output_shape(0)) == shape
 
 
 def test_pad_vector_dim_mismatch():
@@ -758,3 +760,4 @@ def test_pad_vector_dim_mismatch():
     model = Model(model, [parameter_a], "TestModel")
     ppp = PrePostProcessor(model)
     assert not ppp.input().preprocess().pad([0, 0, 2, 0, 1], [0, 0, 4, 1, 1], 0, PaddingMode.CONSTANT)
+    assert list(model.get_output_shape(0)) == shape
