09f4d2ddc5092d076c7622d6be23f0d883173dc6
panhaiqi <haiqi.pan@intel.com>
2024-04-08 15:03:09 +0800

    extend to optional

--- src/tests/functional/plugin/shared/src/behavior/ov_plugin/properties_tests.cpp
+++ src/tests/functional/plugin/shared/src/behavior/ov_plugin/properties_tests.cpp
@@ -218,6 +218,8 @@ std::vector<ov::AnyMap> OVPropertiesTestsWithCompileModelProps::getROMandatoryPr
         res.push_back({{ov::PropertyName(ov::device::type.name(), ov::device::type.mutability), nullptr}});
         res.push_back({{ov::PropertyName(ov::execution_devices.name(), ov::execution_devices.mutability), nullptr}});
         res.push_back({{ov::PropertyName(ov::available_devices.name(), ov::available_devices.mutability), nullptr}});
+        res.push_back(
+            {{ov::PropertyName(ov::hint::execution_mode.name(), ov::hint::execution_mode.mutability), nullptr}});
     }
 
     return res;
