5cddadbe1237cbeae9e30ad7ce1d729e56b893dd
LucaTamSapienza <tutordimatematica.ing@gmail.com>
2024-04-09 19:23:53 +0200

    revert

--- src/frontends/tensorflow_common/src/op/round.cpp
+++ src/frontends/tensorflow_common/src/op/round.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "openvino/op/round.hpp"
-#include "helper_ops/complex_type_mark.hpp"
+
 #include "common_op_table.hpp"
 
 using namespace std;
@@ -15,26 +15,12 @@ namespace tensorflow {
 namespace op {
 
 OutputVector translate_round_op(const NodeContext& node) {
-    default_op_checks(node, 1, {"Round", "ROUND"}, true);
+    default_op_checks(node, 1, {"Round", "ROUND"});
 
     auto input = node.get_input(0);
-    auto complex_type_mark = as_type_ptr<ComplexTypeMark>(input.get_node_shared_ptr());
-
     // using default round mode "half_to_even" in openvino,
     // as TF has only that mode
     auto round_mode = v5::Round::RoundMode::HALF_TO_EVEN;
-
-    if (complex_type_mark) {
-        // Store the complex part type for the output that will be a complex type tensor
-        element::Type complex_part_type = complex_type_mark->get_complex_part_type();
-
-        input = complex_type_mark->input_value(0);
-        auto round = make_shared<v5::Round>(input, round_mode);
-        set_node_name(node.get_name(), round);
-        auto complex_round = make_shared<ComplexTypeMark>(round, complex_part_type);
-        return {complex_round->output(0)};
-    }
-
     auto res = make_shared<v5::Round>(input, round_mode);
     set_node_name(node.get_name(), res);
     return res->outputs();
