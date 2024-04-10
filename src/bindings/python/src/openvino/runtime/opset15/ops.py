731876abc3d8b1e88ade7f599844aefc8fdf9247
mitruska <katarzyna.mitrus@intel.com>
2024-04-10 16:53:22 +0200

    ops.py cleanup

--- src/bindings/python/src/openvino/runtime/opset15/ops.py
+++ src/bindings/python/src/openvino/runtime/opset15/ops.py
@@ -2,18 +2,9 @@
 # Copyright (C) 2018-2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-"""Factory functions for ops added to openvino opset14."""
-from functools import partial
-
-from typing import Union, Optional, List
-
-from openvino.runtime import Node, Type
+"""Factory functions for ops added to openvino opset15."""
 from openvino.runtime.opset_utils import _get_node_factory
-from openvino.runtime.utils.types import TensorShape
-from openvino.runtime.utils.decorators import nameable_op
-from openvino.runtime.utils.types import NodeInput, as_node, as_nodes
 
-_get_node_factory_opset14 = partial(_get_node_factory, "opset14")
-
+_get_node_factory_opset15 = partial(_get_node_factory, "opset15")
 
 # -------------------------------------------- ops ------------------------------------------------
