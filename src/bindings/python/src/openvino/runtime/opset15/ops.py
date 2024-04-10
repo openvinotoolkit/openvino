74f7b4e464d2b9e3fdabc535266793e54665993e
mitruska <katarzyna.mitrus@intel.com>
2024-04-10 17:38:55 +0200

    Imprt partial from functools

--- src/bindings/python/src/openvino/runtime/opset15/ops.py
+++ src/bindings/python/src/openvino/runtime/opset15/ops.py
@@ -3,6 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
 """Factory functions for ops added to openvino opset15."""
+from functools import partial
+
 from openvino.runtime.opset_utils import _get_node_factory
 
 _get_node_factory_opset15 = partial(_get_node_factory, "opset15")
