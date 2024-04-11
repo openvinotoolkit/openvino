af6113a0c823969017ff758199cc3cf9ac90ff93
Maxim Vafin <maxim.vafin@intel.com>
2024-04-11 14:46:07 +0200

    Enable topk tests

--- tests/layer_tests/pytorch_tests/test_topk.py
+++ tests/layer_tests/pytorch_tests/test_topk.py
@@ -62,7 +62,6 @@ class TestTopK(PytorchLayerTest):
     @pytest.mark.nightly
     @pytest.mark.precommit
     @pytest.mark.precommit_fx_backend
-    @pytest.mark.skipif(os.getenv("GITHUB_ACTIONS") == 'true', reason="Ticket - 115085")
     def test_topK(self, input_shape, k, dim, largest, sort, ie_device, precision, ir_version):
         self.input_tensor = np.random.randn(*input_shape).astype(np.float32)
         self._test(*self.create_model(k, dim, largest, sort), ie_device, precision, ir_version)
