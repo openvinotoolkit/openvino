d4acbf06d27bb3b998ab4212915adc4d6e08503e
andsonder <changlu@keter.top>
2024-04-11 13:14:04 +0000

    add paddle rsqrt

---
+++ src/frontends/paddle/src/op/rsqrt.cpp
@@ -0,0 +1,29 @@
+// Copyright (C) 2018-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "default_opset.hpp"
+#include "openvino/frontend/paddle/node_context.hpp"
+
+namespace ov {
+namespace frontend {
+namespace paddle {
+namespace op {
+
+NamedOutputs rsqrt(const NodeContext& node) {
+    auto data = node.get_input("X");
+
+    // Calculate the square root of the input
+    auto sqrt = std::make_shared<default_opset::Power>(data, default_opset::Constant::create(data.get_element_type(), Shape{}, {0.5}));
+    
+    // Take the reciprocal of the square root
+    auto rsqrt = std::make_shared<default_opset::Power>(sqrt, default_opset::Constant::create(data.get_element_type(), Shape{}, {-1.0}));
+
+    return node.default_single_output_mapping({rsqrt}, {"Out"});
+}
+
+}  // namespace op
+}  // namespace paddle
+}  // namespace frontend
+}  // namespace ov
+
