17f5838d3c8a0bef6043f47dcb788b8f7bbfc216
Roman Kazantsev <roman.kazantsev@intel.com>
2024-04-12 14:56:27 +0400

    Update tests/layer_tests/tensorflow_tests/test_tf_ReverseSequence.py

--- tests/layer_tests/tensorflow_tests/test_tf_ReverseSequence.py
+++ tests/layer_tests/tensorflow_tests/test_tf_ReverseSequence.py
@@ -98,7 +98,7 @@ class TestReverseSequenceComplex(CommonTFLayerTest):
     ]
 
     @pytest.mark.parametrize("params", test_data_basic)
-    @pytest.mark.precommit_tf_fe
+    @pytest.mark.precommit
     @pytest.mark.nightly
     def test_reverse_sequence_complex_basic(self, params, ie_device, precision, ir_version, temp_dir,
                                     use_legacy_frontend):
