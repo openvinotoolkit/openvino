ff918677466aaa9efd8d39a59f48a74c3d121df5
Evgeny Kotov <evgeny.kotov@intel.com>
2024-04-12 10:19:18 +0200

    fix template_pattern_transformation namespaces

--- docs/snippets/template_pattern_transformation.cpp
+++ docs/snippets/template_pattern_transformation.cpp
@@ -7,10 +7,14 @@
 #include "openvino/cc/pass/itt.hpp"
 #include "openvino/core/graph_util.hpp"
 #include "openvino/core/rt_info.hpp"
-#include "openvino/opsets/opset3.hpp"
 #include "openvino/pass/manager.hpp"
 #include "openvino/pass/pattern/op/wrap_type.hpp"
 #include "template_model_transformation.hpp"
+#include "openvino/op/divide.hpp"
+#include "openvino/op/power.hpp"
+#include "openvino/op/constant.hpp"
+#include "openvino/op/multiply.hpp"
+#include "openvino/op/relu.hpp"
 
 // ! [graph_rewrite:template_transformation_cpp]
 // template_pattern_transformation.cpp
@@ -19,21 +23,21 @@ ov::pass::DecomposeDivideMatcher::DecomposeDivideMatcher() {
     // Pattern example
     auto input0 = pattern::any_input();
     auto input1 = pattern::any_input();
-    auto div = std::make_shared<ov::opset3::Divide>(input0, input1);
+    auto div = std::make_shared<ov::op::v1::Divide>(input0, input1);
 
     ov::matcher_pass_callback callback = [](pattern::Matcher& m) {
-        auto div = std::dynamic_pointer_cast<ov::opset3::Divide>(m.get_match_root());
+        auto div = std::dynamic_pointer_cast<ov::op::v1::Divide>(m.get_match_root());
         // We can not apply this transformation in case with integer input data type
         if (!div || div->input(0).get_element_type().is_integral()) {
             return false;
         }
 
         // Decompose Divide into Multiply with Power operations
-        auto pow = std::make_shared<ov::opset3::Power>(
+        auto pow = std::make_shared<ov::op::v1::Power>(
             div->input_value(1),
-            opset3::Constant::create(div->get_input_element_type(1), Shape{1}, {-1}));
+            ov::op::v0::Constant::create(div->get_input_element_type(1), Shape{1}, {-1}));
 
-        auto mul = std::make_shared<ov::opset3::Multiply>(div->input_value(0), pow);
+        auto mul = std::make_shared<ov::op::v1::Multiply>(div->input_value(0), pow);
 
         // Save original name to last operation in replacement sub-graph
         mul->set_friendly_name(div->get_friendly_name());
@@ -58,8 +62,8 @@ ov::pass::DecomposeDivideMatcher::DecomposeDivideMatcher() {
 // ! [matcher_pass:relu_fusion]
 ov::pass::ReluReluFusionMatcher::ReluReluFusionMatcher() {
     MATCHER_SCOPE(ReluReluFusionMatcher);
-    auto m_relu1 = ov::pass::pattern::wrap_type<ov::opset3::Relu>(pattern::consumers_count(1));
-    auto m_relu2 = ov::pass::pattern::wrap_type<ov::opset3::Relu>({m_relu1});
+    auto m_relu1 = ov::pass::pattern::wrap_type<ov::op::v0::Relu>(pattern::consumers_count(1));
+    auto m_relu2 = ov::pass::pattern::wrap_type<ov::op::v0::Relu>({m_relu1});
 
     ov::matcher_pass_callback callback = [=](pattern::Matcher& m) {
         // Map that helps to connect labels with matched outputs
@@ -67,7 +71,7 @@ ov::pass::ReluReluFusionMatcher::ReluReluFusionMatcher() {
 
         // Create new Relu operation and add register it for additional execution
         auto new_relu =
-            register_new_node<ov::opset3::Relu>(node_to_output.at(m_relu1).get_node_shared_ptr()->input_value(0));
+            register_new_node<ov::op::v0::Relu>(node_to_output.at(m_relu1).get_node_shared_ptr()->input_value(0));
 
         // Copy runtime info attributes to newly created operation
         ov::copy_runtime_info(m.get_matched_nodes(), new_relu);
