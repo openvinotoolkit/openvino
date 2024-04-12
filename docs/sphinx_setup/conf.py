3173681438b1844cd7021618edc926de03e528e6
Andrzej Kopytko <andrzejx.kopytko@intel.com>
2024-04-12 13:29:21 +0200

    Docs carousel upgrade for 2024 1 (#24004)
    port to https://github.com/openvinotoolkit/openvino/pull/24000

--- docs/sphinx_setup/conf.py
+++ docs/sphinx_setup/conf.py
@@ -218,5 +218,4 @@ def setup(app):
     app.add_js_file('js/custom.js')
     app.add_js_file('js/graphs.js')
     app.add_js_file('js/newsletter.js')
-    app.add_js_file('js/graphs_ov_tf.js')
     app.add_js_file('js/open_sidebar.js')
