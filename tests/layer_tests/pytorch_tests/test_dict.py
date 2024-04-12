1a0bb929f07d23ece04ea9285f2a4fea0e7036ab
Maxim Vafin <maxim.vafin@intel.com>
2024-04-12 13:18:14 +0200

    Disable dict_param

--- tests/layer_tests/pytorch_tests/test_dict.py
+++ tests/layer_tests/pytorch_tests/test_dict.py
@@ -54,6 +54,7 @@ class TestDictParam(PytorchLayerTest):
 
     @pytest.mark.nightly
     @pytest.mark.precommit
+    @pytest.mark.skipif(os.getenv("GITHUB_ACTIONS") == 'true', reason="Ticket - 115085")
     def test_dict_param_convert_model(self, ie_device, precision, ir_version):
         self._test(aten_dict_with_types(), None, "aten::__getitem__", ie_device, precision,
                    ir_version, trace_model=True, use_convert_model=True)
