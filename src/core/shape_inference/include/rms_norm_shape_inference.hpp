3d870b2e09ec307d5e7f36393a7379a4d5bb41b3
Katarzyna Mitrus <katarzyna.mitrus@intel.com>
2024-04-15 10:06:06 +0200

    [Op] Add RMSNorm op core class (#23842)
    ### Details:
     - RMSNorm op core class
    - Registration in the opset and op check (conformance) test will be
    added in the next PRs
    
    Spec PR: 
    - https://github.com/openvinotoolkit/openvino/pull/23569
    
    ### Tickets:
     - 136261

---
+++ src/core/shape_inference/include/rms_norm_shape_inference.hpp
@@ -0,0 +1,65 @@
+// Copyright (C) 2018-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include "openvino/op/rms_norm.hpp"
+#include "utils.hpp"
+
+namespace ov {
+namespace op {
+namespace v14 {
+template <class TShape, class TRShape = result_shape_t<TShape>>
+std::vector<TRShape> shape_infer(const RMSNorm* op,
+                                 const std::vector<TShape>& input_shapes,
+                                 const ITensorAccessor& tensor_accessor = make_tensor_accessor()) {
+    const auto inputs_count = input_shapes.size();
+    const auto has_scale_input = inputs_count == 3;
+    NODE_SHAPE_INFER_CHECK(op, input_shapes, inputs_count == 2 || has_scale_input);
+
+    const auto& data_shape = input_shapes[0];
+    const auto& data_rank = data_shape.rank();
+    const auto& axes_shape = input_shapes[1];
+    const auto& axes_rank = axes_shape.rank();
+
+    NODE_SHAPE_INFER_CHECK(op,
+                           input_shapes,
+                           ov::util::is_rank_compatible_any_of(axes_rank, {0, 1}),
+                           "Axes input must be a scalar or 1D input. Got: ",
+                           axes_shape);
+
+    // Further validation requires data rank to be static
+    if (data_rank.is_dynamic()) {
+        return {data_shape};
+    }
+
+    if (axes_shape.rank().is_static()) {
+        const bool has_axes_compatible = axes_shape.size() == 0 || axes_shape[0].is_dynamic() ||
+                                         cmp::ge(data_rank.get_length(), axes_shape.get_shape()[0]);
+        NODE_SHAPE_INFER_CHECK(op,
+                               input_shapes,
+                               has_axes_compatible,
+                               "Number of the axes can't be higher than the rank of the data shape.");
+    }
+
+    if (has_scale_input) {  // Validate scale input
+        TRShape scale_shape = input_shapes[2];
+        const bool is_scale_shape_broadcastable =
+            TRShape::broadcast_merge_into(scale_shape, data_shape, ov::op::AutoBroadcastType::NUMPY);
+        NODE_SHAPE_INFER_CHECK(op,
+                               input_shapes,
+                               is_scale_shape_broadcastable,
+                               "Scale input shape must be broadcastable to the shape of the data input.");
+    }
+
+    // Axes values validation
+    if (const auto axes_val = ov::op::get_input_const_data_as<TRShape, int64_t>(op, 1, tensor_accessor)) {
+        ov::util::normalize_axes(op, data_rank.get_length(), *axes_val);
+    }
+
+    return {data_shape};
+}
+}  // namespace v14
+}  // namespace op
+}  // namespace ov
---
+++ src/plugins/intel_cpu/tests/unit/shape_inference_test/rms_norm_shape_inference_test.cpp
@@ -0,0 +1,137 @@
+// Copyright (C) 2018-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <gtest/gtest.h>
+
+#include "common_test_utils/test_assertions.hpp"
+#include "utils.hpp"
+
+using namespace ov;
+using namespace ov::intel_cpu;
+using ov::op::v0::Constant;
+using ov::op::v0::Parameter;
+using testing::HasSubstr;
+
+TEST(StaticShapeInferenceTest, RMSNormStaticShapeInferenceTestDefaultCtor) {
+    const auto op = std::make_shared<op::v14::RMSNorm>();
+    const auto data = std::make_shared<Parameter>(element::f16, PartialShape::dynamic());
+    const auto axes = std::make_shared<Parameter>(element::i64, PartialShape::dynamic());
+    const auto scale = std::make_shared<Parameter>(element::f16, PartialShape::dynamic());
+
+    op->set_arguments(ov::OutputVector{data, axes, scale});
+
+    std::vector<StaticShape> static_input_shapes = {StaticShape{2, 3, 8, 6}, StaticShape{1}, StaticShape{1}};
+    int32_t axis_val = -1;
+    const auto const_data = std::unordered_map<size_t, Tensor>{{1, {element::i32, Shape{1}, &axis_val}}};
+    const auto static_output_shapes = shape_inference(op.get(), static_input_shapes, const_data);
+    EXPECT_EQ(static_output_shapes[0], StaticShape({2, 3, 8, 6}));
+}
+
+TEST(StaticShapeInferenceTest, RMSNormStaticShapeInferenceTest2ins) {
+    const auto data = std::make_shared<Parameter>(element::f32, PartialShape::dynamic());
+    const auto axes = std::make_shared<Parameter>(element::i32, PartialShape::dynamic());
+    const auto eps = 1e-5f;
+
+    const auto op = std::make_shared<op::v14::RMSNorm>(data, axes, eps);
+
+    std::vector<StaticShape> static_input_shapes = {StaticShape{2, 3, 8, 6}, StaticShape{1}};
+    int32_t axis_val = -1;
+    const auto const_data = std::unordered_map<size_t, Tensor>{{1, {element::i32, Shape{1}, &axis_val}}};
+    const auto static_output_shapes = shape_inference(op.get(), static_input_shapes, const_data);
+    EXPECT_EQ(static_output_shapes[0], StaticShape({2, 3, 8, 6}));
+}
+
+TEST(StaticShapeInferenceTest, RMSNormStaticShapeInferenceTest3ins) {
+    const auto data = std::make_shared<Parameter>(element::f32, PartialShape::dynamic());
+    const auto axes = std::make_shared<Parameter>(element::i32, PartialShape::dynamic());
+    const auto scale = std::make_shared<Parameter>(element::f32, PartialShape::dynamic());
+    const auto eps = 1e-5f;
+
+    const auto op = std::make_shared<op::v14::RMSNorm>(data, axes, scale, eps);
+
+    std::vector<StaticShape> static_input_shapes = {StaticShape{2, 3, 8, 6}, StaticShape{1}, StaticShape{1}};
+    int32_t axis_val = -1;
+    const auto const_data = std::unordered_map<size_t, Tensor>{{1, {element::i32, Shape{1}, &axis_val}}};
+    const auto static_output_shapes = shape_inference(op.get(), static_input_shapes, const_data);
+    EXPECT_EQ(static_output_shapes[0], StaticShape({2, 3, 8, 6}));
+}
+
+TEST(StaticShapeInferenceTest, RMSNormIncorrectAxisValParam) {
+    const auto data = std::make_shared<Parameter>(element::f32, PartialShape::dynamic());
+    const auto axes = std::make_shared<Parameter>(element::i32, PartialShape::dynamic());
+    const auto eps = 1e-5f;
+
+    const auto op = std::make_shared<op::v14::RMSNorm>(data, axes, eps);
+
+    std::vector<StaticShape> static_input_shapes = {StaticShape{2, 3, 8, 6}, StaticShape{1}};
+    int32_t axis_val = 5;
+    const auto const_data = std::unordered_map<size_t, Tensor>{{1, {element::i32, Shape{1}, &axis_val}}};
+
+    OV_EXPECT_THROW(shape_inference(op.get(), static_input_shapes, const_data),
+                    NodeValidationFailure,
+                    HasSubstr("Parameter axis 5 out of the tensor rank range [-4, 3]"));
+}
+
+TEST(StaticShapeInferenceTest, RMSNormIncorrectAxisValConst) {
+    const auto data = std::make_shared<Parameter>(element::f32, PartialShape::dynamic());
+    const auto axes = std::make_shared<Constant>(element::i32, Shape{}, 5);
+    const auto eps = 1e-5f;
+
+    const auto op = std::make_shared<op::v14::RMSNorm>(data, axes, eps);
+
+    std::vector<StaticShape> static_input_shapes = {StaticShape{2, 3, 8, 6}, StaticShape{}};
+
+    OV_EXPECT_THROW(shape_inference(op.get(), static_input_shapes),
+                    NodeValidationFailure,
+                    HasSubstr("Parameter axis 5 out of the tensor rank range [-4, 3]"));
+}
+
+TEST(StaticShapeInferenceTest, RMSNormIncorrectAxisShapeDim) {
+    const auto data = std::make_shared<Parameter>(element::f32, PartialShape::dynamic());
+    const auto axes = std::make_shared<Parameter>(element::i32, PartialShape::dynamic());
+    const auto eps = 1e-5f;
+
+    const auto op = std::make_shared<op::v14::RMSNorm>(data, axes, eps);
+
+    std::vector<StaticShape> static_input_shapes = {StaticShape{2, 3, 8, 6}, StaticShape{5}};
+    int32_t axis_val = 5;
+    const auto const_data = std::unordered_map<size_t, Tensor>{{1, {element::i32, Shape{1}, &axis_val}}};
+
+    OV_EXPECT_THROW(shape_inference(op.get(), static_input_shapes, const_data),
+                    NodeValidationFailure,
+                    HasSubstr("Number of the axes can't be higher than the rank of the data shape"));
+}
+
+TEST(StaticShapeInferenceTest, RMSNormIncorrectAxisShapeRank) {
+    const auto data = std::make_shared<Parameter>(element::f32, PartialShape::dynamic());
+    const auto axes = std::make_shared<Parameter>(element::i32, PartialShape::dynamic());
+    const auto eps = 1e-5f;
+
+    const auto op = std::make_shared<op::v14::RMSNorm>(data, axes, eps);
+
+    std::vector<StaticShape> static_input_shapes = {StaticShape{2, 3, 8, 6}, StaticShape{1, 5}};
+    int32_t axis_val = 5;
+    const auto const_data = std::unordered_map<size_t, Tensor>{{1, {element::i32, Shape{1}, &axis_val}}};
+
+    OV_EXPECT_THROW(shape_inference(op.get(), static_input_shapes, const_data),
+                    NodeValidationFailure,
+                    HasSubstr("Axes input must be a scalar or 1D input. Got: {1,5}"));
+}
+
+TEST(StaticShapeInferenceTest, RMSNormIncorrectScaleShape) {
+    const auto data = std::make_shared<Parameter>(element::f32, PartialShape::dynamic());
+    const auto axes = std::make_shared<Parameter>(element::i32, PartialShape::dynamic());
+    const auto scale = std::make_shared<Parameter>(element::f32, PartialShape::dynamic());
+    const auto eps = 1e-5f;
+
+    const auto op = std::make_shared<op::v14::RMSNorm>(data, axes, scale, eps);
+
+    std::vector<StaticShape> static_input_shapes = {StaticShape{2, 3, 8, 6}, StaticShape{1}, StaticShape{6, 1}};
+    int32_t axis_val = -1;
+    const auto const_data = std::unordered_map<size_t, Tensor>{{1, {element::i32, Shape{1}, &axis_val}}};
+
+    OV_EXPECT_THROW(shape_inference(op.get(), static_input_shapes, const_data),
+                    NodeValidationFailure,
+                    HasSubstr("Scale input shape must be broadcastable to the shape of the data input"));
+}
