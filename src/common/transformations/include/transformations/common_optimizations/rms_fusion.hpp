63e7c933760d51ab521cd5be619611d11c1ccf7d
Michal Miotk <michal-miotk>
2024-04-16 13:18:56 +0200

    added rms transformations to common space

---
+++ src/common/transformations/include/transformations/common_optimizations/rms_fusion.hpp
@@ -0,0 +1,19 @@
+// Copyright (C) 2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include "openvino/pass/graph_rewrite.hpp"
+
+namespace ov {
+namespace pass {
+
+class RMSFusion : public ov::pass::MatcherPass {
+public:
+    OPENVINO_RTTI("RMSFusion", "0");
+    RMSFusion(uint64_t max_work_group_size);
+};
+
+}   // namespace pass
+}   // namespace ov
