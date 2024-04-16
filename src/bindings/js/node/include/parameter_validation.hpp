d8b388a1244e56524c8521a1c69591a8e1464606
Vishniakov Nikolai <nikolai.vishniakov@intel.com>
2024-04-16 14:49:36 +0200

    Add parameter validation structure

---
+++ src/bindings/js/node/include/parameter_validation.hpp
@@ -0,0 +1,137 @@
+// Copyright (C) 2018-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <napi.h>
+
+const std::string get_type_name(napi_valuetype type) {
+    switch (type)
+    {
+    case napi_undefined:
+        return "Undefined";
+
+    case napi_null:
+        return "Null";
+
+    case napi_boolean:
+        return "Boolean";
+
+    case napi_number:
+        return "Number";
+
+    case napi_string:
+        return "String";
+
+    case napi_symbol:
+        return "Symbol";
+
+    case napi_object:
+        return "Object";
+
+    case napi_function:
+        return "Function";
+
+    case napi_external:
+        return "External";
+
+    case napi_bigint:
+        return "BigInt";
+
+    default:
+        return "Unknown";
+    }
+}
+
+struct Signature {
+    typedef std::function<void(const std::string key, const Napi::Value&)> ValidatorType;
+    std::vector<ValidatorType> attributes_validators;
+
+public:
+    void param(ValidatorType validator) {
+        attributes_validators.push_back(validator);
+    }
+
+    static void boolean(const std::string key, const Napi::Value& value) {
+        return check_type(napi_boolean, key, value);
+    }
+
+    static void number(const std::string key, const Napi::Value& value) {
+        return check_type(napi_number, key, value);
+    }
+
+    static void string(const std::string key, const Napi::Value& value) {
+        return check_type(napi_string, key, value);
+    }
+
+    static void symbol(const std::string key, const Napi::Value& value) {
+        return check_type(napi_symbol, key, value);
+    }
+
+    static void object(const std::string key, const Napi::Value& value) {
+        return check_type(napi_object, key, value);
+    }
+
+    static void function(const std::string key, const Napi::Value& value) {
+        return check_type(napi_function, key, value);
+    }
+
+    static void bigint(const std::string key, const Napi::Value& value) {
+        return check_type(napi_bigint, key, value);
+    }
+
+    static void array(const std::string key, const Napi::Value& value) {
+        if (value.IsArray()) return;
+
+        std::string error_message = Signature::get_error_message(key, "Array", value.Type());
+        throw std::runtime_error(error_message);
+    }
+
+    static void check_type(const napi_valuetype expected_type, const std::string key, const Napi::Value& value) {
+        napi_valuetype real_type = value.Type();
+
+        if (real_type == expected_type) return;
+
+        std::string error_message = Signature::get_error_message(key, expected_type, real_type);
+        throw std::runtime_error(error_message);
+    }
+
+    static std::string get_error_message(std::string key, napi_valuetype expected_type, napi_valuetype real_type) {
+        std::string expected_type_str = get_type_name(expected_type);
+        std::string real_type_str = get_type_name(real_type);
+
+        return get_error_message(key, expected_type_str, real_type_str);
+    }
+
+    static std::string get_error_message(std::string key, std::string expected, napi_valuetype real_type) {
+        std::string real_type_str = get_type_name(real_type);
+
+        return get_error_message(key, expected, real_type_str);
+    }
+
+    static std::string get_error_message(std::string key, std::string expected, std::string real) {
+        return "Argument #" + key + " has type '" + real + "', expected '" + expected + "'";
+    }
+};
+
+std::pair<bool, std::string> validate_args(const Napi::CallbackInfo& info, const std::function<void(Signature&)>& builder) {
+    Signature s = Signature();
+    std::string validation_errors;
+
+    builder(s);
+    size_t index = 0;
+
+    for (const auto& validator : s.attributes_validators) {
+        try {
+            validator(std::to_string(index), info[index]);
+        } catch(std::runtime_error& err) {
+            validation_errors.append(std::string(err.what()) + "\n\t");
+        }
+
+        index++;
+    }
+
+    bool is_errors_empty = validation_errors.empty();
+
+    return std::make_pair(is_errors_empty, validation_errors);
+}
