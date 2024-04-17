1eec2101da91629dabf9a79171b02cfaebf47fb7
awayzjj <zjj51234@foxmail.com>
2024-04-17 08:18:12 +0800

    customize input data generation and exclude tf.half

--- tests/layer_tests/tensorflow_tests/test_tf_Rint.py
+++ tests/layer_tests/tensorflow_tests/test_tf_Rint.py
@@ -1,12 +1,21 @@
 # Copyright (C) 2018-2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+import numpy as np
 import pytest
 import tensorflow as tf
 from common.tf_layer_test_class import CommonTFLayerTest
 
 class TestRint(CommonTFLayerTest):
+    def _prepare_input(self, inputs_info):
+        assert 'input:0' in inputs_info
+        inputs_shape = inputs_info['input:0']
+        inputs_data = {}
+        inputs_data['input:0'] = np.random.rand(*inputs_shape).astype(self.input_type)
+        return inputs_data
+
     def create_tf_rint_net(self, input_shape, input_type):
+        self.input_type = input_type
         tf.compat.v1.reset_default_graph()
         with tf.compat.v1.Session() as sess:
             input = tf.compat.v1.placeholder(input_type, input_shape, 'input')
@@ -19,13 +28,11 @@ class TestRint(CommonTFLayerTest):
         return tf_net, ref_net
 
     @pytest.mark.parametrize("input_shape", [[6], [2, 5, 3], [10, 5, 1, 5]])
-    @pytest.mark.parametrize("input_type", [tf.half, tf.float32, tf.float64])
+    @pytest.mark.parametrize("input_type", [np.float32, np.float64])
     @pytest.mark.precommit
     @pytest.mark.nightly
     def test_rint_basic(self, input_shape, input_type, ie_device, precision,
                         ir_version, temp_dir, use_legacy_frontend):
-        if ie_device == 'CPU' and input_type in [tf.half,]:
-            pytest.skip("CPU error: Requested activation is not supported for tf.half type")
         self._test(*self.create_tf_rint_net(input_shape, input_type),
                 ie_device, precision, ir_version, temp_dir=temp_dir,
                 use_legacy_frontend=use_legacy_frontend)
