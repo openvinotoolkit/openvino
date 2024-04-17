46637c05715cb48ea5b60290ad54c9cca4f1cdb2
Pawel Raasz <pawel.raasz@intel.com>
2024-04-17 11:58:05 +0200

    [core] Add support of types u2 u3 u6 in Constant (#24006)
    ### Details:
     - Add support of new types u2, u3, u6 into Constant.
    
    ### Tickets:
     - [CVS-119224](https://jira.devtools.intel.com/browse/CVS-119224)
    
    ### Related PR:
     - #23810

--- src/core/src/type/element_iterator.cpp
+++ src/core/src/type/element_iterator.cpp
@@ -8,7 +8,7 @@ namespace ov {
 
 namespace element {
 
-size_t get_byte_size(const element::Type& type, const size_t n) {
+size_t get_memory_size(const element::Type& type, const size_t n) {
     auto byte_size = n * type.bitwidth();
     if (element::is_split_bit_type(type)) {
         constexpr size_t storage_unit_size = 24;
