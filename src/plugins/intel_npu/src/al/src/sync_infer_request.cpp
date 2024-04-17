313d780d1902c8c690941887006c355463dc5dc6
David Pava <david.pava@protonmail.com>
2024-04-17 14:52:16 +0300

    [NPU] Adding a check for the input size in zero compiler (#23940)
    ### Details:
    - The compilation of a model without inputs was failing because the
    serialization in zero compiler expected at least one input.
    
    ### Tickets:
     - EISW-107905

--- src/plugins/intel_npu/src/al/src/sync_infer_request.cpp
+++ src/plugins/intel_npu/src/al/src/sync_infer_request.cpp
@@ -16,12 +16,8 @@ SyncInferRequest::SyncInferRequest(const std::shared_ptr<const ICompiledModel>& 
       _metadata(compiledModel->get_network_metadata()) {
     OPENVINO_ASSERT(_compiledModel);
 
-    const std::vector<ov::Output<const ov::Node>>& inputs = get_inputs();
     const std::vector<ov::Output<const ov::Node>>& outputs = get_outputs();
 
-    if (inputs.empty()) {
-        OPENVINO_THROW("Inference request creation: no input found for network " + _metadata.name);
-    }
     if (outputs.empty()) {
         OPENVINO_THROW("Inference request creation: no output found for network " + _metadata.name);
     }
