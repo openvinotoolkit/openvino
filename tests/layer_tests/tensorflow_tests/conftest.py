a0802240b2a70332101f370883e4d6c636d87e93
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2024-04-17 13:39:18 +0200

    renamed hook

--- tests/layer_tests/tensorflow_tests/conftest.py
+++ tests/layer_tests/tensorflow_tests/conftest.py
@@ -34,8 +34,8 @@ def rename_tf_fe_libs(request):
         copy_files_by_pattern(openvino_lib_path, tf_fe_lib_names[0], tf_fe_lib_names[1])
 
 @pytest.fixture(scope="session", autouse=True)
-def openvino_tokenizers(request):
-     # do not import openvino_tokenizers in legacy tests
+def test_openvino_tokenizers(request):
+    # do not import openvino_tokenizers in legacy tests
     if not request.config.getoption('use_legacy_frontend'):
         import_openvino_tokenizers()
 
