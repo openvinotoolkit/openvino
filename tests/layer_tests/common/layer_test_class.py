7cbe9741c4c0e1245eff06f25bc0f8622d0d55f3
Evgeny Kotov <evgeny.kotov@intel.com>
2024-04-23 16:17:35 +0200

    cleanup

--- tests/layer_tests/common/layer_test_class.py
+++ tests/layer_tests/common/layer_test_class.py
@@ -169,7 +169,7 @@ class CommonLayerTest:
         for framework_out_name in framework_res:
             ie_out_name = framework_out_name
             if ie_out_name not in infer_res and len(infer_res) == 1:
-                    ie_res = list(infer_res.values())[0]
+                ie_res = list(infer_res.values())[0]
             else:
                 ie_res = infer_res[ie_out_name]
 
