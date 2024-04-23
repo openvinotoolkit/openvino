9c4f76e003f2f876d1ac9bc7b2d35fc8050112ce
Mateusz <mateusz.mikolajczyk@intel.com>
2024-04-23 10:23:50 +0000

    Add ScatterNDUpdate-15 to PyAPI

--- src/bindings/python/src/openvino/runtime/opset15/ops.py
+++ src/bindings/python/src/openvino/runtime/opset15/ops.py
@@ -4,9 +4,42 @@
 
 """Factory functions for ops added to openvino opset15."""
 from functools import partial
+from typing import Optional
 
+from openvino.runtime import Node, Type
 from openvino.runtime.opset_utils import _get_node_factory
+from openvino.runtime.utils.decorators import nameable_op
+from openvino.runtime.utils.types import NodeInput, as_nodes
 
 _get_node_factory_opset15 = partial(_get_node_factory, "opset15")
 
 # -------------------------------------------- ops ------------------------------------------------
+
+
+@nameable_op
+def scatter_nd_update(
+    data: NodeInput,
+    indices: NodeInput,
+    updates: NodeInput,
+    reduction: Optional[str] = "None",
+    name: Optional[str] = None,
+) -> Node:
+    """Return a node which performs ScatterNDUpdate.
+
+    :param data: Node input representing the tensor to be updated.
+    :param indices: Node input representing the indices at which updates will be applied.
+    :param updates: Node input representing the updates to be applied.
+    :param reduction: The type of operation to perform on the inputs. One of "none", "sum", "sub",
+                      "prod", "min", "max".
+    :param name: Optional name for the output node.
+    :return: New node performing the ScatterNDUpdate.
+    """
+    inputs = as_nodes(data, indices, updates)
+
+    return _get_node_factory_opset15().create(
+        "ScatterNDUpdate",
+        inputs,
+        {
+            "reduction": reduction,
+        },
+    )
