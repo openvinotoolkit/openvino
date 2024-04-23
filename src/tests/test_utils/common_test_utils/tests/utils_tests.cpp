6dde81d7612329dba4403823ec325b0952ff2570
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2024-04-23 13:06:44 +0200

    use fixtures

--- src/tests/test_utils/common_test_utils/tests/utils_tests.cpp
+++ src/tests/test_utils/common_test_utils/tests/utils_tests.cpp
@@ -26,28 +26,29 @@ TEST(UtilsTests, get_directory_return_file_dir) {
     ASSERT_EQ(get_directory("../test/path/my_file.txt"), "../test/path");
 }
 
-TEST(UtilsTests, filter_lines_by_prefix) {
-    auto lines = "abc\nkkb\nabpp\n";
-    auto res = filter_lines_by_prefix(lines, "ab");
-    ASSERT_EQ(res, "abc\nabpp\n");
+class FilterLinesByPrefixTests : public testing::WithParamInterface<std::tuple<std::string, std::string, std::string>> {
+protected:
+    std::string str_origin;
+    std::string prefix;
+    std::string str_expected;
+};
 
-    lines = "abc\nkkb\nkkabpp\n";
-    res = filter_lines_by_prefix(lines, "ab");
-    ASSERT_EQ(res, "abc\n");
+TEST_P(FilterLinesByPrefixTests, ChecksStringFilter) {
+    std::string str_origin = std::get<0>(GetParam());
+    std::string prefix = std::get<1>(GetParam());
+    std::string str_expected = std::get<2>(GetParam());
 
-    lines = "abc\nkkb\nabpp\n";
-    res = filter_lines_by_prefix(lines, "k");
-    ASSERT_EQ(res, "kkb\n");
-
-    lines = "abc\nkkb\nabpp\n";
-    res = filter_lines_by_prefix(lines, "lr");
-    ASSERT_EQ(res, "");
-
-    lines = "";
-    res = filter_lines_by_prefix(lines, "ab");
-    ASSERT_EQ(res, "");
-
-    lines = "\n\n\n";
-    res = filter_lines_by_prefix(lines, "ab");
-    ASSERT_EQ(res, "");
+    ASSERT_EQ(filter_lines_by_prefix(str_origin, prefix), str_expected);
 }
+
+INSTANTIATE_TEST_CASE_P(
+        UtilsTests,
+        FilterLinesByPrefixTests,
+        ::testing::Values(
+                std::make_tuple("abc\nkkb\nabpp\n", "ab", "abc\nabpp\n"),
+                std::make_tuple("abc\nkkb\nkkabpp\n", "ab", "abc\n"),
+                std::make_tuple("abc\nkkb\nabpp\n", "k", "kkb\n"),
+                std::make_tuple("abc\nkkb\nabpp\n", "lr", ""),
+                std::make_tuple("", "ab", ""),
+                std::make_tuple("\n\n\n", "ab", "")));
+
