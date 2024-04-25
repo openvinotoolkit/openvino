56cd9d8fa3b9178c5424bcd23ddde880fc1ad1ee
Xuejun Zhai <xuejun.zhai@intel.com>
2024-04-25 10:33:06 +0800

    Move HETERO/BATCH tests to HETERO/BATCH test binaries (#23823)
    ### Details:
     - Move remaining BATCH/HETERO test cases out of CPU/GPU tests
    
    ### Tickets:
     - *CVS-133977*
    
    ---------
    
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>
    Co-authored-by: Pawel Raasz <pawel.raasz@intel.com>
    Co-authored-by: Chen Peter <peter.chen@intel.com>

--- src/plugins/auto_batch/tests/functional/behavior/ov_plugin/auto_batching_tests.cpp
+++ src/plugins/auto_batch/tests/functional/behavior/ov_plugin/auto_batching_tests.cpp
@@ -13,7 +13,7 @@ const std::vector<bool> get_vs_set{true, false};
 const std::vector<size_t> num_streams{1, 2};
 const std::vector<size_t> num_requests{1, 3, 8, 9, 16, 64};
 const std::vector<size_t> num_batch{1, 4, 8, 16, 32, 64, 128, 256};
-INSTANTIATE_TEST_SUITE_P(smoke_AutoBatching_test,
+INSTANTIATE_TEST_SUITE_P(smoke_TEMPLATE_AutoBatching,
                          AutoBatching_Test,
                          ::testing::Combine(::testing::Values(ov::test::utils::DEVICE_TEMPLATE),
                                             ::testing::ValuesIn(get_vs_set),
@@ -22,9 +22,45 @@ INSTANTIATE_TEST_SUITE_P(smoke_AutoBatching_test,
                                             ::testing::ValuesIn(num_batch)),
                          AutoBatching_Test::getTestCaseName);
 
-INSTANTIATE_TEST_SUITE_P(smoke_AutoBatching_test,
+INSTANTIATE_TEST_SUITE_P(smoke_TEMPLATE_AutoBatching,
                          AutoBatching_Test_DetectionOutput,
                          ::testing::Combine(::testing::Values(ov::test::utils::DEVICE_TEMPLATE),
+                                            ::testing::ValuesIn(get_vs_set),
+                                            ::testing::ValuesIn(num_streams),
+                                            ::testing::ValuesIn(num_requests),
+                                            ::testing::ValuesIn(num_batch)),
+                         AutoBatching_Test_DetectionOutput::getTestCaseName);
+
+INSTANTIATE_TEST_SUITE_P(nightly_CPU_AutoBatching,
+                         AutoBatching_Test,
+                         ::testing::Combine(::testing::Values(ov::test::utils::DEVICE_CPU),
+                                            ::testing::ValuesIn(get_vs_set),
+                                            ::testing::ValuesIn(num_streams),
+                                            ::testing::ValuesIn(num_requests),
+                                            ::testing::ValuesIn(num_batch)),
+                         AutoBatching_Test::getTestCaseName);
+
+INSTANTIATE_TEST_SUITE_P(nightly_CPU_AutoBatching,
+                         AutoBatching_Test_DetectionOutput,
+                         ::testing::Combine(::testing::Values(ov::test::utils::DEVICE_CPU),
+                                            ::testing::ValuesIn(get_vs_set),
+                                            ::testing::ValuesIn(num_streams),
+                                            ::testing::ValuesIn(num_requests),
+                                            ::testing::ValuesIn(num_batch)),
+                         AutoBatching_Test_DetectionOutput::getTestCaseName);
+
+INSTANTIATE_TEST_SUITE_P(nightly_GPU_AutoBatching,
+                         AutoBatching_Test,
+                         ::testing::Combine(::testing::Values(ov::test::utils::DEVICE_GPU),
+                                            ::testing::ValuesIn(get_vs_set),
+                                            ::testing::ValuesIn(num_streams),
+                                            ::testing::ValuesIn(num_requests),
+                                            ::testing::ValuesIn(num_batch)),
+                         AutoBatching_Test::getTestCaseName);
+
+INSTANTIATE_TEST_SUITE_P(nightly_GPU_AutoBatching,
+                         AutoBatching_Test_DetectionOutput,
+                         ::testing::Combine(::testing::Values(ov::test::utils::DEVICE_GPU),
                                             ::testing::ValuesIn(get_vs_set),
                                             ::testing::ValuesIn(num_streams),
                                             ::testing::ValuesIn(num_requests),
