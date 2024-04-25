b122cb654ce49e5d9f1b991eda58465ee90bb3ea
Evgeny Kotov <evgeny.kotov@intel.com>
2024-04-25 19:11:56 +0200

    fix test_tf_Concat failure after Const remove transformation fix

--- tests/layer_tests/tensorflow_tests/test_tf_Concat.py
+++ tests/layer_tests/tensorflow_tests/test_tf_Concat.py
@@ -17,11 +17,12 @@ class TestConcat(CommonTFLayerTest):
             for ind, input_shape in enumerate(input_shapes):
                 placeholders.append(tf.compat.v1.placeholder(tf.float32, input_shape, 'input_{}'.format(ind)))
             if len(input_shapes) == 1:
-                tf.concat(values=placeholders, axis=axis, name='concat')
+                concat = tf.concat(values=placeholders, axis=axis, name='concat')
             elif is_v2:
-                tf.raw_ops.ConcatV2(values=placeholders, axis=axis, name='concat')
+                concat = tf.raw_ops.ConcatV2(values=placeholders, axis=axis, name='concat')
             else:
-                tf.raw_ops.Concat(values=placeholders, concat_dim=axis, name='concat')
+                concat = tf.raw_ops.Concat(values=placeholders, concat_dim=axis, name='concat')
+            tf.math.add(concat, 2, name='x_output')
             tf.compat.v1.global_variables_initializer()
             tf_net = sess.graph_def
 
