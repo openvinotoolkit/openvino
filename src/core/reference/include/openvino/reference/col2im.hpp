48f1a289b94398d08df20e1fd90180f7ec34ee63
p-wysocki <przemyslaw.wysocki@intel.com>
2024-04-25 18:54:47 +0200

    WIP

--- src/core/reference/include/openvino/reference/col2im.hpp
+++ src/core/reference/include/openvino/reference/col2im.hpp
@@ -14,7 +14,6 @@
 namespace ov {
 namespace reference {
 
-
 template <typename T>
 void col2im(const T* data,
             const Shape& data_shape,
@@ -29,12 +28,14 @@ void col2im(const T* data,
     const bool is_batched = data_shape.size() == 3;
     const size_t C_idx = is_batched ? 1 : 0;
     const auto elements_per_block = data_shape[C_idx];
-    const auto kernel_product = kernel_size[0] * kernel_size[1];
+    const auto kernel_product = (*kernel_size[0]) * (*kernel_size[1]);
     const auto channel_count = elements_per_block / kernel_product;
-    const auto image_height = output_size[0];
-    const auto image_width = output_size[1];
+    const auto image_height = *output_size[0];
+    const auto image_width = *output_size[1];
+    const auto column_size = (*output_size[0]) * (*output_size[1]);  // ???
     const auto channels_per_column = channel_count * kernel_product;
 
+    // fill output with zeros to account for missing dilated values
     std::fill_n(out, image_height * image_width * channel_count, T(0));
 
     for (size_t column = 0; column < channels_per_column; ++column) {
@@ -42,14 +43,17 @@ void col2im(const T* data,
         const auto width_offset = (column / kernel_size[1]) % kernel_size[0];
         const auto channel_idx = column / kernel_size[0] / kernel_size[1];
 
-        for (size_t column_height_idx = 0; column_height_idx < height_offset; ++column_height_idx) {
-            size_t image_height_idx = column_height_idx * strides[0] - pads_begin[0] - pads_end[0] + height_offset * dilations[0];
+        for (size_t column_height_idx = 0; column_height_idx < image_height; ++column_height_idx) {
+            const size_t image_height_idx = column_height_idx * strides[0] - pads_begin[0] - pads_end[0] + height_offset * dilations[0];
 
-            for (size_t column_width_idx = 0; column_width_idx < width_offset; ++ column_width_idx) {
-                size_t image_width_idx = column_width_idx * strides[1] - pads_begin[1] - pads_end[1] + width_offset * dilations[1];
+            for (size_t column_width_idx = 0; column_width_idx < image_width; ++column_width_idx) {
+                const size_t image_width_idx = column_width_idx * strides[1] - pads_begin[1] - pads_end[1] + width_offset * dilations[1];
 
                 if (image_height_idx >= 0 && image_height_idx < image_height && image_width_idx >= 0 && image_width_idx < image_width) {
-                    out[(channel_idx * image_height * image_height_idx) * image_width + image_width_idx] += data[(column * output_size[0] + image_height_idx) * output_size[1] + column_width_idx]
+                    const auto img_idx = image_width * (channel_idx * image_height * image_height_idx) + image_width_idx;
+                    const auto data_idx = image_width * (column * image_height + image_height_idx) + column_width_idx;
+                    // addition is necessary because overlapping values need to be summed
+                    out[img_idx] += data[data_idx]
                 }
             }
         }
