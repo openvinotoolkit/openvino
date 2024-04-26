2fd94054c1783526574bd2cc1d5be95ee8129bb0
xipingya <xiping.yan@intel.com>
2024-04-26 03:22:43 +0000

    1: Rename have_1d_zp to have_scalar_zp, use math concept; 2: Reduce if else branches, keep a branch to process most of cases, others will fall back to reference.
    Signed-off-by: xipingya <xiping.yan@intel.com>

--- src/plugins/intel_cpu/src/nodes/gather.h
+++ src/plugins/intel_cpu/src/nodes/gather.h
@@ -106,7 +106,7 @@ private:
     static constexpr size_t GATHER_ZP = 4;
 
     bool have_zp = false;
-    bool have_1d_zp = false;
+    bool have_scalar_zp = false;
     size_t zp_group_size = 1u;
     size_t scale_group_size = 1u;
 
