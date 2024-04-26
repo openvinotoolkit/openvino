9e296e29dd8f899f7b2d0145dcdf9e279bbd39b4
Georgy Krivoruchko <georgy.krivoruchko@intel.com>
2024-04-26 09:58:24 +0400

    [ONNX] Fixed default value of approximate attribute for Gelu (#24215)
    ### Details:
     - Fixed default value of approximate attribute for Gelu
    
    ### Tickets:
     - 135354

--- src/frontends/onnx/frontend/src/op/gelu.cpp
+++ src/frontends/onnx/frontend/src/op/gelu.cpp
@@ -16,7 +16,7 @@ namespace op {
 namespace set_1 {
 ov::OutputVector gelu(const ov::frontend::onnx::Node& node) {
     const auto& inputs = node.get_ov_inputs();
-    std::string approximate = node.get_attribute_value<std::string>("approximate", "");
+    std::string approximate = node.get_attribute_value<std::string>("approximate", "none");
 
     FRONT_END_GENERAL_CHECK(inputs.size() == 1, "Wrong number of inputs, expected 1, found ", inputs.size());
     const auto input_type = inputs[0].get_element_type();
@@ -24,13 +24,13 @@ ov::OutputVector gelu(const ov::frontend::onnx::Node& node) {
                                 input_type == ov::element::f32 || input_type == ov::element::f64,
                             "Wrong input type, expected BFLOAT16, FLOAT16, FLOAT, DOUBLE, but found ",
                             input_type.get_type_name());
-    FRONT_END_GENERAL_CHECK(approximate == "" || approximate == "tanh",
+    FRONT_END_GENERAL_CHECK(approximate == "none" || approximate == "tanh",
                             "Unsupported approximate attribute: ",
                             approximate);
 
     return {std::make_shared<v7::Gelu>(
         inputs[0],
-        approximate == "" ? ov::op::GeluApproximationMode::ERF : ov::op::GeluApproximationMode::TANH)};
+        approximate == "none" ? ov::op::GeluApproximationMode::ERF : ov::op::GeluApproximationMode::TANH)};
 }
 }  // namespace set_1
 }  // namespace op
