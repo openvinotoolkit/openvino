33a76f8f0e4c4ce803ee11a669c8061e0da10b0d
michal-miotk <michal.miotk@intel.com>
2024-05-01 22:16:50 +0200

    enabled 32, 32, 256, 512, format::bfyx test

--- src/plugins/intel_gpu/tests/unit/test_cases/permute_gpu_test.cpp
+++ src/plugins/intel_gpu/tests/unit/test_cases/permute_gpu_test.cpp
@@ -2269,7 +2269,7 @@ INSTANTIATE_TEST_SUITE_P(smoke_permute_f_y_axes_tile,
                              {{32, 196, 128, 1}, format::bfyx},              // permute_f_y_axes
                              {{32, 196, 8, 16}, format::bfyx},              // permute_f_y_axes
                              //{{32, 32, 256, 512}, format::b_fs_yx_fsv32},  // THREE_DIM_TRANSPOSE
-                             //{{32, 32, 256, 512}, format::bfyx},           // PERMUTE_SIMPLE_MEM_COPY
+                             {{32, 32, 256, 512}, format::bfyx},           // PERMUTE_SIMPLE_MEM_COPY
                              {{32, 256, 256, 1}, format::b_fs_yx_fsv32},   // permute_f_y_axes
                              {{32, 32, 16, 4}, format::b_fs_yx_fsv16},     // THREE_DIM_TRANSPOSE
                              {{32, 16, 16, 16}, format::bfyx}, 
