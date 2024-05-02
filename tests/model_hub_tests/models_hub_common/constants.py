5d22a0aeede42ecf855c88de411418aaf8a9f5f8
Kazantsev, Roman <roman.kazantsev@intel.com>
2024-05-03 00:47:06 +0400

    [Model Hub Tests] Adjust env vars names for caching Hugging Face models
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- tests/model_hub_tests/models_hub_common/constants.py
+++ tests/model_hub_tests/models_hub_common/constants.py
@@ -19,17 +19,17 @@ precommit_runtime_heat_duration = os.environ.get('PRECOMMIT_RUNTIME_HEAT_DURATIO
 nigtly_runtime_heat_duration = os.environ.get('NIGHTLY_RUNTIME_HEAT_DURATION', '5')
 
 tf_hub_cache_dir = os.environ.get('TFHUB_CACHE_DIR',
-                                  os.path.join(tempfile.gettempdir(), "tfhub_modules"))
-hf_cache_dir = os.environ.get('HF_HUB_CACHE',
-                              os.path.join(tempfile.gettempdir(), "hugging_face"))
+                                  os.path.join(tempfile.gettempdir(), "tfhub_models"))
+tf_hfhub_cache_dir = os.environ.get('TF_HFHUB_CACHE_DIR',
+                                    os.path.join(tempfile.gettempdir(), "tf_hfhub_models"))
 os.environ['TFHUB_CACHE_DIR'] = tf_hub_cache_dir
-os.environ['HF_HUB_CACHE'] = hf_cache_dir
+os.environ['TF_HFHUB_CACHE_DIR'] = tf_hfhub_cache_dir
 
 no_clean_cache_dir = False
-hf_hub_cache_dir = tempfile.gettempdir()
+pt_hfhub_cache_dir = os.path.join(tempfile.gettempdir(), "pf_hfhub_models")
 if os.environ.get('USE_SYSTEM_CACHE', 'True') == 'False':
     no_clean_cache_dir = True
-    os.environ['HUGGINGFACE_HUB_CACHE'] = hf_hub_cache_dir
+    os.environ['PT_HFHUB_CACHE_DIR'] = pt_hfhub_cache_dir
 
 # supported_devices : CPU, GPU
 test_device = os.environ.get('TEST_DEVICE', 'CPU;GPU').split(';')
