f5f54c56bd060097f7678aba7c926647f510b3a7
rghvsh <116428320+rghvsh@users.noreply.github.com>
2024-05-02 18:04:53 +0530

    Update expand_dims.cpp

--- src/frontends/tensorflow_common/src/op/expand_dims.cpp
+++ src/frontends/tensorflow_common/src/op/expand_dims.cpp
@@ -15,7 +15,7 @@ namespace op {
 
 OutputVector translate_expand_dims_op(const NodeContext& node) {
     default_op_checks(node, 2, {"ExpandDims"});
-    //Check for imaginary types
+    //Check for imaginary types value
     auto input = node.get_input(0);
     auto axis = node.get_input(1);
     auto unsqueeze = make_shared<v0::Unsqueeze>(input, axis);
