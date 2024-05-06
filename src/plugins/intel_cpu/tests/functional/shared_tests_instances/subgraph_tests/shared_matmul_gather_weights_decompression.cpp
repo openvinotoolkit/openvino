e62f2be39ff9de4c810a6a7915bf22e5604c1e0d
Vladislav Golubev <vladislav.golubev@intel.com>
2024-05-06 12:38:21 +0200

    [CPU] Shared compressed weights support

---
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/subgraph_tests/shared_matmul_gather_weights_decompression.cpp
@@ -0,0 +1,43 @@
+// Copyright (C) 2023-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "shared_test_classes/subgraph/shared_matmul_gather_weights_decompression.hpp"
+
+#include "common_test_utils/test_constants.hpp"
+
+using namespace ov::test;
+
+namespace {
+TEST_P(SharedMatmulAndGatherWeightsDecompression, CompareWithRefs) {
+    SKIP_IF_CURRENT_TEST_IS_DISABLED()
+    run();
+    check_results();
+}
+
+const std::vector<GatherDecompressionShapeParams> input_shapes = {
+    {{128, 256}, {{}, {{256, 256}}}, 1, 0},
+    {{128, 256}, {{}, {{256, 256}}}, 1, 0, 16},
+};
+const std::vector<ElementType> weights_precisions = {ov::element::u8, ov::element::u4, ov::element::i4};
+const std::vector<ElementType> decompression_precisions = {ov::element::f32};
+const std::vector<bool> decompression_subtract = {true, false};
+
+// ARM doesn't support compressed matmul weights: in this case weights are folded in float constant
+#if defined(OPENVINO_ARCH_ARM64) || defined(OPENVINO_ARCH_ARM)
+const bool use_matmul_decompression_impl = false;
+#else
+const bool use_matmul_decompression_impl = true;
+#endif  // OPENVINO_ARCH_ARM || OPENVINO_ARCH_ARM64
+
+INSTANTIATE_TEST_SUITE_P(smoke_MatmulAndGatherSharedWeightsDecompression,
+                         SharedMatmulAndGatherWeightsDecompression,
+                         ::testing::Combine(::testing::Values(utils::DEVICE_CPU),
+                                            ::testing::ValuesIn(input_shapes),
+                                            ::testing::ValuesIn(weights_precisions),
+                                            ::testing::ValuesIn(decompression_precisions),
+                                            ::testing::ValuesIn(decompression_subtract),
+                                            ::testing::Values(use_matmul_decompression_impl)),
+                         SharedMatmulAndGatherWeightsDecompression::getTestCaseName);
+
+}  // namespace
