b29d1757ac8dc4b8d2aacad4d38002666ac931a4
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2024-05-06 17:17:32 +0400

    [Transformations] Extend move eltw up pass with per channel case

--- src/common/transformations/include/transformations/common_optimizations/move_eltwise_up_data_movement.hpp
+++ src/common/transformations/include/transformations/common_optimizations/move_eltwise_up_data_movement.hpp
@@ -11,10 +11,25 @@
 namespace ov {
 namespace pass {
 
-class TRANSFORMATIONS_API MoveEltwiseUpThroughDataMov : public ov::pass::MatcherPass {
+class TRANSFORMATIONS_API MoveEltwiseUpThroughDataMovScalar : public ov::pass::MatcherPass {
+public:
+    OPENVINO_RTTI("MoveEltwiseUpThroughDataMovScalar", "0");
+    MoveEltwiseUpThroughDataMovScalar(std::vector<DiscreteTypeInfo> allowed_data_movement_ops);
+};
+
+class TRANSFORMATIONS_API MoveEltwiseUpThroughDataMovPerChannel : public ov::pass::MatcherPass {
+public:
+    OPENVINO_RTTI("MoveEltwiseUpThroughDataMovPerChannel", "0");
+    MoveEltwiseUpThroughDataMovPerChannel();
+};
+
+class TRANSFORMATIONS_API MoveEltwiseUpThroughDataMov : public ov::pass::GraphRewrite {
 public:
     OPENVINO_RTTI("MoveEltwiseUpThroughDataMov", "0");
-    MoveEltwiseUpThroughDataMov(std::vector<DiscreteTypeInfo> allowed_data_movement_ops = get_default_allowed_ops());
+    MoveEltwiseUpThroughDataMov(std::vector<DiscreteTypeInfo> allowed_data_movement_ops = get_default_allowed_ops()) {
+        this->add_matcher<MoveEltwiseUpThroughDataMovScalar>(allowed_data_movement_ops);
+        this->add_matcher<MoveEltwiseUpThroughDataMovPerChannel>();
+    }
 
 private:
     static std::vector<DiscreteTypeInfo> get_default_allowed_ops();
