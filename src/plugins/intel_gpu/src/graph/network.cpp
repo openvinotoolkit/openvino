260f57d57348b755a0549a5ef875b7522e16fdad
Ahn, Paul Y <paul.y.ahn@intel.com>
2024-05-13 19:46:53 +0900

    Add more condition to get output chain - reorder is optimized out and has no mem

--- src/plugins/intel_gpu/src/graph/network.cpp
+++ src/plugins/intel_gpu/src/graph/network.cpp
@@ -659,7 +659,9 @@ network::output_chains_map::iterator network::add_output_chain(std::shared_ptr<p
             } else {
                 const auto& mem_dep = dep.first->output_memory();
                 // Add dep inst to the chain when dep's output is not allocated yet.
-                if (!dep.first->outputs_allocated() || eng.is_the_same_buffer(mem_orig, mem_dep)) {
+                if (!dep.first->outputs_allocated()
+                    || (dep.first->outputs_allocated() && !p_inst->outputs_allocated())
+                    || eng.is_the_same_buffer(mem_orig, mem_dep)) {
                     auto nc_dep = const_cast<primitive_inst*>(dep.first);
                     chain.push_back(nc_dep);
                     add_mdata_chain(nc_dep);
