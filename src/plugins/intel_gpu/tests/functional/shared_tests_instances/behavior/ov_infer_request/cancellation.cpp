b78f4f361ff7c20536f5af5578bfb69b1bb5798f
Zhai, Xuejun <xuejun.zhai@intel.com>
2024-05-14 20:36:51 +0800

    Fix CI format issue
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>

--- src/plugins/intel_gpu/tests/functional/shared_tests_instances/behavior/ov_infer_request/cancellation.cpp
+++ src/plugins/intel_gpu/tests/functional/shared_tests_instances/behavior/ov_infer_request/cancellation.cpp
@@ -13,13 +13,6 @@ auto configs = []() {
     };
 };
 
-auto autoBatchConfigs = []() {
-    return std::vector<ov::AnyMap>{// explicit batch size 4 to avoid fallback to no auto-batching (i.e. plain GPU)
-                                   {ov::device::priorities(std::string(ov::test::utils::DEVICE_GPU) + "(4)"),
-                                    // no timeout to avoid increasing the test time
-                                    ov::auto_batch_timeout(0)}};
-};
-
 INSTANTIATE_TEST_SUITE_P(smoke_BehaviorTests, OVInferRequestCancellationTests,
         ::testing::Combine(
             ::testing::Values(ov::test::utils::DEVICE_GPU),
