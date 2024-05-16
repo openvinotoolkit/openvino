99fd483a0a47c279bde96e81f8d7a264c39c25a1
michal-miotk <michal.miotk@intel.com>
2024-05-16 10:27:38 +0200

    moved fusion tests

--- src/common/transformations/tests/common_optimizations/rms_norm_decomposition_test.cpp
+++ src/common/transformations/tests/common_optimizations/rms_norm_decomposition_test.cpp
@@ -9,15 +9,15 @@
 
 #include <openvino/core/model.hpp>
 #include <openvino/opsets/opset10.hpp>
-#include <intel_gpu/op/rms.hpp>
-#include <plugin/transformations/rms_fusion.hpp>
+#include <ov_ops/rms.hpp>
+#include <transformations/common_optimizations/rms_fusion.hpp>
 #include <transformations/utils/utils.hpp>
 #include <openvino/pass/manager.hpp>
 
 #include "common_test_utils/ov_test_utils.hpp"
 
 using namespace testing;
-using namespace ov::intel_gpu;
+using namespace ov::pass;
 
 TEST_F(TransformationTestsF, RMSNormFusionTest1) {
     {
@@ -42,7 +42,7 @@ TEST_F(TransformationTestsF, RMSNormFusionTest1) {
     {
         auto input = std::make_shared<ov::opset10::Parameter>(ov::element::f32, ov::Shape{1, 2, 6});
         auto rms_const = ov::opset10::Constant::create(ov::element::f32, ov::Shape{6}, {0.029f, 0.014f, 0.003f, 0.013f, 0.015f, 0.009f});
-        auto rms = std::make_shared<op::RMS>(input, rms_const, 1e-5f, ov::element::f16);
+        auto rms = std::make_shared<ov::op::internal::RMS>(input, rms_const, 1e-5f, ov::element::f16);
 
         model_ref = std::make_shared<ov::Model>(ov::NodeVector{rms}, ov::ParameterVector{input});
     }
@@ -137,7 +137,7 @@ TEST_F(TransformationTestsF, RMSNormFusionTest5) {
     {
         auto input = std::make_shared<ov::opset10::Parameter>(ov::element::f32, ov::PartialShape{-1, -1, 6});
         auto rms_const = ov::opset10::Constant::create(ov::element::f32, ov::Shape{6}, {0.029f, 0.014f, 0.003f, 0.013f, 0.015f, 0.009f});
-        auto rms = std::make_shared<op::RMS>(input, rms_const, 1e-5f, ov::element::f16);
+        auto rms = std::make_shared<ov::op::internal::RMS>(input, rms_const, 1e-5f, ov::element::f16);
 
         model_ref = std::make_shared<ov::Model>(ov::NodeVector{rms}, ov::ParameterVector{input});
     }
