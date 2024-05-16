74829b1ad22fdc5cd915bd0ec1bba5a4c20cfe08
Pawel Raasz <pawel.raasz@intel.com>
2024-05-16 08:38:44 +0200

    [core] Use custom functions in reference binary ops impl (#24444)
    ### Details:
    - Custom function used in binary element wise operators to reduce bin
    size (around 130k).
    
    ### Tickets:
     - [CVS-138266](https://jira.devtools.intel.com/browse/CVS-138266)

--- src/core/reference/include/openvino/reference/divide.hpp
+++ src/core/reference/include/openvino/reference/divide.hpp
@@ -16,38 +16,33 @@
 
 namespace ov {
 namespace reference {
+namespace func {
+
+template <class T>
+constexpr T div(const T x, const T y) {
+    return x / y;
+}
+
 // NOTE: Execution throws `std::domain_error` if either a non-integral value or an
 // out-of-bounds value is detected in the input tensor.
-
-// In English: return type is void and T must be an integral type.
-template <typename T>
-typename std::enable_if<std::is_integral<T>::value>::type divide(const T* arg0,
-                                                                 const T* arg1,
-                                                                 T* out,
-                                                                 size_t count,
-                                                                 bool pythondiv) {
-    if (pythondiv) {
-        for (size_t i = 0; i < count; i++) {
-            if (arg1[i] == 0) {
-                throw std::domain_error("integer division by zero");
-            }
-            T quot = arg0[i] / arg1[i];
-            T rem = arg0[i] % arg1[i];
-            if ((rem != 0) && ((arg0[i] < 0) != (arg1[i] < 0))) {
-                out[i] = quot - 1;
-            } else {
-                out[i] = quot;
-            }
-        }
-    } else {
-        for (size_t i = 0; i < count; i++) {
-            if (arg1[i] == 0) {
-                throw std::domain_error("integer division by zero");
-            }
-            out[i] = arg0[i] / arg1[i];
-        }
+template <class T>
+T try_div(const T x, const T y) {
+    if (y == 0) {
+        throw std::domain_error("integer division by zero");
     }
+    return div(x, y);
 }
+
+template <class T>
+T try_python_div(const T x, const T y) {
+    if (y == 0) {
+        throw std::domain_error("integer division by zero");
+    }
+
+    T quot = div(x, y);
+    return (((x < 0) != (y < 0)) && (x % y != 0)) ? quot - T{1} : quot;
+}
+}  // namespace func
 
 template <typename T>
 typename std::enable_if<std::is_integral<T>::value>::type divide(const T* arg0,
@@ -57,26 +52,8 @@ typename std::enable_if<std::is_integral<T>::value>::type divide(const T* arg0,
                                                                  const Shape& arg1_shape,
                                                                  const op::AutoBroadcastSpec& broadcast_spec,
                                                                  bool pythondiv) {
-    auto functor = [pythondiv](T x, T y) -> T {
-        if (pythondiv) {
-            if (y == 0) {
-                throw std::domain_error("integer division by zero");
-            }
-            T quot = x / y;
-            T rem = x % y;
-            if ((rem != 0) && ((x < 0) != (y < 0))) {
-                return quot - 1;
-            } else {
-                return quot;
-            }
-        } else {
-            if (y == 0) {
-                throw std::domain_error("integer division by zero");
-            }
-            return x / y;
-        }
-    };
-    autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, functor);
+    auto div = pythondiv ? func::try_python_div<T> : func::try_div<T>;
+    autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, div);
 }
 
 // In English: return type is void and T must be a standard floating point type, or
@@ -84,13 +61,10 @@ typename std::enable_if<std::is_integral<T>::value>::type divide(const T* arg0,
 template <typename T>
 typename std::enable_if<std::is_floating_point<T>::value || std::is_same<T, bfloat16>::value ||
                         std::is_same<T, float16>::value>::type
-divide(const T* arg0, const T* arg1, T* out, size_t count, bool pythondiv) {
-    (void)pythondiv;
-    for (size_t i = 0; i < count; i++) {
-        // TODO: Here we do not check for div by zero, so we'll get +-inf here
-        // if arg1[i] == 0. Is that the right thing to do? Jury's still out.
-        out[i] = arg0[i] / arg1[i];
-    }
+divide(const T* arg0, const T* arg1, T* out, size_t count, bool) {
+    // TODO: Here we do not check for div by zero, so we'll get +-inf here
+    // if arg1[i] == 0. Is that the right thing to do? Jury's still out.
+    std::transform(arg0, arg0 + count, arg1, out, func::div<T>);
 }
 
 template <typename T>
@@ -102,11 +76,8 @@ divide(const T* arg0,
        const Shape& arg0_shape,
        const Shape& arg1_shape,
        const op::AutoBroadcastSpec& broadcast_spec,
-       bool pythondiv) {
-    (void)pythondiv;
-    autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, [](T x, T y) -> T {
-        return x / y;
-    });
+       bool) {
+    autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, func::div<T>);
 }
 }  // namespace reference
 }  // namespace ov
