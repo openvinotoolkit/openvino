56960b71b197b450c0343d2047a350249fe62a08
p-wysocki <przemyslaw.wysocki@intel.com>
2024-05-17 11:40:47 +0200

    Move evaluate to template plugin

--- src/core/include/openvino/opsets/opset15_tbl.hpp
+++ src/core/include/openvino/opsets/opset15_tbl.hpp
@@ -15,3 +15,4 @@ _OPENVINO_OP_REG(ShapeOf, ov::op::v3)
 // New operations added in opset15
 _OPENVINO_OP_REG(RMSNorm, ov::op::v14)
 _OPENVINO_OP_REG(ScatterNDUpdate, ov::op::v15)
+_OPENVINO_OP_REG(Col2Im, ov::op::v15)
