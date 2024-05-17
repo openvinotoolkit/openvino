679a3f757e87c2e9597c8bb22bfbd514ba39af59
barnasm1 <michal.barnas@intel.com>
2024-05-17 12:05:33 +0200

    [CORE] Change get_input_const_data_as return type from std::unique_ptr to ov::optional (#24443)
    ### Details:
     - Return type replaced
     - Checked if binary size decreased.
    
    ### Tickets:
     - CVS-111396
    
    ---------
    
    Co-authored-by: Pawel Raasz <pawel.raasz@intel.com>

--- src/core/shape_inference/include/rms_norm_shape_inference.hpp
+++ src/core/shape_inference/include/rms_norm_shape_inference.hpp
@@ -54,7 +54,7 @@ std::vector<TRShape> shape_infer(const RMSNorm* op,
     }
 
     // Axes values validation
-    if (const auto axes_val = ov::op::get_input_const_data_as<TRShape, int64_t>(op, 1, tensor_accessor)) {
+    if (auto axes_val = ov::op::get_input_const_data_as<TRShape, int64_t>(op, 1, tensor_accessor)) {
         ov::util::normalize_axes(op, data_rank.get_length(), *axes_val);
     }
 
