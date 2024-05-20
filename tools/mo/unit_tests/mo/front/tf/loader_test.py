0b81a215b26b92e9e23684be9d2883af3c8e9b86
Roman Kazantsev <roman.kazantsev@intel.com>
2024-05-20 19:55:16 +0400

    [MO] Remove test-generator dependency (#24576)
    **Details:** Remove test-generator dependency. It is removed since this
    module seems to be no longer developed and maintained. So it is better
    to remove this dependency. `pytest` can substitute `test-generator`.
    
    **Ticket:** TBD
    
    ---------
    
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- tools/mo/unit_tests/mo/front/tf/loader_test.py
+++ tools/mo/unit_tests/mo/front/tf/loader_test.py
@@ -3,19 +3,15 @@
 
 import unittest.mock
 from io import StringIO
+from openvino.tools.mo.front.tf.loader import load_tf_graph_def
 from unittest.mock import Mock, MagicMock
 
-from generator import generate, generator
 
-from openvino.tools.mo.front.tf.loader import load_tf_graph_def
-
-
-@generator
 class TestLoader(unittest.TestCase):
-    @generate('/path/to/somewhere/my_checkpoint.ckpt', '/path/to/somewhere/my_meta_graph.meta')
     @unittest.mock.patch('sys.stdout', new_callable=StringIO)
-    def test_helper_print_ckpt(self, path, out):
-        mock = Mock(__bool__=MagicMock(side_effect=Exception()))
-        self.assertRaises(Exception, load_tf_graph_def, path, meta_graph_file=mock)
-        self.assertRegex(out.getvalue(),
-                         r'\[ WARNING ] The value for the --input_model command line parameter ends with "\.ckpt"')
+    def test_helper_print_ckpt(self, out):
+        for path in ['/path/to/somewhere/my_checkpoint.ckpt', '/path/to/somewhere/my_meta_graph.meta']:
+            mock = Mock(__bool__=MagicMock(side_effect=Exception()))
+            self.assertRaises(Exception, load_tf_graph_def, path, meta_graph_file=mock)
+            self.assertRegex(out.getvalue(),
+                             r'\[ WARNING ] The value for the --input_model command line parameter ends with "\.ckpt"')
