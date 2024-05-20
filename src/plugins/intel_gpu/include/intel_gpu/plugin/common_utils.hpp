ab23739213470d4b8893784f359de688a72588bb
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2024-05-20 10:34:23 +0400

    [GPU] Avoid force exit for out of resource exception if device is lost (#23826)
    ### Details:
    - Improved CL_OUT_OF_RESOURCES exception handling to avoid force exit
    when device is lost

--- src/plugins/intel_gpu/include/intel_gpu/plugin/common_utils.hpp
+++ src/plugins/intel_gpu/include/intel_gpu/plugin/common_utils.hpp
@@ -111,17 +111,6 @@ inline ov::Shape predict_shape(const std::string& name, const cldnn::layout layo
     return layout.get_shape();
 }
 
-/// WA: Force exit. Any opencl api call can be hang after CL_OUT_OF_RESOURCES.
-inline void ForceExit() {
-    std::cerr << "[GPU] force exit.\n"
-              << "\tDue to the driver bug any subsequent OpenCL API call will cause application hang, "
-              << "so GPU plugin can't finish correctly.\n"
-              << "\tPlease try to update the driver or reduce memory consumption "
-              << "(use smaller batch size, less streams, lower precision, etc)"
-              << "to avoid CL_OUT_OF_RESOURCES exception" << std::endl;
-    std::_Exit(-1);
-}
-
 void convert_and_copy(const ov::ITensor* src, cldnn::memory::ptr dst, cldnn::stream& stream);
 void convert_and_copy(const cldnn::memory::ptr src, ov::ITensor const* dst, const cldnn::stream& stream);
 void convert_and_copy(const ov::ITensor* src, ov::ITensor const* dst, const cldnn::stream& stream);
