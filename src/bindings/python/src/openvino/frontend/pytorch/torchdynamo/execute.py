78b23d12ec34c23b1e3a63cb2ad958a17fafcf61
Cavus Mustafa <mustafa.cavus@intel.com>
2024-05-20 17:43:51 -0700

    TorchFX: Type fix for import

--- src/bindings/python/src/openvino/frontend/pytorch/torchdynamo/execute.py
+++ src/bindings/python/src/openvino/frontend/pytorch/torchdynamo/execute.py
@@ -21,7 +21,7 @@ from openvino.frontend.pytorch.fx_decoder import TorchFXPythonDecoder
 from openvino.frontend.pytorch.torchdynamo.partition import Partitioner
 from openvino.frontend.pytorch.torchdynamo.compile import openvino_compile
 from openvino.runtime import Core, Type, PartialShape
-from openvino.frontend.pytorch.torchdynamo.backend_utils import _get_cache_dir, _get_device, aot_autograd
+from openvino.frontend.pytorch.torchdynamo.backend_utils import _get_cache_dir, _get_device, _get_aot_autograd
 
 from typing import Callable, Optional, Any
 
