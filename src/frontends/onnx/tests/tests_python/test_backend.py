8b03198df09ecfe0a16185f07f708e6db4dd69f6
Georgy Krivoruchko <georgy.krivoruchko@intel.com>
2024-05-23 07:25:28 +0400

    Update test_backend.py

--- src/frontends/onnx/tests/tests_python/test_backend.py
+++ src/frontends/onnx/tests/tests_python/test_backend.py
@@ -686,8 +686,6 @@ tests_expected_to_fail = [
     (
         xfail_issue_139937,
         "OnnxBackendNodeModelTest.test_dequantizelinear_blocked_cpu",
-        "OnnxBackendNodeModelTest.test_group_normalization_epsilon_cpu",
-        "OnnxBackendNodeModelTest.test_group_normalization_example_cpu",
         "OnnxBackendNodeModelTest.test_qlinearmatmul_2D_int8_float16_cpu",
     ),
     (
