1a4ae5e7a35f5fc054e704e62ff9e7255a4da945
Yuan Hu <yuan2.hu@intel.com>
2024-05-23 19:47:57 +0800

    [core] fix thread safe issue in #19832 (#24562)
    ### Details:
    - *I lost code `t_stream_count_map[(void*)this] = item.first;` in
    #19832*
     - *the thread safe issue happen in below workflow*
     - create thread A
    call CustomThreadLocal:local() in thread A -> create stream A (the count
    of stream A is 2)
       destory thread A (the count of stream A is 1)
       create thread B (same thread id with thread A)
    call CustomThreadLocal:local() in thread B -> use stream A(the count of
    stream A is 1, so it's broken)
    - *add testcase, also fix
    https://github.com/openvinotoolkit/openvino/pull/19986/files#r1332774754*
    
    ### Tickets:
     - Closes https://github.com/openvinotoolkit/openvino/issues/24509
    
    ---------
    
    Signed-off-by: HU Yuan2 <yuan2.hu@intel.com>
    Co-authored-by: Wanglei Shen <wanglei.shen@intel.com>

--- tests/stress_tests/common/infer_api/infer_api.cpp
+++ tests/stress_tests/common/infer_api/infer_api.cpp
@@ -35,6 +35,20 @@ void InferAPI2::create_infer_request() {
     infer_request = compiled_model.create_infer_request();
 }
 
+void InferAPI2::create_and_infer(const bool &async) {
+    auto new_infer_request = compiled_model.create_infer_request();
+    fillTensors(new_infer_request, inputs);
+    if (async) {
+        new_infer_request.start_async();
+        new_infer_request.wait();
+    } else {
+        new_infer_request.infer();
+    }
+    for (size_t i = 0; i < outputs.size(); ++i) {
+        const auto &output_tensor = new_infer_request.get_output_tensor(i);
+    }
+}
+
 void InferAPI2::prepare_input() {
     fillTensors(infer_request, inputs);
 }
