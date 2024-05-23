75d7593ee28aca5a1f1c522afe6782830378da4b
Yuan Hu <yuan2.hu@intel.com>
2024-05-23 19:47:57 +0800

    [core] fix thread safe issue in #19832 (#24562)
    ### Details:
    - *I lost code `t_stream_count_map[(void*)this] = item.first;` in
    #19832*
     - *the thread safe issue happen in below workflow*
     - create thread A
    call CustomThreadLocal:local() in thread A -> create stream A (the count
    of stream A is 2)
       destory thread A (the count of stream A is 1)
       create thread B (same thread id with thread A)
    call CustomThreadLocal:local() in thread B -> use stream A(the count of
    stream A is 1, so it's broken)
    - *add testcase, also fix
    https://github.com/openvinotoolkit/openvino/pull/19986/files#r1332774754*
    
    ### Tickets:
     - Closes https://github.com/openvinotoolkit/openvino/issues/24509
    
    ---------
    
    Signed-off-by: HU Yuan2 <yuan2.hu@intel.com>
    Co-authored-by: Wanglei Shen <wanglei.shen@intel.com>

--- tests/stress_tests/unittests/tests_pipelines/tests_pipelines.cpp
+++ tests/stress_tests/unittests/tests_pipelines/tests_pipelines.cpp
@@ -81,3 +81,68 @@ void test_infer_request_inference(const std::string &model, const std::string &t
         infer_request_inference(model, target_device)();
     }
 }
+
+static void test_recreate_and_infer_in_thread_one_model(const std::string &model, const std::string &target_device, const int &n, const bool &async) {
+    auto ie_wrapper = create_infer_api_wrapper();
+    ie_wrapper->read_network(model);
+    ie_wrapper->set_config(target_device, ov::AnyMap{ov::inference_num_threads(2)});
+    ie_wrapper->load_network(target_device);
+    auto fun = recreate_and_infer_in_thread(ie_wrapper, async);
+    for(int y = 0; y < n; y++) {
+        std::vector<std::thread> threads;
+        for (int i = 0; i < 4; i++) {
+            ie_wrapper->create_and_infer(async);
+            threads.emplace_back(fun);
+        }
+        for (int i = 0; i < 4; i++) {
+            threads[i].join();
+        }
+    }
+}
+
+static void test_recreate_and_infer_in_thread_two_model(const std::string &model, const std::string &target_device, const int &n) {
+    std::vector<std::shared_ptr<InferApiBase>> ie_wrapper_vector;
+    for(int i = 0; i < 2; i++) {
+        auto ie_wrapper = create_infer_api_wrapper();
+        ie_wrapper->read_network(model);
+        ie_wrapper->set_config(target_device, ov::AnyMap{ov::inference_num_threads(2)});
+        ie_wrapper->load_network(target_device);
+        ie_wrapper_vector.push_back(ie_wrapper);
+    }
+    auto async_func0 = recreate_and_infer_in_thread(ie_wrapper_vector[0], true);
+    auto async_func1 = recreate_and_infer_in_thread(ie_wrapper_vector[1], true);
+    auto sync_func0 = recreate_and_infer_in_thread(ie_wrapper_vector[0], false);
+    auto sync_func1 = recreate_and_infer_in_thread(ie_wrapper_vector[1], false);
+
+    for(int y = 0; y < n; y++) {
+        std::vector<std::thread> threads;
+        for (int i = 0; i < 4; i++) {
+            if ( i % 2 == 0) {
+                ie_wrapper_vector[0]->create_and_infer(true);
+                threads.emplace_back(async_func0);
+            } else {
+                ie_wrapper_vector[0]->create_and_infer(false);
+                threads.emplace_back(sync_func0);
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            if ( i % 2 == 0) {
+                ie_wrapper_vector[1]->create_and_infer(true);
+                threads.emplace_back(async_func1);
+            } else {
+                ie_wrapper_vector[1]->create_and_infer(false);
+                threads.emplace_back(sync_func1);
+            }
+        }
+        for (int i = 0; i < threads.size(); i++) {
+            threads[i].join();
+        }
+    }
+}
+
+void test_recreate_and_infer_in_thread(const std::string &model, const std::string &target_device, const int &n) {
+    test_recreate_and_infer_in_thread_one_model(model, target_device, n, false);
+    test_recreate_and_infer_in_thread_one_model(model, target_device, n, true);
+    test_recreate_and_infer_in_thread_two_model(model, target_device, n);
+}
+
