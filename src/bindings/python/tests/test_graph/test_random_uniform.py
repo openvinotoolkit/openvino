935000c96fa54bb6e15b61cd0d8f0b542324a0b7
Roboreptile <piotr.krzeminski@intel.com>
2024-05-23 08:56:09 +0000

    [FIX] Pylint pt 2

--- src/bindings/python/tests/test_graph/test_random_uniform.py
+++ src/bindings/python/tests/test_graph/test_random_uniform.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import torch
+import pytest
 import numpy as np
 import tensorflow as tf
 import openvino.runtime.opset8 as ops
@@ -24,10 +25,10 @@ def test_random_uniform():
 
 
 @pytest.mark.parametrize(
-    ("min", "max"),
+    ("min_value", "max_value"),
     [
-        (0, 1),
-        (-10, 10)
+        (-10, 10),
+        (-100, 123)
     ],
 )
 @pytest.mark.parametrize(
@@ -35,45 +36,49 @@ def test_random_uniform():
     [
         (1, 2),
         (367, 123),
-        (123, 367)
+        (93, 43)
     ]
 )
 @pytest.mark.parametrize(
     ("ov_dtype", "torch_dtype", "str_dtype"),
     [
+        (Type.f64, torch.float64, "f64"),
         (Type.f32, torch.float32, "f32"),
+        (Type.f16, torch.float16, "f16"),
+        (Type.bf16, torch.bfloat16, "bf16"),
         (Type.i64, torch.int64, "i64"),
+        (Type.i32, torch.int32, "i32")
     ]
 )
 @pytest.mark.parametrize(
     ("shape"),
     [
-        (6, 6)
+        (4, 4),
+        (6, 6),
+        (10, 5)
     ]
 )
-def test_random_uniform_pytorch_alignment(min, max, global_seed, op_seed, ov_dtype, torch_dtype, str_dtype, shape):
+def test_random_uniform_pytorch_alignment(min_value, max_value, global_seed, op_seed, ov_dtype, torch_dtype, str_dtype, shape):
     
     torch.manual_seed(global_seed)
     expected = torch.empty(shape, dtype = torch_dtype)
-    expected = expected.uniform_(min, max)
+    expected = expected.uniform_(min_value, max_value)
 
     core = Core()
-    output = ops.random_uniform(Shape(shape), ops.constant(min, ov_dtype), ops.constant(max, ov_dtype), str_dtype, global_seed, op_seed, "pytorch")
-    model = Model(output, [], 'random_uniform_test_model')
-    compiled_model = core.compile_model(model, 'TEMPLATE')
+    output = ops.random_uniform(Shape(shape), ops.constant(min_value, ov_dtype), ops.constant(max_value, ov_dtype), str_dtype, global_seed, op_seed, "pytorch")
+    model = Model(output, [], "random_uniform_seed_alignment_test_model_pytorch")
+    compiled_model = core.compile_model(model, "TEMPLATE")
     results = compiled_model.infer_new_request()
     actual = next(iter(results.values()))
 
-    print(expected)
-    print(actual)
-
     assert(np.array_equal(expected, actual))
 
-# Basic tensorflow RandomUniform op only generates random numbers between 0 and 1 (for floats)
+
 @pytest.mark.parametrize(
-    ("min", "max"),
+    ("min_value", "max_value"),
     [
-        (0, 1),
+        (-10, 10),
+        (-100, 123)
     ],
 )
 @pytest.mark.parametrize(
@@ -81,95 +86,51 @@ def test_random_uniform_pytorch_alignment(min, max, global_seed, op_seed, ov_dty
     [
         (1, 2),
         (367, 123),
-        (123, 367)
+        (93, 43)
     ]
 )
 @pytest.mark.parametrize(
     ("ov_dtype", "tensorflow_dtype", "str_dtype"),
     [
-        (Type.f32, "float32", "f32"),
-        (Type.i64, "int64", "i64"),
+        (Type.f64, tf.float64, "f64"),
+        (Type.f32, tf.float32, "f32"),
+        (Type.f16, tf.float16, "f16"),
+        (Type.bf16,tf.bfloat16, "bf16"),
+        (Type.i64, tf.int64, "i64"),
+        (Type.i32, tf.int32, "i32")
     ]
 )
 @pytest.mark.parametrize(
     ("shape"),
     [
-        (6, 6)
+        (4, 4),
+        (6, 6),
+        (10, 5)
     ]
 )
-def test_random_uniform_tensorflow_alignment_float(min, max, global_seed, op_seed, ov_dtype, tensorflow_dtype, str_dtype, shape):
-    
+def test_random_uniform_tensorflow_alignment_float(min_value, max_value, global_seed, op_seed, ov_dtype, tensorflow_dtype, str_dtype, shape):
+
+    session_conf = tf.compat.v1.ConfigProto(
+        intra_op_parallelism_threads=1,
+        inter_op_parallelism_threads=1,
+        use_per_session_threads=False
+    )
+
     tf.compat.v1.reset_default_graph()
     tf.compat.v1.disable_eager_execution()
 
-    session_conf = tf.compat.v1.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
-    with tf.compat.v1.device('/CPU:0'):
+    with tf.compat.v1.device("/CPU:0"):
         with tf.compat.v1.Session(config=session_conf) as sess:
-            expected = tf.raw_ops.RandomUniform(shape=shape, dtype = tensorflow_dtype, seed = global_seed, seed2 = op_seed)
+            tf.random.set_seed(global_seed)
+            expected = tf.random.uniform(shape, minval = min_value, maxval = max_value, dtype = tensorflow_dtype, seed = op_seed)
             sess.run(expected)
             sess.close()
 
     core = Core()
-    output = ops.random_uniform(Shape(shape), ops.constant(min, ov_dtype), ops.constant(max, ov_dtype), str_dtype, global_seed, op_seed, "tensorflow")
-    model = Model(output, [], 'random_uniform_test_model')
-    compiled_model = core.compile_model(model, 'TEMPLATE')
+    output = ops.random_uniform(Shape(shape), ops.constant(min_value, ov_dtype), ops.constant(max_value, ov_dtype), str_dtype, global_seed, op_seed, "tensorflow")
+    model = Model(output, [], "random_uniform_seed_alignment_test_model_tensorflow")
+    compiled_model = core.compile_model(model, "TEMPLATE")
     results = compiled_model.infer_new_request()
     actual = next(iter(results.values()))
 
-    print(expected)
-    print(actual)
-
     assert(np.array_equal(expected, actual))
-
-@pytest.mark.parametrize(
-    ("min", "max"),
-    [
-        (0, 1),
-        (-10, 10)
-    ],
-)
-@pytest.mark.parametrize(
-    ("global_seed", "op_seed"),
-    [
-        (1, 2),
-        (367, 123),
-        (123, 367)
-    ]
-)
-@pytest.mark.parametrize(
-    ("ov_dtype", "tensorflow_dtype", "str_dtype"),
-    [
-        (Type.f32, "float32", "f32"),
-        (Type.i64, "int64", "i64"),
-    ]
-)
-@pytest.mark.parametrize(
-    ("shape"),
-    [
-        (6, 6)
-    ]
-)
-def test_random_uniform_tensorflow_alignment_float(min, max, global_seed, op_seed, ov_dtype, tensorflow_dtype, str_dtype, shape):
-    
-    tf.compat.v1.reset_default_graph()
-    tf.compat.v1.disable_eager_execution()
-
-    session_conf = tf.compat.v1.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
-    with tf.compat.v1.device('/CPU:0'):
-        with tf.compat.v1.Session(config=session_conf) as sess:
-            expected = tf.raw_ops.RandomUniformInt(min, max, shape=shape, dtype = tensorflow_dtype, seed = global_seed, seed2 = op_seed)
-            sess.run(expected)
-            sess.close()
-
-    core = Core()
-    output = ops.random_uniform(Shape(shape), ops.constant(min, ov_dtype), ops.constant(max, ov_dtype), str_dtype, global_seed, op_seed, "tensorflow")
-    model = Model(output, [], 'random_uniform_test_model')
-    compiled_model = core.compile_model(model, 'TEMPLATE')
-    results = compiled_model.infer_new_request()
-    actual = next(iter(results.values()))
-
-    print(expected)
-    print(actual)
-
-    assert(np.array_equal(expected, actual))
-
