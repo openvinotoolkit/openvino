ff50cc441d878d6040b70838c0bc7b9b53f8ec00
Evgeniia Nugmanova <evgeniia.nugmanova@intel.com>
2024-05-24 12:10:30 +0400

    Removes Symbolic Optimizations from MOC

--- src/common/transformations/src/transformations/common_optimizations/moc_transformations.cpp
+++ src/common/transformations/src/transformations/common_optimizations/moc_transformations.cpp
@@ -276,7 +276,6 @@ bool ov::pass::MOCTransformations::run_on_model(const std::shared_ptr<ov::Model>
     REGISTER_PASS(manager, AlignEltwiseInputRanks)
     REGISTER_PASS(manager, SharedOpOptimization)
     REGISTER_PASS(manager, ConstantFolding)
-    REGISTER_PASS(manager, SymbolicOptimizations)
     manager.register_pass<ResolveNameCollisions>(true);
     manager.run_passes(f);
 
