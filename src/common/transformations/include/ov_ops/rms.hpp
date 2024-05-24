65a8ec7d718b70648c382428eb5adcff32de4d6f
Ivan Tikhonov <ivan.tikhonov@intel.com>
2024-05-24 11:33:00 +0400

    ConcatToBroadcast transformation (#24661)
    Details:
    
    Due to the issue in the e2e test in
    https://github.com/openvinotoolkit/openvino/pull/24597 we decided to
    exclude Concat to Tile conversion from the transformation. It will be
    covered in another ticket.
    
    - Add a ConcatToBroadcast transformation to replace Concat having inputs
    from the same output with a Broadcast
    - Add a test for the ConcatToBroadcast transformation
    
    
    Significantly reduce model compile time and performance time.
    
    Tickets:
    [CVS-138829](https://jira.devtools.intel.com/browse/CVS-138829),
    [CVS-138077](https://jira.devtools.intel.com/browse/CVS-138077)
    
    ---------
    
    Signed-off-by: Andrii Staikov <andrii.staikov@intel.com>
    Co-authored-by: Andrii Staikov <andrii.staikov@intel.com>

--- src/common/transformations/include/ov_ops/rms.hpp
+++ src/common/transformations/include/ov_ops/rms.hpp
@@ -35,9 +35,13 @@ public:
 
     std::shared_ptr<Node> clone_with_new_inputs(const ov::OutputVector& new_args) const override;
 
-    double get_epsilon() const { return m_epsilon; }
+    double get_epsilon() const {
+        return m_epsilon;
+    }
 
-    void set_epsilon(double epsilon) { m_epsilon = epsilon; }
+    void set_epsilon(double epsilon) {
+        m_epsilon = epsilon;
+    }
 
 private:
     double m_epsilon{0};
