35a2a188d65d3ca2c06a62a5b55315e600e06b50
Zhai, Xuejun <xuejun.zhai@intel.com>
2024-05-27 08:38:18 +0800

    Fix format issue
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>

--- src/tests/test_utils/common_test_utils/include/common_test_utils/file_utils.hpp
+++ src/tests/test_utils/common_test_utils/include/common_test_utils/file_utils.hpp
@@ -146,11 +146,11 @@ inline int createDirectory(const std::string& dirPath) {
 
 #ifdef OPENVINO_ENABLE_UNICODE_PATH_SUPPORT
 inline int createDirectory(const std::wstring& dirPath) {
-#ifdef _WIN32
+#    ifdef _WIN32
     return _wmkdir(dirPath.c_str());
-#else
+#    else
     return mkdir(ov::util::wstring_to_string(dirPath).c_str(), mode_t(0777));
-#endif
+#    endif
 }
 #endif
 
