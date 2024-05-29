ff3dca7dc5952f6c4758d177371fed7e7821d8e7
Pratham Ingawale <94799826+PRATHAM-SPS@users.noreply.github.com>
2024-05-29 11:38:56 +0530

    [ONNX] Added support for Multinomial-7 operator (#23491)
    ### Details:
     - *Extend ONNX Frontend with Multinomial-7 operator*
    
    
    ### Tickets:
     - *118986*
    
    ---------
    
    Co-authored-by: Andrei Kochin <andrei.kochin@intel.com>
    Co-authored-by: Katarzyna Mitrus <katarzyna.mitrus@intel.com>
    Co-authored-by: Georgy Krivoruchko <georgy.krivoruchko@intel.com>
    Co-authored-by: Piotr Krzemiński <piotrkrzeminski1234@gmail.com>

---
+++ src/frontends/onnx/frontend/src/op/multinomial.cpp
@@ -0,0 +1,53 @@
+// Copyright (C) 2018-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "op/multinomial.hpp"
+
+#include "exceptions.hpp"
+#include "openvino/op/multinomial.hpp"
+#include "utils/common.hpp"
+
+using namespace ov::op;
+using ::ONNX_NAMESPACE::TensorProto_DataType;
+namespace ov {
+namespace frontend {
+namespace onnx {
+namespace op {
+namespace set_1 {
+
+ov::OutputVector multinomial(const ov::frontend::onnx::Node& node) {
+    const auto input = node.get_ov_inputs().at(0);
+
+    const auto sample_size = node.get_attribute_as_constant<int64_t>("sample_size", 1);
+
+    const auto dtype =
+        node.get_attribute_value<int64_t>("dtype",
+                                          static_cast<int64_t>(TensorProto_DataType::TensorProto_DataType_INT32));
+    const auto seed = node.get_attribute_value<float>("seed", 0.0f);
+    const auto target_type = common::get_ov_element_type(dtype);
+    const uint64_t global_seed = 0;
+    // OpenVINO supports only uint64 seeds with a meaningful 0 value (seed will be auto-generated).
+    // Because we use a seed as a just meaningful identifier we may
+    // just interpret its value as a 32-bit value (float zero value is same with
+    // uint32 zero value).
+    // Float -0 value will be interpreted as a valid uint32 value.
+    const void* seed_ptr = &seed;  // To prevent strict-aliasing error
+    const uint64_t seed_uint64 = *static_cast<const uint32_t*>(seed_ptr);
+
+    auto multinomial_op = std::make_shared<ov::op::v13::Multinomial>(input,
+                                                                     sample_size,
+                                                                     target_type,
+                                                                     true,
+                                                                     true,
+                                                                     seed_uint64,
+                                                                     global_seed);
+
+    return {multinomial_op};
+}
+
+}  // namespace set_1
+}  // namespace op
+}  // namespace onnx
+}  // namespace frontend
+}  // namespace ov
