ccaf81b6f7db904266b20acf39a39afe9e4caea1
chenhuwa <chenhu.wang@intel.com>
2024-05-29 10:23:43 +0800

    Ivan comments apply

--- src/tests/ov_helpers/ov_snippets_models/include/subgraph_group_normalization.hpp
+++ src/tests/ov_helpers/ov_snippets_models/include/subgraph_group_normalization.hpp
@@ -10,6 +10,39 @@ namespace ov {
 namespace test {
 namespace snippets {
 
+/* Graph with example shape propogation:
+ * Graph between Reshape[1,2,1,8] and Reshape[1,2,2,4] is a MVN
+ *       Parameter[1,4,2,2], group_num is 2
+ *           |
+ *       Reshape[1,2,1,8]
+ *        |     |
+ *        | ReduceSum[1,2,1,1] Scalar
+ *        |     |              /
+ *        |    Multiply[1,2,1,1]
+ *        |     /
+ *       Substract[1,2,1,8]
+ *        |     |
+ *        |  PowerStatic[1,2,1,8]
+ *        |     |
+ *        |  ReduceSum[1,2,1,1]
+ *        |     |
+ *        |  FMA(Multiply+Add)[1,2,1,1]
+ *        |     |
+ *        |    sqrt[1,2,1,1]
+ *        |     |
+ *        |  PowerStatic[1,2,1,1]
+ *        |     /
+ *       Multiply[1,2,1,8] Parameter[4] Parameter[4]
+ *               |              |             |
+ *        Reshape[1,2,2,4]  Reshape[1,2,2,1] Reshape[1,2,2,1]
+ *                       \         |         /
+ *                            \    |    /
+ *                            FMA(Multiply+Add)[1,2,2,4]
+ *                                 |
+ *                              Reshape[1,4,2,2]
+ *                                 |
+ *                               Result[1,4,2,2]
+ */
 class GroupNormalizationFunction : public SnippetsFunctionBase {
 public:
     explicit GroupNormalizationFunction(const std::vector<PartialShape>& inputShapes, const size_t& numGroup, const float& eps)
