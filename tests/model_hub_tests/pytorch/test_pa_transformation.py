f508384dee0211ab8d8a26d23f6745fff1564cf4
Andrii Staikov <andrii.staikov@intel.com>
2024-05-29 20:23:45 +0330

    move all models to the precommit list, misc changes

--- tests/model_hub_tests/pytorch/test_pa_transformation.py
+++ tests/model_hub_tests/pytorch/test_pa_transformation.py
@@ -5,27 +5,11 @@ from openvino._offline_transformations import paged_attention_transformation
 from openvino._pyopenvino.op import _PagedAttentionExtension
 from optimum.intel import OVModelForCausalLM
 import models_hub_common.utils as utils
-import openvino as ov
 import pytest
 import os
 
-# def get_models_list(file_name: str):
-#     models = []
-#     with open(file_name) as f:
-#         for model_name in f:
-#             model_name = model_name.strip()
-#             # skip comment in model scope file
-#             if model_name.startswith('#'):
-#                 continue
-#             models.append(model_name)
-#     return models
-
-def run_pa(tmp_path, model_id):
+def run_pa(tmp_path, model_id, model_link):
     model = OVModelForCausalLM.from_pretrained(model_id, export=True)
-
-    # Uncomment if you'd like to save the post-Optimum IR
-    # model_path = os.path.join(tmp_path, model_id)
-    # model.save_pretrained(model_path)
 
     paged_attention_transformation(model.model)
 
@@ -38,15 +22,17 @@ def run_pa(tmp_path, model_id):
         for name in names:
             if (("key_cache." in name) or ("value_cache." in name)):
                 shape = input.get_partial_shape()
+                # PagedAttention uses key_cache and value_cache inputs so the last 2 dimensions have to be static
                 assert shape[-1].is_static, f"Dimension {len(shape) - 1} of input '{name}' in '{model_id}' is not static: {shape}"
                 assert shape[-2].is_static, f"Dimension {len(shape) - 2} of input '{name}' in '{model_id}' is not static: {shape}"
 
 @pytest.mark.precommit
-@pytest.mark.parametrize("model_id", utils.get_skipped_model_links("pytorch/models/hf-tiny-random-models-precommit"))
-def test_pa_precommit(tmp_path, model_id, ie_device):
-    run_pa(tmp_path, model_id)
-
-@pytest.mark.nightly
-@pytest.mark.parametrize("model_id", utils.get_skipped_model_links("pytorch/models/hf-tiny-random-models-nightly"))
-def test_pa_nightly(tmp_path, model_id, ie_device):
-    run_pa(tmp_path, model_id)
+@pytest.mark.parametrize("model_name, model_link, mark, reason", utils.get_models_list(os.path.join("pytorch", "models", "hf-tiny-random-models-precommit")))
+def test_pa_precommit(tmp_path, model_name, model_link, mark, reason, ie_device):
+    assert mark is None or mark == 'skip' or mark == 'xfail', \
+        "Incorrect test case: {}, {}".format(model_name, model_link)
+    if mark == 'skip':
+        pytest.skip(reason)
+    elif mark == 'xfail':
+        pytest.xfail(reason)
+    run_pa(tmp_path, model_name, model_link)
