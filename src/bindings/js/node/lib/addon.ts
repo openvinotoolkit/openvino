33161c80802c3a115120f3ca5c6aea40fa0b1d43
almilosz <alicja.miloszewska@intel.com>
2024-05-29 15:17:01 +0200

    Add methods descriptions for Model, Tensor & InferRequest

--- src/bindings/js/node/lib/addon.ts
+++ src/bindings/js/node/lib/addon.ts
@@ -66,17 +66,80 @@ interface CoreConstructor {
   new(): Core;
 }
 
+/**
+ * A user-defined model read by {@link Core.readModel}.
+ */
 interface Model {
-  outputs: Output[];
-  inputs: Output[];
-  output(nameOrId?: string | number): Output;
-  input(nameOrId?: string | number): Output;
-  getName(): string;
-  isDynamic(): boolean;
-  getOutputSize(): number;
-  setFriendlyName(name: string): void;
+  /**
+   * It gets the friendly name for a model. If no friendly name has been set
+   * via {@link Model.setFriendlyName} then the model’s unique name is returned.
+   * @returns A string with a friendly name of the model.
+   */
   getFriendlyName(): string;
-  getOutputShape(): number[];
+  /**
+   * It gets the unique name of the model.
+   * @returns A string with the name of the model.
+   */
+  getName(): string;
+  /**
+   * It returns the shape of the element at the specified index.
+   * @param index The index of the element.
+   */
+  getOutputShape(index: number): number[];
+  /**
+   * It returns the number of outputs of the model.
+   */
+  getOutputSize(): number;
+  /**
+   * It gets the input of a model.
+   * If a model has more than one input, this method throws an exception.
+   */
+  input(): Output;
+  /**
+   * It gets the input of a model identified by the tensor name.
+   * @param name The tensor name.
+   */
+  input(name: string): Output;
+  /**
+   * It gets the input of a model identified by the index.
+   * @param index The index of the input.
+   */
+  input(index: number): Output;
+  /**
+   * It returns true if any of the op’s defined in the model contains a partial
+   * shape.
+   */
+  isDynamic(): boolean;
+    /**
+   * It gets the output of a model.
+   * If a model has more than one output, this method throws an exception.
+   */
+  output(): Output;
+  /**
+   * It gets the output of a model identified by the tensor name.
+   * @param name The tensor name.
+   */
+  output(name: string): Output;
+  /**
+   * It gets the output of a model identified by the index.
+   * @param index The index of the input.
+   */
+  output(index: number): Output;
+  /**
+   * Sets a friendly name for a model. This does not overwrite the unique name
+   * of the model and is retrieved via {@link Model.getFriendlyName}.
+   * Used mainly for debugging.
+   * @param name A string to set as the friendly name.
+   */
+  setFriendlyName(name: string): void;
+  /**
+   * It gets all inputs of a model as an array.
+   */
+  inputs: Output[];
+  /**
+   * It gets all outputs of a model as an array.
+   */
+  outputs: Output[];
 }
 
 /**
@@ -140,31 +203,189 @@ interface CompiledModel {
 
 }
 
+/**
+ * A {@link Tensor} is a lightweight class that represents data used for
+ * inference. There are different ways to create a tensor. You can find them
+ * in {@link TensorConstructor} section.
+ */
 interface Tensor {
+  /**
+   * This property provides access to the tensor's data.
+   *
+   * Its getter returns a subclass of a TypedArray that corresponds to the
+   * tensor's element type e.g. Float32Array corresponds to float32. The
+   * content of TypedArray subclass is a copy of the tensor's underlaying 
+   * memory.
+   *
+   * Its setter fills the underlaying tensor’s memory by copying binary data
+   * buffer from a TypedArray subclass. An exception will be thrown if the size
+   * or type of array mismatches the tensor.
+   */
   data: SupportedTypedArray;
+  /**
+   * It gets the tensor’s element type.
+   */
   getElementType(): element;
+  /**
+   * It gets tensor's data.
+   * @returns A subclass of a TypedArray that corresponds to the tensor's
+   * element type e.g. Float32Array corresponds to float32.
+   */
+  getData(): SupportedTypedArray;
+  /**
+   * It gets the tensor’s shape.
+   */
   getShape(): number[];
-  getData(): number[];
+  /**
+   * It gets the tensor’s size as a total number of elements.
+   */
   getSize(): number;
 }
+
+/**
+ * This interface contains constructors of {@link Tensor} class.
+ *
+ * @remarks
+ * A tensor's memory is being shared with a TypedArray. That means
+ * the responsibility of keeping the reference to TypedArray is on the side
+ * of a user. Any action performed on the TypedArray will be reflected on this
+ * tensor’s memory.
+ */
 interface TensorConstructor {
-  new(type: element | elementTypeString,
-      shape: number[],
-      tensorData?: number[] | SupportedTypedArray): Tensor;
+  /**
+   * It constructs a tensor using element type and shape. New tensor data
+   * will be allocated by default.
+   * @param type The element type of a new tensor.
+   * @param shape The shape of a new tensor.
+   */
+  new(type: element | elementTypeString, shape: number[]): Tensor;
+  /**
+   * It constructs a tensor using element type and shape. New tensor wraps
+   * allocated host memory.
+   * @param type The element type of a new tensor.
+   * @param shape The shape of a new tensor.
+   * @param tensorData A subclass of TypedArray that will be wrapped
+   * by a {@link Tensor}.
+   */
+  new(type: element | elementTypeString, shape: number[],
+    tensorData: SupportedTypedArray): Tensor;
 }
 
+/**
+ * An {@link InferRequest} object is created using
+ * {@link CompiledModel.createInferRequest} method and is specific for a given
+ * deployed model. It is used to make predictions and it can be run in
+ * asynchronous or synchronous manners.
+ */
 interface InferRequest {
-  setTensor(name: string, tensor: Tensor): void;
-  setInputTensor(idxOrTensor: number | Tensor, tensor?: Tensor): void;
-  setOutputTensor(idxOrTensor: number | Tensor, tensor?: Tensor): void;
-  getTensor(nameOrOutput: string | Output): Tensor;
-  getInputTensor(idx?: number): Tensor;
-  getOutputTensor(idx?: number): Tensor;
-  infer(inputData?: { [inputName: string]: Tensor | SupportedTypedArray}
-    | Tensor[] | SupportedTypedArray[]): { [outputName: string] : Tensor};
+  /**
+   * It infers specified input(s) in the synchronous mode.
+   * @remarks
+   * Inputs have to be specified earlier using {@link InferRequest.setTensor}
+   * or {@link InferRequest.setInputTensor}
+   */
+  infer(): { [outputName: string] : Tensor};
+  /**
+   * It infers specified input(s) in the synchronous mode.
+   * @param inputData An object with the key-value pairs where the key is the
+   * input name and value can be either a tensor or a TypedArray. TypedArray
+   * will be wrapped into Tensor underneath using deployed model's input shape
+   * and element type.
+   */
+  infer(inputData: { [inputName: string]: Tensor | SupportedTypedArray})
+    : { [outputName: string] : Tensor};
+  /**
+   * It infers specified input(s) in the synchronous mode.
+   * @param inputData An array with tensors or TypedArrays. TypedArrays will be
+   * wrapped into Tensors underneath using the deployed model's input shape
+   * and element type. Tensors and TypedArrays have to be passed in the correct
+   * order if the model has multiple inputs.
+   */
+  infer(inputData: Tensor[] | SupportedTypedArray[])
+    : { [outputName: string] : Tensor};
+  /**
+   * It infers specified input(s) in the asynchronous mode.
+   * @param inputData An object with the key-value pairs where the key is the
+   * input name and value is a tensor or an array with tensors. Tensors have to
+   * be passed in the correct order if the model has multiple inputs.
+   */
   inferAsync(inputData: { [inputName: string]: Tensor}
     | Tensor[] ): Promise<{ [outputName: string] : Tensor}>;
+  /**
+   * It gets the compiled model used by the InferRequest object.
+   */
   getCompiledModel(): CompiledModel;
+  /**
+   * It gets an input tensor for inference.
+   * @returns The input tensor for the model. If the model has several inputs,
+   * an exception is thrown.
+   */
+  getInputTensor(): Tensor;
+  /**
+   * It gets an input tensor for inference.
+   * @param idx An index of the tensor to get.
+   * @returns A tensor at the specified index. If the tensor with the specified
+   * idx is not found, an exception is thrown.
+   */
+  getInputTensor(idx: number): Tensor;
+  /**
+   * It gets an output tensor for inference.
+   * @returns The output tensor for the model. If the model has several outputs,
+   * an exception is thrown.
+   */
+  getOutputTensor(): Tensor;
+ /**
+   * It gets an output tensor for inference.
+   * @param idx An index of the tensor to get.
+   * @returns A tensor at the specified index. If the tensor with the specified
+   * idx is not found, an exception is thrown.
+   */
+  getOutputTensor(idx?: number): Tensor;
+  /**
+   * It gets an input/output tensor for inference.
+   *
+   * @remarks
+   * If the tensor with the specified name or port is not found, an exception
+   * is thrown.
+   * @param nameOrOutput A tensor name or output object.
+   */
+  getTensor(nameOrOutput: string | Output): Tensor;
+  /**
+   * It sets the input tensor to infer models with a single input.
+   * @param tensor The input tensor. The element type and shape of the tensor
+   * must match the model’s input element type and size. If the model has several
+   * inputs, an exception is thrown.
+   */
+  setInputTensor(tensor: Tensor): void;
+  /**
+   * It sets the input tensor to infer.
+   * @param idx The input tensor index. If idx is greater than the number of
+   * model inputs, an exception is thrown.
+   * @param tensor The input tensor. The element type and shape of the tensor
+   * must match the model’s input element type and size.
+   */
+  setInputTensor(idx: number, tensor: Tensor): void;
+  /**
+   * It sets an output tensor to infer models with a single output.
+   * @param tensor The output tensor. The element type and shape of the tensor
+   * must match the model’s output element type and size. If the model has several
+   * outputs, an exception is thrown.
+   */
+  setOutputTensor(tensor: Tensor): void;
+  /**
+   * It sets the output tensor to infer.
+   * @param idx The output tensor index.
+   * @param tensor The output tensor. The element type and shape of the tensor
+   * must match the model’s output element type and size.
+   */
+  setOutputTensor(idx: number, tensor: Tensor): void;
+  /**
+   * It sets an input/output tensor to infer on.
+   * @param name The input or output tensor name.
+   * @param tensor The tensor. The element type and shape of the tensor
+   * must match the model’s input/output element type and size.
+   */
+  setTensor(name: string, tensor: Tensor): void;
 }
 
 type Dimension = number | [number, number];
