ce7db95d8a04f91d87d775348d8d5f7b33feb518
Ivan Tikhonov <ivan.tikhonov@intel.com>
2024-05-30 14:33:58 +0400

    Use Slice operation directly on CPU and update RoPE patterns (#24379)
    ### Details:
     - SliceToStridedSlice was moved from MOC to Common transformations.
     - Updated RoPE fusion patterns
     - 2 new transformations:  EliminateSlice, SliceSequenceToSingleSlice
    - Updated EliminateStridedSlice transformation to support int32_max
    case, deleted transformation duplicate from CPU
     - Added new unit tests
    
    Tested locally with these models:
    
    - [x] Means that Model compilation passed, and all RoPE related
    transformations were applied in the same order
    - [x] hf-internal-testing/tiny-random-StableLmForCausalLM
    - [x] hf-internal-testing/tiny-random-FalconForCausalLM
    - [x] hf-internal-testing/tiny-random-Starcoder2ForCausalLM
    - [x] hf-internal-testing/tiny-random-LlamaForCausalLM
    - [x] hf-internal-testing/tiny-random-GPTNeoXForCausalLM
    - [x] hf-internal-testing/tiny-random-GPTJForCausalLM
    - [x] hf-internal-testing/tiny-random-CodeGenForCausalLM
    - [x] hf-internal-testing/tiny-random-MistralForCausalLM
    - [x] hf-internal-testing/tiny-random-PhiForCausalLM
    - [x] Qwen/Qwen1.5-7B
    - [x] THUDM/chatglm3-6b
    - [x] EleutherAI/gpt-neox-20b
    - [x] google/gemma-2b-it
    - [x] EleutherAI/gpt-j-6b
    - [x] meta-llama/Meta-Llama-3-8B
    - [x] mistralai/Mistral-7B-v0.1
    
    ### Tickets:
     - *CVS-126971*

--- src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
+++ src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
@@ -784,7 +784,6 @@ void Transformations::PostLpt() {
     // Execute before snippets. Otherwise FQ will be converted to Subgraph
     CPU_REGISTER_PASS_X64(postLPTPassManager, ConvertFqRnnToQuantizedRnn);
 
-    CPU_REGISTER_PASS_X64(postLPTPassManager, ov::pass::EliminateStridedSlice);
     CPU_REGISTER_PASS_X64(postLPTPassManager, ov::pass::RoPEFusion);
     CPU_REGISTER_PASS_X64(postLPTPassManager, CausalMaskPreprocessFusion);
 
