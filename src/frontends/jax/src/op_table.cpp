1554204c6a39c69d345b91f00b6dab78f605a030
Rinne <AsakusaRinne@gmail.com>
2024-06-08 23:32:16 +0800

    Fix ci errors.

--- src/frontends/jax/src/op_table.cpp
+++ src/frontends/jax/src/op_table.cpp
@@ -12,18 +12,17 @@ namespace frontend {
 namespace jax {
 namespace op {
 
-#define OP_CONVERTER(op) OutputVector op(const NodeContext &node)
+#define OP_CONVERTER(op) OutputVector op(const NodeContext& node)
 
 OP_CONVERTER(translate_constant);
 
-} // namespace op
+}  // namespace op
 
 // Supported ops for Jaxpr
 const std::map<std::string, CreatorFunction> get_supported_ops_jaxpr() {
-  return {{"add", op::translate_1to1_match_2_inputs<opset10::Add>},
-          {"constant", op::translate_constant}};
+    return {{"add", op::translate_1to1_match_2_inputs<opset10::Add>}, {"constant", op::translate_constant}};
 };
 
-} // namespace jax
-} // namespace frontend
-} // namespace ov
+}  // namespace jax
+}  // namespace frontend
+}  // namespace ov
