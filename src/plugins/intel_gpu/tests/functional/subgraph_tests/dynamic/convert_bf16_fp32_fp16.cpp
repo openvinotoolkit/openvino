5b14f1fcc50a2fda09ed57576fbcd5031386e62f
michal-miotk <michal.miotk@intel.com>
2024-06-10 13:45:41 +0200

    attempt to make test

---
+++ src/plugins/intel_gpu/tests/functional/subgraph_tests/dynamic/convert_bf16_fp32_fp16.cpp
@@ -0,0 +1,138 @@
+// Copyright (C) 2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "common_test_utils/ov_tensor_utils.hpp"
+#include "shared_test_classes/base/ov_subgraph.hpp"
+#include "transformations/rt_info/decompression.hpp"
+
+#include "openvino/op/parameter.hpp"
+#include "openvino/op/constant.hpp"
+#include "openvino/op/result.hpp"
+#include "openvino/op/matmul.hpp"
+#include "openvino/op/reshape.hpp"
+#include "openvino/op/convert.hpp"
+#include "openvino/op/subtract.hpp"
+#include "openvino/op/transpose.hpp"
+
+namespace {
+using ov::test::InputShape;
+
+struct ShapeParams {
+    ShapeParams() = default;
+    ShapeParams(InputShape data_shape, ov::Shape weights_shape, int weights_group_size = -1)
+        : data_shape(std::move(data_shape)),
+          weights_shape(std::move(weights_shape)),
+          weights_group_size(weights_group_size) {}
+
+    InputShape data_shape;
+    ov::Shape weights_shape;
+    // Decompression group size. If the value is equal to -1, ordinary decompression is used
+    int weights_group_size;
+};
+
+using MatmulWeightsDecompressionParams = std::tuple<ShapeParams,              // input shapes
+                                                    ov::element::Type,        // weights type
+                                                    ov::element::Type,        // activations type
+                                                    bool,                     // transpose on weights
+                                                    bool,                     // decompression subtract
+                                                    bool,                     // reshape on decompression constants
+                                                    bool>;                    // per-tensor zero-point
+
+class BF16WeightsDecompression : public testing::WithParamInterface<MatmulWeightsDecompressionParams>,
+                                   virtual public ov::test::SubgraphBaseTest {
+public:
+    ov::CompiledModel cM;
+    static std::string get_test_case_name(testing::TestParamInfo<MatmulWeightsDecompressionParams> obj) {
+        ShapeParams shape_params;
+        ov::element::Type weights_precision;
+        ov::element::Type activations_precision;
+        bool transpose;
+        bool decompression_sub;
+        bool reshape_on_decompression;
+        bool per_tensor_zp;
+
+        std::tie(shape_params,
+                 weights_precision,
+                 activations_precision,
+                 transpose,
+                 decompression_sub,
+                 reshape_on_decompression,
+                 per_tensor_zp) = obj.param;
+
+        std::ostringstream result;
+        result << "data_shape=";
+        result << ov::test::utils::partialShape2str({shape_params.data_shape.first}) << "_";
+        for (const auto& actual_shape : shape_params.data_shape.second) {
+            result << ov::test::utils::partialShape2str({actual_shape}) << "_";
+        }
+        result << "_" << "weights_shape=" << shape_params.weights_shape << "_";
+        result << "group_size=" << shape_params.weights_group_size << "_";
+        result << "weights_precision=" << weights_precision << "_";
+        result << "activations_precision=" << activations_precision << "_";
+        result << "transpose_weights=" << transpose << "_";
+        result << "decompression_subtract=" << decompression_sub << "_";
+        result << "reshape_on_decompression=" << reshape_on_decompression << "_";
+        result << "per_tensor_zp=" << per_tensor_zp;
+
+        return result.str();
+    }
+
+protected:
+    std::shared_ptr<ov::Model> init_subgraph() {
+        ov::ParameterVector params{std::make_shared<ov::op::v0::Parameter>(ov::element::bf16, ov::Shape{{2, 2}})};
+        auto r = std::make_shared<ov::op::v0::Convert>(params[0], ov::element::f32);
+        r->set_friendly_name("konwersja_bf16_na_f32");
+        const auto weights_subgraph = init_compressed_weights_subgraph();
+        auto matmul_res = std::make_shared<ov::op::v0::MatMul>(weights_subgraph, r);
+        auto end = std::make_shared<ov::op::v0::Convert>(matmul_res, ov::element::f16);
+        end->set_friendly_name("konwersja_fp32_na_f16");
+        return std::make_shared<ov::Model>(ov::NodeVector{end}, params, "BF16WeightsDecompression");
+    }
+
+    std::shared_ptr<ov::Node> init_compressed_weights_subgraph() {
+        auto weights_tensor_left = ov::test::utils::create_and_fill_tensor(ov::element::f32, ov::Shape{{2, 2}});
+        auto weights_left = std::make_shared<ov::op::v0::Constant>(weights_tensor_left);
+        return weights_left;
+    }
+
+    void SetUp() override {
+        init_input_shapes({ov::test::InputShape{ov::PartialShape{3, 3}, 3}, ov::test::InputShape{ov::PartialShape{3, 3}, 5}});
+        targetDevice = ov::test::utils::DEVICE_GPU;
+         std::cout << " initi " << __LINE__ << std::endl;
+        ShapeParams shape_params;
+        ov::element::Type weights_precision;
+        ov::element::Type activations_precision;
+
+        std::cout << " initi " << __LINE__ << std::endl;
+        inType = outType = activations_precision;
+
+        function = init_subgraph();
+
+        std::cout << " initi " << std::endl;
+        if (activations_precision == ov::element::f16) {
+            abs_threshold = 1.0f;
+        } else {
+            abs_threshold = 1e-4f;
+        }
+        cM = core->compile_model(function, targetDevice, {ov::hint::inference_precision(ov::element::f32)});
+    }
+
+    void check_results() {
+        int counter = 0;
+        std::cout << " size is " << cM.get_runtime_model()->get_ordered_ops().size() << std::endl;
+        for ( const auto& n : cM.get_runtime_model()->get_ordered_ops() ) {
+            std::cout << "analyzing " << counter << " " << n->get_friendly_name() << std::endl;
+            if ( counter > 0 ) {
+                std::cout << "input " << n->get_input_element_type(0) << std::endl;
+            }
+            std::cout << " output " << n->get_output_element_type(0) << std::endl;
+            counter++;
+        }
+    }
+};
+
+TEST_F(BF16WeightsDecompression, Inferenceheh) {
+    check_results();
+}
+} // namespace
