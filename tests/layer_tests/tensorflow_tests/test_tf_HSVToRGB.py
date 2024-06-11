153c9ab93cbf01bc7b71f7c7b2ef377b81092582
Roman Kazantsev <roman.kazantsev@intel.com>
2024-06-11 11:51:18 +0400

    Update tests/layer_tests/tensorflow_tests/test_tf_HSVToRGB.py

--- tests/layer_tests/tensorflow_tests/test_tf_HSVToRGB.py
+++ tests/layer_tests/tensorflow_tests/test_tf_HSVToRGB.py
@@ -19,7 +19,7 @@ class TestHSVToRGB(CommonTFLayerTest):
             images_shape = inputs_info['images:0']
             images_shape[-1] = 1
             inputs_data = {}
-            inputs_data['images:0'] = np.tile([0,0,.5], images_shape).astype(self.input_type)
+            inputs_data['images:0'] = np.tile([0, 0, 0.5], images_shape).astype(self.input_type)
         else:
             images_shape = inputs_info['images:0']
             inputs_data = {} 
