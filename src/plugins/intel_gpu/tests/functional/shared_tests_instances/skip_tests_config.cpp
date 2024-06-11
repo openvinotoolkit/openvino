1c62e2162cee7f62df699cbcba956feaa5e75d87
Alina Kladieva <alina.kladieva@intel.com>
2024-06-11 14:56:17 +0200

    Skip on Windows only

--- src/plugins/intel_gpu/tests/functional/shared_tests_instances/skip_tests_config.cpp
+++ src/plugins/intel_gpu/tests/functional/shared_tests_instances/skip_tests_config.cpp
@@ -201,13 +201,13 @@ std::vector<std::string> disabledTestPatterns() {
             R"(.*smoke_RDFT_5d_last_axis/RDFTLayerTest.Inference/IS=\(10.4.8.2.5\)_modelType=f32_Axes=\(0.1.2.3.4\)_SignalSize=\(\).*)",
             // Issue: 136862
             R"(.*smoke_ConditionGPUTest_static/StaticConditionLayerGPUTest.CompareWithRefs/IS=\(3.6\)_netPRC=i8_ifCond=PARAM_targetDevice=GPU_.*)",
-            // TODO: Issue: 142899
-            R"(.*smoke_multipleIterations_stateful_gather_with_initializer.*)",
 
 #if defined(_WIN32)
             // by calc abs_threshold with expected value
             R"(.*smoke_RemoteTensor/OVRemoteTensorBatched_Test.NV12toBGR_buffer/(num_batch_4|num_batch_2).*)",
             R"(.*smoke_Check/ConstantResultSubgraphTest.Inference/SubgraphType=SINGLE_COMPONENT_IS=\[1,3,10,10\]_IT=i16_Device=GPU.*)",
+            // TODO: Issue: 142899
+            R"(.*smoke_multipleIterations_stateful_gather_with_initializer.*)",
 #endif
     };
 }
