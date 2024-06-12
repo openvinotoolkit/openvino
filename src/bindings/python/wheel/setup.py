ca27d43b783986f9684f0dc4886c02e2ff8cf339
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2024-06-12 11:19:51 +0200

    fixed reset cpack for multiconfiguration

--- src/bindings/python/wheel/setup.py
+++ src/bindings/python/wheel/setup.py
@@ -277,14 +277,15 @@ class CustomBuild(build):
                 if not os.path.exists(binary_dir):
                     binary_dir = os.path.join(self.build_temp, binary_dir)
                     self.announce(f"Configuring {comp} cmake project", level=3)
-                    self.spawn(["cmake", f"-DOpenVINODeveloperPackage_DIR={OPENVINO_BINARY_DIR}",
-                                         f"-DPython3_EXECUTABLE={sys.executable}",
-                                         '-DCPACK_GENERATOR=WHEEL',
-                                         f"-DCMAKE_BUILD_TYPE={CONFIG}",
-                                         "-DENABLE_WHEEL=OFF",
-                                         self.cmake_args,
-                                         "-S", source_dir,
-                                         "-B", binary_dir])
+                    self.spawn(["cmake",
+                                f"-DOpenVINODeveloperPackage_DIR={OPENVINO_BINARY_DIR}",
+                                f"-DPython3_EXECUTABLE={sys.executable}",
+                                '-DCPACK_GENERATOR=WHEEL',
+                                f"-DCMAKE_BUILD_TYPE={CONFIG}",
+                                "-DENABLE_WHEEL=OFF",
+                                self.cmake_args,
+                                "-S", source_dir,
+                                "-B", binary_dir])
 
                     self.announce(f"Building {comp} project", level=3)
                     self.spawn(["cmake", "--build", binary_dir,
@@ -310,11 +311,13 @@ class CustomBuild(build):
         # install clibs into a temporary directory (site-packages)
         if not PYTHON_EXTENSIONS_ONLY:
             self.run_command("build_clib")
-            
+
         self.announce(f"revert cpack_generator {CPACK_GENERATOR}", level=3)
-        self.spawn(["cmake", f"-DCPACK_GENERATOR={CPACK_GENERATOR}",
-                            "-S", OPENVINO_SOURCE_DIR,
-                            "-B", OPENVINO_BINARY_DIR])
+        self.spawn(["cmake",
+                    f"-DCPACK_GENERATOR={CPACK_GENERATOR}",
+                    f"-DCMAKE_BUILD_TYPE={CONFIG}",
+                    "-S", OPENVINO_SOURCE_DIR,
+                    "-B", OPENVINO_BINARY_DIR])
 
         # Copy extra package_data content filtered by 'find_packages'
         exclude = ignore_patterns("*ez_setup*", "*__pycache__*", "*.egg-info*")
