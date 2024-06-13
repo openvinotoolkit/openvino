14687dd227a8fcda3a4b9adb39f8a30a4526b6b1
Luo Cheng <cheng.luo@intel.com>
2024-06-13 15:43:56 +0800

    [CPU] Fix sdpa pattern match when adding result (#24854)
    ### Details:
     - *Adding results to a stateful model leads to a crash.*
     - Add test case for(Depending on #24954):
     ```
    ReadValue->Gather->ShapeOf->...
                   |------->Concat->SDPA...
    ```
    
    
    ### Tickets:
     - *[143098](https://jira.devtools.intel.com/browse/CVS-143098)*

--- src/plugins/intel_cpu/tests/unit/transformations/state_concat_sdpa.cpp
+++ src/plugins/intel_cpu/tests/unit/transformations/state_concat_sdpa.cpp
@@ -24,7 +24,22 @@ using namespace ov;
 using namespace ov::intel_cpu;
 using namespace ov::gen_pattern;
 
-static std::shared_ptr<ov::Model> makeSDPA(const ov::PartialShape& inputShape, bool isRef = false, bool hasConvert = false, bool hasMultiquery = false) {
+namespace {
+    enum InsertPoint {
+        At_None,
+        At_Convert,
+        At_Gather,
+        At_MQ_Unsqueeze,
+        At_MQ_Broadcast,
+        At_MQ_Multiply,
+        At_MQ_Reshape,
+        At_End
+    };
+} // namespace
+
+static std::shared_ptr<ov::Model> makeSDPA(const ov::PartialShape& inputShape, bool isRef = false, bool hasConvert = false, bool hasMultiquery = false,
+    InsertPoint at = InsertPoint::At_None) {
+    std::shared_ptr<ov::op::v0::Result> insert_result;
     auto q = std::make_shared<ov::op::v0::Parameter>(element::f32, inputShape);
     auto kvInputShape = inputShape;
     if (hasMultiquery) {
@@ -44,6 +59,10 @@ static std::shared_ptr<ov::Model> makeSDPA(const ov::PartialShape& inputShape, b
     if (hasConvert) {
         pastk = std::make_shared<ov::op::v0::Convert>(pastk, element::f32);
         pastv = std::make_shared<ov::op::v0::Convert>(pastv, element::f32);
+        if (at == InsertPoint::At_Convert) {
+            // insert one should be enough to cover the 'convert' case
+            insert_result = std::make_shared<ov::op::v0::Result>(pastk);
+        }
     }
     if (isRef) {
         ov::intel_cpu::ScaledDotProductAttentionWithKVCache::Config config;
@@ -54,6 +73,9 @@ static std::shared_ptr<ov::Model> makeSDPA(const ov::PartialShape& inputShape, b
         concatV = new_node->output(2);
     } else {
         pastk = std::make_shared<ov::op::v8::Gather>(pastk, beam_idx, op::v0::Constant::create(element::i32, {1}, {0}));
+        if (at == InsertPoint::At_Gather) {
+            insert_result = std::make_shared<ov::op::v0::Result>(pastk);
+        }
         pastv = std::make_shared<ov::op::v8::Gather>(pastv, beam_idx, op::v0::Constant::create(element::i32, {1}, {0}));
         concatK = std::make_shared<ov::op::v0::Concat>(OutputVector{pastk, k}, 2);
         concatV = std::make_shared<ov::op::v0::Concat>(OutputVector{pastv, v}, 2);
@@ -62,6 +84,9 @@ static std::shared_ptr<ov::Model> makeSDPA(const ov::PartialShape& inputShape, b
                 auto beam_idx_shape = makeOP<ov::op::TypeRelaxed<opset1::ShapeOf>>({beam_idx},
                     {{"type_relax", true}, {"input_data_types", {}}, {"output_data_types", {element::i32}}});
                 auto unsqueeze_concat = makeOP<opset1::Unsqueeze>({conat, 2});
+                if (at == InsertPoint::At_MQ_Unsqueeze && !insert_result) {
+                    insert_result = std::make_shared<ov::op::v0::Result>(unsqueeze_concat);
+                }
                 auto concat_shape = makeOP<ov::op::TypeRelaxed<opset1::ShapeOf>>(
                     {conat},
                     {{"type_relax", true}, {"input_data_types", {}}, {"output_data_types", {element::i32}}});
@@ -72,10 +97,19 @@ static std::shared_ptr<ov::Model> makeSDPA(const ov::PartialShape& inputShape, b
                 auto expand_ones = makeOP<opset1::Broadcast>({{1.0f},
                     expand_Abs,
                     axis_mapping}, {{"mode", "numpy"}});
+                if (at == InsertPoint::At_MQ_Broadcast && !insert_result) {
+                    insert_result = std::make_shared<ov::op::v0::Result>(expand_ones);
+                }
                 auto expand_Broadcast = makeOP<opset1::Multiply>({unsqueeze_concat,
                     expand_ones}, {{"auto_broadcast", "numpy"}});
+                if (at == InsertPoint::At_MQ_Multiply && !insert_result) {
+                    insert_result = std::make_shared<ov::op::v0::Result>(expand_Broadcast);
+                }
                 auto expected_shape = makeOP<opset1::Concat>({beam_idx_shape, {inputShape[1]}, gather_ls}, {{"axis", 0}});
                 auto reshape_Reshape = makeOP<opset1::Reshape>({expand_Broadcast, expected_shape}, {{"special_zero", false}});
+                if (at == InsertPoint::At_MQ_Reshape && !insert_result) {
+                    insert_result = std::make_shared<ov::op::v0::Result>(reshape_Reshape);
+                }
                 return reshape_Reshape;
             };
             sdp = std::make_shared<ov::opset13::ScaledDotProductAttention>(q, make_multi_query(concatK), make_multi_query(concatV), false);
@@ -92,6 +126,8 @@ static std::shared_ptr<ov::Model> makeSDPA(const ov::PartialShape& inputShape, b
     auto add = std::make_shared<op::v1::Add>(sdp, op::v0::Constant::create(element::f32, {1}, {1.0f}));
 
     ResultVector results{std::make_shared<ov::op::v0::Result>(add)};
+    if (insert_result)
+        results.push_back(insert_result);
     SinkVector sinks{pastk_assign, pastv_assign};
     return std::make_shared<Model>(results, sinks, ParameterVector{q, k, v, init, beam_idx}, "ConcatSDP");
 }
@@ -156,5 +192,28 @@ TEST(TransformationTests, StateConcatSDPAMixtral) {
         }
         auto res = compare_functions(f, f_ref);
         ASSERT_TRUE(res.first) << res.second;
+    }
+}
+
+TEST(TransformationTests, StateConcatSDPAWithExtraNode) {
+    // when some unexpected extra nodes exist in SDPA, the fusion should fail
+    std::shared_ptr<ov::Model> f(nullptr), f_ref(nullptr);
+    {
+        using namespace ov;
+        auto inputShape = ov::PartialShape{-1, 32, -1, 64};
+        size_t i = static_cast<size_t>(InsertPoint::At_None) + 1;
+        size_t end = static_cast<size_t>(InsertPoint::At_End);
+        // check each position
+        for (; i < end; i++) {
+            InsertPoint at = static_cast<InsertPoint>(i);
+            f = makeSDPA(inputShape, false, true, true, at);
+            f_ref = makeSDPA(inputShape, false, true, true, at);
+            pass::Manager m;
+            m.register_pass<ov::pass::InitNodeInfo>();
+            m.register_pass<StatefulSDPAFusion>();
+            m.run_passes(f);
+            auto res = compare_functions(f, f_ref);
+            ASSERT_TRUE(res.first) << res.second;
+        }
     }
 }
