effb00d41c11eb5ef5664b789ec72284c40607de
Ivan Novoselov <ivan.novoselov@intel.com>
2024-06-18 16:48:52 +0100

    Isolate executor config interface

--- src/plugins/intel_cpu/src/emitters/snippets/x64/kernel_executors/brgemm.cpp
+++ src/plugins/intel_cpu/src/emitters/snippets/x64/kernel_executors/brgemm.cpp
@@ -21,35 +21,46 @@ namespace intel_cpu {
 BrgemmKernelConfig::BrgemmKernelConfig(const element::Type& in0_dtype, const element::Type& in1_dtype, float beta,
                                        bool is_with_amx, bool is_with_comp,
                                        size_t M, size_t N, size_t K, size_t LDA, size_t LDB, size_t LDC) :
-                     dt_in0(DTYPE_CAST(in0_dtype)), dt_in1(DTYPE_CAST(in1_dtype)),
-                     is_with_amx(is_with_amx), is_with_comp(is_with_comp), beta(beta),
-                     M(DIM_CAST(M)), N(DIM_CAST(N)), K(DIM_CAST(K)),
-                     LDA(DIM_CAST(LDA)), LDB(DIM_CAST(LDB)), LDC(DIM_CAST(LDC)) {
-    bool is_int8 = utils::one_of(dt_in0, data_type::u8, data_type::s8) &&
-                   utils::one_of(dt_in1, data_type::u8, data_type::s8);
-    isa = is_with_amx ?
+                     m_dt_in0(DTYPE_CAST(in0_dtype)), m_dt_in1(DTYPE_CAST(in1_dtype)),
+                     m_is_with_amx(is_with_amx), m_is_with_comp(is_with_comp), m_beta(beta),
+                     m_M(DIM_CAST(M)), m_N(DIM_CAST(N)), m_K(DIM_CAST(K)),
+                     m_LDA(DIM_CAST(LDA)), m_LDB(DIM_CAST(LDB)), m_LDC(DIM_CAST(LDC)) {
+    bool is_int8 = utils::one_of(m_dt_in0, data_type::u8, data_type::s8) &&
+                   utils::one_of(m_dt_in1, data_type::u8, data_type::s8);
+    m_isa = is_with_amx ?
             cpu::x64::avx512_core_amx :
-            dt_in0 == dnnl_data_type_t::dnnl_bf16 ?
+            m_dt_in0 == dnnl_data_type_t::dnnl_bf16 ?
                 cpu::x64::avx512_core_bf16 :
                 is_int8 ?
                     cpu::x64::avx512_core_vnni :
                     cpu::x64::avx512_core;
-    // Note: hash could be initialized only after ias
     m_hash = compute_hash();
 }
 
 bool BrgemmKernelConfig::is_completed() const {
-    return !utils::one_of(0, M, N, K, LDA, LDB, LDC);
+    return !utils::one_of(0, m_M, m_N, m_K, m_LDA, m_LDB, m_LDC);
+}
+
+void BrgemmKernelConfig::update(dnnl_dim_t M, dnnl_dim_t N, dnnl_dim_t K, dnnl_dim_t LDA, dnnl_dim_t LDB, dnnl_dim_t LDC) {
+    m_M = M; m_N = N; m_K = K;
+    m_LDA = LDA; m_LDB = LDB; m_LDC = LDC;
+    m_hash = compute_hash();
+}
+
+BrgemmKernelConfig::operator amx_tile_config_t() const {
+    amx_tile_config_t res;
+    res.M = m_M; res.N = m_N; res.K = m_K;
+    return  res;
 }
 
 size_t BrgemmKernelConfig::compute_hash() const {
     size_t seed = 0;
 #define HASH(X) seed = hash_combine(seed, X)
-    HASH(dt_in0); HASH(dt_in1);
-    HASH(is_with_amx); HASH(is_with_comp);
-    HASH(beta); HASH(isa);
-    HASH(M); HASH(N); HASH(K);
-    HASH(LDA); HASH(LDB); HASH(LDC);
+    HASH(m_dt_in0); HASH(m_dt_in1);
+    HASH(m_is_with_amx); HASH(m_is_with_comp);
+    HASH(m_beta); HASH(m_isa);
+    HASH(m_M); HASH(m_N); HASH(m_K);
+    HASH(m_LDA); HASH(m_LDB); HASH(m_LDC);
 #undef HASH
     return seed;
 }
@@ -76,17 +87,17 @@ BrgemmKernelExecutor::BrgemmKernelExecutor(ov::intel_cpu::MultiCacheWeakPtr kern
 std::shared_ptr<BrgemmCompiledKernel> BrgemmKernelExecutor::compile_kernel(const std::shared_ptr<const BrgemmKernelConfig>& config) const {
     OV_CPU_JIT_EMITTER_ASSERT(config, "Invalid config provided for BrgemmKernelDesc::compile_kernel");
     cpu::x64::brgemm_t desc;
-    auto status = brgemm_desc_init(&desc, config->isa, cpu::x64::brgemm_strd,
-                                   config->dt_in0, config->dt_in1,
+    auto status = brgemm_desc_init(&desc, config->get_isa(), cpu::x64::brgemm_strd,
+                                   config->get_dt_in0(), config->get_dt_in1(),
                                    false, false, cpu::x64::brgemm_row_major, 1.f,
-                                   config->beta,
-                                   config->LDA, config->LDB, config->LDC,
-                                   config->M, config->N, config->K, nullptr);
+                                   config->get_beta(),
+                                   config->get_LDA(), config->get_LDB(), config->get_LDC(),
+                                   config->get_M(), config->get_N(), config->get_K(), nullptr);
 
     std::shared_ptr<BrgemmCompiledKernel> compiled_kernel = std::make_shared<BrgemmCompiledKernel>();
 
     OV_CPU_JIT_EMITTER_ASSERT(status == dnnl_success, "Cannot initialize brgemm descriptor due to invalid params");
-    if (config->is_with_amx) {
+    if (config->is_with_amx()) {
         status = brgemm_init_tiles(desc, compiled_kernel->palette);
         OV_CPU_JIT_EMITTER_ASSERT(status == dnnl_success, "Cannot initialize brgemm tiles due to invalid params");
     }
@@ -118,24 +129,23 @@ void BrgemmKernelExecutor::update_config(const ov::snippets::lowered::Expression
                               "Invalid number of in/out port descriptors");
     // Update runtime-defined config fields:
     // Matrix A (first input)
-    config->LDA = DIM_CAST(snippets::utils::get_in_leading_dim(input_pds[0]));
+    const auto LDA = DIM_CAST(snippets::utils::get_in_leading_dim(input_pds[0]));
     const auto& in0_subtensor = get_projected_input_subtensor(input_pds[0]);
-    config->K = DIM_CAST(*in0_subtensor.rbegin());
-    config->M = DIM_CAST(*++in0_subtensor.rbegin());
+    const auto K = DIM_CAST(*in0_subtensor.rbegin());
+    const auto M = DIM_CAST(*++in0_subtensor.rbegin());
     // Matrix B (second input)
     // Non float input 1 => with data repacking
-    if (config->dt_in1 != dnnl_f32) {
+    auto LDB = DIM_CAST(snippets::utils::get_in_leading_dim(input_pds[1]));
+    if (config->get_dt_in1() != dnnl_f32) {
         const auto& brgemm_node = as_type_ptr<ov::intel_cpu::BrgemmCPU>(expr->get_node());
         const auto repacking_buffer_shape = brgemm_node->get_brgemm_copy()->get_repacking_buffer_shape();
         OV_CPU_JIT_EMITTER_ASSERT(!repacking_buffer_shape.empty(), "Repacking buffer shape mustn't be empty");
-        config->LDB = DIM_CAST(repacking_buffer_shape.back());
-    } else {
-        config->LDB = DIM_CAST(snippets::utils::get_in_leading_dim(input_pds[1]));
+        LDB = DIM_CAST(repacking_buffer_shape.back());
     }
-    config->N = DIM_CAST(*get_projected_input_subtensor(input_pds[1]).rbegin());
+    const auto N = DIM_CAST(*get_projected_input_subtensor(input_pds[1]).rbegin());
     // Matrix C (output)
-    config->LDC = DIM_CAST(snippets::utils::get_out_leading_dim(output_pds[0]));
-    config->m_hash = config->compute_hash();
+    const auto LDC = DIM_CAST(snippets::utils::get_out_leading_dim(output_pds[0]));
+    config->update(M, N, K, LDA, LDB, LDC);
 }
 
 void BrgemmKernelExecutor::execute(const BrgemmKernelExecutor* desc, call_args* args) {
@@ -143,14 +153,9 @@ void BrgemmKernelExecutor::execute(const BrgemmKernelExecutor* desc, call_args* 
     const auto& config = std::static_pointer_cast<const BrgemmKernelConfig>(desc->get_config());
     OV_CPU_JIT_EMITTER_ASSERT(kernel && config, "has nullptr compiler kernel or invalid config");
 
-    if (config->is_with_amx) {
-        const auto& amx_tile_config = args->amx_tile_config;
-        if (config->M != amx_tile_config->M || config->K != amx_tile_config->K || config->N != amx_tile_config->N) {
-            amx_tile_config->M = config->M;
-            amx_tile_config->K = config->K;
-            amx_tile_config->N = config->N;
-            cpu::x64::amx_tile_configure(kernel->palette);
-        }
+    if (config->is_with_amx() && config->compatible(args->amx_tile_config)) {
+        *args->amx_tile_config = static_cast<amx_tile_config_t>(*config);
+        cpu::x64::amx_tile_configure(kernel->palette);
     }
 
     cpu::x64::brgemm_kernel_params_t brgemm_p;
@@ -162,8 +167,8 @@ void BrgemmKernelExecutor::execute(const BrgemmKernelExecutor* desc, call_args* 
     brgemm_p.ptr_D = args->C;
     brgemm_p.ptr_buf = args->scratch;
     brgemm_p.ptr_bias = nullptr;
-    brgemm_p.do_post_ops = static_cast<size_t>(config->is_with_comp);
-    brgemm_p.do_apply_comp = static_cast<size_t>(config->is_with_comp);
+    brgemm_p.do_post_ops = static_cast<size_t>(config->is_with_comp());
+    brgemm_p.do_apply_comp = static_cast<size_t>(config->is_with_comp());
     brgemm_p.skip_accm = 0;
     brgemm_p.BS = 1;  // default value
     OV_CPU_JIT_EMITTER_ASSERT(kernel->compiled_kernel, "has nullptr kernel");
