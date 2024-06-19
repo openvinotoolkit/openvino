6d4f06038483dd1e04cdcf68e84534e01c538c99
Pawel Raasz <pawel.raasz@intel.com>
2024-06-19 11:16:27 +0200

    [core] Fix Coverity issues in shape inference library (#25004)
    ### Details:
     - Fix reported issues by Coverity in shape inference library.
    
    ### Tickets:
    - Part of
    [CVS-141674](https://jira.devtools.intel.com/browse/CVS-141674)

--- src/core/shape_inference/include/experimental_detectron_prior_grid_generator_shape_inference.hpp
+++ src/core/shape_inference/include/experimental_detectron_prior_grid_generator_shape_inference.hpp
@@ -21,7 +21,8 @@ std::vector<TRShape> shape_infer(const ExperimentalDetectronPriorGridGenerator* 
     const auto is_flatten = op->get_attrs().flatten;
     const size_t output_size = is_flatten ? 2 : 4;
 
-    TRShape output_shape;
+    auto output_shapes = std::vector<TRShape>(1);
+    auto& output_shape = output_shapes[0];
     output_shape.resize(output_size);
     output_shape[output_size - 1] = 4;
 
@@ -69,7 +70,7 @@ std::vector<TRShape> shape_infer(const ExperimentalDetectronPriorGridGenerator* 
         }
     }
 
-    return {output_shape};
+    return output_shapes;
 }
 }  // namespace v6
 }  // namespace op
