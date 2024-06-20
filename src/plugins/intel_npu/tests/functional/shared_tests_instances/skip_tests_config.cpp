ca9caf33109ba434f44a8f91330041beab0a8b5e
Wenjing Kang <wenjing.kang@intel.com>
2024-06-20 15:21:41 +0800

    Fix compareTensors for u4/i4 failure (#24802)
    ### Details:
     - Update the logic in `compareTensors` when bitwidth is less than 8.
     - Enable the OVInferRequestCheckTensorPrecision tests for i4/u4.
    
    ### Tickets:
     - *142251*
    
    ---------
    
    Signed-off-by: Kang Wenjing <wenjing.kang@intel.com>
    Co-authored-by: Pawel Raasz <pawel.raasz@intel.com>

--- src/plugins/intel_npu/tests/functional/shared_tests_instances/skip_tests_config.cpp
+++ src/plugins/intel_npu/tests/functional/shared_tests_instances/skip_tests_config.cpp
@@ -273,8 +273,6 @@ std::vector<std::string> disabledTestPatterns() {
                 ".*InferRequestCheckTensorPrecision.*type=boolean.*",
                 ".*InferRequestCheckTensorPrecision.*type=bf16.*",
                 ".*InferRequestCheckTensorPrecision.*type=f64.*",
-                ".*InferRequestCheckTensorPrecision.*type=i4.*",
-                ".*InferRequestCheckTensorPrecision.*type=u4.*",
                 ".*InferRequestCheckTensorPrecision.*type=u1\\D.*",
                 // [Track number: E#97469]
                 ".*InferRequestCheckTensorPrecision.*type=i64.*",
