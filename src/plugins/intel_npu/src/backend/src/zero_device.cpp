9a4732b4512d331be5f0a6104ac15134980a98b7
Attila Csok <attila.csok@intel.com>
2024-06-21 01:09:18 -0700

    [intel-npu] Adding npu4000 string to zero device (#25142)
    ### Details:
    - Adding npu4000 id string temporarily for 2024.3 since PR #24607 still
    needs work
     - Will be replaced once PR #24607 gets merged
    
    ### Tickets:
     - none

--- src/plugins/intel_npu/src/backend/src/zero_device.cpp
+++ src/plugins/intel_npu/src/backend/src/zero_device.cpp
@@ -95,6 +95,7 @@ std::string ZeroDevice::getName() const {
 #define NPU_3700_DEVICE_ID   0x6240
 #define NPU_3720_P_DEVICE_ID 0x7D1D
 #define NPU_3720_S_DEVICE_ID 0xAD1D
+#define NPU_4000_DEVICE_ID   0x643E
 
     std::string name;
     switch (device_properties.deviceId) {
@@ -104,6 +105,9 @@ std::string ZeroDevice::getName() const {
     case NPU_3720_P_DEVICE_ID:
     case NPU_3720_S_DEVICE_ID:
         name = ov::intel_npu::Platform::NPU3720;
+        break;
+    case NPU_4000_DEVICE_ID:
+        name = "4000";
         break;
     default:
         name = "AUTO_DETECT";
