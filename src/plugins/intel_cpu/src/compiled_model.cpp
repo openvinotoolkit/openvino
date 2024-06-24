59836c50710b36decc343b5ee134fe0bd5f3bfd0
Vladislav Golubev <vladislav.golubev@intel.com>
2024-06-24 10:15:36 +0200

    [CPU] Avoid shared weights double repacking in latency mode (#24386)
    ### Details:
     - *Weights caching enabled in single stream mode*
     - *Avoid unnecessary weights repacking on single socket machines*
    
    ### Tickets:
     - *CVS-139671*

--- src/plugins/intel_cpu/src/compiled_model.cpp
+++ src/plugins/intel_cpu/src/compiled_model.cpp
@@ -130,13 +130,11 @@ CompiledModel::GraphGuard::Lock CompiledModel::get_graph() const {
                 GraphContext::Ptr ctx;
                 {
                     std::lock_guard<std::mutex> lock{*m_mutex.get()};
-                    // disable weights caching if graph was created only once
-                    auto weightsCache = m_cfg.streamExecutorConfig.get_streams() != 1 ? m_socketWeights[socketId] : nullptr;
                     auto isQuantizedFlag =
                         (m_cfg.lpTransformsMode == Config::On) &&
                         ov::pass::low_precision::LowPrecision::isFunctionQuantized(m_model);
 
-                    ctx = std::make_shared<GraphContext>(m_cfg, weightsCache, isQuantizedFlag, streamsExecutor);
+                    ctx = std::make_shared<GraphContext>(m_cfg, m_socketWeights[socketId], isQuantizedFlag, streamsExecutor);
                 }
                 const std::shared_ptr<const ov::Model> model = m_model;
                 graphLock._graph.CreateGraph(model, ctx);
