f17e9b6318db0ef2ea23268cd20a76b3b47117be
Andrew Park <andrew.park@intel.com>
2024-06-25 17:43:30 +0900

    Fix failure for in-place crop dyn TC

--- src/plugins/intel_gpu/src/graph/primitive_inst.cpp
+++ src/plugins/intel_gpu/src/graph/primitive_inst.cpp
@@ -1016,9 +1016,11 @@ void primitive_inst::update_paddings() {
         return;
     }
     // Reset paddings used in the previous iteration for crop before executing do_runtime_in_place_crop
-    if (_node->is_type<crop>() && _impl_params->output_layouts[0].data_padding.get_dynamic_pad_dims() != tensor(0)) {
-        if (_node->can_be_optimized()) {
-            reset_pad(*_impl_params, _node);
+    for (auto u : get_user_insts()) {
+        if (u->get_node().is_type<crop>() && u->_impl_params->output_layouts[0].data_padding.get_dynamic_pad_dims() != tensor(0)) {
+            if (u->get_node().can_be_optimized()) {
+                reset_pad(*u->_impl_params, u->_node);
+            }
         }
     }
 }
