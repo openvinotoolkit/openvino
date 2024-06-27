3dee531d654d45674b8be32e631d6390f7e9e932
Nikolay Shchegolev <nikolay.shchegolev@intel.com>
2024-06-27 21:16:19 +0400

    [CPU] Limit f16 in Accuracy mode by hardware support (#25243)
    ### Details:
    - *Restrict usage of f16 in transformations pipeline if it's not
    supported by hardware for ACCURACY MODE*
    
    ### Tickets:
     - *145051*

--- src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
+++ src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
@@ -356,7 +356,8 @@ void Transformations::PreLpt(const std::vector<ov::element::Type>& defaultPrecis
         // @todo should we always convert to f32 regardless of hardware support, as it is done for f16?
         if (!hasHardwareSupport(ov::element::bf16))
             map.insert({ov::element::bf16, ov::element::f32});
-        if (!one_of(inferencePrecision, element::f16, element::undefined)) {
+        // TODO: Remove 'hasHardwareSupport' when all nodes are able to handle f16 properly.
+        if (!one_of(inferencePrecision, element::f16, element::undefined) || !hasHardwareSupport(element::f16)) {
             map.insert({ov::element::f16, ov::element::f32});
         }
         return map;
