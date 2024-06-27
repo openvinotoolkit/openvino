openvinotoolkit/openvino

commit 169eeec48f57ddf14ed7226c564f10d3fca005a8

Ability to disable ApiVdalidator search (#25069)
### Details:
 - In some cases API validator search is not robust 

### Tickets:
 - Closes https://github.com/microsoft/vcpkg/issues/38221

cmake/developer_package/api_validator/api_validator.cmake
@@ -2,7 +2,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-if(WIN32)
+function(ov_search_api_validator)
+    if(NOT ENABLE_API_VALIDATOR)
+        return()
+    endif()
+
     # CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION is only set when
     # Visual Studio generators are used, but we need it
     # when we use Ninja as well
@@ -11,8 +15,10 @@ if(WIN32)
             string(REPLACE "\\" "" WINDOWS_SDK_VERSION $ENV{WindowsSDKVersion})
             set(CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION ${WINDOWS_SDK_VERSION})
             message(STATUS "Use ${CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION} Windows SDK version")
+            # set to parent scope as well for later usage in '_ov_add_api_validator_post_build_step'
+            set(CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION ${WINDOWS_SDK_VERSION} PARENT_SCOPE)
         else()
-          message(FATAL_ERROR "WindowsSDKVersion environment variable is not set,\
+            message(FATAL_ERROR "WindowsSDKVersion environment variable is not set,\
 can't find Windows SDK version. Try to use vcvarsall.bat script")
         endif()
     endif()
@@ -41,7 +47,9 @@ can't find Windows SDK version. Try to use vcvarsall.bat script")
             message(STATUS "Found apivalidator: ${ONECORE_API_VALIDATOR}")
         endif()
     endif()
-endif()
+endfunction()
+
+ov_search_api_validator()
 
 function(_ov_add_api_validator_post_build_step_recursive)
     cmake_parse_arguments(API_VALIDATOR "" "TARGET" "" ${ARGN})

cmake/features.cmake
@@ -192,6 +192,8 @@ ov_dependent_option(ENABLE_JS "Enables JS API building" ON "NOT ANDROID;NOT EMSC
 
 ov_option(ENABLE_OPENVINO_DEBUG "Enable output for OPENVINO_DEBUG statements" OFF)
 
+ov_dependent_option (ENABLE_API_VALIDATOR "Enables API Validator usage" ON "WIN32" OFF)
+
 if(NOT BUILD_SHARED_LIBS AND ENABLE_OV_TF_FRONTEND)
     set(FORCE_FRONTENDS_USE_PROTOBUF ON)
 else()

commit 107bbdd4478484d4179793abe56db68a5a0b5e2e

Fixed regression with apiValidator search (#25252)
### Details:
- Regression after
https://github.com/openvinotoolkit/openvino/pull/25069
- With `function` some vars are bounded to its scope, while they are
required for functions below

cmake/developer_package/api_validator/api_validator.cmake
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-function(ov_search_api_validator)
+macro(ov_search_api_validator)
     if(NOT ENABLE_API_VALIDATOR)
         return()
     endif()
@@ -15,8 +15,6 @@ function(ov_search_api_validator)
             string(REPLACE "\\" "" WINDOWS_SDK_VERSION $ENV{WindowsSDKVersion})
             set(CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION ${WINDOWS_SDK_VERSION})
             message(STATUS "Use ${CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION} Windows SDK version")
-            # set to parent scope as well for later usage in '_ov_add_api_validator_post_build_step'
-            set(CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION ${WINDOWS_SDK_VERSION} PARENT_SCOPE)
         else()
             message(FATAL_ERROR "WindowsSDKVersion environment variable is not set,\
 can't find Windows SDK version. Try to use vcvarsall.bat script")
@@ -47,9 +45,12 @@ can't find Windows SDK version. Try to use vcvarsall.bat script")
             message(STATUS "Found apivalidator: ${ONECORE_API_VALIDATOR}")
         endif()
     endif()
-endfunction()
+endmacro()
 
-ov_search_api_validator()
+
+if(ENABLE_API_VALIDATOR)
+    ov_search_api_validator()
+endif()
 
 function(_ov_add_api_validator_post_build_step_recursive)
     cmake_parse_arguments(API_VALIDATOR "" "TARGET" "" ${ARGN})