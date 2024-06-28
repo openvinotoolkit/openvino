213e3a94990b90c4834f61b4b69e3321883869f3
Michal Miotk <michal.miotk@intel.com>
2024-06-28 10:20:12 +0200

    fixed types in test

--- src/plugins/intel_gpu/tests/unit/passes/test_module_fusing_reorder.cpp
+++ src/plugins/intel_gpu/tests/unit/passes/test_module_fusing_reorder.cpp
@@ -266,7 +266,7 @@ TEST_P(test_can_fuse_reorder_onednn, reorder_for_firstconv_onednn)
     layout conv_layout(p.input_data_type, p.output_format, p.out_shape, padding({0, }, 0));
     layout reorder_layout(p.output_data_type, p.output_format, p.out_shape, padding({0, }, 0));
     auto input = engine.allocate_memory({ p.input_data_type, p.input_format, p.in_shape });
-    auto weights = engine.allocate_memory({ p.input_data_type, p.input_format, p.weight_shape });
+    auto weights = engine.allocate_memory({ p.input_data_type, p.weights_format, p.weight_shape });
 
     topology.add(input_layout("input", input->get_layout()));
     topology.add(data("weights", weights));
@@ -297,10 +297,10 @@ TEST_P(test_can_fuse_reorder_onednn, reorder_for_firstconv_onednn)
 
 INSTANTIATE_TEST_SUITE_P(testing_can_fuse_reorder_first_conv, test_can_fuse_reorder_onednn,
                         ::testing::ValuesIn(std::vector<reorder_test_param>{
-                                            reorder_test_param{format::b_fs_yx_fsv16, format::b_fs_yx_fsv32, data_types::f32, data_types::u8, {1, 3, 8, 8}, {1, 32, 8, 8}, {1, 3, 1, 1},
-                                                tensor{1}, tensor{0}, data_types::u8, format::goiyx, true},
-                                            reorder_test_param{format::b_fs_yx_fsv4, format::b_fs_yx_fsv16, data_types::f32, data_types::f16, {1, 3, 8, 8}, {1, 32, 8, 8}, {1, 3, 1, 1},
-                                                tensor{1}, tensor{0}, data_types::f16, format::goiyx, true},
+                                            reorder_test_param{format::bs_fs_yx_bsv8_fsv4, format::b_fs_yx_fsv32, data_types::f32, data_types::u8, {1, 3, 8, 8}, {1, 32, 8, 8}, {1, 3, 1, 1},
+                                                tensor{1}, tensor{0}, data_types::u8, format::oiyx, true},
+                                            reorder_test_param{format::bs_fs_yx_bsv8_fsv2, format::b_fs_yx_fsv16, data_types::f32, data_types::f16, {1, 3, 8, 8}, {1, 32, 8, 8}, {1, 3, 1, 1},
+                                                tensor{1}, tensor{0}, data_types::f16, format::oiyx, true},
                                             }));
 
 class can_fuse_reorder : public ::testing::TestWithParam<std::tuple<data_types, format>> {};
