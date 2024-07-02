146955ecda454328e508e1c3727b3fac0e810717
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2024-07-02 19:35:52 +0400

    [GPU] Serialize micro kernel settings (#25319)
    ### Details:
    - Add microkernels settings serialization/deserialization logic to be
    able to use it after model import in update dispatch data functions
     - Limit head size for micro SDPA by 256.
    
    ### Tickets:
     - *CVS-145786*
     - *CVS-145787*

--- src/plugins/intel_gpu/src/kernel_selector/micro_utils.hpp
+++ src/plugins/intel_gpu/src/kernel_selector/micro_utils.hpp
@@ -6,6 +6,9 @@
 
 #ifdef ENABLE_ONEDNN_FOR_GPU
 
+#include "intel_gpu/graph/serialization/binary_buffer.hpp"
+#include "intel_gpu/graph/serialization/string_serializer.hpp"
+
 #ifdef UNUSED
 #   undef UNUSED
 #endif
@@ -32,13 +35,37 @@ using SizeParams = dnnl::impl::gpu::intel::jit::SizeParams;
 using StrategyRequirement = dnnl::impl::gpu::intel::jit::StrategyRequirement;
 using ShimOptions = dnnl::impl::gpu::intel::micro::ShimOptions;
 using HostLanguage = dnnl::impl::gpu::intel::micro::HostLanguage;
+using Setting = dnnl::impl::gpu::intel::micro::Setting;
 
 // Wrapper for Package which is used in clKernelData with forward declaration
 // to avoid including this header in many places in plugin
 // which may cause symbols conflicts with oneDNN
 struct MicroKernelPackage {
+    MicroKernelPackage() = default;
     explicit MicroKernelPackage(Package _p) : p(_p) {}
     Package p;
+
+    // WARNING: We serialize only microkernels settings, so after deserialization
+    // other struct fields are not initializer properly and can't be used
+    void save(cldnn::BinaryOutputBuffer& ob) const {
+        ob << p.settings.size();
+        for (auto& s : p.settings) {
+            ob << s.name;
+            ob << s.value;
+        }
+    }
+
+    void load(cldnn::BinaryInputBuffer& ib) {
+        size_t n_settings;
+        ib >> n_settings;
+        p.settings.clear();
+        for (size_t i = 0; i < n_settings; i++) {
+            Setting s;
+            ib >> s.name;
+            ib >> s.value;
+            p.settings.push_back(s);
+        }
+    }
 };
 
 inline Package select_gemm_microkernel(GEMMProtocol protocol, HWInformation hw_info, SizeParams sizes, const GEMMProblem &problem,
