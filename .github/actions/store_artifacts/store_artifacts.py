16c402fe999abcc35dd513bbcc120b1e9f1b97e8
Alina Kladieva <alina.kladieva@intel.com>
2024-07-02 13:00:13 +0200

    Use store_artifacts action

--- .github/actions/store_artifacts/store_artifacts.py
+++ .github/actions/store_artifacts/store_artifacts.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import os
+import re
 import sys
 import shutil
 from contextlib import contextmanager
@@ -10,6 +11,7 @@ from pathlib import Path
 def parse_args():
     parser = argparse.ArgumentParser(description='Returns product components changed in a given PR or commit')
     parser.add_argument('-e', '--event_name', help='Name of GitHub event', required=False)
+    parser.add_argument('-b', '--branch_name', help='Name of GitHub branch', required=False)
     parser.add_argument('-s', '--commit_sha', help='Commit hash for which artifacts were generated', required=False)
     parser.add_argument('-a', '--artifacts', type=str, help='Paths to artifacts to store (files/dirs)', required=True)
     parser.add_argument('--storage_dir', help='Directory name to store artifacts in', required=True)
@@ -68,11 +70,16 @@ def main():
     args = parse_args()
 
     event_name = args.event_name or os.getenv('GITHUB_EVENT_NAME')
+    branch_name = args.branch_name or os.getenv('GITHUB_BASE_REF') or os.getenv('GITHUB_REF_NAME')
+    merge_queue_matcher = re.search(r'gh-readonly-queue/(.*?)/pr-', branch_name)
+    if merge_queue_matcher:
+        branch_name = merge_queue_matcher.group(1)
+
     commit_hash = args.commit_sha or os.getenv('GITHUB_EVENT_PULL_REQUEST_HEAD_SHA') or os.getenv('GITHUB_SHA')
     event_type = 'pre_commit' if event_name == 'pull_request' else 'commit'
     storage_root = args.storage_root or os.getenv('ARTIFACTS_SHARE')
 
-    storage = Path(storage_root) / 'dldt' / event_type / commit_hash / args.storage_dir
+    storage = Path(storage_root) / 'dldt' / branch_name / event_type / commit_hash / args.storage_dir
     logger.info(f"Storing artifacts to {storage}")
     rotate_dir(storage)  # TODO: use more stable approach to handle storing artifacts from re-runs
 
@@ -95,10 +102,10 @@ def main():
     if github_server:  # If running from GHA context
         repository = os.getenv('GITHUB_REPOSITORY')
         run_id = os.getenv('GITHUB_RUN_ID')
-        job_id = os.getenv('GITHUB_JOB')
-        job_link = f"{github_server}/{repository}/actions/runs/{run_id}/job/{job_id}"
-        with open(storage / 'job_link.txt', 'w') as file:
-            file.write(job_link)
+        # TODO: write an exact job link, but it's not trivial to get
+        workflow_link = f"{github_server}/{repository}/actions/runs/{run_id}"
+        with open(storage / 'workflow_link.txt', 'w') as file:
+            file.write(workflow_link)
 
     (storage / 'copying_finished').touch()
     if error_found:
