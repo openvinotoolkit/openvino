a8f794cf98b3278f2a03c5510c01d1b8684e8a8b
Evgeniia Nugmanova <evgeniia.nugmanova@intel.com>
2024-07-03 15:43:06 +0400

    More comments for Symbol-related classes

--- src/core/include/openvino/core/symbol.hpp
+++ src/core/include/openvino/core/symbol.hpp
@@ -4,14 +4,6 @@
 
 #pragma once
 
-#include <algorithm>
-#include <memory>
-#include <ostream>
-#include <string>
-#include <unordered_map>
-#include <utility>
-#include <vector>
-
 #include "openvino/core/core_visibility.hpp"
 #include "openvino/core/symbol_utils.hpp"
 
@@ -28,24 +20,49 @@ std::shared_ptr<Symbol> OPENVINO_API ancestor_of(const SharedSymbol& x);
 SharedSymbol OPENVINO_API operator+(const SharedSymbol& lhs, const SharedSymbol& rhs);
 SharedSymbol OPENVINO_API operator-(const SharedSymbol& lhs, const SharedSymbol& rhs);
 
-/// \brief Class representing unique symbol for the purpose of symbolic shape inference. Equality of symbols is being
-/// tracked by Disjoint-set data structure
+/// \brief Class representing unique symbol for the purpose of symbolic shape inference
 /// \ingroup ov_model_cpp_api
 class OPENVINO_API Symbol {
 public:
     /// \brief Default constructs a unique symbol
     Symbol() = default;
+
+    /// @brief Destructor ensures shared MathMaps are cleared from the expired symbols
     ~Symbol();
 
 private:
+    /*
+     * Equality relations between symbols are managed by the Disjoint-set data structure.
+     * m_parent field gives access to a parent tree. Parent tree grows when we use set_equal routine.
+     *
+     * The root of parent tree is called ancestor -- a representative of a group of equal symbols.
+     * To get an ancestor of any symbol, call ancestor_of routine. If current symbol is an ancestor, then its m_parent
+     * field is nullptr and ancestor_of would return current symbol.
+     * */
+
+    SharedSymbol m_parent = nullptr;
+
     friend SharedSymbol ov::symbol::ancestor_of(const SharedSymbol& x);
     friend void ov::symbol::set_equal(const SharedSymbol& lhs, const SharedSymbol& rhs);
 
+    /*
+     * Shared MathMap represents mathematical relations between symbols. Rules:
+     * - map stores only ancestor symbols
+     * - all the Symbols in the map must share the same map as their respective field
+     *
+     * To ensure rules, set_equal routine and operators are managing maps of both symbols.
+     *
+     * MathMap m_add reflects addition and subtraction. Given symbols A, B, C and D
+     * - relation A + B = C would be reflected as key={A, B} value=C in the map
+     * - relation A - B = C would be reflected as key={B, C} value=A in the map
+     * - relation A + B + C = D would be reflected as key={A, B, C} value=D in the map
+     *
+     * During destruction of a symbol, m_add map is being cleared from the records with expired symbols
+     */
+    std::shared_ptr<symbol::MathMap> m_add;
+
     friend SharedSymbol ov::operator+(const SharedSymbol& lhs, const SharedSymbol& rhs);
     friend SharedSymbol ov::operator-(const SharedSymbol& lhs, const SharedSymbol& rhs);
-
-    std::shared_ptr<symbol::MathMap> m_add;
-    SharedSymbol m_parent = nullptr;
 };
 
 }  // namespace ov
--- src/core/include/openvino/core/symbol_utils.hpp
+++ src/core/include/openvino/core/symbol_utils.hpp
@@ -6,12 +6,15 @@
 
 #include <algorithm>
 #include <memory>
+#include <unordered_map>
 #include <vector>
 
 namespace ov {
 class Symbol;
 using SharedSymbol = std::shared_ptr<ov::Symbol>;
 namespace symbol {
+
+/// @brief Helper class to represent Symbol in a MathMap. Using weak_ptr is necessary to avoid cross-links
 class WeakSymbol : public std::weak_ptr<ov::Symbol> {
 public:
     WeakSymbol(const SharedSymbol& a) : std::weak_ptr<ov::Symbol>(a) {}
@@ -22,37 +25,39 @@ public:
             return false;
         return lock().get() == other.lock().get();
     }
-
     bool operator!=(const std::weak_ptr<ov::Symbol>& other) const {
         return !(*this == other);
     }
-
     bool operator<(const std::weak_ptr<ov::Symbol>& rhs) const {
         return std::owner_less<>()(lock(), rhs);
     }
 };
 
+/**
+ * @brief Helper class to represent key of a MathMap -- vector of weak symbols. Vector must be sorted, so that search in
+ * a MathMap would be smooth
+ */
 class WeakSymbolVector : public std::vector<WeakSymbol> {
 public:
     WeakSymbolVector() = default;
     WeakSymbolVector(std::initializer_list<WeakSymbol> init) : std::vector<WeakSymbol>(init) {
         sort();
     };
-
     WeakSymbolVector(const std::vector<WeakSymbol>& init) : std::vector<WeakSymbol>(init) {
         sort();
     };
 
+    /// @brief Syntactic sugar method to sort vector of weak symbols
     void sort() {
         std::sort(begin(), end());
     }
-
+    /// @brief Syntactic sugar method to find out if vector contains a symbol
     bool has(const WeakSymbol& s) const {
         return std::any_of(cbegin(), cend(), [&s](const ov::symbol::WeakSymbol& i) {
             return i == s;
         });
     }
-
+    /// @brief Syntactic sugar method to find out if vector contains a symbol
     bool has(const ov::Symbol* s) const {
         return std::any_of(cbegin(), cend(), [&s](const ov::symbol::WeakSymbol& i) {
             if (i.lock() == nullptr)
@@ -60,7 +65,7 @@ public:
             return i.lock().get() == s;
         });
     }
-
+    /// @brief Syntactic sugar method to replace old_s symbols, if any, with new_s symbols, keeping the vector sorted
     void replace(const WeakSymbol& old_s, const WeakSymbol& new_s) {
         std::replace_if(
             begin(),
@@ -75,9 +80,8 @@ public:
     bool operator==(const ov::symbol::WeakSymbolVector& other) const {
         if (size() != other.size())
             return false;
-        return std::equal(begin(), end(), other.begin());
+        return std::equal(cbegin(), cend(), other.cbegin());
     }
-
     bool operator<(const ov::symbol::WeakSymbolVector& other) const {
         size_t common_size = std::min(size(), other.size());
         for (size_t i = 0; i < common_size; ++i) {
@@ -87,7 +91,9 @@ public:
         return size() < other.size();
     }
 };
+
 using MathMap = std::unordered_map<ov::symbol::WeakSymbolVector, ov::symbol::WeakSymbol>;
+
 }  // namespace symbol
 }  // namespace ov
 
