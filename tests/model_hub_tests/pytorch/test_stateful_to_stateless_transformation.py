f9531d751e6a03d4b7fef321ad8f8f058bb0ad90
Sergey Lyalin <sergey.lyalin@intel.com>
2024-07-03 17:09:35 +0000

    StatefulToStateless: Decoding naming conventions to form new inputs/outpus names and restore their order. Extended test to compare stateful vs. stateless produced by the vanilla optimum-intel.

--- tests/model_hub_tests/pytorch/test_stateful_to_stateless_transformation.py
+++ tests/model_hub_tests/pytorch/test_stateful_to_stateless_transformation.py
@@ -11,6 +11,18 @@ import os
 def get_read_value_ops(model: ov.Model):
     return [op for op in model.get_ops() if op.get_type_name() == 'ReadValue']
 
+def check_desc_tensors(tensors1, tensors2):
+    # order of tensors may not match, comparing by the total amount and names
+    assert len(tensors1) == len(tensors2)
+    assert set(tuple(t.names) for t in tensors1) == set(tuple(t.names) for t in tensors2)
+    for t1 in tensors1:
+        t2_candidates = [t for t in tensors2 if t1.names & t.names]
+        assert len(t2_candidates) == 1
+        t2 = t2_candidates[0]
+        assert t1.names == t2.names
+        assert t1.get_partial_shape() == t2.get_partial_shape()
+        assert t1.get_element_type() == t2.get_element_type()
+
 def run_stateful_to_stateless_in_runtime(tmp_path, model_id, model_link):
     model = OVModelForCausalLM.from_pretrained(model_id, export=True, stateful=True, compile=False)
     assert len(model.model.get_sinks()), f"Input model is not in the expected stateful form because it doesn't have any sinks."
@@ -22,8 +34,16 @@ def run_stateful_to_stateless_in_runtime(tmp_path, model_id, model_link):
     read_value_ops = get_read_value_ops(model.model)
     assert len(sink_ops) == 0, f"Expected stateless model, but there are sinks found: {sink_ops}"
     assert len(read_value_ops) == 0, f"Expected stateless model, but there are ReadValue operations found: {read_value_ops}"
-    # TODO: check names of inputs/output, their count and topological equivalence in respect to the entire model
-    # TODO: compare with originally stateless model
+
+    stateless_model = OVModelForCausalLM.from_pretrained(model_id, export=True, stateful=False, compile=False)
+
+    print(model.model)
+    print(stateless_model.model)
+    check_desc_tensors(model.model.inputs, stateless_model.model.inputs)
+    check_desc_tensors(model.model.outputs, stateless_model.model.outputs)
+
+    core = ov.Core()
+    core.compile_model(model.model, 'CPU')
 
 
 @pytest.mark.precommit
