5e27816219292d1275187e318bd0f482ecae99b7
Alina Kladieva <alina.kladieva@intel.com>
2024-07-03 17:27:28 +0200

    Save artifacts storage path to output

--- .github/actions/store_artifacts/store_artifacts.py
+++ .github/actions/store_artifacts/store_artifacts.py
@@ -26,6 +26,15 @@ def init_logger():
                         datefmt='%m-%d-%Y %H:%M:%S')
 
 
+def set_github_output(name: str, value: str, github_output_var_name: str = 'GITHUB_OUTPUT'):
+    """Sets output variable for a GitHub Action"""
+    logger = logging.getLogger(__name__)
+    # In an environment variable "GITHUB_OUTPUT" GHA stores path to a file to write outputs to
+    with open(os.environ.get(github_output_var_name), 'a+') as file:
+        logger.info(f"Add {name}={value} to {github_output_var_name}")
+        print(f'{name}={value}', file=file)
+
+
 @contextmanager
 def preserve_stats_context():
     """
@@ -80,6 +89,8 @@ def main():
     storage_root = args.storage_root or os.getenv('ARTIFACTS_SHARE')
 
     storage = Path(storage_root) / 'dldt' / branch_name / event_type / commit_hash / args.storage_dir
+    set_github_output("artifacts_storage_path", str(storage))
+
     logger.info(f"Storing artifacts to {storage}")
     rotate_dir(storage)  # TODO: use more stable approach to handle storing artifacts from re-runs
 
