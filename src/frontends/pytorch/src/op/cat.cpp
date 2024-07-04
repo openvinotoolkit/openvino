openvinotoolkit/openvino

commit 09383664cc93031ca7c4a0c92f4e683e47f3593e

[PT FE] aten::cat input [static & dynamic] type alignment via ConvertPromoteTypes (#25077)
### Details:
- aten::cat input **static** and **dynamic** type alignment via
ConvertPromoteTypes
- This PR introduce proposal of inserting ConvertPromoteTypes
recursively for PT FE torch::cat inputs with static and dynamic types,
it is based on the PR with types alignment for the static types:
 https://github.com/openvinotoolkit/openvino/pull/24934
- Enablement of dynamic types adjustment is a small modification of the
conditions (commit
https://github.com/openvinotoolkit/openvino/pull/25077/commits/a8229453e24782804e94f85fa63580f669d99a20)
and can affect existing models (where the types are initially dynamic,
but have the same type when the types are specialized)
- Tests with `trace_model=False` (dynamic types) and `trace_model=True`
(static types)
 
 

### Tickets:
 - 142388/143028

src/frontends/pytorch/src/op/cat.cpp
@@ -4,6 +4,8 @@
 
 #include "openvino/frontend/pytorch/node_context.hpp"
 #include "openvino/op/concat.hpp"
+#include "openvino/op/convert_like.hpp"
+#include "openvino/op/convert_promote_types.hpp"
 #include "openvino/op/parameter.hpp"
 #include "openvino/op/reshape.hpp"
 #include "openvino/op/scatter_elements_update.hpp"
@@ -56,6 +58,36 @@ OutputVector translate_cat_common(const NodeContext& context,
             context.mark_node(std::make_shared<v12::ScatterElementsUpdate>(shape_sliced, axis_const, neg_1, zero));
         return {context.mark_node(std::make_shared<v1::Reshape>(tensor, new_shape, false))};
     }
+
+    const auto first_in_type = list_elems.front().get_element_type();
+    const bool is_mixed_type =
+        list_elems.size() > 1 && (std::any_of(std::next(list_elems.begin()),
+                                              list_elems.end(),
+                                              [&first_in_type](const ov::Output<ov::Node>& input) {
+                                                  return input.get_element_type() != first_in_type ||
+                                                         input.get_element_type() == ov::element::dynamic;
+                                              }));
+    if (is_mixed_type) {
+        auto node_of_type = list_elems[0];
+        for (size_t i = 1; i < list_elems.size(); ++i) {
+            node_of_type = std::make_shared<v14::ConvertPromoteTypes>(node_of_type, list_elems[i], true)->output(0);
+            context.mark_node(node_of_type.get_node_shared_ptr());
+        }
+
+        const auto unified_type = node_of_type.get_element_type();
+        auto inputs_vec = OutputVector(list_elems.begin(), list_elems.end());
+        for (size_t i = 0; i < inputs_vec.size(); ++i) {
+            if (inputs_vec[i].get_element_type() != unified_type ||
+                inputs_vec[i].get_element_type() == ov::element::dynamic) {
+                inputs_vec[i] =
+                    std::make_shared<v1::ConvertLike>(list_elems[i].get_node_shared_ptr(), node_of_type)->output(0);
+                context.mark_node(inputs_vec[i].get_node_shared_ptr());
+            }
+        }
+        auto concat = std::make_shared<v0::Concat>(inputs_vec, axis);
+        return {context.mark_node(concat)};
+    }
+
     auto concat = std::make_shared<v0::Concat>(OutputVector(list_elems.begin(), list_elems.end()), axis);
     return {context.mark_node(concat)};
 }

tests/layer_tests/pytorch_tests/test_cat.py
@@ -3,6 +3,7 @@
 
 import pytest
 import torch
+import numpy as np
 
 from pytorch_layer_test_class import PytorchLayerTest
 
@@ -70,7 +71,7 @@ class TestCat(PytorchLayerTest):
         concat = [data for _ in range(num_repeats)]
         out = np.zeros_like(np.concatenate(concat, axis=1))
         return (data, out)
-        
+
 
     @pytest.mark.nightly
     @pytest.mark.precommit
@@ -104,3 +105,172 @@ class TestCat(PytorchLayerTest):
         model = aten_add_cat() if not out else aten_add_cat_out()
         self._test(model, None, ["aten::cat", "aten::add", "prim::ListConstruct"],
                    ie_device, precision, ir_version, freeze_model=False,  kwargs_to_prepare_input={"out": out, "num_repeats": 4})
+
+
+class TestCatAlignTypes(PytorchLayerTest):
+    def _prepare_input(self, in_types):
+        in_vals = []
+        for i in range(len(in_types)):
+            dtype = in_types[i]
+            in_vals.append(np.random.randn(2, 1, 3).astype(dtype))
+        return in_vals
+
+    def create_model(self, in_count):
+        class aten_align_types_cat_two_args(torch.nn.Module):
+            def forward(self, x, y):
+                ins = [x, y]
+                return torch.cat(ins, 1)
+
+        class aten_align_types_cat_three_args(torch.nn.Module):
+            def forward(self, x, y, z):
+                ins = [x, y, z]
+                return torch.cat(ins, 1)
+
+        if in_count == 2:
+            return aten_align_types_cat_two_args()
+
+        if in_count == 3:
+            return aten_align_types_cat_three_args()
+
+    @pytest.mark.parametrize(("in_types"), [
+        # Two inputs
+        (np.float32, np.int32),
+        (np.int32, np.float32),
+        (np.float16, np.float32),
+        (np.int16, np.float16),
+        (np.int32, np.int64),
+        # # Three inputs
+        (np.float32, np.int32, np.int32),
+        (np.float32, np.int32, np.float32),
+        (np.int32, np.float32, np.int32),
+        (np.float32, np.int32, np.int16),
+        (np.int32, np.float32, np.int16),
+        (np.int16, np.int32, np.int16),
+        (np.float16, np.float32, np.float16),
+        (np.float32, np.float16, np.float32),
+        (np.float16, np.int32, np.int16),
+        (np.int16, np.float16, np.int16),
+    ])
+    @pytest.mark.parametrize(("trace_model"), [True, False])
+    @pytest.mark.nightly
+    @pytest.mark.precommit
+    def test_align_types_cat(self, ie_device, precision, ir_version, in_types, trace_model):
+        self._test(self.create_model(len(in_types)), None, ["aten::cat", "prim::ListConstruct"],
+                   ie_device, precision, ir_version, kwargs_to_prepare_input={"in_types": in_types}, trace_model=trace_model)
+
+
+class TestCatAlignTypesPT(PytorchLayerTest):
+    def _prepare_input(self, in_types):
+        in_vals = [np.random.randn(2, 1, 3).astype(in_types[0])]
+        return in_vals
+
+    def create_model_param_first(self, in_types):
+        class aten_align_types_cat_two_args(torch.nn.Module):
+            def __init__(self):
+                super(aten_align_types_cat_two_args, self).__init__()
+                self.y = torch.randn(2, 1, 3).to(in_types[1])
+
+            def forward(self, x):
+                ins = [x, self.y]
+                return torch.cat(ins, 1)
+
+        class aten_align_types_cat_three_args(torch.nn.Module):
+            def __init__(self):
+                super(aten_align_types_cat_three_args, self).__init__()
+                self.y = torch.randn(2, 1, 3).to(in_types[1])
+                self.z = torch.randn(2, 1, 3).to(in_types[2])
+
+            def forward(self, x):
+                ins = [x, self.y, self.z]
+                return torch.cat(ins, 1)
+
+        in_count = len(in_types)
+        if in_count == 2:
+            return aten_align_types_cat_two_args()
+
+        if in_count == 3:
+            return aten_align_types_cat_three_args()
+
+    def create_model_param_mid(self, in_types):
+        class aten_align_types_cat_three_args(torch.nn.Module):
+            def __init__(self):
+                super(aten_align_types_cat_three_args, self).__init__()
+                self.x = torch.randn(2, 1, 3).to(in_types[1])
+                self.z = torch.randn(2, 1, 3).to(in_types[2])
+
+            def forward(self, y):
+                ins = [self.x, y, self.z]
+                return torch.cat(ins, 1)
+        return aten_align_types_cat_three_args()
+
+    def create_model_param_last(self, in_types):
+        class aten_align_types_cat_three_args(torch.nn.Module):
+            def __init__(self):
+                super(aten_align_types_cat_three_args, self).__init__()
+                self.x = torch.randn(2, 1, 3).to(in_types[1])
+                self.y = torch.randn(2, 1, 3).to(in_types[2])
+
+            def forward(self, z):
+                ins = [self.x, self.y, z]
+                return torch.cat(ins, 1)
+        return aten_align_types_cat_three_args()
+
+    @pytest.mark.parametrize(("in_types"), [
+        # Two inputs (param, const)
+        (np.float16, torch.bfloat16),
+        (np.float32, torch.bfloat16),
+        (np.float32, torch.int32),
+        (np.int32, torch.float32),
+        (np.float16, torch.float32),
+        (np.float16, torch.float32),
+        (np.int16, torch.float16),
+        (np.int32, torch.int64),
+        # Three inputs (param, const, const)
+        (np.float32, torch.int32, torch.int32),
+        (np.float32, torch.int32, torch.float32),
+        (np.int32, torch.float32, torch.int32),
+        (np.float32, torch.int32, torch.int16),
+        (np.int32, torch.float32, torch.int16),
+        (np.int16, torch.int32, torch.int16),
+        (np.float16, torch.float32, torch.float16),
+        (np.float32, torch.float16, torch.float32),
+        (np.float16, torch.int32, torch.int16),
+        (np.int16, torch.float16, torch.int16),
+        (np.float16, torch.bfloat16, torch.float32),
+    ])
+    @pytest.mark.parametrize(("trace_model"), [True, False])
+    @pytest.mark.nightly
+    @pytest.mark.precommit
+    def test_align_types_cat(self, ie_device, precision, ir_version, in_types, trace_model):
+        self._test(self.create_model_param_first(in_types), None, ["aten::cat", "prim::ListConstruct"],
+                   ie_device, precision, ir_version, kwargs_to_prepare_input={"in_types": in_types}, trace_model=trace_model)
+
+    @pytest.mark.parametrize(("in_types"), [
+        # Three inputs (param, const, const)
+        (np.float16, torch.int32, torch.int16),
+        (np.int16, torch.float16, torch.int16),
+        (np.float16, torch.bfloat16, torch.float32),
+        (np.float16, torch.float32, torch.bfloat16),
+        (np.float32, torch.bfloat16, torch.bfloat16),
+    ])
+    @pytest.mark.parametrize(("trace_model"), [True, False])
+    @pytest.mark.nightly
+    @pytest.mark.precommit
+    def test_align_types_cat_param_mid(self, ie_device, precision, ir_version, in_types, trace_model):
+        self._test(self.create_model_param_mid(in_types), None, ["aten::cat", "prim::ListConstruct"],
+                   ie_device, precision, ir_version, kwargs_to_prepare_input={"in_types": in_types}, trace_model=trace_model)
+
+    @pytest.mark.parametrize(("in_types"), [
+        # Three inputs (param, const, const)
+        (np.float16, torch.int32, torch.int16),
+        (np.int16, torch.float16, torch.int16),
+        (np.float16, torch.bfloat16, torch.float32),
+        (np.float16, torch.float32, torch.bfloat16),
+        (np.float32, torch.bfloat16, torch.bfloat16),
+    ])
+    @pytest.mark.parametrize(("trace_model"), [True, False])
+    @pytest.mark.nightly
+    @pytest.mark.precommit
+    def test_align_types_cat_param_last(self, ie_device, precision, ir_version, in_types, trace_model):
+        self._test(self.create_model_param_last(in_types), None, ["aten::cat", "prim::ListConstruct"],
+                   ie_device, precision, ir_version, kwargs_to_prepare_input={"in_types": in_types}, trace_model=trace_model)

commit dbfed83e368393afa83d98718355ca55bbb6ebdd

[PT FE] Fix issue with type alignment in aten::cat (#25354)
### Details:
 - *Fix regression introduced by #25077*

### Tickets:
 - *CVS-144695*
 - *CVS-144789*

src/frontends/pytorch/src/op/cat.cpp
@@ -67,29 +67,28 @@ OutputVector translate_cat_common(const NodeContext& context,
                                                   return input.get_element_type() != first_in_type ||
                                                          input.get_element_type() == ov::element::dynamic;
                                               }));
+    auto inputs_vec = OutputVector(list_elems.begin(), list_elems.end());
     if (is_mixed_type) {
-        auto node_of_type = list_elems[0];
-        for (size_t i = 1; i < list_elems.size(); ++i) {
-            node_of_type = std::make_shared<v14::ConvertPromoteTypes>(node_of_type, list_elems[i], true)->output(0);
-            context.mark_node(node_of_type.get_node_shared_ptr());
+        auto node_of_type = inputs_vec[0];
+        for (size_t i = 1; i < inputs_vec.size(); ++i) {
+            auto cpt = context.mark_node(std::make_shared<v14::ConvertPromoteTypes>(node_of_type, list_elems[i], true));
+            node_of_type = cpt->output(0);
+            inputs_vec[i] = cpt->output(1);
         }
 
+        inputs_vec[0] = node_of_type;
         const auto unified_type = node_of_type.get_element_type();
-        auto inputs_vec = OutputVector(list_elems.begin(), list_elems.end());
-        for (size_t i = 0; i < inputs_vec.size(); ++i) {
+        for (size_t i = 1; i < inputs_vec.size(); ++i) {
             if (inputs_vec[i].get_element_type() != unified_type ||
                 inputs_vec[i].get_element_type() == ov::element::dynamic) {
-                inputs_vec[i] =
-                    std::make_shared<v1::ConvertLike>(list_elems[i].get_node_shared_ptr(), node_of_type)->output(0);
-                context.mark_node(inputs_vec[i].get_node_shared_ptr());
+                inputs_vec[i] = context.mark_node(std::make_shared<v1::ConvertLike>(list_elems[i], node_of_type));
             }
         }
         auto concat = std::make_shared<v0::Concat>(inputs_vec, axis);
         return {context.mark_node(concat)};
     }
 
-    auto concat = std::make_shared<v0::Concat>(OutputVector(list_elems.begin(), list_elems.end()), axis);
-    return {context.mark_node(concat)};
+    return {context.mark_node(std::make_shared<v0::Concat>(inputs_vec, axis))};
 }
 
 OutputVector translate_cat(const NodeContext& context) {

tests/layer_tests/pytorch_tests/test_cat.py
@@ -161,7 +161,7 @@ class TestCatAlignTypes(PytorchLayerTest):
 
 class TestCatAlignTypesPT(PytorchLayerTest):
     def _prepare_input(self, in_types):
-        in_vals = [np.random.randn(2, 1, 3).astype(in_types[0])]
+        in_vals = [np.random.randn(2, 2, 3).astype(in_types[0])]
         return in_vals
 
     def create_model_param_first(self, in_types):
@@ -171,7 +171,8 @@ class TestCatAlignTypesPT(PytorchLayerTest):
                 self.y = torch.randn(2, 1, 3).to(in_types[1])
 
             def forward(self, x):
-                ins = [x, self.y]
+                x_ = torch.split(x, 1, 1)[1]
+                ins = [x_, self.y]
                 return torch.cat(ins, 1)
 
         class aten_align_types_cat_three_args(torch.nn.Module):
@@ -181,7 +182,8 @@ class TestCatAlignTypesPT(PytorchLayerTest):
                 self.z = torch.randn(2, 1, 3).to(in_types[2])
 
             def forward(self, x):
-                ins = [x, self.y, self.z]
+                x_ = torch.split(x, 1, 1)[1]
+                ins = [x_, self.y, self.z]
                 return torch.cat(ins, 1)
 
         in_count = len(in_types)
@@ -199,7 +201,8 @@ class TestCatAlignTypesPT(PytorchLayerTest):
                 self.z = torch.randn(2, 1, 3).to(in_types[2])
 
             def forward(self, y):
-                ins = [self.x, y, self.z]
+                y_ = torch.split(y, 1, 1)[1]
+                ins = [self.x, y_, self.z]
                 return torch.cat(ins, 1)
         return aten_align_types_cat_three_args()
 
@@ -211,7 +214,8 @@ class TestCatAlignTypesPT(PytorchLayerTest):
                 self.y = torch.randn(2, 1, 3).to(in_types[2])
 
             def forward(self, z):
-                ins = [self.x, self.y, z]
+                z_ = torch.split(z, 1, 1)[1]
+                ins = [self.x, self.y, z_]
                 return torch.cat(ins, 1)
         return aten_align_types_cat_three_args()
 