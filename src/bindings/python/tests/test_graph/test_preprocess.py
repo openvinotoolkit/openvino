5098f8b9e8b12027cea25d1583baed131c1e2d3d
panhaiqi <haiqi.pan@intel.com>
2024-07-04 15:11:45 +0800

    check get_output_shape after preprocess

--- src/bindings/python/tests/test_graph/test_preprocess.py
+++ src/bindings/python/tests/test_graph/test_preprocess.py
@@ -730,9 +730,7 @@ def test_graph_set_layout_by_layout_class_thow_exception():
     assert "Layout name is invalid" in str(e.value)
 
 
-@pytest.mark.parametrize(
-    ("pads_begin", "pads_end", "values", "mode"),
-    [([0, 0, 0, 0], [0, 0, 1, 1], 0, PaddingMode.CONSTANT)])
+@pytest.mark.parametrize(("pads_begin", "pads_end", "values", "mode"), [([0, 0, 0, 0], [0, 0, 1, 1], 0, PaddingMode.CONSTANT)])
 def test_pad_vector_constant_layout(pads_begin, pads_end, values, mode):
     shape = [1, 3, 200, 200]
     parameter_a = ops.parameter(shape, dtype=np.float32, name="RGB_input")
@@ -741,14 +739,12 @@ def test_pad_vector_constant_layout(pads_begin, pads_end, values, mode):
     ppp = PrePostProcessor(model)
     ppp.input().tensor().set_shape([1, 3, 199, 199])
     ppp.input().preprocess().pad(pads_begin, pads_end, values, mode)
-    assert ppp.build()
-    assert list(model.get_output_shape(0)) == shape
+    new_model = ppp.build()
+    assert new_model
+    assert list(new_model.get_output_shape(0)) == shape
 
 
-@pytest.mark.parametrize(
-    ("pads_begin", "pads_end", "values", "mode"),
-    [([0, 0, -2, 0], [0, 0, -4, 1], 0, PaddingMode.CONSTANT)]
-)
+@pytest.mark.parametrize(("pads_begin", "pads_end", "values", "mode"), [([0, 0, -2, 0], [0, 0, -4, 1], 0, PaddingMode.CONSTANT)])
 def test_pad_vector_out_of_range(pads_begin, pads_end, values, mode):
     shape = [1, 3, 5, 5]
     parameter_a = ops.parameter(shape, dtype=np.float32, name="A")
@@ -759,13 +755,9 @@ def test_pad_vector_out_of_range(pads_begin, pads_end, values, mode):
         ppp.input().preprocess().pad(pads_begin, pads_end, values, mode)
         ppp.build()
     assert "not aligned with original parameter's shape" in str(e.value)
-    assert list(model.get_output_shape(0)) == shape
 
 
-@pytest.mark.parametrize(
-    ("pads_begin", "pads_end", "values", "mode"),
-    [([0, 0, 2, 0, 1], [0, 0, 4, 1, 1], 0, PaddingMode.CONSTANT)]
-)
+@pytest.mark.parametrize(("pads_begin", "pads_end", "values", "mode"), [([0, 0, 2, 0, 1], [0, 0, 4, 1, 1], 0, PaddingMode.CONSTANT)])
 def test_pad_vector_dim_mismatch(pads_begin, pads_end, values, mode):
     shape = [1, 3, 5, 5]
     parameter_a = ops.parameter(shape, dtype=np.float32, name="A")
@@ -776,7 +768,6 @@ def test_pad_vector_dim_mismatch(pads_begin, pads_end, values, mode):
         ppp.input().preprocess().pad(pads_begin, pads_end, values, mode)
         ppp.build()
     assert "mismatches with rank of input" in str(e.value)
-    assert list(model.get_output_shape(0)) == shape
 
 
 @pytest.mark.parametrize(("pads_begin", "pads_end", "values", "mode"), [([0, 0, 0, 0], [0, 0, 1, 1], 0, PaddingMode.CONSTANT)])
@@ -788,11 +779,12 @@ def test_pad_vector_type_and_ops(pads_begin, pads_end, values, mode):
     ppp = PrePostProcessor(model)
     ppp.input().tensor().set_shape([1, 3, 199, 199])
     ppp.input().preprocess().pad(pads_begin, pads_end, values, mode)
-    assert ppp.build()
+    new_model = ppp.build()
+    assert new_model
     model_operators = [op.get_name().split("_")[0] for op in model.get_ops()]
     expected_ops = ["Parameter", "Constant", "Result", "Pad"]
-    assert list(model.get_output_shape(0)) == shape
-    assert model.get_output_element_type(0) == Type.f32
+    assert list(new_model.get_output_shape(0)) == shape
+    assert new_model.get_output_element_type(0) == Type.f32
     assert len(model_operators) == 6
     for op in expected_ops:
         assert op in model_operators
