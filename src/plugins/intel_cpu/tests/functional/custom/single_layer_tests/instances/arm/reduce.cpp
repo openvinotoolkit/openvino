1d7033618d575dcf83ee3af8d7f4154142cf0270
Aleksandr Voron <aleksandr.voron@intel.com>
2024-07-05 15:55:07 +0200

    added ref test

--- src/plugins/intel_cpu/tests/functional/custom/single_layer_tests/instances/arm/reduce.cpp
+++ src/plugins/intel_cpu/tests/functional/custom/single_layer_tests/instances/arm/reduce.cpp
@@ -43,12 +43,46 @@ const auto params_MultiAxis_5D = testing::Combine(
         testing::Values(emptyFusingSpec),
         testing::ValuesIn(additionalConfig()));
 
+const std::vector<std::vector<int>> axes5D_ref = {
+        {0}
+};
+
+std::vector<CPUSpecificParams> cpuParams_5D_ref = {
+        CPUSpecificParams({ncdhw}, {ncdhw}, {"ref"}, {"ref"}),
+};
+
+std::vector<std::map<std::string, ov::element::Type>> config_infer_prec_f32 = {
+        {{ov::hint::inference_precision.name(), ov::element::f32}}
+    };
+
+const auto params_MultiAxis_5D_ref = testing::Combine(
+        testing::Combine(
+                testing::ValuesIn(axes5D_ref),
+                testing::Values(ov::test::utils::OpType::VECTOR),
+                testing::Values(true),
+                testing::ValuesIn(reductionTypes()),
+                testing::ValuesIn(inpOutPrc()),
+                testing::Values(ElementType::undefined),
+                testing::Values(ElementType::undefined),
+                testing::ValuesIn(inputShapes_5D)),
+        testing::ValuesIn(filterCPUSpecificParams(cpuParams_5D_ref)),
+        testing::Values(emptyFusingSpec),
+        testing::ValuesIn(config_infer_prec_f32));
+
 //There are dedicated instences of smoke_Reduce_MultiAxis_5D_CPU test in arm and x64 folders
 //because ACL does not support 0 as reduction axis
 INSTANTIATE_TEST_SUITE_P(
         smoke_Reduce_MultiAxis_5D_CPU,
         ReduceCPULayerTest,
         params_MultiAxis_5D,
+        ReduceCPULayerTest::getTestCaseName
+);
+
+// Reference implementation testing of ACL unsupported case
+INSTANTIATE_TEST_SUITE_P(
+        smoke_Reduce_MultiAxis_5D_CPU_ref,
+        ReduceCPULayerTest,
+        params_MultiAxis_5D_ref,
         ReduceCPULayerTest::getTestCaseName
 );
 
