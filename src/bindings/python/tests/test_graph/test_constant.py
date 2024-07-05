24cfeb011a6112a1fc0d32558cb8c000d0cbee26
Barnas, Michal <michal.barnas@intel.com>
2024-07-05 14:43:33 +0200

    nan to inf bug repro test

--- src/bindings/python/tests/test_graph/test_constant.py
+++ src/bindings/python/tests/test_graph/test_constant.py
@@ -480,6 +480,59 @@ def test_float_to_f8e4m3_constant(ov_type, numpy_dtype, opset):
 
     assert np.allclose(result, target, equal_nan=True)
 
+@pytest.mark.parametrize(("opset"), OPSETS)
+@pytest.mark.parametrize(
+    ("ov_type", "numpy_dtype"),
+    [
+        (Type.f32, np.float32),
+        (Type.f16, np.float16),
+    ],
+)
+def test_float_to_f8e8m0_constant_matrix(ov_type, numpy_dtype, opset):
+    shape = (2, 2)
+    data = np.full(shape, np.nan)
+
+    compressed_const = opset.constant(data, dtype=ov_type, name="fx_constant")
+    convert_to_fp8 = opset.convert(compressed_const, Type.f8e8m0)
+    convert_back = opset.convert(convert_to_fp8, ov_type)
+    parameter = opset.parameter(ov.PartialShape([-1, -1]), ov_type)
+    add_op = opset.add(parameter, convert_back)
+    model = ov.Model([add_op], [parameter])
+
+    compiled = ov.compile_model(model, "GPU")
+    tensor = np.zeros(data.shape, dtype=numpy_dtype)
+    result = compiled(tensor)[0]
+
+
+    target = np.full(shape, np.nan)
+
+    assert np.allclose(result, target, equal_nan=True)
+
+@pytest.mark.parametrize(("opset"), OPSETS)
+@pytest.mark.parametrize(
+    ("ov_type", "numpy_dtype"),
+    [
+        (Type.f32, np.float32),
+        (Type.f16, np.float16),
+    ],
+)
+def test_float_to_f8e8m0_constant_single_nan(ov_type, numpy_dtype, opset):
+    data = np.array([np.nan], dtype=numpy_dtype)
+
+    compressed_const = opset.constant(data, dtype=ov.Type.f8e8m0, name="f8e8m0_constant")
+    convert = opset.convert(compressed_const, data.dtype)
+    parameter = opset.parameter(ov.PartialShape([-1]), ov_type)
+    add_op = opset.add(parameter, convert)
+    model = ov.Model([add_op], [parameter])
+
+    compiled = ov.compile_model(model)
+    tensor = np.zeros(data.shape, dtype=numpy_dtype)
+    result = compiled(tensor)[0]
+
+    target = [np.nan]
+    target = np.array(target, dtype=numpy_dtype)
+
+    assert np.allclose(result, target, equal_nan=True)
 
 @pytest.mark.parametrize(("opset"), OPSETS)
 @pytest.mark.parametrize(
@@ -492,7 +545,7 @@ def test_float_to_f8e4m3_constant(ov_type, numpy_dtype, opset):
 def test_float_to_f8e8m0_constant(ov_type, numpy_dtype, opset):
     data = np.array([4.75, 4.5, 5.25, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5,
                      0.6, 0.7, 0.8, 0.9, 1, -0.0, 1.1, 1.2, 1.3,
-                     1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 448, 512, np.nan], dtype=numpy_dtype)
+                     1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 448, 512], dtype=numpy_dtype)
 
     compressed_const = opset.constant(data, dtype=ov.Type.f8e8m0, name="f8e8m0_constant")
     convert = opset.convert(compressed_const, data.dtype)
@@ -507,7 +560,7 @@ def test_float_to_f8e8m0_constant(ov_type, numpy_dtype, opset):
     target = [4.0, 4.0, 4.0, 0.0, 0.125, 0.25, 0.25,
               0.5, 0.5, 0.5, 0.5, 1.0, 1.0, 1.0,
               0.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0,
-              2.0, 2.0, 2.0, 2.0, 512, 512, np.nan]
+              2.0, 2.0, 2.0, 2.0, 512, 512]
     target = np.array(target, dtype=numpy_dtype)
 
     assert np.allclose(result, target, equal_nan=True)
