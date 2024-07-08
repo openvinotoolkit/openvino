f6b8a9728c73ba60def7d40fe20fcf6a188d9db6
Ilya Albrecht <ilya.albrecht@intel.com>
2024-07-08 08:06:58 -0700

    [GPU] Removed double increment on memory usage counter
    As in description

--- src/plugins/intel_gpu/src/runtime/engine.cpp
+++ src/plugins/intel_gpu/src/runtime/engine.cpp
@@ -234,7 +234,6 @@ std::map<std::string, uint64_t> engine::get_memory_statistics() const {
 
 void engine::add_memory_used(uint64_t bytes, allocation_type type) {
     auto idx = static_cast<size_t>(type);
-    _memory_usage_data[idx] += bytes;
     auto new_val = _memory_usage_data[idx].fetch_add(bytes);
     // Make sure actual maximum value is stored
     while (new_val > _peak_memory_usage_data[idx]) {
