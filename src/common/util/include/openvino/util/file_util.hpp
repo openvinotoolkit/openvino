82680489f5836bb191c3d09d76d9fde819ee883d
Xuejun Zhai <xuejun.zhai@intel.com>
2024-07-09 16:21:21 +0800

    OVCore to check for symlinks before opening files (SDL requirement) (#24781)
    ### Details:
     - check symlink
     - *...*
    
    ### Tickets:
     - CVS-142055
    
    ---------
    
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>

--- src/common/util/include/openvino/util/file_util.hpp
+++ src/common/util/include/openvino/util/file_util.hpp
@@ -12,6 +12,20 @@
 
 #include "openvino/util/util.hpp"
 
+#if !defined(__EMSCRIPTEN__) && !defined(__ANDROID__)
+#    if (__cplusplus >= 201703L) || (defined(_MSVC_LANG) && (_MSVC_LANG >= 201703L) && (_MSC_VER >= 1913))
+#        if __has_include(<filesystem>)
+#            include <filesystem>
+namespace fs = std::filesystem;
+#        endif
+#    else
+#        define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
+#        define _LIBCPP_NO_EXPERIMENTAL_DEPRECATION_WARNING_FILESYSTEM
+#        include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#    endif
+#endif
+
 namespace ov {
 namespace util {
 
--- src/inference/src/model_reader.cpp
+++ src/inference/src/model_reader.cpp
@@ -118,6 +118,10 @@ std::shared_ptr<ov::Model> read_model(const std::string& modelPath,
     std::string model_path = modelPath;
 #endif
 
+#if !defined(__EMSCRIPTEN__) && !defined(__ANDROID__)
+    OPENVINO_ASSERT(!fs::is_symlink(model_path), "Cannot read model\"" + modelPath + "\". The path is a symlink!");
+#endif
+
     // Try to load with FrontEndManager
     ov::frontend::FrontEndManager manager;
     ov::frontend::FrontEnd::Ptr FE;
