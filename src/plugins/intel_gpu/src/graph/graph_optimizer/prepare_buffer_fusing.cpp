3483f4f4a571daceb1c686658d91b9b7eefb48d1
Andrew Park <andrew.park@intel.com>
2024-07-09 18:05:43 +0900

    Apply comment
    Signed-off-by: Andrew Park <andrew.park@intel.com>

--- src/plugins/intel_gpu/src/graph/graph_optimizer/prepare_buffer_fusing.cpp
+++ src/plugins/intel_gpu/src/graph/graph_optimizer/prepare_buffer_fusing.cpp
@@ -468,6 +468,10 @@ bool crop_in_place_optimization::match(const program_node& node,
 
     const auto& crop_layout = crop_params.get_output_layout();
     for (auto user : node.get_users()) {
+        // If the user node's output shape is already static, the padding
+        // w/ dyn pad mask will not be propagated properly at runtime
+        if (node.is_dynamic() && !user->get_output_layout().is_dynamic())
+            return false;
         // do not optimize when next node is concatenation which is not output
         if (user->is_type<concatenation>() && !user->is_output())
             return false;
@@ -479,12 +483,6 @@ bool crop_in_place_optimization::match(const program_node& node,
         // TODO: Need to allow optimization for gemm user
         if (node.is_dynamic() && (user->is_type<convolution>() || user->is_type<gemm>()))
             return false;
-        // If the user is strieded slice and the output shape is already static,
-        // the padding w/ dyn pad mask will not be propagated properly at runtime
-        if (user->is_type<strided_slice>()) {
-            if (node.is_dynamic() && !user->get_output_layout().is_dynamic())
-                return false;
-        }
         if (user->is_type<reshape>()) {
             // runtime buffer fusing is only handled when there is only one reshape user
             if (node.is_dynamic() && node.get_users().size() != 1)
@@ -637,7 +635,8 @@ void crop_in_place_optimization::update_in_place_crop_padding_simple_data_format
         auto spatial_size = std::max<size_t>(crop_layout.get_partial_shape().size(), 4) - 2;
         crop_axis_legacy = spatial_size - spatial_axis - 1 + 2;
     }
-    if (crop_layout.is_dynamic() && !is_runtime) {
+    // If it's build-time and node is dynamic, only dynamic padding is set first
+    if ((crop_layout.is_dynamic() || input_layout.is_dynamic()) && !is_runtime) {
         auto dyn_pad_sizes = tensor(0).sizes();
         dyn_pad_sizes[crop_axis_legacy] = 1;
         crop_layout.data_padding.set_dynamic_pad(tensor(dyn_pad_sizes));
