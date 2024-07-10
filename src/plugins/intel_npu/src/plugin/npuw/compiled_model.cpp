7a7a95c339023ec723bc1f664fc9daae8873ef19
Kadian <ujjayant.kadian@intel.com>
2024-07-10 12:43:16 +0100

    Changed the logic for applying layout change

--- src/plugins/intel_npu/src/plugin/npuw/compiled_model.cpp
+++ src/plugins/intel_npu/src/plugin/npuw/compiled_model.cpp
@@ -286,8 +286,10 @@ ov::npuw::CompiledModel::CompiledModel(const std::shared_ptr<ov::Model>& model,
                 std::cout << l.get_shape() << "     " << l.get_any_name() << std::endl;
             }
             ov::preprocess::PrePostProcessor ppp(m_compiled_submodels[real_id].model);
-            for (size_t i = 0; i < m_compiled_submodels[real_id].model->inputs().size(); ++i) {
-                ppp.input(i).tensor().set_layout(ov::Layout("NHWC"));
+            for (auto &input : m_compiled_submodels[real_id].model->inputs()) {
+                if (input.get_shape().size() == 4) {
+                    ppp.input(input.get_any_name()).tensor().set_layout(ov::Layout("NHWC"));
+                }
             }
             auto preprocessed_model = ppp.build();
             m_compiled_submodels[real_id].model = preprocessed_model;
