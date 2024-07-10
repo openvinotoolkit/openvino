38e2c7bf485ebeeb765f31ec97bfdf751b66497b
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2024-07-10 17:09:48 +0200

    check clang version

--- src/common/util/include/openvino/util/file_util.hpp
+++ src/common/util/include/openvino/util/file_util.hpp
@@ -13,7 +13,7 @@
 #include "openvino/util/util.hpp"
 
 #if !defined(__EMSCRIPTEN__) && !defined(__ANDROID__)
-#    if (__cplusplus >= 201703L) || (defined(_MSVC_LANG) && (_MSVC_LANG >= 201703L) && (_MSC_VER >= 1913))
+#    if (__cplusplus >= 201703L) || (defined(_MSVC_LANG) && (_MSVC_LANG >= 201703L) && (_MSC_VER >= 1913)) || (defined(__clang__) && (__clang_major__ >= 5))
 #        if __has_include(<filesystem>)
 #            include <filesystem>
 namespace fs = std::filesystem;
