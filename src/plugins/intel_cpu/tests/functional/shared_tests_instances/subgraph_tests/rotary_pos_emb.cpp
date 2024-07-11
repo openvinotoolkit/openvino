a205ff64c1d2c3010c5a4ff9c2d461a0a24f8ed9
Ivan Tikhonov <ivan.tikhonov@intel.com>
2024-07-11 15:45:48 +0400

    Updated RopeFusion pattern for Qwen model to handle Slice op (#25305)
    ### Details:
    Updated RopeFusion pattern for Qwen; Added new functional tests
    
    ### Tickets:
     - *CVS-144011*

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/subgraph_tests/rotary_pos_emb.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/subgraph_tests/rotary_pos_emb.cpp
@@ -7,26 +7,48 @@
 namespace ov {
 namespace test {
 
-INSTANTIATE_TEST_SUITE_P(smoke_RoPETestLlama2,
-                         RoPETestLlama2,
+INSTANTIATE_TEST_SUITE_P(smoke_RoPETestLlama2StridedSlice,
+                         RoPETestLlama2StridedSlice,
                          ::testing::Values(ov::test::utils::DEVICE_CPU),
-                         RoPETestLlama2::getTestCaseName);
+                         RoPETestLlama2StridedSlice::getTestCaseName);
 
-INSTANTIATE_TEST_SUITE_P(smoke_RoPETestChatGLM,
-                         RoPETestChatGLM,
+INSTANTIATE_TEST_SUITE_P(smoke_RoPETestChatGLMStridedSlice,
+                         RoPETestChatGLMStridedSlice,
                          ::testing::Values(ov::test::utils::DEVICE_CPU),
-                         RoPETestChatGLM::getTestCaseName);
+                         RoPETestChatGLMStridedSlice::getTestCaseName);
 
-INSTANTIATE_TEST_SUITE_P(smoke_RoPETestQwen7b,
-                         RoPETestQwen7b,
+INSTANTIATE_TEST_SUITE_P(smoke_RoPETestQwen7bStridedSlice,
+                         RoPETestQwen7bStridedSlice,
                          ::testing::Combine(::testing::Values(true, false),
                                             ::testing::Values(ov::test::utils::DEVICE_CPU)),
-                         RoPETestQwen7b::getTestCaseName);
+                         RoPETestQwen7bStridedSlice::getTestCaseName);
 
-INSTANTIATE_TEST_SUITE_P(smoke_RoPETestGPTJ,
-                         RoPETestGPTJ,
+INSTANTIATE_TEST_SUITE_P(smoke_RoPETestGPTJStridedSlice,
+                         RoPETestGPTJStridedSlice,
                          ::testing::Combine(::testing::Values(true, false),
                                             ::testing::Values(ov::test::utils::DEVICE_CPU)),
-                         RoPETestGPTJ::getTestCaseName);
+                         RoPETestGPTJStridedSlice::getTestCaseName);
+
+INSTANTIATE_TEST_SUITE_P(smoke_RoPETestLlama2Slice,
+                         RoPETestLlama2Slice,
+                         ::testing::Values(ov::test::utils::DEVICE_CPU),
+                         RoPETestLlama2Slice::getTestCaseName);
+
+INSTANTIATE_TEST_SUITE_P(smoke_RoPETestChatGLMSlice,
+                         RoPETestChatGLMSlice,
+                         ::testing::Values(ov::test::utils::DEVICE_CPU),
+                         RoPETestChatGLMSlice::getTestCaseName);
+
+INSTANTIATE_TEST_SUITE_P(smoke_RoPETestQwen7bSlice,
+                         RoPETestQwen7bSlice,
+                         ::testing::Combine(::testing::Values(true, false),
+                                            ::testing::Values(ov::test::utils::DEVICE_CPU)),
+                         RoPETestQwen7bSlice::getTestCaseName);
+
+INSTANTIATE_TEST_SUITE_P(smoke_RoPETestGPTJSlice,
+                         RoPETestGPTJSlice,
+                         ::testing::Combine(::testing::Values(true, false),
+                                            ::testing::Values(ov::test::utils::DEVICE_CPU)),
+                         RoPETestGPTJSlice::getTestCaseName);
 }  // namespace test
 }  // namespace ov
