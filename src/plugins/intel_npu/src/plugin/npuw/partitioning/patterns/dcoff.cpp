7b27abe90d6c24ad30d3b75bfaddc519b9514a17
Kadian <ujjayant.kadian@intel.com>
2024-07-19 13:47:59 +0100

    Corrected the print message

--- src/plugins/intel_npu/src/plugin/npuw/partitioning/patterns/dcoff.cpp
+++ src/plugins/intel_npu/src/plugin/npuw/partitioning/patterns/dcoff.cpp
@@ -334,8 +334,8 @@ bool DCOFFPassBase::matcher_callback(ov::pass::pattern::Matcher& m) {
     ov::Shape static_shape = current_shape.to_shape();
     ov::Shape new_order = {static_shape[1], static_shape[2], static_shape[3]};
     ov::PartialShape new_shape(new_order);
-    std::cout << "New Input weight shapes: " << current_shape << std::endl;
     matched_paramA->set_partial_shape(new_shape);
+    std::cout << "New Input weight shapes: " << matched_paramA->get_partial_shape() << std::endl;
     auto matched_valueB = std::static_pointer_cast<ov::op::v0::Constant>(matched_nodeB);
     auto matched_paramC = std::static_pointer_cast<ov::op::v0::Parameter>(matched_nodeC);
 
