c80d44639e4086e7be2afb3527e8f9be27f0890b
mitruska <katarzyna.mitrus@intel.com>
2024-07-23 16:09:18 +0200

    Cast frame_size to size_t

--- src/core/reference/src/op/stft.cpp
+++ src/core/reference/src/op/stft.cpp
@@ -31,11 +31,12 @@ void stft(const float* signal,
     const auto batch_size = signal_shape[0];
     const auto signal_length = signal_shape[signal_axis];
     const auto num_frames = static_cast<size_t>((signal_length - frame_size) / frame_step) + 1;
-    const auto fft_out_shape = Shape{std::floor(frame_size / 2) + 1, 2};
+    const auto frame_size_dim = static_cast<size_t>(frame_size);
+    const auto fft_out_shape = Shape{static_cast<size_t>(std::floor(frame_size_dim / 2) + 1), 2};
 
-    const auto window_length = window_shape[0] < frame_size ? window_shape[0] : frame_size;
+    const auto window_length = window_shape[0] < frame_size_dim ? window_shape[0] : frame_size_dim;
     std::vector<T> pad_window(frame_size, 0);
-    std::copy(window, window + window_shape[0], pad_window.begin() + (frame_size - window_length) / 2);
+    std::copy(window, window + window_shape[0], pad_window.begin() + (frame_size_dim - window_length) / 2);
 
     for (size_t batch = 0; batch < batch_size; ++batch) {
         const auto batch_idx = batch * signal_length;
@@ -46,12 +47,16 @@ void stft(const float* signal,
             reference::multiply(signal_slice.data(),
                                 pad_window.data(),
                                 signal_slice.data(),
-                                Shape{frame_size},
-                                Shape{frame_size},
+                                Shape{frame_size_dim},
+                                Shape{frame_size_dim},
                                 op::AutoBroadcastType::NUMPY);
             const auto result_idx = batch * num_frames * fft_out_shape[0] * fft_out_shape[1] +
                                     frame_idx * fft_out_shape[0] * fft_out_shape[1];
-            reference::rdft(signal_slice, Shape{frame_size}, {0}, Shape{frame_size, 2}, rdft_result + result_idx);
+            reference::rdft(signal_slice,
+                            Shape{frame_size_dim},
+                            {0},
+                            Shape{frame_size_dim, 2},
+                            rdft_result + result_idx);
         }
     }
     if (!frames_first) {
