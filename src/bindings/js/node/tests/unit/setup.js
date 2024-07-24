a4534fa570f82d8a3b28267735c00224f5ef68c4
almilosz <alicja.miloszewska@intel.com>
2024-07-24 21:19:06 +0200

    Add method for downloading test_model_fp32

--- src/bindings/js/node/tests/unit/setup.js
+++ src/bindings/js/node/tests/unit/setup.js
@@ -1,18 +1,54 @@
-const { downloadFile } = require('./utils.js');
+let https = require('follow-redirects').https;
+const fs = require('fs');
+const path = require('path');
+const { HttpsProxyAgent } = require('https-proxy-agent');
+
+async function main() {
+  const baseArtifactsDir = './tests/unit/test_models';
+  const modelName = 'test_model_fp32';
+  const modelXMLName = `${modelName}.xml`;
+  const modelBINName = `${modelName}.bin`;
+  const baseURL = 'https://github.com/openvinotoolkit/testdata/raw/master/models/test_model/';
+
+  const { env } = process;
+  const proxyUrl = env.http_proxy || env.HTTP_PROXY || env.npm_config_proxy;
+  let agent;
+  if (proxyUrl) {
+    agent = new HttpsProxyAgent(proxyUrl);
+    console.log(`Proxy agent configured using: '${proxyUrl}'`);
+  }
+
+  downloadFile(baseURL + modelXMLName, path.join(baseArtifactsDir, modelXMLName), agent);
+  downloadFile(baseURL + modelBINName, path.join(baseArtifactsDir, modelBINName), agent);
+}
+
+const downloadFile = (url, destination, agent) => {
+  const file = fs.createWriteStream(destination);
+
+  const options = {
+    agent: agent,
+  };
+
+  https.get(url, options, (response) => {
+    if (response.statusCode !== 200) {
+      console.error(`Failed to get '${url}' (${response.statusCode})`);
+      response.resume();
+      return;
+    }
+
+    response.pipe(file);
+
+    file.on('finish', () => {
+      file.close(() => {
+        console.log('Download completed!');
+      });
+    });
+  }).on('error', (err) => {
+    fs.unlink(destination, () => {});
+    console.error(`Error downloading file: ${err.message}`);
+  });
+};
 
 if (require.main === module) {
   main();
-}
-
-async function main() {
-  const baseArtifactsDir = './test_models';
-  const modelName = 'v3-small_224_1.0_float';
-  const modelXMLName = `${modelName}.xml`;
-  const modelBINName = `${modelName}.bin`;
-  const baseURL = 'https://storage.openvinotoolkit.org/repositories/'
-    + 'openvino_notebooks/models/mobelinet-v3-tf/FP32/';
-
-  await downloadFile(baseURL + modelXMLName, modelXMLName, baseArtifactsDir);
-  await downloadFile(baseURL + modelBINName, modelBINName, baseArtifactsDir);
-
 }
--- src/bindings/js/node/tests/unit/utils.js
+++ src/bindings/js/node/tests/unit/utils.js
@@ -3,13 +3,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 const path = require('path');
-const { HttpsProxyAgent } = require('https-proxy-agent');
-const { createWriteStream } = require('node:fs');
-const { mkdir, stat } = require('node:fs/promises');
 
 module.exports = {
   getModelPath,
-  downloadFile,
 };
 
 function getModelPath(isFP16=false) {
@@ -20,66 +16,4 @@ function getModelPath(isFP16=false) {
     xml: path.join(basePath, `${modelName}.xml`),
     bin: path.join(basePath, `${modelName}.bin`),
   };
-}
-
-async function downloadFile(url, filename, destination) {
-  const { env } = process;
-  const timeout = 5000;
-
-  await applyFolderPath(destination);
-
-  const fullPath = path.resolve(destination, filename);
-  const file = createWriteStream(fullPath);
-  const protocolString = new URL(url).protocol === 'https:' ? 'https' : 'http';
-  const module = require(`node:${protocolString}`);
-  const proxyUrl = env.http_proxy || env.HTTP_PROXY || env.npm_config_proxy;
-
-  let agent;
-
-  if (proxyUrl) {
-    agent = new HttpsProxyAgent(proxyUrl);
-    console.log(`Proxy agent configured using: '${proxyUrl}'`);
-  }
-
-  return new Promise((resolve, reject) => {
-    file.on('error', e => {
-      reject(`Error oppening file stream: ${e}`);
-    });
-
-    const getRequest = module.get(url, { agent }, res => {
-      const { statusCode } = res;
-
-      if (statusCode !== 200)
-        return reject(`Server returns status code: ${statusCode}`);
-
-      res.pipe(file);
-
-      file.on('finish', () => {
-        file.close();
-        console.log(`File successfully stored at '${fullPath}'`);
-        resolve();
-      });
-    });
-
-    getRequest.on('error', e => {
-      reject(`Error sending request: ${e}`);
-    });
-
-    getRequest.setTimeout(timeout, () => {
-      getRequest.destroy();
-      reject(`Request timed out after ${timeout}`);
-    });
-  });
-}
-
-async function applyFolderPath(dirPath) {
-  try {
-    await stat(dirPath);
-
-    return;
-  } catch(err) {
-    if (err.code !== 'ENOENT') throw err;
-
-    await mkdir(dirPath, { recursive: true });
-  }
 }
