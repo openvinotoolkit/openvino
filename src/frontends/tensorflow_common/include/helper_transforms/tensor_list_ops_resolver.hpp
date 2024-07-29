a662b6ba11b1d738c7492f38f8c0593dff96f7ad
Kazantsev, Roman <roman.kazantsev@intel.com>
2024-07-29 20:30:06 +0400

    [TF FE] Deduce dynamic element type for TensorList. Fix for deepspeech2
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/frontends/tensorflow_common/include/helper_transforms/tensor_list_ops_resolver.hpp
+++ src/frontends/tensorflow_common/include/helper_transforms/tensor_list_ops_resolver.hpp
@@ -26,6 +26,14 @@ public:
     TensorListSetItemReplacer();
 };
 
+// Replace internal operation TensorListPushBack with a sub-graph
+// that inserts a new tensor into the tail of the container
+class TensorListPushBackReplacer : public ov::pass::MatcherPass {
+public:
+    OPENVINO_RTTI("ov::frontend::tensorflow::pass::TensorListPushBackReplacer");
+    TensorListPushBackReplacer();
+};
+
 // Replace internal operation TensorListGetItem with a sub-graph that gets a tensor from container by index
 class TensorListGetItemReplacer : public ov::pass::MatcherPass {
 public:
