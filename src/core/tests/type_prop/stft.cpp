15a530af447d8d5a9219c4f27d2d9f96a4ff7b4c
mitruska <katarzyna.mitrus@intel.com>
2024-07-30 12:41:47 +0200

    Tests with Parameter input

--- src/core/tests/type_prop/stft.cpp
+++ src/core/tests/type_prop/stft.cpp
@@ -40,6 +40,42 @@ TEST_F(TypePropSTFTTest, default_ctor) {
     EXPECT_EQ(op->get_output_partial_shape(0), (PartialShape{4, 13, 6, 2}));
 }
 
+TEST_F(TypePropSTFTTest, all_inputs_as_params_static_shapes) {
+    const auto signal = std::make_shared<Parameter>(element::f32, PartialShape{4, 48});
+    const auto window = std::make_shared<Parameter>(ov::element::f32, PartialShape{7});
+    const auto frame_size = std::make_shared<Parameter>(ov::element::i64, PartialShape{});
+    const auto frame_step = std::make_shared<Parameter>(ov::element::i64, PartialShape{});
+
+    const auto op = make_op(signal, window, frame_size, frame_step, transform_frames);
+    EXPECT_EQ(op->get_output_size(), 1);
+    EXPECT_EQ(op->get_output_element_type(0), element::f32);
+    EXPECT_EQ(op->get_output_partial_shape(0), (PartialShape{4, -1, -1, 2}));
+}
+
+TEST_F(TypePropSTFTTest, all_inputs_as_params_dyn_shapes) {
+    const auto signal = std::make_shared<Parameter>(element::f32, PartialShape{-1, -1});
+    const auto window = std::make_shared<Parameter>(ov::element::f32, PartialShape{-1});
+    const auto frame_size = std::make_shared<Parameter>(ov::element::i64, PartialShape::dynamic());
+    const auto frame_step = std::make_shared<Parameter>(ov::element::i64, PartialShape::dynamic());
+
+    const auto op = make_op(signal, window, frame_size, frame_step, transform_frames);
+    EXPECT_EQ(op->get_output_size(), 1);
+    EXPECT_EQ(op->get_output_element_type(0), element::f32);
+    EXPECT_EQ(op->get_output_partial_shape(0), (PartialShape{-1, -1, -1, 2}));
+}
+
+TEST_F(TypePropSTFTTest, all_inputs_as_params_dyn_rank) {
+    const auto signal = std::make_shared<Parameter>(element::f32, PartialShape::dynamic());
+    const auto window = std::make_shared<Parameter>(ov::element::f32, PartialShape::dynamic());
+    const auto frame_size = std::make_shared<Parameter>(ov::element::i64, PartialShape::dynamic());
+    const auto frame_step = std::make_shared<Parameter>(ov::element::i64, PartialShape::dynamic());
+
+    const auto op = make_op(signal, window, frame_size, frame_step, transform_frames);
+    EXPECT_EQ(op->get_output_size(), 1);
+    EXPECT_EQ(op->get_output_element_type(0), element::f32);
+    EXPECT_EQ(op->get_output_partial_shape(0), PartialShape::dynamic());
+}
+
 using STFTTestParam = std::tuple<PartialShape, PartialShape, int32_t, int32_t, bool, PartialShape>;
 class TypePropSTFTTestP : public TypePropSTFTTest, public testing::WithParamInterface<STFTTestParam> {
 protected:
@@ -67,7 +103,7 @@ INSTANTIATE_TEST_SUITE_P(
         std::make_tuple(PartialShape{-1, -1}, PartialShape{7}, 11, 3, false, PartialShape{-1, {1, -1}, 6, 2}),
         std::make_tuple(PartialShape{-1, -1}, PartialShape{7}, 11, 3, true, PartialShape{-1, 6, {1, -1}, 2}),
         std::make_tuple(PartialShape{-1, {48, 56}}, PartialShape{7}, 11, 3, true, PartialShape{-1, 6, {13, 16}, 2}),
-        std::make_tuple(PartialShape{{2, 4}, {48, 56}}, PartialShape{7}, 11, 3, true, PartialShape{{2, 4}, 6, {13, 16}, 2}),
+        std::make_tuple(PartialShape{{2, 4}, -1}, PartialShape{7}, 11, 3, true, PartialShape{{2, 4}, 6, {1, -1}, 2}),
         std::make_tuple(PartialShape::dynamic(), PartialShape{7}, 11, 3, true, PartialShape::dynamic()),
         std::make_tuple(PartialShape::dynamic(), PartialShape::dynamic(), 11, 3, true, PartialShape::dynamic())),
     testing::PrintToStringParamName());
