1540f9040742ada47b9ed2b46a0935cb9a50f937
Pavel Durandin <pavel.durandin@intel.com>
2024-07-30 16:40:35 +0400

    Update compile errors on linux

--- src/inference/src/dev/core_impl.cpp
+++ src/inference/src/dev/core_impl.cpp
@@ -597,7 +597,7 @@ ov::Plugin ov::CoreImpl::get_plugin(const std::string& pluginName) const {
             // Check that device plugin name is the same as requested for HW plugins
             if (!plugin_name.empty() && !is_virtual_device(plugin_name)) {
                 OPENVINO_ASSERT(deviceName.find(plugin_name) != std::string::npos,
-                                desc.libraryLocation.c_str(),
+                                ov::util::wstring_to_string(desc.libraryLocation.c_str()),
                                 " is used for ",
                                 deviceName,
                                 " , while it contains implementation for ",
