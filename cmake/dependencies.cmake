bcb6ceb7b7234e3904bcbee50583640c82b29633
xufang <fang.xu@intel.com>
2024-07-30 21:45:07 +0800

    enable CFG for tbb library

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -89,10 +89,10 @@ function(ov_download_tbb)
         # TODO: add target_path to be platform specific as well, to avoid following if
         # build oneTBB 2021.2.1 with Visual Studio 2019 (MSVC 14.21)
         RESOLVE_DEPENDENCY(TBB
-                ARCHIVE_WIN "oneapi-tbb-2021.2.5-win-trim.zip"
+                ARCHIVE_WIN "oneapi-tbb-2021.2.5-win-trim-cfg.zip"
                 TARGET_PATH "${TEMP}/tbb"
                 ENVIRONMENT "TBBROOT"
-                SHA256 "a9384a25861946648db58f7f294c4aa2b1be7ae52748024fef3e13ca2762a1ba"
+                SHA256 "dc1f644061db517c0502b7c4e7ca1ac13fe559604bc3d57e26f0119b2dfab762"
                 USE_NEW_LOCATION TRUE)
     elseif(ANDROID AND X86_64)
         RESOLVE_DEPENDENCY(TBB
