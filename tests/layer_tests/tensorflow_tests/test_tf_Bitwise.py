34a8a24316de6104fe7df346c6fc8417095b2df9
Kazantsev, Roman <roman.kazantsev@intel.com>
2024-07-31 21:02:55 +0400

    Add np.uint16 case
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- tests/layer_tests/tensorflow_tests/test_tf_Bitwise.py
+++ tests/layer_tests/tensorflow_tests/test_tf_Bitwise.py
@@ -60,7 +60,7 @@ class TestBitwise(CommonTFLayerTest):
     @pytest.mark.parametrize('y_shape', [[4], [2, 3, 4]])
     @pytest.mark.parametrize('is_y_const', [True, False])
     @pytest.mark.parametrize('input_type', [np.int8, np.int16, np.int32, np.int64,
-                                            np.uint8, np.uint32, np.uint64])
+                                            np.uint8, np.uint16, np.uint32, np.uint64])
     @pytest.mark.parametrize("op_type", ['BitwiseAnd', 'BitwiseOr', 'BitwiseXor'])
     @pytest.mark.precommit
     @pytest.mark.nightly
