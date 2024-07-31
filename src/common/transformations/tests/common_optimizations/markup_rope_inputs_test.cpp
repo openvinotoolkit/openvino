6b398ec4fcd0e32ef77ef56f345531124affcdec
Zhang Yi3 <yi3.zhang@intel.com>
2024-07-30 22:56:44 -0700

    fix code style

--- src/common/transformations/tests/common_optimizations/markup_rope_inputs_test.cpp
+++ src/common/transformations/tests/common_optimizations/markup_rope_inputs_test.cpp
@@ -1,11 +1,12 @@
 // Copyright (C) 2018-2024 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
+#include "transformations/common_optimizations/markup_rope_inputs.hpp"
+
 #include "common_test_utils/ov_test_utils.hpp"
 #include "openvino/opsets/opset1.hpp"
-#include "ov_ops/rotary_positional_embeddings.hpp"
 #include "openvino/pass/manager.hpp"
-#include "transformations/common_optimizations/markup_rope_inputs.hpp"
+#include "ov_ops/rotary_positional_embeddings.hpp"
 #include "transformations/rt_info/disable_fp16_compression.hpp"
 
 TEST_F(TransformationTestsF, MarkUpRopeInputsTest) {
@@ -32,7 +33,8 @@ TEST_F(TransformationTestsF, MarkUpRopeInputsTest) {
         auto input_a = std::make_shared<ov::opset1::Parameter>(ov::element::f32, ov::Shape{1, 32, 1});
         auto input_b = std::make_shared<ov::opset1::Parameter>(ov::element::f32, ov::Shape{1, 1, 10});
         auto matmul = std::make_shared<ov::opset1::MatMul>(input_a, input_b);
-        auto transpose_order = ov::op::v0::Constant::create(ov::element::i32, ov::Shape{3}, std::vector<int32_t>{0, 2, 1});
+        auto transpose_order =
+            ov::op::v0::Constant::create(ov::element::i32, ov::Shape{3}, std::vector<int32_t>{0, 2, 1});
         auto transpose = std::make_shared<ov::opset1::Transpose>(matmul, transpose_order);
         auto concat = std::make_shared<ov::opset1::Concat>(ov::NodeVector{transpose, transpose}, -1);
         auto cos = std::make_shared<ov::opset1::Cos>(concat);
@@ -40,7 +42,7 @@ TEST_F(TransformationTestsF, MarkUpRopeInputsTest) {
         ov::op::internal::RoPE::Config config;
         auto rope =
             std::make_shared<ov::op::internal::RoPE>(ov::OutputVector{input->output(0), cos->output(0), sin->output(0)},
-                                                    config);
+                                                     config);
         model = std::make_shared<ov::Model>(rope, ov::ParameterVector{input, input_a, input_b}, "model");
     }
 
@@ -51,7 +53,8 @@ TEST_F(TransformationTestsF, MarkUpRopeInputsTest) {
         auto input_a = std::make_shared<ov::opset1::Parameter>(ov::element::f32, ov::Shape{1, 32, 1});
         auto input_b = std::make_shared<ov::opset1::Parameter>(ov::element::f32, ov::Shape{1, 1, 10});
         auto matmul = std::make_shared<ov::opset1::MatMul>(input_a, input_b);
-        auto transpose_order = ov::op::v0::Constant::create(ov::element::i32, ov::Shape{3}, std::vector<int32_t>{0, 2, 1});
+        auto transpose_order =
+            ov::op::v0::Constant::create(ov::element::i32, ov::Shape{3}, std::vector<int32_t>{0, 2, 1});
         auto transpose = std::make_shared<ov::opset1::Transpose>(matmul, transpose_order);
         auto concat = std::make_shared<ov::opset1::Concat>(ov::NodeVector{transpose, transpose}, -1);
         auto cos = std::make_shared<ov::opset1::Cos>(concat);
@@ -64,7 +67,7 @@ TEST_F(TransformationTestsF, MarkUpRopeInputsTest) {
         ov::op::internal::RoPE::Config config;
         auto rope =
             std::make_shared<ov::op::internal::RoPE>(ov::OutputVector{input->output(0), cos->output(0), sin->output(0)},
-                                                    config);
+                                                     config);
         model_ref = std::make_shared<ov::Model>(rope, ov::ParameterVector{input, input_a, input_b}, "model_ref");
     }
     comparator.enable(FunctionsComparator::CmpValues::CONST_VALUES);
