d7d99b0979ea80a01b6a27744aec73196d384c7e
mitruska <katarzyna.mitrus@intel.com>
2024-07-31 20:53:06 +0200

    Init right shift ref tests

---
+++ src/plugins/template/tests/functional/op_reference/bitwise_right_shift.cpp
@@ -0,0 +1,35 @@
+// Copyright (C) 2018-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "openvino/op/bitwise_right_shift.hpp"
+
+#include <gtest/gtest.h>
+
+#include "bitwise.hpp"
+
+using namespace ov;
+
+namespace reference_tests {
+namespace BitwiseOpsRefTestDefinitions {
+namespace {
+
+std::vector<RefBitwiseParams> generateBitwiseParams() {
+    std::vector<RefBitwiseParams> bitwiseParams{
+        Builder{}
+            .opType(BitwiseTypes::BITWISE_RIGHT_SHIFT)
+            .inputs({{{10}, element::i32, std::vector<int32_t>{0, -0, 1, -1, 3, -3, 2, -2, 64, -64}},
+                     {{1}, element::i32, std::vector<int32_t>{1}}})
+            .expected({{10}, element::i32, std::vector<int32_t>{0, 0, 0, -1, 1, -2, 1, -1, 32, -32}}),
+    };
+    return bitwiseParams;
+}
+
+INSTANTIATE_TEST_SUITE_P(smoke_BitwiseRightShift_With_Hardcoded_Refs,
+                         ReferenceBitwiseLayerTest,
+                         ::testing::ValuesIn(generateBitwiseParams()),
+                         ReferenceBitwiseLayerTest::getTestCaseName);
+
+}  // namespace
+}  // namespace BitwiseOpsRefTestDefinitions
+}  // namespace reference_tests
