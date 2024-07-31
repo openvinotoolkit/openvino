20330ef42d659fdec5472b1792a118c3d3e29f0e
mitruska <katarzyna.mitrus@intel.com>
2024-07-31 18:46:26 +0200

    Ini BitwiseLeftShift eval

--- src/core/src/op/bitwise_left_shift.cpp
+++ src/core/src/op/bitwise_left_shift.cpp
@@ -6,6 +6,7 @@
 
 #include "itt.hpp"
 #include "openvino/op/op.hpp"
+#include "openvino/reference/bitwise_left_shift.hpp"
 
 namespace ov {
 namespace op {
@@ -21,6 +22,25 @@ std::shared_ptr<Node> BitwiseLeftShift::clone_with_new_inputs(const OutputVector
     OV_OP_SCOPE(v15_BitwiseLeftShift_clone_with_new_inputs);
     check_new_args_count(this, new_args);
     return std::make_shared<BitwiseLeftShift>(new_args[0], new_args[1], get_autob());
+}
+
+bool BitwiseLeftShift::evaluate(TensorVector& outputs, const TensorVector& inputs) const {
+    OV_OP_SCOPE(v15_BitwiseLeftShift_evaluate);
+    OPENVINO_ASSERT(outputs.size() == 1);
+    OPENVINO_ASSERT(inputs.size() == 2);
+
+    reference::bitwise_left_shift(inputs[0].data<const int32_t>(),
+                                  inputs[1].data<const int32_t>(),
+                                  outputs[0].data<int32_t>(),
+                                  inputs[0].get_shape(),
+                                  inputs[1].get_shape(),
+                                  get_autob());
+    return true;
+}
+
+bool BitwiseLeftShift::has_evaluate() const {
+    OV_OP_SCOPE(v15_BitwiseLeftShift_has_evaluate);
+    return true;
 }
 }  // namespace v15
 }  // namespace op
