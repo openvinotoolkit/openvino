e06a7ee785b7eb29ed76d9a025ea8945447fc367
Aleksandr Voron <aleksandr.voron@intel.com>
2024-08-01 13:35:38 +0200

    [CPU][ARM] MLAS transpose executor deprioritised (#25854)
    ### Details:
    - The latest performance reports on Ampere show ACL transpose executor
    provides better performance rather than MLAS Transpose executor (details
    are in the ticket). Therefore, MLAS Transpose executor priority has been
    decreased.
     - Redundant check has been deleted in ACL Transpose executor.
    
    ### Tickets:
     - CVS-148625

--- src/plugins/intel_cpu/src/nodes/executors/transpose_list.cpp
+++ src/plugins/intel_cpu/src/nodes/executors/transpose_list.cpp
@@ -9,9 +9,9 @@ namespace intel_cpu {
 
 const std::vector<TransposeExecutorDesc>& getTransposeExecutorsList() {
     static const std::vector<TransposeExecutorDesc> descs = {
-            OV_CPU_INSTANCE_MLAS_ARM64(ExecutorType::Mlas, std::make_shared<MlasTransposeExecutorBuilder>())
             OV_CPU_INSTANCE_COMMON(ExecutorType::Common, std::make_shared<RefOptimizedTransposeExecutorBuilder>())
             OV_CPU_INSTANCE_ACL(ExecutorType::Acl, std::make_shared<ACLTransposeExecutorBuilder>())
+            OV_CPU_INSTANCE_MLAS_ARM64(ExecutorType::Mlas, std::make_shared<MlasTransposeExecutorBuilder>())
             OV_CPU_INSTANCE_X64(ExecutorType::jit_x64, std::make_shared<JitTransposeExecutorBuilder>())
             OV_CPU_INSTANCE_COMMON(ExecutorType::Common, std::make_shared<RefTransposeExecutorBuilder>())
     };
