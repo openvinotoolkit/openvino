e3c7252aaab23a39187896b42364fea23e0c1334
Georgy Krivoruchko <georgy.krivoruchko@intel.com>
2024-08-02 14:47:15 +0400

    [ONNX] Aligned behavior for ReduceLogSumExp-11,13,18

--- src/frontends/onnx/tests/tests_python/test_backend.py
+++ src/frontends/onnx/tests/tests_python/test_backend.py
@@ -441,13 +441,7 @@ tests_expected_to_fail = [
         "OnnxBackendNodeModelTest.test_reduce_log_sum_asc_axes_expanded_cpu",
         "OnnxBackendNodeModelTest.test_reduce_log_sum_desc_axes_cpu",
         "OnnxBackendNodeModelTest.test_reduce_log_sum_desc_axes_expanded_cpu",
-        "OnnxBackendNodeModelTest.test_reduce_log_sum_exp_do_not_keepdims_example_cpu",
-        "OnnxBackendNodeModelTest.test_reduce_log_sum_exp_do_not_keepdims_random_cpu",
         "OnnxBackendNodeModelTest.test_reduce_l1_do_not_keepdims_example_cpu",
-        "OnnxBackendNodeModelTest.test_reduce_log_sum_exp_keepdims_example_cpu",
-        "OnnxBackendNodeModelTest.test_reduce_log_sum_exp_negative_axes_keepdims_example_cpu",
-        "OnnxBackendNodeModelTest.test_reduce_log_sum_exp_keepdims_random_cpu",
-        "OnnxBackendNodeModelTest.test_reduce_log_sum_exp_negative_axes_keepdims_random_cpu",
         "OnnxBackendNodeModelTest.test_reduce_prod_do_not_keepdims_example_cpu",
         "OnnxBackendNodeModelTest.test_reduce_prod_do_not_keepdims_random_cpu",
         "OnnxBackendNodeModelTest.test_reduce_prod_keepdims_example_cpu",
@@ -643,7 +637,6 @@ tests_expected_to_fail = [
     (
         xfail_issue_125493,
         "OnnxBackendNodeModelTest.test_reduce_l1_empty_set_cpu",
-        "OnnxBackendNodeModelTest.test_reduce_log_sum_exp_empty_set_cpu",
         "OnnxBackendNodeModelTest.test_reduce_prod_empty_set_cpu",
     ),
     (
