4d5a5208c5633d945bff0730d58bd181faeb51d5
Kim, Mingyu <mingyu.kim@intel.com>
2024-08-03 11:57:25 +0900

    group_size -> group_sizes

--- src/common/transformations/include/ov_ops/dynamic_quantize.hpp
+++ src/common/transformations/include/ov_ops/dynamic_quantize.hpp
@@ -20,18 +20,18 @@ public:
     /// \brief Constructs an DynamicQuantize operation.
     ///
     /// \param data Input tensor with data
-    /// \param group_size Group size for dynamic quantization
+    /// \param group_sizes Group sizes for dynamic quantization
     /// \param dt_scale Data type for scale output
-    DynamicQuantize(const Output<Node>& data, std::vector<size_t> group_size, element::Type dt_scale);
+    DynamicQuantize(const Output<Node>& data, std::vector<size_t> group_sizes, element::Type dt_scale);
 
     void validate_and_infer_types() override;
 
     std::shared_ptr<Node> clone_with_new_inputs(const ov::OutputVector& new_args) const override;
-    std::vector<size_t> get_group_size() const { return m_group_size; };
-    static std::vector<ov::PartialShape> shape_infer(const DynamicQuantize* op, std::vector<ov::PartialShape> input_shapes, const std::vector<size_t> group_size);
+    std::vector<size_t> get_group_sizes() const { return m_group_sizes; };
+    static std::vector<ov::PartialShape> shape_infer(const DynamicQuantize* op, std::vector<ov::PartialShape> input_shapes, const std::vector<size_t> group_sizes);
 
 private:
-    std::vector<size_t> m_group_size;
+    std::vector<size_t> m_group_sizes;
     element::Type m_dt_scale;
 };
 
--- src/common/transformations/src/ov_ops/dynamic_quantize.cpp
+++ src/common/transformations/src/ov_ops/dynamic_quantize.cpp
@@ -12,12 +12,12 @@ namespace ov {
 namespace op {
 namespace internal {
 
-DynamicQuantize::DynamicQuantize(const Output<Node>& data, std::vector<size_t> group_size, element::Type dt_scale)
+DynamicQuantize::DynamicQuantize(const Output<Node>& data, std::vector<size_t> group_sizes, element::Type dt_scale)
     : Op({data})
-    , m_group_size(group_size)
+    , m_group_sizes(group_sizes)
     , m_dt_scale(dt_scale) {
-    OPENVINO_ASSERT(data.get_tensor_ptr()->get_partial_shape().rank() == group_size.size(), "FC input rank should be same as the rank of group_size ",
-                                                                                            data.get_tensor_ptr()->get_partial_shape().rank(), " / ", group_size.size());
+    OPENVINO_ASSERT(data.get_partial_shape().rank() == group_sizes.size(), "FC input rank should be same as the rank of group_size ",
+                                                                                            data.get_tensor_ptr()->get_partial_shape().rank(), " / ", group_sizes.size());
     set_output_size(2);
     validate_and_infer_types();
 }
@@ -27,33 +27,33 @@ void DynamicQuantize::validate_and_infer_types() {
         get_input_partial_shape(0)
     };
 
-    auto out_shapes = shape_infer(this, input_shapes, m_group_size);
+    auto out_shapes = shape_infer(this, input_shapes, m_group_sizes);
     set_output_type(0, element::i8, out_shapes[0]);
     set_output_type(1, m_dt_scale, out_shapes[1]);
 }
 
 std::shared_ptr<Node> DynamicQuantize::clone_with_new_inputs(const ov::OutputVector& new_args) const {
     check_new_args_count(this, new_args);
-    return std::make_shared<DynamicQuantize>(new_args.at(0), m_group_size, m_dt_scale);
+    return std::make_shared<DynamicQuantize>(new_args.at(0), m_group_sizes, m_dt_scale);
 }
 
-std::vector<ov::PartialShape> DynamicQuantize::shape_infer(const DynamicQuantize* op, std::vector<ov::PartialShape> input_shapes, const std::vector<size_t> group_size) {
+std::vector<ov::PartialShape> DynamicQuantize::shape_infer(const DynamicQuantize* op, std::vector<ov::PartialShape> input_shapes, const std::vector<size_t> group_sizes) {
     std::vector<ov::PartialShape> out_shapes;
     out_shapes.push_back(input_shapes[0]);
-    // FIXME: generalize to N-dim case
+
     auto scale_shape = input_shapes[0];
-    OPENVINO_ASSERT(scale_shape.size() == group_size.size(), "Scale_shape and group_size are supposed to have same rank: "
+    OPENVINO_ASSERT(scale_shape.size() == group_sizes.size(), "Scale_shape and group_size are supposed to have same rank: "
                                                              , scale_shape.size()
                                                              , " / "
-                                                             , group_size.size());
+                                                             , group_sizes.size());
     for (size_t i = 0; i < scale_shape.size(); i++) {
         if (scale_shape[i].is_dynamic())
             continue;
 
-        if (group_size[i] == UINT64_MAX)
+        if (group_sizes[i] == UINT64_MAX)
             scale_shape[i] = 1;
         else {
-            scale_shape[i] /= group_size[i];    // if group_size is larger than shape, scale_shape will be 1
+            scale_shape[i] /= group_sizes[i];    // if group_size is larger than shape, scale_shape will be 1
             scale_shape[i] = std::max(scale_shape[i].get_length(), 1L);
         }
     }
