2880468d07ab13a5a13f9ad015414cc34f37fcbc
sunxiaoxia2022 <xiaoxia.sun@intel.com>
2024-08-06 00:15:12 +0800

    change server_wait from another thread to main thread

--- src/inference/src/dev/threading/cpu_message.cpp
+++ src/inference/src/dev/threading/cpu_message.cpp
@@ -23,56 +23,40 @@ void MessageManager::send_message(const MessageInfo& msg_info) {
     {
         std::lock_guard<std::mutex> lock(_msgMutex);
         _messageQueue.push_back(msg_info);
-        // std::cout << "send : " << msg_info.msg_type << ", " << (msg_info.rank.size() > 0 ? msg_info.rank[0] : -1)
-        //           << "\n";
+        // std::cout << "send : " << msg_info.msg_type << "\n";
     }
     _msgCondVar.notify_all();
 }
 
-void MessageManager::infer_wait() {
-    std::unique_lock<std::mutex> lock(_inferMutex);
-    std::this_thread::sleep_for(std::chrono::nanoseconds(1));
-    _inferCondVar.wait(lock);
-}
-
 void MessageManager::server_wait() {
-    if (!_serverThread.joinable()) {
-        assert(_num_sub_streams);
-        MsgType msg_type;
-        _serverThread = std::thread([&]() {
-            int count = 0;
-            while (!_isServerStopped) {
-                std::vector<MessageInfo> msgQueue;
-                {
-                    // std::cout << "server_wait ........" << _isServerStopped << "\n";
-                    std::unique_lock<std::mutex> lock(_msgMutex);
-                    _msgCondVar.wait(lock, [&] {
-                        return !_messageQueue.empty();
-                    });
-                    std::swap(_messageQueue, msgQueue);
-                    // std::cout << "server_wait receive: " << msgQueue[0].msg_type << "\n";
-                }
+    assert(_num_sub_streams);
+    MsgType msg_type;
+    int count = 0;
+    bool isStopped = false;
+    while (!isStopped) {
+        std::vector<MessageInfo> msgQueue;
+        {
+            // std::cout << "server_wait ........" << _isServerStopped << "\n";
+            std::unique_lock<std::mutex> lock(_msgMutex);
+            _msgCondVar.wait(lock, [&] {
+                return !_messageQueue.empty();
+            });
+            std::swap(_messageQueue, msgQueue);
+            // std::cout << "server_wait receive: " << msgQueue[0].msg_type << "\n";
+        }
 
-                for (auto rec_info : msgQueue) {
-                    msg_type = rec_info.msg_type;
-                    if (msg_type == START_INFER) {
-                        Task task = std::move(rec_info.task);
-                        task();
-                    } else if (msg_type == CALL_BACK) {  // CALL_BACK
-                        count++;
-                        // std::cout << "server_wait CALL_BACK: " << count << "/" << _num_sub_streams << "\n";
-                        if (count == _num_sub_streams) {
-                            count = 0;
-                            _inferCondVar.notify_one();
-                        }
-                    } else if (msg_type == QUIT) {
-                        _isServerStopped = true;
-                    }
+        for (auto rec_info : msgQueue) {
+            msg_type = rec_info.msg_type;
+            if (msg_type == CALL_BACK) {  // CALL_BACK
+                count++;
+                // std::cout << "server_wait CALL_BACK: " << count << "/" << _num_sub_streams << "\n";
+                if (count == _num_sub_streams) {
+                    count = 0;
+                    isStopped = true;
                 }
             }
-            // std::cout << "-------- server_wait end ---------\n";
-        });
-    }
+        }
+    };
 }
 
 void MessageManager::set_num_sub_streams(int num_sub_streams) {
@@ -81,15 +65,6 @@ void MessageManager::set_num_sub_streams(int num_sub_streams) {
 
 int MessageManager::get_num_sub_streams() {
     return _num_sub_streams;
-}
-
-void MessageManager::stop_server_thread() {
-    MessageInfo msg_info;
-    msg_info.msg_type = ov::threading::MsgType::QUIT;
-    send_message(msg_info);
-    if (_serverThread.joinable()) {
-        _serverThread.join();
-    }
 }
 
 namespace {
