f86ce7ffac4077ca8751dd9fdb68a9fd27e8ece8
Mateusz <mateusz.mikolajczyk@intel.com>
2024-08-06 18:19:11 +0200

    Add SliceScatter core and reference

---
+++ src/core/shape_inference/include/slice_scatter_shape_inference.hpp
@@ -0,0 +1,129 @@
+// Copyright (C) 2022 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <array>
+
+#include "openvino/core/validation_util.hpp"
+#include "openvino/op/slice_scatter.hpp"
+#include "slice_shape_inference.hpp"
+#include "utils.hpp"
+
+namespace ov {
+namespace op {
+namespace v15 {
+template <class T, class TRShape = result_shape_t<T>>
+std::vector<TRShape> shape_infer(const SliceScatter* op,
+                                 const std::vector<T>& input_shapes,
+                                 const ITensorAccessor& ta = make_tensor_accessor()) {
+    const auto& num_of_inputs = input_shapes.size();
+
+    NODE_VALIDATION_CHECK(op,
+                          num_of_inputs == 5 || num_of_inputs == 6,
+                          "SliceScatter has to have 5 or 6 inputs. Got: ",
+                          num_of_inputs);
+    const auto input_shape = input_shapes[0];
+    const auto& data_rank = input_shape.rank();
+    auto output_shape = input_shapes[0];
+    NODE_SHAPE_INFER_CHECK(op,
+                           input_shapes,
+                           output_shape.merge_rank(input_shapes[1].rank()),
+                           "SliceScatter `data` and `updates` need to have compatible rank.");
+
+    NODE_SHAPE_INFER_CHECK(op,
+                           input_shapes,
+                           output_shape.is_dynamic() || output_shape.rank().get_length() > 0,
+                           "SliceScatter `data` and `updates` input can't be a scalar.");
+    constexpr std::array<char const*, 4> shape_names{"start", "stop", "step", "axes"};
+    for (size_t i = 2; i < input_shapes.size(); ++i) {
+        const auto& shape = input_shapes[i];
+        const auto& shape_rank = shape.rank();
+        NODE_VALIDATION_CHECK(op,
+                              shape_rank.compatible(1),
+                              "SliceScatter `",
+                              shape_names[i - 2],
+                              "` input must be a 1D tensor. Got rank: ",
+                              shape_rank);
+
+        if (output_shape.rank().is_static()) {
+            NODE_VALIDATION_CHECK(op,
+                                  shape_rank.is_dynamic() || static_cast<int64_t>(shape[0].get_min_length()) <=
+                                                                 output_shape.rank().get_length(),
+                                  "SliceScatter `",
+                                  shape_names[i - 2],
+                                  "` input dim size can't be bigger than `data` or `updates` rank.");
+        }
+    }
+
+    const auto& start_shape = input_shapes[2];
+    const auto& stop_shape = input_shapes[3];
+    const auto& step_shape = input_shapes[4];
+
+    NODE_VALIDATION_CHECK(
+        op,
+        start_shape.compatible(stop_shape) && start_shape.compatible(step_shape) && stop_shape.compatible(step_shape),
+        "SliceScatter `start`, `stop`, `step` inputs must have compatible shapes.");
+    // compute constant values of begin, end, and strides if possible
+    const auto start = get_input_bounds<TRShape, int64_t>(op, 2, ta);
+    const auto stop = get_input_bounds<TRShape, int64_t>(op, 3, ta);
+    const auto steps = get_input_const_data_as<TRShape, int64_t>(op, 4, ta);
+    if (step_shape.is_static() && steps) {
+        for (size_t i = 0; i < step_shape[0].get_length(); i++) {
+            NODE_VALIDATION_CHECK(op, (*steps)[i] != 0, "SliceScatter step values must be non-zero.");
+        }
+    }
+    slice::AxesMap axes_map;
+    if (input_shapes.size() > 5) {
+        NODE_VALIDATION_CHECK(
+            op,
+            input_shapes[5].compatible(start_shape),
+            "SliceScatter `axes` input must have compatible shape with `start`, `stop`, `step` inputs.");
+        auto axes = get_input_const_data_as<TRShape, int64_t>(op, 5, ta);
+        if (axes && data_rank.is_static()) {
+            ov::util::normalize_axes(op, data_rank.get_length(), *axes);
+            axes_map.add(*axes);
+            NODE_VALIDATION_CHECK(op, axes_map.is_valid, "SliceScatter values in `axes` input must be unique.");
+        }
+    } else if (start) {
+        axes_map.generate_n(start->size());
+    }
+    auto axis_it = axes_map.m.cbegin();
+    using DimType = typename T::value_type;
+
+    if (output_shape.rank().is_static()) {
+        auto expected_updates_shape = output_shape;
+        for (size_t dim_idx = 0; dim_idx < output_shape.size(); ++dim_idx) {
+            const DimType& input_dim = output_shape[dim_idx];
+
+            if (axes_map.is_valid && (axis_it != axes_map.m.cend()) && (axis_it->first == dim_idx)) {
+                const auto& i = axis_it->second;
+                if (start && stop && steps) {
+                    const auto& step = (*steps)[i];
+                    expected_updates_shape[dim_idx] = slice::make_dim(input_dim, (*start)[i], (*stop)[i], step);
+                } else {
+                    expected_updates_shape[dim_idx] = DimType{0, input_dim.get_max_length()};
+                }
+                ++axis_it;
+            } else if (axes_map.is_valid) {
+                // dimension not on axes list, no change
+                expected_updates_shape[dim_idx] = input_dim;
+            } else {
+                // axes are unknow so any dimension can be sliced
+                expected_updates_shape[dim_idx] = DimType{0, input_dim.get_max_length()};
+            }
+        }
+        NODE_VALIDATION_CHECK(op,
+                              input_shapes[1].compatible(expected_updates_shape),
+                              "SliceScatter updates of shape ",
+                              input_shapes[1],
+                              " are not compatible with expected slice shape ",
+                              expected_updates_shape);
+    }
+
+    return {output_shape};
+}
+}  // namespace v15
+}  // namespace op
+}  // namespace ov
