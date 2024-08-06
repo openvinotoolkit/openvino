a63b74c0cb35848903625eb5a7df164634531b4d
almilosz <alicja.miloszewska@intel.com>
2024-08-06 13:47:29 +0200

    Add noqa

--- src/bindings/python/src/openvino/dev_api/__init__.py
+++ src/bindings/python/src/openvino/dev_api/__init__.py
@@ -1,5 +1,7 @@
 # Copyright (C) 2018-2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+# flake8: noqa
+
 from openvino._pyopenvino.dev_api import evaluate_as_partial_shape
 from openvino._pyopenvino.dev_api import evaluate_both_bounds
