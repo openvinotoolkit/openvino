da0a3220379f41f7518189818b8575d4ffc93bcd
Raasz, Pawel <pawel.raasz@intel.com>
2024-08-07 13:27:28 +0200

    Use pimpl in Tensor descriptor to hide implementation details from public API

--- src/core/src/op/result.cpp
+++ src/core/src/op/result.cpp
@@ -9,8 +9,116 @@
 #include <typeinfo>
 
 #include "itt.hpp"
+#include "openvino/core/descriptor_tensor.hpp"
 
 namespace ov {
+namespace descriptor {
+
+/**
+ * @brief Dedicated tensor descriptor implementation to share input descriptor.
+ */
+class SharedTensor : public ITensorDescriptor {
+public:
+    SharedTensor(std::shared_ptr<ITensorDescriptor> tensor) : m_shared_tensor{std::move(tensor)}, m_output_names{} {
+        OPENVINO_ASSERT(m_shared_tensor, "Cannot set NULL tensor descriptor");
+    }
+
+    /**
+     * @brief Update output tensor descriptor with shared tensors from new input.
+     *
+     * @param output     Output descriptor to be updated with shared tensor.
+     * @param new_input  New input with tensor description.
+     */
+    static void update(Output& output, const Input& new_input) {
+        auto& output_descriptor = TensorExtension::get_descriptor_ptr(output.get_tensor());
+        const auto& input_descriptor = TensorExtension::get_descriptor_ptr(new_input.get_output().get_tensor());
+        if (auto* result_ptr = dynamic_cast<SharedTensor*>(output_descriptor.get())) {
+            result_ptr->set_tensor(input_descriptor);
+        } else {
+            output_descriptor = std::make_shared<SharedTensor>(input_descriptor);
+        }
+    }
+
+    // --- ITensorDescriptor API
+    virtual const element::Type& get_element_type() const override {
+        return m_shared_tensor->get_element_type();
+    }
+
+    virtual const PartialShape& get_partial_shape() const override {
+        return m_shared_tensor->get_partial_shape();
+    }
+
+    virtual const Shape& get_shape() const override {
+        return m_shared_tensor->get_shape();
+    }
+
+    virtual void set_type_shape(const element::Type& et, const PartialShape& shape) override {
+        m_shared_tensor->set_type_shape(et, shape);
+    }
+
+    void set_names(const std::unordered_set<std::string>& names) override {
+        rm_tensor_output_names();
+        m_output_names = names;
+        m_shared_tensor->add_names(m_output_names);
+    }
+
+    void add_names(const std::unordered_set<std::string>& names) override {
+        m_shared_tensor->add_names(names);
+    }
+
+    const std::unordered_set<std::string>& get_names() const override {
+        return m_shared_tensor->get_names();
+    }
+
+    const std::string& get_any_name() const override {
+        return m_shared_tensor->get_any_name();
+    }
+
+    /** @brief Gets runtime map from shared tensor. */
+    const RTMap& rt_map() const override {
+        return m_shared_tensor->rt_map();
+    }
+
+    RTMap& rt_map() override {
+        return m_shared_tensor->rt_map();
+    }
+
+    size_t pointer_hash() const noexcept override {
+        return m_shared_tensor->pointer_hash();
+    }
+
+    OPENVINO_SUPPRESS_DEPRECATED_START
+    std::string& legacy_name() override {
+        return m_shared_tensor->legacy_name();
+    }
+
+    const std::string& legacy_name() const override {
+        return m_shared_tensor->legacy_name();
+    }
+    OPENVINO_SUPPRESS_DEPRECATED_END
+
+private:
+    void set_tensor(std::shared_ptr<ITensorDescriptor> tensor) {
+        OPENVINO_ASSERT(tensor, "Cannot set NULL tensor descriptor");
+        rm_tensor_output_names();
+        m_shared_tensor = std::move(tensor);
+        m_shared_tensor->add_names(m_output_names);
+    }
+
+    void rm_tensor_output_names() {
+        auto names = m_shared_tensor->get_names();
+        for (const auto& output_name : m_output_names) {
+            names.erase(output_name);
+        }
+
+        m_shared_tensor->set_names(names);
+    }
+
+    std::shared_ptr<ITensorDescriptor> m_shared_tensor;
+    std::unordered_set<std::string> m_output_names;
+};
+}  // namespace descriptor
+
 namespace op {
 namespace v0 {
 
@@ -22,10 +130,8 @@ void Result::validate_and_infer_types() {
     OV_OP_SCOPE(v0_Result_validate_and_infer_types);
     NODE_VALIDATION_CHECK(this, get_input_size() == 1, "Argument has ", get_input_size(), " outputs (1 expected).");
 
-    // Result doesn't change change in/out tensors
-    auto& output = get_output_descriptor(0);
-    auto& input = get_input_descriptor(0);
-    output.set_tensor_ptr(input.get_tensor_ptr());
+    // Result shares input tensor but can have specific properties which are added/removed to input.
+    descriptor::SharedTensor::update(get_output_descriptor(0), get_input_descriptor(0));
 }
 
 std::shared_ptr<Node> Result::clone_with_new_inputs(const OutputVector& new_args) const {
