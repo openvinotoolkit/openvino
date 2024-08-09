a8cb29a43f4503e73e389f21bdcc6aa9569020f4
xufang <fang.xu@intel.com>
2024-08-09 11:31:09 +0800

    Revert "update tbb package"
    This reverts commit c32346799221c657cf06adfe119d716cfdebfc6c.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -117,10 +117,10 @@ function(ov_download_tbb)
     elseif(LINUX AND X86_64 AND OPENVINO_GNU_LIBC AND OV_LIBC_VERSION VERSION_GREATER_EQUAL 2.17)
         # build oneTBB 2021.2.1 with gcc 4.8 (glibc 2.17)
         RESOLVE_DEPENDENCY(TBB
-                ARCHIVE_LIN "oneapi-tbb-2021.13.0-lin.tgz"
+                ARCHIVE_LIN "oneapi-tbb-2021.13.0-lin-release.tgz"
                 TARGET_PATH "${TEMP}/tbb"
                 ENVIRONMENT "TBBROOT"
-                SHA256 "bd782f003759e3b689bd33d3f596f8ad1cbedbf7e2e64f33a37d712c6bcabbbc"
+                SHA256 "fd2e889323cd5458be750eefdc026ce6791c723fae60b146c2511a5caeaf01c5"
                 USE_NEW_LOCATION TRUE)
     elseif(YOCTO_AARCH64)
         RESOLVE_DEPENDENCY(TBB
