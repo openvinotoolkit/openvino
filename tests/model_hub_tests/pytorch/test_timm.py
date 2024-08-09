90df951036e681828b713e4831cf0354d54e3a26
Przemyslaw Wysocki <przemyslaw.wysocki@intel.com>
2024-08-09 11:26:26 +0200

    Fix `output_size` dynamic dimensions support for `Col2Im-15` for CPU `StaticShape` (#25959)
    ### Details
    - `trace` case has been fixed, `export` case is still failing for PT FE
    affected models, but now with an error unrelated to `Col2Im`
    
    ### Tickets:
     - CVS-148673

--- tests/model_hub_tests/pytorch/test_timm.py
+++ tests/model_hub_tests/pytorch/test_timm.py
@@ -73,7 +73,8 @@ class TestTimmConvertModel(TestTorchConvertModel):
                                       "vit_base_patch8_224.augreg_in21k",
                                       "beit_base_patch16_224.in22k_ft_in22k",
                                       "sequencer2d_l.in1k",
-                                      "gcresnext26ts.ch_in1k"])
+                                      "gcresnext26ts.ch_in1k",
+                                      "volo_d2_224.sail_in1k"])
     @pytest.mark.precommit
     def test_convert_model_precommit(self, name, ie_device):
         self.mode = "trace"
