326dd3c919eeaa16ed8720f8abdfb2f7a7c47c21
p-wysocki <przemyslaw.wysocki@intel.com>
2024-08-09 13:36:28 +0200

    CR

--- src/core/reference/include/openvino/reference/string_tensor_unpack.hpp
+++ src/core/reference/include/openvino/reference/string_tensor_unpack.hpp
@@ -12,11 +12,11 @@ void string_tensor_unpack(const std::string* data,
                           int32_t* out_ends,
                           uint8_t* out_symbols,
                           const size_t element_count) {
-    size_t offset = 0;
+    int32_t offset = 0;
     for (size_t i = 0; i < element_count; ++i) {
         out_begins[i] = offset;
         out_symbols = std::copy(data[i].begin(), data[i].end(), out_symbols);
-        offset += data[i].length();
+        offset += static_cast<int32_t>(data[i].length());
         out_ends[i] = offset;
     }
 }
