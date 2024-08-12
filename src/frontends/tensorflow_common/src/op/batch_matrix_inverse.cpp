400a77c8da13f1fecdc82ed845fb0e8806b5dc96
Pey-crypto <nandu45@outlook.com>
2024-08-12 17:29:09 +0400

    Rewrote for loop and remove typecasting to improve readability

--- src/frontends/tensorflow_common/src/op/batch_matrix_inverse.cpp
+++ src/frontends/tensorflow_common/src/op/batch_matrix_inverse.cpp
@@ -22,21 +22,23 @@ OutputVector translate_batch_matrix_inverse_op(const NodeContext& node) {
 
     const auto input_shape = input.get_partial_shape();
     TENSORFLOW_OP_VALIDATION(node, input_shape.rank().is_static(), "BatchMatrixInverse input must have static rank");
-    const auto rank = input_shape.rank().get_length();
+    size_t rank = input_shape.rank().get_length();
     TENSORFLOW_OP_VALIDATION(node, rank >= 3, "BatchMatrixInverse input must have at least 3 dimensions");
 
-    std::vector<int64_t> combined_shape = {-1};
-    for (int i = rank - 2; i < rank; ++i ) {
-        combined_shape.push_back(input_shape[i].get_length());
+    std::vector<size_t> shape; 
+    for (const auto& dim : input_shape) {
+        shape.push_back(dim.get_length());
     }
 
+    std::vector<size_t> combined_shape = {0};
+    combined_shape.insert(combined_shape.end(), shape.end() - 2, shape.end());
     auto reshape_to_2d = std::make_shared<ov::op::v1::Reshape>(
         input,
         ov::op::v0::Constant::create(ov::element::i64, {combined_shape.size()}, combined_shape),
         false
     );
     auto inverse_2d = std::make_shared<ov::op::v14::Inverse>(reshape_to_2d, false);
-    auto original_shape = ov::op::v0::Constant::create(ov::element::i64, {static_cast<size_t>(rank)}, input_shape.get_shape());
+    auto original_shape = ov::op::v0::Constant::create(ov::element::i64, {rank}, input_shape.get_shape());
     auto reshape_back = std::make_shared<ov::op::v1::Reshape>(inverse_2d, original_shape, false);
 
     set_node_name(node.get_name(), reshape_back);
