2b876d6d5a94c6f70c18b70d8766afd45a11dc0a
Min, Byungil <byungil.min@intel.com>
2024-08-12 17:00:50 +0900

    [GPU] Bugfix mis-match axes size in ConvertMatMulToFullyConnected (#25570)
    + Resolved execution failure of segnext model
    
    ### Details:
     - *item1*
     - *...*
    
    ### Tickets:
     - 146597
    
    ---------
    
    Signed-off-by: Min, Byung-il <byungil.min@intel.com>

--- src/plugins/intel_gpu/src/plugin/transformations/convert_matmul_to_fc.cpp
+++ src/plugins/intel_gpu/src/plugin/transformations/convert_matmul_to_fc.cpp
@@ -41,10 +41,24 @@ ConvertMatMulToFullyConnected::ConvertMatMulToFullyConnected() {
             return false;
         }
 
-        // fc_input_a and fc_input_b - are the final inputs that will be set to FullyConnected.
-        // So in case of adding new operations that takes matmul inputs we need keep update fc_input_a and fc_input_b.
         auto fc_input_a = pattern_map.at(activations_m);
         auto fc_input_b = pattern_map.at(weights_m);
+
+        // Not to convert fc_input_b which should be fc_input_a of other sibling MatMul
+        auto input_b = fc_input_b.get_node_shared_ptr();
+        for (auto& user : input_b->get_users()) {
+            if (user != matmul && ov::is_type<ov::op::v0::MatMul>(user) && ov::is_type<ov::op::v0::Convert>(input_b)) {
+                auto other_matmul = std::dynamic_pointer_cast<ov::op::v0::MatMul>(user);
+                // Transpose for input_b generates invalid input for other sibling matmul
+                if (input_b == other_matmul->get_input_node_shared_ptr(0) || fc_input_b == fc_input_a ||
+                    (input_b == other_matmul->get_input_node_shared_ptr(1) && matmul->get_transpose_b() != other_matmul->get_transpose_b())) {
+                    return false;
+                }
+            }
+        }
+
+        // fc_input_a and fc_input_b - are the final inputs that will be set to FullyConnected.
+        // So in case of adding new operations that takes matmul inputs we need keep update fc_input_a and fc_input_b.
         bool is_convert = false;
         if (auto convert_node = std::dynamic_pointer_cast<ov::op::v0::Convert>(fc_input_b.get_node_shared_ptr())) {
             is_convert = true;
