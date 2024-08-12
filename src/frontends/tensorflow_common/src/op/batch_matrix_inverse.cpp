11e4774d6570b4aecfde3fd593970976f4c868ac
Pey-crypto <nandu45@outlook.com>
2024-08-13 00:50:32 +0400

    Fix clang format error and missing closer,

--- src/frontends/tensorflow_common/src/op/batch_matrix_inverse.cpp
+++ src/frontends/tensorflow_common/src/op/batch_matrix_inverse.cpp
@@ -3,10 +3,10 @@
 //
 
 #include "common_op_table.hpp"
+#include "openvino/core/validation_util.hpp"
+#include "openvino/op/constant.hpp"
 #include "openvino/op/inverse.hpp"
 #include "openvino/op/reshape.hpp"
-#include "openvino/op/constant.hpp"
-#include "openvino/core/validation_util.hpp"
 
 namespace ov {
 namespace frontend {
@@ -15,7 +15,9 @@ namespace op {
 
 OutputVector translate_batch_matrix_inverse_op(const NodeContext& node) {
     const auto op_type = node.get_op_type();
-    TENSORFLOW_OP_VALIDATION(node, op_type == "BatchMatrixInverse", "Internal error: incorrect usage of translate_batch_matrix_inverse_op.");
+    TENSORFLOW_OP_VALIDATION(node,
+                             op_type == "BatchMatrixInverse",
+                             "Internal error: incorrect usage of translate_batch_matrix_inverse_op.");
 
     const auto input = node.get_input(0);
     const bool adjoint = node.get_attribute<bool>("adjoint", false);
@@ -25,7 +27,7 @@ OutputVector translate_batch_matrix_inverse_op(const NodeContext& node) {
     size_t rank = input_shape.rank().get_length();
     TENSORFLOW_OP_VALIDATION(node, rank >= 3, "BatchMatrixInverse input must have at least 3 dimensions");
 
-    std::vector<size_t> shape; 
+    std::vector<size_t> shape;
     for (const auto& dim : input_shape) {
         shape.push_back(dim.get_length());
     }
@@ -35,14 +37,14 @@ OutputVector translate_batch_matrix_inverse_op(const NodeContext& node) {
     auto reshape_to_2d = std::make_shared<ov::op::v1::Reshape>(
         input,
         ov::op::v0::Constant::create(ov::element::i64, {combined_shape.size()}, combined_shape),
-        false
-    );
+        false);
     auto inverse_2d = std::make_shared<ov::op::v14::Inverse>(reshape_to_2d, false);
     auto original_shape = ov::op::v0::Constant::create(ov::element::i64, {rank}, input_shape.get_shape());
     auto reshape_back = std::make_shared<ov::op::v1::Reshape>(inverse_2d, original_shape, false);
 
     set_node_name(node.get_name(), reshape_back);
     return {reshape_back};
+}
 }  // namespace op
 }  // namespace tensorflow
 }  // namespace frontend
