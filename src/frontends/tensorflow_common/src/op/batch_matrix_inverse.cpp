175abef60ac5601b857a3c8f7e2173646dffa86a
Pey-crypto <nandu45@outlook.com>
2024-08-13 18:02:27 +0400

    rewrote batch_matrix_inverse to use inverse.

--- src/frontends/tensorflow_common/src/op/batch_matrix_inverse.cpp
+++ src/frontends/tensorflow_common/src/op/batch_matrix_inverse.cpp
@@ -14,32 +14,21 @@ namespace tensorflow {
 namespace op {
 
 OutputVector translate_batch_matrix_inverse_op(const NodeContext& node) {
-    const auto op_type = node.get_op_type();
-    TENSORFLOW_OP_VALIDATION(node,
-                             op_type == "BatchMatrixInverse",
-                             "Internal error: incorrect usage of translate_batch_matrix_inverse_op.");
-
-    const auto input = node.get_input(0);
+    default_op_checks(node, 1, {"BatchMatrixInverse"});
+    auto input = node.get_input(0);
     const auto input_shape = input.get_partial_shape();
-    TENSORFLOW_OP_VALIDATION(node, input_shape.rank().is_static(), "BatchMatrixInverse input must have static rank");
     size_t rank = input_shape.rank().get_length();
     TENSORFLOW_OP_VALIDATION(node, rank >= 3, "BatchMatrixInverse input must have at least 3 dimensions");
 
-    std::vector<size_t> shape;
-    for (const auto& dim : input_shape) {
-        shape.push_back(dim.get_length());
-    }
-
     std::vector<size_t> combined_shape = {0};
-    combined_shape.insert(combined_shape.end(), shape.end() - 2, shape.end());
+    combined_shape.insert(combined_shape.end(), input_shape.end() - 2, input_shape.end());
     auto reshape_to_2d = std::make_shared<ov::op::v1::Reshape>(
         input,
         ov::op::v0::Constant::create(ov::element::i64, {combined_shape.size()}, combined_shape),
         false);
-    auto inverse_2d = std::make_shared<ov::op::v14::Inverse>(reshape_to_2d, false);
+    auto inverse_2d = std::make_shared<ov::op::v14::Inverse>(reshape_to_2d, node.get_attribute<bool>("adjoint", false));
     auto original_shape = ov::op::v0::Constant::create(ov::element::i64, {rank}, input_shape.get_shape());
     auto reshape_back = std::make_shared<ov::op::v1::Reshape>(inverse_2d, original_shape, false);
-
     set_node_name(node.get_name(), reshape_back);
     return {reshape_back};
 }
