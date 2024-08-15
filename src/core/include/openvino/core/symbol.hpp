0d11a7b22f1464aa859d447648899f720167ff51
Evgenya Nugmanova <evgeniia.nugmanova@intel.com>
2024-08-15 08:05:04 -0700

    Symbol: operator+; operator-; (#25360)
    ### Details:
     - *Introduces operator+ and operator- for ov::Symbol*
    - *Uses introduced operators in `Add::evaluate_symbol`,
    `Subtract::evaluate_symbol` and shape_infer of Range operation*
    
    Significantly reduces the number of unique symbols in models, allowing
    better optimization of ShapeOf subgraphs in dynamic models.
    
    ### Tickets:
     - *CVS-112292*
    
    ---------
    
    Co-authored-by: Pavel Durandin <pavel.durandin@intel.com>
    Co-authored-by: Michal Lukaszewski <michal.lukaszewski@intel.com>
    Co-authored-by: Anastasia Kuporosova <anastasia.kuporosova@intel.com>
    Co-authored-by: Ivan Tikhonov <ivan.tikhonov@intel.com>

--- src/core/include/openvino/core/symbol.hpp
+++ src/core/include/openvino/core/symbol.hpp
@@ -3,37 +3,46 @@
 //
 
 #pragma once
-
 #include <memory>
+#include <unordered_map>
+#include <vector>
 
 #include "openvino/core/core_visibility.hpp"
 
 namespace ov {
-
 class Symbol;
+using SymbolPtr = std::shared_ptr<ov::Symbol>;
 
 namespace symbol {
 /// \brief If both symbols are valid, sets them as equal
-void OPENVINO_API set_equal(const std::shared_ptr<Symbol>& lhs, const std::shared_ptr<Symbol>& rhs);
+void OPENVINO_API set_equal(const SymbolPtr& lhs, const SymbolPtr& rhs);
 /// \brief Returns true if both symbols are valid and are equal otherwise returns false
-bool OPENVINO_API are_equal(const std::shared_ptr<Symbol>& lhs, const std::shared_ptr<Symbol>& rhs);
+bool OPENVINO_API are_equal(const SymbolPtr& lhs, const SymbolPtr& rhs);
 /// \brief Returns a representative (the most distant parent) of an equality group of this symbol
-std::shared_ptr<Symbol> OPENVINO_API ancestor_of(const std::shared_ptr<Symbol>& x);
+std::shared_ptr<Symbol> OPENVINO_API ancestor_of(const SymbolPtr& x);
 }  // namespace symbol
 
-/// \brief Class representing unique symbol for the purpose of symbolic shape inference. Equality of symbols is being
-/// tracked by Disjoint-set data structure
+SymbolPtr OPENVINO_API operator+(const SymbolPtr& lhs, const SymbolPtr& rhs);
+SymbolPtr OPENVINO_API operator-(const SymbolPtr& lhs, const SymbolPtr& rhs);
+
+/// \brief Class representing unique symbol for the purpose of symbolic shape inference
 /// \ingroup ov_model_cpp_api
 class OPENVINO_API Symbol {
 public:
     /// \brief Default constructs a unique symbol
-    Symbol() = default;
+    Symbol();
+    /// @brief Destructor ensures shared MathMaps are cleared from the expired symbols
+    ~Symbol();
 
 private:
-    friend std::shared_ptr<Symbol> ov::symbol::ancestor_of(const std::shared_ptr<Symbol>& x);
-    friend void ov::symbol::set_equal(const std::shared_ptr<Symbol>& lhs, const std::shared_ptr<Symbol>& rhs);
+    friend SymbolPtr ov::symbol::ancestor_of(const SymbolPtr& x);
+    friend void ov::symbol::set_equal(const SymbolPtr& lhs, const SymbolPtr& rhs);
 
-    std::shared_ptr<Symbol> m_parent = nullptr;
+    friend SymbolPtr ov::operator+(const SymbolPtr& lhs, const SymbolPtr& rhs);
+    friend SymbolPtr ov::operator-(const SymbolPtr& lhs, const SymbolPtr& rhs);
+
+    class Impl;
+    std::unique_ptr<Impl> pimpl;
 };
 
 }  // namespace ov
