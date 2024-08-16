c97bb311c97409dd94497feb0ef041d16229eb0c
Evgeny Talanin <evgeny.talanin@intel.com>
2024-08-16 20:36:44 +0400

    Update convert_pad_to_group_conv.cpp

--- src/common/transformations/src/transformations/op_conversions/convert_pad_to_group_conv.cpp
+++ src/common/transformations/src/transformations/op_conversions/convert_pad_to_group_conv.cpp
@@ -64,7 +64,7 @@ ov::pass::ConvertPadToGroupConvolution::ConvertPadToGroupConvolution() {
             return a < 0;
         };
         if (std::any_of(pad_begin.begin(), pad_begin.end(), pred) ||
-            std::any_of(pad_begin.begin(), pad_begin.end(), pred)) {
+            std::any_of(pad_end.begin(), pad_end.end(), pred)) {
             return false;
         }
 
@@ -74,7 +74,9 @@ ov::pass::ConvertPadToGroupConvolution::ConvertPadToGroupConvolution() {
                         [](ptrdiff_t value) {
                             return value != 0;
                         }) ||
-            std::any_of(pad_end.begin(), pad_end.begin() + 2, [](ptrdiff_t value) {
+            std::any_of(pad_end.begin(),
+                        pad_end.begin() + 2,
+                        [](ptrdiff_t value) {
                 return value != 0;
             })) {
             return false;
