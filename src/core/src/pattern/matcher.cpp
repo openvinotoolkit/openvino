b55e48d1eebcb0de7b095939689860d7eeb239fb
Evgeny Talanin <evgeny.talanin@intel.com>
2024-08-16 20:42:47 +0400

    Update matcher.cpp

--- src/core/src/pattern/matcher.cpp
+++ src/core/src/pattern/matcher.cpp
@@ -44,9 +44,9 @@ MatcherState::~MatcherState() {
                                             m_matcher->m_matched_list.end());
         }
 
-        if (!m_pattern_value_maps.empty()) {
-            m_matcher->m_pattern_value_maps.erase(m_pattern_value_maps.begin() + m_capture_size,
-                                                  m_pattern_value_maps.end());
+        if (!m_matcher->m_pattern_value_maps.empty()) {
+            m_matcher->m_pattern_value_maps.erase(m_matcher->m_pattern_value_maps.begin() + m_capture_size,
+                                                  m_matcher->m_pattern_value_maps.end());
         }
 
         m_matcher->m_pattern_map = m_pattern_value_map;
