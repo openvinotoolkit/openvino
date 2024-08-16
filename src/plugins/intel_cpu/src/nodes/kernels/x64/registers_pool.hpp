d1fc863c423cafbaedc1809d559d10bdeee7fc25
Evgeny Talanin <evgeny.talanin@intel.com>
2024-08-16 21:39:49 +0400

    Update registers_pool.hpp

--- src/plugins/intel_cpu/src/nodes/kernels/x64/registers_pool.hpp
+++ src/plugins/intel_cpu/src/nodes/kernels/x64/registers_pool.hpp
@@ -225,6 +225,8 @@ private:
             return idx;
         } else if (std::is_same<TReg, Xbyak::Opmask>::value) {
             return getFreeOpmask(requestedIdx);
+        } else {
+            static_assert(sizeof(TReg*) == 0, "Undefined behavior");
         }
     }
 
