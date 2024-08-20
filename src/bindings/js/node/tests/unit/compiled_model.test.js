a8df5a71477b9ddfe115e79de48a0576c4df287b
almilosz <alicja.miloszewska@intel.com>
2024-08-20 09:30:52 +0200

    Run lint fix

--- src/bindings/js/node/tests/unit/compiled_model.test.js
+++ src/bindings/js/node/tests/unit/compiled_model.test.js
@@ -13,73 +13,73 @@ describe('ov.CompiledModel tests', () => {
     await checkTestModel(testModels.testModelFP32);
   });
 
-const testXml = getModelPath().xml;
-const core = new ov.Core();
-const properties = {
-  "AUTO_BATCH_TIMEOUT": '1'
-};
-const compiledModel = core.compileModelSync(testXml, 'BATCH:CPU', properties);
+  const testXml = getModelPath().xml;
+  const core = new ov.Core();
+  const properties = {
+    'AUTO_BATCH_TIMEOUT': '1',
+  };
+  const compiledModel = core.compileModelSync(testXml, 'BATCH:CPU', properties);
 
-describe('setProperty() / getProperty()', () => {
+  describe('setProperty() / getProperty()', () => {
 
-  describe('getProperty()', () => {
-    it('returns the value of property from compiled model', () => {
-      assert.strictEqual(compiledModel.getProperty('AUTO_BATCH_TIMEOUT'), '1');
+    describe('getProperty()', () => {
+      it('returns the value of property from compiled model', () => {
+        assert.strictEqual(compiledModel.getProperty('AUTO_BATCH_TIMEOUT'), '1');
+      });
+      it('throws an error when called without arguments', () => {
+        assert.throws(
+          () => compiledModel.getProperty(),
+          /'getProperty' method called with incorrect parameters/
+        );
+      });
+      it('throws an error when called with property name that does not exists', () => {
+        assert.throws(
+          () => compiledModel.getProperty('PROPERTY_THAT_DOES_NOT_EXIST')
+        );
+      });
     });
-    it('throws an error when called without arguments', () => {
-      assert.throws(
-        () => compiledModel.getProperty(),
-        /'getProperty' method called with incorrect parameters/
-      );
-    });
-    it('throws an error when called with property name that does not exists', ()=>{
-      assert.throws(
-        () => compiledModel.getProperty('PROPERTY_THAT_DOES_NOT_EXIST')
-      );
-    });
-  });
 
-  describe('setProperty()', () => {
-    it('sets a properties for compiled model', () => {
-      properties["AUTO_BATCH_TIMEOUT"] = '1000';
-      assert.doesNotThrow(() => compiledModel.setProperty(properties));
-    });
-  
-    it('throws an error when called without an object argument', () => {
-      assert.throws(
-        () => compiledModel.setProperty(),
-        /'setProperty' method called with incorrect parameters/
-      );
-    });
-    it('throws an error when called with wrong argument', () => {
-      assert.throws(
+    describe('setProperty()', () => {
+      it('sets a properties for compiled model', () => {
+        properties['AUTO_BATCH_TIMEOUT'] = '1000';
+        assert.doesNotThrow(() => compiledModel.setProperty(properties));
+      });
+
+      it('throws an error when called without an object argument', () => {
+        assert.throws(
+          () => compiledModel.setProperty(),
+          /'setProperty' method called with incorrect parameters/
+        );
+      });
+      it('throws an error when called with wrong argument', () => {
+        assert.throws(
           () => compiledModel.setProperty(123),
           /'setProperty' method called with incorrect parameters/
-      );
-    });
-  
-    it('throws an error when called with multiple arguments', () => {
-      assert.throws(
-        () => compiledModel.setProperty({"PERFORMANCE_HINT": "THROUGHPUT"}, {"NUM_STREAMS": "AUTO"}),
-        /'setProperty' method called with incorrect parameters/
-      );
-    });
-  
-    it('returns the set property of the compiled model', () => {
-      properties["AUTO_BATCH_TIMEOUT"] = '123';
-      compiledModel.setProperty(properties);
-      assert.strictEqual(compiledModel.getProperty('AUTO_BATCH_TIMEOUT'), 123);
-    });
-  
-    it('retains the last set property when set multiple times', () => {
-      compiledModel.setProperty({"AUTO_BATCH_TIMEOUT": '321'});
-      compiledModel.setProperty({'AUTO_BATCH_TIMEOUT': '132'});
-      assert.strictEqual(compiledModel.getProperty('AUTO_BATCH_TIMEOUT'), 132);
-    });
-  
-    it('allows to pass empty object', () => {
-      assert.doesNotThrow(() => compiledModel.setProperty({}));
+        );
+      });
+
+      it('throws an error when called with multiple arguments', () => {
+        assert.throws(
+          () => compiledModel.setProperty({'PERFORMANCE_HINT': 'THROUGHPUT'}, {'NUM_STREAMS': 'AUTO'}),
+          /'setProperty' method called with incorrect parameters/
+        );
+      });
+
+      it('returns the set property of the compiled model', () => {
+        properties['AUTO_BATCH_TIMEOUT'] = '123';
+        compiledModel.setProperty(properties);
+        assert.strictEqual(compiledModel.getProperty('AUTO_BATCH_TIMEOUT'), 123);
+      });
+
+      it('retains the last set property when set multiple times', () => {
+        compiledModel.setProperty({'AUTO_BATCH_TIMEOUT': '321'});
+        compiledModel.setProperty({'AUTO_BATCH_TIMEOUT': '132'});
+        assert.strictEqual(compiledModel.getProperty('AUTO_BATCH_TIMEOUT'), 132);
+      });
+
+      it('allows to pass empty object', () => {
+        assert.doesNotThrow(() => compiledModel.setProperty({}));
+      });
     });
   });
-});
 });
