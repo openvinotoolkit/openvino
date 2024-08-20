8f74962661b9925c87528f163c3d1457ca4d9ce2
michal-miotk <michal.miotk@intel.com>
2024-08-20 10:26:18 +0000

    Revert "fix for vec size, deleted MAX_SEQ_LENGTH"
    This reverts commit a165f30673b76f9c21fdb70292b2aa3517c32fc6.

--- src/plugins/intel_gpu/src/kernel_selector/kernels/lstm/lstm_kernel_base.cpp
+++ src/plugins/intel_gpu/src/kernel_selector/kernels/lstm/lstm_kernel_base.cpp
@@ -16,11 +16,12 @@ JitConstants LSTMKernelBase::GetJitConstants(const lstm_params& params, bool seq
     if (params.input_forget) {
         jit.AddConstants({MakeJitConstant("INPUT_FORGET", true)});
     }
-    jit.AddConstants({MakeJitConstant("VEC_SIZE", 8)});
+    jit.AddConstants({MakeJitConstant("VEC_SIZE", 4)});
     jit.AddConstants({MakeJitConstant("DIRECTION", static_cast<int>(params.direction))});
     jit.AddConstants({MakeJitConstant("GATE_NUM", 4)});
     if (sequential) {
         jit.AddConstants({MakeJitConstant("SEQUENCE", 1)});
+        jit.AddConstants({MakeJitConstant("MAX_SEQ_LENGTH", params.inputs[0].Feature().v)});
     }
     int num_hidden_kernels;
     int hidden_size;
