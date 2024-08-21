a4969466c1b4f710b08c9fc002766c0fa0fadb33
almilosz <alicja.miloszewska@intel.com>
2024-08-21 11:27:03 +0200

    Move require.main on top

--- src/bindings/js/node/tests/unit/setup.js
+++ src/bindings/js/node/tests/unit/setup.js
@@ -1,9 +1,9 @@
 const { testModels, downloadTestModel } = require('./utils.js');
 
-async function main() {
-  await downloadTestModel(testModels.testModelFP32);
-}
-
 if (require.main === module) {
   main();
+}
+
+async function main() {
+  await downloadTestModel(testModels.testModelFP32);
 }
