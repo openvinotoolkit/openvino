78f6e596428330c2f9357b94dac2564fc63c82e4
mitruska <katarzyna.mitrus@intel.com>
2024-08-21 18:17:21 +0200

    skip overflow tests for i16 u16

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/skip_tests_config.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/skip_tests_config.cpp
@@ -309,6 +309,7 @@ std::vector<std::string> disabledTestPatterns() {
         // Issue: 148527
         R"(.*Snippets.*MatMulTransposeB.*i8.*i8.*)",
         // Issue: 136881
+        R"(.*smoke_CompareWithRefs_4D_BitwiseShift_overflow_i32_cast.*_eltwise_op_type=BitwiseLeft.*_model_type=.*(i16|u16).*)",
     };
 
     // fp32 floor for bf16 models: conversion issue
