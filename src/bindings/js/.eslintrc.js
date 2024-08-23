76b847a9d2ab971a454ab84d84a901c686684c8c
Vishniakov Nikolai <nikolai.vishniakov@intel.com>
2024-08-23 12:02:45 +0200

    Temporary remove @typescript-eslint/eslint-plugin

--- src/bindings/js/.eslintrc.js
+++ src/bindings/js/.eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    // TODO: Temporary disabled
+    // Uncomment when will https://github.com/micromatch/micromatch/issues/264 be fixed
+    // 'plugin:@typescript-eslint/recommended',
     './.eslintrc-global.js',
   ],
   ignorePatterns: ['**/*.js', 'node_modules/', 'types/', 'dist/', 'bin/'],
--- src/bindings/js/node/.eslintrc.js
+++ src/bindings/js/node/.eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
   extends: ['../.eslintrc.js'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  // TODO: Temporary disabled
+  // Uncomment when will https://github.com/micromatch/micromatch/issues/264 be fixed
+  // parser: '@typescript-eslint/parser',
+  // plugins: ['@typescript-eslint'],
 };
