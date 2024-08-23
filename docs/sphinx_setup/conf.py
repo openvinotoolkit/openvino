2464f37c0b44212fb0e1c2d7b054991f568c6582
tadamczx <tomaszx.adamczyk@intel.com>
2024-08-23 08:30:09 +0200

    sidebars restoration

--- docs/sphinx_setup/conf.py
+++ docs/sphinx_setup/conf.py
@@ -114,7 +114,7 @@ html_theme_options = {
 }
 
 html_sidebars = {
-    "**": ["search-field.html", "sidebar-ethical-ads.html"]
+    "**": ["search-field.html", "sidebar-nav-bs.html", "sidebar-ethical-ads.html"]
 }
 
 html_context = {
