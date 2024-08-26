d84cea65cc2ef0c99ed5e1a7cab84cd075bff9f7
p-wysocki <przemyslaw.wysocki@intel.com>
2024-08-26 15:27:12 +0200

    Minor change

--- src/bindings/python/tests/test_runtime/test_compiled_model.py
+++ src/bindings/python/tests/test_runtime/test_compiled_model.py
@@ -96,26 +96,6 @@ def test_export_import_large_model(device):
     assert np.argmax(res[new_compiled.outputs[0]]) == 44372822
 
 
-def test_import_large_model_with_string_input(device):
-    import io
-    core = Core()
-    if props.device.Capability.EXPORT_IMPORT not in core.get_property(device, props.device.capabilities):
-        pytest.skip(f"{core.get_property(device, props.device.full_name)} plugin due-to export, import model API isn't implemented.")
-
-    # model of size of roughly 2.2GB
-    model = generate_model_with_memory([6, 10000, 10000], Type.f32)
-    core = Core()
-    compiled_model = core.compile_model(model, device, {})
-    user_stream = io.BytesIO()
-    compiled_model.export_model(user_stream)
-    user_stream.seek(0)
-
-    new_compiled = core.import_model(user_stream.read().decode('utf-8'), "CPU")
-    img = generate_image([6, 10000, 10000])
-    res = new_compiled.infer_new_request({"input_data": img})
-    assert np.argmax(res[new_compiled.outputs[0]]) == 44372822
-
-
 @pytest.mark.parametrize("input_arguments", [[0], ["data"], []])
 def test_get_input(device, input_arguments):
     compiled_model = generate_relu_compiled_model(device)
