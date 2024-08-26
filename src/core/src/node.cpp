59b65591aafe333534081c37d4684f7cf300bf4f
andrei-cv <andrei.gorbachev@intel.com>
2024-08-26 23:56:48 +0400

    need for nvidia_plugin/src/ops/split.cpp:49

--- src/core/src/node.cpp
+++ src/core/src/node.cpp
@@ -485,7 +485,7 @@ const ov::element::Type& ov::Node::get_input_element_type(size_t i) const {
     return m_inputs[i].get_element_type();
 }
 
-ov::Shape ov::Node::get_input_shape(size_t i) const {
+const ov::Shape& ov::Node::get_input_shape(size_t i) const {
     OPENVINO_ASSERT(i < m_inputs.size(), idx_txt, i, out_of_range_txt);
     return m_inputs[i].get_shape();
 }
