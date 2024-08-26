2aa4e03e244eb49073efcd42da58956cb9c8c946
Jade Cho <jade.cho@intel.com>
2024-08-26 14:14:56 +0900

    [GPU] Optimize reduce and deconvolution (#25770)
    + Swap X and Y axes when the size of X-axis is 1.
    + Add a new reduce kernel for scalar output.
    
    ### Tickets:
     - *138954*

--- src/plugins/intel_gpu/tests/unit/test_cases/reduce_gpu_test.cpp
+++ src/plugins/intel_gpu/tests/unit/test_cases/reduce_gpu_test.cpp
@@ -2202,3 +2202,17 @@ TEST_P(general_reduce_gpu_xy_i8, base_cached) { execute(true); }
 TEST(reduce_gpu, common_bfyx_cached) {
     test_common_bfyx<float>(true);
 }
+
+class reduce_scalar_output_f16_f16 : public ReduceTestBase<data_types::f16, data_types::f16> {};
+TEST_P(reduce_scalar_output_f16_f16, base) { execute(false); }
+
+INSTANTIATE_TEST_SUITE_P(reduce_scalar_output_f16_f16,
+                        reduce_scalar_output_f16_f16,
+                        ::testing::Values(
+                            TestParamType_general_reduce_gpu(1, 1, 1, 1, 1013, 2, format::bfyx, reduce_mode::sum, {3, 2, 1, 0},  "reduce_simple_to_scalar", false, data_types::f16, false, data_types::f16),
+                            TestParamType_general_reduce_gpu(1, 1, 1, 1, 1013, 2, format::bfyx, reduce_mode::min, {3, 2, 1, 0},  "reduce_simple_to_scalar", false, data_types::f16, false, data_types::f16),
+                            TestParamType_general_reduce_gpu(1, 1, 1, 1, 1013, 2, format::bfyx, reduce_mode::mean, {3, 2, 1, 0}, "reduce_simple_to_scalar", false, data_types::f16, false, data_types::f16),
+                            TestParamType_general_reduce_gpu(1, 1, 1, 1, 1013, 2, format::bfyx, reduce_mode::max, {3, 2, 1, 0},  "reduce_simple_to_scalar", false, data_types::f16, false, data_types::f16),
+                            TestParamType_general_reduce_gpu(1, 1, 1, 1, 1024, 1, format::bfyx, reduce_mode::min, {3, 2, 1, 0},  "reduce_simple_to_scalar", false, data_types::f16, false, data_types::f16),
+                            TestParamType_general_reduce_gpu(1, 1, 1, 1, 1025, 1, format::bfyx, reduce_mode::min, {3, 2, 1, 0},  "reduce_simple_to_scalar", false, data_types::f16, false, data_types::f16)
+                        ), general_reduce_gpu::PrintToStringParamName);
