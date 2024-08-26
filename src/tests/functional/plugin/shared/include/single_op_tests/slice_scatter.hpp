1fabad0e65a04fcbe1351fe3eadcddc13bcb7bcb
Mateusz <mateusz.mikolajczyk@intel.com>
2024-08-26 13:22:31 +0200

    Add single_op CPU tests

---
+++ src/tests/functional/plugin/shared/include/single_op_tests/slice_scatter.hpp
@@ -0,0 +1,16 @@
+// Copyright (C) 2018-2024 Intel Corporation
+// Copyright (C) 2018-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include "shared_test_classes/single_op/slice_scatter.hpp"
+
+namespace ov {
+namespace test {
+TEST_P(SliceScatterLayerTest, Inference) {
+    run();
+}
+}  // namespace test
+}  // namespace ov
