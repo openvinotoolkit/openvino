e3b06e9f351e8acb285d7f4e62856672ac8206ce
Anastasia Kuporosova <anastasia.kuporosova@intel.com>
2024-08-27 16:41:32 +0200

    [PyOV] make Tensor responsible for keeping numpy memory (#26209)
    ### Details:
    - if `shared_memory=True` and for other ctors which create Tensor with
    `shared_memory` added ability to keep host memory.
    - if `shared_memory=False` still creates a copy and doesn't tie arrays
    lifetime to Tensor
    - implemented `custom_keep_alive` (custom calling policy) which adds
    such ability
    
    ### Tickets:
     - CVS-121493

--- src/bindings/python/src/pyopenvino/utils/utils.hpp
+++ src/bindings/python/src/pyopenvino/utils/utils.hpp
@@ -66,3 +66,24 @@ namespace utils {
 
 }; // namespace utils
 }; // namespace Common
+
+namespace pybind11 {
+namespace ov_extension {
+    void conditional_keep_alive_impl(size_t Nurse, size_t Patient, size_t Condition, detail::function_call &call, handle ret);
+
+    // Keep patient alive while nurse lives
+    template <size_t Nurse, size_t Patient, size_t Condition>
+    struct conditional_keep_alive {};
+
+}; // namespace ov_extension
+
+    // Process a conditional_keep_alive call policy -- invokes conditional_keep_alive_impl during the the pre-call handler
+    template <size_t Nurse, size_t Patient, size_t Condition>
+    struct detail::process_attribute<ov_extension::conditional_keep_alive<Nurse, Patient, Condition>>
+        : public detail::process_attribute_default<ov_extension::conditional_keep_alive<Nurse, Patient, Condition>> {
+        static void precall(function_call &call) {
+            ov_extension::conditional_keep_alive_impl(Nurse, Patient, Condition, call, handle());
+        }
+    };
+
+}; // namespace pybind11
