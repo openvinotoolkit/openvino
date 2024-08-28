6e5f393e8306ed6592bc600ddca4f6739295f7df
Xin Wang <xin1.wang@intel.com>
2024-08-28 16:43:26 +0800

    Keep blob alive for import_model path

--- src/plugins/intel_npu/src/plugin/src/compiled_model.cpp
+++ src/plugins/intel_npu/src/plugin/src/compiled_model.cpp
@@ -87,8 +87,7 @@ CompiledModel::CompiledModel(const std::shared_ptr<const ov::Model>& model,
       _config(config),
       _logger("CompiledModel", config.get<LOG_LEVEL>()),
       _device(device),
-      _compiler(compiler),
-      _imported(true) {
+      _compiler(compiler) {
     OV_ITT_SCOPED_TASK(itt::domains::NPUPlugin, "CompiledModel::CompiledModel");
     OPENVINO_ASSERT(_networkPtr != nullptr,
                     "NPU CompiledModel: the pointer towards the NetworkDescription object is null");
@@ -140,14 +139,7 @@ std::shared_ptr<ov::ISyncInferRequest> CompiledModel::create_sync_infer_request(
 
 void CompiledModel::export_model(std::ostream& stream) const {
     _logger.debug("CompiledModel::export_model");
-
-    if (_imported) {
-        OPENVINO_THROW("The CompiledModel is created from import_model path."
-                       "Application shall already hold a compiled model in memory!");
-    }
-
     const auto&& blob = _compiler->getCompiledNetwork(_networkPtr);
-
     stream.write(reinterpret_cast<const char*>(blob.data()), blob.size());
     std::stringstream str;
     str << "Blob size: " << blob.size() << ", hash: " << std::hex << hash(blob);
