8afe4095162a3710096891bdf3372b65c65792e3
Piotr Kowalczyk <piotr.kowalczyk@intel.com>
2024-08-28 12:46:33 +0000

    [GPU][EltwiseKernelBase]: Fixed comment due to review.

--- src/plugins/intel_gpu/src/kernel_selector/kernels/eltwise/eltwise_kernel_base.cpp
+++ src/plugins/intel_gpu/src/kernel_selector/kernels/eltwise/eltwise_kernel_base.cpp
@@ -80,9 +80,9 @@ ParamsKey eltwise_params::GetParamsKey() const {
 }
 
 Datatype EltwiseKernelBase::GetAccumulatorType(const eltwise_params &params) const {
-    // NOTE: 'Workaround' discussed with W. Paramuzov of not promoting shift operations. Not sure what should happen
+    // NOTE: Workaround for not promoting shift operations. Not sure what should happen
     // if shift op is just one operation of other elementwise operations. My guess is that is should be promoted as
-    // well, but in reality more robust solution will be needed or (better) assumption that types are not promoted. So
+    // well, but in reality more robust solution will be needed or (better) - assumption that types are not promoted. So
     // probably this is a temporary solution.
     if (params.operations[0].mode == EltwiseMode::RIGHT_SHIFT || params.operations[0].mode == EltwiseMode::LEFT_SHIFT) {
         return params.inputs[0].GetDType();
