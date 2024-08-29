527efe29c0b3a76c0f2e6642305670691e6e5e6e
hub-bla <hubertblaszczyk03@gmail.com>
2024-08-29 20:08:37 +0200

    make code more readable

--- src/frontends/tensorflow_common/src/op/rsqrt.cpp
+++ src/frontends/tensorflow_common/src/op/rsqrt.cpp
@@ -38,15 +38,14 @@ OutputVector translate_rsqrt_op(const NodeContext& node) {
         element::Type complex_part_type = complex_type_mark->get_complex_part_type();
         input = complex_type_mark->input_value(0);
         // input is complex tensor representation in a form [N1, N2, ..., Nk, 2]
-        // where slice [N1, N2, ..., Nk, 0] contains real part of the complex
-        // tensor and slice [N1, N2, ..., Nk, 1] contains imaginary part of the
-        // complex tensor compute sum of squared real and imaginary parts
+        // where slice [N1, N2, ..., Nk, 0] contains real part of the complex tensor
+        // and slice [N1, N2, ..., Nk, 1] contains imaginary part of the complex tensor
 
         auto gather_index_real = make_shared<v0::Constant>(element::i64, Shape{}, 0);
         auto gather_index_imag = make_shared<v0::Constant>(element::i64, Shape{}, 1);
         auto minus_one = make_shared<v0::Constant>(element::i32, Shape{1}, -1);
 
-        // complex_number: z = a + ib ; real_part=a ; imag_part = b
+        // complex_number: z = a + jb ; real_part=a ; imag_part = b
         auto real_part = make_shared<v8::Gather>(input, gather_index_real, minus_one);
         auto imag_part = make_shared<v8::Gather>(input, gather_index_imag, minus_one);
 
@@ -56,22 +55,28 @@ OutputVector translate_rsqrt_op(const NodeContext& node) {
         auto const_one = create_same_type_const_scalar<float>(real_part, 1.0f);
         auto const_minus_one = create_same_type_const_scalar<float>(real_part, -1.0f);
 
-        // a^2 + b^2
+        // sum_sq = a^2 + b^2
         auto sum_sq = std::make_shared<v1::Add>(std::make_shared<v1::Power>(real_part, const_two),
                                                 std::make_shared<v1::Power>(imag_part, const_two));
 
         // |z| = sqrt(sum_sq)
         auto norm = std::make_shared<v0::Sqrt>(sum_sq);
 
+        // if real_part >= 0
         auto real_gte_zero = std::make_shared<v1::GreaterEqual>(real_part, const_zero);
 
+        // then
+        // new_real = sqrt( (real_part + norm) / 2 )
         auto new_real_when_real_gte_zero = std::make_shared<v0::Sqrt>(
             std::make_shared<v1::Divide>(std::make_shared<v1::Add>(real_part, norm), const_two));
 
+        // new_imag = imag_part / (2 * new_real)
         auto new_imag_when_real_gte_zero =
             std::make_shared<v1::Divide>(imag_part,
                                          std::make_shared<v1::Multiply>(new_real_when_real_gte_zero, const_two));
 
+        // else
+        // new_imag = sqrt( (norm -  real_part) / 2 ) * sign
         auto imag_lt_zero = std::make_shared<v1::Less>(imag_part, const_zero);
 
         auto new_imag_when_real_lt_zero_no_sign = std::make_shared<v0::Sqrt>(
@@ -82,6 +87,7 @@ OutputVector translate_rsqrt_op(const NodeContext& node) {
                                          std::make_shared<v0::Negative>(new_imag_when_real_lt_zero_no_sign),
                                          new_imag_when_real_lt_zero_no_sign);
 
+        // new_real = imag / (2 * new_imag)
         auto new_real_when_real_lt_zero =
             std::make_shared<v1::Divide>(imag_part,
                                          std::make_shared<v1::Multiply>(new_imag_when_real_lt_zero, const_two));
@@ -90,25 +96,29 @@ OutputVector translate_rsqrt_op(const NodeContext& node) {
 
         auto new_imag =
             std::make_shared<v1::Select>(real_gte_zero, new_imag_when_real_gte_zero, new_imag_when_real_lt_zero);
+        // endif
 
-        auto new_real_eq_zero = std::make_shared<v1::Equal>(real_part, const_zero);
-
-        auto new_imag_eq_zero = std::make_shared<v1::Equal>(imag_part, const_zero);
-
-        auto real_imag_zero = std::make_shared<v1::LogicalAnd>(new_real_eq_zero, new_imag_eq_zero);
-
-        // rsqrt_real = sqrt_real/(sqrt_real^2 + sqrt_imag^2)
-        // rsqrt_imag = - sqrt_imag/(sqrt_real^2 + sqrt_imag^2)
+        // new_sum_sq = new_real^2 + new_imag^2
         auto new_sum_sq = make_shared<v1::Add>(make_shared<v1::Power>(new_real, const_two),
                                                make_shared<v1::Power>(new_imag, const_two));
 
+        // rsqrt_real = sqrt_real/(sqrt_real^2 + sqrt_imag^2)
         auto rsqrt_real = make_shared<v1::Divide>(new_real, new_sum_sq);
 
+        // rsqrt_imag = - sqrt_imag/(sqrt_real^2 + sqrt_imag^2)
         auto rsqrt_imag = make_shared<v0::Negative>(make_shared<v1::Divide>(new_imag, new_sum_sq));
 
+        // check if z = 0 + 0j
+        // then new_z = inf + nan*j
+        // else new_z = rsqrt_real + rsqrt_imag*j
+        auto new_real_eq_zero = std::make_shared<v1::Equal>(real_part, const_zero);
+        auto new_imag_eq_zero = std::make_shared<v1::Equal>(imag_part, const_zero);
+        auto real_imag_zero = std::make_shared<v1::LogicalAnd>(new_real_eq_zero, new_imag_eq_zero);
+
         auto const_inf = create_same_type_const_scalar<float>(real_part, std::numeric_limits<float>::infinity());
-        auto const_nan = create_same_type_const_scalar<float>(real_part, std::numeric_limits<float>::quiet_NaN());
         auto rsqrt_real_final = std::make_shared<v1::Select>(real_imag_zero, const_inf, rsqrt_real);
+
+        auto const_nan = create_same_type_const_scalar<float>(real_part, std::numeric_limits<float>::quiet_NaN());
         auto rsqrt_imag_final = std::make_shared<v1::Select>(real_imag_zero, const_nan, rsqrt_imag);
 
         auto real_unsqueeze = make_shared<v0::Unsqueeze>(rsqrt_real_final, minus_one);
--- tests/layer_tests/tensorflow_tests/test_tf_Rsqrt.py
+++ tests/layer_tests/tensorflow_tests/test_tf_Rsqrt.py
@@ -60,16 +60,18 @@ class TestRsqrt(CommonTFLayerTest):
 
 class TestComplexRsqrt(CommonTFLayerTest):
     def _prepare_input(self, inputs_info):
-        rng = np.random.default_rng()
+        rng = np.random.default_rng(123813)
+        
         assert 'param_real:0' in inputs_info
         assert 'param_imag:0' in inputs_info
+        
         param_real_shape_1 = inputs_info['param_real:0']
         param_imag_shape_1 = inputs_info['param_imag:0']
+        
         inputs_data = {}
         inputs_data['param_real:0'] =  rng.uniform(-10.0, 10.0, param_real_shape_1).astype(np.float32) 
         inputs_data['param_imag:0'] = rng.uniform(-10.0, 10.0, param_imag_shape_1).astype(np.float32) 
-        print(inputs_data['param_real:0'])
-        print(inputs_data['param_imag:0'])
+        
         return inputs_data
 
     def create_complex_rsqrt_net(self, shape, ir_version, use_legacy_frontend):
