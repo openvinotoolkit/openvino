ef87707683ad4606d391d93e6af4b22e9c5373f4
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2024-08-29 12:59:11 +0400

    [GPU] Use ngraph Transpose+MatMul fusion instead of custom code in layout optimizer (#25708)
    ### Details:
     - Allow Tranpose+Matmul+[Transpose] fusion for static shapes
    - Allow Transpose in `MoveEltwiseUpThroughDataMov` for specific
    Transpose -> Eltwise -> MatMul case
    - Change order of `MoveEltwiseUpThroughDataMov` and
    `ConvertMatMulToFullyConnected` to simplify callback
    - Removed custom code for similar fusion in layout optimizer and related
    debug knob

--- src/plugins/intel_gpu/src/plugin/transformations_pipeline.cpp
+++ src/plugins/intel_gpu/src/plugin/transformations_pipeline.cpp
@@ -832,7 +832,7 @@ void TransformationsPipeline::apply(std::shared_ptr<ov::Model> func) {
         manager.register_pass<ov::pass::RMSFusion>();
         manager.register_pass<ov::intel_gpu::KVCacheFusion>();
         manager.register_pass<ov::intel_gpu::FullyConnectedConvertFusion>();
-        manager.register_pass<ov::intel_gpu::TransposeFusion>();
+        manager.register_pass<ov::intel_gpu::TransposeFusion>(device_info.supports_immad);
 
         if (!device_info.supports_immad) {
             manager.register_pass<ov::intel_gpu::UnsqueezeBroadcastReshapeMatmulFusion>();
