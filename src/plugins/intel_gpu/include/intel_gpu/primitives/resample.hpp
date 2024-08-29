d700d50b90f8f321c77d147dfc1d65e25806e4c1
Damian Kurek <damian.kurek@intel.com>
2024-08-29 10:23:04 +0200

    Fix

--- src/plugins/intel_gpu/include/intel_gpu/primitives/resample.hpp
+++ src/plugins/intel_gpu/include/intel_gpu/primitives/resample.hpp
@@ -96,38 +96,12 @@ struct resample : public primitive_base<resample> {
              InterpolateOp::InterpolateMode operation_type = InterpolateOp::InterpolateMode::LINEAR,
              InterpolateOp::ShapeCalcMode shape_calc_mode = InterpolateOp::ShapeCalcMode::SIZES,
              InterpolateOp::CoordinateTransformMode ctm = InterpolateOp::CoordinateTransformMode::HALF_PIXEL,
-             InterpolateOp::NearestMode nm = InterpolateOp::NearestMode::ROUND_PREFER_FLOOR)
+             InterpolateOp::NearestMode nm = InterpolateOp::NearestMode::ROUND_PREFER_FLOOR,
+	     const int scales_port = 2)
         : primitive_base(id, {input, sizes_id, scales_id}),
           output_size(tensor()),
           num_filter(0),
-          sizes({}),
-          scales({}),
-          axes(axes),
-          pads_begin(pads_begin),
-          pads_end(pads_end),
-          operation_type(operation_type),
-          shape_calc_mode(shape_calc_mode),
-          antialias(antialias),
-          cube_coeff(cube_coeff),
-          coord_trans_mode(ctm),
-          round_mode(nm) {}
-
-    /// @brief resample with dynamic sizes/scales
-    resample(const primitive_id& id,
-             const input_info& input,
-             const input_info& sizes_or_scales_id,
-             const std::vector<int64_t>& axes,
-             const std::vector<size_t>& pads_begin = {},
-             const std::vector<size_t>& pads_end = {},
-             int32_t antialias = 0,
-             float cube_coeff = -0.75f,
-             InterpolateOp::InterpolateMode operation_type = InterpolateOp::InterpolateMode::LINEAR,
-             InterpolateOp::ShapeCalcMode shape_calc_mode = InterpolateOp::ShapeCalcMode::SIZES,
-             InterpolateOp::CoordinateTransformMode ctm = InterpolateOp::CoordinateTransformMode::HALF_PIXEL,
-             InterpolateOp::NearestMode nm = InterpolateOp::NearestMode::ROUND_PREFER_FLOOR)
-        : primitive_base(id, {input, sizes_or_scales_id}),
-          output_size(tensor()),
-          num_filter(0),
+          scales_port(scales_port),
           sizes({}),
           scales({}),
           axes(axes),
@@ -154,6 +128,8 @@ struct resample : public primitive_base<resample> {
     tensor output_size;
     /// @param num_filter Input filter. Only used by bilinear sample_type.
     uint32_t num_filter = 0;
+    /// @param num_filter Input filter. Only used by bilinear sample_type.
+    uint32_t scales_port;
     /// @param sizes Describing output shape for spatial axes.
     std::vector<int64_t> sizes;
     /// @param scales Scales of spatial axes, i.e. output_shape / input_shape
