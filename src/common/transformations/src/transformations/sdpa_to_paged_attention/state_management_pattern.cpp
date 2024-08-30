70c9ea2203fb8c1c83477664aa140c9d7c7fe1bc
Andrii Staikov <andrii.staikov@intel.com>
2024-08-30 17:35:22 +0200

    code style 2

--- src/common/transformations/src/transformations/sdpa_to_paged_attention/state_management_pattern.cpp
+++ src/common/transformations/src/transformations/sdpa_to_paged_attention/state_management_pattern.cpp
@@ -341,8 +341,8 @@ ov::pass::StateManagementPattern::StateManagementPattern(ParameterVector& kv_par
                 auto alibi_constant_values = alibi_constant->cast_vector<float>();
                 bool all_values_nagative =
                     std::all_of(alibi_constant_values.begin(), alibi_constant_values.end(), [&](float value) {
-                                                                                                return value < 0.0;
-                                                                                                             });
+                        return value < 0.0;
+                        });
 
                 if (all_values_nagative && pattern_map.find(mirroring_abs) != pattern_map.end()) {
                     alibi_slopes = std::make_shared<v1::Multiply>(
