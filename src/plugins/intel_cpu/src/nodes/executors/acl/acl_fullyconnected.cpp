3638231d38379b3bbb9953a7349c706cf89889db
Seyed Morteza Hosseini <mortezaho.1376@gmail.com>
2024-09-03 00:32:52 -0700

    Add perf counters

--- src/plugins/intel_cpu/src/nodes/executors/acl/acl_fullyconnected.cpp
+++ src/plugins/intel_cpu/src/nodes/executors/acl/acl_fullyconnected.cpp
@@ -5,6 +5,7 @@
 #include "acl_fullyconnected.hpp"
 
 #include <cstdint>
+#include <chrono>
 #include <memory>
 
 #include "cpu_memory.h"
@@ -19,6 +20,8 @@
 
 #include "arm_neon.h"
 #include "arm_compute/runtime/NEON/NEFunctions.h"
+#include <arm_compute/runtime/Scheduler.h>
+
 #include "arm_compute/core/Types.h"
 
 
@@ -92,24 +95,48 @@ void mm_exec(shared_mem src, shared_mem wei, shared_mem bias, shared_mem scales,
     auto kGroups = weightsNonTransposed ? scalesShape[scaleDimsBase + 0] : scalesShape[scaleDimsBase + 1];
     auto kGroupSize = K / kGroups;
 
-    std::cerr << src->getPrecision() << " " << wei->getPrecision() << " "
-              << dst->getPrecision() << std::endl;
-    std::cerr << M << " " << K << " " << N << std::endl;
-    std::cerr << wei->getDesc().getShape().toString() << std::endl;
-    std::cerr << src->getDesc().getShape().toString() << std::endl;
-    std::cerr << scales->getShape().toString() << std::endl;
-    std::cerr << kGroups << std::endl;
-    std::cerr << kGroupSize << std::endl;
-    std::cerr << subtracts->getShape().toString() << std::endl;
-    std::cerr << subtracts->getPrecision() << std::endl;
-    std::cerr << weightsNonTransposed << std::endl;
-
     const int BLOCK_SIZE = 32;
     int num_blocks_K = (K + BLOCK_SIZE - 1) / BLOCK_SIZE;
     int num_blocks_N = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
     std::memset(pdst, 0, M * N * sizeof(T1));
 
-//    parallel_for2d(M, N, [&](size_t m, size_t n) {
+    std::vector<arm_compute::Tensor> tensorsA;
+    std::vector<arm_compute::TensorInfo> tensorInfosA;
+    tensorsA.resize(num_blocks_N);
+    tensorInfosA.resize(num_blocks_N);
+    std::vector<arm_compute::Tensor> tensorsB;
+    std::vector<arm_compute::TensorInfo> tensorInfosB;
+    tensorsB.resize(num_blocks_N);
+    tensorInfosB.resize(num_blocks_N);
+    std::vector<arm_compute::Tensor> tensorsC;
+    std::vector<arm_compute::TensorInfo> tensorInfosC;
+    tensorsC.resize(num_blocks_N);
+    tensorInfosC.resize(num_blocks_N);
+    for (size_t n_block = 0; n_block < num_blocks_N; ++n_block) {
+        auto aInfo = arm_compute::TensorInfo();
+        aInfo.init(arm_compute::TensorShape(BLOCK_SIZE, M),
+                   arm_compute::Format::F32,
+                   arm_compute::Strides{sizeof(float), K * sizeof(float)},
+                   size_t(0),
+                   size_t(M * K * sizeof(float)));
+        auto bInfo = arm_compute::TensorInfo(arm_compute::TensorShape(BLOCK_SIZE, BLOCK_SIZE),
+                                             1,
+                                             arm_compute::DataType::F32);
+        auto cInfo = arm_compute::TensorInfo();
+        cInfo.init(arm_compute::TensorShape(BLOCK_SIZE, M),
+                   arm_compute::Format::F32,
+                   arm_compute::Strides{sizeof(float), N * sizeof(float)},
+                   size_t(0),
+                   size_t(N * M * sizeof(float)));
+        tensorsA[n_block].allocator()->init(aInfo);
+        tensorsB[n_block].allocator()->init(bInfo);
+        tensorsC[n_block].allocator()->init(cInfo);
+        tensorsB[n_block].allocator()->allocate();
+        tensorInfosA[n_block] = aInfo;
+        tensorInfosB[n_block] = aInfo;
+        tensorInfosC[n_block] = aInfo;
+    }
+
     parallel_for(num_blocks_N, [&](size_t n_block) {
         for (size_t k_block = 0; k_block < num_blocks_K; ++k_block) {
             size_t start_k = k_block * BLOCK_SIZE;
@@ -119,36 +146,10 @@ void mm_exec(shared_mem src, shared_mem wei, shared_mem bias, shared_mem scales,
 
             size_t matrix_block_size_k = end_k - start_k;
             size_t matrix_block_size_n = end_n - start_n;
-            arm_compute::Tensor a, b, c;
-//            std::cout << matrix_block_size_n << " " << matrix_block_size_k << std::endl;
-//            auto aInfo = arm_compute::TensorInfo(arm_compute::TensorShape(matrix_block_size_k, M), 1, arm_compute::DataType::F32);
-//            auto bInfo = arm_compute::TensorInfo(arm_compute::TensorShape(matrix_block_size_k, matrix_block_size_n), 1, arm_compute::DataType::F32);
-//            std::cout << matrix_block_size_k << " - " << matrix_block_size_n << std::endl;
-//            auto cInfo = arm_compute::TensorInfo(arm_compute::TensorShape(matrix_block_size_n, M), 1, arm_compute::DataType::F32);
-//            a.allocator()->init(aInfo);
-//            b.allocator()->init(bInfo);
-//            c.allocator()->init(cInfo);
-//            std::cout << cInfo.strides_in_bytes()[1] << std::endl;
-            auto aInfo = arm_compute::TensorInfo();
-            aInfo.init(arm_compute::TensorShape(matrix_block_size_k, M),
-                       arm_compute::Format::F32,
-                       arm_compute::Strides{sizeof(float), K * sizeof(float)},
-                       size_t(0),
-                       size_t(M * K * sizeof(float)));
-            auto bInfo = arm_compute::TensorInfo(arm_compute::TensorShape(matrix_block_size_k, matrix_block_size_n), 1, arm_compute::DataType::F32);
-            auto cInfo = arm_compute::TensorInfo();
-            cInfo.init(arm_compute::TensorShape(matrix_block_size_n, M),
-                       arm_compute::Format::F32,
-                       arm_compute::Strides{sizeof(float), N * sizeof(float)},
-                       size_t(0),
-                       size_t(N * M * sizeof(float)));
-            a.allocator()->init(aInfo);
-            b.allocator()->init(bInfo);
-            c.allocator()->init(cInfo);
-            b.allocator()->allocate();
 
-            auto _b = reinterpret_cast<float*>(b.buffer());
+            auto _b = reinterpret_cast<float*>(tensorsB[n_block].buffer());
             // TODO CHECK FOR TRANSPOSE CASE
+            auto t1 = std::chrono::high_resolution_clock::now();
             for (size_t n = start_n; n < end_n; ++n) {
                 size_t k = start_k;
                 for (; k + 8 <= end_k; k += 8) {
@@ -212,74 +213,38 @@ void mm_exec(shared_mem src, shared_mem wei, shared_mem bias, shared_mem scales,
                         zerodWei = static_cast<float>(fwei);
                     }
                     _b[bufIdx] = (zerodWei * static_cast<float>(fscale));
-                    // std::cout << k << " " << bufIdx << " " << weiIdx << " " << unsigned(fwei) << " " << fscale << " " << _b[bufIdx] << std::endl;
                 }
             }
-            // std::cout << src->getPrecision() << std::endl;
+            auto t2 = std::chrono::high_resolution_clock::now();
+            auto ms_int = duration_cast<std::chrono::nanoseconds>(t2 - t1);
+            std::cout << "dequantize " << ms_int.count() << " ns" << std::endl;
+
+            t1 = std::chrono::high_resolution_clock::now();
             auto _a = reinterpret_cast<void*>(psrc + start_k);
             auto _c = reinterpret_cast<void*>(pdst + start_n);
-            a.allocator()->import_memory(_a);
-            c.allocator()->import_memory(_c);
+            tensorsA[n_block].allocator()->import_memory(_a);
+            tensorsC[n_block].allocator()->import_memory(_c);
 
             arm_compute::NEGEMM gemm;
             arm_compute::GEMMInfo gemmInfo;
             gemmInfo.set_pretranspose_B(true);
             gemmInfo.set_accumulate(true);
 
-//            arm_compute::Status val = gemm.validate(&aInfo, &bInfo, &cInfo, &cInfo, 1.0f, 1.0f, gemmInfo);
-            gemm.configure(&a, &b, nullptr, &c, 1.0f, 0.0f, gemmInfo);
+            gemm.configure(&tensorsA[n_block], &tensorsB[n_block], nullptr, &tensorsC[n_block], 1.0f, 0.0f, gemmInfo);
             gemm.run();
+            t2 = std::chrono::high_resolution_clock::now();
+            ms_int = duration_cast<std::chrono::nanoseconds>(t2 - t1);
+            std::cout << "gemm " << ms_int.count() << " ns" << std::endl;
         }
     });
-    if (bias != nullptr) {
-        auto pbias = bias->getDataAs<const T1>();
-        for (size_t m = 0; m < M; ++m) {
-            for (size_t n = 0; n < N; ++n) {
-                pdst[m * N + n] += pbias[n];
-            }
-        }
-    }
-
-//    parallel_for2d(M, N, [&](size_t m, size_t n) {
-//        size_t dstIdx = m * N + n;
-//        T1 accum = 0.f;
-//
-//        for (size_t kb = 0; kb < kGroups; kb++) {
-//            T1 groupAccum = 0.f;
-//            for (size_t ki = 0; ki < kGroupSize; ki++) {
-//                auto k = kb * kGroupSize + ki;
-//                size_t srcIdx = m * K + k;
-//                size_t weiIdx = weightsNonTransposed ? k * N + n : n * K + k;
-//                accum += psrc[srcIdx] * _b[weiIdx];
+//    if (bias != nullptr) {
+//        auto pbias = bias->getDataAs<const T1>();
+//        for (size_t m = 0; m < M; ++m) {
+//            for (size_t n = 0; n < N; ++n) {
+//                pdst[m * N + n] += pbias[n];
 //            }
-//            accum += groupAccum;
 //        }
-//        if (bias != nullptr) {
-//            accum += bias->getDataAs<const T1>()[n];
-//        }
-//        pdst[dstIdx] = accum;
-//    });
-
-
-//    parallel_for2d(M, N, [&](size_t m, size_t n) {
-//            size_t dstIdx = m * N + n;
-//            T1 accum = 0.f;
-//
-//            for (size_t kb = 0; kb < kGroups; kb++) {
-//                T1 groupAccum = 0.f;
-//                for (size_t ki = 0; ki < kGroupSize; ki++) {
-//                    auto k = kb * kGroupSize + ki;
-//                    size_t srcIdx = m * K + k;
-//                    size_t weiIdx = weightsNonTransposed ? k * N + n : n * K + k;
-//                    accum += psrc[srcIdx] * _b[weiIdx];
-//                }
-//                accum += groupAccum;
-//            }
-//            if (bias != nullptr) {
-//                accum += bias->getDataAs<const T1>()[n];
-//            }
-//            pdst[dstIdx] = accum;
-//    });
+//    }
 
 //    parallel_for2d(M, N, [&](size_t m, size_t n) {
 //            size_t dstIdx = m * N + n;
