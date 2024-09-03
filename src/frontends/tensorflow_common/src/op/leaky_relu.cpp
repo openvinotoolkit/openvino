80cd3e80492941df3beab5ef6e78fcc5c820c98e
Roman Kazantsev <roman.kazantsev@intel.com>
2024-09-03 10:00:38 +0400

    [TF FE] Fix LeakyRelu translator and stabilize tests for it (#26371)
    **Details:** Handle correctly scalar case for LeakyRelu.
    
    **Ticket:** TBD
    
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/frontends/tensorflow_common/src/op/leaky_relu.cpp
+++ src/frontends/tensorflow_common/src/op/leaky_relu.cpp
@@ -5,6 +5,7 @@
 #include "common_op_table.hpp"
 #include "openvino/op/constant.hpp"
 #include "openvino/op/prelu.hpp"
+#include "utils.hpp"
 
 using namespace std;
 using namespace ov::op;
@@ -17,7 +18,7 @@ ov::OutputVector translate_leaky_relu_op(const NodeContext& node) {
     default_op_checks(node, 1, {"LeakyRelu", "LEAKY_RELU"});
     auto features = node.get_input(0);
     auto alpha = node.get_attribute<float>("alpha", 0.2f);
-    auto alpha_const = make_shared<v0::Constant>(element::f32, Shape{1}, alpha);
+    auto alpha_const = create_same_type_const_scalar<float>(features, alpha);
     auto leaky_relu = make_shared<v0::PRelu>(features, alpha_const);
     set_node_name(node.get_name(), leaky_relu);
     return {leaky_relu};
