bf2e3f600117014f965946ea18287afe1e3302e4
muhd360 <masheikh_b21@et.vjti.ac.in>
2024-09-09 15:17:51 +0530

    index_fill method

---
+++ src/frontends/pytorch/src/op/index_fill_.cpp
@@ -0,0 +1,63 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <memory>
+
+#include "openvino/core/node.hpp"
+#include "openvino/core/node_output.hpp"
+#include "openvino/core/type/element_type.hpp"
+#include "openvino/frontend/pytorch/node_context.hpp"
+#include "openvino/op/broadcast.hpp"
+#include "openvino/op/constant.hpp"
+#include "openvino/op/convert_like.hpp"
+#include "openvino/op/reshape.hpp"
+#include "openvino/op/scatter_elements_update.hpp"
+#include "openvino/op/shape_of.hpp"
+#include "utils.hpp"
+
+using namespace ov::op;
+
+namespace ov {
+namespace frontend {
+namespace pytorch {
+namespace op {
+
+OutputVector translate_index_copy_(const NodeContext& context) {
+    // aten::index_fill_(self, dim, index, value) → Tensor
+    num_inputs_check(context, 4, 4);
+    auto input = context.get_input(0);
+    auto dim = context.get_input(1);
+    auto index = context.get_input(2);
+    auto value = context.get_input(3);
+
+    auto const_1_vec = v0::Constant::create(element::i32, Shape{1}, {value});
+
+    Output<Node> tensor_rank = std::get<1>(get_shape_rank(context, input, true));
+    auto tensor_rank_correct_type = context.mark_node(std::make_shared<v1::ConvertLike>(tensor_rank, dim));
+    auto positive_dim = normalize_axis(context, dim, tensor_rank_correct_type);
+
+    // begin the computation
+    //indx_cpy(dim,idx_def,tensor_def)
+    //indx_fill(dim,idx_def,val)
+    auto tensor_shape = context.mark_node(std::make_shared<v3::ShapeOf>(input, element::i32));
+    auto dim_vec = context.mark_node(std::make_shared<v1::Reshape>(positive_dim, const_1_vec, false));
+    auto broadcasted_index = context.mark_node(std::make_shared<v1::Broadcast>(index, tensor_shape, dim_vec));
+    //reshaping steps
+ 
+    //so we need to create a tensor with the same shape as the input tensor
+    //(val tensor is broadcasted to the shape of the input tensor) then index_copy
+    //val tensor->tensor of shape of input tensor with all values v
+    
+
+    
+    
+    auto result =
+        context.mark_node(std::make_shared<v12::ScatterElementsUpdate>(input, broadcasted_index, index, dim));
+    return {result};
+};
+
+}  // namespace op
+}  // namespace pytorch
+}  // namespace frontend
+}  // namespace ov
