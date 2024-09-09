8056667576de6b2ecc4fe37eef290ce36b652604
Yuhui Shi <dayeye2006@gmail.com>
2024-09-08 19:33:04 -0700

    remove unused code

--- src/frontends/tensorflow_common/src/op/tensor_scater_add.cpp
+++ src/frontends/tensorflow_common/src/op/tensor_scater_add.cpp
@@ -13,9 +13,6 @@ OutputVector translate_tensor_scatter_add_op(const NodeContext& node) {
     auto data = node.get_input(0);
     auto indices = node.get_input(1);
     auto updates = node.get_input(2);
-    // // optional name attribute
-    // auto name = node.get_attribute<std::string>("name", "");
-    // auto node_name = name.empty() ? node.get_name() : name;
     auto reduction = ov::op::v15::ScatterNDUpdate::Reduction::SUM;
     auto scatter_add_op = make_shared<ov::op::v15::ScatterNDUpdate>(data, indices, updates, reduction);
     set_node_name(node.get_name(), scatter_add_op);
