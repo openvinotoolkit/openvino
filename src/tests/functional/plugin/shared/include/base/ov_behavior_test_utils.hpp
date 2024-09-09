26aae196a49c1954bc6e121c5c978d9310a5de9c
Chen Peter <peter.chen@intel.com>
2024-09-09 16:08:41 +0800

    MARK_MANDATORY_API_FOR_HW_DEVICE support NULL

--- src/tests/functional/plugin/shared/include/base/ov_behavior_test_utils.hpp
+++ src/tests/functional/plugin/shared/include/base/ov_behavior_test_utils.hpp
@@ -36,11 +36,14 @@
         return (sw_plugin_in_target_device(ov::test::utils::target_device) ? "" : "mandatory_") + name; \
     }
 
-#define MARK_MANDATORY_API_FOR_HW_DEVICE()                                                              \
-    [](const testing::TestParamInfo<std::string>& info) {                                               \
-        return sw_plugin_in_target_device(ov::test::utils::target_device) ? "optional_" : "mandatory_"; \
+static void (*NULL_getTestCaseName)(const testing::TestParamInfo<std::string>& info) = NULL;
+#define MARK_MANDATORY_API_FOR_HW_DEVICE(GET_TEST_NAME)                                          \
+    [](const testing::TestParamInfo<std::string>& info) {                                                                    \
+        std::string name = "";                                                                                                                 \
+        if (NULL ! = GET_TEST_NAME)                                                                                                   \
+            name += GET_TEST_NAME(info);                                                                                           \
+        return (sw_plugin_in_target_device(ov::test::utils::target_device) ? "" : "mandatory_") + name; \
     }
-
 namespace ov {
 namespace test {
 namespace behavior {
