4d4dcaf10241d1ce70d3dea976880c467484a419
Pavel Durandin <pavel.durandin@intel.com>
2024-09-09 14:09:15 +0400

    [GPU] fix zero array in network unit test (#26470)
    ### Details:
     - Check for zero intermediate buffers
    
    ### Tickets:
     - N/A

--- src/plugins/intel_gpu/tests/unit/module_tests/network_test.cpp
+++ src/plugins/intel_gpu/tests/unit/module_tests/network_test.cpp
@@ -247,7 +247,10 @@ TEST(network_test, scratchpad_test) {
     auto fc1 = net1.get_primitive("fc_prim");
     auto fc2 = net2.get_primitive("fc_prim");
 
-    ASSERT_TRUE(fc1->get_intermediates_memories()[0]->buffer_ptr() != fc2->get_intermediates_memories()[0]->buffer_ptr());
+    if (fc1->get_intermediates_memories().size() > 0 && fc2->get_intermediates_memories().size() > 0) {
+        ASSERT_TRUE(fc1->get_intermediates_memories()[0]->buffer_ptr() !=
+                    fc2->get_intermediates_memories()[0]->buffer_ptr());
+    }
 }
 
 #endif
