039fd27f94c0d4f3d529999be0cd31546b0a8e92
Alexandra Sidorova <alexandra.sidorova@intel.com>
2024-09-10 16:41:45 +0400

    [Snippets] Implemented BaseImpl in op::Buffer

--- src/common/snippets/include/snippets/op/buffer.hpp
+++ src/common/snippets/include/snippets/op/buffer.hpp
@@ -14,15 +14,15 @@ namespace op {
 
 /**
  * @interface Buffer
- * @brief This is a base class for memory storage.
+ * @brief This is a class for memory storage.
+ *        The buffers can have source (called as "IntermediateMemory") and can be without source (called as "NewMemory").
+ *        First one contains memory which was stored by source -> these buffers propagate output shape and element type from parents.
+ *        Second one has passed `element_type` and `shape` by user - these attributes describe Buffer with independent empty memory.
+ *        The both behaviors are implemented via the corresponding classes which are derived from the class "Buffer::BaseImpl".
+ *        It allows user to work with only the class "op::Buffer" - all needed logic is implemented in the field `m_impl`.
  * @ingroup snippets
  */
 class Buffer : public ov::op::Op {
-    enum class Type {
-        NewMemory,
-        IntermediateMemory
-    };
-
 public:
     OPENVINO_OP("Buffer", "SnippetsOpset");
     Buffer() = default;
@@ -36,20 +36,74 @@ public:
 
     std::shared_ptr<Node> clone_with_new_inputs(const OutputVector& new_args) const override;
 
-    size_t get_allocation_size() const;
+    size_t get_allocation_size() const { return m_impl->get_allocation_size(); }
 
     class ShapeInfer : public IShapeInferSnippets {
-        ov::Shape m_shape;
-        Type m_type;
+        std::shared_ptr<IShapeInferSnippets> m_impl_shape_infer {nullptr};
     public:
         explicit ShapeInfer(const std::shared_ptr<ov::Node>& n);
         Result infer(const std::vector<VectorDimsRef>& input_shapes) override;
     };
 
-protected:
-    const Type m_type = Type::NewMemory;
-    const ov::Shape m_output_shape {};
-    const ov::element::Type m_element_type = ov::element::u8;  // u8 - default 1 byte
+private:
+    // Base class for implementations of Buffer
+    class BaseImpl;
+    // This constructor is used only in clone_with_new_inputs
+    Buffer(const OutputVector& arguments, std::shared_ptr<BaseImpl> impl);
+
+    const std::shared_ptr<BaseImpl> m_impl {nullptr};
+
+    class BaseImpl {
+    public:
+        BaseImpl() = default;
+        virtual size_t get_allocation_size() const = 0;
+        virtual std::shared_ptr<BaseImpl> clone() const = 0;
+        virtual void validate_and_infer_types(Buffer* buffer) const = 0;
+        virtual bool visit_attributes(AttributeVisitor& visitor) = 0;
+        virtual std::shared_ptr<IShapeInferSnippets> get_shape_infer() const = 0;
+    };
+
+    // IntermediateMemoryImpl represents intermediate memory.
+    // The buffers with this implementation must have source (parents)
+    class IntermediateMemoryImpl : public BaseImpl {
+        class ShapeInfer;
+    public:
+        IntermediateMemoryImpl() = default;
+
+        size_t get_allocation_size() const override { return utils::get_dynamic_value<size_t>(); }
+        std::shared_ptr<BaseImpl> clone() const override;
+        void validate_and_infer_types(Buffer* buffer) const override;
+        bool visit_attributes(AttributeVisitor& visitor) override { return true; }
+        std::shared_ptr<IShapeInferSnippets> get_shape_infer() const override { return std::make_shared<ShapeInfer>(); }
+    private:
+        class ShapeInfer : public IShapeInferSnippets {
+        public:
+            Result infer(const std::vector<VectorDimsRef>& input_shapes) override;
+        };
+    };
+
+    // NewMemoryImpl represents a new empty memory for allocation with specified shape and element type.
+    // The buffers with this implementation mustn't have source (parents)
+    class NewMemoryImpl : public BaseImpl {
+        class ShapeInfer;
+        ov::Shape m_shape;
+        ov::element::Type m_element_type = ov::element::u8;  // u8 - default 1 byte
+    public:
+        NewMemoryImpl(const ov::Shape& shape, ov::element::Type element_type);
+
+        size_t get_allocation_size() const override;
+        std::shared_ptr<BaseImpl> clone() const override;
+        void validate_and_infer_types(Buffer* buffer) const override;
+        bool visit_attributes(AttributeVisitor& visitor) override;
+        std::shared_ptr<IShapeInferSnippets> get_shape_infer() const override { return std::make_shared<ShapeInfer>(m_shape); }
+    private:
+        class ShapeInfer : public IShapeInferSnippets {
+            ov::Shape m_shape;
+        public:
+            explicit ShapeInfer(ov::Shape shape);
+            Result infer(const std::vector<VectorDimsRef>& input_shapes) override;
+        };
+    };
 };
 
 
--- src/common/snippets/src/op/buffer.cpp
+++ src/common/snippets/src/op/buffer.cpp
@@ -15,81 +15,91 @@ namespace op {
 
 Buffer::Buffer(const ov::Output<ov::Node>& arg) : Buffer(ov::OutputVector{arg}) {}
 
-Buffer::Buffer(const OutputVector& arguments) : Op(arguments), m_type(Type::IntermediateMemory) {
+Buffer::Buffer(const OutputVector& arguments) : Op(arguments), m_impl(std::make_shared<IntermediateMemoryImpl>()) {
     constructor_validate_and_infer_types();
 }
-
-Buffer::Buffer(const ov::Shape& shape, ov::element::Type element_type) : Op(), m_type(Type::NewMemory), m_output_shape(shape), m_element_type(element_type) {
+Buffer::Buffer(const ov::Shape& shape, ov::element::Type element_type) : Op(), m_impl(std::make_shared<NewMemoryImpl>(shape, element_type)) {
+    constructor_validate_and_infer_types();
+}
+Buffer::Buffer(const OutputVector& arguments, std::shared_ptr<BaseImpl> impl) : Op(arguments), m_impl(std::move(impl)) {
     constructor_validate_and_infer_types();
 }
 
 bool Buffer::visit_attributes(AttributeVisitor& visitor) {
     INTERNAL_OP_SCOPE(Buffer_visit_attributes);
-    auto shape = utils::pshape_to_vdims(get_output_partial_shape(0));
-    auto etype = get_output_element_type(0);
-    visitor.on_attribute("shape", shape);
-    visitor.on_attribute("element_type", etype);
+    m_impl->visit_attributes(visitor);
     return true;
 }
 
 void Buffer::validate_and_infer_types() {
     INTERNAL_OP_SCOPE(Buffer_validate_and_infer_types);
-    if (m_type == Type::NewMemory) {
-        OPENVINO_ASSERT(get_input_size() == 0, "NewMemory Buffer mustn't have inputs");
-        set_output_type(0, m_element_type, m_output_shape);
-    } else if (m_type == Type::IntermediateMemory) {
-        OPENVINO_ASSERT(get_input_size() != 0, "IntermediateMemory Buffer must have inputs");
-        const auto inputs = input_values();
-        const auto inshape = get_input_partial_shape(0);
-        const auto intype = get_input_element_type(0);
-        OPENVINO_ASSERT(std::all_of(inputs.cbegin() + 1, inputs.cend(),
-                                    [&](const ov::Output<ov::Node>& in) { return in.get_partial_shape() == inshape && in.get_element_type() == intype; }),
-                        "All inputs of Buffers must have the same shape and element type");
-        set_output_type(0, intype, inshape);
-    } else {
-        OPENVINO_THROW("Unknown Buffer type");
-    }
+    m_impl->validate_and_infer_types(this);
 }
 
 std::shared_ptr<Node> Buffer::clone_with_new_inputs(const OutputVector& new_args) const {
     INTERNAL_OP_SCOPE(Buffer_clone_with_new_inputs);
-    if (m_type == Type::NewMemory) {
-        OPENVINO_ASSERT(new_args.empty(), "NewMemory Buffer mustn't have inputs");
-        return std::make_shared<Buffer>(m_output_shape, m_element_type);
-    } else if (m_type == Type::IntermediateMemory) {
-        return std::make_shared<Buffer>(new_args);
-    } else {
-        OPENVINO_THROW("Unknown Buffer type");
-    }
-}
-
-size_t Buffer::get_allocation_size() const {
-    if (m_type == Type::NewMemory) {
-        const auto pshape = get_output_partial_shape(0);
-        OPENVINO_ASSERT(pshape.is_static(), "If Buffer doesn't have source - output shape must be static");
-        return ov::shape_size(pshape.get_shape());
-    }
-    return utils::get_dynamic_value<size_t>();
+    return std::shared_ptr<Buffer>(new Buffer(new_args, m_impl->clone()));
 }
 
 Buffer::ShapeInfer::ShapeInfer(const std::shared_ptr<ov::Node>& n) {
     const auto& buffer = ov::as_type_ptr<Buffer>(n);
     OPENVINO_ASSERT(buffer, "Got invalid node in Buffer::ShapeInfer");
-    m_type = buffer->m_type;
-    OPENVINO_ASSERT(utils::one_of(m_type, Type::IntermediateMemory, Type::NewMemory), "Got invalid Buffer type");
-    if (m_type == Type::NewMemory)
-        m_shape = buffer->m_output_shape;
+    m_impl_shape_infer = buffer->m_impl->get_shape_infer();
 }
 
 IShapeInferSnippets::Result Buffer::ShapeInfer::infer(const std::vector<VectorDimsRef>& input_shapes) {
-    if (m_type == Type::NewMemory) {
-        OPENVINO_ASSERT(input_shapes.empty(), "NewMemoryBuffer shape inference mustn't have input shapes");
-        return {{m_shape}, ShapeInferStatus::success};
-    } else if (m_type == Type::IntermediateMemory) {
-        OPENVINO_ASSERT(!input_shapes.empty(), "IntermediateMemoryBuffer shape inference must have input shapes");
-        return {{input_shapes[0].get()}, ShapeInferStatus::success};
-    }
-    OPENVINO_THROW("Uknown Buffer type!");
+    return m_impl_shape_infer->infer(input_shapes);
+}
+
+std::shared_ptr<Buffer::BaseImpl> Buffer::IntermediateMemoryImpl::clone() const {
+    return std::make_shared<IntermediateMemoryImpl>();
+}
+
+void Buffer::IntermediateMemoryImpl::validate_and_infer_types(Buffer* buffer) const {
+    OPENVINO_ASSERT(buffer, "Buffer is missed");
+    OPENVINO_ASSERT(buffer->get_input_size() != 0, "IntermediateMemory Buffer must have inputs");
+    const auto inputs = buffer->input_values();
+    const auto inshape = buffer->get_input_partial_shape(0);
+    const auto intype = buffer->get_input_element_type(0);
+    OPENVINO_ASSERT(std::all_of(inputs.cbegin() + 1, inputs.cend(),
+                                [&](const ov::Output<ov::Node>& in) { return in.get_partial_shape() == inshape && in.get_element_type() == intype; }),
+                    "All inputs of Buffers must have the same shape and element type");
+    buffer->set_output_type(0, intype, inshape);
+}
+
+Buffer::IntermediateMemoryImpl::ShapeInfer::Result Buffer::IntermediateMemoryImpl::ShapeInfer::infer(const std::vector<VectorDimsRef>& input_shapes) {
+    OPENVINO_ASSERT(!input_shapes.empty(), "IntermediateMemoryBuffer shape inference must have input shapes");
+    return {{input_shapes[0].get()}, ShapeInferStatus::success};
+}
+
+Buffer::NewMemoryImpl::NewMemoryImpl(const ov::Shape& shape, ov::element::Type element_type)
+    : m_shape(shape), m_element_type(element_type) {}
+
+size_t Buffer::NewMemoryImpl::get_allocation_size() const {
+    return ov::shape_size(m_shape);
+}
+
+std::shared_ptr<Buffer::BaseImpl> Buffer::NewMemoryImpl::clone() const {
+    return std::make_shared<NewMemoryImpl>(m_shape, m_element_type);
+}
+
+void Buffer::NewMemoryImpl::validate_and_infer_types(Buffer* buffer) const {
+    OPENVINO_ASSERT(buffer, "Buffer is missed");
+    OPENVINO_ASSERT(buffer->get_input_size() == 0, "NewMemory Buffer mustn't have inputs");
+    buffer->set_output_type(0, m_element_type, m_shape);
+}
+
+bool Buffer::NewMemoryImpl::visit_attributes(AttributeVisitor& visitor) {
+    visitor.on_attribute("shape", m_shape);
+    visitor.on_attribute("element_type", m_element_type);
+    return true;
+}
+
+Buffer::NewMemoryImpl::ShapeInfer::ShapeInfer(ov::Shape shape) : m_shape(std::move(shape)) {}
+
+Buffer::NewMemoryImpl::ShapeInfer::Result Buffer::NewMemoryImpl::ShapeInfer::infer(const std::vector<VectorDimsRef>& input_shapes) {
+    OPENVINO_ASSERT(input_shapes.empty(), "NewMemoryBuffer shape inference must have input shapes");
+    return {{m_shape}, ShapeInferStatus::success};
 }
 
 } // namespace op
