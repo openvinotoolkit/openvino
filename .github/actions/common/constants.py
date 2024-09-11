bb45cb59f1615ef7701856d0c88a84e47ece5f99
Alina Kladieva <alina.kladieva@intel.com>
2024-09-11 19:05:14 +0200

    [GHA] Artifact folder names consistency check (#26538)
    ### Details:
    To avoid sudden breaking of CI's in repos retrieving these artifacts via
    OV provider or Jenkins
    
    ### Tickets:
     - 152261

--- .github/actions/common/constants.py
+++ .github/actions/common/constants.py
@@ -5,4 +5,16 @@ class EventType(Enum):
     pre_commit = 'pre_commit'
     commit = 'commit'
 
-# TODO: add enum for product type to validate it
+
+productTypes = (
+    'public_linux_debian_10_arm_release',
+    'public_linux_fedora_29_x86_64_release',
+    'public_linux_ubuntu_20_04_x86_64_release',
+    'public_linux_ubuntu_20_04_arm64_release',
+    'public_linux_ubuntu_22_04_x86_64_release',
+    'public_linux_ubuntu_22_04_dpcpp_x86_64_release',
+    'public_linux_ubuntu_24_04_x86_64_release',
+    'public_windows_vs2019_Release',
+    'public_windows_vs2019_Debug',
+)
+ProductType = Enum('ProductType', {t.upper(): t for t in productTypes})
