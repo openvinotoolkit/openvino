40052a2e2aa11f8eb0faa9f8a6be3b28abbd4978
almilosz <alicja.miloszewska@intel.com>
2024-09-11 12:58:58 +0200

    Update .eslintrc-global.js

--- src/bindings/js/.eslintrc-global.js
+++ src/bindings/js/.eslintrc-global.js
@@ -23,5 +23,8 @@ module.exports = {
     'key-spacing': ['error', { beforeColon: false }],
     'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 0, maxEOF: 0 }],
     'keyword-spacing': ['error', { overrides: { catch: { after: false } } }],
+    '@typescript-eslint/no-var-requires': 0,
+    'no-undef': 0,
+    'max-len': 1,
   }
 };
