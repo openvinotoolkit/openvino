bff65654cc122493d6ebabf09442410bec316b2b
Alina Kladieva <alina.kladieva@intel.com>
2024-09-11 16:29:00 +0200

    Add missing product type

--- .github/actions/common/constants.py
+++ .github/actions/common/constants.py
@@ -12,6 +12,7 @@ productTypes = (
     'public_linux_ubuntu_20_04_x86_64_release',
     'public_linux_ubuntu_20_04_arm64_release',
     'public_linux_ubuntu_22_04_x86_64_release',
+    'public_linux_ubuntu_22_04_dpcpp_x86_64_release',
     'public_linux_ubuntu_24_04_x86_64_release',
     'public_windows_vs2019_Release',
     'public_windows_vs2019_Debug',
