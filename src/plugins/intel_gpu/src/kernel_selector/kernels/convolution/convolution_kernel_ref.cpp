c09e7ef2bd15513c490222bd7ddded5047d5c9e1
Nam, David <david.nam@intel.com>
2024-09-12 20:03:43 +0900

    Support if input and output type are fp

--- src/plugins/intel_gpu/src/kernel_selector/kernels/convolution/convolution_kernel_ref.cpp
+++ src/plugins/intel_gpu/src/kernel_selector/kernels/convolution/convolution_kernel_ref.cpp
@@ -123,13 +123,8 @@ bool ConvolutionKernel_Ref::Validate(const Params& params) const {
 
     // int8/uint8 inputs (quantization case) require additional checks
     // require some additional checks.
-    if (input_type == output_type && input_type != Datatype::UINT8 && input_type != Datatype::INT8)
-        return true;
-
-    // For fp model, some convolutions may not be compressed to fp16 depending on the transformation policy,
-    // and those convolutions may have the fused nodes which is of fp16.
-    // Convolution needs to support for this case.
-    if (input_type == Datatype::F32 && output_type == Datatype::F16)
+    if (input_type != Datatype::UINT8 && input_type != Datatype::INT8 &&
+        output_type != Datatype::UINT8 && output_type != Datatype::INT8)
         return true;
 
     // (u)int8 input + fp weights
