ec2b2e72037ecedcf670ef871cd676435370496c
panhaiqi <haiqi.pan@intel.com>
2024-09-13 10:17:30 +0800

    skip GetMetricAndPrintNoThrow_AVAILABLE_DEVICES for hetero

--- src/plugins/hetero/tests/functional/skip_tests_config.cpp
+++ src/plugins/hetero/tests/functional/skip_tests_config.cpp
@@ -14,6 +14,6 @@ std::vector<std::string> disabledTestPatterns() {
         R"(.*smoke_(Multi|Auto|Hetero)_BehaviorTests.*OVPropertiesTests.*SetCorrectProperties.*)",
         R"(.*smoke_(Multi|Auto|Hetero)_BehaviorTests.*OVPropertiesTests.*canSetPropertyAndCheckGetProperty.*)",
         R"(.*OVInferRequestCheckTensorPrecision.*get(Input|Output|Inputs|Outputs)From.*FunctionWith(Single|Several).*type=(u4|u1|i4|boolean).*)",
-    };
+        R"(.*OVGetMetricPropsTest.*OVGetMetricPropsTest.*GetMetricAndPrintNoThrow_AVAILABLE_DEVICES.*)"};
     return retVector;
 }
