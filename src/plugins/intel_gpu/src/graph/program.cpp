cc489a88dcaf325594263a7b772fe0522d74cc85
Tomasz Krupa <tomasz.krupa@intel.com>
2024-09-13 09:06:29 +0000

    Move saving and loading of weights_path from program.cpp to graph.cpp

--- src/plugins/intel_gpu/src/graph/program.cpp
+++ src/plugins/intel_gpu/src/graph/program.cpp
@@ -1723,9 +1723,7 @@ void program::cancel_compilation_context() {
 }
 
 void program::save(cldnn::BinaryOutputBuffer& ob) const {
-    std::string weights_path;
-    weights_path = _config.get_property(ov::intel_gpu::weights_path);
-    ob << weights_path;
+    std::string weights_path = _config.get_property(ov::intel_gpu::weights_path);
 
     std::map<cldnn::memory::ptr, std::vector<const cldnn::program_node*>> mutable_datas_ptrs;
     ob << nodes_map.size();
@@ -1873,11 +1871,6 @@ void program::save(cldnn::BinaryOutputBuffer& ob) const {
 
 void program::load(cldnn::BinaryInputBuffer& ib) {
     init_program();
-
-    std::string weights_path;
-    ib >> weights_path;
-    ov::AnyMap weights_path_property{{"GPU_WEIGHTS_PATH", weights_path}};
-    _config.set_property(weights_path_property);
 
     size_t num_nodes;
     ib >> num_nodes;
