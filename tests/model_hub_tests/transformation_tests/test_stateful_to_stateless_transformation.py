b7e0d1392754f95585aaf70310945ebb9de1fbe6
Raasz, Pawel <pawel.raasz@intel.com>
2024-09-16 16:24:46 +0200

    Update compare result tensor for stateful to stateless transformation test

--- tests/model_hub_tests/transformation_tests/test_stateful_to_stateless_transformation.py
+++ tests/model_hub_tests/transformation_tests/test_stateful_to_stateless_transformation.py
@@ -16,20 +16,38 @@ def get_read_value_ops(model: ov.Model):
 def check_desc_tensors(expected_tensors, tensors):
     # checks if tensors descriptors are same as expected
     pattern_input = re.compile(R"input_restored.((past_key_values|present)\.(\d+)\.(key|value))")
-    pattern_output = re.compile(R"output_restored.((past_key_values|present)\.(\d+)\.(key|value))")
 
     assert len(expected_tensors) == len(tensors)
     for expected in expected_tensors:
         # The `patch_stateful` in optimum use any name instead found key/value names OV will use names restore path
         # Restore expected names to find tensor for compare (can be removed when HG optimum updated)
         expected_names = {m[1] if m else name for m, name in ((pattern_input.match(name), name) for name in expected.names)}
-        expected_names = {name for name in expected_names if not pattern_output.match(name)}
         # tensor names check is relaxed the expected is sub-set of final names
         t_candidates = [t for t in tensors if expected_names.issubset(t.names)]
         assert len(t_candidates) == 1
         tensor = t_candidates[0]
         assert expected.get_element_type() == tensor.get_element_type()
         assert expected.get_partial_shape() == tensor.get_partial_shape()
+
+
+def check_result_desc_tensors(expected_tensors, tensors):
+    # checks if Result tensors descriptors are same as expected
+    pattern_restore_output = re.compile(R"output_restored.((past_key_values|present)\.(\d+)\.(key|value))")
+    pattern_output = re.compile(R"(present\.(\d+)\.(key|value))")
+
+    assert len(expected_tensors) == len(tensors)
+    for expected in expected_tensors:
+        # The `patch_stateful` in optimum use any name instead found key/value names OV will use names restore path
+        # Restore expected names to find tensor for compare (can be removed when HG optimum updated)
+        expected_names = {name for name in expected.names if not pattern_restore_output.match(name)}
+        expected_o_names = {name for name in expected.names if pattern_output.match(name)}
+        expected_names = expected_o_names if expected_o_names else expected_names
+        t_candidates = [t for t in tensors if expected_names.issubset(t.names)]
+        assert len(t_candidates) == 1
+        tensor = t_candidates[0]
+        assert expected.get_element_type() == tensor.get_element_type()
+        assert expected.get_partial_shape() == tensor.get_partial_shape()
+
 
 @retry(3, exceptions=(OSError,), delay=1)
 def run_stateful_to_stateless_in_runtime(tmp_path, model_id, model_link):
@@ -49,7 +67,7 @@ def run_stateful_to_stateless_in_runtime(tmp_path, model_id, model_link):
     print(model.model)
     print(stateless_model.model)
     check_desc_tensors(model.model.inputs, stateless_model.model.inputs)
-    check_desc_tensors(model.model.outputs, stateless_model.model.outputs)
+    check_result_desc_tensors(model.model.outputs, stateless_model.model.outputs)
 
     core = ov.Core()
     core.compile_model(model.model, 'CPU')
