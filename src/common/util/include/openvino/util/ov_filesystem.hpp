5ebddf6d2ed7dbad1e23cd45114aba17ea3c1f84
Michal Barnas <michal.barnas@intel.com>
2024-09-16 12:57:25 +0000

    define include variables

--- src/common/util/include/openvino/util/ov_filesystem.hpp
+++ src/common/util/include/openvino/util/ov_filesystem.hpp
@@ -8,10 +8,14 @@
 #    if (__cplusplus <= 201703L) || (defined(_MSVC_LANG) && (_MSVC_LANG <= 201703L) && (_MSC_VER <= 1913))
 #        if __has_include(<experimental/filesystem>)
 #        include <experimental/filesystem>
+#        define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
+#        define _LIBCPP_NO_EXPERIMENTAL_DEPRECATION_WARNING_FILESYSTEM
 namespace std_fs = std::experimental::filesystem;
 #        endif
 #    elif __has_include(<filesystem>)
 #            include <filesystem>
 namespace std_fs = std::filesystem;
+#    else
+#       error "No std filesystem lib avaliable!"
 #    endif
 #endif
