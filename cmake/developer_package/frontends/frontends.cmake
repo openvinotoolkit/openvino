44802e840aeb67bc390c481837302054c1f703cb
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2024-09-16 08:59:16 +0400

    [Build] Update compilation options to support DPCPP + Windows config (#26320)
    ### Details:
    - Update cmake configs to use OV_COMPILER_IS_INTEL_LLVM on windows
    instead of older intel compiler identifier and options

--- cmake/developer_package/frontends/frontends.cmake
+++ cmake/developer_package/frontends/frontends.cmake
@@ -212,7 +212,7 @@ macro(ov_add_frontend)
     endif()
 
     # remove -Wmissing-declarations warning, because of frontends implementation specific
-    if(CMAKE_COMPILER_IS_GNUCXX OR OV_COMPILER_IS_CLANG OR OV_COMPILER_IS_INTEL_LLVM)
+    if(CMAKE_COMPILER_IS_GNUCXX OR OV_COMPILER_IS_CLANG OR (OV_COMPILER_IS_INTEL_LLVM AND UNIX))
         target_compile_options(${TARGET_NAME} PRIVATE -Wno-missing-declarations)
     endif()
 
