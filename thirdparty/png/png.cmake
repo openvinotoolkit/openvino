c1d401fafe8566785a9a34a6922eba345ae47529
sgui/a3213105 <ysheng.gui@intel.com>
2024-09-18 09:09:52 +0000

    move libpng to static build

--- thirdparty/png/png.cmake
+++ thirdparty/png/png.cmake
@@ -10,7 +10,23 @@ execute_process(COMMAND "${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/png/build.sh"
 message(STATUS "result_var=${result_var}")
 if(result_var STREQUAL  "0")
     set(PNG_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/png/include)
-    set(PNG_LIBRARIES ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/png/lib/libpng.so)
+    find_path(PNG_PKG_FILE
+        NAMES libpng.pc
+        HINTS  "${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/png/lib/" "${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/png/lib/pkgconfig/")
+    if(PNG_PKG_FILE)
+        file(STRINGS "${PNG_PKG_FILE}/libpng.pc"
+            _png_lib_flags REGEX "^Libs.private:+.*")
+        if(_png_lib_flags)
+            string(REPLACE "Libs.private: "
+                "" png_lib_flags "${_png_lib_flags}")
+        endif()
+        message(STATUS "found libpng.pc in ${PNG_PKG_FILE}/libpng.pc, PNG_LIB_FLAGS=${png_lib_flags}, _png_lib_flags=${_png_lib_flags}") 
+    else()
+        message(STATUS "can not find libpng.pc") 
+        set(png_lib_flags "")
+    endif()
+
+    set(PNG_LIBRARIES "${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/png/lib/libpng.a" "${png_lib_flags}")
     set(PNG_FOUND TRUE)
     message(STATUS "PNG is found, PNG_INCLUDE_DIR=${PNG_INCLUDE_DIR}, PNG_LIBRARIES=${PNG_LIBRARIES}") 
 else()
