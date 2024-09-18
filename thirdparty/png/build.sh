8bd8cf711b93b5d559ffa4124c0c7881917b167c
sgui/a3213105 <ysheng.gui@intel.com>
2024-09-18 06:59:28 +0000

    Add build.sh

--- thirdparty/png/build.sh
+++ thirdparty/png/build.sh
@@ -2,7 +2,7 @@ tar zxvf libpng-1.6.37.tar.gz
 cd libpng-1.6.37
 mkdir build
 cd build
-cmake ../ -DPNG_SHARED=ON -DPNG_TESTS=OFF -DCMAKE_INSTALL_PREFIX=./_install
+cmake ../ -DPNG_SHARED=OFF -DPNG_TESTS=OFF -DCMAKE_INSTALL_PREFIX=./_install
 make -j8
 make install
 rm -rf ../../include
