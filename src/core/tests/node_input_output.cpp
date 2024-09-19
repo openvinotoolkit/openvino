5b92b1eed7d5724211fed4cde94d860254ace44e
Andrii Staikov <andrii.staikov@intel.com>
2024-09-19 12:37:11 +0200

    add tests for conversion

--- src/core/tests/node_input_output.cpp
+++ src/core/tests/node_input_output.cpp
@@ -10,9 +10,15 @@
 #include <vector>
 
 #include "openvino/op/add.hpp"
+#include "openvino/op/constant.hpp"
+#include "openvino/op/shape_of.hpp"
+#include "openvino/op/subtract.hpp"
 #include "openvino/op/parameter.hpp"
+#include "openvino/op/variadic_split.hpp"
+#include "openvino/op/topk.hpp"
 
 using namespace ov;
+using namespace ov::op;
 using namespace std;
 
 TEST(node_input_output, input_create) {
@@ -149,4 +155,31 @@ TEST(node_input_output, create_wrong_input_output) {
     EXPECT_THROW(ov::Output<const ov::Node>(nullptr, 0), ov::Exception);
     EXPECT_THROW(ov::Input<ov::Node>(nullptr, 0), ov::Exception);
     EXPECT_THROW(ov::Input<const ov::Node>(nullptr, 0), ov::Exception);
+}
+
+TEST(OutputNodeConversion, OneOutputNode) {
+    auto param = std::make_shared<op::v0::Parameter>(element::i32, Shape{});
+    auto param1 = std::make_shared<op::v0::Parameter>(element::i32, Shape{});
+
+    auto add = std::make_shared<op::v1::Add>(param, param1);
+
+    auto res = std::make_shared<op::v0::Result>(add);
+
+    ASSERT_EQ(add->output(0), add);
+}
+
+TEST(OutputNodeConversion, ManyOutputNode) {
+    auto param = std::make_shared<op::v0::Parameter>(element::i32, Shape{10});
+    auto k = op::v0::Constant::create(ov::element::i32, Shape{}, {3});
+
+    auto topk = std::make_shared<op::v11::TopK>(param,
+                                                k,
+                                                0,
+                                                v11::TopK::Mode::MAX,
+                                                ov::op::v11::TopK::SortType::NONE);
+
+    auto res = std::make_shared<op::v0::Result>(topk);
+
+    ASSERT_EQ(topk->output(0), topk);
+    ASSERT_NE(topk->output(1), topk);
 }
