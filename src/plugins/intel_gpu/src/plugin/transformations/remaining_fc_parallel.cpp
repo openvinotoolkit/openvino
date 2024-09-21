5c54d6cce93441d0a7d73c1d15f666fba1ea4e2a
fishbell <bell.song@intel.com>
2024-09-22 03:07:20 +0800

    fix build issue
    Signed-off-by: fishbell <bell.song@intel.com>

--- src/plugins/intel_gpu/src/plugin/transformations/remaining_fc_parallel.cpp
+++ src/plugins/intel_gpu/src/plugin/transformations/remaining_fc_parallel.cpp
@@ -171,7 +171,7 @@ RemainFCParallelFusion::RemainFCParallelFusion(size_t world_size, size_t world_r
                     }
                 }
                 std::shared_ptr<ov::intel_gpu::op::SyncTensor> sync_node;
-                sync_node = std::make_shared<ov::intel_gpu::op::SyncTensor>(new_fc, world_size, splitted_context.second,
+                sync_node = std::make_shared<ov::intel_gpu::op::SyncTensor>(new_fc, world_size, world_rank, splitted_context.second,
                                                                            new_fc->get_element_type());
                 sync_node->set_friendly_name(new_fc->get_friendly_name()+ "_TP");
 
