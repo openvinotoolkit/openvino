340ace839a117d4094aa298f3cf5c9f2bf38a6c7
Andrei Kashchikhin <andrey.kashchikhin@intel.com>
2024-09-23 15:38:48 +0100

    use another condition

--- src/frontends/tensorflow_lite/tests/convert_tricky_models.cpp
+++ src/frontends/tensorflow_lite/tests/convert_tricky_models.cpp
@@ -21,7 +21,7 @@ static std::string s_manifest = "";
 using Inputs = std::vector<std::vector<float>>;
 using Outputs = std::vector<std::vector<float>>;
 
-#if(defined OPENVINO_ARCH_ARM && defined LINUX)
+#if(defined OPENVINO_ARCH_ARM && ! defined (_WIN32))
 // Ticket: 153164
 OPENVINO_TEST(TensorFlowLiteTrickyModels, DISABLED_tflite_dequantize) {
 #else
