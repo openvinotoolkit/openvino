9221e9c7b06cbd2230ba614cfb7b6c11a1907e97
Andrei Kashchikhin <andrey.kashchikhin@intel.com>
2024-09-23 13:09:20 +0100

    try with __linux__

--- src/common/low_precision_transformations/tests/fake_quantize_transformation.cpp
+++ src/common/low_precision_transformations/tests/fake_quantize_transformation.cpp
@@ -120,7 +120,7 @@ public:
     }
 };
 
-#if(defined OPENVINO_ARCH_ARM && defined LINUX)
+#if defined OPENVINO_ARCH_ARM64 && defined __linux__
 // Ticket: 153155
 TEST_P(FakeQuantizeTransformation, DISABLED_CompareFunctions) {
 #else
