07b5115a9cef7aff92c3dd1166ddb6bba6712012
Andrei Kashchikhin <andrey.kashchikhin@intel.com>
2024-09-23 14:35:01 +0100

    try with lowercase

--- src/common/low_precision_transformations/tests/fake_quantize_transformation.cpp
+++ src/common/low_precision_transformations/tests/fake_quantize_transformation.cpp
@@ -120,7 +120,7 @@ public:
     }
 };
 
-#if defined OPENVINO_ARCH_ARM
+#if defined OPENVINO_ARCH_ARM && defined linux
 // Ticket: 153155
 TEST_P(FakeQuantizeTransformation, DISABLED_CompareFunctions) {
 #else
