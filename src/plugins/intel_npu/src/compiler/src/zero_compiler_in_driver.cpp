b3c3574778f2dba979aed32f85769543ae933eaf
Razvan Apetroaie <razvan-mihai.apetroaie@intel.com>
2024-09-23 13:47:53 +0000

    Parsing I/O weights in the plugin-driver adapter

--- src/plugins/intel_npu/src/compiler/src/zero_compiler_in_driver.cpp
+++ src/plugins/intel_npu/src/compiler/src/zero_compiler_in_driver.cpp
@@ -1070,6 +1070,9 @@ static IODescriptor getIODescriptor(const ze_graph_argument_properties_3_t& arg,
     bool isStateInput = false;
     bool isStateOutput = false;
     bool isShapeTensor = false;
+    bool isInitInputWeights = false;
+    bool isInitOutputWeights = false;
+    bool isMainInputWeights = false;
     if (isStateInputName(nameFromCompiler)) {
         nameFromCompiler = nameFromCompiler.substr(READVALUE_PREFIX.length());
         isStateInput = true;
@@ -1079,6 +1082,15 @@ static IODescriptor getIODescriptor(const ze_graph_argument_properties_3_t& arg,
     } else if (isShapeTensorName(nameFromCompiler)) {
         nameFromCompiler = nameFromCompiler.substr(SHAPE_TENSOR_PREFIX.length());
         isShapeTensor = true;
+    } else if (isInitInputWeightsName(nameFromCompiler)) {
+        nameFromCompiler = nameFromCompiler.substr(INIT_INPUT_WEIGHTS_PREFIX.length());
+        isInitInputWeights = true;
+    } else if (isInitOutputWeightsName(nameFromCompiler)) {
+        nameFromCompiler = nameFromCompiler.substr(INIT_OUTPUT_WEIGHTS_PREFIX.length());
+        isInitOutputWeights = true;
+    } else if (isMainInputWeightsName(nameFromCompiler)) {
+        nameFromCompiler = nameFromCompiler.substr(MAIN_INPUT_WEIGHTS_PREFIX.length());
+        isMainInputWeights = true;
     }
 
     return {std::move(nameFromCompiler),
@@ -1087,6 +1099,9 @@ static IODescriptor getIODescriptor(const ze_graph_argument_properties_3_t& arg,
             isStateInput,
             isStateOutput,
             isShapeTensor,
+            isInitInputWeights,
+            isInitOutputWeights,
+            isMainInputWeights,
             std::nullopt,
             arg.debug_friendly_name,
             std::move(outputTensorNames),
