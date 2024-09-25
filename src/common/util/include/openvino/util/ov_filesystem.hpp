640065ac1a5c1028bce31ed515f7059f6657f05e
Barnas, Michal <michal.barnas@intel.com>
2024-09-25 11:54:42 +0200

    fix windows build

--- src/common/util/include/openvino/util/ov_filesystem.hpp
+++ src/common/util/include/openvino/util/ov_filesystem.hpp
@@ -22,7 +22,7 @@
 #        define CPP_VER_11
 #        if _MSVC_LANG >= 201402L
 #            define CPP_VER_14
-#            if _MSVC_LANG > 201402L
+#            if _MSVC_LANG >= 201703L
 #                define CPP_VER_17
 #                if _MSVC_LANG >= 202002L
 #                    define CPP_VER_20
@@ -32,28 +32,28 @@
 #    endif
 #endif
 
-#ifdef __has_include
+#if defined(_MSC_VER) && defined(CPP_VER_17)
+#    define HAS_FILESYSTEM 1
+#elif defined(_MSC_VER) && defined(CPP_VER_11)
+#    define HAS_EXP_FILESYSTEM 1
+#    define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
+#    define _LIBCPP_NO_EXPERIMENTAL_DEPRECATION_WARNING_FILESYSTEM
+#elif defined(__has_include)
 #    if defined(CPP_VER_17) && (__has_include(<filesystem>)) && (!__has_include(<experimental/filesystem>))
 #        define HAS_FILESYSTEM 1
 #    elif defined(CPP_VER_11) && (__has_include(<experimental/filesystem>))
 #        define HAS_EXP_FILESYSTEM 1
 #    endif
-#elif defined(_MSC_VER) && defined(CPP_VER_17)
-#    define HAS_FILESYSTEM 1
-#elif defined(_MSC_VER) && (defined(CPP_VER_11) || defined(CPP_VER_14))
-#    define HAS_EXP_FILESYSTEM 1
-#    define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
-#    define _LIBCPP_NO_EXPERIMENTAL_DEPRECATION_WARNING_FILESYSTEM
 #endif
 
 #if !defined(HAS_FILESYSTEM) && !defined(HAS_EXP_FILESYSTEM)
 #    error "Neither #include <filesystem> nor #include <experimental/filesystem> is available."
 #elif defined(HAS_FILESYSTEM)
-#    include <filesystem>
-namespace std_fs = std::filesystem;
+    #include <filesystem>
+    namespace std_fs = std::filesystem;
 #elif defined(HAS_EXP_FILESYSTEM)
-#    include <experimental/filesystem>
-namespace std_fs = std::experimental::filesystem;
+    #include <experimental/filesystem>
+    namespace std_fs = std::experimental::filesystem;
 #endif
 
 // #if !defined(__EMSCRIPTEN__) && !defined(__ANDROID__)
