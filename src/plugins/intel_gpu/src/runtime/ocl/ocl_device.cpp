3da6ce249960e0b410caf58723af273349b05394
Kim, SungEun <sungeun.kim@intel.com>
2024-09-26 21:55:05 +0900

    [GPU] update cmake to add xe3

--- src/plugins/intel_gpu/src/runtime/ocl/ocl_device.cpp
+++ src/plugins/intel_gpu/src/runtime/ocl/ocl_device.cpp
@@ -61,6 +61,7 @@ gpu_arch convert_ngen_arch(ngen::HW gpu_arch) {
         case ngen::HW::XeHPG: return gpu_arch::xe_hpg;
         case ngen::HW::XeHPC: return gpu_arch::xe_hpc;
         case ngen::HW::Xe2: return gpu_arch::xe2;
+        case ngen::HW::Xe3: return gpu_arch::xe3;
         case ngen::HW::Gen10:
         case ngen::HW::Unknown: return gpu_arch::unknown;
     }
