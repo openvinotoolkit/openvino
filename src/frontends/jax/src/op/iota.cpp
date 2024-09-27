f9f8c75efb2a94687236bf8c9c4084d73404358e
muhd360 <masheikh_b21@et.vjti.ac.in>
2024-09-27 11:15:33 +0530

    resolve

--- src/frontends/jax/src/op/iota.cpp
+++ src/frontends/jax/src/op/iota.cpp
@@ -16,17 +16,17 @@ OutputVector translate_iota(const NodeContext& context) {
     num_inputs_check(context, 1, 1);
 
     // Get the dtype and size from the input
-    auto dtype = context.get_input(0);
-    auto element1 = dtype.get_element_type();
+    auto dtype = context.const_named_param<element::Type>("dtype");
+    
     auto size = context.const_named_param<int64_t>("size");
 
     // Create constant nodes for start, limit, and step
-    auto start = v0::Constant::create(element1, Shape({}), {0});
-    auto limit = v0::Constant::create(element1, Shape({}), {size});
-    auto step = v0::Constant::create(element1, Shape{}, {1});
+    auto start = v0::Constant::create(element::i64, Shape({}), {0});
+    auto limit = v0::Constant::create(element::i64, Shape({}), {size});
+    auto step = v0::Constant::create(element::i64, Shape{}, {1});
 
     // Create the Range operation
-    auto range = std::make_shared<v4::Range>(start, limit, step);
+    auto range = std::make_shared<v4::Range>(start, limit, step,dtype);
 
     return {range};
 }
