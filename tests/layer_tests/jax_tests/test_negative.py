25798847be85285ace34f6ab70181573e334a47d
Roman Kazantsev <roman.kazantsev@intel.com>
2024-09-28 20:08:36 +0400

    Update tests/layer_tests/jax_tests/test_negative.py

--- tests/layer_tests/jax_tests/test_negative.py
+++ tests/layer_tests/jax_tests/test_negative.py
@@ -38,7 +38,7 @@ class TestNeg(JaxLayerTest):
         [6, 5, 4, 3, 2],
         [1, 1, 9, 1]
     ])
-    @pytest.mark.parametrize('input_type', [np.int8,np.int16,np.int32, np.int64,np.float16, np.float32, np.float64])
+    @pytest.mark.parametrize('input_type', [np.int8, np.int16, np.int32, np.int64,np.float16, np.float32, np.float64])
     @pytest.mark.nightly
     @pytest.mark.precommit
     @pytest.mark.precommit_jax_fe
