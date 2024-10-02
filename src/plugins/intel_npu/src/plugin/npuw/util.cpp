23e0255b22c7bd7159e21c2790e0084c37040bd3
Ekaterina Shiryaeva <ekaterina.shiriaeva@intel.com>
2024-10-02 18:45:16 +0100

    NPUW: AVX2 quick fix - missed to_fp16 function

--- src/plugins/intel_npu/src/plugin/npuw/util.cpp
+++ src/plugins/intel_npu/src/plugin/npuw/util.cpp
@@ -1564,6 +1564,7 @@ void ov::npuw::util::to_f16(ov::Tensor& t) {
     NPUW_ASSERT(t.get_size() % 8 == 0);
     NPUW_ASSERT(t.is_continuous());
 
+#if defined(HAVE_AVX2)
     ov::Tensor tnew(ov::element::f16, shape);
 
     const float* psrc = t.data<float>();
@@ -1579,6 +1580,9 @@ void ov::npuw::util::to_f16(ov::Tensor& t) {
     }
 
     t = std::move(tnew);
+#else
+    throw std::runtime_error("AVX2 support is neccessary but it's not enabled!");
+#endif
 }
 
 inline uint8_t tread_4b(const ov::Tensor& t, std::size_t r, std::size_t c, std::size_t COLS) {
