16a4032edf1e139f76db7bb02accf10a511f698c
Piotr Kowalczyk <piotr.kowalczyk@intel.com>
2024-10-09 13:21:30 +0000

    [Pytorch fronted]: Added support for SearchSorted op.

---
+++ src/frontends/pytorch/src/op/search_sorted.cpp
@@ -0,0 +1,32 @@
+// Copyright (C) 2018-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "openvino/op/search_sorted.hpp"
+
+#include "openvino/frontend/pytorch/node_context.hpp"
+#include "utils.hpp"
+
+namespace ov {
+namespace frontend {
+namespace pytorch {
+namespace op {
+
+using namespace ov::op;
+
+OutputVector translate_search_sorted(const NodeContext& context) {
+    num_inputs_check(context, 2, 5);
+    auto sorted = context.get_input(0);
+    auto values = context.get_input(1);
+    const bool out_int32 = context.const_input<bool>(2);
+    PYTORCH_OP_CONVERSION_CHECK(out_int32 == false, "aten::searchsorted(out_int32=true) unsupported");
+    const bool right_mode = context.const_input<bool>(3);
+    PYTORCH_OP_CONVERSION_CHECK(context.input_is_none(4), "aten::searchsorted(side) unsupported");
+    PYTORCH_OP_CONVERSION_CHECK(context.input_is_none(5), "aten::searchsorted(sorter) unsupported");
+    auto sign = context.mark_node(std::make_shared<ov::op::v15::SearchSorted>(sorted, values, right_mode));
+    return {sign};
+};
+}  // namespace op
+}  // namespace pytorch
+}  // namespace frontend
+}  // namespace ov
