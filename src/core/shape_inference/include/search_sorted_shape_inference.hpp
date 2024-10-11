144787f86b7feeb5482b9da7d0a5d37e86d3a282
p-wysocki <przemyslaw.wysocki@intel.com>
2024-10-11 10:50:10 +0200

    CR

--- src/core/shape_inference/include/search_sorted_shape_inference.hpp
+++ src/core/shape_inference/include/search_sorted_shape_inference.hpp
@@ -14,33 +14,35 @@ template <class TShape, class TRShape = result_shape_t<TShape>, class TDim = typ
 std::vector<TRShape> shape_infer(const SearchSorted* op, const std::vector<TShape>& input_shapes) {
     const auto& sorted_shape = input_shapes[0];
     const auto& values_shape = input_shapes[1];
+    const auto sorted_shape_rank = sorted_shape.rank();
+    const auto values_shape_rank = values_shape.rank();
 
-    if (sorted_shape.rank().is_static()) {
+    if (sorted_shape_rank.is_static()) {
         NODE_SHAPE_INFER_CHECK(op,
                                input_shapes,
-                               sorted_shape.rank().get_length() > 0,
+                               sorted_shape_rank.get_length() > 0,
                                "The sorted sequence input cannot be a scalar.");
     }
 
     TRShape output_shape;
-    if (values_shape.rank().is_dynamic() && sorted_shape.rank().is_static()) {
+    if (values_shape_rank.is_dynamic() && sorted_shape_rank.is_static()) {
         output_shape = sorted_shape;
         output_shape[sorted_shape.size() - 1] = Dimension::dynamic();
     } else {
         output_shape = values_shape;
     }
 
-    if (values_shape.rank().is_static() && values_shape.rank() == 0) {
+    if (values_shape_rank.is_static() && values_shape_rank == 0) {
         NODE_SHAPE_INFER_CHECK(op,
                                input_shapes,
-                               sorted_shape.rank() == 1,
+                               sorted_shape_rank == 1,
                                "If values input is a scalar the sorted sequence must be a 1D tensor.");
     }
 
-    if (sorted_shape.rank().is_static() && values_shape.rank().is_static() && sorted_shape.rank().get_length() > 1) {
+    if (sorted_shape_rank.is_static() && values_shape_rank.is_static() && sorted_shape_rank.get_length() > 1) {
         NODE_SHAPE_INFER_CHECK(op,
                                input_shapes,
-                               sorted_shape.rank().compatible(values_shape.rank()),
+                               sorted_shape.size() == values_shape.size(),
                                "The inputs' ranks have to be compatible.");
         for (size_t i = 0; i < sorted_shape.size() - 1; ++i) {
             NODE_SHAPE_INFER_CHECK(op,
