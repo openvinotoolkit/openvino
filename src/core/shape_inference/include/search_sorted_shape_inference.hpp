8ee18dadcbb50d1de9c01f1a2bb2baddd77206f5
Piotr Kowalczyk <piotr.kowalczyk@intel.com>
2024-10-11 13:47:41 +0000

    [SearchSortedDef]: Bugfixes in shape_infer

--- src/core/shape_inference/include/search_sorted_shape_inference.hpp
+++ src/core/shape_inference/include/search_sorted_shape_inference.hpp
@@ -16,17 +16,26 @@ std::vector<TRShape> shape_infer(const SearchSorted* op, const std::vector<TShap
     op->validate();
     const auto& sorted_shape = input_shapes[0];
     const auto& values_shape = input_shapes[1];
-    auto output_shape = values_shape;
-    TShape::merge_into(output_shape, sorted_shape);
 
-    if (output_shape.rank().is_static()) {
-        auto last_it = output_shape.end() - 1;
-        if (values_shape.rank().is_static()) {
-            *last_it = *(input_shapes[1].end() - 1);
-        } else {
-            *last_it = Dimension::dynamic();
-        }
+    auto output_shape = values_shape;
+
+    // NOTE: The output shape is the same as the values shape - always.
+    // The code below only tries to infer dynamic dims from sorted_shape - if possible.
+
+    // 1. If we know that the sorted sequence is 1D, than output shape can be anything.
+    if (sorted_shape.rank().is_static() && sorted_shape.rank().get_length() == 1) {
+        return {std::move(output_shape)};
     }
+
+    // 2. ND tensor case or rank not known.
+    auto sorted_shape_last_dynamic = sorted_shape;
+    if (sorted_shape.rank().is_static()) {
+        sorted_shape_last_dynamic[sorted_shape.rank().get_length() - 1] = Dimension::dynamic();
+    }
+
+    const bool sorted_values_merge_success = TShape::merge_into(output_shape, sorted_shape_last_dynamic);
+
+    NODE_VALIDATION_CHECK(op, sorted_values_merge_success, "Shapes of sorted sequence and values are not compatible.");
 
     return {std::move(output_shape)};
 }
