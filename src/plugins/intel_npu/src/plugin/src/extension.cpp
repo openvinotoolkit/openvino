c8b77b14669797f2f6e5db886c74685f7657a0d2
Mircea-Aurelian Dan <mircea-aurelian.dan@intel.com>
2024-10-14 11:29:49 +0300

    [`intel-npu`] [`SUPPORT INTERNAL OPERATORS`] [`RMS`] Add support for RMS internal operator (#26899)
    ### Details:
    - *Add `extension` header in `npu plugin` to support OV internal
    operators*
    
    ### Tickets:
     - *CVS-151466*

---
+++ src/plugins/intel_npu/src/plugin/src/extension.cpp
@@ -0,0 +1,14 @@
+// Copyright (C) 2018-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "openvino/core/extension.hpp"
+
+#include "openvino/core/op_extension.hpp"
+#include "ov_ops/rms.hpp"
+
+#define OP_EXTENSION(NAME) std::make_shared<ov::OpExtension<NAME>>(),
+
+#define NPU_SUPPORTED_EXTENSIONS OP_EXTENSION(ov::op::internal::RMS)
+
+OPENVINO_CREATE_EXTENSIONS(std::vector<ov::Extension::Ptr>({NPU_SUPPORTED_EXTENSIONS}));
