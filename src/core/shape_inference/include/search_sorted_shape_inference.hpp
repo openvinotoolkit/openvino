34d6dbfae4ad8d4347c2a9d54cbf82af064453c8
p-wysocki <przemyslaw.wysocki@intel.com>
2024-10-14 12:47:34 +0200

    Clang

--- src/core/shape_inference/include/search_sorted_shape_inference.hpp
+++ src/core/shape_inference/include/search_sorted_shape_inference.hpp
@@ -21,32 +21,29 @@ std::vector<TRShape> shape_infer(const SearchSorted* op, const std::vector<TShap
     output_shape = is_values_rank_static ? values_shape : sorted_shape;
     if (is_sorted_rank_static) {
         const auto sorted_in_rank = sorted_shape.size();
-        NODE_SHAPE_INFER_CHECK(op,
-                               input_shapes,
-                               sorted_in_rank > 0,
-                               "The sorted sequence input cannot be a scalar.");
+        NODE_SHAPE_INFER_CHECK(op, input_shapes, sorted_in_rank > 0, "The sorted sequence input cannot be a scalar.");
         if (sorted_in_rank == 1) {
             output_shape = values_shape;
             return {std::move(output_shape)};
-        }
-        else if (!is_values_rank_static) {
+        } else if (!is_values_rank_static) {
             output_shape[sorted_in_rank - 1] = Dimension::dynamic();
         } else {
             const auto values_in_rank = values_shape.size();
             NODE_SHAPE_INFER_CHECK(op,
-                                input_shapes,
-                                sorted_in_rank == values_in_rank,
-                                "The inputs' ranks have to be compatible. If values input is a scalar the sorted sequence must be a 1D tensor.");
+                                   input_shapes,
+                                   sorted_in_rank == values_in_rank,
+                                   "The inputs' ranks have to be compatible. If values input is a scalar the sorted "
+                                   "sequence must be a 1D tensor.");
             for (size_t i = 0; i < sorted_in_rank - 1; ++i) {
                 NODE_SHAPE_INFER_CHECK(op,
-                                    input_shapes,
-                                    TDim::merge(output_shape[i], sorted_shape[i], values_shape[i]),
-                                    "All dimensions but the last one have to be compatible.");
+                                       input_shapes,
+                                       TDim::merge(output_shape[i], sorted_shape[i], values_shape[i]),
+                                       "All dimensions but the last one have to be compatible.");
             }
         }
     }
     return {std::move(output_shape)};
-    }
+}
 }  // namespace v15
 }  // namespace op
 }  // namespace ov
