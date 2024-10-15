b3e4ddb5aabcc3d8d2fef8e3fa371dd7013de19c
Anastasiya(Asya) Pronina <anastasiya.pronina@intel.com>
2024-10-15 15:11:31 +0200

    Fixed tests for NPUW (#27060)
    ### Details:
    - *Fixed tests for changes in FUNCALL_ASYNC and calls to
    get_default_method()*
    
    ### Tickets:
     - *EISW-143057*
     - *CVS-154787*

--- src/plugins/intel_npu/tests/functional/behavior/npuw/mocks/mock_plugins.cpp
+++ src/plugins/intel_npu/tests/functional/behavior/npuw/mocks/mock_plugins.cpp
@@ -245,7 +245,7 @@ void MockPluginBase<DeviceType>::create_implementation() {
             });
     ON_CALL(*this, get_default_context)
             .WillByDefault([](const ov::AnyMap& remote_properties) -> ov::SoPtr<ov::IRemoteContext> {
-                OPENVINO_NOT_IMPLEMENTED;
+                return std::make_shared<MockRemoteContext>(device_name);
             });
     ON_CALL(*this, import_model(testing::_, testing::_))
             .WillByDefault([](std::istream& model, const ov::AnyMap& properties)
