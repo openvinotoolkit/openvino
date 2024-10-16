e044272ae8d9b81efc1a592582111f2f20b682c3
Durandin, Pavel <pavel.durandin@intel.com>
2024-10-16 12:03:36 +0400

    [GPU] Clean oneDnn cache

--- src/plugins/intel_gpu/include/intel_gpu/plugin/plugin.hpp
+++ src/plugins/intel_gpu/include/intel_gpu/plugin/plugin.hpp
@@ -48,6 +48,11 @@ private:
 public:
     Plugin();
 
+    ~Plugin() 
+    {
+        dnnl::set_primitive_cache_capacity(0);
+    }
+
     std::shared_ptr<ov::ICompiledModel> compile_model(const std::shared_ptr<const ov::Model>& model,
                                                       const ov::AnyMap& properties) const override;
     std::shared_ptr<ov::ICompiledModel> compile_model(const std::shared_ptr<const ov::Model>& model,
