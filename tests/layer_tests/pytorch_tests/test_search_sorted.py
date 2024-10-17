9765fa3965b07935c366a7e63905120c8aaca2fb
Piotr Kowalczyk <piotr.kowalczyk@intel.com>
2024-10-17 09:59:07 +0000

    [pytorch]: SearchSorted unittests: added malicios test cases for conversion.

--- tests/layer_tests/pytorch_tests/test_search_sorted.py
+++ tests/layer_tests/pytorch_tests/test_search_sorted.py
@@ -28,9 +28,14 @@ class TestSearchSorted(PytorchLayerTest):
 
     @pytest.mark.nightly
     @pytest.mark.precommit
-    @pytest.mark.parametrize(("sorted", "values"), [([[1, 3, 5, 7, 9], [2, 4, 6, 8, 10]], [[3, 6, 9], [3, 6, 9]]),([1, 3, 5, 7, 9], [[3, 6, 9],[0, 5, 20]])])
+    @pytest.mark.parametrize(("sorted", "values"), [
+            ([[1, 3, 5, 7, 9], [2, 4, 6, 8, 10]], [[3, 6, 9], [3, 6, 9]]),
+            ([1, 3, 5, 7, 9], [[3, 6, 9],[0, 5, 20]]),
+            ([4091, 4092], [[4091, 4092]]), # fp16 cannot exactly represent 4091 number 
+            ([1.23, 2.99], [[1.355, 2.9991]])     
+        ])
     @pytest.mark.parametrize("right_mode", [False, True])
-    @pytest.mark.parametrize("sorted_type", [np.float32, np.int32, np.int8])
+    @pytest.mark.parametrize("sorted_type", [np.float32, np.float16, np.int8])
     @pytest.mark.parametrize("values_type", [np.float16, np.int32, np.int64])
     def test_searchsorted(self, sorted, values, right_mode, sorted_type, values_type, ie_device, precision, ir_version):
         self.sorted = sorted
