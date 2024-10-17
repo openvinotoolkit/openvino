4d0cfc894c3bfe03c381131a4b022dc7708a6b53
Alexandra Sidorova <alexandra.sidorova@intel.com>
2024-10-17 10:16:28 +0200

    [Snippets] Added MarkInvariantShapePath to support dynamic memory inplace

--- src/common/snippets/src/lowered/loop_info.cpp
+++ src/common/snippets/src/lowered/loop_info.cpp
@@ -176,6 +176,19 @@ bool UnifiedLoopInfo::LoopPortDesc::is_dynamic() const {
     return utils::is_dynamic_value(ptr_increment) || utils::is_dynamic_value(finalization_offset);
 }
 
+bool UnifiedLoopInfo::LoopPortDesc::is_static() const {
+    return !utils::is_dynamic_value(ptr_increment) && !utils::is_dynamic_value(finalization_offset);
+}
+
+bool operator==(const UnifiedLoopInfo::LoopPortDesc& lhs, const UnifiedLoopInfo::LoopPortDesc& rhs) {
+    if (&lhs == &rhs)
+        return true;
+    return lhs.ptr_increment == rhs.ptr_increment && lhs.finalization_offset == rhs.finalization_offset && lhs.data_size == rhs.data_size;
+}
+bool operator!=(const UnifiedLoopInfo::LoopPortDesc& lhs, const UnifiedLoopInfo::LoopPortDesc& rhs) {
+    return !(rhs == lhs);
+}
+
 UnifiedLoopInfo::UnifiedLoopInfo(size_t work_amount, size_t increment,
                                  const std::vector<LoopPort>& entries, const std::vector<LoopPort>& exits,
                                  const SpecificIterationHandlers& handlers)
@@ -319,6 +332,18 @@ void UnifiedLoopInfo::sort_ports() {
     };
     reorder(m_input_ports, m_input_port_descs);
     reorder(m_output_ports, m_output_port_descs);
+}
+
+UnifiedLoopInfo::LoopPortInfo UnifiedLoopInfo::get_loop_port_info(const ExpressionPort& expr_port) {
+    OPENVINO_ASSERT(is_loop_port(expr_port), "Failed get_loop_port: expr_port is not a loop port");
+    const auto is_input = expr_port.get_type() == ExpressionPort::Input;
+    const auto& ports = is_input ? m_input_ports : m_output_ports;
+    const auto& descs = is_input ? m_input_port_descs : m_output_port_descs;
+    const auto it = std::find_if(ports.begin(), ports.end(),
+                                [&expr_port](const LoopPort& port) { return *port.expr_port.get() == expr_port; });
+    const auto index = static_cast<size_t>(std::distance(ports.cbegin(), it));
+    OPENVINO_ASSERT(index < ports.size() && index < descs.size(), "LoopPortInfo has not ben found!");
+    return LoopPortInfo(ports[index], descs[index]);
 }
 
 void UnifiedLoopInfo::replace_with_cloned_descs(size_t actual_port_idx, size_t new_count, bool is_input) {
