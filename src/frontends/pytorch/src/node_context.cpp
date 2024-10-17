0c1573cc84d1f293690965e666ae1e7084d47033
Kazantsev, Roman <roman.kazantsev@intel.com>
2024-10-17 13:35:03 +0400

    [PT FE] Handle None-value case
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/frontends/pytorch/src/node_context.cpp
+++ src/frontends/pytorch/src/node_context.cpp
@@ -4,6 +4,7 @@
 
 #include "openvino/frontend/pytorch/node_context.hpp"
 
+#include "helper_ops/internal_op.hpp"
 #include "openvino/core/validation_util.hpp"
 #include "openvino/frontend/exception.hpp"
 #include "openvino/frontend/pytorch/decoder.hpp"
@@ -147,6 +148,13 @@ std::shared_ptr<ov::Model> NodeContext::convert_subgraph(size_t index) const {
 OutputVector NodeContext::inputs() const {
     OutputVector res;
     for (size_t i = 0; i < m_decoder_inputs.size(); i++) {
+        if (input_is_none(i)) {
+            // some operations like aten.index.tensor can have None inputs
+            auto decoder_with_node = std::make_shared<InternalOpDecoder>("none_value", 1);
+            auto op_with_none = std::make_shared<PtFrameworkNode>(decoder_with_node, ov::OutputVector{});
+            res.push_back(op_with_none->output(0));
+            continue;
+        }
         auto input = m_decoder_inputs.at(i);
         if (input == 0) {
             // Case when input can be inlined (possible only for fx decoder)
