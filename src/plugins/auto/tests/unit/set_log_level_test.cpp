51e509a5422a037b803fce866456c9861161ee0e
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2024-10-18 19:12:33 +0200

    skip all log tests

--- src/plugins/auto/tests/unit/set_log_level_test.cpp
+++ src/plugins/auto/tests/unit/set_log_level_test.cpp
@@ -55,11 +55,9 @@ TEST_P(AutoSetLogLevel, setLogLevelFromConfig) {
     std::string log_level;
     ov::AnyMap config;
     std::tie(log_level, config) = this->GetParam();
-    #if defined(WIN32) && !defined(NDEBUG)
-    if (log_level == "LOG_INFO") {
-        GTEST_SKIP() << "Skipping on Windows in Debug mode due to Issue 155400.";
-    }
-    #endif
+#if defined(WIN32) && !defined(NDEBUG)
+    GTEST_SKIP() << "Skipping on Windows in Debug mode due to Issue 155400.";
+#endif
     plugin->set_device_name("AUTO");
     plugin->compile_model(model, config);
     int a = 0;
