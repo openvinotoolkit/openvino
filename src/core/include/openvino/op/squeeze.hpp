8830412bc5d60355de8e3d5820f3af0bee5c2f43
Michal Barnas <michal.barnas@intel.com>
2024-10-18 14:32:17 +0000

    squeeze v15 no common bese class

--- src/core/include/openvino/op/squeeze.hpp
+++ src/core/include/openvino/op/squeeze.hpp
@@ -41,11 +41,12 @@ private:
 }  // namespace v0
 
 namespace v15 {
-class OPENVINO_API Squeeze : public v0::Squeeze {
+class OPENVINO_API Squeeze : public Op {
 public:
     OPENVINO_OP("Squeeze", "opset15");
 
-    using v0::Squeeze::Squeeze;
+    Squeeze();
+    Squeeze(const Output<Node>& data);
 
     /// \brief Constructs a squeeze operation.
     ///
@@ -54,19 +55,25 @@ public:
     /// \param allow_axis_skip Shape inference result dynamic rank if selected axis has 1 in range of its dynamic
     Squeeze(const Output<Node>& data, const Output<Node>& axes, const bool allow_axis_skip = false);
 
+    void validate_and_infer_types() override;
+    bool evaluate(TensorVector& outputs, const TensorVector& inputs) const override;
+    bool has_evaluate() const override;
+    bool evaluate_lower(TensorVector& outputs) const override;
+    bool evaluate_upper(TensorVector& outputs) const override;
+    bool evaluate_symbol(TensorSymbolVector& output_symbols) const override;
+    bool constant_fold(OutputVector& output_values, const OutputVector& inputs_values) override;
+
     std::shared_ptr<Node> clone_with_new_inputs(const OutputVector& new_args) const override;
+
+    bool is_dynamic() const override;
 
     bool visit_attributes(AttributeVisitor& visitor) override;
 
     bool get_allow_axis_skip() const;
 
-    std::pair<bool, std::reference_wrapper<const ov::PartialShape>> get_deduced_output_shape() const;
-    void set_deduced_output_shape(const ov::PartialShape& output_shapes);
-
 private:
+    Output<Node> get_default_axes_input() const;
     bool m_allow_axis_skip{};
-    ov::PartialShape deduced_output_shape{};
-    bool is_deduced_output_shape{};
 };
 }  // namespace v15
 }  // namespace op
--- src/core/src/op/squeeze.cpp
+++ src/core/src/op/squeeze.cpp
@@ -14,7 +14,7 @@
 
 namespace ov {
 namespace op {
-namespace v0 {
+
 namespace validate {
 namespace {
 
@@ -24,6 +24,7 @@ bool axes_has_and_set_bound(const Node& op) {
 }  // namespace
 }  // namespace validate
 
+namespace v0 {
 Squeeze::Squeeze() : Op() {}
 
 Squeeze::Squeeze(const Output<Node>& data, const Output<Node>& axes) : Op({data, axes}) {
@@ -125,8 +126,14 @@ bool Squeeze::is_dynamic() const {
 }  // namespace v0
 
 namespace v15 {
+Squeeze::Squeeze() : Op() {}
+
+Squeeze::Squeeze(const Output<Node>& data) : Op({data}) {
+    constructor_validate_and_infer_types();
+}
+
 Squeeze::Squeeze(const Output<Node>& data, const Output<Node>& axes, const bool allow_axis_skip)
-    : v0::Squeeze({data, axes}),
+    : Op({data, axes}),
       m_allow_axis_skip{allow_axis_skip} {
     constructor_validate_and_infer_types();
 }
@@ -145,23 +152,88 @@ std::shared_ptr<Node> Squeeze::clone_with_new_inputs(const OutputVector& new_arg
     }
 }
 
+void Squeeze::validate_and_infer_types() {
+    OV_OP_SCOPE(v15_Squeeze_validate_and_infer_types);
+
+    const auto input_shapes = ov::util::get_node_input_partial_shapes(*this);
+    const auto output_shapes = shape_infer(this, input_shapes);
+
+    set_output_type(0, get_input_element_type(0), output_shapes[0]);
+}
+
+bool Squeeze::evaluate(TensorVector& outputs, const TensorVector& inputs) const {
+    OV_OP_SCOPE(v15_Squeeze_evaluate);
+    OPENVINO_ASSERT(outputs.size() == 1);
+
+    const auto output_shapes =
+        shape_infer(this, ov::util::get_tensors_partial_shapes(inputs), make_tensor_accessor(inputs));
+    outputs[0].set_shape(output_shapes.front().get_shape());
+
+    std::memcpy(outputs[0].data(), inputs[0].data(), outputs[0].get_byte_size());
+    return true;
+}
+
+bool Squeeze::has_evaluate() const {
+    OV_OP_SCOPE(v15_Squeeze_has_evaluate);
+    const auto validate_axes_type = [](const element::Type& et) -> bool {
+        switch (et) {
+        case element::i8:
+        case element::i16:
+        case element::i32:
+        case element::i64:
+        case element::u8:
+        case element::u16:
+        case element::u32:
+        case element::u64:
+            return true;
+        default:
+            return false;
+        }
+    };
+
+    return (get_input_size() < 2) || validate_axes_type(get_input_element_type(1));
+}
+
+bool Squeeze::evaluate_lower(TensorVector& output_values) const {
+    OV_OP_SCOPE(v15_Squeeze_evaluate_lower);
+    return validate::axes_has_and_set_bound(*this) && default_lower_bound_evaluator(this, output_values);
+}
+
+bool Squeeze::evaluate_upper(TensorVector& output_values) const {
+    OV_OP_SCOPE(v15_Squeeze_evaluate_upper);
+    return validate::axes_has_and_set_bound(*this) && default_upper_bound_evaluator(this, output_values);
+}
+
+bool Squeeze::evaluate_symbol(TensorSymbolVector& output_symbols) const {
+    return validate::axes_has_and_set_bound(*this) && ov::util::default_symbol_evaluator(this, output_symbols);
+}
+
+bool Squeeze::constant_fold(OutputVector& output_values, const OutputVector& inputs_values) {
+    OV_OP_SCOPE(v15_Squeeze_constant_fold);
+    if (get_output_partial_shape(0).is_dynamic() || is_const_fold_disabled()) {
+        return false;
+    }
+
+    if (auto data_const = std::dynamic_pointer_cast<opset1::Constant>(inputs_values[0].get_node_shared_ptr())) {
+        const auto& shape = get_output_shape(0);
+        output_values[0] = std::make_shared<v0::Constant>(*data_const, shape);
+        return true;
+    }
+    return false;
+}
+
+bool Squeeze::is_dynamic() const {
+    return get_output_partial_shape(0).is_dynamic();
+}
+
 bool Squeeze::visit_attributes(AttributeVisitor& visitor) {
-    OV_OP_SCOPE(v0_Squeeze_visit_attributes);
+    OV_OP_SCOPE(v15_Squeeze_visit_attributes);
     visitor.on_attribute("allow_axis_skip", m_allow_axis_skip);
     return true;
 }
 
 bool Squeeze::get_allow_axis_skip() const {
     return m_allow_axis_skip;
-}
-
-std::pair<bool, std::reference_wrapper<const ov::PartialShape>> Squeeze::get_deduced_output_shape() const {
-    return {is_deduced_output_shape, std::cref(deduced_output_shape)};
-}
-
-void Squeeze::set_deduced_output_shape(const ov::PartialShape& output_shapes) {
-    deduced_output_shape = output_shapes;
-    is_deduced_output_shape = true;
 }
 }  // namespace v15
 }  // namespace op
