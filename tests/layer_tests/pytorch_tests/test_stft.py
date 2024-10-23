2aa496c54a485c37414eb58d1ea41d1949f4a220
mitruska <katarzyna.mitrus@intel.com>
2024-10-23 13:36:46 +0200

    Reduntant test params cleanup

--- tests/layer_tests/pytorch_tests/test_stft.py
+++ tests/layer_tests/pytorch_tests/test_stft.py
@@ -10,7 +10,7 @@ class TestSTFT(PytorchLayerTest):
     def _prepare_input(self, win_length, out=False, out_dtype="float32"):
         import numpy as np
 
-        signal = np.random.randn(1, 256).astype(out_dtype)
+        signal = np.random.randn(2, 256).astype(out_dtype)
         window = np.hanning(win_length).reshape([win_length])
 
         return (np.array(signal, dtype=out_dtype), window.astype(out_dtype))
@@ -62,23 +62,23 @@ class TestSTFT(PytorchLayerTest):
 
 
 class TestSTFTAttrs(PytorchLayerTest):
-    def _prepare_input(self, win_length, default_window=False, out=False, out_dtype="float32"):
+    def _prepare_input(self, out=False, out_dtype="float32"):
         import numpy as np
 
         signal = np.random.randn(2, 512).astype(out_dtype)
         return [np.array(signal, dtype=out_dtype), ]
 
-    def create_model_with_attrs(self, n_fft, hop_length, win_length, default_window, center, pad_mode, normalized, onesided, return_complex):
+    def create_model_with_attrs(self, n_fft, hop_length, win_length, center, pad_mode, normalized, onesided, return_complex):
         import torch
 
         class aten_stft_attrs(torch.nn.Module):
 
-            def __init__(self, n_fft, hop_length, win_length, default_window, center, pad_mode, normalized, onesided, return_complex):
+            def __init__(self, n_fft, hop_length, win_length, center, pad_mode, normalized, onesided, return_complex):
                 super(aten_stft_attrs, self).__init__()
                 self.n_fft = n_fft
                 self.hop_length = hop_length
                 self.win_length = win_length
-                self.window = default_window
+                self.window = None  # Default window
                 self.center = center
                 self.pad_mode = pad_mode
                 self.normalized = normalized
@@ -101,26 +101,26 @@ class TestSTFTAttrs(PytorchLayerTest):
 
         ref_net = None
 
-        return aten_stft_attrs(n_fft, hop_length, win_length, default_window, center, pad_mode, normalized, onesided, return_complex), ref_net, "aten::stft"
+        return aten_stft_attrs(n_fft, hop_length, win_length, center, pad_mode, normalized, onesided, return_complex), ref_net, "aten::stft"
 
     @pytest.mark.nightly
     @pytest.mark.precommit
     @pytest.mark.parametrize(("trace_model"), [True, False])
-    @pytest.mark.parametrize(("n_fft", "hop_length", "win_length", "default_window", "center", "pad_mode", "normalized", "onesided", "return_complex"), [
-        [16, 4, 16, None, False, "reflect", False, True, False],  # default window
-        [16, 4, 14, None, True, "reflect", False, True, False],  # center True
-        [16, 4, 14, None, True, "reflect", False, True, False],  # center True
-        [16, 4, 14, None, True, "replicate", False, True, False],  # center True
-        [16, 4, 14, None, False, "replicate", False, True, False],  # center False
-        [16, None, 16, None, False, "reflect", False, True, False], # hop_length None
-        [16, None, None, None, False, "reflect", False, True, False], # hop_length & win_length None
-        [16, 4, None, None, False, "reflect", False, True, False], # win_length None
-        ## Unsupported cases:
-        [16, 4, 16, None, False, "reflect", True, True, False],  # normalized True
-        [16, 4, 16, None, False, "reflect", False, False, False],  # onesided False
-        [16, 4, 16, None, False, "reflect", False, True, True],  # reutrn_complex True
+    @pytest.mark.parametrize(("n_fft", "hop_length", "win_length", "center", "pad_mode", "normalized", "onesided", "return_complex"), [
+        [16, 4, 16, False, "reflect", False, True, False],  # default window
+        [16, 4, 14, True, "reflect", False, True, False],  # center True
+        [16, 4, 14, True, "reflect", False, True, False],  # center True
+        [16, 4, 14, True, "replicate", False, True, False],  # center True
+        [16, 4, 14, False, "replicate", False, True, False],  # center False
+        [16, None, 16, False, "reflect", False, True, False],  # hop_length None
+        [16, None, None, False, "reflect", False, True, False],  # hop & win length None
+        [16, 4, None, False, "reflect", False, True, False],  # win_length None
+        # Unsupported cases:
+        [16, 4, 16, False, "reflect", True, True, False],  # normalized True
+        [16, 4, 16, False, "reflect", False, False, False],  # onesided False
+        [16, 4, 16, False, "reflect", False, True, True],  # reutrn_complex True
     ])
-    def test_stft_not_supported_attrs(self, n_fft, hop_length, win_length, default_window, center, pad_mode, normalized, onesided, return_complex, ie_device, precision, ir_version, trace_model):
+    def test_stft_not_supported_attrs(self, n_fft, hop_length, win_length, center, pad_mode, normalized, onesided, return_complex, ie_device, precision, ir_version, trace_model):
         if ie_device == "GPU":
             pytest.xfail(reason="STFT op is not supported on GPU yet")
 
@@ -140,5 +140,5 @@ class TestSTFTAttrs(PytorchLayerTest):
             pytest.xfail(
                 reason="aten::stft conversion is currently supported with return_complex=False only")
 
-        self._test(*self.create_model_with_attrs(n_fft, hop_length, win_length, default_window, center, pad_mode, normalized, onesided, return_complex), ie_device, precision,
-                   ir_version, kwargs_to_prepare_input={"win_length": win_length, "default_window": default_window}, trace_model=trace_model)
+        self._test(*self.create_model_with_attrs(n_fft, hop_length, win_length, center, pad_mode, normalized, onesided, return_complex), ie_device, precision,
+                   ir_version, kwargs_to_prepare_input={}, trace_model=trace_model)
