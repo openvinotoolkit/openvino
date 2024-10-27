e90de407f50e1e4923619c087747e146a55cf2cd
Roman Kazantsev <roman.kazantsev@intel.com>
2024-10-27 19:09:49 +0400

    Update src/frontends/jax/src/op/erfc.cpp

--- src/frontends/jax/src/op/erfc.cpp
+++ src/frontends/jax/src/op/erfc.cpp
@@ -20,7 +20,7 @@ OutputVector translate_erfc(const NodeContext& context) {
     num_inputs_check(context, 1, 1);
     auto x = context.get_input(0);
 
-    // create y const of the same type as x
+    // create const one of the same type as x
     auto const_one = create_same_type_const_scalar<int64_t>(x, 1);
     Output<Node> res = make_shared<v0::Erf>(x);
     res = make_shared<v1::Subtract>(const_one, res);
