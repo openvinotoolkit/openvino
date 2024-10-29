openvinotoolkit/openvino

commit 6af61f4d2f84c71cbc57b9306012339e99445012

Exclude JAX FE from all distributions except wheel (#26610)
### Details:
 - Exclude JAX FE from all distributions except wheel

### Tickets:
 - CVS-152012

---------

Co-authored-by: Andrei Kochin <andrei.kochin@intel.com>

cmake/developer_package/frontends/frontends.cmake
@@ -104,13 +104,14 @@ endmacro()
 #                 FILEDESCRIPTION <description> # used on Windows to describe DLL file
 #                 [LINKABLE_FRONTEND] # whether we can use FE API directly or via FEM only
 #                 [SKIP_INSTALL] # private frontend, not for end users
+#                 [DISABLE_CPP_INSTALL] # excludes frontend from all cmake rules
 #                 [PROTOBUF_REQUIRED] # options to denote that protobuf is used
 #                 [PROTOBUF_LITE] # requires only libprotobuf-lite
 #                 [SKIP_NCC_STYLE] # use custom NCC rules
 #                 [LINK_LIBRARIES <lib1 lib2 ...>])
 #
 macro(ov_add_frontend)
-    set(options LINKABLE_FRONTEND PROTOBUF_REQUIRED PROTOBUF_LITE SKIP_NCC_STYLE SKIP_INSTALL)
+    set(options LINKABLE_FRONTEND PROTOBUF_REQUIRED PROTOBUF_LITE SKIP_NCC_STYLE SKIP_INSTALL DISABLE_CPP_INSTALL)
     set(oneValueArgs NAME FILEDESCRIPTION)
     set(multiValueArgs LINK_LIBRARIES PROTO_FILES)
     cmake_parse_arguments(OV_FRONTEND "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
@@ -306,6 +307,11 @@ macro(ov_add_frontend)
     # installation
 
     if(NOT OV_FRONTEND_SKIP_INSTALL)
+        # convert option (OFF / ON) to actual value
+        if(OV_FRONTEND_DISABLE_CPP_INSTALL)
+            set(frontend_exclude_from_all EXCLUDE_FROM_ALL)
+        endif()
+
         if(BUILD_SHARED_LIBS)
             # Note:
             # we use 'framework' as component for deployment scenario, i.e. for libraries itself
@@ -313,10 +319,9 @@ macro(ov_add_frontend)
             set(lib_component "${OV_FRONTEND_NAME}")
             set(dev_component "${OV_CPACK_COMP_CORE_DEV}")
 
-            # TODO: whether we need to do it configuralbe on Windows installer?
             ov_cpack_add_component(${lib_component} HIDDEN)
 
-            if(OV_FRONTEND_LINKABLE_FRONTEND)
+            if(OV_FRONTEND_LINKABLE_FRONTEND AND NOT OV_FRONTEND_DISABLE_CPP_INSTALL)
                 set(export_set EXPORT OpenVINOTargets)
                 set(archive_dest ARCHIVE DESTINATION ${OV_CPACK_ARCHIVEDIR} COMPONENT ${dev_component} ${OV_CPACK_COMP_CORE_DEV_EXCLUDE_ALL})
                 set(namelink NAMELINK_COMPONENT ${OV_CPACK_COMP_LINKS} ${OV_CPACK_COMP_LINKS_EXCLUDE_ALL})
@@ -324,12 +329,13 @@ macro(ov_add_frontend)
                 set(namelink NAMELINK_SKIP)
             endif()
             install(TARGETS ${TARGET_NAME} ${export_set}
-                    RUNTIME DESTINATION ${OV_CPACK_RUNTIMEDIR} COMPONENT ${lib_component}
+                    RUNTIME DESTINATION ${OV_CPACK_RUNTIMEDIR} COMPONENT ${lib_component} ${frontend_exclude_from_all}
                     ${archive_dest}
-                    LIBRARY DESTINATION ${OV_CPACK_LIBRARYDIR} COMPONENT ${lib_component}
+                    LIBRARY DESTINATION ${OV_CPACK_LIBRARYDIR} COMPONENT ${lib_component} ${frontend_exclude_from_all}
                     ${namelink})
 
             # export to build tree
+            # Note: we keep this even with passed DISABLE_CPP_INSTALL to ensure that Python API can be built
             if(OV_FRONTEND_LINKABLE_FRONTEND)
                 export(TARGETS ${TARGET_NAME} NAMESPACE openvino::
                        APPEND FILE "${CMAKE_BINARY_DIR}/OpenVINOTargets.cmake")
@@ -338,7 +344,7 @@ macro(ov_add_frontend)
             ov_install_static_lib(${TARGET_NAME} ${OV_CPACK_COMP_CORE})
         endif()
 
-        if(OV_FRONTEND_LINKABLE_FRONTEND)
+        if(OV_FRONTEND_LINKABLE_FRONTEND AND NOT OV_FRONTEND_DISABLE_CPP_INSTALL)
             # install library development files
             install(DIRECTORY ${${TARGET_NAME}_INCLUDE_DIR}/openvino
                     DESTINATION ${FRONTEND_INSTALL_INCLUDE}

.github/workflows/fedora_29.yml
@@ -147,7 +147,7 @@ jobs:
           python3 -c 'from openvino import Core; Core().get_property("MULTI", "SUPPORTED_PROPERTIES")'
           python3 -c 'from openvino import Core; Core().get_property("HETERO", "SUPPORTED_PROPERTIES")'
           python3 -c 'from openvino import Core; Core().get_property("BATCH", "SUPPORTED_PROPERTIES")'
-          python3 -c 'from openvino.frontend import FrontEndManager; assert len(FrontEndManager().get_available_front_ends()) == 7'
+          python3 -c 'from openvino.frontend import FrontEndManager; assert len(FrontEndManager().get_available_front_ends()) == 6'
           benchmark_app --help
           opt_in_out --help
           ovc --help

.github/workflows/job_debian_packages.yml
@@ -93,7 +93,7 @@ jobs:
           python3 -c 'from openvino import Core; Core().get_property("MULTI", "SUPPORTED_PROPERTIES")'
           python3 -c 'from openvino import Core; Core().get_property("HETERO", "SUPPORTED_PROPERTIES")'
           python3 -c 'from openvino import Core; Core().get_property("BATCH", "SUPPORTED_PROPERTIES")'
-          python3 -c 'from openvino.frontend import FrontEndManager; assert len(FrontEndManager().get_available_front_ends()) == 7'
+          python3 -c 'from openvino.frontend import FrontEndManager; assert len(FrontEndManager().get_available_front_ends()) == 6'
           benchmark_app --help
           opt_in_out --help
           ovc --help

cmake/packaging/archive.cmake
@@ -14,7 +14,9 @@ macro(ov_cpack_settings)
         # filter out some components, which are not needed to be wrapped to conda-forge | brew | conan | vcpkg
         if(NOT OV_CPACK_COMP_${UPPER_COMP}_EXCLUDE_ALL AND
            # python_package is not needed in case of archives, because components like pyopenvino are used, as well as wheels
-           NOT item MATCHES "^${OV_CPACK_COMP_PYTHON_OPENVINO_PACKAGE}_python.*")
+           NOT item MATCHES "^${OV_CPACK_COMP_PYTHON_OPENVINO_PACKAGE}_python.*" AND
+           # It was decided not to distribute JAX as C++ component
+           NOT item STREQUAL "jax")
             list(APPEND CPACK_COMPONENTS_ALL ${item})
         endif()
     endforeach()

cmake/packaging/common-libraries.cmake
@@ -16,7 +16,9 @@ macro(ov_cpack_settings)
            # because in case of VCPKG | CONAN | BREW | CONDA-FORGE distributions, python is either not needed or installed separately
            (NOT item MATCHES "^${OV_CPACK_COMP_PYTHON_OPENVINO_PACKAGE}_python.*" OR ENABLE_PYTHON_PACKAGING) AND
            # the same for pugixml
-           NOT item STREQUAL "pugixml")
+           NOT item STREQUAL "pugixml" AND
+           # It was decided not to distribute JAX as C++ component
+           NOT item STREQUAL "jax")
             list(APPEND CPACK_COMPONENTS_ALL ${item})
         endif()
     endforeach()

cmake/packaging/debian.cmake
@@ -58,7 +58,9 @@ macro(ov_cpack_settings)
            # don't install Intel OpenMP
            NOT item STREQUAL "omp" AND
            # the same for pugixml
-           NOT item STREQUAL "pugixml")
+           NOT item STREQUAL "pugixml" AND
+           # It was decided not to distribute JAX as C++ component
+           NOT item STREQUAL "jax")
             list(APPEND CPACK_COMPONENTS_ALL ${item})
         endif()
     endforeach()
@@ -236,7 +238,8 @@ macro(ov_cpack_settings)
         set(ir_copyright "generic")
     endif()
 
-    if(ENABLE_OV_JAX_FRONTEND)
+    # It was decided not to distribute JAX as C++ component
+    if(ENABLE_OV_JAX_FRONTEND AND OFF)
         set(CPACK_COMPONENT_JAX_DESCRIPTION "OpenVINO JAX Frontend")
         set(CPACK_COMPONENT_JAX_DEPENDS "${OV_CPACK_COMP_CORE}")
         set(CPACK_DEBIAN_JAX_PACKAGE_NAME "libopenvino-jax-frontend-${cpack_name_ver}")

cmake/packaging/npm.cmake
@@ -14,8 +14,10 @@ macro(ov_cpack_settings)
         string(TOUPPER ${item} UPPER_COMP)
         # filter out some components, which are not needed to be wrapped to npm package
         if(NOT OV_CPACK_COMP_${UPPER_COMP}_EXCLUDE_ALL AND
-            # python is not required for npm package
-            NOT item MATCHES "^${OV_CPACK_COMP_PYTHON_OPENVINO_PACKAGE}_python.*")
+           # python is not required for npm package
+           NOT item MATCHES "^${OV_CPACK_COMP_PYTHON_OPENVINO_PACKAGE}_python.*" AND
+           # It was decided not to distribute JAX as C++ component
+           NOT item STREQUAL "jax")
              list(APPEND CPACK_COMPONENTS_ALL ${item})
         endif()
     endforeach()

cmake/packaging/nsis.cmake
@@ -11,7 +11,9 @@ macro(ov_cpack_settings)
     foreach(item IN LISTS cpack_components_all)
         # filter out some components, which are not needed to be wrapped to Windows package
         if(# python wheels are not needed to be wrapped by NSIS installer
-           NOT item STREQUAL OV_CPACK_COMP_PYTHON_WHEELS)
+           NOT item STREQUAL OV_CPACK_COMP_PYTHON_WHEELS AND
+           # It was decided not to distribute JAX as C++ component
+           NOT item STREQUAL "jax")
             list(APPEND CPACK_COMPONENTS_ALL ${item})
         endif()
     endforeach()

commit 25a5767936e1c3678c8f2f581640da0eb323033e

[Wheel] return back JAX FE (#27309)
### Details:
- Regression after
https://github.com/openvinotoolkit/openvino/pull/26610

### Tickets:
 - CVS-156317

cmake/developer_package/frontends/frontends.cmake
@@ -304,6 +304,9 @@ macro(ov_add_frontend)
     # then we need to mark it to be CXX ABI free
     ov_abi_free_target(${TARGET_NAME})
 
+    # public target name
+    set_target_properties(${TARGET_NAME} PROPERTIES EXPORT_NAME frontend::${OV_FRONTEND_NAME})
+
     # installation
 
     if(NOT OV_FRONTEND_SKIP_INSTALL)
@@ -351,9 +354,6 @@ macro(ov_add_frontend)
                     COMPONENT ${dev_component}
                     ${OV_CPACK_COMP_CORE_DEV_EXCLUDE_ALL}
                     FILES_MATCHING PATTERN "*.hpp")
-
-            # public target name
-            set_target_properties(${TARGET_NAME} PROPERTIES EXPORT_NAME frontend::${OV_FRONTEND_NAME})
         endif()
     else()
         # skipped frontend has to be installed in static libraries case