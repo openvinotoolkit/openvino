65c43ea17414c05a7427c028db309a952379ee50
Karol Blaszczak <karol.blaszczak@intel.com>
2024-11-04 12:35:20 +0100

    [DOCS] unhide columns  fix mstr

--- docs/sphinx_setup/_static/js/openVinoDataTables.js
+++ docs/sphinx_setup/_static/js/openVinoDataTables.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
   var pageTitle = document.title;
   var columnDefs;
-  if (pageTitle.includes('Most Efficient Large Language Models for AI PC')) {
-    columnDefs = [
-      { "visible": false, "targets": [1, 2, 3, 4, 5] }
+  if(pageTitle.includes('Most Efficient Large Language Models for AI PC'))
+  {
+    columnDefs= [
+      { "visible": false, "targets": [1] }
     ]
   }
   else {
