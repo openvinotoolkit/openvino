4876eede7517a2a7f20b22dc132542820dcac90a
mitruska <katarzyna.mitrus@intel.com>
2024-11-05 20:51:39 +0100

    Add ref tests

--- src/plugins/template/tests/functional/op_reference/reduce_mean.cpp
+++ src/plugins/template/tests/functional/op_reference/reduce_mean.cpp
@@ -41,6 +41,16 @@ std::vector<ReductionParams> generateReductionParams(const bool keep_dims) {
                         reference_tests::Tensor(reduce(Shape{3, 2}, AxisSet{1}, keep_dims),
                                                 element::Type(IN_ET),
                                                 std::vector<T>{1.5, 3.5, 5.5}))};
+    if (keep_dims == false) {
+        params.push_back(ReductionParams(
+            ReductionType::Mean,
+            keep_dims,
+            std::vector<int64_t>{1, 2},
+            reference_tests::Tensor({2, 0, 4}, element::Type(IN_ET), std::vector<T>{1, 2, 3, 4, 5, 6, 7, 8}),
+            reference_tests::Tensor(Shape{2},
+                                    element::Type(IN_ET),
+                                    std::vector<T>{0, 0})));  // Keep dims false, tensor filled with zero
+    }
     return params;
 }
 
