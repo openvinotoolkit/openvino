c380cba34643ed32d8bc17a7d639098440437d13
Pavel Durandin <pavel.durandin@intel.com>
2024-11-05 22:19:05 +0400

    [GPU] clean oneDNN cache by release memory method (#27403)
    ### Details:
     - Partial fix for https://jira.devtools.intel.com/browse/CVS-156022
     - explicit call for release memory is required
    
    ### Tickets:
     - *ticket-id*

--- src/plugins/intel_gpu/include/intel_gpu/plugin/compiled_model.hpp
+++ src/plugins/intel_gpu/include/intel_gpu/plugin/compiled_model.hpp
@@ -61,6 +61,8 @@ public:
     const std::vector<std::shared_ptr<Graph>>& get_graphs() const;
     std::shared_ptr<Graph> get_graph(size_t n) const;
 
+    void release_memory() override;
+
 private:
     RemoteContextImpl::Ptr m_context;
     ExecutionConfig m_config;
