1b775d0ddc3460e80d2b8faaa0baa4c064cf6809
Roboreptile <piotr.krzeminski@intel.com>
2024-11-07 13:33:59 +0000

    [FIX] Move Identity top opset16

--- src/bindings/python/src/openvino/runtime/opset15/ops.py
+++ src/bindings/python/src/openvino/runtime/opset15/ops.py
@@ -251,24 +251,6 @@ def bitwise_left_shift(
     )
 
 
-@nameable_op
-def identity(
-    data: NodeInput,
-    name: Optional[str] = None,
-) -> Node:
-    """Identity operation is used as a placeholder. It creates a copy of the input to forward to the output.
-
-    :param data: Tensor with data.
-
-    :return: The new node performing BitwiseRightShift operation.
-    """
-    return _get_node_factory_opset15().create(
-        "Identity",
-        as_nodes(data, name=name),
-        {},
-    )
-
-
 @binary_op
 def bitwise_right_shift(
     arg0: NodeInput,
