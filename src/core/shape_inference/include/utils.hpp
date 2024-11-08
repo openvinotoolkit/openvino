4f9abba1d746115018a5bac5ca9ada51ed98ed45
Mateusz Mikolajczyk <mateusz.mikolajczyk@intel.com>
2024-11-08 14:48:32 +0100

    Fix issue with calling get_raw_data_as on empty tensor
    Signed-off-by: Mateusz Mikolajczyk <mateusz.mikolajczyk@intel.com>

--- src/core/shape_inference/include/utils.hpp
+++ src/core/shape_inference/include/utils.hpp
@@ -46,8 +46,12 @@ struct TensorTransform : element::NotSupported<void> {
  */
 template <class T, class TResult = std::vector<T>, class UnaryOperation>
 TResult get_raw_data_as(const element::Type_t et, const void* const ptr, const size_t size, UnaryOperation&& func) {
-    OPENVINO_ASSERT(!!ptr, "ptr is Null");
     TResult out;
+    if (!ptr && size == 0) {
+        // Tensor is empty
+        return out;
+    }
+    OPENVINO_ASSERT(!!ptr, "ptr is Null");
     auto out_it = std::inserter(out, out.end());
 
     using namespace ov::element;
