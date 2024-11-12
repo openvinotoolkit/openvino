151aace776503919eb7fbe68836f5b0cd33904b0
Ahn, Paul Y <paul.y.ahn@intel.com>
2024-11-12 17:24:02 +0900

    fix bad_fuction call issue

--- src/plugins/intel_gpu/src/graph/impls/ocl/primitive_base.hpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl/primitive_base.hpp
@@ -91,8 +91,10 @@ struct typed_primitive_impl_ocl : public typed_primitive_impl<PType> {
     static std::unique_ptr<primitive_impl> create(const typed_program_node<PType>& arg, const kernel_impl_params& impl_param) {
         // concat buffer fusing for dynamic shape is adaptively applied at runtime. So we need to build dynamic impl at build time.
         if (impl_param.can_be_optimized() &&
-            !((impl_param.runtime_skippable() || impl_param.is_type<crop>()) &&
-            impl_param.is_dynamic())) {
+            !((impl_param.is_type<concatenation>() ||
+               impl_param.is_type<strided_slice>() ||
+               impl_param.is_type<crop>() ||
+               impl_param.runtime_skippable()) && impl_param.is_dynamic())) {
             return make_unique<ImplType>(kernel_selector::kernel_data{});
         }
         auto kernel_params = ImplType::get_kernel_params(ImplType::static_canonicalize_shapes(impl_param));
