37b939fe10749905c1aad7ea5b83f86dc8ed3cda
Michal Barnas <michal.barnas@intel.com>
2024-11-14 09:04:23 +0000

    code foramt

--- src/core/shape_inference/include/squeeze_shape_inference.hpp
+++ src/core/shape_inference/include/squeeze_shape_inference.hpp
@@ -16,7 +16,7 @@ template <class T,
           class Lambda,
           class Squeeze,
           typename std::enable_if<std::is_same<Squeeze, ov::op::v0::Squeeze>::value ||
-                                                 std::is_same<Squeeze, ov::op::v15::Squeeze>::value>::type* = nullptr>
+                                  std::is_same<Squeeze, ov::op::v15::Squeeze>::value>::type* = nullptr>
 bool validate_input_and_try_set_output_shape(const Squeeze* op,
                                              std::unique_ptr<std::set<int64_t>>& unique_axes,
                                              const std::vector<T>& input_shapes,
