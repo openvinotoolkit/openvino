43dd13250b0d569a8b03e1cbad860f0fd2d7644c
Anastasiia Pnevskaia <anastasiia.pnevskaia@intel.com>
2024-11-14 16:38:44 +0100

    Fixed OVC app name in telemetry. (#27544)
    ### Details:
    - Fixed the issue, that '`Model Conversion API`' telemetry category
    includes `OVC` and all other tools, now `OVC` has a separate category
    "`OVC`".
    - Refactored `send_params_info()` method and fixed naming of telemetry
    init method.
    
    ---------
    
    Co-authored-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- tools/ovc/openvino/tools/ovc/__init__.py
+++ tools/ovc/openvino/tools/ovc/__init__.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from openvino.tools.ovc.convert import convert_model
-from openvino.tools.ovc.telemetry_utils import is_optimum, init_mo_telemetry
+from openvino.tools.ovc.telemetry_utils import is_optimum, init_ovc_telemetry
 
 import importlib.metadata as importlib_metadata
 
@@ -11,10 +11,10 @@ try:
 except importlib_metadata.PackageNotFoundError:
     optimum_version = None
 
+from openvino.runtime import get_version as get_rt_version  # pylint: disable=no-name-in-module,import-error
+telemetry = init_ovc_telemetry('OpenVINO')
+telemetry.send_event("ov", "import", "general_import")
+
 if is_optimum() and optimum_version is not None:
-    from openvino.runtime import get_version as get_rt_version  # pylint: disable=no-name-in-module,import-error
-    telemetry = init_mo_telemetry("Optimum Intel", optimum_version)
-    telemetry.send_event("ov", "import", "import_from_optimum,ov_version:{}".format(get_rt_version()))
-else:
-    telemetry = init_mo_telemetry()
-    telemetry.send_event("ov", "import", "general_import")
+    telemetry = init_ovc_telemetry("Optimum Intel", optimum_version)
+    telemetry.send_event("optimum", "import", "import_from_optimum,ov_version:{}".format(get_rt_version()))
--- tools/ovc/openvino/tools/ovc/ovc.py
+++ tools/ovc/openvino/tools/ovc/ovc.py
@@ -6,8 +6,8 @@
 import sys
 
 if __name__ == "__main__":
-    from openvino.tools.ovc.telemetry_utils import init_mo_telemetry
+    from openvino.tools.ovc.telemetry_utils import init_ovc_telemetry
     from openvino.tools.ovc.main import main
 
-    init_mo_telemetry()
+    init_ovc_telemetry()
     sys.exit(main())
