bd9272143e343d83095f0ce2c31d5565bacebd7b
Michal Barnas <michal.barnas@intel.com>
2024-11-14 09:07:38 +0000

    avoid code duplication, use function: set_output_shape

--- src/core/shape_inference/include/squeeze_shape_inference.hpp
+++ src/core/shape_inference/include/squeeze_shape_inference.hpp
@@ -67,43 +67,16 @@ bool validate_input_and_try_set_output_shape(const Squeeze* op,
 
     return false;
 }
-}  // namespace util
-namespace v0 {
-/**
- * \brief Do Squeeze shape inference.
- *
- * \tparam T             Type of input/output shapes.
- *
- * \param op             Squeeze operator pointer.
- * \param input_shapes   Squeeze input shapes.
- * \param ta             Tensor accessor to constant data.
- */
+
 template <class T, class TRShape = result_shape_t<T>>
-std::vector<TRShape> shape_infer(const Squeeze* op,
-                                 const std::vector<T>& input_shapes,
-                                 const ITensorAccessor& ta = make_tensor_accessor()) {
+void set_output_shape(const std::unique_ptr<std::set<int64_t>>& unique_axes,
+                      const T& arg_shape,
+                      TRShape& output_shape) {
     using DimType = typename T::value_type;
 
-    const auto& arg_shape = input_shapes[0];
     const auto& arg_rank = arg_shape.rank();
-    auto output_shapes = std::vector<TRShape>(1);
-    auto& output_shape = output_shapes[0];
-    std::unique_ptr<std::set<int64_t>> unique_axes{};
 
-    auto output_shape_for_squeezable_dim = [&]() {
-        return PartialShape::dynamic(arg_rank.get_length() - 1);
-    };
-
-    if (ov::op::util::validate_input_and_try_set_output_shape(op,
-                                                      unique_axes,
-                                                      input_shapes,
-                                                      ta,
-                                                      output_shape,
-                                                      output_shape_for_squeezable_dim)) {
-        return output_shapes;
-    }
-
-    if (arg_rank.is_static() && (unique_axes != nullptr)) {
+    if (arg_rank.is_static() && !!unique_axes) {
         output_shape.resize(0);
         if (unique_axes->empty()) {
             // if only first input provided or axes are empty remove all dimensions equal to 1.
@@ -112,7 +85,6 @@ std::vector<TRShape> shape_infer(const Squeeze* op,
                 })) {
                 // we are unsure if dynamic dimensions would be equal to 1 or not, so we set dynamic output rank
                 output_shape = PartialShape::dynamic();
-                return output_shapes;
             } else {
                 std::copy_if(arg_shape.cbegin(),
                              arg_shape.cend(),
@@ -145,6 +117,43 @@ std::vector<TRShape> shape_infer(const Squeeze* op,
     } else {
         output_shape = PartialShape::dynamic();
     }
+}
+}  // namespace util
+namespace v0 {
+/**
+ * \brief Do Squeeze shape inference.
+ *
+ * \tparam T             Type of input/output shapes.
+ *
+ * \param op             Squeeze operator pointer.
+ * \param input_shapes   Squeeze input shapes.
+ * \param ta             Tensor accessor to constant data.
+ */
+template <class T, class TRShape = result_shape_t<T>>
+std::vector<TRShape> shape_infer(const Squeeze* op,
+                                 const std::vector<T>& input_shapes,
+                                 const ITensorAccessor& ta = make_tensor_accessor()) {
+    const auto& arg_shape = input_shapes[0];
+    const auto& arg_rank = arg_shape.rank();
+    auto output_shapes = std::vector<TRShape>(1);
+    auto& output_shape = output_shapes[0];
+    std::unique_ptr<std::set<int64_t>> unique_axes{};
+
+    auto output_shape_for_squeezable_dim = [&]() {
+        return PartialShape::dynamic(arg_rank.get_length() - 1);
+    };
+
+    if (ov::op::util::validate_input_and_try_set_output_shape(op,
+                                                              unique_axes,
+                                                              input_shapes,
+                                                              ta,
+                                                              output_shape,
+                                                              output_shape_for_squeezable_dim)) {
+        return output_shapes;
+    }
+
+    ov::op::util::set_output_shape(unique_axes, arg_shape, output_shape);
+
     return output_shapes;
 }
 }  // namespace v0
@@ -178,8 +187,6 @@ template <class T, class TRShape = result_shape_t<T>>
 std::vector<TRShape> shape_infer(const Squeeze* op,
                                  const std::vector<T>& input_shapes,
                                  const ITensorAccessor& ta = make_tensor_accessor()) {
-    using DimType = typename T::value_type;
-
     const auto& arg_shape = input_shapes[0];
     const auto& arg_rank = arg_shape.rank();
     auto output_shapes = std::vector<TRShape>(1);
@@ -192,57 +199,18 @@ std::vector<TRShape> shape_infer(const Squeeze* op,
     };
 
     if (ov::op::util::validate_input_and_try_set_output_shape(op,
-                                                      unique_axes,
-                                                      input_shapes,
-                                                      ta,
-                                                      output_shape,
-                                                      output_shape_for_squeezable_dim)) {
+                                                              unique_axes,
+                                                              input_shapes,
+                                                              ta,
+                                                              output_shape,
+                                                              output_shape_for_squeezable_dim)) {
         return output_shapes;
     }
 
-    if (!arg_rank.is_static() || (unique_axes == nullptr) || apply_allow_axis_skip(op, unique_axes, arg_shape)) {
+    if (!arg_rank.is_static() || !unique_axes || apply_allow_axis_skip(op, unique_axes, arg_shape)) {
         output_shape = PartialShape::dynamic();
-    } else if (arg_rank.is_static() && (unique_axes != nullptr)) {
-        output_shape.resize(0);
-        if (unique_axes->empty()) {
-            // if only first input provided or axes are empty remove all dimensions equal to 1.
-            if (std::any_of(arg_shape.cbegin(), arg_shape.cend(), [](const DimType& d) {
-                    return d.is_dynamic() && d.compatible(1);
-                })) {
-                // we are unsure if dynamic dimensions would be equal to 1 or not, so we set dynamic output rank
-                output_shape = PartialShape::dynamic();
-                return output_shapes;
-            } else {
-                std::copy_if(arg_shape.cbegin(),
-                             arg_shape.cend(),
-                             std::back_inserter(output_shape),
-                             [](const DimType& dim) {
-                                 return !dim.compatible(1);
-                             });
-            }
-        } else {
-            int64_t idx = 0;
-            auto rm_axis_iter = unique_axes->cbegin();
-            auto rm_axis_end = unique_axes->cend();
-
-            // Returns true if dimension not squeezable on axis from input axes.
-            const auto not_squeezable_at_axis = [&rm_axis_iter, &rm_axis_end, &idx](const DimType& dim) {
-                if ((rm_axis_iter != rm_axis_end) && (*rm_axis_iter == idx++)) {
-                    ++rm_axis_iter;
-                    // Ignore: Pointed by axis, but not squeezable
-                    return !dim.compatible(1);
-                } else {
-                    return true;
-                }
-            };
-
-            std::copy_if(arg_shape.cbegin(),
-                         arg_shape.cend(),
-                         std::back_inserter(output_shape),
-                         not_squeezable_at_axis);
-        }
     } else {
-        output_shape = PartialShape::dynamic();
+        ov::op::util::set_output_shape(unique_axes, arg_shape, output_shape);
     }
     return output_shapes;
 }
