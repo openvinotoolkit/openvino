392ce25ff4909bf68f82c22b53fc504cddb82898
Piotr Kowalczyk <piotr.kowalczyk@intel.com>
2024-11-15 14:46:23 +0000

    [GPU]: SearchSorted: Fixed compilation warnings.

--- src/plugins/intel_gpu/src/kernel_selector/kernels/search_sorted/search_sorted_kernel_base.cpp
+++ src/plugins/intel_gpu/src/kernel_selector/kernels/search_sorted/search_sorted_kernel_base.cpp
@@ -18,7 +18,7 @@ JitConstants SearchSortedKernelBase::GetJitConstants(const search_sorted_params&
 }
 
 void SearchSortedKernelBase::GetUpdateDispatchDataFunc(KernelData& kd) const {
-    kd.update_dispatch_data_func = [this](const Params& params, KernelData& kd) {
+    kd.update_dispatch_data_func = [](const Params& params, KernelData& kd) {
         const auto& prim_params = static_cast<const search_sorted_params&>(params);
         auto dispatchData = SetDefault(prim_params);
         OPENVINO_ASSERT(kd.kernels.size() == 1, "[GPU] Invalid kernels size for update dispatch data func");
