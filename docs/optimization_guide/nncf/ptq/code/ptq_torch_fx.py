b891285ed06359178821f50b83d98bc9ead2ae5f
Daniil Lyakhov <daniil.lyakhov@intel.com>
2024-11-18 19:54:00 +0100

    Update docs/optimization_guide/nncf/ptq/code/ptq_torch_fx.py
    Co-authored-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- docs/optimization_guide/nncf/ptq/code/ptq_torch_fx.py
+++ docs/optimization_guide/nncf/ptq/code/ptq_torch_fx.py
@@ -44,7 +44,7 @@ with nncf.torch.disable_patching():
 # convert exported Torch model to OpenVINO model
 with nncf.torch.disable_patching():
     exported_quantized_model = torch.export.export(quantized_model, args=(input_fp32,))
-    ov_quantized_model = ov.convert_model(quantized_model, example_input=input_fp32)
+    ov_quantized_model = ov.convert_model(exported_quantized_model, example_input=input_fp32)
 
 # compile the model to transform quantized operations to int8
 model_int8 = ov.compile_model(ov_quantized_model)
