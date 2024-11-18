a5dff718e21f6eb304549fa9e8fac9778739f1bf
yuan.xiong <yuan.xiong@intel.com>
2024-11-18 23:52:57 +0800

    Check data pointer after dynamic_cast
    Signed-off-by: yuan.xiong <yuan.xiong@intel.com>

--- src/plugins/intel_gpu/src/graph/impls/ocl/primitive_base.hpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl/primitive_base.hpp
@@ -175,7 +175,9 @@ protected:
     static std::unique_ptr<primitive_impl> make_deep_copy(const ImplType& impl_ocl) {
         auto prim_impl = make_unique<ImplType>(impl_ocl);
         KernelParamsType* params_ptr = dynamic_cast<KernelParamsType*>((*prim_impl)._kernel_data.params.get());
-        (*prim_impl)._kernel_data.params = make_unique<KernelParamsType>(*params_ptr);
+        if (params_ptr != nullptr) {
+            (*prim_impl)._kernel_data.params = make_unique<KernelParamsType>(*params_ptr);
+        }
         return prim_impl;
     }
 
