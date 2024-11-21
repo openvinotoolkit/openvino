d161e74c5c135726ec462c9585aa917cb3fcbda6
PiotrKrzem <piotr.krzeminski@intel.com>
2024-11-21 15:12:26 +0000

    [ADD] Inverse CPU impl with tests

---
+++ src/plugins/intel_cpu/src/nodes/identity.cpp
@@ -0,0 +1,141 @@
+// Copyright (C) 2018-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "identity.hpp"
+
+#include "openvino/core/parallel.hpp"
+#include "nodes/common/cpu_memcpy.h"
+#include "openvino/op/constant.hpp"
+#include "openvino/op/identity.hpp"
+
+namespace ov {
+namespace intel_cpu {
+namespace node {
+
+bool Identity::isSupportedOperation(const std::shared_ptr<const ov::Node>& op, std::string& errorMessage) noexcept {
+    try {
+        if (op->get_type_info() != op::v16::Identity::get_type_info_static()) {
+            errorMessage = "Only Identity operation from the opset16 is supported by the CPU plugin.";
+            return false;
+        }
+    } catch (...) {
+        return false;
+    }
+    return true;
+}
+
+Identity::Identity(const std::shared_ptr<ov::Node>& op, const GraphContext::CPtr& context)
+        : Node(op, context, NgraphShapeInferFactory(op, PortMask(0, 1, 2))) {
+    std::string errorMessage;
+    if (!isSupportedOperation(op, errorMessage)) {
+        THROW_CPU_NODE_ERR(errorMessage);
+    }\
+
+    auto op = as_type_ptr<op::v16::Identity>(op);
+
+    if (is_type<op::v0::Constant>(op->get_input_node_ptr(0))) {
+        m_const_input = true;
+        constant = ConstantType::Const; // Node always produces the same output
+    } else {
+        m_const_input = false;
+        constant = ConstantType::NoConst; // Node produces output based on input
+    }
+}
+
+void Identity::getSupportedDescriptors() {
+    if (getParentEdges().size() != 1) {
+        THROW_CPU_NODE_ERR("has incorrect number of input edges.");
+    }
+    if (getChildEdges().empty()) {
+        THROW_CPU_NODE_ERR("has incorrect number of output edges.");
+    }
+}
+
+void Identity::initSupportedPrimitiveDescriptors() {
+    auto shape_prc = getOriginalInputPrecisionAtPort(0);
+    auto out_prc = getOriginalOutputPrecisionAtPort(0);
+
+    if (shape_prc != out_prc) {
+        THROW_CPU_NODE_ERR("has to have the same dtype for input and output nodes.")
+    }
+
+    m_out_prc = out_prc;
+
+    addSupportedPrimDesc({{LayoutType::ncsp, shape_prc, m_const_input}},
+                         {{LayoutType::ncsp, out_prc}},
+                         ref_any);
+}
+
+bool Identity::needPrepareParams() const {
+    if (m_out_shape != getDstMemoryAtPort(0)->getShape().getStaticDims()) {
+        return true;
+    }
+    return false;
+}
+
+void Identity::prepareParams() {
+    m_out_shape = getDstMemoryAtPort(0)->getShape().getStaticDims();
+}
+
+bool Identity::needShapeInfer() const {
+    return !m_const_input;
+}
+
+bool Identity::isExecutable() const {
+    return !isInputTensorAtPortEmpty(0);
+}
+
+bool Identity::created() const {
+    return getType() == Type::Identity;
+}
+
+bool Identity::canBeInPlace() const { 
+    return getSrcMemoryAtPort(0) == getDstMemoryAtPort(0); 
+}
+
+std::string Identity::getPrimitiveDescriptorType() const {
+    auto selectedPrimitiveDesc = getSelectedPrimitiveDescriptor();
+
+    impl_desc_type type = impl_desc_type::undef;
+    if (selectedPrimitiveDesc) {
+        type = selectedPrimitiveDesc->getImplementationType();
+    }
+
+    std::string str_type;
+    if (type == impl_desc_type::unknown)
+        str_type = "unknown";
+    else if (str_type.empty())
+        str_type = "undef";
+    else
+        str_type = "ref_any";
+
+    if (selectedPrimitiveDesc) {
+        if (selectedPrimitiveDesc->getConfig().outConfs[0].getMemDesc()->getPrecision() != ov::element::u8) {
+            str_type += "_" + std::string(selectedPrimitiveDesc->getConfig().outConfs[0].getMemDesc()->getPrecision().get_type_name());
+        } else {
+            str_type += "_I8";
+        }
+    }
+
+    return str_type;
+}
+
+void Identity::execute(dnnl::stream strm) {
+    const auto out_el_num = std::accumulate(m_out_shape.begin(), m_out_shape.end(), 1lu, std::multiplies<Dim>());
+
+    if (!canBeInPlace()) {
+        auto input = getSrcDataAtPort(0);
+        auto output = getDstDataAtPort(0);
+
+        cpu_parallel_memcpy(output, input, m_out_prc.size() * out_el_num);
+    }
+}
+
+void Identity::executeDynamicImpl(dnnl::stream strm) {
+    execute(strm);
+}
+
+}   // namespace node
+}   // namespace intel_cpu
+}   // namespace ov
---
+++ src/plugins/intel_cpu/tests/functional/custom/single_layer_tests/classes/identity.cpp
@@ -0,0 +1,79 @@
+// Copyright (C) 2018-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "single_layer_tests/classes/identity.hpp"
+#include "common_test_utils/node_builders/constant.hpp"
+
+using namespace CPUTestUtils;
+
+namespace ov {
+namespace test {
+
+std::string IdentityLayerTestCPU::getTestCaseName(const testing::TestParamInfo<IdentityLayerTestCPUParamSet>& obj) {
+    std::ostringstream result;
+
+    result << "IS="          << std::get<0>(obj.param);
+    result << "_Prc="        << std::get<1>(obj.param);
+    result << "_ConstIn="    << utils::bool2str(std::get<2>(obj.param));
+    result << CPUTestsBase::getTestCaseName(std::get<3>(obj.param));
+
+    const auto& config = std::get<4>(obj.param);
+    if (!config.empty()) {
+        result << "_PluginConf={";
+        for (const auto& conf_item : config) {
+            result << "_" << conf_item.first << "=";
+            conf_item.second.print(result);
+        }
+        result << "}";
+    }
+
+    return result.str();
+}
+
+void IdentityLayerTestCPU::SetUp() {
+    targetDevice = utils::DEVICE_CPU;
+
+    const auto& params             = this->GetParam();
+    const auto& output_shape       = std::get<0>(params);
+    const auto& output_precision   = std::get<1>(params);
+    const auto& const_input        = std::get<2>(params);
+    const auto& cpu_params         = std::get<3>(params);
+    const auto& configuration      = std::get<4>(params);
+
+    std::tie(inFmts, outFmts, priority, selectedType) = cpu_params;
+
+    updateSelectedType("ref_any", m_output_precision, configuration);
+
+    std::vector<InputShape> in_shapes;
+
+    if (!const_input) {
+        in_shapes.push_back({{}, {{m_output_shape}}});
+    } else {
+        in_shapes.push_back({{m_output_shape}, {{m_output_shape}}});
+    }
+    init_input_shapes(in_shapes);
+
+
+    const auto data = std::make_shared<ov::op::v0::Parameter>(m_output_precision, m_output_shape);
+    data->set_friendly_name("data");
+
+    const auto op = std::make_shared<ov::op::v16::Identity>(data);
+    const ov::ResultVector results{std::make_shared<ov::op::v0::Result>(op)};
+
+    function = std::make_shared<ov::Model>(results, ParameterVector{data}, "IdentityLayerTestCPU");
+}
+
+void IdentityLayerTestCPU::generate_inputs(const std::vector<ov::Shape>& targetInputStaticShapes) {
+    inputs.clear();
+    auto tensor = ov::test::utils::create_and_fill_tensor(function->inputs()[0].get_element_type(), targetInputStaticShapes[0], InputGenerateData());
+    inputs.insert({func_input.get_node_shared_ptr(), tensor});
+}
+
+TEST_P(IdentityLayerTestCPU, CompareWithRefs) {
+    run();
+    CheckPluginRelatedResults(compiledModel, "Identity");
+}
+
+}  // namespace test
+}  // namespace ov
