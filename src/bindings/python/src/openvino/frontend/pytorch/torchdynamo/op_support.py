c88e542fc97279015649b0510ed9f527e1d72106
Daniil Lyakhov <daniil.lyakhov@intel.com>
2024-11-25 13:07:31 -0800

    [Frontends][TorchFX] torch.ops.aten._unsafe_index support (#27617)
    ### Details:
     - torch.ops.aten._unsafe_index is supported by the Torch frontend
     - To support Yolo(V8 and V11) TorchFX models quantization with NNCF
    
    ### Tickets:
    - 141640
    
    ---------
    
    Co-authored-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- src/bindings/python/src/openvino/frontend/pytorch/torchdynamo/op_support.py
+++ src/bindings/python/src/openvino/frontend/pytorch/torchdynamo/op_support.py
@@ -145,6 +145,7 @@ class OperatorSupport(OperatorSupport):
             "torch.ops.aten.hardtanh.default": None,
             "torch.ops.aten.hardtanh_.default": None,
             "torch.ops.aten.index.Tensor": None,
+            "torch.ops.aten._unsafe_index.Tensor": None,
             "torch.ops.aten.index_select.default": None,
             "torch.ops.aten.isfinite.default": None,
             "torch.ops.aten.isinf.default": None,
