a5647bd2e2b6e78e11ee9b719052dd7f4fdba847
almilosz <alicja.miloszewska@intel.com>
2024-11-25 16:41:30 +0000

    Remove _pyopenvino import

--- src/bindings/python/src/openvino/test_utils/test_api.py
+++ src/bindings/python/src/openvino/test_utils/test_api.py
@@ -2,8 +2,9 @@
 # Copyright (C) 2018-2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from .test_utils_api import compare_functions as compare_func
-from openvino._pyopenvino import Model
+from .test_utils_api import compare_functions as compare_funcions_base
+from openvino.runtime import Model
+
 
 def compare_functions(lhs: Model, rhs: Model) -> tuple:
-    return compare_func(lhs._Model__model, rhs._Model__model)
+    return compare_funcions_base(lhs._Model__model, rhs._Model__model)
