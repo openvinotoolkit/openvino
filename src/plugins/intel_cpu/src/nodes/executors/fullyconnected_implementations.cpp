459519b10962d6544398222b4196c874ec81aa7f
Aleksandr Voron <aleksandr.voron@intel.com>
2024-11-26 15:27:21 +0100

    update aclLowpFCTypeMapping

--- src/plugins/intel_cpu/src/nodes/executors/fullyconnected_implementations.cpp
+++ src/plugins/intel_cpu/src/nodes/executors/fullyconnected_implementations.cpp
@@ -90,7 +90,7 @@ static const TypeMapping aclFCTypeMapping {
 
 static const TypeMapping aclLowpFCTypeMapping {
     // {src, wei, bia, dst}                  pt<src, wei, bias, dst>
-    {{_i8, _i8, _any, _f32},                 pt(bypass(), bypass(), just<f32>(), bypass())}
+    {{_i8, _i8, _any, _f32},                 pt(bypass(), bypass(), use<3>(), bypass())}
 };
 
 static const MappingNotation dnnlConvolutionMappingNotation {
