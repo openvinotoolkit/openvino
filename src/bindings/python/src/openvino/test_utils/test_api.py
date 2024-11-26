008be376e51c0c4744ec7fc98601cd874076a941
almilosz <alicja.miloszewska@intel.com>
2024-11-26 13:28:21 +0000

    Add compare_tensor_names

--- src/bindings/python/src/openvino/test_utils/test_api.py
+++ src/bindings/python/src/openvino/test_utils/test_api.py
@@ -2,9 +2,9 @@
 # Copyright (C) 2018-2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from .test_utils_api import compare_functions as compare_funcions_base
+from .test_utils_api import compare_functions as compare_functions_base
 from openvino.runtime import Model
 
 
-def compare_functions(lhs: Model, rhs: Model) -> tuple:
-    return compare_funcions_base(lhs._Model__model, rhs._Model__model)
+def compare_functions(lhs: Model, rhs: Model, compare_tensor_names: bool = True) -> tuple:
+    return compare_functions_base(lhs._Model__model, rhs._Model__model, compare_tensor_names)
