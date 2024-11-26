7b3ed0e44bd6d9a53fe9a9c395f2941fa9e5d708
almilosz <alicja.miloszewska@intel.com>
2024-11-26 11:36:11 +0000

    Switch to relative import for ovc

--- src/bindings/python/src/openvino/test_utils/__init__.py
+++ src/bindings/python/src/openvino/test_utils/__init__.py
@@ -2,4 +2,4 @@
 # Copyright (C) 2018-2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from openvino.test_utils.test_api import compare_functions
+from .test_api import compare_functions
