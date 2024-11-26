f54730f97f905f09b2f8eef73726cc679f9fe993
Piotr Kowalczyk <piotr.kowalczyk@intel.com>
2024-11-26 09:23:35 +0000

    [GPU]:SearchSorted: Fixed naming.

--- src/plugins/intel_gpu/src/plugin/ops/search_sorted.cpp
+++ src/plugins/intel_gpu/src/plugin/ops/search_sorted.cpp
@@ -14,9 +14,9 @@ namespace intel_gpu {
 static void CreateSearchSortedOp(ProgramBuilder& p, const std::shared_ptr<ov::op::v15::SearchSorted>& op) {
     validate_inputs_count(op, {2});
     auto inputs = p.GetInputInfo(op);
-    auto roi_align_prim = cldnn::search_sorted(layer_type_name_ID(op), inputs[0], inputs[1], op->get_right_mode());
-    roi_align_prim.output_data_types = get_output_data_types(op, {{ov::element::i64, ov::element::i32}});
-    p.add_primitive(*op, roi_align_prim);
+    auto prim = cldnn::search_sorted(layer_type_name_ID(op), inputs[0], inputs[1], op->get_right_mode());
+    prim.output_data_types = get_output_data_types(op, {{ov::element::i64, ov::element::i32}});
+    p.add_primitive(*op, prim);
 }
 
 REGISTER_FACTORY_IMPL(v15, SearchSorted);
