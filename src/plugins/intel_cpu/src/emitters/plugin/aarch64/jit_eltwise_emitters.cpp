94819d10786af2eaaf68993709216daffef40060
geeky33 <aaryap1204@gmail.com>
2024-11-28 16:13:19 +0530

    All the test cases have passed and removed the aux register

--- src/plugins/intel_cpu/src/emitters/plugin/aarch64/jit_eltwise_emitters.cpp
+++ src/plugins/intel_cpu/src/emitters/plugin/aarch64/jit_eltwise_emitters.cpp
@@ -529,8 +529,6 @@ jit_floor_mod_emitter::jit_floor_mod_emitter(dnnl::impl::cpu::aarch64::jit_gener
 
 size_t jit_floor_mod_emitter::get_inputs_count() const { return 2; }
 
-size_t jit_floor_mod_emitter::get_aux_vecs_count() const { return 1; }
-
 void jit_floor_mod_emitter::emit_impl(const std::vector<size_t> &in_vec_idxs, const std::vector<size_t> &out_vec_idxs) const {
     if (host_isa_ == dnnl::impl::cpu::aarch64::asimd) {
         emit_isa<dnnl::impl::cpu::aarch64::asimd>(in_vec_idxs, out_vec_idxs);
@@ -548,12 +546,11 @@ void jit_floor_mod_emitter::emit_isa(const std::vector<size_t> &in_vec_idxs, con
     TReg dividend = TReg(in_vec_idxs[0]);
     TReg divisor = TReg(in_vec_idxs[1]);
     TReg r = TReg(out_vec_idxs[0]);
-    TReg aux = TReg(aux_vec_idxs[0]);
 
-    h->fdiv(aux.s, dividend.s, divisor.s);
-    h->frintm(aux.s, aux.s);
-    h->fmul(aux.s, aux.s, divisor.s);
-    h->fsub(r.s, dividend.s, aux.s);
+    h->fdiv(r.s, dividend.s, divisor.s);
+    h->frintm(r.s, r.s);
+    h->fmul(r.s, r.s, divisor.s);
+    h->fsub(r.s, dividend.s, r.s);
 }
 
 std::set<std::vector<element::Type>> jit_floor_mod_emitter::get_supported_precisions(const std::shared_ptr<ov::Node>& node) {
