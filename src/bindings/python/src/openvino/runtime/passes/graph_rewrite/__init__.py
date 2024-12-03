89e118f96df478b8e533f296688196608ba52940
Alicja Miloszewska <alicja.miloszewska@intel.com>
2024-12-03 13:08:34 +0100

    [Py OV] Add runtime/passes to openvino namespace (#27785)
    ### Details:
    - copy _openvino/runtime/passes_ to _openvino/_
    - `passes` are not initialized in _openvino/runtime/__init__.py_ so no
    changes to _openvino/__init__.py_
    - update imports for Python API tests (_src/bindings/python/tests_) 
    - add possibility to import `passes` from `openvino` module
    ```python
    from openvino.passes import Manager
    
    manager = Manager()
    ```
    
    Part of `openvino.runtime` deprecation
    
    ### Tickets:
     - *ticket-id*

---
+++ src/bindings/python/src/openvino/runtime/passes/graph_rewrite/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018-2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+# type: ignore
+from openvino.passes.graph_rewrite import GraphRewrite, BackwardGraphRewrite
