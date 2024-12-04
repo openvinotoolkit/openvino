546897e8227686421bd556d4761a0d60fb884bc1
Michal Barnas <michal.barnas@intel.com>
2024-12-04 09:52:54 +0000

    std::fs::path alias

---
+++ src/common/util/include/openvino/util/file_path.hpp
@@ -0,0 +1,17 @@
+// Copyright (C) 2018-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <cstdio>
+
+#include "openvino/util/filesystem.hpp"
+namespace ov {
+namespace util {
+
+namespace fs = std_fs;
+using Path = fs::path;
+
+}  // namespace util
+}  // namespace ov
