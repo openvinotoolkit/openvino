2ef471a26bf1a8dc2b32314e404acf60f6476cea
Wang, Yang <yang4.wang@intel.com>
2024-12-04 15:46:04 +0800

    update.

--- src/plugins/auto/src/schedule.cpp
+++ src/plugins/auto/src/schedule.cpp
@@ -52,8 +52,8 @@ void Schedule::run(ov::threading::Task pipeline_task) {
 }
 
 bool Schedule::run_pipeline_task(ov::threading::Task& pipeline_task,
-                                 NotBusyPriorityWorkerRequests& idle_workerrequests,
-                                 const DeviceName& preferred_device) {
+    NotBusyPriorityWorkerRequests& idle_workerrequests,
+    const DeviceName& preferred_device) {
     WorkerInferRequest* worker_request_ptr = nullptr;
     std::pair<int, WorkerInferRequest*> worker;
     if (idle_workerrequests.try_pop(worker)) {
