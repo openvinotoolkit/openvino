11eda9d1f291b54538c977747d9294096329da6d
Kazantsev, Roman <roman.kazantsev@intel.com>
2024-12-07 23:40:49 +0400

    [TF FE] Stabilize tests for MaxPoolWithArgMax on all platforms
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- tests/layer_tests/tensorflow_tests/test_tf_MaxPoolWithArgmax.py
+++ tests/layer_tests/tensorflow_tests/test_tf_MaxPoolWithArgmax.py
@@ -1,24 +1,30 @@
 # Copyright (C) 2018-2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-import platform
-
 import numpy as np
 import pytest
 import tensorflow as tf
 from common.tf_layer_test_class import CommonTFLayerTest
 
+rng = np.random.default_rng(23534)
+
 
 class TestMaxPoolWithArgmax(CommonTFLayerTest):
     def _prepare_input(self, inputs_info):
-        assert 'input:0' in inputs_info
+        assert 'input:0' in inputs_info, "Test error: inputs_info must contain `input`"
         input_shape = inputs_info['input:0']
         inputs_data = {}
-        inputs_data['input:0'] = np.random.randint(-5, 5, input_shape).astype(self.input_type)
+        if np.issubdtype(self.input_type, np.floating):
+            inputs_data['input:0'] = rng.uniform(-5.0, 5.0, input_shape).astype(self.input_type)
+        elif np.issubdtype(self.input_type, np.signedinteger):
+            inputs_data['input:0'] = rng.integers(-5, 5, input_shape).astype(self.input_type)
+        else:
+            inputs_data['input:0'] = rng.integers(0, 5, input_shape).astype(self.input_type)
         return inputs_data
 
-    def create_max_pool_with_argmax_net(self, input_shape, ksize, strides, input_type, padding, targmax,
-                                        include_batch_in_index, with_second_output):
+    def create_max_pool_with_argmax_net(self, input_shape, ksize, strides,
+                                        input_type, padding, targmax,
+                                        include_batch_in_index):
         self.input_type = input_type
         tf.compat.v1.reset_default_graph()
         # Create the graph and model
@@ -29,49 +35,28 @@ class TestMaxPoolWithArgmax(CommonTFLayerTest):
                                                                 include_batch_in_index=include_batch_in_index
                                                                 )
             tf.identity(max_pool_with_argmax[0], name='max_pool')
-            if with_second_output:
-                tf.identity(max_pool_with_argmax[1], name='output_indices')
+            tf.identity(max_pool_with_argmax[1], name='output_indices')
             tf.compat.v1.global_variables_initializer()
             tf_net = sess.graph_def
-
         return tf_net, None
 
-    test_data_basic = [
-        dict(input_shape=[1, 25, 24, 3],
-             ksize=[1, 1, 1, 1], strides=[1, 1, 1, 1]),
-        dict(input_shape=[1, 10, 20, 3],
-             ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1]),
-    ]
-
-    @pytest.mark.parametrize("params", test_data_basic)
-    @pytest.mark.parametrize("input_type", [
-        np.float32, np.int32
-    ])
-    @pytest.mark.parametrize("padding", [
-        'VALID', 'SAME'
-    ])
-    @pytest.mark.parametrize("targmax", [
-        tf.int32, tf.int64
-    ])
-    @pytest.mark.parametrize("include_batch_in_index", [
-        True, False
-    ])
-    @pytest.mark.parametrize("with_second_output", [
-        True, False
-    ])
+    @pytest.mark.parametrize("input_shape", [[1, 25, 24, 3], [1, 10, 20, 3]])
+    @pytest.mark.parametrize("ksize", [[1, 1, 1, 1], [1, 2, 2, 1]])
+    @pytest.mark.parametrize("strides", [[1, 1, 1, 1], [1, 2, 2, 1]])
+    @pytest.mark.parametrize("input_type", [np.float32, np.int32])
+    @pytest.mark.parametrize("padding", ['VALID', 'SAME'])
+    @pytest.mark.parametrize("targmax", [tf.int32, tf.int64])
+    @pytest.mark.parametrize("include_batch_in_index", [True, False])
     @pytest.mark.precommit
     @pytest.mark.nightly
-    @pytest.mark.xfail(condition=platform.system() in ('Linux', 'Darwin') and platform.machine() in ('arm', 'armv7l',
-                                                                                                     'aarch64',
-                                                                                                     'arm64', 'ARM64'),
-                       reason='Ticket - 126314, 122716')
-    def test_max_pool_with_argmax_basic(self, params, input_type, padding, targmax,
-                                        include_batch_in_index, with_second_output,
+    def test_max_pool_with_argmax_basic(self, input_shape, ksize, strides,
+                                        input_type, padding, targmax,
+                                        include_batch_in_index,
                                         ie_device, precision, ir_version, temp_dir,
                                         use_legacy_frontend):
         self._test(
-            *self.create_max_pool_with_argmax_net(**params, input_type=input_type, padding=padding, targmax=targmax,
-                                                  include_batch_in_index=include_batch_in_index,
-                                                  with_second_output=with_second_output),
+            *self.create_max_pool_with_argmax_net(input_shape=input_shape, ksize=ksize, strides=strides,
+                                                  input_type=input_type, padding=padding, targmax=targmax,
+                                                  include_batch_in_index=include_batch_in_index),
             ie_device, precision, ir_version, temp_dir=temp_dir,
             use_legacy_frontend=use_legacy_frontend)
