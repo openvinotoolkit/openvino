4fda06d97526560493530e119f987ed278daf28f
Anastasia Kuporosova <anastasia.kuporosova@intel.com>
2024-12-10 13:35:30 +0000

    fix

--- src/bindings/python/src/openvino/frontend/__init__.py
+++ src/bindings/python/src/openvino/frontend/__init__.py
@@ -22,7 +22,7 @@ from openvino._pyopenvino import Place
 # extensions
 from openvino._pyopenvino import DecoderTransformationExtension
 from openvino._pyopenvino import ConversionExtension
-from openvino._pyopenvino import OpExtension
+from openvino._pyopenvino.frontend import OpExtension
 from openvino._pyopenvino import ProgressReporterExtension
 from openvino._pyopenvino import TelemetryExtension
 
