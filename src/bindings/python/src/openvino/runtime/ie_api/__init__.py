0551bcf9a608443a64c897c31890936ad7c6f579
Alicja Miloszewska <alicja.miloszewska@intel.com>
2024-12-10 12:28:47 +0100

    Move and rename ie_api.py
    Signed-off-by: Alicja Miloszewska <alicja.miloszewska@intel.com>

---
+++ src/bindings/python/src/openvino/runtime/ie_api/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018-2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+from openvino._ov_api import Tensor
+from openvino._ov_api import Node
+from openvino._ov_api import OVDict
+from openvino._ov_api import tensor_from_file
+
+from openvino._ov_api import Core
+from openvino._ov_api import CompiledModel
+from openvino._ov_api import InferRequest
+from openvino._ov_api import Model
+from openvino._ov_api import AsyncInferQueue
+
+from openvino._ov_api import tensor_from_file
+from openvino._ov_api import compile_model
