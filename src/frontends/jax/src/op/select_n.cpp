a2f58f3dee5fd9fe3fb462a8b97bde2c9f7e000d
11happy <soni5happy@gmail.com>
2024-12-12 00:02:43 +0530

    Implemented jax.lax.select_n
    Signed-off-by: 11happy <soni5happy@gmail.com>

---
+++ src/frontends/jax/src/op/select_n.cpp
@@ -0,0 +1,36 @@
+#include "openvino/frontend/jax/node_context.hpp";
+#include "openvino/op/ops.hpp";
+#include "utils.hpp";
+
+using namespace ov::op;
+
+namespace ov {
+namespace frontend {
+namespace jax {
+namespace op {
+
+OutputVector translate_select_n(const NodeContext& context) {
+    num_inputs_check(context, 2);
+    auto num_inputs = static_cast<int>(context.get_input_size());
+    Output<Node> which = context.get_input(0);
+    OutputVector cases_vector(num_inputs - 1);
+    for(int ind = 1; ind < num_inputs; ++ind) {
+        cases_vector[ind - 1] = context.get_input(ind);
+    }
+    
+    Output<Node> cases = std::make_shared<v0::Concat>(cases_vector, 0);
+    auto which_shape = which.get_shape();
+    std::vector<int64_t> cases_reshape_shape = {num_inputs-1,which_shape[0]};
+    std::vector<int64_t> which_reshape_shape = {1,which_shape[0]};
+    
+    cases = std::make_shared<v1::Reshape>(cases, ov::op::v0::Constant::create(element::i64, Shape{2}, cases_reshape_shape), false);
+    which = std::make_shared<v1::Reshape>(which, ov::op::v0::Constant::create(element::i64, Shape{2}, which_reshape_shape), false);
+    Output<Node> result = std::make_shared<v6::GatherElements>(cases, which, 0);
+    return {result};
+
+};
+
+}  // namespace op
+}  // namespace jax
+}  // namespace frontend
+}  // namespace ov
--- src/frontends/jax/src/op_table.cpp
+++ src/frontends/jax/src/op_table.cpp
@@ -52,6 +52,7 @@ OP_CONVERTER(translate_reduce_window_max);
 OP_CONVERTER(translate_reduce_window_sum);
 OP_CONVERTER(translate_reshape);
 OP_CONVERTER(translate_rsqrt);
+OP_CONVERTER(translate_select_n);
 OP_CONVERTER(translate_slice);
 OP_CONVERTER(translate_squeeze);
 OP_CONVERTER(translate_transpose);
@@ -91,6 +92,7 @@ const std::map<std::string, CreatorFunction> get_supported_ops_jaxpr() {
             {"transpose", op::translate_transpose},
             {"rsqrt", op::translate_rsqrt},
             {"reshape", op::translate_reshape},
+            {"select_n", op::translate_select_n},
             {"slice", op::translate_slice},
             {"sqrt", op::translate_1to1_match_1_input<v0::Sqrt>},
             {"squeeze", op::translate_squeeze},
