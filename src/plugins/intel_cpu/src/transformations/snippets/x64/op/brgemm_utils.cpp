amanmogal/amx

f16_mha_on_avx512_core_amx_f16_target

src/plugins/intel_cpu/src/transformations/snippets/x64/op/brgemm_utils.cpp
@@ -27,7 +27,10 @@ cpu_isa_t get_primitive_isa(const ov::element::Type& dt_in0, bool is_with_amx) {
 
     // Note: AMX might be not used even if it's supported by the hardware, check the BrgemmToBrgemmCPU pass for details
     if (is_with_amx) {
-        SUPPORT_ONE(avx512_core_amx, "Unsupported hardware configuration: amx is supported only on avx512 platforms")
+        if (dt_in0 == ov::element::f16)
+            SUPPORT_ONE(avx512_core_amx_fp16, "Unsupported hardware configuration: amx is supported only on avx512 platforms")
+        else
+            SUPPORT_ONE(avx512_core_amx, "Unsupported hardware configuration: amx is supported only on avx512 platforms")
     } else if (dt_in0 == ov::element::bf16) {
         SUPPORT_ONE(avx512_core_bf16, "Unsupported hardware configuration: bf16 is supported only on avx512 platforms")
     } else if (one_of(dt_in0, ov::element::u8, ov::element::i8)) {
@@ -53,6 +56,10 @@ BRGEMM_TYPE get_brgemm_type(const ov::element::Type& element_type_a, const Dimen
         dnnl::impl::cpu::x64::mayiuse(dnnl::impl::cpu::x64::avx512_core_amx) &&
         K_dim.is_static() && K_dim.get_length() % brgemmVNNIFactor == 0)
         return BRGEMM_TYPE::WITH_AMX;
+    if (element_type_a == ov::element::f16 &&
+        dnnl::impl::cpu::x64::mayiuse(dnnl::impl::cpu::x64::avx512_core_amx_fp16) &&
+        K_dim.is_static() && K_dim.get_length() % brgemmVNNIFactor == 0)
+        return BRGEMM_TYPE::WITH_AMX_F16;
     // Note: this condition reproduces logic from the OneDNN Brgemm implementation. This is needed to align with the
     // backend requirements. More details in onednn/src/cpu/x64/brgemm/brgemm_utils.cpp
     if (element_type_a == ov::element::i8)
@@ -79,6 +86,7 @@ size_t compute_inner_n_block(const ov::element::Type& precision) {
     switch (precision) {
         case element::i8: return 64;
         case element::bf16: return 32;
+        case element::f16: return 32;
         case element::f32: return 16;
         default: OPENVINO_THROW("BrgemmCopyB doesn't support precision ", precision);
     }

openvinotoolkit/openvino

src/plugins/intel_cpu/src/transformations/snippets/x64/op/brgemm_utils.cpp
@@ -35,7 +35,10 @@ cpu_isa_t get_primitive_isa(const ov::element::Type& dt_in0, bool is_with_amx) {
 
     // Note: AMX might be not used even if it's supported by the hardware, check the BrgemmToBrgemmCPU pass for details
     if (is_with_amx) {
-        SUPPORT_ONE(avx512_core_amx, "Unsupported hardware configuration: amx is supported only on avx512 platforms")
+        if (dt_in0 == ov::element::f16)
+            SUPPORT_ONE(avx512_core_amx_fp16, "Unsupported hardware configuration: amx is supported only on avx512 platforms")
+        else
+            SUPPORT_ONE(avx512_core_amx, "Unsupported hardware configuration: amx is supported only on avx512 platforms")
     } else if (dt_in0 == ov::element::bf16) {
         SUPPORT_ONE(avx512_core_bf16, "Unsupported hardware configuration: bf16 is supported only on avx512 platforms")
     } else if (one_of(dt_in0, ov::element::u8, ov::element::i8)) {
@@ -64,7 +67,9 @@ BRGEMM_TYPE get_brgemm_type(const ov::element::Type& element_type_a, bool transp
     if (one_of(element_type_a, element::u8, element::i8, element::bf16) &&
         dnnl::impl::cpu::x64::mayiuse(dnnl::impl::cpu::x64::avx512_core_amx))
         return BRGEMM_TYPE::WITH_AMX;
-
+    if (element_type_a == ov::element::f16 &&
+        dnnl::impl::cpu::x64::mayiuse(dnnl::impl::cpu::x64::avx512_core_amx_fp16))
+        return BRGEMM_TYPE::WITH_AMX;
     // Note: this condition reproduces logic from the OneDNN Brgemm implementation. This is needed to align with the
     // backend requirements. More details in onednn/src/cpu/x64/brgemm/brgemm_utils.cpp
     if (element_type_a == ov::element::i8)
@@ -92,14 +97,11 @@ size_t get_elems_in_vec(const ov::element::Type& precision) {
 namespace repacking {
 size_t compute_inner_n_block(const ov::element::Type& precision) {
     switch (precision) {
-    case element::i8:
-        return 64;
-    case element::bf16:
-        return 32;
-    case element::f32:
-        return 16;
-    default:
-        OPENVINO_THROW("BrgemmCopyB doesn't support precision ", precision);
+        case element::i8: return 64;
+        case element::bf16: return 32;
+        case element::f16: return 32;
+        case element::f32: return 16;
+        default: OPENVINO_THROW("BrgemmCopyB doesn't support precision ", precision);
     }
 }
 