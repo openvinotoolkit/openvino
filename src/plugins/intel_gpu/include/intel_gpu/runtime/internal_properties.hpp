4df5050acf5e5eec77e5f0c9f458506b9b1a51b2
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2024-12-17 14:19:13 +0400

    make it common
    Signed-off-by: Vladimir Paramuzov <vladimir.paramuzov@intel.com>

--- src/plugins/intel_gpu/include/intel_gpu/runtime/internal_properties.hpp
+++ src/plugins/intel_gpu/include/intel_gpu/runtime/internal_properties.hpp
@@ -47,7 +47,6 @@ static constexpr Property<bool, PropertyMutability::RW> allow_static_input_reord
 static constexpr Property<bool, PropertyMutability::RW> partial_build_program{"GPU_PARTIAL_BUILD"};
 static constexpr Property<bool, PropertyMutability::RW> allow_new_shape_infer{"GPU_ALLOW_NEW_SHAPE_INFER"};
 static constexpr Property<bool, PropertyMutability::RW> use_only_static_kernels_for_dynamic_shape{"GPU_USE_ONLY_STATIC_KERNELS_FOR_DYNAMIC_SHAPE"};
-static constexpr Property<std::string, PropertyMutability::RW> dump_graphs{"GPU_DUMP_GRAPHS"};
 static constexpr Property<std::vector<std::string>, PropertyMutability::RW> custom_outputs{"GPU_CUSTOM_OUTPUTS"};
 static constexpr Property<ImplForcingMap, PropertyMutability::RW> force_implementations{"GPU_FORCE_IMPLEMENTATIONS"};
 static constexpr Property<std::string, PropertyMutability::RW> config_file{"CONFIG_FILE"};
@@ -57,6 +56,29 @@ static constexpr Property<bool, PropertyMutability::RW> nv12_two_inputs{"GPU_NV1
 static constexpr Property<float, PropertyMutability::RW> buffers_preallocation_ratio{"GPU_BUFFERS_PREALLOCATION_RATIO"};
 static constexpr Property<size_t, PropertyMutability::RW> max_kernels_per_batch{"GPU_MAX_KERNELS_PER_BATCH"};
 static constexpr Property<bool, PropertyMutability::RW> use_onednn{"USE_ONEDNN"};
+
+static constexpr Property<bool, ov::PropertyMutability::RW> help{"HELP"};
+static constexpr Property<size_t, ov::PropertyMutability::RW> verbose{"VERBOSE"};
+static constexpr Property<bool, ov::PropertyMutability::RW> disable_usm{"DISABLE_USM"};
+static constexpr Property<bool, ov::PropertyMutability::RW> disable_onednn_post_ops{"DISABLE_ONEDNN_POST_OPS"};
+static constexpr Property<std::string, PropertyMutability::RW> dump_graphs{"GPU_DUMP_GRAPHS"};
+static constexpr Property<std::string, ov::PropertyMutability::RW> dump_profiling_data{"DUMP_PROFILING_DATA"};
+static constexpr Property<std::string, ov::PropertyMutability::RW> dump_sources{"DUMP_SOURCES"};
+static constexpr Property<std::string, ov::PropertyMutability::RW> dump_tensors{"DUMP_TENSORS"};
+static constexpr Property<std::string, ov::PropertyMutability::RW> dump_memory_pool{"DUMP_MEMORY_POOL"};
+static constexpr Property<std::string, ov::PropertyMutability::RW> dump_iterations{"DUMP_ITERATIONS"};
+static constexpr Property<bool, ov::PropertyMutability::RW> host_time_profiling{"HOST_TIME_PROFILING"};
+static constexpr Property<size_t, ov::PropertyMutability::RW> impls_cache_capacity{"IMPLS_CACHE_CAPACITY"};
+static constexpr Property<bool, ov::PropertyMutability::RW> disable_async_compilation{"DISABLE_ASYNC_COMPILATION"};
+static constexpr Property<bool, ov::PropertyMutability::RW> disable_shape_agnostic_impls{"DISABLE_SHAPE_AGNOSTIC_IMPLS"};
+static constexpr Property<bool, ov::PropertyMutability::RW> disable_runtime_buffer_fusing{"DISABLE_RUNTIME_BUFFER_FUSING"};
+static constexpr Property<bool, ov::PropertyMutability::RW> disable_memory_reuse{"DISABLE_MEMORY_REUSE"};
+static constexpr Property<bool, ov::PropertyMutability::RW> disable_post_ops_fusions{"DISABLE_POST_OPS_FUSIONS"};
+static constexpr Property<bool, ov::PropertyMutability::RW> disable_horizontal_fc_fusion{"DISABLE_HORIZONTAL_FC_FUSION"};
+static constexpr Property<bool, ov::PropertyMutability::RW> use_usm_host{"USE_USM_HOST"};
+static constexpr Property<bool, ov::PropertyMutability::RW> asym_dynamic_quantization{"ASYM_DYNAMIC_QUANTIZATION"};
+static constexpr Property<std::string, ov::PropertyMutability::RW> mem_prealloc_options{"MEM_PREALLOC_OPTIONS"};
+static constexpr Property<std::string, ov::PropertyMutability::RW> load_dump_raw_binary{"LOAD_DUMP_RAW_BINARY"};
 
 }  // namespace intel_gpu
 }  // namespace ov
