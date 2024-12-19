3590128f0db036d60c20efdbe3aa16ee6673ced3
Shen, Wanglei <wanglei.shen@intel.com>
2024-12-20 00:38:14 +0800

    fix coverity scan issue

--- src/inference/src/os/lin/lin_system_conf.cpp
+++ src/inference/src/os/lin/lin_system_conf.cpp
@@ -220,6 +220,7 @@ CPU::CPU() {
         } else if (valid_cpu_mapping_table.size() == (unsigned)_processors) {
             return 0;
         } else {
+            std::lock_guard<std::mutex> lock{this->_cpu_mutex};
             _processors = valid_cpu_mapping_table.size();
             _cpu_mapping_table.swap(valid_cpu_mapping_table);
             update_valid_processor_linux(std::move(phy_core_list),
