6754c256edd30787ee6b30ac59a5c946f6e944fb
11happy <soni5happy@gmail.com>
2024-12-22 18:01:01 +0530

    (feat): add support for microsoft.range operator for ONNX frontend
    Signed-off-by: 11happy <soni5happy@gmail.com>

--- src/frontends/onnx/tests/onnx_import_com_microsoft.in.cpp
+++ src/frontends/onnx/tests/onnx_import_com_microsoft.in.cpp
@@ -1457,6 +1457,30 @@ OPENVINO_TEST(${BACKEND_NAME}, onnx_com_microsoft_fusedmatmul_2x5x3x6x4_2x6x3x4x
     test_case.run();
 }
 
+OPENVINO_TEST(${BACKEND_NAME}, onnx_com_microsoft_range_with_delta) {
+    const auto model = convert_model("com.microsoft/range_with_delta.onnx");
+    auto test_case = ov::test::TestCase(model, s_device);
+
+    test_case.add_input<float>({0.f});
+    test_case.add_input<float>({10.f});
+    test_case.add_input<float>({1.f});
+    test_case.add_expected_output<float>(Shape{10},
+                                         {0.f,1.f,2.f,3.f,4.f,5.f,6.f,7.f,8.f,9.f});
+
+    test_case.run();
+}
+
+OPENVINO_TEST(${BACKEND_NAME}, onnx_com_microsoft_range_without_delta) {
+    const auto model = convert_model("com.microsoft/range_without_delta.onnx");
+    auto test_case = ov::test::TestCase(model, s_device);
+
+    test_case.add_input<float>({0.f});
+    test_case.add_input<float>({10.f});
+    test_case.add_expected_output<float>(Shape{10},
+                                         {0.f,1.f,2.f,3.f,4.f,5.f,6.f,7.f,8.f,9.f});
+
+    test_case.run();
+}
 OPENVINO_TEST(${BACKEND_NAME}, onnx_com_microsoft_matmul_integer_to_float) {
     const auto model = convert_model("com.microsoft/matmul_integer_to_float.onnx");
     auto test_case = ov::test::TestCase(model, s_device);
