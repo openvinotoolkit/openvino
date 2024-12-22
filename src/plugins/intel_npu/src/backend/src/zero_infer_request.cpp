5ebcc5842f4a9216e0fe03ea1a97a2b2f94c3345
Skrebkov, Artemy <artemy.skrebkov@intel.com>
2024-12-22 22:53:57 +0000

    zeDescriptor.dims cannot be dynamic
    Signed-off-by: Skrebkov, Artemy <artemy.skrebkov@intel.com>

--- src/plugins/intel_npu/src/backend/src/zero_infer_request.cpp
+++ src/plugins/intel_npu/src/backend/src/zero_infer_request.cpp
@@ -54,10 +54,9 @@ void check_level_zero_attributes_match(const IODescriptor& ioDescriptor, const A
                     "Maximum number of dimensions supported: " + std::to_string(ZE_MAX_GRAPH_ARGUMENT_DIMENSIONS_SIZE) +
                         '\n' + "Given: " + std::to_string(ovDimensions.size()));
 
-    const uint32_t dynamicDim = std::numeric_limits<uint32_t>::max();
     for (size_t index = 0; index < ovDimensions.size(); ++index) {
         OPENVINO_ASSERT(
-            ovDimensions[index] == zeDescriptor.info.dims[index] || zeDescriptor.info.dims[index] == dynamicDim,
+            ovDimensions[index] == zeDescriptor.info.dims[index],
             "Shape mismatch for input/output named " + ioDescriptor.nameFromCompiler);
     }
     for (size_t index = ovDimensions.size(); index < ZE_MAX_GRAPH_ARGUMENT_DIMENSIONS_SIZE; ++index) {
