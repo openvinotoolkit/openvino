6ffea93eecee2e9ccc8cde643ad8f6109d1b1659
11happy <soni5happy@gmail.com>
2024-12-23 23:31:48 +0530

    reverted to previous implementation
    Signed-off-by: 11happy <soni5happy@gmail.com>

--- src/frontends/jax/src/op/select_n.cpp
+++ src/frontends/jax/src/op/select_n.cpp
@@ -3,7 +3,12 @@
 //
 
 #include "openvino/frontend/jax/node_context.hpp";
-#include "openvino/op/ops.hpp";
+#include "openvino/op/concat.hpp"
+#include "openvino/op/constant.hpp"
+#include "openvino/op/convert.hpp"
+#include "openvino/op/reshape.hpp"
+#include "openvino/op/gather_elements.hpp"
+
 #include "utils.hpp";
 
 using namespace ov::op;
@@ -18,26 +23,23 @@ OutputVector translate_select_n(const NodeContext& context) {
     auto num_inputs = static_cast<int>(context.get_input_size());
     Output<Node> which = context.get_input(0);
     if (which.get_element_type() == element::boolean) {
-        which = std::make_shared<v0::Convert>(which, element::i32);
+        which = std::make_shared<v0::Convert>(which, element::i32);  
     }
-    OutputVector unsqueezed_cases(num_inputs-1)
-    unsqueezed_cases.reserve(num_inputs - 1);
-    for (int ind = 1; ind < num_inputs; ind++) {
-        auto case_input = context.get_input(ind);
-        auto unsqueeze = std::make_shared<v0::Unsqueeze>(
-            case_input,
-            ov::op::v0::Constant::create(element::i64, Shape{1}, std::vector<int64_t>{0})
-        );
-        unsqueezed_cases[ind-1] = unsqueeze;
+    OutputVector cases_vector(num_inputs - 1);
+    for(int ind = 1; ind < num_inputs; ++ind) {
+        cases_vector[ind - 1] = context.get_input(ind);
     }
-    Output<Node> cases = std::make_shared<v0::Concat>(unsqueezed_cases, 0);
-    auto shape_of = std::make_shared<v3::ShapeOf>(context.get_input(1), element::i64);
-    auto broadcast_which = std::make_shared<v1::Broadcast>(
-        which,
-        shape_of
-    );
-    Output<Node> result = std::make_shared<v6::GatherElements>(cases, broadcast_which, 0);
+    
+    Output<Node> cases = std::make_shared<v0::Concat>(cases_vector, 0);
+    auto which_shape = which.get_shape();
+    std::vector<int64_t> cases_reshape_shape = {num_inputs-1,which_shape[0]};
+    std::vector<int64_t> which_reshape_shape = {1,which_shape[0]};
+    
+    cases = std::make_shared<v1::Reshape>(cases, ov::op::v0::Constant::create(element::i64, Shape{2}, cases_reshape_shape), false);
+    which = std::make_shared<v1::Reshape>(which, ov::op::v0::Constant::create(element::i64, Shape{2}, which_reshape_shape), false);
+    Output<Node> result = std::make_shared<v6::GatherElements>(cases, which, 0);
     return {result};
+
 };
 
 }  // namespace op
