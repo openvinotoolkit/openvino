amanmogal/amx

[OV][JS] Expose the Tensor.isContinuous to Node.js API
* Add a TensorWrap::is_continuous function:
  Calls the underlying Tensor.isContinous function
* Update the addon.ts file with the isContinuous method
* Add unit tests for the isContinuous Api

Closes: #27701

Signed-off-by: Nashez Zubair <nashezzubair@gmail.com>

src/bindings/js/node/src/tensor.cpp
@@ -43,7 +43,8 @@ Napi::Function TensorWrap::get_class(Napi::Env env) {
                         InstanceMethod("getData", &TensorWrap::get_data),
                         InstanceMethod("getShape", &TensorWrap::get_shape),
                         InstanceMethod("getElementType", &TensorWrap::get_element_type),
-                        InstanceMethod("getSize", &TensorWrap::get_size)});
+                        InstanceMethod("getSize", &TensorWrap::get_size),
+                        InstanceMethod("isContinuous", &TensorWrap::is_continuous)});
 }
 
 ov::Tensor TensorWrap::get_tensor() const {
@@ -174,4 +175,13 @@ Napi::Value TensorWrap::get_size(const Napi::CallbackInfo& info) {
     }
     const auto size = static_cast<double>(_tensor.get_size());
     return Napi::Number::New(env, size);
+}
+
+Napi::Value TensorWrap::is_continuous(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    if (info.Length() > 0) {
+        reportError(env, "isContinuous() does not accept any arguments.");
+        return env.Null();
+    }
+    return Napi::Boolean::New(env, _tensor.is_continuous());
 }

openvinotoolkit/openvino

src/bindings/js/node/src/tensor.cpp
@@ -43,7 +43,8 @@ Napi::Function TensorWrap::get_class(Napi::Env env) {
                         InstanceMethod("getData", &TensorWrap::get_data),
                         InstanceMethod("getShape", &TensorWrap::get_shape),
                         InstanceMethod("getElementType", &TensorWrap::get_element_type),
-                        InstanceMethod("getSize", &TensorWrap::get_size)});
+                        InstanceMethod("getSize", &TensorWrap::get_size),
+                        InstanceMethod("isContinuous", &TensorWrap::is_continuous)});
 }
 
 ov::Tensor TensorWrap::get_tensor() const {
@@ -180,4 +181,13 @@ Napi::Value TensorWrap::get_size(const Napi::CallbackInfo& info) {
     }
     const auto size = static_cast<double>(_tensor.get_size());
     return Napi::Number::New(env, size);
+}
+
+Napi::Value TensorWrap::is_continuous(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    if (info.Length() > 0) {
+        reportError(env, "isContinuous() does not accept any arguments.");
+        return env.Undefined();
+    }
+    return Napi::Boolean::New(env, _tensor.is_continuous());
 }