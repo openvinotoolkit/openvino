f6ca7f245721723366d06783a2e0b1c0916c0b7a
BhargavMah <149825128+BhargavMah@users.noreply.github.com>
2024-12-30 19:20:09 +0530

    Update transformation_pipeline.cpp

--- src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
+++ src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
@@ -1132,7 +1132,7 @@ void Transformations::MainSnippets(void) {
                 ov::is_type<ov::op::v1::Subtract>(n) || ov::is_type<ov::op::v4::Swish>(n) ||
                 ov::is_type<ov::op::v0::Tanh>(n)|| ov::is_type<ov::op::v1::LogicalAnd>(n) || 
                 ov::is_type<ov::op::v1::LogicalOr>(n) || ov::is_type<ov::op::v1::LogicalXor>(n) ||
-                ov::is_type<ov::op::v1::LogicalNot>(n););
+                ov::is_type<ov::op::v1::LogicalNot>(n));
 #else
         // CPU Plugin support Swish in Subgraph via conversion to SwichCPU which assumes second input to be constant,
         // and CPU Plugin does not support Mish for x64
