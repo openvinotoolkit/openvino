7b020c74b81f99a9d2703c2ddc2864dc33363c4e
Andrzej Kopytko <andrzejx.kopytko@intel.com>
2025-01-03 11:14:05 +0100

    [DOCS] Enabled button for GenAI benchmarks (#28262)
    ### Details:
     - *item1*
     - *...*
    
    ### Tickets:
     - *ticket-id*

--- docs/sphinx_setup/conf.py
+++ docs/sphinx_setup/conf.py
@@ -187,13 +187,11 @@ except FileNotFoundError:
 html_static_path = ['_static']
 
 html_css_files = [
-    'css/custom.css',
     'css/openvino_sphinx_theme.css',
     'css/button.css',
     'css/input.css',
     'css/textfield.css',
     'css/tabs.css',
-    'css/coveo_custom.css',
     'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css',
 ]
 
