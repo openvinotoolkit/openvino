d85fafd8eea65c68a197c7bbcd1b9596b074320a
Alexandra Sidorova <alexandra.sidorova@intel.com>
2025-01-06 07:00:23 +0100

    [Snippets] Disabled non-inplace ops tokenization on 2nd inputs of MatMuls in MHA, supported transposed_b=true
    [Snippets] Disable second input of MM toknization
    
    [Snippets][Tests] Updated tokenization unit tests
    
    [Snippets][CPU][Tests] Updated cpu func tests
    
    [Snippets] Updated the explanation in the tokenization pass
    
    [Snippets][CPU] Removed  check from tokenization callback
    
    [Snippets] Updated comment in pass of tokenization

--- src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
+++ src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
@@ -1081,10 +1081,7 @@ void Transformations::MainSnippets(void) {
         // Only FP32 dynamic MHA is supported
         if (matmul->is_dynamic())
             return false;
-        // [114487] brgemm kernel in oneDNN requires brgemm_copy_b kernel if MatMul node has transposed_b=True
-        // The current solution with ExtractExplicitMatMulTranspose pass is slower for non-f32 cases than using of
-        // brgemm_copy_b kernel
-        if (matmul->get_transpose_a() || matmul->get_transpose_b())
+        if (matmul->get_transpose_a())
             return false;
         // [150842] The execution of Brgemm INT8/BF16/FP16 on AMX platforms depends on the value of "K % VNNIFactor".
         //          For more details, please teake a look at the ticket 150842
