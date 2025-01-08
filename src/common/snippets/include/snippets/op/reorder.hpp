bb7c1c8324110f8ace21788035920a920ae49d9e
Alexandra Sidorova <alexandra.sidorova@intel.com>
2025-01-08 17:05:27 +0400

    [Snippets][CPU] Added external repacking via BrgemmCopyB (#28179)
    ### Details:
    - *Added two separate implementation for external repacking: in parallel
    section with kernel and in the separate parallel section before kernel
    execution*
    
    ### Tickets:
     - *159886*
    
    ### TODO:
    - [x] *Adjust heuristic of the impl choosing*
    - [x] *Add layout support*
    - [x] *[Cherry-picked] Merge the
    https://github.com/a-sidorova/openvino/pull/266 to this branch*
    - [x] *[Cherry-picked] Merge the
    https://github.com/a-sidorova/openvino/pull/267 to this branch*
    
    ---------
    
    Co-authored-by: Vladislav Golubev <vladislav.golubev@intel.com>

--- src/common/snippets/include/snippets/op/reorder.hpp
+++ src/common/snippets/include/snippets/op/reorder.hpp
@@ -10,35 +10,32 @@
 namespace ov {
 namespace snippets {
 namespace op {
-
 /**
- * @interface Reshape
- * @brief Reshape input tensor to reqiured target shape
+ * @interface Reorder
+ * @brief Reorder reshapes input tensor shape by reqiured target order.
+ *        The tensor data is not updated.
+ *        Note: Order is stored in input PortDescriptor
  * @ingroup snippets
  */
-class Reshape : public ShapeInferOp {
+class Reorder : public ShapeInferOp {
 public:
-    OPENVINO_OP("Reshape", "SnippetsOpset", ShapeInferOp);
-    Reshape(const Output<Node>& x, ov::PartialShape target_shape);
-    Reshape() = default;
+    OPENVINO_OP("Reorder", "SnippetsOpset", ShapeInferOp);
+    Reorder() = default;
+    Reorder(const Output<Node>& x, std::vector<size_t> order);
 
     bool visit_attributes(AttributeVisitor& visitor) override;
     std::shared_ptr<Node> clone_with_new_inputs(const OutputVector& new_args) const override;
     void validate_and_infer_types() override;
 
-    const ov::PartialShape& get_target_shape() const;
-    void set_target_shape(ov::PartialShape shape);
-
     class ShapeInfer : public IShapeInferSnippets {
-        VectorDims target_shape;
-        size_t target_shape_volume = 0;
+        std::vector<size_t> m_target_order {};
     public:
         explicit ShapeInfer(const std::shared_ptr<Node>& n);
         Result infer(const std::vector<VectorDimsRef>& input_shapes) override;
     };
 
 private:
-    ov::PartialShape m_target_shape = {};
+    void custom_constructor_validate_and_infer_types(std::vector<size_t> order);
 };
 
 } // namespace op
