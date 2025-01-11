2e0295f487cbea653766e63985fab5f28a2adf88
sunxiaoxia2022 <xiaoxia.sun@intel.com>
2025-01-11 18:01:04 +0800

    fix ci test issue

--- src/plugins/intel_cpu/tests/unit/streams_info/streams_e2e_test.cpp
+++ src/plugins/intel_cpu/tests/unit/streams_info/streams_e2e_test.cpp
@@ -68,6 +68,9 @@ public:
         CPU& cpu = cpu_info();
         cpu._cpu_mapping_table = test_data.cpu_mapping_table;
         cpu._proc_type_table = test_data.input_proc_type_table;
+        cpu._org_proc_type_table = test_data.input_proc_type_table;
+        cpu._numa_nodes = cpu._proc_type_table.size() > 1 ? static_cast<int>(cpu._proc_type_table.size()) - 1 : 1;
+        cpu._sockets = cpu._numa_nodes;
         std::vector<std::vector<int>> res_proc_type_table = test_data.input_proc_type_table;
 
         if (cpu._cpu_mapping_table.empty()) {
