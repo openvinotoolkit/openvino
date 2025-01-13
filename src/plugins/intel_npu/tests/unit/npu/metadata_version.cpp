113b17edd7c3225df7fdfe694c9300e72a081287
Alexandru Enache <alexandru.enache@intel.com>
2025-01-13 08:08:07 +0000

    Added tests for newer/removed fields cases
    Signed-off-by: Alexandru Enache <alexandru.enache@intel.com>

--- src/plugins/intel_npu/tests/unit/npu/metadata_version.cpp
+++ src/plugins/intel_npu/tests/unit/npu/metadata_version.cpp
@@ -57,8 +57,8 @@ TEST_F(MetadataUnitTests, writeAndReadInvalidMetadataVersion) {
     std::stringstream stream;
     auto meta = MetadataTest(blobSize, std::nullopt);
 
-    constexpr uint32_t dummy_version = MetadataBase::make_version(0x00007E57, 0x0000AC3D);
-    meta.set_version(dummy_version);
+    constexpr uint32_t dummyVersion = MetadataBase::make_version(0x00007E57, 0x0000AC3D);
+    meta.set_version(dummyVersion);
 
     OV_ASSERT_NO_THROW(meta.write(stream));
     ASSERT_ANY_THROW(auto storedMeta = read_metadata_from(stream));
@@ -69,9 +69,9 @@ TEST_F(MetadataUnitTests, writeAndReadMetadataWithNewerMinorVersion) {
     std::stringstream stream;
     auto meta = MetadataTest(blobSize, "some_ov_version");
 
-    constexpr uint32_t dummy_version =
+    constexpr uint32_t dummyVersion =
         MetadataBase::make_version(CURRENT_METADATA_MAJOR_VERSION, CURRENT_METADATA_MINOR_VERSION + 1);
-    meta.set_version(dummy_version);
+    meta.set_version(dummyVersion);
 
     OV_ASSERT_NO_THROW(meta.write(stream));
     std::unique_ptr<MetadataBase> storedMeta;
@@ -132,3 +132,70 @@ INSTANTIATE_TEST_SUITE_P(MetadataUnitTests,
                          MetadataVersionTestFixture,
                          ::testing::ValuesIn(badMetadataVersions),
                          MetadataVersionTestFixture::getTestCaseName);
+
+TEST_F(MetadataUnitTests, writeAndReadMetadataWithNewerFieldAtEnd) {
+    uint64_t blobSize = 0;
+    std::stringstream stream;
+    auto meta = MetadataTest(blobSize, "some_ov_version");
+
+    constexpr uint32_t dummyVersion =
+        MetadataBase::make_version(CURRENT_METADATA_MAJOR_VERSION, CURRENT_METADATA_MINOR_VERSION + 1);
+    meta.set_version(dummyVersion);
+
+    OV_ASSERT_NO_THROW(meta.write(stream));
+
+    // inserting a new field at the end of the blob, between last metadata field and blobDataSize
+    std::string temp = stream.str();
+    size_t offset = MAGIC_BYTES.size() + sizeof(uint64_t);
+    temp.insert(temp.length() - offset, "new metadata field");
+    stream.str("");
+    stream << temp;
+
+    std::unique_ptr<MetadataBase> storedMeta;
+    OV_ASSERT_NO_THROW(storedMeta = read_metadata_from(stream));
+    ASSERT_FALSE(storedMeta->is_compatible());
+}
+
+TEST_F(MetadataUnitTests, writeAndReadMetadataWithNewerFieldAtMiddle) {
+    uint64_t blobSize = 0;
+    std::stringstream stream;
+    auto meta = MetadataTest(blobSize, "some_ov_version");
+
+    constexpr uint32_t dummyVersion =
+        MetadataBase::make_version(CURRENT_METADATA_MAJOR_VERSION + 1, CURRENT_METADATA_MINOR_VERSION);
+    meta.set_version(dummyVersion);
+
+    OV_ASSERT_NO_THROW(meta.write(stream));
+
+    // inserting a new field at the middle of the blob, between metadata version and OV version size
+    std::string temp = stream.str();
+    size_t offset = sizeof(CURRENT_METADATA_VERSION);
+    temp.insert(offset, "new metadata field");
+    stream.str("");
+    stream << temp;
+
+    std::unique_ptr<MetadataBase> storedMeta;
+    EXPECT_ANY_THROW(storedMeta = read_metadata_from(stream));
+}
+
+TEST_F(MetadataUnitTests, writeAndReadMetadataWithRemovedField) {
+    uint64_t blobSize = 0;
+    std::stringstream stream;
+    auto meta = MetadataTest(blobSize, "some_ov_version");
+
+    constexpr uint32_t dummyVersion =
+        MetadataBase::make_version(CURRENT_METADATA_MAJOR_VERSION + 1, CURRENT_METADATA_MINOR_VERSION);
+    meta.set_version(dummyVersion);
+
+    OV_ASSERT_NO_THROW(meta.write(stream));
+
+    // removing fields between metadata version and blob data size
+    std::string temp = stream.str();
+    size_t offset = sizeof(CURRENT_METADATA_VERSION), size = offset + MAGIC_BYTES.size() + sizeof(uint64_t);
+    temp.replace(offset, temp.length() - size, "");
+    stream.str("");
+    stream << temp;
+
+    std::unique_ptr<MetadataBase> storedMeta;
+    EXPECT_ANY_THROW(storedMeta = read_metadata_from(stream));
+}
