a1e56c104a7de923fff7bf9095926a8b8a3921fd
Bogdan Pereanu <bogdan.pereanu@intel.com>
2025-01-13 10:00:22 +0200

    [NPU] Use zero tensor to get correct data (#27980)
    ### Details:
    - *Can not deallocate and re-allocate a newer memory for tensor if
    update mutable command list is not supported. Newer memory should be
    updated in the graph and this can be done only using the updating
    command list feature*
    - *In order to check if the memory was or wasn't re-allocated we are
    checking the unique ID provided by the driver when memory is created*
    
    ### Tickets:
     - *E#134453*
    
    ---------
    
    Signed-off-by: Bogdan Pereanu <bogdan.pereanu@intel.com>

--- src/plugins/intel_npu/tests/functional/behavior/infer_request_run.cpp
+++ src/plugins/intel_npu/tests/functional/behavior/infer_request_run.cpp
@@ -25,6 +25,12 @@ INSTANTIATE_TEST_SUITE_P(smoke_BehaviorTest,
                                             ::testing::ValuesIn(configsInferRequestRunTests)),
                          InferRequestRunTests::getTestCaseName);
 
+INSTANTIATE_TEST_SUITE_P(smoke_BehaviorTest,
+                         SetShapeInferRunTests,
+                         ::testing::Combine(::testing::Values(ov::test::utils::DEVICE_NPU),
+                                            ::testing::ValuesIn(configsInferRequestRunTests)),
+                         InferRequestRunTests::getTestCaseName);
+
 const std::vector<ov::AnyMap> batchingConfigs = {
     {ov::log::level(ov::log::Level::WARNING), ov::intel_npu::batch_mode(ov::intel_npu::BatchMode::PLUGIN)},
     {ov::log::level(ov::log::Level::WARNING), ov::intel_npu::batch_mode(ov::intel_npu::BatchMode::COMPILER)},
