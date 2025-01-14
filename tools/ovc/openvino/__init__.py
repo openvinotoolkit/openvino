7ba586292de6e31fde83ad9b5242a6ab6e666b1b
Anastasia Kuporosova <anastasia.kuporosova@intel.com>
2025-01-14 06:42:48 +0100

    [PyOV] OpExtension (#27664)
    ### Details:
     - Moved `DiscretTypeInfoWrapper class` from cpp to header.
    - Add new overloading for `core.add_extension` which takes op class as
    an argument.
     - Split `PyOp` to header and implementation
     - Add bindings for `OpExtension`
    - Add default implementation for `get_type_info` and
    `clone_with_new_inputs` to allow users create a custom operation without
    adding of these methods.
     - Simplified Op ctor:
     **Before** (and still working approach):
     ```
       def __init__(self, inputs=None):
            super().__init__(self)
            if inputs is not None:
                self.set_arguments(inputs)
                self.constructor_validate_and_infer_types()
    ```
    **After**
     ```
       def __init__(self, inputs=None):
            super().__init__(self, inputs)
    ```
     
    
    ### Tickets:
     - CVS-148474
    
    ---------
    
    Co-authored-by: Michal Lukaszewski <michal.lukaszewski@intel.com>

--- tools/ovc/openvino/__init__.py
+++ tools/ovc/openvino/__init__.py
@@ -32,6 +32,7 @@ from openvino._pyopenvino import Version
 from openvino._pyopenvino import Symbol
 from openvino._pyopenvino import Dimension
 from openvino._pyopenvino import Input
+from openvino._pyopenvino import OpExtension
 from openvino._pyopenvino import Output
 from openvino._pyopenvino import Node
 from openvino._pyopenvino import Strides
@@ -49,7 +50,6 @@ from openvino._pyopenvino import save_model
 from openvino._pyopenvino import layout_helpers
 from openvino._pyopenvino import RemoteContext
 from openvino._pyopenvino import RemoteTensor
-from openvino._pyopenvino import Op
 
 # Import public classes from _ov_api
 from openvino._ov_api import Model
@@ -57,6 +57,7 @@ from openvino._ov_api import Core
 from openvino._ov_api import CompiledModel
 from openvino._ov_api import InferRequest
 from openvino._ov_api import AsyncInferQueue
+from openvino._ov_api import Op
 
 # Import all public modules
 from openvino import runtime as runtime
