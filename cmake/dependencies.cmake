0db72b15ade22231bdbb40c27ee352f8d2fcab2c
Oleg Pipikin <oleg.pipikin@intel.com>
2025-01-14 17:38:49 +0000

    Add cmake options for static builds

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -106,6 +106,21 @@ function(ov_download_tbb)
                 ENVIRONMENT "TBBROOT"
                 SHA256 "63bf0992406d69dfb3c402f46466face2388ca55b6678f29072ba304f3622fe7"
                 USE_NEW_LOCATION TRUE)
+        if (TBB_STATIC_CRT)
+            RESOLVE_DEPENDENCY(TBB
+                               ARCHIVE_WIN "oneapi-tbb-2021.2.5-win-vcruntime.zip"
+                               TARGET_PATH "${TEMP}/tbb"
+                               ENVIRONMENT "TBBROOT"
+                               SHA256 "dc6d5caed18fe4a30e088d1e45f92aac8fa10f542d19019edafa6aeea4465551"
+                               USE_NEW_LOCATION TRUE)
+        else()
+            RESOLVE_DEPENDENCY(TBB
+                               ARCHIVE_WIN "oneapi-tbb-2021.2.5-win-trim-cfg-simplify.zip"
+                               TARGET_PATH "${TEMP}/tbb"
+                               ENVIRONMENT "TBBROOT"
+                               SHA256 "63bf0992406d69dfb3c402f46466face2388ca55b6678f29072ba304f3622fe7"
+                               USE_NEW_LOCATION TRUE)
+            endif()
     elseif(ANDROID AND X86_64)
         RESOLVE_DEPENDENCY(TBB
                 ARCHIVE_ANDROID "tbb2020_20200404_android.tgz"
