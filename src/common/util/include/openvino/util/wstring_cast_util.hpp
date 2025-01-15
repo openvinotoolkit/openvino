42f25d4fabd0e21b044c144597c402f9da4274a7
Michal Barnas <michal.barnas@intel.com>
2025-01-15 14:27:03 +0000

    move wide character string conversions to separate file

---
+++ src/common/util/include/openvino/util/wstring_cast_util.hpp
@@ -0,0 +1,31 @@
+// Copyright (C) 2018-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <string>
+
+#include "openvino/util/util.hpp"
+
+namespace ov {
+namespace util {
+
+#ifdef OPENVINO_ENABLE_UNICODE_PATH_SUPPORT
+/**
+ * @brief Conversion from wide character string to a single-byte chain.
+ * @param wstr A wide-char string
+ * @return A multi-byte string
+ */
+std::string wstring_to_string(const std::wstring& wstr);
+/**
+ * @brief Conversion from single-byte chain to wide character string.
+ * @param str A null-terminated string
+ * @return A wide-char string
+ */
+std::wstring string_to_wstring(const std::string& str);
+
+#endif
+
+}  // namespace util
+}  // namespace ov
