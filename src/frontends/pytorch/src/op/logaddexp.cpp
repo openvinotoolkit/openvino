34b182aaceafa372c08a6d464d0e1de2707568ad
Bharat Jain <bharat.jain@plaksha.edu.in>
2025-01-20 00:54:46 +0530

    fixed unstable decomposition

--- src/frontends/pytorch/src/op/logaddexp.cpp
+++ src/frontends/pytorch/src/op/logaddexp.cpp
@@ -9,6 +9,8 @@
 #include "openvino/op/convert_like.hpp"
 #include "openvino/op/exp.hpp"
 #include "openvino/op/log.hpp"
+#include "openvino/op/maximum.hpp"
+#include "openvino/op/subtract.hpp"
 #include "utils.hpp"
 
 namespace ov {
@@ -29,15 +31,23 @@ OutputVector translate_logaddexp(const NodeContext& context) {
     input1 = context.mark_node(std::make_shared<v0::Convert>(input1, element::f32));
     input2 = context.mark_node(std::make_shared<v0::Convert>(input2, element::f32));
 
-    // Calculate exp(input1) and exp(input2)
-    auto exp1 = context.mark_node(std::make_shared<v0::Exp>(input1));
-    auto exp2 = context.mark_node(std::make_shared<v0::Exp>(input2));
+    // Find maximum of inputs
+    auto max_val = context.mark_node(std::make_shared<v1::Maximum>(input1, input2));
 
-    // Add the exponentials
+    // Calculate x1 - max and x2 - max
+    auto diff1 = context.mark_node(std::make_shared<v1::Subtract>(input1, max_val));
+    auto diff2 = context.mark_node(std::make_shared<v1::Subtract>(input2, max_val));
+
+    // Calculate exp(x1 - max) and exp(x2 - max)
+    auto exp1 = context.mark_node(std::make_shared<v0::Exp>(diff1));
+    auto exp2 = context.mark_node(std::make_shared<v0::Exp>(diff2));
+
+    // Add the scaled exponentials
     auto sum = context.mark_node(std::make_shared<v1::Add>(exp1, exp2));
 
-    // Take the natural logarithm of the sum
-    auto result = context.mark_node(std::make_shared<v0::Log>(sum));
+    // Take the log and add back the maximum
+    auto log_sum = context.mark_node(std::make_shared<v0::Log>(sum));
+    auto result = context.mark_node(std::make_shared<v1::Add>(log_sum, max_val));
 
     // If the output tensor type is different, convert to match
     if (input1.get_element_type() != element::f32) {
