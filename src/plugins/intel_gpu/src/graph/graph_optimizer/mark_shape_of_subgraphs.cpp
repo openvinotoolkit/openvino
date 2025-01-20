b5cc83ef8e78dcb2badb8baf72a311a5c66543dd
Wang Wangwang <wangwang.wang@intel.com>
2025-01-20 15:00:02 +0800

    Skip mark_node for concatenation node if dependency nodes are data and shape_of

--- src/plugins/intel_gpu/src/graph/graph_optimizer/mark_shape_of_subgraphs.cpp
+++ src/plugins/intel_gpu/src/graph/graph_optimizer/mark_shape_of_subgraphs.cpp
@@ -94,10 +94,11 @@ bool mark_shape_of_subgraphs::can_mark_node(const program_node& node) {
             return false;
     }
 
-    // Exclude output node when it's not dynamic, because will disable USM host memory for infer request`s tensors for
-    // PVC and subsequent dGPUs when use CPU reference implementation
-    if (node.is_output() && !node.is_dynamic()) {
-        return false;
+    // Skip mark_node for concatenation node if dependency nodes are data and shape_of
+    auto& dependencies = node.get_dependencies();
+    if (node.is_type<concatenation>() && dependencies.size() == 2) {
+        if (dependencies[0].first->is_type<shape_of>() && dependencies[1].first->is_type<data>())
+            return false;
     }
 
     return true;
