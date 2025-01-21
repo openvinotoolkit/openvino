2c7989270fb24865e1d44b53767fff954923f6fa
liubo-intel <bo4.liu@intel.com>
2025-01-21 09:35:31 +0800

    prepare_table() shows perf influnce, disabled if not neccessary

--- src/plugins/intel_cpu/src/emitters/plugin/x64/jit_bf16_emitters.hpp
+++ src/plugins/intel_cpu/src/emitters/plugin/x64/jit_bf16_emitters.hpp
@@ -17,7 +17,11 @@ public:
                           conversion_mode mode = conversion_mode::default_mode)
         : jit_emitter(host, host_isa, exec_prc),
           mode_(mode) {
-        prepare_table();
+        if ((!dnnl::impl::cpu::x64::mayiuse(dnnl::impl::cpu::x64::avx512_core_bf16) &&
+             !dnnl::impl::cpu::x64::mayiuse(dnnl::impl::cpu::x64::avx2_vnni_2)) ||
+            mode_ == conversion_mode::saturation_mode) {
+            prepare_table();
+        }
     }
 
     size_t get_inputs_num() const override {
