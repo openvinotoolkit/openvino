13f599fd2872283524c818fbc7a4021432ab177d
Kazantsev, Roman <roman.kazantsev@intel.com>
2025-01-22 13:57:48 +0400

    Extend support with p param
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/frontends/pytorch/src/op/bernoulli.cpp
+++ src/frontends/pytorch/src/op/bernoulli.cpp
@@ -19,18 +19,37 @@ using namespace ov;
 using namespace ov::op;
 
 OutputVector translate_bernoulli(const NodeContext& context) {
+    // supported signatures:
+    // 1. aten::bernoulli(input, *, Generator? generator=None) -> Tensor
+    // 2. aten::bernoulli(input, *, Generator? generator=None, Tensor(a!) out) -> Tensor(a!)
+    // 3. aten::bernoulli(input, float p, *, Generator? generator=None) -> Tensor
     num_inputs_check(context, 1, 3);
     uint64_t global_seed = 0;
     auto input = context.get_input(0);
+    auto input_type = input.get_element_type().is_static() ? input.get_element_type() : element::f64;
+    auto input_shape = context.mark_node(std::make_shared<v3::ShapeOf>(input, element::i32));
+    auto probs_threshold = input;
+
+    bool with_p = false;
+    size_t gen_ind = 1;
     if (!context.input_is_none(1)) {
+        auto p = context.get_input(1);
+        with_p = p.get_element_type().is_real() ? true : false;
+        if (with_p) {
+            // need to override probs thresholds and samples type
+            input_type = p.get_element_type().is_static() ? p.get_element_type() : element::f64;
+            gen_ind = 2;
+            probs_threshold = p;
+        }
+    }
+
+    if (!context.input_is_none(gen_ind)) {
         // retrieve seed set to Generator
-        auto gen_const = as_type_ptr<v0::Constant>(context.get_input(1).get_node_shared_ptr());
+        auto gen_const = as_type_ptr<v0::Constant>(context.get_input(static_cast<int>(gen_ind)).get_node_shared_ptr());
         PYTORCH_OP_CONVERSION_CHECK(gen_const, "aten::bernoulli expects a constant representing a generator seed");
         auto seed = gen_const->cast_vector<uint64_t>();
         global_seed = seed.size() > 0 ? seed[0] : global_seed;
     }
-    auto input_type = input.get_element_type().is_static() ? input.get_element_type() : element::f64;
-    auto input_shape = context.mark_node(std::make_shared<v3::ShapeOf>(input, element::i32));
 
     // generate tensor of input shape with elements sampled from u ~ RandomUniform(0, 1) distribution
     // I[u < input] will represent samples of bernoulli distribution with parameter `input`
@@ -44,11 +63,11 @@ OutputVector translate_bernoulli(const NodeContext& context) {
                                                                             0,
                                                                             PhiloxAlignment::PYTORCH));
     if (!input.get_element_type().is_static()) {
-        ru_samples = context.mark_node(std::make_shared<v1::ConvertLike>(ru_samples, input));
+        ru_samples = context.mark_node(std::make_shared<v1::ConvertLike>(ru_samples, probs_threshold));
     }
-    auto bernoulli_samples = context.mark_node(std::make_shared<v1::Less>(ru_samples, input));
+    auto bernoulli_samples = context.mark_node(std::make_shared<v1::Less>(ru_samples, probs_threshold));
 
-    if (!context.input_is_none(2)) {
+    if (!with_p && !context.input_is_none(2)) {
         auto out = context.get_input(2);
         bernoulli_samples = context.mark_node(std::make_shared<v1::ConvertLike>(bernoulli_samples, out));
         context.mutate_input(2, bernoulli_samples);
--- tests/layer_tests/pytorch_tests/test_bernoulli.py
+++ tests/layer_tests/pytorch_tests/test_bernoulli.py
@@ -8,7 +8,7 @@ import torch
 from pytorch_layer_test_class import PytorchLayerTest
 
 
-class TestMultinomial(PytorchLayerTest):
+class TestBernoulli(PytorchLayerTest):
     def _prepare_input(self, input, input_type, out):
         model_inputs = [input.astype(input_type)]
         if out:
@@ -50,9 +50,50 @@ class TestMultinomial(PytorchLayerTest):
     @pytest.mark.precommit
     def test_bernoulli(self, input, input_type, out, seed, ie_device, precision, ir_version):
         if input_type == np.float64:
-            pytest.skip("156027: Incorrect specification or reference for RandonUniform for fp64 output type")
+            pytest.skip("156027: Incorrect specification or reference for RandomUniform for fp64 output type")
         self._test(*self.create_model(out, seed),
                    ie_device, precision, ir_version,
                    kwargs_to_prepare_input={"input": input,
                                             "input_type": input_type,
                                             "out": out})
+
+
+class TestBernoulliWithP(PytorchLayerTest):
+    def _prepare_input(self, input, input_type):
+        model_inputs = [input.astype(input_type)]
+        return model_inputs
+
+    def create_model(self, p, seed):
+        class aten_bernoulli(torch.nn.Module):
+            def __init__(self, p, seed) -> None:
+                super().__init__()
+                gen = torch.Generator()
+                gen.manual_seed(seed)
+                self.gen = gen
+                self.p = p
+                self.forward = self.bernoulli_with_p
+
+            def bernoulli_with_p(self, input):
+                bernoulli_res = torch.bernoulli(input, self.p, generator=self.gen)
+                return bernoulli_res
+
+        ref_net = None
+
+        return aten_bernoulli(p, seed), ref_net, "aten::bernoulli"
+
+    @pytest.mark.parametrize("input", [
+        np.array([[0, 1, 0, 0], [0, 0, 0, 1], [1, 0, 0, 0]]),
+        np.array([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]),
+    ])
+    @pytest.mark.parametrize("input_type", [np.float32, np.int32, np.float64])
+    @pytest.mark.parametrize("p", [0.0, 0.4, 1.0])
+    @pytest.mark.parametrize("seed", [12])
+    @pytest.mark.nightly
+    @pytest.mark.precommit
+    def test_bernoulli(self, input, input_type, p, seed, ie_device, precision, ir_version):
+        if p not in [0.0, 1.0]:
+            pytest.skip("156027: Incorrect specification or reference for RandomUniform for fp64 output type")
+        self._test(*self.create_model(p, seed),
+                   ie_device, precision, ir_version,
+                   kwargs_to_prepare_input={"input": input,
+                                            "input_type": input_type})
