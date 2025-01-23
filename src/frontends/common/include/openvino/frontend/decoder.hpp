openvinotoolkit/openvino

commit a6ab17ecd8e2568e0b6cd642d541e4da08a4424a

Fixed C++ RTTI for Core base classes (#28555)
### Details:
Android support:

<img width="200" alt="{ABD03686-78FD-4F33-A2E8-C3BE1C030D5C}"
src="https://github.com/user-attachments/assets/55bb0600-4f14-448c-b4be-e1e97e354859"
/>

in header files threated as an inline definition, which typically
results in a "weak" symbol
"weak"  symbols are not exported from dll/so libs. 

To make these symbols "strong", we moved the definitions to ".cpp"
files.

src/frontends/common/include/openvino/frontend/decoder.hpp
@@ -49,9 +49,9 @@ struct Union;
 }  // namespace type
 
 /// Plays a role of node, block and module decoder
-class IDecoder {
+class FRONTEND_API IDecoder {
 public:
-    virtual ~IDecoder() = default;
+    virtual ~IDecoder();
 };
 
 class FRONTEND_API DecoderBase : public IDecoder {
@@ -82,9 +82,6 @@ public:
 
     /// \brief Get node name
     virtual const std::string& get_op_name() const = 0;
-
-    /// \brief Destructor
-    virtual ~DecoderBase();
 };
 
 }  // namespace frontend

src/frontends/common/include/openvino/frontend/graph_iterator.hpp
@@ -34,9 +34,6 @@ public:
     /// \brief Return a pointer to a decoder of the current node
     virtual std::shared_ptr<DecoderBase> get_decoder() const = 0;
 
-    /// \brief Destructor
-    virtual ~GraphIterator() = default;
-
     /// \brief Checks if the main model graph contains a function of the requested name in the library
     /// Returns GraphIterator to this function and nullptr, if it does not exist
     virtual std::shared_ptr<GraphIterator> get_body_graph_iterator(const std::string& func_name) const = 0;

src/frontends/common/src/decoder.cpp
@@ -6,4 +6,4 @@
 
 using namespace ov::frontend;
 
-DecoderBase::~DecoderBase() = default;
+IDecoder::~IDecoder() = default;

src/common/low_precision_transformations/include/low_precision/cleanup_transformation.hpp
@@ -17,7 +17,6 @@ namespace low_precision {
 class LP_TRANSFORMATIONS_API CleanupTransformation : public LayerTransformation {
 public:
     CleanupTransformation(const Params& params);
-    virtual ~CleanupTransformation() = default;
 
     bool canBeTransformed(const std::shared_ptr<Node>& layer) const override;
     static bool canBeTransformedStatic(

src/common/low_precision_transformations/include/low_precision/fuse_elementwise_to_fake_quantize.hpp
@@ -19,7 +19,6 @@ namespace low_precision {
 class LP_TRANSFORMATIONS_API FuseElementwiseToFakeQuantizeTransformation : public CleanupTransformation {
 public:
     FuseElementwiseToFakeQuantizeTransformation(const Params& params);
-    virtual ~FuseElementwiseToFakeQuantizeTransformation() = default;
 
     bool canBeTransformed(const std::shared_ptr<Node>& layer) const override;
 };

src/common/low_precision_transformations/include/low_precision/layer_transformation.hpp
@@ -288,7 +288,6 @@ public:
     };
 
     LayerTransformation(const Params& params);
-    virtual ~LayerTransformation() = default;
     virtual bool transform(ov::pass::pattern::Matcher &m) = 0;
 
     virtual bool canBeTransformed(const std::shared_ptr<Node>& layer) const;

src/common/low_precision_transformations/include/low_precision/rt_info/shared_value_attribute.hpp
@@ -15,8 +15,6 @@
 template <class T>
 class LP_TRANSFORMATIONS_API SharedAttribute : public ov::RuntimeAttribute {
 public:
-    virtual ~SharedAttribute() = default;
-
     /**
      * @ingroup ov_transformation_common_api
      * @brief SharedValueAttribute type for shared value attributes.

src/core/include/openvino/core/any.hpp
@@ -536,7 +536,7 @@ class OPENVINO_API Any {
         template <class U, class T, class... Others>
         U convert_impl() const;
 
-        virtual ~Base() = default;
+        virtual ~Base();
     };
 
     template <class T, typename = void>
@@ -610,8 +610,6 @@ class OPENVINO_API Any {
 
         template <typename... Args>
         Impl(Args&&... args) : value(std::forward<Args>(args)...) {}
-
-        virtual ~Impl(){};
 
         const std::type_info& type_info() const override {
             return typeid(T);

commit 581587dea1ad49a5bde3b7500cb0db2b0b90f13c

Fixed C++ RTTI for header only classes (#28624)
### Details:
- Regression after
https://github.com/openvinotoolkit/openvino/pull/28555

src/frontends/common/include/openvino/frontend/decoder.hpp
@@ -82,6 +82,9 @@ public:
 
     /// \brief Get node name
     virtual const std::string& get_op_name() const = 0;
+
+    /// \brief Destructor
+    virtual ~DecoderBase();
 };
 
 }  // namespace frontend

src/frontends/common/include/openvino/frontend/graph_iterator.hpp
@@ -45,14 +45,10 @@ public:
     virtual std::vector<std::string> get_output_names() const = 0;
 
     /// \brief Returns a map from internal tensor name to (user-defined) external name for inputs
-    virtual std::map<std::string, std::string> get_input_names_map() const {
-        return {};
-    }
+    virtual std::map<std::string, std::string> get_input_names_map() const;
 
     /// \brief Returns a map from internal tensor name to (user-defined) external name for outputs
-    virtual std::map<std::string, std::string> get_output_names_map() const {
-        return {};
-    }
+    virtual std::map<std::string, std::string> get_output_names_map() const;
 };
 
 }  // namespace tensorflow

src/frontends/common/src/decoder.cpp
@@ -7,3 +7,5 @@
 using namespace ov::frontend;
 
 IDecoder::~IDecoder() = default;
+
+DecoderBase::~DecoderBase() = default;

src/frontends/common/src/graph_iterator.cpp
@@ -1,0 +1,15 @@
+// Copyright (C) 2018-2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "openvino/frontend/graph_iterator.hpp"
+
+using namespace ov::frontend::tensorflow;
+
+std::map<std::string, std::string> GraphIterator::get_input_names_map() const {
+    return {};
+}
+
+std::map<std::string, std::string> GraphIterator::get_output_names_map() const {
+    return {};
+}

src/frontends/common/src/hash_table.cpp
@@ -6,4 +6,4 @@
 
 using namespace ov::frontend;
 
-HashTable::~HashTable(){};
+HashTable::~HashTable() = default;

src/frontends/pytorch/src/frontend.cpp
@@ -397,7 +397,7 @@ ov::frontend::InputModel::Ptr FrontEnd::load_impl(const std::vector<ov::Any>& va
     size_t extra_variants_num = variants.size() > 0 && variants[variants.size() - 1].is<bool>() ? 1 : 0;
     FRONT_END_GENERAL_CHECK(variants.size() == 1 + extra_variants_num,
                             "PyTorch Frontend supports exactly one parameter in model representation, got ",
-                            std::to_string(variants.size()),
+                            variants.size(),
                             " instead.");
     FRONT_END_GENERAL_CHECK(variants[0].is<std::shared_ptr<IDecoder>>(),
                             "PyTorch Frontend doesn't support provided model type. Please provide supported model "