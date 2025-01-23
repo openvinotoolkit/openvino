480fc7723dd9aa1d070f30b118f1eb883ea55a0c
Kazantsev, Roman <roman.kazantsev@intel.com>
2025-01-23 14:07:53 +0400

    [TF FE] Fix complex tensor support for extensions on MacOS
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

---
+++ src/frontends/common/src/complex_type_mark.cpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2018-2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "openvino/frontend/complex_type_mark.hpp"
+
+using namespace ov::frontend;
+
+void ComplexTypeMark::validate_and_infer_types() {
+    set_output_type(0, ov::element::dynamic, PartialShape::dynamic());
+}
