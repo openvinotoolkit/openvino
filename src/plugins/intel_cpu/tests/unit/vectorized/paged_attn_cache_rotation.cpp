openvinotoolkit/openvino

commit 4addceea7595ce558b8e80520da19338688bca53

Add cache rotation inputs and CPU kernel implementation for cache rotation (#27088)
Tickets:
153783

src/plugins/intel_cpu/tests/unit/vectorized/CMakeLists.txt
@@ -1,0 +1,89 @@
+# Copyright (C) 2018-2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set(TARGET_NAME ov_cpu_unit_tests_vectorized)
+
+if(BUILD_SHARED_LIBS)
+    set (OBJ_LIB $<TARGET_OBJECTS:openvino_intel_cpu_plugin_obj>)
+endif()
+
+if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    ov_add_compiler_flags(/wd5051)
+endif()
+
+if(NOT X86_64)
+    list(APPEND EXCLUDED_SOURCE_PATHS_FOR_UNIT_TEST
+      ${CMAKE_CURRENT_SOURCE_DIR}/paged_attn_cache_rotation.cpp)
+else()
+    list(APPEND EXCLUDED_SOURCE_PATHS_FOR_UNIT_TEST
+      ${CMAKE_CURRENT_SOURCE_DIR}/stub.cpp)
+endif()
+
+if (ENABLE_MLAS_FOR_CPU)
+    set(MLAS_LIBRARY "mlas")
+endif()
+
+if (ENABLE_SHL_FOR_CPU)
+    set(SHL_LIBRARY "shl")
+endif()
+
+ov_add_test_target(
+        NAME ${TARGET_NAME}
+        ROOT ${CMAKE_CURRENT_SOURCE_DIR}
+        INCLUDES
+            PUBLIC
+                $<TARGET_PROPERTY:openvino_intel_cpu_plugin,SOURCE_DIR>/src
+                $<TARGET_PROPERTY:openvino_intel_cpu_plugin,SOURCE_DIR>/src/nodes
+                $<TARGET_PROPERTY:openvino::conditional_compilation,INTERFACE_INCLUDE_DIRECTORIES>
+            PRIVATE
+                $<TARGET_PROPERTY:openvino::snippets,SOURCE_DIR>/include
+        EXCLUDED_SOURCE_PATHS
+            ${EXCLUDED_SOURCE_PATHS_FOR_UNIT_TEST}
+        OBJECT_FILES
+            ${OBJ_LIB}
+        LINK_LIBRARIES
+            gtest
+            gtest_main
+            dnnl
+            gmock
+            openvino_runtime_s
+            unit_test_utils
+            ov_snippets_models
+            snippets_test_utils
+            ${MLAS_LIBRARY}
+            ${SHL_LIBRARY}
+        ADD_CPPLINT
+        LABELS
+            OV UNIT CPU
+)
+
+
+if (ENABLE_SNIPPETS_LIBXSMM_TPP)
+    add_definitions(-DSNIPPETS_LIBXSMM_TPP -DLIBXSMM_DEFAULT_CONFIG)
+    target_compile_definitions(xsmm PRIVATE __BLAS=0)
+    target_link_libraries(${TARGET_NAME} PRIVATE xsmm)
+    target_include_directories(${TARGET_NAME} SYSTEM PRIVATE $<TARGET_PROPERTY:xsmm,INCLUDE_DIRECTORIES>)
+endif()
+
+if (X86_64)
+    ov_avx2_optimization_flags(avx2_flags)
+    ov_avx512_optimization_flags(avx512_flags)
+
+    target_compile_options(${TARGET_NAME} PRIVATE "${avx2_flags};${avx512_flags}")
+    target_compile_definitions(${TARGET_NAME} PRIVATE HAVE_AVX2 HAVE_AVX512F)
+endif()
+
+
+if (WIN32)
+    # Prevents defining min/max as macros
+    target_compile_definitions(${TARGET_NAME} PRIVATE NOMINMAX)
+endif()
+
+target_include_directories(${TARGET_NAME} SYSTEM PRIVATE
+    $<TARGET_PROPERTY:dnnl,INCLUDE_DIRECTORIES>)
+
+target_include_directories(${TARGET_NAME} SYSTEM PRIVATE
+    $<TARGET_PROPERTY:dnnl,SOURCE_DIR>/src/common
+    $<TARGET_PROPERTY:dnnl,SOURCE_DIR>/src/cpu
+    $<TARGET_PROPERTY:dnnl,SOURCE_DIR>/include)

src/plugins/intel_cpu/tests/unit/CMakeLists.txt
@@ -2,6 +2,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+add_subdirectory(vectorized)
+
 set(TARGET_NAME ov_cpu_unit_tests)
 
 if(BUILD_SHARED_LIBS)
@@ -52,6 +54,8 @@ ov_add_test_target(
                 $<TARGET_PROPERTY:openvino::snippets,SOURCE_DIR>/include
         EXCLUDED_SOURCE_PATHS
             ${EXCLUDED_SOURCE_PATHS_FOR_UNIT_TEST}
+            ${CMAKE_CURRENT_SOURCE_DIR}/vectorized
+
         OBJECT_FILES
             ${OBJ_LIB}
         LINK_LIBRARIES
@@ -77,6 +81,7 @@ if (ENABLE_SNIPPETS_LIBXSMM_TPP)
     target_link_libraries(${TARGET_NAME} PRIVATE xsmm)
     target_include_directories(${TARGET_NAME} SYSTEM PRIVATE $<TARGET_PROPERTY:xsmm,INCLUDE_DIRECTORIES>)
 endif()
+
 
 # LTO
 set_target_properties(${TARGET_NAME} PROPERTIES INTERPROCEDURAL_OPTIMIZATION_RELEASE ${ENABLE_LTO})

.github/workflows/job_cxx_unit_tests.yml
@@ -195,6 +195,12 @@ jobs:
           ${{ env.SETUPVARS_COMMAND }}
           ${{ env.INSTALL_TEST_DIR }}/ov_cpu_unit_tests --gtest_print_time=1 --gtest_output=xml:${{ env.INSTALL_TEST_DIR }}/TEST-CPUUnitTests.xml
 
+      - name: CPU plugin unit tests (vectorized)
+        if: fromJSON(inputs.affected-components).CPU.test
+        run: |
+          ${{ env.SETUPVARS_COMMAND }}
+          ${{ env.INSTALL_TEST_DIR }}/ov_cpu_unit_tests_vectorized --gtest_print_time=1 --gtest_output=xml:${{ env.INSTALL_TEST_DIR }}/TEST-CPUUnitTestsVectorized.xml
+
       - name: ov_subgraphs_dumper_tests tests
         run: |
           ${{ env.SETUPVARS_COMMAND }}

src/bindings/python/src/pyopenvino/core/offline_transformations.cpp
@@ -143,15 +143,18 @@ void regmodule_offline_transformations(py::module m) {
 
     m_offline_transformations.def(
         "paged_attention_transformation",
-        [](py::object& ie_api_model, bool use_block_indices_inputs, bool use_score_outputs) {
+        [](py::object& ie_api_model, bool use_block_indices_inputs, bool use_score_outputs, bool allow_cache_rotation) {
             const auto model = Common::utils::convert_to_model(ie_api_model);
             ov::pass::Manager manager;
-            manager.register_pass<ov::pass::SDPAToPagedAttention>(use_block_indices_inputs, use_score_outputs);
+            manager.register_pass<ov::pass::SDPAToPagedAttention>(use_block_indices_inputs,
+                                                                  use_score_outputs,
+                                                                  allow_cache_rotation);
             manager.run_passes(model);
         },
         py::arg("model"),
         py::arg("use_block_indices_inputs") = false,
-        py::arg("use_score_outputs") = false);
+        py::arg("use_score_outputs") = false,
+        py::arg("allow_cache_rotation") = false);
 
     m_offline_transformations.def(
         "stateful_to_stateless_transformation",

src/common/transformations/include/transformations/sdpa_to_paged_attention/state_management_pattern.hpp
@@ -24,8 +24,12 @@ public:
                            ParameterVector& parameters_to_remove,
                            int& layer_index,
                            ov::Output<Node> max_context_len,
-                           ParameterVector& block_indices_inputs,
+                           ParameterVector& block_indices_inputs_for_each_layer,
                            ResultVector& score_results,
-                           bool use_block_indices,
-                           bool use_score_outputs);
+                           bool use_per_layer_block_indices_inputs,
+                           bool use_score_outputs,
+                           bool allow_cache_rotation,
+                           ParameterVector& rotated_block_indices_inputs_for_each_layer,
+                           ParameterVector& rotation_deltas_inputs_for_each_layer,
+                           std::shared_ptr<op::v0::Parameter> model_rotation_trig_lut);
 };

src/common/transformations/src/transformations/sdpa_to_paged_attention/state_management_pattern.cpp
@@ -15,6 +15,7 @@
 #include "openvino/op/gather.hpp"
 #include "openvino/op/multiply.hpp"
 #include "openvino/op/paged_attention.hpp"
+#include "openvino/op/parameter.hpp"
 #include "openvino/op/reshape.hpp"
 #include "openvino/op/scaled_dot_product_attention.hpp"
 #include "openvino/op/select.hpp"
@@ -70,10 +71,14 @@ ov::pass::StateManagementPattern::StateManagementPattern(ParameterVector& kv_par
                                                          ParameterVector& parameters_to_remove,
                                                          int& layer_index,
                                                          Output<Node> max_context_len,
-                                                         ParameterVector& block_indices_inputs,
+                                                         ParameterVector& block_indices_inputs_for_each_layer,
                                                          ResultVector& score_results,
-                                                         bool use_block_indices_inputs,
-                                                         bool use_score_outputs) {
+                                                         bool use_per_layer_block_indices_inputs,
+                                                         bool use_score_outputs,
+                                                         bool allow_cache_rotation,
+                                                         ParameterVector& rotated_block_indices_inputs_for_each_layer,
+                                                         ParameterVector& rotation_deltas_inputs_for_each_layer,
+                                                         std::shared_ptr<op::v0::Parameter> model_rotation_trig_lut) {
     MATCHER_SCOPE(StateManagementPattern);
 
     auto k_current = pattern::any_input();
@@ -176,9 +181,11 @@ ov::pass::StateManagementPattern::StateManagementPattern(ParameterVector& kv_par
                                           &model_remaining_params,
                                           &sliding_window,
                                           &parameters_to_remove,
-                                          &block_indices_inputs,
+                                          &block_indices_inputs_for_each_layer,
                                           &score_results,
-                                          &layer_index](ov::pass::pattern::Matcher& m) {
+                                          &layer_index,
+                                          &rotated_block_indices_inputs_for_each_layer,
+                                          &rotation_deltas_inputs_for_each_layer](ov::pass::pattern::Matcher& m) {
         const auto& pattern_map = m.get_pattern_value_map();
         auto real_q = pattern_map.at(q);
 
@@ -382,11 +389,27 @@ ov::pass::StateManagementPattern::StateManagementPattern(ParameterVector& kv_par
                                                                           max_context_len.get_node_shared_ptr()};
         pa_arguments.insert(pa_arguments.end(), additional_params.begin(), additional_params.end());
 
-        if (use_block_indices_inputs) {
+        if (use_per_layer_block_indices_inputs) {
             auto block_indices = setName(std::make_shared<v0::Parameter>(element::i32, PartialShape{-1}),
                                          "block_indices." + std::to_string(layer_index - 1));
             pa_arguments.insert(pa_arguments.begin() + 7, block_indices);
-            block_indices_inputs.push_back(block_indices);
+            block_indices_inputs_for_each_layer.push_back(block_indices);
+        }
+
+        OPENVINO_ASSERT(pa_arguments.size() == 13);
+
+        if (allow_cache_rotation) {
+            auto rotated_block_indices = setName(std::make_shared<v0::Parameter>(element::i32, PartialShape{-1}),
+                                                 "rotated_block_indices." + std::to_string(layer_index - 1));
+            auto rotation_deltas = setName(std::make_shared<v0::Parameter>(element::i32, PartialShape{-1, -1}),
+                                           "rotation_deltas." + std::to_string(layer_index - 1));
+
+            pa_arguments.insert(pa_arguments.begin() + 13, rotated_block_indices);
+            pa_arguments.insert(pa_arguments.begin() + 14, rotation_deltas);
+            pa_arguments.insert(pa_arguments.begin() + 15, model_rotation_trig_lut);
+
+            rotated_block_indices_inputs_for_each_layer.push_back(rotated_block_indices);
+            rotation_deltas_inputs_for_each_layer.push_back(rotation_deltas);
         }
 
         auto paged_attention = std::make_shared<ov::op::PagedAttentionExtension>(pa_arguments);

commit 174869c07cceebaecebc6eec77b17793f0ebb41a

Fix macOS x86_64 vectorized CPU tests (#28656)
### Details:
- Regression after
https://github.com/openvinotoolkit/openvino/pull/27088
- AVX512 flags are used when they are disabled via cmake's
`ENABLE_AVX512F`

Co-authored-by: Ilya Lavrenov <ilya.lavrenov@intel.com>

---------

Signed-off-by: Tomasz Jankowski <tomasz1.jankowski@intel.com>
Co-authored-by: Ilya Lavrenov <ilya.lavrenov@intel.com>

.github/workflows/mac.yml
@@ -155,6 +155,7 @@ jobs:
             -DENABLE_NCC_STYLE=OFF \
             -DENABLE_TESTS=ON \
             -DENABLE_WHEEL=OFF \
+            -DENABLE_AVX512F=OFF \
             -DCMAKE_COMPILE_WARNING_AS_ERROR=OFF \
             -DENABLE_STRICT_DEPENDENCIES=OFF \
             -DCMAKE_CXX_COMPILER_LAUNCHER=${{ env.CMAKE_CXX_COMPILER_LAUNCHER }} \

cmake/developer_package/compile_flags/os_flags.cmake
@@ -119,8 +119,8 @@ macro(ov_check_compiler_supports_sve flags)
 
     # If the compilation test is successful, set appropriate variables indicating support
     if(CXX_HAS_SVE)
-        set(CXX_SVE_FOUND TRUE CACHE BOOL "SVE available on host")
-        set(CXX_SVE_FOUND TRUE CACHE BOOL "CXX SVE support")
+        set(CXX_SVE_FOUND ON CACHE BOOL "SVE available on host")
+        set(CXX_SVE_FOUND ON CACHE BOOL "CXX SVE support")
         set(CXX_SVE_FLAGS "${flags}" CACHE STRING "CXX SVE flags")
     endif()
 
@@ -129,7 +129,7 @@ macro(ov_check_compiler_supports_sve flags)
 
     # If the compilation test fails, indicate that the support is not found
     if(NOT CXX_SVE_FOUND)
-        set(CXX_SVE_FOUND FALSE CACHE BOOL "CXX SVE support")
+        set(CXX_SVE_FOUND OFF CACHE BOOL "CXX SVE support")
         set(CXX_SVE_FLAGS "" CACHE STRING "CXX SVE flags")
     endif()
 
@@ -143,6 +143,10 @@ endmacro()
 # Provides SSE4.2 compilation flags depending on an OS and a compiler
 #
 macro(ov_sse42_optimization_flags flags)
+    if(NOT ENABLE_SSE42)
+        message(FATAL_ERROR "Internal error: ENABLE_SSE42 if OFF and 'ov_sse42_optimization_flags' must not be called")
+    endif()
+
     if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
         # No such option for MSVC 2019
     elseif(OV_COMPILER_IS_INTEL_LLVM)
@@ -167,6 +171,10 @@ endmacro()
 # Provides AVX2 compilation flags depending on an OS and a compiler
 #
 macro(ov_avx2_optimization_flags flags)
+    if(NOT ENABLE_AVX2)
+        message(FATAL_ERROR "Internal error: ENABLE_AVX2 if OFF and 'ov_avx2_optimization_flags' must not be called")
+    endif()
+
     if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
         set(${flags} /arch:AVX2)
     elseif(OV_COMPILER_IS_INTEL_LLVM)
@@ -189,6 +197,10 @@ endmacro()
 # depending on an OS and a compiler
 #
 macro(ov_avx512_optimization_flags flags)
+    if(NOT ENABLE_AVX512F)
+        message(FATAL_ERROR "Internal error: ENABLE_AVX512F if OFF and 'ov_avx512_optimization_flags' must not be called")
+    endif()
+
     if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
         set(${flags} /arch:AVX512)
     elseif(OV_COMPILER_IS_INTEL_LLVM AND WIN32)
@@ -204,6 +216,10 @@ endmacro()
 # ov_arm_neon_optimization_flags(<output flags>)
 #
 macro(ov_arm_neon_optimization_flags flags)
+    if(NOT (AARCH64 OR ARM))
+        message(FATAL_ERROR "Internal error: platform is not ARM or AARCH64 and 'ov_arm_neon_optimization_flags' must not be called")
+    endif()
+
     if(OV_COMPILER_IS_INTEL_LLVM)
         message(WARNING "Unsupported CXX compiler ${CMAKE_CXX_COMPILER_ID}")
     elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
@@ -234,6 +250,10 @@ endmacro()
 # ov_arm_neon_fp16_optimization_flags(<output flags>)
 #
 macro(ov_arm_neon_fp16_optimization_flags flags)
+    if(NOT ENABLE_NEON_FP16)
+        message(FATAL_ERROR "Internal error: ENABLE_NEON_FP16 if OFF and 'ov_arm_neon_fp16_optimization_flags' must not be called")
+    endif()
+
     if(CMAKE_CXX_COMPILER_ID STREQUAL "Intel" OR CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
         message(WARNING "Unsupported CXX compiler ${CMAKE_CXX_COMPILER_ID} for arm64 platform")
     elseif(ANDROID)
@@ -258,6 +278,10 @@ endmacro()
 # ov_arm_sve_optimization_flags(<output flags>)
 #
 macro(ov_arm_sve_optimization_flags flags)
+    if(NOT ENABLE_SVE)
+        message(FATAL_ERROR "Internal error: ENABLE_SVE if OFF and 'ov_arm_sve_optimization_flags' must not be called")
+    endif()
+
     # Check for compiler SVE support
     ov_check_compiler_supports_sve("-march=armv8-a+sve")
     if(OV_COMPILER_IS_INTEL_LLVM)

src/core/reference/CMakeLists.txt
@@ -24,9 +24,7 @@ set_target_properties(${TARGET_NAME} PROPERTIES EXPORT_NAME reference)
 if(ENABLE_AVX2)
     ov_avx2_optimization_flags(avx2_flags)
 
-    set(OV_REFERENCE_X86_AVX2_SRC
-        ${CMAKE_CURRENT_SOURCE_DIR}/src/op/convert_x86_intrinsics.cpp
-    )
+    set(OV_REFERENCE_X86_AVX2_SRC ${CMAKE_CURRENT_SOURCE_DIR}/src/op/convert_x86_intrinsics.cpp)
     set_source_files_properties(${OV_REFERENCE_X86_AVX2_SRC} PROPERTIES COMPILE_OPTIONS "${avx2_flags}"
                                                                         SKIP_UNITY_BUILD_INCLUSION ON
                                                                         SKIP_PRECOMPILE_HEADERS ON)

src/plugins/intel_cpu/tests/unit/vectorized/CMakeLists.txt
@@ -66,12 +66,20 @@ if (ENABLE_SNIPPETS_LIBXSMM_TPP)
     target_include_directories(${TARGET_NAME} SYSTEM PRIVATE $<TARGET_PROPERTY:xsmm,INCLUDE_DIRECTORIES>)
 endif()
 
-if (X86_64)
-    ov_avx2_optimization_flags(avx2_flags)
-    ov_avx512_optimization_flags(avx512_flags)
+if(X86_64)
+    if(ENABLE_AVX2)
+        ov_avx2_optimization_flags(avx2_flags)
+        list(APPEND compile_flags ${avx2_flags})
+        list(APPEND definitions HAVE_AVX2)
+    endif()
+    if(ENABLE_AVX512F)
+        ov_avx512_optimization_flags(avx512_flags)
+        list(APPEND compile_flags ${avx512_flags})
+        list(APPEND definitions HAVE_AVX512F)
+    endif()
 
-    target_compile_options(${TARGET_NAME} PRIVATE "${avx2_flags};${avx512_flags}")
-    target_compile_definitions(${TARGET_NAME} PRIVATE HAVE_AVX2 HAVE_AVX512F)
+    target_compile_options(${TARGET_NAME} PRIVATE "${compile_flags}")
+    target_compile_definitions(${TARGET_NAME} PRIVATE ${definitions})
 endif()
 
 

src/plugins/intel_cpu/tests/unit/vectorized/paged_attn_cache_rotation.cpp
@@ -416,6 +416,7 @@ protected:
 
         switch (instruction_set) {
             using namespace ov::Extensions::Cpu::XARCH;
+#if defined(HAVE_AVX2)
         case TargetInstructionSet::AVX2:
             rotate_kv_cache_chunk_avx2(chunk_x.data(),
                                        chunk_y.data(),
@@ -424,6 +425,8 @@ protected:
                                        num_elements_to_process,
                                        /* is_tail = */ num_elements_to_process < vec_len_f32_avx2);
             break;
+#endif
+#if defined(HAVE_AVX512F)
         case TargetInstructionSet::AVX512:
             rotate_kv_cache_chunk_avx512(chunk_x.data(),
                                          chunk_y.data(),
@@ -432,6 +435,7 @@ protected:
                                          num_elements_to_process,
                                          /* is_tail = */ num_elements_to_process < vec_len_f32_avx512);
             break;
+#endif
         default:
             FAIL() << "unknown target instruction set";
         }
@@ -454,6 +458,7 @@ TEST_P(CacheRotationKernelInstructionParameterizedTest, OptChunkRotationGivesRef
     test_chunk_rotation_for_type<ov::bfloat16>();
 }
 
+#if defined(HAVE_AVX2) || defined(HAVE_AVX512F)
 auto TEST_STRUCT_TO_NAME_FN =
     [](const testing::TestParamInfo<CacheRotationKernelInstructionParameterizedTest::ParamType>& info) {
         size_t num_elts = std::get<1>(info.param);
@@ -465,19 +470,24 @@ auto TEST_STRUCT_TO_NAME_FN =
         }
         return std::string("unknown");
     };
+#endif
 
+#if defined(HAVE_AVX2)
 INSTANTIATE_TEST_SUITE_P(AVX2,
                          CacheRotationKernelInstructionParameterizedTest,
                          ::testing::Combine(::testing::Values(TargetInstructionSet::AVX2),
                                             ::testing::Range(size_t(0),
                                                              ov::Extensions::Cpu::XARCH::vec_len_f32_avx2 + 1)),
                          TEST_STRUCT_TO_NAME_FN);
+#endif
+#if defined(HAVE_AVX512F)
 INSTANTIATE_TEST_SUITE_P(AVX512,
                          CacheRotationKernelInstructionParameterizedTest,
                          ::testing::Combine(::testing::Values(TargetInstructionSet::AVX512),
                                             ::testing::Range(size_t(0),
                                                              ov::Extensions::Cpu::XARCH::vec_len_f32_avx512 + 1)),
                          TEST_STRUCT_TO_NAME_FN);
+#endif
 
 TYPED_TEST_P(CacheRotationKernelInputTypeParameterizedTest, OptBlockRotationGivesReferenceResults) {
     auto raw_cache_mem_ptr = this->cache_mem.data();