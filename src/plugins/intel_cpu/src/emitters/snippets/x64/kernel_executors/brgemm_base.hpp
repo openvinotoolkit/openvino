68692cc4f5a65b740c2c0505f42f1cf7d317ab47
chenhu-wang <chenhu.wang@intel.com>
2025-01-24 17:33:49 +0800

    apply Alexandra comments

---
+++ src/plugins/intel_cpu/src/emitters/snippets/x64/kernel_executors/brgemm_base.hpp
@@ -0,0 +1,113 @@
+// Copyright (C) 2020-2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <cpu/x64/brgemm/brgemm.hpp>
+
+#include "cpu/x64/cpu_isa_traits.hpp"
+#include "emitters/snippets/brgemm_base.hpp"
+
+namespace ov {
+namespace intel_cpu {
+
+struct BrgemmBaseKernelConfig_x64 : public BrgemmBaseKernelConfig {
+public:
+    BrgemmBaseKernelConfig_x64() = default;
+
+    size_t hash() const override {
+        return m_hash;
+    }
+
+    bool operator==(const BrgemmBaseKernelConfig_x64& rhs) const;
+    bool operator!=(const BrgemmBaseKernelConfig_x64& rhs) const {
+        return !(*this == rhs);
+    }
+
+    void update(int64_t M, int64_t N, int64_t K, int64_t LDA, int64_t LDB, int64_t LDC, float beta);
+
+    dnnl_data_type_t get_dt_in0() const {
+        return get_static_params()->dt_in0;
+    }
+    dnnl_data_type_t get_dt_in1() const {
+        return get_static_params()->dt_in1;
+    }
+
+    dnnl::impl::cpu::x64::cpu_isa_t get_isa() const {
+        return get_static_params()->isa;
+    }
+
+#ifdef SNIPPETS_DEBUG_CAPS
+    std::string to_string() const override;
+#endif
+
+protected:
+    struct StaticBaseParams {
+        StaticBaseParams(const element::Type& in0_dtype,
+                         const element::Type& in1_dtype,
+                         dnnl::impl::cpu::x64::cpu_isa_t primitive_isa,
+                         size_t hash_seed);
+        virtual ~StaticBaseParams() = default;
+
+        const dnnl_data_type_t dt_in0{dnnl_f32}, dt_in1{dnnl_f32};
+        const dnnl::impl::cpu::x64::cpu_isa_t isa{dnnl::impl::cpu::x64::isa_undef};
+
+        size_t hash() const {
+            return m_hash;
+        }
+
+        bool operator==(const StaticBaseParams& rhs) const;
+        bool operator!=(const StaticBaseParams& rhs) const {
+            return !(*this == rhs);
+        }
+#ifdef SNIPPETS_DEBUG_CAPS
+        std::string to_string() const;
+#endif
+    protected:
+        static size_t compute_hash(size_t hash_seed,
+                                   dnnl_data_type_t dt_in0,
+                                   dnnl_data_type_t dt_in1,
+                                   dnnl::impl::cpu::x64::cpu_isa_t isa);
+
+        const size_t m_hash{0};
+    };
+
+    virtual std::shared_ptr<StaticBaseParams> get_static_params() const = 0;
+    size_t compute_hash() const;
+    size_t m_hash{SIZE_MAX};
+};
+
+class BrgemmBaseKernelExecutor_x64 : public BrgemmBaseKernelExecutor {
+public:
+    virtual ~BrgemmBaseKernelExecutor_x64() = default;
+
+protected:
+    static void update_config(const ov::snippets::lowered::ExpressionPtr& expr,
+                              const ov::snippets::lowered::LinearIRCPtr& linear_ir,
+                              BrgemmBaseKernelConfig_x64& config);
+
+    static void create_brgemm_kernel(std::shared_ptr<dnnl::impl::cpu::x64::brgemm_kernel_t>& kernel,
+                                     dnnl_data_type_t dt0,
+                                     dnnl_data_type_t dt1,
+                                     dnnl::impl::cpu::x64::cpu_isa_t isa,
+                                     dnnl_dim_t M,
+                                     dnnl_dim_t N,
+                                     dnnl_dim_t K,
+                                     dnnl_dim_t LDA,
+                                     dnnl_dim_t LDB,
+                                     dnnl_dim_t LDC,
+                                     float beta,
+                                     bool with_amx = false,
+                                     char* palette = nullptr);
+
+    static void execute_brgemm_kernel(const std::shared_ptr<dnnl::impl::cpu::x64::brgemm_kernel_t>& kernel,
+                                      const void* src,
+                                      const void* wei,
+                                      void* dst,
+                                      void* scratch,
+                                      bool with_comp);
+};
+
+}  // namespace intel_cpu
+}  // namespace ov
