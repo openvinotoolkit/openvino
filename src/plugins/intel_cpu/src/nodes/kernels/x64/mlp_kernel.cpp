68368f5ff511ad18f5f097a808d3f05288a269b3
Vladislav Golubev <vladislav.golubev@intel.com>
2025-01-27 11:46:24 +0100

    [CPU] Fix warning in debug build

--- src/plugins/intel_cpu/src/nodes/kernels/x64/mlp_kernel.cpp
+++ src/plugins/intel_cpu/src/nodes/kernels/x64/mlp_kernel.cpp
@@ -503,7 +503,7 @@ void MKernel::run(int M,  // actual M
 }
 
 void MatrixDynQuantPerRow::quantize(size_t BM, ov::bfloat16* psrc, int src_stride) {
-    assert(BM <= M);
+    assert(static_cast<decltype(M)>(BM) <= M);
     parallel_nt_static(0, [&](const size_t ithr, const size_t nthr) {
         size_t start{0}, end{0};
         splitter(BM, nthr, ithr, start, end);
@@ -520,7 +520,7 @@ void MatrixDynQuantPerRow::quantize(size_t BM, ov::bfloat16* psrc, int src_strid
 }
 
 void MatrixDynQuantPerRow::quantize(size_t BM, ov::float16* psrc, int src_stride) {
-    assert(BM <= M);
+    assert(static_cast<decltype(M)>(BM) <= M);
     parallel_nt_static(0, [&](const size_t ithr, const size_t nthr) {
         size_t start{0}, end{0};
         splitter(BM, nthr, ithr, start, end);
