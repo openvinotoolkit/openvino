7d3ca0813130b29514e5340196371d707bb5760b
Vladislav Golubev <vladislav.golubev@intel.com>
2025-01-28 17:09:16 +0100

    [CPU] Fix warning in debug build (#28691)
    ### Details:
    Fix "-Wsign-compare" warning in CPU plugin debug build
    
    ### Tickets:
     - *N\A*

--- src/plugins/intel_cpu/src/nodes/kernels/x64/mlp_kernel.cpp
+++ src/plugins/intel_cpu/src/nodes/kernels/x64/mlp_kernel.cpp
@@ -504,7 +504,7 @@ void MKernel::run(int M,  // actual M
 }
 
 void MatrixDynQuantPerRow::quantize(size_t BM, ov::bfloat16* psrc, int src_stride) {
-    assert(BM <= M);
+    assert(static_cast<int64_t>(BM) <= M);
     parallel_nt_static(0, [&](const size_t ithr, const size_t nthr) {
         size_t start{0}, end{0};
         splitter(BM, nthr, ithr, start, end);
@@ -521,7 +521,7 @@ void MatrixDynQuantPerRow::quantize(size_t BM, ov::bfloat16* psrc, int src_strid
 }
 
 void MatrixDynQuantPerRow::quantize(size_t BM, ov::float16* psrc, int src_stride) {
-    assert(BM <= M);
+    assert(static_cast<int64_t>(BM) <= M);
     parallel_nt_static(0, [&](const size_t ithr, const size_t nthr) {
         size_t start{0}, end{0};
         splitter(BM, nthr, ithr, start, end);
