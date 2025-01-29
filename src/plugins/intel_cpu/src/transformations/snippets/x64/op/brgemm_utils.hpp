eb2fb67ac3ddf3504bc8755adb12e5c2628fd6d6
Alexandra Sidorova <alexandra.sidorova@intel.com>
2025-01-29 09:57:05 +0400

    [Snippets][CPU] Created compute_repacked_n_dim helper

--- src/plugins/intel_cpu/src/transformations/snippets/x64/op/brgemm_utils.hpp
+++ src/plugins/intel_cpu/src/transformations/snippets/x64/op/brgemm_utils.hpp
@@ -60,6 +60,14 @@ namespace repacking {
 size_t compute_inner_n_block(const ov::element::Type& precision);
 /// \brief  Computes inner K block size used by OneDNN implementation. Depends on tensor precision
 size_t compute_inner_k_block(const ov::element::Type& precision);
+
+/// \brief  Computes N dim in output blocked shape of BrgemmCopyB. Depends on tensor precision
+template <
+    typename T,
+    typename = typename std::enable_if<(std::is_same<T, size_t>::value || std::is_same<T, int64_t>::value), bool>::type>
+inline T compute_repacked_n_dim(T n, const ov::element::Type& precision) {
+    return ov::snippets::utils::rnd_up(n, static_cast<T>(compute_inner_n_block(precision)));
+}
 /**
  * @brief Computes leading dimension (LDB) which must be used in brgemm and brgemm_copy_b emitters
  * @param n_block N block size shared between BrgemmCPU and BrgemmCopyB node
@@ -68,8 +76,8 @@ size_t compute_inner_k_block(const ov::element::Type& precision);
 template <
     typename T,
     typename = typename std::enable_if<(std::is_same<T, size_t>::value || std::is_same<T, int64_t>::value), bool>::type>
-T compute_LDB(T n_block, const ov::element::Type& precision) {
-    return ov::snippets::utils::rnd_up(n_block, static_cast<T>(compute_inner_n_block(precision)));
+inline T compute_LDB(T n_block, const ov::element::Type& precision) {
+    return compute_repacked_n_dim(n_block, precision);
 }
 /**
  * @brief Retrieves the expression pointer for the brgemm_copy_b expression corresponding to the given BrgemmCPU
