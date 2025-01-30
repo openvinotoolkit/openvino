53c02aaa33fd6b0a30302f0aaf673fdb29e55f92
p-wysocki <przemyslaw.wysocki@intel.com>
2025-01-30 16:10:08 +0100

    WIP
    Signed-off-by: p-wysocki <przemyslaw.wysocki@intel.com>

--- src/core/reference/include/openvino/reference/segment_max.hpp
+++ src/core/reference/include/openvino/reference/segment_max.hpp
@@ -21,12 +21,11 @@ void segment_max(const T* data,
                  const T empty_segment_value) {
     const T_idx num_segments = output_shape[0];
     const size_t inner_dim_size = std::accumulate(data_shape.begin() + 1, data_shape.end(), 1, std::multiplies<size_t>());
-
-    // Initialize max_values with empty_segment_value for each segment
     std::vector<std::vector<T>> max_values(num_segments, std::vector<T>(inner_dim_size, std::numeric_limits<T>::lowest()));
     std::vector<bool> segment_has_values(num_segments, false);
 
     // Iterate over each element in the first dimension
+    T_idx base_idx;
     for (size_t i = 0; i < data_shape[0]; ++i) {
         const T_idx segment_id = segment_ids[i];
         if (segment_id >= num_segments) {
@@ -34,29 +33,19 @@ void segment_max(const T* data,
         }
         segment_has_values[segment_id] = true;
         // Iterate over each element in the inner dimensions
+        base_idx = i * inner_dim_size;
         for (size_t j = 0; j < inner_dim_size; ++j) {
-            const size_t index = i * inner_dim_size + j;
             // Update the maximum value for the current segment and inner dimension
-            max_values[segment_id][j] = std::max(max_values[segment_id][j], data[index]);
+            max_values[segment_id][j] = std::max(max_values[segment_id][j], data[base_idx + j]);
         }
     }
 
     // Populate the output array with the maximum values for each segment
     for (T_idx segment_id = 0; segment_id < num_segments; ++segment_id) {
+        base_idx = segment_id * inner_dim_size;
         for (size_t j = 0; j < inner_dim_size; ++j) {
-            if (segment_has_values[segment_id]) {
-                out[segment_id * inner_dim_size + j] = max_values[segment_id][j];
-            } else {
-                out[segment_id * inner_dim_size + j] = empty_segment_value;
-            }
+            out[base_idx + j] = segment_has_values[segment_id] ? max_values[segment_id][j] : empty_segment_value;
         }
-    }
-
-    for (T_idx segment_id = 0; segment_id < num_segments; ++segment_id) {
-        for (size_t j = 0; j < inner_dim_size; ++j) {
-            std::cout << out[segment_id * inner_dim_size + j] << " ";
-        }
-        std::cout << std::endl;
     }
 }
 
