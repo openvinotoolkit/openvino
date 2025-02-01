2b1bfe7ed2965cb219c7d094d2cc5171018c23c7
Bhuminjay Soni <Soni5Happy@gmail.com>
2025-02-01 18:06:06 +0530

    Update tests/layer_tests/tensorflow_tests/test_tf_SelectV2.py
    Co-authored-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- tests/layer_tests/tensorflow_tests/test_tf_SelectV2.py
+++ tests/layer_tests/tensorflow_tests/test_tf_SelectV2.py
@@ -95,7 +95,6 @@ class TestComplexSelectV2(CommonTFLayerTest):
     @pytest.mark.parametrize("params", test_data_basic)
     @pytest.mark.precommit
     @pytest.mark.nightly
-
     def test_complex_select_v2(self, params, ie_device, precision, ir_version, temp_dir,
                                   use_legacy_frontend):
         if use_legacy_frontend:
