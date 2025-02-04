27844f0f554ed78a2f326fac7b957a2056eb21ca
Jedrzej Hajduczenia <jedrzej.hajduczenia@intel.com>
2025-02-05 00:12:12 +0100

    Add missing include file in GroupNormalizationFusion shared functional subgraph test

--- src/tests/functional/shared_test_classes/include/shared_test_classes/subgraph/group_normalization_fusion.hpp
+++ src/tests/functional/shared_test_classes/include/shared_test_classes/subgraph/group_normalization_fusion.hpp
@@ -5,6 +5,7 @@
 #pragma once
 
 #include "common_test_utils/data_utils.hpp"
+#include "common_test_utils/ov_tensor_utils.hpp"
 #include "common_test_utils/ov_test_utils.hpp"
 #include "functional_test_utils/crash_handler.hpp"
 #include "shared_test_classes/base/ov_subgraph.hpp"
