6d25c4aae52bee142e96a9b8b03b710e240a3876
PiotrKrzem <piotr.krzeminski@intel.com>
2025-02-04 11:56:54 +0000

    [FIX] Merge conflict

--- src/core/tests/type_prop/paged_attention.cpp
+++ src/core/tests/type_prop/paged_attention.cpp
@@ -1,14 +1,14 @@
-// Copyright (C) 2018-2024 Intel Corporation
+// Copyright (C) 2018-2025 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
 
 #include "openvino/op/paged_attention.hpp"
 
 #include <gtest/gtest.h>
-
 #include "common_test_utils/test_assertions.hpp"
 #include "common_test_utils/type_prop.hpp"
 #include "openvino/op/constant.hpp"
+#include "openvino/op/parameter.hpp"
 
 using namespace testing;
 
@@ -17,26 +17,120 @@ namespace test {
 
 class TypePropPagedAttentionV16Test : public TypePropOpTest<op::v16::PagedAttention> {};
 
-TEST_F(TypePropPagedAttentionV16Test, default_ctor) {
-    const auto data = op::v0::Constant::create(element::f64, Shape{2, 2}, {1.0f, 1.0f, 1.0f, 1.0f});
-    const auto op = make_op();
-    op->set_arguments(OutputVector{data});
-    op->validate_and_infer_types();
+TEST_F(TypePropPagedAttentionV16Test, paged_attention_static_13_inputs) {
+    const auto query = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{3, 4});
+    const auto key = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{3, 4});
+    const auto value = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{3, 4});
+    const auto key_cache = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{6, 2, 5, 4});
+    const auto value_cache = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{6, 2, 5, 4});
+    const auto past_lens = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{5});
+    const auto subsequence_begins = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{5});
+    const auto block_indices = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{15});
+    const auto block_indices_begins = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{8});
+    const auto scale = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{});
+    const auto sliding_window = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{});
+    const auto alibi_slopes = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{9});
+    const auto max_context_len = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{});
 
-    EXPECT_EQ(op->get_input_size(), 1);
-    EXPECT_EQ(op->get_output_size(), 1);
-    EXPECT_EQ(op->get_output_element_type(0), element::f64);
-    EXPECT_EQ(op->get_output_partial_shape(0), PartialShape({2, 2}));
+    ov::OutputVector args = {query,
+                             key,
+                             value,
+                             key_cache,
+                             value_cache,
+                             past_lens,
+                             subsequence_begins,
+                             block_indices,
+                             block_indices_begins,
+                             scale,
+                             sliding_window,
+                             alibi_slopes,
+                             max_context_len};
+    const auto op = std::make_shared<op::v16::PagedAttention>(args);
+    EXPECT_EQ(op->get_output_element_type(0), element::f32);
+    EXPECT_EQ(op->get_output_partial_shape(0), (PartialShape{3, 4}));
 }
 
-TEST_F(TypePropPagedAttentionV16Test, input_data_ctor) {
-    const auto data = op::v0::Constant::create(element::i64, Shape{1, 2}, {1.0f, 1.0f});
-    const auto op = make_op(data);
+TEST_F(TypePropPagedAttentionV16Test, paged_attention_static_16_inputs_eviction_per_block) {
+    const auto query = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{3, 4});
+    const auto key = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{3, 4});
+    const auto value = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{3, 4});
+    const auto key_cache = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{6, 2, 5, 4});
+    const auto value_cache = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{6, 2, 5, 4});
+    const auto past_lens = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{5});
+    const auto subsequence_begins = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{5});
+    const auto block_indices = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{15});
+    const auto block_indices_begins = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{8});
+    const auto scale = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{});
+    const auto sliding_window = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{});
+    const auto alibi_slopes = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{9});
+    const auto max_context_len = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{});
 
-    EXPECT_EQ(op->get_input_size(), 1);
-    EXPECT_EQ(op->get_output_size(), 1);
-    EXPECT_EQ(op->get_output_element_type(0), element::i64);
-    EXPECT_EQ(op->get_output_partial_shape(0), PartialShape({1, 2}));
+    const auto rotated_block_indices = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{3});
+    const auto rotation_deltas = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{12, 1});
+    const auto rotation_trig_lut = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{256, 4});
+
+    ov::OutputVector args = {query,
+                             key,
+                             value,
+                             key_cache,
+                             value_cache,
+                             past_lens,
+                             subsequence_begins,
+                             block_indices,
+                             block_indices_begins,
+                             scale,
+                             sliding_window,
+                             alibi_slopes,
+                             max_context_len,
+                             rotated_block_indices,
+                             rotation_deltas,
+                             rotation_trig_lut};
+
+    const auto op = std::make_shared<op::v16::PagedAttention>(args);
+    EXPECT_EQ(op->get_output_element_type(0), element::f32);
+    EXPECT_EQ(op->get_output_partial_shape(0), (PartialShape{3, 4}));
 }
-}  // namespace test
+
+TEST_F(TypePropPagedAttentionV16Test, paged_attention_static_16_inputs_eviction_per_token) {
+    const auto query = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{3, 4});
+    const auto key = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{3, 4});
+    const auto value = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{3, 4});
+    const auto key_cache = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{6, 2, 5, 4});
+    const auto value_cache = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{6, 2, 5, 4});
+    const auto past_lens = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{5});
+    const auto subsequence_begins = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{5});
+    const auto block_indices = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{15});
+    const auto block_indices_begins = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{8});
+    const auto scale = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{});
+    const auto sliding_window = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{});
+    const auto alibi_slopes = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{9});
+    const auto max_context_len = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{});
+
+    const auto rotated_block_indices = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{3});
+    const auto rotation_deltas = std::make_shared<op::v0::Parameter>(element::i32, PartialShape{12, 5});
+    const auto rotation_trig_lut = std::make_shared<op::v0::Parameter>(element::f32, PartialShape{256, 4});
+
+    ov::OutputVector args = {query,
+                             key,
+                             value,
+                             key_cache,
+                             value_cache,
+                             past_lens,
+                             subsequence_begins,
+                             block_indices,
+                             block_indices_begins,
+                             scale,
+                             sliding_window,
+                             alibi_slopes,
+                             max_context_len,
+                             rotated_block_indices,
+                             rotation_deltas,
+                             rotation_trig_lut};
+
+    const auto op = std::make_shared<op::v16::PagedAttention>(args);
+    EXPECT_EQ(op->get_output_element_type(0), element::f32);
+    EXPECT_EQ(op->get_output_partial_shape(0), (PartialShape{3, 4}));
+}
+
+}  // namespace testing
 }  // namespace ov
