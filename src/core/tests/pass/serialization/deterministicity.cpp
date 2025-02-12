0b99f08bf75aca945a17f805b33af70399b669fd
Michal Barnas <michal.barnas@intel.com>
2025-02-12 19:38:15 +0000

    code format

--- src/core/tests/pass/serialization/deterministicity.cpp
+++ src/core/tests/pass/serialization/deterministicity.cpp
@@ -107,10 +107,10 @@ TEST_F(SerializationDeterministicityTest, ModelWithMultipleLayers) {
 #endif
 
 TEST_F(SerializationDeterministicityTest, ModelWithMultipleOutputs) {
-    const std::string model =
-        ov::test::utils::getModelFromTestModelZoo(ov::util::path_join({SERIALIZED_ZOO, "ir/split_equal_parts_2d.xml"}).string());
-    const std::string weights =
-        ov::test::utils::getModelFromTestModelZoo(ov::util::path_join({SERIALIZED_ZOO, "ir/split_equal_parts_2d.bin"}).string());
+    const std::string model = ov::test::utils::getModelFromTestModelZoo(
+        ov::util::path_join({SERIALIZED_ZOO, "ir/split_equal_parts_2d.xml"}).string());
+    const std::string weights = ov::test::utils::getModelFromTestModelZoo(
+        ov::util::path_join({SERIALIZED_ZOO, "ir/split_equal_parts_2d.bin"}).string());
 
     auto expected = ov::test::readModel(model, weights);
     ov::pass::Serialize(m_out_xml_path_1, m_out_bin_path_1).run_on_model(expected);
@@ -126,10 +126,10 @@ TEST_F(SerializationDeterministicityTest, ModelWithMultipleOutputs) {
 }
 
 TEST_F(SerializationDeterministicityTest, ModelWithConstants) {
-    const std::string model =
-        ov::test::utils::getModelFromTestModelZoo(ov::util::path_join({SERIALIZED_ZOO, "ir/add_abc_initializers.xml"}).string());
-    const std::string weights =
-        ov::test::utils::getModelFromTestModelZoo(ov::util::path_join({SERIALIZED_ZOO, "ir/add_abc_initializers.bin"}).string());
+    const std::string model = ov::test::utils::getModelFromTestModelZoo(
+        ov::util::path_join({SERIALIZED_ZOO, "ir/add_abc_initializers.xml"}).string());
+    const std::string weights = ov::test::utils::getModelFromTestModelZoo(
+        ov::util::path_join({SERIALIZED_ZOO, "ir/add_abc_initializers.bin"}).string());
 
     auto expected = ov::test::readModel(model, weights);
     ov::pass::Serialize(m_out_xml_path_1, m_out_bin_path_1).run_on_model(expected);
