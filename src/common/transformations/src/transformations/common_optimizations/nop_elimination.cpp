eeb5347c4a40a5b77ba9c668ed4a90aa506b3a7a
Zhai, Xuejun <xuejun.zhai@intel.com>
2025-02-14 19:34:55 +0800

    Fix test error
    Signed-off-by: Zhai, Xuejun <xuejun.zhai@intel.com>

--- src/common/transformations/src/transformations/common_optimizations/nop_elimination.cpp
+++ src/common/transformations/src/transformations/common_optimizations/nop_elimination.cpp
@@ -519,7 +519,7 @@ pass::EliminateConcatSplit::EliminateConcatSplit() {
     matcher_pass_callback callback = [=](pattern::Matcher& m) {
         const auto& pattern_map = m.get_pattern_map();
         const auto concat = ov::as_type_ptr<ov::op::v0::Concat>(pattern_map.at(pattern_concat));
-        if (!concat || concat->is_dynamic()) {
+        if (!concat || concat->is_dynamic() || concat->get_users().size()) {
             return false;
         }
         const auto concat_users = concat->get_users();
