514f7ab0b7efd84bbf59b4357326ada3300ae6a0
Srinjoy Dutta <114402816+srinjoydutta03@users.noreply.github.com>
2025-02-14 17:47:15 +0530

    [CPU][ARM64] Implemented JIT Emitter for Eltwise Squared Difference Operation (#28989)
    ### Details:
    - Implemented and added jit_squared_difference_emitter derived class for
    element wise squared difference operation
    - Added entry Algorithm::EltwiseSquaredDifference, in executors/aarch64
    as one of the supported algorithms
    - Added entry in the get_supported_precisions and
    create_eltwise_emitters in kernel/aarch64
    - Added `utils::EltwiseTypes::SQUARED_DIFF` in `jit` kernel check in the
    tests
    
    ### Tests:
    Passed local tests using `./bin/arm64/Release/ov_cpu_func_tests
    --gtest_filter="*smoke*Eltwise*SqDiff*"`
    <img width="487" alt="Screenshot 2025-02-14 at 2 04 25 PM"
    src="https://github.com/user-attachments/assets/deaec2f6-0dcd-4764-86ec-c543ac4742d3"
    />
    
    ### Tickets
    - Closes #27502 
    
    CC:
    @a-sidorova

--- src/plugins/intel_cpu/src/nodes/kernels/aarch64/jit_uni_eltwise_generic.cpp
+++ src/plugins/intel_cpu/src/nodes/kernels/aarch64/jit_uni_eltwise_generic.cpp
@@ -702,6 +702,7 @@ std::shared_ptr<jit_emitter> jit_uni_eltwise_generic<isa>::create_eltwise_emitte
         OV_CASE(Algorithm::EltwiseSigmoid, ov::intel_cpu::aarch64::jit_sigmoid_emitter),
         OV_CASE(Algorithm::EltwiseSoftSign, ov::intel_cpu::aarch64::jit_soft_sign_emitter),
         OV_CASE(Algorithm::EltwiseSqrt, ov::intel_cpu::aarch64::jit_sqrt_emitter),
+        OV_CASE(Algorithm::EltwiseSquaredDifference, ov::intel_cpu::aarch64::jit_squared_difference_emitter),
         OV_CASE(Algorithm::EltwiseSubtract, ov::intel_cpu::aarch64::jit_subtract_emitter),
         OV_CASE(Algorithm::EltwiseSwish, ov::intel_cpu::aarch64::jit_swish_emitter),
         OV_CASE(Algorithm::EltwiseTanh, ov::intel_cpu::aarch64::jit_tanh_emitter));
@@ -836,6 +837,7 @@ std::set<std::vector<element::Type>> eltwise_precision_helper::get_supported_pre
               OV_CASE(Algorithm::EltwiseSigmoid, jit_sigmoid_emitter),
               OV_CASE(Algorithm::EltwiseSoftSign, jit_soft_sign_emitter),
               OV_CASE(Algorithm::EltwiseSqrt, jit_sqrt_emitter),
+              OV_CASE(Algorithm::EltwiseSquaredDifference, jit_squared_difference_emitter),
               OV_CASE(Algorithm::EltwiseSubtract, jit_subtract_emitter),
               OV_CASE(Algorithm::EltwiseSwish, jit_swish_emitter),
               OV_CASE(Algorithm::EltwiseTanh, jit_tanh_emitter));
