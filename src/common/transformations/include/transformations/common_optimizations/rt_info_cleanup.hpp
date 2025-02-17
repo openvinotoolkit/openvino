2c3628ed8b3be4c762f894126a9035d2583e2e73
Tikhonov Ivan <ivan.tikhonov@intel.com>
2025-02-17 19:27:13 +0400

    handle decompression attribute

--- src/common/transformations/include/transformations/common_optimizations/rt_info_cleanup.hpp
+++ src/common/transformations/include/transformations/common_optimizations/rt_info_cleanup.hpp
@@ -6,29 +6,33 @@
 
 #include "openvino/pass/graph_rewrite.hpp"
 #include "transformations/rt_info/dequantization_node.hpp"
+#include "transformations/rt_info/decompression.hpp"
 #include "transformations_visibility.hpp"
 
 namespace ov::pass {
 
 /**
  * @ingroup ov_transformation_common_api
- * @brief RTInfoCleanup erases "disable_const_folding" and "dequantization" attributes from
+ * @brief RTInfoCleanup erases "disable_const_folding", "dequantization" and "decompression" attributes from
  * all ops in the ov model.
  */
 class TRANSFORMATIONS_API RTInfoCleanup : public ov::pass::MatcherPass {
 public:
     OPENVINO_MATCHER_PASS_RTTI("RTInfoCleanup");
     explicit RTInfoCleanup() {
+        using namespace ov::pass;
+
         MATCHER_SCOPE(RTInfoCleanup);
-        auto any_op = std::make_shared<ov::pass::pattern::op::True>();
-        ov::matcher_pass_callback callback = [=](ov::pass::pattern::Matcher& m) {
+        auto any_op = std::make_shared<pattern::op::True>();
+        ov::matcher_pass_callback callback = [=](pattern::Matcher& m) {
             auto root = m.get_match_root();
-            ov::pass::enable_constant_folding(root);
+            enable_constant_folding(root);
             unmark_dequantization_node(root);
+            unmark_as_decompression(root);
             return true;
         };
 
-        auto m = std::make_shared<ov::pass::pattern::Matcher>(any_op, "RTInfoCleanup");
+        auto m = std::make_shared<pattern::Matcher>(any_op, "RTInfoCleanup");
         this->register_matcher(m, callback);
     }
 };
