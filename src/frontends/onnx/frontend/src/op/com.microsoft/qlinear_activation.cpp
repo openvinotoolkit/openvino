90c93bb0b9f2c6e6161412b8e586edfa002e6360
Vatsala Shanubhag <vatsala.shanubhag@intel.com>
2025-02-18 21:56:42 +0530

    [ONNX] Added QLinearAveragePool from com.microsoft domain (#28799)
    Details:
    Microsoft Contrib Operator "QLinearAveragePool" for ONNX RT
    
    Tickets:
    N/A
    
    ---------
    
    Co-authored-by: Georgy Krivoruchko <georgy.krivoruchko@intel.com>

--- src/frontends/onnx/frontend/src/op/com.microsoft/qlinear_activation.cpp
+++ src/frontends/onnx/frontend/src/op/com.microsoft/qlinear_activation.cpp
@@ -6,6 +6,7 @@
 #include "exceptions.hpp"
 #include "openvino/frontend/exception.hpp"
 #include "openvino/op/add.hpp"
+#include "openvino/op/avg_pool.hpp"
 #include "openvino/op/constant.hpp"
 #include "openvino/op/convert.hpp"
 #include "openvino/op/divide.hpp"
@@ -57,21 +58,12 @@ ov::OutputVector qlinear_activation(const ov::frontend::onnx::Node& node, const 
 }
 
 ov::OutputVector qlinear_sigmoid(const ov::frontend::onnx::Node& node) {
-    // Original documentation:
-    // https://github.com/microsoft/onnxruntime/blob/main/docs/ContribOperators.md#commicrosoftqlinearsigmoid
-    // f(x) = quantize(Sigmoid(dequantize(x)))
-
     return qlinear_activation(node, [](const std::shared_ptr<ov::Node>& input_dequantized) {
         return std::make_shared<v0::Sigmoid>(input_dequantized);
     });
 }
 
 ov::OutputVector qlinear_leaky_relu(const ov::frontend::onnx::Node& node) {
-    // Original documentation:
-    // https://github.com/microsoft/onnxruntime/blob/main/docs/ContribOperators.md#commicrosoftqlinearleakyrelu
-    // f(x) = quantize(alpha * dequantize(x)) for x < 0,
-    //        quantize(dequantize(x)) for x >= 0
-
     return qlinear_activation(node, [&](const std::shared_ptr<ov::Node>& input_dequantized) {
         auto alpha =
             v0::Constant::create(input_dequantized->get_element_type(), {}, {node.get_attribute_value<float>("alpha")});
@@ -80,10 +72,44 @@ ov::OutputVector qlinear_leaky_relu(const ov::frontend::onnx::Node& node) {
     });
 }
 
-namespace {
-ONNX_OP("QLinearSigmoid", OPSET_SINCE(1), com_microsoft::opset_1::qlinear_sigmoid, MICROSOFT_DOMAIN);
+ov::OutputVector qlinear_avg_pool(const ov::frontend::onnx::Node& node) {
+    return qlinear_activation(node, [&](const std::shared_ptr<ov::Node>& input_dequantized) {
+        const auto kernel_shape = node.get_attribute_value<std::vector<int64_t>>("kernel_shape");
+        auto strides = node.get_attribute_value<std::vector<int64_t>>("strides", std::vector<int64_t>{1});
+        auto pads = node.get_attribute_value<std::vector<int64_t>>("pads", std::vector<int64_t>{0});
+        const auto ceil_mode = node.get_attribute_value<int64_t>("ceil_mode", 0);
+        const auto count_include_pad = node.get_attribute_value<int64_t>("count_include_pad", 0);
+        const auto auto_pad = node.get_attribute_value<std::string>("auto_pad", "NOTSET");
+
+        const auto input_rank = input_dequantized->get_shape().size();
+        const size_t num_spatial_dims = input_rank - 2;
+
+        pads.resize(num_spatial_dims * 2, pads.size() == 1 ? pads[0] : 0);
+        strides.resize(num_spatial_dims, strides.size() == 1 ? strides[0] : 1);
+
+        auto avg_pool = std::make_shared<v1::AvgPool>(input_dequantized,
+                                                      Strides(strides.begin(), strides.end()),
+                                                      Shape(pads.begin(), pads.begin() + num_spatial_dims),
+                                                      Shape(pads.begin() + num_spatial_dims, pads.end()),
+                                                      Shape(kernel_shape.begin(), kernel_shape.end()),
+                                                      count_include_pad == 0,
+                                                      ceil_mode != 0 ? RoundingType::CEIL : RoundingType::FLOOR,
+                                                      auto_pad == "SAME_UPPER"   ? PadType::SAME_UPPER
+                                                      : auto_pad == "SAME_LOWER" ? PadType::SAME_LOWER
+                                                                                 : PadType::EXPLICIT);
+
+        return avg_pool;
+    });
 }
-ONNX_OP("QLinearLeakyRelu", OPSET_SINCE(1), com_microsoft::opset_1::qlinear_leaky_relu, MICROSOFT_DOMAIN);
+
+bool register_multiple_operators(void) {
+    ONNX_OP_M("QLinearSigmoid", OPSET_SINCE(1), com_microsoft::opset_1::qlinear_sigmoid, MICROSOFT_DOMAIN);
+    ONNX_OP_M("QLinearLeakyRelu", OPSET_SINCE(1), com_microsoft::opset_1::qlinear_leaky_relu, MICROSOFT_DOMAIN);
+    ONNX_OP_M("QLinearAveragePool", OPSET_SINCE(1), com_microsoft::opset_1::qlinear_avg_pool, MICROSOFT_DOMAIN);
+    return true;
+}
+
+static bool registered = register_multiple_operators();
 
 }  // namespace opset_1
 }  // namespace com_microsoft
