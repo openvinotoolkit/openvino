b42c84b30ccaed8cc1568e17c9a711f4933267c0
cyberholics <victorkingoshimua@gmail.com>
2025-02-18 08:13:02 +0100

    Fix aten::ravel: Add input check and update reshape flag

--- src/frontends/pytorch/src/op/ravel.cpp
+++ src/frontends/pytorch/src/op/ravel.cpp
@@ -9,13 +9,17 @@ namespace op {
 
 OutputVector translate_ravel(const NodeContext& context) {
     OV_OP_SCOPE(v0_PyTorch_ravel);
+
+    // Ensure exactly one input is provided
+    num_inputs_check(context, 1, 1);
+
     auto input = context.get_input(0);
     
     // Create shape tensor [-1] for flattening
     auto shape = ov::op::v0::Constant::create(element::i64, Shape{1}, {-1});
     
     // Reshape input tensor to a 1D tensor
-    return {std::make_shared<ov::op::v1::Reshape>(input, shape, true)};
+    return {std::make_shared<ov::op::v1::Reshape>(input, shape, false)};
 }
 
 }  // namespace op
