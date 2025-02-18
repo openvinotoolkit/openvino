c31b732dae838875b4928a7a537de4205ad32de6
Michal Miotk <michal.miotk@intel.com>
2025-02-18 12:31:27 +0000

    scatter el update in dynamic version, fix problem with not same idx when output is padded

--- src/plugins/intel_gpu/src/graph/impls/registry/scatter_elements_update_impls.cpp
+++ src/plugins/intel_gpu/src/graph/impls/registry/scatter_elements_update_impls.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include "predicates.hpp"
 #include "registry.hpp"
 #include "intel_gpu/primitives/scatter_elements_update.hpp"
 #include "primitive_inst.h"
@@ -18,6 +19,7 @@ using namespace cldnn;
 const std::vector<std::shared_ptr<cldnn::ImplementationManager>>& Registry<scatter_elements_update>::get_implementations() {
     static const std::vector<std::shared_ptr<ImplementationManager>> impls = {
         OV_GPU_CREATE_INSTANCE_OCL(ocl::ScatterElementsUpdateImplementationManager, shape_types::static_shape)
+        OV_GPU_CREATE_INSTANCE_OCL(ocl::ScatterElementsUpdateImplementationManager, shape_types::dynamic_shape)
     };
 
     return impls;
