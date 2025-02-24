7cb24dc72ccedb45b95f43306c7611b1a8b56a2f
Elizabeth Pinero <elizabeth.pinero.r@gmail.com>
2025-02-24 06:06:18 -0500

    Added get_class method

--- src/bindings/js/node/src/node_wrap.cpp
+++ src/bindings/js/node/src/node_wrap.cpp
@@ -1,30 +1,43 @@
 #include "node_wrap.h"
 
+// Initialize the static member
 Napi::FunctionReference NodeWrap::constructor;
 
-//NodeWrap
-NodeWrap::NodeWrap(const Napi::CallbackInfo& info, ov::Node* node) : Napi::ObjectWrap<NodeWrap>(info) {
-    this->node_ = node;
+// NodeWrap constructor
+NodeWrap::NodeWrap(const Napi::CallbackInfo& info, ov::Node* node)
+    : Napi::ObjectWrap<NodeWrap>(info), node_(node) {
+    assert(node_ != nullptr && "Node pointer cannot be null");
 }
 
-Napi::Value NodeWrap::get_name(const Napi::CallbackInfo& info) {
+Napi::Value NodeWrap::get_name(const Napi::CallbackInfo& info) const {
+    assert(node_ != nullptr && "Node pointer cannot be null");
     return Napi::String::New(info.Env(), this->node_->get_name());
 }
 
+// Return the Napi::Function that represents the Node class
+Napi::Function NodeWrap::get_class(Napi::Env env) {
+    if (constructor.IsEmpty()) {
+        throw Napi::Error::New(env, "NodeWrap class is not initialized. Call Init first.");
+    }
+    return constructor.Value();
+}
+
 Napi::Object NodeWrap::New(Napi::Env env, ov::Node* node) {
-    Napi::Object obj = Napi::ObjectWrap<NodeWrap>::New(env, node);
+    Napi::Object obj = constructor.New({});
+    NodeWrap* wrap = Napi::ObjectWrap<NodeWrap>::Unwrap(obj);
+    wrap->node_ = node;
     return obj;
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
     Napi::Function func = DefineClass(env, "Node", {
-      InstanceMethod("getName", &NodeWrap::get_name),
-    }, [](const Napi::CallbackInfo& info) {
-        ov::Node* node = info[0].As<Napi::External<ov::Node>>().Data();
-        return NodeWrap::New(env, node);
+        InstanceMethod("getName", &NodeWrap::get_name),
     });
 
-    constructor = Napi::Persistent(func);
+    // Store the class constructor in the static member
+    NodeWrap::constructor = Napi::Persistent(func);
+    NodeWrap::constructor.SuppressDestruct();
+
     exports.Set("Node", func);
     return exports;
 }
