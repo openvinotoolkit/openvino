0ea04f59bf66f059b0501980b72ba7747a846218
Elizabeth Pinero <elizabeth.pinero.r@gmail.com>
2025-02-24 06:30:09 -0500

    Added type definitions

--- src/bindings/js/node/lib/addon.ts
+++ src/bindings/js/node/lib/addon.ts
@@ -221,7 +221,6 @@ interface Model {
    * @param index The index of the output.
    */
   getOutputElementType(index: number): string;
-
   /**
    * It gets the operations (nodes) of the model.
    * @returns An array of Node objects representing the operations in the model.
@@ -285,6 +284,7 @@ interface Node {
    */
   getName(): string;
 }
+
 /**
  * CompiledModel represents a model that is compiled for a specific device
  * by applying multiple optimization transformations,
