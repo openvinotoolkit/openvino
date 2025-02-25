95d04acb44112037fbb5cbbed54847965f07f8ec
Zhang Yi <yi3.zhang@intel.com>
2025-02-25 10:16:02 +0800

    make ConvertPagedAttnInputs  configurable

--- src/common/transformations/src/transformations/common_optimizations/convert_pagedattn_inputs.cpp
+++ src/common/transformations/src/transformations/common_optimizations/convert_pagedattn_inputs.cpp
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "convert_pagedattn_inputs.hpp"
+#include "transformations/common_optimizations/convert_pagedattn_inputs.hpp"
 
 #include <cstdint>
 #include <memory>
@@ -17,7 +17,7 @@
 #include "transformations/utils/utils.hpp"
 using namespace ov::gen_pattern;
 
-ov::intel_cpu::ConvertPagedAttnInputs::ConvertPagedAttnInputs(const Config& config) : m_config(config) {
+ov::pass::ConvertPagedAttnInputs::ConvertPagedAttnInputs(const KVCacheConfig& config) : m_config(config) {
     MATCHER_SCOPE(ConvertPagedAttnInputs);
 
     auto Q = ov::pass::pattern::any_input(ov::pass::pattern::has_static_rank());
@@ -68,11 +68,6 @@ ov::intel_cpu::ConvertPagedAttnInputs::ConvertPagedAttnInputs(const Config& conf
                                                           rotation_deltas,
                                                           rotation_trig_lut});
     auto result = pa_1 | pa_2;
-    auto key_cache_prec = this->m_config.keyCachePrecision;
-    auto value_cache_prec = this->m_config.valueCachePrecision;
-    auto key_cache_gs = this->m_config.keyCacheGroupSize;
-    auto value_cache_gs = this->m_config.valueCacheGroupSize;
-    auto infer_prec = this->m_config.inferencePrecision;
     ov::matcher_pass_callback callback = [=](ov::pass::pattern::Matcher& m) {
         const auto pa_op = m.get_match_root();
         auto key_cache = ov::as_type_ptr<ov::op::v0::Parameter>(pa_op->get_input_node_shared_ptr(3));
@@ -83,10 +78,12 @@ ov::intel_cpu::ConvertPagedAttnInputs::ConvertPagedAttnInputs(const Config& conf
         };
         auto init_cache_shape = [&](const size_t head_nums,
                                     const size_t head_size,
+                                    const size_t block_size,
                                     const ov::element::Type precision,
                                     const size_t group_size,
-                                    const bool bychannel) {
-            ov::Dimension::value_type block_size = 32;
+                                    const bool bychannel,
+                                    const std::vector<size_t>& orders) {
+            size_t _block_size = block_size;
             ov::Dimension::value_type _head_nums = head_nums;
             ov::Dimension::value_type _head_size = head_size;
             ov::Dimension::value_type _group_size = group_size;
@@ -99,17 +96,24 @@ ov::intel_cpu::ConvertPagedAttnInputs::ConvertPagedAttnInputs(const Config& conf
             size_t group_num = _head_size / _group_size;
             if (precision == ov::element::u8) {
                 if (bychannel) {
-                    block_size += 2 * sizeof(float);
+                    _block_size += 2 * sizeof(float);
                 } else {
                     _head_size += sizeof(float) * 2 * group_num;
                 }
             } else if (precision == ov::element::u4) {
                 _head_size += sizeof(float) * 2 * group_num * 2;
             }
-            return ov::PartialShape{-1, _head_nums, block_size, _head_size};
+            auto block_shape = ov::PartialShape::dynamic(4);
+
+            block_shape[orders[0]] = -1;
+            block_shape[orders[1]] = _head_nums;
+            block_shape[orders[2]] = _block_size;
+            block_shape[orders[3]] = _head_size;
+
+            return block_shape;
         };
-        auto key_cache_precision = format_cache_precision(key_cache_prec, infer_prec);
-        auto value_cache_precision = format_cache_precision(value_cache_prec, infer_prec);
+        auto key_cache_precision = format_cache_precision(m_config.keyCachePrecision, m_config.inferencePrecision);
+        auto value_cache_precision = format_cache_precision(m_config.valueCachePrecision, m_config.inferencePrecision);
         key_cache->set_element_type(key_cache_precision);
         value_cache->set_element_type(value_cache_precision);
         if (!pa_op->get_rt_info().count("num_k_heads") || !pa_op->get_rt_info().count("k_head_size") ||
@@ -121,14 +125,18 @@ ov::intel_cpu::ConvertPagedAttnInputs::ConvertPagedAttnInputs(const Config& conf
         }
         const auto key_cache_shape = init_cache_shape(pa_op->get_rt_info()["num_k_heads"].as<size_t>(),
                                                       pa_op->get_rt_info()["k_head_size"].as<size_t>(),
+                                                      m_config.keyCacheBlockSize,
                                                       key_cache_precision,
-                                                      key_cache_gs,
-                                                      false);
+                                                      m_config.keyCacheGroupSize,
+                                                      m_config.keyCacheQuantBychannel,
+                                                      m_config.keyCacheDimOrder);
         const auto value_cache_shape = init_cache_shape(pa_op->get_rt_info()["num_v_heads"].as<size_t>(),
                                                         pa_op->get_rt_info()["v_head_size"].as<size_t>(),
+                                                        m_config.valueCacheBlockSize,
                                                         value_cache_precision,
-                                                        value_cache_gs,
-                                                        false);
+                                                        m_config.valueCacheGroupSize,
+                                                        m_config.valueCacheQuantBychannel,
+                                                        m_config.valueCacheDimOrder);
 
         key_cache->set_partial_shape(key_cache_shape);
         value_cache->set_partial_shape(value_cache_shape);
@@ -137,4 +145,13 @@ ov::intel_cpu::ConvertPagedAttnInputs::ConvertPagedAttnInputs(const Config& conf
 
     auto m = std::make_shared<ov::pass::pattern::Matcher>(result, matcher_name);
     this->register_matcher(m, callback);
+}
+
+
+void ov::pass::ConvertPagedAttnInputs::setKVCacheConfig(const KVCacheConfig& config) {
+    m_config = config;
+}
+
+const ov::pass::ConvertPagedAttnInputs::KVCacheConfig& ov::pass::ConvertPagedAttnInputs::getKVCacheConfig() const {
+    return m_config;
 }
--- src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
+++ src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
@@ -27,6 +27,7 @@
 #include "transformations/common_optimizations/add_fake_quantize_fusion.hpp"
 #include "transformations/common_optimizations/augru_cell_fusion.hpp"
 #include "transformations/common_optimizations/common_optimizations.hpp"
+#include "transformations/common_optimizations/convert_pagedattn_inputs.hpp"
 #include "transformations/common_optimizations/convert_quantize_dequantize.hpp"
 #include "transformations/common_optimizations/fq_mul_fusion.hpp"
 #include "transformations/common_optimizations/fuse_rotary_positional_embeddings.hpp"
@@ -140,7 +141,6 @@
 #include "transformations/cpu_opset/arm/pass/mish_decomposition.hpp"
 #include "transformations/cpu_opset/common/pass/causal_mask_preprocess_fusion.hpp"
 #include "transformations/cpu_opset/common/pass/convert_fq_rnn_to_quantized_rnn.hpp"
-#include "transformations/cpu_opset/common/pass/convert_pagedattn_inputs.hpp"
 #include "transformations/cpu_opset/common/pass/decompose_integer_divide.hpp"
 #include "transformations/cpu_opset/common/pass/decompose_rms_norm.hpp"
 #include "transformations/cpu_opset/common/pass/insert_convert_after_extension.hpp"
@@ -447,7 +447,20 @@ void Transformations::PreLpt(const std::vector<ov::element::Type>& defaultPrecis
 
     CPU_REGISTER_PASS_COMMON(manager, ov::pass::AUGRUCellFusion);
     CPU_REGISTER_PASS_COMMON(manager, SDPASubgraphFusion);
-    CPU_REGISTER_PASS_COMMON(manager, ConvertPagedAttnInputs, config);
+    ov::pass::ConvertPagedAttnInputs::KVCacheConfig cacheConfig;
+    cacheConfig.keyCachePrecision = config.keyCachePrecision;
+    cacheConfig.valueCachePrecision = config.valueCachePrecision;
+    cacheConfig.inferencePrecision = config.inferencePrecision;
+    cacheConfig.keyCacheGroupSize = config.keyCacheGroupSize;
+    cacheConfig.valueCacheGroupSize = config.valueCacheGroupSize;
+    cacheConfig.keyCacheBlockSize = 32;
+    cacheConfig.valueCacheBlockSize = 32;
+    // TODO enable quant_by_channel when available.
+    cacheConfig.keyCacheQuantBychannel = false;
+    cacheConfig.valueCacheQuantBychannel = false;
+    cacheConfig.keyCacheDimOrder = {0, 1, 2, 3};
+    cacheConfig.valueCacheDimOrder = {0, 1, 2, 3};
+    CPU_REGISTER_PASS_COMMON(manager, ov::pass::ConvertPagedAttnInputs, cacheConfig);
     CPU_REGISTER_PASS_COMMON(manager, ov::pass::CommonOptimizations);
     CPU_REGISTER_PASS_X64(manager, ov::pass::KeepConstPrecision, decompression_precisions, false, true);
     CPU_SET_CALLBACK_X64(
