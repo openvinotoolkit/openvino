bfdbeea04146954f60efb039b20013c51cf66a0b
p-wysocki <przemyslaw.wysocki@intel.com>
2025-02-26 17:09:28 +0100

    [WIP] Add cmake check
    Signed-off-by: p-wysocki <przemyslaw.wysocki@intel.com>

---
+++ src/bindings/python/scripts/generate_pyapi_stubs.bat
@@ -0,0 +1,68 @@
+@echo off
+
+REM Copyright (C) 2018-2025 Intel Corporation
+REM SPDX-License-Identifier: Apache-2.0
+
+setlocal EnableDelayedExpansion
+
+REM Invalid expressions
+set "invalid_expressions=ov::op::v1::Add ov::op::v1::Divide ov::op::v1::Multiply ov::op::v1::Subtract ov::op::v1::Divide ov::Node ov::Input<ov::Node> ov::descriptor::Tensor <Type: 'undefined'> ov::Output<ov::Node const> ov::float16 ov::EncryptionCallbacks ov::streams::Num <Dimension:"
+
+REM Invalid identifiers
+set "invalid_identifiers=<locals>"
+
+REM Unresolved names
+set "unresolved_names=InferRequestWrapper RemoteTensorWrapper capsule VASurfaceTensorWrapper _abc._abc_data"
+
+REM Function to escape characters for regex
+:escape_characters
+set "escaped_error=%~1"
+set "escaped_error=!escaped_error:\=\\!"
+set "escaped_error=!escaped_error:/=\/!"
+set "escaped_error=!escaped_error:$=\$!"
+set "escaped_error=!escaped_error:*=\*!"
+set "escaped_error=!escaped_error:.=\.!"
+set "escaped_error=!escaped_error:^=\^!"
+set "escaped_error=!escaped_error:|=\|!"
+exit /b 0
+
+REM Create regex pattern
+set "invalid_expressions_regex="
+for %%e in (%invalid_expressions%) do (
+    call :escape_characters "%%e"
+    set "invalid_expressions_regex=!invalid_expressions_regex!.*!escaped_error!.*|"
+)
+set "invalid_expressions_regex=%invalid_expressions_regex:~0,-1%"
+
+set "invalid_identifiers_regex="
+for %%e in (%invalid_identifiers%) do (
+    call :escape_characters "%%e"
+    set "invalid_identifiers_regex=!invalid_identifiers_regex!.*!escaped_error!.*|"
+)
+set "invalid_identifiers_regex=%invalid_identifiers_regex:~0,-1%"
+
+set "unresolved_names_regex="
+for %%e in (%unresolved_names%) do (
+    call :escape_characters "%%e"
+    set "unresolved_names_regex=!unresolved_names_regex!.*!escaped_error!.*|"
+)
+set "unresolved_names_regex=%unresolved_names_regex:~0,-1%"
+
+REM Set the output directory
+if "%~1"=="" (
+    set "output_dir=%~dp0.."
+) else (
+    set "output_dir=%~1"
+)
+
+python -m pybind11_stubgen --output-dir "%output_dir%" --root-suffix "" --ignore-invalid-expressions "%invalid_expressions_regex%" --ignore-invalid-identifiers "%invalid_identifiers_regex%" --ignore-unresolved-names "%unresolved_names_regex%" --numpy-array-use-type-var --exit-code openvino
+
+REM Workaround for pybind11-stubgen issue where it doesn't import some modules for stubs generated from .py files
+set "pyi_file=%output_dir%\openvino\_ov_api.pyi"
+if exist "%pyi_file%" (
+    powershell -Command "(gc '%pyi_file%') -replace '(^.*$)', 'import typing`r`nimport pathlib`r`n$1' | Out-File -encoding ASCII '%pyi_file%'"
+) else (
+    echo File %pyi_file% not found.
+)
+
+endlocal
---
+++ src/bindings/python/scripts/generate_pyapi_stubs.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+# Copyright (C) 2018-2025 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+invalid_expressions=(
+    # The classes bindings' will be soon added to pyopenvino. Ticket: 163077
+    "ov::op::v1::Add"
+    "ov::op::v1::Divide"
+    "ov::op::v1::Multiply"
+    "ov::op::v1::Subtract"
+    "ov::op::v1::Divide"
+
+    # Circular dependency Input/Output/Node/Tensor. Ticket: 163078
+    "ov::Node"
+    "ov::Input<ov::Node>"
+    "ov::descriptor::Tensor"
+    "<Type: 'undefined'>"
+    "ov::Output<ov::Node const>"
+
+    # New bindings required, ticket: 163094
+    "ov::float16"
+    "ov::EncryptionCallbacks"
+    "ov::streams::Num"
+
+    # Other issues, ticket: 163094
+    "<Dimension:"
+)
+
+invalid_identifiers=(
+    # Ticket: 163093
+    "<locals>"
+)
+
+unresolved_names=(
+    # Circular dependencies, ticket: 163078
+    "InferRequestWrapper"
+
+    # Other issues, ticket: 163094
+    "RemoteTensorWrapper"
+    "capsule"
+    "VASurfaceTensorWrapper"
+    "_abc._abc_data"
+)
+
+create_regex_pattern() {
+    local errors=("$@")
+    local regex_pattern=""
+
+    for error in "${errors[@]}"; do
+        escaped_error=$(printf '%s\n' "$error" | sed -e 's/[]\/$*.^|[]/\\&/g')
+        regex_pattern+=".*$escaped_error.*|"
+    done
+    regex_pattern="${regex_pattern%|}"
+    echo "$regex_pattern"
+}
+
+invalid_expressions_regex=$(create_regex_pattern "${invalid_expressions[@]}")
+invalid_identifiers_regex=$(create_regex_pattern "${invalid_identifiers[@]}")
+unresolved_names_regex=$(create_regex_pattern "${unresolved_names[@]}")
+
+# Set the output directory
+if [ -z "$1" ]; then
+    output_dir="$(dirname "$0")/../src"
+else
+    output_dir="$1"
+fi
+
+python -m pybind11_stubgen \
+            --output-dir "$output_dir" \
+            --root-suffix "" \
+            --ignore-invalid-expressions "$invalid_expressions_regex" \
+            --ignore-invalid-identifiers "$invalid_identifiers_regex" \
+            --ignore-unresolved-names "$unresolved_names_regex" \
+            --numpy-array-use-type-var \
+            --exit-code \
+            openvino
+
+# Check if the command was successful
+if [ $? -ne 0 ]; then
+    echo "Error: pybind11-stubgen failed."
+    exit 1
+fi
+
+# Check if the stubs were actually generated
+if [ "$(ls -A $output_dir/openvino)" ]; then
+    echo "Stub files generated successfully."
+else
+    echo "No stub files were generated."
+    exit 1
+fi
+
+# Workaround for pybind11-stubgen issue where it doesn't import some modules for stubs generated from .py files
+# Ticket: 163225
+pyi_file="$output_dir/openvino/_ov_api.pyi"
+if [ -f "$pyi_file" ]; then
+    sed -i '2i import typing' "$pyi_file"
+    sed -i '2i import pathlib' "$pyi_file"
+else
+    echo "File $pyi_file not found."
+    exit 1
+fi
+echo "Stub files generated in $output_dir"
