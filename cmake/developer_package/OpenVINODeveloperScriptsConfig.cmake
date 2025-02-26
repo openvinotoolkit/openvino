37b27cfc68f26c0fc7d54944b55da15bde303d7e
Ilya Lavrenov <ilya.lavrenov@intel.com>
2025-02-26 15:22:04 +0100

    Avoid flags duplication

--- cmake/developer_package/OpenVINODeveloperScriptsConfig.cmake
+++ cmake/developer_package/OpenVINODeveloperScriptsConfig.cmake
@@ -205,6 +205,8 @@ set(CMAKE_POLICY_DEFAULT_CMP0026 NEW)
 set(CMAKE_POLICY_DEFAULT_CMP0042 NEW)
 # CMake 3.1+: Simplify variable reference and escape sequence evaluation.
 set(CMAKE_POLICY_DEFAULT_CMP0053 NEW)
+# CMake 3.3+: Honor visibility properties for all target types
+set(CMAKE_POLICY_DEFAULT_CMP0063 NEW)
 # CMake 3.9+: `RPATH` settings on macOS do not affect `install_name`.
 set(CMAKE_POLICY_DEFAULT_CMP0068 NEW)
 # CMake 3.12+: find_package() uses <PackageName>_ROOT variables.
