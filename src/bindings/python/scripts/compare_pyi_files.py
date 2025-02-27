6bf37846cc1d45eb1346c2b59a54f2b5d587ed7c
p-wysocki <przemyslaw.wysocki@intel.com>
2025-02-27 18:31:00 +0100

    Regex out the version string
    Signed-off-by: p-wysocki <przemyslaw.wysocki@intel.com>

--- src/bindings/python/scripts/compare_pyi_files.py
+++ src/bindings/python/scripts/compare_pyi_files.py
@@ -28,8 +28,11 @@ def normalize_imports(lines):
 
 
 # Ignore differences in memory addresses like in function _get_node_factory at 0x7fdad9f53640
-def remove_memory_addresses(lines):
-    return [re.sub(r'at 0x[0-9a-fA-F]+', 'at <memory_address>', line) for line in lines]
+# Ignore differences in version strings like __version__: str = '2025.1.0-18207-10035956d7f-pyi_files'
+def remove_unique_strings(lines):
+    lines = [re.sub(r'at 0x[0-9a-fA-F]+', 'at <memory_address>', line) for line in lines]
+    lines = [re.sub(r'__version__: str = \'[^\']+\'', '__version__: str = \'<version_string>\'', line) for line in lines]
+    return lines
 
 
 def compare_files(file1, file2):
@@ -42,8 +45,8 @@ def compare_files(file1, file2):
         normalized_lines1 = normalize_imports(lines1)
         normalized_lines2 = normalize_imports(lines2)
 
-        normalized_lines1 = remove_memory_addresses(normalized_lines1)
-        normalized_lines2 = remove_memory_addresses(normalized_lines2)
+        normalized_lines1 = remove_unique_strings(normalized_lines1)
+        normalized_lines2 = remove_unique_strings(normalized_lines2)
         # DEBUG: Display differences, this fails on CI for one init and I'm not sure why
         if normalized_lines1 != normalized_lines2:
             for line1, line2 in zip(normalized_lines1, normalized_lines2):
