66162bdecee2fbd8437a3256c1860923dcf342d9
Maxim Vafin <maxim.vafin@intel.com>
2025-02-28 00:24:55 +0100

    Disable test
    Signed-off-by: Maxim Vafin <maxim.vafin@intel.com>

--- tests/layer_tests/pytorch_tests/test_arange.py
+++ tests/layer_tests/pytorch_tests/test_arange.py
@@ -109,7 +109,6 @@ class TestArange(PytorchLayerTest):
 
     @pytest.mark.nightly
     @pytest.mark.precommit_torch_export
-    @pytest.mark.precommit_fx_backend
     @pytest.mark.parametrize("dtype", [None,
                                        skip_if_export("float32"),
                                        skip_if_export("float64"),
@@ -121,28 +120,25 @@ class TestArange(PytorchLayerTest):
     @pytest.mark.parametrize("use_out", [skip_check(True), False])
     def test_arange_end_only(self, dtype, end, use_out, ie_device, precision, ir_version):
         self._test(*self.create_model(dtype, 1, use_out), ie_device, precision, ir_version,
-                   kwargs_to_prepare_input={"end": end}, aot_autograd=True)
+                   kwargs_to_prepare_input={"end": end})
 
     @pytest.mark.nightly
-    @pytest.mark.precommit_fx_backend
     @pytest.mark.parametrize("dtype", [None, "float32", "float64", "int32", "int64", "int8"])
     @pytest.mark.parametrize("start,end", [(0, 1), (-1, 1), (1, 5), (0.5, 2.5)])
     def test_arange_start_end(self, dtype, end, start, ie_device, precision, ir_version):
         self._test(*self.create_model(dtype, 2), ie_device, precision, ir_version,
-                   kwargs_to_prepare_input={"end": end, "start": start, "dtype": dtype}, aot_autograd=True)
+                   kwargs_to_prepare_input={"end": end, "start": start, "dtype": dtype})
 
     @pytest.mark.nightly
     @pytest.mark.precommit
-    @pytest.mark.precommit_fx_backend
     @pytest.mark.parametrize("dtype", [None, "float32", "float64", "int32", "int64", "int8"])
     @pytest.mark.parametrize("start,end,step", [(0, 1, 1), (-2, 1, 1.25), (1, -5, -1), (1, 10, 2), (-1, -5, -2)])
     def test_arange_start_end_step(self, dtype, end, start, step, ie_device, precision, ir_version):
         self._test(*self.create_model(dtype, 3), ie_device, precision, ir_version,
-                   kwargs_to_prepare_input={"end": end, "start": start, "step": step, "dtype": dtype}, aot_autograd=True)
+                   kwargs_to_prepare_input={"end": end, "start": start, "step": step, "dtype": dtype})
 
     @pytest.mark.nightly
     @pytest.mark.precommit_torch_export
-    @pytest.mark.precommit_fx_backend    
     @pytest.mark.parametrize("dtype", [skip_check(None),
                                        skip_if_export("float32"),
                                        skip_if_export("float64"),
@@ -153,21 +149,19 @@ class TestArange(PytorchLayerTest):
     @pytest.mark.parametrize("end", [1, 2, 3])
     def test_arange_end_only_with_prim_dtype(self, dtype, end, ie_device, precision, ir_version):
         self._test(*self.create_model(dtype, 1, False, True), ie_device, precision, ir_version,
-                   kwargs_to_prepare_input={"end": end, "ref_dtype": dtype}, aot_autograd=True)
+                   kwargs_to_prepare_input={"end": end, "ref_dtype": dtype})
 
     @pytest.mark.nightly
-    @pytest.mark.precommit_fx_backend
     @pytest.mark.parametrize("dtype", ["float32", "float64", "int32", "int64", "int8"])
     @pytest.mark.parametrize("start,end", [(0, 1), (-1, 1), (1, 5), (0.5, 2.5)])
     def test_arange_start_end_with_prim_dtype(self, dtype, end, start, ie_device, precision, ir_version):
         self._test(*self.create_model(dtype, 2, ref_dtype=True), ie_device, precision, ir_version,
-                   kwargs_to_prepare_input={"end": end, "start": start, "ref_dtype": dtype}, aot_autograd=True)
+                   kwargs_to_prepare_input={"end": end, "start": start, "ref_dtype": dtype})
 
     @pytest.mark.nightly
     @pytest.mark.precommit
-    @pytest.mark.precommit_fx_backend
     @pytest.mark.parametrize("dtype", ["float32", "float64", "int32", "int64", "int8"])
     @pytest.mark.parametrize("start,end,step", [(0, 1, 1), (-2, 1, 1.25), (1, -5, -1), (1, 10, 2), (-1, -5, -2)])
     def test_arange_start_end_step_with_prim_dtype(self, dtype, end, start, step, ie_device, precision, ir_version):
         self._test(*self.create_model(dtype, 3, ref_dtype=True), ie_device, precision, ir_version,
-                   kwargs_to_prepare_input={"end": end, "start": start, "step": step, "ref_dtype": dtype}, aot_autograd=True)
+                   kwargs_to_prepare_input={"end": end, "start": start, "step": step, "ref_dtype": dtype})
