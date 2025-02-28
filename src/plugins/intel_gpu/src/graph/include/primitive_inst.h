a30bd9dcafa899ceb97cdce977d3b1407f435d93
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2025-02-28 15:10:34 +0400

    [GPU] Removed get_kernels() duplication (#29208)
    Signed-off-by: Vladimir Paramuzov <vladimir.paramuzov@intel.com>

--- src/plugins/intel_gpu/src/graph/include/primitive_inst.h
+++ src/plugins/intel_gpu/src/graph/include/primitive_inst.h
@@ -122,7 +122,6 @@ struct primitive_impl {
     }
 
     virtual void set_kernels(cldnn::kernels_cache::compiled_kernels kernels) {}
-    virtual std::vector<kernel::ptr> get_kernels() { return {}; }
 
     bool need_weights_reorder() const { return _weights_reorder_params != nullptr; }
     std::shared_ptr<WeightsReorderParams> get_weights_reorder_params() const { return _weights_reorder_params; }
