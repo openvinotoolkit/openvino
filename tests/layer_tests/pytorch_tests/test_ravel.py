0a91169b7e9a03933e0b9bcdf2f58e598ea25d97
cyberholics <victorkingoshimua@gmail.com>
2025-03-02 17:09:31 +0100

    Implemented changes on test

--- tests/layer_tests/pytorch_tests/test_ravel.py
+++ tests/layer_tests/pytorch_tests/test_ravel.py
@@ -4,7 +4,6 @@ import numpy as np
 from common.layer_test_class import CommonLayerTest
 from pytorch_layer_test_class import PytorchLayerTest
 
-# Define the test class
 class TestRavel(PytorchLayerTest):
     def _prepare_input(self):
         return (torch.randn(2, 3, 4),)
@@ -14,10 +13,10 @@ class TestRavel(PytorchLayerTest):
             def forward(self, x):
                 return torch.ravel(x)
 
-        return RavelModel(), None
+        return RavelModel(), None, "aten::ravel"
 
     @pytest.mark.precommit
     @pytest.mark.nightly
     @pytest.mark.parametrize("device", ["CPU", "GPU"])
-    def test_ravel(self, device):
-        self._test(*self.create_model(), ie_device=device)
+    def test_ravel(self, ie_device, precision, ir_version):
+        self._test(*self.create_model(), ie_device, precision, ir_version)
