291d79859817a1a1eaab9f2565e3b250a8a22165
Harshitha Manne <141660450+darksapien23151@users.noreply.github.com>
2025-03-05 00:34:11 +0530

    Update nan_to_num.cpp
    Typechecking for the input

--- src/frontends/pytorch/src/op/nan_to_num.cpp
+++ src/frontends/pytorch/src/op/nan_to_num.cpp
@@ -1,30 +1,19 @@
-// Copyright (C) 2018-2024 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
-//
-
-#include "openvino/frontend/pytorch/node_context.hpp"
-#include "openvino/op/convert.hpp"
-#include "openvino/op/select.hpp"
-#include "openvino/op/equal.hpp"
-#include "openvino/op/is_nan.hpp"
-#include "openvino/op/is_inf.hpp"
-#include "openvino/op/constant.hpp"
-
-namespace ov {
-namespace frontend {
-namespace pytorch {
-namespace op {
-
-using namespace ov::op;
-
 OutputVector translate_nan_to_num_fx(const NodeContext& context) {
     num_inputs_check(context, 1, 4);
-    
+
     auto x = context.get_input(0);
-    
-    Output<Node> nan_replacement = v0::Constant::create(element::f32, Shape{}, {0.0f});
-    Output<Node> posinf_replacement = v0::Constant::create(element::f32, Shape{}, {std::numeric_limits<float>::max()});
-    Output<Node> neginf_replacement = v0::Constant::create(element::f32, Shape{}, {std::numeric_limits<float>::lowest()});
+
+    // Checking if input is an integer type
+    if (x.get_element_type().is_integral()) {
+        // Wrapping integer tensor before returning to avoid issues with OutputVector expectations
+        return {context.mark_node(x)};
+    }
+
+    auto x_type = x.get_element_type();
+
+    auto nan_replacement = context.mark_node(v0::Constant::create(x_type, Shape{}, {0}));
+    auto posinf_replacement = context.mark_node(v0::Constant::create(x_type, Shape{}, {std::numeric_limits<float>::max()}));
+    auto neginf_replacement = context.mark_node(v0::Constant::create(x_type, Shape{}, {std::numeric_limits<float>::lowest()}));
 
     if (!context.input_is_none(1)) {
         nan_replacement = context.get_input(1);
@@ -37,9 +26,8 @@ OutputVector translate_nan_to_num_fx(const NodeContext& context) {
     }
 
     auto is_nan = context.mark_node(std::make_shared<v10::IsNan>(x));
-    auto is_posinf = context.mark_node(std::make_shared<v10::IsInf>(x, v10::IsInf::Attributes(/*detect_negative=*/false, /*detect_positive=*/true)));
-    auto is_neginf = context.mark_node(std::make_shared<v10::IsInf>(x, v10::IsInf::Attributes(/*detect_negative=*/true, /*detect_positive=*/false)));
-
+    auto is_posinf = context.mark_node(std::make_shared<v10::IsInf>(x, v10::IsInf::Attributes(false, true)));
+    auto is_neginf = context.mark_node(std::make_shared<v10::IsInf>(x, v10::IsInf::Attributes(true, false)));
 
     auto replaced_nan = context.mark_node(std::make_shared<v1::Select>(is_nan, nan_replacement, x));
     auto replaced_posinf = context.mark_node(std::make_shared<v1::Select>(is_posinf, posinf_replacement, replaced_nan));
@@ -47,8 +35,3 @@ OutputVector translate_nan_to_num_fx(const NodeContext& context) {
 
     return {replaced_neginf};
 };
-
-}  // namespace op
-}  // namespace pytorch
-}  // namespace frontend
-}  // namespace ov
