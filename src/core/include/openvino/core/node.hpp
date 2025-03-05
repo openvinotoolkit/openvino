42b3b18f27f8b6f87c8a50da9b1f92c6285fb4e2
Pawel Raasz <pawel.raasz@intel.com>
2025-03-05 11:08:22 +0100

    [transformations][core] Resolve tensor names collision to be unique (#29092)
    ### Details:
     - Resolve tensor names collision to ensure they are unique.
    
    ### Tickets:
     - [CVS-162938](https://jira.devtools.intel.com/browse/CVS-162938)
    
    ---------
    
    Signed-off-by: Raasz, Pawel <pawel.raasz@intel.com>

--- src/core/include/openvino/core/node.hpp
+++ src/core/include/openvino/core/node.hpp
@@ -115,8 +115,6 @@ class OPENVINO_API Node : public std::enable_shared_from_this<Node> {
     friend class Output;
 
     friend class Model;
-    // To fix collisions in generated friendly name
-    friend class pass::ResolveNameCollisions;
 
 protected:
     descriptor::Input& get_input_descriptor(size_t position);
