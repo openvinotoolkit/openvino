7da6d4ff0b05f20494907e0038ca5af3e4e575f3
Kim, Eddy <eddy.kim@intel.com>
2025-03-05 14:01:37 +0900

    added a temporary transform pass

---
+++ src/plugins/intel_gpu/src/plugin/transformations/swap_mul_transpose.hpp
@@ -0,0 +1,18 @@
+// Copyright (C) 2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include "openvino/pass/graph_rewrite.hpp"
+#include "openvino/pass/pass.hpp"
+
+namespace ov::intel_gpu {
+
+class SwapMulTranspose: public ov::pass::MatcherPass {
+public:
+    OPENVINO_MATCHER_PASS_RTTI("ov::intel_gpu::SwapMulTranspose");
+    SwapMulTranspose();
+};
+
+}   // namespace ov::intel_gpu
