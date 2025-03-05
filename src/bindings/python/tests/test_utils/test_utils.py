5729c6b52795ca279e2daa247c8709ba2e621a83
Oleg Pipikin <oleg.pipikin@intel.com>
2025-03-05 15:40:00 +0100

    Fixes + new tests

--- src/bindings/python/tests/test_utils/test_utils.py
+++ src/bindings/python/tests/test_utils/test_utils.py
@@ -5,19 +5,11 @@
 import pytest
 import os
 import numpy as np
+import openvino as ov
 from pathlib import Path
 from openvino.utils import deprecated, get_cmake_path
 from tests.utils.helpers import compare_models, get_relu_model
 from openvino.utils.postponed_constant import make_postponed_constant
-from openvino import (
-    Model,
-    op,
-    opset13,
-    Shape,
-    Tensor,
-    Type,
-    serialize
-)
 
 def test_compare_functions():
     try:
@@ -27,7 +19,6 @@ def test_compare_functions():
         assert status
     except RuntimeError:
         print("openvino.test_utils.compare_functions is not available")  # noqa: T201
-
 
 def test_compare_models_pass():
     model = get_relu_model()
@@ -105,42 +96,91 @@ def test_cmake_file_not_found(monkeypatch):
 
     assert result == ""
 
-def test_postponned_constant():
-    class Maker:
-        def __init__(self):
-            self.called = False
 
-        def __call__(self) -> Tensor:
-            self.called = True
-            tensor_data = np.asarray([2, 2, 2, 2], dtype=np.float32).astype(np.float32)
-            print("create tensor")
-            return Tensor(tensor_data)
+class Maker:
+    def __init__(self):
+        self.calls_count = 0
 
-        def is_called(self):
-            return self.called
+    def __call__(self) -> ov.Tensor:
+        self.calls_count += 1
+        tensor_data = np.array([2, 2, 2, 2], dtype=np.float32).reshape(1, 1, 2, 2).astype(np.float32)
+        print("create tensor")
+        return ov.Tensor(tensor_data)
+
+    def called_times(self):
+        return self.calls_count
 
 
-    def create_model(maker):
-        input_shape = Shape([1, 1, 2, 2])
-        param_node = op.Parameter(Type.f32, input_shape)
+def create_model(maker):
+    input_shape = ov.Shape([1, 2, 1, 2])
+    param_node = ov.opset13.parameter(input_shape, ov.Type.f32, name="data")
 
-        postponned_constant = make_postponed_constant(Type.f32, input_shape, maker)
+    postponned_constant = make_postponed_constant(ov.Type.f32, input_shape, maker)
 
-        add_1 = opset13.add(param_node, postponned_constant)
+    add_1 = ov.opset13.add(param_node, postponned_constant)
 
-        const_2 = op.Constant(Type.f32, input_shape, [1, 2, 3, 4])
-        add_2 = opset13.add(add_1, const_2)
+    const_2 = ov.op.Constant(ov.Type.f32, input_shape, [1, 2, 3, 4])
+    add_2 = ov.opset13.add(add_1, const_2)
 
-        return Model(add_2, [param_node], "test_model")
+    return ov.Model(add_2, [param_node], "test_model")
 
+def test_save_postponned_constant():
     maker = Maker()
     model = create_model(maker)
-    assert maker.is_called() is False
+    assert maker.called_times() is 0
 
     model_export_file_name = "out.xml"
     weights_export_file_name = "out.bin"
-    serialize(model, model_export_file_name, weights_export_file_name)
-    assert maker.is_called() is True
+    ov.save_model(model, model_export_file_name, compress_to_fp16=False)
+
+    assert maker.called_times() > 0
 
     os.remove(model_export_file_name)
     os.remove(weights_export_file_name)
+
+def test_save_postponned_constant_twice():
+    maker = Maker()
+    model = create_model(maker)
+    assert maker.called_times() is 0
+
+    model_export_file_name = "out.xml"
+    weights_export_file_name = "out.bin"
+    ov.save_model(model, model_export_file_name, compress_to_fp16=False)
+    assert maker.called_times() is 1
+    ov.save_model(model, model_export_file_name, compress_to_fp16=False)
+    assert maker.called_times() is 2
+
+    os.remove(model_export_file_name)
+    os.remove(weights_export_file_name)
+
+def test_serialize_postponned_constant():
+    maker = Maker()
+    model = create_model(maker)
+    assert maker.called_times() is 0
+
+    model_export_file_name = "out.xml"
+    weights_export_file_name = "out.bin"
+    ov.serialize(model, model_export_file_name, weights_export_file_name)
+    os.remove(model_export_file_name)
+    os.remove(weights_export_file_name)
+
+    assert maker.called_times() > 0
+
+def test_infer_postponned_constant():
+    maker = Maker()
+    model = create_model(maker)
+    assert maker.called_times() is 0
+
+    compiled_model = ov.compile_model(model, "CPU")
+    assert isinstance(compiled_model, ov.CompiledModel)
+
+    request = compiled_model.create_infer_request()
+    input_data = np.ones([1, 2, 1, 2], dtype=np.float32)
+    input_tensor = ov.Tensor(input_data)
+
+    results = request.infer({"data": input_tensor})
+
+    assert maker.called_times() > 0
+
+    expected_output = np.array([4, 5, 6, 7], dtype=np.float32).reshape(1, 2, 1, 2)
+    assert np.allclose(results[list(results)[0]], expected_output, 1e-4, 1e-4)
