c90519c61268a5d23a49976e91942a91695a4b63
PiotrKrzem <piotr.krzeminski@intel.com>
2025-03-05 09:29:55 +0000

    [FIX] Clang

--- src/plugins/template/backend/ops/paged_attention.cpp
+++ src/plugins/template/backend/ops/paged_attention.cpp
@@ -12,29 +12,29 @@ bool evaluate(ov::TensorVector& outputs, const ov::TensorVector& inputs) {
     using T = typename ov::element_type_traits<ET>::value_type;
 
     ov::reference::paged_attention<T>(outputs[0].data<T>(),
-                                   outputs[1].data<T>(),
-                                   inputs[0].data<T>(),         // q
-                                   inputs[1].data<T>(),         // k
-                                   inputs[2].data<T>(),         // v
-                                   inputs[3].data<T>(),         // kc
-                                   inputs[4].data<T>(),         // vc
-                                   inputs[0].get_shape(),       // qs
-                                   inputs[1].get_shape(),       // kvs
-                                   inputs[3].get_shape(),       // kvcs
-                                   inputs[5].data<int32_t>(),   // pl
-                                   inputs[6].data<int32_t>(),   // sb
-                                   inputs[7].data<int32_t>(),   // bi
-                                   inputs[8].data<int32_t>(),   // bib
-                                   inputs[9].data<T>(),         // sc --
-                                   inputs[10].data<int32_t>(),  // sw --
-                                   inputs[11].data<T>(),        // as
-                                   inputs[12].data<int32_t>(),  // mcl --
-                                   inputs[13].data<int32_t>(),  // rbi
-                                   inputs[14].data<int32_t>(),  // rd
-                                   inputs[15].data<int32_t>(),  // trl
-                                   inputs[13].get_shape(),      // kvcs
-                                   inputs[14].get_shape(),      // kvcs
-                                   inputs[15].get_shape());     // kvcs
+                                      outputs[1].data<T>(),
+                                      inputs[0].data<T>(),         // q
+                                      inputs[1].data<T>(),         // k
+                                      inputs[2].data<T>(),         // v
+                                      inputs[3].data<T>(),         // kc
+                                      inputs[4].data<T>(),         // vc
+                                      inputs[0].get_shape(),       // qs
+                                      inputs[1].get_shape(),       // kvs
+                                      inputs[3].get_shape(),       // kvcs
+                                      inputs[5].data<int32_t>(),   // pl
+                                      inputs[6].data<int32_t>(),   // sb
+                                      inputs[7].data<int32_t>(),   // bi
+                                      inputs[8].data<int32_t>(),   // bib
+                                      inputs[9].data<T>(),         // sc --
+                                      inputs[10].data<int32_t>(),  // sw --
+                                      inputs[11].data<T>(),        // as
+                                      inputs[12].data<int32_t>(),  // mcl --
+                                      inputs[13].data<int32_t>(),  // rbi
+                                      inputs[14].data<int32_t>(),  // rd
+                                      inputs[15].data<int32_t>(),  // trl
+                                      inputs[13].get_shape(),      // kvcs
+                                      inputs[14].get_shape(),      // kvcs
+                                      inputs[15].get_shape());     // kvcs
     return true;
 }
 
