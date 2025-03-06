b4677a65f85f3ddb4bed7e791b87c36cf9ec3a8a
Oleg Pipikin <oleg.pipikin@intel.com>
2025-03-06 11:25:48 +0100

    fix

--- src/bindings/python/src/openvino/utils/postponed_constant.py
+++ src/bindings/python/src/openvino/utils/postponed_constant.py
@@ -4,11 +4,16 @@
 
 
 from typing import Callable, List, Optional
-import openvino
+from openvino._pyopenvino import Op
+from openvino._pyopenvino import Type
+from openvino._pyopenvino import Shape
+from openvino._pyopenvino import Tensor
+from openvino._pyopenvino import PartialShape
+
 
 """Postponed Constant is a way to materialize a big constant only when it is going to be serialized to IR and then immediately dispose."""
-class PostponedConstant(openvino.Op):
-    def __init__(self, element_type: openvino.Type, shape: openvino.Shape, maker: Callable[[openvino.Tensor], None], name : Optional[str] = None) -> None:
+class PostponedConstant(Op):
+    def __init__(self, element_type: Type, shape: Shape, maker: Callable[[Tensor], None], name: Optional[str] = None) -> None:
         super().__init__(self)
         self.get_rt_info()["postponed_constant"] = True  # value doesn't matter
         self.m_element_type = element_type
@@ -18,19 +23,20 @@ class PostponedConstant(openvino.Op):
             self.friendly_name = name
         self.constructor_validate_and_infer_types()
 
-    def evaluate(self, outputs: List[openvino.Tensor], _: List[openvino.Tensor]) -> bool:
+    def evaluate(self, outputs: List[Tensor], _: List[Tensor]) -> bool:
         self.m_maker(outputs[0])
         return True
 
     def validate_and_infer_types(self) -> None:
-        self.set_output_type(0, self.m_element_type, openvino.PartialShape(self.m_shape))
+        self.set_output_type(0, self.m_element_type, PartialShape(self.m_shape))
 
     def clone_with_new_inputs(self, new_inputs):
-        return PostponedConstant(self.m_element_type, self.m_shape, self.m_maker)
+        return PostponedConstant(self.m_element_type, self.m_shape, self.m_maker, self.friendly_name)
 
     def has_evaluate(self):
         return True
 
+
 # `maker` is a function that returns ov.Tensor that represents a target Constant
-def make_postponed_constant(element_type: openvino.Type, shape: openvino.Shape, maker: Callable[[openvino.Tensor], None], name : Optional[str] = None) -> openvino.Op:
+def make_postponed_constant(element_type: Type, shape: Shape, maker: Callable[[Tensor], None], name: Optional[str] = None) -> Op:
     return PostponedConstant(element_type, shape, maker, name)
