17d009edf1e14df4bbd6303370ea958363ef61a4
Maxim Vafin <maxim.vafin@intel.com>
2025-03-09 23:25:01 +0100

    Fi code style
    Signed-off-by: Maxim Vafin <maxim.vafin@intel.com>

--- src/frontends/pytorch/src/op/exp.cpp
+++ src/frontends/pytorch/src/op/exp.cpp
@@ -2,11 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include "openvino/op/exp.hpp"
+
 #include "openvino/frontend/complex_type_mark.hpp"
 #include "openvino/op/constant.hpp"
-#include "openvino/op/convert_like.hpp"
-#include "openvino/op/exp.hpp"
-#include "openvino/op/subtract.hpp"
 #include "utils.hpp"
 
 namespace ov {
@@ -23,7 +22,7 @@ OutputVector translate_exp(const NodeContext& context) {
     // This const only needed for type alignment
     auto dummy_const = context.mark_node(v0::Constant::create(element::f32, Shape({}), {0.5}))->output(0);
     align_eltwise_input_types(context, x, dummy_const, false, true);
-    
+
     auto exp = ComplexTypeMark::exp(context, x);
 
     return {exp};
