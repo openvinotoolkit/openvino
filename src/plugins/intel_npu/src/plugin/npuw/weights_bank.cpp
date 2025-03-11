ecdf55f0046e5f8a67003aa1fd9173ca25c185ea
Alexey Smirnov <alexey.smirnov@intel.com>
2025-03-11 12:32:24 +0000

    Fix clang format

--- src/plugins/intel_npu/src/plugin/npuw/weights_bank.cpp
+++ src/plugins/intel_npu/src/plugin/npuw/weights_bank.cpp
@@ -36,14 +36,12 @@ private:
     std::mutex m_mutex;
 };
 
-Bank::Bank(const std::shared_ptr<const ov::ICore>& core,
-            const std::string& alloc_device,
-            const std::string& bank_name)
-        : m_core(core),
-        m_alloc_device(alloc_device),
-        m_bank_name(bank_name),
-        // FIXME: Only NPU allocation is supported for now
-        m_ctx(m_core->get_default_context("NPU")._ptr) {}
+Bank::Bank(const std::shared_ptr<const ov::ICore>& core, const std::string& alloc_device, const std::string& bank_name)
+    : m_core(core),
+      m_alloc_device(alloc_device),
+      m_bank_name(bank_name),
+      // FIXME: Only NPU allocation is supported for now
+      m_ctx(m_core->get_default_context("NPU")._ptr) {}
 
 int64_t Bank::registerLT(const LazyTensor& tensor, const std::string& device) {
     const std::string& device_for_alloc = m_alloc_device.empty() ? device : m_alloc_device;
