29ded01099de692918812029b50748ccbdcccefb
Anastasia Kuporosova <anastasia.kuporosova@intel.com>
2025-03-11 14:07:41 +0000

    [PyOV] small improvements

--- src/bindings/python/src/pyopenvino/core/tensor.cpp
+++ src/bindings/python/src/pyopenvino/core/tensor.cpp
@@ -215,29 +215,29 @@ void regclass_Tensor(py::module m) {
             py::arg("end"));
 
     cls.def(py::init([](py::object& image) {
-                if (!py::isinstance(image, py::module::import("PIL.Image").attr("Image"))) {
-                    throw py::type_error("Input must be a PIL.Image.Image object");
-                }
-                auto numpy = py::module::import("numpy");
-                py::array np_array = numpy.attr("array")(image);
+            if (!py::isinstance(image, py::module::import("PIL.Image").attr("Image"))) {
+                    throw py::type_error("Input argument must be a PIL.Image.Image/numpy.array/List[int, float, str] object");
+            }
+            auto numpy = py::module::import("numpy");
+            py::array np_array = numpy.attr("array")(image);
 
-                return Common::object_from_data<ov::Tensor>(np_array, false);
-            }),
-            py::arg("image"),
-            R"(
-                Constructs Tensor from a Pillow Image.
+            return Common::object_from_data<ov::Tensor>(np_array, false);
+        }),
+        py::arg("image"),
+        R"(
+            Constructs Tensor from a Pillow Image.
 
-                :param image: Pillow Image to create the tensor from.
-                :type image: PIL.Image.Image
-                :Example:
-                .. code-block:: python
+            :param image: Pillow Image to create the tensor from.
+            :type image: PIL.Image.Image
+            :Example:
+            .. code-block:: python
 
-                    from PIL import Image
-                    import openvino as ov
+                from PIL import Image
+                import openvino as ov
 
-                    img = Image.open("example.jpg")
-                    tensor = ov.Tensor(img)
-            )");
+                img = Image.open("example.jpg")
+                tensor = ov.Tensor(img)
+        )");
 
     cls.def("get_element_type",
             &ov::Tensor::get_element_type,
