e299d352357d25b07af51cd816d1245acdd30b1c
alexandruenache1111 <alexandru.enache@intel.com>
2025-03-12 12:03:32 +0000

    Switch to ov version representation as unsigned shorts instead of string
    Signed-off-by: alexandruenache1111 <alexandru.enache@intel.com>

--- src/plugins/intel_npu/src/plugin/include/metadata.hpp
+++ src/plugins/intel_npu/src/plugin/include/metadata.hpp
@@ -75,32 +75,31 @@ constexpr std::string_view MAGIC_BYTES = "OVNPU";
 /**
  * @brief List of supported version formats.
  */
-constexpr uint32_t METADATA_VERSION_1_0{MetadataBase::make_version(1, 0)};
+constexpr uint32_t METADATA_VERSION_2_0{MetadataBase::make_version(2, 0)};
 
 /**
  * @brief Current metadata version.
  */
-constexpr uint32_t CURRENT_METADATA_VERSION{METADATA_VERSION_1_0};
+constexpr uint32_t CURRENT_METADATA_VERSION{METADATA_VERSION_2_0};
 
 constexpr uint16_t CURRENT_METADATA_MAJOR_VERSION{MetadataBase::get_major(CURRENT_METADATA_VERSION)};
 constexpr uint16_t CURRENT_METADATA_MINOR_VERSION{MetadataBase::get_minor(CURRENT_METADATA_VERSION)};
 
-#define NPU_STRINGIFY(x) #x
-#define NPU_TOSTRING(x)  NPU_STRINGIFY(x)
-
-#define CURRENT_OPENVINO_VERSION         \
-    NPU_TOSTRING(OPENVINO_VERSION_MAJOR) \
-    "." NPU_TOSTRING(OPENVINO_VERSION_MINOR) "." NPU_TOSTRING(OPENVINO_VERSION_PATCH)
-
 struct OpenvinoVersion {
 private:
-    std::string _version;
-    uint32_t _size;
+    uint16_t _major;
+    uint16_t _minor;
+    uint16_t _patch;
 
 public:
     OpenvinoVersion();
 
-    OpenvinoVersion(std::string_view version);
+    constexpr OpenvinoVersion(uint16_t major, uint16_t minor, uint16_t patch)
+        : _major(major),
+          _minor(minor),
+          _patch(patch) {}
+
+    OpenvinoVersion(const OpenvinoVersion& version);
 
     /**
      * @brief Reads version data from a stream.
@@ -112,11 +111,18 @@ public:
      */
     void write(std::ostream& stream);
 
-    /**
-     * @brief Gets the version string.
-     */
-    std::string get_version() const;
+    uint16_t get_major() const;
+
+    uint16_t get_minor() const;
+
+    uint16_t get_patch() const;
+
+    bool operator!=(const OpenvinoVersion& version);
 };
+
+constexpr OpenvinoVersion CURRENT_OPENVINO_VERSION(OPENVINO_VERSION_MAJOR,
+                                                   OPENVINO_VERSION_MINOR,
+                                                   OPENVINO_VERSION_PATCH);
 
 /**
  * @brief Template for metadata class handling.
@@ -128,13 +134,13 @@ struct Metadata : public MetadataBase {};
  * @brief Template specialization for metadata version 1.0.
  */
 template <>
-struct Metadata<METADATA_VERSION_1_0> : public MetadataBase {
+struct Metadata<METADATA_VERSION_2_0> : public MetadataBase {
 protected:
     OpenvinoVersion _ovVersion;
     uint64_t _blobDataSize;
 
 public:
-    Metadata(uint64_t blobSize, std::optional<std::string_view> ovVersion = std::nullopt);
+    Metadata(uint64_t blobSize, std::optional<OpenvinoVersion> ovVersion = std::nullopt);
 
     void read(std::istream& stream) override;
 
