70f5c85049ac85522c529d046a902d85c6d5a776
Jyc323 <always_dan@outlook.com>
2025-03-12 23:03:21 -0700

    finish gather.cpp, test python file and register in op_table.cpp

---
+++ src/frontends/jax/src/op/gather.cpp
@@ -0,0 +1,36 @@
+// Copyright (C) 2018-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "openvino/frontend/jax/node_context.hpp"
+#include "openvino/op/constant.hpp"
+#include "openvino/op/gather.hpp"
+#include "utils.hpp"
+
+using namespace std;
+using namespace ov;
+using namespace ov::op;
+
+namespace ov {
+namespace frontend {
+namespace jax {
+namespace op {
+
+OutputVector translate_gather(const NodeContext& context) {
+    num_inputs_check(context, 2);
+
+    Output<Node> inputs = context.get_input(0);
+    Output<Node> indices = context.get_input(1);
+
+    int64_t axis = context.const_named_param<int64_t>("axis");
+    auto axis_node = make_shared<v0::Constant>(element::i64, Shape{}, axis);
+
+    Output<Node> res = make_shared<v1::Gather>(inputs, indices, axis_node);
+
+    return {res};
+};
+
+}  // namespace op
+}  // namespace jax
+}  // namespace frontend
+}  // namespace ov
---
+++ tests/layer_tests/jax_tests/test_gather.py
@@ -0,0 +1,58 @@
+# Copyright (C) 2018-2025 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+import numpy as np
+import pytest
+from jax import numpy as jnp
+from jax import lax
+
+from jax_layer_test_class import JaxLayerTest
+
+rng = np.random.default_rng(98765)
+
+
+class TestGather(JaxLayerTest):
+    def _prepare_input(self):
+        data = rng.integers(-10, 10, self.data_shape).astype(self.input_type)
+        indices = rng.integers(0, self.data_shape[self.axis], self.indices_shape).astype(np.int32)
+
+        return [jnp.array(data), jnp.array(indices)]
+
+    def create_model(self, data_shape, indices_shape, axis, input_type):
+        self.data_shape = data_shape
+        self.indices_shape = indices_shape
+        self.axis = axis
+        self.input_type = input_type
+
+        def jax_gather(data, indices):
+            if indices.ndim == 1 and len(indices.shape) == 1:
+                indices = indices[:, None]
+
+            dimension_numbers = lax.GatherDimensionNumbers(
+                offset_dims=tuple(i for i in range(len(data.shape)) if i != axis),
+                collapsed_slice_dims=(axis,),  # Collapse gathered axis
+                start_index_map=(axis,)  # Indices correspond to the chosen axis
+            )
+
+            slice_sizes = list(data.shape)
+            slice_sizes[axis] = 1  # Gather one element per index
+
+            return lax.gather(
+                data, indices, dimension_numbers=dimension_numbers, slice_sizes=slice_sizes
+            )
+
+        return jax_gather, None, 'gather'
+
+    @pytest.mark.parametrize('data_shape,indices_shape,axis', [
+        ([5], [3], 0),
+        ([4, 6], [2, 3], 1),
+        ([3, 4, 5], [2, 1], 2),
+    ])
+    @pytest.mark.parametrize('input_type', [np.int32, np.int64, np.float32, np.float64])
+    @pytest.mark.nightly
+    @pytest.mark.precommit
+    @pytest.mark.precommit_jax_fe
+    def test_gather(self, data_shape, indices_shape, axis, input_type,
+                    ie_device, precision, ir_version):
+        self._test(*self.create_model(data_shape, indices_shape, axis, input_type),
+                   ie_device, precision, ir_version)
