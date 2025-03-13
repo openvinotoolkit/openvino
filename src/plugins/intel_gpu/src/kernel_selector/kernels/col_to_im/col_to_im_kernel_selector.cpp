2059cbeb2bd6b0f7807daaa4629bf17181fac4d7
Min, Byungil <byungil.min@intel.com>
2025-03-13 16:46:12 +0900

    [GPU] Implement col2im
    Signed-off-by: Min, Byungil <byungil.min@intel.com>

---
+++ src/plugins/intel_gpu/src/kernel_selector/kernels/col_to_im/col_to_im_kernel_selector.cpp
@@ -0,0 +1,17 @@
+// Copyright (C) 2018-2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "col_to_im_kernel_selector.h"
+#include "col_to_im_kernel_ref.h"
+
+namespace kernel_selector {
+
+col_to_im_kernel_selector::col_to_im_kernel_selector() {
+    Attach<ColToImKernelRef>();
+}
+
+KernelsData col_to_im_kernel_selector::GetBestKernels(const Params& params) const {
+    return GetNaiveBestKernel(params, KernelType::COL_TO_IM);
+}
+}  // namespace kernel_selector
