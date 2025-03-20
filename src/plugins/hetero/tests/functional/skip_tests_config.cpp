846ac2db62a1b09f6c5728568a0b671ee3d53f95
Pawel Raasz <pawel.raasz@intel.com>
2025-03-20 10:53:49 +0000

    Update skipped tests for NPU
    Signed-off-by: Pawel Raasz <pawel.raasz@intel.com>

--- src/plugins/hetero/tests/functional/skip_tests_config.cpp
+++ src/plugins/hetero/tests/functional/skip_tests_config.cpp
@@ -15,7 +15,7 @@ std::vector<std::string> disabledTestPatterns() {
         R"(.*smoke_(Multi|Auto|Hetero)_BehaviorTests.*OVPropertiesTests.*canSetPropertyAndCheckGetProperty.*)",
         R"(.*OVInferRequestCheckTensorPrecision.*get(Input|Output|Inputs|Outputs)From.*FunctionWith(Single|Several).*type=(u4|u1|i4|boolean).*)",
         R"(.*OVGetMetricPropsTest.*OVGetMetricPropsTest.*GetMetricAndPrintNoThrow_AVAILABLE_DEVICES.*)"
-        // CACHE_MODE is not supported on NPU
+        // CACHE_MODE property is not supported on NPU
         R"(.*OVCompiledModelBaseTest.*import_from_.*_blob.*targetDevice=(HETERO.NPU).*)",
         R"(.*OVCompiledModelBaseTest.*compile_from_.*_blob.*targetDevice=(HETERO.NPU).*)",
         R"(.*OVCompiledModelBaseTest.*compile_from_cached_weightless_blob.*targetDevice=(HETERO.NPU).*)",
