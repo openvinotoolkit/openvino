eb85af4688d4c793ad96d98977bc30721488882f
Mateusz Mikolajczyk <mateusz.mikolajczyk@intel.com>
2025-03-21 18:04:20 +0100

    Add scale func tests
    Signed-off-by: Mateusz Mikolajczyk <mateusz.mikolajczyk@intel.com>

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/single_layer_tests/paged_attention.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/single_layer_tests/paged_attention.cpp
@@ -2,59 +2,86 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include "single_op_tests/paged_attention.hpp"
+
 #include <vector>
 
-#include "single_op_tests/paged_attention.hpp"
-#include "shared_test_classes/single_op/paged_attention.hpp"
 #include "common_test_utils/test_constants.hpp"
+#include "shared_test_classes/single_op/paged_attention.hpp"
 
 namespace {
 using ov::test::PagedAttentionLayerTest;
 const std::vector<ov::element::Type> inputPrecisions = {
-        ov::element::f32,
+    ov::element::f32,
 };
 
+INSTANTIATE_TEST_SUITE_P(smoke_PagedAttention_basic_static,
+                         PagedAttentionLayerTest,
+                         ::testing::Combine(::testing::Values(ov::test::static_shapes_to_test_representation({
+                                                {2, 8},         // InputShape query,
+                                                {2, 8},         // InputShape key,
+                                                {2, 8},         // InputShape value,
+                                                {3, 2, 32, 4},  // InputShape key_cache,
+                                                {3, 2, 32, 4},  // InputShape value_cache
+                                            })),
+                                            ::testing::Values(ov::test::PagedAttentionIntVectorsStruct{
+                                                {0, 1},
+                                                {0, 1, 2},
+                                                {0, 1, 2},
+                                                {0, 1, 2},
+                                            }),
+                                            ::testing::Values(ov::test::PagedAttentionMiscInpStruct{{1}, 1, {2, 4}, 1}),
+                                            ::testing::Values(std::nullopt),
+                                            ::testing::ValuesIn(inputPrecisions),
+                                            ::testing::Values(ov::test::utils::DEVICE_CPU)),
+                         PagedAttentionLayerTest::getTestCaseName);
 
-INSTANTIATE_TEST_SUITE_P(smoke_PagedAttention_basic_static, PagedAttentionLayerTest,
-        ::testing::Combine(
-                ::testing::Values(ov::test::static_shapes_to_test_representation({
-                        {2, 8}, // InputShape query,
-                        {2, 8}, // InputShape key,
-                        {2, 8}, // InputShape value,
-                        {3, 2, 32, 4}, // InputShape key_cache,
-                        {3, 2, 32, 4}, // InputShape value_cache
-                })),
-                ::testing::Values(ov::test::PagedAttentionIntVectorsStruct{
-                        {0, 1},
-                        {0, 1, 2},
-                        {0, 1, 2},
-                        {0, 1, 2},
-                }),
-                ::testing::Values(ov::test::PagedAttentionMiscInpStruct{1, 1, {2, 4}, 1}),
-                ::testing::Values(std::nullopt),
-                ::testing::ValuesIn(inputPrecisions),
-                ::testing::Values(ov::test::utils::DEVICE_CPU)),
-                PagedAttentionLayerTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(
+    smoke_PagedAttention_static_rotation,
+    PagedAttentionLayerTest,
+    ::testing::Combine(::testing::Values(ov::test::static_shapes_to_test_representation({
+                           {2, 8},         // InputShape query,
+                           {2, 8},         // InputShape key,
+                           {2, 8},         // InputShape value,
+                           {1, 2, 32, 4},  // InputShape key_cache,
+                           {1, 2, 32, 4},  // InputShape value_cache
+                       })),
+                       ::testing::Values(ov::test::PagedAttentionIntVectorsStruct{
+                           {0, 1},
+                           {0, 1, 2},
+                           {0},
+                           {0, 0, 0},
+                       }),
+                       ::testing::Values(ov::test::PagedAttentionMiscInpStruct{{1}, 0, {0, 0}, 10}),
+                       ::testing::Values(ov::test::PagedAttentionRotationStruct{{0, 0, 0, 0}, {4, 32}, {4, 4}}),
+                       ::testing::ValuesIn(inputPrecisions),
+                       ::testing::Values(ov::test::utils::DEVICE_CPU)),
+    PagedAttentionLayerTest::getTestCaseName);
 
-INSTANTIATE_TEST_SUITE_P(smoke_PagedAttention_static_rotation, PagedAttentionLayerTest,
-                        ::testing::Combine(
-                                ::testing::Values(ov::test::static_shapes_to_test_representation({
-                                        {2, 8}, // InputShape query,
-                                        {2, 8}, // InputShape key,
-                                        {2, 8}, // InputShape value,
-                                        {1, 2, 32, 4}, // InputShape key_cache,
-                                        {1, 2, 32, 4}, // InputShape value_cache
-                                })),
-                                ::testing::Values(ov::test::PagedAttentionIntVectorsStruct{
-                                        {0, 1},
-                                        {0, 1, 2},
-                                        {0},
-                                        {0, 0, 0},
-                                }),
-                                ::testing::Values(ov::test::PagedAttentionMiscInpStruct{1, 0, {0, 0}, 10}),
-                                ::testing::Values(ov::test::PagedAttentionRotationStruct{{0, 0, 0, 0}, {4, 32}, {4, 4}}),
-                                ::testing::ValuesIn(inputPrecisions),
-                                ::testing::Values(ov::test::utils::DEVICE_CPU)),
-                                PagedAttentionLayerTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_PagedAttention_scale,
+                         PagedAttentionLayerTest,
+                         ::testing::Combine(::testing::Values(ov::test::static_shapes_to_test_representation({
+                                                {2, 8},         // InputShape query,
+                                                {2, 8},         // InputShape key,
+                                                {2, 8},         // InputShape value,
+                                                {3, 2, 32, 4},  // InputShape key_cache,
+                                                {3, 2, 32, 4},  // InputShape value_cache
+                                            })),
+                                            ::testing::Values(ov::test::PagedAttentionIntVectorsStruct{
+                                                {0, 1},
+                                                {0, 1, 2},
+                                                {0, 1, 2},
+                                                {0, 1, 2},
+                                            }),
+                                            ::testing::ValuesIn({
+                                                // Negative scale NaN?
+                                                // ov::test::PagedAttentionMiscInpStruct{{-0.5}, 1, {2, 4}, 1},
+                                                ov::test::PagedAttentionMiscInpStruct{{1.5}, 1, {2, 4}, 1},
+                                                ov::test::PagedAttentionMiscInpStruct{{5}, 1, {2, 4}, 1},
+                                                ov::test::PagedAttentionMiscInpStruct{{}, 1, {2, 4}, 1},
+                                            }),
+                                            ::testing::Values(std::nullopt),
+                                            ::testing::ValuesIn(inputPrecisions),
+                                            ::testing::Values(ov::test::utils::DEVICE_CPU)),
+                         PagedAttentionLayerTest::getTestCaseName);
 }  // namespace
-
