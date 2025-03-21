e3115bd0e6cfd9a0c3949552e88bae36995d52bc
Raasz, Pawel <pawel.raasz@intel.com>
2025-03-21 07:19:33 +0000

    Add comment about const_cast in transpose
    Signed-off-by: Raasz, Pawel <pawel.raasz@intel.com>

--- src/core/src/op/transpose.cpp
+++ src/core/src/op/transpose.cpp
@@ -96,6 +96,7 @@ bool Transpose::evaluate(TensorVector& outputs, const TensorVector& inputs) cons
         };
 
         auto out_ptr = int4_iterator(static_cast<uint8_t*>(out.data()));
+        // The int4_iterator not supports const pointer but these data are not modified
         auto in_ptr = int4_iterator(static_cast<uint8_t*>(const_cast<void*>(arg.data())));
         if ((arg_type == ov::element::i4 || arg_type == ov::element::u4) && arg.get_shape().size() == 2) {
             for (size_t i = 0; i < out_shape[0]; i++) {
