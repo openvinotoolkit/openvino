5423671d5053ef5a7584ecc5aebba493fecd211a
Leonardo Monteiro <leomont11@hotmail.com>
2025-03-24 12:27:56 -0300

    Apply clang-format to rad2deg.cpp

--- src/frontends/pytorch/src/op/rad2deg.cpp
+++ src/frontends/pytorch/src/op/rad2deg.cpp
@@ -5,8 +5,8 @@
 #include <cmath>
 
 #include "openvino/frontend/pytorch/node_context.hpp"
-#include "openvino/op/multiply.hpp"
 #include "openvino/op/constant.hpp"
+#include "openvino/op/multiply.hpp"
 
 namespace ov {
 namespace frontend {
@@ -16,7 +16,7 @@ namespace op {
 OutputVector translate_rad2deg(const NodeContext& context) {
     // Ensure that the operation has exactly one input (the input tensor)
     num_inputs_check(context, 1, 1);
-    
+
     // Retrieve the input tensor
     auto input = context.get_input(0);
 
@@ -24,10 +24,10 @@ OutputVector translate_rad2deg(const NodeContext& context) {
     auto input_type = input.get_element_type();
 
     const double pi_val = std::atan(1.0) * 4;
-    
+
     // Create a constant node with the conversion factor (180 / π)
     auto conversion_factor = context.mark_node(ov::op::v0::Constant::create(input_type, Shape{}, {180.0 / pi_val}));
-    
+
     // Apply the multiplication operation to convert radians to degrees
     auto result = context.mark_node(std::make_shared<ov::op::v1::Multiply>(input, conversion_factor));
 
