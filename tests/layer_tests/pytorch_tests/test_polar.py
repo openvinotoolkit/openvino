21f72459960a4a73bd022db5b0a4db1a6d88a6ab
Vijay Kumar <82241873+vijaykr338@users.noreply.github.com>
2025-03-26 01:01:11 +0530

    Update test_polar.py

--- tests/layer_tests/pytorch_tests/test_polar.py
+++ tests/layer_tests/pytorch_tests/test_polar.py
@@ -4,14 +4,14 @@
 import numpy as np
 import pytest
 import torch
-
+import openvino as ov
 from pytorch_layer_test_class import PytorchLayerTest
 
 class TestPolar(PytorchLayerTest):
-    def _prepare_input(self):
+    def _prepare_input(self, input_shape=(1, 1000), dtype=np.float32):
         return (
-            np.random.uniform(0, 10, (1, 1000)).astype(np.float32),
-            np.random.uniform(-np.pi, np.pi, (1, 1000)).astype(np.float32)
+            np.random.uniform(0, 10, input_shape).astype(dtype),
+            np.random.uniform(-np.pi, np.pi, input_shape).astype(dtype)
         )
 
     def create_model(self):
@@ -19,14 +19,14 @@ class TestPolar(PytorchLayerTest):
             def forward(self, abs, angle):
                 complex_tensor = torch.polar(abs, angle)
                 return torch.view_as_real(complex_tensor)
-
+        ref_net = None
         return PolarModel(), None, "aten::polar"
 
     @pytest.mark.parametrize("input_case", [
-        (1, 1000),  
-        (2, 500),     
-        (5, 200),     
-        (10, 100),   
+        (1, 1000),
+        (2, 500),
+        (5, 200),
+        (10, 100),
     ])
     @pytest.mark.parametrize("dtype", [
         np.float32,
@@ -35,10 +35,8 @@ class TestPolar(PytorchLayerTest):
     @pytest.mark.nightly
     @pytest.mark.precommit
     def test_polar(self, input_case, dtype, ie_device, precision, ir_version):
-        self.input_shape = input_case
-        self._prepare_input = lambda: (
-            np.random.uniform(0, 10, input_case).astype(dtype),
-            np.random.uniform(-np.pi, np.pi, input_case).astype(dtype)
-        )
-        self._test(*self.create_model(), ie_device, precision, ir_version, trace_model=True,
-                    use_convert_model=True, kwargs_to_prepare_input={})
+        atol = 1e-4 if precision == "FP32" else 1e-3
+        rtol = 1e-4
+        self._test(*self.create_model(), ie_device, precision, ir_version,
+                   kwargs_to_prepare_input={"input_shape": input_case, "dtype": dtype},
+                   trace_model=True, use_convert_model=True, custom_eps=atol, dynamic_shapes=False)
