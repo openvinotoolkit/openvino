30df05536e0a9772db25573b3026f4c6c2917233
Bogdan Pereanu <bogdan.pereanu@intel.com>
2025-03-25 08:52:04 +0200

    [NPU] Optimize backend flow (#29373)
    ### Details:
     - *Removing NPUBackend class*
    
    ### Tickets:
     - *CVS-163728*
    
    ---------
    
    Signed-off-by: Bogdan Pereanu <bogdan.pereanu@intel.com>

--- src/plugins/intel_npu/src/plugin/include/plugin.hpp
+++ src/plugins/intel_npu/src/plugin/include/plugin.hpp
@@ -8,7 +8,8 @@
 #include <memory>
 #include <string>
 
-#include "backends.hpp"
+#include "backends_registry.hpp"
+#include "intel_npu/common/npu.hpp"
 #include "intel_npu/config/config.hpp"
 #include "intel_npu/utils/logger/logger.hpp"
 #include "metrics.hpp"
@@ -52,7 +53,11 @@ public:
                                     const ov::AnyMap& properties) const override;
 
 private:
-    std::shared_ptr<NPUBackends> _backends;
+    std::unique_ptr<BackendsRegistry> _backendsRegistry;
+
+    //  _backend might not be set by the plugin; certain actions, such as offline compilation, might be supported.
+    //  Appropriate checks are needed in plugin/metrics/properties when actions depend on a backend.
+    ov::SoPtr<IEngineBackend> _backend;
 
     std::map<std::string, std::string> _config;
     std::shared_ptr<OptionsDesc> _options;
