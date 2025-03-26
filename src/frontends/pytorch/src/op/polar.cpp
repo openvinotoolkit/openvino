7c9e7457014e6bf6c95d5953f98b63aed1ece87f
Vijay Kumar <82241873+vijaykr338@users.noreply.github.com>
2025-03-26 22:11:27 +0530

    Update polar.cpp

--- src/frontends/pytorch/src/op/polar.cpp
+++ src/frontends/pytorch/src/op/polar.cpp
@@ -1,3 +1,7 @@
+// Copyright (C) 2018-2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
 #include "openvino/frontend/complex_type_mark.hpp"
 #include "openvino/op/convert.hpp"
 #include "openvino/op/cos.hpp"
