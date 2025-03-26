cb7f800cb8c115712fa901c17cbaf8fc1fc9ee09
Leonardo Monteiro <leomont11@hotmail.com>
2025-03-26 12:46:07 -0300

    fix test and add more data type

--- tests/layer_tests/pytorch_tests/test_rad2deg.py
+++ tests/layer_tests/pytorch_tests/test_rad2deg.py
@@ -8,19 +8,20 @@ from pytorch_layer_test_class import PytorchLayerTest
 
 
 class TestRad2Deg(PytorchLayerTest):
-    def _prepare_input(self, input_shape=(2, 2)):
-        return (np.random.uniform(-np.pi, np.pi, size=input_shape).astype(np.float32),)
+    def _prepare_input(self, input_shape=(2, 2), dtype=np.float32):
+        return (np.random.uniform(-np.pi, np.pi, size=input_shape).astype(dtype),)
 
     def create_model(self):
         class aten_rad2deg(torch.nn.Module):
             def forward(self, x):
-                return x * (180.0 / np.pi)
+                return torch.rad2deg(x)
         ref_net = None  
-        return aten_rad2deg(), ref_net, "aten::mul"
+        return aten_rad2deg(), ref_net, "aten::rad2deg"
 
-    @pytest.mark.parametrize("input_shape", [(2, 2), (3, 3), (4, 5)])
+    @pytest.mark.parametrize("input_shape", [(3, 3)])
+    @pytest.mark.parametrize("dtype", [np.float32, np.float64, np.int32])
     @pytest.mark.nightly
     @pytest.mark.precommit
-    def test_rad2deg(self, input_shape, ie_device, precision, ir_version):
+    def test_rad2deg(self, input_shape, dtype, ie_device, precision, ir_version):
         self._test(*self.create_model(), ie_device, precision, ir_version,
-                   kwargs_to_prepare_input={"input_shape": input_shape})
+                   kwargs_to_prepare_input={"input_shape": input_shape, "dtype": dtype})
