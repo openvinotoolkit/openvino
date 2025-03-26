217f9449917d1be87f4c8d39c7f32b43ecf2edf4
Samay10 <samayashar@gmail.com>
2025-03-26 18:43:30 +0530

    [PyTorch FE] Updated quantized_linear_relu.cpp

--- src/frontends/pytorch/src/op/quantized_linear_relu.cpp
+++ src/frontends/pytorch/src/op/quantized_linear_relu.cpp
@@ -1,3 +1,7 @@
+// Copyright (C) 2018-2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
 #include "openvino/frontend/pytorch/node_context.hpp"
 #include "openvino/op/constant.hpp"
 #include "openvino/op/relu.hpp"
