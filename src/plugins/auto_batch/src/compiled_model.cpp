9df9e900d74e913b56e686c8b2ea6a044072a368
Aditya Sankhla <adityasankhla14@gmail.com>
2025-03-27 23:57:42 +0530

    chore: removed duplicate properties

--- src/plugins/auto_batch/src/compiled_model.cpp
+++ src/plugins/auto_batch/src/compiled_model.cpp
@@ -207,15 +207,12 @@ ov::Any CompiledModel::get_property(const std::string& name) const {
         } else if (name == ov::loaded_from_cache) {
             return m_compiled_model_without_batch->get_property(ov::loaded_from_cache.name());
         } else if (name == ov::supported_properties) {
-            auto hw_properties = m_compiled_model_without_batch->get_property(ov::supported_properties.name())
+            auto device_supported_props = m_compiled_model_without_batch->get_property(ov::supported_properties.name())
                                    .as<std::vector<ov::PropertyName>>();
             std::vector<ov::PropertyName> properties{
                 ov::PropertyName{ov::supported_properties.name(), ov::PropertyMutability::RO},
-                ov::PropertyName{ov::optimal_number_of_infer_requests.name(), ov::PropertyMutability::RO},
-                ov::PropertyName{ov::model_name.name(), ov::PropertyMutability::RO},
-                ov::PropertyName{ov::execution_devices.name(), ov::PropertyMutability::RO},
                 ov::PropertyName{ov::auto_batch_timeout.name(), ov::PropertyMutability::RW}};
-            properties.insert(properties.end(), hw_properties.begin(), hw_properties.end());
+            properties.insert(properties.end(), device_supported_props.begin(), device_supported_props.end());
             return properties;
         } else if (name == ov::auto_batch_timeout) {
             uint32_t time_out = m_time_out;
