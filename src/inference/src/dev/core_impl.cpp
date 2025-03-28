e450bd1e02d92c76af0ef99958e72e628df70916
Kim, Eddy <eddy.kim@intel.com>
2025-03-28 22:07:57 +0900

    updated to check parsed config once

--- src/inference/src/dev/core_impl.cpp
+++ src/inference/src/dev/core_impl.cpp
@@ -777,9 +777,10 @@ ov::SoPtr<ov::ICompiledModel> ov::CoreImpl::compile_model(const std::shared_ptr<
     if (cacheManager && device_supports_model_caching(plugin, parsed._config) && !is_proxy_device(plugin)) {
         CacheContent cacheContent{cacheManager, parsed._core_config.get_enable_mmap()};
         cacheContent.blobId = ov::ModelCache::compute_hash(model, create_compile_config(plugin, parsed._config));
-        if (parsed._config.find(ov::cache_mode.name()) != parsed._config.end() &&
-            parsed._config.at(ov::cache_mode.name()).as<ov::CacheMode>() == ov::CacheMode::OPTIMIZE_SIZE) {
-            cacheContent.model = std::const_pointer_cast<ov::Model>(model);
+        if (auto mode_it = parsed._config.find(ov::cache_mode.name()); mode_it != parsed._config.end()) {
+            if (mode_it->second.as<ov::CacheMode>() == ov::CacheMode::OPTIMIZE_SIZE) {
+                cacheContent.model = std::const_pointer_cast<ov::Model>(model);
+            }
         }
         std::unique_ptr<CacheGuardEntry> lock = cacheGuard.get_hash_lock(cacheContent.blobId);
         res = load_model_from_cache(cacheContent, plugin, parsed._config, ov::SoPtr<ov::IRemoteContext>{}, [&]() {
@@ -816,9 +817,10 @@ ov::SoPtr<ov::ICompiledModel> ov::CoreImpl::compile_model(const std::shared_ptr<
         CacheContent cacheContent{cacheManager, parsed._core_config.get_enable_mmap()};
         cacheContent.blobId = ov::ModelCache::compute_hash(model, create_compile_config(plugin, parsed._config));
         std::unique_ptr<CacheGuardEntry> lock = cacheGuard.get_hash_lock(cacheContent.blobId);
-        if (parsed._config.find(ov::cache_mode.name()) != parsed._config.end() &&
-            parsed._config.at(ov::cache_mode.name()).as<ov::CacheMode>() == ov::CacheMode::OPTIMIZE_SIZE) {
-            cacheContent.model = std::const_pointer_cast<ov::Model>(model);
+        if (auto mode_it = parsed._config.find(ov::cache_mode.name()); mode_it != parsed._config.end()) {
+            if (mode_it->second.as<ov::CacheMode>() == ov::CacheMode::OPTIMIZE_SIZE) {
+                cacheContent.model = std::const_pointer_cast<ov::Model>(model);
+            }
         }
         res = load_model_from_cache(cacheContent, plugin, parsed._config, context, [&]() {
             return compile_model_and_cache(plugin, model, parsed._config, context, cacheContent);
