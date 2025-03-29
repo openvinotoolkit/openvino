e64a44415030791d14bb16f1a1d136cf64ee4159
Raghavyadav17 <take2home241@gmail.com>
2025-03-29 10:59:14 +0530

    dummy change

--- src/frontends/pytorch/src/op/quantized_convnd.cpp
+++ src/frontends/pytorch/src/op/quantized_convnd.cpp
@@ -47,11 +47,6 @@ Output<ov::Node> translate_quantized_convnd_base(const NodeContext& context,bool
     auto pad_type = ov::op::PadType::EXPLICIT;
 
     std::shared_ptr<ov::Node> conv;
-    if(is_1d){
-        strides={strides[0]};
-        pads={pads[0],pads[1]};
-        dilations={dilations[0]};
-    }
 
     if (groups == 1) {
         conv = std::make_shared<v1::Convolution>(input, weight, strides, pads, pads, dilations, pad_type);
@@ -94,14 +89,7 @@ OutputVector translate_quantized_convnd_relu(const NodeContext& context) {
     return {quantize(context, relu->output(0), scale, zero_point, context.get_input(0))};
 }
 
-OutputVector translate_quantized_conv1d_relu(const NodeContext& context){
-    num_inputs_check(context,4,4);
-    auto scale=context.get_input(2);
-    auto zero_point=context.get_input(3);
-    auto conv=translate_quantized_convnd_base(context,true);
-    auto relu=context.mark_node(std::make_shared<v0::Relu>(conv));
-    return {quantize(context,relu->output(0),scale,zero_point,context.get_input(0))};
-}
+
 
 }  // namespace op
 }  // namespace pytorch
