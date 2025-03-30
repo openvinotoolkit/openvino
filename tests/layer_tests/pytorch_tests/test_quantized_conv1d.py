d7426063493e82b6910dc70a4e704fffadfb7b76
Haisheng Chen <hac048@ucsd.edu>
2025-03-29 23:01:59 -0700

    update

--- tests/layer_tests/pytorch_tests/test_quantized_conv1d.py
+++ tests/layer_tests/pytorch_tests/test_quantized_conv1d.py
@@ -61,7 +61,7 @@ class TestQuantizedConv1D(PytorchLayerTest):
                               {'weights_shape': [3, 3, 3], 'strides': 1, 'pads': 0, 'dilations': 2, 'groups': 1},
                               {'weights_shape': [3, 3, 3], 'strides': 1, 'pads': 0, 'dilations': 1, 'groups': 1},
                               {'weights_shape': [3, 3, 3], 'strides': 1, 'pads': 0, 'dilations': 1, 'groups': 1},
-                              {'weights_shape': [3, 1, 3], 'strides': 1, 'pads': 0, 'dilations': 1, 'groups': 3},
+                              {'weights_shape': [3, 3, 3], 'strides': 1, 'pads': 0, 'dilations': 1, 'groups': 3},
                               ])
 
     @pytest.mark.parametrize("bias", [True, False])
