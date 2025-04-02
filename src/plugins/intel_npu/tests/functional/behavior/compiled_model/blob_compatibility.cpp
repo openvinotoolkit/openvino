201096e4d76fa41beb0ed57015a619653c865ccb
MirceaDan99 <mircea-aurelian.dan@intel.com>
2025-04-02 19:03:27 +0300

    Update `blob_compatibility_tool` & tests & add compiled blobs

--- src/plugins/intel_npu/tests/functional/behavior/compiled_model/blob_compatibility.cpp
+++ src/plugins/intel_npu/tests/functional/behavior/compiled_model/blob_compatibility.cpp
@@ -2,24 +2,24 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include "overload/compiled_model/blob_compatibility.hpp"
+
 #include <gtest/gtest.h>
 
-#include <blob_compatibility.hpp>
+using namespace ov::test::behavior;
 
-const std::vector<ov::test::behavior::BlobPair> config = {
-    // MTL
-    // ov release 2025.0.0
-    {"MTL", "blob_compatibility_dummy_model_MTL_ov_release_2025_0_0.blob"},
-    {"MTL", "blob_compatibility_dummy_model_stateful_MTL_ov_release_2025_0_0.blob"},
-    {"MTL", "blob_compatibility_dummy_model_dynamic_shapes_MTL_ov_release_2025_0_0.blob"}
+const std::vector<std::string> models = {"dummy_model",
+                                         "dummy_model_stateful",
+                                         "dummy_model_dynamic_shapes"};  // fixed prefix
+const std::vector<std::string> platforms = {"MTL", "LNL"};
+const std::vector<std::string> ov_releases = {"ov_2024_6_0", "ov_2025_0_0", "ov_2025_1_0"};
+const std::vector<std::string> drivers = {"driver_1688", "driver_1003967"};
 
-    // LNL
-    // ov release 2025.0.0
-    {"LNL", "blob_compatibility_dummy_model_LNL_ov_release_2025_0_0.blob"},
-    {"LNL", "blob_compatibility_dummy_model_stateful_LNL_ov_release_2025_0_0.blob"},
-    {"LNL", "blob_compatibility_dummy_model_dynamic_shapes_LNL_ov_release_2025_0_0.blob"}};
-
-INSTANTIATE_TEST_SUITE(smoke_Behavior_NPU,
-                       OVBlobCompatibilityNPU,
-                       ::testing::ValuesIn(config),
-                       ov::test::utils::appendPlatformTypeTestName<OVBlobCompatibilityNPU>)
+INSTANTIATE_TEST_SUITE_P(smoke_Behavior_NPU,
+                         OVBlobCompatibilityNPU,
+                         ::testing::Combine(::testing::Values(ov::test::utils::DEVICE_NPU),
+                                            ::testing::ValuesIn(models),
+                                            ::testing::ValuesIn(platforms),
+                                            ::testing::ValuesIn(ov_releases),
+                                            ::testing::ValuesIn(drivers)),
+                         ov::test::utils::appendPlatformTypeTestName<OVBlobCompatibilityNPU>);
