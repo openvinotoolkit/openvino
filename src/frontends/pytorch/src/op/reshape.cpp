14bec359e033d273fc428c5f8c4a56b646bfda00
Ziad Hassan <ziadmohamed3105@gmail.com>
2025-04-04 17:21:10 +0200

    Moved aten::ravel implementation to a separate file ravel.cpp

--- src/frontends/pytorch/src/op/reshape.cpp
+++ src/frontends/pytorch/src/op/reshape.cpp
@@ -46,30 +46,6 @@ OutputVector translate_reshape(const NodeContext& context) {
     }
 };
 
-OutputVector translate_ravel(const NodeContext& context) {
-    // Schema: aten::ravel(Tensor input) -> Tensor
-    // The ravel operation is equivalent to flattening the tensor.
-    num_inputs_check(context, 1, 1, true);
-    auto tensor = context.get_input(0);
-    auto new_shape = context.mark_node(v0::Constant::create(element::i32, Shape{1}, {-1}));
-
-    auto complex_type_mark = as_type_ptr<ComplexTypeMark>(tensor.get_node_shared_ptr());
-    if (complex_type_mark) {
-        tensor = complex_type_mark->get_data();
-
-        new_shape = context.mark_node(v0::Constant::create(element::i32, Shape{2}, {-1, 2}));
-    }
-
-    auto reshape = context.mark_node(std::make_shared<v1::Reshape>(tensor, new_shape, false));
-
-    if (complex_type_mark) {
-        const auto& complex_dtype = complex_type_mark->get_complex_part_type();
-        return {context.mark_node(std::make_shared<ComplexTypeMark>(reshape, complex_dtype))};
-    } else {
-        return {reshape};
-    }
-};
-
 }  // namespace op
 }  // namespace pytorch
 }  // namespace frontend
