1e3cc543b4a1dd3929f63a4e65ade60f4e298aa5
lsanleo <leonardo.santolin@itsdigitalacademy.com>
2025-04-07 15:47:36 +0200

    Fix problems whit copyTo function

--- src/bindings/js/node/src/tensor.cpp
+++ src/bindings/js/node/src/tensor.cpp
@@ -196,18 +196,24 @@ Napi::Value TensorWrap::is_continuous(const Napi::CallbackInfo& info) {
 void TensorWrap::copy_to(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
 
+    // Controlla che ci sia un solo argomento e che sia un oggetto
     if (info.Length() != 1 || !info[0].IsObject()) {
         Napi::TypeError::New(env, "The copyTo method requires one argument of type Tensor.").ThrowAsJavaScriptException();
-        return; 
+        return;
     }
 
+    if (!info[0].InstanceOf(TensorWrap::get_class(env))) {
+        Napi::TypeError::New(env, "Argument must be an instance of Tensor.").ThrowAsJavaScriptException();
+        return;
+    }
     TensorWrap* otherTensorWrap = Napi::ObjectWrap<TensorWrap>::Unwrap(info[0].As<Napi::Object>());
     openvino::runtime::Tensor& otherTensor = otherTensorWrap->_tensor;
 
     if (_tensor.get_element_type() != otherTensor.get_element_type()) {
         Napi::TypeError::New(env, "Tensors must have the same element type to copy data.").ThrowAsJavaScriptException();
-        return; 
+        return;
     }
+
     if (_tensor.get_shape() != otherTensor.get_shape()) {
         Napi::TypeError::New(env, "Tensors must have the same shape to copy data.").ThrowAsJavaScriptException();
         return;
@@ -215,4 +221,3 @@ void TensorWrap::copy_to(const Napi::CallbackInfo& info) {
 
     _tensor.data() = otherTensor.data();
 }
-
