145288600c7e88cd55cdf86887707c17cf962dc2
Mingyu Kim <mingyu.kim@intel.com>
2025-04-08 18:30:25 +0900

    [GPU] Optimize dynamic quantize kernel (#29971)
    ### Details:
     - Optimized dynamic_quanize kernel for gs128 and gs256 case
     - Now the performance is on-par with per-token dyn_quan kernel
    
    ### Tickets:
     - 148732

--- src/plugins/intel_gpu/tests/functional/subgraph_tests/dynamic/matmul_weights_decompression.cpp
+++ src/plugins/intel_gpu/tests/functional/subgraph_tests/dynamic/matmul_weights_decompression.cpp
@@ -416,8 +416,8 @@ INSTANTIATE_TEST_SUITE_P(MatMulCompressedWeights_corner_cases_big,
 const std::vector<uint64_t> group_size = {128, 256, std::numeric_limits<int64_t>::max()};
 INSTANTIATE_TEST_SUITE_P(smoke_MatMulCompressedWeights_dyn_quan,
                          MatmulWeightsDecompression,
-                         ::testing::Combine(::testing::Values(ShapeParams{{{-1, -1, 4096}, {{1024, 1, 4096}, {1, 1, 4096}, {1024, 1, 4096}}},
-                                                                            {4096, 4096}, 128}),  // shape
+                         ::testing::Combine(::testing::Values(ShapeParams{{{-1, -1, 1024}, {{1024, 1, 1024}, {1, 1, 1024}, {1024, 1, 1024}}},
+                                                                            {1024, 1024}, 128}),  // shape
                                             ::testing::Values(ov::element::u4),
                                             ::testing::Values(ov::element::f16),
                                             ::testing::Values(false),
