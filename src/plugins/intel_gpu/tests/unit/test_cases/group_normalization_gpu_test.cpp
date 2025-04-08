db1366c1636afc1666d37f314920a636f84d3bbc
Michal Miotk <michal.miotk@intel.com>
2025-04-08 09:03:05 +0000

    one more lock

--- src/plugins/intel_gpu/tests/unit/test_cases/group_normalization_gpu_test.cpp
+++ src/plugins/intel_gpu/tests/unit/test_cases/group_normalization_gpu_test.cpp
@@ -213,7 +213,7 @@ TEST(group_normalization, input_bfyx_output_fsv16) {
 
     auto outputs_g = network_g.execute();
     auto output_g = outputs_g.at("output").get_memory();
-    cldnn::mem_lock<float> output_mem_g(output_g, get_test_stream());
+    cldnn::mem_lock<float, mem_lock_type::read> output_mem_g(output_g, get_test_stream());
 
     // Disable mem reuse to avoid wrong reuse due to not calculating of memory dependencies in the below model creation flow
     config.set_property(ov::intel_gpu::enable_memory_pool(false));
@@ -230,7 +230,7 @@ TEST(group_normalization, input_bfyx_output_fsv16) {
 
     auto outputs_t = network_t.execute();
     auto output_t = outputs_g.at("output").get_memory();
-    cldnn::mem_lock<float> output_mem_t(output_t, get_test_stream());
+    cldnn::mem_lock<float, mem_lock_type::read> output_mem_t(output_t, get_test_stream());
 
     ASSERT_EQ(output_mem_g.size(), output_mem_t.size());
     ASSERT_EQ(outputs_g.begin()->first, outputs_t.begin()->first);
