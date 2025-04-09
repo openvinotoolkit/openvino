9943c0d172f5d222d4f6d6cf308a8293018d5fd9
Nam, David <david.nam@intel.com>
2025-04-09 18:13:46 +0900

    [GPU] Fix wrong index order in Gather

--- src/plugins/intel_gpu/src/graph/gather.cpp
+++ src/plugins/intel_gpu/src/graph/gather.cpp
@@ -47,6 +47,7 @@ layout gather_inst::calc_output_layout(gather_node const& node, kernel_impl_para
         switch (input_layout.format) {
         case format::bfyx:
         case format::bfzyx:
+        case format::bfwzyx:
         case format::b_fs_zyx_fsv16:
         case format::b_fs_zyx_fsv32:
             output_format = format::get_default_format(dims_converted.size());
--- src/plugins/intel_gpu/src/kernel_selector/kernels/gather/gather_kernel_ref.cpp
+++ src/plugins/intel_gpu/src/kernel_selector/kernels/gather/gather_kernel_ref.cpp
@@ -196,24 +196,30 @@ static std::string GetDictionaryIndexOrder(const gather_params& params, size_t a
 static std::string GetIndicesIdxOrder(const gather_params& params, size_t axis, int64_t batch_dim) {
     std::vector<std::string> idx_order;
 
-    if ((axis == (size_t)batch_dim) && (axis > 1) && (params.inputs[1].GetDims().size() > 4)) {
-        idx_order = GetFinalIndexOrder(params.outputs[0].GetDims().size());
-    } else {
-        idx_order = GetOrder(params.outputs[0].GetDims().size());
-        auto zero_val = "0";
+    const size_t output_rank = params.outputs[0].GetDims().size();
+    const size_t indices_rank = params.inputs[1].GetDims().size();
+    const size_t indices_dims_num = GetNonEmptyDimsNumber(params.inputs[1]);
 
-        size_t indices_dims_num = GetNonEmptyDimsNumber(params.inputs[1]);
+    idx_order = GetOrder(output_rank);
+    const auto zero_val = "0";
 
-        // Shift indices of Gather indices input related to output dims
-        for (size_t i = batch_dim; i < indices_dims_num; i++)
-            idx_order[i] = idx_order[axis + i - batch_dim];
-
-        for (size_t i = indices_dims_num; i < idx_order.size(); i++)
+     // Shift indices of Gather indices input related to output dims
+    for (size_t i = static_cast<size_t>(batch_dim); i < indices_dims_num; i++) {
+        size_t output_idx = axis + i - static_cast<size_t>(batch_dim);
+        if (output_idx < idx_order.size()) {
+            idx_order[i] = idx_order[output_idx];
+        } else {
             idx_order[i] = zero_val;
+        }
+    }
 
-        // Fix size to inputs[1] dims size
-        for (size_t i = 0; i < params.outputs[0].GetDims().size() - params.inputs[1].GetDims().size(); i++)
-            idx_order.pop_back();
+    for (size_t i = indices_dims_num; i < idx_order.size(); ++i) {
+        idx_order[i] = zero_val;
+    }
+
+    // Fix size to inputs[1] dims size
+    while (idx_order.size() > indices_rank) {
+        idx_order.pop_back();
     }
 
     return GetOrderString(idx_order);
