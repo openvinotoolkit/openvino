f8d407861b6ed6650a1bccb4b257433d8785445f
Shen, Wanglei <wanglei.shen@intel.com>
2025-04-11 20:56:23 +0800

    test

--- src/inference/src/system_conf.cpp
+++ src/inference/src/system_conf.cpp
@@ -500,10 +500,8 @@ void set_cpu_used(const std::vector<int>& cpu_ids, const int used) {
     const auto cpu_size = static_cast<int>(cpu_ids.size());
     std::cout << "step 20 cpu_size = " << cpu_size << std::endl;
     std::cout << "step 21 _cpu_mapping_table " << std::endl;
-    for (auto &row : cpu._cpu_mapping_table)
-    {
-        for (auto &i : row)
-        {
+    for (auto& row : cpu._cpu_mapping_table) {
+        for (auto& i : row) {
             std::cout << i << ",";
         }
         std::cout << std::endl;
@@ -518,19 +516,15 @@ void set_cpu_used(const std::vector<int>& cpu_ids, const int used) {
         ov::threading::update_proc_type_table(cpu._cpu_mapping_table, cpu._numa_nodes, cpu._proc_type_table);
     }
     std::cout << "step 22 _cpu_mapping_table " << std::endl;
-    for (auto &row : cpu._cpu_mapping_table)
-    {
-        for (auto &i : row)
-        {
+    for (auto& row : cpu._cpu_mapping_table) {
+        for (auto& i : row) {
             std::cout << i << ",";
         }
         std::cout << std::endl;
     }
     std::cout << "step 23 _proc_type_table " << std::endl;
-    for (auto &row : cpu._proc_type_table)
-    {
-        for (auto &i : row)
-        {
+    for (auto& row : cpu._proc_type_table) {
+        for (auto& i : row) {
             std::cout << i << ",";
         }
         std::cout << std::endl;
