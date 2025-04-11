34b1fa4478db215d21fc88f34fe744dcf6a29e7d
Tomasz Krupa <tomasz.krupa@intel.com>
2025-04-11 13:04:58 +0000

    Add test

--- src/core/src/op/util/weightless_caching_attributes.cpp
+++ src/core/src/op/util/weightless_caching_attributes.cpp
@@ -17,8 +17,7 @@ OPENVINO_API void ov::copy_weightless_cache_attr(const std::shared_ptr<ov::Node>
 
     if (weightless_caching_attr != rt_info.end()) {
         to->get_rt_info()[ov::WeightlessCacheAttribute::get_type_info_static()] = weightless_caching_attr->second;
-    } else if (from->get_type_info() == ov::op::v0::Convert::get_type_info_static() &&
-               to->get_type_info() == ov::op::v0::Constant::get_type_info_static()) {
+    } else if (ov::is_type<ov::op::v0::Convert>(from) && ov::is_type<ov::op::v0::Constant>(to)) {
         auto convert_node = ov::as_type_ptr<ov::op::v0::Convert>(from);
         auto const_node = convert_node->get_input_node_ptr(0);
         weightless_caching_attr = const_node->get_rt_info().find(ov::WeightlessCacheAttribute::get_type_info_static());
