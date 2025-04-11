e83447b3a01337351e672bb7469734c1c7bd1ed4
Labman42 <ybian@umd.edu>
2025-04-11 17:56:48 -0400

    add copyright

--- src/frontends/pytorch/src/op/renorm.cpp
+++ src/frontends/pytorch/src/op/renorm.cpp
@@ -1,3 +1,6 @@
+// Copyright (C) 2018-2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
 #include "openvino/frontend/pytorch/node_context.hpp"
 #include "openvino/op/abs.hpp"
 #include "openvino/op/broadcast.hpp"
--- tests/layer_tests/pytorch_tests/test_renorm.py
+++ tests/layer_tests/pytorch_tests/test_renorm.py
@@ -1,3 +1,6 @@
+# Copyright (C) 2018-2025 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
 import pytest
 import torch
 import numpy as np
