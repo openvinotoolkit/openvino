a19593940ec037ce5beb67a1ce24beb921df61db
Kim, SungEun <sungeun.kim@intel.com>
2025-04-14 05:57:37 +0900

    remove 'coverity'

--- src/plugins/intel_gpu/src/plugin/transformations/decompose_reduce_scalar_output.cpp
+++ src/plugins/intel_gpu/src/plugin/transformations/decompose_reduce_scalar_output.cpp
@@ -34,7 +34,7 @@
 ov::intel_gpu::DecomposeReduceForScalarOutput::DecomposeReduceForScalarOutput() {
     auto check_reduce_shape = [=](Output<Node> output) -> bool {
         const auto reduce = ov::as_type_ptr<op::util::ArithmeticReductionKeepDims>(output.get_node_shared_ptr());
-        OPENVINO_ASSERT(reduce != nullptr, "[coverity] output is not reduce.");
+        OPENVINO_ASSERT(reduce != nullptr, "output is not reduce.");
         auto& input_shape = reduce->input_value(0).get_partial_shape();
         auto& reduce_shape = reduce->input_value(1).get_partial_shape();
         if (reduce_shape.is_dynamic() || reduce_shape.size() != 1) {
