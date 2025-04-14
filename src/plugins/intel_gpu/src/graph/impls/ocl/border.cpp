33420b5981c4d3acf7a8d428ba92db2dd19bfe3a
Kim, SungEun <sungeun.kim@intel.com>
2025-04-14 19:49:07 +0900

    remove checking zero_input

--- src/plugins/intel_gpu/src/graph/impls/ocl/border.cpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl/border.cpp
@@ -154,7 +154,7 @@ protected:
         const auto& prim_params = static_cast<const kernel_selector::border_params&>(*_kernel_data.params);
         std::vector<BufferDescriptor> internal_buffers;
 
-        if (zero_input || prim_params.inputs[0].LogicalSize() == 0) {
+        if (prim_params.inputs[0].LogicalSize() == 0) {
             internal_buffers.emplace_back(1, ov::element::u8);
         }
 
