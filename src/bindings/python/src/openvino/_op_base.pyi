26a398b7e12b98743e252a50f42f7cfd2f816478
p-wysocki <przemyslaw.wysocki@intel.com>
2025-04-14 15:31:57 +0200

    Cleanup
    Signed-off-by: p-wysocki <przemyslaw.wysocki@intel.com>

--- src/bindings/python/src/openvino/_op_base.pyi
+++ src/bindings/python/src/openvino/_op_base.pyi
@@ -3,10 +3,8 @@ from __future__ import annotations
 from openvino._pyopenvino import Node
 from openvino._pyopenvino import Op as OpBase
 from openvino._pyopenvino import Output
-from typing import Any
 import openvino._pyopenvino
-import typing
-__all__ = ['Any', 'Node', 'Op', 'OpBase', 'Output']
+__all__ = ['Node', 'Op', 'OpBase', 'Output']
 class Op(openvino._pyopenvino.Op):
-    def __init__(self, py_obj: Op, inputs: typing.Any = None) -> None:
+    def __init__(self, py_obj: Op, inputs: typing.Union[typing.List[typing.Union[openvino._pyopenvino.Node, openvino._pyopenvino.Output]], typing.Tuple[typing.Union[openvino._pyopenvino.Node, openvino._pyopenvino.Output, typing.List[typing.Union[openvino._pyopenvino.Node, openvino._pyopenvino.Output]]]], NoneType] = None) -> None:
         ...
