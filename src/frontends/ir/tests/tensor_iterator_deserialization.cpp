248442db233165a307250fb05e07dd0a1cee8d40
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2025-04-14 12:09:00 +0200

    Remove unnecessary ops.hpp includes from IR frontend
    Signed-off-by: Tomasz Jankowski <tomasz1.jankowski@intel.com>

--- src/frontends/ir/tests/tensor_iterator_deserialization.cpp
+++ src/frontends/ir/tests/tensor_iterator_deserialization.cpp
@@ -3,8 +3,12 @@
 //
 
 #include "frontend_test.hpp"
-#include "openvino/opsets/opset1.hpp"
-#include "openvino/opsets/opset8.hpp"
+#include "openvino/op/tensor_iterator.hpp"
+#include "openvino/opsets/opset1_decl.hpp"
+#include "openvino/opsets/opset3_decl.hpp"
+#include "openvino/opsets/opset4_decl.hpp"
+#include "openvino/opsets/opset5_decl.hpp"
+#include "openvino/opsets/opset8_decl.hpp"
 
 class IRFrontendTestsTensorIterator : public ::testing::Test, public IRFrontendTestsImpl {
 protected:
