eca8b74eac614b5d13061210ef1ea94e0d846fa6
Tikhonov Ivan <ivan.tikhonov@intel.com>
2025-04-15 13:21:14 +0400

    Fix coverity issue

--- src/common/transformations/src/transformations/convert_precision.cpp
+++ src/common/transformations/src/transformations/convert_precision.cpp
@@ -1065,10 +1065,15 @@ bool extend_reverse_type(const std::shared_ptr<ov::Node>& node, const precisions
 
 template <typename src_type, typename dst_type>
 inline dst_type convert_value(src_type val) {
-    if (val > std::numeric_limits<dst_type>::max()) {
-        return std::numeric_limits<dst_type>::max();
-    } else if (val < std::numeric_limits<dst_type>::lowest()) {
-        return std::numeric_limits<dst_type>::lowest();
+    if (std::numeric_limits<src_type>::max() > std::numeric_limits<dst_type>::max()) {
+        if (val > std::numeric_limits<dst_type>::max()) {
+            return std::numeric_limits<dst_type>::max();
+        }
+    }
+    if (std::numeric_limits<src_type>::lowest() < std::numeric_limits<dst_type>::lowest()) {
+        if (val < std::numeric_limits<dst_type>::lowest()) {
+            return std::numeric_limits<dst_type>::lowest();
+        }
     }
     return static_cast<dst_type>(val);
 }
