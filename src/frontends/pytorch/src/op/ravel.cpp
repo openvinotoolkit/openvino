4b25bf9e6228d057661f4478ddc87d9f0372f02b
Ziad Hassan <ziadmhassan@aucegypt.edu>
2025-04-15 15:04:01 +0200

    Added support for aten::ravel and made minor fixes

---
+++ src/frontends/pytorch/src/op/ravel.cpp
@@ -0,0 +1,44 @@
+// Copyright (C) 2018-2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "openvino/frontend/complex_type_mark.hpp"
+#include "openvino/frontend/pytorch/node_context.hpp"
+#include "openvino/op/reshape.hpp"
+#include "utils.hpp"
+
+namespace ov {
+namespace frontend {
+namespace pytorch {
+namespace op {
+
+using namespace ov::op;
+
+OutputVector translate_ravel(const NodeContext& context) {
+    // Schema: aten::ravel(Tensor input) -> Tensor
+    // The ravel operation is equivalent to flattening the tensor.
+    num_inputs_check(context, 1, 1, true);
+    auto tensor = context.get_input(0);
+    auto new_shape = context.mark_node(v0::Constant::create(element::i32, Shape{1}, {-1}));
+
+    auto complex_type_mark = as_type_ptr<ComplexTypeMark>(tensor.get_node_shared_ptr());
+    if (complex_type_mark) {
+        tensor = complex_type_mark->get_data();
+
+        new_shape = context.mark_node(v0::Constant::create(element::i32, Shape{2}, {-1, 2}));
+    }
+
+    auto reshape = context.mark_node(std::make_shared<v1::Reshape>(tensor, new_shape, false));
+
+    if (complex_type_mark) {
+        const auto& complex_dtype = complex_type_mark->get_complex_part_type();
+        return {context.mark_node(std::make_shared<ComplexTypeMark>(reshape, complex_dtype))};
+    } else {
+        return {reshape};
+    }
+};
+
+}  // namespace op
+}  // namespace pytorch
+}  // namespace frontend
+}  // namespace ov
