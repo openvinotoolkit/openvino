d04d7acbcb695444371e8dc2b2d94661890323e0
Ahn, Paul Y <paul.y.ahn@intel.com>
2025-04-17 01:04:31 +0900

    Follow-up code review

--- src/plugins/intel_gpu/src/graph/impls/onednn/fully_connected_onednn.cpp
+++ src/plugins/intel_gpu/src/graph/impls/onednn/fully_connected_onednn.cpp
@@ -370,11 +370,11 @@ public:
                 auto ifm = arg.get_dependency(1).get_output_layout().get_dim(1);
                 auto ngroups = scale_layout.get_dim(1);
                 group_size = ifm / ngroups;
+                OPENVINO_ASSERT((ngroups == 1 || group_size % 32 == 0), "group_size should be aligned to 32 if it is not a single scale group");
                 if (!is_four_bit_weight) {
                     // 8-bit quantized weight
                     attr->set_scales(DNNL_ARG_WEIGHTS, per_oc, dnnl::memory::dims{}, ds_data_type);
                 } else {
-                    OPENVINO_ASSERT((ngroups == 1 || group_size % 32 == 0), "group_size should be aligned to 32 if it is not a single scale group");
                     // OneDNN does not support scalar zero-point for s4 and u8 type. Need to broadcast it.
                     attr->set_scales(DNNL_ARG_WEIGHTS, grouped, {group_size, 1}, ds_data_type);
                 }
