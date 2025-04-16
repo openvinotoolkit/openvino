8ac1d478eef66211d98d0fc04ee568e15d6adb97
XinyuWuu <xinyu.wuxinyu@outlook.com>
2025-04-16 19:41:09 +0800

    random number generator

--- tests/layer_tests/pytorch_tests/test_isin.py
+++ tests/layer_tests/pytorch_tests/test_isin.py
@@ -4,7 +4,6 @@
 import numpy as np
 import pytest
 import torch
-from numpy.random import randint, randn
 from pytorch_layer_test_class import PytorchLayerTest
 
 
@@ -18,7 +17,14 @@ class aten_isin(torch.nn.Module):
 
 
 class TestIsin(PytorchLayerTest):
-    def _prepare_input(self, in_type, inputs, scalar):
+    def _prepare_input(self, in_type_idx, inputs_idx, scalar):
+        rng = np.random.default_rng(12345)
+        inputs = [
+            (np.concatenate([np.ones(2), rng.random(5)]), np.ones([20, 20])),
+            (rng.integers(-10, 10, (10,)), rng.integers(-10, 10, (10, 10))),
+            (rng.integers(-10, 10, (10, 5)), rng.integers(-10, 10, (3, 10, 3))),
+        ][inputs_idx]
+        in_type = [np.float32, np.int32, np.int16, np.float16, np.int8][in_type_idx]
         elements = inputs[0].astype(in_type)
         test_elements = inputs[1].astype(in_type)
         if scalar == 0:
@@ -28,30 +34,14 @@ class TestIsin(PytorchLayerTest):
         return elements, test_elements
 
     @pytest.mark.parametrize("invert", [False, True])
-    @pytest.mark.parametrize(
-        "in_type",
-        [
-            np.float32,
-            np.int32,
-            np.int16,
-            np.float16,
-            np.int8,
-        ],
-    )
+    @pytest.mark.parametrize("in_type_idx", [0, 1, 2, 3, 4])
     @pytest.mark.parametrize("scalar", [0, 1, -1])
-    @pytest.mark.parametrize(
-        "inputs",
-        [
-            (np.concatenate([np.ones(2), randn(5)]), np.ones([20, 20])),
-            (randint(-10, 10, (10,)), randint(-10, 10, (10, 10))),
-            (randint(-10, 10, (10, 5)), randint(-10, 10, (3, 10, 3))),
-        ],
-    )
+    @pytest.mark.parametrize("inputs_idx", [0, 1, 2])
     @pytest.mark.nightly
     @pytest.mark.precommit
     @pytest.mark.precommit_torch_export
     def test_isin(
-        self, invert, in_type, inputs, scalar, ie_device, precision, ir_version
+        self, invert, in_type_idx, inputs_idx, scalar, ie_device, precision, ir_version
     ):
         self._test(
             aten_isin(invert),
@@ -61,8 +51,8 @@ class TestIsin(PytorchLayerTest):
             precision,
             ir_version,
             kwargs_to_prepare_input={
-                "in_type": in_type,
-                "inputs": inputs,
+                "in_type_idx": in_type_idx,
+                "inputs_idx": inputs_idx,
                 "scalar": scalar,
             },
         )
