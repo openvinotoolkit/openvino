3348c89ed5b81c6044df6dbf8be9a9be6e1fd6ec
lsanleo <leonardo.santolin@itsdigitalacademy.com>
2025-04-17 17:47:16 +0200

    Fix #30150: Fix Pyright errors in frontend/frontend.py

--- src/bindings/python/src/openvino/frontend/frontend.py
+++ src/bindings/python/src/openvino/frontend/frontend.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-2025 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from typing import Union
+from typing import Union, Optional, cast, TYPE_CHECKING
 
 from openvino._pyopenvino import FrontEnd as FrontEndBase
 from openvino._pyopenvino import FrontEndManager as FrontEndManagerBase
@@ -17,28 +17,28 @@ class FrontEnd(FrontEndBase):
     def convert(self, model: Union[Model, InputModel]) -> Model:
         converted_model = super().convert(model)
         if isinstance(model, InputModel):
-            return Model(converted_model)
-        return converted_model
+            return Model(converted_model)  # type: ignore
+        return cast(Model, converted_model)
 
     def convert_partially(self, model: InputModel) -> Model:
-        return Model(super().convert_partially(model))
+        return Model(super().convert_partially(model))  # type: ignore
 
     def decode(self, model: InputModel) -> Model:
-        return Model(super().decode(model))
+        return Model(super().decode(model))  # type: ignore
 
     def normalize(self, model: Model) -> None:
         super().normalize(model)
 
 
 class FrontEndManager(FrontEndManagerBase):
-    def load_by_framework(self, framework: str) -> Union[FrontEnd, None]:
+    def load_by_framework(self, framework: str) -> Optional[FrontEnd]:
         fe = super().load_by_framework(framework)
         if fe is not None:
             return FrontEnd(fe)
-        return fe
+        return None
 
-    def load_by_model(self, model: str) -> Union[FrontEnd, None]:
+    def load_by_model(self, model: str) -> Optional[FrontEnd]:
         fe = super().load_by_model(model)
         if fe is not None:
             return FrontEnd(fe)
-        return fe
+        return None
