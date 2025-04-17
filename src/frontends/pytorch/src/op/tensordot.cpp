014c1612d989cb48b666191dd35d6b4ab1e30162
lsanleo <leonardo.santolin@itsdigitalacademy.com>
2025-04-17 11:36:56 +0200

    fix errors in tensordot.cpp file

--- src/frontends/pytorch/src/op/tensordot.cpp
+++ src/frontends/pytorch/src/op/tensordot.cpp
@@ -1,6 +1,6 @@
+#include "openvino/frontend/pytorch/node_context.hpp"
 #include "openvino/op/matmul.hpp"
 #include "openvino/op/reduce_sum.hpp"
-#include "openvino/frontend/pytorch/node_context.hpp"
 #include "utils.hpp"
 
 namespace ov {
@@ -12,17 +12,17 @@ using namespace ov::op;
 
 OutputVector translate_tensordot(const NodeContext& context) {
     num_inputs_check(context, 3, 4);
-    
+
     auto a = context.get_input(0);
     auto b = context.get_input(1);
     auto matmul_result = context.mark_node(std::make_shared<v0::MatMul>(a, b, false, false));
-    
+
     if (!context.input_is_none(2)) {
-        auto dims_a = context.get_input(2);  
+        auto dims_a = context.get_input(2);
         auto reduce_sum_result = context.mark_node(std::make_shared<v1::ReduceSum>(matmul_result, dims_a, true));
         return {reduce_sum_result};
     }
-    
+
     return {matmul_result};
 }
 
