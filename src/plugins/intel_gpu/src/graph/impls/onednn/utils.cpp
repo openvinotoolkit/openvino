1b6f1e1ea7049d60504ff829dbb8f6a812bc95b6
Ahn, Paul Y <paul.y.ahn@intel.com>
2025-04-19 02:40:02 +0900

    Support strides for updated target format after rebase

--- src/plugins/intel_gpu/src/graph/impls/onednn/utils.cpp
+++ src/plugins/intel_gpu/src/graph/impls/onednn/utils.cpp
@@ -326,13 +326,35 @@ dnnl::memory::desc layout_to_memory_desc(cldnn::layout l, dnnl::memory::format_t
         if (target_fmt == dnnl::memory::format_tag::ab) {
             strides.push_back(1);
             strides.push_back(padded_dims[0]);
-        } else if (target_fmt == dnnl::memory::format_tag::ba) {
-            strides.push_back(1);
-            strides.push_back(padded_dims[1]);
         } else if (target_fmt == dnnl::memory::format_tag::abc) {
             strides.push_back(1);
             strides.push_back(padded_dims[0]);
             strides.push_back(padded_dims[0] * padded_dims[1]);
+        } else if (target_fmt == dnnl::memory::format_tag::acb) {
+            strides.push_back(1);
+            strides.push_back(padded_dims[0]);
+            strides.push_back(padded_dims[0] * padded_dims[2]);
+        } else if (target_fmt == dnnl::memory::format_tag::abdc) {
+            strides.push_back(1);
+            strides.push_back(padded_dims[0]);
+            strides.push_back(padded_dims[0] * padded_dims[1]);
+            strides.push_back(padded_dims[0] * padded_dims[1] * padded_dims[3]);
+        } else if (target_fmt == dnnl::memory::format_tag::abced) {
+            strides.push_back(1);
+            strides.push_back(padded_dims[0]);
+            strides.push_back(padded_dims[0] * padded_dims[1]);
+            strides.push_back(padded_dims[0] * padded_dims[1] * padded_dims[2]);
+            strides.push_back(padded_dims[0] * padded_dims[1] * padded_dims[2] * padded_dims[4]);
+        } else if (target_fmt == dnnl::memory::format_tag::abcdfe) {
+            strides.push_back(1);
+            strides.push_back(padded_dims[0]);
+            strides.push_back(padded_dims[0] * padded_dims[1]);
+            strides.push_back(padded_dims[0] * padded_dims[1] * padded_dims[2]);
+            strides.push_back(padded_dims[0] * padded_dims[1] * padded_dims[2] * padded_dims[3]);
+            strides.push_back(padded_dims[0] * padded_dims[1] * padded_dims[2] * padded_dims[3] * padded_dims[5]);
+        } else if (target_fmt == dnnl::memory::format_tag::ba) {
+            strides.push_back(1);
+            strides.push_back(padded_dims[1]);
         } else {
             auto pitches = l.get_pitches();
             strides.assign(pitches.begin(), pitches.end());
