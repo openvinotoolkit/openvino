e68f22127342903ace06f9daa87939ab853350ab
Prabha14039 <abhinavg760@gmail.com>
2025-04-19 13:19:46 +0530

    Fix input handling in Op class and enable pyright for _op_base.py - Enable pyright type checking, improve input handling with list flattening and Output type filtering

--- src/bindings/python/src/openvino/_op_base.py
+++ src/bindings/python/src/openvino/_op_base.py
@@ -13,9 +13,22 @@ class Op(OpBase):
         super().__init__(py_obj)
         self._update_type_info()
         if isinstance(inputs, tuple):
-            inputs = None if len(inputs) == 0 else list(inputs)
+            # inputs = None if len(inputs) == 0 else list(inputs)
+            if len(inputs) == 0:
+                inputs = None
             if inputs is not None and len(inputs) == 1 and isinstance(inputs[0], list):
                 inputs = inputs[0]
+
+        if isinstance(inputs,list):
+            flat_list = []
+            for items in inputs:
+                if isinstance(items,list):
+                    flat_list.extend(items)
+                else:
+                    flat_list.append(items)
+            inputs = flat_list
+
         if inputs is not None:
-            self.set_arguments(inputs)
+            output_list = [item for item in inputs if isinstance(item, Output)]
+            self.set_arguments(output_list)
             self.constructor_validate_and_infer_types()
