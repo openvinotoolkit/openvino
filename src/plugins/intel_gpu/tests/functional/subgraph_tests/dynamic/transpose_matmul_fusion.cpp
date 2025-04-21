41ce278e1ee5235a568ccddb1a36ada214cbc815
Wang, Yang <yang4.wang@intel.com>
2025-04-21 15:56:02 +0800

    update.

--- src/plugins/intel_gpu/tests/functional/subgraph_tests/dynamic/transpose_matmul_fusion.cpp
+++ src/plugins/intel_gpu/tests/functional/subgraph_tests/dynamic/transpose_matmul_fusion.cpp
@@ -2,14 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include "common_test_utils/ov_tensor_utils.hpp"
+#include "openvino/op/matmul.hpp"
+#include "openvino/op/transpose.hpp"
 #include "shared_test_classes/base/ov_subgraph.hpp"
-#include "subgraphs_builders.hpp"
 
 namespace {
 
 using TransposeOrderParams = std::tuple<std::vector<int64_t>,  // allowed transpose orders
                                         ov::element::Type>;    // input precision
-class TransposeMatmulFuseTest : public testing::WithParamInterface<TransposeOrderParams>, virtual public ov::test::SubgraphBaseTest {
+class TransposeMatmulFuseTest : public ::testing::Test, public testing::WithParamInterface<TransposeOrderParams> {
 public:
     static std::string getTestCaseName(testing::TestParamInfo<TransposeOrderParams> obj) {
         std::vector<int64_t> target_order;
@@ -27,11 +29,9 @@ public:
     }
 
 protected:
-    std::shared_ptr<ov::Model> init_subgraph(std::vector<ov::PartialShape>& input_shapes,
-                                             ov::element::Type& input_precision,
-                                             const std::vector<int64_t>& target_transpose_order) {
-        ov::PartialShape input_a_shape = input_shapes[0];
-        ov::PartialShape input_b_shape = input_shapes[1];
+    std::shared_ptr<ov::Model> init_subgraph(ov::element::Type& input_precision, const std::vector<int64_t>& target_transpose_order) {
+        ov::PartialShape input_a_shape = ov::PartialShape{-1, -1, -1, -1};
+        ov::PartialShape input_b_shape = ov::PartialShape{-1, -1};
 
         auto input_a = std::make_shared<ov::op::v0::Parameter>(input_precision, input_a_shape);
         auto input_b = std::make_shared<ov::op::v0::Parameter>(input_precision, input_b_shape);
@@ -45,28 +45,6 @@ protected:
         return model;
     }
 
-    void SetUp() override {
-        targetDevice = ov::test::utils::DEVICE_GPU;
-        std::tie(order, input_precision) = GetParam();
-        ov::Shape input_a_shape = {5, 6, 7, 8};
-        ov::Shape input_b_shape = {8, 7};
-        ov::Shape permuted_shape_a(input_a_shape.size());
-        for (size_t i = 0; i < permuted_shape_a.size(); ++i) {
-            permuted_shape_a[i] = input_a_shape[order[i]];
-        }
-        ov::Shape permuted_shape_b(input_b_shape.size());
-        for (size_t i = 0; i < permuted_shape_b.size(); ++i) {
-            permuted_shape_b[i] = input_a_shape[order[order.size() - 1 - i]];
-        }
-        std::vector<ov::test::InputShape> permuted_input_shapes_dyn = {
-            std::make_pair(ov::PartialShape{-1, -1, -1, -1}, std::vector<ov::Shape>{input_a_shape}),
-            std::make_pair(ov::PartialShape{-1, -1}, std::vector<ov::Shape>{permuted_shape_b}),
-        };
-
-        init_input_shapes(permuted_input_shapes_dyn);
-        function = init_subgraph(inputDynamicShapes, input_precision, order);
-    }
-
 private:
     ov::element::Type input_precision = ov::element::f16;
     std::vector<int64_t> order = {0, 1, 2, 3};
@@ -76,8 +54,24 @@ TEST_P(TransposeMatmulFuseTest, smoke_allowed_transpose_order) {
     std::vector<int64_t> target_order;
     ov::element::Type input_precision;
     std::tie(target_order, input_precision) = GetParam();
+    auto function = init_subgraph(input_precision, target_order);
+
+    std::string targetDevice = ov::test::utils::DEVICE_GPU;
+    ov::Shape input_a_shape = {5, 6, 7, 8};
+    ov::Shape input_b_shape = {8, 7};
+    ov::Shape permuted_shape_b(input_b_shape.size());
+    for (size_t i = 0; i < permuted_shape_b.size(); ++i) {
+        permuted_shape_b[i] = input_a_shape[target_order[target_order.size() - 1 - i]];
+    }
+
+    auto input_tensor_a = ov::test::utils::create_and_fill_tensor(input_precision, input_a_shape, 0.0f, 1.0f);
+    auto input_tensor_b = ov::test::utils::create_and_fill_tensor(input_precision, permuted_shape_b, 0.0f, 1.0f);
+
+    auto core = ov::test::utils::PluginCache::get().core();
     ov::CompiledModel cM = core->compile_model(function, targetDevice, {ov::hint::inference_precision(input_precision)});
     auto request = cM.create_infer_request();
+    request.set_input_tensor(0, ov::Tensor(input_precision, input_a_shape, input_tensor_a.data()));
+    request.set_input_tensor(1, ov::Tensor(input_precision, permuted_shape_b, input_tensor_b.data()));
     request.infer();
 }
 
