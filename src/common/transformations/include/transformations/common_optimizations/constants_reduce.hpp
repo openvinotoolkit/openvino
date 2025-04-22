7294f31206f39cfc8c88b80a2623f6f8c7cf46ad
Damian Kurek <damian.kurek@intel.com>
2025-04-22 13:12:29 +0200

    Update constants_reduce.hpp

--- src/common/transformations/include/transformations/common_optimizations/constants_reduce.hpp
+++ src/common/transformations/include/transformations/common_optimizations/constants_reduce.hpp
@@ -12,7 +12,6 @@ namespace ov::pass {
 class TRANSFORMATIONS_API ConstantsReduce : public ov::pass::ModelPass {
 public:
     OPENVINO_MODEL_PASS_RTTI("ConstantsReduce");
-    ConstantsReduce() = default;
     bool run_on_model(const std::shared_ptr<ov::Model>& m) override;
 };
 
