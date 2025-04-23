2704d8f0cf69eefae65a2e97c4d3a8e8e0fbc468
HU Yuan2 <yuan2.hu@intel.com>
2025-04-23 19:36:45 +0800

    simple code and add boradcast checking
    Signed-off-by: HU Yuan2 <yuan2.hu@intel.com>

--- src/plugins/intel_cpu/src/shape_inference/custom/scaled_attn.cpp
+++ src/plugins/intel_cpu/src/shape_inference/custom/scaled_attn.cpp
@@ -21,49 +21,8 @@ public:
         const auto& query_dims = input_shapes.front().get();
         VectorDims present_v_dims = input_shapes.back().get();
         const auto& beam_idx_dims = input_shapes.end()[-3].get();
-        const auto& permute_axes = m_config.permute_axes;
-        if (permute_axes.empty()) {
-            // [B, H, L, S]
-            present_v_dims[0] = beam_idx_dims[0];
-            present_v_dims[2] += query_dims[2];
-            if (input_shapes.size() == 7 && !m_config.is_causal) {
-                const auto& attn_mask_dims = input_shapes[3].get();
-                if ((query_dims[2] != attn_mask_dims[2] && attn_mask_dims[2] != 1) ||
-                    (present_v_dims[2] != attn_mask_dims[3] && attn_mask_dims[3] != 1)) {
-                    const auto& cur_k_dims = input_shapes[1].get();
-                    const auto& cur_v_dims = input_shapes[2].get();
-                    const auto& cache_k_dims = input_shapes[5].get();
-                    const auto& cache_v_dims = input_shapes[6].get();
-                    OPENVINO_THROW("attention_mask do not match q and k,",
-                                   " query_dims:",
-                                   ov::intel_cpu::vec2str(query_dims),
-                                   " cur_k_dims:",
-                                   ov::intel_cpu::vec2str(cur_k_dims),
-                                   " cur_v_dims:",
-                                   ov::intel_cpu::vec2str(cur_v_dims),
-                                   " attn_mask_dims:",
-                                   ov::intel_cpu::vec2str(attn_mask_dims),
-                                   " beam_idx_dims:",
-                                   ov::intel_cpu::vec2str(beam_idx_dims),
-                                   " cache_k_dims:",
-                                   ov::intel_cpu::vec2str(cache_k_dims),
-                                   " cache_v_dims:",
-                                   ov::intel_cpu::vec2str(cache_v_dims));
-                }
-            }
-            // normal and fast path
-            if (present_v_dims[3] == query_dims[3]) {
-                return {{query_dims, present_v_dims, present_v_dims}, ShapeInferStatus::success};
-            }
-
-            // diff kv feature size
-            auto output_dims = query_dims;
-            output_dims[3] = present_v_dims[3];
-            auto present_k_dims = present_v_dims;
-            present_k_dims[3] = query_dims[3];
-            return {{output_dims, present_k_dims, present_v_dims}, ShapeInferStatus::success};
-        }
-
+        const auto& permute_axes_origin = m_config.permute_axes;
+        auto permute_axes = (permute_axes_origin.empty()) ? std::vector<size_t>{0, 1, 2, 3} : permute_axes_origin;
         // permute_axes[0,1,2,3] gives axis indices of B,H,L,S for query & present_kv
         const size_t batch_index = permute_axes[0];
         const size_t length_index = permute_axes[2];
@@ -75,43 +34,56 @@ public:
         for (size_t i = 0; i < n_dims; i++) {
             output_dims[i] = query_dims[permute_axes[i]];
         }
-
         if (input_shapes.size() == 7 && !m_config.is_causal) {
             const auto& attn_mask_dims = input_shapes[3].get();
-            if ((query_dims[length_index] != attn_mask_dims[2] && attn_mask_dims[2] != 1) ||
-                (present_v_dims[length_index] != attn_mask_dims[3] && attn_mask_dims[3] != 1)) {
+            bool attn_mask_ok = true;
+            auto attn_mask_dims_size = attn_mask_dims.size();
+            if (attn_mask_dims_size >= 2) {
+                auto check_broadcast = [](const size_t& target, const size_t& to) -> bool {
+                    return (target != to && target != 1) ? false : true;
+                };
+                auto weight_dims = output_dims;
+                weight_dims[3] = present_v_dims[length_index];
+                auto offset = weight_dims.size() - attn_mask_dims_size;
+                for (int i = attn_mask_dims_size - 1; i >= 0; i--) {
+                    attn_mask_ok = attn_mask_ok && check_broadcast(attn_mask_dims[i], weight_dims[i + offset]);
+                }
+            } else {
+                attn_mask_ok = false;
+            }
+            if (!attn_mask_ok) {
                 const auto& cur_k_dims = input_shapes[1].get();
                 const auto& cur_v_dims = input_shapes[2].get();
                 const auto& cache_k_dims = input_shapes[5].get();
                 const auto& cache_v_dims = input_shapes[6].get();
                 OPENVINO_THROW("attention_mask do not match q and k,",
-                               " query_dims:",
-                               ov::intel_cpu::vec2str(query_dims),
-                               " cur_k_dims:",
-                               ov::intel_cpu::vec2str(cur_k_dims),
-                               " cur_v_dims:",
-                               ov::intel_cpu::vec2str(cur_v_dims),
-                               " attn_mask_dims:",
-                               ov::intel_cpu::vec2str(attn_mask_dims),
-                               " beam_idx_dims:",
-                               ov::intel_cpu::vec2str(beam_idx_dims),
-                               " cache_k_dims:",
-                               ov::intel_cpu::vec2str(cache_k_dims),
-                               " cache_v_dims:",
-                               ov::intel_cpu::vec2str(cache_v_dims));
+                        " query_dims:",
+                        ov::intel_cpu::vec2str(query_dims),
+                        " cur_k_dims:",
+                        ov::intel_cpu::vec2str(cur_k_dims),
+                        " cur_v_dims:",
+                        ov::intel_cpu::vec2str(cur_v_dims),
+                        " attn_mask_dims:",
+                        ov::intel_cpu::vec2str(attn_mask_dims),
+                        " beam_idx_dims:",
+                        ov::intel_cpu::vec2str(beam_idx_dims),
+                        " cache_k_dims:",
+                        ov::intel_cpu::vec2str(cache_k_dims),
+                        " cache_v_dims:",
+                        ov::intel_cpu::vec2str(cache_v_dims));
             }
         }
 
         // normal and fast path
         if (present_v_dims[3] == query_dims[3]) {
             return {{output_dims, present_v_dims, present_v_dims}, ShapeInferStatus::success};
+        } else {
+            // diff kv feature size
+            output_dims[3] = present_v_dims[3];
+            auto present_k_dims = present_v_dims;
+            present_k_dims[3] = query_dims[3];
+            return {{output_dims, present_k_dims, present_v_dims}, ShapeInferStatus::success};
         }
-
-        // diff kv feature size
-        output_dims[3] = present_v_dims[3];
-        auto present_k_dims = present_v_dims;
-        present_k_dims[3] = query_dims[3];
-        return {{output_dims, present_k_dims, present_v_dims}, ShapeInferStatus::success};
     }
 
     [[nodiscard]] port_mask_t get_port_mask() const override {
