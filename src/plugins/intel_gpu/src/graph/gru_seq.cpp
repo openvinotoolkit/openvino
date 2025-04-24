98e5f4594ff9d3406cadf5830fd21dc974da7aa9
Michal Miotk <michal.miotk@intel.com>
2025-04-24 12:10:22 +0200

    faster output layout calc

--- src/plugins/intel_gpu/src/graph/gru_seq.cpp
+++ src/plugins/intel_gpu/src/graph/gru_seq.cpp
@@ -11,7 +11,22 @@ namespace cldnn {
 GPU_DEFINE_PRIMITIVE_TYPE_ID(gru_seq)
 
 layout gru_seq_inst::calc_output_layout(gru_seq_node const& node, kernel_impl_params const& impl_param) {
-    return gru_seq_inst::calc_output_layouts<ov::PartialShape>(node, impl_param)[0];
+    auto desc = impl_param.typed_desc<gru_seq>();
+
+    const auto& input_layout = impl_param.get_input_layout(0);
+    const auto& input_pshape = input_layout.get_partial_shape();
+    const auto& input_layout_hidden = impl_param.get_input_layout(1);
+    const auto& input_pshape_hidden = input_layout_hidden.get_partial_shape();
+    const auto& gru_batch_size = input_pshape[0];
+    const auto& gru_seq_length = input_pshape[1];
+    const auto& gru_hidden_size = input_pshape_hidden[2];
+
+    auto first_out_fmt = cldnn::format::bfyx;
+    if (node.get_preferred_output_fmt() != format::any) {
+        first_out_fmt = node.get_preferred_output_fmt();
+    }
+
+    return cldnn::layout{ov::PartialShape{gru_batch_size, desc->num_directions(), gru_seq_length, gru_hidden_size}, input_layout.data_type, first_out_fmt};
 }
 
 template<typename ShapeType>
