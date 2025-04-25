2831961651c5a3a58379748677d09c1125799c20
Pawel Raasz <pawel.raasz@intel.com>
2025-04-25 11:19:25 +0000

    Fix control flow issues
    Signed-off-by: Pawel Raasz <pawel.raasz@intel.com>

--- src/core/reference/include/openvino/reference/divide.hpp
+++ src/core/reference/include/openvino/reference/divide.hpp
@@ -40,18 +40,22 @@ T try_python_div(const T x, const T y) {
     }
 
     T quot = div(x, y);
-    return (((x < 0) != (y < 0)) && (x % y != 0)) ? quot - T{1} : quot;
+    if constexpr (std::is_unsigned_v<T>) {
+        return quot;
+    } else {
+        return (((x < 0) != (y < 0)) && (x % y != 0)) ? quot - T{1} : quot;
+    }
 }
 }  // namespace func
 
 template <typename T>
-typename std::enable_if<std::is_integral<T>::value>::type divide(const T* arg0,
-                                                                 const T* arg1,
-                                                                 T* out,
-                                                                 const Shape& arg0_shape,
-                                                                 const Shape& arg1_shape,
-                                                                 const op::AutoBroadcastSpec& broadcast_spec,
-                                                                 bool pythondiv) {
+std::enable_if_t<std::is_integral_v<T>> divide(const T* arg0,
+                                               const T* arg1,
+                                               T* out,
+                                               const Shape& arg0_shape,
+                                               const Shape& arg1_shape,
+                                               const op::AutoBroadcastSpec& broadcast_spec,
+                                               bool pythondiv) {
     auto div = pythondiv ? func::try_python_div<T> : func::try_div<T>;
     autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, div);
 }
@@ -59,8 +63,7 @@ typename std::enable_if<std::is_integral<T>::value>::type divide(const T* arg0,
 // In English: return type is void and T must be a standard floating point type, or
 // bfloat16, or float16.
 template <typename T>
-typename std::enable_if<std::is_floating_point<T>::value || std::is_same<T, bfloat16>::value ||
-                        std::is_same<T, float16>::value>::type
+std::enable_if_t<std::is_floating_point_v<T> || std::is_same_v<T, bfloat16> || std::is_same_v<T, float16>>
 divide(const T* arg0, const T* arg1, T* out, size_t count, bool) {
     // TODO: Here we do not check for div by zero, so we'll get +-inf here
     // if arg1[i] == 0. Is that the right thing to do? Jury's still out.
@@ -68,15 +71,14 @@ divide(const T* arg0, const T* arg1, T* out, size_t count, bool) {
 }
 
 template <typename T>
-typename std::enable_if<std::is_floating_point<T>::value || std::is_same<T, bfloat16>::value ||
-                        std::is_same<T, float16>::value>::type
-divide(const T* arg0,
-       const T* arg1,
-       T* out,
-       const Shape& arg0_shape,
-       const Shape& arg1_shape,
-       const op::AutoBroadcastSpec& broadcast_spec,
-       bool) {
+std::enable_if_t<std::is_floating_point_v<T> || std::is_same_v<T, bfloat16> || std::is_same_v<T, float16>> divide(
+    const T* arg0,
+    const T* arg1,
+    T* out,
+    const Shape& arg0_shape,
+    const Shape& arg1_shape,
+    const op::AutoBroadcastSpec& broadcast_spec,
+    bool) {
     autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, func::div<T>);
 }
 }  // namespace reference
