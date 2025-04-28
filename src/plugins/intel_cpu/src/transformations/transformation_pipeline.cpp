1b2006934f1512861dd5cec707a88c6f38d4d216
Zhang Yi <yi3.zhang@intel.com>
2025-04-28 16:43:13 +0800

    [CPU]Enable 4bit cache compression for pagedAttn (#29763)
    ### Details:
     - *Key cache of pagedAttn supports u4 by_channel/by_hidden compression*
    - *Value Cache of pagedAttn supports u4 by_channel/bu_hidden
    compression*
    - *Add cpu internal property VALUE_CACHE_QUANT_MODE to control value
    compression schema*
    
    ### Tickets:
     - *CVS-161864*

--- src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
+++ src/plugins/intel_cpu/src/transformations/transformation_pipeline.cpp
@@ -469,9 +469,10 @@ void Transformations::PreLpt(const std::vector<ov::element::Type>& defaultPrecis
     cacheConfig.keyCacheBlockSize = 32;
     cacheConfig.valueCacheBlockSize = 32;
 
-    bool byChannel = node::PagedAttention::isQuantByChannel(config.keyCacheQuantMode, cacheConfig.keyCachePrecision);
-    cacheConfig.keyCacheQuantBychannel = byChannel;
-    cacheConfig.valueCacheQuantBychannel = false;
+    cacheConfig.keyCacheQuantBychannel =
+        node::PagedAttention::isQuantByChannel(config.keyCacheQuantMode, config.keyCachePrecision, true);
+    cacheConfig.valueCacheQuantBychannel =
+        node::PagedAttention::isQuantByChannel(config.valueCacheQuantMode, config.valueCachePrecision, false);
     cacheConfig.keyCacheDimOrder = {0, 1, 2, 3};
     cacheConfig.valueCacheDimOrder = {0, 1, 2, 3};
     auto update_paged_attention_shape_func = [](const ov::element::Type& precision,
@@ -486,7 +487,11 @@ void Transformations::PreLpt(const std::vector<ov::element::Type>& defaultPrecis
                 head_size += sizeof(float) * 2 * group_num;
             }
         } else if (precision == ov::element::u4) {
-            head_size += sizeof(float) * 2 * group_num * 2;
+            if (bychannel) {
+                block_size += 2 * sizeof(float) * 2;
+            } else {
+                head_size += sizeof(float) * 2 * group_num * 2;
+            }
         }
     };
     CPU_REGISTER_PASS_COMMON(manager, ov::pass::ConvertPagedAttnInputs, cacheConfig, update_paged_attention_shape_func);
