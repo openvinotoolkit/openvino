1a95fd9206707dd5682b36e2c71d3e345b994c1f
Maxim Vafin <maxim.vafin@intel.com>
2025-04-28 14:57:09 +0200

    Add inlined_extension import
    Signed-off-by: Maxim Vafin <maxim.vafin@intel.com>

--- src/bindings/python/src/openvino/frontend/pytorch/__init__.py
+++ src/bindings/python/src/openvino/frontend/pytorch/__init__.py
@@ -13,5 +13,6 @@ try:
     from openvino.frontend.pytorch.py_pytorch_frontend import ConversionExtensionPytorch as ConversionExtension
     from openvino.frontend.pytorch.py_pytorch_frontend import OpExtensionPytorch as OpExtension
     from openvino.frontend.pytorch.module_extension import ModuleExtension
+    from openvino.frontend.pytorch.inlined_extension import inlined_extension
 except ImportError as err:
     raise ImportError(f"OpenVINO PyTorch frontend is not available, please make sure the frontend is built. {err}")
