d5ea38a877a84dc3f81aa3e3fe8c3cc2d254840d
Michal Miotk <michal.miotk@intel.com>
2025-04-28 13:08:19 +0200

    added validate

--- src/plugins/intel_gpu/src/graph/impls/onednn/gru_seq_onednn.hpp
+++ src/plugins/intel_gpu/src/graph/impls/onednn/gru_seq_onednn.hpp
@@ -4,8 +4,9 @@
 
 #include "gru_seq_inst.h"
 #include "intel_gpu/runtime/utils.hpp"
-#include  "registry/implementation_manager.hpp"
+#include "registry/implementation_manager.hpp"
 #include "transformations/utils/utils.hpp"
+#include "impls/onednn/utils.hpp"
 
 #include <memory>
 
@@ -21,6 +22,40 @@ struct GRUSeqImplementationManager : public ImplementationManager {
     bool validate_impl(const program_node& node) const override {
         assert(node.is_type<gru_seq>());
         return true;
+        const auto& config = node.get_program().get_config();
+        const auto& info = node.get_program().get_engine().get_device_info();
+        if (info.arch == gpu_arch::unknown || !config.get_use_onednn())
+            return false;
+        const auto& gru_seq_node = node.as<gru_seq>();
+        const auto& in_layout = gru_seq_node.get_input_layout(0);
+        const auto& out_layout = gru_seq_node.get_output_layout(0);
+
+        if (node.get_input_layout(0).format != cldnn::format::bfyx && node.get_input_layout(0).format != cldnn::format::fbyx
+            && node.get_input_layout(0).format != cldnn::format::ybfx)
+            return false;
+
+        if (!is_supported_pad(in_layout) || !is_supported_pad(out_layout))
+            return false;
+
+        auto in0_dt = node.get_input_layout(0).data_type;
+        auto in1_dt = node.get_input_layout(1).data_type;
+        auto in2_dt = node.get_input_layout(2).data_type;
+        auto in3_dt = node.get_input_layout(3).data_type;
+        auto in4_dt = node.get_input_layout(4).data_type;
+        auto out0_dt = node.get_output_layout(0).data_type;
+        auto out1_dt = node.get_output_layout(1).data_type;
+        bool f16_case = everyone_is(data_types::f16, in0_dt, in1_dt, in2_dt, in3_dt, in4_dt, out0_dt, out1_dt);
+        bool f32_case = everyone_is(data_types::f32, in0_dt, in1_dt, in2_dt, in3_dt, in4_dt, out0_dt, out1_dt);
+        bool u8u8u8_case = one_of(out0_dt, {data_types::u8, data_types::f32}) && everyone_is(data_types::i8, in3_dt) &&
+            everyone_is(data_types::u8, in0_dt, in1_dt, out1_dt) && everyone_is(data_types::f32, in2_dt, in4_dt);
+        bool f32u8f32_case = everyone_is(data_types::u8, in0_dt) && everyone_is(data_types::i8, in2_dt, in3_dt) &&
+            one_of(out0_dt, {data_types::u8, data_types::f32}) && everyone_is(data_types::f32, in1_dt, in4_dt, out1_dt);
+        bool s8s8s8_case = everyone_is(data_types::i8, in0_dt, in1_dt, out0_dt, out1_dt) && one_of(out0_dt, {data_types::i8, data_types::f32}) &&
+            everyone_is(data_types::f32, in2_dt, in4_dt);
+        bool f32s8f32_case = everyone_is(data_types::i8, in0_dt, in1_dt, in2_dt) && one_of(out0_dt, {data_types::i8, data_types::f32}) &&
+            everyone_is(data_types::f32, in1_dt, in4_dt, out1_dt);
+
+        return f16_case || f32_case || u8u8u8_case || f32u8f32_case || s8s8s8_case || f32s8f32_case;
     }
 
     in_out_fmts_t query_formats(const program_node& node) const override {
