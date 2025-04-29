85cbab3d2f4734aebe75b9f5a1a5253d6b2e3094
Ivan Tikhonov <ivan.tikhonov@intel.com>
2025-04-29 11:04:26 +0400

    Apply suggestions from code review

--- src/core/include/openvino/core/attribute_adapter.hpp
+++ src/core/include/openvino/core/attribute_adapter.hpp
@@ -38,9 +38,6 @@ public:
     virtual void set_as_any(const ov::Any& x) {
         OPENVINO_NOT_IMPLEMENTED;
     }
-    virtual ov::Any get_as_any() {
-        OPENVINO_NOT_IMPLEMENTED;
-    }
 };
 
 /// \brief Provides access to values via get/set methods from an m_value, typically from
@@ -67,9 +64,6 @@ public:
         } else {
             OPENVINO_THROW("Bad cast from: ", x.type_info().name(), " to: ", typeid(VAT).name());
         }
-    }
-    ov::Any get_as_any() override {
-        return {get()};
     }
 };
 
