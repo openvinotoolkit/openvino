c51da57bc1d6ad7d7ce0a8cddd41e613e9653be6
Andrew Kwangwoong Park <andrew.park@intel.com>
2025-04-30 15:09:27 +0900

    [GPU] Implement shape canonicalization for 3D SDPA support (#30303)
    ### Details:
    - Currently there is known limitation of SDPA for GPU plugin that the
    rank of the input shape must be 4. So, the SDPAs of vision embeddings
    merger model among qwen VLM models are decomposed and memory allocation
    issue occurs because it's trying to allocate a very large size for the
    output memory of the decomposed gemm
    - Implement manual SDPA shape canonicalization to extend 3D shape using
    heads_num dimension=1 to 4D shape
    
    ### Tickets:
     - 165817

--- src/plugins/intel_gpu/src/graph/impls/ocl/scaled_dot_product_attention.cpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl/scaled_dot_product_attention.cpp
@@ -236,7 +236,10 @@ protected:
         }
     }
 
-    static kernel_selector::sdpa_configuration get_sdpa_configuration(const kernel_impl_params& impl_param) {
+    static kernel_selector::sdpa_configuration get_sdpa_configuration(const kernel_impl_params& impl_param,
+                                                                      const std::vector<int64_t>& input_q_transpose_order,
+                                                                      const std::vector<int64_t>& input_k_transpose_order,
+                                                                      const std::vector<int64_t>& input_v_transpose_order) {
         kernel_selector::sdpa_configuration config;
 
         auto transpose_pshape = [](const ov::PartialShape& pshape, const std::vector<int64_t>& order) {
@@ -251,9 +254,9 @@ protected:
         };
 
         const auto& desc = impl_param.typed_desc<scaled_dot_product_attention>();
-        const auto query_shape = transpose_pshape(impl_param.get_input_layout(0).get_partial_shape(), desc->input_q_transpose_order);
-        const auto key_shape = transpose_pshape(impl_param.get_input_layout(1).get_partial_shape(), desc->input_k_transpose_order);
-        const auto value_shape = transpose_pshape(impl_param.get_input_layout(2).get_partial_shape(), desc->input_v_transpose_order);
+        const auto query_shape = transpose_pshape(impl_param.get_input_layout(0).get_partial_shape(), input_q_transpose_order);
+        const auto key_shape = transpose_pshape(impl_param.get_input_layout(1).get_partial_shape(), input_k_transpose_order);
+        const auto value_shape = transpose_pshape(impl_param.get_input_layout(2).get_partial_shape(), input_v_transpose_order);
 
         OPENVINO_ASSERT(key_shape == value_shape, "[GPU] The shapes of key and value inputs are expected to be equal");
 
@@ -335,12 +338,38 @@ public:
             data_inputs_num++;
         }
 
-        params.conf = get_sdpa_configuration(impl_param);
+        auto extend_order_in_num_heads_dim = [](const std::vector<int64_t>& order, size_t rank = 4) {
+            if (order.size() == rank) {
+                return order;
+            }
 
-        params.input0_order = desc->input_q_transpose_order;
-        params.input1_order = desc->input_k_transpose_order;
-        params.input2_order = desc->input_v_transpose_order;
-        params.output_order = desc->output_transpose_order;
+            std::vector<int64_t> extended_order(rank, 0);
+            const size_t num_heads_dim = 1;
+            // For 3D dimension, extend it to 4D by adding 1 for num_heads_dim
+            for (size_t i = 0, j = 0; i < rank; ++i) {
+                if (i == num_heads_dim) {
+                    extended_order[num_heads_dim] = 1;
+                } else {
+                    extended_order[i] = (static_cast<size_t>(order[j]) < num_heads_dim) ? order[j] : order[j] + 1;
+                    j++;
+                }
+            }
+            return extended_order;
+        };
+        auto extended_input_q_transpose_order = extend_order_in_num_heads_dim(desc->input_q_transpose_order);
+        auto extended_input_k_transpose_order = extend_order_in_num_heads_dim(desc->input_k_transpose_order);
+        auto extended_input_v_transpose_order = extend_order_in_num_heads_dim(desc->input_v_transpose_order);
+        auto extended_output_transpose_order = extend_order_in_num_heads_dim(desc->output_transpose_order);
+
+        params.conf = get_sdpa_configuration(impl_param,
+                                             extended_input_q_transpose_order,
+                                             extended_input_k_transpose_order,
+                                             extended_input_v_transpose_order);
+
+        params.input0_order = extended_input_q_transpose_order;
+        params.input1_order = extended_input_k_transpose_order;
+        params.input2_order = extended_input_v_transpose_order;
+        params.output_order = extended_output_transpose_order;
 
         if (indirect && has_indirect_inputs(impl_param)) {
             params.beam_table = convert_data_tensor(impl_param.get_input_layout(get_beam_table_id(desc)));
@@ -389,18 +418,48 @@ public:
         return params;
     }
 
+    static kernel_impl_params static_canonicalize_shapes(const kernel_impl_params& impl_params) {
+        auto updated_impl_params = impl_params;
+
+        auto extend_pshape_to_rank_in_num_heads_dim = [](ov::PartialShape pshape, size_t rank = 4) {
+            if (pshape.size() == rank) {
+                return pshape;
+            }
+            const size_t num_heads_dim = 1;
+            pshape.insert(pshape.begin() + num_heads_dim, ov::Dimension(1));
+            return pshape;
+        };
+
+        // For scale of 1D tensor or attention_mask of empty shape, use extend_shape_to_rank_from_end as before
+        for (auto& input_layout : updated_impl_params.input_layouts) {
+            input_layout.set_partial_shape(input_layout.get_partial_shape().size() <= 1 ?
+                                           extend_shape_to_rank_from_end(input_layout.get_partial_shape()) :
+                                           extend_pshape_to_rank_in_num_heads_dim(input_layout.get_partial_shape()));
+        }
+
+        auto& output_layout = updated_impl_params.output_layouts[0];
+        output_layout.set_partial_shape(extend_pshape_to_rank_in_num_heads_dim(output_layout.get_partial_shape()));
+
+        return updated_impl_params;
+    }
+
+    kernel_impl_params canonicalize_shapes(const kernel_impl_params& impl_params) const override {
+        return static_canonicalize_shapes(impl_params);
+    }
+
     static std::unique_ptr<primitive_impl> create(const typed_program_node<scaled_dot_product_attention>& arg, const kernel_impl_params& impl_param) {
         std::vector<kernel_selector::kernel_data> kernels_data;
-        auto sdpa_kernel_params = get_kernel_params(impl_param, impl_param.is_dynamic());
         auto& kernel_selector = kernel_selector_t::Instance();
-        kernels_data.push_back(kernel_selector.get_best_kernel(sdpa_kernel_params));
+        auto params = impl_param.output_layouts[0].get_partial_shape().size() == 4 ? impl_param : static_canonicalize_shapes(impl_param);
 
-        if (has_indirect_inputs(impl_param)) {
-            auto indirect_kernel_params = get_kernel_params(impl_param, impl_param.is_dynamic(), true);
+        auto sdpa_kernel_params = get_kernel_params(params, params.is_dynamic());
+        kernels_data.push_back(kernel_selector.get_best_kernel(sdpa_kernel_params));
+        if (has_indirect_inputs(params)) {
+            auto indirect_kernel_params = get_kernel_params(params, params.is_dynamic(), true);
             kernels_data.push_back(kernel_selector.get_best_kernel(indirect_kernel_params));
         }
 
-        const auto& gfx_ver = impl_param.get_program().get_engine().get_device_info().gfx_ver;
+        const auto& gfx_ver = params.get_program().get_engine().get_device_info().gfx_ver;
         if (gfx_ver.major == 12 && gfx_ver.minor == 74) { // ARL only
             sdpa_kernel_params.should_use_sdpa_opt = true;
             kernels_data.push_back(kernel_selector.get_best_kernel(sdpa_kernel_params));
@@ -427,6 +486,12 @@ public:
         if (_kernels_data.size() == 3) {
             (_kernels_data[2].update_dispatch_data_func)(*_kernels_data[default_sdpa].params, _kernels_data[2]);
         }
+    }
+
+    void update(primitive_inst& inst, const kernel_impl_params& impl_params) override {
+        auto new_impl_params = impl_params.output_layouts[0].get_partial_shape().size() == 4 ? impl_params : canonicalize_shapes(impl_params);
+        update_dispatch_data(new_impl_params);
+        inst.update_shape_info_tensor(new_impl_params);
     }
 };
 
