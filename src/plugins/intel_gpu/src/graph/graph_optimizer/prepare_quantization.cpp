50430f12965f3bbeacabe7d710461e11adf42e71
Min, Byungil <byungil.min@intel.com>
2025-04-30 19:29:21 +0900

    [GPU] Add explanation
    Signed-off-by: Min, Byungil <byungil.min@intel.com>

--- src/plugins/intel_gpu/src/graph/graph_optimizer/prepare_quantization.cpp
+++ src/plugins/intel_gpu/src/graph/graph_optimizer/prepare_quantization.cpp
@@ -511,8 +511,10 @@ static void optimize_weights_decompression_parameters(fully_connected_node& fc_n
     auto need_reorder = [&](size_t dep_id) {
         auto dep_layout = fc_node.get_input_layout(dep_id);
         auto dep_pshape = dep_layout.get_partial_shape();
-        if (dep_pshape.size() == 0)
+        if (dep_pshape.size() == 0) {
+            // ConvertU4WeightsZeroPointToScalar pass generates scalar const layer
             return false;
+        }
 
         // Group for scale_idx is always 1, whereas zero_point_idx is 0.
         auto groups_idx = (dep_pshape.size() > 1) ? 1 : 0;
