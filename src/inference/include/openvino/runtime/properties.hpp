17630b5c01b9a4ca9ce2b996f40cdc68ff580fc5
Kim, Mingyu <mingyu.kim@intel.com>
2025-05-02 10:34:45 +0900

    [GPU] New option dynamic_quantization_group_size_unsupported

--- src/inference/include/openvino/runtime/properties.hpp
+++ src/inference/include/openvino/runtime/properties.hpp
@@ -597,6 +597,14 @@ static constexpr Property<uint64_t, PropertyMutability::RW> dynamic_quantization
     "DYNAMIC_QUANTIZATION_GROUP_SIZE"};
 
 /**
+ * @brief This property defines unsupported group size for dynamic quantization optimization
+ * @ingroup ov_runtime_cpp_prop_api
+ *
+ * If dynamic_quantization_group_size is larger than this unsupported value, dynamic quantization will be disabled.
+ */
+static constexpr Property<uint64_t, PropertyMutability::RW> dynamic_quantization_group_size_unsupported{
+    "DYNAMIC_QUANTIZATION_GROUP_SIZE_UNSUPPORTED"};
+/**
  * @brief Hint for device to use specified precision for kv cache compression
  * @ingroup ov_runtime_cpp_prop_api
  */
