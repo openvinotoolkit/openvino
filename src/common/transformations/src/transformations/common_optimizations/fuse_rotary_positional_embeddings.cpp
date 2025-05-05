2e44ffaed7b80006a6d791e10ef9e09c1c8fdadc
Andrii Staikov <andrii.staikov@intel.com>
2025-05-05 11:46:08 +0200

    remove debug prints

--- src/common/transformations/src/transformations/common_optimizations/fuse_rotary_positional_embeddings.cpp
+++ src/common/transformations/src/transformations/common_optimizations/fuse_rotary_positional_embeddings.cpp
@@ -41,8 +41,6 @@
 #include "transformations/symbolic_transformations/symbolic_optimizations.hpp"
 #include "transformations/utils/utils.hpp"
 
-#include "openvino/pass/visualize_tree.hpp"
-
 using namespace ov::gen_pattern;
 
 ov::pass::RoPEFusion::RoPEFusion(bool support_2d_rope) : m_support_2d_rope(support_2d_rope) {
@@ -52,7 +50,6 @@ bool ov::pass::RoPEFusion::run_on_model(const std::shared_ptr<ov::Model>& model)
     RUN_ON_MODEL_SCOPE(RoPEFusion);
     ov::pass::SymbolicOptimizations symbolic_optimizations(false, get_pass_config());
 
-    std::cout << "SETTING UP RoPEFusion" << std::endl;
     auto symbolic_ctx_manager = symbolic_optimizations.get_manager();
 
     symbolic_ctx_manager->register_pass<ov::pass::RoPEFusionFlux>();
@@ -75,10 +72,7 @@ bool ov::pass::RoPEFusion::run_on_model(const std::shared_ptr<ov::Model>& model)
 
     symbolic_ctx_manager->register_pass<ov::pass::RoPEShareCosSin>();
 
-    std::cout << "About to run the transformations" << std::endl;
-    bool a = symbolic_optimizations.get_manager()->run_passes(model);
-    std::cout << "Run the transformations" << std::endl;
-    return a;
+    return symbolic_optimizations.get_manager()->run_passes(model);
 }
 
 // This is a utility function used in the work around in ChatGLM pattern.
