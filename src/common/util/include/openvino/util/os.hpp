a71937f0d34359c3365042023fd54a6f758130cf
Nikolay Shchegolev <nikolay.shchegolev@intel.com>
2025-05-07 13:31:38 +0400

    The second version

--- src/common/util/include/openvino/util/os.hpp
+++ src/common/util/include/openvino/util/os.hpp
@@ -8,16 +8,7 @@
 #    include <windows.h>
 #endif
 
-namespace ov {
-namespace util {
-
-#if defined(_WIN32)
-size_t get_os_encoded_version();
-#else
-constexpr size_t get_os_encoded_version() {
-    return 0;
-}
-#endif
+namespace ov::util {
 
 // The GetProcessMitigationPolicy function has been implemented since Windows 8 (0x0602),
 // so don't compile it on a lower version.
@@ -29,5 +20,4 @@ constexpr bool may_i_use_dynamic_code() {
 }
 #endif
 
-}  // namespace util
-}  // namespace ov
+}  // namespace ov::util
