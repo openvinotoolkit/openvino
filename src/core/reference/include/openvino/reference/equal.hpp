6f3c883c8221c30a98ca97e03fe4d82dce49fd17
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2025-05-12 14:32:28 +0200

    Reduce code bloat
    Signed-off-by: Tomasz Jankowski <tomasz1.jankowski@intel.com>

--- src/core/reference/include/openvino/reference/equal.hpp
+++ src/core/reference/include/openvino/reference/equal.hpp
@@ -26,8 +26,6 @@ void equal(const T* arg0, const T* arg1, char* out, size_t count) {
 /**
  * @brief Reference implementation of binary elementwise Equal operator.
  *
- * Used for integral types with custom `equal` function (reduce binary size).
- *
  * @param arg0            Pointer to input 0 data.
  * @param arg1            Pointer to input 1 data.
  * @param out             Pointer to output data.
@@ -35,67 +33,20 @@ void equal(const T* arg0, const T* arg1, char* out, size_t count) {
  * @param arg_shape1      Input 1 shape.
  * @param broadcast_spec  Broadcast specification mode.
  */
-template <typename T, typename U, typename std::enable_if_t<std::is_integral_v<T>>* = nullptr>
+template <typename T, typename U>
 void equal(const T* arg0,
            const T* arg1,
            U* out,
            const Shape& arg0_shape,
            const Shape& arg1_shape,
            const op::AutoBroadcastSpec& broadcast_spec) {
-    if constexpr (sizeof(T) == 1) {
-        autobroadcast_binop(reinterpret_cast<const int8_t*>(arg0),
-                            reinterpret_cast<const int8_t*>(arg1),
-                            out,
-                            arg0_shape,
-                            arg1_shape,
-                            broadcast_spec,
-                            func::equal<int8_t>);
-    } else if constexpr (sizeof(T) == 2) {
-        autobroadcast_binop(reinterpret_cast<const int16_t*>(arg0),
-                            reinterpret_cast<const int16_t*>(arg1),
-                            out,
-                            arg0_shape,
-                            arg1_shape,
-                            broadcast_spec,
-                            func::equal<int16_t>);
-    } else if constexpr (sizeof(T) == 4) {
-        autobroadcast_binop(reinterpret_cast<const int32_t*>(arg0),
-                            reinterpret_cast<const int32_t*>(arg1),
-                            out,
-                            arg0_shape,
-                            arg1_shape,
-                            broadcast_spec,
-                            func::equal<int32_t>);
-    } else if constexpr (sizeof(T) == 8) {
-        autobroadcast_binop(reinterpret_cast<const int64_t*>(arg0),
-                            reinterpret_cast<const int64_t*>(arg1),
-                            out,
-                            arg0_shape,
-                            arg1_shape,
-                            broadcast_spec,
-                            func::equal<int64_t>);
+    if constexpr (std::is_integral_v<T>) {
+        using S = std::make_signed_t<T>;
+        const auto sig0 = reinterpret_cast<const S*>(arg0);
+        const auto sig1 = reinterpret_cast<const S*>(arg1);
+        autobroadcast_binop(sig0, sig1, out, arg0_shape, arg1_shape, broadcast_spec, func::equal<int8_t>);
+    } else {
+        autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, std::equal_to<T>());
     }
-}
-
-/**
- * @brief Reference implementation of binary elementwise Equal operator.
- *
- * Used for floating-point types to (avoid warning compare floating point with `==`).
- *
- * @param arg0            Pointer to input 0 data.
- * @param arg1            Pointer to input 1 data.
- * @param out             Pointer to output data.
- * @param arg_shape0      Input 0 shape.
- * @param arg_shape1      Input 1 shape.
- * @param broadcast_spec  Broadcast specification mode.
- */
-template <typename T, typename U, typename std::enable_if_t<ov::is_floating_point<T>()>* = nullptr>
-void equal(const T* arg0,
-           const T* arg1,
-           U* out,
-           const Shape& arg0_shape,
-           const Shape& arg1_shape,
-           const op::AutoBroadcastSpec& broadcast_spec) {
-    autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, std::equal_to<T>());
 }
 }  // namespace ov::reference
--- src/core/reference/include/openvino/reference/not_equal.hpp
+++ src/core/reference/include/openvino/reference/not_equal.hpp
@@ -27,39 +27,10 @@ void not_equal(const T* arg0,
                const Shape& arg1_shape,
                const op::AutoBroadcastSpec& broadcast_spec) {
     if constexpr (std::is_integral_v<T>) {
-        if constexpr (sizeof(T) == 1) {
-            autobroadcast_binop(reinterpret_cast<const int8_t*>(arg0),
-                                reinterpret_cast<const int8_t*>(arg1),
-                                out,
-                                arg0_shape,
-                                arg1_shape,
-                                broadcast_spec,
-                                func::not_equal<int8_t>);
-        } else if constexpr (sizeof(T) == 2) {
-            autobroadcast_binop(reinterpret_cast<const int16_t*>(arg0),
-                                reinterpret_cast<const int16_t*>(arg1),
-                                out,
-                                arg0_shape,
-                                arg1_shape,
-                                broadcast_spec,
-                                func::not_equal<int16_t>);
-        } else if constexpr (sizeof(T) == 4) {
-            autobroadcast_binop(reinterpret_cast<const int32_t*>(arg0),
-                                reinterpret_cast<const int32_t*>(arg1),
-                                out,
-                                arg0_shape,
-                                arg1_shape,
-                                broadcast_spec,
-                                func::not_equal<int32_t>);
-        } else if constexpr (sizeof(T) == 8) {
-            autobroadcast_binop(reinterpret_cast<const int64_t*>(arg0),
-                                reinterpret_cast<const int64_t*>(arg1),
-                                out,
-                                arg0_shape,
-                                arg1_shape,
-                                broadcast_spec,
-                                func::not_equal<int64_t>);
-        }
+        using S = std::make_signed_t<T>;
+        const auto sig0 = reinterpret_cast<const S*>(arg0);
+        const auto sig1 = reinterpret_cast<const S*>(arg1);
+        autobroadcast_binop(sig0, sig1, out, arg0_shape, arg1_shape, broadcast_spec, func::not_equal<int64_t>);
     } else {
         autobroadcast_binop(arg0, arg1, out, arg0_shape, arg1_shape, broadcast_spec, func::not_equal<T>);
     }
