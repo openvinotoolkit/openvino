23fed5ecaee3e18f0a0037c91575e2b1836e5dd2
Barnas, Michal <michal.barnas@intel.com>
2025-05-12 14:38:04 +0200

    Add Unicode path support for Windows in FileStorageCacheManager

--- src/inference/src/cache_manager.hpp
+++ src/inference/src/cache_manager.hpp
@@ -104,7 +104,11 @@ class FileStorageCacheManager final : public ICacheManager {
     std::string m_cachePath;
 
     ov::util::Path getBlobFile(const std::string& blobHash) const {
+#if defined(_WIN32) && defined(OPENVINO_ENABLE_UNICODE_PATH_SUPPORT)
+        return ov::util::string_to_wstring(ov::util::make_path(m_cachePath, blobHash + ".blob"));
+#else
         return ov::util::make_path(m_cachePath, blobHash + ".blob");
+#endif
     }
 
 public:
