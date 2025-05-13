bbf42d50da63afe55e67e4f88f46161b08b9c354
chenhu-wang <chenhu.wang@intel.com>
2025-05-13 23:04:46 +0800

    copyb as op and outside of block

--- src/plugins/intel_cpu/src/emitters/snippets/aarch64/kernel_executors/gemm_copy_b.hpp
+++ src/plugins/intel_cpu/src/emitters/snippets/aarch64/kernel_executors/gemm_copy_b.hpp
@@ -4,6 +4,98 @@
 
 #pragma once
 
+#include "common/utils.hpp"
+#include "cpu_memory.h"
+#include "emitters/snippets/brgemm_generic.hpp"
+#include "emitters/utils.hpp"
+#include "kai/ukernels/matmul/matmul_clamp_f32_f32_f32p/kai_matmul_clamp_f32_f32_f32p8x1biasf32_6x8x4_neon_mla.h"
+#include "kai/ukernels/matmul/matmul_clamp_f32_f32_f32p/kai_matmul_clamp_f32_f32_f32p_interface.h"
+#include "kai/ukernels/matmul/pack/kai_rhs_pack_kxn_f32p8x1biasf32_f32_f32_neon.h"
+
 namespace ov::intel_cpu::aarch64 {
 
-}  // namespace ov::intel_cpu
+struct GemmCopyBKernelKaiConfig : public snippets::KernelExecutorBase::GenericConfig {
+public:
+    GemmCopyBKernelKaiConfig() = default;
+    GemmCopyBKernelKaiConfig(const size_t N, const size_t K, const size_t n_blk_size);
+
+    bool operator==(const GemmCopyBKernelKaiConfig& rhs) const;
+    bool operator!=(const GemmCopyBKernelKaiConfig& rhs) const {
+        return !(*this == rhs);
+    }
+
+    [[nodiscard]] std::unique_ptr<GenericConfig> get_clone_ptr() const override {
+        return std::make_unique<GemmCopyBKernelKaiConfig>(*this);
+    }
+
+    [[nodiscard]] bool is_completed() const override;
+
+#ifdef SNIPPETS_DEBUG_CAPS
+    virtual std::string to_string() const override;
+#endif
+
+    void update(size_t N, size_t K, size_t n);
+
+    size_t hash() const override {
+        return m_hash;
+    }
+
+    [[nodiscard]] dnnl_dim_t get_N() const {
+        return m_N;
+    }
+    [[nodiscard]] dnnl_dim_t get_K() const {
+        return m_K;
+    }
+    [[nodiscard]] dnnl_dim_t get_n() const {
+        return m_n_blk_size;
+    }
+
+private:
+    [[nodiscard]] size_t compute_hash() const;
+
+    size_t m_N;
+    size_t m_K;
+    size_t m_n_blk_size;
+    size_t m_hash{SIZE_MAX};
+};
+
+struct GemmCopyBKaiCompiledKernel {
+    std::shared_ptr<kai_matmul_clamp_f32_f32_f32p_ukernel> gemm_copyb_kernel = nullptr;
+};
+
+class GemmCopyBKaiKernelExecutor
+    : public snippets::KernelExecutor<GemmCopyBKernelKaiConfig, GemmCopyBKaiCompiledKernel> {
+public:
+    GemmCopyBKaiKernelExecutor(GemmCopyBKernelKaiConfig config);
+
+    void update_kernel(const GemmCopyBKernelKaiConfig& config,
+                       std::shared_ptr<GemmCopyBKaiCompiledKernel>& kernel) const override final {}
+
+    // Function that will be called in runtime to execute the kernel
+    static void execute(const GemmCopyBKaiKernelExecutor* executor, void* in0, void* out0);
+    void* get_bias_mem() const {
+        return biasMem.data();
+    }
+    mutable size_t biasSize = 0;
+
+private:
+    void update_config(const ov::snippets::lowered::ExpressionPtr& expr,
+                       const ov::snippets::lowered::LinearIRCPtr& linear_ir,
+                       GemmCopyBKernelKaiConfig& config) const override;
+
+    mutable std::vector<uint8_t> biasMem;
+
+    static constexpr kai_matmul_clamp_f32_f32_f32p_ukernel ukernel{
+        kai_get_m_step_matmul_clamp_f32_f32_f32p8x1biasf32_6x8x4_neon_mla,
+        kai_get_n_step_matmul_clamp_f32_f32_f32p8x1biasf32_6x8x4_neon_mla,
+        kai_get_nr_matmul_clamp_f32_f32_f32p8x1biasf32_6x8x4_neon_mla,
+        kai_get_kr_matmul_clamp_f32_f32_f32p8x1biasf32_6x8x4_neon_mla,
+        kai_get_sr_matmul_clamp_f32_f32_f32p8x1biasf32_6x8x4_neon_mla,
+        kai_get_lhs_offset_matmul_clamp_f32_f32_f32p8x1biasf32_6x8x4_neon_mla,
+        kai_get_rhs_packed_offset_matmul_clamp_f32_f32_f32p8x1biasf32_6x8x4_neon_mla,
+        kai_get_dst_offset_matmul_clamp_f32_f32_f32p8x1biasf32_6x8x4_neon_mla,
+        kai_get_dst_size_matmul_clamp_f32_f32_f32p8x1biasf32_6x8x4_neon_mla,
+        kai_run_matmul_clamp_f32_f32_f32p8x1biasf32_6x8x4_neon_mla};
+};
+
+}  // namespace ov::intel_cpu::aarch64
---
+++ src/plugins/intel_cpu/src/transformations/snippets/aarch64/pass/lowered/adjust_brgemm_copy_b_loop_ports.cpp
@@ -0,0 +1,105 @@
+// Copyright (C) 2018-2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "adjust_brgemm_copy_b_loop_ports.hpp"
+
+#include "snippets/itt.hpp"
+#include "snippets/lowered/expressions/buffer_expression.hpp"
+#include "snippets/lowered/loop_manager.hpp"
+#include "snippets/utils/utils.hpp"
+#include "transformations/snippets/aarch64/op/gemm_copy_b.hpp"
+#include "transformations/snippets/aarch64/op/gemm_cpu.hpp"
+
+namespace ov::intel_cpu {
+
+bool pass::aarch64::AdjustBrgemmCopyBLoopPorts::update_loop_info(
+    const std::shared_ptr<snippets::lowered::UnifiedLoopInfo>& loop_info) {
+    OPENVINO_ASSERT(loop_info, "Invalid loop info pointer");
+    bool modified = false;
+    auto caller = [&](snippets::lowered::LoopPort& loop_port,
+                      snippets::lowered::UnifiedLoopInfo::LoopPortDesc& loop_desc) {
+        const auto& p = *loop_port.get_expr_port();
+        if (p.get_type() == snippets::lowered::ExpressionPort::Input && p.get_index() == 1) {
+            const auto& node = p.get_expr()->get_node();
+            if (auto brg = as_type_ptr<ov::intel_cpu::aarch64::GemmCPU>(node)) {
+                // from format KN to NK64n(64 is n block), and for each K64n, repack to nK8n
+                std::cout << "loop_port.get_dim_idx():" << loop_port.get_dim_idx() << std::endl;
+                if (loop_port.is_incremented()) {
+                    // N blocking loop
+                    if (loop_port.get_dim_idx() == 0) {
+                        std::cout << "loop_port.get_dim_idx() == 0" << std::endl;
+                        // int64_t k_blk_size = 16; // K dimension if k w/o block
+                        const auto& in_0_shape = brg->get_input_shape(0);
+                        int64_t k_blk_size = in_0_shape.back(); // K dimension if k w/o block
+                        std::cout << "K:" << k_blk_size << std::endl;
+                        loop_desc.ptr_increment =
+                            snippets::utils::dynamic_safe_mul(loop_desc.ptr_increment, k_blk_size);
+                        loop_desc.finalization_offset =
+                            snippets::utils::dynamic_safe_mul(loop_desc.finalization_offset, k_blk_size);
+                    } else {
+                        OPENVINO_THROW("Unexpected loop port dimension index in AdjustBrgemmCopyBLoopPorts");
+                    }
+                    modified = true;
+                }
+            }
+        }
+    };
+    loop_info->iterate_through_infos(caller);
+    return modified;
+}
+
+bool pass::aarch64::AdjustBrgemmCopyBLoopPorts::run(const snippets::lowered::LinearIR& linear_ir) {
+    OV_ITT_SCOPED_TASK(ov::pass::itt::domains::SnippetsTransform, "Snippets::AdjustBrgemmCopyBLoopPorts")
+
+    bool modified = false;
+
+    // auto get_repacking_loop_idces = [](const snippets::lowered::ExpressionPtr& brgemm_expr) {
+    //     // Repacking may be extracted outside the snippets kernel. In this case, brgemm parent expression is a
+    //     // parameter.
+    //     const auto& brgemm_in1 = brgemm_expr->get_input_port_connector(1)->get_source();
+    //     const auto& shape_infer_seq = ov::snippets::utils::get_first_parent_shape_infer_expr_seq(brgemm_in1.get_expr());
+    //     const auto source =
+    //         shape_infer_seq.empty() ? brgemm_in1 : shape_infer_seq.back()->get_input_port_connector(0)->get_source();
+    //     if (is_type<ov::op::v0::Parameter>(source.get_expr()->get_node())) {
+    //         return std::vector<size_t>{};
+    //     }
+    //     const auto repacking_expr = brgemm_utils::repacking::get_copy_b_expr(brgemm_expr);
+    //     OPENVINO_ASSERT(repacking_expr, "BrgemmCopyB expression is not found");
+    //     return repacking_expr->get_loop_ids();
+    // };
+
+    for (const auto& expr : linear_ir) {
+        const auto brgemm = ov::as_type_ptr<ov::intel_cpu::aarch64::GemmCPU>(expr->get_node());
+        if (!brgemm) {
+            continue;
+        }
+        std::cout << "brgemm1:" << std::endl;
+        const auto& brgemm_loop_ids = expr->get_loop_ids();
+        std::cout << "brgemm_loop_ids.size:" << brgemm_loop_ids.size() << std::endl;
+        // const auto& repacking_loop_ids = get_repacking_loop_idces(expr);
+        // // Continue if there is no blocking loop
+        // if (brgemm_loop_ids.empty() && repacking_loop_ids.empty()) {
+        //     continue;
+        // }
+
+        // OPENVINO_ASSERT(brgemm_loop_ids.size() > repacking_loop_ids.size(), "Invalid BrgemmCopyB loop configuration");
+        const auto& loop_manager = linear_ir.get_loop_manager();
+        // for (auto i = repacking_loop_ids.size(); i < brgemm_loop_ids.size(); i++) {
+        for (auto i = 1; i < brgemm_loop_ids.size(); i++) {
+            std::cout << "brgemm_loop_ids:" << i << std::endl;
+            const auto& loop = loop_manager->get_loop_info(brgemm_loop_ids[i]);
+            auto uni_loop = ov::as_type_ptr<snippets::lowered::UnifiedLoopInfo>(loop);
+            if (!uni_loop) {
+                uni_loop = ov::as_type_ptr<snippets::lowered::ExpandedLoopInfo>(loop)->get_unified_loop_info();
+            }
+            if (!m_affected_loops.count(uni_loop) && update_loop_info(uni_loop)) {
+                m_affected_loops.insert(uni_loop);
+                modified = true;
+            }
+        }
+    }
+
+    return modified;
+}
+}  // namespace ov::intel_cpu
