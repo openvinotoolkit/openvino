0e924303345b3b053ab0957d11734f2541a542ef
p-wysocki <przemyslaw.wysocki@intel.com>
2025-05-13 15:59:44 +0200

    Include Jenkins
    Signed-off-by: p-wysocki <przemyslaw.wysocki@intel.com>

--- tests/layer_tests/tensorflow_tests/test_tf_SparseFillEmptyRows.py
+++ tests/layer_tests/tensorflow_tests/test_tf_SparseFillEmptyRows.py
@@ -5,7 +5,6 @@ import numpy as np
 import pytest
 import tensorflow as tf
 from common.tf_layer_test_class import CommonTFLayerTest
-from common.utils.tf_utils import run_in_jenkins
 
 rng = np.random.default_rng(475912)
 
@@ -88,7 +87,7 @@ class TestSparseFillEmptyRows(CommonTFLayerTest):
     def test_sparse_fill_empty_rows(self, data_type,
                                    dense_shape, values_count, empty_rows,
                                    ie_device, precision, ir_version, temp_dir):
-        if ie_device == 'GPU' or run_in_jenkins():
+        if ie_device == 'GPU':
             pytest.skip("operation extension is not supported on GPU")
         self._test(*self.create_sparse_fill_empty_rows_net(data_type,
                                                          dense_shape, values_count, empty_rows),
