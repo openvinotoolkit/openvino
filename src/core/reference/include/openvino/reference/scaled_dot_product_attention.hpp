a629a28e95dbfe64c4f8356aa672082cd85625f0
Piotr Kowalczyk <piotr.kowalczyk@intel.com>
2025-05-14 11:41:03 +0000

    [ref[:[sdpa]: Refactored impl.

--- src/core/reference/include/openvino/reference/scaled_dot_product_attention.hpp
+++ src/core/reference/include/openvino/reference/scaled_dot_product_attention.hpp
@@ -8,8 +8,8 @@
 #include "openvino/core/shape.hpp"
 #include "openvino/reference/add.hpp"
 #include "openvino/reference/matmul.hpp"
+#include "openvino/reference/multiply.hpp"
 #include "openvino/reference/softmax.hpp"
-#include "openvino/reference/transpose.hpp"
 
 namespace helpers {
 template <typename T>
@@ -27,23 +27,8 @@ std::vector<T> CreateAttentionMask(const ov::Shape& shape) {
     return maskData;
 }
 
-// --------------------------------------------------------
-// This overloads are intentionally not implemented! If the linker ever complains about them,
-// it means that something went wrong in scaled_dot_product_attention().
 template <typename T>
-const T* GetRawPtr(const char* maskBool);
-
-template <typename T>
-std::vector<T> CreateAttentionMaskFromBool(const T* maskBool, const ov::Shape& shape);
-// --------------------------------------------------------
-
-template <typename T>
-typename std::enable_if<!std::is_same<T, char>::value, const T*>::type GetRawPtr(const T* maskBool) {
-    return maskBool;
-}
-
-template <typename T>
-std::vector<T> CreateAttentionMaskFromBool(const char* maskBool, const ov::Shape& shape) {
+std::vector<T> CreateAttentionMaskFromOvBoolean(const char* maskBool, const ov::Shape& shape) {
     std::vector<T> maskData(shape_size(shape));
 
     std::transform(maskBool, maskBool + shape_size(shape), maskData.begin(), [](char val) {
@@ -72,7 +57,6 @@ void scaled_dot_product_attention(const T* query,
                                   const Shape& value_shape,
                                   const Shape& mask_shape,
                                   const Shape& output_shape) {
-    std::cout << "{REF}: scaled_dot_product_attention" << std::endl;
     static_assert(std::is_same<T, TMask>::value || std::is_same<TMask, char>::value,
                   "T and TMask must be either the same type, or the TMask must be char(ov::element::boolean)");
 
@@ -82,11 +66,11 @@ void scaled_dot_product_attention(const T* query,
     std::vector<T> attentionMaskData;
     if (mask && !is_causal) {
         biasShape = mask_shape;
-        if (std::is_same<TMask, char>::value) {
-            attentionMaskData = helpers::CreateAttentionMaskFromBool<T>(mask, biasShape);
+        if constexpr (std::is_same<TMask, char>::value) {
+            attentionMaskData = helpers::CreateAttentionMaskFromOvBoolean<T>(mask, biasShape);
             bias = attentionMaskData.data();
         } else {
-            bias = helpers::GetRawPtr<T>(mask);
+            bias = mask;
         }
     }
 
@@ -101,39 +85,18 @@ void scaled_dot_product_attention(const T* query,
     const float defaultScaleVal = 1.0f / static_cast<float>(std::sqrt(query_shape[query_shape.size() - 1]));
     const T scaleVal = scale ? *scale : static_cast<T>(defaultScaleVal);
 
-    auto keyTransposedShape = key_shape;
-    std::swap(keyTransposedShape[key_shape.size() - 2], keyTransposedShape[key_shape.size() - 1]);
-
-    std::vector<T> keyTransposed(key, key + shape_size(key_shape));
-    std::vector<int64_t> axesOrder;
-    for (size_t i = 0; i < key_shape.size(); ++i) {
-        axesOrder.push_back(i);
-    }
-    std::swap(axesOrder[key_shape.size() - 2], axesOrder[key_shape.size() - 1]);
-
-    ov::reference::transpose(reinterpret_cast<const char*>(key),
-                             reinterpret_cast<char*>(keyTransposed.data()),
-                             key_shape,
-                             sizeof(T),
-                             axesOrder,
-                             keyTransposedShape);
-
     auto qkShape = query_shape;
     qkShape[qkShape.size() - 1] = key_shape[key_shape.size() - 2];
 
     std::vector<T> qkData(shape_size(qkShape), 0);
-    ov::reference::matmul<T>(query,
-                             keyTransposed.data(),
-                             qkData.data(),
-                             query_shape,
-                             keyTransposedShape,
-                             qkShape,
-                             false,
-                             false);
+    ov::reference::matmul<T>(query, key, qkData.data(), query_shape, key_shape, qkShape, false, true);
 
-    std::transform(qkData.begin(), qkData.end(), qkData.begin(), [scaleVal](T& val) {
-        return val * scaleVal;
-    });
+    ov::reference::multiply<T>(qkData.data(),
+                               &scaleVal,
+                               qkData.data(),
+                               qkShape,
+                               Shape{1},
+                               ov::op::AutoBroadcastType::NUMPY);
 
     if (bias) {
         ov::reference::add<T>(qkData.data(), bias, qkData.data(), qkShape, biasShape, ov::op::AutoBroadcastType::NUMPY);
