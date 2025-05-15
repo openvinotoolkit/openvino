e96c66931669de5dc7bf4c03fb20164702353ac8
Arseniy Obolenskiy <gooddoog@student.su>
2025-05-15 12:04:00 +0200

    Remove unused variable

--- src/plugins/intel_cpu/tests/functional/custom/single_layer_tests/matmul_sparse.cpp
+++ src/plugins/intel_cpu/tests/functional/custom/single_layer_tests/matmul_sparse.cpp
@@ -100,8 +100,6 @@ protected:
         std::mt19937 gen_f(123);
         std::uniform_real_distribution<float> dist_f(0.f, 1.f);
 
-        size_t countZero = 0;
-
         res[0] = startFrom;
         res[vec_len - 1] = upTo;
         for (size_t i = 1; i < vec_len - 1; i++) {
@@ -109,7 +107,6 @@ protected:
                 res[i] = static_cast<int8_t>(dist(gen));
             } else {
                 res[i] = 0;
-                countZero++;
             }
         }
 
