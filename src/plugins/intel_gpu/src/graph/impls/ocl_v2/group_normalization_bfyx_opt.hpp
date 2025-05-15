03b41fe3895589dbf90e21afda72df7702082647
Steve Yoo <steve.yoo@intel.com>
2025-05-15 14:33:32 +0000

    [GPU] Move query_formats to base class

--- src/plugins/intel_gpu/src/graph/impls/ocl_v2/group_normalization_bfyx_opt.hpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl_v2/group_normalization_bfyx_opt.hpp
@@ -11,13 +11,14 @@
 #include "intel_gpu/primitives/eltwise.hpp"
 #include "program_node.h"
 #include "registry/implementation_manager.hpp"
+#include "group_normalization_base.hpp"
 
 using namespace cldnn;  // TODO: Remove once namespaces are aligned
 namespace ov::intel_gpu::ocl {
 
-struct GroupNormalizationBfyxOpt : public ImplementationManager {
+struct GroupNormalizationBfyxOpt : public GroupNormalizationBase {
     OV_GPU_PRIMITIVE_IMPL("ocl::group_norm::bfyx_opt")
-    explicit GroupNormalizationBfyxOpt(shape_types shape_type, ValidateFunc vf = nullptr) : ImplementationManager(impl_types::ocl, shape_type, std::move(vf)) {}
+    explicit GroupNormalizationBfyxOpt(shape_types shape_type, ValidateFunc vf = nullptr) : GroupNormalizationBase(shape_type, std::move(vf)) {}
     [[nodiscard]] std::unique_ptr<primitive_impl> create_impl(const program_node& node, const RuntimeParams& params) const override;
     [[nodiscard]] bool validate_impl(const program_node& node) const override {
         static constexpr std::array supported_fmts = {
@@ -47,17 +48,6 @@ struct GroupNormalizationBfyxOpt : public ImplementationManager {
         }
 
         return true;
-    }
-    [[nodiscard]] in_out_fmts_t query_formats(const program_node& node) const override {
-        std::vector<format::type> in_fmts(node.get_dependencies().size(), format::any);
-        std::vector<format::type> out_fmts(node.get_outputs_count(), format::any);
-
-        for (size_t i = 1; i < node.get_dependencies().size(); i++) {
-            size_t in_rank = node.get_input_layout(i).get_rank();
-            in_fmts[i] = format::get_default_format(in_rank);
-        }
-
-        return {in_fmts, out_fmts};
     }
 };
 
