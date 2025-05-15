a4ee9b751166b50db3ca63b73895e8b2af2a74b8
Steve Yoo <steve.yoo@intel.com>
2025-05-15 14:33:32 +0000

    [GPU] Move query_formats to base class

--- src/plugins/intel_gpu/src/graph/impls/ocl_v2/group_normalization_fsv16.hpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl_v2/group_normalization_fsv16.hpp
@@ -11,15 +11,14 @@
 #include "intel_gpu/primitives/eltwise.hpp"
 #include "program_node.h"
 #include "registry/implementation_manager.hpp"
+#include "group_normalization_base.hpp"
 
 using namespace cldnn;  // TODO: Remove once namespaces are aligned
-
 namespace ov::intel_gpu::ocl {
 
-struct GroupNormalizationFsv16Opt : public ImplementationManager {
+struct GroupNormalizationFsv16Opt : public GroupNormalizationBase {
     OV_GPU_PRIMITIVE_IMPL("ocl::group_norm::fsv16_opt")
-    explicit GroupNormalizationFsv16Opt(shape_types shape_type, ValidateFunc vf = nullptr)
-        : ImplementationManager(impl_types::ocl, shape_type, std::move(vf)) {}
+    explicit GroupNormalizationFsv16Opt(shape_types shape_type, ValidateFunc vf = nullptr) : GroupNormalizationBase(shape_type, std::move(vf)) {}
     [[nodiscard]] std::unique_ptr<primitive_impl> create_impl(const program_node& node, const RuntimeParams& params) const override;
     [[nodiscard]] bool validate_impl(const program_node& node) const override {
         static constexpr std::array supported_fmts = {
@@ -58,17 +57,6 @@ struct GroupNormalizationFsv16Opt : public ImplementationManager {
         constexpr size_t fsv = 16;
         // feature paddings should be multiples of fsv.
         return in0_layout.data_padding._lower_size[1] % fsv == 0;
-    }
-    [[nodiscard]] in_out_fmts_t query_formats(const program_node& node) const override {
-        std::vector<format::type> in_fmts(node.get_dependencies().size(), format::any);
-        std::vector<format::type> out_fmts(node.get_outputs_count(), format::any);
-
-        for (size_t i = 1; i < node.get_dependencies().size(); i++) {
-            size_t in_rank = node.get_input_layout(i).get_rank();
-            in_fmts[i] = format::get_default_format(in_rank);
-        }
-
-        return {in_fmts, out_fmts};
     }
 };
 
--- src/plugins/intel_gpu/src/graph/impls/ocl_v2/group_normalization_ref.hpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl_v2/group_normalization_ref.hpp
@@ -11,13 +11,14 @@
 #include "intel_gpu/primitives/eltwise.hpp"
 #include "program_node.h"
 #include "registry/implementation_manager.hpp"
+#include "group_normalization_base.hpp"
 
 using namespace cldnn;  // TODO: Remove once namespaces are aligned
 namespace ov::intel_gpu::ocl {
 
-struct GroupNormalizationRef : public ImplementationManager {
+struct GroupNormalizationRef : public GroupNormalizationBase {
     OV_GPU_PRIMITIVE_IMPL("ocl::group_norm::ref")
-    explicit GroupNormalizationRef(shape_types shape_type, ValidateFunc vf = nullptr) : ImplementationManager(impl_types::ocl, shape_type, std::move(vf)) {}
+    explicit GroupNormalizationRef(shape_types shape_type, ValidateFunc vf = nullptr) : GroupNormalizationBase(shape_type, std::move(vf)) {}
     std::unique_ptr<primitive_impl> create_impl(const program_node& node, const RuntimeParams& params) const override;
     [[nodiscard]] bool validate_impl(const program_node& node) const override {
         static constexpr std::array supported_fmts = {format::bfyx, format::bfzyx, format::b_fs_yx_fsv16};
@@ -42,17 +43,6 @@ struct GroupNormalizationRef : public ImplementationManager {
         }
 
         return true;
-    }
-    [[nodiscard]] in_out_fmts_t query_formats(const program_node& node) const override {
-        std::vector<format::type> in_fmts(node.get_dependencies().size(), format::any);
-        std::vector<format::type> out_fmts(node.get_outputs_count(), format::any);
-
-        for (size_t i = 1; i < node.get_dependencies().size(); i++) {
-            size_t in_rank = node.get_input_layout(i).get_rank();
-            in_fmts[i] = format::get_default_format(in_rank);
-        }
-
-        return {in_fmts, out_fmts};
     }
 };
 
