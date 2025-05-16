e5bd962d0c5cc563384db09393d16d7be46c9c3c
Pawel Raasz <pawel.raasz@intel.com>
2025-05-16 06:46:40 +0000

    Fix CID 2354410, 2354663
    Signed-off-by: Pawel Raasz <pawel.raasz@intel.com>

--- samples/c/hello_nv12_input_classification/main.c
+++ samples/c/hello_nv12_input_classification/main.c
@@ -8,6 +8,7 @@
 #include <string.h>
 
 #include "openvino/c/openvino.h"
+#include "samples_util/path_util.h"
 
 /**
  * @brief Struct to store infer results
@@ -198,7 +199,7 @@ int main(int argc, char** argv) {
     ov_output_const_port_t* input_port = NULL;
     ov_output_const_port_t* output_port = NULL;
     ov_layout_t* model_layout = NULL;
-    ov_shape_t input_shape;
+    ov_shape_t input_shape = {.rank = 0, .dims = NULL};
 
     // -------- Get OpenVINO runtime version --------
     ov_version_t version = {.description = NULL, .buildNumber = NULL};
@@ -210,7 +211,10 @@ int main(int argc, char** argv) {
 
     // -------- Parsing and validation of input arguments --------
     const char* input_model = argv[1];
-    const char* input_image_path = argv[2];
+    char input_image_path[PATH_MAX];
+    if (!sanitize_path(argv[2], input_image_path, sizeof(input_image_path))) {
+        goto err;
+    }
     const char* device_name = argv[4];
 
     // -------- Step 1. Initialize OpenVINO Runtime Core --------
