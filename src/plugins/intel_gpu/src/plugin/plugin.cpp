e3d55347f7fcea94fe51542b727c80a5a34cf493
Michal Miotk <michal.miotk@intel.com>
2025-05-20 15:34:52 +0200

    more descriptive query, added test

--- src/plugins/intel_gpu/src/plugin/plugin.cpp
+++ src/plugins/intel_gpu/src/plugin/plugin.cpp
@@ -655,7 +655,9 @@ uint32_t Plugin::get_max_batch_size(const ov::AnyMap& options) const {
     uint32_t n_streams = static_cast<uint32_t>(config.get_num_streams());
     uint64_t occupied_device_mem = 0;
     auto statistic_result = get_metric(ov::intel_gpu::memory_statistics.name(), options).as<std::map<std::string, uint64_t>>();
-    auto occupied_usm_dev = statistic_result.find("usm_device");
+    std::ostringstream usm_device_oss;
+    usm_device_oss << cldnn::allocation_type::usm_device;
+    auto occupied_usm_dev = statistic_result.find(usm_device_oss.str());
     if (occupied_usm_dev != statistic_result.end()) {
         occupied_device_mem = occupied_usm_dev->second;
     }
