33d391c6cc0dd02e67020186725d1cfbf1f5a7d5
Oleg Pipikin <oleg.pipikin@intel.com>
2025-05-21 17:43:31 +0200

    Update precomp.hpp for shared_test_classes

--- src/tests/functional/shared_test_classes/src/precomp.hpp
+++ src/tests/functional/shared_test_classes/src/precomp.hpp
@@ -6,4 +6,9 @@
 
 #include "openvino/pch/precomp_core.hpp"
 
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
+
+#include "shared_test_classes/base/ov_subgraph.hpp"
+#include "common_test_utils/file_utils.hpp"
+#include "openvino/core/model.hpp"
+#include "transformations/convert_precision.hpp"
