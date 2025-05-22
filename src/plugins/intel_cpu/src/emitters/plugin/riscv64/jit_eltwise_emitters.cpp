b583ed19145142f6637b4b71821a19325a8c50ce
Bhuminjay Soni <Soni5Happy@gmail.com>
2025-05-22 22:12:52 +0530

    Update src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.cpp
    Co-authored-by: Alexandra Sidorova <alexa.sanechka@ya.ru>

--- src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.cpp
+++ src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.cpp
@@ -341,16 +341,13 @@ size_t jit_mod_emitter::get_inputs_num() const {
     return 2;
 }
 size_t jit_mod_emitter::aux_vecs_count() const {
-    if(exec_prc_ == ov::element::f32) {
+    if (exec_prc_ == ov::element::f32) {
         return 1;
-    }
-    else if(exec_prc_ == ov::element::i32) {
+    } else if (exec_prc_ == ov::element::i32) {
         return 0;
-    }
-    else {
+    } else {
         OPENVINO_THROW("Unsupported precision");
     }
-    
 }
 size_t jit_mod_emitter::aux_fp_gprs_count() const {
     if(exec_prc_ == ov::element::f32) {
