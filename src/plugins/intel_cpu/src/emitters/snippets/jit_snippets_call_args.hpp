07c7aac7cb1593ce3e3c5091f66de65d88aed9e1
Arseniy Obolenskiy <arseniy.obolenskiy@intel.com>
2025-05-23 13:03:13 +0200

    Fix array leangth & compilation error

--- src/plugins/intel_cpu/src/emitters/snippets/jit_snippets_call_args.hpp
+++ src/plugins/intel_cpu/src/emitters/snippets/jit_snippets_call_args.hpp
@@ -16,7 +16,9 @@ namespace ov::intel_cpu {
 #if defined(OPENVINO_ARCH_ARM64)
 #    define SNIPPETS_MAX_DATA_PTR_COUNT 23
 #else
-#    define SNIPPETS_MAX_DATA_PTR_COUNT 11
+// Issue: 168073
+// TODO: decrease max array size
+#    define SNIPPETS_MAX_DATA_PTR_COUNT 24
 #endif
 
 #define GET_OFF(field)           offsetof(jit_snippets_call_args, field)
