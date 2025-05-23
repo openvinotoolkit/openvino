bfba83922c2ea4bb8ed7207c5592578bdcf82050
Vladislav Golubev <vladislav.golubev@intel.com>
2025-05-23 23:52:00 +0900

    Unused methods/structures are removed

--- src/plugins/intel_cpu/tests/functional/shared_tests_instances/low_precision_transformations/x64/eliminate_fake_quantize_transformation.cpp
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/low_precision_transformations/x64/eliminate_fake_quantize_transformation.cpp
@@ -14,7 +14,6 @@ namespace {
 const std::vector<EliminateFakeQuantizeTransformationTestValues> testValues = {
     {
         {1, 3, 16, 16},
-        LayerTestsUtils::LayerTransformationParamsNGraphFactory::createParamsU8I8(),
         {
             ov::element::f32,
             { 256ul, {}, { 0.f }, { 255.f / 2.f }, { 0.f }, { 255.f / 2.f }, ov::element::f32 },
@@ -28,7 +27,6 @@ const std::vector<EliminateFakeQuantizeTransformationTestValues> testValues = {
     },
     {
         {1, 3, 16, 16},
-        LayerTestsUtils::LayerTransformationParamsNGraphFactory::createParamsU8I8(),
         {
             ov::element::f32,
             { 256ul, {}, { 0.f }, { 255.f / 2.f }, { 0.f }, { 255.f / 2.f }, ov::element::f32 },
