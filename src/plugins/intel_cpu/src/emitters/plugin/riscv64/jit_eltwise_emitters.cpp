a8b7c72cec6818bf02b4410828e2a423a29da680
Bhuminjay Soni <Soni5Happy@gmail.com>
2025-05-29 23:01:34 +0530

    Update src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.cpp
    Co-authored-by: Alexandra Sidorova <alexa.sanechka@ya.ru>

--- src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.cpp
+++ src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.cpp
@@ -567,14 +567,14 @@ std::set<std::vector<element::Type>> jit_minimum_emitter::get_supported_precisio
 jit_logical_and_emitter::jit_logical_and_emitter(ov::intel_cpu::riscv64::jit_generator* host, ov::intel_cpu::riscv64::cpu_isa_t host_isa,
                                  const std::shared_ptr<ov::Node>& node)
     : jit_emitter(host, host_isa, get_arithmetic_binary_exec_precision(node)) {
-        prepare_table();
-    }
+    prepare_table();
+}
 
 jit_logical_and_emitter::jit_logical_and_emitter(ov::intel_cpu::riscv64::jit_generator* host, ov::intel_cpu::riscv64::cpu_isa_t host_isa,
                                  const ov::element::Type exec_prc)
     : jit_emitter(host, host_isa, exec_prc) {
-        prepare_table();
-    }
+    prepare_table();
+}
 
 size_t jit_logical_and_emitter::get_inputs_num() const {
     return 2;
