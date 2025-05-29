3b26bd36a1dab6094bb80b572b7b2907b2090e3c
Piotr Kowalczyk <piotr.kowalczyk@intel.com>
2025-05-29 07:12:07 +0000

    [cpu]: [gatherND]: Supports negative indices.

--- src/tests/test_utils/common_test_utils/src/node_builders/gather_nd.cpp
+++ src/tests/test_utils/common_test_utils/src/node_builders/gather_nd.cpp
@@ -57,17 +57,8 @@ std::shared_ptr<ov::Node> make_gather_nd8(const ov::Output<Node>& data_node,
         const auto max_dim = *std::max_element(begin(data_shape), end(data_shape));
 
         std::vector<int> indices_values(indices_count * slice_rank);
-        double startFrom = 0.0;
-        auto range = max_dim;
-
-        if (targetDevice == "CPU") {
-            // Only CPU plugin supports negative indices.
-            startFrom = -static_cast<double>(max_dim);
-            range = 2 * max_dim;
-        }
-
-        ov::test::utils::fill_data_random<int>(indices_values.data(), indices_count * slice_rank, range, startFrom);
-
+        const double startFrom = -static_cast<double>(max_dim);
+        ov::test::utils::fill_data_random<int>(indices_values.data(), indices_count * slice_rank, 2*max_dim, startFrom);
         auto indices_data = indices_values.data();
         for (int i = 0; i < indices_count; i++) {
             for (int dim = 0; dim < slice_rank; dim++) {
