e789381bc1408482bd7dcec6d7589708f68a26b3
Dmitry Matveev <dmitry.matveev@intel.com>
2025-05-29 20:25:16 +0000

    NPUW: Extend the "tiny" threshold to 16K elements

--- src/plugins/intel_npu/src/plugin/npuw/partitioning/traits.cpp
+++ src/plugins/intel_npu/src/plugin/npuw/partitioning/traits.cpp
@@ -9,13 +9,13 @@ namespace ov {
 namespace npuw {
 
 bool partitioning::traits::is_tiny_shape(const ov::Shape &shape) {
-    const auto total = std::accumulate(shape.begin(), shape.end(), std::size_t{1}, std::multiplies<std::size_t>());
-
-    // The initial check
-    if ((shape.size() == 0 || (shape.size() == 1 && shape[0] <= 10)) || (total <= 10)) {
+    const std::size_t TINY_THRESHOLD = 16384;
+    if (shape.size() == 0 || (shape.size() == 1 && shape[0] <= TINY_THRESHOLD)) {
         return true;
     }
-    return false;
+
+    const auto total = std::accumulate(shape.begin(), shape.end(), std::size_t{1}, std::multiplies<std::size_t>());
+    return (total <= TINY_THRESHOLD);
 }
 
 bool partitioning::traits::is_tiny_scalar(const std::shared_ptr<ov::Node> &node) {
