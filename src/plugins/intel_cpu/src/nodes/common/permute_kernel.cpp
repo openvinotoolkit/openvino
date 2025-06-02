984979812c861f5bbf69adde0b98586ad44f68eb
liang <gxgaoliang@126.com>
2025-06-02 23:03:37 +0800

    Try to fix 1D transpose unit test (#30761)
    ### Details:
    - The `Transpose` can't be eliminated from the model, because the test
    case only has a single OP. It connects from `Parameter` to `Result`
    node.
    - The exception shows `Permute` kernel didn't handle the input/output
    buffers are the same. This case should be optimized.
    
    
    ![image](https://github.com/user-attachments/assets/13e832c4-fcd0-40f9-93ad-69e402521307)
    
    
    ### Tickets:
     - *165449*
    
    ---------
    
    Co-authored-by: Maksim Kutakov <maxim.kutakov@gmail.com>

--- src/plugins/intel_cpu/src/nodes/common/permute_kernel.cpp
+++ src/plugins/intel_cpu/src/nodes/common/permute_kernel.cpp
@@ -220,6 +220,16 @@ void PermuteKernel::optimizedExecute(const uint8_t* src_data, const uint8_t* dst
     }
 
     switch (jcp.n) {
+    case 0:
+    // This is a degenerate case that is only possible if the tensor has 0 or 1st rank
+    // Such a situation is possible in the following graph:
+    //  Parameter
+    //     |
+    //  Transpose
+    //     |
+    //  Result
+    // The elimination of the Transpose node will not be performed
+    // So copy from input buffer to output buffer without any permutation
     case 1:
         parallel_for(dst_dims[0], [&](int i0) {
             auto arg = jit_args_permute();
