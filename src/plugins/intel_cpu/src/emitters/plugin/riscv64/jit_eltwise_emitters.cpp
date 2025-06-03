4f177ddc485955e7910c5f48c2370f69c98afb6a
arunthakur009 <nk9599341@gmail.com>
2025-06-03 23:22:10 +0530

    Fixed the code style

--- src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.cpp
+++ src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.cpp
@@ -495,25 +495,30 @@ std::set<std::vector<element::Type>> jit_floor_emitter::get_supported_precisions
     return {{element::f32}};
 }
 /// GREATER EQUAL ///
-jit_greater_equal_emitter::jit_greater_equal_emitter(jit_generator* host, cpu_isa_t host_isa, const element::Type exec_prc):
-    jit_emitter(host, host_isa, exec_prc) {
+jit_greater_equal_emitter::jit_greater_equal_emitter(jit_generator* host,
+                                                     cpu_isa_t host_isa,
+                                                     const element::Type exec_prc)
+    : jit_emitter(host, host_isa, exec_prc) {
     prepare_table();
 }
 
-jit_greater_equal_emitter::jit_greater_equal_emitter(jit_generator* host, cpu_isa_t host_isa, const std::shared_ptr<ov::Node>& node) :
-    jit_emitter(host, host_isa, get_arithmetic_binary_exec_precision(node)) {
+jit_greater_equal_emitter::jit_greater_equal_emitter(jit_generator* host,
+                                                     cpu_isa_t host_isa,
+                                                     const std::shared_ptr<ov::Node>& node)
+    : jit_emitter(host, host_isa, get_arithmetic_binary_exec_precision(node)) {
     prepare_table();
 }
 
 size_t jit_greater_equal_emitter::get_inputs_num() const {
-    return 2;  
+    return 2;
 }
 
 size_t jit_greater_equal_emitter::aux_fp_gprs_count() const {
-    return 1;  
+    return 1;
 }
 
-void jit_greater_equal_emitter::emit_impl(const std::vector<size_t>& in_vec_idxs, const std::vector<size_t>& out_vec_idxs) const {
+void jit_greater_equal_emitter::emit_impl(const std::vector<size_t>& in_vec_idxs,
+                                          const std::vector<size_t>& out_vec_idxs) const {
     if (host_isa_ == ov::intel_cpu::riscv64::cpu_isa_t::gv) {
         emit_isa<ov::intel_cpu::riscv64::cpu_isa_t::gv>(in_vec_idxs, out_vec_idxs);
     } else {
@@ -522,15 +527,15 @@ void jit_greater_equal_emitter::emit_impl(const std::vector<size_t>& in_vec_idxs
 }
 
 template <ov::intel_cpu::riscv64::cpu_isa_t isa>
-void jit_greater_equal_emitter::emit_isa(const std::vector<size_t>& in_vec_idxs, const std::vector<size_t>& out_vec_idxs) const {
+void jit_greater_equal_emitter::emit_isa(const std::vector<size_t>& in_vec_idxs,
+                                         const std::vector<size_t>& out_vec_idxs) const {
     VReg src0 = VReg(in_vec_idxs[0]);
     VReg src1 = VReg(in_vec_idxs[1]);
     VReg dst = VReg(out_vec_idxs[0]);
-    
+
     FReg one = FReg(aux_fp_gpr_idxs[0]);
     load_table_val("one", one);
 
-    
     h->vmv_v_x(dst, zero);
     h->vmfge_vv(mask_vreg(), src0, src1);
     h->vfadd_vf(dst, dst, one, VM::masked);
@@ -540,7 +545,8 @@ void jit_greater_equal_emitter::register_table_entries() {
     push_arg_entry_of("one", CONST_1_F);
 }
 
-std::set<std::vector<element::Type>> jit_greater_equal_emitter::get_supported_precisions(const std::shared_ptr<ov::Node>& node) {
+std::set<std::vector<element::Type>> jit_greater_equal_emitter::get_supported_precisions(
+    const std::shared_ptr<ov::Node>& node) {
     return {{element::f32, element::f32}};
 }
 /// MAXIMUM ///
--- src/plugins/intel_cpu/src/nodes/kernels/riscv64/jit_uni_eltwise_generic.cpp
+++ src/plugins/intel_cpu/src/nodes/kernels/riscv64/jit_uni_eltwise_generic.cpp
@@ -440,7 +440,7 @@ std::shared_ptr<jit_emitter> jit_uni_eltwise_generic<isa>::create_eltwise_emitte
               OV_CASE(Algorithm::EltwiseDivide, jit_divide_emitter),
               OV_CASE(Algorithm::EltwiseExp, jit_exp_emitter),
               OV_CASE(Algorithm::EltwiseFloor, jit_floor_emitter),
-              OV_CASE(Algorithm::EltwiseGreaterEqual,jit_greater_equal_emitter),
+              OV_CASE(Algorithm::EltwiseGreaterEqual, jit_greater_equal_emitter),
               OV_CASE(Algorithm::EltwiseMaximum, jit_maximum_emitter),
               OV_CASE(Algorithm::EltwiseMinimum, jit_minimum_emitter),
               OV_CASE(Algorithm::EltwiseMod, jit_mod_emitter),
