b76f5daf9a7845a46ac32e52a3913c1daf362cca
Nam, David <david.nam@intel.com>
2025-06-05 00:18:23 +0900

    Add desc_size to serialize

--- src/plugins/intel_gpu/src/graph/impls/onednn/convolution_onednn.cpp
+++ src/plugins/intel_gpu/src/graph/impls/onednn/convolution_onednn.cpp
@@ -322,7 +322,7 @@ public:
             auto prim_desc = std::make_shared<dnnl::convolution_forward::primitive_desc>(
                                     ib.get_engine().get_onednn_engine(),
                                     dnnl::prop_kind::forward_inference, dnnl::algorithm::convolution_direct,
-                                    input_md, weights_md, nullptr, output_md,
+                                    input_md, weights_md, output_md,
                                     strides, dilates, padding_l, padding_r,
                                     *_attrs.get());
             _pd = *prim_desc;
