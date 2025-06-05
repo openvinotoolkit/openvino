ca25d1060b59ff24405c1308ded607cde96178af
Kim, Eddy <eddy.kim@intel.com>
2025-06-05 20:49:29 +0900

    updated can_fuse_reorder_to_prev

--- src/plugins/intel_gpu/src/graph/layout_optimizer.cpp
+++ src/plugins/intel_gpu/src/graph/layout_optimizer.cpp
@@ -138,15 +138,12 @@ bool layout_optimizer::can_fuse_reorder(program_node& prev, program_node& next, 
     auto next_dt = next.get_output_layout().data_type;
     auto use_onednn_impls = has_all_enabled_onednn_impls_optimization_attribute();
 
-    if (prev.is_dynamic() && next.is_dynamic()) {
-        if (next.is_type<permute>())
-            return true;
-        if (next.is_type<group_normalization>() && fmt_prev == format::b_fs_yx_fsv16 && fmt_next == format::bfyx)
-            return true;
+    if (prev.is_dynamic() || next.is_dynamic()) {
+        if (!next.is_type<permute>() &&
+            !next.is_type<group_normalization>()) {
+            return false;
+        }
     }
-
-    if (prev.is_dynamic() || next.is_dynamic())
-        return false;
 
     auto is_input_idx = [&](size_t idx) -> bool {
         if (&next.get_dependency(idx) == &prev)
@@ -247,6 +244,9 @@ bool layout_optimizer::can_fuse_reorder(program_node& prev, program_node& next, 
 
     if (next.is_type<quantize>() && (fmt_prev == format::bfyx || fmt_prev == format::bfzyx) &&
         prev.is_input() && (prev_dt == data_types::u8 || prev_dt == data_types::i8))
+        return true;
+
+    if (next.is_type<group_normalization>() && fmt_prev == format::b_fs_yx_fsv16 && fmt_next == format::bfyx)
         return true;
 
     if (!use_onednn_impls || next.get_preferred_impl_type() == impl_types::ocl) {
@@ -363,10 +363,10 @@ bool layout_optimizer::can_fuse_reorder_to_prev(program_node& prev, reorder_node
     }
 
     if (prev.is_dynamic() || (!node.get_users().empty() && node.get_users().front()->is_dynamic())) {
-        if (!prev.is_type<permute>() &&
-            !prev.is_type<group_normalization>()) {
+        // if (!prev.is_type<permute>() &&
+        //     !prev.is_type<group_normalization>()) {
             return false;
-        }
+        // }
     }
 
     // Ref kernels are the main for depth_to_space, region_yolo and detection_output. It can do anything. Should not see next.
@@ -429,8 +429,8 @@ bool layout_optimizer::can_fuse_reorder_to_prev(program_node& prev, reorder_node
         if (allow_new_shape_infer && (fmt_prev.dimension() != fmt_next.dimension()))
             return false;
 
-        if (dt_prev != node.get_output_layout().data_type)
-            return false;
+        // if (dt_prev != node.get_output_layout().data_type)
+        //     return false;
 
         return true;
     }
