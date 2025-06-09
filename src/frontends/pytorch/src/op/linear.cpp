0edd766ed1c745809ffc14624accab9b4522e100
Maxim Vafin <maxim.vafin@intel.com>
2025-06-09 13:09:11 +0200

    [PT FE] Support bitnet quantized models
    Signed-off-by: Maxim Vafin <maxim.vafin@intel.com>

--- src/frontends/pytorch/src/op/linear.cpp
+++ src/frontends/pytorch/src/op/linear.cpp
@@ -64,6 +64,28 @@ OutputVector translate_linear_ext(const NodeContext& context) {
 };
 
 namespace {
+
+Output<Node> low_precision_subgraph(const NodeContext& context,
+                                    const Output<Node>& x,
+                                    const Output<Node>& weights,
+                                    const Output<Node>& zero_points,
+                                    const Output<Node>& scales) {
+    auto new_qweight = context.mark_node(std::make_shared<v0::Convert>(weights, scales.get_element_type()));
+    auto new_qzeros = context.mark_node(std::make_shared<v0::Convert>(zero_points, scales.get_element_type()));
+
+    auto w_s = context.mark_node(std::make_shared<v1::Subtract>(new_qweight, new_qzeros));
+    auto weight = context.mark_node(std::make_shared<v1::Multiply>(w_s, scales));
+    auto weight_shape = weights.get_shape();
+    if (weight_shape.size() > 2) {
+        auto out_shape = v0::Constant::create(element::i32,
+                                              {2},
+                                              std::vector<int32_t>{static_cast<int32_t>(weights.get_shape()[0]), -1});
+        weight = context.mark_node(std::make_shared<v1::Reshape>(weight, out_shape, false));
+    }
+    weight = context.mark_node(std::make_shared<v1::ConvertLike>(weight, x));
+    return weight;
+}
+
 uint32_t rearrange_awq_bits(uint32_t num) {
     uint32_t result = 0;
     uint32_t mask = 0xF;
@@ -81,6 +103,22 @@ uint32_t rearrange_awq_bits(uint32_t num) {
     return result;
 }
 
+uint16_t rearrange_bitnet_bits(uint16_t num) {
+    uint16_t result = 0;
+    const uint16_t mask = 0x0003;
+
+    result |= (num & (mask << 0)) << 0;
+    result |= (num & (mask << 8)) >> 6;
+    result |= (num & (mask << 4)) << 0;
+    result |= (num & (mask << 10)) >> 4;
+    result |= (num & (mask << 6)) << 2;
+    result |= (num & (mask << 12)) >> 2;
+    result |= (num & (mask << 8)) << 4;
+    result |= (num & (mask << 14)) << 0;
+
+    return result;
+}
+
 Output<Node> rearrange_constant(const Output<Node>& c, uint32_t groups) {
     auto constant = ov::as_type_ptr<v0::Constant>(c.get_node_shared_ptr());
     FRONT_END_OP_CONVERSION_CHECK(constant, "weight must be Constant.");
@@ -93,6 +131,7 @@ Output<Node> rearrange_constant(const Output<Node>& c, uint32_t groups) {
     for (size_t i = 0; i < shape_size(constant->get_shape()); i++) {
         dst[i] = rearrange_awq_bits(src[i]);
     }
+    new_qweight->set_friendly_name(constant->get_friendly_name());
     return new_qweight;
 }
 }  // namespace
@@ -110,24 +149,49 @@ OutputVector translate_linear_awq(const NodeContext& context) {
 
     auto new_qweight = rearrange_constant(qweight, static_cast<uint32_t>(groups));
     auto new_qzeros = rearrange_constant(qzeros, 1);
-    new_qweight = context.mark_node(std::make_shared<v0::Convert>(new_qweight, scales.get_element_type()));
-    new_qzeros = context.mark_node(std::make_shared<v0::Convert>(new_qzeros, scales.get_element_type()));
-
-    auto w_s = context.mark_node(std::make_shared<v1::Subtract>(new_qweight, new_qzeros));
     FRONT_END_OP_CONVERSION_CHECK(scales.get_partial_shape().is_static(), "Scales must be constant.");
     auto scales_shape = scales.get_shape();
     auto new_scales_shape =
         v0::Constant::create(element::i32, {3}, std::vector<uint64_t>{scales_shape[0], 1, scales_shape[1]});
-    scales = context.mark_node(std::make_shared<v1::Reshape>(scales, new_scales_shape, false));
-    auto weight = context.mark_node(std::make_shared<v1::Multiply>(w_s, scales));
-    auto out_shape =
-        v0::Constant::create(element::i32, {2}, std::vector<int32_t>{static_cast<int32_t>(qweight.get_shape()[0]), -1});
-    weight = context.mark_node(std::make_shared<v1::Reshape>(weight, out_shape, false));
-    weight = context.mark_node(std::make_shared<v1::ConvertLike>(weight, x));
+    auto new_scales = context.mark_node(std::make_shared<v1::Reshape>(scales, new_scales_shape, false));
+    auto weight = low_precision_subgraph(context, x, new_qweight, new_qzeros, new_scales);
 
     auto matmul = context.mark_node(std::make_shared<v0::MatMul>(x, weight, false, false));
     if (!context.input_is_none(6)) {
         auto bias = context.get_input(6);
+
+        if (bias.get_element_type() == element::f16 || bias.get_element_type() == element::bf16) {
+            bias = context.mark_node(std::make_shared<v1::ConvertLike>(bias, x));
+        }
+        matmul = context.mark_node(std::make_shared<v1::Add>(matmul, bias));
+    }
+    return {matmul};
+};
+
+OutputVector translate_linear_bitnet(const NodeContext& context) {
+    num_inputs_check(context, 3, 4);
+    auto x = context.get_input(0);
+    auto weight = context.get_input(1);
+    auto scales = context.get_input(2);
+
+    auto constant = ov::as_type_ptr<v0::Constant>(weight.get_node_shared_ptr());
+    FRONT_END_OP_CONVERSION_CHECK(constant, "weight must be Constant.");
+    auto src = reinterpret_cast<const uint16_t*>(constant->get_data_ptr());
+    auto initial_shape = constant->get_shape();
+    FRONT_END_OP_CONVERSION_CHECK(initial_shape.size() == 2, "Only 2D constants are supported.");
+    auto new_shape = Shape{initial_shape[0] * 4, initial_shape[1]};
+    auto new_weight = std::make_shared<v0::Constant>(element::u2, new_shape);
+    auto dst = const_cast<uint16_t*>(reinterpret_cast<const uint16_t*>(new_weight->get_data_ptr()));
+    for (size_t i = 0; i < shape_size(initial_shape) / 2; i++) {
+        dst[i] = rearrange_bitnet_bits(src[i]);
+    }
+    new_weight->set_friendly_name(constant->get_friendly_name());
+    auto zero_point = context.mark_node(std::make_shared<v0::Constant>(element::u2, Shape{}, 1));
+    auto mm_weight = low_precision_subgraph(context, x, new_weight, zero_point, scales);
+
+    auto matmul = context.mark_node(std::make_shared<v0::MatMul>(x, mm_weight, false, true));
+    if (!context.input_is_none(3)) {
+        auto bias = context.get_input(3);
 
         if (bias.get_element_type() == element::f16 || bias.get_element_type() == element::bf16) {
             bias = context.mark_node(std::make_shared<v1::ConvertLike>(bias, x));
