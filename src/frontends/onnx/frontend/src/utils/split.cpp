6356e71838f934aa4333a19dc9c96735d09ffe06
LiangGao <liang.gao@intel.com>
2025-06-11 16:08:25 +0800

    Fix dynamic shape

--- src/frontends/onnx/frontend/src/utils/split.cpp
+++ src/frontends/onnx/frontend/src/utils/split.cpp
@@ -21,9 +21,9 @@ OutputVector make_split(const Output<ov::Node>& value, const std::vector<int64_t
 }
 
 OutputVector make_split(const Output<ov::Node>& value, int64_t num_splits, int64_t axis) {
-    auto axis_shape = value.get_partial_shape()[axis];
-    if (axis_shape.is_static()) {
-        auto axis_len = axis_shape.get_length();
+    auto value_shape = value.get_partial_shape();
+    if (value_shape.rank().is_static() && value_shape.size() > axis && value_shape[axis].is_static()) {
+        auto axis_len = value_shape[axis].get_length();
         // In ONNX definition, if the tensor is not evenly splittable the last chunk will be smaller.
         // Handle the case when axis_len is not divisible by num_splits
         if (axis_len % num_splits) {
