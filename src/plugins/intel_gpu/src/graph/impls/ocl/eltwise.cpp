1af537fb38a45015a40ac85802ba21bab919aa90
Kim, SungEun <sungeun.kim@intel.com>
2025-06-16 20:12:57 +0900

    Revert "add format for eltwise"
    This reverts commit 7be3177b8ecc4bc6df255e39f99179ae34f9b9b0.

--- src/plugins/intel_gpu/src/graph/impls/ocl/eltwise.cpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl/eltwise.cpp
@@ -306,7 +306,6 @@ attach_eltwise_impl::attach_eltwise_impl() {
 
         std::make_tuple(data_types::i8, format::b_fs_yx_fsv4),
         std::make_tuple(data_types::u8, format::b_fs_yx_fsv4),
-        std::make_tuple(data_types::f16, format::b_fs_yx_fsv4),
         std::make_tuple(data_types::f32, format::b_fs_yx_fsv4),
 
         std::make_tuple(data_types::i8, format::b_fs_yx_fsv32),
