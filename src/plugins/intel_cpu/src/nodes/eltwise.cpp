b998e02d6ffce1ade84d9bb300c80a413cbe0f69
Vladislav Denisov <vlad.roix@yandex.ru>
2025-06-16 20:07:45 +0400

    [CPU] [ARM64] jit erf emitter (#30983)
    ### Details:
     - [CPU] [ARM64] jit erf emitter
    
    ### Tickets:
     - https://github.com/openvinotoolkit/openvino/issues/27499

--- src/plugins/intel_cpu/src/nodes/eltwise.cpp
+++ src/plugins/intel_cpu/src/nodes/eltwise.cpp
@@ -850,7 +850,6 @@ public:
 #elif defined(OPENVINO_ARCH_ARM64)
         if (one_of(algorithm,
                    Algorithm::EltwiseHsigmoid,
-                   Algorithm::EltwiseErf,
                    Algorithm::EltwiseBitwiseAnd,
                    Algorithm::EltwiseBitwiseNot,
                    Algorithm::EltwiseBitwiseOr,
