45cabca0d63ba47e5b1ff8a64b20039ee3688fd5
Vladislav Golubev <vladislav.golubev@intel.com>
2025-06-17 17:05:40 +0200

    clang-tidy

--- src/plugins/intel_cpu/src/emitters/snippets/x64/jit_parallel_loop_emitters.hpp
+++ src/plugins/intel_cpu/src/emitters/snippets/x64/jit_parallel_loop_emitters.hpp
@@ -24,10 +24,10 @@ protected:
 
     size_t work_amount = 0;
     size_t wa_increment = 0;
-    std::vector<bool> is_incremented = {};
-    std::vector<int64_t> ptr_increments = {};
-    std::vector<int64_t> finalization_offsets = {};
-    std::vector<int64_t> data_sizes = {};
+    std::vector<bool> is_incremented;
+    std::vector<int64_t> ptr_increments;
+    std::vector<int64_t> finalization_offsets;
+    std::vector<int64_t> data_sizes;
     size_t num_inputs = 0;
     size_t num_outputs = 0;
     size_t loop_id = 0;
