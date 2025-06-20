e700626836c0ff4781936242f9092d3ff3e2a578
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2025-06-20 17:05:36 +0200

    Test log type
    Signed-off-by: Tomasz Jankowski <tomasz1.jankowski@intel.com>

--- src/core/tests/logger.cpp
+++ src/core/tests/logger.cpp
@@ -13,13 +13,14 @@
 
 namespace ov::util::test {
 
-using LogEntries = std::tuple<const char*, int, const char*>;
+using LogEntries = std::tuple<LOG_TYPE, const char*, int, const char*>;
 
 class TestLogHelper : public testing::TestWithParam<LogEntries> {
     std::ostream* const actual_out_stream = &std::cout;
     std::streambuf* const actual_out_buf = actual_out_stream->rdbuf();
 
     // LogEntries
+    LOG_TYPE m_log_type;
     const char* m_log_path;
     int m_log_line;
     const char* m_log_message;
@@ -30,7 +31,7 @@ protected:
         actual_out_stream->flush();
         actual_out_stream->rdbuf(m_mock_out_stream.rdbuf());
 
-        std::tie(m_log_path, m_log_line, m_log_message) = GetParam();
+        std::tie(m_log_type, m_log_path, m_log_line, m_log_message) = GetParam();
 
         m_log_handler = [message = &m_callback_message](const std::string& msg) {
             *message = msg;
@@ -43,12 +44,20 @@ protected:
     }
 
     auto log_test_params() {
-        LogHelper{LOG_TYPE::_LOG_TYPE_INFO, m_log_path, m_log_line, get_log_handler()}.stream() << m_log_message;
+        LogHelper{m_log_type, m_log_path, m_log_line, get_log_handler()}.stream() << m_log_message;
     }
 
     auto get_log_regex() const {
+        static auto log_prefix_pattern =
+            std::map<LOG_TYPE, const std::string>{{LOG_TYPE::_LOG_TYPE_ERROR, R"(\[ERR\])"},
+                                                  {LOG_TYPE::_LOG_TYPE_WARNING, R"(\[WARN\])"},
+                                                  {LOG_TYPE::_LOG_TYPE_INFO, R"(\[INFO\])"},
+                                                  {LOG_TYPE::_LOG_TYPE_DEBUG, R"(\[DEBUG\])"}};
         std::stringstream log_regex;
-        log_regex << m_log_path << ".*" << m_log_line << ".*" << m_log_message;
+        if (LOG_TYPE::_LOG_TYPE_DEBUG_EMPTY != m_log_type) {
+            log_regex << log_prefix_pattern[m_log_type] << ".*" << m_log_path << ".*" << m_log_line << ".*";
+        }
+        log_regex << m_log_message;
         return std::regex{log_regex.str()};
     }
 
@@ -91,6 +100,11 @@ TEST_P(TestLogHelper, no_log) {
 
 INSTANTIATE_TEST_SUITE_P(Logging,
                          TestLogHelper,
-                         ::testing::ValuesIn(std::vector<LogEntries>{{"the_path", 42, "tEst-mEssagE"},
-                                                                     {"in the middle", 0.f, "the nowhere"}}));
+                         ::testing::ValuesIn(std::vector<LogEntries>{
+                             {LOG_TYPE::_LOG_TYPE_WARNING, "the_file_path", 41, "ignored"},
+                             {LOG_TYPE::_LOG_TYPE_ERROR, "the_file_path", 42, "happened"},
+                             {LOG_TYPE::_LOG_TYPE_INFO, "too-long-file", 3141592, "to read"},
+                             {LOG_TYPE::_LOG_TYPE_DEBUG, "in the middle", 0.f, "the nowhere"},
+                             {LOG_TYPE::_LOG_TYPE_DEBUG_EMPTY, "a:\\main.c", -101, "loading..."},
+                         }));
 }  // namespace ov::util::test
