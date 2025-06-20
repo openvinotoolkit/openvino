f2632b9cdbc8e0b1c13e963ddafe7d2177b6a70d
Arseniy Obolenskiy <arseniy.obolenskiy@intel.com>
2025-06-20 10:47:32 +0200

    Apply format remarks

--- src/plugins/intel_cpu/src/emitters/snippets/aarch64/jit_reductions_emitters.cpp
+++ src/plugins/intel_cpu/src/emitters/snippets/aarch64/jit_reductions_emitters.cpp
@@ -8,15 +8,15 @@
 
 namespace ov::intel_cpu::aarch64 {
 
-/// reduce_max ///
+/// horizon_max ///
 jit_horizon_max_emitter::jit_horizon_max_emitter(dnnl::impl::cpu::aarch64::jit_generator* host,
-                                               dnnl::impl::cpu::aarch64::cpu_isa_t host_isa,
-                                               const std::shared_ptr<ov::Node>& node)
+                                                 dnnl::impl::cpu::aarch64::cpu_isa_t host_isa,
+                                                 const std::shared_ptr<ov::Node>& node)
     : jit_emitter(host, host_isa, node, get_arithmetic_binary_exec_precision(node)) {}
 
 jit_horizon_max_emitter::jit_horizon_max_emitter(dnnl::impl::cpu::aarch64::jit_generator* host,
-                                               dnnl::impl::cpu::aarch64::cpu_isa_t host_isa,
-                                               const ov::element::Type exec_prc)
+                                                 dnnl::impl::cpu::aarch64::cpu_isa_t host_isa,
+                                                 const ov::element::Type exec_prc)
     : jit_emitter(host, host_isa, exec_prc) {}
 
 size_t jit_horizon_max_emitter::get_inputs_count() const {
@@ -24,7 +24,7 @@ size_t jit_horizon_max_emitter::get_inputs_count() const {
 }
 
 void jit_horizon_max_emitter::emit_impl(const std::vector<size_t>& in_vec_idxs,
-                                       const std::vector<size_t>& out_vec_idxs) const {
+                                        const std::vector<size_t>& out_vec_idxs) const {
     if (host_isa_ == dnnl::impl::cpu::aarch64::asimd) {
         emit_isa<dnnl::impl::cpu::aarch64::asimd>(in_vec_idxs, out_vec_idxs);
     } else {
@@ -34,7 +34,7 @@ void jit_horizon_max_emitter::emit_impl(const std::vector<size_t>& in_vec_idxs,
 
 template <dnnl::impl::cpu::aarch64::cpu_isa_t isa>
 void jit_horizon_max_emitter::emit_isa(const std::vector<size_t>& in_vec_idxs,
-                                      const std::vector<size_t>& out_vec_idxs) const {
+                                       const std::vector<size_t>& out_vec_idxs) const {
     h->fmaxv(Xbyak_aarch64::SReg(out_vec_idxs[0]), Xbyak_aarch64::VReg4S(in_vec_idxs[0]));
 }
 
@@ -43,15 +43,15 @@ std::set<std::vector<element::Type>> jit_horizon_max_emitter::get_supported_prec
     return {{element::f32}};
 }
 
-/// reduce_sum ///
+/// horizon_sum ///
 jit_horizon_sum_emitter::jit_horizon_sum_emitter(dnnl::impl::cpu::aarch64::jit_generator* host,
-                                               dnnl::impl::cpu::aarch64::cpu_isa_t host_isa,
-                                               const std::shared_ptr<ov::Node>& node)
+                                                 dnnl::impl::cpu::aarch64::cpu_isa_t host_isa,
+                                                 const std::shared_ptr<ov::Node>& node)
     : jit_emitter(host, host_isa, node, get_arithmetic_binary_exec_precision(node)) {}
 
 jit_horizon_sum_emitter::jit_horizon_sum_emitter(dnnl::impl::cpu::aarch64::jit_generator* host,
-                                               dnnl::impl::cpu::aarch64::cpu_isa_t host_isa,
-                                               const ov::element::Type exec_prc)
+                                                 dnnl::impl::cpu::aarch64::cpu_isa_t host_isa,
+                                                 const ov::element::Type exec_prc)
     : jit_emitter(host, host_isa, exec_prc) {}
 
 size_t jit_horizon_sum_emitter::get_inputs_count() const {
@@ -59,7 +59,7 @@ size_t jit_horizon_sum_emitter::get_inputs_count() const {
 }
 
 void jit_horizon_sum_emitter::emit_impl(const std::vector<size_t>& in_vec_idxs,
-                                       const std::vector<size_t>& out_vec_idxs) const {
+                                        const std::vector<size_t>& out_vec_idxs) const {
     if (host_isa_ == dnnl::impl::cpu::aarch64::asimd) {
         emit_isa<dnnl::impl::cpu::aarch64::asimd>(in_vec_idxs, out_vec_idxs);
     } else {
@@ -69,7 +69,7 @@ void jit_horizon_sum_emitter::emit_impl(const std::vector<size_t>& in_vec_idxs,
 
 template <dnnl::impl::cpu::aarch64::cpu_isa_t isa>
 void jit_horizon_sum_emitter::emit_isa(const std::vector<size_t>& in_vec_idxs,
-                                      const std::vector<size_t>& out_vec_idxs) const {
+                                       const std::vector<size_t>& out_vec_idxs) const {
     const uint32_t in = in_vec_idxs[0];
     const uint32_t out = out_vec_idxs[0];
 
