d0f878e577bb03082926aed26d793d57bb5dab4e
Alexander Nesterov <nesterov.alexander@outlook.com>
2025-06-22 16:56:11 +0200

    Extract MVNRefExecutor into a separate implementation file for better modularity and maintainability.

---
+++ src/plugins/intel_cpu/src/nodes/executors/common/ref_mvn.hpp
@@ -0,0 +1,27 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include "nodes/executors/mvn_config.hpp"
+#include "cpu_types.h"
+
+namespace ov::intel_cpu {
+namespace legacy {
+
+class MVNRefExecutor : public legacy::MVNExecutorBase {
+public:
+    MVNRefExecutor(const MVNAttrs& mvnAttrs);
+
+    void exec(const uint8_t* src_data,
+              uint8_t* dst_data,
+              const void* post_ops_data_,
+              const VectorDims& shape5d) override;
+
+private:
+    void mvn_ref(const uint8_t* src_data, uint8_t* dst_data, const VectorDims& shape5d);
+};
+
+}  // namespace legacy
+}  // namespace ov::intel_cpu
--- src/plugins/intel_cpu/src/nodes/mvn.h
+++ src/plugins/intel_cpu/src/nodes/mvn.h
@@ -62,19 +62,6 @@ private:
     std::shared_ptr<legacy::MVNExecutorBase> execPtr = nullptr;
     bool canUseAclExecutor = false;
     std::shared_ptr<legacy::MVNExecutor> aclExecPtr = nullptr;
-
-    class MVNRefExecutor : public legacy::MVNExecutorBase {
-    public:
-        MVNRefExecutor(const MVNAttrs& mvnAttrs);
-
-        void exec(const uint8_t* src_data,
-                  uint8_t* dst_data,
-                  const void* post_ops_data_,
-                  const VectorDims& shape5d) override;
-
-    private:
-        void mvn_ref(const uint8_t* src_data, uint8_t* dst_data, const VectorDims& shape5d);
-    };
 };
 
 }  // namespace ov::intel_cpu::node
