d4136f99842dedb8a4c03befd33b3d5e930343b6
kinnam888 <125236300+kinnam888@users.noreply.github.com>
2025-06-23 18:42:26 +0800

    [RISCV64]: Implement CPU plugin just-in-time emitter for LessEqual operation  (#31058)
    ### Details:
     - *Implement Jit Emitter for LessEqual*
    
    ### Tickets:
     - #30252
     -  Test Result
    
    ![image](https://github.com/user-attachments/assets/f7750355-b10b-4cca-934e-3a2520e3a778)
    
    ---------
    
    Signed-off-by: Kin Nam, Ng <kin.nam.ng@intel.com>

--- src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.hpp
+++ src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.hpp
@@ -181,6 +181,28 @@ private:
     void emit_isa(const std::vector<size_t>& in_vec_idxs, const std::vector<size_t>& out_vec_idxs) const;
     void register_table_entries() override;
 };
+
+class jit_less_equal_emitter : public jit_emitter {
+public:
+    jit_less_equal_emitter(ov::intel_cpu::riscv64::jit_generator* host,
+                           ov::intel_cpu::riscv64::cpu_isa_t host_isa,
+                           const ov::element::Type exec_prc = ov::element::f32);
+    jit_less_equal_emitter(ov::intel_cpu::riscv64::jit_generator* host,
+                           ov::intel_cpu::riscv64::cpu_isa_t host_isa,
+                           const std::shared_ptr<ov::Node>& node);
+    size_t get_inputs_num() const override;
+    size_t aux_fp_gprs_count() const override;
+
+    static std::set<std::vector<element::Type>> get_supported_precisions(
+        const std::shared_ptr<ov::Node>& node = nullptr);
+
+private:
+    void emit_impl(const std::vector<size_t>& in_vec_idxs, const std::vector<size_t>& out_vec_idxs) const override;
+    template <ov::intel_cpu::riscv64::cpu_isa_t isa>
+    void emit_isa(const std::vector<size_t>& in_vec_idxs, const std::vector<size_t>& out_vec_idxs) const;
+    void register_table_entries() override;
+};
+
 class jit_maximum_emitter : public jit_emitter {
 public:
     jit_maximum_emitter(jit_generator* host, cpu_isa_t host_isa, const element::Type exec_prc = element::f32);
