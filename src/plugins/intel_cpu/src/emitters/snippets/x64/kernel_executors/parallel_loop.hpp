958fd1add52562c14349cdc8fb3ab1b9350ab5cd
Vladislav Golubev <vladislav.golubev@intel.com>
2025-06-27 15:37:09 +0200

    1. Introduced ParallelLoopExecutor::call_args 2. Remove loop_args_t from executor config (it should be passed from jit_snippets_call_args in dynamic case, and from emitter in static case)

--- src/plugins/intel_cpu/src/emitters/snippets/x64/kernel_executors/parallel_loop.hpp
+++ src/plugins/intel_cpu/src/emitters/snippets/x64/kernel_executors/parallel_loop.hpp
@@ -9,16 +9,13 @@
 
 namespace ov::intel_cpu {
 
-struct ParallelLoopConfig : public ov::snippets::KernelExecutorBase::GenericConfig {
+class ParallelLoopConfig : public ov::snippets::KernelExecutorBase::GenericConfig {
 public:
     ParallelLoopConfig() = default;
-    ParallelLoopConfig(const jit_snippets_call_args::loop_args_t& loop_args, size_t increment)
-        : m_loop_args(loop_args),
-          m_increment(increment) {}
+    ParallelLoopConfig(size_t increment) : m_increment(increment) {}
 
     [[nodiscard]] bool is_completed() const override {
-        // todo: do we need a more detailed check on whether the work_amount is initialized properly?
-        return m_loop_args.m_work_amount >= 0;
+        return true;
     }
 
     [[nodiscard]] std::unique_ptr<GenericConfig> get_clone_ptr() const override {
@@ -27,22 +24,17 @@ public:
 
     [[nodiscard]] size_t hash() const override;
 
-    [[nodiscard]] const jit_snippets_call_args::loop_args_t& get_loop_args() const {
-        return m_loop_args;
-    }
     [[nodiscard]] size_t get_increment() const {
         return m_increment;
     }
 
 #ifdef SNIPPETS_DEBUG_CAPS
     [[nodiscard]] std::string to_string() const override {
-        // TODO: implement a more detailed string representation
-        return {};
+        return "increment = " + std::to_string(m_increment);
     };
 #endif
 
 protected:
-    jit_snippets_call_args::loop_args_t m_loop_args;
     size_t m_increment = 0;
 };
 
@@ -51,10 +43,16 @@ class ParallelLoopKernel {};
 
 class ParallelLoopExecutor : public snippets::KernelExecutor<ParallelLoopConfig, ParallelLoopKernel> {
 public:
-    ParallelLoopExecutor(ParallelLoopConfig config) : KernelExecutor(std::move(config)) {}
     using loop_preamble_t = void (*)(int64_t, void*);
+    struct call_args {
+        jit_snippets_call_args::loop_args_t* loop_args = nullptr;
+        loop_preamble_t preamble_ptr = nullptr;
+        uintptr_t** mem_ptrs = nullptr;
+    };
+    ParallelLoopExecutor(ParallelLoopConfig config) : KernelExecutor(std::move(config)) {}
+
     /** Function that will be called in runtime to execute the kernel */
-    static void execute(const ParallelLoopExecutor* executor, uintptr_t** stack_ptr, loop_preamble_t preamble_ptr);
+    static void execute(const ParallelLoopExecutor* executor, call_args* call_args);
 
 protected:
     /*** Updates stored kernel config based on runtime info from expression (e.g. new input shapes). */
@@ -65,5 +63,6 @@ protected:
      * performed if necessary. */
     void update_kernel(const ParallelLoopConfig& c, std::shared_ptr<ParallelLoopKernel>& kernel) const override;
 };
+#define GET_OFF_PARALLEL_LOOP_ARGS(field) offsetof(ParallelLoopExecutor::call_args, field)
 
 }  // namespace ov::intel_cpu
