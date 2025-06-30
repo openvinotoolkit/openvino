1e05d242c9d2aaab93e1b50bd8a04fb5fe00a6f6
Arseniy Obolenskiy <arseniy.obolenskiy@intel.com>
2025-06-30 16:06:35 +0200

    extract to parent class

--- src/plugins/intel_cpu/src/emitters/snippets/aarch64/jit_memory_emitters.cpp
+++ src/plugins/intel_cpu/src/emitters/snippets/aarch64/jit_memory_emitters.cpp
@@ -104,7 +104,26 @@ jit_load_memory_emitter::jit_load_memory_emitter(jit_generator* h, cpu_isa_t isa
 
 void jit_load_memory_emitter::emit_impl(const std::vector<size_t>& in, const std::vector<size_t>& out) const {
     if (host_isa_ == dnnl::impl::cpu::aarch64::asimd) {
-        emit_isa<dnnl::impl::cpu::aarch64::asimd>(in, out);
+        if (is_offset_runtime) {
+            XReg aux_reg(aux_gpr_idxs.back());
+            XReg base_reg(in[0]);
+            XReg reg_runtime_params = dnnl::impl::cpu::aarch64::abi_param1;
+            // load the runtime offset from args.buffer_offsets[buffer_cluster_id]
+            h->ldr(aux_reg,
+               ptr(reg_runtime_params,
+                   static_cast<int32_t>(GET_OFF(buffer_offsets) + buffer_cluster_id * sizeof(size_t))));
+            // bump the pointer
+            h->add(base_reg, base_reg, aux_reg);
+        }
+
+        load_emitter->emit_code(in, out, aux_vec_idxs, aux_gpr_idxs);
+
+        if (is_offset_runtime) {
+            XReg aux_reg(aux_gpr_idxs.back());
+            XReg base_reg(in[0]);
+            // subtract back so we leave the pointer unchanged for the caller
+            h->sub(base_reg, base_reg, aux_reg);
+        }
     } else {
         OV_CPU_JIT_EMITTER_THROW("Doesn't support isa ", host_isa_);
     }
@@ -114,26 +133,7 @@ template <cpu_isa_t isa>
 void jit_load_memory_emitter::emit_isa(const std::vector<size_t>& in, const std::vector<size_t>& out) const {
     OV_CPU_JIT_EMITTER_ASSERT(load_emitter != nullptr, "Load CPU emitter isn't initialized!");
 
-    if (is_offset_runtime) {
-        XReg aux_reg(aux_gpr_idxs.back());
-        XReg base_reg(in[0]);
-        XReg reg_runtime_params = dnnl::impl::cpu::aarch64::abi_param1;
-        // load the runtime offset from args.buffer_offsets[buffer_cluster_id]
-        h->ldr(aux_reg,
-               ptr(reg_runtime_params,
-                   static_cast<int32_t>(GET_OFF(buffer_offsets) + buffer_cluster_id * sizeof(size_t))));
-        // bump the pointer
-        h->add(base_reg, base_reg, aux_reg);
-    }
-
     load_emitter->emit_code(in, out, aux_vec_idxs, aux_gpr_idxs);
-
-    if (is_offset_runtime) {
-        XReg aux_reg(aux_gpr_idxs.back());
-        XReg base_reg(in[0]);
-        // subtract back so we leave the pointer unchanged for the caller
-        h->sub(base_reg, base_reg, aux_reg);
-    }
 }
 
 void jit_load_memory_emitter::emit_data() const {
@@ -167,26 +167,7 @@ void jit_load_broadcast_emitter::emit_isa(const std::vector<size_t>& in, const s
     auto src = XReg(in[0]);
     auto dst = TReg(out[0]);
 
-    if (is_offset_runtime) {
-        XReg aux_reg(aux_gpr_idxs.back());
-        XReg base_reg(src);
-        XReg reg_runtime_params = dnnl::impl::cpu::aarch64::abi_param1;
-        // load the runtime offset from args.buffer_offsets[buffer_cluster_id]
-        h->ldr(aux_reg,
-               ptr(reg_runtime_params,
-                   static_cast<int32_t>(GET_OFF(buffer_offsets) + buffer_cluster_id * sizeof(size_t))));
-        // bump the pointer
-        h->add(base_reg, base_reg, aux_reg);
-    }
-
     h->uni_ld1rw(dst.s, src, compiled_byte_offset);
-
-    if (is_offset_runtime) {
-        XReg aux_reg(aux_gpr_idxs.back());
-        XReg base_reg(src);
-        // subtract back so we leave the pointer unchanged for the caller
-        h->sub(base_reg, base_reg, aux_reg);
-    }
 }
 
 jit_store_memory_emitter::jit_store_memory_emitter(jit_generator* h, cpu_isa_t isa, const ExpressionPtr& expr)
@@ -214,26 +195,7 @@ template <cpu_isa_t isa>
 void jit_store_memory_emitter::emit_isa(const std::vector<size_t>& in, const std::vector<size_t>& out) const {
     OV_CPU_JIT_EMITTER_ASSERT(store_emitter != nullptr, "Store CPU emitter isn't initialized!");
 
-    if (is_offset_runtime) {
-        XReg aux_reg(aux_gpr_idxs.back());
-        XReg base_reg(out[0]);
-        // load the runtime offset from args.buffer_offsets[buffer_cluster_id]
-        XReg reg_runtime_params = dnnl::impl::cpu::aarch64::abi_param1;
-        h->ldr(aux_reg,
-               ptr(reg_runtime_params,
-                   static_cast<int32_t>(GET_OFF(buffer_offsets) + buffer_cluster_id * sizeof(size_t))));
-        // bump the pointer
-        h->add(base_reg, base_reg, aux_reg);
-    }
-
     store_emitter->emit_code(in, out, aux_vec_idxs, aux_gpr_idxs);
-
-    if (is_offset_runtime) {
-        XReg aux_reg(aux_gpr_idxs.back());
-        XReg base_reg(out[0]);
-        // subtract back so we leave the pointer unchanged
-        h->sub(base_reg, base_reg, aux_reg);
-    }
 }
 
 void jit_store_memory_emitter::emit_data() const {
