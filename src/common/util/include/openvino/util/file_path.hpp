ba1f4dc2135b3b9a46c6cb2b96df6bffeb8ba359
Arseniy Obolenskiy <arseniy.obolenskiy@intel.com>
2025-07-01 12:37:07 +0200

    Add `OPENVINO_CPP_VER_AT_LEAST_23` macro support and simplify filesystem includes (#31080)
    ### Details:
    - Add `OPENVINO_CPP_VER_AT_LEAST_23` macro
    - [CPU] Use correct macro for `std::to_underlying`
    - Remove filesystem workarounds for C++ version < 17
    
    ### Tickets:
     - N/A

--- src/common/util/include/openvino/util/file_path.hpp
+++ src/common/util/include/openvino/util/file_path.hpp
@@ -5,34 +5,15 @@
 #pragma once
 
 #include <cstdio>
-
-#include "openvino/util/filesystem.hpp"
+#include <filesystem>
 
 namespace ov::util {
 
-#if defined(OPENVINO_HAS_FILESYSTEM)
 // There are known issues with usage of std::filesystem::path unicode represenataion:
 // * https://gcc.gnu.org/bugzilla/show_bug.cgi?id=95048
 // * https://stackoverflow.com/questions/58521857/cross-platform-way-to-handle-stdstring-stdwstring-with-stdfilesystempath
 // Working compiler versions has been designated with godbolt.
 using Path = std::filesystem::path;
-#elif defined(OPENVINO_HAS_EXP_FILESYSTEM)
-// Known issues:
-// * error C2280: 'std::u32string std::experimental::filesystem::v1::path::u32string(void) const': attempting to
-// * filesystem error: Cannot convert character sequence: Invalid in or incomplete multibyte or wide character
-
-///
-/// @typedef Path
-/// @brief Alias for std::experimental::filesystem::path.
-///
-/// This alias is used to simplify the usage of filesystem paths.
-///
-/// @note The experimental version of std::filesystem::path may not support all features correctly.
-/// It is recommended to use this alias with caution and consider upgrading to C++17 or higher
-/// for full support of std::filesystem::path.
-///
-using Path = std::experimental::filesystem::path;
-#endif
 
 #if !defined(__GNUC__) || (__GNUC__ > 12 || __GNUC__ == 12 && __GNUC_MINOR__ >= 3)
 #    define GCC_NOT_USED_OR_VER_AT_LEAST_12_3
--- src/core/include/openvino/core/visibility.hpp
+++ src/core/include/openvino/core/visibility.hpp
@@ -87,6 +87,9 @@
 #                define OPENVINO_CPP_VER_AT_LEAST_17
 #                if __cplusplus >= 202002L
 #                    define OPENVINO_CPP_VER_AT_LEAST_20
+#                    if __cplusplus >= 202302L
+#                        define OPENVINO_CPP_VER_AT_LEAST_23
+#                    endif
 #                endif
 #            endif
 #        endif
@@ -100,6 +103,9 @@
 #                define OPENVINO_CPP_VER_AT_LEAST_17
 #                if _MSVC_LANG >= 202002L
 #                    define OPENVINO_CPP_VER_AT_LEAST_20
+#                    if _MSVC_LANG >= 202302L
+#                        define OPENVINO_CPP_VER_AT_LEAST_23
+#                    endif
 #                endif
 #            endif
 #        endif
