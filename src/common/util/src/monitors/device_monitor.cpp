def292750e92123e71fb3af26ae044c2f0b5c54a
Wang <yang4.wang@intel.com>
2025-07-09 15:45:18 +0800

    remove unnecessary shared pointer

--- src/common/util/src/monitors/device_monitor.cpp
+++ src/common/util/src/monitors/device_monitor.cpp
@@ -14,13 +14,11 @@ namespace ov {
 namespace util {
 
 std::map<std::string, float> get_device_utilization(const std::string& device_id) {
-    std::shared_ptr<ov::util::IDeviceMonitor> m_device_monitor;
-    if (device_id.empty())
-        m_device_monitor = std::make_shared<ov::util::CPUDeviceMonitor>();
-    else
-        m_device_monitor = std::make_shared<ov::util::XPUDeviceMonitor>(device_id);
     try {
-        return m_device_monitor->get_utilization();
+        if (device_id.empty())
+            ov::util::CPUDeviceMonitor().get_utilization();
+        else
+            ov::util::XPUDeviceMonitor(device_id).get_utilization();
     } catch (...) {
         // Handle exceptions and return an empty map
         return {};
