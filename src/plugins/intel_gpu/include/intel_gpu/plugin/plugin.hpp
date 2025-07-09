a869fba186d960c0c65a6b408be68abb7c969ace
Susanta Bhattacharjee <susanta.bhattacharjee@intel.com>
2025-07-09 14:53:33 +0530

    Weightless caching for onnx models
    Checked all constant nodes whether anyone has weightless cache attribute
    present. If none has, only then weightless attributes have been set.

--- src/plugins/intel_gpu/include/intel_gpu/plugin/plugin.hpp
+++ src/plugins/intel_gpu/include/intel_gpu/plugin/plugin.hpp
@@ -30,6 +30,7 @@ private:
                                                          const std::shared_ptr<RemoteContextImpl>& context) const;
     void transform_model(std::shared_ptr<ov::Model>& model, const ExecutionConfig& config, const std::shared_ptr<RemoteContextImpl>& context) const;
     void register_primitives() const;
+    bool is_weightless_cache_attributes_set(const std::shared_ptr<const ov::Model>& model) const;
     void set_weightless_cache_attributes(const std::shared_ptr<const ov::Model>& model) const;
     std::string get_device_id_from_config(const ov::AnyMap& config) const;
     std::string get_device_id(const ov::AnyMap& config) const;
