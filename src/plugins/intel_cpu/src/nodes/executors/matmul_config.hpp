f3666ad3265eb90266c5e62998d907f1ef097aeb
Egor Duplensky <egor.duplensky@gmail.com>
2025-07-11 13:01:23 +0200

    [CPU] Refactor mamtul node

--- src/plugins/intel_cpu/src/nodes/executors/matmul_config.hpp
+++ src/plugins/intel_cpu/src/nodes/executors/matmul_config.hpp
@@ -4,13 +4,29 @@
 
 #pragma once
 
+#include <cstdint>
+#include <vector>
+
 #include "executor_config.hpp"
+#include "post_ops.hpp"
 
 namespace ov::intel_cpu {
 
 struct MatMulAttrs {
-    bool transposeA;
-    bool transposeB;
+    bool transposeA = false;
+    bool transposeB = false;
+    bool withBias = false;
+    bool weightsNonTransposed = false;
+    bool sparseWeights = false;
+    uint64_t dynamicQuantizationGroupSize = 0;
+    bool nonConstantWeights = true;
+    bool fcSemantic = false;
+
+    // DQ scales for quantization
+    std::vector<float> dqScales;
+
+    // Post-operations for fused operations
+    PostOps postOps;
 };
 
 using MatMulConfig = executor::Config<MatMulAttrs>;
