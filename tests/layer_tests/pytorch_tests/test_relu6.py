0aa9d8a45b20141d7d4237ea2b82715f4dbf0a18
Sonal kumari <165447633+Hmm-1224@users.noreply.github.com>
2025-07-12 22:57:53 +0530

    Update test_relu6.py

--- tests/layer_tests/pytorch_tests/test_relu6.py
+++ tests/layer_tests/pytorch_tests/test_relu6.py
@@ -3,25 +3,19 @@ import torch
 import numpy as np
 from pytorch_layer_test_class import PytorchLayerTest
 
-class aten_quantized_relu6(torch.nn.Module):
+class aten_relu6(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.scale = 1.0
-        self.zero_point = 0
-        self.relu6 = torch.nn.quantized.ReLU6()
-
+        self.relu6 = torch.nn.ReLU6(
     def forward(self, x):
-        x_q = torch.quantize_per_tensor(x, scale=self.scale, zero_point=self.zero_point, dtype=torch.quint8)
-        out_q = self.relu6(x_q)
-        return out_q.dequantize()
+        return self.relu6(x)
 
-class TestQuantizedReLU6(PytorchLayerTest):
+class TestReLU6(PytorchLayerTest):
     def _prepare_input(self):
         data = np.random.uniform(-5.0, 10.0, size=(1, 3, 8, 8)).astype(np.float32)
-        return (torch.from_numpy(data),)
-
+        return (torch.from_numpy(data),) 
     @pytest.mark.nightly
     @pytest.mark.precommit
-    def test_quantized_relu6(self, ie_device, precision, ir_version):
-        model = aten_quantized_relu6()
-        self._test(model, None, "quantized::relu6", ie_device, precision, ir_version)
+    def test_relu6(self, ie_device, precision, ir_version):
+        model = aten_relu6()
+        self._test(model, None, "aten::relu6", ie_device, precision, ir_version)
