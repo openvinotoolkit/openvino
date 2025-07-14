86528631afd7b3713e05af609c71b5e945b55a3c
Andrii Staikov <andrii.staikov@intel.com>
2025-07-14 17:55:16 +0200

    Update log_util.hpp

--- src/core/dev_api/openvino/core/log_util.hpp
+++ src/core/dev_api/openvino/core/log_util.hpp
@@ -169,17 +169,17 @@ bool is_verbose_logging();
                                   graph_value.get_partial_shape());                                         \
         } while (0)
 
-#    define OPENVINO_LOG_NODE2(matcher, num_arguments)                                                          \
-        do {                                                                                                    \
-            bool non_empty_args___ = num_arguments != 0;                                                        \
-            OPENVINO_LOG_MATCHING(                                                                              \
-                matcher,                                                                                        \
-                ov::util::LevelString::get(),                                                                   \
-                (non_empty_args___ != 0 ? OPENVINO_BLOCK_BODY_RIGHT : OPENVINO_BLOCK_END),                      \
-                OPENVINO_GREEN,                                                                                 \
-                " NODES' TYPE AND PREDICATE MATCHED.",                                                          \
-                (non_empty_args___ != 0 ? " CHECKING " + std::to_string(num_arguments) + " PATTERN ARGUMENTS: " \
-                                        : ""));                                                                 \
+#    define OPENVINO_LOG_NODE2(matcher, num_arguments)                                                        \
+        do {                                                                                                  \
+            bool _non_empty_args = num_arguments != 0;                                                        \
+            OPENVINO_LOG_MATCHING(                                                                            \
+                matcher,                                                                                      \
+                ov::util::LevelString::get(),                                                                 \
+                (_non_empty_args != 0 ? OPENVINO_BLOCK_BODY_RIGHT : OPENVINO_BLOCK_END),                      \
+                OPENVINO_GREEN,                                                                               \
+                " NODES' TYPE AND PREDICATE MATCHED.",                                                        \
+                (_non_empty_args != 0 ? " CHECKING " + std::to_string(num_arguments) + " PATTERN ARGUMENTS: " \
+                                        : ""));                                                               \
         } while (0)
 
 #    define OPENVINO_LOG_NODE3(matcher)                         \
@@ -606,16 +606,16 @@ bool is_verbose_logging();
                                   "  NODES' TYPE MATCHED, but PREDICATE FAILED"); \
         } while (0);
 
-#    define OPENVINO_LOG_WRAPTYPE3(matcher, num_arguments)                                                         \
-        do {                                                                                                       \
-            bool non_empty_args___ = num_arguments != 0;                                                           \
-            OPENVINO_LOG_MATCHING(                                                                                 \
-                matcher,                                                                                           \
-                ov::util::LevelString::get(),                                                                      \
-                (non_empty_args___ ? OPENVINO_BLOCK_BODY_RIGHT : OPENVINO_BLOCK_END),                              \
-                OPENVINO_GREEN,                                                                                    \
-                " NODES' TYPE AND PREDICATE MATCHED.",                                                             \
-                (non_empty_args___ ? " CHECKING " + std::to_string(num_arguments) + " PATTERN ARGUMENTS: " : "")); \
+#    define OPENVINO_LOG_WRAPTYPE3(matcher, num_arguments)                                                       \
+        do {                                                                                                     \
+            bool _non_empty_args = num_arguments != 0;                                                           \
+            OPENVINO_LOG_MATCHING(                                                                               \
+                matcher,                                                                                         \
+                ov::util::LevelString::get(),                                                                    \
+                (_non_empty_args ? OPENVINO_BLOCK_BODY_RIGHT : OPENVINO_BLOCK_END),                              \
+                OPENVINO_GREEN,                                                                                  \
+                " NODES' TYPE AND PREDICATE MATCHED.",                                                           \
+                (_non_empty_args ? " CHECKING " + std::to_string(num_arguments) + " PATTERN ARGUMENTS: " : "")); \
         } while (0);
 
 #    define OPENVINO_LOG_WRAPTYPE4(matcher, status, num_arguments)                                        \
