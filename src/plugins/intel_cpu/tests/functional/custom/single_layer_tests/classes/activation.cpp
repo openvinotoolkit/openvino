db0b296ac002d9ffae87ce0fd8ab92df9436f89d
Mahdi Jafari <mahdi.jfri.79@gmail.com>
2025-07-18 10:23:48 -0400

    Do not test mish on arm32

--- src/plugins/intel_cpu/tests/functional/custom/single_layer_tests/classes/activation.cpp
+++ src/plugins/intel_cpu/tests/functional/custom/single_layer_tests/classes/activation.cpp
@@ -285,7 +285,10 @@ const std::map<utils::ActivationTypes, std::vector<std::vector<float>>>& activat
         {Ceiling,     {{}}},
         {Negative,    {{}}},
         {Swish,       {{0.1f}}},
+// On arm32 Mish is decomposed
+#if !defined(OPENVINO_ARCH_ARM)
         {Mish,        {{}}},
+#endif
 // On other platforms HSigmoid is decomposed
 #if defined(OPENVINO_ARCH_X86_64) || defined(OPENVINO_ARCH_ARM64) || defined(OPENVINO_ARCH_RISCV64)
         {HSigmoid,    {{}}},
