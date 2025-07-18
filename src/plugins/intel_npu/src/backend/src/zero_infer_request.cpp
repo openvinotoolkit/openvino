40f6d52ca669ce84cb34c347a2d9afd246bd6d80
Insoo Woo <insoo.woo@intel.com>
2025-07-18 11:43:14 -0700

    add NPU_LLVM_BACKEND
    Signed-off-by: Insoo Woo <insoo.woo@intel.com>

--- src/plugins/intel_npu/src/backend/src/zero_infer_request.cpp
+++ src/plugins/intel_npu/src/backend/src/zero_infer_request.cpp
@@ -314,6 +314,7 @@ void ZeroInferRequest::create_pipeline() {
 
     _logger.debug("ZeroInferRequest::create_pipeline - constructing pipeline");
 
+    #ifdef NPU_LLVM_BACKEND
     if (_graph->get_handle() == nullptr) {
         // Construct pipeline
         _pipeline = std::make_unique<DynamicPipeline>(_config,
@@ -323,6 +324,7 @@ void ZeroInferRequest::create_pipeline() {
                                                       _levelZeroOutputTensors,
                                                       batchSize.has_value() ? batchSize.value() : DEFAULT_BATCH_SIZE);
     } else
+    #endif
     {
     // Construct pipeline
     _pipeline = std::make_unique<Pipeline>(_config,
