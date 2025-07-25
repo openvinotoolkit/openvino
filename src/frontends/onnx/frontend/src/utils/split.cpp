da113b2c155909cbb75c15f125be87830bacf0bd
LiangGao <liang.gao@intel.com>
2025-07-25 11:40:58 +0800

    Align with onnx's implementation

--- src/frontends/onnx/frontend/src/utils/split.cpp
+++ src/frontends/onnx/frontend/src/utils/split.cpp
@@ -29,15 +29,8 @@ OutputVector make_split(const Output<ov::Node>& value, int64_t num_splits, int64
         // In ONNX definition, if the tensor is not evenly splittable the last chunk will be smaller.
         // Handle the case when axis_len is not divisible by num_splits
         if (axis_len % num_splits) {
-            auto avg_axis = axis_len / num_splits + 1;
+            auto avg_axis = (axis_len + num_splits - 1) / num_splits;  // Round up division
             auto last_output_value = axis_len % avg_axis;
-            auto total_len = avg_axis * (num_splits - 1) + last_output_value;
-            FRONT_END_GENERAL_CHECK(total_len == axis_len && last_output_value > 0,
-                                    "The split parameters are not valid. The axis length ",
-                                    axis_len,
-                                    " split into",
-                                    num_splits,
-                                    ", can not promise the last chunk will be smaller than the others.");
             std::vector<int64_t> split_lengths(num_splits, avg_axis);
             split_lengths.back() = last_output_value;
             return make_split(value, split_lengths, axis);
