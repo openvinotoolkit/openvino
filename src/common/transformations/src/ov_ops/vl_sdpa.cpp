79b1f953980e8341b597248d4485ee2db575c4f1
Cecilia Peng <cecilia.peng@intel.com>
2025-07-28 05:50:28 +0800

    strict input shapes check

--- src/common/transformations/src/ov_ops/vl_sdpa.cpp
+++ src/common/transformations/src/ov_ops/vl_sdpa.cpp
@@ -9,6 +9,7 @@
 #include "openvino/op/scaled_dot_product_attention.hpp"
 #include "ov_ops/augru_sequence.hpp"
 #include "scaled_dot_product_attention_shape_inference.hpp"
+
 namespace ov {
 namespace op {
 namespace internal {
@@ -62,6 +63,40 @@ void VLSDPA::validate_and_infer_types() {
                           "The element type of the input tensor must be a floating-point.");
 
     const auto& input_shapes = ov::util::get_node_input_partial_shapes(*this);
+
+    // validate input shapes
+    // VLSDPA node is only optimized by QWen2.x-VL model at the moment. Therefore,
+    // the strict check is applied, which could be relaxed once we see similar patterns in
+    // more models and corresponding kernel implements the function.
+    auto shape_q = input_shapes[0];
+    auto shape_k = input_shapes[1];
+    auto shape_v = input_shapes[2];
+
+    auto shape_q_rank = shape_q.rank();
+    NODE_VALIDATION_CHECK(this,
+                          shape_q_rank.is_static() && shape_q_rank.get_length() == 3,
+                          "Query input rank length must be 3.");
+    auto shape_v_rank = shape_v.rank();
+    NODE_VALIDATION_CHECK(this,
+                          shape_v_rank.is_static() && shape_v_rank.get_length() == 3,
+                          "Key input rank length must be 3.");
+    auto shape_k_rank = shape_v.rank();
+    NODE_VALIDATION_CHECK(this,
+                          shape_k_rank.is_static() && shape_k_rank.get_length() == 3,
+                          "Value input rank length must be 3.");
+
+    NODE_VALIDATION_CHECK(this,
+                          (m_order_q == m_order_k &&
+                           m_order_q == m_order_v &&
+                           m_order_q == m_order_out),
+                          "Value of m_order* must be equal.");
+
+    if (m_order_q.size() > 0) {
+        NODE_VALIDATION_CHECK(this,
+                             (m_order_q == std::vector<int64_t>{1, 0, 2}),
+                             "Value of order_q must be {1, 0, 2}.");
+    }
+
     // const auto output_shapes = shape_infer(this, input_shapes);
     // transpose shape into BHLS(4D), or HLS(3D)
     auto transpose_pshape = [](const ov::PartialShape& pshape, const std::vector<int64_t>& order) {
