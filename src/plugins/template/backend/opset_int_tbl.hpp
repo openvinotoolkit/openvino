e1ce9e0550c72d640ff4b3a88da5e23cf81de4e8
LiangGao <liang.gao@intel.com>
2025-07-28 13:55:08 +0800

    Fix unit test error

--- src/plugins/template/backend/opset_int_tbl.hpp
+++ src/plugins/template/backend/opset_int_tbl.hpp
@@ -176,6 +176,7 @@ _OPENVINO_OP_REG(BitwiseRightShift, ov::op::v15)
 _OPENVINO_OP_REG(SliceScatter, ov::op::v15)
 _OPENVINO_OP_REG(SearchSorted, ov::op::v15)
 
+_OPENVINO_OP_REG(AvgPool, ov::op::v16)
 _OPENVINO_OP_REG(Identity, ov::op::v16)
 _OPENVINO_OP_REG(ISTFT, ov::op::v16)
 _OPENVINO_OP_REG(SegmentMax, ov::op::v16)
