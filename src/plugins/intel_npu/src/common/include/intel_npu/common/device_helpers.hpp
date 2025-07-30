20a34173eb7179b0f063b409cfebe78395871761
Bogdan Pereanu <bogdan.pereanu@intel.com>
2025-07-30 16:05:46 +0300

    [NPU] Add support to import a model from a memory mapped file (#31425)
    ### Details:
     - Add support to import a model from a memory-mapped file
    - Address and size shall be multiples of the default page size (4096) to
    enable this feature
     - Plugin will export all the time a blob that is aligned to this value
    - In case of iStream, the plugin will create a tensor aligned to this
    value
    - In case of an ov::Tensor, the user shall align the buffer to this size
    if the buffer wasn't opened using mmap
     - Metadata will also be aligned to the page size
    
    ### Tickets:
     - *[CVS-168515](https://jira.devtools.intel.com/browse/CVS-168515)*
    
    ---------
    
    Signed-off-by: Bogdan Pereanu <bogdan.pereanu@intel.com>

--- src/plugins/intel_npu/src/common/include/intel_npu/common/device_helpers.hpp
+++ src/plugins/intel_npu/src/common/include/intel_npu/common/device_helpers.hpp
@@ -9,6 +9,8 @@
 
 #include "intel_npu/npu_private_properties.hpp"
 
+namespace intel_npu {
+
 namespace utils {
 bool isNPUDevice(const uint32_t deviceId);
 uint32_t getSliceIdBySwDeviceId(const uint32_t swDevId);
@@ -17,3 +19,5 @@ std::string getCompilationPlatform(const std::string_view platform,
                                    const std::string& deviceId,
                                    std::vector<std::string> availableDevicesNames);
 }  // namespace utils
+
+}  // namespace intel_npu
