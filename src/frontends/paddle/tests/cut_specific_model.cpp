4d24c003355e2dd3bfd63d85fd22f543e1e4b299
Yuan Hu <yuan2.hu@intel.com>
2025-07-31 13:02:44 +0800

    [PaddlePaddle] upgrade the paddle version to 3.0.0 (#31312)
    ### Details:
     - *update paddle version to 3.0.0*
    - *because the PIR support is blocked by
    https://github.com/PaddlePaddle/Paddle/issues/73619, so this PR only
    support paddle export model with FLAGS_enable_pir_api=0*
    
    ### Tickets:
     - *CVS-167349*
    
    ---------
    
    Signed-off-by: HU Yuan2 <yuan2.hu@intel.com>
    Co-authored-by: Yu Xu <yu.xu@intel.com>

--- src/frontends/paddle/tests/cut_specific_model.cpp
+++ src/frontends/paddle/tests/cut_specific_model.cpp
@@ -14,10 +14,17 @@ static CutModelParam getTestData_2in_2out() {
     CutModelParam res;
     res.m_frontEndName = PADDLE_FE;
     res.m_modelsPath = std::string(TEST_PADDLE_MODELS_DIRNAME);
-    res.m_modelName = "2in_2out/2in_2out.pdmodel";
+    res.m_modelName = "2in_2out/2in_2out" + std::string(TEST_PADDLE_MODEL_EXT);
     res.m_oldInputs = {"inputX1", "inputX2"};
     res.m_newInputs = {"add1.tmp_0"};
-    res.m_oldOutputs = {"save_infer_model/scale_0.tmp_0", "save_infer_model/scale_1.tmp_0"};
+    if (std::string(TEST_GEN_TAG) == "ge3") {
+        res.m_oldOutputs = {"relu3a.tmp_0/Result", "relu3b.tmp_0/Result"};
+    } else if (std::string(TEST_GEN_TAG) == "ge2") {
+        res.m_oldOutputs = {"save_infer_model/scale_0.tmp_0", "save_infer_model/scale_1.tmp_0"};
+    } else {
+        // Unexpected TEST_GEN_TAG value. This branch is reached if TEST_GEN_TAG is neither "ge2" nor "ge3".
+        throw std::runtime_error("Unsupported TEST_PADDLE_VERSION: " + std::string(TEST_GEN_TAG));
+    }
     res.m_newOutputs = {"add2.tmp_0"};
     res.m_tensorValueName = "conv2dX2.tmp_0";
     res.m_tensorValue = {1, 2, 3, 4, 5, 6, 7, 8, 9};
