e9d0e02d169cb61bea6ecf02f3b2ced494efe30c
Yury Gaydaychuk <yury.gaydaychuk@intel.com>
2025-07-31 12:49:09 +0200

    .

--- src/plugins/intel_cpu/tests/functional/custom/single_layer_tests/classes/convolution.cpp
+++ src/plugins/intel_cpu/tests/functional/custom/single_layer_tests/classes/convolution.cpp
@@ -63,7 +63,7 @@ std::string ConvolutionLayerCPUTest::getTestCaseName(const testing::TestParamInf
             result << "_" << item.first << "=" << item.second.as<std::string>();
         }
     }
-    result << std::to_string(obj.index);
+    // result << std::to_string(obj.index);
 
     return result.str();
 }
