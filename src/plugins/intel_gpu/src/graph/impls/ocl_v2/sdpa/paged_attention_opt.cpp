c60503f1ca3dbd44ee7d481c0a0cf8dceedd8ab6
Taylor Yeonbok Lee <taylor.lee@intel.com>
2025-08-02 11:23:40 +0900

    Increase parallel tasks for 2nd token phase kv_cache_update in key_by_channel for optimizing requantization

--- src/plugins/intel_gpu/src/graph/impls/ocl_v2/sdpa/paged_attention_opt.cpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl_v2/sdpa/paged_attention_opt.cpp
@@ -86,6 +86,18 @@ static size_t get_heads_per_wi(const size_t kv_group_size) {
     return 1;
 }
 
+static size_t get_num_k_head_size_partitions(const kv_cache_update_params& params) {
+    size_t head_size_partition = 1;
+    if (getenv("NUM") != nullptr)
+        return std::stoi(getenv("NUM"));
+    if (params.is_key_by_channel) {
+        if (params.conf.k_head_size % subgroup_size == 0)
+            head_size_partition = std::min(static_cast<size_t>(16),
+                                           std::max(static_cast<size_t>(1), static_cast<size_t>(params.conf.k_head_size / subgroup_size)));
+    }
+    return head_size_partition;
+}
+
 inline size_t get_generate_stage_block_size(size_t head_size) {
     auto preferred_block_size = {4, 2, 1};
     for (const auto& block_size : preferred_block_size) {
@@ -784,6 +796,7 @@ protected:
                 jit.make("IS_KEY_BY_CHANNEL", 1);
                 jit.make("ADJUSTED_K_HEAD_SIZE", desc->k_head_size);
                 jit.make("ADJUSTED_PAGED_ATTENTION_BLOCK_SIZE", paged_attention_block_size + scales_zp_size);
+                jit.AddConstant(MakeJitConstant("NUM_K_HEAD_SIZE_PARTITIONS", get_num_k_head_size_partitions(params)));
             } else {
                 jit.make("ADJUSTED_K_HEAD_SIZE", desc->k_head_size + scales_zp_size);
                 jit.make("ADJUSTED_PAGED_ATTENTION_BLOCK_SIZE", paged_attention_block_size);
@@ -846,8 +859,8 @@ protected:
             } else {
                 const auto& key_input = params.input_layouts[0];
                 const auto sequences_number = key_input.get_partial_shape()[0].get_length();
-
-                wgs.global = {static_cast<size_t>(sequences_number), heads_number, subgroup_size};
+                size_t head_size_partition = get_num_k_head_size_partitions(params);
+                wgs.global = {static_cast<size_t>(sequences_number), heads_number, subgroup_size * head_size_partition};
                 wgs.local = {1, 1, subgroup_size};
             }
 
