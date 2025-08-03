8bc2ef680ca7cc97aee8433f41db4aacee14f258
cecilia peng <cecilia.peng@intel.com>
2025-08-04 07:03:39 +0800

    Update src/plugins/intel_gpu/include/intel_gpu/runtime/utils.hpp
    Co-authored-by: Pawel Raasz <pawel.raasz@intel.com>

--- src/plugins/intel_gpu/include/intel_gpu/runtime/utils.hpp
+++ src/plugins/intel_gpu/include/intel_gpu/runtime/utils.hpp
@@ -231,14 +231,7 @@ inline std::string to_string(const T& v) {
 // Overload << operator for vectors
 template <typename T>
 std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
-    os << "[";
-    for (size_t i = 0; i < vec.size(); ++i) {
-        os << vec[i];
-        if (i != vec.size() - 1) {
-            os << ", ";
-        }
-    }
-    os << "]";
+    os << "[" << ov::util::join(vec) << "];
     return os;
 }
 
