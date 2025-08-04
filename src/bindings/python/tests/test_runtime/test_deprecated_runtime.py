e1b3dc10a190eb70608bf8159c08b07483857a87
Przemyslaw Wysocki <przemyslaw.wysocki@intel.com>
2025-08-04 16:30:15 +0200

    [PyOV] Skip some tests failing on experimental free-threaded Python (#31586)
    ### Details:
    - Python 3.13t is experimental and I don't think we should be worried
    about fixing these issues until official, full support for free threaded
    Python is out
     - This PR is needed for enablement of building 3.13t wheels on Jenkins
    
    ### Tickets:
     - 170718
    
    Signed-off-by: p-wysocki <przemyslaw.wysocki@intel.com>

--- src/bindings/python/tests/test_runtime/test_deprecated_runtime.py
+++ src/bindings/python/tests/test_runtime/test_deprecated_runtime.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from contextlib import nullcontext as does_not_raise
 import warnings
 import operator
+import sysconfig
 
 with pytest.warns(DeprecationWarning, match="The `openvino.runtime` module is deprecated and will be removed in the 2026.0 release."):
     import openvino.runtime as ov
@@ -167,6 +168,7 @@ def test_output_replace():
     assert res.input_value(0).get_node() == exp
 
 
+@pytest.mark.skipif(sysconfig.get_config_var("Py_GIL_DISABLED"), reason="Ticket: 171534")
 @pytest.mark.parametrize("share_inputs", [True, False])
 def test_infer_queue(device, share_inputs):
     jobs = 8
