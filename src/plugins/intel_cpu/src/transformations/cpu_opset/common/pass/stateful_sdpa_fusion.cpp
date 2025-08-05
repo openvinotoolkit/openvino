b3ca0d9d7c653e12ec0c573477a051ea3dec935f
Evgeny Kotov <evgeny.kotov@intel.com>
2025-08-05 20:35:21 +0200

    cleanup

--- src/plugins/intel_cpu/src/transformations/cpu_opset/common/pass/stateful_sdpa_fusion.cpp
+++ src/plugins/intel_cpu/src/transformations/cpu_opset/common/pass/stateful_sdpa_fusion.cpp
@@ -342,21 +342,15 @@ StatefulSDPAFusion::StatefulSDPAFusion() {
 bool SDPASubgraphFusion::run_on_model(const std::shared_ptr<ov::Model>& f) {
     RUN_ON_FUNCTION_SCOPE(SDPASubgraphFusion);
     
-    // Now we can safely use shared PassConfig because SymbolicOptimizations 
-    // has been fixed to not modify shared configuration
     ov::pass::SymbolicOptimizations symbolic_optimizations(false, get_pass_config());
     const auto& ctx_manager = symbolic_optimizations.get_manager();
 
-    // Register our passes
     ctx_manager->register_pass<ov::pass::SimplifyGatherShapeOf>();
     ctx_manager->register_pass<ov::pass::transpose_sinking::TSShapeOfForward>();
     ctx_manager->register_pass<StatefulSDPAFusion>();
     CPU_REGISTER_PASS_X64((*ctx_manager), ov::intel_cpu::SDPAFuseTransposeReshape);
 
-    // This should now work correctly without affecting shared PassConfig
-    symbolic_optimizations.run_on_model(f);
-    
-    return false;
+    return symbolic_optimizations.run_on_model(f);
 }
 
 }  // namespace ov::intel_cpu
