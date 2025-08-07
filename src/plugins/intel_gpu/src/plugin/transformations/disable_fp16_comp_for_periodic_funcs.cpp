da6fbd793e7f23669c75bf6a5ebd4e992be42263
Ahn, Paul Y <paul.y.ahn@intel.com>
2025-08-07 18:01:15 +0900

    follow-up code review

--- src/plugins/intel_gpu/src/plugin/transformations/disable_fp16_comp_for_periodic_funcs.cpp
+++ src/plugins/intel_gpu/src/plugin/transformations/disable_fp16_comp_for_periodic_funcs.cpp
@@ -12,7 +12,6 @@
 #include "openvino/op/sin.hpp"
 #include "openvino/op/cos.hpp"
 #include "openvino/op/reshape.hpp"
-#include "openvino/op/transpose.hpp"
 #include "openvino/op/slice.hpp"
 #include "openvino/op/strided_slice.hpp"
 #include "openvino/op/split.hpp"
@@ -23,11 +22,7 @@
 #include "openvino/op/constant.hpp"
 #include "openvino/op/convert.hpp"
 #include "openvino/op/util/broadcast_base.hpp"
-// #include "openvino/op/util/precision_sensitive_attribute.hpp"
 #include "openvino/op/variadic_split.hpp"
-#include "openvino/op/util/gather_base.hpp"
-#include "openvino/op/util/gather_nd_base.hpp"
-#include "openvino/op/identity.hpp"
 #include "openvino/op/util/binary_elementwise_arithmetic.hpp"
 #include "openvino/op/util/unary_elementwise_arithmetic.hpp"
 #include "openvino/op/mvn.hpp"
@@ -80,39 +75,34 @@ ov::intel_gpu::DisableFP16CompressionForPeriodicFuncs::DisableFP16CompressionFor
             return false;
         }
 
-        // If sin/cos propagates through a user node,
-        // it means that the sin/cos node is directly followed by a convert node from the user.
-        // Therefore, disable_fp16_compression is not set in this case.
-        for (const auto& output : node->outputs()) {
-            for (const auto& input : output.get_target_inputs()) {
-                auto next_node = input.get_node()->shared_from_this();
-                if (is_propagate_through_node(next_node)) {
-                    return false;
-                }
+        // Check if sin/cos is followed by propagate-through operations.
+        // If so, skip disable_fp16_compression because convert_precision pass will insert
+        // Convert nodes at the end of the propagate-through chain (before the first
+        // non-propagate-through node), not immediately after sin/cos.
+        // In this case, do not apply disable_fp16_compression.
+        for (auto& user : node->get_users()) {
+            if (is_propagate_through_node(user)) {
+                return false;
             }
         }
 
-        // Disable FP16 compression for the current node
+        // Disable FP16 compression for the current sin/cos node
         ov::disable_fp16_compression(node);
 
-        auto current_node = node;
-
-        OPENVINO_ASSERT(current_node && !current_node->inputs().empty(),
-                        "current_node should not be null and have inputs");
-
-        // Traverse the input chain to find the first non-propagate-through nodes
+        // Traverse the input chain to find non-propagate-through nodes
+        // and apply disable_fp16_compression to prevent fp16 precision data loss
         std::queue<std::shared_ptr<ov::Node>> nodes_to_process;
         std::unordered_set<std::shared_ptr<ov::Node>> visited;
 
-        nodes_to_process.push(current_node);
-        visited.insert(current_node);
+        nodes_to_process.push(node);
+        visited.insert(node);
 
         /*
         * Use BFS to traverse input chain and find non-propagate-through nodes
         * to apply disable_fp16_compression, preventing fp16 precision data loss.
         */
         while (!nodes_to_process.empty()) {
-            current_node = nodes_to_process.front();
+            auto current_node = nodes_to_process.front();
             nodes_to_process.pop();
 
             OPENVINO_ASSERT(current_node, "current_node should not be null");
@@ -121,7 +111,8 @@ ov::intel_gpu::DisableFP16CompressionForPeriodicFuncs::DisableFP16CompressionFor
                 continue;
             }
 
-            if ((node != current_node) && ov::fp16_compression_is_disabled(current_node)) {
+            if ((node != current_node)
+                && ov::fp16_compression_is_disabled(current_node)) {
                 continue;
             }
 
@@ -131,7 +122,7 @@ ov::intel_gpu::DisableFP16CompressionForPeriodicFuncs::DisableFP16CompressionFor
             }
 
             // For propagate-through nodes, add all input nodes to processing queue
-            for (size_t i = 0; i < current_node->inputs().size(); ++i) {
+            for (size_t i = 0; i < current_node->get_input_size(); ++i) {
                 auto input_node = current_node->get_input_node_shared_ptr(i);
                 if (input_node && visited.find(input_node) == visited.end()) {
                     nodes_to_process.push(input_node);
