openvinotoolkit/openvino

commit a38cac5ccfa2110ca570f5343cbf39c43cdf9e3e

[NPU] Dynamic batching via BatchMode::PLUGIN  (#30923)
The clone of https://github.com/openvinotoolkit/openvino/pull/30723
Ticket: 168772

### Details:
#### Dynamic batch case

Managed to infer this case:
`./benchmark_app.exe -m <resnet-v1-50.xml> -d NPU -load_config
plugin_batch_dynamic.json -shape [1..3,3,244,244] -data_shape
[2,3,244,244] `
Where _plugin_batch_dynamic.json_:
```
{
        "NPU": {"NPU_BATCH_MODE": "PLUGIN"}
} 
```
Compiled with batch resized to 1:
```
func.func @main(%arg0: tensor<1x3x244x244xui8>) -> tensor<1x1000xf32> {
```

Result:
```
[ INFO ] data  ([N,C,H,W], u8, [2,3,244,244], dyn:[1..3,3,244,244]):    random (image/numpy array is expected)

...

[WARNING] 12:13:57.69 [IGraph] Networks using dynamic batch are handled by the plugin
IGraph::determine_batch_size tensors candidateBatchSize 2
[DEBUG] 12:13:57.69 [IGraph] Batching is handled by the plugin

...

[Pipeline] Pipeline - initialize started, batch 2, _number_of_command_lists 2
[Step 4/11] Reading model files
[ INFO ] Loading model files
[ INFO ] Read model took 11.37 ms
[ INFO ] Original model I/O parameters:
[ INFO ] Network inputs:
[ INFO ]     data (node: data) : f32 / [...] / [?,3,224,224]
[ INFO ] Network outputs:
[ INFO ]     prob (node: prob) : f32 / [...] / [?,1000]
[Step 5/11] Resizing model to match image sizes and given batch
[ INFO ] Number of test configurations is calculated basing on -data_shape parameter
[ WARNING ] data: layout is not set explicitly, so it is defaulted to NCHW. It is STRONGLY recommended to set layout manually to avoid further issues.
[ INFO ] Reshaping model: 'data': [1..3,3,244,244]
[ INFO ] Reshape model took 1.84 ms
[Step 6/11] Configuring input of the model
[ INFO ] Model batch size: 2
[ INFO ] Network inputs:
[ INFO ]     data (node: data) : u8 / [N,C,H,W] / [1..3,3,244,244]
[ INFO ] Network outputs:
[ INFO ]     prob (node: prob) : f32 / [...] / [1..3,1000]

...

[Step 11/11] Dumping statistics report
[ INFO ] Execution Devices: [ NPU ]

src/plugins/intel_npu/src/backend/src/zero_pipeline.cpp
@@ -16,20 +16,29 @@
 #include "intel_npu/utils/zero/zero_types.hpp"
 
 namespace intel_npu {
-
 Pipeline::Pipeline(const Config& config,
                    const std::shared_ptr<ZeroInitStructsHolder>& init_structs,
                    const std::shared_ptr<IGraph>& graph,
                    const std::vector<std::vector<std::shared_ptr<ov::ITensor>>>& input_tensors,
-                   const std::vector<std::shared_ptr<ov::ITensor>>& output_tensors)
+                   const std::vector<std::shared_ptr<ov::ITensor>>& output_tensors,
+                   size_t batch_size)
     : _init_structs(init_structs),
       _graph(graph),
       _config(config),
       _id(_graph->get_unique_id()),
-      _number_of_command_lists(_graph->get_batch_size().has_value() ? *_graph->get_batch_size() : 1),
+      _number_of_command_lists(_graph->get_batch_size().has_value() ? *_graph->get_batch_size() : batch_size),
       _logger("Pipeline", _config.get<LOG_LEVEL>()) {
     OV_ITT_SCOPED_TASK(itt::domains::LevelZeroBackend, "Zero_infer_request::Pipeline::Pipeline");
-    _logger.debug("Pipeline - initialize started");
+    auto batch = _graph->get_batch_size().has_value() ? *_graph->get_batch_size() : batch_size;
+
+    if (_init_structs->getCommandQueueDdiTable().version() < ZE_MAKE_VERSION(1, 1) &&
+        _config.get<RUN_INFERENCES_SEQUENTIALLY>()) {
+        _graph->resize_last_submitted_event(_number_of_command_lists);
+    }
+
+    _logger.info("Pipeline - initialize started, batch %i, number_of_command_lists %i",
+                 batch,
+                 _number_of_command_lists);
 
     OPENVINO_ASSERT(_sync_output_with_fences || !_config.get<RUN_INFERENCES_SEQUENTIALLY>() ||
                         _init_structs->getCommandQueueDdiTable().version() >= ZE_MAKE_VERSION(1, 1),
@@ -79,6 +88,7 @@ Pipeline::Pipeline(const Config& config,
     }
 
     for (size_t i = 0; i < _number_of_command_lists; i++) {
+        _logger.debug("Pipeline - set args for command list number: %zu", i);
         size_t io_index = 0;
         for (const auto& desc : graph->get_input_descriptors()) {
             if (isMainInputWeightsName(desc.info.name)) {
@@ -86,7 +96,8 @@ Pipeline::Pipeline(const Config& config,
                 continue;
             }
 
-            if (input_tensors.at(io_index).size() > 1) {
+            if (io_index < input_tensors.size() && input_tensors.at(io_index).size() > 1) {
+                _logger.debug("Pipeline - set args for input index: %zu", io_index);
                 void* data = nullptr;
                 auto remote_tensor = std::dynamic_pointer_cast<ZeroRemoteTensor>(input_tensors.at(io_index).at(i));
                 if (remote_tensor == nullptr) {

src/plugins/intel_npu/src/backend/include/zero_infer_request.hpp
@@ -89,6 +89,7 @@ private:
     std::unique_ptr<Pipeline> _pipeline;
 
     bool _pipelineIsCreated = false;
+    bool _pipelineNeedsReallocation = false;
 };
 
 }  //  namespace intel_npu

src/plugins/intel_npu/src/backend/include/zero_pipeline.hpp
@@ -19,7 +19,8 @@ public:
              const std::shared_ptr<ZeroInitStructsHolder>& init_structs,
              const std::shared_ptr<IGraph>& graph,
              const std::vector<std::vector<std::shared_ptr<ov::ITensor>>>& input_tensors,
-             const std::vector<std::shared_ptr<ov::ITensor>>& output_tensors);
+             const std::vector<std::shared_ptr<ov::ITensor>>& output_tensors,
+             size_t batch_size = 1);
 
     Pipeline(const Pipeline&) = delete;
     Pipeline& operator=(const Pipeline&) = delete;

src/plugins/intel_npu/src/common/include/intel_npu/common/igraph.hpp
@@ -11,9 +11,22 @@
 #include "intel_npu/network_metadata.hpp"
 #include "intel_npu/utils/zero/zero_utils.hpp"
 #include "intel_npu/utils/zero/zero_wrappers.hpp"
+#include "openvino/runtime/itensor.hpp"
 #include "openvino/runtime/profiling_info.hpp"
+#include "openvino/runtime/so_ptr.hpp"
 
 namespace intel_npu {
+
+struct IONodeMetadata {
+    IONodeMetadata() = default;
+    IONodeMetadata(const ArgumentDescriptor& d);
+    std::optional<ArgumentDescriptor> descriptor;
+    std::optional<size_t> extract_batch(const ov::Shape& shape) const;
+    std::optional<size_t> extract_batch(const ov::Shape& shape, std::optional<ov::PartialShape> partial_shape) const;
+    std::optional<size_t> extract_batch_impl(const ov::Shape& shape,
+                                             std::optional<ov::PartialShape> partial_shape,
+                                             size_t index) const;
+};
 
 class IGraph : public std::enable_shared_from_this<IGraph> {
 public:
@@ -54,12 +67,22 @@ public:
 
     void set_last_submitted_event(const std::shared_ptr<Event>& event, size_t indexOfCommandList);
     const std::shared_ptr<Event>& get_last_submitted_event(size_t indexOfCommandList) const;
+    void resize_last_submitted_event(size_t batch);
+    void set_batch_size(std::size_t batch);
+    void reset_last_batch_size();
 
     uint32_t get_unique_id();
     void set_last_submitted_id(uint32_t id_index);
     uint32_t get_last_submitted_id() const;
 
     const std::optional<std::size_t> get_batch_size() const;
+    std::optional<size_t> determine_batch_size(const NetworkMetadata& metadata,
+                                               const std::vector<ov::SoPtr<ov::ITensor>>& input_tensors,
+                                               const IONodeMetadata& input_output_info) const;
+
+    std::optional<size_t> get_batch_size(const NetworkMetadata& metadata,
+                                         const std::vector<ov::SoPtr<ov::ITensor>>& tensors,
+                                         const IONodeMetadata& input_output_info);
 
 protected:
     /**
@@ -79,7 +102,6 @@ protected:
      * @returns The batch size deduced by the algorithm or the default value of 1 if batching cannot be performed inside
      * the plugin.
      */
-    std::optional<size_t> get_batch_size(const NetworkMetadata& metadata);
 
     ze_graph_handle_t _handle = nullptr;
     NetworkMetadata _metadata;

src/plugins/intel_npu/src/common/src/igraph.cpp
@@ -13,6 +13,39 @@ constexpr std::size_t DEFAULT_BATCH_SIZE = 1;
 
 namespace intel_npu {
 
+IONodeMetadata::IONodeMetadata(const ArgumentDescriptor& d) : descriptor(d) {}
+
+std::optional<size_t> IONodeMetadata::extract_batch_impl(const ov::Shape& shape,
+                                                         std::optional<ov::PartialShape> partial_shape,
+                                                         size_t index) const {
+    if (partial_shape.has_value()) {
+        if (partial_shape.value()[index].is_dynamic()) {
+            return shape[index];
+        }
+        return std::nullopt;
+    }
+    return shape[index];
+}
+
+std::optional<size_t> IONodeMetadata::extract_batch(const ov::Shape& shape) const {
+    return extract_batch(shape, std::nullopt);
+}
+
+std::optional<size_t> IONodeMetadata::extract_batch(const ov::Shape& shape,
+                                                    std::optional<ov::PartialShape> partial_shape) const {
+    if (descriptor.has_value()) {
+        auto nLayout = descriptor.value().info.networkLayout;
+        if (nLayout == ZE_GRAPH_ARGUMENT_LAYOUT_NCHW || nLayout == ZE_GRAPH_ARGUMENT_LAYOUT_NHWC ||
+            nLayout == ZE_GRAPH_ARGUMENT_LAYOUT_NCDHW || nLayout == ZE_GRAPH_ARGUMENT_LAYOUT_NDHWC ||
+            nLayout == ZE_GRAPH_ARGUMENT_LAYOUT_NC) {
+            return extract_batch_impl(shape, partial_shape.has_value() ? partial_shape : std::nullopt, 0);
+        } else if (nLayout == ZE_GRAPH_ARGUMENT_LAYOUT_CN) {
+            return extract_batch_impl(shape, partial_shape.has_value() ? partial_shape : std::nullopt, 1);
+        }
+    }
+    return std::nullopt;  // TODO get layout from shape
+}
+
 IGraph::IGraph(ze_graph_handle_t handle, NetworkMetadata metadata, const Config& config, std::optional<ov::Tensor> blob)
     : _handle(handle),
       _metadata(std::move(metadata)),
@@ -79,6 +112,18 @@ const std::shared_ptr<Event>& IGraph::get_last_submitted_event(size_t indexOfCom
     return _last_submitted_event[indexOfCommandList];
 }
 
+void IGraph::resize_last_submitted_event(size_t batch) {
+    _last_submitted_event.resize(batch);
+}
+
+void IGraph::set_batch_size(std::size_t batch) {
+    _batch_size = batch;
+}
+
+void IGraph::reset_last_batch_size() {
+    _batch_size.reset();
+}
+
 uint32_t IGraph::get_unique_id() {
     return _unique_id++;
 }
@@ -91,7 +136,79 @@ uint32_t IGraph::get_last_submitted_id() const {
     return _last_submitted_id;
 }
 
-std::optional<size_t> IGraph::get_batch_size(const NetworkMetadata& metadata) {
+std::optional<size_t> IGraph::determine_batch_size(const NetworkMetadata& metadata,
+                                                   const std::vector<ov::SoPtr<ov::ITensor>>& tensors,
+                                                   const IONodeMetadata& input_output_info) const {
+    if (get_batch_size() > 1) {
+        return get_batch_size();
+    }
+
+    if (tensors.empty()) {
+        return std::nullopt;  // Return std::nullopt if no input tensors are set
+    }
+
+    const auto& first_tensor = tensors.at(0);
+    if (!first_tensor) {
+        return std::nullopt;  // Return std::nullopt if the first tensor is null
+    }
+
+    const auto& first_shape = first_tensor->get_shape();
+    if (first_shape.empty()) {
+        return std::nullopt;  // Return std::nullopt if the shape is empty
+    }
+
+    if (!metadata.inputs.at(0).shapeFromIRModel.has_value()) {
+        _logger.debug("Batching on the plugin is not used, batching is handled by the compiler");
+        return std::nullopt;
+    }
+
+    // A first dimensionin shape  may be appeared 'C' as well.
+    // We need to get batch Idx and determine a true batch value here.
+    // Let's use input_output_info as a helper.
+    const ov::PartialShape& firstPartialShape = *metadata.inputs.at(0).shapeFromIRModel;
+    auto candidateBatchSizeIfExist = input_output_info.extract_batch(first_shape, firstPartialShape);
+    if (!candidateBatchSizeIfExist.has_value()) {
+        return std::nullopt;  // Return std::nullopt if there is no batch dimension
+    }
+    const size_t candidateBatchSize = candidateBatchSizeIfExist.value();
+    _logger.debug("Candidate batch size: %zu, shape: %s", candidateBatchSize, first_shape.to_string().c_str());
+    auto checkBatchSizeConsistency = [candidateBatchSize,
+                                      &input_output_info](const std::vector<ov::SoPtr<ov::ITensor>>& tensors) {
+        for (const auto& tensor : tensors) {
+            if (!tensor) {
+                return false;  // Tensor is null
+            }
+
+            const auto& shape = tensor->get_shape();
+            if (shape.empty()) {
+                return false;  // Inconsistent batch size
+            }
+
+            auto batchIfExist = input_output_info.extract_batch(shape);
+            if (!batchIfExist.has_value()) {
+                return false;  // no batch size to check
+            }
+
+            if (batchIfExist.value() != candidateBatchSize) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    if (!checkBatchSizeConsistency(tensors)) {
+        _logger.warning("Inconsistent batch sizes in input tensors");
+        return std::nullopt;  // Return std::nullopt if batch sizes are inconsistent
+    }
+
+    _logger.debug("Determined batch size: %zu", candidateBatchSize);
+
+    return candidateBatchSize;
+}
+
+std::optional<size_t> IGraph::get_batch_size(const NetworkMetadata& metadata,
+                                             const std::vector<ov::SoPtr<ov::ITensor>>& tensors,
+                                             const IONodeMetadata& input_output_info) {
     if (!metadata.outputs.at(0).shapeFromIRModel.has_value()) {
         _logger.debug("Batching on the plugin is not used, batching is handled by the compiler");
         return std::nullopt;
@@ -99,8 +216,10 @@ std::optional<size_t> IGraph::get_batch_size(const NetworkMetadata& metadata) {
 
     const ov::PartialShape& firstOutputShape = *metadata.outputs.at(0).shapeFromIRModel;
     if (firstOutputShape.is_dynamic()) {
-        _logger.warning("Networks using dynamic shapes are not supported when batching is handled by the plugin");
-        return std::nullopt;
+        _logger.debug(
+            "Networks using dynamic batch are handled by the plugin. Let's determine batch size over tensors: %zu",
+            tensors.size());
+        return !tensors.empty() ? determine_batch_size(metadata, tensors, input_output_info) : std::nullopt;
     }
     if (firstOutputShape.rank().get_length() == 0) {
         _logger.warning("Networks using rank 0 shapes for inputs/outputs are not supported when batching is "
@@ -108,7 +227,7 @@ std::optional<size_t> IGraph::get_batch_size(const NetworkMetadata& metadata) {
         return std::nullopt;
     }
 
-    const size_t candidateBatchSize = firstOutputShape[BATCH_AXIS].get_length();
+    const size_t candidateBatchSize = firstOutputShape[BATCH_AXIS].get_max_length();
     if (candidateBatchSize == 0 || candidateBatchSize == DEFAULT_BATCH_SIZE) {
         _logger.debug("Batching on the plugin is not used, batching is handled by the compiler");
         return std::nullopt;

src/plugins/intel_npu/src/compiler_adapter/src/graph.cpp
@@ -119,8 +119,11 @@ void Graph::initialize(const Config& config) {
 
         if (arg3.type == ZE_GRAPH_ARGUMENT_TYPE_INPUT) {
             _input_descriptors.push_back(ArgumentDescriptor{arg3, index});
+            _logger.debug("got pfnGetArgumentProperties3 for input: %s", _input_descriptors.back().to_string().c_str());
         } else {
             _output_descriptors.push_back(ArgumentDescriptor{arg3, index});
+            _logger.debug("got pfnGetArgumentProperties3 for output: %s",
+                          _output_descriptors.back().to_string().c_str());
         }
     }
 
@@ -162,7 +165,8 @@ void Graph::initialize(const Config& config) {
     //  _zeGraphExt->initializeGraph(). The driver will not access the original blob from this moment on, so we are
     //  releasing it here to avoid unnecessary memory usage.
     _blobIsReleased = release_blob(config);
-    _batch_size = get_batch_size(_metadata);
+
+    _batch_size = get_batch_size(_metadata, {}, {});
 
     if (_zeroInitStruct->getCommandQueueDdiTable().version() < ZE_MAKE_VERSION(1, 1) &&
         config.get<RUN_INFERENCES_SEQUENTIALLY>()) {

src/plugins/intel_npu/src/compiler_adapter/src/ze_graph_ext_wrappers.cpp
@@ -36,6 +36,11 @@
 
 #define UseCopyForNativeBinary(T) (T < ZE_GRAPH_EXT_VERSION_1_7)
 
+namespace {
+constexpr std::size_t BATCH_AXIS = 0;
+constexpr std::size_t DEFAULT_BATCH_SIZE = 1;
+}  // namespace
+
 namespace intel_npu {
 
 ZeGraphExtWrappers::ZeGraphExtWrappers(const std::shared_ptr<ZeroInitStructsHolder>& zeroInitStruct)
@@ -348,6 +353,7 @@ ze_graph_handle_t ZeGraphExtWrappers::getGraphHandle(const uint8_t& blobData, si
  */
 static IODescriptor getIODescriptor(const ze_graph_argument_properties_3_t& arg,
                                     const std::optional<ze_graph_argument_metadata_t>& metadata) {
+    auto logger = Logger::global().clone("getIODescriptor");
     ov::element::Type_t precision = zeroUtils::toOVElementType(arg.devicePrecision);
     ov::Shape shapeFromCompiler;
     ov::PartialShape shapeFromIRModel;
@@ -369,7 +375,14 @@ static IODescriptor getIODescriptor(const ze_graph_argument_properties_3_t& arg,
                 // lower bound is ignored, so we set it to 1 just to satisfy the Dimension constructor,
                 // upper bound is set to the value from shapeFromCompiler as it is filled with upper bounds
                 // in case of dynamic dimensions
-                shapeFromIRModel.push_back(ov::Dimension(1, shapeFromCompiler[id]));
+                if (id == BATCH_AXIS && shapeFromCompiler[id] == DEFAULT_BATCH_SIZE) {
+                    logger.info("Ignore dynamic batch size upper limit, but keep the dimension dynamic as a metadata "
+                                "from compiler has been lost.");
+                    // We need to kepp batch dimension dynamic
+                    shapeFromIRModel.push_back(ov::Dimension(1, dynamicDim));
+                } else {
+                    shapeFromIRModel.push_back(ov::Dimension(1, shapeFromCompiler[id]));
+                }
             }
         }
     }

commit 90d96dece5a1041a4ba1f7926d96c19fa1a50017

[NPU] Fix dynamic models regressions (#31583)
### Details:
Regressions are caused by
https://github.com/openvinotoolkit/openvino/pull/30923.

### Tickets:
 - E-177902

src/plugins/intel_npu/src/backend/src/zero_infer_request.cpp
@@ -385,32 +385,35 @@ void ZeroInferRequest::set_tensor(const ov::Output<const ov::Node>& port, const 
             _logger.debug("ZeroInferRequest::set_tensor - got the same tensor, do nothing");
             return;
         }
-        // Check if batch has been changed
-        if (get_user_input(foundPort.idx) != nullptr) {
-            _logger.debug(
-                "ZeroInferRequest::set_tensor - check if input tensors may have their sizes changed, existing: %zu and "
-                "count: %zu, new: %zu - to determine whether we need for pipeline reallocation",
-                get_user_input(foundPort.idx)->get_byte_size(),
-                get_user_inputs(foundPort.idx).size(),
-                tensor->get_byte_size());
-            if (get_user_input(foundPort.idx)->get_byte_size() * get_user_inputs(foundPort.idx).size() !=
-                tensor->get_byte_size()) {
-                _pipelineNeedsReallocation = true;
-            }
-        }
 
         std::vector<ov::SoPtr<ov::ITensor>> tensorVector = {tensor};
         _graph->reset_last_batch_size();
         auto batchSizeCandidate =
             _graph->get_batch_size(_metadata, tensorVector, _graphInputDescriptors.at(foundPort.idx));
-        if (is_batched_input(foundPort.idx) || batchSizeCandidate.has_value()) {
+
+        // Check if batch has been changed
+        if (batchSizeCandidate.has_value()) {
+            if (get_user_input(foundPort.idx) != nullptr) {
+                _logger.debug("ZeroInferRequest::set_tensor - check if input tensors may have their sizes changed, "
+                              "existing: %zu and "
+                              "count: %zu, new: %zu - to determine whether we need for pipeline reallocation",
+                              get_user_input(foundPort.idx)->get_byte_size(),
+                              get_user_inputs(foundPort.idx).size(),
+                              tensor->get_byte_size());
+                if (get_user_input(foundPort.idx)->get_byte_size() * get_user_inputs(foundPort.idx).size() !=
+                    tensor->get_byte_size()) {
+                    _pipelineNeedsReallocation = true;
+                }
+            }
+            _graph->set_batch_size(batchSizeCandidate.has_value() ? batchSizeCandidate.value() : DEFAULT_BATCH_SIZE);
+        }
+
+        if (is_batched_input(foundPort.idx)) {
             // resize vector size to 1 if set_tensor is called after set_tensors
             get_level_zero_inputs(foundPort.idx).resize(1);
             get_level_zero_inputs(foundPort.idx).shrink_to_fit();
             get_user_inputs(foundPort.idx).resize(1);
             get_user_inputs(foundPort.idx).shrink_to_fit();
-
-            _graph->set_batch_size(batchSizeCandidate.has_value() ? batchSizeCandidate.value() : DEFAULT_BATCH_SIZE);
         }
 
         get_user_input(foundPort.idx) = tensor;
@@ -838,7 +841,7 @@ void ZeroInferRequest::infer_async() {
                                 OPENVINO_THROW("Empty buffer");
                             }
 
-                            _logger.info(
+                            _logger.debug(
                                 "Batched Tensors - Tensor by index: %zu is not allocated in the current Level Zero "
                                 "context, copy bytes from user tensor: %zu, into L0 with expected size: %zu",
                                 inputIndex,
@@ -852,11 +855,11 @@ void ZeroInferRequest::infer_async() {
             } else {
                 void* levelZeroBuffer = get_level_zero_input(inputIndex)->data();
 
-                _logger.info("Batched Tensors - Tensor by index: %zu is not allocated in the current Level Zero "
-                             "context or must be "
-                             "in a continued memory space, copy into L0 with size: %zu",
-                             inputIndex,
-                             get_level_zero_input(inputIndex)->get_byte_size());
+                _logger.debug("Batched Tensors - Tensor by index: %zu is not allocated in the current Level Zero "
+                              "context or must be "
+                              "in a continued memory space, copy into L0 with size: %zu",
+                              inputIndex,
+                              get_level_zero_input(inputIndex)->get_byte_size());
                 size_t copied_bytes_from_user = 0;
                 for (size_t i = 0; i < userTensor.size(); i++) {
                     auto userBatchRemoteTensor = std::dynamic_pointer_cast<ZeroRemoteTensor>(userTensor.at(i)._ptr);

src/plugins/intel_npu/src/backend/src/zero_pipeline.cpp
@@ -36,9 +36,9 @@ Pipeline::Pipeline(const Config& config,
         _graph->resize_last_submitted_event(_number_of_command_lists);
     }
 
-    _logger.info("Pipeline - initialize started, batch %i, number_of_command_lists %i",
-                 batch,
-                 _number_of_command_lists);
+    _logger.debug("Pipeline - initialize started, batch %i, number_of_command_lists %i",
+                  batch,
+                  _number_of_command_lists);
 
     OPENVINO_ASSERT(_sync_output_with_fences || !_config.get<RUN_INFERENCES_SEQUENTIALLY>() ||
                         _init_structs->getCommandQueueDdiTable().version() >= ZE_MAKE_VERSION(1, 1),