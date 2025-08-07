1d3383fb13272d40bc097d76d22a969f77f815c6
Evgeny Kotov <evgeny.kotov@intel.com>
2025-08-07 17:11:03 +0200

    clang fixes

--- src/core/src/pattern/op/pattern.cpp
+++ src/core/src/pattern/op/pattern.cpp
@@ -627,11 +627,12 @@ op::Predicate output_index_matches(size_t expected_index) {
 op::Predicate output_index_matches(const std::vector<size_t>& expected_indices) {
     std::string indices_str = "{";
     for (size_t i = 0; i < expected_indices.size(); ++i) {
-        if (i > 0) indices_str += ", ";
+        if (i > 0)
+            indices_str += ", ";
         indices_str += std::to_string(expected_indices[i]);
     }
     indices_str += "}";
-    
+
     return op::Predicate(
         [=](const Output<Node>& output) -> bool {
             const auto output_index = output.get_index();
