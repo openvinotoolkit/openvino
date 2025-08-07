776e5044e20bf779b3a94228d7d671ae0cd9e5c8
Sonal kumari <165447633+Hmm-1224@users.noreply.github.com>
2025-08-08 00:05:53 +0530

    Update jit_eltwise_emitters.hpp

--- src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.hpp
+++ src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.hpp
@@ -242,7 +242,7 @@ private:
 /// Greater///
 class jit_greater_emitter : public jit_emitter {
 public:
-    jit_greater_emitter(jit_generator_t* host, cpu_isa_t host_isa, const element::Type exec_prc = element::f32);
+    jit_greater_emitter(jit_generator_t* host, cpu_isa_t host_isa, element::Type exec_prc = element::f32);
     jit_greater_emitter(jit_generator_t* host, cpu_isa_t host_isa, const std::shared_ptr<ov::Node>& node);
 
     size_t get_inputs_num() const override;
