73889246eb5178c55b59c75a2e56e2b6fcf5a2cc
Sonal kumari <165447633+Hmm-1224@users.noreply.github.com>
2025-08-08 00:02:46 +0530

    Update jit_eltwise_emitters.cpp

--- src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.cpp
+++ src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.cpp
@@ -822,7 +822,7 @@ void jit_greater_emitter::emit_impl(const std::vector<size_t>& in_vec_idxs,
     if (host_isa_ == ov::intel_cpu::riscv64::cpu_isa_t::gv) {
         emit_isa<ov::intel_cpu::riscv64::cpu_isa_t::gv>(in_vec_idxs, out_vec_idxs);
     } else {
-        OPENVINO_THROW("Can't create jit eltwise kernel for GREATER");
+        OV_CPU_JIT_EMITTER_THROW("Can't create jit eltwise kernel for GREATER");
     }
 }
 
