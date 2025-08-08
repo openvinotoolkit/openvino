f2e89573fb7e7a9f8e82ac7eb827e6b48ddd7fa3
Sonal kumari <165447633+Hmm-1224@users.noreply.github.com>
2025-08-08 17:13:56 +0530

    Update jit_eltwise_emitters.cpp

--- src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.cpp
+++ src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.cpp
@@ -827,6 +827,45 @@ jit_floor_mod_emitter::jit_floor_mod_emitter(jit_generator_t* host,
     prepare_table();
 }
 
+void jit_floor_mod_emitter::emit_impl(const std::vector<size_t>& in_vec_idxs,
+                                      const std::vector<size_t>& out_vec_idxs) const {
+    if (host_isa_ == ov::intel_cpu::riscv64::cpu_isa_t::gv) {
+        emit_isa<ov::intel_cpu::riscv64::cpu_isa_t::gv>(in_vec_idxs, out_vec_idxs);
+    } else {
+        OV_CPU_JIT_EMITTER_THROW("Can't create jit eltwise kernel for FLOOR_MOD");
+    }
+}
+template <cpu_isa_t isa>
+void jit_floor_mod_emitter::emit_isa(const std::vector<size_t>& in_vec_idxs,
+                                     const std::vector<size_t>& out_vec_idxs) const {
+    OV_CPU_JIT_EMITTER_ASSERT(exec_prc_ == element::f32, "JIT Floor Mod emitter supports only f32 precision");
+
+    const VReg src0 = VReg(in_vec_idxs[0]);
+    const VReg src1 = VReg(in_vec_idxs[1]);
+    const VReg dst = VReg(out_vec_idxs[0]);
+    const VReg tmp1 = VReg(aux_vec_idxs[0]);
+    const VReg tmp2 = VReg(aux_vec_idxs[1]);
+    auto fone = FReg(aux_fp_gpr_idxs[0]);
+
+    load_table_val("one", fone);
+
+    h->vfdiv_vv(tmp1, src0, src1);
+
+    h->vfcvt_x_f_v(tmp2, tmp1);
+    h->vfcvt_f_x_v(tmp2, tmp2);
+    h->vmflt_vv(mask_vreg(), tmp1, tmp2);
+    h->vfsub_vf(tmp2, tmp2, fone, VM::masked);
+    h->vfmul_vv(tmp1, tmp2, src1);
+    h->vfsub_vv(dst, src0, tmp1);
+}
+void jit_floor_mod_emitter::register_table_entries() {
+    push_arg_entry_of("one", CONST_1_F);
+}
+std::set<std::vector<element::Type>> jit_floor_mod_emitter::get_supported_precisions(
+    [[maybe_unused]] const std::shared_ptr<ov::Node>& node) {
+    return {{element::f32, element::f32}};
+}
+
 ///  Greater ///
 jit_greater_emitter::jit_greater_emitter(jit_generator_t* host, cpu_isa_t host_isa, const element::Type exec_prc)
     : jit_emitter(host, host_isa, exec_prc) {
@@ -875,7 +914,8 @@ size_t jit_greater_emitter::aux_fp_gprs_count() const {
     return 1;
 }
 
-std::set<std::vector<element::Type>> jit_greater_emitter::get_supported_precisions(const std::shared_ptr<ov::Node>& node){
+std::set<std::vector<element::Type>> jit_greater_emitter::get_supported_precisions(
+    const std::shared_ptr<ov::Node>& node) {
     return {{element::f32, element::f32}};
 }
 
@@ -889,45 +929,6 @@ size_t jit_floor_mod_emitter::aux_vecs_count() const {
 
 size_t jit_floor_mod_emitter::aux_fp_gprs_count() const {
     return 1;
-}
-
-void jit_floor_mod_emitter::emit_impl(const std::vector<size_t>& in_vec_idxs,
-                                      const std::vector<size_t>& out_vec_idxs) const {
-    if (host_isa_ == ov::intel_cpu::riscv64::cpu_isa_t::gv) {
-        emit_isa<ov::intel_cpu::riscv64::cpu_isa_t::gv>(in_vec_idxs, out_vec_idxs);
-    } else {
-        OV_CPU_JIT_EMITTER_THROW("Can't create jit eltwise kernel for FLOOR_MOD");
-    }
-}
-template <cpu_isa_t isa>
-void jit_floor_mod_emitter::emit_isa(const std::vector<size_t>& in_vec_idxs,
-                                     const std::vector<size_t>& out_vec_idxs) const {
-    OV_CPU_JIT_EMITTER_ASSERT(exec_prc_ == element::f32, "JIT Floor Mod emitter supports only f32 precision");
-
-    const VReg src0 = VReg(in_vec_idxs[0]);
-    const VReg src1 = VReg(in_vec_idxs[1]);
-    const VReg dst = VReg(out_vec_idxs[0]);
-    const VReg tmp1 = VReg(aux_vec_idxs[0]);
-    const VReg tmp2 = VReg(aux_vec_idxs[1]);
-    auto fone = FReg(aux_fp_gpr_idxs[0]);
-
-    load_table_val("one", fone);
-
-    h->vfdiv_vv(tmp1, src0, src1);
-
-    h->vfcvt_x_f_v(tmp2, tmp1);
-    h->vfcvt_f_x_v(tmp2, tmp2);
-    h->vmflt_vv(mask_vreg(), tmp1, tmp2);
-    h->vfsub_vf(tmp2, tmp2, fone, VM::masked);
-    h->vfmul_vv(tmp1, tmp2, src1);
-    h->vfsub_vv(dst, src0, tmp1);
-}
-void jit_floor_mod_emitter::register_table_entries() {
-    push_arg_entry_of("one", CONST_1_F);
-}
-std::set<std::vector<element::Type>> jit_floor_mod_emitter::get_supported_precisions(
-    [[maybe_unused]] const std::shared_ptr<ov::Node>& node) {
-    return {{element::f32, element::f32}};
 }
 
 /// GELU ERF ///
