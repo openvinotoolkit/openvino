4d5e82304de6049fbc20126e22b8ac9ae56f8fc9
liubo-intel <bo4.liu@intel.com>
2025-08-08 05:13:38 -0400

    use sperate pr for CPU convert SIMD compatibility changes

--- src/plugins/intel_cpu/src/nodes/common/cpu_convert.cpp
+++ src/plugins/intel_cpu/src/nodes/common/cpu_convert.cpp
@@ -73,7 +73,7 @@ template <>
     const auto& f16vec = gen.xmm3;
     const auto& f32vec = gen.ymm4;
 
-    gen.uni_vmovdqu(f16vec, gen.xword[src]);
+    gen.movdqu(f16vec, gen.xword[src]);
     gen.vcvtph2ps(f32vec, f16vec);
     gen.vmovups(gen.yword[dst], f32vec);
 }
@@ -85,7 +85,7 @@ template <>
 
     gen.vmovups(f32vec, gen.yword[src]);
     gen.vcvtps2ph(f16vec, f32vec, 0);
-    gen.uni_vmovdqu(gen.xword[dst], f16vec);
+    gen.movdqu(gen.xword[dst], f16vec);
 }
 
 template <typename src_t, typename dst_t>
