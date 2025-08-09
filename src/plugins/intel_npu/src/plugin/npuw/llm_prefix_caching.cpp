20e49fd4b8458ca78968faf2dc7a32d0b8f27480
intelgaoxiong <xiong.gao@intel.com>
2025-08-09 06:28:48 -0700

    Refine classes and functions.
    Signed-off-by: intelgaoxiong <xiong.gao@intel.com>

--- src/plugins/intel_npu/src/plugin/npuw/llm_prefix_caching.cpp
+++ src/plugins/intel_npu/src/plugin/npuw/llm_prefix_caching.cpp
@@ -6,58 +6,59 @@
 
 #include "llm_prefix_caching.hpp"
 
+#include "llm_infer_request.hpp"
+#include "logging.hpp"
+
 namespace ov {
 namespace npuw {
 
-void PrefixCacheManager::putBlock(const std::shared_ptr<KVBlock>& block) {
-    // Don't cache a non-full block
+void PrefixCacheManager::put_block(const std::shared_ptr<KVBlock>& block) {
+    // Do not cache incomplete blocks
     if (!block->is_full) {
         return;
     }
 
-    std::shared_ptr<KVBlock> retrieved_block;
-    if (getBlock(block->block_hash, retrieved_block)) {
-        // Block has been cached
-        updateLRU(retrieved_block);
+    std::lock_guard<std::mutex> lock(mutex);
+
+    // Check if the block is already cached
+    auto it = cache_map.find(block->block_hash);
+    if (it != cache_map.end()) {
+        update_lru(it->second);
         return;
     }
 
-    {
-        // Add block in cache
-        std::lock_guard<std::mutex> lock(mutex);
-        // Add block in cache
-        block->block_id = cache_map.size();
+    // Add block to cache
+    block->block_id = cache_map.size();
 
-        if (cache_map.size() >= max_cache_size) {
-            // evict LRU block
-            auto lru_block = lru_list.back();
-            cache_map.erase(lru_block->block_hash);
-            lru_list.pop_back();
-        }
-
-        cache_map[block->block_hash] = block;
-        lru_list.push_front(block);
+    if (cache_map.size() >= max_cache_size) {
+        // Evict the least recently used block
+        auto lru_block = lru_list.back();
+        cache_map.erase(lru_block->block_hash);
+        lru_list.pop_back();
     }
+
+    cache_map[block->block_hash] = block;
+    lru_list.push_front(block);
 }
 
-void PrefixCacheManager::updateLRU(const std::shared_ptr<KVBlock>& block) {
+void PrefixCacheManager::update_lru(const std::shared_ptr<KVBlock>& block) {
     lru_list.remove(block);
     lru_list.push_front(block);
 }
 
-bool PrefixCacheManager::getBlock(uint64_t combined_hash, std::shared_ptr<KVBlock>& out_block) {
+bool PrefixCacheManager::get_block(uint64_t combined_hash, std::shared_ptr<KVBlock>& out_block) {
     std::lock_guard<std::mutex> lock(mutex);
     auto it = cache_map.find(combined_hash);
     if (it != cache_map.end()) {
         out_block = it->second;
-        updateLRU(out_block);
+        update_lru(out_block);
         return true;
     }
 
     return false;
 }
 
-void PrefixCacheManager::printCacheStatus(bool verbose) {
+void PrefixCacheManager::print_cache_status(bool verbose) const {
     std::cout << "Cache Status:" << std::endl;
     std::cout << "Max Cache Size: " << max_cache_size << std::endl;
     std::cout << "Number of Cached Blocks: " << cache_map.size() << std::endl;
@@ -82,6 +83,42 @@ void PrefixCacheManager::printCacheStatus(bool verbose) {
         }
         std::cout << "----------------------------------------" << std::endl;
     }
+}
+
+std::vector<size_t> calculate_hashes(const ov::SoPtr<ov::ITensor>& input_ids) {
+    const char* data = reinterpret_cast<const char*>(input_ids->data());
+    const auto data_elem_size = input_ids->get_element_type().size();
+    size_t total_size = input_ids->get_shape()[INPUT_IDS_SEQ_LEN_DIM];
+
+    std::vector<size_t> prompt_hashes(total_size);
+
+    size_t prefix_hash = 0;
+    for (size_t i = 0; i < total_size; ++i) {
+        const char* token_data = reinterpret_cast<const char*>(input_ids->data()) + i * data_elem_size;
+        size_t token_hash = std::hash<std::string_view>{}(std::string_view(token_data, data_elem_size));
+        prefix_hash = prefix_hash * 31 + token_hash;
+        prompt_hashes[i] = prefix_hash;
+    }
+
+    return prompt_hashes;
+}
+
+std::unordered_map<std::string, std::string> create_output_to_input_name_mapping(
+    const std::shared_ptr<const ov::ICompiledModel>& compiled_model,
+    const std::unordered_map<std::string, ov::Output<const ov::Node>>& in_ports) {
+    std::unordered_map<std::string, std::string> input_name_map;
+    for (std::size_t i = kStartOutputKVCacheLayers; i < compiled_model->outputs().size(); ++i) {
+        const auto& output_name = compiled_model->outputs()[i].get_any_name();
+        std::string input_name = std::regex_replace(output_name, std::regex("present"), "past_key_values");
+        if (in_ports.find(input_name) == in_ports.end()) {
+            LOG_DEBUG("Input name " << input_name << " doesn't contain kv cache. Skipping.");
+            continue;
+        }
+
+        input_name_map[output_name] = input_name;
+    }
+
+    return input_name_map;
 }
 
 }  // namespace npuw
