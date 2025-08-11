4fd3d42eacd3c586e9c8bebbeff3ab70b8f5b1ab
Egor Duplenskii <egor.duplensky@gmail.com>
2025-08-11 08:46:01 +0200

    [AUTO][HETERO][TESTS] Replace 'std::tie' with structured bindings where possible (#31565)
    This both simplifies and speeds up the code.
    Assembly difference https://godbolt.org/z/bqdsf8ca3
    Local perf difference
    https://quick-bench.com/q/llroDSJ6LHtdWpK9WhKTDq5LESw

--- src/plugins/auto_batch/tests/unit/plugin_set_property_test.cpp
+++ src/plugins/auto_batch/tests/unit/plugin_set_property_test.cpp
@@ -15,10 +15,7 @@ public:
 
 public:
     static std::string getTestCaseName(testing::TestParamInfo<set_property_params> obj) {
-        ov::AnyMap properties;
-        bool throw_exception;
-
-        std::tie(properties, throw_exception) = obj.param;
+        const auto& [properties, throw_exception] = obj.param;
         std::string res = "";
         if (properties.size() > 0) {
             res += "SetProperty_";
