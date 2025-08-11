794c23f3fc72b9329647488cf43798ce618b095f
Oleg Pipikin <oleg.pipikin@intel.com>
2025-08-11 12:22:26 +0200

    Apply comments

--- src/plugins/template/backend/ops/subtract.cpp
+++ src/plugins/template/backend/ops/subtract.cpp
@@ -8,29 +8,34 @@
 #include "openvino/core/type/element_iterator.hpp"
 #include "openvino/core/type/element_type_traits.hpp"
 #include "openvino/op/subtract.hpp"
+#include "openvino/reference/convert.hpp"
 
 namespace {
 template <ov::element::Type_t ET>
-bool evaluate(const std::shared_ptr<ov::op::v1::Subtract>& op, ov::TensorVector& outputs, const ov::TensorVector& inputs) {
-    using T = typename ov::element_type_traits<ov::element::i8>::value_type;
-    auto in0_shape = inputs[0].get_shape();
-    auto in1_shape = inputs[1].get_shape();
+bool evaluate_u2(const std::shared_ptr<ov::op::v1::Subtract>& op, ov::TensorVector& outputs, const ov::TensorVector& inputs) {
+    constexpr const auto conversion_type = ov::element::i8;
+    using T = typename ov::element_type_traits<conversion_type>::value_type;
+    const auto& in0_shape = inputs[0].get_shape();
+    const auto& in1_shape = inputs[1].get_shape();
+    const auto& out_shape = outputs[0].get_shape();
 
-    auto iter0 = ov::element::iterator<ov::element::u2>(inputs[0].data());
-    auto in0_data = std::vector<int8_t>(iter0, iter0 + inputs[0].get_size());
-    ov::Tensor converted_in0(ov::element::i8, in0_shape, in0_data.data());
+    ov::Tensor converted_in0(conversion_type, in0_shape);
+    ov::reference::convert(ov::element::iterator<ov::element::u2>(inputs[0].data()),
+                           ov::element::iterator<conversion_type>(converted_in0.data()),
+                           shape_size(in0_shape));
 
-    auto iter1 = ov::element::iterator<ov::element::u2>(inputs[1].data());
-    auto in1_data = std::vector<int8_t>(iter1, iter1 + inputs[1].get_size());
-    ov::Tensor converted_in1(ov::element::i8, in1_shape, in1_data.data());
+    ov::Tensor converted_in1(conversion_type, in1_shape);
+    ov::reference::convert(ov::element::iterator<ov::element::u2>(inputs[1].data()),
+                           ov::element::iterator<conversion_type>(converted_in1.data()),
+                           shape_size(in1_shape));
 
-    ov::Tensor converted_out(ov::element::i8, outputs[0].get_shape());
+    ov::Tensor converted_out(conversion_type, out_shape);
 
     ov::reference::subtract<T>(converted_in0.data<T>(), converted_in1.data<T>(), converted_out.data<T>(), in0_shape, in1_shape, op->get_autob());
 
-    auto iter_out = ov::element::iterator<ov::element::u2>(outputs[0].data());
-    auto out_data_ptr = reinterpret_cast<int8_t*>(converted_out.data());
-    std::copy(out_data_ptr, out_data_ptr + converted_out.get_size(), iter_out);
+    ov::reference::convert(ov::element::iterator<conversion_type>(converted_out.data()),
+                           ov::element::iterator<ov::element::u2>(outputs[0].data()),
+                           shape_size(out_shape));
 
     return true;
 }
@@ -42,8 +47,8 @@ bool evaluate_node<ov::op::v1::Subtract>(std::shared_ptr<ov::Node> node,
                                     const ov::TensorVector& inputs) {
     switch (node->get_output_element_type(0)) {
     case ov::element::u2:
-        return evaluate<ov::element::u2>(ov::as_type_ptr<ov::op::v1::Subtract>(node), outputs, inputs);
+        return evaluate_u2<ov::element::u2>(ov::as_type_ptr<ov::op::v1::Subtract>(node), outputs, inputs);
     default:
-        OPENVINO_THROW("Unhandled data type ", node->get_element_type().get_type_name(), " in evaluate_node()");
+        OPENVINO_THROW("Unhandled data type ", node->get_element_type(), " in evaluate_node()");
     }
 }
