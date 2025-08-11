05897e8e440601e87b780439c12e74e4c05f50b9
sunxiaoxia2022 <xiaoxia.sun@intel.com>
2025-08-11 13:49:40 +0800

    change core_type on PTL

--- src/inference/src/dev/threading/cpu_streams_executor.cpp
+++ src/inference/src/dev/threading/cpu_streams_executor.cpp
@@ -152,9 +152,14 @@ struct CPUStreamsExecutor::Impl {
                                                             .set_max_concurrency(concurrency)
                                                             .set_max_threads_per_core(max_threads_per_core)});
             } else if (stream_type == STREAM_WITH_CORE_TYPE) {
-                const auto real_core_type = (core_type == MAIN_CORE_PROC || core_type == HYPER_THREADING_PROC)
-                                                ? custom::info::core_types().back()
-                                                : custom::info::core_types().front();
+                auto core_types = tbb::info::core_types();
+                auto real_core_type = (core_type == MAIN_CORE_PROC || core_type == HYPER_THREADING_PROC)
+                                          ? custom::info::core_types().back()
+                                          : custom::info::core_types().front();
+                // For PTL, core_types=[LPECore, Ecore, Pcore]
+                if (core_type == EFFICIENT_CORE_PROC && core_types.size() >= MIN_CORE_TYPES_FOR_PTL) {
+                    real_core_type = core_types[1];
+                }
                 _taskArena.reset(new custom::task_arena{custom::task_arena::constraints{}
                                                             .set_core_type(real_core_type)
                                                             .set_max_concurrency(concurrency)
