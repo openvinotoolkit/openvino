36752fe4e83dfcee485ff8e38c51524a8bcebec6
PiotrKrzem <piotr.krzeminski@intel.com>
2025-08-13 12:28:50 +0200

    [FIX] Shape inference

--- src/core/shape_inference/include/paged_attention_shape_inference.hpp
+++ src/core/shape_inference/include/paged_attention_shape_inference.hpp
@@ -14,7 +14,7 @@ template <class TShape, class TRShape = result_shape_t<TShape>>
 std::vector<TRShape> shape_infer(const PagedAttentionExtension* op,
                                  const std::vector<TShape>& input_shapes,
                                  const ITensorAccessor& ta = make_tensor_accessor()) {
-    NODE_VALIDATION_CHECK(op, input_shapes.size() == 14 || input_shapes.size() == 17);
+    NODE_VALIDATION_CHECK(op, input_shapes.size() == 17 || input_shapes.size() == 20);
     auto output_shapes = std::vector<TRShape>(2);
 
     // Value head_size may be not same with key
