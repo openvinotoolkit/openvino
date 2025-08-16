288cd8e22772a46d3af1da40907492d17c18af08
RudraCodesForU <rudrajyotichakraborty459@gmail.com>
2025-08-16 10:02:22 +0530

    Implemented RISCV64 JIT emitter for SoftSign operation

--- src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.hpp
+++ src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.hpp
@@ -259,6 +259,35 @@ private:
 
     void register_table_entries() override;
 };
+/// SoftSign ///
+class jit_softsign_emitter : public jit_emitter {
+public:
+    jit_softsign_emitter(ov::intel_cpu::riscv64::jit_generator_t* host,
+                         ov::intel_cpu::riscv64::cpu_isa_t host_isa,
+                         ov::element::Type exec_prc = ov::element::f32);
+
+    jit_softsign_emitter(ov::intel_cpu::riscv64::jit_generator_t* host,
+                         ov::intel_cpu::riscv64::cpu_isa_t host_isa,
+                         const std::shared_ptr<ov::Node>& node,
+                         ov::element::Type exec_prc = ov::element::f32);
+
+    size_t get_inputs_num() const override;
+    size_t aux_vecs_count() const override;
+    size_t aux_fp_gprs_count() const override;
+
+    static std::set<std::vector<element::Type>> get_supported_precisions(
+        const std::shared_ptr<ov::Node>& node = nullptr);
+
+private:
+    void emit_impl(const std::vector<size_t>& in_vec_idxs,
+                   const std::vector<size_t>& out_vec_idxs) const override;
+
+    template <ov::intel_cpu::riscv64::cpu_isa_t isa>
+    void emit_isa(const std::vector<size_t>& in_vec_idxs,
+                  const std::vector<size_t>& out_vec_idxs) const;
+
+    void register_table_entries() override;
+};
 
 /// Greater///
 class jit_greater_emitter : public jit_emitter {
--- src/plugins/intel_cpu/tests/functional/custom/single_layer_tests/classes/activation.cpp
+++ src/plugins/intel_cpu/tests/functional/custom/single_layer_tests/classes/activation.cpp
@@ -238,6 +238,7 @@ std::string ActivationLayerCPUTest::getPrimitiveType(const utils::ActivationType
             (activation_type == utils::ActivationTypes::Relu) ||
             (activation_type == utils::ActivationTypes::PReLu) ||
             (activation_type == utils::ActivationTypes::Sigmoid) ||
+            (activation_type == utils::ActivationTypes::SoftSign) ||
             (activation_type == utils::ActivationTypes::Sqrt) ||
             (activation_type == utils::ActivationTypes::Tanh))
             return "jit";
