a93f5fe892526953ceb0a6b823bb3a9b717cdc82
Arseniy Obolenskiy <arseniy.obolenskiy@intel.com>
2025-08-18 12:26:04 +0200

    [Snippets][CPU] Enable ARM64 gemm_cpu_blocking unit tests

--- src/plugins/intel_cpu/src/transformations/snippets/aarch64/pass/lowered/gemm_cpu_blocking.cpp
+++ src/plugins/intel_cpu/src/transformations/snippets/aarch64/pass/lowered/gemm_cpu_blocking.cpp
@@ -6,10 +6,16 @@
 
 #include <cassert>
 #include <cstddef>
+#include <memory>
 #include <tuple>
 
 #include "openvino/core/type.hpp"
 #include "snippets/lowered/expression.hpp"
+#include "snippets/lowered/linear_ir.hpp"
+#include "snippets/lowered/pass/brgemm_blocking.hpp"
+#include "snippets/lowered/pass/pass.hpp"
+#include "snippets/lowered/specific_loop_iter_handlers.hpp"
+#include "snippets/lowered/specific_loop_iter_types.hpp"
 #include "snippets/utils/utils.hpp"
 #include "transformations/snippets/aarch64/op/gemm_cpu.hpp"
 
@@ -30,6 +36,25 @@ std::tuple<size_t, size_t, size_t> GemmCPUBlocking::get_blocking_params(
     const size_t& k_blk = ov::snippets::utils::get_full_dim_value();
 
     return std::make_tuple(m_blk, n_blk, k_blk);
+}
+
+bool GemmCPUBlocking::DummyPass::run([[maybe_unused]] snippets::lowered::LinearIR& linear_ir,
+                                     [[maybe_unused]] snippets::lowered::LinearIR::constExprIt begin,
+                                     [[maybe_unused]] snippets::lowered::LinearIR::constExprIt end) {
+    return true;
+}
+
+std::shared_ptr<snippets::lowered::pass::PassBase> GemmCPUBlocking::DummyPass::merge(
+    const std::shared_ptr<snippets::lowered::pass::PassBase>& other) {
+    return !other || ov::is_type<DummyPass>(other) ? std::make_shared<DummyPass>() : nullptr;
+}
+
+snippets::lowered::SpecificIterationHandlers GemmCPUBlocking::get_k_loop_handlers(size_t work_amount,
+                                                                                  size_t block_size) const {
+    snippets::lowered::SpecificIterationHandlers handlers =
+        ov::snippets::lowered::pass::BrgemmBlockingBase::get_default_blocking_loop_handlers(work_amount, block_size);
+    handlers.register_pass<ov::snippets::lowered::SpecificLoopIterType::FIRST_ITER, DummyPass>();
+    return handlers;
 }
 
 }  // namespace ov::intel_cpu::pass
--- src/plugins/intel_cpu/src/transformations/snippets/aarch64/pass/lowered/gemm_cpu_blocking.hpp
+++ src/plugins/intel_cpu/src/transformations/snippets/aarch64/pass/lowered/gemm_cpu_blocking.hpp
@@ -5,6 +5,7 @@
 #pragma once
 
 #include "snippets/lowered/pass/brgemm_blocking.hpp"
+#include "snippets/lowered/specific_loop_iter_handlers.hpp"
 #include "transformations/snippets/aarch64/op/gemm_cpu.hpp"
 
 namespace ov::intel_cpu::pass {
@@ -18,7 +19,27 @@ class GemmCPUBlocking : public ov::snippets::lowered::pass::BrgemmBlocking<ov::i
 public:
     OPENVINO_RTTI("GemmCPUBlocking", "", BrgemmBlocking)
 
+    /**
+     * @interface DummyPass
+     * @brief The empty pass which is used to force insertion of first specific iteration of loop by K dimension
+     *        Note: the pass is in public section to have opportunity to validate blocking loop in tests
+     * @ingroup snippets
+     */
+    class DummyPass : public snippets::lowered::pass::RangedPass {
+    public:
+        DummyPass() = default;
+        OPENVINO_RTTI("DummyPass", "", snippets::lowered::pass::RangedPass)
+        bool run(snippets::lowered::LinearIR& linear_ir,
+                 snippets::lowered::LinearIR::constExprIt begin,
+                 snippets::lowered::LinearIR::constExprIt end) override;
+        std::shared_ptr<snippets::lowered::pass::PassBase> merge(
+            const std::shared_ptr<snippets::lowered::pass::PassBase>& other) override;
+    };
+
 private:
+    snippets::lowered::SpecificIterationHandlers get_k_loop_handlers(size_t work_amount,
+                                                                     size_t block_size) const override;
+
     std::tuple<size_t, size_t, size_t> get_blocking_params(
         const ov::snippets::lowered::ExpressionPtr& gemm_expr) const override;
 };
