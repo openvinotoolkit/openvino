646a1480d7b474b5c96c283f787f8aab97a2cee7
Yuan Hu <yuan2.hu@intel.com>
2025-08-18 16:01:10 +0800

    [paddle] disable random failed test cases (#31671)
    ### Details:
    - *some paddle test cases cause random failed, disable them to make CI
    stable*
    
    ### Tickets:
     - *CVS-130605*
     - *CVS-170348*
    
    ---------
    
    Signed-off-by: HU Yuan2 <yuan2.hu@intel.com>

--- src/frontends/paddle/tests/skip_tests_config.cpp
+++ src/frontends/paddle/tests/skip_tests_config.cpp
@@ -14,5 +14,11 @@ std::vector<std::string> disabledTestPatterns() {
         ".*FrontendLibCloseTest.*",
 #endif
         ".*testUnloadLibBeforeDeletingDependentObject.*",
+        // CVS-130605, CVS-170348
+        ".*paddle_yolo_box_uneven_wh_yolo_box_uneven_wh_pdmodel.*",
+        ".*paddle_loop_dyn_loop_dyn_pdmodel.*",
+        ".*paddle_scatter_test_1_scatter_test_1_pdmodel.*",
+        ".*paddle_top_k_.*",
+        ".*generate_proposals.*",
     };
 }
