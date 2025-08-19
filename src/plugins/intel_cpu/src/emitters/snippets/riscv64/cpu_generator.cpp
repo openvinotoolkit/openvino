d53f90d5ca958abc02b6da45a5a9c164823be494
Arseniy Obolenskiy <arseniy.obolenskiy@intel.com>
2025-08-19 07:33:05 +0200

    1

---
+++ src/plugins/intel_cpu/src/emitters/snippets/riscv64/cpu_generator.cpp
@@ -0,0 +1,200 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "cpu_generator.hpp"
+
+#include <nodes/kernels/riscv64/cpu_isa_traits.hpp>
+#include <nodes/kernels/riscv64/jit_generator.hpp>
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <set>
+#include <utility>
+#include <vector>
+
+#include "cache/multi_cache.h"
+#include "emitters/plugin/riscv64/jit_eltwise_emitters.hpp"
+#include "emitters/snippets/cpu_runtime_configurator.hpp"
+#include "jit_snippets_emitters.hpp"
+#include "jit_memory_emitters.hpp"
+#include "openvino/core/except.hpp"
+#include "openvino/core/node.hpp"
+#include "openvino/core/node_output.hpp"
+#include "openvino/core/type.hpp"
+#include "openvino/core/type/element_type.hpp"
+#include "openvino/op/add.hpp"
+#include "openvino/op/parameter.hpp"
+#include "openvino/op/result.hpp"
+#include "snippets/emitter.hpp"
+#include "snippets/generator.hpp"
+#include "snippets/lowered/expression.hpp"
+#include "snippets/op/buffer.hpp"
+#include "snippets/op/load.hpp"
+#include "snippets/op/scalar.hpp"
+#include "snippets/op/store.hpp"
+#include "snippets/op/vector_buffer.hpp"
+#include "snippets/runtime_configurator.hpp"
+#include "snippets/target_machine.hpp"
+
+namespace ov {
+
+#define CREATE_SNIPPETS_EMITTER(e_type, ...)                                                      \
+    {[this](const snippets::lowered::ExpressionPtr& expr) -> std::shared_ptr<snippets::Emitter> { \
+         return std::make_shared<e_type>(h.get(), isa, expr, ##__VA_ARGS__);                      \
+     },                                                                                           \
+     [](const std::shared_ptr<ov::Node>& n) -> std::set<std::vector<element::Type>> {             \
+         return e_type::get_supported_precisions(n);                                              \
+     }}
+
+#define CREATE_CPU_EMITTER(e_type)                                                                \
+    {[this](const snippets::lowered::ExpressionPtr& expr) -> std::shared_ptr<snippets::Emitter> { \
+         return std::make_shared<e_type>(h.get(), isa, expr->get_node());                         \
+     },                                                                                           \
+     [](const std::shared_ptr<ov::Node>& n) -> std::set<std::vector<element::Type>> {             \
+         return e_type::get_supported_precisions(n);                                              \
+     }}
+
+class jit_snippet : public ov::intel_cpu::riscv64::jit_generator_t {
+public:
+    DECLARE_CPU_JIT_AUX_FUNCTIONS(jit_snippet)
+
+    ~jit_snippet() override = default;
+
+    jit_snippet() = default;
+
+    void generate() override {}
+};
+
+namespace intel_cpu::riscv64 {
+
+CompiledSnippetCPU::CompiledSnippetCPU(std::unique_ptr<ov::intel_cpu::riscv64::jit_generator_t> h)
+    : h_compiled(std::move(h)) {
+    OPENVINO_ASSERT(h_compiled && h_compiled->jit_ker(), "Got invalid jit generator or kernel was not compiled");
+}
+
+const uint8_t* CompiledSnippetCPU::get_code() const {
+    return h_compiled->jit_ker();
+}
+
+size_t CompiledSnippetCPU::get_code_size() const {
+    return h_compiled->getSize();
+}
+
+bool CompiledSnippetCPU::empty() const {
+    return get_code_size() == 0;
+}
+
+CPUTargetMachine::CPUTargetMachine(ov::intel_cpu::riscv64::cpu_isa_t host_isa, ov::intel_cpu::MultiCacheWeakPtr cache)
+    : TargetMachine(std::make_shared<CPURuntimeConfigurator>(cache)),
+      h(new jit_snippet()),
+      isa(host_isa),
+      compiled_kernel_cache(std::move(cache)) {
+    // data movement
+    jitters[op::v0::Parameter::get_type_info_static()] = CREATE_SNIPPETS_EMITTER(jit_nop_emitter);
+    jitters[op::v0::Result::get_type_info_static()] = CREATE_SNIPPETS_EMITTER(jit_nop_emitter);
+    jitters[snippets::op::Buffer::get_type_info_static()] = CREATE_SNIPPETS_EMITTER(jit_nop_emitter);
+    jitters[snippets::op::VectorBuffer::get_type_info_static()] = CREATE_SNIPPETS_EMITTER(jit_nop_emitter);
+    jitters[snippets::op::Scalar::get_type_info_static()] = CREATE_SNIPPETS_EMITTER(jit_scalar_emitter);
+
+    // memory access
+    jitters[snippets::op::Load::get_type_info_static()] = CREATE_SNIPPETS_EMITTER(jit_load_memory_emitter);
+    jitters[snippets::op::Store::get_type_info_static()] = CREATE_SNIPPETS_EMITTER(jit_store_memory_emitter);
+
+    // binary operations
+    jitters[op::v1::Add::get_type_info_static()] = CREATE_CPU_EMITTER(ov::intel_cpu::riscv64::jit_add_emitter);
+}
+
+std::shared_ptr<snippets::TargetMachine> CPUTargetMachine::clone() const {
+    return std::make_shared<CPUTargetMachine>(isa, compiled_kernel_cache);
+}
+
+bool CPUTargetMachine::is_supported() const {
+    return ov::intel_cpu::riscv64::mayiuse(ov::intel_cpu::riscv64::gv);
+}
+
+snippets::CompiledSnippetPtr CPUTargetMachine::get_snippet() {
+    OPENVINO_ASSERT(h->create_kernel(), "Failed to create jit_kernel in get_snippet()");
+
+    const auto& result =
+        std::make_shared<CompiledSnippetCPU>(std::unique_ptr<ov::intel_cpu::riscv64::jit_generator_t>(h.release()));
+    // Note that we reset all the generated code, since it was copied into CompiledSnippetCPU
+    h = std::make_unique<jit_snippet>();
+    return result;
+}
+
+size_t CPUTargetMachine::get_lanes() const {
+    switch (isa) {
+    case ov::intel_cpu::riscv64::gv:
+        // RISC-V Vector Extension lanes depend on VLEN, assume 128-bit VLEN with 32-bit elements
+        return 4;  // 128-bit / 32-bit = 4 lanes for float32
+    default:
+        OPENVINO_THROW("unknown isa ", isa);
+    }
+}
+
+std::vector<snippets::Reg> CPUTargetMachine::get_abi_arg_regs() const {
+    // RISC-V ABI argument registers: a0-a7 (x10-x17)
+    std::vector<snippets::Reg> res;
+    for (size_t i = 0; i < 8; ++i) {
+        res.emplace_back(snippets::RegType::gpr, 10 + i);  // a0-a7 are x10-x17
+    }
+    return res;
+}
+
+std::vector<snippets::Reg> CPUTargetMachine::get_gp_reg_pool() const {
+    const auto num_gp_regs = 32;
+    std::vector<snippets::Reg> reg_pool;
+    for (size_t i = 0; i < num_gp_regs; i++) {
+        // Reserve: x0 (zero), x1 (ra), x2 (sp), x3 (gp), x4 (tp), x8 (s0/fp)
+        if (i != 0 && i != 1 && i != 2 && i != 3 && i != 4 && i != 8) {
+            reg_pool.emplace_back(snippets::RegType::gpr, i);
+        }
+    }
+    return reg_pool;
+}
+
+std::vector<snippets::Reg> CPUTargetMachine::get_vec_reg_pool() const {
+    const auto num_vec_regs = 32;  // RISC-V has 32 vector registers v0-v31
+    std::vector<snippets::Reg> reg_pool;
+    reg_pool.reserve(num_vec_regs);
+    for (int i = 0; i < num_vec_regs; i++) {
+        // v0 is typically reserved for masks, so exclude it
+        if (i != 0) {
+            reg_pool.emplace_back(snippets::RegType::vec, static_cast<size_t>(i));
+        }
+    }
+    return reg_pool;
+}
+
+ov::intel_cpu::riscv64::cpu_isa_t CPUTargetMachine::get_isa() const {
+    return isa;
+}
+
+CPUGenerator::CPUGenerator(ov::intel_cpu::riscv64::cpu_isa_t isa_, ov::intel_cpu::MultiCacheWeakPtr cache)
+    : Generator(std::make_shared<CPUTargetMachine>(isa_, std::move(cache))) {}
+CPUGenerator::CPUGenerator(const std::shared_ptr<CPUTargetMachine>& target) : Generator(target) {}
+
+std::shared_ptr<snippets::Generator> CPUGenerator::clone() const {
+    const auto& cpu_target_machine = std::dynamic_pointer_cast<CPUTargetMachine>(target);
+    OPENVINO_ASSERT(cpu_target_machine,
+                    "Failed to clone CPUGenerator: the instance contains incompatible TargetMachine type");
+    return std::make_shared<CPUGenerator>(cpu_target_machine);
+}
+
+ov::snippets::RegType CPUGenerator::get_specific_op_out_reg_type(const ov::Output<ov::Node>& out) const {
+    // For basic Add operation, use vector register
+    const auto op = out.get_node_shared_ptr();
+    if (ov::is_type<op::v1::Add>(op)) {
+        return ov::snippets::RegType::vec;
+    }
+    return ov::snippets::RegType::undefined;
+}
+
+bool CPUGenerator::uses_precompiled_kernel([[maybe_unused]] const std::shared_ptr<snippets::Emitter>& e) const {
+    return false;
+}
+
+}  // namespace intel_cpu::riscv64
+
+}  // namespace ov
---
+++ src/plugins/intel_cpu/src/emitters/snippets/riscv64/jit_memory_emitters.cpp
@@ -0,0 +1,233 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "jit_memory_emitters.hpp"
+
+#include <common/utils.hpp>
+#include <nodes/kernels/riscv64/cpu_isa_traits.hpp>
+#include <nodes/kernels/riscv64/jit_generator.hpp>
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <vector>
+
+#include "emitters/plugin/riscv64/jit_emitter.hpp"
+#include "emitters/snippets/jit_snippets_call_args.hpp"
+#include "openvino/core/type.hpp"
+#include "openvino/core/type/element_type.hpp"
+#include "snippets/lowered/expression.hpp"
+#include "snippets/lowered/expressions/buffer_expression.hpp"
+#include "snippets/lowered/loop_manager.hpp"
+#include "snippets/op/load.hpp"
+#include "snippets/op/memory_access.hpp"
+#include "snippets/op/store.hpp"
+#include "snippets/utils/utils.hpp"
+#include "utils/general_utils.h"
+#include "xbyak_riscv/xbyak_riscv.hpp"
+
+namespace ov::intel_cpu::riscv64 {
+
+using jit_generator_t = ov::intel_cpu::riscv64::jit_generator_t;
+using cpu_isa_t = ov::intel_cpu::riscv64::cpu_isa_t;
+using ExpressionPtr = ov::snippets::lowered::ExpressionPtr;
+
+jit_memory_emitter::jit_memory_emitter(jit_generator_t* h,
+                                       cpu_isa_t isa,
+                                       const ExpressionPtr& expr,
+                                       emitter_in_out_map in_out_type)
+    : jit_emitter(h, isa) {
+    in_out_type_ = in_out_type;
+
+    const auto n = expr->get_node();
+    src_prc = n->get_input_element_type(0);
+    dst_prc = n->get_output_element_type(0);
+
+    const auto& memory_access = std::dynamic_pointer_cast<ov::snippets::modifier::MemoryAccess>(expr->get_node());
+    if (in_out_type_ == emitter_in_out_map::gpr_to_vec) {
+        OV_CPU_JIT_EMITTER_ASSERT(memory_access->is_memory_access_input_port(0), "must be input port - memory access");
+        count = memory_access->get_input_count();
+        compiled_byte_offset = memory_access->get_input_offset();
+        buffer_cluster_id = get_parent_buffer_cluster_id(expr);
+    } else if (in_out_type_ == emitter_in_out_map::vec_to_gpr) {
+        OV_CPU_JIT_EMITTER_ASSERT(memory_access->is_memory_access_output_port(0),
+                                  "must be output port - memory access");
+        count = memory_access->get_output_count();
+        compiled_byte_offset = memory_access->get_output_offset();
+        buffer_cluster_id = get_consumer_buffer_cluster_id(expr);
+    } else {
+        OV_CPU_JIT_EMITTER_THROW("unsupported in_out_type");
+    }
+
+    if (ov::snippets::utils::is_dynamic_value(compiled_byte_offset)) {
+        is_offset_runtime = true;
+        // Compiled byte offset is zero to manually `add` runtime offset before operation and `sub` after to reset
+        // pointer in the register
+        compiled_byte_offset = 0;
+        OV_CPU_JIT_EMITTER_ASSERT(buffer_cluster_id != SIZE_MAX, "Incorrect buffer offset in call_args");
+    }
+}
+
+size_t jit_memory_emitter::get_parent_buffer_cluster_id(const ov::snippets::lowered::ExpressionPtr& expr) {
+    OV_CPU_JIT_EMITTER_ASSERT(expr->get_input_count() == 1, "MemoryAccess must have one parent");
+    const auto& parent_expr = expr->get_input_port_connector(0)->get_source().get_expr();
+    if (const auto buffer = ov::as_type_ptr<ov::snippets::lowered::BufferExpression>(parent_expr)) {
+        return buffer->get_cluster_id();
+    }
+    return SIZE_MAX;
+}
+
+size_t jit_memory_emitter::get_consumer_buffer_cluster_id(const ov::snippets::lowered::ExpressionPtr& expr) {
+    OV_CPU_JIT_EMITTER_ASSERT(expr->get_output_count() == 1, "MemoryAccess must have one output");
+    const auto& consumers = expr->get_output_port_connector(0)->get_consumers();
+    for (const auto& consumer : consumers) {
+        if (const auto buffer = ov::as_type_ptr<ov::snippets::lowered::BufferExpression>(consumer.get_expr())) {
+            return buffer->get_cluster_id();
+        }
+    }
+    return SIZE_MAX;
+}
+
+jit_load_memory_emitter::jit_load_memory_emitter(jit_generator_t* h, cpu_isa_t isa, const ExpressionPtr& expr)
+    : jit_memory_emitter(h, isa, expr, emitter_in_out_map::gpr_to_vec) {
+    bool is_supported_precision =
+        any_of(src_prc, ov::element::f32, ov::element::i32) && src_prc == dst_prc;
+    OV_CPU_JIT_EMITTER_ASSERT(is_supported_precision, "Unsupported precision pair.");
+
+    const auto load = ov::as_type_ptr<snippets::op::Load>(expr->get_node());
+    OV_CPU_JIT_EMITTER_ASSERT(load != nullptr, "Expects Load expression");
+    count = load->get_count();
+    byte_size = src_prc.size();
+}
+
+size_t jit_memory_emitter::aux_gprs_count() const {
+    // for runtime arguments
+    return is_offset_runtime ? 1 : 0;
+}
+
+std::vector<size_t> jit_memory_emitter::get_available_aux_gprs() const {
+    OV_CPU_JIT_EMITTER_ASSERT(IMPLICATION(is_offset_runtime, !aux_gpr_idxs.empty()),
+                              "If offset is dynamic, memory emitter need to have one aux gpr at least!");
+    auto available_aux_gprs = aux_gpr_idxs;
+    if (is_offset_runtime) {
+        available_aux_gprs.pop_back();
+    }
+    return available_aux_gprs;
+}
+
+void jit_memory_emitter::emit_code_impl(const std::vector<size_t>& in_idxs,
+                                        const std::vector<size_t>& out_idxs,
+                                        const std::vector<size_t>& pool_vec_idxs,
+                                        const std::vector<size_t>& pool_gpr_idxs) const {
+    std::vector<size_t> pool_fp_gpr_idxs; // Empty for now
+    emitter_preamble(in_idxs, out_idxs, pool_vec_idxs, pool_gpr_idxs, pool_fp_gpr_idxs);
+
+    auto reg_runtime_params = Xbyak_riscv::a0;  // First ABI parameter register
+    Xbyak_riscv::Reg aux_gpr = is_offset_runtime ? Xbyak_riscv::Reg(aux_gpr_idxs.back()) : Xbyak_riscv::zero;
+
+    Xbyak_riscv::Reg data_reg = Xbyak_riscv::zero;
+    if (in_out_type_ == emitter_in_out_map::gpr_to_vec) {
+        data_reg = Xbyak_riscv::Reg(in_idxs[0]);
+    } else if (in_out_type_ == emitter_in_out_map::vec_to_gpr) {
+        data_reg = Xbyak_riscv::Reg(out_idxs[0]);
+    } else {
+        OV_CPU_JIT_EMITTER_THROW("unsupported in_out_type");
+    }
+
+    if (is_offset_runtime) {
+        // load the runtime offset from args.buffer_offsets[buffer_cluster_id]
+        const auto offset = GET_OFF(buffer_offsets) + buffer_cluster_id * sizeof(size_t);
+        h->lw(aux_gpr, reg_runtime_params, static_cast<int32_t>(offset));
+        // bump the pointer
+        h->add(data_reg, data_reg, aux_gpr);
+    }
+
+    emit_impl(in_idxs, out_idxs);
+
+    if (is_offset_runtime) {
+        // subtract back so we leave the pointer unchanged for the caller
+        h->sub(data_reg, data_reg, aux_gpr);
+    }
+
+    emitter_postamble();
+}
+
+void jit_load_memory_emitter::emit_impl(const std::vector<size_t>& in, const std::vector<size_t>& out) const {
+    if (host_isa_ == ov::intel_cpu::riscv64::gv) {
+        emit_isa<ov::intel_cpu::riscv64::gv>(in, out);
+    } else {
+        OV_CPU_JIT_EMITTER_THROW("Doesn't support isa ", host_isa_);
+    }
+}
+
+template <cpu_isa_t isa>
+void jit_load_memory_emitter::emit_isa(const std::vector<size_t>& in, const std::vector<size_t>& out) const {
+    auto src = Xbyak_riscv::Reg(in[0]);
+    auto dst = Xbyak_riscv::VReg(out[0]);
+
+    // Set vector configuration for the load
+    h->vsetivli(Xbyak_riscv::zero, count, 
+               byte_size == 4 ? Xbyak_riscv::SEW::e32 : Xbyak_riscv::SEW::e32, 
+               Xbyak_riscv::LMUL::m1);
+    
+    // Load vector data from memory
+    if (compiled_byte_offset == 0) {
+        h->vle32_v(dst, src);  // Vector load for 32-bit elements
+    } else {
+        // Use temporary register to calculate address with offset
+        auto tmp_gpr = Xbyak_riscv::Reg(aux_gpr_idxs.empty() ? Xbyak_riscv::t0.getIdx() : aux_gpr_idxs[0]);
+        h->addi(tmp_gpr, src, static_cast<int32_t>(compiled_byte_offset));
+        h->vle32_v(dst, tmp_gpr);
+    }
+}
+
+void jit_load_memory_emitter::emit_data() const {
+    // No additional data emission needed for basic load
+}
+
+jit_store_memory_emitter::jit_store_memory_emitter(jit_generator_t* h, cpu_isa_t isa, const ExpressionPtr& expr)
+    : jit_memory_emitter(h, isa, expr, emitter_in_out_map::vec_to_gpr) {
+    bool is_supported_precision =
+        any_of(dst_prc, ov::element::f32, ov::element::i32) && src_prc == dst_prc;
+    OV_CPU_JIT_EMITTER_ASSERT(is_supported_precision, "Unsupported precision pair.");
+
+    const auto store = ov::as_type_ptr<snippets::op::Store>(expr->get_node());
+    OV_CPU_JIT_EMITTER_ASSERT(store != nullptr, "Expects Store expression");
+    count = store->get_count();
+    byte_size = dst_prc.size();
+}
+
+void jit_store_memory_emitter::emit_impl(const std::vector<size_t>& in, const std::vector<size_t>& out) const {
+    if (host_isa_ == ov::intel_cpu::riscv64::gv) {
+        emit_isa<ov::intel_cpu::riscv64::gv>(in, out);
+    } else {
+        OV_CPU_JIT_EMITTER_THROW("Doesn't support isa ", host_isa_);
+    }
+}
+
+template <cpu_isa_t isa>
+void jit_store_memory_emitter::emit_isa(const std::vector<size_t>& in, const std::vector<size_t>& out) const {
+    auto src = Xbyak_riscv::VReg(in[0]);
+    auto dst = Xbyak_riscv::Reg(out[0]);
+
+    // Set vector configuration for the store
+    h->vsetivli(Xbyak_riscv::zero, count, 
+               byte_size == 4 ? Xbyak_riscv::SEW::e32 : Xbyak_riscv::SEW::e32, 
+               Xbyak_riscv::LMUL::m1);
+    
+    // Store vector data to memory
+    if (compiled_byte_offset == 0) {
+        h->vse32_v(src, dst);  // Vector store for 32-bit elements
+    } else {
+        // Use temporary register to calculate address with offset
+        auto tmp_gpr = Xbyak_riscv::Reg(aux_gpr_idxs.empty() ? Xbyak_riscv::t0.getIdx() : aux_gpr_idxs[0]);
+        h->addi(tmp_gpr, dst, static_cast<int32_t>(compiled_byte_offset));
+        h->vse32_v(src, tmp_gpr);
+    }
+}
+
+void jit_store_memory_emitter::emit_data() const {
+    // No additional data emission needed for basic store
+}
+
+}  // namespace ov::intel_cpu::riscv64
