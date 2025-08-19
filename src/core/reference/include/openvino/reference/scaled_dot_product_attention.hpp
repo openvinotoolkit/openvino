f30532ebf70ce1abcd5904a2e4398554ae57f65f
mitruska <katarzyna.mitrus@intel.com>
2025-08-19 15:48:54 +0000

    Reference impl sink in update

--- src/core/reference/include/openvino/reference/scaled_dot_product_attention.hpp
+++ src/core/reference/include/openvino/reference/scaled_dot_product_attention.hpp
@@ -7,8 +7,10 @@
 
 #include "openvino/core/shape.hpp"
 #include "openvino/reference/add.hpp"
+#include "openvino/reference/concat.hpp"
 #include "openvino/reference/matmul.hpp"
 #include "openvino/reference/multiply.hpp"
+#include "openvino/reference/slice.hpp"
 #include "openvino/reference/softmax.hpp"
 namespace ov::reference {
 namespace helpers {
@@ -41,6 +43,15 @@ std::vector<T> create_attention_mask_from_ov_boolean(const char* mask_bool, cons
 
     return mask_data;
 }
+
+void slice_last_dimension(const char* input, char* output, const Shape& input_shape, size_t element_size) {
+    std::vector<int64_t> start_indices(input_shape.size(), 0);
+    std::vector<int64_t> steps(input_shape.size(), 1);
+    Shape output_shape = input_shape;
+    output_shape.back() -= 1;
+    ov::reference::slice(input, input_shape, output, output_shape, element_size, start_indices, steps, {-1});
+}
+
 }  // namespace helpers
 }  // namespace ov::reference
 
@@ -52,12 +63,14 @@ void scaled_dot_product_attention(const T* query,
                                   const T* value,
                                   const TMask* mask,
                                   const T* scale,
+                                  const T* sink,
                                   T* output,
                                   bool is_causal,
                                   const Shape& query_shape,
                                   const Shape& key_shape,
                                   const Shape& value_shape,
                                   const Shape& mask_shape,
+                                  const Shape& sink_shape,
                                   const Shape& output_shape) {
     static_assert(std::is_same_v<T, TMask> || std::is_same_v<TMask, char>,
                   "T and TMask must be either the same type, or the TMask must be char(ov::element::boolean)");
@@ -109,10 +122,32 @@ void scaled_dot_product_attention(const T* query,
                               ov::op::AutoBroadcastType::NUMPY);
     }
 
+
+    auto gk_softmax_shape = qk_shape;
+    if (sink) {
+        gk_softmax_shape[gk_softmax_shape.size() - 1] += sink_shape[sink_shape.size() - 1];
+        std::vector<T> qk_data_with_sink(qk_data.size() + shape_size(sink_shape), 0);
+        ov::reference::concat({reinterpret_cast<const char*>(qk_data.data()), reinterpret_cast<const char*>(sink)},
+                              reinterpret_cast<char*>(qk_data_with_sink.data()),
+                              {qk_shape, sink_shape},
+                              gk_softmax_shape,
+                              qk_shape.size() - 1,
+                              sizeof(T));
+        qk_data = qk_data_with_sink;
+    }
     std::vector<T> qk_data_softmax(qk_data.size(), 0);
-    ov::reference::softmax<T>(qk_data.data(), qk_data_softmax.data(), qk_shape, ov::AxisSet{qk_shape.size() - 1});
+    ov::reference::softmax<T>(qk_data.data(),
+                              qk_data_softmax.data(),
+                              gk_softmax_shape,
+                              ov::AxisSet{gk_softmax_shape.size() - 1});
+
+    if (sink) {
+        ov::reference::helpers::slice_last_dimension(reinterpret_cast<const char*>(qk_data_softmax.data()),
+                                                     reinterpret_cast<char*>(qk_data_softmax.data()),
+                                                     gk_softmax_shape,
+                                                     sizeof(T));
+    }
     ov::reference::matmul<T>(qk_data_softmax.data(), value, output, qk_shape, value_shape, output_shape, false, false);
 }
-
 }  // namespace reference
 }  // namespace ov
--- src/plugins/template/backend/ops/scaled_dot_product_attention.cpp
+++ src/plugins/template/backend/ops/scaled_dot_product_attention.cpp
@@ -18,6 +18,8 @@ bool evaluate(const std::shared_ptr<ov::op::v13::ScaledDotProductAttention>& op,
     const TMask* mask = inputs.size() == 4 ? inputs[3].data<const TMask>() : nullptr;
     const T* scale = inputs.size() == 5 ? inputs[4].data<const T>() : nullptr;
     auto mask_shape = inputs.size() == 4 ? inputs[3].get_shape() : ov::Shape{};
+    const T* sink = inputs.size() == 6 ? inputs[5].data<const T>() : nullptr;
+    auto sink_shape = inputs.size() == 6 ? inputs[5].get_shape() : ov::Shape{};
 
     // Hack below is needed to support dynamic shapes in the reference implementation...
     const auto input_shapes = ov::util::get_tensors_partial_shapes(inputs);
@@ -31,12 +33,14 @@ bool evaluate(const std::shared_ptr<ov::op::v13::ScaledDotProductAttention>& op,
                                                           inputs[2].data<const T>(),
                                                           mask,
                                                           scale,
+                                                          sink,
                                                           outputs[0].data<T>(),
                                                           op->get_causal(),
                                                           inputs[0].get_shape(),
                                                           inputs[1].get_shape(),
                                                           inputs[2].get_shape(),
                                                           mask_shape,
+                                                          sink_shape,
                                                           outputs[0].get_shape());
 
     return true;
