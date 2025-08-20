aa32f14ff008572a0cc95f0b2ec34f890d86b60f
ethan <ethan.yang@intel.com>
2025-08-20 08:54:22 -0700

    add complex support to unsqueeze

--- src/frontends/pytorch/src/op_table.cpp
+++ src/frontends/pytorch/src/op_table.cpp
@@ -274,6 +274,7 @@ OP_CONVERTER(translate_upsample_nearest1d);
 OP_CONVERTER(translate_upsample_nearest2d);
 OP_CONVERTER(translate_upsample_nearest3d);
 OP_CONVERTER(translate_upsample_trilinear3d);
+OP_CONVERTER(translate_unsqueeze);
 OP_CONVERTER(translate_var);
 OP_CONVERTER(translate_var_mean);
 OP_CONVERTER(translate_view_as_complex);
@@ -748,7 +749,7 @@ const std::unordered_map<std::string, CreatorFunction> get_supported_ops_ts() {
         {"aten::unflatten", op::translate_unflatten},
         {"aten::unfold", op::translate_unfold},
         // aten::unsafe_chunk - Supported in limited set of patterns
-        {"aten::unsqueeze", op::quantizable_op<op::translate_1to1_match_2_inputs<opset10::Unsqueeze>>},
+        {"aten::unsqueeze", op::translate_unsqueeze},
         {"aten::upsample_bicubic2d", op::translate_upsample_bicubic2d},
         {"aten::upsample_bilinear2d", op::translate_upsample_bilinear2d},
         {"aten::upsample_linear1d", op::translate_upsample_linear1d},
@@ -1071,8 +1072,8 @@ const std::unordered_map<std::string, CreatorFunction> get_supported_ops_fx() {
         {"aten.triu.default", op::translate_triu},
         {"aten.unbind.int", op::translate_unbind_int_fx},
         {"aten.unfold.default", op::translate_unfold},
-        {"aten.unsqueeze.default", op::translate_1to1_match_2_inputs<opset10::Unsqueeze>},
-        {"aten.unsqueeze_copy.default", op::translate_1to1_match_2_inputs<opset10::Unsqueeze>},
+        {"aten.unsqueeze.default", op::translate_unsqueeze},
+        {"aten.unsqueeze_copy.default", op::translate_unsqueeze},
         {"aten.upsample_bicubic2d.vec", op::translate_upsample_bicubic2d},
         {"aten.upsample_bilinear2d.vec", op::translate_upsample_bilinear2d},
         {"aten.upsample_linear1d.vec", op::translate_upsample_linear1d},
--- tests/layer_tests/pytorch_tests/test_unsqueeze.py
+++ tests/layer_tests/pytorch_tests/test_unsqueeze.py
@@ -71,3 +71,44 @@ class TestUnsqueezeCopy(PytorchLayerTest):
     @pytest.mark.precommit_fx_backend
     def test_unsqueeze_copy(self, dim, ie_device, precision, ir_version):
         self._test(*self.create_model(dim), ie_device, precision, ir_version)
+        
+class TestUnsqueezeWithComplex(PytorchLayerTest):
+    def _prepare_input(self):
+        import numpy as np
+        return (np.random.randn(5, 10).astype(np.float32),)
+
+    def create_model(self, inplace=False, dim=0):
+        import torch
+
+        class aten_unsqueeze(torch.nn.Module):
+            def __init__(self, dim):
+                super(aten_unsqueeze, self).__init__()
+                self.op = torch.unsqueeze
+                self.dim = dim
+
+            def forward(self, x):
+                x = torch.view_as_complex(x)
+                return x, self.op(x, self.dim)
+
+        class aten_unsqueeze_(torch.nn.Module):
+            def __init__(self, dim):
+                super(aten_unsqueeze_, self).__init__()
+                self.dim = dim
+
+            def forward(self, x):
+                x = torch.view_as_complex(x)
+                return x, x.unsqueeze_(self.dim)
+
+        ref_net = None
+        model_class, op = (aten_unsqueeze, "aten::unsqueeze") if not inplace else (aten_unsqueeze_, "aten::unsqueeze_")
+
+        return model_class(dim), ref_net, op
+
+    @pytest.mark.parametrize("inplace", [False, skip_if_export(True)])
+    @pytest.mark.parametrize("dim", [0, 1, -1])
+    @pytest.mark.nightly
+    @pytest.mark.precommit
+    @pytest.mark.precommit_torch_export
+    @pytest.mark.precommit_fx_backend
+    def test_unsqueeze(self, inplace, dim, ie_device, precision, ir_version):
+        self._test(*self.create_model(inplace, dim), ie_device, precision, ir_version)
