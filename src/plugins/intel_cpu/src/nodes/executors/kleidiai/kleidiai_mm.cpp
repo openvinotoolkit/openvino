7e1f7b6e98579446f841ee36a9f0728a526a07e2
Ashwin.Sekhar <Ashwin.Sekhar@fujitsu.com>
2025-08-20 11:01:00 +0530

    Int4 inference enablement with Kleidiai kernel for ARM

--- src/plugins/intel_cpu/src/nodes/executors/kleidiai/kleidiai_mm.cpp
+++ src/plugins/intel_cpu/src/nodes/executors/kleidiai/kleidiai_mm.cpp
@@ -54,7 +54,7 @@ static bool useDynamicQuantizationImpl(const FCAttrs& attrs, const MemoryDescPtr
         return false;
     }
 
-    return weightDesc->getPrecision() == element::i8;
+    return weightDesc->getPrecision() == element::i8 || weightDesc->getPrecision() == element::i4;
 }
 
 bool MatMulKleidiAIExecutor::supports(const FCConfig& config) {
@@ -126,7 +126,7 @@ MatMulKleidiAIExecutor::MatMulKleidiAIExecutor(const FCAttrs& attrs,
     } else {
         ukernel_i8 = hasInt8MMSupport() ? &ukernel_i8_imm : &ukernel_i8_dotprod;
         MemoryPtr weightsMemory = memory.at(ARG_WEI);
-        if (!attrs.weightsNonTransposed) {
+        if (!attrs.weightsNonTransposed && weightsMemory->getDescPtr()->getPrecision() == element::i8) {
             auto dnnlSrcDesc = MemoryDescUtils::convertToDnnlMemoryDesc(originalWeightsDesc);
             auto dnnlDstDesc = acl_fc_executor::makeTransposedWeightDescriptor(dnnlSrcDesc, dnnlSrcDesc);
             weightsMemory = acl_fc_executor::reorderData(dnnlSrcDesc, dnnlDstDesc, memory.at(ARG_WEI), context);
@@ -136,31 +136,69 @@ MatMulKleidiAIExecutor::MatMulKleidiAIExecutor(const FCAttrs& attrs,
         nr = ukernel_i8->get_nr();
         kr = ukernel_i8->get_kr();
         sr = ukernel_i8->get_sr();
+        if (weightsMemory->getDescPtr()->getPrecision() == element::i4) {
+            isInt4 = true;
+            mr = ukernel_i4.get_mr();
+            nr = ukernel_i4.get_nr();
+            kr = ukernel_i4.get_kr();
+            sr = ukernel_i4.get_sr();
+        } else {
+            mr = ukernel_i8.get_mr();
+            nr = ukernel_i8.get_nr();
+            kr = ukernel_i8.get_kr();
+            sr = ukernel_i8.get_sr();
+        }
 
         auto* bias = biasMem->getDataAs<float>();
-        auto* rhs_native_qs8cx = weightsMemory->getDataAs<int8_t>();
+        // native weights memory is in s8 or s4
+        auto* rhs_native_qcx = weightsMemory->getDataAs<int8_t>();
         float* rhs_scales = static_cast<float*>(memory.at(ARG_WEI | ARG_ATTR_SCALES)->getData());
 
-        const size_t rhsPackedSize = kai_get_rhs_packed_size_rhs_pack_kxn_qsi8cxp_qsi8cx_neon(N, K, nr, kr, sr);
-        auto rhsPackedDesc = std::make_shared<CpuBlockedMemoryDesc>(i8, Shape({rhsPackedSize}));
-        rhsPackedMem = std::make_shared<Memory>(context->getEngine(), rhsPackedDesc);
-        auto* rhs_packed_qs8cx = static_cast<int8_t*>(rhsPackedMem->getData());
+        if (weightsMemory->getDescPtr()->getPrecision() == element::i4) {
+            const size_t rhsPackedSize = kai_get_rhs_packed_size_rhs_pack_nxk_qsi4cxp_qs4cxs1s0(N, K, nr, kr, sr);
+            auto rhsPackedDesc = std::make_shared<CpuBlockedMemoryDesc>(i8, Shape({rhsPackedSize}));
+            rhsPackedMem = std::make_shared<Memory>(context->getEngine(), rhsPackedDesc);
+            auto* rhs_packed_qs4cx = static_cast<int8_t*>(rhsPackedMem->getData());
 
-        kai_rhs_pack_qsi8cx_params params{};
-        params.lhs_zero_point = 1;
+            kai_rhs_pack_nxk_qsi4cxp_qs4cxs1s0_params params{};
+            params.lhs_zero_point = 1;
+            params.rhs_zero_point = 0;
 
-        kai_run_rhs_pack_kxn_qsi8cxp_qsi8cx_neon(1,
-                                                 N,
-                                                 K,
-                                                 nr,
-                                                 kr,
-                                                 sr,
-                                                 rhs_native_qs8cx,
-                                                 bias,
-                                                 rhs_scales,
-                                                 rhs_packed_qs8cx,
-                                                 0,
-                                                 &params);
+            kai_run_rhs_pack_nxk_qsi4cxp_qs4cxs1s0(1,
+                                                   N,
+                                                   K,
+                                                   nr,
+                                                   kr,
+                                                   sr,
+                                                   (const uint8_t*)(rhs_native_qcx),
+                                                   NULL,
+                                                   (const float*)(rhs_scales),
+                                                   rhs_packed_qs4cx,
+                                                   0,
+                                                   &params);
+
+        } else {
+            const size_t rhsPackedSize = kai_get_rhs_packed_size_rhs_pack_kxn_qsi8cxp_qsi8cx_neon(N, K, nr, kr, sr);
+            auto rhsPackedDesc = std::make_shared<CpuBlockedMemoryDesc>(i8, Shape({rhsPackedSize}));
+            rhsPackedMem = std::make_shared<Memory>(context->getEngine(), rhsPackedDesc);
+            auto* rhs_packed_qs8cx = static_cast<int8_t*>(rhsPackedMem->getData());
+
+            kai_rhs_pack_qsi8cx_params params{};
+            params.lhs_zero_point = 1;
+
+            kai_run_rhs_pack_kxn_qsi8cxp_qsi8cx_neon(1,
+                                                     N,
+                                                     K,
+                                                     nr,
+                                                     kr,
+                                                     sr,
+                                                     rhs_native_qcx,
+                                                     bias,
+                                                     rhs_scales,
+                                                     rhs_packed_qs8cx,
+                                                     0,
+                                                     &params);
+        }
 
         // Create scratchpad to initialize memory for LHS in update()
         scratchPad = context->getScratchPad();
@@ -238,8 +276,9 @@ void MatMulKleidiAIExecutor::execute(const MemoryArgs& memory) {
         });
     } else {
         // Create packed LHS and RHS
-        auto* lhs_packed_qa8dx = lhsPackedMem->getDataAs<int8_t>();
-        auto* rhs_packed_qs8cx = rhsPackedMem->getDataAs<int8_t>();
+        if (isInt4) {
+            auto* lhs_packed_qa8dx = lhsPackedMem->getDataAs<int8_t>();
+            auto* rhs_packed_qs4cx = rhsPackedMem->getDataAs<int8_t>();
 
         constexpr size_t m_step = BLOCK_SIZE_M_INT8;
         constexpr size_t n_step = 4;
@@ -282,6 +321,86 @@ void MatMulKleidiAIExecutor::execute(const MemoryArgs& memory) {
                                        FLOAT_MAX);
             });
         });
+            constexpr size_t m_step = BLOCK_SIZE;
+            constexpr size_t n_step = 8;
+            const size_t M_BLOCKS = (M + m_step - 1) / m_step;
+            const size_t N_BLOCKS = (N + n_step - 1) / n_step;
+
+            const size_t lhs_packed_offset = ukernel_i4.get_lhs_packed_offset(0, K);
+
+            parallel_for(M_BLOCKS, [&](size_t m_blk) {
+                const size_t M_iter = std::min(M - m_blk * m_step, m_step);
+                auto* lhs_packed_qa8dx_B = lhs_packed_qa8dx + m_blk * packedlhs_block_in_bytes;
+
+                kai_run_lhs_quant_pack_qai8dxp_f32(M_iter,
+                                                   K,
+                                                   mr,
+                                                   kr,
+                                                   sr,
+                                                   0,
+                                                   lhs + m_blk * m_step * K,  // LHS (F32)
+                                                   lhs_stride,
+                                                   lhs_packed_qa8dx_B  // lhs packed output
+                );
+                parallel_for(N_BLOCKS, [&](size_t n_blk) {
+                    //  matmul exec
+                    const size_t rhs_packed_offset = ukernel_i4.get_rhs_packed_offset(n_blk * n_step, K);
+                    const size_t dst_offset = ukernel_i4.get_dst_offset(m_blk * m_step, n_blk * n_step, dst_stride_row);
+                    const void* rhs_ptr = static_cast<const void*>(rhs_packed_qs4cx + rhs_packed_offset);
+                    const auto* lhs_ptr = static_cast<const void*>(lhs_packed_qa8dx_B + lhs_packed_offset);
+                    float* dst_ptr = (dst + dst_offset / sizeof(float));
+                    const size_t N_iter = std::min(N - n_blk * n_step, n_step);
+                    ukernel_i4.run_matmul(M_iter,
+                                          N_iter,
+                                          K,
+                                          lhs_ptr,
+                                          rhs_ptr,
+                                          dst_ptr,
+                                          dst_stride_row,
+                                          dst_stride_col,
+                                          FLOAT_MIN,
+                                          FLOAT_MAX);
+                });
+            });
+
+        } else {
+            auto* lhs_packed_qa8dx = lhsPackedMem->getDataAs<int8_t>();
+            auto* rhs_packed_qs8cx = rhsPackedMem->getDataAs<int8_t>();
+
+            kai_run_lhs_quant_pack_qai8dxp_f32(M,
+                                               K,  // Dimensions
+                                               mr,
+                                               kr,
+                                               sr,
+                                               0,                // Packing dimensions
+                                               lhs,              // LHS (F32)
+                                               lhs_stride,       // LHS stride
+                                               lhs_packed_qa8dx  // LHS packed
+            );
+
+            const size_t lhs_packed_offset = ukernel_i8.get_lhs_packed_offset(0, K);
+            const auto* lhs_ptr = static_cast<const void*>(lhs_packed_qa8dx + lhs_packed_offset);
+
+            parallel_for(n_blocks, [&](size_t n_block) {
+                size_t n_start = (n_block * BLOCK_SIZE);
+                size_t n_end = std::min(n_start + BLOCK_SIZE, N);
+                size_t n_block_size = n_end - n_start;
+                const size_t rhs_packed_offset = ukernel_i8.get_rhs_packed_offset(n_start, K);
+                const size_t dst_offset = ukernel_i8.get_dst_offset(0, n_start, dst_stride_row);
+                const auto* rhs_ptr = static_cast<const void*>(rhs_packed_qs8cx + rhs_packed_offset);
+                float* dst_ptr = (dst + dst_offset / sizeof(float));
+                ukernel_i8.run_matmul(M,
+                                      n_block_size,
+                                      K,
+                                      lhs_ptr,
+                                      rhs_ptr,
+                                      dst_ptr,
+                                      dst_stride_row,
+                                      dst_stride_col,
+                                      FLOAT_MIN,
+                                      FLOAT_MAX);
+            });
+        }
     }
 }
 
