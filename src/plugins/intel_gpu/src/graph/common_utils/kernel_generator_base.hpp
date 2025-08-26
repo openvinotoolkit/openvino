f9c3afe15a76eaec34b8ff1b62af0010099c0c08
zaixing.wang <zaixing.wang@intel.com>
2025-08-26 10:44:52 +0800

    clean

--- src/plugins/intel_gpu/src/graph/common_utils/kernel_generator_base.hpp
+++ src/plugins/intel_gpu/src/graph/common_utils/kernel_generator_base.hpp
@@ -10,7 +10,6 @@
 #include "intel_gpu/graph/kernel_impl_params.hpp"
 #include "intel_gpu/primitives/reorder.hpp"
 #include "intel_gpu/runtime/kernel_args.hpp"
-// #include "include/primitive_inst.h"
 
 namespace micro {
 struct MicroKernelPackage;
@@ -30,9 +29,7 @@ using cldnn::WeightsReorderParams;
 using KernelLanguage = cldnn::kernel_language;
 
 struct KernelData;
-
 struct ImplRuntimeParams {};
-
 
 struct DispatchDataFunc {
     using FuncType = std::function<void(const RuntimeParams&, KernelData&, ImplRuntimeParams*)>;
