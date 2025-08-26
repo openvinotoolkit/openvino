bf6213963bba627c1cf5ee8cb51c8531a2491dc7
Alexander Nesterov <nesterov.alexander@outlook.com>
2025-08-26 12:48:06 +0200

    refactoring eltwise

--- src/plugins/intel_cpu/src/nodes/executors/acl/acl_eltwise.hpp
+++ src/plugins/intel_cpu/src/nodes/executors/acl/acl_eltwise.hpp
@@ -4,39 +4,27 @@
 
 #pragma once
 
-#include <arm_compute/runtime/IFunction.h>
-#include <arm_compute/runtime/Tensor.h>
-
-#include <memory>
-#include <vector>
-
-#include "memory_desc/cpu_memory_desc.h"
+#include "acl_common_executor.hpp"
 #include "nodes/executors/eltwise_config.hpp"
-#include "nodes/executors/executor.hpp"
-#include "nodes/executors/memory_arguments.hpp"
-#include "onednn/iml_type_mapper.h"
 
 namespace ov::intel_cpu {
 
-class AclEltwiseExecutor : public Executor {
+class ACLEltwiseExecutor : public ACLCommonExecutor {
 public:
-    AclEltwiseExecutor(EltwiseAttrs attrs, const MemoryArgs& memory, const ExecutorContext::CPtr& context);
+    ACLEltwiseExecutor(const EltwiseAttrs& attrs, const MemoryArgs& memory, const ExecutorContext::CPtr& context);
+
     static bool supports(const EltwiseConfig& config);
 
-    bool update(const MemoryArgs& memory) override;
-    void execute(const MemoryArgs& memory) override;
+    void updateTensorsShapes(ACLShapes& aclMemoryShapes) override;
 
-    [[nodiscard]] impl_desc_type implType() const override {
-        return m_implType;
-    }
+    arm_compute::Status validateTensorsInfo(const ACLInfos& aclMemoryInfos) override;
+
+    ACLFunction configureFunction(const ACLTensors& aclMemoryTensors) override;
 
 private:
-    bool init(const std::vector<MemoryDescPtr>& srcDescs, const std::vector<MemoryDescPtr>& dstDescs);
-
     EltwiseAttrs aclEltwiseAttrs;
-    impl_desc_type m_implType = impl_desc_type::acl;
-    std::vector<arm_compute::Tensor> srcTensors, dstTensors;
-    std::unique_ptr<arm_compute::IFunction> ifunc;
 };
+
+using ACLEltwiseExecutorPtr = std::shared_ptr<ACLEltwiseExecutor>;
 
 }  // namespace ov::intel_cpu
