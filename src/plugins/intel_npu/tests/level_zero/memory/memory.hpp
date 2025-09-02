a25af336172f61fe35fe2d6b7be7fdb82df2f6cb
alexandruenache1111 <alexandru.enache@intel.com>
2025-09-02 10:52:00 +0000

    Initial changes for allocateTwice and new binary
    Signed-off-by: alexandruenache1111 <alexandru.enache@intel.com>

---
+++ src/plugins/intel_npu/tests/level_zero/memory/memory.hpp
@@ -0,0 +1,22 @@
+// Copyright (C) 2018-2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <gtest/gtest.h>
+#include "zero_memory.hpp"
+
+using namespace intel_npu;
+
+class MemoryAllocator : public ::testing::TestWithParam<size_t> {
+protected:
+    void SetUp() override;
+
+    void TearDown() override;
+
+public:
+    std::shared_ptr<zeroMemory::HostMemSharedAllocator> allocator;
+
+    static std::string getTestCaseName(testing::TestParamInfo<size_t> obj) {
+        return std::to_string(obj.param);
+    }
+};
