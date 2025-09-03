0158e8968d15f961c5b07ef7fa158b2a04876696
Georgy Krivoruchko <georgy.krivoruchko@intel.com>
2025-09-03 15:02:14 +0400

    First implementation of GraphIterator calling mechanism

--- src/frontends/onnx/tests/graph_iterator.cpp
+++ src/frontends/onnx/tests/graph_iterator.cpp
@@ -1,7 +1,7 @@
 // Copyright (C) 2018-2025 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
-#include "openvino/frontend/tensorflow_lite/graph_iterator.hpp"
+#include "openvino/frontend/onnx/graph_iterator.hpp"
 
 #include <onnx/onnx_pb.h>
 
@@ -18,7 +18,7 @@ using ::ONNX_NAMESPACE::Version;
 TEST_P(FrontEndLoadFromTest, testLoadUsingSimpleGraphIterator) {
     ov::frontend::FrontEnd::Ptr fe;
 
-    class SimpleIterator : public ov::frontend::tensorflow_lite::GraphIterator {
+    class SimpleIterator : public ov::frontend::onnx::GraphIterator {
     public:
         size_t size() const override {
             return 0;
@@ -28,7 +28,7 @@ TEST_P(FrontEndLoadFromTest, testLoadUsingSimpleGraphIterator) {
         bool is_end() const override {
             return true;
         };
-        std::shared_ptr<ov::frontend::tensorflow_lite::DecoderBase> get_decoder() const override {
+        std::shared_ptr<ov::frontend::onnx::DecoderBase> get_decoder() const override {
             return nullptr;
         };
         size_t get_subgraph_size() const override {
@@ -44,9 +44,9 @@ TEST_P(FrontEndLoadFromTest, testLoadUsingSimpleGraphIterator) {
     auto iter = std::make_shared<SimpleIterator>();
 
     {
-        auto graph_iter = std::dynamic_pointer_cast<ov::frontend::tensorflow_lite::GraphIterator>(iter);
-        ASSERT_NO_THROW(m_frontEnd = m_fem.load_by_framework("tflite"))
-            << "Could not create the TFLITE FE using a pointer GraphIterator";
+        auto graph_iter = std::dynamic_pointer_cast<ov::frontend::onnx::GraphIterator>(iter);
+        ASSERT_NO_THROW(m_frontEnd = m_fem.load_by_framework("onnx"))
+            << "Could not create the ONNX FE using a pointer GraphIterator";
         ASSERT_NE(m_frontEnd, nullptr);
 
         ASSERT_EQ(m_frontEnd->supported(graph_iter), true);
