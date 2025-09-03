542824b65c154bb7fb3af30600c946d6cca7693e
liang <liang.gao@intel.com>
2025-09-03 15:49:53 +0800

    Fix abs Operator int accuracy issue (#31920)
    ### Details:
     - Current Abs operator's implementation
    
    1.  Convert int32 to float
    2. Apply sign mask
    3. Convert float to int32
    
    Step 3 will result in a precision loss.
    
    
    ### Tickets:
     - [CVS-171704](https://jira.devtools.intel.com/browse/CVS-171704)

--- src/plugins/intel_cpu/src/emitters/plugin/x64/jit_dnnl_ext_emitters.hpp
+++ src/plugins/intel_cpu/src/emitters/plugin/x64/jit_dnnl_ext_emitters.hpp
@@ -85,21 +85,6 @@ public:
     }
 };
 
-class jit_abs_emitter : public jit_dnnl_emitter {
-public:
-    jit_abs_emitter(dnnl::impl::cpu::x64::jit_generator_t* host,
-                    dnnl::impl::cpu::x64::cpu_isa_t host_isa,
-                    const std::shared_ptr<ov::Node>& n,
-                    ov::element::Type exec_prc = ov::element::f32)
-        : jit_dnnl_emitter(host, host_isa, n, exec_prc) {
-        kind = dnnl_eltwise_abs;
-        alpha = 0.F;
-        beta = 0.F;
-
-        set_injector();
-    }
-};
-
 class jit_clamp_emitter : public jit_dnnl_emitter {
 public:
     jit_clamp_emitter(dnnl::impl::cpu::x64::jit_generator_t* host,
