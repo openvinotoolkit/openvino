b7a647533ecab8e09a3effe8e3f8966bbf36ade0
Xin Wang <xin1.wang@intel.com>
2025-09-04 14:55:18 +0800

    Add test for remote tensor and host tensor Signed-off-by: Xin Wang <xin1.wang@intel.com>

--- src/plugins/intel_npu/tests/functional/behavior/dynamic_pipeline/compile_and_infer_different_shape.cpp
+++ src/plugins/intel_npu/tests/functional/behavior/dynamic_pipeline/compile_and_infer_different_shape.cpp
@@ -9,6 +9,8 @@
 #include "intel_npu/config/options.hpp"
 #include "intel_npu/npu_private_properties.hpp"
 
+const std::vector<std::string> models = {"MaxPool_canonical.xml", "CustomNet_canonical_strides_1x1_no_fork.xml"};
+
 const std::vector<ov::AnyMap> configs = {{{"NPU_COMPILER_TYPE", "MLIR"},
                                           {"NPU_PLATFORM", "NPU4000"},
                                           {"NPU_COMPILATION_MODE", "HostCompile"},
@@ -19,5 +21,6 @@ const std::vector<ov::AnyMap> configs = {{{"NPU_COMPILER_TYPE", "MLIR"},
 INSTANTIATE_TEST_SUITE_P(smoke_BehaviorTests,
                          InferRequestDynamicShapeTests,
                          ::testing::Combine(::testing::Values(ov::test::utils::DEVICE_NPU),
+                                            ::testing::ValuesIn(models),
                                             ::testing::ValuesIn(configs)),
                          ov::test::utils::appendPlatformTypeTestName<InferRequestDynamicShapeTests>);
