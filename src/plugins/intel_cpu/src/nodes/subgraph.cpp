openvinotoolkit/openvino

commit 813b3ac907d3328883e6840c26930080b59b5666

[sns]Matmul via blockwised with kleidiai (#30534)
### Details:
- *Implement Matmul via blockwised small gemm with KleidiAI, such that
blocks could fused with more ops, for example in MHA case*
- *Second input repack as separate copyB node, and repack outside of
loops to shared with each M block for better perf*
- *This is production of perf analysis
PR:https://github.com/openvinotoolkit/openvino/pull/29858*

### Tickets:
 - *CVS-167012*

src/plugins/intel_cpu/src/nodes/subgraph.cpp
@@ -54,6 +54,11 @@
 #if defined(OPENVINO_ARCH_ARM64)
 #    include "emitters/snippets/aarch64/cpu_generator.hpp"
 #    include "executors/aarch64/subgraph.hpp"
+#    include "snippets/lowered/pass/insert_reg_spills.hpp"
+#    include "transformations/snippets/aarch64/pass/brgemm_to_gemm_cpu.hpp"
+#    include "transformations/snippets/aarch64/pass/lowered/adjust_gemm_copy_b_loop_ports.hpp"
+#    include "transformations/snippets/aarch64/pass/lowered/gemm_cpu_blocking.hpp"
+#    include "transformations/snippets/aarch64/pass/lowered/insert_gemm_copy_buffers.hpp"
 #    include "transformations/snippets/aarch64/shape_inference.hpp"
 #else
 #    include "emitters/snippets/x64/cpu_generator.hpp"
@@ -558,6 +563,9 @@ Subgraph::DataFlowPasses Subgraph::getDataFlowPasses() {
                                                repacked_constant_input_config);
     }
     SNIPPETS_REGISTER_PASS_ABSOLUTE_X86_64(Place::PipelineEnd, ov::intel_cpu::pass::RemoveConverts);
+    SNIPPETS_REGISTER_PASS_RELATIVE_ARM64(Place::Before,
+                                          ov::snippets::pass::PropagatePrecision,
+                                          ov::intel_cpu::pass::BrgemmToGemmCPU);
     SNIPPETS_REGISTER_PASS_ABSOLUTE_COMMON(Place::PipelineEnd, ov::intel_cpu::pass::MulAddToFMA);
 
 #ifdef SNIPPETS_LIBXSMM_TPP
@@ -588,11 +596,10 @@ Subgraph::DataFlowPasses Subgraph::getDataFlowPasses() {
 
 Subgraph::ControlFlowPasses Subgraph::getControlFlowPasses() {
     ControlFlowPasses backend_passes;
-#if defined(OPENVINO_ARCH_X86_64) || (defined(OPENVINO_ARCH_ARM64) && defined(SNIPPETS_LIBXSMM_TPP))
+#if defined(OPENVINO_ARCH_X86_64) || defined(OPENVINO_ARCH_ARM64)
     using PassPosition = ov::snippets::pass::PassPosition;
     using Place = PassPosition::Place;
 #endif
-
 #if defined(OPENVINO_ARCH_X86_64)
 #    define SNIPPETS_REGISTER_PASS_RELATIVE_X86_64(PASS_PLACE, TARGET_PASS, PASS, ...)             \
         backend_passes.emplace_back(PassPosition(PASS_PLACE, TARGET_PASS::get_type_info_static()), \
@@ -639,6 +646,15 @@ Subgraph::ControlFlowPasses Subgraph::getControlFlowPasses() {
                                            ov::intel_cpu::pass::InitRepackedConstantInputs,
                                            context->getParamsCache(),
                                            repacked_constant_input_config);
+    SNIPPETS_REGISTER_PASS_RELATIVE_ARM64(Place::After,
+                                          ov::snippets::lowered::pass::MarkLoops,
+                                          ov::intel_cpu::pass::GemmCPUBlocking);
+    SNIPPETS_REGISTER_PASS_RELATIVE_ARM64(Place::After,
+                                          ov::snippets::lowered::pass::InitLoops,
+                                          ov::intel_cpu::pass::aarch64::AdjustGemmCopyBLoopPorts);
+    SNIPPETS_REGISTER_PASS_RELATIVE_ARM64(Place::Before,
+                                          ov::snippets::lowered::pass::InsertBuffers,
+                                          ov::intel_cpu::pass::aarch64::InsertGemmCopyBuffers);
 
 #ifdef SNIPPETS_LIBXSMM_TPP
     SNIPPETS_REGISTER_PASS_RELATIVE_X86_64(Place::Before,
@@ -715,6 +731,10 @@ void Subgraph::optimizeIR() {
 
     const auto control_flow_config = std::make_shared<ov::snippets::lowered::pass::PassConfig>();
     const auto control_flow_passes = getControlFlowPasses();
+#if defined(OPENVINO_ARCH_ARM64)
+    // enable it after emitters for RegSpillBegin and RegSpillEnd are implemented on ARM in CVS-162498
+    control_flow_config->disable<ov::snippets::lowered::pass::InsertRegSpills>();
+#endif
 
 #ifdef SNIPPETS_LIBXSMM_TPP
     // Note: temporary disabled. Re-enable after ticket 132833 is resolved

src/common/snippets/include/snippets/utils/utils.hpp
@@ -12,6 +12,7 @@
 #include "snippets/shape_types.hpp"
 #include "snippets/lowered/expression.hpp"
 #include "snippets/lowered/expression_port.hpp"
+#include "snippets/lowered/port_descriptor.hpp"
 
 #include "openvino/op/fake_quantize.hpp"
 #include "openvino/op/constant.hpp"
@@ -40,6 +41,15 @@ constexpr inline bool is_full_dim_value(size_t value) {
     return value == get_full_dim_value();
 }
 /* ---------------------- */
+template <typename T,
+          typename = typename std::enable_if<(std::is_same<T, ov::Output<Node>>::value ||
+                                              std::is_same<T, ov::Input<Node>>::value),
+                                             void>::type>
+void set_full_port_desc(const T& port, size_t rank = 2) {
+    const auto& shape_rank = port.get_partial_shape().size();
+    const std::vector<size_t> full_dim_subtensor(std::min(shape_rank, rank), ov::snippets::utils::get_full_dim_value());
+    ov::snippets::lowered::PortDescriptorUtils::set_port_descriptor(port, full_dim_subtensor);
+}
 
 // Get non-scalar Constant count that will be created after FakeQuantize decomposition.
 // This count is needed to know exact count of non-scalar Constants during tokenization.

src/plugins/intel_cpu/src/emitters/snippets/aarch64/cpu_generator.cpp
@@ -9,6 +9,8 @@
 #include "emitters/plugin/aarch64/jit_conversion_emitters.hpp"
 #include "emitters/plugin/aarch64/jit_eltwise_emitters.hpp"
 #include "emitters/snippets/aarch64/jit_fill_emitter.hpp"
+#include "emitters/snippets/aarch64/jit_gemm_copy_b_emitter.hpp"
+#include "emitters/snippets/aarch64/jit_gemm_emitter.hpp"
 #include "emitters/snippets/aarch64/jit_kernel_emitter.hpp"
 #include "emitters/snippets/aarch64/jit_loop_emitters.hpp"
 #include "emitters/snippets/aarch64/jit_memory_emitters.hpp"
@@ -24,6 +26,8 @@
 #include "snippets/lowered/expression.hpp"
 #include "snippets/snippets_isa.hpp"
 #include "transformations/cpu_opset/common/op/swish_cpu.hpp"
+#include "transformations/snippets/aarch64/op/gemm_copy_b.hpp"
+#include "transformations/snippets/aarch64/op/gemm_cpu.hpp"
 #include "transformations/snippets/common/op/fused_mul_add.hpp"
 
 #ifdef SNIPPETS_LIBXSMM_TPP
@@ -155,6 +159,7 @@ CPUTargetMachine::CPUTargetMachine(dnnl::impl::cpu::aarch64::cpu_isa_t host_isa,
     // data movement
     jitters[op::v0::Parameter::get_type_info_static()] = CREATE_SNIPPETS_EMITTER(jit_nop_emitter);
     jitters[op::v0::Result::get_type_info_static()] = CREATE_SNIPPETS_EMITTER(jit_nop_emitter);
+    jitters[snippets::op::Buffer::get_type_info_static()] = CREATE_SNIPPETS_EMITTER(jit_nop_emitter);
     jitters[snippets::op::VectorBuffer::get_type_info_static()] = CREATE_SNIPPETS_EMITTER(jit_nop_emitter);
     jitters[snippets::op::RankNormalization::get_type_info_static()] = CREATE_SNIPPETS_EMITTER(jit_nop_emitter);
     jitters[snippets::op::BroadcastMove::get_type_info_static()] = CREATE_SNIPPETS_EMITTER(jit_broadcast_move_emitter);
@@ -217,7 +222,10 @@ CPUTargetMachine::CPUTargetMachine(dnnl::impl::cpu::aarch64::cpu_isa_t host_isa,
     jitters[ov::op::v0::Sqrt::get_type_info_static()] = CREATE_CPU_EMITTER(jit_sqrt_emitter);
     jitters[ov::intel_cpu::SwishNode::get_type_info_static()] = CREATE_CPU_EMITTER(jit_swish_emitter);
     jitters[ov::op::v0::Tanh::get_type_info_static()] = CREATE_CPU_EMITTER(jit_tanh_emitter);
-
+    jitters[ov::intel_cpu::aarch64::GemmCPU::get_type_info_static()] =
+        CREATE_SNIPPETS_EMITTER(jit_gemm_emitter, configurator->get_kernel_executor_table());
+    jitters[ov::intel_cpu::aarch64::GemmCopyB::get_type_info_static()] =
+        CREATE_SNIPPETS_EMITTER(jit_gemm_copy_b_emitter, configurator->get_kernel_executor_table());
 #ifdef SNIPPETS_LIBXSMM_TPP
     // brgemm
     jitters[ov::intel_cpu::tpp::op::BrgemmTPP::get_type_info_static()] =
@@ -321,7 +329,10 @@ std::shared_ptr<snippets::Generator> CPUGenerator::clone() const {
 
 ov::snippets::RegType CPUGenerator::get_specific_op_out_reg_type(const ov::Output<ov::Node>& out) const {
     const auto op = out.get_node_shared_ptr();
-    if (ov::as_type_ptr<intel_cpu::FusedMulAdd>(op) || ov::as_type_ptr<intel_cpu::SwishNode>(op)) {
+    if (is_type_any_of<intel_cpu::aarch64::GemmCPU, intel_cpu::aarch64::GemmCopyB>(op)) {
+        return ov::snippets::RegType::gpr;
+    }
+    if (ov::is_type_any_of<intel_cpu::FusedMulAdd, intel_cpu::SwishNode>(op)) {
         return ov::snippets::RegType::vec;
     }
     return ov::snippets::RegType::undefined;

src/plugins/intel_cpu/src/emitters/snippets/aarch64/jit_gemm_copy_b_emitter.cpp
@@ -1,0 +1,81 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "jit_gemm_copy_b_emitter.hpp"
+
+#include "snippets/utils/utils.hpp"
+#include "transformations/snippets/aarch64/op/gemm_copy_b.hpp"
+#include "transformations/snippets/aarch64/op/gemm_utils.hpp"
+
+namespace ov::intel_cpu::aarch64 {
+
+using jit_generator = dnnl::impl::cpu::aarch64::jit_generator;
+using cpu_isa_t = dnnl::impl::cpu::aarch64::cpu_isa_t;
+using ExpressionPtr = ov::snippets::lowered::ExpressionPtr;
+
+jit_gemm_copy_b_emitter::jit_gemm_copy_b_emitter(jit_generator* h,
+                                                 cpu_isa_t isa,
+                                                 const ExpressionPtr& expr,
+                                                 const snippets::KernelExecutorTablePtr& kernel_table)
+    : jit_emitter(h, isa) {
+    in_out_type_ = emitter_in_out_map::gpr_to_gpr;
+    const auto gemm_repack = ov::as_type_ptr<GemmCopyB>(expr->get_node());
+    OV_CPU_JIT_EMITTER_ASSERT(gemm_repack, "expects GemmCopyB node");
+    const auto& child_gemms = ov::intel_cpu::aarch64::gemm_utils::repacking::get_gemm_exprs(expr);
+    size_t n_blk_size = 0;
+    for (size_t i = 0; i < child_gemms.size(); i++) {
+        const auto& gemm_in1_subtensor =
+            ov::snippets::utils::get_projected_subtensor(child_gemms[i]->get_input_port(1));
+        const auto& current_block = *gemm_in1_subtensor.rbegin();
+        if (current_block != snippets::utils::get_dynamic_value<size_t>() && current_block > n_blk_size) {
+            n_blk_size = current_block;
+        }
+    }
+    OV_CPU_JIT_EMITTER_ASSERT(n_blk_size > 0, "n_blk_size of gemm_repack is expected to be greater than 0.");
+    GemmCopyBKernelKaiConfig kernel_config(n_blk_size);
+    m_kernel_executor = kernel_table->register_kernel<GemmCopyBKaiKernelExecutor>(expr, kernel_config);
+}
+
+std::set<std::vector<element::Type>> jit_gemm_copy_b_emitter::get_supported_precisions(
+    const std::shared_ptr<ov::Node>& node) {
+    // Note: Brgemm currently supports only fp32 on arm
+    return {{element::f32}};
+}
+
+void jit_gemm_copy_b_emitter::validate_arguments(const std::vector<size_t>& in, const std::vector<size_t>& out) const {
+    OV_CPU_JIT_EMITTER_ASSERT(in.size() == 1, "Expects 1 input reg, got", in.size());
+    OV_CPU_JIT_EMITTER_ASSERT(out.size() == 1, "Expects 1 output reg, got", out.size());
+}
+
+void jit_gemm_copy_b_emitter::emit_impl(const std::vector<size_t>& in, const std::vector<size_t>& out) const {
+    validate_arguments(in, out);
+    // todo: use optimized reg spill after CVS-162498
+    std::unordered_set<size_t> exclude = {};
+    store_context(exclude);
+
+    Xbyak_aarch64::XReg x0(0);
+    Xbyak_aarch64::XReg x1(1);
+    Xbyak_aarch64::XReg x2(2);
+    h->str(Xbyak_aarch64::XReg(in[0]), pre_ptr(h->sp, -get_vec_length()));
+    h->str(Xbyak_aarch64::XReg(out[0]), pre_ptr(h->sp, -get_vec_length()));
+    h->ldr(x2, post_ptr(h->sp, get_vec_length()));
+    h->ldr(x1, post_ptr(h->sp, get_vec_length()));
+    const auto& compiled_kernel = get_compiled_kernel_ptr();
+    h->mov(x0, compiled_kernel);
+
+    Xbyak_aarch64::XReg func_reg(9);
+    h->mov(func_reg, get_execute_function_ptr());
+    h->blr(func_reg);
+
+    restore_context(exclude);
+}
+
+const uintptr_t jit_gemm_copy_b_emitter::get_compiled_kernel_ptr() const {
+    return reinterpret_cast<const uintptr_t>(m_kernel_executor.get());
+}
+
+const uintptr_t jit_gemm_copy_b_emitter::get_execute_function_ptr() const {
+    return reinterpret_cast<const uintptr_t>(GemmCopyBKaiKernelExecutor::execute);
+}
+}  // namespace ov::intel_cpu::aarch64

src/plugins/intel_cpu/src/emitters/snippets/aarch64/jit_gemm_copy_b_emitter.hpp
@@ -1,0 +1,35 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include "emitters/plugin/aarch64/jit_emitter.hpp"
+#include "emitters/snippets/aarch64/kernel_executors/gemm_copy_b.hpp"
+
+namespace ov::intel_cpu::aarch64 {
+class jit_gemm_copy_b_emitter : public jit_emitter {
+public:
+    jit_gemm_copy_b_emitter(dnnl::impl::cpu::aarch64::jit_generator* h,
+                            dnnl::impl::cpu::aarch64::cpu_isa_t isa,
+                            const ov::snippets::lowered::ExpressionPtr& expr,
+                            const snippets::KernelExecutorTablePtr& kernel_table);
+
+    size_t get_inputs_count() const override {
+        return 1;
+    }
+
+    static std::set<std::vector<element::Type>> get_supported_precisions(
+        const std::shared_ptr<ov::Node>& node = nullptr);
+
+protected:
+    void validate_arguments(const std::vector<size_t>& in, const std::vector<size_t>& out) const override;
+    void emit_impl(const std::vector<size_t>& in, const std::vector<size_t>& out) const override;
+
+    const uintptr_t get_execute_function_ptr() const;
+    const uintptr_t get_compiled_kernel_ptr() const;
+
+    std::shared_ptr<GemmCopyBKaiKernelExecutor> m_kernel_executor = nullptr;
+};
+
+}  // namespace ov::intel_cpu::aarch64

src/plugins/intel_cpu/src/emitters/snippets/aarch64/jit_gemm_emitter.cpp
@@ -1,0 +1,71 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "jit_gemm_emitter.hpp"
+
+#include "snippets/utils/utils.hpp"
+
+using namespace Xbyak_aarch64;
+
+namespace ov::intel_cpu::aarch64 {
+
+using jit_generator = dnnl::impl::cpu::aarch64::jit_generator;
+using cpu_isa_t = dnnl::impl::cpu::aarch64::cpu_isa_t;
+using ExpressionPtr = ov::snippets::lowered::ExpressionPtr;
+
+jit_gemm_emitter::jit_gemm_emitter(jit_generator* h,
+                                   cpu_isa_t isa,
+                                   const ExpressionPtr& expr,
+                                   const snippets::KernelExecutorTablePtr& kernel_table)
+    : jit_emitter(h, isa) {
+    in_out_type_ = emitter_in_out_map::gpr_to_gpr;
+    GemmKernelKaiConfig kernel_config;
+    m_kernel_executor_kai = kernel_table->register_kernel<GemmKaiKernelExecutor>(expr, kernel_config);
+}
+
+std::set<std::vector<element::Type>> jit_gemm_emitter::get_supported_precisions(const std::shared_ptr<ov::Node>& node) {
+    // Note: currently supports only fp32 on arm
+    return {{element::f32, element::f32}};
+}
+
+void jit_gemm_emitter::validate_arguments(const std::vector<size_t>& in, const std::vector<size_t>& out) const {
+    OV_CPU_JIT_EMITTER_ASSERT(in.size() == 2, "Expects 2 input regs, got", in.size());
+    OV_CPU_JIT_EMITTER_ASSERT(out.size() == 1, "Expects 1 output reg, got", out.size());
+}
+
+void jit_gemm_emitter::emit_impl(const std::vector<size_t>& in, const std::vector<size_t>& out) const {
+    validate_arguments(in, out);
+    // todo: use optimized reg spill after CVS-162498
+    std::unordered_set<size_t> exclude = {};
+    store_context(exclude);
+
+    Xbyak_aarch64::XReg x0(0);
+    Xbyak_aarch64::XReg x1(1);
+    Xbyak_aarch64::XReg x2(2);
+    Xbyak_aarch64::XReg x3(3);
+    h->str(Xbyak_aarch64::XReg(in[0]), pre_ptr(h->sp, -get_vec_length()));
+    h->str(Xbyak_aarch64::XReg(in[1]), pre_ptr(h->sp, -get_vec_length()));
+    h->str(Xbyak_aarch64::XReg(out[0]), pre_ptr(h->sp, -get_vec_length()));
+    h->ldr(x3, post_ptr(h->sp, get_vec_length()));
+    h->ldr(x2, post_ptr(h->sp, get_vec_length()));
+    h->ldr(x1, post_ptr(h->sp, get_vec_length()));
+    const auto& compiled_kernel = get_compiled_kernel_ptr();
+    h->mov(x0, compiled_kernel);
+
+    Xbyak_aarch64::XReg func_reg(9);
+    h->mov(func_reg, get_execute_function_ptr());
+    h->blr(func_reg);
+
+    restore_context(exclude);
+}
+
+const uintptr_t jit_gemm_emitter::get_compiled_kernel_ptr() const {
+    return reinterpret_cast<const uintptr_t>(m_kernel_executor_kai.get());
+}
+
+const uintptr_t jit_gemm_emitter::get_execute_function_ptr() const {
+    return reinterpret_cast<const uintptr_t>(GemmKaiKernelExecutor::execute);
+}
+
+}  // namespace ov::intel_cpu::aarch64

src/plugins/intel_cpu/src/emitters/snippets/aarch64/jit_gemm_emitter.hpp
@@ -1,0 +1,37 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include "emitters/plugin/aarch64/jit_emitter.hpp"
+#include "emitters/snippets/aarch64/kernel_executors/gemm.hpp"
+#include "emitters/snippets/brgemm_generic.hpp"
+
+namespace ov::intel_cpu::aarch64 {
+
+class jit_gemm_emitter : public jit_emitter {
+public:
+    jit_gemm_emitter(dnnl::impl::cpu::aarch64::jit_generator* h,
+                     dnnl::impl::cpu::aarch64::cpu_isa_t isa,
+                     const ov::snippets::lowered::ExpressionPtr& expr,
+                     const snippets::KernelExecutorTablePtr& kernel_table);
+
+    size_t get_inputs_count() const override {
+        return 2;
+    }
+
+    static std::set<std::vector<element::Type>> get_supported_precisions(
+        const std::shared_ptr<ov::Node>& node = nullptr);
+
+protected:
+    void validate_arguments(const std::vector<size_t>& in, const std::vector<size_t>& out) const override;
+    void emit_impl(const std::vector<size_t>& in, const std::vector<size_t>& out) const override;
+
+    const uintptr_t get_execute_function_ptr() const;
+    const uintptr_t get_compiled_kernel_ptr() const;
+
+    std::shared_ptr<GemmKaiKernelExecutor> m_kernel_executor_kai = nullptr;
+};
+
+}  // namespace ov::intel_cpu::aarch64

src/plugins/intel_cpu/src/emitters/snippets/aarch64/kernel_executors/gemm.cpp
@@ -1,0 +1,87 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "gemm.hpp"
+
+#include "openvino/core/parallel.hpp"
+#include "transformations/snippets/aarch64/op/gemm_utils.hpp"
+#include "transformations/tpp/common/op/brgemm.hpp"
+
+namespace ov::intel_cpu::aarch64 {
+
+void GemmKernelKaiConfig::update(int64_t M, int64_t N, int64_t K, int64_t LDA, int64_t LDB, int64_t LDC, float beta) {
+    BrgemmGenericKernelConfig::update(M, N, K, LDA, LDB, LDC, beta);
+    m_hash = BrgemmGenericKernelConfig::compute_hash();
+}
+
+bool GemmKernelKaiConfig::operator==(const GemmKernelKaiConfig& rhs) const {
+    return BrgemmGenericKernelConfig::operator==(rhs) && m_hash == rhs.m_hash;
+}
+
+GemmKaiKernelExecutor::GemmKaiKernelExecutor(GemmKernelKaiConfig config)
+    : snippets::KernelExecutor<GemmKernelKaiConfig, GemmCompiledKernel>(std::move(config)) {}
+
+void GemmKaiKernelExecutor::update_kernel(const GemmKernelKaiConfig& config,
+                                          std::shared_ptr<GemmCompiledKernel>& kernel) const {
+    if (kernel == nullptr) {
+        // universal kernel could be used in any config and shape, as excuted peice by peice as binary call.
+        // config is passed as binary call parameters.
+        kernel = std::make_shared<GemmCompiledKernel>();
+    }
+}
+
+void GemmKaiKernelExecutor::update_config(const ov::snippets::lowered::ExpressionPtr& expr,
+                                          const ov::snippets::lowered::LinearIRCPtr& linear_ir,
+                                          GemmKernelKaiConfig& config) const {
+    const auto [M, N, K, beta] = BrgemmKernelExecutorHelper::get_runtime_brgemm_params(expr, linear_ir);
+
+    const auto LDA = snippets::utils::get_dim_stride(expr->get_input_port(0));
+    const auto LDC = snippets::utils::get_dim_stride(expr->get_output_port(0));
+    const auto LDB = snippets::utils::get_dim_stride(expr->get_input_port(1));
+    config.update(M, N, K, LDA, LDB, LDC, beta);
+}
+
+void GemmKaiKernelExecutor::execute(const GemmKaiKernelExecutor* executor, void* in0, void* in1, void* out0) {
+    OV_CPU_JIT_EMITTER_ASSERT(executor, "has nullptr executor");
+    // matmul for input1 and slices of repacked input2
+    const auto& config = static_cast<const GemmKernelKaiConfig&>(executor->get_config());
+    const auto& kernel = executor->get_kernel();
+    const auto& ukernel = *kernel->gemm_ukernel;
+    const auto& M = config.get_M();
+    const auto& N = config.get_N();
+    const auto& K = config.get_K();
+    const auto& lda = config.get_LDA();
+    const auto& ldc = config.get_LDC();
+    const size_t& BLOCK_SIZE = ov::intel_cpu::aarch64::gemm_utils::repacking::get_inner_n_block(element::f32);
+    size_t n_blocks = ov::snippets::utils::div_up(N, BLOCK_SIZE);
+    const size_t lhs_stride = lda * sizeof(float);  // K not split, it's also K * sizeof(float)
+    const size_t dst_stride_row = ldc * sizeof(float);
+    const size_t dst_stride_col = sizeof(float);
+    for (size_t n_block = 0; n_block < n_blocks; n_block++) {
+        size_t n_start = n_block * BLOCK_SIZE;
+        size_t n_end = std::min(n_start + BLOCK_SIZE, static_cast<size_t>(N));
+        size_t n_block_size = n_end - n_start;
+        // rhs_packed_offset is n_start*(k+1), as packed mem as 8*(K+1) blocks. If k blocked, then lda.
+        const size_t rhs_packed_offset = ukernel.get_rhs_packed_offset(n_start, K);
+        // m_idx is 0 as dst already point current block.
+        const size_t dst_offset = ukernel.get_dst_offset(0, n_start, dst_stride_row);
+        // in0, in1, out is point to current block memory, based on block loop info, and shift done in loop begin and
+        // end emitters(adjusted copyb loop info as repack outside block loops).
+        float* rhs_ptr = static_cast<float*>(in1) + rhs_packed_offset / sizeof(float);
+        float* dst_ptr = (static_cast<float*>(out0) + dst_offset / (sizeof(float)));
+        ukernel.run_matmul(M,
+                           n_block_size,
+                           K,
+                           in0,
+                           lhs_stride,
+                           rhs_ptr,
+                           dst_ptr,
+                           dst_stride_row,
+                           dst_stride_col,
+                           std::numeric_limits<float>::min(),
+                           std::numeric_limits<float>::max());
+    }
+}
+
+}  // namespace ov::intel_cpu::aarch64

commit 572ad8885537d8b4f73946239fce24d4ec782356

[Snippets][CPU] Fix TPP MatMul execution on ARM64 (#31971)
### Details:
Fix BrgemmTPP regression on ARM64 after
https://github.com/openvinotoolkit/openvino/commit/813b3ac907d3328883e6840c26930080b59b5666

Fixed two problems in this scope:
 - Compilation error due to missing include
- GemmCPU (KleidiAI) was used instead of BrgemmTPP due to incorrect
passes order

### Tickets:
 - N/A

src/plugins/intel_cpu/src/nodes/subgraph.cpp
@@ -100,6 +100,8 @@
 #        include "transformations/tpp/x64/pass/eltwise_to_eltwise_tpp.hpp"
 #        include "transformations/tpp/x64/pass/fuse_tpp_to_equations.hpp"
 #        include "transformations/tpp/x64/pass/scalar_to_scalar_tpp.hpp"
+#    elif defined(OPENVINO_ARCH_ARM64)
+#        include "snippets/lowered/pass/insert_loops.hpp"
 #    endif
 #endif
 
@@ -594,7 +596,7 @@ Subgraph::DataFlowPasses Subgraph::getDataFlowPasses() {
                                            ov::intel_cpu::tpp::pass::EltwiseToEltwiseTPP,
                                            ov::intel_cpu::tpp::pass::FuseTPPToEquations);
     SNIPPETS_REGISTER_PASS_RELATIVE_ARM64(Place::Before,
-                                          ov::snippets::pass::PropagatePrecision,
+                                          ov::intel_cpu::pass::BrgemmToGemmCPU,
                                           ov::intel_cpu::tpp::pass::BrgemmToBrgemmTPP);
 #endif
 