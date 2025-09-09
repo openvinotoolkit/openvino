5d3168fdbf25aa2a829b25e72f74bef55af2d339
liang <gxgaoliang@126.com>
2025-09-09 14:06:51 +0800

    Using std::next
    Co-authored-by: Pawel Raasz <pawel.raasz@intel.com>

--- src/core/reference/src/op/interpolate.cpp
+++ src/core/reference/src/op/interpolate.cpp
@@ -141,8 +141,8 @@ InterpolateEvalHelper::InfoForGenericLinearONNXMode InterpolateEvalHelper::get_i
 
     std::vector<int64_t> input_index_multipliers(spatial_rank, 1);
     std::vector<int64_t> output_index_multipliers(spatial_rank, 1);
-    auto input_shape_end_it = input_shape.rbegin() + spatial_rank - 1;
-    auto out_shape_end_it = output_shape.rbegin() + spatial_rank - 1;
+    const auto input_shape_end_it =std::next(input_shape.rbegin(), spatial_rank - 1);
+    const auto out_shape_end_it = std::next(output_shape.rbegin(), spatial_rank - 1);
     std::partial_sum(input_shape.rbegin(),
                      input_shape_end_it,
                      input_index_multipliers.rbegin() + 1,
