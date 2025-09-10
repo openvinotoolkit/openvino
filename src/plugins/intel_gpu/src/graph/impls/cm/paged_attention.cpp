d15f12c977e2fc53b524a724eb51ef97eef6514e
Li, Tingqian <tingqian.li@intel.com>
2025-09-10 17:49:22 +0800

    fix building error on windows.

--- src/plugins/intel_gpu/src/graph/impls/cm/paged_attention.cpp
+++ src/plugins/intel_gpu/src/graph/impls/cm/paged_attention.cpp
@@ -157,8 +157,8 @@ public:
             auto out_shape = params.output_layouts[0].get_shape();
             const size_t kv_len = get_max_context_len(params) / STRIDE * STRIDE;
             const size_t q_len = out_shape[0];
-            const uint M = q_len / STRIDE;   //# will slient drop the tails which is less than `stride`
-            const uint N = kv_len / STRIDE;
+            const uint32_t M = q_len / STRIDE;   //# will slient drop the tails which is less than `stride`
+            const uint32_t N = kv_len / STRIDE;
             const size_t q_stride_pad = round_up_to(M, BLOCK_WG_M);
             const size_t N_kq_groups = ceil_div(N, BLOCK_WG_N);
 
@@ -169,9 +169,9 @@ public:
             if (block_size > 1) {
                 OPENVINO_ASSERT(block_size % STRIDE == 0, "sparse block_size must be devidable by stride.");
                 const size_t sum_per_n_token_in_block = block_size / STRIDE;  // FIXME
-                const uint sum_per_token_in_block = block_size / STRIDE;
-                const uint k_block_in_group = BLOCK_WG_N / sum_per_token_in_block;
-                const uint k_block_pad = k_block_in_group * N_kq_groups;
+                const uint32_t sum_per_token_in_block = block_size / STRIDE;
+                const uint32_t k_block_in_group = BLOCK_WG_N / sum_per_token_in_block;
+                const uint32_t k_block_pad = k_block_in_group * N_kq_groups;
                 auto count_kq_exp_partial_sum = static_cast<int64_t>(desc->heads_num * q_stride_pad * k_block_pad);
                 internal_buffers.emplace_back(count_kq_exp_partial_sum, ov::element::f32);       // 3: kq_exp_partial_sum
 
