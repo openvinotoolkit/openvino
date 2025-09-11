dadd35ada50bd555624edfba55822de82fcbef2f
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2025-09-11 14:24:25 +0200

    clang format 2

--- src/core/include/openvino/pass/manager.hpp
+++ src/core/include/openvino/pass/manager.hpp
@@ -30,7 +30,7 @@ public:
     explicit Manager(std::shared_ptr<PassConfig> pass_config, std::string name = "UnnamedManager");
 
     //// \brief Construct Manager with a copied PassConfig instance; it will not share PassConfig as in the constructor
-    ///above
+    /// above
     explicit Manager(const PassConfig& pass_config, std::string name = "UnnamedManager");
 
     /// \brief Register given transformation class type to execution list
