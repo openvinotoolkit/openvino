openvinotoolkit/openvino

commit 9579e0baa0c2525673d18769f8bf1b7d1555a874

[GPU] Fix accuracy degradation issue for hbonet0.5/hbonet1.0/nanodet-m-1.5x-416 models caused by eltwise kernels (#31140)
### Details
- Fix accuracy degradation issue for
hbonet0.5/hbonet1.0/nanodet-m-1.5x-416 models caused by none-zeros
values existed in the blocked format (b_fs_yx_fsv16/b_fs_yx_fsv32)
padded memory area in eltwise kernels.

### Description of the issue

#### Symptom
Onednn Concatenation got different output values before/after commit
d7f0f3404b when with blocked format memory.

#### Root cause
- eltwise_blocked_opt kernel will copy extra values from input (to pad
features to 16/32 block) to output for the blocked format (fsv16/fsv32)
padded memory area, instead of using zeros.
- Onednn Concatenation requires blocked format padded memory area to be
padded with zeros
[link](https://uxlfoundation.github.io/oneDNN/dev_guide_understanding_memory_formats.html#what-if-channels-are-not-multiples-of-8-or-16).
When the Concatenation's input comes from eltwise_blocked_opt or
generic_eltwise_ref kernel's output and its blocked format padded memory
area is not zero-padded, the onednn concatenation will have wrong output
values, and cause the model's accuracy degradation.
- When running model inference for the second time, crop's output memory
is shared with other primitives and thus may have non-zero values in the
padded memory area, and since it will not be filled with zeros in the
eltwise_blocked_opt or generic_eltwise_ref kernel by default, the onednn
concatenation after crop then will get wrong outputs.

#### How to fix it
- Skip copying extra values from input in eltwise_blocked_opt kernel.
- Fill zeros to crop if has blocked format memory and shaved with
others, and followed by onednn concatenation.

#### The code and line that caused this issue

https://github.com/openvinotoolkit/openvino/blob/0dcc5adfd89dc9151f0c4448e346d0ec030f70e6/src/plugins/intel_gpu/src/kernel_selector/cl_kernels/eltwise_blocked_opt.cl#L64

#### Reproduction step and snapshot

src/plugins/intel_gpu/src/graph/primitive_inst.cpp
@@ -2025,21 +2025,28 @@ void primitive_inst::prepare_primitive() {
 
     // After all dependencies are configured, check if the current primitive instance requires its output memory to be reset (e.g., when its user
     // is a convolution that requires zeroed-out data paddings)
+    auto skip_reset = true;
     if (is_dynamic() && need_reset_output_memory() && !can_be_optimized() && !get_node().is_type<input_layout>()) {
         const auto& users = get_user_insts();
         const auto skip_concat = users.size() == 1 && users.front()->get_node().is_type<concatenation>() && users.front()->get_node().is_runtime_skippable() &&
                                  users.front()->_allocation_done_by_other;
-        const auto skip_reset = skip_concat;
+        skip_reset = skip_concat;
+    }
+    // Need to reset crop's output to zeros, if followed by onednn concatenation that requires zero-padding for blocked format memory
+    if (get_node().is_type<crop>() && get_node().can_share_buffer() && _impl_params->get_output_layout(0).format.is_blocked() &&
+        get_node().get_users().size() == 1 && get_node().get_users().front()->is_type<concatenation>() &&
+        get_node().get_users().front()->get_selected_impl()->is_onednn()) {
+        skip_reset = false;
+    }
 
-        if (!skip_reset) {
-            for (const auto& output : _outputs) {
-                if (output != nullptr) {
-                    GPU_DEBUG_TRACE_DETAIL << id() << " : Resetting output memory (" << output->buffer_ptr() << ")" << std::endl;
-                    // Use marker to ensure proper synchronization for both events and barriers
-                    auto dep_events = out_of_order_queue ? std::vector<event::ptr>{get_network().get_stream().enqueue_marker(_impl_params->dep_events)}
-                                                         : std::vector<event::ptr>{};
-                    add_dep_event(output->fill(get_network().get_stream(), dep_events));
-                }
+    if (!skip_reset) {
+        for (const auto& output : _outputs) {
+            if (output != nullptr) {
+                GPU_DEBUG_TRACE_DETAIL << id() << " : Resetting output memory (" << output->buffer_ptr() << ")" << std::endl;
+                // Use marker to ensure proper synchronization for both events and barriers
+                auto dep_events = out_of_order_queue ? std::vector<event::ptr>{get_network().get_stream().enqueue_marker(_impl_params->dep_events)}
+                                                     : std::vector<event::ptr>{};
+                add_dep_event(output->fill(get_network().get_stream(), dep_events));
             }
         }
     }

src/plugins/intel_gpu/src/kernel_selector/cl_kernels/eltwise_blocked_opt.cl
@@ -61,7 +61,7 @@ KERNEL(eltwise_blocked_opt)(INPUTS_DECLS
     const uint f_block = (inner_f + outer_f * F_BLOCK_COUNT);
 
     // Feature axis of input tensor is smaller than inner block size : No need to calculate this block
-    if ((f_block*VEC_SIZE) > OUTPUT_FEATURE_NUM || b > OUTPUT_BATCH_NUM) {
+    if ((f_block*VEC_SIZE) >= OUTPUT_FEATURE_NUM || b > OUTPUT_BATCH_NUM) {
         return;
     }
 

src/plugins/intel_gpu/tests/unit/test_cases/concatenation_gpu_test.cpp
@@ -7,11 +7,12 @@
 #include "concatenation_inst.h"
 #include "permute_inst.h"
 
-#include <intel_gpu/primitives/input_layout.hpp>
 #include <intel_gpu/primitives/convolution.hpp>
+#include <intel_gpu/primitives/crop.hpp>
 #include <intel_gpu/primitives/data.hpp>
-#include <intel_gpu/primitives/reorder.hpp>
 #include <intel_gpu/primitives/grid_sample.hpp>
+#include <intel_gpu/primitives/input_layout.hpp>
+#include <intel_gpu/primitives/reorder.hpp>
 
 #include <algorithm>
 #include <cmath>
@@ -1662,6 +1663,122 @@ TEST(concat_gpu_onednn, basic_input_types) {
 
     for (size_t x = 0; x < output_layout.count(); ++x) {
         ASSERT_EQ(output_vec[x], output_ptr[x]);
+    }
+}
+
+TEST(concat_gpu_onednn, b_fs_yx_fsv16_input_types) {
+    auto& engine = get_test_engine();
+    if (!engine.get_device_info().supports_immad)
+        return;
+
+    tests::random_generator rg(GET_SUITE_NAME);
+    const int32_t input_b = 1, input_f = 88, input_y = 52, input_x = 52;
+    const int32_t input_fsv16 = 16;
+    const int32_t input_fs = input_f / input_fsv16 + 1;
+    const int32_t input_f_padding = input_fs * input_fsv16 - input_f;
+    auto test_dt = data_types::f16;
+    auto test_format = format::b_fs_yx_fsv16;
+
+    layout input0_layout = { test_dt, test_format, { input_b, input_f, input_x, input_y } };
+    layout input1_layout = { test_dt, test_format, { input_b, input_f, input_x, input_y } };
+    auto input0 = engine.allocate_memory(input0_layout);
+    auto input1 = engine.allocate_memory(input1_layout);
+
+    using test_data_type = ov::float16;
+    auto data_input0 = rg.generate_random_5d<test_data_type>(input_b, input_fs, input_y, input_x, input_fsv16, -1, 1);
+    auto data_input1 = rg.generate_random_5d<test_data_type>(input_b, input_fs, input_y, input_x, input_fsv16, -1, 1);
+    // zero-pad for the padding part
+    for(int b=0; b < input_b; ++b) {
+        for(int f=0; f < input_fs; ++f) {
+            for(int y=0; y < input_y; ++y) {
+                for(int x=0; x < input_x; ++x) {
+                    for(int v=0; v < input_fsv16; ++v) {
+                        if(f == input_fs-1 && v >= input_fsv16 - input_f_padding) {
+                            data_input0[b][f][y][x][v] = 0;
+                            data_input1[b][f][y][x][v] = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    auto data_input0_flat = flatten_5d(format::bfzyx, data_input0);
+    auto data_input1_flat = flatten_5d(format::bfzyx, data_input1);
+
+    set_values<test_data_type>(input0, data_input0_flat);
+    set_values<test_data_type>(input1, data_input1_flat);
+
+    auto output_name = "reorder";
+    layout reorder_layout = { test_dt, format::bfyx, { input_b, input_f * 2, input_x, input_y } };
+
+    topology topology(
+            input_layout("input0", input0_layout),
+            input_layout("input1", input1_layout),
+            concatenation("concat",
+                          { input_info("input0"), input_info("input1") },
+                          1,
+                          test_dt),
+            reorder("reorder", input_info("concat"), reorder_layout)
+    );
+
+    ov::intel_gpu::ImplementationDesc impl_test = { test_format, std::string(""), impl_types::onednn };
+    ov::intel_gpu::ImplementationDesc impl_ref = { test_format, std::string(""), impl_types::ocl };
+
+    ExecutionConfig cfg_test = get_test_default_config(engine);
+    cfg_test.set_property(ov::intel_gpu::custom_outputs(std::vector<std::string>{ output_name }));
+    cfg_test.set_property(ov::intel_gpu::force_implementations(ov::intel_gpu::ImplForcingMap{ {"concat", impl_test} }));
+
+    ExecutionConfig cfg_ref = get_test_default_config(engine);
+    cfg_ref.set_property(ov::intel_gpu::custom_outputs(std::vector<std::string>{ output_name }));
+    cfg_ref.set_property(ov::intel_gpu::force_implementations(ov::intel_gpu::ImplForcingMap{ {"concat", impl_ref} }));
+
+    network network_test(engine, topology, cfg_test);
+    network network_ref(engine, topology, cfg_ref);
+
+    network_test.set_input_data("input0", input0);
+    network_test.set_input_data("input1", input1);
+    network_ref.set_input_data("input0", input0);
+    network_ref.set_input_data("input1", input1);
+
+    auto outputs_test = network_test.execute();
+    auto outputs_ref = network_ref.execute();
+
+    ASSERT_EQ(outputs_test.size(), size_t(1));
+    ASSERT_EQ(outputs_test.begin()->first, output_name);
+    ASSERT_EQ(outputs_ref.size(), size_t(1));
+    ASSERT_EQ(outputs_ref.begin()->first, output_name);
+
+    auto output_memory_test = outputs_test.at(output_name).get_memory();
+    auto output_layout_test = output_memory_test->get_layout();
+    int y_size_test = output_layout_test.spatial(1);
+    int x_size_test = output_layout_test.spatial(0);
+    int f_size_test = output_layout_test.feature();
+    int b_size_test = output_layout_test.batch();
+
+    ASSERT_EQ(output_layout_test.format, format::bfyx);
+    ASSERT_EQ(y_size_test, input_y);
+    ASSERT_EQ(x_size_test, input_x);
+    ASSERT_EQ(f_size_test, input_f * 2);
+    ASSERT_EQ(b_size_test, input_b);
+
+    auto output_memory_ref = outputs_ref.at(output_name).get_memory();
+    auto output_layout_ref = output_memory_ref->get_layout();
+    int y_size_ref = output_layout_ref.spatial(1);
+    int x_size_ref = output_layout_ref.spatial(0);
+    int f_size_ref = output_layout_ref.feature();
+    int b_size_ref = output_layout_ref.batch();
+
+    ASSERT_EQ(output_layout_ref.format, format::bfyx);
+    ASSERT_EQ(y_size_ref, input_y);
+    ASSERT_EQ(x_size_ref, input_x);
+    ASSERT_EQ(f_size_ref, input_f * 2);
+    ASSERT_EQ(b_size_ref, input_b);
+
+    cldnn::mem_lock<test_data_type> output_ptr_test(output_memory_test, get_test_stream());
+    cldnn::mem_lock<test_data_type> output_ptr_ref(output_memory_ref, get_test_stream());
+
+    for (size_t x = 0; x < output_layout_test.count(); ++x) {
+        ASSERT_EQ(output_ptr_test[x], output_ptr_ref[x]);
     }
 }
 

src/plugins/intel_gpu/tests/unit/test_cases/crop_gpu_test.cpp
@@ -1393,6 +1393,106 @@ TEST(crop_cpu, basic_in2x3x2x2_crop_all_bfyx_disable_usm) {
     }
 }
 
+TEST(crop_gpu, basic_in1x176x52x52_crop_b_fs_yx_fsv16) {
+    tests::random_generator rg(GET_SUITE_NAME);
+    auto& engine = get_test_engine();
+
+    auto batch_num = 1;
+    auto feature_num = 176;
+    auto y_size = 52;
+    auto x_size = 52;
+    auto F_BLOCK = 16;
+    auto fs_num = feature_num / F_BLOCK;
+    auto feature_num_half = feature_num / 2;
+    auto feature_offset = feature_num_half;
+    auto feature_blocks = (feature_num_half + F_BLOCK - 1) / F_BLOCK;
+
+    auto test_dt = data_types::f16;
+    auto test_format = format::b_fs_yx_fsv16;
+    using test_data_type = ov::float16;
+
+    layout input0_layout = { test_dt, test_format, { batch_num, feature_num, x_size, y_size } };
+    auto input0 = engine.allocate_memory(input0_layout);
+
+    auto data_input = rg.generate_random_5d<test_data_type>(batch_num, fs_num, y_size, x_size, F_BLOCK, -1, 1);
+    auto data_input_flat = flatten_5d(format::bfzyx, data_input);
+    set_values<test_data_type>(input0, data_input_flat);
+
+    topology topology(
+            input_layout("input0", input0_layout),
+            crop("crop1", input_info("input0"), {batch_num, feature_num_half, x_size, y_size }, {0, 0, 0, 0}),
+            crop("crop2", input_info("input0"), {batch_num, feature_num_half, x_size, y_size }, {0, feature_offset, 0, 0})
+    );
+
+    ExecutionConfig config = get_test_default_config(engine);
+    config.set_property(ov::intel_gpu::optimize_data(true));
+    config.set_property(ov::intel_gpu::custom_outputs(topology.get_primitives_ids()));
+
+    network network(engine, topology, config);
+    network.set_input_data("input0", input0);
+    auto outputs = network.execute();
+
+    auto output = outputs.at("crop1").get_memory();
+    cldnn::mem_lock<test_data_type> output_ptr(output, get_test_stream());
+
+    auto offset = 0;
+    for (auto b = 0; b < batch_num; b++) {
+        for (auto fs = 0; fs < feature_blocks; fs++) {
+            for (auto y = 0; y < y_size; y++) {
+                for (auto x = 0; x < x_size; x++) {
+                    for (auto fsv = 0; fsv < F_BLOCK; fsv++) {
+                        offset = b * feature_blocks * y_size * x_size * F_BLOCK +
+                                 fs * y_size * x_size * F_BLOCK +
+                                 y * x_size * F_BLOCK +
+                                 x * F_BLOCK +
+                                 fsv;
+                        if (fs == feature_blocks -1 && fsv >= F_BLOCK / 2) {
+                            ASSERT_EQ(output_ptr[offset], 0);
+                        } else {
+                            ASSERT_EQ(output_ptr[offset], data_input_flat[offset]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    auto output_2 = outputs.at("crop2").get_memory();
+    cldnn::mem_lock<test_data_type> output_ptr_2(output_2, get_test_stream());
+
+    auto f = 0, fs_orig = 0, fsv_orig = 0, offset_orig = 0;
+    for (auto b = 0; b < batch_num; b++) {
+        for (auto fs = 0; fs < feature_blocks; fs++) {
+            for (auto y = 0; y < y_size; y++) {
+                for (auto x = 0; x < x_size; x++) {
+                    for (auto fsv = 0; fsv < F_BLOCK; fsv++) {
+                        offset = b * feature_blocks * y_size * x_size * F_BLOCK +
+                                 fs * y_size * x_size * F_BLOCK +
+                                 y * x_size * F_BLOCK +
+                                 x * F_BLOCK +
+                                 fsv;
+
+                        f = fs * F_BLOCK + fsv;
+                        fs_orig = (f + feature_offset) / F_BLOCK;
+                        fsv_orig = (f + feature_offset) % F_BLOCK;
+                        offset_orig = b * feature_blocks * y_size * x_size * F_BLOCK +
+                                 fs_orig * y_size * x_size * F_BLOCK +
+                                 y * x_size * F_BLOCK +
+                                 x * F_BLOCK +
+                                 fsv_orig;
+
+                        if (fs == feature_blocks -1 && fsv >= F_BLOCK / 2) {
+                            ASSERT_EQ(output_ptr_2[offset], 0);
+                        } else {
+                            ASSERT_EQ(output_ptr_2[offset], data_input_flat[offset_orig]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 TEST(crop_gpu, dynamic_in1x4x1x1_split) {
     auto& engine = get_test_engine();
 

commit 0502a72aee0bed629e2418fa0d603ca3f1b75580

[GPU] fix performance drop for crop in yolo11 model (#32062)
### Details
- Fix performance regression introduced by PR
https://github.com/openvinotoolkit/openvino/pull/31140.
 
### Description of the issue

#### Symptom
manual_yolo11 model performance dropped from 362.4 FPS to 318.25 FPS on
GPU.

#### Root cause
- previous PR will force all crop primitives followed by onednn
concatenation to clean its GPU memory by filling with zeros if it is
blocked format.
- manual_yolo11 model also has many such crop primitives, so its
performance will drop.

#### How to fix it
- Found that filling GPU memory with zeros can be skipped if the crop
primitive uses eltwise_blocked_opt kernel and is not dynamic, so just
skip it by checking crop primitive's kernel name.

#### The code and line that caused this issue

https://github.com/openvinotoolkit/openvino/blob/453c8ee337f4a1cadebb66551bb40d6a216c1001/src/plugins/intel_gpu/src/graph/primitive_inst.cpp#L2036

#### Reproduction step and snapshot
- benchmark_app
`benchmark_app -inference_only false -b 1 -t 60 -nireq 4 -d GPU.0 -hint
none -nstreams 2 -m INT8/1/ov/optimized/manual_yolo11.xml`

#### Problematic graph
- crop primitive (eltwise_blocked_opt kernel) followed by onednn
concatenation in manual_yolo11
<img width="570" height="616" alt="image"
src="https://github.com/user-attachments/assets/c3c0b598-c147-4d23-940d-9a4ac9b4649e"
/>

#### Checklist 
 - [x] Is it a proper fix? (not a workaround) 
 - [ ] Did you include test case for this fix, if necessary? No need
- [ ] Did you review existing test that can be extended to cover this
scenario? Which test did you review?
 
### Tickets:
 - CVS-173402

---------

Signed-off-by: yuan.xiong <yuan.xiong@intel.com>

src/plugins/intel_gpu/src/graph/primitive_inst.cpp
@@ -2036,7 +2036,9 @@ void primitive_inst::prepare_primitive() {
     if (get_node().is_type<crop>() && get_node().can_share_buffer() && _impl_params->get_output_layout(0).format.is_blocked() &&
         get_node().get_users().size() == 1 && get_node().get_users().front()->is_type<concatenation>() &&
         get_node().get_users().front()->get_selected_impl() && get_node().get_users().front()->get_selected_impl()->is_onednn()) {
-        skip_reset = false;
+        if (get_node().get_selected_impl()->get_kernel_name().find("eltwise_blocked_opt") == std::string::npos || is_dynamic()) {
+            skip_reset = false;
+        }
     }
 
     if (!skip_reset) {