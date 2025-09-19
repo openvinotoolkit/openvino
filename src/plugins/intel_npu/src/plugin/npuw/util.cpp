openvinotoolkit/openvino

commit 6dce178b0fe59bc88fcb927b3c520c3e268339f5

Transpose/Permute/Concat/To_f16 optimization (#30899)
### Details:

Optimize transpose/permute/concat/to_f16 function with AVX2 instruction.

### Tickets:
[EISW-143728](https://jira.devtools.intel.com/browse/EISW-143728)

---------

Co-authored-by: Eugene Smirnov <eugene.smirnov@intel.com>

src/plugins/intel_npu/src/plugin/npuw/util.cpp
@@ -633,43 +633,25 @@ ov::Tensor ov::npuw::util::transpose(const ov::Tensor& t) {
 
     const auto IN_ROWS = shape[0] * shape[1];
     const auto IN_COLS = shape[2];
-    for (std::size_t i = 0; i < IN_ROWS; i++) {
-        for (std::size_t j = 0; j < IN_COLS; j++) {
-            switch (t.get_element_type()) {
-            case ov::element::i4:
-                twrite_4b(tnew, tread_4b(t, i, j, IN_COLS), j, i, IN_ROWS);
-                break;
-            case ov::element::f32:
-                twrite<float>(tnew, tread<float>(t, i, j, IN_COLS), j, i, IN_ROWS);
-                break;
-            default:
-                NPUW_ASSERT(false && "Element type is not supported yet");
-            }
-        }
+
+    switch (t.get_element_type()) {
+    case ov::element::i4: {
+        NPUW_ASSERT(shape[2] % 2 == 0);
+        const uint8_t* src = static_cast<const uint8_t*>(t.data());
+        uint8_t* dst = static_cast<uint8_t*>(tnew.data());
+        ov::npuw::util::XARCH::transpose_i4(src, dst, IN_ROWS, IN_COLS);
+        break;
+    }
+    case ov::element::f32: {
+        const float* src = static_cast<const float*>(t.data());
+        float* dst = static_cast<float*>(tnew.data());
+        ov::npuw::util::XARCH::transpose_f32(src, dst, IN_ROWS, IN_COLS);
+        break;
+    }
+    default:
+        NPUW_ASSERT(false && "Element type is not supported yet");
     }
     return tnew;
-}
-
-template <typename T>
-void permute120(const ov::Tensor& src, ov::Tensor& dst) {
-    const ov::Shape src_shape = src.get_shape();
-    const ov::Shape dst_shape = dst.get_shape();
-    NPUW_ASSERT(src_shape.size() == 3);  // Yes, so far only transpose 3D tensors
-
-    const T* pSrc = static_cast<const T*>(src.data());
-    T* pDst = static_cast<T*>(dst.data());
-
-    // DSTs [b,r,c] map to SRC's [r,c,b]
-
-    for (std::size_t b = 0; b < dst_shape[0]; b++) {
-        for (std::size_t r = 0; r < dst_shape[1]; r++) {
-            for (std::size_t c = 0; c < dst_shape[2]; c++) {
-                auto dst_idx = b * dst_shape[1] * dst_shape[2] + r * dst_shape[2] + c;
-                auto src_idx = r * src_shape[1] * src_shape[2] + c * src_shape[1] + b;
-                pDst[dst_idx] = pSrc[src_idx];
-            }
-        }
-    }
 }
 
 ov::Tensor ov::npuw::util::permute(const ov::Tensor& t, const std::vector<std::size_t>& axes) {
@@ -683,76 +665,90 @@ ov::Tensor ov::npuw::util::permute(const ov::Tensor& t, const std::vector<std::s
                     t.get_element_type() == ov::element::f16);
         ov::Shape tshape = {shape[0], shape[2], shape[1]};
         ov::Tensor tnew(t.get_element_type(), tshape);
-
-        for (std::size_t p = 0; p < shape[0]; p++) {
-            for (std::size_t r = 0; r < shape[1]; r++) {
-                for (std::size_t c = 0; c < shape[2]; c++) {
-                    switch (t.get_element_type()) {
-                    case ov::element::i4:
-                        twrite_4b(tnew, tread_4b(t, p * shape[1] + r, c, shape[2]), p * shape[2] + c, r, shape[1]);
-                        break;
-                    case ov::element::f32:
-                        twrite<float>(tnew,
-                                      tread<float>(t, p * shape[1] + r, c, shape[2]),
-                                      p * shape[2] + c,
-                                      r,
-                                      shape[1]);
-                        break;
-                    case ov::element::f16:
-                        twrite<uint16_t>(tnew,
-                                         tread<uint16_t>(t, p * shape[1] + r, c, shape[2]),
-                                         p * shape[2] + c,
-                                         r,
-                                         shape[1]);
-                        break;
-                    default:
-                        NPUW_ASSERT(false && "Element type is not supported yet");
-                    }
-                }
-            }
+        switch (t.get_element_type()) {
+        case ov::element::i4: {
+            NPUW_ASSERT(shape[2] % 2 == 0);
+            const uint8_t* src = static_cast<const uint8_t*>(t.data());
+            uint8_t* dst = static_cast<uint8_t*>(tnew.data());
+            ov::parallel_for(shape[0], [&](size_t p) {
+                const uint8_t* src_ptr = src + p * shape[1] * shape[2];
+                uint8_t* dst_ptr = dst + p * shape[1] * shape[2];
+                ov::npuw::util::XARCH::transpose_i4(src_ptr, dst_ptr, shape[1], shape[2]);
+            });
+            break;
+        }
+        case ov::element::f32: {
+            const float* src = static_cast<const float*>(t.data());
+            float* dst = static_cast<float*>(tnew.data());
+            ov::parallel_for(shape[0], [&](size_t p) {
+                const float* src_ptr = src + p * shape[1] * shape[2];
+                float* dst_ptr = dst + p * shape[1] * shape[2];
+                ov::npuw::util::XARCH::transpose_f32(src_ptr, dst_ptr, shape[1], shape[2]);
+            });
+            break;
+        }
+        case ov::element::f16: {
+            const uint16_t* src = static_cast<const uint16_t*>(t.data());
+            uint16_t* dst = static_cast<uint16_t*>(tnew.data());
+            ov::parallel_for(shape[0], [&](size_t p) {
+                const uint16_t* src_ptr = src + p * shape[1] * shape[2];
+                uint16_t* dst_ptr = dst + p * shape[1] * shape[2];
+                ov::npuw::util::XARCH::transpose_f16(src_ptr, dst_ptr, shape[1], shape[2]);
+            });
+            break;
+        }
+        default:
+            NPUW_ASSERT(false && "Element type is not supported yet");
         }
         return tnew;
     } else if (axes[0] == 1 && axes[1] == 0 && axes[2] == 2) {
         NPUW_ASSERT(t.get_element_type() == ov::element::i4 || t.get_element_type() == ov::element::f16);
         ov::Shape tshape = {shape[1], shape[0], shape[2]};
         ov::Tensor tnew(t.get_element_type(), tshape);
-
-        // Iterate over output tensor coordinates
-        for (std::size_t p = 0; p < tshape[0]; p++) {
-            for (std::size_t r = 0; r < tshape[1]; r++) {
-                for (std::size_t c = 0; c < tshape[2]; c++) {
-                    switch (t.get_element_type()) {
-                    case ov::element::i4:
-                        twrite_4b(tnew,
-                                  tread_4b(t, r, p * shape[2] + c, shape[1] * shape[2]),
-                                  p * tshape[1] + r,
-                                  c,
-                                  tshape[2]);
-                        break;
-                    case ov::element::f16:
-                        twrite<uint16_t>(tnew,
-                                         tread<uint16_t>(t, r, p * shape[2] + c, shape[1] * shape[2]),
-                                         p * tshape[1] + r,
-                                         c,
-                                         tshape[2]);
-                        break;
-                    default:
-                        NPUW_ASSERT(false && "Element type is not supported yet");
-                    }
+        switch (t.get_element_type()) {
+        case ov::element::i4: {
+            NPUW_ASSERT(shape[2] % 2 == 0);
+            const uint8_t* src = static_cast<const uint8_t*>(t.data());
+            uint8_t* dst = static_cast<uint8_t*>(tnew.data());
+            for (size_t p = 0; p < shape[0]; ++p) {
+                for (size_t r = 0; r < shape[1]; ++r) {
+                    std::copy_n(&src[(p * shape[1] * shape[2] + r * shape[2]) / 2],
+                                shape[2] / 2,
+                                &dst[(r * shape[0] * shape[2] + p * shape[2]) / 2]);
                 }
             }
+            break;
+        }
+        case ov::element::f16: {
+            const uint16_t* src = static_cast<const uint16_t*>(t.data());
+            uint16_t* dst = static_cast<uint16_t*>(tnew.data());
+            ov::parallel_for2d(shape[0], shape[1], [&](size_t p, size_t r) {
+                const size_t src_off = (p * shape[1] + r) * shape[2];
+                const size_t dst_off = (r * shape[0] + p) * shape[2];
+                std::copy_n(src + src_off, shape[2], dst + dst_off);
+            });
+            break;
+        }
+        default:
+            NPUW_ASSERT(false && "Element type is not supported yet");
         }
         return tnew;
     } else if (axes[0] == 1 && axes[1] == 2 && axes[2] == 0) {
         ov::Shape tshape = {shape[1], shape[2], shape[0]};
         ov::Tensor tnew(t.get_element_type(), tshape);
         switch (t.get_element_type()) {
-        case ov::element::f32:
-            permute120<uint32_t>(t, tnew);
+        case ov::element::f32: {
+            const float* src = static_cast<const float*>(t.data());
+            float* dst = static_cast<float*>(tnew.data());
+            ov::npuw::util::XARCH::transpose_f32(src, dst, shape[0], shape[1] * shape[2]);
             break;
-        case ov::element::f16:
-            permute120<uint16_t>(t, tnew);
+        }
+        case ov::element::f16: {
+            const uint16_t* src = static_cast<const uint16_t*>(t.data());
+            uint16_t* dst = static_cast<uint16_t*>(tnew.data());
+            ov::npuw::util::XARCH::transpose_f16(src, dst, shape[0], shape[1] * shape[2]);
             break;
+        }
         default:
             NPUW_ASSERT(false && "Element type is not supported yet");
         }
@@ -768,7 +764,6 @@ ov::Tensor ov::npuw::util::concat(const std::vector<ov::Tensor>& tt, std::size_t
     const auto type = tt.front().get_element_type();
     auto shape = tt.front().get_shape();
     std::size_t new_dim = 0;
-
     std::vector<std::size_t> offsets;
     std::vector<std::size_t> lens;
     for (auto&& t : tt) {
@@ -793,24 +788,26 @@ ov::Tensor ov::npuw::util::concat(const std::vector<ov::Tensor>& tt, std::size_t
         uint8_t* pDst = static_cast<uint8_t*>(tnew.data());
 
         const bool is_4bit = (type == ov::element::i4 || type == ov::element::u4);
-        for (std::size_t t_idx = 0; t_idx < tt.size(); t_idx++) {
+        ov::parallel_for(tt.size(), [&](size_t t_idx) {
             const uint8_t* pSrc = static_cast<const uint8_t*>(tt[t_idx].data());
 
             const auto copy_size = lens[t_idx] * shape[1] * shape[2];
             const auto copy_len = is_4bit ? copy_size / 2 : copy_size * type.size();
 
-            std::copy_n(pSrc, copy_len, pDst);
-            pDst += copy_len;
-        }
+            const auto dst_offset =
+                is_4bit ? offsets[t_idx] * shape[1] * shape[2] / 2 : offsets[t_idx] * shape[1] * shape[2] * type.size();
+            uint8_t* pDstIdx = pDst + dst_offset;
+
+            std::copy_n(pSrc, copy_len, pDstIdx);
+        });
         return tnew;
     } else if (axis == 2) {
         ov::Tensor tnew(tt.front().get_element_type(), shape);
         uint8_t* pDst = static_cast<uint8_t*>(tnew.data());
 
         const bool is_4bit = (type == ov::element::i4 || type == ov::element::u4);
-        for (std::size_t t_idx = 0; t_idx < tt.size(); t_idx++) {
+        ov::parallel_for(tt.size(), [&](size_t t_idx) {
             const auto& t_src = tt[t_idx];
-
             for (std::size_t r = 0; r < shape[0] * shape[1]; r++) {
                 const auto r_offset = is_4bit ? new_dim * r / 2 : new_dim * r * type.size();
                 const auto c_offset = is_4bit ? offsets[t_idx] / 2 : offsets[t_idx] * type.size();
@@ -823,7 +820,7 @@ ov::Tensor ov::npuw::util::concat(const std::vector<ov::Tensor>& tt, std::size_t
 
                 std::copy_n(pSrcRow, copy_len, pDstRow);
             }
-        }
+        });
         return tnew;
     } else {
         NPUW_ASSERT(false && "Not supported yet");

src/plugins/intel_npu/tests/unit/npuw/permute.cpp
@@ -1,0 +1,256 @@
+// Copyright (C) 2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#ifdef HAVE_AVX2
+#    include "permute.hpp"
+
+namespace details {
+
+// Permute function.
+template <typename T, bool Int4 = false>
+void permute(const T* input, T* output, const std::vector<size_t>& dims, const std::vector<size_t>& order) {
+    static_assert(!Int4 || (Int4 && std::is_same_v<T, uint8_t>), "For int4 data input data must be of type uint8_t");
+
+    size_t ndim = dims.size();
+    assert(order.size() == ndim);
+
+    // Compute new dims.
+    std::vector<size_t> new_dims = details::reorder(dims, order);
+
+    // Compute strides.
+    std::vector<size_t> old_strides = details::compute_strides(dims);
+    std::vector<size_t> new_strides = details::compute_strides(new_dims);
+
+    size_t total = std::accumulate(dims.begin(), dims.end(), size_t(1), std::multiplies<size_t>());
+
+    // For each index in output, compute corresponding input index.
+    // Use a flat index and unravel it to multi-index in new order, then map to old order.
+    for (size_t out_idx = 0; out_idx < total; ++out_idx) {
+        // Unravel out_idx to multi-index in new_dims.
+        std::vector<size_t> new_multi(ndim);
+        size_t tmp = out_idx;
+        for (size_t i = 0; i < ndim; ++i) {
+            new_multi[i] = tmp / new_strides[i];
+            tmp = tmp % new_strides[i];
+        }
+        // Map new_multi to old_multi.
+        std::vector<size_t> old_multi(ndim);
+        for (size_t i = 0; i < ndim; ++i)
+            old_multi[order[i]] = new_multi[i];
+
+        // Compute flat input index.
+        size_t in_idx = 0;
+        for (size_t i = 0; i < ndim; ++i)
+            in_idx += old_multi[i] * old_strides[i];
+
+        if constexpr (Int4) {
+            uint8_t in_byte = input[in_idx / 2];
+            uint8_t in_val = (in_idx % 2 == 0) ? (in_byte & 0x0F) : ((in_byte >> 4) & 0x0F);
+
+            size_t out_byte_idx = out_idx / 2;
+            if (out_idx % 2 == 0) {
+                output[out_byte_idx] = (output[out_byte_idx] & 0xF0) | (in_val & 0x0F);
+            } else {
+                output[out_byte_idx] = (output[out_byte_idx] & 0x0F) | ((in_val & 0x0F) << 4);
+            }
+        } else {
+            output[out_idx] = input[in_idx];
+        }
+    }
+}
+
+}  // namespace details
+
+namespace {
+
+void PermuteTestsBase::make_input() {
+    size_t nElements = shape_size(input_shape);
+
+    ASSERT_EQ((type.bitwidth() * nElements) % 8, 0)
+        << "Input len has to be byte boundary aligned, but was " << type.bitwidth() * nElements << " bits";
+    const size_t nBytes = type.bitwidth() * nElements / 8;
+
+    input.resize(nBytes);
+    ref_output.resize(nBytes);
+    output.resize(nBytes);
+
+    std::fill(ref_output.begin(), ref_output.end(), 0);
+    std::fill(output.begin(), output.end(), 0);
+
+    std::mt19937 rng(42);  // Fixed seed for reproducibility
+    std::uniform_int_distribution<int> dist(0, 100);
+
+    for (size_t i = 0; i < nBytes; ++i) {
+        input[i] = static_cast<int8_t>(dist(rng));
+    }
+
+    inTensor = ov::Tensor(type, input_shape, input.data());
+}
+
+void PermuteTestsBase::SetUp(const PermuteTestsParams& getParam) {
+    ShapesInitializer shapeInit;
+
+    std::tie(type, shapeInit, axes) = getParam;
+
+    std::vector<int> input;
+    shapeInit(input);
+
+    input_shape = ov::Shape{input.begin(), input.end()};
+
+    make_input();
+
+    make_ref_output();
+}
+
+std::string PermuteTestsBase::ToString() const {
+    std::ostringstream result;
+    result << (isNegative() ? "NEGATIVE_" : "") << "[";
+
+    for (size_t i = 0; i != input_shape.size(); i++) {
+        result << input_shape[i] << ((i + 1 == input_shape.size()) ? "" : "x");
+    }
+    result << "]"
+           << "_type_" << type;
+
+    result << "_axis";
+
+    for (size_t i = 0; i < axes.size(); i++) {
+        result << "_" << axes[i];
+    }
+
+    return result.str();
+}
+
+/**
+ * Negative test cases has to be carefully reviewed, to still remain positive runs at some points
+ * @return
+ */
+bool PermuteTestsBase::isNegative() const {
+    return false;
+}
+
+void PermuteTestsBase::make_ref_output() {
+    std::vector<size_t> dims(input_shape.begin(), input_shape.end());
+    if (type == ov::element::f16) {
+        details::permute<uint16_t>(reinterpret_cast<const uint16_t*>(input.data()),
+                                   reinterpret_cast<uint16_t*>(ref_output.data()),
+                                   dims,
+                                   axes);
+    } else if (type == ov::element::f32) {
+        details::permute<float>(reinterpret_cast<const float*>(input.data()),
+                                reinterpret_cast<float*>(ref_output.data()),
+                                dims,
+                                axes);
+    } else if (type == ov::element::i4) {
+        details::permute<uint8_t, true>(reinterpret_cast<const uint8_t*>(input.data()),
+                                        reinterpret_cast<uint8_t*>(ref_output.data()),
+                                        dims,
+                                        axes);
+    } else {
+        throw std::runtime_error("Unsupported element type in PermuteTestsBase::make_ref_output");
+    }
+}
+
+TEST_P(PermuteTests, permute) {
+    ASSERT_NO_THROW_WITH_MESSAGE(outTensor = ov::npuw::util::permute(inTensor, axes));
+    int8_t* dst = static_cast<int8_t*>(outTensor.data());
+    output = std::vector<int8_t>(dst, dst + output.size());
+    ASSERT_TRUE(details::ArraysMatch(output, ref_output));
+}
+
+const auto TestCases201 = ::testing::Combine(
+        ::testing::ValuesIn({ov::element::Type_t::i4, ov::element::Type_t::f32}),
+        ::details::ShapesIn({Tensors{input={1, 2, 4};
+}
+, Tensors {
+    input = {1, 10, 128};
+}
+, Tensors {
+    input = {1, 16, 256};
+}
+, Tensors {
+    input = {1, 16, 300};
+}
+}),
+::testing::ValuesIn(
+    {
+    std::vector<std::size_t>({2, 0, 1})
+}
+)
+);
+
+INSTANTIATE_TEST_SUITE_P(PermuteTests201, PermuteTests, TestCases201, PermuteTests::getTestCaseName);
+
+const auto TestCases021 = ::testing::Combine(
+        ::testing::ValuesIn({ov::element::Type_t::i4, ov::element::Type_t::f16, ov::element::Type_t::f32}),
+        ::details::ShapesIn({Tensors{input={1, 10, 16};
+}
+, Tensors {
+    input = {1, 10, 128};
+}
+, Tensors {
+    input = {1, 16, 256};
+}
+, Tensors {
+    input = {1, 16, 300};
+}
+}),
+::testing::ValuesIn(
+    {
+    std::vector<std::size_t>({0, 2, 1})
+}
+)
+);
+
+INSTANTIATE_TEST_SUITE_P(PermuteTests021, PermuteTests, TestCases021, PermuteTests::getTestCaseName);
+
+const auto TestCases102 = ::testing::Combine(
+        ::testing::ValuesIn({ov::element::Type_t::i4, ov::element::Type_t::f16}),
+        ::details::ShapesIn({Tensors{input={1, 10, 16};
+}
+, Tensors {
+    input = {1, 10, 128};
+}
+, Tensors {
+    input = {1, 16, 256};
+}
+, Tensors {
+    input = {1, 16, 300};
+}
+}),
+::testing::ValuesIn(
+    {
+    std::vector<std::size_t>({1, 0, 2})
+}
+)
+);
+
+INSTANTIATE_TEST_SUITE_P(PermuteTests102, PermuteTests, TestCases102, PermuteTests::getTestCaseName);
+
+const auto TestCases120 = ::testing::Combine(
+        ::testing::ValuesIn({ov::element::Type_t::f16, ov::element::Type_t::f32}),
+        ::details::ShapesIn({Tensors{input={1, 10, 16};
+}
+, Tensors {
+    input = {1, 10, 128};
+}
+, Tensors {
+    input = {1, 16, 256};
+}
+, Tensors {
+    input = {1, 16, 300};
+}
+}),
+::testing::ValuesIn(
+    {
+    std::vector<std::size_t>({1, 2, 0})
+}
+)
+);
+
+INSTANTIATE_TEST_SUITE_P(PermuteTests120, PermuteTests, TestCases120, PermuteTests::getTestCaseName);
+
+}  // anonymous namespace
+
+#endif  // __AVX2__

src/plugins/intel_npu/src/plugin/CMakeLists.txt
@@ -50,7 +50,7 @@ cross_compiled_file(${TARGET_NAME}
         ARCH AVX2 ANY
                     npuw/util_xarch.cpp
         API         npuw/util_xarch.hpp
-        NAME        unpack_i4i8 unpack_u4i8 unpack_i4f16 unpack_i4f16_scale unpack_i4f16_z unpack_u4f16 unpack_u4f16_scale_zp unpack_u4f16_asymm_zp unpack_u4f16_z unpack_u4f32 unpack_i8f16 unpack_i8f16_scale unpack_u8f16 to_f16 copy_row_as_column
+        NAME        unpack_i4i8 unpack_u4i8 unpack_i4f16 unpack_i4f16_scale unpack_i4f16_z unpack_u4f16 unpack_u4f16_scale_zp unpack_u4f16_asymm_zp unpack_u4f16_z unpack_u4f32 unpack_i8f16 unpack_i8f16_scale unpack_u8f16 to_f16 copy_row_as_column transpose_i4 transpose_f16 transpose_f32
         NAMESPACE   ov::npuw::util::XARCH
 )
 

commit 52c8d8d591a32800a1c00ad9ea6df3e35838532c

[NPUW] Fixed oob in case of permute_021 int4 and num_channels > 1 (#32100)
### Details:
- regression on GPTQ model caused by PR:
https://github.com/openvinotoolkit/openvino/pull/30899
- also fixed compilation issue dueto msbuild: C2668 ambiguous fpclassify
invocation [this is optional]

### Tickets:
 - E-143728

---------

Co-authored-by: Alexander Kalistratov <alexander.kalistratov@intel.com>

src/plugins/intel_npu/src/plugin/npuw/util.cpp
@@ -671,8 +671,8 @@ ov::Tensor ov::npuw::util::permute(const ov::Tensor& t, const std::vector<std::s
             const uint8_t* src = static_cast<const uint8_t*>(t.data());
             uint8_t* dst = static_cast<uint8_t*>(tnew.data());
             ov::parallel_for(shape[0], [&](size_t p) {
-                const uint8_t* src_ptr = src + p * shape[1] * shape[2];
-                uint8_t* dst_ptr = dst + p * shape[1] * shape[2];
+                const uint8_t* src_ptr = src + p * shape[1] * shape[2] / 2;
+                uint8_t* dst_ptr = dst + p * shape[1] * shape[2] / 2;
                 ov::npuw::util::XARCH::transpose_i4(src_ptr, dst_ptr, shape[1], shape[2]);
             });
             break;

src/plugins/intel_npu/tests/unit/npuw/permute.cpp
@@ -195,6 +195,9 @@ const auto TestCases021 = ::testing::Combine(
 , Tensors {
     input = {1, 16, 300};
 }
+, Tensors {
+    input = {32,128,1288};
+}
 }),
 ::testing::ValuesIn(
     {

src/tests/test_utils/common_test_utils/src/ov_tensor_utils.cpp
@@ -367,11 +367,21 @@ inline bool less_or_equal(double a, double b) {
 }
 
 template <typename T>
+inline constexpr bool is_ov_integral =
+    std::is_same_v<T, char> || std::is_same_v<T, int8_t> || std::is_same_v<T, int16_t> || std::is_same_v<T, int32_t> ||
+    std::is_same_v<T, int64_t> || std::is_same_v<T, uint8_t> || std::is_same_v<T, uint16_t> ||
+    std::is_same_v<T, uint32_t> || std::is_same_v<T, uint64_t>;
+
+template <typename T>
 inline bool value_is_out_of_limits(T value, bool upper_bound_check) {
-    bool out_of_limits = std::isnan(value) || std::isinf(value);
-    out_of_limits |=
-        upper_bound_check ? value >= std::numeric_limits<T>::max() : value <= std::numeric_limits<T>::lowest();
-    return out_of_limits;
+    if constexpr (is_ov_integral<T>) {
+        return false;
+    } else {
+        bool out_of_limits = std::isnan(value) || std::isinf(value);
+        out_of_limits |=
+            upper_bound_check ? value >= std::numeric_limits<T>::max() : value <= std::numeric_limits<T>::lowest();
+        return out_of_limits;
+    }
 }
 
 template <typename T1, typename T2>