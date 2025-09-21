52626aaa9357a3863267e1e5d200b5e9e8a438e7
sunxiaoxia2022 <xiaoxia.sun@intel.com>
2025-09-21 20:27:08 +0800

    rm unused header

--- src/plugins/intel_cpu/src/graph_context.cpp
+++ src/plugins/intel_cpu/src/graph_context.cpp
@@ -18,7 +18,6 @@
 #include "openvino/runtime/threading/cpu_streams_executor.hpp"
 #include "openvino/runtime/threading/istreams_executor.hpp"
 #include "sub_memory_manager.hpp"
-#include "thread_pool_imp.hpp"
 #include "weights_cache.hpp"
 
 namespace ov::intel_cpu {
