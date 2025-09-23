860eb28fd118f01e3ae6c858353b13278ff1e2b5
Arseniy Obolenskiy <arseniy.obolenskiy@intel.com>
2025-09-23 10:53:48 +0200

    build issue fix

--- src/plugins/intel_cpu/src/itt.h
+++ src/plugins/intel_cpu/src/itt.h
@@ -23,15 +23,15 @@ OV_ITT_DOMAIN(ov_intel_cpu_LT);
 }  // namespace ov::intel_cpu::itt::domains
 
 #if defined(SELECTIVE_BUILD_ANALYZER)
-#    define CPU_LPT_SCOPE(region)             OV_SCOPE(ov_intel_cpu, region)
-#    define CPU_GRAPH_OPTIMIZER_SCOPE(region) OV_SCOPE(ov_intel_cpu, region)
+#    define CPU_LPT_SCOPE(region)             OV_SCOPE(intel_cpu, region)
+#    define CPU_GRAPH_OPTIMIZER_SCOPE(region) OV_SCOPE(intel_cpu, region)
 #elif defined(SELECTIVE_BUILD)
 #    define CPU_LPT_SCOPE(region)                                          \
-        if (OV_CC_SCOPE_IS_ENABLED(OV_PP_CAT3(ov_intel_cpu, _, region)) == 0) \
-        OPENVINO_THROW(std::string(OV_PP_TOSTRING(OV_PP_CAT3(ov_op_exec, _, region))), " is disabled!")
+        if (OV_CC_SCOPE_IS_ENABLED(OV_PP_CAT3(intel_cpu, _, region)) == 0) \
+        OPENVINO_THROW(std::string(OV_PP_TOSTRING(OV_PP_CAT3(intel_cpu, _, region))), " is disabled!")
 #    define CPU_GRAPH_OPTIMIZER_SCOPE(region)                              \
-        if (OV_CC_SCOPE_IS_ENABLED(OV_PP_CAT3(ov_intel_cpu, _, region)) == 0) \
-        OPENVINO_THROW(std::string(OV_PP_TOSTRING(OV_PP_CAT3(ov_intel_cpu, _, region))), " is disabled!")
+        if (OV_CC_SCOPE_IS_ENABLED(OV_PP_CAT3(intel_cpu, _, region)) == 0) \
+        OPENVINO_THROW(std::string(OV_PP_TOSTRING(OV_PP_CAT3(intel_cpu, _, region))), " is disabled!")
 #else
 #    define CPU_LPT_SCOPE(region) OV_ITT_SCOPED_TASK(ov::intel_cpu::itt::domains::ov_intel_cpu, OV_PP_TOSTRING(region))
 #    define CPU_GRAPH_OPTIMIZER_SCOPE(region)
