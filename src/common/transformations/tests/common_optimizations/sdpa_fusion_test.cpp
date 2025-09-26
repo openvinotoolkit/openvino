3ede2216756d897a0ccbb2672fb9c0c4ac45d7f9
Andrii Staikov <andrii.staikov@intel.com>
2025-09-26 15:25:58 +0200

    refactor

--- src/common/transformations/tests/common_optimizations/sdpa_fusion_test.cpp
+++ src/common/transformations/tests/common_optimizations/sdpa_fusion_test.cpp
@@ -27,6 +27,7 @@
 #include "openvino/op/subtract.hpp"
 #include "openvino/op/transpose.hpp"
 #include "openvino/op/unsqueeze.hpp"
+#include "openvino/pass/visualize_tree.hpp"
 
 using namespace std;
 using namespace testing;
@@ -898,13 +899,13 @@ TEST_F(TransformationTestsF, SDPAFusionTest12) {
 
 TEST_F(TransformationTestsF, SDPAFusionTest13) {
     const PartialShape query_shape{1, 64, 1, 64};
-    const PartialShape key_shape{1, 8, 8, 1, 64};
-    const PartialShape value_shape{1, 8, 8, 1, 64};
+    const PartialShape key_shape{1, 8, 8, 3, 64};
+    const PartialShape value_shape{1, 8, 8, 3, 64};
 
-    const PartialShape mask_shape{1, 1, 1, 1};
+    const PartialShape mask_shape{1, 1, 1, 3};
 
-    const Shape key_reshaped{1, 64, 1, 64};
-    const Shape value_reshaped{1, 64, 1, 64};
+    const Shape key_reshaped{1, 64, 3, 64};
+    const Shape value_reshaped{1, 64, 3, 64};
 
     const Shape sinks_shape{1, 64, 1, 1};
     std::vector<size_t> sinks_broadcast_shape{1, 64, 1, 1};
