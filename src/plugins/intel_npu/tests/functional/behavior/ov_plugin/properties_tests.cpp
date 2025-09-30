5d0910ac4c814368161ce2a7985c925445c53441
Mircea-Aurelian Dan <mircea-aurelian.dan@intel.com>
2025-09-30 18:56:37 +0300

    Add test to check mismatch between registered properties and hardoced ones for testing

--- src/plugins/intel_npu/tests/functional/behavior/ov_plugin/properties_tests.cpp
+++ src/plugins/intel_npu/tests/functional/behavior/ov_plugin/properties_tests.cpp
@@ -56,11 +56,11 @@ void invalidate_umd_caching() {
 #elif defined(__linux__)
 #    include <dlfcn.h>
 bool is_cid_loaded() {
-    return dlopen("libnpu_driver_compiler.so", RTLD_NOLOAD) != NULL;
+    return dlopen("libnpu_driver_compiler.so", RTLD_LAZY | RTLD_NOLOAD) != NULL;
 }
 
 bool is_cip_loaded() {
-    return dlopen("libnpu_mlir_compiler.so", RTLD_NOLOAD) != NULL;
+    return dlopen("libnpu_mlir_compiler.so", RTLD_LAZY | RTLD_NOLOAD) != NULL;
 }
 
 void invalidate_umd_caching() {}
@@ -69,28 +69,43 @@ void invalidate_umd_caching() {}
 
 }  // namespace
 
+using OVPropertiesTestsMismatchesNPU = OVPropertiesTestsNPU;
+
 namespace ov::test::behavior {
+
+TEST_P(OVPropertiesTestsMismatchesNPU, DetectPotentialPropertyMismatches) {
+    // Update properties' list with compiler properties
+    core->get_property(target_device, ov::supported_properties);
+    auto registeredProperties = core->get_property(target_device, ov::intel_npu::registered_properties);
+    auto propertiesVector = properties.begin()->second.as<std::vector<ov::AnyMap>>();
+    std::vector<ov::PropertyName> propertyNames, mismatchingProperties;
+    for (const auto& prop : propertiesVector) {
+        propertyNames.push_back(prop.begin()->first);
+    }
+
+    for (const auto& registeredProperty : registeredProperties) {
+        if (registeredProperty.find("NPUW") != ov::PropertyName::npos) {
+            continue;
+        }
+        if (std::find(propertyNames.begin(), propertyNames.end(), registeredProperty) == propertyNames.end()) {
+            mismatchingProperties.push_back(registeredProperty);
+        }
+    }
+
+    ASSERT_TRUE(mismatchingProperties.empty())
+        << "Mismatching properties: " << ov::Any(mismatchingProperties).as<std::string>();
+}
 
 TEST_P(OVPropertiesTestsNPU, GetSupportedPropertiesImplyCompilerLoad) {
     invalidate_umd_caching();
     core->get_property(target_device, ov::supported_properties, properties);
     if (auto it = properties.find(ov::intel_npu::compiler_type.name());
         it != properties.end() && it->second == ov::intel_npu::CompilerType::MLIR) {
-        ASSERT_TRUE(is_cip_loaded());
+        ASSERT_TRUE(!is_cid_loaded() && is_cip_loaded());
     } else {
-        ASSERT_TRUE(is_cid_loaded());
+        ASSERT_TRUE(is_cid_loaded() && !is_cip_loaded());
     }
 }
-
-const std::vector<ov::AnyMap> runtimeProperties = {{{ov::cache_dir.name(), ""}}};
-
-const std::vector<ov::AnyMap> compileTimeProperties = {
-    {{ov::intel_npu::dma_engines.name(), 1}},
-};
-
-const std::vector<ov::AnyMap> bothProperties = {
-    {{ov::log::level.name(), ov::log::Level::INFO}},
-};
 
 }  // namespace ov::test::behavior
 
@@ -103,10 +118,127 @@ constexpr std::vector<T> operator+(const std::vector<T>& vec1, const std::vector
     return result;
 }
 
+const std::vector<ov::AnyMap> runtimeProperties = {
+    {{ov::cache_dir.name(), ""}},
+    {{ov::hint::compiled_blob.name(), ov::Tensor()}},
+    {{ov::loaded_from_cache.name(), true}},
+    {{ov::internal::caching_properties.name(), ""}},
+    {{ov::internal::exclusive_async_requests.name(), true}},
+    {{ov::intel_npu::profiling_type.name(), ov::intel_npu::ProfilingType::INFER}},
+    {{ov::hint::model_priority.name(), ov::hint::Priority::HIGH}},
+    {{ov::intel_npu::create_executor.name(), 3}},
+    {{ov::intel_npu::defer_weights_load.name(), true}},
+    {{ov::weights_path.name(), ""}},
+    {{ov::hint::model.name(), std::shared_ptr<ov::Model>(nullptr)}},
+    {{ov::hint::model.name(), std::shared_ptr<const ov::Model>(nullptr)}},
+    {{ov::num_streams.name(), ov::streams::Num(2)}},
+    {{ov::hint::enable_cpu_pinning.name(), true}},
+    {{ov::workload_type.name(), ov::WorkloadType::EFFICIENT}},
+    {{ov::intel_npu::compiler_type.name(), ov::intel_npu::CompilerType::MLIR}},
+    {{ov::intel_npu::bypass_umd_caching.name(), true}},
+    {{ov::intel_npu::run_inferences_sequentially.name(), true}},
+    {{ov::intel_npu::disable_version_check.name(), true}},
+};
+
+const std::vector<ov::AnyMap> compileTimeProperties = {
+    {{ov::cache_mode.name(), ov::CacheMode::OPTIMIZE_SIZE}},
+    {{ov::intel_npu::batch_mode.name(), ov::intel_npu::BatchMode::PLUGIN}},
+    {{ov::intel_npu::dma_engines.name(), 1}},
+    {{ov::intel_npu::compilation_mode.name(), ""}},
+    {{ov::hint::execution_mode.name(), ov::hint::ExecutionMode::ACCURACY}},
+    {{ov::intel_npu::dynamic_shape_to_static.name(), true}},
+    {{ov::intel_npu::compilation_mode_params.name(), ""}},
+    {{ov::intel_npu::tiles.name(), 1}},
+    {{ov::intel_npu::stepping.name(), 1}},
+    {{ov::intel_npu::max_tiles.name(), 2}},
+    {{ov::intel_npu::dma_engines.name(), 2}},
+    {{ov::intel_npu::backend_compilation_params.name(), ""}},
+    {{ov::compilation_num_threads.name(), 3}},
+    {{ov::intel_npu::compiler_dynamic_quantization.name(), true}},
+    {{ov::intel_npu::qdq_optimization.name(), true}},
+    {{ov::intel_npu::qdq_optimization_aggressive.name(), true}},
+    {{ov::intel_npu::batch_compiler_mode_settings.name(), true}},
+    {{ov::intel_npu::weightless_blob.name(), true}},
+    {{ov::intel_npu::separate_weights_version.name(), ov::intel_npu::WSVersion::ONE_SHOT}},
+    {{ov::intel_npu::ws_compile_call_number.name(), 2}},
+};
+
+const std::vector<ov::AnyMap> bothProperties = {
+    {{ov::hint::performance_mode.name(), ov::hint::PerformanceMode::THROUGHPUT}},
+    {{ov::hint::num_requests.name(), 3}},
+    {{ov::hint::inference_precision.name(), ov::element::i8}},
+    {{ov::enable_profiling.name(), true}},
+    {{ov::log::level.name(), ov::log::Level::INFO}},
+    {{ov::intel_npu::platform.name(), ov::intel_npu::Platform::AUTO_DETECT}},
+    {{ov::device::id.name(), "NPU.0"}},
+    {{ov::internal::supported_properties.name(), ""}},
+    {{ov::intel_npu::turbo.name(), true}},
+};
+
+const std::vector<ov::AnyMap> metrics = {
+    {{ov::available_devices.name(), "NPU"}},
+    {{ov::device::architecture.name(), "3720"}},
+    {{ov::device::gops.name(),
+      std::map<ov::element::Type, float>{{ov::element::bf16, .0f},
+                                         {ov::element::f16, 5734.39990234375f},
+                                         {ov::element::i8, 11468.7998046875f},
+                                         {ov::element::u8, 11468.7998046875f}}}},
+    {{ov::device::luid.name(),
+      ov::device::LUID{/* .MAX_LUID_SIZE = 8, */ /* .luid  = */ std::
+                           array<uint8_t, /* MAX_LUID_SIZE = */ 8>{0xAB, 0xCD, 0xEF, 0x01, 0x23, 0x45, 0x67, 0x89}}}},
+    {{ov::device::pci_info.name(),
+      ov::device::PCIInfo{/* .domain = */ 0xFFEEDDCC,
+                          /* .bus = */ 0xBBAA9988,
+                          /* .device = */ 0x77665544,
+                          /* .function = */ 0x33221100}}},
+    {{ov::device::type.name(), ov::device::Type::DISCRETE}},
+    {{ov::device::uuid.name(),
+      ov::device::UUID{/* .MAX_UUID_SIZE = 16, */ /* .uuid  = */ std::array<uint8_t, /* MAX_UUID_SIZE = */ 16>{0xAB,
+                                                                                                               0xCD,
+                                                                                                               0xEF,
+                                                                                                               0x01,
+                                                                                                               0x23,
+                                                                                                               0x45,
+                                                                                                               0x67,
+                                                                                                               0x89,
+                                                                                                               0x98,
+                                                                                                               0x76,
+                                                                                                               0x54,
+                                                                                                               0x32,
+                                                                                                               0x10,
+                                                                                                               0xFE,
+                                                                                                               0xDC,
+                                                                                                               0xBA}}}},
+    {{ov::execution_devices.name(), std::vector<std::string>{"NPU"}}},
+    {{ov::device::full_name.name(), "Intel(R) AI Boost"}},
+    {{ov::intel_npu::backend_name.name(), "LEVEL0"}},
+    {{ov::intel_npu::compiler_version.name(), 123456}},
+    {{ov::intel_npu::device_alloc_mem_size.name(), 0}},
+    {{ov::intel_npu::device_total_mem_size.name(), 16 * 1024 * 1024}},
+    {{ov::intel_npu::driver_version.name(), 1688}},
+    {{ov::intel_npu::registered_properties.name(), ""}},
+    {{ov::optimal_number_of_infer_requests.name(), 1}},
+    {{ov::device::capabilities.name(), std::vector<std::string>{"FP16", "INT8", "EXPORT_IMPORT"}}},
+    {{ov::range_for_async_infer_requests.name(), std::tuple<unsigned int, unsigned int, unsigned int>{1, 10, 1}}},
+    {{ov::range_for_streams.name(), std::tuple<unsigned int, unsigned int>{1, 4}}},
+    {{ov::supported_properties.name(), ""}}};
+
+const std::vector<ov::AnyMap> allProperties = {
+    {{"",
+      runtimeProperties + compileTimeProperties + bothProperties + metrics}}  // std::vector<std::vector<ov::AnyMap>>
+};
+
+INSTANTIATE_TEST_SUITE_P(smoke_BehaviorTests,
+                         OVPropertiesTestsMismatchesNPU,
+                         ::testing::Combine(::testing::Values(ov::test::utils::DEVICE_NPU),
+                                            ::testing::ValuesIn(allProperties)),
+                         ov::test::utils::appendPlatformTypeTestName<OVPropertiesTestsNPU>);
+
 INSTANTIATE_TEST_SUITE_P(smoke_BehaviorTests,
                          OVPropertiesTestsNPU,
                          ::testing::Combine(::testing::Values(ov::test::utils::DEVICE_NPU),
                                             ::testing::ValuesIn(runtimeProperties + compileTimeProperties +
                                                                 bothProperties)),
-                         (ov::test::utils::appendPlatformTypeTestName<OVPropertiesTestsNPU>));
+                         (ov::test::utils::appendPlatformTypeTestName<OVPropertiesTestsNPU, true>));
+
 }  // namespace
