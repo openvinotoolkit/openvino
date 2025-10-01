d6d178c803c21a4e01c3f76411cc6190841c48dc
Mingyu Kim <mingyu.kim@intel.com>
2025-10-01 11:45:38 +0900

    [GPU] Add data range check log for validate_output_buffer (#32254)
    ### Details:
    - Print min/max value of each node so that we can understand the data
    range
     - Changed option name to have "GPU_" prefix
    - Do not throw exception when inf/nan is found. It is because this PR
    moves the logic to destructor.
    
    ### Tickets:
     - 174296

--- src/plugins/intel_gpu/include/intel_gpu/runtime/internal_properties.hpp
+++ src/plugins/intel_gpu/include/intel_gpu/runtime/internal_properties.hpp
@@ -173,7 +173,7 @@ static constexpr Property<ShapePredictor::Settings, ov::PropertyMutability::RW> 
 static constexpr Property<std::vector<std::string>, ov::PropertyMutability::RW> load_dump_raw_binary{"GPU_LOAD_DUMP_RAW_BINARY"};
 static constexpr Property<bool, ov::PropertyMutability::RW> could_use_flashattn_v2{"GPU_COULD_USE_FLASHATTN_V2"};
 static constexpr Property<uint64_t, PropertyMutability::RW> dynamic_quantization_group_size_max{"GPU_DYNAMIC_QUANTIZATION_GROUP_SIZE_MAX"};
-static constexpr Property<bool, ov::PropertyMutability::RW> validate_output_buffer{"VALIDATE_OUTPUT_BUFFER"};
+static constexpr Property<bool, ov::PropertyMutability::RW> validate_output_buffer{"GPU_VALIDATE_OUTPUT_BUFFER"};
 }  // namespace ov::intel_gpu
 
 namespace cldnn {
