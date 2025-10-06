6ef6e9d895475148907a36937e29aa61d6e1928e
Arseniy Obolenskiy <arseniy.obolenskiy@intel.com>
2025-10-06 11:04:51 +0200

    Address review comments

--- src/common/snippets/tests/src/lowered/linear_ir.cpp
+++ src/common/snippets/tests/src/lowered/linear_ir.cpp
@@ -19,11 +19,7 @@ namespace ov {
 namespace test {
 namespace snippets {
 
-namespace {
-ov::snippets::VectorDims make_dims(std::initializer_list<size_t> values) {
-    return ov::snippets::VectorDims(values);
-}
-}  // namespace
+using namespace ov::snippets;
 
 TEST(LinearIRReplaceWithNode, PreservesPerOutputDescriptors) {
     const auto param = std::make_shared<ov::op::v0::Parameter>(ov::element::f32, ov::Shape{1, 6});
@@ -35,24 +31,21 @@ TEST(LinearIRReplaceWithNode, PreservesPerOutputDescriptors) {
     auto factory = std::make_shared<ov::snippets::IShapeInferSnippetsFactory>();
     ov::snippets::lowered::LinearIR linear_ir(model, factory);
 
-    const auto& split_expr = linear_ir.get_expr_by_node(op);
-    ASSERT_NE(nullptr, split_expr);
+    const auto& online_sm_expr = linear_ir.get_expr_by_node(op);
+    ASSERT_NE(nullptr, online_sm_expr);
 
-    init_expr_descriptors(split_expr);
+    const std::vector<VectorDims> subtensors = {get_default_subtensor(2), VectorDims{1, 3}, VectorDims{1, 6}};
+    const std::vector<VectorDims> layouts = {VectorDims{}, VectorDims{0, 1}, VectorDims{1, 0}};
+    init_expr_descriptors(online_sm_expr, subtensors, layouts);
 
-    split_expr->get_output_port_descriptor(0)->set_subtensor(make_dims({1, 3}));
-    split_expr->get_output_port_descriptor(0)->set_layout(make_dims({0, 1}));
-    split_expr->get_output_port_descriptor(1)->set_subtensor(make_dims({1, 6}));
-    split_expr->get_output_port_descriptor(1)->set_layout(make_dims({1, 0}));
-
-    const auto expected_desc_0 = split_expr->get_output_port_descriptor(0)->clone();
-    const auto expected_desc_1 = split_expr->get_output_port_descriptor(1)->clone();
+    const auto expected_desc_0 = online_sm_expr->get_output_port_descriptor(0)->clone();
+    const auto expected_desc_1 = online_sm_expr->get_output_port_descriptor(1)->clone();
 
     ASSERT_NE(expected_desc_0->get_subtensor(), expected_desc_1->get_subtensor());
     ASSERT_NE(expected_desc_0->get_layout(), expected_desc_1->get_layout());
 
     const auto new_node = std::make_shared<ov::snippets::op::OnlineSoftmax>(param);
-    linear_ir.replace_with_node({split_expr}, new_node);
+    linear_ir.replace_with_node({online_sm_expr}, new_node);
 
     const auto& new_expr = linear_ir.get_expr_by_node(new_node);
     ASSERT_NE(nullptr, new_expr);
