ae161777576be8bc3dc35212cc22724cbc380428
Kasprzak, Jakub <jakub.kasprzak@intel.com>
2025-10-09 15:41:15 +0000

    Restore pooling impl and convolution ref impl

--- src/plugins/intel_gpu/src/kernel_selector/kernels/convolution/convolution_kernel_ref.cpp
+++ src/plugins/intel_gpu/src/kernel_selector/kernels/convolution/convolution_kernel_ref.cpp
@@ -109,7 +109,7 @@ ConvolutionKernelBase::DispatchData ConvolutionKernel_Ref::SetDefault(const conv
 }
 
 KernelsPriority ConvolutionKernel_Ref::GetKernelsPriority(const Params& /*params*/) const {
-    return FORCE_PRIORITY_1;
+    return DONT_USE_IF_HAVE_SOMETHING_ELSE;
 }
 
 bool ConvolutionKernel_Ref::Validate(const Params& params) const {
