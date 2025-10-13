9b7681993482ff5b43296aff781ac6514c501309
Sorin Butnariu <sorin.butnariu@intel.com>
2025-10-13 07:34:36 +0000

    Exit if driver not found
    gtest Instantiations cannot contain Plugin initialization
    
    Overload tests from OV: CoreThreading and QueryModel
    
    Add PV driver skip in test setup
    
    Signed-off-by: Sorin Butnariu <sorin.butnariu@intel.com>

--- src/plugins/intel_npu/tests/functional/behavior/ov_plugin/query_model.cpp
+++ src/plugins/intel_npu/tests/functional/behavior/ov_plugin/query_model.cpp
@@ -2,21 +2,22 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "behavior/ov_plugin/query_model.hpp"
-#include "common/utils.hpp"
+#include "query_model.hpp"
 #include "common/npu_test_env_cfg.hpp"
 
 namespace ov {
 namespace test {
 namespace behavior {
 
-INSTANTIATE_TEST_SUITE_P(compatibility_smoke_BehaviorTests_OVClassModelTestP, OVClassModelTestP,
+INSTANTIATE_TEST_SUITE_P(compatibility_smoke_BehaviorTests_OVClassModelTestP,
+                         OVClassModelTestP,
                          ::testing::Values(ov::test::utils::DEVICE_NPU),
                          (ov::test::utils::appendPlatformTypeTestName<OVClassModelTestP>));
 
-INSTANTIATE_TEST_SUITE_P(compatibility_smoke_BehaviorTests_OVClassQueryModelTestTests, OVClassQueryModelTest,
+INSTANTIATE_TEST_SUITE_P(compatibility_smoke_BehaviorTests_OVClassQueryModelTestTests,
+                         OVClassQueryModelTestNPU,
                          ::testing::Values(ov::test::utils::DEVICE_NPU),
-                         (ov::test::utils::appendDriverVersionTestName<OVClassQueryModelTest>));
+                         (ov::test::utils::appendPlatformTypeTestName<OVClassQueryModelTestNPU>));
 
 }  // namespace behavior
 }  // namespace test
