dbfc99f204aa6bec522b89220c6d7e073ce6a558
Oleg Pipikin <oleg.pipikin@intel.com>
2025-10-15 16:23:17 +0200

    Add tests

---
+++ src/tests/test_utils/unit_test_utils/mocks/mock_engine/mock_extensions.hpp
@@ -0,0 +1,7 @@
+// Copyright (C) 2018-2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "openvino/core/core_visibility.hpp"
+
+OPENVINO_API_C(int get_mock_extension_counter();)
