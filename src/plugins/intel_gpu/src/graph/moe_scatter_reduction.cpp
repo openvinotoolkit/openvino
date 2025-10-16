13379d58a53de00aaa5b2d7b4838ae553bd14e29
Lee, Chon Ming <chon.ming.lee@intel.com>
2025-10-16 19:10:56 +0800

    moe_scatter_reduction

---
+++ src/plugins/intel_gpu/src/graph/moe_scatter_reduction.cpp
@@ -0,0 +1,65 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "moe_scatter_reduction_inst.h"
+#include "primitive_type_base.h"
+#include "json_object.h"
+#include "to_string_utils.h"
+#include <string>
+#include <vector>
+
+namespace cldnn {
+GPU_DEFINE_PRIMITIVE_TYPE_ID(moe_scatter_reduction)
+
+layout moe_scatter_reduction_inst::calc_output_layout(moe_scatter_reduction_node const& node, kernel_impl_params const& impl_param) {
+    auto output_layouts = calc_output_layouts<ov::PartialShape>(node, impl_param);
+    return output_layouts[0];
+}
+
+template<typename ShapeType>
+std::vector<layout> moe_scatter_reduction_inst::calc_output_layouts(moe_scatter_reduction_node const& /*node*/, const kernel_impl_params& impl_param) {
+    const auto& desc = impl_param.typed_desc<moe_scatter_reduction>();
+    const auto num_active_experts_per_token = desc->num_active_experts_per_token;
+
+    const auto& input0_layout = impl_param.get_input_layout(0);
+
+    const auto& input_shapes = impl_param.input_layouts[0].get<ShapeType>();
+    const auto& hidden_size = input_shapes[1];
+    OPENVINO_ASSERT(hidden_size.is_static(), impl_param.desc->id, " hidden size dimension (shape[1]) must be static");
+
+    const auto& input0_pshape = input0_layout.get_partial_shape();
+    auto input0_len = input0_pshape.size();
+
+    input0_len = input0_len;
+    if (impl_param.input_layouts[0].is_dynamic())
+        return {layout{ov::PartialShape{ov::Dimension::dynamic(), ov::Dimension(hidden_size)},
+                impl_param.input_layouts[0].data_type, impl_param.input_layouts[0].format}};
+    const auto num_tokens = impl_param.input_layouts[0].get_shape()[0] /
+      num_active_experts_per_token;
+    const auto& out_shape = ov::PartialShape{ov::Dimension(num_tokens), ov::Dimension(hidden_size)};
+    return {layout{out_shape, impl_param.input_layouts[0].data_type, impl_param.input_layouts[0].format}};
+}
+
+template std::vector<layout> moe_scatter_reduction_inst::calc_output_layouts<ov::PartialShape>(moe_scatter_reduction_node const& node,
+    const kernel_impl_params& impl_param);
+
+std::string moe_scatter_reduction_inst::to_string(moe_scatter_reduction_node const& node) {
+    auto node_info = node.desc_to_json();
+    auto desc = node.get_primitive();
+
+    std::stringstream primitive_description;
+
+    json_composite moe_scatter_reduction_info;
+    moe_scatter_reduction_info.add("input id", node.input().id());
+    if (desc->output_data_types[0].has_value())
+       moe_scatter_reduction_info.add("out dt: ", dt_to_str(*desc->output_data_types[0]));
+    node_info->add("moe_scatter_reduction_info", moe_scatter_reduction_info);
+    node_info->dump(primitive_description);
+
+    return primitive_description.str();
+}
+
+moe_scatter_reduction_inst::typed_primitive_inst(network& network, moe_scatter_reduction_node const& node) : parent(network, node) { }
+}  // namespace cldnn
+
