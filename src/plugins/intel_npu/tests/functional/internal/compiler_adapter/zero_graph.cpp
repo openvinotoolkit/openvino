dfa01c90495513d315555d8b52b73dca52ba262a
Alexandru Enache <alexandru.enache@intel.com>
2025-10-17 11:41:27 +0300

    [NPU] Add tests for ZeGraphExtWrappers (#32376)
    ### Details:
     - Adding internal functional tests for wrappers.
    
    ### Tickets:
     - *C-173806*
    
    ---------
    
    Signed-off-by: alexandruenache1111 <alexandru.enache@intel.com>

---
+++ src/plugins/intel_npu/tests/functional/internal/compiler_adapter/zero_graph.cpp
@@ -0,0 +1,22 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "zero_graph.hpp"
+
+#include <common_test_utils/test_assertions.hpp>
+
+namespace {
+std::vector<int> graphDescflags = {ZE_GRAPH_FLAG_NONE,
+                                   ZE_GRAPH_FLAG_DISABLE_CACHING,
+                                   ZE_GRAPH_FLAG_ENABLE_PROFILING,
+                                   ZE_GRAPH_FLAG_INPUT_GRAPH_PERSISTENT};
+
+// tested versions interval is [1.5, CURRENT + 1)
+auto extVersions = ::testing::Range(ZE_MAKE_VERSION(1, 5), ZE_GRAPH_EXT_VERSION_CURRENT + 1);
+
+INSTANTIATE_TEST_SUITE_P(smoke_BehaviorTest,
+                         ZeroGraphTest,
+                         ::testing::Combine(::testing::ValuesIn(graphDescflags), extVersions),
+                         ZeroGraphTest::getTestCaseName);
+}  // namespace
---
+++ src/plugins/intel_npu/tests/functional/internal/compiler_adapter/zero_graph.hpp
@@ -0,0 +1,249 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <gtest/gtest.h>
+#include <stdlib.h>
+
+#include <common_test_utils/test_assertions.hpp>
+
+#include "common/npu_test_env_cfg.hpp"
+#include "common_test_utils/subgraph_builders/multi_single_conv.hpp"
+#include "common_test_utils/test_constants.hpp"
+#include "intel_npu/utils/utils.hpp"
+#include "intel_npu/utils/zero/zero_mem.hpp"
+#include "intel_npu/utils/zero/zero_utils.hpp"
+#include "ir_serializer.hpp"
+#include "ze_graph_ext_wrappers.hpp"
+#include "zero_init_mock.hpp"
+
+using namespace intel_npu;
+
+namespace {
+size_t calculate_size_with_alignment_padding(size_t size, size_t alignment) {
+    return size + alignment - (size % alignment);
+}
+
+size_t get_file_size(std::ifstream& file) {
+    auto size = file.tellg();
+    file.seekg(0, std::ios::end);
+    size = file.tellg() - size;
+    file.seekg(0, std::ios::beg);
+
+    return size;
+}
+}  // namespace
+
+namespace ov::test::behavior {
+class ZeroGraphTest : public ::testing::TestWithParam<std::tuple<int, int>> {
+public:
+    std::shared_ptr<ZeroInitStructsHolder> zeroInitStruct;
+
+    std::shared_ptr<ZeGraphExtWrappers> zeGraphExt;
+
+    SerializedIR serializedIR;
+
+    GraphDescriptor graphDescriptor;
+
+    std::shared_ptr<ov::Model> model;
+
+    std::shared_ptr<driver_compiler_utils::IRSerializer> irSerializer;
+
+    std::string blobPath;
+
+    int extVersion;
+
+    int graphDescFlag;
+
+    static std::string getTestCaseName(const testing::TestParamInfo<std::tuple<int, int>>& obj) {
+        int flag, version;
+        std::tie(flag, version) = obj.param;
+        std::string targetDevice = ov::test::utils::DEVICE_NPU;
+
+        std::ostringstream result;
+        result << "targetDevice=" << targetDevice << "_";
+        result << "targetPlatform=" << ov::test::utils::getTestsPlatformFromEnvironmentOr(targetDevice) << "_";
+        result << "graphDescriptorFlag=" + std::to_string(flag) << "_";
+        result << "extVersion=" + std::to_string(ZE_MAJOR_VERSION(version)) + "." +
+                      std::to_string(ZE_MINOR_VERSION(version));
+        return result.str();
+    }
+
+    void serializeIR() {
+        auto compilerProperties = zeroInitStruct->getCompilerProperties();
+        const ze_graph_compiler_version_info_t& compilerVersion = compilerProperties.compilerVersion;
+        const auto maxOpsetVersion = compilerProperties.maxOVOpsetVersionSupported;
+        serializedIR = irSerializer->serializeIR(model, compilerVersion, maxOpsetVersion);
+    }
+
+protected:
+    void SetUp() override {
+        using namespace ::driver_compiler_utils;
+
+        std::tie(graphDescFlag, extVersion) = GetParam();
+
+        const std::string BLOB_NAME = "blob_compatibility_dummy_model_MTL_ov_2025_1_0_driver_1003967.blob";
+        blobPath = ov::test::utils::NpuTestEnvConfig::getInstance().OV_NPU_TESTS_BLOBS_PATH + BLOB_NAME;
+
+        model = ov::test::utils::make_multi_single_conv();
+
+        std::shared_ptr<ZeroInitStructsMock> zeroInitMock = std::make_shared<ZeroInitStructsMock>(extVersion);
+
+        zeroInitStruct = std::reinterpret_pointer_cast<ZeroInitStructsHolder>(zeroInitMock);
+
+        zeGraphExt = std::make_shared<ZeGraphExtWrappers>(zeroInitStruct);
+
+        auto compilerProperties = zeroInitStruct->getCompilerProperties();
+        const auto maxOpsetVersion = compilerProperties.maxOVOpsetVersionSupported;
+        irSerializer = std::make_shared<IRSerializer>(IRSerializer(model, maxOpsetVersion));
+    }
+
+    void TearDown() override {
+        zeGraphExt->destroyGraph(graphDescriptor);
+    }
+};
+
+TEST_P(ZeroGraphTest, GetGraphInitIR) {
+    serializeIR();
+    OV_ASSERT_NO_THROW(graphDescriptor = zeGraphExt->getGraphDescriptor(serializedIR, "", graphDescFlag));
+
+    uint32_t initCommandQueueOrdinal = 0;
+    OV_ASSERT_NO_THROW(initCommandQueueOrdinal =
+                           zeroUtils::findCommandQueueGroupOrdinal(zeroInitStruct->getDevice(),
+                                                                   ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COMPUTE));
+    OV_ASSERT_NO_THROW(zeGraphExt->initializeGraph(graphDescriptor, initCommandQueueOrdinal));
+}
+
+TEST_P(ZeroGraphTest, GetGraphInitBlob) {
+    std::ifstream blobStream(blobPath, std::ios::binary | std::ios::in);
+    ASSERT_TRUE(blobStream.is_open());
+    size_t size = get_file_size(blobStream);
+
+    std::vector<uint8_t> blob(size);
+    blobStream.read(reinterpret_cast<char*>(blob.data()), size);
+    blobStream.close();
+
+    OV_ASSERT_NO_THROW(graphDescriptor = zeGraphExt->getGraphDescriptor(blob.data(), blob.size()));
+
+    uint32_t initCommandQueueOrdinal = 0;
+    OV_ASSERT_NO_THROW(initCommandQueueOrdinal =
+                           zeroUtils::findCommandQueueGroupOrdinal(zeroInitStruct->getDevice(),
+                                                                   ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COMPUTE));
+    OV_ASSERT_NO_THROW(zeGraphExt->initializeGraph(graphDescriptor, initCommandQueueOrdinal));
+}
+
+TEST_P(ZeroGraphTest, GetNetworkMeta) {
+    serializeIR();
+    OV_ASSERT_NO_THROW(graphDescriptor = zeGraphExt->getGraphDescriptor(serializedIR, "", graphDescFlag));
+
+    OV_ASSERT_NO_THROW(NetworkMetadata meta = zeGraphExt->getNetworkMeta(graphDescriptor));
+}
+
+TEST_P(ZeroGraphTest, QueryGraph) {
+    serializeIR();
+    OV_ASSERT_NO_THROW(zeGraphExt->queryGraph(std::move(serializedIR), ""));
+}
+
+TEST_P(ZeroGraphTest, GetGraphBinary) {
+    std::ifstream blobStream(blobPath, std::ios::binary | std::ios::in);
+    ASSERT_TRUE(blobStream.is_open());
+    size_t size = get_file_size(blobStream);
+
+    std::vector<uint8_t> blob(size);
+    blobStream.read(reinterpret_cast<char*>(blob.data()), size);
+    blobStream.close();
+
+    OV_ASSERT_NO_THROW(graphDescriptor = zeGraphExt->getGraphDescriptor(blob.data(), blob.size()));
+
+    uint32_t initCommandQueueOrdinal = 0;
+    OV_ASSERT_NO_THROW(initCommandQueueOrdinal =
+                           zeroUtils::findCommandQueueGroupOrdinal(zeroInitStruct->getDevice(),
+                                                                   ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COMPUTE));
+    OV_ASSERT_NO_THROW(zeGraphExt->initializeGraph(graphDescriptor, initCommandQueueOrdinal));
+
+    const uint8_t* blobPtr = nullptr;
+    OV_ASSERT_NO_THROW(zeGraphExt->getGraphBinary(graphDescriptor, blob, blobPtr, size));
+}
+
+TEST_P(ZeroGraphTest, SetGraphArgOnNullBuffer) {
+    serializeIR();
+
+    OV_ASSERT_NO_THROW(graphDescriptor = zeGraphExt->getGraphDescriptor(serializedIR, "", graphDescFlag));
+
+    uint32_t initCommandQueueOrdinal = 0;
+    OV_ASSERT_NO_THROW(initCommandQueueOrdinal =
+                           zeroUtils::findCommandQueueGroupOrdinal(zeroInitStruct->getDevice(),
+                                                                   ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COMPUTE));
+    OV_ASSERT_NO_THROW(zeGraphExt->initializeGraph(graphDescriptor, initCommandQueueOrdinal));
+
+    ASSERT_ANY_THROW(zeGraphExt->setGraphArgumentValue(graphDescriptor, 0, nullptr));
+}
+
+TEST_P(ZeroGraphTest, GetInitSetArgsDestroyGraphAlignedMemoryIR) {
+    serializeIR();
+
+    OV_ASSERT_NO_THROW(graphDescriptor = zeGraphExt->getGraphDescriptor(serializedIR, "", graphDescFlag));
+
+    uint32_t initCommandQueueOrdinal = 0;
+    OV_ASSERT_NO_THROW(initCommandQueueOrdinal =
+                           zeroUtils::findCommandQueueGroupOrdinal(zeroInitStruct->getDevice(),
+                                                                   ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COMPUTE));
+    OV_ASSERT_NO_THROW(zeGraphExt->initializeGraph(graphDescriptor, initCommandQueueOrdinal));
+
+    size_t totalSize = 1 * 3 * 24 * 24 * sizeof(float);
+    std::unique_ptr<ZeroMem> buffer;
+    OV_ASSERT_NO_THROW(buffer =
+                           std::make_unique<ZeroMem>(zeroInitStruct, totalSize, ::utils::STANDARD_PAGE_SIZE, false));
+
+    OV_ASSERT_NO_THROW(zeGraphExt->setGraphArgumentValue(graphDescriptor, 0, buffer->data()));
+}
+
+TEST_P(ZeroGraphTest, GetInitSetArgsDestroyGraphAlignedMemoryMallocBlob) {
+    std::ifstream blobStream(blobPath, std::ios::binary | std::ios::in);
+    ASSERT_TRUE(blobStream.is_open());
+    size_t size = get_file_size(blobStream);
+    size = calculate_size_with_alignment_padding(size, ::utils::STANDARD_PAGE_SIZE);
+
+    uint8_t* blob = static_cast<uint8_t*>(::operator new(size, std::align_val_t(::utils::STANDARD_PAGE_SIZE)));
+    blobStream.read(reinterpret_cast<char*>(blob), size);
+    blobStream.close();
+
+    OV_ASSERT_NO_THROW(graphDescriptor = zeGraphExt->getGraphDescriptor(blob, size));
+
+    uint32_t initCommandQueueOrdinal = 0;
+    OV_ASSERT_NO_THROW(initCommandQueueOrdinal =
+                           zeroUtils::findCommandQueueGroupOrdinal(zeroInitStruct->getDevice(),
+                                                                   ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COMPUTE));
+    OV_ASSERT_NO_THROW(zeGraphExt->initializeGraph(graphDescriptor, initCommandQueueOrdinal));
+
+    std::unique_ptr<ZeroMem> buffer;
+    OV_ASSERT_NO_THROW(buffer = std::make_unique<ZeroMem>(zeroInitStruct, size, ::utils::STANDARD_PAGE_SIZE, false));
+
+    OV_ASSERT_NO_THROW(zeGraphExt->setGraphArgumentValue(graphDescriptor, 0, buffer->data()));
+}
+
+TEST_P(ZeroGraphTest, GetInitSetArgsDestroyGraphNotAlignedMemoryMallocBlob) {
+    std::ifstream blobStream(blobPath, std::ios::binary | std::ios::in);
+    ASSERT_TRUE(blobStream.is_open());
+    size_t size = get_file_size(blobStream);
+
+    uint8_t* blob = static_cast<uint8_t*>(::operator new(size, std::align_val_t(::utils::STANDARD_PAGE_SIZE)));
+    blobStream.read(reinterpret_cast<char*>(blob), size);
+    blobStream.close();
+
+    OV_ASSERT_NO_THROW(graphDescriptor = zeGraphExt->getGraphDescriptor(blob, size));
+
+    uint32_t initCommandQueueOrdinal = 0;
+    OV_ASSERT_NO_THROW(initCommandQueueOrdinal =
+                           zeroUtils::findCommandQueueGroupOrdinal(zeroInitStruct->getDevice(),
+                                                                   ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COMPUTE));
+    OV_ASSERT_NO_THROW(zeGraphExt->initializeGraph(graphDescriptor, initCommandQueueOrdinal));
+
+    std::unique_ptr<ZeroMem> buffer;
+    OV_ASSERT_NO_THROW(buffer = std::make_unique<ZeroMem>(zeroInitStruct, size, ::utils::STANDARD_PAGE_SIZE, false));
+
+    OV_ASSERT_NO_THROW(zeGraphExt->setGraphArgumentValue(graphDescriptor, 0, static_cast<char*>(buffer->data()) + 1));
+}
+}  // namespace ov::test::behavior
