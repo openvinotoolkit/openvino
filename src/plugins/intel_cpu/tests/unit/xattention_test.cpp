2939bc6daa0637e2bc7aa8e2108b3283fbb5c39f
mangguo <mang.guo@intel.com>
2025-10-20 22:58:32 +0800

    Add test case compare with reference implementation

--- src/plugins/intel_cpu/tests/unit/xattention_test.cpp
+++ src/plugins/intel_cpu/tests/unit/xattention_test.cpp
@@ -8,10 +8,12 @@
 
 #include <gtest/gtest.h>
 
+#include "common_test_utils/data_utils.hpp"
 #include "common_test_utils/test_common.hpp"
 #include "nodes/kernels/scaled_attn/transpose_kernel.hpp"
 #include "openvino/core/parallel.hpp"
 #include "openvino/core/type/element_type.hpp"
+#include "openvino/reference/xattention.hpp"
 #include "utils/plain_tensor.hpp"
 
 using namespace ov::intel_cpu;
@@ -26,6 +28,15 @@ struct XAttr {
 
 using XAttentionParams =
     std::tuple<ov::element::Type, std::vector<size_t>, std::vector<size_t>, XAttr, std::vector<bool>>;
+
+template <class T>
+void inline fill_data_random(T* pointer, std::size_t size, const float min, const float max, const int seed) {
+    std::mt19937 gen(seed);
+    std::uniform_real_distribution<double> dist(min, max);
+    for (std::size_t i = 0; i < size; i++) {
+        pointer[i] = static_cast<T>(dist(gen));
+    }
+}
 
 class XAttentionTest : public ov::test::TestsCommon, public testing::WithParamInterface<XAttentionParams> {
 public:
@@ -49,60 +60,66 @@ public:
         result << ",threshold=" << xattr.threshold;
         return result.str();
     }
-};
 
-TEST_P(XAttentionTest, simpleTest) {
-    const auto& [data_type, query_shape, key_shape, xattr, expected_values] = this->GetParam();
-    size_t element_size = ov::element::Type(data_type).size();
-    PlainTensor query;
-    PlainTensor key;
-    query.resize(query_shape, element_size, data_type);
-    key.resize(key_shape, element_size, data_type);
+public:
+    template <typename T>
+    void compareWithRef() {
+        const auto& [data_type, query_shape, key_shape, xattr, expected_values] = this->GetParam();
+        auto element_size = ov::element::Type(data_type).size();
 
-    for (size_t h = 0; h < query_shape[1]; h++) {
-        for (size_t b = 0; b < query_shape[0]; b++) {
-            for (size_t s = 0; s < query_shape[3]; s++) {
-                float v = static_cast<float>(b + s) / 100.0f;
-                if (data_type == ov::element::f32) {
-                    *key.ptr<float>(b, h, 0, s) = v;
-                    *query.ptr<float>(b, h, 0, s) = 1.0f;
-                } else if (data_type == ov::element::bf16) {
-                    *key.ptr<ov::bfloat16, ov::element::bf16>(b, h, 0, s) = v;
-                    *query.ptr<ov::bfloat16, ov::element::bf16>(b, h, 0, s) = 1.0f;
-                } else {
-                    OPENVINO_THROW("xattention: unsupported precision: ", data_type);
-                }
+        PlainTensor query;
+        PlainTensor key;
+        query.resize(query_shape, element_size, data_type);
+        key.resize(key_shape, element_size, data_type);
+
+        fill_data_random<T>(query.ptr<T>(), ov::shape_size(query_shape.begin(), query_shape.end()), 0, 2, 0);
+        fill_data_random<T>(key.ptr<T>(), ov::shape_size(key_shape.begin(), key_shape.end()), 0, 1, 0);
+
+        Xattn xattn;
+        xattn.init(query.size(0),
+                   query.size(1),
+                   query.size(2),
+                   query.size(3),
+                   key.size(1),
+                   xattr.stride,
+                   xattr.block_size,
+                   ov::element::f32);
+        PlainTensor mask;
+        xattn.estimate(query, key, xattr.block_size, xattr.stride, xattr.threshold, mask);
+
+        ov::reference::XAttentionBlockSelector<T> selector(xattr.threshold, xattr.block_size, xattr.stride);
+        ov::Shape query_shape_ref = {query_shape[1], query_shape[0], query_shape[3]};
+        ov::Shape key_shape_ref = {key_shape[1], key_shape[0], key_shape[3]};
+        auto ref_result = selector.select_blocks(query.ptr<T>(), query_shape_ref, key.ptr<T>(), key_shape_ref);
+
+        for (size_t h = 0; h < query.size(1); h++) {
+            for (const auto& idx : ref_result[h]) {
+                auto row = idx.first;
+                auto col = idx.second;
+                EXPECT_EQ(*mask.ptr<bool>(h, row, col), true);
             }
         }
     }
+};
 
-    Xattn xattn;
-    xattn.init(query.size(0),
-               query.size(1),
-               query.size(2),
-               query.size(3),
-               key.size(2),
-               xattr.stride,
-               xattr.block_size,
-               ov::element::f32);
-    PlainTensor mask;
-    xattn.estimate(query, key, xattr.block_size, xattr.stride, xattr.threshold, mask);
-    size_t k_num_blocks = div_up(key_shape[0], xattr.block_size);
-    for (size_t i = 0; i < expected_values.size(); i++) {
-        EXPECT_EQ(*mask.ptr<bool>(0, i / k_num_blocks, i % k_num_blocks), expected_values[i]);
-    }
+TEST_P(XAttentionTest, testWithRef) {
+        compareWithRef<float>();
 }
 
 const std::vector<XAttentionParams> params = {
-    {ov::element::f32, {8, 1, 1, 8}, {8, 1, 1, 8}, {4, 2, 0.8f}, {true, false, true, true}},
+    {ov::element::f32,
+     {8, 1, 1, 8},  // {B, H, L, S}
+     {8, 1, 1, 8},
+     {4, 2, 0.8f},
+     {true, false, true, true}},
     {ov::element::f32,
      {16, 1, 1, 32},
      {16, 1, 1, 32},
      {4, 2, 0.3f},
      {true, false, false, false, true, true, false, false, true, false, true, false, true, false, false, true}},
     {ov::element::f32,
-     {16, 1, 1, 32},
-     {16, 1, 1, 32},
+     {16, 2, 1, 32},
+     {16, 2, 1, 32},
      {4, 2, 0.7f},
      {true, false, false, false, true, true, false, false, true, true, true, false, true, false, true, true}}};
 
