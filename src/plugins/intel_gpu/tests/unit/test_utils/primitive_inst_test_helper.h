e28634c1ccd7134d40728f37a8f1283fc05fe963
Mingyu Kim <mingyu.kim@intel.com>
2025-10-21 12:57:02 +0900

    [GPU] memory reset bugfix (#32479)
    ### Description of the issue(symptom, root-cause, how it was resolved)
     - OneDNN requires padding in the input to be zero-filled.
    - When non-aligned memory is reused in other places, it is difficult to
    guarantee zero-filling.
    - This PR changes to enable `need_reset_output_memory` if the user is
    convolution and buffer size is not aligned
    - The issue happens for the shape like this: [2, 9, 112, 112] @
    b_fs_yx_fsv16 format
     - It was corrupted when the buffer is reused by other part of network.
    - It may have performance impact because reset will be added
    additionally when condition is met.
    
    #### Reproduction step and snapshot (if applicable. Do not attach for
    customer model)
    - $ OV_DYNAMIC_QUANTIZATION_GROUP_SIZE=128 python whowhatbench/wwb.py
    --target-model
    stable-diffusion-xl-1.0-inpainting-0.1/pytorch/ov/OV_FP16-INT8_ASYM
    --gt-data stable-diffusion-xl-1.0-inpainting-0.1__NAT/reference.csv
    --genai --device GPU.1 --model-type image-inpainting
    
    #### Problematic graph
     - It is not relevant to graph
    
    #### Checklist
     - [x] Is it a proper fix? (not a workaround)
     - [x] Did you include test case for this fix, if necessary?
    - [x] Did you review existing test that can be extended to cover this
    scenario? Which test did you review?
    
    ### Tickets:
     - 167267

--- src/plugins/intel_gpu/tests/unit/test_utils/primitive_inst_test_helper.h
+++ src/plugins/intel_gpu/tests/unit/test_utils/primitive_inst_test_helper.h
@@ -14,5 +14,8 @@ public:
     static void set_allocation_done_by_other(const std::shared_ptr<primitive_inst>& inst, bool val) {
         inst->_allocation_done_by_other = val;
     }
+    static bool need_reset_output_memory(const std::shared_ptr<primitive_inst>& inst) {
+        return inst->need_reset_output_memory();
+    }
 };
 }  // namespace cldnn
