a5d15c134df021c9e56ae77e54661835066a4557
p-wysocki <przemyslaw.wysocki@intel.com>
2025-10-22 13:30:38 +0200

    Apply CR
    Signed-off-by: p-wysocki <przemyslaw.wysocki@intel.com>

--- src/bindings/python/tests/test_graph/test_pooling.py
+++ src/bindings/python/tests/test_graph/test_pooling.py
@@ -282,6 +282,20 @@ avg_pooling_opset16_test_params = [
         [1, 3, 10, 10],  # input_shape
         [1, 3, 5, 5],  # expected_output_shape
     ),
+    (
+        [
+            [2, 2],  # strides
+            [1, 1],  # pads_begin
+            [1, 1],  # pads_end
+            [2, 2],  # kernel_shape
+            False,  # exclude_pad
+            "ceil_torch",  # rounding_type
+            None,  # auto_pad
+            [1, 1],  # dilations
+        ],
+        [1, 1, 5, 5],  # input_shape
+        [1, 1, 3, 3],  # expected_output_shape
+    ),
 ]
 
 
@@ -300,20 +314,16 @@ def test_avg_pool_opset16_with_dilations(op_params, input_shape, expected_output
     assert node.get_output_element_type(0) == Type.f32
 
 
-def test_avg_pool_opset16_without_dilations():
-    param = ov.parameter([1, 1, 4, 4], name="A", dtype=np.float32)
-    node = ov16.avg_pool(
-        param,
-        strides=[2, 2],
-        pads_begin=[0, 0],
-        pads_end=[0, 0],
-        kernel_shape=[2, 2],
-        exclude_pad=True,
-        rounding_type="floor",
-        name="avg_pool_no_dilations"
-    )
+@pytest.mark.parametrize(
+    ("op_params", "input_shape", "expected_output_shape"),
+    avg_pooling_test_params,
+)
+@pytest.mark.parametrize("op_name", ["avg_pool", "avgPool", "avgPoolOpset16"])
+def test_avg_pool_opset16_backward_compatibility(op_params, input_shape, expected_output_shape, op_name):
+    param = ov.parameter(input_shape, name="A", dtype=np.float32)
+    node = ov16.avg_pool(param, *op_params, name=op_name)
     assert node.get_type_name() == "AvgPool"
-    assert node.get_friendly_name() == "avg_pool_no_dilations"
+    assert node.get_friendly_name() == op_name
     assert node.get_output_size() == 1
-    assert list(node.get_output_shape(0)) == [1, 1, 2, 2]
+    assert list(node.get_output_shape(0)) == expected_output_shape
     assert node.get_output_element_type(0) == Type.f32
