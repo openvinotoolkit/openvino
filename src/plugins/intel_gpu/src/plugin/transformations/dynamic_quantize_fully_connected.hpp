3b085fea9ef25e9886a322374cea3cb6ae7496bf
Mingyu Kim <mingyu.kim@intel.com>
2025-10-22 11:25:14 +0900

    [GPU] enable gs128 by default for int8 models in xe2+ platforms (#32493)
    ### Details:
     - Enable gs128 by default for int8 model when it runs on XE2+ platforms
     - It is not enabled for other cases because of performance concern
    - Selection of gs128 is not exposed to user directly. When the config is
    set as PER-TOKEN(which is a default value) and conditions are met(int8
    layer, XE2+ platform), gs128 is chosen within GPU plugin.
    
    ### Tickets:
     - 167267

--- src/plugins/intel_gpu/src/plugin/transformations/dynamic_quantize_fully_connected.hpp
+++ src/plugins/intel_gpu/src/plugin/transformations/dynamic_quantize_fully_connected.hpp
@@ -11,7 +11,7 @@ namespace ov::intel_gpu {
 class DynamicQuantizeFullyConnected: public ov::pass::MatcherPass {
 public:
     OPENVINO_MATCHER_PASS_RTTI("DynamicQuantizeFullyConnected");
-    DynamicQuantizeFullyConnected(uint64_t group_size, bool asymmetric = false, bool precompute_sum = true);
+    DynamicQuantizeFullyConnected(uint64_t group_size, bool asymmetric = false, bool precompute_sum = true, bool use_gs128_for_int8_per_token = false);
 };
 
 }   // namespace ov::intel_gpu
