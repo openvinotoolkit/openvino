7e0d96014876e9cab40815b8ea9305cbb7c8636a
Alexander Nesterov <nesterov.alexander@outlook.com>
2025-10-22 20:46:23 +0200

    Remove JitConv3DExecutorF32 implementation, associated helper functions, and redundant includes to simplify and clean up AArch64 JIT 3D Convolution code.

--- src/plugins/intel_cpu/src/nodes/executors/aarch64/jit_conv3d_f32.hpp
+++ src/plugins/intel_cpu/src/nodes/executors/aarch64/jit_conv3d_f32.hpp
@@ -9,10 +9,6 @@
 #include <memory>
 #include <vector>
 
-#include "nodes/executors/convolution_config.hpp"
-#include "nodes/executors/executor.hpp"
-#include "nodes/executors/memory_arguments.hpp"
-
 #include <cpu/aarch64/jit_generator.hpp>
 
 namespace ov::intel_cpu {
@@ -51,42 +47,5 @@ private:
 
     jit_fn ker_{nullptr};
 };
-
-class JitConv3DExecutorF32 : public Executor {
-public:
-    JitConv3DExecutorF32(const ConvAttrs& attrs, const MemoryArgs& memory, const ExecutorContext::CPtr& context);
-
-    bool update(const MemoryArgs& memory) override {
-        m_memory = memory;
-        return true;
-    }
-    void execute(const MemoryArgs& memory) override;
-    void execute() override {
-        execute(m_memory);
-    }
-    void exec([[maybe_unused]] const std::vector<MemoryCPtr>& src,
-              [[maybe_unused]] const std::vector<MemoryPtr>& dst) override {}
-
-    [[nodiscard]] impl_desc_type implType() const override {
-        return impl_desc_type::jit_asimd;
-    }
-
-    static bool supports(const ConvConfig& cfg);
-
-private:
-    void run_naive_fp32(const MemoryArgs& memory);
-    void ensure_weights_packed(const MemoryArgs& memory);
-
-    std::unique_ptr<JitConv3DKernelF32> m_ip_kernel;
-
-    ConvAttrs m_attrs;
-    MemoryArgs m_memory;
-
-    std::vector<float> m_wei_packed;  // [OC, KD, KH, KW, Ct=4]
-    bool m_wei_packed_ready{false};
-    size_t m_padded_C{0};
-};
-
-using JitConv3DExecutorF32Ptr = std::shared_ptr<JitConv3DExecutorF32>;
 
 }  // namespace ov::intel_cpu
