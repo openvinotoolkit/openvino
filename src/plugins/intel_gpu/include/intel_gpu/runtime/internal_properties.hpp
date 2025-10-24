5201cdf4ba2037585f8a9bd5cc3b5c96f55b9d63
gta <gta@DUT4020PTLH.fm.intel.com>
2025-10-24 01:18:02 +0000

    add allow_bypass_xattn

--- src/plugins/intel_gpu/include/intel_gpu/runtime/internal_properties.hpp
+++ src/plugins/intel_gpu/include/intel_gpu/runtime/internal_properties.hpp
@@ -177,6 +177,7 @@ static constexpr Property<uint64_t, PropertyMutability::RW> dynamic_quantization
 static constexpr Property<bool, ov::PropertyMutability::RW> validate_output_buffer{"GPU_VALIDATE_OUTPUT_BUFFER"};
 static constexpr Property<float, ov::PropertyMutability::RW> mem_pool_util_threshold{"GPU_MEM_POOL_UTIL_THRESHOLD"};
 static constexpr Property<bool, ov::PropertyMutability::RW> dump_src_after_exec{"GPU_DUMP_SRC_TENSORS_AFTER_EXEC"};
+static constexpr Property<bool, ov::PropertyMutability::RW> allow_bypass_xattn{"GPU_ALLOW_BYPASS_XATTN_EXEC"};
 }  // namespace ov::intel_gpu
 
 namespace cldnn {
