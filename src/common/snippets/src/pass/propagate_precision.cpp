ffc348c335595dab0d9fb155b7d58443231e4bc7
Evgeny Kotov <evgeny.kotov@intel.com>
2025-10-27 12:22:56 +0100

    Fix implicit Node to Output conversion in transformations (#32535)
    ### Details:
    This PR fixes implicit Node to Output conversion issues found left after
    https://github.com/openvinotoolkit/openvino/pull/30337
    
    Changes:
    - lora_subgraph_horizontal_fusion.cpp: Use input_value() instead of
    get_input_node_shared_ptr() when filling OutputVector for
    LoraSubgraphFused constructor
    - propagate_precision.cpp: Use input_value() instead of
    get_input_node_shared_ptr() when creating ConvertSaturation node
    
    These issues are classified as non-critical because the inputs are
    typically single-output nodes (ReadValue, Convert, etc.) and don't cause
    exceptions in real scenarios. However, they violate the best practice of
    avoiding implicit conversions when creating nodes.
    
    ### Tickets:
     - 165972

--- src/common/snippets/src/pass/propagate_precision.cpp
+++ src/common/snippets/src/pass/propagate_precision.cpp
@@ -147,9 +147,9 @@ bool ov::snippets::pass::PropagatePrecision::run_on_model(const std::shared_ptr<
 
                     if (can_be_fused(actual_after, required_after)) {
                         // fuse existing convert
-                        auto convert = std::make_shared<ov::snippets::op::ConvertSaturation>(
-                            existing_convert->get_input_node_shared_ptr(0),
-                            required_after);
+                        auto convert =
+                            std::make_shared<ov::snippets::op::ConvertSaturation>(existing_convert->input_value(0),
+                                                                                  required_after);
                         copy_runtime_info(parent_output.get_node_shared_ptr(), convert);
                         op->set_argument(op_input.get_index(), convert);
                         continue;
