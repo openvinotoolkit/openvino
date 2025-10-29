9aba9bc041594fb4bb9711f4be69ad9b4a711ccd
Evgeny Kotov <evgeny.kotov@intel.com>
2025-10-29 14:45:20 +0100

    Fix ONNX ConvTranspose auto_pad behavior to comply with spec when output_shape is not provided

--- src/frontends/onnx/frontend/src/op/where.cpp
+++ src/frontends/onnx/frontend/src/op/where.cpp
@@ -12,7 +12,6 @@ namespace ai_onnx {
 namespace opset_1 {
 ov::OutputVector where(const ov::frontend::onnx::Node& node) {
     ov::OutputVector ov_inputs{node.get_ov_inputs()};
-
     return {std::make_shared<ov::op::v1::Select>(ov_inputs.at(0), ov_inputs.at(1), ov_inputs.at(2))};
 }
 ONNX_OP("Where", OPSET_SINCE(1), ai_onnx::opset_1::where);
