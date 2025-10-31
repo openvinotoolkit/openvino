8faa050c5ebcb534cd54e6ae695c188f49b8b675
Vladislav Golubev <vladislav.golubev@intel.com>
2025-10-31 19:03:09 +0900

    Introduced REPLACE_QDQ_WITH_CLAMP env variable

--- src/plugins/intel_gpu/src/plugin/transformations_pipeline.cpp
+++ src/plugins/intel_gpu/src/plugin/transformations_pipeline.cpp
@@ -399,7 +399,8 @@ void TransformationsPipeline::apply(std::shared_ptr<ov::Model> func) {
             // 2. Fuse FQ->Convert->DQ to a single FQ
             manager.register_pass<ov::pass::ConvertQuantizeDequantize>(TypeVector{i16, u16, i32}, TypeVector{f16, f32}, true);
             // 3. Strip FQ layers with unsupported levels
-            bool replace_with_clamp = true;
+            bool replace_with_clamp = ov::util::getenv_bool("REPLACE_QDQ_WITH_CLAMP", true);
+            std::cout << "[ QDQ STRIPPING INFO ] replace_with_clamp = " << replace_with_clamp << std::endl;
             manager.register_pass<FQStrippingTransformation>(std::set<size_t>{levels::int16}, replace_with_clamp);
         }
 
