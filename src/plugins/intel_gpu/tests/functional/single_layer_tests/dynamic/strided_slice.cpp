b46ec5d1aa6f0d0e2a58d9745da3a3a88734fc67
Kim, SungEun <sungeun.kim@intel.com>
2025-11-04 13:46:39 +0900

    add test-case for CVS-175804

--- src/plugins/intel_gpu/tests/functional/single_layer_tests/dynamic/strided_slice.cpp
+++ src/plugins/intel_gpu/tests/functional/single_layer_tests/dynamic/strided_slice.cpp
@@ -406,6 +406,9 @@ const std::vector<InputShape> inputShapesDynamic5D = {
 
         {{1, -1, 16, 32, -1},
         {{ 1, 16, 16, 32, 1 }, { 1, 32, 16, 32, 1 }, { 1, 64, 16, 32, 1 }}},
+
+        {{-1, -1, -1, -1, 4},
+        {{2, 1, 3, 4, 4}}},
 };
 
 INSTANTIATE_TEST_SUITE_P(smoke_CompareWithRefs_Common_Dynamic_5D, StridedSliceLayerGPUTest,
@@ -416,6 +419,28 @@ INSTANTIATE_TEST_SUITE_P(smoke_CompareWithRefs_Common_Dynamic_5D, StridedSliceLa
                              ::testing::ValuesIn(rest_input_types)),
                          StridedSliceLayerGPUTest::getTestCaseName);
 
+const std::vector<StridedSliceParams> testCasesCommon5D_shrink = {
+        StridedSliceParams{ { 0, 2, 5, 4 }, { 1, 4, 28, 27 }, { 1, 1, 1, 1 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 },  { },  { },  { } },
+        StridedSliceParams{ { 0, 0, 10, 20 }, { 1, 5, 28, 26 }, { 1, 1, 1, 2 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 },  { },  { },  { } },
+        StridedSliceParams{ { 0, 0, 0, 20 }, { 1, 2, 30, 30 }, { 1, 1, 2, 1 }, { 0, 0, 0, 1 }, { 0, 1, 0, 1 },  { },  { },  { } },
+        StridedSliceParams{ { 0, 1, 2, 10 }, { 1, 5, 32, 18 }, { 1, 1, 1, 2 }, { 0, 0, 1, 0 }, { 0, 0, 0, 1 },  { },  { },  { } },
+        StridedSliceParams{ { 0, 0, 2, 10 }, { 1, 8, 32, 18 }, { 1, 2, 1, 2 },  { 0, 0, 1, 0 }, { 0, 0, 0, 1 },  { },  { },  { } },
+        StridedSliceParams{ { 0, 0, 2 }, { 1, 8, 32 }, { 1, 2, 1 },  { 1, 0, 1 }, { 0, 0, 0 },  { },  { },  { 0, 1, 0} },
+        StridedSliceParams{ { 1 }, { 2 }, { 1 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0 },  { 1, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0 } },
+};
+
+const std::vector<InputShape> inputShapesDynamic5D_shrink = {
+        {{-1, -1, -1, -1, 4},
+        {{2, 1, 3, 4, 4}}},
+};
+
+INSTANTIATE_TEST_SUITE_P(smoke_CompareWithRefs_Common_Dynamic_5D_shrink, StridedSliceLayerGPUTest,
+                         ::testing::Combine(
+                             ::testing::ValuesIn(inputShapesDynamic5D_shrink),
+                             ::testing::ValuesIn(testCasesCommon5D_shrink),
+                             ::testing::ValuesIn(model_types),
+                             ::testing::ValuesIn(rest_input_types)),
+                         StridedSliceLayerGPUTest::getTestCaseName);
 
 const std::vector<StridedSliceParams> testCasesCommon6D = {
         StridedSliceParams{ { 0, 2, 5, 4 }, { 1, 4, 28, 27 }, { 1, 1, 1, 1 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 },  { },  { },  { } },
