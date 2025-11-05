89ce4c9b4ba7a4dc623ffeeee00abf901e634962
Andrii Staikov <andrii.staikov@intel.com>
2025-11-05 16:43:14 +0100

    remove unnecessary

--- src/frontends/onnx/tests/tests_python/test_backend.py
+++ src/frontends/onnx/tests/tests_python/test_backend.py
@@ -630,6 +630,8 @@ tests_expected_to_fail = [
     ),
     (
         xfail_issue_139937,
+        "OnnxBackendNodeModelTest.test_dequantizelinear_blocked_cpu",
+        "OnnxBackendNodeModelTest.test_qlinearmatmul_2D_int8_float16_cpu",
     ),
     (
         xfail_issue_139938,
