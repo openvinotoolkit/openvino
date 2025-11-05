fd48bf4d0d9b5acc35d2ab88d36a49cb79584602
Maksim Kutakov <maxim.kutakov@gmail.com>
2025-11-05 11:09:57 +0100

    Remove unnecessary log message

--- src/plugins/intel_gpu/src/plugin/transformations_pipeline.cpp
+++ src/plugins/intel_gpu/src/plugin/transformations_pipeline.cpp
@@ -400,7 +400,6 @@ void TransformationsPipeline::apply(std::shared_ptr<ov::Model> func) {
             manager.register_pass<ov::pass::ConvertQuantizeDequantize>(TypeVector{i16, u16}, TypeVector{f32}, true);
             // 3. Strip FQ layers with unsupported levels
             bool replace_with_clamp = ov::util::getenv_bool("REPLACE_QDQ_WITH_CLAMP", true);
-            std::cout << "[ QDQ STRIPPING INFO ] replace_with_clamp = " << replace_with_clamp << std::endl;
             manager.register_pass<FQStrippingTransformation>(std::set<size_t>{levels::int16}, replace_with_clamp);
             manager.register_pass<ov::pass::Validate>();
         }
