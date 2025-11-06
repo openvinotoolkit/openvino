0202f26c950d67cf729a3e6ce131aa2eda3c42b2
Zhang Yi <yi3.zhang@intel.com>
2025-11-06 15:01:53 +0800

    fix compilation

--- src/plugins/intel_gpu/src/graph/graph_optimizer/mark_runtime_skippable_nodes.cpp
+++ src/plugins/intel_gpu/src/graph/graph_optimizer/mark_runtime_skippable_nodes.cpp
@@ -296,7 +296,7 @@ void mark_runtime_skippable_nodes::run(program& p) {
             auto prim = impl_params->typed_desc<resample>();
             const auto& pads_begin = prim->pads_begin;
             const auto& pads_end = prim->pads_end;
-            bool no_padding = all_zeroes(pads_begin) && all_zeroes(pads_begin);
+            bool no_padding = all_zeroes(pads_begin) && all_zeroes(pads_end);
             if (!node.has_fused_primitives() && no_padding) {
                 node.can_be_optimized(true);
                 node.set_runtime_skippable(true);
