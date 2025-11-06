9cadda504ec279a0497b18c928b33f8bf1114d5d
Bogdan Pereanu <bogdan.pereanu@intel.com>
2025-11-06 20:40:32 +0200

    Enable ZE_NPU_CONTEXT_OPTION_ENABLE_IDLE_OPTIMIZATIONS
    Signed-off-by: Bogdan Pereanu <bogdan.pereanu@intel.com>

--- src/plugins/intel_npu/src/utils/src/zero/zero_init.cpp
+++ src/plugins/intel_npu/src/utils/src/zero/zero_init.cpp
@@ -295,12 +295,45 @@ ZeroInitStructsHolder::ZeroInitStructsHolder()
     graph_profiling_npu_dditable_ext_decorator =
         std::make_unique<ze_graph_profiling_ddi_table_ext_decorator>(_graph_profiling_ddi_table_ext);
 
+    // Query our context extension version
+    std::string context_ext_name;
+    uint32_t context_ext_version = 0;
+    std::tie(context_ext_version, context_ext_name) =
+        queryDriverExtensionVersion(ZE_CONTEXT_NPU_EXT_NAME, ZE_CONTEXT_NPU_EXT_VERSION_CURRENT, extProps, count);
+
+    log.debug("NPU context extension version %d.%d",
+              ZE_MAJOR_VERSION(context_ext_version),
+              ZE_MINOR_VERSION(context_ext_version));
+
+    // Load our context extension
+    ze_context_npu_dditable_ext_last_t* _context_npu_dditable_ext = nullptr;
+    if (context_ext_version) {
+        THROW_ON_FAIL_FOR_LEVELZERO(
+            "zeDriverGetExtensionFunctionAddress " + context_ext_name,
+            zeDriverGetExtensionFunctionAddress(driver_handle,
+                                                context_ext_name.c_str(),
+                                                reinterpret_cast<void**>(&_context_npu_dditable_ext)));
+    }
+
+    context_npu_dditable_ext_decorator =
+        std::make_unique<ze_context_npu_dditable_ext_decorator>(_context_npu_dditable_ext, context_ext_version);
+
     uint32_t device_count = 1;
     // Get our target device
     THROW_ON_FAIL_FOR_LEVELZERO("zeDeviceGet", zeDeviceGet(driver_handle, &device_count, &device_handle));
 
     // Create context - share between the compiler and the backend
-    ze_context_desc_t context_desc = {ZE_STRUCTURE_TYPE_CONTEXT_DESC, 0, 0};
+    ze_context_desc_t context_desc = {ZE_STRUCTURE_TYPE_CONTEXT_DESC, nullptr, 0};
+
+    ze_context_properties_npu_ext_t context_properties = {};
+    if (context_ext_version >= ZE_MAKE_VERSION(1, 0)) {
+        context_options |= ZE_NPU_CONTEXT_OPTION_ENABLE_IDLE_OPTIMIZATIONS;
+
+        context_properties.stype = ZE_STRUCTURE_TYPE_CONTEXT_PROPERTIES_NPU_EXT;
+        context_properties.options = context_options;
+        context_desc.pNext = static_cast<const void*>(&context_properties);
+    }
+
     THROW_ON_FAIL_FOR_LEVELZERO("zeContextCreate", zeContextCreate(driver_handle, &context_desc, &context));
     log.debug("ZeroInitStructsHolder initialize complete");
 
@@ -338,8 +371,17 @@ const std::shared_ptr<ZeroInitStructsHolder>& ZeroInitStructsHolder::getInstance
 
 ZeroInitStructsHolder::~ZeroInitStructsHolder() {
     if (context) {
+        if (context_npu_dditable_ext_decorator->version() >= ZE_MAKE_VERSION(1, 0)) {
+            context_options &= ~(ZE_NPU_CONTEXT_OPTION_ENABLE_IDLE_OPTIMIZATIONS);
+            ze_context_properties_npu_ext_t context_properties = {ZE_STRUCTURE_TYPE_CONTEXT_PROPERTIES_NPU_EXT,
+                                                                  nullptr,
+                                                                  context_options};
+            context_npu_dditable_ext_decorator->pfnSetProperties(context, &context_properties);
+        }
+
         log.debug("ZeroInitStructsHolder - performing zeContextDestroy");
         auto result = zeContextDestroy(context);
+        context = nullptr;
         if (result != ZE_RESULT_SUCCESS) {
             if (result == ZE_RESULT_ERROR_UNINITIALIZED) {
                 log.warning("zeContextDestroy failed to destroy the context; Level zero context was already destroyed");
