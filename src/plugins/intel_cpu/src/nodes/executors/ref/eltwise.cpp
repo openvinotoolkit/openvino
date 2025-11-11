61e28924b989a357e9101d8ae9a28c6b24f4f6b6
LiangGao <liang.gao@intel.com>
2025-11-11 13:42:29 +0800

    Fix clang

--- src/plugins/intel_cpu/src/nodes/executors/ref/eltwise.cpp
+++ src/plugins/intel_cpu/src/nodes/executors/ref/eltwise.cpp
@@ -404,16 +404,16 @@ void EltwiseRefExecutor<T, Enable>::exec(const jit_eltwise_call_args_ptrs& args_
 }
 
 template <>
-void EltwiseRefExecutor<int64_t>::exec(const jit_eltwise_call_args_ptrs& args_ptrs, const VectorDims& dims_out) {
+void EltwiseRefExecutor<int64_t>::exec(const jit_eltwise_call_args_ptrs& args_ptrs, const VectorDims& ) {
     // Only supported Clamp Operator
     if (this->m_opData.algo == Algorithm::EltwiseClamp) {
-        const int64_t* src_ptr_f = reinterpret_cast<const int64_t*>(args_ptrs.src_ptr[0]);
-        int64_t* dst_ptr_f = reinterpret_cast<int64_t*>(args_ptrs.dst_ptr);
-        int64_t lower = static_cast<int64_t>(this->m_opData.alpha);
-        int64_t upper = static_cast<int64_t>(this->m_opData.beta);
+        const auto* src_ptr_i = reinterpret_cast<const int64_t*>(args_ptrs.src_ptr[0]);
+        auto* dst_ptr_i = reinterpret_cast<int64_t*>(args_ptrs.dst_ptr);
+        auto lower = static_cast<int64_t>(this->m_opData.alpha);
+        auto upper = static_cast<int64_t>(this->m_opData.beta);
 
         parallel_for(this->m_fullWorkAmount, [&](size_t i) {
-            dst_ptr_f[i] = (src_ptr_f[i] < lower) ? lower : (src_ptr_f[i] > upper) ? upper : src_ptr_f[i];
+            dst_ptr_i[i] = (src_ptr_i[i] < lower) ? lower : (src_ptr_i[i] > upper) ? upper : src_ptr_i[i];
         });
         return;
     }
