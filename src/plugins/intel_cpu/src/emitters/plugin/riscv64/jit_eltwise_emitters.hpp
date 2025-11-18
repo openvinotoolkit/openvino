1ca4ac938e3bd68e0fb65512f548fe06de10d092
Ram <136904304+IamRam3@users.noreply.github.com>
2025-11-18 23:59:13 +0530

    [CPU][RV64] Implement jit_squared_difference_emitter (#32722)
    **Overview:** 
    - This PR fixes - **#30260**
    
    **Description:**
    - Added JIT emitter for SQUARED_DIFF (fp32).
    - Integrated emitter support in the JIT Executor for RISC-V.
    - Used auxiliary registers to store intermediate results and avoid
    overwriting registers — please advise whether this approach is useful or
    not.
    - Updated functional tests — initially 58 tests failed as expected
    (before emitter implementation).
    After emitter integration, only 9 tests fail (likely due to partial
    datatype support — only f32 implemented, not i32).
    
    How to test:
    1. Build OpenVINO for RISC-V (with tests enabled).
    2. Run:
    ~/xuantie-install/bin/qemu-riscv64 -cpu rv64,x-v=true,vext_spec=v1.0 \
      ./bin/riscv64/Release/ov_cpu_func_tests \
      --gtest_filter="*smoke*Eltwise*SqDiff*"
    
    **CC**:
       @aobolensk

--- src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.hpp
+++ src/plugins/intel_cpu/src/emitters/plugin/riscv64/jit_eltwise_emitters.hpp
@@ -993,6 +993,26 @@ private:
     void emit_isa(const std::vector<size_t>& in_vec_idxs, const std::vector<size_t>& out_vec_idxs) const;
 };
 
+class jit_squared_difference_emitter : public jit_emitter {
+public:
+    jit_squared_difference_emitter(ov::intel_cpu::riscv64::jit_generator_t* host,
+                                   ov::intel_cpu::riscv64::cpu_isa_t host_isa,
+                                   const std::shared_ptr<ov::Node>& node);
+    jit_squared_difference_emitter(ov::intel_cpu::riscv64::jit_generator_t* host,
+                                   ov::intel_cpu::riscv64::cpu_isa_t host_isa,
+                                   ov::element::Type exec_prc = ov::element::f32);
+
+    size_t get_inputs_num() const override;
+
+    static std::set<std::vector<element::Type>> get_supported_precisions(
+        const std::shared_ptr<ov::Node>& node = nullptr);
+
+private:
+    void emit_impl(const std::vector<size_t>& in_vec_idxs, const std::vector<size_t>& out_vec_idxs) const override;
+    template <ov::intel_cpu::riscv64::cpu_isa_t isa>
+    void emit_isa(const std::vector<size_t>& in_vec_idxs, const std::vector<size_t>& out_vec_idxs) const;
+};
+
 class jit_subtract_emitter : public jit_emitter {
 public:
     jit_subtract_emitter(ov::intel_cpu::riscv64::jit_generator_t* host,
