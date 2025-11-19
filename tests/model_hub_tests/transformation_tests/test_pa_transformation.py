4420f004fb17c42c94471199b5c862939cdcffa3
Kim, SungEun <sungeun.kim@intel.com>
2025-11-19 09:11:23 +0900

    Revert "[TRANSFORMATIONS] Move SDPAToPA functionality from GenAI to transformation (#32570)"
    This reverts commit d555df89281daf515b807405c58b969dd4c6c936.

--- tests/model_hub_tests/transformation_tests/test_pa_transformation.py
+++ tests/model_hub_tests/transformation_tests/test_pa_transformation.py
@@ -4,7 +4,6 @@
 from huggingface_hub import snapshot_download
 from openvino._offline_transformations import paged_attention_transformation
 from openvino._pyopenvino.op import _PagedAttentionExtension
-from openvino._pyopenvino import Type as OVType
 from optimum.intel import OVModelForCausalLM
 from optimum.intel.openvino import OVModelForVisualCausalLM
 from typing import Union
@@ -53,11 +52,10 @@ def apply_transformation_and_compare_diffs(ov_model: ov.Model,
         names = list(input.get_names()) # names stored in as set (in this case usually of 1 element)
         for name in names:
             if (("key_cache." in name) or ("value_cache." in name)):
-                assert input.get_element_type() == OVType.dynamic
                 shape = input.get_partial_shape()
-                for i in range(shape.rank.get_length()):
-                    # PagedAttention uses key_cache and value_cache inputs with all 4 dims being dynamic
-                    assert shape[i].is_dynamic, "Dimension {i} of input '{name}' in '{model_id}' is not dynamic: {shape}" 
+                # PagedAttention uses key_cache and value_cache inputs so the last 2 dimensions have to be static
+                assert shape[-1].is_static, f"Dimension {len(shape) - 1} of input '{name}' in '{model_id}' is not static: {shape}"
+                assert shape[-2].is_static, f"Dimension {len(shape) - 2} of input '{name}' in '{model_id}' is not static: {shape}"
 
     interesting_input_patterns = {}
     interesting_output_patterns = {}
