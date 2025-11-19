5bb37032e4c865e959c00c3891b7784483827885
wilson-seok <wilson.seok@intel.com>
2025-11-19 15:53:53 +0000

    fix unit test faiure

--- src/plugins/intel_gpu/tests/unit/fusions/mvn_fusion_test.cpp
+++ src/plugins/intel_gpu/tests/unit/fusions/mvn_fusion_test.cpp
@@ -30,6 +30,7 @@ struct mvn_test_params {
     size_t expected_fused_primitives;
     size_t expected_fused_primitives_onednn;
     size_t expected_not_fused_primitives;
+    bool mvn_ref_non_fusing_network = false;
 };
 
 class MVNFusingTest : public ::BaseFusingTest<mvn_test_params> {
@@ -80,7 +81,7 @@ public:
 #define CASE_MVN_I8_6       { 2, 16, 8, 8 },    { 1, 1, 1, 1 },     data_types::i8, format::b_fs_yx_fsv16, {1, 2, 3}, true, data_types::f32, format::bfyx
 #define CASE_MVN_I8_7       { 2, 16, 1, 8 },    { 1, 1, 8, 1 },     data_types::i8, format::b_fs_yx_fsv16, {1, 2, 3}, true, data_types::f32, format::bfyx
 #define CASE_MVN_I8_8       { 2, 16, 3, 8 },    { 1, 1, 3, 8 },     data_types::i8, format::b_fs_yx_fsv16, {3}, true, data_types::f32, format::bfyx
-#define CASE_MVN_I8_8_NA    { 2, 15, 3, 8 },    { 1, 1, 3, 8 },     data_types::i8, format::b_fs_yx_fsv16, {3}, true, data_types::f32, format::bfyx
+#define CASE_MVN_I8_8_NA    { 2, 15, 3, 8 },    { 1, 1, 3, 8 },     data_types::i8, format::bfyx, {3}, true, data_types::f32, format::bfyx
 #define CASE_MVN_3D_I8_1    { 1, 16, 8, 8, 8 }, { 1, 16, 8, 8, 8 }, data_types::i8, format::bfzyx, {2, 3, 4}, true, data_types::f32, format::bfzyx
 #define CASE_MVN_3D_I8_2    { 2, 16, 8, 8, 8 }, { 2, 16, 8, 8, 8 }, data_types::i8, format::bfzyx, {1, 2, 3, 4}, true, data_types::f32, format::bfzyx
 #define CASE_MVN_3D_I8_3    { 2, 16, 8, 8, 8 }, { 2, 1, 8, 8, 1 },  data_types::i8, format::bfzyx, {1, 2, 3, 4}, true, data_types::f32, format::bfzyx
