13700e2c4f575d505ddb744f6f0a5ac9c4ebf9ec
Bogdan Pereanu <bogdan.pereanu@intel.com>
2025-11-25 13:59:35 +0200

    Use only one public property
    Signed-off-by: Bogdan Pereanu <bogdan.pereanu@intel.com>

--- src/plugins/intel_npu/src/plugin/src/properties.cpp
+++ src/plugins/intel_npu/src/plugin/src/properties.cpp
@@ -488,9 +488,7 @@ void Properties::registerPluginProperties() {
     TRY_REGISTER_SIMPLE_PROPERTY(ov::intel_npu::npuw::whisper::enabled, NPUW_WHISPER);
     TRY_REGISTER_SIMPLE_PROPERTY(ov::intel_npu::inputs_with_dynamic_strides, INPUTS_WITH_DYNAMIC_STRIDES);
     TRY_REGISTER_SIMPLE_PROPERTY(ov::intel_npu::outputs_with_dynamic_strides, OUTPUTS_WITH_DYNAMIC_STRIDES);
-    TRY_REGISTER_SIMPLE_PROPERTY(ov::intel_npu::inputs_with_dynamic_strides_string, INPUTS_WITH_DYNAMIC_STRIDES_STRING);
-    TRY_REGISTER_SIMPLE_PROPERTY(ov::intel_npu::outputs_with_dynamic_strides_string,
-                                 OUTPUTS_WITH_DYNAMIC_STRIDES_STRING);
+    TRY_REGISTER_SIMPLE_PROPERTY(ov::intel_npu::dynamic_strides, DYNAMIC_STRIDES);
 
     // 2. Metrics (static device and enviroment properties)
     // ========
@@ -631,10 +629,7 @@ void Properties::registerCompiledModelProperties() {
     TRY_REGISTER_COMPILEDMODEL_PROPERTY_IFSET(ov::intel_npu::inputs_with_dynamic_strides, INPUTS_WITH_DYNAMIC_STRIDES);
     TRY_REGISTER_COMPILEDMODEL_PROPERTY_IFSET(ov::intel_npu::outputs_with_dynamic_strides,
                                               OUTPUTS_WITH_DYNAMIC_STRIDES);
-    TRY_REGISTER_COMPILEDMODEL_PROPERTY_IFSET(ov::intel_npu::inputs_with_dynamic_strides_string,
-                                              INPUTS_WITH_DYNAMIC_STRIDES_STRING);
-    TRY_REGISTER_COMPILEDMODEL_PROPERTY_IFSET(ov::intel_npu::outputs_with_dynamic_strides_string,
-                                              OUTPUTS_WITH_DYNAMIC_STRIDES_STRING);
+    TRY_REGISTER_COMPILEDMODEL_PROPERTY_IFSET(ov::intel_npu::dynamic_strides, DYNAMIC_STRIDES);
 
     TRY_REGISTER_VARPUB_PROPERTY(ov::intel_npu::batch_mode, BATCH_MODE, false);
 
