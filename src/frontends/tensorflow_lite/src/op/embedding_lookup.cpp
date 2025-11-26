4291e7a547a700fbc0bb8209e27610f8e42797c6
Anoob <anoob.anto.kodankandath@intel.com>
2025-11-26 12:30:09 +0530

    [TF Lite FE] Embedding Lookup implementation (#32820)
    Implementing embedding lookup op for tflite frontend using gather op.
    
    The openvino Gather op expects the inputs to be:
    1.data: The tensor from which slices are gathered. 2.indices: Tensor
    with indexes to gather.
    3.axis: Dimension index to gather data from.
    
    The tfl EmbeddingLookupOp provides:
    1.lookup: Tensor with indices to look for.
    2.value: Tensor holding the data values.
    
    ### Details:
     - *item1*
     - *...*
    
    ### Tickets:
     - *ticket-id*
    
    Signed-off-by: Anoob Anto Kodankandath <anoob.anto.kodankandath@intel.com>
    Signed-off-by: Ritul Jasuja <ritul.jasuja@intel.com>

---
+++ src/frontends/tensorflow_lite/src/op/embedding_lookup.cpp
@@ -0,0 +1,28 @@
+// Copyright (C) 2018-2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "common_op_table.hpp"
+#include "op_translation_utils.hpp"
+#include "utils.hpp"
+
+using namespace std;
+
+namespace ov {
+namespace frontend {
+namespace tensorflow_lite {
+namespace op {
+
+OutputVector embedding_lookup(const ov::frontend::tensorflow_lite::NodeContext& node) {
+    auto axis = opset10::Constant::create(element::i32, {}, {0});
+    auto lookup_indices = node.get_input(0);
+    auto data_values = node.get_input(1);
+    auto res = make_shared<opset10::Gather>(data_values, lookup_indices, axis);
+    res->set_friendly_name(node.get_name());
+    return res->outputs();
+}
+
+}  // namespace op
+}  // namespace tensorflow_lite
+}  // namespace frontend
+}  // namespace ov
