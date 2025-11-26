191ad6a30ec1f447a2f1c18bb04ecf305c394426
Shen, Wanglei <wanglei.shen@intel.com>
2025-11-26 08:59:05 +0800

    update for comment

--- src/plugins/intel_cpu/src/cpu_streams_calculation.cpp
+++ src/plugins/intel_cpu/src/cpu_streams_calculation.cpp
@@ -721,7 +721,7 @@ int get_model_prefer_threads(const int num_streams,
             if ((proc_type_table[0][MAIN_CORE_PROC] < config.threads || config.threads == 0) &&
                 (ov::get_number_of_blocked_cores() || proc_type_table[0][LP_EFFICIENT_CORE_PROC] > 0) &&
                 proc_type_table[0][EFFICIENT_CORE_PROC] <= 2 * proc_type_table[0][MAIN_CORE_PROC]) {
-                if (int8_intensive || llm_related) {
+                if (llm_related) {
                     config.modelPreferThreadsLatency = proc_type_table[0][MAIN_CORE_PROC];
                 } else {
                     config.modelPreferThreadsLatency =
