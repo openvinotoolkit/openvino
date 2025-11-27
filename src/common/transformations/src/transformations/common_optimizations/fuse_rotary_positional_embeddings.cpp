defecec6bbcf1322f17a59c10919ad40895c38dd
Zhang Yi <yi3.zhang@intel.com>
2025-11-27 22:19:44 +0800

    make pattern versatile

--- src/common/transformations/src/transformations/common_optimizations/fuse_rotary_positional_embeddings.cpp
+++ src/common/transformations/src/transformations/common_optimizations/fuse_rotary_positional_embeddings.cpp
@@ -787,10 +787,10 @@ ov::pass::RoPEFusionChatGLMHF::RoPEFusionChatGLMHF() {
                                                    {{"special_zero", false}});
 
     auto vsplit_out0 = pattern::wrap_type<op::v1::VariadicSplit>(
-        {reshape, 3, {"ndims", "ndims"}},
+        {reshape, 3, pattern::any_input()},
         pattern::output_index_matches(0) && pattern::shape_matches("[?, head_cnt, 1, ndims]"));
     auto vsplit_out1 = pattern::wrap_type<op::v1::VariadicSplit>(
-        {reshape, 3, {"ndims", "ndims"}},
+        {reshape, 3, pattern::any_input()},
         pattern::output_index_matches(1) && pattern::shape_matches("[?, head_cnt, 1, ndims]"));
     auto slice_1 = NewGenSlice(reshape, 0, "ndims", 1, 3) | vsplit_out0;
 
