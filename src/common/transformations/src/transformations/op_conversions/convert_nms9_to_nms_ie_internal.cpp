032ffd1f0790dae3a9ac29723c3d68bd2965f29b
Evgeny Kotov <evgeny.kotov@intel.com>
2025-12-05 15:55:30 +0100

    Remove deprecated create_ie_output_name and get_ie_output_name functions from transformation utils

--- src/common/transformations/src/transformations/op_conversions/convert_nms9_to_nms_ie_internal.cpp
+++ src/common/transformations/src/transformations/op_conversions/convert_nms9_to_nms_ie_internal.cpp
@@ -16,7 +16,6 @@
 #include "openvino/op/reshape.hpp"
 #include "openvino/pass/pattern/op/wrap_type.hpp"
 #include "ov_ops/nms_ie_internal.hpp"
-#include "transformations/utils/utils.hpp"
 
 ov::pass::ConvertNMS9ToNMSIEInternal::ConvertNMS9ToNMSIEInternal() {
     MATCHER_SCOPE(ConvertNMS9ToNMSIEInternal);
@@ -110,18 +109,20 @@ ov::pass::ConvertNMS9ToNMSIEInternal::ConvertNMS9ToNMSIEInternal() {
         Output<Node> output_0 = nms_legacy->output(0);
         if (nms_9->output(0).get_element_type() != output_0.get_element_type()) {
             output_0 = std::make_shared<ov::op::v0::Convert>(output_0, nms_9->output(0).get_element_type());
-            OPENVINO_SUPPRESS_DEPRECATED_START
-            output_0.get_node_shared_ptr()->set_friendly_name(op::util::create_ie_output_name(nms_9->output(0)));
-            OPENVINO_SUPPRESS_DEPRECATED_END
+            const auto& prev_layer_0 = nms_9->output(0).get_node_shared_ptr();
+            const std::string friendly_name_0 = prev_layer_0->get_friendly_name() +
+                (prev_layer_0->get_output_size() != 1 ? "." + std::to_string(nms_9->output(0).get_index()) : "");
+            output_0.get_node_shared_ptr()->set_friendly_name(friendly_name_0);
             new_ops.emplace_back(output_0.get_node_shared_ptr());
         }
 
         Output<Node> output_2 = nms_legacy->output(2);
         if (nms_9->output(2).get_element_type() != output_2.get_element_type()) {
             output_2 = std::make_shared<ov::op::v0::Convert>(output_2, nms_9->output(2).get_element_type());
-            OPENVINO_SUPPRESS_DEPRECATED_START
-            output_2.get_node_shared_ptr()->set_friendly_name(op::util::create_ie_output_name(nms_9->output(2)));
-            OPENVINO_SUPPRESS_DEPRECATED_END
+            const auto& prev_layer_2 = nms_9->output(2).get_node_shared_ptr();
+            const std::string friendly_name_2 = prev_layer_2->get_friendly_name() +
+                (prev_layer_2->get_output_size() != 1 ? "." + std::to_string(nms_9->output(2).get_index()) : "");
+            output_2.get_node_shared_ptr()->set_friendly_name(friendly_name_2);
             new_ops.emplace_back(output_2.get_node_shared_ptr());
         }
 
