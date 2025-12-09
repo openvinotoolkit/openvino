831b58a9db881626f54c5d8888b58f6765b8ff3b
Beheshti <nazanin.beheshti@intel.com>
2025-12-09 07:20:49 -0800

    required changes for CoPilot tests

--- src/common/low_precision_transformations/src/split.cpp
+++ src/common/low_precision_transformations/src/split.cpp
@@ -97,6 +97,11 @@ bool SplitTransformation::transform(ov::pass::pattern::Matcher& m) {
             copy_runtime_info({ newSplit, subtract }, subtract);
             parent = subtract;
         }
+        if (parent.get_element_type() != splitedMul[i].get_element_type()) {
+            auto convert =
+                std::make_shared<ov::op::TypeRelaxed<ov::opset1::Convert>>(splitedMul[i], parent.get_element_type());
+            splitedMul[i] = convert;
+        }
 
         const auto multiply = std::make_shared<ov::op::TypeRelaxed<ov::opset1::Multiply>>(parent, splitedMul[i]);
         NetworkHelper::setOutDataPrecisionForTypeRelaxed(multiply, dequantization.multiply->get_output_element_type(0));
