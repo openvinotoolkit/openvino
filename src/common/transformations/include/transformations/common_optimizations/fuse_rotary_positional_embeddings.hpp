880a34c9f3f88b937e5b8f6e4fca7ad9d24c43ca
Chen, Peter <peter.chen@intel.com>
2025-12-10 19:05:07 +0800

    Revert "[GPU][CPU]Enable Rope Fusion of GPT-OSS. (#33087)"
    This reverts commit 4312e0a10d44a1f7e30454dd04adfe0d5029320a.

--- src/common/transformations/include/transformations/common_optimizations/fuse_rotary_positional_embeddings.hpp
+++ src/common/transformations/include/transformations/common_optimizations/fuse_rotary_positional_embeddings.hpp
@@ -21,7 +21,6 @@ class TRANSFORMATIONS_API RoPEFusionIOSlicing;
 class TRANSFORMATIONS_API RoPEFusionPreprocess;
 class TRANSFORMATIONS_API RoPEFusionCosSinPreprocess;
 class TRANSFORMATIONS_API RoPEShareCosSin;
-class TRANSFORMATIONS_API RoPEFusionGPTOSS;
 
 }  // namespace pass
 }  // namespace ov
@@ -90,12 +89,6 @@ private:
     std::shared_ptr<ov::Node> m_shared_cos0;
     std::shared_ptr<ov::Node> m_shared_sin0;
     std::vector<std::shared_ptr<ov::Node>> m_shared_inputs{2, nullptr};
-};
-
-class ov::pass::RoPEFusionGPTOSS : public ov::pass::MatcherPass {
-public:
-    OPENVINO_MATCHER_PASS_RTTI("RoPEFusionGPTOSS");
-    RoPEFusionGPTOSS();
 };
 
 /**
