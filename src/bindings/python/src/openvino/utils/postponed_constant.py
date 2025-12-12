373b80413986b140ab4483a84c828dd42f354d20
Alicja Miloszewska <alicja.miloszewska@intel.com>
2025-12-12 16:04:18 +0100

    [PyOV] Remove deprecated PostponedConstant overload (#33207)
    ### Tickets:
     - [CVS-173175](https://jira.devtools.intel.com/browse/CVS-173175)

--- src/bindings/python/src/openvino/utils/postponed_constant.py
+++ src/bindings/python/src/openvino/utils/postponed_constant.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-from typing import Optional, cast, overload
+from typing import Optional
 from collections.abc import Callable
 from openvino import Op, Type, Shape, Tensor, PartialShape, TensorVector
 
@@ -15,20 +15,8 @@ class PostponedConstant(Op):
     to IR and then immediately disposes of it.
     """
 
-    @overload
     def __init__(
         self, element_type: Type, shape: Shape, maker: Callable[[], Tensor], name: Optional[str] = None
-    ) -> None:
-        ...
-
-    @overload
-    def __init__(
-        self, element_type: Type, shape: Shape, maker: Callable[[Tensor], None], name: Optional[str] = None
-    ) -> None:
-        ...
-
-    def __init__(
-        self, element_type: Type, shape: Shape, maker: Callable, name: Optional[str] = None
     ) -> None:
         """Creates a PostponedConstant.
 
@@ -36,11 +24,8 @@ class PostponedConstant(Op):
         :type element_type: openvino.Type
         :param shape: Shape of the constant.
         :type shape: openvino.Shape
-        :param maker: A callable that returns a Tensor or modifies the provided Tensor to
-                    represent the constant.
-                    Note: It's recommended to use a callable without arguments (returns Tensor)
-                    to avoid unnecessary tensor data copies.
-        :type maker: Union[Callable[[], Tensor], Callable[[Tensor], None]]
+        :param maker: A callable that returns a Tensor.
+        :type maker: Callable[[], Tensor]
         :param name: Optional name for the constant.
         :type name: Optional[str]
 
@@ -60,30 +45,10 @@ class PostponedConstant(Op):
         self.m_maker = maker
         if name is not None:
             self.friendly_name = name
-
-        if (
-            callable(self.m_maker)
-            and hasattr(self.m_maker.__call__, "__code__")
-            and self.m_maker.__call__.__code__.co_argcount > 1
-        ):
-            from openvino._pyopenvino.util import deprecation_warning
-
-            deprecation_warning(
-                "PostponedConstant.__init__ with Callable[[Tensor], None]",
-                "2026.0",
-                "Please use PostponedConstant's 'maker' argument as Callable[[], Tensor] "
-                "instead of Callable[[Tensor], None].",
-                3,
-            )
-
         self.constructor_validate_and_infer_types()
 
     def evaluate(self, outputs: TensorVector, _: list[Tensor]) -> bool:  # type: ignore
-        num_args = self.m_maker.__call__.__code__.co_argcount
-        if num_args == 1:
-            outputs[0] = cast(Callable[[], Tensor], self.m_maker)()
-        else:
-            cast(Callable[[Tensor], None], self.m_maker)(outputs[0])
+        outputs[0] = self.m_maker()
         return True
 
     def validate_and_infer_types(self) -> None:
@@ -97,5 +62,8 @@ class PostponedConstant(Op):
 
 
 # `maker` is a function that returns ov.Tensor that represents a target Constant
-def make_postponed_constant(element_type: Type, shape: Shape, maker: Callable, name: Optional[str] = None) -> Op:
+def make_postponed_constant(element_type: Type,
+                            shape: Shape,
+                            maker: Callable[[], Tensor],
+                            name: Optional[str] = None) -> Op:
     return PostponedConstant(element_type, shape, maker, name)
