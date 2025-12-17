d03a28011608ab40ec02e2f59bf8ffb66f7daef0
Aleksandr Voron <aleksandr.voron@intel.com>
2025-12-17 09:13:50 +0100

    [CPU][ARM] Limit cases when ACL int8 convolution executor is chosen (#33040)
    ### Details:
     - Do not fuse per-channel DQ scales to Convolution
    - Do not select int8 executor if output precision is quantized and FQ is
    not fused (i.e. requantize scale can not be applied)
    
    ### Tickets:
     - *ticket-id*

--- src/plugins/intel_cpu/src/graph_optimizer.cpp
+++ src/plugins/intel_cpu/src/graph_optimizer.cpp
@@ -298,7 +298,13 @@ void GraphOptimizer::FuseConvMatmulFCDeconvAndDQScales(Graph& graph) {
                 return false;
             }
         }
+
+#if defined(OPENVINO_ARCH_ARM) || defined(OPENVINO_ARCH_ARM64)
+        // Per-channel DQ scales fusion is not supported by ACL
+        return scalesDims[channelAxis] == 1;
+#else
         return true;
+#endif
     };
 
     auto initializeDeQuantizedScales = [](const NodePtr& node, const NodePtr& scales) {
