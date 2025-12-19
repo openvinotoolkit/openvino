e0f7b5723ebe05f9682dfa5c44e805d78243279e
Wangwang Wang <wangwang.wang@intel.com>
2025-12-19 17:33:11 +0800

    Remove debug messages

--- src/plugins/intel_gpu/src/plugin/transformations_pipeline.cpp
+++ src/plugins/intel_gpu/src/plugin/transformations_pipeline.cpp
@@ -569,10 +569,10 @@ void TransformationsPipeline::apply(std::shared_ptr<ov::Model> func) {
             // Disable XAttention if GPU Xe2/Xe3 architectures is unavaiable or IGC incompatiable.
             auto check_xattn_gpu_compatibility  = [&](void) -> bool {
                         auto& engine = m_context->get_engine();
-                        // const auto& info = engine.get_device_info();
-                        // if (info.arch != cldnn::gpu_arch::xe2 && info.arch != cldnn::gpu_arch::xe3) { // CM optimized for systolic-array architectures
-                        //     return false;
-                        // }
+                        const auto& info = engine.get_device_info();
+                        if (info.arch != cldnn::gpu_arch::xe2 && info.arch != cldnn::gpu_arch::xe3 && info.arch != cldnn::gpu_arch::xe_hpg) { // CM optimized for systolic-array architectures
+                            return false;
+                        }
 
 #ifdef GPU_DEBUG_CONFIG
                         if (!config.get_use_cm()) {
