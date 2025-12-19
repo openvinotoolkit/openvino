d4d3bcdefcc11936c77d117851ff8c72d85fc6b9
Razvan Apetroaie <razvan-mihai.apetroaie@intel.com>
2025-12-19 14:03:33 +0200

    [NPU] Weights separation tests fix (#33310)
    ### Tickets:
     - *EISW-197365*

--- src/plugins/intel_npu/tests/functional/behavior/weights_separation.hpp
+++ src/plugins/intel_npu/tests/functional/behavior/weights_separation.hpp
@@ -103,8 +103,8 @@ public:
                                                const bool alternativeWeights = false) {
         constexpr auto precision = element::f32;
 
-        const uint8_t weightsValue = !alternativeWeights ? 1 : 2;
-        auto weights = std::make_shared<op::v0::Constant>(precision, Shape{50000}, std::vector<uint8_t>{weightsValue});
+        const float weightsValue = !alternativeWeights ? 1.0f : 2.0f;
+        auto weights = std::make_shared<op::v0::Constant>(precision, Shape{5}, std::vector<float>{weightsValue});
         auto input = std::make_shared<op::v0::Parameter>(precision, Shape{1});
         auto add = std::make_shared<op::v1::Add>(input, weights);
 
