c6960a45ea3014ade0263c5b6e04e48f8553754d
Liu, Dan1 <dan1.liu@intel.com>
2025-12-19 10:42:05 +0800

    update log to show compiler version

--- src/plugins/intel_npu/src/compiler_adapter/src/plugin_compiler_adapter.cpp
+++ src/plugins/intel_npu/src/compiler_adapter/src/plugin_compiler_adapter.cpp
@@ -76,14 +76,14 @@ PluginCompilerAdapter::PluginCompilerAdapter(const std::shared_ptr<ZeroInitStruc
     try {
         auto vclCompilerPtr = VCLCompilerImpl::getInstance();
         auto vclLib = vclCompilerPtr->getLinkedLibrary();
-        _logger.info("PLUGIN VCL compiler is loading");
+        _logger.error("PLUGIN VCL compiler is loading");
         if (vclCompilerPtr && vclLib) {
             _compiler = ov::SoPtr<intel_npu::ICompiler>(vclCompilerPtr, vclLib);
         } else {
             throw std::runtime_error("VCL compiler or library is nullptr");
         }
     } catch (const std::exception& vcl_exception) {
-        _logger.info("VCL compiler load failed: %s. Trying to load MLIR compiler...", vcl_exception.what());
+        _logger.error("VCL compiler load failed: %s. Trying to load MLIR compiler...", vcl_exception.what());
         std::string baseName = "npu_mlir_compiler";
         auto libPath = ov::util::make_plugin_library_name(ov::util::get_ov_lib_path(), baseName + OV_BUILD_POSTFIX);
         try {
