e496aed4d243702a8087c4981de0b15aaf34e9b9
Bogdan Pereanu <bogdan.pereanu@intel.com>
2025-12-22 22:11:46 +0200

    [NPU] Update npu zero ext and use correct stype (#33302)
    ### Details:
     - *Update npu zero ext*
     - *Use correct stype*
     - *Initialize variables*
    
    ### Tickets:
     - *N/A*
    
    ---------
    
    Signed-off-by: Bogdan Pereanu <bogdan.pereanu@intel.com>

--- src/plugins/intel_npu/tests/functional/internal/compiler_adapter/zero_init_mock.hpp
+++ src/plugins/intel_npu/tests/functional/internal/compiler_adapter/zero_init_mock.hpp
@@ -4,13 +4,11 @@
 
 #pragma once
 
-#include <ze_api.h>
-#include <ze_command_queue_npu_ext.h>
-#include <ze_graph_ext.h>
 #include <ze_intel_npu_uuid.h>
 
 #include <memory>
 #include <mutex>
+#include <optional>
 
 #include "intel_npu/utils/logger/logger.hpp"
 #include "intel_npu/utils/zero/zero_api.hpp"
@@ -24,26 +22,28 @@ struct ZeroInitStructsMock {
     ~ZeroInitStructsMock();
 
     void initNpuDriver();
+    void getExtensionFunctionAddress(const std::string& name, const uint32_t version, void** function_address);
 
-    std::shared_ptr<intel_npu::ZeroApi> zero_api;
+    std::shared_ptr<intel_npu::ZeroApi> _zero_api;
 
-    intel_npu::Logger log;
+    Logger _log;
 
-    ze_context_handle_t context = nullptr;
-    ze_driver_handle_t driver_handle = nullptr;
-    ze_device_handle_t device_handle = nullptr;
+    ze_context_handle_t _context = nullptr;
+    ze_driver_handle_t _driver_handle = nullptr;
+    ze_device_handle_t _device_handle = nullptr;
 
-    std::map<std::string, uint32_t> driver_extension_properties;
-    std::unique_ptr<ze_graph_dditable_ext_decorator> graph_dditable_ext_decorator;
-    std::unique_ptr<ze_command_queue_npu_dditable_ext_decorator> command_queue_npu_dditable_ext_decorator;
-    std::unique_ptr<ze_graph_profiling_ddi_table_ext_decorator> graph_profiling_npu_dditable_ext_decorator;
+    std::map<std::string, uint32_t> _driver_extension_properties;
+    std::unique_ptr<ze_graph_dditable_ext_decorator> _graph_dditable_ext_decorator;
+    std::unique_ptr<ze_command_queue_npu_dditable_ext_decorator> _command_queue_npu_dditable_ext_decorator;
+    std::unique_ptr<ze_graph_profiling_dditable_ext_decorator> _graph_profiling_npu_dditable_ext_decorator;
+    std::unique_ptr<ze_driver_npu_dditable_ext_decorator> _driver_npu_dditable_ext_decorator;
 
-    ze_driver_properties_t driver_properties = {};
-    uint32_t mutable_command_list_ext_version = 0;
+    ze_driver_properties_t _driver_properties = {};
+    uint32_t _mutable_command_list_ext_version = 0;
 
-    ze_api_version_t ze_drv_api_version = {};
+    ze_api_version_t _ze_drv_api_version = {};
 
-    std::unique_ptr<ze_device_graph_properties_t> compiler_properties = nullptr;
+    std::optional<ze_device_graph_properties_t> _compiler_properties = std::nullopt;
 
     bool _external_memory_standard_allocation_supported = false;
     bool _external_memory_fd_win32_supported = false;
