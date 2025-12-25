8dded71e939dc7adc4b8bfcb0eeeaa3caf2de871
intelgaoxiong <xiong.gao@intel.com>
2025-12-25 02:46:47 -0800

    Introduce in NPUW_MOE_TOKEN_CHUNK_SIZE.
    Signed-off-by: intelgaoxiong <xiong.gao@intel.com>

--- src/plugins/intel_npu/src/al/include/intel_npu/config/npuw.hpp
+++ src/plugins/intel_npu/src/al/include/intel_npu/config/npuw.hpp
@@ -118,6 +118,7 @@ DEFINE_OPT(NPUW_SPATIAL, bool, false, npuw::partitioning::spatial, RunTime);
 DEFINE_OPT(NPUW_F16IC, bool, true, npuw::partitioning::f16_interconnect, RunTime);
 DEFINE_OPT(NPUW_SPATIAL_NWAY, std::size_t, 128, npuw::partitioning::spatial_nway, RunTime);
 DEFINE_OPT(NPUW_SPATIAL_DYN, bool, true, npuw::partitioning::spatial_dyn, RunTime);
+DEFINE_OPT(NPUW_MOE_TOKEN_CHUNK_SIZE, uint64_t, 128, npuw::partitioning::moe_token_chunk_size, RunTime);
 DEFINE_OPT(NPUW_ATTN, std::string, "STATIC", npuw::partitioning::attn, RunTime);
 DEFINE_OPT(NPUW_ATTN_DYN, bool, true, npuw::partitioning::attn_dyn, RunTime);
 DEFINE_OPT(NPUW_ATTN_NO_COPY, bool, false, npuw::partitioning::attn_no_copy, RunTime);
