8c4c05a5266f2189c3beced5dc95bd421070be57
Beheshti <nazanin.beheshti@intel.com>
2025-12-30 15:18:12 -0800

    refactoring

--- src/plugins/intel_gpu/src/graph/graph_optimizer/prepare_quantization.cpp
+++ src/plugins/intel_gpu/src/graph/graph_optimizer/prepare_quantization.cpp
@@ -325,16 +325,11 @@ void prepare_quantization::handle_quantize_node(program& p, quantize_node& quant
     if (l > 2 && l <= 256 && !quantize_node.get_scale_shift_opt()) {
         prepare_scale_shift_opt(p, quantize_node);
     }
-    else if (256 < l && l <= 65536) {
-        auto& input_node = quantize_node.get_dependency(0);
-        auto input_layout = input_node.get_output_layout();
-        auto input_element_type = input_layout.data_type;
-        // Example: check if input is u8 or i8
-        bool input_8bit = false;
-        if (input_element_type == data_types::u8 || input_element_type == data_types::i8) {
-            input_8bit = true;
-        }
-        if (l > 2 && l <= 65536 && !quantize_node.get_scale_shift_opt() && input_8bit) {
+    else if (l > 256 && l <= 65536) {
+        const auto& input_node = quantize_node.get_dependency(0);
+        auto input_dt = input_node.get_output_layout().data_type;
+
+        if (!quantize_node.get_scale_shift_opt() && one_of(input_dt, { data_types::u8, data_types::i8 })) {
             prepare_scale_shift_opt(p, quantize_node);
         }
     }
