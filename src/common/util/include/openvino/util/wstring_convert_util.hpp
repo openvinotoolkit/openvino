fb6ab5962b1a0fb635026fbb706eebfeb1de6c3b
Pawel Raasz <pawel.raasz@intel.com>
2026-01-05 11:23:41 +0100

    [core] Use view as input for string <-> wstring utils (#33414)
    ### Details:
    - Change inputs for string and wstring conversion util to view to be
    compatible wider range of input types and avoid make copies
    
    ### Tickets:
     - N/A
    
    ---------
    
    Signed-off-by: Raasz, Pawel <pawel.raasz@intel.com>

--- src/common/util/include/openvino/util/wstring_convert_util.hpp
+++ src/common/util/include/openvino/util/wstring_convert_util.hpp
@@ -5,6 +5,7 @@
 #pragma once
 
 #include <string>
+#include <string_view>
 
 #include "openvino/util/util.hpp"
 
@@ -16,13 +17,14 @@ namespace ov::util {
  * @param wstr A wide-char string
  * @return A multi-byte string
  */
-std::string wstring_to_string(const std::wstring& wstr);
+std::string wstring_to_string(const std::wstring_view wstr);
+
 /**
  * @brief Conversion from single-byte chain to wide character string.
- * @param str A null-terminated string
+ * @param str A multi-byte string
  * @return A wide-char string
  */
-std::wstring string_to_wstring(const std::string& str);
+std::wstring string_to_wstring(const std::string_view str);
 
 #endif
 
