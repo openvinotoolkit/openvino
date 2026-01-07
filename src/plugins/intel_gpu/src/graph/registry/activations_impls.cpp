277db1cf3513494d7fe6d25f93dd5731a37cb1e6
LiangGao <liang.gao@intel.com>
2026-01-07 15:02:01 +0800

    Update code based on code review to fix GPU regressions

--- src/plugins/intel_gpu/src/graph/registry/activations_impls.cpp
+++ src/plugins/intel_gpu/src/graph/registry/activations_impls.cpp
@@ -11,10 +11,18 @@ namespace ov::intel_gpu {
 
 using namespace cldnn;
 
+std::function<bool(const program_node& node)> not_in_shape_flow_and_ocl_supported_dtype() {
+    return [](const program_node& node) {
+        const auto& in_layout = node.get_input_layout(0);
+        const auto& out_layout = node.get_output_layout(0);
+        return !node.is_in_shape_of_subgraph() && !one_of(data_types::i64, {in_layout.data_type, out_layout.data_type});
+    };
+}
+
 const std::vector<std::shared_ptr<cldnn::ImplementationManager>>& Registry<activation>::get_implementations() {
     static const std::vector<std::shared_ptr<ImplementationManager>> impls = {
-        OV_GPU_GET_INSTANCE_OCL(activation, shape_types::static_shape, not_in_shape_flow())
-        OV_GPU_GET_INSTANCE_OCL(activation, shape_types::dynamic_shape, not_in_shape_flow())
+        OV_GPU_GET_INSTANCE_OCL(activation, shape_types::static_shape, not_in_shape_flow_and_ocl_supported_dtype())
+        OV_GPU_GET_INSTANCE_OCL(activation, shape_types::dynamic_shape, not_in_shape_flow_and_ocl_supported_dtype())
         OV_GPU_GET_INSTANCE_CPU(activation, shape_types::static_shape, in_shape_flow())
         OV_GPU_GET_INSTANCE_CPU(activation, shape_types::dynamic_shape, in_shape_flow())
     };
