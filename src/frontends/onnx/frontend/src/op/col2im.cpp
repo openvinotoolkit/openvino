36e586fc3026d33c90c6d6fd11180c1158de4c3d
김대현 (Kim DaeHyun) <daehyun99@kakao.com>
2026-01-08 02:51:16 +0900

    Apply clang-format

--- src/frontends/onnx/frontend/src/op/col2im.cpp
+++ src/frontends/onnx/frontend/src/op/col2im.cpp
@@ -3,10 +3,11 @@
 //
 
 #include "openvino/op/col2im.hpp"
-#include "utils/common.hpp"
+
 #include "core/operator_set.hpp"
-#include "openvino/core/validation_util.hpp"
 #include "exceptions.hpp"
+#include "openvino/core/validation_util.hpp"
+#include "utils/common.hpp"
 using namespace ov::op::v15;
 
 namespace ov {
@@ -18,9 +19,9 @@ ov::OutputVector col2im(const ov::frontend::onnx::Node& node) {
     // 1. get inputs
     common::default_op_checks(node, 3);
     const auto inputs = node.get_ov_inputs();
-    const auto& data = inputs[0]; // input
-    const auto& output_size = inputs[1]; // image_shape
-    const auto& kernel_size = inputs[2]; // block_shape
+    const auto& data = inputs[0];         // input
+    const auto& output_size = inputs[1];  // image_shape
+    const auto& kernel_size = inputs[2];  // block_shape
 
     // 2. get attributes
     const size_t spatial_rank = 2;
@@ -35,37 +36,32 @@ ov::OutputVector col2im(const ov::frontend::onnx::Node& node) {
     std::vector<size_t> pads_end;
 
     // ov::op::v15::Col2Im only supports 2D spatial dimensions
-    CHECK_VALID_NODE(
-        node,
-        dilations.size() == spatial_rank, 
-        "Col2Im 'dilations' attribute must have size [2]. Got: ", dilations.size());
-    CHECK_VALID_NODE(
-        node,
-        strides.size() == spatial_rank,
-        "Col2Im 'strides' attribute must have size [2]. Got: ", strides.size());
-    CHECK_VALID_NODE(
-        node,
-        pads.size() == spatial_rank * 2,
-        "Col2Im 'pads' attribute must have size [4]. Got: ", pads.size());
+    CHECK_VALID_NODE(node,
+                     dilations.size() == spatial_rank,
+                     "Col2Im 'dilations' attribute must have size [2]. Got: ",
+                     dilations.size());
+    CHECK_VALID_NODE(node,
+                     strides.size() == spatial_rank,
+                     "Col2Im 'strides' attribute must have size [2]. Got: ",
+                     strides.size());
+    CHECK_VALID_NODE(node,
+                     pads.size() == spatial_rank * 2,
+                     "Col2Im 'pads' attribute must have size [4]. Got: ",
+                     pads.size());
 
     // spatial_rank == pads.size() / 2
     pads_begin.assign(pads.begin(), pads.begin() + spatial_rank);
     pads_end.assign(pads.begin() + spatial_rank, pads.end());
 
     // 3. return Col2Im
-    return {std::make_shared<ov::op::v15::Col2Im>(
-        data,
-        output_size,
-        kernel_size,
-        strides,
-        dilations,
-        pads_begin,
-        pads_end)->outputs()};
+    return {
+        std::make_shared<ov::op::v15::Col2Im>(data, output_size, kernel_size, strides, dilations, pads_begin, pads_end)
+            ->outputs()};
 }
 
 ONNX_OP("Col2Im", OPSET_SINCE(18), ai_onnx::opset_18::col2im);
-} // namespace opset_18
-} // namespace ai_onnx
-} // namespace onnx
-} // namespace frontend
-} // namespace ov
+}  // namespace opset_18
+}  // namespace ai_onnx
+}  // namespace onnx
+}  // namespace frontend
+}  // namespace ov
