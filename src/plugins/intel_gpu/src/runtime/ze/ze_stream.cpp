863ea10e74be425aaaa7ff35d5b7e210964ba651
Kasprzak, Jakub <jakub.kasprzak@intel.com>
2026-01-08 12:18:55 +0000

    Update onednn L0 submodule and fix profiling

--- src/plugins/intel_gpu/src/runtime/ze/ze_stream.cpp
+++ src/plugins/intel_gpu/src/runtime/ze/ze_stream.cpp
@@ -397,7 +397,7 @@ dnnl::stream& ze_stream::get_onednn_stream() {
     OPENVINO_ASSERT(m_queue_type == QueueTypes::in_order, "[GPU] Can't create onednn stream handle as onednn doesn't support out-of-order queue");
     OPENVINO_ASSERT(_engine.get_device_info().vendor_id == INTEL_VENDOR_ID, "[GPU] Can't create onednn stream handle as for non-Intel devices");
     if (!_onednn_stream) {
-        _onednn_stream = std::make_shared<dnnl::stream>(dnnl::l0_interop::make_stream(_engine.get_onednn_engine(), m_command_list));
+        _onednn_stream = std::make_shared<dnnl::stream>(dnnl::l0_interop::make_stream(_engine.get_onednn_engine(), m_command_list, m_ev_factory->is_profiling_enabled()));
     }
 
     return *_onednn_stream;
