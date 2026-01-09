dae635c75029dac5be16cc83d7c39513d9bdc07c
Beheshti <nazanin.beheshti@intel.com>
2026-01-09 08:32:01 -0800

    Apply reviwer comments

--- src/common/low_precision_transformations/src/split.cpp
+++ src/common/low_precision_transformations/src/split.cpp
@@ -97,14 +97,8 @@ bool SplitTransformation::transform(ov::pass::pattern::Matcher& m) {
             copy_runtime_info({ newSplit, subtract }, subtract);
             parent = subtract;
         }
-        if (parent.get_element_type() != splitedMul[i].get_element_type()) {
-            auto convert =
-                std::make_shared<ov::op::TypeRelaxed<ov::opset1::Convert>>(splitedMul[i], parent.get_element_type());
-            splitedMul[i] = convert;
-        }
 
-        const auto multiply = std::make_shared<ov::op::TypeRelaxed<ov::opset1::Multiply>>(parent, splitedMul[i]);
-        NetworkHelper::setOutDataPrecisionForTypeRelaxed(multiply, dequantization.multiply->get_output_element_type(0));
+        const auto multiply = dequantization.multiply->clone_with_new_inputs({ parent, splitedMul[i] });
         copy_runtime_info({ newSplit, multiply }, multiply);
 
         lastNodes.push_back(multiply);
