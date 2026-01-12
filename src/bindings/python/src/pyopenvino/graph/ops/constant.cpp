3f8df6989692b265bd4d39cb7ca7ba4427aac11d
Przemyslaw Wysocki <przemyslaw.wysocki@intel.com>
2026-01-12 11:12:18 +0100

    [PyOV] Transposed packing of `u3` and `u6` types (#33349)
    ### Details:
     - Expose `u3` and `u6` types in Python API
     - Add custom algorithms for packing and unpacking these types
    
    ### Tickets:
     - CVS-178748
    
    ---------
    
    Signed-off-by: p-wysocki <przemyslaw.wysocki@intel.com>

--- src/bindings/python/src/pyopenvino/graph/ops/constant.cpp
+++ src/bindings/python/src/pyopenvino/graph/ops/constant.cpp
@@ -108,8 +108,9 @@ void regclass_graph_op_Constant(py::module m) {
             return _cast_vector<int32_t>(self);
         } else if (element_type == ov::element::i64) {
             return _cast_vector<int64_t>(self);
-        } else if (element_type == ov::element::u8 || element_type == ov::element::u1 ||
-                   element_type == ov::element::u2 || element_type == ov::element::u4) {
+        } else if (element_type == ov::element::u1 || element_type == ov::element::u2 ||
+                   element_type == ov::element::u3 || element_type == ov::element::u4 ||
+                   element_type == ov::element::u6 || element_type == ov::element::u8) {
             return _cast_vector<uint8_t>(self);
         } else if (element_type == ov::element::u16) {
             return _cast_vector<uint16_t>(self);
@@ -158,8 +159,9 @@ void regclass_graph_op_Constant(py::module m) {
             return _get_buffer_info<int32_t>(self);
         } else if (element_type == ov::element::i64) {
             return _get_buffer_info<int64_t>(self);
-        } else if (element_type == ov::element::u8 || element_type == ov::element::u1 ||
-                   element_type == ov::element::u2 || element_type == ov::element::u4) {
+        } else if (element_type == ov::element::u1 || element_type == ov::element::u2 ||
+                   element_type == ov::element::u3 || element_type == ov::element::u4 ||
+                   element_type == ov::element::u6 || element_type == ov::element::u8) {
             return _get_buffer_info<uint8_t>(self);
         } else if (element_type == ov::element::u16) {
             return _get_buffer_info<uint16_t>(self);
