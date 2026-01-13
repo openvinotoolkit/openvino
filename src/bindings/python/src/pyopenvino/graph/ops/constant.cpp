6e8e95f569b383f299bdc2b5e2687d77a44f51fd
Kartikeya Srivastava <kartikeyasrivastava769@gmail.com>
2026-01-14 00:03:45 +0530

    remove PyErr_WarnEx

--- src/bindings/python/src/pyopenvino/graph/ops/constant.cpp
+++ src/bindings/python/src/pyopenvino/graph/ops/constant.cpp
@@ -70,9 +70,6 @@ void regclass_graph_op_Constant(py::module m) {
                 if (shared_memory) {
                     OPENVINO_WARN(
                         "Creating a String Constant with shared memory is not supported. Data will be copied.");
-                    PyErr_WarnEx(PyExc_RuntimeWarning,
-                                 "Creating a String Constant with shared memory is not supported. Data will be copied.",
-                                 1);
                 }
                 ov::Shape shape(array.shape(), array.shape() + array.ndim());
 
