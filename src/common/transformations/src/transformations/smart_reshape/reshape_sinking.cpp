8de30af8c758fea8ee688ce1dcaa52df04e547eb
Evgeny Kotov <evgeny.kotov@intel.com>
2026-01-14 17:25:39 +0100

    clang

--- src/common/transformations/src/transformations/smart_reshape/reshape_sinking.cpp
+++ src/common/transformations/src/transformations/smart_reshape/reshape_sinking.cpp
@@ -34,8 +34,8 @@ ReshapeSinkingMatMul::ReshapeSinkingMatMul() {
      *     |    shape=[1, S, O]                      |    shape=[B, S, O]
      */
     auto input_pattern = pattern::any_input(pattern::has_static_rank());
-    auto reshape_label =
-        pattern::wrap_type<v1::Reshape>({pattern::any_input(), pattern::wrap_type<v0::Constant>()}, pattern::rank_equals(2));
+    auto reshape_label = pattern::wrap_type<v1::Reshape>({pattern::any_input(), pattern::wrap_type<v0::Constant>()},
+                                                         pattern::rank_equals(2));
 
     auto matmul_label =
         pattern::wrap_type<v0::MatMul>({reshape_label, pattern::wrap_type<v0::Constant>()}, pattern::rank_equals(2));
