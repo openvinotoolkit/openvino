49a936a7f468789343fcc6897db35911d2478938
Kim, Eddy <eddy.kim@intel.com>
2026-01-15 20:02:10 +0900

    updated default config

--- src/plugins/intel_gpu/src/graph/impls/ocl_v2/sdpa/sdpa_gen_micro.cpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl_v2/sdpa/sdpa_gen_micro.cpp
@@ -1461,8 +1461,7 @@ void SDPAMicroGenerator::init_microkernels(const kernel_impl_params& params,
                                      is_paged_attention,
                                      is_prefill);
         break;
-    case gpu_arch::xe2:
-    case gpu_arch::xe3: {
+    default: {
         config = choose_config_xe2(static_cast<int32_t>(k_head_size),
                                    static_cast<int32_t>(nkeys_v),
                                    thin_q,
@@ -1472,8 +1471,6 @@ void SDPAMicroGenerator::init_microkernels(const kernel_impl_params& params,
                                    is_prefill);
         break;
     }
-    default:
-        break;
     }
 
     OPENVINO_ASSERT(config != nullptr);
