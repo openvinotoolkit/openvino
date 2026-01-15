6e789695ad7945d5f56f48bb2d76a7133fd19a1a
Wanglei Shen <wanglei.shen@intel.com>
2026-01-15 08:54:12 +0000

    update logic of default prefer threads latency (#33539)
    ### Details:
     - *update logic of default prefer threads latency for i5_1335U*
    
    ### Tickets:
     - *CVS-178991*

--- src/plugins/intel_cpu/src/cpu_streams_calculation.cpp
+++ src/plugins/intel_cpu/src/cpu_streams_calculation.cpp
@@ -618,7 +618,8 @@ int get_model_prefer_threads(const int num_streams,
         bool use_all_cores = proc_type_table[0][MAIN_CORE_PROC] <= (proc_type_table[0][EFFICIENT_CORE_PROC] /
                                                                     (int8_intensive ? int8_threshold : fp32_threshold));
 
-        if (use_all_cores && !is_LLM) {
+        if (use_all_cores &&
+            (!is_LLM || proc_type_table[0][EFFICIENT_CORE_PROC] > 2 * proc_type_table[0][MAIN_CORE_PROC])) {
             config.modelPreferThreadsLatency =
                 proc_type_table[0][MAIN_CORE_PROC] + proc_type_table[0][EFFICIENT_CORE_PROC];
         } else {
