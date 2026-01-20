cba8950d11cc71c6087a4adc3f1480d92760f107
Patrik Stepan <patrik.stepan@intel.com>
2026-01-20 13:36:40 +0200

    [NPU] Use PLUGIN as default compiler type (#33595)
    ### Details:
     - *Make NPU_COMPILER_TYPE a public property*
     - *Make NPU_PLATFORM a public property*
     - *Allow OpenVINO caching to be used with NPU_COMPILER_TYPE=PLUGIN*
     - *Add Panther Lake to the list of recognized devices*
    - *Try to use PLUGIN as default compiler type on Lunar Lake and Panther
    Lake, when the compiler library is available and when the project was
    built with ENABLE_INTEL_NPU_COMPILER=ON. The later condition is required
    to avoid side effects in internal compiler validation*
    
    ### Tickets:
     - *CVS-175845*
    
    ---------
    
    Signed-off-by: Bogdan Pereanu <bogdan.pereanu@intel.com>
    Signed-off-by: Stepan <patrik.stepan@intel.com>
    Co-authored-by: Bogdan Pereanu <bogdan.pereanu@intel.com>

--- src/inference/include/openvino/runtime/intel_npu/properties.hpp
+++ src/inference/include/openvino/runtime/intel_npu/properties.hpp
@@ -26,6 +26,45 @@ namespace ov {
 namespace intel_npu {
 
 /**
+ * @brief [Only for NPU Plugin]
+ * Type: string
+ * Type of NPU compiler to be used for compilation of a network
+ * @note Configuration API v 2.0
+ */
+enum class CompilerType { PLUGIN, DRIVER };
+
+/**
+ * @brief Prints a string representation of ov::intel_npu::CompilerType to a stream
+ * @param out An output stream to send to
+ * @param fmt A compiler type value to print to a stream
+ * @return A reference to the `out` stream
+ * @note Configuration API v 2.0
+ */
+inline std::ostream& operator<<(std::ostream& out, const CompilerType& fmt) {
+    switch (fmt) {
+    case CompilerType::PLUGIN: {
+        out << "PLUGIN";
+    } break;
+    case CompilerType::DRIVER: {
+        out << "DRIVER";
+    } break;
+    default:
+        out << static_cast<uint32_t>(fmt);
+        break;
+    }
+    return out;
+}
+
+/**
+ * @brief [Only for NPU Plugin]
+ * Type: Arbitrary string.
+ * This option allows to specify device.
+ * The plugin accepts any value given through this option. If the device is not available, either the driver or the
+ * compiler will throw an exception depending on the flow running at the time.
+ */
+static constexpr ov::Property<std::string> platform{"NPU_PLATFORM"};
+
+/**
  * @brief [Only for NPU plugin]
  * Type: uint64_t
  * Read-only property to get size of already allocated NPU DDR memory (both for discrete/integrated NPU devices)
@@ -52,6 +91,13 @@ static constexpr ov::Property<uint64_t, ov::PropertyMutability::RO> device_total
  * @ingroup ov_runtime_npu_prop_cpp_api
  */
 static constexpr ov::Property<uint32_t, ov::PropertyMutability::RO> driver_version{"NPU_DRIVER_VERSION"};
+
+/**
+ * @brief [Only for NPU Plugin]
+ * Type: string
+ * Selects the type of NPU compiler to be used for compilation of a network.
+ */
+static constexpr ov::Property<CompilerType> compiler_type{"NPU_COMPILER_TYPE"};
 
 /**
  * @brief [Only for NPU plugin]
