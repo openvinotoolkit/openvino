9a6c16e09b3708c85446a0746062341dd91e3a8e
Arseniy Obolenskiy <arseniy.obolenskiy@intel.com>
2026-01-21 13:26:09 +0100

    get rid of common changes

--- src/common/snippets/include/snippets/target_machine.hpp
+++ src/common/snippets/include/snippets/target_machine.hpp
@@ -63,11 +63,6 @@ public:
     virtual CompiledSnippetPtr get_snippet() = 0;
 
     /**
-     * @brief notifies the target machine that code emission is about to start
-     */
-    virtual void begin_code_section() {}
-
-    /**
      * @brief gets number of lanes supported by target's vector ISA
      * @return number of lanes
      */
