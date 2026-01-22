79ef92a731543445f3f0bdf3e8c74d8ebfb0b215
Xin Wang <xin1.wang@intel.com>
2026-01-22 18:33:58 +0800

    [NPU] Update vcl allocator and NetworkDescription for aligned allocation (#33717)
    ### Details:
    - *Introduced `ByteAlignedAllocator` and `AlignedVector` to enforce
    memory alignment to the standard page size for compiled network data,
    replacing standard `std::vector<uint8_t>` allocations in
    `vcl_allocator_vector` and `vcl_allocator_vector_2`. Added runtime
    checks to throw errors if alignment is not met.*
    - *Added a new constructor and member to `NetworkDescription` to support
    storing the compiled network as an `ov::Tensor`
    (`compiledNetworkTensor`), alongside the original byte vector*
    - Updated downstream usage in `PluginCompilerAdapter` to prioritize the
    new `compiledNetworkTensor` field when available, falling back to the
    legacy byte vector if necessary.
    
    ### Tickets:
     - *C179659*
    
    ---------
    
    Signed-off-by: Xin Wang <xin1.wang@intel.com>

--- src/plugins/intel_npu/src/compiler_adapter/src/plugin_compiler_adapter.cpp
+++ src/plugins/intel_npu/src/compiler_adapter/src/plugin_compiler_adapter.cpp
@@ -112,7 +112,12 @@ std::shared_ptr<IGraph> PluginCompilerAdapter::compile(const std::shared_ptr<con
     auto networkDesc = _compiler->compile(model, config);
     _logger.debug("compile end");
 
-    ov::Tensor tensor = make_tensor_from_vector(networkDesc.compiledNetwork);
+    ov::Tensor tensor;
+    if (networkDesc.compiledNetwork.size() > 0) {
+        tensor = make_tensor_from_vector(networkDesc.compiledNetwork);
+    } else {
+        tensor = std::move(networkDesc.compiledNetworkTensor);
+    }
     GraphDescriptor graphDesc;
     NetworkMetadata networkMeta;
 
@@ -180,7 +185,12 @@ std::shared_ptr<IGraph> PluginCompilerAdapter::compileWS(const std::shared_ptr<o
         std::vector<std::shared_ptr<NetworkDescription>> initNetworkDescriptions =
             std::move(initMainNetworkDescriptions);
 
-        tensorMain = make_tensor_from_vector(mainNetworkDescription->compiledNetwork);
+        if (mainNetworkDescription->compiledNetwork.size() > 0) {
+            tensorMain = make_tensor_from_vector(mainNetworkDescription->compiledNetwork);
+        } else {
+            tensorMain = std::move(mainNetworkDescription->compiledNetworkTensor);
+        }
+
         if (_zeGraphExt) {
             // Depending on the config, we may get an error when trying to
             // get the graph handle from the compiled network
@@ -201,7 +211,12 @@ std::shared_ptr<IGraph> PluginCompilerAdapter::compileWS(const std::shared_ptr<o
         tensorsInits.reserve(initNetworkDescriptions.size());
         initNetworkMetadata.reserve(initNetworkDescriptions.size());
         for (auto& networkDesc : initNetworkDescriptions) {
-            ov::Tensor tensor = make_tensor_from_vector(networkDesc->compiledNetwork);
+            ov::Tensor tensor;
+            if (networkDesc->compiledNetwork.size() > 0) {
+                tensor = make_tensor_from_vector(networkDesc->compiledNetwork);
+            } else {
+                tensor = std::move(networkDesc->compiledNetworkTensor);
+            }
             GraphDescriptor initGraphDesc;
             NetworkMetadata initNetworkMeta;
             if (_zeGraphExt) {
@@ -236,7 +251,12 @@ std::shared_ptr<IGraph> PluginCompilerAdapter::compileWS(const std::shared_ptr<o
 
         while (auto networkDescription =
                    std::make_shared<NetworkDescription>(_compiler->compileWsIterative(targetModel, localConfig, i++))) {
-            ov::Tensor tensor = make_tensor_from_vector(networkDescription->compiledNetwork);
+            ov::Tensor tensor;
+            if (networkDescription->compiledNetwork.size() > 0) {
+                tensor = make_tensor_from_vector(networkDescription->compiledNetwork);
+            } else {
+                tensor = std::move(networkDescription->compiledNetworkTensor);
+            }
             GraphDescriptor graphDesc = _zeGraphExt->getGraphDescriptor(tensor.data(), tensor.get_byte_size());
             NetworkMetadata networkMetadata = _zeGraphExt->getNetworkMeta(graphDesc);
 
