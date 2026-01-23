ca9a932aef3918f94f0413a3d1ff02fdaaefc2b3
Nose Takafumi <nose.takafumi@fujitsu.com>
2026-01-23 06:05:31 +0000

    Remove implementations for ocl kernel

--- src/plugins/intel_gpu/src/runtime/sycl/sycl_common.hpp
+++ src/plugins/intel_gpu/src/runtime/sycl/sycl_common.hpp
@@ -34,33 +34,5 @@ inline bool is_device_available(const device_info& info) {
     return false;
 }
 
-inline void rethrow(std::string message, cl_int error, const device_info& info) {
-    if (error != CL_OUT_OF_RESOURCES) {
-        OPENVINO_THROW(message);
-    }
-    // For CL_OUT_OF_RESOURCES exception there are 2 possible cases:
-    // 1. Real out of resource
-    // 2. Device is lost during application run, plugin may throw an exception
-    if (is_device_available(info)) {
-        std::stringstream ss;
-        ss << "[GPU] CL_OUT_OF_RESOURCES exception.\n"
-           << "\tDue to a driver bug, any subsequent OpenCL API call may cause the application to hang, "
-           << "so the GPU plugin may be unable to finish correctly.\n"
-           << "\tThe CL_OUT_OF_RESOURCES error typically occurs in two cases:\n"
-           << "\t1. An actual lack of memory for the current inference.\n"
-           << "\t2. An out-of-bounds access to GPU memory from a kernel.\n"
-           << "\tFor case 1, you may try adjusting some model parameters (e.g., using a smaller batch size, lower inference precision, fewer streams, etc.)"
-           << " to reduce the required memory size. For case 2, please submit a bug report to the OpenVINO team.\n"
-           << "\tAdditionally, please try updating the driver to the latest version.\n";
-        OPENVINO_THROW(ss.str());
-    } else {
-        OPENVINO_THROW(message);
-    }
-}
-
-inline void rethrow(const ::sycl::exception& error, const device_info& info) {
-    rethrow(SYCL_ERR_MSG_FMT(error), error.code().value(), info);
-}
-
 }  // namespace sycl
 }  // namespace cldnn
