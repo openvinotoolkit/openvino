a0fa658b797e3237136e56f539fdf7dd91ef8a5b
Nam, David <david.nam@intel.com>
2026-01-24 05:49:28 +0900

    [GPU] Allocate intermediate buffer even when concat is optimized out

--- src/plugins/intel_gpu/src/graph/primitive_inst.cpp
+++ src/plugins/intel_gpu/src/graph/primitive_inst.cpp
@@ -661,6 +661,49 @@ void primitive_inst::realloc_if_needed(bool prev_execution_skipped) {
     OV_ITT_SCOPED_TASK(ov::intel_gpu::itt::domains::intel_gpu_plugin, openvino::itt::handle("realloc_if_needed: " + id()));
     GPU_DEBUG_PROFILED_STAGE(instrumentation::pipeline_stage::memory_allocation);
 
+    auto alloc_intermediates = [&]() {
+        // intermediate memory allocation is required for primitives consisting of multiple kernels in dynamic case
+        if (_impl == nullptr)
+            return;
+
+        const auto& buffer_descs = _impl->get_internal_buffer_descs(*_impl_params);
+        if (buffer_descs.empty())
+            return;
+
+        GPU_DEBUG_CODE(std::string memalloc_info = "");
+        for (size_t i = 0; i < buffer_descs.size(); ++i) {
+            auto need_lockable = buffer_descs[i].m_lockable;
+            auto alloc_type = i < _intermediates_memory.size() ? _intermediates_memory[i]->get_allocation_type()
+                                                                : allocation_type::unknown;
+            bool can_reuse = true;
+            can_reuse &= alloc_type != allocation_type::unknown &&
+                            buffer_descs[i].m_layout.bytes_count() <= _max_intermediates_memory_sizes[i];
+            can_reuse &= (need_lockable && alloc_type != cldnn::allocation_type::usm_device) ||
+                            (!need_lockable && alloc_type != cldnn::allocation_type::usm_host);
+
+            if (can_reuse) {
+                _intermediates_memory[i] = get_network().get_engine().reinterpret_buffer(*_intermediates_memory[i], buffer_descs[i].m_layout);
+                GPU_DEBUG_CODE(memalloc_info += ((_intermediates_memory.size() > 1) ? ("i" + to_string(i) + ":") : "") + "reuse_buffer");
+            } else {
+                // TODO: If there is a kernel which requires reset internal buffer in the future,
+                // we'll need additional handle for that purpose like need_reset_output_memory
+                const bool need_reset = false;
+                if (i < _intermediates_memory.size()) {
+                    _intermediates_memory[i] = allocate_internal_buffer(buffer_descs[i].m_layout, i, need_reset, need_lockable);
+                    _max_intermediates_memory_sizes[i] = _intermediates_memory[i]->size();
+                } else {
+                    // i-th layout has not been allocated yet
+                    _intermediates_memory.push_back(allocate_internal_buffer(buffer_descs[i].m_layout, i, need_reset, need_lockable));
+                    _max_intermediates_memory_sizes.push_back(_intermediates_memory[i]->size());
+                }
+                GPU_DEBUG_CODE(memalloc_info +=
+                                (((_intermediates_memory.size() > 1) ? ("i" + to_string(i) + ":") : "") +
+                                (_intermediates_memory[i]->from_memory_pool ? "from_pool" : "new_alloc")));
+            }
+        }
+        GPU_DEBUG_PROFILED_STAGE_MEMALLOC_INFO(memalloc_info);
+    };
+
     const auto& users = get_user_insts();
     if (users.size() == 1 && users.front()->get_node().is_type<concatenation>() && users.front()->get_node().is_runtime_skippable()) {
         auto concat_inst = users.front();
@@ -671,6 +714,7 @@ void primitive_inst::realloc_if_needed(bool prev_execution_skipped) {
             }
             this->_outputs[0] = concat_inst->_outputs[0];
             GPU_DEBUG_TRACE_DETAIL << id() << ": use concat user's memory " << this->_outputs[0]->buffer_ptr() << std::endl;
+            alloc_intermediates();
             return;
         }
     }
@@ -1155,46 +1199,7 @@ void primitive_inst::realloc_if_needed(bool prev_execution_skipped) {
     }
 
     _mem_allocated = true;
-    // intermediate memory allocation is required for primitives consisting of multiple kernels in dynamic case
-    {
-        if (_impl == nullptr)
-            return;
-        const auto& buffer_descs = _impl->get_internal_buffer_descs(*_impl_params);
-        if (buffer_descs.empty())
-            return;
-        GPU_DEBUG_CODE(std::string memalloc_info = "");
-        for (size_t i = 0; i < buffer_descs.size(); ++i) {
-            auto need_lockable = buffer_descs[i].m_lockable;
-            auto alloc_type = i < _intermediates_memory.size() ? _intermediates_memory[i]->get_allocation_type()
-                                                               : allocation_type::unknown;
-            bool can_reuse = true;
-            can_reuse &= alloc_type != allocation_type::unknown &&
-                         buffer_descs[i].m_layout.bytes_count() <= _max_intermediates_memory_sizes[i];
-            can_reuse &= (need_lockable && alloc_type != cldnn::allocation_type::usm_device) ||
-                         (!need_lockable && alloc_type != cldnn::allocation_type::usm_host);
-
-            if (can_reuse) {
-                _intermediates_memory[i] = get_network().get_engine().reinterpret_buffer(*_intermediates_memory[i], buffer_descs[i].m_layout);
-               GPU_DEBUG_CODE(memalloc_info += ((_intermediates_memory.size() > 1) ? ("i" + to_string(i) + ":") : "") + "reuse_buffer");
-            } else {
-                // TODO: If there is a kernel which requires reset internal buffer in the future,
-                // we'll need additional handle for that purpose like need_reset_output_memory
-                const bool need_reset = false;
-                if (i < _intermediates_memory.size()) {
-                    _intermediates_memory[i] = allocate_internal_buffer(buffer_descs[i].m_layout, i, need_reset, need_lockable);
-                    _max_intermediates_memory_sizes[i] = _intermediates_memory[i]->size();
-                } else {
-                    // i-th layout has not been allocated yet
-                    _intermediates_memory.push_back(allocate_internal_buffer(buffer_descs[i].m_layout, i, need_reset, need_lockable));
-                    _max_intermediates_memory_sizes.push_back(_intermediates_memory[i]->size());
-                }
-                GPU_DEBUG_CODE(memalloc_info +=
-                               (((_intermediates_memory.size() > 1) ? ("i" + to_string(i) + ":") : "") +
-                                (_intermediates_memory[i]->from_memory_pool ? "from_pool" : "new_alloc")));
-            }
-        }
-        GPU_DEBUG_PROFILED_STAGE_MEMALLOC_INFO(memalloc_info);
-    }
+    alloc_intermediates();
 }
 
 bool primitive_inst::use_async_compilation() {
