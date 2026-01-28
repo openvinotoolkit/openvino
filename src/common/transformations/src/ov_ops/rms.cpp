0df4b778ceda3d47d147ffb35e51f690cb9da07d
Andrew Park <andrew.park@intel.com>
2026-01-28 18:39:41 +0900

    Support RMS Normalization without Learnable Affine Parameters

--- src/common/transformations/src/ov_ops/rms.cpp
+++ src/common/transformations/src/ov_ops/rms.cpp
@@ -8,16 +8,18 @@ namespace ov {
 namespace op {
 namespace internal {
 
-RMS::RMS(const Output<Node>& data, const Output<Node>& gamma, double epsilson, const ov::element::Type output_type)
+RMS::RMS(const Output<Node>& data, const Output<Node>& gamma, double epsilson, const ov::element::Type output_type, bool elementwise_affine)
     : Op({data, gamma}),
       m_epsilon(epsilson),
-      m_output_type(output_type) {
+      m_output_type(output_type),
+      m_elementwise_affine(elementwise_affine) {
     validate_and_infer_types();
 }
 
 bool RMS::visit_attributes(ov::AttributeVisitor& visitor) {
     visitor.on_attribute("epsilon", m_epsilon);
     visitor.on_attribute("output_type", m_output_type);
+    visitor.on_attribute("elementwise_affine", m_elementwise_affine);
     return true;
 }
 
@@ -28,7 +30,7 @@ void RMS::validate_and_infer_types() {
 
 std::shared_ptr<Node> RMS::clone_with_new_inputs(const ov::OutputVector& new_args) const {
     check_new_args_count(this, new_args);
-    return std::make_shared<RMS>(new_args.at(0), new_args.at(1), m_epsilon, m_output_type);
+    return std::make_shared<RMS>(new_args.at(0), new_args.at(1), m_epsilon, m_output_type, m_elementwise_affine);
 }
 
 }  // namespace internal
