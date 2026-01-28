afcab2a580ddf50bd1248ca8ae1b82fbd4cdacc9
Nikolay Shchegolev <nikolay.shchegolev@intel.com>
2026-01-28 13:10:18 +0400

    [TEST] SubgraphBaseTest execution time optimizations

--- src/plugins/intel_npu/tests/functional/shared_tests_instances/skip_tests_config.cpp
+++ src/plugins/intel_npu/tests/functional/shared_tests_instances/skip_tests_config.cpp
@@ -229,9 +229,9 @@ bool categoryRuleEnabler(const std::string& category,
     return false;
 }
 
-std::vector<std::string> disabledTestPatterns();
+const std::vector<std::regex>& disabled_test_patterns();
 
-std::vector<std::string> disabledTestPatterns() {
+const std::vector<std::regex>& disabled_test_patterns() {
     // Initialize skip registry
     static const auto skipRegistry = []() {
         SkipRegistry _skipRegistry;
@@ -293,9 +293,10 @@ std::vector<std::string> disabledTestPatterns() {
     }();
     // clang-format on
 
-    std::vector<std::string> matchingPatterns;
+    static std::vector<std::regex> patterns;
+    patterns.clear();
     const auto currentTestName = getCurrentTestName();
-    matchingPatterns.emplace_back(skipRegistry.getMatchingPattern(currentTestName));
+    patterns.emplace_back(skipRegistry.getMatchingPattern(currentTestName));
 
-    return matchingPatterns;
+    return patterns;
 }
