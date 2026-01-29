3685f7d81148f786fdc8eb63adfcdc01ee3da2c4
Arseniy Obolenskiy <arseniy.obolenskiy@intel.com>
2026-01-29 14:37:15 +0100

    [GPU] Enable Select PDPD broadcast handling (#33848)
    ### Details:
    Add GPU Select support for PDPD auto-broadcast by aligning inputs to
    output rank before reshaping
    
    ### Tickets:
     - CVS-177384

--- src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/select.cpp
+++ src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/select.cpp
@@ -50,6 +50,24 @@ const std::vector<std::vector<ov::Shape>> numpyShapes = {
     {{2, 2, 2, 2, 2}, {2, 2, 2, 2, 2}, {2, 2, 2, 2}}
 };
 
+const std::vector<std::vector<ov::Shape>> pdpdShapes = {
+    {{4, 5}, {2, 3, 4, 5}, {4, 5}},
+    {{1}, {2, 3, 4}, {1}},
+    {{3, 4}, {2, 3, 4}, {3, 4}},
+    {{1, 4}, {2, 3, 4}, {1, 4}},
+    {{1, 4, 1}, {2, 3, 4, 5}, {3, 4, 1}},
+    {{1, 1}, {2, 3, 4, 5}, {4, 1}},
+    {{4, 1, 1}, {2, 3, 4, 5, 6}, {4, 1, 1}},
+};
+
+const std::vector<std::vector<ov::Shape>> pdpdAxis1Shapes = {
+    {{1, 3, 4, 1}, {1, 2, 3, 4, 1}, {1, 3, 4, 1}},
+    {{3, 4}, {2, 3, 4, 5}, {3, 4}},
+    {{1, 3, 4, 1}, {1, 2, 3, 4, 5}, {1, 3, 4, 1}},
+    {{2, 3, 4}, {1, 2, 3, 4, 5}, {2, 3, 4}},
+    {{3, 4, 1}, {2, 3, 4, 5}, {3, 4, 1}},
+};
+
 INSTANTIATE_TEST_SUITE_P(smoke_CLDNN_TestsSelect_none,
                          SelectLayerTest,
                          ::testing::Combine(::testing::ValuesIn(ov::test::static_shapes_to_test_representation(noneShapes)),
@@ -63,5 +81,21 @@ INSTANTIATE_TEST_SUITE_P(smoke_CLDNN_TestsSelect_numpy,
                          ::testing::Combine(::testing::ValuesIn(ov::test::static_shapes_to_test_representation(numpyShapes)),
                                             ::testing::ValuesIn(inputPrecision),
                                             ::testing::Values(ov::op::AutoBroadcastType::NUMPY),
+                                            ::testing::Values(ov::test::utils::DEVICE_GPU)),
+                         SelectLayerTest::getTestCaseName);
+
+INSTANTIATE_TEST_SUITE_P(smoke_CLDNN_TestsSelect_pdpd,
+                         SelectLayerTest,
+                         ::testing::Combine(::testing::ValuesIn(ov::test::static_shapes_to_test_representation(pdpdShapes)),
+                                            ::testing::ValuesIn(inputPrecision),
+                                            ::testing::Values(ov::op::AutoBroadcastType::PDPD),
+                                            ::testing::Values(ov::test::utils::DEVICE_GPU)),
+                         SelectLayerTest::getTestCaseName);
+
+INSTANTIATE_TEST_SUITE_P(smoke_CLDNN_TestsSelect_pdpd_axis1,
+                         SelectLayerTest,
+                         ::testing::Combine(::testing::ValuesIn(ov::test::static_shapes_to_test_representation(pdpdAxis1Shapes)),
+                                            ::testing::ValuesIn(inputPrecision),
+                                            ::testing::Values(ov::op::AutoBroadcastSpec(ov::op::AutoBroadcastType::PDPD, 1)),
                                             ::testing::Values(ov::test::utils::DEVICE_GPU)),
                          SelectLayerTest::getTestCaseName);
