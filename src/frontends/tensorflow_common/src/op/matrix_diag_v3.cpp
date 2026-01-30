145b572f7f229c72546357aa1e3314e364c66b1b
Mrinal Chaturvedi <mrinal.chaturvedi27@gmail.com>
2026-01-30 23:15:56 +0530

    Fix formatting issues and remove redundant CMake source

--- src/frontends/tensorflow_common/src/op/matrix_diag_v3.cpp
+++ src/frontends/tensorflow_common/src/op/matrix_diag_v3.cpp
@@ -25,10 +25,9 @@ namespace frontend {
 namespace tensorflow {
 namespace op {
 
-    // Optimized MatrixDiagV3 implementation for k = 0 (main diagonal only)
+// Optimized MatrixDiagV3 implementation for k = 0 (main diagonal only)
 OutputVector translate_matrix_diag_v3_op(const NodeContext& node) {
     default_op_checks(node, 5, {"MatrixDiagV3"});
-
 
     auto diagonal = node.get_input(0);
     auto k = node.get_input(1);
@@ -36,11 +35,9 @@ OutputVector translate_matrix_diag_v3_op(const NodeContext& node) {
     auto num_cols = node.get_input(3);
     auto padding_value = node.get_input(4);
 
-
     auto align = node.get_attribute<string>("align", "RIGHT_LEFT");
 
     auto diagonal_shape_node = make_shared<v3::ShapeOf>(diagonal, element::i64);
-
 
     auto k_shape = k.get_partial_shape();
     Output<Node> k_lower, k_upper;
@@ -69,30 +66,22 @@ OutputVector translate_matrix_diag_v3_op(const NodeContext& node) {
     OPENVINO_ASSERT(k_lower_const && k_upper_const,
                     "MatrixDiagV3: only constant k is supported for k=0 implementation");
 
-    OPENVINO_ASSERT(k_lower_const->cast_vector<int32_t>()[0] == 0 &&
-                    k_upper_const->cast_vector<int32_t>()[0] == 0,
+    OPENVINO_ASSERT(k_lower_const->cast_vector<int32_t>()[0] == 0 && k_upper_const->cast_vector<int32_t>()[0] == 0,
                     "MatrixDiagV3: only k=0 is supported in this implementation");
 
-
-
-
     // ===== K=0 OPTIMIZED PATH =====
-
 
     auto unsqueeze_axis = make_shared<v0::Constant>(element::i64, Shape{2}, vector<int64_t>{0, -1});
     auto unsqueeze_diag = make_shared<v0::Unsqueeze>(diagonal, unsqueeze_axis);
     auto unsqueeze_diag_shape = make_shared<v3::ShapeOf>(unsqueeze_diag, element::i64);
 
-
-    auto last_dim = make_shared<v1::StridedSlice>(
-        unsqueeze_diag_shape,
-        make_shared<v0::Constant>(element::i64, Shape{1}, vector<int64_t>{-2}),
-        make_shared<v0::Constant>(element::i64, Shape{1}, vector<int64_t>{-1}),
-        make_shared<v0::Constant>(element::i64, Shape{1}, vector<int64_t>{1}),
-        vector<int64_t>{0},
-        vector<int64_t>{0}
-    );
-
+    auto last_dim =
+        make_shared<v1::StridedSlice>(unsqueeze_diag_shape,
+                                      make_shared<v0::Constant>(element::i64, Shape{1}, vector<int64_t>{-2}),
+                                      make_shared<v0::Constant>(element::i64, Shape{1}, vector<int64_t>{-1}),
+                                      make_shared<v0::Constant>(element::i64, Shape{1}, vector<int64_t>{1}),
+                                      vector<int64_t>{0},
+                                      vector<int64_t>{0});
 
     auto neg_one_i32 = make_shared<v0::Constant>(element::i32, Shape{}, -1);
     auto last_dim_squeezed = make_shared<v0::Squeeze>(last_dim);
@@ -104,75 +93,53 @@ OutputVector translate_matrix_diag_v3_op(const NodeContext& node) {
     auto rows_is_auto = make_shared<v1::Equal>(num_rows_scalar, neg_one_i32);
     auto cols_is_auto = make_shared<v1::Equal>(num_cols_scalar, neg_one_i32);
 
-
     auto output_rows = make_shared<v1::Select>(rows_is_auto, last_dim_i32, num_rows_scalar);
     auto output_cols = make_shared<v1::Select>(cols_is_auto, last_dim_i32, num_cols_scalar);
-
 
     auto output_rows_i64 = make_shared<v0::Convert>(output_rows, element::i64);
     auto output_cols_i64 = make_shared<v0::Convert>(output_cols, element::i64);
 
     auto diag_shape = make_shared<v3::ShapeOf>(diagonal, element::i64);
 
-
     auto one_i64 = make_shared<v0::Constant>(element::i64, Shape{1}, 1);
-    auto output_rows_unsqueezed = make_shared<v0::Unsqueeze>(
-        output_rows_i64,
-        make_shared<v0::Constant>(element::i64, Shape{1}, 0)
-    );
-    auto padding_shape = make_shared<v0::Concat>(
-        OutputVector{one_i64, diag_shape, output_rows_unsqueezed},
-        0
-    );
+    auto output_rows_unsqueezed =
+        make_shared<v0::Unsqueeze>(output_rows_i64, make_shared<v0::Constant>(element::i64, Shape{1}, 0));
+    auto padding_shape = make_shared<v0::Concat>(OutputVector{one_i64, diag_shape, output_rows_unsqueezed}, 0);
 
     auto padding_val_scalar = make_shared<v0::Squeeze>(padding_value);
     auto padding = make_shared<v3::Broadcast>(padding_val_scalar, padding_shape);
 
-
     auto zero_padded = make_shared<v0::Concat>(OutputVector{unsqueeze_diag, padding}, -1);
-    auto batch_shape = make_shared<v1::StridedSlice>(
-        unsqueeze_diag_shape,
-        make_shared<v0::Constant>(element::i64, Shape{1}, 0),
-        make_shared<v0::Constant>(element::i64, Shape{1}, -2),
-        make_shared<v0::Constant>(element::i64, Shape{1}, 1),
-        vector<int64_t>{0},
-        vector<int64_t>{0}
-    );
-
+    auto batch_shape = make_shared<v1::StridedSlice>(unsqueeze_diag_shape,
+                                                     make_shared<v0::Constant>(element::i64, Shape{1}, 0),
+                                                     make_shared<v0::Constant>(element::i64, Shape{1}, -2),
+                                                     make_shared<v0::Constant>(element::i64, Shape{1}, 1),
+                                                     vector<int64_t>{0},
+                                                     vector<int64_t>{0});
 
     auto n_plus_rows = make_shared<v1::Add>(
         make_shared<v0::Unsqueeze>(last_dim_squeezed, make_shared<v0::Constant>(element::i64, Shape{1}, 0)),
-        output_rows_unsqueezed
-    );
+        output_rows_unsqueezed);
     auto flat_size = make_shared<v1::Multiply>(
         make_shared<v0::Unsqueeze>(last_dim_squeezed, make_shared<v0::Constant>(element::i64, Shape{1}, 0)),
-        n_plus_rows
-    );
+        n_plus_rows);
 
     auto reshape_shape1 = make_shared<v0::Concat>(OutputVector{batch_shape, flat_size}, 0);
     auto reshaped = make_shared<v1::Reshape>(zero_padded, reshape_shape1, false);
 
+    auto output_size = make_shared<v1::Multiply>(
+        output_rows_unsqueezed,
+        make_shared<v0::Unsqueeze>(output_cols_i64, make_shared<v0::Constant>(element::i64, Shape{1}, 0)));
+    auto sliced = make_shared<v8::Slice>(reshaped,
+                                         make_shared<v0::Constant>(element::i64, Shape{1}, 0),
+                                         output_size,
+                                         make_shared<v0::Constant>(element::i64, Shape{1}, 1),
+                                         make_shared<v0::Constant>(element::i64, Shape{1}, -1));
 
-    auto output_size = make_shared<v1::Multiply>(output_rows_unsqueezed,
-                                                   make_shared<v0::Unsqueeze>(output_cols_i64,
-                                                   make_shared<v0::Constant>(element::i64, Shape{1}, 0)));
-    auto sliced = make_shared<v8::Slice>(
-        reshaped,
-        make_shared<v0::Constant>(element::i64, Shape{1}, 0),
-        output_size,
-        make_shared<v0::Constant>(element::i64, Shape{1}, 1),
-        make_shared<v0::Constant>(element::i64, Shape{1}, -1)
-    );
-
-
-    auto output_cols_unsqueezed = make_shared<v0::Unsqueeze>(
-        output_cols_i64,
-        make_shared<v0::Constant>(element::i64, Shape{1}, 0)
-    );
-    auto final_shape = make_shared<v0::Concat>(
-        OutputVector{diag_shape, output_rows_unsqueezed, output_cols_unsqueezed},
-        0
-    );
+    auto output_cols_unsqueezed =
+        make_shared<v0::Unsqueeze>(output_cols_i64, make_shared<v0::Constant>(element::i64, Shape{1}, 0));
+    auto final_shape =
+        make_shared<v0::Concat>(OutputVector{diag_shape, output_rows_unsqueezed, output_cols_unsqueezed}, 0);
     auto result = make_shared<v1::Reshape>(sliced, final_shape, false);
 
     set_node_name(node.get_name(), result);
