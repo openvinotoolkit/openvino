5bcb66cc5710b0d3f36ab6ccb0bcf5ad2dac95fc
LiangGao <liang.gao@intel.com>
2026-01-30 13:24:11 +0800

    Fix itt resource memory leak issue

---
+++ src/common/shutdown/shutdown.cpp
@@ -0,0 +1,30 @@
+// Copyright (C) 2026 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+#include "openvino/itt.hpp"
+
+#if defined(_WIN32) && !defined(__MINGW32__) && !defined(__MINGW64__)
+#    include <windows.h>
+BOOL WINAPI DllMain(HINSTANCE hinstDLL,  // handle to DLL module
+                    DWORD fdwReason,     // reason for calling function
+                    LPVOID lpReserved)   // reserved
+{
+    // Perform actions based on the reason for calling.
+    switch (fdwReason) {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+        break;
+
+    case DLL_PROCESS_DETACH:
+        openvino::itt::internal::shutdown();
+        break;
+    }
+    return TRUE;  // Successful DLL_PROCESS_ATTACH.
+}
+#elif defined(__linux__) || defined(__APPLE__)
+extern "C" __attribute__((destructor)) void library_unload();
+void library_unload() {
+    openvino::itt::internal::shutdown();
+}
+#endif
