97f75616bd85d14955dd1f90158fd226b1ea2fb7
LiangGao <liang.gao@intel.com>
2026-02-02 19:09:55 +0800

    Add lost file

---
+++ src/common/shutdown/shutdown.hpp
@@ -0,0 +1,17 @@
+// Copyright (C) 2026 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#if defined(_MSC_VER)
+#    define OV_SHUTDOWN_SECTION_NAME ".shutdown_sec$u"
+#    pragma section(".shutdown_sec$u", read)
+#    define DECLARE_OV_SHUTDOWN_FUNC(func) __declspec(allocate(OV_SHUTDOWN_SECTION_NAME)) void (*__##func)(void) = func;
+#elif defined(__GNUC__) || defined(__clang__)
+#    define OV_SHUTDOWN_SECTION_NAME "__shutdown_sec"
+#    define DECLARE_OV_SHUTDOWN_FUNC(func) \
+        __attribute__((section(OV_SHUTDOWN_SECTION_NAME))) void (*__##func)(void) = func;
+#else
+#    error "Compiler not supported"
+#endif
