8e94a1d1a1b6f380ca7894ed4cba1df6047ff950
Arseniy Obolenskiy <gooddoog@student.su>
2026-02-02 12:56:00 +0100

    [PT FE] Enable PyTorch tests on ARM CPU (#29664)
    ### Details:
    Enable part of PyTorch model tests (that are already working well) on
    ARM64 platform
    
    ### Tickets:
     - N/A

--- tests/model_hub_tests/pytorch/test_llm.py
+++ tests/model_hub_tests/pytorch/test_llm.py
@@ -5,6 +5,7 @@ import copy
 import inspect
 
 import numpy as np
+import platform
 import pytest
 import torch
 from huggingface_hub import snapshot_download
@@ -239,12 +240,19 @@ class TestLLMModel(TestTorchConvertModel):
 
         return pkv, for_pkv["attention_mask"]
 
-    @pytest.mark.parametrize("type,name", [
-        ("opt_gptq", "katuni4ka/opt-125m-gptq"),
-        ("llama", "TinyLlama/TinyLlama-1.1B-Chat-v1.0"),
-        ("gpt2", "openai-community/gpt2"),
-        ("llama_awq", "casperhansen/tinyllama-1b-awq")
-    ])
+    def get_supported_precommit_models():
+        models = [
+            ("gpt2", "openai-community/gpt2"),
+        ]
+        if platform.machine() not in ['arm', 'armv7l', 'aarch64', 'arm64', 'ARM64']:
+            models.extend([
+                ("opt_gptq", "katuni4ka/opt-125m-gptq"),
+                ("llama", "TinyLlama/TinyLlama-1.1B-Chat-v1.0"),
+                ("llama_awq", "casperhansen/tinyllama-1b-awq"),
+            ])
+        return models
+
+    @pytest.mark.parametrize("type,name", get_supported_precommit_models())
     @pytest.mark.precommit
     @pytest.mark.nightly
     def test_convert_model_precommit(self, name, type, ie_device):
