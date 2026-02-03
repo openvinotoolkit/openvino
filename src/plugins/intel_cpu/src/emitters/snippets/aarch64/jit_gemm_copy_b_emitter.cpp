21933ed5a2b9337db4f099d2c4ac9d3b3ef84a6a
Arseniy Obolenskiy <arseniy.obolenskiy@intel.com>
2026-02-03 09:32:38 +0100

    tidy

--- src/plugins/intel_cpu/src/emitters/snippets/aarch64/jit_gemm_copy_b_emitter.cpp
+++ src/plugins/intel_cpu/src/emitters/snippets/aarch64/jit_gemm_copy_b_emitter.cpp
@@ -34,11 +34,12 @@ using jit_generator = dnnl::impl::cpu::aarch64::jit_generator;
 using cpu_isa_t = dnnl::impl::cpu::aarch64::cpu_isa_t;
 using ExpressionPtr = ov::snippets::lowered::ExpressionPtr;
 
-jit_gemm_copy_b_emitter::jit_gemm_copy_b_emitter(jit_generator* h,
-                                                 cpu_isa_t isa,
-                                                 const ExpressionPtr& expr,
-                                                 const snippets::KernelExecutorTablePtr& kernel_table,
-                                                 const ov::intel_cpu::MultiCacheWeakPtr& compiled_kernel_cache)
+jit_gemm_copy_b_emitter::jit_gemm_copy_b_emitter(
+    jit_generator* h,
+    cpu_isa_t isa,
+    const ExpressionPtr& expr,
+    const snippets::KernelExecutorTablePtr& kernel_table,
+    [[maybe_unused]] const ov::intel_cpu::MultiCacheWeakPtr& compiled_kernel_cache)
     : jit_binary_call_emitter(h, isa, expr->get_live_regs()) {
     in_out_type_ = emitter_in_out_map::gpr_to_gpr;
     const auto gemm_repack = ov::as_type_ptr<GemmCopyB>(expr->get_node());
