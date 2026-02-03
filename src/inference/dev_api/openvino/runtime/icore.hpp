16cc4d392b471026e9466ad3cbd0e207d490e4d6
Alicja Miloszewska <alicja.miloszewska@intel.com>
2026-02-03 16:34:57 +0100

    Revery formatting changes

--- src/inference/dev_api/openvino/runtime/icore.hpp
+++ src/inference/dev_api/openvino/runtime/icore.hpp
@@ -267,9 +267,7 @@ public:
      * @param keep_core_property Whether to return core-level properties
      * @return map of properties that are supported by device
      */
-    virtual AnyMap get_supported_property(const std::string& full_device_name,
-                                          const AnyMap& properties,
-                                          const bool keep_core_property = true) const = 0;
+    virtual AnyMap get_supported_property(const std::string& full_device_name, const AnyMap& properties, const bool keep_core_property = true) const = 0;
 
     virtual bool device_supports_model_caching(const std::string& device_name) const = 0;
 
