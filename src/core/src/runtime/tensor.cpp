efe7dd6ae76ecac761e5bd1603b1db5eb58b6984
Pawel Raasz <pawel.raasz@intel.com>
2026-02-06 09:32:42 +0100

    [core] Fix Coverity CID 2536763 (#33927)
    ### Details:
    - Fix overflow by implicit cast to signed in read file as tensor by
    explicit convert.
    - If cast result is negative the file `read` return `0` as `gcount()`
    which triggers exception.
    
    ### Tickets:
     - CVS-180108
    
    ---------
    
    Signed-off-by: Pawel Raasz <pawel.raasz@intel.com>
    Co-authored-by: Roman Kazantsev <roman.kazantsev@intel.com>

--- src/core/src/runtime/tensor.cpp
+++ src/core/src/runtime/tensor.cpp
@@ -185,13 +185,9 @@ void read_tensor_via_ifstream(const std::filesystem::path& file_name, Tensor& te
     OPENVINO_ASSERT(tensor.get_element_type() != ov::element::string);
     std::ifstream fin(file_name, std::ios::binary);
     fin.seekg(offset);
-    auto bytes_to_read = tensor.get_byte_size();
+    const auto bytes_to_read = static_cast<std::streamsize>(tensor.get_byte_size());
     fin.read(static_cast<char*>(tensor.data()), bytes_to_read);
-    OPENVINO_ASSERT(static_cast<size_t>(fin.gcount()) == bytes_to_read,
-                    "Cannot read ",
-                    bytes_to_read,
-                    "bytes from ",
-                    file_name);
+    OPENVINO_ASSERT(fin.gcount() == bytes_to_read, "Cannot read ", bytes_to_read, " bytes from ", file_name);
 }
 
 ov::Tensor wrap_obj_to_viewtensor(const std::shared_ptr<void>& shared_obj,
