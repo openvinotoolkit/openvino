2851fb6b4c2e8a63c22c16e9810537815ba96b43
LiangGao <liang.gao@intel.com>
2026-02-06 13:47:04 +0800

    Refactor some code for itt fix

--- src/common/shutdown/shutdown.hpp
+++ src/common/shutdown/shutdown.hpp
@@ -1,21 +1,18 @@
 // Copyright (C) 2026 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
-
 #pragma once
+#include <functional>
+#include <vector>
 
-#if defined(_MSC_VER)
-#    define OV_SHUTDOWN_SECTION_NAME ".shutdown_sec$u"
-#    pragma section(".shutdown_sec$u", read)
-#    define DECLARE_OV_SHUTDOWN_FUNC(func) __declspec(allocate(OV_SHUTDOWN_SECTION_NAME)) void (*__##func)(void) = func;
-#elif defined(__GNUC__) || defined(__clang__)
-#    if defined(__APPLE__)
-#        define OV_SHUTDOWN_SECTION_NAME "__DATA,__shutdown_sec"
-#    else
-#        define OV_SHUTDOWN_SECTION_NAME "__shutdown_sec"
-#    endif
-#    define DECLARE_OV_SHUTDOWN_FUNC(func) \
-        __attribute__((section(OV_SHUTDOWN_SECTION_NAME))) void (*__##func)(void) = func;
-#else
-#    error "Compiler not supported"
-#endif
+namespace ov {
+    // Registers a shutdown callback
+    bool register_shutdown_callback(const std::function<void()>& func);
+    // Returns all registered shutdown callbacks
+    const std::vector<std::function<void()>>& shutdown_callbacks();
+}
+
+#define OV_REGISTER_SHUTDOWN_CALLBACK(func) \
+    namespace { \
+        static bool ov_shutdown_register_##func = ov::register_shutdown_callback(func); \
+    }
