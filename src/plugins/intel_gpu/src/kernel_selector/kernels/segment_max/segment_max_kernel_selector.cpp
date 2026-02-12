726414a921981f9428e8e48cce0d04b7d7cbe14d
wilson-seok <wilson.seok@intel.com>
2026-02-12 11:14:43 +0000

    add opt kernel

--- src/plugins/intel_gpu/src/kernel_selector/kernels/segment_max/segment_max_kernel_selector.cpp
+++ src/plugins/intel_gpu/src/kernel_selector/kernels/segment_max/segment_max_kernel_selector.cpp
@@ -4,10 +4,14 @@
 
 #include "segment_max_kernel_selector.h"
 #include "segment_max_kernel_ref.h"
+#include "segment_max_kernel_opt.h"
 
 namespace kernel_selector {
 
-segment_max_kernel_selector::segment_max_kernel_selector() { Attach<SegmentMaxKernelRef>(); }
+segment_max_kernel_selector::segment_max_kernel_selector() {
+    Attach<SegmentMaxKernelRef>();
+    Attach<SegmentMaxKernelOpt>();
+}
 
 KernelsData segment_max_kernel_selector::GetBestKernels(const Params& params) const {
     return GetNaiveBestKernel(params, KernelType::SEGMENT_MAX);
