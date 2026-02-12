a96d29219614e1b8168393471ac66efd8e97e820
Aleksandr Voron <aleksandr.voron@intel.com>
2026-02-12 18:07:06 +0100

    fix Copyright year

--- src/plugins/intel_cpu/src/transformations/cpu_opset/arm/pass/fallback_unsupported_lp_conv_to_fp16.cpp
+++ src/plugins/intel_cpu/src/transformations/cpu_opset/arm/pass/fallback_unsupported_lp_conv_to_fp16.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2020-2026 Intel Corporation
+// Copyright (C) 2018-2026 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
 #include "fallback_unsupported_lp_conv_to_fp16.hpp"
