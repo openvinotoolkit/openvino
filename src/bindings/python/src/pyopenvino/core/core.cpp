f60c938f3d8c263594f420a725a822cacefa9674
Vladislav Golubev <vladislav.golubev@intel.com>
2026-02-16 14:58:40 +0100

    Trigger Copyright checker failures

--- src/bindings/python/src/pyopenvino/core/core.cpp
+++ src/bindings/python/src/pyopenvino/core/core.cpp
@@ -1,7 +1,6 @@
-// Copyright (C) 2018-2026 Intel Corporation
+// Copyright (C) 2018-2026 Intel Corporation  
 // SPDX-License-Identifier: Apache-2.0
 //
-
 #include "pyopenvino/core/core.hpp"
 
 #include <pybind11/stl.h>
