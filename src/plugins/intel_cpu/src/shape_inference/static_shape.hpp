10e50faf6fe7135a945f64d60197907c5a45f97d
Arseniy Obolenskiy <arseniy.obolenskiy@intel.com>
2026-02-18 15:47:31 +0100

    [CPU] Refactor string serialization to use ov::util::join

--- src/plugins/intel_cpu/src/shape_inference/static_shape.hpp
+++ src/plugins/intel_cpu/src/shape_inference/static_shape.hpp
@@ -317,13 +317,7 @@ private:
 
 template <class T, typename = std::enable_if_t<is_static_shape_adapter<T>()>>
 std::ostream& operator<<(std::ostream& out, const T& shape) {
-    out << '{';
-    if (!shape.empty()) {
-        std::copy(shape.cbegin(), shape.cend() - 1, std::ostream_iterator<StaticDimension>(out, ","));
-        out << shape[shape.size() - 1];
-    }
-    out << '}';
-    return out;
+    return out << '{' << ov::util::join(shape, ",") << '}';
 }
 
 template <class T, class U, typename = std::enable_if_t<is_static_shape_adapter<T>() && is_static_shape_adapter<U>()>>
