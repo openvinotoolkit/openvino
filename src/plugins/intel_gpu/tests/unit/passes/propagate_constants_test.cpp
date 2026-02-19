08e1c61d2bb8f7ad5014d9da1d8b536a01520962
wilson-seok <wilson.seok@intel.com>
2026-02-19 14:59:46 +0000

    [GPU] Remove invalid propagate_constants unit test without compile_graph

--- src/plugins/intel_gpu/tests/unit/passes/propagate_constants_test.cpp
+++ src/plugins/intel_gpu/tests/unit/passes/propagate_constants_test.cpp
@@ -9,30 +9,6 @@
 using namespace cldnn;
 using namespace ::tests;
 
-TEST(propagate_constants, select_impl_for_static_node_without_compile_graph) {
-    auto& engine = get_test_engine();
-
-    auto input_mem = engine.allocate_memory({{1, 3, 4, 4}, data_types::f32, format::bfyx});
-
-    topology topology(
-        input_layout("input", input_mem->get_layout()),
-        reorder("reorder", input_info("input"), format::bfyx, data_types::f32)
-    );
-
-    ExecutionConfig config = get_test_default_config(engine);
-    config.set_property(ov::intel_gpu::optimize_data(true));
-    config.set_property(ov::intel_gpu::allow_new_shape_infer(true));
-
-    auto prog = program::build_program(engine, topology, config, false, true);
-
-    auto& reorder_node = prog->get_node("reorder");
-    ASSERT_EQ(reorder_node.get_selected_impl(), nullptr);
-
-    program_wrapper::apply_opt_pass<propagate_constants>(*prog);
-
-    ASSERT_NE(reorder_node.get_selected_impl(), nullptr);
-}
-
 TEST(propagate_constants, select_impl_after_dynamic_to_static_transition) {
     auto& engine = get_test_engine();
 
