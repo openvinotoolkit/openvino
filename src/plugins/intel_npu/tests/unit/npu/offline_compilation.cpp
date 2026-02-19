b5aba1211d912f3cfb80e48230dcba4b60bd16a1
alexandruenache1111 <alexandru.enache@intel.com>
2026-02-19 16:53:44 +0000

    Move log assert to class SetUp
    Signed-off-by: alexandruenache1111 <alexandru.enache@intel.com>

--- src/plugins/intel_npu/tests/unit/npu/offline_compilation.cpp
+++ src/plugins/intel_npu/tests/unit/npu/offline_compilation.cpp
@@ -6,6 +6,7 @@
 
 #include "common_test_utils/subgraph_builders/multi_single_conv.hpp"
 #include "common_test_utils/test_assertions.hpp"
+#include "common_test_utils/test_constants.hpp"
 #include "intel_npu/npu_private_properties.hpp"
 #include "openvino/core/log.hpp"
 #include "openvino/runtime/core.hpp"
@@ -13,59 +14,51 @@
 #include "openvino/runtime/properties.hpp"
 
 using namespace ov::intel_npu;
+using namespace ov::test::utils;
 
-using OfflineCompilationUnitTests = ::testing::Test;
+class OfflineCompilationUnitTests : public ::testing::Test {
+protected:
+    void SetUp() override {
+        std::function<void(std::string_view)> log_cb = [&](std::string_view msg) {
+            std::lock_guard<std::mutex> lock(logs_mutex);
+            logs.append(msg);
+            logs.push_back('\n');
+        };
+        ov::util::set_log_callback(log_cb);
 
-TEST_F(OfflineCompilationUnitTests, CompilationPassesWithCiPWhenDriverNotInstalledSetProperty) {
+        OV_ASSERT_NO_THROW(core.set_property(DEVICE_NPU, {{ov::log::level.name(), ov::log::Level::WARNING}}));
+        // triggers Plugin constructor
+        OV_ASSERT_NO_THROW(core.get_property(DEVICE_NPU, ov::hint::enable_cpu_pinning.name()));
+
+        ov::util::reset_log_callback();
+
+        ASSERT_NE(logs.find("Only offline compilation can be done"), std::string::npos);
+    }
+
     ov::Core core;
-    ov::AnyMap config;
     std::string logs;
     std::mutex logs_mutex;
+};
 
-    std::function<void(std::string_view)> log_cb = [&](std::string_view msg) {
-        std::lock_guard<std::mutex> lock(logs_mutex);
-        logs.append(msg);
-        logs.push_back('\n');
-    };
-
-    ov::util::set_log_callback(log_cb);
+TEST_F(OfflineCompilationUnitTests, CompileWithCiPWhenDriverNotInstalledSetProperty) {
+    ov::Core core;
+    ov::AnyMap config;
 
     config[ov::intel_npu::compiler_type.name()] = ov::intel_npu::CompilerType::PLUGIN;
     config[ov::intel_npu::platform.name()] = ov::intel_npu::Platform::NPU5010;
-    config[ov::log::level.name()] = ov::log::Level::WARNING;
-    core.set_property("NPU", config);
+    core.set_property(DEVICE_NPU, config);
 
     std::shared_ptr<ov::Model> model = ov::test::utils::make_multi_single_conv();
-    OV_ASSERT_NO_THROW(core.compile_model(model, "NPU"));
-
-    ov::util::reset_log_callback();
-
-    ASSERT_NE(logs.find("Only offline compilation can be done"), std::string::npos);
+    OV_ASSERT_NO_THROW(core.compile_model(model, DEVICE_NPU));
 }
 
-TEST_F(OfflineCompilationUnitTests, CompilationPassesWithCiPWhenDriverNotInstalled) {
+TEST_F(OfflineCompilationUnitTests, CompileWithCiPWhenDriverNotInstalled) {
     ov::Core core;
     ov::AnyMap config;
-    std::string logs;
-    std::mutex logs_mutex;
-
-    std::function<void(std::string_view)> log_cb = [&](std::string_view msg) {
-        std::lock_guard<std::mutex> lock(logs_mutex);
-        logs.append(msg);
-        logs.push_back('\n');
-    };
-
-    ov::util::set_log_callback(log_cb);
 
     config[ov::intel_npu::compiler_type.name()] = ov::intel_npu::CompilerType::PLUGIN;
     config[ov::intel_npu::platform.name()] = ov::intel_npu::Platform::NPU5010;
-    config[ov::log::level.name()] = ov::log::Level::WARNING;
-    core.set_property("NPU", config);
 
     std::shared_ptr<ov::Model> model = ov::test::utils::make_multi_single_conv();
-    OV_ASSERT_NO_THROW(core.compile_model(model, "NPU", config));
-
-    ov::util::reset_log_callback();
-
-    ASSERT_NE(logs.find("Only offline compilation can be done"), std::string::npos);
+    OV_ASSERT_NO_THROW(core.compile_model(model, DEVICE_NPU, config));
 }
