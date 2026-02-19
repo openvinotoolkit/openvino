5a5b076833a9d5f3b66a7550d5a87ed85541bcfd
alexandruenache1111 <alexandru.enache@intel.com>
2026-02-19 10:03:31 +0000

    Update test
    Signed-off-by: alexandruenache1111 <alexandru.enache@intel.com>

--- src/plugins/intel_npu/tests/unit/npu/offline_compilation.cpp
+++ src/plugins/intel_npu/tests/unit/npu/offline_compilation.cpp
@@ -5,6 +5,9 @@
 #include <gtest/gtest.h>
 
 #include "common_test_utils/subgraph_builders/multi_single_conv.hpp"
+#include "common_test_utils/test_assertions.hpp"
+#include "intel_npu/npu_private_properties.hpp"
+#include "openvino/core/log.hpp"
 #include "openvino/runtime/core.hpp"
 #include "openvino/runtime/intel_npu/properties.hpp"
 #include "openvino/runtime/properties.hpp"
@@ -13,13 +16,28 @@ using namespace ov::intel_npu;
 
 using OfflineCompilationUnitTests = ::testing::Test;
 
-TEST_F(OfflineCompilationUnitTests, CompilationFailsWithPluginCompilerTypeAndMissingDriver) {
+TEST_F(OfflineCompilationUnitTests, CompilationPassesWithCiPWhenDriverNotInstalled) {
     ov::Core core;
-
     ov::AnyMap config;
+    std::string logs;
+    std::mutex logs_mutex;
+
+    std::function<void(std::string_view)> log_cb = [&](std::string_view msg) {
+        std::lock_guard<std::mutex> lock(logs_mutex);
+        logs.append(msg);
+        logs.push_back('\n');
+    };
+
     config[ov::intel_npu::compiler_type.name()] = ov::intel_npu::CompilerType::PLUGIN;
+    config[ov::intel_npu::platform.name()] = ov::intel_npu::Platform::NPU3720;
     core.set_property("NPU", config);
 
+    ov::util::set_log_callback(log_cb);
+
     std::shared_ptr<ov::Model> model = ov::test::utils::make_multi_single_conv();
-    EXPECT_ANY_THROW(core.compile_model(model, "NPU", {}));
+    OV_ASSERT_NO_THROW(core.compile_model(model, "NPU", {}));
+
+    ov::util::reset_log_callback();
+
+    ASSERT_NE(logs.find("Only offline compilation can be done"), std::string::npos);
 }
