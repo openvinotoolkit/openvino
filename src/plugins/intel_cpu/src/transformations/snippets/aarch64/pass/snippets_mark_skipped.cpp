440fa000e3e72550cce78ab7ef876942e46c925d
Aleksandr Voron <aleksandr.voron@intel.com>
2026-02-24 20:38:07 +0100

    addressed comments

--- src/plugins/intel_cpu/src/transformations/snippets/aarch64/pass/snippets_mark_skipped.cpp
+++ src/plugins/intel_cpu/src/transformations/snippets/aarch64/pass/snippets_mark_skipped.cpp
@@ -264,22 +264,22 @@ bool isSuitableMatMulWithConstantPath(const std::shared_ptr<Node>& node) {
 }
 
 #if defined(OPENVINO_ARCH_ARM64)
-void markACLInt8ConvFQChain(const std::shared_ptr<Node>& node) {
+bool isACLInt8ConvFQChainMarked(const std::shared_ptr<Node>& node) {
     if (!match_acl_int8_conv_fq_chain(node)) {
-        return;
+        return false;
     }
     snippets::pass::SetSnippetsNodeType(node, snippets::pass::SnippetsNodeType::SkippedByPlugin);
 
     const auto mul = ov::as_type_ptr<ov::op::v1::Multiply>(node->get_input_node_shared_ptr(0));
     if (!mul) {
-        return;
+        return true;
     }
     snippets::pass::SetSnippetsNodeType(mul, snippets::pass::SnippetsNodeType::SkippedByPlugin);
 
-    const auto add = ov::as_type_ptr<ov::op::v1::Add>(mul->get_input_node_shared_ptr(0));
-    if (add) {
+    if (const auto add = ov::as_type_ptr<ov::op::v1::Add>(mul->get_input_node_shared_ptr(0)); add) {
         snippets::pass::SetSnippetsNodeType(add, snippets::pass::SnippetsNodeType::SkippedByPlugin);
     }
+    return true;
 }
 #endif
 
@@ -293,7 +293,9 @@ bool SnippetsMarkSkipped::run_on_model(const std::shared_ptr<ov::Model>& m) {
             continue;
         }
 #if defined(OPENVINO_ARCH_ARM64)
-        markACLInt8ConvFQChain(node);
+        if (isACLInt8ConvFQChainMarked(node)) {
+            continue;
+        }
 #endif
         // We perform this check separately because we mark here only weights path
         // Matmul itself will be checked further
