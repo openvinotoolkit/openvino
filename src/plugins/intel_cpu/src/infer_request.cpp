c3b18d06e4b1a15258b362d61fe6de2a62d60ece
Vasanth Tovinkere <vasanth.tovinkere@intel.com>
2026-02-23 22:57:11 -0800

    [OV][ITT] Revert SyncInferenceCPU to TASK_BASE
    Signed-off-by: Vasanth Tovinkere <vasanth.tovinkere@intel.com>

--- src/plugins/intel_cpu/src/infer_request.cpp
+++ src/plugins/intel_cpu/src/infer_request.cpp
@@ -102,7 +102,7 @@ void SyncInferRequest::update_external_tensor_ptrs() {
 }
 
 void SyncInferRequest::infer() {
-    OV_ITT_SCOPED_REGION_BASE(itt::domains::ov_cpu_inference, m_profiling_task);
+    OV_ITT_SCOPED_TASK_BASE(itt::domains::ov_cpu_inference, m_profiling_task);
     auto graphLock = m_compiled_model.lock();
     auto&& graph = graphLock._graph;
     auto message = ov::threading::message_manager();
