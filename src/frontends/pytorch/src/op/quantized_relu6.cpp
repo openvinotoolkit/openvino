8023b878faa3fedf23205be4f2c7437a7fba7d87
Aayushya Tiwari <kb24.aayush@gmail.com>
2026-02-27 20:48:07 +0530

    Add quantized::relu6 support and layer test

--- src/frontends/pytorch/src/op/quantized_relu6.cpp
+++ src/frontends/pytorch/src/op/quantized_relu6.cpp
@@ -1,28 +1,19 @@
-// Copyright (C) 2018-2026 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
-//
-
-
 #include "openvino/frontend/pytorch/node_context.hpp"
 #include "openvino/op/clamp.hpp"
-#include "openvino/op/constant.hpp"
 #include "utils_quantize.hpp"
-
 namespace ov {
 namespace frontend {
 namespace pytorch {
 namespace op {
 using namespace ov::op;
-
 OutputVector translate_quantized_relu6(const NodeContext& context) {
-    num_inputs_check(context, 3, 3);
+    num_inputs_check(context, 2, 2);
     const auto x = context.get_input(0);
-    const auto scale = context.get_input(1);
-    const auto zero_point = context.get_input(2);
     const auto relu6 = context.mark_node(std::make_shared<v0::Clamp>(x, 0.0, 6.0));
-    return {quantize(context, relu6, scale, zero_point, x)};
+    const auto quantized_pt_node = cast_quantized_fw_node(x.get_node_shared_ptr());
+    FRONT_END_OP_CONVERSION_CHECK(quantized_pt_node, "Input must be a quantized tensor");
+    return {quantize(context, relu6, quantized_pt_node->get_scale(), quantized_pt_node->get_zero_point(), x)};
 }
-
 }  // namespace op
 }  // namespace pytorch
 }  // namespace frontend
