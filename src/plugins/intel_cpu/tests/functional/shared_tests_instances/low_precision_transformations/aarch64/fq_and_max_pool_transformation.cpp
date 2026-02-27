71c92c9480005c257704c2175bd043f04bbc96fc
Aleksandr Voron <aleksandr.voron@intel.com>
2026-02-27 14:20:10 +0100

    added FakeQuantizeAndAvgPoolTransformation and FakeQuantizeAndMaxPoolTransformation

---
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/low_precision_transformations/aarch64/fq_and_max_pool_transformation.cpp
@@ -0,0 +1,41 @@
+// Copyright (C) 2018-2026 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <vector>
+
+#include "common_test_utils/test_constants.hpp"
+#include "low_precision_transformations/fake_quantize_and_max_pool_transformation.hpp"
+#include "ov_lpt_models/common/fake_quantize_on_data.hpp"
+
+using namespace LayerTestsDefinitions;
+
+namespace {
+const std::vector<ov::element::Type> precisions = {
+    ov::element::f32
+};
+
+const ov::builder::subgraph::FakeQuantizeOnData fakeQuantizeI8 = {
+    256ul, {}, {-12.8f}, {12.7f}, {-12.8f}, {12.7f}
+};
+
+const ov::builder::subgraph::FakeQuantizeOnData fakeQuantizeU8 = {
+    256ul, {}, {0.f}, {25.5f}, {0.f}, {25.5f}
+};
+
+INSTANTIATE_TEST_SUITE_P(smoke_LPT_i8,
+                         FakeQuantizeAndMaxPoolTransformation,
+                         ::testing::Combine(::testing::ValuesIn(precisions),
+                                            ::testing::Values(ov::PartialShape({1, 32, 72, 48})),
+                                            ::testing::Values(ov::test::utils::DEVICE_CPU),
+                                            ::testing::Values(fakeQuantizeI8)),
+                         FakeQuantizeAndMaxPoolTransformation::getTestCaseName);
+
+INSTANTIATE_TEST_SUITE_P(smoke_LPT_u8,
+                         FakeQuantizeAndMaxPoolTransformation,
+                         ::testing::Combine(::testing::ValuesIn(precisions),
+                                            ::testing::Values(ov::PartialShape({1, 32, 72, 48})),
+                                            ::testing::Values(ov::test::utils::DEVICE_CPU),
+                                            ::testing::Values(fakeQuantizeU8)),
+                         FakeQuantizeAndMaxPoolTransformation::getTestCaseName);
+}  // namespace
