89e547da8ff97b6b267bf7dabe1234c0dd92f789
Andrii Staikov <andrii.staikov@intel.com>
2026-03-02 14:58:39 +0100

    fix test typo

--- src/core/tests/pass/serialization/rt_info_serialization.cpp
+++ src/core/tests/pass/serialization/rt_info_serialization.cpp
@@ -35,7 +35,7 @@ protected:
     }
 
     void TearDown() override {
-        // ov::test::utils::removeIRFiles(m_out_xml_path, m_out_bin_path);
+        ov::test::utils::removeIRFiles(m_out_xml_path, m_out_bin_path);
     }
 
     std::shared_ptr<ov::Model> getWithIRFrontend(const std::string& model_path, const std::string& weights_path) {
