62fc523a7ec4e690c87cd8f13ac99a40f78e8661
Egor Tiuvaev <egor.tyuvaev@intel.com>
2026-03-02 14:06:17 +0100

    Add license to new files

--- tests/memory_tests/src/gpu_memory_sampling.cpp
+++ tests/memory_tests/src/gpu_memory_sampling.cpp
@@ -1,3 +1,6 @@
+// Copyright (C) 2018-2026 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
 
 #include <iostream>
 #include "gpu_memory_sampling.hpp"
--- tests/memory_tests/src/system_memory_sampling.cpp
+++ tests/memory_tests/src/system_memory_sampling.cpp
@@ -1,3 +1,6 @@
+// Copyright (C) 2018-2026 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
 
 #include "system_memory_sampling.hpp"
 
