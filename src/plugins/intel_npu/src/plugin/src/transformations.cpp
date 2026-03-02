f7fadec0fc61052dd310aafeb58d2d97e7f9c519
DariaMityagina <daria.mityagina@intel.com>
2026-03-02 10:11:17 +0000

    BatchMode::PLUGIN shouldn't send a model to the compiler if we failed to process it in the plugin - clang

--- src/plugins/intel_npu/src/plugin/src/transformations.cpp
+++ src/plugins/intel_npu/src/plugin/src/transformations.cpp
@@ -243,7 +243,7 @@ std::tuple<std::shared_ptr<ov::Model>, bool> handlePluginBatching(
     } catch (const std::exception& ex) {
         if (batchMode == ov::intel_npu::BatchMode::AUTO) {
             logger.info("Couldn't validate and reshape the model. Batching will be handled by compiler. Error: %s",
-                    ex.what());
+                        ex.what());
             updateBatchMode(ov::intel_npu::BatchMode::COMPILER);
         } else {
             OPENVINO_THROW("Couldn't validate and reshape the model for PLUGIN batch mode. Error: %s", ex.what());
