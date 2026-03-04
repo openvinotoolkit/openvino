74bac8744a8882a2b3252479beaad670ccf5c163
Bogdan Pereanu <bogdan.pereanu@intel.com>
2026-03-04 09:30:19 +0200

    Revert supports_model_caching changes to fix caching issues
    Signed-off-by: Bogdan Pereanu <bogdan.pereanu@intel.com>

--- src/inference/src/dev/plugin.cpp
+++ src/inference/src/dev/plugin.cpp
@@ -113,9 +113,12 @@ ov::Any ov::Plugin::get_property(const std::string& name, const AnyMap& argument
 }
 
 bool ov::Plugin::supports_model_caching(const ov::AnyMap& arguments) const {
-    return util::contains(get_property(ov::internal::supported_properties), ov::internal::caching_properties) &&
-           is_property_supported(ov::device::capabilities.name(), arguments) &&
-           util::contains(get_property(ov::device::capabilities, arguments), ov::device::capability::EXPORT_IMPORT);
+    bool supported(false);
+    supported =
+        util::contains(get_property(ov::supported_properties), ov::device::capabilities) &&
+        util::contains(get_property(ov::device::capabilities, arguments), ov::device::capability::EXPORT_IMPORT) &&
+        util::contains(get_property(ov::internal::supported_properties), ov::internal::caching_properties);
+    return supported;
 }
 
 bool ov::Plugin::is_property_supported(const std::string& name, const ov::AnyMap& arguments) const {
