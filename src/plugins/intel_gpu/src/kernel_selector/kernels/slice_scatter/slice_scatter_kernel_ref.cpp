837294a77db6b0e90fc4dd703a8aebe311903e3c
wilson-seok <wilson.seok@intel.com>
2026-03-04 17:44:00 +0000

    refactor(GPU): extract shared addJitConstantsForParam into slice_scatter_kernel_utils.h
    Deduplicate identical addJitConstantsForParam function and associated constants (MAX_SUPPORTED_DIM, JIT_AXES_BUFF_SIZE_NAME) from slice_scatter_kernel_ref.cpp and slice_scatter_kernel_opt.cpp into a shared header slice_scatter_kernel_utils.h.

--- src/plugins/intel_gpu/src/kernel_selector/kernels/slice_scatter/slice_scatter_kernel_ref.cpp
+++ src/plugins/intel_gpu/src/kernel_selector/kernels/slice_scatter/slice_scatter_kernel_ref.cpp
@@ -3,53 +3,14 @@
 //
 
 #include "slice_scatter_kernel_ref.h"
+#include "slice_scatter_kernel_utils.h"
+
 #include <kernel_selector_utils.h>
 #include <vector>
 
-namespace {
-static constexpr size_t MAX_SUPPORTED_DIM = 5;
-static constexpr char JIT_AXES_BUFF_SIZE_NAME[] = "AXES_BUFFER_SIZE";
-
-// Generates macros:
-// - name_BUFFER
-// - name_VAL0, name_VAL1 ...
-void addJitConstantsForParam(kernel_selector::JitConstants& jit,
-                             const std::string& name,
-                             const std::vector<std::int64_t>& compile_time_param,
-                             kernel_selector::Datatype type,
-                             const std::function<std::string(std::string, size_t)>& dynamic_access_decorator) {
-    using namespace kernel_selector;
-    const std::string BUFF_CONST_NAME = name + "_BUFFER";
-    const std::string BUFF_PTR_NAME = name + "_buffer_ptr";
-    const auto jit_name_decorator = [](std::string name, size_t i) {
-        return name + "_VAL" + std::to_string(i);
-    };
-
-    if (compile_time_param.empty()) {
-        // Dynamic param:
-        const std::string type_str = toCLType(type);
-        jit.AddConstant(
-            MakeJitConstant(BUFF_CONST_NAME, "__global const " + type_str + "* restrict " + BUFF_PTR_NAME + ","));
-
-        for (size_t i = 0; i < MAX_SUPPORTED_DIM; ++i) {
-            const std::string i_str = std::to_string(i);
-            const std::string jit_name = jit_name_decorator(name, i);
-            const std::string access_str = dynamic_access_decorator(BUFF_PTR_NAME, i);
-            jit.AddConstant(
-                MakeJitConstant(jit_name, i_str + " < " + JIT_AXES_BUFF_SIZE_NAME + " ? (" + access_str + ") : -1"));
-        }
-    } else {
-        // Static param:
-        jit.AddConstant(MakeJitConstant(BUFF_CONST_NAME, ""));
-        for (size_t i = 0; i < MAX_SUPPORTED_DIM; ++i) {
-            const std::string jit_name = jit_name_decorator(name, i);
-            const int64_t val = i < compile_time_param.size() ? compile_time_param[i] : -1;
-            jit.AddConstant(MakeJitConstant(jit_name, val));
-        }
-    }
-}
-
-}  // anonymous namespace
+using kernel_selector::slice_scatter_utils::addJitConstantsForParam;
+using kernel_selector::slice_scatter_utils::JIT_AXES_BUFF_SIZE_NAME;
+using kernel_selector::slice_scatter_utils::MAX_SUPPORTED_DIM;
 
 namespace kernel_selector {
 
