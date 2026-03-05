5f1d5744cc8cb7cbe57ee61dd43c37eb8434aa33
tovinkere <vasanth.tovinkere@gmail.com>
2026-03-05 09:51:56 -0800

    Revert Read/Compile model ITT markers
    Removing additional markers that were used for Condition Compilation checks

--- src/inference/src/cpp/core.cpp
+++ src/inference/src/cpp/core.cpp
@@ -80,7 +80,7 @@ std::map<std::string, Version> Core::get_versions(const std::string& device_name
 std::shared_ptr<ov::Model> Core::read_model(const std::filesystem::path& model_path,
                                             const std::filesystem::path& bin_path,
                                             const ov::AnyMap& properties) const {
-    OV_ITT_SCOPED_REGION_BASE(ov::itt::domains::OV, "Read model");
+    OV_ITT_SCOPED_REGION_BASE(ov::itt::domains::Phases, "Read model");
     OV_CORE_CALL_STATEMENT(return _impl->read_model(model_path, bin_path, properties););
 }
 
@@ -89,8 +89,6 @@ std::shared_ptr<ov::Model> Core::read_model(const std::wstring& model_path,
                                             const std::wstring& bin_path,
                                             const ov::AnyMap& properties) const {
     OV_ITT_SCOPED_REGION_BASE(ov::itt::domains::Phases, "Read model");
-    // Keeping the old task for conditional compilation and not on by dfault
-    OV_ITT_SCOPED_TASK(ov::itt::domains::OV, "Read model");
     return read_model(ov::util::make_path(model_path), ov::util::make_path(bin_path), properties);
 }
 #endif
@@ -99,22 +97,16 @@ std::shared_ptr<ov::Model> Core::read_model(const std::string& model_path,
                                             const std::string& bin_path,
                                             const AnyMap& properties) const {
     OV_ITT_SCOPED_REGION_BASE(ov::itt::domains::Phases, "Read model");
-    // Keeping the old task for conditional compilation and not on by dfault
-    OV_ITT_SCOPED_TASK(ov::itt::domains::OV, "Read model");
     return read_model(ov::util::make_path(model_path), ov::util::make_path(bin_path), properties);
 }
 
 std::shared_ptr<ov::Model> Core::read_model(const std::string& model, const ov::Tensor& weights) const {
     OV_ITT_SCOPED_REGION_BASE(ov::itt::domains::Phases, "Read model");
-    // Keeping the old task for conditional compilation and not on by dfault
-    OV_ITT_SCOPED_TASK(ov::itt::domains::OV, "Read model");
     OV_CORE_CALL_STATEMENT(return _impl->read_model(model, weights););
 }
 
 CompiledModel Core::compile_model(const std::shared_ptr<const ov::Model>& model, const AnyMap& config) {
     OV_ITT_SCOPED_REGION_BASE(ov::itt::domains::Phases, "Compile model");
-    // Keeping the old task for conditional compilation and not on by dfault
-    OV_ITT_SCOPED_TASK(ov::itt::domains::OV, "Read model");
     return compile_model(model, ov::default_device_name, config);
 }
 
@@ -122,8 +114,6 @@ CompiledModel Core::compile_model(const std::shared_ptr<const ov::Model>& model,
                                   const std::string& device_name,
                                   const AnyMap& config) {
     OV_ITT_SCOPED_REGION_BASE(ov::itt::domains::Phases, "Compile model");
-    // Keeping the old task for conditional compilation and not on by dfault
-    OV_ITT_SCOPED_TASK(ov::itt::domains::OV, "Compile model");
     OV_CORE_CALL_STATEMENT({
         auto exec = _impl->compile_model(model, device_name, config);
         return {exec._ptr, exec._so};
@@ -132,8 +122,6 @@ CompiledModel Core::compile_model(const std::shared_ptr<const ov::Model>& model,
 
 CompiledModel Core::compile_model(const std::filesystem::path& model_path, const AnyMap& config) {
     OV_ITT_SCOPED_REGION_BASE(ov::itt::domains::Phases, "Compile model");
-    // Keeping the old task for conditional compilation and not on by dfault
-    OV_ITT_SCOPED_TASK(ov::itt::domains::OV, "Compile model");
     return compile_model(model_path, ov::default_device_name, config);
 }
 
@@ -141,8 +129,6 @@ CompiledModel Core::compile_model(const std::filesystem::path& model_path,
                                   const std::string& device_name,
                                   const AnyMap& config) {
     OV_ITT_SCOPED_REGION_BASE(ov::itt::domains::Phases, "Compile model");
-    // Keeping the old task for conditional compilation and not on by dfault
-    OV_ITT_SCOPED_TASK(ov::itt::domains::OV, "Compile model");
     OV_CORE_CALL_STATEMENT({
         auto exec = _impl->compile_model(model_path, device_name, config);
         return {exec._ptr, exec._so};
@@ -151,24 +137,18 @@ CompiledModel Core::compile_model(const std::filesystem::path& model_path,
 
 CompiledModel Core::compile_model(const std::string& model_path, const AnyMap& config) {
     OV_ITT_SCOPED_REGION_BASE(ov::itt::domains::Phases, "Compile model");
-    // Keeping the old task for conditional compilation and not on by dfault
-    OV_ITT_SCOPED_TASK(ov::itt::domains::OV, "Compile model");
     return compile_model(ov::util::make_path(model_path), ov::default_device_name, config);
 }
 
 #ifdef OPENVINO_ENABLE_UNICODE_PATH_SUPPORT
 CompiledModel Core::compile_model(const std::wstring& model_path, const AnyMap& config) {
     OV_ITT_SCOPED_REGION_BASE(ov::itt::domains::Phases, "Compile model");
-    // Keeping the old task for conditional compilation and not on by dfault
-    OV_ITT_SCOPED_TASK(ov::itt::domains::OV, "Compile model");
     return compile_model(ov::util::make_path(model_path), config);
 }
 #endif
 
 CompiledModel Core::compile_model(const std::string& model_path, const std::string& device_name, const AnyMap& config) {
     OV_ITT_SCOPED_REGION_BASE(ov::itt::domains::Phases, "Compile model");
-    // Keeping the old task for conditional compilation and not on by dfault
-    OV_ITT_SCOPED_TASK(ov::itt::domains::OV, "Compile model");
     return compile_model(ov::util::make_path(model_path), device_name, config);
 }
 
@@ -177,8 +157,6 @@ CompiledModel Core::compile_model(const std::wstring& model_path,
                                   const std::string& device_name,
                                   const AnyMap& config) {
     OV_ITT_SCOPED_REGION_BASE(ov::itt::domains::Phases, "Compile model");
-    // Keeping the old task for conditional compilation and not on by dfault
-    OV_ITT_SCOPED_TASK(ov::itt::domains::OV, "Compile model");
     return compile_model(ov::util::make_path(model_path), device_name, config);
 }
 #endif
@@ -188,8 +166,6 @@ CompiledModel Core::compile_model(const std::string& model,
                                   const std::string& device_name,
                                   const AnyMap& config) {
     OV_ITT_SCOPED_REGION_BASE(ov::itt::domains::Phases, "Compile model");
-    // Keeping the old task for conditional compilation and not on by dfault
-    OV_ITT_SCOPED_TASK(ov::itt::domains::OV, "Compile model");
     OV_CORE_CALL_STATEMENT({
         auto exec = _impl->compile_model(model, weights, device_name, config);
         return {exec._ptr, exec._so};
@@ -200,8 +176,6 @@ CompiledModel Core::compile_model(const std::shared_ptr<const ov::Model>& model,
                                   const RemoteContext& context,
                                   const AnyMap& config) {
     OV_ITT_SCOPED_REGION_BASE(ov::itt::domains::Phases, "Compile model");
-    // Keeping the old task for conditional compilation and not on by dfault
-    OV_ITT_SCOPED_TASK(ov::itt::domains::OV, "Compile model");
     OV_CORE_CALL_STATEMENT({
         auto exec = _impl->compile_model(model, ov::SoPtr<ov::IRemoteContext>{context._impl, context._so}, config);
         return {exec._ptr, exec._so};
@@ -238,7 +212,7 @@ void Core::add_extension(const std::vector<std::shared_ptr<ov::Extension>>& exte
 
 CompiledModel Core::import_model(std::istream& modelStream, const std::string& device_name, const AnyMap& config) {
     OV_ITT_SCOPED_REGION_BASE(ov::itt::domains::Phases, "Import Model");
-    // Keeping the old task for conditional compilation and not on by default
+    // Preserving the old task for conditional compilation and not on by default
     OV_ITT_SCOPED_TASK(ov::itt::domains::OV, "Core::import_model");
     OV_CORE_CALL_STATEMENT({
         auto exec = _impl->import_model(modelStream, device_name, config);
@@ -248,7 +222,7 @@ CompiledModel Core::import_model(std::istream& modelStream, const std::string& d
 
 CompiledModel Core::import_model(std::istream& modelStream, const RemoteContext& context, const AnyMap& config) {
     OV_ITT_SCOPED_REGION_BASE(ov::itt::domains::Phases, "Import Model");
-    // Keeping the old task for conditional compilation and not on by default
+    // Preserving the old task for conditional compilation and not on by default
     OV_ITT_SCOPED_TASK(ov::itt::domains::OV, "Core::import_model");
     OV_CORE_CALL_STATEMENT({
         auto exec = _impl->import_model(modelStream, ov::SoPtr<ov::IRemoteContext>{context._impl, context._so}, config);
@@ -258,7 +232,7 @@ CompiledModel Core::import_model(std::istream& modelStream, const RemoteContext&
 
 CompiledModel Core::import_model(const ov::Tensor& model, const std::string& device_name, const AnyMap& config) {
     OV_ITT_SCOPED_REGION_BASE(ov::itt::domains::Phases, "Import Model");
-    // Keeping the old task for conditional compilation and not on by default
+    // Preserving the old task for conditional compilation and not on by default
     OV_ITT_SCOPED_TASK(ov::itt::domains::OV, "Core::import_model");
     OV_CORE_CALL_STATEMENT({
         auto exec = _impl->import_model(model, device_name, config);
@@ -268,7 +242,7 @@ CompiledModel Core::import_model(const ov::Tensor& model, const std::string& dev
 
 CompiledModel Core::import_model(const ov::Tensor& model, const RemoteContext& context, const AnyMap& config) {
     OV_ITT_SCOPED_REGION_BASE(ov::itt::domains::Phases, "Import Model");
-    // Keeping the old task for conditional compilation and not on by default
+    // Preserving the old task for conditional compilation and not on by default
     OV_ITT_SCOPED_TASK(ov::itt::domains::OV, "Core::import_model");
     OV_CORE_CALL_STATEMENT({
         auto exec = _impl->import_model(model, ov::SoPtr<ov::IRemoteContext>{context._impl, context._so}, config);
