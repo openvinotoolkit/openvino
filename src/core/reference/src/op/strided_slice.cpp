7e9becae1fc7585dc9636c4b545090c382dca657
LiangGao <liang.gao@intel.com>
2026-03-05 11:32:12 +0800

    Add more condition to promise the execution

--- src/core/reference/src/op/strided_slice.cpp
+++ src/core/reference/src/op/strided_slice.cpp
@@ -30,8 +30,10 @@ void strided_slice(const char* arg,
     if (hasZeroDims(sp.reshape_in_shape) || hasZeroDims(sp.reshape_out_shape)) {
         return;
     }
+    OPENVINO_ASSERT(elem_type.bitwidth() >= 8,
+                    "StridedSlice reference implementation does not support element types with bitwidth less than 8.");
 
-    auto in_memory_size = ov::util::get_memory_size_safe(elem_type, sp.reshape_in_shape).value_or(0);
+    auto in_memory_size = ov::util::get_memory_size_safe(elem_type, sp.reshape_in_shape).value_or(1);
     ov::AlignedBuffer slice_out_buffer(in_memory_size);
     slice(arg,
           slice_out_buffer.get_ptr<char>(),
@@ -42,10 +44,12 @@ void strided_slice(const char* arg,
           sp.reshape_in_shape,
           elem_type.size());
 
-    auto out_memory_size = ov::util::get_memory_size_safe(elem_type, sp.reshape_out_shape).value_or(0);
+    auto out_memory_size = ov::util::get_memory_size_safe(elem_type, sp.reshape_out_shape).value_or(1);
     ov::AlignedBuffer reshape_out_buffer(out_memory_size);
 
-    reshape(slice_out_buffer.get_ptr<char>(), reshape_out_buffer.get_ptr<char>(), out_memory_size);
+    auto copy_size = std::min(in_memory_size, out_memory_size);
+
+    reshape(slice_out_buffer.get_ptr<char>(), reshape_out_buffer.get_ptr<char>(), copy_size);
 
     reverse(reshape_out_buffer.get_ptr<char>(),
             out,
