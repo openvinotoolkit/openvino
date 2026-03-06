185b9457cdcd6c0bbf0395c0a074c7d06fe54012
Maxim Vafin <maxim.vafin@intel.com>
2026-03-06 11:30:14 +0100

    [PT FE] Suport aten::where(cond)

--- src/frontends/pytorch/src/op/where.cpp
+++ src/frontends/pytorch/src/op/where.cpp
@@ -3,8 +3,12 @@
 //
 
 #include "openvino/frontend/pytorch/node_context.hpp"
+#include "openvino/op/constant.hpp"
 #include "openvino/op/convert.hpp"
+#include "openvino/op/non_zero.hpp"
 #include "openvino/op/select.hpp"
+#include "openvino/op/split.hpp"
+#include "openvino/op/squeeze.hpp"
 #include "utils.hpp"
 
 namespace ov {
@@ -17,7 +21,24 @@ using namespace ov::op;
 OutputVector translate_where(const NodeContext& context) {
     num_inputs_check(context, 1, 3);
     auto cond = context.get_input(0);
-    PYTORCH_OP_CONVERSION_CHECK(!context.input_is_none(1), "aten::where(cond) unsupported");
+    if (context.input_is_none(1)) {
+        // aten::where(cond) is equivalent to torch.nonzero(cond, as_tuple=True):
+        // returns a tuple of 1D index tensors, one per dimension of cond.
+        const auto& cond_shape = cond.get_partial_shape();
+        PYTORCH_OP_CONVERSION_CHECK(cond_shape.rank().is_static(),
+                                    "aten::where(cond) with input of dynamic rank is not supported");
+        const auto ndim = static_cast<int>(cond_shape.rank().get_length());
+        auto non_zero = context.mark_node(std::make_shared<v3::NonZero>(cond));
+        auto axis = context.mark_node(v0::Constant::create(element::i32, Shape{}, {0}));
+        OutputVector result;
+        if (ndim > 0) {
+            auto split = context.mark_node(std::make_shared<v1::Split>(non_zero, axis, ndim));
+            for (int i = 0; i < ndim; ++i) {
+                result.push_back(context.mark_node(std::make_shared<v0::Squeeze>(split->output(i), axis)));
+            }
+        }
+        return {context.mark_node(make_list_construct(result))};
+    }
     auto bool_cond = context.mark_node(std::make_shared<v0::Convert>(cond, element::boolean));
     Output<Node> x;
     Output<Node> y;
