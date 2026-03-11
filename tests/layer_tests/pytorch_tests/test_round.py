cb3fc574b16f65992694f5ef0595b1aaae89f9ea
suryasidd <surya.siddharth.pemmaraju@intel.com>
2026-03-10 19:12:42 -0700

    Fixed formatting

--- tests/layer_tests/pytorch_tests/test_round.py
+++ tests/layer_tests/pytorch_tests/test_round.py
@@ -9,14 +9,13 @@ from pytorch_layer_test_class import PytorchLayerTest, skip_if_export
 class TestRound(PytorchLayerTest):
     def _prepare_input(self, out=False, dtype="float32"):
         import numpy as np
-
         input = self.random.randn(1, 3, 224, 224, dtype=dtype)
         if dtype == "float64":
             # fp64 can fail by accuracy, because pytorch rounds fp64 value and ov will round fp32 value.
             # To remove sporadic accuracy fails we will round the number to 6 decimal places.
             input = np.round(input, 6)
         if not out:
-            return (input,)
+            return (input, )
         return (input, np.zeros_like(input))
 
     def create_model(self, out=False):
@@ -28,18 +27,20 @@ class TestRound(PytorchLayerTest):
                 if out:
                     self.forward = self.forward_out
 
+
             def forward(self, x):
                 return torch.round(x)
 
             def forward_out(self, x, y):
                 return torch.round(x, out=y), y
 
+
         return aten_round(out), "aten::round"
 
     @pytest.mark.nightly
     @pytest.mark.precommit
-    @pytest.mark.precommit_fx_backend
     @pytest.mark.precommit_torch_export
+    @pytest.mark.precommit_fx_backend
     @pytest.mark.parametrize("out", [skip_if_export(True), False])
     @pytest.mark.parametrize("dtype", ["float32", "float64", "int32", "int64"])
     def test_round(self, out, dtype, ie_device, precision, ir_version):
@@ -51,13 +52,11 @@ class TestRound(PytorchLayerTest):
 class TestRoundScalar(PytorchLayerTest):
     def _prepare_input_int(self):
         import numpy as np
-
-        return (np.array(self.random.randint(low=-5, high=5)),)
+        return (np.array(self.random.randint(low=-5, high=5)), )
 
     def _prepare_input_float(self):
         import numpy as np
-
-        return (np.array(self.random.uniform(low=-5, high=5)),)
+        return (np.array(self.random.uniform(low=-5, high=5)), )
 
     def create_model(self, input_type="float"):
         import torch
@@ -70,11 +69,13 @@ class TestRoundScalar(PytorchLayerTest):
                 else:
                     self.forward = self.forward_float
 
-            def forward_int(self, x: int):
+
+            def forward_int(self, x:int):
                 return torch.round(x)
 
-            def forward_float(self, x: float):
+            def forward_float(self, x:float):
                 return torch.round(x)
+
 
         return aten_round(input_type), "aten::round"
 
