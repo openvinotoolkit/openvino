9a08c64b502bb4de47b6e41dc69924b83d82da7c
LiangGao <liang.gao@intel.com>
2026-03-19 22:48:34 +0800

    Fix clang issue

--- src/plugins/intel_cpu/src/nodes/executors/ref/eltwise.cpp
+++ src/plugins/intel_cpu/src/nodes/executors/ref/eltwise.cpp
@@ -427,8 +427,8 @@ bool EltwiseRef64bExecutor<T, Enable>::supports([[maybe_unused]] const EltwiseCo
 
 template <typename T, typename Enable>
 void EltwiseRef64bExecutor<T, Enable>::exec(const jit_eltwise_call_args_ptrs& args_ptrs,
-                                            const VectorDims& dims_out,
-                                            const CpuParallelPtr& cpu_parallel) {
+                                            [[maybe_unused]] const VectorDims& dims_out,
+                                            [[maybe_unused]] const CpuParallelPtr& cpu_parallel) {
     // Only supported Clamp Operator
     if (this->m_opData.algo == Algorithm::EltwiseClamp) {
         const auto* src_ptr_i = reinterpret_cast<const T*>(args_ptrs.src_ptr[0]);
