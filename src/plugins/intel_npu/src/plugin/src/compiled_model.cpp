85f3186df86262deea1d6e0b037705d986397117
Bogdan Pereanu <bogdan.pereanu@intel.com>
2026-03-20 07:15:26 +0000

    [NPU] Refactor zero infer request (#34763)
    ### Details:
     - *Refactoring zero infer request*
    
    ### Tickets:
     - *N/A*
    
    ### AI Assistance:
     - *AI assistance used: no / yes*
    - *If yes, summarize how AI was used and what human validation was
    performed (build/tests/manual checks).*
    
    ---------
    
    Signed-off-by: Bogdan Pereanu <bogdan.pereanu@intel.com>

--- src/plugins/intel_npu/src/plugin/src/compiled_model.cpp
+++ src/plugins/intel_npu/src/plugin/src/compiled_model.cpp
@@ -69,9 +69,8 @@ std::shared_ptr<ov::IAsyncInferRequest> CompiledModel::create_infer_request() co
             "The driver is not applicable. The driver doesn't exist or is too old to run inference for this blob.");
     }
 
-    const std::shared_ptr<SyncInferRequest>& syncInferRequest =
+    const std::shared_ptr<InferRequest>& syncInferRequest =
         _device->createInferRequest(shared_from_this(), _propertiesManager->getConfig());
-    syncInferRequest->initialize_states();
 
     return std::make_shared<AsyncInferRequest>(syncInferRequest,
                                                get_task_executor(),
