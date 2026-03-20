ff7082ae3d0d67b832974dda412fe810175a5245
Arseniy Obolenskiy <gooddoog@student.su>
2026-03-20 09:51:57 +0100

    Address review comment

--- src/plugins/hetero/src/remote_context.cpp
+++ src/plugins/hetero/src/remote_context.cpp
@@ -13,7 +13,9 @@ namespace ov {
 namespace hetero {
 
 RemoteContext::RemoteContext(std::map<std::string, ov::SoPtr<ov::IRemoteContext>> contexts)
-    : m_contexts(std::move(contexts)) {}
+    : m_contexts(std::move(contexts)) {
+    OPENVINO_ASSERT(!m_contexts.empty(), "HETERO RemoteContext must have at least one underlying context");
+}
 
 const ov::AnyMap& RemoteContext::get_property() const {
     return m_contexts.begin()->second->get_property();
