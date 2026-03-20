10babda3e782dd0ebb75074e9be29a38008db412
Aditya Goyal <goyaladitya2403@gmail.com>
2026-03-20 14:18:57 +0530

    Strengthen regression test with fixed inputs and explicit assertions

--- src/plugins/intel_cpu/tests/functional/custom/subgraph_tests/src/common/reduce_min_concat_const.cpp
+++ src/plugins/intel_cpu/tests/functional/custom/subgraph_tests/src/common/reduce_min_concat_const.cpp
@@ -4,37 +4,41 @@
 
 // Regression test for GitHub issue #33255.
 
-#include "openvino/op/concat.hpp"
-#include "openvino/op/reduce_min.hpp"
-#include "shared_test_classes/base/ov_subgraph.hpp"
-#include "utils/cpu_test_utils.hpp"
+#include <gtest/gtest.h>
 
-using namespace CPUTestUtils;
+#include "common_test_utils/ov_plugin_cache.hpp"
+#include "openvino/op/concat.hpp"
+#include "openvino/op/constant.hpp"
+#include "openvino/op/parameter.hpp"
+#include "openvino/op/reduce_min.hpp"
+#include "openvino/op/result.hpp"
 
 namespace ov {
 namespace test {
 
-class ReduceMinAfterConcatConstTest : public SubgraphBaseStaticTest {};
-
-TEST_F(ReduceMinAfterConcatConstTest, smoke_ReduceMinAfterConcatConst) {
-    targetDevice = ov::test::utils::DEVICE_CPU;
-
-    const ov::Shape paramShape = {7};
-    ov::ParameterVector params{std::make_shared<ov::op::v0::Parameter>(ov::element::i32, paramShape)};
-
+TEST(ReduceMinAfterConcatConstTest, smoke_ReduceMinAfterConcatConst) {
+    auto param = std::make_shared<ov::op::v0::Parameter>(ov::element::i32, ov::Shape{7});
     auto constData =
         ov::op::v0::Constant::create(ov::element::i32, ov::Shape{6}, std::vector<int32_t>{0, -1, 1, 1, 0, 0});
-
-    auto concat = std::make_shared<ov::op::v0::Concat>(ov::NodeVector{params[0], constData}, 0);
-
+    auto concat = std::make_shared<ov::op::v0::Concat>(ov::NodeVector{param, constData}, 0);
     auto axes = ov::op::v0::Constant::create(ov::element::i64, ov::Shape{1}, std::vector<int64_t>{0});
     auto reduceMin = std::make_shared<ov::op::v1::ReduceMin>(concat, axes, false);
+    auto result = std::make_shared<ov::op::v0::Result>(reduceMin);
+    auto model = std::make_shared<ov::Model>(ov::ResultVector{result}, ov::ParameterVector{param});
 
-    function = std::make_shared<ov::Model>(ov::ResultVector{std::make_shared<ov::op::v0::Result>(reduceMin)},
-                                           params,
-                                           "ReduceMinAfterConcatConst");
+    auto core = ov::test::utils::PluginCache::get().core();
+    auto compiled = core->compile_model(model, "CPU");
+    auto req = compiled.create_infer_request();
 
-    run();
+    // param input: {5, 3, 7, 2, 4, 8, 6}, const: {0, -1, 1, 1, 0, 0}
+    // concatenated: {5, 3, 7, 2, 4, 8, 6, 0, -1, 1, 1, 0, 0}, min = -1
+    std::vector<int32_t> inputData{5, 3, 7, 2, 4, 8, 6};
+    req.set_input_tensor(ov::Tensor(ov::element::i32, ov::Shape{7}, inputData.data()));
+    req.infer();
+
+    auto output = req.get_output_tensor(0);
+    ASSERT_EQ(output.get_shape(), ov::Shape({})) << "Expected scalar output, got shape " << output.get_shape();
+    ASSERT_EQ(output.data<int32_t>()[0], -1) << "Expected minimum value -1";
 }
 
 }  // namespace test
