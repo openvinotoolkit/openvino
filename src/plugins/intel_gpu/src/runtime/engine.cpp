35231be197c0942485dd8e6fe5c02e06a4bb7473
sahil <sahilbhatane6@gmail.com>
2026-03-23 16:39:49 +0530

    Fix missing space in GPU allocation error message

--- src/plugins/intel_gpu/src/runtime/engine.cpp
+++ src/plugins/intel_gpu/src/runtime/engine.cpp
@@ -309,7 +309,7 @@ bool engine::check_allocatable(const layout& layout, allocation_type type) {
         OPENVINO_ASSERT(!exceed_allocatable_mem_size,
                         "[GPU] Exceeded max size of memory object allocation: ",
                         "requested ", layout.bytes_count(), " bytes, "
-                        "but max alloc size supported by device is ", get_device_info().max_alloc_mem_size, " bytes.",
+                        "but max alloc size supported by device is ", get_device_info().max_alloc_mem_size, " bytes. ",
                         "Please try to reduce batch size or use lower precision.");
     }
 
