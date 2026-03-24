80aef7932a18e0dc60c349155b0797ba325fdf7c
czekun <chen.zekun@intel.com>
2026-03-23 19:25:42 -0700

    add transpose check for output, remove reshape handling. expand tests.

--- src/common/transformations/src/transformations/op_conversions/convert_weight_compressed_conv1x1_to_matmul.cpp
+++ src/common/transformations/src/transformations/op_conversions/convert_weight_compressed_conv1x1_to_matmul.cpp
@@ -111,37 +111,35 @@ ov::pass::ConvertWeightCompressedConv1x1ToMatmul::ConvertWeightCompressedConv1x1
         auto zp = (pattern_map.count(weights_zp_m) > 0) ? pattern_map.at(weights_zp_m).get_node_shared_ptr() : nullptr;
         auto activation = pattern_map.at(first_input_m).get_node_shared_ptr();
 
-        // expects NHWC -> NCHW transpose on activation
-        const auto act_order = ov::as_type_ptr<ov::op::v0::Constant>(pattern_map.at(a_order_m).get_node_shared_ptr());
-        OPENVINO_ASSERT(act_order);
-        const auto act_order_data = act_order->cast_vector<int64_t>();
-        const auto act_rank = act_order_data.size();
-        const auto is_act_transpose = pattern_map.count(transpose_activations_m) > 0;
-        if (is_act_transpose) {
+        const auto ensure_transpose_order = [](const std::shared_ptr<ov::op::v0::Constant>& order_node,
+                                               const std::array<size_t, 3>& order_offset) {
+            OPENVINO_ASSERT(order_node);
+            const auto order_data = order_node->cast_vector<int64_t>();
+            const auto order_rank = order_data.size();
+            OPENVINO_ASSERT(order_rank >= 3);  // to make an input to conv2/3d, rank should be at least 3
             // 0,3,1,2
-            if (act_rank < 3 || act_order_data[act_rank - 1] != act_rank - 2 ||
-                act_order_data[act_rank - 2] != act_rank - 3 || act_order_data[act_rank - 3] != act_rank - 1) {
+            if (order_data[order_rank - 3] != static_cast<int64_t>(order_rank - order_offset[0]) ||
+                order_data[order_rank - 2] != static_cast<int64_t>(order_rank - order_offset[1]) ||
+                order_data[order_rank - 1] != static_cast<int64_t>(order_rank - order_offset[2])) {
                 return false;
             }
-        } else {
-            // reshape is not transposing, so expects C==1 or HW==1
-            const auto act_reshape =
-                ov::as_type_ptr<ov::op::v1::Reshape>(pattern_map.at(reshape_activations_m).get_node_shared_ptr());
-            const auto& dimC = act_order_data[act_rank - 3];
-            const auto& dimH = act_order_data[act_rank - 2];
-            const auto& dimW = act_order_data[act_rank - 1];
-            if (act_rank < 3 || !((dimH == 1 && dimW == 1) || dimC == 1)) {
+            return true;
+        };
+        if (pattern_map.count(transpose_activations_m) > 0) {
+            const auto act_order =
+                ov::as_type_ptr<ov::op::v0::Constant>(pattern_map.at(a_order_m).get_node_shared_ptr());
+            // expects NHWC -> NCHW transpose on activation, 4 - [0,3,1,2]
+            if (!ensure_transpose_order(act_order, {1, 3, 2})) {
                 return false;
             }
-            // not eliminate reshape, simply replace with NHWC to avoid incompatible activation shape
-            auto new_order_data = act_order_data;
-            new_order_data[act_rank - 1] = dimC;  // C
-            new_order_data[act_rank - 2] = dimW;  // W
-            new_order_data[act_rank - 3] = dimH;  // H
-            const auto new_order = ov::op::v0::Constant::create(element::i64, Shape{act_rank}, new_order_data);
-            const auto new_reshape =
-                std::make_shared<ov::op::v1::Reshape>(activation, new_order, act_reshape->get_special_zero());
-            activation = new_reshape;
+        }
+        if (pattern_map.count(transpose_output_m) > 0) {
+            const auto out_order =
+                ov::as_type_ptr<ov::op::v0::Constant>(pattern_map.at(c_order_m).get_node_shared_ptr());
+            // expects NCHW -> NHWC transpose on result, 4 - [0,2,3,1]
+            if (!ensure_transpose_order(out_order, {2, 1, 3})) {
+                return false;
+            }
         }
 
         auto reshape_const_to_2d = [](std::shared_ptr<ov::Node> node) {
