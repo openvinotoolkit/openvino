7d7568c98ce355477ab241762fc3e8bf45d725db
Mircea-Aurelian Dan <mircea-aurelian.dan@intel.com>
2026-04-01 18:04:19 +0300

    Fix writing wrong number of bytes during `export_model` depending on actual encrypted blobs sizes & add logic for standard allocations during `import_model`

--- src/plugins/intel_npu/src/compiler_adapter/src/graph.cpp
+++ src/plugins/intel_npu/src/compiler_adapter/src/graph.cpp
@@ -108,15 +108,16 @@ std::pair<uint64_t, std::optional<std::vector<uint64_t>>> Graph::export_blob(
 
     size_t size = utils::align_size_to_standard_page_size(blobSize);
     size_t paddingSize = size - blobSize;
-
     if (encryptionCallbackOpt.has_value()) {
         std::string tmpBlobStr(reinterpret_cast<const char*>(blobPtr), blobSize);
-        if (paddingSize > 0) {
-            // need to encrypt blob as a whole with padding included, otherwise encryption key mismatches might occur
-            std::fill_n(std::back_inserter(tmpBlobStr), paddingSize, 0);
-        }
         auto encryptedBlobStr = encryptionCallbackOpt.value()(tmpBlobStr);
         tmpBlobStr.clear();
+        size = utils::align_size_to_standard_page_size(encryptedBlobStr.size());
+        paddingSize = size - encryptedBlobStr.size();
+        if (paddingSize > 0) {
+            // cannot encrypt padding due to potential mismatches when decrypting
+            std::fill_n(std::back_inserter(encryptedBlobStr), paddingSize, 0);
+        }
 
         if (size > static_cast<decltype(size)>(std::numeric_limits<std::streamsize>::max())) {
             OPENVINO_THROW("Blob size is too large to be represented on a std::streamsize!");
@@ -139,7 +140,7 @@ std::pair<uint64_t, std::optional<std::vector<uint64_t>>> Graph::export_blob(
         for (const uint8_t* it = blobPtr; it != blobPtr + blobSize; ++it) {
             result = ((result << 7) + result) + static_cast<uint32_t>(*it);
         }
-        _logger.info("Blob size: %ld, hash: %x", blobSize, result);
+        _logger.info("Blob size: %llu, hash: %x", blobSize, result);
     }
 
     if (paddingSize > 0) {
@@ -152,7 +153,7 @@ std::pair<uint64_t, std::optional<std::vector<uint64_t>>> Graph::export_blob(
             }
         }
 
-        _logger.info("Blob size with padding: %ld", size);
+        _logger.info("Blob size with padding: %llu", size);
     }
 
     _logger.info("Write blob to stream successfully.");
