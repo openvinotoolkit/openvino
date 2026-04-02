d96c9b7f404b9258efc1728f3ba58344a37a43c6
Mircea-Aurelian Dan <mircea-aurelian.dan@intel.com>
2026-04-02 15:03:53 +0300

    Add more tests

--- src/plugins/intel_npu/tests/functional/shared_tests_instances/behavior/compiled_model/model_cache.cpp
+++ src/plugins/intel_npu/tests/functional/shared_tests_instances/behavior/compiled_model/model_cache.cpp
@@ -13,7 +13,7 @@ using namespace ov::test::behavior;
 
 namespace {
 
-std::vector<ov::AnyMap> config = {{{}}, {ov::enable_weightless(true)}};
+std::vector<ov::AnyMap> config = {{}, {ov::enable_weightless(true)}};
 
 INSTANTIATE_TEST_SUITE_P(smoke_BehaviorTests,
                          WeightlessCacheAccuracy,
@@ -23,6 +23,6 @@ INSTANTIATE_TEST_SUITE_P(smoke_BehaviorTests,
                                             ::testing::Values(ov::element::f16),  // m_model_dtype
                                             ::testing::ValuesIn(config),          // config parsed with std::ignore
                                             ::testing::Values(ov::test::utils::DEVICE_NPU)),  // m_target_device
-                         ov::test::utils::appendPlatformTypeTestName<WeightlessCacheAccuracy>);
+                         WeightlessCacheAccuracy::get_test_case_name);
 
 }  // namespace
