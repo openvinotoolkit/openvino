d4a69211362b7116291b8d9e5d42212a8975a62b
yanlan song <bell.song@intel.com>
2026-04-09 10:36:09 +0800

    [PA support SD] support a tree mask for dynamic tree speculative decoding (#34644)
    ### Details:
    - split the tree mask impl from
    https://github.com/openvinotoolkit/openvino/pull/33638
    - Compute query-to-query attention bias based on tree attention passed
    from draft model
    draft model will produce a tree shaped struct with the nodes as
    candidates for main model, such as below:
        
    <img width="286" height="166" alt="image"
    src="https://github.com/user-attachments/assets/8e524df2-277a-49a2-b83c-ac4f39b41a99"
    />
    
    this PR is to handle the internal attention among these candidate nodes,
    when then number_of_assistant_tokens is set, a mask with shape of
    [batch_mask_size_in_tokens] will be applied, when sequence length is 1,
    the mask shape is [number_of_assistant_tokens *
    num_of_assistant_tokens], illustrated below the mask size for 8
    assistant tokens is 8x8:
    
    <img width="209" height="195" alt="image"
    src="https://github.com/user-attachments/assets/92fc621c-c25e-4f8e-819e-9a5d23858c9b"
    />
    
     - Add unit tests in paged_attention_gpu_test.cpp
     - vllm reference impl: https://github.com/vllm-project/vllm/pull/20401
    - additional kv-update is required as part of pipeline after the
    auto-regressive decoding to ensure the accracy, this is separately
    handled in https://github.com/openvinotoolkit/openvino/pull/34875
    
    ### Tickets:
    - [CVS-178891](https://jira.devtools.intel.com/browse/CVS-178891),
    [CVS-178772](https://jira.devtools.intel.com/browse/CVS-178772)
    
    ---------
    
    Signed-off-by: xuchen-intel <chen.xu@intel.com>
    Signed-off-by: fishbell <bell.song@intel.com>
    Co-authored-by: xuchen-intel <chen.xu@intel.com>
    Co-authored-by: Chen Peter <peter.chen@intel.com>

--- src/plugins/intel_cpu/tests/functional/custom/subgraph_tests/src/x64/paged_attn_score.cpp
+++ src/plugins/intel_cpu/tests/functional/custom/subgraph_tests/src/x64/paged_attn_score.cpp
@@ -139,6 +139,8 @@ public:
         auto adaptive_rkv_diversity_block_set_indices_begins =
             std::make_shared<ov::op::v0::Constant>(ov::element::i32, Shape{0}, std::vector<int32_t>{0});
         auto token_type_ids = std::make_shared<ov::op::v0::Constant>(ov::element::i32, ov::Shape{0}, std::vector<int32_t>{});
+        auto qq_bias = std::make_shared<ov::op::v0::Constant>(ov::element::u8, Shape{0}, std::vector<uint8_t>{0});
+        auto qq_bias_begins = std::make_shared<ov::op::v0::Constant>(ov::element::i32, Shape{0}, std::vector<int32_t>{0});
         ParameterVector params =
             {q, k, v, key_cache, value_cache, past_lens, subsequence_begins, block_indices, block_indices_begins};
         auto paged_attn = std::make_shared<op::PagedAttentionExtension>(OutputVector{q,
@@ -166,7 +168,9 @@ public:
                                                                                      adaptive_rkv_evictable_sizes,
                                                                                      adaptive_rkv_diversity_block_set_indices,
                                                                                      adaptive_rkv_diversity_block_set_indices_begins,
-                                                                                     token_type_ids});
+                                                                                     token_type_ids,
+                                                                                     qq_bias,
+                                                                                     qq_bias_begins});
         paged_attn->get_rt_info()["num_k_heads"] = head_num;
         paged_attn->get_rt_info()["k_head_size"] = head_size;
         paged_attn->get_rt_info()["num_v_heads"] = head_num;
