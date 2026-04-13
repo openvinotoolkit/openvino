68d408dcd318cca2992419a558a784012a036de1
Mikołaj Roszczyk <mikolaj.roszczyk@intel.com>
2026-04-13 14:35:24 +0000

    unify zet_api and zes_api

--- src/plugins/intel_gpu/src/runtime/ze/compute_runtime/ze_stypes.h
+++ src/plugins/intel_gpu/src/runtime/ze/compute_runtime/ze_stypes.h
@@ -10,10 +10,11 @@
 #define _ZE_STYPES_H
 
 #include <level_zero/ze_api.h>
-#include <zes_api.h>
-#include <zet_api.h>
+#include <level_zero/zes_api.h>
+#include <level_zero/zet_api.h>
 
 #include <cstdint>
+
 using ze_structure_type_ext_t = uint32_t;
 using zet_structure_type_ext_t = uint32_t;
 using zes_structure_type_ext_t = uint32_t;
