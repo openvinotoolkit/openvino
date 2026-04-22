3cd791b73632705ef1e95973b1bef5a8184a0bed
Mikołaj Roszczyk <mikolaj.roszczyk@intel.com>
2026-04-22 17:02:21 +0000

    overcomplicating utils.cmake

--- src/plugins/intel_gpu/cmake/utils.cmake
+++ src/plugins/intel_gpu/cmake/utils.cmake
@@ -13,11 +13,18 @@ function(ov_gpu_set_runtime_interface_for TARGET_NAME)
         target_compile_definitions(${TARGET_NAME} PRIVATE OV_GPU_WITH_ZE_RT=1)
         if(NOT _ov_gpu_skip_link)
             target_link_libraries(${TARGET_NAME} PRIVATE openvino::zero_loader)
+        else()
+            target_include_directories(${TARGET_NAME} PRIVATE
+                $<TARGET_PROPERTY:openvino::zero_loader,INTERFACE_INCLUDE_DIRECTORIES>
+                $<TARGET_PROPERTY:LevelZero::Headers,INTERFACE_INCLUDE_DIRECTORIES>)
         endif()
     elseif(GPU_RT_TYPE STREQUAL "OCL")
         target_compile_definitions(${TARGET_NAME} PRIVATE OV_GPU_WITH_OCL_RT=1)
         if(NOT _ov_gpu_skip_link)
             target_link_libraries(${TARGET_NAME} PRIVATE OpenCL::OpenCL)
+        else()
+            target_include_directories(${TARGET_NAME} PRIVATE
+                $<TARGET_PROPERTY:OpenCL::OpenCL,INTERFACE_INCLUDE_DIRECTORIES>)
         endif()
     else()
         message(FATAL_ERROR "Invalid GPU runtime type: `${GPU_RT_TYPE}` Only `L0` and `OCL` are supported")
