ed2781509e02887f3bd944db8d49d43166a67485
Kasprzak, Jakub <jakub.kasprzak@intel.com>
2026-04-24 09:39:11 +0000

    Add image support check

--- src/plugins/intel_gpu/src/runtime/ze/ze_device.cpp
+++ src/plugins/intel_gpu/src/runtime/ze/ze_device.cpp
@@ -325,7 +325,9 @@ memory_capabilities init_memory_caps(ze_device_handle_t device, const device_inf
             memory_caps.push_back(allocation_type::usm_device);
         }
     }
-    memory_caps.push_back(allocation_type::ze_image);
+    if (info.supports_image) {
+        memory_caps.push_back(allocation_type::ze_image);
+    }
 
     return memory_capabilities(memory_caps);
 }
