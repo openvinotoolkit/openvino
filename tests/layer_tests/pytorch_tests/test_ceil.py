61d5534bead1633650a6707fd0b98ef98e44157a
adarsh kumar <adarshkumar2024@gmail.com>
2026-04-27 04:24:33 +0000

    Add PyTorch layer tests for prim operators

--- tests/layer_tests/pytorch_tests/test_ceil.py
+++ tests/layer_tests/pytorch_tests/test_ceil.py
@@ -1,54 +1,20 @@
-# Copyright (C) 2018-2026 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-
 import pytest
-
-from pytorch_layer_test_class import PytorchLayerTest, skip_if_export
-
+from pytorch_layer_test_class import PytorchLayerTest
 
 class TestCeil(PytorchLayerTest):
-    def _prepare_input(self, out=False, dtype="float32"):
+    def _prepare_input(self):
         import numpy as np
-        input = self.random.randn(1, 3, 224, 224, dtype=dtype)
+        return (np.random.randn(1, 3, 224, 224).astype(np.float32),)
 
-        if dtype == "float64":
-            input = np.round(input, 6)
-
-        if not out:
-            return (input, )
-        return (input, np.zeros_like(input))
-
-    def create_model(self, out=False):
+    def create_model(self):
         import torch
 
-        class aten_ceil(torch.nn.Module):
-            def __init__(self, out):
-                super().__init__()
-                if out:
-                    self.forward = self.forward_out
-
+        class Model(torch.nn.Module):
             def forward(self, x):
                 return torch.ceil(x)
 
-            def forward_out(self, x, y):
-                return torch.ceil(x, out=y), y
+        return Model(), "aten::ceil"
 
-        return aten_ceil(out), "aten::ceil"
-
-    @pytest.mark.nightly
     @pytest.mark.precommit
-    @pytest.mark.precommit_torch_export
-    @pytest.mark.precommit_fx_backend
-    @pytest.mark.parametrize("out", [skip_if_export(True), False])
-    @pytest.mark.parametrize("dtype", ["float32", "float64", "int32", "int64"])
-    def test_ceil(self, out, dtype, ie_device, precision, ir_version):
-        if ie_device == "GPU" and dtype not in ["float32", "float64"]:
-            pytest.xfail(reason="ceil is not supported on GPU for integer types")
-
-        self._test(
-            *self.create_model(out),
-            ie_device,
-            precision,
-            ir_version,
-            kwargs_to_prepare_input={"out": out, "dtype": dtype}
-        )
+    def test_ceil(self, ie_device, precision, ir_version):
+        self._test(*self.create_model(), ie_device, precision, ir_version)
--- tests/layer_tests/pytorch_tests/test_int.py
+++ tests/layer_tests/pytorch_tests/test_int.py
@@ -1,31 +1,20 @@
-# Copyright (C) 2018-2026 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-
 import pytest
-
 from pytorch_layer_test_class import PytorchLayerTest
-
 
 class TestInt(PytorchLayerTest):
     def _prepare_input(self):
         import numpy as np
-        return (np.array([3.7], dtype=np.float32), )
+        return (np.array([1.5], dtype=np.float32),)
 
     def create_model(self):
         import torch
 
-        class aten_int(torch.nn.Module):
+        class Model(torch.nn.Module):
             def forward(self, x):
-                return x.int()
+                return int(x)
 
-        return aten_int(), "aten::to"
+        return Model(), "prim::int"
 
-    @pytest.mark.nightly
     @pytest.mark.precommit
     def test_int(self, ie_device, precision, ir_version):
-        self._test(
-            *self.create_model(),
-            ie_device,
-            precision,
-            ir_version
-        )
+        self._test(*self.create_model(), ie_device, precision, ir_version)
