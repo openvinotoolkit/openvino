113a2bdf26bafd5876093acddcc783979eeff074
Andrii Staikov <andrii.staikov@intel.com>
2026-04-27 17:05:19 +0200

    Update src/tests/functional/plugin/shared/include/subgraph_tests/gated_delta_net.hpp

--- src/tests/functional/plugin/shared/include/subgraph_tests/gated_delta_net.hpp
+++ src/tests/functional/plugin/shared/include/subgraph_tests/gated_delta_net.hpp
@@ -12,11 +12,11 @@ TEST_P(GatedDeltaNet, CompareWithRefs) {
     SKIP_IF_CURRENT_TEST_IS_DISABLED();
     run();
     auto function = compiledModel.get_runtime_model();
-    CheckNumberOfNodesWithTypes(function, {"GatedDeltaNet"}, 1);
-    CheckNumberOfNodesWithTypes(function, {"Transpose"}, 0);
-    CheckNumberOfNodesWithTypes(function, {"Concat"}, 0);
-    CheckNumberOfNodesWithTypes(function, {"ReduceSum"}, 0);
-    CheckNumberOfNodesWithTypes(function, {"Multiply"}, 0);
-    CheckNumberOfNodesWithTypes(function, {"Divide"}, 0);
+    CheckNumberOfNodesWithType(function, {"GatedDeltaNet"}, 1);
+    CheckNumberOfNodesWithType(function, {"Transpose"}, 0);
+    CheckNumberOfNodesWithType(function, {"Concat"}, 0);
+    CheckNumberOfNodesWithType(function, {"ReduceSum"}, 0);
+    CheckNumberOfNodesWithType(function, {"Multiply"}, 0);
+    CheckNumberOfNodesWithType(function, {"Divide"}, 0);
 };
 }  // namespace ov::test
