66a90e1f7f5b81074c32557205f8397c7ee83737
Andrii Staikov <andrii.staikov@intel.com>
2026-04-27 17:06:21 +0200

    Apply suggestions from code review
    Co-authored-by: Andrii Staikov <andrii.staikov@intel.com>

--- src/tests/functional/plugin/shared/include/subgraph_tests/rotary_pos_emb.hpp
+++ src/tests/functional/plugin/shared/include/subgraph_tests/rotary_pos_emb.hpp
@@ -20,98 +20,98 @@ TEST_P(RoPETestQwenVL, CompareWithRefs) {
     SKIP_IF_CURRENT_TEST_IS_DISABLED();
     run();
     auto function = compiledModel.get_runtime_model();
-    CheckNumberOfNodesWithTypes(function, {"RoPE"}, 1);
+    CheckNumberOfNodesWithType(function, {"RoPE"}, 1);
 };
 
 TEST_P(RoPETestLlama2StridedSlice, CompareWithRefs) {
     SKIP_IF_CURRENT_TEST_IS_DISABLED();
     run();
     auto function = compiledModel.get_runtime_model();
-    CheckNumberOfNodesWithTypes(function, {"RoPE"}, 1);
+    CheckNumberOfNodesWithType(function, {"RoPE"}, 1);
 };
 
 TEST_P(RoPETestChatGLMStridedSlice, CompareWithRefs) {
     SKIP_IF_CURRENT_TEST_IS_DISABLED();
     run();
     auto function = compiledModel.get_runtime_model();
-    CheckNumberOfNodesWithTypes(function, {"RoPE"}, 1);
+    CheckNumberOfNodesWithType(function, {"RoPE"}, 1);
 };
 
 TEST_P(RoPETestQwen7bStridedSlice, CompareWithRefs) {
     SKIP_IF_CURRENT_TEST_IS_DISABLED();
     run();
     auto function = compiledModel.get_runtime_model();
-    CheckNumberOfNodesWithTypes(function, {"RoPE"}, 1);
+    CheckNumberOfNodesWithType(function, {"RoPE"}, 1);
 };
 
 TEST_P(RoPETestGPTJStridedSlice, CompareWithRefs) {
     SKIP_IF_CURRENT_TEST_IS_DISABLED();
     run();
     auto function = compiledModel.get_runtime_model();
-    CheckNumberOfNodesWithTypes(function, {"RoPE"}, 1);
+    CheckNumberOfNodesWithType(function, {"RoPE"}, 1);
 };
 
 TEST_P(RoPETestRotateHalfWithoutTranspose, CompareWithRefs) {
     SKIP_IF_CURRENT_TEST_IS_DISABLED();
     run();
     auto function = compiledModel.get_runtime_model();
-    CheckNumberOfNodesWithTypes(function, {"RoPE"}, 1);
+    CheckNumberOfNodesWithType(function, {"RoPE"}, 1);
 }
 
 TEST_P(RoPETestLlama2Slice, CompareWithRefs) {
     SKIP_IF_CURRENT_TEST_IS_DISABLED();
     run();
     auto function = compiledModel.get_runtime_model();
-    CheckNumberOfNodesWithTypes(function, {"RoPE"}, 1);
+    CheckNumberOfNodesWithType(function, {"RoPE"}, 1);
 };
 
 TEST_P(RoPETestChatGLMSlice, CompareWithRefs) {
     SKIP_IF_CURRENT_TEST_IS_DISABLED();
     run();
     auto function = compiledModel.get_runtime_model();
-    CheckNumberOfNodesWithTypes(function, {"RoPE"}, 1);
+    CheckNumberOfNodesWithType(function, {"RoPE"}, 1);
 };
 
 TEST_P(RoPETestQwen7bSlice, CompareWithRefs) {
     SKIP_IF_CURRENT_TEST_IS_DISABLED();
     run();
     auto function = compiledModel.get_runtime_model();
-    CheckNumberOfNodesWithTypes(function, {"RoPE"}, 1);
+    CheckNumberOfNodesWithType(function, {"RoPE"}, 1);
 };
 
 TEST_P(RoPETestGPTJSlice, CompareWithRefs) {
     SKIP_IF_CURRENT_TEST_IS_DISABLED();
     run();
     auto function = compiledModel.get_runtime_model();
-    CheckNumberOfNodesWithTypes(function, {"RoPE"}, 1);
+    CheckNumberOfNodesWithType(function, {"RoPE"}, 1);
 };
 
 TEST_P(RoPETestChatGLM2DRoPEStridedSlice, CompareWithRefs) {
     SKIP_IF_CURRENT_TEST_IS_DISABLED();
     run();
     auto function = compiledModel.get_runtime_model();
-    CheckNumberOfNodesWithTypes(function, {"RoPE"}, 1);
+    CheckNumberOfNodesWithType(function, {"RoPE"}, 1);
 };
 
 TEST_P(RoPETestChatGLMHF, CompareWithRefs) {
     SKIP_IF_CURRENT_TEST_IS_DISABLED();
     run();
     auto function = compiledModel.get_runtime_model();
-    CheckNumberOfNodesWithTypes(function, {"RoPE"}, 1);
+    CheckNumberOfNodesWithType(function, {"RoPE"}, 1);
 };
 
 TEST_P(RoPETestGPTOSS, CompareWithRefs) {
     SKIP_IF_CURRENT_TEST_IS_DISABLED();
     run();
     auto function = compiledModel.get_runtime_model();
-    CheckNumberOfNodesWithTypes(function, {"RoPE"}, 1);
+    CheckNumberOfNodesWithType(function, {"RoPE"}, 1);
 };
 
 TEST_P(RoPETestLtxVideo, CompareWithRefs) {
     SKIP_IF_CURRENT_TEST_IS_DISABLED();
     run();
     auto function = compiledModel.get_runtime_model();
-    CheckNumberOfNodesWithTypes(function, {"RoPE"}, 1);
+    CheckNumberOfNodesWithType(function, {"RoPE"}, 1);
 };
 
 }  // namespace test
