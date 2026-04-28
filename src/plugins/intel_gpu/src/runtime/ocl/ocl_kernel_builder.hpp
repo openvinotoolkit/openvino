38fa6289a89847aad888ae9594e164654f1f37a3
Jakub Kasprzak <jakub.kasprzak@intel.com>
2026-04-28 17:26:20 +0200

    Improve comment

--- src/plugins/intel_gpu/src/runtime/ocl/ocl_kernel_builder.hpp
+++ src/plugins/intel_gpu/src/runtime/ocl/ocl_kernel_builder.hpp
@@ -79,7 +79,8 @@ class ocl_kernel_builder : public kernel_builder{
             }
             for (auto& k : kernels) {
                 const auto &entry_point = k.getInfo<CL_KERNEL_FUNCTION_NAME>();
-                // Workaround for GSD-12674
+                // Intel_Symbol_Table_Void_Program does not correspond to any real kernel
+                // Ignore this entry point as workaround for GSD-12674
                 if (entry_point == "Intel_Symbol_Table_Void_Program") {
                     continue;
                 }
