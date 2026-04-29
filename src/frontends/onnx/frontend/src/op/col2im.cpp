1b3ad15dfc61beb37cbdf337101f7e434217e624
김대현 (Kim DaeHyun) <daehyun99@kakao.com>
2026-04-29 21:24:22 +0900

    Fix build error

--- src/frontends/onnx/frontend/src/op/col2im.cpp
+++ src/frontends/onnx/frontend/src/op/col2im.cpp
@@ -59,8 +59,9 @@ ov::OutputVector col2im(const ov::frontend::onnx::Node& node) {
                                                   dilations,
                                                   pads_begin,
                                                   pads_end)};
+}
 
-    ONNX_OP("Col2Im", OPSET_SINCE(1), ai_onnx::opset_18::col2im);
+ONNX_OP("Col2Im", OPSET_SINCE(1), ai_onnx::opset_18::col2im);
 }  // namespace opset_18
 }  // namespace ai_onnx
 }  // namespace onnx
