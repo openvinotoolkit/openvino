51ede59505224c676412a889c5acce7b88713e55
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2026-04-29 13:50:18 +0000

    Added tests for all supported Reduce* ops

--- src/plugins/intel_gpu/tests/functional/single_layer_tests/reduce.cpp
+++ src/plugins/intel_gpu/tests/functional/single_layer_tests/reduce.cpp
@@ -8,8 +8,11 @@
 #include "shared_test_classes/base/ov_subgraph.hpp"
 #include "openvino/op/parameter.hpp"
 #include "openvino/op/constant.hpp"
-#include "openvino/op/reduce_sum.hpp"
+#include "openvino/op/reduce_max.hpp"
 #include "openvino/op/reduce_mean.hpp"
+#include "openvino/op/reduce_min.hpp"
+#include "openvino/op/reduce_prod.hpp"
+#include "openvino/op/reduce_sum.hpp"
 #include "openvino/op/add.hpp"
 #include "openvino/op/multiply.hpp"
 #include "openvino/runtime/intel_gpu/properties.hpp"
@@ -103,14 +106,15 @@ INSTANTIATE_TEST_SUITE_P(
                        ::testing::Values(false)),
     ReduceSumSqueezeTest::getTestCaseName);
 
-using ReduceMeanParams = std::tuple<ov::Shape,             // Input shape
-                                    ov::element::Type,     // Input precision
-                                    std::vector<int64_t>,  // Reduce axes
-                                    bool>;                 // Keep dims
+using ReduceParams = std::tuple<ov::Shape,             // Input shape
+                                ov::element::Type,     // Input precision
+                                std::vector<int64_t>,  // Reduce axes
+                                bool>;                 // Keep dims
 
-class ReduceMeanTest : public testing::WithParamInterface<ReduceMeanParams>, virtual public ov::test::SubgraphBaseStaticTest {
+template <typename ReduceOp>
+class ReduceTest : public testing::WithParamInterface<ReduceParams>, virtual public ov::test::SubgraphBaseStaticTest {
 public:
-    static std::string getTestCaseName(const testing::TestParamInfo<ReduceMeanParams>& obj) {
+    static std::string getTestCaseName(const testing::TestParamInfo<ReduceParams>& obj) {
         const auto& [input_shape, precision, axes, keep_dims] = obj.param;
         std::ostringstream result;
         result << "IS=" << ov::test::utils::vec2str(input_shape) << "_";
@@ -126,22 +130,34 @@ protected:
         const auto& [input_shape, precision, axes, keep_dims] = GetParam();
         auto input = std::make_shared<ov::op::v0::Parameter>(precision, input_shape);
         auto axes_node = ov::op::v0::Constant::create(ov::element::i64, {axes.size()}, axes);
-        auto reduce = std::make_shared<ov::op::v1::ReduceMean>(input, axes_node, keep_dims);
+        auto reduce = std::make_shared<ReduceOp>(input, axes_node, keep_dims);
         auto result = std::make_shared<ov::op::v0::Result>(reduce);
-        function = std::make_shared<ov::Model>(ov::ResultVector{result}, ov::ParameterVector{input}, "ReduceMean");
+        function = std::make_shared<ov::Model>(ov::ResultVector{result}, ov::ParameterVector{input}, "Reduce");
     }
 };
 
-TEST_P(ReduceMeanTest, Inference) {
-    run();
-}
+using ReduceMeanTest = ReduceTest<ov::op::v1::ReduceMean>;
+using ReduceMaxTest = ReduceTest<ov::op::v1::ReduceMax>;
+using ReduceMinTest = ReduceTest<ov::op::v1::ReduceMin>;
+using ReduceProdTest = ReduceTest<ov::op::v1::ReduceProd>;
+using ReduceSumTest = ReduceTest<ov::op::v1::ReduceSum>;
 
-INSTANTIATE_TEST_SUITE_P(smoke_ReduceMeanTest,
-                         ReduceMeanTest,
-                         ::testing::Combine(::testing::Values(ov::Shape{1, 24, 1024, 64}),
-                                            ::testing::Values(ov::element::f32),
-                                            ::testing::Values(std::vector<int64_t>{3}),
-                                            ::testing::Values(true)),
-                         ReduceMeanTest::getTestCaseName);
+TEST_P(ReduceMeanTest, Inference) { run(); }
+TEST_P(ReduceMaxTest, Inference) { run(); }
+TEST_P(ReduceMinTest, Inference) { run(); }
+TEST_P(ReduceProdTest, Inference) { run(); }
+TEST_P(ReduceSumTest, Inference) { run(); }
+
+const auto reduce_test_params = ::testing::Combine(::testing::Values(ov::Shape{1, 24, 1024, 64}),
+                                                   ::testing::Values(ov::element::f32),
+                                                   ::testing::Values(std::vector<int64_t>{3}),
+                                                   ::testing::Values(true));
+// ::testing::Values(true, false));
+
+INSTANTIATE_TEST_SUITE_P(smoke_ReduceMeanTest, ReduceMeanTest, reduce_test_params, ReduceMeanTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_ReduceMaxTest, ReduceMaxTest, reduce_test_params, ReduceMaxTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_ReduceMinTest, ReduceMinTest, reduce_test_params, ReduceMinTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_ReduceProdTest, ReduceProdTest, reduce_test_params, ReduceProdTest::getTestCaseName);
+INSTANTIATE_TEST_SUITE_P(smoke_ReduceSumTest, ReduceSumTest, reduce_test_params, ReduceSumTest::getTestCaseName);
 
 }  // namespace
