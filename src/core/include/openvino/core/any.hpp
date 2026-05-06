5f9108ea9210638da76abb88e5b5fb46c983bcfe
Mikołaj Roszczyk <mikolaj.roszczyk@intel.com>
2026-05-06 09:46:07 +0000

    clang format fix

--- src/core/include/openvino/core/any.hpp
+++ src/core/include/openvino/core/any.hpp
@@ -234,7 +234,8 @@ struct Read<
 
         while (c != '}') {
             std::string key, value;
-            if (is.peek() == '\'' || is.peek() == '"') {    // enabling keys with ":" sign
+            if (is.peek() == '\'' || is.peek() == '"') {
+                // enabling keys with ":" sign
                 char separator = is.peek();
                 is >> c;
                 std::getline(is, key, separator);
