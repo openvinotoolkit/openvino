adc13749e92e6dccff8aa921a0999fb8555d7db3
Kasprzak, Jakub <jakub.kasprzak@intel.com>
2026-05-07 15:51:30 +0000

    Change ze_resource to ze_owner

--- src/plugins/intel_gpu/src/runtime/ze/ze_holder.hpp
+++ src/plugins/intel_gpu/src/runtime/ze/ze_holder.hpp
@@ -175,46 +175,41 @@ struct ze_resource_info<ze_resource_type::usm_memory> {
 };
 
 template <ze_resource_type resource_type>
-struct ze_resource {
+struct ze_owner {
 public:
     using handle_t = typename ze_resource_info<resource_type>::handle_t;
     using deleter_t = typename ze_resource_info<resource_type>::deleter_t;
 
-    explicit ze_resource(handle_t handle, bool take_ownership = true) : _handle(handle), _is_owner(take_ownership) {
-        OPENVINO_ASSERT(_handle != nullptr, "[GPU] Can not create ze_resource with nullptr handle");
+    explicit ze_owner(handle_t handle) : _handle(handle) {
+        OPENVINO_ASSERT(_handle != nullptr, "[GPU] Can not create ze_owner with nullptr handle");
     }
-    ze_resource(const ze_resource<resource_type>& other) = delete;
-    ze_resource& operator=(const ze_resource<resource_type>& other) = delete;
-    ~ze_resource() {
-        if (_is_owner) {
-            deleter_t{}(_handle);
-        }
+    ze_owner(const ze_owner<resource_type>& other) = delete;
+    ze_owner& operator=(const ze_owner<resource_type>& other) = delete;
+    ~ze_owner() {
+        deleter_t{}(_handle);
     }
     handle_t get_handle() const {
         return _handle;
     }
 private:
     const handle_t _handle;
-    const bool _is_owner;
 };
 
 template <>
-struct ze_resource<ze_resource_type::usm_memory> {
+struct ze_owner<ze_resource_type::usm_memory> {
 public:
     static constexpr ze_resource_type resource_type = ze_resource_type::usm_memory;
     using handle_t = typename ze_resource_info<ze_resource_type::usm_memory>::handle_t;
     using deleter_t = typename ze_resource_info<ze_resource_type::usm_memory>::deleter_t;
 
-    explicit ze_resource(ze_context_handle_t context, handle_t handle, bool take_ownership = true) : _context(context), _handle(handle), _is_owner(take_ownership) {
-        OPENVINO_ASSERT(_context != nullptr, "[GPU] Can not create ze_resource with nullptr context");
-        OPENVINO_ASSERT(_handle != nullptr, "[GPU] Can not create ze_resource with nullptr handle");
+    explicit ze_owner(ze_context_handle_t context, handle_t handle) : _context(context), _handle(handle) {
+        OPENVINO_ASSERT(_context != nullptr, "[GPU] Can not create ze_owner with nullptr context");
+        OPENVINO_ASSERT(_handle != nullptr, "[GPU] Can not create ze_owner with nullptr handle");
     }
-    ze_resource(const ze_resource<resource_type>& other) = delete;
-    ze_resource& operator=(const ze_resource<resource_type>& other) = delete;
-    ~ze_resource() {
-        if (_is_owner) {
-            deleter_t{}(_context, _handle);
-        }
+    ze_owner(const ze_owner<resource_type>& other) = delete;
+    ze_owner& operator=(const ze_owner<resource_type>& other) = delete;
+    ~ze_owner() {
+        deleter_t{}(_context, _handle);
     }
     handle_t get_handle() const {
         return _handle;
@@ -222,7 +217,6 @@ public:
 private:
     const ze_context_handle_t _context;
     const handle_t _handle;
-    const bool _is_owner;
 };
 
 template <ze_resource_type _resource_type>
@@ -238,15 +232,15 @@ public:
     ze_holder(ze_holder&&) = default;
     ze_holder& operator=(const ze_holder&) = default;
     ze_holder& operator=(ze_holder&&) = default;
-    explicit ze_holder(handle_t handle, parent_holder_t parent, bool take_ownership = true) {
+    explicit ze_holder(handle_t handle, parent_holder_t parent) {
         OPENVINO_ASSERT(!parent.is_empty(), "[GPU] Parent holder can not be empty when creating holder");
         // No need to check handle as resource ctor will throw for nullptr
-        std::shared_ptr<ze_resource<resource_type>> resource;
+        std::shared_ptr<ze_owner<resource_type>> resource;
         if constexpr (resource_type == ze_resource_type::usm_memory) {
             // USM memory requires context (parent) for destruction, so we need to pass it to the resource
-            resource = std::make_shared<ze_resource<resource_type>>(parent.get_handle(), handle, take_ownership);
+            resource = std::make_shared<ze_owner<resource_type>>(parent.get_handle(), handle);
         } else {
-            resource = std::make_shared<ze_resource<resource_type>>(handle, take_ownership);
+            resource = std::make_shared<ze_owner<resource_type>>(handle);
         }
         _holder = std::move(resource);
         _parent = std::move(parent);
@@ -274,7 +268,7 @@ public:
         _parent.drop();
     }
 private:
-    std::shared_ptr<ze_resource<resource_type>> _holder;
+    std::shared_ptr<ze_owner<resource_type>> _holder;
     parent_holder_t _parent;
 };
 
@@ -289,8 +283,8 @@ public:
     ze_holder(ze_holder&&) = default;
     ze_holder& operator=(const ze_holder&) = default;
     ze_holder& operator=(ze_holder&&) = default;
-    explicit ze_holder(handle_t handle, bool take_ownership = true)
-        : _holder(std::make_shared<ze_resource<resource_type>>(handle, take_ownership)) {}
+    explicit ze_holder(handle_t handle)
+        : _holder(std::make_shared<ze_owner<resource_type>>(handle)) {}
     ~ze_holder() {
         drop();
     }
@@ -308,7 +302,7 @@ public:
         _holder.reset();
     }
 private:
-    std::shared_ptr<ze_resource<resource_type>> _holder;
+    std::shared_ptr<ze_owner<resource_type>> _holder;
 };
 
 using ze_holder_variant = std::variant<
