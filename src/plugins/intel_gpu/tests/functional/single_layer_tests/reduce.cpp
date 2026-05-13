c83e1b59a2ae3161f57a7bf44a8586b35fdaa261
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2026-05-13 15:29:12 +0200

    Moved reduction tests to mlir_op (#128)

--- src/plugins/intel_gpu/tests/functional/single_layer_tests/reduce.cpp
+++ src/plugins/intel_gpu/tests/functional/single_layer_tests/reduce.cpp
@@ -2,21 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "common_test_utils/file_utils.hpp"
 #include "common_test_utils/ov_tensor_utils.hpp"
 #include "common_test_utils/file_utils.hpp"
 #include "shared_test_classes/base/ov_subgraph.hpp"
 #include "openvino/op/parameter.hpp"
 #include "openvino/op/constant.hpp"
-#include "openvino/op/reduce_max.hpp"
-#include "openvino/op/reduce_mean.hpp"
-#include "openvino/op/reduce_min.hpp"
-#include "openvino/op/reduce_prod.hpp"
 #include "openvino/op/reduce_sum.hpp"
 #include "openvino/op/add.hpp"
 #include "openvino/op/multiply.hpp"
 #include "openvino/runtime/intel_gpu/properties.hpp"
-#include "shared_test_classes/base/ov_subgraph.hpp"
 
 namespace {
 
@@ -106,58 +100,4 @@ INSTANTIATE_TEST_SUITE_P(
                        ::testing::Values(false)),
     ReduceSumSqueezeTest::getTestCaseName);
 
-using ReduceParams = std::tuple<ov::Shape,             // Input shape
-                                ov::element::Type,     // Input precision
-                                std::vector<int64_t>,  // Reduce axes
-                                bool>;                 // Keep dims
-
-template <typename ReduceOp>
-class ReduceTest : public testing::WithParamInterface<ReduceParams>, virtual public ov::test::SubgraphBaseStaticTest {
-public:
-    static std::string getTestCaseName(const testing::TestParamInfo<ReduceParams>& obj) {
-        const auto& [input_shape, precision, axes, keep_dims] = obj.param;
-        std::ostringstream result;
-        result << "IS=" << ov::test::utils::vec2str(input_shape) << "_";
-        result << "axes=" << ov::test::utils::vec2str(axes) << "_";
-        result << "keep_dims=" << keep_dims << "_";
-        result << "precision=" << precision;
-        return result.str();
-    }
-
-protected:
-    void SetUp() override {
-        targetDevice = ov::test::utils::DEVICE_GPU;
-        const auto& [input_shape, precision, axes, keep_dims] = GetParam();
-        auto input = std::make_shared<ov::op::v0::Parameter>(precision, input_shape);
-        auto axes_node = ov::op::v0::Constant::create(ov::element::i64, {axes.size()}, axes);
-        auto reduce = std::make_shared<ReduceOp>(input, axes_node, keep_dims);
-        auto result = std::make_shared<ov::op::v0::Result>(reduce);
-        function = std::make_shared<ov::Model>(ov::ResultVector{result}, ov::ParameterVector{input}, "Reduce");
-    }
-};
-
-using ReduceMeanTest = ReduceTest<ov::op::v1::ReduceMean>;
-using ReduceMaxTest = ReduceTest<ov::op::v1::ReduceMax>;
-using ReduceMinTest = ReduceTest<ov::op::v1::ReduceMin>;
-using ReduceProdTest = ReduceTest<ov::op::v1::ReduceProd>;
-using ReduceSumTest = ReduceTest<ov::op::v1::ReduceSum>;
-
-TEST_P(ReduceMeanTest, Inference) { run(); }
-TEST_P(ReduceMaxTest, Inference) { run(); }
-TEST_P(ReduceMinTest, Inference) { run(); }
-TEST_P(ReduceProdTest, Inference) { run(); }
-TEST_P(ReduceSumTest, Inference) { run(); }
-
-const auto reduce_test_params = ::testing::Combine(::testing::Values(ov::Shape{1, 24, 1024, 64}),
-                                                   ::testing::Values(ov::element::f32),
-                                                   ::testing::Values(std::vector<int64_t>{3}),
-                                                   ::testing::Values(true));
-// ::testing::Values(true, false));
-
-INSTANTIATE_TEST_SUITE_P(smoke_ReduceMeanTest, ReduceMeanTest, reduce_test_params, ReduceMeanTest::getTestCaseName);
-INSTANTIATE_TEST_SUITE_P(smoke_ReduceMaxTest, ReduceMaxTest, reduce_test_params, ReduceMaxTest::getTestCaseName);
-INSTANTIATE_TEST_SUITE_P(smoke_ReduceMinTest, ReduceMinTest, reduce_test_params, ReduceMinTest::getTestCaseName);
-INSTANTIATE_TEST_SUITE_P(smoke_ReduceProdTest, ReduceProdTest, reduce_test_params, ReduceProdTest::getTestCaseName);
-INSTANTIATE_TEST_SUITE_P(smoke_ReduceSumTest, ReduceSumTest, reduce_test_params, ReduceSumTest::getTestCaseName);
-
-}  // namespace
+} // namespace
