3dc3ac8b1b96056091fae4d65272dcb8210f6328
Zhulin Egor <142201265+Quaant@users.noreply.github.com>
2026-05-13 20:12:52 +0300

    Update thirdparty/dependencies.cmake
    Co-authored-by: Arseniy Obolenskiy <gooddoog@student.su>

--- thirdparty/dependencies.cmake
+++ thirdparty/dependencies.cmake
@@ -37,7 +37,6 @@ if(ENABLE_LTO)
     set(CMAKE_INTERPROCEDURAL_OPTIMIZATION_RELEASE ON)
 endif()
 
-
 if(NOT ENABLE_PROFILING_ITT STREQUAL "OFF")
     find_package(ittapi QUIET)
     if(ittapi_FOUND)
