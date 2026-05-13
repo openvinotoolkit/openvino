ce41e0c11967ee7b603e22adf1ce740754fec783
Mostafa Faheem <mostafaaafaheem@gmail.com>
2026-05-13 15:36:07 +0300

    Avoid copies in NV12 shape inference

--- src/core/shape_inference/include/rgb_bgr_to_nv12_shape_inference.hpp
+++ src/core/shape_inference/include/rgb_bgr_to_nv12_shape_inference.hpp
@@ -8,8 +8,7 @@
 #include "openvino/op/util/convert_color_to_nv12_base.hpp"
 #include "utils.hpp"
 
-namespace ov {
-namespace op {
+namespace ov::op {
 template <class TShape, class TRShape = result_shape_t<TShape>>
 std::vector<TRShape> shape_infer(const util::ConvertColorToNV12Base* op, const std::vector<TShape>& input_shapes) {
     NODE_VALIDATION_CHECK(op, input_shapes.size() == 1, "RGBtoNV12/BGRtoNV12 shall have exactly one input");
@@ -22,12 +21,10 @@ std::vector<TRShape> shape_infer(const util::ConvertColorToNV12Base* op, const s
 
     if (rank.is_dynamic()) {
         if (op->is_single_plane()) {
-            auto out_shape = TRShape{ov::Dimension::dynamic(), ov::Dimension::dynamic(), ov::Dimension::dynamic(), 1};
-            return {out_shape};
+            return {TRShape{ov::Dimension::dynamic(), ov::Dimension::dynamic(), ov::Dimension::dynamic(), 1}}; // Y + UV planes
         } else {
-            auto y_shape = TRShape{ov::Dimension::dynamic(), ov::Dimension::dynamic(), ov::Dimension::dynamic(), 1};
-            auto uv_shape = TRShape{ov::Dimension::dynamic(), ov::Dimension::dynamic(), ov::Dimension::dynamic(), 2};
-            return {y_shape, uv_shape};
+            return {TRShape{ov::Dimension::dynamic(), ov::Dimension::dynamic(), ov::Dimension::dynamic(), 1},  // Y plane
+                    TRShape{ov::Dimension::dynamic(), ov::Dimension::dynamic(), ov::Dimension::dynamic(), 2}}; // UV plane
         }
     }
 
@@ -40,22 +37,12 @@ std::vector<TRShape> shape_infer(const util::ConvertColorToNV12Base* op, const s
     NODE_SHAPE_INFER_CHECK(op, input_shapes, dim::is_divisible(shape_rgb[2], 2), "Image width must be even");
 
     if (op->is_single_plane()) {
-        auto out_shape = TRShape{shape_rgb[0], shape_rgb[1], shape_rgb[2], 1};
-        out_shape[1] *= 3;
-        out_shape[1] /= 2;
-
-        NODE_SHAPE_INFER_CHECK(op, input_shapes, !dim::is_empty(out_shape[1]), "Image height computation failed");
-
-        return {out_shape};
+        auto height = shape_rgb[1] * 3 / 2;
+        NODE_SHAPE_INFER_CHECK(op, input_shapes, !dim::is_empty(height), "Image height computation failed");
+        return {TRShape{shape_rgb[0], std::move(height), shape_rgb[2], 1}};
     } else {
-        auto y_shape = TRShape{shape_rgb[0], shape_rgb[1], shape_rgb[2], 1};
-        auto uv_shape = TRShape{shape_rgb[0], shape_rgb[1], shape_rgb[2], 2};
-
-        uv_shape[1] /= 2;
-        uv_shape[2] /= 2;
-
-        return {y_shape, uv_shape};
+        return {TRShape{shape_rgb[0], shape_rgb[1], shape_rgb[2], 1},  // Y plane
+                TRShape{shape_rgb[0], shape_rgb[1] / 2, shape_rgb[2] / 2, 2}}; // UV plane
     }
 }
-}  // namespace op
-}  // namespace ov
+}  // namespace ov::op
