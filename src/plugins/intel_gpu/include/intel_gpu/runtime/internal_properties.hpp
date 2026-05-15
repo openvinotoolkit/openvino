76a83c4124d8e329f82f086f8c561999f0b8ab8e
Mikołaj Roszczyk <mikolaj.roszczyk@intel.com>
2026-05-15 08:53:53 +0000

    moved Read<ImplForcingMap> to seperate file

--- src/plugins/intel_gpu/include/intel_gpu/runtime/internal_properties.hpp
+++ src/plugins/intel_gpu/include/intel_gpu/runtime/internal_properties.hpp
@@ -188,61 +188,6 @@ static constexpr Property<bool, ov::PropertyMutability::RW> list_layers{"GPU_LIS
 static constexpr Property<bool, ov::PropertyMutability::RW> print_input_data_shapes{"GPU_PRINT_INPUT_DATA_SHAPES"};
 }  // namespace ov::intel_gpu
 
-namespace ov::util {
-
-// specific parser for ImplForcingMap supporting quotation marks interpretation. 
-// it allows the keys to contain colons (:) which should not be treated as separators
-// to be removed when the support for quotation marks is implemented in generic ov::Any
-template <>
-struct Read<ov::intel_gpu::ImplForcingMap> {
-    void operator()(std::istream& is, ov::intel_gpu::ImplForcingMap& map) const {
-        char c;
-
-        is >> c;
-        OPENVINO_ASSERT(c == '{',
-                        "Failed to parse ov::intel_gpu::ImplForcingMap. Starting symbol is not '{', it's ", c);
-
-        while (c != '}') {
-            std::string key, value;
-            if (is.peek() == '\'' || is.peek() == '"') {
-                is >> c;
-                const char separator = c;
-                std::getline(is, key, separator);
-                OPENVINO_ASSERT(is.get() == ':',
-                                "Parsing error: Separator (:) needed after key name. format: {" +
-                                    std::string(1, separator) + "key" + std::string(1, separator) + ":value}");
-            } else {
-                std::getline(is, key, ':');
-            }
-
-            size_t enclosed_container_level = 0;
-
-            while (is.good()) {
-                is >> c;
-                if (c == ',') {
-                    if (enclosed_container_level == 0)
-                        break;
-                }
-                if (c == '{' || c == '[')
-                    ++enclosed_container_level;
-                if (c == '}' || c == ']') {
-                    if (enclosed_container_level == 0)
-                        break;
-                    --enclosed_container_level;
-                }
-
-                value += c;
-            }
-            map.emplace(from_string<cldnn::primitive_id>(key), from_string<ov::intel_gpu::ImplementationDesc>(value));
-        }
-
-        OPENVINO_ASSERT(c == '}',
-                        "Failed to parse ov::intel_gpu::ImplForcingMap. Ending symbol is not '}', it's ", c);
-    }
-};
-
-}  // namespace ov::util
-
 namespace cldnn {
 using ov::intel_gpu::QueueTypes;
 }  // namespace cldnn
