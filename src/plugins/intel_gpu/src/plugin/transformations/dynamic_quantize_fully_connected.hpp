7fd595103c72635d34ce3adb3d2bdf38f04437d9
Andrew Park <andrew.park@intel.com>
2026-05-18 13:50:10 +0900

    Clarify INT8 dyn-quant vs int4 weight quantization in hybrid linear-attn WA
    Signed-off-by: Andrew Park <andrew.park@intel.com>

--- src/plugins/intel_gpu/src/plugin/transformations/dynamic_quantize_fully_connected.hpp
+++ src/plugins/intel_gpu/src/plugin/transformations/dynamic_quantize_fully_connected.hpp
@@ -4,8 +4,6 @@
 
 #pragma once
 
-#include <string>
-
 #include "openvino/pass/graph_rewrite.hpp"
 
 namespace ov::intel_gpu {
@@ -16,11 +14,6 @@ public:
     DynamicQuantizeFullyConnected(uint64_t group_size, bool asymmetric = false, bool precompute_sum = true, bool use_gs128_for_int8_per_token = false);
     static bool ShouldUseGs128(uint64_t is_wei_i8u8, bool use_gs128_for_int8_per_token, uint64_t group_size) {
         return (is_wei_i8u8 && use_gs128_for_int8_per_token && group_size == UINT64_MAX);
-    }
-    // WA: Mamba2 linear_attn.out_proj input has a wide dynamic range and is unstable
-    // under per-token INT8 dyn-quant; force gs=128 for this FC only.
-    static bool ShouldUseGs128ForLinearAttnOutProj(const std::string& friendly_name, uint64_t group_size) {
-        return group_size == UINT64_MAX && friendly_name.find("linear_attn.out_proj") != std::string::npos;
     }
 };
 
