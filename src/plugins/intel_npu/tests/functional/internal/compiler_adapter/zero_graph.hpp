c4e5faa1ea7eaedeee8f47b66dee006bfa0e4b83
MirceaDan99 <mircea-aurelian.dan@intel.com>
2026-05-25 13:27:46 +0000

    Refactor test to strictly check `getGraphDescriptor` method with `secureCompile` flag set

--- src/plugins/intel_npu/tests/functional/internal/compiler_adapter/zero_graph.hpp
+++ src/plugins/intel_npu/tests/functional/internal/compiler_adapter/zero_graph.hpp
@@ -409,41 +409,17 @@ TEST_P(IsOptionSupported, PropertySupportedByDriver) {
 using EncryptionCallbacks = ZeroGraphTest;
 
 TEST_P(EncryptionCallbacks, EncryptionCallbacksSetSecureCompileFlag) {
-    model = ov::test::utils::make_conv_pool_relu({1, 3, 227, 227}, ov::element::f32);
-
-    auto options = std::make_shared<OptionsDesc>();
-    options->add<COMPILER_TYPE>();
-    options->add<CACHE_ENCRYPTION_CALLBACKS>();
-    options->add<MODEL_SERIALIZER_VERSION>();
-    auto npu_config = std::make_unique<FilteredConfig>(options);
-    npu_config->enable(MODEL_SERIALIZER_VERSION::key().data(), true);
-    for (const auto& [propertyName, propertyValue] : configuration) {
-        npu_config->enable(propertyName, true);
-    }
-    npu_config->updateAny(configuration);
-
-    std::shared_ptr<ICompilerAdapter> compiler;
-    compiler =
-        npu_config->get<COMPILER_TYPE>() == intel_npu::CompilerType::DRIVER
-            ? std::dynamic_pointer_cast<ICompilerAdapter>(std::make_shared<DriverCompilerAdapter>(zeroInitStruct))
-            : std::dynamic_pointer_cast<ICompilerAdapter>(std::make_shared<PluginCompilerAdapter>(zeroInitStruct));
-
-    // WA for error `[NPU_VCL] Unsupported IR API version! Val: 48.0`
-    if (compiler->is_option_supported(MODEL_SERIALIZER_VERSION::key().data())) {
-        npu_config->update(
-            {{MODEL_SERIALIZER_VERSION::key().data(),
-              MODEL_SERIALIZER_VERSION::toString(ov::intel_npu::ModelSerializerVersion::ALL_WEIGHTS_COPY)}});
-    } else {
-        npu_config->enable(MODEL_SERIALIZER_VERSION::key().data(), false);
-    }
-
+    auto localZeGraphExt = std::make_shared<ZeGraphExtWrappers>(zeroInitStruct);
+    serializeIR();
     if (zeroInitStruct->getGraphDdiTable().version() < ZE_MAKE_VERSION(1, 17)) {
-        OV_EXPECT_THROW(compiler->compile(model, *npu_config),
-                        ov::Exception,
-                        testing::HasSubstr(
-                            "Secure compilation was requested, but the current driver version does not support it."));
+        OV_EXPECT_THROW(
+            localZeGraphExt->getGraphDescriptor(serializedIR, "", bypassUmdCache(), /* secureCompile = */ true),
+            ov::Exception,
+            testing::HasSubstr(
+                "Secure compilation was requested, but the current driver version does not support it."));
     } else {
-        OV_ASSERT_NO_THROW(compiler->compile(model, *npu_config));
+        OV_ASSERT_NO_THROW(
+            localZeGraphExt->getGraphDescriptor(serializedIR, "", bypassUmdCache(), /* secureCompile = */ true));
     }
 }
 
