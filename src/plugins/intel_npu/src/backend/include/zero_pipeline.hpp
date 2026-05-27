73f04f4d02c1d746e5ba130800af53226e5b402b
Liu, Dan1 <dan1.liu@intel.com>
2026-05-28 01:16:12 +0800

    clean IDynamicGraph

--- src/plugins/intel_npu/src/backend/include/zero_pipeline.hpp
+++ src/plugins/intel_npu/src/backend/include/zero_pipeline.hpp
@@ -4,7 +4,7 @@
 
 #pragma once
 
-#include "intel_npu/common/idynamic_graph.hpp"
+#include "intel_npu/common/graph_arguments.hpp"
 #include "intel_npu/common/igraph.hpp"
 #include "intel_npu/utils/zero/zero_tensor.hpp"
 #include "intel_npu/utils/zero/zero_wrappers.hpp"
@@ -36,11 +36,6 @@ public:
                                         const std::shared_ptr<ZeroTensor>& tensor,
                                         size_t batch_index,
                                         const std::shared_ptr<ov::ITensor>& userTensor = nullptr) = 0;
-
-    /// Run VM-runtime output shape prediction for dynamic pipelines.
-    /// Default no-op so static pipelines need not implement it.
-    virtual void predict_output_shape(std::vector<IDynamicGraph::MemRefType>& /*inputs*/,
-                                      std::vector<IDynamicGraph::MemRefType>& /*outputs*/) {}
 
     std::vector<ov::ProfilingInfo> get_profiling_info() const;
 
