3febebc76b267eefa3e836f74b7491828b984402
Michal Barnas <michal.barnas@intel.com>
2026-05-28 11:17:06 +0000

    use ov util to file size

--- src/core/xml_util/src/weights_provider.cpp
+++ src/core/xml_util/src/weights_provider.cpp
@@ -9,6 +9,7 @@
 #include "openvino/runtime/aligned_buffer.hpp"
 #include "openvino/runtime/shared_buffer.hpp"
 #include "openvino/util/common_util.hpp"
+#include "openvino/util/file_util.hpp"
 
 namespace ov::util {
 
@@ -50,12 +51,11 @@ size_t BufferWeightsProvider::size() const {
 
 FileWeightsProvider::FileWeightsProvider(std::filesystem::path weights_path)
     : m_weights_path(std::move(weights_path)),
+      m_weights_size(ov::util::file_size(m_weights_path)),
       m_weights_source_id(std::hash<std::decay_t<decltype(m_weights_path.native())>>{}(m_weights_path.native())),
       m_weights_source_handle(std::make_shared<ov::AlignedBuffer>()) {
     std::ifstream weights_stream(m_weights_path, std::ios::binary);
     OPENVINO_ASSERT(weights_stream.is_open(), "Weights file ", m_weights_path, " cannot be opened!");
-
-    m_weights_size = static_cast<size_t>(std::filesystem::file_size(m_weights_path));
 }
 
 std::shared_ptr<ov::AlignedBuffer> FileWeightsProvider::load_region(size_t offset, size_t size) {
