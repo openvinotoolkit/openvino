16207e3b64136e60354b0ce6789bd4b54fe08cae
Mikolaj Roszczyk <mikolaj.roszczyk@intel.com>
2026-06-02 14:29:58 +0200

    [GPU][Level Zero] Names unification L0->ZE (#36067)
    ### Details:
    - Level Zero is sometimes abbreviated to ```L0``` and sometimes to
    ```ZE```
     - Unification of Level Zero functions to using abbreviation ```ZE```
    
    ### AI Assistance:
     - AI assistance used: yes
    - for finding all the affected places where GPU_RT_TYPE needs to be
    changed

--- src/plugins/intel_gpu/cmake/utils.cmake
+++ src/plugins/intel_gpu/cmake/utils.cmake
@@ -3,7 +3,7 @@
 #
 
 function(ov_gpu_set_runtime_interface_for TARGET_NAME)
-    if(GPU_RT_TYPE STREQUAL "L0")
+    if(GPU_RT_TYPE STREQUAL "ZE")
         target_compile_definitions(${TARGET_NAME} PRIVATE OV_GPU_WITH_ZE_RT=1)
         target_link_libraries(${TARGET_NAME} PRIVATE openvino::zero_loader)
     elseif(GPU_RT_TYPE STREQUAL "OCL")
@@ -12,6 +12,6 @@ function(ov_gpu_set_runtime_interface_for TARGET_NAME)
     elseif(GPU_RT_TYPE STREQUAL "SYCL")
         target_compile_definitions(${TARGET_NAME} PRIVATE OV_GPU_WITH_SYCL_RT=1)
     else()
-        message(FATAL_ERROR "Invalid GPU runtime type: `${GPU_RT_TYPE}` Only `L0`, `OCL` and `SYCL` are supported")
+        message(FATAL_ERROR "Invalid GPU runtime type: `${GPU_RT_TYPE}` Only `ZE`, `OCL` and `SYCL` are supported")
     endif()
 endfunction()
