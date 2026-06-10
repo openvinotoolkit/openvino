d5af89a632a6892d79b142b157cdac7d7a104e78
jvincent2021 <jaison.vincentsolomon@intel.com>
2026-06-10 13:49:15 +0530

    Update test_benchmark_app.py to fix the copyright header issue

--- tests/samples_tests/smoke_tests/test_benchmark_app.py
+++ tests/samples_tests/smoke_tests/test_benchmark_app.py
@@ -1,3 +1,5 @@
+# Copyright (C) 2018-2026 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
 """
  Copyright (C) 2018-2026 Intel Corporation
  Licensed under the Apache License, Version 2.0 (the "License");
