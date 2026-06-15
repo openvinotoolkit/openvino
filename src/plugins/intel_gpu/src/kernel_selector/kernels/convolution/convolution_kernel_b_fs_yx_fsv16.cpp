ba89b013da5c1de03abdb8998021d9a067cef118
Mikołaj Roszczyk <mikolaj.roszczyk@intel.com>
2026-06-15 08:33:24 +0000

    revert changes for ze

--- src/plugins/intel_gpu/src/kernel_selector/kernels/convolution/convolution_kernel_b_fs_yx_fsv16.cpp
+++ src/plugins/intel_gpu/src/kernel_selector/kernels/convolution/convolution_kernel_b_fs_yx_fsv16.cpp
@@ -22,12 +22,7 @@ bool post_reorder_fused(const convolution_params& params) {
 }
 }  // namespace
 
-ConvolutionKernel_b_fs_yx_fsv16::ConvolutionKernel_b_fs_yx_fsv16()
-#ifdef OV_GPU_WITH_ZE_RT
-    : ConvolutionKernelBase("convolution_gpu_bfyx_f16_ze") {
-#else
-    : ConvolutionKernelBase("convolution_gpu_bfyx_f16") {
-#endif
+ConvolutionKernel_b_fs_yx_fsv16::ConvolutionKernel_b_fs_yx_fsv16() : ConvolutionKernelBase("convolution_gpu_bfyx_f16") {
     std::vector<size_t> outputBlockWidths = { 2, 4, 8 };
     std::vector<std::string> executionModes = ConvolutionKernelBase::autoTuneOptions;
 
