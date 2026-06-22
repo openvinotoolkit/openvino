17ff6b011925dea85931ceee6a2916b356401fd7
Raasz, Pawel <pawel.raasz@intel.com>
2026-06-22 08:42:28 +0000

    Loop check if can evaluate body
    Signed-off-by: Raasz, Pawel <pawel.raasz@intel.com>

--- src/core/src/op/loop.cpp
+++ src/core/src/op/loop.cpp
@@ -349,29 +349,32 @@ Output<Node> Loop::get_concatenated_slices(const Output<Node>& value,
 
 bool Loop::evaluate(TensorVector& outputs, const TensorVector& inputs) const {
     OV_OP_SCOPE(v5_Loop_evaluate);
-    EvaluationContext evaluation_context;
-    reference::loop(m_bodies[0],
-                    m_output_descriptions[0],
-                    m_input_descriptions[0],
-                    m_special_body_ports,
-                    outputs,
-                    inputs,
-                    evaluation_context);
-    return true;
+    return evaluate(outputs, inputs, {});
 }
 
 bool Loop::evaluate(TensorVector& outputs,
                     const TensorVector& inputs,
                     const EvaluationContext& evaluation_context) const {
     OV_OP_SCOPE(v5_Loop_evaluate);
-    reference::loop(m_bodies[0],
-                    m_output_descriptions[0],
-                    m_input_descriptions[0],
-                    m_special_body_ports,
-                    outputs,
-                    inputs,
-                    evaluation_context);
-    return true;
+    const auto can_evaluate = [](const auto& ops) -> bool {
+        return std::all_of(ops.begin(), ops.end(), [](const auto& node) {
+            return is_type<op::v0::Parameter>(node) || node->has_evaluate();
+        });
+    };
+
+    if (auto body = get_function(); body && can_evaluate(body->get_ops())) {
+        reference::loop(body,
+                        m_output_descriptions[0],
+                        m_input_descriptions[0],
+                        m_special_body_ports,
+                        outputs,
+                        inputs,
+                        evaluation_context);
+        return true;
+    } else {
+        // Loop body has node with no evaluate, return false instead throw.
+        return false;
+    }
 }
 
 bool Loop::has_evaluate() const {
