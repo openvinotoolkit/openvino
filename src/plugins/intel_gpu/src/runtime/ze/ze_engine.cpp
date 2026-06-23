0395b7f75e192a406b715b3856058b8ba8a1880c
Michal Miotk <michal.miotk@intel.com>
2026-06-23 20:30:47 +0000

    cleaning

--- src/plugins/intel_gpu/src/runtime/ze/ze_engine.cpp
+++ src/plugins/intel_gpu/src/runtime/ze/ze_engine.cpp
@@ -165,7 +165,7 @@ memory_ptr ze_engine::create_mmap_hostbuffer(const void* mmapped_address, size_t
     OPENVINO_NOT_IMPLEMENTED;
 }
 
-memory_ptr ze_engine::create_hostbuffer(const void* mmapped_address, size_t data_size, allocation_type _allocation_type, const layout output_layout) {
+memory_ptr ze_engine::create_hostbuffer(const void* cpu_address, size_t data_size, allocation_type _allocation_type, const layout output_layout) {
     OPENVINO_NOT_IMPLEMENTED;
 }
 
