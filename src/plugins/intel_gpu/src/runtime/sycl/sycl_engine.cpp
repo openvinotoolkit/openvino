f2e781229f32301ba304e1a2163b9dd1339e613f
Michal Miotk <michal.miotk@intel.com>
2026-06-25 10:17:37 +0000

    sycl stubs

--- src/plugins/intel_gpu/src/runtime/sycl/sycl_engine.cpp
+++ src/plugins/intel_gpu/src/runtime/sycl/sycl_engine.cpp
@@ -155,6 +155,10 @@ memory_ptr sycl_engine::create_mmap_hostbuffer(const void* mmapped_address, size
     OPENVINO_NOT_IMPLEMENTED;
 }
 
+memory_ptr sycl_engine::create_hostbuffer(const void* cpu_address, size_t data_size, allocation_type _allocation_type, const layout output_layout) {
+    OPENVINO_NOT_IMPLEMENTED;
+}
+
 memory::ptr sycl_engine::reinterpret_buffer(const memory& memory, const layout& new_layout) {
     OPENVINO_ASSERT(memory.get_engine() == this, "[GPU] trying to reinterpret buffer allocated by a different engine");
     OPENVINO_ASSERT(new_layout.format.is_image() == memory.get_layout().format.is_image(),
