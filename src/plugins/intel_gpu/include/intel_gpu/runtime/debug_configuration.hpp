babea35531f3b1e3e1901d3f6a40435651d02365
Damian Kurek <damian.kurek@intel.com>
2026-06-29 11:34:18 +0200

    Apply suggestion from @Lyamin-Roman
    Co-authored-by: Roman Lyamin <Roman.Lyamin@intel.com>

--- src/plugins/intel_gpu/include/intel_gpu/runtime/debug_configuration.hpp
+++ src/plugins/intel_gpu/include/intel_gpu/runtime/debug_configuration.hpp
@@ -99,5 +99,5 @@ static constexpr const char* prefix = "GPU_Debug: ";
 #define GPU_DEBUG_COUT              GPU_DEBUG_LOG_RAW(ov::intel_gpu::LogLevel::DISABLED, ov::log::Level::NO)
 #define GPU_DEBUG_INFO              GPU_DEBUG_LOG_RAW(ov::intel_gpu::LogLevel::INFO, ov::log::Level::INFO)
 #define GPU_DEBUG_LOG               GPU_DEBUG_LOG_RAW(ov::intel_gpu::LogLevel::LOG, ov::log::Level::INFO)
-#define GPU_DEBUG_TRACE             GPU_DEBUG_LOG_RAW(ov::intel_gpu::LogLevel::TRACE, ov::log::Level::TRACE)
-#define GPU_DEBUG_TRACE_DETAIL      GPU_DEBUG_LOG_RAW(ov::intel_gpu::LogLevel::TRACE_DETAIL, ov::log::Level::DEBUG)
+#define GPU_DEBUG_TRACE             GPU_DEBUG_LOG_RAW(ov::intel_gpu::LogLevel::TRACE, ov::log::Level::DEBUG)
+#define GPU_DEBUG_TRACE_DETAIL      GPU_DEBUG_LOG_RAW(ov::intel_gpu::LogLevel::TRACE_DETAIL, ov::log::Level::TRACE)
