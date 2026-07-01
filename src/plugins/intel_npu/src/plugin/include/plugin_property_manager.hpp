fb94ead8b2279f1b3f940c46897a3ade9e31497e
Bogdan Pereanu <bogdan.pereanu@intel.com>
2026-07-01 15:44:02 +0100

    Remove metrics and move helpers to device_helpers
    Signed-off-by: Bogdan Pereanu <bogdan.pereanu@intel.com>

--- src/plugins/intel_npu/src/plugin/include/plugin_property_manager.hpp
+++ src/plugins/intel_npu/src/plugin/include/plugin_property_manager.hpp
@@ -8,6 +8,7 @@
 #include <memory>
 #include <mutex>
 #include <string>
+#include <tuple>
 #include <vector>
 
 #include "compiler_option_support_helper.hpp"
@@ -50,7 +51,6 @@ private:
         FilteredConfig config;
         ov::SoPtr<IEngineBackend> backend;
         std::shared_ptr<CompilerOptionSupportHelper> optionSupportHelper;
-        std::shared_ptr<Metrics> metrics;
         Logger& logger;
         ov::intel_npu::CompilerType currentlyUsedCompiler;
         bool compatibilityCheckSupported;
@@ -69,7 +69,6 @@ private:
 
     ov::SoPtr<IEngineBackend> _backend;
     std::shared_ptr<CompilerOptionSupportHelper> _compilerOptionSupportHelper;
-    std::shared_ptr<Metrics> _metrics = nullptr;
     Logger& _logger;
 
     ov::intel_npu::CompilerType _currentlyUsedCompiler = ov::intel_npu::CompilerType::PREFER_PLUGIN;
@@ -80,7 +79,7 @@ private:
 
     std::map<std::string, PropertyDescriptor> _properties;
 
-    const std::vector<ov::PropertyName> _cachingProperties = [] {
+    inline static const std::vector<ov::PropertyName> _cachingProperties = [] {
         std::vector<ov::PropertyName> properties = {
             ov::cache_mode.name(),
             ov::enable_profiling.name(),
@@ -112,9 +111,22 @@ private:
         return properties;
     }();
 
-    const std::vector<ov::PropertyName> _internalSupportedProperties = {ov::internal::caching_properties.name(),
-                                                                        ov::internal::caching_with_mmap.name(),
-                                                                        ov::internal::cache_header_alignment.name()};
+    inline static const std::vector<ov::PropertyName> _internalSupportedProperties = {
+        ov::internal::caching_properties.name(),
+        ov::internal::caching_with_mmap.name(),
+        ov::internal::cache_header_alignment.name()};
+
+    static constexpr uint32_t _maxNumOfOptimalInferRequests = 8u;
+    inline static const std::vector<std::string> _optimizationCapabilities = {
+        ov::device::capability::FP16,
+        ov::device::capability::INT8,
+        ov::device::capability::EXPORT_IMPORT,
+    };
+    inline static const std::tuple<uint32_t, uint32_t, uint32_t> _rangeForAsyncInferRequests{
+        1u,
+        _maxNumOfOptimalInferRequests,
+        1u};
+    inline static const std::tuple<uint32_t, uint32_t> _rangeForStreams{0u, _maxNumOfOptimalInferRequests};
 
     mutable std::mutex _mutex;
 };
