dc0d5d536c0379d5ba3dc1dfe4372018d11a1967
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2026-07-03 11:41:33 +0200

    using ShapeOfBase and GatherBase

--- src/common/transformations/src/transformations/paged_attention/attention_mask_shape_replacer.cpp
+++ src/common/transformations/src/transformations/paged_attention/attention_mask_shape_replacer.cpp
@@ -8,9 +8,10 @@
 #include "openvino/core/rt_info.hpp"
 #include "openvino/core/validation_util.hpp"
 #include "openvino/op/constant.hpp"
-#include "openvino/op/gather.hpp"
 #include "openvino/op/parameter.hpp"
 #include "openvino/op/shape_of.hpp"
+#include "openvino/op/util/gather_base.hpp"
+#include "openvino/op/util/shape_of_base.hpp"
 #include "openvino/pass/pattern/op/pattern.hpp"
 #include "openvino/pass/pattern/op/wrap_type.hpp"
 
@@ -20,7 +21,6 @@ using ov::pass::pattern::wrap_type;
 
 namespace v0 = ov::op::v0;
 namespace v3 = ov::op::v3;
-namespace v8 = ov::op::v8;
 
 ov::pass::AttentionMaskShapeReplacer::AttentionMaskShapeReplacer(const Output<Node>& input_source) {
     MATCHER_SCOPE(AttentionMaskShapeReplacer);
@@ -28,8 +28,8 @@ ov::pass::AttentionMaskShapeReplacer::AttentionMaskShapeReplacer(const Output<No
     auto attn_mask = wrap_type<v0::Parameter>(has_static_rank() && [](const Output<Node>& output) {
         return output.get_names().count("attention_mask") > 0;
     });
-    auto shape_of = wrap_type<v3::ShapeOf>({attn_mask});
-    auto gather = wrap_type<v8::Gather>({shape_of, any_input(), any_input()});
+    auto shape_of = wrap_type<ov::op::util::ShapeOfBase>({attn_mask});
+    auto gather = wrap_type<ov::op::util::GatherBase>({shape_of, any_input(), any_input()});
 
     ov::matcher_pass_callback callback = [=](ov::pass::pattern::Matcher& m) {
         const auto& pattern_map = m.get_pattern_value_map();
