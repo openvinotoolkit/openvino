da3ddf182a4337ca481d3bbb7806d5c291de9f11
Andrei Anufriev <andrey.anufriev@intel.com>
2026-07-06 18:27:00 +0200

    Fixed code style.

--- src/bindings/python/src/openvino/frontend/pytorch/ts_decoder.py
+++ src/bindings/python/src/openvino/frontend/pytorch/ts_decoder.py
@@ -69,8 +69,11 @@ class TorchScriptPythonDecoder(Decoder):
                 elif hasattr(pt_module.config, "to_dict"):
                     self.config = pt_module.config.to_dict()
                 # remove nncf quantization config from model to avoid issues with lm_eval
-                if self.config and self.config.get("quantization_config", None) and \
-                   self.config["quantization_config"].get("quant_method", None) == "nncf":
+                if (
+                    self.config
+                    and self.config.get("quantization_config", None)
+                    and self.config["quantization_config"].get("quant_method", None) == "nncf"
+                ):
                     self.config["quantization_config"] = {}  # type: ignore
             try:
                 pt_module = self._get_scripted_model(
