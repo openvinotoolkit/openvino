7f42feffb2861b6c77866bac4ae0e95c415ce8dd
alexandruenache1111 <alexandru.enache@intel.com>
2026-07-17 07:06:36 +0100

    Fix failing test

--- src/plugins/intel_npu/tests/functional/behavior/compiled_model/property.hpp
+++ src/plugins/intel_npu/tests/functional/behavior/compiled_model/property.hpp
@@ -795,7 +795,7 @@ TEST_P(CheckCpuPinning, CheckCompileModelWithCpuPinningFromSetProperty) {
     ov::Core core;
     ov::CompiledModel compiled_model;
 
-    ov::test::utils::LoggerLevelGuard levelGuard(ov::log::Level::WARNING);
+    core.set_property(deviceName, ov::log::level(ov::log::Level::WARNING));
 
     // Keep this std::function alive while logging is active.
     std::function<void(std::string_view)> log_cb = [&](std::string_view msg) {
