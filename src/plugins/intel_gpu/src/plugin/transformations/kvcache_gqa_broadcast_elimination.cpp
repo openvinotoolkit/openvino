b9a890a2c2201b03e8ef2b015bd8db21f8b390e1
Beheshti <nazanin.beheshti@intel.com>
2026-07-17 12:27:38 -0700

    change pattern matching and change sdpa gpu func test

--- src/plugins/intel_gpu/src/plugin/transformations/kvcache_gqa_broadcast_elimination.cpp
+++ src/plugins/intel_gpu/src/plugin/transformations/kvcache_gqa_broadcast_elimination.cpp
@@ -8,6 +8,7 @@
 #include "openvino/pass/pattern/op/wrap_type.hpp"
 #include "openvino/op/reshape.hpp"
 #include "openvino/op/concat.hpp"
+#include "openvino/op/broadcast.hpp"
 #include "openvino/core/partial_shape.hpp"
 #include "intel_gpu/op/sdpa.hpp"
 #include <openvino/util/pp.hpp>
@@ -30,37 +31,43 @@ namespace ov::intel_gpu {
             return in_ps.rank().is_static() && out_ps.rank().is_static() && in_ps.size() == 5 && out_ps.size() == 4;
         };
 
-        auto broadcast_concat = [is_reshape_4d_to_5d](const ov::Output<ov::Node>& output) {
-            auto concat = ov::as_type<ov::op::v0::Concat>(output.get_node());
-            if (concat == nullptr || concat->get_input_size() < 2) {
-                return false;
-            }
-            auto first = concat->get_input_node_ptr(0);
-            if (!is_reshape_4d_to_5d(first)) {
-                return false;
-            }
-            for (size_t i = 1; i < concat->get_input_size(); ++i) {
-                if (concat->get_input_node_ptr(i) != first) {
+        auto reshape_4d_to_5d_expand = [is_reshape_4d_to_5d](const ov::Output<ov::Node>& output) {
+            if (auto concat = ov::as_type<ov::op::v0::Concat>(output.get_node())) {
+                if (concat->get_input_size() < 2) {
                     return false;
-
                 }
+                auto first = concat->get_input_node_ptr(0);
+                if (!is_reshape_4d_to_5d(first)) {
+                    return false;
+                }
+                for (size_t i = 1; i < concat->get_input_size(); ++i) {
+                    if (concat->get_input_node_ptr(i) != first) {
+                        return false;
+                    }
+                }
+                return true;
             }
-            return true;
+
+            if (auto broadcast = ov::as_type<ov::op::v3::Broadcast>(output.get_node())) {
+                return is_reshape_4d_to_5d(broadcast->get_input_node_ptr(0));
+            }
+
+            return false;
         };
 
         auto input_a_m = any_input();
         auto input_attn_mask_m = any_input();
         auto input_scale_m = any_input();
 
-        auto concat_key_m = wrap_type<ov::op::v0::Concat>(broadcast_concat);
+        auto expand_key_m = wrap_type<ov::op::v0::Concat, ov::op::v3::Broadcast>(reshape_4d_to_5d_expand);
 
         auto reshape2_pattern_key_m = any_input();
-        auto reshape_5d_to_4d_key_m = wrap_type<ov::op::v1::Reshape>({ concat_key_m, reshape2_pattern_key_m }, reshape_5d_to_4d);
+        auto reshape_5d_to_4d_key_m = wrap_type<ov::op::v1::Reshape>({ expand_key_m, reshape2_pattern_key_m }, reshape_5d_to_4d);
 
-        auto concat_value_m = wrap_type<ov::op::v0::Concat>(broadcast_concat);
+        auto expand_value_m = wrap_type<ov::op::v0::Concat, ov::op::v3::Broadcast>(reshape_4d_to_5d_expand);
 
         auto reshape2_pattern_value_m = any_input();
-        auto reshape_5d_to_4d_value_m = wrap_type<ov::op::v1::Reshape>({ concat_value_m, reshape2_pattern_value_m }, reshape_5d_to_4d);
+        auto reshape_5d_to_4d_value_m = wrap_type<ov::op::v1::Reshape>({ expand_value_m, reshape2_pattern_value_m }, reshape_5d_to_4d);
 
         auto sdpa_without_attn_mask_m = wrap_type<op::SDPA>({ input_a_m, reshape_5d_to_4d_key_m, reshape_5d_to_4d_value_m });
         auto sdpa_with_attn_mask_m =
@@ -73,15 +80,30 @@ namespace ov::intel_gpu {
         ov::matcher_pass_callback callback = [OV_CAPTURE_CPY_AND_THIS](ov::pass::pattern::Matcher& m) {
             const auto& pattern_map = m.get_pattern_value_map();
             auto sdpa = ov::as_type_ptr<op::SDPA>(m.get_match_root());
-            if (pattern_map.count(concat_key_m) == 0 || pattern_map.count(concat_value_m) == 0) {
+            if (pattern_map.count(expand_key_m) == 0 || pattern_map.count(expand_value_m) == 0) {
                 return false;
             }
-            // Concat -> Reshape(4d->5d) -> original source that was broadcast.
-            auto broadcast_source = [](const ov::Output<ov::Node>& concat_out) {
-                return concat_out.get_node()->input_value(0).get_node()->input_value(0);
+            // (Concat|Broadcast) -> Reshape(4d->5d) -> original source that was expanded.
+            std::cout << "concant boradcast reshape " << std::endl;
+            auto expand_source = [](const ov::Output<ov::Node>& expand_out) {
+                auto node = expand_out.get_node();
+                if (auto concat = ov::as_type<ov::op::v0::Concat>(node)) {
+                    return concat->input_value(0).get_node()->input_value(0);
+                }
+                if (auto broadcast = ov::as_type<ov::op::v3::Broadcast>(node)) {
+                    return broadcast->input_value(0).get_node()->input_value(0);
+                }
+                return ov::Output<ov::Node>();
             };
-            sdpa->input(1).replace_source_output(broadcast_source(pattern_map.at(concat_key_m)));
-            sdpa->input(2).replace_source_output(broadcast_source(pattern_map.at(concat_value_m)));
+
+            auto key_source = expand_source(pattern_map.at(expand_key_m));
+            auto value_source = expand_source(pattern_map.at(expand_value_m));
+            if (!key_source.get_node() || !value_source.get_node()) {
+                return false;
+            }
+
+            sdpa->input(1).replace_source_output(key_source);
+            sdpa->input(2).replace_source_output(value_source);
             return true;
         };
         auto m = std::make_shared<ov::pass::pattern::Matcher>(sdpa_m, "KVCacheGQABroadcastElimination");
