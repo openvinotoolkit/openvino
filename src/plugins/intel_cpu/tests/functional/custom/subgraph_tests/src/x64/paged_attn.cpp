0d5367d775cf0187e8fa82e5668af98fb2807403
Ashish Bisht <ashish.bisht@fujitsu.com>
2026-07-18 16:01:37 +0530

    Changes to test case (x86)

--- src/plugins/intel_cpu/tests/functional/custom/subgraph_tests/src/x64/paged_attn.cpp
+++ src/plugins/intel_cpu/tests/functional/custom/subgraph_tests/src/x64/paged_attn.cpp
@@ -3,34 +3,7 @@
 //
 
 #include "common_test_utils/include/common_test_utils/ov_tensor_utils.hpp"
-#include "common_test_utils/node_builders/constant.hpp"
 #include "internal_properties.hpp"
-#include "openvino/core/type/float16.hpp"
-#include "openvino/op/add.hpp"
-#include "openvino/op/broadcast.hpp"
-#include "openvino/op/concat.hpp"
-#include "openvino/op/convert_like.hpp"
-#include "openvino/op/divide.hpp"
-#include "openvino/op/gather.hpp"
-#include "openvino/op/greater.hpp"
-#include "openvino/op/matmul.hpp"
-#include "openvino/op/multiply.hpp"
-#include "openvino/op/paged_attention.hpp"
-#include "openvino/op/parameter.hpp"
-#include "openvino/op/range.hpp"
-#include "openvino/op/reshape.hpp"
-#include "openvino/op/scaled_dot_product_attention.hpp"
-#include "openvino/op/select.hpp"
-#include "openvino/op/shape_of.hpp"
-#include "openvino/op/softmax.hpp"
-#include "openvino/op/sqrt.hpp"
-#include "openvino/op/squeeze.hpp"
-#include "openvino/op/transpose.hpp"
-#include "openvino/op/unsqueeze.hpp"
-#include "shared_test_classes/base/ov_subgraph.hpp"
-#include "transformations/rt_info/keep_const_precision.hpp"
-#include "utils/cpu_test_utils.hpp"
-#include "utils/general_utils.h"
 #include "custom/subgraph_tests/src/classes/paged_attn.hpp"
 
 using namespace ov::test;
@@ -39,33 +12,6 @@ using namespace ov::op;
 
 namespace ov {
 namespace test {
-
-TEST_P(PagedAttnVSSDPATest, CompareWithRefs) {
-    SKIP_IF_CURRENT_TEST_IS_DISABLED();
-    const auto& [inType, inputShapes, extendBlockIndices, enableXattn, sinkInput, slidingWindow, additional_config,
-                 addSharedReader] = this->GetParam();
-    const bool isSageAttn =
-        intel_cpu::contains_key_value(additional_config, {ov::intel_cpu::enable_sage_attn.name(), true});
-    if (inType == ElementType::bf16 && !ov::with_cpu_x86_bfloat16())
-        GTEST_SKIP();
-    if (isSageAttn && !(ov::with_cpu_x86_avx512_core_amx_int8() || CPUTestUtils::with_cpu_x86_avx2_vnni_2()))
-        GTEST_SKIP();
-
-    past_len_count = 0;
-
-    // compare the logits from paged attn and sdpa
-    auto actualOutputs = run_test(function, extendBlockIndices, sinkInput);
-    // reference model doesn't support sage attention
-    if (isSageAttn) {
-        configuration[ov::intel_cpu::enable_sage_attn.name()] = false;
-    }
-    // Reset past_len_count before running reference test to ensure consistent mask generation
-    past_len_count = 0;
-    auto expectedOutputs = run_ref_test(functionRefs, sinkInput);
-    for (size_t i = 0; i < actualOutputs.size(); i++) {
-        ov::test::utils::compare(expectedOutputs[i], actualOutputs[i], abs_threshold, rel_threshold);
-    }
-}
 
 namespace {
 const std::vector<ov::AnyMap> additional_configs = {{{ov::intel_cpu::enable_sage_attn.name(), true}},
@@ -119,38 +65,6 @@ INSTANTIATE_TEST_SUITE_P(smoke_PagedAttnSharedKVCache,
                                                 {ov::intel_cpu::enable_sage_attn.name(), false}}),
                                             ::testing::Values(true)),   // addSharedReader
                          PagedAttnTestBase::getTestCaseName);
-}  // namespace
-
-TEST_P(PagedAttnVSMatmulTest, CompareWithRefs) {
-    SKIP_IF_CURRENT_TEST_IS_DISABLED();
-    const auto& [inType,
-                 inputShapes,
-                 extendBlockIndices,
-                 enableXattn,
-                 sinkInput,
-                 slidingWindow,
-                 additional_config,
-                 addSharedReader] = this->GetParam();
-    ASSERT_FALSE(addSharedReader) << "PagedAttnVSMatmulTest does not support shared KV-cache (addSharedReader=true)";
-    const bool isSageAttn =
-        intel_cpu::contains_key_value(additional_config, {ov::intel_cpu::enable_sage_attn.name(), true});
-    if (inType == ElementType::bf16 && !ov::with_cpu_x86_bfloat16())
-        GTEST_SKIP();
-    if (isSageAttn && !(ov::with_cpu_x86_avx512_core_amx_int8() || CPUTestUtils::with_cpu_x86_avx2_vnni_2()))
-        GTEST_SKIP();
-    // compare the logits from paged attn and sdpa
-    auto actualOutputs = run_test(function, extendBlockIndices, false);
-    // reference model doesn't support sage attention, disable it
-    if (isSageAttn) {
-        configuration[ov::intel_cpu::enable_sage_attn.name()] = false;
-    }
-    auto expectedOutputs = run_ref_test(functionRefs);
-    for (size_t i = 0; i < actualOutputs.size(); i++) {
-        ov::test::utils::compare(expectedOutputs[i], actualOutputs[i], abs_threshold, rel_threshold);
-    }
-}
-
-namespace {
 
 const std::vector<InputShapes> inputShapes = {  // greedy search
     {
