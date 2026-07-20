d64a94eee6996a73296de84fae6d8b1a1dae4e69
jvincent2021 <jaison.vincentsolomon@intel.com>
2026-07-20 11:57:39 +0530

    Update src/plugins/intel_cpu/src/config.cpp
    Co-authored-by: Pawel Raasz <pawel.raasz@intel.com>

--- src/plugins/intel_cpu/src/config.cpp
+++ src/plugins/intel_cpu/src/config.cpp
@@ -603,11 +603,7 @@ void Config::updateProperties() {
     } else {
         _config.insert({ov::enable_profiling.name(), "NO"});
     }
-    if (multiAppThreadSyncExecution) {
-        _config.insert({ov::intel_cpu::multi_app_thread_sync_execution.name(), "YES"});
-    } else {
-        _config.insert({ov::intel_cpu::multi_app_thread_sync_execution.name(), "NO"});
-    }
+   _config.insert(ov::intel_cpu::multi_app_thread_sync_execution.name(multiAppThreadSyncExecution));
     if (exclusiveAsyncRequests) {
         _config.insert({ov::internal::exclusive_async_requests.name(), "YES"});
     } else {
