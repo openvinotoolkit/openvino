2451eef99d311154c598a750215691300e659741
Proshunin, Nikolay <nikolay.proshunin@intel.com>
2026-07-21 07:53:50 -0700

    [GPU] BF16 OCL: broadcast and strided_slice support

--- src/plugins/intel_gpu/src/graph/impls/ocl/broadcast.cpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl/broadcast.cpp
@@ -145,6 +145,7 @@ attach_broadcast_impl::attach_broadcast_impl() {
     auto types = {
         data_types::f32,
         data_types::f16,
+        data_types::bf16,
         data_types::i8,
         data_types::u8,
         data_types::i32,
