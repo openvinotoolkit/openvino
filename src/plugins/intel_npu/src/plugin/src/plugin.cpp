fa996e1bc3a0d42e02fcbe70604a090084cd424a
Ivanov <sergey.ivanov@intel.com>
2026-08-03 12:01:18 +0200

    Get WeighSharedCtx from cache

--- src/plugins/intel_npu/src/plugin/src/plugin.cpp
+++ src/plugins/intel_npu/src/plugin/src/plugin.cpp
@@ -25,6 +25,7 @@
 #include "npuw/serialization.hpp"
 #include "openvino/core/rt_info/weightless_caching_attributes.hpp"
 #include "openvino/op/constant.hpp"
+#include "openvino/runtime/internal_properties.hpp"
 #include "openvino/runtime/intel_npu/properties.hpp"
 #include "openvino/runtime/properties.hpp"
 #include "openvino/runtime/shared_buffer.hpp"
@@ -39,6 +40,17 @@ using namespace intel_npu;
 constexpr std::string_view NPU_PLUGIN_LIB_NAME = "openvino_intel_npu_plugin";
 constexpr std::string_view NO_BACKEND_MESSAGE = "No backend registered during model import";
 constexpr std::string_view NPUW_MODEL_IMPORTED_MESSAGE = "Finished importing the NPUW compiled model";
+
+ov::internal::WeightSharingCtxPtr extract_weight_sharing_context(ov::AnyMap& properties) {
+    const auto property_name = ov::internal::model_sharing_context.name();
+    const auto property_it = properties.find(property_name);
+    if (property_it == properties.end()) {
+        return nullptr;
+    }
+
+    auto ctx = property_it->second.as<ov::internal::WeightSharingCtxPtr>();
+    return ctx;
+}
 
 /**
  * @brief Just checks if there is any "WeightlessCacheAttribute" present in the model. In the negative case, an error is
@@ -312,6 +324,7 @@ std::shared_ptr<ov::ICompiledModel> Plugin::compile_model(const std::shared_ptr<
     // activate the NPUW path
     auto useNpuwKey = ov::intel_npu::use_npuw.name();
     ov::AnyMap localProperties = properties;
+    auto weightSharingContext = extract_weight_sharing_context(localProperties);
     if (localProperties.count(useNpuwKey)) {
         if (localProperties.at(useNpuwKey).as<bool>() == true) {
             return ov::npuw::ICompiledModel::create(model->clone(), shared_from_this(), localProperties);
@@ -512,7 +525,13 @@ std::shared_ptr<ov::ICompiledModel> Plugin::compile_model(const std::shared_ptr<
 
     std::shared_ptr<ov::ICompiledModel> compiledModel;
     try {
-        compiledModel = std::make_shared<CompiledModel>(model, shared_from_this(), device, graph, localConfig, batch);
+        compiledModel = std::make_shared<CompiledModel>(model,
+                                   shared_from_this(),
+                                   device,
+                                   graph,
+                                   localConfig,
+                                   batch,
+                                   weightSharingContext);
     } catch (const std::exception& ex) {
         OPENVINO_THROW(ex.what());
     } catch (...) {
