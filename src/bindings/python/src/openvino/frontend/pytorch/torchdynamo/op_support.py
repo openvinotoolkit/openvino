7a73a3044a10c1f941d2a125868acf34e61881c0
ynimmaga <yamini.nimmagadda@intel.com>
2026-08-03 14:35:22 +0000

    Revert "torchdynamo: minor cleanups and bug fixes"
    This reverts commit 4dca3262df192559f93567195f581802e185873d.

--- src/bindings/python/src/openvino/frontend/pytorch/torchdynamo/op_support.py
+++ src/bindings/python/src/openvino/frontend/pytorch/torchdynamo/op_support.py
@@ -16,6 +16,7 @@ from torch.fx.passes.tools_common import CALLABLE_NODE_OPS
 from openvino.frontend.pytorch.torchdynamo.backend_utils import _get_disabled_ops
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
 
 
 class OperatorSupport(OpSupport):
