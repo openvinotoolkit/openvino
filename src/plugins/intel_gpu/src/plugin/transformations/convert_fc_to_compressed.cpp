5b95fee68d7857c5b27fb7e370be0eed249e3f1f
vmadananth <varsha.madananth@intel.com>
2026-08-04 10:38:01 -0700

    Remove unused debug_configuration.hpp include from convert_fc_to_compressed.cpp

--- src/plugins/intel_gpu/src/plugin/transformations/convert_fc_to_compressed.cpp
+++ src/plugins/intel_gpu/src/plugin/transformations/convert_fc_to_compressed.cpp
@@ -22,7 +22,6 @@
 #include "openvino/pass/pattern/op/wrap_type.hpp"
 #include "transformations/utils/utils.hpp"
 #include "openvino/core/graph_util.hpp"
-#include "intel_gpu/runtime/debug_configuration.hpp"
 
 #include "compressed_weights_pattern.hpp"
 
