4ef3046e1726d4378c05399d1ffd9d85a5383014
Xin Wang <xin1.wang@intel.com>
2026-08-05 17:23:40 +0800

    Fix format Signed-off-by: Xin Wang <xin1.wang@intel.com>
    Signed-off-by: Xin Wang <xin1.wang@intel.com>

--- src/plugins/intel_npu/src/compiler_adapter/src/dynamic_graph.cpp
+++ src/plugins/intel_npu/src/compiler_adapter/src/dynamic_graph.cpp
@@ -42,8 +42,9 @@ uint32_t getCommandQueueOptions(const FilteredConfig& config,
 void populateRuntimeConfigChain(NpuVMRuntimeConfigChain& configChain,
                                 const FilteredConfig& config,
                                 const std::shared_ptr<ZeroInitStructsHolder>& zeroInitStruct) {
-    configChain.append(NPU_VM_RUNTIME_CONFIG_TYPE_QUEUE_PRIORITY,
-                       static_cast<npu_vm_runtime_config_value_t>(zeroUtils::toZeQueuePriority(config.get<MODEL_PRIORITY>())));
+    configChain.append(
+        NPU_VM_RUNTIME_CONFIG_TYPE_QUEUE_PRIORITY,
+        static_cast<npu_vm_runtime_config_value_t>(zeroUtils::toZeQueuePriority(config.get<MODEL_PRIORITY>())));
     if (config.has<WORKLOAD_TYPE>()) {
         const auto workloadType = zeroUtils::toZeQueueWorkloadType(config.get<WORKLOAD_TYPE>());
         if (workloadType.has_value()) {
@@ -51,8 +52,7 @@ void populateRuntimeConfigChain(NpuVMRuntimeConfigChain& configChain,
                                static_cast<npu_vm_runtime_config_value_t>(workloadType.value()));
         }
     }
-    configChain.append(NPU_VM_RUNTIME_CONFIG_TYPE_QUEUE_OPTIONS,
-                       getCommandQueueOptions(config, zeroInitStruct));
+    configChain.append(NPU_VM_RUNTIME_CONFIG_TYPE_QUEUE_OPTIONS, getCommandQueueOptions(config, zeroInitStruct));
 }
 
 }  // namespace
