5f7f26b7b051c3bf37d7245a5778bfa11c0d6fc1
Mostafa Faheem <mostafaaafaheem@gmail.com>
2026-08-06 18:01:24 +0300

    Add test files to sources.cmake

--- src/core/tests/type_prop/sources.cmake
+++ src/core/tests/type_prop/sources.cmake
@@ -37,6 +37,7 @@ set(OV_CORE_TESTS_TYPE_PROP_SRCS
     ${CMAKE_CURRENT_LIST_DIR}/convert.cpp
     ${CMAKE_CURRENT_LIST_DIR}/convert_color_i420.cpp
     ${CMAKE_CURRENT_LIST_DIR}/convert_color_nv12.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/convert_color_to_nv12.cpp
     ${CMAKE_CURRENT_LIST_DIR}/convert_promote_types.cpp
     ${CMAKE_CURRENT_LIST_DIR}/convolution.cpp
     ${CMAKE_CURRENT_LIST_DIR}/convolution_backprop_data.cpp
