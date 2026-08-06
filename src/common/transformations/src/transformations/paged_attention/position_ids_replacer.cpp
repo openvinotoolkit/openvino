e95f5a0ac5bd366b50dd300bd1f8136c647136cc
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2026-08-06 17:45:07 +0200

    clang fix

--- src/common/transformations/src/transformations/paged_attention/position_ids_replacer.cpp
+++ src/common/transformations/src/transformations/paged_attention/position_ids_replacer.cpp
@@ -67,8 +67,7 @@ ov::pass::PositionIDsReplacer::PositionIDsReplacer(const Output<Node>& position_
 
     auto add = wrap_type<v1::Add>({mul, position_embed});
 
-    ov::matcher_pass_callback callback = [=, shared_unsqueeze = std::shared_ptr<v0::Unsqueeze>()](
-                                             Matcher& m) mutable {
+    ov::matcher_pass_callback callback = [=, shared_unsqueeze = std::shared_ptr<v0::Unsqueeze>()](Matcher& m) mutable {
         const auto& pattern_map = m.get_pattern_value_map();
         const auto matched = pattern_map.at(position_ids_pattern).get_node_shared_ptr();
         // position_ids is detached and the model derives positions internally: redirect the embedding to a
