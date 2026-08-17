57ed8d370d692b1da28734afaaf831cc064a3049
Michal Miotk <michal.miotk@intel.com>
2026-08-17 14:38:31 +0200

    fix code style

--- src/common/util/src/os/win/win_mmap_object.cpp
+++ src/common/util/src/os/win/win_mmap_object.cpp
@@ -677,9 +677,10 @@ void MapHolder::setup(HANDLE file_handle, size_t offset, size_t size, bool no_pl
 
 void MapHolder::set(const std::filesystem::path& path, size_t offset, size_t size, bool no_placeholder, MmapMode mode) {
     const bool writable = mode == MmapMode::READ_WRITE;
-    auto fh = ::CreateFileW(path.c_str(),
+    auto fh = ::CreateFileW(
+        path.c_str(),
         writable ? (GENERIC_READ | GENERIC_WRITE) : GENERIC_READ,
-        writable ? (FILE_SHARE_READ | FILE_SHARE_WRITE| FILE_SHARE_DELETE) : (FILE_SHARE_READ | FILE_SHARE_DELETE),
+        writable ? (FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE) : (FILE_SHARE_READ | FILE_SHARE_DELETE),
         nullptr,
         OPEN_EXISTING,
         FILE_ATTRIBUTE_NORMAL | FILE_FLAG_RANDOM_ACCESS,
