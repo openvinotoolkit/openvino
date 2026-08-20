d5ca6df883c0bbe341e2d36a81ca1b96fbb1fde2
Stefania Hergane <stefania-persida.hergane@intel.com>
2026-08-20 13:08:02 +0100

    [NPU Tests] Add NF4/FP4/FP8 precision coverage for InferRequest tensor precision tests
    * Extend precision coverage for `OVInferRequestCheckTensorPrecision` and `OVInferRequestIOTensorSetPrecisionTest`
    by adding nf4, f4e2m1, f8e4m3, f8e5m2 and f8e8m0 to the tested precisions.
    
    * Update npu_skip_func_tests.xml accordingly:
      ** skip f4e2m1 and f8e8m0 (not supported yet on NPU devices available in OV CI)
      ** skip f8e4m3/f8e5m2 on NPU3720, NPU4000 (run on NPU5010)
      ** nf4 not supported by NPU3720; consolidate the i4/u4/nf4 "4-bit network outputs" skip into a single block
    
    Signed-off-by: Stefania Hergane <stefania-persida.hergane@intel.com>

--- src/plugins/intel_npu/tests/functional/behavior/ov_infer_request/io_tensor.cpp
+++ src/plugins/intel_npu/tests/functional/behavior/ov_infer_request/io_tensor.cpp
@@ -271,6 +271,11 @@ const std::vector<ov::AnyMap> autoConfigs = {
 
 const std::vector<ov::element::Type> prcs = {
     ov::element::boolean,
+    ov::element::nf4,
+    ov::element::f4e2m1,
+    ov::element::f8e4m3,
+    ov::element::f8e5m2,
+    ov::element::f8e8m0,
     ov::element::bf16,
     ov::element::f16,
     ov::element::f32,
