4bf9c250e3a027afe37b32f93748b0e4710079d7
Alexander Nesterov <nesterov.alexander@outlook.com>
2026-08-24 11:21:09 +0200

    [GPU][Vulkan] Separate memory allocation ownership

--- src/plugins/intel_gpu/src/runtime/vulkan/vulkan_memory_internal.hpp
+++ src/plugins/intel_gpu/src/runtime/vulkan/vulkan_memory_internal.hpp
@@ -11,6 +11,7 @@ namespace detail {
 
 void check_vulkan_memory_result(VkResult result, const char* operation);
 bool transition_foreign_buffer_ownership(const std::shared_ptr<vulkan_device>& device_owner, VkBuffer buffer, bool acquire) noexcept;
+vulkan_buffer_allocation::ptr allocate_vulkan_buffer(vulkan_engine& engine, size_t requested_size, vulkan_buffer_memory_usage usage);
 
 }  // namespace detail
 }  // namespace cldnn::vulkan
