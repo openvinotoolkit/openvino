b0d879ece1770d0ecfdf939b47b46031f1d58d2b
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2026-08-27 08:00:29 +0200

    Deprecate LazyBuffer
    Signed-off-by: Tomasz Jankowski <tomasz1.jankowski@intel.com>

--- src/core/src/runtime/lazy_buffer.cpp
+++ src/core/src/runtime/lazy_buffer.cpp
@@ -8,6 +8,7 @@
 #include <mutex>
 #include <utility>
 
+#include "openvino/core/deprecated.hpp"
 #include "openvino/core/except.hpp"
 #include "openvino/core/memory_util.hpp"
 #include "openvino/util/file_util.hpp"
@@ -15,6 +16,7 @@
 #include "openvino/util/parallel_read_streambuf.hpp"
 
 namespace ov {
+OPENVINO_SUPPRESS_DEPRECATED_START
 LazyBuffer::LazyBuffer(std::filesystem::path file_path, size_t offset, size_t byte_size)
     : AlignedBuffer(),
       m_file_path{std::move(file_path)},
@@ -94,4 +96,5 @@ void LazyBuffer::hint_prefetch() const {
 void LazyBuffer::hint_evict() noexcept {}
 
 void LazyBuffer::hint_evict(size_t offset, size_t size) noexcept {}
+OPENVINO_SUPPRESS_DEPRECATED_END
 }  // namespace ov
