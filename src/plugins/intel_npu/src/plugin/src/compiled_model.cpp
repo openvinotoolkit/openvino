9d4e859fd5a3926ab131fddca6d9808b26b35c20
MirceaDan99 <mircea-aurelian.dan@intel.com>
2026-09-01 14:46:14 +0000

    Assert that graph is a weightful ELF one if `NPU_EXPORT_RAW_BLOB` was set and throw otherwise

--- src/plugins/intel_npu/src/plugin/src/compiled_model.cpp
+++ src/plugins/intel_npu/src/plugin/src/compiled_model.cpp
@@ -122,36 +122,40 @@ void CompiledModel::export_model(std::ostream& stream) const {
         std::tie(blobSizesBeforeVersioning, initBlobSizes) = _graph->export_blob(stream);
     }
 
-    if (!_propertiesManager->getConfig().get<EXPORT_RAW_BLOB>()) {
-        std::optional<std::vector<ov::Layout>> inputLayouts = std::vector<ov::Layout>();
-        std::optional<std::vector<ov::Layout>> outputLayouts = std::vector<ov::Layout>();
-
-        for (const ov::Output<const ov::Node>& nodeOutput : inputs()) {
-            inputLayouts->push_back(
-                std::dynamic_pointer_cast<const ov::op::v0::Parameter>(nodeOutput.get_node_shared_ptr())->get_layout());
-        }
-        for (const ov::Output<const ov::Node>& nodeOutput : outputs()) {
-            outputLayouts->push_back(
-                std::dynamic_pointer_cast<const ov::op::v0::Result>(nodeOutput.get_node_shared_ptr())->get_layout());
-        }
-
-        std::optional<uint32_t> compilerVersion = std::nullopt;
-        if (_propertiesManager->getConfig().has(ov::intel_npu::compiler_version.name())) {
-            compilerVersion = _propertiesManager->getConfig().get<COMPILER_VERSION>();
-        }
-
-        Metadata<CURRENT_METADATA_VERSION>(blobSizesBeforeVersioning,
-                                           CURRENT_OPENVINO_VERSION,
-                                           initBlobSizes,
-                                           _batchSize,
-                                           inputLayouts,
-                                           outputLayouts,
-                                           compilerVersion,
-                                           blobSizeAfterEncryption,
-                                           _graph->get_compatibility_descriptor(),
-                                           _graph->get_blob_type())
-            .write(stream);
+    if (_propertiesManager->getConfig().get<EXPORT_RAW_BLOB>()) {
+        OPENVINO_ASSERT(_graph->get_blob_type() == BlobType::ELF && _graph->get_kind() == GraphKind::Weightful,
+                        "Requested raw blob export, but the graph is not a weightful ELF one.");
+        return;
     }
+
+    std::optional<std::vector<ov::Layout>> inputLayouts = std::vector<ov::Layout>();
+    std::optional<std::vector<ov::Layout>> outputLayouts = std::vector<ov::Layout>();
+
+    for (const ov::Output<const ov::Node>& nodeOutput : inputs()) {
+        inputLayouts->push_back(
+            std::dynamic_pointer_cast<const ov::op::v0::Parameter>(nodeOutput.get_node_shared_ptr())->get_layout());
+    }
+    for (const ov::Output<const ov::Node>& nodeOutput : outputs()) {
+        outputLayouts->push_back(
+            std::dynamic_pointer_cast<const ov::op::v0::Result>(nodeOutput.get_node_shared_ptr())->get_layout());
+    }
+
+    std::optional<uint32_t> compilerVersion = std::nullopt;
+    if (_propertiesManager->getConfig().has(ov::intel_npu::compiler_version.name())) {
+        compilerVersion = _propertiesManager->getConfig().get<COMPILER_VERSION>();
+    }
+
+    Metadata<CURRENT_METADATA_VERSION>(blobSizesBeforeVersioning,
+                                       CURRENT_OPENVINO_VERSION,
+                                       initBlobSizes,
+                                       _batchSize,
+                                       inputLayouts,
+                                       outputLayouts,
+                                       compilerVersion,
+                                       blobSizeAfterEncryption,
+                                       _graph->get_compatibility_descriptor(),
+                                       _graph->get_blob_type())
+        .write(stream);
 }
 
 std::shared_ptr<const ov::Model> CompiledModel::get_runtime_model() const {
