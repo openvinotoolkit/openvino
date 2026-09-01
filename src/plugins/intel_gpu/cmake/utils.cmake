8581192140e68925c9d636b7a9a4d6b7c9eee3b2
Alexander Nesterov <nesterov.alexander@outlook.com>
2026-09-01 22:30:15 +0200

    [GPU] Parameterize runtime stack composition
    Signed-off-by: Alexander Nesterov <nesterov.alexander@outlook.com>

--- src/plugins/intel_gpu/cmake/utils.cmake
+++ src/plugins/intel_gpu/cmake/utils.cmake
@@ -2,18 +2,35 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function(ov_gpu_resolve_runtime OUT_VAR)
+    set(oneValueArgs RUNTIME)
+    cmake_parse_arguments(ARG "" "${oneValueArgs}" "" ${ARGN})
+
+    if(ARG_RUNTIME)
+        set(runtime "${ARG_RUNTIME}")
+    elseif(DEFINED OV_GPU_RT AND NOT OV_GPU_RT STREQUAL "")
+        set(runtime "${OV_GPU_RT}")
+    else()
+        set(runtime "${GPU_RT_TYPE}")
+    endif()
+
+    set(${OUT_VAR} "${runtime}" PARENT_SCOPE)
+endfunction()
+
 function(ov_gpu_set_runtime_definitions_for TARGET_NAME)
-    if(GPU_RT_TYPE STREQUAL "ZE")
+    ov_gpu_resolve_runtime(runtime ${ARGN})
+
+    if(runtime STREQUAL "ZE")
         target_compile_definitions(${TARGET_NAME} PRIVATE OV_GPU_WITH_ZE_RT=1)
         target_compile_definitions(${TARGET_NAME} PRIVATE OV_GPU_DEFAULT_ZE_RT=1)
-    elseif(GPU_RT_TYPE STREQUAL "OCL")
+    elseif(runtime STREQUAL "OCL")
         target_compile_definitions(${TARGET_NAME} PRIVATE OV_GPU_WITH_OCL_RT=1)
         target_compile_definitions(${TARGET_NAME} PRIVATE OV_GPU_DEFAULT_OCL_RT=1)
-    elseif(GPU_RT_TYPE STREQUAL "SYCL")
+    elseif(runtime STREQUAL "SYCL")
         target_compile_definitions(${TARGET_NAME} PRIVATE OV_GPU_WITH_SYCL_RT=1)
         target_compile_definitions(${TARGET_NAME} PRIVATE OV_GPU_DEFAULT_SYCL_RT=1)
     else()
-        message(FATAL_ERROR "Invalid GPU runtime type: `${GPU_RT_TYPE}`")
+        message(FATAL_ERROR "Invalid GPU runtime type: `${runtime}`")
     endif()
 endfunction()
 
@@ -32,19 +49,22 @@ function(ov_gpu_use_header_target_for TARGET_NAME HEADER_TARGET)
 endfunction()
 
 function(ov_gpu_link_runtime_dependencies_for TARGET_NAME)
-    if(GPU_RT_TYPE STREQUAL "ZE")
+    ov_gpu_resolve_runtime(runtime ${ARGN})
+
+    if(runtime STREQUAL "ZE")
         target_link_libraries(${TARGET_NAME} PRIVATE openvino::zero_loader)
-    elseif(GPU_RT_TYPE STREQUAL "OCL")
+    elseif(runtime STREQUAL "OCL")
         target_link_libraries(${TARGET_NAME} PRIVATE OpenCL::OpenCL)
-    elseif(GPU_RT_TYPE STREQUAL "SYCL")
+    elseif(runtime STREQUAL "SYCL")
         # SYCL toolchain dependencies are attached by add_sycl_to_target.
     else()
-        message(FATAL_ERROR "Invalid GPU runtime type: `${GPU_RT_TYPE}`")
+        message(FATAL_ERROR "Invalid GPU runtime type: `${runtime}`")
     endif()
 endfunction()
 
 function(ov_gpu_set_runtime_interface_for TARGET_NAME)
-    ov_gpu_set_runtime_definitions_for(${TARGET_NAME})
-    ov_gpu_link_runtime_dependencies_for(${TARGET_NAME})
+    ov_gpu_resolve_runtime(runtime ${ARGN})
+    ov_gpu_set_runtime_definitions_for(${TARGET_NAME} RUNTIME ${runtime})
+    ov_gpu_link_runtime_dependencies_for(${TARGET_NAME} RUNTIME ${runtime})
     ov_gpu_set_opencl_api_version_for(${TARGET_NAME})
 endfunction()
