0286ec5efae8d53da4a338fd5ff48dd6b6dd6384
Arseniy Obolenskiy <gooddoog@student.su>
2026-09-03 05:07:13 +0200

    fmt

--- src/common/transformations/src/transformations/common_optimizations/convert_quantize_dequantize.cpp
+++ src/common/transformations/src/transformations/common_optimizations/convert_quantize_dequantize.cpp
@@ -135,12 +135,12 @@ ConvertQuantizeDequantize::ConvertQuantizeDequantize(const ov::element::TypeVect
         if (!op_util::get_single_value(output_high, out_high_val))
             return false;
 
-#define PRECISION_LIMITS_FOR(type)                                                                           \
-    {                                                                                                        \
-        ov::element::type,                                                                                   \
-            std::pair<float, float>{                                                                        \
-                static_cast<float>(std::numeric_limits<ov::fundamental_type_for<ov::element::type>>::min()), \
-                static_cast<float>(std::numeric_limits<ov::fundamental_type_for<ov::element::type>>::max())} \
+#define PRECISION_LIMITS_FOR(type)                                                                          \
+    {                                                                                                       \
+        ov::element::type, std::pair<float, float> {                                                        \
+            static_cast<float>(std::numeric_limits<ov::fundamental_type_for<ov::element::type>>::min()),    \
+                static_cast<float>(std::numeric_limits<ov::fundamental_type_for<ov::element::type>>::max()) \
+        }                                                                                                   \
     }
 
         static const std::unordered_map<ov::element::Type_t, std::pair<float, float>> supported_intervals{
