31fc0b42bd53ff3992b47e4ae99f6dd78233beb0
MirceaDan99 <mircea-aurelian.dan@intel.com>
2026-09-07 09:21:31 +0000

    Add suggested changes & fix failing HostCompile test on Ubuntu

--- src/plugins/intel_npu/src/plugin/src/compiled_model.cpp
+++ src/plugins/intel_npu/src/plugin/src/compiled_model.cpp
@@ -97,10 +97,10 @@ void CompiledModel::export_model(std::ostream& stream) const {
     _logger.debug("CompiledModel::export_model");
 
     const bool rawBlobExportRequested = _propertiesManager->getConfig().get<EXPORT_RAW_BLOB>();
-    OPENVINO_ASSERT(rawBlobExportRequested
-                        ? _graph->get_blob_type() == BlobType::ELF && _graph->get_kind() == GraphKind::Weightful
-                        : true,
-                    "Requested raw blob export, but the graph is not a weightful ELF one.");
+    if (rawBlobExportRequested) {
+        OPENVINO_ASSERT(_graph->get_blob_type() == BlobType::ELF && _graph->get_kind() == GraphKind::Weightful,
+                        "Requested raw blob export, but the graph is not a weightful ELF one.");
+    }
 
     uint64_t blobSizesBeforeVersioning;
     std::optional<uint64_t> blobSizeAfterEncryption = std::nullopt;
