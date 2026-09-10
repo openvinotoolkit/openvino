c8866f20cfee488447caed47299eebc72b5c8022
Michal Miotk <michal.miotk@intel.com>
2026-09-10 09:03:40 +0000

    Revert "is in shape of subgraph check"
    This reverts commit 01a7c47623ebd02fd0ccf19ad14af92faf465428.

--- src/plugins/intel_gpu/src/graph/program.cpp
+++ src/plugins/intel_gpu/src/graph/program.cpp
@@ -1582,8 +1582,7 @@ void program::set_layout_optimizer_attributes(layout_optimizer& lo) {
             prim.type() == cldnn::resample::type_id() || prim.type() == cldnn::reorg_yolo::type_id())
             lo.set_optimization_attribute(layout_optimizer::optimization_attributes_type::bfyx_only_layer, 1);
 
-        if (!prim.is_in_shape_of_subgraph() &&
-            prim.is_in_data_flow() &&
+        if (prim.is_in_data_flow() &&
             prim.type() != cldnn::convolution::type_id() &&
             prim.type() != cldnn::deconvolution::type_id() &&
             prim.type() != cldnn::activation::type_id() &&
