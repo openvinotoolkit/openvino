14afcbbfe9398d18372cef55fa3b5466dc0c2d68
Oleg Pipikin <oleg.pipikin@intel.com>
2026-09-10 13:27:37 +0200

    Revert changes of transpose operation

--- src/core/reference/include/openvino/reference/transpose.hpp
+++ src/core/reference/include/openvino/reference/transpose.hpp
@@ -11,9 +11,6 @@
 #include <vector>
 
 #include "openvino/core/shape.hpp"
-#include "openvino/core/type/element_iterator.hpp"
-#include "openvino/reference/utils/coordinate_index.hpp"
-#include "openvino/reference/utils/coordinate_transform.hpp"
 
 namespace ov {
 namespace reference {
@@ -67,30 +64,11 @@ void transpose_4bit(const uint8_t* data,
                     const std::vector<int64_t>& axes_order,
                     const Shape& out_shape);
 
-template <element::Type_t ET>
-void transpose_by_iterator(const uint8_t* data,
-                           uint8_t* out,
-                           const Shape& data_shape,
-                           const std::vector<int64_t>& axes_order,
-                           const Shape& out_shape) {
-    static_assert(ET == element::Type_t::u2 || ET == element::Type_t::u3 || ET == element::Type_t::u6,
-                  "transpose_by_iterator supports only u2, u3, and u6 element types");
-
-    const size_t ndim = data_shape.size();
-    auto in_it = ov::element::iterator<ET>(reinterpret_cast<const int8_t*>(data));
-    auto out_it = ov::element::iterator<ET>(reinterpret_cast<int8_t*>(out));
-
-    ov::Coordinate src_coord(ndim);
-    const ov::CoordinateTransformBasic dst_transform{out_shape};
-    for (const auto& dst_coord : dst_transform) {
-        for (size_t j = 0; j < ndim; ++j)
-            src_coord[axes_order[j]] = dst_coord[j];
-
-        const size_t dst_idx = ov::coordinate_index(dst_coord, out_shape);
-        const size_t src_idx = ov::coordinate_index(src_coord, data_shape);
-        *(out_it + dst_idx) = *(in_it + src_idx);
-    }
-}
+void transpose_2bit(const uint8_t* data,
+                    uint8_t* out,
+                    const Shape& data_shape,
+                    const std::vector<int64_t>& axes_order,
+                    const Shape& out_shape);
 
 }  // namespace reference
 }  // namespace ov
