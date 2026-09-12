bd16e1c3ebf271c202f620442235fff71f0938d6
Ricardo Alves de Souza <ricardoasouz@hotmail.com>
2026-09-12 07:51:35 +0100

    Fix reduce reshape elimination with keep_dims=false

--- src/common/transformations/tests/common_optimizations/nop_elimination.cpp
+++ src/common/transformations/tests/common_optimizations/nop_elimination.cpp
@@ -314,6 +314,23 @@ TEST(nop_elimination, reshape_arithmetical_reduce_elimination_dynamic) {
     ASSERT_TRUE(count_ops_of_type<op::v1::Reshape>(f) == 0);
 }
 
+TEST(nop_elimination, reshape_arithmetical_reduce_elimination_keep_dims_false_negative) {
+    auto data = std::make_shared<op::v0::Parameter>(element::f32, Shape{4, 11, 4});
+    auto axes = op::v0::Constant::create<int64_t>(element::i64, Shape{1}, {1});
+
+    auto reduce = std::make_shared<op::v1::ReduceMean>(data, axes, false);
+    auto reshape_pattern = op::v0::Constant::create<int64_t>(element::i64, Shape{2}, {16, 1});
+    auto reshape = std::make_shared<op::v1::Reshape>(reduce, reshape_pattern, false);
+
+    auto model = std::make_shared<ov::Model>(OutputVector{reshape}, ParameterVector{data});
+
+    pass::Manager manager;
+    manager.register_pass<ov::pass::NopElimination>(false);
+    manager.run_passes(model);
+
+    ASSERT_EQ(count_ops_of_type<op::v1::Reshape>(model), 1);
+}
+
 TEST(nop_elimination, reshape_logical_reduce_elimination_dynamic) {
     auto arg = std::make_shared<op::v0::Parameter>(element::boolean, PartialShape({-1, 96, 100, 100}));
     auto reduce_axes = v0::Constant::create(element::i64, Shape{2}, {2, 3});
