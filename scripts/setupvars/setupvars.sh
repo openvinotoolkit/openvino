4c646d17a6514f0905095a4cd96ac3cdcd18b48d
Alicja Miloszewska <alicja.miloszewska@intel.com>
2026-09-14 13:30:20 +0200

    Drop 3.10 in docs, requirements and setupvars

--- scripts/setupvars/setupvars.sh
+++ scripts/setupvars/setupvars.sh
@@ -100,7 +100,7 @@ if command -v lsb_release >/dev/null 2>&1; then
 fi
 
 PYTHON_VERSION_MAJOR="3"
-MIN_REQUIRED_PYTHON_VERSION_MINOR="10"
+MIN_REQUIRED_PYTHON_VERSION_MINOR="11"
 MAX_SUPPORTED_PYTHON_VERSION_MINOR="14"
 
 check_python_version () {
