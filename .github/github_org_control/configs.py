c222a8a675b8627b8258220511b0c20d057e1670
Alicja Miloszewska <alicja.miloszewska@intel.com>
2026-09-15 17:18:20 +0200

    Revert as 3.11 requires extra changes in workflows

--- .github/github_org_control/configs.py
+++ .github/github_org_control/configs.py
@@ -14,8 +14,8 @@ import json
 from pathlib import Path
 
 
-if sys.version_info[:2] < (3, 11):
-    raise Exception("Python version must be >= 3.11")
+if sys.version_info[:2] < (3, 10):
+    raise Exception("Python version must be >= 3.9")
 
 
 class ConfigException(Exception):
