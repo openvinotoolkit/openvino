f07768661c2c16c5d9100112da56a62ca2615ba3
Alicja Miloszewska <alicja.miloszewska@intel.com>
2026-09-15 16:22:35 +0200

    Update configs.py and ncc_naming_style

--- .github/github_org_control/configs.py
+++ .github/github_org_control/configs.py
@@ -14,8 +14,8 @@ import json
 from pathlib import Path
 
 
-if sys.version_info[:2] < (3, 9):
-    raise Exception("Python version must be >= 3.9")
+if sys.version_info[:2] < (3, 11):
+    raise Exception("Python version must be >= 3.11")
 
 
 class ConfigException(Exception):
