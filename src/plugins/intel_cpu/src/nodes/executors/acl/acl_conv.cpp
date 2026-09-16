2d1cace59774e581fd9ff6bab9408bce0ba8883b
Aleksandr Voron <aleksandr.voron@intel.com>
2026-09-16 20:23:56 +0200

    fix clang

--- src/plugins/intel_cpu/src/nodes/executors/acl/acl_conv.cpp
+++ src/plugins/intel_cpu/src/nodes/executors/acl/acl_conv.cpp
@@ -7,8 +7,6 @@
 #include <arm_compute/core/CoreTypes.h>
 #include <arm_compute/core/Error.h>
 #include <arm_compute/core/QuantizationInfo.h>
-#include <arm_compute/core/TensorInfo.h>
-#include <arm_compute/core/TensorShape.h>
 #include <arm_compute/core/Types.h>
 #include <arm_compute/runtime/NEON/functions/NEConvolutionLayer.h>
 
