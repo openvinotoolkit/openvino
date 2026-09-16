df5468a0116113e3ee54f5fa8dc710ad4e1a3f44
Tomasz Krupa <tomasz.krupa@intel.com>
2026-09-16 11:43:39 +0000

    Code style

--- src/plugins/intel_gpu/src/graph/rms.cpp
+++ src/plugins/intel_gpu/src/graph/rms.cpp
@@ -19,6 +19,7 @@ std::vector<layout> rms_inst::calc_output_layouts(rms_node const& node, kernel_i
     auto input_layout = impl_param.get_input_layout();
     auto output_type = desc->output_data_types[0].value_or(input_layout.data_type);
 
+	std::vector<layout> output_layouts;
     if (impl_param.has_fused_primitives()) {
         const auto& fused_prims = node.get_fused_primitives();
         auto dq_it = std::find_if(fused_prims.begin(), fused_prims.end(), [](const cldnn::fused_primitive_desc& f) {
@@ -30,13 +31,16 @@ std::vector<layout> rms_inst::calc_output_layouts(rms_node const& node, kernel_i
             dq_op.set_attrs(fused_prims.back().typed_desc<dynamic_quantize>()->attrs);
             const auto output_shapes = ov::op::internal::DynamicQuantize::shape_infer(&dq_op, {input_layout.get_partial_shape()});
             auto dq_attrs = fused_prims.back().typed_desc<dynamic_quantize>()->attrs;
-            return {layout{output_shapes[0], dq_attrs.quantization_dt, input_layout.format}, layout{output_shapes[1], dq_attrs.scale_dt, input_layout.format}};
+            output_layouts = {layout{output_shapes[0], dq_attrs.quantization_dt, input_layout.format}, layout{output_shapes[1], dq_attrs.scale_dt, input_layout.format}};
         } else {
             output_type = impl_param.get_output_element_type();
         }
     }
+    if (output_layouts.empty()) {
+	    output_layouts = { layout(input_layout.get_partial_shape(), output_type, input_layout.format) };
+    }
 
-    return { layout(input_layout.get_partial_shape(), output_type, input_layout.format) };
+    return output_layouts;
 }
 
 template std::vector<layout> rms_inst::calc_output_layouts<ov::PartialShape>(
