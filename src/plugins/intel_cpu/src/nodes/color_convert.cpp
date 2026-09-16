c613b5be99ba97b9b78178c1ef140af63bad15c8
Voron, Aleksandr <aleksandr.voron@intel.com>
2026-09-16 15:27:39 +0200

    fix2

--- src/plugins/intel_cpu/src/nodes/color_convert.cpp
+++ src/plugins/intel_cpu/src/nodes/color_convert.cpp
@@ -530,15 +530,15 @@ const jit_uni_converter& jit_converter_create() {
     auto createKernel = []() {
         std::unique_ptr<jit_uni_converter> kernel;
 
-        if (mayiuse(cpu_isa_t::avx512_core)) {
+        if (ov::with_cpu_x86_avx512_core()) {
             auto converter = new JitConverter<T[16]>;
             kernel.reset(converter);
             converter->init();
-        } else if (mayiuse(cpu_isa_t::avx2)) {
+        } else if (ov::with_cpu_x86_avx2()) {
             auto converter = new JitConverter<T[8]>;
             kernel.reset(converter);
             converter->init();
-        } else if (mayiuse(cpu_isa_t::sse41)) {
+        } else if (ov::with_cpu_x86_sse42()) {
             auto converter = new JitConverter<T[4]>;
             kernel.reset(converter);
             converter->init();
@@ -866,15 +866,15 @@ const jit_uni_converter& jit_converter_create() {
     auto createKernel = []() {
         std::unique_ptr<jit_uni_converter> kernel;
 
-        if (mayiuse(cpu_isa_t::avx512_core)) {
+        if (ov::with_cpu_x86_avx512_core()) {
             auto converter = new JitConverter<T[16]>;
             kernel.reset(converter);
             converter->init();
-        } else if (mayiuse(cpu_isa_t::avx2)) {
+        } else if (ov::with_cpu_x86_avx2()) {
             auto converter = new JitConverter<T[8]>;
             kernel.reset(converter);
             converter->init();
-        } else if (mayiuse(cpu_isa_t::sse41)) {
+        } else if (ov::with_cpu_x86_sse42()) {
             auto converter = new JitConverter<T[4]>;
             kernel.reset(converter);
             converter->init();
