93964cd8253e40aca0914dfc0e466cf6f25029ef
suryasidd <surya.siddharth.pemmaraju@intel.com>
2026-09-17 07:21:47 +0200

    Fixed copyright issues

--- src/bindings/python/src/openvino/frontend/pytorch/torchdynamo/vllm/lm_head.py
+++ src/bindings/python/src/openvino/frontend/pytorch/torchdynamo/vllm/lm_head.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2018-2025 Intel Corporation
+# Copyright (C) 2018-2026 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
 """Run lm_head through OV instead of torch (OV_LM_HEAD=1, default off).
