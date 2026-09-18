d93343ccdfcbff0aa9141338caa780f86f5b5855
Arseniy Obolenskiy <gooddoog@student.su>
2026-09-18 18:50:22 +0200

    address comment

--- src/plugins/intel_cpu/tests/unit/snippets_transformations/common/lowered/buffer_allocation.cpp
+++ src/plugins/intel_cpu/tests/unit/snippets_transformations/common/lowered/buffer_allocation.cpp
@@ -46,46 +46,10 @@ namespace snippets {
 
 #if defined(OPENVINO_ARCH_X86_64) || defined(OPENVINO_ARCH_ARM64)
 
-#if defined(OPENVINO_ARCH_X86_64)
-using BrgemmConfig = intel_cpu::brgemm_utils::BrgemmConfig;
-#endif
-
 class BufferAllocationCPUTest : public BufferAllocationTest {
 protected:
     std::shared_ptr<ov::snippets::IShapeInferSnippetsFactory> GetShapeInferFactory() const override {
         return std::make_shared<ov::snippets::CPUShapeInferSnippetsFactory>();
-    }
-
-    std::vector<ov::snippets::lowered::pass::PassPipeline::PositionedPassLowered> getBackendSpecificPasses() override {
-        std::vector<ov::snippets::lowered::pass::PassPipeline::PositionedPassLowered> backend_passes;
-
-#if defined(OPENVINO_ARCH_X86_64)
-        // Add BrgemmCPUBlocking after MarkLoops
-        backend_passes.emplace_back(
-            ov::snippets::pass::PassPosition(ov::snippets::pass::PassPosition::Place::After,
-                                             ov::snippets::lowered::pass::MarkLoops::get_type_info_static()),
-            std::make_shared<ov::intel_cpu::pass::BrgemmCPUBlocking>());
-
-        // Add InsertBrgemmCopyBuffers after SplitLoops
-        backend_passes.emplace_back(
-            ov::snippets::pass::PassPosition(ov::snippets::pass::PassPosition::Place::After,
-                                             ov::snippets::lowered::pass::SplitLoops::get_type_info_static()),
-            std::make_shared<ov::intel_cpu::pass::InsertBrgemmCopyBuffers>());
-#elif defined(OPENVINO_ARCH_ARM64)
-        // Add GemmCPUBlocking after MarkLoops
-        backend_passes.emplace_back(
-            ov::snippets::pass::PassPosition(ov::snippets::pass::PassPosition::Place::After,
-                                             ov::snippets::lowered::pass::MarkLoops::get_type_info_static()),
-            std::make_shared<ov::intel_cpu::pass::GemmCPUBlocking>());
-
-        // Add InsertGemmCopyBuffers after SplitLoops
-        backend_passes.emplace_back(
-            ov::snippets::pass::PassPosition(ov::snippets::pass::PassPosition::Place::After,
-                                             ov::snippets::lowered::pass::SplitLoops::get_type_info_static()),
-            std::make_shared<ov::intel_cpu::pass::aarch64::InsertGemmCopyBuffers>());
-#endif
-
-        return backend_passes;
     }
 
     static void MarkOp(const std::shared_ptr<ov::Node>& node,
@@ -95,7 +59,25 @@ protected:
     }
 };
 
-class MHAFP32BufferAllocationTest : public BufferAllocationCPUTest {
+#if defined(OPENVINO_ARCH_X86_64)
+
+using BrgemmConfig = intel_cpu::brgemm_utils::BrgemmConfig;
+
+class X64BufferAllocationCPUTest : public BufferAllocationCPUTest {
+protected:
+    std::vector<ov::snippets::lowered::pass::PassPipeline::PositionedPassLowered> getBackendSpecificPasses() override {
+        return {
+            {ov::snippets::pass::PassPosition(ov::snippets::pass::PassPosition::Place::After,
+                                              ov::snippets::lowered::pass::MarkLoops::get_type_info_static()),
+             std::make_shared<ov::intel_cpu::pass::BrgemmCPUBlocking>()},
+            {ov::snippets::pass::PassPosition(ov::snippets::pass::PassPosition::Place::After,
+                                              ov::snippets::lowered::pass::SplitLoops::get_type_info_static()),
+             std::make_shared<ov::intel_cpu::pass::InsertBrgemmCopyBuffers>()},
+        };
+    }
+};
+
+class MHAFP32BufferAllocationTest : public X64BufferAllocationCPUTest {
 protected:
     std::shared_ptr<ov::Model> GetModel(const std::vector<ov::PartialShape>& shapes) const override {
         const auto subtensor_scalar = std::vector<size_t>{1};
@@ -112,7 +94,6 @@ protected:
         const auto load_reshape = std::make_shared<ov::snippets::op::LoadReorder>(parameter1, 1, 0, order);
         const auto store = std::make_shared<ov::snippets::op::Store>(load_reshape);
         const auto relu0 = std::make_shared<ov::op::v0::Relu>(store);
-#if defined(OPENVINO_ARCH_X86_64)
         const BrgemmConfig brgemm_config(dnnl::impl::cpu::x64::cpu_isa_t::avx512_core,
                                          ov::element::f32,
                                          ov::element::f32,
@@ -123,14 +104,6 @@ protected:
             std::make_shared<ov::intel_cpu::BrgemmCPU>(OutputVector{parameter0, relu0}, brgemm_config);
 
         const auto relu1 = std::make_shared<ov::op::v0::Relu>(brgemm_cpu0);
-#elif defined(OPENVINO_ARCH_ARM64)
-        using PortDescriptor = ov::snippets::modifier::MemoryAccess::PortDescriptor;
-        const auto gemm_cpu0 =
-            std::make_shared<ov::intel_cpu::aarch64::GemmCPU>(parameter0, relu0,
-                                                               PortDescriptor{}, PortDescriptor{}, PortDescriptor{});
-
-        const auto relu1 = std::make_shared<ov::op::v0::Relu>(gemm_cpu0);
-#endif
 
         // Decomposed Softmax
         const auto reduce_max = std::make_shared<ov::snippets::op::ReduceMax>(relu1, 3);
@@ -143,18 +116,10 @@ protected:
         const auto power = std::make_shared<ov::snippets::op::PowerStatic>(reduce_sum, -1.f);
         const auto multiply = std::make_shared<ov::op::v1::Multiply>(exp, power);
 
-#if defined(OPENVINO_ARCH_X86_64)
         const auto brgemm_cpu1 =
             std::make_shared<ov::intel_cpu::BrgemmCPU>(OutputVector{multiply, parameter2}, brgemm_config);
 
         const auto relu2 = std::make_shared<ov::op::v0::Relu>(brgemm_cpu1);
-#elif defined(OPENVINO_ARCH_ARM64)
-        const auto gemm_cpu1 =
-            std::make_shared<ov::intel_cpu::aarch64::GemmCPU>(multiply, parameter2,
-                                                               PortDescriptor{}, PortDescriptor{}, PortDescriptor{});
-
-        const auto relu2 = std::make_shared<ov::op::v0::Relu>(gemm_cpu1);
-#endif
 
         const auto body = std::make_shared<ov::Model>(std::make_shared<ov::snippets::op::Result>(relu2), ov::ParameterVector{parameter0, parameter1, parameter2});
 
@@ -162,13 +127,8 @@ protected:
         MarkOp(store, {subtensor_scalar}, {subtensor_scalar});
         MarkOp(power, {subtensor_power}, {subtensor_power});
 
-#if defined(OPENVINO_ARCH_X86_64)
         MarkOp(brgemm_cpu0, {subtensor_full, subtensor_full}, {subtensor_full});
         MarkOp(brgemm_cpu1, {subtensor_full, subtensor_full}, {subtensor_full});
-#elif defined(OPENVINO_ARCH_ARM64)
-        MarkOp(gemm_cpu0, {subtensor_full, subtensor_full}, {subtensor_full});
-        MarkOp(gemm_cpu1, {subtensor_full, subtensor_full}, {subtensor_full});
-#endif
 
         ov::snippets::lowered::PortDescriptorUtils::get_port_descriptor_ptr(load_reshape->input(0))->set_layout(order);
 
@@ -176,8 +136,7 @@ protected:
     }
 };
 
-#if defined(OPENVINO_ARCH_X86_64)
-class MHABF16AMXBufferAllocationTest : public BufferAllocationCPUTest {
+class MHABF16AMXBufferAllocationTest : public X64BufferAllocationCPUTest {
 protected:
     std::shared_ptr<ov::Model> GetModel(const std::vector<ov::PartialShape>& shapes) const override {
         const auto subtensor_scalar = std::vector<size_t>{1};
@@ -251,7 +210,85 @@ protected:
         return body;
     }
 };
-#endif  // OPENVINO_ARCH_X86_64
+
+#elif defined(OPENVINO_ARCH_ARM64)
+
+class Aarch64BufferAllocationCPUTest : public BufferAllocationCPUTest {
+protected:
+    std::vector<ov::snippets::lowered::pass::PassPipeline::PositionedPassLowered> getBackendSpecificPasses() override {
+        return {
+            {ov::snippets::pass::PassPosition(ov::snippets::pass::PassPosition::Place::After,
+                                              ov::snippets::lowered::pass::MarkLoops::get_type_info_static()),
+             std::make_shared<ov::intel_cpu::pass::GemmCPUBlocking>()},
+            {ov::snippets::pass::PassPosition(ov::snippets::pass::PassPosition::Place::After,
+                                              ov::snippets::lowered::pass::SplitLoops::get_type_info_static()),
+             std::make_shared<ov::intel_cpu::pass::aarch64::InsertGemmCopyBuffers>()},
+        };
+    }
+};
+
+class MHAFP32BufferAllocationTest : public Aarch64BufferAllocationCPUTest {
+protected:
+    std::shared_ptr<ov::Model> GetModel(const std::vector<ov::PartialShape>& shapes) const override {
+        const auto subtensor_scalar = std::vector<size_t>{1};
+        const auto subtensor_power = std::vector<size_t>{1, ov::snippets::utils::get_full_dim_value()};
+        const auto subtensor_full = std::vector<size_t>(2, ov::snippets::utils::get_full_dim_value());
+
+        // Dims are selected in order to have blocking loops by each dim
+        OPENVINO_ASSERT(shapes.size() == 3, "Incorrect count of input shapes");
+        const auto parameter0 = std::make_shared<ov::op::v0::Parameter>(ov::element::f32, shapes[0]);
+        const auto parameter1 = std::make_shared<ov::op::v0::Parameter>(ov::element::f32, shapes[1]);
+        const auto parameter2 = std::make_shared<ov::op::v0::Parameter>(ov::element::f32, shapes[2]);
+
+        const auto order = std::vector<size_t>{0, 2, 3, 1};
+        const auto load_reshape = std::make_shared<ov::snippets::op::LoadReorder>(parameter1, 1, 0, order);
+        const auto store = std::make_shared<ov::snippets::op::Store>(load_reshape);
+        const auto relu0 = std::make_shared<ov::op::v0::Relu>(store);
+
+        using PortDescriptor = ov::snippets::modifier::MemoryAccess::PortDescriptor;
+        const auto gemm_cpu0 = std::make_shared<ov::intel_cpu::aarch64::GemmCPU>(parameter0,
+                                                                                 relu0,
+                                                                                 PortDescriptor{},
+                                                                                 PortDescriptor{},
+                                                                                 PortDescriptor{});
+
+        const auto relu1 = std::make_shared<ov::op::v0::Relu>(gemm_cpu0);
+
+        // Decomposed Softmax
+        const auto reduce_max = std::make_shared<ov::snippets::op::ReduceMax>(relu1, 3);
+        ov::snippets::op::ReduceBase::compute_and_set_reduce_subtensors(reduce_max);
+        const auto subtract = std::make_shared<ov::op::v1::Subtract>(relu1, reduce_max);
+        const auto exp = std::make_shared<ov::op::v0::Exp>(subtract);
+
+        const auto reduce_sum = std::make_shared<ov::snippets::op::ReduceSum>(exp, 3);
+        ov::snippets::op::ReduceBase::compute_and_set_reduce_subtensors(reduce_sum);
+        const auto power = std::make_shared<ov::snippets::op::PowerStatic>(reduce_sum, -1.f);
+        const auto multiply = std::make_shared<ov::op::v1::Multiply>(exp, power);
+
+        const auto gemm_cpu1 = std::make_shared<ov::intel_cpu::aarch64::GemmCPU>(multiply,
+                                                                                 parameter2,
+                                                                                 PortDescriptor{},
+                                                                                 PortDescriptor{},
+                                                                                 PortDescriptor{});
+
+        const auto relu2 = std::make_shared<ov::op::v0::Relu>(gemm_cpu1);
+
+        const auto body = std::make_shared<ov::Model>(std::make_shared<ov::snippets::op::Result>(relu2),
+                                                      ov::ParameterVector{parameter0, parameter1, parameter2});
+
+        MarkOp(load_reshape, {subtensor_scalar}, {subtensor_scalar});
+        MarkOp(store, {subtensor_scalar}, {subtensor_scalar});
+        MarkOp(power, {subtensor_power}, {subtensor_power});
+        MarkOp(gemm_cpu0, {subtensor_full, subtensor_full}, {subtensor_full});
+        MarkOp(gemm_cpu1, {subtensor_full, subtensor_full}, {subtensor_full});
+
+        ov::snippets::lowered::PortDescriptorUtils::get_port_descriptor_ptr(load_reshape->input(0))->set_layout(order);
+
+        return body;
+    }
+};
+
+#endif  // OPENVINO_ARCH_X86_64 || OPENVINO_ARCH_ARM64
 
 TEST_P(MHAFP32BufferAllocationTest, BufferAllocationCPU) {
     Validate();
