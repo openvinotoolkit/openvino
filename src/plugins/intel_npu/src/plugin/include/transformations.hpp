afe4771776a25909720034dad8143b98530e628d
Liu, Dan1 <dan1.liu@intel.com>
2026-09-24 10:44:09 +0800

    fix build issue

--- src/plugins/intel_npu/src/plugin/include/transformations.hpp
+++ src/plugins/intel_npu/src/plugin/include/transformations.hpp
@@ -10,7 +10,6 @@
 #include <set>
 #include <string>
 
-#include "intel_npu/common/filtered_config.hpp"
 #include "intel_npu/config/config.hpp"
 #include "intel_npu/config/options.hpp"
 #include "intel_npu/utils/logger/logger.hpp"
@@ -20,7 +19,7 @@
 namespace intel_npu {
 
 void enable_host_compile_if_needed(const std::shared_ptr<const ov::Model>& model,
-                                   FilteredConfig& config,
+                                   Config& config,
                                    const Logger& logger);
 
 namespace batch_helpers {
